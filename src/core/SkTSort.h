eff416bec1bc08685af1dc4265b9d860e43b8240
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2009-03-18 03:08:15 +0000

    fix heapsort
    git-svn-id: http://skia.googlecode.com/svn/trunk@126 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkTSort.h
+++ src/core/SkTSort.h
@@ -21,44 +21,29 @@
 #include "SkTypes.h"
 
 template <typename T>
-void SkTHeapSort_SiftDown(T array[], int root, int bottom)
-{
-    int root2 = root << 1;
-
-    while (root2 <= bottom)
-    {
-        int maxChild;
-
-        if (root2 == bottom)
-            maxChild = root2;
-        else if (array[root2] > array[root2 + 1])
-            maxChild = root2;
-        else
-            maxChild = root2 + 1;
-
-        if (array[root] < array[maxChild])
-        {
-            SkTSwap<T>(array[root], array[maxChild]);
-            root = maxChild;
-            root2 = root << 1;
+void SkTHeapSort_SiftDown(T array[], int root, int bottom) {
+    while (root*2 + 1 <= bottom) {
+        int child = root * 2 + 1;
+        if (child+1 <= bottom && array[child] < array[child+1]) {
+            child += 1;
         }
-        else
+        if (array[root] < array[child]) {
+            SkTSwap<T>(array[root], array[child]);
+            root = child;
+        } else {
             break;
+        }
     }
 }
 
-template <typename T>
-void SkTHeapSort(T array[], int count)
-{
+template <typename T> void SkTHeapSort(T array[], int count) {
     int i;
-
-    for (i = count/2 - 1; i >= 0; --i)
-        SkTHeapSort_SiftDown<T>(array, i, count);
-
-    for (i = count - 2; i >= 0; --i)
-    {
-        SkTSwap<T>(array[0], array[i + 1]);
-        SkTHeapSort_SiftDown<T>(array, 0, i);
+    for (i = count/2 - 1; i >= 0; --i) {
+        SkTHeapSort_SiftDown<T>(array, i, count-1);
+    }
+    for (i = count - 1; i > 0; --i) {
+        SkTSwap<T>(array[0], array[i]);
+        SkTHeapSort_SiftDown<T>(array, 0, i-1);
     }
 }
 
--- tests/SortTest.cpp
+++ tests/SortTest.cpp
@@ -1,6 +1,7 @@
 #include "Test.h"
 #include "SkRandom.h"
 #include "SkTSearch.h"
+#include "SkTSort.h"
 
 extern "C" {
     int compare_int(const void* a, const void* b) {
@@ -8,19 +9,38 @@ extern "C" {
     }
 }
 
+static void rand_array(SkRandom& rand, int array[], int n) {
+    for (int j = 0; j < n; j++) {
+        array[j] = rand.nextS() & 0xFF;
+    }
+}
+
+static void check_sort(skiatest::Reporter* reporter, const char label[],
+                       const int array[], int n) {
+    for (int j = 1; j < n; j++) {
+        if (array[j-1] > array[j]) {
+            SkString str;
+           str.printf("%sSort [%d] failed %d %d", label, n,
+                      array[j-1], array[j]);
+            reporter->reportFailed(str);
+        }
+    }
+}
+
 static void TestSort(skiatest::Reporter* reporter) {
-    int         array[100];
+    int         array[500];
     SkRandom    rand;
 
-    for (int i = 0; i < 1000; i++) {
-        int j, count = rand.nextRangeU(1, SK_ARRAY_COUNT(array));
-        for (j = 0; j < count; j++) {
-            array[j] = rand.nextS() & 0xFF;
-        }
+    for (int i = 0; i < 10000; i++) {
+        int count = rand.nextRangeU(1, SK_ARRAY_COUNT(array));
+
+        rand_array(rand, array, count);
         SkQSort(array, count, sizeof(int), compare_int);
-        for (j = 1; j < count; j++) {
-            REPORTER_ASSERT(reporter, array[j-1] <= array[j]);
-        }
+        check_sort(reporter, "Quick", array, count);
+
+        rand_array(rand, array, count);
+        SkTHeapSort<int>(array, count);
+        check_sort(reporter, "Heap", array, count);
     }
 }
 
