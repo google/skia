289e4fca4394ca0964b9b76a834e61bcdec821c9
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2009-08-31 18:04:51 +0000

    add missing files
    git-svn-id: http://skia.googlecode.com/svn/trunk@345 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ forth/ForthParser.h
@@ -0,0 +1,35 @@
+#ifndef ForthParser_DEFINED
+#define ForthParser_DEFINED
+
+#include "SkTDict.h"
+
+class ForthWord;
+class FCode;
+
+class ForthParser {
+public:
+    ForthParser();
+    ~ForthParser();
+
+    const char* parse(const char text[], FCode*);
+
+    void addWord(const char name[], ForthWord* word) {
+        this->add(name, strlen(name), word);
+    }
+
+    void add(const char name[], size_t len, ForthWord* word) {
+        (void)fDict.set(name, len, word);
+    }
+
+    ForthWord* find(const char name[], size_t len) const {
+        ForthWord* word;
+        return fDict.find(name, len, &word) ? word : NULL;
+    }
+
+private:
+    void addStdWords();
+
+    SkTDict<ForthWord*> fDict;
+};
+
+#endif
