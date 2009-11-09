7079a6d2495b85025a13ad2208d9c2a4071daf6e
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2009-11-09 16:01:36 +0000

    update dox for SkAvoidXfermode (as best I could)
    fix 565 and 4444 implementations
    
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@420 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/effects/SkAvoidXfermode.h
+++ include/effects/SkAvoidXfermode.h
@@ -31,16 +31,21 @@ public:
         kTargetColor_Mode   //!< draw only on top of the opColor
     };
 
-    /** This xfermode will draw the src everywhere except on top of the opColor
-        or, depending on the Mode, draw only on top of the opColor.
-        @param opColor  the color to avoid (or to target depending on Mode).
-                        note: the alpha in opColor is ignored
-        @param tolerance    How closely we compare a pixel to the opColor.
-                            0 - only operate if exact match
-                            255 - maximum gradation (blending) based on how
-                            similar the pixel is to our opColor (max tolerance)
-        @param mode If we should avoid or target the opColor
-    */
+    /** This xfermode draws, or doesn't draw, based on the destination's
+        distance from an op-color.
+     
+        There are two modes, and each mode interprets a tolerance value.
+     
+        Avoid: In this mode, drawing is allowed only on destination pixels that
+               are different from the op-color.
+               Tolerance near 0: avoid anything close to the op-color
+               Tolerance near 255: avoid only colors very close to the op-color
+     
+        Target: In this mode, drawing only occurs on destination pixels that
+                are similar to the op-color
+                Tolerance near 0: draw on colors that are very close to op-color
+                Tolerance near 255: draw on colors that  to the op-color
+     */
     SkAvoidXfermode(SkColor opColor, U8CPU tolerance, Mode mode);
 
     // overrides from SkXfermode
