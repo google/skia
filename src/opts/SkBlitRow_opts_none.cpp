f0f4e9abba62a405b7a41e40fcee20b45eb348ee
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2009-11-13 19:00:49 +0000

    remove const modifiers on function return types (unneeded, and caused an error on some gccs).
    git-svn-id: http://skia.googlecode.com/svn/trunk@425 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/opts/SkBlitRow_opts_none.cpp
+++ src/opts/SkBlitRow_opts_none.cpp
@@ -2,14 +2,14 @@
 
 // Platform impl of Platform_procs with no overrides
 
-const SkBlitRow::Proc SkBlitRow::PlatformProcs4444(unsigned flags) {
+SkBlitRow::Proc SkBlitRow::PlatformProcs4444(unsigned flags) {
     return NULL;
 }
 
-const SkBlitRow::Proc SkBlitRow::PlatformProcs565(unsigned flags) {
+SkBlitRow::Proc SkBlitRow::PlatformProcs565(unsigned flags) {
     return NULL;
 }
 
-const SkBlitRow::Proc32 SkBlitRow::PlatformProcs32(unsigned flags) {
+SkBlitRow::Proc32 SkBlitRow::PlatformProcs32(unsigned flags) {
     return NULL;
 }
