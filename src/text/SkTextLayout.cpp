4b4637172ea9b807f463471a501298f72486eec8
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2010-02-11 13:56:04 +0000

    experimental draft, please ignore
    git-svn-id: http://skia.googlecode.com/svn/trunk@499 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ src/text/SkTextLayout.cpp
@@ -0,0 +1,72 @@
+#include "SkTextLayout.h"
+
+SkTextStyle::SkTextStyle() {
+    fPaint.setAntiAlias(true);
+}
+
+SkTextStyle::SkTextStyle(const SkTextStyle& src) : fPaint(src.fPaint) {}
+
+SkTextStyle::SkTextStyle(const SkPaint& paint) : fPaint(paint) {}
+
+SkTextStyle::~SkTextStyle() {}
+
+///////////////////////////////////////////////////////////////////////////////
+
+SkTextLayout::SkTextLayout() {
+    fBounds.setEmpty();
+    fDefaultStyle = new SkTextStyle;
+}
+
+SkTextLayout::~SkTextLayout() {
+    fDefaultStyle->unref();
+    fLines.deleteAll();
+}
+
+void SkTextLayout::setText(const char text[], size_t length) {
+    fText.setCount(length);
+    memcpy(fText.begin(), text, length);
+}
+
+void SkTextLayout::setBounds(const SkRect& bounds) {
+    fBounds = bounds;
+    // if width changed, inval cache
+}
+
+SkTextStyle* SkTextLayout::setDefaultStyle(SkTextStyle* style) {
+    SkRefCnt_SafeAssign(fDefaultStyle, style);
+    return style;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+struct SkTextLayout::GlyphRun {
+    GlyphRun();
+    ~GlyphRun();
+
+    SkPoint*    fLocs;
+    uint16_t*   fGlyphIDs;
+    int         fCount;
+};
+
+SkTextLayout::GlyphRun::GlyphRun() : fLocs(NULL), fGlyphIDs(NULL), fCount(0) {}
+
+SkTextLayout::GlyphRun::~GlyphRun() {
+    delete[] fLocs;
+    delete[] fGlyphIDs;
+}
+
+struct SkTextLayout::Line {
+    Line() {}
+    ~Line();
+
+    SkScalar                fBaselineY;
+    SkTDArray<GlyphRun*>    fRuns;
+};
+
+SkTextLayout::Line::~Line() {
+    fRuns.deleteAll();
+}
+
+void SkTextLayout::draw(SkCanvas* canvas) {
+}
+
