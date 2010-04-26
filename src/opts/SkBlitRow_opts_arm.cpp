e932a42ddbebb3ad8303fed8e58db6a68590082d
agl@chromium.org <agl@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2010-04-26 21:45:08 +0000

    Fix internal compiler error on ARM.
    For the moment, at least, switch back to the old Neon code because the
    new code is triggering internal compiler errors in Chromium ARM.
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@559 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/opts/SkBlitRow_opts_arm.cpp
+++ src/opts/SkBlitRow_opts_arm.cpp
@@ -770,7 +770,7 @@ static void S32A_D565_Opaque_Dither_neon (uint16_t * SK_RESTRICT dst,
 
 	    /* calculate 'd', which will be 0..7 */
 	    /* dbase[] is 0..7; alpha is 0..256; 16 bits suffice */
-#if 1
+#if ANDROID
 	    /* SkAlpha255To256() semantic a+1 vs a+a>>7 */
 	    alpha8 = vaddw_u8(vmovl_u8(sa), vdup_n_u8(1));
 #else
