a0b60b62e657e3a1bde165ca61c8232223ae9dd8
reed@android.com <reed@android.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2010-09-09 16:07:53 +0000

    move SkMallocPixelRef into its own .cpp file, rather than embedding it inside SkBitmap.cpp.
    core_files.mk updated, but if your build does not reference that, you will
    need to add SkMallocPixelRef.cpp to your list
    
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@597 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkBitmap.cpp
+++ src/core/SkBitmap.cpp
@@ -377,56 +377,6 @@ SkGpuTexture* SkBitmap::getTexture() const {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-SkMallocPixelRef::SkMallocPixelRef(void* storage, size_t size,
-                                   SkColorTable* ctable) {
-    SkASSERT(storage);
-    fStorage = storage;
-    fSize = size;
-    fCTable = ctable;
-    ctable->safeRef();
-}
-
-SkMallocPixelRef::~SkMallocPixelRef() {
-    SkSafeUnref(fCTable);
-    sk_free(fStorage);
-}
-
-void* SkMallocPixelRef::onLockPixels(SkColorTable** ct) {
-    *ct = fCTable;
-    return fStorage;
-}
-
-void SkMallocPixelRef::onUnlockPixels() {
-    // nothing to do
-}
-
-void SkMallocPixelRef::flatten(SkFlattenableWriteBuffer& buffer) const {
-    this->INHERITED::flatten(buffer);
-
-    buffer.write32(fSize);
-    buffer.writePad(fStorage, fSize);
-    if (fCTable) {
-        buffer.writeBool(true);
-        fCTable->flatten(buffer);
-    } else {
-        buffer.writeBool(false);
-    }
-}
-
-SkMallocPixelRef::SkMallocPixelRef(SkFlattenableReadBuffer& buffer) : INHERITED(buffer, NULL) {
-    fSize = buffer.readU32();
-    fStorage = sk_malloc_throw(fSize);
-    buffer.read(fStorage, fSize);
-    if (buffer.readBool()) {
-        fCTable = SkNEW_ARGS(SkColorTable, (buffer));
-    } else {
-        fCTable = NULL;
-    }
-}
-
-static SkPixelRef::Registrar reg("SkMallocPixelRef",
-                                 SkMallocPixelRef::Create);
-
 /** We explicitly use the same allocator for our pixels that SkMask does,
  so that we can freely assign memory allocated by one class to the other.
  */
