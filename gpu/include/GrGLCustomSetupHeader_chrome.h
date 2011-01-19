e3c64c599ed7b4f7d441c7eb0b10012381a47e38
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-01-19 17:35:08 +0000

    add experimental custom GL header for chrome
    git-svn-id: http://skia.googlecode.com/svn/trunk@714 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ gpu/include/GrGLCustomSetupHeader_chrome.h
@@ -0,0 +1,12 @@
+#ifndef Chrome_GR_GL_CUSTOM_SETUP_HEADER
+#define Chrome_GR_GL_CUSTOM_SETUP_HEADER
+
+#define GR_INCLUDE_GLES2        <GLES2/gl2.h>
+#define GR_INCLUDE_GLES2ext     <GLES2/gl2ext.h>
+
+#define GR_GL_FUNC
+
+#define GR_GL_PROC_ADDRESS(X)       &X
+#define GR_GL_PROC_ADDRESS_HEADER
+
+#endif
