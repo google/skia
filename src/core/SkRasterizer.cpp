feb8cc870df055099c6fcdfebdd6c1486bd91b27
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-04-19 20:11:25 +0000

    code style
    git-svn-id: http://skia.googlecode.com/svn/trunk@1153 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkRasterizer.cpp
+++ src/core/SkRasterizer.cpp
@@ -25,21 +25,19 @@ SkRasterizer::SkRasterizer(SkFlattenableReadBuffer&) {}
 
 bool SkRasterizer::rasterize(const SkPath& fillPath, const SkMatrix& matrix,
                              const SkIRect* clipBounds, SkMaskFilter* filter,
-                             SkMask* mask, SkMask::CreateMode mode)
-{
+                             SkMask* mask, SkMask::CreateMode mode) {
     SkIRect storage;
     
-    if (clipBounds && filter && SkMask::kJustRenderImage_CreateMode != mode)
-    {        
+    if (clipBounds && filter && SkMask::kJustRenderImage_CreateMode != mode) {        
         SkIPoint    margin;
         SkMask      srcM, dstM;
         
         srcM.fFormat = SkMask::kA8_Format;
         srcM.fBounds.set(0, 0, 1, 1);
         srcM.fImage = NULL;
-        if (!filter->filterMask(&dstM, srcM, matrix, &margin))
+        if (!filter->filterMask(&dstM, srcM, matrix, &margin)) {
             return false;
-        
+        }
         storage = *clipBounds;
         storage.inset(-margin.fX, -margin.fY);
         clipBounds = &storage;
@@ -52,8 +50,7 @@ bool SkRasterizer::rasterize(const SkPath& fillPath, const SkMatrix& matrix,
 */
 bool SkRasterizer::onRasterize(const SkPath& fillPath, const SkMatrix& matrix,
                              const SkIRect* clipBounds,
-                             SkMask* mask, SkMask::CreateMode mode)
-{
+                             SkMask* mask, SkMask::CreateMode mode) {
     SkPath  devPath;
     
     fillPath.transform(matrix, &devPath);
