1ed5c7cb32ef34f5abd419224c9370126750a2f2
mike@reedtribe.org <mike@reedtribe.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-04-20 11:01:37 +0000

    code style
    git-svn-id: http://skia.googlecode.com/svn/trunk@1157 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkBlitter_Sprite.cpp
+++ src/core/SkBlitter_Sprite.cpp
@@ -18,19 +18,16 @@
 #include "SkSpriteBlitter.h"
 
 SkSpriteBlitter::SkSpriteBlitter(const SkBitmap& source)
-    : fSource(&source)
-{
+        : fSource(&source) {
     fSource->lockPixels();
 }
 
-SkSpriteBlitter::~SkSpriteBlitter()
-{
+SkSpriteBlitter::~SkSpriteBlitter() {
     fSource->unlockPixels();
 }
 
 void SkSpriteBlitter::setup(const SkBitmap& device, int left, int top,
-                            const SkPaint& paint)
-{
+                            const SkPaint& paint) {
     fDevice = &device;
     fLeft = left;
     fTop = top;
@@ -38,29 +35,25 @@ void SkSpriteBlitter::setup(const SkBitmap& device, int left, int top,
 }
 
 #ifdef SK_DEBUG
-void SkSpriteBlitter::blitH(int x, int y, int width)
-{
+void SkSpriteBlitter::blitH(int x, int y, int width) {
     SkASSERT(!"how did we get here?");
 }
 
-void SkSpriteBlitter::blitAntiH(int x, int y, const SkAlpha antialias[], const int16_t runs[])
-{
+void SkSpriteBlitter::blitAntiH(int x, int y, const SkAlpha antialias[],
+                                const int16_t runs[]) {
     SkASSERT(!"how did we get here?");
 }
 
-void SkSpriteBlitter::blitV(int x, int y, int height, SkAlpha alpha)
-{
+void SkSpriteBlitter::blitV(int x, int y, int height, SkAlpha alpha) {
     SkASSERT(!"how did we get here?");
 }
 
-void SkSpriteBlitter::blitMask(const SkMask&, const SkIRect& clip)
-{
+void SkSpriteBlitter::blitMask(const SkMask&, const SkIRect& clip) {
     SkASSERT(!"how did we get here?");
 }
 #endif
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 // returning null means the caller will call SkBlitter::Choose() and
 // have wrapped the source bitmap inside a shader
@@ -68,8 +61,7 @@ SkBlitter* SkBlitter::ChooseSprite( const SkBitmap& device,
                                     const SkPaint& paint,
                                     const SkBitmap& source,
                                     int left, int top,
-                                    void* storage, size_t storageSize)
-{
+                                    void* storage, size_t storageSize) {
     /*  We currently ignore antialiasing and filtertype, meaning we will take our
         special blitters regardless of these settings. Ignoring filtertype seems fine
         since by definition there is no scale in the matrix. Ignoring antialiasing is
@@ -83,19 +75,22 @@ SkBlitter* SkBlitter::ChooseSprite( const SkBitmap& device,
     SkSpriteBlitter* blitter;
 
     switch (device.getConfig()) {
-    case SkBitmap::kRGB_565_Config:
-        blitter = SkSpriteBlitter::ChooseD16(source, paint, storage, storageSize);
-        break;
-    case SkBitmap::kARGB_8888_Config:
-        blitter = SkSpriteBlitter::ChooseD32(source, paint, storage, storageSize);
-        break;
-    default:
-        blitter = NULL;
-        break;
+        case SkBitmap::kRGB_565_Config:
+            blitter = SkSpriteBlitter::ChooseD16(source, paint, storage,
+                                                 storageSize);
+            break;
+        case SkBitmap::kARGB_8888_Config:
+            blitter = SkSpriteBlitter::ChooseD32(source, paint, storage,
+                                                 storageSize);
+            break;
+        default:
+            blitter = NULL;
+            break;
     }
 
-    if (blitter)
+    if (blitter) {
         blitter->setup(device, left, top, paint);
+    }
     return blitter;
 }
 
