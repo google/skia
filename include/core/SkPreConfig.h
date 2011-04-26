2f99d8bde3a3bc7e873a14add91ecbe098ec8fd6
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-04-26 20:04:16 +0000

    http://codereview.appspot.com/4438070/
    update SK_API for non-windows builds
    
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@1192 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkPreConfig.h
+++ include/core/SkPreConfig.h
@@ -101,12 +101,16 @@
 #if !defined(SKIA_IMPLEMENTATION)
     #define SKIA_IMPLEMENTATION 0
 #endif
- 
-#if defined(WIN32) && defined(SKIA_DLL)
-    #if SKIA_IMPLEMENTATION
-        #define SK_API __declspec(dllexport)
+
+#if defined(SKIA_DLL)
+    #if defined(WIN32)
+        #if SKIA_IMPLEMENTATION
+            #define SK_API __declspec(dllexport)
+        #else
+            #define SK_API __declspec(dllimport)
+        #endif
     #else
-        #define SK_API __declspec(dllimport)
+        #define SK_API __attribute__((visibility("default")))
     #endif
 #else
     #define SK_API
