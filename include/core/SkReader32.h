81347b7953913f6b06edbff5a431e06c7054c57b
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-04-26 17:49:03 +0000

    add helper available()
    git-svn-id: http://skia.googlecode.com/svn/trunk@1188 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkReader32.h
+++ include/core/SkReader32.h
@@ -43,6 +43,10 @@ public:
     bool eof() const { return fCurr >= fStop; }
     const void* base() const { return fBase; }
     const void* peek() const { return fCurr; }
+
+    uint32_t available() const { return fStop - fCurr; }
+    bool isAvailable(uint32_t size) const { return fCurr + size <= fStop; }
+    
     void rewind() { fCurr = fBase; }
 
     void setOffset(size_t offset) {
