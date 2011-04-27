31a5840ac8cf83b9453f3937a02101661cc8fc7f
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-04-27 21:00:02 +0000

    Accidentally omitted this file from r1203, :(
    git-svn-id: http://skia.googlecode.com/svn/trunk@1204 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/SkGpuDevice.cpp
+++ src/gpu/SkGpuDevice.cpp
@@ -327,7 +327,7 @@ void SkGpuDevice::gainFocus(SkCanvas* canvas, const SkMatrix& matrix,
     convert_matrixclip(fContext, matrix, clipStack, clip, this->getOrigin());
 
     if (fNeedClear) {
-        fContext->clear(0x0);
+        fContext->clear(NULL, 0x0);
         fNeedClear = false;
     }
 }
@@ -596,7 +596,7 @@ private:
 ///////////////////////////////////////////////////////////////////////////////
 
 void SkGpuDevice::clear(SkColor color) {
-    fContext->clear(color);
+    fContext->clear(NULL, color);
 }
 
 void SkGpuDevice::drawPaint(const SkDraw& draw, const SkPaint& paint) {
