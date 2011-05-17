16bab87a78cfaf6a4f334e1af910c46883f460af
bungeman@google.com <bungeman@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-05-17 14:24:46 +0000

    Add GL context creation for X so that gm can run GPU on Linux. http://codereview.appspot.com/4548047/
    git-svn-id: http://skia.googlecode.com/svn/trunk@1343 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/utils/SkEGLContext.h
+++ include/utils/SkEGLContext.h
@@ -1,20 +1,36 @@
 #ifndef SkEGLContext_DEFINED
 #define SkEGLContext_DEFINED
 
-#include "SkTypes.h"
+#if defined(SK_BUILD_FOR_MAC)
+    #include <AGL/agl.h>
+#elif defined(SK_BUILD_FOR_UNIX)
+    #include <X11/Xlib.h>
+    #include <GL/glx.h>
+#else
+
+#endif
 
 /**
  *  Create an offscreen opengl context
  */
 class SkEGLContext {
 public:
-	SkEGLContext();
-	~SkEGLContext();
+    SkEGLContext();
+    ~SkEGLContext();
 
-	bool init(int width, int height);
+    bool init(int width, int height);
 
 private:
-	void* fContext;
+#if defined(SK_BUILD_FOR_MAC)
+    AGLContext context;
+#elif defined(SK_BUILD_FOR_UNIX)
+    GLXContext context;
+    Display *display;
+    Pixmap pixmap;
+    GLXPixmap glxPixmap;
+#else
+
+#endif
 };
 
 #endif
--- src/utils/mac/SkEGLContext_mac.cpp
+++ src/utils/mac/SkEGLContext_mac.cpp
@@ -1,59 +1,60 @@
 #include "SkEGLContext.h"
+//#include "SkTypes.h"
 #include <AGL/agl.h>
 
-SkEGLContext::SkEGLContext() : fContext(NULL) {
+SkEGLContext::SkEGLContext() : context(NULL) {
 }
 
 SkEGLContext::~SkEGLContext() {
-	if (fContext) {
-		aglDestroyContext((AGLContext)fContext);
-	}
+    if (this->context) {
+        aglDestroyContext(this->context);
+    }
 }
 
 bool SkEGLContext::init(int width, int height) {
-	GLint major, minor;
-	AGLContext ctx;
+    GLint major, minor;
+    AGLContext ctx;
 
-	aglGetVersion(&major, &minor);
-//	SkDebugf("---- agl version %d %d\n", major, minor);
+    aglGetVersion(&major, &minor);
+    //SkDebugf("---- agl version %d %d\n", major, minor);
 
-	const GLint pixelAttrs[] = {
-		AGL_RGBA,
-		AGL_STENCIL_SIZE, 8,
+    const GLint pixelAttrs[] = {
+        AGL_RGBA,
+        AGL_STENCIL_SIZE, 8,
 /*
-		AGL_SAMPLE_BUFFERS_ARB, 1,
-		AGL_MULTISAMPLE,
-		AGL_SAMPLES_ARB, 2,
+        AGL_SAMPLE_BUFFERS_ARB, 1,
+        AGL_MULTISAMPLE,
+        AGL_SAMPLES_ARB, 2,
 */
-		AGL_ACCELERATED,
-		AGL_NONE
-	};
-	AGLPixelFormat format = aglChoosePixelFormat(NULL, 0, pixelAttrs);
-	//AGLPixelFormat format = aglCreatePixelFormat(pixelAttrs);
-//	SkDebugf("----- agl format %p\n", format);
-	ctx = aglCreateContext(format, NULL);
-//	SkDebugf("----- agl context %p\n", ctx);
-	aglDestroyPixelFormat(format);
+        AGL_ACCELERATED,
+        AGL_NONE
+    };
+    AGLPixelFormat format = aglChoosePixelFormat(NULL, 0, pixelAttrs);
+    //AGLPixelFormat format = aglCreatePixelFormat(pixelAttrs);
+    //SkDebugf("----- agl format %p\n", format);
+    ctx = aglCreateContext(format, NULL);
+    //SkDebugf("----- agl context %p\n", ctx);
+    aglDestroyPixelFormat(format);
 
 /*
-	static const GLint interval = 1;
-	aglSetInteger(ctx, AGL_SWAP_INTERVAL, &interval);
+    static const GLint interval = 1;
+    aglSetInteger(ctx, AGL_SWAP_INTERVAL, &interval);
 */
 
-	aglSetCurrentContext(ctx);
-	fContext = (void*)ctx;
+    aglSetCurrentContext(ctx);
+    this->context = ctx;
 
-	// Now create our FBO render target
+    // Now create our FBO render target
 
-	GLuint fboID;
-	GLuint cbID;
+    GLuint fboID;
+    GLuint cbID;
     GLuint dsID;
-	glGenFramebuffersEXT(1, &fboID);
-	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
-	glGenRenderbuffers(1, &cbID);
-	glBindRenderbuffer(GL_RENDERBUFFER, cbID);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, width, height);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, cbID);
+    glGenFramebuffersEXT(1, &fboID);
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboID);
+    glGenRenderbuffers(1, &cbID);
+    glBindRenderbuffer(GL_RENDERBUFFER, cbID);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, cbID);
     glGenRenderbuffers(1, &dsID);
     glBindRenderbuffer(GL_RENDERBUFFER, dsID);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_STENCIL, width, height);
