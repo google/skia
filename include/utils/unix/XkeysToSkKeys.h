890e9861b17a8418f4cb1dc910258205dd22e022
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-05-17 20:44:54 +0000

    add more keys
    git-svn-id: http://skia.googlecode.com/svn/trunk@1357 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/utils/unix/XkeysToSkKeys.h
+++ include/utils/unix/XkeysToSkKeys.h
@@ -8,6 +8,14 @@
 
 SkKey XKeyToSkKey(KeySym keysym) {
     switch (keysym) {
+        case XK_BackSpace:
+            return kBack_SkKey;
+        case XK_Return:
+            return kOK_SkKey;
+        case XK_Home:
+            return kHome_SkKey;
+        case XK_End:
+            return kEnd_SkKey;
         case XK_Right:
             return kRight_SkKey;
         case XK_Left:
