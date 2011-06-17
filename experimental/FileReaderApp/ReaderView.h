f823fa438701ffaa16a8ec094699f867c9859e91
yangsu@google.com <yangsu@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-06-17 18:31:01 +0000

    Added FileReaderApp to experimental and it's corresponding gyp file
    git-svn-id: http://skia.googlecode.com/svn/trunk@1641 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ experimental/FileReaderApp/ReaderView.h
@@ -0,0 +1,23 @@
+#include "SkView.h"
+#include "SkColor.h"
+#include "SkBitmap.h"
+
+/*
+ * Pipe Reader with File IO. This view reads from the data file produced by the 
+ * Pipe Writer. 
+ */
+
+class ReaderView : public SkView {
+public:
+    ReaderView();
+    virtual void draw(SkCanvas* canvas);
+    
+private:
+    int     fFilePos;
+    int     fFront;
+    int     fBack;
+    SkColor fBGColor;
+    SkBitmap fBufferBitmaps[2];
+    typedef SkView INHERITED;
+};
+
---
+++ experimental/FileReaderApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CocoaSampleApp
+//
+//  Created by Yang Su on 6/14/11.
+//  Copyright 2011 Google Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
