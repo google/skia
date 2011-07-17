64e52b635ab30fcaf7c772daedecdcdbf9aab4e2
yangsu@google.com <yangsu@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-07-17 14:44:55 +0000

    Removed old files in iOSSampleApp
    git-svn-id: http://skia.googlecode.com/svn/trunk@1880 2bbb7eff-a529-9590-31e7-b0007b416f81

--- experimental/iOSSampleApp/SkIOSNotifier.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface SkIOSNotifier : NSObject
-- (void)receiveSkEvent:(NSNotification*)notification;
-+ (void)postTimedSkEvent:(NSTimeInterval)ti;
-+ (void)timerFireMethod:(NSTimer*)theTimer;
-@end
