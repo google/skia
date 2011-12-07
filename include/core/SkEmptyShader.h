59ccef695cef28a74ab2ea13d5a6c9017af45402
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-12-07 14:59:50 +0000

    add more SK_OVERRIDEs update dox for SkEmptyShader
    git-svn-id: http://skia.googlecode.com/svn/trunk@2817 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkEmptyShader.h
+++ include/core/SkEmptyShader.h
@@ -15,24 +15,26 @@
 
 /**
  *  \class SkEmptyShader
- *  A Shader that always draws nothing.
+ *  A Shader that always draws nothing. Its setContext always returns false,
+ *  so it never expects that its shadeSpan() methods will get called.
  */
 class SK_API SkEmptyShader : public SkShader {
 public:
-    SkEmptyShader();
+    SkEmptyShader() {}
 
-    virtual uint32_t getFlags();
-    virtual uint8_t getSpan16Alpha() const;
-    virtual bool setContext(const SkBitmap& device, const SkPaint& paint,
-                            const SkMatrix& matrix);
-    virtual void shadeSpan(int x, int y, SkPMColor span[], int count);
-    virtual void shadeSpan16(int x, int y, uint16_t span[], int count);
-    virtual void shadeSpanAlpha(int x, int y, uint8_t alpha[], int count);
+    virtual uint32_t getFlags() SK_OVERRIDE;
+    virtual uint8_t getSpan16Alpha() const SK_OVERRIDE;
+    virtual bool setContext(const SkBitmap&, const SkPaint&,
+                            const SkMatrix&) SK_OVERRIDE;
+    virtual void shadeSpan(int x, int y, SkPMColor span[], int count) SK_OVERRIDE;
+    virtual void shadeSpan16(int x, int y, uint16_t span[], int count) SK_OVERRIDE;
+    virtual void shadeSpanAlpha(int x, int y, uint8_t alpha[], int count) SK_OVERRIDE;
 
 protected:
     SkEmptyShader(SkFlattenableReadBuffer&);
-    virtual Factory getFactory();
-    virtual void flatten(SkFlattenableWriteBuffer&);
+
+    virtual Factory getFactory() SK_OVERRIDE;
+    virtual void flatten(SkFlattenableWriteBuffer&) SK_OVERRIDE;
 
 private:
     typedef SkShader INHERITED;
