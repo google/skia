f920a5724fd1e0656c93ccf0cdbf7a91a8f51b03
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-12-16 20:53:22 +0000

    sk_float_round isn't a function, so call this instead: sk_float_floor((x) + 0.5f)
    git-svn-id: http://skia.googlecode.com/svn/trunk@2895 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkScalar.h
+++ include/core/SkScalar.h
@@ -119,7 +119,7 @@
 
     #define SkScalarFloorToScalar(x)    sk_float_floor(x)
     #define SkScalarCeilToScalar(x)     sk_float_ceil(x)
-    #define SkScalarRoundToScalar(x)    sk_float_round(x)
+    #define SkScalarRoundToScalar(x)    sk_float_floor((x) + 0.5f)
 
     #define SkScalarFloorToInt(x)       sk_float_floor2int(x)
     #define SkScalarCeilToInt(x)        sk_float_ceil2int(x)
