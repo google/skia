6309ea3a7275957e369be7f6f9d7eb095df0ce47
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-01-05 17:24:35 +0000

    add asComponentTable() query (will impl in SkTableColorFilter next)
    git-svn-id: http://skia.googlecode.com/svn/trunk@2971 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkColorFilter.cpp
+++ src/core/SkColorFilter.cpp
@@ -19,6 +19,10 @@ bool SkColorFilter::asColorMatrix(SkScalar matrix[20]) {
     return false;
 }
 
+bool SkColorFilter::asComponentTable(SkBitmap*) {
+    return false;
+}
+
 void SkColorFilter::filterSpan16(const uint16_t s[], int count, uint16_t d[]) {
     SkASSERT(this->getFlags() & SkColorFilter::kHasFilter16_Flag);
     SkDEBUGFAIL("missing implementation of SkColorFilter::filterSpan16");
