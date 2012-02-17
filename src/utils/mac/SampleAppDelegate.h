6c5bf8d9fe6fe263f583a4c22d04e3be879ecfeb
caryclark@google.com <caryclark@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-02-17 14:08:18 +0000

    Added BSD license language where missing. See Chromium bug 98464, Review URL: https://codereview.appspot.com/5671083
    git-svn-id: http://skia.googlecode.com/svn/trunk@3218 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/utils/mac/SampleAppDelegate.h
+++ src/utils/mac/SampleAppDelegate.h
@@ -1,3 +1,12 @@
+
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+
 #import <Cocoa/Cocoa.h>
 #import "SkSampleNSView.h"
 #import "SkOptionsTableView.h"
