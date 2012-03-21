56d3a23c3e0a53777fb25898951718d44dd9c027
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-03-21 12:25:48 +0000

    Cast the result of SK_OFFSETOF to size_t to match offsetof() and avoid narrowing conversion errors
    git-svn-id: http://skia.googlecode.com/svn/trunk@3446 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkTypes.h
+++ include/core/SkTypes.h
@@ -204,7 +204,7 @@ static inline bool SkIsU16(long x) {
 
 //////////////////////////////////////////////////////////////////////////////
 #ifndef SK_OFFSETOF
-    #define SK_OFFSETOF(type, field)    ((char*)&(((type*)1)->field) - (char*)1)
+    #define SK_OFFSETOF(type, field)    (size_t)((char*)&(((type*)1)->field) - (char*)1)
 #endif
 
 /** Returns the number of entries in an array (not a pointer)
