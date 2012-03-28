fa1dd11f0e82fbcbcaabf7f3ccb3c556ac6d2ad0
vandebo@chromium.org <vandebo@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-03-28 17:33:26 +0000

    Revert r3523 and r2522
    Review URL: https://codereview.appspot.com/5939051
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@3524 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/ports/SkOSFile_stdio.cpp
+++ src/ports/SkOSFile_stdio.cpp
@@ -36,16 +36,10 @@ size_t sk_fgetsize(SkFILE* f)
 {
     SkASSERT(f);
 
-    long curr = ::ftell((FILE*)f);       // remember where we are
-    if (curr < 0) {
-        return 0;
-    }
+    size_t  curr = ::ftell((FILE*)f);       // remember where we are
     ::fseek((FILE*)f, 0, SEEK_END);         // go to the end
-    long size = ::ftell((FILE*)f);        // record the size
-    if (size < 0) {
-        size = 0;
-    }
-    ::fseek((FILE*)f, curr, SEEK_SET);        // go back to our prev loc
+    size_t size = ::ftell((FILE*)f);        // record the size
+    ::fseek((FILE*)f, (long)curr, SEEK_SET);        // go back to our prev loc
     return size;
 }
 
