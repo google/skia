567b376900f4ab76c9948d1d22d2dcb00eab2c39
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-03-30 14:35:04 +0000

    Reland 3507
    git-svn-id: http://skia.googlecode.com/svn/trunk@3554 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/GrInOrderDrawBuffer.cpp
+++ src/gpu/GrInOrderDrawBuffer.cpp
@@ -7,7 +7,6 @@
  */
 
 
-
 #include "GrInOrderDrawBuffer.h"
 #include "GrRenderTarget.h"
 #include "GrTexture.h"
@@ -51,11 +50,6 @@ GrInOrderDrawBuffer::~GrInOrderDrawBuffer() {
     this->releaseGeometry();
     GrSafeUnref(fQuadIndexBuffer);
     GrSafeUnref(fAutoFlushTarget);
-}
-
-void GrInOrderDrawBuffer::initializeDrawStateAndClip(const GrDrawTarget& target) {
-    this->copyDrawState(target);
-    this->setClip(target.getClip());
 }
 
 void GrInOrderDrawBuffer::setQuadIndexBuffer(const GrIndexBuffer* indexBuffer) {
