a2224b6cdd568dba8d43c1207ffd091d4fd55eee
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-05-17 17:12:38 +0000

    test-only change: update run.sh and rearrange dirs to ease addition of more tests Review URL: https://codereview.appspot.com/6220044
    git-svn-id: http://skia.googlecode.com/svn/trunk@3995 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/tests/run.sh
+++ tools/tests/run.sh
@@ -7,6 +7,11 @@
 # TODO: make it look in Release and/or Debug
 SKDIFF_BINARY=out/Debug/skdiff
 
+# Compare contents of all files within directories $1 and $2,
+# EXCEPT for any dotfiles.
+# If there are any differences, a description is written to stdout and
+# we exit with a nonzero return value.
+# Otherwise, we write nothing to stdout and return.
 function compare_directories {
   diff --exclude=.* $1 $2
   if [ $? != 0 ]; then
@@ -15,14 +20,20 @@ function compare_directories {
   fi
 }
 
-# Test skdiff...
-#
-SKDIFF_TESTDIR=tools/tests/skdiff
-SKDIFF_OUTPUT_DIR=$SKDIFF_TESTDIR/output-actual
-rm -rf $SKDIFF_OUTPUT_DIR
-mkdir -p $SKDIFF_OUTPUT_DIR
-$SKDIFF_BINARY $SKDIFF_TESTDIR/baseDir $SKDIFF_TESTDIR/comparisonDir \
-  $SKDIFF_OUTPUT_DIR &>$SKDIFF_OUTPUT_DIR/stdout
-compare_directories $SKDIFF_TESTDIR/output-expected $SKDIFF_OUTPUT_DIR
+# Run skdiff with arguments in $1 (plus implicit final argument causing skdiff
+# to write its output, if any, to directory $2/output-actual).
+# Then compare its results against those in $2/output-expected.
+function skdiff_test {
+  SKDIFF_ARGS="$1"
+  ACTUAL_OUTPUT_DIR="$2/output-actual"
+  EXPECTED_OUTPUT_DIR="$2/output-expected"
 
+  rm -rf $ACTUAL_OUTPUT_DIR
+  mkdir -p $ACTUAL_OUTPUT_DIR
+  $SKDIFF_BINARY $SKDIFF_ARGS $ACTUAL_OUTPUT_DIR &>$ACTUAL_OUTPUT_DIR/stdout
+  compare_directories $EXPECTED_OUTPUT_DIR $ACTUAL_OUTPUT_DIR
+}
+
+SKDIFF_TESTDIR=tools/tests/skdiff
+skdiff_test "$SKDIFF_TESTDIR/baseDir $SKDIFF_TESTDIR/comparisonDir" "$SKDIFF_TESTDIR/test1"
 echo "All tests passed."
