32f3a49bfbc989c01f2986ae7a5ec886699e6f4c
bungeman@google.com <bungeman@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-05-18 19:06:41 +0000

    CreateTypefaceFromStream for GDI. http://codereview.appspot.com/5616047/
    git-svn-id: http://skia.googlecode.com/svn/trunk@4001 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/sfnt/SkOTUtils.h
+++ src/sfnt/SkOTUtils.h
@@ -9,9 +9,29 @@
 #define SkOTUtils_DEFINED
 
 #include "SkOTTableTypes.h"
+class SkStream;
 
 struct SkOTUtils {
+    /**
+      *  Calculates the OpenType checksum for data.
+      */
     static uint32_t CalcTableChecksum(SK_OT_ULONG *data, size_t length);
+
+    /**
+      *  Renames an sfnt font. On failure (invalid data or not an sfnt font)
+      *  returns NULL.
+      *
+      *  Essentially, this removes any existing 'name' table and replaces it
+      *  with a new one in which FontFamilyName, FontSubfamilyName,
+      *  UniqueFontIdentifier, FullFontName, and PostscriptName are fontName.
+      *
+      *  The new 'name' table records will be written with the Windows,
+      *  UnicodeBMPUCS2, and English_UnitedStates settings.
+      *
+      *  fontName and fontNameLen must be specified in terms of ASCII chars.
+      */
+    static SkData* RenameFont(SkStream* fontData,
+                              const char* fontName, int fontNameLen);
 };
 
 #endif
