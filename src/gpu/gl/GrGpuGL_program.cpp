0ae2ca2830231d074ac6fc61bf5550e85d12ed53
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-06-04 13:31:48 +0000

    Revert r4134 due to bot failures
    git-svn-id: http://skia.googlecode.com/svn/trunk@4136 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/gl/GrGpuGL_program.cpp
+++ src/gpu/gl/GrGpuGL_program.cpp
@@ -438,9 +438,6 @@ bool GrGpuGL::flushGraphicsState(GrPrimitiveType type) {
 
     for (int s = 0; s < GrDrawState::kNumStages; ++s) {
         if (this->isStageEnabled(s)) {
-
-            this->flushBoundTextureAndParams(s);
-
             this->flushTextureMatrixAndDomain(s);
 
             this->flushTexelSize(s);
@@ -458,18 +455,6 @@ bool GrGpuGL::flushGraphicsState(GrPrimitiveType type) {
         }
     }
     this->flushColorMatrix();
-
-    GrIRect* rect = NULL;
-    GrIRect clipBounds;
-    if (drawState.isClipState() &&
-        fClip.hasConservativeBounds()) {
-        fClip.getConservativeBounds().roundOut(&clipBounds);
-        rect = &clipBounds;
-    }
-    // This must come after textures are flushed because a texture may need
-    // to be msaa-resolved (which will modify bound FBO state).
-    this->flushRenderTarget(rect);
-
     return true;
 }
 
