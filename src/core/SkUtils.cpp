3a52d68b4001afd8c7036e80558fba7c87f45a0c
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-11 13:30:08 +0000

    Revert SkNO_RETURN_HINT fix in favor of a simpler fix
    git-svn-id: http://skia.googlecode.com/svn/trunk@4529 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkUtils.cpp
+++ src/core/SkUtils.cpp
@@ -9,11 +9,6 @@
 
 #include "SkUtils.h"
 
-// declared in SkPostConfig.h, see comment there.
-void SkNoReturnHint() {}
-
-///////////////////////////////////////////////////////////////////////////////
-
 #if 0
 #define assign_16_longs(dst, value)             \
     do {                                        \
