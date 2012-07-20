ca0062ee5e74eddff886ada2a200d25946ea52cf
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-20 11:20:32 +0000

    add SkPaint::getPosTextPath(), with gm to test it Review URL: https://codereview.appspot.com/6427055
    git-svn-id: http://skia.googlecode.com/svn/trunk@4684 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkPaint.h
+++ include/core/SkPaint.h
@@ -825,6 +825,9 @@ public:
     void getTextPath(const void* text, size_t length, SkScalar x, SkScalar y,
                      SkPath* path) const;
 
+    void getPosTextPath(const void* text, size_t length, 
+                        const SkPoint pos[], SkPath* path) const;
+
 #ifdef SK_BUILD_FOR_ANDROID
     const SkGlyph& getUnicharMetrics(SkUnichar);
     const SkGlyph& getGlyphMetrics(uint16_t);
