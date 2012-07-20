472b379b5bdc6566f809f18d58a7cb4d773f7aff
keyar@chromium.org <keyar@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-20 22:34:27 +0000

    render_pictures can now do tiling based rendering.
    Review URL: https://codereview.appspot.com/6401057
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@4707 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/render_pictures_main.cpp
+++ tools/render_pictures_main.cpp
@@ -10,6 +10,7 @@
 #include "SamplePipeControllers.h"
 #include "SkCanvas.h"
 #include "SkColorPriv.h"
+#include "SkDevice.h"
 #include "SkGPipe.h"
 #include "SkImageEncoder.h"
 #include "SkOSFile.h"
@@ -17,22 +18,35 @@
 #include "SkStream.h"
 #include "SkString.h"
 #include "SkTArray.h"
+#include "SkTDArray.h"
 #include "picture_utils.h"
 
-typedef void (*RenderFunc) (SkPicture*, SkBitmap*);
+enum {
+    kDefaultTileWidth = 256,
+    kDefaultTileHeight = 256
+};
 
 static void usage(const char* argv0) {
     SkDebugf("SkPicture rendering tool\n");
     SkDebugf("\n"
 "Usage: \n"
-"     %s <input>... <outputDir> \n\n"
+"     %s <input>... <outputDir> \n"
+"     [--pipe | --tile]"
 , argv0);
+    SkDebugf("\n\n");
     SkDebugf(
 "     input:     A list of directories and files to use as input.\n"
 "                    Files are expected to have the .skp extension.\n");
     SkDebugf(
 "     outputDir: directory to write the rendered images.\n");
+    SkDebugf(
+"     --pipe : Render using a SkGPipe\n");
+    SkDebugf(
+"     --tile : Render using tiles.\n");
 }
+
+static void simple_render(SkPicture* pict, SkBitmap* bitmap);
+typedef void (*RenderFunc) (SkPicture*, SkBitmap*);
 
 static void make_output_filepath(SkString* path, const SkString& dir,
                                  const SkString& name) {
@@ -94,6 +108,76 @@ static void pipe_render(SkPicture* picture, SkBitmap* bitmap) {
     pipe_run(picture, &canvas);
 }
 
+struct TileInfo {
+    SkBitmap* fBitmap;
+    SkCanvas* fCanvas;
+};
+
+// Clips the tile to an area that is completely in what the SkPicture says is the
+// drawn-to area. This is mostly important for tiles on the right and bottom edges
+// as they may go over this area and the picture may have some commands that
+// draw outside of this area and so should not actually be written.
+static void clip_tile(SkPicture* picture, const TileInfo& tile) {
+    SkRect clip = SkRect::MakeWH(SkIntToScalar(picture->width()),
+                                 SkIntToScalar(picture->height()));
+    tile.fCanvas->clipRect(clip);
+}
+
+static void add_tile(SkPicture* picture, SkTDArray<TileInfo>* tiles,
+                     int tile_x_start, int tile_y_start) {
+    TileInfo* tile = tiles->push();
+
+    tile->fBitmap = new SkBitmap();
+    sk_tools::setup_bitmap(tile->fBitmap, kDefaultTileWidth, kDefaultTileHeight);
+
+    tile->fCanvas = new SkCanvas(*(tile->fBitmap));
+    tile->fCanvas->translate(SkIntToScalar(-tile_x_start), SkIntToScalar(-tile_y_start));
+    clip_tile(picture, *tile);
+}
+
+static void setup_tiles(SkPicture* picture, SkTDArray<TileInfo>* tiles) {
+    for (int tile_y_start = 0; tile_y_start < picture->height();
+         tile_y_start += kDefaultTileWidth) {
+        for (int tile_x_start = 0; tile_x_start < picture->width();
+             tile_x_start += kDefaultTileHeight) {
+            add_tile(picture, tiles, tile_x_start, tile_y_start);
+        }
+    }
+}
+
+static void copy_tiles_to_bitmap(const SkTDArray<TileInfo>& tiles, SkBitmap* bitmap) {
+    SkCanvas destination(*bitmap);
+
+    int tile_index = 0;
+    for (int tile_y_start = 0; tile_y_start < bitmap->height();
+         tile_y_start += kDefaultTileWidth) {
+        for (int tile_x_start = 0; tile_x_start < bitmap->width();
+             tile_x_start += kDefaultTileHeight) {
+            SkBitmap source = tiles[tile_index].fCanvas->getDevice()->accessBitmap(false);
+            destination.drawBitmap(source, tile_x_start, tile_y_start);
+            ++tile_index;
+        }
+    }
+}
+
+static void tile_render(SkPicture* picture, SkBitmap* bitmap) {
+    sk_tools::setup_bitmap(bitmap, picture->width(), picture->height());
+
+    SkTDArray<TileInfo> tiles;
+    setup_tiles(picture, &tiles);
+
+    for (int i = 0; i < tiles.count(); ++i) {
+        tiles[i].fCanvas->drawPicture(*picture);
+    }
+
+    copy_tiles_to_bitmap(tiles, bitmap);
+
+    for (int i = 0; i < tiles.count(); ++i) {
+        delete tiles[i].fCanvas;
+        delete tiles[i].fBitmap;
+    }
+}
+
 static void render_picture(const SkString& inputPath, const SkString& outputDir,
                            RenderFunc renderFunc) {
     SkString inputFilename;
@@ -137,15 +221,26 @@ static void parse_commandline(int argc, char* const argv[], SkTArray<SkString>* 
     for (++argv; argv < stop; ++argv) {
         if (0 == strcmp(*argv, "--pipe")) {
             *renderFunc = pipe_render;
+        } else if (0 == strcmp(*argv, "--tile")) {
+            *renderFunc = tile_render;
+        } else if ((0 == strcmp(*argv, "-h")) || (0 == strcmp(*argv, "--help"))) {
+            usage(argv0);
+            exit(-1);
         } else {
             inputs->push_back(SkString(*argv));
         }
+    }
+
+    if (inputs->count() < 2) {
+        usage(argv0);
+        exit(-1);
     }
 }
 
 int main(int argc, char* const argv[]) {
     SkTArray<SkString> inputs;
     RenderFunc renderFunc = simple_render;
+
     parse_commandline(argc, argv, &inputs, &renderFunc);
     SkString outputDir = inputs[inputs.count() - 1];
 
