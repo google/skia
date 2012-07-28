2558896e2cf33861aa602f6734053475095111c3
chudy@google.com <chudy@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-28 23:26:10 +0000

    Added missing files to fix build.
    TBR
    
    Review URL: https://codereview.appspot.com/6455058
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@4822 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ debugger/QT/SkRasterWidget.h
@@ -0,0 +1,80 @@
+/*
+ * SkRasterWidget.h
+ *
+ *  Created on: Jul 28, 2012
+ *      Author: chudy
+ */
+
+
+#ifndef SKRASTERWIDGET_H_
+#define SKRASTERWIDGET_H_
+
+#include "SkGpuDevice.h"
+#include "SkDevice.h"
+#include "SkDebugCanvas.h"
+
+#include <QApplication>
+#include <QtGui>
+#include <QWidget>
+
+class  SkRasterWidget : public QWidget {
+
+public:
+    SkRasterWidget(QWidget* parent = NULL);
+
+    ~SkRasterWidget();
+
+    void drawTo(int index) {
+        fIndex = index;
+        this->update();
+    }
+
+    void setDebugCanvas(SkDebugCanvas* debugCanvas) {
+        fDebugCanvas = debugCanvas;
+        fIndex = debugCanvas->getSize();
+        this->update();
+    }
+
+    int getBitmapHeight() {
+        return fBitmap.height();
+    }
+
+    int getBitmapWidth() {
+        return fBitmap.width();
+    }
+
+    const SkMatrix& getCurrentMatrix() {
+        return fMatrix;
+    }
+
+    const SkIRect& getCurrentClip() {
+        return fClip;
+    }
+
+    void setTranslate(SkIPoint transform) {
+        fTransform = transform;
+    }
+
+    void setScale(float scale) {
+        fScaleFactor = scale;
+    }
+
+protected:
+    void paintEvent(QPaintEvent* event);
+
+    void resizeEvent(QResizeEvent* event);
+
+private:
+    SkBitmap fBitmap;
+    SkDebugCanvas* fDebugCanvas;
+    SkDevice* fDevice;
+
+    SkMatrix fMatrix;
+    SkIRect fClip;
+
+    int fIndex;
+    SkIPoint fTransform;
+    float fScaleFactor;
+};
+
+#endif /* SKRASTERWIDGET_H_ */
