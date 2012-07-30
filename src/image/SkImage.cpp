18b1784b1f472ce91ec78133d1dedba74b721c39
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-30 19:50:31 +0000

    update/add copyright notices
    git-svn-id: http://skia.googlecode.com/svn/trunk@4839 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -1,15 +1,18 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #include "SkImage_Base.h"
 #include "SkImagePriv.h"
 #include "SkBitmap.h"
 #include "SkCanvas.h"
 
-///////////////////////////////////////////////////////////////////////////////
-
 static SkImage_Base* asIB(SkImage* image) {
     return static_cast<SkImage_Base*>(image);
 }
-
-///////////////////////////////////////////////////////////////////////////////
 
 uint32_t SkImage::NextUniqueID() {
     static int32_t gUniqueID;
