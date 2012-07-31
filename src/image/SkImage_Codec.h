d829b5cbff1a6e149af4961f577368a6fb7c2e39
mike@reedtribe.org <mike@reedtribe.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-07-31 03:57:11 +0000

    add SkImage from encoded data
    git-svn-id: http://skia.googlecode.com/svn/trunk@4845 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ src/image/SkImage_Codec.h
@@ -0,0 +1,14 @@
+//
+//  SkImage_Codec.h
+//  sfnt
+//
+//  Created by Mike Reed on 7/30/12.
+//
+//
+
+#ifndef __sfnt__SkImage_Codec__
+#define __sfnt__SkImage_Codec__
+
+#include <iostream>
+
+#endif /* defined(__sfnt__SkImage_Codec__) */
