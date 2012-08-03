0430083bb4d82f4844dd8601f691223cfa3bddb6
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-08-03 18:22:53 +0000

    Add .0 on float GLSL literals to fix GLPrograms test break
    git-svn-id: http://skia.googlecode.com/svn/trunk@4952 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/effects/GrMorphologyEffect.cpp
+++ src/gpu/effects/GrMorphologyEffect.cpp
@@ -80,7 +80,7 @@ void GrGLMorphologyEffect ::emitFS(GrGLShaderBuilder* builder,
     }
     const char* imgInc = builder->getUniformCStr(fImageIncrementUni);
 
-    code->appendf("\t\tvec2 coord = %s - %d * %s;\n",
+    code->appendf("\t\tvec2 coord = %s - %d.0 * %s;\n",
                    builder->fSampleCoords.c_str(), fRadius, imgInc);
     code->appendf("\t\tfor (int i = 0; i < %d; i++) {\n", this->width());
     code->appendf("\t\t\tvalue = %s(value, ", func);
