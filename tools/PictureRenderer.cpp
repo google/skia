b3fb7c1f467c4d43dff0d98683d3043f0176e07c
keyar@chromium.org <keyar@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-08-20 21:02:49 +0000

    Added copyright notice to PictureRenderer.cpp
    Review URL: https://codereview.appspot.com/6453156
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@5203 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/PictureRenderer.cpp
+++ tools/PictureRenderer.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #include "PictureRenderer.h"
 #include "SamplePipeControllers.h"
 #include "SkCanvas.h"
