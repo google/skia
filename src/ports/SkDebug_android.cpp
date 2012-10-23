60952b86360ca0fdd58b96784b8bd8ebdf9904dd
djsollen@google.com <djsollen@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-10-23 17:37:30 +0000

    Enable Android executables (like skia_launcher) to redirect SkDebugf output to stdout as well as the system logs.
    Review URL: https://codereview.appspot.com/6733065
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@6059 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/ports/SkDebug_android.cpp
+++ src/ports/SkDebug_android.cpp
@@ -14,9 +14,22 @@ static const size_t kBufferSize = 256;
 #define LOG_TAG "skia"
 #include <android/log.h>
 
+static bool gSkDebugToStdOut = false;
+
+extern "C" void AndroidSkDebugToStdOut(bool debugToStdOut) {
+    gSkDebugToStdOut = debugToStdOut;
+}
+
 void SkDebugf(const char format[], ...) {
     va_list args;
     va_start(args, format);
     __android_log_vprint(ANDROID_LOG_DEBUG, LOG_TAG, format, args);
+
+    // Print debug output to stdout as well.  This is useful for command
+    // line applications (e.g. skia_launcher)
+    if (gSkDebugToStdOut) {
+        vprintf(format, args);
+    }
+
     va_end(args);
 }
