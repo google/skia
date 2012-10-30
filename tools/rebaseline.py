5967ab9f8b133479fcbde107bc5a651dcc98f515
senorblanco@chromium.org <senorblanco@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-10-30 14:34:55 +0000

    Simple rebaseline tool:  Add support for ANGLE, DirectWrite builders. Set svn mime type to image/png.
    Review URL: https://codereview.appspot.com/6820054
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@6199 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/rebaseline.py
+++ tools/rebaseline.py
@@ -17,10 +17,10 @@ import os, subprocess, sys, tempfile
 pairs = [ 
    ['base-shuttle-win7-intel-float',
     'Skia_Shuttle_Win7_Intel_Float_Release_32'],
-#    ['base-shuttle-win7-intel-angle',
-#     'Skia_Shuttle_Win7_Intel_Float_ANGLE_Release_32'],
-#    ['base-shuttle-win7-intel-directwrite',
-#     'Skia_Shuttle_Win7_Intel_Float_DirectWrite_Release_32'],
+   ['base-shuttle-win7-intel-angle',
+    'Skia_Shuttle_Win7_Intel_Float_ANGLE_Release_32'],
+   ['base-shuttle-win7-intel-directwrite',
+    'Skia_Shuttle_Win7_Intel_Float_DirectWrite_Release_32'],
    ['base-shuttle_ubuntu12_ati5770',
     'Skia_Shuttle_Ubuntu12_ATI5770_Float_Release_64'],
    ['base-macmini',
@@ -42,9 +42,12 @@ if len(sys.argv) != 2:
     exit(1)
 
 testname = sys.argv[1]
-testtypes = [ '4444', '565', '8888', 'gpu', 'pdf' ]
 
 for pair in pairs:
+    if (pair[0] == 'base-shuttle-win7-intel-angle'):
+        testtypes = [ 'angle' ]
+    else:
+        testtypes = [ '4444', '565', '8888', 'gpu', 'pdf' ]
     print pair[0] + ':'
     for testtype in testtypes:
         infilename = testname + '_' + testtype + '.png'
@@ -61,4 +64,6 @@ for pair in pairs:
         cmd = [ 'cp', temp.name, outfilename ]
         subprocess.call(cmd);
         cmd = [ 'svn', 'add', '--quiet', outfilename ]
+        subprocess.call(cmd)
+        cmd = [ 'svn', 'propset', '--quiet', 'svn:mime-type', 'image/png', outfilename ];
         subprocess.call(cmd)
