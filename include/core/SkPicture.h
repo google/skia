459e6623f1c014136f2a8df34c4af80f994eccf0
scroggo@google.com <scroggo@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-11-14 21:08:53 +0000

    Remove a warning for comparing signed and unsigned integers.
    Review URL: https://codereview.appspot.com/6851050
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@6420 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkPicture.h
+++ include/core/SkPicture.h
@@ -158,7 +158,7 @@ protected:
     // V8 : Add an option for encoding bitmaps
     // V9 : Allow the reader and writer of an SKP disagree on whether to support
     //      SK_SUPPORT_HINTING_SCALE_FACTOR
-    static const int PICTURE_VERSION = 9;
+    static const uint32_t PICTURE_VERSION = 9;
 
     // fPlayback, fRecord, fWidth & fHeight are protected to allow derived classes to
     // install their own SkPicturePlayback-derived players,SkPictureRecord-derived
