a9c1fc19561984c4850548a9b13d9c85770934f7
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-12-10 15:50:37 +0000

    don't convert clipPath -> clipRect if the path is inverse-filled
    git-svn-id: http://skia.googlecode.com/svn/trunk@6734 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkPictureRecord.cpp
+++ src/core/SkPictureRecord.cpp
@@ -360,7 +360,7 @@ bool SkPictureRecord::clipRect(const SkRect& rect, SkRegion::Op op, bool doAA) {
 bool SkPictureRecord::clipPath(const SkPath& path, SkRegion::Op op, bool doAA) {
 
     SkRect r;
-    if (path.isRect(&r)) {
+    if (!path.isInverseFillType() && path.isRect(&r)) {
         return this->clipRect(r, op, doAA);
     }
 
