fdba4041c350b72791a1844ff0bc0af28cbb4199
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-12-18 16:57:03 +0000

    change all interfaces for SkRasterizer to const, in preparation for marking it as immutable/re-entrant safe. Review URL: https://codereview.appspot.com/6936064
    git-svn-id: http://skia.googlecode.com/svn/trunk@6878 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkRasterizer.cpp
+++ src/core/SkRasterizer.cpp
@@ -16,7 +16,7 @@ SK_DEFINE_INST_COUNT(SkRasterizer)
 
 bool SkRasterizer::rasterize(const SkPath& fillPath, const SkMatrix& matrix,
                              const SkIRect* clipBounds, SkMaskFilter* filter,
-                             SkMask* mask, SkMask::CreateMode mode) {
+                             SkMask* mask, SkMask::CreateMode mode) const {
     SkIRect storage;
 
     if (clipBounds && filter && SkMask::kJustRenderImage_CreateMode != mode) {
@@ -41,7 +41,7 @@ bool SkRasterizer::rasterize(const SkPath& fillPath, const SkMatrix& matrix,
 */
 bool SkRasterizer::onRasterize(const SkPath& fillPath, const SkMatrix& matrix,
                              const SkIRect* clipBounds,
-                             SkMask* mask, SkMask::CreateMode mode) {
+                             SkMask* mask, SkMask::CreateMode mode) const {
     SkPath  devPath;
 
     fillPath.transform(matrix, &devPath);
