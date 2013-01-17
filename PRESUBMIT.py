f3232d1ed50cc68ff6847ebddbfc7aba24e0ec4b
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-01-17 14:50:59 +0000

    Adding CheckChangeHasOnlyOneEol to upload and commit presubmit checks Review URL: https://codereview.appspot.com/7151043
    git-svn-id: http://skia.googlecode.com/svn/trunk@7247 2bbb7eff-a529-9590-31e7-b0007b416f81

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -9,23 +9,44 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into gcl.
 """
 
+def _CommonChecks(input_api, output_api):
+  """Presubmit checks common to upload and commit."""
+  results = []
+  sources = lambda x: (x.LocalPath().endswith('.h') or
+                       x.LocalPath().endswith('.gypi') or
+                       x.LocalPath().endswith('.gyp') or
+                       x.LocalPath().endswith('.py') or
+                       x.LocalPath().endswith('.sh') or
+                       x.LocalPath().endswith('.cpp'))
+  results.extend(
+      input_api.canned_checks.CheckChangeHasOnlyOneEol(
+          input_api, output_api, source_file_filter=sources))
+  return results
+
 
 def CheckChangeOnUpload(input_api, output_api):
-  """Presubmit checks for the change on upload."""
-  return []
+  """Presubmit checks for the change on upload.
+
+  The following are the presubmit checks:
+  * Check change has one and only one EOL.
+  """
+  results = []
+  results.extend(_CommonChecks(input_api, output_api))
+  return results
 
 
 def CheckChangeOnCommit(input_api, output_api):
   """Presubmit checks for the change on commit.
 
   The following are the presubmit checks:
+  * Check change has one and only one EOL.
   * Ensures that the Skia tree is not closed in
     http://skia-tree-status.appspot.com/
   """
   results = []
+  results.extend(_CommonChecks(input_api, output_api))
   results.extend(
       input_api.canned_checks.CheckTreeIsOpen(
           input_api, output_api, json_url=(
               'http://skia-tree-status.appspot.com/banner-status?format=json')))
   return results
-
