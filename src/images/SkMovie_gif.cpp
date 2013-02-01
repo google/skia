bb89613380332065a2abf71b8b229eabcf2c0be8
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-02-01 19:05:48 +0000

    landing https://codereview.appspot.com/7225077/ -- tweak for GIF 5.x api
    git-svn-id: http://skia.googlecode.com/svn/trunk@7523 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/images/SkMovie_gif.cpp
+++ src/images/SkMovie_gif.cpp
@@ -40,7 +40,11 @@ static int Decode(GifFileType* fileType, GifByteType* out, int size) {
 
 SkGIFMovie::SkGIFMovie(SkStream* stream)
 {
+#if GIFLIB_MAJOR < 5
     fGIF = DGifOpen( stream, Decode );
+#else
+    fGIF = DGifOpen( stream, Decode, NULL );
+#endif
     if (NULL == fGIF)
         return;
 
