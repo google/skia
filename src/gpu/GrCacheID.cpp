1da8bf6eaa489e2af5569be0780ea43d42b1be29
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-02-05 20:04:38 +0000

    Re-enable static asserts with windows warning fix.
    git-svn-id: http://skia.googlecode.com/svn/trunk@7595 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/GrCacheID.cpp
+++ src/gpu/GrCacheID.cpp
@@ -8,19 +8,15 @@
 #include "GrTypes.h"
 #include "SkThread.h"       // for sk_atomic_inc
 
-// Well, the dummy_ "fix" caused a warning on windows, so hiding all of it
-// until we can find a universal fix.
-#if 0
 // This used to be a global scope, but we got a warning about unused variable
 // so we moved it into here. We just want it to compile, so we can test the
 // static asserts.
 static inline void dummy_function_to_avoid_unused_var_warning() {
-    GrCacheID::Key kAssertKey;
+    static const GrCacheID::Key kAssertKey;
     GR_STATIC_ASSERT(sizeof(kAssertKey.fData8) == sizeof(kAssertKey.fData32));
     GR_STATIC_ASSERT(sizeof(kAssertKey.fData8) == sizeof(kAssertKey.fData64));
     GR_STATIC_ASSERT(sizeof(kAssertKey.fData8) == sizeof(kAssertKey));
 }
-#endif
 
 GrCacheID::Domain GrCacheID::GenerateDomain() {
     static int32_t gNextDomain = kInvalid_Domain + 1;
