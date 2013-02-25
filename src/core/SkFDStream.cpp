10863051df145299937704589a4f2ebb991fb2fa
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-02-25 21:02:21 +0000

    remove duplicate impl for getFileName() on SkFDStream
    git-svn-id: http://skia.googlecode.com/svn/trunk@7854 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkFDStream.cpp
+++ src/core/SkFDStream.cpp
@@ -14,7 +14,6 @@ SkFDStream::SkFDStream(int, bool) : fFD(-1), fCloseWhenDone(false) {}
 SkFDStream::~SkFDStream() {}
 bool SkFDStream::rewind() { return false; }
 size_t SkFDStream::read(void*, size_t) { return 0; }
-const char* SkFDStream::getFileName() { return NULL; }
 
 #else
 
