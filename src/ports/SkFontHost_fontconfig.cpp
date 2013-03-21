9dcf4651194d739eaa7b3325b41e3039681a6d38
edisonn@google.com <edisonn@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-03-21 13:54:16 +0000

    When we read from a RemoteFontStream (might happen maybe with other streams), stream's data might be partially consumed. If so, when we try to read the whole content of the font, we fail, because we read only part of the content. Review URL: https://codereview.chromium.org/12926004
    git-svn-id: http://skia.googlecode.com/svn/trunk@8291 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/ports/SkFontHost_fontconfig.cpp
+++ src/ports/SkFontHost_fontconfig.cpp
@@ -247,6 +247,13 @@ SkTypeface* SkFontHost::Deserialize(SkStream* stream) {
 static SkStream* open_stream(const FontConfigTypeface* face, int* ttcIndex) {
     SkStream* stream = face->getLocalStream();
     if (stream) {
+        // TODO: fix issue 1176.
+        // As of now open_stream will return a stream and unwind it, but the
+        // SkStream is not thread safe, and if two threads use the stream they
+        // may collide and print preview for example could still fail,
+        // or there could be some failures in rendering if this stream is used
+        // there.
+        stream->rewind();
         stream->ref();
         // should have been provided by CreateFromStream()
         *ttcIndex = 0;
