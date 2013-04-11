9c5f969e7067c0d78b27a5c4245238eee2d7dfe8
scroggo@google.com <scroggo@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-04-11 18:32:01 +0000

    Include gif image decoding in images/ on linux.
    Build SkImageDecoder_libgif in images project and link against
    libgif on linux.
    
    Ensure that the GIF decoder is used in skimage.
    
    Requires a new dependency on libgif-dev when building on linux.
    
    Review URL: https://codereview.chromium.org/14029011
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@8627 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/skimage_main.cpp
+++ tools/skimage_main.cpp
@@ -159,8 +159,15 @@ int tool_main(int argc, char** argv) {
 void forceLinking();
 
 void forceLinking() {
+    // This function leaks, but that is okay because it is not intended
+    // to be called. It is only here so that the linker will include the
+    // decoders.
     SkDEBUGCODE(SkImageDecoder *creator = ) CreateJPEGImageDecoder();
     SkASSERT(creator);
+#ifdef SK_BUILD_FOR_UNIX
+    SkDEBUGCODE(creator = ) CreateGIFImageDecoder();
+    SkASSERT(creator);
+#endif
 }
 
 #if !defined SK_BUILD_FOR_IOS
