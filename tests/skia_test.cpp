ae83f6a6ad90ff016561dfcf3cf1e0490fd71d28
scroggo@google.com <scroggo@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-04-11 20:11:40 +0000

    Better fix for the patched build.
    skia_test is also built on Android in pathops_unittest, which needed
    to also depend on the flags project.
    
    Remove the hack in skia_test of providing the full path.
    
    Review URL: https://codereview.chromium.org/14177002
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@8632 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tests/skia_test.cpp
+++ tests/skia_test.cpp
@@ -5,7 +5,7 @@
  * found in the LICENSE file.
  */
 
-#include "../tools/flags/SkCommandLineFlags.h"
+#include "SkCommandLineFlags.h"
 #include "SkGraphics.h"
 #include "Test.h"
 #include "SkOSFile.h"
