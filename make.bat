c77256b83b92fef3a796ea7036bf725d51194c08
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-05-07 19:31:20 +0000

    Make make.bat and gyp_skia work with VS 2012.
    Review URL: https://codereview.chromium.org/14963005/
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@9045 2bbb7eff-a529-9590-31e7-b0007b416f81

--- make.bat
+++ make.bat
@@ -2,25 +2,31 @@
 @rem
 @rem    Use of this source code is governed by a BSD-style license that can be
 @rem    found in the LICENSE file.
+@ECHO OFF
 
-@rem Launches make.py on Windows, after setting Visual Studio environment variables.
-@rem See http://code.google.com/p/skia/wiki/GettingStartedOnWindows
+rem Launches make.py on Windows, after setting Visual Studio environment variables.
+rem See http://code.google.com/p/skia/wiki/GettingStartedOnWindows
 
-@if "%DevEnvDir%"=="" goto setup_env_vars
+if "%DevEnvDir%"=="" goto setup_env_vars
 
 :run_python
-@rem Run make.py and propagate its return value.
+rem Run make.py and propagate its return value.
 python make.py %*
-@exit /B %ERRORLEVEL%
+exit /B %ERRORLEVEL%
 
 :setup_env_vars
-@rem Visual Studio environment variables aren't set yet, so run vcvars32.bat
-@if "%VS100COMNTOOLS%"=="" goto error_no_VS100COMNTOOLS
-call "%VS100COMNTOOLS%..\..\VC\bin\vcvars32.bat"
-@if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-@goto run_python
+rem Visual Studio environment variables aren't set yet, so run vcvars32.bat
+if DEFINED VS110COMNTOOLS (
+    call "%VS110COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+) else if DEFINED VS100COMNTOOLS (
+    call "%VS100COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+) else (
+    goto error_no_VS
+)
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
+goto run_python
 
-:error_no_VS100COMNTOOLS
-@echo ERROR: VS100COMNTOOLS environment variable not set.
-@echo Are you sure Visual Studio 2010 is installed?
-@exit /B 1
+:error_no_VS
+echo ERROR: Neither VS100COMNTOOLS nor VS110COMNTOOLS environment variable is set.
+echo Are you sure Visual Studio 2010 or 2012 is installed?
+exit /B 1
