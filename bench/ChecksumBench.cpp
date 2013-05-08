b2bba23a2126df31cf95ca99dd2e22d6c42587a0
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-05-08 14:49:50 +0000

    Remove third-party cityhash, unused since r8992
    R=djsollen@google.com
    
    Review URL: https://codereview.chromium.org/15027013
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@9059 2bbb7eff-a529-9590-31e7-b0007b416f81

--- bench/ChecksumBench.cpp
+++ bench/ChecksumBench.cpp
@@ -7,7 +7,6 @@
 #include "SkBenchmark.h"
 #include "SkCanvas.h"
 #include "SkChecksum.h"
-#include "SkCityHash.h"
 #include "SkMD5.h"
 #include "SkRandom.h"
 #include "SkSHA1.h"
@@ -16,9 +15,7 @@
 enum ChecksumType {
     kChecksum_ChecksumType,
     kMD5_ChecksumType,
-    kSHA1_ChecksumType,
-    kCityHash32,
-    kCityHash64
+    kSHA1_ChecksumType
 };
 
 class ComputeChecksumBench : public SkBenchmark {
@@ -45,8 +42,6 @@ protected:
             case kChecksum_ChecksumType: return "compute_checksum";
             case kMD5_ChecksumType: return "compute_md5";
             case kSHA1_ChecksumType: return "compute_sha1";
-            case kCityHash32: return "compute_cityhash32";
-            case kCityHash64: return "compute_cityhash64";
             default: SK_CRASH(); return "";
         }
     }
@@ -75,18 +70,6 @@ protected:
                     sha1.finish(digest);
                 }
             } break;
-            case kCityHash32: {
-                for (int i = 0; i < N; i++) {
-                    volatile uint32_t result = SkCityHash::Compute32(reinterpret_cast<char*>(fData), sizeof(fData));
-                    sk_ignore_unused_variable(result);
-                }
-            } break;
-            case kCityHash64: {
-                for (int i = 0; i < N; i++) {
-                    volatile uint64_t result = SkCityHash::Compute64(reinterpret_cast<char*>(fData), sizeof(fData));
-                    sk_ignore_unused_variable(result);
-                }
-            } break;
         }
 
     }
@@ -100,11 +83,7 @@ private:
 static SkBenchmark* Fact0(void* p) { return new ComputeChecksumBench(p, kChecksum_ChecksumType); }
 static SkBenchmark* Fact1(void* p) { return new ComputeChecksumBench(p, kMD5_ChecksumType); }
 static SkBenchmark* Fact2(void* p) { return new ComputeChecksumBench(p, kSHA1_ChecksumType); }
-static SkBenchmark* Fact3(void* p) { return new ComputeChecksumBench(p, kCityHash32); }
-static SkBenchmark* Fact4(void* p) { return new ComputeChecksumBench(p, kCityHash64); }
 
 static BenchRegistry gReg0(Fact0);
 static BenchRegistry gReg1(Fact1);
 static BenchRegistry gReg2(Fact2);
-static BenchRegistry gReg3(Fact3);
-static BenchRegistry gReg4(Fact4);
