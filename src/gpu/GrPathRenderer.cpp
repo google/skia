f438c972589f91fec69b734e9f334d1fa2aa9aa8
bsalomon@google.com <bsalomon@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-05-16 19:58:46 +0000

    Pass bounds into draw calls in path renderers.
    R=jvanverth@google.com, robertphillips@google.com
    
    Review URL: https://codereview.chromium.org/14882011
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@9171 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/gpu/GrPathRenderer.cpp
+++ src/gpu/GrPathRenderer.cpp
@@ -12,3 +12,15 @@ SK_DEFINE_INST_COUNT(GrPathRenderer)
 
 GrPathRenderer::GrPathRenderer() {
 }
+
+void GrPathRenderer::GetPathDevBounds(const SkPath& path,
+                                      int devW, int devH,
+                                      const SkMatrix& matrix,
+                                      SkRect* bounds) {
+    if (path.isInverseFillType()) {
+        *bounds = SkRect::MakeWH(SkIntToScalar(devW), SkIntToScalar(devH));
+        return;
+    }
+    *bounds = path.getBounds();
+    matrix.mapRect(bounds);
+}
