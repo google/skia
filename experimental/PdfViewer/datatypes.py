a9c9f7befd3b045993a67396b088587164983395
edisonn@google.com <edisonn@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-14 19:22:19 +0000

    Object model generated from pdf spec 1.4, draft code
    Review URL: https://codereview.chromium.org/17095004
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@9625 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ experimental/PdfViewer/datatypes.py
@@ -0,0 +1,48 @@
+
+class PdfName:
+  def __init__(self, name, abr=''):
+    self.fName = name
+    self.fAbr = abr
+    
+  def toCpp(self):
+    return '\"' + self.fName + '\"'
+
+class PdfString:
+  def __init__(self, value):
+    self.fValue = value
+    
+  def toCpp(self):
+    return '\"' + self.fValue + '\"'
+
+class PdfInteger:
+  def __init__(self, value):
+    self.fValue = value
+
+  def toCpp(self):
+    return str(self.fValue)
+
+class PdfNumber:
+  def __init__(self, value):
+    self.fValue = value
+
+  def toCpp(self):
+    return str(self.fValue)
+
+class PdfString:
+  def __init__(self, value):
+    self.fValue = value
+
+  def toCpp(self):
+    return self.fValue
+
+class PdfBoolean:
+  def __init__(self, value):
+    self.fValue = value
+
+  def toCpp(self):
+    return self.fValue
+
+class CppNull:
+  def toCpp(self):
+    return 'NULL'
+    
