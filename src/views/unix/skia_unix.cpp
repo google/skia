2a277c099edb16b6e6ebe50acf3e575ccd80f657
reed@google.com <reed@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-17 13:42:43 +0000

    use select() so we can wait for XEvents or our timer
    poll for xevents using XPending
    
    BUG=
    
    Review URL: https://codereview.chromium.org/17275003
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@9633 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/views/unix/skia_unix.cpp
+++ src/views/unix/skia_unix.cpp
@@ -18,14 +18,7 @@
 
 SkOSWindow* gWindow;
 
-static void catch_alarm(int sig)
-{
-    SkEvent::ServiceQueueTimer();
-}
-
 int main(int argc, char** argv){
-    signal(SIGALRM, catch_alarm);
-
     gWindow = create_sk_window(NULL, argc, argv);
 
     // drain any events that occurred before gWindow was assigned.
@@ -43,20 +36,4 @@ int main(int argc, char** argv){
 
 // SkEvent handlers
 
-void SkEvent::SignalNonEmptyQueue()
-{
-    if (gWindow) {
-        gWindow->post_linuxevent();
-    }
-}
 
-void SkEvent::SignalQueueTimer(SkMSec delay)
-{
-    itimerval newTimer;
-    newTimer.it_interval.tv_sec = 0;
-    newTimer.it_interval.tv_usec = 0;
-    newTimer.it_value.tv_sec = 0;
-    newTimer.it_value.tv_usec = delay * 1000;
-
-    setitimer(ITIMER_REAL, &newTimer, NULL);
-}
