27d302581a09d55cd8b2e64e42b23fceca66e33a
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-19 18:52:42 +0000

    fix https://code.google.com/p/skia/issues/detail?id=1356 ('32-bit Mac compiler doesn't like https://code.google.com/p/skia/source/detail?r=9686')
    git-svn-id: http://skia.googlecode.com/svn/trunk@9688 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkString.h
+++ include/core/SkString.h
@@ -60,9 +60,9 @@ static inline char *SkStrDup(const char string[]) {
 
 
 #define SkStrAppendU32_MaxSize  10
-char*   SkStrAppendU32(char buffer[], int32_t);
+char*   SkStrAppendU32(char buffer[], uint32_t);
 #define SkStrAppendU64_MaxSize  20
-char*   SkStrAppendU64(char buffer[], int64_t, int minDigits);
+char*   SkStrAppendU64(char buffer[], uint64_t, int minDigits);
 
 #define SkStrAppendS32_MaxSize  (SkStrAppendU32_MaxSize + 1)
 char*   SkStrAppendS32(char buffer[], int32_t);
