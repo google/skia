f2e7dbb09fabc31fe5b16f6dc1a16593f518ca7d
bungeman@google.com <bungeman@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-07-16 14:59:24 +0000

    Split TLS implementation into its own translation unit.
    SkTLS has it's own header separate from SkThread, and having SkThread own
    the platform implementation of SkTLS is problematic with Chromium. The
    simplest way to clean this up is to put the implementation in its own set
    of files, where it is also more easily found.
    
    R=robertphillips@google.com
    
    Review URL: https://codereview.chromium.org/19240007
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@10105 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/ports/SkThread_pthread.cpp
+++ src/ports/SkThread_pthread.cpp
@@ -6,7 +6,6 @@
  * found in the LICENSE file.
  */
 #include "SkThread.h"
-#include "SkTLS.h"
 
 #include <pthread.h>
 #include <errno.h>
@@ -196,25 +195,3 @@ void SkMutex::release() {
 }
 
 #endif // !SK_USE_POSIX_THREADS
-
-///////////////////////////////////////////////////////////////////////////////
-
-static pthread_key_t gSkTLSKey;
-static pthread_once_t gSkTLSKey_Once = PTHREAD_ONCE_INIT;
-
-static void sk_tls_make_key() {
-    (void)pthread_key_create(&gSkTLSKey, SkTLS::Destructor);
-}
-
-void* SkTLS::PlatformGetSpecific(bool forceCreateTheSlot) {
-    // should we use forceCreateTheSlot to potentially skip calling pthread_once
-    // and just return NULL if we've never been called with
-    // forceCreateTheSlot==true ?
-
-    (void)pthread_once(&gSkTLSKey_Once, sk_tls_make_key);
-    return pthread_getspecific(gSkTLSKey);
-}
-
-void SkTLS::PlatformSetSpecific(void* ptr) {
-    (void)pthread_setspecific(gSkTLSKey, ptr);
-}
