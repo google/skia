e2e4803734a21e579a1faaabec5421ebbdf0b330
fmalita@google.com <fmalita@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-07-22 17:01:37 +0000

    Revert "Add SkImage->draw() call with src and dst rects."
    This reverts commit 9c730e27e0bb550dc228ad496cadeed8787b2244.
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@10238 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/image/SkImage_Picture.cpp
+++ src/image/SkImage_Picture.cpp
@@ -15,7 +15,6 @@ public:
     virtual ~SkImage_Picture();
 
     virtual void onDraw(SkCanvas*, SkScalar, SkScalar, const SkPaint*) SK_OVERRIDE;
-    virtual void onDrawRectToRect(SkCanvas*, const SkRect*, const SkRect&, const SkPaint*) SK_OVERRIDE;
 
     SkPicture* getPicture() { return fPicture; }
 
@@ -40,11 +39,6 @@ SkImage_Picture::~SkImage_Picture() {
 void SkImage_Picture::onDraw(SkCanvas* canvas, SkScalar x, SkScalar y,
                              const SkPaint* paint) {
     SkImagePrivDrawPicture(canvas, fPicture, x, y, paint);
-}
-
-void SkImage_Picture::onDrawRectToRect(SkCanvas* canvas, const SkRect* src, const SkRect& dst,
-                             const SkPaint* paint) {
-    SkImagePrivDrawPicture(canvas, fPicture, src, dst, paint);
 }
 
 SkImage* SkNewImageFromPicture(const SkPicture* srcPicture) {
