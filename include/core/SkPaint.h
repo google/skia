5ec696e3d52a9a9f449cbc1c2e94b25569430703
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-07 15:22:05 +0000

    Remove operator== from SkPaint
    R=mtklein@google.com, reed@google.com
    
    Author: sglez@google.com
    
    Review URL: https://chromiumcodereview.appspot.com/21949007
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@10603 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkPaint.h
+++ include/core/SkPaint.h
@@ -67,11 +67,6 @@ public:
 
     SkPaint& operator=(const SkPaint&);
 
-    SK_API friend bool operator==(const SkPaint& a, const SkPaint& b);
-    friend bool operator!=(const SkPaint& a, const SkPaint& b) {
-        return !(a == b);
-    }
-
     void flatten(SkFlattenableWriteBuffer&) const;
     void unflatten(SkFlattenableReadBuffer&);
 
