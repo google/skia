fb7b1bf5345e8e9570a2b1e472b2fe2d73a1d626
edisonn@google.com <edisonn@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-14 22:05:06 +0000

    pdfviewer: workaround gyp issue with multiple files, not being translated into an array in linux (works in mac, as per spec)
    Review URL: https://codereview.chromium.org/23157003
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@10726 2bbb7eff-a529-9590-31e7-b0007b416f81

--- experimental/PdfViewer/copy_files.py
+++ experimental/PdfViewer/copy_files.py
@@ -2,8 +2,24 @@ import os
 import shutil
 import sys
 
+def copyfile(src, dst):
+  fsrc = None
+  fdst = None
+  try:
+    fsrc = open(src, 'rb')
+    fdst = open(dst, 'wb')
+    shutil.copyfileobj(fsrc, fdst)
+  finally:
+    if fdst:
+      fdst.close()
+    if fsrc:
+      fsrc.close()
+
 dstdir = sys.argv[1]
 
+if not os.path.exists(dstdir):
+  os.makedirs(dstdir)
+
 for i in range(2, len(sys.argv)):
-  shutil.copy(sys.argv[i], dstdir)
+  copyfile(sys.argv[i], os.path.join(dstdir, os.path.basename(sys.argv[i])))
 
