25816740cfc7b932d83058c06784f370a5be7701
scroggo@google.com <scroggo@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-19 18:38:08 +0000

    skia: Added SkDiscardableMemory interface.
    Chrome's implementation of SkDiscardableMemory is provided here:
    https://codereview.chromium.org/23206002. SkDiscardableMemory is
    intended to be used for image caching.
    
    R=reed@google.com, scroggo@google.com, hclam@chromium.org, jamesr@chromium.org
    BUG=229120
    
    Author=ernstm@chromium.org
    
    Submitted on behalf of ernstm@chromium.org due to commit queue bug.
    
    Review URL: https://codereview.chromium.org/22950012
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@10797 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ src/ports/SkDiscardableMemory_none.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkDiscardableMemory.h"
+
+SkDiscardableMemory* SkDiscardableMemory::Create(size_t bytes) {
+    return NULL;
+}
