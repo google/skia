4e7df65d81e24c376a1104ccc21422d3c2199ded
skia.committer@gmail.com <skia.committer@gmail.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-27 07:02:54 +0000

    Sanitizing source files in Housekeeper-Nightly
    git-svn-id: http://skia.googlecode.com/svn/trunk@10929 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tests/DrawPathTest.cpp
+++ tests/DrawPathTest.cpp
@@ -320,7 +320,7 @@ static void TestDrawPath(skiatest::Reporter* reporter) {
     if (false) test_crbug131181();
     test_infinite_dash(reporter);
     test_crbug_165432(reporter);
-    
+
     if (false) {    // working on a fix
         test_big_aa_rect(reporter);
     }
