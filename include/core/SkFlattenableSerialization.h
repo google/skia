c78b8f2f738d3e20ceb971f2b0da0a0802eab9bd
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-11-08 15:24:12 +0000

    Removed unnecessary define
    BUG=
    R=bsalomon@google.com, sugoi@google.com
    
    Author: sugoi@chromium.org
    
    Review URL: https://codereview.chromium.org/59033009
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@12189 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkFlattenableSerialization.h
+++ include/core/SkFlattenableSerialization.h
@@ -16,11 +16,4 @@ SK_API SkData* SkValidatingSerializeFlattenable(SkFlattenable*);
 SK_API SkFlattenable* SkValidatingDeserializeFlattenable(const void* data, size_t size,
                                                          SkFlattenable::Type type);
 
-// Temporary fix for canary build
-#define SkSerializeFlattenable(flattenable) \
-SkValidatingSerializeFlattenable(flattenable)
-
-#define SkDeserializeFlattenable(data, size) \
-SkValidatingDeserializeFlattenable(data, size, SkFlattenable::kSkImageFilter_Type)
-
 #endif
