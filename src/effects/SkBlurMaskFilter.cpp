ad99358bdd7ca4ffb047121946abf0d5fdbde1e8
robertphillips@google.com <robertphillips@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-11-11 18:45:18 +0000

    Fix memory leak in filterRRectToNine
    https://codereview.chromium.org/65723005/
    
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@12222 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/effects/SkBlurMaskFilter.cpp
+++ src/effects/SkBlurMaskFilter.cpp
@@ -333,6 +333,8 @@ SkBlurMaskFilterImpl::filterRRectToNine(const SkRRect& rrect, const SkMatrix& ma
         return kFalse_FilterReturn;
     }
 
+    SkAutoMaskFreeImage amf(srcM.fImage);
+
     if (!this->filterMask(&patch->fMask, srcM, matrix, &margin)) {
         return kFalse_FilterReturn;
     }
