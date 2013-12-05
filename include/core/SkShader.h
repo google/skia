91246b9fc7740547d4b1b3f144684483c8e968cd
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-12-05 15:43:19 +0000

    Update documentation around drawBitmap and shaders
    R=reed@google.com
    
    Author: djsollen@google.com
    
    Review URL: https://codereview.chromium.org/105823004
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@12507 2bbb7eff-a529-9590-31e7-b0007b416f81

--- include/core/SkShader.h
+++ include/core/SkShader.h
@@ -334,6 +334,9 @@ public:
      *  exceed implementation limits (currently at 64K - 1)) then SkEmptyShader
      *  may be returned.
      *
+     *  If the src is kA8_Config then that mask will be colorized using the color on
+     *  the paint.
+     *
      *  @param src  The bitmap to use inside the shader
      *  @param tmx  The tiling mode to use when sampling the bitmap in the x-direction.
      *  @param tmy  The tiling mode to use when sampling the bitmap in the y-direction.
