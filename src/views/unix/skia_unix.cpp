1b3fea7172457f26fb8dd72f0ea59eedba920133
tfarina@chromium.org <tfarina@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-01-11 16:42:10 +0000

    Cleanup: Remove unused includes from skia_unix.cpp
    This also move gWindow into the main function.
    
    BUG=None
    TEST=SampleApp
    R=mtklein@google.com
    
    Review URL: https://codereview.chromium.org/133113002
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@13035 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/views/unix/skia_unix.cpp
+++ src/views/unix/skia_unix.cpp
@@ -1,37 +1,26 @@
-
 /*
  * Copyright 2011 Google Inc.
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-#include "X11/Xlib.h"
-#include "X11/keysym.h"
 
 #include "SkApplication.h"
 #include "SkEvent.h"
 #include "SkWindow.h"
-#include "SkTypes.h"
-
-#include <signal.h>
-#include <sys/time.h>
-
-SkOSWindow* gWindow;
 
 int main(int argc, char** argv){
-    gWindow = create_sk_window(NULL, argc, argv);
+    SkOSWindow* window = create_sk_window(NULL, argc, argv);
 
-    // drain any events that occurred before gWindow was assigned.
+    // drain any events that occurred before |window| was assigned.
     while (SkEvent::ProcessEvent());
 
     // Start normal Skia sequence
     application_init();
 
-    gWindow->loop();
+    window->loop();
 
-    delete gWindow;
+    delete window;
     application_term();
     return 0;
 }
-
-// SkEvent handlers
