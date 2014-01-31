5c3b335bb886dc8eb21b596b2b70a51814010363
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-01-31 13:10:05 +0000

    Add shim for SkOrderedReadBuffer.h too.
    Sorry, I specifically looked for this and failed the first time.
    
    BUG=skia:
    R=robertphillips@google.com
    
    Author: mtklein@chromium.org
    
    Review URL: https://codereview.chromium.org/151093002
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@13259 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ src/core/SkOrderedReadBuffer.h
@@ -0,0 +1,9 @@
+// Temporary shim to keep a couple dependencies working in Chromium.
+#ifndef SkOrderedReadBuffer_DEFINED
+#define SkOrderedReadBuffer_DEFINED
+
+#include "SkReadBuffer.h"
+
+typedef SkReadBuffer SkOrderedReadBuffer;
+
+#endif//SkOrderedReadBuffer_DEFINED
