d7a9fcc61f2c513064f67b125c0b2eb918768bb7
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-03-06 15:37:53 +0000

    Revert of Manually set fFilterValues in SkConvolutionFilter1D
    Revert of https://codereview.chromium.org/184323003                             
                                                                                    
    Reason for revert:                                                              
    Caused unexpected CompareGM failures-                                           
    http://108.170.217.252:10117/builders/Test-Win8-ShuttleA-GTX660-x86_64-Debug/builds/461/steps/CompareGMs/logs/stdio
    http://108.170.217.252:10117/builders/Test-Win7-ShuttleA-HD2000-x86_64-Release/builds/2062/steps/CompareGMs/logs/stdio
    http://108.170.217.252:10117/builders/Test-ChromeOS-Alex-GMA3150-x86-Release/builds/1400/steps/CompareGMs/logs/stdio
                                                                                    
    TBR=                                                                            
    NOTREECHECKS=True                                                             
    NOTRY=True
    
    
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@13683 2bbb7eff-a529-9590-31e7-b0007b416f81

--- src/core/SkConvolver.cpp
+++ src/core/SkConvolver.cpp
@@ -294,10 +294,8 @@ void SkConvolutionFilter1D::AddFilter(int filterOffset,
         filterLength = lastNonZero + 1 - firstNonZero;
         SkASSERT(filterLength > 0);
 
-        // Calling fFilterValues.reset(), or push_back() in a loop, are expensive.
-        fFilterValues.resize_back(filterLength);
         for (int i = firstNonZero; i <= lastNonZero; i++) {
-            fFilterValues[i - firstNonZero] = filterValues[i];
+            fFilterValues.push_back(filterValues[i]);
         }
     } else {
         // Here all the factors were zeroes.
