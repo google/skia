0d317d303aeb200dd4ef9e42bf16b538ac42e717
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-04-16 22:10:48 +0000

    install_dependencides tool for posix-type systems
    R=mtklein@google.com
    
    Author: halcanary@google.com
    
    Review URL: https://codereview.chromium.org/239303010
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@14231 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ tools/install_dependencides.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright 2014 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+if command -v lsb_release > /dev/null ; then
+    case $(lsb_release -i -s) in
+        Ubuntu)
+            sudo apt-get install \
+                build-essential \
+		libfreetype6-dev \
+		libfontconfig-dev \
+		libpng12-dev \
+		libgif-dev \
+		libqt4-dev \
+		clang
+	    if [ $(lsb_release -r -s) = '14.04' ] ; then
+		sudo apt-get install \
+		    ninja-build
+	    fi
+            exit
+            ;;
+    esac
+fi
+
+echo 'unknown system'
+exit 1
+
