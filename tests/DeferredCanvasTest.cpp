8cca4cc56074571f20f28b5caf7b96b5c241de75
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-05-07 14:33:57 +0000

    Split CPU and GPU DeferredCanvas tests
    Nothing important, just happened to notice this and figured it was good hygiene.
    DEF_TESTs run in parallel, while DEF_GPUTESTs run all run serially.
    
    BUG=skia:
    
    Committed: http://code.google.com/p/skia/source/detail?r=14603
    
    R=junov@chromium.org, mtklein@google.com
    
    Author: mtklein@chromium.org
    
    Review URL: https://codereview.chromium.org/271483002
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@14608 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tests/DeferredCanvasTest.cpp
+++ tests/DeferredCanvasTest.cpp
@@ -816,7 +816,7 @@ static void TestDeferredCanvasCreateCompatibleDevice(skiatest::Reporter* reporte
     REPORTER_ASSERT(reporter, notificationCounter.fStorageAllocatedChangedCount == 1);
 }
 
-DEF_GPUTEST(DeferredCanvas, reporter, factory) {
+DEF_TEST(DeferredCanvas_CPU, reporter) {
     TestDeferredCanvasBitmapAccess(reporter);
     TestDeferredCanvasFlush(reporter);
     TestDeferredCanvasSilentFlush(reporter);
@@ -830,7 +830,10 @@ DEF_GPUTEST(DeferredCanvas, reporter, factory) {
     TestDeferredCanvasWritePixelsToSurface(reporter);
     TestDeferredCanvasSurface(reporter, NULL);
     TestDeferredCanvasSetSurface(reporter, NULL);
-    if (NULL != factory) {
+}
+
+DEF_GPUTEST(DeferredCanvas_GPU, reporter, factory) {
+    if (factory != NULL) {
         TestDeferredCanvasSurface(reporter, factory);
         TestDeferredCanvasSetSurface(reporter, factory);
     }
