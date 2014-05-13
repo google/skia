44d4210146be4ba76244f0f06a76578d7c8eaf58
commit-bot@chromium.org <commit-bot@chromium.org@2bbb7eff-a529-9590-31e7-b0007b416f81>
2014-05-13 19:18:55 +0000

    BugChomper: Get the oauth_client_secret from GS
    BUG=skia:
    NOTRY=true
    (SkipBuildbotRuns)
    
    R=jcgregorio@google.com
    
    Author: borenet@google.com
    
    Review URL: https://codereview.chromium.org/288593003
    
    git-svn-id: http://skia.googlecode.com/svn/trunk@14718 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/bug_chomper/run_server.sh
+++ tools/bug_chomper/run_server.sh
@@ -7,5 +7,10 @@ go get github.com/gorilla/securecookie
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR
+
+if [[ ! -f oauth_client_secret.json ]]; then
+  gsutil cp gs://chromium-skia-gm/bugchomper/oauth_client_secret.json .
+fi
+
 GOPATH="$GOPATH:$DIR" go run $DIR/src/server/server.go $@
 
