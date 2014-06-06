e10a4ff45aa4889e1ce3b4caec3f055244832bab
humper <humper@google.com>
2014-06-06 12:21:03 -0700

    repair math error introduced by windows build fix
    BUG=skia:
    TBR=jvanverth
    NOTRY=True
    NOTREECHECKS=True
    
    Author: humper@google.com
    
    Review URL: https://codereview.chromium.org/320593003

--- src/effects/SkBlurMaskFilter.cpp
+++ src/effects/SkBlurMaskFilter.cpp
@@ -695,6 +695,9 @@ void GrGLRectBlurEffect::setData(const GrGLUniformManager& uman,
 
     uman.set4f(fProxyRectUniform, rect.fLeft, rect.fTop, rect.fRight, rect.fBottom);
     uman.set1f(fProfileSizeUniform, SkScalarCeilToScalar(6*rbe.getSigma()));
+
+    SkDebugf( "proxy rect set to %f %f %f %f\n", rect.fLeft, rect.fTop, rect.fRight, rect.fBottom);
+    SkDebugf(" Profile size set to %f\n", SkScalarCeilToScalar(6*rbe.getSigma()));
 }
 
 bool GrRectBlurEffect::CreateBlurProfileTexture(GrContext *context, float sigma,
@@ -796,8 +799,8 @@ bool SkBlurMaskFilterImpl::directFilterMaskGPU(GrContext* context,
     SkMatrix ctm = context->getMatrix();
     SkScalar xformedSigma = this->computeXformedSigma(ctm);
 
-    float pad=SkScalarCeilToScalar(6*xformedSigma)/2.0f;
-    rect.outset(pad, pad);
+    int pad=SkScalarCeilToInt(6*xformedSigma)/2;
+    rect.outset(SkIntToScalar(pad), SkIntToScalar(pad));
 
     SkAutoTUnref<GrEffectRef> effect(GrRectBlurEffect::Create(
             context, rect, xformedSigma));
