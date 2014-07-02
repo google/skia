3fa56b322acd1fc722124f349d155fc29d8500cc
mtklein <mtklein@chromium.org>
2014-07-02 12:38:38 -0700

    Exclude Clang on Windows too.  Comment this up a bit.
    BUG=391016
    R=tomhudson@chromium.org, mtklein@google.com, rnk@chromium.org, thakis@chromium.org
    
    Author: mtklein@chromium.org
    
    Review URL: https://codereview.chromium.org/363983004

--- src/opts/SkBlitRow_opts_SSE4.h
+++ src/opts/SkBlitRow_opts_SSE4.h
@@ -10,12 +10,14 @@
 
 #include "SkBlitRow.h"
 
-/* Check if we are able to build assembly code, GCC/AT&T syntax.
- * Had problems with LLVM-GCC 4.2.
- * MemorySanitizer cannot handle assembly code.
+/* Check if we are able to build assembly code, GCC/AT&T syntax:
+ *  1) Clang and GCC are generally OK.  OS X's old LLVM-GCC 4.2 can't handle it;
+ *  2) We're intentionally not linking this in even when supported (Clang) on Windows;
+ *  3) MemorySanitizer cannot instrument assembly at all.
  */
-#if (defined(__clang__) || (defined(__GNUC__) && !defined(SK_BUILD_FOR_MAC))) \
-    && !defined(MEMORY_SANITIZER)
+#if /* 1)*/ (defined(__clang__) || (defined(__GNUC__) && !defined(SK_BUILD_FOR_MAC))) \
+    /* 2)*/ && !defined(SK_BUILD_FOR_WIN)                                             \
+    /* 3)*/ && !defined(MEMORY_SANITIZER)
 extern "C" void S32A_Opaque_BlitRow32_SSE4_asm(SkPMColor* SK_RESTRICT dst,
                                                const SkPMColor* SK_RESTRICT src,
                                                int count, U8CPU alpha);
