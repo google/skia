MotorolaMobilityLLC/external-skia

Get additional DW font metrics when available.
BUG=chromium:395043
R=eae@chromium.org, caryclark@google.com

Author: bungeman@google.com

Review URL: https://codereview.chromium.org/412993002

src/ports/SkScalerContext_win_dw.cpp
@@ -26,6 +26,7 @@
 #include "SkTypeface_win_dw.h"
 
 #include <dwrite.h>
+#include <dwrite_1.h>
 
 static bool isLCD(const SkScalerContext::Rec& rec) {
     return SkMask::kLCD16_Format == rec.fMaskFormat ||
@@ -448,7 +449,7 @@ void SkScalerContext_DW::generateMetrics(SkGlyph* glyph) {
 
 void SkScalerContext_DW::generateFontMetrics(SkPaint::FontMetrics* metrics) {
     if (NULL == metrics) {
-      return;
+        return;
     }
 
     sk_bzero(metrics, sizeof(*metrics));
@@ -468,10 +469,8 @@ void SkScalerContext_DW::generateFontMetrics(SkPaint::FontMetrics* metrics) {
 
     SkScalar upem = SkIntToScalar(dwfm.designUnitsPerEm);
 
-    metrics->fTop = -fTextSizeRender * SkIntToScalar(dwfm.ascent) / upem;
-    metrics->fAscent = metrics->fTop;
+    metrics->fAscent = -fTextSizeRender * SkIntToScalar(dwfm.ascent) / upem;
     metrics->fDescent = fTextSizeRender * SkIntToScalar(dwfm.descent) / upem;
-    metrics->fBottom = metrics->fDescent;
     metrics->fLeading = fTextSizeRender * SkIntToScalar(dwfm.lineGap) / upem;
     metrics->fXHeight = fTextSizeRender * SkIntToScalar(dwfm.xHeight) / upem;
     metrics->fUnderlineThickness = fTextSizeRender * SkIntToScalar(dwfm.underlineThickness) / upem;
@@ -479,6 +478,33 @@ void SkScalerContext_DW::generateFontMetrics(SkPaint::FontMetrics* metrics) {
 
     metrics->fFlags |= SkPaint::FontMetrics::kUnderlineThinknessIsValid_Flag;
     metrics->fFlags |= SkPaint::FontMetrics::kUnderlinePositionIsValid_Flag;
+
+    if (NULL != fTypeface->fDWriteFontFace1.get()) {
+        DWRITE_FONT_METRICS1 dwfm1;
+        fTypeface->fDWriteFontFace1->GetMetrics(&dwfm1);
+        metrics->fTop = -fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxTop) / upem;
+        metrics->fBottom = -fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxBottom) / upem;
+        metrics->fXMin = fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxLeft) / upem;
+        metrics->fXMax = fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxRight) / upem;
+
+        metrics->fMaxCharWidth = metrics->fXMax - metrics->fXMin;
+    } else {
+        AutoTDWriteTable<SkOTTableHead> head(fTypeface->fDWriteFontFace.get());
+        if (head.fExists &&
+            head.fSize >= sizeof(SkOTTableHead) &&
+            head->version == SkOTTableHead::version1)
+        {
+            metrics->fTop = -fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->yMax) / upem;
+            metrics->fBottom = -fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->yMin) / upem;
+            metrics->fXMin = fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->xMin) / upem;
+            metrics->fXMax = fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->xMax) / upem;
+
+            metrics->fMaxCharWidth = metrics->fXMax - metrics->fXMin;
+        } else {
+            metrics->fTop = metrics->fAscent;
+            metrics->fBottom = metrics->fDescent;
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////

google/skia

src/ports/SkScalerContext_win_dw.cpp
@@ -26,6 +26,7 @@
 #include "SkTypeface_win_dw.h"
 
 #include <dwrite.h>
+#include <dwrite_1.h>
 
 static bool isLCD(const SkScalerContext::Rec& rec) {
     return SkMask::kLCD16_Format == rec.fMaskFormat ||
@@ -493,10 +494,8 @@ void SkScalerContext_DW::generateFontMetrics(SkPaint::FontMetrics* mx,
     }
 
     if (my) {
-        my->fTop = -fTextSizeRender * SkIntToScalar(dwfm.ascent) / upem;
-        my->fAscent = my->fTop;
+        my->fAscent = -fTextSizeRender * SkIntToScalar(dwfm.ascent) / upem;
         my->fDescent = fTextSizeRender * SkIntToScalar(dwfm.descent) / upem;
-        my->fBottom = my->fDescent;
         my->fLeading = fTextSizeRender * SkIntToScalar(dwfm.lineGap) / upem;
         my->fXHeight = fTextSizeRender * SkIntToScalar(dwfm.xHeight) / upem;
         my->fUnderlineThickness = fTextSizeRender * SkIntToScalar(dwfm.underlineThickness) / upem;
@@ -504,6 +503,33 @@ void SkScalerContext_DW::generateFontMetrics(SkPaint::FontMetrics* mx,
 
         my->fFlags |= SkPaint::FontMetrics::kUnderlineThinknessIsValid_Flag;
         my->fFlags |= SkPaint::FontMetrics::kUnderlinePositionIsValid_Flag;
+
+        if (NULL != fTypeface->fDWriteFontFace1.get()) {
+            DWRITE_FONT_METRICS1 dwfm1;
+            fTypeface->fDWriteFontFace1->GetMetrics(&dwfm1);
+            my->fTop = -fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxTop) / upem;
+            my->fBottom = -fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxBottom) / upem;
+            my->fXMin = fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxLeft) / upem;
+            my->fXMax = fTextSizeRender * SkIntToScalar(dwfm1.glyphBoxRight) / upem;
+
+            my->fMaxCharWidth = my->fXMax - my->fXMin;
+        } else {
+            AutoTDWriteTable<SkOTTableHead> head(fTypeface->fDWriteFontFace.get());
+            if (head.fExists &&
+                head.fSize >= sizeof(SkOTTableHead) &&
+                head->version == SkOTTableHead::version1)
+            {
+                my->fTop = -fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->yMax) / upem;
+                my->fBottom = -fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->yMin) / upem;
+                my->fXMin = fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->xMin) / upem;
+                my->fXMax = fTextSizeRender * (int16_t)SkEndian_SwapBE16(head->xMax) / upem;
+
+                my->fMaxCharWidth = my->fXMax - my->fXMin;
+            } else {
+                my->fTop = my->fAscent;
+                my->fBottom = my->fDescent;
+            }
+        }
     }
 }
 