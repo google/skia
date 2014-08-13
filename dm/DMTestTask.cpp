59598b0a9b29302fb9c38d445695b9b9fde8bfee
halcanary <halcanary@google.com>
2014-08-13 10:30:57 -0700

    DM: Replace --pathOpsVerbose with --veryVerbose
    R=caryclark@google.com, mtklein@google.com
    
    Author: halcanary@google.com
    
    Review URL: https://codereview.chromium.org/468963002

--- dm/DMTestTask.cpp
+++ dm/DMTestTask.cpp
@@ -1,6 +1,7 @@
 #include "DMTestTask.h"
 #include "DMUtil.h"
 #include "SkCommandLineFlags.h"
+#include "SkCommonFlags.h"
 
 // When PathOps threaded tests get going, they're briefly a big consumer of lots of RAM.
 // We disable the internal threading there by default on 32-bit builds.
@@ -8,13 +9,12 @@ static const bool is32Bit = sizeof(void*) == 4;
 
 DEFINE_bool2(pathOpsExtended,     x, false, "Run extended pathOps tests.");
 DEFINE_bool2(pathOpsSingleThread, z, is32Bit, "Disallow pathOps tests from using threads.");
-DEFINE_bool2(pathOpsVerbose,      V, false, "Tell pathOps tests to be verbose.");
 
 namespace DM {
 
 bool TestReporter::allowExtendedTest() const { return FLAGS_pathOpsExtended; }
 bool TestReporter::allowThreaded()     const { return !FLAGS_pathOpsSingleThread; }
-bool TestReporter::verbose()           const { return FLAGS_pathOpsVerbose; }
+bool TestReporter::verbose()           const { return FLAGS_veryVerbose; }
 
 static SkString test_name(const char* name) {
     SkString result("test ");
