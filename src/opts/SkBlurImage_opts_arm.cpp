39cd5b9041cb91a91b753765a13608fa7156d00f
halcanary <halcanary@google.com>
2014-08-20 14:40:09 -0700

    Turn off NEON SkBoxBlurGetPlatformProcs for ARM64 (for now)
    BUG=skia:2845
    R=djsollen@google.com, senorblanco@google.com, senorblanco@chromium.org
    
    Author: halcanary@google.com
    
    Review URL: https://codereview.chromium.org/491973002

--- src/opts/SkBlurImage_opts_arm.cpp
+++ src/opts/SkBlurImage_opts_arm.cpp
@@ -14,6 +14,8 @@ bool SkBoxBlurGetPlatformProcs(SkBoxBlurProc* boxBlurX,
                                SkBoxBlurProc* boxBlurYX) {
 #if SK_ARM_NEON_IS_NONE
     return false;
+#elif defined(SK_CPU_ARM64)  // Temporary fix for
+    return false;            // http://skbug.com/2845
 #else
 #if SK_ARM_NEON_IS_DYNAMIC
     if (!sk_cpu_arm_has_neon()) {
