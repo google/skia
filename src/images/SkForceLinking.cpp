748783d18c0721bce3146950cfcd4adabc90bde8
djsollen <djsollen@google.com>
2014-08-28 12:04:41 -0700

    Ensure that we don't reference decoders that we don't build
    R=scroggo@google.com, robertphillips@google.com
    
    Author: djsollen@google.com
    
    Review URL: https://codereview.chromium.org/517663002

--- src/images/SkForceLinking.cpp
+++ src/images/SkForceLinking.cpp
@@ -18,10 +18,13 @@ int SkForceLinking(bool doNotPassTrue) {
         CreateWEBPImageDecoder();
         CreateBMPImageDecoder();
         CreateICOImageDecoder();
+        CreateWBMPImageDecoder();
+        // Only link hardware texture codecs on platforms that build them. See images.gyp
+#ifndef SK_BUILD_FOR_ANDROID_FRAMEWORK
         CreatePKMImageDecoder();
         CreateKTXImageDecoder();
         CreateASTCImageDecoder();
-        CreateWBMPImageDecoder();
+#endif
         // Only link GIF and PNG on platforms that build them. See images.gyp
 #if !defined(SK_BUILD_FOR_MAC) && !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_NACL) \
         && !defined(SK_BUILD_FOR_IOS)
