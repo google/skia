22b5ab644e0381e672c6fcf8aff26bf82ee71989
rmistry <rmistry@google.com>
2014-10-22 06:19:51 -0700

    Add back page_sets/skia_googlespreadsheet_desktop
    Absolutely could not get the old page to work so I just created a new page.
    
    NOTRY=true
    BUG=skia:3008
    
    Review URL: https://codereview.chromium.org/671883002

---
+++ tools/skp/page_sets/skia_googlespreadsheet_desktop.py
@@ -0,0 +1,45 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+# pylint: disable=W0401,W0614
+
+
+from telemetry.page import page as page_module
+from telemetry.page import page_set as page_set_module
+
+
+class SkiaBuildbotDesktopPage(page_module.Page):
+
+  def __init__(self, url, page_set):
+    super(SkiaBuildbotDesktopPage, self).__init__(
+        url=url,
+        page_set=page_set,
+        credentials_path='data/credentials.json')
+    self.user_agent_type = 'desktop'
+    self.archive_data_file = 'data/skia_googlespreadsheet_desktop.json'
+
+  def RunSmoothness(self, action_runner):
+    action_runner.ScrollElement()
+
+  def RunNavigateSteps(self, action_runner):
+    action_runner.NavigateToPage(self)
+    action_runner.Wait(10)
+
+
+class GoogleSpreadsheetDesktopPageSet(page_set_module.PageSet):
+
+  """ Pages designed to represent the median, not highly optimized web """
+
+  def __init__(self):
+    super(GoogleSpreadsheetDesktopPageSet, self).__init__(
+      user_agent_type='desktop',
+      archive_data_file='data/skia_googlespreadsheet_desktop.json')
+
+    urls_list = [
+      # Why: from Tom W's list.
+      ('https://docs.google.com/spreadsheets/d/'
+       '1YnmSPu-p-1nj-lkWd8q_GRgzjiWzg_6A-HvFYqVoVxI/edit?usp=sharing'),
+    ]
+
+    for url in urls_list:
+      self.AddPage(SkiaBuildbotDesktopPage(url, self))
