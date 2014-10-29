89154343f0b6cf02e03fceb5c4a90d627692a676
Greg Humphreys <humper@google.com>
2014-10-29 12:30:16 -0400

    delete webtry from main skia repo; it's been moved to buildbots
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/688713002

--- experimental/webtry/setup/setup_jail.sh
+++
@@ -1,8 +0,0 @@
-apt-get install -y g++ libfreetype6 libfreetype6-dev libpng12-0 libpng12-dev \
-libglu1-mesa-dev mesa-common-dev freeglut3-dev libgif-dev libfontconfig \
-libfontconfig-dev git python wget libpoppler-cpp-dev libosmesa6-dev
-
-mkdir /skia_build
-chmod 777 /skia_build
-
-mkdir /skia_build/bin
