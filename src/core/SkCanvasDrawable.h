79c77a405a7b04f8e8add3586b004114ea708c68
reed <reed@google.com>
2014-11-11 07:54:10 -0800

    update dox
    BUG=skia:
    TBR=djsollen@google.com
    
    Review URL: https://codereview.chromium.org/697363004

--- src/core/SkCanvasDrawable.h
+++ src/core/SkCanvasDrawable.h
@@ -14,9 +14,11 @@ class SkCanvas;
 struct SkRect;
 
 /**
- *  Base-class to capture a set of drawing commands (sent to SkCanvas). Instances of this class
- *  need not be thread-safe, but they must be able to be used in a thread different from where
- *  they were created.
+ *  Base-class for objects that draw into SkCanvas.
+ *
+ *  The object has a generation ID, which is guaranteed to be unique across all drawables. To
+ *  allow for clients of the drawable that may want to cache the results, the drawable must
+ *  change its generation ID whenever its internal state changes such that it will draw differently.
  */
 class SkCanvasDrawable : public SkRefCnt {
 public:
@@ -45,6 +47,11 @@ public:
      */
     bool getBounds(SkRect*);
 
+    /**
+     *  Calling this invalidates the previous generation ID, and causes a new one to be computed
+     *  the next time getGenerationID() is called. Typically this is called by the object itself,
+     *  in response to its internal state changing.
+     */
     void notifyDrawingChanged();
 
 protected:
