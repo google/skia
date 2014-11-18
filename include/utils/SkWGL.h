6fd5e6e2618b4ca05791e7eaf47274515ce3ca2a
halcanary <halcanary@google.com>
2014-11-18 11:30:20 -0800

    http://skbug.com/3130 step 4 (include/utils/SkWGL.h gone for good)
    BUG=skia:3130
    
    Review URL: https://codereview.chromium.org/729363003

--- include/utils/SkWGL.h
+++
@@ -1,8 +0,0 @@
-// TODO(halcanary): in chromium/src, change references to this file in
-// skia/BUILD.gn and skia/skia_library.gypi, then delete this file.
-#ifndef include_utils_SkWGL_DEFINED
-#define include_utils_SkWGL_DEFINED
-#ifdef SK_BUILD_FOR_WIN32
-#include "../../src/utils/win/SkWGL.h"
-#endif  // SK_BUILD_FOR_WIN32
-#endif  // include_utils_SkWGL_DEFINED
