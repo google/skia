0e912464cecf50cc152c3fb93c2aac31c8a99cac
robertphillips <robertphillips@google.com>
2014-12-12 12:47:59 -0800

    Harden validation for SkPathRef
    Review URL: https://codereview.chromium.org/801003003

--- src/core/SkPathRef.cpp
+++ src/core/SkPathRef.cpp
@@ -452,11 +452,9 @@ void SkPathRef::validate() const {
     if (!fBoundsIsDirty && !fBounds.isEmpty()) {
         bool isFinite = true;
         for (int i = 0; i < fPointCnt; ++i) {
-            SkASSERT(!fPoints[i].isFinite() || (
-                     fBounds.fLeft - fPoints[i].fX   < SK_ScalarNearlyZero &&
-                     fPoints[i].fX - fBounds.fRight  < SK_ScalarNearlyZero &&
-                     fBounds.fTop  - fPoints[i].fY   < SK_ScalarNearlyZero &&
-                     fPoints[i].fY - fBounds.fBottom < SK_ScalarNearlyZero));
+            SkASSERT(!fPoints[i].isFinite() ||
+		     (fPoints[i].fX >= fBounds.fLeft && fPoints[i].fX <= fBounds.fRight &&
+		      fPoints[i].fY >= fBounds.fTop && fPoints[i].fY <= fBounds.fBottom));
             if (!fPoints[i].isFinite()) {
                 isFinite = false;
             }
