5d17286b3596c369132f10422a35a2cfca5c009a
robertphillips <robertphillips@google.com>
2015-01-13 10:54:37 -0800

    Revert of Switch to a more complete method of filtering hoisted layers (patchset #1 id:1 of https://codereview.chromium.org/842323003/)
    Reason for revert:
    nanobench woes
    
    Original issue's description:
    > Switch to a more complete method of filtering hoisted layers
    >
    > Committed: https://skia.googlesource.com/skia/+/74668a8e818fd891a08c4966cc5973b04c1ad992
    
    TBR=bsalomon@google.com
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/851773002

--- src/gpu/GrLayerCache.h
+++ src/gpu/GrLayerCache.h
@@ -164,7 +164,7 @@ public:
         SkASSERT(SK_InvalidGenID != pictureID);
 
         if (fPaint) {
-            if (fPaint->getImageFilter()) {
+            if (fPaint->getImageFilter() && fPaint->getImageFilter()->canFilterImageGPU()) {
                 fFilter = SkSafeRef(fPaint->getImageFilter());
                 fPaint->setImageFilter(NULL);
             }
