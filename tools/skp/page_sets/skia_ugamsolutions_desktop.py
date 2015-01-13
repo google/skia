bb928a0c0a4ddc11b05771e9eaa33f1058cc022a
rmistry <rmistry@google.com>
2015-01-13 11:52:28 -0800

    Add pageset for ugamsolutions.com
    BUG=skia:3306
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/851743004

---
+++ tools/skp/page_sets/skia_ugamsolutions_desktop.py
@@ -0,0 +1,41 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+# pylint: disable=W0401,W0614
+
+
+from telemetry.page import page as page_module
+from telemetry.page import page_set as page_set_module
+
+
+class SkiaBuildbotDesktopPage(page_module.Page):
+
+  def __init__(self, url, page_set):
+    super(SkiaBuildbotDesktopPage, self).__init__(
+        url=url,
+        page_set=page_set,
+        credentials_path='data/credentials.json')
+    self.user_agent_type = 'desktop'
+    self.archive_data_file = 'data/skia_ugamsolutions_desktop.json'
+
+  def RunNavigateSteps(self, action_runner):
+    action_runner.NavigateToPage(self)
+    action_runner.Wait(15)
+
+
+class SkiaUgamsolutionsDesktopPageSet(page_set_module.PageSet):
+
+  """ Pages designed to represent the median, not highly optimized web """
+
+  def __init__(self):
+    super(SkiaUgamsolutionsDesktopPageSet, self).__init__(
+      user_agent_type='desktop',
+      archive_data_file='data/skia_ugamsolutions_desktop.json')
+
+    urls_list = [
+      # Why: for crbug.com/447291
+      'http://www.ugamsolutions.com',
+    ]
+
+    for url in urls_list:
+      self.AddUserStory(SkiaBuildbotDesktopPage(url, self))
