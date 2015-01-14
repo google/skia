3f915c0c9aa511bef628419f63208e49a10ddcb5
mtklein <mtklein@chromium.org>
2015-01-14 05:02:39 -0800

    upstream google3 include paths for jsoncpp
    BUG=skia:
    
    No public API changes
    TBR=reed@google.com
    
    Review URL: https://codereview.chromium.org/853613002

--- include/utils/SkJSONCPP.h
+++ include/utils/SkJSONCPP.h
@@ -10,18 +10,24 @@
 #ifndef SkJSONCPP_DEFINED
 #define SkJSONCPP_DEFINED
 
-#ifdef SK_BUILD_FOR_WIN
-    // json includes xlocale which generates warning 4530 because we're
-    // compiling without exceptions;
-    // see https://code.google.com/p/skia/issues/detail?id=1067
-    #pragma warning(push)
-    #pragma warning(disable : 4530)
-#endif
-#include "json/reader.h"
-#include "json/value.h"
-#include "json/writer.h"
-#ifdef SK_BUILD_FOR_WIN
-    #pragma warning(pop)
+#ifdef GOOGLE3
+    #include "third_party/jsoncpp/reader.h"
+    #include "third_party/jsoncpp/value.h"
+    #include "third_party/jsoncpp/writer.h"
+#else
+    #ifdef SK_BUILD_FOR_WIN
+        // json includes xlocale which generates warning 4530 because we're
+        // compiling without exceptions;
+        // see https://code.google.com/p/skia/issues/detail?id=1067
+        #pragma warning(push)
+        #pragma warning(disable : 4530)
+    #endif
+    #include "json/reader.h"
+    #include "json/value.h"
+    #include "json/writer.h"
+    #ifdef SK_BUILD_FOR_WIN
+        #pragma warning(pop)
+    #endif
 #endif
 
 #endif // SkJSONCPP_DEFINED
