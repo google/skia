7c348a8097db763cecd3f9703f39714196ab7ec5
mtklein <mtklein@chromium.org>
2015-01-14 09:25:01 -0800

    DM can see into src/gpu.  No need for relative include.
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/853613004

--- tests/ClipCacheTest.cpp
+++ tests/ClipCacheTest.cpp
@@ -8,7 +8,7 @@
 #include "Test.h"
 // This is a GR test
 #if SK_SUPPORT_GPU
-#include "../../src/gpu/GrClipMaskManager.h"
+#include "GrClipMaskManager.h"
 #include "GrContextFactory.h"
 #include "SkGpuDevice.h"
 
