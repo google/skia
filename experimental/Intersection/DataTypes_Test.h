17a2b5473d0144d10749ef8d26a10fbdab580226
caryclark <caryclark@google.com>
2015-01-29 10:43:09 -0800

    The original instantiation of pathops was in the experimental/Intersection directory. Anything of value has been copied into the mainline.
    The obsolete gyp files are also included, along with a pixman test that never functioned but accidentally referenced some of these deleted files.
    
    Review URL: https://codereview.chromium.org/867213004

--- experimental/Intersection/DataTypes_Test.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DataTypes_Test_h__
-#define __DataTypes_Test_h__
-
-const double PointEpsilon = 0.000001;
-const double SquaredEpsilon = PointEpsilon * PointEpsilon;
-
-#endif
--- experimental/Intersection/QuarticRoot.h
+++
@@ -1,5 +0,0 @@
-int reducedQuarticRoots(const double t4, const double t3, const double t2, const double t1,
-        const double t0, const bool oneHint, double s[4]);
-
-int quarticRootsReal(int firstCubicRoot, const double A, const double B, const double C,
-        const double D, const double E, double s[4]);
