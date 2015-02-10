ee9c49efc9c0dd15c1e1f950dd8be39a30e933a5
mtklein <mtklein@chromium.org>
2015-02-10 07:47:31 -0800

    rewrite ambiguous comment
    It took me a few reads, but it eventually swapped from bunny to duck.
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/913733002

--- tools/CrashHandler.h
+++ tools/CrashHandler.h
@@ -4,8 +4,8 @@
 // If possible (and not already done), and SK_CRASH_HANDLER is defined,
 // register a handler for a stack trace when we crash.
 //
-// Currently this works on Linux and Mac and Windows, but on Linux, you will
-// get much better results than we can deliver by running "catchsegv <program>".
+// Currently this works on Linux and Mac and Windows.
+// On Linux, our output is garbage compared to catchsegv.  Use catchsegv if possible.
 void SetupCrashHandler();
 
 #endif//CrashHandler_DEFINED
