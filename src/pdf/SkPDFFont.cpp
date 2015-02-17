d1371a6019189820653aaf20f72ee8f5d0ee3fef
mtklein <mtklein@chromium.org>
2015-02-17 06:33:38 -0800

    Upstream support for sfntly in Google3
    See cr/86110021.
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/913143007

--- src/pdf/SkPDFFont.cpp
+++ src/pdf/SkPDFFont.cpp
@@ -26,7 +26,13 @@
 #include "SkUtils.h"
 
 #if defined (SK_SFNTLY_SUBSETTER)
-#include SK_SFNTLY_SUBSETTER
+    #include SK_SFNTLY_SUBSETTER
+#endif
+
+#if defined (GOOGLE3)
+    // #including #defines doesn't work in with this build system.
+    #include "typography/font/sfntly/src/sample/chromium/font_subsetter.h"
+    #define SK_SFNTLY_SUBSETTER  // For the benefit of #ifdefs below.
 #endif
 
 // PDF's notion of symbolic vs non-symbolic is related to the character set, not
