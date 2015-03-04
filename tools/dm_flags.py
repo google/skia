82b33dbcb2b29471c2c2740b00c0b9a0197c957e
mtklein <mtklein@chromium.org>
2015-03-04 13:58:05 -0800

    DM: no PDF for Daisy either
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/974413002

--- tools/dm_flags.py
+++ tools/dm_flags.py
@@ -31,8 +31,8 @@ def get_args(bot):
       configs.extend(['msaa4', 'nvprmsaa4'])
     else:
       configs.extend(['msaa16', 'nvprmsaa16'])
-  # Runs out of memory on Android bots.
-  if 'Android' not in bot:
+  # Runs out of memory on Android bots and Daisy.  Everyone else seems fine.
+  if 'Android' not in bot and 'Daisy' not in bot:
     configs.append('pdf')
 
   # Xoom and NP are running out of RAM when we run all these modes.  skia:3255
