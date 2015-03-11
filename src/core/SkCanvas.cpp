c275493d4247a2c5ae845d340c4297258f4684bc
reed <reed@chromium.org>
2015-03-10 19:57:10 -0700

    simplify clip inside a layer -- will be applied in restore
    BUG=skia:
    TBR=
    NOTREECHECKS=True
    
    speculative land to measure performance
    
    Review URL: https://codereview.chromium.org/997763002

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -833,7 +833,6 @@ static bool bounds_affects_clip(SkCanvas::SaveFlags flags) {
 bool SkCanvas::clipRectBounds(const SkRect* bounds, SaveFlags flags,
                                SkIRect* intersection, const SkImageFilter* imageFilter) {
     SkIRect clipBounds;
-    SkRegion::Op op = SkRegion::kIntersect_Op;
     if (!this->getClipDeviceBounds(&clipBounds)) {
         return false;
     }
@@ -842,8 +841,6 @@ bool SkCanvas::clipRectBounds(const SkRect* bounds, SaveFlags flags,
 
     if (imageFilter) {
         imageFilter->filterBounds(clipBounds, ctm, &clipBounds);
-        // Filters may grow the bounds beyond the device bounds.
-        op = SkRegion::kReplace_Op;
     }
     SkIRect ir;
     if (bounds) {
@@ -863,9 +860,10 @@ bool SkCanvas::clipRectBounds(const SkRect* bounds, SaveFlags flags,
     }
 
     if (bounds_affects_clip(flags)) {
-        fClipStack->clipDevRect(ir, op);
+        // Simplify the current clips since they will be applied properly during restore()
+        fClipStack->clipDevRect(ir, SkRegion::kReplace_Op);
         // early exit if the clip is now empty
-        if (!fMCRec->fRasterClip.op(ir, op)) {
+        if (!fMCRec->fRasterClip.op(ir, SkRegion::kReplace_Op)) {
             return false;
         }
     }
