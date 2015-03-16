903dcb08b146cbaf81420a734a64692038b467ca
bungeman <bungeman@google.com>
2015-03-16 13:00:09 -0400

    Add __STDC_LIMIT_MACROS to SkDraw.cpp.

--- src/core/SkDraw.cpp
+++ src/core/SkDraw.cpp
@@ -4,6 +4,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#define __STDC_LIMIT_MACROS
 
 #include "SkDraw.h"
 #include "SkBlitter.h"
