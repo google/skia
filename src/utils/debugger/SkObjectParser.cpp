46bffd6fbea7cc17c62cf469db145694cc650665
robertphillips <robertphillips@google.com>
2015-03-24 11:09:07 -0700

    Fix assert in debugger
    Review URL: https://codereview.chromium.org/1029903002

--- src/utils/debugger/SkObjectParser.cpp
+++ src/utils/debugger/SkObjectParser.cpp
@@ -26,7 +26,7 @@ SkString* SkObjectParser::BitmapToString(const SkBitmap& bitmap) {
     mBitmap->appendS32(bitmap.height());
 
     const char* gColorTypeStrings[] = {
-        "None", "A8", "565", "4444", "RGBA", "BGRA", "Index8"
+        "None", "A8", "565", "4444", "RGBA", "BGRA", "Index8", "G8"
     };
     SkASSERT(kLastEnum_SkColorType + 1 == SK_ARRAY_COUNT(gColorTypeStrings));
 
