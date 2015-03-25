038cb5e8d8b095f3e1f87b7a68aa8b7fa6906826
halcanary <halcanary@google.com>
2015-03-25 10:22:53 -0700

    SkTHash: add checksum include
    Review URL: https://codereview.chromium.org/1030883006

--- src/core/SkTHash.h
+++ src/core/SkTHash.h
@@ -1,6 +1,14 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #ifndef SkTHash_DEFINED
 #define SkTHash_DEFINED
 
+#include "SkChecksum.h"
 #include "SkTypes.h"
 #include "SkTemplates.h"
 
