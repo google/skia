f559de433890e55d3cce909fac2e0d1fb17327fd
mtklein <mtklein@chromium.org>
2015-04-06 07:25:04 -0700

    Expand bench to cover no-draw SkPictures too.
    This looks a lot closer to what Chromium's profiling is showing.
    
    BUG=chromium:470553
    
    Review URL: https://codereview.chromium.org/1063723002

--- bench/PictureOverheadBench.cpp
+++ bench/PictureOverheadBench.cpp
@@ -6,24 +6,30 @@
  */
 
 // A benchmark designed to isolate the constant overheads of picture recording.
-// We record a very tiny (one op) picture; one op is better than none, as it forces
-// us to allocate memory to store that op... we can't just cheat by holding onto NULLs.
+// We record an empty picture and a picture with one draw op to force memory allocation.
 
 #include "Benchmark.h"
 #include "SkCanvas.h"
 #include "SkPictureRecorder.h"
 
+template <bool kDraw>
 struct PictureOverheadBench : public Benchmark {
-    const char* onGetName() override { return "picture_overhead"; }
+    const char* onGetName() override {
+        return kDraw ? "picture_overhead_draw" : "picture_overhead_nodraw";
+    }
     bool isSuitableFor(Backend backend) override { return backend == kNonRendering_Backend; }
 
     void onDraw(const int loops, SkCanvas*) override {
         SkPictureRecorder rec;
         for (int i = 0; i < loops; i++) {
-            SkCanvas* c = rec.beginRecording(SkRect::MakeWH(2000,3000));
-                c->drawRect(SkRect::MakeXYWH(10, 10, 1000, 1000), SkPaint());
+            rec.beginRecording(SkRect::MakeWH(2000,3000));
+            if (kDraw) {
+                rec.getRecordingCanvas()->drawRect(SkRect::MakeXYWH(10, 10, 1000, 1000), SkPaint());
+            }
             SkAutoTUnref<SkPicture> pic(rec.endRecordingAsPicture());
         }
     }
 };
-DEF_BENCH(return new PictureOverheadBench;)
+
+DEF_BENCH(return (new PictureOverheadBench<false>);)
+DEF_BENCH(return (new PictureOverheadBench< true>);)
