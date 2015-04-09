a1e41c6d9a2029eb836c6120bb154ccd25e1588d
reed <reed@google.com>
2015-04-09 13:43:22 -0700

    default to 8888 for ganesh in sampleapp on iOS
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1078843002

--- experimental/iOSSampleApp/SkSampleUIView.mm
+++ experimental/iOSSampleApp/SkSampleUIView.mm
@@ -1,7 +1,14 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #import "SkSampleUIView.h"
 
-#define SKGL_CONFIG         kEAGLColorFormatRGB565
-//#define SKGL_CONFIG         kEAGLColorFormatRGBA8
+//#define SKGL_CONFIG         kEAGLColorFormatRGB565
+#define SKGL_CONFIG         kEAGLColorFormatRGBA8
 
 #define FORCE_REDRAW
 
