54b666b144e1281c0edf499e5ec1c59edaef6572
enne <enne@chromium.org>
2015-04-17 12:42:49 -0700

    Trace contention for lockPixels mutex
    Review URL: https://codereview.chromium.org/1090103002

--- src/core/SkPixelRef.cpp
+++ src/core/SkPixelRef.cpp
@@ -8,6 +8,7 @@
 #include "SkBitmapCache.h"
 #include "SkPixelRef.h"
 #include "SkThread.h"
+#include "SkTraceEvent.h"
 
 #ifdef SK_BUILD_FOR_WIN32
     // We don't have SK_BASE_MUTEX_INIT on Windows.
@@ -155,7 +156,9 @@ bool SkPixelRef::lockPixels(LockRec* rec) {
     SkASSERT(!fPreLocked || SKPIXELREF_PRELOCKED_LOCKCOUNT == fLockCount);
 
     if (!fPreLocked) {
+        TRACE_EVENT_BEGIN0("skia", "SkPixelRef::lockPixelsMutex");
         SkAutoMutexAcquire  ac(*fMutex);
+        TRACE_EVENT_END0("skia", "SkPixelRef::lockPixelsMutex");
 
         if (1 == ++fLockCount) {
             SkASSERT(fRec.isZero());
