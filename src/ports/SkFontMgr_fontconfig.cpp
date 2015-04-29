1d4ec0a1ad4998c32c6a28ff4b85000553ff686d
bungeman <bungeman@google.com>
2015-04-29 11:32:57 -0700

    FontConfig character/lang match requires weak family.
    In FontConfig the normal priority order of matches is character, family,
    language; however family can be marked as weak which changes the order to
    character, language, family. This is what is desired in
    matchFamilyStyleCharacter.
    
    Review URL: https://codereview.chromium.org/1118553002

--- src/ports/SkFontMgr_fontconfig.cpp
+++ src/ports/SkFontMgr_fontconfig.cpp
@@ -771,7 +771,10 @@ protected:
         FCLocker lock;
 
         SkAutoFcPattern pattern;
-        FcPatternAddString(pattern, FC_FAMILY, (FcChar8*)familyName);
+        FcValue familyNameValue;
+        familyNameValue.type = FcTypeString;
+        familyNameValue.u.s = reinterpret_cast<const FcChar8*>(familyName);
+        FcPatternAddWeak(pattern, FC_FAMILY, familyNameValue, FcFalse);
         fcpattern_from_skfontstyle(style, pattern);
 
         SkAutoFcCharSet charSet;
