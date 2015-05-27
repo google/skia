b72ec98b2090e5728354dac66eb1886a28e4da6b
joshualitt <joshualitt@chromium.org>
2015-05-27 10:31:12 -0700

    Fall back to older inorder buffer if we have NVPR
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1143153008

--- src/gpu/GrInOrderDrawBuffer.cpp
+++ src/gpu/GrInOrderDrawBuffer.cpp
@@ -7,11 +7,16 @@
 
 #include "GrInOrderDrawBuffer.h"
 
-static const bool kEnableReordering = true;
+// We will use the reordering buffer, unless we have NVPR.
+// TODO move NVPR to batch so we can reorder
+static inline bool allow_reordering(const GrShaderCaps* caps) {
+    return !caps->pathRenderingSupport();
+}
 
 GrInOrderDrawBuffer::GrInOrderDrawBuffer(GrContext* context)
     : INHERITED(context)
-    , fCommands(GrCommandBuilder::Create(context->getGpu(), kEnableReordering))
+    , fCommands(GrCommandBuilder::Create(context->getGpu(),
+                                         allow_reordering(context->getGpu()->caps()->shaderCaps())))
     , fPathIndexBuffer(kPathIdxBufferMinReserve * sizeof(char)/4)
     , fPathTransformBuffer(kPathXformBufferMinReserve * sizeof(float)/4)
     , fPipelineBuffer(kPipelineBufferMinReserve)
