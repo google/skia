699b852e48da8f71c19fcaa13bb109efd68e5c7d
jschuh <jschuh@chromium.org>
2015-06-04 15:10:37 -0700

    Remove unused PackBits methods and fix length checks
    Also a bit of general cleanup.
    
    BUG=chromium:486944
    
    Review URL: https://codereview.chromium.org/1152163004

--- include/core/SkPackBits.h
+++ include/core/SkPackBits.h
@@ -14,11 +14,6 @@
 
 class SkPackBits {
 public:
-    /** Given the number of 16bit values that will be passed to Pack16,
-        returns the worst-case size needed for the dst[] buffer.
-    */
-    static size_t ComputeMaxSize16(int count);
-
     /** Given the number of 8bit values that will be passed to Pack8,
         returns the worst-case size needed for the dst[] buffer.
     */
@@ -26,54 +21,26 @@ public:
 
     /** Write the src array into a packed format. The packing process may end
         up writing more bytes than it read, so dst[] must be large enough.
-        @param src      Input array of 16bit values
-        @param count    Number of entries in src[]
-        @param dst      Buffer (allocated by caller) to write the packed data
-                        into
-        @return the number of bytes written to dst[]
-    */
-    static size_t Pack16(const uint16_t src[], int count, uint8_t dst[]);
-
-    /** Write the src array into a packed format. The packing process may end
-        up writing more bytes than it read, so dst[] must be large enough.
         @param src      Input array of 8bit values
-        @param count    Number of entries in src[]
+        @param srcSize  Number of entries in src[]
         @param dst      Buffer (allocated by caller) to write the packed data
                         into
+        @param dstSize  Number of bytes in the output buffer.
         @return the number of bytes written to dst[]
     */
-    static size_t Pack8(const uint8_t src[], int count, uint8_t dst[]);
-
-    /** Unpack the data in src[], and expand it into dst[]. The src[] data was
-        written by a previous call to Pack16.
-        @param src  Input data to unpack, previously created by Pack16.
-        @param srcSize  Number of bytes of src to unpack
-        @param dst  Buffer (allocated by caller) to expand the src[] into.
-        @return the number of dst elements (not bytes) written into dst.
-    */
-    static int Unpack16(const uint8_t src[], size_t srcSize, uint16_t dst[]);
+    static size_t Pack8(const uint8_t src[], size_t srcSize, uint8_t dst[],
+                        size_t dstSize);
 
     /** Unpack the data in src[], and expand it into dst[]. The src[] data was
         written by a previous call to Pack8.
         @param src      Input data to unpack, previously created by Pack8.
         @param srcSize  Number of bytes of src to unpack
         @param dst      Buffer (allocated by caller) to expand the src[] into.
+        @param dstSize  Number of bytes in the output buffer.
         @return the number of bytes written into dst.
     */
-    static int Unpack8(const uint8_t src[], size_t srcSize, uint8_t dst[]);
-
-    /** Unpack the data from src[], skip the first dstSkip bytes, then write
-        dstWrite bytes into dst[]. The src[] data was written by a previous
-        call to Pack8. Return the number of bytes actually writtten into dst[]
-        @param src      Input data to unpack, previously created by Pack8.
-        @param dst      Buffer (allocated by caller) to expand the src[] into.
-        @param dstSkip  Number of bytes of unpacked src to skip before writing
-                        into dst
-        @param dstWrite Number of bytes of unpacked src to write into dst (after
-                        skipping dstSkip bytes)
-    */
-    static void Unpack8(uint8_t dst[], size_t dstSkip, size_t dstWrite,
-                        const uint8_t src[]);
+    static int Unpack8(const uint8_t src[], size_t srcSize, uint8_t dst[],
+                       size_t dstSize);
 };
 
 #endif
--- tests/PackBitsTest.cpp
+++ tests/PackBitsTest.cpp
@@ -8,57 +8,8 @@
 #include "SkPackBits.h"
 #include "Test.h"
 
-static const uint16_t gTest0[] = { 0, 0, 1, 1 };
-static const uint16_t gTest1[] = { 1, 2, 3, 4, 5, 6 };
-static const uint16_t gTest2[] = { 0, 0, 0, 1, 2, 3, 3, 3 };
-static const uint16_t gTest3[] = { 0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 0, 0, 1 };
-
 #include "SkRandom.h"
 static SkRandom gRand;
-static void rand_fill(uint16_t buffer[], int count) {
-    for (int i = 0; i < count; i++)
-        buffer[i] = (uint16_t)gRand.nextU();
-}
-
-static void test_pack16(skiatest::Reporter* reporter) {
-    static const struct {
-        const uint16_t* fSrc;
-        int             fCount;
-    } gTests[] = {
-        { gTest0, SK_ARRAY_COUNT(gTest0) },
-        { gTest1, SK_ARRAY_COUNT(gTest1) },
-        { gTest2, SK_ARRAY_COUNT(gTest2) },
-        { gTest3, SK_ARRAY_COUNT(gTest3) }
-    };
-
-    for (size_t i = 0; i < SK_ARRAY_COUNT(gTests); i++) {
-        uint8_t dst[100];
-        size_t dstSize = SkPackBits::Pack16(gTests[i].fSrc,
-                                            gTests[i].fCount, dst);
-        uint16_t src[100];
-        int srcCount = SkPackBits::Unpack16(dst, dstSize, src);
-        bool match = gTests[i].fCount == srcCount && memcmp(gTests[i].fSrc, src,
-                                    gTests[i].fCount * sizeof(uint16_t)) == 0;
-        REPORTER_ASSERT(reporter, match);
-    }
-
-    for (int n = 1000; n; n--) {
-        int size = 50;
-        uint16_t src[100], src2[100];
-        uint8_t dst[200];
-        rand_fill(src, size);
-
-        size_t dstSize = SkPackBits::Pack16(src, size, dst);
-        size_t maxSize = SkPackBits::ComputeMaxSize16(size);
-        REPORTER_ASSERT(reporter, maxSize >= dstSize);
-
-        int srcCount = SkPackBits::Unpack16(dst, dstSize, src2);
-        REPORTER_ASSERT(reporter, size == srcCount);
-        bool match = memcmp(src, src2, size * sizeof(uint16_t)) == 0;
-        REPORTER_ASSERT(reporter, match);
-    }
-}
-
 static const uint8_t gTest80[] = { 0, 0, 1, 1 };
 static const uint8_t gTest81[] = { 1, 2, 3, 4, 5, 6 };
 static const uint8_t gTest82[] = { 0, 0, 0, 1, 2, 3, 3, 3 };
@@ -86,10 +37,15 @@ static void test_pack8(skiatest::Reporter* reporter) {
         uint8_t dst[100];
         size_t maxSize = SkPackBits::ComputeMaxSize8(gTests[i].fCount);
         size_t dstSize = SkPackBits::Pack8(gTests[i].fSrc,
-                                           gTests[i].fCount, dst);
+                                           gTests[i].fCount, dst, maxSize - 1);
+        REPORTER_ASSERT(reporter, dstSize == 0);
+        dstSize = SkPackBits::Pack8(gTests[i].fSrc,
+                                           gTests[i].fCount, dst, sizeof(dst));
         REPORTER_ASSERT(reporter, dstSize <= maxSize);
         uint8_t src[100];
-        int srcCount = SkPackBits::Unpack8(dst, dstSize, src);
+        int srcCount = SkPackBits::Unpack8(dst, dstSize, src, gTests[i].fCount - 1);
+        REPORTER_ASSERT(reporter, srcCount == 0);
+        srcCount = SkPackBits::Unpack8(dst, dstSize, src, sizeof(src));
         bool match = gTests[i].fCount == srcCount &&
                     memcmp(gTests[i].fSrc, src,
                            gTests[i].fCount * sizeof(uint8_t)) == 0;
@@ -102,30 +58,18 @@ static void test_pack8(skiatest::Reporter* reporter) {
             uint8_t dst[600];
             rand_fill(src, size);
 
-            size_t dstSize = SkPackBits::Pack8(src, size, dst);
+            size_t dstSize = SkPackBits::Pack8(src, size, dst, sizeof(dst));
             size_t maxSize = SkPackBits::ComputeMaxSize8(size);
             REPORTER_ASSERT(reporter, maxSize >= dstSize);
 
-            size_t srcCount = SkPackBits::Unpack8(dst, dstSize, src2);
+            size_t srcCount = SkPackBits::Unpack8(dst, dstSize, src2, size);
             REPORTER_ASSERT(reporter, size == srcCount);
             bool match = memcmp(src, src2, size * sizeof(uint8_t)) == 0;
             REPORTER_ASSERT(reporter, match);
-
-            for (int j = 0; j < 100; j++) {
-                uint32_t skip = gRand.nextU() % size;
-                uint32_t write = gRand.nextU() % size;
-                if (skip + write > size) {
-                    write = size - skip;
-                }
-                SkPackBits::Unpack8(src, skip, write, dst);
-                bool match = memcmp(src, src2 + skip, write) == 0;
-                REPORTER_ASSERT(reporter, match);
-            }
         }
     }
 }
 
 DEF_TEST(PackBits, reporter) {
     test_pack8(reporter);
-    test_pack16(reporter);
 }
