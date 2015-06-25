b0c5071a376ecfb608c49886f2c86b1ce40b1389
mtklein <mtklein@chromium.org>
2015-06-25 10:26:46 -0700

    What did we learn today?  255 != 256
    vcvt_n_f32_u32 and _u32_f32 work in power-of-2 fixed point, so (...,8)
    meant 'please multiply or divide by 256'.  We need to use 255.  :(
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1204363002

--- src/opts/SkPMFloat_neon.h
+++ src/opts/SkPMFloat_neon.h
@@ -12,14 +12,14 @@ inline SkPMFloat::SkPMFloat(SkPMColor c) {
     uint8x8_t   fix8    = (uint8x8_t)vdup_n_u32(c);
     uint16x8_t  fix8_16 = vmovl_u8(fix8);
     uint32x4_t  fix8_32 = vmovl_u16(vget_low_u16(fix8_16));
-    fVec = vcvtq_n_f32_u32(fix8_32, 8);
+    fVec = vmulq_f32(vcvtq_f32_u32(fix8_32), vdupq_n_f32(1.0f/255));
     SkASSERT(this->isValid());
 }
 
 inline SkPMColor SkPMFloat::round() const {
-    // vcvtq_n_u32_f32 truncates, so we round manually by adding a half before converting.
-    float32x4_t rounded = vaddq_f32(fVec, vdupq_n_f32(0.5f/255));
-    uint32x4_t  fix8_32 = vcvtq_n_u32_f32(rounded, 8);
+    // vcvt_u32_f32 truncates, so we round manually by adding a half before converting.
+    float32x4_t rounded = vmlaq_f32(vdupq_n_f32(0.5f), fVec, vdupq_n_f32(255));
+    uint32x4_t  fix8_32 = vcvtq_u32_f32(rounded);
     uint16x4_t  fix8_16 = vqmovn_u32(fix8_32);
     uint8x8_t   fix8    = vqmovn_u16(vcombine_u16(fix8_16, vdup_n_u16(0)));
     SkPMColor c = vget_lane_u32((uint32x2_t)fix8, 0);
