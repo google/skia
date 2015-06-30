73bb435a5896d47f633f5e58e299b88d086c71c0
bsalomon <bsalomon@google.com>
2015-06-30 09:38:35 -0700

    Remove default for --images in common flags
    Review URL: https://codereview.chromium.org/1214363002

--- tools/flags/SkCommonFlags.cpp
+++ tools/flags/SkCommonFlags.cpp
@@ -24,7 +24,7 @@ DEFINE_string(gpuAPI, "", "Force use of specific gpu API.  Using \"gl\" "
               "Defaults to empty string, which selects the API native to the "
               "system.");
 
-DEFINE_string(images, "resources", "Directory of images to decode.");
+DEFINE_string(images, "", "Directory of images to decode.");
 
 DEFINE_string2(match, m, NULL,
                "[~][^]substring[$] [...] of GM name to run.\n"
