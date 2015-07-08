4f3ad2948c87662ecfc7a94d3dac2bc118ebe5d2
ethannicholas <ethannicholas@google.com>
2015-07-08 13:19:51 -0700

    disabled GrAALinearizingConvexRenderer for now
    BUG=505579
    
    Review URL: https://codereview.chromium.org/1219653004

--- src/gpu/GrAddPathRenderers_default.cpp
+++ src/gpu/GrAddPathRenderers_default.cpp
@@ -9,6 +9,7 @@
 
 #include "GrStencilAndCoverPathRenderer.h"
 #include "GrAAHairLinePathRenderer.h"
+#include "GrAAConvexPathRenderer.h"
 #include "GrAALinearizingConvexPathRenderer.h"
 #include "GrAADistanceFieldPathRenderer.h"
 #include "GrContext.h"
@@ -33,6 +34,7 @@ void GrPathRenderer::AddPathRenderers(GrContext* ctx, GrPathRendererChain* chain
     if (GrPathRenderer* pr = GrAAHairLinePathRenderer::Create()) {
         chain->addPathRenderer(pr)->unref();
     }
-    chain->addPathRenderer(SkNEW(GrAALinearizingConvexPathRenderer))->unref();
+//    chain->addPathRenderer(SkNEW(GrAALinearizingConvexPathRenderer))->unref();
+    chain->addPathRenderer(SkNEW(GrAAConvexPathRenderer))->unref();
     chain->addPathRenderer(SkNEW_ARGS(GrAADistanceFieldPathRenderer, (ctx)))->unref();
 }
