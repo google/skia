3aa0fb4d80c76b559ff4b82d5e569993aea06da1
scroggo <scroggo@chromium.org>
2015-07-08 10:48:40 -0700

    Use the upstream version of libwebp, v0.4.3.
    DEPS:
    Update to pull v0.4.3 of libwebp from upstream
    
    gyp/libwebp.gyp:
    Add new files, as referenced by the gyp file used by Chromium.
    
    resource/tests:
    Add regression tests for particular images.
    
    BUG=skia:3442
    BUG=skia:3315
    BUG=skia:3429
    
    Review URL: https://codereview.chromium.org/1178013008

--- tests/BadIcoTest.cpp
+++ tests/BadIcoTest.cpp
@@ -11,23 +11,25 @@
 #include "SkImageDecoder.h"
 #include "SkOSFile.h"
 
-DEF_TEST(BadIco, reporter) {
-    const char* const badIcos [] = {
+DEF_TEST(BadImage, reporter) {
+    const char* const badImages [] = {
         "sigabort_favicon.ico",
         "sigsegv_favicon.ico",
         "sigsegv_favicon_2.ico",
         "ico_leak01.ico",
         "ico_fuzz0.ico",
-        "ico_fuzz1.ico"
+        "ico_fuzz1.ico",
+        "skbug3442.webp",
+        "skbug3429.webp",
     };
 
-    const char* badIcoFolder = "invalid_images";
+    const char* badImagesFolder = "invalid_images";
 
-    SkString resourcePath = GetResourcePath(badIcoFolder);
+    SkString resourcePath = GetResourcePath(badImagesFolder);
 
     SkBitmap bm;
-    for (size_t i = 0; i < SK_ARRAY_COUNT(badIcos); ++i) {
-        SkString fullPath = SkOSPath::Join(resourcePath.c_str(), badIcos[i]);
+    for (size_t i = 0; i < SK_ARRAY_COUNT(badImages); ++i) {
+        SkString fullPath = SkOSPath::Join(resourcePath.c_str(), badImages[i]);
         bool success = SkImageDecoder::DecodeFile(fullPath.c_str(), &bm);
         // These files are invalid, and should not decode. More importantly,
         // though, we reached here without crashing.
