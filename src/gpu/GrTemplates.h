398920532291cb47d44b992fa2e56dd37f8ec195
joshualitt <joshualitt@chromium.org>
2015-07-10 14:14:03 -0700

    remove some unused stuff
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1231163002

--- src/gpu/GrTemplates.h
+++ src/gpu/GrTemplates.h
@@ -22,47 +22,4 @@ template <typename Dst, typename Src> Dst GrTCast(Src src) {
     return data.dst;
 }
 
-/**
- * takes a T*, saves the value it points to,  in and restores the value in the
- * destructor
- * e.g.:
- * {
- *      GrAutoTRestore<int*> autoCountRestore;
- *      if (useExtra) {
- *          autoCountRestore.reset(&fCount);
- *          fCount += fExtraCount;
- *      }
- *      ...
- * }  // fCount is restored
- */
-template <typename T> class GrAutoTRestore : SkNoncopyable {
-public:
-    GrAutoTRestore() : fPtr(NULL), fVal() {}
-
-    GrAutoTRestore(T* ptr) {
-        fPtr = ptr;
-        if (ptr) {
-            fVal = *ptr;
-        }
-    }
-
-    ~GrAutoTRestore() {
-        if (fPtr) {
-            *fPtr = fVal;
-        }
-    }
-
-    // restores previously saved value (if any) and saves value for passed T*
-    void reset(T* ptr) {
-        if (fPtr) {
-            *fPtr = fVal;
-        }
-        fPtr = ptr;
-        fVal = *ptr;
-    }
-private:
-    T* fPtr;
-    T  fVal;
-};
-
 #endif
