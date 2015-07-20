942930dcaa51f66d82cdaf46ae62efebd16c8cd0
mtklein <mtklein@chromium.org>
2015-07-20 10:35:30 -0700

    565 support for SIMD xfermodes
    This uses the most basic approach possible:
      - to load an Sk4px from 565, convert to SkPMColors on the stack serially then load those SkPMColors.
      - to store an Sk4px to 565, store to SkPMColors on the stack then convert to 565 serially.
    
    Clearly, we can optimize these loads and stores.  That's a TODO.
    
    The code using SkPMFloat is the same idea but a little more long-term viable, as we're only operating on one pixel at a time anyway.  We could probably write 565 <-> SkPMFloat methods, but I'd rather not until it's really compelling.
    
    The speedups are varied but similar across SSE and NEON: a few uninteresting, many 50% faster, some 2x faster, and SoftLight ~4x faster.
    
    This will cause minor GM diffs, but I don't think any layout test changes.
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1245673002

--- src/core/Sk4px.h
+++ src/core/Sk4px.h
@@ -10,6 +10,7 @@
 
 #include "SkNx.h"
 #include "SkColor.h"
+#include "SkColorPriv.h"
 
 // This file may be included multiple times by .cpp files with different flags, leading
 // to different definitions.  Usually that doesn't matter because it's all inlined, but
@@ -46,6 +47,14 @@ public:
     void store4(SkPMColor[4]) const;
     void store2(SkPMColor[2]) const;
     void store1(SkPMColor[1]) const;
+
+    // Same as above for 565.
+    static Sk4px Load4(const SkPMColor16 src[4]);
+    static Sk4px Load2(const SkPMColor16 src[2]);
+    static Sk4px Load1(const SkPMColor16 src[1]);
+    void store4(SkPMColor16 dst[4]) const;
+    void store2(SkPMColor16 dst[2]) const;
+    void store1(SkPMColor16 dst[1]) const;
 
     // 1, 2, or 4 SkPMColors with 16-bit components.
     // This is most useful as the result of a multiply, e.g. from mulWiden().
@@ -99,8 +108,8 @@ public:
 
     // A generic driver that maps fn over a src array into a dst array.
     // fn should take an Sk4px (4 src pixels) and return an Sk4px (4 dst pixels).
-    template <typename Fn>
-    static void MapSrc(int n, SkPMColor* dst, const SkPMColor* src, const Fn& fn) {
+    template <typename Fn, typename Dst>
+    static void MapSrc(int n, Dst* dst, const SkPMColor* src, const Fn& fn) {
         // This looks a bit odd, but it helps loop-invariant hoisting across different calls to fn.
         // Basically, we need to make sure we keep things inside a single loop.
         while (n > 0) {
@@ -129,8 +138,8 @@ public:
     }
 
     // As above, but with dst4' = fn(dst4, src4).
-    template <typename Fn>
-    static void MapDstSrc(int n, SkPMColor* dst, const SkPMColor* src, const Fn& fn) {
+    template <typename Fn, typename Dst>
+    static void MapDstSrc(int n, Dst* dst, const SkPMColor* src, const Fn& fn) {
         while (n > 0) {
             if (n >= 8) {
                 Sk4px dst0 = fn(Load4(dst+0), Load4(src+0)),
@@ -157,8 +166,8 @@ public:
     }
 
     // As above, but with dst4' = fn(dst4, src4, alpha4).
-    template <typename Fn>
-    static void MapDstSrcAlpha(int n, SkPMColor* dst, const SkPMColor* src, const SkAlpha* a,
+    template <typename Fn, typename Dst>
+    static void MapDstSrcAlpha(int n, Dst* dst, const SkPMColor* src, const SkAlpha* a,
                                const Fn& fn) {
         while (n > 0) {
             if (n >= 8) {
@@ -188,6 +197,39 @@ public:
 private:
     typedef Sk16b INHERITED;
 };
+
+// TODO: specialize these per-backend
+
+inline Sk4px Sk4px::Load4(const SkPMColor16 src[4]) {
+    SkPMColor src32[4];
+    for (int i = 0; i < 4; i++) { src32[i] = SkPixel16ToPixel32(src[i]); }
+    return Load4(src32);
+}
+inline Sk4px Sk4px::Load2(const SkPMColor16 src[2]) {
+    SkPMColor src32[2];
+    for (int i = 0; i < 2; i++) { src32[i] = SkPixel16ToPixel32(src[i]); }
+    return Load2(src32);
+}
+inline Sk4px Sk4px::Load1(const SkPMColor16 src[1]) {
+    SkPMColor src32 = SkPixel16ToPixel32(src[0]);
+    return Load1(&src32);
+}
+
+inline void Sk4px::store4(SkPMColor16 dst[4]) const {
+    SkPMColor dst32[4];
+    this->store4(dst32);
+    for (int i = 0; i < 4; i++) { dst[i] = SkPixel32ToPixel16(dst32[i]); }
+}
+inline void Sk4px::store2(SkPMColor16 dst[2]) const {
+    SkPMColor dst32[2];
+    this->store2(dst32);
+    for (int i = 0; i < 2; i++) { dst[i] = SkPixel32ToPixel16(dst32[i]); }
+}
+inline void Sk4px::store1(SkPMColor16 dst[1]) const {
+    SkPMColor dst32;
+    this->store1(&dst32);
+    dst[0] = SkPixel32ToPixel16(dst32);
+}
 
 }  // namespace
 
