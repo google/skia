765d6ad975b56253c4b6aadd8721bff1fac2d827
mtklein <mtklein@chromium.org>
2015-08-06 11:17:06 -0700

    __ARM_NEON is defined both by ARMv8 and ARMv7 + -mfpu=neon.
    Can't hurt to simplify a little.
    
    BUG=skia:4117
    
    Review URL: https://codereview.chromium.org/1274513003

--- include/core/SkPreConfig.h
+++ include/core/SkPreConfig.h
@@ -188,8 +188,7 @@
 
 // All 64-bit ARM chips have NEON.  Many 32-bit ARM chips do too.
 // TODO: Why don't we want NEON on iOS?
-#if !defined(SK_ARM_HAS_NEON) && !defined(SK_BUILD_FOR_IOS) && \
-    (defined(SK_CPU_ARM64) || defined(__ARM_NEON__))
+#if !defined(SK_ARM_HAS_NEON) && !defined(SK_BUILD_FOR_IOS) && defined(__ARM_NEON)
     #define SK_ARM_HAS_NEON
 #endif
 
