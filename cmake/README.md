e0f06a47522b81d9d81485bfd5c8e3333211db5a
mtklein <mtklein@chromium.org>
2015-08-28 11:51:06 -0700

    Example CMake build for Skia.
    This works only on Mac, probably only on 64-bit,
    and doesn't support SkCodec.
    
    BUG=skia:4269
    
    Review URL: https://codereview.chromium.org/1319543003

---
+++ cmake/README.md
@@ -0,0 +1,27 @@
+CMake build for Skia
+====================
+This directory contains experiemental CMake build files for Skia.
+They are primarily targeted at building Skia as it would be shipped,
+not at day-to-day Skia development.
+
+Quickstart
+----------
+    $ cd skia/cmake
+    $ cmake . -G Ninja     # Other CMake generators should work fine.
+    $ ninja
+    $ ls -l libskia.* example
+    $ ./example
+    $ open example.png
+If that works, you should see "Hello World!" with a green-to-purple gradient.
+
+Currently supported platforms
+-----------------------------
+  (None.  This is still super experimental.)
+
+Currently maybe-kinda-working platforms
+---------------------------------------
+  - x86-64 Mac OS X built with Clang (i.e. mtklein's laptop).
+
+Caveats
+-------
+  - SkCodec, Skia's new image decoder library, does not yet build.
---
+++ cmake/example.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "../include/core/SkCanvas.h"
+#include "../include/core/SkData.h"
+#include "../include/core/SkSurface.h"
+#include "../include/effects/SkGradientShader.h"
+#include "../include/gpu/GrContext.h"
+
+// These headers are just handy for writing this example file.  Nothing Skia specific.
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <memory>
+
+#if defined(__APPLE__)
+    #include <OpenGL/OpenGL.h>
+    static void setup_gl_context() {
+        // This is not meant to represent good form.  It's just a quick hack to get us going.
+        CGLPixelFormatAttribute attributes[] = { (CGLPixelFormatAttribute)0 };
+        CGLPixelFormatObj format;
+        GLint npix;
+        CGLChoosePixelFormat(attributes, &format, &npix);
+        CGLContextObj context;
+        CGLCreateContext(format, nullptr, &context);
+        CGLSetCurrentContext(context);
+        CGLReleasePixelFormat(format);
+    }
+#endif
+
+// Most pointers returned by Skia are derived from SkRefCnt,
+// meaning we need to call ->unref() on them when done rather than delete them.
+template <typename T> std::shared_ptr<T> adopt(T* ptr) {
+    return std::shared_ptr<T>(ptr, [](T* p) { p->unref(); });
+}
+
+static std::shared_ptr<SkSurface> create_raster_surface(int w, int h) {
+    std::cout << "Using raster surface" << std::endl;
+    return adopt(SkSurface::NewRasterN32Premul(w, h));
+}
+
+static std::shared_ptr<SkSurface> create_opengl_surface(int w, int h) {
+    std::cout << "Using opengl surface" << std::endl;
+    std::shared_ptr<GrContext> grContext = adopt(GrContext::Create(kOpenGL_GrBackend, 0));
+    return adopt(SkSurface::NewRenderTarget(grContext.get(),
+                                            SkSurface::kNo_Budgeted,
+                                            SkImageInfo::MakeN32Premul(w,h)));
+}
+
+int main(int, char**) {
+    setup_gl_context();
+    srand(time(nullptr));
+    std::shared_ptr<SkSurface> surface = (rand() % 2) ? create_raster_surface(320, 240)
+                                                      : create_opengl_surface(320, 240);
+
+    // Create a left-to-right green-to-purple gradient shader.
+    SkPoint pts[] = { {0,0}, {320,240} };
+    SkColor colors[] = { 0xFF00FF00, 0xFFFF00FF };
+    std::shared_ptr<SkShader> shader = adopt(
+            SkGradientShader::CreateLinear(pts, colors, nullptr, 2, SkShader::kRepeat_TileMode));
+
+    // Our text will draw with this paint: size 24, antialiased, with the shader.
+    SkPaint paint;
+    paint.setTextSize(24);
+    paint.setAntiAlias(true);
+    paint.setShader(shader.get());
+
+    // Draw to the surface via its SkCanvas.
+    SkCanvas* canvas = surface->getCanvas();   // We don't manage this pointer's lifetime.
+    static const char* msg = "Hello world!";
+    canvas->clear(SK_ColorWHITE);
+    canvas->drawText(msg, strlen(msg), 90,120, paint);
+
+    // Grab a snapshot of the surface as an immutable SkImage.
+    std::shared_ptr<SkImage> image = adopt(surface->newImageSnapshot());
+    // Encode that image as a .png into a blob in memory.
+    std::shared_ptr<SkData> png = adopt(image->encode(SkImageEncoder::kPNG_Type, 100));
+
+    // This code is no longer Skia-specific.  We just dump the .png to disk.  Any way works.
+    static const char* path = "example.png";
+    std::ofstream(path, std::ios::out | std::ios::binary)
+        .write((const char*)png->data(), png->size());
+    std::cout << "Wrote " << path << std::endl;
+
+    return 0;
+}
