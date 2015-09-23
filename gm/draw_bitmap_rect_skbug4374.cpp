5cd406f0f6dfe74f052cf8f79bd200cffaed65b8
halcanary <halcanary@google.com>
2015-09-23 12:46:24 -0700

    GM for http://skbug.com/4374
    BUG=skia:4374
    
    Review URL: https://codereview.chromium.org/1367623002

---
+++ gm/draw_bitmap_rect_skbug4374.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "Resources.h"
+#include "gm.h"
+// http://skbug.com/4374
+DEF_SIMPLE_GM(draw_bitmap_rect_skbug4734, canvas, 64, 64) {
+    SkBitmap source;
+    if (GetResourceAsBitmap("randPixels.png", &source)) {
+        SkRect rect = SkRect::Make(source.bounds());
+        rect.inset(0.5, 1.5);
+        SkRect dst;
+        SkMatrix::MakeScale(8.0f).mapRect(&dst, rect);
+        canvas->drawBitmapRect(source, rect, dst, nullptr);
+    }
+}
