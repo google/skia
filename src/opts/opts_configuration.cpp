1deaf3e5065211480874f5bb967e3d9ea8169159
Travis Veralrud <travis@bueller.us>
2015-10-16 09:47:33 -0700

    Change deployment target of iOS targets to 8.0

--- src/opts/opts_configuration.cpp
+++ src/opts/opts_configuration.cpp
@@ -2,16 +2,18 @@
 
 #if TARGET_OS_SIMULATOR
 
-#include "SkBitmapProcState_opts_none.cpp"
-#include "SkBlitMask_opts_none.cpp"
-#include "SkBlitRow_opts_none.cpp"
-#include "SkOpts.cpp"
+//#include "SkBitmapProcState_opts_none.cpp"
+//#include "SkBlitMask_opts_none.cpp"
+//#include "SkBlitRow_opts_none.cpp"
+//#include "SkOpts.cpp"
+//#include "SkOpts_ssse3.cpp"
+//#include "SkOpts_sse41.cpp"
 
 #else
 
-#include "SkBitmapProcState_arm_neon.cpp"
-#include "SkBlitMask_opts_arm_neon.cpp"
-#include "SkBlitRow_opts_arm_neon.cpp"
-#include "SkOpts_neon.cpp"
+//#include "SkBitmapProcState_arm_neon.cpp"
+//#include "SkBlitMask_opts_arm_neon.cpp"
+//#include "SkBlitRow_opts_arm_neon.cpp"
+//#include "SkOpts_neon.cpp"
 
 #endif
