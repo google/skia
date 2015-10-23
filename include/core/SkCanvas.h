19e82e3b9f6fb5a02fc033121f2047b4d886112e
bsalomon <bsalomon@google.com>
2015-10-23 09:27:42 -0700

    Update SrcRectConstraint doc. Previous doc never matched the implementation or the c2d spec.
    Review URL: https://codereview.chromium.org/1411103003

--- include/core/SkCanvas.h
+++ include/core/SkCanvas.h
@@ -805,7 +805,7 @@ public:
 
         /**
          *  If kFast is specified, the implementation may sample outside of the src-rect
-         *  (if specified) by at most 1 pixel when filtering. This allows greater flexibility
+         *  (if specified) by half the width of filter. This allows greater flexibility
          *  to the implementation and can make the draw much faster.
          */
         kFast_SrcRectConstraint,
@@ -885,7 +885,7 @@ public:
     void drawBitmapRect(const SkBitmap& bitmap, const SkIRect& isrc, const SkRect& dst,
                         const SkPaint* paint, SrcRectConstraint = kStrict_SrcRectConstraint);
     void drawBitmapRect(const SkBitmap& bitmap, const SkRect& dst, const SkPaint* paint,
-                       SrcRectConstraint = kStrict_SrcRectConstraint);
+                        SrcRectConstraint = kStrict_SrcRectConstraint);
 
     /**
      *  Draw the bitmap stretched differentially to fit into dst.
