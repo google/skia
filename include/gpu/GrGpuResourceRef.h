d9ffaed6d21f85c717117904da26a2bac2295607
reed <reed@google.com>
2015-11-10 04:55:08 -0800

    apply mozilla patch
    BUG=skia:4547
    
    Review URL: https://codereview.chromium.org/1421793009

--- include/gpu/GrGpuResourceRef.h
+++ include/gpu/GrGpuResourceRef.h
@@ -182,7 +182,11 @@ public:
         this->release();
     }
 
-    operator bool() const { return SkToBool(fResource); }
+    explicit operator bool() const { return SkToBool(fResource); }
+
+    bool operator==(const GrPendingIOResource& other) const {
+        return fResource == other.fResource;
+    }
 
     T* get() const { return fResource; }
 
