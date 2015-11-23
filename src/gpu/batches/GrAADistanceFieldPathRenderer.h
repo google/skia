512e437e1e07159a258dd3c5b907576bd1aefc1e
jvanverth <jvanverth@google.com>
2015-11-23 11:50:02 -0800

    Add stroking support to distance field path renderer
    Also slightly increases sizes of paths accepted for distance field
    caching.
    
    Committed: https://skia.googlesource.com/skia/+/5ce76efd1c847308c7bcac17bd87d567c42cd786
    
    Committed: https://skia.googlesource.com/skia/+/73ee6770260dbeeabc4a78eee4f13533f0031211
    
    Review URL: https://codereview.chromium.org/1460873002

--- src/gpu/batches/GrAADistanceFieldPathRenderer.h
+++ src/gpu/batches/GrAADistanceFieldPathRenderer.h
@@ -33,13 +33,30 @@ private:
     bool onDrawPath(const DrawPathArgs&) override;
 
     struct PathData {
-        struct Key {
+        class Key {
+        public:
+            // default ctor needed for new of uninitialized PathData
+            // since fStroke has no default ctor
+            Key() 
+                : fGenID(0)
+                , fDimension(0)
+                , fStroke(SkStrokeRec::kFill_InitStyle) {}
+            Key(uint32_t genID, uint32_t dim, const SkStrokeRec& stroke)
+                : fGenID(genID)
+                , fDimension(dim)
+                , fStroke(stroke) {}
+           
+            bool operator==(const Key& other) const {
+                return other.fGenID == fGenID && other.fDimension == fDimension &&
+                       fStroke.hasEqualEffect(other.fStroke);
+            }
+           
+        private:
             uint32_t   fGenID;
             // rendered size for stored path (32x32 max, 64x64 max, 128x128 max)
             uint32_t   fDimension;
-            bool operator==(const Key& other) const {
-                return other.fGenID == fGenID && other.fDimension == fDimension;
-            }
+            // stroking information
+            SkStrokeRec fStroke;
         };
         Key                   fKey;
         SkScalar              fScale;
