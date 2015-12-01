c19247fd03b3498d3d6a6b44421ba75dd49a6ca0
lsalzman <lsalzman@mozilla.com>
2015-12-01 07:21:09 -0800

    check that SSE2 is available before using __vectorcall
    BUG=skia:4563
    
    Review URL: https://codereview.chromium.org/1489873002

--- include/private/SkTLogic.h
+++ include/private/SkTLogic.h
@@ -123,7 +123,9 @@ template <typename R, typename... Args> struct is_function<R(Args...)> : true_ty
 template <typename R, typename... Args> struct is_function<R __cdecl (Args...)> : true_type {};
 template <typename R, typename... Args> struct is_function<R __stdcall (Args...)> : true_type {};
 template <typename R, typename... Args> struct is_function<R __fastcall (Args...)> : true_type {};
+#if SK_CPU_SSE_LEVEL >= SK_CPU_SSE_LEVEL_SSE2
 template <typename R, typename... Args> struct is_function<R __vectorcall (Args...)> : true_type {};
+#endif
 #else
 template <typename R, typename... Args> struct is_function<R __cdecl (Args...)> : true_type {};
 template <typename R, typename... Args> struct is_function<R __vectorcall (Args...)> : true_type {};
