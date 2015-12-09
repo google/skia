9f0337ed4b5b5f7b3a049175365658470061b3cb
bsalomon <bsalomon@google.com>
2015-12-09 06:27:59 -0800

    Attempt to land cache purge again [and regen bot logs if still failing]
    TBR=
    
    Review URL: https://codereview.chromium.org/1510103002

--- src/gpu/GrResourceCache.h
+++ src/gpu/GrResourceCache.h
@@ -60,7 +60,7 @@ public:
     // purged. Large values disable the feature (as the ring buffer of flush timestamps would be
     // large). This is currently the default until we decide to enable this feature
     // of the cache by default.
-    static const int    kDefaultMaxUnusedFlushes    = 1024;
+    static const int    kDefaultMaxUnusedFlushes    = 64;
 
     /** Used to access functionality needed by GrGpuResource for lifetime management. */
     class ResourceAccess;
