e848ff59651e4441b2c303ef14db10fd678cee9f
reed <reed@google.com>
2015-12-18 05:23:09 -0800

    Revert of Create a hash table from id<-->key in SkImageFilter::CacheImpl (patchset #7 id:120001 of https://codereview.chromium.org/1514893003/ )
    Reason for revert:
    speculative revert to try to unblock DEPS roll
    
    https://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_asan_rel_ng/builds/94665
    
    Original issue's description:
    > Create a hash table from id<-->key in SkImageFilter::CacheImpl
    >
    > There is memory leak in the SkImageFilter::Cache. There are two sources
    > of memory leak:
    > 1. The cache filling up quickly.
    > 2. A slow small leak that never stops.
    > This CL solves the first issue, which prevents the cache filling up quickly.
    > This CL creates a new hash table that index the
    > SkImageFilter::uniqueID to an array of keys, and with the existing
    > key<-->Value hash table, we can have SkImageFilters proactively
    > purge content derived cached content when destroyed.
    >
    > BUG=489543
    > GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1514893003
    >
    > Committed: https://skia.googlesource.com/skia/+/f5d1f8dcc841516d7ea63c151b13059af40ca76d
    
    TBR=mtklein@google.com,junov@chromium.org,xidachen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=489543
    
    Review URL: https://codereview.chromium.org/1537923002

--- include/core/SkImageFilter.h
+++ include/core/SkImageFilter.h
@@ -42,7 +42,6 @@ public:
         virtual bool get(const Key& key, SkBitmap* result, SkIPoint* offset) const = 0;
         virtual void set(const Key& key, const SkBitmap& result, const SkIPoint& offset) = 0;
         virtual void purge() {}
-        virtual void purgeByImageFilterId(uint32_t) {}
     };
 
     enum SizeConstraint {
