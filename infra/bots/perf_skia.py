f2e2a1e290ad6de558fd26965f2f5c60c9828014
borenet <borenet@chromium.org>
2016-03-08 05:33:03 -0800

    Swarming bots: fix trybots
    Requires a recipe-side change as well.
    
    NOTRY=true
    BUG=skia:4763
    GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1768333002
    
    Review URL: https://codereview.chromium.org/1768333002

--- infra/bots/perf_skia.py
+++ infra/bots/perf_skia.py
@@ -21,11 +21,14 @@ def main():
   parser.add_argument('--slave_name', required=True)
   parser.add_argument('--revision', required=True)
   parser.add_argument('--swarm_out_dir', required=True)
+  parser.add_argument('--issue')
+  parser.add_argument('--patchset')
   args = parser.parse_args()
   with utils.print_timings():
     bot = common.BotInfo(args.builder_name, os.path.abspath(args.swarm_out_dir))
     bot.perf_steps(args.revision, args.master_name, args.slave_name,
-                   args.build_number)
+                   args.build_number, issue=args.issue or None,
+                   patchset=args.patchset or None)
 
 
 if __name__ == '__main__':
