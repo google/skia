308febaebd7048041ecd3ef4487e893238119d08
bsalomon <bsalomon@google.com>
2016-05-03 10:11:55 -0700

    Revert of Make GrStyle more sk_sp savy (patchset #1 id:1 of https://codereview.chromium.org/1947543002/ )
    Reason for revert:
    Crashing Ubuntu/GCC bot (but not clang)
    
    Original issue's description:
    > Make GrStyle more sk_sp savy
    > GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1947543002
    >
    > Committed: https://skia.googlesource.com/skia/+/46db22f1cfc65a7a4f1de13d774718347362f60f
    
    TBR=robertphillips@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/1943973002

--- src/gpu/GrStyle.cpp
+++ src/gpu/GrStyle.cpp
@@ -7,7 +7,7 @@
 
 #include "GrStyle.h"
 
-void GrStyle::initPathEffect(sk_sp<SkPathEffect> pe) {
+void GrStyle::initPathEffect(SkPathEffect* pe) {
     if (!pe) {
         fDashInfo.fType = SkPathEffect::kNone_DashType;
         return;
@@ -17,7 +17,7 @@ void GrStyle::initPathEffect(sk_sp<SkPathEffect> pe) {
         if (fStrokeRec.getStyle() == SkStrokeRec::kFill_Style) {
             fPathEffect.reset(nullptr);
         } else {
-            fPathEffect = std::move(pe);
+            fPathEffect.reset(SkSafeRef(pe));
             fDashInfo.fType = SkPathEffect::kDash_DashType;
             fDashInfo.fIntervals.reset(info.fCount);
             fDashInfo.fPhase = info.fPhase;
@@ -26,7 +26,7 @@ void GrStyle::initPathEffect(sk_sp<SkPathEffect> pe) {
             return;
         }
     } else {
-        fPathEffect = std::move(pe);
+        fPathEffect.reset(SkSafeRef(pe));
     }
     fDashInfo.fType = SkPathEffect::kNone_DashType;
     fDashInfo.fIntervals.reset(0);
