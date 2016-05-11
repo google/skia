2139303e4c0a9cbcfac695977a80eb026a9296ab
brianosman <brianosman@google.com>
2016-05-10 20:29:15 -0700

    Make GrGammaEffect have explicit sRGB modes, plus exponential mode.
    Convert it to a "standard" FP that just transforms the input color.
    
    For now, we still infer the sRGB transfer curves from the exponent,
    but I'm hoping that eventually SkGammas will provide us with the
    exact curve we're supposed to be applying. In any case, this adds
    support for doing the inverse transformation, as well, which will
    be needed in an upcoming Vulkan YUV change, among other things.
    
    BUG=skia:
    GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1954863002
    
    Review-Url: https://codereview.chromium.org/1954863002

--- src/gpu/effects/GrGammaEffect.h
+++ src/gpu/effects/GrGammaEffect.h
@@ -8,35 +8,40 @@
 #ifndef GrGammaEffect_DEFINED
 #define GrGammaEffect_DEFINED
 
-#include "GrSingleTextureEffect.h"
+#include "GrFragmentProcessor.h"
 
-class GrGammaEffect : public GrSingleTextureEffect {
+class GrGammaEffect : public GrFragmentProcessor {
 public:
+    enum class Mode {
+        kLinearToSRGB,
+        kSRGBToLinear,
+        kExponential,
+    };
+
     /**
-     * Creates an effect that applies a gamma curve. The source texture is always
-     * sampled unfiltered and with clamping.
-     */
-    static const GrFragmentProcessor* Create(GrTexture*, SkScalar gamma);
+    * Creates an effect that applies a gamma curve.
+    */
+    static const GrFragmentProcessor* Create(SkScalar gamma);
 
     const char* name() const override { return "Gamma"; }
 
-    bool gammaIsSRGB() const { return fGammaIsSRGB; }
+    Mode mode() const { return fMode; }
     SkScalar gamma() const { return fGamma; }
 
 private:
-    GrGammaEffect(GrTexture*, SkScalar gamma);
+    GrGammaEffect(Mode mode, SkScalar gamma);
 
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
     void onGetGLSLProcessorKey(const GrGLSLCaps&, GrProcessorKeyBuilder*) const override;
     bool onIsEqual(const GrFragmentProcessor&) const override;
     void onComputeInvariantOutput(GrInvariantOutput* inout) const override;
 
-    bool fGammaIsSRGB;
+    Mode fMode;
     SkScalar fGamma;
 
     GR_DECLARE_FRAGMENT_PROCESSOR_TEST;
 
-    typedef GrSingleTextureEffect INHERITED;
+    typedef GrFragmentProcessor INHERITED;
 };
 
 #endif
