d584d46c9943c641e0111b969b400be06351a949
bsalomon <bsalomon@google.com>
2016-05-16 09:09:18 -0700

    Run GM benchs in bench mode GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1978253002
    Review-Url: https://codereview.chromium.org/1978253002

--- bench/GMBench.cpp
+++ bench/GMBench.cpp
@@ -22,6 +22,7 @@ bool GMBench::isSuitableFor(Backend backend) {
 }
 
 void GMBench::onDraw(int loops, SkCanvas* canvas) {
+    fGM->setMode(skiagm::GM::kBench_Mode);
     // Do we care about timing the draw of the background (once)?
     // Does the GM ever rely on drawBackground to lazily compute something?
     fGM->drawBackground(canvas);
