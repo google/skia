7888f780d85ced5c572f0227c16b88ed59bcffe5
brianosman <brianosman@google.com>
2016-05-17 11:47:32 -0700

    Set the "mutable format" bit on sRGB images at creation time.
    This allows us to later create an image view with a linear format, for
    legacy-mode non-sRGB access.
    
    No actual API change (just a comment change in public header).
    
    TBR=bsalomon@google.com
    BUG=skia:5308
    GOLD_TRYBOT_URL= https://gold.skia.org/search2?unt=true&query=source_type%3Dgm&master=false&issue=1981333004
    
    Review-Url: https://codereview.chromium.org/1981333004

--- include/gpu/vk/GrVkTypes.h
+++ include/gpu/vk/GrVkTypes.h
@@ -30,6 +30,10 @@
  * Vulkan textures are really const GrVkImageInfo*
  */
 struct GrVkImageInfo {
+    /**
+     * If the image's format is sRGB (GrVkFormatIsSRGB returns true), then the image must have
+     * been created with VkImageCreateFlags containing VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT.
+     */
     VkImage        fImage;
     VkDeviceMemory fAlloc;    // can be VK_NULL_HANDLE iff Tex is an RT and uses borrow semantics
     VkImageTiling  fImageTiling;
