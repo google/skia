ce8ea4c55b6ecaf7132edce46b1c1ed38d7437fd
jcgregorio <jcgregorio@google.com>
2016-05-31 12:51:31 -0700

    skiaserve: Add /quitquitquit handler. Will be used in the hosted debugger.
    BUG=skia:
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2023023002
    TBR=ethannicholas
    
    Review-Url: https://codereview.chromium.org/2023023002

--- tools/skiaserve/urlhandlers/UrlHandler.h
+++ tools/skiaserve/urlhandlers/UrlHandler.h
@@ -154,3 +154,12 @@ public:
                const char* url, const char* method,
                const char* upload_data, size_t* upload_data_size) override;
 };
+
+class QuitHandler : public UrlHandler {
+public:
+    bool canHandle(const char* method, const char* url) override;
+    int handle(Request* request, MHD_Connection* connection,
+               const char* url, const char* method,
+               const char* upload_data, size_t* upload_data_size) override;
+};
+
