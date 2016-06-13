be36d59e664513294c02ff9691a3c1817a712fee
msarett <msarett@google.com>
2016-06-13 11:25:03 -0700

    Fix SkImage::asLegacyBitmap() to respect color space info
    BUG=skia:5428
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2061783002
    
    Review-Url: https://codereview.chromium.org/2061783002

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -319,8 +319,8 @@ bool SkImage::asLegacyBitmap(SkBitmap* bitmap, LegacyBitmapMode mode) const {
 bool SkImage_Base::onAsLegacyBitmap(SkBitmap* bitmap, LegacyBitmapMode mode) const {
     // As the base-class, all we can do is make a copy (regardless of mode).
     // Subclasses that want to be more optimal should override.
-    SkImageInfo info = SkImageInfo::MakeN32(this->width(), this->height(),
-                                    this->isOpaque() ? kOpaque_SkAlphaType : kPremul_SkAlphaType);
+    SkImageInfo info = this->onImageInfo().makeColorType(kN32_SkColorType)
+            .makeAlphaType(this->isOpaque() ? kOpaque_SkAlphaType : kPremul_SkAlphaType);
     if (!bitmap->tryAllocPixels(info)) {
         return false;
     }
