d4d0d1f4f338f9513e925eb022dd15253eb2b4db
Matthew Leibowitz <mattleibow@live.com>
2016-07-12 03:31:52 +0200

    Added empty C files

---
+++ include/c/xamarin/sk_x_patheffect.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2016 Xamarin Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// EXPERIMENTAL EXPERIMENTAL EXPERIMENTAL EXPERIMENTAL
+// DO NOT USE -- FOR INTERNAL TESTING ONLY
+
+#ifndef sk_x_patheffect_DEFINED
+#define sk_x_patheffect_DEFINED
+
+#include "sk_types.h"
+#include "xamarin/sk_x_types.h"
+
+SK_C_PLUS_PLUS_BEGIN_GUARD
+
+//
+
+SK_C_PLUS_PLUS_END_GUARD
+
+#endif
---
+++ src/c/xamarin/sk_x_patheffect.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2016 Xamarin Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkPath.h"
+
+#include "xamarin/sk_x_patheffect.h"
+
+#include "../sk_types_priv.h"
+#include "sk_x_types_priv.h"
+
+//
