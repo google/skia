aec570e2d72cbb861be18dddd1a472b93f3b87d0
mtklein <mtklein@chromium.org>
2016-07-22 08:35:27 -0700

    Don't ask GLX for EGL procs.
    Seems to fix my woes.
    
    BUG=skia:
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2168293003
    
    Review-Url: https://codereview.chromium.org/2168293003

--- src/gpu/gl/glx/GrGLCreateNativeInterface_glx.cpp
+++ src/gpu/gl/glx/GrGLCreateNativeInterface_glx.cpp
@@ -13,6 +13,12 @@
 #include <GL/glx.h>
 
 static GrGLFuncPtr glx_get(void* ctx, const char name[]) {
+    // Avoid calling glXGetProcAddress() for EGL procs.
+    // We don't expect it to ever succeed, but somtimes it returns non-null anyway.
+    if (0 == strncmp(name, "egl", 3)) {
+        return nullptr;
+    }
+
     SkASSERT(nullptr == ctx);
     SkASSERT(glXGetCurrentContext());
     return glXGetProcAddress(reinterpret_cast<const GLubyte*>(name));
