a825d5317c87784fa4ee7ff8e51ec3c2a9e0d2e3
egdaniel <egdaniel@google.com>
2016-08-11 16:17:39 -0700

    Revert of Fix 4444 on Vulkan devices who don't support RGBA_4444 (patchset #4 id:60001 of https://codereview.chromium.org/2238563002/ )
    Reason for revert:
    test failing on some gpus
    
    Original issue's description:
    > Fix 4444 on Vulkan devices who don't support RGBA_4444
    >
    > BUG=skia:
    > GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2238563002
    >
    > Committed: https://skia.googlesource.com/skia/+/637b3bf2b9c10398d823bd015a722842d4f2f971
    
    TBR=bsalomon@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:
    
    Review-Url: https://codereview.chromium.org/2240713003

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -137,16 +137,7 @@ void GrVkCaps::initGLSLCaps(const VkPhysicalDeviceProperties& properties,
             glslCaps->fConfigTextureSwizzle[i] = GrSwizzle::RRRR();
             glslCaps->fConfigOutputSwizzle[i] = GrSwizzle::AAAA();
         } else {
-            if (kRGBA_4444_GrPixelConfig == config) {
-                // The vulkan spec does not require R4G4B4A4 to be supported for texturing so we
-                // store the data in a B4G4R4A4 texture and then swizzle it when doing texture reads
-                // or writing to outputs. Since we're not actually changing the data at all, the
-                // only extra work is the swizzle in the shader for all operations.
-                glslCaps->fConfigTextureSwizzle[i] = GrSwizzle::BGRA();
-                glslCaps->fConfigOutputSwizzle[i] = GrSwizzle::BGRA();
-            } else {
-                glslCaps->fConfigTextureSwizzle[i] = GrSwizzle::RGBA();
-            }
+            glslCaps->fConfigTextureSwizzle[i] = GrSwizzle::RGBA();
         }
     }
 
