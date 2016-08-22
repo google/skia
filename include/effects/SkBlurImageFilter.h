e426babe7552b1cb4e27cdf4e90826feabb9e3b0
vjiaoblack <vjiaoblack@google.com>
2016-08-22 10:51:27 -0700

    Moving SkBlurImageFilter into core
    BUG=skia:
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2255803003
    
    Committed: https://skia.googlesource.com/skia/+/dd3259eb95c3b47e11eefa3b176365a112a32b48
    Review-Url: https://codereview.chromium.org/2255803003

--- include/effects/SkBlurImageFilter.h
+++ include/effects/SkBlurImageFilter.h
@@ -9,40 +9,14 @@
 #define SkBlurImageFilter_DEFINED
 
 #include "SkImageFilter.h"
-#include "SkSize.h"
 
-class SK_API SkBlurImageFilter : public SkImageFilter {
+class SK_API SkBlurImageFilter {
 public:
     static sk_sp<SkImageFilter> Make(SkScalar sigmaX, SkScalar sigmaY,
                                      sk_sp<SkImageFilter> input,
-                                     const CropRect* cropRect = nullptr);
-
-    SkRect computeFastBounds(const SkRect&) const override;
-
-    SK_TO_STRING_OVERRIDE()
-    SK_DECLARE_PUBLIC_FLATTENABLE_DESERIALIZATION_PROCS(SkBlurImageFilter)
-
-#ifdef SK_SUPPORT_LEGACY_IMAGEFILTER_PTR
-    static SkImageFilter* Create(SkScalar sigmaX, SkScalar sigmaY, SkImageFilter* input = nullptr,
-                                 const CropRect* cropRect = nullptr) {
-        return Make(sigmaX, sigmaY, sk_ref_sp<SkImageFilter>(input), cropRect).release();
+                                     const SkImageFilter::CropRect* cropRect = nullptr) {
+        return SkImageFilter::MakeBlur(sigmaX, sigmaY, input, cropRect);
     }
-#endif
-
-protected:
-    void flatten(SkWriteBuffer&) const override;
-    sk_sp<SkSpecialImage> onFilterImage(SkSpecialImage* source, const Context&,
-                                        SkIPoint* offset) const override;
-    SkIRect onFilterNodeBounds(const SkIRect& src, const SkMatrix&, MapDirection) const override;
-
-private:
-    SkBlurImageFilter(SkScalar sigmaX,
-                      SkScalar sigmaY,
-                      sk_sp<SkImageFilter> input,
-                      const CropRect* cropRect);
-
-    SkSize   fSigma;
-    typedef SkImageFilter INHERITED;
 };
 
 #endif
