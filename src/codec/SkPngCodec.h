c0444615ed76360f680619ad4d1f92cda6181a50
msarett <msarett@google.com>
2016-09-16 11:45:58 -0700

    Support Float32 output from SkColorSpaceXform
    * Adds Float32 support to SkColorSpaceXform
    * Changes API to allows clients to ask for F32, updates clients to
      new API
    * Adds Sk4f_load4 and Sk4f_store4 to SkNx
    * Make use of new xform in SkGr.cpp
    
    BUG=skia:
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2339233003
    CQ_INCLUDE_TRYBOTS=master.client.skia:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD-Trybot
    
    Committed: https://skia.googlesource.com/skia/+/43d6651111374b5d1e4ddd9030dcf079b448ec47
    Review-Url: https://codereview.chromium.org/2339233003

--- src/codec/SkPngCodec.h
+++ src/codec/SkPngCodec.h
@@ -64,7 +64,7 @@ protected:
     SkSwizzler* swizzler() { return fSwizzler; }
 
     // Initialize variables used by applyXformRow.
-    void initializeXformAlphaAndWidth();
+    void initializeXformParams();
 
     /**
      *  Pass available input to libpng to process it.
@@ -128,9 +128,10 @@ private:
     virtual void setRange(int firstRow, int lastRow, void* dst, size_t rowBytes) = 0;
     virtual Result decode(int* rowsDecoded) = 0;
 
-    XformMode   fXformMode;
-    SkAlphaType fXformAlphaType;
-    int         fXformWidth;
+    XformMode                      fXformMode;
+    SkColorSpaceXform::ColorFormat fXformColorFormat;
+    SkAlphaType                    fXformAlphaType;
+    int                            fXformWidth;
 
 #ifdef SK_GOOGLE3_PNG_HACK
     bool        fNeedsToRereadHeader;
