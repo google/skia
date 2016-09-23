1deeabe9ce5884bd89382c9487a51720219045cc
Mike Reed <reed@google.com>
2016-09-23 15:46:12 -0400

    replace Arithmetic xfermode with imagefilter
    BUG=skia:
    
    Change-Id: Ia3f3b721854c516f3b7f8c44f71f40a8a2eeb9b4

--- include/effects/SkXfermodeImageFilter.h
+++ include/effects/SkXfermodeImageFilter.h
@@ -26,6 +26,12 @@ public:
         return Make(std::move(mode), std::move(background), nullptr, nullptr);
     }
 
+    static sk_sp<SkImageFilter> MakeArithmetic(float k1, float k2, float k3, float k4,
+                                               bool enforcePMColor,
+                                               sk_sp<SkImageFilter> background,
+                                               sk_sp<SkImageFilter> foreground,
+                                               const SkImageFilter::CropRect* cropRect);
+
 #ifdef SK_SUPPORT_LEGACY_XFERMODE_PTR
     static SkImageFilter* Create(SkXfermode* mode, SkImageFilter* background,
                                  SkImageFilter* foreground = NULL,
