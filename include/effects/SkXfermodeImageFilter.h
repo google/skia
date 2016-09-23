9c08d7f22a59a302fa10928d44e7c225ae61f2eb
Mike Reed <reed@google.com>
2016-09-23 11:43:54 -0400

    change SkXfermodeImageFilter to carry no impl information
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2584
    
    Change-Id: Ibf2dfa7722348ae71a99f1c895de0d2996350e58

--- include/effects/SkXfermodeImageFilter.h
+++ include/effects/SkXfermodeImageFilter.h
@@ -10,19 +10,18 @@
 
 #include "SkImageFilter.h"
 
-class SkBitmap;
 class SkXfermode;
 
-class SK_API SkXfermodeImageFilter : public SkImageFilter {
-    /**
-     * This filter takes an xfermode, and uses it to composite the foreground
-     * over the background.  If foreground or background is NULL, the input
-     * bitmap (src) is used instead.
-     */
-
+/**
+ * This filter takes an xfermode, and uses it to composite the foreground
+ * over the background.  If foreground or background is NULL, the input
+ * bitmap (src) is used instead.
+ */
+class SK_API SkXfermodeImageFilter {
 public:
     static sk_sp<SkImageFilter> Make(sk_sp<SkXfermode> mode, sk_sp<SkImageFilter> background,
-                                     sk_sp<SkImageFilter> foreground, const CropRect* cropRect);
+                                     sk_sp<SkImageFilter> foreground,
+                                     const SkImageFilter::CropRect* cropRect);
     static sk_sp<SkImageFilter> Make(sk_sp<SkXfermode> mode, sk_sp<SkImageFilter> background) {
         return Make(std::move(mode), std::move(background), nullptr, nullptr);
     }
@@ -30,7 +29,7 @@ public:
 #ifdef SK_SUPPORT_LEGACY_XFERMODE_PTR
     static SkImageFilter* Create(SkXfermode* mode, SkImageFilter* background,
                                  SkImageFilter* foreground = NULL,
-                                 const CropRect* cropRect = NULL) {
+                                 const SkImageFilter::CropRect* cropRect = NULL) {
         return Make(sk_ref_sp(mode), 
                     sk_ref_sp(background),
                     sk_ref_sp(foreground),
@@ -39,7 +38,8 @@ public:
 #endif
 #ifdef SK_SUPPORT_LEGACY_IMAGEFILTER_PTR
     static sk_sp<SkImageFilter> Make(sk_sp<SkXfermode> mode, SkImageFilter* background,
-                                     SkImageFilter* foreground, const CropRect* cropRect) {
+                                     SkImageFilter* foreground,
+                                     const SkImageFilter::CropRect* cropRect) {
         return Make(std::move(mode),
                     sk_ref_sp(background),
                     sk_ref_sp(foreground),
@@ -50,30 +50,10 @@ public:
     }
 #endif
 
-    SK_TO_STRING_OVERRIDE()
-    SK_DECLARE_PUBLIC_FLATTENABLE_DESERIALIZATION_PROCS(SkXfermodeImageFilter)
-
-protected:
-    sk_sp<SkSpecialImage> onFilterImage(SkSpecialImage* source, const Context&,
-                                        SkIPoint* offset) const override;
-
-#if SK_SUPPORT_GPU
-    sk_sp<SkSpecialImage> filterImageGPU(SkSpecialImage* source,
-                                         sk_sp<SkSpecialImage> background,
-                                         const SkIPoint& backgroundOffset,
-                                         sk_sp<SkSpecialImage> foreground,
-                                         const SkIPoint& foregroundOffset,
-                                         const SkIRect& bounds,
-                                         const OutputProperties& outputProperties) const;
-#endif
-
-    SkXfermodeImageFilter(sk_sp<SkXfermode> mode, sk_sp<SkImageFilter> inputs[2],
-                          const CropRect* cropRect);
-    void flatten(SkWriteBuffer&) const override;
+    SK_DECLARE_FLATTENABLE_REGISTRAR_GROUP();
 
 private:
-    sk_sp<SkXfermode> fMode;
-    typedef SkImageFilter INHERITED;
+    SkXfermodeImageFilter();    // can't instantiate
 };
 
 #endif
