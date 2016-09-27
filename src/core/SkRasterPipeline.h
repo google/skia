b18b1a2c4aaab4337cbbf3cc87d9110d6eb4538c
Mike Klein <mtklein@chromium.org>
2016-09-27 17:36:49 -0400

    hack up simpler one function version of pipeline
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2717
    
    Change-Id: I45852a3e5d4c5b5e9315302c46601aee0d32265f

--- src/core/SkRasterPipeline.h
+++ src/core/SkRasterPipeline.h
@@ -53,25 +53,27 @@
  *
  * To add an EasyFn stage to the pipeline, call append<fn>() instead of append(&fn).
  * For the last stage of a pipeline, it's a slight performance benefit to call last<fn>().
+ *
+ * TODO: explain tail
  */
 
 class SkRasterPipeline {
 public:
     struct Stage;
-    using Fn = void(SK_VECTORCALL *)(Stage*, size_t, Sk4f,Sk4f,Sk4f,Sk4f,
-                                                     Sk4f,Sk4f,Sk4f,Sk4f);
-    using EasyFn = void(void*, size_t, Sk4f&, Sk4f&, Sk4f&, Sk4f&,
-                                       Sk4f&, Sk4f&, Sk4f&, Sk4f&);
+    using Fn = void(SK_VECTORCALL *)(Stage*, size_t, size_t, Sk4f,Sk4f,Sk4f,Sk4f,
+                                                             Sk4f,Sk4f,Sk4f,Sk4f);
+    using EasyFn = void(void*, size_t, size_t, Sk4f&, Sk4f&, Sk4f&, Sk4f&,
+                                               Sk4f&, Sk4f&, Sk4f&, Sk4f&);
 
     struct Stage {
         template <typename T>
         T ctx() { return static_cast<T>(fCtx); }
 
-        void SK_VECTORCALL next(size_t x, Sk4f v0, Sk4f v1, Sk4f v2, Sk4f v3,
-                                          Sk4f v4, Sk4f v5, Sk4f v6, Sk4f v7) {
+        void SK_VECTORCALL next(size_t x, size_t tail, Sk4f v0, Sk4f v1, Sk4f v2, Sk4f v3,
+                                                       Sk4f v4, Sk4f v5, Sk4f v6, Sk4f v7) {
             // Stages are logically a pipeline, and physically are contiguous in an array.
             // To get to the next stage, we just increment our pointer to the next array element.
-            fNext(this+1, x, v0,v1,v2,v3, v4,v5,v6,v7);
+            fNext(this+1, x,tail, v0,v1,v2,v3, v4,v5,v6,v7);
         }
 
         // It makes next() a good bit cheaper if we hold the next function to call here,
@@ -88,46 +90,21 @@ public:
     void run(size_t x, size_t n);
     void run(size_t n) { this->run(0, n); }
 
-    // Use this append() if your stage is sensitive to the number of pixels you're working with:
-    //   - body will always be called for a full 4 pixels
-    //   - tail will always be called for a single pixel
-    // Typically this is only an essential distintion for stages that read or write memory.
-    void append(Fn body, const void* body_ctx,
-                Fn tail, const void* tail_ctx);
+    // body() will only be called with tail=0, indicating it always works on a full 4 pixels.
+    // tail() will only be called with tail=1..3 to handle any jagged end of n % 4 pixels.
+    void append(Fn body, Fn tail, const void* ctx = nullptr);
 
-    // Most stages don't actually care if they're working on 4 or 1 pixel.
-    void append(Fn fn, const void* ctx = nullptr) {
-        this->append(fn, ctx, fn, ctx);
-    }
-
-    // Most 4 pixel or 1 pixel variants share the same context pointer.
-    void append(Fn body, Fn tail, const void* ctx = nullptr) {
-        this->append(body, ctx, tail, ctx);
-    }
-
-
-    // Versions of append that can be used with static EasyFns (see SK_RASTER_STAGE).
-    template <EasyFn body, EasyFn tail>
-    void append(const void* body_ctx, const void* tail_ctx) {
-        this->append(Easy<body>, body_ctx,
-                     Easy<tail>, tail_ctx);
-    }
-    template <EasyFn body, EasyFn tail>
-    void last(const void* body_ctx, const void* tail_ctx) {
-        this->append(Last<body>, body_ctx,
-                     Last<tail>, tail_ctx);
-    }
-
+    // Version of append that can be used with static EasyFn (see SK_RASTER_STAGE).
     template <EasyFn fn>
-    void append(const void* ctx = nullptr) { this->append<fn, fn>(ctx, ctx); }
+    void append(const void* ctx = nullptr) {
+        this->append(Body<fn>, Tail<fn>, ctx);
+    }
+
+    // If this is the last stage you're going to add, last() is a bit faster than append().
     template <EasyFn fn>
-    void last(const void* ctx = nullptr) { this->last<fn, fn>(ctx, ctx); }
-
-    template <EasyFn body, EasyFn tail>
-    void append(const void* ctx = nullptr) { this->append<body, tail>(ctx, ctx); }
-    template <EasyFn body, EasyFn tail>
-    void last(const void* ctx = nullptr) { this->last<body, tail>(ctx, ctx); }
-
+    void last(const void* ctx = nullptr) {
+        this->append(BodyLast<fn>, TailLast<fn>, ctx);
+    }
 
     // Append all stages to this pipeline.
     void extend(const SkRasterPipeline&);
@@ -138,22 +115,50 @@ private:
     // This no-op default makes fBodyStart and fTailStart unconditionally safe to call,
     // and is always the last stage's fNext as a sort of safety net to make sure even a
     // buggy pipeline can't walk off its own end.
-    static void SK_VECTORCALL JustReturn(Stage*, size_t, Sk4f,Sk4f,Sk4f,Sk4f,
-                                                         Sk4f,Sk4f,Sk4f,Sk4f);
+    static void SK_VECTORCALL JustReturn(Stage*, size_t, size_t, Sk4f,Sk4f,Sk4f,Sk4f,
+                                                                 Sk4f,Sk4f,Sk4f,Sk4f);
+
+    // These functions construct Fns out of EasyFn kernels.
+    //
+    // The Body versions pass 0 through literally into their kernel so that the
+    // optimizer can completely drop any "if (tail) { ... }" code.
+    //
+    // The Last versions of these functions return directly instead of chaining to the next stage.
 
     template <EasyFn kernel>
-    static void SK_VECTORCALL Easy(SkRasterPipeline::Stage* st, size_t x,
+    static void SK_VECTORCALL Body(SkRasterPipeline::Stage* st, size_t x, size_t tail,
                                    Sk4f  r, Sk4f  g, Sk4f  b, Sk4f  a,
                                    Sk4f dr, Sk4f dg, Sk4f db, Sk4f da) {
-        kernel(st->ctx<void*>(), x, r,g,b,a, dr,dg,db,da);
-        st->next(x, r,g,b,a, dr,dg,db,da);
+        kernel(st->ctx<void*>(), x,0, r,g,b,a, dr,dg,db,da);
+        st->next(x,tail, r,g,b,a, dr,dg,db,da);  // It's faster to pass tail than 0.
     }
 
     template <EasyFn kernel>
-    static void SK_VECTORCALL Last(SkRasterPipeline::Stage* st, size_t x,
+    static void SK_VECTORCALL Tail(SkRasterPipeline::Stage* st, size_t x, size_t tail,
                                    Sk4f  r, Sk4f  g, Sk4f  b, Sk4f  a,
                                    Sk4f dr, Sk4f dg, Sk4f db, Sk4f da) {
-        kernel(st->ctx<void*>(), x, r,g,b,a, dr,dg,db,da);
+    #if defined(__clang__)
+        __builtin_assume(tail > 0);
+    #endif
+        kernel(st->ctx<void*>(), x,tail, r,g,b,a, dr,dg,db,da);
+        st->next(x,tail, r,g,b,a, dr,dg,db,da);
+    }
+
+    template <EasyFn kernel>
+    static void SK_VECTORCALL BodyLast(SkRasterPipeline::Stage* st, size_t x, size_t tail,
+                                       Sk4f  r, Sk4f  g, Sk4f  b, Sk4f  a,
+                                       Sk4f dr, Sk4f dg, Sk4f db, Sk4f da) {
+        kernel(st->ctx<void*>(), x,0, r,g,b,a, dr,dg,db,da);
+    }
+
+    template <EasyFn kernel>
+    static void SK_VECTORCALL TailLast(SkRasterPipeline::Stage* st, size_t x, size_t tail,
+                                       Sk4f  r, Sk4f  g, Sk4f  b, Sk4f  a,
+                                       Sk4f dr, Sk4f dg, Sk4f db, Sk4f da) {
+    #if defined(__clang__)
+        __builtin_assume(tail > 0);
+    #endif
+        kernel(st->ctx<void*>(), x,tail, r,g,b,a, dr,dg,db,da);
     }
 
     Stages fBody,
@@ -164,9 +169,9 @@ private:
 
 // These are always static, and we _really_ want them to inline.
 // If you find yourself wanting a non-inline stage, write a SkRasterPipeline::Fn directly.
-#define SK_RASTER_STAGE(name)                                       \
-    static SK_ALWAYS_INLINE void name(void* ctx, size_t x,          \
-                            Sk4f&  r, Sk4f&  g, Sk4f&  b, Sk4f&  a, \
+#define SK_RASTER_STAGE(name)                                           \
+    static SK_ALWAYS_INLINE void name(void* ctx, size_t x, size_t tail, \
+                            Sk4f&  r, Sk4f&  g, Sk4f&  b, Sk4f&  a,     \
                             Sk4f& dr, Sk4f& dg, Sk4f& db, Sk4f& da)
 
 #endif//SkRasterPipeline_DEFINED
