974ccfab295f21e574017165bd88e09f4ed5ffb3
Mike Klein <mtklein@chromium.org>
2016-09-29 16:01:01 -0400

    SkRasterPipeline: first steps into AVX
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2821
    CQ_INCLUDE_TRYBOTS=master.client.skia:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD-Trybot
    
    Change-Id: Ib3485591bd69657c90478e12348f8deeb83c68c6

--- src/opts/SkOpts_avx.cpp
+++ src/opts/SkOpts_avx.cpp
@@ -8,7 +8,21 @@
 #include "SkOpts.h"
 
 #define SK_OPTS_NS avx
+#include "SkRasterPipeline_opts.h"
 
 namespace SkOpts {
-    void Init_avx() { }
+    void Init_avx() {
+
+    #define STAGE(stage, kCallNext) \
+        stages_8  [SkRasterPipeline::stage] = stage_8  <SK_OPTS_NS::stage, kCallNext>; \
+        stages_1_7[SkRasterPipeline::stage] = stage_1_7<SK_OPTS_NS::stage, kCallNext>
+
+        STAGE(constant_color, true);
+        STAGE(lerp_constant_float , true);
+        STAGE(srcover , true);
+
+        STAGE(scale_u8, true);
+
+    #undef STAGE
+    }
 }
