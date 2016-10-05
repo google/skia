dbbf27f874af4f9b472a7022f4f566cd95feb53e
Mike Klein <mtklein@chromium.org>
2016-10-05 13:14:21 -0400

    SkRasterPipeline: 8x pipelines
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2990
    CQ_INCLUDE_TRYBOTS=master.client.skia:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD-Trybot
    
    Change-Id: I1c82e5755d8e44cc0b9c6673d04b117f85d71a3a

--- src/opts/SkOpts_hsw.cpp
+++ src/opts/SkOpts_hsw.cpp
@@ -8,8 +8,69 @@
 #include "SkOpts.h"
 
 #define SK_OPTS_NS hsw
+#include "SkRasterPipeline_opts.h"
 
 namespace SkOpts {
-    void Init_hsw() { }
+    void Init_hsw() {
+
+    #define STAGE(stage, kCallNext) \
+        stages_8[SkRasterPipeline::stage] = stage_8<SK_OPTS_NS::stage, kCallNext>; \
+        stages_7[SkRasterPipeline::stage] = stage_7<SK_OPTS_NS::stage, kCallNext>
+
+        STAGE(just_return, false);
+/*
+        STAGE(store_565 , false);
+        STAGE(store_srgb, false);
+        STAGE(store_f16 , false);
+
+        STAGE(load_s_565 , true);
+        STAGE(load_s_srgb, true);
+        STAGE(load_s_f16 , true);
+
+        STAGE(load_d_565 , true);
+        STAGE(load_d_srgb, true);
+        STAGE(load_d_f16 , true);
+
+        STAGE(scale_u8, true);
+
+        STAGE(lerp_u8            , true);
+        STAGE(lerp_565           , true);
+        */
+        STAGE(lerp_constant_float, true);
+
+        STAGE(constant_color, true);
+
+    #undef STAGE
+
+    #define STAGE(stage) \
+        stages_8[SkRasterPipeline::stage] = SK_OPTS_NS::stage; \
+        stages_7[SkRasterPipeline::stage] = SK_OPTS_NS::stage
+
+        STAGE(dst);
+        STAGE(dstatop);
+        STAGE(dstin);
+        STAGE(dstout);
+        STAGE(dstover);
+        STAGE(srcatop);
+        STAGE(srcin);
+        STAGE(srcout);
+        STAGE(srcover);
+        STAGE(clear);
+        STAGE(modulate);
+        STAGE(multiply);
+        STAGE(plus_);
+        STAGE(screen);
+        STAGE(xor_);
+        STAGE(colorburn);
+        STAGE(colordodge);
+        STAGE(darken);
+        STAGE(difference);
+        STAGE(exclusion);
+        STAGE(hardlight);
+        STAGE(lighten);
+        STAGE(overlay);
+        STAGE(softlight);
+    #undef STAGE
+    }
 }
 
