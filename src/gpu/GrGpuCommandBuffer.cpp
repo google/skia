2c9a3c57e57325f73263ed37439ea634be70e494
Greg Daniel <egdaniel@google.com>
2016-10-10 13:16:57 -0400

    Don't pass in RT to individual Gpu CommandBuffer calls
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3126
    
    Change-Id: I571de4bfb1da612d61171321d5224a9a19d8e545

--- src/gpu/GrGpuCommandBuffer.cpp
+++ src/gpu/GrGpuCommandBuffer.cpp
@@ -19,21 +19,19 @@ void GrGpuCommandBuffer::submit(const SkIRect& bounds) {
     this->onSubmit(bounds);
 }
 
-void GrGpuCommandBuffer::clear(const GrFixedClip& clip, GrColor color, GrRenderTarget* rt) {
+void GrGpuCommandBuffer::clear(const GrFixedClip& clip, GrColor color) {
+    GrRenderTarget* rt = this->renderTarget();
     SkASSERT(rt);
     SkASSERT(!clip.scissorEnabled() ||
              (SkIRect::MakeWH(rt->width(), rt->height()).contains(clip.scissorRect()) &&
               SkIRect::MakeWH(rt->width(), rt->height()) != clip.scissorRect()));
-    this->onClear(rt, clip, color);
+    this->onClear(clip, color);
 }
 
 void GrGpuCommandBuffer::clearStencilClip(const GrFixedClip& clip,
-                                          bool insideStencilMask,
-                                          GrRenderTarget* rt) {
-    SkASSERT(rt);
-    this->onClearStencilClip(rt, clip, insideStencilMask);
+                                          bool insideStencilMask) {
+    this->onClearStencilClip(clip, insideStencilMask);
 }
-
 
 bool GrGpuCommandBuffer::draw(const GrPipeline& pipeline,
                               const GrPrimitiveProcessor& primProc,
