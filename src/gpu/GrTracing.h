b211b4076138598ffad08376326e2301ce36c2bc
Greg Daniel <egdaniel@google.com>
2016-10-12 12:25:53 -0400

    Add systrace support to gpu backend.
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3269
    
    Change-Id: I5d1364535cf846fbdfffb5b7db1ef308bef64791

--- src/gpu/GrTracing.h
+++ src/gpu/GrTracing.h
@@ -13,6 +13,59 @@
 #include "SkTLazy.h"
 #include "SkTraceEvent.h"
 
+#ifdef SK_BUILD_FOR_ANDROID
+#include "android/trace.h"
+#include <dlfcn.h>
+
+/**
+ * This class supports the use of ATrace for apps running on android. To actually use the trace, you
+ * need to have the app running of the device (e.g. Viewer), and the run systrace from the command
+ * line. (e.g. <android_sdk>/platform-tools/systrace/systrace.py -a org.skia.viewer -b 4096 -t 5
+ *                                                               -o out.html sched gfx wm view freq)
+ */
+class GrGpuATrace : public SkNoncopyable {
+public:
+    static void *(*ATrace_beginSection) (const char* sectionName);
+    static void *(*ATrace_endSection) (void);
+    static void *(*ATrace_isEnabled) (void);
+
+    typedef void *(*fp_ATrace_beginSection) (const char* sectionName);
+    typedef void *(*fp_ATrace_endSection) (void);
+    typedef void *(*fp_ATrace_isEnabled) (void);
+
+    static void Init() {
+        void *lib = dlopen("libandroid.so", RTLD_NOW | RTLD_LOCAL);
+        if (lib != NULL) {
+            ATrace_beginSection = reinterpret_cast<fp_ATrace_beginSection>(
+                dlsym(lib, "ATrace_beginSection"));
+            ATrace_endSection = reinterpret_cast<fp_ATrace_endSection>(
+                dlsym(lib, "ATrace_endSection"));
+            ATrace_isEnabled = reinterpret_cast<fp_ATrace_isEnabled>(
+                dlsym(lib, "ATrace_isEnabled"));
+        }
+    }
+
+    GrGpuATrace(const char* str) {
+        if (ATrace_isEnabled != nullptr && ATrace_isEnabled()) {
+            ATrace_beginSection(str);
+        }
+    }
+
+    ~GrGpuATrace() {
+        if (ATrace_isEnabled != nullptr && ATrace_isEnabled()) {
+            ATrace_endSection();
+        }
+    }
+};
+
+    #define GR_INIT_ATRACE GrGpuATrace::Init()
+    #define GR_ATRACE(str) GrGpuATrace SK_MACRO_APPEND_LINE(GAT)(str)
+#else // ifdef SK_BUILD_FOR_ANDROID
+    #define GR_INIT_ATRACE
+    #define GR_ATRACE(str)
+#endif
+
+
 /**
  * Marker generation class used for adding and removing markers around code blocks
  */
