418b7971bfb2b210806d1ada7a6882d6eb088b0a
Brian Salomon <bsalomon@google.com>
2016-10-14 15:34:23 -0400

    Rework GL fbo blit limitations as finer grained flags.
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3480
    
    Change-Id: Idd86f96a92420ec98c7cf07962f4857e0b81b9c6

--- src/gpu/gl/GrGLCaps.h
+++ src/gpu/gl/GrGLCaps.h
@@ -73,6 +73,7 @@ public:
         kLast_MSFBOType = kMixedSamples_MSFBOType
     };
 
+#if 0
     enum BlitFramebufferSupport {
         kNone_BlitFramebufferSupport,
         /**
@@ -86,6 +87,16 @@ public:
          */
         kRectsAndFormatsMatchForMSAASrc_BlitFramebufferSupport,
         kFull_BlitFramebufferSupport
+    };
+#endif
+    enum BlitFramebufferFlags {
+        kNoSupport_BlitFramebufferFlag                    = 1 << 0,
+        kNoScalingOrMirroring_BlitFramebufferFlag         = 1 << 1,
+        kResolveMustBeFull_BlitFrambufferFlag             = 1 << 2,
+        kNoMSAADst_BlitFramebufferFlag                    = 1 << 3,
+        kNoFormatConversion_BlitFramebufferFlag           = 1 << 4,
+        kNoFormatConversionForMSAASrc_BlitFramebufferFlag = 1 << 5,
+        kRectsMustMatchForMSAASrc_BlitFramebufferFlag     = 1 << 6,
     };
 
     enum InvalidateFBType {
@@ -235,7 +246,7 @@ public:
     /**
      * What functionality is supported by glBlitFramebuffer.
      */
-    BlitFramebufferSupport blitFramebufferSupport() const { return fBlitFramebufferSupport; }
+    uint32_t blitFramebufferSupport() const { return fBlitFramebufferFlags; }
 
     /**
      * Is the MSAA FBO extension one where the texture is multisampled when bound to an FBO and
@@ -419,7 +430,7 @@ private:
     bool fRGBAToBGRAReadbackConversionsAreSlow : 1;
     bool fDoManualMipmapping : 1;
 
-    BlitFramebufferSupport fBlitFramebufferSupport;
+    uint32_t fBlitFramebufferFlags;
 
     /** Number type of the components (with out considering number of bits.) */
     enum FormatType {
