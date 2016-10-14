ef7aa8743e2ab0140fe0f505481f72aebf2e6ead
Matt Sarett <msarett@google.com>
2016-10-14 11:14:48 -0400

    Cache dst LUTs in SkColorSpaceXform
    This is only useful in the rare case that the dst does not
    fall into one of our main paths.
    
    But it's a good optimization, since this does happen,
    and typically, the dst won't change.
    
    ColorCodecBench z620 --nonstd --xform_only
    Without Patch 511us
    With Patch    348us
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3400
    
    Change-Id: Ibf68d9ce7072680465662922f4aa15630545e3d6

--- src/core/SkColorSpaceXform_Base.h
+++ src/core/SkColorSpaceXform_Base.h
@@ -11,6 +11,19 @@
 #include "SkColorSpace.h"
 #include "SkColorSpace_Base.h"
 #include "SkColorSpaceXform.h"
+#include "SkResourceCache.h"
+
+class SkColorSpaceXform_Base : public SkColorSpaceXform {
+protected:
+    virtual bool onApply(ColorFormat dstFormat, void* dst, ColorFormat srcFormat, const void* src,
+                         int count, SkAlphaType alphaType) const = 0;
+
+    virtual void onSetCachedFields(const SkResourceCache::Rec& rec) = 0;
+
+private:
+    friend class SkColorSpaceXform;
+    friend struct SkColorSpaceXformRec;
+};
 
 enum SrcGamma {
     kLinear_SrcGamma,
@@ -31,7 +44,7 @@ enum ColorSpaceMatch {
 };
 
 template <SrcGamma kSrc, DstGamma kDst, ColorSpaceMatch kCSM>
-class SkColorSpaceXform_Base : public SkColorSpaceXform {
+class SkColorSpaceXform_XYZ : public SkColorSpaceXform_Base {
 public:
     static constexpr int kDstGammaTableSize = 1024;
 
@@ -39,21 +52,64 @@ protected:
     bool onApply(ColorFormat dstFormat, void* dst, ColorFormat srcFormat, const void* src,
                  int count, SkAlphaType alphaType) const override;
 
+    void onSetCachedFields(const SkResourceCache::Rec& rec) override;
+
 private:
-    SkColorSpaceXform_Base(SkColorSpace* srcSpace, const SkMatrix44& srcToDst,
-                           SkColorSpace* dstSpace);
+    SkColorSpaceXform_XYZ(SkColorSpace* srcSpace, const SkMatrix44& srcToDst,
+                          SkColorSpace* dstSpace);
 
     sk_sp<SkColorLookUpTable> fColorLUT;
 
     // Contain pointers into storage or pointers into precomputed tables.
     const float*              fSrcGammaTables[3];
     const uint8_t*            fDstGammaTables[3];
-    SkAutoMalloc              fStorage;
+    sk_sp<SkData>             fSrcStorage;
+    sk_sp<SkData>             fDstStorage;
 
     float                     fSrcToDst[16];
 
     friend class SkColorSpaceXform;
     friend std::unique_ptr<SkColorSpaceXform> SlowIdentityXform(SkColorSpace* space);
+};
+
+static void* gColorSpaceXformAddress;
+struct SkColorSpaceXformKey : public SkResourceCache::Key {
+public:
+    sk_sp<SkColorSpace> fDstSpace;
+
+    SkColorSpaceXformKey(SkColorSpace* dstSpace)
+        : fDstSpace(sk_ref_sp(dstSpace))
+    {
+        this->init(&gColorSpaceXformAddress, 0, sizeof(SkColorSpace*));
+    }
+};
+
+struct SkColorSpaceXformRec : public SkResourceCache::Rec {
+    SkColorSpaceXformKey fKey;
+
+    // No need to ref these pointers.  They point into storage, so are valid while
+    // storage is valid.
+    const uint8_t*       fDstGammaTables[3];
+    sk_sp<SkData>        fStorage;
+
+    SkColorSpaceXformRec(const SkColorSpaceXformKey& key, const uint8_t* dstTables[3],
+                         sk_sp<SkData> storage)
+        : fKey(key)
+        , fStorage(std::move(storage))
+    {
+        fDstGammaTables[0] = dstTables[0];
+        fDstGammaTables[1] = dstTables[1];
+        fDstGammaTables[2] = dstTables[2];
+    }
+
+    const Key& getKey() const override { return fKey; }
+    size_t bytesUsed() const override { return sizeof(SkColorSpaceXformRec) + fStorage->size(); }
+    const char* getCategory() const override { return "SkColorSpaceXform"; }
+
+    static bool Visitor(const SkResourceCache::Rec& rec, void* context) {
+        ((SkColorSpaceXform_Base*) context)->onSetCachedFields(rec);
+        return true;
+    }
 };
 
 // For testing.  Bypasses opts for when src and dst color spaces are equal.
