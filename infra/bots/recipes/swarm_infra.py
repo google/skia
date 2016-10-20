61013b235f474cf1c02d7b69ff36bd05700f9a38
borenet <borenet@chromium.org>
2016-10-20 11:38:30 -0700

    Add retries to the InfraTests bot's "update go pkgs" step
    BUG=skia:5874
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2437143002
    
    Review-Url: https://chromiumcodereview.appspot.com/2437143002

--- infra/bots/recipes/swarm_infra.py
+++ infra/bots/recipes/swarm_infra.py
@@ -15,16 +15,32 @@ DEPS = [
 ]
 
 
+UPDATE_GO_ATTEMPTS = 5
+
+
 def RunSteps(api):
   api.vars.setup()
   api.core.checkout_steps()
 
+  # Attempt to update go dependencies. This fails flakily sometimes, so perform
+  # multiple attempts.
   gopath = api.vars.checkout_root.join('gopath')
   env = {'GOPATH': gopath}
-  api.step('update_go_pkgs',
-           cmd=['go', 'get', '-u', 'go.skia.org/infra/...'],
-           env=env)
+  name = 'update go pkgs'
+  for attempt in xrange(UPDATE_GO_ATTEMPTS):
+    step_name = name
+    if attempt > 0:
+      step_name += ' (attempt %d)' % (attempt + 1)
+    try:
+      api.step(step_name,
+               cmd=['go', 'get', '-u', 'go.skia.org/infra/...'],
+               env=env)
+      break
+    except api.step.StepFailure:
+      if attempt == UPDATE_GO_ATTEMPTS - 1:
+        raise
 
+  # Run the infra tests.
   infra_tests = api.vars.skia_dir.join(
       'infra', 'bots', 'infra_tests.py')
   api.step('infra_tests',
@@ -43,4 +59,32 @@ def GenTests(api):
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]')
+  )
+
+  yield (
+    api.test('failed_one_update') +
+      api.properties(buildername='Housekeeper-PerCommit-InfraTests',
+                     mastername='client.skia.fyi',
+                     slavename='dummy-slave',
+                     buildnumber=5,
+                     revision='abc123',
+                     path_config='kitchen',
+                     swarm_out_dir='[SWARM_OUT_DIR]') +
+    api.step_data('update go pkgs', retcode=1)
+  )
+
+  yield (
+    api.test('failed_all_updates') +
+      api.properties(buildername='Housekeeper-PerCommit-InfraTests',
+                     mastername='client.skia.fyi',
+                     slavename='dummy-slave',
+                     buildnumber=5,
+                     revision='abc123',
+                     path_config='kitchen',
+                     swarm_out_dir='[SWARM_OUT_DIR]') +
+    api.step_data('update go pkgs', retcode=1) +
+    api.step_data('update go pkgs (attempt 2)', retcode=1) +
+    api.step_data('update go pkgs (attempt 3)', retcode=1) +
+    api.step_data('update go pkgs (attempt 4)', retcode=1) +
+    api.step_data('update go pkgs (attempt 5)', retcode=1)
   )
