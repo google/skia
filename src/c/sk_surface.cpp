63ad119e007dcb768fa8103a8cbd94d0610f0132
Tyler Hoeflicker <thoeflicker@bluebeam.com>
2016-10-24 11:22:34 -0700

    Add region support

--- src/c/sk_surface.cpp
+++ src/c/sk_surface.cpp
@@ -22,6 +22,7 @@
 #include "sk_path.h"
 #include "sk_picture.h"
 #include "sk_surface.h"
+#include "sk_region.h"
 
 #include "sk_types_priv.h"
 
@@ -94,3 +95,54 @@ sk_surface_t* sk_surface_new_render_target(gr_context_t* context, bool budgeted,
     }
     return ToSurface(SkSurface::MakeRenderTarget(AsGrContext(context), (SkBudgeted)budgeted, info, sampleCount, surfProps).release());
 }
+
+/* REGIONS */
+sk_region_t* sk_region_new() { return (sk_region_t*)new SkRegion(); }
+
+sk_region_t* sk_region_new2(const sk_region_t* region) { return (sk_region_t*)new SkRegion(*AsRegion(region)); }
+
+void sk_region_delete(sk_region_t* cpath) { delete AsRegion(cpath); }
+
+void sk_region_contains(sk_region_t* r, const sk_region_t* region) {
+	AsRegion(r)->contains(*AsRegion(region));
+}
+
+void sk_region_contains2(sk_region_t* r, int x, int y) {
+	AsRegion(r)->contains(x, y);
+}
+
+bool sk_region_intersects(sk_region_t* r, const sk_region_t* src) {
+	return AsRegion(r)->intersects(*AsRegion(src));
+}
+
+bool sk_region_set_path(sk_region_t* dst, const sk_path_t* t)
+{
+	SkRegion region = *AsRegion(dst);
+	return region.setPath(AsPath(*t), region);
+}
+
+bool sk_region_set_rect(sk_region_t* dst, const sk_irect_t* rect)
+{
+	SkRegion region = *AsRegion(dst);
+	return region.setRect(AsIRect(*rect));
+}
+
+bool sk_region_op(sk_region_t* dst, int left, int top, int right, int bottom, SkRegion::Op op)
+{
+	SkRegion region = *AsRegion(dst);
+	return region.op(left, top, right, bottom, op);
+}
+
+bool sk_region_op2(sk_region_t* dst, sk_region_t* src, SkRegion::Op op)
+{
+	SkRegion region = *AsRegion(dst);
+	return region.op(*AsRegion(src), op);
+}
+
+sk_irect_t sk_region_get_bounds(sk_region_t* r)
+{
+	SkRegion region = *AsRegion(r);
+	return ToIRect(AsRegion(r)->getBounds());
+}
+
+/*END REGIONS*/
