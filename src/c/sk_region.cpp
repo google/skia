cf696ea4ec7eda974739ab78fee84075799773ef
Matthew Leibowitz <mattleibow@live.com>
2016-10-25 06:29:12 +0200

    Use the correct referencing.

--- src/c/sk_region.cpp
+++ src/c/sk_region.cpp
@@ -15,13 +15,15 @@ sk_region_t *sk_region_new() {
 }
 
 sk_region_t *sk_region_new2(const sk_region_t *region) {
-  return ToRegion(new SkRegion(*AsRegion(region)));
+  return ToRegion(new SkRegion(AsRegion(*region)));
 }
 
-void sk_region_delete(sk_region_t *cpath) { delete AsRegion(cpath); }
+void sk_region_delete(sk_region_t *region) {
+  delete AsRegion(region);
+}
 
 void sk_region_contains(sk_region_t *r, const sk_region_t *region) {
-  AsRegion(r)->contains(*AsRegion(region));
+  AsRegion(r)->contains(AsRegion(*region));
 }
 
 void sk_region_contains2(sk_region_t *r, int x, int y) {
@@ -29,30 +31,25 @@ void sk_region_contains2(sk_region_t *r, int x, int y) {
 }
 
 bool sk_region_intersects(sk_region_t *r, const sk_region_t *src) {
-  return AsRegion(r)->intersects(*AsRegion(src));
+  return AsRegion(r)->intersects(AsRegion(*src));
 }
 
 bool sk_region_set_path(sk_region_t *dst, const sk_path_t *t, const sk_region_t* clip) {
-  SkRegion region = *AsRegion(dst);
-  return region.setPath(AsPath(*t), *AsRegion(clip));
+  return AsRegion(dst)->setPath(AsPath(*t), AsRegion(*clip));
 }
 
 bool sk_region_set_rect(sk_region_t *dst, const sk_irect_t *rect) {
-  SkRegion region = *AsRegion(dst);
-  return region.setRect(AsIRect(*rect));
+  return AsRegion(dst)->setRect(AsIRect(*rect));
 }
 
 bool sk_region_op(sk_region_t *dst, int left, int top, int right, int bottom, sk_region_op_t op) {
-  SkRegion region = *AsRegion(dst);
-  return region.op(left, top, right, bottom, (SkRegion::Op)op);
+  return AsRegion(dst)->op(left, top, right, bottom, (SkRegion::Op)op);
 }
 
 bool sk_region_op2(sk_region_t *dst, sk_region_t *src, sk_region_op_t op) {
-  SkRegion region = *AsRegion(dst);
-  return region.op(*AsRegion(src), (SkRegion::Op)op);
+  return AsRegion(dst)->op(AsRegion(*src), (SkRegion::Op)op);
 }
 
 sk_irect_t sk_region_get_bounds(sk_region_t *r) {
-  SkRegion region = *AsRegion(r);
   return ToIRect(AsRegion(r)->getBounds());
 }
