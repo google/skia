399a57a62119344cd747b8a4a008dffec88a4f8c
Kevin Lubick <kjlubick@google.com>
2016-10-26 14:11:17 -0400

    Make fuzzers use cleaner interface
    signalBoring() no longer exit(0); it uses longjmp to return making this
    compatible with libfuzzer
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3963
    
    Change-Id: Iebcfc14cc6b0a19c5dd015cd39875c81fa44003e

--- fuzz/FuzzParsePath.cpp
+++ fuzz/FuzzParsePath.cpp
@@ -39,9 +39,9 @@ static void add_white(Fuzz* fuzz, SkString* atom) {
         atom->append(" ");
         return;
     }
-    int reps = fuzz->nextRangeU(0, 2);
+    int reps = fuzz->nextRange(0, 2);
     for (int rep = 0; rep < reps; ++rep) {
-        int index = fuzz->nextRangeU(0, (int) SK_ARRAY_COUNT(gWhiteSpace) - 1);
+        int index = fuzz->nextRange(0, (int) SK_ARRAY_COUNT(gWhiteSpace) - 1);
         if (gWhiteSpace[index]) {
             atom->append(&gWhiteSpace[index], 1);
         }
@@ -72,15 +72,15 @@ static void add_some_white(Fuzz* fuzz, SkString* atom) {
 
 SkString MakeRandomParsePathPiece(Fuzz* fuzz) {
     SkString atom;
-    int index = fuzz->nextRangeU(0, (int) SK_ARRAY_COUNT(gLegal) - 1);
+    int index = fuzz->nextRange(0, (int) SK_ARRAY_COUNT(gLegal) - 1);
     const Legal& legal = gLegal[index];
     gEasy ? atom.append("\n") : add_white(fuzz, &atom);
     char symbol = legal.fSymbol | (fuzz->nextBool() ? 0x20 : 0);
     atom.append(&symbol, 1);
-    int reps = fuzz->nextRangeU(1, 3);
+    int reps = fuzz->nextRange(1, 3);
     for (int rep = 0; rep < reps; ++rep) {
         for (int index = 0; index < legal.fScalars; ++index) {
-            SkScalar coord = fuzz->nextRangeF(0, 100);
+            SkScalar coord = fuzz->nextRange(0.0f, 100.0f);
             add_white(fuzz, &atom);
             atom.appendScalar(coord);
             if (rep < reps - 1 && index < legal.fScalars - 1) {
@@ -89,11 +89,11 @@ SkString MakeRandomParsePathPiece(Fuzz* fuzz) {
                 add_some_white(fuzz, &atom);
             }
             if ('A' == legal.fSymbol && 1 == index) {
-                atom.appendScalar(fuzz->nextRangeF(-720, 720));
+                atom.appendScalar(fuzz->nextRange(-720.0f, 720.0f));
                 add_comma(fuzz, &atom);
-                atom.appendU32(fuzz->nextRangeU(0, 1));
+                atom.appendU32(fuzz->nextRange(0, 1));
                 add_comma(fuzz, &atom);
-                atom.appendU32(fuzz->nextRangeU(0, 1));
+                atom.appendU32(fuzz->nextRange(0, 1));
                 add_comma(fuzz, &atom);
             }
         }
@@ -104,7 +104,7 @@ SkString MakeRandomParsePathPiece(Fuzz* fuzz) {
 DEF_FUZZ(ParsePath, fuzz) {
     SkPath path;
     SkString spec;
-    uint32_t count = fuzz->nextRangeU(0, 40);
+    uint32_t count = fuzz->nextRange(0, 40);
     for (uint32_t i = 0; i < count; ++i) {
         spec.append(MakeRandomParsePathPiece(fuzz));
     }
