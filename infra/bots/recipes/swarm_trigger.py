a4dbddea93a6799e9ab3e43a30f4e3147c565fc1
Ben Wagner <benjaminwagner@google.com>
2016-10-26 10:34:30 -0400

    Add recipe support for Pixel and Pixel XL.
    BUG=skia:
    
    Change-Id: I782ef741838860bfeace13c335f9a82403047220

--- infra/bots/recipes/swarm_trigger.py
+++ infra/bots/recipes/swarm_trigger.py
@@ -131,6 +131,8 @@ def swarm_dimensions(builder_cfg):
         'Nexus7v2':      ('flo',        'M'),
         'Nexus9':        ('flounder',   'NRD91D'),
         'NexusPlayer':   ('fugu',       'NRD90R'),
+        'Pixel':         ('sailfish',   'NDE63P'),
+        'PixelXL':       ('marlin',     'NDE63P'),
       }[builder_cfg['model']]
       dimensions['device_type'] = device_type
       dimensions['device_os'] = device_os
