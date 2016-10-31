603737535e6096dbfedc2648ee31cded53a63aad
Kevin Lubick <kjlubick@google.com>
2016-10-31 12:52:54 -0400

    Make fuzzers use cleaner interface
    signalBoring() no longer exists.  When the fuzzer runs out of randomness,
    it just returns 0.  Fuzzers should not go into infinite loops if this
    happens.  do while loops are particularly error-prone.
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=3963
    
    Change-Id: Iebcfc14cc6b0a19c5dd015cd39875c81fa44003e

--- fuzz/Fuzz.h
+++ fuzz/Fuzz.h
@@ -12,8 +12,6 @@
 #include "SkTRegistry.h"
 #include "SkTypes.h"
 
-#include <vector>
-
 class Fuzz : SkNoncopyable {
 public:
     explicit Fuzz(sk_sp<SkData>);
@@ -24,38 +22,13 @@ public:
     size_t remaining();
 
     template <typename T>
-    bool SK_WARN_UNUSED_RESULT next(T* n);
+    T next();
 
-    // UBSAN reminds us that bool can only legally hold 0 or 1.
-    bool SK_WARN_UNUSED_RESULT next(bool* b) {
-        uint8_t byte;
-        if (!this->next(&byte)) {
-            return false;
-        }
-        *b = (byte & 1) == 1;
-        return true;
-    }
-
-    // The nextFoo methods are deprecated.
-    // TODO(kjlubick): replace existing uses with next() and remove these.
-    bool nextBool();
-    uint8_t  nextB();
-    uint32_t nextU();
-    // This can be nan, +- infinity, 0, anything.
-    float    nextF();
-    // Returns a float between [0..1) as a IEEE float
-    float    nextF1();
-
-    // Return the next fuzzed value [min, max) as an unsigned 32bit integer.
-    uint32_t nextRangeU(uint32_t min, uint32_t max);
-    /**
-     *  Returns next fuzzed value [min...max) as a float.
-     *  Will not be Infinity or NaN.
-     */
-    float    nextRangeF(float    min, float    max);
+    // nextRange returns values only in [min, max].
+    template <typename T>
+    T nextRange(T min, T max);
 
     void signalBug   ();  // Tell afl-fuzz these inputs found a bug.
-    void signalBoring();  // Tell afl-fuzz these inputs are not worth testing.
 
 private:
     template <typename T>
@@ -65,15 +38,39 @@ private:
     int fNextByte;
 };
 
-template <typename T>
-bool Fuzz::next(T* n) {
-    if (fNextByte + sizeof(T) > fBytes->size()) {
-        return false;
-    }
+// UBSAN reminds us that bool can only legally hold 0 or 1.
+template <>
+inline bool Fuzz::next<bool>() {
+  return (this->next<uint8_t>() & 1) == 1;
+}
 
-    memcpy(n, fBytes->bytes() + fNextByte, sizeof(T));
+template <typename T>
+T Fuzz::next() {
+    if (fNextByte + sizeof(T) > fBytes->size()) {
+        return 0;
+    }
+    T n;
+    memcpy(&n, fBytes->bytes() + fNextByte, sizeof(T));
     fNextByte += sizeof(T);
-    return true;
+    return n;
+}
+
+template <typename T>
+T Fuzz::nextRange(T min, T max) {
+    if (min > max) {
+        SkDebugf("Check mins and maxes (%d, %d)\n", min, max);
+        this->signalBug();
+    }
+    T n = this->next<T>();
+    // we don't care about the distribution of these, the fuzzer will learn
+    // what to put in there to make it interesting.
+    if (n < min) {
+        return min;
+    }
+    if (n > max) {
+        return max;
+    }
+    return n;
 }
 
 struct Fuzzable {
