240e455bbe4ef35698912e9e70accf5bd69be1e3
Robert Phillips <robertphillips@google.com>
2016-11-01 16:24:46 -0400

    Back SkSpecialImage_Gpu with a GrTextureProxy
    This is split out of https://codereview.chromium.org/2215323003/ (Start using RenderTargetProxy (omnibus))
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4227
    
    Change-Id: Ia9b9d42fb2a0caf61bbfa3ebcc84308c56f541fc

--- src/gpu/GrTextureProxy.cpp
+++ src/gpu/GrTextureProxy.cpp
@@ -7,40 +7,79 @@
 
 #include "GrTextureProxy.h"
 
+#include "GrContext.h"
 #include "GrTextureProvider.h"
 
-GrTextureProxy::GrTextureProxy(const GrSurfaceDesc& srcDesc, SkBackingFit fit, SkBudgeted budgeted,
+GrTextureProxy::GrTextureProxy(GrContext* context, 
+                               const GrSurfaceDesc& srcDesc, SkBackingFit fit, SkBudgeted budgeted,
                                const void* /*srcData*/, size_t /*rowBytes*/)
-    : INHERITED(srcDesc, fit, budgeted) {
+    : INHERITED(srcDesc, fit, budgeted)
+    , fContext(context) {
     // TODO: Handle 'srcData' here
 }
 
 GrTextureProxy::GrTextureProxy(sk_sp<GrTexture> tex)
     : INHERITED(std::move(tex), SkBackingFit::kExact) {
+    fContext = fTarget->getContext();
 }
 
-GrTexture* GrTextureProxy::instantiate(GrTextureProvider* texProvider) {
+GrTexture* GrTextureProxy::instantiate() {
     if (fTarget) {
         return fTarget->asTexture();
     }
 
     if (SkBackingFit::kApprox == fFit) {
-        fTarget = texProvider->createApproxTexture(fDesc);
+        fTarget = fContext->textureProvider()->createApproxTexture(fDesc);
     } else {
-        fTarget = texProvider->createTexture(fDesc, fBudgeted);
+        fTarget = fContext->textureProvider()->createTexture(fDesc, fBudgeted);
     }
+
+#ifdef SK_DEBUG
+    if (kInvalidGpuMemorySize != fGpuMemorySize) {
+        SkASSERT(fTarget->gpuMemorySize() == fGpuMemorySize);
+    }
+#endif
 
     return fTarget->asTexture();
 }
 
-sk_sp<GrTextureProxy> GrTextureProxy::Make(const GrSurfaceDesc& desc,
+size_t GrTextureProxy::onGpuMemorySize() const {
+    size_t textureSize;
+
+    if (GrPixelConfigIsCompressed(fDesc.fConfig)) {
+        textureSize = GrCompressedFormatDataSize(fDesc.fConfig, fDesc.fWidth, fDesc.fHeight);
+    } else {
+        textureSize = (size_t) fDesc.fWidth * fDesc.fHeight * GrBytesPerPixel(fDesc.fConfig);
+    }
+
+//    if (this->texturePriv().hasMipMaps()) {
+        // We don't have to worry about the mipmaps being a different size than
+        // we'd expect because we never change fDesc.fWidth/fHeight.
+        textureSize += textureSize/3;
+//    }
+
+    SkASSERT(!SkToBool(fDesc.fFlags & kRenderTarget_GrSurfaceFlag));
+    SkASSERT(textureSize <= GrSurface::WorstCaseSize(fDesc));
+
+    return textureSize;
+}
+
+sk_sp<GrTextureProxy> GrTextureProxy::Make(GrContext* context,
+                                           const GrSurfaceDesc& desc,
                                            SkBackingFit fit,
                                            SkBudgeted budgeted,
                                            const void* srcData,
                                            size_t rowBytes) {
-    // TODO: handle 'srcData' (we could use the wrapped version if there is data)
-    SkASSERT(!srcData && !rowBytes);
-    return sk_sp<GrTextureProxy>(new GrTextureProxy(desc, fit, budgeted, srcData, rowBytes));
+    if (srcData) {
+        // If we have srcData, for now, we create a wrapped GrTextureProxy
+        sk_sp<GrTexture> tex = sk_ref_sp(context->textureProvider()->createTexture(desc, budgeted,
+                                                                                   srcData,
+                                                                                   rowBytes));
+        return GrTextureProxy::Make(std::move(tex));
+    }
+
+    return sk_sp<GrTextureProxy>(new GrTextureProxy(context, desc, fit, budgeted,
+                                                    srcData, rowBytes));
 }
 
 sk_sp<GrTextureProxy> GrTextureProxy::Make(sk_sp<GrTexture> tex) {
