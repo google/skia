078abe3cb5bca5336eedc5e5abdf9e93fc0df241
Matt Sarett <msarett@google.com>
2016-11-04 10:12:48 -0400

    Fix color xforms for Index8 bmps
    Thanks to Gold for catching this.
    
    BUG=skia:4895
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4426
    
    Change-Id: Icc816d933e9e2fd312858a5430fa21a47722563e

--- src/codec/SkPngCodec.cpp
+++ src/codec/SkPngCodec.cpp
@@ -231,7 +231,7 @@ bool SkPngCodec::createColorTable(const SkImageInfo& dstInfo, int* ctableCount) 
     if (png_get_tRNS(fPng_ptr, fInfo_ptr, &alphas, &numColorsWithAlpha, nullptr)) {
         // If we are performing a color xform, it will handle the premultiply.  Otherwise,
         // we'll do it here.
-        bool premultiply =  !this->colorXform() && needs_premul(dstInfo, this->getInfo());
+        bool premultiply = !this->colorXform() && needs_premul(dstInfo, this->getInfo());
 
         // Choose which function to use to create the color table. If the final destination's
         // colortype is unpremultiplied, the color table will store unpremultiplied colors.
@@ -1109,11 +1109,6 @@ void SkPngCodec::initializeXformParams() {
         default:
             break;
     }
-}
-
-static inline bool apply_xform_on_decode(SkColorType dstColorType, SkEncodedInfo::Color srcColor) {
-    // We will apply the color xform when reading the color table, unless F16 is requested.
-    return SkEncodedInfo::kPalette_Color != srcColor || kRGBA_F16_SkColorType == dstColorType;
 }
 
 void SkPngCodec::initializeSwizzler(const SkImageInfo& dstInfo, const Options& options) {
