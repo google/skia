dc0f60dbfdf4d74d994b3acaaa5a335da96d324a
Robert Phillips <robertphillips@google.com>
2016-11-08 16:21:48 -0500

    Consolidate GrSurfaceProxy Make methods
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4559
    
    Change-Id: Ica599c28a5aef1ed4094f47a4ac119e2d204d652

--- include/private/GrTextureProxy.h
+++ include/private/GrTextureProxy.h
@@ -17,13 +17,6 @@ class GrTextureProvider;
 // This class delays the acquisition of textures until they are actually required
 class GrTextureProxy : virtual public GrSurfaceProxy {
 public:
-    // TODO: need to refine ownership semantics of 'srcData' if we're in completely
-    // deferred mode
-    static sk_sp<GrTextureProxy> Make(const GrCaps&, GrTextureProvider*, const GrSurfaceDesc&,
-                                      SkBackingFit, SkBudgeted,
-                                      const void* srcData = nullptr, size_t rowBytes = 0);
-    static sk_sp<GrTextureProxy> Make(sk_sp<GrTexture>);
-
     GrTextureProxy* asTextureProxy() override { return this; }
     const GrTextureProxy* asTextureProxy() const override { return this; }
 
@@ -31,11 +24,13 @@ public:
     GrTexture* instantiate(GrTextureProvider*);
 
 protected:
+    friend class GrSurfaceProxy; // for ctors
+
     // Deferred version
     GrTextureProxy(const GrSurfaceDesc& srcDesc, SkBackingFit, SkBudgeted,
                    const void* srcData, size_t srcRowBytes);
     // Wrapped version
-    GrTextureProxy(sk_sp<GrTexture> tex);
+    GrTextureProxy(sk_sp<GrSurface>);
 
 private:
     size_t onGpuMemorySize() const override;
