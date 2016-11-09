6e4b594b545134588e982f219d96067972cc166c
scroggo <scroggo@google.com>
2016-11-09 12:03:17 -0800

    Revert of added constant folding & branch elimination to skslc (patchset #3 id:40001 of https://codereview.chromium.org/2489673002/ )
    Reason for revert:
    Breaking the DEPS roll:
    
    ../../third_party/skia/src/sksl/SkSLIRGenerator.cpp:257:70: error: chosen constructor is explicit in copy-initialization
                return std::unique_ptr<Statement>(new Block(s.fPosition, { }, fSymbolTable));
                                                                         ^~~
    ../../build/linux/debian_wheezy_amd64-sysroot/usr/lib/gcc/x86_64-linux-gnu/4.6/../../../../include/c++/4.6/debug/vector:77:7: note: constructor declared here
          vector(const _Allocator& __a = _Allocator())
          ^
    ../../third_party/skia/src/sksl/ir/SkSLBlock.h:20:70: note: passing argument to parameter 'statements' here
        Block(Position position, std::vector<std::unique_ptr<Statement>> statements,
                                                                         ^
    
    Original issue's description:
    > added constant folding & branch elimination to skslc
    >
    > BUG=skia:
    > GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2489673002
    >
    > Committed: https://skia.googlesource.com/skia/+/6136310ee8f43247548bcefcaeca6d43023c10aa
    
    TBR=benjaminwagner@google.com,bsalomon@google.com,ethannicholas@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:
    
    Review-Url: https://codereview.chromium.org/2494523002

--- src/sksl/SkSLIRGenerator.h
+++ src/sksl/SkSLIRGenerator.h
@@ -89,11 +89,6 @@ private:
     std::unique_ptr<Statement> convertDiscard(const ASTDiscardStatement& d);
     std::unique_ptr<Statement> convertDo(const ASTDoStatement& d);
     std::unique_ptr<Expression> convertBinaryExpression(const ASTBinaryExpression& expression);
-    // Returns null if it cannot fold the expression. Note that unlike most other functions here, a 
-    // null return does not represent a compilation error.
-    std::unique_ptr<Expression> constantFold(const Expression& left,
-                                             Token::Kind op,
-                                             const Expression& right);
     std::unique_ptr<Extension> convertExtension(const ASTExtension& e);
     std::unique_ptr<Statement> convertExpressionStatement(const ASTExpressionStatement& s);
     std::unique_ptr<Statement> convertFor(const ASTForStatement& f);
