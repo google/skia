3534e0d2e48d80d3d8b42aaf8b42837ff52981e5
Florin Malita <fmalita@chromium.org>
2016-11-14 09:58:07 -0500

    External SkImageGenerator API
    Introduce an SkImageGenerator API to support the implementation of
    externally-managed image decode and scale caches.
    
    BUG=skia:5806
    R=reed@google.com
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4720
    
    Change-Id: Ibfe37af5471f78f28f88f9d5e80938882be1a344

--- src/core/SkBitmapProvider.cpp
+++ src/core/SkBitmapProvider.cpp
@@ -7,6 +7,7 @@
 
 #include "SkBitmapProvider.h"
 #include "SkImage_Base.h"
+#include "SkImageCacherator.h"
 #include "SkPixelRef.h"
 
 int SkBitmapProvider::width() const {
@@ -45,4 +46,34 @@ void SkBitmapProvider::notifyAddedToCache() const {
 
 bool SkBitmapProvider::asBitmap(SkBitmap* bm) const {
     return as_IB(fImage)->getROPixels(bm, SkImage::kAllow_CachingHint);
+}
+
+bool SkBitmapProvider::accessScaledPixels(const SkRect& srcRect,
+                                          const SkMatrix& invMatrix,
+                                          SkFilterQuality fq,
+                                          SkBitmap* scaledBitmap,
+                                          SkRect* adjustedSrcRect,
+                                          SkFilterQuality* adjustedFilterQuality) const {
+    if (!fImage) {
+        return false;
+    }
+
+    SkImageCacherator* cacherator = as_IB(fImage)->peekCacherator();
+    if (!cacherator) {
+        return false;
+    }
+
+    // TODO: stash the matrix someplace to avoid invert()?
+    SkMatrix m;
+    if (!invMatrix.invert(&m)) {
+        return false;
+    }
+    sk_sp<SkImage> scaledImage;
+    std::tie(scaledImage, *adjustedSrcRect, *adjustedFilterQuality) =
+        cacherator->directAccessScaledPixels(srcRect, m, fq);
+    if (!scaledImage) {
+        return false;
+    }
+
+    return scaledImage->asLegacyBitmap(scaledBitmap, SkImage::kRO_LegacyBitmapMode);
 }
