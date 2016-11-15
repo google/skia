3667b4970d6972675be58a331be7c5ba02f3d39e
Robert Phillips <robertphillips@google.com>
2016-11-15 14:46:09 -0500

    Add kRenderTarget_GrSurfaceFlag to GrVkGpu::onWrapBackendRenderTarget
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4868
    
    Change-Id: I7f169d050c47c074cf63ef9b63fb7df8efdaed62
    Reviewed-on: https://skia-review.googlesource.com/4868
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/gpu/vk/GrVkGpu.cpp
+++ src/gpu/vk/GrVkGpu.cpp
@@ -802,7 +802,7 @@ sk_sp<GrRenderTarget> GrVkGpu::onWrapBackendRenderTarget(const GrBackendRenderTa
 
     GrSurfaceDesc desc;
     desc.fConfig = wrapDesc.fConfig;
-    desc.fFlags = kCheckAllocation_GrSurfaceFlag;
+    desc.fFlags = kCheckAllocation_GrSurfaceFlag | kRenderTarget_GrSurfaceFlag;
     desc.fWidth = wrapDesc.fWidth;
     desc.fHeight = wrapDesc.fHeight;
     desc.fSampleCnt = SkTMin(wrapDesc.fSampleCnt, this->caps()->maxSampleCount());
