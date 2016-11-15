ce03ec0e0f56109e8547e2dba7be2a9b2f85206b
Mike Klein <mtklein@chromium.org>
2016-11-14 20:30:51 -0500

    While we can, restrict SkFixedAlloc/SkFallbackAlloc to POD.
    This trims the overhead down to a uniform 4 bytes.
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4786
    
    Change-Id: I92127a0c2d6c23a4a372eca39842e22195d2dc99

--- src/core/SkFixedAlloc.cpp
+++ src/core/SkFixedAlloc.cpp
@@ -11,23 +11,37 @@ SkFixedAlloc::SkFixedAlloc(void* ptr, size_t len)
     : fBuffer((char*)ptr), fUsed(0), fLimit(len) {}
 
 void SkFixedAlloc::undo() {
-    // This function is essentially make() in reverse.
-
-    // First, read the Footer we stamped at the end.
-    Footer footer;
-    memcpy(&footer, fBuffer + fUsed - sizeof(Footer), sizeof(Footer));
-
-    // That tells us where the T starts and how to destroy it.
-    footer.dtor(fBuffer + fUsed - sizeof(Footer) - footer.len);
-
-    // We can reuse bytes that stored the Footer, the T, and any that we skipped for alignment.
-    fUsed -= sizeof(Footer) + footer.len + footer.skip;
+    uint32_t skip_and_len;
+    memcpy(&skip_and_len, fBuffer + fUsed - 4, 4);
+    fUsed -= skip_and_len + 4;
 }
 
 void SkFixedAlloc::reset() {
-    while (fUsed) {
-        this->undo();
+    fUsed = 0;
+}
+
+void* SkFixedAlloc::alloc(size_t align, size_t len) {
+    auto aligned = ((uintptr_t)(fBuffer+fUsed) + align-1) & ~(align-1);
+    size_t skip = aligned - (uintptr_t)(fBuffer+fUsed);
+
+    if (!SkTFitsIn<uint32_t>(skip + len) ||
+        fUsed + skip + len + 4 > fLimit) {
+        return nullptr;
     }
+
+    // Skip ahead until aligned.
+    fUsed += skip;
+
+    // Allocate len bytes.
+    void* ptr = (fBuffer+fUsed);
+    fUsed += len;
+
+    // Stamp a footer that we can use to clean up.
+    uint32_t skip_and_len = SkToU32(skip + len);
+    memcpy(fBuffer+fUsed, &skip_and_len, 4);
+    fUsed += 4;
+
+    return ptr;
 }
 
 
@@ -37,8 +51,7 @@ void SkFallbackAlloc::undo() {
     if (fHeapAllocs.empty()) {
         return fFixedAlloc->undo();
     }
-    HeapAlloc alloc = fHeapAllocs.back();
-    alloc.deleter(alloc.ptr);
+    sk_free(fHeapAllocs.back());
     fHeapAllocs.pop_back();
 }
 
