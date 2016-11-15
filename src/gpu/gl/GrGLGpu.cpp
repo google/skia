948897ebc1da29c92d62a21bab8ef3a8eee95a09
csmartdalton <csmartdalton@google.com>
2016-11-15 14:24:57 -0700

    Fix flushing of HWAA state relating to mixed samples
    Fixes the sense of the "stencilEnabled" argument sent to
    flushHWAAState.
    
    BUG=skia:
    
    Change-Id: If7097f2819607864c95b0ffd04dc1d302fa66fdc

--- src/gpu/gl/GrGLGpu.cpp
+++ src/gpu/gl/GrGLGpu.cpp
@@ -2064,7 +2064,7 @@ bool GrGLGpu::flushGLState(const GrPipeline& pipeline, const GrPrimitiveProcesso
     this->flushStencil(stencil);
     this->flushScissor(pipeline.getScissorState(), glRT->getViewport(), glRT->origin());
     this->flushWindowRectangles(pipeline.getWindowRectsState(), glRT);
-    this->flushHWAAState(glRT, pipeline.isHWAntialiasState(), stencil.isDisabled());
+    this->flushHWAAState(glRT, pipeline.isHWAntialiasState(), !stencil.isDisabled());
 
     // This must come after textures are flushed because a texture may need
     // to be msaa-resolved (which will modify bound FBO state).
