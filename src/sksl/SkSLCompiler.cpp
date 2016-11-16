4560898a8ff7de2b28a726c99968f2e84c644d10
Brian Salomon <bsalomon@google.com>
2016-11-15 19:09:52 -0500

    Add support for image load to SkSL
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=4865
    
    Change-Id: I4647e6b255946ced2b1b8cb05e62f0f5a8ad28b6

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -113,6 +113,9 @@ Compiler::Compiler()
 
     ADD_TYPE(ISampler2D);
 
+    ADD_TYPE(Image2D);
+    ADD_TYPE(IImage2D);
+
     ADD_TYPE(GSampler1D);
     ADD_TYPE(GSampler2D);
     ADD_TYPE(GSampler3D);
