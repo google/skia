50a8332e4ac2f2dac3fcf1ceef38f25c25af3ce9
Ravi Mistry <rmistry@google.com>
2016-11-16 09:22:59 -0500

    Add Gerrit support to RecreateSKPs
    BUG=skia:5979
    
    Change-Id: I5527c9d4219b879d5220136949704ea0fc556f85

--- infra/bots/upload_skps.py
+++ infra/bots/upload_skps.py
@@ -9,31 +9,28 @@ import argparse
 import os
 import subprocess
 import sys
+import urllib2
 
 sys.path.insert(0, os.path.join(os.getcwd(), 'common'))
 # pylint:disable=F0401
 from py.utils import git_utils
 
 
-
+# rmistry
 CHROMIUM_SKIA = 'https://chromium.googlesource.com/skia.git'
+SKIA_COMMITTER_EMAIL = 'update-skps@skia.org'
+SKIA_COMMITTER_NAME = 'UpdateSKPs'
 COMMIT_MSG = '''Update SKP version
 
 Automatic commit by the RecreateSKPs bot.
 
-TBR=
+TBR=%s
 NO_MERGE_BUILDS
-'''
-SKIA_COMMITTER_EMAIL = 'skia.buildbots@gmail.com'
-SKIA_COMMITTER_NAME = 'skia.buildbots'
+''' % SKIA_COMMITTER_EMAIL
 SKIA_REPO = 'https://skia.googlesource.com/skia.git'
 
 
-def main(target_dir):
-  subprocess.check_call(['git', 'config', '--local', 'user.name',
-                         SKIA_COMMITTER_NAME])
-  subprocess.check_call(['git', 'config', '--local', 'user.email',
-                         SKIA_COMMITTER_EMAIL])
+def main(target_dir, gitcookies):
   if CHROMIUM_SKIA in subprocess.check_output(['git', 'remote', '-v']):
     subprocess.check_call(['git', 'remote', 'set-url', 'origin', SKIA_REPO,
                            CHROMIUM_SKIA])
@@ -53,20 +50,30 @@ def main(target_dir):
                           % e.output)
     sys.exit(1)
 
-  # Upload the new version, land the update CL.
-  with git_utils.GitBranch(branch_name='update_skp_version',
-                           commit_msg=COMMIT_MSG,
-                           commit_queue=True):
-    upload_script = os.path.join(
-        os.getcwd(), 'infra', 'bots', 'assets', 'skp', 'upload.py')
-    subprocess.check_call(['python', upload_script, '-t', target_dir])
-    subprocess.check_call(['go', 'run', gen_tasks])
-    subprocess.check_call([
-        'git', 'add', os.path.join('infra', 'bots', 'tasks.json')])
+  # Skip GCE Auth in depot_tools/gerrit_utils.py. Use gitcookies instead.
+  os.environ['SKIP_GCE_AUTH_FOR_GIT'] = True
+  # Upload the new version, land the update CL as the update-skps user.
+  config_dict = {
+    'user.name': SKIA_COMMITTER_NAME,
+    'user.email': SKIA_COMMITTER_EMAIL,
+    'http.cookiefile': gitcookies,
+  }
+  with git_utils.GitLocalConfig(config_dict):
+    with git_utils.GitBranch(branch_name='update_skp_version',
+                             commit_msg=COMMIT_MSG,
+                             commit_queue=True):
+      upload_script = os.path.join(
+          os.getcwd(), 'infra', 'bots', 'assets', 'skp', 'upload.py')
+      subprocess.check_call(['python', upload_script, '-t', target_dir])
+      subprocess.check_call(['go', 'run', gen_tasks])
+      subprocess.check_call([
+          'git', 'add', os.path.join('infra', 'bots', 'tasks.json')])
 
 
 if '__main__' == __name__:
   parser = argparse.ArgumentParser()
+  # TODO(rmistry): Add a path to the gitcookies file!
   parser.add_argument("--target_dir")
+  parser.add_argument("--gitcookies")
   args = parser.parse_args()
-  main(args.target_dir)
+  main(args.target_dir, args.gitcookies)
