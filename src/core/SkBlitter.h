99bba9ea829323a7941d4f3a9c98da7a76432d1b
Yuqian Li <liyuqian@google.com>
2016-11-21 09:44:59 -0500

    Add missing virtual functions for SkRectClipCheckBlitter
    This CL is related to the issue that we tried to fix in
    https://skia-review.googlesource.com/c/5078/
    
    Specifically, SkARGB32_Blitter overrides blitAntiH2 and blitAntiV2 and
    they're different from blitAntiRuns. Therefore, if
    SkRectClipCheckBlitter does not override those two functions, we're
    going to get different results in debug and release build.
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=5082
    
    Change-Id: I7f9565d494f0a6c45ab5a06fbedb73ef233b9a1f
    Reviewed-on: https://skia-review.googlesource.com/5082
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Yuqian Li <liyuqian@google.com>

--- src/core/SkBlitter.h
+++ src/core/SkBlitter.h
@@ -258,6 +258,8 @@ public:
                               SkAlpha leftAlpha, SkAlpha rightAlpha) override;
     void blitMask(const SkMask&, const SkIRect& clip) override;
     const SkPixmap* justAnOpaqueColor(uint32_t* value) override;
+    void blitAntiH2(int x, int y, U8CPU a0, U8CPU a1) override;
+    void blitAntiV2(int x, int y, U8CPU a0, U8CPU a1) override;
 
     int requestRowsPreserved() const override {
         return fBlitter->requestRowsPreserved();
