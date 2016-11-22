060772f1452cca80e8bd8c719feb3b8adb53fb09
Mike Klein <mtklein@chromium.org>
2016-11-22 08:57:45 -0500

    Guard against buggy ucrt\math.h.
    BUG=666707
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=5089
    
    CQ_INCLUDE_TRYBOTS=master.client.skia:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD-Trybot
    
    Change-Id: I3bebfdf635d541d92fb84236f0f6fae2da39d691

--- src/opts/SkOpts_avx.cpp
+++ src/opts/SkOpts_avx.cpp
@@ -5,9 +5,14 @@
  * found in the LICENSE file.
  */
 
+#include "SkSafe_math.h"   // Keep this first.
 #include "SkOpts.h"
 
 #define SK_OPTS_NS avx
+
+#if defined(_INC_MATH) && !defined(INC_MATH_IS_SAFE_NOW)
+    #error We have included ucrt\math.h without protecting it against ODR violation.
+#endif
 
 namespace SkOpts {
     void Init_avx() { }
