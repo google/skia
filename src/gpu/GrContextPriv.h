40f30cef21d5cfb5c17eb6a8184ad39d5d1fabbb
Brian Osman <brianosman@google.com>
2016-11-22 15:25:24 -0500

    Added GrSurfaceContext and GrTextureContext
    This lets copy-to-texture to be treated like copy-to-rt.
    To match current behavior, though, copies to texture are
    still executed immediately (forcing a flush).
    
    Once MDB is enabled, copies to texture will be deferred.
    
    BUG=skia:
    
    GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=5093
    
    Change-Id: Icc0ce5435507a5f0a237c22eedef879824952367

--- src/gpu/GrContextPriv.h
+++ src/gpu/GrContextPriv.h
@@ -9,6 +9,7 @@
 #define GrContextPriv_DEFINED
 
 #include "GrContext.h"
+#include "GrTextureContext.h"
 
 /** Class that adds methods to GrContext that are only intended for use internal to Skia.
     This class is purely a privileged window into GrContext. It should never have additional
@@ -21,6 +22,9 @@ public:
     sk_sp<GrRenderTargetContext> makeWrappedRenderTargetContext(sk_sp<GrRenderTarget> rt,
                                                                 sk_sp<SkColorSpace> colorSpace,
                                                                 const SkSurfaceProps* = nullptr);
+
+    // Create a textureContext that wraps an existing texture
+    sk_sp<GrTextureContext> makeWrappedTextureContext(sk_sp<GrTexture> tex);
 
     sk_sp<GrRenderTargetContext> makeBackendTextureRenderTargetContext(
                                                          const GrBackendTextureDesc& desc,
