d8418c4322ac831316f2b2e3da076e4a9b8a1295
Hal Canary <halcanary@google.com>
2016-12-01 11:19:26 -0500

    SkImageEncoder::* going away
    Approval from https://review.skia.org/5006
    TBR=reed@google.com
    
    Change-Id: I43d1a24fa3e924b3c24b224f2451ab990b440dc8

--- include/images/SkForceLinking.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2013 Google Inc.
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-// TODO(halcanary): delete this file.
