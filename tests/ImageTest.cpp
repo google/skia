d61a640a799993338c07b455281d9215f65b8a40
Brian Osman <brianosman@google.com>
2016-12-05 16:12:48 -0500

    Remove SkImage::makeTextureImage
    No one (other than test code) was using this API, and it lacks the context
    to do the right thing. Specifically, if this forces a decode of an encoded
    image, we don't know the intended use (re: color spaces) to determine how
    we should decode.
    
    BUG=skia:
    
    Change-Id: I6ff700b3a5adce8257f35c5e3dd5ba557b2a3219

--- tests/ImageTest.cpp
+++ tests/ImageTest.cpp
@@ -455,68 +455,6 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(c, reporter, ctxInfo) {
     }
 }
 
-DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkImage_newTextureImage, reporter, contextInfo) {
-    GrContext* context = contextInfo.grContext();
-    sk_gpu_test::TestContext* testContext = contextInfo.testContext();
-
-    GrContextFactory otherFactory;
-    GrContextFactory::ContextType otherContextType =
-            GrContextFactory::NativeContextTypeForBackend(testContext->backend());
-    ContextInfo otherContextInfo = otherFactory.getContextInfo(otherContextType);
-    testContext->makeCurrent();
-
-    std::function<sk_sp<SkImage>()> imageFactories[] = {
-        create_image,
-        create_codec_image,
-        create_data_image,
-        // Create an image from a picture.
-        create_picture_image,
-        // Create a texture image.
-        [context] { return create_gpu_image(context); },
-        // Create a texture image in a another GrContext.
-        [testContext, otherContextInfo] {
-            otherContextInfo.testContext()->makeCurrent();
-            sk_sp<SkImage> otherContextImage = create_gpu_image(otherContextInfo.grContext());
-            testContext->makeCurrent();
-            return otherContextImage;
-        }
-    };
-
-    for (auto factory : imageFactories) {
-        sk_sp<SkImage> image(factory());
-        if (!image) {
-            ERRORF(reporter, "Error creating image.");
-            continue;
-        }
-        GrTexture* origTexture = as_IB(image)->peekTexture();
-
-        sk_sp<SkImage> texImage(image->makeTextureImage(context));
-        if (!texImage) {
-            // We execpt to fail if image comes from a different GrContext.
-            if (!origTexture || origTexture->getContext() == context) {
-                ERRORF(reporter, "newTextureImage failed.");
-            }
-            continue;
-        }
-        GrTexture* copyTexture = as_IB(texImage)->peekTexture();
-        if (!copyTexture) {
-            ERRORF(reporter, "newTextureImage returned non-texture image.");
-            continue;
-        }
-        if (origTexture) {
-            if (origTexture != copyTexture) {
-                ERRORF(reporter, "newTextureImage made unnecessary texture copy.");
-            }
-        }
-        if (image->width() != texImage->width() || image->height() != texImage->height()) {
-            ERRORF(reporter, "newTextureImage changed the image size.");
-        }
-        if (image->alphaType() != texImage->alphaType()) {
-            ERRORF(reporter, "newTextureImage changed image alpha type.");
-        }
-    }
-}
-
 DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkImage_makeNonTextureImage, reporter, contextInfo) {
     GrContext* context = contextInfo.grContext();
 
@@ -531,9 +469,7 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkImage_makeNonTextureImage, reporter, contex
         sk_sp<SkImage> image = factory();
         if (!image->isTextureBacked()) {
             REPORTER_ASSERT(reporter, image->makeNonTextureImage().get() == image.get());
-            if (!(image = image->makeTextureImage(context))) {
-                continue;
-            }
+            continue;
         }
         auto rasterImage = image->makeNonTextureImage();
         if (!rasterImage) {
