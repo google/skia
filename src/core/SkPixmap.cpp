069654bba0b086f1769fb1fd1df02e35a40de1c3
Hal Canary <halcanary@google.com>
2016-12-07 15:24:59 -0500

    SkBitmap::ComputeIsOpaque -> SkPixmap::computeIsOpaque
    Motivation:  Twice internal Skia clients have to do something awkward like this:
    
        bool ComputeIsOpaque(const SkPixmap& pixmap) {
          SkBitmap bm;
          return bm.installPixels(pixmap) && SkBitmap::ComputeIsOpaque(bm);
        }
    
    Change-Id: I7263c06f754c1305ecb07c4c005d9cfb9d1f523d

--- src/core/SkPixmap.cpp
+++ src/core/SkPixmap.cpp
@@ -332,3 +332,82 @@ SkColor SkPixmap::getColor(int x, int y) const {
             return SkColorSetARGB(0, 0, 0, 0);
     }
 }
+
+bool SkPixmap::computeIsOpaque() const {
+    const int height = this->height();
+    const int width = this->width();
+
+    switch (this->colorType()) {
+        case kAlpha_8_SkColorType: {
+            unsigned a = 0xFF;
+            for (int y = 0; y < height; ++y) {
+                const uint8_t* row = this->addr8(0, y);
+                for (int x = 0; x < width; ++x) {
+                    a &= row[x];
+                }
+                if (0xFF != a) {
+                    return false;
+                }
+            }
+            return true;
+        } break;
+        case kIndex_8_SkColorType: {
+            const SkColorTable* ctable = this->ctable();
+            if (nullptr == ctable) {
+                return false;
+            }
+            const SkPMColor* table = ctable->readColors();
+            SkPMColor c = (SkPMColor)~0;
+            for (int i = ctable->count() - 1; i >= 0; --i) {
+                c &= table[i];
+            }
+            return 0xFF == SkGetPackedA32(c);
+        } break;
+        case kRGB_565_SkColorType:
+        case kGray_8_SkColorType:
+            return true;
+            break;
+        case kARGB_4444_SkColorType: {
+            unsigned c = 0xFFFF;
+            for (int y = 0; y < height; ++y) {
+                const SkPMColor16* row = this->addr16(0, y);
+                for (int x = 0; x < width; ++x) {
+                    c &= row[x];
+                }
+                if (0xF != SkGetPackedA4444(c)) {
+                    return false;
+                }
+            }
+            return true;
+        } break;
+        case kBGRA_8888_SkColorType:
+        case kRGBA_8888_SkColorType: {
+            SkPMColor c = (SkPMColor)~0;
+            for (int y = 0; y < height; ++y) {
+                const SkPMColor* row = this->addr32(0, y);
+                for (int x = 0; x < width; ++x) {
+                    c &= row[x];
+                }
+                if (0xFF != SkGetPackedA32(c)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        case kRGBA_F16_SkColorType: {
+            const SkHalf* row = (const SkHalf*)this->addr();
+            for (int y = 0; y < height; ++y) {
+                for (int x = 0; x < width; ++x) {
+                    if (row[4 * x + 3] < SK_Half1) {
+                        return false;
+                    }
+                }
+                row += this->rowBytes() >> 1;
+            }
+            return true;
+        }
+        default:
+            break;
+    }
+    return false;
+}
