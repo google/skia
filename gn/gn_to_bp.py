c8c334617d2b5ff83792e5603d3057f5ce15d879
Leon Scroggins III <scroggo@google.com>
2016-12-13 16:19:39 -0500

    Do not build the ktx encoder for android framework
    Move SkKTXImageEncoder.cpp into an optional block, and disable that
    block for the android framework. Use a new define to determine whether
    to define the entry point, rather than using
    SK_BUILD_FOR_ANDROID_FRAMEWORK.
    
    Change-Id: I41103459135af744cf5715f27783c63dc37a7ad1

--- gn/gn_to_bp.py
+++ gn/gn_to_bp.py
@@ -109,6 +109,7 @@ cc_library {
 
 # We'll run GN to get the main source lists and include directories for Skia.
 gn_args = {
+  'skia_enable_ktx_encoder':         'false',
   'skia_enable_vulkan_debug_layers': 'false',
   'skia_use_system_expat':           'true',
   'skia_use_vulkan':                 'true',
