7d060d687201d2c19d4e2e58a0dad8cbecd5f5dd
Mike Reed <reed@google.com>
2016-12-14 15:59:56 -0500

    speedup dynamicwstream
    Before: 30ms
    After:  27ms for non-4-bytes writes
            18ms for 4-bytes writes
    
    BUG=skia:
    
    Change-Id: Id06ecad3b9fe426747e02accf1393595e3356ce3

--- include/core/SkStream.h
+++ include/core/SkStream.h
@@ -194,11 +194,19 @@ public:
 
     // helpers
 
-    bool    write8(U8CPU);
-    bool    write16(U16CPU);
-    bool    write32(uint32_t);
+    bool write8(U8CPU value)   {
+        uint8_t v = SkToU8(value);
+        return this->write(&v, 1);
+    }
+    bool write16(U16CPU value) {
+        uint16_t v = SkToU16(value);
+        return this->write(&v, 2);
+    }
+    bool write32(uint32_t v) {
+        return this->write(&v, 4);
+    }
 
-    bool    writeText(const char text[]) {
+    bool writeText(const char text[]) {
         SkASSERT(text);
         return this->write(text, strlen(text));
     }
@@ -376,12 +384,14 @@ public:
     virtual ~SkDynamicMemoryWStream();
 
     bool write(const void* buffer, size_t size) override;
-    size_t bytesWritten() const override { return fBytesWritten; }
+    size_t bytesWritten() const override;
     // random access write
     // modifies stream and returns true if offset + size is less than or equal to getOffset()
     bool write(const void* buffer, size_t offset, size_t size);
     bool read(void* buffer, size_t offset, size_t size);
-    size_t getOffset() const { return fBytesWritten; }
+
+    // Why do we have this as a separate method???
+    size_t getOffset() const { return this->bytesWritten(); }
 
     // copy what has been written to the stream into dst
     void copyTo(void* dst) const;
@@ -401,10 +411,18 @@ private:
     struct Block;
     Block*  fHead;
     Block*  fTail;
-    size_t  fBytesWritten;
+    size_t  fBytesWrittenBeforeTail;
     mutable sk_sp<SkData> fCopy;  // is invalidated if we write after it is created
 
-    void invalidateCopy();
+    void invalidateCopy() {
+        fCopy.reset(nullptr);
+    }
+
+#ifdef SK_DEBUG
+    void validate() const;
+#else
+    void validate() const {}
+#endif
 
     // For access to the Block type.
     friend class SkBlockMemoryStream;
