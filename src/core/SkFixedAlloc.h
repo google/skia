7db0403806d45d04e36af0496d14bf6438a20f5c
Herb Derby <herb@google.com>
2016-12-20 17:02:51 -0500

    WIP: unified allocator
    Change-Id: Ia8efd09b2f3139a57182889ba84d1610eae92749

--- src/core/SkFixedAlloc.h
+++ src/core/SkFixedAlloc.h
@@ -11,6 +11,7 @@
 #include "SkTFitsIn.h"
 #include "SkTypes.h"
 #include <new>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -110,6 +111,116 @@ private:
 
     SkFixedAlloc*          fFixedAlloc;
     std::vector<HeapAlloc> fHeapAllocs;
+};
+
+class SkArenaAlloc {
+public:
+    SkArenaAlloc() { }
+    SkArenaAlloc(char* b, size_t s)
+    : fBase{b}
+    , fDtorCursor{b}
+    , fCursor{b}
+    , fEnd{b + s}
+    {
+        this->installFooter(EndChain, 0);
+    }
+
+    template <typename T, typename... Args>
+    T* make(Args&&... args) {
+        auto mask = alignof(T) - 1;
+
+        // Align fCursor for this allocation.
+        char* objStart = (char*)((uintptr_t)(fCursor + mask) & ~mask);
+        size_t overhead = 0;
+        if (!std::is_trivially_destructible<T>::value) {
+            overhead = fCursor == fDtorCursor ? sizeof(Footer) : sizeof(Footer) + sizeof(int32_t);
+        }
+
+        if (objStart + sizeof(T) + overhead > fEnd) {
+            this->ensureSpace(sizeof(T));
+            objStart = (char*)((uintptr_t)(fCursor + mask) & ~mask);
+        }
+
+        SkASSERT(objStart + sizeof(T) + overhead < fEnd);
+
+        ptrdiff_t padding = objStart - fCursor;
+        // Advance cursor to end of the object.
+        fCursor = objStart + sizeof(T);
+
+        if (!std::is_trivially_destructible<T>::value) {
+            if (fCursor != fDtorCursor) {
+                this->installSkipFooter(fCursor - fDtorCursor);
+            }
+            Releaser releaser = [](char* objEnd) {
+                char* objStart = objEnd - (sizeof(T) + sizeof(Footer));
+                ((T*)objStart)->~T();
+                return objStart;
+            };
+
+            this->installFooter(releaser, padding);
+        }
+
+        return new(objStart) T(std::forward<Args>(args)...);
+    };
+
+private:
+    using Footer = int32_t;
+    using Releaser = char*(*)(char*);
+    void ensureSpace(size_t size) {}
+
+    void installFooter(Releaser releaser, ptrdiff_t padding) {
+        ptrdiff_t releaserDiff = (char *)releaser - (char *)EndChain;
+
+        if (padding >= 32
+            || releaserDiff >= (1 << 26)
+            || releaserDiff < -(1 << 26)) {
+            // Ran out of space, or code not store info in the Footer.
+            SkFAIL("Constraints are busted.");
+        }
+
+        Footer footer = (Footer)(SkLeftShift((int64_t)releaserDiff, 5) | padding);
+        memcpy(fCursor, &footer, sizeof(Footer));
+        fCursor += sizeof(Footer);
+        fDtorCursor = fCursor;
+    }
+
+    void installSkipFooter(size_t size) {
+        if ((int32_t)size == size) {
+            int32_t smallSize = size;
+            memmove(fCursor, &smallSize, sizeof(int32_t));
+            fCursor += sizeof(int32_t);
+            this->installFooter(NextNear, 0);
+        } else {
+            memmove(fCursor, &size, sizeof(size_t));
+            fCursor += sizeof(size_t);
+            this->installFooter(NextFar, 0);
+        }
+    }
+
+    static char* EndChain(char*) { return nullptr; }
+
+    static char* NextNear(char* end) {
+        char* start = end - (sizeof(Footer) + sizeof(int32_t));
+        int32_t distanceNext;
+        memmove(&distanceNext, start, sizeof(int32_t));
+        free(start);
+        return start + distanceNext;
+    }
+
+    static char* NextFar(char* end) {
+        char* start = end - (sizeof(Footer) + sizeof(ptrdiff_t));
+        ptrdiff_t distanceNext;
+        memmove(&distanceNext, start, sizeof(ptrdiff_t));
+        free(start);
+        return start + distanceNext;
+    }
+
+
+    char* fBase        {nullptr};
+    char* fDtorCursor  {nullptr};
+    char* fCursor      {nullptr};
+    char* fEnd         {nullptr};
+
 };
 
 #endif//SkFixedAlloc_DEFINED
