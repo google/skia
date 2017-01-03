d0c4717600f4c1c802a5b997ca767d39979fcb3b
Matt Sarett <msarett@google.com>
2017-01-03 11:09:58 -0500

    Add F16 support to SkPNGImageEncoder
    BUG=skia:
    
    Change-Id: Ifd221365a7b9f9a4a4fc5382621e0da7189e1148

--- src/images/transform_scanline.h
+++ src/images/transform_scanline.h
@@ -12,6 +12,7 @@
 #include "SkBitmap.h"
 #include "SkColor.h"
 #include "SkColorPriv.h"
+#include "SkHalf.h"
 #include "SkPreConfig.h"
 #include "SkRasterPipeline.h"
 #include "SkUnPreMultiply.h"
@@ -96,8 +97,7 @@ static void transform_scanline_444(char* SK_RESTRICT dst, const char* SK_RESTRIC
 
 template <bool kIsRGBA>
 static inline void transform_scanline_legacy_unpremultiply(char* SK_RESTRICT dst,
-                                                           const char* SK_RESTRICT src, int width,
-                                                           int) {
+                                                           const char* SK_RESTRICT src, int width) {
     const uint32_t* srcP = (const SkPMColor*)src;
     const SkUnPreMultiply::Scale* table = SkUnPreMultiply::GetScaleTable();
 
@@ -133,20 +133,20 @@ static inline void transform_scanline_legacy_unpremultiply(char* SK_RESTRICT dst
  * Transform from legacy kPremul, kRGBA_8888_SkColorType to 4-bytes-per-pixel unpremultiplied RGBA.
  */
 static void transform_scanline_legacy_rgbA(char* SK_RESTRICT dst, const char* SK_RESTRICT src,
-                                           int width, int bpp) {
-    transform_scanline_legacy_unpremultiply<true>(dst, src, width, bpp);
+                                           int width, int) {
+    transform_scanline_legacy_unpremultiply<true>(dst, src, width);
 }
 
 /**
  * Transform from legacy kPremul, kBGRA_8888_SkColorType to 4-bytes-per-pixel unpremultiplied RGBA.
  */
 static void transform_scanline_legacy_bgrA(char* SK_RESTRICT dst, const char* SK_RESTRICT src,
-                                           int width, int bpp) {
-    transform_scanline_legacy_unpremultiply<false>(dst, src, width, bpp);
+                                           int width, int) {
+    transform_scanline_legacy_unpremultiply<false>(dst, src, width);
 }
 
 template <bool kIsRGBA>
-static inline void transform_scanline_unpremultiply(void* dst, const void* src, int width, int) {
+static inline void transform_scanline_unpremultiply(void* dst, const void* src, int width) {
     SkRasterPipeline p;
     p.append(SkRasterPipeline::load_8888, &src);
     if (!kIsRGBA) {
@@ -164,16 +164,16 @@ static inline void transform_scanline_unpremultiply(void* dst, const void* src, 
  * Transform from kPremul, kRGBA_8888_SkColorType to 4-bytes-per-pixel unpremultiplied RGBA.
  */
 static void transform_scanline_rgbA(char* SK_RESTRICT dst, const char* SK_RESTRICT src,
-                                    int width, int bpp) {
-    transform_scanline_unpremultiply<true>(dst, src, width, bpp);
+                                    int width, int) {
+    transform_scanline_unpremultiply<true>(dst, src, width);
 }
 
 /**
  * Transform from kPremul, kBGRA_8888_SkColorType to 4-bytes-per-pixel unpremultiplied RGBA.
  */
 static void transform_scanline_bgrA(char* SK_RESTRICT dst, const char* SK_RESTRICT src,
-                                    int width, int bpp) {
-    transform_scanline_unpremultiply<false>(dst, src, width, bpp);
+                                    int width, int) {
+    transform_scanline_unpremultiply<false>(dst, src, width);
 }
 
 /**
@@ -218,4 +218,27 @@ static void transform_scanline_4444(char* SK_RESTRICT dst, const char* SK_RESTRI
         *dst++ = b;
         *dst++ = a;
     }
+}
+
+/**
+ * Transform from kRGBA_F16 to 4-bytes-per-pixel RGBA.
+ */
+static void transform_scanline_F16(char* SK_RESTRICT dst, const char* SK_RESTRICT src, int width,
+                                   int) {
+    SkRasterPipeline p;
+    p.append(SkRasterPipeline::load_f16, (const void**) &src);
+    p.append(SkRasterPipeline::store_8888, (void**) &dst);
+    p.run(0, 0, width);
+}
+
+/**
+ * Transform from kPremul, kRGBA_F16 to 4-bytes-per-pixel RGBA.
+ */
+static void transform_scanline_F16_premul(char* SK_RESTRICT dst, const char* SK_RESTRICT src,
+                                          int width, int) {
+    SkRasterPipeline p;
+    p.append(SkRasterPipeline::load_f16, (const void**) &src);
+    p.append(SkRasterPipeline::unpremul);
+    p.append(SkRasterPipeline::store_8888, (void**) &dst);
+    p.run(0, 0, width);
 }
