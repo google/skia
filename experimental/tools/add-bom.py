2971a4033c4d83ac5e48cf03cd4097e9cd71de34
Hal Canary <halcanary@google.com>
2017-01-03 14:11:56 -0500

    experimental/tools/add-bom.py
    NOTRY=true
    Change-Id: Ib6ab71711fda010cc2e702bae08154974adf310f

---
+++ experimental/tools/add-bom.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import re
+import subprocess
+import sys
+
+endings = re.compile(r'.*(\.cpp|\.h|\.c|\.hh)$')
+
+os.chdir(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+
+def all_ascii(path):
+    with open(path, 'rb') as f:
+        while True:
+            buff = f.read(4096)
+            if 0 == len(buff):
+                return True
+            if any(ord(c) > 128 for c in buff):
+                return False
+
+for path in subprocess.check_output(['git', 'ls-files']).split('\n'):
+    if not endings.match(path) or all_ascii(path):
+        continue
+    print path
+    with open(path, 'rb') as f:
+        contents = f.read()
+    with open(path, 'wb') as o:
+        o.write('\xEF\xBB\xBF /* byte order mark */\n')
+        o.write(contents)
+
