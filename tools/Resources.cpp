6eabb4dd77dacddd25e7be8f05e1ef38eb9d278f
Matt Sarett <msarett@google.com>
2017-01-06 08:44:53 -0500

    Respect SkColorSpace in SkPNGImageEncoder
    This only changes behavior when the input SkBitmap/SkPixmap is
    tagged with a non-null SkColorSpace.  Android tags their bitmaps
    as sRGB when linear blending is enabled.  So this only changes
    behavior in Android when linear blending is turned on.
    *If linear blending is turned on, this will do a color correct
     encode (which is the desired behavior).
    *If linear blending is turned off, this will do a legacy encode.
    
    TODO: Add support for F16.
    TODO: Add color space support to WEBP.
    TODO: Tag encoded images with ICC profiles (when it makes sense).
    
    BUG=skia:
    
    Change-Id: Idd8a2836371d24a453d953e6fe2e76a87751be96

--- tools/Resources.cpp
+++ tools/Resources.cpp
@@ -36,8 +36,9 @@ bool GetResourceAsBitmap(const char* resource, SkBitmap* dst) {
     SkPMColor ctStorage[256];
     sk_sp<SkColorTable> ctable(new SkColorTable(ctStorage, 256));
     int count = ctable->count();
-    return dst->tryAllocPixels(gen->getInfo(), nullptr, ctable.get()) &&
-        gen->getPixels(gen->getInfo().makeColorSpace(nullptr), dst->getPixels(), dst->rowBytes(),
+    SkImageInfo info = gen->getInfo().makeColorSpace(nullptr);
+    return dst->tryAllocPixels(info, nullptr, ctable.get()) &&
+        gen->getPixels(info, dst->getPixels(), dst->rowBytes(),
                        const_cast<SkPMColor*>(ctable->readColors()), &count);
 }
 
