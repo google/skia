dcb878ff5ae4677515411df61fe37db7dda8fe6b
Mike Klein <mtklein@chromium.org>
2017-01-08 13:48:24 -0500

    SkSplicer
    I think I may have cracked the compile-ahead-of-time-splice-at-runtime nut.
    
    Change-Id: I1ec7dcb769613713ce56978c58038f606f87d63d

---
+++ src/splicer/SkSplicer_generated.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkSplicer_generated_DEFINED
+#define SkSplicer_generated_DEFINED
+
+static const char kSplice_load_f16[] = { 'a', 'b', 'b', 'a' };
+static const char kSplice_store_f16[] = { 'w', 'a', 't', 'e', 'r' };
+static const char kSplice_unpremul[] = { 'l', 'o', 'o' };
+
+#endif//SkSplicer_generated_DEFINED
