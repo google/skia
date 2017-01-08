3253eb33496fc3b81eeb4e2d7fdb19c6bfea01d3
Mike Reed <reed@google.com>
2017-01-08 14:19:23 -0500

    make SkRBuffer always validate
    BUG=skia:6102
    
    Change-Id: Ic9fb259b2e980d00e179340945c50492f1803a4f

--- src/core/SkBuffer.cpp
+++ src/core/SkBuffer.cpp
@@ -9,56 +9,49 @@
 
 #include <string.h>
 
-////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-void SkRBuffer::readNoSizeCheck(void* buffer, size_t size)
-{
-    SkASSERT((fData != 0 && fStop == 0) || fPos + size <= fStop);
-    if (buffer)
-        memcpy(buffer, fPos, size);
-    fPos += size;
-}
-
-const void* SkRBuffer::skip(size_t size)
-{
-    const void* result = fPos;
-    readNoSizeCheck(nullptr, size);
-    return result;
-}
-
-size_t SkRBuffer::skipToAlign4()
-{
-    size_t pos = this->pos();
-    size_t n = SkAlign4(pos) - pos;
-    fPos += n;
-    return n;
-}
-
-bool SkRBufferWithSizeCheck::read(void* buffer, size_t size) {
-    fError = fError || (size > static_cast<size_t>(fStop - fPos));
-    if (!fError && (size > 0)) {
-        readNoSizeCheck(buffer, size);
+bool SkRBuffer::read(void* buffer, size_t size) {
+    if (fValid && size <= this->available()) {
+        if (buffer) {
+            memcpy(buffer, fPos, size);
+        }
+        fPos += size;
+        return true;
+    } else {
+        fValid = false;
+        return false;
     }
-    return !fError;
 }
 
-void* SkWBuffer::skip(size_t size)
-{
+bool SkRBuffer::skipToAlign4() {
+    intptr_t pos = reinterpret_cast<intptr_t>(fPos);
+    size_t n = SkAlign4(pos) - pos;
+    if (fValid && n <= this->available()) {
+        fPos += n;
+        return true;
+    } else {
+        fValid = false;
+        return false;
+    }
+}
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void* SkWBuffer::skip(size_t size) {
     void* result = fPos;
     writeNoSizeCheck(nullptr, size);
     return fData == nullptr ? nullptr : result;
 }
 
-void SkWBuffer::writeNoSizeCheck(const void* buffer, size_t size)
-{
+void SkWBuffer::writeNoSizeCheck(const void* buffer, size_t size) {
     SkASSERT(fData == 0 || fStop == 0 || fPos + size <= fStop);
     if (fData && buffer)
         memcpy(fPos, buffer, size);
     fPos += size;
 }
 
-size_t SkWBuffer::padToAlign4()
-{
+size_t SkWBuffer::padToAlign4() {
     size_t pos = this->pos();
     size_t n = SkAlign4(pos) - pos;
 
@@ -84,54 +77,5 @@ size_t SkWBuffer::padToAlign4()
 #else
     #define AssertBuffer32(buffer)
 #endif
-
-void* sk_buffer_write_int32(void* buffer, int32_t value)
-{
-    AssertBuffer32(buffer);
-    *(int32_t*)buffer = value;
-    return (char*)buffer + sizeof(int32_t);
-}
-
-void* sk_buffer_write_int32(void* buffer, const int32_t values[], int count)
-{
-    AssertBuffer32(buffer);
-    SkASSERT(count >= 0);
-
-    memcpy((int32_t*)buffer, values, count * sizeof(int32_t));
-    return (char*)buffer + count * sizeof(int32_t);
-}
-
-const void* sk_buffer_read_int32(const void* buffer, int32_t* value)
-{
-    AssertBuffer32(buffer);
-    if (value)
-        *value = *(const int32_t*)buffer;
-    return (const char*)buffer + sizeof(int32_t);
-}
-
-const void* sk_buffer_read_int32(const void* buffer, int32_t values[], int count)
-{
-    AssertBuffer32(buffer);
-    SkASSERT(count >= 0);
-
-    if (values)
-        memcpy(values, (const int32_t*)buffer, count * sizeof(int32_t));
-    return (const char*)buffer + count * sizeof(int32_t);
-}
-
-void* sk_buffer_write_ptr(void* buffer, void* ptr)
-{
-    AssertBuffer32(buffer);
-    *(void**)buffer = ptr;
-    return (char*)buffer + sizeof(void*);
-}
-
-const void* sk_buffer_read_ptr(const void* buffer, void** ptr)
-{
-    AssertBuffer32(buffer);
-    if (ptr)
-        *ptr = *(void**)buffer;
-    return (const char*)buffer + sizeof(void*);
-}
 
 #endif
