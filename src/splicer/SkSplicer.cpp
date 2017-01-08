9ba70809037482c2ac7a2ddbba774fb0650687dc
Mike Klein <mtklein@chromium.org>
2017-01-08 15:41:46 -0500

    SkSplicer
    I think I may have cracked the compile-ahead-of-time-splice-at-runtime nut.
    
    This compiles stages ahead of time using clang, then splices them together at runtime.
    This means the stages can be written in simple C++.
    
    The this performs identically to our Xbyak experiment, and already supports more stages.  As written this stands alone from SkRasterPipeline_opts.h, but I'm fairly confident that the bulk (the STAGE implementations) can ultimately be shared.
    
    Change-Id: I1ec7dcb769613713ce56978c58038f606f87d63d

---
+++ src/splicer/SkSplicer.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkCpu.h"
+#include "SkOpts.h"
+#include "SkRasterPipeline.h"
+#include "SkStream.h"
+#include <sys/mman.h>
+
+#include "SkSplicer_shared.h"
+#include "SkSplicer_generated.h"
+
+
+namespace {
+
+    // Stages expect these constants to be set to these values.
+    // It's fine to rearrange and add new ones if you update SkSplicer_constants.
+    static const SkSplicer_constants kConstants = { 0x000000ff, 1.0f, 255.0f, 1/255.0f };
+
+    // Short x86-64 instruction sequences that we'll use as glue to splice together Stages.
+    static const uint8_t   vzeroupper[] = { 0xc5, 0xf8, 0x77 };        // clear top half of all ymm
+    static const uint8_t          ret[] = { 0xc3 };                    // return
+    static const uint8_t  movabsq_rcx[] = { 0x48, 0xb9 };              // move next 8 bytes into rcx
+    static const uint8_t  movabsq_rdx[] = { 0x48, 0xba };              // move next 8 bytes into rdx
+    static const uint8_t   addq_8_rdi[] = { 0x48, 0x83, 0xc7, 0x08 };  // rdi += 8
+    static const uint8_t cmpq_rsi_rdi[] = { 0x48, 0x39, 0xf7 };        // rdi cmp? rsi
+    static const uint8_t      jb_near[] = { 0x0f, 0x8c };              // jump relative next 4 bytes
+                                                                       //  if cmp set unsigned < bit
+
+    // We do this a lot, so it's nice to infer the correct size.  Works fine with arrays.
+    template <typename T>
+    void splice(SkWStream* stream, const T& val) {
+        stream->write(&val, sizeof(val));
+    }
+
+    // Copy len bytes from src to memory that's executable.  Free with munmap(fn, len).
+    static void* copy_to_executable_mem(const void* src, size_t len) {
+        // TODO: w^x
+        auto fn = mmap(nullptr, len, PROT_WRITE|PROT_EXEC, MAP_ANON|MAP_PRIVATE, -1, 0);
+        return memcpy(fn, src, len);
+    }
+
+    struct Spliced {
+
+        Spliced(const SkRasterPipeline::Stage* stages, int nstages) {
+            // We always create a backup interpreter pipeline,
+            //   - to handle any program we can't, and
+            //   - to handle the n < 8 tails.
+            fBackup     = SkOpts::compile_pipeline(stages, nstages);
+            fSplicedLen = 0;
+            fSpliced    = nullptr;
+            // If we return early anywhere in here, !fSpliced means we'll use fBackup instead.
+
+            // To keep things simple, only one target supported: Haswell+ x86-64.
+            if (!SkCpu::Supports(SkCpu::HSW) || sizeof(void*) != 8) {
+                return;
+            }
+
+            SkDynamicMemoryWStream buf;
+            //splice(&buf, (uint16_t)0x0b0f);  // uncomment to trap on function entry.
+
+            // Put the address of kConstants in rcx, Stage argument 4 "k".
+            splice(&buf, movabsq_rcx);
+            splice(&buf, &kConstants);
+
+            // We'll loop back to here as long as x<n after x+=8.
+            auto loop_start = buf.bytesWritten();  // Think of this like a label, loop_start:
+
+            for (int i = 0; i < nstages; i++) {
+                // If a stage has a context pointer, load it into rdx, Stage argument 3 "ctx".
+                if (stages[i].ctx) {
+                    splice(&buf, movabsq_rdx);
+                    splice(&buf, stages[i].ctx);
+                }
+
+                // Splice in the code for the Stages, generated offline into SkSplicer_generated.h.
+                switch(stages[i].stage) {
+                    case SkRasterPipeline::clear:      splice(&buf, kSplice_clear     ); break;
+                    case SkRasterPipeline::plus_:      splice(&buf, kSplice_plus      ); break;
+                    case SkRasterPipeline::srcover:    splice(&buf, kSplice_srcover   ); break;
+                    case SkRasterPipeline::clamp_0:    splice(&buf, kSplice_clamp_0   ); break;
+                    case SkRasterPipeline::clamp_1:    splice(&buf, kSplice_clamp_1   ); break;
+                    case SkRasterPipeline::clamp_a:    splice(&buf, kSplice_clamp_a   ); break;
+                    case SkRasterPipeline::swap:       splice(&buf, kSplice_swap      ); break;
+                    case SkRasterPipeline::premul:     splice(&buf, kSplice_premul    ); break;
+                    case SkRasterPipeline::unpremul:   splice(&buf, kSplice_unpremul  ); break;
+                    case SkRasterPipeline::load_8888:  splice(&buf, kSplice_load_8888 ); break;
+                    case SkRasterPipeline::store_8888: splice(&buf, kSplice_store_8888); break;
+                    case SkRasterPipeline::load_f16:   splice(&buf, kSplice_load_f16  ); break;
+                    case SkRasterPipeline::store_f16:  splice(&buf, kSplice_store_f16 ); break;
+
+                    // No joy (probably just not yet implemented).
+                    default: return;
+                }
+            }
+
+            // See if we should loop back to handle more pixels.
+            splice(&buf, addq_8_rdi);    // x += 8
+            splice(&buf, cmpq_rsi_rdi);  // if (x < n) { goto loop_start; }
+            splice(&buf, jb_near);
+            splice(&buf, (int)loop_start - (int)(buf.bytesWritten() + 4));
+
+            // Nope!  We're done.
+            splice(&buf, vzeroupper);
+            splice(&buf, ret);
+
+            auto data = buf.detachAsData();
+            fSplicedLen = data->size();
+            fSpliced    = copy_to_executable_mem(data->data(), fSplicedLen);
+        }
+
+        // Spliced is stored in a std::function, so it needs to be copyable.
+        Spliced(const Spliced& o) : fBackup    (o.fBackup)
+                                  , fSplicedLen(o.fSplicedLen)
+                                  , fSpliced   (copy_to_executable_mem(o.fSpliced, fSplicedLen)) {}
+
+        // copy_to_executable_mem() allocated fSpliced with mmap(), so munmap() it.
+        ~Spliced() {
+            if (fSpliced) {
+                munmap(fSpliced, fSplicedLen);
+            }
+        }
+
+        // Here's where we call fSpliced if we created it, fBackup if not.
+        void operator()(size_t x, size_t y, size_t n) const {
+            if (fSpliced) {
+                // TODO: At some point we will want to pass in y...
+                using Fn = void(size_t x, size_t n);
+                ((Fn*)fSpliced)(x,n);
+
+                // Fall through to fBackup for any n<8 last pixels.
+                size_t body = n/8*8;
+                x += body;
+                n -= body;
+            }
+            fBackup(x,y,n);
+        }
+
+        std::function<void(size_t, size_t, size_t)> fBackup;
+        size_t                                      fSplicedLen;
+        void*                                       fSpliced;
+    };
+
+}
+
+std::function<void(size_t, size_t, size_t)> SkRasterPipeline::jit() const {
+    return Spliced(fStages.data(), SkToInt(fStages.size()));
+}
