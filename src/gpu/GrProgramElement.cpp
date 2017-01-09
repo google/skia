332666e5bc2da53e109d853a72333d92cc7c4837
Brian Salomon <bsalomon@google.com>
2017-01-09 10:14:34 -0500

    Remove unused unique ID from GrProgramElement
    Change-Id: I27140b378ad665c40f6edced1a655150ebd4842c

--- src/gpu/GrProgramElement.cpp
+++ src/gpu/GrProgramElement.cpp
@@ -9,15 +9,6 @@
 #include "GrGpuResourceRef.h"
 #include "SkAtomics.h"
 
-uint32_t GrProgramElement::CreateUniqueID() {
-    static int32_t gUniqueID = SK_InvalidUniqueID;
-    uint32_t id;
-    do {
-        id = static_cast<uint32_t>(sk_atomic_inc(&gUniqueID) + 1);
-    } while (id == SK_InvalidUniqueID);
-    return id;
-}
-
 void GrProgramElement::addPendingIOs() const {
     for (int i = 0; i < fGpuResources.count(); ++i) {
         fGpuResources[i]->markPendingIO();
