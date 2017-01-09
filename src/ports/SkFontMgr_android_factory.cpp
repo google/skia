9ab7b211f31fbf3b21e04884c2b6f3a69d589fbe
Ben Wagner <bungeman@google.com>
2017-01-09 16:23:44 -0500

    Remove SkUseTestFontConfigFile.
    This method and its associated globals are no longer used by any user
    and can now be removed.
    
    BUG=skia:2817
    
    Change-Id: Ia627e2494f568a733999b980aec9284467d2640d

--- src/ports/SkFontMgr_android_factory.cpp
+++ src/ports/SkFontMgr_android_factory.cpp
@@ -10,42 +10,11 @@
 #include "SkFontMgr.h"
 #include "SkFontMgr_android.h"
 
-// For test only.
-static const char* gTestFontsXml = nullptr;
-static const char* gTestFallbackFontsXml = nullptr;
-static const char* gTestBasePath = nullptr;
-
-void SkUseTestFontConfigFile(const char* fontsXml, const char* fallbackFontsXml,
-                             const char* basePath)
-{
-    gTestFontsXml = fontsXml;
-    gTestFallbackFontsXml = fallbackFontsXml;
-    gTestBasePath = basePath;
-    SkASSERT(gTestFontsXml);
-    SkASSERT(gTestFallbackFontsXml);
-    SkASSERT(gTestBasePath);
-    SkDEBUGF(("Test BasePath: %s Fonts: %s FallbackFonts: %s\n",
-              gTestBasePath, gTestFontsXml, gTestFallbackFontsXml));
-}
-
 #ifdef SK_LEGACY_FONTMGR_FACTORY
 SkFontMgr* SkFontMgr::Factory() {
 #else
 sk_sp<SkFontMgr> SkFontMgr::Factory() {
 #endif
-    // These globals exist so that Chromium can override the environment.
-    // TODO: these globals need to be removed, and Chromium use SkFontMgr_New_Android instead.
-    if ((gTestFontsXml || gTestFallbackFontsXml) && gTestBasePath) {
-        SkFontMgr_Android_CustomFonts custom = {
-            SkFontMgr_Android_CustomFonts::kOnlyCustom,
-            gTestBasePath,
-            gTestFontsXml,
-            gTestFallbackFontsXml,
-            false /* fIsolated */
-        };
-        return SkFontMgr_New_Android(&custom);
-    }
-
     return SkFontMgr_New_Android(nullptr);
 }
 
