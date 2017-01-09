89cb821e9e9033281a33394709a1feed7eb0523a
Brian Salomon <bsalomon@google.com>
2017-01-09 10:48:23 -0500

    Separate SkArithmeticImageFilter from SkXfermodeImageFilter.
    Change-Id: I145eed7276456b546ca5c66bc1a0f0f74a84f138
    Reviewed-on: https://skia-review.googlesource.com/6728
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- include/effects/SkXfermodeImageFilter.h
+++ include/effects/SkXfermodeImageFilter.h
@@ -8,13 +8,13 @@
 #ifndef SkXfermodeImageFilter_DEFINED
 #define SkXfermodeImageFilter_DEFINED
 
+#include "SkArithmeticImageFilter.h"
 #include "SkBlendMode.h"
 #include "SkImageFilter.h"
 
 /**
- * This filter takes an xfermode, and uses it to composite the foreground
- * over the background.  If foreground or background is NULL, the input
- * bitmap (src) is used instead.
+ * This filter takes a SkBlendMode, and uses it to composite the foreground over the background.
+ * If foreground or background is NULL, the input bitmap (src) is used instead.
  */
 class SK_API SkXfermodeImageFilter {
 public:
@@ -25,16 +25,19 @@ public:
         return Make(mode, std::move(background), nullptr, nullptr);
     }
 
+    // Arithmetic image filtering used to be implemented using SkXfermode. Some clients still rely
+    // on these factories existing in this class.
     static sk_sp<SkImageFilter> MakeArithmetic(float k1, float k2, float k3, float k4,
-                                               bool enforcePMColor,
-                                               sk_sp<SkImageFilter> background,
+                                               bool enforcePMColor, sk_sp<SkImageFilter> background,
                                                sk_sp<SkImageFilter> foreground,
-                                               const SkImageFilter::CropRect* cropRect);
+                                               const SkImageFilter::CropRect* cropRect) {
+        return SkArithmeticImageFilter::Make(k1, k2, k3, k4, enforcePMColor, std::move(background),
+                                             std::move(foreground), cropRect);
+    }
     static sk_sp<SkImageFilter> MakeArithmetic(float k1, float k2, float k3, float k4,
                                                bool enforcePMColor,
                                                sk_sp<SkImageFilter> background) {
-        return MakeArithmetic(k1, k2, k3, k4, enforcePMColor, std::move(background),
-                              nullptr, nullptr);
+        return SkArithmeticImageFilter::Make(k1, k2, k3, k4, enforcePMColor, std::move(background));
     }
 
     SK_DECLARE_FLATTENABLE_REGISTRAR_GROUP();
