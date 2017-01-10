72ff26146d02218c63c73d06b54e74b705e9cc0f
Brian Osman <brianosman@google.com>
2017-01-10 18:47:27 +0000

    Revert "Add Gray8 pixel config"
    This reverts commit f2956459f707de596dcb2c79a7ee1fa62d599c0d.
    
    Reason for revert: GM and image failures on some bots (rendering red, not gray).
    
    Original change's description:
    > Add Gray8 pixel config
    > 
    > This is still just linear (non-sRGB), but adding sRGB will
    > be the next step. I've verified that this is really making
    > R8 textures when uploading Gray8 bitmaps. Tests pass, and
    > the all_bitmap_configs GM still renders correctly (unlike
    > when we just mapped Gray8 to Alpha8).
    > 
    > This adds another pixel config, which could grow our cache
    > footprint, but the benefits of not using 4bpp for 1bpp data
    > should outweigh that?
    > 
    > BUG=skia:6110
    > 
    > Change-Id: I4fc4c2479fc25f1d278e174a9bb5b542a0cb184c
    > Reviewed-on: https://skia-review.googlesource.com/6817
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    > 
    
    TBR=bsalomon@google.com,robertphillips@google.com,brianosman@google.com,reviews@skia.org
    BUG=skia:6110
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I95a4fc0450a569d5791f6bceb7fae61c7e5eba61

--- src/gpu/GrContext.cpp
+++ src/gpu/GrContext.cpp
@@ -680,7 +680,6 @@ static inline GrPixelConfig GrPixelConfigFallback(GrPixelConfig config) {
     static const GrPixelConfig kFallback[] = {
         kUnknown_GrPixelConfig,        // kUnknown_GrPixelConfig
         kRGBA_8888_GrPixelConfig,      // kAlpha_8_GrPixelConfig
-        kUnknown_GrPixelConfig,        // kGray_8_GrPixelConfig
         kUnknown_GrPixelConfig,        // kIndex_8_GrPixelConfig
         kRGBA_8888_GrPixelConfig,      // kRGB_565_GrPixelConfig
         kRGBA_8888_GrPixelConfig,      // kRGBA_4444_GrPixelConfig
@@ -701,22 +700,21 @@ static inline GrPixelConfig GrPixelConfigFallback(GrPixelConfig config) {
 
     GR_STATIC_ASSERT(0  == kUnknown_GrPixelConfig);
     GR_STATIC_ASSERT(1  == kAlpha_8_GrPixelConfig);
-    GR_STATIC_ASSERT(2  == kGray_8_GrPixelConfig);
-    GR_STATIC_ASSERT(3  == kIndex_8_GrPixelConfig);
-    GR_STATIC_ASSERT(4  == kRGB_565_GrPixelConfig);
-    GR_STATIC_ASSERT(5  == kRGBA_4444_GrPixelConfig);
-    GR_STATIC_ASSERT(6  == kRGBA_8888_GrPixelConfig);
-    GR_STATIC_ASSERT(7  == kBGRA_8888_GrPixelConfig);
-    GR_STATIC_ASSERT(8  == kSRGBA_8888_GrPixelConfig);
-    GR_STATIC_ASSERT(9  == kSBGRA_8888_GrPixelConfig);
-    GR_STATIC_ASSERT(10 == kRGBA_8888_sint_GrPixelConfig);
-    GR_STATIC_ASSERT(11 == kETC1_GrPixelConfig);
-    GR_STATIC_ASSERT(12 == kLATC_GrPixelConfig);
-    GR_STATIC_ASSERT(13 == kR11_EAC_GrPixelConfig);
-    GR_STATIC_ASSERT(14 == kASTC_12x12_GrPixelConfig);
-    GR_STATIC_ASSERT(15 == kRGBA_float_GrPixelConfig);
-    GR_STATIC_ASSERT(16 == kAlpha_half_GrPixelConfig);
-    GR_STATIC_ASSERT(17 == kRGBA_half_GrPixelConfig);
+    GR_STATIC_ASSERT(2  == kIndex_8_GrPixelConfig);
+    GR_STATIC_ASSERT(3  == kRGB_565_GrPixelConfig);
+    GR_STATIC_ASSERT(4  == kRGBA_4444_GrPixelConfig);
+    GR_STATIC_ASSERT(5  == kRGBA_8888_GrPixelConfig);
+    GR_STATIC_ASSERT(6  == kBGRA_8888_GrPixelConfig);
+    GR_STATIC_ASSERT(7  == kSRGBA_8888_GrPixelConfig);
+    GR_STATIC_ASSERT(8  == kSBGRA_8888_GrPixelConfig);
+    GR_STATIC_ASSERT(9  == kRGBA_8888_sint_GrPixelConfig);
+    GR_STATIC_ASSERT(10 == kETC1_GrPixelConfig);
+    GR_STATIC_ASSERT(11 == kLATC_GrPixelConfig);
+    GR_STATIC_ASSERT(12 == kR11_EAC_GrPixelConfig);
+    GR_STATIC_ASSERT(13 == kASTC_12x12_GrPixelConfig);
+    GR_STATIC_ASSERT(14 == kRGBA_float_GrPixelConfig);
+    GR_STATIC_ASSERT(15 == kAlpha_half_GrPixelConfig);
+    GR_STATIC_ASSERT(16 == kRGBA_half_GrPixelConfig);
     GR_STATIC_ASSERT(SK_ARRAY_COUNT(kFallback) == kGrPixelConfigCnt);
 }
 
