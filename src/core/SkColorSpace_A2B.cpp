67b9c683f4e47993946c39f7cb92f8cd6aab6f4d
Matt Sarett <msarett@google.com>
2017-01-11 17:38:59 -0500

    Unify ICC support for gray jpegs and gray pngs
    (1) Parse ICC gray profiles into RGB SkColorSpace objects.  This is
        easy - "gray transfer fn + white point" is subset of "RGB transfer fns
        + matrix".  This makes it easier/possible for the drawing code to reason
        about color spaces attached to kGray buffers.
    (2) Allow gray images to be tagged with gray ICCs OR rgb ICCs.  ICC gray
        forces to designer to use "D50 gray".  It is not uncommon to see gray
        images with RGB profiles - and this actually allows the designer to
        choose more kinds of gray (ex: sRGB gray).
    (3) Make SkJpegCodec support gray images with RGB ICCs.
    (4) Make SkPngCodec support gray images with Gray ICCs.
    (5) Delete gray from SkColorSpace_A2B - we no longer create these objects
        for gray profiles.
    
    BUG=skia:
    
    Change-Id: Id5eca803798330c54a19c4657def2e5976d1941e

--- src/core/SkColorSpace_A2B.cpp
+++ src/core/SkColorSpace_A2B.cpp
@@ -7,10 +7,12 @@
 
 #include "SkColorSpace_A2B.h"
 
-SkColorSpace_A2B::SkColorSpace_A2B(InputColorFormat inputColorFormat, std::vector<Element> elements, 
+SkColorSpace_A2B::SkColorSpace_A2B(ICCTypeFlag iccType, std::vector<Element> elements,
                                    PCS pcs, sk_sp<SkData> profileData)
     : INHERITED(std::move(profileData))
-    , fInputColorFormat(inputColorFormat)
+    , fICCType(iccType)
     , fElements(std::move(elements))
     , fPCS(pcs)
-{}
+{
+    SkASSERT(kRGB_ICCTypeFlag == iccType || kCMYK_ICCTypeFlag == iccType);
+}
