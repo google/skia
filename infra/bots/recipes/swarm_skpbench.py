323eff753272dd5ba079f9948c768ebc1a46656a
Eric Boren <borenet@google.com>
2017-01-12 08:46:20 -0500

    Move recipe expectations into example.py recipes inside of modules
    Each top-level recipe still needs one test and expectation file.
    
    BUG=skia:6070
    
    Change-Id: I3b14e8a11a86af68f5558177536f0018b644be4d

--- infra/bots/recipes/swarm_skpbench.py
+++ infra/bots/recipes/swarm_skpbench.py
@@ -14,43 +14,28 @@ DEPS = [
 ]
 
 
-TEST_BUILDERS = {
-  'client.skia': {
-    'skiabot-linux-swarm-000': [
-      'Perf-Android-Clang-PixelC-GPU-TegraX1-arm64-Release-GN_Android_Skpbench',
-      ('Perf-Android-Clang-PixelC-GPU-TegraX1-arm64-Release-' +
-      'GN_Android_Vulkan_Skpbench'),
-    ],
-  },
-}
-
-
 def RunSteps(api):
   api.skpbench.run()
 
 
 def GenTests(api):
-  for mastername, slaves in TEST_BUILDERS.iteritems():
-    for slavename, builders_by_slave in slaves.iteritems():
-      for builder in builders_by_slave:
-        test = (
-          api.test(builder) +
-          api.properties(buildername=builder,
-                         mastername=mastername,
-                         slavename=slavename,
-                         buildnumber=5,
-                         revision='abc123',
-                         path_config='kitchen',
-                         swarm_out_dir='[SWARM_OUT_DIR]') +
-          api.path.exists(
-              api.path['start_dir'].join('skia'),
-              api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                           'skp', 'VERSION'),
-          ) +
-          api.step_data('get swarming bot id',
-              stdout=api.raw_io.output('skia-bot-123')) +
-          api.step_data('get swarming task id',
-              stdout=api.raw_io.output('123456'))
-        )
-
-        yield test
+  b = 'Perf-Android-Clang-PixelC-GPU-TegraX1-arm64-Release-GN_Android_Skpbench'
+  yield (
+    api.test(b) +
+    api.properties(buildername=b,
+                   mastername='fake-master',
+                   slavename='fake-slave',
+                   buildnumber=5,
+                   revision='abc123',
+                   path_config='kitchen',
+                   swarm_out_dir='[SWARM_OUT_DIR]') +
+    api.path.exists(
+        api.path['start_dir'].join('skia'),
+        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
+                                   'skp', 'VERSION'),
+    ) +
+    api.step_data('get swarming bot id',
+        stdout=api.raw_io.output('skia-bot-123')) +
+    api.step_data('get swarming task id',
+        stdout=api.raw_io.output('123456'))
+  )
