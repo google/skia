2b861d3a391f2a9c93f97163d783c78935b7e72d
Eric Boren <borenet@google.com>
2017-01-12 08:15:38 -0500

    Move most recipes into modules
    The modules will eventually move into a shared repository.
    
    BUG=skia:6070
    
    Change-Id: I622265d98a5446f9f3ebf2c6a197770398623769
    Reviewed-on: https://skia-review.googlesource.com/6942
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/bots/recipes/swarm_skpbench.py
+++ infra/bots/recipes/swarm_skpbench.py
@@ -7,17 +7,10 @@
 
 
 DEPS = [
-  'build/file',
-  'core',
   'recipe_engine/path',
   'recipe_engine/properties',
-  'recipe_engine/python',
   'recipe_engine/raw_io',
-  'recipe_engine/step',
-  'recipe_engine/time',
-  'run',
-  'flavor',
-  'vars',
+  'skpbench',
 ]
 
 
@@ -32,84 +25,8 @@ TEST_BUILDERS = {
 }
 
 
-import calendar
-
-
-def _run(api, title, *cmd, **kwargs):
-  return api.run(api.step, title, cmd=list(cmd),
-                 cwd=api.vars.skia_dir, **kwargs)
-
-
-def _adb(api, title, *cmd, **kwargs):
-  if 'infra_step' not in kwargs:
-    kwargs['infra_step'] = True
-  return _run(api, title, 'adb', *cmd, **kwargs)
-
-
-def skpbench_steps(api):
-  """benchmark Skia using skpbench."""
-  app = api.vars.skia_out.join(api.vars.configuration, 'skpbench')
-  _adb(api, 'push skpbench', 'push', app, api.vars.android_bin_dir)
-
-  skpbench_dir = api.vars.slave_dir.join('skia', 'tools', 'skpbench')
-  table = api.path.join(api.vars.swarming_out_dir, 'table')
-
-  config = 'gpu,esinst4'
-  if 'Vulkan' in api.vars.builder_name:
-    config = 'vk'
-
-  skpbench_args = [
-        api.path.join(api.vars.android_bin_dir, 'skpbench'),
-        api.path.join(api.vars.android_data_dir, 'skps'),
-        '--adb',
-        '--resultsfile', table,
-        '--config', config]
-
-  api.run(api.python, 'skpbench',
-      script=skpbench_dir.join('skpbench.py'),
-      args=skpbench_args)
-
-  skiaperf_args = [
-    table,
-    '--properties',
-    'gitHash',      api.vars.got_revision,
-    'build_number', api.vars.build_number,
-  ]
-
-  skiaperf_args.extend(['no_buildbot', 'True'])
-  skiaperf_args.extend(['swarming_bot_id', api.vars.swarming_bot_id])
-  skiaperf_args.extend(['swarming_task_id', api.vars.swarming_task_id])
-
-  now = api.time.utcnow()
-  ts = int(calendar.timegm(now.utctimetuple()))
-  api.file.makedirs('perf_dir', api.vars.perf_data_dir)
-  json_path = api.path.join(
-      api.vars.perf_data_dir,
-      'skpbench_%s_%d.json' % (api.vars.got_revision, ts))
-
-  skiaperf_args.extend([
-    '--outfile', json_path
-  ])
-
-  keys_blacklist = ['configuration', 'role', 'is_trybot']
-  skiaperf_args.append('--key')
-  for k in sorted(api.vars.builder_cfg.keys()):
-    if not k in keys_blacklist:
-      skiaperf_args.extend([k, api.vars.builder_cfg[k]])
-
-  api.run(api.python, 'Parse skpbench output into Perf json',
-      script=skpbench_dir.join('skiaperf.py'),
-      args=skiaperf_args)
-
-
 def RunSteps(api):
-  api.core.setup()
-  try:
-    api.flavor.install(skps=True)
-    skpbench_steps(api)
-  finally:
-    api.flavor.cleanup_steps()
-  api.run.check_failure()
+  api.skpbench.run()
 
 
 def GenTests(api):
@@ -137,4 +54,3 @@ def GenTests(api):
         )
 
         yield test
-
--- infra/bots/recipes/upload_dm_results.py
+++ infra/bots/recipes/upload_dm_results.py
@@ -7,96 +7,13 @@
 
 
 DEPS = [
-  'build/file',
-  'recipe_engine/json',
-  'recipe_engine/path',
+  'upload_dm_results',
   'recipe_engine/properties',
-  'recipe_engine/shutil',
-  'recipe_engine/step',
-  'recipe_engine/time',
 ]
 
 
-import calendar
-
-
-DM_JSON = 'dm.json'
-GS_BUCKET = 'gs://skia-infra-gm'
-UPLOAD_ATTEMPTS = 5
-VERBOSE_LOG = 'verbose.log'
-
-
-def cp(api, name, src, dst, extra_args=None):
-  cmd = ['gsutil', 'cp']
-  if extra_args:
-    cmd.extend(extra_args)
-  cmd.extend([src, dst])
-
-  name = 'upload %s' % name
-  for i in xrange(UPLOAD_ATTEMPTS):
-    step_name = name
-    if i > 0:
-      step_name += ' (attempt %d)' % (i+1)
-    try:
-      api.step(step_name, cmd=cmd)
-      break
-    except api.step.StepFailure:
-      if i == UPLOAD_ATTEMPTS - 1:
-        raise
-
-
 def RunSteps(api):
-  builder_name = api.properties['buildername']
-  revision = api.properties['revision']
-
-  results_dir = api.path['start_dir'].join('dm')
-
-  # Move dm.json and verbose.log to their own directory.
-  json_file = results_dir.join(DM_JSON)
-  log_file = results_dir.join(VERBOSE_LOG)
-  tmp_dir = api.path['start_dir'].join('tmp_upload')
-  api.shutil.makedirs('tmp dir', tmp_dir, infra_step=True)
-  api.shutil.copy('copy dm.json', json_file, tmp_dir)
-  api.shutil.copy('copy verbose.log', log_file, tmp_dir)
-  api.shutil.remove('rm old dm.json', json_file)
-  api.shutil.remove('rm old verbose.log', log_file)
-
-  # Upload the images.
-  image_dest_path = '/'.join((GS_BUCKET, 'dm-images-v1'))
-  files_to_upload = api.file.glob(
-      'find images',
-      results_dir.join('*'),
-      test_data=['someimage.png'],
-      infra_step=True)
-  if len(files_to_upload) > 0:
-    cp(api, 'images', results_dir.join('*'), image_dest_path)
-
-  # Upload the JSON summary and verbose.log.
-  now = api.time.utcnow()
-  summary_dest_path = '/'.join([
-      'dm-json-v1',
-      str(now.year ).zfill(4),
-      str(now.month).zfill(2),
-      str(now.day  ).zfill(2),
-      str(now.hour ).zfill(2),
-      revision,
-      builder_name,
-      str(int(calendar.timegm(now.utctimetuple())))])
-
-  # Trybot results are further siloed by issue/patchset.
-  issue = str(api.properties.get('issue', ''))
-  patchset = str(api.properties.get('patchset', ''))
-  if api.properties.get('patch_storage', '') == 'gerrit':
-    issue = str(api.properties['patch_issue'])
-    patchset = str(api.properties['patch_set'])
-  if issue and patchset:
-    summary_dest_path = '/'.join((
-        'trybot', summary_dest_path, issue, patchset))
-
-  summary_dest_path = '/'.join((GS_BUCKET, summary_dest_path))
-
-  cp(api, 'JSON and logs', tmp_dir.join('*'), summary_dest_path,
-     ['-z', 'json,log'])
+  api.upload_dm_results.run()
 
 
 def GenTests(api):
