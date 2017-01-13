d9b6abb1a16493d85d4372bbf57b254023816feb
Brian Osman <brianosman@google.com>
2017-01-13 14:58:32 -0500

    Remove (unused) YUV API from SkPixelRef
    YUV conversion happens via SkImage now.
    
    BUG=skia:
    
    Change-Id: I6e1fa18effb72cbb00a173a346769b873e372c40
    Reviewed-on: https://skia-review.googlesource.com/7034
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- include/core/SkPixelRef.h
+++ include/core/SkPixelRef.h
@@ -18,7 +18,6 @@
 #include "SkRefCnt.h"
 #include "SkSize.h"
 #include "SkString.h"
-#include "SkYUVSizeInfo.h"
 
 class SkColorTable;
 struct SkIRect;
@@ -193,31 +192,6 @@ public:
 
     bool requestLock(const LockRequest&, LockResult*);
 
-    /**
-     *  If this can efficiently return YUV data, this should return true.
-     *  Otherwise this returns false and does not modify any of the parameters.
-     *
-     *  @param sizeInfo   Output parameter indicating the sizes and required
-     *                    allocation widths of the Y, U, and V planes.
-     *  @param colorSpace Output parameter.
-     */
-    bool queryYUV8(SkYUVSizeInfo* sizeInfo, SkYUVColorSpace* colorSpace) const {
-        return this->onQueryYUV8(sizeInfo, colorSpace);
-    }
-
-    /**
-     *  Returns true on success and false on failure.
-     *  Copies YUV data into the provided YUV planes.
-     *
-     *  @param sizeInfo   Needs to exactly match the values returned by the
-     *                    query, except the WidthBytes may be larger than the
-     *                    recommendation (but not smaller).
-     *  @param planes     Memory for each of the Y, U, and V planes.
-     */
-    bool getYUV8Planes(const SkYUVSizeInfo& sizeInfo, void* planes[3]) {
-        return this->onGetYUV8Planes(sizeInfo, planes);
-    }
-
     /** Populates dst with the pixels of this pixelRef, converting them to colorType. */
     bool readPixels(SkBitmap* dst, SkColorType colorType, const SkIRect* subset = NULL);
 
@@ -283,13 +257,6 @@ protected:
 
     // default impl does nothing.
     virtual void onNotifyPixelsChanged();
-
-    virtual bool onQueryYUV8(SkYUVSizeInfo*, SkYUVColorSpace*) const {
-        return false;
-    }
-    virtual bool onGetYUV8Planes(const SkYUVSizeInfo&, void*[3] /*planes*/) {
-        return false;
-    }
 
     /**
      *  Returns the size (in bytes) of the internally allocated memory.
