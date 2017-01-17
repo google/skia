9f87a9b003a70d81757733d9ab117ca69dd76533
Eric Boren <borenet@google.com>
2017-01-17 14:10:22 -0500

    Roll skia-recipes
    BUG=skia:
    
    Change-Id: I5cd47dd47747565c141240241b1373b34d69bee5

--- infra/bots/recipes/swarm_infra.py
+++ infra/bots/recipes/swarm_infra.py
@@ -38,6 +38,7 @@ def GenTests(api):
                      mastername='client.skia.fyi',
                      slavename='dummy-slave',
                      buildnumber=5,
+                     repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]')
@@ -49,6 +50,7 @@ def GenTests(api):
                      mastername='client.skia.fyi',
                      slavename='dummy-slave',
                      buildnumber=5,
+                     repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]') +
@@ -61,6 +63,7 @@ def GenTests(api):
                      mastername='client.skia.fyi',
                      slavename='dummy-slave',
                      buildnumber=5,
+                     repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]') +
