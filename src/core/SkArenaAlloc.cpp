2958ef607d6b9e14858b42c43f41f9c8bce00a0f
Herb Derby <herb@google.com>
2017-01-23 15:53:45 -0500

    Fix comparison that overflows for addresses near uint max. - Fix Assert
    TBR=mtklein@google.com
    BUG=chromium:683578
    
    Change-Id: Iba503d1febace367c71f79a3b9accc0ec3e50f11

--- src/core/SkArenaAlloc.cpp
+++ src/core/SkArenaAlloc.cpp
@@ -123,7 +123,7 @@ void SkArenaAlloc::ensureSpace(size_t size, size_t alignment) {
 char* SkArenaAlloc::allocObject(size_t size, size_t alignment) {
     size_t mask = alignment - 1;
     char* objStart = (char*)((uintptr_t)(fCursor + mask) & ~mask);
-    if (objStart + size > fEnd) {
+    if (size > (size_t)(fEnd - objStart)) {
         this->ensureSpace(size, alignment);
         objStart = (char*)((uintptr_t)(fCursor + mask) & ~mask);
     }
@@ -142,9 +142,13 @@ restart:
     char* objStart = (char*)((uintptr_t)(fCursor + skipOverhead + mask) & ~mask);
     size_t totalSize = sizeIncludingFooter + skipOverhead;
 
-    if (objStart + totalSize > fEnd) {
+    if (totalSize > (size_t)(fEnd - objStart)) {
         this->ensureSpace(totalSize, alignment);
         goto restart;
+    }
+
+    if (objStart + totalSize > fEnd) {
+        SkDebugf("objStart: %p, totalSize: %llu, fEnd: %p\n", objStart, totalSize, fEnd);
     }
 
     SkASSERT(objStart + totalSize <= fEnd);
