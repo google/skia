de72410c48f1cc1de64a3dbbb00d99edacc0a722
Jim Van Verth <jvanverth@google.com>
2017-01-23 13:14:04 -0500

    Add geometric implementation for ambient shadows
    BUG=skia:6119
    
    Change-Id: I3140522f223c35fc059a33b593064897485dff7c

---
+++ src/effects/shadows/SkShadowTessellator.cpp
@@ -0,0 +1,315 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkShadowTessellator.h"
+#include "SkGeometry.h"
+#include "GrPathUtils.h"
+
+static bool compute_normal(const SkPoint& p0, const SkPoint& p1, SkScalar radius, SkScalar dir,
+                           SkVector* newNormal) {
+    SkVector normal;
+    // compute perpendicular
+    normal.fX = p0.fY - p1.fY;
+    normal.fY = p1.fX - p0.fX;
+    if (!normal.normalize()) {
+        return false;
+    }
+    normal *= radius*dir;
+    *newNormal = normal;
+    return true;
+}
+
+static void compute_radial_steps(const SkVector& v1, const SkVector& v2, SkScalar r,
+                                 SkScalar* rotSin, SkScalar* rotCos, int* n) {
+    const SkScalar kRecipPixelsPerArcSegment = 0.25f;
+
+    SkScalar rCos = v1.dot(v2);
+    SkScalar rSin = v1.cross(v2);
+    SkScalar theta = SkScalarATan2(rSin, rCos);
+
+    SkScalar steps = r*theta*kRecipPixelsPerArcSegment;
+
+    SkScalar dTheta = theta / steps;
+    *rotSin = SkScalarSinCos(dTheta, rotCos);
+    *n = SkScalarFloorToInt(steps);
+}
+
+SkAmbientShadowTessellator::SkAmbientShadowTessellator(const SkMatrix& viewMatrix,
+                                                       const SkPath& path,
+                                                       SkScalar radius,
+                                                       GrColor umbraColor,
+                                                       bool transparent)
+    : fRadius(radius)
+    , fUmbraColor(umbraColor)
+    , fTransparent(transparent)
+    , fPrevInnerIndex(-1) {
+
+    fTransparent = true;
+
+    // Outer ring: 3*numPts
+    // Middle ring: numPts
+    fPositions.setReserve(4 * path.countPoints());
+    fColors.setReserve(4 * path.countPoints());
+    // Outer ring: 12*numPts
+    // Middle ring: 0
+    fIndices.setReserve(12 * path.countPoints());
+
+    fInitPoints.setReserve(3);
+
+    // walk around the path, tessellate and generate outer ring
+    // if original path is transparent, will accumulate sum of points for centroid
+    SkPath::Iter iter(path, true);
+    SkPoint pts[4];
+    SkPath::Verb verb;
+    if (fTransparent) {
+        *fPositions.push() = SkPoint::Make(0, 0);
+        *fColors.push() = umbraColor;
+        fCentroidCount = 0;
+    }
+    while ((verb = iter.next(pts)) != SkPath::kDone_Verb) {
+        switch (verb) {
+            case SkPath::kLine_Verb:
+                this->lineTo(viewMatrix, pts[1]);
+                break;
+            case SkPath::kQuad_Verb:
+                this->quadTo(viewMatrix, pts);
+                break;
+            case SkPath::kCubic_Verb:
+                this->cubicTo(viewMatrix, pts);
+                break;
+            case SkPath::kConic_Verb:
+                this->conicTo(viewMatrix, pts, iter.conicWeight());
+                break;
+            case SkPath::kMove_Verb:
+            case SkPath::kClose_Verb:
+            case SkPath::kDone_Verb:
+                break;
+        }
+    }
+
+    SkVector normal;
+    if (compute_normal(fPositions[fPrevInnerIndex], fPositions[fFirstVertex], fRadius, fDirection,
+                       &normal)) {
+        addArc(normal);
+
+        // add final edge
+        *fPositions.push() = fPositions[fFirstVertex] + normal;
+        *fColors.push() = 0;
+
+        *fIndices.push() = fPrevInnerIndex;
+        *fIndices.push() = fPositions.count() - 2;
+        *fIndices.push() = fFirstVertex;
+    }
+
+    *fIndices.push() = fPositions.count() - 2;
+    *fIndices.push() = fPositions.count() - 1;
+    *fIndices.push() = fFirstVertex;
+
+    // finalize centroid
+    if (fTransparent) {
+        fPositions[0] *= SkScalarFastInvert(fCentroidCount);
+
+        *fIndices.push() = 0;
+        *fIndices.push() = fPrevInnerIndex;
+        *fIndices.push() = fFirstVertex;
+    }
+
+    // final fan
+    fPrevInnerIndex = fFirstVertex;
+    fPrevNormal = normal;
+    addArc(fFirstNormal);
+
+    *fIndices.push() = fFirstVertex;
+    *fIndices.push() = fPositions.count() - 1;
+    *fIndices.push() = fFirstVertex + 1;    
+}
+
+// tesselation tolerance values, in device space pixels
+static const SkScalar kQuadTolerance = 0.2f;
+static const SkScalar kCubicTolerance = 0.2f;
+static const SkScalar kConicTolerance = 0.5f;
+
+void SkAmbientShadowTessellator::lineTo(const SkPoint& p)  {
+    if (fInitPoints.count() < 2) {
+        *fInitPoints.push() = p;
+        return;
+    } else if (fInitPoints.count() == 2) {
+        // determine if cw or ccw
+        SkVector v0 = fInitPoints[1] - fInitPoints[0];
+        SkVector v1 = p - fInitPoints[0];
+        SkScalar perpDot = v0.fX*v1.fY - v0.fY*v1.fX;
+        if (SkScalarNearlyZero(perpDot)) {
+            // nearly parallel, just treat as straight line and continue
+            fInitPoints[1] = p;
+            return;
+        } 
+
+        // if perpDot > 0, winding is ccw
+        fDirection = (perpDot > 0) ? -1 : 1;
+
+        // add first quad
+        if (!compute_normal(fInitPoints[0], fInitPoints[1], fRadius, fDirection,
+                            &fFirstNormal)) {
+            // first two points are incident, make the third point the second and continue
+            fInitPoints[1] = p;
+            return;
+        }
+
+        fFirstVertex = fPositions.count();
+        fPrevNormal = fFirstNormal;
+        fPrevInnerIndex = fFirstVertex + 2; 
+
+        *fPositions.push() = fInitPoints[0];
+        *fColors.push() = fUmbraColor;
+        *fPositions.push() = fInitPoints[0] + fFirstNormal;
+        *fColors.push() = 0;
+        *fPositions.push() = fInitPoints[1];
+        *fColors.push() = fUmbraColor;
+        *fPositions.push() = fInitPoints[1] + fFirstNormal;
+        *fColors.push() = 0;
+
+        *fIndices.push() = fFirstVertex;
+        *fIndices.push() = fFirstVertex + 1;
+        *fIndices.push() = fPrevInnerIndex;
+
+        *fIndices.push() = fFirstVertex + 1;
+        *fIndices.push() = fFirstVertex + 3;
+        *fIndices.push() = fPrevInnerIndex;
+
+        // if transparent, add points to first one in array and add to center fan
+        if (fTransparent) {
+            fPositions[0] += fInitPoints[0];
+            fPositions[0] += fInitPoints[1];
+            fCentroidCount += 2;
+
+            *fIndices.push() = 0;
+            *fIndices.push() = fFirstVertex;
+            *fIndices.push() = fPrevInnerIndex;
+        }
+
+        // to ensure we skip this block next time
+        *fInitPoints.push() = p;
+    }
+
+    SkVector normal;
+    if (compute_normal(fPositions[fPrevInnerIndex], p, fRadius, fDirection, &normal)) {
+        addArc(normal);
+        addEdge(p, normal);
+    }
+}
+
+void SkAmbientShadowTessellator::lineTo(const SkMatrix& m, SkPoint p)  {
+    m.mapPoints(&p, 1);
+    this->lineTo(p);
+}
+
+void SkAmbientShadowTessellator::quadTo(const SkPoint pts[3]) {
+    int maxCount = GrPathUtils::quadraticPointCount(pts, kQuadTolerance);
+    fPointBuffer.setReserve(maxCount);
+    SkPoint* target = fPointBuffer.begin();
+    int count = GrPathUtils::generateQuadraticPoints(pts[0], pts[1], pts[2],
+                                                     kQuadTolerance, &target, maxCount);
+    fPointBuffer.setCount(count);
+    for (int i = 0; i < count - 1; i++) {
+        this->lineTo(fPointBuffer[i]);
+    }
+    this->lineTo(fPointBuffer[count - 1]);
+
+}
+
+void SkAmbientShadowTessellator::quadTo(const SkMatrix& m, SkPoint pts[3]) {
+    m.mapPoints(pts, 3);
+    this->quadTo(pts);
+}
+
+void SkAmbientShadowTessellator::cubicTo(const SkMatrix& m, SkPoint pts[4]) {
+    m.mapPoints(pts, 4);
+    int maxCount = GrPathUtils::cubicPointCount(pts, kCubicTolerance);
+    fPointBuffer.setReserve(maxCount);
+    SkPoint* target = fPointBuffer.begin();
+    int count = GrPathUtils::generateCubicPoints(pts[0], pts[1], pts[2], pts[3],
+                                                 kCubicTolerance, &target, maxCount);
+    fPointBuffer.setCount(count);
+    for (int i = 0; i < count - 1; i++) {
+        this->lineTo(fPointBuffer[i]);
+    }
+    this->lineTo(fPointBuffer[count - 1]);
+}
+
+void SkAmbientShadowTessellator::conicTo(const SkMatrix& m, SkPoint pts[3], SkScalar w) {
+    m.mapPoints(pts, 3);
+    SkAutoConicToQuads quadder;
+    const SkPoint* quads = quadder.computeQuads(pts, w, kConicTolerance);
+    SkPoint lastPoint = *(quads++);
+    int count = quadder.countQuads();
+    for (int i = 0; i < count; ++i) {
+        SkPoint quadPts[3];
+        quadPts[0] = lastPoint;
+        quadPts[1] = quads[0];
+        quadPts[2] = i == count - 1 ? pts[2] : quads[1];
+        this->quadTo(quadPts);
+        lastPoint = quadPts[2];
+        quads += 2;
+    }
+}
+
+void SkAmbientShadowTessellator::addArc(const SkVector& nextNormal) {
+    // fill in fan from previous quad
+    SkScalar rotSin, rotCos;
+    int numSteps;
+    compute_radial_steps(fPrevNormal, nextNormal, fRadius, &rotSin, &rotCos, &numSteps);
+    SkVector prevNormal = fPrevNormal;
+    for (int i = 0; i < numSteps; ++i) {
+        SkVector nextNormal;
+        nextNormal.fX = prevNormal.fX*rotCos - prevNormal.fY*rotSin;
+        nextNormal.fY = prevNormal.fY*rotCos + prevNormal.fX*rotSin;
+        *fPositions.push() = fPositions[fPrevInnerIndex] + nextNormal;
+        *fColors.push() = 0;
+        *fIndices.push() = fPrevInnerIndex;
+        *fIndices.push() = fPositions.count() - 2;
+        *fIndices.push() = fPositions.count() - 1;
+
+        prevNormal = nextNormal;
+    }
+}
+
+
+void SkAmbientShadowTessellator::addEdge(const SkPoint& nextPoint, const SkVector& nextNormal) {
+    // close out previous arc
+    *fPositions.push() = fPositions[fPrevInnerIndex] + nextNormal;
+    *fColors.push() = 0;
+    *fIndices.push() = fPrevInnerIndex;
+    *fIndices.push() = fPositions.count() - 2;
+    *fIndices.push() = fPositions.count() - 1;
+
+    // add next quad
+    *fPositions.push() = nextPoint;
+    *fColors.push() = fUmbraColor;
+    *fPositions.push() = nextPoint + nextNormal;
+    *fColors.push() = 0;
+
+    *fIndices.push() = fPrevInnerIndex;
+    *fIndices.push() = fPositions.count() - 3;
+    *fIndices.push() = fPositions.count() - 2;
+
+    *fIndices.push() = fPositions.count() - 3;
+    *fIndices.push() = fPositions.count() - 1;
+    *fIndices.push() = fPositions.count() - 2;
+
+    // if transparent, add point to first one in array and add to center fan
+    if (fTransparent) {
+        fPositions[0] += nextPoint;
+        ++fCentroidCount;
+
+        *fIndices.push() = 0;
+        *fIndices.push() = fPrevInnerIndex;
+        *fIndices.push() = fPositions.count() - 2;
+    }
+
+    fPrevInnerIndex = fPositions.count() - 2;
+    fPrevNormal = nextNormal;
+}
