c5338f0558a3e8bc14386f00357e34ac3cdd0dbf
Herb Derby <herb@google.com>
2017-01-23 16:57:09 -0500

    Fix comparison that overflows for addresses near uint max. - Fix Assert
    TBR=mtklein@google.com
    BUG=chromium:683578
    
    Change-Id: Iba503d1febace367c71f79a3b9accc0ec3e50f11

--- src/core/SkArenaAlloc.h
+++ src/core/SkArenaAlloc.h
@@ -68,6 +68,7 @@ public:
 
     template <typename T, typename... Args>
     T* make(Args&&... args) {
+        SkASSERT(SkTFitsIn<uint32_t>(sizeof(T)));
         char* objStart;
         if (skstd::is_trivially_destructible<T>::value) {
             objStart = this->allocObject(sizeof(T), alignof(T));
@@ -139,6 +140,7 @@ private:
         SkASSERT(SkTFitsIn<uint32_t>(count));
         char* objStart;
         size_t arraySize = count * sizeof(T);
+        SkASSERT(SkTFitsIn<uint32_t>(arraySize));
 
         if (skstd::is_trivially_destructible<T>::value) {
             objStart = this->allocObject(arraySize, alignof(T));
