c6ab0f4d7a043ebf7568efcae335b96c9f4ccd04
Matt Sarett <msarett@google.com>
2017-01-23 09:41:08 -0500

    Allow conversion from non-opaque to opaque [chrome/m57]
    BUG:683787
    
    Change-Id: I1b78cc8d1b5d3917a2a952da036b93022e99e053
    Reviewed-on: https://skia-review.googlesource.com/7374
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Matt Sarett <msarett@google.com>
    (cherry picked from commit bfe8dca7dfdd9cafbccba0a637f2fcd58c7a54fb)

--- src/core/SkImageInfoPriv.h
+++ src/core/SkImageInfoPriv.h
@@ -69,9 +69,10 @@ static inline bool SkImageInfoValidConversion(const SkImageInfo& dst, const SkIm
         return false;
     }
 
-    if (kOpaque_SkAlphaType == dst.alphaType() && kOpaque_SkAlphaType != src.alphaType()) {
-        return false;
-    }
+    // TODO (msarett): This is diabled for crbug.com/683787.  How should we handle this case?
+    //if (kOpaque_SkAlphaType == dst.alphaType() && kOpaque_SkAlphaType != src.alphaType()) {
+    //    return false;
+    //}
 
     return true;
 }
