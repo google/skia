434d076901817f900950ea52f70fb3f098734d50
Jim Van Verth <jvanverth@google.com>
2017-01-23 12:57:50 -0500

    Add geometric implementation for ambient shadows
    BUG=skia:6119
    
    Change-Id: I3140522f223c35fc059a33b593064897485dff7c

--- samplecode/SampleAndroidShadows.cpp
+++ samplecode/SampleAndroidShadows.cpp
@@ -406,14 +406,14 @@ protected:
 
         if (fShowObject) {
             canvas->drawPath(path, paint);
-        } else {
+        }/* else {
             SkPaint strokePaint;
 
             strokePaint.setColor(paint.getColor());
             strokePaint.setStyle(SkPaint::kStroke_Style);
 
             canvas->drawPath(path, strokePaint);
-        }
+        }*/
     }
 
     void onDrawContent(SkCanvas* canvas) override {
@@ -431,27 +431,27 @@ protected:
         canvas->translate(200, 90);
         lightPos.fX += 200;
         lightPos.fY += 90;
-        this->drawShadowedPath(canvas, fRectPath, 2, paint, kAmbientAlpha,
+        this->drawShadowedPath(canvas, fRectPath, 64, paint, kAmbientAlpha,
                                lightPos, kLightWidth, kSpotAlpha);
 
-        paint.setColor(SK_ColorRED);
-        canvas->translate(250, 0);
-        lightPos.fX += 250;
-        this->drawShadowedPath(canvas, fRRPath, 4, paint, kAmbientAlpha,
-                               lightPos, kLightWidth, kSpotAlpha);
+        //paint.setColor(SK_ColorRED);
+        //canvas->translate(250, 0);
+        //lightPos.fX += 250;
+        //this->drawShadowedPath(canvas, fRRPath, 4, paint, kAmbientAlpha,
+        //                       lightPos, kLightWidth, kSpotAlpha);
 
-        paint.setColor(SK_ColorBLUE);
-        canvas->translate(-250, 110);
-        lightPos.fX -= 250;
-        lightPos.fY += 110;
-        this->drawShadowedPath(canvas, fCirclePath, 8, paint, 0.0f,
-                               lightPos, kLightWidth, 0.5f);
+        //paint.setColor(SK_ColorBLUE);
+        //canvas->translate(-250, 110);
+        //lightPos.fX -= 250;
+        //lightPos.fY += 110;
+        //this->drawShadowedPath(canvas, fCirclePath, 8, paint, 0.0f,
+        //                       lightPos, kLightWidth, 0.5f);
 
-        paint.setColor(SK_ColorGREEN);
-        canvas->translate(250, 0);
-        lightPos.fX += 250;
-        this->drawShadowedPath(canvas, fRRPath, 64, paint, kAmbientAlpha,
-                               lightPos, kLightWidth, kSpotAlpha);
+        //paint.setColor(SK_ColorGREEN);
+        //canvas->translate(250, 0);
+        //lightPos.fX += 250;
+        //this->drawShadowedPath(canvas, fRRPath, 64, paint, kAmbientAlpha,
+        //                       lightPos, kLightWidth, kSpotAlpha);
     }
 
 protected:
