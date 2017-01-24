fdb1bdf1aa4e7f14befcb3acc3f586e7734190ea
Jim Van Verth <jvanverth@google.com>
2017-01-24 16:27:57 -0500

    Add geometric implementation for ambient shadows
    BUG=skia:6119
    
    Change-Id: I3140522f223c35fc059a33b593064897485dff7c
    Reviewed-on: https://skia-review.googlesource.com/7273
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

---
+++ src/effects/shadows/SkShadowTessellator.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkShadowTessellator_DEFINED
+#define SkShadowTessellator_DEFINED
+
+#include "GrColor.h"
+#include "SkTDArray.h"
+#include "SkPoint.h"
+
+class SkMatrix;
+class SkPath;
+
+/**
+ * This class generates an ambient shadow for a path by walking the path, outsetting by the
+ * radius, and setting inner and outer colors to umbraColor and penumbraColor, respectively.
+ * If transparent is true, then the center of the ambient shadow will be filled in.
+ */
+class SkAmbientShadowTessellator {
+public:
+    SkAmbientShadowTessellator(const SkMatrix& viewMatrix, const SkPath& path, SkScalar radius,
+                               GrColor umbraColor, GrColor penumbraColor, bool transparent);
+
+    int      vertexCount() { return fPositions.count(); }
+    SkPoint* positions() { return fPositions.begin(); }
+    GrColor* colors() { return fColors.begin(); }
+    int      indexCount() { return fIndices.count(); }
+    uint16_t* indices() { return fIndices.begin(); }
+
+private:
+    void handleLine(const SkPoint& p);
+    void handleLine(const SkMatrix& m, SkPoint p);
+
+    void handleQuad(const SkPoint pts[3]);
+    void handleQuad(const SkMatrix& m, SkPoint pts[3]);
+
+    void handleCubic(const SkMatrix& m, SkPoint pts[4]);
+
+    void handleConic(const SkMatrix& m, SkPoint pts[3], SkScalar w);
+
+    void addArc(const SkVector& nextNormal);
+    void finishArcAndAddEdge(const SkVector& nextPoint, const SkVector& nextNormal);
+    void addEdge(const SkVector& nextPoint, const SkVector& nextNormal);
+
+    SkScalar            fRadius;
+    GrColor             fUmbraColor;
+    GrColor             fPenumbraColor;
+    bool                fTransparent;
+
+    SkTDArray<SkPoint>  fPositions;
+    SkTDArray<GrColor>  fColors;
+    SkTDArray<uint16_t> fIndices;
+
+    int                 fPrevInnerIndex;
+    SkVector            fPrevNormal;
+    int                 fFirstVertex;
+    SkVector            fFirstNormal;
+    SkScalar            fDirection;
+    int                 fCentroidCount;
+
+    // first three points
+    SkTDArray<SkPoint>  fInitPoints;
+    // temporary buffer
+    SkTDArray<SkPoint>  fPointBuffer;
+};
+
+#endif
