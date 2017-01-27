01b67713de98b1166894dba69a9d8f0619aaaf8f
Florin Malita <fmalita@chromium.org>
2017-01-27 13:26:22 -0500

    SkRasterPipeline shader adapter
    (lifted from https://skia-review.googlesource.com/c/7088/)
    
    R=mtklein@google.com,herb@google.com,reed@google.com
    
    CQ_INCLUDE_TRYBOTS=skia.primary:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD
    
    Change-Id: Idddb84069423c5fc535bea0a65a5b21a4d07084d

--- src/core/SkShader.cpp
+++ src/core/SkShader.cpp
@@ -5,6 +5,7 @@
  * found in the LICENSE file.
  */
 
+#include "SkArenaAlloc.h"
 #include "SkAtomics.h"
 #include "SkBitmapProcShader.h"
 #include "SkColorShader.h"
@@ -13,9 +14,11 @@
 #include "SkPaint.h"
 #include "SkPicture.h"
 #include "SkPictureShader.h"
+#include "SkRasterPipeline.h"
 #include "SkReadBuffer.h"
 #include "SkScalar.h"
 #include "SkShader.h"
+#include "SkTLazy.h"
 #include "SkWriteBuffer.h"
 
 #if SK_SUPPORT_GPU
@@ -260,6 +263,43 @@ bool SkShader::appendStages(SkRasterPipeline* pipeline,
                             const SkMatrix& ctm,
                             const SkPaint& paint) const {
     return this->onAppendStages(pipeline, dst, scratch, ctm, paint, nullptr);
+}
+
+bool SkShader::onAppendStages(SkRasterPipeline* p,
+                              SkColorSpace* cs,
+                              SkArenaAlloc* alloc,
+                              const SkMatrix& ctm,
+                              const SkPaint& paint,
+                              const SkMatrix* localM) const {
+    // Legacy shaders handle the paint opacity internally,
+    // but RP applies it as a separate stage.
+    SkTCopyOnFirstWrite<SkPaint> opaquePaint(paint);
+    if (paint.getAlpha() != SK_AlphaOPAQUE) {
+        opaquePaint.writable()->setAlpha(SK_AlphaOPAQUE);
+    }
+
+    ContextRec rec{ *opaquePaint, ctm, localM, ContextRec::kPM4f_DstType, cs };
+
+    // TODO: re-evaluate/improve?
+    static constexpr size_t kMaxContextSize = 4096;
+    if (this->contextSize(rec) > kMaxContextSize) {
+        return false;
+    }
+
+    struct Buffer {
+        Context* ctx;
+        uint8_t bytes[kMaxContextSize];
+        ~Buffer() { if (ctx) ctx->~Context(); }
+    };
+
+    auto b = alloc->make<Buffer>();
+    b->ctx = this->createContext(rec, b->bytes);
+    if (!b->ctx) {
+        return false;
+    }
+
+    p->append(SkRasterPipeline::shader_adapter, b->ctx);
+    return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
