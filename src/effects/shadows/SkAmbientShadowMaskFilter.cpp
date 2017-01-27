b393d88cc4bc3dacee9a2416607fe90678109cf8
Brian Salomon <bsalomon@google.com>
2017-01-26 21:51:47 -0500

    start on ambient shadow cache
    Change-Id: If882186225621af4af4b4ddae0c786ec33ff40f3

--- src/effects/shadows/SkAmbientShadowMaskFilter.cpp
+++ src/effects/shadows/SkAmbientShadowMaskFilter.cpp
@@ -195,6 +195,112 @@ bool SkAmbientShadowMaskFilterImpl::canFilterMaskGPU(const SkRRect& devRRect,
 static const float kHeightFactor = 1.0f / 128.0f;
 static const float kGeomFactor = 64.0f;
 
+#include "SkResourceCache.h"
+#include "GrShape.h"
+
+namespace {
+class TessPathData : public SkRefCnt {
+public:
+    TessPathData(std::unique_ptr<const SkPoint[]>&& positions,
+                 std::unique_ptr<const GrColor[]>&& colors,
+                 std::unique_ptr<const uint16_t[]>&& indices,
+                 int vertexCnt, int indexCnt) : fVertexCnt(vertexCnt), fIndexCnt(indexCnt), fPositions(std::move(positions)), fColors(std::move(colors)), fIndices(std::move(indices)) {
+        SkASSERT(SkToBool(indices) == SkToBool(indexCnt));
+    }
+
+    int vertexCount() const { return fVertexCnt; }
+    const SkPoint* positions() const { return fPositions.get(); }
+    const GrColor* colors() const { return fColors.get(); }
+    int indexCount() const { return fIndexCnt; }
+    const uint16_t* indices() const { return fIndices.get(); }
+
+    size_t size() const {
+        size_t size = sizeof(*this) + fVertexCnt * (sizeof(SkPoint) + sizeof(GrColor));
+        size += fIndexCnt * sizeof(uint16_t);
+        return size;
+    }
+
+private:
+    int fVertexCnt;
+    int fIndexCnt;
+    std::unique_ptr<const SkPoint[]> fPositions;
+    std::unique_ptr<const GrColor[]> fColors;
+    std::unique_ptr<const uint16_t[]> fIndices;
+};
+
+class TessPathRec : public SkResourceCache::Rec {
+public:
+    TessPathRec(const SkResourceCache::Key& key,
+                const SkMatrix& viewMatrix,
+                SkScalar occluderHeight,
+                SkScalar ambientAlpha,
+                uint32_t flags,
+                sk_sp<TessPathData> data) : fData(std::move(data)), fOccluderHeight(occluderHeight), fAmbientAlpha(ambientAlpha), fFlags(flags), fOriginalMatrix(viewMatrix) {
+        fKey.reset(new (new uint8_t[key.size()]) SkResourceCache::Key);
+        memcpy(fKey.get(), &key, key.size());
+    }
+
+    const Key& getKey() const override { return *fKey; }
+    size_t bytesUsed() const override { return fData->size(); }
+
+    // for memory usage diagnostics
+    virtual const char* getCategory() const override { return "tessellated shadow mask"; }
+
+    sk_sp<TessPathData> refData() const { return fData; }
+
+    const SkMatrix& originalViewMatrix() const  { return fOriginalMatrix; }
+    SkScalar occluderHeight() const { return fOccluderHeight; }
+    SkScalar ambientAlpha() const { return fAmbientAlpha; }
+    SkScalar flags() const { return fFlags; }
+
+private:
+    std::unique_ptr<SkResourceCache::Key> fKey;
+    sk_sp<TessPathData> fData;
+    SkScalar fOccluderHeight;
+    SkScalar fAmbientAlpha;
+    uint32_t fFlags;
+    SkMatrix fOriginalMatrix;
+};
+
+struct FindContext {
+    FindContext(const SkMatrix* viewMatrix) : fViewMatrix(viewMatrix) {}
+    const SkMatrix* fViewMatrix;
+    SkVector fTranslate = {0, 0};
+    sk_sp<TessPathData> fData;
+    SkScalar fOccluderHeight;
+    SkScalar fAmbientAlpha;
+    uint32_t fFlags;
+};
+
+bool FindVisitor(const SkResourceCache::Rec& baseRec, void* ctx) {
+    FindContext* findContext = (FindContext*)ctx;
+    const SkMatrix& viewMatrix = *findContext->fViewMatrix;
+    if (findContext->fViewMatrix->hasPerspective()) {
+        return false;
+    }
+
+    const TessPathRec& rec = static_cast<const TessPathRec&>(baseRec);
+    const SkMatrix& recMatrix = rec.originalViewMatrix();
+    SkASSERT(!recMatrix.hasPerspective());
+    if (recMatrix.getScaleX() != viewMatrix.getScaleX() ||
+        recMatrix.getSkewX() != viewMatrix.getSkewX() ||
+        recMatrix.getScaleY() != viewMatrix.getScaleY() ||
+        recMatrix.getSkewY() != viewMatrix.getSkewY()) {
+        return false;
+    }
+    if (findContext->fOccluderHeight != rec.occluderHeight() ||
+        findContext->fAmbientAlpha != rec.ambientAlpha() ||
+        findContext->fFlags != rec.flags()) {
+        return false;
+    }
+    findContext->fTranslate.fX = viewMatrix.getTranslateX() - recMatrix.getTranslateX();
+    findContext->fTranslate.fY = viewMatrix.getTranslateY() - recMatrix.getTranslateY();
+    findContext->fData = rec.refData();
+    return true;
+}
+
+}
+
 bool SkAmbientShadowMaskFilterImpl::directFilterMaskGPU(GrTextureProvider* texProvider,
                                                         GrRenderTargetContext* rtContext,
                                                         GrPaint&& paint,
@@ -202,6 +308,7 @@ bool SkAmbientShadowMaskFilterImpl::directFilterMaskGPU(GrTextureProvider* texPr
                                                         const SkMatrix& viewMatrix,
                                                         const SkStrokeRec&,
                                                         const SkPath& path) const {
+    FindContext context(&viewMatrix);
     SkASSERT(rtContext);
     // TODO: this will not handle local coordinates properly
 
@@ -229,25 +336,64 @@ bool SkAmbientShadowMaskFilterImpl::directFilterMaskGPU(GrTextureProvider* texPr
     }
 #endif
 
-    SkScalar radius = fOccluderHeight * kHeightFactor * kGeomFactor;
-    SkScalar umbraAlpha = SkScalarInvert((1.0f+SkTMax(fOccluderHeight * kHeightFactor, 0.0f)));
-    // umbraColor is the interior value, penumbraColor the exterior value.
-    // umbraAlpha is the factor that is linearly interpolated from outside to inside, and
-    // then "blurred" by the ShadowEdgeFP. It is then multiplied by fAmbientAlpha to get
-    // the final alpha.
-    GrColor  umbraColor = GrColorPackRGBA(0, 0, fAmbientAlpha*255.9999f, umbraAlpha*255.9999f);
-    GrColor  penumbraColor = GrColorPackRGBA(0, 0, fAmbientAlpha*255.9999f, 0);
+    static void* kNamespace;
+    GrShape shape(path, GrStyle::SimpleFill());
+    SkResourceCache::Key* key = nullptr;
+    SkAutoSTArray<32 * 4, uint8_t> keyStorage;
+    int shapeKeySize = shape.unstyledKeySize();
+    if (shapeKeySize >= 0) {
+        shapeKeySize *= 4;
+        keyStorage.reset(shapeKeySize + sizeof(SkResourceCache::Key));
+        key = new (keyStorage.begin()) SkResourceCache::Key();
+        shape.writeUnstyledKey((uint32_t*)(keyStorage.begin() + sizeof(*key)));
+        key->init(&kNamespace, 0, shapeKeySize);
 
-    GrAmbientShadowTessellator tess(SkMatrix::I(), path, radius, umbraColor, penumbraColor,
-                                SkToBool(fFlags & SkShadowFlags::kTransparentOccluder_ShadowFlag));
+        SkResourceCache::Find(*key, FindVisitor, &context);
+    }
+
+    sk_sp<TessPathData> data;
+    SkMatrix translateM;
+    bool addToCache = false;
+    if (context.fData) {
+        data = std::move(context.fData);
+        translateM.setTranslate(context.fTranslate);
+    } else {
+        addToCache = SkToBool(key);
+        translateM = SkMatrix::I();
+        SkScalar radius = fOccluderHeight * kHeightFactor * kGeomFactor;
+        SkScalar umbraAlpha = SkScalarInvert((1.0f+SkTMax(fOccluderHeight * kHeightFactor, 0.0f)));
+        // umbraColor is the interior value, penumbraColor the exterior value.
+        // umbraAlpha is the factor that is linearly interpolated from outside to inside, and
+        // then "blurred" by the ShadowEdgeFP. It is then multiplied by fAmbientAlpha to get
+        // the final alpha.
+        GrColor  umbraColor = GrColorPackRGBA(0, 0, fAmbientAlpha*255.9999f, umbraAlpha*255.9999f);
+        GrColor  penumbraColor = GrColorPackRGBA(0, 0, fAmbientAlpha*255.9999f, 0);
+
+        GrAmbientShadowTessellator tess(SkMatrix::I(), path, radius, umbraColor, penumbraColor,
+                                    SkToBool(fFlags & SkShadowFlags::kTransparentOccluder_ShadowFlag));
+        std::unique_ptr<SkPoint[]> positions(new SkPoint[tess.vertexCount()]);
+        std::unique_ptr<GrColor[]> colors(new GrColor[tess.vertexCount()]);
+        memcpy(positions.get(), tess.positions(), sizeof(SkPoint) * tess.vertexCount());
+        memcpy(colors.get(), tess.colors(), sizeof(GrColor) * tess.vertexCount());
+        std::unique_ptr<uint16_t[]> indices;
+        if (tess.indexCount()) {
+            indices.reset(new uint16_t[tess.indexCount()]);
+            memcpy(indices.get(), tess.indices(), sizeof(uint16_t) * tess.indexCount());
+        }
+        std::unique_ptr<const SkPoint[]> cp(std::move(positions));
+        data.reset(new TessPathData(std::move(positions), std::move(colors), std::move(indices), tess.vertexCount(), tess.indexCount()));
+    }
 
     sk_sp<ShadowEdgeFP> edgeFP(new ShadowEdgeFP);
     paint.addColorFragmentProcessor(edgeFP);
 
     rtContext->drawVertices(clip, std::move(paint), SkMatrix::I(), kTriangles_GrPrimitiveType,
-                            tess.vertexCount(), tess.positions(), nullptr,
-                            tess.colors(), tess.indices(), tess.indexCount());
+                            data->vertexCount(), data->positions(), nullptr,
+                            data->colors(), data->indices(), data->indexCount());
 
+    if (addToCache) {
+        SkResourceCache::Add(new TessPathRec(*key, viewMatrix, fOccluderHeight, fAmbientAlpha, fFlags, std::move(data)));
+    }
     return true;
 }
 
