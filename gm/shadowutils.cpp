53e917371b9895b1b2f8edb4902e044148d216a6
Brian Salomon <bsalomon@google.com>
2017-02-01 11:07:34 -0500

    Add a GM for SkShadowUtils and fix two issues.
    1) Transform the light x,y to local space before computing offset to path bounds center
    
    2) Modulate the shadow color by the color filter's output color.
    
    Change-Id: I480476df79b959f11c1eca0ba2a49a134d355cbb

---
+++ gm/shadowutils.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm.h"
+#include "SkCanvas.h"
+#include "SkPath.h"
+#include "SkShadowUtils.h"
+
+void draw_shadow(SkCanvas* canvas, const SkPath& path, int height, SkColor color, SkPoint3 lightPos,
+                 SkScalar lightR, bool isAmbient, uint32_t flags) {
+    SkScalar ambientAlpha = isAmbient ? .5f : 0.f;
+    SkScalar spotAlpha = isAmbient ? 0.f : .5f;
+    SkShadowUtils::DrawShadow(canvas, path, height, lightPos, lightR, ambientAlpha, spotAlpha,
+                              color, flags);
+}
+
+static constexpr int kW = 500;
+static constexpr int kH = 500;
+
+DEF_SIMPLE_GM(shadow_utils, canvas, kW, kH) {
+    SkTArray<SkPath> paths;
+    paths.push_back().addRoundRect(SkRect::MakeWH(50, 50), 10, 10);
+    SkRRect oddRRect;
+    oddRRect.setNinePatch(SkRect::MakeWH(50, 50), 9, 13, 6, 16);
+    paths.push_back().addRRect(oddRRect);
+    paths.push_back().addRect(SkRect::MakeWH(50, 50));
+    paths.push_back().addCircle(25, 25, 25);
+    paths.push_back().cubicTo(100, 50, 20, 100, 0, 0);
+
+    static constexpr SkScalar kPad = 15.f;
+    static constexpr SkPoint3 kLightPos = {kW / 2, kH / 2, 40};
+    static constexpr SkScalar kLightR = 100.f;
+    static constexpr SkScalar kHeight = 25.f;
+    canvas->translate(3 * kPad, 3 * kPad);
+    canvas->save();
+    SkScalar x = 0;
+    SkScalar dy = 0;
+    for (auto flags : {kNone_ShadowFlag, kTransparentOccluder_ShadowFlag}) {
+        for (const auto& path : paths) {
+            SkScalar w = path.getBounds().width() + kHeight;
+            SkScalar dx = w + kPad;
+            if (x + dx > kW - 3 * kPad) {
+                canvas->restore();
+                canvas->translate(0, dy);
+                canvas->save();
+                x = 0;
+                dy = 0;
+            }
+
+            draw_shadow(canvas, path, kHeight, SK_ColorRED, kLightPos, kLightR, true, flags);
+            draw_shadow(canvas, path, kHeight, SK_ColorBLUE, kLightPos, kLightR, false, flags);
+
+            // Draw the path outline in green on top of the ambient and spot shadows.
+            SkPaint paint;
+            paint.setColor(SK_ColorGREEN);
+            paint.setAntiAlias(true);
+            paint.setStyle(SkPaint::kStroke_Style);
+            paint.setStrokeWidth(0);
+            canvas->drawPath(path, paint);
+
+            canvas->translate(dx, 0);
+            x += dx;
+            dy = SkTMax(dy, path.getBounds().height() + kPad + kHeight);
+        }
+    }
+    // Show where the light is in x,y as a circle (specified in device space).
+    SkMatrix m = canvas->getTotalMatrix();
+    if (m.invert(&m)) {
+        canvas->save();
+        canvas->concat(m);
+        SkPaint paint;
+        paint.setColor(SK_ColorBLACK);
+        paint.setAntiAlias(true);
+        canvas->drawCircle(kLightPos.fX, kLightPos.fY, kLightR / 10.f, paint);
+        canvas->restore();
+    }
+}
