d689f7a5313a7fd501c2572c493fe6d91a88cba4
Hans Wennborg <hans@chromium.org>
2017-01-31 16:14:24 -0800

    GR_PROXY_MOVE: Work around Win/Clang eval order bug
    BUG=chromium:687259
    
    Change-Id: I145dac240a3c4f89cf1b6bf6ff54ba73cd110ebf
    Reviewed-on: https://skia-review.googlesource.com/7831
    Reviewed-by: Hans Wennborg <hwennborg@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/gpu/effects/GrProxyMove.h
+++ src/gpu/effects/GrProxyMove.h
@@ -11,6 +11,9 @@
 // In a few places below we rely on braced initialization order being defined by the C++ spec (left
 // to right). We use operator-> on a sk_sp and then in a later argument std::move() the sk_sp. GCC
 // 4.9.0 and earlier has a bug where the left to right order evaluation isn't implemented correctly.
+//
+// Clang has the same bug when targeting Windows (http://crbug.com/687259).
+// TODO(hans): Remove work-around once Clang is fixed.
 #if defined(__GNUC__) && !defined(__clang__)
 #   define GCC_VERSION (__GNUC__ * 10000  + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #   if (GCC_VERSION > 40900)
@@ -19,6 +22,8 @@
 #       define GCC_EVAL_ORDER_BUG 1
 #   endif
 #   undef GCC_VERSION
+#elif defined(_MSC_VER) && defined(__clang__)
+#   define GCC_EVAL_ORDER_BUG 1
 #else
 #   define GCC_EVAL_ORDER_BUG 0
 #endif
