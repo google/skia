cdf651c116bf8d964fe503049b4d0339d03e7b1d
Brian Osman <brianosman@google.com>
2017-02-02 13:32:56 -0500

    ImGui WIP imported from Rietveld
    BUG=skia:
    
    Change-Id: Iac2a63e37228d33141cb55b7e4d60bf11b7e9ae1

--- tools/viewer/Viewer.h
+++ tools/viewer/Viewer.h
@@ -27,6 +27,8 @@ public:
     void onIdle() override;
     bool onTouch(intptr_t owner, sk_app::Window::InputState state, float x, float y);
     void onUIStateChanged(const SkString& stateName, const SkString& stateValue);
+    bool onKey(sk_app::Window::Key key, sk_app::Window::InputState state, uint32_t modifiers);
+    bool onChar(SkUnichar c, uint32_t modifiers);
 
 private:
     void initSlides();
@@ -56,6 +58,9 @@ private:
 
     // whether to split the screen and draw two copies of the slide, one with sRGB and one without
     bool                   fSplitScreen;
+
+    SkBitmap               fImGuiFontBitmap;
+    bool                   fShowImGuiTestWindow;
 
     sk_app::Window::BackendType fBackendType;
 
