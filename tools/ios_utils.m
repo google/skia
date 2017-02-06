0752a34151e3cac8df5a87c15d5ae584853662f5
Mike Klein <mtklein@chromium.org>
2017-02-06 09:26:14 -0500

    iOS: cd into Documents folder at startup
    CQ_INCLUDE_TRYBOTS=skia.primary:Test-iOS-Clang-iPadMini4-GPU-GX6450-arm-Release
    
    Change-Id: I7beadad742bc9444491c7a315a827297a636d70d

---
+++ tools/ios_utils.m
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#import <Foundation/Foundation.h>
+#include "ios_utils.h"
+#include <unistd.h>
+
+void cd_Documents() {
+    @autoreleasepool {
+        NSURL* dir = [[[NSFileManager defaultManager]
+            URLsForDirectory:NSDocumentDirectory
+                   inDomains:NSUserDomainMask] lastObject];
+        chdir([dir.path UTF8String]);
+    }
+}
