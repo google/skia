e57430be3558a53d5fd20ed7dd3c9889dac9bb95
Kevin Lubick <kjlubick@google.com>
2017-02-08 15:42:23 -0500

    Reboot NexusPlayers with adb and shut them down if install fails
    BUG=skia:6045
    
    Change-Id: Ibd31e4cc44b71633377b05ad33c1bdab9024f78c

--- infra/bots/recipe_modules/run/api.py
+++ infra/bots/recipe_modules/run/api.py
@@ -47,6 +47,10 @@ class SkiaStepApi(recipe_api.RecipeApi):
       raise self.m.step.StepFailure('Failed build steps: %s' %
                                     ', '.join([f.name for f in self._failed]))
 
+  @property
+  def failed_steps(self):
+    return self._failed[:]
+
   def run_once(self, fn, *args, **kwargs):
     if not fn.__name__ in self._already_ran:
       self._already_ran[fn.__name__] = fn(*args, **kwargs)
