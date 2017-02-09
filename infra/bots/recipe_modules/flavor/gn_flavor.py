c3eeed3aa4aacfe539ee11de35b78a2391ea2f5e
Mike Klein <mtklein@chromium.org>
2017-02-09 15:22:53 -0500

    Add a -Mini build bot to build the smallest Skia possible.
    This bot wants everything possible to turn off turned off.
    
    CQ_INCLUDE_TRYBOTS=skia.primary:Build-Ubuntu-Clang-x86_64-Release-Mini
    
    Change-Id: Ida7ee2406f8fecda9bd408cd9880a3838590a81b

--- infra/bots/recipe_modules/flavor/gn_flavor.py
+++ infra/bots/recipe_modules/flavor/gn_flavor.py
@@ -77,6 +77,21 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
       args['skia_use_fontconfig'] = 'false'
     if extra_config == 'Mesa':
       args['skia_use_mesa'] = 'true'
+    if extra_config == 'Mini':
+      args.update({
+        'is_component_build':     'true',   # Proves we can link a coherent .so.
+        'is_official_build':      'true',   # No debug symbols.
+        'skia_enable_effects':    'false',
+        'skia_enable_gpu':        'false',
+        'skia_enable_pdf':        'false',
+        'skia_enable_splicer':    'false',
+        'skia_enable_tools':      'false',  # Some tools need GPU or effects.
+        'skia_use_expat':         'false',
+        'skia_use_libjpeg_turbo': 'false',
+        'skia_use_libpng':        'false',
+        'skia_use_libwebp':       'false',
+        'skia_use_zlib':          'false',
+      })
     if extra_config == 'NoGPU':
       args['skia_enable_gpu'] = 'false'
     if extra_config == 'Shared':
