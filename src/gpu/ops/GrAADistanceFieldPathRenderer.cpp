c5ec0a2cc39c06ee7c545f329b548d9b11e77a6d
Jim Van Verth <jvanverth@google.com>
2017-02-10 15:24:39 -0500

    Use SDF path miplevels based on the original path's size.
    Should produce sharper results than arbitrary fixed sizes.
    Adds a new test to pathfill GM.
    
    BUG=chromium:682918
    
    Change-Id: I5a394098665d01e995a244fde278236f1471e6c9

--- src/gpu/ops/GrAADistanceFieldPathRenderer.cpp
+++ src/gpu/ops/GrAADistanceFieldPathRenderer.cpp
@@ -10,6 +10,7 @@
 
 #include "GrBuffer.h"
 #include "GrContext.h"
+#include "GrDistanceFieldGenFromVector.h"
 #include "GrDrawOpTest.h"
 #include "GrOpFlushState.h"
 #include "GrPipelineBuilder.h"
@@ -20,10 +21,10 @@
 #include "effects/GrDistanceFieldGeoProc.h"
 #include "ops/GrMeshDrawOp.h"
 
-#include "SkPathOps.h"
 #include "SkAutoMalloc.h"
 #include "SkDistanceFieldGen.h"
-#include "GrDistanceFieldGenFromVector.h"
+#include "SkMathPriv.h"
+#include "SkPathOps.h"
 
 #define ATLAS_TEXTURE_WIDTH 2048
 #define ATLAS_TEXTURE_HEIGHT 2048
@@ -39,9 +40,12 @@ static int g_NumFreedShapes = 0;
 #endif
 
 // mip levels
-static const int kSmallMIP = 32;
-static const int kMediumMIP = 73;
-static const int kLargeMIP = 162;
+static const int kMinMIP = 16;
+static const int kMinMIPLog = 4;
+static const int kMaxMIP = 162;
+
+static const int kMaxDim = 73;
+static const int kMaxSize = 2*kMaxMIP;
 
 // Callback to clear out internal path cache when eviction occurs
 void GrAADistanceFieldPathRenderer::HandleEviction(GrDrawOpAtlas::AtlasID id, void* pr) {
@@ -107,14 +111,14 @@ bool GrAADistanceFieldPathRenderer::onCanDrawPath(const CanDrawPathArgs& args) c
         return false;
     }
 
-    // Only support paths with bounds within kMediumMIP by kMediumMIP,
-    // scaled to have bounds within 2.0f*kLargeMIP by 2.0f*kLargeMIP.
+    // Only support paths with bounds within kMaxDim by kMaxDim,
+    // scaled to have bounds within kMaxSize by kMaxSize.
     // The goal is to accelerate rendering of lots of small paths that may be scaling.
     SkScalar maxScale = args.fViewMatrix->getMaxScale();
     SkRect bounds = args.fShape->styledBounds();
     SkScalar maxDim = SkMaxScalar(bounds.width(), bounds.height());
 
-    return maxDim <= kMediumMIP && maxDim * maxScale <= 2.0f*kLargeMIP;
+    return maxDim <= kMaxDim && maxDim * maxScale <= kMaxSize;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -241,20 +245,28 @@ private:
             const SkRect& bounds = args.fShape.bounds();
             SkScalar maxDim = SkMaxScalar(bounds.width(), bounds.height());
             SkScalar size = maxScale * maxDim;
-            SkScalar desiredDimension;
-            // For minimizing (or the common case of identity) transforms, we try to
-            // create the DF at the appropriately sized native src-space path resolution.
+            // We try to create the DF at a power of two scaled path resolution (1/2, 1, 2, 4, etc)
             // In the majority of cases this will yield a crisper rendering.
-            if (size <= maxDim && maxDim < kSmallMIP) {
-                desiredDimension = maxDim;
-            } else if (size <= kSmallMIP) {
-                desiredDimension = kSmallMIP;
-            } else if (size <= maxDim) {
-                desiredDimension = maxDim;
-            } else if (size <= kMediumMIP) {
-                desiredDimension = kMediumMIP;
+            SkScalar mipScale = 1.0f;
+            // for small paths, scale up to a min size between kMinMIP and 2*kMinMIP
+            if (maxDim < kMinMIP) {
+                mipScale = SkIntToScalar(1 << (kMinMIPLog - SkPrevLog2(maxDim)));
+            // for larger paths, initial scale is half the original
+            // gives us a size between kMinMIP and kMaxDim/2 (or close to (kMinMIP, 2*kMinMIP])
+            } else if (maxDim > 2*kMinMIP) {
+                mipScale = 0.5f;
+            }
+            SkScalar smallMipSize = mipScale*maxDim;
+
+            SkScalar desiredDimension;
+            if (size <= smallMipSize) {
+                desiredDimension = smallMipSize;
+            } else if (size <= 2*smallMipSize) {
+                desiredDimension = 2*smallMipSize;
+            } else if (size <= 4*smallMipSize) {
+                desiredDimension = 4*smallMipSize;
             } else {
-                desiredDimension = kLargeMIP;
+                desiredDimension = kMaxMIP;
             }
 
             // check to see if path is cached
