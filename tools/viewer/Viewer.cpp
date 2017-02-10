126b759968fb32070be454a3a89757a6fab444c7
csmartdalton <csmartdalton@google.com>
2017-02-10 15:31:53 -0500

    Enable threaded SkTaskGroup in Viewer
    BUG=skia:
    
    Change-Id: I7f467c094fb0bcb983bd86d07cb9bd7be34666b3

--- tools/viewer/Viewer.cpp
+++ tools/viewer/Viewer.cpp
@@ -180,6 +180,7 @@ Viewer::Viewer(int argc, char** argv, void* platformData)
     , fZoomLevel(0.0f)
     , fZoomScale(SK_Scalar1)
 {
+    static SkTaskGroup::Enabler kTaskGroupEnabler;
     SkGraphics::Init();
     memset(fPaintTimes, 0, sizeof(fPaintTimes));
     memset(fFlushTimes, 0, sizeof(fFlushTimes));
