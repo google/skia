c5add4860a63d49f8ae6e221669d1cebef1ff308
Dominic Mazzoni <dmazzoni@chromium.org>
2017-02-13 08:15:34 -0800

    Implement SkHighContrastFilter
    This is a color filter to apply several contrast adjustments for users
    with low vision, including inverting the colors (in either RGB or HSL
    space), applying gamma correction, converting to grayscale, and increasing
    the contrast.
    
    BUG=skia:6235
    
    CQ_INCLUDE_TRYBOTS=skia.primary:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD
    
    Change-Id: Icb8f3e290932d8bcd9387fb1f39dd20767e15cf6

---
+++ include/effects/SkHighContrastFilter.h
@@ -0,0 +1,69 @@
+/*
+* Copyright 2017 Google Inc.
+*
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file.
+*/
+
+#ifndef SkHighContrastFilter_DEFINED
+#define SkHighContrastFilter_DEFINED
+
+#include "SkColorFilter.h"
+#include "SkPaint.h"
+
+enum class HighContrastInvertStyle {
+    kNoInvert,
+    kInvertBrightness,
+    kInvertLightness,
+};
+
+/**
+ *  Configuration struct for SkHighContrastFilter.
+ *
+ *  Provides transformations to improve contrast for users with low vision.
+ */
+struct SkHighContrastConfig {
+    SkHighContrastConfig() {
+        fGrayscale = false;
+        fInvertStyle = HighContrastInvertStyle::kInvertLightness;
+        fContrast = 0.0f;
+    }
+
+    SkHighContrastConfig(bool grayscale,
+                         HighContrastInvertStyle invertStyle,
+                         SkScalar contrast)
+        : fGrayscale(grayscale),
+          fInvertStyle(invertStyle),
+          fContrast(contrast) {}
+
+    // If true, the color will be converted to grayscale.
+    bool fGrayscale;
+
+    // Whether to invert brightness, lightness, or neither.
+    HighContrastInvertStyle fInvertStyle;
+
+    // After grayscale and inverting, the contrast can be adjusted linearly.
+    // The valid range is -1.0 through 1.0, where 0.0 is no adjustment.
+    SkScalar  fContrast;
+};
+
+/**
+ *  Color filter that provides transformations to improve contrast
+ *  for users with low vision.
+ *
+ *  Applies the following transformations in this order. Each of these
+ *  can be configured using SkHighContrastConfig.
+ *
+ *    - Conversion to grayscale
+ *    - Color inversion (either in RGB or HSL space)
+ *    - Increasing the resulting contrast.
+ *
+ */
+class SK_API SkHighContrastFilter : public SkColorFilter {
+public:
+    static sk_sp<SkColorFilter> Make(const SkHighContrastConfig& config);
+
+    SK_DECLARE_FLATTENABLE_REGISTRAR_GROUP()
+};
+
+#endif
