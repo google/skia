4cced4aca43fa89213e8cb3af8920057c6df8560
Matt Sarett <msarett@google.com>
2017-02-14 13:50:43 -0500

    Picture backed images must have a bit depth and color space
    Enforce that picture backed images created by the public API
    must have a non-null SkColorSpace.
    
    SkPictureShader uses a private call to get around this restriction.
    
    BUG=skia:
    
    Change-Id: I2fc11a8ffe583035d09e83abf40b827fbf575321

---
+++ src/core/SkPictureImageGenerator.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkImageGenerator.h"
+#include "SkTLazy.h"
+
+class SkPictureImageGenerator : SkImageGenerator {
+public:
+    static SkImageGenerator* Create(const SkISize&, const SkPicture*, const SkMatrix*,
+                                    const SkPaint*, SkImage::BitDepth, sk_sp<SkColorSpace>);
+
+protected:
+    bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes, SkPMColor ctable[],
+                     int* ctableCount) override;
+    bool onComputeScaledDimensions(SkScalar scale, SupportedSizes*) override;
+    bool onGenerateScaledPixels(const SkPixmap&) override;
+
+#if SK_SUPPORT_GPU
+    GrTexture* onGenerateTexture(GrContext*, const SkImageInfo&, const SkIPoint&) override;
+#endif
+
+private:
+    SkPictureImageGenerator(const SkImageInfo& info, const SkPicture*, const SkMatrix*,
+                            const SkPaint*);
+
+    sk_sp<const SkPicture> fPicture;
+    SkMatrix               fMatrix;
+    SkTLazy<SkPaint>       fPaint;
+
+    typedef SkImageGenerator INHERITED;
+};
