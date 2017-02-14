314842fab46515eb74700d7cdf433e2ef649007c
Matt Sarett <msarett@google.com>
2017-02-14 18:19:26 -0500

    Report wide gamut images from SkAndroidCodec
    The client wants to be able to tag bitmaps as "wide gamut"
    which will initially just mean P3.
    
    Unfortunately, we can't tag the Skia bitmap with a color
    space because that will have the additional consequence
    of enabling linear blending when drawing to that bitmap.
    
    So the client wrapper bitmap class can have a bool that
    indicates if they are wide gamut.  We will need to set
    this bool properly on outputs from BF and BRD, so it
    makes sense to share the logic here.
    
    BUG=skia:
    
    Change-Id: Ifc34369d96aa9dd92ae2af72aac1cfa17fdc4b94

--- include/codec/SkAndroidCodec.h
+++ include/codec/SkAndroidCodec.h
@@ -79,9 +79,6 @@ public:
      *  @param outputColorType Color type that the client will decode to
      *
      *  Returns the appropriate color space to decode to.
-     *
-     *  For now, this just returns a default.  This could be updated to take
-     *  requests for wide gamut modes or specific output spaces.
      */
     sk_sp<SkColorSpace> computeOutputColorSpace(SkColorType outputColorType);
 
@@ -272,8 +269,8 @@ private:
 
     // This will always be a reference to the info that is contained by the
     // embedded SkCodec.
-    const SkImageInfo& fInfo;
-
+    const SkImageInfo&       fInfo;
     std::unique_ptr<SkCodec> fCodec;
+    const bool               fIsWideGamut;
 };
 #endif // SkAndroidCodec_DEFINED
