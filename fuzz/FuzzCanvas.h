24ac42b373d9202a89538a1873df9275870c7632
Hal Canary <halcanary@google.com>
2017-02-14 13:35:14 -0500

    Fuzz PDF, N32, and Null Canvases
    run `fuzz --type pdf_canvas` or `fuzz --type null_canvas` or
    `fuzz --type n32_canvas`
    
    Change-Id: Id70179d5578ed1e67006aef7823bf75fc1d7a4a6
    Reviewed-on: https://skia-review.googlesource.com/8418
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Hal Canary <halcanary@google.com>

---
+++ fuzz/FuzzCanvas.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef FuzzCanvas_DEFINED
+#define FuzzCanvas_DEFINED
+
+class Fuzz;
+class SkCanvas;
+
+void FuzzCanvas(Fuzz*, SkCanvas*);
+
+#endif  // FuzzCanvas_DEFINED
