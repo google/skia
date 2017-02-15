d00ea19b86ddf683cc32e7d854dcf9a0bbc82af5
Matt Sarett <msarett@google.com>
2017-02-15 15:51:29 -0500

    Use non-linear blending flag in raster pipeline blitter
    BUG=skia:
    
    Change-Id: I00b52e6de3b46b7d26c1df2aa63c521b6c7ece2d

--- src/core/SkCoreBlitters.h
+++ src/core/SkCoreBlitters.h
@@ -204,6 +204,6 @@ SkBlitter* SkBlitter_ChooseD565(const SkPixmap& device, const SkPaint& paint,
 
 // Returns nullptr if no SkRasterPipeline blitter can be constructed for this paint.
 SkBlitter* SkCreateRasterPipelineBlitter(const SkPixmap&, const SkPaint&, const SkMatrix& ctm,
-                                         SkArenaAlloc*, bool blendCorrectly);
+                                         SkArenaAlloc*);
 
 #endif
