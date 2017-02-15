2ae3a2ec299b9b529496bbd06b8276263c8df9b0
Matt Sarett <msarett@google.com>
2017-02-15 08:48:02 -0500

    Suggest P3 for wide gamut images in SkAndroidCodec
    This will prevent us from clipping the gamut
    to sRGB.
    
    BUG=skia:
    
    Change-Id: Ifc34369d96aa9dd92ae2af72aac1cfa17fdc4b94
    Reviewed-on: https://skia-review.googlesource.com/8025
    Commit-Queue: Matt Sarett <msarett@google.com>
    Reviewed-by: Leon Scroggins <scroggo@google.com>

--- include/codec/SkAndroidCodec.h
+++ include/codec/SkAndroidCodec.h
@@ -79,9 +79,6 @@ public:
      *  @param outputColorType Color type that the client will decode to
      *
      *  Returns the appropriate color space to decode to.
-     *
-     *  For now, this just returns a default.  This could be updated to take
-     *  requests for wide gamut modes or specific output spaces.
      */
     sk_sp<SkColorSpace> computeOutputColorSpace(SkColorType outputColorType);
 
