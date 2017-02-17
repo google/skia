deda10198a95b330e9cfc390bb149248db5473b7
Mike Klein <mtklein@chromium.org>
2017-02-17 09:41:09 -0500

    SkJumper: start on asm
    Will follow up with Linux, then Android aarch64 and armv7, then iOS, then Windows.
    
    Change-Id: Ifcf1edabdfe5df0a91bd089f09523aba95cdf5ef

--- src/jumper/SkJumper.cpp
+++ src/jumper/SkJumper.cpp
@@ -7,7 +7,6 @@
 
 #include "SkCpu.h"
 #include "SkJumper.h"
-#include "SkJumper_generated.h"
 #include "SkRasterPipeline.h"
 #include "SkTemplates.h"
 
@@ -50,20 +49,45 @@ static const SkJumper_constants kConstants = {
     M(clamp_y)        \
     M(linear_gradient_2stops)
 
-// Declare the portable, single pixel stages that are linked into Skia from SkJumper_stages.o.
 extern "C" {
+    // Declare the portable, single pixel stages that are linked into Skia from SkJumper_stages.o.
     void sk_start_pipeline(size_t, void**, const SkJumper_constants*);
 
     // We use void() as a convenient stand-in for the real stage function type.
     // We never call these directly, so we don't really need to know their real types.
     void sk_just_return(void);
-#define M(st) void sk_##st(void);
-    STAGES(M)
-#undef M
+    #define M(st) void sk_##st(void);
+        STAGES(M)
+    #undef M
+
+    // Same deal for platform-specific stages from SkJumper_generated_*.o.
+#if defined(__x86_64__) || defined(_M_X64)
+    void sk_start_pipeline_sse2 (size_t, void**, const SkJumper_constants*);
+    void sk_start_pipeline_sse41(size_t, void**, const SkJumper_constants*);
+    void sk_start_pipeline_hsw  (size_t, void**, const SkJumper_constants*);
+
+    void sk_start_pipeline_ms_sse2 (size_t, void**, const SkJumper_constants*);
+    void sk_start_pipeline_ms_sse41(size_t, void**, const SkJumper_constants*);
+    void sk_start_pipeline_ms_hsw  (size_t, void**, const SkJumper_constants*);
+
+    void sk_just_return_sse2 (void);
+    void sk_just_return_sse41(void);
+    void sk_just_return_hsw  (void);
+
+    #define M(st) void sk_##st##_sse2(void);
+        STAGES(M)
+    #undef M
+    #define M(st) void sk_##st##_sse41(void);
+        STAGES(M)
+    #undef M
+    #define M(st) void sk_##st##_hsw(void);
+        STAGES(M)
+    #undef M
+#endif
 }
 
-// Translate SkRasterPipeline's enum to pointers to our portable, single pixel stages.
-static void* portable_lookup(SkRasterPipeline::StockStage st) {
+// Translate SkRasterPipeline's enum to stage pointers.
+static void* lookup_portable(SkRasterPipeline::StockStage st) {
     switch (st) {
         default: return nullptr;
     #define M(st) case SkRasterPipeline::st: return (void*)sk_##st;
@@ -72,46 +96,27 @@ static void* portable_lookup(SkRasterPipeline::StockStage st) {
     }
 }
 
-// The non-portable options are pre-compiled static data arrays pulled in from SkJumper_generated.h.
-#if defined(__aarch64__)
-    static void* aarch64_lookup(SkRasterPipeline::StockStage st) {
+#if defined(__x86_64__) || defined(_M_X64)
+    static void* lookup_sse2(SkRasterPipeline::StockStage st) {
         switch (st) {
             default: return nullptr;
-        #define M(st) case SkRasterPipeline::st: return (void*)aarch64_sk_##st;
+        #define M(st) case SkRasterPipeline::st: return (void*)sk_##st##_sse2;
             STAGES(M)
         #undef M
         }
     }
-#elif defined(__ARM_NEON__)
-    static void* armv7_lookup(SkRasterPipeline::StockStage st) {
+    static void* lookup_sse41(SkRasterPipeline::StockStage st) {
         switch (st) {
             default: return nullptr;
-        #define M(st) case SkRasterPipeline::st: return (void*)armv7_sk_##st;
+        #define M(st) case SkRasterPipeline::st: return (void*)sk_##st##_sse41;
             STAGES(M)
         #undef M
         }
     }
-#elif defined(__x86_64__) || defined(_M_X64)
-    static void* sse2_lookup(SkRasterPipeline::StockStage st) {
+    static void* lookup_hsw(SkRasterPipeline::StockStage st) {
         switch (st) {
             default: return nullptr;
-        #define M(st) case SkRasterPipeline::st: return (void*)sse2_sk_##st;
-            STAGES(M)
-        #undef M
-        }
-    }
-    static void* sse41_lookup(SkRasterPipeline::StockStage st) {
-        switch (st) {
-            default: return nullptr;
-        #define M(st) case SkRasterPipeline::st: return (void*)sse41_sk_##st;
-            STAGES(M)
-        #undef M
-        }
-    }
-    static void* hsw_lookup(SkRasterPipeline::StockStage st) {
-        switch (st) {
-            default: return nullptr;
-        #define M(st) case SkRasterPipeline::st: return (void*)hsw_sk_##st;
+        #define M(st) case SkRasterPipeline::st: return (void*)sk_##st##_hsw;
             STAGES(M)
         #undef M
         }
@@ -120,62 +125,40 @@ static void* portable_lookup(SkRasterPipeline::StockStage st) {
 
 bool SkRasterPipeline::run_with_jumper(size_t x, size_t n) const {
     // We'll look for the best vector instruction set and stride we can use.
-    size_t stride                                 = 0;
-    void* (*lookup)(SkRasterPipeline::StockStage) = nullptr;
-    void* start_pipeline                          = nullptr;
-    void* just_return                             = nullptr;
+    size_t stride                                                     = 0;
+    void* (*lookup)(SkRasterPipeline::StockStage)                     = nullptr;
+    void (*start_pipeline)(size_t, void**, const SkJumper_constants*) = nullptr;
+    void (*just_return)(void)                                         = nullptr;
 
-#if defined(__aarch64__)
+#if defined(__x86_64__) || defined(_M_X64)
     stride         = 4;
-    lookup         = aarch64_lookup;
-    start_pipeline = (void*)aarch64_sk_start_pipeline;
-    just_return    = (void*)aarch64_sk_just_return;
-
-#elif defined(__ARM_NEON__)
-    if (SkCpu::Supports(SkCpu::NEON|SkCpu::NEON_FMA|SkCpu::VFP_FP16)) {
-        stride         = 2;
-        lookup         = armv7_lookup;
-        start_pipeline = (void*)armv7_sk_start_pipeline;
-        just_return    = (void*)armv7_sk_just_return;
-    }
-
-#elif defined(__x86_64__) || defined(_M_X64)
-    stride         = 4;
-    lookup         = sse2_lookup;
-    start_pipeline = (void*)sse2_sk_start_pipeline;
-    just_return    = (void*)sse2_sk_just_return;
+    lookup         = lookup_sse2;
+    start_pipeline = sk_start_pipeline_sse2;
+    just_return    = sk_just_return_sse2;
     if (SkCpu::Supports(SkCpu::SSE41)) {
         stride         = 4;
-        lookup         = sse41_lookup;
-        start_pipeline = (void*)sse41_sk_start_pipeline;
-        just_return    = (void*)sse41_sk_just_return;
+        lookup         = lookup_sse41;
+        start_pipeline = sk_start_pipeline_sse41;
+        just_return    = sk_just_return_sse41;
     }
     if (SkCpu::Supports(SkCpu::HSW)) {
         stride         = 8;
-        lookup         = hsw_lookup;
-        start_pipeline = (void*)hsw_sk_start_pipeline;
-        just_return    = (void*)hsw_sk_just_return;
+        lookup         = lookup_hsw;
+        start_pipeline = sk_start_pipeline_hsw;
+        just_return    = sk_just_return_hsw;
     }
 #endif
 
 #if defined(_MSC_VER)
-    if (start_pipeline == (void*)sse2_sk_start_pipeline) {
-        start_pipeline =  (void*)sse2_sk_start_pipeline_ms;
-    }
-    if (start_pipeline == (void*)sse41_sk_start_pipeline) {
-        start_pipeline =  (void*)sse41_sk_start_pipeline_ms;
-    }
-    if (start_pipeline == (void*)hsw_sk_start_pipeline) {
-        start_pipeline =  (void*)hsw_sk_start_pipeline_ms;
-    }
+    if (start_pipeline == sk_start_pipeline_sse2)  { start_pipeline = sk_start_pipeline_ms_sse2; }
+    if (start_pipeline == sk_start_pipeline_sse41) { start_pipeline = sk_start_pipeline_ms_sse41; }
+    if (start_pipeline == sk_start_pipeline_hsw)   { start_pipeline = sk_start_pipeline_ms_hsw; }
 #endif
 
     SkAutoSTMalloc<64, void*> program(2*fStages.size() + 1);
-
-    // If possible, build and run a program to run at full vector stride.
     const size_t limit = x+n;
 
-    if (stride) {
+    auto build_and_run = [&] {
         void** ip = program.get();
         for (auto&& st : fStages) {
             auto fn = lookup(st.stage);
@@ -187,32 +170,27 @@ bool SkRasterPipeline::run_with_jumper(size_t x, size_t n) const {
         }
         *ip = (void*)just_return;
 
-        auto start = (decltype(&sk_start_pipeline))start_pipeline;
         while (x + stride <= limit) {
-            start(x, program.get(), &kConstants);
+            start_pipeline(x, program.get(), &kConstants);
             x += stride;
+        }
+        return true;
+    };
+
+    // If possible, build and run a program to run at full vector stride.
+    if (stride) {
+        if (!build_and_run()) {
+            return false;
         }
     }
-
-    // If there's any leftover, build and run stride=1 portable code.
+    // Finish up any leftover with portable code one pixel at a time.
     if (x < limit) {
-        stride = 1;
-
-        void** ip = program.get();
-        for (auto&& st : fStages) {
-            auto fn = portable_lookup(st.stage);
-            if (!fn) {
-                return false;
-            }
-            *ip++ = fn;
-            *ip++ = st.ctx;
-        }
-        *ip = (void*)sk_just_return;
-
-        auto start = sk_start_pipeline;
-        while (x + stride <= limit) {
-            start(x, program.get(), &kConstants);
-            x += stride;
+        stride         = 1;
+        lookup         = lookup_portable;
+        start_pipeline = sk_start_pipeline;
+        just_return    = sk_just_return;
+        if (!build_and_run()) {
+            return false;
         }
     }
 
