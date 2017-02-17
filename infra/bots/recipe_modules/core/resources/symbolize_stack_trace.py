3d9567cdd8846b3d90c0919f80e78f90d88d240f
Kevin Lubick <kjlubick@google.com>
2017-02-17 14:30:57 -0500

    Stream logs in symbolized dm/nanobench
    BUG=skia:
    
    Change-Id: I693bec4daa819f3c7802aa54f452b47f228b370b

--- infra/bots/recipe_modules/core/resources/symbolize_stack_trace.py
+++ infra/bots/recipe_modules/core/resources/symbolize_stack_trace.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 # pylint: disable=line-too-long
 
+import collections
 import os
 import re
 import subprocess
@@ -28,10 +29,15 @@ import sys
 # about the logs of previous steps without using a wrapper like this.
 
 def main(basedir, cmd):
-  logs = ""
+  logs = collections.deque(maxlen=200)
   retcode = 0
   try:
-    logs = subprocess.check_output(cmd, stderr=subprocess.STDOUT).strip()
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    for line in iter(proc.stdout.readline, ''):
+      sys.stdout.write(line)
+      logs.append(line)
+
   except subprocess.CalledProcessError as e:
     retcode = e.returncode
     logs = e.output
@@ -51,11 +57,18 @@ def main(basedir, cmd):
   # The extra_path strips off the not-useful prefix and leaves just the
   # important src/gpu/Frobulator.cpp:13 bit.
   extra_path = r'/.*\.\./'
-  for line in logs.splitlines():
+  is_first = True
+  for line in logs:
     line = line.strip()
 
     m = re.search(stack_line, line)
     if m:
+      if is_first:
+        print '#######################################'
+        print 'symbolized stacktrace follows'
+        print '#######################################'
+        is_first = False
+
       path = m.group('path')
       addr = m.group('addr')
       addr2 = m.group('addr2')
@@ -71,8 +84,7 @@ def main(basedir, cmd):
             path = path[len(basedir)+1:]
           sym = re.sub(extra_path, '', sym)
           line = path + ' ' + sym
-
-    print line
+      print line
 
   sys.exit(retcode)
 
