2028625f9b92b3423d841a926f785140a8955a84
Mike Klein <mtklein@chromium.org>
2017-02-17 12:48:29 +0000

    Revert "Take missed opportunity: memset for A8."
    This reverts commit 9cee33e631679fc8195e7892b5aa5fdffb0b568e.
    
    Reason for revert: Chrome roll?
    
    Original change's description:
    > Take missed opportunity: memset for A8.
    > 
    > Just recently noticed we weren't doing this.
    > It seems to be triggering all the time.
    > 
    > Draws the same of course.
    > 
    > Change-Id: Ie3d869a05194a5d52d635643eba1c96c8a21ce67
    > Reviewed-on: https://skia-review.googlesource.com/8471
    > Reviewed-by: Mike Reed <reed@google.com>
    > Commit-Queue: Mike Klein <mtklein@chromium.org>
    > 
    
    TBR=mtklein@chromium.org,msarett@google.com,reed@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I26a3f2b1a9ae9093180e9c5cc61094b239377c17
    Reviewed-on: https://skia-review.googlesource.com/8606
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Mike Klein <mtklein@chromium.org>

--- src/core/SkRasterPipelineBlitter.cpp
+++ src/core/SkRasterPipelineBlitter.cpp
@@ -224,10 +224,16 @@ void SkRasterPipelineBlitter::blitH(int x, int y, int w) {
 
     if (fCanMemsetInBlitH) {
         switch (fDst.shiftPerPixel()) {
-            case 0:    memset  ((uint8_t *)fDstPtr + x, fMemsetColor, w); return;
-            case 1: sk_memset16((uint16_t*)fDstPtr + x, fMemsetColor, w); return;
-            case 2: sk_memset32((uint32_t*)fDstPtr + x, fMemsetColor, w); return;
-            case 3: sk_memset64((uint64_t*)fDstPtr + x, fMemsetColor, w); return;
+            // TODO: case 0: memset (for A8)
+            case 1:
+                sk_memset16((uint16_t*)fDstPtr + x, fMemsetColor, w);
+                return;
+            case 2:
+                sk_memset32((uint32_t*)fDstPtr + x, fMemsetColor, w);
+                return;
+            case 3:
+                sk_memset64((uint64_t*)fDstPtr + x, fMemsetColor, w);
+                return;
             default: break;
         }
     }
