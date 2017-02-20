9da3e4a481fd5aacaeffe13b123a5f848345003b
Kevin Lubick <kjlubick@google.com>
2017-02-20 09:14:23 -0500

    Blacklisting scaled_tilemodes_npot on GalaxyTab3
    It now fails after a few minutes instead of timing 
    out after 4 hours, so that's an improvement.
    
    BUG=skia:
    
    Change-Id: I3e9319711953a5dad7898eea507cd4d83db7842d

--- infra/bots/recipe_modules/sktest/api.py
+++ infra/bots/recipe_modules/sktest/api.py
@@ -389,6 +389,10 @@ def dm_flags(bot):
       blacklist([config, 'gm', '_', 'multipicturedraw_rectclip_simple'])
       blacklist([config, 'gm', '_', 'multipicturedraw_rrectclip_simple'])
 
+  if 'Vivante' in bot:
+    # This is an older device. I don't know why these break it.
+    blacklist(['gpu',     'gm', '_', 'scaled_tilemodes_npot'])
+
   if blacklisted:
     args.append('--blacklist')
     args.extend(blacklisted)
