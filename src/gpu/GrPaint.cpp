94cce4cb2cf40b5ed3a1928c02618c1a98b1e0fa
Brian Salomon <bsalomon@google.com>
2017-02-21 16:32:20 -0500

    Make GrXferProcessor and related classes private.
    Change-Id: I81ea6f5ea5c8b7b23848ef24524a7e48e531efe8
    Reviewed-on: https://skia-review.googlesource.com/8819
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/gpu/GrPaint.cpp
+++ src/gpu/GrPaint.cpp
@@ -6,11 +6,15 @@
  */
 
 #include "GrPaint.h"
-
 #include "GrProcOptInfo.h"
+#include "GrXferProcessor.h"
 #include "effects/GrCoverageSetOpXP.h"
 #include "effects/GrPorterDuffXferProcessor.h"
 #include "effects/GrSimpleTextureEffect.h"
+
+void GrPaint::setPorterDuffXPFactory(SkBlendMode mode) {
+    fXPFactory = GrPorterDuffXPFactory::Get(mode);
+}
 
 void GrPaint::setCoverageSetOpXPFactory(SkRegion::Op regionOp, bool invertCoverage) {
     fXPFactory = GrCoverageSetOpXPFactory::Get(regionOp, invertCoverage);
@@ -73,4 +77,23 @@ void GrPaint::addCoverageTextureProcessor(GrContext* ctx, sk_sp<GrTextureProxy> 
                                           const GrSamplerParams& params) {
     this->addCoverageFragmentProcessor(GrSimpleTextureEffect::Make(ctx, std::move(proxy),
                                                                    nullptr, matrix, params));
+}
+
+bool GrPaint::isConstantBlendedColor(GrColor* constantColor) const {
+    // This used to do a more sophisticated analysis but now it just explicitly looks for common
+    // cases.
+    static const GrXPFactory* kSrc = GrPorterDuffXPFactory::Get(SkBlendMode::kSrc);
+    static const GrXPFactory* kClear = GrPorterDuffXPFactory::Get(SkBlendMode::kClear);
+    if (kClear == fXPFactory) {
+        *constantColor = GrColor_TRANSPARENT_BLACK;
+        return true;
+    }
+    if (this->numColorFragmentProcessors()) {
+        return false;
+    }
+    if (kSrc == fXPFactory || (!fXPFactory && fColor.isOpaque())) {
+        *constantColor = fColor.toGrColor();
+        return true;
+    }
+    return false;
 }
