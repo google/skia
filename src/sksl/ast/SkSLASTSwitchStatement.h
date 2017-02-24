0465404c76e17d18fa6d8427a4470d08fc0afa25
Ethan Nicholas <ethannicholas@google.com>
2017-02-24 14:19:56 -0500

    Revert "Revert "skslc switch support"
    This reverts commit 7d975fc200bbbea991ec4c04c08f3a5ea7b847af.
    
    BUG=skia:
    
    Change-Id: I9856f73ac5c877337d9791fc976b7b0cd5187ff5

---
+++ src/sksl/ast/SkSLASTSwitchStatement.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_ASTSWITCHSTATEMENT
+#define SKSL_ASTSWITCHSTATEMENT
+
+#include "SkSLASTStatement.h"
+#include "SkSLASTSwitchCase.h"
+
+namespace SkSL {
+
+/**
+ * A 'switch' statement.
+ */
+struct ASTSwitchStatement : public ASTStatement {
+    ASTSwitchStatement(Position position, std::unique_ptr<ASTExpression> value,
+                       std::vector<std::unique_ptr<ASTSwitchCase>> cases)
+    : INHERITED(position, kSwitch_Kind)
+    , fValue(std::move(value))
+    , fCases(std::move(cases)) {}
+
+    SkString description() const override {
+        SkString result = SkStringPrintf("switch (%s) {\n", + fValue->description().c_str());
+        for (const auto& c : fCases) {
+            result += c->description();
+        }
+        result += "}";
+        return result;
+    }
+
+    const std::unique_ptr<ASTExpression> fValue;
+    const std::vector<std::unique_ptr<ASTSwitchCase>> fCases;
+
+    typedef ASTStatement INHERITED;
+};
+
+} // namespace
+
+#endif
