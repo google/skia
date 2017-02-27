656228ada508f4f1ee864433cef56e0188a6ecf6
Robert Phillips <robertphillips@google.com>
2017-02-27 12:45:50 -0500

    Add per-Flush callback to GrDrawingManager
    This will allow internal systems (e.g., fonts & path renderers) to create per-flush atlases.
    
    Depends on: https://skia-review.googlesource.com/c/8988/ (Allow GrSurfaceProxy-derived classes to use flags when instantiating)
    
    Change-Id: I307796595d651cf376838bff1f9e4385c3753547

--- src/gpu/GrTextureRenderTargetProxy.cpp
+++ src/gpu/GrTextureRenderTargetProxy.cpp
@@ -8,19 +8,21 @@
 #include "GrTextureRenderTargetProxy.h"
 
 // Deferred version
-// This class is virtually derived from GrSurfaceProxy (via both GrTextureProxy and 
+// This class is virtually derived from GrSurfaceProxy (via both GrTextureProxy and
 // GrRenderTargetProxy) so its constructor must be explicitly called.
 GrTextureRenderTargetProxy::GrTextureRenderTargetProxy(const GrCaps& caps,
                                                        const GrSurfaceDesc& desc,
                                                        SkBackingFit fit,
-                                                       SkBudgeted budgeted)
-    : GrSurfaceProxy(desc, fit, budgeted)
-    , GrTextureProxy(desc, fit, budgeted, nullptr, 0) // 4 now textures w/ data are always wrapped
-    , GrRenderTargetProxy(caps, desc, fit, budgeted) {
+                                                       SkBudgeted budgeted,
+                                                       uint32_t flags)
+    : GrSurfaceProxy(desc, fit, budgeted, flags)
+    // for now textures w/ data are always wrapped
+    , GrTextureProxy(desc, fit, budgeted, nullptr, 0, flags) 
+    , GrRenderTargetProxy(caps, desc, fit, budgeted, flags) {
 }
 
 // Wrapped version
-// This class is virtually derived from GrSurfaceProxy (via both GrTextureProxy and 
+// This class is virtually derived from GrSurfaceProxy (via both GrTextureProxy and
 // GrRenderTargetProxy) so its constructor must be explicitly called.
 GrTextureRenderTargetProxy::GrTextureRenderTargetProxy(sk_sp<GrSurface> surf)
     : GrSurfaceProxy(surf, SkBackingFit::kExact)
