bfe15f61a277a50b05703307f56e184cc00b1aa7
Ethan Nicholas <ethannicholas@google.com>
2017-03-01 11:46:51 -0500

    changed skslc layout parsing strategy
    Change-Id: I322f0682a8753beb858012b00ee7e4ec6b206c2f
    Reviewed-on: https://skia-review.googlesource.com/9081
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ src/sksl/lex.layout.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "disable_flex_warnings.h"
+#include "lex.layout.c"
