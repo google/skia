5151adaee202a3b83588c3a5c4701006cc8e392a
Brian Osman <brianosman@google.com>
2017-03-02 13:48:04 -0500

    Use GrSemaphore rather than GrFence for external texture data
    BUG=skia:
    
    Change-Id: I0d23eb9dcf5c01c71d3571ef97690af68b900807

--- src/gpu/GrSemaphore.h
+++ src/gpu/GrSemaphore.h
@@ -13,15 +13,16 @@
 class GrGpu;
 
 class GrSemaphore : public SkRefCnt {
-public:
+protected:
+    explicit GrSemaphore(const GrGpu* gpu) : fGpu(gpu) {}
+
     // This function should only be used in the case of exporting and importing a GrSemaphore object
     // from one GrContext to another. When exporting, the GrSemaphore should be set to a null GrGpu,
     // and when importing it should be set to the GrGpu of the current context. Once exported, a
     // GrSemaphore should not be used with its old context.
     void resetGpu(const GrGpu* gpu) { fGpu = gpu; }
 
-protected:
-    explicit GrSemaphore(const GrGpu* gpu) : fGpu(gpu) {}
+    friend class GrGLExternalTextureData; // resetGpu
 
     const GrGpu* fGpu;
 };
