4ab2548f04f73568e0ee9cacaaae50935a287aa6
Mike Klein <mtklein@chromium.org>
2017-03-03 09:21:30 -0500

    Strengthen is_official_build, update docs.
    This makes is_official_build turn off all development targets and
    features in Skia, including building third-party dependencies from
    source.
    
    This will intentionally break some external users, who will find
    themselves no longer able to find third-party headers or link against
    third-party libraries.  These users have been building with our testing
    third-party dependencies unknowingly.  They'll need to either explicitly
    turn back on building each dependency from source
    (skia_use_system_foo=false) or disable that dependency entirely
    (skia_use_foo=false).
    
    is_skia_standalone is now basically !is_official_build, so I've
    propagated that through, removing is_skia_standalone.  In a few places
    we were using it as a stand-in for defined(ndk), so I've just written
    defined(ndk) there.  Duh.
    
    gn_to_bp:
    
        is_offical_build's new strength also makes gn_to_bp.py simpler to
        write.  In spirit, Android builds are official Skia builds that also
        build DM and nanobench.
    
        It seems that SkJumper (src/jumper/*) is (unintentionally) enabled
        on Android.  Switching to an is_official_build would have disabled
        that.  But as that accidental launch seems to have gone fine, I've
        kept it explicitly enabled.
    
        In the end, no changes to Android.bp or its SkUserConfig.h.
    
    The -Mini builder no longer needs to explicitly disable tools.
    CQ_INCLUDE_TRYBOTS=skia.primary:Build-Ubuntu-Clang-x86_64-Release-Mini
    
    Change-Id: Id06e53268a5caf55c6046ada354a0863c3031c73

--- infra/bots/recipe_modules/flavor/gn_flavor.py
+++ infra/bots/recipe_modules/flavor/gn_flavor.py
@@ -80,11 +80,10 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
     if extra_config == 'Mini':
       args.update({
         'is_component_build':     'true',   # Proves we can link a coherent .so.
-        'is_official_build':      'true',   # No debug symbols.
+        'is_official_build':      'true',   # No debug symbols, no tools.
         'skia_enable_effects':    'false',
         'skia_enable_gpu':        'false',
         'skia_enable_pdf':        'false',
-        'skia_enable_tools':      'false',  # Some tools need GPU or effects.
         'skia_use_expat':         'false',
         'skia_use_libjpeg_turbo': 'false',
         'skia_use_libpng':        'false',
