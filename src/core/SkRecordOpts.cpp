d4e4d60af66aacbf71deabe4df703ccb97b8a6eb
Yuqian Li <liyuqian@google.com>
2017-03-06 14:57:53 -0500

    Turn off savelayer optimization for Android framework
    This is a simpler version of https://skia-review.googlesource.com/c/8970/
    
    Note that we couldn't do the optimization and add back the clip effect by
    another clipRect or clipPath call because the final matrix is not available at
    recording time.
    
    BUG=skia:
    
    Change-Id: I3ca091bdc1ee1d265a79fb88d0a6f9afe2b27e58

--- src/core/SkRecordOpts.cpp
+++ src/core/SkRecordOpts.cpp
@@ -297,7 +297,12 @@ void SkRecordOptimize(SkRecord* record) {
     //     https://bugs.chromium.org/p/skia/issues/detail?id=5548
 //    SkRecordNoopSaveRestores(record);
 
+    // Turn off this optimization completely for Android framework
+    // because it makes the following Android CTS test fail:
+    // android.uirendering.cts.testclasses.LayerTests#testSaveLayerClippedWithAlpha
+#ifndef SK_BUILD_FOR_ANDROID_FRAMEWORK
     SkRecordNoopSaveLayerDrawRestores(record);
+#endif
     SkRecordMergeSvgOpacityAndFilterLayers(record);
 
     record->defrag();
@@ -305,7 +310,10 @@ void SkRecordOptimize(SkRecord* record) {
 
 void SkRecordOptimize2(SkRecord* record) {
     multiple_set_matrices(record);
+    // See why we turn this off in SkRecordOptimize above.
+#ifndef SK_BUILD_FOR_ANDROID_FRAMEWORK
     SkRecordNoopSaveRestores(record);
+#endif
     SkRecordNoopSaveLayerDrawRestores(record);
     SkRecordMergeSvgOpacityAndFilterLayers(record);
 
