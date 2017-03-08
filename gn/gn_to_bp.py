0e8f4f9efab3be8ebbaa7942b339b1b5979e60a9
Mike Klein <mtklein@chromium.org>
2017-03-08 18:54:25 +0000

    Revert "Turn on SkJumper all the time."
    This reverts commit 135555101cbde68656f028c41506113374a4026f.
    
    Reason for revert: forgot g3, seems to break Chrome NaCl builders?
    
    https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Win/builds/15969/steps/compile/logs/stdio
    
    Original change's description:
    > Turn on SkJumper all the time.
    > 
    > If the previous CL sticks, it's now easy to turn on SkJumper everywhere:
    > I was mostly holding back because of build system complexity.
    > 
    > This has the main effect of turning on SkJumper in Chromium.
    > It's already been on on our local test bots and on Android framework.
    > 
    > Change-Id: I7fbfc6aaaa7dace9c3f2cb509583c69b10997dbf
    > Reviewed-on: https://skia-review.googlesource.com/9380
    > Reviewed-by: Mike Klein <mtklein@chromium.org>
    > Commit-Queue: Mike Klein <mtklein@chromium.org>
    > 
    
    TBR=mtklein@chromium.org,herb@google.com,reviews@skia.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I69747e9e75b2a8e3d78f6c150c9b6d6453e00632

--- gn/gn_to_bp.py
+++ gn/gn_to_bp.py
@@ -192,6 +192,7 @@ cc_test {
 # We'll run GN to get the main source lists and include directories for Skia.
 gn_args = {
   'is_official_build':  'true',
+  'skia_enable_jumper': 'true',
   'skia_enable_tools':  'true',
   'skia_use_vulkan':    'true',
   'target_cpu':         '"none"',
