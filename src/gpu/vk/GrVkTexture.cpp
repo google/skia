766fcbb01cece88ad88808581b4ecc5a5cb60e01
Brian Osman <brianosman@google.com>
2017-03-13 09:33:09 -0400

    Treat cross context images as Ganesh-created resources
    Always create them budgeted, and register them with the cache (not as
    wrapped resources).
    
    Re-land (with fixes) of: https://skia-review.googlesource.com/9497
    
    BUG=skia:
    
    Change-Id: I2df7198adc99efa3eea99fc86b0b2930136f22c7
    Reviewed-on: https://skia-review.googlesource.com/9544
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/gpu/vk/GrVkTexture.cpp
+++ src/gpu/vk/GrVkTexture.cpp
@@ -96,10 +96,13 @@ sk_sp<GrVkTexture> GrVkTexture::MakeWrappedTexture(GrVkGpu* gpu,
         return nullptr;
     }
 
-    GrVkImage::Wrapped wrapped = kBorrow_GrWrapOwnership == ownership ? GrVkImage::kBorrowed_Wrapped
-                                                                      : GrVkImage::kAdopted_Wrapped;
-
-    return sk_sp<GrVkTexture>(new GrVkTexture(gpu, kWrapped, desc, *info, imageView, wrapped));
+    if (kAdoptAndCache_GrWrapOwnership == ownership) {
+        return sk_sp<GrVkTexture>(new GrVkTexture(gpu, SkBudgeted::kYes, desc, *info, imageView));
+    } else {
+        GrVkImage::Wrapped wrapped = kBorrow_GrWrapOwnership == ownership
+                ? GrVkImage::kBorrowed_Wrapped : GrVkImage::kAdopted_Wrapped;
+        return sk_sp<GrVkTexture>(new GrVkTexture(gpu, kWrapped, desc, *info, imageView, wrapped));
+    }
 }
 
 GrVkTexture::~GrVkTexture() {
