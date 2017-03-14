2544b63fb4a486d62d5711dbd1c09177ef58ee2d
Brian Osman <brianosman@google.com>
2017-03-14 10:26:58 -0400

    Add const to GrColorSpaceXform member arguments
    BUG=skia:
    
    Change-Id: I643c486066a0cd48e9c985ab5f3ee453c2a533c5

--- src/gpu/GrColorSpaceXform.cpp
+++ src/gpu/GrColorSpaceXform.cpp
@@ -53,12 +53,12 @@ private:
     uint64_t fSequence;
 };
 
-GrColorSpaceXform::GrColorSpaceXform(const SkMatrix44& srcToDst) 
+GrColorSpaceXform::GrColorSpaceXform(const SkMatrix44& srcToDst)
     : fSrcToDst(srcToDst) {}
 
 static SkSpinlock gColorSpaceXformCacheSpinlock;
 
-sk_sp<GrColorSpaceXform> GrColorSpaceXform::Make(SkColorSpace* src, SkColorSpace* dst) {
+sk_sp<GrColorSpaceXform> GrColorSpaceXform::Make(const SkColorSpace* src, const SkColorSpace* dst) {
     if (!src || !dst) {
         // Invalid
         return nullptr;
