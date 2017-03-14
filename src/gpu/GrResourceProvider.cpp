0d4e2833ad0a5736d52ab3b222269a1005a02eef
Robert Phillips <robertphillips@google.com>
2017-03-14 14:49:42 -0400

    More pre-emptive proxification
    Change-Id: I2bf38202750e91e66a8044a790ccfe34d59e08a7

--- src/gpu/GrResourceProvider.cpp
+++ src/gpu/GrResourceProvider.cpp
@@ -416,5 +416,3 @@ void GrResourceProvider::takeOwnershipOfSemaphore(sk_sp<GrSemaphore> semaphore) 
 void GrResourceProvider::releaseOwnershipOfSemaphore(sk_sp<GrSemaphore> semaphore) {
     semaphore->resetGpu(nullptr);
 }
-
-
