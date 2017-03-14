27e49f500ef202013f172eeb4cc478784d620124
Joe Gregorio <jcgregorio@google.com>
2017-03-14 09:40:43 -0400

    noworky
    BUG=skia:
    
    Change-Id: I5d731cba8d1cfdcf051907cf5d3dcae8b553a537

---
+++ tools/fiddle/native.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+
+#include "gl/GrGLInterface.h"
+#include "gl/GrGLAssembleInterface.h"
+
+#include <dlfcn.h>
+#include <cstdio>
+
+typedef unsigned char GLubyte;
+
+typedef void* (*GLXGetCurrentContextProc)(void);
+typedef GrGLFuncPtr (*GLXGetProcAddressProc)(const GLubyte* name);
+
+class GLXLoader {
+public:
+    GLXLoader() {
+        fLibrary = dlopen("libGL.so.1", RTLD_LAZY);
+        fprintf(stderr, "%p", fLibrary);
+    }
+    ~GLXLoader() {
+        if (fLibrary) {
+            dlclose(fLibrary);
+        }
+    }
+    void* handle() const {
+        return (nullptr == fLibrary) ? RTLD_DEFAULT : fLibrary;
+    }
+private:
+    void* fLibrary;
+};
+
+class GLXProcGetter {
+public:
+    GLXProcGetter() {
+        fGetCurrentContext = (GLXGetCurrentContextProc) dlsym(fLoader.handle(), "eglXGetCurrentContext");
+        fGetProcAddress = (GLXGetProcAddressProc) dlsym(fLoader.handle(), "eglXGetProcAddress");
+    }
+    GrGLFuncPtr getProc(const GLubyte* name) const {
+        return fGetProcAddress(name);
+    }
+    void* getCurrentContext(void) const {
+        return fGetCurrentContext();
+    }
+private:
+    GLXLoader fLoader;
+    GLXGetCurrentContextProc fGetCurrentContext;
+    GLXGetProcAddressProc fGetProcAddress;
+};
+
+static GrGLFuncPtr glx_get(void* ctx, const char name[]) {
+    // Avoid calling glXGetProcAddress() for EGL procs.
+    // We don't expect it to ever succeed, but somtimes it returns non-null anyway.
+    if (0 == strncmp(name, "egl", 3)) {
+        return nullptr;
+    }
+
+    SkASSERT(nullptr != ctx);
+    const GLXProcGetter* getter = (const GLXProcGetter*) ctx;
+    SkASSERT(nullptr != getter->getCurrentContext())
+    return getter->getProc(reinterpret_cast<const GLubyte*>(name));
+}
+
+const GrGLInterface* GrGLCreateNativeInterface2() {
+    GLXProcGetter getter;
+
+    if (nullptr == getter.getCurrentContext()) {
+        return nullptr;
+    }
+
+    return GrGLAssembleInterface(&getter, glx_get);
+}
