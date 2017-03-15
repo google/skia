5eefb853c68f76fc5001bc6e8e9e5db2772ac135
Mike Reed <reed@google.com>
2017-03-15 19:08:55 -0400

    remove legacy clipop names
    BUG=skia:
    
    Change-Id: Ie34288ef77554eefe726a1bc07a64d0972785004
    Reviewed-on: https://skia-review.googlesource.com/9784
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- include/core/SkClipOp.h
+++ include/core/SkClipOp.h
@@ -18,57 +18,12 @@ enum class SkClipOp {
 
     // Goal: remove these, since they can grow the current clip
 
-#ifdef SK_SUPPORT_EXOTIC_CLIPOPS
-    kUnion         = 2,
-    kXOR           = 3,
-    kReverseDifference = 4,
-    kReplace       = 5,
-#endif
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // End of enum
-
-
-
-
-
-
-
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // Nothing to see here
-
-
-
-
-
-
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // Turn back!
-
-
-
-
-
-    // Private Internal enums -- do not use -- destined to be removed at any moment!
     kUnion_deprecated             = 2,
     kXOR_deprecated               = 3,
     kReverseDifference_deprecated = 4,
     kReplace_deprecated           = 5,
 
     kMax_EnumValue = kReplace_deprecated,
-
-#ifdef SK_SUPPORT_LEGACY_CLIPOP_EXOTIC_NAMES
-    kUnion_private_internal_do_not_use             = kUnion_deprecated,
-    kXOR_private_internal_do_not_use               = kXOR_deprecated,
-    kReverseDifference_private_internal_do_not_use = kReverseDifference_deprecated,
-    kReplace_private_internal_do_not_use           = kReplace_deprecated,
-#endif
 };
 
 #endif
