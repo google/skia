ceca7eab82f0d6fa9daa00e4201b142af5d61e2e
Mike Reed <reed@google.com>
2017-03-16 16:44:25 +0000

    Revert[3] "store vertices arrays inline with object"""
    This reverts commit 7d9f9e30204ee8a380443b868e4cc281319a2051.
    
    Reason for revert: speculative revert to try to fix google3
    
    Original change's description:
    > Revert[2] "store vertices arrays inline with object""
    > 
    > This reverts commit 9e62df6ecd1000860ad19ab9425579dfb7002ba0.
    > 
    > Reason for revert: behavior in reader32 fixed
    > 
    > Fix is here: https://skia-review.googlesource.com/c/9729/
    > 
    > Original change's description:
    > > Revert "store vertices arrays inline with object"
    > > 
    > > This reverts commit eaaebb19a17d213355e7a70e0cfabe4ba61929d4.
    > > 
    > > Reason for revert: may call SkReader32::read(null, 0) -- reader needs to handle this
    > > 
    > > Original change's description:
    > > > store vertices arrays inline with object
    > > > 
    > > > Also unify some of naming (esp. around texCoords)
    > > > 
    > > > BUG=skia:6366
    > > > 
    > > > Change-Id: I5a6793f029cccf0cd0a2c1d180b259ce4eab526f
    > > > Reviewed-on: https://skia-review.googlesource.com/9705
    > > > Commit-Queue: Mike Reed <reed@google.com>
    > > > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > > > 
    > > 
    > > TBR=bsalomon@google.com,reed@google.com,reviews@skia.org
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=skia:6366
    > > 
    > > Change-Id: Ie421654bcd74d74f8be6676291e3d6e16e2a7a16
    > > Reviewed-on: https://skia-review.googlesource.com/9727
    > > Reviewed-by: Mike Reed <reed@google.com>
    > > Commit-Queue: Mike Reed <reed@google.com>
    > > 
    > 
    > TBR=bsalomon@google.com,reviews@skia.org,reed@google.com
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=skia:6366
    > 
    > Change-Id: I1f12108fff8f551d66455cfadd6d5dd9412e9aa8
    > Reviewed-on: https://skia-review.googlesource.com/9760
    > Reviewed-by: Mike Reed <reed@google.com>
    > Commit-Queue: Mike Reed <reed@google.com>
    > 
    
    TBR=bsalomon@google.com,reviews@skia.org,reed@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:6366
    
    Change-Id: Ie23130a07fbecd5664e37291bc167008a6b496bc

--- gm/vertices.cpp
+++ gm/vertices.cpp
@@ -214,8 +214,7 @@ static void draw_batching(SkCanvas* canvas, bool useObject) {
     // Triangle fans can't batch so we convert to regular triangles,
     static constexpr int kNumTris = kMeshIndexCnt - 2;
     SkVertices::Builder builder(SkCanvas::kTriangles_VertexMode, kMeshVertexCnt, 3 * kNumTris,
-                                SkVertices::kHasColors_BuilderFlag |
-                                SkVertices::kHasTexCoords_BuilderFlag);
+                                SkVertices::kHasColors_Flag | SkVertices::kHasTexs_Flag);
 
     SkPoint* pts = builder.positions();
     SkPoint* texs = builder.texCoords();
