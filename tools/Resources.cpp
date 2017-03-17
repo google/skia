6e33dda0f869156c911fdb6d7ac94eb60a7c2ff4
Ben Wagner <bungeman@google.com>
2017-03-17 17:56:28 -0400

    GetResourceAsData to use SkData::MakeFromFileName directly.
    The current code creates an SkFILEStream and then reads all the bytes
    from it into an SkData. Instead, let the SkData just mmap the file.
    
    Change-Id: I79e3550a84e6f54ccbbd7284f5cda81ce1fa9221

--- tools/Resources.cpp
+++ tools/Resources.cpp
@@ -59,15 +59,7 @@ SkStreamAsset* GetResourceAsStream(const char* resource) {
 
 sk_sp<SkData> GetResourceAsData(const char* resource) {
     SkString resourcePath = GetResourcePath(resource);
-    std::unique_ptr<SkFILEStream> stream(new SkFILEStream(resourcePath.c_str()));
-    if (!stream->isValid()) {
-        SkDebugf("Resource %s not found.\n", resource);
-        return nullptr;
-    }
-    size_t bytes = stream->getLength();
-    sk_sp<SkData> data = SkData::MakeUninitialized(bytes);
-    stream->read(data->writable_data(), bytes);
-    return data;
+    return SkData::MakeFromFileName(resourcePath.c_str());
 }
 
 sk_sp<SkTypeface> MakeResourceAsTypeface(const char* resource) {
