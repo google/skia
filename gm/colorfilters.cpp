fc74e91db720159e8db4a1c721bfeb300c9ac959
Cary Clark <caryclark@skia.org>
2017-03-21 09:07:02 -0400

    fix fiddles
    fix fiddle parsing
    
    
    canvas test
    
    BUG=skia:
    
    Change-Id: I1361d171bf81b98255cc921fa47228fdcb67429c

--- gm/colorfilters.cpp
+++ gm/colorfilters.cpp
@@ -84,3 +84,17 @@ private:
 //////////////////////////////////////////////////////////////////////////////
 
 DEF_GM(return new ColorFiltersGM;)
+
+DEF_SIMPLE_GM(blendy, canvas, 256, 256) {
+    SkPaint normal, blender;
+    normal.setColor(0xFF58a889);
+    blender.setColor(0xFF8958a8);
+    canvas->clear(0);
+    for (SkBlendMode m : { SkBlendMode::kSrcOver, SkBlendMode::kSrcIn, SkBlendMode::kSrcOut } ) {
+        normal.setBlendMode(SkBlendMode::kSrcOver);
+        canvas->drawOval(SkRect::MakeXYWH(30, 30, 30, 80), normal);
+        blender.setBlendMode(m);
+        canvas->drawOval(SkRect::MakeXYWH(10, 50, 80, 30), blender);
+        canvas->translate(70, 70);
+    }
+}
