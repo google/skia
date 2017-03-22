e218f0129772eff27a8a007574f74e0d8002027d
Robert Phillips <robertphillips@google.com>
2017-03-22 11:53:27 -0400

    Make SkImageCacherator be deferred
    Split out of: https://skia-review.googlesource.com/c/8823/ (Remove GrFragmentProcessor-derived class' GrTexture-based ctors)
    
    Change-Id: I16cf0aea9d887e5ebe053e9b5c94a970dc254beb

--- src/image/SkImage_Generator.cpp
+++ src/image/SkImage_Generator.cpp
@@ -83,18 +83,15 @@ bool SkImage_Generator::getROPixels(SkBitmap* bitmap, SkColorSpace* dstColorSpac
 }
 
 #if SK_SUPPORT_GPU
+#include "GrContext.h"
+
 sk_sp<GrTextureProxy> SkImage_Generator::asTextureProxyRef(GrContext* context,
                                                            const GrSamplerParams& params,
                                                            SkColorSpace* dstColorSpace,
                                                            sk_sp<SkColorSpace>* texColorSpace,
                                                            SkScalar scaleAdjust[2]) const {
-    sk_sp<GrTexture> tex(fCache.lockAsTexture(context, params, dstColorSpace,
-                                              texColorSpace, this, scaleAdjust));
-    if (!tex) {
-        return nullptr;
-    }
-
-    return GrSurfaceProxy::MakeWrapped(std::move(tex));
+    return fCache.lockAsTextureProxy(context, params, dstColorSpace,
+                                     texColorSpace, this, scaleAdjust);
 }
 #endif
 
