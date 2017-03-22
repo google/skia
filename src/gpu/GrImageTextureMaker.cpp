dde3964746f3464deef6cd8b18900603d2b2098f
Robert Phillips <robertphillips@google.com>
2017-03-22 12:21:03 -0400

    Make SkImageCacherator be deferred
    Split out of: https://skia-review.googlesource.com/c/8823/ (Remove GrFragmentProcessor-derived class' GrTexture-based ctors)
    
    Change-Id: I16cf0aea9d887e5ebe053e9b5c94a970dc254beb

--- src/gpu/GrImageTextureMaker.cpp
+++ src/gpu/GrImageTextureMaker.cpp
@@ -32,16 +32,21 @@ GrImageTextureMaker::GrImageTextureMaker(GrContext* context, SkImageCacherator* 
 }
 
 GrTexture* GrImageTextureMaker::refOriginalTexture(bool willBeMipped, SkColorSpace* dstColorSpace) {
-    return fCacher->lockTexture(this->context(), fOriginalKey, fClient, fCachingHint, willBeMipped,
-                                dstColorSpace);
+    sk_sp<GrTextureProxy> proxy = fCacher->lockTextureProxy(this->context(), fOriginalKey,
+                                                            fClient, fCachingHint, willBeMipped,
+                                                            dstColorSpace);
+    if (!proxy) {
+        return nullptr;
+    }
+
+    sk_sp<GrTexture> tex(SkSafeRef(proxy->instantiate(this->context()->resourceProvider())));
+    return tex.release();
 }
 
 sk_sp<GrTextureProxy> GrImageTextureMaker::refOriginalTextureProxy(bool willBeMipped,
                                                                    SkColorSpace* dstColorSpace) {
-    sk_sp<GrTexture> tex(fCacher->lockTexture(this->context(), fOriginalKey, fClient, fCachingHint,
-                                              willBeMipped, dstColorSpace));
-
-    return GrSurfaceProxy::MakeWrapped(std::move(tex));
+    return fCacher->lockTextureProxy(this->context(), fOriginalKey, fClient, fCachingHint,
+                                     willBeMipped, dstColorSpace);
 }
 
 void GrImageTextureMaker::makeCopyKey(const CopyParams& stretch, GrUniqueKey* paramsCopyKey,
