bf3bf86709361ef5ba2055d8aa82b129f19c61ff
Robert Iannucci <iannucci@google.com>
2017-03-24 18:11:15 -0700

    Manually roll recipes
    build:
      crrev.com/232ecb4e8e9a3175db1c14449dca0230ae01b80c Switch to an explicit swarming.summary placeholder instead of json.output.
    
    Bug: skia:
    Change-Id: I7c0dd6ce6225d48541c574ff6f38ea70e1823f1f
    Reviewed-on: https://skia-review.googlesource.com/10126
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/bots/recipe_modules/swarming/api.py
+++ infra/bots/recipe_modules/swarming/api.py
@@ -171,7 +171,7 @@ class SkiaSwarmingApi(recipe_api.RecipeApi):
       swarming_tasks.append(swarming_task)
     step_results = self.m.swarming.trigger(swarming_tasks)
     for step_result in step_results:
-      self._add_log_links(step_result)
+      self._add_log_links(step_result, step_result.json.output)
     return swarming_tasks
 
   def collect_swarming_task(self, swarming_task):
@@ -190,25 +190,26 @@ class SkiaSwarmingApi(recipe_api.RecipeApi):
       states_infra_failure = (
           self.m.swarming.State.EXPIRED, self.m.swarming.State.TIMED_OUT,
           self.m.swarming.State.BOT_DIED, self.m.swarming.State.CANCELED)
-      if step_result.json.output['shards'][0]['state'] in states_infra_failure:
+      summary = step_result.swarming.summary
+      if summary['shards'][0]['state'] in states_infra_failure:
         step_result.presentation.status = self.m.step.EXCEPTION
         raise self.m.step.InfraFailure(e.name, step_result)
       raise
     finally:
       step_result = self.m.step.active_result
       # Add log link.
-      self._add_log_links(step_result)
+      self._add_log_links(step_result, step_result.swarming.summary)
     return rv
 
-  def _add_log_links(self, step_result):
+  def _add_log_links(self, step_result, summary):
     """Add Milo log links to all shards in the step."""
     ids = []
-    shards = step_result.json.output.get('shards')
+    shards = summary.get('shards')
     if shards:
       for shard in shards:
         ids.append(shard['id'])
     else:
-      for _, task in step_result.json.output.get('tasks', {}).iteritems():
+      for _, task in summary.get('tasks', {}).iteritems():
         ids.append(task['task_id'])
     for idx, task_id in enumerate(ids):
       link = MILO_LOG_LINK % task_id
