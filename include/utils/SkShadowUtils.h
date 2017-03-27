fcbb18cedbdc6c82d4286441c09a7a107e596ae1
Jim Van Verth <jvanverth@google.com>
2017-03-27 11:02:52 -0400

    Add perspective support for shadows
    Bug: skia:
    Change-Id: Id07d3050afb3f0d001b885e482adb8d03125b619

--- include/utils/SkShadowUtils.h
+++ include/utils/SkShadowUtils.h
@@ -11,6 +11,7 @@
 #include "SkColor.h"
 #include "SkScalar.h"
 #include "../private/SkShadowFlags.h"
+#include <functional>
 
 class SkCanvas;
 class SkPath;
@@ -25,7 +26,7 @@ public:
      * @param canvas  The canvas on which to draw the shadows.
      * @param path  The occluder used to generate the shadows.
      * @param occluderHeight  The vertical offset of the occluder from the canvas. This is
-     *  indepdendent of the canvas's current matrix.
+     *  independent of the canvas's current matrix.
      * @param lightPos  The 3D position of the light relative to the canvas plane. This is
      *  independent of the canvas's current matrix.
      * @param lightRadius  The radius of the disc light.
@@ -41,6 +42,30 @@ public:
                            SkScalar spotAlpha, SkColor color,
                            uint32_t flags = SkShadowFlags::kNone_ShadowFlag,
                            SkResourceCache* cache = nullptr);
+
+   /**
+    * Draw an offset spot shadow and outlining ambient shadow for the given path using a disc
+    * light. Takes a function to vary the z value based on the transformed x and y position.
+    * This shadow will not be cached, as the assumption is that this will be used for animation.
+    *
+    * @param canvas  The canvas on which to draw the shadows.
+    * @param path  The occluder used to generate the shadows.
+    * @param heightFunc  A function which returns the vertical offset of the occluder from the
+    *  canvas based on local x and y values (the current matrix is not applied).
+    * @param lightPos  The 3D position of the light relative to the canvas plane. This is
+    *  independent of the canvas's current matrix.
+    * @param lightRadius  The radius of the disc light.
+    * @param ambientAlpha  The maximum alpha of the ambient shadow.
+    * @param spotAlpha  The maxium alpha of the spot shadow.
+    * @param color  The shadow color.
+    * @param flags  Options controlling opaque occluder optimizations and shadow appearance. See
+    *               SkShadowFlags.
+    */
+    static void DrawUncachedShadow(SkCanvas* canvas, const SkPath& path,
+                                   std::function<SkScalar(SkScalar, SkScalar)> heightFunc,
+                                   const SkPoint3& lightPos, SkScalar lightRadius,
+                                   SkScalar ambientAlpha, SkScalar spotAlpha, SkColor color,
+                                   uint32_t flags = SkShadowFlags::kNone_ShadowFlag);
 };
 
 #endif
