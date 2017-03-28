81331a9ee567fea0738fd9058a3d1c3979c3baf3
Ethan Nicholas <ethannicholas@google.com>
2017-03-28 11:21:42 -0400

    skslc can now be compiled with no Skia dependencies, in preparation for its eventual role in Skia's build process.
    Bug: skia:
    Change-Id: Iaa9933f4fc4a64bec60aa897c509a3513f457a78

---
+++ src/sksl/SkSLStringStream.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_STRINGSTREAM
+#define SKSL_STRINGSTREAM
+
+#include "SkSLOutputStream.h"
+
+namespace SkSL {
+
+class StringStream : public OutputStream {
+public:
+    void write8(int8_t b) override {
+        fBuffer += (char) b;
+    }
+
+    void write(const char* s) override {
+        fBuffer += s;
+    }
+
+    void write(const void* s, size_t size) override {
+        fBuffer.append((const char*) s, size);
+    }
+
+    const void* data() const {
+        return fBuffer.c_str();
+    }
+
+    size_t size() const {
+        return fBuffer.size();
+    }
+
+private:
+    String fBuffer;
+};
+
+} // namespace
+
+#endif
