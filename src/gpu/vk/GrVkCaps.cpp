e0ff6ab272dcc5c4d6f3b509da7e0e97acada814
Eric Karl <ericrk@chromium.org>
2017-03-30 15:19:07 -0700

    Respect kRectsMustMatchForMSAASrc_BlitFramebufferFlag in dst setup
    Crurently, when preparing a texture for blitFramebuffer, we ignore the
    kRectsMustMatchForMSAASrc_BlitFramebufferFlag, and may attempt to
    copy from one src rect to a different dst rect.
    
    This change updates initDescForDstCopy and setupDstTexture to allocate
    larger textures if necessary and accomodate this flags requirements.
    
    Bug: 658277
    Change-Id: I500f10dba5700f5f7a7acad04bcdbc9ac9994835
    Reviewed-on: https://skia-review.googlesource.com/10247
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -53,7 +53,12 @@ GrVkCaps::GrVkCaps(const GrContextOptions& contextOptions, const GrVkInterface* 
     this->init(contextOptions, vkInterface, physDev, featureFlags, extensionFlags);
 }
 
-bool GrVkCaps::initDescForDstCopy(const GrRenderTarget* src, GrSurfaceDesc* desc) const {
+bool GrVkCaps::initDescForDstCopy(const GrRenderTarget* src, GrSurfaceDesc* desc,
+                                  bool* rectsMustMatch, bool* disallowSubrect) const {
+    // Vk doesn't use rectsMustMatch or disallowSubrect. Always return false.
+    *rectsMustMatch = false;
+    *disallowSubrect = false;
+
     // We can always succeed here with either a CopyImage (none msaa src) or ResolveImage (msaa).
     // For CopyImage we can make a simple texture, for ResolveImage we require the dst to be a
     // render target as well.
