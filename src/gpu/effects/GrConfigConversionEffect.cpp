ad45e550c084fb55c5ffb62087973d44f51b57f7
Brian Osman <brianosman@google.com>
2017-03-31 15:40:37 -0400

    DO NOT LAND: Quickly checks for round-trip PM conversion.
    Bug: skia:
    Change-Id: I5a4e7d04d78332eb76b35d6eaa4d4316ce257029

--- src/gpu/effects/GrConfigConversionEffect.cpp
+++ src/gpu/effects/GrConfigConversionEffect.cpp
@@ -15,6 +15,8 @@
 #include "glsl/GrGLSLFragmentProcessor.h"
 #include "glsl/GrGLSLFragmentShaderBuilder.h"
 
+#include <cstdlib>
+
 class GrGLConfigConversionEffect : public GrGLSLFragmentProcessor {
 public:
     void emitCode(EmitArgs& args) override {
@@ -49,7 +51,6 @@ public:
                 fragBuilder->codeAppendf(
                     "%s = vec4(floor(%s.rgb * %s.a * 255.0 + 0.001) / 255.0, %s.a);",
                     tmpVar.c_str(), tmpVar.c_str(), tmpVar.c_str(), tmpVar.c_str());
-
                 break;
             case GrConfigConversionEffect::kDivByAlpha_RoundUp_PMConversion:
                 fragBuilder->codeAppendf(
@@ -249,6 +250,11 @@ void GrConfigConversionEffect::TestForPreservingPMConversions(GrContext* context
     if (failed) {
         *pmToUPMRule = kPMConversionCnt;
         *upmToPMRule = kPMConversionCnt;
+        SkFAIL("No round trip conversion found!\n");
+        _Exit(1);
+    } else {
+        SkDebugf("Round-trip conversion rule: (%d, %d)\n", *pmToUPMRule, *upmToPMRule);
+        _Exit(0);
     }
 }
 
