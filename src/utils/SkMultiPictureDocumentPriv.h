9539dbe382271df14c4c266db524b8260790ecd3
Hal Canary <halcanary@google.com>
2017-04-03 16:06:42 -0400

    SkMultiPictureDocument : make public
    Also clean up MultiPictureDocumentReader interface to make public.
    
    Change-Id: I06354e852b46a6d85ca31870ae289e00ff425faf

--- src/utils/SkMultiPictureDocumentPriv.h
+++ src/utils/SkMultiPictureDocumentPriv.h
@@ -8,25 +8,10 @@
 #ifndef SkMultiPictureDocumentPriv_DEFINED
 #define SkMultiPictureDocumentPriv_DEFINED
 
-#include "SkTArray.h"
-#include "SkSize.h"
+#include "SkMultiPictureDocument.h"
 
-namespace SkMultiPictureDocumentProtocol {
-static constexpr char kMagic[] = "Skia Multi-Picture Doc\n\n";
-
-static constexpr char kEndPage[] = "SkMultiPictureEndPage";
-
-const uint32_t kVersion = 2;
-
-inline SkSize Join(const SkTArray<SkSize>& sizes) {
-    SkSize joined = SkSize::Make(0, 0);
-    for (SkSize s : sizes) {
-        joined = SkSize::Make(SkTMax(joined.width(), s.width()),
-                              SkTMax(joined.height(), s.height()));
-    }
-    return joined;
-}
-
-}
+bool SkMultiPictureDocumentReadPageSizes(SkStreamSeekable* src,
+                                         SkDocumentPage* dstArray,
+                                         int dstArrayCount);
 
 #endif  // SkMultiPictureDocumentPriv_DEFINED
