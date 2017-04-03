ccc38c6becda85716b3e18422c2c947a3ea99d55
Hal Canary <halcanary@google.com>
2017-04-03 16:06:42 -0400

    SkMultiPictureDocument : make public
    Bug: skia:
    Change-Id: I06354e852b46a6d85ca31870ae289e00ff425faf

--- src/utils/SkMultiPictureDocument.h
+++ src/utils/SkMultiPictureDocument.h
@@ -1,48 +1,7 @@
 /*
- * Copyright 2016 Google Inc.
+ * Copyright 2017 Google Inc.
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-#ifndef SkMultiPictureDocument_DEFINED
-#define SkMultiPictureDocument_DEFINED
-
-/*
-  This format is not intended to be used in production.
-
-  For clients looking for a way to represent a document in memory,
-
-    struct Doc {
-        std::vector<sk_sp<SkPicture>> fPages;
-        std::vector<SkSize> fPageSizes;
-    };
-
-  or
-
-    struct Page {
-        sk_sp<SkPicture> fPage;
-        SkSize fPageSize;
-    };
-    std::vector<Page> pages;
-
-  would work much better.
-
-  Multi-SkPicture (MSKP) files are still useful for debugging and
-  testing.
-
-  The downsides of this format are currently:
-  - no way to extract a single page; must read the entire file at once.
-  - must use `dm` to convert to another format before passing into
-    standard skp tools.
-  - `dm` can extract the first page to skp, but no others.
-
-  TODO(halcanary): replace with somthing that addresses these issues.
- */
-
-#include "SkDocument.h"
-
-/** Writes into an experimental, undocumented file format that is
-    useful for debugging documents printed via Skia. */
-SK_API sk_sp<SkDocument> SkMakeMultiPictureDocument(SkWStream* dst);
-
-#endif  // SkMultiPictureDocument_DEFINED
+#include "../../include/core/SkMultiPictureDocument.h"
