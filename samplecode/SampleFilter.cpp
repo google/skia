73f5d115e545326e92716cdc230e34af503a5fb3
Mike Reed <reed@google.com>
2017-04-03 14:41:44 -0400

    Revert[4] "clean up (partially) colortable api""""
    Fixes:
    - create temp api for android to pass nullptr
    - don't release and access sk_sp<SkData> at the same time in parameters
    
    This reverts commit b14131c1851eea6acbd34cc42a8f860daed36b21.
    
    Bug: skia:
    Change-Id: Ic0e4f62520ba9f35455499ed30d306ad19d998a8

--- samplecode/SampleFilter.cpp
+++ samplecode/SampleFilter.cpp
@@ -26,11 +26,9 @@ static void make_bm(SkBitmap* bm) {
         SkPreMultiplyColor(SK_ColorRED), SkPreMultiplyColor(SK_ColorGREEN),
         SkPreMultiplyColor(SK_ColorBLUE), SkPreMultiplyColor(SK_ColorWHITE)
     };
-    SkColorTable* ctable = new SkColorTable(colors, 4);
     bm->allocPixels(SkImageInfo::Make(2, 2, kIndex_8_SkColorType,
                                       kOpaque_SkAlphaType),
-                    nullptr, ctable);
-    ctable->unref();
+                    SkColorTable::Make(colors, 4));
 
     *bm->getAddr8(0, 0) = 0;
     *bm->getAddr8(1, 0) = 1;
