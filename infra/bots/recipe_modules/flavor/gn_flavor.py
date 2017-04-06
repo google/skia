43b9c6bbf66b0927a99062c68dff9ea8358f82db
Eric Boren <borenet@google.com>
2017-04-06 07:53:30 -0400

    Use bundled recipes on all Test/Perf tasks
    It won't save as much time for faster machines, but it should still
    shave a few seconds, and it'll be nice to be consistent.
    
    Bug: skia:5813
    Change-Id: I5724e7aae83851edff3129265a9ffee5c3f95825
    Reviewed-on: https://skia-review.googlesource.com/11340
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- infra/bots/recipe_modules/flavor/gn_flavor.py
+++ infra/bots/recipe_modules/flavor/gn_flavor.py
@@ -170,8 +170,7 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
       args = [self.m.vars.slave_dir] + [str(x) for x in cmd]
       with self.m.step.context({'cwd': self.m.vars.skia_dir, 'env': env}):
         self._py('symbolized %s' % name,
-                 self.m.vars.infrabots_dir.join('recipe_modules', 'core',
-                 'resources', 'symbolize_stack_trace.py'),
+                 self.module.resource('symbolize_stack_trace.py'),
                  args=args,
                  infra_step=False)
 
