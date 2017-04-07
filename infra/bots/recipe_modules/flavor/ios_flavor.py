bc5f93d22d392f63ca0abf604d5dcffc66b47c4a
Stephan Altmueller <stephana@google.com>
2017-04-07 10:43:12 -0400

    Enable ios on Raspberry Pi
    Bug: skia:
    Change-Id: I0124c6554173fa3b22b077a198612bb30691d1fb

--- infra/bots/recipe_modules/flavor/ios_flavor.py
+++ infra/bots/recipe_modules/flavor/ios_flavor.py
@@ -36,11 +36,16 @@ class iOSFlavorUtils(gn_flavor.GNFlavorUtils):
               args=[self.out_dir.join(app)])
 
   def step(self, name, cmd, env=None, **kwargs):
-    app = self.m.vars.skia_out.join(self.m.vars.configuration, cmd[0])
+    app_name = cmd[0]
+    app_path = self.m.vars.skia_out.join(self.m.vars.configuration, app_name)
 
+    # Install the app.
+    self._run(name, ['ideviceinstaller','-i', app_path])
+
+    # Run the app.
     self._run(name,
-              ['ios-deploy', '-b', '%s.app' % app,
-               '-I', '--args', ' '.join(map(str, cmd[1:]))])
+              ['idevicedebug', '-d', 'run', 'com.google.%s' % app_name] +
+              map(str, cmd[1:]))
 
   def _run_ios_script(self, script, first, *rest):
     full = self.m.vars.skia_dir.join('platform_tools/ios/bin/ios_' + script)
