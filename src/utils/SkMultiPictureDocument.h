3341a57815d88ade04edeb9880e3f15a7d622549
Hal Canary <halcanary@google.com>
2017-04-07 11:59:52 -0400

    SkMultiPictureDocument : make public
    Change-Id: I06354e852b46a6d85ca31870ae289e00ff425faf

--- src/utils/SkMultiPictureDocument.h
+++ src/utils/SkMultiPictureDocument.h
@@ -1,42 +1,19 @@
 /*
- * Copyright 2016 Google Inc.
+ * Copyright 2017 Google Inc.
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#ifndef SkMultiPictureDocument_old_DEFINED
+#define SkMultiPictureDocument_old_DEFINED
 
-#ifndef SkMultiPictureDocument_DEFINED
-#define SkMultiPictureDocument_DEFINED
+#include "../../include/utils/SkMultiPictureDocument.h"
 
-#include "SkDocument.h"
+// This header is deprecated in favor of the one in include/.
 
-struct SkDeserialProcs;
-struct SkSerialProcs;
-class SkStreamSeekable;
+using SkDocumentPage = SkMultiPictureDocument::Page;
+#define SkMakeMultiPictureDocument SkMultiPictureDocument::Make
+#define SkMultiPictureDocumentReadPageCount SkMultiPictureDocument::ReadPageCount
+#define SkMultiPictureDocumentRead SkMultiPictureDocument::Read
 
-/**
- *  Writes into a file format that is similar to SkPicture::serialize()
- */
-SK_API sk_sp<SkDocument> SkMakeMultiPictureDocument(SkWStream* dst, const SkSerialProcs* = nullptr);
-
-struct SkDocumentPage {
-    sk_sp<SkPicture> fPicture;
-    SkSize fSize;
-};
-
-/**
- *  Returns the number of pages in the SkMultiPictureDocument.
- */
-SK_API int SkMultiPictureDocumentReadPageCount(SkStreamSeekable* src);
-
-/**
- *  Read the SkMultiPictureDocument into the provided array of pages.
- *  dstArrayCount must equal SkMultiPictureDocumentReadPageCount().
- *  Return false on error.
- */
-SK_API bool SkMultiPictureDocumentRead(SkStreamSeekable* src,
-                                       SkDocumentPage* dstArray,
-                                       int dstArrayCount,
-                                       const SkDeserialProcs* = nullptr);
-
-#endif  // SkMultiPictureDocument_DEFINED
+#endif  // SkMultiPictureDocument_old_DEFINED
