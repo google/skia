123b7b8ebd2f8abb7e3b2d498f299ae281b24b74
Robert Phillips <robertphillips@google.com>
2017-04-11 09:09:24 -0400

    Disable deferred proxies for Chrome branch
    Chrome is branching again and Proxy instantiation is still occurring at a bad spot in Skia.
    
    Change-Id: I2f6bed7f1833737aa9bb7fd819ff108cffaeaf1d
    Reviewed-on: https://skia-review.googlesource.com/13133
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/ProxyRefTest.cpp
+++ tests/ProxyRefTest.cpp
@@ -47,6 +47,8 @@ int32_t GrIORefProxy::getPendingWriteCnt_TestOnly() const {
     return fPendingWrites;
 }
 
+#ifndef SK_DISABLE_DEFERRED_PROXIES
+
 static const int kWidthHeight = 128;
 
 static void check_refs(skiatest::Reporter* reporter,
@@ -188,4 +190,6 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(ProxyRefTest, reporter, ctxInfo) {
         }
     }
 }
+#endif
+
 #endif
