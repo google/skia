ee660751141f94d9638da40e959850bb457208f3
Leon Scroggins III <scroggo@google.com>
2017-04-19 16:49:27 -0400

    Make SkPngCodec only read as much of the stream as necessary
    Previously, SkPngCodec assumed that the stream only contained one
    image, which ended at the end of the stream. It read the stream in
    arbitrarily-sized chunks, and then passed that data to libpng for
    processing.
    
    If a stream contains more than one image, this may result in reading
    beyond the end of the image, making future reads read the wrong data.
    
    Now, SkPngCodec starts by reading 8 bytes at a time. After the
    signature, 8 bytes is enough to know which chunk is next and how many
    bytes are in the chunk.
    
    When decoding the size, we stop when we reach IDAT, and when decoding
    the image, we stop when we reach IEND.
    
    This manual parsing is necessary to support APNG, which is planned in
    the future. It also allows us to remove the SK_GOOGLE3_PNG_HACK, which
    was a workaround for reading more than necessary at the beginning of
    the image.
    
    Add a test that simulates the issue, by decoding a special stream that
    reports an error if the codec attempts to read beyond the end.
    
    Temporarily disable the partial decoding tests for png. A larger change
    will be necessary to get those working again, and no clients are
    currently relying on incrementally decoding PNGs (i.e. decode part of
    an image, then decode further with more data).
    
    BUG:34073812
    
    Change-Id: If832f7b20565411226fb5be3c305a4d16bf9269d

---
+++ tests/CodecExactReadTest.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "Resources.h"
+#include "Test.h"
+
+#include "SkBitmap.h"
+#include "SkCodec.h"
+#include "SkData.h"
+#include "SkStream.h"
+
+namespace {
+// This class emits a skiatest failure if a client attempts to read beyond its
+// end. Since it is used with complete, valid images, and contains nothing
+// after the encoded image data, it will emit a failure if the client attempts
+// to read beyond the logical end of the data.
+class MyStream : public SkStream {
+public:
+    MyStream(sk_sp<SkData> data, skiatest::Reporter* r)
+        : fStream(std::move(data))
+        , fReporter(r)
+    {}
+
+    size_t read(void* buf, size_t bytes) override {
+        const size_t remaining = fStream.getLength() - fStream.getPosition();
+        REPORTER_ASSERT(fReporter, bytes <= remaining);
+        return fStream.read(buf, bytes);
+    }
+
+    bool rewind() override {
+        return fStream.rewind();
+    }
+
+    bool isAtEnd() const override {
+        return fStream.isAtEnd();
+    }
+private:
+    SkMemoryStream fStream;
+    skiatest::Reporter* fReporter;  // Unowned
+};
+} // namespace
+
+// Test that SkPngCodec does not attempt to read its input beyond the logical
+// end of its data. Some other SkCodecs do, but some Android apps rely on not
+// doing so for PNGs.
+DEF_TEST(Codec_end, r) {
+    for (const char* path : { "plane.png" }) {
+        sk_sp<SkData> data(GetResourceAsData(path));
+        if (!data) {
+            return;
+        }
+
+        std::unique_ptr<SkCodec> codec(SkCodec::NewFromStream(new MyStream(std::move(data), r)));
+        if (!codec) {
+            ERRORF(r, "Failed to create a codec from %s\n", path);
+            return;
+        }
+
+        auto info = codec->getInfo().makeColorType(kN32_SkColorType);
+        SkBitmap bm;
+        bm.allocPixels(info);
+
+        auto result = codec->getPixels(bm.info(), bm.getPixels(), bm.rowBytes());
+        REPORTER_ASSERT(r, SkCodec::kSuccess == result);
+
+        // Rewind and do an incremental decode.
+        result = codec->startIncrementalDecode(bm.info(), bm.getPixels(), bm.rowBytes());
+        REPORTER_ASSERT(r, SkCodec::kSuccess == result);
+
+        result = codec->incrementalDecode();
+        REPORTER_ASSERT(r, SkCodec::kSuccess == result);
+    }
+}
