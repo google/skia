44d1b548ef9c167a8b55eb9529db110996ae3bfe
Eric Boren <borenet@google.com>
2017-04-19 15:07:02 -0400

    Recipes: add git module, check for credentials first
    Error out if we don't have .gitconfig and .netrc. This should help
    identify improperly-setup bots.
    
    Bug: skia:6473
    Change-Id: I6e2ccb00faa77d2384104ee893c70843670f85d3

---
+++ infra/bots/recipe_modules/git/api.py
@@ -0,0 +1,55 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Wrapper for Git which checks the validity of bot credentials."""
+
+
+from recipe_engine import recipe_api
+
+
+class GitApi(recipe_api.RecipeApi):
+  def __init__(self, *args, **kwargs):
+    super(GitApi, self).__init__(*args, **kwargs)
+    self._checked = False
+    self._git = 'git'
+
+
+  def _check(self):
+    """Check credentials."""
+    if self._checked:
+      return
+
+    # This should really go in __init__, but self.m.properties hasn't been
+    # loaded at that point.
+    if 'Win' in self.m.properties.get('buildername', ''):
+      self._git = 'git.bat'
+
+    # Verify that the credential files are present.
+    self.m.python.inline('Check git creds', program='''
+import os
+import sys
+
+gitconfig = os.path.join(os.path.expanduser('~'), '.gitconfig')
+netrc_base = '.netrc'
+if sys.platform.startswith('win'):
+  netrc_base = '_netrc'
+netrc = os.path.join(os.path.expanduser('~'), netrc_base)
+
+has_gitconfig = os.path.isfile(gitconfig)
+has_netrc = os.path.isfile(netrc)
+
+if not has_gitconfig and not has_netrc:
+  raise Exception('Missing %s and %s' % (gitconfig, netrc))
+if not has_gitconfig:
+  raise Exception('Missing %s' % gitconfig)
+if not has_netrc:
+  raise Exception('Missing %s' % netrc)
+''')
+    self._checked = True
+
+  def __call__(self, *cmd):
+    self._check()
+    name = 'git %s' % cmd[0]
+    self.m.run(self.m.step, name, cmd=[self._git] + list(cmd), infra_step=True)
