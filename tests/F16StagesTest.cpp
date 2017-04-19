c84acf7c23cc5e605e2e6f7112f703953b83ad28
Mike Klein <mtklein@chromium.org>
2017-04-19 17:19:30 -0400

    test f16<->f32 conversion stages
    Change-Id: I439463cf90935c5e8fe2369cbcf45e07f3af62c7

---
+++ tests/F16StagesTest.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkRasterPipeline.h"
+#include "Test.h"
+
+DEF_TEST(F16Stages, r) {
+    // Make sure SkRasterPipeline::load_f16 and store_f16 can handle a range of
+    // ordinary (0<=x<=1) and interesting (x<0, x>1) values.
+    float floats[16] = {
+        0.0f, 0.25f, 0.5f, 1.0f,
+        -1.25f, -0.5f, 1.25f, 2.0f,
+        0,0,0,0, 0,0,0,0,  // pad a bit to make sure we qualify for platform-specific code
+    };
+    uint16_t halfs[16] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+
+    float*    f32 = floats;
+    uint16_t* f16 = halfs;
+
+    SkRasterPipeline p;
+    p.append(SkRasterPipeline:: load_f32, &f32);
+    p.append(SkRasterPipeline::store_f16, &f16);
+    p.append(SkRasterPipeline:: load_f16, &f16);
+    p.append(SkRasterPipeline::store_f32, &f32);
+    p.run(0,16/4);
+
+    REPORTER_ASSERT(r, f32[0] == 0.00f);
+    REPORTER_ASSERT(r, f32[1] == 0.25f);
+    REPORTER_ASSERT(r, f32[2] == 0.50f);
+    REPORTER_ASSERT(r, f32[3] == 1.00f);
+
+    REPORTER_ASSERT(r, f32[4] == -1.25f);
+    REPORTER_ASSERT(r, f32[5] == -0.50f);
+    REPORTER_ASSERT(r, f32[6] == +1.25f);
+    REPORTER_ASSERT(r, f32[7] == +2.00f);
+}
