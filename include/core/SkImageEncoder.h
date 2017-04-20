ef644fc6ef6d7dedc4fcf85e07b82d805fe24c91
Matt Sarett <msarett@google.com>
2017-04-20 18:17:42 -0400

    SkEncodeImage() API with options struct
    Bug: skia:
    Change-Id: Iab908b567df516a5806dbaeea904e977ec37f0ec

--- include/core/SkImageEncoder.h
+++ include/core/SkImageEncoder.h
@@ -13,30 +13,103 @@
 #include "SkStream.h"
 
 /**
+ * Various options to control encode behavior.
+ */
+struct SkEncodeOptions {
+    /**
+     * Options that only affect JPEG encodes.
+     * |fQuality| must be in [0, 100] where 0 corresponds to the lowest quality.
+     *
+     */
+    struct JPEG { int fQuality; };
+
+    /**
+     * Options that only affect WEBP encodes.
+     * |fQuality| must be in [0.0f, 100.0f] where 0.0f corresponds to the lowest quality.
+     */
+    struct WEBP { float fQuality; };
+
+    /**
+     * Options that only affect PNG encodes.
+     */
+    struct PNG {};
+
+    union Format {
+        JPEG fJPEG;
+        WEBP fWEBP;
+        PNG  fPNG;
+    };
+
+    /**
+     * Container for format specific options.
+     */
+    Format                     fFormat;
+
+    /**
+     * If the input is premultiplied, this controls the unpremultiplication behavior.
+     * The encoder can convert to linear before unpremultiplying or ignore the transfer
+     * function and unpremultiply the input as is.
+     */
+    SkTransferFunctionBehavior fUnpremulBehavior;
+
+    SkEncodeOptions(SkEncodedImageFormat format)
+        : fUnpremulBehavior(SkTransferFunctionBehavior::kRespect)
+    {
+        switch (format) {
+            case SkEncodedImageFormat::kJPEG:
+                fFormat.fJPEG.fQuality = 100;
+                break;
+            case SkEncodedImageFormat::kWEBP:
+                fFormat.fWEBP.fQuality = 100.0f;
+                break;
+            default:
+                break;
+        }
+    }
+};
+
+/**
  * Encode SkPixmap in the given binary image format.
  *
  * @param  dst     results are written to this stream.
  * @param  src     source pixels.
  * @param  format  image format, not all formats are supported.
- * @param  quality range from 0-100, not all formats respect quality.
+ * @param  options switches to enable encoder-specific behaviors.
  *
  * @return false iff input is bad or format is unsupported.
  *
  * Will always return false if Skia is compiled without image
  * encoders.
- *
- * For examples of encoding an image to a file or to a block of memory,
- * see tools/sk_tool_utils.h.
  */
-SK_API bool SkEncodeImage(SkWStream* dst, const SkPixmap& src,
-                          SkEncodedImageFormat format, int quality);
-/**
- * The following helper function wraps SkEncodeImage().
- */
-inline bool SkEncodeImage(SkWStream* dst, const SkBitmap& src, SkEncodedImageFormat f, int q) {
+SK_API bool SkEncodeImage(SkWStream* dst, const SkPixmap& src, SkEncodedImageFormat format,
+                          const SkEncodeOptions& options);
+
+SK_API sk_sp<SkData> SkEncodeImage(const SkPixmap& src, SkEncodedImageFormat format,
+                                   const SkEncodeOptions& options);
+
+static inline bool SkEncodeImage(SkWStream* dst, const SkPixmap& src, SkEncodedImageFormat format,
+                                 int quality) {
+    SkEncodeOptions opts(format);
+    opts.fUnpremulBehavior = SkTransferFunctionBehavior::kIgnore;
+    switch (format) {
+        case SkEncodedImageFormat::kJPEG:
+            opts.fFormat.fJPEG.fQuality = quality;
+            break;
+        case SkEncodedImageFormat::kWEBP:
+            opts.fFormat.fWEBP.fQuality = (float) quality;
+            break;
+        default:
+            break;
+    }
+
+    return SkEncodeImage(dst, src, format, opts);
+}
+
+static inline bool SkEncodeImage(SkWStream* dst, const SkBitmap& src, SkEncodedImageFormat format,
+                                 int quality) {
     SkAutoLockPixels autoLockPixels(src);
     SkPixmap pixmap;
-    return src.peekPixels(&pixmap) && SkEncodeImage(dst, pixmap, f, q);
+    return src.peekPixels(&pixmap) && SkEncodeImage(dst, pixmap, format, quality);
 }
 
 #endif  // SkImageEncoder_DEFINED
