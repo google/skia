cc59084a2bf073a85c0f6ff43b7480faf8a611d3
Matt Sarett <msarett@google.com>
2017-04-20 18:17:08 -0400

    SkEncodeImage() API with options struct
    Bug: skia:
    Change-Id: Iab908b567df516a5806dbaeea904e977ec37f0ec

--- src/images/SkImageEncoder.cpp
+++ src/images/SkImageEncoder.cpp
@@ -7,23 +7,49 @@
 
 #include "SkImageEncoderPriv.h"
 
-bool SkEncodeImage(SkWStream* dst, const SkPixmap& src,
-                   SkEncodedImageFormat format, int quality) {
+bool SkEncodeImage(SkWStream* dst, const SkPixmap& src, SkEncodedImageFormat format,
+                   const SkEncodeOptions& options) {
+    if (SkTransferFunctionBehavior::kRespect == options.fUnpremulBehavior) {
+        if (!src.colorSpace() || (!src.colorSpace()->gammaCloseToSRGB() &&
+                                  !src.colorSpace()->gammaIsLinear())) {
+            return false;
+        }
+    }
+
     #ifdef SK_USE_CG_ENCODER
-        (void)quality;
         return SkEncodeImageWithCG(dst, src, format);
     #elif SK_USE_WIC_ENCODER
+        int quality = 100;
+        switch(format) {
+            case SkEncodedImageFormat::kJPEG:
+                quality = options.fFormat.fJPEG.fQuality;
+                break;
+            case SkEncodedImageFormat::kWEBP:
+                quality = (int) options.fFormat.fWEBP.fQuality;
+            default:
+                break;
+        }
         return SkEncodeImageWithWIC(dst, src, format, quality);
     #else
         switch(format) {
             case SkEncodedImageFormat::kJPEG:
-                return SkEncodeImageAsJPEG(dst, src, quality);
+                return SkEncodeImageAsJPEG(dst, src, options);
             case SkEncodedImageFormat::kPNG:
-                return SkEncodeImageAsPNG(dst, src, SkEncodeOptions());
+                return SkEncodeImageAsPNG(dst, src, options);
             case SkEncodedImageFormat::kWEBP:
-                return SkEncodeImageAsWEBP(dst, src, quality);
+                return SkEncodeImageAsWEBP(dst, src, options);
             default:
                 return false;
         }
     #endif
+}
+
+sk_sp<SkData> SkEncodeImage(const SkPixmap& src, SkEncodedImageFormat format,
+                            const SkEncodeOptions& options) {
+    SkDynamicMemoryWStream dst;
+    if (SkEncodeImage(&dst, src, format, options)) {
+        return dst.detachAsData();
+    }
+
+    return nullptr;
 }
