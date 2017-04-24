705638431c42a40fca71412f38e7d06168b8f8dd
Brian Osman <brianosman@google.com>
2017-04-24 13:08:49 -0400

    GrBackendTextureImageGenerator
    This allows the lazy creation of wrapped GPU images.
    
    Bug: skia:
    Change-Id: I6c37b12c8ab5bce94b91190e5f0beb91d31ae81b

---
+++ src/gpu/GrBackendTextureImageGenerator.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef GrBackendTextureImageGenerator_DEFINED
+#define GrBackendTextureImageGenerator_DEFINED
+
+#include "SkImageGenerator.h"
+#include "GrBackendSurface.h"
+
+class GrBackendTextureImageGenerator : public SkImageGenerator {
+public:
+
+    // These match the definitions in SkImage, whence they came
+    typedef void* ReleaseCtx;
+    typedef void (*ReleaseProc)(ReleaseCtx);
+
+    static std::unique_ptr<SkImageGenerator> Make(const GrBackendTexture&, GrSurfaceOrigin,
+                                                  SkAlphaType, sk_sp<SkColorSpace>,
+                                                  ReleaseProc, ReleaseCtx);
+    ~GrBackendTextureImageGenerator();
+
+protected:
+    bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes, SkPMColor ctable[],
+                     int* ctableCount) override;
+    bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes, const Options& opts)
+                     override;
+
+#if SK_SUPPORT_GPU
+    sk_sp<GrTextureProxy> onGenerateTexture(GrContext*, const SkImageInfo&,
+                                            const SkIPoint&) override;
+#endif
+
+private:
+    GrBackendTextureImageGenerator(const SkImageInfo& info, const GrBackendTexture&,
+                                   GrSurfaceOrigin, ReleaseProc, ReleaseCtx);
+
+    static void ReleaseRefHelper_TextureReleaseProc(ReleaseCtx ctx);
+
+    class RefHelper : public SkNVRefCnt<RefHelper> {
+    public:
+        RefHelper(ReleaseProc releaseProc, ReleaseCtx releaseCtx)
+            : fReleaseProc(releaseProc)
+            , fReleaseCtx(releaseCtx) { }
+
+        ~RefHelper();
+
+    private:
+        ReleaseProc fReleaseProc;
+        ReleaseCtx fReleaseCtx;
+    };
+
+    GrBackendTexture fTexture;
+    GrSurfaceOrigin fSurfaceOrigin;
+    RefHelper* fRefHelper;
+
+    typedef SkImageGenerator INHERITED;
+};
+#endif  // GrBackendTextureImageGenerator_DEFINED
