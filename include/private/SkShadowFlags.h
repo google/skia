6566a7a2feb1a6ffa852f27583f324bf0d577816
Jim Van Verth <jvanverth@google.com>
2017-04-28 17:30:30 -0400

    Optimizations for analytic shadows.
    Lots of changes here:
    * Batch circle shadows with rrect shadows
    * Avoid checking matrix and path conditions twice
    * Remove lots of checks for 1/2 pixel radii
      (needed before to force the rrect through the
       regular GPU path)
    * Fix scaling effect on ambient blur width
    * Remove unused flags
    
    Bug: skia:6119
    Change-Id: If0eb78ec4d19d9f978b19bdbc3a7e558a4db2ed9

--- include/private/SkShadowFlags.h
+++ include/private/SkShadowFlags.h
@@ -14,12 +14,8 @@ enum SkShadowFlags {
     /** The occluding object is not opaque. Knowing that the occluder is opaque allows
     * us to cull shadow geometry behind it and improve performance. */
     kTransparentOccluder_ShadowFlag = 0x01,
-    /** Use a larger umbra for a darker shadow */
-    kLargerUmbra_ShadowFlag = 0x02,
-    /** Use a Gaussian for the edge function rather than smoothstep */
-    kGaussianEdge_ShadowFlag = 0x04,
     /** mask for all shadow flags */
-    kAll_ShadowFlag = 0x07
+    kAll_ShadowFlag = 0x01
 };
 
 #endif
