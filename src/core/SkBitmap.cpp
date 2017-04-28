1884780a190589d2964e781a40a99d03d95de6fd
Ben Wagner <benjaminwagner@google.com>
2017-04-28 16:59:51 +0000

    Revert "Only store width and height on SkPixelRef"
    This reverts commit 2cbb6662e329981840f90ef4edd62f70f69e6030.
    
    Reason for revert: Likely cause of Chromium DEPS roll failure; speculative revert.
    
    Original change's description:
    > Only store width and height on SkPixelRef
    > 
    > Bug: skia:6535
    > Change-Id: Id91e8d1e82f593be7d4b23ca5abde752f2666a77
    > Reviewed-on: https://skia-review.googlesource.com/14105
    > Commit-Queue: Matt Sarett <msarett@google.com>
    > Reviewed-by: Mike Reed <reed@google.com>
    > 
    
    TBR=djsollen@google.com,msarett@google.com,reed@google.com,stani@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I12a024a71833f33432d5ea8cffdfc642b8b4240a

--- src/core/SkBitmap.cpp
+++ src/core/SkBitmap.cpp
@@ -171,6 +171,9 @@ bool SkBitmap::setAlphaType(SkAlphaType newAlphaType) {
     }
     if (fInfo.alphaType() != newAlphaType) {
         fInfo = fInfo.makeAlphaType(newAlphaType);
+        if (fPixelRef) {
+            fPixelRef->changeAlphaType(newAlphaType);
+        }
     }
     SkDEBUGCODE(this->validate();)
     return true;
@@ -195,15 +198,32 @@ void SkBitmap::setPixelRef(sk_sp<SkPixelRef> pr, int dx, int dy) {
 #ifdef SK_DEBUG
     if (pr) {
         if (kUnknown_SkColorType != fInfo.colorType()) {
-            SkASSERT(fInfo.width() <= pr->width());
-            SkASSERT(fInfo.height() <= pr->height());
+            const SkImageInfo& prInfo = pr->info();
+            SkASSERT(fInfo.width() <= prInfo.width());
+            SkASSERT(fInfo.height() <= prInfo.height());
+            SkASSERT(fInfo.colorType() == prInfo.colorType());
+            switch (prInfo.alphaType()) {
+                case kUnknown_SkAlphaType:
+                    SkASSERT(fInfo.alphaType() == kUnknown_SkAlphaType);
+                    break;
+                case kOpaque_SkAlphaType:
+                case kPremul_SkAlphaType:
+                    SkASSERT(fInfo.alphaType() == kOpaque_SkAlphaType ||
+                             fInfo.alphaType() == kPremul_SkAlphaType);
+                    break;
+                case kUnpremul_SkAlphaType:
+                    SkASSERT(fInfo.alphaType() == kOpaque_SkAlphaType ||
+                             fInfo.alphaType() == kUnpremul_SkAlphaType);
+                    break;
+            }
         }
     }
 #endif
 
     fPixelRef = std::move(pr);
     if (fPixelRef) {
-        fPixelRefOrigin.set(SkTPin(dx, 0, fPixelRef->width()), SkTPin(dy, 0, fPixelRef->height()));
+        const SkImageInfo& info = fPixelRef->info();
+        fPixelRefOrigin.set(SkTPin(dx, 0, info.width()), SkTPin(dy, 0, info.height()));
         this->updatePixelsFromRef();
     } else {
         // ignore dx,dy if there is no pixelref
@@ -657,9 +677,9 @@ bool SkBitmap::internalCopyTo(SkBitmap* dst, SkColorType dstColorType, Allocator
     //      if (src_pixelref->info == dst_pixelref->info)
     //
     if (srcPM.colorType() == dstColorType && tmpDst.getSize() == srcPM.getSize64()) {
-        if (tmpDst.info() == this->info() && tmpDst.pixelRef()->width() == fPixelRef->width() &&
-                tmpDst.pixelRef()->height() == fPixelRef->height()) {
-            tmpDst.pixelRef()->cloneGenID(*fPixelRef);
+        SkPixelRef* dstPixelRef = tmpDst.pixelRef();
+        if (dstPixelRef->info() == fPixelRef->info()) {
+            dstPixelRef->cloneGenID(*fPixelRef);
         }
     }
 
@@ -896,7 +916,7 @@ bool SkBitmap::ReadRawPixels(SkReadBuffer* buffer, SkBitmap* bitmap) {
     if (!pr) {
         return false;
     }
-    bitmap->setInfo(info);
+    bitmap->setInfo(pr->info());
     bitmap->setPixelRef(std::move(pr), 0, 0);
     return true;
 }
@@ -936,8 +956,8 @@ void SkBitmap::validate() const {
         SkASSERT(fPixelRef->rowBytes() == fRowBytes);
         SkASSERT(fPixelRefOrigin.fX >= 0);
         SkASSERT(fPixelRefOrigin.fY >= 0);
-        SkASSERT(fPixelRef->width() >= (int)this->width() + fPixelRefOrigin.fX);
-        SkASSERT(fPixelRef->height() >= (int)this->height() + fPixelRefOrigin.fY);
+        SkASSERT(fPixelRef->info().width() >= (int)this->width() + fPixelRefOrigin.fX);
+        SkASSERT(fPixelRef->info().height() >= (int)this->height() + fPixelRefOrigin.fY);
         SkASSERT(fPixelRef->rowBytes() >= fInfo.minRowBytes());
     }
 }
--- src/image/SkImage_Raster.cpp
+++ src/image/SkImage_Raster.cpp
@@ -28,7 +28,7 @@
 // fixes https://bug.skia.org/5096
 static bool is_not_subset(const SkBitmap& bm) {
     SkASSERT(bm.pixelRef());
-    SkISize dim = SkISize::Make(bm.pixelRef()->width(), bm.pixelRef()->height());
+    SkISize dim = bm.pixelRef()->info().dimensions();
     SkASSERT(dim != bm.dimensions() || bm.pixelRefOrigin().isZero());
     return dim == bm.dimensions();
 }
