c5709f3230a482a2e3bb59d7bc0db3b28a54948f
Eric Boren <borenet@google.com>
2017-04-28 15:48:47 -0400

    [recipes] Remove inputs to BundleRecipes task
    This should enable deduplication.
    
    Bug: skia:
    Change-Id: I2be98f648acd666c420885f08377c256f9102a8f

--- infra/bots/recipes/bundle_recipes.py
+++ infra/bots/recipes/bundle_recipes.py
@@ -18,14 +18,10 @@ DEPS = [
 
 
 def RunSteps(api):
-  api.core.setup()
+  api.vars.setup()
 
   bundle_dir = api.vars.swarming_out_dir.join('recipe_bundle')
   with api.step.context({'cwd': api.vars.skia_dir}):
-    if api.vars.is_trybot:
-      # Recipe bundling requires that any changes be committed.
-      api.run(api.step, 'Commit Patch', infra_step=True,
-              cmd=['git', 'commit', '-a', '-m', 'Commit Patch'])
     recipes_py = api.vars.infrabots_dir.join('recipes.py')
     api.run(api.step, 'Bundle Recipes', infra_step=True,
             cmd=['python', recipes_py, 'bundle', '--destination', bundle_dir])
@@ -37,18 +33,5 @@ def GenTests(api):
   yield (
     api.test('BundleRecipes') +
     api.properties(buildername='Housekeeper-PerCommit-BundleRecipes',
-                   repository='https://skia.googlesource.com/skia.git',
-                   revision='abc123',
-                   path_config='kitchen',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   patch_issue=500,
-                   patch_set=1,
-                   patch_storage='gerrit') +
-    api.properties.tryserver(
-        buildername='Housekeeper-PerCommit-BundleRecipes',
-        gerrit_project='skia',
-        gerrit_url='https://skia-review.googlesource.com/') +
-    api.path.exists(
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    )
+                   swarm_out_dir='[SWARM_OUT_DIR]')
   )
