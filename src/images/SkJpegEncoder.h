872e68292a2fe4c672e0152bfdc492b6f6948af0
Matt Sarett <msarett@google.com>
2017-04-28 14:47:15 -0400

    Add support for row-by-row jpeg encoding
    Bug: 713862
    Bug: skia:6409
    Change-Id: I787b7c49662a00b89ae0ef35845dfbd6be3e6fb1

---
+++ src/images/SkJpegEncoder.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkJpegEncoder_DEFINED
+#define SkJpegEncoder_DEFINED
+
+#include "SkTypes.h"
+
+class SkJpegEncoder : SkNoncopyable {
+public:
+
+    // TODO (skbug.com/1501):
+    // Since jpegs are always opaque, this encoder ignores the alpha channel and treats the
+    // pixels as opaque.
+    // Another possible behavior is to blend the pixels onto opaque black.  We'll need to add
+    // an option for this - and an SkTransferFunctionBehavior.
+
+    struct Options {
+        /**
+         * |fQuality| must be in [0, 100] where 0 corresponds to the lowest quality.
+         */
+        int fQuality = 100;
+    };
+
+    /**
+     *  Encode the |src| pixels to the |dst| stream.
+     *  |options| may be used to control the encoding behavior.
+     *
+     *  Returns true on success.  Returns false on an invalid or unsupported |src|.
+     */
+    static bool Encode(SkWStream* dst, const SkPixmap& src, const Options& options);
+
+    /**
+     *  Create a jpeg encoder that will encode the |src| pixels to the |dst| stream.
+     *  |options| may be used to control the encoding behavior.
+     *
+     *  |dst| is unowned but must remain valid for the lifetime of the object.
+     *
+     *  This returns nullptr on an invalid or unsupported |src|.
+     */
+    static std::unique_ptr<SkJpegEncoder> Make(SkWStream* dst, const SkPixmap& src,
+                                               const Options& options);
+
+    /**
+     *  Encode |numRows| rows of input.  Returns true on success.  If the caller requests more
+     *  rows than are remaining in the src, this will silently encode all of the remaining rows.
+     *
+     *  Returns false on failure.  This should only fail if numRows <= 0 or if the encode is
+     *  already complete.
+     */
+    bool encodeRows(int numRows);
+};
+
+#endif
