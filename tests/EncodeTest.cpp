a9b18327dea6f16e33c55971e2a118ace106fa26
Matt Sarett <msarett@google.com>
2017-05-01 16:14:16 -0400

    Add support for row-by-row jpeg encoding
    Bug: 713862
    Change-Id: I787b7c49662a00b89ae0ef35845dfbd6be3e6fb1

---
+++ tests/EncodeTest.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "Resources.h"
+#include "Test.h"
+
+#include "SkBitmap.h"
+#include "SkJpegEncoder.h"
+#include "SkStream.h"
+
+DEF_TEST(Encode_Jpeg, r) {
+    SkBitmap bitmap;
+    bool success = GetResourceAsBitmap("mandrill_128.png", &bitmap);
+    if (!success) {
+        return;
+    }
+
+    SkPixmap src;
+    success = bitmap.peekPixels(&src);
+    REPORTER_ASSERT(r, success);
+    if (!success) {
+        return;
+    }
+
+    SkDynamicMemoryWStream dst0, dst1, dst2, dst3;
+    SkJpegEncoder::Encode(&dst0, src, SkJpegEncoder::Options());
+
+    auto encoder1 = SkJpegEncoder::Make(&dst1, src, SkJpegEncoder::Options());
+    for (int i = 0; i < src.height(); i++) {
+        encoder1->encodeRows(1);
+    }
+
+    auto encoder2 = SkJpegEncoder::Make(&dst2, src, SkJpegEncoder::Options());
+    for (int i = 0; i < src.height(); i+=3) {
+        encoder2->encodeRows(3);
+    }
+
+    auto encoder3 = SkJpegEncoder::Make(&dst3, src, SkJpegEncoder::Options());
+    encoder3->encodeRows(200);
+
+    sk_sp<SkData> data0 = dst0.detachAsData();
+    sk_sp<SkData> data1 = dst1.detachAsData();
+    sk_sp<SkData> data2 = dst2.detachAsData();
+    sk_sp<SkData> data3 = dst3.detachAsData();
+    REPORTER_ASSERT(r, data0->equals(data1.get()));
+    REPORTER_ASSERT(r, data0->equals(data2.get()));
+    REPORTER_ASSERT(r, data0->equals(data3.get()));
+}
