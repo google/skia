0d4f6a1b4407eaa340ba530bf24ed41d7204dd63
Mike Klein <mtklein@chromium.org>
2017-05-01 21:34:14 +0000

    Revert "Revert "SaveLayerRec::fClipMask -> raw pointer""
    This reverts commit 3354969a4a4adcea159f3a0b01ad0a7c04fc9115.
    
    Reason for revert: not the problem?
    
    Original change's description:
    > Revert "SaveLayerRec::fClipMask -> raw pointer"
    > 
    > This reverts commit a6b72cb5729306fdd676d739c1e2c53afa0786a7.
    > 
    > Reason for revert: red bots
    > 
    > Original change's description:
    > > SaveLayerRec::fClipMask -> raw pointer
    > > 
    > > Use raw pointers for optional clip mask plumbing, to match the backdrop
    > > API.
    > > 
    > > Change-Id: I7eb0ee5896faf34cc05789ba0703f35a4ab6a4f2
    > > Reviewed-on: https://skia-review.googlesource.com/14901
    > > Reviewed-by: Mike Reed <reed@google.com>
    > > Commit-Queue: Florin Malita <fmalita@chromium.org>
    > > 
    > 
    > TBR=fmalita@chromium.org,reed@google.com
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > 
    > Change-Id: I4387620cdc3410018af9cef221e5cf8d09015380
    > Reviewed-on: https://skia-review.googlesource.com/14955
    > Reviewed-by: Mike Klein <mtklein@chromium.org>
    > Commit-Queue: Mike Klein <mtklein@chromium.org>
    > 
    
    TBR=mtklein@chromium.org,reviews@skia.org,fmalita@chromium.org,reed@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I6dde9e0441928f7a7423139a5c39f520f892f4b5

--- src/core/SkLiteDL.h
+++ src/core/SkLiteDL.h
@@ -29,7 +29,7 @@ public:
 #endif
 
     void save();
-    void saveLayer(const SkRect*, const SkPaint*, const SkImageFilter*, sk_sp<SkImage>,
+    void saveLayer(const SkRect*, const SkPaint*, const SkImageFilter*, const SkImage*,
                    const SkMatrix*, SkCanvas::SaveLayerFlags);
     void restore();
 
