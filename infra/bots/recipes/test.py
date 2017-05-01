2005d77c01a04ccc02173db812d1b1bbc7c1ce65
Leon Scroggins III <scroggo@google.com>
2017-05-01 13:09:42 -0400

    Blacklist SkImage_makeTextureImage on NVIDIA Win10 Vulkan
    Bug: skia:6554
    Change-Id: I7c1086ce6afff84c1b1463ec58c2c6a9949aaa2e
    Reviewed-on: https://skia-review.googlesource.com/14907
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Commit-Queue: Leon Scroggins <scroggo@google.com>

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -510,6 +510,9 @@ def dm_flags(bot):
     match.append('~PathOpsSimplify') # skia:6479
     blacklist(['_', 'gm', '_', 'fast_slow_blurimagefilter']) # skia:6480
 
+  if ('Win10' in bot and 'Vulkan' in bot
+      and ('GTX1070' in bot or 'GTX660' in bot)):
+    blacklist('_ test _ SkImage_makeTextureImage') # skia:6554
 
   if blacklisted:
     args.append('--blacklist')
