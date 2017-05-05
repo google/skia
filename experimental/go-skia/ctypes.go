4a6fcfd0c3c997c92fe98a504fcaf2b0e63e8a09
Stephan Altmueller <stephana@google.com>
2017-05-05 16:29:22 -0400

    WIP: golang bindings
    BUG=skia:
    
    Change-Id: I96ff7c3715164c533202ce300ab0312b1b07f884

---
+++ experimental/go-skia/ctypes.go
@@ -0,0 +1,51 @@
+//+build ignore
+
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package skia
+
+/*
+#cgo CFLAGS: -I../../include/c
+#include "../../include/c/sk_types.h"
+*/
+import "C"
+
+type Color C.sk_color_t
+
+type ColorType C.sk_colortype_t
+
+const (
+	UNKNOWN_COLORTYPE   ColorType = C.UNKNOWN_SK_COLORTYPE
+	RGBA_8888_COLORTYPE ColorType = C.RGBA_8888_SK_COLORTYPE
+	BGRA_8888_COLORTYPE ColorType = C.BGRA_8888_SK_COLORTYPE
+	ALPHA_8_COLORTYPE   ColorType = C.ALPHA_8_SK_COLORTYPE
+)
+
+type AlphaType C.sk_alphatype_t
+
+const (
+	OPAQUE_ALPHATYPE   AlphaType = C.OPAQUE_SK_ALPHATYPE
+	PREMUL_ALPHATYPE   AlphaType = C.PREMUL_SK_ALPHATYPE
+	UNPREMUL_ALPHATYPE AlphaType = C.UNPREMUL_SK_ALPHATYPE
+)
+
+type PixelGeometry C.sk_pixelgeometry_t
+
+const (
+	UNKNOWN_SK_PIXELGEOMETRY PixelGeometry = C.UNKNOWN_SK_PIXELGEOMETRY
+	RGB_H_SK_PIXELGEOMETRY   PixelGeometry = C.RGB_H_SK_PIXELGEOMETRY
+	BGR_H_SK_PIXELGEOMETRY   PixelGeometry = C.BGR_H_SK_PIXELGEOMETRY
+	RGB_V_SK_PIXELGEOMETRY   PixelGeometry = C.RGB_V_SK_PIXELGEOMETRY
+	BGR_V_SK_PIXELGEOMETRY   PixelGeometry = C.BGR_V_SK_PIXELGEOMETRY
+)
+
+type ImageInfo C.sk_imageinfo_t
+
+type SurfaceProps C.sk_surfaceprops_t
+
+type Rect C.sk_rect_t
