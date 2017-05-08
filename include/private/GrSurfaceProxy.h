15853bd0ee3026c6230a000783061e91393a56e5
Robert Phillips <robertphillips@google.com>
2017-05-08 18:08:01 +0000

    Revert "Revert "Re-enable deferred proxies in Chrome""
    This reverts commit 05814de6ba5087ad71f189d6413246ef1d518e4b.
    
    Reason for revert: Chrome layout test suppressions have landed
    
    Original change's description:
    > Revert "Re-enable deferred proxies in Chrome"
    > 
    > This reverts commit 555c49c3479edf1692579c06a5c13c39d3741692.
    > 
    > Reason for revert: layout tests
    > 
    > Original change's description:
    > > Re-enable deferred proxies in Chrome
    > > 
    > > Now that https://skia-review.googlesource.com/c/14605/ (Allow TextureSamplers to have null GrTexture pointer) has landed we should be able to re-enable deferred proxies in Chrome.
    > > 
    > > Bug: 715488
    > > Change-Id: Ibd38e5ddf0558b94f09e29f17837e6abfd22fa34
    > > Reviewed-on: https://skia-review.googlesource.com/15252
    > > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > > Commit-Queue: Robert Phillips <robertphillips@google.com>
    > > 
    > 
    > TBR=bsalomon@google.com,robertphillips@google.com
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > 
    > Change-Id: Ie90e64a2e4d5efc4b6909a04f8a862dd3a708d97
    > Reviewed-on: https://skia-review.googlesource.com/15809
    > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > Commit-Queue: Robert Phillips <robertphillips@google.com>
    > 
    
    TBR=bsalomon@google.com,robertphillips@google.com,reviews@skia.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: I957e8e59af2b2f441c75bae2eda4dcd5ed82b34d

--- include/private/GrSurfaceProxy.h
+++ include/private/GrSurfaceProxy.h
@@ -24,8 +24,6 @@ class GrSurfaceProxyPriv;
 class GrTextureOpList;
 class GrTextureProxy;
 
-//#define SK_DISABLE_DEFERRED_PROXIES 1
-
 // This class replicates the functionality GrIORef<GrSurface> but tracks the
 // utilitization for later resource allocation (for the deferred case) and
 // forwards on the utilization in the wrapped case
