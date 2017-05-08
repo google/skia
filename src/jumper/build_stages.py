892af1ec7bb0deb5f40557a0d4838df9dd74a0b6
Mike Klein <mtklein@chromium.org>
2017-05-08 15:17:57 -0400

    add a guide to contributing to SkJumper
    Change-Id: Icd40cf5eff3d2156a3ca00d7950059d5b77f48bf
    Reviewed-on: https://skia-review.googlesource.com/15890
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>

--- src/jumper/build_stages.py
+++ src/jumper/build_stages.py
@@ -11,8 +11,9 @@ import sys
 
 clang   = sys.argv[1] if len(sys.argv) > 1 else 'clang-4.0'
 objdump = sys.argv[2] if len(sys.argv) > 2 else 'gobjdump'
+ccache  = sys.argv[3] if len(sys.argv) > 3 else 'ccache'
 
-clang = ['ccache', clang, '-x', 'c++']
+clang = [ccache, clang, '-x', 'c++']
 
 
 cflags = ['-std=c++11', '-Os', '-DJUMPER',
