5fe0e1cc6a3f197c4aec1474ce5ff8aa506668a1
Brian Osman <brianosman@google.com>
2017-05-08 11:07:42 -0400

    SkImage::isValid
    Lets clients know if an image is drawable to a particular GrContext
    (or to CPU). Checks for abandoned GrContexts beneath GPU backed
    images, as well as context mis-match.
    
    Bug: skia:
    Change-Id: Ibe88c7ce8091f965c14f6023a3597be4b70c3f99

--- include/core/SkImageGenerator.h
+++ include/core/SkImageGenerator.h
@@ -51,6 +51,14 @@ public:
     const SkImageInfo& getInfo() const { return fInfo; }
 
     /**
+     *  Can this generator be used to produce images that will be drawable to the specified context
+     *  (or to CPU, if context is nullptr)?
+     */
+    bool isValid(GrContext* context) const {
+        return this->onIsValid(context);
+    }
+
+    /**
      *  Decode into the given pixels, a block of memory of size at
      *  least (info.fHeight - 1) * rowBytes + (info.fWidth *
      *  bytesPerPixel)
@@ -165,6 +173,8 @@ protected:
 
     virtual bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes,
                              SkPMColor ctable[], int* ctableCount);
+
+    virtual bool onIsValid(GrContext*) const;
 
     virtual bool onQueryYUV8(SkYUVSizeInfo*, SkYUVColorSpace*) const {
         return false;
