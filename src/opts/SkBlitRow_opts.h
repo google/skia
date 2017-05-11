cb2f18af0ac87c34e010a0d3b695ed5911735893
Matteo Franchin <matteo.franchin@arm.com>
2017-05-11 18:03:52 +0100

    Faster NEON implementation of blit_row_s32a_opaque
    Add a NEON implementation of alpha blending which works on 8 pixels at
    a time. Note that a number of variations of this code were considered.
    Here are some notes:
    
    - A 16 pixels at a time version was considered. This performs well for
      the case of extreme alpha (all-opaque or all-transparent pixels), but
      performs worst than the 8 pixels version when there are frequent
      transitions of alpha. Also gcc 6.2.1 seems to have troubles with
      register pressure when using this version.
    
    - If the branch to detect the fully-opaque or fully-transparent cases
      is removed, then the performance increases significantly for images
      which are all partially transparent (especially on ARM Cortex A72),
      but can significantly decrease for images that are almost fully
      opaque or fully transparent.
    
    - An alternative blending employing SkMulDiv255Round was tested. This
      turned out to be slower than the version using the SkPMSrcOver
      approach.
    
    This implementation is a compromise to the effects described above.
    This patch produces a ~10% improvements on the nanobench's sub-scores
    repeatTile_BGRA_8888_A, constXTile_MM_filter_trans, constXTile_CC_trans,
    constXTile_RR_filter_trans when running on ARM Cortex A72. Improvements
    of greater magnitude (30% to 50%) are observed when running on ARM
    Cortex A53.
    
    CQ_INCLUDE_TRYBOTS=skia.primary:Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Release-SKNX_NO_SIMD
    Change-Id: I1f0c9f549057613bbffd26e6651f3beeb0019af9
    Bug: skia:

--- src/opts/SkBlitRow_opts.h
+++ src/opts/SkBlitRow_opts.h
@@ -142,50 +142,65 @@ void blit_row_s32a_opaque(SkPMColor* dst, const SkPMColor* src, int len, U8CPU a
     }
 
 #elif defined(SK_ARM_HAS_NEON)
-    while (len >= 4) {
-        if ((src[0] | src[1] | src[2] | src[3]) == 0x00000000) {
-            // All 16 source pixels are transparent.  Nothing to do.
-            src += 4;
-            dst += 4;
-            len -= 4;
+    // The implementation below performs alpha blending consistently with SkPMSrcOver, which at the
+    // time of writing computes the color components (numbers in the interval [0, 255]) as:
+    //
+    //   result_i = src_i + trunc(f(src_alpha, dst_i))
+    //
+    // where, f(x, y) = ((256 - x) * y) / 256. A better approximation could be obtained using,
+    // g(x, y) = ((255 - x) * y) / 255 and rounding to the nearest integer, rather than towards
+    // zero (using libm names, this means rint rather than trunc). f is used as it is typically
+    // quicker to compute than g. Some quick math shows that f = g + error, where
+    // error = x * y / (255 * 256). In the worst case, error ~< 1.0, but it should be noted that
+    // the truncation toward zero is working against the error (e.g. take the case x = y = 255).
+    // In practice, the two formulas give different results in 28.6% of all the combinations of
+    // (x, y). This difference is either +1 (2.25%) or -1 (26.37%), meaning that the formula using
+    // f tends to give lower values than the one based on g.
+
+    // Do 8-pixels at a time. This seems a good compromise as it shows performance improvements for
+    // a variety of input data when compared to the 4-pixels code below. A 16-pixels at a time
+    // version of this code was also tested, but it underperformed on some of the platforms under
+    // test for inputs with frequent transitions of alpha (corresponding to changes of the
+    // conditions [~]alpha_u64 == 0 below). It may be worth revisiting the situation in the future.
+    while (len >= 8) {
+        // Load 8 pixels in 4 NEON registers. src_col.val[i] will contain the same color component
+        // for 8 consecutive pixels (e.g. src_col.val[3] will contain all alpha components of 8
+        // pixels).
+        uint8x8x4_t src_col = vld4_u8(reinterpret_cast<uint8_t const*>(src));
+        src += 8;
+        len -= 8;
+
+        // We now detect 2 special cases: the first occurs when all alphas are zero (the 8 pixels
+        // are all transparent), the second when all alphas are fully set (they are all opaque).
+        uint8x8_t alphas = src_col.val[3];
+        uint64_t alphas_u64 = vget_lane_u64(vreinterpret_u64_u8(alphas), 0);
+        if (alphas_u64 == 0) {
+            // All pixels transparent.
+            dst += 8;
             continue;
         }
 
-        if ((src[0] & src[1] & src[2] & src[3]) >= 0xFF000000) {
-            // All 16 source pixels are opaque.  SrcOver becomes Src.
-            dst[0] = src[0];
-            dst[1] = src[1];
-            dst[2] = src[2];
-            dst[3] = src[3];
-            src += 4;
-            dst += 4;
-            len -= 4;
+        if ((~alphas_u64) == 0) {
+            // All pixels opaque.
+            vst4_u8(reinterpret_cast<uint8_t*>(dst), src_col);
+            dst += 8;
             continue;
         }
 
-        // Load 4 source and destination pixels.
-        auto src0 = vreinterpret_u8_u32(vld1_u32(src+0)),
-             src2 = vreinterpret_u8_u32(vld1_u32(src+2)),
-             dst0 = vreinterpret_u8_u32(vld1_u32(dst+0)),
-             dst2 = vreinterpret_u8_u32(vld1_u32(dst+2));
+        uint8x8x4_t dst_col = vld4_u8(reinterpret_cast<uint8_t*>(dst));
+        uint16x8_t nalphas = vsubw_u8(vdupq_n_u16(256), alphas);
 
-        // TODO: This math is wrong.
-        const uint8x8_t alphas = vcreate_u8(0x0707070703030303);
-        auto invSA0_w = vsubw_u8(vdupq_n_u16(256), vtbl1_u8(src0, alphas)),
-             invSA2_w = vsubw_u8(vdupq_n_u16(256), vtbl1_u8(src2, alphas));
-
-        auto dstInvSA0 = vmulq_u16(invSA0_w, vmovl_u8(dst0)),
-             dstInvSA2 = vmulq_u16(invSA2_w, vmovl_u8(dst2));
-
-        dst0 = vadd_u8(src0, vshrn_n_u16(dstInvSA0, 8));
-        dst2 = vadd_u8(src2, vshrn_n_u16(dstInvSA2, 8));
-
-        vst1_u32(dst+0, vreinterpret_u32_u8(dst0));
-        vst1_u32(dst+2, vreinterpret_u32_u8(dst2));
-
-        src += 4;
-        dst += 4;
-        len -= 4;
+        uint8x8x4_t result;
+        result.val[0] = vadd_u8(src_col.val[0],
+                                vshrn_n_u16(vmulq_u16(nalphas,  vmovl_u8(dst_col.val[0])), 8));
+        result.val[1] = vadd_u8(src_col.val[1],
+                                vshrn_n_u16(vmulq_u16(nalphas,  vmovl_u8(dst_col.val[1])), 8));
+        result.val[2] = vadd_u8(src_col.val[2],
+                                vshrn_n_u16(vmulq_u16(nalphas,  vmovl_u8(dst_col.val[2])), 8));
+        result.val[3] = vadd_u8(src_col.val[3],
+                                vshrn_n_u16(vmulq_u16(nalphas,  vmovl_u8(dst_col.val[3])), 8));
+        vst4_u8(reinterpret_cast<uint8_t*>(dst), result);
+        dst += 8;
     }
 #endif
 
