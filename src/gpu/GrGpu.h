32c6756f9ec0be3e115f762b29dc4199bcb8d42e
Robert Phillips <robertphillips@google.com>
2017-05-15 12:52:27 -0400

    Rename finishOpList to finishFlush and call it less frequently
    It is expected that this will fix the Vulkan perf regression associated with splitting up the opLists
    
    Change-Id: I1bbb6f71745bb08d4a8c2f7810e5ab84cca75e84

--- src/gpu/GrGpu.h
+++ src/gpu/GrGpu.h
@@ -378,7 +378,7 @@ public:
 
     // Called by GrOpList when flushing.
     // Provides a hook for post-flush actions (e.g. Vulkan command buffer submits).
-    virtual void finishOpList() {}
+    virtual void finishFlush() {}
 
     virtual GrFence SK_WARN_UNUSED_RESULT insertFence() = 0;
     virtual bool waitFence(GrFence, uint64_t timeout = 1000) = 0;
