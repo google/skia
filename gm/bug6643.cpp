644328f7f8eda2d3921f6e7f568b23315757d184
Mike Klein <mtklein@chromium.org>
2017-05-16 12:03:15 -0400

    clamp to premul in dither
    Dither can bump color values above alpha (duh), or below
    zero (duh), so clamp back to premul after dithering.
    
    BUG=skia:6644,skia:6643
    
    Change-Id: Ida107e866380e06130af0d01467117bca929ba44

---
+++ gm/bug6643.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkGradientShader.h"
+#include "SkPictureRecorder.h"
+#include "gm.h"
+
+DEF_SIMPLE_GM(bug6643, canvas, 200, 200) {
+    SkColor colors[] = { SK_ColorTRANSPARENT, SK_ColorGREEN, SK_ColorTRANSPARENT };
+
+    SkPaint p;
+    p.setAntiAlias(true);
+    p.setShader(SkGradientShader::MakeSweep(100, 100, colors, nullptr, SK_ARRAY_COUNT(colors),
+                                            SkGradientShader::kInterpolateColorsInPremul_Flag,
+                                            nullptr));
+
+    SkPictureRecorder recorder;
+    recorder.beginRecording(200, 200)->drawPaint(p);
+
+    p.setShader(SkShader::MakePictureShader(recorder.finishRecordingAsPicture(),
+                                            SkShader::kRepeat_TileMode, SkShader::kRepeat_TileMode,
+                                            nullptr, nullptr));
+    canvas->drawColor(SK_ColorWHITE);
+    canvas->drawPaint(p);
+}
