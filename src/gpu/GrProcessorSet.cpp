3ea0ece0efb8497795a993c3e88a7b5ed80cc8a1
Brian Salomon <bsalomon@google.com>
2017-05-17 09:33:09 -0400

    Remove static initializer of GrProcessorSet::gEmpty using a constexpr constructor.
    Change-Id: I9d4cb34c54458ad42a413f251d1018217de26b90

--- src/gpu/GrProcessorSet.cpp
+++ src/gpu/GrProcessorSet.cpp
@@ -11,8 +11,6 @@
 #include "GrXferProcessor.h"
 #include "effects/GrPorterDuffXferProcessor.h"
 
-const GrProcessorSet GrProcessorSet::gEmpty{GrProcessorSet::Empty::kEmpty};
-
 GrProcessorSet::GrProcessorSet(GrPaint&& paint) : fXP(paint.getXPFactory()) {
     fFlags = 0;
     if (paint.numColorFragmentProcessors() <= kMaxColorProcessors) {
