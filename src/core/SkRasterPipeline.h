8ad1339bf3640cf49843eeb8d4a709b4f27477e8
Mike Reed <reed@google.com>
2017-05-19 06:56:32 -0400

    stage version of vertices
    This CL, just to limit its size/complexity, only handles
    colors but not textures. Future CLs will cover everything.
    
    Performance is pretty exciting. Its faster than the old code-path,
    and when we fix a bug in pathutils to preserve opaqueness, it gets
    a lot faster (8 -> 5)
    
    Bug: skia:
    Change-Id: I4113060e25fe25fe4e6a0ea59bd4fa5e33abc668

--- src/core/SkRasterPipeline.h
+++ src/core/SkRasterPipeline.h
@@ -82,7 +82,7 @@
     M(exclusion) M(hardlight) M(lighten) M(overlay) M(softlight) \
     M(hue) M(saturation) M(color) M(luminosity)                  \
     M(luminance_to_alpha)                                        \
-    M(matrix_2x3) M(matrix_3x4) M(matrix_4x5)                    \
+    M(matrix_2x3) M(matrix_3x4) M(matrix_4x5) M(matrix_4x3)      \
     M(matrix_perspective)                                        \
     M(parametric_r) M(parametric_g) M(parametric_b)              \
     M(parametric_a)                                              \
