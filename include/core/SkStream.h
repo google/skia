c7e9bf21da669c99f27046358a4bacc55947a384
Hal Canary <halcanary@google.com>
2017-05-23 11:47:05 -0400

    SkStream: DynamicMemoryWStream gets writeToAndReset
    This is similar to copyToAndReset().
    
    Also use this in SkPDF, for minor memory savings.
    
    Change-Id: I352a980e6dd54eb05d74cd057bd50e02312753b0

--- include/core/SkStream.h
+++ include/core/SkStream.h
@@ -384,10 +384,13 @@ public:
 
     /** More efficient version of read(dst, 0, bytesWritten()). */
     void copyTo(void* dst) const;
-    void writeToStream(SkWStream* dst) const;
+    bool writeToStream(SkWStream* dst) const;
 
     /** Equivalent to copyTo() followed by reset(), but may save memory use. */
     void copyToAndReset(void* dst);
+
+    /** Equivalent to writeToStream() followed by reset(), but may save memory use. */
+    bool writeToAndReset(SkWStream* dst);
 
     /** Return the contents as SkData, and then reset the stream. */
     sk_sp<SkData> detachAsData();
