09fc4d9a1bb8370e494bf35e14882f307c8f8d42
Mike Klein <mtklein@chromium.org>
2017-05-23 18:14:01 +0000

    Revert "add knob to turn off fancy SkJumper features"
    This reverts commit 5373609d90d8f84b75718b15f3522f9d2f4226cb.
    
    Reason for revert: doesn't look like we'll need this.
    
    Original change's description:
    > add knob to turn off fancy SkJumper features
    > 
    > This is a new public API for testing (layout tests).
    > 
    > Change-Id: I10345231bad373c741b1e9656e546000538121b3
    > Reviewed-on: https://skia-review.googlesource.com/17712
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: Mike Klein <mtklein@chromium.org>
    > 
    
    TBR=mtklein@chromium.org,fmalita@chromium.org,reed@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: Ieed2576d7fc06528384b7476508610e0e29b894f

--- src/jumper/SkJumper.cpp
+++ src/jumper/SkJumper.cpp
@@ -114,8 +114,6 @@ static const SkJumper_Engine kPortable = {
 static SkJumper_Engine gPlatform = kPortable;
 static SkOnce gChooseEngineOnce;
 
-bool gSkJumperEnableFancyFeatures = true;
-
 static SkJumper_Engine choose_engine() {
 #if __has_feature(memory_sanitizer)
     // We'll just run portable code.
@@ -139,7 +137,7 @@ static SkJumper_Engine choose_engine() {
     }
 
 #elif defined(__x86_64__) || defined(_M_X64)
-    if (gSkJumperEnableFancyFeatures && SkCpu::Supports(SkCpu::HSW)) {
+    if (1 && SkCpu::Supports(SkCpu::HSW)) {
         return {
         #define M(stage) ASM(stage, hsw),
             { SK_RASTER_PIPELINE_STAGES(M) },
@@ -147,7 +145,7 @@ static SkJumper_Engine choose_engine() {
         #undef M
         };
     }
-    if (gSkJumperEnableFancyFeatures && SkCpu::Supports(SkCpu::AVX)) {
+    if (1 && SkCpu::Supports(SkCpu::AVX)) {
         return {
         #define M(stage) ASM(stage, avx),
             { SK_RASTER_PIPELINE_STAGES(M) },
@@ -155,7 +153,7 @@ static SkJumper_Engine choose_engine() {
         #undef M
         };
     }
-    if (gSkJumperEnableFancyFeatures && SkCpu::Supports(SkCpu::SSE41)) {
+    if (1 && SkCpu::Supports(SkCpu::SSE41)) {
         return {
         #define M(stage) ASM(stage, sse41),
             { SK_RASTER_PIPELINE_STAGES(M) },
