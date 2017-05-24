f37420a8c51a784028e2f8ac2f9a1dfa943d6af2
Brian Salomon <bsalomon@google.com>
2017-05-24 10:07:30 -0400

    Clear atlas textures at creation in Chrome
    Bug: chromium:656320
    Change-Id: Ia65274aa733f199be188579821e745920493aefc

--- include/gpu/GrCaps.h
+++ include/gpu/GrCaps.h
@@ -176,6 +176,8 @@ public:
     /** True in environments that will issue errors if memory uploaded to buffers
         is not initialized (even if not read by draw calls). */
     bool mustClearUploadedBufferData() const { return fMustClearUploadedBufferData; }
+    /** True when there is a performance cost to using an atlas that has not been cleared. */
+    bool mustClearAtlases() const { return fMustClearAtlases; }
 
     bool wireframeMode() const { return fWireframeMode; }
 
@@ -219,6 +221,7 @@ protected:
     bool fPreferClientSideDynamicBuffers             : 1;
     bool fFullClearIsFree                            : 1;
     bool fMustClearUploadedBufferData                : 1;
+    bool fMustClearAtlases                           : 1;
 
     // Driver workaround
     bool fUseDrawInsteadOfClear                      : 1;
