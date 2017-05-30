1658c48fb08c47db1280f7a820dab0012fcd08ab
Mike Reed <reed@google.com>
2017-05-30 14:25:24 -0400

    move perlinenoise2 into effects
    Bug: skia:
    Change-Id: I5c178bdc5901d15c6924b3fb1f29119ab3cc701d

--- include/effects/SkPerlinNoiseShader.h
+++ include/effects/SkPerlinNoiseShader.h
@@ -8,10 +8,7 @@
 #ifndef SkPerlinNoiseShader_DEFINED
 #define SkPerlinNoiseShader_DEFINED
 
-#include "SkShader.h"
-
-struct SkISize;
-class SkShader;
+#include "../../src/core/SkShaderBase.h"
 
 /** \class SkPerlinNoiseShader
 
@@ -25,8 +22,31 @@ class SkShader;
     The algorithm used is described here :
     http://www.w3.org/TR/SVG/filters.html#feTurbulenceElement
 */
-class SK_API SkPerlinNoiseShader {
+class SK_API SkPerlinNoiseShader : public SkShaderBase {
 public:
+    struct StitchData;
+    struct PaintingData;
+
+    /**
+     *  About the noise types : the difference between the first 2 is just minor tweaks to the
+     *  algorithm, they're not 2 entirely different noises. The output looks different, but once the
+     *  noise is generated in the [1, -1] range, the output is brought back in the [0, 1] range by
+     *  doing :
+     *  kFractalNoise_Type : noise * 0.5 + 0.5
+     *  kTurbulence_Type   : abs(noise)
+     *  Very little differences between the 2 types, although you can tell the difference visually.
+     *  "Improved" is based on the Improved Perlin Noise algorithm described at
+     *  http://mrl.nyu.edu/~perlin/noise/. It is quite distinct from the other two, and the noise is
+     *  a 2D slice of a 3D noise texture. Minor changes to the Z coordinate will result in minor
+     *  changes to the noise, making it suitable for animated noise.
+     */
+    enum Type {
+        kFractalNoise_Type,
+        kTurbulence_Type,
+        kImprovedNoise_Type,
+        kFirstType = kFractalNoise_Type,
+        kLastType = kImprovedNoise_Type
+    };
     /**
      *  This will construct Perlin noise of the given type (Fractal Noise or Turbulence).
      *
@@ -43,15 +63,74 @@ public:
      */
     static sk_sp<SkShader> MakeFractalNoise(SkScalar baseFrequencyX, SkScalar baseFrequencyY,
                                             int numOctaves, SkScalar seed,
-                                            const SkISize* tileSize = nullptr);
+                                            const SkISize* tileSize = NULL);
     static sk_sp<SkShader> MakeTurbulence(SkScalar baseFrequencyX, SkScalar baseFrequencyY,
                                           int numOctaves, SkScalar seed,
-                                          const SkISize* tileSize = nullptr);
+                                          const SkISize* tileSize = NULL);
+    /**
+     * Creates an Improved Perlin Noise shader. The z value is roughly equivalent to the seed of the
+     * other two types, but minor variations to z will only slightly change the noise.
+     */
+    static sk_sp<SkShader> MakeImprovedNoise(SkScalar baseFrequencyX, SkScalar baseFrequencyY,
+                                             int numOctaves, SkScalar z);
+    /**
+     * Create alias for CreateTurbulunce until all Skia users changed
+     * its code to use the new naming
+     */
+    static sk_sp<SkShader> MakeTubulence(SkScalar baseFrequencyX, SkScalar baseFrequencyY,
+                                         int numOctaves, SkScalar seed,
+                                         const SkISize* tileSize = NULL) {
+        return MakeTurbulence(baseFrequencyX, baseFrequencyY, numOctaves, seed, tileSize);
+    }
 
-    SK_DECLARE_FLATTENABLE_REGISTRAR_GROUP()
+    class PerlinNoiseShaderContext : public Context {
+    public:
+        PerlinNoiseShaderContext(const SkPerlinNoiseShader& shader, const ContextRec&);
+        ~PerlinNoiseShaderContext() override;
+
+        void shadeSpan(int x, int y, SkPMColor[], int count) override;
+
+    private:
+        SkPMColor shade(const SkPoint& point, StitchData& stitchData) const;
+        SkScalar calculateTurbulenceValueForPoint(
+            int channel,
+            StitchData& stitchData, const SkPoint& point) const;
+        SkScalar calculateImprovedNoiseValueForPoint(int channel, const SkPoint& point) const;
+        SkScalar noise2D(int channel,
+                         const StitchData& stitchData, const SkPoint& noiseVector) const;
+
+        SkMatrix fMatrix;
+        PaintingData* fPaintingData;
+
+        typedef Context INHERITED;
+    };
+
+#if SK_SUPPORT_GPU
+    sk_sp<GrFragmentProcessor> asFragmentProcessor(const AsFPArgs&) const override;
+#endif
+
+    SK_TO_STRING_OVERRIDE()
+    SK_DECLARE_PUBLIC_FLATTENABLE_DESERIALIZATION_PROCS(SkPerlinNoiseShader)
+
+protected:
+    void flatten(SkWriteBuffer&) const override;
+    Context* onMakeContext(const ContextRec&, SkArenaAlloc*) const override;
 
 private:
-    SkPerlinNoiseShader() = delete;
+    SkPerlinNoiseShader(SkPerlinNoiseShader::Type type, SkScalar baseFrequencyX,
+                        SkScalar baseFrequencyY, int numOctaves, SkScalar seed,
+                        const SkISize* tileSize);
+    ~SkPerlinNoiseShader() override;
+
+    const SkPerlinNoiseShader::Type fType;
+    const SkScalar                  fBaseFrequencyX;
+    const SkScalar                  fBaseFrequencyY;
+    const int                       fNumOctaves;
+    const SkScalar                  fSeed;
+    const SkISize                   fTileSize;
+    const bool                      fStitchTiles;
+
+    typedef SkShaderBase INHERITED;
 };
 
 #endif
