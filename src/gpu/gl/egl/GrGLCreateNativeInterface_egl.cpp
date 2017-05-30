80ecee75bc0d3d6e8d987b4abda1bd0d56a41e90
Joe Gregorio <jcgregorio@google.com>
2017-05-30 16:36:13 -0400

    egl - Fix EGL interface by creating a dummy context before assembling the interface.
    Bug: skia:
    Change-Id: I2cce80318925fe88f9407646acb67628a8e48810

--- src/gpu/gl/egl/GrGLCreateNativeInterface_egl.cpp
+++ src/gpu/gl/egl/GrGLCreateNativeInterface_egl.cpp
@@ -24,6 +24,67 @@ static GrGLFuncPtr egl_get_gl_proc(void* ctx, const char name[]) {
     return ptr;
 }
 
+static const EGLint configAttribs[] = {
+    EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+    EGL_BLUE_SIZE, 8,
+    EGL_GREEN_SIZE, 8,
+    EGL_RED_SIZE, 8,
+    EGL_DEPTH_SIZE, 8,
+    EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+    EGL_NONE
+};
+
+
+static const int pbufferWidth = 9;
+static const int pbufferHeight = 9;
+
+static const EGLint pbufferAttribs[] = {
+    EGL_WIDTH, pbufferWidth,
+    EGL_HEIGHT, pbufferHeight,
+    EGL_NONE,
+};
+
 const GrGLInterface* GrGLCreateNativeInterface() {
-    return GrGLAssembleInterface(nullptr, egl_get_gl_proc);
+    // It turns out eglGetProcAddress doesn't work unless there's an active
+    // OpenGL context, so create a 'dummy' context for use just long enough
+    // to make eglGetProcAdresss work when we pass it to
+    // GrGlAssembleInterface().
+
+    EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (EGL_NO_DISPLAY == eglDpy) {
+        return nullptr;
+    }
+
+    EGLint major, minor;
+    if (EGL_TRUE != eglInitialize(eglDpy, &major, &minor)) {
+        return nullptr;
+    }
+
+    EGLint numConfigs;
+    EGLConfig eglCfg;
+    if (EGL_TRUE != eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs)) {
+        return nullptr;
+    }
+
+    EGLSurface eglSurf = eglCreatePbufferSurface(eglDpy, eglCfg, pbufferAttribs);
+    if (EGL_NO_SURFACE == eglSurf) {
+        return nullptr;
+    }
+
+    if (EGL_TRUE != eglBindAPI(EGL_OPENGL_API)) {
+        return nullptr;
+    }
+
+    EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
+    if (EGL_NO_CONTEXT == eglCtx) {
+        return nullptr;
+    }
+    if (EGL_FALSE == eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx)) {
+        return nullptr;
+    }
+    const GrGLInterface* ret = GrGLAssembleInterface(nullptr, egl_get_gl_proc);
+
+    eglTerminate(eglDpy);
+
+    return ret;
 }
