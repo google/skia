5967ce39852f7ad2c800e59e14ff4c1c898bb021
Joe Gregorio <jcgregorio@google.com>
2017-05-30 17:12:15 -0400

    Move fiddle to use EGL instead of OSMesa.
    To test this turn on egl, e.g. --args='skia_use_egl=true', and run by altering the
    library path to point to the right directory of the EGL driver you want to use, for example:
    
      LD_LIBRARY_PATH=/usr/lib/nvidia-367/ ./out/Release/fiddle  | ./tools/fiddle/parse-fiddle-output 
    
    Bug: skia:
    Change-Id: I2cce80318925fe88f9407646acb67628a8e48810

--- tools/fiddle/fiddle_main.cpp
+++ tools/fiddle/fiddle_main.cpp
@@ -90,33 +90,75 @@ static SkData* encode_snapshot(const sk_sp<SkSurface>& surface) {
     return img ? img->encode() : nullptr;
 }
 
-#if defined(__linux) && !defined(__ANDROID__)
-    #include <GL/osmesa.h>
-    static sk_sp<GrContext> create_grcontext() {
-        // We just leak the OSMesaContext... the process will die soon anyway.
-        if (OSMesaContext osMesaContext = OSMesaCreateContextExt(OSMESA_BGRA, 0, 0, 0, nullptr)) {
-            static uint32_t buffer[16 * 16];
-            OSMesaMakeCurrent(osMesaContext, &buffer, GL_UNSIGNED_BYTE, 16, 16);
-        }
 
-        auto osmesa_get = [](void* ctx, const char name[]) {
-            SkASSERT(nullptr == ctx);
-            SkASSERT(OSMesaGetCurrentContext());
-            return OSMesaGetProcAddress(name);
-        };
-        sk_sp<const GrGLInterface> mesa(GrGLAssembleInterface(nullptr, osmesa_get));
-        if (!mesa) {
-            return nullptr;
-        }
-        return sk_sp<GrContext>(GrContext::Create(
-                                        kOpenGL_GrBackend,
-                                        reinterpret_cast<intptr_t>(mesa.get())));
+#if defined(__linux) && !defined(__ANDROID__)
+#include <EGL/egl.h>
+
+static const EGLint configAttribs[] = {
+    EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+    EGL_BLUE_SIZE, 8,
+    EGL_GREEN_SIZE, 8,
+    EGL_RED_SIZE, 8,
+    EGL_DEPTH_SIZE, 8,
+    EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+    EGL_NONE
+};
+
+
+static const int pbufferWidth = 9;
+static const int pbufferHeight = 9;
+
+static const EGLint pbufferAttribs[] = {
+    EGL_WIDTH, pbufferWidth,
+    EGL_HEIGHT, pbufferHeight,
+    EGL_NONE,
+};
+
+static sk_sp<GrContext> create_grcontext() {
+    EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (EGL_NO_DISPLAY == eglDpy) {
+        return nullptr;
     }
+
+    EGLint major, minor;
+    if (EGL_TRUE != eglInitialize(eglDpy, &major, &minor)) {
+        return nullptr;
+    }
+
+    EGLint numConfigs;
+    EGLConfig eglCfg;
+    if (EGL_TRUE != eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs)) {
+        return nullptr;
+    }
+
+    EGLSurface eglSurf = eglCreatePbufferSurface(eglDpy, eglCfg, pbufferAttribs);
+    if (EGL_NO_SURFACE == eglSurf) {
+        return nullptr;
+    }
+
+    if (EGL_TRUE != eglBindAPI(EGL_OPENGL_API)) {
+        return nullptr;
+    }
+
+    EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
+    if (EGL_NO_CONTEXT == eglCtx) {
+        return nullptr;
+    }
+    if (EGL_FALSE == eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx)) {
+        return nullptr;
+    }
+
+    auto interface = GrGLCreateNativeInterface();
+    if (!interface) {
+        return nullptr;
+    }
+    eglTerminate(eglDpy);
+
+    return sk_sp<GrContext>(GrContext::Create(kOpenGL_GrBackend, (GrBackendContext)interface));
+}
 #else
     static sk_sp<GrContext> create_grcontext() { return nullptr; }
 #endif
-
-
 
 int main(int argc, char** argv) {
     SkCommandLineFlags::Parse(argc, argv);
