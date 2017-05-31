9067b62e7d9a682ad7119dbd0e9213feb1f96bf0
Robert Phillips <robertphillips@google.com>
2017-05-31 12:50:18 -0400

    Detect Intel wrapped in ANGLE
    Grasping at straws here since I can no longer reproduce this locally.
    
    The vendor string in Chrome always seems to be "Google Inc." which defeats the vendor checks.
    
    Bug: 720325
    Change-Id: I7f05b825d54eb29be76776bdaeee1b632a2eb8d2

--- src/gpu/gl/GrGLUtil.cpp
+++ src/gpu/gl/GrGLUtil.cpp
@@ -330,6 +330,11 @@ GrGLRenderer GrGLGetRendererFromString(const char* rendererString) {
         if (0 == strncmp(rendererString, kMaliTStr, SK_ARRAY_COUNT(kMaliTStr) - 1)) {
             return kMaliT_GrGLRenderer;
         }
+        static const char kANGLEStr[] = "ANGLE";
+        if (0 == strncmp(rendererString, kANGLEStr, SK_ARRAY_COUNT(kANGLEStr) - 1) &&
+            strstr(rendererString, "Intel")) {
+            return kANGLE_Intel_GrGLRenderer;
+       }
     }
     return kOther_GrGLRenderer;
 }
