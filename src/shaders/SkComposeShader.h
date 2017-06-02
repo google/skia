5680c1a28c8467d75ebaed16a8a1451df4aba210
Mike Reed <reed@google.com>
2017-06-02 15:51:11 -0400

    add mixershader
    Bug: skia:
    Change-Id: I3bbb2cb8d0a84fca0309654498548ebc94d8938f

--- src/shaders/SkComposeShader.h
+++ src/shaders/SkComposeShader.h
@@ -15,31 +15,57 @@ class SkColorSpacXformer;
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-/** \class SkComposeShader
-    This subclass of shader returns the composition of two other shaders, combined by
-    a xfermode.
-*/
+/*
+ *  One class with two combining behaviors: either apply a blendmode or lerp
+ *
+ *  We store both values (mode and lerp), but only some modes are illegal, so we use one of them
+ *  as a sentinel to know that we're in the lerp case. We use kSrc, since the factory will see
+ *  kSrc and just return one of the shader parameters, rather than create a composit shader.
+ */
 class SkComposeShader : public SkShaderBase {
 public:
-    /** Create a new compose shader, given shaders A, B, and a combining xfermode mode.
-        When the xfermode is called, it will be given the result from shader A as its
-        "dst", and the result from shader B as its "src".
-        mode->xfer32(sA_result, sB_result, ...)
-        @param shaderA  The colors from this shader are seen as the "dst" by the xfermode
-        @param shaderB  The colors from this shader are seen as the "src" by the xfermode
-        @param mode     The xfermode that combines the colors from the two shaders. If mode
-                        is null, then SRC_OVER is assumed.
-    */
     SkComposeShader(sk_sp<SkShader> sA, sk_sp<SkShader> sB, SkBlendMode mode)
         : fShaderA(std::move(sA))
         , fShaderB(std::move(sB))
+        , fLerpT(GetIllegalLerp())
         , fMode(mode)
-    {}
+    {
+        SkASSERT(IsLegalMode(mode));
+    }
+
+    SkComposeShader(sk_sp<SkShader> sA, sk_sp<SkShader> sB, float lerpT)
+        : fShaderA(std::move(sA))
+        , fShaderB(std::move(sB))
+        , fLerpT(lerpT)
+        , fMode(GetIllegalMode())
+    {
+        SkASSERT(IsLegalLerp(lerpT));
+    }
+
+    // Helpers to distinguish between BlendMode and Lerp, since we store both but only use
+    // one or the other.
+
+    static SkBlendMode GetIllegalMode() { return SkBlendMode::kSrc; }
+    static float GetIllegalLerp() { return 0; }
+
+    static bool IsLegalMode(SkBlendMode mode) {
+        return mode != SkBlendMode::kSrc && mode != SkBlendMode::kDst;
+    }
+
+    static bool IsLegalLerp(float t) {
+        return t > 0 && t < 1;
+    }
+
+    bool isMode() const { return fMode != GetIllegalMode(); }
+    bool isLerp() const { return fMode == GetIllegalMode(); }
+
 
 #if SK_SUPPORT_GPU
     sk_sp<GrFragmentProcessor> asFragmentProcessor(const AsFPArgs&) const override;
 #endif
 
+    // Only used for isMode() == true
+    //
     class ComposeShaderContext : public Context {
     public:
         // When this object gets destroyed, it will call contextA and contextB's destructor
@@ -80,7 +106,8 @@ protected:
 private:
     sk_sp<SkShader>     fShaderA;
     sk_sp<SkShader>     fShaderB;
-    SkBlendMode         fMode;
+    const float         fLerpT;
+    const SkBlendMode   fMode;
 
     typedef SkShaderBase INHERITED;
 };
