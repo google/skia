30d20960c1b0a255b6cf3081246885dd989963ea
Mike Klein <mtklein@chromium.org>
2017-06-05 13:14:02 -0400

    lowp: add some big easy stages
    srcover_rgba_8888, lerp_u8, lerp_1_float, scale_u8, scale_1_float...
    this is enough for _lots_ of drawing.
    
    Change-Id: Ibe42adb8b1da6c66db3085851561dc9070556ee3

--- src/jumper/SkJumper.cpp
+++ src/jumper/SkJumper.cpp
@@ -70,14 +70,19 @@ using StartPipelineFn = void(size_t,size_t,size_t,void**,K*);
 #endif
 
 // Some stages have low-precision (~15 bit) versions from SkJumper_stages_lowp.cpp.
-#define LOWP_STAGES(M)  \
-    M(constant_color)   \
-    M(load_8888)        \
-    M(store_8888)       \
-    M(swap_rb)          \
-    M(swap)             \
-    M(move_src_dst)     \
-    M(move_dst_src)     \
+#define LOWP_STAGES(M)   \
+    M(constant_color)    \
+    M(load_8888)         \
+    M(store_8888)        \
+    M(srcover_rgba_8888) \
+    M(lerp_1_float)      \
+    M(lerp_u8)           \
+    M(scale_1_float)     \
+    M(scale_u8)          \
+    M(swap_rb)           \
+    M(swap)              \
+    M(move_src_dst)      \
+    M(move_dst_src)      \
     M(srcover)
 
 extern "C" {
