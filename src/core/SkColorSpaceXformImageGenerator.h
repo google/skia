a73d741f44a9ac0320be5e4aea0154d286f48aa9
Matt Sarett <msarett@google.com>
2017-06-05 14:46:23 -0400

    Create an SkColorSpaceXform image generator
    This should be immediately useful in the Skia-Android
    rendering pipeline.
    
    Possible future uses include creating a "renderable"
    SkImage from a bitmap with a funny color space.
    
    Inspired by:
    https://skia-review.googlesource.com/c/13981/
    
    Bug: b/62347704
    Change-Id: I388c7af1fc43834b8ad22022d0caf3ac90b734c8

---
+++ src/core/SkColorSpaceXformImageGenerator.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkColorSpaceXformImageGenerator_DEFINED
+#define SkColorSpaceXformImageGenerator_DEFINED
+
+#include "SkImageGenerator.h"
+
+class SkColorSpaceXformImageGenerator : public SkImageGenerator {
+public:
+
+    enum class Id {
+        kDefault,  // Images created with this generator have their own unique id
+        kCloneSrc, // Images created with this generator share an id with the |src| SkBitmap
+    };
+    std::unique_ptr<SkImageGenerator> Make(const SkBitmap& src, sk_sp<SkColorSpace> dst, Id id);
+
+protected:
+    bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes,
+                     const Options& opts) override;
+
+#if SK_SUPPORT_GPU
+    sk_sp<GrTextureProxy> onGenerateTexture(GrContext*, const SkImageInfo&,
+                                            const SkIPoint&) override;
+#endif
+
+private:
+    SkBitmap            fSrc;
+    sk_sp<SkColorSpace> fDst;
+
+    SkColorSpaceXformImageGenerator(const SkBitmap& src, sk_sp<SkColorSpace> dst, Id id);
+
+    friend class SkImageGenerator;
+
+    typedef SkImageGenerator INHERITED;
+};
+
+#endif // SkColorSpaceXformImageGenerator_DEFINED
