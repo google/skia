70898afe073c49d8151b25cc5bf234f61c76ffae
Yuqian Li <liyuqian@google.com>
2017-06-05 13:36:32 -0400

    Add TiledDrawScheduler so we can concurrently draw and enque (instead of finishing enque before draw). The highlight is that we can now achieve 9x speedup compared to 5x in all our previous approaches (including multi-picture draw).
    The schedulers here are experimental. I'd like to move on to try initializing
    once for each draw before further polishing and optimizing the schedule
    mechanism.
    
    Bug: skia:
    Change-Id: Idc3d030d475af9645c24c5372ff62b9a402206cc
    Reviewed-on: https://skia-review.googlesource.com/17826
    Reviewed-by: Mike Reed <reed@google.com>
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Yuqian Li <liyuqian@google.com>

--- src/core/SkThreadedBMPDevice.h
+++ src/core/SkThreadedBMPDevice.h
@@ -11,10 +11,37 @@
 #include "SkDraw.h"
 #include "SkBitmapDevice.h"
 
+#include <future>
+
+class TiledDrawScheduler {
+public:
+    using WorkFunc = std::function<void(int, int)>;
+
+    virtual ~TiledDrawScheduler() {}
+
+    virtual void signal() = 0; // signal that one more draw is available for all tiles
+
+    // Tell scheduler that no more draw calls will be added (no signal will be called).
+    virtual void finish() = 0;
+
+    // Handle the next draw available. This method will block until
+    //   (1) the next draw is finished, or
+    //   (2) the finish is called
+    // The method will return true for case (1) and false for case (2).
+    // When there's no draw available and we haven't called finish, we will just wait.
+    // In many cases, the parameter tileIndex specifies the tile that the next draw should happen.
+    // However, for some schedulers, that tileIndex may only be a hint and the scheduler is free
+    // to find another tile to draw. In that case, tileIndex will be changed to the actual tileIndex
+    // where the draw happens.
+    virtual bool next(int& tileIndex) = 0;
+};
+
 ///////////////////////////////////////////////////////////////////////////////
 class SkThreadedBMPDevice : public SkBitmapDevice {
 public:
-    SkThreadedBMPDevice(const SkBitmap& bitmap, int threads);
+    // When threads = 0, we make fThreadCnt = fTileCnt
+    SkThreadedBMPDevice(const SkBitmap& bitmap, int tiles, int threads = 0);
+    ~SkThreadedBMPDevice() { finishThreads(); }
 
 protected:
     void drawPaint(const SkPaint& paint) override;
@@ -47,9 +74,18 @@ private:
 
     SkIRect transformDrawBounds(const SkRect& drawBounds) const;
 
+    void startThreads();
+    void finishThreads();
+
+    static constexpr int MAX_QUEUE_SIZE = 100000;
+
+    const int fTileCnt;
     const int fThreadCnt;
-    SkTArray<SkIRect> fThreadBounds;
-    SkTArray<DrawElement> fQueue;
+    std::unique_ptr<TiledDrawScheduler> fScheduler;
+    SkTArray<SkIRect> fTileBounds;
+    SkTArray<std::future<void>> fThreadFutures;
+    DrawElement fQueue[MAX_QUEUE_SIZE];
+    int fQueueSize;
 
     typedef SkBitmapDevice INHERITED;
 };
