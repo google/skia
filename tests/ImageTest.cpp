543459618df0aa81f6aaa10bfd50f4dcf3a76ea5
Robert Phillips <robertphillips@google.com>
2017-06-08 12:16:51 -0400

    Destroy ANGLE displays in destroyGLContext (take 2)
    This CL also constrains the lifespan of ANGLE contexts (i.e., it makes them single use)
    
    Bug: skia:6711
    Change-Id: I9fa47e890efbcaa60ac7c0ce7d66027658d7b35c

--- tests/ImageTest.cpp
+++ tests/ImageTest.cpp
@@ -447,18 +447,7 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(c, reporter, ctxInfo) {
 }
 
 GrContextFactory::ContextType pick_second_context_type(const sk_gpu_test::ContextInfo& info) {
-    switch (info.backend()) {
-        case kOpenGL_GrBackend:
-#if defined(SK_BUILD_FOR_WIN) || defined(SK_BUILD_FOR_UNIX) || defined (SK_BUILD_FOR_MAC)
-            return GrContextFactory::kGL_ContextType;
-#else
-            return GrContextFactory::kGLES_ContextType;
-#endif
-        case kVulkan_GrBackend:
-            return GrContextFactory::kVulkan_ContextType;
-    }
-    SkFAIL("Unknown backend type.");
-    return GrContextFactory::kGL_ContextType;
+    return info.type();
 }
 
 DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkImage_makeTextureImage, reporter, contextInfo) {
@@ -1025,6 +1014,12 @@ static void check_images_same(skiatest::Reporter* reporter, const SkImage* a, co
 }
 
 DEF_GPUTEST_FOR_RENDERING_CONTEXTS(DeferredTextureImage, reporter, ctxInfo) {
+    if (ctxInfo.type() >= GrContextFactory::kANGLE_D3D9_ES2_ContextType &&
+        ctxInfo.type() <= GrContextFactory::kANGLE_GL_ES3_ContextType) {
+        // This test currently doesn't work with ANGLE
+        return;
+    }
+
     GrContext* context = ctxInfo.grContext();
     sk_gpu_test::TestContext* testContext = ctxInfo.testContext();
     sk_sp<GrContextThreadSafeProxy> proxy = context->threadSafeProxy();
