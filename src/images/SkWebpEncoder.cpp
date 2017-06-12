9fdc4c5b44c1e264319389e170eb66abb7753548
Matt Sarett <msarett@google.com>
2017-06-12 16:17:30 -0400

    Always encode images with sRGB encoded pixels
    Bug: skia:
    Change-Id: Icb25bc21a30e88f21df5b0e267d5a3a05535e44a
    Reviewed-on: https://skia-review.googlesource.com/19544
    Commit-Queue: Matt Sarett <msarett@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    (cherry picked from commit 1950e0a868774330330555a9a368992218f42240)

--- src/images/SkWebpEncoder.cpp
+++ src/images/SkWebpEncoder.cpp
@@ -194,16 +194,7 @@ bool SkWebpEncoder::Encode(SkWStream* stream, const SkPixmap& pixmap, const Opti
     // If there is no need to embed an ICC profile, we write directly to the input stream.
     // Otherwise, we will first encode to |tmp| and use a mux to add the ICC chunk.  libwebp
     // forces us to have an encoded image before we can add a profile.
-    sk_sp<SkData> icc;
-    if (SkColorSpace* cs = pixmap.colorSpace()) {
-        sk_sp<SkColorSpace> owned;
-        if (pixmap.colorType() == kRGBA_F16_SkColorType) {
-            // We'll be converting to 8-bit sRGB, so we'd better tag it that way.
-            owned = as_CSB(pixmap.colorSpace())->makeSRGBGamma();
-            cs = owned.get();
-        }
-        icc = icc_from_color_space(*cs);
-    }
+    sk_sp<SkData> icc = icc_from_color_space(pixmap.info());
     SkDynamicMemoryWStream tmp;
     pic.custom_ptr = icc ? (void*)&tmp : (void*)stream;
 
