977bcc676edded4ab0e1c0508c3efbaa6ddf3af1
Brian Salomon <bsalomon@google.com>
2017-06-14 13:00:03 -0400

    Force AAType to MSAA if the render target has MSAA and the API doesn't support disabling it.
    Change-Id: I88c29b8117fa82c6f41166b9333537a06bb841cc

--- src/gpu/text/GrStencilAndCoverTextContext.cpp
+++ src/gpu/text/GrStencilAndCoverTextContext.cpp
@@ -607,7 +607,7 @@ void GrStencilAndCoverTextContext::TextRun::draw(GrContext* ctx,
 
         // The run's "font" overrides the anti-aliasing of the passed in SkPaint!
         GrAAType aaType = GrChooseAAType(this->aa(), renderTargetContext->fsaaType(),
-                                         GrAllowMixedSamples::kYes);
+                                         GrAllowMixedSamples::kYes, *renderTargetContext->caps());
 
         std::unique_ptr<GrDrawOp> op = GrDrawPathRangeOp::Make(
                 viewMatrix, fTextRatio, fTextInverseRatio * x, fTextInverseRatio * y,
