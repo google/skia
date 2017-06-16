0620207bd23b69a183b8b78a89873515f95ec0d0
Eric Boren <borenet@google.com>
2017-06-16 10:50:39 -0400

    [recipes] Switch to file module from recipe_engine
    Bug: skia:6704
    Change-Id: Id3cefa643316e5676d2038a75240e16d929d6d63

--- infra/bots/recipe_modules/isolate/__init__.py
+++ infra/bots/recipe_modules/isolate/__init__.py
@@ -9,10 +9,10 @@
 
 
 DEPS = [
-  'file',
   'depot_tools/git',
   'depot_tools/gsutil',
   'recipe_engine/context',
+  'recipe_engine/file',
   'recipe_engine/json',
   'recipe_engine/path',
   'recipe_engine/properties',
