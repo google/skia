dcde58c3577781e85c06cfb870d17192cbbe08c2
Florin Malita <fmalita@chromium.org>
2017-06-19 10:21:43 -0400

    Disable dithering of const paints
    ... except for 565.
    
    Change-Id: I8ab633661c54583478234a46942ef804eb74a619

--- src/core/SkPaintPriv.cpp
+++ src/core/SkPaintPriv.cpp
@@ -10,7 +10,7 @@
 #include "SkPaintPriv.h"
 #include "SkImage.h"
 #include "SkPaint.h"
-#include "SkShader.h"
+#include "SkShaderBase.h"
 #include "SkXfermodePriv.h"
 
 static bool changes_alpha(const SkPaint& paint) {
@@ -69,3 +69,22 @@ void SkPaintPriv::ScaleFontMetrics(SkPaint::FontMetrics* metrics, SkScalar scale
     metrics->fUnderlinePosition *= scale;
 }
 
+bool SkPaintPriv::ShouldDither(const SkPaint& p, SkColorType dstCT) {
+    // The paint dither flag can veto.
+    if (!p.isDither()) {
+        return false;
+    }
+
+#ifndef SK_SUPPORT_LEGACY_DITHERING
+    // We always dither 565 when requested.
+    if (dstCT == SkColorType::kRGB_565_SkColorType) {
+        return true;
+    }
+
+    // Otherwise, dither is only needed for non-const paints.
+    return p.getImageFilter() || p.getMaskFilter()
+        || !p.getShader() || !as_SB(p.getShader())->isConstant();
+#else
+    return true;
+#endif
+}
