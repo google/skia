4c7ee4166d314b1b9f943a0f4fc4d0e0390b33c2
Robert Phillips <robertphillips@google.com>
2017-06-19 11:53:45 -0400

    Fix regression
    Change-Id: Ifd7a9260a8a4535e8440c0d837bfdc445a921aa4

---
+++ src/gpu/GrCoordTransform.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "GrCoordTransform.h"
+#include "GrResourceProvider.h"
+#include "GrTextureProxy.h"
+
+void GrCoordTransform::reset(GrResourceProvider* resourceProvider, const SkMatrix& m,
+                             GrTextureProxy* proxy, bool normalize) {
+    SkASSERT(proxy);
+    SkASSERT(!fInProcessor);
+
+    fMatrix = m;
+    fProxy = proxy;
+    fNormalize = normalize;
+    fReverseY = kBottomLeft_GrSurfaceOrigin == proxy->origin();
+}
