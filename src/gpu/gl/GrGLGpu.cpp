76631d832709e206ebf4c06c05d19be8c7918a9c
Jim Van Verth <jvanverth@google.com>
2017-06-26 10:23:02 -0400

    Fix TransferBufferTest for TegraX1
    Bug: skia:
    Change-Id: Ia64f48581e8003baec2eeaa4821354350b3333f4

--- src/gpu/gl/GrGLGpu.cpp
+++ src/gpu/gl/GrGLGpu.cpp
@@ -818,10 +818,12 @@ bool GrGLGpu::onTransferPixels(GrTexture* texture,
     SkASSERT(this->caps()->isConfigTexturable(texConfig));
 
     if (!check_write_and_transfer_input(glTex, texture, config)) {
+        SkDebugf("Failed check_write_and_transfer_input");
         return false;
     }
 
     if (width <= 0 || width > SK_MaxS32 || height <= 0 || height > SK_MaxS32) {
+        SkDebugf("Invalid size");
         return false;
     }
 
@@ -841,9 +843,11 @@ bool GrGLGpu::onTransferPixels(GrTexture* texture,
                                                &width, &height,
                                                &pixels,
                                                &rowBytes)) {
+        SkDebugf("AdjustWritePixelParams failed");
         return false;
     }
     if (width < 0 || width < 0) {
+        SkDebugf("Negative size");
         return false;
     }
 
@@ -862,6 +866,7 @@ bool GrGLGpu::onTransferPixels(GrTexture* texture,
     GrGLenum externalType;
     if (!this->glCaps().getTexImageFormats(texConfig, config, &internalFormat,
                                            &externalFormat, &externalType)) {
+        SkDebugf("getTexImageFormats failed");
         return false;
     }
 
