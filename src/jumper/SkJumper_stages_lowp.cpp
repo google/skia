ac45550bfb1610c54b4548ece01bdbee19fadeb4
Mike Klein <mtklein@chromium.org>
2017-06-26 11:42:44 -0400

    lean more on the compiler in lowp stages
    This refactors {from,to}_{byte,8888} to lean a bit more on the compiler,
    and to share code between the two.  The algorithm is not exactly the
    same, but it's comparable, and the results of course are identical.
    
    This new algorithm is a lot easier to generalize to AVX2, and parallels
    the full-precision {from,to}_{byte,8888} functions in _stages.cpp.
    
    Change-Id: I31ea90d65967bf4ede2497d1e2197cb0e7648bf8

--- src/jumper/SkJumper_stages_lowp.cpp
+++ src/jumper/SkJumper_stages_lowp.cpp
@@ -119,61 +119,41 @@ SI void store(T* dst, V v, size_t tail) {
     unaligned_store(dst, v);
 }
 
-SI void from_8888(U32 rgba, F* r, F* g, F* b, F* a) {
-    // Split the 8 pixels into low and high halves, and reinterpret as vectors of 16-bit values.
-    U16 lo = unaligned_load<U16>((const uint32_t*)&rgba + 0),
-        hi = unaligned_load<U16>((const uint32_t*)&rgba + 4);
-
-    // Shuffle so that the 4 bytes of each color channel are contiguous...
-    lo = _mm_shuffle_epi8(lo, _mm_setr_epi8(0,4,8,12, 1,5,9,13, 2,6,10,14, 3,7,11,15));
-    hi = _mm_shuffle_epi8(hi, _mm_setr_epi8(0,4,8,12, 1,5,9,13, 2,6,10,14, 3,7,11,15));
-
-    // ...then get all 8 bytes of each color channel together into a single register.
-    U16 rg = _mm_unpacklo_epi32(lo,hi),
-        ba = _mm_unpackhi_epi32(lo,hi);
-
-    // Unpack as 16-bit values into the high half of each 16-bit lane, to get a free *256.
-    U16 R = _mm_unpacklo_epi8(U16(0), rg),
-        G = _mm_unpackhi_epi8(U16(0), rg),
-        B = _mm_unpacklo_epi8(U16(0), ba),
-        A = _mm_unpackhi_epi8(U16(0), ba);
-
-    // Now we scale from [0,255] to [0,32768].  Ideally that's 32768/255 = 128.50196,
-    // but we can approximate that very cheaply as 256*32897/65536 = 128.50391.
-    // 0 and 255 map to 0 and 32768 correctly, and nothing else is off by more than 1.
-    *r = _mm_mulhi_epu16(R, U16(32897));
-    *g = _mm_mulhi_epu16(G, U16(32897));
-    *b = _mm_mulhi_epu16(B, U16(32897));
-    *a = _mm_mulhi_epu16(A, U16(32897));
+// Scale from [0,255] up to [0,32768].
+SI F from_wide_byte(U16 bytes) {
+    // Ideally we'd scale by 32768/255 = 128.50196, but instead we'll approximate
+    // that a little more cheaply as 256*32897/65536 = 128.50391.
+    // 0 and 255 map to 0 and 32768 correctly, and nothing else is off by more than 1 bit.
+    return _mm_mulhi_epu16(bytes << 8, U16(32897));
 }
 SI F from_byte(U8 bytes) {
-    // See from_8888() just above.
-    U16 hi = _mm_unpacklo_epi8(U16(0), widen_cast<__m128i>(bytes));
-    return (F)_mm_mulhi_epu16(hi, U16(32897));
+    return from_wide_byte(__builtin_convertvector(bytes, U16));
+}
+
+// Pack from [0,32768] down to [0,255].
+SI U16 to_wide_byte(F v) {
+    // The simplest thing works great: divide by 128 and saturate.
+    return _mm_min_epi16(v>>7, U16(255));
+}
+SI U8 to_byte(F v) {
+    // Like to_wide_byte(), but we'll bake the saturation into the 16->8 bit pack.
+    // Only the bottom 8 bytes are of interest... it doesn't matter what we pack on top.
+    __m128i packed = _mm_packus_epi16(v>>7, v>>7);
+    return unaligned_load<U8>(&packed);
+}
+
+SI void from_8888(U32 rgba, F* r, F* g, F* b, F* a) {
+    *r = from_wide_byte(__builtin_convertvector((rgba >>  0) & 0xff, U16));
+    *g = from_wide_byte(__builtin_convertvector((rgba >>  8) & 0xff, U16));
+    *b = from_wide_byte(__builtin_convertvector((rgba >> 16) & 0xff, U16));
+    *a = from_wide_byte(__builtin_convertvector((rgba >> 24) & 0xff, U16));
 }
 
 SI U32 to_8888(F r, F g, F b, F a) {
-    // We want to interlace and pack these values from [0,32768] to [0,255].
-    // Luckily the simplest possible thing works great: >>7, then saturate.
-    // The 'u' in packus handles the saturation to [0,255] we need.
-    U16 rb = _mm_packus_epi16(r>>7,b>>7), // r0 r1 r2 r3 r4 r5 r6 r7 b0 b1 b2 b3 b4 b5 b6 b7
-        ga = _mm_packus_epi16(g>>7,a>>7);
-
-    U16 rg = _mm_unpacklo_epi8(rb, ga),   // r0 g0 r1 g1 ...                           r7 g7
-        ba = _mm_unpackhi_epi8(rb, ga);   // b0 a0       ...                           b7 a7
-
-    U16 lo = _mm_unpacklo_epi16(rg, ba),  // r0 g0 b0 a0 ...                     r3 g3 b3 a3
-        hi = _mm_unpackhi_epi16(rg, ba);  // r4 g4 b4 a4 ...                     r7 g7 b7 a7
-
-    U32 px;
-    memcpy((uint32_t*)&px + 0, &lo, sizeof(lo));
-    memcpy((uint32_t*)&px + 4, &hi, sizeof(hi));
-    return px;
-}
-SI U8 to_byte(F v) {
-    // See to_8888() just above.
-    U16 packed = _mm_packus_epi16(v>>7, v>>7);  // Doesn't really matter what we pack on top.
-    return unaligned_load<U8>(&packed);
+    return __builtin_convertvector(to_wide_byte(r), U32) <<  0
+         | __builtin_convertvector(to_wide_byte(g), U32) <<  8
+         | __builtin_convertvector(to_wide_byte(b), U32) << 16
+         | __builtin_convertvector(to_wide_byte(a), U32) << 24;
 }
 
 // Stages!
