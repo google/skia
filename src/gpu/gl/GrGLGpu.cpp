50df169f81b35b6b2d3050844e17ca5f97547e88
Jim Van Verth <jvanverth@google.com>
2017-06-26 16:30:53 -0400

    Add shading language version to GL dump
    Bug: skia:6759
    Change-Id: I021a8487ccb1d127b2005285e439be34d644f333

--- src/gpu/gl/GrGLGpu.cpp
+++ src/gpu/gl/GrGLGpu.cpp
@@ -203,7 +203,7 @@ GrGpu* GrGLGpu::Create(GrBackendContext backendContext, const GrContextOptions& 
     return nullptr;
 }
 
-static bool gPrintStartupSpew;
+static bool gPrintStartupSpew = true;
 
 GrGLGpu::GrGLGpu(GrGLContext* ctx, GrContext* context)
     : GrGpu(context)
@@ -257,14 +257,17 @@ GrGLGpu::GrGLGpu(GrGLContext* ctx, GrContext* context)
         const GrGLubyte* vendor;
         const GrGLubyte* renderer;
         const GrGLubyte* version;
+        const GrGLubyte* glslVersion;
         GL_CALL_RET(vendor, GetString(GR_GL_VENDOR));
         GL_CALL_RET(renderer, GetString(GR_GL_RENDERER));
         GL_CALL_RET(version, GetString(GR_GL_VERSION));
+        GL_CALL_RET(glslVersion, GetString(GR_GL_SHADING_LANGUAGE_VERSION));
         SkDebugf("------------------------- create GrGLGpu %p --------------\n",
                  this);
         SkDebugf("------ VENDOR %s\n", vendor);
         SkDebugf("------ RENDERER %s\n", renderer);
         SkDebugf("------ VERSION %s\n",  version);
+        SkDebugf("------ SHADING LANGUAGE VERSION %s\n", glslVersion);
         SkDebugf("------ EXTENSIONS\n");
         this->glContext().extensions().print();
         SkDebugf("\n");
