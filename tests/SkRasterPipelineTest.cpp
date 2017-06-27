9f2b3d1fbfa16146184a13b778fbe2fa5355c51b
Mike Klein <mtklein@chromium.org>
2017-06-27 17:26:50 -0400

    remove unused "swap" stage
    Change-Id: I25619f010f8ac6441529cfe8dff2d8c42d7400cf
    Reviewed-on: https://skia-review.googlesource.com/20988
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>

--- tests/SkRasterPipelineTest.cpp
+++ tests/SkRasterPipelineTest.cpp
@@ -21,10 +21,8 @@ DEF_TEST(SkRasterPipeline, r) {
     void* store_ctx  = &result;
 
     SkRasterPipeline_<256> p;
-    p.append(SkRasterPipeline::load_f16, &load_s_ctx);
-    p.append(SkRasterPipeline::move_src_dst);
-    p.append(SkRasterPipeline::load_f16, &load_d_ctx);
-    p.append(SkRasterPipeline::swap);
+    p.append(SkRasterPipeline::load_f16,     &load_s_ctx);
+    p.append(SkRasterPipeline::load_f16_dst, &load_d_ctx);
     p.append(SkRasterPipeline::srcover);
     p.append(SkRasterPipeline::store_f16, &store_ctx);
     p.run(0,0,1);
