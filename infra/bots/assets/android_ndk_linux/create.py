8368e3680e060114bca801925b30bc9a7b6bf205
Derek Sollenberger <djsollen@google.com>
2017-06-27 11:55:05 -0400

    Upgrade android build (and infrastructure) to use ndk r15.
    Bug: 6672
    Change-Id: Ia6c6bae8a9adfd75172d7116487dfa63c3d69670

--- infra/bots/assets/android_ndk_linux/create.py
+++ infra/bots/assets/android_ndk_linux/create.py
@@ -15,7 +15,7 @@ import os.path
 import shutil
 import subprocess
 
-NDK_VER = "android-ndk-r14"
+NDK_VER = "android-ndk-r15b"
 NDK_URL = \
     "https://dl.google.com/android/repository/%s-linux-x86_64.zip" % NDK_VER
 
