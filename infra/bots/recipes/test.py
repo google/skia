6af5db61a72fd241a7e8d6985dde72761d23de7d
Ben Wagner <benjaminwagner@google.com>
2017-06-27 13:44:36 -0400

    Turn on verbose logging on flakily dying bots.
    No-Try: true
    
    Change-Id: I2c1b598f03d8536aa3ef118630a0aed63fe54f9e

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -168,6 +168,9 @@ def dm_flags(bot):
   if 'SK_FORCE_RASTER_PIPELINE_BLITTER' in bot:
     args.remove('tests')
 
+  # Some people don't like verbose output.
+  verbose = False
+
   blacklisted = []
   def blacklist(quad):
     config, src, options, name = quad.split(' ') if type(quad) is str else quad
@@ -442,6 +445,10 @@ def dm_flags(bot):
   if 'Vulkan' in bot and 'IntelIris540' in bot and 'Ubuntu' in bot:
     match.extend(['~VkHeapTests']) # skia:6245
 
+  if 'Intel' in bot and 'Ubuntu' in bot and not 'Vulkan' in bot:
+    # TODO(dogben): Track down what's causing bots to die.
+    verbose = True
+
   if 'Vulkan' in bot and 'IntelIris540' in bot and 'Win' in bot:
     # skia:6398
     blacklist(['vk', 'gm', '_', 'aarectmodes'])
@@ -551,12 +558,15 @@ def dm_flags(bot):
     args.append('--noRAW_threading')
 
   if 'Valgrind' in bot and 'PreAbandonGpuContext' in bot:
-    args.append('--verbose')
+    verbose = True
 
   if 'NexusPlayer' in bot and 'CPU' in bot:
     # The Nexus Player's image decoding tests are slow enough that swarming
     # times it out for not printing anything frequently enough.  --verbose
     # makes dm print something every time we start or complete a task.
+    verbose = True
+
+  if verbose:
     args.append('--verbose')
 
   return args
