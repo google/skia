72d291c595cebe0b93117f526bc792c20b228e34
Ethan Nicholas <ethannicholas@google.com>
2017-06-29 09:09:17 -0400

    Re-re-land sksl fragment processor support
    This reverts commit 5ce397205528f82084fc650c2ce27d246c01da33.
    
    Bug: skia:
    Change-Id: I88260c90004610a1cf8ad1a87c2b4b222525bbb6

---
+++ src/gpu/effects/GrDitherEffect.fp
@@ -0,0 +1,17 @@
+void main() {
+    // Generate a random number based on the fragment position. For this
+    // random number generator, we use the "GLSL rand" function
+    // that seems to be floating around on the internet. It works under
+    // the assumption that sin(<big number>) oscillates with high frequency
+    // and sampling it will generate "randomness". Since we're using this
+    // for rendering and not cryptography it should be OK.
+
+    // For each channel c, add the random offset to the pixel to either bump
+    // it up or let it remain constant during quantization.
+    float r = fract(sin(dot(sk_FragCoord.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    sk_OutColor = clamp(1.0 / 255.0 * vec4(r) + sk_InColor, 0, 1);
+}
+
+@test(testData) {
+    return GrDitherEffect::Make();
+}
--- src/sksl/ir/SkSLLayout.h
+++ src/sksl/ir/SkSLLayout.h
@@ -8,6 +8,7 @@
 #ifndef SKSL_LAYOUT
 #define SKSL_LAYOUT
 
+#include "SkSLString.h"
 #include "SkSLUtil.h"
 
 namespace SkSL {
@@ -42,6 +43,16 @@ struct Layout {
         kR8I,
     };
 
+    // used by SkSL processors
+    enum Key {
+        // field is not a key
+        kNo_Key,
+        // field is a key
+        kKey_Key,
+        // key is 0 or 1 depending on whether the matrix is an identity matrix
+        kIdentity_Key,
+    };
+
     static const char* FormatToStr(Format format) {
         switch (format) {
             case Format::kUnspecified:  return "";
@@ -55,7 +66,6 @@ struct Layout {
             case Format::kR8I:          return "r8i";
         }
         ABORT("Unexpected format");
-        return "";
     }
 
     static bool ReadFormat(String str, Format* format) {
@@ -90,7 +100,7 @@ struct Layout {
     Layout(int location, int offset, int binding, int index, int set, int builtin,
            int inputAttachmentIndex, bool originUpperLeft, bool overrideCoverage,
            bool blendSupportAllEquations, Format format, bool pushconstant, Primitive primitive,
-           int maxVertices, int invocations)
+           int maxVertices, int invocations, String when, Key key)
     : fLocation(location)
     , fOffset(offset)
     , fBinding(binding)
@@ -105,7 +115,9 @@ struct Layout {
     , fPushConstant(pushconstant)
     , fPrimitive(primitive)
     , fMaxVertices(maxVertices)
-    , fInvocations(invocations) {}
+    , fInvocations(invocations)
+    , fWhen(when)
+    , fKey(key) {}
 
     Layout()
     : fLocation(-1)
@@ -122,7 +134,8 @@ struct Layout {
     , fPushConstant(false)
     , fPrimitive(kUnspecified_Primitive)
     , fMaxVertices(-1)
-    , fInvocations(-1) {}
+    , fInvocations(-1)
+    , fKey(kNo_Key) {}
 
     String description() const {
         String result;
@@ -215,6 +228,22 @@ struct Layout {
             result += separator + "invocations = " + to_string(fInvocations);
             separator = ", ";
         }
+        if (fWhen.size()) {
+            result += separator + "when = " + fWhen;
+            separator = ", ";
+        }
+        switch (fKey) {
+            case kNo_Key:
+                break;
+            case kKey_Key:
+                result += separator + "key";
+                separator = ", ";
+                break;
+            case kIdentity_Key:
+                result += separator + "key=identity";
+                separator = ", ";
+                break;
+        }
         if (result.size() > 0) {
             result = "layout (" + result + ")";
         }
@@ -261,6 +290,8 @@ struct Layout {
     Primitive fPrimitive;
     int fMaxVertices;
     int fInvocations;
+    String fWhen;
+    Key fKey;
 };
 
 } // namespace
