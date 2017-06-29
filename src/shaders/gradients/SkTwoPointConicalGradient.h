981deec8b2928f07e3899849bed31bfbb8c19b9b
Florin Malita <fmalita@chromium.org>
2017-06-29 15:47:16 -0400

    Delete non-raster-pipeline SkTwoPointConicalGradient impl
    Change-Id: If39a2b01d6099fef14695a4ce05b57a3d5c3d99c
    Reviewed-on: https://skia-review.googlesource.com/21320
    Reviewed-by: Mike Reed <reed@google.com>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- src/shaders/gradients/SkTwoPointConicalGradient.h
+++ src/shaders/gradients/SkTwoPointConicalGradient.h
@@ -11,50 +11,11 @@
 #include "SkColorSpaceXformer.h"
 #include "SkGradientShaderPriv.h"
 
-// TODO(dominikg): Worth making it truly immutable (i.e. set values in constructor)?
-// Should only be initialized once via init(). Immutable afterwards.
-struct TwoPtRadial {
-    enum {
-        // This value is outside the range SK_FixedMin to SK_FixedMax.
-        kDontDrawT  = 0x80000000
-    };
-
-    float   fCenterX, fCenterY;
-    float   fDCenterX, fDCenterY;
-    float   fRadius;
-    float   fDRadius;
-    float   fA;
-    float   fRadius2;
-    float   fRDR;
-    bool    fFlipped;
-
-    void init(const SkPoint& center0, SkScalar rad0,
-              const SkPoint& center1, SkScalar rad1,
-              bool flipped);
-
-    static bool DontDrawT(SkFixed t) {
-        return kDontDrawT == (uint32_t)t;
-    }
-};
-
-
 class SkTwoPointConicalGradient final : public SkGradientShaderBase {
-    TwoPtRadial fRec;
 public:
     SkTwoPointConicalGradient(const SkPoint& start, SkScalar startRadius,
                               const SkPoint& end, SkScalar endRadius,
                               bool flippedGrad, const Descriptor&);
-
-    class TwoPointConicalGradientContext : public SkGradientShaderBase::GradientShaderBaseContext {
-    public:
-        TwoPointConicalGradientContext(const SkTwoPointConicalGradient&, const ContextRec&);
-        ~TwoPointConicalGradientContext() override {}
-
-        void shadeSpan(int x, int y, SkPMColor dstC[], int count) override;
-
-    private:
-        typedef SkGradientShaderBase::GradientShaderBaseContext INHERITED;
-    };
 
     SkShader::GradientType asAGradient(GradientInfo* info) const  override;
 #if SK_SUPPORT_GPU
@@ -76,7 +37,6 @@ public:
 protected:
     SkTwoPointConicalGradient(SkReadBuffer& buffer);
     void flatten(SkWriteBuffer& buffer) const override;
-    Context* onMakeContext(const ContextRec&, SkArenaAlloc*) const override;
     sk_sp<SkShader> onMakeColorSpace(SkColorSpaceXformer* xformer) const override;
 
     bool adjustMatrixAndAppendStages(SkArenaAlloc* alloc,
@@ -84,7 +44,7 @@ protected:
                                      SkRasterPipeline* tPipeline,
                                      SkRasterPipeline* postPipeline) const override;
 
-    bool isRasterPipelineOnly() const override;
+    bool isRasterPipelineOnly() const override { return true; }
 
 private:
     SkPoint fCenter1;
