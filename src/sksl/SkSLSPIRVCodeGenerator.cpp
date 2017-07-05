c8b9422ba1a54fab40592a4808d47456d9535d82
Ethan Nicholas <ethannicholas@google.com>
2017-07-05 09:52:43 -0400

    SPIR-V support for more integer operations
    Bug: skia:
    Change-Id: Ic4ff8dd8b08df8c44222bd7844f4a872377ff240

--- src/sksl/SkSLSPIRVCodeGenerator.cpp
+++ src/sksl/SkSLSPIRVCodeGenerator.cpp
@@ -2194,6 +2194,23 @@ SpvId SPIRVCodeGenerator::writeBinaryExpression(const BinaryExpression& b, Outpu
         case Token::PERCENT:
             return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpFMod,
                                               SpvOpSMod, SpvOpUMod, SpvOpUndef, out);
+        case Token::SHL:
+            return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpUndef,
+                                              SpvOpShiftLeftLogical, SpvOpShiftLeftLogical,
+                                              SpvOpUndef, out);
+        case Token::SHR:
+            return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpUndef,
+                                              SpvOpShiftRightArithmetic, SpvOpShiftRightLogical,
+                                              SpvOpUndef, out);
+        case Token::BITWISEAND:
+            return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpUndef,
+                                              SpvOpBitwiseAnd, SpvOpBitwiseAnd, SpvOpUndef, out);
+        case Token::BITWISEOR:
+            return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpUndef,
+                                              SpvOpBitwiseOr, SpvOpBitwiseOr, SpvOpUndef, out);
+        case Token::BITWISEXOR:
+            return this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpUndef,
+                                              SpvOpBitwiseXor, SpvOpBitwiseXor, SpvOpUndef, out);
         case Token::PLUSEQ: {
             SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpFAdd,
                                                       SpvOpIAdd, SpvOpIAdd, SpvOpUndef, out);
@@ -2235,6 +2252,46 @@ SpvId SPIRVCodeGenerator::writeBinaryExpression(const BinaryExpression& b, Outpu
         case Token::PERCENTEQ: {
             SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs, SpvOpFMod,
                                                       SpvOpSMod, SpvOpUMod, SpvOpUndef, out);
+            ASSERT(lvalue);
+            lvalue->store(result, out);
+            return result;
+        }
+        case Token::SHLEQ: {
+            SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs,
+                                                      SpvOpUndef, SpvOpShiftLeftLogical,
+                                                      SpvOpShiftLeftLogical, SpvOpUndef, out);
+            ASSERT(lvalue);
+            lvalue->store(result, out);
+            return result;
+        }
+        case Token::SHREQ: {
+            SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs,
+                                                      SpvOpUndef, SpvOpShiftRightArithmetic,
+                                                      SpvOpShiftRightLogical, SpvOpUndef, out);
+            ASSERT(lvalue);
+            lvalue->store(result, out);
+            return result;
+        }
+        case Token::BITWISEANDEQ: {
+            SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs,
+                                                      SpvOpUndef, SpvOpBitwiseAnd, SpvOpBitwiseAnd,
+                                                      SpvOpUndef, out);
+            ASSERT(lvalue);
+            lvalue->store(result, out);
+            return result;
+        }
+        case Token::BITWISEOREQ: {
+            SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs,
+                                                      SpvOpUndef, SpvOpBitwiseOr, SpvOpBitwiseOr,
+                                                      SpvOpUndef, out);
+            ASSERT(lvalue);
+            lvalue->store(result, out);
+            return result;
+        }
+        case Token::BITWISEXOREQ: {
+            SpvId result = this->writeBinaryOperation(resultType, *operandType, lhs, rhs,
+                                                      SpvOpUndef, SpvOpBitwiseXor, SpvOpBitwiseXor,
+                                                      SpvOpUndef, out);
             ASSERT(lvalue);
             lvalue->store(result, out);
             return result;
