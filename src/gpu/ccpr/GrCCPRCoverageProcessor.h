4f2bcda8cbdea8083b524dace219490b27f0dcf4
Chris Dalton <csmartdalton@google.com>
2017-07-05 10:52:48 -0600

    Coverage counting path renderer
    Initial implementation of a GPU path renderer that draws antialiased
    paths by counting coverage in an offscreen buffer.
    
    Bug: skia:
    Change-Id: I003d8cfdf8dc62641581b5ea2dc4f0aa00108df6

---
+++ src/gpu/ccpr/GrCCPRCoverageProcessor.h
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrCCPRCoverageProcessor_DEFINED
+#define GrCCPRCoverageProcessor_DEFINED
+
+#include "GrGeometryProcessor.h"
+#include "glsl/GrGLSLGeometryProcessor.h"
+#include "glsl/GrGLSLVarying.h"
+
+class GrGLSLFragmentBuilder;
+
+/**
+ * This is the geometry processor for the simple convex primitive shapes (triangles and closed curve
+ * segments) from which ccpr paths are composed. The output is a single-channel alpha value,
+ * positive for clockwise primitives and negative for counter-clockwise, that indicates coverage.
+ *
+ * The caller is responsible to render all modes for all applicable primitives into a cleared,
+ * floating point, alpha-only render target using SkBlendMode::kPlus. Once all of a path's
+ * primitives have been drawn, the render target contains a composite coverage count that can then
+ * be used to draw the path (see GrCCPRPathProcessor).
+ *
+ * Caller provides the primitives' (x,y) points in an fp32 RG texel buffer.
+ *
+ * The draw buffers should be laid out with a single, "kVec4i_GrVertexAttribType", per-instance
+ * attrib for each primitive. The contents of this int32x4 instance attrib vary based on primitive
+ * type:
+ *
+ * Triangle (GrPrimitiveType::kTriangles):
+ *
+ *     {packed_atlas_offset,  // (offsetY << 16) | offsetX
+ *      P2_idx,               // P2 index in texel buffer. (Out of order to WAR a driver bug.)
+ *      P0_idx,
+ *      P1_idx}
+ *
+ * Quadratic (GrPrimitiveType::kTriangles):
+ *
+ *     {packed_atlas_offset,  // (offsetY << 16) | offsetX
+ *      0,                    // Unused.
+ *      P1_idx,               // Control point index in texel buffer.
+ *      P0_P2_idx}            // The endpoints (P0 and P2) are adjacent in the texel buffer.
+ *
+ * Cubic (GrPrimitiveType::kLinesAdjacency):
+ *
+ *     {packed_atlas_offset,  // (offsetY << 16) | offsetX
+ *      0,                    // Unused.
+ *      P1_P2_TSl_TSm_idx,    // The control points (P1 and P2) are adjacent in the texel buffer,
+ *                            // followed immediately by the homogenous KLM roots ({tl,sl}, {tm,sm})
+ *      P0_P3_idx}            // The endpoints (P0 and P3) are adjacent in the texel buffer.
+ */
+class GrCCPRCoverageProcessor : public GrGeometryProcessor {
+public:
+    // Use top-left to avoid a uniform access in the fragment shader.
+    static constexpr GrSurfaceOrigin kAtlasOrigin = kTopLeft_GrSurfaceOrigin;
+
+    enum class Mode {
+        kTriangleHulls,
+        kTriangleEdges,
+        kCombinedTriangleHullsAndEdges,
+        kTriangleCorners,
+        kQuadraticHulls,
+        kQuadraticFlatEdges,
+        kSerpentineInsets,
+        kSerpentineBorders,
+        kLoopInsets,
+        kLoopBorders
+    };
+    static const char* GetProcessorName(Mode);
+
+    GrCCPRCoverageProcessor(Mode, GrBuffer* pointsBuffer);
+
+    const char* instanceAttrib() const { return fInstanceAttrib.fName; }
+    const char* name() const override { return GetProcessorName(fMode); }
+    void getGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
+    GrGLSLPrimitiveProcessor* createGLSLInstance(const GrShaderCaps&) const override;
+
+#ifdef SK_DEBUG
+    static constexpr float kDebugBloat = 50;
+
+    // Increases the 1/2 pixel AA bloat by a factor of kDebugBloat and outputs color instead of
+    // coverage (coverage=+1 -> green, coverage=0 -> black, coverage=-1 -> red).
+    void enableDebugVisualizations() { fDebugVisualizations = true; }
+    bool debugVisualizations() const { return fDebugVisualizations; }
+
+    static void Validate(GrRenderTarget* atlasTexture);
+#endif
+
+    class PrimitiveProcessor;
+
+private:
+    const Mode         fMode;
+    const Attribute&   fInstanceAttrib;
+    BufferAccess       fPointsBufferAccess;
+    SkDEBUGCODE(bool   fDebugVisualizations = false;)
+
+    typedef GrGeometryProcessor INHERITED;
+};
+
+/**
+ * This class represents the actual SKSL implementation for the various primitives and modes of
+ * GrCCPRCoverageProcessor.
+ */
+class GrCCPRCoverageProcessor::PrimitiveProcessor : public GrGLSLGeometryProcessor {
+public:
+    // We achieve conservative raster by defining pixel-size boxes around vertices, and then drawing
+    // convex hulls around those boxes. We slightly undershoot a radius of 0.5 so vertices that fall
+    // on integer boundaries don't accidentally bleed into neighbor pixels.
+    static constexpr float kAABloatRadius = 0.491111f;
+
+    enum class CoverageType {
+        kOne,
+        kInterpolated,
+        kShader
+    };
+
+    PrimitiveProcessor(CoverageType coverageType)
+        : fCoverageType(coverageType)
+        , fGeomWind("wind", kFloat_GrSLType, GrShaderVar::kNonArray, kLow_GrSLPrecision)
+        , fFragWind(kFloat_GrSLType)
+        , fFragCoverageTimesWind(kFloat_GrSLType) {}
+
+    void setData(const GrGLSLProgramDataManager& pdman, const GrPrimitiveProcessor&,
+                 FPCoordTransformIter&& transformIter) final {
+        this->setTransformDataHelper(SkMatrix::I(), pdman, &transformIter);
+    }
+
+    void onEmitCode(EmitArgs& args, GrGPArgs* gpArgs) final;
+
+protected:
+    virtual void resetVaryings(GrGLSLVaryingHandler*) {}
+    virtual void onEmitVertexShader(const GrCCPRCoverageProcessor&, GrGLSLVertexBuilder*,
+                                    const char* rtAdjustName, const TexelBufferHandle& pointsBuffer,
+                                    GrGPArgs*) const = 0;
+    virtual void emitWind(GrGLSLGeometryBuilder*, const char* windName,
+                          const char* rtAdjustName) const;
+    virtual void onEmitGeometryShader(GrGLSLGeometryBuilder*, const char* emitVertexFn,
+                                      const char* wind, const char* rtAdjustName) const = 0;
+    virtual void emitPerVertexGeometryCode(SkString* fnBody, const char* position,
+                                           const char* coverage, const char* wind) const {}
+    virtual void emitShaderCoverage(GrGLSLFragmentBuilder*, const char* outputCoverage) const {
+        SkFAIL("Shader coverage not implemented when using CoverageType::kShader.");
+    }
+
+    int emitHullGeometry(GrGLSLGeometryBuilder*, int numPts, const char* wedgeIdx,
+                         const char* emitVertexFn, const char* hullPts,
+                         const char* insetPts = nullptr) const;
+    void emitEdgeDistanceEquation(GrGLSLGeometryBuilder*, const char* leftPt, const char* rightPt,
+                                  const char* distanceEquationName) const;
+    int emitEdgeGeometry(GrGLSLGeometryBuilder*, const char* emitVertexFn,
+                         const char* leftPt, const char* rightPt,
+                         const char* distanceEquation = nullptr) const;
+    int definePseudoSampleLocations(GrGLSLFragmentBuilder*, const char* samplesName) const;
+
+private:
+    void emitVertexShader(const GrCCPRCoverageProcessor&, GrGLSLVertexBuilder*,
+                          const char* rtAdjustName, const TexelBufferHandle& pointsBuffer,
+                          GrGPArgs* gpArgs) const;
+    void emitGeometryShader(const GrCCPRCoverageProcessor&, GrGLSLGeometryBuilder*,
+                            const char* rtAdjustName) const;
+    void emitCoverage(const GrCCPRCoverageProcessor&, GrGLSLFragmentBuilder*,
+                      const char* outputColor, const char* outputCoverage) const;
+
+    const CoverageType   fCoverageType;
+    GrShaderVar          fGeomWind;
+    GrGLSLGeoToFrag      fFragWind;
+    GrGLSLGeoToFrag      fFragCoverageTimesWind;
+
+    typedef GrGLSLGeometryProcessor INHERITED;
+};
+
+#endif
