a30b6fd1e554b159d7fa3410141b69c860ad113b
Hal Canary <halcanary@google.com>
2017-07-05 11:25:42 -0400

    SkPDF: Refactor PDFShader to use ShTHashMap<>
    my tests run ~14% faster.
    
      - Split out gradient shaders from image shaders.  new compilation
        unit: SkPDFGradientShader
      - Common functions InverseTransformBBox and PopulateTilingPatternDict
        moved to SkPDFUtils
      - Split SkPDFShader::State into image and gradient structures.
      - SkPDFCanon is now a simpler structure, with no logic of its own.
        I am considering just moving all of its fields into SkPDFDocument
      - SkPDFShader::State (the image/fallback shader) now is POD, making
        the use of a hashmap for canonicalization straightforward.
        Formerly, we used a linear search.
      - Do not bother trying to canonicalize the falback image shader.
      - SkPDFGradientShader::Key is not POD; comparison of two objects
        requires looking at the contents of two variable-sized arrays.
        We now pre-calculate the hash of the arrays using SkOpts::hash and
        store a hash for the object in the fHash field.
        Using that hash, we can now canonicalize using a hashmap instead
        of a linar search!
      - several static functions renamed to follow style guidelines
      - stop using codeFunction function pointer; I find that less
        clear than it could be.
      - operator==() for SkPDFShader::State and SkPDFGradientShader::Key is
        now much simpler and can now be inlined.
      - SkArrayEqual template in SkPDFUtils.h
    
    No change to PDF output.
    
    BUG=skia:3585
    Change-Id: I354ad1b600be6d6749abccb58d13db257370bc0b

---
+++ src/pdf/SkPDFGradientShader.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef SkPDFGradientShader_DEFINED
+#define SkPDFGradientShader_DEFINED
+
+#include "SkPDFTypes.h"
+#include "SkPDFUtils.h"
+#include "SkShader.h"
+
+class SkMatrix;
+class SkPDFDocument;
+struct SkIRect;
+
+namespace SkPDFGradientShader {
+
+sk_sp<SkPDFObject> Make(SkPDFDocument* doc,
+                        SkShader* shader,
+                        const SkMatrix& matrix,
+                        const SkIRect& surfaceBBox);
+
+struct Key {
+    SkShader::GradientType fType;
+    SkShader::GradientInfo fInfo;
+    std::unique_ptr<SkColor[]> fColors;
+    std::unique_ptr<SkScalar[]> fStops;
+    SkMatrix fCanvasTransform;
+    SkMatrix fShaderTransform;
+    SkIRect fBBox;
+    uint32_t fHash;
+};
+
+struct KeyHash {
+    uint32_t operator()(const Key& k) const { return k.fHash; }
+};
+
+using HashMap = SkTHashMap<Key, sk_sp<SkPDFObject>, KeyHash>;
+
+inline bool operator==(const SkShader::GradientInfo& u, const SkShader::GradientInfo& v) {
+    return u.fColorCount    == v.fColorCount
+        && u.fPoint[0]      == v.fPoint[0]
+        && u.fPoint[1]      == v.fPoint[1]
+        && u.fRadius[0]     == v.fRadius[0]
+        && u.fRadius[1]     == v.fRadius[1]
+        && u.fTileMode      == v.fTileMode
+        && u.fGradientFlags == v.fGradientFlags
+        && SkPackedArrayEqual(u.fColors, v.fColors, u.fColorCount)
+        && SkPackedArrayEqual(u.fColorOffsets, v.fColorOffsets, u.fColorCount);
+}
+
+inline bool operator==(const Key& u, const Key& v) {
+    SkASSERT(u.fInfo.fColors       == u.fColors.get());
+    SkASSERT(u.fInfo.fColorOffsets == u.fStops.get());
+    SkASSERT(v.fInfo.fColors       == v.fColors.get());
+    SkASSERT(v.fInfo.fColorOffsets == v.fStops.get());
+    return u.fType            == v.fType
+        && u.fInfo            == v.fInfo
+        && u.fCanvasTransform == v.fCanvasTransform
+        && u.fShaderTransform == v.fShaderTransform
+        && u.fBBox            == v.fBBox;
+}
+inline bool operator!=(const Key& u, const Key& v) { return !(u == v); }
+
+}  // namespace SkPDFGradientShader
+#endif  // SkPDFGradientShader_DEFINED
