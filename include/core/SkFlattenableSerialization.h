5e64d48b3c58043ce38e7fe650e590354c011b04
Adrienne Walker <enne@chromium.org>
2017-07-05 13:31:53 -0700

    Add serialization overloads for typeface and textblob
    This is an alternative to https://skia-review.googlesource.com/c/18942/
    which chooses instead to expose a small set of interfaces instead of
    the entire SkReadBuffer.
    
    Bug: chromium: 737629
    Change-Id: I034d6eb3a35068b0e1ef3361ff133ada2062c6f9

--- include/core/SkFlattenableSerialization.h
+++ include/core/SkFlattenableSerialization.h
@@ -12,11 +12,19 @@
 
 class SkData;
 class SkImageFilter;
+class SkTextBlob;
+class SkTypeface;
 
 SK_API SkData* SkValidatingSerializeFlattenable(SkFlattenable*);
 SK_API SkFlattenable* SkValidatingDeserializeFlattenable(const void* data, size_t size,
                                                          SkFlattenable::Type type);
 
 SK_API sk_sp<SkImageFilter> SkValidatingDeserializeImageFilter(const void* data, size_t size);
+
+SK_API sk_sp<SkData> SkValidatingSerializeTypeface(SkTypeface*);
+SK_API sk_sp<SkTypeface> SkValidatingDeserializeTypeface(const void* data, size_t size);
+
+SK_API sk_sp<SkData> SkValidatingSerializeTextBlob(SkTextBlob*);
+SK_API sk_sp<SkTextBlob> SkValidatingDeserializeTextBlob(const void* data, size_t size);
 
 #endif
--- src/core/SkFlattenableSerialization.cpp
+++ src/core/SkFlattenableSerialization.cpp
@@ -10,6 +10,8 @@
 #include "SkData.h"
 #include "SkValidatingReadBuffer.h"
 #include "SkWriteBuffer.h"
+#include "SkTextBlob.h"
+#include "SkTypeface.h"
 
 SkData* SkValidatingSerializeFlattenable(SkFlattenable* flattenable) {
     SkBinaryWriteBuffer writer;
@@ -29,4 +31,32 @@ SkFlattenable* SkValidatingDeserializeFlattenable(const void* data, size_t size,
 sk_sp<SkImageFilter> SkValidatingDeserializeImageFilter(const void* data, size_t size) {
     return sk_sp<SkImageFilter>((SkImageFilter*)SkValidatingDeserializeFlattenable(
                                 data, size, SkImageFilter::GetFlattenableType()));
+}
+
+sk_sp<SkData> SkValidatingSerializeTypeface(SkTypeface* typeface) {
+    SkBinaryWriteBuffer writer;
+    writer.writeTypeface(typeface);
+    size_t size = writer.bytesWritten();
+    auto data = SkData::MakeUninitialized(size);
+    writer.writeToMemory(data->writable_data());
+    return data;
+}
+
+sk_sp<SkTypeface> SkValidatingDeserializeTypeface(const void* data, size_t size) {
+    SkValidatingReadBuffer buffer(data, size);
+    return buffer.readTypeface();
+}
+
+sk_sp<SkData> SkValidatingSerializeTextBlob(SkTextBlob* blob) {
+    SkBinaryWriteBuffer writer;
+    blob->flatten(writer);
+    size_t size = writer.bytesWritten();
+    auto data = SkData::MakeUninitialized(size);
+    writer.writeToMemory(data->writable_data());
+    return data;
+}
+
+sk_sp<SkTextBlob> SkValidatingDeserializeTextBlob(const void* data, size_t size) {
+    SkValidatingReadBuffer buffer(data, size);
+    return SkTextBlob::MakeFromBuffer(buffer);
 }
