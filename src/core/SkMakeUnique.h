6ddf11219e49f6be3f4c1c256df33d14f834cac1
Herb Derby <herb@google.com>
2017-07-07 15:20:03 -0400

    Experimental blur code.
    This uses a new method of blurring that runs the three 
    passes of the box filter in a single pass. This implementation
    currently only does 1x1 pixel at a time, but it should be simple
    to expand to 4x4 pixels at a time.
    
    On the  blur_10_normal_high_quality benchmark, the new and old
    code perform the same. But, for the blur_100.50_normal_high_quality
    benchmark, the new code is about twice as slow.
    
    Change-Id: Iea37294abc7c27de5ad569adf8bc62df77eafd02

--- src/core/SkMakeUnique.h
+++ src/core/SkMakeUnique.h
@@ -18,6 +18,11 @@ std::unique_ptr<T> make_unique(Args&&... args) {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
+template<typename T>
+std::unique_ptr<T> make_unique_default(size_t n) {
+    return std::unique_ptr<T>(new typename std::remove_extent<T>::type[n]);
+}
+
 }
 
 #endif  // SkMakeUnique_DEFINED
