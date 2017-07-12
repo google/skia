9450f8dc3c5a8d066abc374d1256b620893b7f4a
Brian Salomon <bsalomon@google.com>
2017-07-12 11:23:50 -0400

    Make ShadowRRectOp a non-legacy GrMeshDrawOp
    This also removes the ability to have an arbitrary GrPaint with this op and as a consequence simplifies the op and its GrGeometryProcessor.
    
    Change-Id: I19cc1c6f73a47e8925fc826291aad42e9423164d

--- src/gpu/effects/GrShadowGeoProc.cpp
+++ src/gpu/effects/GrShadowGeoProc.cpp
@@ -41,7 +41,6 @@ public:
                              uniformHandler,
                              gpArgs->fPositionVar,
                              rsgp.inPosition()->fName,
-                             rsgp.localMatrix(),
                              args.fFPCoordTransformHandler);
 
         fragBuilder->codeAppend("float d = length(shadowParams.xy);");
@@ -55,17 +54,7 @@ public:
 
     void setData(const GrGLSLProgramDataManager& pdman, const GrPrimitiveProcessor& proc,
                  FPCoordTransformIter&& transformIter) override {
-        this->setTransformDataHelper(proc.cast<GrRRectShadowGeoProc>().localMatrix(),
-                                     pdman, &transformIter);
-    }
-
-    static inline void GenKey(const GrGeometryProcessor& gp,
-                              const GrShaderCaps&,
-                              GrProcessorKeyBuilder* b) {
-        const GrRRectShadowGeoProc& rsgp = gp.cast<GrRRectShadowGeoProc>();
-        uint16_t key;
-        key = rsgp.localMatrix().hasPerspective() ? 0x1 : 0x0;
-        b->add32(key);
+        this->setTransformDataHelper(SkMatrix::I(), pdman, &transformIter);
     }
 
 private:
@@ -74,19 +63,12 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////////
 
-GrRRectShadowGeoProc::GrRRectShadowGeoProc(const SkMatrix& localMatrix)
-    : fLocalMatrix(localMatrix) {
-
+GrRRectShadowGeoProc::GrRRectShadowGeoProc() {
     this->initClassID<GrRRectShadowGeoProc>();
     fInPosition = &this->addVertexAttrib("inPosition", kVec2f_GrVertexAttribType,
                                          kHigh_GrSLPrecision);
     fInColor = &this->addVertexAttrib("inColor", kVec4ub_GrVertexAttribType);
     fInShadowParams = &this->addVertexAttrib("inShadowParams", kVec4f_GrVertexAttribType);
-}
-
-void GrRRectShadowGeoProc::getGLSLProcessorKey(const GrShaderCaps& caps,
-                                               GrProcessorKeyBuilder* b) const {
-    GrGLSLRRectShadowGeoProc::GenKey(*this, caps, b);
 }
 
 GrGLSLPrimitiveProcessor* GrRRectShadowGeoProc::createGLSLInstance(const GrShaderCaps&) const {
@@ -99,6 +81,6 @@ GR_DEFINE_GEOMETRY_PROCESSOR_TEST(GrRRectShadowGeoProc);
 
 #if GR_TEST_UTILS
 sk_sp<GrGeometryProcessor> GrRRectShadowGeoProc::TestCreate(GrProcessorTestData* d) {
-    return GrRRectShadowGeoProc::Make(GrTest::TestMatrix(d->fRandom));
+    return GrRRectShadowGeoProc::Make();
 }
 #endif
