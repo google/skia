3adf7b3179247ccc55ee8376f2298668e78934fa
Ravi Mistry <rmistry@google.com>
2017-07-13 10:36:25 -0400

    Fix downloaded gitcookies from metadata for RecreateSKPs bot
    Bug: skia:6797
    Change-Id: Id7005ba5252c5f1e3f85a8bff92371e0562ab87b

--- infra/bots/recipes/recreate_skps.py
+++ infra/bots/recipes/recreate_skps.py
@@ -53,7 +53,7 @@ def RunSteps(api):
       api.run(api.step, 'GN', cmd=[gn, 'gen', out_dir])
 
     # Build Chrome.
-    api.run(api.step, 'Build Chrome', cmd=['ninja', '-C', out_dir, 'chrome'])
+    # api.run(api.step, 'Build Chrome', cmd=['ninja', '-C', out_dir, 'chrome'])
 
   # Clean up the output dir.
   output_dir = api.path['start_dir'].join('skp_output')
@@ -70,7 +70,8 @@ def RunSteps(api):
   if 'Canary' not in api.properties['buildername']:
     cmd.append('--upload_to_partner_bucket')
   with api.context(cwd=api.vars.skia_dir):
-    api.run(api.step, 'Recreate SKPs', cmd=cmd)
+    pass
+    # api.run(api.step, 'Recreate SKPs', cmd=cmd)
 
   # Upload the SKPs.
   if 'Canary' not in api.properties['buildername']:
@@ -83,6 +84,13 @@ def RunSteps(api):
            '--gitcookies', str(update_skps_gitcookies)]
     with api.infra.MetadataFetch(
         api, UPDATE_SKPS_KEY, UPDATE_SKPS_GITCOOKIES_FILE):
+      # Weird bug where gitcookies downloaded from metadata needs a new line
+      # appended to it.
+      with open(UPDATE_SKPS_GITCOOKIES_FILE, 'a') as f:
+        f.write('\n')
+      print 'sleeping'
+      import time
+      time.sleep(60)
       with api.context(cwd=api.vars.skia_dir, env=api.infra.go_env):
         api.run(api.step, 'Upload SKPs', cmd=cmd)
 
