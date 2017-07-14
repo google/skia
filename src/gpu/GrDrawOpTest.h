fc26f3ca5f8f01b4ef47fd7ced4c589205e45ae8
Brian Salomon <bsalomon@google.com>
2017-07-14 15:27:56 -0400

    Remove GrAnalyticRectOp, the last legacy GrMeshDrawOp
    Change-Id: I2b8d1f4c1585af4a157d269a526874bccc74ac81
    Reviewed-on: https://skia-review.googlesource.com/23484
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- src/gpu/GrDrawOpTest.h
+++ src/gpu/GrDrawOpTest.h
@@ -31,10 +31,6 @@ void GrDrawRandomOp(SkRandom*, GrRenderTargetContext*, GrPaint&&);
     friend std::unique_ptr<GrDrawOp> Op##__Test(GrPaint&& paint, SkRandom* random, \
                                                 GrContext* context, GrFSAAType fsaaType)
 
-/** Variations for GrLegacyMeshDrawOps. To be deleted. */
-#define GR_LEGACY_MESH_DRAW_OP_TEST_DEFINE(Op) \
-    std::unique_ptr<GrLegacyMeshDrawOp> Op##__Test(SkRandom* random, GrContext* context)
-
 /** Helper for op test factories to pick a random stencil state. */
 const GrUserStencilSettings* GrGetRandomStencil(SkRandom* random, GrContext*);
 
