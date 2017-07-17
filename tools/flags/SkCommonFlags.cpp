c41d7f7fe2b2fb8a914a5b20dbf8fe1805ab663d
Yuqian Li <liyuqian@google.com>
2017-07-17 16:08:00 -0400

    New analytic AA scan converter using delta (I call it DAA for now)
    DAA is:
    
    1. Much simpler than AAA.
       SkScan_AAAPath.cpp is about 1700 lines.
       SkScan_DAAPath.cpp is about 300 lines.
       The whole DAA CL is only about 800 lines.
    
    2. Much faster than AAA for complicated paths.
       The speedup applies to GL backend (including ccpr)!
       Here's the frame time of 'SampleApp --slide Chart' on macbook pro:
         AAA-raster: 33ms
         DAA-raster: 21ms
         AAA-gl:     30ms
         DAA-gl:     20ms
         AAA-ccpr:   18ms
         DAA-ccpr:   12ms
       My linux desktop doesn't have SSE3 so the speedup is smaller
       (~25% for Chart). I believe that DAA is so fast that I can enable
       it for any paths (AAA is not enabled by default for complicated
       paths because it is slow.)
    
    3. The SkCoverageDelta is suitable for threaded backend with
       out-of-order concurrent scan conversion as commented in the source
       code. Maybe we can also just send deltas to GPU.
    
    4. Similar to most analytic path renderers, the quality is on the best
       ground-truth level, unless there are intersections within a pixel.
       The intersections look good to my eyes although theoretically that
       could be arbitrary far from the ground truth (see my AAA slides).
    
    5. For simple paths, such as circle, triangle, rrect, etc., DAA is
       slower than AAA. But DAA is faster than our older supersampling
       scan converter in most cases. As those simple paths usually don't
       constitute the bottleneck of a picture (skp or svg), I strongly
       recommend use DAA.
    
    Finally, please check https://skia-review.googlesource.com/c/22420/
    which accelerate DAA by specializing blitCoverageDeltas for
    SkARGB32_Blitter and SkARGB32_Black_Blitter. It brings a little(<5%)
    speedup. But I couldn't figure out how to reduce the duplicate code
    so I don't intend to land it.
    
    Bug: skia:
    Change-Id: I3b7ed6a727447922e645b1acb737a506e7c09a4c

--- tools/flags/SkCommonFlags.cpp
+++ tools/flags/SkCommonFlags.cpp
@@ -72,6 +72,9 @@ DEFINE_bool(forceAnalyticAA, false, "Force analytic anti-aliasing even if the pa
                                     "whether it's concave or convex, we consider a path complicated"
                                     "if its number of points is comparable to its resolution.");
 
+DEFINE_bool(deltaAA, false,
+            "If true, use delta anti-aliasing in all cases (it overrides forceAnalyticAA.");
+
 bool CollectImages(SkCommandLineFlags::StringArray images, SkTArray<SkString>* output) {
     SkASSERT(output);
 
