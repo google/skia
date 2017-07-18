38fb308f3156aab1e42a61d77d08906a06263879
Brian Salomon <bsalomon@google.com>
2017-07-18 17:03:16 +0000

    Revert "Improvements to GPU dither."
    This reverts commit 1806e33e6a09b1361de4925d32389a01e0e2c7e7.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Improvements to GPU dither.
    > 
    > 1) Makes the range of the offset dependent on the config.
    > 
    > 2) Uses an ordered dither on GPUs that support integers in shaders.
    > 
    > 3) Enables dithering for all paints with dither flag when the color type of the dst is 4444
    > 
    > 4) Dithers r,g,b and clamps to 0,a rather than dithering all four channels (same as CPU backend).
    > 
    > Bug: skia:
    > Change-Id: Ie22c3adc38c6d1dbbcd97e4b7d16fc843e392c2e
    > Reviewed-on: https://skia-review.googlesource.com/23485
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Robert Phillips <robertphillips@google.com>
    
    TBR=bsalomon@google.com,robertphillips@google.com
    
    Change-Id: Ie82e88bd9032bf8eee745d32d9b57c335a8997c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:
    Reviewed-on: https://skia-review.googlesource.com/24325
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/gpu/effects/GrDitherEffect.cpp
+++ src/gpu/effects/GrDitherEffect.cpp
@@ -23,7 +23,7 @@ public:
         GrGLSLFPFragmentBuilder* fragBuilder = args.fFragBuilder;
         const GrDitherEffect& _outer = args.fFp.cast<GrDitherEffect>();
         (void) _outer;
-        fragBuilder->codeAppendf("float value;\nfloat range;\n@switch (%d) {\n    case 0:\n        range = 0.0039215686274509803;\n        break;\n    case 1:\n        range = 0.015873015873015872;\n        break;\n    default:\n        range = 0.0083333333333333332;\n        break;\n}\n@if (sk_Caps.integerSupport) {\n    int x = int(sk_FragCoord.x);\n    int y = int(sk_FragCoord.y);\n    uint m = uint((((((y & 1) << 5 | (x & 1) << 4) | (y & 2) << 2) | (x & 2) << 1) | (y & 4) >> 1) | (x & 4) >> 2);\n    value = float(m) / 64.0 - 0.4921875;\n} else {\n    value = fract(sin(dot(sk_FragCoord.xy, vec2(12.989800000000001, 78.233000000000004))) * 43758.545299999998) - 0.5;\n}\n%s = vec4(clamp(%s.xyz + value * range, 0.0, %s.w), %s.w);\n", _outer.rangeType(), args.fOutputColor, args.fInputColor ? args.fInputColor : "vec4(1)", args.fInputColor ? args.fInputColor : "vec4(1)", args.fInputColor ? args.fInputColor : "vec4(1)");
+        fragBuilder->codeAppendf("float r = fract(sin(dot(sk_FragCoord.xy, vec2(12.989800000000001, 78.233000000000004))) * 43758.545299999998) - 0.5;\n%s = clamp(0.0039215686274509803 * vec4(r) + %s, 0.0, 1.0);\n", args.fOutputColor, args.fInputColor ? args.fInputColor : "vec4(1)");
     }
 private:
     void onSetData(const GrGLSLProgramDataManager& pdman, const GrFragmentProcessor& _proc) override {
@@ -33,20 +33,17 @@ GrGLSLFragmentProcessor* GrDitherEffect::onCreateGLSLInstance() const {
     return new GrGLSLDitherEffect();
 }
 void GrDitherEffect::onGetGLSLProcessorKey(const GrShaderCaps& caps, GrProcessorKeyBuilder* b) const {
-    b->add32(fRangeType);
 }
 bool GrDitherEffect::onIsEqual(const GrFragmentProcessor& other) const {
     const GrDitherEffect& that = other.cast<GrDitherEffect>();
     (void) that;
-    if (fRangeType != that.fRangeType) return false;
     return true;
 }
 GR_DEFINE_FRAGMENT_PROCESSOR_TEST(GrDitherEffect);
 #if GR_TEST_UTILS
 sk_sp<GrFragmentProcessor> GrDitherEffect::TestCreate(GrProcessorTestData* testData) {
 
-    float range = testData->fRandom->nextRangeF(0.001f, 0.05f);
-    return sk_sp<GrFragmentProcessor>(new GrDitherEffect(range));
+    return GrDitherEffect::Make();
 }
 #endif
 #endif
