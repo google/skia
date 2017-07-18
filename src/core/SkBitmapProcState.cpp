001e72263fee570f3a4398d58f9222c9d2c4926a
Florin Malita <fmalita@chromium.org>
2017-07-18 04:13:11 -0400

    Add guard to unblock the Chromium roll
    TBR=
    
    Change-Id: I87f96fb88479de770490ce4cce6ad840acb90241
    Reviewed-on: https://skia-review.googlesource.com/24261
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- src/core/SkBitmapProcState.cpp
+++ src/core/SkBitmapProcState.cpp
@@ -66,11 +66,15 @@ static bool just_trans_general(const SkMatrix& matrix) {
  *  for the purpose of filtering.
  */
 static bool just_trans_integral(const SkMatrix& m) {
+#ifdef SK_SUPPORT_LEGACY_BILERP
+    return false;
+#else
     static constexpr SkScalar tol = SK_Scalar1 / 256;
 
     return m.getType() <= SkMatrix::kTranslate_Mask
         && SkScalarNearlyEqual(m.getTranslateX(), SkScalarRoundToScalar(m.getTranslateX()), tol)
         && SkScalarNearlyEqual(m.getTranslateY(), SkScalarRoundToScalar(m.getTranslateY()), tol);
+#endif
 }
 
 static bool valid_for_filtering(unsigned dimension) {
