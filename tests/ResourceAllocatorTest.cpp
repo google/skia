4cd3713549aafb1ece5f49f47ab59d9595b7a474
Robert Phillips <robertphillips@google.com>
2017-07-18 15:14:15 -0400

    Add initial version of GrResourceAllocator's free pool
    Change-Id: Ibd60303ffb1d3ea814dad0cee3a521f94da63ca8

--- tests/ResourceAllocatorTest.cpp
+++ tests/ResourceAllocatorTest.cpp
@@ -19,9 +19,10 @@
 // assigned different GrSurfaces.
 static void overlap_test(skiatest::Reporter* reporter, GrResourceProvider* resourceProvider) {
     GrSurfaceDesc desc;
-    desc.fConfig = kRGBA_8888_GrPixelConfig;
+    desc.fOrigin = kTopLeft_GrSurfaceOrigin;
     desc.fWidth  = 64;
     desc.fHeight = 64;
+    desc.fConfig = kRGBA_8888_GrPixelConfig;
 
     sk_sp<GrSurfaceProxy> p1 = GrSurfaceProxy::MakeDeferred(resourceProvider, desc,
                                                             SkBackingFit::kApprox,
@@ -42,10 +43,40 @@ static void overlap_test(skiatest::Reporter* reporter, GrResourceProvider* resou
     REPORTER_ASSERT(reporter, p1->underlyingUniqueID() != p2->underlyingUniqueID());
 }
 
+// Basic test that two proxies with non-overlapping intervals and compatible descriptors are
+// assigned the same GrSurface. This also acts as a test of the ResourceAllocator's 
+// free pool.
+static void non_overlap_test(skiatest::Reporter* reporter, GrResourceProvider* resourceProvider) {
+    GrSurfaceDesc desc;
+    desc.fOrigin = kTopLeft_GrSurfaceOrigin;
+    desc.fWidth  = 64;
+    desc.fHeight = 64;
+    desc.fConfig = kRGBA_8888_GrPixelConfig;
+
+    sk_sp<GrSurfaceProxy> p1 = GrSurfaceProxy::MakeDeferred(resourceProvider, desc,
+                                                            SkBackingFit::kApprox,
+                                                            SkBudgeted::kNo);
+    sk_sp<GrSurfaceProxy> p2 = GrSurfaceProxy::MakeDeferred(resourceProvider, desc,
+                                                            SkBackingFit::kApprox,
+                                                            SkBudgeted::kNo);
+
+    GrResourceAllocator alloc(resourceProvider);
+
+    alloc.addInterval(p1.get(), 0, 2);
+    alloc.addInterval(p2.get(), 3, 5);
+
+    alloc.assign();
+
+    REPORTER_ASSERT(reporter, p1->priv().peekSurface());
+    REPORTER_ASSERT(reporter, p2->priv().peekSurface());
+    REPORTER_ASSERT(reporter, p1->underlyingUniqueID() == p2->underlyingUniqueID());
+}
+
 DEF_GPUTEST_FOR_ALL_CONTEXTS(ResourceAllocatorTest, reporter, ctxInfo) {
     GrResourceProvider* resourceProvider = ctxInfo.grContext()->resourceProvider();
 
     overlap_test(reporter, resourceProvider);
+    non_overlap_test(reporter, resourceProvider);
 }
 
 #endif
