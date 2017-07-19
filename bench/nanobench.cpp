36ad0a0b9c195fb992c072e46a70aef03b944982
Florin Malita <fmalita@chromium.org>
2017-07-19 15:36:49 -0400

    Crashy McCrashface
    Experiment with iOS nanobench crashes
    
    Change-Id: Ie7fea5bbde97da60e92be47765cafa449c2366b3

--- bench/nanobench.cpp
+++ bench/nanobench.cpp
@@ -1197,9 +1197,9 @@ int main(int argc, char** argv) {
     if (FLAGS_forceAnalyticAA) {
         gSkForceAnalyticAA = true;
     }
-    if (FLAGS_forceRasterPipeline) {
+//    if (FLAGS_forceRasterPipeline) {
         gSkForceRasterPipelineBlitter = true;
-    }
+//    }
 
     int runs = 0;
     BenchmarkStream benchStream;
