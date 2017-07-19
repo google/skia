4a1c014fd370fd7bc9f9a565593c14039d72aa29
Brian Osman <brianosman@google.com>
2017-07-19 17:02:17 -0400

    First stab at JSON event tracer
    Not yet thread safe (so it forces threading off).
    Builds JSON on the fly, so overhead is certainly bad.
    
    Plan to fix all of that, but this at least "works".
    
    Bug: skia:
    Change-Id: I3fbc22382b99418a508c670be2770195c0a1c364

---
+++ tools/trace/SkChromeTracingTracer.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkChromeTracingTracer_DEFINED
+#define SkChromeTracingTracer_DEFINED
+
+#include "SkEventTracer.h"
+#include "SkJSONCPP.h"
+#include "SkString.h"
+
+/**
+ * A SkEventTracer implementation that logs events to JSON for viewing with chrome://tracing.
+ */
+class SkChromeTracingTracer : public SkEventTracer {
+public:
+    SkChromeTracingTracer() {}
+
+    SkEventTracer::Handle addTraceEvent(char phase,
+                                        const uint8_t* categoryEnabledFlag,
+                                        const char* name,
+                                        uint64_t id,
+                                        int numArgs,
+                                        const char** argNames,
+                                        const uint8_t* argTypes,
+                                        const uint64_t* argValues,
+                                        uint8_t flags) override;
+
+    void updateTraceEventDuration(const uint8_t* categoryEnabledFlag,
+                                  const char* name,
+                                  SkEventTracer::Handle handle) override;
+
+    const uint8_t* getCategoryGroupEnabled(const char* name) override;
+
+    const char* getCategoryGroupName(const uint8_t* categoryEnabledFlag) override {
+        static const char* category = "category?";
+        return category;
+    }
+
+    void writeToFile(const char* filename);
+
+private:
+    Json::Value fRoot;
+};
+
+#endif
