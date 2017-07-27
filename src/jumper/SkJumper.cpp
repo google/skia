e884ddc255bb67b6456438ad5cb39b7f048cd4f1
Mike Klein <mtklein@chromium.org>
2017-07-27 12:19:00 -0400

    clean up SK_SUPPORT_LEGACY_WIN32_JUMPER
    Change-Id: Icae3c6ce80a0bef097ea1010a4d065cc9d5a4c88

--- src/jumper/SkJumper.cpp
+++ src/jumper/SkJumper.cpp
@@ -154,8 +154,7 @@ extern "C" {
         LOWP_STAGES(M)
     #undef M
 
-#elif (defined(__i386__) || defined(_M_IX86)) && \
-        !(defined(_MSC_VER) && defined(SK_SUPPORT_LEGACY_WIN32_JUMPER))
+#elif defined(__i386__) || defined(_M_IX86)
     StartPipelineFn ASM(start_pipeline,sse2);
     StageFn ASM(just_return,sse2);
     #define M(st) StageFn ASM(st,sse2);
@@ -270,8 +269,7 @@ static SkJumper_Engine choose_engine() {
         };
     }
 
-#elif (defined(__i386__) || defined(_M_IX86)) && \
-        !(defined(_MSC_VER) && defined(SK_SUPPORT_LEGACY_WIN32_JUMPER))
+#elif defined(__i386__) || defined(_M_IX86)
     if (1 && SkCpu::Supports(SkCpu::SSE2)) {
         return {
         #define M(stage) ASM(stage, sse2),
