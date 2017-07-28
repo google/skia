eee5183a322dd902de743ac02f33a5fd3181b0b6
Greg Daniel <egdaniel@google.com>
2017-07-28 11:31:19 -0400

    Add placeholder GrMtlTypes in include/gpu/mtl
    This is needed since there are currently no files in the include dir, and
    some of our tests that check all the public headers complain if it doesn't
    exists.
    
    Bug: skia:6896
    Change-Id: I3f70293b64b5096a55d6ba0ea5f4e6ebbfd7f62b
    Reviewed-on: https://skia-review.googlesource.com/28003
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

---
+++ include/gpu/mtl/GrMtlTypes.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrMtlTypes_DEFINED
+#define GrMtlTypes_DEFINED
+
+#include "GrTypes.h"
+
+// This is a placeholder class until we fill it out. This is needed so we can have the mtl include
+// path in our BUILD.gn
+
+#endif
