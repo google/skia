ca1c96e00be19af9c6ebc9b2803502da2c84e389
Ben Wagner <bungeman@google.com>
2017-07-31 10:18:09 -0400

    Remove union from SkFontStyle.
    This is prompted by a bug in vc++ reported at
    https://developercommunity.visualstudio.com/content/problem/86829/constexpr-initializing-struct-in-union-only-zero-i.html
    
    Visual C++ will not properly constexpr initialize a struct inside a
    union. Removing this construct also allows us to avoid writing and
    reading from different members of a union. By using memcmp as the
    comparison operator (and asserting that this is valid) compilers will
    tuen the comparison operator into a single compare instruction.
    
    Change-Id: I76e069ecce77ef21b7ad3821bf9c29e1b0843618

--- include/core/SkFontStyle.h
+++ include/core/SkFontStyle.h
@@ -44,23 +44,27 @@ public:
         kOblique_Slant,
     };
 
-    SkFontStyle();
+    constexpr SkFontStyle()
+        : fWeight(kNormal_Weight), fWidth(kNormal_Width), fSlant(kUpright_Slant)
+    { }
 
-    constexpr SkFontStyle(int weight, int width, Slant slant) : fUnion {{
-        static_cast<uint16_t>(SkTPin<int>(weight, kInvisible_Weight, kExtraBlack_Weight)),
-        static_cast<uint8_t >(SkTPin<int>(width, kUltraCondensed_Width, kUltraExpanded_Width)),
-        static_cast<uint8_t >(SkTPin<int>(slant, kUpright_Slant, kOblique_Slant))
-    }} { }
+    constexpr SkFontStyle(int weight, int width, Slant slant)
+        : fWeight(static_cast<uint16_t>(SkTPin<int>(weight, kInvisible_Weight, kExtraBlack_Weight)))
+        , fWidth (static_cast<uint8_t >(SkTPin<int>(width, kUltraCondensed_Width, kUltraExpanded_Width)))
+        , fSlant (static_cast<uint8_t >(SkTPin<int>(slant, kUpright_Slant, kOblique_Slant)))
+    { }
 
     static SkFontStyle FromOldStyle(unsigned oldStyle);
 
     bool operator==(const SkFontStyle& rhs) const {
-        return fUnion.fU32 == rhs.fUnion.fU32;
+        static_assert(sizeof(SkFontStyle) == sizeof(fWeight) + sizeof(fWidth) + sizeof(fSlant),
+                      "SkFontStyle must be packed.");
+        return memcmp(this, &rhs, sizeof(SkFontStyle)) == 0;
     }
 
-    int weight() const { return fUnion.fR.fWeight; }
-    int width() const { return fUnion.fR.fWidth; }
-    Slant slant() const { return (Slant)fUnion.fR.fSlant; }
+    int weight() const { return fWeight; }
+    int width() const { return fWidth; }
+    Slant slant() const { return (Slant)fSlant; }
 
     static constexpr SkFontStyle Normal() {
         return SkFontStyle(kNormal_Weight, kNormal_Width, kUpright_Slant);
@@ -76,14 +80,9 @@ public:
     }
 
 private:
-    union {
-        struct {
-            uint16_t fWeight;   // 100 .. 900
-            uint8_t  fWidth;    // 1 .. 9
-            uint8_t  fSlant;    // 0 .. 2
-        } fR;
-        uint32_t    fU32;
-    } fUnion;
+    uint16_t fWeight;   // 100 .. 900
+    uint8_t fWidth;     // 1 .. 9
+    uint8_t fSlant;     // 0 .. 2
 };
 
 #endif
