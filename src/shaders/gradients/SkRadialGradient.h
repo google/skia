ad6b1d9bfd41f0c74e9217bae75c990e17ad2d35
Florin Malita <fmalita@chromium.org>
2017-08-02 11:03:44 -0400

    Delete legacy radial gradient impl
    Raster pipeline FTW!
    
    Change-Id: Ia2e600cf6c6e780f5a1519374c178197cf229776

--- src/shaders/gradients/SkRadialGradient.h
+++ src/shaders/gradients/SkRadialGradient.h
@@ -14,16 +14,6 @@ class SkRadialGradient final : public SkGradientShaderBase {
 public:
     SkRadialGradient(const SkPoint& center, SkScalar radius, const Descriptor&);
 
-    class RadialGradientContext : public SkGradientShaderBase::GradientShaderBaseContext {
-    public:
-        RadialGradientContext(const SkRadialGradient&, const ContextRec&);
-
-        void shadeSpan(int x, int y, SkPMColor dstC[], int count) override;
-
-    private:
-        typedef SkGradientShaderBase::GradientShaderBaseContext INHERITED;
-    };
-
     GradientType asAGradient(GradientInfo* info) const override;
 #if SK_SUPPORT_GPU
     sk_sp<GrFragmentProcessor> asFragmentProcessor(const AsFPArgs&) const override;
@@ -35,13 +25,12 @@ public:
 protected:
     SkRadialGradient(SkReadBuffer& buffer);
     void flatten(SkWriteBuffer& buffer) const override;
-    Context* onMakeContext(const ContextRec&, SkArenaAlloc*) const override;
     sk_sp<SkShader> onMakeColorSpace(SkColorSpaceXformer* xformer) const override;
     
     void appendGradientStages(SkArenaAlloc* alloc, SkRasterPipeline* tPipeline,
                               SkRasterPipeline* postPipeline) const override;
 
-    bool onIsRasterPipelineOnly() const override;
+    bool onIsRasterPipelineOnly() const override { return true; }
 
 private:
     const SkPoint fCenter;
