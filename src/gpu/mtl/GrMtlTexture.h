0d473f8d8da6135bfef50786843443f8d97080f3
Greg Daniel <egdaniel@google.com>
2017-08-03 16:25:13 -0400

    Add GrMtlTexture classes
    Adds support for basic Texture creation.
    
    Bug: skia:
    Change-Id: I9a3f15bef1c88054c19e952e231cad94ad69f296

---
+++ src/gpu/mtl/GrMtlTexture.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrMtlTexture_DEFINED
+#define GrMtlTexture_DEFINED
+
+#include "GrTexture.h"
+
+#import <Metal/Metal.h>
+
+class GrMtlGpu;
+
+class GrMtlTexture : public GrTexture {
+public:
+    static sk_sp<GrMtlTexture> CreateNewTexture(GrMtlGpu*, SkBudgeted budgeted,
+                                                const GrSurfaceDesc&, int mipLevels);
+
+    static sk_sp<GrMtlTexture> MakeWrappedTexture(GrMtlGpu*, const GrSurfaceDesc&,
+                                                  GrWrapOwnership);
+
+    ~GrMtlTexture() override;
+
+    id<MTLTexture> mtlTexture() const { return fTexture; }
+
+    GrBackendObject getTextureHandle() const override;
+
+    void textureParamsModified() override {}
+
+    bool reallocForMipmap(GrMtlGpu* gpu, uint32_t mipLevels);
+
+    void setRelease(GrTexture::ReleaseProc proc, GrTexture::ReleaseCtx ctx) override {
+        // Since all MTLResources are inherently ref counted, we can call the Release proc when we
+        // delete the GrMtlTexture without worry of the MTLTexture getting deleted before it is done
+        // on the GPU.
+        fReleaseProc = proc;
+        fReleaseCtx = ctx;
+    }
+
+protected:
+    GrMtlTexture(GrMtlGpu*, const GrSurfaceDesc&);
+
+    GrMtlGpu* getMtlGpu() const;
+
+    void onAbandon() override {
+        fTexture = nil;
+    }
+    void onRelease() override {
+        fTexture = nil;
+    }
+
+private:
+    enum Wrapped { kWrapped };
+    GrMtlTexture(GrMtlGpu*, SkBudgeted, const GrSurfaceDesc&, id<MTLTexture>, bool isMipMapped);
+   // GrMtlTexture(GrMtlGpu*, Wrapped, const GrSurfaceDesc&, GrMtlImage::Wrapped wrapped);
+
+    id<MTLTexture> fTexture;
+
+    ReleaseProc fReleaseProc = nullptr;
+    ReleaseCtx fReleaseCtx = nullptr;
+
+    typedef GrTexture INHERITED;
+};
+
+#endif
