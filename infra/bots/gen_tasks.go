ae532f68a035e50530c846233fe60dabdd9e3414
Ben Wagner <benjaminwagner@google.com>
2017-08-02 23:24:16 -0400

    Add Win10 QuadroP400 bots.
    Bug: skia:6883
    Change-Id: I0812bc0715f10e3722535af2cfc6fdf812cc487f
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/26366
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -184,8 +184,8 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 		if !ok {
 			glog.Fatalf("Entry %q not found in OS mapping.", os)
 		}
-		// Chrome Golo has a different Windows image.
-		if parts["model"] == "Golo" && os == "Win10" {
+		// These machines have a different Windows image.
+		if parts["model"] == "Golo" && os == "Win10" && parts["cpu_or_gpu_value"] == "GT610" {
 			d["os"] = "Windows-10-10586"
 		}
 	} else {
@@ -267,6 +267,7 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 					"IntelIris540":  "8086:1926-21.20.16.4590",
 					"IntelIris6100": "8086:162b-20.19.15.4624",
 					"RadeonR9M470X": "1002:6646-22.19.165.512",
+					"QuadroP400":    "10de:1cb3-22.21.13.8205",
 				}[parts["cpu_or_gpu_value"]]
 				if !ok {
 					glog.Fatalf("Entry %q not found in Win GPU mapping.", parts["cpu_or_gpu_value"])
