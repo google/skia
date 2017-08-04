1867eb9c66a3f717a501be2ed771973bbad2883d
Robert Phillips <robertphillips@google.com>
2017-08-04 12:40:40 -0400

    DeferredDisplayList API proposal
    Change-Id: I75faa483ab5a6b779c8de56ea56b9d90b990f43a

---
+++ include/core/SkDeferredDisplayListMaker.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkDeferredDisplayListMaker_DEFINED
+#define SkDeferredDisplayListMaker_DEFINED
+
+#include "SkRefCnt.h"
+#include "SkSurfaceCharacterization.h"
+
+#include "../private/SkDeferredDisplayList.h"
+
+class SkCanvas;
+class SkSurface; // TODO: remove
+
+/*
+ * This class is intended to be used as:
+ *   Get an SkSurfaceCharacterization from the ultimate intended destination SkSurface
+ *   Create one of these on the stack
+ *   Get the canvas and render into it
+ *   Snap off and hold on to an SkDeferredDisplayList
+ *   Once your app actually needs the pixels, call SkSurface::draw(SkDeferredDisplayList*)
+ *
+ * This class never accesses the GPU but performs all the cpu work it can. It
+ * is thread-safe (i.e., one can break a scene into tiles and perform their cpu-side
+ * work in parallel ahead of time).
+ */
+class SkDeferredDisplayListMaker {
+public:
+    SkDeferredDisplayListMaker(const SkSurfaceCharacterization&);
+
+    const SkSurfaceCharacterization& characterization() const {
+        return fCharacterization;
+    }
+
+    // The backing canvas will become invalid (and this entry point will return
+    // null) once 'finish' is called.
+    SkCanvas* canvas();
+
+    std::unique_ptr<SkDeferredDisplayList> finish();
+
+private:
+    SkSurfaceCharacterization fCharacterization;
+
+    sk_sp<SkSurface> fSurface; // temporary until we have a real implementation
+};
+
+#endif
