71711d4c7ed80baa4226d8999a9f990e9021722b
Mike Reed <reed@google.com>
2017-08-06 21:27:53 -0400

    clean up useage of SkFloatBits
    Bug: skia:
    Change-Id: I6d3a0019f2fcf11feca69123e4ce6eb35de43613

--- tests/PathOpsSkpTest.cpp
+++ tests/PathOpsSkpTest.cpp
@@ -4,6 +4,8 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
+#include "../private/SkFloatBits.h"
 #include "PathOpsExtendedTest.h"
 
 #define TEST(name) { name, #name }
