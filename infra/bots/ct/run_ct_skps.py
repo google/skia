0ac06e47269a40c177747310a613d213c95d1d6d
Ravi Mistry <rmistry@google.com>
2017-08-08 07:39:09 -0400

    Bot updates for SkiaCT tasks.
    Use gcc-4.8 when compiling for Ubuntu14. Rename the CT tasks to make it
    clear their child tasks run on Ubuntu14.
    
    Move to Debian-9.1 bots created with the changes in
    https://skia-review.googlesource.com/c/31281 and
    https://skia-review.googlesource.com/c/31820
    
    Change the cache dir used by the ct_skps recipe so that changing the
    bot name doesn't invalidate the cache.
    
    Bug: skia:6921
    Bug: skia:6818
    No-Try: true
    Change-Id: Idaac8187427b5c2c7bffe66ad6674496b8cd0c0e
    Reviewed-on: https://skia-review.googlesource.com/31242
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- infra/bots/ct/run_ct_skps.py
+++ infra/bots/ct/run_ct_skps.py
@@ -35,11 +35,18 @@ def main():
                            'isolateserver all artifacts in this dir.')
   parser.add_argument('-b', '--builder', required=True,
                       help='The name of the builder.')
+  parser.add_argument('-r', '--chromium_build', required=True,
+                      help='The chromium build used to create the SKP repo.')
+  parser.add_argument('-p', '--page_type', required=True,
+                      help='The CT page type.')
+  parser.add_argument('-n', '--num_slaves', required=True,
+                      help='Total number of slaves used to run the build.')
   args = parser.parse_args()
 
   tool_path = os.path.join(PARENT_DIR, args.tool)
-  skps_dir = os.path.join(REPOS_BASE_DIR, 'skps', args.builder,
-                          'slave%d' % args.slave_num)
+  skps_dir = os.path.join(
+      REPOS_BASE_DIR, 'skps', args.chromium_build, args.page_type,
+      args.num_slaves, 'slave%d' % args.slave_num)
   resource_path = os.path.join(SKIA_SRC_DIR, 'resources')
 
   cmd = [tool_path]
