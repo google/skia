1042862b353a3adb8e82f27b8fb5412ba0f50d20
Ben Wagner <benjaminwagner@google.com>
2017-08-09 17:51:55 -0400

    Fail dm/nanobench when unrecognized configs.
    Change-Id: I23e31355e2faaab919d92abdb37a6f70cd2da1ff

--- bench/nanobench.cpp
+++ bench/nanobench.cpp
@@ -97,6 +97,8 @@ static SkString to_string(int n) {
     return str;
 }
 
+DECLARE_bool(undefok);
+
 DEFINE_int32(loops, kDefaultLoops, loops_help_txt().c_str());
 
 DEFINE_int32(samples, 10, "Number of samples to measure for each bench.");
@@ -488,6 +490,17 @@ void create_configs(SkTArray<Config>* configs) {
     for (int i = 0; i < array.count(); ++i) {
         create_config(array[i].get(), configs);
     }
+
+    // If no just default configs were requested, then we're okay.
+    if (array.count() == 0 || FLAGS_config.count() == 0 ||
+        // If we've been told to ignore undefined flags, we're okay.
+        FLAGS_undefok ||
+        // Otherwise, make sure that all specified configs have been created.
+        array.count() == configs.count()) {
+        return
+    }
+    SkDebugf("Invalid --config. Use --undefok to bypass this warning.");
+    exit(1);
 }
 
 // disable warning : switch statement contains default but no 'case' labels
