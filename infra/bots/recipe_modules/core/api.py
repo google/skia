a5b207ccab054e00c1c6ad581b349169f22740e2
Ben Wagner <bungeman@google.com>
2017-08-10 13:55:37 -0400

    NOT COMMIT: try bisecting Chromium comand buffer.
    Change-Id: Ieb065ecbd1ac71a1b528613068bd7deb77becf00

--- infra/bots/recipe_modules/core/api.py
+++ infra/bots/recipe_modules/core/api.py
@@ -119,7 +119,7 @@ class SkiaApi(recipe_api.RecipeApi):
       chromium.name = 'src'
       chromium.managed = False
       chromium.url = 'https://chromium.googlesource.com/chromium/src.git'
-      chromium.revision = 'origin/lkcr'
+      chromium.revision = 'c83e47a22fe4'
 
     # Run bot_update.
 
