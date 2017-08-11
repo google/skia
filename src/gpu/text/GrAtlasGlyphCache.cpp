86fd20daf54c560e6bc36b47757aa7abf2a7a2db
Brian Osman <brianosman@google.com>
2017-08-11 13:34:02 -0400

    Replace log2 with SkNextLog2
    log2 didn't exist on Android until API 18, and it's doing double
    precision math. I think this integer version is what we want?
    
    Bug: skia:
    Change-Id: I4909153c56a266688355349cda5d553b69f5c942
    Reviewed-on: https://skia-review.googlesource.com/33680
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/gpu/text/GrAtlasGlyphCache.cpp
+++ src/gpu/text/GrAtlasGlyphCache.cpp
@@ -12,6 +12,7 @@
 #include "GrResourceProvider.h"
 #include "GrSurfacePriv.h"
 #include "SkAutoMalloc.h"
+#include "SkMathPriv.h"
 #include "SkString.h"
 
 #include "SkDistanceFieldGen.h"
@@ -39,7 +40,7 @@ bool GrAtlasGlyphCache::initAtlas(GrMaskFormat format) {
 GrAtlasGlyphCache::GrAtlasGlyphCache(GrContext* context, float maxTextureBytes)
         : fContext(context), fPreserveStrike(nullptr) {
     // Calculate RGBA size. Must be between 1024 x 512 and MaxTextureSize x MaxTextureSize / 2
-    int log2MaxTextureSize = log2(context->caps()->maxTextureSize());
+    int log2MaxTextureSize = SkPrevLog2(context->caps()->maxTextureSize());
     int log2MaxDim = 10;
     for (; log2MaxDim <= log2MaxTextureSize; ++log2MaxDim) {
         int maxDim = 1 << log2MaxDim;
