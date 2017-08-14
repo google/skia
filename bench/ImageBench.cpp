c9c9c87089f0b300b3adc48012f466e6b46d6b56
Brian Salomon <bsalomon@google.com>
2017-08-14 10:40:58 -0400

    Add benchmark for overlapping texture backed images
    Change-Id: I5ab9f6e81055451ac912a66537843d1a49f3b479

--- bench/ImageBench.cpp
+++ bench/ImageBench.cpp
@@ -8,6 +8,7 @@
 #include "Benchmark.h"
 #include "SkCanvas.h"
 #include "SkImage.h"
+#include "SkRandom.h"
 #include "SkSurface.h"
 
 class Image2RasterBench : public Benchmark {
@@ -62,4 +63,65 @@ private:
 
     typedef Benchmark INHERITED;
 };
+
+class OverlappingTextureBackedImages : public Benchmark {
+public:
+    OverlappingTextureBackedImages() {
+        fName.set("overlapping_texture_image_bench");
+    }
+
+    bool isSuitableFor(Backend backend) override {
+        return kGPU_Backend == backend;
+    }
+
+protected:
+    const char* onGetName() override {
+        return fName.c_str();
+    }
+
+    void onPerCanvasPreDraw(SkCanvas* canvas) override {
+        auto ii = SkImageInfo::Make(kImageSize, kImageSize, kRGBA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+        auto surf = canvas->makeSurface(ii);
+        SkRandom random;
+        for (int i = 0; i < kNumImages; ++i) {
+            surf->getCanvas()->clear(random.nextU());
+            SkPaint paint;
+            paint.setColor(random.nextU());
+            surf->getCanvas()->drawRect(SkRect::MakeLTRB(3, 3, kImageSize - 3, kImageSize - 3), paint);
+            fImages[i] = surf->makeImageSnapshot();
+        }
+    }
+
+    void onPerCanvasPostDraw(SkCanvas*) override {
+        for (int i = 0; i < kNumImages; ++i) {
+            fImages[i].reset();
+        }
+    }
+
+    void onDraw(int loops, SkCanvas* canvas) override {
+        SkScalar x = 0, y = 0;
+        for (int i = 0; i < loops; i++) {
+            canvas->drawImage(fImages[i % kNumImages].get(), x, y);
+            x += kImageSize / 2;
+            y += kImageSize / 3.5;
+            if (x > 500.f) {
+                x = 0.f;
+            }
+            if (y > 500.f) {
+                y = 0.f;
+            }
+        }
+    }
+
+private:
+    static const int kNumImages = 5;
+    static const int kImageSize = 30;
+    sk_sp<SkImage> fImages[kNumImages];
+    SkString fName;
+
+    typedef Benchmark INHERITED;
+};
+
+
 DEF_BENCH( return new Image2RasterBench; )
+DEF_BENCH( return new OverlappingTextureBackedImages);
