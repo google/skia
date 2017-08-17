60845d841ad7a7c2359e47c5151a9be85e7b026f
Ben Wagner <benjaminwagner@google.com>
2017-08-16 20:11:38 -0400

    Add SurfacePro2017 tasks.
    Bug: skia:
    Change-Id: I8f9fa7ecc5d195658944f8f29a91ddaf47c066fc

--- infra/bots/recipes/perf.py
+++ infra/bots/recipes/perf.py
@@ -155,7 +155,10 @@ def nanobench_flags(api, bot):
     match.append('~GLInstancedArraysBench') # skia:4714
   if 'IntelIris540' in bot and 'ANGLE' in bot:
     match.append('~tile_image_filter_tiled_64')  # skia:6082
-  if ('Vulkan' in bot and ('IntelIris540' in bot or 'IntelIris640' in bot) and
+  if ('Vulkan' in bot and
+      ('IntelHD615' in bot or
+       'IntelIris540' in bot or
+       'IntelIris640' in bot) and
       'Win' in bot):
     # skia:6398
     match.append('~GM_varied_text_clipped_lcd')
