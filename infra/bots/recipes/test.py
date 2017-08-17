fb5eeb54b52a15b5057cb2fbbffea3be8e4e4c48
Ben Wagner <benjaminwagner@google.com>
2017-08-16 20:11:38 -0400

    Add SurfacePro2017 tasks.
    Not adding Vulkan configs because it mostly produces garbage -- see
    skia:6398.
    
    Bug: skia:6978
    NoTry: true
    Change-Id: I8f9fa7ecc5d195658944f8f29a91ddaf47c066fc

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -501,7 +501,8 @@ def dm_flags(api, bot):
       ('IntelIris540' in bot or 'IntelIris640' in bot)):
     match.extend(['~VkHeapTests']) # skia:6245
 
-  if 'Vulkan' in bot and 'IntelIris540' in bot and 'Win' in bot:
+  if 'Vulkan' in bot and ('IntelIris540' in bot or
+                          'IntelHD615' in bot) and 'Win' in bot:
     # skia:6398
     blacklist(['vk', 'gm', '_', 'aarectmodes'])
     blacklist(['vk', 'gm', '_', 'aaxfermodes'])
