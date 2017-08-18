b186236e1d726f49d26a3b32c9df37d78822a6b2
Greg Daniel <egdaniel@google.com>
2017-08-18 09:31:14 -0400

    Add GrMtlRenderTarget class
    Adds basic support for creating non-msaa pure render targets.
    
    Bug: skia:
    Change-Id: I63d2d474b86fc0ff5d8ee7757c08abbfd5e6c6ef

---
+++ src/gpu/mtl/GrMtlRenderTarget.h
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrMtlRenderTarget_DEFINED
+#define GrMtlRenderTarget_DEFINED
+
+#include "GrRenderTarget.h"
+
+#import <Metal/Metal.h>
+
+class GrMtlGpu;
+
+class GrMtlRenderTarget: public GrRenderTarget {
+public:
+    static sk_sp<GrMtlRenderTarget> CreateNewRenderTarget(GrMtlGpu*, const GrSurfaceDesc&,
+                                                          SkBudgeted);
+
+    static sk_sp<GrMtlRenderTarget> MakeWrappedRenderTarget(GrMtlGpu*, const GrSurfaceDesc&);
+
+    ~GrMtlRenderTarget() override;
+
+    // override of GrRenderTarget
+    ResolveType getResolveType() const override {
+        return kCantResolve_ResolveType;
+#if 0 // TODO figure this once we support msaa
+        if (this->numColorSamples() > 1) {
+            return kCanResolve_ResolveType;
+        }
+        return kAutoResolves_ResolveType;
+#endif
+    }
+
+    bool canAttemptStencilAttachment() const override {
+        return true;
+    }
+
+    GrBackendObject getRenderTargetHandle() const override;
+
+protected:
+    GrMtlRenderTarget(GrMtlGpu* gpu,
+                      const GrSurfaceDesc& desc,
+                      id<MTLTexture> renderTexture,
+                      id<MTLTexture> resolveTexture);
+
+    GrMtlRenderTarget(GrMtlGpu* gpu,
+                      const GrSurfaceDesc& desc,
+                      id<MTLTexture> renderTexture);
+
+    GrMtlGpu* getMtlGpu() const;
+
+    void onAbandon() override;
+    void onRelease() override;
+
+    // This accounts for the texture's memory and any MSAA renderbuffer's memory.
+    size_t onGpuMemorySize() const override {
+        // The plus 1 is to account for the resolve texture or if not using msaa the RT itself
+        int numColorSamples = this->numColorSamples() + 1;
+        return GrSurface::ComputeSize(this->config(), this->width(), this->height(),
+                                      numColorSamples, false);
+    }
+
+    id<MTLTexture> fRenderTexture;
+    id<MTLTexture> fResolveTexture;
+
+private:
+    GrMtlRenderTarget(GrMtlGpu* gpu,
+                      const GrSurfaceDesc& desc,
+                      SkBudgeted,
+                      id<MTLTexture> renderTexture,
+                      id<MTLTexture> resolveTexture);
+
+    GrMtlRenderTarget(GrMtlGpu* gpu,
+                      const GrSurfaceDesc& desc,
+                      SkBudgeted,
+                      id<MTLTexture> renderTexture);
+
+    static sk_sp<GrMtlRenderTarget> Make(GrMtlGpu*,
+                                         const GrSurfaceDesc&,
+                                         SkBudgeted,
+                                         id<MTLTexture> renderTexture,
+                                         bool isWrapped);
+
+    bool completeStencilAttachment() override;
+};
+
+
+#endif
+
