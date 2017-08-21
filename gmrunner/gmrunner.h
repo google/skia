a5ac4e02946bb798d4d4de165521d4969075b8d7
Stephan Altmueller <stephana@google.com>
2017-08-21 16:47:10 -0400

    WIP: cts app
    Bug: skia:
    Change-Id: I43c0ee079374d7c2575bba0652d783c961af4073

---
+++ gmrunner/gmrunner.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// EXPERIMENTAL EXPERIMENTAL EXPERIMENTAL EXPERIMENTAL
+// DO NOT USE -- FOR INTERNAL TESTING ONLY
+
+#ifndef gmrunner_DEFINED
+#define gmrunner_DEFINED
+
+#include <string>
+
+struct ImageData {
+  char* data;
+  int height;
+  int width;
+  int byteOrder;
+};
+
+bool gmrunner_init();
+std::string gmrunner_run_test(const char* testName, ImageData** byteOrder);
+
+#endif
