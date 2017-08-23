6cdfedf2b7b01524978b7188e0b294f0f8d40d97
Cary Clark <caryclark@skia.org>
2017-08-23 09:38:24 -0400

    simplify path validate
    Reduce path validation interfaces,
    deferring the harder work of rewriting
    the callers until later.
    
    R=reed@google.com,enne@chromium.org
    
    Change-Id: Iea56f1cd1be93bb1d96b50836a9bd3cd4872ad23

--- include/core/SkPath.h
+++ include/core/SkPath.h
@@ -1119,10 +1119,18 @@ public:
     static const int kPathRefGenIDBitCnt = 32;
 #endif
 
-    bool isValid() const;
-    bool pathRefIsValid() const { return fPathRef->isValid(); }
-    SkDEBUGCODE(void validate() const { SkASSERT(this->isValid()); } )
-    SkDEBUGCODE(void experimentalValidateRef() const { fPathRef->validate(); } )
+    /** Returns if SkPath data is consistent. Corrupt SkPath data is detected if
+        internal values are out of range or internal storage does not match
+        array dimensions.
+
+        @return  true if SkPath data is consistent
+     */
+    bool isValid() const { return this->isValidImpl() && fPathRef->isValid(); }
+
+    /** Asserts if SkPath data is inconsistent.
+        Debugging check intended for internal use only.
+     */
+    SkDEBUGCODE(void validate() const { SkASSERT(this->isValidImpl()); } )
 
 private:
     enum SerializationOffsets {
@@ -1182,6 +1190,8 @@ private:
     inline bool hasOnlyMoveTos() const;
 
     Convexity internalGetConvexity() const;
+    bool isValidImpl() const;
+    SkDEBUGCODE(void validateRef() const { fPathRef->validate(); } )
 
     bool isRectContour(bool allowPartial, int* currVerb, const SkPoint** pts,
                        bool* isClosed, Direction* direction) const;
@@ -1215,6 +1225,7 @@ private:
     friend class SkBench_AddPathTest; // perf test reversePathTo
     friend class PathTest_Private; // unit test reversePathTo
     friend class ForceIsRRect_Private; // unit test isRRect
+    friend class FuzzPath; // for legacy access to validateRef
 };
 
 #endif
