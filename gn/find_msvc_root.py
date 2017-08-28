53db461b4cfafce7737967fd8cab56fa98778e2e
Matthew Leibowitz <mattleibow@live.com>
2017-08-28 22:35:30 +0200

    we can't assume a version of MSVC

--- gn/find_msvc_root.py
+++ gn/find_msvc_root.py
@@ -5,16 +5,20 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import subprocess
+import os
 import sys
 
 msvc = int(sys.argv[1])
 windk = sys.argv[2]
 
-# for now, just assume
-vc_version = '14.11.25503'
-
 if msvc == 2015:
   print windk + '/VC'
 else:
+  # get the first version
+  dir = os.listdir(windk + '/VC/Tools/MSVC/')[0]
+  if os.path.exists(dir):
+    print dir
+    sys.exit(0)
+  # fallback to a version
+  vc_version = '14.11.25503'
   print windk + '/VC/Tools/MSVC/' + vc_version
