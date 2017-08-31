6df4d6be0d025211400de6a910e17f2a87219887
Brian Salomon <bsalomon@google.com>
2017-08-31 15:57:32 +0000

    Revert "Revert "Revert "Remove GrBackendRenderTargetDesc in favor of GrBackendRenderTarget."""
    This reverts commit 71554bc256b705fe959b7aa2fb2f24ed48782362.
    
    Reason for revert: Google3
    
    Original change's description:
    > Revert "Revert "Remove GrBackendRenderTargetDesc in favor of GrBackendRenderTarget.""
    > 
    > This reverts commit 807371c15bd742efb98a9df6e1dee73e8bda8af5.
    > 
    > Docs-Preview: https://skia.org/?cl=40260
    > Change-Id: I28e0434c455155ff39a5aaa4141abdf442474e87
    > Reviewed-on: https://skia-review.googlesource.com/40260
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com
    
    Change-Id: Ifdfa896a70db69935473276d12dce54de5c6b6f7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/41500
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- experimental/SkV8Example/SkV8Example.cpp
+++ experimental/SkV8Example/SkV8Example.cpp
@@ -8,12 +8,13 @@
  */
 #include <v8.h>
 #include <include/libplatform/libplatform.h>
+
 #include "SkV8Example.h"
 #include "Global.h"
 #include "JsContext.h"
 #include "Path2D.h"
 #include "Path2DBuilder.h"
-#include "GrBackendSurface.h"
+
 #include "gl/GrGLUtil.h"
 #include "gl/GrGLDefines.h"
 #include "gl/GrGLInterface.h"
@@ -83,17 +84,19 @@ void SkV8ExampleWindow::windowSizeChanged() {
             exit(1);
         }
 
-        GrGLFramebufferInfo framebufferInfo;
-        GR_GL_GetIntegerv(fCurIntf, GR_GL_FRAMEBUFFER_BINDING, &framebufferInfo.fFBOID);
-        GrBackendRenderTarget backendRenderTarget(SkScalarRoundToInt(this->width()),
-                                                  SkScalarRoundToInt(this->height()),
-                                                  attachmentInfo.fSampleCount,
-                                                  attachmentInfo.fStencilBits,
-                                                  kSkia8888_GrPixelConfig,
-                                                  framebufferInfo);
+        GrBackendRenderTargetDesc desc;
+        desc.fWidth = SkScalarRoundToInt(this->width());
+        desc.fHeight = SkScalarRoundToInt(this->height());
+        desc.fConfig = kSkia8888_GrPixelConfig;
+        desc.fOrigin = kBottomLeft_GrSurfaceOrigin;
+        desc.fSampleCnt = attachmentInfo.fSampleCount;
+        desc.fStencilBits = attachmentInfo.fStencilBits;
+        GrGLint buffer;
+        GR_GL_GetIntegerv(fCurIntf, GR_GL_FRAMEBUFFER_BINDING, &buffer);
+        desc.fRenderTargetHandle = buffer;
+
         SkSafeUnref(fCurSurface);
-        fCurSurface = SkSurface::MakeFromBackendRenderTarget(fCurContext, backendRenderTarget,
-                                                             kBottomLeft_GrSurfaceOrigin,
+        fCurSurface = SkSurface::MakeFromBackendRenderTarget(fCurContext, desc,
                                                              nullptr, nullptr).release();
     }
 }
