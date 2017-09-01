d1c1f4c8198daa7a0298561c380a1d8599225589
Robert Phillips <robertphillips@google.com>
2017-09-01 12:17:03 -0400

    Add gn plumbing for mdbviz tool
    Change-Id: I06e6b63c2742da069f48ff5d7defafc63a485af7

---
+++ gn/call.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import subprocess
+import sys
+
+subprocess.check_call(sys.argv[1:])
+
---
+++ tools/mdbviz/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <QtWidgets>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("Google");
+    QCoreApplication::setApplicationName("MDB Viz");
+    QCoreApplication::setApplicationVersion("0.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addVersionOption();
+    parser.process(app);
+
+    MainWindow mainWin;
+    mainWin.show();
+
+    return app.exec();
+}
+
+
