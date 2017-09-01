776e217154bd70b2710ffbe1487c094e0ec0b389
Mike Klein <mtklein@chromium.org>
2017-09-01 11:20:46 -0400

    pixmap erase fast-path
    Brian and I saw a measurable speedup on software path rendering when
    calling memset(..., 0, ...) instead of looping over one memset per
    scanline.  This brings that fast path to the masses.
    
    Change-Id: I792416e30e924efdd6ab40d445148b5c59f7bc62

--- src/core/SkPixmap.cpp
+++ src/core/SkPixmap.cpp
@@ -116,6 +116,14 @@ bool SkPixmap::erase(SkColor color, const SkIRect& inArea) const {
     const int width = area.width();
     const int rowBytes = this->rowBytes();
 
+    if (color == 0
+          && width == this->rowBytesAsPixels()
+          && inArea == this->bounds()) {
+        // All formats represent SkColor(0) as byte 0.
+        memset(this->writable_addr(), 0, height * rowBytes);
+        return true;
+    }
+
     switch (this->colorType()) {
         case kGray_8_SkColorType: {
             if (255 != a) {
