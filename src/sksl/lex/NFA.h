3de0e496b5215bcf4b26db2d69dc84376dfd68d2
Ethan Nicholas <ethannicholas@google.com>
2017-09-06 16:59:14 -0400

    Initial checkin of SkSL lexical analyzer generator (not actually in use yet).
    Bug: skia:
    Change-Id: I283feb9ac1cef134588204107ea019ce657e37b3
    Reviewed-on: https://skia-review.googlesource.com/42084
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

---
+++ src/sksl/lex/NFA.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_NFA
+#define SKSL_NFA
+
+#include "NFAState.h"
+#include "RegexNode.h"
+
+/**
+ * A nondeterministic finite automaton for matching regular expressions. The NFA is initialized with
+ * a number of regular expressions, and then matches a string against all of them simultaneously.
+ */
+struct NFA {
+    /**
+     * Adds a new regular expression to the set of expressions matched by this automaton, returning
+     * its index.
+     */
+    int addRegex(const RegexNode& regex) {
+        std::vector<int> accept;
+        // we reserve token 0 for END_OF_FILE, so this starts at 1
+        accept.push_back(this->addState(NFAState(++fRegexCount)));
+        std::vector<int> startStates = regex.createStates(this, accept);
+        fStartStates.insert(fStartStates.end(), startStates.begin(), startStates.end());
+        return fStartStates.size() - 1;
+    }
+
+    /**
+     * Adds a new state to the NFA, returning its index.
+     */
+    int addState(NFAState s) {
+        fStates.push_back(std::move(s));
+        return fStates.size() - 1;
+    }
+
+    /**
+     * Matches a string against all of the regexes added to this NFA. Returns the index of the first
+     * (in addRegex order) matching expression, or -1 if no match. This is relatively slow and used
+     * only for debugging purposes; the NFA should be converted to a DFA before actual use.
+     */
+    int match(std::string s) const;
+
+    int fRegexCount = 0;
+
+    std::vector<NFAState> fStates;
+
+    std::vector<int> fStartStates;
+};
+
+#endif
