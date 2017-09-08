f256051c3b893da465bf22caed2ae59d983b17ce
nagarajan.n <nagarajan.n@samsung.com>
2017-09-08 14:05:30 +0530

    Move memory initialization check in SkSampler::Fill()
    Move memory initialization check in SkSampler::Fill() to simplify
    the code
    
    Bug: None
    Change-Id: I115e5b7857514811400503b133c012a0c5ce7585

--- src/codec/SkSampler.cpp
+++ src/codec/SkSampler.cpp
@@ -14,6 +14,11 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
         uint64_t colorOrIndex, SkCodec::ZeroInitialized zeroInit) {
     SkASSERT(dst != nullptr);
 
+    // If memory is zero initialized, we may not need to fill
+    if (SkCodec::kYes_ZeroInitialized == zeroInit && 0 == colorOrIndex) {
+        return;
+    }
+
     // Calculate bytes to fill.  We use getSafeSize since the last row may not be padded.
     const size_t bytesToFill = info.getSafeSize(rowBytes);
     const int width = info.width();
@@ -23,15 +28,9 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
     switch (info.colorType()) {
         case kRGBA_8888_SkColorType:
         case kBGRA_8888_SkColorType: {
-            // If memory is zero initialized, we may not need to fill
-            uint32_t color = (uint32_t) colorOrIndex;
-            if (SkCodec::kYes_ZeroInitialized == zeroInit && 0 == color) {
-                return;
-            }
-
             uint32_t* dstRow = (uint32_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset32((uint32_t*) dstRow, color, width);
+                sk_memset32((uint32_t*) dstRow, colorOrIndex, width);
                 dstRow = SkTAddOffset<uint32_t>(dstRow, rowBytes);
             }
             break;
@@ -44,15 +43,9 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
             // bits of SK_ColorBLACK are identical to the 565 representation
             // for black.
 
-            // If memory is zero initialized, we may not need to fill
-            uint16_t color = (uint16_t) colorOrIndex;
-            if (SkCodec::kYes_ZeroInitialized == zeroInit && 0 == color) {
-                return;
-            }
-
             uint16_t* dstRow = (uint16_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset16((uint16_t*) dstRow, color, width);
+                sk_memset16((uint16_t*) dstRow, colorOrIndex, width);
                 dstRow = SkTAddOffset<uint16_t>(dstRow, rowBytes);
             }
             break;
@@ -65,22 +58,12 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
             // bits of SK_ColorBLACK are identical to the grayscale representation
             // for black.
 
-            // If memory is zero initialized, we may not need to fill
-            if (SkCodec::kYes_ZeroInitialized == zeroInit && 0 == (uint8_t) colorOrIndex) {
-                return;
-            }
-
             memset(dst, (uint8_t) colorOrIndex, bytesToFill);
             break;
         case kRGBA_F16_SkColorType: {
-            uint64_t color = colorOrIndex;
-            if (SkCodec::kYes_ZeroInitialized == zeroInit && 0 == color) {
-                return;
-            }
-
             uint64_t* dstRow = (uint64_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset64((uint64_t*) dstRow, color, width);
+                sk_memset64((uint64_t*) dstRow, colorOrIndex, width);
                 dstRow = SkTAddOffset<uint64_t>(dstRow, rowBytes);
             }
             break;
