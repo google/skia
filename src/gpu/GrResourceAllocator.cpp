eb527d96136d2e68194afd1d964a046ea99553f3
Robert Phillips <robertphillips@google.com>
2017-09-13 15:25:47 -0400

    Pull non-substantive changes out of explicit GPU resource allocation CL
    Change-Id: Ib6a289553ecd15c722599b7dc0d347a7800801cb

--- src/gpu/GrResourceAllocator.cpp
+++ src/gpu/GrResourceAllocator.cpp
@@ -134,3 +134,35 @@ void GrResourceAllocator::assign() {
         fActiveIntvls.insertByIncreasingEnd(cur);
     }
 }
+
+#ifdef SK_DEBUG
+void GrResourceAllocator::dump() {
+    unsigned int min = fNumOps+1;
+    unsigned int max = 0;
+    for(const Interval* cur = fIntvlList.peekHead(); cur; cur = cur->fNext) {
+        SkDebugf("{ %d,%d }: [%d, %d]\n",
+                 cur->fProxy->uniqueID().asUInt(), cur->fProxy->underlyingUniqueID().asUInt(),
+                 cur->fStart, cur->fEnd);
+        if (min > cur->fStart) {
+            min = cur->fStart;
+        }
+        if (max < cur->fEnd) {
+            max = cur->fEnd;
+        }
+    }
+
+    for(const Interval* cur = fIntvlList.peekHead(); cur; cur = cur->fNext) {
+        SkDebugf("{ %3d,%3d }: ",
+                 cur->fProxy->uniqueID().asUInt(), cur->fProxy->underlyingUniqueID().asUInt());
+        for (unsigned int i = min; i <= max; ++i) {
+            if (i >= cur->fStart && i <= cur->fEnd) {
+                SkDebugf("x");
+            } else {
+                SkDebugf(" ");
+            }
+        }
+        SkDebugf("\n");
+    }
+}
+#endif
+
