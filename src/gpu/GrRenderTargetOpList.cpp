b4cc88f2cf204a23c20cd693d075e463584caa26
Robert Phillips <robertphillips@google.com>
2017-09-14 12:16:59 -0400

    Test opList DAG completeness
    This CL contains test code that double checks that each TextureSampler
    has been processed (presubably by addDependency).
    
    Change-Id: I1805d7356e5b060f344bd201bd435ae398204e29

--- src/gpu/GrRenderTargetOpList.cpp
+++ src/gpu/GrRenderTargetOpList.cpp
@@ -12,6 +12,7 @@
 #include "GrGpuCommandBuffer.h"
 #include "GrRect.h"
 #include "GrRenderTargetContext.h"
+#include "GrResourceAllocator.h"
 #include "instanced/InstancedRendering.h"
 #include "ops/GrClearOp.h"
 #include "ops/GrCopySurfaceOp.h"
@@ -252,6 +253,34 @@ bool GrRenderTargetOpList::copySurface(const GrCaps& caps,
     return true;
 }
 
+void GrRenderTargetOpList::gatherOpList(GrResourceAllocator* alloc) const {
+    SkASSERT(!this->isInstantiated());
+
+    unsigned int cur = alloc->numOps();
+
+    SkDebugf("----------------------------------------\n");
+    SkDebugf("gather for RT opList #%d { %d,%d }: %s\n", this->uniqueID(),
+                                                  fTarget.get()->uniqueID().asUInt(),
+                                                  fTarget.get()->underlyingUniqueID().asUInt());
+
+    alloc->addInterval(fTarget.get(), cur, cur+fRecordedOps.count()-1);
+
+    auto gather = [ alloc ] (GrSurfaceProxy* p) {
+        alloc->addInterval(p);
+    };
+    for (int i = 0; i < fRecordedOps.count(); ++i) {
+        const GrOp* op = fRecordedOps[i].fOp.get();
+
+        SkASSERT(alloc->curOp() == cur+i);
+        SkDebugf("opList #%d (%s): %d\n", this->uniqueID(), op->name(), cur+i);
+
+        op->visitProxies(gather);
+
+        alloc->incOps();
+    }
+    SkDebugf("----------------------------------------\n");
+}
+
 static inline bool can_reorder(const SkRect& a, const SkRect& b) { return !GrRectsOverlap(a, b); }
 
 bool GrRenderTargetOpList::combineIfPossible(const RecordedOp& a, GrOp* b,
@@ -314,6 +343,8 @@ void GrRenderTargetOpList::recordOp(std::unique_ptr<GrOp> op,
                 GrOP_INFO("\t\t\tBackward: Combined op info:\n");
                 GrOP_INFO(SkTabString(candidate.fOp->dumpInfo(), 4).c_str());
                 GR_AUDIT_TRAIL_OPS_RESULT_COMBINED(fAuditTrail, candidate.fOp.get(), op.get());
+
+                op->markAsHandled();
                 return;
             }
             // Stop going backwards if we would cause a painter's order violation.
