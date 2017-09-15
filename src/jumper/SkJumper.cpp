c66b516c3be15cd2d1fb88023824d8e3036f89bb
Nico Weber <thakis@chromium.org>
2017-09-15 17:21:38 -0400

    Disable SkJumper in cross builds for now.
    Bug: chromium:762167
    Change-Id: Ia23f6dbfc0466aef4ca9d1a5b9ff343d79dc83bb

--- src/jumper/SkJumper.cpp
+++ src/jumper/SkJumper.cpp
@@ -17,13 +17,20 @@
 #if !defined(__has_feature)
     #define __has_feature(x) 0
 #endif
+#if !defined(SK_JUMPER_ENABLED)
+#if __has_feature(memory_sanitizer)
+#define SK_JUMPER_ENABLED 0
+#else
+#define SK_JUMPER_ENABLED 1
+#endif
+#endif
 
 #define M(st) +1
 static const int kNumStages = SK_RASTER_PIPELINE_STAGES(M);
 #undef M
 
 #ifndef SK_JUMPER_DISABLE_8BIT
-    #if 0 && !__has_feature(memory_sanitizer) && (defined(__x86_64__) || defined(_M_X64))
+    #if SK_JUMPER_ENABLED && (defined(__x86_64__) || defined(_M_X64))
         #include <atomic>
 
         #define M(st) #st,
@@ -102,7 +109,7 @@ using StartPipelineFn = void(size_t,size_t,size_t,size_t, void**);
 
 extern "C" {
 
-#if __has_feature(memory_sanitizer)
+#if !SK_JUMPER_ENABLED
     // We'll just run baseline code.
 
 #elif defined(__arm__)
@@ -172,7 +179,8 @@ extern "C" {
 
 }
 
-#if !__has_feature(memory_sanitizer) && (defined(__x86_64__) || defined(_M_X64))
+#if SK_JUMPER_ENABLED
+#if defined(__x86_64__) || defined(_M_X64)
     template <SkRasterPipeline::StockStage st>
     static constexpr StageFn* hsw_lowp() { return nullptr; }
 
@@ -217,6 +225,7 @@ extern "C" {
         LOWP_STAGES(M)
     #undef M
 #endif
+#endif
 
 // Engines comprise everything we need to run SkRasterPipelines.
 struct SkJumper_Engine {
@@ -237,7 +246,7 @@ static SkJumper_Engine gEngine = kBaseline;
 static SkOnce gChooseEngineOnce;
 
 static SkJumper_Engine choose_engine() {
-#if __has_feature(memory_sanitizer)
+#if !SK_JUMPER_ENABLED
     // We'll just run baseline code.
 
 #elif defined(__arm__)
@@ -316,7 +325,8 @@ static SkJumper_Engine choose_engine() {
     static SkOnce gChooseLowpOnce;
 
     static SkJumper_Engine choose_lowp() {
-    #if !__has_feature(memory_sanitizer) && (defined(__x86_64__) || defined(_M_X64))
+    #if SK_JUMPER_ENABLED
+    #if defined(__x86_64__) || defined(_M_X64)
         if (1 && SkCpu::Supports(SkCpu::HSW)) {
             return {
             #define M(st) hsw_lowp<SkRasterPipeline::st>(),
@@ -363,6 +373,7 @@ static SkJumper_Engine choose_engine() {
             sk_just_return_lowp,
         #undef M
         };
+    #endif
     #endif
         return kNone;
     }
