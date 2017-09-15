b858da928b5ba20adff914637526b847aea9035f
Ben Wagner <benjaminwagner@google.com>
2017-09-15 15:50:07 -0400

    Fix float-cast-overflow in SkOpSpan::sortableTop.
    Bug: skia:4632
    Change-Id: I5af8bb7b04673307cdf7865bb7d56a041ba27d83
    Reviewed-on: https://skia-review.googlesource.com/47320
    Reviewed-by: Cary Clark <caryclark@skia.org>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -542,7 +542,6 @@ def dm_flags(api, bot):
     match.append('~^PathBigCubic$')
     match.append('~^PathOpsCubicIntersection$')
     match.append('~^PathOpsCubicLineIntersection$')
-    match.append('~^PathOpsFailOp$')
     match.append('~^PathOpsOpCubicsThreaded$')
     match.append('~^PathOpsOpLoopsThreaded$')
 
