c62c75afc4c1303b0795ae81976a2242e52845fd
Mike Klein <mtklein@chromium.org>
2017-09-15 14:57:02 -0400

    Bump stored lowp uniform color to 16-bit storage.
    This makes loading into 16-bit channels more natural in _lowp.cpp.
    
    Change-Id: I1ed393873654060ef52f4632d670465528006bbd

--- src/jumper/SkJumper_stages_lowp.cpp
+++ src/jumper/SkJumper_stages_lowp.cpp
@@ -137,11 +137,10 @@ SI D join(S lo, S hi) {
 // ~~~~~~ Basic / misc. stages ~~~~~~ //
 
 STAGE(uniform_color, const SkJumper_UniformColorCtx* c) {
-    auto rgba = (const uint8_t*)&c->rgba;
-    r = rgba[0];
-    g = rgba[1];
-    b = rgba[2];
-    a = rgba[3];
+    r = c->rgba[0];
+    g = c->rgba[1];
+    b = c->rgba[2];
+    a = c->rgba[3];
 }
 STAGE(black_color, Ctx::None) { r = g = b =   0; a = 255; }
 STAGE(white_color, Ctx::None) { r = g = b = 255; a = 255; }
