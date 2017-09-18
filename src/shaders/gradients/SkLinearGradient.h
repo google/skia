e7021ac706b7d9adf48b11eb0b4e4d3a4cca23a4
Florin Malita <fmalita@chromium.org>
2017-09-18 12:29:56 -0400

    Delete the legacy linear gradient impl
    Change-Id: I0eb705195446ec084ead49c9f712bf19d8892305

--- src/shaders/gradients/SkLinearGradient.h
+++ src/shaders/gradients/SkLinearGradient.h
@@ -9,46 +9,10 @@
 #define SkLinearGradient_DEFINED
 
 #include "SkGradientShaderPriv.h"
-#include "SkNx.h"
-
-struct Sk4fStorage {
-    float fArray[4];
-
-    operator Sk4f() const {
-        return Sk4f::Load(fArray);
-    }
-
-    Sk4fStorage& operator=(const Sk4f& src) {
-        src.store(fArray);
-        return *this;
-    }
-};
 
 class SkLinearGradient : public SkGradientShaderBase {
 public:
     SkLinearGradient(const SkPoint pts[2], const Descriptor&);
-
-    class LinearGradientContext : public SkGradientShaderBase::GradientShaderBaseContext {
-    public:
-        LinearGradientContext(const SkLinearGradient&, const ContextRec&);
-
-        void shadeSpan(int x, int y, SkPMColor dstC[], int count) override;
-
-        struct Rec {
-            Sk4fStorage fColor;
-            float       fPos;
-            float       fPosScale;
-        };
-    private:
-        SkTDArray<Rec>  fRecs;
-        bool            fApplyAlphaAfterInterp;
-
-        void shade4_clamp(int x, int y, SkPMColor dstC[], int count);
-        template <bool, bool> void shade4_dx_clamp(SkPMColor dstC[], int count, float fx, float dx,
-                                                   float invDx, const float dither[2]);
-
-        typedef SkGradientShaderBase::GradientShaderBaseContext INHERITED;
-    };
 
     GradientType asAGradient(GradientInfo* info) const override;
 #if SK_SUPPORT_GPU
