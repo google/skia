adda9ed55d2f54c725f745db321e821f8285bcf0
Kevin Lubick <kjlubick@google.com>
2017-09-21 12:00:11 -0400

    [WASM] Add POC compile bot for WebAssembly
    Fix core.gni to use not use Assembler for none cpu.
    
    Right now, there are no outputs because we aren't compiling
    dm or nanobench.  However, this still compiles the skia
    library and creates two executables, so it's a good canary
    for a real WASM build.
    
    Additional note: the two executables in question don't draw
    anything to the screen via GL, which is still not possible with
    Skia+WASM.
    
    Bug: skia:
    Change-Id: I0d767467e94e40d01070e34223dd90e96f1c96f2

--- infra/bots/recipes/compile.py
+++ infra/bots/recipes/compile.py
@@ -90,6 +90,7 @@ TEST_BUILDERS = [
   'Build-Ubuntu-Clang-x86_64-Debug-MSAN',
   'Build-Ubuntu-Clang-x86_64-Release-Mini',
   'Build-Ubuntu-Clang-x86_64-Release-Vulkan',
+  'Build-Ubuntu-EMCC-wasm-Release',
   'Build-Ubuntu-GCC-arm-Release-Chromecast',
   'Build-Ubuntu-GCC-x86-Debug',
   'Build-Ubuntu-GCC-x86_64-Debug-GN',
