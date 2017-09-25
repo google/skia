f4c75de85c7cd8691f2ffa8d7a86182a26d60f6a
Ben Wagner <benjaminwagner@google.com>
2017-09-25 13:19:07 -0400

    Specify CPU for CT Swarming tasks.
    The Chrome-GPU pool has different machines with the same GPU, so it's
    important to lock down to a specific CPU for perf testing.
    
    For CPU tasks, specify the CPU in case any new VMs are added to the pool
    in the future. It turns out the Swarming bots these tasks run on do not
    support AVX2, only AVX, so fix that in multiple places.
    
    Change-Id: I66648c4ea58b835af0a63d36f4830b26d7c65c5f

--- infra/bots/ct/run_ct_skps.py
+++ infra/bots/ct/run_ct_skps.py
@@ -63,7 +63,7 @@ def main():
     # Add Nanobench specific arguments.
     config = '8888'
     cpu_or_gpu = 'CPU'
-    cpu_or_gpu_value = 'AVX2'
+    cpu_or_gpu_value = 'AVX'
     if 'GPU' in args.builder:
       config = 'gl'
       cpu_or_gpu = 'GPU'
