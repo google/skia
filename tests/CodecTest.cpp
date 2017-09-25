b8f49ae13137a0a74867b33e9fc81e892bfb601c
Leon Scroggins III <scroggo@google.com>
2017-09-25 10:13:08 -0400

    Fix truncated webp images
    Bug: b/65290323
    
    If a webp file is truncated such that no rows can be decoded,
    WebPIDecGetRGB does not initialize its "last_y" parameter. We use
    rowsDecoded (passed as last_y) to determine which remaining rows to
    fill.
    
    Check the return value of WebPIDecGetRGB. If it fails (returns null),
    or rowsDecoded is <= 0 (matching Chromium's check), return
    kInvalidInput, since there is nothing to draw.
    
    Note that this is a change in behavior for Android. Previously we
    would decode an empty webp to just a transparent rectangle, whereas
    now we simply fail. I think this is a change for the better.
    
    Add a test.
    
    Remove test images inc0.webp and inc1.webp. These just decode to empty
    rectangles.
    
    Change-Id: I64d40be91c574b45963f9a43d8dd8f4929dd2939

--- tests/CodecTest.cpp
+++ tests/CodecTest.cpp
@@ -1513,3 +1513,24 @@ DEF_TEST(Codec_EncodeICC, r) {
     test_encode_icc(r, SkEncodedImageFormat::kJPEG, SkTransferFunctionBehavior::kIgnore);
     test_encode_icc(r, SkEncodedImageFormat::kWEBP, SkTransferFunctionBehavior::kIgnore);
 }
+
+DEF_TEST(Codec_webp_rowsDecoded, r) {
+    const char* path = "baby_tux.webp";
+    sk_sp<SkData> data(GetResourceAsData(path));
+    if (!data) {
+        return;
+    }
+
+    // Truncate this file so that the header is available but no rows can be
+    // decoded. This should create a codec but fail to decode.
+    size_t truncatedSize = 5000;
+    sk_sp<SkData> subset = SkData::MakeSubset(data.get(), 0, truncatedSize);
+    std::unique_ptr<SkCodec> codec = SkCodec::MakeFromData(std::move(subset));
+    if (!codec) {
+        ERRORF(r, "Failed to create a codec for %s truncated to only %lu bytes",
+               path, truncatedSize);
+        return;
+    }
+
+    test_info(r, codec.get(), codec->getInfo(), SkCodec::kInvalidInput, nullptr);
+}
