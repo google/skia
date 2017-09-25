b42fe01b35f427c3e24a95ba1951982786317fdb
Mike Reed <reed@google.com>
2017-09-25 17:14:06 -0400

    guard old apis for querying byte-size of a bitmap/imageinfo/pixmap
    The new (simpler) API always..
    - returns size_t, or 0 if the calculation overflowed
    - returns the trimmed size (does not include rowBytes padding for the last row)
    
    Bug: skia:
    Change-Id: I05173e877918327c7b207d2f7f1ab0db36892e2e

--- bench/ColorCodecBench.cpp
+++ bench/ColorCodecBench.cpp
@@ -88,10 +88,10 @@ void ColorCodecBench::onDelayedSetup() {
         fDstSpace = static_cast<SkColorSpace_XYZ*>(fDstSpace.get())->makeLinearGamma();
     }
 
-    fDst.reset(fDstInfo.getSafeSize(fDstInfo.minRowBytes()));
+    fDst.reset(fDstInfo.getMinSize());
 
     if (FLAGS_xform_only) {
-        fSrc.reset(fSrcInfo.getSafeSize(fSrcInfo.minRowBytes()));
+        fSrc.reset(fSrcInfo.getMinSize());
         fSrcSpace = codec->getInfo().refColorSpace();
         codec->getPixels(fSrcInfo, fSrc.get(), fSrcInfo.minRowBytes());
     }
