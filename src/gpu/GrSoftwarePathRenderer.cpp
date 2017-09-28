6af50e4903bc18d4dd0a259f5be85cd6d3eb1455
Brian Osman <brianosman@google.com>
2017-09-28 10:10:55 -0400

    Make threaded proxy generation MDB-friendly, and defer instantiation
    Replaces GrPrepareCallback with GrDeferredProxyUploader, stored directly
    on GrTextureProxy. Op lists now store a list of referenced proxies that
    are being generated by worker threads. At flush time, iterate over those
    proxies, and invoke their uploader.
    
    Lifetime of the uploader object is now tied to the proxy, but the ASAP
    upload function will free the proxy's uploader, if it's called.
    
    Bug: skia:
    Change-Id: Ieb2c6a805d19990012839a8e103c3ca5b8d3dfc6

--- src/gpu/GrSoftwarePathRenderer.cpp
+++ src/gpu/GrSoftwarePathRenderer.cpp
@@ -9,10 +9,10 @@
 #include "GrAuditTrail.h"
 #include "GrClip.h"
 #include "GrContextPriv.h"
+#include "GrDeferredProxyUploader.h"
 #include "GrGpuResourcePriv.h"
 #include "GrOpFlushState.h"
 #include "GrOpList.h"
-#include "GrPrepareCallback.h"
 #include "GrResourceProvider.h"
 #include "GrSWMaskHelper.h"
 #include "SkMakeUnique.h"
@@ -175,7 +175,7 @@ static sk_sp<GrTextureProxy> make_deferred_mask_texture_proxy(GrContext* context
 namespace {
 
 /**
- * Payload class for use with GrMaskUploaderPrepareCallback. The software path renderer only draws
+ * Payload class for use with GrTDeferredProxyUploader. The software path renderer only draws
  * a single path into the mask texture. This stores all of the information needed by the worker
  * thread's call to drawShape (see below, in onDrawPath).
  */
@@ -317,15 +317,9 @@ bool GrSoftwarePathRenderer::onDrawPath(const DrawPathArgs& args) {
                 return false;
             }
 
-            // TODO: I believe the assignUniqueKeyToProxy below used to instantiate the proxy before
-            // before the draw that used the result was being flushed, so the upload was succeeding.
-            // With assignUniqueKeyToProxy no longer forcing an instantiation it will have to happen
-            // explicitly elsewhere.
-            proxy->instantiate(fResourceProvider);
-
-            auto uploader = skstd::make_unique<GrMaskUploaderPrepareCallback<SoftwarePathData>>(
-                    proxy, *boundsForMask, *args.fViewMatrix, *args.fShape, aa);
-            GrMaskUploaderPrepareCallback<SoftwarePathData>* uploaderRaw = uploader.get();
+            auto uploader = skstd::make_unique<GrTDeferredProxyUploader<SoftwarePathData>>(
+                    *boundsForMask, *args.fViewMatrix, *args.fShape, aa);
+            GrTDeferredProxyUploader<SoftwarePathData>* uploaderRaw = uploader.get();
 
             auto drawAndUploadMask = [uploaderRaw] {
                 TRACE_EVENT0("skia", "Threaded SW Mask Render");
@@ -337,10 +331,10 @@ bool GrSoftwarePathRenderer::onDrawPath(const DrawPathArgs& args) {
                 } else {
                     SkDEBUGFAIL("Unable to allocate SW mask.");
                 }
-                uploaderRaw->getSemaphore()->signal();
+                uploaderRaw->signalAndFreeData();
             };
             taskGroup->add(std::move(drawAndUploadMask));
-            args.fRenderTargetContext->getOpList()->addPrepareCallback(std::move(uploader));
+            proxy->texPriv().setDeferredUploader(std::move(uploader));
         } else {
             GrSWMaskHelper helper;
             if (!helper.init(*boundsForMask)) {
