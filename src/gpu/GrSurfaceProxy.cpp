467dc963eefdc46825e996a8e5a9316ca43674ca
Robert Phillips <robertphillips@google.com>
2017-10-02 13:33:33 -0400

    [M61 cherrypick] Disable deferred texture proxies
    Bug: 769760
    Change-Id: I6fac93100fbae22ea9b72bf9fb1c61a79afafbe9

--- src/gpu/GrSurfaceProxy.cpp
+++ src/gpu/GrSurfaceProxy.cpp
@@ -187,14 +187,25 @@ sk_sp<GrTextureProxy> GrSurfaceProxy::MakeDeferred(GrResourceProvider* resourceP
     GrSurfaceDesc copyDesc = desc;
     copyDesc.fSampleCnt = caps->getSampleCount(desc.fSampleCnt, desc.fConfig);
 
+    // Temporarily force instantiation of the deferred proxies for crbug.com/769760
     if (willBeRT) {
         // We know anything we instantiate later from this deferred path will be
         // both texturable and renderable
-        return sk_sp<GrTextureProxy>(new GrTextureRenderTargetProxy(*caps, copyDesc, fit,
-                                                                    budgeted, flags));
+        sk_sp<GrTextureProxy> temp(new GrTextureRenderTargetProxy(*caps, copyDesc, fit,
+                                                                  budgeted, flags));
+        if (!temp->instantiate(resourceProvider)) {
+            return nullptr;
+        }
+
+        return temp;
     }
 
-    return sk_sp<GrTextureProxy>(new GrTextureProxy(copyDesc, fit, budgeted, nullptr, 0, flags));
+    sk_sp<GrTextureProxy> temp(new GrTextureProxy(copyDesc, fit, budgeted, nullptr, 0, flags));
+    if (!temp->instantiate(resourceProvider)) {
+        return nullptr;
+    }
+
+    return temp;
 }
 
 sk_sp<GrTextureProxy> GrSurfaceProxy::MakeDeferred(GrResourceProvider* resourceProvider,
