ce878510478fb7a425ffdabe372ac4b97313b91c
Kevin Lubick <kjlubick@google.com>
2017-10-02 12:20:22 -0400

    Add Linux Coverage Bot
    Bug: skia:
    Change-Id: I7dcfa2307a239734a614990aca899ea37129126b

--- infra/bots/recipe_modules/flavor/gn_flavor.py
+++ infra/bots/recipe_modules/flavor/gn_flavor.py
@@ -59,7 +59,14 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
       cxx  = emscripten_sdk + '/emscripten/incoming/em++'
       extra_cflags.append('-Wno-unknown-warning-option')
 
-    if compiler != 'MSVC' and configuration == 'Debug':
+    if extra_config == 'Coverage':
+      extra_cflags.append('-O0')
+      extra_cflags.append('-fprofile-instr-generate')
+      extra_cflags.append('-fcoverage-mapping')
+      extra_ldflags.append('-fprofile-instr-generate')
+      extra_ldflags.append('-fcoverage-mapping')
+
+    elif compiler != 'MSVC' and configuration == 'Debug':
       extra_cflags.append('-O1')
 
     if extra_config == 'Exceptions':
@@ -126,6 +133,8 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
         'skia_use_icu':        'false',
         'skia_enable_gpu':     'false',
       })
+    if extra_config == 'Coverage':
+      args['skia_use_system_freetype2'] = 'true'
 
     sanitize = ''
     if extra_config == 'UBSAN_float_cast_overflow':
@@ -208,6 +217,12 @@ class GNFlavorUtils(default_flavor.DefaultFlavorUtils):
       # We don't care about malloc(), fprintf, etc. used in signal handlers.
       # If we're in a signal handler, we're already crashing...
       env['TSAN_OPTIONS'] = 'report_signal_unsafe=0'
+
+    if 'Coverage' == extra_config:
+      env['LLVM_PROFILE_FILE'] = self.m.path.join(self.m.vars.swarming_out_dir,
+                                                 'output.profraw')
+      # TODO(kjlubick): remove so this hack to keep the command from going forever
+      cmd += ['--match', 'xfermode']
 
     to_symbolize = ['dm', 'nanobench']
     if name in to_symbolize and self.m.vars.is_linux:
