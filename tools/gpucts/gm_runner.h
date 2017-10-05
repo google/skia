61a0bf34f01f55dcd83c442e024e052afd5ad6ca
Hal Canary <halcanary@google.com>
2017-10-05 11:49:08 -0400

    GPU-CTS Program
    Add new application, called GPU-CTS (GPU Compatibility Test Suite),
    which executes skia gms against OpenGL and Vulkan backends.  Makes use
    of googletest library for consistancy with Android CTS programs.
    
    Add googletest to DEPS
    
    gm_knowledge.h header as a stub for future work on validating gm output.
    
    gm_runner can be re-used in other programs.  Talks to Skia and GM with a
    simple API.
    
    gpuctx executable wraps gm_runner and googletest together.
    
    Change-Id: Ie7350b22164fa73e44121c39b0f36da4038a700b

---
+++ tools/gpucts/gm_runner.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef gm_runner_DEFINED
+#define gm_runner_DEFINED
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "gm_knowledge.h"
+
+// A Skia GM is a single drawing test that uses one
+namespace skiagm {
+   class GM;
+}
+
+namespace gm_runner {
+
+struct SkiaBackend;
+
+using GMFactory = skiagm::GM* (*)(void*);
+
+class SkiaContext {
+public:
+    SkiaContext();
+    ~SkiaContext();
+    // These backend pointers are only valid for as long as the SkiaContext
+    // is in scope.
+    std::vector<SkiaBackend*> backends() const;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> fImpl;
+};
+
+std::vector<GMFactory> GetGMs();
+
+std::string GetName(GMFactory);
+
+std::string GetName(const SkiaBackend*);
+
+GMK_ImageData Evaluate(SkiaBackend*, GMFactory, std::vector<uint32_t>* storage);
+
+}  // namespace gm_runner
+
+#endif  // gm_runner_DEFINED
---
+++ tools/gpucts/gpucts.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm_runner.h"
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wused-but-marked-unused"
+#endif
+
+#include "gtest/gtest.h"
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+struct GMTestCase {
+    gm_runner::GMFactory fGMFactory;
+    gm_runner::SkiaBackend* fSurfaceFactory;
+};
+
+struct GMTest : public testing::Test {
+    GMTestCase fTest;
+    GMTest(GMTestCase t) : fTest(t) {}
+    void TestBody() override {
+        std::vector<uint32_t> pixels;
+        GMK_ImageData imgData =
+                gm_runner::Evaluate(fTest.fSurfaceFactory, fTest.fGMFactory, &pixels);
+        EXPECT_TRUE(imgData.pix);
+        if (!imgData.pix) {
+            return;
+        }
+        std::string gmName = gm_runner::GetName(fTest.fGMFactory);
+        float result = GMK_Check(imgData, gmName.c_str());
+        EXPECT_EQ(result, 0);
+    }
+};
+
+struct GMTestFactory : public testing::internal::TestFactoryBase {
+    GMTestCase fTest;
+    GMTestFactory(GMTestCase t) : fTest(t) {}
+    testing::Test* CreateTest() override { return new GMTest(fTest); }
+};
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    gm_runner::SkiaContext context;
+    std::vector<gm_runner::SkiaBackend*> backends = context.backends();
+    std::vector<gm_runner::GMFactory> gms = gm_runner::GetGMs();
+    for (auto backend : backends) {
+        std::string test = std::string("SkiaGM_") + GetName(backend);
+        for (auto gmFactory : gms) {
+            std::string gmName = gm_runner::GetName(gmFactory);
+            testing::internal::MakeAndRegisterTestInfo(
+                        test.c_str(),
+                        gmName.c_str(),
+                        nullptr,
+                        nullptr,
+                        testing::internal::CodeLocation(__FILE__, __LINE__),
+                        testing::internal::GetTestTypeId(),
+                        testing::Test::SetUpTestCase,
+                        testing::Test::TearDownTestCase,
+                        new GMTestFactory(GMTestCase{gmFactory, backend}));
+        }
+    }
+    return RUN_ALL_TESTS();
+}
+
