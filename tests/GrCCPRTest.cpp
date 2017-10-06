472bc405c285a1c2ae3058a5d7a51d8dadcb3c93
Chris Dalton <csmartdalton@google.com>
2017-10-06 12:57:15 -0600

    CCPR: Add workaround for PowerVR crash
    Bug: skia:
    Change-Id: Icd00f81fda5366813f9c959fdc91b0415894cbfc

---
+++ tests/GrCCPRTest.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkTypes.h"
+#include "Test.h"
+
+#if SK_SUPPORT_GPU
+
+#include "GrContext.h"
+#include "GrContextPriv.h"
+#include "GrClip.h"
+#include "GrRenderTargetContext.h"
+#include "GrRenderTargetContextPriv.h"
+#include "GrShape.h"
+#include "GrPathRenderer.h"
+#include "GrPaint.h"
+#include "SkMatrix.h"
+#include "SkRect.h"
+#include "ccpr/GrCoverageCountingPathRenderer.h"
+#include <cmath>
+
+
+
+#include "gl/GrGLGpu.h"
+
+static constexpr int kCanvasSize = 100;
+
+class CCPRPathDrawer {
+public:
+    CCPRPathDrawer(GrContext* ctx)
+            : fCtx(ctx)
+            , fCCPR(GrCoverageCountingPathRenderer::CreateIfSupported(*fCtx->caps()))
+            , fRTC(fCtx->makeDeferredRenderTargetContext(SkBackingFit::kExact, kCanvasSize,
+                                                         kCanvasSize, kRGBA_8888_GrPixelConfig,
+                                                         nullptr)) {
+        if (fCCPR) {
+            fCtx->contextPriv().addOnFlushCallbackObject(fCCPR.get());
+        }
+    }
+
+    ~CCPRPathDrawer() {
+        if (fCCPR) {
+            fCtx->contextPriv().testingOnly_flushAndRemoveOnFlushCallbackObject(fCCPR.get());
+        }
+    }
+
+    bool valid() { return fCCPR && fRTC; }
+
+    void clear() { fRTC->clear(nullptr, 0, true); }
+
+    void drawPath(const SkPath& path, GrColor4f color = GrColor4f(0, 1, 0, 1)) {
+        GrPaint paint;
+        paint.setColor4f(color);
+        GrNoClip noClip;
+        SkIRect clipBounds = SkIRect::MakeWH(kCanvasSize, kCanvasSize);
+        SkMatrix matrix = SkMatrix::I();
+        GrShape shape(path);
+        fCCPR->drawPath({fCtx, std::move(paint), &GrUserStencilSettings::kUnused, fRTC.get(),
+                         &noClip, &clipBounds, &matrix, &shape, GrAAType::kCoverage, false});
+    }
+
+    void flush() {
+        fCtx->flush();
+    }
+
+private:
+    GrContext* const                        fCtx;
+    sk_sp<GrCoverageCountingPathRenderer>   fCCPR;
+    sk_sp<GrRenderTargetContext>            fRTC;
+};
+
+DEF_GPUTEST_FOR_RENDERING_CONTEXTS(GrCCPRTest, reporter, ctxInfo) {
+    SkDebugf("@@@@@@@@> begin e ../../tests/GrCCPRTest.cpp\n");
+    GrContext* const ctx = ctxInfo.grContext();
+    SkDebugf("@@@@@@@@> ctx=%p\n", ctx);
+    if (!GrCoverageCountingPathRenderer::IsSupported(*ctx->caps())) {
+    SkDebugf("@@@@@@@@> not supported\n");
+        return;
+    }
+
+    SkDebugf("@@@@@@@@@> 1\n");
+    CCPRPathDrawer ccpr(ctx);
+    if (!ccpr.valid()) {
+    SkDebugf("@@@@@@@@> 2\n");
+        ERRORF(reporter, "could not create render target context for ccpr.");
+        return;
+    }
+
+    SkDebugf("@@@@@@@@> 3\n");
+    // Test very busy paths.
+    static constexpr int kNumBusyVerbs = 1 << 17;
+    ccpr.clear();
+    SkPath busyPath;
+    busyPath.moveTo(0, 0); // top left
+    busyPath.lineTo(kCanvasSize, kCanvasSize); // bottom right
+    for (int i = 2; i < kNumBusyVerbs; ++i) {
+        float offset = i * ((float)kCanvasSize / kNumBusyVerbs);
+        busyPath.lineTo(kCanvasSize - offset, kCanvasSize + offset); // offscreen
+    }
+    ccpr.drawPath(busyPath);
+    SkDebugf("@@@@@@@@> 4\n");
+
+
+    const GrGLContextInfo& ctxInfo = ((GrGLGpu*)ctx->getGpu())->ctxInfo();
+    SkDebugf("@@@> ctxInfo.standard(): %i\n", ctxInfo.standard());
+    SkDebugf("@@@> ctxInfo.version(): %i\n", ctxInfo.version());
+    SkDebugf("@@@> ctxInfo.glslGeneration(): %i\n", ctxInfo.glslGeneration());
+    SkDebugf("@@@> ctxInfo.vendor(): %i\n", ctxInfo.vendor());
+    SkDebugf("@@@> ctxInfo.renderer(): %i\n", ctxInfo.renderer());
+    SkDebugf("@@@> ctxInfo.angleBackend(): %i\n", ctxInfo.angleBackend());
+    SkDebugf("@@@> ctxInfo.angleVendor(): %i\n", ctxInfo.angleVendor());
+    SkDebugf("@@@> ctxInfo.angleRenderer(): %i\n", ctxInfo.angleRenderer());
+    SkDebugf("@@@> ctxInfo.driver(): %i\n", ctxInfo.driver());
+    SkDebugf("@@@> ctxInfo.driverVersion(): %x\n", ctxInfo.driverVersion());
+
+    ccpr.flush(); // If this crashes, fiddle with fMaxInstancesPerDrawArraysWithoutCrashing in your
+                  // platform's GrGLCaps.
+    SkDebugf("@@@@@@@@> 5\n");
+}
+
+#endif
