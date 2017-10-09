9a9d7eb90a6b36a4686057fac4dc54a91c7c9bd2
Mike Reed <reed@google.com>
2017-10-09 10:09:59 -0400

    Revert "Revert "change computeByteSize to return max_size_t on overflow""
    This reverts commit 384f0a7d6626026f21313e85e51890d747171ee8.
    
    Bug: skia:
    Change-Id: I392ef5c1a5172181caf81ae270befeba6e778241

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -423,7 +423,7 @@ sk_sp<SkImage> SkImageMakeRasterCopyAndAssignColorSpace(const SkImage* src,
 
     size_t rowBytes = info.minRowBytes();
     size_t size = info.computeByteSize(rowBytes);
-    if (size == 0) {
+    if (size == SK_MaxSizeT) {
         return nullptr;
     }
     auto data = SkData::MakeUninitialized(size);
