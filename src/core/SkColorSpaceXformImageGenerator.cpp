2563ff942c0af72be399da87f15e0153ef2a4df3
Greg Daniel <egdaniel@google.com>
2017-10-09 15:46:01 -0400

    Add support for allocating mips inside of ColorSpaceXormImageGenerator
    Bug: skia:
    Change-Id: Ie0052e6591bcf1ae46d10d5026a6e4d06d2c3052
    Reviewed-on: https://skia-review.googlesource.com/57360
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- src/core/SkColorSpaceXformImageGenerator.cpp
+++ src/core/SkColorSpaceXformImageGenerator.cpp
@@ -87,7 +87,7 @@ sk_sp<GrTextureProxy> SkColorSpaceXformImageGenerator::onGenerateTexture(
 
     sk_sp<GrRenderTargetContext> renderTargetContext = ctx->makeDeferredRenderTargetContext(
             SkBackingFit::kExact, info.width(), info.height(), kRGBA_8888_GrPixelConfig, nullptr,
-            0, false, kTopLeft_GrSurfaceOrigin);
+            0, willNeedMipMaps, kTopLeft_GrSurfaceOrigin);
     if (!renderTargetContext) {
         return nullptr;
     }
