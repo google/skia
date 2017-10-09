9daaecf6a5378a80fb55c548ec2e04fe009b983e
Ben Wagner <benjaminwagner@google.com>
2017-10-09 22:58:53 +0000

    Revert "Revert "Fix a couple float-cast-overflow in SkScan*.""
    This reverts commit b6abb9b4e088abee8b8dfcce9c9f7eb759518198.
    
    Reason for revert: It doesn't make sense that this CL would affect the tests implicated in the perf regression in skia:7143, and the revert had no effect on the perf of those tests. Seems like the perf alert was either noise or due to a different CL.
    
    Original change's description:
    > Revert "Fix a couple float-cast-overflow in SkScan*."
    > 
    > This reverts commit 3cd0bef0fd9d062bbcc313c329b4f31925e8ded7.
    > 
    > Reason for revert: https://bugs.chromium.org/p/skia/issues/detail?id=7143
    > 
    > Original change's description:
    > > Fix a couple float-cast-overflow in SkScan*.
    > > 
    > > Bug: skia:5060
    > > Change-Id: I60a48993c77631aaad9354bb86b13204dc618bf4
    > > Reviewed-on: https://skia-review.googlesource.com/47422
    > > Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    > > Reviewed-by: Mike Reed <reed@google.com>
    > 
    > TBR=benjaminwagner@google.com,reed@google.com
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: skia:7143
    > Change-Id: I0f19720a7d8344789a375bbb6b9e28bf4f4e55ae
    > Reviewed-on: https://skia-review.googlesource.com/57240
    > Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    > Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    
    TBR=benjaminwagner@google.com,reed@google.com
    
    Change-Id: I29ac47d6665e2e52ee2a6500488dc407c8d2af1c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:7143

--- include/core/SkRect.h
+++ include/core/SkRect.h
@@ -411,6 +411,14 @@ struct SK_API SkRect {
         return r;
     }
 
+    // The largest SkRect for which round() is well-defined.
+    static SkRect SK_WARN_UNUSED_RESULT MakeLargestS32() {
+        const SkRect r = MakeLTRB(SK_MinS32FitsInFloat, SK_MinS32FitsInFloat,
+                                  SK_MaxS32FitsInFloat, SK_MaxS32FitsInFloat);
+        SkASSERT(r == Make(r.roundOut()));
+        return r;
+    }
+
     static constexpr SkRect SK_WARN_UNUSED_RESULT MakeWH(SkScalar w, SkScalar h) {
         return SkRect{0, 0, w, h};
     }
