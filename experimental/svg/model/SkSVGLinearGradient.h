a140f6770fbe8a1652a1d6b82079ed2a8021dcd6
Florin Malita <fmalita@chromium.org>
2017-10-09 14:27:40 -0400

    [SVGDom] Factor out common gradient logic
    In preparation of radial gradient support, move common logic into an
    abstract base class (SkSVGGradient).
    
    Change-Id: Ie5361048ca8fddd9070c573c8daef0d0f57dc95e

--- experimental/svg/model/SkSVGLinearGradient.h
+++ experimental/svg/model/SkSVGLinearGradient.h
@@ -8,46 +8,36 @@
 #ifndef SkSVGLinearGradient_DEFINED
 #define SkSVGLinearGradient_DEFINED
 
-#include "SkSVGHiddenContainer.h"
+#include "SkSVGGradient.h"
 #include "SkSVGTypes.h"
 
-class SkSVGLinearGradient : public SkSVGHiddenContainer {
+class SkSVGLinearGradient final : public SkSVGGradient {
 public:
     ~SkSVGLinearGradient() override = default;
     static sk_sp<SkSVGLinearGradient> Make() {
         return sk_sp<SkSVGLinearGradient>(new SkSVGLinearGradient());
     }
 
-    void setHref(const SkSVGStringType&);
-    void setGradientTransform(const SkSVGTransformType&);
-    void setSpreadMethod(const SkSVGSpreadMethod&);
     void setX1(const SkSVGLength&);
     void setY1(const SkSVGLength&);
     void setX2(const SkSVGLength&);
     void setY2(const SkSVGLength&);
 
 protected:
-    bool onAsPaint(const SkSVGRenderContext&, SkPaint*) const override;
-
     void onSetAttribute(SkSVGAttribute, const SkSVGValue&) override;
 
+    sk_sp<SkShader> onMakeShader(const SkSVGRenderContext&,
+                                 const SkColor*, const SkScalar*, int count,
+                                 SkShader::TileMode, const SkMatrix&) const override;
 private:
     SkSVGLinearGradient();
-
-    void collectColorStops(const SkSVGRenderContext&,
-                           SkSTArray<2, SkScalar, true>*,
-                           SkSTArray<2, SkColor, true>*) const;
 
     SkSVGLength fX1 = SkSVGLength(0  , SkSVGLength::Unit::kPercentage);
     SkSVGLength fY1 = SkSVGLength(0  , SkSVGLength::Unit::kPercentage);
     SkSVGLength fX2 = SkSVGLength(100, SkSVGLength::Unit::kPercentage);
     SkSVGLength fY2 = SkSVGLength(0  , SkSVGLength::Unit::kPercentage);
 
-    SkSVGStringType    fHref;
-    SkSVGTransformType fGradientTransform = SkSVGTransformType(SkMatrix::I());
-    SkSVGSpreadMethod  fSpreadMethod = SkSVGSpreadMethod(SkSVGSpreadMethod::Type::kPad);
-
-    typedef SkSVGHiddenContainer INHERITED;
+    typedef SkSVGGradient INHERITED;
 };
 
 #endif // SkSVGLinearGradient_DEFINED
