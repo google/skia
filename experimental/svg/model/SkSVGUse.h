6a69c053ff244a11ae41002bfc09b54a5dc9ec68
Florin Malita <fmalita@chromium.org>
2017-10-11 14:02:11 -0400

    [SVGDom] Initial <use> support
    https://www.w3.org/TR/SVG/struct.html#UseElement
    
    Change-Id: Id85c2648255dcd4763bf11c7bdb8b1490286f1e4
    Reviewed-on: https://skia-review.googlesource.com/58260
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

---
+++ experimental/svg/model/SkSVGUse.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkSVGUse_DEFINED
+#define SkSVGUse_DEFINED
+
+#include "SkSVGTransformableNode.h"
+#include "SkSVGTypes.h"
+
+/**
+ * Implements support for <use> (reference) elements.
+ * (https://www.w3.org/TR/SVG/struct.html#UseElement)
+ */
+class SkSVGUse final : public SkSVGTransformableNode {
+public:
+    ~SkSVGUse() override = default;
+
+    static sk_sp<SkSVGUse> Make() { return sk_sp<SkSVGUse>(new SkSVGUse()); }
+
+    void appendChild(sk_sp<SkSVGNode>) override;
+
+    void setHref(const SkSVGStringType&);
+    void setX(const SkSVGLength&);
+    void setY(const SkSVGLength&);
+
+protected:
+    void onSetAttribute(SkSVGAttribute, const SkSVGValue&) override;
+
+    bool onPrepareToRender(SkSVGRenderContext*) const override;
+    void onRender(const SkSVGRenderContext&) const override;
+    SkPath onAsPath(const SkSVGRenderContext&) const override;
+
+private:
+    SkSVGUse();
+
+    SkSVGStringType    fHref;
+    SkSVGLength        fX = SkSVGLength(0);
+    SkSVGLength        fY = SkSVGLength(0);
+
+    typedef SkSVGTransformableNode INHERITED;
+};
+
+#endif // SkSVGUse_DEFINED
