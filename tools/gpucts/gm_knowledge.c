f6e8bf7882833c05cd62eacdebbeb539c39eb77a
Hal Canary <halcanary@google.com>
2017-10-11 16:00:31 -0400

    GPU-CTS Program
    Add new application, called GPU-CTS (GPU Compatibility Test Suite),
    which executes skia gms against OpenGL and Vulkan backends.  Makes use
    of googletest library for consistancy with Android CTS programs.
    
    Add googletest to DEPS
    
    gm_knowledge.h header as a stub for future work on validating gm output.
    
    gm_runner can be re-used in other programs.  Talks to Skia and GM with a
    simple API.
    
    gpuctx executable wraps gm_runner and googletest together.
    
    Change-Id: Ie7350b22164fa73e44121c39b0f36da4038a700b

---
+++ tools/gpucts/gm_knowledge.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm_knowledge.h"
+
+// placeholder function definitions:
+float GMK_Check(GMK_ImageData d, const char* n) { return 0; }
+bool GMK_IsGoodGM(const char* n) { return true; }
