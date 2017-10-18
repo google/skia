e1916f42065a4abc5a401e1a45f61c1728f41045
Leon Scroggins III <scroggo@google.com>
2017-10-18 11:15:21 -0400

    Add an Option for orientation on JPEG encodes
    Add libexif, which is already used by Android, and use it to write the
    orientation. Write a makefile based on the Android.bp in Android, minus
    warnings. (libexif has an LGPL license.)
    
    Add a test that verifies all the orientations work.
    
    Optionally enable writing the orientation (and therefore including
    libexif). Chromium does not currently need it, and Android does not
    expose an API that would allow using it.
    
    Bug: skia:7138
    Change-Id: Iaeff44c36aebe0e639666979dc00e1b7594bbeb1

---
+++ third_party/libexif/config.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "../externals/libexif/config.h"
+
+#if defined(_MSC_VER)
+    #include <BaseTsd.h>
+    typedef SSIZE_T ssize_t;
+#endif
