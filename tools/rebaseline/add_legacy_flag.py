544762a08e24f4a3413af1a2be667b2fad3b751d
Yuqian Li <liyuqian@google.com>
2017-10-18 13:55:31 -0400

    Automatically generate CLs for legacy guard flags
    For example, a single command
    
     $ python add_legacy_flag.py SK_SUPPORT_LEGACY_AA_CHOICE -a /data/android -c ~/chromium/src -g legacyflag
    
    generates the following CLs
    
     * https://critique.corp.google.com/#review/172474970
     * https://chromium-review.googlesource.com/c/chromium/src/+/725820
     * https://googleplex-android-review.git.corp.google.com/#/c/platform/external/skia/+/3088783/
    
    
    Bug: skia:
    Change-Id: Ie9a875f19ac86e0de83a8962ab384b10f8085c17

---
+++ tools/rebaseline/add_legacy_flag.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# Copyright (c) 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+README = """
+Automatically add a specific legacy flag to multiple Skia client repos.
+
+This would only work on Google desktop.
+
+Example usage:
+  $ python add_legacy_flag.py SK_SUPPORT_LEGACY_SOMETHING \\
+      -a /data/android -c ~/chromium/src -g legacyflag
+
+If you only need to add the flag to one repo, for example, Android, please give
+only -a (--android-dir) argument:
+  $ python add_legacy_flag.py SK_SUPPORT_LEGACY_SOMETHING -a /data/android
+
+"""
+
+import os, sys
+import argparse
+import subprocess
+import getpass
+from random import randint
+
+
+ANDROID_TOOLS_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'android')
+
+sys.path.append(ANDROID_TOOLS_DIR)
+
+
+import upload_to_android
+
+
+def add_to_google3(args):
+  print "Run prodaccess to gain access to Google3:"
+  subprocess.check_call(['prodaccess'])
+
+  google3_dir = "/google/src/cloud/%s/%s/google3" % (getpass.getuser(),
+                                                     args.google3)
+
+  if not os.path.isdir(google3_dir):
+    print "Creating citc workspace %s:" % args.google3
+    subprocess.check_call(['g4', 'citc', args.google3])
+
+  os.chdir(google3_dir)
+
+  build_file = os.path.join('third_party', 'skia', 'HEAD', 'BUILD')
+
+  # reset any previous change on the same citc client
+  subprocess.check_call(['g4', 'revert', build_file])
+  subprocess.check_call(['g4', 'sync'])
+
+  with open(build_file) as f:
+    lines = f.readlines()
+
+  line_index = lines.index("DEFINES_GOOGLE3 = [\n")
+  lines.insert(line_index + 1, "    \"%s\",\n" % args.flag)
+
+  with open(build_file, 'w') as f:
+    for line in lines:
+      f.write(line)
+
+  subprocess.check_call(['g4', 'mail', '-m', 'benjaminwagner,liyuqian',
+                         '--desc', '"Add %s"' % args.flag])
+
+
+def add_to_android(args):
+  repo_binary = upload_to_android.init_work_dir(args.android_dir);
+
+  # Create repo branch.
+  subprocess.check_call('%s start flag .' % repo_binary, shell=True)
+
+  # Add flag to SkUserConfigManual.h.
+  config_file = os.path.join('include', 'config', 'SkUserConfigManual.h')
+  with open(config_file, 'a') as f:
+    f.write('\n')
+    f.write('#ifndef %s\n'    % args.flag)
+    f.write('  #define %s\n'  % args.flag)
+    f.write('#endif//%s\n'    % args.flag)
+  subprocess.check_call('git add %s' % config_file, shell=True)
+
+  message = ('Add %s\n\n'
+             'Test: Presubmit checks will test this change.' % args.flag)
+
+  subprocess.check_call('git commit -m "%s"' % message, shell=True)
+
+  # Upload to Android Gerrit.
+  subprocess.check_call('%s upload --verify' % repo_binary, shell=True)
+
+  # Remove repo branch
+  subprocess.check_call('%s abandon flag' % repo_binary, shell=True)
+
+
+def add_to_chromium(args):
+  os.chdir(args.chromium_dir)
+
+  branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+  branch = branch.strip()
+
+  EXPECTED_STASH_OUT = "No local changes to save"
+  stash_output = subprocess.check_output(['git', 'stash']).strip()
+
+  if branch != "master" or stash_output != EXPECTED_STASH_OUT:
+    print ("Please checkout a clean master branch at your chromium repo (%s) "
+        "before running this script") % args.chromium_dir
+    if stash_output != EXPECTED_STASH_OUT:
+      subprocess.check_call(['git', 'stash', 'pop'])
+    exit()
+
+  # Use random number to avoid branch name collision.
+  # We'll delete the branch in the end.
+  random = randint(1, 10000)
+  subprocess.check_call(['git', 'checkout', '-b', 'legacyflag_%d' % random])
+
+  config_file = os.path.join('skia', 'config', 'SkUserConfig.h')
+  with open(config_file) as f:
+    lines = f.readlines()
+
+  separator = (
+    "///////////////////////// Imported from BUILD.gn and skia_common.gypi\n")
+  index = lines.index(separator)
+
+  lines.insert(index, "#ifndef {0}\n#define {0}\n#endif\n\n".format(args.flag))
+
+  with open(config_file, 'w') as f:
+    for line in lines:
+      f.write(line)
+
+  subprocess.check_call('git commit -a -m "Add %s"' % args.flag, shell=True)
+  subprocess.check_call('git cl upload -m "Add %s" -f' % args.flag, shell=True)
+
+  subprocess.check_call(['git', 'checkout', 'master'])
+  subprocess.check_call(['git', 'branch', '-D', 'legacyflag_%d' % random])
+
+
+def main():
+  if len(sys.argv) <= 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    print README
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--android-dir', '-a', required=False,
+      help='Directory where an Android checkout will be created (if it does '
+           'not already exist). Note: ~1GB space will be used.')
+  parser.add_argument(
+      '--chromium-dir', '-c', required=False,
+      help='Directory of an EXISTING Chromium checkout (e.g., ~/chromium/src)')
+  parser.add_argument(
+      '--google3', '-g', required=False,
+      help='Google3 workspace to be created (if it does not already exist).')
+  parser.add_argument('flag', type=str, help='legacy flag name')
+
+  args = parser.parse_args()
+
+  if not args.android_dir and not args.chromium_dir and not args.google3:
+    print """
+Nothing to do. Please give me at least one of these three arguments:
+  -a (--android-dir)
+  -c (--chromium-dir)
+  -g (--google3)
+"""
+    exit()
+
+  end_message = "CLs generated. Now go review and land them:\n"
+  if args.chromium_dir:
+    args.chromium_dir = os.path.expanduser(args.chromium_dir)
+    add_to_chromium(args)
+    end_message += " * https://googleplex-android-review.git.corp.google.com\n"
+  if args.android_dir:
+    args.android_dir = os.path.expanduser(args.android_dir)
+    add_to_android(args)
+    end_message += " * https://chromium-review.googlesource.com\n"
+  if args.google3:
+    add_to_google3(args)
+    end_message += " * https://critique.corp.google.com\n"
+
+  print end_message
+
+
+if __name__ == '__main__':
+  main()
