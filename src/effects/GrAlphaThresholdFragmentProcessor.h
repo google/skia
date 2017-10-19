c3e94b09a54402cb83d855f2077edda1eab32a9d
Brian Osman <brianosman@google.com>
2017-10-19 11:22:52 -0400

    Remove color space xform from alpha threshold FP
    This does math on the sampled color. In order to do that math in the
    destination color space, I split the behavior up - the threshold FP
    now operates on the input color, and we construct a series with a
    simple texture effect (possibly wrapped in a color xform), then the
    threshold.
    
    I also added a GM that verifies this behavior. All other GMs using
    this effect were operating on a layer source, which is always
    created in the destination color space. The new GM explicitly makes
    a DAG with an image source, so the alpha threshold filter needs to
    handle any color space mismatch.
    
    Bug: skia:
    Change-Id: I1ed08c99f4eed17f68176bf751677a3ae1614fe3

--- src/effects/GrAlphaThresholdFragmentProcessor.h
+++ src/effects/GrAlphaThresholdFragmentProcessor.h
@@ -18,55 +18,40 @@
 class GrAlphaThresholdFragmentProcessor : public GrFragmentProcessor {
 public:
     inline OptimizationFlags optFlags(float outerThreshold);
-    sk_sp<GrColorSpaceXform> colorXform() const { return fColorXform; }
     float innerThreshold() const { return fInnerThreshold; }
     float outerThreshold() const { return fOuterThreshold; }
 
-    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrTextureProxy> image,
-                                                     sk_sp<GrColorSpaceXform>
-                                                             colorXform,
-                                                     sk_sp<GrTextureProxy>
-                                                             mask,
+    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrTextureProxy> mask,
                                                      float innerThreshold,
                                                      float outerThreshold,
                                                      const SkIRect& bounds) {
         return std::unique_ptr<GrFragmentProcessor>(new GrAlphaThresholdFragmentProcessor(
-                image, colorXform, mask, innerThreshold, outerThreshold, bounds));
+                mask, innerThreshold, outerThreshold, bounds));
     }
     GrAlphaThresholdFragmentProcessor(const GrAlphaThresholdFragmentProcessor& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
     const char* name() const override { return "AlphaThresholdFragmentProcessor"; }
 
 private:
-    GrAlphaThresholdFragmentProcessor(sk_sp<GrTextureProxy> image,
-                                      sk_sp<GrColorSpaceXform> colorXform,
-                                      sk_sp<GrTextureProxy> mask, float innerThreshold,
+    GrAlphaThresholdFragmentProcessor(sk_sp<GrTextureProxy> mask, float innerThreshold,
                                       float outerThreshold, const SkIRect& bounds)
             : INHERITED(kGrAlphaThresholdFragmentProcessor_ClassID, kNone_OptimizationFlags)
-            , fImage(std::move(image))
-            , fColorXform(colorXform)
             , fMask(std::move(mask))
             , fInnerThreshold(innerThreshold)
             , fOuterThreshold(outerThreshold)
-            , fImageCoordTransform(SkMatrix::I(), fImage.proxy())
             , fMaskCoordTransform(
                       SkMatrix::MakeTrans(SkIntToScalar(-bounds.x()), SkIntToScalar(-bounds.y())),
                       fMask.proxy()) {
-        this->addTextureSampler(&fImage);
         this->addTextureSampler(&fMask);
-        this->addCoordTransform(&fImageCoordTransform);
         this->addCoordTransform(&fMaskCoordTransform);
     }
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
     void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
     bool onIsEqual(const GrFragmentProcessor&) const override;
     GR_DECLARE_FRAGMENT_PROCESSOR_TEST
-    TextureSampler fImage;
-    sk_sp<GrColorSpaceXform> fColorXform;
     TextureSampler fMask;
     float fInnerThreshold;
     float fOuterThreshold;
-    GrCoordTransform fImageCoordTransform;
     GrCoordTransform fMaskCoordTransform;
     typedef GrFragmentProcessor INHERITED;
 };
