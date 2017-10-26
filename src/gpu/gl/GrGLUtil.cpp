25019175eb82f2b0788e43987702f91551d88544
Greg Daniel <egdaniel@google.com>
2017-10-26 13:32:33 -0400

    Add GalliumLLVM GrGLRenderer and fix alpha8 renderability
    Bug: skia:
    Change-Id: I20f41966ed140327e0a92618da8e4091e1de1ea8
    Reviewed-on: https://skia-review.googlesource.com/64260
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- src/gpu/gl/GrGLUtil.cpp
+++ src/gpu/gl/GrGLUtil.cpp
@@ -372,6 +372,9 @@ GrGLRenderer GrGLGetRendererFromString(const char* rendererString) {
         if (0 == strcmp("Mesa Offscreen", rendererString)) {
             return kOSMesa_GrGLRenderer;
         }
+        if (strstr(rendererString, "llvmpipe")) {
+            return kGalliumLLVM_GrGLRenderer;
+        }
         static const char kMaliTStr[] = "Mali-T";
         if (0 == strncmp(rendererString, kMaliTStr, SK_ARRAY_COUNT(kMaliTStr) - 1)) {
             return kMaliT_GrGLRenderer;
