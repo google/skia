511a9d49998ec6a74c375e6cfc55f660f7987c40
Robert Phillips <robertphillips@google.com>
2017-10-27 15:55:31 -0400

    Fix GrDefaultPathRender inversely wound path bug
    Bug: 769898
    Change-Id: I3b1a43b1e114b35105493a0cfa01a1f01b65fa56
    Reviewed-on: https://skia-review.googlesource.com/64065
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

--- src/gpu/ops/GrDefaultPathRenderer.cpp
+++ src/gpu/ops/GrDefaultPathRenderer.cpp
@@ -568,8 +568,10 @@ bool GrDefaultPathRenderer::internalDrawPath(GrRenderTargetContext* renderTarget
     SkScalar srcSpaceTol = GrPathUtils::scaleToleranceToSrc(tol, viewMatrix, path.getBounds());
 
     SkRect devBounds;
-    GetPathDevBounds(path, renderTargetContext->width(), renderTargetContext->height(), viewMatrix,
-                     &devBounds);
+    GetPathDevBounds(path,
+                     renderTargetContext->asRenderTargetProxy()->worstCaseWidth(),
+                     renderTargetContext->asRenderTargetProxy()->worstCaseHeight(),
+                     viewMatrix, &devBounds);
 
     for (int p = 0; p < passCount; ++p) {
         if (lastPassIsBounds && (p == passCount-1)) {
