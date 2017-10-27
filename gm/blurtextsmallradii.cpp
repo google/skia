77ed9debb4af31c2d0594ef22943f913ea416860
Herb Derby <herb@google.com>
2017-10-27 16:53:31 -0400

    Direct evaluation of gaussian
    Change-Id: Ibd0caa73cadd06b637f55ba7bd4fefcfe7ac73db
    Bug: skia:

--- gm/blurtextsmallradii.cpp
+++ gm/blurtextsmallradii.cpp
@@ -10,19 +10,23 @@
 #include "SkColor.h"
 
 // GM to check the behavior from chrome bug:745290
-DEF_SIMPLE_GM(blurSmallRadii, canvas, 100, 100) {
-    double sigmas[] = {0.5, 0.75, 1.0, 1.5, 2.5};
+DEF_SIMPLE_GM(blurSmallRadii, canvas, 100, 335) {
+    double sigmas[] = {0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5};
     SkPaint paint;
 
     for (auto sigma : sigmas) {
         paint.setColor(SK_ColorBLACK);
         paint.setAntiAlias(true);
-        paint.setMaskFilter(SkBlurMaskFilter::Make(kNormal_SkBlurStyle, sigma));
-        canvas->drawString("Guest", 20, 10, paint);
+        paint.setTextSize(24);
+        if (sigma != 0.0) {
+            paint.setMaskFilter(SkBlurMaskFilter::Make(kNormal_SkBlurStyle, sigma));
+        }
+        canvas->drawString("Guest", 20, 24, paint);
 
         paint.setMaskFilter(nullptr);
         paint.setColor(SK_ColorWHITE);
-        canvas->drawString("Guest", 20, 10, paint);
-        canvas->translate(0, 20);
+        canvas->drawString("Guest", 20, 24, paint);
+
+        canvas->translate(0, 30);
     }
 }
