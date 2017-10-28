830f4f688761df51294cc850668cee2ba05538eb
Florin Malita <fmalita@chromium.org>
2017-10-28 10:01:23 -0400

    Re-land: Stop using fixed point recs for gradient positioning info
    The fixed point gradient records are historical relics.  Instead of
    round-tripping through FP, just use the float positions directly.
    
    We can remove the recs completely in a follow-up, after rebaselining
    existing clients.
    
    Change-Id: Ic3c8e58af6dfa76744799513c9e60af0462014d0

--- src/shaders/gradients/SkGradientShader.cpp
+++ src/shaders/gradients/SkGradientShader.cpp
@@ -20,6 +20,7 @@
 #include "SkWriteBuffer.h"
 #include "../../jumper/SkJumper.h"
 
+#include <cmath>
 
 enum GradientSerializationFlags {
     // Bits 29:31 used for various boolean flags
@@ -498,11 +499,6 @@ bool SkGradientShaderBase::onAsLuminanceColor(SkColor* lum) const {
     return true;
 }
 
-static inline int SkFixedToFFFF(SkFixed x) {
-    SkASSERT((unsigned)x <= SK_Fixed1);
-    return x - (x >> 16);
-}
-
 static constexpr int kGradientTextureSize = 256;
 
 void SkGradientShaderBase::initLinearBitmap(SkBitmap* bitmap, GradientBitmapType bitmapType) const {
@@ -540,13 +536,14 @@ void SkGradientShaderBase::initLinearBitmap(SkBitmap* bitmap, GradientBitmapType
     // our own CS for identity/no transform.
     auto* cs = bitmapType != GradientBitmapType::kLegacy ? fColorSpace.get() : nullptr;
 
-    // TODO: refactor to avoid using fRecs.
-    static constexpr unsigned kCacheShift = 8;
+    // Historically, stops have been mapped to [0, 256), then truncated to get the
+    // texture index.  This seems to produce the best results for some common distributions,
+    // so we preserve this behavior.
+    const auto kClamp = std::nextafterf(kGradientTextureSize, 0);
 
     int prevIndex = 0;
     for (int i = 1; i < fColorCount; i++) {
-        int nextIndex = (fColorCount == 2) ? (kGradientTextureSize - 1)
-            : SkFixedToFFFF(fRecs[i].fPos) >> kCacheShift;
+        int nextIndex = std::min(this->getPos(i) * kGradientTextureSize, kClamp);
         SkASSERT(nextIndex < kGradientTextureSize);
 
         if (nextIndex > prevIndex) {
@@ -654,13 +651,8 @@ void SkGradientShaderBase::commonAsAGradient(GradientInfo* info) const {
                 memcpy(info->fColors, fOrigColors, fColorCount * sizeof(SkColor));
             }
             if (info->fColorOffsets) {
-                if (fColorCount == 2) {
-                    info->fColorOffsets[0] = 0;
-                    info->fColorOffsets[1] = SK_Scalar1;
-                } else if (fColorCount > 2) {
-                    for (int i = 0; i < fColorCount; ++i) {
-                        info->fColorOffsets[i] = SkFixedToScalar(fRecs[i].fPos);
-                    }
+                for (int i = 0; i < fColorCount; ++i) {
+                    info->fColorOffsets[i] = this->getPos(i);
                 }
             }
         }
@@ -685,7 +677,7 @@ void SkGradientShaderBase::toString(SkString* str) const {
     if (fColorCount > 2) {
         str->append(" points: (");
         for (int i = 0; i < fColorCount; ++i) {
-            str->appendScalar(SkFixedToScalar(fRecs[i].fPos));
+            str->appendScalar(this->getPos(i));
             if (i < fColorCount-1) {
                 str->append(", ");
             }
