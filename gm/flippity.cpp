ea7bbba23d7feb8e0a51c2b86902571a5eba7cd4
Robert Phillips <robertphillips@google.com>
2017-10-30 13:33:39 -0400

    Add a GM to test out odd matrix draws
    Change-Id: I90c15bf019161abc5dab78e0af20ef15ff1395d9

---
+++ gm/flippity.cpp
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm.h"
+#include "sk_tool_utils.h"
+
+#include "SkColorFilter.h"
+#include "SkSurface.h"
+#include "SkImage_Gpu.h"
+
+static const int kNumMatrices = 6;
+static const int kImageSize = 128;
+static const int kLabelSize = 32;
+
+static const int kCellSize = kImageSize+2*kLabelSize;
+static const int kGMWidth  = kNumMatrices*kCellSize;
+static const int kGMHeight = 2*kCellSize;
+
+static const int kLL = 0;
+static const int kLR = 1;
+static const int kUL = 2;
+static const int kUR = 3;
+static const int kNumLabels = 4;
+
+static const SkPoint kPoints[kNumLabels] = {
+    {          0, kImageSize },     // kLL
+    { kImageSize, kImageSize },     // kLR
+    {          0,          0 },     // kUL
+    { kImageSize,          0 },     // kUR
+};
+
+static const SkColor kColors[kNumMatrices] = {
+    SK_ColorRED,
+    SK_ColorGREEN,
+    SK_ColorBLUE,
+    SK_ColorCYAN,
+    SK_ColorMAGENTA,
+    SK_ColorYELLOW,
+};
+
+#if 0
+#include "SkImageEncoder.h"
+
+static void write_bm(const char* name, const SkBitmap& bm) {
+
+    static int foo = 0;
+
+    char fullName[256];
+    snprintf(fullName, 256, "d:\\src\\bugs\\%s%d.png", name, foo++);
+    fullName[255] = '\0';
+
+    SkFILEWStream file(fullName);
+    SkEncodeImage(&file, bm, SkEncodedImageFormat::kPNG, 100);
+}
+#endif
+
+// Create a fixed size text label like "LL" or "LR".
+static sk_sp<SkImage> make_text_image(GrContext* context, const char* text) {
+    SkPaint paint;
+    paint.setAntiAlias(true);
+    paint.setTextSize(32);
+
+    SkRect bounds;
+    paint.measureText(text, strlen(text), &bounds);
+    const SkMatrix mat = SkMatrix::MakeRectToRect(bounds, SkRect::MakeWH(kLabelSize, kLabelSize),
+                                                  SkMatrix::kFill_ScaleToFit);
+
+    const SkImageInfo ii = SkImageInfo::MakeN32Premul(kLabelSize, kLabelSize);
+    sk_sp<SkSurface> surf = SkSurface::MakeRaster(ii);
+
+    SkCanvas* canvas = surf->getCanvas();
+
+    canvas->clear(SK_ColorWHITE);
+    canvas->concat(mat);
+    canvas->drawText(text, strlen(text), 0, 0, paint);
+
+    sk_sp<SkImage> image = surf->makeImageSnapshot();
+
+    return image->makeTextureImage(context, nullptr);
+}
+
+static void swap_rows(SkBitmap* bm, int rowOffset) {
+    SkASSERT(rowOffset >= 0 && rowOffset < bm->height());
+
+    int otherRow = bm->height() - rowOffset - 1;
+    SkASSERT(rowOffset != otherRow);
+
+    uint32_t* r1 = bm->getAddr32(0, rowOffset);
+    uint32_t* r2 = bm->getAddr32(0, otherRow);
+
+    for (int x = 0; x < bm->width(); ++x) {
+        uint32_t tmp = r1[x];
+        r1[x] = r2[x];
+        r2[x] = tmp;
+    }
+
+}
+
+// Create an image with each corner marked w/ "LL", "LR", etc., with the origin either bottom-left
+// or top-left.
+static sk_sp<SkImage> make_image(GrContext* context, const SkTArray<sk_sp<SkImage>>& labels,
+                                 bool bottomLeftOrigin) {
+    SkASSERT(kNumLabels == labels.count());
+
+    SkImageInfo ii = SkImageInfo::Make(kImageSize, kImageSize,
+                                       kN32_SkColorType, kOpaque_SkAlphaType);
+    SkBitmap bm;
+    bm.allocPixels(ii);
+    SkCanvas canvas(bm);
+
+    canvas.clear(SK_ColorWHITE);
+    for (int i = 0; i < kNumLabels; ++i) {
+        canvas.drawImage(labels[i],
+                         0.0 != kPoints[i].fX ? kPoints[i].fX-kLabelSize : 0,
+                         0.0 != kPoints[i].fY ? kPoints[i].fY-kLabelSize : 0);
+    }
+
+    GrSurfaceDesc desc;
+    desc.fOrigin = kTopLeft_GrSurfaceOrigin;
+    desc.fWidth = kImageSize;
+    desc.fHeight = kImageSize;
+    desc.fConfig = kRGBA_8888_GrPixelConfig;
+
+    if (bottomLeftOrigin) {
+        // flip the data & the origin
+        for (int y = 0; y < kImageSize/2; ++y) {
+            swap_rows(&bm, y);
+        }
+        desc.fOrigin = kBottomLeft_GrSurfaceOrigin;
+    }
+
+    sk_sp<GrTextureProxy> proxy = GrSurfaceProxy::MakeDeferred(context->resourceProvider(),
+                                                               desc, SkBudgeted::kYes,
+                                                               bm.getPixels(), bm.rowBytes());
+
+    return sk_make_sp<SkImage_Gpu>(context, kNeedNewImageUniqueID, kOpaque_SkAlphaType,
+                                   std::move(proxy), nullptr, SkBudgeted::kYes);
+}
+
+static void create_matrices(SkMatrix* mats) {
+    mats[0].setAll( 0, -1, 1,
+                   -1,  0, 1,
+                    0,  0, 1);
+
+    mats[1].setAll( 1,  0, 0,
+                    0, -1, 1,
+                    0,  0, 1);
+
+    // flip x
+    mats[2].setAll(-1,  0, 1,
+                    0,  1, 0,
+                    0,  0, 1);
+
+    mats[3].setAll( 0,  1, 0,
+                   -1,  0, 1,
+                    0,  0, 1);
+//    mats[3].setRotate(-90, 0.5f, 0.5f);
+
+    // flip both x & y == rotate 180
+    mats[4].setAll(-1,  0, 1,
+                    0, -1, 1,
+                    0,  0, 1);
+//    mats[4].setRotate(180, 0.5f, 0.5f);
+
+    // identity
+    mats[5].setAll( 1,  0, 0,
+                    0,  1, 0,
+                    0,  0, 1);
+//    mats[5].setRotate(0, 0.5f, 0.5f);
+}
+
+class FlippityGM : public skiagm::GM {
+public:
+    FlippityGM() {
+        this->setBGColor(sk_tool_utils::color_to_565(0xFFCCCCCC));
+    }
+
+protected:
+
+    SkString onShortName() override {
+        return SkString("flippity");
+    }
+
+    SkISize onISize() override {
+        return SkISize::Make(kGMWidth, kGMHeight);
+    }
+
+    void onOnceBeforeDraw() override {
+
+        create_matrices(fMatrices);
+
+
+        for (int i = 0; i < kNumMatrices; ++i) {
+//            SkMatrix yFlip = SkMatrix::I();
+//            yFlip.setAll(1.0f,  0.0f, 0.0f,
+//                         0.0f, -1.0f, 1.0f,
+//                         0.0f,  0.0f, 1.0f);
+
+//            fMatrices[i].preConcat(yFlip);
+            fMatrices[i][SkMatrix::kMTransX] *= kImageSize;
+            fMatrices[i][SkMatrix::kMTransY] *= kImageSize;
+        }
+
+#if 0
+        SkMatrix yFlip = SkMatrix::I();
+        yFlip.setAll(1.0f,  0.0f, 0.0f,
+                     0.0f, -1.0f, 1.0f,
+                     0.0f,  0.0f, 1.0f);
+
+        for (int i = 0; i < kNumMatrices; ++i) {
+            fMatrices[i].preConcat(yFlip);
+            fMatrices[i].preScale(1.0f/kImageSize, 1.0f/kImageSize);
+            fMatrices[i].postScale(kImageSize, kImageSize);
+
+            SkMatrix tmp;
+            SkAssertResult(fMatrices[i].invert(&tmp));
+            fMatrices[i] = tmp;
+        }
+#endif
+    }
+
+    // Draw the reference image and the four corner labels in the matrix' coordinate space
+    void drawImageWithMatrixAndLabels(SkCanvas* canvas, SkImage* image, const SkMatrix& mat,
+                                      SkColor color) {
+        SkPaint paint;
+//        paint.setColorFilter(SkColorFilter::MakeModeFilter(color, SkBlendMode::kModulate));
+
+        canvas->save();
+            canvas->concat(mat);
+
+            canvas->drawImage(image, 0, 0, &paint);
+
+#if 1
+            for (int i = 0; i < kNumLabels; ++i) {
+                canvas->drawImage(fLabels[i],
+                                  0.0f == kPoints[i].fX ? -kLabelSize : kPoints[i].fX,
+                                  0.0f == kPoints[i].fY ? -kLabelSize : kPoints[i].fY,
+                                  &paint);
+            }
+#endif
+
+        canvas->restore();
+    }
+
+    void onDraw(SkCanvas* canvas) override {
+        GrContext* context = canvas->getGrContext();
+
+        SkASSERT(!fLabels.count());
+        fLabels.push_back(make_text_image(context, "LL"));
+        fLabels.push_back(make_text_image(context, "LR"));
+        fLabels.push_back(make_text_image(context, "UL"));
+        fLabels.push_back(make_text_image(context, "UR"));
+        SkASSERT(kNumLabels == fLabels.count());
+
+        // Top row gets TL image
+        {
+            sk_sp<SkImage> referenceImage = make_image(context, fLabels, false);
+
+            canvas->save();
+            canvas->translate(kLabelSize, kLabelSize);
+
+            for (int i = 0; i < kNumMatrices; ++i) {
+                this->drawImageWithMatrixAndLabels(canvas, referenceImage.get(),
+                                                   fMatrices[i], kColors[i]);
+
+                canvas->translate(kImageSize+2*kLabelSize, 0);
+            }
+            canvas->restore();
+        }
+
+        // Bottom row gets BL image
+        {
+            sk_sp<SkImage> referenceImage = make_image(context, fLabels, true);
+
+            canvas->save();
+            canvas->translate(kLabelSize, kImageSize+3*kLabelSize);
+
+            for (int i = 0; i < kNumMatrices; ++i) {
+                this->drawImageWithMatrixAndLabels(canvas, referenceImage.get(),
+                                                   fMatrices[i], kColors[i]);
+
+                canvas->translate(kCellSize, 0);
+            }
+            canvas->restore();
+        }
+
+        // separator grid
+        canvas->drawLine(0, kCellSize, kGMWidth, kCellSize, SkPaint());
+        for (int i = 0; i < kNumMatrices; ++i) {
+            canvas->drawLine(i * kCellSize, 0, i * kCellSize, kGMHeight, SkPaint());
+        }
+
+#if 0
+        const SkRect box = SkRect::MakeWH(kImageWidth, kImageHeight);
+
+        SkPaint stroke;
+        stroke.setColor(SK_ColorRED);
+        stroke.setStrokeWidth(1.0f);
+        stroke.setStyle(SkPaint::kStroke_Style);
+
+        for (int i = 0; i < 6; ++i) {
+            canvas->save();
+
+            canvas->translate(i*kImageWidth, 0);
+            /**/canvas->concat(fMatrices[i]);
+
+            sk_sp<SkImage> image = make_image(context, false);
+            canvas->drawImage(std::move(image), 0, 0);
+
+            canvas->drawRect(box, stroke);
+
+            canvas->restore();
+        }
+#endif
+    }
+
+private:
+    SkMatrix                 fMatrices[kNumMatrices];
+    SkTArray<sk_sp<SkImage>> fLabels;
+
+    typedef GM INHERITED;
+};
+
+DEF_GM(return new FlippityGM;)
