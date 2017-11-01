3be398fd6243fb7e0a76abadaea8220d41fb0049
Mike Klein <mtklein@chromium.org>
2017-11-01 13:31:28 -0400

    add lowp 2-pt gradient and gradient clamp
    I was originally going to add these to help
    test a lowp dither, but after looking at
    diffs I don't think lowp dither is a good idea.
    
    Non-dithered lowp gradients look fine to me so far.
    
    Change-Id: I8f5e75aec726983186214845ca38cfa0d54496b3

--- src/jumper/SkJumper_stages_lowp.cpp
+++ src/jumper/SkJumper_stages_lowp.cpp
@@ -744,6 +744,28 @@ STAGE_PP(lerp_565, const SkJumper_MemoryCtx* ctx) {
     a = lerp(da, a, ca);
 }
 
+// ~~~~~~ Gradient stages ~~~~~~ //
+
+// Clamp x to [0,1], both sides inclusive (think, gradients).
+// Even repeat and mirror funnel through a clamp to handle bad inputs like +Inf, NaN.
+SI F clamp_01(F v) { return min(max(0, v), 1); }
+
+STAGE_GG(clamp_x_1, Ctx::None) { x = clamp_01(x); }
+
+STAGE_GP(evenly_spaced_2_stop_gradient, const void* ctx) {
+    // TODO: Rename Ctx SkJumper_EvenlySpacedGradientCtx.
+    struct Ctx { float f[4], b[4]; };
+    auto c = (const Ctx*)ctx;
+
+    auto round = [](F x) { return cast<U16>(x * 255.0f + 0.5f); };
+
+    auto t = x;
+    r = round(mad(t, c->f[0], c->b[0]));
+    g = round(mad(t, c->f[1], c->b[1]));
+    b = round(mad(t, c->f[2], c->b[2]));
+    a = round(mad(t, c->f[3], c->b[3]));
+}
+
 // ~~~~~~ Compound stages ~~~~~~ //
 
 STAGE_PP(srcover_rgba_8888, const SkJumper_MemoryCtx* ctx) {
