dc89ad9b027a13c962b4d47ee27423fda6a886ec
Hal Canary <halcanary@google.com>
2017-11-01 16:18:54 -0400

    GPU-CTS: make_gmkb, gm_knowledge (GM Knowledgebase)
    Add a real implementation for gm_knowledge.h  This depends on
    the presence of files in the form $GMK_DIR/foo/{max,min}.png
    
    The implementation also writes out failures in $GMK_REPORT/.
    
    Add a utility: experimental/make_gmkb which is a stand-alone
    go executable that generates the foo/{max,min}.png data.
    
    Change-Id: Ibe1e9a7e7de143d14eee3877f5f2d2d8713f7f49

---
+++ tools/gpucts/gm_knowledge.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm_knowledge.h"
+
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "SkOSFile.h"
+
+#include "png_interface.h"
+
+constexpr char GMK_DIR_ENV[] ="GMK_DIR";
+constexpr char GMK_REPORT_ENV[] = "GMK_REPORT";
+constexpr char PATH_MAX_PNG[] = "max.png";
+constexpr char PATH_MIN_PNG[] = "min.png";
+
+static std::string path_join(const std::string& u, const char* v) {
+    return u + "/" + v;
+}
+
+static std::string getenv_or_abort(const char* key) {
+    const char* value = getenv(key);
+    if (!value) {
+        fprintf(stderr, "Missing environment variable: '%s'.\n", key);
+        abort();
+    }
+    return std::string(value);
+}
+
+int get_error(uint32_t value, uint32_t value_max, uint32_t value_min) {
+    int error = 0;
+    for (int j : {0, 8, 16, 24}) {
+        uint8_t    v = (value     >> j) & 0xFF,
+                vmin = (value_min >> j) & 0xFF,
+                vmax = (value_max >> j) & 0xFF;
+        if (v > vmax) {
+            error = std::max(v - vmax, error);
+        } else if (v < vmin) {
+            error = std::max(vmin - v, error);
+        }
+    }
+    return error;
+}
+
+// Assumes that for each GM foo, there are files $GMK_DIR/foo/{max,min}.png
+
+float GMK_Check(GMK_ImageData image, const char* name, const char* backend) {
+    if (image.width <= 0 || image.height <= 0) {
+        return 9009.0f;
+    }
+    std::string path = path_join(getenv_or_abort(GMK_DIR_ENV), name);
+    if (!sk_isdir(path.c_str())) {
+        return 9000.0f;
+    }
+    std::string max_path = path_join(path.c_str(), PATH_MAX_PNG);
+    std::vector<uint32_t> max_image_pixels, min_image_pixels;
+    GMK_ImageData max_image = ReadPngRgba8888FromFile(max_path.c_str(), &max_image_pixels);
+    if (!max_image.pix) {
+        return 9001.0f;
+    }
+    std::string min_path = path_join(path.c_str(), PATH_MIN_PNG);
+    GMK_ImageData min_image = ReadPngRgba8888FromFile(min_path.c_str(), &min_image_pixels);
+    if (!min_image.pix) {
+        return 9002.0f;
+    }
+    if (max_image.width != min_image.width || max_image.height != min_image.height) {
+        return 9003.0f;
+    }
+    if (max_image.width != image.width || max_image.height != image.height) {
+        return 9004.0f;
+    }
+    size_t N = (unsigned)image.width * (unsigned)image.height;
+    assert(max_image_pixels.size() == N);
+    assert(min_image_pixels.size() == N);
+    int badness = 0;
+    int badPixelCount = 0;
+    for (size_t i = 0; i < N; ++i) {
+        int error = get_error(image.pix[i], max_image.pix[i], min_image.pix[i]);
+        if (error > 0) {
+            badness = std::max(error, badness);
+            ++badPixelCount;
+        }
+    }
+    if (badness > 0) {
+        std::string report_directory = getenv_or_abort(GMK_REPORT_ENV);
+        sk_mkdir(report_directory.c_str());
+        report_directory = path_join(report_directory, backend);
+        sk_mkdir(report_directory.c_str());
+        std::string report_subdirectory = path_join(report_directory, name);
+        sk_mkdir(report_subdirectory.c_str());
+        std::string error_path = path_join(report_subdirectory, "image.png");
+        SkAssertResult(WritePngRgba8888ToFile(image, error_path.c_str()));
+        std::vector<uint32_t> errors(N);
+        for (size_t i = 0; i < N; ++i) {
+            int error = get_error(image.pix[i], max_image.pix[i], min_image.pix[i]);
+            errors[i] = error > 0 ? 0xFF000000 + error : 0x00000000;
+        }
+        error_path = path_join(report_subdirectory, "errors.png");
+        SkAssertResult(WritePngRgba8888ToFile(
+                    GMK_ImageData{errors.data(), image.width, image.height},
+                    error_path.c_str()));
+        auto report_path = path_join(report_subdirectory, "report.html");
+        std::ofstream(report_path.c_str())
+               << "<style>img{border:1px green solid}</style>"
+               << "maximum error: " << badness << "\n<br>\n"
+               << "bad pixel counts: " << badPixelCount << "\n<br>\n"
+               << "<img src=\"image.png\">\n<img src=\"errors.png\">\n";
+    }
+    return (float)badness;
+}
+
+bool GMK_IsGoodGM(const char* name) {
+    std::string path = path_join(getenv_or_abort(GMK_DIR_ENV), name);
+    return sk_isdir(path.c_str());
+}
+
---
+++ tools/gpucts/make_gmkb.go
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"sync"
+)
+
+const (
+	dot_png = ".png"
+	min_png = "min.png"
+	max_png = "max.png"
+)
+
+type ParamSet struct {
+	Config     []string `json:"config"`
+	SourceType []string `json:"source_type"`
+}
+
+type SRDigest struct {
+	Status string   `json:"status"`
+	Params ParamSet `json:"paramset"`
+	URL    string   `json:"URL"`
+}
+
+type TestRecord struct {
+	TestName string     `json:"testName"`
+	Digests  []SRDigest `json:"digests"`
+}
+
+func in(v string, a []string) bool {
+	for _, u := range a {
+		if u == v {
+			return true
+		}
+	}
+	return false
+}
+
+func process_test(testName string, imgUrls []string, output string) error {
+	output_directory := path.Join(output, testName)
+	var img_max image.NRGBA
+	var img_min image.NRGBA
+	for _, url := range imgUrls {
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		img, err := png.Decode(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Printf("Missing: %s %s\n", testName, url)
+			continue // Not an error
+		}
+		if img_max.Rect.Max.X == 0 {
+			img_max = to_nrgba(img)
+			img_min = copy_nrgba(img_max)
+			continue
+		}
+		w := img.Bounds().Max.X - img.Bounds().Min.X
+		h := img.Bounds().Max.Y - img.Bounds().Min.Y
+		if img_max.Rect.Max.X != w || img_max.Rect.Max.Y != h {
+			return errors.New("size mismatch")
+		}
+		img_copy := to_nrgba(img)
+		for i, value := range img_copy.Pix {
+			if value > img_max.Pix[i] {
+				img_max.Pix[i] = value
+			} else if value < img_min.Pix[i] {
+				img_min.Pix[i] = value
+			}
+		}
+	}
+	if img_max.Rect.Max.X == 0 {
+		return nil
+	}
+	err := os.Mkdir(output_directory, os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		return err
+	}
+	err = write_png_to_file(path.Join(output_directory, min_png), &img_min)
+	if err != nil {
+		return err
+	}
+	err = write_png_to_file(path.Join(output_directory, max_png), &img_max)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+func readMetaJsonFile(filename string) ([]TestRecord, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	dec := json.NewDecoder(file)
+	var records []TestRecord
+	err = dec.Decode(&records)
+	return records, err
+}
+
+func write_png_to_file(path string, img image.Image) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	err = png.Encode(file, img)
+	file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func to_nrgba(img image.Image) image.NRGBA {
+	switch v := img.(type) {
+	case *image.NRGBA:
+		return *v
+	}
+	nimg := *image.NewNRGBA(img.Bounds())
+	draw.Draw(&nimg, img.Bounds(), img, image.Point{0, 0}, draw.Src)
+	return nimg
+}
+
+func copy_nrgba(src image.NRGBA) image.NRGBA {
+	dst := image.NRGBA{make([]uint8, len(src.Pix)), src.Stride, src.Rect}
+	copy(dst.Pix, src.Pix)
+	return dst
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Printf("Usage:\n  %s INPUT.json OUTPUT_DIRECTORY\n\n", os.Args[0])
+		os.Exit(1)
+	}
+	input := os.Args[1]
+	output := os.Args[2]
+	err := os.Mkdir(output, os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+
+	records, err := readMetaJsonFile(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, record := range records {
+		var goodUrls []string
+		for _, digest := range record.Digests {
+			if (in("vk", digest.Params.Config) ||
+				in("gles", digest.Params.Config)) &&
+				digest.Status == "positive" {
+				goodUrls = append(goodUrls, digest.URL)
+			}
+		}
+		wg.Add(1)
+		go func(testName string, imgUrls []string, output string) {
+			defer wg.Done()
+			err := process_test(testName, imgUrls, output)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("\r%-60s", testName)
+		}(record.TestName, goodUrls, output)
+	}
+	wg.Wait()
+	fmt.Printf("\r%60s\n", "" )
+}
