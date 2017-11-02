cd1aca9fa93f82ef64bc71f23a2b84a12126c7d4
Yuqian Li <liyuqian@google.com>
2017-11-02 17:33:21 -0400

    Simplify fill path call by removing do_fill_path
    The git diff is not very informative for this CL.
    Here's a better diff:
    
    1. do_fill_path is removed and its content is copied to AntiFillPath
    
    2. Any call to do_fill_path is removed.
    
    3. std::function FillPathFunc is removed (and replaced by direct
       AAAFillPath, DAAFillPath, and SAAFillPath call).
    
    4. The old call chain is:
         AntiFillPath -> (AAAFillPath/DAAFillPath/...)
                      -> do_fill_path
                      -> specific FillPathFunc
       The new call chain is:
         AntiFillPath -> AAAFillPath/DAAFillPath/SAAFillPath
    
    This is made possible by the removal of SK_SUPPORT_LEGACY_AA_CHOICE
    which makes sure that AntiFillPath is the only function that makes
    the choice of AAA/DAA/SAA.
    
    In the next CL, I'll improve the structure of SkScan::AntiFillPath
    to prepare for Threaded Backend's init-once change.
    
    Bug: skia:
    Change-Id: If6ebbdab207cadb7bfe2cb3fcf33ea3d180c3896

--- src/core/SkScan.h
+++ src/core/SkScan.h
@@ -89,10 +89,15 @@ private:
                               const SkRegion*, SkBlitter*);
     static void HairLineRgn(const SkPoint[], int count, const SkRegion*, SkBlitter*);
     static void AntiHairLineRgn(const SkPoint[], int count, const SkRegion*, SkBlitter*);
-    static void AAAFillPath(const SkPath& path, const SkRegion& origClip, SkBlitter* blitter,
-                            bool forceRLE = false); // SkAAClip uses forceRLE
-    static void DAAFillPath(const SkPath& path, const SkRegion& origClip, SkBlitter* blitter,
-                            bool forceRLE = false);
+    static void AAAFillPath(const SkPath& path, SkBlitter* blitter, bool isInverse,
+                            const SkIRect& ir, const SkIRect& clipBounds, bool containedInClip,
+                            bool forceRLE);
+    static void DAAFillPath(const SkPath& path, SkBlitter* blitter, bool isInverse,
+                            const SkIRect& ir, const SkIRect& clipBounds, bool containedInClip,
+                            bool forceRLE);
+    static void SAAFillPath(const SkPath& path, SkBlitter* blitter, bool isInverse,
+                            const SkIRect& ir, const SkIRect& clipBounds, bool containedInClip,
+                            bool forceRLE);
 };
 
 /** Assign an SkXRect from a SkIRect, by promoting the src rect's coordinates
