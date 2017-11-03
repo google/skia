53ec7dc7cb523f220a9f5cd713b241c706779c81
Herb Derby <herb@google.com>
2017-11-03 13:36:55 -0400

    Gauss filter calculation
    Change-Id: I921ef815d4f788c312aa729f353b6ea154140555
    Reviewed-on: https://skia-review.googlesource.com/67723
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

---
+++ src/core/SkGaussFilter.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkGaussFilter_DEFINED
+#define SkGaussFilter_DEFINED
+
+#include <cstdint>
+
+// Define gaussian filters for values of sigma < 2. Produce values good to 1 part in 1,000,000.
+// Gaussian produces values as defined in the SVG 1.1 spec:
+// https://www.w3.org/TR/SVG/filters.html#feGaussianBlurElement
+// Bessel produces values as defined in "Scale-Space for Discrete Signals" by Tony Lindeberg
+class SkGaussFilter {
+public:
+    enum class Type : bool {
+        Gaussian,
+        Bessel
+    };
+
+    // Type selects which method is used to calculate the gaussian factors.
+    SkGaussFilter(double sigma, Type type);
+
+    int radius() const { return fN - 1; }
+    int width() const { return 2 * this->radius() + 1; }
+
+    // Take an array of values where the gaussian factors will be placed. Return the number of
+    // values filled.
+    int filterDouble(double values[5]) const;
+
+private:
+    double fBasis[5];
+    int    fN;
+};
+
+#endif  // SkGaussFilter_DEFINED
+
+
