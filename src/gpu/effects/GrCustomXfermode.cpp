f676600ff46cb5012609f60327b21d5609ddc2df
Mike Klein <mtklein@chromium.org>
2017-11-03 08:36:52 -0400

    turn on extended Clang warnings on Windows too
    Plus some small rearrangements of the various warning lists.
    
    Change-Id: Ied58f940341d69ddab971a529fd01b1e96b65641

--- src/gpu/effects/GrCustomXfermode.cpp
+++ src/gpu/effects/GrCustomXfermode.cpp
@@ -200,9 +200,13 @@ void CustomXP::onGetBlendInfo(BlendInfo* blendInfo) const {
 ///////////////////////////////////////////////////////////////////////////////
 
 // See the comment above GrXPFactory's definition about this warning suppression.
-#if defined(__GNUC__) || defined(__clang)
+#if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#endif
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnon-virtual-dtor"
 #endif
 class CustomXPFactory : public GrXPFactory {
 public:
@@ -228,8 +232,11 @@ private:
 
     typedef GrXPFactory INHERITED;
 };
-#if defined(__GNUC__) || defined(__clang)
+#if defined(__GNUC__)
 #pragma GCC diagnostic pop
+#endif
+#if defined(__clang__)
+#pragma clang diagnostic pop
 #endif
 
 sk_sp<const GrXferProcessor> CustomXPFactory::makeXferProcessor(
