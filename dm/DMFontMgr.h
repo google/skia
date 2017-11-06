07bf07823452d42096aa3cef2a576d8ffea99a6c
Mike Klein <mtklein@chromium.org>
2017-11-06 12:02:47 -0500

    add a SkFontMgr to DM that returns portable fonts
    Controlled by --[no]nativeFonts, and still defaults to native fonts.
    
    Change-Id: Ib2879e69fadb63ddb5a17a7e4ae227941893b8cf

---
+++ dm/DMFontMgr.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef DMFontMgr_DEFINED
+#define DMFontMgr_DEFINED
+
+#include "SkFontMgr.h"
+
+// An SkFontMgr that always uses sk_tool_utils::create_portable_typeface().
+
+namespace DM {
+
+    // Returned by DM::FontMgr below.
+    class FontStyleSet final : public SkFontStyleSet {
+    public:
+        explicit FontStyleSet(int familyIndex);
+
+        int count() override;
+        void getStyle(int index, SkFontStyle* style, SkString* name) override;
+        SkTypeface* createTypeface(int index) override;
+        SkTypeface* matchStyle(const SkFontStyle& pattern) override;
+
+    private:
+        const char* fFamilyName;
+    };
+
+    struct FontMgr final : public SkFontMgr {
+
+        int onCountFamilies() const override;
+        void onGetFamilyName(int index, SkString* familyName) const override;
+
+        SkFontStyleSet* onCreateStyleSet(int index) const override;
+        SkFontStyleSet* onMatchFamily(const char familyName[]) const override;
+
+        SkTypeface* onMatchFamilyStyle(const char familyName[],
+                                       const SkFontStyle&) const override;
+        SkTypeface* onMatchFamilyStyleCharacter(const char familyName[],
+                                                const SkFontStyle&,
+                                                const char* bcp47[], int bcp47Count,
+                                                SkUnichar character) const override;
+        SkTypeface* onMatchFaceStyle(const SkTypeface*,
+                                     const SkFontStyle&) const override;
+
+        sk_sp<SkTypeface> onMakeFromData(sk_sp<SkData>, int ttcIndex) const override;
+        sk_sp<SkTypeface> onMakeFromStreamIndex(std::unique_ptr<SkStreamAsset>,
+                                                int ttcIndex) const override;
+        sk_sp<SkTypeface> onMakeFromStreamArgs(std::unique_ptr<SkStreamAsset>,
+                                               const SkFontArguments&) const override;
+        sk_sp<SkTypeface> onMakeFromFontData(std::unique_ptr<SkFontData>) const override;
+        sk_sp<SkTypeface> onMakeFromFile(const char path[], int ttcIndex) const override;
+
+        sk_sp<SkTypeface> onLegacyMakeTypeface(const char familyName[], SkFontStyle) const override;
+    };
+
+}  // namespace DM
+
+#endif//DMFontMgr_DEFINED
