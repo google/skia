eb8b090562c9c227d7e927c7de81b65aaf38cf37
Cary Clark <caryclark@google.com>
2017-11-07 12:01:50 -0500

    first rough cut at isolating pathops
    try at building teeny version of pathops
    with minimal number of other dependences
    
    tested on windows only so far
    
    R=behdad@google.com
    Bug: skia:
    Change-Id: Ice4cbd7f1562bd2e33894879fd9d2ddff691c8e6

---
+++ tools/teenypathops/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkPathOps.h"
+#include "SkPath.h"
+
+int main(int argc, char** argv) {
+    SkPath test;
+    Simplify(test, &test);
+}
