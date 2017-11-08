8aa7fbf888804890450f58ea935f9fae4c643427
Ben Wagner <benjaminwagner@google.com>
2017-11-08 14:35:56 -0500

    Guard VFPv3 ASM with an ifdef.
    Change-Id: Id31de8dc0d9d68369896f2686068b29e07135a39

--- include/private/SkFixed.h
+++ include/private/SkFixed.h
@@ -94,8 +94,10 @@ inline SkFixed SkFixedMul_longlong(SkFixed a, SkFixed b) {
 }
 #define SkFixedMul(a,b)     SkFixedMul_longlong(a,b)
 
-
-#if defined(SK_CPU_ARM32)
+// The VCVT float-to-fixed instruction is part of the VFPv3 instruction set. Clang has
+// __ARM_VFPV3__, but GCC does not seem to have an equivalent define. Generally armv7 has VFPv3.
+#if (defined(__clang__) && defined(__ARM_VFPV3__)) || (!defined(__clang__) && SK_ARM_ARCH >= 7)
+static_assert(false, "Using the VFPv3 impl.");
     /* This guy does not handle NaN or other obscurities, but is faster than
        than (int)(x*65536).  When built on Android with -Os, needs forcing
        to inline or we lose the speed benefit.
@@ -107,6 +109,11 @@ inline SkFixed SkFixedMul_longlong(SkFixed a, SkFixed b) {
         memcpy(&y, &x, sizeof(y));
         return y;
     }
+    #undef SkFloatToFixed
+    #define SkFloatToFixed(x)  SkFloatToFixed_arm(x)
+#endif
+
+#if defined(SK_CPU_ARM32)
     inline SkFixed SkFixedMul_arm(SkFixed x, SkFixed y)
     {
         int32_t t;
@@ -121,9 +128,6 @@ inline SkFixed SkFixedMul_longlong(SkFixed a, SkFixed b) {
     }
     #undef SkFixedMul
     #define SkFixedMul(x, y)        SkFixedMul_arm(x, y)
-
-    #undef SkFloatToFixed
-    #define SkFloatToFixed(x)  SkFloatToFixed_arm(x)
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
