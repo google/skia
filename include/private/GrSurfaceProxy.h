8d5ce2d9ede0c241b906f1a0df9dac3cf3c3aa55
Robert Phillips <robertphillips@google.com>
2017-11-10 15:28:13 -0500

    Patch up ref counting of proxies
    Bug: skia:
    Change-Id: If746283d788368bf7aad6d285f181d8531768e61
    Reviewed-on: https://skia-review.googlesource.com/70024
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- include/private/GrSurfaceProxy.h
+++ include/private/GrSurfaceProxy.h
@@ -131,7 +131,7 @@ protected:
         SkASSERT(fTarget);
 
         SkASSERT(fTarget->fRefCnt > 0);
-        fTarget->fRefCnt += (fRefCnt-1); // don't xfer the proxy's creation ref
+        fTarget->fRefCnt += SkTMax(0, fRefCnt-1); // don't xfer the proxy's creation ref
         fTarget->fPendingReads += fPendingReads;
         fTarget->fPendingWrites += fPendingWrites;
     }
