16472d3e61af1352f8157ccbf7ae9d67783516f5
Ethan Nicholas <ethannicholas@google.com>
2017-11-10 14:07:35 -0500

    sksl enum support
    Bug: skia:
    Change-Id: I4d505b31cf8b59de12bcdbca410aafc085977ba9

--- src/gpu/effects/GrBlurredEdgeFragmentProcessor.cpp
+++ src/gpu/effects/GrBlurredEdgeFragmentProcessor.cpp
@@ -31,7 +31,8 @@ public:
                 "0.017999999999999999);\n        break;\n    case 1:\n        factor = "
                 "half(smoothstep(1.0, 0.0, float(factor)));\n        break;\n}\n%s = "
                 "half4(factor);\n",
-                args.fInputColor ? args.fInputColor : "half4(1)", _outer.mode(), args.fOutputColor);
+                args.fInputColor ? args.fInputColor : "half4(1)", (int)_outer.mode(),
+                args.fOutputColor);
     }
 
 private:
@@ -43,7 +44,7 @@ GrGLSLFragmentProcessor* GrBlurredEdgeFragmentProcessor::onCreateGLSLInstance() 
 }
 void GrBlurredEdgeFragmentProcessor::onGetGLSLProcessorKey(const GrShaderCaps& caps,
                                                            GrProcessorKeyBuilder* b) const {
-    b->add32(fMode);
+    b->add32((int32_t)fMode);
 }
 bool GrBlurredEdgeFragmentProcessor::onIsEqual(const GrFragmentProcessor& other) const {
     const GrBlurredEdgeFragmentProcessor& that = other.cast<GrBlurredEdgeFragmentProcessor>();
