32914f6b5921e187b5734790bb87598d9689955e
Ben Wagner <benjaminwagner@google.com>
2017-11-13 14:46:07 -0500

    Switch Win builders from Win2k8 to Win2016.
    Bug: skia:7296
    Change-Id: Id30df3ed2264679338913f275caa69b8e2278075

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -190,7 +190,7 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 			"Ubuntu14":   DEFAULT_OS_UBUNTU,
 			"Ubuntu16":   "Ubuntu-16.10",
 			"Ubuntu17":   "Ubuntu-17.04",
-			"Win":        "Windows-2008ServerR2-SP1",
+			"Win":        "Windows-10.0-14393",
 			"Win10":      "Windows-10-15063",
 			"Win2k8":     "Windows-2008ServerR2-SP1",
 			"Win2016":    "Windows-10.0-14393",
