db8642e6a4b0347b341d9497acef822d1256d364
Chris Dalton <csmartdalton@google.com>
2017-11-14 10:47:46 -0700

    Fix precision caps and rrect/ellipse effect precisions
    Replaces all the complex precision caps with a single flag that says
    whether "float" == fp32. Updates the ellipse and rrect effects to
    use float coords, and use the scale workaround when float != fp32.
    
    Bug: skia:7190
    Change-Id: Ieccff9f38acd05e5cec78fe90d01a5da901a9307

--- src/sksl/SkSLCPP.h
+++ src/sksl/SkSLCPP.h
@@ -18,7 +18,7 @@ using std::abs;
 // macros to make sk_Caps.<cap name> work from C++ code
 #define sk_Caps (*args.fShaderCaps)
 
-#define floatPrecisionVaries floatPrecisionVaries()
+#define floatIs32Bits floatIs32Bits()
 
 // functions to make GLSL constructors work from C++ code
 inline SkPoint float2(float xy) { return SkPoint::Make(xy, xy); }
