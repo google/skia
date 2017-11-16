e42180022720f2fcfd3c634cad855506a7940591
Chris Dalton <csmartdalton@google.com>
2017-11-15 18:27:09 -0700

    Fix precision caps and rrect/ellipse effect precisions
    Replaces all the complex precision caps with a single flag that says
    whether "float" == fp32. Updates the ellipse and rrect effects to
    use float coords, and use the scale workaround when float != fp32.
    
    Bug: skia:7190
    Change-Id: Ieccff9f38acd05e5cec78fe90d01a5da901a9307
    Reviewed-on: https://skia-review.googlesource.com/70961
    Commit-Queue: Chris Dalton <csmartdalton@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

--- src/gpu/gl/GrGLCaps.h
+++ src/gpu/gl/GrGLCaps.h
@@ -428,7 +428,7 @@ private:
                            GrGLenum* externalType) const;
 
     void init(const GrContextOptions&, const GrGLContextInfo&, const GrGLInterface*);
-    void initGLSL(const GrGLContextInfo&);
+    void initGLSL(const GrGLContextInfo&, const GrGLInterface*);
     bool hasPathRenderingSupport(const GrGLContextInfo&, const GrGLInterface*);
 
     void onApplyOptionsOverrides(const GrContextOptions& options) override;
@@ -440,8 +440,6 @@ private:
     // This must be called after initFSAASupport().
     void initConfigTable(const GrContextOptions&, const GrGLContextInfo&, const GrGLInterface*,
                          GrShaderCaps*);
-
-    void initShaderPrecisionTable(const GrGLContextInfo&, const GrGLInterface*, GrShaderCaps*);
 
     GrGLStandard fStandard;
 
--- tests/ApplyGammaTest.cpp
+++ tests/ApplyGammaTest.cpp
@@ -105,7 +105,7 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(ApplyGamma, reporter, ctxInfo) {
     SkAutoTMalloc<uint32_t> read(kW * kH);
 
     // We allow more error on GPUs with lower precision shader variables.
-    float error = context->caps()->shaderCaps()->floatPrecisionVaries() ? 1.2f : 0.5f;
+    float error = context->caps()->shaderCaps()->halfIs32Bits() ? 0.5f : 1.2f;
 
     for (auto toSRGB : { false, true }) {
         sk_sp<SkSurface> dst(SkSurface::MakeRenderTarget(context, SkBudgeted::kNo, ii));
