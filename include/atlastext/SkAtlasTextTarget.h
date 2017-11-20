a0ba714ad5ee26b3f1929aa572eb77cc71809e64
Brian Salomon <bsalomon@google.com>
2017-11-20 13:17:43 -0500

    Require glyph positions in SkAtlasTextTarget::drawText
    Change-Id: Idf0977befc8ed4fd9eb3b733db5e945457b2164c
    Reviewed-on: https://skia-review.googlesource.com/73701
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- include/atlastext/SkAtlasTextTarget.h
+++ include/atlastext/SkAtlasTextTarget.h
@@ -14,6 +14,7 @@
 
 class SkAtlasTextContext;
 class SkAtlasTextFont;
+struct SkPoint;
 
 /** Represents a client-created renderable surface and is used to draw text into the surface. */
 class SK_API SkAtlasTextTarget {
@@ -28,11 +29,12 @@ public:
                                                    void* handle);
 
     /**
-     * Enqueues a text draw in the target. The meaning of 'color' here is interpreted by the
-     * client's SkAtlasTextRenderer when it actually renders the text.
+     * Enqueues a text draw in the target. The caller provides an array of glyphs and their
+     * positions. The meaning of 'color' here is interpreted by the client's SkAtlasTextRenderer
+     * when it actually renders the text.
      */
-    virtual void drawText(const SkGlyphID*, int glyphCnt, SkScalar x, SkScalar y, uint32_t color,
-                          const SkAtlasTextFont& font) = 0;
+    virtual void drawText(const SkGlyphID[], const SkPoint[], int glyphCnt, uint32_t color,
+                          const SkAtlasTextFont&) = 0;
 
     /** Issues all queued text draws to SkAtlasTextRenderer. */
     virtual void flush() = 0;
