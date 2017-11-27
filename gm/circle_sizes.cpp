2074567a4fbfd53aae9170037f6c8baa06608912
Hal Canary <halcanary@google.com>
2017-11-27 09:33:44 -0500

    SkPDF: less tolerance for small circles
    BUG=chromium:772953
    Change-Id: I9452ac8ad66ab8be26a9388f8c85a1f0e61bd392

---
+++ gm/circle_sizes.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "gm.h"
+// https://crbug.com/772953
+DEF_SIMPLE_GM(circle_sizes, canvas, 128, 128) {
+    SkPaint p;
+    p.setAntiAlias(true);
+    for (int i = 0; i < 16; ++i) {
+        canvas->drawCircle({14.0f + 32.0f * (i % 4),
+                            14.0f + 32.0f * (i / 4)}, i + 1.0f, p);
+    }
+}
