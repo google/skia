dfaa057c15180a18f65d86317618a97d683e99c4
Hal Canary <halcanary@google.com>
2017-11-27 09:33:44 -0500

    SkPDF: less tolerance for small circles
    BUG=chromium:772953
    Change-Id: I9452ac8ad66ab8be26a9388f8c85a1f0e61bd392
    Reviewed-on: https://skia-review.googlesource.com/75960
    Reviewed-by: Cary Clark <caryclark@google.com>
    Commit-Queue: Hal Canary <halcanary@google.com>

--- src/pdf/SkPDFDevice.cpp
+++ src/pdf/SkPDFDevice.cpp
@@ -952,8 +952,9 @@ void SkPDFDevice::internalDrawPath(const SkClipStack& clipStack,
     if (!content.entry()) {
         return;
     }
+    constexpr SkScalar kToleranceScale = 0.0625f;  // smaller = better conics (circles).
     SkScalar matrixScale = matrix.mapRadius(1.0f);
-    SkScalar tolerance = matrixScale > 0.0f ? 0.25f / matrixScale : 0.25f;
+    SkScalar tolerance = matrixScale > 0.0f ? kToleranceScale / matrixScale : kToleranceScale;
     bool consumeDegeratePathSegments =
            paint.getStyle() == SkPaint::kFill_Style ||
            (paint.getStrokeCap() != SkPaint::kRound_Cap &&
