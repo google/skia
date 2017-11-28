54da93ebb37f79be420e2ace6cd3da887697393e
Jim Van Verth <jvanverth@google.com>
2017-11-28 15:39:12 -0500

    Bundle resources and skps directories into iOS app.
    Will load resources/ for dm, nanobench and viewer, and skps/ if
    that directory exists in the main Skia directory.
    Also updates file code on iOS to fall back to bundle directory.
    
    Bug: skia:7339
    Change-Id: I244f67559c866451a6d02c3f1c4948d89457ec84

---
+++ gn/checkdir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+dirpath, = sys.argv[1:]
+
+print os.path.isdir(dirpath)
+
+
