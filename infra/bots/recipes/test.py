cb419cd0469fba2e19d258a48b870800ab68342a
Greg Daniel <egdaniel@google.com>
2017-11-28 11:20:40 -0500

    testing processor validation two-point
    Bug: skia:
    Change-Id: I85bf82e0e02607b78470b7a5f8f918e9f53f0154

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -704,6 +704,9 @@ def dm_flags(api, bot):
     args.append('--blacklist')
     args.extend(blacklisted)
 
+  match = []
+  mattach.append("ProcessorOptimizationValidationTest")
+
   if match:
     args.append('--match')
     args.extend(match)
