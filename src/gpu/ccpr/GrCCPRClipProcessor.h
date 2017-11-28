997843565d764f8acbfc2923d3c167e863c9b052
Chris Dalton <csmartdalton@google.com>
2017-11-28 15:59:25 -0700

    Add analytic clip FPs that read from the CCPR atlas
    Bug: skia:7190
    Change-Id: Ie31d368f52910e6917efdeb1b024370b06fc11ee

---
+++ src/gpu/ccpr/GrCCPRClipProcessor.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrCCPRClipProcessor_DEFINED
+#define GrCCPRClipProcessor_DEFINED
+
+#include "GrFragmentProcessor.h"
+#include "ccpr/GrCoverageCountingPathRenderer.h"
+
+class GrCCPRClipProcessor : public GrFragmentProcessor {
+public:
+    using ClipPath = GrCoverageCountingPathRenderer::ClipPath;
+
+    GrCCPRClipProcessor(const ClipPath*, bool mustCheckBounds, SkPath::FillType overrideFillType);
+
+    const char* name() const override { return "GrCCPRClipProcessor"; }
+    std::unique_ptr<GrFragmentProcessor> clone() const override;
+    void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
+    bool onIsEqual(const GrFragmentProcessor&) const override;
+    GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
+
+private:
+    const ClipPath* const fClipPath;
+    const bool fMustCheckBounds;
+    const SkPath::FillType fOverrideFillType;
+    const TextureSampler fAtlasAccess;
+
+    class Impl;
+
+    typedef GrFragmentProcessor INHERITED;
+};
+
+#endif
