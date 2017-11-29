6985b77c09c868433b1fa39932738d2b84cd123b
Chris Dalton <csmartdalton@google.com>
2017-11-29 14:38:24 -0700

    Make sure to visit clips and dst proxies during gather
    Bug: skia:7190
    Change-Id: I0cd4f7734047550c7904f44892ef266498842e0c

--- src/gpu/GrResourceAllocator.cpp
+++ src/gpu/GrResourceAllocator.cpp
@@ -50,7 +50,14 @@ void GrResourceAllocator::addInterval(GrSurfaceProxy* proxy,
 
     if (Interval* intvl = fIntvlHash.find(proxy->uniqueID().asUInt())) {
         // Revise the interval for an existing use
-        SkASSERT(intvl->end() <= start && intvl->end() <= end);
+#ifdef SK_DEBUG
+        if (proxy->priv().isDirectDstRead_debugOnly()) {
+            // Direct reads from the render target itself should occur w/in the existing interval
+            SkASSERT(intvl->start() <= start && intvl->end() >= end);
+        } else {
+            SkASSERT(intvl->end() <= start && intvl->end() <= end);
+        }
+#endif
         intvl->extendEnd(end);
         return;
     }
