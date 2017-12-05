41357cca2552063b3ff2945270aa8a9f77b4568c
Kevin Lubick <kjlubick@google.com>
2017-12-05 15:54:19 -0500

    Don't set min frequency for Android devices
    Bug: skia:
    Change-Id: Iaacbc8eadaa4da9832a53d4794a2214625bef9b6

--- infra/bots/recipe_modules/flavor/gn_android_flavor.py
+++ infra/bots/recipe_modules/flavor/gn_android_flavor.py
@@ -128,8 +128,8 @@ subprocess.check_output([ADB, 'shell', 'echo "userspace" > '
 # If scaling_max_freq is lower than our attempted setting, it won't take.
 subprocess.check_output([ADB, 'shell', 'echo %d > '
     '/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq' % freq])
-subprocess.check_output([ADB, 'shell', 'echo %d > '
-    '/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' % freq])
+subprocess.check_output([ADB, 'shell', 'echo 0 > '
+    '/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq'])
 subprocess.check_output([ADB, 'shell', 'echo %d > '
     '/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed' % freq])
 time.sleep(5)
