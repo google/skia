54013320bd3b6c3803f7ca13168b4b32a72fe4f2
Greg Daniel <egdaniel@google.com>
2017-12-06 15:29:51 -0500

    Add define to use customize vulkan header.
    Bug: skia:
    Change-Id: Ia87c81a54603a02b2f8f51a735bf173a49afe6c7

--- include/gpu/vk/GrVkDefines.h
+++ include/gpu/vk/GrVkDefines.h
@@ -11,33 +11,40 @@
 
 #ifdef SK_VULKAN
 
-#if defined(SK_BUILD_FOR_WIN) || defined(SK_BUILD_FOR_WIN32)
-#   if !defined(VK_USE_PLATFORM_WIN32_KHR)
-#      define VK_USE_PLATFORM_WIN32_KHR
-#   endif
-#elif defined(SK_BUILD_FOR_ANDROID)
-#   if !defined(VK_USE_PLATFORM_ANDROID_KHR)
-#      define VK_USE_PLATFORM_ANDROID_KHR
-#   endif
-#elif defined(SK_BUILD_FOR_UNIX)
-#   if defined(__Fuchsia__)
-#     if !defined(VK_USE_PLATFORM_MAGMA_KHR)
-#       define VK_USE_PLATFORM_MAGMA_KHR
+#ifdef SK_VULKAN_HEADER
+#include SK_VULKAN_HEADER
+#else
+// This is deprecated and all clients should define their own custum header shim that sets up
+// defines and includes the vulkan.h header. Then they should define SK_VULKAN_HEADER or set the
+// skia_vulkan_header in gn to point to their custom header.
+#  if defined(SK_BUILD_FOR_WIN) || defined(SK_BUILD_FOR_WIN32)
+#     if !defined(VK_USE_PLATFORM_WIN32_KHR)
+#        define VK_USE_PLATFORM_WIN32_KHR
 #     endif
-#   else
-#     if !defined(VK_USE_PLATFORM_XCB_KHR)
-#        define VK_USE_PLATFORM_XCB_KHR
+#  elif defined(SK_BUILD_FOR_ANDROID)
+#     if !defined(VK_USE_PLATFORM_ANDROID_KHR)
+#        define VK_USE_PLATFORM_ANDROID_KHR
 #     endif
-#   endif
-#endif
+#  elif defined(SK_BUILD_FOR_UNIX)
+#     if defined(__Fuchsia__)
+#       if !defined(VK_USE_PLATFORM_MAGMA_KHR)
+#         define VK_USE_PLATFORM_MAGMA_KHR
+#       endif
+#     else
+#       if !defined(VK_USE_PLATFORM_XCB_KHR)
+#          define VK_USE_PLATFORM_XCB_KHR
+#       endif
+#     endif
+#  endif
 
 // We create our own function table and never directly call any functions via vk*(). So no need to
 // include the prototype functions.
-#if !defined(VK_NO_PROTOTYPES) && !defined(SK_BUILD_FOR_ANDROID_FRAMEWORK)
-#define VK_NO_PROTOTYPES
-#endif
+#  if !defined(VK_NO_PROTOTYPES) && !defined(SK_BUILD_FOR_ANDROID_FRAMEWORK)
+#    define VK_NO_PROTOTYPES
+#  endif
 
-#include <vulkan/vulkan.h>
+#  include <vulkan/vulkan.h>
+#endif // SK_VULKAN_HEADER
 
 #define SKIA_REQUIRED_VULKAN_HEADER_VERSION 17
 #if VK_HEADER_VERSION < SKIA_REQUIRED_VULKAN_HEADER_VERSION
