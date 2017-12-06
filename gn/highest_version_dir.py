e7137755c6af4e7cff8671e4eec662f71a58d4de
Brian Osman <brianosman@google.com>
2017-12-06 11:54:59 -0500

    Add win_toolchain_version to support MSVC 2017 Updates
    To make this simpler in the future, add a python script that finds the
    most recent 2017 toolchain, and use that. If/when we update the bot
    toolchain, this will be even more helpful, because the Chromium toolchain
    isn't perfectly in sync with the general update channel, so people are
    likely to have a newer/older release locally.
    
    Note that explicitly setting win_toolchain_version in your GN args
    suppresses the python script, so you can choose to build with whatever
    version you need.
    
    Bug: skia:
    Change-Id: Iaf6d0dd9be2623472118c3ad27b20023a205d67c

---
+++ gn/highest_version_dir.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+dirpath, = sys.argv[1:]
+
+print sorted(os.listdir(dirpath))[-1]
