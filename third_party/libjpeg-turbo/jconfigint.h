4f498fc6f07ffd57b747e5cd2f99a03c6281aa22
Leon Scroggins III <scroggo@google.com>
2017-12-06 16:21:01 -0500

    Update to the latest version of libjpeg-turbo
    Bug: b/70203010
    
    From https://github.com/libjpeg-turbo/libjpeg-turbo/commit/c308d434.
    This commit fixes a bug in BitmapRegionDecoder, and is the tip of tree.
    
    Rather than using our mirror, just pull in upstream directly. Move our
    config files into third_party/libjpeg-turbo, so we can just DEPS to
    upstream. These files are unchanged, except jconfig.h, where I added a
    comment regarding arithmetic coding.
    
    Add a test image which demonstrates the bug.
    
    Change-Id: I00f8f961f69e407dc31ca6d15c66518aa0acbafd
    Reviewed-on: https://skia-review.googlesource.com/81442
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Leon Scroggins <scroggo@google.com>

---
+++ third_party/libjpeg-turbo/jconfigint.h
@@ -0,0 +1,42 @@
+/* jconfigint.h.  Customized for android on the basis of jconfigint.h.in. */
+
+#ifndef __JCONFIGINT_H__
+#define __JCONFIGINT_H__
+
+
+#define BUILD ""
+
+/* How to obtain function inlining. */
+#ifndef INLINE
+  #ifndef TURBO_FOR_WINDOWS
+    #define INLINE inline __attribute__((always_inline))
+  #else
+    #if defined(__GNUC__)
+      #define INLINE inline __attribute__((always_inline))
+    #elif defined(_MSC_VER)
+      #define INLINE __forceinline
+    #else
+      #define INLINE
+    #endif
+  #endif
+#endif
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libjpeg-turbo"
+
+/* Version number of package */
+#define VERSION "1.5.1"
+
+/* The size of `size_t', as reported by the compiler through the
+ * builtin macro __SIZEOF_SIZE_T__. If the compiler does not
+ * report __SIZEOF_SIZE_T__ add a custom rule for the compiler
+ * here. */
+#ifdef __SIZEOF_SIZE_T__
+#define SIZEOF_SIZE_T __SIZEOF_SIZE_T__
+#elif __WORDSIZE==64 || defined(_WIN64)
+#define SIZEOF_SIZE_T 8
+#else
+#define SIZEOF_SIZE_T 4
+#endif
+
+#endif // __JCONFIGINT_H__
