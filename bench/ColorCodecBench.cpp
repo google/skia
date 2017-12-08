10026d27618fd60e038df5856e323119b9b11b73
Hal Canary <halcanary@google.com>
2017-12-08 11:38:47 -0500

    resources: remove most uses of GetResourcePath()
    Going forward, we will standardize on GetResourceAsData(), which will
    make it easier to run tests in environments without access to the
    filesystem.
    
    Change-Id: Iaf70b71b0ca5ed8cd1a5538a60ef185ae8736188

--- bench/ColorCodecBench.cpp
+++ bench/ColorCodecBench.cpp
@@ -74,8 +74,7 @@ void ColorCodecBench::onDelayedSetup() {
         matrix.set3x3(0.30f, 0.31f, 0.28f, 0.32f, 0.33f, 0.29f, 0.27f, 0.30f, 0.30f);
         fDstSpace = SkColorSpace::MakeRGB(gamma, matrix);
     } else {
-        sk_sp<SkData> dstData = SkData::MakeFromFileName(
-                GetResourcePath("icc_profiles/HP_ZR30w.icc").c_str());
+        sk_sp<SkData> dstData = GetResourceAsData("icc_profiles/HP_ZR30w.icc");
         SkASSERT(dstData);
         fDstSpace = SkColorSpace::MakeICC(dstData->data(), dstData->size());
     }
