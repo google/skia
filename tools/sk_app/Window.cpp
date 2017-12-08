c3c5901d867bd451f5b12e1a6a709df1d668bf8e
Brian Osman <brianosman@google.com>
2017-12-08 15:24:24 -0500

    Create a Window::Layer interface to reduce sk_app glue code
    This also makes it possible to manage other parts of viewer, etc (like
    the stats screen, command set, even samples) as additional layers in the
    stack. For now, it just removes a lot of boilerplate.
    
    Bug: skia:
    Change-Id: Ic2f80690fc76c683b3736287dc2b738c50d38614

--- tools/sk_app/Window.cpp
+++ tools/sk_app/Window.cpp
@@ -13,45 +13,7 @@
 
 namespace sk_app {
 
-static void default_backend_created_func(void* userData) {}
-
-static bool default_char_func(SkUnichar c, uint32_t modifiers, void* userData) {
-    return false;
-}
-
-static bool default_key_func(Window::Key key, Window::InputState state, uint32_t modifiers,
-                             void* userData) {
-    return false;
-}
-
-static bool default_mouse_func(int x, int y, Window::InputState state, uint32_t modifiers,
-                               void* userData) {
-    return false;
-}
-
-static bool default_mouse_wheel_func(float delta, uint32_t modifiers, void* userData) {
-    return false;
-}
-
-static bool default_touch_func(intptr_t owner, Window::InputState state, float x, float y,
-                               void* userData) {
-    return false;
-}
-
-static void default_ui_state_changed_func(
-        const SkString& stateName, const SkString& stateValue, void* userData) {}
-
-static void default_paint_func(SkCanvas*, void* userData) {}
-
-Window::Window() : fBackendCreatedFunc(default_backend_created_func)
-                 , fCharFunc(default_char_func)
-                 , fKeyFunc(default_key_func)
-                 , fMouseFunc(default_mouse_func)
-                 , fMouseWheelFunc(default_mouse_wheel_func)
-                 , fTouchFunc(default_touch_func)
-                 , fUIStateChangedFunc(default_ui_state_changed_func)
-                 , fPaintFunc(default_paint_func) {
-}
+Window::Window() {}
 
 void Window::detach() {
     delete fWindowContext;
@@ -59,31 +21,60 @@ void Window::detach() {
 }
 
 void Window::onBackendCreated() {
-    fBackendCreatedFunc(fBackendCreatedUserData);
+    for (int i = 0; i < fLayers.count(); ++i) {
+        fLayers[i]->onBackendCreated();
+    }
 }
 
 bool Window::onChar(SkUnichar c, uint32_t modifiers) {
-    return fCharFunc(c, modifiers, fCharUserData);
+    for (int i = fLayers.count() - 1; i >= 0; --i) {
+        if (fLayers[i]->onChar(c, modifiers)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Window::onKey(Key key, InputState state, uint32_t modifiers) {
-    return fKeyFunc(key, state, modifiers, fKeyUserData);
+    for (int i = fLayers.count() - 1; i >= 0; --i) {
+        if (fLayers[i]->onKey(key, state, modifiers)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Window::onMouse(int x, int y, InputState state, uint32_t modifiers) {
-    return fMouseFunc(x, y, state, modifiers, fMouseUserData);
+    for (int i = fLayers.count() - 1; i >= 0; --i) {
+        if (fLayers[i]->onMouse(x, y, state, modifiers)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Window::onMouseWheel(float delta, uint32_t modifiers) {
-    return fMouseWheelFunc(delta, modifiers, fMouseWheelUserData);
+    for (int i = fLayers.count() - 1; i >= 0; --i) {
+        if (fLayers[i]->onMouseWheel(delta, modifiers)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Window::onTouch(intptr_t owner, InputState state, float x, float y) {
-    return fTouchFunc(owner, state, x, y, fTouchUserData);
+    for (int i = fLayers.count() - 1; i >= 0; --i) {
+        if (fLayers[i]->onTouch(owner, state, x, y)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 void Window::onUIStateChanged(const SkString& stateName, const SkString& stateValue) {
-    return fUIStateChangedFunc(stateName, stateValue, fUIStateChangedUserData);
+    for (int i = 0; i < fLayers.count(); ++i) {
+        fLayers[i]->onUIStateChanged(stateName, stateValue);
+    }
 }
 
 void Window::onPaint() {
@@ -96,7 +87,9 @@ void Window::onPaint() {
         // draw into the canvas of this surface
         SkCanvas* canvas = backbuffer->getCanvas();
 
-        fPaintFunc(canvas, fPaintUserData);
+        for (int i = 0; i < fLayers.count(); ++i) {
+            fLayers[i]->onPaint(canvas);
+        }
 
         canvas->flush();
 
