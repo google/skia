25abd6bc1b68d562c932d79662214e14e2ce8042
Mike Klein <mtklein@chromium.org>
2017-12-12 17:07:49 -0500

    JUMPER_IS_AVX2 -> JUMPER_IS_HSW
    We need to be a bit more pedantic here to support builds that may be
    using AVX2 as part of their baseline but perhaps not enabling all the
    related features SkJumper would like to use.
    
    E.g. we've seen Tensorflow build with AVX2 and FMA, but not F16C.
    
    So check all three {AVX2,FMA,F16C}, and only then build stages in HSW
    mode.  I've updated the define as a reminder.
    
    This only affects builds using these features for their _baseline_
    stages... the offline-compiled stages in SkJumper_generated.S are
    not affected.
    
    Change-Id: I9bfb3bae3589d35043b748782cefa8c213726d6a

--- src/jumper/SkJumper_stages.cpp
+++ src/jumper/SkJumper_stages.cpp
@@ -624,7 +624,7 @@ STAGE(to_srgb, Ctx::None) {
         const float c = 1.130026340485f,
                     d = 0.141387879848f;
     #elif defined(JUMPER_IS_SSE2) || defined(JUMPER_IS_SSE41) || \
-          defined(JUMPER_IS_AVX ) || defined(JUMPER_IS_AVX2 )
+          defined(JUMPER_IS_AVX ) || defined(JUMPER_IS_HSW )
         const float c = 1.130048394203f,
                     d = 0.141357362270f;
     #elif defined(JUMPER_IS_NEON)
@@ -1162,7 +1162,7 @@ STAGE(matrix_perspective, const float* m) {
 SI void gradient_lookup(const SkJumper_GradientCtx* c, U32 idx, F t,
                         F* r, F* g, F* b, F* a) {
     F fr, br, fg, bg, fb, bb, fa, ba;
-#if defined(JUMPER_IS_AVX2) || defined(JUMPER_IS_AVX512)
+#if defined(JUMPER_IS_HSW) || defined(JUMPER_IS_AVX512)
     if (c->stopCount <=8) {
         fr = _mm256_permutevar8x32_ps(_mm256_loadu_ps(c->fs[0]), idx);
         br = _mm256_permutevar8x32_ps(_mm256_loadu_ps(c->bs[0]), idx);
