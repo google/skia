530f3715fe423650776daa3804c5404932e45ab7
Robert Phillips <robertphillips@google.com>
2017-12-14 12:37:12 -0500

    Add GrBackendTexture accessor to SkImage
    This will improve the type-safety of the backend objects.
    
    Change-Id: I12c381cbd179048f7eb4376ed82cd666d2b3e781

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -171,6 +171,11 @@ GrBackendObject SkImage::getTextureHandle(bool flushPendingGrContextIO,
     return as_IB(this)->onGetTextureHandle(flushPendingGrContextIO, origin);
 }
 
+GrBackendTexture SkImage::getBackendTexture(bool flushPendingGrContextIO,
+                                            GrSurfaceOrigin* origin) const {
+    return as_IB(this)->onGetBackendTexture(flushPendingGrContextIO, origin);
+}
+
 bool SkImage::isValid(GrContext* context) const {
     if (context && context->abandoned()) {
         return false;
@@ -185,6 +190,11 @@ GrTexture* SkImage::getTexture() const { return nullptr; }
 bool SkImage::isTextureBacked() const { return false; }
 
 GrBackendObject SkImage::getTextureHandle(bool, GrSurfaceOrigin*) const { return 0; }
+
+GrBackendTexture SkImage::getBackendTexture(bool flushPendingGrContextIO,
+                                            GrSurfaceOrigin* origin) const {
+    return GrBackendTexture(); // invalid
+}
 
 bool SkImage::isValid(GrContext* context) const {
     if (context) {
