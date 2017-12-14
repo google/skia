a50c9c7279fde89539c9dcedfc4d15f27c2d1277
Mike Reed <reed@google.com>
2017-12-14 16:27:52 -0500

    fix pdf_none
    Bug: skia:
    Change-Id: I36e317ce306c5757988d63e39883495b4a1b0308
    Reviewed-on: https://skia-review.googlesource.com/85440
    Reviewed-by: Mike Reed <reed@google.com>

--- src/pdf/SkDocument_PDF_None.cpp
+++ src/pdf/SkDocument_PDF_None.cpp
@@ -4,14 +4,29 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
 #include "SkDocument.h"
-sk_sp<SkDocument> SkDocument::MakePDF(SkWStream*,
-                                      SkScalar,
-                                      const SkDocument::PDFMetadata&,
-                                      sk_sp<SkPixelSerializer>,
-                                      bool) {
+
+sk_sp<SkDocument> SkDocument::MakePDF(SkWStream* stream, const PDFMetadata& metadata) {
     return nullptr;
 }
-sk_sp<SkDocument> SkDocument::MakePDF(const char path[], SkScalar) {
+
+sk_sp<SkDocument> SkDocument::MakePDF(SkWStream* stream) {
     return nullptr;
 }
+
+#ifdef SK_SUPPORT_LEGACY_PDF_PIXELSERIALIZER
+sk_sp<SkDocument> SkDocument::MakePDF(SkWStream* stream,
+                                      SkScalar dpi,
+                                      const PDFMetadata& metadata,
+                                      sk_sp<SkPixelSerializer> jpegEncoder,
+                                      bool pdfa) {
+    return nullptr;
+}
+sk_sp<SkDocument> SkDocument::MakePDF(SkWStream* stream, SkScalar dpi) {
+    return nullptr;
+}
+sk_sp<SkDocument> SkDocument::MakePDF(const char path[], SkScalar dpi) {
+    return nullptr;
+}
+#endif
