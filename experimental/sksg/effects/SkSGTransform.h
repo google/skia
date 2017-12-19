0b37c0c34ba73144ff7b82878f7cea8820c15f54
Florin Malita <fmalita@chromium.org>
2017-12-19 11:40:57 -0500

    Initial scene graph (SkSG)
    Sketching a thin (as in close-to-skia-semantics) scene graph API, focused on
    external animation, inval tracking and minimal repaint.
    
    Only a few concrete classes/feature so far:
    
    * Rect/Color/Transform/Group
    * basic inval tracking
    * a trivial animated sample with inval visualization
    
    Pretty much everything (especially naming) is volatile, so treat accordingly.
    
    The interesting bits to review are likely Node.{h,cpp} for inval and
    SampleSGInval.cpp for usage.
    
    Initial class hierarchy:
    
      * Node: invalidation/ancestors tracking
      |
       -- * RenderNode: onRender(SkCanvas)
      |   |
      |    -- * Draw (concrete): rendering a [geometry, paint] tuple
      |   |
      |    -- * Group (concrete): grouping multiple RenderNodes
      |   |
      |    -- * EffectNode: single-descendant effect wrapper
      |       |
      |        -- * Transform (concrete): transform effect
      |
       -- * PaintNode: onMakePaint()
      |   |
      |    -- * Color (concrete): SkColor paint wrapper
      |
       -- * GeometryNode: onComputeBounds(), onDraw(SkCanvas, SkPaint)
          |
           -- * Rect (concrete): SkRect wrapper
    
    
    Change-Id: Iacf9b773c181a7582ecd31ee968562f179d1aa1b

---
+++ experimental/sksg/effects/SkSGTransform.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkSGTransform_DEFINED
+#define SkSGTransform_DEFINED
+
+#include "SkSGEffectNode.h"
+
+#include "SkMatrix.h"
+
+namespace sksg {
+
+/**
+ * Concrete Effect node, wrapping an SkMatrix.
+ */
+class Transform : public EffectNode {
+public:
+    static sk_sp<Transform> Make(sk_sp<RenderNode> child, const SkMatrix& matrix) {
+        return sk_sp<Transform>(new Transform(std::move(child), matrix));
+    }
+
+    SG_ATTRIBUTE(Matrix, SkMatrix, fMatrix)
+
+protected:
+    Transform(sk_sp<RenderNode>, const SkMatrix&);
+
+    void onRender(SkCanvas*) const override;
+
+    void onRevalidate(InvalidationController*, const SkMatrix&) override;
+
+private:
+    SkMatrix fMatrix;
+
+    typedef EffectNode INHERITED;
+};
+
+} // namespace sksg
+
+#endif // SkSGTransform_DEFINED
