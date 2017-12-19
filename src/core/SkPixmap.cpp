bc9f3499c345ce59f8cd3889c580fa75db54034b
Mike Klein <mtklein@chromium.org>
2017-12-19 14:48:04 -0500

    remove bicubic clamp in SkImageShader
    We ought to be able to handle out-of-gamut colors now, whatever
    their provenance.  Should be we can just not clamp here.
    
    Bug: skia:5733
    
    Change-Id: Icd9851543638fd81755afee2eac2347702b88e9f
    Reviewed-on: https://skia-review.googlesource.com/87000
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkPixmap.cpp
+++ src/core/SkPixmap.cpp
@@ -251,10 +251,6 @@ bool SkPixmap::scalePixels(const SkPixmap& dst, SkFilterQuality quality) const {
     // Trick: if src and dst are both unpremul, we can give the correct result if we change both
     //        to premul (or opaque), since the draw will not try to blend or otherwise interpret
     //        the pixels' alpha.
-    //
-    // Today this works except in kHigh_SkFilterQuality, where we incorrectly clamp assuming the
-    // colors are premul. TODO: fix the HQ mode.
-    //
     if (srcPtr->alphaType() == kUnpremul_SkAlphaType &&
         dstPtr->alphaType() == kUnpremul_SkAlphaType)
     {
