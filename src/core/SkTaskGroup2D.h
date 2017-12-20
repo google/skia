73809c593cc29eef4a0c208f64d77dcc7f4d582c
Yuqian Li <liyuqian@google.com>
2017-12-20 13:16:56 -0500

    Add init-once to threaded backend
    For the simplicity of this CL, I haven't enabled DAA for init-once yet.
    The current init-once is only enabled for draw path, and it simply
    generates the dev path in the init-once phase.
    
    I also managed to remove the THREADED_DRAW macro and the SkTaskGroup2D's
    thread data structs to simplify the code.
    
    Bug: skia:
    Change-Id: Ie9a9ef9fc453acbdeb48b06b93d578c626961e3f

--- src/core/SkTaskGroup2D.h
+++ src/core/SkTaskGroup2D.h
@@ -13,22 +13,37 @@
 #include <mutex>
 #include <vector>
 
-// A 2D grid (height rows x width columns) of tasks.
-//
-// The task on row i and column j is abstracted as Work2D(i, j). We guarantee that the task on the
-// same row will be executed in order (i.e., Work2D(1, 1) is guaranteed to finish before calling
-// Work2D(1, 2)). Tasks in different rows can happen in any order.
+// A 2D grid (height rows x width columns) of tasks to be executed on a given executor with
+// threadCnt number of threads.
 //
 // The height (number of rows) is fixed. The width (number of columns) may be dynamically expanded.
 //
-// The tasks will eventually be executed on the executor with threadCnt number of hardware threads.
+// The task on row i and column j is abstracted as WorkFn(i, j, t). Parameter t is the thread id and
+// it shouldn't affect the work to be done. It's only used to allow some thread local variables
+// (e.g., thread local allocators). We guarantee that the task on the same row will be executed in
+// order (i.e., WorkFn(1, 1, t) is guaranteed to finish before calling WorkFn(1, 2, t)). Tasks in
+// different rows can happen in any order.
+//
+// There are also width number of init calls (fInit), one per column. WorkFn(i, j, t) may return
+// false if column j requires initialization but it's not initialized yet. In that case, a thread t
+// need to call InitFn(j, t) once to unblock all rows that depend on the initialization of column j.
+// (Again, t shouldn't affect the init work to be done; it's just for some thread local variables).
+// The init calls have no order requirement so we can call them in any order.
+//
+// Multiple therads may try to init the same column j at the same time. InitFn is expected to handle
+// this gracefully (e.g., let only one thread do the init and return immediately for other threads).
 class SkTaskGroup2D {
 public:
-    using Work2D = std::function<void(int, int)>;
+    // Return false iff the column needs an unfinished initialization.
+    using WorkFn = std::function<bool(int, int, int)>;
 
-    SkTaskGroup2D(Work2D&& work, int height, SkExecutor* executor, int threadCnt)
-            : fWork(work), fHeight(height), fThreadCnt(threadCnt), fIsFinishing(false), fWidth(0)
-            , fThreadsGroup(new SkTaskGroup(*executor)) {}
+    // Return false iff no initialization is done for this column in this call (either because it's
+    // already being initialized by another thread, or there's no initialization work to do.)
+    using InitFn = std::function<bool(int, int)>;
+
+    SkTaskGroup2D(WorkFn&& work, InitFn&& init, int height, SkExecutor* executor, int threadCnt)
+            : fWork(work), fInit(init), fHeight(height), fThreadCnt(threadCnt), fIsFinishing(false)
+            , fWidth(0), fThreadsGroup(new SkTaskGroup(*executor)) {}
 
     virtual ~SkTaskGroup2D() {}
 
@@ -47,7 +62,16 @@ protected:
     // Finish all tasks on the threadId and then return.
     virtual void work(int threadId) = 0;
 
-    Work2D      fWork; // fWork(i, j) is the task to be done on row i and column j
+    // initCol must be thread local
+    void initSomething(int& initCol, int threadId) {
+        bool didSomeInit = false;
+        while (initCol < fWidth && !didSomeInit) {
+            didSomeInit = fInit(initCol++, threadId);
+        }
+    }
+
+    WorkFn      fWork;
+    InitFn      fInit;
     const int   fHeight;
     const int   fThreadCnt;
 
@@ -60,28 +84,19 @@ protected:
 // A simple spinning task group that assumes height equals threadCnt.
 class SkSpinningTaskGroup2D final : public SkTaskGroup2D {
 public:
-    SkSpinningTaskGroup2D(Work2D&& w, int h, SkExecutor* x, int t)
-            : SkTaskGroup2D(std::move(w), h, x, t), fRowData(h) {
+    SkSpinningTaskGroup2D(WorkFn&& w, InitFn&& i, int h, SkExecutor* x, int t)
+            : SkTaskGroup2D(std::move(w), std::move(i), h, x, t) {
         SkASSERT(h == t); // height must be equal to threadCnt
     }
 
 protected:
     void work(int threadId) override;
-
-private:
-    // alignas(MAX_CACHE_LINE) to avoid false sharing by cache lines
-    struct alignas(MAX_CACHE_LINE) RowData {
-        RowData() : fNextColumn(0) {}
-
-        int fNextColumn; // next column index to be executed
-    };
-
-    std::vector<RowData>  fRowData;
 };
 
 class SkFlexibleTaskGroup2D final : public SkTaskGroup2D {
 public:
-    SkFlexibleTaskGroup2D(Work2D&&, int, SkExecutor*, int);
+    SkFlexibleTaskGroup2D(WorkFn&& w, InitFn&& i, int h, SkExecutor* x, int t)
+            : SkTaskGroup2D(std::move(w), std::move(i), h, x, t), fRowData(h) {}
 
 protected:
     void work(int threadId) override;
@@ -91,18 +106,11 @@ private:
     struct alignas(MAX_CACHE_LINE) RowData {
         RowData() : fNextColumn(0) {}
 
-        int         fNextColumn; // next column index to be executed
+        int         fNextColumn; // next column index to work
         std::mutex  fMutex;      // the mutex for the thread to acquire
     };
 
-    struct alignas(MAX_CACHE_LINE) ThreadData {
-        ThreadData() : fRowIndex(0) {}
-
-        int fRowIndex; // the row that the current thread is working on
-    };
-
     std::vector<RowData>    fRowData;
-    std::vector<ThreadData> fThreadData;
 };
 
 #endif//SkTaskGroup2D_DEFINED
