86f200d7143532304322af4e16c4bfb8e08f7c51
Mike Reed <reed@google.com>
2017-12-20 11:35:19 -0500

    add serialprocs to MultiDocument, hide redundant methods
    Bug: skia:
    Change-Id: I6521e93af79439bd8c1d2f5130a68492044a2ee9

--- include/core/SkSerialProcs.h
+++ include/core/SkSerialProcs.h
@@ -36,7 +36,7 @@ typedef sk_sp<SkPicture> (*SkDeserialPictureProc)(const void* data, size_t lengt
 typedef sk_sp<SkImage> (*SkDeserialImageProc)(const void* data, size_t length, void* ctx);
 typedef sk_sp<SkTypeface> (*SkDeserialTypefaceProc)(const void* data, size_t length, void* ctx);
 
-struct SkSerialProcs {
+SK_API struct SkSerialProcs {
     SkSerialPictureProc fPictureProc = nullptr;
     void*               fPictureCtx = nullptr;
 
@@ -47,7 +47,7 @@ struct SkSerialProcs {
     void*                fTypefaceCtx = nullptr;
 };
 
-struct SkDeserialProcs {
+SK_API struct SkDeserialProcs {
     SkDeserialPictureProc   fPictureProc = nullptr;
     void*                   fPictureCtx = nullptr;
 
