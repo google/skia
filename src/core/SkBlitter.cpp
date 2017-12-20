5fc914968c97599613f779b3f7d3ecc1f98276e8
Yuqian Li <liyuqian@google.com>
2017-12-20 11:10:24 -0500

    Add init-once to threaded backend
    For the simplicity of this CL, I haven't enabled DAA for init-once yet.
    The current init-once is only enabled for draw path, and it simply
    generates the dev path in the init-once phase.
    
    I also managed to remove the THREADED_DRAW macro and the SkTaskGroup2D's
    thread data structs to simplify the code.
    
    Bug: skia:
    Change-Id: Ie9a9ef9fc453acbdeb48b06b93d578c626961e3f

--- src/core/SkBlitter.cpp
+++ src/core/SkBlitter.cpp
@@ -79,9 +79,13 @@ void SkBlitter::blitFatAntiRect(const SkRect& rect) {
 }
 
 void SkBlitter::blitCoverageDeltas(SkCoverageDeltaList* deltas, const SkIRect& clip,
-                                   bool isEvenOdd, bool isInverse, bool isConvex) {
-    int         runSize = clip.width() + 1; // +1 so we can set runs[clip.width()] = 0
-    void*       storage = this->allocBlitMemory(runSize * (sizeof(int16_t) + sizeof(SkAlpha)));
+                                   bool isEvenOdd, bool isInverse, bool isConvex,
+                                   SkArenaAlloc* alloc) {
+    // We cannot use blitter to allocate the storage because the same blitter might be used across
+    // many threads.
+    int     runSize = clip.width() + 1; // +1 so we can set runs[clip.width()] = 0
+    void*   storage = alloc->makeArrayDefault<char>(runSize * (sizeof(int16_t) + sizeof(SkAlpha)));
+
     int16_t*    runs    = reinterpret_cast<int16_t*>(storage);
     SkAlpha*    alphas  = reinterpret_cast<SkAlpha*>(runs + runSize);
     runs[clip.width()]  = 0; // we must set the last run to 0 so blitAntiH can stop there
