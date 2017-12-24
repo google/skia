0b343082ddf805885e858cd591d50fa71711d208
Mike Reed <reed@google.com>
2017-12-23 20:54:53 -0500

    remove approxbytes api
    Bug: skia:
    Change-Id: I292bc9ab52fe8df3ce97a2ad4b06085b0332b19d

--- src/core/SkBigPicture.cpp
+++ src/core/SkBigPicture.cpp
@@ -15,10 +15,8 @@
 SkBigPicture::SkBigPicture(const SkRect& cull,
                            SkRecord* record,
                            SnapshotArray* drawablePicts,
-                           SkBBoxHierarchy* bbh,
-                           size_t approxBytesUsedBySubPictures)
+                           SkBBoxHierarchy* bbh)
     : fCullRect(cull)
-    , fApproxBytesUsedBySubPictures(approxBytesUsedBySubPictures)
     , fRecord(record)               // Take ownership of caller's ref.
     , fDrawablePicts(drawablePicts) // Take ownership.
     , fBBH(bbh)                     // Take ownership of caller's ref.
@@ -54,12 +52,7 @@ void SkBigPicture::partialPlayback(SkCanvas* canvas,
 }
 
 SkRect SkBigPicture::cullRect()            const { return fCullRect; }
-int    SkBigPicture::approximateOpCount()   const { return fRecord->count(); }
-size_t SkBigPicture::approximateBytesUsed() const {
-    size_t bytes = sizeof(*this) + fRecord->bytesUsed() + fApproxBytesUsedBySubPictures;
-    if (fBBH) { bytes += fBBH->bytesUsed(); }
-    return bytes;
-}
+int    SkBigPicture::approximateOpCount()  const { return fRecord->count(); }
 
 int SkBigPicture::drawableCount() const {
     return fDrawablePicts ? fDrawablePicts->count() : 0;
