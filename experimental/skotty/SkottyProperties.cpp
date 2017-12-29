4064893f1e26de2d4650a948ab693f223d488710
Florin Malita <fmalita@chromium.org>
2017-12-29 13:01:40 -0500

    Initial Lottie loader impl (Skotty)
    WIP
    
    Bug: skia:
    Change-Id: I96a02be7eb4fb4cb3831f59bf2b3908ea190c0dd

---
+++ experimental/skotty/SkottyProperties.cpp
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkottyProperties.h"
+
+#include "SkColor.h"
+#include "SkottyPriv.h"
+#include "SkPath.h"
+
+namespace  skotty {
+
+namespace {
+
+using PointArray = SkSTArray<64, SkPoint, true>;
+
+bool ParsePoints(const Json::Value& v, PointArray* pts) {
+    if (!v.isArray()) {
+        return false;
+    }
+
+    for (Json::ArrayIndex i = 0; i < v.size(); ++i) {
+        const auto& pt = v[i];
+        if (!pt.isArray() || pt.size() != 2 ||
+            !pt[0].isConvertibleTo(Json::realValue) ||
+            !pt[1].isConvertibleTo(Json::realValue)) {
+            return false;
+        }
+
+        pts->push_back(SkPoint::Make(ParseScalar(pt[0], 0), ParseScalar(pt[1], 0)));
+    }
+    return true;
+}
+
+} // namespace
+
+bool ScalarValue::Parse(const Json::Value& v, ScalarValue* scalar) {
+    if (v.isNull() || !v.isConvertibleTo(Json::realValue))
+        return false;
+
+    scalar->fVal = ParseScalar(v, 0);
+    return true;
+}
+
+bool VectorValue::Parse(const Json::Value& v, VectorValue* vec) {
+    SkASSERT(vec->fVals.empty());
+
+    if (!v.isArray())
+        return false;
+
+    for (Json::ArrayIndex i = 0; i < v.size(); ++i) {
+        const auto& el = v[i];
+        if (el.isNull() || !el.isConvertibleTo(Json::realValue))
+            return false;
+
+        vec->fVals.emplace_back(ParseScalar(el, 0));
+    }
+
+    return true;
+}
+
+bool ShapeValue::Parse(const Json::Value& v, ShapeValue* shape) {
+    PointArray inPts,
+               outPts,
+               verts;
+
+    // Some files appear to wrap these in arrays for no reason.
+    if (v.isArray()) {
+        return Parse(v[0], shape);
+    }
+
+    if (!v.isObject() ||
+        !ParsePoints(v["i"], &inPts) ||
+        !ParsePoints(v["o"], &outPts) ||
+        !ParsePoints(v["v"], &verts) ||
+        inPts.count() != outPts.count() ||
+        inPts.count() != verts.count()) {
+
+        LOG("!! could not parse shape: >%s<\n", v.toStyledString().c_str());
+        return false;
+    }
+
+    SkASSERT(shape->fVertices.empty());
+    for (int i = 0; i < inPts.count(); ++i) {
+        shape->fVertices.emplace_back(BezierVertex({inPts[i], outPts[i], verts[i]}));
+    }
+
+    shape->fClose = ParseBool(v["c"], false);
+
+    return true;
+}
+
+template <>
+SkColor VectorValue::as<SkColor>() const {
+    // best effort to turn this into a color
+    const auto r = fVals.count() > 0 ? fVals[0].as<SkScalar>() : 0,
+               g = fVals.count() > 1 ? fVals[1].as<SkScalar>() : 0,
+               b = fVals.count() > 2 ? fVals[2].as<SkScalar>() : 0,
+               a = fVals.count() > 3 ? fVals[3].as<SkScalar>() : 1;
+
+    return SkColorSetARGB(SkTPin<SkScalar>(a, 0, 1) * 255,
+                          SkTPin<SkScalar>(r, 0, 1) * 255,
+                          SkTPin<SkScalar>(g, 0, 1) * 255,
+                          SkTPin<SkScalar>(b, 0, 1) * 255);
+}
+
+template <>
+SkPath ShapeValue::as<SkPath>() const {
+    SkPath path;
+
+    if (!fVertices.empty()) {
+        path.moveTo(fVertices.front().fVertex);
+    }
+
+    const auto& addCubic = [](const BezierVertex& from, const BezierVertex& to, SkPath* path) {
+        path->cubicTo(from.fVertex + from.fOutPoint,
+                      to.fVertex   + to.fInPoint,
+                      to.fVertex);
+    };
+
+    for (int i = 1; i < fVertices.count(); ++i) {
+        addCubic(fVertices[i - 1], fVertices[i], &path);
+    }
+
+    if (fClose) {
+        addCubic(fVertices.back(), fVertices.front(), &path);
+    }
+
+    return path;
+}
+
+static SkScalar lerp_scalar(SkScalar v0, SkScalar v1, float t) {
+    SkASSERT(t >= 0 && t <= 1);
+    return v0 * (1 - t) + v1 * t;
+}
+
+static SkPoint lerp_point(const SkPoint& v0, const SkPoint& v1, float t) {
+    SkASSERT(t >= 0 && t <= 1);
+    return SkPoint::Make(lerp_scalar(v0.x(), v1.x(), t),
+                         lerp_scalar(v0.y(), v1.y(), t));
+}
+
+template <>
+void Keyframed<ScalarValue>::Keyframe::lerp(float t, ScalarValue* v) const {
+    *v = lerp_scalar(fV0, fV1, t);
+}
+
+template <>
+void Keyframed<VectorValue>::Keyframe::lerp(float t, VectorValue* v) const {
+    SkASSERT(fV0.cardinality() == fV1.cardinality());
+    SkASSERT(v->cardinality() == 0);
+
+    v->fVals.reserve(fV0.cardinality());
+    for (int i = 0; i < fV0.fVals.count(); ++i) {
+        v->fVals.emplace_back(lerp_scalar(fV0.fVals[i], fV1.fVals[i], t));
+    }
+}
+
+template <>
+void Keyframed<ShapeValue>::Keyframe::lerp(float t, ShapeValue* v) const {
+    SkASSERT(fV0.cardinality() == fV1.cardinality());
+    SkASSERT(v->cardinality() == 0);
+
+    v->fVertices.reserve(fV0.cardinality());
+    for (int i = 0; i < fV0.fVertices.count(); ++i) {
+        v->fVertices.push_back(
+            BezierVertex({
+                lerp_point(fV0.fVertices[i].fInPoint , fV1.fVertices[i].fInPoint , t),
+                lerp_point(fV0.fVertices[i].fOutPoint, fV1.fVertices[i].fOutPoint, t),
+                lerp_point(fV0.fVertices[i].fVertex  , fV1.fVertices[i].fVertex  , t)
+            }));
+    }
+}
+
+} // namespace skotty
