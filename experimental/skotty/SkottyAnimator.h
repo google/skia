77c662e7e6e2f1aeba0c40d2de1b3892fa31fb35
Florin Malita <fmalita@chromium.org>
2017-12-28 23:07:30 -0500

    Initial Bodymovin loader
    WIP
    
    Bug: skia:
    Change-Id: I96a02be7eb4fb4cb3831f59bf2b3908ea190c0dd

---
+++ experimental/skotty/SkottyAnimator.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkottyAnimator_DEFINED
+#define SkottyAnimator_DEFINED
+
+#include "SkottyProperties.h"
+#include "SkTypes.h"
+
+#include <functional>
+#include <memory>
+
+namespace skotty {
+
+class AnimatorBase : public SkNoncopyable {
+public:
+    virtual ~AnimatorBase() = default;
+
+    virtual void tick(SkMSec) = 0;
+
+protected:
+    AnimatorBase()  = default;
+};
+
+template <typename PropT, typename AttrT, typename NodeT>
+class Animator : public AnimatorBase {
+public:
+    Animator(sk_sp<NodeT> node, std::unique_ptr<Keyframed<PropT>> prop,
+             std::function<void(const sk_sp<NodeT>&, const AttrT&)> applyFunc)
+        : fProp(std::move(prop))
+        , fTarget(std::move(node))
+        , fFunc(std::move(applyFunc)) {}
+
+    void tick(SkMSec t) override {
+        const auto& prop = (*fProp)(t);
+        fFunc(fTarget, prop.template as<AttrT>());
+    }
+
+private:
+    std::unique_ptr<Keyframed<PropT>>              fProp;
+    sk_sp<NodeT>                                           fTarget;
+    std::function<void(const sk_sp<NodeT>&, const AttrT&)> fFunc;
+};
+
+} // namespace skotty
+
+#endif // SkottyAnimator_DEFINED
