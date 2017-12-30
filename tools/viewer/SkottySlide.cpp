ad6897ebdf416b6fe6a61baf2bc95d62643ab075
Florin Malita <fmalita@chromium.org>
2017-12-30 10:04:49 -0500

    Initial Lottie loader impl (Skotty)
    WIP
    
    Coarse workflow:
    
    * Construction
    
      1) build a Json tree
      2) collect asset IDs (for preComp/image layer resolution)
      3) "attach" pass
         - traverse the Json tree
         - build an SkSG dom, one fragment at a time
         - attach "animator" objects to the dom, for each animated prop
      4) done, we can throw away the Json tree
    
    * For each animation tick
    
      1) iterate over active animators and poke their respective dom nodes/attributes
      2) revalidate the SkSG dom
      3) draw the SkSG dom
    
    Note: post construction, things are super-simple - we just poke SkSG DOM attributes
    with interpolated values, and everything else is handled by SkSG (invalidation,
    revalidation, render).
    
    Change-Id: I96a02be7eb4fb4cb3831f59bf2b3908ea190c0dd

---
+++ tools/viewer/SkottySlide.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkottySlide.h"
+
+#include "SkAnimTimer.h"
+#include "Skotty.h"
+#include "SkStream.h"
+
+SkottySlide::SkottySlide(const SkString& name, const SkString& path)
+    : fPath(path) {
+    fName = name;
+}
+
+void SkottySlide::load(SkScalar, SkScalar) {
+    auto stream = SkStream::MakeFromFile(fPath.c_str());
+    fAnimation = skotty::Animation::Make(stream.get());
+
+    if (fAnimation) {
+        SkDebugf("loaded Bodymovin animation v: %s, size: [%f %f], fr: %f\n",
+                 fAnimation->version().c_str(),
+                 fAnimation->size().width(),
+                 fAnimation->size().height(),
+                 fAnimation->frameRate());
+    } else {
+        SkDebugf("failed to load Bodymovin animation: %s\n", fPath.c_str());
+    }
+}
+
+void SkottySlide::unload() {
+    fAnimation.reset();
+}
+
+SkISize SkottySlide::getDimensions() const {
+    return fAnimation? fAnimation->size().toCeil() : SkISize::Make(0, 0);
+}
+
+void SkottySlide::draw(SkCanvas* canvas) {
+    if (fAnimation) {
+        fAnimation->render(canvas);
+    }
+}
+
+bool SkottySlide::animate(const SkAnimTimer& timer) {
+    if (fAnimation) {
+        fAnimation->animationTick(timer.msec());
+    }
+    return true;
+}
+
+bool SkottySlide::onChar(SkUnichar c) {
+    switch (c) {
+    case 'I':
+        if (fAnimation) {
+            fShowAnimationInval = !fShowAnimationInval;
+            fAnimation->setShowInval(fShowAnimationInval);
+        }
+        break;
+    default:
+        break;
+    }
+
+    return INHERITED::onChar(c);
+}
