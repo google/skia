84ad2d57ac1b53e703db2b48979e2d912597cdab
Florin Malita <fmalita@chromium.org>
2017-12-30 10:31:37 -0500

    Initial Lottie loader impl (Skotty)
    Coarse workflow:
    
    * Construction
    
      1) build a Json tree
      2) collect asset IDs (for preComp/image layer resolution)
      3) "attach" pass
         - traverse the Json tree
         - build an SkSG dom, one fragment at a time
         - attach "animator" objects to the dom, for each animated prop
      4) done, we can throw away the Json tree
    
    * For each animation tick
    
      1) iterate over active animators and poke their respective dom nodes/attributes
      2) revalidate the SkSG dom
      3) draw the SkSG dom
    
    Note: post construction, things are super-simple - we just poke SkSG DOM attributes
    with interpolated values, and everything else is handled by SkSG (invalidation,
    revalidation, render).
    
    Change-Id: I96a02be7eb4fb4cb3831f59bf2b3908ea190c0dd

---
+++ experimental/skotty/SkottyAnimator.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkottyAnimator.h"
+
+namespace skotty {
+
+namespace {
+
+SkScalar lerp_scalar(SkScalar v0, SkScalar v1, float t) {
+    SkASSERT(t >= 0 && t <= 1);
+    return v0 * (1 - t) + v1 * t;
+}
+
+SkPoint lerp_point(const SkPoint& v0, const SkPoint& v1, float t) {
+    SkASSERT(t >= 0 && t <= 1);
+    return SkPoint::Make(lerp_scalar(v0.x(), v1.x(), t),
+                         lerp_scalar(v0.y(), v1.y(), t));
+}
+
+} // namespace
+
+template <>
+void KeyframeInterval<ScalarValue>::lerp(float t, ScalarValue* v) const {
+    *v = lerp_scalar(fV0, fV1, t);
+}
+
+template <>
+void KeyframeInterval<VectorValue>::lerp(float t, VectorValue* v) const {
+    SkASSERT(fV0.cardinality() == fV1.cardinality());
+    SkASSERT(v->cardinality() == 0);
+
+    v->fVals.reserve(fV0.cardinality());
+    for (int i = 0; i < fV0.fVals.count(); ++i) {
+        v->fVals.emplace_back(lerp_scalar(fV0.fVals[i], fV1.fVals[i], t));
+    }
+}
+
+template <>
+void KeyframeInterval<ShapeValue>::lerp(float t, ShapeValue* v) const {
+    SkASSERT(fV0.cardinality() == fV1.cardinality());
+    SkASSERT(v->cardinality() == 0);
+
+    v->fVertices.reserve(fV0.cardinality());
+    for (int i = 0; i < fV0.fVertices.count(); ++i) {
+        v->fVertices.push_back(
+            BezierVertex({
+                lerp_point(fV0.fVertices[i].fInPoint , fV1.fVertices[i].fInPoint , t),
+                lerp_point(fV0.fVertices[i].fOutPoint, fV1.fVertices[i].fOutPoint, t),
+                lerp_point(fV0.fVertices[i].fVertex  , fV1.fVertices[i].fVertex  , t)
+            }));
+    }
+}
+
+} // namespace skotty
