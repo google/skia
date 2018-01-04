41ed96aec5e46cc6d01617e2840489200e60015f
Florin Malita <fmalita@chromium.org>
2018-01-03 23:31:58 -0500

    [skotty,sksg] Add support for geometry merge
    Change-Id: Ia5edbfeae61779ead6031f6dd4e33794b3eefdc0

--- experimental/sksg/SkSGGeometryNode.cpp
+++ experimental/sksg/SkSGGeometryNode.cpp
@@ -7,11 +7,18 @@
 
 #include "SkSGGeometryNode.h"
 
+#include "SkPath.h"
+
 namespace sksg {
 
 void GeometryNode::draw(SkCanvas* canvas, const SkPaint& paint) const {
     SkASSERT(!this->hasInval());
     this->onDraw(canvas, paint);
+}
+
+SkPath GeometryNode::asPath() const {
+    SkASSERT(!this->hasInval());
+    return this->onAsPath();
 }
 
 } // namespace sksg
