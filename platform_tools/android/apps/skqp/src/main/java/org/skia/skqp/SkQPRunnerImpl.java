ef888f19a0d48b1f00c5206867541b6988e5c8d1
Stephan Altmueller <stephana@google.com>
2018-01-05 09:41:14 -0500

    First version of SkQP app to run on Firbase Testlab
    Adds activities to the skqp app so it can run as an Android
    app (as opposed to just instrumentation tests).
    A user can trigger the tests via a button.
    Adds the an intent receiver so the tests can be triggered on
    Firebase Testlab via the gameloop option.
    
    It adds the run_testlab.go script to run an apk across devices
    on Firebase Testlab.
    
    Bug: skia:
    Change-Id: I3ff5c37d743fa47913a916a0fa1e7db3c2cc79c7

---
+++ platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQPRunnerImpl.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.skia.skqp;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import org.skia.skqp.SkQPException;
+import java.io.IOException;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+
+public class SkQPRunnerImpl {
+    protected native void nInit(AssetManager assetManager, String dataDir);
+    protected native float nExecuteGM(int gm, int backend) throws SkQPException;
+    protected native String[] nExecuteUnitTest(int test);
+    protected native void nMakeReport();
+
+    protected String[] mGMs;
+    protected String[] mBackends;
+    protected String[] mUnitTests;
+
+    protected static final String kSkiaGM = "SkiaGM_";
+    protected static final String kSkiaUnitTests = "Skia_UnitTests";
+    protected static final String LOG_PREFIX = "org.skis.skqp";
+
+    static {
+      System.loadLibrary("skqp_app");
+    }
+
+    protected void runTests(Context context, String outputDirPath) {
+        Log.w(LOG_PREFIX, "Output Dir: " + outputDirPath);
+        File outputDir = new File(outputDirPath);
+        if (outputDir.exists()) {
+          try {
+              deleteDirectoryContents(outputDir);
+          } catch (IOException e) {
+              Log.w(LOG_PREFIX, "DeleteDirectoryContents: " + e.getMessage());
+          }
+        }
+
+        // Note: nInit will initialize the mGMs, mBackends and mUnitTests fields.
+        AssetManager assetManager = context.getResources().getAssets();
+        this.nInit(assetManager, outputDirPath);
+
+        for (int backend = 0; backend < mBackends.length; backend++) {
+          String classname = kSkiaGM + mBackends[backend];
+          for (int gm = 0; gm < mGMs.length; gm++) {
+              String testName = kSkiaGM + mBackends[backend] + "_" +mGMs[gm];
+              float value = java.lang.Float.MAX_VALUE;
+              String error = null;
+              Log.w(LOG_PREFIX, "Running: " + testName);
+              try {
+                  value = this.nExecuteGM(gm, backend);
+              } catch (SkQPException exept) {
+                  error = exept.getMessage();
+              }
+              if (error != null) {
+                // Record error message and carry on.
+              } else if (value != 0) {
+                // Record failure and carry on.
+                  // SkQPRunner.Fail(desc, notifier, String.format(
+                  //             "Image mismatch: max channel diff = %f", value));
+              } else {
+                // Record success for this test.
+              }
+          }
+      }
+      for (int unitTest = 0; unitTest < mUnitTests.length; unitTest++) {
+          String testName = kSkiaUnitTests + "_" + mUnitTests[unitTest];
+          Log.w(LOG_PREFIX, "Running: " + testName);
+          String[] errors = this.nExecuteUnitTest(unitTest);
+          if (errors != null && errors.length > 0) {
+              for (String error : errors) {
+                // Record unit test failures.
+              }
+          } else {
+            // Record success.
+          }
+      }
+      nMakeReport();
+    }
+
+    protected static void deleteDirectoryContents(File f) throws IOException {
+      for (File s : f.listFiles()) {
+          if (s.isDirectory()) {
+              deleteDirectoryContents(s);
+          }
+          s.delete();
+      }
+    }
+}
+
