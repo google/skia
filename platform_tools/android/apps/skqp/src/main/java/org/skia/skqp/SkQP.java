be96d62876fde1db6d4015aba46a7cde13db2c2d
Stephan Altmueller <stephana@google.com>
2018-01-08 12:52:57 -0500

    First version of SkQP app to run on Firbase Testlab
    Adds activities to the skqp app so it can run as an Android
    app (as opposed to just instrumentation tests).
    A user can trigger the tests via a button.
    Adds the an intent receiver so the tests can be triggered on
    Firebase Testlab via the gameloop option.
    
    It adds the run_testlab.go script to run an apk across devices
    on Firebase Testlab.
    
    Bug: skia:
    Change-Id: I3ff5c37d743fa47913a916a0fa1e7db3c2cc79c7

--- platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQP.java
+++ platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQP.java
@@ -7,8 +7,90 @@
 
 package org.skia.skqp;
 
-import org.junit.runner.RunWith;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+import java.io.File;
+import java.io.IOException;
 
-@RunWith(SkQPRunner.class)
-public class SkQP {}
+public class SkQP {
+    protected native void nInit(AssetManager assetManager, String dataDir);
+    protected native float nExecuteGM(int gm, int backend) throws SkQPException;
+    protected native String[] nExecuteUnitTest(int test);
+    protected native void nMakeReport();
+
+    protected String[] mGMs;
+    protected String[] mBackends;
+    protected String[] mUnitTests;
+
+    protected static final String kSkiaGM = "SkiaGM_";
+    protected static final String kSkiaUnitTests = "Skia_UnitTests";
+    protected static final String LOG_PREFIX = "org.skis.skqp";
+
+    static {
+      System.loadLibrary("skqp_app");
+    }
+
+    protected void runTests(Context context, String outputDirPath) {
+        Log.w(LOG_PREFIX, "Output Dir: " + outputDirPath);
+        File outputDir = new File(outputDirPath);
+        if (outputDir.exists()) {
+          try {
+              deleteDirectoryContents(outputDir);
+          } catch (IOException e) {
+              Log.w(LOG_PREFIX, "DeleteDirectoryContents: " + e.getMessage());
+          }
+        }
+
+        // Note: nInit will initialize the mGMs, mBackends and mUnitTests fields.
+        AssetManager assetManager = context.getResources().getAssets();
+        this.nInit(assetManager, outputDirPath);
+
+        for (int backend = 0; backend < mBackends.length; backend++) {
+          String classname = kSkiaGM + mBackends[backend];
+          for (int gm = 0; gm < mGMs.length; gm++) {
+              String testName = kSkiaGM + mBackends[backend] + "_" +mGMs[gm];
+              float value = java.lang.Float.MAX_VALUE;
+              String error = null;
+              Log.w(LOG_PREFIX, "Running: " + testName);
+              try {
+                  value = this.nExecuteGM(gm, backend);
+              } catch (SkQPException exept) {
+                  error = exept.getMessage();
+              }
+              if (error != null) {
+                // Record error message and carry on.
+              } else if (value != 0) {
+                // Record failure and carry on.
+                  // SkQPRunner.Fail(desc, notifier, String.format(
+                  //             "Image mismatch: max channel diff = %f", value));
+              } else {
+                // Record success for this test.
+              }
+          }
+      }
+      for (int unitTest = 0; unitTest < mUnitTests.length; unitTest++) {
+          String testName = kSkiaUnitTests + "_" + mUnitTests[unitTest];
+          Log.w(LOG_PREFIX, "Running: " + testName);
+          String[] errors = this.nExecuteUnitTest(unitTest);
+          if (errors != null && errors.length > 0) {
+              for (String error : errors) {
+                // Record unit test failures.
+              }
+          } else {
+            // Record success.
+          }
+      }
+      nMakeReport();
+    }
+
+    protected static void deleteDirectoryContents(File f) throws IOException {
+      for (File s : f.listFiles()) {
+          if (s.isDirectory()) {
+              deleteDirectoryContents(s);
+          }
+          s.delete();
+      }
+    }
+}
 
--- platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQPRunner.java
+++ platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQPRunner.java
@@ -21,30 +21,8 @@ import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
 public class SkQPRunner extends Runner {
-    private native void nInit(AssetManager assetManager, String dataDir);
-    private native float nExecuteGM(int gm, int backend) throws SkQPException;
-    private native String[] nExecuteUnitTest(int test);
-    private native void nMakeReport();
-
-    private AssetManager mAssetManager;
-    private String[] mGMs;
-    private String[] mBackends;
-    private String[] mUnitTests;
-
-    private static boolean sOnceFlag = false;
-    private static final String kSkiaGM = "SkiaGM_";
-    private static final String kSkiaUnitTests = "Skia_UnitTests";
-
     private Description mDescription;
-
-    private static void DeleteDirectoryContents(File f) throws IOException {
-        for (File s : f.listFiles()) {
-            if (s.isDirectory()) {
-                SkQPRunner.DeleteDirectoryContents(s);
-            }
-            s.delete();
-        }
-    }
+    private SkQP impl;
 
     private static void Fail(Description desc, RunNotifier notifier, String failure) {
         notifier.fireTestFailure(new Failure(desc, new Throwable(failure)));
@@ -53,37 +31,30 @@ public class SkQPRunner extends Runner {
     ////////////////////////////////////////////////////////////////////////////
 
     public SkQPRunner(Class testClass) {
-        synchronized (SkQPRunner.class) {
-            if (sOnceFlag) {
-                throw new IllegalStateException("Error multiple SkQPs defined");
-            }
-            sOnceFlag = true;
-        }
-        System.loadLibrary("skqp_app");
-
+        impl = new SkQP();
         Context context = InstrumentationRegistry.getTargetContext();
         File filesDir = context.getFilesDir();
         try {
-            SkQPRunner.DeleteDirectoryContents(filesDir);
+            SkQP.deleteDirectoryContents(filesDir);
         } catch (IOException e) {
             Log.w("org.skis.skqp", "DeleteDirectoryContents: " + e.getMessage());
         }
 
         Resources resources = context.getResources();
-        mAssetManager = resources.getAssets();
-        this.nInit(mAssetManager, filesDir.getAbsolutePath());
+        AssetManager mAssetManager = resources.getAssets();
+        impl.nInit(mAssetManager, filesDir.getAbsolutePath());
 
         mDescription = Description.createSuiteDescription(testClass);
         Annotation annots[] = new Annotation[0];
-        for (int backend = 0; backend < mBackends.length; backend++) {
-            String classname = kSkiaGM + mBackends[backend];
-            for (int gm = 0; gm < mGMs.length; gm++) {
-                mDescription.addChild(Description.createTestDescription(classname, mGMs[gm], annots));
+        for (int backend = 0; backend < impl.mBackends.length; backend++) {
+            String classname = SkQP.kSkiaGM + impl.mBackends[backend];
+            for (int gm = 0; gm < impl.mGMs.length; gm++) {
+                mDescription.addChild(Description.createTestDescription(classname, impl.mGMs[gm], annots));
             }
         }
-        for (int unitTest = 0; unitTest < mUnitTests.length; unitTest++) {
-            mDescription.addChild(Description.createTestDescription(kSkiaUnitTests,
-                        mUnitTests[unitTest], annots));
+        for (int unitTest = 0; unitTest < impl.mUnitTests.length; unitTest++) {
+            mDescription.addChild(Description.createTestDescription(SkQP.kSkiaUnitTests,
+                        impl.mUnitTests[unitTest], annots));
         }
     }
 
@@ -91,20 +62,20 @@ public class SkQPRunner extends Runner {
     public Description getDescription() { return mDescription; }
 
     @Override
-    public int testCount() { return mUnitTests.length + mGMs.length * mBackends.length; }
+    public int testCount() { return impl.mUnitTests.length + impl.mGMs.length * impl.mBackends.length; }
 
     @Override
     public void run(RunNotifier notifier) {
         Annotation annots[] = new Annotation[0];
-        for (int backend = 0; backend < mBackends.length; backend++) {
-            String classname = kSkiaGM + mBackends[backend];
-            for (int gm = 0; gm < mGMs.length; gm++) {
-                Description desc = Description.createTestDescription(classname, mGMs[gm], annots);
+        for (int backend = 0; backend < impl.mBackends.length; backend++) {
+            String classname = SkQP.kSkiaGM + impl.mBackends[backend];
+            for (int gm = 0; gm < impl.mGMs.length; gm++) {
+                Description desc = Description.createTestDescription(classname, impl.mGMs[gm], annots);
                 notifier.fireTestStarted(desc);
                 float value = java.lang.Float.MAX_VALUE;
                 String error = null;
                 try {
-                    value = this.nExecuteGM(gm, backend);
+                    value = impl.nExecuteGM(gm, backend);
                 } catch (SkQPException exept) {
                     error = exept.getMessage();
                 }
@@ -117,11 +88,11 @@ public class SkQPRunner extends Runner {
                 notifier.fireTestFinished(desc);
             }
         }
-        for (int unitTest = 0; unitTest < mUnitTests.length; unitTest++) {
+        for (int unitTest = 0; unitTest < impl.mUnitTests.length; unitTest++) {
             Description desc = Description.createTestDescription(
-                    kSkiaUnitTests, mUnitTests[unitTest], annots);
+                          SkQP.kSkiaUnitTests, impl.mUnitTests[unitTest], annots);
             notifier.fireTestStarted(desc);
-            String[] errors = this.nExecuteUnitTest(unitTest);
+            String[] errors = impl.nExecuteUnitTest(unitTest);
             if (errors != null && errors.length > 0) {
                 for (String error : errors) {
                     SkQPRunner.Fail(desc, notifier, error);
@@ -129,7 +100,7 @@ public class SkQPRunner extends Runner {
             }
             notifier.fireTestFinished(desc);
         }
-        this.nMakeReport();
+        impl.nMakeReport();
     }
 }
 
