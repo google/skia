f484882b36a376d7bede90b34c23652d9de86afe
Yuqian Li <liyuqian@google.com>
2018-01-08 13:40:45 -0500

    Remove legacy containedInClip code
    It seems that we've already rebaselined the golden images
    
    TBR: reed@google.com
    Bug: skia:7271
    Change-Id: I841dc4933f0cad015c3c6da7bd08ddfb2a35c5a2
    Reviewed-on: https://skia-review.googlesource.com/92060
    Reviewed-by: Yuqian Li <liyuqian@google.com>
    Commit-Queue: Yuqian Li <liyuqian@google.com>

--- src/core/SkScan_DAAPath.cpp
+++ src/core/SkScan_DAAPath.cpp
@@ -319,10 +319,8 @@ void gen_alpha_deltas(const SkPath& path, const SkIRect& clipBounds, Deltas& res
 // blitRegion, sk_blit_above, sk_blit_below in SkScan::AntiFillPath to maintain the draw order. If
 // we do that, be caureful that blitRect may throw exception if the rect is empty.
 void SkScan::DAAFillPath(const SkPath& path, SkBlitter* blitter, const SkIRect& ir,
-                         const SkIRect& clipBounds, bool containedInClip, bool forceRLE) {
-#if !defined(SK_SUPPORT_LEGACY_AA_BEHAVIOR)
-    containedInClip = clipBounds.contains(ir);
-#endif
+                         const SkIRect& clipBounds, bool forceRLE) {
+    bool containedInClip = clipBounds.contains(ir);
     bool isEvenOdd  = path.getFillType() & 1;
     bool isConvex   = path.isConvex();
     bool isInverse  = path.isInverseFillType();
