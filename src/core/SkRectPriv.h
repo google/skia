3b3e54fffa8da58095d21ad98ad5d8259ea49d06
Mike Reed <reed@google.com>
2018-01-09 10:37:27 -0500

    use 64bit math to compute is a rect is empty
    Will work next to try to make isEmpty() private
    
    Bug: skia:7470
    Change-Id: I7b43028ecd86dca68e0c67225712516d2f2f88a2

--- src/core/SkRectPriv.h
+++ src/core/SkRectPriv.h
@@ -12,14 +12,6 @@
 
 class SkRectPriv {
 public:
-    // Returns true iff width and height are positive. Catches inverted, empty, and overflowing
-    // (way too big) rects. This is used by clients that want a non-empty rect that they can also
-    // actually use its computed width/height.
-    //
-    static bool PositiveDimensions(const SkIRect& r) {
-        return r.width() > 0 && r.height() > 0;
-    }
-
     static SkRect MakeLargestS32() {
         const int32_t ihalf = SK_MaxS32 >> 1;
         const SkScalar half = SkIntToScalar(ihalf);
