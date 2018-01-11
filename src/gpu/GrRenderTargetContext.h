16d441e25f65eba96465ced0a0bed049dea157a1
Brian Salomon <bsalomon@google.com>
2018-01-11 10:06:37 -0500

    Make GrTextureOp capable of edge antialiasing.
    Bug: skia:
    Change-Id: I0088bdbb6a76811611fa4628656bf9513c5bf04a

--- src/gpu/GrRenderTargetContext.h
+++ src/gpu/GrRenderTargetContext.h
@@ -145,11 +145,10 @@ public:
      * Creates an op that draws a subrectangle of a texture. The passed color is modulated by the
      * texture's color. 'srcRect' specifies the rectangle of the texture to draw. 'dstRect'
      * specifies the rectangle to draw in local coords which will be transformed by 'viewMatrix' to
-     * device space. The edges of the rendered rectangle are not antialiased. This asserts that the
-     * view matrix does not have perspective.
+     * device space. This asserts that the view matrix does not have perspective.
      */
     void drawTextureAffine(const GrClip& clip, sk_sp<GrTextureProxy>, GrSamplerState::Filter,
-                           GrColor, const SkRect& srcRect, const SkRect& dstRect,
+                           GrColor, const SkRect& srcRect, const SkRect& dstRect, GrAA aa,
                            const SkMatrix& viewMatrix, sk_sp<GrColorSpaceXform>);
 
     /**
