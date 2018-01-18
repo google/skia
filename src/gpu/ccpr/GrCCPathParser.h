e4dc604e1c41e83beaa3a1b3d53d0c35a2c91b73
Chris Dalton <csmartdalton@google.com>
2018-01-18 12:19:24 -0700

    Extract a GrCCPathParser class that is decoupled from atlas ops
    Allows coverage counts ultimately to be drawn either to an atlas or
    directly to the framebuffer.
    
    Bug: skia:
    Change-Id: I6cc07fce562c223381b89586d19ae98298bafe4d

--- src/gpu/ccpr/GrCCPathParser.h
+++ src/gpu/ccpr/GrCCPathParser.h
@@ -5,82 +5,71 @@
  * found in the LICENSE file.
  */
 
-#ifndef GrCCCoverageOp_DEFINED
-#define GrCCCoverageOp_DEFINED
+#ifndef GrCCPathParser_DEFINED
+#define GrCCPathParser_DEFINED
 
 #include "GrMesh.h"
+#include "GrNonAtomicRef.h"
 #include "SkRect.h"
 #include "SkRefCnt.h"
 #include "ccpr/GrCCCoverageProcessor.h"
 #include "ccpr/GrCCGeometry.h"
 #include "ops/GrDrawOp.h"
 
-class GrCCCoverageOp;
 class GrOnFlushResourceProvider;
 class SkMatrix;
 class SkPath;
 
 /**
- * This class produces GrCCCoverageOps that render coverage count masks and atlases. A path is
- * added to the current op in two steps:
- *
- *   1) parsePath(ScissorMode, viewMatrix, path, &devBounds, &devBounds45);
- *
- *   <client decides where to put the mask within an atlas, if wanted>
- *
- *   2) saveParsedPath(offsetX, offsetY, clipBounds);
- *
- * The client can flush the currently saved paths to a GrCCCoverageOp by calling emitOp, and
- * retrieve all emitted ops after calling finalize().
+ * This class parses SkPaths into CCPR primitives in GPU buffers, then issues calls to draw their
+ * coverage counts.
  */
-class GrCCCoverageOpsBuilder {
+class GrCCPathParser : public GrNonAtomicRef<GrCCPathParser> {
 public:
     // Indicates whether a path should enforce a scissor clip when rendering its mask. (Specified
     // as an int because these values get used directly as indices into arrays.)
     enum class ScissorMode : int { kNonScissored = 0, kScissored = 1 };
     static constexpr int kNumScissorModes = 2;
 
-    GrCCCoverageOpsBuilder(int maxTotalPaths, int maxPathPoints, int numSkPoints, int numSkVerbs)
-            : fPathsInfo(maxTotalPaths)
-            , fLocalDevPtsBuffer(maxPathPoints + 1)  // Overallocate by one point to accomodate for
-                                                     // overflow with Sk4f. (See parsePath.)
-            , fGeometry(numSkPoints, numSkVerbs)
-            , fTallies{PrimitiveTallies(), PrimitiveTallies()}
-            , fScissorBatches(maxTotalPaths) {}
+    GrCCPathParser(int maxTotalPaths, int maxPathPoints, int numSkPoints, int numSkVerbs);
 
-    ~GrCCCoverageOpsBuilder() {
+    ~GrCCPathParser() {
         // Enforce the contract that the client always calls saveParsedPath or discardParsedPath.
         SkASSERT(!fParsingPath);
     }
 
-    // Parses an SkPath into a temporary staging area. The path will not yet be included in the next
-    // Op unless there is a matching call to saveParsedPath. The user must complement this with a
+    using CoverageCountBatchID = int;
+
+    // Parses an SkPath into a temporary staging area. The path will not be included in the current
+    // batch until there is a matching call to saveParsedPath. The user must complement this with a
     // following call to either saveParsedPath or discardParsedPath.
     //
     // Returns two tight bounding boxes: device space and "45 degree" (| 1 -1 | * devCoords) space.
     //                                                                 | 1  1 |
     void parsePath(const SkMatrix&, const SkPath&, SkRect* devBounds, SkRect* devBounds45);
 
-    // Parses a device-space SkPath into a temporary staging area. The path will not yet be included
-    // in the next Op unless there is a matching call to saveParsedPath. The user must complement
+    // Parses a device-space SkPath into a temporary staging area. The path will not be included in
+    // the current batch until there is a matching call to saveParsedPath. The user must complement
     // this with a following call to either saveParsedPath or discardParsedPath.
     void parseDeviceSpacePath(const SkPath&);
 
-    // Commits the currently-parsed path from staging to the next Op, and specifies whether the mask
-    // should be rendered with a scissor clip in effect. Accepts an optional post-device-space
+    // Commits the currently-parsed path from staging to the current batch, and specifies whether
+    // the mask should be rendered with a scissor in effect. Accepts an optional post-device-space
     // translate for placement in an atlas.
     void saveParsedPath(ScissorMode, const SkIRect& clippedDevIBounds, int16_t atlasOffsetX,
                         int16_t atlasOffsetY);
     void discardParsedPath();
 
-    // Flushes all currently-saved paths internally to a GrCCCoverageOp.
-    //
-    // NOTE: if there is a parsed path in the staging area, it will not be included. But the client
-    // may still call saveParsedPath to include it in a future Op.
-    void emitOp(SkISize drawBounds);
+    // Compiles the outstanding saved paths into a batch, and returns an ID that can be used to draw
+    // their coverage counts in the future.
+    CoverageCountBatchID closeCurrentBatch();
 
-    // Builds GPU buffers and returns the list of GrCCCoverageOps as specified by calls to emitOp.
-    bool finalize(GrOnFlushResourceProvider*, SkTArray<std::unique_ptr<GrCCCoverageOp>>*);
+    // Builds internal GPU buffers and prepares for calls to drawCoverageCount. Caller must close
+    // the current batch before calling this method, and cannot parse new paths afer.
+    bool finalize(GrOnFlushResourceProvider*);
+
+    // Called after finalize. Draws the given batch of parsed paths.
+    void drawCoverageCount(GrOpFlushState*, CoverageCountBatchID, const SkIRect& drawBounds) const;
 
 private:
     using PrimitiveTallies = GrCCGeometry::PrimitiveTallies;
@@ -89,87 +78,47 @@ private:
     struct PathInfo {
         ScissorMode fScissorMode;
         int16_t fAtlasOffsetX, fAtlasOffsetY;
-        std::unique_ptr<GrCCCoverageOp> fTerminatingOp;
     };
 
-    // Every PathInfo with a mode of kScissored has a corresponding ScissorBatch.
-    struct ScissorBatch {
-        PrimitiveTallies fInstanceCounts;
+    // Defines a batch of CCPR primitives. Start indices are deduced by looking at the previous
+    // CoverageCountBatch in the list.
+    struct CoverageCountBatch {
+        PrimitiveTallies fEndNonScissorIndices;
+        int fEndScissorSubBatchIdx;
+    };
+
+    // Defines a sub-batch from CoverageCountBatch that will be drawn with the given scissor rect.
+    // Start indices are deduced by looking at the previous ScissorSubBatch in the list.
+    struct ScissorSubBatch {
+        PrimitiveTallies fEndPrimitiveIndices;
         SkIRect fScissor;
     };
 
     void parsePath(const SkPath&, const SkPoint* deviceSpacePts);
     void endContourIfNeeded(bool insideContour);
 
+    void drawRenderPass(GrOpFlushState*, const GrPipeline&, CoverageCountBatchID,
+                        GrCCCoverageProcessor::RenderPass, int PrimitiveTallies::*instanceType,
+                        const SkIRect& drawBounds) const;
+
     // Staging area for the path being parsed.
     SkDEBUGCODE(int fParsingPath = false);
+    const SkAutoSTArray<32, SkPoint> fLocalDevPtsBuffer;
     int fCurrPathPointsIdx;
     int fCurrPathVerbsIdx;
-    PrimitiveTallies fCurrPathTallies;
+    PrimitiveTallies fCurrPathPrimitiveCounts;
 
-    SkSTArray<32, PathInfo, true> fPathsInfo;
-
-    const SkAutoSTArray<32, SkPoint> fLocalDevPtsBuffer;
     GrCCGeometry fGeometry;
-
-    PrimitiveTallies fTallies[kNumScissorModes];
-    SkTArray<ScissorBatch, true> fScissorBatches;
-
-    std::unique_ptr<GrCCCoverageOp> fTerminatingOp;
-
-    friend class GrCCCoverageOp;  // For ScissorBatch.
-};
-
-/**
- * This Op renders coverage count masks and atlases. Create it using GrCCCoverageOpsBuilder.
- */
-class GrCCCoverageOp : public GrDrawOp {
-public:
-    DEFINE_OP_CLASS_ID
-
-    // GrDrawOp interface.
-    const char* name() const override { return "GrCCCoverageOp"; }
-    FixedFunctionFlags fixedFunctionFlags() const override { return FixedFunctionFlags::kNone; }
-    RequiresDstTexture finalize(const GrCaps&, const GrAppliedClip*,
-                                GrPixelConfigIsClamped) override {
-        return RequiresDstTexture::kNo;
-    }
-    bool onCombineIfPossible(GrOp* other, const GrCaps& caps) override { return false; }
-    void onPrepare(GrOpFlushState*) override {}
-    void onExecute(GrOpFlushState*) override;
-
-private:
-    static constexpr int kNumScissorModes = GrCCCoverageOpsBuilder::kNumScissorModes;
-    using PrimitiveTallies = GrCCGeometry::PrimitiveTallies;
-    using ScissorBatch = GrCCCoverageOpsBuilder::ScissorBatch;
-
-    GrCCCoverageOp(SkTArray<ScissorBatch, true>&& scissorBatches, const SkISize& drawBounds)
-            : INHERITED(ClassID())
-            , fScissorBatches(std::move(scissorBatches))
-            , fDrawBounds(drawBounds) {
-        this->setBounds(SkRect::MakeIWH(fDrawBounds.width(), fDrawBounds.height()),
-                        GrOp::HasAABloat::kNo, GrOp::IsZeroArea::kNo);
-    }
-
-    void setInstanceBuffer(sk_sp<GrBuffer> instanceBuffer,
-                           const PrimitiveTallies baseInstances[kNumScissorModes],
-                           const PrimitiveTallies endInstances[kNumScissorModes]);
-
-    void drawMaskPrimitives(GrOpFlushState*, const GrPipeline&, GrCCCoverageProcessor::RenderPass,
-                            int PrimitiveTallies::*instanceType) const;
+    SkSTArray<32, PathInfo, true> fPathsInfo;
+    SkSTArray<32, CoverageCountBatch, true> fCoverageCountBatches;
+    SkSTArray<32, ScissorSubBatch, true> fScissorSubBatches;
+    PrimitiveTallies fTotalPrimitiveCounts[kNumScissorModes];
+    int fMaxMeshesPerDraw = 0;
 
     sk_sp<GrBuffer> fInstanceBuffer;
     PrimitiveTallies fBaseInstances[kNumScissorModes];
-    PrimitiveTallies fInstanceCounts[kNumScissorModes];
-    const SkTArray<ScissorBatch, true> fScissorBatches;
-    const SkISize fDrawBounds;
-
-    mutable SkTArray<GrMesh> fMeshesScratchBuffer;
-    mutable SkTArray<GrPipeline::DynamicState, true> fDynamicStatesScratchBuffer;
-
-    friend class GrCCCoverageOpsBuilder;
-
-    typedef GrDrawOp INHERITED;
+    mutable SkSTArray<32, GrMesh> fMeshesScratchBuffer;
+    mutable SkSTArray<32, GrPipeline::DynamicState> fDynamicStatesScratchBuffer;
 };
 
 #endif
