17ee8f52dc27d094277031520c8e184af7c8af4b
Mike Reed <reed@google.com>
2018-01-17 23:04:04 -0500

    drawimage + maskfilter bug fix
    bug: we ignored maskfilter in spriteblitter case
    
    Bug: skia:
    Change-Id: I2c9423396a7a9ed3b81cd86e3a5edebbf34538db

--- gm/blurimagevmask.cpp
+++ gm/blurimagevmask.cpp
@@ -55,3 +55,18 @@ DEF_SIMPLE_GM(blurimagevmask, canvas, 700, 1200) {
     }
 
 }
+
+#include "Resources.h"
+DEF_SIMPLE_GM(blur_image, canvas, 500, 500) {
+    auto image = GetResourceAsImage("images/mandrill_128.png");
+
+    SkPaint paint;
+    paint.setMaskFilter(SkBlurMaskFilter::Make(kNormal_SkBlurStyle, 4));
+
+    // both of these should draw with the blur, but (formerally) we had a bug where the unscaled
+    // version (taking the spriteblitter code path) ignore the maskfilter.
+
+    canvas->drawImage(image, 10, 10, &paint);
+    canvas->scale(1.01f, 1.01f);
+    canvas->drawImage(image, 10 + image->width() + 10.f, 10, &paint);
+}
