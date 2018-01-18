35c909bb36a707305a10c95c5ae796c266d0ae98
Mike Reed <reed@google.com>
2018-01-18 13:46:21 -0500

    detect fixed overflow in drawPoints
    This particular test this is addressing is:
    
    1. very large device (>32K in a test case)
    2. drawPoints has special (fast) case where it converts all of the pts (rects) into fixed point
    3. overflows fixed -- assert
    
    Paths already don't draw into a device this large (they are limited by 32K I think, smaller if aa)
    The (theoretical) solution for paths is to tile the draw itself. Perhaps that approach could be
    applied everywhere in bitmapdevice...
    
    This older issue sums up this idea: https://bugs.chromium.org/p/skia/issues/detail?id=2122
    Will look into this general solution in a separate CL/effort.
    
    Bug: skia:7425
    Change-Id: I57f8da92df78127e6b8e42e422c43e50bb5748d6

--- src/core/SkRectPriv.h
+++ src/core/SkRectPriv.h
@@ -9,6 +9,7 @@
 #define SkRectPriv_DEFINED
 
 #include "SkRect.h"
+#include "SkMathPriv.h"
 
 class SkRectPriv {
 public:
@@ -44,6 +45,12 @@ public:
         r->fRight =  SkMaxScalar(pt.fX, r->fRight);
         r->fTop    = SkMinScalar(pt.fY, r->fTop);
         r->fBottom = SkMaxScalar(pt.fY, r->fBottom);
+    }
+
+    // conservative check. will return false for very large values that "could" fit
+    static bool FitsInFixed(const SkRect& r) {
+        return SkFitsInFixed(r.fLeft) && SkFitsInFixed(r.fTop) &&
+               SkFitsInFixed(r.fRight) && SkFitsInFixed(r.fBottom);
     }
 };
 
