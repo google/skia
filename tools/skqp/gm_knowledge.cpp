e5293f0307031187ab6e03232f7b41187346ee69
Hal Canary <halcanary@google.com>
2018-01-19 12:30:02 -0500

    SkQP: use bitmap.pixmap()
    Change-Id: I8e372f75a1a0b32db88808b0b4d7314c7fba9cda

--- tools/skqp/gm_knowledge.cpp
+++ tools/skqp/gm_knowledge.cpp
@@ -57,13 +57,6 @@ static float set_error_code(gmkb::Error* error_out, gmkb::Error error) {
     return FLT_MAX;
 }
 
-static SkPixmap to_pixmap(const SkBitmap& bitmap) {
-    SkPixmap pixmap;
-    SkAssertResult(bitmap.peekPixels(&pixmap));
-    return pixmap;
-}
-
-
 static bool WritePixmapToFile(const SkPixmap& pixmap, const char* path) {
     SkFILEWStream wStream(path);
     SkPngEncoder::Options options;
@@ -101,7 +94,7 @@ static SkBitmap ReadPngRgba8888FromFile(skqp::AssetManager* assetManager, const 
         SkImageInfo info = SkImageInfo::Make(size.width(), size.height(), kColorType, kAlphaType);
         bitmap.allocPixels(info);
         SkASSERT(bitmap.rowBytes() == (unsigned)bitmap.width() * sizeof(uint32_t));
-        if (SkCodec::kSuccess != codec->getPixels(to_pixmap(bitmap))) {
+        if (SkCodec::kSuccess != codec->getPixels(bitmap.pixmap())) {
             bitmap.reset();
         }
     }
@@ -194,7 +187,7 @@ float Check(const uint32_t* pixels,
             errors[i] = error > 0 ? 0xFF000000 + (unsigned)error : 0x00000000;
         }
         error_path = SkOSPath::Join(report_subdirectory.c_str(), PATH_ERR_PNG);
-        SkAssertResult(WritePixmapToFile(to_pixmap(errorBitmap), error_path.c_str()));
+        SkAssertResult(WritePixmapToFile(errorBitmap.pixmap(), error_path.c_str()));
 
         SkString report_path = SkOSPath::Join(report_subdirectory.c_str(), PATH_REPORT);
 
