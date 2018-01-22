d2ad4768ad6a13846a9169de2e6114cc2e2c650c
Greg Daniel <egdaniel@google.com>
2018-01-22 16:11:35 -0500

    Make use of VkSemaphores thread safe assuming use of the same VkQueue.
    This allows us to re-enable support for multiple GrContexts in
    GrBackendTextureImageGenerator.
    
    Bug: skia:
    Change-Id: Ifd6ac1ad81cdfbd1fd986467d8beb359399d6588

--- src/gpu/mtl/GrMtlGpu.h
+++ src/gpu/mtl/GrMtlGpu.h
@@ -67,6 +67,11 @@ public:
     sk_sp<GrSemaphore> SK_WARN_UNUSED_RESULT makeSemaphore(bool isOwned) override {
         return nullptr;
     }
+    sk_sp<GrSemaphore> wrapBackendSemaphore(const GrBackendSemaphore& semaphore,
+                                            GrResourceProvider::SemaphoreWrapType wrapType,
+                                            GrWrapOwnership ownership) override { return nullptr; }
+    void insertSemaphore(sk_sp<GrSemaphore> semaphore, bool flush) override {}
+    void waitSemaphore(sk_sp<GrSemaphore> semaphore) override {}
     sk_sp<GrSemaphore> prepareTextureForCrossContextUsage(GrTexture*) override { return nullptr; }
 
 private:
@@ -128,11 +133,6 @@ private:
     void onResolveRenderTarget(GrRenderTarget* target, GrSurfaceOrigin) override { return; }
 
     void onFinishFlush(bool insertedSemaphores) override {}
-
-    sk_sp<GrSemaphore> onWrapBackendSemaphore(const GrBackendSemaphore& semaphore,
-                                            GrWrapOwnership ownership) override { return nullptr; }
-    void onInsertSemaphore(sk_sp<GrSemaphore> semaphore, bool flush) override {}
-    void onWaitSemaphore(sk_sp<GrSemaphore> semaphore) override {}
 
     GrStencilAttachment* createStencilAttachmentForRenderTarget(const GrRenderTarget*,
                                                                 int width,
