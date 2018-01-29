52266100e3063146a22f843352fa6b524abae3f6
Mike Reed <reed@google.com>
2018-01-29 10:55:12 -0500

    validate that contours begin with moveTo
    Bug: skia:7507
    Change-Id: Iba6e99443c56402ac1417a094f08d346c6e5a1a8

--- src/core/SkPathRef.cpp
+++ src/core/SkPathRef.cpp
@@ -211,28 +211,36 @@ static bool deduce_pts_conics(const uint8_t verbs[], int vCount, int* ptCountPtr
     SkSafeMath safe;
     int ptCount = 0;
     int conicCount = 0;
+    bool needsMoveTo = true;            // set to true before each contour
+    bool invalidSequence = false;       // tracks that each contour begins with moveTo(s)
+
     for (int i = 0; i < vCount; ++i) {
         switch (verbs[i]) {
             case SkPath::kMove_Verb:
+                needsMoveTo = false;   // fall-through
             case SkPath::kLine_Verb:
+                invalidSequence |= needsMoveTo;
                 ptCount = safe.addInt(ptCount, 1);
                 break;
             case SkPath::kConic_Verb:
                 conicCount += 1;
                 // fall-through
             case SkPath::kQuad_Verb:
+                invalidSequence |= needsMoveTo;
                 ptCount = safe.addInt(ptCount, 2);
                 break;
             case SkPath::kCubic_Verb:
+                invalidSequence |= needsMoveTo;
                 ptCount = safe.addInt(ptCount, 3);
                 break;
             case SkPath::kClose_Verb:
+                needsMoveTo = true;  // now we need another moveTo before a line/quad/etc.
                 break;
             default:
                 return false;
         }
     }
-    if (!safe) {
+    if (!safe || invalidSequence) {
         return false;
     }
     *ptCountPtr = ptCount;
