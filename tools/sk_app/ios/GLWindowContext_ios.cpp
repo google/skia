b28cce2f24895bd93a91cb4107669a571792a08f
Brian Salomon <bsalomon@google.com>
2018-01-31 10:59:06 -0500

    start on redef of samp count
    Bug: skia:
    Change-Id: Ida22c6b22c1365e563c9046b611e88bf5eb3ff33

--- tools/sk_app/ios/GLWindowContext_ios.cpp
+++ tools/sk_app/ios/GLWindowContext_ios.cpp
@@ -67,6 +67,7 @@ sk_sp<const GrGLInterface> GLWindowContext_ios::onInitializeContext() {
 
         SDL_GL_GetAttribute(SDL_GL_STENCIL_SIZE, &fStencilBits);
         SDL_GL_GetAttribute(SDL_GL_MULTISAMPLESAMPLES, &fSampleCount);
+        fSampleCount = SkTMax(fSampleCount, 1);
 
         SDL_GL_GetDrawableSize(fWindow, &fWidth, &fHeight);
         glViewport(0, 0, fWidth, fHeight);
