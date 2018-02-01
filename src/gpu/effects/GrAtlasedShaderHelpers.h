1b561513b392c0c8e566490adc8fb40a8e9178ca
Chris Dalton <csmartdalton@google.com>
2018-02-01 10:20:41 -0700

    ccpr: Don't use flat interpolation when it is slow
    Bug: skia:
    Change-Id: I1bc087187541183fdbaa5f2b93e8b8d287ac8ef8

--- src/gpu/effects/GrAtlasedShaderHelpers.h
+++ src/gpu/effects/GrAtlasedShaderHelpers.h
@@ -20,6 +20,8 @@ static void append_index_uv_varyings(GrGLSLPrimitiveProcessor::EmitArgs& args,
                                      GrGLSLVarying *uv,
                                      GrGLSLVarying *texIdx,
                                      GrGLSLVarying *st) {
+    using Interpolation = GrGLSLVaryingHandler::Interpolation;
+
     // This extracts the texture index and texel coordinates from the same variable
     // Packing structure: texel coordinates are multiplied by 2 (or shifted left 1)
     //                    texture index is stored as lower bits of both x and y
@@ -40,11 +42,9 @@ static void append_index_uv_varyings(GrGLSLPrimitiveProcessor::EmitArgs& args,
     args.fVaryingHandler->addVarying("TextureCoords", uv);
     args.fVertBuilder->codeAppendf("%s = unormTexCoords * %s;", uv->vsOut(), atlasSizeInvName);
 
-    if (args.fShaderCaps->integerSupport()) {
-        args.fVaryingHandler->addFlatVarying("TexIndex", texIdx);
-    } else {
-        args.fVaryingHandler->addVarying("TexIndex", texIdx);
-    }
+    args.fVaryingHandler->addVarying("TexIndex", texIdx, args.fShaderCaps->integerSupport()
+                                                                 ? Interpolation::kMustBeFlat
+                                                                 : Interpolation::kCanBeFlat);
     args.fVertBuilder->codeAppendf("%s = texIdx;", texIdx->vsOut());
 
     if (st) {
