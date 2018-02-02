d902eec617d0622775bd6dbf558c4761fbbbe1d9
Robert Phillips <robertphillips@google.com>
2018-02-02 12:29:38 -0500

    Update LazyProxy creation to allow for a non-textureable result
    Change-Id: Ic284b4f4220afa714159e07f57798a94fd40d63a

--- src/gpu/GrTextureRenderTargetProxy.cpp
+++ src/gpu/GrTextureRenderTargetProxy.cpp
@@ -9,6 +9,7 @@
 
 #include "GrCaps.h"
 #include "GrTexture.h"
+#include "GrTexturePriv.h"
 #include "GrRenderTarget.h"
 #include "GrSurfaceProxyPriv.h"
 
@@ -104,10 +105,14 @@ sk_sp<GrSurface> GrTextureRenderTargetProxy::createSurface(
 }
 
 #ifdef SK_DEBUG
-void GrTextureRenderTargetProxy::validateLazyTexture(const GrTexture* texture) {
-    SkASSERT(texture->asRenderTarget());
-    SkASSERT(texture->asRenderTarget()->numStencilSamples() == this->numStencilSamples());
-    SkASSERT(GrMipMapped::kNo == this->mipMapped());
+void GrTextureRenderTargetProxy::validateLazySurface(const GrSurface* surface) {
+    // Anything checked here should also be checking the GrTextureProxy version
+    SkASSERT(surface->asTexture());
+    SkASSERT(surface->asTexture()->texturePriv().mipMapped() == this->mipMapped());
+
+    // Anything checked here should also be checking the GrRenderTargetProxy version
+    SkASSERT(surface->asRenderTarget());
+    SkASSERT(surface->asRenderTarget()->numStencilSamples() == this->numStencilSamples());
 }
 #endif
 
