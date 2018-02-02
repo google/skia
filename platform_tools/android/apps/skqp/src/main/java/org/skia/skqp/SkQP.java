394096c0afcd2f45c0da573cae45050d5f959ff6
Hal Canary <halcanary@google.com>
2018-02-02 15:03:59 -0500

    SkQP: refactor java code
    COMMIT=false
    
    Change-Id: I62c8d60ce3ea21c35d0cc6232ebf2003ade9458f

--- platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQP.java
+++ platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQP.java
@@ -13,6 +13,10 @@ import android.util.Log;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runner.Description;
+
 public class SkQP {
     protected native void nInit(AssetManager assetManager, String dataDir, boolean experimentalMode);
     protected native float nExecuteGM(int gm, int backend) throws SkQPException;
@@ -25,62 +29,125 @@ public class SkQP {
 
     protected static final String kSkiaGM = "skqp_";
     protected static final String kSkiaUnitTests = "skqp_unitTest";
-    protected static final String LOG_PREFIX = "org.skia.skqp";
+    protected static final String TAG = "org.skia.skqp";
 
     static {
       System.loadLibrary("skqp_app");
     }
 
+    protected void runUnitTest(RunNotifier notifier, int unitTest, Description desc) {
+        String utName = this.mUnitTests[unitTest];
+        Log.v(TAG, String.format("Test %s started", utName));
+        if (notifier != null) {
+            notifier.fireTestStarted(desc);
+        }
+        String[] errors = this.nExecuteUnitTest(unitTest);
+        if (errors != null && errors.length > 0) {
+            Log.w(TAG, String.format("[FAIL] Test %s had %d failures.", utName, errors.length));
+            for (String error : errors) {
+                SkQP.Fail(desc, notifier, error);
+                Log.w(TAG, String.format("[FAIL] %s: %s", utName, error));
+            }
+        } else {
+            Log.i(TAG, String.format("Test %s passed.", utName));
+        }
+        if (notifier != null) {
+            notifier.fireTestFinished(desc);
+        }
+    }
+
+    protected void runRenderTest(RunNotifier notifier, int backend, int gm, Description desc) {
+        String gmName = String.format("%s/%s", this.mBackends[backend], this.mGMs[gm]);
+        Log.v(TAG, String.format("Rendering Test %s started.", gmName));
+        if (notifier != null) {
+            notifier.fireTestStarted(desc);
+        }
+        float value = java.lang.Float.MAX_VALUE;
+        String error = null;
+        try {
+            value = this.nExecuteGM(gm, backend);
+        } catch (SkQPException exept) {
+            error = exept.getMessage();
+        }
+        if (error != null) {
+            SkQP.Fail(desc, notifier, String.format("Exception: %s", error));
+            Log.w(TAG, String.format("[ERROR] %s: %s", gmName, error));
+        } else if (value != 0) {
+            SkQP.Fail(desc, notifier, String.format(
+                        "Image mismatch: max channel diff = %f", value));
+            Log.w(TAG, String.format("[FAIL] %s: %f > 0", gmName, value));
+        } else {
+            Log.i(TAG, String.format("Rendering Test %s passed", gmName));
+        }
+        if (notifier != null) {
+            notifier.fireTestFinished(desc);
+        }
+    }
+
+    public void runSingleTest(RunNotifier notifier, Description desc) {
+        String classname = desc.getClassName();
+        String method = desc.getMethodName();
+        if (classname.equals(SkQP.kSkiaUnitTests)) {
+            for (int unitTest = 0; unitTest < this.mUnitTests.length; unitTest++) {
+                if (this.mUnitTests[unitTest].equals(method)) {
+                    this.runUnitTest(notifier, unitTest, desc);
+                    break;
+                }
+            }
+        } else {
+            for (int backend = 0; backend < this.mBackends.length; backend++) {
+                String backendClassname = SkQP.kSkiaGM + this.mBackends[backend];
+                if (backendClassname.equals(classname)) {
+                    for (int gm = 0; gm < this.mGMs.length; gm++) {
+                        if (this.mBackends[backend].equals(method)) {
+                            this.runRenderTest(notifier, backend, gm, desc);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        this.nMakeReport();
+    }
+
+    public void run(RunNotifier notifier) {
+        int numberOfTests = this.mUnitTests.length + this.mGMs.length * this.mBackends.length;
+        int testNumber = 1;
+        for (int backend = 0; backend < this.mBackends.length; backend++) {
+            String classname = SkQP.kSkiaGM + this.mBackends[backend];
+            for (int gm = 0; gm < this.mGMs.length; gm++) {
+                Log.v(TAG, String.format("Test %d of %d.", testNumber, numberOfTests));
+                testNumber++;
+                Description desc =
+                        Description.createTestDescription(classname, this.mGMs[gm]);
+                this.runRenderTest(notifier, backend, gm, desc);
+            }
+        }
+        for (int unitTest = 0; unitTest < this.mUnitTests.length; unitTest++) {
+            String utName = this.mUnitTests[unitTest];
+            Log.v(TAG, String.format("Test %d of %d.", testNumber, numberOfTests));
+            testNumber++;
+            Description desc = Description.createTestDescription(SkQP.kSkiaUnitTests, utName);
+            this.runSingleTest(notifier, desc);
+        }
+        this.nMakeReport();
+    }
+
     protected void runTests(Context context, String outputDirPath) {
-        Log.i(LOG_PREFIX, "Output Dir: " + outputDirPath);
+        Log.i(TAG, "Output Dir: " + outputDirPath);
         File outputDir = new File(outputDirPath);
         try {
             ensureEmtpyDirectory(outputDir);
         } catch (IOException e) {
-            Log.e(LOG_PREFIX, "ensureEmtpyDirectory:" + e.getMessage());
+            Log.e(TAG, "ensureEmtpyDirectory:" + e.getMessage());
         }
 
         // Note: nInit will initialize the mGMs, mBackends and mUnitTests fields.
         AssetManager assetManager = context.getResources().getAssets();
         this.nInit(assetManager, outputDirPath, true);
-
-        for (int backend = 0; backend < mBackends.length; backend++) {
-          String classname = kSkiaGM + mBackends[backend];
-          for (int gm = 0; gm < mGMs.length; gm++) {
-              String testName = kSkiaGM + mBackends[backend] + "_" +mGMs[gm];
-              float value = java.lang.Float.MAX_VALUE;
-              String error = null;
-              Log.w(LOG_PREFIX, "Running: " + testName);
-              try {
-                  value = this.nExecuteGM(gm, backend);
-              } catch (SkQPException exept) {
-                  error = exept.getMessage();
-              }
-              if (error != null) {
-                // Record error message and carry on.
-              } else if (value != 0) {
-                // Record failure and carry on.
-                  // SkQPRunner.Fail(desc, notifier, String.format(
-                  //             "Image mismatch: max channel diff = %f", value));
-              } else {
-                // Record success for this test.
-              }
-          }
-        }
-        for (int unitTest = 0; unitTest < mUnitTests.length; unitTest++) {
-            String testName = kSkiaUnitTests + "_" + mUnitTests[unitTest];
-            Log.w(LOG_PREFIX, "Running: " + testName);
-            String[] errors = this.nExecuteUnitTest(unitTest);
-            if (errors != null && errors.length > 0) {
-                for (String error : errors) {
-                  Log.w(LOG_PREFIX, "Error running " + testName + ":" + error);
-                }
-            } else {
-              Log.i(LOG_PREFIX, "Test: " + testName + " finished successfully.");
-            }
-        }
-        Log.i(LOG_PREFIX, "Finished running all tests.");
-        nMakeReport();
+        this.run(null);
+        Log.i(TAG, String.format("output written to \"%s\"", outputDirPath));
     }
 
     protected static void ensureEmtpyDirectory(File f) throws IOException {
@@ -102,5 +169,11 @@ public class SkQP {
         throw new IOException("Unable to delete:" + f.getAbsolutePath());
       }
     }
+    private static void Fail(Description desc, RunNotifier notifier, String failure) {
+        if (notifier != null) {
+            notifier.fireTestFailure(new Failure(desc, new Throwable(failure)));
+        }
+    }
+
 }
 
--- platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQPRunner.java
+++ platform_tools/android/apps/skqp/src/main/java/org/skia/skqp/SkQPRunner.java
@@ -14,22 +14,15 @@ import android.support.test.InstrumentationRegistry;
 import android.util.Log;
 import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
-import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
 @RunWith(SkQPRunner.class)
 public class SkQPRunner extends Runner {
     private Description mDescription;
     private SkQP impl;
-    private static final String TAG = SkQP.LOG_PREFIX;
-
-    private static void Fail(Description desc, RunNotifier notifier, String failure) {
-        notifier.fireTestFailure(new Failure(desc, new Throwable(failure)));
-    }
 
     private static File GetOutputDir() {
         Context c = InstrumentationRegistry.getTargetContext();
@@ -45,26 +38,25 @@ public class SkQPRunner extends Runner {
         try {
             SkQP.ensureEmtpyDirectory(filesDir);
         } catch (IOException e) {
-            Log.w(TAG, "ensureEmtpyDirectory: " + e.getMessage());
+            Log.w(SkQP.TAG, "ensureEmtpyDirectory: " + e.getMessage());
         }
-        Log.i(TAG, "output path = " + filesDir.getAbsolutePath());
+        Log.i(SkQP.TAG, "output path = " + filesDir.getAbsolutePath());
 
         Resources resources = InstrumentationRegistry.getTargetContext().getResources();
         AssetManager mAssetManager = resources.getAssets();
         impl.nInit(mAssetManager, filesDir.getAbsolutePath(), false);
 
         mDescription = Description.createSuiteDescription(testClass);
-        Annotation annots[] = new Annotation[0];
         for (int backend = 0; backend < impl.mBackends.length; backend++) {
             String classname = SkQP.kSkiaGM + impl.mBackends[backend];
             for (int gm = 0; gm < impl.mGMs.length; gm++) {
                 mDescription.addChild(
-                        Description.createTestDescription(classname, impl.mGMs[gm], annots));
+                        Description.createTestDescription(classname, impl.mGMs[gm]));
             }
         }
         for (int unitTest = 0; unitTest < impl.mUnitTests.length; unitTest++) {
             mDescription.addChild(Description.createTestDescription(SkQP.kSkiaUnitTests,
-                        impl.mUnitTests[unitTest], annots));
+                        impl.mUnitTests[unitTest]));
         }
     }
 
@@ -75,63 +67,9 @@ public class SkQPRunner extends Runner {
     public int testCount() {
         return impl.mUnitTests.length + impl.mGMs.length * impl.mBackends.length;
     }
-
     @Override
     public void run(RunNotifier notifier) {
-        int numberOfTests = this.testCount();
-        int testNumber = 1;
-        Annotation annots[] = new Annotation[0];
-        for (int backend = 0; backend < impl.mBackends.length; backend++) {
-            String classname = SkQP.kSkiaGM + impl.mBackends[backend];
-            for (int gm = 0; gm < impl.mGMs.length; gm++) {
-                String gmName = String.format("%s/%s", impl.mBackends[backend], impl.mGMs[gm]);
-                Log.v(TAG, String.format("Rendering Test %s started (%d/%d).",
-                                         gmName, testNumber, numberOfTests));
-                testNumber++;
-                Description desc =
-                        Description.createTestDescription(classname, impl.mGMs[gm], annots);
-                notifier.fireTestStarted(desc);
-                float value = java.lang.Float.MAX_VALUE;
-                String error = null;
-                try {
-                    value = impl.nExecuteGM(gm, backend);
-                } catch (SkQPException exept) {
-                    error = exept.getMessage();
-                }
-                if (error != null) {
-                    SkQPRunner.Fail(desc, notifier, String.format("Exception: %s", error));
-                    Log.w(TAG, String.format("[ERROR] %s: %s", gmName, error));
-                } else if (value != 0) {
-                    SkQPRunner.Fail(desc, notifier, String.format(
-                                "Image mismatch: max channel diff = %f", value));
-                    Log.w(TAG, String.format("[FAIL] %s: %f > 0", gmName, value));
-                } else {
-                    Log.i(TAG, String.format("Rendering Test %s passed", gmName));
-                }
-                notifier.fireTestFinished(desc);
-            }
-        }
-        for (int unitTest = 0; unitTest < impl.mUnitTests.length; unitTest++) {
-            String utName = impl.mUnitTests[unitTest];
-            Log.v(TAG, String.format("Test %s started (%d/%d).",
-                                     utName, testNumber, numberOfTests));
-            testNumber++;
-            Description desc = Description.createTestDescription(
-                          SkQP.kSkiaUnitTests, utName, annots);
-            notifier.fireTestStarted(desc);
-            String[] errors = impl.nExecuteUnitTest(unitTest);
-            if (errors != null && errors.length > 0) {
-                Log.w(TAG, String.format("[FAIL] Test %s had %d failures.", utName, errors.length));
-                for (String error : errors) {
-                    SkQPRunner.Fail(desc, notifier, error);
-                    Log.w(TAG, String.format("[FAIL] %s: %s", utName, error));
-                }
-            } else {
-                Log.i(TAG, String.format("Test %s passed.", utName));
-            }
-            notifier.fireTestFinished(desc);
-        }
-        impl.nMakeReport();
-        Log.i(TAG, String.format("output written to \"%s\"", GetOutputDir().getAbsolutePath()));
+        impl.run(notifier);
+        Log.i(SkQP.TAG, String.format("output written to \"%s\"", GetOutputDir().getAbsolutePath()));
     }
 }
