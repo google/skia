b6bfb536276fc07f43e276fec158a9a8cc329f53
Ravi Mistry <rmistry@google.com>
2018-02-09 07:49:24 -0500

    Use the right keys in trigger_wait_ac_task.py
    NoTry: true
    Bug: skia:7469
    Change-Id: I2a77574f35354f75e628968f943be54afa9e3bf9
    Reviewed-on: https://skia-review.googlesource.com/105981
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- infra/bots/android_compile/trigger_wait_ac_task.py
+++ infra/bots/android_compile/trigger_wait_ac_task.py
@@ -141,8 +141,8 @@ def TriggerAndWait(options):
                ' Android tree is currently broken. Marking this bot as '
                'successful')
         print
-        print 'With patch logs are here: %s' % ret["WithPatchLog"]
-        print 'No patch logs are here: %s' % ret["NoPatchLog"]
+        print 'With patch logs are here: %s' % ret["withpatch_log"]
+        print 'No patch logs are here: %s' % ret["nopatch_log"]
         return 0
 
     sys.stdout.write('.')
