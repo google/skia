4b924825a477a83c4b32830c3a1212130dd1c427
Robert Phillips <robertphillips@google.com>
2018-02-13 16:36:35 -0500

    Use a lazy proxy in OnFlushCallbackTest to fulfill some old TODOs
    Change-Id: I194e38c87c2f341f16a5048f02027165d7c28ae9

--- src/gpu/GrOnFlushResourceProvider.cpp
+++ src/gpu/GrOnFlushResourceProvider.cpp
@@ -53,8 +53,6 @@ sk_sp<GrRenderTargetContext> GrOnFlushResourceProvider::makeRenderTargetContext(
     return renderTargetContext;
 }
 
-// TODO: we only need this entry point as long as we have to pre-allocate the atlas.
-// Remove it ASAP.
 sk_sp<GrRenderTargetContext> GrOnFlushResourceProvider::makeRenderTargetContext(
                                                         sk_sp<GrSurfaceProxy> proxy,
                                                         sk_sp<SkColorSpace> colorSpace,
@@ -84,6 +82,10 @@ sk_sp<GrRenderTargetContext> GrOnFlushResourceProvider::makeRenderTargetContext(
 
 bool GrOnFlushResourceProvider::instatiateProxy(GrSurfaceProxy* proxy) {
     auto resourceProvider = fDrawingMgr->getContext()->contextPriv().resourceProvider();
+
+    if (GrSurfaceProxy::LazyState::kNot != proxy->lazyInstantiationState()) {
+        return proxy->priv().doLazyInstantiation(resourceProvider);
+    }
 
     return proxy->instantiate(resourceProvider);
 }
