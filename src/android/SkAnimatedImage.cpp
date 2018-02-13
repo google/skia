51b8ac28f56e23c90f6c2099c5223f448639fb46
Kevin Lubick <kjlubick@google.com>
2018-02-13 10:25:00 -0500

    Address MSAN bug in SkAnimatedImage::decodeNextFrame
    Bug: skia:
    Change-Id: I77537d6624e7d03fbbfe96046300a9dd7d609064

--- src/android/SkAnimatedImage.cpp
+++ src/android/SkAnimatedImage.cpp
@@ -176,6 +176,7 @@ int SkAnimatedImage::decodeNextFrame() {
             // Static image. This is okay.
             frameInfo.fRequiredFrame = SkCodec::kNone;
             frameInfo.fAlphaType = fCodec->getInfo().alphaType();
+            frameInfo.fDisposalMethod = SkCodecAnimation::DisposalMethod::kKeep;
             // These fields won't be read.
             frameInfo.fDuration = INT_MAX;
             frameInfo.fFullyReceived = true;
