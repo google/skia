50db093b97b7f48013b1da327e127bf70598d2f8
Ben Wagner <bungeman@google.com>
2018-02-15 10:20:04 -0500

    Add color fonts.
    Change-Id: Id2b7449048591892ff802484d5e3745a7e1402bb

--- tools/fonts/SkTestScalerContext.h
+++ tools/fonts/SkTestScalerContext.h
@@ -99,4 +99,89 @@ private:
     friend class SkTestScalerContext;
 };
 
+#include "SkTHash.h"
+class SkSVGDOM;
+
+struct SkSVGTestTypefaceGlyphData {
+    const char* fSvgResourcePath;
+    SkPoint fOrigin;
+    SkScalar fAdvance;
+    SkUnichar fUnicode; //TODO: this limits to 1:1
+};
+
+class SkSVGTestTypeface : public SkTypeface {
+public:
+    SkSVGTestTypeface(const char* name,
+                      int fUpem,
+                      const SkPaint::FontMetrics& metrics,
+                      const SkSVGTestTypefaceGlyphData* data, int dataCount,
+                      const SkFontStyle& style);
+    ~SkSVGTestTypeface() override;
+    void getAdvance(SkGlyph* glyph) const;
+    void getFontMetrics(SkPaint::FontMetrics* metrics) const;
+    void getMetrics(SkGlyph* glyph) const;
+    void getPath(SkGlyphID glyph, SkPath* path) const;
+
+    static sk_sp<SkSVGTestTypeface> Default();
+    void exportTtxCbdt(SkWStream*) const;
+    void exportTtxSbix(SkWStream*) const;
+    void exportTtxColr(SkWStream*) const;
+protected:
+    void exportTtxCommon(SkWStream*) const;
+
+    SkScalerContext* onCreateScalerContext(const SkScalerContextEffects&,
+                                           const SkDescriptor* desc) const override;
+    void onFilterRec(SkScalerContextRec* rec) const override;
+    std::unique_ptr<SkAdvancedTypefaceMetrics> onGetAdvancedMetrics() const override;
+
+    SkStreamAsset* onOpenStream(int* ttcIndex) const override {
+        return nullptr;
+    }
+
+    void onGetFontDescriptor(SkFontDescriptor* desc, bool* isLocal) const override;
+
+    int onCharsToGlyphs(const void* chars, Encoding encoding,
+                        uint16_t glyphs[], int glyphCount) const override;
+
+    int onCountGlyphs() const override {
+        return fGlyphs.count();
+    }
+
+    int onGetUPEM() const override {
+        return fUpem;
+    }
+
+    void onGetFamilyName(SkString* familyName) const override;
+    SkTypeface::LocalizedStrings* onCreateFamilyNameIterator() const override;
+
+    int onGetVariationDesignPosition(SkFontArguments::VariationPosition::Coordinate coordinates[],
+                                     int coordinateCount) const override
+    {
+        return 0;
+    }
+
+    int onGetTableTags(SkFontTableTag tags[]) const override {
+        return 0;
+    }
+
+    size_t onGetTableData(SkFontTableTag tag, size_t offset,
+                          size_t length, void* data) const override {
+        return 0;
+    }
+private:
+    struct Glyph {
+        Glyph(sk_sp<SkSVGDOM> svg, const SkSVGTestTypefaceGlyphData& data);
+        ~Glyph();
+        sk_sp<SkSVGDOM> fSvg;
+        SkPoint fOrigin;
+        SkScalar fAdvance;
+    };
+    SkString fName;
+    int fUpem;
+    const SkPaint::FontMetrics fFontMetrics;
+    SkTArray<Glyph> fGlyphs;
+    SkTHashMap<SkUnichar, SkGlyphID> fCMap;
+    friend class SkSVGTestScalerContext;
+};
+
 #endif
