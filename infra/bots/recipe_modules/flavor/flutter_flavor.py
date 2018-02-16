1701ecea23b05d66476de4358d4ff74fd19c3a67
Eric Boren <borenet@google.com>
2018-02-16 15:32:53 -0500

    [recipes] Expand depot_tools DEPS hack for flutter and PDFium builds
    Bug: skia:7609
    Change-Id: Iaaaa2390d87656a4fa40e44fc31c35573abec7b5
    Reviewed-on: https://skia-review.googlesource.com/108186
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/bots/recipe_modules/flavor/flutter_flavor.py
+++ infra/bots/recipe_modules/flavor/flutter_flavor.py
@@ -22,10 +22,17 @@ class FlutterFlavorUtils(default_flavor.DefaultFlavorUtils):
 
     with self.m.context(cwd=flutter_dir):
       # Runhook to generate the gn binary in buildtools.
+      # TODO(borenet): Remove this hack and replace with
+      # 'self.m.gclient.runhooks()' after the transition to Kitchen:
+      # https://bugs.chromium.org/p/skia/issues/detail?id=7050
+      depot_tools = self.m.vars.checkout_root.join('depot_tools')
+      self.m.git.checkout(
+          'https://chromium.googlesource.com/chromium/tools/depot_tools.git',
+          dir_path=depot_tools, ref='master')
       self.m.run(
           self.m.step,
           'runhook',
-          cmd=['gclient', 'runhooks'])
+          cmd=[depot_tools.join('gclient'), 'runhooks'])
 
       # Setup GN args.
       gn_args = [
