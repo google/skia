32f14a567d98206a20b447141482524b1e4d3a5a
Florin Malita <fmalita@chromium.org>
2018-02-20 10:04:20 -0500

    Delete SkottieSlide2
    Now superseded by the generic SlideDir.
    
    TBR=
    
    Change-Id: Ia9adfc54dcb86ebb0cd50d2e1f4739896279de8f

--- tools/viewer/SkottieSlide.h
+++ tools/viewer/SkottieSlide.h
@@ -38,41 +38,4 @@ private:
     typedef Slide INHERITED;
 };
 
-class SkottieSlide2 : public Slide {
-public:
-    SkottieSlide2(const SkString& path);
-    ~SkottieSlide2() override = default;
-
-    void load(SkScalar winWidth, SkScalar winHeight) override;
-    void unload() override;
-
-    SkISize getDimensions() const override;
-
-    void draw(SkCanvas*) override;
-    bool animate(const SkAnimTimer&) override;
-    bool onMouse(SkScalar x, SkScalar y, sk_app::Window::InputState, uint32_t modifiers) override;
-
-private:
-    class AnimationWrapper;
-
-    struct Rec {
-        sk_sp<AnimationWrapper>             fWrapper;
-        bool                                fShowAnimationInval = false;
-
-        explicit Rec(sk_sp<AnimationWrapper>);
-        Rec(Rec&& o);
-    };
-
-    int findCell(float x, float y) const;
-
-    SkString                     fPath;
-    SkTArray<Rec>                fAnims;
-    std::unique_ptr<sksg::Scene> fScene;
-
-    SkMSec                       fTimeBase = 0;
-    int                          fTrackingCell = -1;
-
-    typedef Slide INHERITED;
-};
-
 #endif // SkottieSlide_DEFINED
