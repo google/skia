b4a8a629406fd84c04ecbb1bd2301cfbff91121d
Hal Canary <halcanary@google.com>
2018-02-21 15:49:41 -0500

    SkFloatToDecimal: optimize the less common cases.
    bench PDFScalar_random goes from 120 ns to 70 ns.
    
    Change-Id: I6254f5c900395ee470ffee26303915025a8f0dda
    Reviewed-on: https://skia-review.googlesource.com/131151
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Auto-Submit: Hal Canary <halcanary@google.com>

--- src/utils/SkFloatToDecimal.cpp
+++ src/utils/SkFloatToDecimal.cpp
@@ -13,6 +13,22 @@
 
 #include "SkTypes.h"
 
+// returns `value * pow(base, e)`, assuming `e` is positive.
+static double pow_by_squaring(double value, double base, int e) {
+    // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+    SkASSERT(e > 0);
+    while (true) {
+        if (e & 1) {
+            value *= base;
+        }
+        e >>= 1;
+        if (0 == e) {
+            return value;
+        }
+        base *= base;
+    }
+}
+
 // Return pow(10.0, e), optimized for common cases.
 static double pow10(int e) {
     switch (e) {
@@ -34,14 +50,10 @@ static double pow10(int e) {
         case 15: return 1e+15;
         default:
             if (e > 15) {
-                double value = 1e+15;
-                while (e-- > 15) { value *= 10.0; }
-                return value;
+                return pow_by_squaring(1e+15, 10.0, e - 15);
             } else {
                 SkASSERT(e < 0);
-                double value = 1.0;
-                while (e++ < 0) { value /= 10.0; }
-                return value;
+                return pow_by_squaring(1.0, 0.1, -e);
             }
     }
 }
