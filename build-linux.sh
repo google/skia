ed04b06f5cc73e6e3cdd95e6875366fc66d04303
Adrian Biedrzycki <a@modules.io>
2018-02-21 19:50:08 -0800

    Add linux build script

---
+++ build-linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./tools/git-sync-deps
+./bin/gn gen out/Static --args='is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false'
+ninja -C out/Static
