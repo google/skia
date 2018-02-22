7ad124d335c61857a61fdbfea21f8d159b44f197
Avaer Kazmer <a@modules.io>
2018-02-21 20:08:11 -0500

    Clean up windows build script

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,3 +1,3 @@
-tools\git-sync-deps
-bin\gn gen out\Static --args="is_official_build=true skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
-ninja -C out/Static
+python .\tools\git-sync-deps
+.\bin\gn.exe gen out\Static --args="is_official_build=true skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
+ninja -C out\Static
