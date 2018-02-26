e01e4e36310ed34a42b7ef66d595df16d6385091
Brian Salomon <bsalomon@google.com>
2018-02-26 10:43:26 -0500

    Start on new GPU pixel ops implementation.
    The new ops attempt to be less optimal by falling back to CPU conversions
    rather than relying on intermediate draws and complex coordination between
    GrContext and GrGpu to determine how conversions are performed.
    
    This adds the new writePixels implementation.
    
    Change-Id: I7496d86d5a40277ed2ca63668881c160e54d80d3

--- include/gpu/GrCaps.h
+++ include/gpu/GrCaps.h
@@ -185,6 +185,23 @@ public:
         return this->getRenderTargetSampleCount(requestedCount, config);
     }
 
+    /**
+     * Some backends have restrictions on what types of render targets for which
+     * GrGpu::writePixels() will succeed. If this returns false then the caller should implement a
+     * fallback where a temporary texture is created, pixels are written to it, and then that is
+     * copied or drawn into the the render target.
+     */
+    virtual bool renderTargetWritePixelsSupported(bool isAlsoTexture, int sampleCnt) const = 0;
+
+    /**
+     * Given a dst pixel config and a src color type what color type must the caller coax the
+     * the data into in order to use GrGpu::writePixels().
+     */
+    virtual GrColorType supportedWritePixelsColorType(GrPixelConfig config,
+                                                      GrColorType /*srcColorType*/) const {
+        return GrPixelConfigToColorType(config);
+    }
+
     bool suppressPrints() const { return fSuppressPrints; }
 
     size_t bufferMapThreshold() const {
