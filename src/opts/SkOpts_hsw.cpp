53b095975842dbf22d5a0e14070e548e75fa743f
Mike Klein <mtklein@chromium.org>
2018-02-27 10:37:40 -0500

    make SkJumper stages normal Skia code
    Enough clients are using Clang now that we can say, use Clang to build
    if you want these software pipeline stages to go fast.
    
    This lets us drop the offline build aspect of SkJumper stages, instead
    building as part of Skia using the SkOpts framework.
    
    Change-Id: Iaaa792ffe4deab3508d2dc5d0008c163c24b3383

---
+++ src/opts/SkOpts_hsw.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkOpts.h"
+
+#define SK_OPTS_NS hsw
+#include "SkUtils_opts.h"
+
+namespace SkOpts {
+    void Init_hsw() {
+    }
+}
