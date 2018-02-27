db1e5c6474c1cd0969006a0c0554f8fdc63c42c8
Kevin Lubick <kjlubick@google.com>
2018-02-27 08:30:43 -0500

    Port 3 API fuzzers to be oss-fuzz friendly
    Also Remove ScaleToSides, which we hadn't been running for a while.
    
    Bug: skia:
    Change-Id: I772dad722c34681392d5b635b3de716f3b00d597
    Reviewed-on: https://skia-review.googlesource.com/110443
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

---
+++ fuzz/oss_fuzz/FuzzAPIImageFilter.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "../Fuzz.h"
+
+void fuzz_ImageFilter(Fuzz* f);
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    auto fuzz = Fuzz(SkData::MakeWithoutCopy(data, size));
+    fuzz_ImageFilter(&fuzz);
+    return 0;
+}
