b601013223cf37a018226371534a74400537c166
Mike Klein <mtklein@chromium.org>
2018-02-27 10:37:40 -0500

    make SkJumper stages normal Skia code
    Enough clients are using Clang now that we can say, use Clang to build
    if you want these software pipeline stages to go fast.
    
    This lets us drop the offline build aspect of SkJumper stages, instead
    building as part of Skia using the SkOpts framework.
    
    I think everything should work, except I've (temporarily) removed
    AVX-512 support.  I will put this back in a follow up.
    
    I have had to drop Windows down to __vectorcall and our narrower
    stage calling convention that keeps the d-registers on the stack.
    I tried forcing sysv_abi, but that crashed Clang.  :/
    
    Added a TODO to up the same narrower stage calling convention
    for lowp stages... we just *don't* today, for no good reason.
    
    Change-Id: Iaaa792ffe4deab3508d2dc5d0008c163c24b3383

--- src/opts/SkOpts_sse41.cpp
+++ src/opts/SkOpts_sse41.cpp
@@ -8,10 +8,23 @@
 #include "SkOpts.h"
 
 #define SK_OPTS_NS sse41
+#include "SkRasterPipeline_opts.h"
 #include "SkBlitRow_opts.h"
 
 namespace SkOpts {
     void Init_sse41() {
         blit_row_s32a_opaque = sse41::blit_row_s32a_opaque;
+
+    #define M(st) stages_highp[SkRasterPipeline::st] = (StageFn)SK_OPTS_NS::st;
+        SK_RASTER_PIPELINE_STAGES(M)
+        just_return_highp = (StageFn)SK_OPTS_NS::just_return;
+        start_pipeline_highp = SK_OPTS_NS::start_pipeline;
+    #undef M
+
+    #define M(st) stages_lowp[SkRasterPipeline::st] = (StageFn)SK_OPTS_NS::lowp::st;
+        SK_RASTER_PIPELINE_STAGES(M)
+        just_return_lowp = (StageFn)SK_OPTS_NS::lowp::just_return;
+        start_pipeline_lowp = SK_OPTS_NS::lowp::start_pipeline;
+    #undef M
     }
 }
