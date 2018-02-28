d284e9a0388fb8ca86047340e0dfbdb487e97442
Hal Canary <halcanary@google.com>
2018-02-28 14:10:42 -0500

    SkPDF: encode metadata strings correctly
    BUG=skia:7669
    Change-Id: I3a90a2406854cc9bcfdd299e09ae3d6e610f2cc7

--- tests/PDFDocumentTest.cpp
+++ tests/PDFDocumentTest.cpp
@@ -195,3 +195,30 @@ DEF_TEST(SkPDF_pdfa_document, r) {
         }
     }
 }
+
+
+DEF_TEST(SkPDF_unicode_metadata, r) {
+    REQUIRE_PDF_DOCUMENT(SkPDF_unicode_metadata, r);
+    SkDocument::PDFMetadata pdfMetadata;
+    pdfMetadata.fTitle   = "𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙"; // Out of basic multilingual plane
+    pdfMetadata.fAuthor  = "ABCDEFGHIJ"; // ASCII
+    pdfMetadata.fSubject = "αβγδεζηθικ"; // inside  basic multilingual plane
+    pdfMetadata.fPDFA = true;
+    SkDynamicMemoryWStream wStream;
+    {
+        auto doc = SkDocument::MakePDF(&wStream, pdfMetadata);
+        doc->beginPage(612, 792)->drawColor(SK_ColorCYAN);
+    }
+    sk_sp<SkData> data(wStream.detachAsData());
+    static const char* expectations[] = {
+        "<</Title <FEFFD835DCD0D835DCD1D835DCD2D835DCD3D835DCD4D835DCD5D835DCD"
+            "6D835DCD7D835DCD8D835DCD9>",
+        "/Author (ABCDEFGHIJ)",
+        "Subject <FEFF03B103B203B303B403B503B603B703B803B903BA>",
+    };
+    for (const char* expectation : expectations) {
+        if (!contains(data->bytes(), data->size(), expectation)) {
+            ERRORF(r, "PDF expectation missing: '%s'.", expectation);
+        }
+    }
+}
