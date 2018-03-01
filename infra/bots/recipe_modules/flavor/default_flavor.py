89dfa891b8b43780c19e14fa635b128438e821f4
Stephan Altmueller <stephana@google.com>
2018-03-01 16:41:09 -0500

    master wip: skqp firebase and upload
    Bug: skia:
    Change-Id: I1bf265f46c99360eb3a9eb684886f93de48085fe

--- infra/bots/recipe_modules/flavor/default_flavor.py
+++ infra/bots/recipe_modules/flavor/default_flavor.py
@@ -157,3 +157,10 @@ class DefaultFlavorUtils(object):
   def cleanup_steps(self):
     """Run any device-specific cleanup steps."""
     pass
+
+  def skqp_model_exists(self):
+    """Returns true the hash of the SKQP model exists"""
+    return False
+    # p = 'platform_tools/android/apps/skqp/src/main/assets/files.checksum'
+    # model_path = self.m.skia_dir.join(*p.split('/'))
+    # self.m.path.exists(model_path)
