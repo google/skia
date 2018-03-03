71cf67ddc01331aa09a70d9c20184d389302d56e
Mike Reed <mike@reedtribe.org>
2018-03-03 12:30:17 -0500

    SkDArray to reduce size of SkTDArray
    move impl into c file
    
    Bug: skia:
    Change-Id: I19e59ea63ab6cec04292234923d2768b535e5794

--- src/core/SkAnnotation.cpp
+++ src/core/SkAnnotation.cpp
@@ -46,3 +46,95 @@ void SkAnnotateLinkToDestination(SkCanvas* canvas, const SkRect& rect, SkData* n
     }
     canvas->drawAnnotation(rect, SkAnnotationKeys::Link_Named_Dest_Key(), name);
 }
+
+////////////////////////////
+#include "SkDArray.h"
+
+SkDArray::SkDArray(const void* src, size_t elemSize, int count) : fElemSize(elemSize) {
+    SkASSERT(elemSize);
+    SkASSERT(src || count == 0);
+
+    fReserve = fCount = 0;
+    fArray = nullptr;
+    if (count) {
+        this->setCount(count);
+        memcpy(fArray, src, this->countBytes());
+    }
+}
+
+SkDArray::SkDArray(const SkDArray& src)
+: fArray(nullptr), fElemSize(src.fElemSize), fReserve(0), fCount(0) {
+    SkDArray tmp(src.fArray, src.fElemSize, src.fCount);
+    this->swap(tmp); // use move
+}
+SkDArray::SkDArray(SkDArray&& src)
+: fArray(nullptr), fElemSize(src.fElemSize), fReserve(0), fCount(0) {
+    this->swap(src);
+}
+
+SkDArray& SkDArray::operator=(const SkDArray& src) {
+    if (this != &src) {
+        if (src.countBytes() > this->reserveBytes()) {
+            SkDArray tmp(src.fArray, src.fElemSize, src.fCount);
+            this->swap(tmp);
+        } else {
+            sk_careful_memcpy(fArray, src.fArray, src.countBytes());
+            fElemSize = src.fElemSize;
+            fCount = src.fCount;
+        }
+    }
+    return *this;
+}
+SkDArray& SkDArray::operator=(SkDArray&& src) {
+    if (this != &src) {
+        this->swap(src);
+        src.reset();
+    }
+    return *this;
+}
+
+void* SkDArray::append(int count, const void* src) {
+    int oldCount = fCount;
+    if (count)  {
+        SkASSERT(src == nullptr || fArray == nullptr ||
+                 (const char*)src + count * fElemSize <= (const char*)fArray ||
+                 (const char*)fArray + oldCount * fElemSize <= (const char*)src);
+
+        this->adjustCount(count);
+        if (src) {
+            memcpy((char*)fArray + oldCount * fElemSize, src, fElemSize * count);
+        }
+    }
+    return (char*)fArray + oldCount * fElemSize;
+}
+
+void* SkDArray::insert(int index, int count, const void* src) {
+    SkASSERT(count);
+    SkASSERT(index <= fCount);
+    size_t oldCount = fCount;
+    this->adjustCount(count);
+    char* dst = (char*)fArray + index * fElemSize;
+    memmove(dst + count * fElemSize, dst, fElemSize * (oldCount - index));
+    if (src) {
+        memcpy(dst, src, fElemSize * count);
+    }
+    return dst;
+}
+
+void SkDArray::remove(int index, int count) {
+    SkASSERT(index + count <= fCount);
+    fCount = fCount - count;
+    memmove((char*)fArray + index * fElemSize,
+            (const char*)fArray + (index + count) * fElemSize,
+            fElemSize * (fCount - index));
+}
+
+void SkDArray::removeShuffle(int index, int count) {
+    SkASSERT(index < fCount);
+    int newCount = fCount - count;
+    fCount = newCount;
+    if (index != newCount) {
+        memcpy((char*)fArray + index * fElemSize, (const char*)fArray + newCount * fElemSize,
+               fElemSize);
+    }
+}
