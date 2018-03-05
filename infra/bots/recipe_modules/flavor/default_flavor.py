02916f7a7addfc20c031c5f3a6cc4847aad012c7
Stephan Altmueller <stephana@google.com>
2018-03-05 16:46:00 -0500

    DO NOT LAND: Dev branch test SKQP on Firebase Testlab and Upload
    No-Tree-Checks: true
    No-Try: true
    No-Presubmit: true
    Change-Id: I4fc9be03afac514c60f4c056292084a0f9879be8

--- infra/bots/recipe_modules/flavor/default_flavor.py
+++ infra/bots/recipe_modules/flavor/default_flavor.py
@@ -157,3 +157,10 @@ class DefaultFlavorUtils(object):
   def cleanup_steps(self):
     """Run any device-specific cleanup steps."""
     pass
+
+  def skqp_model_exists(self):
+    """Returns true the hash of the SKQP model exists"""
+    p = 'platform_tools/android/apps/skqp/src/main/assets/files.checksum'
+    model_path = self.m.vars.slave_dir.join('skia', *p.split("/"))
+    raise Exception(model_path)
+    return self.m.path.exists(model_path)
