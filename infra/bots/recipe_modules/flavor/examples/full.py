bfd8ca246548960c64f3b2bdb86996709614648b
Stephan Altmueller <stephana@google.com>
2018-03-06 10:31:03 -0500

    Test SKQP on Firebase Testlab and Upload
    - adds building the testlab driver (run_testlab) as a separate step
    - adds gcloud isolate necessary to run testlab
    - adds Testlab support and uploading a verified APK to GCS (with
      meta data attached).
    
    Bug: skia:
    Change-Id: I1bf265f46c99360eb3a9eb684886f93de48085fe

--- infra/bots/recipe_modules/flavor/examples/full.py
+++ infra/bots/recipe_modules/flavor/examples/full.py
@@ -5,8 +5,12 @@
 
 DEPS = [
   'flavor',
+  'recipe_engine/file',
+  'recipe_engine/path',
   'recipe_engine/properties',
+  'recipe_engine/python',
   'recipe_engine/raw_io',
+  'recipe_engine/step',
   'run',
   'vars',
 ]
@@ -61,6 +65,7 @@ TEST_BUILDERS = [
   'Build-Debian9-Clang-arm-Release-Chromebook_GLES',
   'Build-Debian9-Clang-arm-Release-Android_ASAN',
   'Build-Debian9-Clang-arm64-Release-Android_ASAN',
+  'Build-Debian9-Clang-universal-devrel-Android_SKQP',
   'Build-Debian9-Clang-x86_64-Debug-Chromebook_GLES',
   'Build-Debian9-Clang-x86_64-Debug-SK_USE_DISCARDABLE_SCALEDIMAGECACHE',
   'Build-Debian9-Clang-x86_64-Release-Fast',
@@ -104,6 +109,7 @@ TEST_BUILDERS = [
   'Test-Android-Clang-Nexus7-CPU-Tegra3-arm-Release-All-Android',
   'Test-Android-Clang-Pixel-GPU-Adreno530-arm64-Debug-All-Android',
   'Test-ChromeOS-Clang-SamsungChromebookPlus-GPU-MaliT860-arm-Release-All',
+  'Test-Debian9-Clang-GCE-CPU-AVX2-universal-devrel-All-Android_SKQP',
   'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-Coverage',
   'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Release-All-TSAN',
   'Test-Debian9-GCC-GCE-CPU-AVX2-x86_64-Release-All',
