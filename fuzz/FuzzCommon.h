f061821a72e507d7bec12eedc1aa60ea83860a4e
Cary Clark <caryclark@skia.org>
2018-03-09 10:33:48 -0500

    pathmeasure fuzzer
    R=kjlubick@google.com, reed@google.com
    Bug: skia:
    Change-Id: I16a8b09312e5d1d1783bd6a4b791636ad8f63889

--- fuzz/FuzzCommon.h
+++ fuzz/FuzzCommon.h
@@ -86,3 +86,50 @@ inline void Fuzz::next(SkRegion* region) {
         }
     }
 }
+
+static void build_path(Fuzz* fuzz,
+               SkPath* path,
+               int last_verb) {
+  while (!fuzz->exhausted()) {
+    // Use a uint8_t to conserve bytes.  This makes our "fuzzed bytes footprint"
+    // smaller, which leads to more efficient fuzzing.
+    uint8_t operation;
+    fuzz->next(&operation);
+    SkScalar a,b,c,d,e,f;
+
+    switch (operation % (last_verb + 1)) {
+      case SkPath::Verb::kMove_Verb:
+        fuzz->next(&a, &b);
+        path->moveTo(a, b);
+        break;
+
+      case SkPath::Verb::kLine_Verb:
+        fuzz->next(&a, &b);
+        path->lineTo(a, b);
+        break;
+
+      case SkPath::Verb::kQuad_Verb:
+        fuzz->next(&a, &b, &c, &d);
+        path->quadTo(a, b, c, d);
+        break;
+
+      case SkPath::Verb::kConic_Verb:
+        fuzz->next(&a, &b, &c, &d, &e);
+        path->conicTo(a, b, c, d, e);
+        break;
+
+      case SkPath::Verb::kCubic_Verb:
+        fuzz->next(&a, &b, &c, &d, &e, &f);
+        path->cubicTo(a, b, c, d, e, f);
+        break;
+
+      case SkPath::Verb::kClose_Verb:
+        path->close();
+        break;
+
+      case SkPath::Verb::kDone_Verb:
+        // In this case, simply exit.
+        return;
+    }
+  }
+}
