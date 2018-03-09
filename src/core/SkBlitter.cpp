2ac541e66ce1afe174b31c24705ca47891816078
Yuqian Li <liyuqian@google.com>
2018-03-09 21:36:52 +0000

    Revert "Check the boundary while cumulating deltas in a mask"
    This reverts commit d9779bcf93c2e11699230a54214309732b9161a7.
    
    Reason for revert: breaks gold
    
    Original change's description:
    > Check the boundary while cumulating deltas in a mask
    > 
    > Without the fix,
    > 
    > ./out/Debug/dm --config t8888 -m complexclip_bw
    > 
    > will trigger the assert.
    > 
    > Note that this boundary check is already in place when deltas are
    > cumulated without using a mask.
    > 
    > Bug: skia:
    > Change-Id: I68bfe2b7196d440743d3a7535d097bb8ec7689c3
    > Reviewed-on: https://skia-review.googlesource.com/113210
    > Commit-Queue: Yuqian Li <liyuqian@google.com>
    > Reviewed-by: Cary Clark <caryclark@skia.org>
    
    TBR=liyuqian@google.com,reed@google.com,caryclark@skia.org
    
    Change-Id: Ice0e3618b02ef48b0f2a9f0662a12f76078e984a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:
    Reviewed-on: https://skia-review.googlesource.com/113170
    Reviewed-by: Yuqian Li <liyuqian@google.com>
    Commit-Queue: Yuqian Li <liyuqian@google.com>

--- src/core/SkBlitter.cpp
+++ src/core/SkBlitter.cpp
@@ -113,12 +113,7 @@ void SkBlitter::blitCoverageDeltas(SkCoverageDeltaList* deltas, const SkIRect& c
             SkIRect rowIR = SkIRect::MakeLTRB(clip.fLeft, y, clip.fRight, y + 1);
             SkSTArenaAlloc<SkCoverageDeltaMask::MAX_SIZE> alloc;
             SkCoverageDeltaMask mask(&alloc, rowIR);
-
-            int i = 0;
-            // skip deltas with x less than clip.fLeft; they must be precision errors
-            for(; i < deltas->count(y) && deltas->getDelta(y, i).fX < clip.fLeft; ++i)
-                ;
-            for(; i < deltas->count(y) && deltas->getDelta(y, i).fX < clip.fRight; ++i) {
+            for(int i = 0; i < deltas->count(y); ++i) {
                 const SkCoverageDelta& delta = deltas->getDelta(y, i);
                 mask.addDelta(delta.fX, y, delta.fDelta);
             }
@@ -135,8 +130,7 @@ void SkBlitter::blitCoverageDeltas(SkCoverageDeltaList* deltas, const SkIRect& c
         SkFixed coverage = 0;       // init coverage to 0
 
         // skip deltas with x less than clip.fLeft; they must be precision errors
-        for(; i < deltas->count(y) && deltas->getDelta(y, i).fX < clip.fLeft; ++i)
-            ;
+        for(; i < deltas->count(y) && deltas->getDelta(y, i).fX < clip.fLeft; ++i);
         for(; i < deltas->count(y) && deltas->getDelta(y, i).fX < clip.fRight; ++i) {
             const SkCoverageDelta& delta = deltas->getDelta(y, i);
             SkASSERT(delta.fX >= lastX);    // delta must be x sorted
