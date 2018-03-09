d830d872f781911253cf14ca16f6ceca3dd0cb1a
Cary Clark <caryclark@skia.org>
2018-03-09 11:14:20 -0500

    pathmeasure fuzzer
    R=kjlubick@google.com, reed@google.com
    Bug: skia:
    Change-Id: I16a8b09312e5d1d1783bd6a4b791636ad8f63889

---
+++ fuzz/FuzzPathMeasure.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "Fuzz.h"
+#include "FuzzCommon.h"
+#include "SkPathMeasure.h"
+
+DEF_FUZZ(PathMeasure, fuzz) {
+    SkScalar resScale;
+    fuzz->next(&resScale);
+    uint8_t bits;
+    fuzz->next(&bits);
+    SkScalar distance[6];
+    for (auto index = 0; index < 6; ++index) {
+        fuzz->next(&distance[index]);
+    }
+    SkPath path;
+    BuildPath(fuzz, &path, SkPath::Verb::kDone_Verb);
+    SkPathMeasure measure(path, bits & 1, resScale);
+    SkPoint position;
+    SkVector tangent;
+    (void) measure.getPosTan(distance[0], &position, &tangent);
+    SkPath dst;
+    (void) measure.getSegment(distance[1], distance[2], &dst, (bits >> 1) & 1);
+    (void) measure.nextContour();
+    (void) measure.getPosTan(distance[3], &position, &tangent);
+    (void) measure.getSegment(distance[4], distance[5], &dst, (bits >> 2) & 1);
+}
