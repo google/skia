24f25dc8bca79fc8bb6a4676b2b341bd17fd77ab
Herb Derby <herb@google.com>
2018-03-12 13:56:21 -0400

    Move ComputeAxisAlignmentForHText to Rec.
    BUG=skia:7515
    
    Change-Id: I81d7950e508e0893f3591cc07596c10e8aaa636a
    Reviewed-on: https://skia-review.googlesource.com/113749
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- src/core/SkScalerContext.h
+++ src/core/SkScalerContext.h
@@ -56,7 +56,6 @@ enum SkAxisAlignment {
  *  than a nested struct inside SkScalerContext (where it started).
  */
 struct SkScalerContextRec {
-
     uint32_t    fFontID;
     SkScalar    fTextSize, fPreScaleX, fPreSkewX;
     SkScalar    fPost2x2[2][2];
@@ -136,6 +135,8 @@ public:
                    fPost2x2[0][1], fPost2x2[1][0], fPost2x2[1][1]);
         msg.appendf("  frame %g miter %g format %d join %d cap %d flags %#hx\n",
                    fFrameWidth, fMiterLimit, fMaskFormat, fStrokeJoin, fStrokeCap, fFlags);
+        msg.appendf("  lum bits %x, device gamma %d, paint gamma %d contrast %d\n",
+                    fLumBits, fDeviceGamma, fPaintGamma, fContrast);
         return msg;
     }
 
@@ -187,6 +188,8 @@ public:
                          SkMatrix* remainingWithoutRotation = nullptr,
                          SkMatrix* remainingRotation = nullptr,
                          SkMatrix* total = nullptr);
+
+    SkAxisAlignment computeAxisAlignmentForHText() const;
 
     inline SkPaint::Hinting getHinting() const;
     inline void setHinting(SkPaint::Hinting);
@@ -338,7 +341,7 @@ public:
     *  Return the axis (if any) that the baseline for horizontal text should land on.
     *  As an example, the identity matrix will return kX_SkAxisAlignment
     */
-    SkAxisAlignment computeAxisAlignmentForHText();
+    SkAxisAlignment computeAxisAlignmentForHText() const;
 
     static SkDescriptor* CreateDescriptorAndEffectsUsingPaint(
         const SkPaint& paint, const SkSurfaceProps* surfaceProps,
