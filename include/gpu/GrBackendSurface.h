906e73fe80063b2d1f1ecf5dce52a68f48d711a7
Brian Salomon <bsalomon@google.com>
2018-03-14 14:50:39 -0400

    Cleanup SkImage YUV texture factories
    Change-Id: Ic1bbf5f2817cebab938b4f31000126a6ab5c44d5

--- include/gpu/GrBackendSurface.h
+++ include/gpu/GrBackendSurface.h
@@ -146,6 +146,7 @@ public:
 private:
     // Friending for access to the GrPixelConfig
     friend class SkImage;
+    friend class SkImage_Gpu;
     friend class SkSurface;
     friend class GrBackendTextureImageGenerator;
     friend class GrProxyProvider;
@@ -153,6 +154,7 @@ private:
     friend class GrGLGpu;
     friend class GrVkGpu;
     friend class PromiseImageHelper;
+
     GrPixelConfig config() const { return fConfig; }
 
     int fWidth;         //<! width in pixels
