dad74845bd0f4bc46f415e77f2d629549ec18546
Robert Phillips <robertphillips@google.com>
2018-03-15 08:01:22 -0400

    Remove SK_RASTER_RECORDER_IMPLEMENTATION
    Change-Id: I9608ce5ce2b9d38c5e356d8428a8ff05df6ccda7

--- src/core/SkDeferredDisplayList.cpp
+++ src/core/SkDeferredDisplayList.cpp
@@ -10,19 +10,8 @@
 #include "SkCanvas.h"
 #include "SkSurface.h"
 
-#ifndef SK_RASTER_RECORDER_IMPLEMENTATION
 SkDeferredDisplayList::SkDeferredDisplayList(const SkSurfaceCharacterization& characterization,
                                              sk_sp<LazyProxyData> lazyProxyData)
         : fCharacterization(characterization)
         , fLazyProxyData(std::move(lazyProxyData)) {
 }
-#endif
-
-#ifdef SK_RASTER_RECORDER_IMPLEMENTATION
-// Placeholder. Ultimately, the SkSurface_Gpu will pass the wrapped opLists to its
-// renderTargetContext.
-bool SkDeferredDisplayList::draw(SkSurface* surface) const {
-    surface->getCanvas()->drawImage(fImage.get(), 0, 0);
-    return true;
-}
-#endif
