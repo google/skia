1207e581116fea534a546a9f41c45e833c505d0c
Cary Clark <caryclark@skia.org>
2018-03-21 13:15:30 -0400

    wip looking for flutter picture bug
    wip looking for flutter picture bug
    
    Bug: skia:
    Change-Id: I615e2c15e4d8b54860bfcc712ab2a4f9731dece8

--- gm/bug6643.cpp
+++ gm/bug6643.cpp
@@ -27,3 +27,68 @@ DEF_SIMPLE_GM(bug6643, canvas, 200, 200) {
     canvas->drawColor(SK_ColorWHITE);
     canvas->drawPaint(p);
 }
+
+#include "SkTextBlob.h"
+
+static sk_sp<SkTextBlob> makeBlob() {
+    SkTextBlobBuilder textBlobBuilder;
+    const char bunny[] = "/(^x^)\\";
+    const int len = sizeof(bunny) - 1;
+    uint16_t glyphs[len];
+    SkPaint paint;
+    paint.textToGlyphs(bunny, len, glyphs);
+    paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
+    int runs[] = { 3, 1, 3 };
+    SkPoint textPos = { 20, 100 };
+    int glyphIndex = 0;
+    for (auto runLen : runs) {
+        paint.setTextSize(1 == runLen ? 20 : 50);
+        const SkTextBlobBuilder::RunBuffer& run = 
+                textBlobBuilder.allocRun(paint, runLen, textPos.fX, textPos.fY);
+        memcpy(run.glyphs, &glyphs[glyphIndex], sizeof(glyphs[0]) * runLen);
+        textPos.fX += paint.measureText(&glyphs[glyphIndex], sizeof(glyphs[0]) * runLen, nullptr);
+        glyphIndex += runLen;
+    }
+    return textBlobBuilder.make();
+}
+
+static void drawBlob(SkCanvas* canvas, sk_sp<SkTextBlob> blob) {
+    SkPaint paint;
+    canvas->drawTextBlob(blob.get(), 10, 10, paint);
+}
+
+static void drawXform(SkCanvas*canvas) {
+    sk_sp<SkTextBlob> blob = makeBlob();
+    drawBlob(canvas, blob);
+    canvas->save();
+    canvas->scale(1.2f, 1.2f);
+    canvas->translate(150, 0);
+    drawBlob(canvas, blob);
+    canvas->save();
+    canvas->scale(1.2f, 1.2f);
+    canvas->translate(150, 0);
+    drawBlob(canvas, blob);
+    canvas->restore();
+    canvas->restore();
+}
+
+DEF_SIMPLE_GM(flutterbug, canvas, 200, 200) {
+    drawXform(canvas);
+    SkPictureRecorder recorder;
+    SkCanvas* rCanvas = recorder.beginRecording(200, 200);
+    SkPaint p;
+    rCanvas->scale(1.2f, 1.2f);
+    drawXform(rCanvas);
+    sk_sp<SkPicture> picture = recorder.finishRecordingAsPicture();
+    canvas->translate(0, 100);
+    canvas->drawPicture(picture);
+    canvas->translate(0, 120);
+    canvas->scale(1.2f, 1.2f);
+    drawXform(canvas);
+    canvas->scale(1.2f, 1.2f);
+    rCanvas->scale(1.2f, 1.2f);
+    canvas->translate(0, 120);
+    drawXform(canvas);
+    canvas->translate(0, 120);
+    canvas->drawPicture(picture);
+}
