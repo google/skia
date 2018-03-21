4040f68837f82710a1a69437e7606a6239c42fa9
Herb Derby <herb@google.com>
2018-03-21 16:23:49 -0400

    Move cache difference code into remote cache files
    BUG=skia:7515
    
    Change-Id: I59e75d460b4ed4d0a737c833520b2335808a4ce4

--- src/core/SkRemoteGlyphCache.cpp
+++ src/core/SkRemoteGlyphCache.cpp
@@ -101,4 +101,184 @@ sk_sp<SkTypeface> SkRemoteGlyphCacheGPU::decodeTypeface(const void* buf, size_t 
     return *typeFace;
 }
 
+// -- TrackLayerDevice -----------------------------------------------------------------------------
+class TrackLayerDevice : public SkNoPixelsDevice {
+public:
+    TrackLayerDevice(const SkIRect& bounds, const SkSurfaceProps& props)
+            : SkNoPixelsDevice(bounds, props) { }
+    SkBaseDevice* onCreateDevice(const CreateInfo& cinfo, const SkPaint*) override {
+        const SkSurfaceProps surfaceProps(this->surfaceProps().flags(), cinfo.fPixelGeometry);
+        return new TrackLayerDevice(this->getGlobalBounds(), surfaceProps);
+    }
+};
 
+// -- SkStrikeCacheDifferenceSpec ------------------------------------------------------------------
+SkTextBlobCacheDiffCanvas::SkTextBlobCacheDiffCanvas(
+    int width, int height,
+    const SkMatrix& deviceMatrix,
+    const SkSurfaceProps& props,
+    SkScalerContextFlags flags,
+    SkStrikeCacheDifferenceSpec* strikeDiffs)
+    : SkNoDrawCanvas{new TrackLayerDevice{SkIRect::MakeWH(width, height), props}}
+    , fDeviceMatrix{deviceMatrix}
+    , fSurfaceProps{props}
+    , fScalerContextFlags{flags}
+    , fStrikeCacheDiff{strikeDiffs} { }
+
+SkCanvas::SaveLayerStrategy SkTextBlobCacheDiffCanvas::getSaveLayerStrategy(
+    const SaveLayerRec&rec)
+{
+    return kFullLayer_SaveLayerStrategy;
+}
+
+void SkTextBlobCacheDiffCanvas::onDrawTextBlob(
+        const SkTextBlob* blob, SkScalar x, SkScalar y, const SkPaint& paint) {
+    SkPoint position{x, y};
+
+    SkPaint runPaint{paint};
+    SkTextBlobRunIterator it(blob);
+        for (;!it.done(); it.next()) {
+        // applyFontToPaint() always overwrites the exact same attributes,
+        // so it is safe to not re-seed the paint for this reason.
+        it.applyFontToPaint(&runPaint);
+        runPaint.setFlags(this->getTopDevice()->filterTextFlags(runPaint));
+        if (auto looper = runPaint.getLooper()) {
+            this->processLooper(position, it, runPaint, looper);
+        } else {
+            this->processGlyphRun(position, it, runPaint);
+        }
+    }
+}
+
+void SkTextBlobCacheDiffCanvas::processLooper(
+        const SkPoint& position,
+        const SkTextBlobRunIterator& it,
+        const SkPaint& origPaint,
+        SkDrawLooper* looper)
+{
+    SkSTArenaAlloc<48> alloc;
+    auto context = looper->makeContext(this, &alloc);
+    SkPaint runPaint = origPaint;
+    while (context->next(this, &runPaint)) {
+        this->save();
+        this->processGlyphRun(position, it, runPaint);
+        this->restore();
+        runPaint = origPaint;
+    }
+}
+
+void SkTextBlobCacheDiffCanvas::processGlyphRun(
+        const SkPoint& position,
+        const SkTextBlobRunIterator& it,
+        const SkPaint& runPaint)
+{
+
+    if (runPaint.getTextEncoding() != SkPaint::TextEncoding::kGlyphID_TextEncoding) {
+        return;
+    }
+
+    // All other alignment modes need the glyph advances. Use the slow drawing mode.
+    if (runPaint.getTextAlign() != SkPaint::kLeft_Align) {
+        return;
+    }
+
+    using PosFn = SkPoint(*)(int index, const SkScalar* pos);
+    PosFn posFn;
+    switch (it.positioning()) {
+        case SkTextBlob::kDefault_Positioning:
+            // Default positioning needs advances. Can't do that.
+            return;
+
+        case SkTextBlob::kHorizontal_Positioning:
+            posFn = [](int index, const SkScalar* pos) {
+                return SkPoint{pos[index], 0};
+            };
+
+            break;
+
+        case SkTextBlob::kFull_Positioning:
+            posFn = [](int index, const SkScalar* pos) {
+                return SkPoint{pos[2 * index], pos[2 * index + 1]};
+            };
+            break;
+
+        default:
+            posFn = nullptr;
+            SK_ABORT("unhandled positioning mode");
+    }
+
+    SkMatrix blobMatrix{fDeviceMatrix};
+    blobMatrix.preConcat(this->getTotalMatrix());
+    if (blobMatrix.hasPerspective()) {
+        return;
+    }
+    blobMatrix.preTranslate(position.x(), position.y());
+
+    SkMatrix runMatrix{blobMatrix};
+    runMatrix.preTranslate(it.offset().x(), it.offset().y());
+
+    using MapFn = SkPoint(*)(const SkMatrix& m, SkPoint pt);
+    MapFn mapFn;
+    switch ((int)runMatrix.getType()) {
+        case SkMatrix::kIdentity_Mask:
+        case SkMatrix::kTranslate_Mask:
+            mapFn = [](const SkMatrix& m, SkPoint pt) {
+                pt.offset(m.getTranslateX(), m.getTranslateY());
+                return pt;
+            };
+            break;
+        case SkMatrix::kScale_Mask:
+        case SkMatrix::kScale_Mask | SkMatrix::kTranslate_Mask:
+            mapFn = [](const SkMatrix& m, SkPoint pt) {
+                return SkPoint{pt.x() * m.getScaleX() + m.getTranslateX(),
+                               pt.y() * m.getScaleY() + m.getTranslateY()};
+            };
+            break;
+        case SkMatrix::kAffine_Mask | SkMatrix::kScale_Mask:
+        case SkMatrix::kAffine_Mask | SkMatrix::kScale_Mask | SkMatrix::kTranslate_Mask:
+            mapFn = [](const SkMatrix& m, SkPoint pt) {
+                return SkPoint{
+                        pt.x() * m.getScaleX() + pt.y() * m.getSkewX() + m.getTranslateX(),
+                        pt.x() * m.getSkewY() + pt.y() * m.getScaleY() + m.getTranslateY()};
+            };
+            break;
+        default:
+            mapFn = nullptr;
+            SK_ABORT("Bad matrix.");
+    }
+
+    SkAutoDescriptor ad;
+    SkScalerContextRec rec;
+    SkScalerContextEffects effects;
+
+    SkScalerContext::MakeRecAndEffects(runPaint, &fSurfaceProps, &runMatrix,
+                                       fScalerContextFlags, &rec, &effects);
+
+    auto desc = SkScalerContext::AutoDescriptorGivenRecAndEffects(rec, effects, &ad);
+
+    auto typefaceID = SkTypefaceProxy::DownCast(runPaint.getTypeface())->remoteTypefaceID();
+    auto& addGlyph = fStrikeCacheDiff->findStrikeDifferences(*desc, typefaceID);
+
+    auto cache = SkGlyphCache::FindStrikeExclusive(*desc);
+    bool isSubpixel = SkToBool(rec.fFlags & SkScalerContext::kSubpixelPositioning_Flag);
+    SkAxisAlignment axisAlignment = SkAxisAlignment::kNone_SkAxisAlignment;
+    if (it.positioning() == SkTextBlob::kHorizontal_Positioning) {
+        axisAlignment = rec.computeAxisAlignmentForHText();
+    }
+    auto pos = it.pos();
+    const uint16_t* glyphs = it.glyphs();
+    for (uint32_t index = 0; index < it.glyphCount(); index++) {
+        SkIPoint subPixelPos{0, 0};
+        if (runPaint.isAntiAlias() && isSubpixel) {
+            SkPoint glyphPos = mapFn(runMatrix, posFn(index, pos));
+            subPixelPos = SkFindAndPlaceGlyph::SubpixelAlignment(axisAlignment, glyphPos);
+        }
+
+        if (cache &&
+            cache->isGlyphCached(glyphs[index], subPixelPos.x(), subPixelPos.y())) {
+            continue;
+        }
+
+        addGlyph(glyphs[index], subPixelPos);
+    }
+}
