d603196806a50de466ae3349a408301bae4c5f78
Joe Gregorio <joe@bitworking.org>
2018-03-21 11:58:34 -0400

    Add Node as a bot asset.
    Bug: skia:
    Change-Id: I5c48cb741017be4eda014faea77ab88dec2e9637

---
+++ infra/bots/assets/node/create.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Create the asset."""
+
+
+import argparse
+import subprocess
+
+NODE_URL = "https://nodejs.org/dist/v8.10.0/node-v8.10.0-linux-x64.tar.xz"
+
+def create_asset(target_dir):
+  """Create the asset."""
+  p1 = subprocess.Popen(["curl", NODE_URL], stdout=subprocess.PIPE)
+  p2 = subprocess.Popen(["tar", "-C", target_dir, "-xf" "-"], stdin=p1.stdout)
+  p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+  _,_ = p2.communicate()
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--target_dir', '-t', required=True)
+  args = parser.parse_args()
+  create_asset(args.target_dir)
+
+
+if __name__ == '__main__':
+  main()
