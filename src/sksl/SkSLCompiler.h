b86b6d21b5d2d3ef45ad966a2c90d66767a1e10a
Ethan Nicholas <ethannicholas@google.com>
2018-03-22 10:10:44 -0400

    switched SPIR-V vertexid to vertexindex
    Bug: skia:
    Change-Id: I32c10e2874feea63d7fa048057ef57e27d5874bc
    Reviewed-on: https://skia-review.googlesource.com/115880
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    (cherry picked from commit 9eded2c211773133b86a964b357404ae6b021d7d)

--- src/sksl/SkSLCompiler.h
+++ src/sksl/SkSLCompiler.h
@@ -25,8 +25,8 @@
 #define SK_TRANSFORMEDCOORDS2D_BUILTIN 10005
 #define SK_TEXTURESAMPLERS_BUILTIN     10006
 #define SK_FRAGCOORD_BUILTIN              15
-#define SK_VERTEXID_BUILTIN                5
-#define SK_INSTANCEID_BUILTIN              6
+#define SK_VERTEXID_BUILTIN               42
+#define SK_INSTANCEID_BUILTIN             43
 #define SK_CLIPDISTANCE_BUILTIN            3
 #define SK_INVOCATIONID_BUILTIN            8
 #define SK_POSITION_BUILTIN                0
