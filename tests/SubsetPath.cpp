80e1d56e198c5fd9fe6db0c945bd558053a8dc6a
Mike Klein <mtklein@chromium.org>
2018-03-22 11:36:52 -0400

    implement SkTDArray with std::vector
    It's always worth seeing if we can get away with replacing custom data
    structures with ones from the standard library.  Our array-like types
    are all good candidates to replace with std::vector, and it's especially
    easy to start with SkTDArray.  Unlike the others, it has no preallocated
    S-variant, which is tricky to make work with std::vector.
    
    SkTDArray also has known integer overflow bugs, leading to out of range
    writes.  It'd be _very_ nice to ditch it for a better standard vector.
    
    I removed a bunch of unused or little-used methods, and updated a couple
    call sites that used methods in unusual or dangerous ways.
    
    I've had to tweak GrAAConvexTessellator and SkBaseShadowTessellator just
    a touch to work within the constraints of an std::vector impl.  It's not
    intended to be legal to write to the reserved-but-not-counted elements
    of an SkTDArray, but you can get away with it in our old implementation.
    This version now uses setCount() to actually reserve and count them, and
    should have the same performance and use the same amount of memory.
    
    The PathMeasure_explosion GM I added recently to reproduce this bug now
    draws without triggering undefined behavior or ASAN errors, provided you
    have ~40GB of RAM.
    
    Bug: skia:7674
    
    Change-Id: I4eacae18a976cd4a6d218102f8ca5d973d4d7d0e
    Reviewed-on: https://skia-review.googlesource.com/115982
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>

--- tests/SubsetPath.cpp
+++ tests/SubsetPath.cpp
@@ -193,7 +193,7 @@ SkPath SubsetVerbs::getSubsetPath() const {
     bool addLineTo = false;
     while ((verb = iter.next(pts)) != SkPath::kDone_Verb) {
         bool enabled = SkPath::kLine_Verb <= verb && verb <= SkPath::kCubic_Verb
-            ? fSelected[verbIndex++] : false;
+            ? (bool)fSelected[verbIndex++] : false;
         if (enabled) {
             if (addMoveTo) {
                 result.moveTo(pts[0]);
