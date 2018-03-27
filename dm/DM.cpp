33f02edb14bb2ad802324daa99886709990893a1
Robert Phillips <robertphillips@google.com>
2018-03-27 08:06:57 -0400

    Switch DDL rendering to be a Via in DM (take 2)
    This will let us also render the GMs via DDLs.
    
    TBR=mtklein@google.com
    Change-Id: If7c2460d964822a6decc33cf5e8e685e67923127
    Reviewed-on: https://skia-review.googlesource.com/116463
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- dm/DM.cpp
+++ dm/DM.cpp
@@ -957,6 +957,8 @@ static Sink* create_via(const SkString& tag, Sink* wrapped) {
     VIA("tiles",     ViaTiles, 256, 256, nullptr,            wrapped);
     VIA("tiles_rt",  ViaTiles, 256, 256, new SkRTreeFactory, wrapped);
 
+    VIA("ddl",       ViaDDL, 3,            wrapped);
+
     if (FLAGS_matrix.count() == 4) {
         SkMatrix m;
         m.reset();
