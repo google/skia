c89ebf66901cc3f02c79c2f54199aaf970d4299f
Greg Daniel <egdaniel@google.com>
2018-03-29 08:33:30 -0400

    Add test for importing AHardwareBuffers in and out of Vulkan.
    Also cherry-picks in changes:
    Add ctor to GrVkAlloc (https://skia-review.googlesource.com/111660)
    Add testing only helper to flush and sync gpu (https://skia-review.googlesource.com/113122)
    
    No-Tree-Checks: true
    No-Try: true
    No-Presubmit: true
    Bug: skia:
    Change-Id: I732f371af7f48f5a590fd161021a69cbc6f2583c

--- include/gpu/vk/GrVkTypes.h
+++ include/gpu/vk/GrVkTypes.h
@@ -31,19 +31,32 @@
  * Vulkan textures are really const GrVkImageInfo*
  */
 struct GrVkAlloc {
-    VkDeviceMemory fMemory = VK_NULL_HANDLE;  // can be VK_NULL_HANDLE iff is an RT and is borrowed
-    VkDeviceSize   fOffset = 0;
-    VkDeviceSize   fSize = 0;    // this can be indeterminate iff Tex uses borrow semantics
-    uint32_t       fFlags= 0;
+    GrVkAlloc()
+            : fMemory(VK_NULL_HANDLE)
+            , fOffset(0)
+            , fSize(0)
+            , fFlags(0)
+            , fUsesSystemHeap(false) {}
+
+    GrVkAlloc(VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, uint32_t flags)
+            : fMemory(memory)
+            , fOffset(offset)
+            , fSize(size)
+            , fFlags(flags)
+            , fUsesSystemHeap(false) {}
+
+    VkDeviceMemory fMemory;  // can be VK_NULL_HANDLE iff is an RT and is borrowed
+    VkDeviceSize   fOffset;
+    VkDeviceSize   fSize;    // this can be indeterminate iff Tex uses borrow semantics
+    uint32_t       fFlags;
 
     enum Flag {
         kNoncoherent_Flag = 0x1,   // memory must be flushed to device after mapping
     };
 private:
     friend class GrVkHeap; // For access to usesSystemHeap
-    bool fUsesSystemHeap = false;
+    bool fUsesSystemHeap;
 };
-
 struct GrVkImageInfo {
     /**
      * If the image's format is sRGB (GrVkFormatIsSRGB returns true), then the image must have
@@ -55,6 +68,8 @@ struct GrVkImageInfo {
     VkImageLayout  fImageLayout;
     VkFormat       fFormat;
     uint32_t       fLevelCount;
+    uint32_t       fInitialQueueFamily = VK_QUEUE_FAMILY_IGNORED;
+    uint32_t       fCurrentQueueFamily = VK_QUEUE_FAMILY_IGNORED;
 
     // This gives a way for a client to update the layout of the Image if they change the layout
     // while we're still holding onto the wrapped texture. They will first need to get a handle
