f54d5f5aeb7b175b9f904ac24a62530ae26b7d5a
Brian Salomon <bsalomon@google.com>
2018-04-04 12:19:54 -0400

    Remove deprectated GrContext::MakeGL overloads that take raw ptr
    Change-Id: Iae12bf61fa1e2f5d4b13b98198cddb7482656fdf

--- src/gpu/GrDirectContext.cpp
+++ src/gpu/GrDirectContext.cpp
@@ -100,15 +100,6 @@ sk_sp<GrContext> GrContext::MakeGL(sk_sp<const GrGLInterface> interface) {
     return MakeGL(std::move(interface), defaultOptions);
 }
 
-sk_sp<GrContext> GrContext::MakeGL(const GrGLInterface* interface) {
-    return MakeGL(sk_ref_sp(interface));
-}
-
-sk_sp<GrContext> GrContext::MakeGL(const GrGLInterface* interface,
-                                   const GrContextOptions& options) {
-    return MakeGL(sk_ref_sp(interface), options);
-}
-
 sk_sp<GrContext> GrContext::MakeGL(sk_sp<const GrGLInterface> interface,
                                    const GrContextOptions& options) {
     sk_sp<GrContext> context(new GrDirectContext(kOpenGL_GrBackend));
