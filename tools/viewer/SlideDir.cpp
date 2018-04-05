670ad57d4932bfe2019ea28cfd2c8be12828df7e
Florin Malita <fmalita@chromium.org>
2018-04-05 15:15:59 -0400

    Clip SlideDir slides to their declared dimensions
    (to avoid spilling cell content)
    
    TBR=
    Change-Id: I7bbbbf004d35faab5f264d9914bcd24d2bb8eaeb

--- tools/viewer/SlideDir.cpp
+++ tools/viewer/SlideDir.cpp
@@ -72,6 +72,8 @@ protected:
     }
 
     void onRender(SkCanvas* canvas) const override {
+        SkAutoCanvasRestore acr(canvas, true);
+        canvas->clipRect(SkRect::Make(fSlide->getDimensions()), true);
         fSlide->draw(canvas);
     }
 
