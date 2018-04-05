f14915411b42a59bdc1eb12dab684b67798b6300
Florin Malita <fmalita@chromium.org>
2018-04-05 11:57:21 -0400

    Viewer SVG support
    Change-Id: I93ee61271ebe960063bec16ba472b3fd243ee149

---
+++ tools/viewer/SvgSlide.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SvgSlide_DEFINED
+#define SvgSlide_DEFINED
+
+#include "Slide.h"
+
+class SkSVGDOM;
+
+class SvgSlide final : public Slide {
+public:
+    SvgSlide(const SkString& name, const SkString& path);
+
+    void load(SkScalar winWidth, SkScalar winHeight) override;
+    void unload() override;
+
+    SkISize getDimensions() const override;
+
+    void draw(SkCanvas*) override;
+private:
+    const SkString  fPath;
+
+    SkSize          fWinSize = SkSize::MakeEmpty();
+    sk_sp<SkSVGDOM> fDom;
+
+    typedef Slide INHERITED;
+};
+
+#endif // SvgSlide_DEFINED
