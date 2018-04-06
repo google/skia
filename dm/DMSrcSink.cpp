ea176c608f6ce4f4420e129e30e12299e51c8de8
Brian Osman <brianosman@google.com>
2018-04-06 15:28:23 -0400

    Remove scanlineOrder switch statements that always do the same thing
    I think these date back to GIF codec having a third value of this enum
    that meant "interlaced" ?
    
    Change-Id: Iaff11a55ad3cff44da2307149991a22447dfe746
    Reviewed-on: https://skia-review.googlesource.com/119146
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- dm/DMSrcSink.cpp
+++ dm/DMSrcSink.cpp
@@ -616,14 +616,9 @@ Error CodecSrc::draw(SkCanvas* canvas) const {
                     return "Could not start scanline decoder";
                 }
 
-                switch (codec->getScanlineOrder()) {
-                    case SkCodec::kTopDown_SkScanlineOrder:
-                    case SkCodec::kBottomUp_SkScanlineOrder:
-                        // We do not need to check the return value.  On an incomplete
-                        // image, memory will be filled with a default value.
-                        codec->getScanlines(dst, height, rowBytes);
-                        break;
-                }
+                // We do not need to check the return value.  On an incomplete
+                // image, memory will be filled with a default value.
+                codec->getScanlines(dst, height, rowBytes);
             }
 
             draw_to_canvas(canvas, bitmapInfo, dst, rowBytes, fDstColorType);
