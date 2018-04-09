42f1cf7b14bf61efcc8528204923d45b2e1b292c
Mike Reed <reed@google.com>
2018-04-09 15:02:02 -0400

    add trim-rect to raster tiling
    Bug: skia:
    Change-Id: I8bda668a99bcdb2a9a74a8278ec0cf1004acba6e

--- src/core/SkBitmapDevice.cpp
+++ src/core/SkBitmapDevice.cpp
@@ -23,6 +23,19 @@
 #include "SkTLazy.h"
 #include "SkVertices.h"
 
+struct Bounder {
+    SkRect  fBounds;
+    bool    fHasBounds;
+
+    Bounder(const SkRect& r, const SkPaint& paint) {
+        if ((fHasBounds = paint.canComputeFastBounds())) {
+            (void)paint.computeFastBounds(r, &fBounds);
+        }
+    }
+
+    operator const SkRect* () const { return fHasBounds ? &fBounds : nullptr; }
+};
+
 class SkDrawTiler {
     enum {
         // 8K is 1 too big, since 8K << supersample == 32768 which is too big for SkFixed
@@ -31,6 +44,7 @@ class SkDrawTiler {
 
     SkBitmapDevice* fDevice;
     SkPixmap        fRootPixmap;
+    SkIRect         fSrcBounds;
 
     // Used for tiling and non-tiling
     SkDraw          fDraw;
@@ -43,11 +57,23 @@ class SkDrawTiler {
     bool            fDone, fNeedsTiling;
 
 public:
-    SkDrawTiler(SkBitmapDevice* dev) : fDevice(dev) {
+    SkDrawTiler(SkBitmapDevice* dev, const SkRect* bounds) : fDevice(dev) {
         // we need fDst to be set, and if we're actually drawing, to dirty the genID
         if (!dev->accessPixels(&fRootPixmap)) {
             // NoDrawDevice uses us (why?) so we have to catch this case w/ no pixels
             fRootPixmap.reset(dev->imageInfo(), nullptr, 0);
+        }
+
+        if (bounds) {
+            SkRect devBounds;
+            dev->ctm().mapRect(&devBounds, *bounds);
+            if (devBounds.intersect(SkRect::MakeIWH(fRootPixmap.width(), fRootPixmap.height()))) {
+                fSrcBounds = devBounds.roundOut();
+            } else {
+                fSrcBounds = SkIRect::MakeLTRB(-1, -1, -1, -1); // nothing to draw
+            }
+        } else {
+            fSrcBounds = SkIRect::MakeWH(fRootPixmap.width(), fRootPixmap.height());
         }
 
         fDone = false;
@@ -58,7 +84,8 @@ public:
             // fDraw.fDst is reset each time in setupTileDraw()
             fDraw.fMatrix = &fTileMatrix;
             fDraw.fRC = &fTileRC;
-            fOrigin.set(-kMaxDim, 0); // we'll step/increase it before using it
+            // we'll step/increase it before using it
+            fOrigin.set(fSrcBounds.fLeft - kMaxDim, fSrcBounds.fTop);
         } else {
             fDraw.fDst = fRootPixmap;
             fDraw.fMatrix = &dev->ctm();
@@ -98,15 +125,15 @@ private:
         SkASSERT(fNeedsTiling);
 
         // We do fRootPixmap.width() - kMaxDim instead of fOrigin.fX + kMaxDim to avoid overflow.
-        if (fOrigin.fX >= fRootPixmap.width() - kMaxDim) {    // too far
-            fOrigin.fX = 0;
+        if (fOrigin.fX >= fSrcBounds.fRight - kMaxDim) {    // too far
+            fOrigin.fX = fSrcBounds.fLeft;
             fOrigin.fY += kMaxDim;
         } else {
             fOrigin.fX += kMaxDim;
         }
         // fDone = next origin will be invalid.
-        fDone = fOrigin.fX >= fRootPixmap.width() - kMaxDim &&
-                fOrigin.fY >= fRootPixmap.height() - kMaxDim;
+        fDone = fOrigin.fX >= fSrcBounds.fRight - kMaxDim &&
+                fOrigin.fY >= fSrcBounds.fBottom - kMaxDim;
 
         SkIRect bounds = SkIRect::MakeXYWH(fOrigin.x(), fOrigin.y(), kMaxDim, kMaxDim);
         SkASSERT(!bounds.isEmpty());
@@ -122,8 +149,12 @@ private:
     }
 };
 
-#define LOOP_TILER(code)                                    \
-    SkDrawTiler priv_tiler(this);                           \
+// Passing a bounds allows the tiler to only visit the dst-tiles that might intersect the
+// drawing. If null is passed, the tiler has to visit everywhere. The bounds is expected to be
+// in local coordinates, as the tiler itself will transform that into device coordinates.
+//
+#define LOOP_TILER(code, boundsPtr)                         \
+    SkDrawTiler priv_tiler(this, boundsPtr);                \
     while (const SkDraw* priv_draw = priv_tiler.next()) {   \
         priv_draw->code;                                    \
     }
@@ -282,16 +313,16 @@ bool SkBitmapDevice::onReadPixels(const SkPixmap& pm, int x, int y) {
 ///////////////////////////////////////////////////////////////////////////////
 
 void SkBitmapDevice::drawPaint(const SkPaint& paint) {
-    LOOP_TILER( drawPaint(paint))
+    LOOP_TILER( drawPaint(paint), nullptr)
 }
 
 void SkBitmapDevice::drawPoints(SkCanvas::PointMode mode, size_t count,
                                 const SkPoint pts[], const SkPaint& paint) {
-    LOOP_TILER( drawPoints(mode, count, pts, paint, nullptr))
+    LOOP_TILER( drawPoints(mode, count, pts, paint, nullptr), nullptr)
 }
 
 void SkBitmapDevice::drawRect(const SkRect& r, const SkPaint& paint) {
-    LOOP_TILER( drawRect(r, paint))
+    LOOP_TILER( drawRect(r, paint), Bounder(r, paint))
 }
 
 void SkBitmapDevice::drawOval(const SkRect& oval, const SkPaint& paint) {
@@ -311,14 +342,14 @@ void SkBitmapDevice::drawRRect(const SkRRect& rrect, const SkPaint& paint) {
     // required to override drawRRect.
     this->drawPath(path, paint, nullptr, true);
 #else
-    LOOP_TILER( drawRRect(rrect, paint))
+    LOOP_TILER( drawRRect(rrect, paint), Bounder(rrect.getBounds(), paint))
 #endif
 }
 
 void SkBitmapDevice::drawPath(const SkPath& path,
                               const SkPaint& paint, const SkMatrix* prePathMatrix,
                               bool pathIsMutable) {
-    SkDrawTiler tiler(this);
+    SkDrawTiler tiler(this, !path.isInverseFillType() ? Bounder(path.getBounds(), paint) : nullptr);
     if (tiler.needsTiling()) {
         pathIsMutable = false;
     }
@@ -336,7 +367,7 @@ void SkBitmapDevice::drawBitmap(const SkBitmap& bitmap, SkScalar x, SkScalar y,
 
 void SkBitmapDevice::drawBitmap(const SkBitmap& bitmap, const SkMatrix& matrix,
                                 const SkRect* dstOrNull, const SkPaint& paint) {
-    LOOP_TILER( drawBitmap(bitmap, matrix, dstOrNull, paint))
+    LOOP_TILER( drawBitmap(bitmap, matrix, dstOrNull, paint), nullptr)
 }
 
 static inline bool CanApplyDstMatrixAsCTM(const SkMatrix& m, const SkPaint& paint) {
@@ -460,25 +491,26 @@ void SkBitmapDevice::drawBitmapRect(const SkBitmap& bitmap,
 }
 
 void SkBitmapDevice::drawSprite(const SkBitmap& bitmap, int x, int y, const SkPaint& paint) {
-    LOOP_TILER( drawSprite(bitmap, TILER_X(x), TILER_Y(y), paint))
+    LOOP_TILER( drawSprite(bitmap, TILER_X(x), TILER_Y(y), paint), nullptr)
 }
 
 void SkBitmapDevice::drawText(const void* text, size_t len,
                               SkScalar x, SkScalar y, const SkPaint& paint) {
-    LOOP_TILER( drawText((const char*)text, len, x, y, paint, &fSurfaceProps))
+    LOOP_TILER( drawText((const char*)text, len, x, y, paint, &fSurfaceProps), nullptr)
 }
 
 void SkBitmapDevice::drawPosText(const void* text, size_t len, const SkScalar xpos[],
                                  int scalarsPerPos, const SkPoint& offset, const SkPaint& paint) {
     LOOP_TILER( drawPosText((const char*)text, len, xpos, scalarsPerPos, offset, paint,
-                            &fSurfaceProps))
+                            &fSurfaceProps), nullptr)
 }
 
 void SkBitmapDevice::drawVertices(const SkVertices* vertices, SkBlendMode bmode,
                                   const SkPaint& paint) {
     LOOP_TILER( drawVertices(vertices->mode(), vertices->vertexCount(), vertices->positions(),
                              vertices->texCoords(), vertices->colors(), bmode,
-                             vertices->indices(), vertices->indexCount(), paint))
+                             vertices->indices(), vertices->indexCount(), paint),
+               &vertices->bounds())
 }
 
 void SkBitmapDevice::drawDevice(SkBaseDevice* device, int x, int y, const SkPaint& origPaint) {
@@ -491,7 +523,7 @@ void SkBitmapDevice::drawDevice(SkBaseDevice* device, int x, int y, const SkPain
     }
 
     LOOP_TILER( drawSprite(static_cast<SkBitmapDevice*>(device)->fBitmap,
-                           TILER_X(x), TILER_Y(y), *paint))
+                           TILER_X(x), TILER_Y(y), *paint), nullptr)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
