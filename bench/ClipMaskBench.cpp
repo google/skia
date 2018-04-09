cc90910511c83c080e8299affc556658a2f2b6a2
Mike Reed <reed@google.com>
2018-04-09 15:02:02 -0400

    add trim-rect to raster tiling
    This allows the tiler to optimally visit only the tiles that might intersect
    the drawing. Not all call-sites can cheaply compute their bounds, so for those
    we just pass nullptr, which tells the tiler to visit all of the tiles.
    
    Bug: 818693
    Bug: 813316
    Bug: 820470
    Change-Id: I8bda668a99bcdb2a9a74a8278ec0cf1004acba6e

--- bench/ClipMaskBench.cpp
+++ bench/ClipMaskBench.cpp
@@ -66,3 +66,40 @@ DEF_BENCH(return new ClipMaskBench("picture", [](int size) -> sk_sp<SkImage> {
                                     SkColorSpace::MakeSRGB());
 });)
 
+/////////
+#include "SkSurface.h"
+#include "SkPath.h"
+
+class RasterTileBench : public Benchmark {
+    sk_sp<SkSurface> fSurf;
+    SkPath           fPath;
+    SkString       fName;
+public:
+    RasterTileBench() : fName("rastertile") {
+        int W = 2014 * 20;
+        int H = 20;
+        fSurf = SkSurface::MakeRasterN32Premul(W, H);
+
+        fPath.moveTo(0, 0);
+        fPath.cubicTo(20, 10, 10, 15, 30, 5);
+    }
+
+protected:
+    const char* onGetName() override { return fName.c_str(); }
+
+    void onDraw(int loops, SkCanvas* canvas) override {
+        SkPaint paint;
+        paint.setStyle(SkPaint::kStroke_Style);
+        paint.setStrokeWidth(1.1f);
+        paint.setAntiAlias(true);
+
+        for (int i = 0; i < loops; ++i) {
+            for (int j = 0; j < 1000; ++j) {
+                fSurf->getCanvas()->drawPath(fPath, paint);
+            }
+        }
+    }
+
+private:
+};
+DEF_BENCH(return new RasterTileBench;)
