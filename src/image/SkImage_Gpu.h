0573630a97bb07ba7ac5b7c846ecc47abdc6ad82
Robert Phillips <robertphillips@google.com>
2018-04-10 09:32:55 -0400

    Make SkImage_Gpus always have a threadSafeProxy but optionally a GrContext
    This is for the DDL task where PromiseImages are being created in a situation
    where there is no real GrContext (but there is a threadSafeProxy). This allows
    us to more clearly tell the state of a given SkImage_Gpu.
    
    Change-Id: I666dcc07d9220764f10b3b5c00b90c756759ba08

--- src/image/SkImage_Gpu.h
+++ src/image/SkImage_Gpu.h
@@ -23,8 +23,8 @@ class GrTexture;
 
 class SkImage_Gpu : public SkImage_Base {
 public:
-    SkImage_Gpu(GrContext*, uint32_t uniqueID, SkAlphaType, sk_sp<GrTextureProxy>,
-                sk_sp<SkColorSpace>, SkBudgeted);
+    SkImage_Gpu(GrContext*, sk_sp<GrContextThreadSafeProxy>, uint32_t uniqueID,
+                SkAlphaType, sk_sp<GrTextureProxy>, sk_sp<SkColorSpace>, SkBudgeted);
     ~SkImage_Gpu() override;
 
     SkImageInfo onImageInfo() const override;
@@ -34,7 +34,6 @@ public:
     bool getROPixels(SkBitmap*, SkColorSpace* dstColorSpace, CachingHint) const override;
     sk_sp<SkImage> onMakeSubset(const SkIRect&) const override;
 
-    GrContext* context() const override { return fContext; }
     GrTextureProxy* peekProxy() const override {
         return fProxy.get();
     }
@@ -92,7 +91,7 @@ public:
         called when we call the textureDoneProc. Thus when the textureDoneProc gets called the
         client is able to cleanup all GPU objects and meta data needed for the textureFulfill call.
 
-        @param context             Gpu context
+        @param threadSafeProxy     proxy for the GrContext
         @param backendFormat       format of promised gpu texture
         @param width               width of promised gpu texture
         @param height              height of promised gpu texture
@@ -111,7 +110,7 @@ public:
         @param textureContext      state passed to textureFulfillProc and textureReleaseProc
         @return                    created SkImage, or nullptr
      */
-    static sk_sp<SkImage> MakePromiseTexture(GrContext* context,
+    static sk_sp<SkImage> MakePromiseTexture(sk_sp<GrContextThreadSafeProxy> threadSafeProxy,
                                              const GrBackendFormat& backendFormat,
                                              int width,
                                              int height,
@@ -134,13 +133,13 @@ public:
     bool onIsValid(GrContext*) const override;
 
 private:
-    GrContext*             fContext;
-    sk_sp<GrTextureProxy>  fProxy;
-    const SkAlphaType      fAlphaType;
-    const SkBudgeted       fBudgeted;
-    sk_sp<SkColorSpace>    fColorSpace;
-    mutable SkAtomic<bool> fAddedRasterVersionToCache;
-
+    GrContext*                      fContext;
+    sk_sp<GrContextThreadSafeProxy> fThreadSafeProxy;
+    sk_sp<GrTextureProxy>           fProxy;
+    const SkAlphaType               fAlphaType;
+    const SkBudgeted                fBudgeted;
+    sk_sp<SkColorSpace>             fColorSpace;
+    mutable SkAtomic<bool>          fAddedRasterVersionToCache;
 
     typedef SkImage_Base INHERITED;
 };
