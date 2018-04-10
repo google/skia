401223434f18117980151f9eb8ff5e53e9e218bd
Mike Klein <mtklein@chromium.org>
2018-04-10 10:05:31 -0400

    hard-roll skcms into Skia
    Adding roll.sh to make it easy.
    
    Change-Id: I7887c5f9a41c5b68a5dec89ebc8ac86a1707fef6

---
+++ src/skcms/roll.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright 2018 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+#set -x
+
+tmp=$(mktemp -d)
+git clone --quiet --depth 1 https://skia.googlesource.com/skcms.git $tmp
+
+git rm --quiet    LICENSE
+git rm --quiet    skcms.h
+git rm --quiet    skcms.c
+git rm --quiet -r src
+
+cp    $tmp/LICENSE .
+cp    $tmp/skcms.h .
+cp    $tmp/skcms.c .
+cp -r $tmp/src     .
+
+git add .
+
+rm -rf $tmp
