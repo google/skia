f1c34140800bdb1c9ff15308257252d0b771d080
Cary Clark <caryclark@skia.org>
2018-04-16 12:01:58 -0400

    a more involved path is rect bug
    This is bug number ten in the series, and is the
    most interesting. It exploits that the code tracks
    corners 0, 2, and 3 but not corner 1.
    
    Changing the code to track all corners is the biggest
    so far, and while it (hopefully) simplifies things,
    the presence of new code may signify more bugs to come.
    
    R=robertphillip@google.com
    
    Bug: 824145,skia::7792
    Change-Id: Ia18e4d80fbed06ae6d9c89dcb4c462c5610213cc

--- gm/pathfill.cpp
+++ gm/pathfill.cpp
@@ -433,7 +433,7 @@ DEF_SIMPLE_GM(rotatedcubicpath, canvas, 200, 200) {
 DEF_GM( return new PathFillGM; )
 DEF_GM( return new PathInverseFillGM; )
 
-DEF_SIMPLE_GM(bug7792, canvas, 600, 600) {
+DEF_SIMPLE_GM(bug7792, canvas, 600, 800) {
     // from skbug.com/7792 bug description
     SkPaint p;
     SkPath path;
@@ -534,6 +534,17 @@ DEF_SIMPLE_GM(bug7792, canvas, 600, 600) {
     path.lineTo(150, 75);
     path.lineTo(150, 150);
     path.lineTo(75, 150);
+    path.close();
+    canvas->drawPath(path, p);
+    // from skbug.com/7792#c29
+    canvas->translate(-200 * 2, 200);
+    path.reset();
+    path.moveTo(75, 75);
+    path.lineTo(150, 75);
+    path.lineTo(150, 150);
+    path.lineTo(75, 150);
+    path.lineTo(75, 250);
+    path.moveTo(75, 75);
     path.close();
     canvas->drawPath(path, p);
 }
