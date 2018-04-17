09c6dbf4cdf6f39e828bae6543b939ed217c656f
Mike Reed <reed@google.com>
2018-04-17 11:06:31 -0400

    only recheck fNeedsTiling if we computed fSrcBounds
    Bug: skia:
    Change-Id: I64cc83d31e8aaf8221e78c3b5301ffcce45449c1

--- src/core/SkBitmapDevice.cpp
+++ src/core/SkBitmapDevice.cpp
@@ -81,12 +81,12 @@ public:
                 dev->ctm().mapRect(&devBounds, *bounds);
                 if (devBounds.intersect(SkRect::Make(clipR))) {
                     fSrcBounds = devBounds.roundOut();
+                    // Check again, now that we have computed srcbounds.
+                    fNeedsTiling = fSrcBounds.right() > kMaxDim || fSrcBounds.bottom() > kMaxDim;
                 } else {
                     fNeedsTiling = false;
                     fDone = true;
                 }
-                // Check again, now that we have computed srcbounds.
-                fNeedsTiling = fSrcBounds.right() > kMaxDim || fSrcBounds.bottom() > kMaxDim;
             } else {
                 fSrcBounds = clipR;
             }
