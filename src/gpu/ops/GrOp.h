2748391716b04f700a689ac99bc13fd67d812418
Robert Phillips <robertphillips@google.com>
2018-04-20 12:43:18 -0400

    Add ability to dump opList info at flush-time
    When debugging DDL it is useful to just see which ops are being drawn instead of the entire reordering history.
    
    Change-Id: I89708fecbd53c097b00e65b0da426bfb8046b0ee
    Reviewed-on: https://skia-review.googlesource.com/122780
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/gpu/ops/GrOp.h
+++ src/gpu/ops/GrOp.h
@@ -51,6 +51,9 @@ class GrRenderTargetOpList;
     #define GrOP_INFO(...)
 #endif
 
+// Print out op information at flush time
+#define GR_FLUSH_TIME_OP_SPEW 0
+
 // A helper macro to generate a class static id
 #define DEFINE_OP_CLASS_ID \
     static uint32_t ClassID() { \
