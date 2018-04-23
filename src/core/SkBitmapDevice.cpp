9bd21154dbb924849ceb3203a5df933e94c5502d
Mike Reed <reed@google.com>
2018-04-23 11:23:08 -0400

    check for finite after applying ctm
    Bug: oss-fuzz:7914
    Change-Id: If4f5eae736bb93451d2ff1c802e7c84a62a38cbc

--- src/core/SkBitmapDevice.cpp
+++ src/core/SkBitmapDevice.cpp
@@ -79,7 +79,7 @@ public:
             if (bounds) {
                 SkRect devBounds;
                 dev->ctm().mapRect(&devBounds, *bounds);
-                if (devBounds.intersect(SkRect::Make(clipR))) {
+                if (devBounds.isFinite() && devBounds.intersect(SkRect::Make(clipR))) {
                     fSrcBounds = devBounds.roundOut();
                     // Check again, now that we have computed srcbounds.
                     fNeedsTiling = fSrcBounds.right() > kMaxDim || fSrcBounds.bottom() > kMaxDim;
