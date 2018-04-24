e1fdee2a7725b13317c422f3edaa3bc3f3a89564
Eric Boren <borenet@google.com>
2018-04-24 09:19:03 -0400

    [infra] Split RecreateSKPs into two parts: build Chrome, recreate SKPs
    Bug: skia:
    Change-Id: I5eea6219f38c6955cf596329c9d22d780e23e442

--- infra/bots/recipes/recreate_skps.py
+++ infra/bots/recipes/recreate_skps.py
@@ -8,28 +8,15 @@
 
 DEPS = [
   'core',
-  'depot_tools/gclient',
   'infra',
   'recipe_engine/context',
   'recipe_engine/file',
   'recipe_engine/path',
   'recipe_engine/properties',
   'recipe_engine/python',
-  'recipe_engine/raw_io',
-  'recipe_engine/step',
   'run',
   'vars',
 ]
-
-
-TEST_BUILDERS = {
-  'client.skia.compile': {
-    'skiabot-linux-swarm-000': [
-      'Housekeeper-Nightly-RecreateSKPs_Canary',
-      'Housekeeper-Weekly-RecreateSKPs',
-    ],
-  },
-}
 
 
 def RunSteps(api):
@@ -37,46 +24,31 @@ def RunSteps(api):
   api.core.setup()
 
   src_dir = api.vars.checkout_root.join('src')
-  out_dir = src_dir.join('out', 'Release')
-
-  with api.context(cwd=src_dir):
-    # Call GN.
-    platform = 'linux64'  # This bot only runs on linux; don't bother checking.
-    gn = src_dir.join('buildtools', platform, 'gn')
-    gn_env = {'CPPFLAGS': '-DSK_ALLOW_CROSSPROCESS_PICTUREIMAGEFILTERS=1',
-              'GYP_GENERATORS': 'ninja'}
-    with api.context(env=gn_env):
-      api.run(api.step, 'GN', cmd=[gn, 'gen', out_dir])
-
-    # Build Chrome.
-    api.run(api.step, 'Build Chrome', cmd=['ninja', '-C', out_dir, 'chrome'])
+  out_dir = api.vars.build_dir.join('out', 'Release')
 
   # Clean up the output dir.
   output_dir = api.path['start_dir'].join('skp_output')
-  if api.path.exists(output_dir):
-    api.run.rmtree(output_dir)
   api.file.ensure_directory('makedirs skp_output', output_dir)
 
   # Capture the SKPs.
   asset_dir = api.vars.infrabots_dir.join('assets', 'skp')
-  cmd = ['python', asset_dir.join('create.py'),
-         '--chrome_src_path', src_dir,
-         '--browser_executable', src_dir.join('out', 'Release', 'chrome'),
+  cmd = ['--chrome_src_path', src_dir,
+         '--browser_executable', out_dir.join('chrome'),
          '--target_dir', output_dir]
   # TODO(rmistry): Uncomment the below after skbug.com/6797 is fixed.
   # if 'Canary' not in api.properties['buildername']:
   #   cmd.append('--upload_to_partner_bucket')
   with api.context(cwd=api.vars.skia_dir):
-    api.run(api.step, 'Recreate SKPs', cmd=cmd)
+    api.run(api.python, 'Recreate SKPs', script=asset_dir.join('create.py'),
+            args=cmd)
 
   # Upload the SKPs.
   if 'Canary' not in api.properties['buildername']:
     api.infra.update_go_deps()
-    cmd = ['python',
-           api.vars.skia_dir.join('infra', 'bots', 'upload_skps.py'),
-           '--target_dir', output_dir]
+    cmd = ['--target_dir', output_dir]
     with api.context(cwd=api.vars.skia_dir, env=api.infra.go_env):
-      api.run(api.step, 'Upload SKPs', cmd=cmd)
+      api.run(api.python, 'Upload SKPs',
+              script=api.vars.infrabots_dir.join('upload_skps.py'), args=cmd)
 
 
 def GenTests(api):
@@ -87,8 +59,7 @@ def GenTests(api):
                      repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
-                     swarm_out_dir='[SWARM_OUT_DIR]') +
-      api.path.exists(api.path['start_dir'].join('skp_output'))
+                     swarm_out_dir='[SWARM_OUT_DIR]')
   )
 
   builder = 'Housekeeper-Weekly-RecreateSKPs'
@@ -98,17 +69,5 @@ def GenTests(api):
                      repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
-                     swarm_out_dir='[SWARM_OUT_DIR]') +
-      api.path.exists(api.path['start_dir'].join('skp_output'))
-  )
-
-  yield (
-      api.test('failed_upload') +
-      api.properties(buildername=builder,
-                     repository='https://skia.googlesource.com/skia.git',
-                     revision='abc123',
-                     path_config='kitchen',
-                     swarm_out_dir='[SWARM_OUT_DIR]') +
-      api.path.exists(api.path['start_dir'].join('skp_output')) +
-      api.step_data('Upload SKPs', retcode=1)
+                     swarm_out_dir='[SWARM_OUT_DIR]')
   )
