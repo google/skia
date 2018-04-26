dc900ce2c43622610e720337e70a11c9cfbdb3b8
Herb Derby <herb@google.com>
2018-04-26 12:59:08 -0400

    Track tail of strike cache
    Change-Id: Ic5d5ae5d4c45d0bc5b82dd2257752184a8b529d0

--- src/core/SkStrikeCache.cpp
+++ src/core/SkStrikeCache.cpp
@@ -342,16 +342,6 @@ void SkStrikeCache::forEachStrike(std::function<void(const SkGlyphCache&)> visit
     }
 }
 
-SkStrikeCache::Node* SkStrikeCache::internalGetTail() const {
-    Node* node = fHead;
-    if (node) {
-        while (node->fNext) {
-            node = node->fNext;
-        }
-    }
-    return node;
-}
-
 size_t SkStrikeCache::internalPurge(size_t minBytesNeeded) {
     this->validate();
 
@@ -415,6 +405,10 @@ void SkStrikeCache::internalAttachToHead(Node* node) {
         node->fNext = fHead;
     }
     fHead = node;
+    
+    if (fTail == nullptr) {
+        fTail = node;
+    }
 
     fCacheCount += 1;
     fTotalMemoryUsed += node->fCache.getMemoryUsed();
@@ -432,6 +426,8 @@ void SkStrikeCache::internalDetachCache(Node* node) {
     }
     if (node->fNext) {
         node->fNext->fPrev = node->fPrev;
+    } else {
+        fTail = node->fPrev;
     }
     node->fPrev = node->fNext = nullptr;
 }
