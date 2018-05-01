a995508620f2404d7ee855e97d8f3a781ba72313
Khushal <khushalsagar@chromium.org>
2018-05-01 15:30:38 -0700

    fonts: Bandaid fix for desc mismatch in SkRemoteGlyphCache.
    Since the typeface proxies on the client don't perform the same
    filtering done on the server during SkDescriptor generation, it causes
    the desc mismatches during raster. Disable this filtering on the server
    until this is resolved.
    
    Bug: 831354
    Change-Id: I5683372fb497a4874dede5aec9c734cd1392872c

--- tests/SkRemoteGlyphCacheTest.cpp
+++ tests/SkRemoteGlyphCacheTest.cpp
@@ -89,8 +89,6 @@ DEF_TEST(SkRemoteGlyphCache_TypefaceSerialization, reporter) {
                                       server_tf->uniqueID());
 }
 
-#if 0
-TODO(khushalsagar): Re-enable once crbug.com/831354 is fixed.
 DEF_TEST(SkRemoteGlyphCache_StrikeSerialization, reporter) {
     sk_sp<DiscardableManager> discardableManager = sk_make_sp<DiscardableManager>();
     SkStrikeServer server(discardableManager.get());
@@ -124,7 +122,6 @@ DEF_TEST(SkRemoteGlyphCache_StrikeSerialization, reporter) {
         }
     }
 }
-#endif
 
 DEF_TEST(SkRemoteGlyphCache_StrikeLockingServer, reporter) {
     sk_sp<DiscardableManager> discardableManager = sk_make_sp<DiscardableManager>();
