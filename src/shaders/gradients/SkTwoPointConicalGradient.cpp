3a14afcac17f8c9afe7bef4020736ad8f2216b84
Mike Reed <reed@google.com>
2018-05-02 16:16:10 -0400

    check for valid matrix in twopoint gradient
    Bug: oss-fuzz:6146
    Change-Id: Ibac5e0730131be432330b8d07648c2ba64b1d6b5

--- src/shaders/gradients/SkTwoPointConicalGradient.cpp
+++ src/shaders/gradients/SkTwoPointConicalGradient.cpp
@@ -5,10 +5,10 @@
  * found in the LICENSE file.
  */
 
-#include "SkTwoPointConicalGradient.h"
-
+#include "SkFloatingPoint.h"
 #include "SkRasterPipeline.h"
 #include "SkReadBuffer.h"
+#include "SkTwoPointConicalGradient.h"
 #include "SkWriteBuffer.h"
 #include "../../jumper/SkJumper.h"
 
@@ -57,7 +57,7 @@ sk_sp<SkShader> SkTwoPointConicalGradient::Create(const SkPoint& c0, SkScalar r0
 
     if (SkScalarNearlyZero((c0 - c1).length())) {
         // Concentric case: we can pretend we're radial (with a tiny twist).
-        const SkScalar scale = 1.0f / SkTMax(r0, r1);
+        const SkScalar scale = sk_ieee_float_divide(1, SkTMax(r0, r1));
         gradientMatrix = SkMatrix::MakeTrans(-c1.x(), -c1.y());
         gradientMatrix.postScale(scale, scale);
 
@@ -73,7 +73,9 @@ sk_sp<SkShader> SkTwoPointConicalGradient::Create(const SkPoint& c0, SkScalar r0
 
         gradientType = SkScalarNearlyZero(r1 - r0) ? Type::kStrip : Type::kFocal;
     }
-
+    if (!gradientMatrix.isFinite()) {
+        return nullptr;
+    }
     FocalData focalData;
     if (gradientType == Type::kFocal) {
         const auto dCenter = (c0 - c1).length();
