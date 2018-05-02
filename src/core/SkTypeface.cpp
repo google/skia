fa2b68cf10a412857785bf733488b7b7371267e0
Herb Derby <herb@google.com>
2018-05-02 17:40:24 -0400

    Make a cached unichar to glyph
    Change-Id: Id2064029076418fc474c9f5312a9388786417d3c

--- src/core/SkTypeface.cpp
+++ src/core/SkTypeface.cpp
@@ -13,14 +13,36 @@
 #include "SkMutex.h"
 #include "SkOTTable_OS_2.h"
 #include "SkOnce.h"
+#include "SkSharedMutex.h"
 #include "SkStream.h"
+#include "SkTHash.h"
 #include "SkTypeface.h"
 #include "SkTypefaceCache.h"
+#include "SkUtils.h"
+
+class SkTypeface::CodePointCache {
+public:
+    explicit CodePointCache(SkTypeface*);
+
+    size_t utfNToGlyphs(
+        const void *text,
+        size_t bytes,
+        SkTypeface::Encoding encoding,
+        SkGlyphID glyphs[]);
+
+private:
+    SkTypeface* const fTypeface;
+    SkSharedMutex fMu;
+    SkTHashMap<SkUnichar, SkGlyphID> fMap;
+};
 
 SkTypeface::SkTypeface(const SkFontStyle& style, bool isFixedPitch)
-    : fUniqueID(SkTypefaceCache::NewFontID()), fStyle(style), fIsFixedPitch(isFixedPitch) { }
+    : fUniqueID(SkTypefaceCache::NewFontID())
+    , fStyle(style)
+    , fIsFixedPitch(isFixedPitch)
+    , fCodePointCache(skstd::make_unique<CodePointCache>(this)) { }
 
-SkTypeface::~SkTypeface() { }
+SkTypeface::~SkTypeface() = default;
 
 #ifdef SK_WHITELIST_SERIALIZED_TYPEFACES
 extern void WhitelistSerializeTypeface(const SkTypeface*, SkWStream* );
@@ -259,6 +281,11 @@ int SkTypeface::charsToGlyphs(const void* chars, Encoding encoding,
     return this->onCharsToGlyphs(chars, encoding, glyphs, glyphCount);
 }
 
+size_t SkTypeface::charsToGlyphs(SkUtfNGlyphIDAllocation *alloc) {
+    return fCodePointCache->utfNToGlyphs(
+        alloc->utfN, alloc->byteLength, alloc->encoding, alloc->glyphs.get());
+}
+
 int SkTypeface::countGlyphs() const {
     return this->onCountGlyphs();
 }
@@ -367,4 +394,73 @@ bool SkTypeface::onComputeBounds(SkRect* bounds) const {
 std::unique_ptr<SkAdvancedTypefaceMetrics> SkTypeface::onGetAdvancedMetrics() const {
     SkDEBUGFAIL("Typefaces that need to work with PDF backend must override this.");
     return nullptr;
+}
+
+SkTypeface::CodePointCache::CodePointCache(SkTypeface* typeface)
+    : fTypeface{typeface} { }
+
+size_t SkTypeface::CodePointCache::utfNToGlyphs(
+    const void *text, size_t bytes, SkTypeface::Encoding encoding, uint16_t glyphs[])
+{
+    // Just no.
+    if (bytes >= std::numeric_limits<uint32_t>::max()) {
+        return 0;
+    }
+
+    size_t glyphCount = 0;
+
+    struct Missing {
+        uint32_t codepoint;
+        uint32_t position;
+    };
+
+    struct MissingLess {
+        bool operator()(const Missing &a, const Missing &b) {
+            return a.codepoint < b.codepoint;
+        }
+    };
+
+    std::vector<Missing> missing;
+
+    fMu.acquireShared();
+    if (fMap.count() > 0) {
+        auto convert = [&glyphs, &missing, this](size_t i, uint32_t c) {
+            if (auto glyph = fMap.find(c)) {
+                glyphs[i] = *glyph;
+            } else {
+                missing.push_back(Missing{c, SkTo<uint32_t>(i)});
+            }
+        };
+        glyphCount = SkParseUnicode(text, bytes, encoding, convert);
+        fMu.releaseShared();
+    } else {
+        // Can release the lock early for first time.
+        fMu.releaseShared();
+        auto convert = [&missing](size_t i, uint32_t c) {
+            missing.emplace_back(Missing{c, SkTo<uint32_t>(i)});
+        };
+        glyphCount = SkParseUnicode(text, bytes, encoding, convert);
+    }
+
+    if (missing.empty() || glyphCount == 0) {
+        return glyphCount;
+    } else {
+        // Sort to get the codepoints in order.
+        std::sort(missing.begin(), missing.end(), MissingLess());
+
+        // Find all the missing pairs, and fill in the glyphs using positions.
+        uint32_t prevCodepoint = std::numeric_limits<uint32_t>::max(); // Not a valid code point
+        uint16_t glyph = 0;
+        for (auto& m : missing) {
+            if (m.codepoint != prevCodepoint) {
+                fTypeface->onCharsToGlyphs(&m.codepoint, kUTF32_Encoding, &glyph, 1);
+                prevCodepoint = m.codepoint;
+                SkAutoMutexAcquire l(fMu);
+                fMap.set(m.codepoint, glyph);
+            }
+            glyphs[m.position] = glyph;
+        }
+    }
+
+    return glyphCount;
 }
