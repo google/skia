7909d9a81ab0c0d53295785fe3c276229ecb3176
Chris Dalton <csmartdalton@google.com>
2018-05-03 09:30:29 -0600

    Add support for building with MoltenVK
    Bug: skia:
    Change-Id: If55785d7fcc6e2c92c961ac390700add874c8d6d

--- tools/gpu/vk/VkTestUtils.cpp
+++ tools/gpu/vk/VkTestUtils.cpp
@@ -15,6 +15,12 @@ namespace sk_gpu_test {
 
 bool LoadVkLibraryAndGetProcAddrFuncs(PFN_vkGetInstanceProcAddr* instProc,
                                                    PFN_vkGetDeviceProcAddr* devProc) {
+#ifdef SK_MOLTENVK
+    // MoltenVK is a statically linked framework, so there is no Vulkan library to load.
+    *instProc = &vkGetInstanceProcAddr;
+    *devProc = &vkGetDeviceProcAddr;
+    return true;
+#else
     static void* vkLib = nullptr;
     static PFN_vkGetInstanceProcAddr localInstProc = nullptr;
     static PFN_vkGetDeviceProcAddr localDevProc = nullptr;
@@ -38,6 +44,7 @@ bool LoadVkLibraryAndGetProcAddrFuncs(PFN_vkGetInstanceProcAddr* instProc,
     *instProc = localInstProc;
     *devProc = localDevProc;
     return true;
+#endif
 }
 }
 
