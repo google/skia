df65785c3cedd890e46c211b2153449fdb04096c
Brian Salomon <bsalomon@google.com>
2018-05-04 13:43:19 -0400

    Make GPU lattice/nine patch not bleed across cells.
    Consolidate code for handling various image/bitmap and lattice/ninepatch flavors.
    
    Makes refTextureForParams virtual on GrTextureProducer. Previously both subclasses had non-virtual flavors of this.
    
    Bug: b/77917978
    
    Change-Id: I14787faef33c4617ef359039e81453d683f33ff1

--- src/gpu/GrTextureAdjuster.cpp
+++ src/gpu/GrTextureAdjuster.cpp
@@ -65,8 +65,11 @@ sk_sp<GrTextureProxy> GrTextureAdjuster::refTextureProxyCopy(const CopyParams& c
     return copy;
 }
 
-sk_sp<GrTextureProxy> GrTextureAdjuster::refTextureProxySafeForParams(const GrSamplerState& params,
-                                                                      SkScalar scaleAdjust[2]) {
+sk_sp<GrTextureProxy> GrTextureAdjuster::onRefTextureProxyForParams(
+        const GrSamplerState& params,
+        SkColorSpace* dstColorSpace,
+        sk_sp<SkColorSpace>* texColorSpace,
+        SkScalar scaleAdjust[2]) {
     sk_sp<GrTextureProxy> proxy = this->originalProxyRef();
     CopyParams copyParams;
 
@@ -75,6 +78,9 @@ sk_sp<GrTextureProxy> GrTextureAdjuster::refTextureProxySafeForParams(const GrSa
         return nullptr;
     }
 
+    if (texColorSpace) {
+        *texColorSpace = sk_ref_sp(fColorSpace);
+    }
     SkASSERT(this->width() <= fContext->caps()->maxTextureSize() &&
              this->height() <= fContext->caps()->maxTextureSize());
 
@@ -104,7 +110,7 @@ std::unique_ptr<GrFragmentProcessor> GrTextureAdjuster::createFragmentProcessor(
     }
     SkScalar scaleAdjust[2] = { 1.0f, 1.0f };
     sk_sp<GrTextureProxy> proxy(
-            this->refTextureProxySafeForParams(samplerState, scaleAdjust));
+            this->refTextureProxyForParams(samplerState, nullptr, nullptr, scaleAdjust));
     if (!proxy) {
         return nullptr;
     }
