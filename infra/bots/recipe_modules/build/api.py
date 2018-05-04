e7903bbb6b0eaaf35e08c0a6e58c621556c44b5e
Eric Boren <borenet@google.com>
2018-05-04 12:11:18 -0400

    [recipes] Separate build and test flavors
    Bug: skia:6473
    Change-Id: If1d6e1f64ec53624e7b540abd9d8a61cc82c4e0a

---
+++ infra/bots/recipe_modules/build/api.py
@@ -0,0 +1,50 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Build Skia for various platforms."""
+
+
+from recipe_engine import recipe_api
+
+from . import android
+from . import chromebook
+from . import chromecast
+from . import default
+from . import flutter
+from . import util
+
+
+class BuildApi(recipe_api.RecipeApi):
+  def __init__(self, buildername, *args, **kwargs):
+    b = buildername
+    if 'Android' in b and not 'Flutter' in b:
+      self.compile_fn = android.compile_fn
+      self.copy_fn = android.copy_extra_build_products
+    elif 'Chromebook' in b:
+      self.compile_fn = chromebook.compile_fn
+      self.copy_fn = chromebook.copy_extra_build_products
+    elif 'Chromecast' in b:
+      self.compile_fn = chromecast.compile_fn
+      self.copy_fn = chromecast.copy_extra_build_products
+    elif 'Flutter' in b:
+      self.compile_fn = flutter.compile_fn
+      self.copy_fn = flutter.copy_extra_build_products
+    else:
+      self.compile_fn = default.compile_fn
+      self.copy_fn = default.copy_extra_build_products
+    super(BuildApi, self).__init__(*args, **kwargs)
+
+  def __call__(self, target):
+    """Compile the code."""
+    self.compile_fn(self.m, target, self.out_dir)
+
+  def copy_build_products(self, src, dst):
+    """Copy whitelisted build products from src to dst."""
+    util.copy_whitelisted_build_products(self.m, src, dst)
+    self.copy_fn(self.m, src, dst)
+
+  @property
+  def out_dir(self):
+    return self.m.vars.skia_out.join(self.m.vars.configuration)
