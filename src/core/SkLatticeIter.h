a172bd75619900fc0abda81670e772f07ba4a1d9
Leon Scroggins III <scroggo@google.com>
2018-05-08 11:44:36 -0400

    Make the src rects of truths in SkLatticeIter be integral.
    This will make the non-bleed gpu implementation more sane as we will
    know that the input src rects are at integers and if offset by +/-0.5
    we will be at pixel centers.
    
    No change in behavior, since src rects really were integral all along.
    
    Bug: b/77917978
    
    No-Tree-Checks: true
    No-Try: true
    No-Presubmit: true
    Change-Id: I7193ff0b38de5d10debc24a0121e643573c47cda
    Reviewed-On: https://skia-review.googlesource.com/125740
    Commit-Queue: Leon Scroggins <scroggo@google.com>
    Reviewed-By: Leon Scroggins <scroggo@google.com>
    Reviewed-on: https://skia-review.googlesource.com/126823

--- src/core/SkLatticeIter.h
+++ src/core/SkLatticeIter.h
@@ -34,8 +34,19 @@ public:
      *  isFixedColor and fixedColor specify if the rectangle is filled with a fixed color.
      *  If (*isFixedColor) is true, then (*fixedColor) contains the rectangle color.
      */
-    bool next(SkRect* src, SkRect* dst, bool* isFixedColor = nullptr,
+    bool next(SkIRect* src, SkRect* dst, bool* isFixedColor = nullptr,
               SkColor* fixedColor = nullptr);
+
+    /** Version of above that converts the integer src rect to a scalar rect. */
+    bool next(SkRect* src, SkRect* dst, bool* isFixedColor = nullptr,
+              SkColor* fixedColor = nullptr) {
+        SkIRect isrcR;
+        if (this->next(&isrcR, dst, isFixedColor, fixedColor)) {
+            *src = SkRect::Make(isrcR);
+            return true;
+        }
+        return false;
+    }
 
     /**
      *  Apply a matrix to the dst points.
@@ -50,8 +61,8 @@ public:
     }
 
 private:
-    SkTArray<SkScalar> fSrcX;
-    SkTArray<SkScalar> fSrcY;
+    SkTArray<int> fSrcX;
+    SkTArray<int> fSrcY;
     SkTArray<SkScalar> fDstX;
     SkTArray<SkScalar> fDstY;
     SkTArray<SkCanvas::Lattice::RectType> fRectTypes;
