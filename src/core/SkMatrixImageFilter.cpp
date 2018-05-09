39a1e4402d0d695d545027e58e900417e85d47b6
Robert Phillips <robertphillips@google.com>
2018-05-09 15:08:53 -0400

    Fix clipping of SkMatrixImageFilter input (take 2)
    Bug: skia:7931
    Change-Id: I2951a727bd3f61aeda0e1e322cd76e48f677f75f

--- src/core/SkMatrixImageFilter.cpp
+++ src/core/SkMatrixImageFilter.cpp
@@ -131,7 +131,15 @@ SkIRect SkMatrixImageFilter::onFilterNodeBounds(const SkIRect& src, const SkMatr
     matrix.postConcat(ctm);
     SkRect floatBounds;
     matrix.mapRect(&floatBounds, SkRect::Make(src));
-    return floatBounds.roundOut();
+    SkIRect result = floatBounds.roundOut();
+
+    if (kReverse_MapDirection == direction && kNone_SkFilterQuality != fFilterQuality) {
+        // When filtering we might need some pixels in the source that might be otherwise
+        // clipped off.
+        result.outset(1, 1);
+    }
+
+    return result;
 }
 
 void SkMatrixImageFilter::toString(SkString* str) const {
