26991171646868d8987c6fbcd91c5f73d2200244
Khushal <khushalsagar@chromium.org>
2018-05-15 11:54:34 -0700

    fonts: Handle fallback to using paths for text rendering for remoting.
    SkRemoteGlyphCache only sends images for glyphs, even for cases where
    the gpu falls back to drawing text as paths. This includes cases in
    SkDraw::ShouldDrawTextAsPaths and when the glyph exceeds the max bounds
    that can fit on the atlas. Fix this by identifying these cases in the
    renderer and sending paths instead.
    
    Note: We still don't handle distance field text correctly.
    
    R=herb@google.com, bsalomon@google.com
    
    Bug: skia:7913
    Change-Id: I17d4eccbeaa2e995ae67b61c76cebd27f8280329

--- src/core/SkGlyphCache.cpp
+++ src/core/SkGlyphCache.cpp
@@ -188,7 +188,9 @@ const void* SkGlyphCache::findImage(const SkGlyph& glyph) {
     return glyph.fImage;
 }
 
-void SkGlyphCache::initializeImage(SkGlyph* glyph, const volatile void* data, size_t size) {
+bool SkGlyphCache::initializeImage(SkGlyph* glyph, const volatile void* data, size_t size) {
+    if (glyph->fImage) return false;
+
     if (glyph->fWidth > 0 && glyph->fWidth < kMaxGlyphWidth) {
         size_t allocSize = glyph->allocImage(&fAlloc);
         // check that alloc() actually succeeded
@@ -198,6 +200,8 @@ void SkGlyphCache::initializeImage(SkGlyph* glyph, const volatile void* data, si
             fMemoryUsed += size;
         }
     }
+
+    return true;
 }
 
 const SkPath* SkGlyphCache::findPath(const SkGlyph& glyph) {
@@ -217,6 +221,24 @@ const SkPath* SkGlyphCache::findPath(const SkGlyph& glyph) {
         }
     }
     return glyph.fPathData ? glyph.fPathData->fPath : nullptr;
+}
+
+bool SkGlyphCache::initializePath(SkGlyph* glyph, const volatile void* data, size_t size) {
+    if (glyph->fPathData) return false;
+
+    if (glyph->fWidth) {
+        SkGlyph::PathData* pathData = fAlloc.make<SkGlyph::PathData>();
+        glyph->fPathData = pathData;
+        pathData->fIntercept = nullptr;
+        SkPath* path = new SkPath;
+        if (!path->readFromMemory(const_cast<const void*>(data), size)) {
+            delete path;
+            return false;
+        }
+        pathData->fPath = path;
+        fMemoryUsed += ComputePathSize(*path);
+    }
+    return true;
 }
 
 #include "../pathops/SkPathOpsCubic.h"
