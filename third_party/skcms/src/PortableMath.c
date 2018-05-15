7df9581bf362a314535aeb98e7b8609930fbc6b1
skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com <skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com>
2018-05-15 15:06:08 +0000

    Roll skia/third_party/skcms 28e8b32..14c31bf (6 commits)
    https://skia.googlesource.com/skcms.git/+log/28e8b32..14c31bf
    
    2018-05-15 mtklein@chromium.org temporarily turn on SKCMS_LEGACY_POWF for GN builds
    2018-05-15 mtklein@chromium.org spin sources list out of BUILD.gn
    2018-05-14 mtklein@chromium.org Reland "remove loop in powf"
    2018-05-14 mtklein@chromium.org Revert "remove loop in powf"
    2018-05-14 brianosman@google.com Adjust fuzzers to test against parametric approximated tables
    2018-05-14 mtklein@chromium.org remove loop in powf
    
    
    The AutoRoll server is located here: https://skcms-skia-roll.skia.org
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    TBR=herb@google.com
    
    Change-Id: I0ff3903370a7c07ce7e194ab44c35ff546b7f973

--- third_party/skcms/src/PortableMath.c
+++ third_party/skcms/src/PortableMath.c
@@ -50,13 +50,16 @@ float exp2f_(float x) {
 }
 
 float powf_(float x, float y) {
-    // Handling all the integral powers first increases our precision a little. If y is very large,
-    // this loop may never terminate, but for any reasonably large y, the approximation is fine.
+#if defined(SKCMS_LEGACY_POWF)
     float r = 1.0f;
     while (y >= 1.0f && y < 32) {
         r *= x;
         y -= 1.0f;
     }
-
-    return (x == 0) || (x == 1) ? x : r * exp2f_(log2f_(x) * y);
+    return (x == 0) || (x == 1) ? x
+                                : r * exp2f_(log2f_(x) * y);
+#else
+    return (x == 0) || (x == 1) ? x
+                                : exp2f_(log2f_(x) * y);
+#endif
 }
