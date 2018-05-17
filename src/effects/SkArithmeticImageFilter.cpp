02333cc93f0e544ea00f287cc05e22edbaf932fb
Kevin Lubick <kjlubick@google.com>
2018-05-17 11:29:10 -0400

    Return nullptr when ReadBuffer becomes invalid
    This especially helps in SkDrawLooper because we can bail out early
    instead of looping for a potentially long time, e.g. when fuzzed
    input says count is a large number.
    
    This also cleans up validate in a few spots, and adds validateCanReadN
    as a helper function.
    
    Bug: skia:7937
    Change-Id: Ic5eff357c8cadc91eeafc6e39c78c570ba74df2f

--- src/effects/SkArithmeticImageFilter.cpp
+++ src/effects/SkArithmeticImageFilter.cpp
@@ -86,6 +86,9 @@ sk_sp<SkFlattenable> ArithmeticImageFilterImpl::CreateProc(SkReadBuffer& buffer)
         k[i] = buffer.readScalar();
     }
     const bool enforcePMColor = buffer.readBool();
+    if (!buffer.isValid()) {
+        return nullptr;
+    }
     return SkArithmeticImageFilter::Make(k[0], k[1], k[2], k[3], enforcePMColor, common.getInput(0),
                                          common.getInput(1), &common.cropRect());
 }
