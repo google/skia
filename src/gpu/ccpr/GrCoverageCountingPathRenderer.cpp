fb0d3ef06c560717959bffd4b04ba0ee04198711
Chris Dalton <csmartdalton@google.com>
2018-05-21 14:26:17 -0600

    ccpr: Handle winding and even-odd in the same shader
    We are already waiting for an entire texture lookup anyway. A couple
    extra flops should still complete before the texture fetch. This also
    gives us better batching.
    
    Bug: skia:
    Change-Id: I83a7a8ba6c05cd1ad6b1756a987429233e69ed6c

--- src/gpu/ccpr/GrCoverageCountingPathRenderer.cpp
+++ src/gpu/ccpr/GrCoverageCountingPathRenderer.cpp
@@ -210,10 +210,10 @@ void GrCoverageCountingPathRenderer::preFlush(GrOnFlushResourceProvider* onFlush
             SkDEBUGCODE(numSkippedPaths += op->numSkippedInstances_debugOnly());
         }
         for (auto& clipsIter : rtPendingPaths->fClipPaths) {
-            clipsIter.second.placePathInAtlas(resources.get(), onFlushRP);
+            clipsIter.second.renderPathInAtlas(resources.get(), onFlushRP);
         }
     }
-    SkASSERT(resources->pathInstanceCount() == numPathDraws - numSkippedPaths);
+    SkASSERT(resources->nextPathInstanceIdx() == numPathDraws - numSkippedPaths);
 
     // Allocate the atlases and create instance buffers to draw them.
     if (!resources->finalize(onFlushRP, atlasDraws)) {
