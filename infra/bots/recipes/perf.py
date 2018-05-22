b80bbce8f170da796eed74618305a3f73a7146ce
Greg Daniel <egdaniel@google.com>
2018-05-22 10:34:24 -0400

    Add more blacklists for intel 405 vulkan perf bot.
    Bug: skia:
    Change-Id: I39cae17623e1b186607258cdd6929d797fcd6e15

--- infra/bots/recipes/perf.py
+++ infra/bots/recipes/perf.py
@@ -208,6 +208,7 @@ def nanobench_flags(api, bot):
   if 'IntelHD405' in bot and api.vars.is_linux and 'Vulkan' in bot:
     # skia:7322
     match.append('~desk_tiger8svg.skp_1')
+    match.append('~desk_wowwiki.skp')
     match.append('~keymobi_sfgate.skp_1')
     match.append('~keymobi_techcrunch_com.skp_1.1')
     match.append('~keymobi_techcrunch.skp_1.1')
