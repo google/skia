b46199c4645d972f4c062b8e85126864363604d7
Mike Klein <mtklein@chromium.org>
2018-05-24 12:33:23 -0400

    SkColorSpaceXformSteps
    Lots TODO.
    
    Change-Id: I95edb764b85a5140d432adb506c3b537869e6df4

---
+++ src/core/SkColorSpaceXformSteps.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkColorSpaceXformSteps_DEFINED
+#define SkColorSpaceXformSteps_DEFINED
+
+#include "SkColorSpace.h"
+#include "SkImageInfo.h"
+
+struct SkColorSpaceXformSteps {
+    SkColorSpaceXformSteps(SkColorSpace* src, SkAlphaType srcAT,
+                           SkColorSpace* dst);
+
+    // Source pipeline steps, pre-blend.
+    bool early_unpremul;
+    bool linearize_src;
+    bool late_unpremul;
+    bool gamut_transform;
+    bool early_encode;
+    bool premul;
+
+    // Destination pipeline steps, pre-blend.
+    bool linearize_dst;
+
+    // Post-blend steps.
+    bool late_encode;
+
+/* TODO
+    SkColorSpaceTransferFn srcTFInv,  // Apply for linearize_src.
+                           dstTFInv,  // Apply for linearize_dst.
+                           dstTF;     // Apply for early_encode or late_encode.
+    float src_to_dst_matrix[9];       // Apply this 3x3 row-major matrix for gamut_transform.
+*/
+};
+
+
+
+#endif//SkColorSpaceXformSteps_DEFINED
---
+++ tests/SkColorSpaceXformStepsTest.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkColorSpaceXformSteps.h"
+#include "Test.h"
+
+template <typename T>
+static void check_eq(skiatest::Reporter* r, SkColorSpaceXformSteps steps, T baseline) {
+    REPORTER_ASSERT(r, steps.early_unpremul  == baseline.early_unpremul);
+    REPORTER_ASSERT(r, steps.linearize_src   == baseline.linearize_src);
+    REPORTER_ASSERT(r, steps.late_unpremul   == baseline.late_unpremul);
+    REPORTER_ASSERT(r, steps.gamut_transform == baseline.gamut_transform);
+    REPORTER_ASSERT(r, steps.early_encode    == baseline.early_encode);
+    REPORTER_ASSERT(r, steps.premul          == baseline.premul);
+    REPORTER_ASSERT(r, steps.linearize_dst   == baseline.linearize_dst);
+    REPORTER_ASSERT(r, steps.late_encode     == baseline.late_encode);
+}
+
+DEF_TEST(SkColorSpaceXformSteps, r) {
+    auto srgb_L = SkColorSpace::MakeSRGB(),
+         srgb_N = SkColorSpace::MakeSRGB()->makeNonlinearBlending();
+
+    struct {
+        sk_sp<SkColorSpace> src, dst;
+        SkAlphaType         srcAT;
+
+        bool early_unpremul;
+        bool linearize_src;
+        bool late_unpremul;
+
+        bool gamut_transform;
+        bool early_encode;
+        bool premul;
+
+        bool linearize_dst;
+        bool late_encode;
+    } tests[] = {
+        // This is essentially legacy 8888, non-linearly blended srcs and dsts.
+        // With optimization one day these will all be false.
+        { srgb_N, srgb_N, kPremul_SkAlphaType,
+            true,   // src is encoded as f(s)*a,a, so we unpremul to f(s),a before linearizing.
+            true,   // Linearize to s,a.
+            false,
+
+            true,  // Gamut transform.
+            true,  // Non-linear blending, so we encode to sRGB g(s),a early.
+            true,  // Premul to g(s)*a,a
+
+            false,  // Non-linear blending, so no need to linearize dst.
+            false,  // Non-linear blending, so the output of our blend function is what we want.
+        },
+
+        // This is <canvas>'s linearly blended sRGB use case.
+        { srgb_L, srgb_L, kPremul_SkAlphaType,
+            false,  // src is encoded as f(s*a),a, so we linearize before unpremul.
+            true,   // Linearize,
+            true,   // then unpremul (we haven't optimized away unpremul-premul pairs yet).
+
+            true,   // (We haven't optimized away identity gamut transforms yet.)
+            false,  // We're doing linear blending, so we don't encode to sRGB yet.
+            true,   // (We haven't optimized away unpremul-premul pairs yet.)
+
+            true,   // We're doing linear blending, so we need to linearize dst.
+            true,   // Once blending is done, finally encode to sRGB.
+        },
+
+        { srgb_L, srgb_N, kPremul_SkAlphaType,
+            false,  // src is encoded as f(s*a),a, so we linearize before unpremul.
+            true,   // Linearize,
+            true,   // then unpremul.
+
+            true,   // (We haven't optimized away identity gamut transforms yet.)
+            true,   // We're doing non-linear blending, so encode back to sRGB now.
+            true,   // (non-linear) premul
+
+            false,  // We're doing non-linear blending, so dst is already ready to blend.
+            false,  // The output of the blend is just what we want.
+        },
+
+        { srgb_N, srgb_L, kPremul_SkAlphaType,
+            true,   // src is encoded as f(s)*a,a, so we unpremul to f(s),a before linearizing.
+            true,   // Linearize to s,a.
+            false,
+
+            true,   // (We haven't optimized away identity gamut transforms yet.)
+            false,  // We're doing linear blending, so we don't encode to sRGB yet.
+            true,   // (linear) premul
+
+            true,   // We're doing linear blending, so we need to linearize dst.
+            true,   // Once blending is done, finally encode to sRGB.
+        },
+
+        // TODO:
+        //   This function has essentially 256 different outputs.
+        //   It's an interesting question how to test to make sure it's correct.
+        //
+        // Here are some ideas for further test cases:
+        //  - all combinations with the same gamut and transfer function,
+        //    varying alpha type and non-linear blending bit (above is 1, 7 cases left).
+        //
+        //  - all combinations in both directions with the same gamut and different
+        //    (non-identity) transfer function  (16 cases)
+        //
+        //  - all combinations in both directions with different gamuts and the same
+        //    (non-identity) transfer function  (16 cases)
+        //
+        //  - all combinations in both directions with different gamuts and different
+        //    (non-identity) transfer function  (16 cases)
+        //
+        //  - mix some identity transfer functions in there to taste.
+        //  - test unpremul sources?  are they any different than opaque?
+    };
+
+    for (auto t : tests) {
+        SkColorSpaceXformSteps steps(t.src.get(), t.srcAT, t.dst.get());
+        check_eq(r, steps, t);
+
+        // Opaque and unpremul sources should always go through the same steps.
+        if (t.srcAT == kOpaque_SkAlphaType) {
+            SkColorSpaceXformSteps unpremul(t.src.get(), kUnpremul_SkAlphaType, t.dst.get());
+            check_eq(r, unpremul, steps);
+        }
+    }
+}
