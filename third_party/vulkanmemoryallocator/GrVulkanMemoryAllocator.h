03da0117a995a940c404bddadaff9f40f63f45b3
Greg Daniel <egdaniel@google.com>
2018-05-24 16:58:36 -0400

    Create API for GrVkMemoryAllocator and impliment use of AMD VulkanMemoryAllocator on this API.
    Bug: skia:
    Change-Id: I1e122e1b11ab308c2f83cb98c36c81511f4507d0

---
+++ third_party/vulkanmemoryallocator/GrVulkanMemoryAllocator.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// We use this header to include vk_mem_alloc.h to make sure we always include GrVkDefines.h first.
+// We need to do this so that the corect defines are setup before we include vulkan.h inside of
+// vk_mem_alloc.h
+
+#ifndef GrVulkanMemoryAllocator_DEFINED
+#define GrVulkanMemoryAllocator_DEFINED
+
+#include "../../include/gpu/vk/GrVkDefines.h"
+#include "include/vk_mem_alloc.h"
+
+#endif
