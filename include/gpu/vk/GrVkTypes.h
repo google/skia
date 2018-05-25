98bddf930e4a8767bca60d84d585c386fabfc6b7
Greg Daniel <egdaniel@google.com>
2018-05-25 17:25:17 +0000

    Revert "Create API for GrVkMemoryAllocator and impliment use of AMD VulkanMemoryAllocator on this API."
    This reverts commit 26c0e4c1f54759249c2d61b50fb5430bd73793f2.
    
    Reason for revert: breaks roll into fuchsia
    
    Original change's description:
    > Create API for GrVkMemoryAllocator and impliment use of AMD VulkanMemoryAllocator on this API.
    > 
    > Bug: skia:
    > Change-Id: I1e122e1b11ab308c2f83cb98c36c81511f4507d0
    > Reviewed-on: https://skia-review.googlesource.com/129980
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Jim Van Verth <jvanverth@google.com>
    
    TBR=egdaniel@google.com,jvanverth@google.com,bsalomon@google.com,bungeman@google.com
    
    Change-Id: I6c74978f778987c422e6162e7dd85ea9c6baa0e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:
    Reviewed-on: https://skia-review.googlesource.com/130182
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- include/gpu/vk/GrVkTypes.h
+++ include/gpu/vk/GrVkTypes.h
@@ -26,9 +26,6 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
-
-typedef intptr_t GrVkBackendMemory;
-
 /**
  * Types for interacting with Vulkan resources created externally to Skia. GrBackendObjects for
  * Vulkan textures are really const GrVkImageInfo*
@@ -39,7 +36,6 @@ struct GrVkAlloc {
             , fOffset(0)
             , fSize(0)
             , fFlags(0)
-            , fBackendMemory(0)
             , fUsesSystemHeap(false) {}
 
     GrVkAlloc(VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, uint32_t flags)
@@ -47,18 +43,15 @@ struct GrVkAlloc {
             , fOffset(offset)
             , fSize(size)
             , fFlags(flags)
-            , fBackendMemory(0)
             , fUsesSystemHeap(false) {}
 
-    VkDeviceMemory    fMemory;  // can be VK_NULL_HANDLE iff is an RT and is borrowed
-    VkDeviceSize      fOffset;
-    VkDeviceSize      fSize;    // this can be indeterminate iff Tex uses borrow semantics
-    uint32_t          fFlags;
-    GrVkBackendMemory fBackendMemory; // handle to memory allocated via GrVkMemoryAllocator.
+    VkDeviceMemory fMemory;  // can be VK_NULL_HANDLE iff is an RT and is borrowed
+    VkDeviceSize   fOffset;
+    VkDeviceSize   fSize;    // this can be indeterminate iff Tex uses borrow semantics
+    uint32_t       fFlags;
 
     enum Flag {
         kNoncoherent_Flag = 0x1,   // memory must be flushed to device after mapping
-        kMappable_Flag    = 0x2,   // memory is able to be mapped.
     };
 
     bool operator==(const GrVkAlloc& that) const {
