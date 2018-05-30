9febd0b9f18ccbfaa92735d77209bf77a13faa4f
Leon Scroggins III <scroggo@google.com>
2018-05-30 14:41:11 -0400

    Add a dummy SkColorTable.cpp in src/codec
    I'm planning to move the real SkColorTable.cpp to src/codec, but that
    will require updating Chromium's BUILD.gn file to build it. Put this one
    here so that Chromium has something to build until the real move takes
    place.
    
    Change-Id: I1f217cfc1f7841db34781510ef851497c58c086d
    Reviewed-on: https://skia-review.googlesource.com/130940
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Leon Scroggins <scroggo@google.com>

---
+++ src/codec/SkColorTable.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// Dummy file to assist in landing https://skia-review.googlesource.com/c/skia/+/130845
