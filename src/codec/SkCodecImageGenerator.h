e512f880c0790af916f13fd6c002a172ace791fe
Leon Scroggins III <scroggo@google.com>
2018-05-30 15:33:46 -0400

    Move SkColorTable to src/codec
    It is no longer needed anywhere else, and simply happens to be an
    implementation detail of SkCodec (at least for now).
    
    Remove references to SkColorTable in other classes, and clean up some
    includes of SkConvertPixels I found along the way.
    
    Remove unused includes/methods on SkColorTable
    
    Change-Id: I46c8e46f5b77c37710b6cf595d48107d55871d52

--- src/codec/SkCodecImageGenerator.h
+++ src/codec/SkCodecImageGenerator.h
@@ -8,7 +8,6 @@
 #define SkCodecImageGenerator_DEFINED
 
 #include "SkCodec.h"
-#include "SkColorTable.h"
 #include "SkData.h"
 #include "SkImageGenerator.h"
 
@@ -38,7 +37,6 @@ private:
 
     std::unique_ptr<SkCodec> fCodec;
     sk_sp<SkData> fData;
-    sk_sp<SkColorTable> fColorTable;
 
     typedef SkImageGenerator INHERITED;
 };
