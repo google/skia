efe60359a5edb67ad39f6efe57b04b4ebdf2bb8a
Robert Phillips <robertphillips@google.com>
2018-05-30 12:27:33 -0400

    Allow CCPR in DDL mode (take 2)
    Bug: skia:7988
    Change-Id: I704ff793d80b18e7312048538874498824803580

--- include/private/SkDeferredDisplayList.h
+++ include/private/SkDeferredDisplayList.h
@@ -11,7 +11,10 @@
 #include "SkSurfaceCharacterization.h"
 
 #if SK_SUPPORT_GPU
+#include <map>
 #include "GrOpList.h"
+
+struct GrCCPerOpListPaths;
 #endif
 
 class SkSurface;
@@ -22,7 +25,7 @@ class SkSurface;
  *
  * TODO: we probably need to expose this class so users can query it for memory usage.
  */
-class SkDeferredDisplayList {
+class SK_API SkDeferredDisplayList {
 public:
 
 #if SK_SUPPORT_GPU
@@ -42,6 +45,7 @@ public:
 
     SkDeferredDisplayList(const SkSurfaceCharacterization& characterization,
                           sk_sp<LazyProxyData>);
+    ~SkDeferredDisplayList();
 
     const SkSurfaceCharacterization& characterization() const {
         return fCharacterization;
@@ -54,7 +58,11 @@ private:
     const SkSurfaceCharacterization fCharacterization;
 
 #if SK_SUPPORT_GPU
+    // This needs to match the same type in GrCoverageCountingPathRenderer.h
+    using PendingPathsMap = std::map<uint32_t, sk_sp<GrCCPerOpListPaths>>;
+
     SkTArray<sk_sp<GrOpList>>    fOpLists;
+    PendingPathsMap              fPendingPaths;  // This is the path data from CCPR.
 #endif
     sk_sp<LazyProxyData>         fLazyProxyData;
 };
