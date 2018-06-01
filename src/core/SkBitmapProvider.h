d3110f0a4cf380db145e7e1db232799f762c38aa
Brian Osman <brianosman@google.com>
2018-06-01 11:02:08 -0400

    Dest color space no longer impacts mipmaps or texture sampling
    PS5: Removes SkDestinationSurfaceColorMode, tracking of mipmap
    mode on GrTexture, sRGB decode state per-texture. Because we
    were often choosing sRGB configs for RGB color types, legacy
    rendering would then be incorrect (too dark). So...
    
    PS7: Stops ever using sRGB pixel configs when translating
    image info or color type. Also removes a bunch of GrCaps bits
    and a GrContextOption that are no longer relevant.
    
    PS9: Adjusts surface creation unit test expectations, and
    changes the raster rules accordingly.
    
    At this point, sRGB configs are (obviously) going to be broken.
    
    Locally, I ran 8888, gl, and the gbr- versions of both. Across
    all GMs x configs, there are 13 diffs. 12 are GMs that create
    surfaces with a color-space attached (and thus, the offscreen
    is no longer getting sRGB pixel config). The only remainder
    constructs an SkPictureImageGenerator, (with an attached color
    space) and renders it to the gbr-gl canvas, which triggers a
    a tagged surface inside the generator.
    
    Bug: skia:
    Change-Id: Ie5edfa157dd799f3121e8173fc4f97f6c8ed6789

--- src/core/SkBitmapProvider.h
+++ src/core/SkBitmapProvider.h
@@ -13,20 +13,17 @@
 
 class SkBitmapProvider {
 public:
-    explicit SkBitmapProvider(const SkImage* img, SkColorSpace* dstColorSpace)
-        : fImage(img)
-        , fDstColorSpace(dstColorSpace) {
+    explicit SkBitmapProvider(const SkImage* img)
+        : fImage(img) {
         SkASSERT(img);
     }
     SkBitmapProvider(const SkBitmapProvider& other)
         : fImage(other.fImage)
-        , fDstColorSpace(other.fDstColorSpace)
     {}
 
     int width() const;
     int height() const;
     uint32_t getID() const;
-    SkColorSpace* dstColorSpace() const { return fDstColorSpace; }
 
     SkImageInfo info() const;
     bool isVolatile() const;
@@ -47,7 +44,6 @@ private:
     // SkBitmapProvider is always short-lived/stack allocated, and the source image and destination
     // color space are guaranteed to outlive its scope => we can store raw ptrs to avoid ref churn.
     const SkImage* fImage;
-    SkColorSpace*  fDstColorSpace;
 };
 
 #endif
--- src/gpu/GrCaps.cpp
+++ src/gpu/GrCaps.cpp
@@ -44,7 +44,6 @@ GrCaps::GrCaps(const GrContextOptions& options) {
     fNPOTTextureTileSupport = false;
     fSRGBSupport = false;
     fSRGBWriteControl = false;
-    fSRGBDecodeDisableSupport = false;
     fDiscardRenderTargetSupport = false;
     fReuseScratchTextures = true;
     fReuseScratchBuffers = true;
@@ -156,7 +155,6 @@ void GrCaps::dumpJSON(SkJSONWriter* writer) const {
     writer->appendBool("NPOT Texture Tile Support", fNPOTTextureTileSupport);
     writer->appendBool("sRGB Support", fSRGBSupport);
     writer->appendBool("sRGB Write Control", fSRGBWriteControl);
-    writer->appendBool("sRGB Decode Disable", fSRGBDecodeDisableSupport);
     writer->appendBool("Discard Render Target Support", fDiscardRenderTargetSupport);
     writer->appendBool("Reuse Scratch Textures", fReuseScratchTextures);
     writer->appendBool("Reuse Scratch Buffers", fReuseScratchBuffers);
