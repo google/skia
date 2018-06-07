40eb0647bf53188405ef0144a8450a604c7bdf7a
Greg Daniel <egdaniel@google.com>
2018-06-07 14:22:59 -0400

    Require mips to be allocated at texture creation time and disable late allocations.
    If we get a non-mipped texture for a draw that wants to be use mip map filter, we
    will copy the texture into a new mipped texture.
    
    Clean up of unused code in the GPU backends for reallocating for mips will be done
    in a follow up CL.
    
    Bug: skia:
    Change-Id: Idab588c1abf4bbbf7eeceb3727d500e5df274188

--- src/gpu/GrGpu.cpp
+++ src/gpu/GrGpu.cpp
@@ -138,13 +138,7 @@ sk_sp<GrTexture> GrGpu::wrapBackendTexture(const GrBackendTexture& backendTex,
         backendTex.height() > this->caps()->maxTextureSize()) {
         return nullptr;
     }
-    sk_sp<GrTexture> tex = this->onWrapBackendTexture(backendTex, ownership);
-    if (tex && !backendTex.hasMipMaps()) {
-        // Ganesh will not ever allocate mipmaps for a wrapped resource. By setting this flag here,
-        // it will be propagated to any proxy that wraps this texture.
-        tex->texturePriv().setDoesNotSupportMipMaps();
-    }
-    return tex;
+    return this->onWrapBackendTexture(backendTex, ownership);
 }
 
 sk_sp<GrTexture> GrGpu::wrapRenderableBackendTexture(const GrBackendTexture& backendTex,
@@ -163,11 +157,6 @@ sk_sp<GrTexture> GrGpu::wrapRenderableBackendTexture(const GrBackendTexture& bac
         return nullptr;
     }
     sk_sp<GrTexture> tex = this->onWrapRenderableBackendTexture(backendTex, sampleCnt, ownership);
-    if (tex && !backendTex.hasMipMaps()) {
-        // Ganesh will not ever allocate mipmaps for a wrapped resource. By setting this flag here,
-        // it will be propagated to any proxy that wraps this texture.
-        tex->texturePriv().setDoesNotSupportMipMaps();
-    }
     SkASSERT(!tex || tex->asRenderTarget());
     return tex;
 }
