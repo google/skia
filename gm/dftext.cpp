8345aa6302c7ca25a4b9c2df913e83ef79220ab7
Jim Van Verth <jvanverth@google.com>
2018-06-07 11:30:53 -0400

    Change persp dftext to only antialiased
    Originally I had aliased text in here because I thought it would trigger
    the bug, but it doesn't and it's a bad idea for perspective text.
    Changed it to a reasonably large size just to get another test case.
    
    Bug: skia:8042
    Change-Id: Ia81161063c76431e04503349ad6b33afb7523605
    Reviewed-on: https://skia-review.googlesource.com/132833
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

--- gm/dftext.cpp
+++ gm/dftext.cpp
@@ -194,8 +194,6 @@ protected:
             canvas->drawText(text, textLen, 0, 0, paint);
         }
         {
-            paint.setSubpixelText(false);
-            paint.setAntiAlias(false);
             SkAutoCanvasRestore acr(canvas, true);
             SkMatrix persp;
             persp.setAll(0.9839f, 0, 0,
@@ -203,8 +201,7 @@ protected:
                          0.0002352f, -0.0003844f, 1);
             canvas->concat(persp);
             canvas->translate(1075, -245);
-            canvas->scale(375, 375);
-            paint.setTextSize(0.1f);
+            paint.setTextSize(37.5f);
             canvas->drawText(text, textLen, 0, 0, paint);
         }
 
