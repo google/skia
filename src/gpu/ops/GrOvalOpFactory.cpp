7c04ff5453f5b7756feedce5f4eb9ddf92a917d7
Brian Osman <brianosman@google.com>
2018-06-08 16:35:44 -0400

    Avoid degenerate matrices (that trigger asserts) in test code
    I encountered this on an in-progress CL when I jiggled the GLPrograms
    test a bit.
    
    Change-Id: I8454bf0f35836e3a668f74918fa0c5ef0e58a9b5

--- src/gpu/ops/GrOvalOpFactory.cpp
+++ src/gpu/ops/GrOvalOpFactory.cpp
@@ -3119,7 +3119,10 @@ GR_DRAW_OP_TEST_DEFINE(CircleOp) {
         SkScalar rotate = random->nextSScalar1() * 360.f;
         SkScalar translateX = random->nextSScalar1() * 1000.f;
         SkScalar translateY = random->nextSScalar1() * 1000.f;
-        SkScalar scale = random->nextSScalar1() * 100.f;
+        SkScalar scale;
+        do {
+            scale = random->nextSScalar1() * 100.f;
+        } while (scale == 0);
         SkMatrix viewMatrix;
         viewMatrix.setRotate(rotate);
         viewMatrix.postTranslate(translateX, translateY);
@@ -3148,7 +3151,10 @@ GR_DRAW_OP_TEST_DEFINE(ButtCapDashedCircleOp) {
     SkScalar rotate = random->nextSScalar1() * 360.f;
     SkScalar translateX = random->nextSScalar1() * 1000.f;
     SkScalar translateY = random->nextSScalar1() * 1000.f;
-    SkScalar scale = random->nextSScalar1() * 100.f;
+    SkScalar scale;
+    do {
+        scale = random->nextSScalar1() * 100.f;
+    } while (scale == 0);
     SkMatrix viewMatrix;
     viewMatrix.setRotate(rotate);
     viewMatrix.postTranslate(translateX, translateY);
