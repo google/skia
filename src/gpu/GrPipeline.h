f18c756360bde970c95a9ca5a47f8e6e12a6ba87
Robert Phillips <robertphillips@google.com>
2018-06-13 09:01:36 -0400

    Pull non-substantive changes out of omnibus CL
    https://skia-review.googlesource.com/c/skia/+/131500 (Move op memory
    storage to GrContext) is still a bit of a monster. Offload the trivial
    stuff.
    
    Change-Id: I27eb0c441d08111491b3cfee3b93d260c26c2790
    Reviewed-on: https://skia-review.googlesource.com/134501
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/gpu/GrPipeline.h
+++ src/gpu/GrPipeline.h
@@ -234,8 +234,6 @@ private:
 
     // This value is also the index in fFragmentProcessors where coverage processors begin.
     int fNumColorProcessors;
-
-    typedef SkRefCnt INHERITED;
 };
 
 #endif
