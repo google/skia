1f5ad6770123e2299b0dbdaa3839086c2c2f304b
Florin Malita <fmalita@chromium.org>
2018-06-14 14:47:46 -0400

    Reland [skjson] Size-constrained input API
    Pass an explicit input size instead of requiring a C string.
    
    Thanks to mtklein's clever trick, this has no measurable perf impact.
    
    Change-Id: Ic8cb1dc75f4d0814e5b2c80038d1b8d3a7b072ab

--- modules/skjson/src/FuzzSkJSON.cpp
+++ modules/skjson/src/FuzzSkJSON.cpp
@@ -5,20 +5,12 @@
  * found in the LICENSE file.
  */
 
-#include "SkAutoMalloc.h"
 #include "SkData.h"
 #include "SkJSON.h"
 #include "SkStream.h"
 
 void FuzzSkJSON(sk_sp<SkData> bytes) {
-    // TODO: add a size + len skjson::DOM factory?
-    SkAutoMalloc data(bytes->size() + 1);
-    auto* c_str = static_cast<char*>(data.get());
-
-    memcpy(c_str, bytes->data(), bytes->size());
-    c_str[bytes->size()] = '\0';
-
-    skjson::DOM dom(c_str);
+    skjson::DOM dom(static_cast<const char*>(bytes->data()), bytes->size());
     SkDynamicMemoryWStream wstream;
     dom.write(&wstream);
 }
