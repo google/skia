b0c9dfebdaa441a8c2248b6c9b121283e78b503b
Brian Osman <brianosman@google.com>
2018-06-14 10:48:26 -0400

    Remove GrPixelConfig argument to GrColorSpaceXform[Effect]::Make
    This was for sRGB configs, which are no longer treated special.
    Pulling this out of https://skia-review.googlesource.com/c/skia/+/132090
    to land separately.
    
    Change-Id: Id04c4e82ad4ffe375cfd384490839b0488e54d44

--- src/shaders/SkColorShader.cpp
+++ src/shaders/SkColorShader.cpp
@@ -198,9 +198,7 @@ SkShader::GradientType SkColor4Shader::asAGradient(GradientInfo* info) const {
 
 std::unique_ptr<GrFragmentProcessor> SkColor4Shader::asFragmentProcessor(
         const GrFPArgs& args) const {
-    // Construct an xform assuming float inputs. The color space can have a transfer function on
-    // it, which will be applied below.
-    auto colorSpaceXform = GrColorSpaceXform::Make(fColorSpace.get(), kRGBA_float_GrPixelConfig,
+    auto colorSpaceXform = GrColorSpaceXform::Make(fColorSpace.get(),
                                                    args.fDstColorSpaceInfo->colorSpace());
     GrColor4f color = GrColor4f::FromSkColor4f(fColor4);
     if (colorSpaceXform) {
