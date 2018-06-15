53ab986e3a68811b2c00da4bd572c4c1c71ca8c5
Ben Wagner <bungeman@google.com>
2018-06-15 16:56:22 +0000

    Revert "Reland "added 565 to 8888 conversion for gpu LCD text rendering for macOS""
    This reverts commit 0513dd8675534afdd605cea32778a4b4671b2c3d.
    
    Reason for revert: Still has static initializer.
    
    Original change's description:
    > Reland "added 565 to 8888 conversion for gpu LCD text rendering for macOS"
    > 
    > This reverts commit cf274da6faa9d32e08053180c8113fcaab666028.
    > 
    > Reason for revert: removed the static initializer
    > 
    > Original change's description:
    > > Revert "added 565 to 8888 conversion for gpu LCD text rendering for macOS"
    > >
    > > This reverts commit e6f2ecafaf0efecacfd4256f280e26ee74cb7e16.
    > >
    > > Reason for revert: breaking chrome roll cause of static initializers
    > >
    > > Original change's description:
    > > > added 565 to 8888 conversion for gpu LCD text rendering for macOS
    > > >
    > > > Bug: skia:
    > > > Change-Id: Ie24160bb098d388bf4ad69d0c2f9f8ed4beb215c
    > > > Reviewed-on: https://skia-review.googlesource.com/134508
    > > > Commit-Queue: Timothy Liang <timliang@google.com>
    > > > Reviewed-by: Jim Van Verth <jvanverth@google.com>
    > >
    > > TBR=egdaniel@google.com,jvanverth@google.com,timliang@google.com
    > >
    > > Change-Id: Ida97a4085c93fcb990999ab71f99364ec2ef86b7
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: skia:
    > > Reviewed-on: https://skia-review.googlesource.com/135000
    > > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > > Commit-Queue: Greg Daniel <egdaniel@google.com>
    > 
    > Change-Id: I40c29b73970c55b3579a0169bbad666a798b2348
    > Bug: skia:
    > Reviewed-on: https://skia-review.googlesource.com/135021
    > Reviewed-by: Jim Van Verth <jvanverth@google.com>
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Timothy Liang <timliang@google.com>
    
    TBR=egdaniel@google.com,jvanverth@google.com,timliang@google.com
    
    Change-Id: I2632d02adc9ba791e5a55adafe0ad10a63f50073
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:

--- src/gpu/GrContext.cpp
+++ src/gpu/GrContext.cpp
@@ -133,7 +133,6 @@ bool GrContext::initCommon(const GrContextOptions& options) {
                                                options.fSortRenderTargets));
 
     fGlyphCache = new GrGlyphCache(fCaps.get(), options.fGlyphCacheTextureMaximumBytes);
-    GrGlyphCache::InitMask();
 
     fTextBlobCache.reset(new GrTextBlobCache(TextBlobCacheOverBudgetCB,
                                              this, this->uniqueID()));
