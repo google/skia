9c7ed74a18c774b68c80621401bd420df778e0a8
Ethan Nicholas <ethannicholas@google.com>
2018-06-15 14:35:18 -0400

    added GrSkSLFP and converted DitherEffect to use it
    Bug: skia:
    Change-Id: I84b71165eab1712355f3c7669cee2d33d259f3df

--- src/gpu/glsl/GrGLSLShaderBuilder.h
+++ src/gpu/glsl/GrGLSLShaderBuilder.h
@@ -111,6 +111,8 @@ public:
 
     void codeAppend(const char* str) { this->code().append(str); }
 
+    void codeAppend(const char* str, size_t length) { this->code().append(str, length); }
+
     void codePrependf(const char format[], ...) SK_PRINTF_LIKE(2, 3) {
        va_list args;
        va_start(args, format);
