5b694475e648cbd778e5b25ff1e1972ed8d06aa8
ziadb <ziadb@google.com>
2018-06-15 10:55:45 -0400

    SkAR: drawing text, shapes, rotation modes enabled
    To run this app, you need to create an out directory as such:
    bin/gn gen out/arm64 --args='ndk="NDK_PATH" target_cpu="ABI"'
    
    For now, the only supported ABI is arm64
    
    Change-Id: I012f0c6a0550d80a0028f42177d5ca72974d848d
    Bug: skia:

---
+++ platform_tools/android/apps/arcore/src/main/java/org/skia/arcore/HelloArActivity.java
@@ -0,0 +1,358 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.skia.arcore;
+
+import android.hardware.display.DisplayManager;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * This is a simple example that shows how to create an augmented reality (AR) application using the
+ * ARCore C API.
+ */
+public class HelloArActivity extends AppCompatActivity
+        implements GLSurfaceView.Renderer, DisplayManager.DisplayListener {
+    private static final String TAG = HelloArActivity.class.getSimpleName();
+    private static final int SNACKBAR_UPDATE_INTERVAL_MILLIS = 1000; // In milliseconds.
+
+    private GLSurfaceView mSurfaceView;
+
+    private boolean mViewportChanged = false;
+    private int mViewportWidth;
+    private int mViewportHeight;
+
+    private int mCurrentObjectRotation = 0;
+    private float mCurrentValue = 0;
+
+    // Opaque native pointer to the native application instance.
+    private long mNativeApplication;
+    private GestureDetector mGestureDetector;
+
+    private Snackbar mLoadingMessageSnackbar;
+    private Handler mPlaneStatusCheckingHandler;
+    private final Runnable mPlaneStatusCheckingRunnable =
+            new Runnable() {
+                @Override
+                public void run() {
+                    // The runnable is executed on main UI thread.
+                    try {
+                        if (JniInterface.hasDetectedPlanes(mNativeApplication)) {
+                            if (mLoadingMessageSnackbar != null) {
+                                mLoadingMessageSnackbar.dismiss();
+                            }
+                            mLoadingMessageSnackbar = null;
+                        } else {
+                            mPlaneStatusCheckingHandler.postDelayed(
+                                    mPlaneStatusCheckingRunnable, SNACKBAR_UPDATE_INTERVAL_MILLIS);
+                        }
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            };
+
+    private ActionMode mActionMode = null;
+    private float mX = 0;
+    private float mY = 0;
+    private int mDrawMode = -1;
+
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Inflate a menu resource providing context menu items
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.draw_menu, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.draw_text:
+                    mDrawMode = 1;
+                    JniInterface.onTouched(mNativeApplication, mX, mY, mDrawMode);
+                    mode.finish(); // Action picked, so close the CAB
+                    return true;
+                case R.id.draw_shape:
+                    mDrawMode = 2;
+                    JniInterface.onTouched(mNativeApplication, mX, mY, mDrawMode);
+                    mode.finish(); // Action picked, so close the CAB
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //hide notifications bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+
+        mGestureDetector =
+                new GestureDetector(
+                        this,
+                        new GestureDetector.SimpleOnGestureListener() {
+                            @Override
+                            public boolean onSingleTapUp(final MotionEvent e) {
+                                mSurfaceView.queueEvent(
+                                        new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                JniInterface.onTouched(mNativeApplication, e.getX(), e.getY(), mDrawMode);
+                                            }
+                                        });
+                                return true;
+                            }
+
+                            @Override
+                            public boolean onDown(MotionEvent e) {
+                                return true;
+                            }
+                        });
+
+        mSurfaceView.setOnTouchListener(
+                new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        /*if (mActionMode != null) {
+                            return false;
+                            *//*if (mDrawMode == 0) {
+                                return false;
+                            }
+                            return mGestureDetector.onTouchEvent(event);*//*
+                        }
+
+                        // Start the CAB using the ActionMode.Callback defined above
+                        mX = event.getX();
+                        mY = event.getY();
+                        mActionMode = startActionMode(mActionModeCallback);*/
+                        return mGestureDetector.onTouchEvent(event);
+                    }
+                });
+
+        // Set up renderer.
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+        mSurfaceView.setRenderer(this);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        JniInterface.assetManager = getAssets();
+        mNativeApplication = JniInterface.createNativeApplication(getAssets());
+
+        mPlaneStatusCheckingHandler = new Handler();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.rotation_mode, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.rotation_axis_aligned:
+                mCurrentObjectRotation = 0;
+                break;
+            case R.id.rotation_camera_aligned:
+                mCurrentObjectRotation = 1;
+                break;
+            case R.id.rotation_snap_aligned:
+                mCurrentObjectRotation = 2;
+                break;
+            case R.id.action:
+                mCurrentValue = 180;
+                JniInterface.onAction(mNativeApplication, mCurrentValue);
+                return true;
+            default:
+                return true;
+        }
+        JniInterface.onObjectRotationChanged(mNativeApplication, mCurrentObjectRotation);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
+        JniInterface.onResume(mNativeApplication, getApplicationContext(), this);
+        mSurfaceView.onResume();
+
+        mLoadingMessageSnackbar =
+                Snackbar.make(
+                        HelloArActivity.this.findViewById(android.R.id.content),
+                        "Searching for surfaces...",
+                        Snackbar.LENGTH_INDEFINITE);
+        // Set the snackbar background to light transparent black color.
+        mLoadingMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+        mLoadingMessageSnackbar.show();
+        mPlaneStatusCheckingHandler.postDelayed(
+                mPlaneStatusCheckingRunnable, SNACKBAR_UPDATE_INTERVAL_MILLIS);
+
+        // Listen to display changed events to detect 180° rotation, which does not cause a config
+        // change or view resize.
+        getSystemService(DisplayManager.class).registerDisplayListener(this, null);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSurfaceView.onPause();
+        JniInterface.onPause(mNativeApplication);
+
+        mPlaneStatusCheckingHandler.removeCallbacks(mPlaneStatusCheckingRunnable);
+
+        getSystemService(DisplayManager.class).unregisterDisplayListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // Synchronized to avoid racing onDrawFrame.
+        synchronized (this) {
+            JniInterface.destroyNativeApplication(mNativeApplication);
+            mNativeApplication = 0;
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Standard Android full-screen functionality.
+            getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        JniInterface.onGlSurfaceCreated(mNativeApplication);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        mViewportWidth = width;
+        mViewportHeight = height;
+        mViewportChanged = true;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Synchronized to avoid racing onDestroy.
+        synchronized (this) {
+            if (mNativeApplication == 0) {
+                return;
+            }
+            if (mViewportChanged) {
+                int displayRotation = getWindowManager().getDefaultDisplay().getRotation();
+                JniInterface.onDisplayGeometryChanged(
+                        mNativeApplication, displayRotation, mViewportWidth, mViewportHeight);
+                mViewportChanged = false;
+            }
+            JniInterface.onGlSurfaceDrawFrame(mNativeApplication);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+
+    // DisplayListener methods
+    @Override
+    public void onDisplayAdded(int displayId) {
+    }
+
+    @Override
+    public void onDisplayRemoved(int displayId) {
+    }
+
+    @Override
+    public void onDisplayChanged(int displayId) {
+        mViewportChanged = true;
+    }
+}
