173cf372a95bf5fb138298454f63ca0c94ae0928
Ben Wagner <benjaminwagner@google.com>
2018-06-15 17:11:20 -0400

    Upgrade MacOS on MacBook10.1.
    Change-Id: I5d90f0c95a2d0199ec498dda7ad70064bb98aa22

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -425,6 +425,10 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 			// TODO(dogben): Temporarily add image dimension during upgrade.
 			d["image"] = "windows-server-2016-dc-v20180410"
 		}
+		if d["os"] == DEFAULT_OS_MAC && parts["model"] == "MacBook10.1" {
+			// TODO(dogben): Get all Mac bots on the same version.
+			d["os"] = "Mac-10.13.5"
+		}
 	} else {
 		d["os"] = DEFAULT_OS_DEBIAN
 	}
