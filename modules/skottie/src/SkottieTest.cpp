034cb1952a0bc341ea8e6c82a11e74ed9c1d4e9f
Florin Malita <fmalita@chromium.org>
2018-06-18 12:14:59 -0400

    [skottie] Fix OOB access in Parse<SkPoint>
    SkJSON requires valid array indices, so callers must guard against
    out-of-bounds conditions explicitly.
    
    Bug: oss-fuzz:8956
    Change-Id: I50b96b088e44a4c1a569e6911d4be5d75799b464

---
+++ modules/skottie/src/SkottieTest.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "Skottie.h"
+#include "SkStream.h"
+
+#include "Test.h"
+
+DEF_TEST(Skottie_Fuzz, reporter) {
+    static constexpr const char* tests[] = {
+        "{\"v\":\" \",\"fr\":3,\"w\":4,\"h\":3,\"layers\":[{\"ty\": 1, \"sw\": 10, \"sh\": 10,"
+            " \"sc\":\"#ffffff\", \"ks\":{\"o\":{\"a\": true, \"k\":"
+            " [{\"t\": 0, \"s\": 0, \"e\": 1, \"i\": {\"x\":[]}}]}}}]}",
+    };
+
+    for (const auto& tst : tests) {
+        SkMemoryStream stream(tst, strlen(tst));
+
+        // Passes if parsing doesn't crash.
+        auto animation = skottie::Animation::Make(&stream);
+    }
+}
