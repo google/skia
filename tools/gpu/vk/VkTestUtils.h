4b67036d7852cf5a563ee78a4e4afe037327b898
Greg Daniel <egdaniel@google.com>
2018-06-18 10:34:15 -0400

    Make GrVkBackendContext no longer derive from SkRefCnt.
    Bug: skia:
    Change-Id: I7e8e4912e7ef6fb00a7e2a00407aed5e83211799

--- tools/gpu/vk/VkTestUtils.h
+++ tools/gpu/vk/VkTestUtils.h
@@ -13,9 +13,21 @@
 #ifdef SK_VULKAN
 
 #include "vk/GrVkDefines.h"
+#include "vk/GrVkInterface.h"
+
+struct GrVkBackendContext;
 
 namespace sk_gpu_test {
     bool LoadVkLibraryAndGetProcAddrFuncs(PFN_vkGetInstanceProcAddr*, PFN_vkGetDeviceProcAddr*);
+
+    using CanPresentFn = std::function<bool(VkInstance, VkPhysicalDevice,
+                                            uint32_t queueFamilyIndex)>;
+
+    bool CreateVkBackendContext(const GrVkInterface::GetInstanceProc& getInstanceProc,
+                                const GrVkInterface::GetDeviceProc& getDeviceProc,
+                                GrVkBackendContext* ctx,
+                                uint32_t* presentQueueIndexPtr = nullptr,
+                                CanPresentFn canPresent = CanPresentFn());
 }
 
 #endif
