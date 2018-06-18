d246ddee28898d7053a240f5f3dd83886c0a148c
Mike Reed <reed@google.com>
2018-06-18 10:33:37 -0400

    include minimal code needed from pathops
    Bug: skia:
    Change-Id: I525cf4a2a56ffd7dae6c590a5d1d2dfc9a399516

---
+++ src/pathops/SkDummyPathOps.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkPathOps.h"
+
+bool Op(const SkPath& one, const SkPath& two, SkPathOp op, SkPath* result) {
+    return false;
+}
+
+bool Simplify(const SkPath& path, SkPath* result) {
+    return false;
+}
+
+bool TightBounds(const SkPath& path, SkRect* result) {
+    return false;
+}
+
