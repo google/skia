111fc2cb874bb490f7650346ee82e3936e78956b
Ruiqi Mao <ruiqimao@google.com>
2018-06-18 13:22:30 -0400

    Merge branch 'master' of https://skia.googlesource.com/skia
    add NIMA backend to a sample
    
    Bug: skia:
    Change-Id: Id2fa74fc31b49f9b07cc83e7f60477c7ab4f8d83

---
+++ samplecode/Nima.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef Nima_DEFINED
+#define Nima_DEFINED
+
+#include <nima/Actor.hpp>
+#include <nima/ActorImage.hpp>
+#include <nima/Vec2D.hpp>
+
+#include "SkCanvas.h"
+#include "SkImage.h"
+
+class SampleActor;
+class SampleActorImage;
+
+class SampleActor : public nima::Actor {
+public:
+    SampleActor(std::string baseName);
+    ~SampleActor();
+
+    void render(SkCanvas* canvas) const;
+
+private:
+    sk_sp<SkImage>                fTexture;
+    std::vector<SampleActorImage> fActorImages;
+    std::unique_ptr<SkPaint>      fPaint;
+
+    typedef nima::Actor INHERITED;
+};
+
+class SampleActorImage {
+public:
+    SampleActorImage(nima::ActorImage* actorImage, sk_sp<SkImage> texture, SkPaint* paint);
+    ~SampleActorImage();
+
+    void render(const SampleActor* actor, SkCanvas* canvas) const;
+
+    int drawOrder() const { return fActorImage->drawOrder(); }
+
+private:
+    nima::Vec2D deform(const nima::Vec2D& position, float* boneIdx, float* boneWgt) const;
+
+private:
+    nima::ActorImage* fActorImage;
+    sk_sp<SkImage>    fTexture;
+    SkPaint*          fPaint;
+};
+
+#endif
