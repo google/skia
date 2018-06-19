21df20e605d6ff36396f824ecb4dc7a753596ce5
Brian Salomon <bsalomon@google.com>
2018-06-19 13:40:32 -0400

    Remove hack to call ushort2 float2 when integers aren't supported.
    Moves and renames function that converts GrSLType to string to GrTypesPriv.h and deletes GrGLSL.cpp
    
    Bug: skia:
    Change-Id: I6b39315092a0c0ce95dca1c363f998fde15de0c3

--- include/private/GrTypesPriv.h
+++ include/private/GrTypesPriv.h
@@ -373,6 +373,85 @@ enum GrSLPrecision {
 
 static const int kGrSLPrecisionCount = kLast_GrSLPrecision + 1;
 
+static inline const char* GrSLTypeString(GrSLType t) {
+    switch (t) {
+        case kVoid_GrSLType:
+            return "void";
+        case kHalf_GrSLType:
+            return "half";
+        case kHalf2_GrSLType:
+            return "half2";
+        case kHalf3_GrSLType:
+            return "half3";
+        case kHalf4_GrSLType:
+            return "half4";
+        case kFloat_GrSLType:
+            return "float";
+        case kFloat2_GrSLType:
+            return "float2";
+        case kFloat3_GrSLType:
+            return "float3";
+        case kFloat4_GrSLType:
+            return "float4";
+        case kUint2_GrSLType:
+            return "uint2";
+        case kInt2_GrSLType:
+            return "int2";
+        case kInt3_GrSLType:
+            return "int3";
+        case kInt4_GrSLType:
+            return "int4";
+        case kFloat2x2_GrSLType:
+            return "float2x2";
+        case kFloat3x3_GrSLType:
+            return "float3x3";
+        case kFloat4x4_GrSLType:
+            return "float4x4";
+        case kHalf2x2_GrSLType:
+            return "half2x2";
+        case kHalf3x3_GrSLType:
+            return "half3x3";
+        case kHalf4x4_GrSLType:
+            return "half4x4";
+        case kTexture2DSampler_GrSLType:
+            return "sampler2D";
+        case kTextureExternalSampler_GrSLType:
+            return "samplerExternalOES";
+        case kTexture2DRectSampler_GrSLType:
+            return "sampler2DRect";
+        case kBufferSampler_GrSLType:
+            return "samplerBuffer";
+        case kBool_GrSLType:
+            return "bool";
+        case kInt_GrSLType:
+            return "int";
+        case kUint_GrSLType:
+            return "uint";
+        case kShort_GrSLType:
+            return "short";
+        case kShort2_GrSLType:
+            return "short2";
+        case kShort3_GrSLType:
+            return "short3";
+        case kShort4_GrSLType:
+            return "short4";
+        case kUShort_GrSLType:
+            return "ushort";
+        case kUShort2_GrSLType:
+            return "ushort2";
+        case kUShort3_GrSLType:
+            return "ushort3";
+        case kUShort4_GrSLType:
+            return "ushort4";
+        case kTexture2D_GrSLType:
+            return "texture2D";
+        case kSampler_GrSLType:
+            return "sampler";
+    }
+    SK_ABORT("Unknown shader var type.");
+    return "";  // suppress warning
+}
+
 /** Is the shading language type float (including vectors/matrices)? */
 static inline bool GrSLTypeIsFloatType(GrSLType type) {
     switch (type) {
