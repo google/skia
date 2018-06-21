03a94d9562b9e038b1de939c45a81d5a254b9a89
Herb Derby <herb@google.com>
2018-06-21 20:03:04 +0000

    Revert "Add SkGlyphRunList"
    This reverts commit f2e4a039df33d9246bd0ec68602d4d06e6199553.
    
    Reason for revert: Breaks asan tests
    
    Original change's description:
    > Add SkGlyphRunList
    > 
    > Extend the glyph run system with a glyph run list. This
    > allows the processing of text blobs.
    > 
    > Add original text an cluster to runs for PDF.
    > 
    > Change-Id: If4867d000e45f8975a30e982fc8fdbe104ef4332
    > Reviewed-on: https://skia-review.googlesource.com/135627
    > Reviewed-by: Ben Wagner <bungeman@google.com>
    > Commit-Queue: Herb Derby <herb@google.com>
    
    TBR=bungeman@google.com,herb@google.com
    
    Change-Id: I903592714da901383efc7e5f47ce3dfd529e2aca
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- tests/GlyphRunTest.cpp
+++ tests/GlyphRunTest.cpp
@@ -7,11 +7,9 @@
 
 #include "SkGlyphRun.h"
 
-#include "SkTextBlob.h"
-
 #include "Test.h"
 
-DEF_TEST(GlyphRunBasic, reporter) {
+DEF_TEST(GlyphRunInfo, reporter) {
     SkGlyphID glyphs[] = {100, 3, 240, 3, 234, 111, 3, 4, 10, 11};
     uint16_t count = SK_ARRAY_COUNT(glyphs);
 
@@ -20,47 +18,5 @@ DEF_TEST(GlyphRunBasic, reporter) {
 
     SkGlyphRunBuilder builder;
     builder.prepareDrawText(paint, glyphs, count, SkPoint::Make(0, 0));
-}
 
-DEF_TEST(GlyphRunBlob, reporter) {
-    uint16_t count = 10;
-
-    auto tf = SkTypeface::MakeFromName("monospace", SkFontStyle());
-
-    SkPaint font;
-    font.setTypeface(tf);
-    font.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
-    font.setTextAlign(SkPaint::kLeft_Align);
-    font.setStyle(SkPaint::kFill_Style);
-    font.setHinting(SkPaint::kNormal_Hinting);
-    font.setTextSize(1u);
-
-    SkTextBlobBuilder blobBuilder;
-    SkRect bounds = SkRect::MakeWH(10, 10);
-    for (int runNum = 0; runNum < 2; runNum++) {
-        const auto& runBuffer = blobBuilder.allocRunPosH(font, count, runNum, &bounds);
-        SkASSERT(runBuffer.utf8text == nullptr);
-        SkASSERT(runBuffer.clusters == nullptr);
-
-        for (int i = 0; i < count; i++) {
-            runBuffer.glyphs[i] = static_cast<SkGlyphID>(i + runNum * 10);
-            runBuffer.pos[i] = SkIntToScalar(i + runNum * 10);
-        }
-    }
-
-    auto blob = blobBuilder.make();
-
-    SkPaint paint;
-    paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
-
-    SkGlyphRunBuilder runBuilder;
-    runBuilder.prepareTextBlob(font, *blob, SkPoint::Make(0, 0));
-
-    auto runList = runBuilder.useGlyphRunList();
-
-    REPORTER_ASSERT(reporter, runList->size() == 2);
-    for (auto& run : *runList) {
-        REPORTER_ASSERT(reporter, run.runSize() == 10);
-        REPORTER_ASSERT(reporter, run.uniqueSize() == 10);
-    }
 }
