2125be76e3a2bee09c02f4e34434d89adc431da2
Ben Wagner <bungeman@google.com>
2018-06-22 10:03:08 -0400

    Remove include/ports/SkFontMgr.h
    The SkFontMgr.h header has been moved to core/SkFontMgr.h and the
    ports/SkFontMgr.h header is simply a forwarding header. Remove the
    forwarding header now that users have been updated.
    
    Change-Id: Icecef2ecb3ac20fc2c885429b06cdb306524303d

--- include/ports/SkFontMgr.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2018 Google LLC.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-#ifndef ports_SkFontMgr_DEFINED
-#define ports_SkFontMgr_DEFINED
-#include "../core/SkFontMgr.h"
-#endif
