f608a6438885d8e57ecca7d412ccb07ec3a6421b
Ben Wagner <benjaminwagner@google.com>
2018-06-22 15:45:01 -0400

    Add OpenCL test job that runs hello-opencl.
    Bug: skia:8081
    Change-Id: Id1cd7ea0e55187fedb856f715e837ea178cb4ea7

--- infra/bots/assets/opencl_linux/create.py
+++ infra/bots/assets/opencl_linux/create.py
@@ -16,10 +16,12 @@ import shutil
 import subprocess
 import utils
 
-# Use libOpenCL.so from the ocl-icd-opencl-dev Debian package.
 PKGS = [
+    # Use libOpenCL.so from the ocl-icd-opencl-dev and ocl-icd-libopencl1 Debian packages.
     'ocl-icd-opencl-dev',
     'ocl-icd-libopencl1',
+    # Include the Beignet driver for Intel.
+    'beignet-opencl-icd',
 ]
 
 def create_asset(target_dir):
@@ -30,10 +32,24 @@ def create_asset(target_dir):
     # Extract to CWD.
     for f in os.listdir('.'):
       subprocess.check_call(['dpkg-deb', '--extract', f, '.'])
-    # Copy usr/lib/x86_64-linux-gnu/* to target_dir.
+    # Move usr/lib/x86_64-linux-gnu/* to target_dir.
     lib_dir = os.path.join(os.getcwd(), 'usr', 'lib', 'x86_64-linux-gnu')
     for f in os.listdir(lib_dir):
       shutil.move(os.path.join(lib_dir, f), target_dir)
+    # Validate assumptions below.
+    with open(os.path.join(os.getcwd(), 'etc', 'OpenCL', 'vendors',
+                           'intel-beignet-x86_64-linux-gnu.icd')) as icd:
+      expected = '/usr/lib/x86_64-linux-gnu/beignet/libcl.so'
+      actual = os.path.normpath(icd.readlines()[0].strip())
+      if expected != actual:
+        raise Exception('Expected beignet ICD to point to %s, but it now points'
+                        'to %s; please update create.py.' % (expected, actual))
+
+  # Generate vendors dir.
+  os.mkdir(os.path.join(target_dir, 'vendors'))
+  # Currently only Beignet.
+  with open(os.path.join(target_dir, 'vendors', 'beignet.icd'), 'w') as icd:
+    icd.write('../beignet/libcl.so\n')
 
 
 def main():
