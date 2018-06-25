8700cab8c436af8884ec1612c7f7c03b83a98798
Kevin Lubick <kjlubick@google.com>
2018-06-25 16:55:43 -0400

    Adventures with WASM
    So far, can compile with compile.sh, but not really with
    GN/ninja (the compilation into many object files and a link
    at the end seems to mess emscripten up)
    
    Bug: skia:
    Change-Id: If6b300e2b102469e17841265c7866f1a81094d70

---
+++ experimental/wasm/compile.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+EMSDK="/usr/local/google/home/kjlubick/wasm/"
+
+EM_VERSION="1.38.6"
+
+source ${EMSDK}/emsdk_env.sh
+
+echo "Compiling"
+
+${EMSDK}/emscripten/${EM_VERSION}/em++ -O3 -std=c++14 \
+-Iinclude/config \
+-Iinclude/core \
+-Iinclude/private \
+-Iinclude/pathops \
+-Iinclude/utils \
+-s WASM=1 \
+-s NO_EXIT_RUNTIME=1 \
+-s ERROR_ON_UNDEFINED_SYMBOLS=1 \
+-s ERROR_ON_MISSING_LIBRARIES=1 \
+-o out/wasm/hello_wasm.html \
+experimental/wasm/wasm_main.cpp \
+src/core/SkPath.cpp \
+src/core/SkPathRef.cpp \
+src/core/SkPoint.cpp \
+src/core/SkPathMeasure.cpp \
+src/core/SkRect.cpp \
+src/core/SkGeometry.cpp \
+src/ports/SkMemory_malloc.cpp \
+src/ports/SkDebug_stdio.cpp
+
+# Add the following for debugging (bloats production code size otherwise)
+# list of all (most?) settings: https://github.com/kripken/emscripten/blob/incoming/src/settings.js
+#-s ASSERTIONS=1 \
+#-s DEMANGLE_SUPPORT=1 \
+
+#python -m SimpleHTTPServer 8000
