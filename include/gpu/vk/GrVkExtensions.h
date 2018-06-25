8ad3ba7fa211c4987d79ea16e88654b53b4bed12
Greg Daniel <egdaniel@google.com>
2018-06-25 11:42:30 -0400

    Let client pass in full extension to GrVkBackendContext.
    Bug: skia:
    Change-Id: I772ab4ccbca0f4f7e7d429d6c421b07d97f0606f

---
+++ include/gpu/vk/GrVkExtensions.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrVkExtensions_DEFINED
+#define GrVkExtensions_DEFINED
+
+#include "../private/SkTArray.h"
+#include "SkString.h"
+
+/**
+ * Helper class that eats in an array of extensions strings for instance and device and allows for
+ * quicker querying if an extension is present.
+ */
+class GrVkExtensions {
+public:
+#ifdef SK_SUPPORT_LEGACY_VULKAN_INTERFACE
+    // TODO: Remove once we remove the old fExtensions from GrVkBackendContext
+    GrVkExtensions(uint32_t extensionFlags);
+
+    // TODO: Remove once we remove the old fExtensions from GrVkBackendContext
+    static void GetExtensionArrayFromFlags(uint32_t extensionFlags,
+                                           SkTArray<const char*>* extensions);
+#else
+    GrVkExtensions(uint32_t instanceExtensionCount, const char* const* instanceExtensions,
+                   uint32_t deviceExtensionCount, const char* const* deviceExtensions);
+#endif
+
+    bool hasExtension(const char[]) const;
+
+private:
+    SkTArray<SkString>  fExtensionStrings;
+};
+
+#endif
