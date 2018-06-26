873b701c01c05dde68a837fa8cf278a4bcba2f2b
Ben Wagner <benjaminwagner@google.com>
2018-06-26 14:35:08 -0400

    Add OpenCL test job that runs hello-opencl.
    Bug: skia:8081
    Change-Id: Id1cd7ea0e55187fedb856f715e837ea178cb4ea7

--- infra/bots/assets/opencl_linux/create.py
+++ infra/bots/assets/opencl_linux/create.py
@@ -11,13 +11,15 @@
 
 import argparse
 import common
+import multiprocessing
 import os
 import shutil
 import subprocess
 import utils
 
-# Use libOpenCL.so from the ocl-icd-opencl-dev Debian package.
 PKGS = [
+    # Use libOpenCL.so from the ocl-icd-opencl-dev and ocl-icd-libopencl1 Debian
+    # packages.
     'ocl-icd-opencl-dev',
     'ocl-icd-libopencl1',
 ]
@@ -30,10 +32,53 @@ def create_asset(target_dir):
     # Extract to CWD.
     for f in os.listdir('.'):
       subprocess.check_call(['dpkg-deb', '--extract', f, '.'])
-    # Copy usr/lib/x86_64-linux-gnu/* to target_dir.
+    # Move usr/lib/x86_64-linux-gnu/* to target_dir.
     lib_dir = os.path.join(os.getcwd(), 'usr', 'lib', 'x86_64-linux-gnu')
     for f in os.listdir(lib_dir):
       shutil.move(os.path.join(lib_dir, f), target_dir)
+
+  # Check out and build Beignet. Following instructions here:
+  # https://www.freedesktop.org/wiki/Software/Beignet/
+  with utils.tmp_dir():
+    # Install build deps. Note use of clang-3.9, libclang-3.9-dev, and llvm-3.9
+    # so that the Beignet lib works on Debian 9.
+    beignet_build_deps = ['clang-3.9', 'cmake', 'libclang-3.9-dev',
+                          'libdrm-dev', 'libedit-dev', 'libegl1-mesa-dev',
+                          'libtinfo-dev', 'libxext-dev', 'libxfixes-dev',
+                          'llvm-3.9', 'ocl-icd-dev', 'ocl-icd-opencl-dev',
+                          'pkg-config', 'python', 'zlib1g-dev']
+    apt_get_cmd = ['sudo', 'apt-get', 'install'] + beignet_build_deps
+    print 'Running "%s"' % ' '.join(apt_get_cmd)
+    subprocess.check_call(apt_get_cmd)
+    # Check out repo.
+    subprocess.check_call(['git', 'clone', '--depth', '1',
+                           'https://anongit.freedesktop.org/git/beignet.git'])
+    # Configure the build.
+    build_dir = os.path.join(os.getcwd(), 'beignet', 'build')
+    os.mkdir(build_dir)
+    os.chdir(build_dir)
+    # We override the default BEIGNET_INSTALL_DIR because we don't control where
+    # this asset is placed on the filesystem. Using a relative path here causes
+    # Beignet to load its sub-libraries from a path relative to LD_LIBRARY_PATH.
+    subprocess.check_call(['cmake', '-DBEIGNET_INSTALL_DIR=beignet/', '../'])
+    # Build and package the library.
+    subprocess.check_call(['make', '-j%d' % multiprocessing.cpu_count(),
+                           'package'])
+    # Extract library.
+    tarfile = 'Beignet-1.4.-Linux.tar.gz'
+    match = 'Beignet-1.4.-Linux/usr/local/beignet'
+    strip_components = len(match.split('/'))-1  # Keep beignet.
+    subprocess.check_call(['tar', 'xzf', tarfile,
+                           '--directory', target_dir,
+                           '--strip-components=%d' % strip_components,
+                           '--anchored', match])
+
+  # Generate vendors dir.
+  os.mkdir(os.path.join(target_dir, 'vendors'))
+  # Currently only Beignet.
+  with open(os.path.join(target_dir, 'vendors', 'beignet.icd'), 'w') as icd:
+    # Use relative path, which will be resolved by LD_LIBRARY_PATH.
+    icd.write('beignet/libcl.so\n')
 
 
 def main():
