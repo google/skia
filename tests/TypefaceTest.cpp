410d42fce2f5a34a80622356911dbf2dd6f5378a
Ben Wagner <bungeman@google.com>
2018-06-26 11:22:37 -0400

    Add SkTypeface::getVariationDesignParameters
    This adds a way for users to query the axis parameters for a typeface.
    
    Change-Id: Idc2ac0d84bc7ae2ca484ae410cba5b01883418e5

--- tests/TypefaceTest.cpp
+++ tests/TypefaceTest.cpp
@@ -264,6 +264,11 @@ protected:
     {
         return 0;
     }
+    int onGetVariationDesignParameters(SkFontParameters::Variation::Axis parameters[],
+                                       int parameterCount) const override
+    {
+        return 0;
+    }
     int onGetTableTags(SkFontTableTag tags[]) const override { return 0; }
     size_t onGetTableData(SkFontTableTag, size_t, size_t, void*) const override { return 0; }
 };
