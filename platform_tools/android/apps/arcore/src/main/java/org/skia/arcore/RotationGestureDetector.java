fcda3c62c50aec91a7ce9d91ab12114593165aa2
ziadb <ziadb@google.com>
2018-06-27 14:05:43 -0400

    Refactored & added comments to main .cc app
    Bug: skia:
    Change-Id: Ic0c845fc29e5de33a0d7c8304a30fa3074944786

---
+++ platform_tools/android/apps/arcore/src/main/java/org/skia/arcore/RotationGestureDetector.java
@@ -0,0 +1,83 @@
+package org.skia.arcore;
+
+import android.view.MotionEvent;
+
+public class RotationGestureDetector {
+    private static final int INVALID_POINTER_ID = -1;
+    private float fX, fY, sX, sY;
+    private int ptrID1, ptrID2;
+    private float mAngle;
+
+    private OnRotationGestureListener mListener;
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public RotationGestureDetector(OnRotationGestureListener listener){
+        mListener = listener;
+        ptrID1 = INVALID_POINTER_ID;
+        ptrID2 = INVALID_POINTER_ID;
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                ptrID1 = event.getPointerId(event.getActionIndex());
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                ptrID2 = event.getPointerId(event.getActionIndex());
+                sX = event.getX(event.findPointerIndex(ptrID1));
+                sY = event.getY(event.findPointerIndex(ptrID1));
+                fX = event.getX(event.findPointerIndex(ptrID2));
+                fY = event.getY(event.findPointerIndex(ptrID2));
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(ptrID1 != INVALID_POINTER_ID && ptrID2 != INVALID_POINTER_ID){
+                    float nfX, nfY, nsX, nsY;
+                    nsX = event.getX(event.findPointerIndex(ptrID1));
+                    nsY = event.getY(event.findPointerIndex(ptrID1));
+                    nfX = event.getX(event.findPointerIndex(ptrID2));
+                    nfY = event.getY(event.findPointerIndex(ptrID2));
+
+                    mAngle = angleBetweenLines(fX, fY, sX, sY, nfX, nfY, nsX, nsY);
+
+                    sX = nsX;
+                    sY = nsY;
+                    fX = nfX;
+                    fY = nfY;
+
+                    if (mListener != null) {
+                        mListener.OnRotation(this);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                ptrID1 = INVALID_POINTER_ID;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                ptrID2 = INVALID_POINTER_ID;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                ptrID1 = INVALID_POINTER_ID;
+                ptrID2 = INVALID_POINTER_ID;
+                break;
+        }
+        return true;
+    }
+
+    private float angleBetweenLines (float fX, float fY, float sX, float sY, float nfX, float nfY, float nsX, float nsY)
+    {
+        float angle1 = (float) Math.atan2( (fY - sY), (fX - sX) );
+        float angle2 = (float) Math.atan2( (nfY - nsY), (nfX - nsX) );
+
+        float angle = ((float)Math.toDegrees(angle1 - angle2)) % 360;
+        if (angle < -180.f) angle += 360.0f;
+        if (angle > 180.f) angle -= 360.0f;
+        return angle;
+    }
+
+    public static interface OnRotationGestureListener {
+        public void OnRotation(RotationGestureDetector rotationDetector);
+    }
+}
