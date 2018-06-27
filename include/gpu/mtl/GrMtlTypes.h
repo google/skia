07c59c8588e1976f24b29ab960e1e1bb60cb2177
Timothy Liang <timliang@google.com>
2018-06-27 13:25:57 -0400

    Bug: skia:
    Change-Id: I58c7307f41df4694b0b46014bedd681adefed265

--- include/gpu/mtl/GrMtlTypes.h
+++ include/gpu/mtl/GrMtlTypes.h
@@ -9,8 +9,38 @@
 #define GrMtlTypes_DEFINED
 
 #include "GrTypes.h"
+#include "../private/GrMtlTrampoline.h"
 
-// This is a placeholder class until we fill it out. This is needed so we can have the mtl include
-// path in our BUILD.gn
+/**
+ * Declares typedefs for Metal types used in Ganesh cpp code
+ */
+typedef unsigned int GrMTLPixelFormat;
+
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Types for interacting with Metal resources created externally to Skia. Holds the MTLTexture as a
+ * const void* and will extract the relevant info so it can be easily accessed by Ganesh cpp code.
+ * This is used by GrBackendObjects.
+ */
+struct GrMtlTextureInfo {
+public:
+    int fWidth = 0;
+    int fHeight = 0;
+    int fSampleCnt = 1;
+    int fLevelCount = 1;
+    GrMTLPixelFormat fFormat = 0; // MTLPixelFormatInvalid
+    const void* fTexture = nullptr;
+
+    GrMtlTextureInfo(const void* mtlTexture = nullptr) {
+        if (mtlTexture) {
+            GrMtlTrampoline::ExtractMTLTextureInfo(mtlTexture, this);
+        }
+    }
+
+    bool operator==(const GrMtlTextureInfo& that) const {
+        return fWidth == that.fWidth && fHeight == that.fHeight && fSampleCnt == that.fSampleCnt &&
+        fLevelCount == that.fLevelCount && fFormat == that.fFormat && fTexture == that.fTexture;
+    }
+};
 
 #endif
