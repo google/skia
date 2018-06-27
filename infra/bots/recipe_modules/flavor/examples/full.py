e2c3a642db73425a20adc26d2623fe634be7e46e
Mike Klein <mtklein@chromium.org>
2018-06-27 11:14:10 -0400

    rename Chromecast bots to Clang
    We updated them to use Clang a while ago but forgot to rename the bots.
    
    Change-Id: I01c353b48d5aff7a30d63d382bdf6bdfb3e0b196

--- infra/bots/recipe_modules/flavor/examples/full.py
+++ infra/bots/recipe_modules/flavor/examples/full.py
@@ -58,7 +58,7 @@ TEST_BUILDERS = [
   'Perf-Android-Clang-GalaxyS7_G930FD-GPU-MaliT880-arm64-Debug-All-Android',
   'Perf-Android-Clang-Nexus5x-GPU-Adreno418-arm64-Debug-All-Android',
   'Perf-ChromeOS-Clang-SamsungChromebookPlus-GPU-MaliT860-arm-Release-All',
-  'Perf-Chromecast-GCC-Chorizo-CPU-Cortex_A7-arm-Release-All',
+  'Perf-Chromecast-Clang-Chorizo-CPU-Cortex_A7-arm-Release-All',
   'Perf-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-MSAN',
   'Perf-Debian9-Clang-GCE-CPU-AVX2-x86_64-Release-All-ASAN',
   'Test-Android-Clang-AndroidOne-GPU-Mali400MP2-arm-Release-All-Android',
