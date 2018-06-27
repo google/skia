d2b7c4d35db1851ab9fd5ef97425d344abf1ad93
ziadb <ziadb@google.com>
2018-06-27 14:27:26 -0400

    SkAR: Refactoring touch & draw code, full support of translating/scaling/rotating old anchors, drawing in Ganesh only, light estimation calculation
    Bug: skia:
    Change-Id: I1d042655f1a367a9ecd81513fc2f4f05e883ccce

--- platform_tools/android/apps/arcore/src/main/cpp/anchor_wrapper.cc
+++ platform_tools/android/apps/arcore/src/main/cpp/anchor_wrapper.cc
@@ -17,23 +17,53 @@
 #include "hello_ar_application.h"
 #include "arcore_c_api.h"
 #include "anchor_wrapper.h"
+#include <memory>
 
 namespace hello_ar {
 
-    AnchorWrapper::AnchorWrapper(ArAnchor *anchor) : anchor(anchor) {}
+    AnchorWrapper::AnchorWrapper() :
+            anchor(nullptr), matrixInfo(nullptr) {}
 
-    const ArAnchor* AnchorWrapper::GetArAnchor() {
+    AnchorWrapper::AnchorWrapper(ArAnchor* anchor) :
+            anchor(anchor), matrixInfo(nullptr) {}
+
+    AnchorWrapper::~AnchorWrapper() {
+        //Matrix info will be deleted
+    }
+
+    ArAnchor* AnchorWrapper::GetArAnchor() const {
         return anchor;
     }
+
+    glm::vec4 AnchorWrapper::GetAnchorPos(ArSession* arSession) {
+        float poseRaw[] = {0, 0, 0, 0, 0, 0, 0};
+        ArPose* anchorPose = nullptr;
+        ArPose_create(arSession, poseRaw, &anchorPose);
+        ArAnchor_getPose(arSession, GetArAnchor(), anchorPose);
+        ArPose_getPoseRaw(arSession, anchorPose, poseRaw);
+        ArPose_destroy(anchorPose);
+        glm::vec4 anchorPos = glm::vec4(poseRaw[4], poseRaw[5], poseRaw[6], 1);
+        return anchorPos;
+    }
+
+    util::MatrixComputationInfo* AnchorWrapper::GetMatrixInfo() {
+        return matrixInfo.get();
+    }
+
     DrawableType AnchorWrapper::GetDrawableType() {
         return drawableType;
     }
 
-    void AnchorWrapper::SetArAnchor(ArAnchor* anchor) {
-        this->anchor = anchor;
+    void AnchorWrapper::SetArAnchor(ArAnchor* a) {
+        anchor = a;
     }
-    void AnchorWrapper::SetDrawableType(DrawableType drawableType) {
-        this->drawableType = drawableType;
+
+    void AnchorWrapper::SetMatrixInfo(std::unique_ptr<util::MatrixComputationInfo> info) {
+        matrixInfo = std::move(info);
+    }
+
+    void AnchorWrapper::SetDrawableType(DrawableType d) {
+        drawableType = d;
     }
 
 
