86ceceeecbe8ee93c7c92a0926fdc6e5e870ff60
Jim Van Verth <jvanverth@google.com>
2018-06-28 12:46:23 -0400

    Adjust ellipse center epsilon further to fix dropouts
    Bug: skia:
    Change-Id: I93d754cdb7ad7f552d96fcbdf4137f2c699e3a55

--- src/gpu/ops/GrOvalOpFactory.cpp
+++ src/gpu/ops/GrOvalOpFactory.cpp
@@ -598,7 +598,7 @@ private:
             if (args.fShaderCaps->halfIs32Bits()) {
                 fragBuilder->codeAppend("grad_dot = max(grad_dot, 1.0e-6);");
             } else {
-                fragBuilder->codeAppend("grad_dot = max(grad_dot, 3.1e-5);");
+                fragBuilder->codeAppend("grad_dot = max(grad_dot, 5.0e-5);");
             }
             fragBuilder->codeAppend("half invlen = inversesqrt(grad_dot);");
             fragBuilder->codeAppend("half edgeAlpha = clamp(0.5-test*invlen, 0.0, 1.0);");
@@ -754,7 +754,7 @@ private:
             if (args.fShaderCaps->halfIs32Bits()) {
                 fragBuilder->codeAppend("grad_dot = max(grad_dot, 1.0e-6);");
             } else {
-                fragBuilder->codeAppend("grad_dot = max(grad_dot, 3.1e-5);");
+                fragBuilder->codeAppend("grad_dot = max(grad_dot, 5.0e-5);");
             }
             fragBuilder->codeAppend("half invlen = inversesqrt(grad_dot);");
             if (DIEllipseStyle::kHairline == diegp.fStyle) {
