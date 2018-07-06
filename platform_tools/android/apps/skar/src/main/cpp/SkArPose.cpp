732768b171f33e172ebd6b4272e71e2f32a06806
ziadb <ziadb@google.com>
2018-07-06 13:16:49 -0400

    SkAR: Refactoring touch & draw code, full support of translating/scaling/rotating old anchors, drawing in Ganesh only, light estimation calculation
    Bug: skia:
    Change-Id: I1d042655f1a367a9ecd81513fc2f4f05e883ccce

---
+++ platform_tools/android/apps/skar/src/main/cpp/SkArPose.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkArPose.h"
+
+std::unique_ptr<SkArPose> SkArPose::Make(sk_sp<SkArSession> session,
+                                         const float poseRaw[7]) {
+    ArPose* pose = nullptr;
+    ArPose_create(session.get()->getArSession(), poseRaw, &pose);
+    return std::unique_ptr<SkArPose>(new SkArPose(pose));
+}
+
+SkArPose::~SkArPose() {
+    ArPose_destroy(fArPose);
+}
+
+ArPose* SkArPose::getArPose() {
+    return fArPose;
+}
+
+void SkArPose::getPoseRaw(sk_sp<SkArSession> session, float outPoseRaw[7]) const {
+    ArPose_getPoseRaw(session->getArSession(), fArPose, outPoseRaw);
+}
