d174bc841785ae2ac8e1cc14948d3dce8a3aab44
Leon Scroggins III <scroggo@google.com>
2018-07-12 15:13:07 -0400

    Switch SkCodec to use skcms, step 1
    Bug: skia:6839
    Bug: skia:8052
    
    Create an skcms_Profile instead of an SkColorSpace when creating an
    SkCodec. Eventually we'll move the SkImageInfo (and its SkColorSpace)
    entirely to clients (e.g. SkAndroidCodec, SkCodecImageGenerator), but
    for now, create it with SkEncodedInfo::makeImageInfo.
    
    Create new SkEncodedInfo::Colors for the special PNG cases that we
    want to map to specific SkColorTypes.
    
    SkEncodedInfo:
    - Add ICCProfile, which owns an skcms_ICCProfile
     - FIXME: maybe we should have a single instance for
      SRGB like SkColorSpace?
    - Add kXAlpha_Color, for kAlpha_8. Since I'm not longer creating
      an SkImageInfo (at least in SkPngCodec), it needs a way to pass
      this info to the caller.
    - Add k565_Color, for the same reason. Matt originally had this in
      https://codereview.chromium.org/2212563003/#ps120001, but didn't
      land that version. I like it though. Mike didn't like the bits
      per component for 565, but it seems like a sensible hack, much
      like the existing one for kAlpha_8
    - Add width and height. These were removed for redundancy with
      SkImageInfo, but it makes sense to have them here without it.
    BUILD.gn:
    - Build the new SkEncodedInfo.cpp
      FIXME: This will cause problems with Chromium roll :(
      It wasn't too hard to fix with SkColorTable.cpp, but it was
      tedious.
    SkCodec:
    - Remove the constructor with an SkImageInfo. Edit the other one
      to drop width and height (now in SkEncodedInfo) and take a RHS
      reference to SkEncodedInfo
    - Create the SkImageInfo from fEncodedInfo (for now)
    SkBmpCodec (etc)
    - Adapt to the changes above
    - Create a new SkEncodedInfo w/o profile for the swizzler.
    SkHeifCodec:
    - TODO: Can we share the ICC data, rather than copy?
    SkPngCodec:
    - use the new SkEncodedInfo::Colors rather than a custom SkImageInfo
    SkRawCodec:
    - Remove SkEncodedInfo from SkDngImage, which doesn't actually need it.
      This is helpful since we don't know all the info yet.
    - Rewrite gAdobeRGB_toXYZD50 as an skcms_Matrix3x3
    SkSwizzler:
    - Add cases for the new SkEncodedInfo::Colors
    
    TODO: Also use skcms's version of SkColorSpaceXform?
    
    Change-Id: Ic0d3bb752b03f13be886b80331987aa5a5713fc0

--- src/codec/SkSwizzler.cpp
+++ src/codec/SkSwizzler.cpp
@@ -890,6 +890,7 @@ SkSwizzler* SkSwizzler::CreateSwizzler(const SkEncodedInfo& encodedInfo,
                         return nullptr;
                 }
                 break;
+            case SkEncodedInfo::kXAlpha_Color:
             case SkEncodedInfo::kGrayAlpha_Color:
                 switch (dstInfo.colorType()) {
                     case kRGBA_8888_SkColorType:
@@ -963,6 +964,10 @@ SkSwizzler* SkSwizzler::CreateSwizzler(const SkEncodedInfo& encodedInfo,
                         return nullptr;
                 }
                 break;
+            case SkEncodedInfo::k565_Color:
+                // Treat 565 exactly like RGB (since it's still encoded as 8 bits per component).
+                // We just mark as 565 when we have a hint that there are only 5/6/5 "significant"
+                // bits in each channel.
             case SkEncodedInfo::kRGB_Color:
                 switch (dstInfo.colorType()) {
                     case kRGBA_8888_SkColorType:
