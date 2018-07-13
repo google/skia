a07bf6788e926c41d52ae4bed592ec8a187ee4ab
Robert Phillips <robertphillips@google.com>
2018-07-13 10:18:38 -0400

    Add bullet proofing to image_subset GM for TSAN failures
    The most common TSAN failure is:
    
    	glmsaa8 gm  image_subset
    
    with this stack trace:
    
    5.73m	    /b/s/w/ir/build/dm(_ZNK7SkImage10makeSubsetERK7SkIRect+0x96) [0x1a7c356]
        /b/s/w/ir/build/dm(_ZNK7SkImage10makeSubsetERK7SkIRect+0x96) [0x1a7c356]
    5.73m	    /b/s/w/ir/build/dm() [0x12cf587]
        /b/s/w/ir/build/dm() [0x12cf587]
    5.73m	    /b/s/w/ir/build/dm(_ZN6skiagm8SimpleGM6onDrawEP8SkCanvas+0x25) [0x12ba205]
    
    Change-Id: Ife1c27af935ef3f56073360e9781a89fe8206586
    Reviewed-on: https://skia-review.googlesource.com/141280
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -127,7 +127,8 @@ sk_sp<SkImage> SkImage::MakeFromEncoded(sk_sp<SkData> encoded, const SkIRect* su
     if (nullptr == encoded || 0 == encoded->size()) {
         return nullptr;
     }
-    return SkImage::MakeFromGenerator(SkImageGenerator::MakeFromEncoded(encoded), subset);
+    return SkImage::MakeFromGenerator(SkImageGenerator::MakeFromEncoded(std::move(encoded)),
+                                      subset);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
