103dcca605303f1f1be5757eb411eac240837a11
Herb Derby <herb@google.com>
2018-07-16 14:52:26 -0400

    Remove drawText support from SkOverdrawCanvas
    This canvas is used by android, and never uses the drawText
    method. I think this is the last call to processText which
    will be deleted shortly.
    
    Change-Id: I134abff47b0b7fda02297e4b4496e2f0413a3957
    Reviewed-on: https://skia-review.googlesource.com/141546
    Reviewed-by: Derek Sollenberger <djsollen@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- src/core/SkOverdrawCanvas.cpp
+++ src/core/SkOverdrawCanvas.cpp
@@ -63,14 +63,7 @@ SkOverdrawCanvas::SkOverdrawCanvas(SkCanvas* canvas)
 
 void SkOverdrawCanvas::onDrawText(const void* text, size_t byteLength, SkScalar x, SkScalar y,
                                   const SkPaint& paint) {
-    ProcessOneGlyphBounds processBounds(this);
-    SkSurfaceProps props(0, kUnknown_SkPixelGeometry);
-    this->getProps(&props);
-    auto cache = SkStrikeCache::FindOrCreateStrikeExclusive(
-            paint, &props, SkScalerContextFlags::kNone, &this->getTotalMatrix());
-    SkFindAndPlaceGlyph::ProcessText(paint.getTextEncoding(), (const char*) text, byteLength,
-                                     SkPoint::Make(x, y), SkMatrix(), paint.getTextAlign(),
-                                     cache.get(), processBounds);
+    SK_ABORT("This canvas does not support draw text.");
 }
 
 void SkOverdrawCanvas::drawPosTextCommon(const void* text, size_t byteLength, const SkScalar pos[],
