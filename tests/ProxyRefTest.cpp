2e06e4221d75cbf3c5a6a1505a549536514be52a
Robert Phillips <robertphillips@google.com>
2018-07-18 11:20:14 -0400

    Fix explicit allocation bug
    Change-Id: I9866f563e02b2ab290cc46ede05f8eda21f6d3b2

--- tests/ProxyRefTest.cpp
+++ tests/ProxyRefTest.cpp
@@ -18,16 +18,12 @@
 #include "GrTexture.h"
 #include "GrTextureProxy.h"
 
-int32_t GrIORefProxy::getProxyRefCnt_TestOnly() const {
-    return fRefCnt;
-}
-
 int32_t GrIORefProxy::getBackingRefCnt_TestOnly() const {
     if (fTarget) {
         return fTarget->fRefCnt;
     }
 
-    return fRefCnt;
+    return -1; // no backing GrSurface
 }
 
 int32_t GrIORefProxy::getPendingReadCnt_TestOnly() const {
@@ -54,12 +50,12 @@ static void check_refs(skiatest::Reporter* reporter,
                        int32_t expectedBackingRefs,
                        int32_t expectedNumReads,
                        int32_t expectedNumWrites) {
-    REPORTER_ASSERT(reporter, proxy->getProxyRefCnt_TestOnly() == expectedProxyRefs);
+    REPORTER_ASSERT(reporter, proxy->priv().getProxyRefCnt() == expectedProxyRefs);
     REPORTER_ASSERT(reporter, proxy->getBackingRefCnt_TestOnly() == expectedBackingRefs);
     REPORTER_ASSERT(reporter, proxy->getPendingReadCnt_TestOnly() == expectedNumReads);
     REPORTER_ASSERT(reporter, proxy->getPendingWriteCnt_TestOnly() == expectedNumWrites);
 
-    SkASSERT(proxy->getProxyRefCnt_TestOnly() == expectedProxyRefs);
+    SkASSERT(proxy->priv().getProxyRefCnt() == expectedProxyRefs);
     SkASSERT(proxy->getBackingRefCnt_TestOnly() == expectedBackingRefs);
     SkASSERT(proxy->getPendingReadCnt_TestOnly() == expectedNumReads);
     SkASSERT(proxy->getPendingWriteCnt_TestOnly() == expectedNumWrites);
