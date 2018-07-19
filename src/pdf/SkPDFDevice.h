736db1081b378ac8c167dfbc0322470d28c1cb3e
Herb Derby <herb@google.com>
2018-07-19 12:52:16 -0400

    Convert PDF device to use glyph runs lists
    Change-Id: I3d98aadead463a42dd8338c63593be22acb6dd68
    Reviewed-on: https://skia-review.googlesource.com/142502
    Reviewed-by: Hal Canary <halcanary@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- src/pdf/SkPDFDevice.h
+++ src/pdf/SkPDFDevice.h
@@ -22,6 +22,7 @@
 #include "SkTextBlob.h"
 #include "SkKeyedImage.h"
 
+class SkGlyphRunList;
 class SkKeyedImage;
 class SkPath;
 class SkPDFArray;
@@ -97,7 +98,6 @@ public:
     void drawPosText(const void* text, size_t len,
                      const SkScalar pos[], int scalarsPerPos,
                      const SkPoint& offset, const SkPaint&) override;
-    void drawTextBlob(const SkTextBlob*, SkScalar x, SkScalar y, const SkPaint &) override;
     void drawGlyphRunList(SkGlyphRunList* glyphRunList) override;
     void drawVertices(const SkVertices*, const SkMatrix* bones, int boneCount, SkBlendMode,
                       const SkPaint&) override;
