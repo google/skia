297c3c8e85bdb7ed79aaa6a596f52874d79e7a25
Ben Wagner <bungeman@google.com>
2018-07-19 08:55:10 -0400

    Ignore NTDDI_VERSION with DirectWrite.
    Some builds set NTDDI_VERSION to a very old version of Windows to ensure
    they run on those older versions of Windows. The dwrite_3.h header uses
    NTDDI_VERSION in an odd manner, hiding immutable declarations of enums,
    structs, and interfaces. NTDDI_VERSION was indended to hide functions
    and extensions to structs. DWrite has one function (to create the
    factory) and no structs which will be extended.
    
    Skia is already tested to run on the oldest supported Windows platform
    (Windows 7), so an external build building Skia with NTDDI_VERSION as a
    build define should have no issues at runtime if Skia ignores it. If
    Skia doesn't ignore NTDDI_VERSION in this case the DWrite backend will
    be runtime limited to interfaces in very old versions of Windows,
    instead of using newer interfaces which become available at runtime.
    
    Change-Id: I3e9ac2c4116d75588bfff391928d3cd446d6363a
    Reviewed-on: https://skia-review.googlesource.com/142324
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: Herb Derby <herb@google.com>

--- src/ports/SkRemotableFontMgr_win_dw.cpp
+++ src/ports/SkRemotableFontMgr_win_dw.cpp
@@ -4,6 +4,8 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#include "SkDWriteNTDDI_VERSION.h"
+
 #include "SkTypes.h"
 #if defined(SK_BUILD_FOR_WIN)
 
