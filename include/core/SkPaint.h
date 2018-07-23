f73c3fced7d516fe4e7282bcb29726a5d2b57890
Herb Derby <herb@google.com>
2018-07-23 16:20:53 -0400

    Centralize bitmap glyph positioning
    Change-Id: Id21567c1511eee9d2e9e20c7ae93544530cfdb81

--- include/core/SkPaint.h
+++ include/core/SkPaint.h
@@ -31,6 +31,7 @@ class SkData;
 class SkDescriptor;
 class SkDrawLooper;
 class SkGlyph;
+class SkGlyphDraw;
 struct SkRect;
 class SkGlyphCache;
 class SkImageFilter;
@@ -1535,6 +1536,7 @@ private:
     friend class SkCanonicalizePaint;
     friend class SkCanvas;
     friend class SkDraw;
+    friend class SkGlyphDraw;
     friend class SkPaintPriv;
     friend class SkPDFDevice;
     friend class SkScalerContext;  // for computeLuminanceColor()
