3ec7f1a74601558eb7399df25eff14727b5b07ba
Hal Canary <halcanary@google.com>
2018-07-25 16:52:48 -0400

    SkUTF
    Create new header and namespace, `SkUTF` where we are putting all of our
    robust, well documented UTF-8, UTF-16, and UTF-32 functions:
    `SkUTF::{Count,Next,To}UTF{8,16,32}()`.
    
    SkUTF.h and SkUTF.cpp do not depend on the rest of Skia and are suitable
    for re-use in other modules.
    
    Some of the old UTF-{8,16} functions still live in SkUtils.h; their use
    will be phased out in future CLs.
    
    Also added more unit testing and cleaned up old tests.
    
    Removed functions that were unused outside of tests or used only once.
    
    Change-Id: Iaa59b8705abccf9c4ba082f855da368a0bad8380

--- src/ports/SkRemotableFontMgr_win_dw.cpp
+++ src/ports/SkRemotableFontMgr_win_dw.cpp
@@ -398,7 +398,7 @@ public:
 
         WCHAR str[16];
         UINT32 strLen = static_cast<UINT32>(
-            SkUTF16_FromUnichar(character, reinterpret_cast<uint16_t*>(str)));
+            SkUTF::ToUTF16(character, reinterpret_cast<uint16_t*>(str)));
         SkTScopedComPtr<IDWriteTextLayout> fallbackLayout;
         HR_GENERAL(dwFactory->CreateTextLayout(str, strLen, fallbackFormat.get(),
                                                200.0f, 200.0f,
