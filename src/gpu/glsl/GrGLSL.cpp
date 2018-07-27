cf0df5b2384841ef188bb7051afbf527193405a5
Brian Salomon <bsalomon@google.com>
2018-07-27 14:55:21 -0400

    Remove unused kTexture2D_GrSLType and kSampler_GrSLType
    Also remove unused GrSLTypeIsCombinedSampler2DType
    
    Bug: skia:
    Change-Id: I9c670535dbbc30ddeb3636acb5b784580a968ee4

--- src/gpu/glsl/GrGLSL.cpp
+++ src/gpu/glsl/GrGLSL.cpp
@@ -99,10 +99,6 @@ const char* GrGLSLTypeString(const GrShaderCaps* shaderCaps, GrSLType t) {
             return "ubyte3";
         case kUByte4_GrSLType:
             return "ubyte4";
-        case kTexture2D_GrSLType:
-            return "texture2D";
-        case kSampler_GrSLType:
-            return "sampler";
     }
     SK_ABORT("Unknown shader var type.");
     return ""; // suppress warning
