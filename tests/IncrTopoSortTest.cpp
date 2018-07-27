16c4ef9a14dea48b3556270c66fd3222f4f424d9
Robert Phillips <robertphillips@google.com>
2018-07-27 15:12:07 -0400

    First version of incremental topological sort
    In order to solve both the VRAM budget for explicit resource allocation and DDL incremental flush problems we will need to always have the opLists be sorted.
    
    This will also help partial flushes.
    
    Change-Id: I3ac2baf622415925ab5c403b7800f3fc49e59838

---
+++ tests/IncrTopoSortTest.cpp
@@ -0,0 +1,310 @@
+
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkRefCnt.h"
+#include "SkTSort.h"
+#include "Test.h"
+
+#include "sk_tool_utils.h"
+
+
+
+class Graph {
+public:
+    class Node : public SkRefCnt {
+    public:
+        uint32_t id() const { return fID; }
+        int indexInSort() const { SkASSERT(fIndexInSort >= 0); return fIndexInSort; }
+        bool visited() const { return fVisited; }
+
+        void print() const {
+            SkDebugf("%d: id %d", fIndexInSort, fID);
+            if (fNodesIDependOn.count()) {
+                SkDebugf("I depend on (%d): ", fNodesIDependOn.count());
+                for (Node* tmp : fNodesIDependOn) {
+                    SkDebugf("%d, ", tmp->id());
+                }
+            }
+            if (fNodesThatDependOnMe.count()) {
+                SkDebugf("(%d) depend on me: ", fNodesThatDependOnMe.count());
+                for (Node* tmp : fNodesThatDependOnMe) {
+                    SkDebugf("%d, ", tmp->id());
+                }
+            }
+            SkDebugf("\n");
+        }
+
+        void validate(skiatest::Reporter* reporter) const {
+            for (Node* dependedOn : fNodesIDependOn) {
+                REPORTER_ASSERT(reporter, dependedOn->indexInSort() < this->indexInSort());
+            }
+            for (Node* dependent : fNodesThatDependOnMe) {
+                REPORTER_ASSERT(reporter, this->indexInSort() < dependent->indexInSort());
+            }
+            REPORTER_ASSERT(reporter, !fVisited); // this flag should only be true w/in addEdges
+        }
+
+        static bool CompareIndicesGT(Node* const& a, Node* const& b) {
+            return a->indexInSort() > b->indexInSort();
+        }
+
+    private:
+        friend class Graph;
+
+        Node(uint32_t id) : fID(id), fIndexInSort(-1), fVisited(false) {}
+
+        void setIndexInSort(int indexInSort) { fIndexInSort = indexInSort; }
+        void setVisited(bool visited) { fVisited = visited; }
+        void addDependency(Node* dependedOn) {
+            SkASSERT(dependedOn->indexInSort() < this->indexInSort());
+            fNodesIDependOn.push(dependedOn);
+
+            dependedOn->addDependent(this);
+        }
+        void addDependent(Node* dependent) {
+            fNodesThatDependOnMe.push(dependent);
+        }
+
+        uint32_t         fID;
+        SkTDArray<Node*> fNodesIDependOn;  // These nodes must appear before this one in the sort
+        SkTDArray<Node*> fNodesThatDependOnMe; // These ones must appear after this one
+        int              fIndexInSort;
+        bool             fVisited;
+    };
+
+    class Queue {
+    public:
+        Queue() : fFront(0) {}
+
+        int count() const {
+            return fFoo.count() - fFront;
+        }
+
+        bool isEmpty() const {
+            return 0 == this->count();
+        }
+
+        void push(Node* node) {
+            fFoo.push(node);
+        }
+
+        Node* top() {
+            return fFoo[fFront];
+        }
+
+        void pop() {
+            fFront++;
+        }
+
+    private:
+        int              fFront;
+        SkTDArray<Node*> fFoo;
+    };
+
+    Graph(int numNodes, skiatest::Reporter* reporter) : fNodes(numNodes), fReporter(reporter) {}
+
+    Node* addNode(uint32_t id) {
+        sk_sp<Node> tmp(new Node(id));
+
+        fNodes.push_back(tmp);       // The graph gets the creation ref
+        tmp->setIndexInSort(fNodes.count()-1);
+        this->validate();
+        return tmp.release();
+    }
+
+    // 'dependedOn' must appear before 'dependent' in the sort
+    bool addEdge(Node* dependedOn, Node* dependent) {
+        // TODO: this would be faster if all the SkTDArray code was stripped out of
+        // addEdges but, when used in MDB sorting this entry point will never be used.
+        SkTDArray<Node*> tmp(&dependedOn, 1);
+        return this->addEdges(tmp, dependent);
+    }
+
+    // All the nodes in 'dependedOn' must appear before 'dependent' in the sort.
+    // This is O(v + e + cost_of_sorting(b)) where:
+    //    v: number of nodes
+    //    e: number of edges
+    //    b: number of new edges in 'dependedOn'
+    bool addEdges(SkTDArray<Node*> dependedOn, Node* dependent) {
+        this->validate();
+
+        // remove any of the new dependencies that are already satisfied
+        for (int i = 0; i < dependedOn.count(); ++i) {
+            if (dependedOn[i]->indexInSort() < dependent->indexInSort()) {
+                dependent->addDependency(dependedOn[i]);
+                dependedOn.removeShuffle(i);
+                i--;
+            }
+        }
+
+        // sort the remaining dependencies in descending order
+        // TODO: QSort is waaay overkill here!
+        SkTQSort<Node*>(dependedOn.begin(), dependedOn.end() - 1, Node::CompareIndicesGT);
+
+#ifdef SK_DEBUG
+        int lastIndex = fNodes.count();
+        for (int i = 0; i < dependedOn.count(); ++i) {
+            SkASSERT(lastIndex > dependedOn[i]->indexInSort());
+            lastIndex = dependedOn[i]->indexInSort();
+        }
+#endif
+
+        // TODO: although this is the general algorithm, I think this can be simplified for our
+        // use case (i.e., the same dependent for all the new edges).
+        int lowerBound = fNodes.count();
+        for (int i = 0; i < dependedOn.count(); ++i) {
+            if (dependedOn[i]->indexInSort() < lowerBound) {
+                this->shift(lowerBound);
+            }
+
+            if (!dependent->visited()) {
+                this->dfs(dependent, dependedOn[i]->indexInSort());
+            }
+
+            lowerBound = SkTMin(dependent->indexInSort(), lowerBound);
+        }
+
+        this->shift(lowerBound);
+
+        this->validate();
+        return false;
+    }
+
+    void getActual(SkString* actual) const {
+        for (int i = 0; i < fNodes.count(); ++i) {
+            actual->appendU32(fNodes[i]->id());
+            if (i < fNodes.count()-1) {
+                actual->append(",");
+            }
+        }
+    }
+
+    void print() const {
+        SkDebugf("-------------------\n");
+        for (int i = 0; i < fNodes.count(); ++i) {
+            fNodes[i]->print();
+        }
+    }
+
+    void validate() const {
+        REPORTER_ASSERT(fReporter, fQueue.isEmpty());
+
+        for (int i = 0; i < fNodes.count(); ++i) {
+            REPORTER_ASSERT(fReporter, fNodes[i]->indexInSort() == i);
+
+            fNodes[i]->validate(fReporter);
+        }
+    }
+
+private:
+    void shift(int index) {
+        int numRemoved = 0;
+        while (fQueue.count()) {
+            sk_sp<Node> node = fNodes[index];
+
+            if (node->visited()) {
+                node->setVisited(false);  // reset for future use
+                numRemoved++;
+            } else {
+                SkASSERT(!fNodes[index - numRemoved]);
+                fNodes[index - numRemoved] = node;
+                node->setIndexInSort(index - numRemoved);
+            }
+
+            Node* tmp = fQueue.top();
+            while (fQueue.count() && node.get() == tmp) {
+                numRemoved--;
+
+                SkASSERT(!fNodes[index - numRemoved]);
+                fNodes[index - numRemoved] = sk_ref_sp(tmp);
+                tmp->setIndexInSort(index - numRemoved);
+
+                fQueue.pop();
+                tmp = fQueue.top();
+            }
+
+            index++;
+        }
+    }
+
+    void dfs(Node* node, int ) {
+        node->setVisited(true);
+
+        for (int i = 0; i < node->)
+    }
+
+    SkTArray<sk_sp<Node>> fNodes;
+
+    Queue fQueue;     // only used in addEdges
+
+    skiatest::Reporter* fReporter;
+};
+
+
+static void test_diamond(skiatest::Reporter* reporter) {
+    Graph g(4, reporter);
+
+    // Create the graph (the '.' is the pointy side of the arrow):
+    //        1
+    //      .    \
+    //     /      .
+    //   0          3
+    //     \      .
+    //       .   /
+    //         2
+    // Possible topological orders are [0,2,1,3] and [0,1,2,3].
+
+    Graph::Node* node3 = g.addNode(3);
+    Graph::Node* node2 = g.addNode(2);
+    Graph::Node* node1 = g.addNode(1);
+    g.validate();
+
+    SkTDArray<Graph::Node*> dependedOn;
+    dependedOn.push(node1);
+    dependedOn.push(node2);
+
+    g.addEdges(dependedOn, node3); // nodes 1 and 2 must come before node3
+    g.print();
+
+    Graph::Node* node0 = g.addNode(0);
+    g.addEdge(node0, node1);            // node0 must come before node1
+    g.print();
+    g.addEdge(node0, node2);            // node0 must come before node2
+    g.print();
+
+    const SkString kExpected0("0,2,1,3");
+    const SkString kExpected1("0,1,2,3");
+    SkString actual;
+    g.getActual(&actual);
+
+    REPORTER_ASSERT(reporter, kExpected0 == actual || kExpected1 == actual);
+}
+
+static void test_lopsided_binary_tree(skiatest::Reporter* reporter) {
+    Graph g(7, reporter);
+
+    // Create the graph (the '.' is the pointy side of the arrow):
+    //        0
+    //       /  \
+    //      .    .
+    //     1      2
+    //           /  \
+    //          .    .
+    //         3      4
+    //               /  \
+    //              .    .
+    //             5      6
+    //
+    // Possible topological order is: [0,1,2,3,4,5,6].
+
+}
+
+DEF_TEST(IncrTopoSort, reporter) {
+    test_diamond(reporter);
+    test_lopsided_binary_tree(reporter);
+}
