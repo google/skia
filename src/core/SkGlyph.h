e42dc2dbd9e561f862d11892bffe3ebfe5d15374
Herb Derby <herb@google.com>
2018-08-01 14:49:26 -0400

    Hoist path check into regenerateGlyphRunList.
    Now, regenerateGlyphRunList is making all the rendering
    position, and cache choice decisions. Mostly.
    
    Change-Id: Ib946e2ef2f422c915b324ac93459c65bf559f608
    Reviewed-on: https://skia-review.googlesource.com/144905
    Auto-Submit: Herb Derby <herb@google.com>
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Jim Van Verth <jvanverth@google.com>

--- src/core/SkGlyph.h
+++ src/core/SkGlyph.h
@@ -162,6 +162,10 @@ public:
 
     void initWithGlyphID(SkPackedGlyphID glyph_id);
 
+    bool isEmpty() {
+        return fWidth == 0 || fHeight == 0;
+    }
+
     size_t formatAlignment() const;
     size_t allocImage(SkArenaAlloc* alloc);
 
