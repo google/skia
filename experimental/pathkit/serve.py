2811c4032cd1f3b64454ee9756066ddbbc3e5690
Kevin Lubick <kjlubick@google.com>
2018-08-01 14:04:53 -0400

    Productionize PathKit
    Getting ready to put on npm
    
    
    
    Bug: skia:
    Change-Id: I15f14dd8acd77331729998ae3e30d73e4b006761

---
+++ experimental/pathkit/serve.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Google LLC
+
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    pass
+
+Handler.extensions_map['.js'] = 'application/javascript'
+Handler.extensions_map['.wasm'] = 'application/wasm'
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
