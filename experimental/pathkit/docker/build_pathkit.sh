80a88443b271b4add74d85ff388b0aa4bcc5b734
Kevin Lubick <kjlubick@google.com>
2018-08-02 14:36:42 -0400

    Add Dockerfile for building WASM stuff
    Bug: skia:8216
    Change-Id: I993bba38b4978ca5eebb97e5b5b21729d55a072d

---
+++ experimental/pathkit/docker/build_pathkit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright 2018 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This assumes it is being run inside a docker container of emsdk-base
+# and a Skia checkout has been mounted at /SRC and the output directory
+# is mounted at /OUT
+
+# For example:
+# docker run -v $SKIA_ROOT:/SRC -v $SKIA_ROOT/out/dockerpathkit:/OUT gcr.io/skia-public/emsdk-release:1.38.6 /SRC/experimental/pathkit/docker/build_pathkit.sh
+
+BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
+BUILD_DIR=/OUT $BASE_DIR/../compile.sh
