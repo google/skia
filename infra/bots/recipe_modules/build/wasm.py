ecd856dd4ff82a03e787a13b01abe4229991703e
Kevin Lubick <kjlubick@google.com>
2018-08-02 14:23:06 -0400

    Add Dockerfile for building WASM stuff
    Bug: skia:8216
    Change-Id: I993bba38b4978ca5eebb97e5b5b21729d55a072d

---
+++ infra/bots/recipe_modules/build/wasm.py
@@ -0,0 +1,29 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+def compile_fn(api, checkout_root, out_dir):
+  DOCKER_IMAGE = 'gcr.io/skia-public/emsdk-release:1.38.6'
+  INNER_BUILD_SCRIPT = '/SRC/experimental/pathkit/docker/build_pathkit.sh'
+
+  api.python.inline(
+      name='Authenticate Docker',
+      program='''
+import os
+
+os.system("gcloud auth print-access-token | "
+    "docker login -u oauth2accesstoken --password-stdin https://gcr.io")
+''',
+      infra_step=True)
+
+  api.run(
+    api.step,
+    'Build PathKit with Docker',
+    cmd=['docker', 'run', '--rm', '-v', '%s:/SRC' % checkout_root,
+         '-v', '%s:/OUT' % out_dir,
+         DOCKER_IMAGE, INNER_BUILD_SCRIPT]
+    )
+
+def copy_extra_build_products(api, out_dir, dst):
+  pass
