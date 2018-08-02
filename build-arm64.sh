0643a29eaa43805c455137b595a6d8fc53ea34a3
Avaer Kazmer <a@modules.io>
2018-08-02 17:22:11 +0000

    Update arm64 build script

--- build-arm64.sh
+++ build-arm64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./tools/git-sync-deps
-./bin/gn gen out/Static --args='is_official_build=true ndk="/work/android-ndk-r17b" target_cpu="arm64" skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false'
+./bin/gn gen out/Static --args='ndk="/work/android-ndk-r17b" target_cpu="arm64" is_official_build=true skia_use_freetype=true skia_use_fontconfig=true skia_use_expat=false skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false'
 ninja -C out/Static
