7d95f7123eb63612189ed2713954c4623b710a35
Ethan Nicholas <ethannicholas@google.com>
2018-08-03 16:16:57 -0400

    added SkSL saturate() function
    Bug: skia:8220
    Change-Id: Ib2e58ac77345a2aa53302c6c1484d52533556f93

--- src/gpu/ops/GrDashOp.cpp
+++ src/gpu/ops/GrDashOp.cpp
@@ -951,7 +951,7 @@ void GLDashingCircleEffect::onEmitCode(EmitArgs& args, GrGPArgs* gpArgs) {
     if (dce.aaMode() != AAMode::kNone) {
         fragBuilder->codeAppendf("half diff = dist - %s.x;", circleParams.fsIn());
         fragBuilder->codeAppend("diff = 1.0 - diff;");
-        fragBuilder->codeAppend("half alpha = clamp(diff, 0.0, 1.0);");
+        fragBuilder->codeAppend("half alpha = saturate(diff);");
     } else {
         fragBuilder->codeAppendf("half alpha = 1.0;");
         fragBuilder->codeAppendf("alpha *=  dist < %s.x + 0.5 ? 1.0 : 0.0;", circleParams.fsIn());
