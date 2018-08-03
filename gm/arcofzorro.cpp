98e4c223a4db5d14ebd9d7935407f7f893be0405
Robert Phillips <robertphillips@google.com>
2018-08-03 09:25:35 -0400

    Recreate Android porter-duff blend modes
    Change-Id: I9cec402c1d6539008d4345b88ce846e8e8acdbda

--- gm/arcofzorro.cpp
+++ gm/arcofzorro.cpp
@@ -7,7 +7,11 @@
 
 #include "gm.h"
 #include "sk_tool_utils.h"
+#include "SkBitmap.h"
+#include "SkPath.h"
 #include "SkRandom.h"
+
+#include "Resources.h"
 
 namespace skiagm {
 
@@ -27,51 +31,59 @@ protected:
     }
 
     SkISize onISize() override {
-        return SkISize::Make(1000, 1000);
+        return SkISize::Make(128, 128);
     }
 
     void onDraw(SkCanvas* canvas) override {
-        SkRandom rand;
 
-        SkRect rect = SkRect::MakeXYWH(10, 10, 200, 200);
+        sk_tool_utils::draw_checkerboard(canvas, SK_ColorLTGRAY, SK_ColorDKGRAY, 8);
 
-        SkPaint p;
-
-        p.setStyle(SkPaint::kStroke_Style);
-        p.setStrokeWidth(35);
-        int xOffset = 0, yOffset = 0;
-        int direction = 0;
-
-        for (float arc = 134.0f; arc < 136.0f; arc += 0.01f) {
-            SkColor color = rand.nextU();
-            color |= 0xff000000;
-            p.setColor(color);
-
-            canvas->save();
-            canvas->translate(SkIntToScalar(xOffset), SkIntToScalar(yOffset));
-            canvas->drawArc(rect, 0, arc, false, p);
-            canvas->restore();
-
-            switch (direction) {
-            case 0:
-                xOffset += 10;
-                if (xOffset >= 700) {
-                    direction = 1;
-                }
-                break;
-            case 1:
-                xOffset -= 10;
-                yOffset += 10;
-                if (xOffset < 50) {
-                    direction = 2;
-                }
-                break;
-            case 2:
-                xOffset += 10;
-                break;
-            }
+        SkImageInfo ii = SkImageInfo::MakeN32Premul(128, 128);
+        SkBitmap blueRect;
+        {
+            blueRect.allocPixels(ii);
+            SkCanvas c1(blueRect);
+            c1.clear(SK_ColorTRANSPARENT);
+            SkPaint p;
+            p.setColor(SK_ColorBLUE);
+            c1.drawRect(SkRect::MakeXYWH(16, 40, 64, 64), p);
         }
 
+        SkBitmap redCircle;
+        {
+            redCircle.allocPixels(ii);
+            SkCanvas c1(redCircle);
+            c1.clear(SK_ColorTRANSPARENT);
+            SkPaint p;
+            p.setColor(SK_ColorRED);
+            c1.drawCircle(72, 40, 32, p);
+        }
+
+
+//        canvas->saveLayer(nullptr, nullptr);
+
+        SkPaint p;
+        canvas->drawBitmap(redCircle, 0, 0, &p);
+        p.setBlendMode(SkBlendMode::kDstIn);
+        canvas->drawBitmap(blueRect, 0, 0, &p);
+
+#if 0
+        SkPaint paint;
+        paint.setColor(sk_tool_utils::color_to_565(0xFFCCCCCC));
+        paint.setStyle(SkPaint::kFill_Style);
+        paint.setAntiAlias(true);
+        paint.setBlendMode(SkBlendMode::kSrc);
+
+        // kSrcIn - works
+        // kDst - works
+        // kDstATop - works
+        SkPath path = sk_tool_utils::make_star(SkRect::MakeXYWH(64, 64, 128, 128));
+        path.setFillType(SkPath::kInverseEvenOdd_FillType);
+
+        canvas->drawPath(path, paint);
+#endif
+
+//        canvas->restore();
     }
 
 private:
