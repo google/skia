e3132270a9dbf56957ffcf6addc0dbfe9b7ec934
Robert Phillips <robertphillips@google.com>
2018-08-03 11:09:57 -0400

    Update GrTextureStripAtlas for DDLs (take 2)
    Change-Id: I4a3f71ffe4a14785e7befddc378929cf4dcf7d8e

--- src/gpu/effects/GrTextureStripAtlas.cpp
+++ src/gpu/effects/GrTextureStripAtlas.cpp
@@ -8,10 +8,10 @@
 #include "GrTextureStripAtlas.h"
 #include "GrContext.h"
 #include "GrContextPriv.h"
+#include "GrDDLTextureStripAtlas.h"
 #include "GrDynamicTextureStripAtlas.h"
 #include "SkBitmap.h"
 
-////////////////////////////////////////////////////////////////////////////////
 GrTextureStripAtlasManager::~GrTextureStripAtlasManager() {
     this->deleteAllAtlases();
 }
@@ -30,15 +30,40 @@ void GrTextureStripAtlasManager::abandon() {
     this->deleteAllAtlases();
 }
 
-sk_sp<GrTextureStripAtlas> GrTextureStripAtlasManager::refAtlas(
-                                                          const GrTextureStripAtlas::Desc& desc) {
+void GrTextureStripAtlasManager::finish(GrProxyProvider* proxyProvider) {
+    for (AtlasHash::Iter iter(&fAtlasCache); !iter.done(); ++iter) {
+        AtlasEntry* tmp = &(*iter);
+        tmp->fAtlas->finish(proxyProvider);
+    }
+}
+
+
+sk_sp<GrTextureStripAtlas> GrTextureStripAtlasManager::addStrip(
+                                                          GrContext* context,
+                                                          const GrTextureStripAtlas::Desc& desc,
+                                                          const SkBitmap& bitmap,
+                                                          int* row) {
+    SkASSERT(kPremul_SkAlphaType == bitmap.alphaType());
+
     AtlasEntry* entry = fAtlasCache.find(desc);
     if (!entry) {
-        // TODO: Does the AtlasEntry need a copy of the Desc if the GrTextureStripAtlas has one?
-        entry = new AtlasEntry(desc, sk_sp<GrTextureStripAtlas>(new GrTextureStripAtlas(desc)));
+        sk_sp<GrTextureStripAtlas> atlas;
 
+        if (!context->contextPriv().resourceProvider()) {
+            atlas.reset(new GrDDLTextureStripAtlas(desc));
+        } else {
+            atlas.reset(new GrDynamicTextureStripAtlas(desc));
+        }
+
+        entry = new AtlasEntry(sk_sp<GrTextureStripAtlas>(std::move(atlas)));
         fAtlasCache.add(entry);
+    }
+
+    *row = entry->fAtlas->addStrip(context, bitmap);
+    if (*row < 0) {
+        return nullptr;
     }
 
     return entry->fAtlas;
 }
+
