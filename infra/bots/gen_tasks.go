d3cc9995e62e1a96519f889f9cb894972643ca8c
Ben Wagner <benjaminwagner@google.com>
2018-08-03 13:30:45 -0400

    Update remaining Mac bots to 10.13.6
    Bug: chromium:862292
    Change-Id: I8fdff24938f000da550e1c5420c7ac312f09dce7

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -43,7 +43,7 @@ const (
 
 	DEFAULT_OS_DEBIAN    = "Debian-9.4"
 	DEFAULT_OS_LINUX_GCE = DEFAULT_OS_DEBIAN
-	DEFAULT_OS_MAC       = "Mac-10.13.3"
+	DEFAULT_OS_MAC       = "Mac-10.13.6"
 	DEFAULT_OS_UBUNTU    = "Ubuntu-14.04"
 	DEFAULT_OS_WIN       = "Windows-2016Server-14393"
 
@@ -450,14 +450,6 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 		if d["os"] == DEFAULT_OS_WIN {
 			// TODO(dogben): Temporarily add image dimension during upgrade.
 			d["image"] = "windows-server-2016-dc-v20180710"
-		}
-		// TODO(dogben): Most Mac bots are on 10.13.6. Mac builders are stuck on
-		// 10.13.3 for now because of unresolved issues with building iOS and
-		// MoltenVK. MacMini7.1 are stuck on 10.13.3 because they run *SAN, which
-		// (currently) needs to have an XCode installed that matches the XCode of
-		// the builder.
-		if d["os"] == DEFAULT_OS_MAC && !(parts["model"] == "MacMini7.1" || parts["role"] == "Build") {
-			d["os"] = "Mac-10.13.6"
 		}
 	} else {
 		d["os"] = DEFAULT_OS_DEBIAN
