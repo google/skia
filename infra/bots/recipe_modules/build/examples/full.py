137ba1cdc5446ba22fc2fb4140c682d6fe9a534a
Ben Wagner <benjaminwagner@google.com>
2018-08-03 15:06:55 -0400

    Add Win OpenCL Build job.
    Bug: skia:8081
    Change-Id: Ibcb82389668bd9ea7395e498696417d5701761c0

--- infra/bots/recipe_modules/build/examples/full.py
+++ infra/bots/recipe_modules/build/examples/full.py
@@ -58,6 +58,7 @@ TEST_BUILDERS = [
   'Build-Mac-Clang-x86_64-Release-MoltenVK_Vulkan',
   'Build-Win-Clang-arm64-Release-Android',
   'Build-Win-Clang-x86-Debug-Exceptions',
+  'Build-Win-Clang-x86_64-Debug-OpenCL',
   'Build-Win-Clang-x86_64-Release-Vulkan',
   'Housekeeper-PerCommit-CheckGeneratedFiles',
 ]
