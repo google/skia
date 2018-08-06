6fae4d49aec9b4eb9916f4dc316f758fbec94a95
Kevin Lubick <kjlubick@google.com>
2018-08-06 14:49:39 -0400

    [PathKit] Add more Path2D functionality and move some methods to be members
    Adds arc, arcTo, rect and Path2D names for quadTo, cubicTo, close.
    
    Adds conic verb support (approximated with 2 quads).
    
    Breaking changes:
    Some functions have been moved to be member functions:
    PathKit.Simplify(path) -> path.simplify()
    PathKit.ToCanvas(path, ctx) -> path.toCanvas(ctx)
    PathKit.ToSVGString(path) -> path.toSVGString()
    PathKit.ToPath2D(path) -> path.toPath2D()
    PathKit.ToCmds(path) -> path.toCmds()
    PathKit.ResolveBuilder(builder) -> builder.resolve()
    PathKit.GetBoundaryPathFromRegion(region) -> region.getBoundaryPath()
    
    Pathkit.ApplyPathOp(pathOne, pathTwo, op) still exists, but there's
    now also pathOne.op(pathTwo, op) for cases when that's easier.
    
    As per custom with version 0.x.y projects, I'm bumping the
    minor version (in npm) for these breaking changes instead of the
    major version (which will happen when we are version >= 1.0.0).
    
    This also has some small improvements to the output code size.
    The biggest jump was from enabling the closure compiler on the
    helper JS, which trimmed it down by about 40%. Using the closure
    compiler requires the JRE on the bots, which prompted the emsdk-base
    image change.
    
    Bug: skia:8216
    Change-Id: I40902d23380093c34d1679df0255bcb0eaa77b01

--- experimental/pathkit/docker/build_pathkit.sh
+++ experimental/pathkit/docker/build_pathkit.sh
@@ -9,7 +9,7 @@
 # is mounted at /OUT
 
 # For example:
-# docker run -v $SKIA_ROOT:/SRC -v $SKIA_ROOT/out/dockerpathkit:/OUT gcr.io/skia-public/emsdk-release:1.38.6 /SRC/experimental/pathkit/docker/build_pathkit.sh
+# docker run -v $SKIA_ROOT:/SRC -v $SKIA_ROOT/out/dockerpathkit:/OUT gcr.io/skia-public/emsdk-release:1.38.6_jre /SRC/experimental/pathkit/docker/build_pathkit.sh
 
 BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 BUILD_DIR=/OUT $BASE_DIR/../compile.sh $@
