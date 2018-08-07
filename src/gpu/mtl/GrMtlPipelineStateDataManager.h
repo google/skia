ac90f307149aeeb2f37da9b7eb61fbc6e424d5d1
Timothy Liang <timliang@google.com>
2018-08-07 17:04:16 -0400

    created GrMtlPipelineStateDataManager for metal gpu backend
    Bug: skia:
    Change-Id: I630b1fc2e3a4195b8df8b04a02be8fc95bb9c4fe

---
+++ src/gpu/mtl/GrMtlPipelineStateDataManager.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrMtlPipelineStateDataManager_DEFINED
+#define GrMtlPipelineStateDataManager_DEFINED
+
+#include "glsl/GrGLSLProgramDataManager.h"
+#include "GrMtlUniformHandler.h"
+#include "SkAutoMalloc.h"
+
+class GrMtlBuffer;
+class GrMtlGpu;
+
+class GrMtlPipelineStateDataManager : public GrGLSLProgramDataManager {
+public:
+    typedef GrMtlUniformHandler::UniformInfoArray UniformInfoArray;
+
+    GrMtlPipelineStateDataManager(const UniformInfoArray&,
+                                  uint32_t geometryUniformSize,
+                                  uint32_t fragmentUniformSize);
+
+    void set1i(UniformHandle, int32_t) const override;
+    void set1iv(UniformHandle, int arrayCount, const int32_t v[]) const override;
+    void set1f(UniformHandle, float v0) const override;
+    void set1fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set2f(UniformHandle, float, float) const override;
+    void set2fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set3f(UniformHandle, float, float, float) const override;
+    void set3fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set4f(UniformHandle, float, float, float, float) const override;
+    void set4fv(UniformHandle, int arrayCount, const float v[]) const override;
+    // matrices are column-major, the first two upload a single matrix, the latter two upload
+    // arrayCount matrices into a uniform array.
+    void setMatrix2f(UniformHandle, const float matrix[]) const override;
+    void setMatrix3f(UniformHandle, const float matrix[]) const override;
+    void setMatrix4f(UniformHandle, const float matrix[]) const override;
+    void setMatrix2fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+    void setMatrix3fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+    void setMatrix4fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+
+    // for nvpr only
+    void setPathFragmentInputTransform(VaryingHandle u, int components,
+                                       const SkMatrix& matrix) const override {
+        SK_ABORT("Only supported in NVPR, which is not in Metal");
+    }
+
+    void uploadUniformBuffers(GrMtlGpu* gpu,
+                              GrMtlBuffer* geometryBuffer,
+                              GrMtlBuffer* fragmentBuffer) const;
+
+private:
+    struct Uniform {
+        uint32_t fBinding;
+        uint32_t fOffset;
+        SkDEBUGCODE(
+            GrSLType    fType;
+            int         fArrayCount;
+        );
+    };
+
+    template<int N> inline void setMatrices(UniformHandle, int arrayCount,
+                                            const float matrices[]) const;
+
+    void* getBufferPtrAndMarkDirty(const Uniform& uni) const;
+
+    uint32_t fGeometryUniformSize;
+    uint32_t fFragmentUniformSize;
+
+    SkTArray<Uniform, true> fUniforms;
+
+    mutable SkAutoMalloc fGeometryUniformData;
+    mutable SkAutoMalloc fFragmentUniformData;
+    mutable bool         fGeometryUniformsDirty;
+    mutable bool         fFragmentUniformsDirty;
+};
+
+#endif
