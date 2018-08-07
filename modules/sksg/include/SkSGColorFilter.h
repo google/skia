7975ae86664b8da8202d8b036b7aafec35554f4d
Florin Malita <fmalita@chromium.org>
2018-08-07 11:09:49 -0400

    [skottie] Initial Fill layer effect support
    Overwrite the layer content color with a color filter.
    
    Change-Id: I39f920225affb2641cc11ab1f0c1456d89b47cb7

---
+++ modules/sksg/include/SkSGColorFilter.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkSGColorFilter_DEFINED
+#define SkSGColorFilter_DEFINED
+
+#include "SkSGEffectNode.h"
+
+#include "SkBlendMode.h"
+
+class SkColorFilter;
+
+namespace sksg {
+
+class Color;
+
+/**
+ * Base class for nodes which apply a color filter when rendering their descendants.
+ *
+ */
+class ColorFilter : public EffectNode {
+protected:
+    explicit ColorFilter(sk_sp<RenderNode>);
+
+    void onRender(SkCanvas*) const final;
+
+    sk_sp<SkColorFilter> fColorFilter;
+
+private:
+    typedef EffectNode INHERITED;
+};
+
+/**
+ * Concrete SkModeColorFilter Effect node.
+ *
+ */
+class ColorModeFilter final : public ColorFilter {
+public:
+    ~ColorModeFilter() override;
+
+    static sk_sp<ColorModeFilter> Make(sk_sp<RenderNode> child, sk_sp<Color> color,
+                                       SkBlendMode mode) {
+        return (child && color)
+            ? sk_sp<ColorModeFilter>(new ColorModeFilter(std::move(child), std::move(color), mode))
+            : nullptr;
+    }
+
+    SG_ATTRIBUTE(Mode , SkBlendMode, fMode )
+
+protected:
+    SkRect onRevalidate(InvalidationController*, const SkMatrix&) override;
+
+private:
+    ColorModeFilter(sk_sp<RenderNode>, sk_sp<Color>, SkBlendMode);
+
+    sk_sp<Color> fColor;
+    SkBlendMode  fMode;
+
+    typedef ColorFilter INHERITED;
+};
+
+} // namespace sksg
+
+#endif // SkSGColorFilter_DEFINED
