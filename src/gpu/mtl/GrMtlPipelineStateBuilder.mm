44636e9ab50ab20af6125d0e5d00f13f811489f3
Timothy Liang <timliang@google.com>
2018-08-08 10:50:21 -0400

    created GrMtlPipelineStateDataManager for metal gpu backend
    Bug: skia:
    Change-Id: I630b1fc2e3a4195b8df8b04a02be8fc95bb9c4fe
    Reviewed-on: https://skia-review.googlesource.com/145329
    Commit-Queue: Timothy Liang <timliang@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/gpu/mtl/GrMtlPipelineStateBuilder.mm
+++ src/gpu/mtl/GrMtlPipelineStateBuilder.mm
@@ -1,9 +1,9 @@
 /*
-* Copyright 2018 Google Inc.
-*
-* Use of this source code is governed by a BSD-style license that can be
-* found in the LICENSE file.
-*/
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 
 #include "GrMtlPipelineStateBuilder.h"
 
