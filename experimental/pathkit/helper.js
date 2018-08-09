e239f49ebc9f693ffb3346ded16c467c358f0f3c
Kevin Lubick <kjlubick@google.com>
2018-08-09 11:44:43 -0400

    [PathKit] Add PathOps tests imported from the C++ tests.
    Because of https://skia-review.googlesource.com/c/skia/+/146165 and
    https://skia-review.googlesource.com/c/skia/+/146100 we have a way
    to turn PathOps tests into JSON, which has input paths (as Cmd arrays),
    combination verb and expected output.
    
    In this CL, we make tests from the JSON, compare them to the expected
    output and, optionally, create SVGs to visualize the difference if any.
    
    API changes (nothing breaking on release builds):
     - Remove SkRegion exposure (debug/test only), which was going to be
    used for this purpose, but the approach in this CL works fine.
     - Add loadCmdsTypedArray(cmd) helper function to JS [see helper.js].
    This was previously known as `floatTypedArrayFrom2D` in the
    old shell.html, and is now exposed to avoid clients having to
    implement this boilerplate by themselves.
     - Add set/getFillType - mostly for testing the difference between
    a Winding and an EvenOdd path.
     - Exposes SkRect as a JS Object.  No need to call delete() on this.
    
    Bug: skia:8216
    Change-Id: I2cd25ce2e1e7f285c79c596678678e62135963f0

---
+++ experimental/pathkit/helper.js
@@ -0,0 +1,61 @@
+// Adds any extra JS functions/helpers we want to the PathKit Library.
+// Wrapped in a function to avoid leaking global variables.
+(function(PathKit){
+
+  // Caching the Float32Arrays can save having to reallocate them
+  // over and over again.
+  var Float32ArrayCache = {};
+
+  // Takes a 2D array of commands and puts them into the WASM heap
+  // as a 1D array.  This allowing them to referenced from the C++ code.
+  // Returns a 2 element array, with the first item being essentially a
+  // pointer to the array and the second item being the lengh of
+  // the new 1D array.
+  //
+  // Example usage:
+  // let cmds = [
+  //   [PathKit.MOVE_VERB, 0, 10],
+  //   [PathKit.LINE_VERB, 30, 40],
+  //   [PathKit.QUAD_VERB, 20, 50, 45, 60],
+  // ];
+  //
+  // // The following uses ES6 syntactic sugar "Array Destructuring".
+  // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Array_destructuring
+  // let [ptr, len] = PathKit.loadCmdsTypedArray(cmds);
+  // let path = PathKit.FromCmds(ptr, len);
+  //
+  // If arguments at index 1... in each cmd row are strings, they will be
+  // parsed as hex, and then converted to floats using SkBits2FloatUnsigned
+  PathKit['loadCmdsTypedArray'] = function(arr) {
+    var len = 0;
+    for (var r = 0; r < arr.length; r++) {
+      len += arr[r].length;
+    }
+
+    var ta;
+    if (Float32ArrayCache[len]) {
+      ta = Float32ArrayCache[len];
+    } else {
+      ta = new Float32Array(len);
+      Float32ArrayCache[len] = ta;
+    }
+    // Flatten into a 1d array
+    var i = 0;
+    for (var r = 0; r < arr.length; r++) {
+      for (var c = 0; c < arr[r].length; c++) {
+        var item = arr[r][c];
+        if (typeof item === 'string') {
+          // Converts hex to an int, which can be passed to SkBits2FloatUnsigned
+          item = PathKit.SkBits2FloatUnsigned(parseInt(item));
+        }
+        ta[i] = item;
+        i++;
+      }
+    }
+
+    var ptr = PathKit._malloc(ta.length * ta.BYTES_PER_ELEMENT);
+    PathKit.HEAPF32.set(ta, ptr / ta.BYTES_PER_ELEMENT);
+    return [ptr, len];
+  }
+}(Module)); // When this file is loaded in, the high level object is "Module";
+
