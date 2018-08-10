6ed63968a39131df7a6b01600bf32c32800be05d
Timothy Liang <timliang@google.com>
2018-08-10 09:49:44 -0400

    implemented GrMtlPipelineState for metal gpu backend
    Bug: skia:
    Change-Id: I4284b760411d9ec24cccfb89352406dbb696c3c9
    Reviewed-on: https://skia-review.googlesource.com/145896
    Commit-Queue: Timothy Liang <timliang@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/gpu/mtl/GrMtlPipelineState.h
+++ src/gpu/mtl/GrMtlPipelineState.h
@@ -9,10 +9,16 @@
 #define GrMtlPipelineState_DEFINED
 
 #include "GrMtlBuffer.h"
+#include "GrMtlPipelineStateDataManager.h"
+#include "glsl/GrGLSLProgramBuilder.h"
 
 #import <metal/metal.h>
 
 class GrMtlGpu;
+class GrMtlPipelineStateDataManager;
+class GrMtlSampler;
+class GrMtlTexture;
+class GrPipeline;
 
 /**
  * Wraps a MTLRenderPipelineState object and also contains more info about the pipeline as needed
@@ -20,21 +26,97 @@ class GrMtlGpu;
  */
 class GrMtlPipelineState {
 public:
-    GrMtlPipelineState(GrMtlGpu* gpu,
-                       id<MTLRenderPipelineState> pipelineState,
-                       MTLPixelFormat pixelFormat,
-                       GrMtlBuffer* geometryUniformBuffer,
-                       GrMtlBuffer* fragmentUniformBuffer);
+    using UniformInfoArray = GrMtlPipelineStateDataManager::UniformInfoArray;
+    using UniformHandle = GrGLSLProgramDataManager::UniformHandle;
+
+    GrMtlPipelineState(
+            GrMtlGpu* gpu,
+            id<MTLRenderPipelineState> pipelineState,
+            MTLPixelFormat pixelFormat,
+            const GrGLSLBuiltinUniformHandles& builtinUniformHandles,
+            const UniformInfoArray& uniforms,
+            GrMtlBuffer* geometryUniformBuffer,
+            GrMtlBuffer* fragmentUniformBuffer,
+            uint32_t numSamplers,
+            std::unique_ptr<GrGLSLPrimitiveProcessor> geometryProcessor,
+            std::unique_ptr<GrGLSLXferProcessor> xferPRocessor,
+            std::unique_ptr<std::unique_ptr<GrGLSLFragmentProcessor>[]> fragmentProcessors,
+            int fFragmentProcessorCnt);
 
     id<MTLRenderPipelineState> mtlPipelineState() { return fPipelineState; }
 
-private:
-    GrMtlGpu* fGpu;
+    void setData(const GrPrimitiveProcessor& primPRoc, const GrPipeline& pipeline,
+                 const GrTextureProxy* const primProcTextures[]);
 
+    void bind(id<MTLRenderCommandEncoder>);
+
+private:
+    /**
+    * We use the RT's size and origin to adjust from Skia device space to Metal normalized device
+    * space and to make device space positions have the correct origin for processors that require
+    * them.
+    */
+    struct RenderTargetState {
+        SkISize         fRenderTargetSize;
+        GrSurfaceOrigin fRenderTargetOrigin;
+
+        RenderTargetState() { this->invalidate(); }
+        void invalidate() {
+            fRenderTargetSize.fWidth = -1;
+            fRenderTargetSize.fHeight = -1;
+            fRenderTargetOrigin = (GrSurfaceOrigin)-1;
+        }
+
+        /**
+        * Gets a float4 that adjusts the position from Skia device coords to Metals normalized
+        * device coords. Assuming the transformed position, pos, is a homogeneous float3, the vec,
+        * v, is applied as such:
+        * pos.x = dot(v.xy, pos.xz)
+        * pos.y = dot(v.zw, pos.yz)
+        */
+        void getRTAdjustmentVec(float* destVec) {
+            destVec[0] = 2.f / fRenderTargetSize.fWidth;
+            destVec[1] = -1.f;
+            if (kBottomLeft_GrSurfaceOrigin == fRenderTargetOrigin) {
+                destVec[2] = -2.f / fRenderTargetSize.fHeight;
+                destVec[3] = 1.f;
+            } else {
+                destVec[2] = 2.f / fRenderTargetSize.fHeight;
+                destVec[3] = -1.f;
+            }
+        }
+    };
+
+    void setRenderTargetState(const GrRenderTargetProxy*);
+
+    struct SamplerBindings {
+        id<MTLSamplerState> fSampler;
+        id<MTLTexture> fTexture;
+        GrShaderFlags fVisibility;
+
+        SamplerBindings(const GrSamplerState& state, GrTexture* texture, GrShaderFlags flags,
+                        GrMtlGpu*);
+    };
+
+    GrMtlGpu* fGpu;
     id<MTLRenderPipelineState> fPipelineState;
     MTLPixelFormat             fPixelFormat;
-    sk_sp<GrMtlBuffer>         fGeometryUniformBuffer;
-    sk_sp<GrMtlBuffer>         fFragmentUniformBuffer;
+
+    RenderTargetState fRenderTargetState;
+    GrGLSLBuiltinUniformHandles fBuiltinUniformHandles;
+
+    sk_sp<GrMtlBuffer> fGeometryUniformBuffer;
+    sk_sp<GrMtlBuffer> fFragmentUniformBuffer;
+
+    int fNumSamplers;
+    SkTArray<SamplerBindings> fSamplerBindings;
+
+    std::unique_ptr<GrGLSLPrimitiveProcessor> fGeometryProcessor;
+    std::unique_ptr<GrGLSLXferProcessor> fXferProcessor;
+    std::unique_ptr<std::unique_ptr<GrGLSLFragmentProcessor>[]> fFragmentProcessors;
+    int fFragmentProcessorCnt;
+
+    GrMtlPipelineStateDataManager fDataManager;
 };
 
 #endif
