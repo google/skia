c36731a593dfa61c6e3d2e20b499d326fdbdb96c
Brian Osman <brianosman@google.com>
2018-08-15 11:16:06 -0400

    Make SkColor4f <-> SkColor skip sRGB conversion
    This lets us remove the old S32 functions, and fix a couple bugs.
    I think this is a good first step to using SkColor4f for paint colors.
    
    Bug: skia:
    Change-Id: I0337c2b6db29b73c2f682f85b9cf68c985de7cce

--- tests/SRGBTest.cpp
+++ tests/SRGBTest.cpp
@@ -7,39 +7,10 @@
 
 #include "SkPM4f.h"
 #include "SkRasterPipeline.h"
-#include "SkSRGB.h"
 #include "SkTypes.h"
 #include "Test.h"
 #include <math.h>
 #include "../src/jumper/SkJumper.h"
-
-static uint8_t linear_to_srgb(float l) {
-    return (uint8_t)sk_linear_to_srgb(Sk4f{l})[0];
-}
-
-DEF_TEST(sk_linear_to_srgb, r) {
-    // All bytes should round trip.
-    for (int i = 0; i < 256; i++) {
-        int actual = linear_to_srgb(sk_linear_from_srgb[i]);
-        if (i != actual) {
-            ERRORF(r, "%d -> %d\n", i, actual);
-        }
-    }
-
-    // Should be monotonic between 0 and 1.
-    uint8_t prev = 0;
-    for (float f = FLT_MIN; f <= 1.0f; ) {  // We don't bother checking denorm values.
-        uint8_t srgb = linear_to_srgb(f);
-
-        REPORTER_ASSERT(r, srgb >= prev);
-        prev = srgb;
-
-        union { float flt; uint32_t bits; } pun = { f };
-        pun.bits++;
-        SkDEBUGCODE(pun.bits += 127);
-        f = pun.flt;
-    }
-}
 
 DEF_TEST(sk_pipeline_srgb_roundtrip, r) {
     uint32_t reds[256];
