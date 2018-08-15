0143316c36926cd32546c9636098d1084e51aa2e
Leon Scroggins III <scroggo@google.com>
2018-08-15 16:24:40 -0400

    Switch SkCodec to use skcms, step 1
    Bug: skia:6839
    Bug: skia:8052
    
    Create an skcms_Profile instead of an SkColorSpace when creating an
    SkCodec. Eventually we'll move the SkImageInfo (and its SkColorSpace)
    entirely to clients (e.g. SkAndroidCodec, SkCodecImageGenerator), but
    for now, create it with SkEncodedInfo::makeImageInfo.
    
    Create new SkEncodedInfo::Colors for the special PNG cases that we
    want to map to specific SkColorTypes.
    
    SkEncodedInfo:
    - Add ICCProfile, which owns an skcms_ICCProfile
     - FIXME: maybe we should have a single instance for
      SRGB like SkColorSpace?
    - Add kXAlpha_Color, for kAlpha_8. Since I'm not longer creating
      an SkImageInfo (at least in SkPngCodec), it needs a way to pass
      this info to the caller.
    - Add k565_Color, for the same reason. Matt originally had this in
      https://codereview.chromium.org/2212563003/#ps120001, but didn't
      land that version. I like it though. Mike didn't like the bits
      per component for 565, but it seems like a sensible hack, much
      like the existing one for kAlpha_8
    - Add width and height. These were removed for redundancy with
      SkImageInfo, but it makes sense to have them here without it.
    BUILD.gn:
    - Build the new SkEncodedInfo.cpp
      FIXME: This will cause problems with Chromium roll :(
      It wasn't too hard to fix with SkColorTable.cpp, but it was
      tedious.
    SkCodec:
    - Remove the constructor with an SkImageInfo. Edit the other one
      to drop width and height (now in SkEncodedInfo) and take a RHS
      reference to SkEncodedInfo
    - Create the SkImageInfo from fEncodedInfo (for now)
    - Consolidate choosing skcms_AlphaFormat for Transform here
    SkBmpCodec (etc)
    - Adapt to the changes above
    - Create a new SkEncodedInfo w/o profile for the swizzler.
    SkPngCodec:
    - use the new SkEncodedInfo::Colors rather than a custom SkImageInfo
    SkRawCodec:
    - Remove SkEncodedInfo from SkDngImage, which doesn't actually need it.
      This is helpful since we don't know all the info yet.
    - Rewrite gAdobeRGB_toXYZD50 as an skcms_Matrix3x3
    SkWebpCodec:
    - Remove premul_step computation, and simplify to just rely on
      the base class' handling of applying the transform.
    SkSwizzler:
    - Add cases for the new SkEncodedInfo::Colors
    
    Change-Id: Ic0d3bb752b03f13be886b80331987aa5a5713fc0

---
+++ tests/EncodedInfoTest.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "Resources.h"
+#include "Test.h"
+#include "sk_tool_utils.h"
+
+#include "SkBitmap.h"
+#include "SkCodec.h"
+#include "SkData.h"
+#include "SkEncodedImageFormat.h"
+#include "SkImageInfo.h"
+#include "SkImageEncoder.h"
+
+DEF_TEST(AlphaEncodedInfo, r) {
+    auto codec = SkCodec::MakeFromStream(GetResourceAsStream("images/grayscale.jpg"));
+    REPORTER_ASSERT(r, codec->getInfo().colorType() == kGray_8_SkColorType);
+
+    SkBitmap bm;
+    bm.allocPixels(codec->getInfo().makeColorType(kAlpha_8_SkColorType).makeColorSpace(nullptr));
+    auto result = codec->getPixels(codec->getInfo(), bm.getPixels(), bm.rowBytes());
+    REPORTER_ASSERT(r, result == SkCodec::kSuccess);
+
+    auto data = SkEncodeBitmap(bm, SkEncodedImageFormat::kPNG, 100);
+    REPORTER_ASSERT(r, data);
+
+    codec = SkCodec::MakeFromData(std::move(data));
+    REPORTER_ASSERT(r, codec);
+    // TODO: Make SkEncodedInfo public and compare to its version of kAlpha_8.
+    REPORTER_ASSERT(r, codec->getInfo().colorType() == kAlpha_8_SkColorType);
+
+    SkBitmap bm2;
+    bm2.allocPixels(codec->getInfo().makeColorSpace(nullptr));
+    result = codec->getPixels(bm2.pixmap());
+    REPORTER_ASSERT(r, result == SkCodec::kSuccess);
+
+    REPORTER_ASSERT(r, sk_tool_utils::equal_pixels(bm.pixmap(), bm2.pixmap(), 0, true));
+}
