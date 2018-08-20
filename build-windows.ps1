11792bf11e93b0003788d1487a524dbcd195868b
Avaer Kazmer <a@modules.io>
2018-08-19 21:11:30 -0400

    Clean up build-windows script

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,3 +1,4 @@
+set PATH=%PATH%;C:\Users\avaer\.windows-build-tools\python27
 python .\tools\git-sync-deps
 .\bin\gn.exe gen out\Static --args="is_official_build=true skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
-ninja -C out\Static
+.\bin\ninja.exe -C out\Static
