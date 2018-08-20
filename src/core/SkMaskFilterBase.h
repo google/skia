3d4e6ff827219ac1a3c6a7fb23d2bdce0d48f4e0
Robert Phillips <robertphillips@google.com>
2018-08-20 10:42:17 -0400

    Change drawPathWithMaskFilter to drawShapeWithMaskFilter
    Change-Id: I584765177f2bb250a449d506d38f21f879fecdc6

--- src/core/SkMaskFilterBase.h
+++ src/core/SkMaskFilterBase.h
@@ -24,6 +24,7 @@ class GrPaint;
 class GrRenderTarget;
 class GrRenderTargetContext;
 class GrResourceProvider;
+class GrShape;
 class GrTexture;
 class GrTextureProxy;
 
@@ -89,8 +90,6 @@ public:
      *    canFilterMaskGPU is called
      *    if (it returns true)
      *        the returned mask rect is used for quick rejecting
-     *        either directFilterMaskGPU or directFilterRRectMaskGPU is then called
-     *        if (neither of them handle the blur)
      *            the mask rect is used to generate the mask
      *            filterMaskGPU is called to filter the mask
      *
@@ -99,7 +98,8 @@ public:
      *        filterMaskGPU(devShape, ...)
      * this would hide the RRect special case and the mask generation
      */
-    virtual bool canFilterMaskGPU(const SkRRect& devRRect,
+    virtual bool canFilterMaskGPU(const GrShape&,
+                                  const SkRect& devSpaceShapeBounds,
                                   const SkIRect& clipBounds,
                                   const SkMatrix& ctm,
                                   SkRect* maskRect) const;
@@ -113,8 +113,7 @@ public:
                                      GrPaint&& paint,
                                      const GrClip&,
                                      const SkMatrix& viewMatrix,
-                                     const SkStrokeRec& strokeRec,
-                                     const SkPath& path) const;
+                                     const GrShape& shape) const;
     /**
      *  Try to directly render a rounded rect mask filter into the target.  Returns
      *  true if drawing was successful.  If false is returned then paint is unmodified.
