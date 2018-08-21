2ea9e4c90a265b6fb3c4e83404c4e198e2a77344
Michael Ludwig <michaelludwig@google.com>
2018-08-21 16:20:34 -0400

    Base Gradient FP Refactor =========================
    Redefines how gradients will be written in the GPU back-end:
    
    They are split into three fragment processor components: master, layout, and colorizer.
    The layout FP is responsible for converting the fragment position into an interpolant value, t.
    Each high-level gradient--such as linear, radial, etc.--are implemented solely in a layout FP.
    The colorizer FP is responsible for converting t into a color.
    The master FP invokes the layout, clamps t into the proper domain, and then invokes the colorizer.
    GrGradientShader provides factory functions to create FP graphs from SkGradientShader instances.
    This pattern is documented in gpu/gradients/README.md.
    
    Goals for current CL
    ====================
    
    Outline the FP components by providing .fp implementations for the simplest gradients.
    Defines a two-color single interval colorizer and a linear gradient layout, and the master effect.
    A MakeLinear() factory function is provided that can convert SkGradientShaders that fit these constraints.
    SkLinearGradient first attempts to use the new system, falling back to the original GrGradientEffect.
    
    Future CLs
    ==========
    
    To keep the CL reviews manageable, additional dependent CLs will be added that gradually replace past functionality.
    A CL for each layout will be defined.
    CLs for the different analytic colorizer cases and the textured gradient case will be defined.
    Once the new system supports all current layouts and colorizer capabilities, all old GPU gradient code will be removed.
    After this clean-up, analytic colorization can hopefully be expanded to reduce the usage of textured gradients.
    
    Bug: skia:
    Change-Id: Iafe7b8b4071491a71c473babcd7bedda659150c1

---
+++ src/gpu/gradients/GrGradientShader.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrGradientShader_DEFINE
+#define GrGradientShader_DEFINE
+
+#include "GrFPArgs.h"
+#include "SkGradientShaderPriv.h"
+
+namespace GrGradientShader {
+    std::unique_ptr<GrFragmentProcessor> MakeLinear(
+        const SkGradientShaderBase& shader, const GrFPArgs& args,
+        const SkPoint& start, const SkPoint& end);
+}
+
+#endif // GrGradientShader_DEFINE
