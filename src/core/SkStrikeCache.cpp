4503961061e65bb69d14ba5ed230de36f6cfbd0d
Herb Derby <herb@google.com>
2018-08-21 11:58:00 -0400

    Fix possible nullptr deref
    Check that the fPathData object exists before
    derefing to get the path.
    
    BUG=chromium:873020
    
    Change-Id: Ie381178f49905a0dc3f5c3a3477453d4785507f4

--- src/core/SkStrikeCache.cpp
+++ src/core/SkStrikeCache.cpp
@@ -334,7 +334,7 @@ bool SkStrikeCache::desperationSearchForPath(
         if (loose_compare(node->fCache.getDescriptor(), desc)) {
             if (node->fCache.isGlyphCached(glyphID, 0, 0)) {
                 SkGlyph* from = node->fCache.getRawGlyphByID(SkPackedGlyphID(glyphID));
-                if (from->fPathData->fPath != nullptr) {
+                if (from->fPathData != nullptr && from->fPathData->fPath != nullptr) {
                     // We can just copy the path out by value here, so no need to worry
                     // about the lifetime of this desperate-match node.
                     *path = *from->fPathData->fPath;
