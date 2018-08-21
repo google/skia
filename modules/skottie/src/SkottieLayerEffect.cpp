7c953f5feb759e50306965dc536236b831baeab2
Florin Malita <fmalita@chromium.org>
2018-08-21 14:34:02 -0400

    [skottie] Split-up Skottie.cpp
    Introduce more granular compilation units for major Skottie layer types.
    
    Change-Id: Iee2ef05cbcdda06467674824eb295ae6d7dc5bb9

---
+++ modules/skottie/src/SkottieLayerEffect.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkottiePriv.h"
+
+#include "SkJSON.h"
+#include "SkottieJson.h"
+#include "SkSGColor.h"
+#include "SkSGColorFilter.h"
+
+namespace skottie {
+namespace internal {
+
+namespace {
+
+sk_sp<sksg::RenderNode> AttachFillLayerEffect(const skjson::ArrayValue* jeffect_props,
+                                              AnimatorScope* ascope,
+                                              sk_sp<sksg::RenderNode> layer) {
+    if (!jeffect_props) return layer;
+
+    sk_sp<sksg::Color> color_node;
+
+    for (const skjson::ObjectValue* jprop : *jeffect_props) {
+        if (!jprop) continue;
+
+        switch (const auto ty = ParseDefault<int>((*jprop)["ty"], -1)) {
+        case 2: // color
+            color_node = AttachColor(*jprop, ascope, "v");
+            break;
+        default:
+            LOG("?? Ignoring unsupported fill effect poperty type: %d\n", ty);
+            break;
+        }
+    }
+
+    return color_node
+        ? sksg::ColorModeFilter::Make(std::move(layer), std::move(color_node), SkBlendMode::kSrcIn)
+        : nullptr;
+}
+
+} // namespace
+
+sk_sp<sksg::RenderNode> AnimationBuilder::attachLayerEffects(const skjson::ArrayValue& jeffects,
+                                                             AnimatorScope* ascope,
+                                                             sk_sp<sksg::RenderNode> layer) {
+    for (const skjson::ObjectValue* jeffect : jeffects) {
+        if (!jeffect) continue;
+
+        switch (const auto ty = ParseDefault<int>((*jeffect)["ty"], -1)) {
+        case 21: // Fill
+            layer = AttachFillLayerEffect((*jeffect)["ef"], ascope, std::move(layer));
+            break;
+        default:
+            LOG("?? Unsupported layer effect type: %d\n", ty);
+            break;
+        }
+    }
+
+    return layer;
+}
+
+} // namespace internal
+} // namespace skottie
---
+++ modules/skottie/src/SkottiePrecompLayer.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkottiePriv.h"
+
+#include "SkJSON.h"
+#include "SkottieAnimator.h"
+#include "SkottieJson.h"
+#include "SkottieValue.h"
+#include "SkMakeUnique.h"
+#include "SkSGRenderNode.h"
+#include "SkSGScene.h"
+#include "SkTLazy.h"
+
+namespace skottie {
+namespace internal {
+
+sk_sp<sksg::RenderNode> AnimationBuilder::attachPrecompLayer(const skjson::ObjectValue& jlayer,
+                                                             AnimatorScope* ascope) {
+    const skjson::ObjectValue* time_remap = jlayer["tm"];
+    const auto start_time = ParseDefault<float>(jlayer["st"], 0.0f),
+             stretch_time = ParseDefault<float>(jlayer["sr"], 1.0f);
+    const auto requires_time_mapping = !SkScalarNearlyEqual(start_time  , 0) ||
+                                       !SkScalarNearlyEqual(stretch_time, 1) ||
+                                       time_remap;
+
+    AnimatorScope local_animators;
+    auto precomp_layer = this->attachAssetRef(jlayer,
+                                              requires_time_mapping ? &local_animators : ascope,
+                                              &AnimationBuilder::attachComposition);
+
+    // Applies a bias/scale/remap t-adjustment to child animators.
+    class CompTimeMapper final : public sksg::GroupAnimator {
+    public:
+        CompTimeMapper(sksg::AnimatorList&& layer_animators, float time_bias, float time_scale)
+            : INHERITED(std::move(layer_animators))
+            , fTimeBias(time_bias)
+            , fTimeScale(time_scale) {}
+
+        void onTick(float t) override {
+            // When time remapping is active, |t| is driven externally.
+            if (fRemappedTime.isValid()) {
+                t = *fRemappedTime.get();
+            }
+
+            this->INHERITED::onTick((t + fTimeBias) * fTimeScale);
+        }
+
+        void remapTime(float t) { fRemappedTime.set(t); }
+
+    private:
+        const float    fTimeBias,
+                       fTimeScale;
+        SkTLazy<float> fRemappedTime;
+
+        using INHERITED = sksg::GroupAnimator;
+    };
+
+    if (requires_time_mapping) {
+        const auto t_bias  = -start_time,
+                   t_scale = sk_ieee_float_divide(1, stretch_time);
+        auto time_mapper = skstd::make_unique<CompTimeMapper>(std::move(local_animators),
+                                                              t_bias, t_scale);
+        if (time_remap) {
+            // The lambda below captures a raw pointer to the mapper object.  That should be safe,
+            // because both the lambda and the mapper are scoped/owned by ctx->fAnimators.
+            auto* raw_mapper = time_mapper.get();
+            auto  frame_rate = fFrameRate;
+            BindProperty<ScalarValue>(*time_remap, ascope,
+                    [raw_mapper, frame_rate](const ScalarValue& t) {
+                        raw_mapper->remapTime(t * frame_rate);
+                    });
+        }
+        ascope->push_back(std::move(time_mapper));
+    }
+
+    return precomp_layer;
+}
+
+} // namespace internal
+} // namespace skottie
