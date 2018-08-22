2876feb535c6a334570120c2346daf3538174f25
Leon Scroggins III <scroggo@google.com>
2018-08-22 16:46:32 -0400

    Revert "Switch SkCodec to use skcms" and follow on change
    This reverts commit 81886e8f9461c7049751c6a2c194a1df632dd362 and
    f8ae5ce20cf6df2dab4149fb2838d9d8dc6bd1af
    ("Fix CMYK handling in JPEG codec")
    
    This fixes the Android build, which was failing a CTS test with this
    change.
    
    Bug: skia:6839
    Bug: skia:8052
    
    TBR=djsollen@google.com
    As with the original, no API change
    
    Change-Id: Ic744a610e9f431707f871de44f9f64040bc60d14

--- src/codec/SkBmpCodec.h
+++ src/codec/SkBmpCodec.h
@@ -38,7 +38,7 @@ public:
 
 protected:
 
-    SkBmpCodec(SkEncodedInfo&& info, std::unique_ptr<SkStream>,
+    SkBmpCodec(int width, int height, const SkEncodedInfo& info, std::unique_ptr<SkStream>,
             uint16_t bitsPerPixel, SkCodec::SkScanlineOrder rowOrder);
 
     SkEncodedImageFormat onGetEncodedFormat() const override { return SkEncodedImageFormat::kBMP; }
@@ -103,7 +103,7 @@ protected:
      * than RGBA.
      */
     static constexpr SkColorType kXformSrcColorType = kBGRA_8888_SkColorType;
-    static constexpr auto kXformSrcColorFormat = skcms_PixelFormat_BGRA_8888;
+    static constexpr auto kXformSrcColorFormat = SkColorSpaceXform::kBGRA_8888_ColorFormat;
 
 private:
 
