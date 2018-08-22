789f9ca55ea77d0dcf4b64015e3127753878b0d2
Robert Phillips <robertphillips@google.com>
2018-08-22 15:57:55 -0400

    Add GPU-side caching of mask-filtered path masks
    Change-Id: If3085633665638849f3445d2eae5d2f187853cf4

--- src/core/SkMaskFilter.cpp
+++ src/core/SkMaskFilter.cpp
@@ -324,10 +324,10 @@ SkMaskFilterBase::onAsFragmentProcessor(const GrFPArgs&) const {
 bool SkMaskFilterBase::onHasFragmentProcessor() const { return false; }
 
 bool SkMaskFilterBase::canFilterMaskGPU(const GrShape& shape,
-                                        const SkRect& devSpaceShapeBounds,
+                                        const SkIRect& devSpaceShapeBounds,
                                         const SkIRect& clipBounds,
                                         const SkMatrix& ctm,
-                                        SkRect* maskRect) const {
+                                        SkIRect* maskRect) const {
     return false;
 }
 
