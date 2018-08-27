4e2b4ca318a9629c97d9493da328fd8b7a1dfbb7
Kevin Lubick <kjlubick@google.com>
2018-08-27 12:52:41 -0400

    Add docker-based test for lottie-web
    Goal is to get the output into Gold.
    
    Bug: skia:
    Change-Id: I100c9ce23dcc5033a27287211cbf0db898960da9

---
+++ infra/lottiecap/docker/lottiecap_gold.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Copyright 2018 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This assumes it is being run inside a docker container of gold-karma-chrome-tests
+# and a Skia checkout has been mounted at /SRC and the output directory
+# is mounted at /OUT
+
+# For example:
+# docker run -v $SKIA_ROOT:/SRC -v /tmp/dockerout:/OUT gcr.io/skia-public/gold-lottie-web-puppeteer:68.0.3440.106_v1 /SRC/infra/lottiecap/docker/test_pathkit.sh
+
+set -ex
+
+#BASE_DIR is the dir this script is in ($SKIA_ROOT/infra/pathkit/docker)
+BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
+LOTTIECAP_DIR=$BASE_DIR/../../../tools/lottiecap
+
+# Start the aggregator in the background
+/opt/gold-aggregator $@ &
+
+
+cd /SRC/tools/lottiecap/
+
+find /LOTTIE_FILES -not -path /LOTTIE_FILES -exec \
+    node /SRC/tools/lottiecap/lottiecap.js --port 8082 \
+            --lottie_player /usr/local/lib/node_modules/lottie-web/build/player/lottie.min.js \
+            --in_docker \
+            --post_to http://localhost:8081/report_gold_data \
+            --input {} \;
+
+
+# Tell the aggregator to dump the json
+# This curl command gets the HTTP code and stores it into $CODE
+CODE=`curl -s -o /dev/null -I -w "%{http_code}" -X POST localhost:8081/dump_json`
+if [ $CODE -ne 200 ]; then
+    # If we don't get 200 back, something is wrong with writing to disk, so exit with error
+    exit 1
+fi
