a49fcbdf309615f3ebbafe0f1a34cc76bd789d83
Ben Wagner <benjaminwagner@google.com>
2018-08-27 18:09:52 -0400

    [infra] Update Win Radeon drivers.
    No-Try: true
    Change-Id: I7b4418f8ee2bf94c5cdc1b22aed76e3448b17030

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -545,8 +545,8 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 					"IntelHD4400":   "8086:0a16-20.19.15.4963",
 					"IntelIris540":  "8086:1926-24.20.100.6229",
 					"IntelIris6100": "8086:162b-20.19.15.4963",
-					"RadeonHD7770":  "1002:683d-23.20.15033.5003",
-					"RadeonR9M470X": "1002:6646-23.20.15033.5003",
+					"RadeonHD7770":  "1002:683d-24.20.13001.1010",
+					"RadeonR9M470X": "1002:6646-24.20.13001.1010",
 					"QuadroP400":    "10de:1cb3-23.21.13.9103",
 				}[parts["cpu_or_gpu_value"]]
 				if !ok {
