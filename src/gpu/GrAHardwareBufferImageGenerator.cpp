f854c032038f11d1c2c95bf7dd5e0ad2c24ac770
Stan Iliev <stani@google.com>
2018-08-27 17:37:01 -0400

    Add support for hardware buffers with protected content
    Use EGL_EXT_protected_content extension if present to render
    AHardwareBuffer with AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT.
    
    Bug: skia:
    Change-Id: I1a5fa4cdc8f432b89ae9ef889175b50a25b846e0

--- src/gpu/GrAHardwareBufferImageGenerator.cpp
+++ src/gpu/GrAHardwareBufferImageGenerator.cpp
@@ -31,6 +31,29 @@
 #include <GLES/gl.h>
 #include <GLES/glext.h>
 
+#define PROT_CONTENT_EXT_STR "EGL_EXT_protected_content"
+#define EGL_PROTECTED_CONTENT_EXT 0x32C0
+
+static bool has_egl_protected_content_impl() {
+    EGLDisplay dpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    const char* exts = eglQueryString(dpy, EGL_EXTENSIONS);
+    size_t cropExtLen = strlen(PROT_CONTENT_EXT_STR);
+    size_t extsLen = strlen(exts);
+    bool equal = !strcmp(PROT_CONTENT_EXT_STR, exts);
+    bool atStart = !strncmp(PROT_CONTENT_EXT_STR " ", exts, cropExtLen+1);
+    bool atEnd = (cropExtLen+1) < extsLen
+                  && !strcmp(" " PROT_CONTENT_EXT_STR, exts + extsLen - (cropExtLen+1));
+    bool inMiddle = strstr(exts, " " PROT_CONTENT_EXT_STR " ");
+    return equal || atStart || atEnd || inMiddle;
+}
+
+static bool has_egl_protected_content() {
+    // Only compute whether the extension is present once the first time this
+    // function is called.
+    static bool hasIt = has_egl_protected_content_impl();
+    return hasIt;
+}
+
 std::unique_ptr<SkImageGenerator> GrAHardwareBufferImageGenerator::Make(
         AHardwareBuffer* graphicBuffer, SkAlphaType alphaType, sk_sp<SkColorSpace> colorSpace) {
     AHardwareBuffer_Desc bufferDesc;
@@ -51,14 +74,16 @@ std::unique_ptr<SkImageGenerator> GrAHardwareBufferImageGenerator::Make(
     }
     SkImageInfo info = SkImageInfo::Make(bufferDesc.width, bufferDesc.height, colorType,
                                          alphaType, std::move(colorSpace));
+    bool createProtectedImage = bufferDesc.usage & AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT;
     return std::unique_ptr<SkImageGenerator>(new GrAHardwareBufferImageGenerator(info, graphicBuffer,
-            alphaType));
+            alphaType, createProtectedImage));
 }
 
 GrAHardwareBufferImageGenerator::GrAHardwareBufferImageGenerator(const SkImageInfo& info,
-        AHardwareBuffer* hardwareBuffer, SkAlphaType alphaType)
+        AHardwareBuffer* hardwareBuffer, SkAlphaType alphaType, bool isProtectedContent)
     : INHERITED(info)
-    , fHardwareBuffer(hardwareBuffer) {
+    , fHardwareBuffer(hardwareBuffer)
+    , fIsProtectedContent(isProtectedContent) {
     AHardwareBuffer_acquire(fHardwareBuffer);
 }
 
@@ -153,11 +178,15 @@ static GrBackendTexture make_gl_backend_texture(
         GrContext* context, AHardwareBuffer* hardwareBuffer,
         int width, int height, GrPixelConfig config,
         GrAHardwareBufferImageGenerator::DeleteImageProc* deleteProc,
-        GrAHardwareBufferImageGenerator::DeleteImageCtx* deleteCtx) {
+        GrAHardwareBufferImageGenerator::DeleteImageCtx* deleteCtx,
+        bool isProtectedContent) {
     while (GL_NO_ERROR != glGetError()) {} //clear GL errors
 
-    EGLClientBuffer  clientBuffer = eglGetNativeClientBufferANDROID(hardwareBuffer);
+    bool createProtectedImage = isProtectedContent && has_egl_protected_content();
+    EGLClientBuffer clientBuffer = eglGetNativeClientBufferANDROID(hardwareBuffer);
     EGLint attribs[] = { EGL_IMAGE_PRESERVED_KHR, EGL_TRUE,
+                         createProtectedImage ? EGL_PROTECTED_CONTENT_EXT : EGL_NONE,
+                         createProtectedImage ? EGL_TRUE : EGL_NONE,
                          EGL_NONE };
     EGLDisplay display = eglGetCurrentDisplay();
     // eglCreateImageKHR will add a ref to the AHardwareBuffer
@@ -218,13 +247,14 @@ static GrBackendTexture make_backend_texture(
         GrContext* context, AHardwareBuffer* hardwareBuffer,
         int width, int height, GrPixelConfig config,
         GrAHardwareBufferImageGenerator::DeleteImageProc* deleteProc,
-        GrAHardwareBufferImageGenerator::DeleteImageCtx* deleteCtx) {
+        GrAHardwareBufferImageGenerator::DeleteImageCtx* deleteCtx,
+        bool isProtectedContent) {
     if (context->abandoned() || kOpenGL_GrBackend != context->contextPriv().getBackend()) {
         // Check if GrContext is not abandoned and the backend is GL.
         return GrBackendTexture();
     }
     return make_gl_backend_texture(context, hardwareBuffer, width, height, config, deleteProc,
-                                   deleteCtx);
+                                   deleteCtx, isProtectedContent);
 }
 
 static void free_backend_texture(GrBackendTexture* backendTexture) {
@@ -295,9 +325,11 @@ void GrAHardwareBufferImageGenerator::makeProxy(GrContext* context) {
     AHardwareBuffer_acquire(hardwareBuffer);
 
     GrTexture** ownedTexturePtr = &fOwnedTexture;
+    const bool isProtectedContent = fIsProtectedContent;
 
     fCachedProxy = proxyProvider->createLazyProxy(
-            [context, hardwareBuffer, width, height, pixelConfig, ownedTexturePtr]
+            [context, hardwareBuffer, width, height, pixelConfig, ownedTexturePtr,
+             isProtectedContent]
             (GrResourceProvider* resourceProvider) {
                 if (!resourceProvider) {
                     AHardwareBuffer_release(hardwareBuffer);
@@ -312,7 +344,8 @@ void GrAHardwareBufferImageGenerator::makeProxy(GrContext* context) {
                 GrBackendTexture backendTex = make_backend_texture(context, hardwareBuffer,
                                                                    width, height, pixelConfig,
                                                                    &deleteImageProc,
-                                                                   &deleteImageCtx);
+                                                                   &deleteImageCtx,
+                                                                   isProtectedContent);
                 if (!backendTex.isValid()) {
                     return sk_sp<GrTexture>();
                 }
