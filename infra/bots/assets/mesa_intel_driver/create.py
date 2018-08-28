5636d9c85f3dc94e0afac68784fd71967f23dc81
Kevin Lubick <kjlubick@google.com>
2018-08-28 16:42:14 -0400

    Add mesa_intel_driver asset and dockerfile for creating
    Bug: skia:
    Change-Id: Ia733783d703da803f76361c61b938d9ce70c88f0

---
+++ infra/bots/assets/mesa_intel_driver/create.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Create the asset."""
+
+
+import argparse
+import subprocess
+
+DOCKER_IMAGE="gcr.io/skia-public/mesa-driver-builder:latest"
+BUILD_SCRIPT="/opt/build_mesa.sh"
+MESA_VERSION="18.1.7"
+
+
+def create_asset(target_dir):
+  """Create the asset."""
+  cmd = [
+    'docker', 'run', '-v', '%s:/OUT' % target_dir,
+    '-e', 'MESA_VERSION=%s' % MESA_VERSION,
+    DOCKER_IMAGE, BUILD_SCRIPT
+  ]
+  print('Running docker cmd', cmd)
+  subprocess.check_output(cmd)
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--target_dir', '-t', required=True)
+  args = parser.parse_args()
+  create_asset(args.target_dir)
+
+
+if __name__ == '__main__':
+  main()
