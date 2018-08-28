1d628726e51605f285cbd28ee04886831dcadbee
Herb Derby <herb@google.com>
2018-08-28 15:33:19 -0400

    Managing the context was dropped. Adding back in.
    This was accidently removed in cl/149048
    
    BUG=chromium:877611
    
    Change-Id: I9608b45848c27fc79e0f26e40bb9a1c4deb50f5b

--- tests/SkRemoteGlyphCacheTest.cpp
+++ tests/SkRemoteGlyphCacheTest.cpp
@@ -5,6 +5,7 @@
  * found in the LICENSE file.
  */
 
+#include "Resources.h"
 #include "SkDraw.h"
 #include "SkGraphics.h"
 #include "SkMutex.h"
@@ -162,6 +163,36 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkRemoteGlyphCache_StrikeSerialization, repor
     SkBitmap actual = RasterBlob(clientBlob, 10, 10, paint, ctxInfo.grContext());
     COMPARE_BLOBS(expected, actual, reporter);
     REPORTER_ASSERT(reporter, !discardableManager->hasCacheMiss());
+
+    // Must unlock everything on termination, otherwise valgrind complains about memory leaks.
+    discardableManager->unlockAndDeleteAll();
+}
+
+DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkRemoteGlyphCache_ReleaseTypeFace, reporter, ctxInfo) {
+    sk_sp<DiscardableManager> discardableManager = sk_make_sp<DiscardableManager>();
+    SkStrikeServer server(discardableManager.get());
+    SkStrikeClient client(discardableManager, false);
+
+    // Server.
+    auto serverTf = MakeResourceAsTypeface("fonts/HangingS.ttf");
+    auto serverTfData = server.serializeTypeface(serverTf.get());
+    REPORTER_ASSERT(reporter, serverTf->unique());
+
+    {
+        const SkPaint paint;
+        int glyphCount = 10;
+        auto serverBlob = buildTextBlob(serverTf, glyphCount);
+        const SkSurfaceProps props(SkSurfaceProps::kLegacyFontHost_InitType);
+        SkTextBlobCacheDiffCanvas cache_diff_canvas(10, 10, SkMatrix::I(), props, &server,
+                                                    MakeSettings(ctxInfo.grContext()));
+        cache_diff_canvas.drawTextBlob(serverBlob.get(), 0, 0, paint);
+        REPORTER_ASSERT(reporter, !serverTf->unique());
+
+        std::vector<uint8_t> serverStrikeData;
+        server.writeStrikeData(&serverStrikeData);
+    }
+    REPORTER_ASSERT(reporter, serverTf->unique());
+
 
     // Must unlock everything on termination, otherwise valgrind complains about memory leaks.
     discardableManager->unlockAndDeleteAll();
