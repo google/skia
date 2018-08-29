fcc4664bf49e9f2764adb71ff8b60b0ed9fd3897
Robert Phillips <robertphillips@google.com>
2018-08-29 12:17:43 -0400

    Always force GPU generated masks be bottom left
    Bug: 878195
    Change-Id: Ie5e89753c2251ee1b4bd60ac2b02cedb7f5292f7

--- src/gpu/GrBlurUtils.cpp
+++ src/gpu/GrBlurUtils.cpp
@@ -80,7 +80,8 @@ static bool sw_draw_with_mask_filter(GrContext* context,
 
     if (key.isValid()) {
         // TODO: this cache look up is duplicated in draw_shape_with_mask_filter for gpu
-        filteredMask = proxyProvider->findOrCreateProxyByUniqueKey(key, kTopLeft_GrSurfaceOrigin);
+        filteredMask = proxyProvider->findOrCreateProxyByUniqueKey(key,
+                                                                   kTopLeft_GrSurfaceOrigin);
     }
 
     SkIRect drawRect;
@@ -156,6 +157,8 @@ static bool sw_draw_with_mask_filter(GrContext* context,
             return false;
         }
 
+        SkASSERT(kTopLeft_GrSurfaceOrigin == filteredMask->origin());
+
         drawRect = dstM.fBounds;
 
         if (key.isValid()) {
@@ -176,7 +179,7 @@ static sk_sp<GrTextureProxy> create_mask_GPU(GrContext* context,
     sk_sp<GrRenderTargetContext> rtContext(
         context->contextPriv().makeDeferredRenderTargetContextWithFallback(
             SkBackingFit::kApprox, maskRect.width(), maskRect.height(), kAlpha_8_GrPixelConfig,
-            nullptr, sampleCnt));
+            nullptr, sampleCnt, GrMipMapped::kNo, kTopLeft_GrSurfaceOrigin));
     if (!rtContext) {
         return nullptr;
     }
@@ -387,7 +390,7 @@ static void draw_shape_with_mask_filter(GrContext* context,
         if (maskKey.isValid()) {
             // TODO: this cache look up is duplicated in sw_draw_with_mask_filter for raster
             filteredMask = proxyProvider->findOrCreateProxyByUniqueKey(
-                                                maskKey, renderTargetContext->origin());
+                                                maskKey, kTopLeft_GrSurfaceOrigin);
         }
 
         if (!filteredMask) {
@@ -402,6 +405,8 @@ static void draw_shape_with_mask_filter(GrContext* context,
                                                          std::move(maskProxy),
                                                          viewMatrix,
                                                          maskRect);
+                SkASSERT(kTopLeft_GrSurfaceOrigin == filteredMask->origin());
+
                 if (filteredMask && maskKey.isValid()) {
                     proxyProvider->assignUniqueKeyToProxy(maskKey, filteredMask.get());
                 }
