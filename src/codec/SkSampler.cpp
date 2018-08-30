791cc9994feee49ed4b5bcd2261746863b1969ae
Nigel Tao <nigeltao@google.com>
2018-08-31 09:37:34 +1000

    Fix SkSampler::Fill/Gray_8 when width != stride
    Bug: skia:
    Change-Id: I68296bc9902c008d30a0ef1e748f22edd8c56255

--- src/codec/SkSampler.cpp
+++ src/codec/SkSampler.cpp
@@ -18,8 +18,6 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
         return;
     }
 
-    // Calculate bytes to fill.
-    const size_t bytesToFill = info.computeByteSize(rowBytes);
     const int width = info.width();
     const int numRows = info.height();
 
@@ -29,7 +27,7 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
         case kBGRA_8888_SkColorType: {
             uint32_t* dstRow = (uint32_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset32((uint32_t*) dstRow, 0, width);
+                sk_memset32(dstRow, 0, width);
                 dstRow = SkTAddOffset<uint32_t>(dstRow, rowBytes);
             }
             break;
@@ -37,18 +35,23 @@ void SkSampler::Fill(const SkImageInfo& info, void* dst, size_t rowBytes,
         case kRGB_565_SkColorType: {
             uint16_t* dstRow = (uint16_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset16((uint16_t*) dstRow, 0, width);
+                sk_memset16(dstRow, 0, width);
                 dstRow = SkTAddOffset<uint16_t>(dstRow, rowBytes);
             }
             break;
         }
-        case kGray_8_SkColorType:
-            memset(dst, 0, bytesToFill);
+        case kGray_8_SkColorType: {
+            uint8_t* dstRow = (uint8_t*) dst;
+            for (int row = 0; row < numRows; row++) {
+                memset(dstRow, 0, width);
+                dstRow = SkTAddOffset<uint8_t>(dstRow, rowBytes);
+            }
             break;
+        }
         case kRGBA_F16_SkColorType: {
             uint64_t* dstRow = (uint64_t*) dst;
             for (int row = 0; row < numRows; row++) {
-                sk_memset64((uint64_t*) dstRow, 0, width);
+                sk_memset64(dstRow, 0, width);
                 dstRow = SkTAddOffset<uint64_t>(dstRow, rowBytes);
             }
             break;
