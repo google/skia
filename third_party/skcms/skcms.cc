9951cbf4ca79a2a0dc8fda7baf503bca6f7d267a
skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com <skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com>
2018-08-31 16:40:38 +0000

    Roll skia/third_party/skcms a6687f8b4d51..80e0493eab5f (1 commits)
    https://skia.googlesource.com/skcms.git/+log/a6687f8b4d51..80e0493eab5f
    
    2018-08-31 mtklein@google.com Clang added pragma attribute in 5.0
    
    
    The AutoRoll server is located here: https://autoroll.skia.org/r/skcms-skia-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    TBR=stephana@google.com
    
    Change-Id: I937163750ced10a0408fd35038f86487bf5633d5
    Reviewed-on: https://skia-review.googlesource.com/151008
    Reviewed-by: skcms-skia-autoroll <skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com>
    Commit-Queue: skcms-skia-autoroll <skcms-skia-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com>

--- third_party/skcms/skcms.cc
+++ third_party/skcms/skcms.cc
@@ -1878,8 +1878,10 @@ namespace baseline {
 }
 
 // Now, instantiate any other versions of run_program() we may want for runtime detection.
-#if !defined(SKCMS_PORTABLE) && (defined(__clang__) || defined(__GNUC__)) \
-        && defined(__x86_64__) && !defined(__AVX2__)
+#if !defined(SKCMS_PORTABLE) &&                           \
+        (( defined(__clang__) && __clang_major__ >= 5) || \
+         (!defined(__clang__) && defined(__GNUC__)))      \
+     && defined(__x86_64__) && !defined(__AVX2__)
 
     #if defined(__clang__)
         #pragma clang attribute push(__attribute__((target("avx2,f16c"))), apply_to=function)
