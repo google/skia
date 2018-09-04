b086c5f10ecb6285c34b9db8e87c126089dc9d0d
Jim Van Verth <jvanverth@google.com>
2018-09-04 10:03:36 -0400

    Ignore desk_googlespreadsheet.skp_1.1 on Intel 540 bot
    Bug: skia:7322
    Change-Id: Ic096f4837fd245475a5966a1b5da403027cf429f

--- infra/bots/recipes/perf.py
+++ infra/bots/recipes/perf.py
@@ -192,6 +192,8 @@ def nanobench_flags(api, bot):
     # skia:7322
     match.append('~desk_carsvg.skp_1')
     match.append('~desk_tiger8svg.skp_1')
+    match.append('~desk_googlespreadsheet.skp_1_mpd')
+    match.append('~desk_googlespreadsheet.skp_1.1')
     match.append('~desk_wowwiki.skp')
     match.append('~keymobi_booking_com.skp_1')
     match.append('~keymobi_booking_com.skp_1_mpd')
