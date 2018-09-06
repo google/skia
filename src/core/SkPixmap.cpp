7874574996e1476d77acc869887a425a1710a9c0
Mike Klein <mtklein@google.com>
2018-09-06 11:25:21 -0400

    clamp after color pipeline when dst is normalized
    Most code working with unorm values doesn't expect to see out-of-gamut
    colors, but unorms _can_ store some out-of-gamut colors when
    premultiplied and alpha is <1.
    
    Clamping after the color (= shader + color filter) pipeline prevents us
    from creating those values.  This CL does appear to fix the problem we
    were seeing with xfermodes2 in the narrow config.
    
    I've kept scalepixels_unpremul working by using our old trick,
    remembering that unpremul->unpremul involves the same steps as
    premul->opaque.  We'll take the unpremul pixels, pun them to premul to
    avoid data loss, scale them up, and then avoid unpremul on the way out
    by punning the surface as opaque.  It all kind of just barely hangs
    together.
    
    Change-Id: I677d00ecfe4692ba7799ae963d8b8d1ccd4b00ac

--- src/core/SkPixmap.cpp
+++ src/core/SkPixmap.cpp
@@ -295,16 +295,18 @@ bool SkPixmap::scalePixels(const SkPixmap& actualDst, SkFilterQuality quality) c
         return src.readPixels(dst);
     }
 
-    // If src and dst are both unpremul, we'll fake them out to appear as if premul.
+    // If src and dst are both unpremul, we'll fake the source out to appear as if premul,
+    // and mark the destination as opaque.  This odd combination allows us to scale unpremul
+    // pixels without ever premultiplying them (perhaps losing information in the color channels).
+    // This is an idiosyncratic feature of scalePixels(), and is tested by scalepixels_unpremul GM.
     bool clampAsIfUnpremul = false;
     if (src.alphaType() == kUnpremul_SkAlphaType &&
         dst.alphaType() == kUnpremul_SkAlphaType) {
         src.reset(src.info().makeAlphaType(kPremul_SkAlphaType), src.addr(), src.rowBytes());
-        dst.reset(dst.info().makeAlphaType(kPremul_SkAlphaType), dst.addr(), dst.rowBytes());
+        dst.reset(dst.info().makeAlphaType(kOpaque_SkAlphaType), dst.addr(), dst.rowBytes());
 
-        // In turn, we'll need to tell the image shader to clamp to [0,1] instead
-        // of the usual [0,a] when using a bicubic scaling (kHigh_SkFilterQuality)
-        // or a gamut transformation.
+        // We'll need to tell the image shader to clamp to [0,1] instead of the
+        // usual [0,a] when using a bicubic scaling (kHigh_SkFilterQuality).
         clampAsIfUnpremul = true;
     }
 
