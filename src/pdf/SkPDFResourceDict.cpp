9486924d46f4dd49d5672585d6906b8495045817
Hal Canary <halcanary@google.com>
2018-09-12 09:12:36 -0400

    SkPDF: SkPDFMakeArray; clean up pdfutils.cpp
    New templated function SkPDFMakeArray that for example replaced this
    code:
    
        auto array = sk_make_sp<SkPDFArray>();
        array->reserve(4);
        array->appendInt(0);
        array->appendInt(0);
        array->appendInt(width);
        array->appendInt(height);
    
    with this code:
    
        auto array = SkPDFMakeArray(0, 0, width, height);
    
    Move some functions from SkPDFUtils to the only place they
    are used and make them static: SkPDFUtils::AppendTransform,
    SkPDFUtils::DrawFormXObject, SkPDFUtils::WriteString, and
    SkPDFUtils::AppendCubic.
    
    Change-Id: I891339fa2d1e5819f22fb8d10d8d8ef75c9507e3

--- src/pdf/SkPDFResourceDict.cpp
+++ src/pdf/SkPDFResourceDict.cpp
@@ -7,65 +7,63 @@
 
 #include "SkPDFResourceDict.h"
 #include "SkPDFTypes.h"
+#include "SkStream.h"
 
-// Sanity check that the values of enum SkPDFResourceType correspond to the
+// Sanity check that the values of enum ResourceType correspond to the
 // expected values as defined in the arrays below.
-// If these are failing, you may need to update the resource_type_prefixes
-// and resource_type_names arrays below.
-static_assert(SkPDFResourceDict::kExtGState_ResourceType == 0, "resource_type_mismatch");
-static_assert(SkPDFResourceDict::kPattern_ResourceType == 1, "resource_type_mismatch");
-static_assert(SkPDFResourceDict::kXObject_ResourceType == 2, "resource_type_mismatch");
-static_assert(SkPDFResourceDict::kFont_ResourceType == 3, "resource_type_mismatch");
+// If these are failing, you may need to update the kResourceTypePrefixes
+// and kResourceTypeNames arrays below.
+static_assert(0 == SkPDFResourceDict::kExtGState_ResourceType, "resource_type_mismatch");
+static_assert(1 == SkPDFResourceDict::kPattern_ResourceType,   "resource_type_mismatch");
+static_assert(2 == SkPDFResourceDict::kXObject_ResourceType,   "resource_type_mismatch");
+static_assert(3 == SkPDFResourceDict::kFont_ResourceType,      "resource_type_mismatch");
 
-static const char resource_type_prefixes[] = {
+constexpr size_t kMaxResourceNameLength = 1 + SkStrAppendS32_MaxSize;
+
+static char* get_resource_name(char dst[kMaxResourceNameLength],
+                               SkPDFResourceDict::ResourceType type, int key) {
+    static const char kResourceTypePrefixes[] = {
         'G',
         'P',
         'X',
         'F'
-};
-
-static const char* resource_type_names[] = {
-        "ExtGState",
-        "Pattern",
-        "XObject",
-        "Font"
-};
-
-char SkPDFResourceDict::GetResourceTypePrefix(
-        SkPDFResourceDict::SkPDFResourceType type) {
-    SkASSERT(type >= 0);
-    SkASSERT(type < SkPDFResourceDict::kResourceTypeCount);
-
-    return resource_type_prefixes[type];
+    };
+    SkASSERT((unsigned)type < SK_ARRAY_COUNT(kResourceTypePrefixes));
+    dst[0] = kResourceTypePrefixes[type];
+    return SkStrAppendS32(dst + 1, key);
 }
 
-static const char* get_resource_type_name(
-        SkPDFResourceDict::SkPDFResourceType type) {
-    SkASSERT(type >= 0);
-    SkASSERT(type < SK_ARRAY_COUNT(resource_type_names));
-
-    return resource_type_names[type];
-}
-
-SkString SkPDFResourceDict::getResourceName(
-        SkPDFResourceDict::SkPDFResourceType type, int key) {
-    return SkStringPrintf("%c%d", SkPDFResourceDict::GetResourceTypePrefix(type), key);
+void SkPDFResourceDict::WriteResourceName(
+        SkWStream* dst, SkPDFResourceDict::ResourceType type, int key) {
+    char buffer[1 + kMaxResourceNameLength];
+    buffer[0] = '/';
+    char* end = get_resource_name(buffer + 1, type, key);
+    dst->write(buffer, (size_t)(end - buffer));
 }
 
 template <typename T>
 static void add_subdict(
         std::vector<sk_sp<T>> resourceList,
-        SkPDFResourceDict::SkPDFResourceType type,
+        SkPDFResourceDict::ResourceType type,
         SkPDFDict* dst) {
     if (0 == resourceList.size()) {
         return;
     }
     auto resources = sk_make_sp<SkPDFDict>();
     for (size_t i = 0; i < resourceList.size(); i++) {
-        resources->insertObjRef(SkPDFResourceDict::getResourceName(type, SkToInt(i)),
+        char buffer[kMaxResourceNameLength];
+        char* end = get_resource_name(buffer, type, SkToInt(i));
+        resources->insertObjRef(SkString(buffer, (size_t)(end - buffer)),
                                 std::move(resourceList[i]));
     }
-    dst->insertObject(get_resource_type_name(type), std::move(resources));
+    static const char* kResourceTypeNames[] = {
+        "ExtGState",
+        "Pattern",
+        "XObject",
+        "Font"
+    };
+    SkASSERT((unsigned)type < SK_ARRAY_COUNT(kResourceTypeNames));
+    dst->insertObject(kResourceTypeNames[type], std::move(resources));
 }
 
 sk_sp<SkPDFDict> SkPDFResourceDict::Make(std::vector<sk_sp<SkPDFObject>> graphicStateResources,
