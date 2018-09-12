43322743af17aba4f3bb9c7526c3cb33b4160ef0
Mike Klein <mtklein@google.com>
2018-09-12 18:04:49 -0400

    add a linear gradient to P3 gm
    I've tweaked the indicators into overlays so there can be one for each
    pixel sampled.  This lets me test both gradient corners.
    
    I've dropped displaying the P3 value... not sure if it's as useful as
    the other two, and I'm worried about information overload.  There's
    already a lot to look at.
    
    Change-Id: I57a41c3af0162e8132e50c47a0b967714a57e9e5

--- gm/p3.cpp
+++ gm/p3.cpp
@@ -8,46 +8,58 @@
 #include "gm.h"
 #include "SkColorSpace.h"
 #include "SkColorSpaceXformSteps.h"
+#include "SkGradientShader.h"
 #include "SkString.h"
 
-static void mark_good(SkCanvas* canvas, SkScalar x = 300, SkScalar y = 40) {
-    canvas->saveLayer(nullptr, nullptr);
+template <typename Fn>
+static void mark(SkCanvas* canvas, Fn&& fn) {
+    SkPaint alpha;
+    alpha.setAlpha(0x50);
+    canvas->saveLayer(nullptr, &alpha);
+        canvas->translate(140,40);
+        canvas->scale(2,2);
+        fn();
+    canvas->restore();
+}
+
+static void mark_good(SkCanvas* canvas) {
+    mark(canvas, [&]{
         SkPaint paint;
 
         // A green circle.
         paint.setColor(SkColorSetRGB(27, 158, 119));
-        canvas->drawCircle(x,y, 12, paint);
+        canvas->drawCircle(0,0, 12, paint);
 
         // Cut out a check mark.
         paint.setBlendMode(SkBlendMode::kSrc);
         paint.setColor(0x00000000);
         paint.setStrokeWidth(2);
         paint.setStyle(SkPaint::kStroke_Style);
-        canvas->drawLine(x-6, y,
-                         x-1, y+5, paint);
-        canvas->drawLine(x-1, y+5,
-                         x+7, y-5, paint);
-    canvas->restore();
+        canvas->drawLine(-6, 0,
+                         -1, 5, paint);
+        canvas->drawLine(-1, +5,
+                         +7, -5, paint);
+    });
 }
 
-static void mark_bad(SkCanvas* canvas, SkScalar x = 300, SkScalar y = 40) {
-    canvas->saveLayer(nullptr, nullptr);
+static void mark_bad(SkCanvas* canvas) {
+    mark(canvas, [&] {
         SkPaint paint;
 
         // A red circle.
         paint.setColor(SkColorSetRGB(231, 41, 138));
-        canvas->drawCircle(x,y, 12, paint);
+        canvas->drawCircle(0,0, 12, paint);
 
         // Cut out an 'X'.
         paint.setBlendMode(SkBlendMode::kSrc);
         paint.setColor(0x00000000);
         paint.setStrokeWidth(2);
         paint.setStyle(SkPaint::kStroke_Style);
-        canvas->drawLine(x-5,y-5,
-                         x+5,y+5, paint);
-        canvas->drawLine(x+5,y-5,
-                         x-5,y+5, paint);
-    canvas->restore();
+        canvas->drawLine(-5,-5,
+                         +5,+5, paint);
+        canvas->drawLine(+5,-5,
+                         -5,+5, paint);
+    });
 }
 
 static bool nearly_equal(SkColor4f x, SkColor4f y) {
@@ -68,35 +80,34 @@ static SkColor4f transform(SkColor4f c, SkColorSpace* src, SkColorSpace* dst) {
     return c;
 }
 
-DEF_SIMPLE_GM(p3, canvas, 320, 240) {
-    auto p3 = SkColorSpace::MakeRGB(SkColorSpace::kSRGB_RenderTargetGamma,
-                                    SkColorSpace::kDCIP3_D65_Gamut);
-    auto native = canvas->imageInfo().refColorSpace();
-
+static void compare_pixel(const char* label,
+                          SkCanvas* canvas, int x, int y,
+                          SkColor4f color, SkColorSpace* cs) {
     SkPaint text;
     text.setAntiAlias(true);
+    auto canvas_cs = canvas->imageInfo().refColorSpace();
 
-    // Draw a P3 red rectangle.
-    SkPaint paint;
-    paint.setColor4f({1,0,0,1}, p3.get());
-    canvas->drawRect({10,10,70,70}, SkPaint{});
-    canvas->drawRect({10,10,70,70}, paint);
+    // I'm not really sure if this makes things easier or harder to follow,
+    // but we sniff the canvas to grab its current y-translate, so that (x,y)
+    // can be written in sort of chunk-relative terms.
+    const SkMatrix& m = canvas->getTotalMatrix();
+    SkASSERT(m.isTranslate());
+    SkScalar dy = m.getTranslateY();
+    SkASSERT(dy == (int)dy);
+    y += (int)dy;
 
-    // Read it back as floats in the color space of the canvas.
     SkBitmap bm;
-    bm.allocPixels(SkImageInfo::Make(60,60, kRGBA_F32_SkColorType, kUnpremul_SkAlphaType, native));
-    if (!canvas->readPixels(bm, 10,10)) {
-        canvas->drawString("can't readPixels() on this canvas :(", 100,20, text);
+    bm.allocPixels(SkImageInfo::Make(1,1, kRGBA_F32_SkColorType, kUnpremul_SkAlphaType, canvas_cs));
+    if (!canvas->readPixels(bm, x,y)) {
         mark_good(canvas);
+        canvas->drawString("can't readPixels() on this canvas :(", 100,20, text);
         return;
     }
 
-    // Let's look at one pixel and see if it matches the paint color.
     SkColor4f pixel;
-    memcpy(&pixel, bm.getAddr(10,10), sizeof(pixel));
+    memcpy(&pixel, bm.getAddr(0,0), sizeof(pixel));
 
-    // We should see pixel represent the paint color faithfully in the canvas' native colorspace.
-    SkColor4f expected = transform(paint.getColor4f(), nullptr, native.get());
+    SkColor4f expected = transform(color,cs, canvas_cs.get());
     if (canvas->imageInfo().colorType() < kRGBA_F16_SkColorType) {
         // We can't expect normalized formats to hold values outside [0,1].
         expected = expected.pin();
@@ -117,16 +128,61 @@ DEF_SIMPLE_GM(p3, canvas, 320, 240) {
         const char* label;
         SkColor4f   color;
     } lines[] = {
-        {"Native:"  , pixel                                   },
-        {"Expected:", expected                                },
-        {"P3:"      , transform(pixel, native.get(), p3.get())},
+        {"Pixel:"   , pixel   },
+        {"Expected:", expected},
     };
 
     SkAutoCanvasRestore saveRestore(canvas, true);
+    canvas->drawString(label, 80,20, text);
     for (auto l : lines) {
+        canvas->translate(0,20);
         canvas->drawString(l.label,               80,20, text);
         canvas->drawString(fmt(l.color).c_str(), 140,20, text);
-        canvas->translate(0,20);
+    }
+}
+
+DEF_SIMPLE_GM(p3, canvas, 450, 200) {
+    auto p3 = SkColorSpace::MakeRGB(SkColorSpace::kSRGB_RenderTargetGamma,
+                                    SkColorSpace::kDCIP3_D65_Gamut);
+
+    // Draw a P3 red rectangle and check the corner.
+    {
+        SkPaint paint;
+        paint.setColor4f({1,0,0,1}, p3.get());
+        canvas->drawRect({10,10,70,70}, SkPaint{});
+        canvas->drawRect({10,10,70,70}, paint);
+
+        compare_pixel("drawRect P3 red ",
+                      canvas, 10,10,
+                      {1,0,0,1}, p3.get());
+    }
+
+    canvas->translate(0,80);
+
+    // Draw a gradient from P3 red to P3 green, and check the corners.
+    {
+
+        SkPoint points[] = {{10.5,10.5}, {69.5,69.5}};
+        SkColor4f colors[] = {{1,0,0,1}, {0,1,0,1}};
+
+        SkPaint paint;
+        paint.setShader(SkGradientShader::MakeLinear(points, colors, p3,
+                                                     nullptr, SK_ARRAY_COUNT(colors),
+                                                     SkShader::kClamp_TileMode));
+        canvas->drawRect({10,10,70,70}, SkPaint{});
+        canvas->drawRect({10,10,70,70}, paint);
+
+        canvas->save();
+            compare_pixel("gradient P3 red",
+                          canvas, 10,10,
+                          {1,0,0,1}, p3.get());
+
+            canvas->translate(180, 0);
+
+            compare_pixel("gradient P3 green",
+                          canvas, 69,69,
+                          {0,1,0,1}, p3.get());
+        canvas->restore();
     }
 
     // TODO: draw P3 colors more ways
