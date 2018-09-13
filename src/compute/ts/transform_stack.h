ab8cd49cadd0aee8454e3b7fb816447a857c0b24
Allan MacKinnon <allanmac@google.com>
2018-09-13 15:00:16 -0700

    Partial support for projective transforms
    NOTREECHECKS=true
    NOTRY=true
    NOPRESUBMIT=true
    Bug: skia:
    Change-Id: I2879c1e30099fe78e8838f3e16a95e5d71f5400d

--- src/compute/ts/transform_stack.h
+++ src/compute/ts/transform_stack.h
@@ -16,156 +16,175 @@
 //
 //
 
-#include <skc/skc.h>
+#include <stdint.h>
+
+//
+//
+//
+
+struct ts_transform_stack;
 
 //
 //
 //
 
 #if 1
-typedef float  skc_transform_float_t;
-#define SKC_TRANSFORM_FLOAT_SUFFIX f
+typedef float  ts_transform_float_t;
+#define TS_TRANSFORM_FLOAT_SUFFIX f
 #else
-typedef double skc_transform_float_t;
-#define SKC_TRANSFORM_FLOAT_SUFFIX
+typedef double ts_transform_float_t;
+#define TS_TRANSFORM_FLOAT_SUFFIX
 #endif
 
 //
 //
 //
 
-struct skc_transform_stack;
+typedef uint64_t ts_transform_weakref_t;
+
+#define TS_TRANSFORM_WEAKREF_INVALID UINT64_MAX;
 
 //
 //
 //
 
-struct skc_transform_stack *
-skc_transform_stack_create(const uint32_t size);
+typedef enum ts_transform_type
+{
+  TS_TRANSFORM_TYPE_INVALID,
+  TS_TRANSFORM_TYPE_AFFINE,
+  TS_TRANSFORM_TYPE_PROJECTIVE
+} ts_transform_type_e;
+
+//
+//
+//
+
+struct ts_transform_stack *
+ts_transform_stack_create(const uint32_t size);
 
 void
-skc_transform_stack_release(struct skc_transform_stack * const ts);
+ts_transform_stack_release(struct ts_transform_stack * const ts);
 
 //
 //
 //
 
 uint32_t
-skc_transform_stack_save(struct skc_transform_stack * const ts);
+ts_transform_stack_save(struct ts_transform_stack * const ts);
 
 void
-skc_transform_stack_restore(struct skc_transform_stack * const ts, uint32_t const restore);
+ts_transform_stack_restore(struct ts_transform_stack * const ts, uint32_t const restore);
 
 //
 //
 //
 
-skc_transform_float_t *
-skc_transform_stack_top_transform(struct skc_transform_stack * const ts);
+ts_transform_float_t *
+ts_transform_stack_top_transform(struct ts_transform_stack * const ts);
 
-skc_transform_weakref_t *
-skc_transform_stack_top_weakref(struct skc_transform_stack * const ts);
-
-//
-//
-//
-
-void
-skc_transform_stack_drop(struct skc_transform_stack * const ts);
-
-void
-skc_transform_stack_dup(struct skc_transform_stack * const ts);
+ts_transform_weakref_t *
+ts_transform_stack_top_weakref(struct ts_transform_stack * const ts);
 
 //
 //
 //
 
 void
-skc_transform_stack_transform_affine(struct skc_transform_stack * const ts,
-                                     skc_transform_float_t        const x_pre,
-                                     skc_transform_float_t        const y_pre,
-                                     skc_transform_float_t      * const x_post,
-                                     skc_transform_float_t      * const y_post);
+ts_transform_stack_dup(struct ts_transform_stack * const ts);
+
+void
+ts_transform_stack_drop(struct ts_transform_stack * const ts);
 
 //
 //
 //
 
 void
-skc_transform_stack_push_matrix(struct skc_transform_stack * const ts,
-                                skc_transform_float_t        const sx,
-                                skc_transform_float_t        const shx,
-                                skc_transform_float_t        const tx,
-                                skc_transform_float_t        const shy,
-                                skc_transform_float_t        const sy,
-                                skc_transform_float_t        const ty,
-                                skc_transform_float_t        const w0,
-                                skc_transform_float_t        const w1,
-                                skc_transform_float_t        const w2);
+ts_transform_stack_transform_xy(struct ts_transform_stack * const ts,
+                                ts_transform_float_t        const x,
+                                ts_transform_float_t        const y,
+                                ts_transform_float_t      * const xp,
+                                ts_transform_float_t      * const yp);
 
-void
-skc_transform_stack_push_identity(struct skc_transform_stack * const ts);
-
-void
-skc_transform_stack_push_affine(struct skc_transform_stack * const ts,
-                                skc_transform_float_t        const sx,
-                                skc_transform_float_t        const shx,
-                                skc_transform_float_t        const tx,
-                                skc_transform_float_t        const shy,
-                                skc_transform_float_t        const sy,
-                                skc_transform_float_t        const ty);
-
-void
-skc_transform_stack_push_translate(struct skc_transform_stack * const ts,
-                                   skc_transform_float_t        const tx,
-                                   skc_transform_float_t        const ty);
-
-void
-skc_transform_stack_push_scale(struct skc_transform_stack * const ts,
-                               skc_transform_float_t        const sx,
-                               skc_transform_float_t        const sy);
-
-void
-skc_transform_stack_push_shear(struct skc_transform_stack * const ts,
-                               skc_transform_float_t        const shx,
-                               skc_transform_float_t        const shy);
-
-
-void
-skc_transform_stack_push_skew_x(struct skc_transform_stack * const ts,
-                                skc_transform_float_t        const theta);
-
-void
-skc_transform_stack_push_skew_y(struct skc_transform_stack * const ts,
-                                skc_transform_float_t        const theta);
-
-void
-skc_transform_stack_push_rotate(struct skc_transform_stack * const ts,
-                                skc_transform_float_t        const theta);
-
-void
-skc_transform_stack_push_rotate_xy2(struct skc_transform_stack * const ts,
-                                    skc_transform_float_t        const theta,
-                                    skc_transform_float_t        const cx,
-                                    skc_transform_float_t        const cy,
-                                    skc_transform_float_t        const tx,
-                                    skc_transform_float_t        const ty);
-
-void
-skc_transform_stack_push_rotate_xy(struct skc_transform_stack * const ts,
-                                   skc_transform_float_t        const theta,
-                                   skc_transform_float_t        const cx,
-                                   skc_transform_float_t        const cy);
-
-void
-skc_transform_stack_push_rotate_scale_xy(struct skc_transform_stack * const ts,
-                                         skc_transform_float_t        const theta,
-                                         skc_transform_float_t        const sx,
-                                         skc_transform_float_t        const sy,
-                                         skc_transform_float_t        const cx,
-                                         skc_transform_float_t        const cy);
 //
-// Quadrilateral coordinates are skc_transform_float_t2 structs:
+//
+//
+
+void
+ts_transform_stack_push_matrix(struct ts_transform_stack * const ts,
+                               ts_transform_float_t        const sx,
+                               ts_transform_float_t        const shx,
+                               ts_transform_float_t        const tx,
+                               ts_transform_float_t        const shy,
+                               ts_transform_float_t        const sy,
+                               ts_transform_float_t        const ty,
+                               ts_transform_float_t        const w0,
+                               ts_transform_float_t        const w1,
+                               ts_transform_float_t        const w2);
+
+void
+ts_transform_stack_push_identity(struct ts_transform_stack * const ts);
+
+void
+ts_transform_stack_push_affine(struct ts_transform_stack * const ts,
+                               ts_transform_float_t        const sx,
+                               ts_transform_float_t        const shx,
+                               ts_transform_float_t        const tx,
+                               ts_transform_float_t        const shy,
+                               ts_transform_float_t        const sy,
+                               ts_transform_float_t        const ty);
+
+void
+ts_transform_stack_push_translate(struct ts_transform_stack * const ts,
+                                  ts_transform_float_t        const tx,
+                                  ts_transform_float_t        const ty);
+
+void
+ts_transform_stack_push_scale(struct ts_transform_stack * const ts,
+                              ts_transform_float_t        const sx,
+                              ts_transform_float_t        const sy);
+
+void
+ts_transform_stack_push_shear(struct ts_transform_stack * const ts,
+                              ts_transform_float_t        const shx,
+                              ts_transform_float_t        const shy);
+
+
+void
+ts_transform_stack_push_skew_x(struct ts_transform_stack * const ts,
+                               ts_transform_float_t        const theta);
+
+void
+ts_transform_stack_push_skew_y(struct ts_transform_stack * const ts,
+                               ts_transform_float_t        const theta);
+
+void
+ts_transform_stack_push_rotate(struct ts_transform_stack * const ts,
+                               ts_transform_float_t        const theta);
+
+void
+ts_transform_stack_push_rotate_xy2(struct ts_transform_stack * const ts,
+                                   ts_transform_float_t        const theta,
+                                   ts_transform_float_t        const cx,
+                                   ts_transform_float_t        const cy,
+                                   ts_transform_float_t        const tx,
+                                   ts_transform_float_t        const ty);
+
+void
+ts_transform_stack_push_rotate_xy(struct ts_transform_stack * const ts,
+                                  ts_transform_float_t        const theta,
+                                  ts_transform_float_t        const cx,
+                                  ts_transform_float_t        const cy);
+
+void
+ts_transform_stack_push_rotate_scale_xy(struct ts_transform_stack * const ts,
+                                        ts_transform_float_t        const theta,
+                                        ts_transform_float_t        const sx,
+                                        ts_transform_float_t        const sy,
+                                        ts_transform_float_t        const cx,
+                                        ts_transform_float_t        const cy);
+//
+// Quadrilateral coordinates are ts_transform_float_t2 structs:
 //
 //   float2[4] = { xy0, xy1, xy2, xy3 }
 //
@@ -174,33 +193,62 @@ skc_transform_stack_push_rotate_scale_xy(struct skc_transform_stack * const ts,
 //   float[8]  = { x0, y0, x1, y1, x2, y2, x3, y3 };
 //
 
-bool
-skc_transform_stack_push_quad_to_unit(struct skc_transform_stack * const ts,
-                                      float const quad_src[8]);
+ts_transform_type_e
+ts_transform_stack_push_quad_to_unit(struct ts_transform_stack * const ts,
+                                     ts_transform_float_t        const quad[8]);
 
-bool
-skc_transform_stack_push_unit_to_quad(struct skc_transform_stack * const ts,
-                                      float const quad_dst[8]);
+ts_transform_type_e
+ts_transform_stack_push_unit_to_quad(struct ts_transform_stack * const ts,
+                                     ts_transform_float_t        const quad[8]);
 
-bool
-skc_transform_stack_push_rect_to_quad(struct skc_transform_stack * const ts,
-                                      float const x0,
-                                      float const y0,
-                                      float const x1,
-                                      float const y1,
-                                      float const quad_dst[8]);
+ts_transform_type_e
+ts_transform_stack_push_quad_to_quad(struct ts_transform_stack * const ts,
+                                     ts_transform_float_t        const quad_src[8],
+                                     ts_transform_float_t        const quad_dst[8]);
 
-bool
-skc_transform_stack_push_quad_to_quad(struct skc_transform_stack * const ts,
-                                      float const quad_src[8],
-                                      float const quad_dst[8]);
+ts_transform_type_e
+ts_transform_stack_push_rect_to_quad(struct ts_transform_stack * const ts,
+                                     ts_transform_float_t        const x0,
+                                     ts_transform_float_t        const y0,
+                                     ts_transform_float_t        const x1,
+                                     ts_transform_float_t        const y1,
+                                     ts_transform_float_t        const quad_dst[8]);
 
 //
+// The second matrix on the stack (TOS[-1]) is post-multiplied by the
+// top matrix on the stack (TOS[0]).
 //
+// The result replaces TOS[0] and TOS[-1] is unmodified.
+//
+// The stack effect of concat is:
+//
+//   | B |    | A*B |
+//   | A |    |  A  |
+//   | . | => |  .  |
+//   | . |    |  .  |
+//   | . |    |  .  |
 //
 
 void
-skc_transform_stack_concat(struct skc_transform_stack * const ts);
+ts_transform_stack_concat(struct ts_transform_stack * const ts);
+
+//
+// The second matrix on the stack (TOS[-1]) is post-multiplied by the
+// top matrix on the stack (TOS[0]).
+//
+// The result replaces both matrices.
+//
+// The stack effect of multiply is:
+//
+//   | B |    | A*B |
+//   | A |    |  .  |
+//   | . | => |  .  |
+//   | . |    |  .  |
+//   | . |    |  .  |
+//
+
+void
+ts_transform_stack_multiply(struct ts_transform_stack * const ts);
 
 //
 //
