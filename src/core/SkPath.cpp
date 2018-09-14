0bc5be69e862476c98d4a4b37b732ca59a1349ad
Robert Phillips <robertphillips@google.com>
2018-09-14 10:51:55 -0400

    Debug path
    Change-Id: Ic9fe6106b4a0693084b75f0544b672cea78be3cc

--- src/core/SkPath.cpp
+++ src/core/SkPath.cpp
@@ -1716,13 +1716,13 @@ SkPath& SkPath::reverseAddPath(const SkPath& srcPath) {
         src = tmp.set(srcPath);
     }
 
-    SkPathRef::Editor ed(&fPathRef, src->fPathRef->countPoints(), src->fPathRef->countVerbs());
+    SkPathRef::Editor ed(&fPathRef, src->countVerbs(), src->countPoints());
 
     const SkPoint* pts = src->fPathRef->pointsEnd();
-    // we will iterator through src's verbs backwards
+    // we will iterate through src's verbs backwards
     const uint8_t* verbs = src->fPathRef->verbsMemBegin(); // points at the last verb
     const uint8_t* verbsEnd = src->fPathRef->verbs(); // points just past the first verb
-    const SkScalar* conicWeights = src->fPathRef->conicWeightsEnd();
+    const SkScalar* conicWeights = src->fPathRef->conicWeights();
 
     bool needMove = true;
     bool needClose = false;
@@ -1840,6 +1840,7 @@ void SkPath::transform(const SkMatrix& matrix, SkPath* dst) const {
         SkPathRef::CreateTransformedCopy(&dst->fPathRef, *fPathRef.get(), matrix);
 
         if (this != dst) {
+          dst->fLastMoveToIndex = fLastMoveToIndex;
             dst->fFillType = fFillType;
             dst->fConvexity.store(fConvexity);
             dst->fIsVolatile = fIsVolatile;
