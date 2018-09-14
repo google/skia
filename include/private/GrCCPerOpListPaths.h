beb191de21f16618907b73242593def052dce3aa
Chris Dalton <csmartdalton@google.com>
2018-09-14 00:44:22 -0600

    ccpr: Remove the dangling pointer from GrCCDrawPathsOp
    Actually takes a ref on fOwningPerOpListPaths, instead of just hoping
    it will continue to exist. Removes unnecessary asserts surrounding
    dangling pointers.
    
    Bug: skia:8359
    Change-Id: Ie80da55510f320452bd9ee3a4b38bd59d48681a0

--- include/private/GrCCPerOpListPaths.h
+++ include/private/GrCCPerOpListPaths.h
@@ -21,11 +21,8 @@ class GrCCPerFlushResources;
  * Tracks all the CCPR paths in a given opList that will be drawn when it flushes.
  */
 // DDL TODO: given the usage pattern in DDL mode, this could probably be non-atomic refcounting.
-class GrCCPerOpListPaths : public SkRefCnt {
-public:
-    ~GrCCPerOpListPaths();
-
-    SkTInternalLList<GrCCDrawPathsOp> fDrawOps; // This class does not own these ops.
+struct GrCCPerOpListPaths : public SkRefCnt {
+    SkTInternalLList<GrCCDrawPathsOp> fDrawOps;  // This class does not own these ops.
     std::map<uint32_t, GrCCClipPath> fClipPaths;
     SkSTArenaAlloc<10 * 1024> fAllocator{10 * 1024 * 2};
     sk_sp<const GrCCPerFlushResources> fFlushResources;
