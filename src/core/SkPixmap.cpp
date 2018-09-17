581cf0dfc6b9ed83d6ede9a65dc6dd95546456b8
Mike Klein <mtklein@google.com>
2018-09-17 11:41:15 -0400

    erase _every_ row of F32 SkPixmaps
    We're doing a really thorough job of erasing the first row now.
    Yet another piece of code that should probably be a draw.
    
    Bug: oss-fuzz:9120
    
    Change-Id: Icd62c3ba3df127f0b89731ec004109c1b0144fc5
    Reviewed-on: https://skia-review.googlesource.com/154880
    Auto-Submit: Mike Klein <mtklein@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/core/SkPixmap.cpp
+++ src/core/SkPixmap.cpp
@@ -265,7 +265,7 @@ bool SkPixmap::erase(const SkColor4f& origColor, const SkIRect* subset) const {
     if (pm.colorType() == kRGBA_F32_SkColorType) {
         const SkPM4f rgba = color.premul();
         for (int y = 0; y < pm.height(); ++y) {
-            auto row = (float*)pm.writable_addr();
+            auto row = (float*)pm.writable_addr(0, y);
             for (int x = 0; x < pm.width(); ++x) {
                 row[4*x+0] = rgba.r();
                 row[4*x+1] = rgba.g();
