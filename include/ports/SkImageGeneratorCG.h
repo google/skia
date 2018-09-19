cc2f3a2f0f8b5628a7049660fc0d43234020ca7a
Ben Wagner <bungeman@google.com>
2018-09-19 16:52:10 -0400

    SkImageGeneratorCG.h to only expose factory method.
    Instead of requiring the implementation details of the SkImageGenerator
    which backs the CG image generator to be present in the header, move the
    class into the implementation and only expose the factory method.
    
    Change-Id: I728276ffcfb6bb4e7302280459a79210e8d67475

--- include/ports/SkImageGeneratorCG.h
+++ include/ports/SkImageGeneratorCG.h
@@ -8,34 +8,13 @@
 #include "SkTypes.h"
 #if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 
-#include "SkCGUtils.h"
 #include "SkData.h"
-#include "SkEncodedOrigin.h"
 #include "SkImageGenerator.h"
-#include "SkTemplates.h"
 
-class SK_API SkImageGeneratorCG : public SkImageGenerator {
-public:
-    static std::unique_ptr<SkImageGenerator> MakeFromEncodedCG(sk_sp<SkData>);
+#include <memory>
 
-protected:
-    sk_sp<SkData> onRefEncodedData() override;
-
-    bool onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes, const Options&)
-    override;
-
-private:
-    /*
-     * Takes ownership of the imageSrc
-     */
-    SkImageGeneratorCG(const SkImageInfo& info, const void* imageSrc, sk_sp<SkData> data,
-                       SkEncodedOrigin origin);
-
-    SkAutoTCallVProc<const void, CFRelease> fImageSrc;
-    sk_sp<SkData>                           fData;
-    const SkEncodedOrigin                   fOrigin;
-
-    typedef SkImageGenerator INHERITED;
-};
+namespace SkImageGeneratorCG {
+SK_API std::unique_ptr<SkImageGenerator> MakeFromEncodedCG(sk_sp<SkData>);
+}
 
 #endif //defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
