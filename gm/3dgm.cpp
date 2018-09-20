23d55d86e538acdf753a5541027d9ec6498aa51f
Florin Malita <fmalita@chromium.org>
2018-09-20 12:55:22 -0400

    [skottie] GM exercising the webfont loader
    Sets up a fake web font loader which serves a local/resource webfont to
    the sample text animation.
    
    Also rename resources/skotty/skotty_* -> resources/skottie/skottie_*.
    
    Change-Id: I4af5b24fc3cc5c63c78801979f9be56744047134

--- gm/3dgm.cpp
+++ gm/3dgm.cpp
@@ -43,7 +43,7 @@ public:
 
 protected:
     void onOnceBeforeDraw() override {
-        if (auto stream = GetResourceAsStream("skotty/skotty_sample_2.json")) {
+        if (auto stream = GetResourceAsStream("skottie/skottie_sample_2.json")) {
             fAnim = skottie::Animation::Make(stream.get());
         }
 
