36795fea9a1998aebb3059b6fed22552afd18cbc
Brian Osman <brianosman@google.com>
2018-09-20 16:56:00 -0400

    Work toward removing SkPM4f
    Added SkColor4f premul/unpremul that just return SkColor4f.
    Renamed existing premul to toPM4f. For many uses of SkPM4f,
    conversion to pure SkColor4f code was simple. In all other
    cases, continue to use SkPM4f for now.
    
    Bug: skia:
    Change-Id: I344c3290ee7af6bbe86c3ff74a2df2f5e87afa38

--- src/effects/SkLayerDrawLooper.cpp
+++ src/effects/SkLayerDrawLooper.cpp
@@ -55,8 +55,8 @@ static SkColor4f xferColor(const SkColor4f& src, const SkColor4f& dst, SkBlendMo
         case SkBlendMode::kDst:
             return dst;
         default: {
-            SkPM4f pmS = src.premul();
-            SkPM4f pmD = dst.premul();
+            SkColor4f pmS = src.premul();
+            SkColor4f pmD = dst.premul();
             return SkBlendMode_Apply(mode, pmS, pmD).unpremul();
         }
     }
