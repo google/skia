e1e52034bec03956bee9fcfe2970a9e9d230c58f
Robert Phillips <robertphillips@google.com>
2018-09-24 11:54:59 -0400

    Test YUV images in DDL
    Change-Id: Id0f2f84851dacc66c2c266a30cafa0b628b12eb4

--- tests/VkBackendSurfaceTest.cpp
+++ tests/VkBackendSurfaceTest.cpp
@@ -29,7 +29,7 @@ DEF_GPUTEST_FOR_VULKAN_CONTEXT(VkImageLayoutTest, reporter, ctxInfo) {
     GrVkGpu* gpu = static_cast<GrVkGpu*>(context->contextPriv().getGpu());
 
     GrBackendTexture backendTex = gpu->createTestingOnlyBackendTexture(nullptr, 1, 1,
-                                                                       kRGBA_8888_GrPixelConfig,
+                                                                       GrColorType::kRGBA_8888,
                                                                        false,
                                                                        GrMipMapped::kNo);
     REPORTER_ASSERT(reporter, backendTex.isValid());
