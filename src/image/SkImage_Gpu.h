79730acbfdc9833c071005f4d6a33d1d804ac23f
Robert Phillips <robertphillips@google.com>
2018-09-25 10:40:04 -0400

    Add SkImage_Gpu::MakePromiseYUVATexture
    This is pulled out of:
    
    https://skia-review.googlesource.com/c/skia/+/151983 (Test YUV images in DDL)
    
    Bug: skia:7903
    Change-Id: Ie6710439d4d922da8d4a41df83d12149c144fc81
    Reviewed-on: https://skia-review.googlesource.com/156761
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/image/SkImage_Gpu.h
+++ src/image/SkImage_Gpu.h
@@ -8,17 +8,16 @@
 #ifndef SkImage_Gpu_DEFINED
 #define SkImage_Gpu_DEFINED
 
-#include "GrClip.h"
 #include "GrContext.h"
 #include "GrGpuResourcePriv.h"
 #include "GrSurfaceProxyPriv.h"
-#include "SkBitmap.h"
 #include "SkGr.h"
 #include "SkImagePriv.h"
 #include "SkImage_Base.h"
-#include "SkSurface.h"
 
 class GrTexture;
+
+class SkBitmap;
 
 class SkImage_Gpu : public SkImage_Base {
 public:
@@ -122,11 +121,24 @@ public:
                                              PromiseDoneProc promiseDoneProc,
                                              TextureContext textureContext);
 
+    static sk_sp<SkImage> MakePromiseYUVATexture(GrContext* context,
+                                                 SkYUVColorSpace yuvColorSpace,
+                                                 const GrBackendFormat yuvaFormats[],
+                                                 const SkYUVAIndex yuvaIndices[4],
+                                                 int imageWidth,
+                                                 int imageHeight,
+                                                 GrSurfaceOrigin imageOrigin,
+                                                 sk_sp<SkColorSpace> imageColorSpace,
+                                                 TextureFulfillProc textureFulfillProc,
+                                                 TextureReleaseProc textureReleaseProc,
+                                                 PromiseDoneProc promiseDoneProc,
+                                                 TextureContext textureContexts[]);
+
     /** Implementation of MakeFromYUVTexturesCopy and MakeFromNV12TexturesCopy */
     static sk_sp<SkImage> MakeFromYUVATexturesCopyImpl(GrContext* ctx,
                                                        SkYUVColorSpace colorSpace,
                                                        const GrBackendTexture yuvaTextures[],
-                                                       SkYUVAIndex yuvaIndices[4],
+                                                       const SkYUVAIndex yuvaIndices[4],
                                                        SkISize size,
                                                        GrSurfaceOrigin origin,
                                                        sk_sp<SkColorSpace> imageColorSpace);
@@ -153,7 +165,7 @@ public:
 private:
     static sk_sp<SkImage> ConvertYUVATexturesToRGB(
             GrContext* ctx, SkYUVColorSpace colorSpace, const GrBackendTexture yuvaTextures[],
-            SkYUVAIndex yuvaIndices[4], SkISize size, GrSurfaceOrigin origin,
+            const SkYUVAIndex yuvaIndices[4], SkISize size, GrSurfaceOrigin origin,
             SkBudgeted isBudgeted, GrRenderTargetContext* renderTargetContext);
 
     sk_sp<GrContext>      fContext;
