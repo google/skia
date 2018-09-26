2393f21b63139179a44e87761e2b054ecf0f5643
Stephen White <senorblanco@chromium.org>
2018-09-26 15:27:18 -0400

    GrTessellator: fix for "pointy vertex" edge-AA artifact.
    The "pointy vertex" test was only considering the distance from the
    next (outgoing) edge to the previous point. It must also consider the
    distance from the previous (incoming) edge to the next point. If
    either are less than a quarter pixel, the vertex is considered pointy
    and should be removed.
    
    Bug: 884166
    Change-Id: I7f1d5b98e418d8f2a1c11643259d3cd74d08f286

---
+++ gm/crbug_887103.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm.h"
+#include "SkCanvas.h"
+#include "SkPath.h"
+
+DEF_SIMPLE_GM(crbug_887103, canvas, 500, 600) {
+    SkPaint paint;
+
+    paint.setAntiAlias(true);
+    paint.setStyle(SkPaint::kFill_Style);
+
+    SkPath path;
+    canvas->translate(0, 0);
+    path.moveTo(510,  20);
+    path.lineTo(500,  20);
+    path.lineTo(510, 500);
+
+    path.moveTo(500,  20);
+    path.lineTo(510, 500);
+    path.lineTo(500, 510);
+
+    path.moveTo(500,  30);
+    path.lineTo(510,  10);
+    path.lineTo( 10,  30);
+    canvas->drawPath(path, paint);
+}
