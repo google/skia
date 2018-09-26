5dc726f1f72880ce041243811e16cf751c044164
Mike Klein <mtklein@google.com>
2018-09-26 13:58:14 -0400

    bump NDK packages to r19-beta2
    Originally this started as a bump to r18, the latest stable release.
    That's why each packages goes up two versions, from r17 -> r18 ->
    r19-beta2.
    
    But our ASAN bots started failing with r18, and despite quite a bit of
    investigating, I've been unable to find anything wrong with our code.
    
    Luckily, r19 is coming soon, and it looks like its beta2 works fine.
    I think we should just skip r18.
    
    Original change's description:
    > bump NDK packages to r18
    >
    > Newer Clang, GCC is gone.
    >
    > Clang now supports half-float math on ARMv8, which is pretty neat.
    >
    > They've dropped support for everything below NDK version 16,
    > which happens to be what we target for 32-bit ARM.
    >
    > Change-Id: Idd1b1b557c5ecaabec4040026fd2ad5adfee5ee7
    > Reviewed-on: https://skia-review.googlesource.com/157260
    > Reviewed-by: Derek Sollenberger <djsollen@google.com>
    > Commit-Queue: Mike Klein <mtklein@google.com>
    
    Cq-Include-Trybots: skia.primary:Test-Android-Clang-Pixel-CPU-Snapdragon821-arm64-Release-All-Android_ASAN
    Change-Id: I0736558fc9b732113854b1b295642e1708b04581

--- infra/bots/recipe_modules/flavor/android.py
+++ infra/bots/recipe_modules/flavor/android.py
@@ -350,7 +350,7 @@ if actual_freq != str(freq):
     if 'ASAN' in self.m.vars.extra_tokens:
       asan_setup = self.m.vars.slave_dir.join(
             'android_ndk_linux', 'toolchains', 'llvm', 'prebuilt',
-            'linux-x86_64', 'lib64', 'clang', '6.0.2', 'bin',
+            'linux-x86_64', 'lib64', 'clang', '8.0.2', 'bin',
             'asan_device_setup')
       self.m.run(self.m.python.inline, 'Setting up device to run ASAN',
         program="""
