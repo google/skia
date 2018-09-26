2521129a2489daa28416f4fb21d6391a6a0f47fd
Hal Canary <halcanary@google.com>
2018-09-26 16:33:52 -0400

    SkPDF: Fix skia:8397, reduce RAM use, stop copying ClipStacks.
    5% reduction in RAM use for running all gms.
    
    97% reduction in RAM use for a particular test case:
        SkRandom rand;
        SkPaint paint;
        for (int i = 400000; i-- > 0;) {
            SkPoint p0 = {0,   (float)rand.nextRangeU(0, 792)};
            SkPoint p1 = {612, (float)rand.nextRangeU(0, 792)};
            canvas->drawLine(p0, p1, paint);
        }
    
    Bug: skia:8397
    Change-Id: Ieb86c0eabac45b120a97fe5c749fdb26d8a85267

--- src/pdf/SkPDFDevice.h
+++ src/pdf/SkPDFDevice.h
@@ -112,7 +112,7 @@ public:
 
     /** Returns a SkStream with the page contents.
      */
-    std::unique_ptr<SkStreamAsset> content() const;
+    std::unique_ptr<SkStreamAsset> content();
 
     SkPDFCanon* getCanon() const;
 
@@ -122,7 +122,7 @@ public:
     // later being our representation of an object in the PDF file.
     struct GraphicStateEntry {
         SkMatrix fMatrix = SkMatrix::I();
-        SkClipStack fClipStack;
+        uint32_t fClipStackGenID = SkClipStack::kWideOpenGenID;
         SkColor fColor = SK_ColorBLACK;
         SkScalar fTextScaleX = 1;  // Zero means we don't care what the value is.
         SkPaint::Style fTextFill = SkPaint::kFill_Style;  // Only if TextScaleX is non-zero.
@@ -168,12 +168,23 @@ private:
     std::vector<sk_sp<SkPDFObject>> fShaderResources;
     std::vector<sk_sp<SkPDFFont>> fFontResources;
 
-    struct ContentEntry {
-        GraphicStateEntry fState;
-        SkDynamicMemoryWStream fContent;
+    SkSinglyLinkedList<SkDynamicMemoryWStream> fContentEntries;
+    struct GraphicStackState {
+        GraphicStackState(SkDynamicMemoryWStream* s = nullptr) : fContentStream(s) {}
+        void updateClip(const SkClipStack* clipStack, const SkIRect& bounds);
+        void updateMatrix(const SkMatrix& matrix);
+        void updateDrawingState(const SkPDFDevice::GraphicStateEntry& state);
+        void push();
+        void pop();
+        void drainStack();
+        SkPDFDevice::GraphicStateEntry* currentEntry() { return &fEntries[fStackDepth]; }
+        // Conservative limit on save depth, see impl. notes in PDF 1.4 spec.
+        static const int kMaxStackDepth = 11;
+        SkPDFDevice::GraphicStateEntry fEntries[kMaxStackDepth + 1];
+        int fStackDepth = 0;
+        SkDynamicMemoryWStream* fContentStream;
     };
-    SkSinglyLinkedList<ContentEntry> fContentEntries;
-
+    GraphicStackState fActiveStackState;
     SkPDFDocument* fDocument;
 
     ////////////////////////////////////////////////////////////////////////////
@@ -192,12 +203,12 @@ private:
     // returns nullptr and does not create a content entry.
     // setUpContentEntry and finishContentEntry can be used directly, but
     // the preferred method is to use the ScopedContentEntry helper class.
-    ContentEntry* setUpContentEntry(const SkClipStack* clipStack,
+    SkDynamicMemoryWStream* setUpContentEntry(const SkClipStack* clipStack,
                                     const SkMatrix& matrix,
                                     const SkPaint& paint,
                                     bool hasText,
                                     sk_sp<SkPDFObject>* dst);
-    void finishContentEntry(SkBlendMode, sk_sp<SkPDFObject> dst, SkPath* shape);
+    void finishContentEntry(const SkClipStack*, SkBlendMode, sk_sp<SkPDFObject> dst, SkPath* shape);
     bool isContentEmpty();
 
     void populateGraphicStateEntryFromPaint(const SkMatrix& matrix,
