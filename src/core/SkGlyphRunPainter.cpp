5dee1802aed9495edaa7e6d2711e38deda80a822
Herb Derby <herb@google.com>
2018-09-27 13:31:00 -0400

    Have Renderer use combined path code
    Currently, the GPU code uses the unified code path
    in painter to draw. Have the remote glyph cache use it too.
    
    Change-Id: Ibb1728a9602dbb3ace3a57781c5e7a20cdbe3cdf
    Reviewed-on: https://skia-review.googlesource.com/157521
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/core/SkGlyphRunPainter.cpp
+++ src/core/SkGlyphRunPainter.cpp
@@ -595,10 +595,12 @@ void GrTextContext::regenerateGlyphRunList(GrTextBlob* cacheBlob,
             // Given a glyph that is not ARGB, draw it.
             auto perPath = [textScale, runIndex, cacheBlob, &pathCache]
                            (const SkGlyph& glyph, SkPoint position) {
-                const SkPath* path = pathCache->findPath(glyph);
-                if (path != nullptr) {
-                    cacheBlob->appendPathGlyph(
-                            runIndex, *path, position.fX, position.fY, textScale, false);
+                if (!glyph.isEmpty()) {
+                    const SkPath* path = pathCache->findPath(glyph);
+                    if (path != nullptr) {
+                        cacheBlob->appendPathGlyph(
+                                runIndex, *path, position.fX, position.fY, textScale, false);
+                    }
                 }
             };
 
