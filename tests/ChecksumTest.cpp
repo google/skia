24dbdf4dc37616500d8cac1d49509e1c4853127e
Mike Klein <mtklein@google.com>
2018-10-04 13:58:31 -0400

    tune SkOpts::hash_fn() quality
    We noticed a hash collision scenario we'd like to avoid, and it looks
    pretty easy.  The new test failed on my desktop at both asserts,
    depending on whether I build 32- or 64-bit, and also on my
    CRC-supporting phone.  The Murmur3 path should be fine.
    
    Change-Id: I005de38d68f9b0461536c29e48cf7e5be5caac3e

--- tests/ChecksumTest.cpp
+++ tests/ChecksumTest.cpp
@@ -43,3 +43,31 @@ DEF_TEST(GoodHash, r) {
     REPORTER_ASSERT(r, SkGoodHash()(( int32_t)4) ==  614249093);
     REPORTER_ASSERT(r, SkGoodHash()((uint32_t)4) ==  614249093);
 }
+
+DEF_TEST(ChecksumCollisions, r) {
+    // We noticed a few workloads that would cause hash collisions due to the way
+    // our optimized hashes split into three concurrent hashes and merge those hashes together.
+    //
+    // One of these two workloads ought to cause an unintentional hash collision on very similar
+    // data in those old algorithms, the float version on 32-bit x86 and double elsewhere.
+    {
+        float a[9] = { 0, 1, 2,
+                       3, 4, 5,
+                       6, 7, 8, };
+        float b[9] = { 1, 2, 0,
+                       4, 5, 3,
+                       7, 8, 6, };
+
+        REPORTER_ASSERT(r, SkOpts::hash(a, sizeof(a)) != SkOpts::hash(b, sizeof(b)));
+    }
+    {
+        double a[9] = { 0, 1, 2,
+                        3, 4, 5,
+                        6, 7, 8, };
+        double b[9] = { 1, 2, 0,
+                        4, 5, 3,
+                        7, 8, 6, };
+
+        REPORTER_ASSERT(r, SkOpts::hash(a, sizeof(a)) != SkOpts::hash(b, sizeof(b)));
+    }
+}
