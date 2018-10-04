9147421ad822bb9359e466b3e8165f03cae2cf95
Mike Klein <mtklein@google.com>
2018-10-04 11:15:17 -0400

    hash tf, and whole colorspace
    Change-Id: I088124ead162d369c5c9f8bdd3fc0a39b2b3de46

--- src/core/SkColorSpaceXformSteps.cpp
+++ src/core/SkColorSpaceXformSteps.cpp
@@ -24,7 +24,7 @@ SkColorSpaceXformSteps::SkColorSpaceXformSteps(SkColorSpace* src, SkAlphaType sr
     if (!src) { src = sk_srgb_singleton(); }
     if (!dst) { dst = src; }
 
-    if (src == dst && srcAT == dstAT) {
+    if (SkColorSpace::Equals(src,dst) && srcAT == dstAT) {
         return;
     }
 
@@ -55,9 +55,6 @@ SkColorSpaceXformSteps::SkColorSpaceXformSteps(SkColorSpace* src, SkAlphaType sr
     src->   transferFn(&this->srcTF   .fG);
     dst->invTransferFn(&this->dstTFInv.fG);
 
-    SkColorSpaceTransferFn dstTF;
-    dst->transferFn(&dstTF.fG);
-
     this->srcTF_is_sRGB = src->gammaCloseToSRGB();
     this->dstTF_is_sRGB = dst->gammaCloseToSRGB();
 
@@ -65,7 +62,7 @@ SkColorSpaceXformSteps::SkColorSpaceXformSteps(SkColorSpace* src, SkAlphaType sr
     if ( this->flags.linearize       &&
         !this->flags.gamut_transform &&
          this->flags.encode          &&
-        0 == memcmp(&srcTF, &dstTF, sizeof(SkColorSpaceTransferFn)))
+         src->transferFnHash() == dst->transferFnHash())
     {
         this->flags.linearize  = false;
         this->flags.encode     = false;
