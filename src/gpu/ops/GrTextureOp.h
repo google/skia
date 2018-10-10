fcd5065b4cf5cc06d9018b9f2f17672dba31c44a
Brian Salomon <bsalomon@google.com>
2018-10-10 10:47:04 -0400

    Add experimental API to draw a set of SkImages in one SkCanvas call.
    The client provides a src and dst rect for each image as well as
    a bitfield that indicates whether each edge of the image should be
    antialiased. This per-edge AA is useful for tiled compositors.
    
    Rather than take a full SkPaint this API only takes an alpha, a filter
    quality (which is pinned to kLow), and a blend mode. This is a likely
    point of future evolution.
    
    Currently the API is only fully implemented for kSrcOver on the GPU
    backend. With other blend modes or on other backends AA will be ignored
    for images that do not have all four edge AA flags set.
    
    Change-Id: I143998dda8ad6a25f64e18cd600392ba553030ac

--- src/gpu/ops/GrTextureOp.h
+++ src/gpu/ops/GrTextureOp.h
@@ -6,6 +6,7 @@
  */
 
 #include "GrColor.h"
+#include "GrRenderTargetContext.h"
 #include "GrSamplerState.h"
 #include "GrTypesPriv.h"
 #include "SkCanvas.h"
@@ -35,6 +36,16 @@ std::unique_ptr<GrDrawOp> Make(GrContext*,
                                GrAAType,
                                GrQuadAAFlags,
                                SkCanvas::SrcRectConstraint,
+                               const SkMatrix& viewMatrix,
+                               sk_sp<GrColorSpaceXform> textureXform,
+                               sk_sp<GrColorSpaceXform> paintXform);
+
+std::unique_ptr<GrDrawOp> Make(GrContext*,
+                               const GrRenderTargetContext::TextureSetEntry[],
+                               int cnt,
+                               GrSamplerState::Filter,
+                               GrColor,
+                               GrAAType,
                                const SkMatrix& viewMatrix,
                                sk_sp<GrColorSpaceXform> textureXform,
                                sk_sp<GrColorSpaceXform> paintXform);
