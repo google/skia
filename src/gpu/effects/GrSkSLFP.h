0c1d0c5a6efc621ba58f8f4ae8d46b538921b2fa
Ethan Nicholas <ethannicholas@google.com>
2018-10-11 10:32:40 -0400

    converted AARectEffect to new FP system
    Bug: skia:
    Change-Id: I0e4141c7f547bab92c65a6abff120ed04d5c2c66

--- src/gpu/effects/GrSkSLFP.h
+++ src/gpu/effects/GrSkSLFP.h
@@ -9,12 +9,17 @@
 #define GrSkSLFP_DEFINED
 
 #include "GrCaps.h"
-#include "GrFragmentProcessor.h"
+#include "GrContext.h"
+#include "GrContextPriv.h"
 #include "GrCoordTransform.h"
+#include "GrFragmentProcessor.h"
 #include "GrShaderCaps.h"
 #include "SkSLCompiler.h"
 #include "SkSLPipelineStageCodeGenerator.h"
 #include "SkRefCnt.h"
+#include "glsl/GrGLSLFragmentProcessor.h"
+#include "glsl/GrGLSLProgramDataManager.h"
+#include "glsl/GrGLSLUniformHandler.h"
 #include "../private/GrSkSLFPFactoryCache.h"
 
 #if GR_TEST_UTILS
@@ -25,9 +30,16 @@
 
 class GrContext;
 class GrSkSLFPFactory;
+class GrGLSLSkSLFP;
 
 class GrSkSLFP : public GrFragmentProcessor {
 public:
+    using UniformHandle = GrGLSLUniformHandler::UniformHandle;
+
+    template<typename Inputs, typename ExtraData>
+    using DataHookFn = void(const GrGLSLProgramDataManager& pdman, const GrGLSLSkSLFP& glslProc,
+                            const Inputs& inputs, ExtraData* extraData);
+
     /**
      * Returns a new unique identifier. Each different SkSL fragment processor should call
      * NewIndex once, statically, and use this index for all calls to Make.
@@ -66,23 +78,72 @@ public:
      * 'NewIndex()'. Each given SkSL string should have a single, statically defined index
      * associated with it.
      */
-    static std::unique_ptr<GrSkSLFP> Make(
-                   GrContext* context,
-                   int index,
-                   const char* name,
-                   const char* sksl,
-                   const void* inputs,
-                   size_t inputSize);
+    template<typename Inputs>
+    static std::unique_ptr<GrSkSLFP> Make(GrContext* context, int index, const char* name,
+                                          const char* sksl, const Inputs& inputs) {
+        void* resultBytes = GrSkSLFP::operator new(sizeof(GrSkSLFP) + sizeof(Inputs));
+        void* inputBytes = (int8_t*) resultBytes + sizeof(GrSkSLFP);
+        memcpy(inputBytes, &inputs, sizeof(Inputs));
+        GrSkSLFP* result = new (resultBytes) GrSkSLFP(context->contextPriv().getFPFactoryCache(),
+                                                      context->contextPriv().caps()->shaderCaps(),
+                                                      index, name, sksl, inputBytes,
+                                                      sizeof(Inputs), nullptr, 0, nullptr);
+        return std::unique_ptr<GrSkSLFP>(result);
+    }
+
+    /**
+     * As the above overload of Make, but also tracks an additional extraData struct which is shared
+     * between all instances of the FP. The extraData struct is copied into the GrGLSLSkSLFP and is
+     * passed as an argument to the setDataHook function. setDataHook is called after the automatic
+     * handling of 'in uniform' variables is completed and is responsible for providing values for
+     * all non-'in' uniform variables.
+     */
+    template<typename Inputs, typename ExtraData>
+    static std::unique_ptr<GrSkSLFP> Make(GrContext* context, int index, const char* name,
+                                          const char* sksl, const Inputs& inputs,
+                                          const ExtraData& extraData,
+                                          DataHookFn<Inputs, ExtraData>* setDataHook) {
+        void* resultBytes = GrSkSLFP::operator new(sizeof(GrSkSLFP) + sizeof(Inputs) +
+                                                   sizeof(ExtraData));
+        void* inputBytes = (int8_t*) resultBytes + sizeof(GrSkSLFP);
+        memcpy(inputBytes, &inputs, sizeof(Inputs));
+        void* extraDataBytes = (int8_t*) resultBytes + sizeof(GrSkSLFP) + sizeof(Inputs);
+        memcpy(extraDataBytes, &extraData, sizeof(ExtraData));
+        auto genericFn = [setDataHook] (const GrGLSLProgramDataManager& pdman,
+                                        const GrGLSLSkSLFP& glslProc,
+                                        const void* inputs,
+                                        void* extraData) {
+            (*setDataHook)(pdman, glslProc, *reinterpret_cast<const Inputs*>(inputs),
+                           reinterpret_cast<ExtraData*>(extraData));
+        };
+        return std::unique_ptr<GrSkSLFP>(new (resultBytes) GrSkSLFP(
+                                                        context->contextPriv().getFPFactoryCache(),
+                                                        context->contextPriv().caps()->shaderCaps(),
+                                                        index, name, sksl, inputBytes,
+                                                        sizeof(Inputs), extraDataBytes,
+                                                        sizeof(ExtraData), genericFn));
+    }
 
     const char* name() const override;
 
     void addChild(std::unique_ptr<GrFragmentProcessor> child);
 
+    void setSetDataHook(void (*hook)(const GrGLSLProgramDataManager& pdman,
+                                     const GrGLSLSkSLFP& glslProc,
+                                     const GrSkSLFP& proc));
+
+    const void* inputs() const { return fInputs; }
+
     std::unique_ptr<GrFragmentProcessor> clone() const override;
 
 private:
+    using UntypedDataHookFn = std::function<void(const GrGLSLProgramDataManager&,
+                                                 const GrGLSLSkSLFP&,
+                                                 const void*,
+                                                 void*)>;
     GrSkSLFP(sk_sp<GrSkSLFPFactoryCache> factoryCache, const GrShaderCaps* shaderCaps, int fIndex,
-             const char* name, const char* sksl, const void* inputs, size_t inputSize);
+             const char* name, const char* sksl, const void* inputs, size_t inputSize,
+             const void* extraData, size_t extraDataSize, UntypedDataHookFn setDataHook);
 
     GrSkSLFP(const GrSkSLFP& other);
 
@@ -106,9 +167,15 @@ private:
 
     const char* fSkSL;
 
-    const std::unique_ptr<int8_t[]> fInputs;
+    const void* fInputs;
 
     size_t fInputSize;
+
+    const void* fExtraData;
+
+    size_t fExtraDataSize;
+
+    UntypedDataHookFn fSetDataHook;
 
     mutable SkSL::String fKey;
 
@@ -119,6 +186,41 @@ private:
     friend class GrGLSLSkSLFP;
 
     friend class GrSkSLFPFactory;
+};
+
+class GrGLSLSkSLFP : public GrGLSLFragmentProcessor {
+public:
+    GrGLSLSkSLFP(SkSL::Context* context, const std::vector<const SkSL::Variable*>* uniformVars,
+                 SkSL::String glsl, std::vector<SkSL::Compiler::FormatArg> formatArgs,
+                 void* extraData);
+
+    GrSLType uniformType(const SkSL::Type& type);
+
+    /**
+     * Returns the extraData pointer.
+     */
+    void* extraData() const;
+
+    void emitCode(EmitArgs& args) override;
+
+    void onSetData(const GrGLSLProgramDataManager& pdman,
+                   const GrFragmentProcessor& _proc) override;
+
+    UniformHandle uniformHandle(int index) const { return fUniformHandles[index]; }
+
+private:
+    const SkSL::Context& fContext;
+
+    const std::vector<const SkSL::Variable*>& fUniformVars;
+
+    // nearly-finished GLSL; still contains printf-style "%s" format tokens
+    const SkSL::String fGLSL;
+
+    std::vector<SkSL::Compiler::FormatArg> fFormatArgs;
+
+    std::vector<UniformHandle> fUniformHandles;
+
+    void* fExtraData;
 };
 
 /**
@@ -146,6 +248,8 @@ public:
     std::shared_ptr<SkSL::Program> fBaseProgram;
 
     std::vector<const SkSL::Variable*> fInputVars;
+
+    std::vector<const SkSL::Variable*> fUniformVars;
 
     std::vector<const SkSL::Variable*> fKeyVars;
 
