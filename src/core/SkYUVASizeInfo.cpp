3017bcecca2fbe3c0f1d015e20d0dea856b8dd92
Robert Phillips <robertphillips@google.com>
2018-10-12 14:48:19 -0400

    Widen internal API to support more complex YUV formats
    Bug: skia:7901
    Change-Id: I7cd8c5c15a080f00c1657804ee817389b4020450

---
+++ src/core/SkYUVASizeInfo.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkYUVASizeInfo.h"
+#include "SkTemplates.h"
+
+void SkYUVASizeInfo::computePlanes(void* base, void* planes[4]) const {
+    planes[0] = base;
+    for (int i = 1; i < 4; ++i) {
+        planes[i] = SkTAddOffset<void>(planes[i-1], fWidthBytes[i-1] * fSizes[i-1].height());
+    }
+}
