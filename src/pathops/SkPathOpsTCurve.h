5fdeb0e443ee7da567234e3a2b1a125edce3a6c6
Cary Clark <caryclark@skia.org>
2018-10-15 10:36:51 -0400

    remove pathop template
    Pathops used templates for curve intersection.
    Since only one template is required if curves share
    an abstract base, remove the template altogether.
    
    This makes the code easier to read, but
    will do little to make it smaller or faster.
    
    TBR=reed@google.com
    
    Bug: skia:
    Change-Id: I00c08210e47efed83295276ae89ad64e7ec07ade

--- src/pathops/SkPathOpsTCurve.h
+++ src/pathops/SkPathOpsTCurve.h
@@ -22,6 +22,9 @@ public:
     virtual bool collapsed() const = 0;
     virtual bool controlsInside() const = 0;
     virtual void debugInit() = 0;
+#if DEBUG_T_SECT
+    virtual void dumpID(int id) const = 0;
+#endif
     virtual SkDVector dxdyAtT(double t) const = 0;
     virtual bool hullIntersects(const SkDQuad& , bool* isLinear) const = 0;
     virtual bool hullIntersects(const SkDConic& , bool* isLinear) const = 0;
