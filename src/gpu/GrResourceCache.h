4e6897e0384cefd5483838e88488e56695803d1a
Brian Salomon <bsalomon@google.com>
2018-10-15 14:32:15 -0400

    Add single thread assertions to GrResourceCache functions called by GrGpuResource
    Resources can call into GrResourceCache on via call stacks that don't include
    GrContext.
    
    Change-Id: Ibbea5615e2524e4bc99106cbaeba8fcc7e624215

--- src/gpu/GrResourceCache.h
+++ src/gpu/GrResourceCache.h
@@ -23,6 +23,7 @@ class GrCaps;
 class GrProxyProvider;
 class SkString;
 class SkTraceMemoryDump;
+class GrSingleOwner;
 
 struct GrGpuResourceFreedMessage {
     GrGpuResource* fResource;
@@ -52,7 +53,7 @@ struct GrGpuResourceFreedMessage {
  */
 class GrResourceCache {
 public:
-    GrResourceCache(const GrCaps*, uint32_t contextUniqueID);
+    GrResourceCache(const GrCaps*, GrSingleOwner* owner, uint32_t contextUniqueID);
     ~GrResourceCache();
 
     // Default maximum number of budgeted resources in the cache.
@@ -352,6 +353,7 @@ private:
     SkTDArray<GrGpuResource*>           fResourcesWaitingForFreeMsg;
 
     uint32_t                            fContextUniqueID;
+    GrSingleOwner*                      fSingleOwner;
 
     // This resource is allowed to be in the nonpurgeable array for the sake of validate() because
     // we're in the midst of converting it to purgeable status.
