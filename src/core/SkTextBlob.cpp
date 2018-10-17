da579e2f81072b8a8c82a553bb5e73c8ccaa7f6c
Hal Canary <halcanary@google.com>
2018-10-17 17:06:16 -0400

    SkRunFont in noncopyable
    Change-Id: Ic52a9fc56c992c799ae8fb791a74340885b0a2d1

--- src/core/SkTextBlob.cpp
+++ src/core/SkTextBlob.cpp
@@ -23,9 +23,9 @@
 #endif
 
 SkRunFont::SkRunFont(const SkPaint& paint)
-        : fSize(paint.getTextSize())
+        : fTypeface(SkPaintPriv::RefTypefaceOrDefault(paint))
+        , fSize(paint.getTextSize())
         , fScaleX(paint.getTextScaleX())
-        , fTypeface(SkPaintPriv::RefTypefaceOrDefault(paint))
         , fSkewX(paint.getTextSkewX())
         , fAlign(paint.getTextAlign())
         , fHinting(paint.getHinting())
@@ -434,7 +434,7 @@ bool SkTextBlobBuilder::mergeRun(const SkPaint &font, SkTextBlob::GlyphPositioni
     }
 
     if (run->positioning() != positioning
-        || run->font() != font
+        || run->font() != SkRunFont(font)
         || (run->glyphCount() + count < run->glyphCount())) {
         return false;
     }
