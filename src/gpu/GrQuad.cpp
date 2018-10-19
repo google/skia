66ae619ce9ced100351571bb55475b3d47643745
Michael Ludwig <michaelludwig@google.com>
2018-10-19 09:50:36 -0400

    Consolidate aa resolution for per-edge aa quads
    Bug: skia:
    Change-Id: I5c8f0c6bf3178c2fd623eb3d9779a9382ca40747

--- src/gpu/GrQuad.cpp
+++ src/gpu/GrQuad.cpp
@@ -7,6 +7,8 @@
 
 #include "GrQuad.h"
 
+#include "GrTypesPriv.h"
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Functions for identifying the quad type from its coordinates, which are kept debug-only since
 // production code should rely on the matrix to derive the quad type more efficiently. These are
@@ -49,6 +51,49 @@ GrQuadType GrPerspQuad::quadType() const {
 static bool aa_affects_rect(float ql, float qt, float qr, float qb) {
     return !SkScalarIsInt(ql) || !SkScalarIsInt(qr) || !SkScalarIsInt(qt) || !SkScalarIsInt(qb);
 }
+
+template <typename Q>
+void GrResolveAATypeForQuad(GrAAType requestedAAType, GrQuadAAFlags requestedEdgeFlags,
+                            const Q& quad, GrQuadType knownType,
+                            GrAAType* outAAType, GrQuadAAFlags* outEdgeFlags) {
+    // Most cases will keep the requested types unchanged
+    *outAAType = requestedAAType;
+    *outEdgeFlags = requestedEdgeFlags;
+
+    switch (requestedAAType) {
+        // When aa type is coverage, disable AA if the edge configuration doesn't actually need it
+        case GrAAType::kCoverage:
+            if (requestedEdgeFlags == GrQuadAAFlags::kNone) {
+                // Turn off anti-aliasing
+                *outAAType = GrAAType::kNone;
+            } else {
+                // For coverage AA, if the quad is a rect and it lines up with pixel boundaries
+                // then overall aa and per-edge aa can be completely disabled
+                if (knownType == GrQuadType::kRect_QuadType && !quad.aaHasEffectOnRect()) {
+                    *outAAType = GrAAType::kNone;
+                    *outEdgeFlags = GrQuadAAFlags::kNone;
+                }
+            }
+            break;
+        // For no or msaa anti aliasing, override the edge flags since edge flags only make sense
+        // when coverage aa is being used.
+        case GrAAType::kNone:
+            *outEdgeFlags = GrQuadAAFlags::kNone;
+            break;
+        case GrAAType::kMSAA:
+            *outEdgeFlags = GrQuadAAFlags::kAll;
+            break;
+        case GrAAType::kMixedSamples:
+            SK_ABORT("Should not use mixed sample AA with edge AA flags");
+            break;
+    }
+};
+
+// Instantiate GrResolve... for GrQuad and GrPerspQuad
+template void GrResolveAATypeForQuad(GrAAType, GrQuadAAFlags, const GrQuad&, GrQuadType,
+                                     GrAAType*, GrQuadAAFlags*);
+template void GrResolveAATypeForQuad(GrAAType, GrQuadAAFlags, const GrPerspQuad&, GrQuadType,
+                                     GrAAType*, GrQuadAAFlags*);
 
 GrQuadType GrQuadTypeForTransformedRect(const SkMatrix& matrix) {
     if (matrix.rectStaysRect()) {
