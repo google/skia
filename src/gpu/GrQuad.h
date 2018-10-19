29fc73e51c09239ea7f87cd4048628504c91d4f8
Michael Ludwig <michaelludwig@google.com>
2018-10-19 09:36:57 -0400

    Add quad type and persp/aa utilities to GrQuad
    This refactor makes some of the quad logic in GrTextureOp available for
    use with other quad GrOps.
    
    Bug: skia:
    Change-Id: I1c173cfdf61b33c8422ddd8b91406a970a1c8e5d

--- src/gpu/GrQuad.h
+++ src/gpu/GrQuad.h
@@ -13,6 +13,24 @@
 #include "SkPoint.h"
 #include "SkPoint3.h"
 
+// Rectangles transformed by matrices (view or local) can be classified in three ways:
+//  1. Stays a rectangle - the matrix rectStaysRect() is true, or x(0) == x(1) && x(2) == x(3)
+//     and y(0) == y(2) && y(1) == y(3). Or under mirrors, x(0) == x(2) && x(1) == x(3) and
+//     y(0) == y(1) && y(2) == y(3).
+//  2. Is a quadrilateral - the matrix does not have perspective, but may rotate or skew, or
+//     ws() == all ones.
+//  3. Is a perspective quad - the matrix has perspective, subsuming all previous quad types.
+enum class GrQuadType {
+    kRect_QuadType,
+    kStandard_QuadType,
+    kPerspective_QuadType
+};
+
+// If an SkRect is transformed by this matrix, what class of quad is required to represent it. Since
+// quadType() is only provided on Gr[Persp]Quad in debug builds, production code should use this
+// to efficiently determine quad types.
+GrQuadType GrQuadTypeForTransformedRect(const SkMatrix& matrix);
+
 /**
  * GrQuad is a collection of 4 points which can be used to represent an arbitrary quadrilateral. The
  * points make a triangle strip with CCW triangles (top-left, bottom-left, top-right, bottom-right).
@@ -49,6 +67,13 @@ public:
     Sk4f x4f() const { return Sk4f::Load(fX); }
     Sk4f y4f() const { return Sk4f::Load(fY); }
 
+    // True if anti-aliasing affects this quad. Requires quadType() == kRect_QuadType
+    bool aaHasEffectOnRect() const;
+
+#ifdef SK_DEBUG
+    GrQuadType quadType() const;
+#endif
+
 private:
     float fX[4];
     float fY[4];
@@ -79,6 +104,15 @@ public:
     Sk4f y4f() const { return Sk4f::Load(fY); }
     Sk4f w4f() const { return Sk4f::Load(fW); }
     Sk4f iw4f() const { return Sk4f::Load(fIW); }
+
+    bool hasPerspective() const { return (w4f() != Sk4f(1.f)).anyTrue(); }
+
+    // True if anti-aliasing affects this quad. Requires quadType() == kRect_QuadType
+    bool aaHasEffectOnRect() const;
+
+#ifdef SK_DEBUG
+    GrQuadType quadType() const;
+#endif
 
 private:
     float fX[4];
