01308896e3474dce13bc8bd86526f9e94ce3ff4b
Mike Klein <mtklein@google.com>
2018-10-19 11:38:44 +0000

    Revert "Use OpenGL sampler objects when available."
    This reverts commit 0f7e55a818f4661309871f2db2fe2c2196135bda.
    
    Reason for revert: Chrome roll, even alone (https://chromium-review.googlesource.com/c/chromium/src/+/1290785), maybe Android roll too?
    
    Original change's description:
    > Use OpenGL sampler objects when available.
    > 
    > Bug: skia:8471
    > Change-Id: Ida3d08aa72772b8bbd08707048182bee5d27407c
    > Reviewed-on: https://skia-review.googlesource.com/c/163123
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,brianosman@google.com
    
    Change-Id: Ieee6823e83b9992a7281806b5a9156cd91fb22b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:8471

--- src/gpu/gl/GrGLTexture.cpp
+++ src/gpu/gl/GrGLTexture.cpp
@@ -68,7 +68,8 @@ GrGLTexture::GrGLTexture(GrGLGpu* gpu, const GrSurfaceDesc& desc, const IDDesc& 
 void GrGLTexture::init(const GrSurfaceDesc& desc, const IDDesc& idDesc) {
     SkASSERT(0 != idDesc.fInfo.fID);
     SkASSERT(0 != idDesc.fInfo.fFormat);
-    fParamsTimestamp = GrGpu::kExpiredTimestamp;
+    fTexParams.invalidate();
+    fTexParamsTimestamp = GrGpu::kExpiredTimestamp;
     fID = idDesc.fInfo.fID;
     fFormat = idDesc.fInfo.fFormat;
     fTextureIDOwnership = idDesc.fOwnership;
