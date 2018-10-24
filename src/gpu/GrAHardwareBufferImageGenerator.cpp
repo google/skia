22740dda5899fed514b8f59c5b66adb42887c42d
Greg Daniel <egdaniel@google.com>
2018-10-24 16:20:09 -0400

    Add support for external textures in Vulkan, specifically GrAHardwareBufferImageGenerator.
    Bug: skia:
    Change-Id: Ic2e79734cd1a450bc30dcd441eaed10d8b8ccda2

--- src/gpu/GrAHardwareBufferImageGenerator.cpp
+++ src/gpu/GrAHardwareBufferImageGenerator.cpp
@@ -186,15 +186,76 @@ static GrBackendTexture make_vk_backend_texture(
         return GrBackendTexture();
     }
 
-    SkASSERT(format == hwbFormatProps.format);
-    SkASSERT(SkToBool(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT & hwbFormatProps.formatFeatures) &&
-             SkToBool(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT & hwbFormatProps.formatFeatures) &&
-             SkToBool(VK_FORMAT_FEATURE_TRANSFER_DST_BIT & hwbFormatProps.formatFeatures));
+    VkExternalFormatANDROID externalFormat;
+    externalFormat.sType = VK_STRUCTURE_TYPE_EXTERNAL_FORMAT_ANDROID;
+    externalFormat.pNext = nullptr;
+    externalFormat.externalFormat = 0;  // If this is zero it is as if we aren't using this struct.
 
-    const VkExternalMemoryImageCreateInfo externalMemoryImageInfo {
-        VK_STRUCTURE_TYPE_EXTERNAL_MEMORY_IMAGE_CREATE_INFO, // sType
-        nullptr, // pNext
-        VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID, // handleTypes
+    GrVkYcbcrConversionInfo ycbcrConversion;
+
+    if (hwbFormatProps.format != VK_FORMAT_UNDEFINED) {
+        // TODO: We should not assume the transfer features here and instead should have a way for
+        // Ganesh's tracking of intenral images to report whether or not they support transfers.
+        SkASSERT(SkToBool(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT & hwbFormatProps.formatFeatures) &&
+                 SkToBool(VK_FORMAT_FEATURE_TRANSFER_SRC_BIT & hwbFormatProps.formatFeatures) &&
+                 SkToBool(VK_FORMAT_FEATURE_TRANSFER_DST_BIT & hwbFormatProps.formatFeatures));
+
+    } else {
+        // We have an external only format
+        SkASSERT(SkToBool(VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT & hwbFormatProps.formatFeatures));
+        SkASSERT(format == VK_FORMAT_UNDEFINED);
+        externalFormat.externalFormat = hwbFormatProps.externalFormat;
+
+        VkSamplerYcbcrConversionCreateInfo ycbcrCreateInfo;
+        memset(&ycbcrCreateInfo, 0, sizeof(VkSamplerYcbcrConversionCreateInfo));
+        ycbcrCreateInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_YCBCR_CONVERSION_CREATE_INFO;
+        ycbcrCreateInfo.pNext = &externalFormat;
+        ycbcrCreateInfo.format = VK_FORMAT_UNDEFINED;
+        ycbcrCreateInfo.ycbcrModel = hwbFormatProps.suggestYcbcrModel;
+        ycbcrCreateInfo.ycbcrRange = hwbFormatProps.suggestYcbcrRange;
+        // Componets is ignored for external format conversions;
+        // ycbcrCreateInfo.components = {0, 0, 0, 0};
+        ycbcrCreateInfo.xChromaOffset = hwbFormatProps.suggestedXChromaOffset;
+        ycbcrCreateInfo.yChromaOffset = hwbFormatProps.suggestedYChromaOffset;
+
+        if (VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_LINEAR_FILTER_BIT &
+            hwbFormatProps.formatFeatures) {
+            // TODO: It is probably better here to look at the requested sampler for the draw and if
+            // it only wants nearest then it probably shouldn't be forced into linear. However, that
+            // requires the need to track the desired filter in our caching of the proxy and
+            // possibly need to keep switching back and forth if hte requested filter changes. For
+            // now just upgrading to linear. This issue is moot if the format supports
+            // VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_SEPARATE_RECONSTRUCTION_FILTER_BIT
+            // since the image can be sampled with linear or nearest.
+            ycbcrCreateInfo.chromaFilter = VK_FILTER_LINEAR;
+            if (VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_SEPARATE_RECONSTRUCTION_FILTER_BIT &
+                hwbFormatProps.formatFeatures) {
+                ycbcrConversion.fAllowableFilters = GrYcbcrConversionFilters::kNearestOrLinear;
+            } else {
+                ycbcrConversion.fAllowableFilters = GrYcbcrConversionFilters::kLinearOnly;
+            }
+        } else {
+            ycbcrCreateInfo.chromaFilter = VK_FILTER_NEAREST;
+            ycbcrConversion.fAllowableFilters = GrYcbcrConversionFilters::kNearestOnly;
+        }
+
+        // False is the default here and (assuming format feature support) setting it to true may
+        // cause a performance hit. Until there is a strong reason to use explicit reconstruction
+        // this will be kept as false.
+        ycbcrCreateInfo.forceExplicitReconstruction = VK_FALSE;
+
+        VK_CALL(CreateSamplerYcbcrConversion(device, &ycbcrCreateInfo, nullptr,
+                                             &ycbcrConversion.fYcbcrConversion));
+        if (ycbcrConversion.fYcbcrConversion == VK_NULL_HANDLE) {
+            return GrBackendTexture();
+        }
+    }
+    SkASSERT(format == hwbFormatProps.format);
+
+    const VkExternalMemoryImageCreateInfo externalMemoryImageInfo{
+            VK_STRUCTURE_TYPE_EXTERNAL_MEMORY_IMAGE_CREATE_INFO,                 // sType
+            &externalFormat,                                                     // pNext
+            VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID,  // handleTypes
     };
     VkImageUsageFlags usageFlags = VK_IMAGE_USAGE_SAMPLED_BIT |
                                    VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
@@ -225,6 +286,10 @@ static GrBackendTexture make_vk_backend_texture(
     VkImage image;
     err = VK_CALL(CreateImage(device, &imageCreateInfo, nullptr, &image));
     if (VK_SUCCESS != err) {
+        if (ycbcrConversion.fYcbcrConversion != VK_NULL_HANDLE) {
+            VK_CALL(DestroySamplerYcbcrConversion(device, ycbcrConversion.fYcbcrConversion,
+                                                  nullptr));
+        }
         return GrBackendTexture();
     }
 
@@ -266,6 +331,10 @@ static GrBackendTexture make_vk_backend_texture(
         }
     }
     if (!foundHeap) {
+        if (ycbcrConversion.fYcbcrConversion != VK_NULL_HANDLE) {
+            VK_CALL(DestroySamplerYcbcrConversion(device, ycbcrConversion.fYcbcrConversion,
+                                                  nullptr));
+        }
         VK_CALL(DestroyImage(device, image, nullptr));
         return GrBackendTexture();
     }
@@ -292,6 +361,10 @@ static GrBackendTexture make_vk_backend_texture(
 
     err = VK_CALL(AllocateMemory(device, &allocInfo, nullptr, &memory));
     if (VK_SUCCESS != err) {
+        if (ycbcrConversion.fYcbcrConversion != VK_NULL_HANDLE) {
+            VK_CALL(DestroySamplerYcbcrConversion(device, ycbcrConversion.fYcbcrConversion,
+                                                  nullptr));
+        }
         VK_CALL(DestroyImage(device, image, nullptr));
         return GrBackendTexture();
     }
@@ -305,6 +378,10 @@ static GrBackendTexture make_vk_backend_texture(
 
     err = VK_CALL(BindImageMemory2(device, 1, &bindImageInfo));
     if (VK_SUCCESS != err) {
+        if (ycbcrConversion.fYcbcrConversion != VK_NULL_HANDLE) {
+            VK_CALL(DestroySamplerYcbcrConversion(device, ycbcrConversion.fYcbcrConversion,
+                                                  nullptr));
+        }
         VK_CALL(DestroyImage(device, image, nullptr));
         VK_CALL(FreeMemory(device, memory, nullptr));
         return GrBackendTexture();
@@ -322,6 +399,7 @@ static GrBackendTexture make_vk_backend_texture(
     // support that extension. Or if we know the source of the AHardwareBuffer is not from a
     // "foreign" device we can leave them as external.
     imageInfo.fCurrentQueueFamily = VK_QUEUE_FAMILY_EXTERNAL;
+    imageInfo.fYcbcrConversionInfo = ycbcrConversion;
 
     *deleteProc = GrAHardwareBufferImageGenerator::DeleteVkImage;
     *deleteCtx = new VulkanCleanupHelper(gpu, image, memory);
@@ -458,7 +536,6 @@ GrBackendFormat get_backend_format(GrBackendApi backend, uint32_t bufferFormat) 
         }
     } else if (backend == GrBackendApi::kVulkan) {
         switch (bufferFormat) {
-            //TODO: find out if we can detect, which graphic buffers support GR_GL_TEXTURE_2D
             case AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM:
                 return GrBackendFormat::MakeVk(VK_FORMAT_R8G8B8A8_UNORM);
             case AHARDWAREBUFFER_FORMAT_R16G16B16A16_FLOAT:
@@ -472,7 +549,7 @@ GrBackendFormat get_backend_format(GrBackendApi backend, uint32_t bufferFormat) 
             case AHARDWAREBUFFER_FORMAT_R8G8B8_UNORM:
                 return GrBackendFormat::MakeVk(VK_FORMAT_R8G8B8_UNORM);
             default:
-                return GrBackendFormat::MakeVk(VK_FORMAT_R8G8B8_UNORM);
+                return GrBackendFormat::MakeVk(VK_FORMAT_UNDEFINED);
         }
     }
     return GrBackendFormat();
@@ -502,6 +579,12 @@ sk_sp<GrTextureProxy> GrAHardwareBufferImageGenerator::makeProxy(GrContext* cont
     GrTextureType textureType = GrTextureType::k2D;
     if (context->contextPriv().getBackend() == GrBackendApi::kOpenGL) {
         textureType = GrTextureType::kExternal;
+    } else if (context->contextPriv().getBackend() == GrBackendApi::kVulkan) {
+        VkFormat* format = backendFormat.getVkFormat();
+        SkASSERT(format);
+        if (*format == VK_FORMAT_UNDEFINED) {
+            textureType = GrTextureType::kExternal;
+        }
     }
 
     auto proxyProvider = context->contextPriv().proxyProvider();
