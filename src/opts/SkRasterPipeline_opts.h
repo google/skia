d6f90acd795748e326764eddb0d8a1c8461aeacb
Mike Klein <mtklein@google.com>
2018-10-26 12:07:57 -0400

    also catch 1^y == 1 as a special case
    We kind of really should already have this, but the unit tests just
    noticed it was missing loudly when trying to use apply_tf() for sRGB.
    
    skcms already does exactly this.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7a6f61e6ce1b7569e7b63e6f228f82fd55510979

--- src/opts/SkRasterPipeline_opts.h
+++ src/opts/SkRasterPipeline_opts.h
@@ -720,8 +720,8 @@ SI F approx_pow2(F x) {
 }
 
 SI F approx_powf(F x, F y) {
-    return if_then_else(x == 0, 0
-                              , approx_pow2(approx_log2(x) * y));
+    return if_then_else((x == 0)|(x == 1), x
+                                         , approx_pow2(approx_log2(x) * y));
 }
 
 SI F from_half(U16 h) {
