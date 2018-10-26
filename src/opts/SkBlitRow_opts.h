20457af3e6e13bd226ef9e291bc78d012c0bb51f
Mike Klein <mtklein@google.com>
2018-10-26 10:24:26 -0400

    move unspecialized routines out of SkOpts
    No src/opts/SkOpts_foo.cpp ever replaces these function pointers with
    a specialized version, so there's no value to the indirection.
    
    I kind of want to rewrite most of this, but I've not done that here.
    It's all just the same code moved around.
    
    Change-Id: Iecb81a64aff3e9ed18c1a3c6d2eb1a6e94e966de

--- src/opts/SkBlitRow_opts.h
+++ src/opts/SkBlitRow_opts.h
@@ -8,7 +8,6 @@
 #ifndef SkBlitRow_opts_DEFINED
 #define SkBlitRow_opts_DEFINED
 
-#include "Sk4px.h"
 #include "SkColorData.h"
 #include "SkMSAN.h"
 
@@ -18,25 +17,6 @@
 #endif
 
 namespace SK_OPTS_NS {
-
-// Color32 uses the blend_256_round_alt algorithm from tests/BlendTest.cpp.
-// It's not quite perfect, but it's never wrong in the interesting edge cases,
-// and it's quite a bit faster than blend_perfect.
-//
-// blend_256_round_alt is our currently blessed algorithm.  Please use it or an analogous one.
-static inline
-void blit_row_color32(SkPMColor* dst, const SkPMColor* src, int count, SkPMColor color) {
-    unsigned invA = 255 - SkGetPackedA32(color);
-    invA += invA >> 7;
-    SkASSERT(invA < 256);  // We've should have already handled alpha == 0 externally.
-
-    Sk16h colorHighAndRound = Sk4px::DupPMColor(color).widenHi() + Sk16h(128);
-    Sk16b invA_16x(invA);
-
-    Sk4px::MapSrc(count, dst, src, [&](const Sk4px& src4) -> Sk4px {
-        return (src4 * invA_16x).addNarrowHi(colorHighAndRound);
-    });
-}
 
 #if defined(SK_ARM_HAS_NEON)
 
