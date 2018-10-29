84d3e86c19c4fdab913377078a0aa34445deede3
Hal Canary <halcanary@google.com>
2018-10-29 16:39:10 -0400

    tools/skqp/make_universal_apk -> python
    Change-Id: Icd532cb1c4528874fdeefb2bb8fc84d416688c97

--- infra/skqp/build_apk.sh
+++ infra/skqp/build_apk.sh
@@ -6,10 +6,6 @@
 #
 # Assumes this is in a docker container with a skia repo mounted at /SRC
 
-SKIA_ROOT="$(cd "$(dirname "$0")/../.."; pwd)"
+[ -d /OUT ] || echo '/OUT is missing!'
 
-"$SKIA_ROOT"/tools/skqp/make_universal_apk x86
-
-# Clean out previous builds
-rm -rf /OUT/*
-cp "$SKIA_ROOT"/out/skqp/skqp-universal-debug.apk /OUT/skqp-universal-x86-debug.apk
+SKQP_OUTPUT_DIR=/OUT python /SRC/tools/skqp/make_universal_apk.py x86
