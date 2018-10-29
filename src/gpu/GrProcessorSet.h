bdc12f4c4250da15964014d6802852d8a2e62a57
Brian Osman <brianosman@google.com>
2018-10-29 15:37:14 -0400

    Keep paint color range/precision into ops and GPs (using GrColor4h)
    We still degrade to bytes when creating vertices (tagged TODO4F).
    
    Bug: skia:
    Change-Id: Id8a1d9eec7978d52b059cd9952666bc1217ee073

--- src/gpu/GrProcessorSet.h
+++ src/gpu/GrProcessorSet.h
@@ -141,7 +141,7 @@ public:
      */
     Analysis finalize(const GrProcessorAnalysisColor& colorInput,
                       const GrProcessorAnalysisCoverage coverageInput, const GrAppliedClip*,
-                      bool isMixedSamples, const GrCaps&, GrColor* inputColorOverride);
+                      bool isMixedSamples, const GrCaps&, GrColor4h* inputColorOverride);
 
     bool isFinalized() const { return SkToBool(kFinalized_Flag & fFlags); }
 
