25705fd89324b996f97bf51e8fed4d329e2067a1
Jim Van Verth <jvanverth@google.com>
2018-11-02 13:18:15 -0400

    Draw YUVA images with multitexture
    Bug: skia:7901
    Change-Id: I99cde1acc27c1cfb730671463a2c17537926cd99

--- src/gpu/GrImageTextureMaker.h
+++ src/gpu/GrImageTextureMaker.h
@@ -12,6 +12,7 @@
 #include "SkImage.h"
 
 class SkImage_Lazy;
+class SkImage_GpuYUVA;
 
 /** This class manages the conversion of generator-backed images to GrTextures. If the caching hint
     is kAllow the image's ID is used for the cache key. */
@@ -39,5 +40,38 @@ private:
 
     typedef GrTextureMaker INHERITED;
 };
+
+/** This class manages the conversion of generator-backed YUVA images to GrTextures. */
+class GrYUVAImageTextureMaker : public GrTextureMaker {
+public:
+    GrYUVAImageTextureMaker(GrContext* context, const SkImage* client);
+
+protected:
+    // TODO: consider overriding this, for the case where the underlying generator might be
+    //       able to efficiently produce a "stretched" texture natively (e.g. picture-backed)
+    //          GrTexture* generateTextureForParams(const CopyParams&) override;
+    sk_sp<GrTextureProxy> refOriginalTextureProxy(bool willBeMipped,
+                                                  AllowedTexGenType onlyIfFast) override;
+
+    void makeCopyKey(const CopyParams& stretch, GrUniqueKey* paramsCopyKey) override;
+    void didCacheCopy(const GrUniqueKey& copyKey, uint32_t contextUniqueID) override {}
+
+    std::unique_ptr<GrFragmentProcessor> createFragmentProcessor(
+        const SkMatrix& textureMatrix,
+        const SkRect& constraintRect,
+        FilterConstraint filterConstraint,
+        bool coordsLimitedToConstraintRect,
+        const GrSamplerState::Filter* filterOrNullForBicubic) override;
+
+    SkAlphaType alphaType() const override;
+    SkColorSpace* colorSpace() const override;
+
+private:
+    const SkImage_GpuYUVA*  fImage;
+    GrUniqueKey             fOriginalKey;
+
+    typedef GrTextureMaker INHERITED;
+};
+
 
 #endif
