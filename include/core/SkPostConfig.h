ef1c83a0c5df76446e7a9832e37b16f5dfb6ef12
Mike Klein <mtklein@google.com>
2018-11-02 13:12:32 -0400

    make big-endian very opt-in
    Change-Id: I578d064660cb20b834b80898ddc18e2f36394f11
    Reviewed-on: https://skia-review.googlesource.com/c/167685
    Reviewed-by: Hal Canary <halcanary@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- include/core/SkPostConfig.h
+++ include/core/SkPostConfig.h
@@ -40,6 +40,12 @@
 #  error "must define either SK_CPU_LENDIAN or SK_CPU_BENDIAN"
 #endif
 
+#if defined(SK_CPU_BENDIAN) && !defined(I_ACKNOWLEDGE_SKIA_DOES_NOT_SUPPORT_BIG_ENDIAN)
+    #error "The Skia team is not endian-savvy enough to support big-endian CPUs."
+    #error "If you still want to use Skia,"
+    #error "please define I_ACKNOWLEDGE_SKIA_DOES_NOT_SUPPORT_BIG_ENDIAN."
+#endif
+
 /**
  * Ensure the port has defined all of SK_X32_SHIFT, or none of them.
  */
