3d664bf772342de7d551a0f49a5369d57edb099b
Mike Reed <reed@google.com>
2018-11-05 09:42:56 -0500

    add SkFontPriv.h
    Bug: skia:2664
    Change-Id: I1751aee25c64880e528432441be7fa6129fc1e0f

---
+++ src/core/SkFontPriv.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkFontPriv_DEFINED
+#define SkFontPriv_DEFINED
+
+#include "SkFont.h"
+#include "SkMatrix.h"
+#include "SkTypeface.h"
+
+class SkFontPriv {
+public:
+    /**
+     *  Return a matrix that applies the paint's text values: size, scale, skew
+     */
+    static SkMatrix MakeTextMatrix(SkScalar size, SkScalar scaleX, SkScalar skewX) {
+        SkMatrix m = SkMatrix::MakeScale(size * scaleX, size);
+        if (skewX) {
+            m.postSkew(skewX, 0);
+        }
+        return m;
+    }
+
+    static SkMatrix MakeTextMatrix(const SkFont& font) {
+        return MakeTextMatrix(font.getSize(), font.getScaleX(), font.getSkewX());
+    }
+
+    // returns -1 if buffer is invalid for specified encoding
+    static int ValidCountText(const void* text, size_t length, SkTextEncoding);
+
+    static SkTypeface* GetTypefaceOrDefault(const SkFont& font) {
+        return font.getTypeface() ? font.getTypeface() : SkTypeface::GetDefaultTypeface();
+    }
+
+    static sk_sp<SkTypeface> RefTypefaceOrDefault(const SkFont& font) {
+        return font.getTypeface() ? font.refTypeface() : SkTypeface::MakeDefault();
+    }
+};
+
+#endif
