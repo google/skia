f89862bcdabdeb3e34354e1be5fdf90f9c13d386
Ben Wagner <benjaminwagner@google.com>
2018-11-06 11:24:54 -0500

    Add subset of Ubuntu18 jobs.
    Bug: 784662
    Change-Id: I1ea9f1bf1635c36895eede80df6709509a85df90

--- tools/LsanSuppressions.cpp
+++ tools/LsanSuppressions.cpp
@@ -20,6 +20,7 @@ extern "C" {
         return "leak:libfontconfig\n"    // FontConfig looks like it leaks, but it doesn't.
                "leak:libGL.so\n"         // For NVidia driver.
                "leak:libGLX_nvidia.so\n" // For NVidia driver.
+               "leak:libnvidia-glcore.so\n" // For NVidia driver.
                "leak:__strdup\n"         // An eternal mystery, skia:2916.
                ;
     }
