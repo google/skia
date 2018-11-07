4ac02115e3bed3d2d51756ef18d4d1f55286bf47
Mike Reed <reed@google.com>
2018-11-07 11:30:04 -0500

    transition to enum class for SkFontHinting
    Bug: skia:8527
    Change-Id: I8c1f2b9d97b51ca577c2e912c6cb048f5b6e2dc4

--- include/core/SkFontTypes.h
+++ include/core/SkFontTypes.h
@@ -18,11 +18,25 @@ enum SkTextEncoding : uint8_t {
     kGlyphID_SkTextEncoding,
 };
 
+#ifdef SK_SUPPORT_LEGACY_NONCLASS_HINTINGENUM
 enum SkFontHinting : uint8_t {
     kNo_SkFontHinting     = 0, //!< glyph outlines unchanged
     kSlight_SkFontHinting = 1, //!< minimal modification to improve constrast
     kNormal_SkFontHinting = 2, //!< glyph outlines modified to improve constrast
     kFull_SkFontHinting   = 3, //!< modifies glyph outlines for maximum constrast
 };
+#else
+enum class SkFontHinting {
+    kNone,      //!< glyph outlines unchanged
+    kSlight,    //!< minimal modification to improve constrast
+    kNormal,    //!< glyph outlines modified to improve constrast
+    kFull,      //!< modifies glyph outlines for maximum constrast
+};
+
+#define kNo_SkFontHinting       SkFontHinting::kNone
+#define kSlight_SkFontHinting   SkFontHinting::kSlight
+#define kNormal_SkFontHinting   SkFontHinting::kNormal
+#define kFull_SkFontHinting     SkFontHinting::kFull
+#endif
 
 #endif
