8529e6e5baacb605f69cb9175e155ce2b3e8cf84
Herb Derby <herb@google.com>
2018-11-08 16:40:26 -0500

    Reland "Move remove ptr args to MakeRecAndEffects"
    This is a reland of b07aba42145e29e0e995b373844c59846058a9ba
    
    Original change's description:
    > Move remove ptr args to MakeRecAndEffects
    > 
    > Move this conversion out through the transitive closure of calls. As you
    > move up the stack, everything becomes refs instread of pointers.
    > 
    > Reorder args of MakeRecAndEffects and setupCache to match the majority of other
    > calls.
    > 
    > Change-Id: I72baf457cd9140f76ee5f7122493284c4be5bcd0
    > Reviewed-on: https://skia-review.googlesource.com/c/169765
    > Reviewed-by: Ben Wagner <bungeman@google.com>
    > Commit-Queue: Herb Derby <herb@google.com>
    
    Change-Id: I2ff7f218ecc7b18ae6a2b293cecdb059eea77562

--- src/core/SkGlyphRunPainter.cpp
+++ src/core/SkGlyphRunPainter.cpp
@@ -180,7 +180,7 @@ void SkGlyphRunListPainter::drawForBitmapDevice(
             SkScalar textScale = pathPaint.setupForAsPaths();
 
             auto pathCache = SkStrikeCache::FindOrCreateStrikeExclusive(
-                    pathPaint, &props, fScalerContextFlags, nullptr);
+                    pathPaint, props, fScalerContextFlags, SkMatrix::I());
 
             SkTDArray<PathAndPos> pathsAndPositions;
             pathsAndPositions.setReserve(runSize);
@@ -204,7 +204,7 @@ void SkGlyphRunListPainter::drawForBitmapDevice(
                     paint);
         } else {
             auto cache = SkStrikeCache::FindOrCreateStrikeExclusive(
-                    paint, &props, fScalerContextFlags, &deviceMatrix);
+                    paint, props, fScalerContextFlags, deviceMatrix);
 
             // Add rounding and origin.
             SkMatrix matrix = deviceMatrix;
@@ -599,8 +599,8 @@ void GrTextBlob::generateFromGlyphRunList(GrGlyphCache* glyphCache,
             fBlob->setHasBitmap();
             fBlob->setSubRunHasW(fRunIndex, glyphCacheMatrix.hasPerspective());
             SkExclusiveStrikePtr fallbackCache =
-                    fBlob->setupCache(fRunIndex, fProps, fScalerContextFlags,
-                                     fallbackPaint, &glyphCacheMatrix);
+                    fBlob->setupCache(fRunIndex,
+                                      fallbackPaint, fProps, fScalerContextFlags, glyphCacheMatrix);
             sk_sp<GrTextStrike> strike = fGlyphCache->getStrike(fallbackCache.get());
             const SkPoint* glyphPos = positions.data();
             for (auto glyphID : glyphIDs) {
@@ -649,7 +649,7 @@ void GrTextBlob::generateFromGlyphRunList(GrGlyphCache* glyphCache,
 
             {
                 auto cache = this->setupCache(
-                        runIndex, props, flags, distanceFieldPaint, &SkMatrix::I());
+                        runIndex, distanceFieldPaint, props, flags, SkMatrix::I());
 
                 sk_sp<GrTextStrike> currStrike = glyphCache->getStrike(cache.get());
 
@@ -698,7 +698,7 @@ void GrTextBlob::generateFromGlyphRunList(GrGlyphCache* glyphCache,
 
             SkScalar textScale = pathPaint.setupForAsPaths();
             auto pathCache = SkStrikeCache::FindOrCreateStrikeExclusive(
-                    pathPaint, &props, scalerContextFlags, &SkMatrix::I());
+                    pathPaint, props, scalerContextFlags, SkMatrix::I());
 
             // Given a glyph that is not ARGB, draw it.
             auto perPath = [textScale, runIndex, this, &pathCache]
@@ -723,7 +723,7 @@ void GrTextBlob::generateFromGlyphRunList(GrGlyphCache* glyphCache,
             this->setHasBitmap();
 
             auto cache = this->setupCache(
-                    runIndex, props, scalerContextFlags, runPaint, &viewMatrix);
+                    runIndex, runPaint, props, scalerContextFlags, viewMatrix);
 
             sk_sp<GrTextStrike> currStrike = glyphCache->getStrike(cache.get());
 
