fab56c052d6827b537c931a7dee734c6f306372a
Hal Canary <halcanary@google.com>
2018-11-12 11:42:21 -0500

    PDF EXPERIMENT [WIP]
    Change-Id: I0175f69772c94f788d65c654b8e4747cacb384ac

---
+++ experimental/pdf_experiment/IRef.h
@@ -0,0 +1,9 @@
+// Copyright 2018 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#ifndef IRef_DEFINED
+#define IRef_DEFINED
+
+// PDF Indirect Reference.
+struct IRef { int fValue; };
+
+#endif  // IRef_DEFINED
