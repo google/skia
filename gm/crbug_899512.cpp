908bb23983d27ccd469c91556a313effe31c5052
Brian Salomon <bsalomon@google.com>
2018-11-19 10:05:08 -0500

    When outsetting for rect blur fast path use abs of scale factors.
    Fixes an issue where negative scales caused an inset rather than an outset.
    
    Add GM.
    
    Bug: chromium:899512
    Change-Id: I9164c76da479af80d4f5389b057ec52a946726fb
    Reviewed-on: https://skia-review.googlesource.com/c/171641
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

---
+++ gm/crbug_899512.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkBlurMaskFilter.h"
+#include "SkCanvas.h"
+#include "SkColorFilter.h"
+#include "SkLayerDrawLooper.h"
+#include "gm.h"
+
+DEF_SIMPLE_GM(crbug_899512, canvas, 520, 520) {
+    // comment this line below to solve the problem
+    SkMatrix matrix;
+    matrix.setAll(-1, 0, 220, 0, 1, 0, 0, 0, 1);
+    canvas->concat(matrix);
+    SkPaint paint;
+    paint.setMaskFilter(SkMaskFilter::MakeBlur(kNormal_SkBlurStyle, 6.2735f, false));
+    paint.setColorFilter(SkColorFilter::MakeModeFilter(SK_ColorBLACK, SkBlendMode::kSrcIn));
+    canvas->drawRect(SkRect::MakeXYWH(0, 10, 200, 200), paint);
+}
