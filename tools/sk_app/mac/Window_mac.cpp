f1ca068edb3f03dd49b1361c53c00d99473c2ee9
Mike Klein <mtklein@google.com>
2018-11-27 14:34:53 -0500

    workaround black screen in viewer on Mac 10.14
    Change-Id: I54e487eef8e3ab3bf9939ce219ef2a1642ca274d

--- tools/sk_app/mac/Window_mac.cpp
+++ tools/sk_app/mac/Window_mac.cpp
@@ -78,6 +78,10 @@ bool Window_mac::initWindow() {
         return false;
     }
 
+    // Workaround for a bug in SDL that causes a black screen until you move the window on 10.14.
+    SDL_PumpEvents();
+    SDL_SetWindowSize(fWindow, initialWidth,initialHeight);
+
     return true;
 }
 
