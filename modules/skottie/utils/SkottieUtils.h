44edc0e5d8330a10c49031256426b4a92563fb3e
Florin Malita <fmalita@chromium.org>
2018-11-28 11:39:39 -0500

    [skottie] Add a custom property manager util class
    To facilitate demo code consolidation, introduce a custom property
    manager which filters for node names starting with '$' and treats all
    properties sharing the same name unitarily.
    
    Update the Colorize GM to use this new helper.
    
    Also revisit the PropertyObserver interface:
    
     * aliases for client-facing value types
     * introduce a new (decomposed) TransformPropertyValue, to replace component-wise setters
     * consolidate the PropertyHandle interface to only expose get()/set()
    
    Bug: skia:
    Change-Id: I9aa9ee80c1fb57bbfbacab0fc3f017da909b24d9

--- modules/skottie/utils/SkottieUtils.h
+++ modules/skottie/utils/SkottieUtils.h
@@ -8,10 +8,16 @@
 #ifndef SkottieUtils_DEFINED
 #define SkottieUtils_DEFINED
 
+#include "SkColor.h"
 #include "Skottie.h"
+#include "SkottieProperty.h"
 #include "SkString.h"
+#include "SkTHash.h"
 
 #include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 class SkAnimCodecPlayer;
 class SkData;
@@ -49,6 +55,96 @@ private:
     const SkString fDir;
 
     using INHERITED = skottie::ResourceProvider;
+};
+
+/**
+ * CustomPropertyManager implements a property management scheme where color/opacity/transform
+ * attributes are grouped and manipulated by name (one-to-many mapping).
+ *
+ *   - setters apply the value to all properties in a named group
+ *
+ *   - getters return all the managed property groups, and the first value within each of them
+ *     (unchecked assumption: all properties within the same group have the same value)
+ *
+ * Use CustomPropertyManagerBuilder to filter nodes at animation build time, and instantiate a
+ * CustomPropertyManager.
+ */
+class CustomPropertyManager final {
+public:
+    ~CustomPropertyManager();
+
+    using PropKey = std::string;
+
+    std::vector<PropKey> getColorProps() const;
+    skottie::ColorPropertyValue getColor(const PropKey&) const;
+    bool setColor(const PropKey&, const skottie::ColorPropertyValue&);
+
+    std::vector<PropKey> getOpacityProps() const;
+    skottie::OpacityPropertyValue getOpacity(const PropKey&) const;
+    bool setOpacity(const PropKey&, const skottie::OpacityPropertyValue&);
+
+    std::vector<PropKey> getTransformProps() const;
+    skottie::TransformPropertyValue getTransform(const PropKey&) const;
+    bool setTransform(const PropKey&, const skottie::TransformPropertyValue&);
+
+private:
+    friend class CustomPropertyManagerBuilder;
+
+    template <typename T>
+    using PropGroup = std::vector<std::unique_ptr<T>>;
+
+    template <typename T>
+    using PropMap = std::unordered_map<PropKey, PropGroup<T>>;
+
+    template <typename T>
+    std::vector<PropKey> getProps(const PropMap<T>& container) const;
+
+    template <typename V, typename T>
+    V get(const PropKey&, const PropMap<T>& container) const;
+
+    template <typename V, typename T>
+    bool set(const PropKey&, const V&, const PropMap<T>& container);
+
+    CustomPropertyManager(PropMap<skottie::ColorPropertyHandle>,
+                          PropMap<skottie::OpacityPropertyHandle>,
+                          PropMap<skottie::TransformPropertyHandle>);
+
+    PropMap<skottie::ColorPropertyHandle>     fColorMap;
+    PropMap<skottie::OpacityPropertyHandle>   fOpacityMap;
+    PropMap<skottie::TransformPropertyHandle> fTransformMap;
+};
+
+/**
+ * A builder for CustomPropertyManager.  Only accepts node names starting with '$'.
+ */
+class CustomPropertyManagerBuilder final : public skottie::PropertyObserver {
+public:
+    CustomPropertyManagerBuilder();
+    ~CustomPropertyManagerBuilder() override;
+
+    std::unique_ptr<CustomPropertyManager> build();
+
+    void onColorProperty    (const char node_name[],
+                             const LazyHandle<skottie::ColorPropertyHandle>&) override;
+    void onOpacityProperty  (const char node_name[],
+                             const LazyHandle<skottie::OpacityPropertyHandle>&) override;
+    void onTransformProperty(const char node_name[],
+                             const LazyHandle<skottie::TransformPropertyHandle>&) override;
+
+private:
+    std::string acceptProperty(const char* name) const {
+        static constexpr char kPrefix = '$';
+
+        return (name[0] == kPrefix && name[1] != '\0')
+            ? std::string(name + 1)
+            : std::string();
+    }
+
+    CustomPropertyManager::PropMap<skottie::ColorPropertyHandle>     fColorMap;
+    CustomPropertyManager::PropMap<skottie::OpacityPropertyHandle>   fOpacityMap;
+    CustomPropertyManager::PropMap<skottie::TransformPropertyHandle> fTransformMap;
+
+    using INHERITED = skottie::PropertyObserver;
 };
 
 } // namespace skottie_utils
