aace1a0b3b3c7f7a9ec017711747106faad0d35a
Wei Li <weili@chromium.org>
2018-11-27 19:05:16 -0800

    Fix font creation lost italic style on Mac
    On Mac 10.11, when creating a font from its name and style by using a
    font descriptor, sometimes the created font lost the italic style. To
    work around this problem, we check the font style after the creation, if
    it is not correct, we will resort to font creation with symbolic traits
    which creates the correct font in this case.
    
    BUG=skia:8447
    
    Change-Id: I8b57e5a81d0d19d9fb0a7bd2951de75f2c41e236

--- tests/TypefaceTest.cpp
+++ tests/TypefaceTest.cpp
@@ -320,6 +320,16 @@ DEF_TEST(Typeface_serialize, reporter) {
     check_serialize_behaviors(SkTypeface::MakeFromStream(
                                          GetResourceAsStream("fonts/Distortable.ttf")),
                               true, reporter);
-
 }
 
+#ifdef SK_BUILD_FOR_MAC
+// This test makes sure the legacy typeface creation does not lose its italic
+// style. See https://bugs.chromium.org/p/skia/issues/detail?id=8447 for more
+// context.
+DEF_TEST(LegacyMakeTypeface, reporter) {
+    sk_sp<SkFontMgr> fm = SkFontMgr::RefDefault();
+    sk_sp<SkTypeface> typeface = fm->legacyMakeTypeface("Arial", SkFontStyle::Italic());
+
+    REPORTER_ASSERT(reporter, typeface->isItalic());
+}
+#endif
