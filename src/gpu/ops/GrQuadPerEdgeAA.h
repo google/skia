e87ea3d5e0a299dd20d9d2d2d7459ac4ba8bd78a
Michael Ludwig <michaelludwig@google.com>
2018-11-30 13:25:55 -0500

    Share GP implementation between GrFillRectOp and GrTextureOp
    Bug: skia:
    Change-Id: I6c71dac4cf88f274a39f121f0b5315e6c5b914c8

--- src/gpu/ops/GrQuadPerEdgeAA.h
+++ src/gpu/ops/GrQuadPerEdgeAA.h
@@ -9,15 +9,15 @@
 #define GrQuadPerEdgeAA_DEFINED
 
 #include "GrColor.h"
-#include "GrPrimitiveProcessor.h"
+#include "GrGeometryProcessor.h"
 #include "GrQuad.h"
 #include "GrSamplerState.h"
 #include "GrTypesPriv.h"
-#include "glsl/GrGLSLPrimitiveProcessor.h"
 #include "SkPoint.h"
 #include "SkPoint3.h"
 
-class GrGLSLColorSpaceXformHelper;
+class GrColorSpaceXform;
+class GrShaderCaps;
 
 namespace GrQuadPerEdgeAA {
 
@@ -65,81 +65,11 @@ namespace GrQuadPerEdgeAA {
         unsigned fUsesCoverageAA: 1;
     };
 
-    // Utility class that manages the attribute state necessary to render a particular batch of
-    // quads. It is similar to a geometry processor but is meant to be included in a has-a
-    // relationship by specialized GP's that provide further functionality on top of the per-edge AA
-    // coverage.
-    //
-    // For performance reasons, this uses fixed names for the attribute variables; since it defines
-    // the majority of attributes a GP will likely need, this shouldn't be too limiting.
-    //
-    // In terms of responsibilities, the actual geometry processor must still call emitTransforms(),
-    // using the localCoords() attribute as the 4th argument; it must set the transform data helper
-    // to use the identity matrix; it must manage the color space transform for the quad's paint
-    // color; it should include getKey() in the geometry processor's key builder; and it should
-    // add these attributes at construction time.
-    class GPAttributes {
-    public:
-        using Attribute = GrPrimitiveProcessor::Attribute;
+    sk_sp<GrGeometryProcessor> MakeProcessor(const VertexSpec& spec);
 
-        GPAttributes(const VertexSpec& vertexSpec);
-
-        const Attribute& positionsAndMaxCoverage() const { return fPositions; }
-        const Attribute& colors() const { return fColors; }
-        const Attribute& localCoords() const { return fLocalCoords; }
-        const Attribute& domain() const { return fDomain; }
-        const Attribute& edgeDistances() const { return fAAEdgeDistances; }
-
-        bool hasVertexColors() const { return fColors.isInitialized(); }
-
-        bool usesCoverageAA() const { return fAAEdgeDistances.isInitialized(); }
-
-        bool hasLocalCoords() const { return fLocalCoords.isInitialized(); }
-
-        bool hasDomain() const { return fDomain.isInitialized(); }
-
-        bool needsPerspectiveInterpolation() const;
-
-        const Attribute* attributes() const { return &fPositions; }
-        int attributeCount() const {  return 5; }
-
-        uint32_t getKey() const;
-
-        // Functions to be called at appropriate times in a processor's onEmitCode() block. These
-        // are separated into discrete pieces so that they can be interleaved with the rest of the
-        // processor's shader code as needed. The functions take char* arguments for the names of
-        // variables the emitted code must declare, so that the calling GP can ensure there's no
-        // naming conflicts with their own code.
-
-        // Returns the GrShaderVar to store in GrGPArgs
-        GrShaderVar emitPosition(GrGLSLPrimitiveProcessor::EmitArgs& args,
-                                 const char* posVarName) const;
-
-        void emitColor(GrGLSLPrimitiveProcessor::EmitArgs& args, const char* colorVarName) const;
-
-        // localCoordName will be declared as a float2, with any domain applied after any
-        // perspective division is performed.
-        //
-        // Note: this should only be used if the local coordinates need to be passed separately
-        // from the standard coord transform process that is used by FPs.
-        // FIXME: This can go in two directions from here, if GrTextureOp stops needing per-quad
-        // domains it can be removed and GrTextureOp rewritten to use coord transforms. Or
-        // emitTransform() in the primitive builder can be updated to have a notion of domain for
-        // local coords, and all domain-needing code (blurs, filters, etc.) can switch to that magic
-        void emitExplicitLocalCoords(GrGLSLPrimitiveProcessor::EmitArgs& args,
-                                     const char* localCoordName, const char* domainVarName) const;
-
-        void emitCoverage(GrGLSLPrimitiveProcessor::EmitArgs& args, const char* edgeDistName) const;
-    private:
-        Attribute fPositions;       // named "position" in SkSL
-        Attribute fColors;          // named "color" in SkSL
-        Attribute fLocalCoords;     // named "localCoord" in SkSL
-        Attribute fDomain;          // named "domain" in SkSL
-        Attribute fAAEdgeDistances; // named "aaEdgeDist" in SkSL
-
-        // The positions attribute is always a vec4 and can't be used to encode perspectiveness
-        bool fNeedsPerspective;
-    };
+    sk_sp<GrGeometryProcessor> MakeTexturedProcessor(const VertexSpec& spec,
+            const GrShaderCaps& caps, GrTextureType textureType, GrPixelConfig textureConfig,
+            const GrSamplerState::Filter filter, sk_sp<GrColorSpaceXform> textureColorSpaceXform);
 
     // Fill vertices with the vertex data needed to represent the given quad. The device position,
     // local coords, vertex color, domain, and edge coefficients will be written and/or computed
@@ -147,7 +77,6 @@ namespace GrQuadPerEdgeAA {
     // then its corresponding function argument is ignored.
     //
     // Returns the advanced pointer in vertices.
-    // TODO4F: Switch GrColor to GrVertexColor
     void* Tessellate(void* vertices, const VertexSpec& spec, const GrPerspQuad& deviceQuad,
                      const SkPMColor4f& color, const GrPerspQuad& localQuad, const SkRect& domain,
                      GrQuadAAFlags aa);
