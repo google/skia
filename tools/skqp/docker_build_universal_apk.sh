9e86b07b13aa26cce20c64912d326d37945fc566
Hal Canary <halcanary@google.com>
2018-12-04 13:16:01 -0500

    SkQP: helpful script: docker_build_universal_apk.sh
    No-Try: true
    Change-Id: Iecaebef0678ae355a1e70ec3dce7d4fbb0f766e4
    Reviewed-on: https://skia-review.googlesource.com/c/174307
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Auto-Submit: Hal Canary <halcanary@google.com>

---
+++ tools/skqp/docker_build_universal_apk.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Copyright 2018 Google LLC.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Note: you may need to run as root for docker permissions.
+
+OUT="$(mktemp -d "${TMPDIR:-/tmp}/skqp_apk.XXXXXXXXXX")"
+BUILD="$(mktemp -d "${TMPDIR:-/tmp}/skqp_apk_build.XXXXXXXXXX")"
+SKIA_ROOT="$(cd "$(dirname "$0")/../.."; pwd)"
+
+cd "${SKIA_ROOT}/infra/skqp/docker"
+
+docker build -t android-skqp ./android-skqp/
+docker run --rm --privileged -d --name android_em \
+        --env=DEVICE="Samsung Galaxy S6" \
+        ---volume="$SKIA_ROOT":/SRC \
+        --volume="$OUT":/OUT \
+        --volume="$BUILD":/BUILD \
+        android-skqp
+docker exec \
+    --env=SKQP_OUTPUT_DIR=/OUT \
+    --env=SKQP_BUILD_DIR=/BUILD \
+    android_em /SRC/tools/skqp/make_universal_apk.py
+docker kill android_em
+rm -r "$BUILD"
+chmod -R 0777 "$OUT"
+ls -l "$OUT"/*.apk
