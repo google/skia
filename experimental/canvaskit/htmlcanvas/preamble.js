4c79a04aba47c220d257594b480738a7b5963c90
Kevin Lubick <kjlubick@google.com>
2018-12-10 12:28:40 -0500

    [canvaskit] Refactor JS into own files
    Bug: skia:
    Change-Id: Ia191c2db778591af21d2a6126f053c17c4f677f1

---
+++ experimental/canvaskit/htmlcanvas/preamble.js
@@ -0,0 +1,13 @@
+// Adds compile-time JS functions to augment the CanvasKit interface.
+// Specifically, the code that emulates the HTML Canvas interface
+// (which is called HTMLCanvas or similar to avoid confusion with
+// SkCanvas).
+(function(CanvasKit) {
+
+  CanvasKit._testing = {};
+
+// This intentionally dangles because we want all the htmlcanvas
+// JS code to be in the same scope, but JS doesn't support
+// namespaces like C++ does. Thus, we simply include this
+// preamble.js file, all the source .js files and then postamble.js
+// to bundle everything in the same scope.
