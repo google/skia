ea83fcbd189697e93a771bd5f7559880b115ee14
Michael Ludwig <michaelludwig@google.com>
2018-12-11 13:43:57 -0500

    Update 405 vk blacklist
    Bug: skia: 7322
    Change-Id: If4dcb5400a9e8bc0a9a98a95b79d79650fd28d20
    Reviewed-on: https://skia-review.googlesource.com/c/176586
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- infra/bots/recipes/perf.py
+++ infra/bots/recipes/perf.py
@@ -182,6 +182,7 @@ def nanobench_flags(api, bot):
   if 'IntelHD405' in bot and api.vars.is_linux and 'Vulkan' in bot:
     # skia:7322
     match.append('~desk_carsvg.skp_1')
+    match.append('~desk_googlehome.skp_1_mpd')
     match.append('~desk_tiger8svg.skp_1')
     match.append('~desk_wowwiki.skp')
     match.append('~desk_ynevsvg.skp_1.1')
