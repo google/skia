54e448c5e0993eddab56128ca32445790f7a0217
Mike Reed <reed@google.com>
2018-12-11 10:35:03 -0500

    impl intercepts with fonts
    Bug: skia:
    Change-Id: Ib3958c61822716846e12647afdfef4ecd979b474

--- src/core/SkTextToPathIter.h
+++ src/core/SkTextToPathIter.h
@@ -13,11 +13,17 @@
 #include "SkStrikeCache.h"
 
 class SkTextBaseIter {
+public:
+    const SkFont&   getFont() const { return fFont; }
+    const SkPaint&  getPaint() const { return fPaint; }
+    SkScalar        getPathScale() const { return fScale; }
+
 protected:
-    SkTextBaseIter(const char text[], size_t length, const SkPaint& paint,
-                   bool applyStrokeAndPathEffects);
+    SkTextBaseIter(const char text[], size_t length, SkTextEncoding,
+                   const SkFont&, const SkPaint& paint, bool applyStrokeAndPathEffects);
 
     SkExclusiveStrikePtr fCache;
+    SkFont               fFont;
     SkPaint              fPaint;
     SkScalar             fScale;
     SkScalar             fPrevAdvance;
@@ -30,13 +36,10 @@ protected:
 
 class SkTextToPathIter : SkTextBaseIter {
 public:
-    SkTextToPathIter(const char text[], size_t length, const SkPaint& paint,
-                     bool applyStrokeAndPathEffects)
-                     : SkTextBaseIter(text, length, paint, applyStrokeAndPathEffects) {
-    }
-
-    const SkPaint&  getPaint() const { return fPaint; }
-    SkScalar        getPathScale() const { return fScale; }
+    SkTextToPathIter(const char text[], size_t length, SkTextEncoding encoding,
+                     const SkFont& font, const SkPaint& paint, bool applyStrokeAndPathEffects)
+         : SkTextBaseIter(text, length, encoding, font, paint, applyStrokeAndPathEffects)
+    {}
 
     /**
      *  Returns false when all of the text has been consumed
@@ -51,9 +54,10 @@ public:
         kPosText
     };
 
-    SkTextInterceptsIter(const char text[], size_t length, const SkPaint& paint,
-                         const SkScalar bounds[2], SkScalar x, SkScalar y, TextType textType)
-         : SkTextBaseIter(text, length, paint, false)
+    SkTextInterceptsIter(const char text[], size_t length, SkTextEncoding encoding, const SkFont& font,
+                         const SkPaint& paint, const SkScalar bounds[2], SkScalar x, SkScalar y,
+                         TextType textType)
+         : SkTextBaseIter(text, length, encoding, font, paint, false)
     {
         fBoundsBase[0] = bounds[0];
         fBoundsBase[1] = bounds[1];
