e855b9decd485ff2e8aecc8e9a5bc6729e8b2f51
Herbert Derby <herb@google.com>
2018-12-10 21:08:13 -0500

    Use SkFont for bitmap device rendering
    Change-Id: Id392a1534e1b04baa3295e79af785e40dfd7cfb6
    Reviewed-on: https://skia-review.googlesource.com/c/176227
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: Mike Reed <reed@google.com>

--- include/core/SkFont.h
+++ include/core/SkFont.h
@@ -496,6 +496,7 @@ private:
 
     friend class SkCanonicalizeFont;
     friend class SkFontPriv;
+    friend class SkGlyphRunListPainter;
     friend class SkPaint;
     friend class SVGTextBuilder;
 };
