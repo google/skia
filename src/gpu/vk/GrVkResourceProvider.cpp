61df4ce50577b3ee5ed0268b36f2b8c884c97181
Ethan Nicholas <ethannicholas@google.com>
2018-12-12 11:18:40 -0500

    fix for GrVkGpu not always unreffing all command pools
    Bug: skia:
    Change-Id: I2a122aa25f5a8bedb413b2dca3b53637f8ddd604

--- src/gpu/vk/GrVkResourceProvider.cpp
+++ src/gpu/vk/GrVkResourceProvider.cpp
@@ -330,6 +330,11 @@ void GrVkResourceProvider::recycleStandardUniformBufferResource(const GrVkResour
 }
 
 void GrVkResourceProvider::destroyResources(bool deviceLost) {
+    SkTaskGroup* taskGroup = fGpu->getContext()->contextPriv().getTaskGroup();
+    if (taskGroup) {
+        taskGroup->wait();
+    }
+
     // Release all copy pipelines
     for (int i = 0; i < fCopyPipelines.count(); ++i) {
         fCopyPipelines[i]->unref(fGpu);
