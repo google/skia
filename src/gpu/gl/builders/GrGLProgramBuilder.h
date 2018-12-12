66ddf9d4e3348c52aefc7834e618c550eca7ff60
Ethan Nicholas <ethannicholas@google.com>
2018-12-12 12:45:23 -0500

    we now cache GLSL shaders when we can't cache the binaries
    Bug: skia:
    Change-Id: I56552049c141cbd69fa6b55653e38820d541bf2f

--- src/gpu/gl/builders/GrGLProgramBuilder.h
+++ src/gpu/gl/builders/GrGLProgramBuilder.h
@@ -66,6 +66,8 @@ private:
                                  SkSL::Program::Inputs* outInputs);
     void computeCountsAndStrides(GrGLuint programID, const GrPrimitiveProcessor& primProc,
                                  bool bindAttribLocations);
+    void storeShaderInCache(const SkSL::Program::Inputs& inputs, GrGLuint programID,
+                            const SkSL::String& glsl);
     GrGLProgram* finalize();
     void bindProgramResourceLocations(GrGLuint programID);
     bool checkLinkStatus(GrGLuint programID);
