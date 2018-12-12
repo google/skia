044044f5a2338b8e94b5207128849bbd65283c96
Brian Osman <brianosman@google.com>
2018-12-12 09:58:26 -0500

    Update win_toolchain, and refactor how it's built
    The old win_toolchain script required a Chromium checkout, and
    extracted portions of the win_toolchain from that to build the
    Skia asset. Instead, use the depot_tools script that assembles
    a toolchain from a locally installed MSVC.
    
    The create script doesn't do that, but relies on the user to
    run that script first. Automating everything would be a nice
    follow-up.
    
    With the new strategy, the toolchain directory is simpler, and
    no longer contains the depot_tools kruft or extra directories.
    Adjust the bot scripts accordingly. (Renaming the directory to
    win_toolchain from 't' would be a nice touch, too).
    
    Finally, I built the new toolchain with the updated process,
    and included the ARM64 compiler and libraries, so we can set
    up a bot to build Windows ARM64.
    
    Docs-Preview: https://skia.org/?cl=176968
    Bug: skia:8569
    Change-Id: I4bdf3cfb29d50f4464853445d0226241e70c33b4
    Reviewed-on: https://skia-review.googlesource.com/c/176968
    Reviewed-by: Mike Klein <mtklein@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- infra/bots/assets/win_toolchain/create.py
+++ infra/bots/assets/win_toolchain/create.py
@@ -6,20 +6,27 @@
 # found in the LICENSE file.
 
 
-"""Download an updated VS toolchain"""
+"""
+Create an updated VS toolchain
 
+Before you can run this script, you need a collated VC toolchain + Windows SDK.
+To generate that, run depot_tools/win_toolchain/package_from_installed.py
+That script pulls all of the compiler and SDK bits from your locally installed
+version of Visual Studio. The comments in that script include instructions on
+which components need to be installed (C++, ARM64, etc...)
+
+That script produces a .zip file with a SHA filename. Unzip that file, then
+pass the unzipped directory as the src_dir to this script.
+"""
 
 import argparse
 import common
-import json
 import os
 import shlex
 import shutil
 import subprocess
 import sys
 import utils
-
-import win_toolchain_utils
 
 
 # By default the toolchain includes a bunch of unnecessary stuff with long path
@@ -33,9 +40,6 @@ IGNORE_LIST = [
   'AccChecker',
 ]
 
-REPO_CHROME = 'https://chromium.googlesource.com/chromium/src.git'
-
-
 def filter_toolchain_files(dirname, files):
   """Callback for shutil.copytree. Return lists of files to skip."""
   split = dirname.split(os.path.sep)
@@ -45,83 +49,18 @@ def filter_toolchain_files(dirname, files):
        return files
   return []
 
-
-def get_toolchain_dir(toolchain_dir_output):
-  """Find the toolchain directory."""
-  prefix = 'vs_path = '
-  for line in toolchain_dir_output.splitlines():
-    if line.startswith(prefix):
-      return line[len(prefix):].strip('"')
-  raise Exception('Unable to find toolchain dir in output:\n%s' % (
-                  toolchain_dir_output))
-
-
-def gen_toolchain(chrome_path, msvs_version, target_dir):
-  """Update the VS toolchain and copy it to the target_dir."""
-  with utils.chdir(os.path.join(chrome_path, 'src')):
-    subprocess.check_call([utils.GCLIENT, 'sync'])
-    depot_tools = subprocess.check_output([
-        'python', os.path.join('build', 'find_depot_tools.py')]).rstrip()
-    with utils.git_branch():
-      vs_toolchain_py = os.path.join('build', 'vs_toolchain.py')
-      env = os.environ.copy()
-      env['GYP_MSVS_VERSION'] = msvs_version
-      subprocess.check_call(['python', vs_toolchain_py, 'update'], env=env)
-      output = subprocess.check_output(['python', vs_toolchain_py,
-                                        'get_toolchain_dir'], env=env).rstrip()
-      src_dir = get_toolchain_dir(output)
-      # Mock out absolute paths in win_toolchain.json.
-      win_toolchain_utils.abstract(os.path.join('build', 'win_toolchain.json'),
-                                   os.path.dirname(depot_tools))
-
-      # Copy the toolchain files to the target_dir.
-      build = os.path.join(os.getcwd(), 'build')
-      dst_build = os.path.join(target_dir, 'src', 'build')
-      os.makedirs(dst_build)
-      for f in ('find_depot_tools.py', 'vs_toolchain.py', 'win_toolchain.json'):
-        shutil.copyfile(os.path.join(build, f), os.path.join(dst_build, f))
-
-      shutil.copytree(os.path.join(os.getcwd(), 'tools', 'gyp', 'pylib'),
-                      os.path.join(target_dir, 'src', 'tools', 'gyp', 'pylib'))
-
-      dst_depot_tools = os.path.join(target_dir, 'depot_tools')
-      os.makedirs(dst_depot_tools)
-      for f in ('gclient.py', 'breakpad.py'):
-        shutil.copyfile(os.path.join(depot_tools, f),
-                        os.path.join(dst_depot_tools, f))
-      toolchain_dst = os.path.join(
-          target_dir, 'depot_tools', os.path.relpath(src_dir, depot_tools))
-      shutil.copytree(src_dir, toolchain_dst, ignore=filter_toolchain_files)
-
-
-def create_asset(target_dir, msvs_version, chrome_path=None):
-  """Create the asset."""
-  if not os.path.isdir(target_dir):
-    os.makedirs(target_dir)
-  with utils.tmp_dir() as tmp_dir:
-    if not chrome_path:
-      print ('Syncing Chrome from scratch. If you already have a checkout, '
-             'specify --chrome_path to save time.')
-      chrome_path = os.path.join(tmp_dir.name, 'src')
-    if not os.path.isdir(chrome_path):
-      subprocess.check_call([utils.GCLIENT, 'config', REPO_CHROME, '--managed'])
-      subprocess.check_call([utils.GCLIENT, 'sync'])
-
-    gen_toolchain(chrome_path, msvs_version, target_dir)
-
 def main():
   if sys.platform != 'win32':
     print >> sys.stderr, 'This script only runs on Windows.'
     sys.exit(1)
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--msvs_version', required=True)
-  parser.add_argument('--chrome_path')
+  parser.add_argument('--src_dir', '-s', required=True)
   parser.add_argument('--target_dir', '-t', required=True)
   args = parser.parse_args()
+  src_dir = os.path.abspath(args.src_dir)
   target_dir = os.path.abspath(args.target_dir)
-  create_asset(target_dir, args.msvs_version, args.chrome_path)
-
+  shutil.copytree(src_dir, target_dir, ignore=filter_toolchain_files)
 
 if __name__ == '__main__':
   main()
