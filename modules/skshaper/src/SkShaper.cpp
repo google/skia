075dac5dfe151b1fd05e2b110893361f51b05f96
Florin Malita <fmalita@chromium.org>
2018-12-12 10:38:15 -0500

    SkShaper per-line callback
    Tweak SkShaper to call out for each line, instead of bundling everything
    as a text blob.
    
    Change-Id: Ic522f88afcf31cefd873dc8b5cde1ac2e107c64f

---
+++ modules/skshaper/src/SkShaper.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "SkShaper.h"
+
+SkShaper::LineHandler::Buffer SkTextBlobBuilderLineHandler::newLineBuffer(const SkFont& font,
+                                                                          int count) {
+    const auto& runBuffer = fBuilder.allocRunPos(font, count);
+    return { runBuffer.glyphs, reinterpret_cast<SkPoint*>(runBuffer.pos), nullptr };
+}
+
+sk_sp<SkTextBlob> SkTextBlobBuilderLineHandler::makeBlob() {
+    return fBuilder.make();
+}
