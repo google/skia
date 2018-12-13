02081eb89b15d2c7fc6867f47be6ac0f3265b85a
Michael Ludwig <michaelludwig@google.com>
2018-12-13 12:56:55 -0500

    Update black list for bot 405
    Bug: skia:7322
    Change-Id: I3990adfcf32814504d429fba06a16716f514e214

--- infra/bots/recipes/perf.py
+++ infra/bots/recipes/perf.py
@@ -183,6 +183,7 @@ def nanobench_flags(api, bot):
     # skia:7322
     match.append('~desk_carsvg.skp_1')
     match.append('~desk_googlehome.skp')
+    match.append('~desk_nostroke_tiger8svg.skp_1')
     match.append('~desk_tiger8svg.skp_1')
     match.append('~desk_wowwiki.skp')
     match.append('~desk_ynevsvg.skp_1.1')
