cc4f7ebf280cd475c35ee10de007c6ff5709f73f
Hal Canary <halcanary@google.com>
2018-12-17 12:03:25 -0500

    Test-SKQP: stop checking for success
    CQ_INCLUDE_TRYBOTS=skia.primary:Build-Debian9-Clang-x86-devrel-Android_SKQP,Test-Debian9-Clang-NUC7i5BNK-CPU-Emulator-x86-devrel-All-Android_SKQP
    
    Change-Id: Ie258270784472d036a0d4b925bd071a764b737f8
    Reviewed-on: https://skia-review.googlesource.com/c/177899
    Commit-Queue: Hal Canary <halcanary@google.com>
    Reviewed-by: Hal Canary <halcanary@google.com>

--- infra/skqp/run_skqp.sh
+++ infra/skqp/run_skqp.sh
@@ -30,5 +30,7 @@ adb shell am instrument -w org.skia.skqp | tee "$tmp_file" | fold -s
 
 adb logcat -d TestRunner org.skia.skqp skia DEBUG "*:S"
 
-grep -q '^OK ' "$tmp_file"
+if ! grep -q '^OK ' "$tmp_file"; then
+    echo 'this test failed'
+fi
 
