25f287dc4697dd1c056d5d152b8745c7369cf4e2
Greg Daniel <egdaniel@google.com>
2018-12-20 10:47:58 -0500

    Don't include system vulkan header when building skia files.
    With this change clients will still have to have vulkan/vulkan_core.h
    on their include path when compiling files that include Skia. However,
    it will not be required when compiling Skia files.
    
    Bug: skia:
    Change-Id: I1cd75f4f18d3097c8a1606c3e8a51a371b01b565

--- include/gpu/vk/GrVkTypes.h
+++ include/gpu/vk/GrVkTypes.h
@@ -9,11 +9,14 @@
 #ifndef GrVkTypes_DEFINED
 #define GrVkTypes_DEFINED
 
-#ifdef SK_VULKAN
+#include "SkTypes.h"
+
+#if defined(SK_VULKAN) && !SKIA_IMPLEMENTATION
 #include <vulkan/vulkan_core.h>
 #else
 #include "../../third_party/vulkan/vulkan/vulkan_core.h"
 #endif
+
 #ifndef VK_VERSION_1_1
 #error Skia requires the use of Vulkan 1.1 headers
 #endif
