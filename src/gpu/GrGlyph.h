1901defd5acd8686ca6d1c8ddfd1624265ff771d
Herb Derby <herb@google.com>
2018-12-20 14:47:03 -0500

    Convert GrGlyph::PackedID to SkPackedGlyphID
    This changes ripples through many areas of the code like the
    hash map.
    
    This will simplify making SkGlyphCache and GrTextStrike work
    together in future CLs.
    
    * Convert ctor to take an SkGlyph
    * Remove many unused functions
    
    Change-Id: I43ea414f03dfbb683ba7b46bf48a93c7a78895c6

--- src/gpu/GrGlyph.h
+++ src/gpu/GrGlyph.h
@@ -16,34 +16,52 @@
 #include "SkFixed.h"
 #include "SkPath.h"
 
-/*  Need this to be quad-state:
-    - complete w/ image
-    - just metrics
-    - failed to get image, but has metrics
-    - failed to get metrics
- */
 struct GrGlyph {
     enum MaskStyle {
         kCoverage_MaskStyle,
         kDistance_MaskStyle
     };
 
-    typedef uint32_t PackedID;
-
-    static GrIRect16 SkIRectToGrIRect16(const SkIRect& rect) {
-        return GrIRect16::MakeXYWH(SkTo<int16_t>(rect.x()),
-                                   SkTo<int16_t>(rect.y()),
-                                   SkTo<uint16_t>(rect.width()),
-                                   SkTo<uint16_t>(rect.height()));
+    static GrMaskFormat FormatFromSkGlyph(const SkGlyph& glyph) {
+        SkMask::Format format = static_cast<SkMask::Format>(glyph.fMaskFormat);
+        switch (format) {
+            case SkMask::kBW_Format:
+            case SkMask::kSDF_Format:
+                // fall through to kA8 -- we store BW and SDF glyphs in our 8-bit cache
+            case SkMask::kA8_Format:
+                return kA8_GrMaskFormat;
+            case SkMask::k3D_Format:
+                return kA8_GrMaskFormat; // ignore the mul and add planes, just use the mask
+            case SkMask::kLCD16_Format:
+                return kA565_GrMaskFormat;
+            case SkMask::kARGB32_Format:
+                return kARGB_GrMaskFormat;
+            default:
+                SkDEBUGFAIL("unsupported SkMask::Format");
+                return kA8_GrMaskFormat;
+        }
     }
 
-    GrGlyph(GrGlyph::PackedID packed, const SkIRect& bounds, GrMaskFormat format, MaskStyle style)
-        : fPackedID{packed}
-        , fMaskFormat{format}
-        , fMaskStyle{style}
-        , fBounds{SkIRectToGrIRect16(bounds)} {}
+    static GrIRect16 BoundsFromSkGlyph(const SkGlyph& glyph) {
+        return GrIRect16::MakeXYWH(glyph.fLeft,
+                                   glyph.fTop,
+                                   glyph.fWidth,
+                                   glyph.fHeight);
+    }
 
-    const PackedID         fPackedID;
+    static MaskStyle MaskStyleFromSkGlyph(const SkGlyph& skGlyph) {
+        return (SkMask::Format)skGlyph.fMaskFormat == SkMask::kSDF_Format
+           ? GrGlyph::MaskStyle::kDistance_MaskStyle
+           : GrGlyph::MaskStyle::kCoverage_MaskStyle;
+    }
+
+    GrGlyph(const SkGlyph& skGlyph)
+        : fPackedID{skGlyph.getPackedID()}
+        , fMaskFormat{FormatFromSkGlyph(skGlyph)}
+        , fMaskStyle{MaskStyleFromSkGlyph(skGlyph)}
+        , fBounds{BoundsFromSkGlyph(skGlyph)} {}
+
+    const SkPackedGlyphID  fPackedID;
     const GrMaskFormat     fMaskFormat;
     const MaskStyle        fMaskStyle;
     const GrIRect16        fBounds;
@@ -52,55 +70,16 @@ struct GrGlyph {
 
     int width() const { return fBounds.width(); }
     int height() const { return fBounds.height(); }
-    bool isEmpty() const { return fBounds.isEmpty(); }
-    uint16_t glyphID() const { return UnpackID(fPackedID); }
     uint32_t pageIndex() const { return GrDrawOpAtlas::GetPageIndexFromID(fID); }
     MaskStyle maskStyle() const { return fMaskStyle; }
 
-    ///////////////////////////////////////////////////////////////////////////
-
-    static unsigned ExtractSubPixelBitsFromFixed(SkFixed pos) {
-        // two most significant fraction bits from fixed-point
-        return (pos >> 14) & 3;
-    }
-
-    static PackedID FromSkGlyph(const SkGlyph& skGlyph) {
-        GrGlyph::MaskStyle maskStyle = (SkMask::Format)skGlyph.fMaskFormat == SkMask::kSDF_Format
-                                       ? GrGlyph::MaskStyle::kDistance_MaskStyle
-                                       : GrGlyph::MaskStyle::kCoverage_MaskStyle;
-        SkPackedGlyphID skPackedID = skGlyph.getPackedID();
-        GrGlyph::PackedID packedID = GrGlyph::Pack(skPackedID.code(),
-                                                   skPackedID.getSubXFixed(),
-                                                   skPackedID.getSubYFixed(),
-                                                   maskStyle);
-        return packedID;
-    }
-
-    static PackedID Pack(uint16_t glyphID, SkFixed x, SkFixed y, MaskStyle ms) {
-        x = ExtractSubPixelBitsFromFixed(x);
-        y = ExtractSubPixelBitsFromFixed(y);
-        int dfFlag = (ms == kDistance_MaskStyle) ? 0x1 : 0x0;
-        return (dfFlag << 20) | (x << 18) | (y << 16) | glyphID;
-    }
-
-    static SkFixed UnpackFixedX(PackedID packed) {
-        return ((packed >> 18) & 3) << 14;
-    }
-
-    static SkFixed UnpackFixedY(PackedID packed) {
-        return ((packed >> 16) & 3) << 14;
-    }
-
-    static uint16_t UnpackID(PackedID packed) {
-        return (uint16_t)packed;
-    }
-
-    static const GrGlyph::PackedID& GetKey(const GrGlyph& glyph) {
+    // GetKey and Hash for the the hash table.
+    static const SkPackedGlyphID& GetKey(const GrGlyph& glyph) {
         return glyph.fPackedID;
     }
 
-    static uint32_t Hash(GrGlyph::PackedID key) {
-        return SkChecksum::Mix(key);
+    static uint32_t Hash(SkPackedGlyphID key) {
+        return SkChecksum::Mix(key.hash());
     }
 };
 
