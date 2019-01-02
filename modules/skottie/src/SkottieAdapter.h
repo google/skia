9356393f5b483263922f39a8600da1cddc670915
Florin Malita <fmalita@chromium.org>
2019-01-02 09:26:13 -0500

    [skottie] Derive most adapters from SkNVRefCnt
    Bug: skia:
    Change-Id: I6355a3802090a8da2c29486bd769d6a6b12af10f
    Reviewed-on: https://skia-review.googlesource.com/c/180368
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- modules/skottie/src/SkottieAdapter.h
+++ modules/skottie/src/SkottieAdapter.h
@@ -44,9 +44,10 @@ namespace skottie {
     p_type f##p_name = p_default;                   \
   public:
 
-class RRectAdapter final : public SkRefCnt {
+class RRectAdapter final : public SkNVRefCnt<RRectAdapter> {
 public:
     explicit RRectAdapter(sk_sp<sksg::RRect>);
+    ~RRectAdapter();
 
     ADAPTER_PROPERTY(Position, SkPoint , SkPoint::Make(0, 0))
     ADAPTER_PROPERTY(Size    , SkSize  ,  SkSize::Make(0, 0))
@@ -56,17 +57,16 @@ private:
     void apply();
 
     sk_sp<sksg::RRect> fRRectNode;
-
-    using INHERITED = SkRefCnt;
 };
 
-class PolyStarAdapter final : public SkRefCnt {
+class PolyStarAdapter final : public SkNVRefCnt<PolyStarAdapter> {
 public:
     enum class Type {
         kStar, kPoly,
     };
 
     PolyStarAdapter(sk_sp<sksg::Path>, Type);
+    ~PolyStarAdapter();
 
     ADAPTER_PROPERTY(Position      , SkPoint , SkPoint::Make(0, 0))
     ADAPTER_PROPERTY(PointCount    , SkScalar, 0)
@@ -81,13 +81,12 @@ private:
 
     sk_sp<sksg::Path> fPathNode;
     Type              fType;
-
-    using INHERITED = SkRefCnt;
 };
 
-class TransformAdapter final : public SkRefCnt {
+class TransformAdapter final : public SkNVRefCnt<TransformAdapter> {
 public:
     explicit TransformAdapter(sk_sp<sksg::Matrix>);
+    ~TransformAdapter();
 
     ADAPTER_PROPERTY(AnchorPoint, SkPoint , SkPoint::Make(0, 0))
     ADAPTER_PROPERTY(Position   , SkPoint , SkPoint::Make(0, 0))
@@ -102,8 +101,6 @@ private:
     void apply();
 
     sk_sp<sksg::Matrix> fMatrixNode;
-
-    using INHERITED = SkRefCnt;
 };
 
 class GradientAdapter : public SkRefCnt {
@@ -125,8 +122,6 @@ protected:
 
 private:
     void apply();
-
-    using INHERITED = SkRefCnt;
 };
 
 class LinearGradientAdapter final : public GradientAdapter {
@@ -149,9 +144,10 @@ private:
     using INHERITED = GradientAdapter;
 };
 
-class TrimEffectAdapter final : public SkRefCnt {
+class TrimEffectAdapter final : public SkNVRefCnt<TrimEffectAdapter> {
 public:
     explicit TrimEffectAdapter(sk_sp<sksg::TrimEffect>);
+    ~TrimEffectAdapter();
 
     ADAPTER_PROPERTY(Start , SkScalar,   0)
     ADAPTER_PROPERTY(End   , SkScalar, 100)
@@ -161,13 +157,12 @@ private:
     void apply();
 
     sk_sp<sksg::TrimEffect> fTrimEffect;
-
-    using INHERITED = SkRefCnt;
 };
 
-class TextAdapter final : public SkRefCnt {
+class TextAdapter final : public SkNVRefCnt<TextAdapter> {
 public:
     explicit TextAdapter(sk_sp<sksg::Group> root);
+    ~TextAdapter();
 
     ADAPTER_PROPERTY(Text, TextValue, TextValue())
 
@@ -186,8 +181,6 @@ private:
 
     bool                   fHadFill   : 1, //  - state cached from the prev apply()
                            fHadStroke : 1; //  /
-
-    using INHERITED = SkRefCnt;
 };
 
 #undef ADAPTER_PROPERTY
