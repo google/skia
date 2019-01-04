d120964feaf1c3543faf965524d869f2ac7a42d1
Brian Osman <brianosman@google.com>
2019-01-04 17:03:29 -0500

    Goofy experiment
    Bug: skia:
    Change-Id: Id9402bef22825d55d021c5a2f9e5e41791aabaf4

--- gm/points.cpp
+++ gm/points.cpp
@@ -6,6 +6,11 @@
  */
 #include "gm.h"
 #include "SkRandom.h"
+#include "SkTextUtils.h"
+#include "sk_tool_utils.h"
+#include "SkPath.h"
+#include "SkPathMeasure.h"
+#include "SkVertices.h"
 
 namespace skiagm {
 
@@ -70,4 +75,93 @@ private:
 static GM* MyFactory(void*) { return new PointsGM; }
 static GMRegistry reg(MyFactory);
 
+}
+
+// Particle:
+// Position (x, y)
+// Color
+namespace {
+
+static constexpr int kMaxParticles = 4096;
+SkPoint pos[kMaxParticles];
+SkColor clr[kMaxParticles];
+int life[kMaxParticles];
+int nextFree[kMaxParticles];
+int firstFree = 0;
+}
+
+DEF_SIMPLE_GM_BG(particles, canvas, 500, 500, SK_ColorWHITE) {
+    static bool initialized = false;
+    static SkScalar totalLength = 0;
+    static SkTArray<SkPath> contours;
+    static SkTArray<SkScalar> lengths;
+    static SkRandom rand;
+
+    if (!initialized) {
+        initialized = true;
+
+        for (int i = 0; i < kMaxParticles; ++i) {
+            pos[i] = SkPoint::Make(SK_ScalarInfinity, SK_ScalarInfinity);
+            life[i] = 0;
+            nextFree[i] = i + 1;
+            clr[i] = SK_ColorBLUE;
+        }
+        nextFree[kMaxParticles - 1] = -1;
+
+        const char* msg = "Hello World";
+        SkFont font(sk_tool_utils::create_portable_typeface());
+        font.setSize(36);
+
+        SkPath path;
+        SkTextUtils::GetPath(msg, strlen(msg), kUTF8_SkTextEncoding, 0, 0, font, &path);
+
+        SkPathMeasure pm(path, false);
+        do {
+            SkPath contour;
+            if (pm.getSegment(0, SK_ScalarInfinity, &contour, true)) {
+                contours.push_back(contour);
+                totalLength += pm.getLength();
+                lengths.push_back(pm.getLength());
+            }
+        } while (pm.nextContour());
+    }
+
+    const int kNewPerFrame = 180;
+    const int kMinLifetime = 10;
+    const int kMaxLifetime = 20;
+
+    // Age old particles
+    for (int i = 0; i < kMaxParticles; ++i) {
+        if (life[i] == 1) {
+            pos[i] = SkPoint::Make(SK_ScalarInfinity, SK_ScalarInfinity);
+            life[i] = 0;
+            nextFree[i] = firstFree;
+            firstFree = i;
+        } else if (life[i]) {
+            --life[i];
+            pos[i].fY -= rand.nextRangeScalar(0.5f, 1.5f);
+            pos[i].fX += rand.nextRangeScalar(-0.5f, 0.5f);
+        }
+    }
+
+    // Spawn new particles
+    for (int i = 0; i < kNewPerFrame && firstFree >= 0; ++i) {
+        int idx = firstFree;
+        firstFree = nextFree[idx];
+
+        life[idx] = rand.nextRangeU(kMinLifetime, kMaxLifetime);
+        SkScalar len = rand.nextRangeScalar(0, totalLength);
+        int c = 0;
+        while (c < contours.count() && len > lengths[c]) {
+            len -= lengths[c++];
+        }
+        SkPathMeasure cm(contours[c], false);
+        cm.getPosTan(len, pos + idx, nullptr);
+    }
+
+    SkPaint paint;
+    paint.setColor(SK_ColorBLUE);
+    paint.setStrokeCap(SkPaint::kRound_Cap);
+
+    canvas->drawPoints(SkCanvas::kPoints_PointMode, kMaxParticles, pos, paint);
 }
