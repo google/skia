1a977a38ac1e32e5243982591855d405b96167ae
Mike Reed <reed@google.com>
2019-01-05 16:35:13 -0500

    add drawString to ease the pain of converting to skfont
    Bug: skia:
    Change-Id: I88418f87883b66204427fbdab471e74c78835ac4

--- gm/colorwheel.cpp
+++ gm/colorwheel.cpp
@@ -40,22 +40,22 @@ DEF_SIMPLE_GM(colorwheel, canvas, 256, 256) {
 
 DEF_SIMPLE_GM(colorwheelnative, canvas, 128, 28) {
     SkPaint paint;
-    sk_tool_utils::set_portable_typeface(&paint, "sans-serif", SkFontStyle::Bold());
-    paint.setTextSize(18.0f);
+    SkFont font(sk_tool_utils::create_portable_typeface("sans-serif", SkFontStyle::Bold()), 18);
+    font.setEdging(SkFont::Edging::kAlias);
 
     canvas->clear(SK_ColorLTGRAY);
     paint.setColor(SK_ColorRED);
-    canvas->drawString("R", 8.0f, 20.0f, paint);
+    canvas->drawString("R", 8.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorGREEN);
-    canvas->drawString("G", 24.0f, 20.0f, paint);
+    canvas->drawString("G", 24.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorBLUE);
-    canvas->drawString("B", 40.0f, 20.0f, paint);
+    canvas->drawString("B", 40.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorCYAN);
-    canvas->drawString("C", 56.0f, 20.0f, paint);
+    canvas->drawString("C", 56.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorMAGENTA);
-    canvas->drawString("M", 72.0f, 20.0f, paint);
+    canvas->drawString("M", 72.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorYELLOW);
-    canvas->drawString("Y", 88.0f, 20.0f, paint);
+    canvas->drawString("Y", 88.0f, 20.0f, font, paint);
     paint.setColor(SK_ColorBLACK);
-    canvas->drawString("K", 104.0f, 20.0f, paint);
+    canvas->drawString("K", 104.0f, 20.0f, font, paint);
 }
