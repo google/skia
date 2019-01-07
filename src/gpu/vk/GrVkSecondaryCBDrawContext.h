3d92a49a5e8b4482bf8c2f8d338958d972477f7e
Greg Daniel <egdaniel@google.com>
2019-01-07 12:59:03 -0500

    Add wait function to GrVkSecondaryCBDrawContext for semaphores.
    Bug: skia:
    Change-Id: Icd4875fa8fa495f9ce02135bc0a8c37608ed09df
    Reviewed-on: https://skia-review.googlesource.com/c/181661
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

--- src/gpu/vk/GrVkSecondaryCBDrawContext.h
+++ src/gpu/vk/GrVkSecondaryCBDrawContext.h
@@ -11,6 +11,7 @@
 #include "SkTypes.h"
 #include "SkRefCnt.h"
 
+class GrBackendSemaphore;
 class GrContext;
 struct GrVkDrawableInfo;
 class SkCanvas;
@@ -66,6 +67,21 @@ public:
     // Records all the draws to the imported secondary command buffer and sets any dependent
     // offscreen draws to the GPU.
     void flush();
+
+    /** Inserts a list of GPU semaphores that Skia will have the driver wait on before executing
+        commands for this secondary CB. The wait semaphores will get added to the VkCommandBuffer
+        owned by this GrContext when flush() is called, and not the command buffer which the
+        Secondary CB is from. This will guarantee that the driver waits on the semaphores before
+        the secondary command buffer gets executed. Skia will take ownership of the underlying
+        semaphores and delete them once they have been signaled and waited on. If this call returns
+        false, then the GPU back-end will not wait on any passed in semaphores, and the client will
+        still own the semaphores.
+
+        @param numSemaphores   size of waitSemaphores array
+        @param waitSemaphores  array of semaphore containers
+        @return                true if GPU is waiting on semaphores
+    */
+    bool wait(int numSemaphores, const GrBackendSemaphore waitSemaphores[]);
 
     // This call will release all resources held by the draw context. The client must call
     // releaseResources() before deleting the drawing context. However, the resources also include
