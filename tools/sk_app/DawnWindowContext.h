795af8f3b10b84a3ab200bd04db86e176dc8761c
Stephen White <senorblanco@chromium.org>
2019-01-07 17:13:55 -0500

    Dawn backend (experimental).
    Change-Id: If03831fc4c542d1afe524c806c930ce88a3f23c0

---
+++ tools/sk_app/DawnWindowContext.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef DawnWindowContext_DEFINED
+#define DawnWindowContext_DEFINED
+
+#include "SkRefCnt.h"
+#include "SkSurface.h"
+
+#include "WindowContext.h"
+#include "dawn/dawncpp.h"
+#include "dawn/dawn_wsi.h"
+
+class GrContext;
+
+namespace sk_app {
+
+class DawnWindowContext : public WindowContext {
+public:
+    DawnWindowContext(const DisplayParams&, dawn::TextureFormat swapChainFormat);
+    ~DawnWindowContext() override;
+    sk_sp<SkSurface> getBackbufferSurface() override;
+    void swapBuffers() override;
+    bool isValid() override { return SkToBool(fBackendContext.get()); }
+
+    void resize(int w, int h) override;
+
+    void setDisplayParams(const DisplayParams& params) override;
+
+protected:
+    bool isGpuContext() override { return true; }
+    void initializeContext(int width, int height);
+    virtual sk_sp<GrDawnBackendContext> onInitializeContext() = 0;
+    virtual void onDestroyContext() = 0;
+    virtual void onSwapBuffers() = 0;
+    void destroyContext();
+    virtual dawnSwapChainImplementation createSwapChainImplementation(int width, int height, const DisplayParams& params) = 0;
+
+    sk_sp<GrDawnBackendContext>    fBackendContext;
+    sk_sp<SkSurface>              fSurface;
+    dawnSwapChainImplementation   fSwapChainImplementation;
+    dawn::TextureFormat           fSwapChainFormat;
+    dawn::SwapChain               fSwapChain;
+    dawn::Device                  fDevice;
+};
+
+}   // namespace sk_app
+
+#endif
