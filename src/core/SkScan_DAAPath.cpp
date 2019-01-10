2dd08aa4adc9894656c0f0db8eee3d585e588a35
Mike Klein <mtklein@google.com>
2019-01-10 14:04:52 -0500

    remove templating on isPartial
    Instead of templating this, check at runtime.  It's very predictable.
    
    Logically we could just go further and do the multiply---it's certainly
    not much work---but SkFixedMul_lowprec() is indeed pretty low-precision.
    Always calling it causes more diffs that I'd rather just avoid for now.
    
    Even with this version of the CL there are still some diffs here... must
    be that some of the "partial" rows are actually full rows.  Hopefully
    there are few enough that it can sneak through layout tests etc.
    
    Rename some static functions to match our usual style.
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I7ad790cdac2c5f68d2e9f9f7d5fe42a0307e8388

--- src/core/SkScan_DAAPath.cpp
+++ src/core/SkScan_DAAPath.cpp
@@ -47,39 +47,31 @@ don't ever have to worry about under/overflow.
 
 */
 
-///////////////////////////////////////////////////////////////////////////////
-
-// The following helper functions are the same as those from SkScan_AAAPath.cpp
-// except that we use SkFixed everywhere instead of SkAlpha.
-
 static inline SkFixed SkFixedMul_lowprec(SkFixed a, SkFixed b) {
     return (a >> 8) * (b >> 8);
 }
 
 // Return the alpha of a trapezoid whose height is 1
-static inline SkFixed trapezoidToAlpha(SkFixed l1, SkFixed l2) {
+static inline SkFixed trapezoid_to_alpha(SkFixed l1, SkFixed l2) {
     SkASSERT(l1 >= 0 && l2 >= 0);
     return (l1 + l2) >> 1;
 }
 
 // The alpha of right-triangle (a, a*b)
-static inline SkFixed partialTriangleToAlpha(SkFixed a, SkFixed b) {
+static inline SkFixed partial_triangle_to_alpha(SkFixed a, SkFixed b) {
     SkASSERT(a <= SK_Fixed1);
     // SkFixedMul(SkFixedMul(a, a), b) >> 1
     // return ((((a >> 8) * (a >> 8)) >> 8) * (b >> 8)) >> 1;
     return (a >> 11) * (a >> 11) * (b >> 11);
 }
 
-static inline SkFixed getPartialAlpha(SkFixed alpha, SkFixed partialMultiplier) {
-    // DAA should not be so sensitive to the precision (compared to AAA).
-    return SkFixedMul_lowprec(alpha, partialMultiplier);
-}
 
-///////////////////////////////////////////////////////////////////////////////
-
-template<bool isPartial, class Deltas>
-static inline void add_coverage_delta_segment(int y, SkFixed rowHeight, const SkAnalyticEdge* edge,
-        SkFixed nextX, Deltas* deltas) { // rowHeight=fullAlpha
+template <class Deltas>
+static inline void add_coverage_delta_segment(int y,
+                                              SkFixed rowHeight,
+                                              const SkAnalyticEdge* edge,
+                                              SkFixed nextX,
+                                              Deltas* deltas) {
     SkASSERT(rowHeight <= SK_Fixed1 && rowHeight >= 0);
 
     // Let's see if multiplying sign is faster than multiplying edge->fWinding.
@@ -87,7 +79,7 @@ static inline void add_coverage_delta_segment(int y, SkFixed rowHeight, const Sk
     int sign = edge->fWinding == 1 ? 1 : -1;
 
     SkFixed l = SkTMin(edge->fX, nextX);
-    SkFixed r = edge->fX + nextX - l;
+    SkFixed r = edge->fX + nextX - l;  // a.k.a. SkTMax(edge->fX, nextX).
     int     L = SkFixedFloorToInt(l);
     int     R = SkFixedCeilToInt(r);
     int     len = R - L;
@@ -99,11 +91,11 @@ static inline void add_coverage_delta_segment(int y, SkFixed rowHeight, const Sk
         }
         case 1: {
             SkFixed fixedR  = SkIntToFixed(R);
-            SkFixed alpha   = trapezoidToAlpha(fixedR - l, fixedR - r);
-            if (isPartial) {
-                alpha = getPartialAlpha(alpha, rowHeight);
+            SkFixed alpha   = trapezoid_to_alpha(fixedR - l, fixedR - r);
+            if (rowHeight < SK_Fixed1) {
+                alpha = SkFixedMul_lowprec(alpha, rowHeight);
             }
-            deltas->addDelta(L,     y,  alpha * sign);
+            deltas->addDelta(L,     y,  (            alpha) * sign);
             deltas->addDelta(L + 1, y,  (rowHeight - alpha) * sign);
             return;
         }
@@ -111,8 +103,8 @@ static inline void add_coverage_delta_segment(int y, SkFixed rowHeight, const Sk
             SkFixed middle  = SkIntToFixed(L + 1);
             SkFixed x1      = middle - l;
             SkFixed x2      = r - middle;
-            SkFixed alpha1  = partialTriangleToAlpha(x1, edge->fDY);
-            SkFixed alpha2  = rowHeight - partialTriangleToAlpha(x2, edge->fDY);
+            SkFixed alpha1  = partial_triangle_to_alpha(x1, edge->fDY);
+            SkFixed alpha2  = rowHeight - partial_triangle_to_alpha(x2, edge->fDY);
             deltas->addDelta(L,     y,  alpha1 * sign);
             deltas->addDelta(L + 1, y,  (alpha2 - alpha1) * sign);
             deltas->addDelta(L + 2, y,  (rowHeight - alpha2) * sign);
@@ -136,8 +128,8 @@ static inline void add_coverage_delta_segment(int y, SkFixed rowHeight, const Sk
         deltas->addDelta(L + i, y, dY * sign); // the increment is always a rect of height = dY
     }
 
-    SkFixed alphaR2     = alpha1 + dY * (len - 3);                      // the alpha at R - 2
-    SkFixed lastAlpha   = rowHeight - partialTriangleToAlpha(last, dY); // the alpha at R - 1
+    SkFixed alphaR2     = alpha1 + dY * (len - 3);                         // the alpha at R - 2
+    SkFixed lastAlpha   = rowHeight - partial_triangle_to_alpha(last, dY); // the alpha at R - 1
     deltas->addDelta(R - 1, y, (lastAlpha - alphaR2) * sign);
     deltas->addDelta(R,     y, (rowHeight - lastAlpha) * sign);
 }
@@ -275,7 +267,7 @@ void gen_alpha_deltas(const SkPath& path, const SkIRect& clippedIR, const SkIRec
                 SkFixed rowHeight = currE->fLowerY - currE->fUpperY;
                 SkFixed nextX = currE->fX + SkFixedMul(currE->fDX, rowHeight);
                 if (iy >= clippedIR.fTop && iy < clippedIR.fBottom) {
-                    add_coverage_delta_segment<true>(iy, rowHeight, currE, nextX, &result);
+                    add_coverage_delta_segment(iy, rowHeight, currE, nextX, &result);
                 }
                 continue;
             }
@@ -285,7 +277,7 @@ void gen_alpha_deltas(const SkPath& path, const SkIRect& clippedIR, const SkIRec
             SkFixed nextX;
             if (rowHeight != SK_Fixed1) {   // it's a partial row
                 nextX = currE->fX + SkFixedMul(currE->fDX, rowHeight);
-                add_coverage_delta_segment<true>(iy, rowHeight, currE, nextX, &result);
+                add_coverage_delta_segment(iy, rowHeight, currE, nextX, &result);
             } else {                        // it's a full row so we can leave it to the while loop
                 iy--;                       // compensate the iy++ in the while loop
                 nextX = currE->fX;
@@ -309,7 +301,7 @@ void gen_alpha_deltas(const SkPath& path, const SkIRect& clippedIR, const SkIRec
                 }
 
                 // Add current edge's coverage deltas on this full row
-                add_coverage_delta_segment<false>(iy, SK_Fixed1, currE, nextX, &result);
+                add_coverage_delta_segment(iy, SK_Fixed1, currE, nextX, &result);
             }
 
             // last partial row
@@ -317,7 +309,7 @@ void gen_alpha_deltas(const SkPath& path, const SkIRect& clippedIR, const SkIRec
                     iy >= clippedIR.fTop && iy < clippedIR.fBottom) {
                 rowHeight = currE->fLowerY - SkIntToFixed(iy);
                 nextX = currE->fX + SkFixedMul(currE->fDX, rowHeight);
-                add_coverage_delta_segment<true>(iy, rowHeight, currE, nextX, &result);
+                add_coverage_delta_segment(iy, rowHeight, currE, nextX, &result);
             }
         // Intended assignment to fWinding to restore the maybe-negated winding (during updateLine)
         } while ((currE->fWinding = originalWinding) && currE->update(currE->fLowerY, sortY));
