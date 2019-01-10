e1b29fd5e9c2a59c5ac470ebddd62d4b778867ef
Brian Osman <brianosman@google.com>
2019-01-10 10:51:08 -0500

    Goofy experiment
    Bug: skia:
    Change-Id: Id9402bef22825d55d021c5a2f9e5e41791aabaf4

--- tools/viewer/Viewer.cpp
+++ tools/viewer/Viewer.cpp
@@ -10,6 +10,7 @@
 #include "GrContext.h"
 #include "GrContextPriv.h"
 #include "ImageSlide.h"
+#include "ParticlesSlide.h"
 #include "Resources.h"
 #include "SKPSlide.h"
 #include "SampleSlide.h"
@@ -615,6 +616,12 @@ void Viewer::initSlides() {
         if (!SkCommandLineFlags::ShouldSkip(FLAGS_match, slide->getName().c_str())) {
             fSlides.push_back(slide);
         }
+    }
+
+    // Particle demo
+    {
+        sk_sp<Slide> slide(new ParticlesSlide());
+        fSlides.push_back(std::move(slide));
     }
 
     for (const auto& info : gExternalSlidesInfo) {
@@ -1865,6 +1872,7 @@ void Viewer::drawImGui() {
                 this->updateTitle();
             });
         }
+
         ImGui::End();
     }
 
