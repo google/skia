6944788884a9636ed370f1e5b24d1f820117fcc1
Derek Sollenberger <djsollen@google.com>
2019-01-10 11:25:03 -0500

    Add additional information (e.g. colorspace) to the GpuDrawHandler
    Bug: b/115613038
    Change-Id: I375fc5f1bac13557b35e39658d678c61f70024c8

--- include/core/SkDrawable.h
+++ include/core/SkDrawable.h
@@ -9,6 +9,7 @@
 #define SkDrawable_DEFINED
 
 #include "SkFlattenable.h"
+#include "SkImageInfo.h"
 #include "SkScalar.h"
 
 class GrBackendDrawableInfo;
@@ -74,13 +75,14 @@ public:
     /**
      * Snaps off a GpuDrawHandler to represent the state of the SkDrawable at the time the snap is
      * called. This is used for executing GPU backend specific draws intermixed with normal Skia GPU
-     * draws. The GPU API, which will be used for the draw, as well as the full matrix and device
-     * clip bounds are passed in as inputs.
+     * draws. The GPU API, which will be used for the draw, as well as the full matrix, device clip
+     * bounds and imageInfo of the target buffer are passed in as inputs.
      */
     std::unique_ptr<GpuDrawHandler> snapGpuDrawHandler(GrBackendApi backendApi,
                                                        const SkMatrix& matrix,
-                                                       const SkIRect& clipBounds) {
-        return this->onSnapGpuDrawHandler(backendApi, matrix, clipBounds);
+                                                       const SkIRect& clipBounds,
+                                                       const SkImageInfo& bufferInfo) {
+        return this->onSnapGpuDrawHandler(backendApi, matrix, clipBounds, bufferInfo);
     }
 
     SkPicture* newPictureSnapshot();
@@ -133,7 +135,8 @@ protected:
     virtual void onDraw(SkCanvas*) = 0;
 
     virtual std::unique_ptr<GpuDrawHandler> onSnapGpuDrawHandler(GrBackendApi, const SkMatrix&,
-                                                                 const SkIRect& /*clipBounds*/) {
+                                                                 const SkIRect& /*clipBounds*/,
+                                                                 const SkImageInfo&) {
         return nullptr;
     }
 
