6bd9e10c925a376038d3880c316f3dd04cd51ebc
Mike Reed <reed@google.com>
2019-01-14 16:52:17 -0500

    unflatten directly to font for legacy skps
    All of this extra code/parameter-passing for fonts to the paint unflattener
    can go away when we can drop support for older skps
    
    Bug: skia:
    Change-Id: I4d626f0925a54564b7c386ed80953a0291c8bc1a

--- src/core/SkReadBuffer.h
+++ src/core/SkReadBuffer.h
@@ -112,7 +112,10 @@ public:
     void readRegion(SkRegion* region);
 
     void readPath(SkPath* path);
-    bool readPaint(SkPaint* paint) { return SkPaintPriv::Unflatten(paint, *this); }
+
+    SkReadPaintResult readPaint(SkPaint* paint, SkFont* font) {
+        return SkPaintPriv::Unflatten(paint, *this, font);
+    }
 
     SkFlattenable* readFlattenable(SkFlattenable::Type);
     template <typename T> sk_sp<T> readFlattenable() {
@@ -288,7 +291,13 @@ public:
     void readRegion (SkRegion*  out) { *out = SkRegion();         }
     void readString (SkString*  out) { *out = SkString();         }
     void readPath   (SkPath*    out) { *out = SkPath();           }
-    bool readPaint  (SkPaint*   out) { *out = SkPaint(); return false; }
+    SkReadPaintResult readPaint  (SkPaint*   out, SkFont* font) {
+        *out = SkPaint();
+        if (font) {
+            *font = SkFont();
+        }
+        return kFailed_ReadPaint;
+    }
 
     SkPoint readPoint() { return {0,0}; }
 
