76ccba08af5158512d82ef2b2141b31e1b24a492
Ben Wagner <benjaminwagner@google.com>
2019-01-15 13:13:54 -0500

    Try DDL on Intel
    Bug: skia:
    Change-Id: I8575517acbc9070b744ea04f6665562b16202b09

--- src/gpu/vk/GrVkPipeline.cpp
+++ src/gpu/vk/GrVkPipeline.cpp
@@ -13,6 +13,16 @@
 #include "GrVkGpu.h"
 #include "GrVkRenderTarget.h"
 #include "GrVkUtil.h"
+#include <atomic>
+
+static std::atomic<int32_t> gPipelineCnt;
+
+// static
+void GrVkPipeline::assertZeroCount() {
+    int pipelineCnt = gPipelineCnt.load(std::memory_order_acquire);
+    SkASSERTF(pipelineCnt <= 0,
+              "Seems to be %d VkPipelines that haven't been destroyed.", pipelineCnt);
+}
 
 static inline VkFormat attrib_type_to_vkformat(GrVertexAttribType type) {
     switch (type) {
@@ -496,6 +506,15 @@ static void setup_dynamic_state(VkPipelineDynamicStateCreateInfo* dynamicInfo,
     dynamicInfo->pDynamicStates = dynamicStates;
 }
 
+static void wrappedCreate(GrVkGpu* gpu, VkPipelineCache cache, VkGraphicsPipelineCreateInfo* pipelineCreateInfo, VkPipeline* vkPipeline) {
+    gPipelineCnt.fetch_add(+1, std::memory_order_relaxed);
+    VkResult err = GR_VK_CALL(gpu->vkInterface(), CreateGraphicsPipelines(gpu->device(),
+                                                                          cache, 1,
+                                                                          pipelineCreateInfo,
+                                                                          nullptr, vkPipeline));
+    SkASSERTF(!err, "Failed to create pipeline. Error: %d\n", err);
+}
+
 GrVkPipeline* GrVkPipeline::Create(GrVkGpu* gpu, const GrPrimitiveProcessor& primProc,
                                    const GrPipeline& pipeline, const GrStencilSettings& stencil,
                                    VkPipelineShaderStageCreateInfo* shaderStageInfo,
@@ -557,20 +576,15 @@ GrVkPipeline* GrVkPipeline::Create(GrVkGpu* gpu, const GrPrimitiveProcessor& pri
     pipelineCreateInfo.basePipelineIndex = -1;
 
     VkPipeline vkPipeline;
-    VkResult err = GR_VK_CALL(gpu->vkInterface(), CreateGraphicsPipelines(gpu->device(),
-                                                                          cache, 1,
-                                                                          &pipelineCreateInfo,
-                                                                          nullptr, &vkPipeline));
-    if (err) {
-        SkDebugf("Failed to create pipeline. Error: %d\n", err);
-        return nullptr;
-    }
-
+    wrappedCreate(gpu, cache, &pipelineCreateInfo, &vkPipeline);
     return new GrVkPipeline(vkPipeline);
 }
 
 void GrVkPipeline::freeGPUData(GrVkGpu* gpu) const {
+    SkASSERT(!freed);
     GR_VK_CALL(gpu->vkInterface(), DestroyPipeline(gpu->device(), fPipeline, nullptr));
+    gPipelineCnt.fetch_add(-1, std::memory_order_acq_rel);
+    freed = true;
 }
 
 void GrVkPipeline::SetDynamicScissorRectState(GrVkGpu* gpu,
