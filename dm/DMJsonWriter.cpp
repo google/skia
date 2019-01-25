aae638807ca3276b6e9ec493caaa0d382df4970c
Brian Osman <brianosman@google.com>
2019-01-25 10:02:27 -0500

    Switch DM JSON output to use SkJSONWriter
    Bug: skia:
    Change-Id: I3ceccd050a294260da54ff8a01a4837eb0cfb369
    Reviewed-on: https://skia-review.googlesource.com/c/186866
    Reviewed-by: Mike Klein <mtklein@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- dm/DMJsonWriter.cpp
+++ dm/DMJsonWriter.cpp
@@ -11,6 +11,7 @@
 #include "SkCommonFlags.h"
 #include "SkData.h"
 #include "SkJSONCPP.h"
+#include "SkJSONWriter.h"
 #include "SkMutex.h"
 #include "SkOSFile.h"
 #include "SkOSPath.h"
@@ -40,58 +41,78 @@ void JsonWriter::DumpJson() {
         return;
     }
 
-    Json::Value root;
+    SkString path = SkOSPath::Join(FLAGS_writePath[0], "dm.json");
+    sk_mkdir(FLAGS_writePath[0]);
+    SkFILEWStream stream(path.c_str());
+    SkJSONWriter writer(&stream, SkJSONWriter::Mode::kPretty);
+
+    writer.beginObject(); // root
 
     for (int i = 1; i < FLAGS_properties.count(); i += 2) {
-        root[FLAGS_properties[i-1]] = FLAGS_properties[i];
+        writer.appendString(FLAGS_properties[i-1], FLAGS_properties[i]);
     }
+
+    writer.beginObject("key");
     for (int i = 1; i < FLAGS_key.count(); i += 2) {
-        root["key"][FLAGS_key[i-1]] = FLAGS_key[i];
+        writer.appendString(FLAGS_key[i-1], FLAGS_key[i]);
+    }
+    writer.endObject();
+
+    int maxResidentSetSizeMB = sk_tools::getMaxResidentSetSizeMB();
+    if (maxResidentSetSizeMB != -1) {
+        writer.appendS32("max_rss_MB", maxResidentSetSizeMB);
     }
 
     {
         SkAutoMutexAcquire lock(&gBitmapResultLock);
+        writer.beginArray("results");
         for (int i = 0; i < gBitmapResults.count(); i++) {
-            Json::Value result;
-            result["key"]["name"]              = gBitmapResults[i].name.c_str();
-            result["key"]["config"]            = gBitmapResults[i].config.c_str();
-            result["key"]["source_type"]       = gBitmapResults[i].sourceType.c_str();
-            result["options"]["ext"]           = gBitmapResults[i].ext.c_str();
-            result["options"]["gamma_correct"] = gBitmapResults[i].gammaCorrect ? "yes" : "no";
-            result["md5"]                      = gBitmapResults[i].md5.c_str();
+            writer.beginObject();
+
+            writer.beginObject("key");
+            writer.appendString("name"       , gBitmapResults[i].name.c_str());
+            writer.appendString("config"     , gBitmapResults[i].config.c_str());
+            writer.appendString("source_type", gBitmapResults[i].sourceType.c_str());
 
             // Source options only need to be part of the key if they exist.
             // Source type by source type, we either always set options or never set options.
             if (!gBitmapResults[i].sourceOptions.isEmpty()) {
-                result["key"]["source_options"] = gBitmapResults[i].sourceOptions.c_str();
+                writer.appendString("source_options", gBitmapResults[i].sourceOptions.c_str());
             }
+            writer.endObject(); // key
 
-            root["results"].append(result);
+            writer.beginObject("options");
+            writer.appendString("ext"          , gBitmapResults[i].ext.c_str());
+            writer.appendString("gamma_correct", gBitmapResults[i].gammaCorrect ? "yes" : "no");
+            writer.endObject(); // options
+
+            writer.appendString("md5", gBitmapResults[i].md5.c_str());
+
+            writer.endObject(); // 1 result
         }
+        writer.endArray(); // results
     }
 
     {
         SkAutoMutexAcquire lock(gFailureLock);
-        for (int i = 0; i < gFailures.count(); i++) {
-            Json::Value result;
-            result["file_name"]     = gFailures[i].fileName;
-            result["line_no"]       = gFailures[i].lineNo;
-            result["condition"]     = gFailures[i].condition;
-            result["message"]       = gFailures[i].message.c_str();
-
-            root["test_results"]["failures"].append(result);
+        if (gFailures.count() > 0) {
+            writer.beginObject("test_results");
+            writer.beginArray("failures");
+            for (int i = 0; i < gFailures.count(); i++) {
+                writer.beginObject();
+                writer.appendString("file_name", gFailures[i].fileName);
+                writer.appendS32   ("line_no"  , gFailures[i].lineNo);
+                writer.appendString("condition", gFailures[i].condition);
+                writer.appendString("message"  , gFailures[i].message.c_str());
+                writer.endObject(); // 1 failure
+            }
+            writer.endArray(); // failures
+            writer.endObject(); // test_results
         }
     }
 
-    int maxResidentSetSizeMB = sk_tools::getMaxResidentSetSizeMB();
-    if (maxResidentSetSizeMB != -1) {
-        root["max_rss_MB"] = sk_tools::getMaxResidentSetSizeMB();
-    }
-
-    SkString path = SkOSPath::Join(FLAGS_writePath[0], "dm.json");
-    sk_mkdir(FLAGS_writePath[0]);
-    SkFILEWStream stream(path.c_str());
-    stream.writeText(Json::StyledWriter().write(root).c_str());
+    writer.endObject(); // root
+    writer.flush();
     stream.flush();
 }
 
