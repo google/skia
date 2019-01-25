8dfe74503a261de641b8bfda523ae1483269e5f9
Ben Wagner <benjaminwagner@google.com>
2019-01-25 17:38:08 -0500

    Try DDL and ASAN on Intel
    Bug: skia:8710
    Change-Id: I55c6e5d483b4aeed5cd44c64219539ca7214187f

--- tools/LsanSuppressions.cpp
+++ tools/LsanSuppressions.cpp
@@ -18,12 +18,12 @@ extern "C" {
     const char* __lsan_default_suppressions();
     const char* __lsan_default_suppressions() {
         return "leak:libfontconfig\n"       // FontConfig looks like it leaks, but it doesn't.
-               "leak:libGL.so\n"            // For NVidia driver.
-               "leak:libGLX_nvidia.so\n"    // For NVidia driver.
-               "leak:libnvidia-glcore.so\n" // For NVidia driver.
-               "leak:libnvidia-tls.so\n"    // For NVidia driver.
+               //"leak:libGL.so\n"            // For NVidia driver.
+               //"leak:libGLX_nvidia.so\n"    // For NVidia driver.
+               //"leak:libnvidia-glcore.so\n" // For NVidia driver.
+               //"leak:libnvidia-tls.so\n"    // For NVidia driver.
                "leak:libxcb.so\n"           // For Mesa driver. skia:8710
-               "leak:__strdup\n"            // An eternal mystery, skia:2916.
+               //"leak:__strdup\n"            // An eternal mystery, skia:2916.
                ;
     }
 
