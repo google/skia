4261fcce7784c4b342ce3d774a60f23183287380
Hal Canary <halcanary@google.com>
2019-01-25 12:50:35 -0500

    third_party/icu:  use upstream
        change DEPS
        simplify icu/BUILD.gn
        add icu/icu.gni
        add icu/icudt*l.dat binary data file
        script: icu/make_data_assembly.py
        script: icu/update_icu.sh for rebuilding
    
    Bug: skia: 8702
    Change-Id: I99789749ba84ae737f6c62475d0d676cd36715bb

--- third_party/icu/SkLoadICU.h
+++ third_party/icu/SkLoadICU.h
@@ -4,49 +4,9 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-#ifndef load_icu_DEFINED
-#define load_icu_DEFINED
+#ifndef SkLoadICU_DEFINED
+#define SkLoadICU_DEFINED
 
-#include "SkTypes.h"
+void SkLoadICU();
 
-#ifdef SK_BUILD_FOR_WIN
-
-#include "../private/SkLeanWindows.h"
-
-#include "unicode/uvernum.h"
-#include "unicode/udata.h"
-
-#define ICU_UTIL_DATA_SYMBOL "icudt" U_ICU_VERSION_SHORT "_dat"
-#define ICU_UTIL_DATA_SHARED_MODULE_NAME "icudt.dll"
-
-inline void SkLoadICU() {
-    HMODULE module = LoadLibraryA(ICU_UTIL_DATA_SHARED_MODULE_NAME);
-    if (!module) {
-        SK_ABORT("Failed to load " ICU_UTIL_DATA_SHARED_MODULE_NAME "\n");
-    }
-    FARPROC addr = GetProcAddress(module, ICU_UTIL_DATA_SYMBOL);
-    if (!addr) {
-        SK_ABORT("Symbol " ICU_UTIL_DATA_SYMBOL " missing in "
-                 ICU_UTIL_DATA_SHARED_MODULE_NAME ".\n");
-    }
-    UErrorCode err = U_ZERO_ERROR;
-    udata_setCommonData(reinterpret_cast<void*>(addr), &err);
-    if (err != U_ZERO_ERROR) {
-        SkDebugf("udata_setCommonData() returned %d.\n", (int)err);
-        SK_ABORT("");
-    }
-    udata_setFileAccess(UDATA_ONLY_PACKAGES, &err);
-    if (err != U_ZERO_ERROR) {
-        SkDebugf("udata_setFileAccess() returned %d.\n", (int)err);
-        SK_ABORT("");
-    }
-}
-
-#undef ICU_UTIL_DATA_SHARED_MODULE_NAME
-#undef ICU_UTIL_DATA_SYMBOL
-
-#else
-inline void SkLoadICU() {}
-#endif  // SK_BUILD_FOR_WIN
-#endif  // load_icu_DEFINED
-
+#endif  // SkLoadICU_DEFINED
