77aec5546f0a95e82356d750d9413530141c0d40
Ben Wagner <benjaminwagner@google.com>
2019-01-27 20:27:49 -0500

    Add DDL jobs for Intel
    To work around skia:8709, I'm moving the DDL tests to IntelIris640. The
    ASAN jobs still show memory leaks in the driver, but at least I can
    identify which calls are leaking and suppress the LSAN warnings.
    
    Adds a SK_HAS_UBSAN define that indicates that the LSAN header files
    are available. Adds __lsan::ScopedDisabler in four locations for two
    Vulkan API calls:
     - CreateGraphicsPipelines in GrVkPipeline::Create and
       GrVkCopyPipeline::Create
     - CreateDescriptorSetLayout in
       GrVkDescriptorSetManager::DescriptorPoolManager::DescriptorPoolManager
       constructor (2 calls)
    
    Also adds a regular suppression for libxcb.so.
    
    See bugs listed below for more details on the leaks.
    
    Bug: skia:8710, skia:8712, skia:8713
    Change-Id: I55c6e5d483b4aeed5cd44c64219539ca7214187f

--- src/gpu/vk/GrVkPipeline.cpp
+++ src/gpu/vk/GrVkPipeline.cpp
@@ -14,6 +14,10 @@
 #include "GrVkRenderTarget.h"
 #include "GrVkUtil.h"
 
+#if defined(SK_HAS_UBSAN)
+#include <sanitizer/lsan_interface.h>
+#endif
+
 static inline VkFormat attrib_type_to_vkformat(GrVertexAttribType type) {
     switch (type) {
         case kFloat_GrVertexAttribType:
@@ -557,10 +561,17 @@ GrVkPipeline* GrVkPipeline::Create(GrVkGpu* gpu, const GrPrimitiveProcessor& pri
     pipelineCreateInfo.basePipelineIndex = -1;
 
     VkPipeline vkPipeline;
-    VkResult err = GR_VK_CALL(gpu->vkInterface(), CreateGraphicsPipelines(gpu->device(),
-                                                                          cache, 1,
-                                                                          &pipelineCreateInfo,
-                                                                          nullptr, &vkPipeline));
+    VkResult err;
+    {
+#if defined(SK_HAS_UBSAN)
+        // skia:8712
+        __lsan::ScopedDisabler lsanDisabler;
+#endif
+        err = GR_VK_CALL(gpu->vkInterface(), CreateGraphicsPipelines(gpu->device(),
+                                                                     cache, 1,
+                                                                     &pipelineCreateInfo,
+                                                                     nullptr, &vkPipeline));
+    }
     if (err) {
         SkDebugf("Failed to create pipeline. Error: %d\n", err);
         return nullptr;
