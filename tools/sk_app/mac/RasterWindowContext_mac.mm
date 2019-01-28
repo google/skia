c2fec54fc7d454e9f9fde797dbfedfaaf07b0843
Jim Van Verth <jvanverth@google.com>
2019-01-28 14:46:04 -0500

    Switch sk_app to use native window creation on MacOS.
    Bug: skia:
    Change-Id: I1763aab0b4bdb650128c1fcc3aa3a05d194496ca

---
+++ tools/sk_app/mac/RasterWindowContext_mac.mm
@@ -0,0 +1,229 @@
+
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "../GLWindowContext.h"
+#include "SkCanvas.h"
+#include "SkColorFilter.h"
+#include "WindowContextFactory_mac.h"
+#include "gl/GrGLInterface.h"
+#include "sk_tool_utils.h"
+
+#include <OpenGL/gl.h>
+
+#include <Cocoa/Cocoa.h>
+
+using sk_app::DisplayParams;
+using sk_app::window_context_factory::MacWindowInfo;
+using sk_app::GLWindowContext;
+
+@interface RasterView : NSOpenGLView
+@end
+
+@implementation RasterView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // not sure why the parent isn't getting this, but we'll pass it up
+    [[self superview] drawRect:dirtyRect];
+}
+
+@end
+
+namespace {
+
+// TODO: This still uses GL to handle the update rather than using a purely raster backend,
+// for historical reasons. Writing a pure raster backend would be better in the long run.
+
+class RasterWindowContext_mac : public GLWindowContext {
+public:
+    RasterWindowContext_mac(const MacWindowInfo&, const DisplayParams&);
+
+    ~RasterWindowContext_mac() override;
+
+    sk_sp<SkSurface> getBackbufferSurface() override;
+
+    void onSwapBuffers() override;
+
+    sk_sp<const GrGLInterface> onInitializeContext() override;
+    void onDestroyContext() override;
+
+private:
+    NSView*              fMainView;
+    RasterView*          fRasterView;
+    NSOpenGLContext*     fGLContext;
+    NSOpenGLPixelFormat* fPixelFormat;
+    sk_sp<SkSurface>     fBackbufferSurface;
+
+    typedef GLWindowContext INHERITED;
+};
+
+RasterWindowContext_mac::RasterWindowContext_mac(const MacWindowInfo& info,
+                                                 const DisplayParams& params)
+    : INHERITED(params)
+    , fMainView(info.fMainView) {
+
+    // any config code here (particularly for msaa)?
+
+    this->initializeContext();
+}
+
+RasterWindowContext_mac::~RasterWindowContext_mac() {
+    this->destroyContext();
+}
+
+sk_sp<const GrGLInterface> RasterWindowContext_mac::onInitializeContext() {
+    SkASSERT(nil != fMainView);
+
+    // set up pixel format
+    constexpr int kMaxAttributes = 18;
+    NSOpenGLPixelFormatAttribute attributes[kMaxAttributes];
+    int numAttributes = 0;
+    attributes[numAttributes++] = NSOpenGLPFAAccelerated;
+    attributes[numAttributes++] = NSOpenGLPFAClosestPolicy;
+    attributes[numAttributes++] = NSOpenGLPFAOpenGLProfile;
+    attributes[numAttributes++] = NSOpenGLProfileVersion3_2Core;
+    attributes[numAttributes++] = NSOpenGLPFAColorSize;
+    attributes[numAttributes++] = 24;
+    attributes[numAttributes++] = NSOpenGLPFAAlphaSize;
+    attributes[numAttributes++] = 8;
+    attributes[numAttributes++] = NSOpenGLPFADepthSize;
+    attributes[numAttributes++] = 0;
+    attributes[numAttributes++] = NSOpenGLPFAStencilSize;
+    attributes[numAttributes++] = 8;
+    attributes[numAttributes++] = NSOpenGLPFADoubleBuffer;
+    if (fDisplayParams.fMSAASampleCount > 1) {
+        attributes[numAttributes++] = NSOpenGLPFASampleBuffers;
+        attributes[numAttributes++] = 1;
+        attributes[numAttributes++] = NSOpenGLPFASamples;
+        attributes[numAttributes++] = fDisplayParams.fMSAASampleCount;
+    } else {
+        attributes[numAttributes++] = NSOpenGLPFASampleBuffers;
+        attributes[numAttributes++] = 0;
+    }
+    attributes[numAttributes++] = 0;
+    SkASSERT(numAttributes <= kMaxAttributes);
+
+    fPixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
+    if (nil == fPixelFormat) {
+        return nullptr;
+    }
+
+    // create context
+    fGLContext = [[NSOpenGLContext alloc] initWithFormat:fPixelFormat shareContext:nil];
+    if (nil == fGLContext) {
+        [fPixelFormat release];
+        fPixelFormat = nil;
+        return nullptr;
+    }
+
+    // create view
+    NSRect rect = fMainView.bounds;
+    fRasterView = [[RasterView alloc] initWithFrame:rect];
+    if (nil == fRasterView) {
+        [fGLContext release];
+        fGLContext = nil;
+        [fPixelFormat release];
+        fPixelFormat = nil;
+        return nullptr;
+    }
+    [fRasterView setTranslatesAutoresizingMaskIntoConstraints:NO];
+
+    // attach OpenGL view to main view
+    [fMainView addSubview:fRasterView];
+    NSDictionary *views = NSDictionaryOfVariableBindings(fRasterView);
+
+    [fMainView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[fRasterView]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+
+    [fMainView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[fRasterView]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+
+    // make context current
+    GLint swapInterval = 1;
+    [fGLContext setValues:&swapInterval forParameter:NSOpenGLCPSwapInterval];
+    [fRasterView setOpenGLContext:fGLContext];
+    [fRasterView setPixelFormat:fPixelFormat];
+    [fRasterView setWantsBestResolutionOpenGLSurface:YES];
+    [fGLContext setView:fRasterView];
+
+    [fGLContext makeCurrentContext];
+
+    glClearStencil(0);
+    glClearColor(0, 0, 0, 0);
+    glStencilMask(0xffffffff);
+    glClear(GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+    GLint stencilBits;
+    [fPixelFormat getValues:&stencilBits forAttribute:NSOpenGLPFAStencilSize forVirtualScreen:0];
+    fStencilBits = stencilBits;
+    GLint sampleCount;
+    [fPixelFormat getValues:&sampleCount forAttribute:NSOpenGLPFASamples forVirtualScreen:0];
+    fSampleCount = sampleCount;
+    fSampleCount = SkTMax(fSampleCount, 1);
+
+    const NSRect backingRect = [fRasterView convertRectToBacking:fRasterView.bounds];
+    fWidth = backingRect.size.width;
+    fHeight = backingRect.size.height;
+    glViewport(0, 0, fWidth, fHeight);
+
+    // make the offscreen image
+    SkImageInfo info = SkImageInfo::Make(fWidth, fHeight, fDisplayParams.fColorType,
+                                         kPremul_SkAlphaType, fDisplayParams.fColorSpace);
+    fBackbufferSurface = SkSurface::MakeRaster(info);
+    return GrGLMakeNativeInterface();
+}
+
+void RasterWindowContext_mac::onDestroyContext() {
+    fBackbufferSurface.reset(nullptr);
+
+    [fRasterView removeFromSuperview];
+    [fRasterView release];
+    fRasterView = nil;
+    [fGLContext release];
+    fGLContext = nil;
+    [fPixelFormat release];
+    fPixelFormat = nil;
+}
+
+sk_sp<SkSurface> RasterWindowContext_mac::getBackbufferSurface() { return fBackbufferSurface; }
+
+void RasterWindowContext_mac::onSwapBuffers() {
+    if (fBackbufferSurface) {
+        // We made/have an off-screen surface. Get the contents as an SkImage:
+        sk_sp<SkImage> snapshot = fBackbufferSurface->makeImageSnapshot();
+
+        sk_sp<SkSurface> gpuSurface = INHERITED::getBackbufferSurface();
+        SkCanvas* gpuCanvas = gpuSurface->getCanvas();
+        gpuCanvas->drawImage(snapshot, 0, 0);
+        gpuCanvas->flush();
+
+        [fGLContext flushBuffer];
+    }
+}
+
+}  // anonymous namespace
+
+namespace sk_app {
+namespace window_context_factory {
+
+WindowContext* NewRasterForMac(const MacWindowInfo& info, const DisplayParams& params) {
+    WindowContext* ctx = new RasterWindowContext_mac(info, params);
+    if (!ctx->isValid()) {
+        delete ctx;
+        return nullptr;
+    }
+    return ctx;
+}
+
+}  // namespace window_context_factory
+}  // namespace sk_app
---
+++ tools/sk_app/mac/Window_mac.mm
@@ -0,0 +1,305 @@
+/*
+* Copyright 2019 Google Inc.
+*
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file.
+*/
+
+#include "SkUtils.h"
+#include "Timer.h"
+#include "WindowContextFactory_mac.h"
+#include "Window_mac.h"
+
+@interface MainView : NSView
+
+- (MainView*)initWithWindow:(sk_app::Window*)initWindow;
+
+@end
+
+@interface WindowDelegate : NSObject<NSWindowDelegate>
+
+- (WindowDelegate*)initWithWindow:(sk_app::Window*)initWindow;
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////
+
+using sk_app::Window;
+
+namespace sk_app {
+
+static int gWindowCount = 0;
+
+Window* Window::CreateNativeWindow(void*) {
+    Window_mac* window = new Window_mac();
+    if (!window->initWindow()) {
+        delete window;
+        return nullptr;
+    }
+
+    ++gWindowCount;
+    return window;
+}
+
+bool Window_mac::initWindow() {
+    if (fRequestedDisplayParams.fMSAASampleCount != fMSAASampleCount) {
+        this->closeWindow();
+    }
+
+    // we already have a window
+    if (fWindow) {
+        return true;
+    }
+
+    constexpr int initialWidth = 1280;
+    constexpr int initialHeight = 960;
+
+    NSUInteger windowStyle = (NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask |
+                              NSMiniaturizableWindowMask);
+
+    NSRect windowRect = NSMakeRect(100, 100, initialWidth, initialHeight);
+    fWindow = [[NSWindow alloc] initWithContentRect:windowRect styleMask:windowStyle
+                                backing:NSBackingStoreBuffered defer:NO];
+    if (nil == fWindow) {
+        return false;
+    }
+    WindowDelegate* delegate = [[WindowDelegate alloc] initWithWindow:this];
+    [fWindow setDelegate:delegate];
+    [delegate release];
+
+    // create view
+    MainView* view = [[[MainView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)] initWithWindow:this];
+    if (nil == view) {
+        [fWindow release];
+        fWindow = nil;
+        return false;
+    }
+
+    // attach view to window
+    [fWindow setContentView:view];
+
+    return true;
+}
+
+void Window_mac::closeWindow() {
+    [fWindow release];
+    fWindow = nil;
+}
+
+void Window_mac::setTitle(const char* title) {
+    NSString *titleString = [NSString stringWithCString:title encoding:NSUTF8StringEncoding];
+    [fWindow setTitle:titleString];
+}
+
+void Window_mac::show() {
+    [NSApp activateIgnoringOtherApps:YES];
+
+    [fWindow makeKeyAndOrderFront:NSApp];
+}
+
+bool Window_mac::attach(BackendType attachType) {
+    this->initWindow();
+
+    window_context_factory::MacWindowInfo info;
+    info.fMainView = this->view();
+    switch (attachType) {
+        case kRaster_BackendType:
+            fWindowContext = NewRasterForMac(info, fRequestedDisplayParams);
+            break;
+
+        case kNativeGL_BackendType:
+        default:
+            fWindowContext = NewGLForMac(info, fRequestedDisplayParams);
+            break;
+    }
+    this->onBackendCreated();
+
+    return (SkToBool(fWindowContext));
+}
+
+void Window_mac::onInval() {
+    [[fWindow contentView] setNeedsDisplay:YES];
+}
+
+}   // namespace sk_app
+
+///////////////////////////////////////////////////////////////////////////////
+
+@implementation WindowDelegate {
+    sk_app::Window* fWindow;
+}
+
+- (WindowDelegate*)initWithWindow:(sk_app::Window *)initWindow {
+    fWindow = initWindow;
+
+    return self;
+}
+
+- (void)windowDidResize:(NSNotification *)notification {
+    sk_app::Window_mac* macWindow = reinterpret_cast<sk_app::Window_mac*>(fWindow);
+    const NSRect mainRect = [macWindow->view() frame];
+    const NSRect backingRect = [macWindow->view() convertRectToBacking:mainRect];
+
+    fWindow->onResize(backingRect.size.width, backingRect.size.height);
+}
+
+- (BOOL)windowShouldClose:(NSWindow*)sender {
+    --sk_app::gWindowCount;
+    if (sk_app::gWindowCount < 1) {
+        [NSApp terminate:self];
+    }
+
+    reinterpret_cast<sk_app::Window_mac*>(fWindow)->closeWindow();
+
+    return FALSE;
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////
+
+@implementation MainView {
+    sk_app::Window* fWindow;
+}
+
+- (MainView*)initWithWindow:(sk_app::Window *)initWindow {
+    fWindow = initWindow;
+
+    return self;
+}
+
+- (BOOL)isOpaque {
+    return YES;
+}
+
+- (BOOL)canBecomeKeyView {
+    return YES;
+}
+
+- (BOOL)acceptsFirstResponder {
+    return YES;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    fWindow->onPaint();
+}
+
+static Window::Key get_key(unsigned short vk) {
+    // This will work with an ANSI QWERTY keyboard.
+    // Something more robust would be needed to support alternate keyboards.
+    static const struct {
+        unsigned short fVK;
+        Window::Key    fKey;
+    } gPair[] = {
+        { 0x33, Window::Key::kBack },
+        { 0x24, Window::Key::kOK },
+        { 0x7E, Window::Key::kUp },
+        { 0x7D, Window::Key::kDown },
+        { 0x7B, Window::Key::kLeft },
+        { 0x7C, Window::Key::kRight },
+        { 0x30, Window::Key::kTab },
+        { 0x74, Window::Key::kPageUp },
+        { 0x79, Window::Key::kPageDown },
+        { 0x73, Window::Key::kHome },
+        { 0x77, Window::Key::kEnd },
+        { 0x75, Window::Key::kDelete },
+        { 0x35, Window::Key::kEscape },
+        { 0x38, Window::Key::kShift },
+        { 0x3C, Window::Key::kShift },
+        { 0x3B, Window::Key::kCtrl },
+        { 0x3E, Window::Key::kCtrl },
+        { 0x3A, Window::Key::kOption },
+        { 0x3D, Window::Key::kOption },
+        { 0x00, Window::Key::kA },
+        { 0x08, Window::Key::kC },
+        { 0x09, Window::Key::kV },
+        { 0x07, Window::Key::kX },
+        { 0x10, Window::Key::kY },
+        { 0x06, Window::Key::kZ },
+    };
+    for (size_t i = 0; i < SK_ARRAY_COUNT(gPair); i++) {
+        if (gPair[i].fVK == vk) {
+            return gPair[i].fKey;
+        }
+    }
+
+    return Window::Key::kNONE;
+}
+
+static uint32_t get_modifiers(const NSEvent* event) {
+    NSUInteger modifierFlags = [event modifierFlags];
+    auto modifiers = 0;
+
+    if (modifierFlags & NSEventModifierFlagShift) {
+        modifiers |= Window::kShift_ModifierKey;
+    }
+    if (modifierFlags & NSEventModifierFlagControl) {
+        modifiers |= Window::kControl_ModifierKey;
+    }
+    if (modifierFlags & NSEventModifierFlagOption) {
+        modifiers |= Window::kOption_ModifierKey;
+    }
+
+    if ((NSKeyDown == [event type] || NSKeyUp == [event type]) &&
+        NO == [event isARepeat]) {
+        modifiers |= Window::kFirstPress_ModifierKey;
+    }
+
+    return modifiers;
+}
+
+- (void)keyDown:(NSEvent *)event {
+    Window::Key key = get_key([event keyCode]);
+    if (key != Window::Key::kNONE) {
+        if (!fWindow->onKey(key, Window::kDown_InputState, get_modifiers(event))) {
+            [[self superview] keyDown:event];
+        }
+    } else {
+        NSString* characters = [event charactersIgnoringModifiers];
+        if ([characters length] > 0) {
+            unichar firstChar = [characters characterAtIndex:0];
+            (void) fWindow->onChar((SkUnichar) firstChar, get_modifiers(event));
+        }
+    }
+}
+
+- (void)keyUp:(NSEvent *)event {
+    Window::Key key = get_key([event keyCode]);
+    if (key != Window::Key::kNONE) {
+        (void) fWindow->onKey(key, Window::kUp_InputState, get_modifiers(event));
+    }
+}
+
+- (void)mouseDown:(NSEvent *)event {
+    const NSPoint pos = [event locationInWindow];
+    const NSRect rect = [self frame];
+    fWindow->onMouse(pos.x, rect.size.height - pos.y, Window::kDown_InputState,
+                     get_modifiers(event));
+}
+
+- (void)mouseDragged:(NSEvent *)event {
+    [self mouseMoved:event];
+}
+
+- (void)mouseUp:(NSEvent *)event {
+    const NSPoint pos = [event locationInWindow];
+    const NSRect rect = [self frame];
+    fWindow->onMouse(pos.x, rect.size.height - pos.y, Window::kUp_InputState,
+                     get_modifiers(event));
+}
+
+- (void)mouseMoved:(NSEvent *)event {
+    const NSPoint pos = [event locationInWindow];
+    const NSRect rect = [self frame];
+    fWindow->onMouse(pos.x, rect.size.height - pos.y, Window::kMove_InputState,
+                     get_modifiers(event));
+}
+
+- (void)scrollWheel:(NSEvent *)event {
+    // TODO: support hasPreciseScrollingDeltas?
+    fWindow->onMouseWheel([event scrollingDeltaY], get_modifiers(event));
+}
+
+
+@end
+
