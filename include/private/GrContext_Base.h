75139762a120b96a209ef50ea82a3d2ac53111e2
Robert Phillips <robertphillips@google.com>
2019-01-30 12:46:02 -0500

    Add outline of new GrContext hierarchy
    This begins the process of splitting GrContext into:
    
    GrContext_Base, GrImageContext, GrRecordingContext and GrDirectContext.
    
    Change-Id: I3c43045f2a5549b049e95791d65f74d4e16de36f

---
+++ include/private/GrContext_Base.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrContext_Base_DEFINED
+#define GrContext_Base_DEFINED
+
+#include "SkRefCnt.h"
+#include "GrTypes.h"
+
+class GrBaseContextPriv;
+class GrContext;
+class GrImageContext;
+class GrRecordingContext;
+
+class SK_API GrContext_Base : public SkRefCnt {
+public:
+    virtual ~GrContext_Base();
+
+    /*
+     * The 3D API backing this context
+     */
+    GrBackendApi backend() const { return fBackend; }
+
+    /**
+     * An identifier for this context. The id is used by all compatible contexts. For example,
+     * if SkImages are created on one thread using an image creation context, then fed into a
+     * DDL Recorder on second thread (which has a recording context) and finally replayed on
+     * a third thread with a direct context, then all three contexts will report the same id.
+     * It is an error for an image to be used with contexts that report different ids.
+     */
+    uint32_t uniqueID() const { return fUniqueID; }
+
+    // Provides access to functions that aren't part of the public API.
+    GrBaseContextPriv priv();
+    const GrBaseContextPriv priv() const;
+
+protected:
+    friend class GrBaseContextPriv; // for hidden functions
+
+    GrContext_Base(GrBackendApi backend, uint32_t uniqueID);
+
+    GrContext_Base* asBaseContext() { return this; }
+    virtual GrImageContext* asImageContext() { return nullptr; }
+    virtual GrRecordingContext* asRecordingContext() { return nullptr; }
+    virtual GrContext* asDirectContext() { return nullptr; }
+
+private:
+    const GrBackendApi fBackend;
+    const uint32_t     fUniqueID;
+
+    typedef SkRefCnt INHERITED;
+};
+
+#endif
---
+++ src/gpu/GrImageContextPriv.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrImageContextPriv_DEFINED
+#define GrImageContextPriv_DEFINED
+
+#include "GrImageContext.h"
+
+/** Class that exposes methods on GrImageContext that are only intended for use internal to Skia.
+    This class is purely a privileged window into GrImageContext. It should never have
+    additional data members or virtual methods. */
+class GrImageContextPriv {
+public:
+    // from GrContext_Base
+
+    // from GrImageContext
+
+private:
+    explicit GrImageContextPriv(GrImageContext* context) : fContext(context) {}
+    GrImageContextPriv(const GrImageContextPriv&); // unimpl
+    GrImageContextPriv& operator=(const GrImageContextPriv&); // unimpl
+
+    // No taking addresses of this type.
+    const GrImageContextPriv* operator&() const;
+    GrImageContextPriv* operator&();
+
+    GrImageContext* fContext;
+
+    friend class GrImageContext; // to construct/copy this type.
+};
+
+inline GrImageContextPriv GrImageContext::priv() { return GrImageContextPriv(this); }
+
+inline const GrImageContextPriv GrImageContext::priv () const {
+    return GrImageContextPriv(const_cast<GrImageContext*>(this));
+}
+
+#endif
