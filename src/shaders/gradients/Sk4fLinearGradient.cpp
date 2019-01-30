6d4dda82ea0cfc919845bb66ca5d7734f409d62f
Mike Klein <mtklein@google.com>
2019-01-30 13:11:33 -0500

    remove shadeSpan4f()
    This shouldn't cause any diffs, provided I've implemented the no-stages
    shadeSpan() fallback correctly.  (I'm not sure which shaders don't have
    stages at this point.)
    
    Bug: skia:8730
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ia0d777b526e05b7262ff77daec5ef4d8b0afdb9b

--- src/shaders/gradients/Sk4fLinearGradient.cpp
+++ src/shaders/gradients/Sk4fLinearGradient.cpp
@@ -184,20 +184,6 @@ LinearGradient4fContext::shadeSpan(int x, int y, SkPMColor dst[], int count) {
     }
 }
 
-void SkLinearGradient::
-LinearGradient4fContext::shadeSpan4f(int x, int y, SkPMColor4f dst[], int count) {
-    SkASSERT(count > 0);
-
-    // 4f dests are dithered at a later stage, if needed.
-    static constexpr float bias0 = 0,
-                           bias1 = 0;
-    if (fColorsArePremul) {
-        this->shadePremulSpan<SkPMColor4f, ApplyPremul::False>(x, y, dst, count, bias0, bias1);
-    } else {
-        this->shadePremulSpan<SkPMColor4f, ApplyPremul::True >(x, y, dst, count, bias0, bias1);
-    }
-}
-
 template<typename dstType, ApplyPremul premul>
 void SkLinearGradient::
 LinearGradient4fContext::shadePremulSpan(int x, int y, dstType dst[], int count,
