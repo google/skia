f5e5db1168bd9343bc61ca1eea7f28d491c79fae
Mike Reed <reed@google.com>
2019-01-31 13:55:07 -0500

    fix tiler to handle large float rects
    The issue is around needing to go from a float rect to a clipped (intersected
    with the clip bounds) irect.
    
    The old code promoted the clip bounds to rect,
    intersected with the rect, and then rounded to ints. This is problematic
    since convert float --> int can produce float values that are larger than
    their int counter-parts. (e.g. int:67108863 becomes float:67108864).
    This error goes on to confuse subsequent logic.
    
    The fix is to round the rect to int first (knowing that very large floats
    will saturate to maxint), and then intersect.
    
    Bug: 927075
    Change-Id: If6c7d72ff15184700b87532e1922e576d1348ee8

--- tests/RectTest.cpp
+++ tests/RectTest.cpp
@@ -160,3 +160,19 @@ DEF_TEST(Rect_center, reporter) {
     REPORTER_ASSERT(reporter, !SkScalarIsFinite(r.height()));
 }
 
+#include "SkSurface.h"
+DEF_TEST(big_tiled_rect_crbug_927075, reporter) {
+    const int w = 67108863;
+    const int h = 1;
+    const auto info = SkImageInfo::MakeN32Premul(w, h);
+
+    auto surf = SkSurface::MakeRaster(info);
+    auto canvas = surf->getCanvas();
+
+    const SkRect r = { 257, 213, 67109120, 214 };
+    SkPaint paint;
+    paint.setAntiAlias(true);
+
+    canvas->translate(-r.fLeft, -r.fTop);
+    canvas->drawRect(r, paint);
+}
