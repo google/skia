54fbdd7a28f8e9f6442c2529114716782fce2e0d
Kevin Lubick <kjlubick@google.com>
2019-01-31 07:59:47 -0500

    POC docker container to build cmake
    Bug: skia:8732
    Change-Id: Ib84a46b4f0ca7f88fca4e6123c1517a84f3444b2

--- infra/bots/recipe_modules/build/canvaskit.py
+++ infra/bots/recipe_modules/build/canvaskit.py
@@ -13,17 +13,17 @@ BUILD_PRODUCTS_ISOLATE_WHITELIST_WASM = [
 def compile_fn(api, checkout_root, _ignore):
   out_dir = api.vars.cache_dir.join('docker', 'canvaskit')
   configuration = api.vars.builder_cfg.get('configuration', '')
-  extra         = api.vars.builder_cfg.get('extra_config',   '')
+  extra = api.vars.builder_cfg.get('extra_config', '')
 
   # We want to make sure the directories exist and were created by chrome-bot,
   # because if that isn't the case, docker will make them and they will be
   # owned by root, which causes mysterious failures. To mitigate this risk
   # further, we don't use the same out_dir as everyone else (thus the _ignore)
-  # param. Instead, we use a "wasm" subdirectory in the "docker" named_cache.
+  # param. Instead, we use a "canvaskit" subdirectory in the "docker" named_cache.
   api.file.ensure_directory('mkdirs out_dir', out_dir, mode=0777)
 
   # This uses the emscriptem sdk docker image and says "run the
-  # build_pathkit.sh helper script in there". Additionally, it binds two
+  # build_canvaskit.sh helper script in there". Additionally, it binds two
   # folders: the Skia checkout to /SRC and the output directory to /OUT
   # The called helper script will make the compile happen and put the
   # output in the right spot.  The neat thing is that since the Skia checkout
@@ -32,8 +32,8 @@ def compile_fn(api, checkout_root, _ignore):
   # toolchain changes.
   # Of note, the wasm build doesn't re-use any intermediate steps from the
   # previous builds, so it's essentially a build from scratch every time.
-  cmd = ['docker', 'run', '--rm', '-v', '%s:/SRC' % checkout_root,
-         '-v', '%s:/OUT' % out_dir,
+  cmd = ['docker', 'run', '--rm', '--volume', '%s:/SRC' % checkout_root,
+         '--volume', '%s:/OUT' % out_dir,
          DOCKER_IMAGE, INNER_BUILD_SCRIPT]
   if 'CPU' in extra:
     cmd.append('cpu') # It defaults to gpu
