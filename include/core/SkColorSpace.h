a69c50127b0187221cd6ef8fb6a43280b6abbcb4
Brian Osman <brianosman@google.com>
2019-02-01 16:01:50 -0500

    Remove SkColorSpaceTransferFn
    This has been replaced by skcms_TransferFunction
    
    Bug: skia:
    Change-Id: Ie9eb1eb0c6a301b1ed7807559c34a87d14b14f0a
    Reviewed-on: https://skia-review.googlesource.com/c/188637
    Commit-Queue: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- include/core/SkColorSpace.h
+++ include/core/SkColorSpace.h
@@ -37,25 +37,6 @@ struct SK_API SkColorSpacePrimaries {
     bool toXYZD50(skcms_Matrix3x3* toXYZD50) const;
 };
 
-/**
- *  Contains the coefficients for a common transfer function equation, specified as
- *  a transformation from a curved space to linear.
- *
- *  LinearVal = sign(InputVal) * (  C*|InputVal| + F       ), for 0.0f <= |InputVal| <  D
- *  LinearVal = sign(InputVal) * ( (A*|InputVal| + B)^G + E), for D    <= |InputVal|
- *
- *  Function must be positive and increasing.
- */
-struct SK_API SkColorSpaceTransferFn {
-    float fG;
-    float fA;
-    float fB;
-    float fC;
-    float fD;
-    float fE;
-    float fF;
-};
-
 namespace SkNamedTransferFn {
 
 // Like SkNamedGamut::kSRGB, keeping this bitwise exactly the same as skcms makes things fastest.
@@ -156,8 +137,6 @@ public:
      *
      *  If not, returns false.
      */
-    bool isNumericalTransferFn(SkColorSpaceTransferFn* fn) const;
-
     bool isNumericalTransferFn(skcms_TransferFunction* fn) const;
 
     /**
