4dd6f50d68d22712fbccf5d731b680d8e8342b10
Brian Osman <brianosman@google.com>
2019-02-01 15:33:42 -0500

    Remove SkICC::WriteToICC
    All clients are using SkWriteICCProfile, which uses types that are more
    consistent with the rest of Skia's colorspace API.
    
    Bug: skia:
    Change-Id: I1a416027839be95f51a8cabf5dd5d4395cbd2ec6

--- include/core/SkICC.h
+++ include/core/SkICC.h
@@ -9,18 +9,11 @@
 #define SkICC_DEFINED
 
 #include "SkData.h"
-#include "SkMatrix44.h"
-#include "SkRefCnt.h"
 
 struct skcms_Matrix3x3;
 struct skcms_TransferFunction;
-struct SkColorSpaceTransferFn;
 
 SK_API sk_sp<SkData> SkWriteICCProfile(const skcms_TransferFunction&,
                                        const skcms_Matrix3x3& toXYZD50);
-
-namespace SkICC {
-    SK_API sk_sp<SkData> WriteToICC(const SkColorSpaceTransferFn&, const SkMatrix44&);
-}
 
 #endif//SkICC_DEFINED
