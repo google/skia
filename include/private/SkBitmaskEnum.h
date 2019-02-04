19ee0edc18074680a6feca469f6607f87df6b544
Mike Klein <mtklein@google.com>
2019-02-04 10:03:04 -0500

    use std::underlying_type
    Change-Id: If05a8e99c5754859a2b04f040097eff3461a42e6

--- include/private/SkBitmaskEnum.h
+++ include/private/SkBitmaskEnum.h
@@ -14,7 +14,7 @@ template <typename T> struct is_bitmask_enum : std::false_type {};
 }
 
 template <typename E> SK_WHEN(skstd::is_bitmask_enum<E>::value, E) operator|(E l, E r) {
-    using U = skstd::underlying_type_t<E>;
+    using U = typename std::underlying_type<E>::type;
     return static_cast<E>(static_cast<U>(l) | static_cast<U>(r));
 }
 
@@ -23,7 +23,7 @@ template <typename E> SK_WHEN(skstd::is_bitmask_enum<E>::value, E&) operator|=(E
 }
 
 template <typename E> SK_WHEN(skstd::is_bitmask_enum<E>::value, E) operator&(E l, E r) {
-    using U = skstd::underlying_type_t<E>;
+    using U = typename std::underlying_type<E>::type;
     return static_cast<E>(static_cast<U>(l) & static_cast<U>(r));
 }
 
