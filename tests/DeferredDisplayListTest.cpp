8ad18eac2c0a6d173fd12907c73d7f7f953eff45
Robert Phillips <robertphillips@google.com>
2019-02-05 10:04:32 -0500

    Add support for Mtl backend to DeferredDisplayListTest
    Change-Id: Ie576bc8442e5a646c33191100be1be5f89d72290

--- tests/DeferredDisplayListTest.cpp
+++ tests/DeferredDisplayListTest.cpp
@@ -42,9 +42,6 @@
 #include "gl/GrGLCaps.h"
 #include "gl/GrGLDefines.h"
 #include "gl/GrGLTypes.h"
-#ifdef SK_VULKAN
-#include <vulkan/vulkan_core.h>
-#endif
 
 // Try to create a backend format from the provided colorType and config. Return an invalid
 // backend format if the combination is infeasible.
@@ -54,6 +51,10 @@ static GrBackendFormat create_backend_format(GrContext* context,
     const GrCaps* caps = context->priv().caps();
 
     switch (context->backend()) {
+#ifdef SK_METAL
+    case GrBackendApi::kMetal:
+        return caps->getBackendFormatFromColorType(ct);
+#endif
     case GrBackendApi::kOpenGL: {
         const GrGLCaps* glCaps = static_cast<const GrGLCaps*>(caps);
         GrGLStandard standard = glCaps->standard();
@@ -705,7 +706,6 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(DDLNonTextureabilityTest, reporter, ctxInfo) 
         GrBackendTexture backend;
         sk_sp<SkSurface> s = params.make(context, &backend);
         if (!s) {
-            params.cleanUpBackEnd(context, backend);
             continue;
         }
 
