fe911935f6c923ddfb342aa0bbcfdab4b60d8dd0
Chris Dalton <csmartdalton@google.com>
2019-02-05 17:42:14 -0700

    Reland "Add a GpuGM class for GPU-only tests"
    This is a reland of f5efa7890272bd7a3daf052af5ffa546903eb9a6
    
    Original change's description:
    > Add a GpuGM class for GPU-only tests
    > 
    > Bug: skia:8731
    > Change-Id: Ic81f21621c0183bb356601eb663fa1ee334bca55
    > Reviewed-on: https://skia-review.googlesource.com/c/189134
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Reviewed-by: Mike Klein <mtklein@google.com>
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    
    Bug: skia:8731
    Change-Id: Ia322d7e9280b67ebea1edbefc820cae72cd9c3a6

--- gm/etc1.cpp
+++ gm/etc1.cpp
@@ -22,7 +22,7 @@
 #include "ops/GrFillRectOp.h"
 
 // Basic test of Ganesh's ETC1 support
-class ETC1GM : public skiagm::GM {
+class ETC1GM : public skiagm::GpuGM {
 public:
     ETC1GM() {
         this->setBGColor(0xFFCCCCCC);
@@ -62,20 +62,8 @@ protected:
         }
     }
 
-    void onDraw(SkCanvas* canvas) override {
-        GrRenderTargetContext* renderTargetContext =
-            canvas->internal_private_accessTopLayerRenderTargetContext();
-        if (!renderTargetContext) {
-            skiagm::GM::DrawGpuOnlyMessage(canvas);
-            return;
-        }
-
-        GrContext* context = canvas->getGrContext();
-        if (!context || context->abandoned()) {
-            DrawFailureMessage(canvas, "GrContext unavailable or abandoned.");
-            return;
-        }
-
+    void onDraw(GrContext* context, GrRenderTargetContext* renderTargetContext,
+                SkCanvas* canvas) override {
         sk_sp<SkImage> image = SkImage::MakeFromCompressed(context, fETC1Data,
                                                            kTexWidth, kTexHeight,
                                                            SkImage::kETC1_CompressionType);
