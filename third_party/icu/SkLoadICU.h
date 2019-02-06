6e88d7a820c2d13043d4fd666787c0d904020fd2
Hal Canary <halcanary@google.com>
2019-02-06 12:29:11 -0500

    SkLoadICU: simplify
    Change-Id: I98cd1f36ca96a92b72974ae228465199638e4bea

--- third_party/icu/SkLoadICU.h
+++ third_party/icu/SkLoadICU.h
@@ -9,7 +9,7 @@
 
 #include "SkTypes.h"
 
-#ifdef SK_BUILD_FOR_WIN
+#if defined(SK_BUILD_FOR_WIN) && defined(SK_USING_THIRD_PARTY_ICU)
 
 #include "../private/SkLeanWindows.h"
 
@@ -19,34 +19,38 @@
 #define ICU_UTIL_DATA_SYMBOL "icudt" U_ICU_VERSION_SHORT "_dat"
 #define ICU_UTIL_DATA_SHARED_MODULE_NAME "icudt.dll"
 
-inline void SkLoadICU() {
-    HMODULE module = LoadLibraryA(ICU_UTIL_DATA_SHARED_MODULE_NAME);
-    if (!module) {
-        SK_ABORT("Failed to load " ICU_UTIL_DATA_SHARED_MODULE_NAME "\n");
-    }
-    FARPROC addr = GetProcAddress(module, ICU_UTIL_DATA_SYMBOL);
-    if (!addr) {
-        SK_ABORT("Symbol " ICU_UTIL_DATA_SYMBOL " missing in "
-                 ICU_UTIL_DATA_SHARED_MODULE_NAME ".\n");
-    }
-    UErrorCode err = U_ZERO_ERROR;
-    udata_setCommonData(reinterpret_cast<void*>(addr), &err);
-    if (err != U_ZERO_ERROR) {
-        SkDebugf("udata_setCommonData() returned %d.\n", (int)err);
-        SK_ABORT("");
-    }
-    udata_setFileAccess(UDATA_ONLY_PACKAGES, &err);
-    if (err != U_ZERO_ERROR) {
-        SkDebugf("udata_setFileAccess() returned %d.\n", (int)err);
-        SK_ABORT("");
-    }
+static inline bool SkLoadICU() {
+    SkOnce once;
+    once([] {
+        HMODULE module = LoadLibraryA(ICU_UTIL_DATA_SHARED_MODULE_NAME);
+        if (!module) {
+            SK_ABORT("Failed to load " ICU_UTIL_DATA_SHARED_MODULE_NAME "\n");
+        }
+        FARPROC addr = GetProcAddress(module, ICU_UTIL_DATA_SYMBOL);
+        if (!addr) {
+            SK_ABORT("Symbol " ICU_UTIL_DATA_SYMBOL " missing in "
+                     ICU_UTIL_DATA_SHARED_MODULE_NAME ".\n");
+        }
+        UErrorCode err = U_ZERO_ERROR;
+        udata_setCommonData(reinterpret_cast<void*>(addr), &err);
+        if (err != U_ZERO_ERROR) {
+            SkDebugf("udata_setCommonData() returned %d.\n", (int)err);
+            SK_ABORT("");
+        }
+        udata_setFileAccess(UDATA_ONLY_PACKAGES, &err);
+        if (err != U_ZERO_ERROR) {
+            SkDebugf("udata_setFileAccess() returned %d.\n", (int)err);
+            SK_ABORT("");
+        }
+    });
+    return true;
 }
 
 #undef ICU_UTIL_DATA_SHARED_MODULE_NAME
 #undef ICU_UTIL_DATA_SYMBOL
 
 #else
-inline void SkLoadICU() {}
+inline bool SkLoadICU() { return true; }
 #endif  // SK_BUILD_FOR_WIN
 #endif  // load_icu_DEFINED
 
