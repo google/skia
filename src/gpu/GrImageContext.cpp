59b8d4c904715be66594d24fab8d40223bfd410e
Robert Phillips <robertphillips@google.com>
2019-02-07 10:44:10 -0500

    Move proxyProvider and singleOwner to GrImageContext
    This starts to beef up the capabilities of the GrImageContext in preparation for its future responsibilities (i.e., creating promise images w/o a recordingContext).
    
    Note that the proxyProvider still has different behavior if it has a full context vs. a reduced context. I intend to just let this behavior remain as is.
    
    Change-Id: Idb9d99a548ef928fc1b9dc1e5a34f74343bb0b4b

--- src/gpu/GrImageContext.cpp
+++ src/gpu/GrImageContext.cpp
@@ -7,10 +7,25 @@
 
 #include "GrImageContext.h"
 
+#include "GrCaps.h"
+#include "GrImageContextPriv.h"
+#include "GrProxyProvider.h"
+#include "GrSkSLFPFactoryCache.h"
+
 GrImageContext::GrImageContext(GrBackendApi backend,
                                const GrContextOptions& options,
-                               uint32_t uniqueID)
-            : INHERITED(backend, options, uniqueID) {
+                               uint32_t contextID)
+            : INHERITED(backend, options, contextID) {
+    fProxyProvider.reset(new GrProxyProvider(this));
 }
 
 GrImageContext::~GrImageContext() {}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+sk_sp<const GrCaps> GrImageContextPriv::refCaps() const {
+    return fContext->refCaps();
+}
+
+sk_sp<GrSkSLFPFactoryCache> GrImageContextPriv::fpFactoryCache() {
+    return fContext->fpFactoryCache();
+}
