3d482995e5957b1b1fdc3cf41d8b65af42ee9ec3
Jim Van Verth <jvanverth@google.com>
2019-02-07 10:48:05 -0500

    Set correct uniform size for Metal.
    There are two fixes here. The first pads out the uniform buffer to
    match the alignment of the largest element in the buffer, as required
    by Metal. The second ensures that the RTHeight uniform is only added
    to the shader if required by the program inputs, as otherwise it wasn't
    being included in the uniform data.
    
    Bug: skia:8737
    Change-Id: Icbe57456f3ec79faada231278c58137ca9865dd2
    Reviewed-on: https://skia-review.googlesource.com/c/189875
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/sksl/SkSLMetalCodeGenerator.cpp
+++ src/sksl/SkSLMetalCodeGenerator.cpp
@@ -687,7 +687,7 @@ void MetalCodeGenerator::writeFunction(const FunctionDefinition& f) {
                 this->write(")]]");
             }
         }
-        if (fProgram.fKind == Program::kFragment_Kind) {
+        if (fProgram.fInputs.fRTHeight) {
             if (fInterfaceBlockNameMap.empty()) {
             // FIXME - Possibly have a different way of passing in u_skRTHeight or flip y axis
             // in a different way altogether.
@@ -844,7 +844,7 @@ void MetalCodeGenerator::writeInterfaceBlock(const InterfaceBlock& intf) {
     }
     fIndentation++;
     writeFields(structType->fields(), structType->fOffset, &intf);
-    if (fProgram.fKind == Program::kFragment_Kind) {
+    if (fProgram.fInputs.fRTHeight) {
         this->writeLine("float u_skRTHeight;");
     }
     fIndentation--;
@@ -1261,7 +1261,7 @@ void MetalCodeGenerator::writeInterfaceBlocks() {
             wroteInterfaceBlock = true;
         }
     }
-    if (!wroteInterfaceBlock && (fProgram.fKind == Program::kFragment_Kind)) {
+    if (!wroteInterfaceBlock && fProgram.fInputs.fRTHeight) {
         // FIXME - Possibly have a different way of passing in u_skRTHeight or flip y axis
         // in a different way altogether.
         this->writeLine("struct sksl_synthetic_uniforms {");
