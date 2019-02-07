ac7fbbfd21fe22328578dc5f03e6033dfe4de923
Mike Klein <mtklein@google.com>
2019-02-07 16:18:22 -0500

    move some methods out of line
    This started because I noticed some repeated symbols while disassembling
    object files.  I decided to fix up one or two to kill time while waiting
    for Blink layout tests, but then I got a little quixotic.  In the end, I
    ran:
    
        ninja -C out && git add -u && nm -U -j out/obj/gm/gm.*.o | sort | uniq -c | sort -g | c++filt | less
    
    And then outlined things that stuck out as funny, either because they
    were big, because they were virtual, because there were many copies of
    them, or some combination of those factors.
    
    I'm on the fence about moving a few of SkRefCnt's virtuals out of line,
    so I've left SkRefCnt.h unchanged for now.
    
    I'm not sure that this is important.
    
    Change-Id: I425d05aa4beabbae40dd8df465155bfb909ef43a

--- src/image/SkSurface_Raster.cpp
+++ src/image/SkSurface_Raster.cpp
@@ -198,3 +198,8 @@ sk_sp<SkSurface> SkSurface::MakeRaster(const SkImageInfo& info, size_t rowBytes,
     }
     return sk_make_sp<SkSurface_Raster>(info, std::move(pr), props);
 }
+
+sk_sp<SkSurface> SkSurface::MakeRasterN32Premul(int width, int height,
+                                                const SkSurfaceProps* surfaceProps) {
+    return MakeRaster(SkImageInfo::MakeN32Premul(width, height), surfaceProps);
+}
