1a4cc11c32650b4021b07235f9c4bea0ab97b232
Stephen White <senorblanco@chromium.org>
2019-02-07 16:20:44 -0500

    Dawn backend (experimental).
    Change-Id: If03831fc4c542d1afe524c806c930ce88a3f23c0

--- src/gpu/GrBackendSurface.cpp
+++ src/gpu/GrBackendSurface.cpp
@@ -11,6 +11,11 @@
 
 #include "gl/GrGLUtil.h"
 
+#ifdef SK_DAWN
+#include "dawn/GrDawnTypes.h"
+#include "dawn/GrDawnUtil.h"
+#endif
+
 #ifdef SK_VULKAN
 #include "vk/GrVkImageLayout.h"
 #include "vk/GrVkTypes.h"
@@ -97,6 +102,22 @@ const GrVkYcbcrConversionInfo* GrBackendFormat::getVkYcbcrConversionInfo() const
     return nullptr;
 }
 
+#ifdef SK_DAWN
+GrBackendFormat::GrBackendFormat(dawn::TextureFormat format)
+        : fBackend(GrBackendApi::kDawn)
+        , fValid(true)
+        , fDawnFormat(format)
+        , fTextureType(GrTextureType::k2D) {
+}
+
+const dawn::TextureFormat* GrBackendFormat::getDawnFormat() const {
+    if (this->isValid() && GrBackendApi::kDawn == fBackend) {
+        return &fDawnFormat;
+    }
+    return nullptr;
+}
+#endif
+
 #ifdef SK_METAL
 GrBackendFormat::GrBackendFormat(GrMTLPixelFormat mtlFormat)
         : fBackend(GrBackendApi::kMetal)
@@ -154,6 +175,11 @@ bool GrBackendFormat::operator==(const GrBackendFormat& that) const {
                    fVk.fYcbcrConversionInfo == that.fVk.fYcbcrConversionInfo;
 #endif
             break;
+        case GrBackendApi::kDawn:
+#ifdef SK_DAWN
+            return fDawnFormat == that.fDawnFormat;
+            break;
+#endif
 #ifdef SK_METAL
         case GrBackendApi::kMetal:
             return fMtlFormat == that.fMtlFormat;
@@ -166,6 +192,19 @@ bool GrBackendFormat::operator==(const GrBackendFormat& that) const {
     }
     return false;
 }
+
+#ifdef SK_DAWN
+GrBackendTexture::GrBackendTexture(int width,
+                                   int height,
+                                   const GrDawnImageInfo& dawnInfo)
+        : fIsValid(true)
+        , fWidth(width)
+        , fHeight(height)
+        , fConfig(GrDawnFormatToPixelConfig(dawnInfo.fFormat))
+        , fMipMapped(GrMipMapped(dawnInfo.fLevelCount > 1))
+        , fBackend(GrBackendApi::kDawn)
+        , fDawnInfo(dawnInfo) {}
+#endif
 
 GrBackendTexture::GrBackendTexture(int width,
                                    int height,
@@ -230,6 +269,7 @@ GrBackendTexture::GrBackendTexture(int width,
         , fBackend(GrBackendApi::kMock)
         , fMockInfo(mockInfo) {}
 
+
 GrBackendTexture::~GrBackendTexture() {
     this->cleanup();
 }
@@ -242,7 +282,12 @@ void GrBackendTexture::cleanup() {
 #endif
 }
 
-GrBackendTexture::GrBackendTexture(const GrBackendTexture& that) : fIsValid(false) {
+GrBackendTexture::GrBackendTexture(const GrBackendTexture& that)
+    : fIsValid(false)
+#ifdef SK_DAWN
+    , fDawnInfo()
+#endif
+{
     *this = that;
 }
 
@@ -272,6 +317,11 @@ GrBackendTexture& GrBackendTexture::operator=(const GrBackendTexture& that) {
             fMtlInfo = that.fMtlInfo;
             break;
 #endif
+#ifdef SK_DAWN
+        case GrBackendApi::kDawn:
+            fDawnInfo = that.fDawnInfo;
+            break;
+#endif
         case GrBackendApi::kMock:
             fMockInfo = that.fMockInfo;
             break;
@@ -281,6 +331,16 @@ GrBackendTexture& GrBackendTexture::operator=(const GrBackendTexture& that) {
     fIsValid = that.fIsValid;
     return *this;
 }
+
+#ifdef SK_DAWN
+bool GrBackendTexture::getDawnImageInfo(GrDawnImageInfo* outInfo) const {
+    if (this->isValid() && GrBackendApi::kDawn == fBackend) {
+        *outInfo = fDawnInfo;
+        return true;
+    }
+    return false;
+}
+#endif
 
 bool GrBackendTexture::getVkImageInfo(GrVkImageInfo* outInfo) const {
 #ifdef SK_VULKAN
@@ -383,6 +443,22 @@ bool GrBackendTexture::TestingOnly_Equals(const GrBackendTexture& t0, const GrBa
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef SK_DAWN
+GrBackendRenderTarget::GrBackendRenderTarget(int width,
+                                             int height,
+                                             int sampleCnt,
+                                             int stencilBits,
+                                             const GrDawnImageInfo& dawnInfo)
+        : fIsValid(true)
+        , fWidth(width)
+        , fHeight(height)
+        , fSampleCnt(sampleCnt)
+        , fStencilBits(stencilBits)
+        , fConfig(GrDawnFormatToPixelConfig(dawnInfo.fFormat))
+        , fBackend(GrBackendApi::kDawn)
+        , fDawnInfo(dawnInfo) {}
+#endif
 
 GrBackendRenderTarget::GrBackendRenderTarget(int width,
                                              int height,
@@ -501,6 +577,11 @@ GrBackendRenderTarget& GrBackendRenderTarget::operator=(const GrBackendRenderTar
             fVkInfo.assign(that.fVkInfo, this->isValid());
 #endif
             break;
+#ifdef SK_DAWN
+        case GrBackendApi::kDawn:
+            fDawnInfo = that.fDawnInfo;
+            break;
+#endif
 #ifdef SK_METAL
         case GrBackendApi::kMetal:
             fMtlInfo = that.fMtlInfo;
@@ -515,6 +596,16 @@ GrBackendRenderTarget& GrBackendRenderTarget::operator=(const GrBackendRenderTar
     fIsValid = that.fIsValid;
     return *this;
 }
+
+#ifdef SK_DAWN
+bool GrBackendRenderTarget::getDawnImageInfo(GrDawnImageInfo* outInfo) const {
+    if (this->isValid() && GrBackendApi::kDawn == fBackend) {
+        *outInfo = fDawnInfo;
+        return true;
+    }
+    return false;
+}
+#endif
 
 bool GrBackendRenderTarget::getVkImageInfo(GrVkImageInfo* outInfo) const {
 #ifdef SK_VULKAN
---
+++ src/gpu/dawn/GrDawnCaps.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrDawnCaps_DEFINED
+#define GrDawnCaps_DEFINED
+
+#include "GrCaps.h"
+#include "GrContextOptions.h"
+#include "GrDawnUtil.h"
+#include "GrBackendSurface.h"
+
+class GrDawnCaps : public GrCaps {
+public:
+    GrDawnCaps(const GrContextOptions& contextOptions);
+
+    bool isConfigTexturable(GrPixelConfig config) const override;
+
+    bool isConfigCopyable(GrPixelConfig config) const override {
+        return true;
+    }
+
+    bool onCanCopySurface(const GrSurfaceProxy* dst, const GrSurfaceProxy* src,
+                          const SkIRect& srcRect, const SkIPoint& dstPoint) const override {
+        return true;
+    }
+
+    bool initDescForDstCopy(const GrRenderTargetProxy* src, GrSurfaceDesc* desc,
+                            GrSurfaceOrigin* origi, bool* rectsMustMatch,
+                            bool* disallowSubrect) const override {
+        return false;
+    }
+
+    bool validateBackendTexture(const GrBackendTexture&, SkColorType,
+                                GrPixelConfig*) const override {
+        return true;
+    }
+    bool validateBackendRenderTarget(const GrBackendRenderTarget&, SkColorType,
+                                     GrPixelConfig*) const override {
+        return true;
+    }
+
+    bool getConfigFromBackendFormat(const GrBackendFormat&, SkColorType,
+                                    GrPixelConfig*) const override {
+        return false;
+    }
+
+    bool getYUVAConfigFromBackendFormat(const GrBackendFormat&,
+                                        GrPixelConfig*) const override {
+        return false;
+    }
+    bool getYUVAConfigFromBackendTexture(const GrBackendTexture&,
+                                         GrPixelConfig*) const override {
+        return false;
+    }
+
+    GrBackendFormat getBackendFormatFromGrColorType(GrColorType ct, GrSRGBEncoded srgbEncoded) const override {
+        GrPixelConfig config = GrColorTypeToPixelConfig(ct, srgbEncoded);
+        dawn::TextureFormat format;
+        if (config == kUnknown_GrPixelConfig || !GrPixelConfigToDawnFormat(config, &format)) {
+            return GrBackendFormat();
+        }
+        return GrBackendFormat::MakeDawn(format);
+    }
+#ifdef GR_TEST_UTILS
+    GrBackendFormat onCreateFormatFromBackendTexture(
+        const GrBackendTexture& backendTex) const override {
+        return GrBackendFormat(); // Dawn BackendFormat not yet implemented.
+    }
+#endif
+
+    bool surfaceSupportsReadPixels(const GrSurface*) const override {
+        return true;
+    }
+
+    bool onSurfaceSupportsWritePixels(const GrSurface* surface) const override {
+        return true;
+    }
+
+    int getRenderTargetSampleCount(int requestedCount, GrPixelConfig) const override {
+        return 1;
+    }
+
+    int maxRenderTargetSampleCount(GrPixelConfig) const override {
+        return 1;
+    }
+
+    bool performPartialClearsAsDraws() const override {
+        return true;
+    }
+
+    GrSurfaceOrigin renderTargetOrigin() const override {
+        return kTopLeft_GrSurfaceOrigin;
+    }
+
+    typedef GrCaps INHERITED;
+};
+
+#endif
