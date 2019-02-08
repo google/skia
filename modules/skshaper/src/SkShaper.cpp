a333a9bbcd88d141d0f41c9a795193c04735dadb
Ben Wagner <bungeman@google.com>
2019-02-08 17:46:38 -0500

    Fix cluster reporting from shaper.
    Bug: skia:8420
    Change-Id: I7eea8c6b3af2153a1ac1189782fc6cbaaf9ee5c6

--- modules/skshaper/src/SkShaper.cpp
+++ modules/skshaper/src/SkShaper.cpp
@@ -6,19 +6,31 @@
  */
 
 #include "SkShaper.h"
-
+#include "SkSpan.h"
 #include "SkTextBlobPriv.h"
 
 SkShaper::RunHandler::Buffer SkTextBlobBuilderRunHandler::newRunBuffer(const RunInfo&,
                                                                        const SkFont& font,
                                                                        int glyphCount,
-                                                                       int textCount) {
+                                                                       SkSpan<const char> utf8) {
     const auto& runBuffer = SkTextBlobBuilderPriv::AllocRunTextPos(&fBuilder, font, glyphCount,
-                                                                   textCount, SkString());
+                                                                   utf8.size(), SkString());
+    if (runBuffer.utf8text && fUtf8Text) {
+        memcpy(runBuffer.utf8text, utf8.data(), utf8.size());
+    }
+    fClusters = runBuffer.clusters;
+    fGlyphCount = glyphCount;
+    fClusterOffset = utf8.data() - fUtf8Text;
+
     return { runBuffer.glyphs,
              runBuffer.points(),
-             runBuffer.utf8text,
              runBuffer.clusters };
+}
+
+void SkTextBlobBuilderRunHandler::commitRun() {
+    for (int i = 0; i < fGlyphCount; ++i) {
+        fClusters[i] -= fClusterOffset;
+    }
 }
 
 sk_sp<SkTextBlob> SkTextBlobBuilderRunHandler::makeBlob() {
