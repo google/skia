47fc648f210e37e058967a6f5be044f93576e4ab
Mike Reed <reed@google.com>
2019-02-12 17:21:19 -0500

    add colorpicker
    Bug: skia:
    Change-Id: I8c249fd5f63ecd2304d008fb6e634a7ec74279ea

--- samplecode/SampleSG.cpp
+++ samplecode/SampleSG.cpp
@@ -17,19 +17,23 @@
 #include "SkSGRect.h"
 #include "SkSGScene.h"
 
+#include "imgui.h"
+
 struct PerNodeInfo {
     // key
     sksg::Draw* fDraw;
 
     // value(s)
     sksg::GeometryNode* fGeo;
-    sksg::PaintNode*    fPaint;
+    sksg::Color*        fPaint;
 };
 
 class SampleSG : public Sample {
     SkTDArray<PerNodeInfo> fSideCar;
     sk_sp<sksg::Group> fGroup;
     std::unique_ptr<sksg::Scene> fScene;
+    // non-null if we are selected
+    sksg::Draw* fSelected = nullptr;
 
     PerNodeInfo* findInfo(sksg::Draw* key) {
         for (int i = 0; i < fSideCar.count(); ++i) {
@@ -40,12 +44,21 @@ class SampleSG : public Sample {
         return nullptr;
     }
 
-    void appendNode(sk_sp<sksg::Draw> d, sk_sp<sksg::GeometryNode> g, sk_sp<sksg::PaintNode> p) {
+    void appendNode(sk_sp<sksg::Draw> d, sk_sp<sksg::GeometryNode> g, sk_sp<sksg::Color> p) {
         fGroup->addChild(d);
         auto sc = fSideCar.append();
         sc->fDraw  = d.get();
         sc->fGeo   = g.get();
         sc->fPaint = p.get();
+    }
+
+    void updateColor() {
+        if (fSelected) {
+            auto info = this->findInfo(fSelected);
+            SkColor4f c = SkColor4f::FromColor(info->fPaint->getColor());
+            ImGui::ColorEdit4("Color", c.vec());
+            info->fPaint->setColor(c.toSkColor());
+        }
     }
 
 public:
@@ -75,31 +88,35 @@ protected:
     }
 
     void onDrawContent(SkCanvas* canvas) override {
+        this->updateColor();
+
         fScene->render(canvas);
     }
 
     Click* onFindClickHandler(SkScalar x, SkScalar y, unsigned modi) override {
         if (auto node = fScene->nodeAt({x, y})) {
+            SkASSERT(this->findInfo((sksg::Draw*)node) != nullptr);
+            fSelected = (sksg::Draw*)node;
             Click* click = new Click(this);
             click->fMeta.setPtr("node", (void*)node);
             return click;
         }
+        fSelected = nullptr;
         return this->INHERITED::onFindClickHandler(x, y, modi);
     }
 
     bool onClick(Click* click) override {
         sksg::Draw* node = nullptr;
         if (click->fMeta.findPtr("node", (void**)&node)) {
-            if (auto info = this->findInfo(node)) {
-                auto geo = info->fGeo;
-                sksg::Rect* r = (sksg::Rect*)geo;
-                SkScalar dx = click->fCurr.fX - click->fPrev.fX;
-                SkScalar dy = click->fCurr.fY - click->fPrev.fY;
-                r->setL(r->getL() + dx);
-                r->setR(r->getR() + dx);
-                r->setT(r->getT() + dy);
-                r->setB(r->getB() + dy);
-            }
+            auto info = this->findInfo(node);
+            auto geo = info->fGeo;
+            sksg::Rect* r = (sksg::Rect*)geo;
+            SkScalar dx = click->fCurr.fX - click->fPrev.fX;
+            SkScalar dy = click->fCurr.fY - click->fPrev.fY;
+            r->setL(r->getL() + dx);
+            r->setR(r->getR() + dx);
+            r->setT(r->getT() + dy);
+            r->setB(r->getB() + dy);
             return true;
         }
         return false;
