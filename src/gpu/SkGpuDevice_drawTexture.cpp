1025b09290b2899bb670b8b492f7aaed98f2c1ef
Michael Ludwig <michaelludwig@google.com>
2019-02-15 11:09:16 -0500

    Formalize edge-aa interaction with GrAA
    With this formalization, the edge-AA APIs in GrRTC can distinguish
    between tiling cases and regular drawing cases implemented using
    kNone or kAll for the AA flags. This means fillRectToRect can be
    implemented in terms of fillRectWithEdgeAA.
    
    It also means the drawTexture cases will properly handle
    isolated draws and tiled draws when drawing into MSAA.
    
    Bug: skia:
    Change-Id: I248dd001919228a958cf84b6bc91363b58b72c0b

--- src/gpu/SkGpuDevice_drawTexture.cpp
+++ src/gpu/SkGpuDevice_drawTexture.cpp
@@ -150,7 +150,7 @@ static void draw_texture(const SkPaint& paint, const SkMatrix& ctm, const SkRect
     }
     GrQuadAAFlags aaFlags = aa == GrAA::kYes ? GrQuadAAFlags::kAll : GrQuadAAFlags::kNone;
     rtc->drawTexture(clip, std::move(proxy), filter, paint.getBlendMode(), color, srcRect, dstRect,
-                     aaFlags, constraint, ctm, std::move(textureXform));
+                     aa, aaFlags, constraint, ctm, std::move(textureXform));
 }
 
 //////////////////////////////////////////////////////////////////////////////
