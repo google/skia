23b96c0e23297d359595d081e576544ea9207d26
Brian Osman <brianosman@google.com>
2019-02-19 13:04:40 -0500

    First pass comments to document SkReflected.h
    Bug: skia:
    Change-Id: I6fe50ef0f783a9093090037c4b22af1400c0136e
    Reviewed-on: https://skia-review.googlesource.com/c/193363
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- modules/particles/include/SkReflected.h
+++ modules/particles/include/SkReflected.h
@@ -17,6 +17,30 @@
 class SkFieldVisitor;
 class SkRandom;
 
+/**
+ * Classes and macros for a lightweight reflection system.
+ *
+ * Classes that derive from SkReflected have several features:
+ *   - Access to an SkReflected::Type instance, via static GetType() or virtual getType()
+ *     The Type instance can be used to create additional instances (fFactory), get the name
+ *     of the type, and answer queries of the form "is X derived from Y".
+ *   - Given a string containing a type name, SkReflected can create an instance of that type.
+ *   - SkReflected::VisitTypes can be used to enumerate all Types, or all Types derived from a
+ *     particular base class.
+ *
+ * Together, this simplifies the implementation of serialization and other dynamic type factories.
+ *
+ * Finally, all SkReflected-derived types must implement visitFields, which provides field-level
+ * reflection, in conjunction with SkFieldVisitor. See SkFieldVisitor, below.
+ *
+ * To create a new reflected class:
+ *   - Derive the class (directly or indirectly) from SkReflected.
+ *   - Ensure that the class can be default constructed.
+ *   - In the public area of the class declaration, add REFLECTED(<ClassName>, <BaseClassName>).
+ *     If the class is abstract, use REFLECTED_ABSTRACT(<ClassName>, <BaseClassName>) instead.
+ *   - Add a one-time call to REGISTER_REFLECTED(<ClassName>) at initialization time.
+ *   - Implement visitFields(), as described below.
+ */
 class SkReflected : public SkRefCnt {
 public:
     typedef sk_sp<SkReflected>(*Factory)();
@@ -92,10 +116,30 @@ private:
 
 struct SkPoint;
 
+/**
+ * SkFieldVisitor is an interface that can be implemented by any class to visit all fields of
+ * SkReflected types, and of types that implement the visitFields() function.
+ *
+ * Classes implementing the interface must supply implementations of virtual functions that visit
+ * basic types (float, int, bool, SkString, etc...), as well as helper methods for entering the
+ * scope of an object or array.
+ *
+ * All visit functions supply a field name, and a non-constant reference to an actual field.
+ * This allows visitors to serialize or deserialize collections of objects, or perform edits on
+ * existing objects.
+ *
+ * Classes that implement visitFields (typically derived from SkReflected) should simply call
+ * visit() for each of their fields, passing a (unique) field name, and the actual field. If your
+ * class has derived fields, it's best to only visit() the fields that you would serialize, then
+ * enforce any constraints afterwards.
+ *
+ * See SkParticleSerialization.h for example visitors that perform serialization to and from JSON.
+ */
 class SkFieldVisitor {
 public:
     virtual ~SkFieldVisitor() {}
 
+    // Visit functions for primitive types, to be implemented by derived visitors.
     virtual void visit(const char*, float&) = 0;
     virtual void visit(const char*, int&) = 0;
     virtual void visit(const char*, bool&) = 0;
@@ -104,12 +148,15 @@ public:
     virtual void visit(const char*, SkPoint&) = 0;
     virtual void visit(const char*, SkColor4f&) = 0;
 
+    // Specific virtual signature for SkCurve, to allow for heavily customized UI in SkGuiVisitor.
     virtual void visit(const char* name, SkCurve& c) {
         this->enterObject(name);
         c.visitFields(this);
         this->exitObject();
     }
 
+    // Default visit function for structs with no special behavior. It is assumed that any such
+    // struct implements visitFields(SkFieldVisitor*) to recursively visit each of its fields.
     template <typename T>
     void visit(const char* name, T& value) {
         this->enterObject(name);
@@ -117,6 +164,9 @@ public:
         this->exitObject();
     }
 
+    // Specialization for SkTArrays. In conjunction with the enterArray/exitArray virtuals, this
+    // allows visitors to resize an array (for deserialization), and apply a single edit operation
+    // (remove or move a single element). Each element of the array is visited as normal.
     template <typename T, bool MEM_MOVE>
     void visit(const char* name, SkTArray<T, MEM_MOVE>& arr) {
         arr.resize_back(this->enterArray(name, arr.count()));
@@ -126,6 +176,11 @@ public:
         this->exitArray().apply(arr);
     }
 
+    // Specialization for sk_sp pointers to types derived from SkReflected. Those types are known
+    // to implement visitFields. This allows the visitor to modify the contents of the object, or
+    // even replace it with an entirely new object. The virtual function uses SkReflected as a
+    // common type, but uses SkReflected::Type to communicate the required base-class. In this way,
+    // the new object can be verified to match the type of the original (templated) pointer.
     template <typename T>
     void visit(const char* name, sk_sp<T>& obj) {
         this->enterObject(name);
@@ -147,6 +202,7 @@ public:
     }
 
 protected:
+    // Helper struct to allow exitArray to specify a single operation performed on the array.
     struct ArrayEdit {
         enum class Verb {
             kNone,
