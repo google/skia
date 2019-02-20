8245d86c72cea4b9e383acd4c1456d55004f5b64
Mike Klein <mtklein@google.com>
2019-02-20 09:34:47 -0500

    update NDK to r19b
    Nothing interesting.  We were already on r19's beta.
    
    Change-Id: I4d58e7f82317427d06ef5b138681c00b476ecd9a
    Reviewed-on: https://skia-review.googlesource.com/c/193469
    Commit-Queue: Mike Klein <mtklein@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: Mike Klein <mtklein@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- infra/bots/assets/android_ndk_windows/create.py
+++ infra/bots/assets/android_ndk_windows/create.py
@@ -15,7 +15,7 @@ import os.path
 import shutil
 import subprocess
 
-NDK_VER = "android-ndk-r19-beta2"
+NDK_VER = "android-ndk-r19b"
 NDK_URL = \
     "https://dl.google.com/android/repository/%s-windows-x86_64.zip" % NDK_VER
 
