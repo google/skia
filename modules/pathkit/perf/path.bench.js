8abc8429457128456866b52bb0bebaa800319d91
Kevin Lubick <kjlubick@google.com>
2019-02-26 07:59:48 -0500

    [pathkit] fix perf
    Docs-Preview: https://skia.org/?cl=195360
    Bug: skia:
    Change-Id: I4109810832b69a71960f6e619e32288ae8fa5023
    Reviewed-on: https://skia-review.googlesource.com/c/195360
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- modules/pathkit/perf/path.bench.js
+++ modules/pathkit/perf/path.bench.js
@@ -9,7 +9,7 @@ describe('PathKit\'s Path Behavior', function() {
         } else {
             PathKitInit({
                 locateFile: (file) => '/pathkit/'+file,
-            }).then((_PathKit) => {
+            }).ready().then((_PathKit) => {
                 PathKit = _PathKit;
                 resolve();
             });
