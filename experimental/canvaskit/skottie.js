fe09f8f29f335c8395122f1c5a555735ee743972
Kevin Lubick <kjlubick@google.com>
2019-02-27 16:25:06 -0500

    [canvaskit] Support image assets for Skottie
    There's a lot of refactoring here to fix up the
    namespace of the JS (see pre/postamble js).
    
    A reviewer can skip all the changes to interface.js,
    as they are mostly whitespace changes.
    
    Bug: skia:
    Change-Id: I7cedeb98f04d4446ac4dfb555a416f30490b2b94

---
+++ experimental/canvaskit/skottie.js
@@ -0,0 +1,44 @@
+// Adds compile-time JS functions to augment the CanvasKit interface.
+// Specifically, anything that should only be on the Skottie builds of canvaskit.
+
+
+CanvasKit.MakeManagedAnimation = function(json, imgs, fonts) {
+  if (!CanvasKit._MakeManagedAnimation) {
+    throw 'Not compiled with MakeManagedAnimation';
+  }
+  if (!imgs && !fonts) {
+    return CanvasKit._MakeManagedAnimation(json, 0, nullptr, nullptr, nullptr,
+                                                 0, nullptr, nullptr, nullptr);
+  }
+  var imgNamePtrs = [];
+  var imgDataPtrs = [];
+  var imgSizes    = [];
+
+  var keys = Object.keys(imgs);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var buffer = imgs[key];
+    var data = new Uint8Array(buffer);
+
+    var iptr = CanvasKit._malloc(data.byteLength);
+    CanvasKit.HEAPU8.set(data, iptr);
+    imgDataPtrs.push(iptr);
+    imgSizes.push(data.byteLength);
+
+    // lengthBytesUTF8 and stringToUTF8Array are defined in the emscripten
+    // JS.  See https://kripken.github.io/emscripten-site/docs/api_reference/preamble.js.html#stringToUTF8
+    // Add 1 for null terminator
+    var strLen = lengthBytesUTF8(key) + 1;
+    var strPtr = CanvasKit._malloc(strLen);
+
+    stringToUTF8(key, strPtr, strLen);
+    imgNamePtrs.push(strPtr);
+  }
+
+  var namesPtr     = copy1dArray(imgNamePtrs, CanvasKit.HEAPU32);
+  var imgsPtr      = copy1dArray(imgDataPtrs, CanvasKit.HEAPU32);
+  var imgSizesPtr  = copy1dArray(imgSizes,    CanvasKit.HEAPU32);
+
+  return CanvasKit._MakeManagedAnimation(json, keys.length, namesPtr, imgsPtr, imgSizesPtr,
+                                               0, nullptr, nullptr, nullptr);
+};
