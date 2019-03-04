0b04911629f300d5c810d25f3d36cc459a2e949a
Leon Scroggins III <scroggo@google.com>
2019-03-04 13:53:02 -0500

    Fix Android host-side Windows build
    Bug: b/123085931
    Bug: b/117921091
    
    Based on ag/6614841
    
    Don't use the default Windows FontMgr/FontHost. Android will
    handle fonts.
    
    Remove _HAS_EXCEPTIONS=0, which was added by gn/BUILD.gn
    
    Change-Id: I93a72a550b2dbb9adc498638853f6e95c5d4e94f

--- gn/gn_to_bp_utils.py
+++ gn/gn_to_bp_utils.py
@@ -106,6 +106,8 @@ def WriteUserConfig(userConfigPath, defines):
   defines.remove('SKIA_IMPLEMENTATION=1')  # don't export this define.
   if 'WIN32_LEAN_AND_MEAN' in defines:     # Controlled by the Android build
     defines.remove('WIN32_LEAN_AND_MEAN')
+  if '_HAS_EXCEPTIONS=0' in defines:       # Controlled by the Android build
+    defines.remove('_HAS_EXCEPTIONS=0')
 
   #... and all the #defines we want to put in SkUserConfig.h.
   with open(userConfigPath, 'w') as f:
