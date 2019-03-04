573b43023a002d13d8ebc911ebb1237ad5b47ae3
Nathaniel Nifong <nifong@google.com>
2019-03-04 14:38:26 -0500

    Initial wasm debugger
    Bug: skia:
    Change-Id: I4d38e60f03bb315dee9c9db655ce168e9b3b475f

---
+++ experimental/wasm-skp-debugger/serve.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Google LLC
+
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    pass
+
+Handler.extensions_map['.js'] = 'application/javascript'
+# Without the correct MIME type, async compilation doesn't work
+Handler.extensions_map['.wasm'] = 'application/wasm'
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
