bd66269c5bfac14f20347b4b02bc805d04370581
Ben Wagner <benjaminwagner@google.com>
2019-03-04 17:45:30 -0500

    [infra] Upgrade Debian GCE bots to 9.8.
    Change-Id: I4996779ad7a634bce6e39eb6defa4bc0cea026d7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/197203
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -41,7 +41,7 @@ const (
 	ISOLATE_WIN_TOOLCHAIN_NAME = "Housekeeper-PerCommit-IsolateWinToolchain"
 
 	DEFAULT_OS_DEBIAN    = "Debian-9.4"
-	DEFAULT_OS_LINUX_GCE = DEFAULT_OS_DEBIAN
+	DEFAULT_OS_LINUX_GCE = "Debian-9.8"
 	DEFAULT_OS_MAC       = "Mac-10.13.6"
 	DEFAULT_OS_UBUNTU    = "Ubuntu-14.04"
 	DEFAULT_OS_WIN       = "Windows-2016Server-14393"
@@ -526,7 +526,7 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 				glog.Fatalf("Please update defaultSwarmDimensions for SKQP::Emulator %s %s.", parts["os"], parts["model"])
 			}
 			d["cpu"] = "x86-64-i5-7260U"
-			d["os"] = "Debian-9.4"
+			d["os"] = DEFAULT_OS_DEBIAN
 			// KVM means Kernel-based Virtual Machine, that is, can this vm virtualize commands
 			// For us, this means, can we run an x86 android emulator on it.
 			// kjlubick tried running this on GCE, but it was a bit too slow on the large install.
