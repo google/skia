327abd464c7d3fc14e60f21d5baf815a0d83d06b
Stephen White <senorblanco@chromium.org>
2019-03-05 11:18:44 -0500

    Dawn backend (experimental).
    Change-Id: If03831fc4c542d1afe524c806c930ce88a3f23c0

--- src/gpu/GrShaderCaps.h
+++ src/gpu/GrShaderCaps.h
@@ -304,6 +304,7 @@ private:
     friend class GrGLCaps;
     friend class GrMockCaps;
     friend class GrMtlCaps;
+    friend class GrDawnCaps;
     friend class GrVkCaps;
     friend class SkSL::ShaderCapsFactory;
 };
