4a44bae613e82e9803a347347a437d7f3db59236
Mike Klein <mtklein@google.com>
2019-03-07 12:03:49 -0600

    treat F16Norm like F16 as far as SW tranfer functions go
    Change-Id: I5dd9ad47fc43d4ba859c6b8f1253e1788258c48e

--- src/core/SkColorSpaceXformSteps.h
+++ src/core/SkColorSpaceXformSteps.h
@@ -37,7 +37,13 @@ struct SkColorSpaceXformSteps {
     void apply(SkRasterPipeline*, bool src_is_normalized) const;
 
     void apply(SkRasterPipeline* p, SkColorType srcCT) const {
+    #if 0
         this->apply(p, srcCT < kRGBA_F16_SkColorType);
+    #else
+        // F16Norm is normalized, but to make diffing with F16 easier we
+        // intentionally take the slower, non-normalized path here.
+        this->apply(p, srcCT < kRGBA_F16Norm_SkColorType);
+    #endif
     }
 
     Flags flags;
