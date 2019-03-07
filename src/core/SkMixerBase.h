4d4d944623b510ed1bba657f6d2ef8cc4a445329
Mike Reed <reed@google.com>
2019-03-07 17:43:25 -0500

    Experimental: add mixers
    Bug: skia:
    Change-Id: Ic9650b8ea6f6278e6bfd657e90befbf9e71f383c

---
+++ src/core/SkMixerBase.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkMixerPriv_DEFINED
+#define SkMixerPriv_DEFINED
+
+#include "SkMixer.h"
+
+class GrFragmentProcessor;
+class GrRecordingContext;
+class SkArenaAlloc;
+class SkRasterPipeline;
+class SkString;
+
+class SkMixerBase : public SkMixer {
+public:
+    virtual void appendStages(SkRasterPipeline*, SkColorSpace*, SkArenaAlloc*) const = 0;
+
+#if SK_SUPPORT_GPU
+    /**
+     *  A subclass may implement this factory function to work with the GPU backend. It returns
+     *  a GrFragmentProcessor that implemets the color filter in GPU shader code.
+     *
+     *  The fragment processor receives a premultiplied input color and produces a premultiplied
+     *  output color.
+     *
+     *  A null return indicates that the color filter isn't implemented for the GPU backend.
+     */
+    virtual std::unique_ptr<GrFragmentProcessor> asFragmentProcessor(
+            GrRecordingContext*, const GrColorSpaceInfo& dstColorSpaceInfo) const;
+#endif
+
+    static void RegisterFlattenables();
+
+    static SkFlattenable::Type GetFlattenableType() {
+        return kSkMixer_Type;
+    }
+
+    SkFlattenable::Type getFlattenableType() const override {
+        return kSkMixer_Type;
+    }
+
+    static sk_sp<SkMixer> Deserialize(const void* data, size_t size,
+                                      const SkDeserialProcs* procs = nullptr) {
+        return sk_sp<SkMixer>(static_cast<SkMixer*>(
+                                  SkFlattenable::Deserialize(
+                                  kSkMixer_Type, data, size, procs).release()));
+    }
+
+private:
+    typedef SkFlattenable SkMixer;
+};
+
+#endif
