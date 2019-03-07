848e9e1dfc239203f248d73124d53fa16021ae4e
Mike Reed <reed@google.com>
2019-03-07 09:36:36 -0500

    undo old colorspace hacks related to savelayers
    ... there appears also to be a bug in the 'global' imagefiltercache,
    where it doesn't respect the outputProps colorspace..?
    
    Bug: skia:8793, skia:8830
    Change-Id: Ib8a113fa21eae5f581b44b051a790be9133aa411

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -951,11 +951,19 @@ void SkCanvas::DrawDeviceWithFilter(SkBaseDevice* src, const SkImageFilter* filt
     }
 }
 
+// This is shared by all backends, but contains raster-specific thoughts. Can we defer to the
+// device to perform this?
 static SkImageInfo make_layer_info(const SkImageInfo& prev, int w, int h, const SkPaint* paint) {
     // Need to force L32 for now if we have an image filter.
-    // If filters ever support other colortypes, e.g. sRGB or F16, we can remove this check.
+    // If filters ever support other colortypes, e.g. F16, we can modify this check.
     if (paint && paint->getImageFilter()) {
+#ifdef SK_SUPPORT_LEGACY_RASTERLAYERCOLORSPACE
         return SkImageInfo::MakeN32Premul(w, h);
+#else
+        // TODO: can we query the imagefilter, to see if it can handle floats (so we don't always
+        //       use N32 when the layer itself was float)?
+        return SkImageInfo::MakeN32Premul(w, h, prev.refColorSpace());
+#endif
     }
 
     SkColorType ct = prev.colorType();
