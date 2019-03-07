ec841f71ee85df3e198d5b59bb6b9d924dc3ca27
Kevin Lubick <kjlubick@google.com>
2019-03-07 08:43:11 -0500

    [canvaskit] Increase timeout and add logging to further diagnose test flakiness
    Bug: skia:8810
    Change-Id: Ie1ddbd70a7521aaf7bd0e9fda7ad7a53a01db4b7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/198600
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- experimental/canvaskit/tests/canvaskitinit.js
+++ experimental/canvaskit/tests/canvaskitinit.js
@@ -7,7 +7,11 @@ const LoadCanvasKit = new Promise(function(resolve, reject) {
     CanvasKitInit({
         locateFile: (file) => '/canvaskit/'+file,
     }).ready().then((loaded) => {
+        console.log('canvaskit loaded', new Date());
         CanvasKit = loaded;
         resolve();
+    }).catch((e) => {
+        console.error('canvaskit failed to load', new Date(), e);
+        reject();
     });
 });
