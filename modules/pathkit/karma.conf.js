521f6c21cf63348c8133fbdc5eac33e20b411d5b
Kevin Lubick <kjlubick@google.com>
2019-03-11 16:52:03 -0400

    [pathkit] Clean up perf/test init
    This will hopefully help with flakiness.
    
    Bug: skia:8810
    Change-Id: Id2fa9abcc0e95f0cf8b08557215766b4f9c57478

--- modules/pathkit/karma.conf.js
+++ modules/pathkit/karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
       { pattern: 'tests/*.json', included:false, served:true},
       'tests/testReporter.js',
       'npm-wasm/bin/test/pathkit.js',
+      'tests/pathkitinit.js',
       'tests/*.spec.js'
     ],
 
@@ -39,8 +40,8 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    browserDisconnectTimeout: 15000,
-    browserNoActivityTimeout: 15000,
+    browserDisconnectTimeout: 20000,
+    browserNoActivityTimeout: 20000,
 
     // start these browsers
     browsers: ['Chrome'],
