e9e8bcf945ca105b447263021f077b92336179f7
Robert Phillips <robertphillips@google.com>
2019-03-11 11:03:13 -0400

    Always try to reduce opList splitting in DDL contexts/drawingManagers
    This may get us in trouble w/ local DDL testing (since we run all our GMs through DDLs). For Chrome this shouldn't yet be a problem (since they are only using DDLs or compositing).
    
    This does mean we're on a tight timeline to land predictive intermediate flushes before Chrome starts using DDLs for rasterization.
    
    Change-Id: I0bb95c075cff3ee49498ff267d76c3a61d16373e

--- src/gpu/GrRecordingContext.cpp
+++ src/gpu/GrRecordingContext.cpp
@@ -64,7 +64,9 @@ bool GrRecordingContext::init(sk_sp<const GrCaps> caps, sk_sp<GrSkSLFPFactoryCac
     return true;
 }
 
-void GrRecordingContext::setupDrawingManager(bool explicitlyAllocate, bool sortOpLists) {
+void GrRecordingContext::setupDrawingManager(bool explicitlyAllocate,
+                                             bool sortOpLists,
+                                             bool reduceOpListSplitting) {
     GrPathRendererChain::Options prcOptions;
     prcOptions.fAllowPathMaskCaching = this->options().fAllowPathMaskCaching;
 #if GR_TEST_UTILS
@@ -94,16 +96,12 @@ void GrRecordingContext::setupDrawingManager(bool explicitlyAllocate, bool sortO
     }
 #endif
 
-    // SHORT TERM TODO: until intermediate flushes at allocation time are added we need to obey the
-    // reduceOpListSplitting flag. Once that lands we should always reduce opList splitting in
-    // DDL contexts/drawing managers. We should still obey the options for non-DDL drawing managers
-    // until predictive intermediate flushes are added (i.e., we can't reorder forever).
     fDrawingManager.reset(new GrDrawingManager(this,
-                                                prcOptions,
-                                                textContextOptions,
-                                                explicitlyAllocate,
-                                                sortOpLists,
-                                                this->options().fReduceOpListSplitting));
+                                               prcOptions,
+                                               textContextOptions,
+                                               explicitlyAllocate,
+                                               sortOpLists,
+                                               reduceOpListSplitting));
 }
 
 void GrRecordingContext::abandonContext() {
