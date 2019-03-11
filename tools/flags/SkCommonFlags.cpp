88b93da63d46600f78c34759a83f47bc250d5456
Robert Phillips <robertphillips@google.com>
2019-03-11 14:45:43 -0400

    Always explicitly allocate except in Android Framework (take 2)
    This will turn on explicit allocation (w/o opList sorting) in Chrome. It leaves the old allocation system in place in Android Framework and some of Skia's older bots.
    
    We want:
      Flutter and Chrome to always explicitly allocate but not sort opLists outside of DDLs
      Android to never explicitly allocate and, thus, automatically never sort opLists
    
    This cannot land until after the following Chrome CL lands:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1516096 (Disable opList sorting within Skia)
    
    
    Change-Id: Ic7d6a1a77a08f2fe42324773f62cccf8175ab3d7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/199931
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tools/flags/SkCommonFlags.cpp
+++ tools/flags/SkCommonFlags.cpp
@@ -166,7 +166,6 @@ DEFINE_string(pr, "all",
               "[~]none [~]dashline [~]nvpr [~]ccpr [~]aahairline [~]aaconvex [~]aalinearizing "
               "[~]small [~]tess] [~]all");
 
-DEFINE_bool(disableExplicitAlloc, false, "Disable explicit allocation of GPU resources");
 DEFINE_bool(reduceOpListSplitting, false, "Improve opList sorting");
 
 void SetCtxOptionsFromCommonFlags(GrContextOptions* ctxOptions) {
@@ -177,12 +176,6 @@ void SetCtxOptionsFromCommonFlags(GrContextOptions* ctxOptions) {
     ctxOptions->fSuppressGeometryShaders = FLAGS_noGS;
     ctxOptions->fGpuPathRenderers = CollectGpuPathRenderersFromFlags();
     ctxOptions->fDisableDriverCorrectnessWorkarounds = FLAGS_disableDriverCorrectnessWorkarounds;
-
-    if (FLAGS_disableExplicitAlloc) {
-        ctxOptions->fExplicitlyAllocateGPUResources = GrContextOptions::Enable::kNo;
-        // Can't have sorting enabled when explicit allocation is disabled.
-        ctxOptions->fSortRenderTargets = GrContextOptions::Enable::kNo;
-    }
 
     if (FLAGS_reduceOpListSplitting) {
         ctxOptions->fReduceOpListSplitting = GrContextOptions::Enable::kYes;
