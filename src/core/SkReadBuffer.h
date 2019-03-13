08e6bd4e6cdf775600086c84f89857535d241cf3
Mike Klein <mtklein@google.com>
2019-03-13 14:11:35 +0000

    Revert "add private non-copying SkReadBuffer::readString()"
    This reverts commit 232783f0c5c9e3244c977554e368c5a07768d397.
    
    Reason for revert: several fuzzers find nullptr deref.  Will investigate and reland.
    
    Original change's description:
    > add private non-copying SkReadBuffer::readString()
    > 
    > ... and use it when looking up deserialized type factories.
    > 
    > Change-Id: I23d6dd549d4258908a961971a8de380776c91e1c
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/171220
    > Commit-Queue: Mike Klein <mtklein@google.com>
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    
    TBR=mtklein@google.com,khushalsagar@chromium.org
    
    Change-Id: I552f8de413dd52e1480c8ce1e7e84a072a54cb93
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/200940
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/core/SkReadBuffer.h
+++ src/core/SkReadBuffer.h
@@ -99,6 +99,7 @@ public:
     // peek
     uint8_t peekByte();
 
+    // strings -- the caller is responsible for freeing the string contents
     void readString(SkString* string);
 
     // common data structures
@@ -209,8 +210,6 @@ public:
     SkFilterQuality checkFilterQuality();
 
 private:
-    const char* readString(size_t* length);
-
     void setInvalid();
     bool readArray(void* value, size_t size, size_t elementSize);
     void setMemory(const void*, size_t);
