d93d2702f1977f0b33cb1efc24e496a9068c4a86
Mike Klein <mtklein@google.com>
2019-03-14 09:00:30 -0500

    implement luminance-to-alpha as a matrix
    Maybe we try this?
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I8b0c55c0ff3c4b726a7992fd493dac008c7d3cb7

--- src/effects/SkLumaColorFilter.cpp
+++ src/effects/SkLumaColorFilter.cpp
@@ -6,37 +6,13 @@
  */
 
 #include "SkLumaColorFilter.h"
-#include "SkColorData.h"
-#include "SkEffectPriv.h"
-#include "SkRasterPipeline.h"
-#include "SkString.h"
-
-#if SK_SUPPORT_GPU
-#include "GrContext.h"
-#include "effects/GrLumaColorFilterEffect.h"
-#include "glsl/GrGLSLFragmentProcessor.h"
-#include "glsl/GrGLSLFragmentShaderBuilder.h"
-#endif
-
-void SkLumaColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const {
-    rec.fPipeline->append(SkRasterPipeline::luminance_to_alpha);
-}
 
 sk_sp<SkColorFilter> SkLumaColorFilter::Make() {
-    return sk_sp<SkColorFilter>(new SkLumaColorFilter);
+    const SkScalar luma_to_alpha[] = {
+              0,       0,       0, 0, 0,
+              0,       0,       0, 0, 0,
+              0,       0,       0, 0, 0,
+        0.2126f, 0.7152f, 0.0722f, 0, 0,
+    };
+    return SkColorFilter::MakeMatrixFilterRowMajor255(luma_to_alpha);
 }
-
-SkLumaColorFilter::SkLumaColorFilter() : INHERITED() {}
-
-sk_sp<SkFlattenable> SkLumaColorFilter::CreateProc(SkReadBuffer&) {
-    return Make();
-}
-
-void SkLumaColorFilter::flatten(SkWriteBuffer&) const {}
-
-#if SK_SUPPORT_GPU
-std::unique_ptr<GrFragmentProcessor> SkLumaColorFilter::asFragmentProcessor(
-        GrRecordingContext*, const GrColorSpaceInfo&) const {
-    return GrLumaColorFilterEffect::Make();
-}
-#endif
