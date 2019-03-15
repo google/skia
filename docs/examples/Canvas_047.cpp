d32da7a5780323aa1e64ee6cafa2446d41d4533a
Hal Canary <halcanary@google.com>
2019-03-15 17:38:52 +0000

    Revert "Compile all fiddle examples locally"
    This reverts commit 83c2f70b51c1f276e491cd63336c943a027c3474.
    
    Reason for revert: broke some builders
    
    Original change's description:
    > Compile all fiddle examples locally
    > [......]
    > 
    > Change-Id: I14a31348a9ccaaa31f65424b91e3a3533d2583a7
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/198824
    > Commit-Queue: Hal Canary <halcanary@google.com>
    > Reviewed-by: Leon Scroggins <scroggo@google.com>
    > Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    
    TBR=hcm@google.com,halcanary@google.com,bungeman@google.com,jcgregorio@google.com,scroggo@google.com
    
    Change-Id: I8a6aa444d7f62aa73565ca4a7393e050872b6ecc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- docs/examples/Canvas_047.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 Google LLC.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-#include "fiddle/examples.h"
-// HASH=c0d5fa544759704768f47cac91ae3832
-REG_FIDDLE(Canvas_047, 256, 256, true, 0) {
-void draw(SkCanvas* canvas) {
-    SkDebugf("isIdentity %s\n", canvas->getTotalMatrix().isIdentity() ? "true" : "false");
-}
-}  // END FIDDLE
--- docs/examples/Rect_053.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 Google LLC.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-#include "fiddle/examples.h"
-// HASH=795061764b10c9e05efb466c9cb60644
-REG_FIDDLE(Rect_053, 256, 256, true, 0) {
-void draw(SkCanvas* canvas) {
-    SkDebugf("%s intersection", SkRect::Intersects({10, 40, 50, 80}, {30, 60, 70, 90}) ? "" : "no ");
-}
-}  // END FIDDLE
