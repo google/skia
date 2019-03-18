b20ec4d8378b427a82c8e9981a62f63dd314f575
Robert Phillips <robertphillips@google.com>
2019-03-18 11:00:51 -0400

    Unblock Flutter
    There is a lot more cleanup associated w/ removing this but this is the minimum that will un-block Flutter.
    
    Change-Id: I7b8595007f912d9ddbb2c9d7a9989e10101f9c92

--- include/private/GrContext_Base.h
+++ include/private/GrContext_Base.h
@@ -8,16 +8,6 @@
 #ifndef GrContext_Base_DEFINED
 #define GrContext_Base_DEFINED
 
-// Perform old-style (non-explicit) allocation in the Android Framework and on older
-// (non-Vulkan supporting) Android devices. The latter is to, at least, have some of
-// Skia's bots exercise the old allocation scheme.
-#if !defined(SK_OLD_STYLE_RESOURCE_ALLOCATION)
-    #if defined(SK_BUILD_FOR_ANDROID_FRAMEWORK) || \
-        (defined(SK_BUILD_FOR_ANDROID) && !defined(SK_VULKAN))
-            #define SK_OLD_STYLE_RESOURCE_ALLOCATION
-    #endif
-#endif
-
 #include "SkRefCnt.h"
 #include "GrContextOptions.h"
 #include "GrTypes.h"
@@ -67,7 +57,7 @@ protected:
      */
     const GrContextOptions& options() const { return fOptions; }
 
-    bool explicitlyAllocateGPUResources() const;
+    bool explicitlyAllocateGPUResources() const { return true; }
 
     const GrCaps* caps() const;
     sk_sp<const GrCaps> refCaps() const;
