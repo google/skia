d88c2e8e09794eb15a05017ef9e79b1ce9532f86
Hal Canary <halcanary@google.com>
2019-03-19 10:15:25 -0400

    Docs: rename examples files to match names
    Change-Id: I9a6b2780cfdd91cdd16d5a08eb9ba1214492cb7e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/201982
    Commit-Queue: Hal Canary <halcanary@google.com>
    Reviewed-by: Hal Canary <halcanary@google.com>

--- docs/examples/Point_offset_3.cpp
+++ docs/examples/Point_offset_3.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 #include "fiddle/examples.h"
 // HASH=02750ceaa874f956e6e6544ef6b858ee
-REG_FIDDLE(Point_offset, 256, 128, false, 0) {
+REG_FIDDLE(Point_offset_3, 256, 128, false, 0) {
 void draw(SkCanvas* canvas) {
     SkPaint paint;
     paint.setAntiAlias(true);
