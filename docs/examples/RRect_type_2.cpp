0bcf995e06239f0157f51b8f09ec14a198d09197
Hal Canary <halcanary@google.com>
2019-03-19 15:54:16 -0400

    Docs: fix more case-sensitive names
    Change-Id: I487a3f9a68e6f6a00edfa6aec3448d5bae7a7911

--- docs/examples/RRect_type_2.cpp
+++ docs/examples/RRect_type_2.cpp
@@ -3,7 +3,7 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 #include "fiddle/examples.h"
 // HASH=1080805c8449406a4e26d694bc56d2dc
-REG_FIDDLE(RRect_type, 256, 100, false, 0) {
+REG_FIDDLE(RRect_type_2, 256, 100, false, 0) {
 void draw(SkCanvas* canvas) {
     SkRRect rrect = SkRRect::MakeRect({10, 10, 100, 50});
     SkRRect rrect2(rrect);
