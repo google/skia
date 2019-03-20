17dabb2022dbb70dac5aed3c140808af2ae3f527
Mike Klein <mtklein@google.com>
2019-03-20 10:18:47 -0500

    upgrade clang_linux to LLVM 8
    Released today.
    
    Change-Id: I53132f9ff2753b8518f3d531d74a873a73455b44
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/202300
    Auto-Submit: Mike Klein <mtklein@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>

--- infra/bots/assets/clang_linux/create.py
+++ infra/bots/assets/clang_linux/create.py
@@ -15,7 +15,7 @@ import subprocess
 import tempfile
 
 REPO = "https://llvm.googlesource.com/"
-BRANCH = "release_70"
+BRANCH = "release_80"
 
 def create_asset(target_dir):
   # CMake will sometimes barf if we pass it a relative path.
