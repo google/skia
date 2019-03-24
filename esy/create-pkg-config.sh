0d8bea9a7880d9a87da3eb483b43d21e930cc32a
Manuel Hornung <manuel@hornung.in>
2019-03-24 13:46:59 +0100

    Replace test with a slimmer source version and fix install

--- esy/create-pkg-config.sh
+++ esy/create-pkg-config.sh
@@ -1,3 +1,15 @@
+if which x86_64-w64-mingw32-gcc; then
+    # TODO find out what is needed here
+    platformSpecificFlags=
+else
+    if [ "$(uname)" == "Darwin" ]; then
+        platformSpecificFlags="-framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation -std=c++1y -lstdc++"
+    else
+        # TODO find out what is needed here
+        platformSpecificFlags=
+    fi
+fi
+
 touch $cur__lib/skia.pc
 cat >$cur__lib/skia.pc << EOF
 includedir=$cur__root/include
@@ -7,5 +19,5 @@ Name: skia
 Description: 2D graphics library
 Version: $cur__version
 Cflags: -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils
-Libs: -L\${libdir} -lskia
+Libs: -L\${libdir} $platformSpecificFlags -lskia
 EOF
--- esy/test.sh
+++ esy/test.sh
@@ -7,16 +7,16 @@ if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
     # Copy runtime mingw files
     cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll $BUILDDIR/.
-    ADDITIONAL_LIBS=
+    export PKG_CONFIG_PATH=$cur__lib;$PKG_CONFIG_PATH
 else
     CC=gcc
-    if [ "$(uname)" == "Darwin" ]; then
-        ADDITIONAL_LIBS="-framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation -framework QuartzCore -framework Cocoa -framework Foundation"
-    else
-        ADDITIONAL_LIBS=-lm
-    fi
+    export PKG_CONFIG_PATH=$cur__lib:$PKG_CONFIG_PATH
+    # if [ "$(uname)" == "Darwin" ]; then
+    # else
+    # fi
 fi
 
+echo "PKG_CONFIG_PATH: $PKG_CONFIG_PATH"
 echo "Using build directory: $BUILDDIR"
 echo "Root directory: $ROOTDIR"
 
@@ -30,51 +30,10 @@ echo "Using compiler: $CC"
 export PATH=$PATH:$BUILDDIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BUILDDIR
 
-# $CC \
-# $ROOTDIR/tools/flags/SkCommandLineFlags.cpp \
-#       "tools/gpu/GrContextFactory.cpp",
-#       "tools/gpu/GrTest.cpp",
-#       "tools/gpu/MemoryCache.cpp",
-#       "tools/gpu/MemoryCache.h",
-#       "tools/gpu/ProxyUtils.cpp",
-#       "tools/gpu/TestContext.cpp",
-#       "tools/gpu/atlastext/GLTestAtlasTextRenderer.cpp",
-#       "tools/gpu/gl/GLTestContext.cpp",
-#       "tools/gpu/gl/command_buffer/GLTestContext_command_buffer.cpp",
-#       "tools/gpu/gl/null/NullGLTestContext.cpp",
-#       "tools/gpu/mock/MockTestContext.cpp",
-#       "tools/gpu/gl/mac/CreatePlatformGLTestContext_mac.cpp",
-# $ROOTDIR/tools/sk_app/CommandSet.cpp \
-# $ROOTDIR/tools/sk_app/GLWindowContext.cpp \
-# $ROOTDIR/tools/sk_app/Window.cpp \
-# $ROOTDIR/tools/sk_app/mac/GLWindowContext_mac.mm \
-# $ROOTDIR/tools/sk_app/mac/RasterWindowContext_mac.mm \
-# $ROOTDIR/tools/sk_app/mac/Window_mac.mm \
-# $ROOTDIR/tools/sk_app/mac/main_mac.mm \
-# $ROOTDIR/example/HelloWorld.cpp \
-# -o HelloWorld.exe \
-# -I$ROOTDIR/tools \
-# -I$INCLUDEDIR/android \
-# -I$INCLUDEDIR/atlastext \
-# -I$INCLUDEDIR/c \
-# -I$INCLUDEDIR/codec \
-# -I$INCLUDEDIR/config \
-# -I$INCLUDEDIR/core \
-# -I$INCLUDEDIR/docs \
-# -I$INCLUDEDIR/effects \
-# -I$INCLUDEDIR/encode \
-# -I$INCLUDEDIR/gpu \
-# -I$INCLUDEDIR/pathops \
-# -I$INCLUDEDIR/ports \
-# -I$INCLUDEDIR/private \
-# -I$INCLUDEDIR/svg \
-# -I$INCLUDEDIR/third_party \
-# -I$INCLUDEDIR/utils \
-# -L$BUILDDIR \
-# -lskia \
-# -std=c++1y \
-# -fobjc-arc \
-# $ADDITIONAL_LIBS \
-# --verbose
+$CC \
+$ROOTDIR/esy/test.cpp \
+-o test.exe \
+`pkg-config --cflags --libs skia` \
+--verbose
 
-./HelloWorld.exe
+./test.exe $BUILDDIR/test.png
