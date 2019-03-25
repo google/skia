d9d6f3618a12a9e563f5964dd0761cdfda09b257
Brian Salomon <bsalomon@google.com>
2019-03-25 11:06:12 -0400

    Avoid special shader for opaque alpha when drawing simple rectangles.
    It's a wasted shader compiler and also seems to cause a rendering bug
    in Windows Chrome on ANGLE.
    
    Bug: chromium:942565
    Change-Id: I56373ac596d4d1548d6c86cda7127bf12190a0ae

--- src/gpu/GrProcessorSet.cpp
+++ src/gpu/GrProcessorSet.cpp
@@ -235,6 +235,7 @@ GrProcessorSet::Analysis GrProcessorSet::finalize(
     }
     fFragmentProcessorOffset = colorFPsToEliminate;
     fColorFragmentProcessorCnt -= colorFPsToEliminate;
+    analysis.fHasColorFragmentProcessor = (fColorFragmentProcessorCnt != 0);
 
     bool hasMixedSampledCoverage = (GrFSAAType::kMixedSamples == fsaaType)
             && !userStencil->testAlwaysPasses((clip) ? clip->hasStencilClip() : false);
