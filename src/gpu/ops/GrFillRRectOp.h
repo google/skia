8d07007c4ed040b4b3744b80c771406f9a451c2f
Chris Dalton <csmartdalton@google.com>
2019-03-26 11:22:56 -0600

    Add MSAA and non-aa modes to GrFillRRect Op
    Adds a non-aa mode and an MSAA mode that uses the sample mask. Also
    adds a new cap to decide whether we prefer this new sample mask Op for
    large round rects, or whether it's faster to just continue drawing
    them as paths like before.
    
    Bug: skia:
    Change-Id: Iea7d9a78766f67c196a02cb833c84a0ac3f1bbac

--- src/gpu/ops/GrFillRRectOp.h
+++ src/gpu/ops/GrFillRRectOp.h
@@ -16,11 +16,16 @@ class GrFillRRectOp : public GrDrawOp {
 public:
     DEFINE_OP_CLASS_ID
 
-    static std::unique_ptr<GrFillRRectOp> Make(GrRecordingContext*, const SkMatrix&,
-                                               const SkRRect&, const GrCaps&, GrPaint&&);
+    static std::unique_ptr<GrFillRRectOp> Make(
+            GrRecordingContext*, GrAAType, const SkMatrix& viewMatrix, const SkRRect&,
+            const GrCaps&, GrPaint&&);
 
     const char* name() const override { return "GrFillRRectOp"; }
-    FixedFunctionFlags fixedFunctionFlags() const override { return FixedFunctionFlags::kNone; }
+    FixedFunctionFlags fixedFunctionFlags() const override {
+        return (GrAAType::kMSAA == fAAType)
+                ? FixedFunctionFlags::kUsesHWAA
+                : FixedFunctionFlags::kNone;
+    }
     GrProcessorSet::Analysis finalize(
             const GrCaps&, const GrAppliedClip*, GrFSAAType, GrClampType) override;
     CombineResult onCombineIfPossible(GrOp*, const GrCaps&) override;
@@ -35,22 +40,24 @@ private:
     enum class Flags {
         kNone = 0,
         kUseHWDerivatives = 1 << 0,
-        kHasLocalCoords = 1 << 1,
-        kWideColor = 1 << 2
+        kHasPerspective = 1 << 1,
+        kHasLocalCoords = 1 << 2,
+        kWideColor = 1 << 3
     };
 
     GR_DECL_BITFIELD_CLASS_OPS_FRIENDS(Flags);
 
     class Processor;
 
-    GrFillRRectOp(const GrShaderCaps&, const SkMatrix&, const SkRRect&, GrPaint&&);
+    GrFillRRectOp(GrAAType, const SkRRect&, Flags, const SkMatrix& totalShapeMatrix,
+                  GrPaint&&, const SkRect& devBounds);
 
     // These methods are used to append data of various POD types to our internal array of instance
     // data. The actual layout of the instance buffer can vary from Op to Op.
-    template <typename T> inline void* appendInstanceData(int count) {
+    template <typename T> inline T* appendInstanceData(int count) {
         static_assert(std::is_pod<T>::value, "");
         static_assert(4 == alignof(T), "");
-        return fInstanceData.push_back_n(sizeof(T) * count);
+        return reinterpret_cast<T*>(fInstanceData.push_back_n(sizeof(T) * count));
     }
 
     template <typename T, typename... Args>
@@ -61,9 +68,10 @@ private:
 
     void writeInstanceData() {}  // Halt condition.
 
+    const GrAAType fAAType;
     const SkPMColor4f fOriginalColor;
     const SkRect fLocalRect;
-    Flags fFlags = Flags::kNone;
+    Flags fFlags;
     GrProcessorSet fProcessors;
 
     SkSTArray<sizeof(float) * 16 * 4, char, /*MEM_MOVE=*/ true> fInstanceData;
