37e7df6d28672e75672c91a0ce0e09fb0abb06b7
Robert Phillips <robertphillips@google.com>
2019-03-26 09:44:00 -0400

    Fix bug in GrResourceAllocator's intermediate flushing
    Without the missing increment of fCurOpListIndex in the intermediate flush case, the drawing manager could end up re-executing an entire opList.
    
    Bug: 942538
    Change-Id: I1984dc3924871e4f50ed75a9c5f62c63a97a25dd

--- src/gpu/GrResourceAllocator.cpp
+++ src/gpu/GrResourceAllocator.cpp
@@ -47,6 +47,7 @@ void GrResourceAllocator::markEndOfOpList(int opListIndex) {
     }
 
     fEndOfOpListOpIndices.push_back(this->curOp()); // This is the first op index of the next opList
+    SkASSERT(fEndOfOpListOpIndices.count() <= fNumOpLists);
 }
 
 GrResourceAllocator::~GrResourceAllocator() {
@@ -318,6 +319,9 @@ bool GrResourceAllocator::assign(int* startIndex, int* stopIndex, AssignError* o
         return false;          // nothing to render
     }
 
+    SkASSERT(fCurOpListIndex < fNumOpLists);
+    SkASSERT(fNumOpLists == fEndOfOpListOpIndices.count());
+
     *startIndex = fCurOpListIndex;
     *stopIndex = fEndOfOpListOpIndices.count();
 
@@ -334,6 +338,7 @@ bool GrResourceAllocator::assign(int* startIndex, int* stopIndex, AssignError* o
     while (Interval* cur = fIntvlList.popHead()) {
         if (fEndOfOpListOpIndices[fCurOpListIndex] < cur->start()) {
             fCurOpListIndex++;
+            SkASSERT(fCurOpListIndex < fNumOpLists);
         }
 
         this->expire(cur->start());
@@ -361,6 +366,9 @@ bool GrResourceAllocator::assign(int* startIndex, int* stopIndex, AssignError* o
                     // guaranteed to survive the partial flush lest they become zombies (i.e.,
                     // holding a deleted surface proxy).
                     if (const Interval* tmp = fIntvlList.peekHead()) {
+                        SkASSERT(fEndOfOpListOpIndices[fCurOpListIndex] == tmp->start());
+                        fCurOpListIndex++;
+                        SkASSERT(fCurOpListIndex < fNumOpLists);
                         this->expire(tmp->start());
                     } else {
                         this->expire(std::numeric_limits<unsigned int>::max());
@@ -418,6 +426,9 @@ bool GrResourceAllocator::assign(int* startIndex, int* stopIndex, AssignError* o
                 // guaranteed to survive the partial flush lest they become zombies (i.e.,
                 // holding a deleted surface proxy).
                 if (const Interval* tmp = fIntvlList.peekHead()) {
+                    SkASSERT(fEndOfOpListOpIndices[fCurOpListIndex] == tmp->start());
+                    fCurOpListIndex++;
+                    SkASSERT(fCurOpListIndex < fNumOpLists);
                     this->expire(tmp->start());
                 } else {
                     this->expire(std::numeric_limits<unsigned int>::max());
