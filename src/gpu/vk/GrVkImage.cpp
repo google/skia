e1d096b5fc857495c03b6ee2835c3b55acc135cb
Greg Daniel <egdaniel@google.com>
2019-03-29 09:48:05 -0400

    Remove read access flags from src of memory barriers.
    The src access flags on memory barriers are used to make writes to a specific
    memory location by a specific agent and reference object available to the
    entire domain (device in this case). When the previous operation done on a
    reference was a read, we don't not need to make changes to that memory available.
    
    Bug: skia:8933
    Change-Id: I756b391f02cb8bf47bbe386a0b9e3b7ba788098e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/204960
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    (cherry picked from commit ab79ff989e8a0a9b98cf28c1dd8d774756c9492a)

--- src/gpu/vk/GrVkImage.cpp
+++ src/gpu/vk/GrVkImage.cpp
@@ -62,11 +62,10 @@ VkAccessFlags GrVkImage::LayoutToSrcAccessMask(const VkImageLayout layout) {
         flags = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
     } else if (VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL == layout) {
         flags = VK_ACCESS_TRANSFER_WRITE_BIT;
-    } else if (VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL == layout) {
-        flags = VK_ACCESS_TRANSFER_READ_BIT;
-    } else if (VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL == layout) {
-        flags = VK_ACCESS_SHADER_READ_BIT;
-    } else if (VK_IMAGE_LAYOUT_PRESENT_SRC_KHR == layout) {
+    } else if (VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL == layout ||
+               VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL == layout ||
+               VK_IMAGE_LAYOUT_PRESENT_SRC_KHR == layout) {
+        // There are no writes that need to be made available
         flags = 0;
     }
     return flags;
