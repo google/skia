c7914d02c203376eb2825303eca497b0b63f9da1
Mike Reed <reed@google.com>
2019-03-30 14:27:53 -0400

    Start to unify signatures: return bool
    Change-Id: If4a8e06f3a176697e3f5136dfc7ab1fbb347da70
    Bug: skia:8937

--- src/effects/SkLumaColorFilter.cpp
+++ src/effects/SkLumaColorFilter.cpp
@@ -18,10 +18,11 @@
 #include "glsl/GrGLSLFragmentShaderBuilder.h"
 #endif
 
-void SkLumaColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const {
+bool SkLumaColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const {
     rec.fPipeline->append(SkRasterPipeline::luminance_to_alpha);
     rec.fPipeline->append(SkRasterPipeline::clamp_0);
     rec.fPipeline->append(SkRasterPipeline::clamp_1);
+    return true;
 }
 
 sk_sp<SkColorFilter> SkLumaColorFilter::Make() {
--- src/effects/SkTableColorFilter.cpp
+++ src/effects/SkTableColorFilter.cpp
@@ -99,7 +99,7 @@ public:
         kB_Flag = 1 << 3,
     };
 
-    void onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const override {
+    bool onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const override {
         const uint8_t *r = gIdentityTable,
                       *g = gIdentityTable,
                       *b = gIdentityTable,
@@ -122,6 +122,7 @@ public:
         if (!definitelyOpaque) {
             p->append(SkRasterPipeline::premul);
         }
+        return true;
     }
 
 protected:
