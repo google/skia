7ed9fd52e1b0c9eaf9faaf962787a2c8cc1291c9
Michael Ludwig <michaelludwig@google.com>
2019-04-02 14:41:22 -0400

    Do not lose existing paint shaders when drawing alpha images
    This fixes the case where SkBitmapDevice or SkDraw falls back to use an SkShader
    to draw the image. Previously it just assigned a new shader to a paint clone,
    which worked fine with color images that are defined to ignore any set shader.
    However, with alpha images it would overwrite the effect. This changes it to
    compose the image shader with the original shader.
    
    It fixes the behavior of GMs bleed_alpha_image_shader, bleed_alpha_bmp_shader,
    and alpha_image.
    
    Bug: skia:
    Change-Id: Iaf0552ddb9991dd8d414b76f625a45cd167b7070

--- src/shaders/SkImageShader.cpp
+++ src/shaders/SkImageShader.cpp
@@ -272,6 +272,22 @@ sk_sp<SkShader> SkMakeBitmapShader(const SkBitmap& src, SkShader::TileMode tmx,
                                tmx, tmy, localMatrix);
 }
 
+sk_sp<SkShader> SkMakeBitmapShaderForPaint(const SkPaint& paint, const SkBitmap& src,
+                                           SkShader::TileMode tmx, SkShader::TileMode tmy,
+                                           const SkMatrix* localMatrix, SkCopyPixelsMode mode) {
+    auto s = SkMakeBitmapShader(src, tmx, tmy, localMatrix, mode);
+    if (!s) {
+        return nullptr;
+    }
+    if (src.colorType() == kAlpha_8_SkColorType && paint.getShader()) {
+        // Compose the image shader with the paint's shader. Alpha images+shaders should output the
+        // texture's alpha multiplied by the shader's color. DstIn (d*sa) will achieve this with
+        // the source image and dst shader (MakeCompose takes dst first, src second).
+        s = SkShader::MakeCompose(paint.refShader(), std::move(s), SkBlendMode::kDstIn);
+    }
+    return s;
+}
+
 void SkShaderBase::RegisterFlattenables() { SK_REGISTER_FLATTENABLE(SkImageShader); }
 
 bool SkImageShader::onAppendStages(const SkStageRec& rec) const {
