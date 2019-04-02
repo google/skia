c274e99d0f51104363a41f8b46729bf67a4b347e
Jim Van Verth <jvanverth@google.com>
2019-04-02 11:45:59 -0400

    Updates to iOS build
    * Enable data bundling for all test apps unless specifically disabled.
    * Add support to bundle symbols so that the stack trace in Instruments
      is correct.
    
    Bug: skia:7525
    Change-Id: I5eef9fa21ecee8f790b0736f5e23c9d678e47bef

---
+++ gn/gen_symbols_ios.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+#
+# Copyright 2019 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import subprocess
+import sys
+
+# Arguments to the script:
+#  app              path to binary to package, e.g. out/Debug/gen/dm
+app = sys.argv[1]     # pylint: disable=unbalanced-tuple-unpacking
+
+# write dSYM directory
+subprocess.check_call(['dsymutil', app])
