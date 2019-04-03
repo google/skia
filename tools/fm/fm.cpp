aaed15a4e35e722cf26d2a29321e092f7194b447
Mike Klein <mtklein@google.com>
2019-04-03 10:31:25 -0400

    don't have fm_bot send fm empty sources to run
    Running fm_bot at head I'm confronted with a puzzling error:
    
        2019/04/03 10:26:01
        out/fm -b cpu -w foo -s big_rrect_rect_aa_effect filterbitmap_image_mandrill_512.png                    #failed (exit status 1):
        Don't understand --source ... bailing out.
    
        2019/04/03 10:26:05 1 invocations of out/fm failed
    
    And of course running that command works fine.  Puzzling!
    
    To fix this, we must ctake care not to subslice sources past its end,
    which fills with defaults, a bunch of extra "" sources.  And add some ''
    around the unknown source to make the error message easier to parse.
    
    Serves me right for not testing this before I landed that tweak...
    
    Change-Id: I5c2abb35733b32a1142b0f42c4323112969ad2bb

--- tools/fm/fm.cpp
+++ tools/fm/fm.cpp
@@ -414,7 +414,7 @@ int main(int argc, char** argv) {
             }
         }
 
-        fprintf(stderr, "Don't understand --source %s... bailing out.\n", source.c_str());
+        fprintf(stderr, "Don't understand source '%s'... bailing out.\n", source.c_str());
         return 1;
     }
 
