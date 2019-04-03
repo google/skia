4aee0d13e58f8e48398fcbab6819289eb03ad232
Hal Canary <halcanary@google.com>
2019-04-03 13:35:20 -0400

    documentation examples: stop abusing enums
    Change-Id: I40224b3781193f3653395e6bb2ffd6d9543b78f6

--- docs/examples/ColorTypeBytesPerPixel.cpp
+++ docs/examples/ColorTypeBytesPerPixel.cpp
@@ -3,10 +3,26 @@
 #include "fiddle/examples.h"
 // HASH=09ef49d07cb7005ba3e34d5ea53896f5
 REG_FIDDLE(ColorTypeBytesPerPixel, 256, 192, false, 0) {
+static const char* color_type_to_string(SkColorType v) {
+    switch (v) {
+        case kUnknown_SkColorType: return "Unknown";
+        case kAlpha_8_SkColorType: return "Alpha_8";
+        case kRGB_565_SkColorType: return "RGB_565";
+        case kARGB_4444_SkColorType: return "ARGB_4444";
+        case kRGBA_8888_SkColorType: return "RGBA_8888";
+        case kRGB_888x_SkColorType: return "RGB_888x";
+        case kBGRA_8888_SkColorType: return "BGRA_8888";
+        case kRGBA_1010102_SkColorType: return "RGBA_1010102";
+        case kRGB_101010x_SkColorType: return "RGB_101010x";
+        case kGray_8_SkColorType: return "Gray_8";
+        case kRGBA_F16Norm_SkColorType: return "RGBA_F16Norm";
+        case kRGBA_F16_SkColorType: return "RGBA_F16";
+        case kRGBA_F32_SkColorType: return "RGBA_F32";
+        default: return nullptr;
+    }
+}
+
 void draw(SkCanvas* canvas) {
-    const char* colors[] = { "Unknown", "Alpha_8", "RGB_565", "ARGB_4444", "RGBA_8888", "RGB_888x",
-                             "BGRA_8888", "RGBA_1010102", "RGB_101010x", "Gray_8", "RGBA_F16Norm",
-                             "RGBA_F16" };
     SkPaint paint;
     SkFont font(SkTypeface::MakeFromName("monospace", SkFontStyle()), 10);
     int y = 15;
@@ -19,7 +35,7 @@ void draw(SkCanvas* canvas) {
                                  } ) {
         int result = SkColorTypeBytesPerPixel(colorType);
         SkString string;
-        string.printf("%13s %4d", colors[(int) colorType], result);
+        string.printf("%13s %4d", color_type_to_string(colorType), result);
         canvas->drawString(string, 10, y += 14, font, paint);
     }
 }
--- docs/examples/ImageInfo_bytesPerPixel.cpp
+++ docs/examples/ImageInfo_bytesPerPixel.cpp
@@ -3,10 +3,26 @@
 #include "fiddle/examples.h"
 // HASH=9b6de4a07b2316228e9340e5a3b82134
 REG_FIDDLE(ImageInfo_bytesPerPixel, 256, 256, true, 0) {
+static const char* color_type_to_string(SkColorType v) {
+    switch (v) {
+        case kUnknown_SkColorType: return "Unknown";
+        case kAlpha_8_SkColorType: return "Alpha_8";
+        case kRGB_565_SkColorType: return "RGB_565";
+        case kARGB_4444_SkColorType: return "ARGB_4444";
+        case kRGBA_8888_SkColorType: return "RGBA_8888";
+        case kRGB_888x_SkColorType: return "RGB_888x";
+        case kBGRA_8888_SkColorType: return "BGRA_8888";
+        case kRGBA_1010102_SkColorType: return "RGBA_1010102";
+        case kRGB_101010x_SkColorType: return "RGB_101010x";
+        case kGray_8_SkColorType: return "Gray_8";
+        case kRGBA_F16Norm_SkColorType: return "RGBA_F16Norm";
+        case kRGBA_F16_SkColorType: return "RGBA_F16";
+        case kRGBA_F32_SkColorType: return "RGBA_F32";
+        default: return nullptr;
+    }
+}
+
 void draw(SkCanvas* canvas) {
-    const char* colors[] = {"Unknown", "Alpha_8", "RGB_565", "ARGB_4444", "RGBA_8888", "RGB_888x",
-                            "BGRA_8888", "RGBA_1010102", "RGB_101010x", "Gray_8", "RGBA_F16Norm",
-                            "RGBA_F16"};
     for (SkColorType colorType : {
     kUnknown_SkColorType, kAlpha_8_SkColorType, kRGB_565_SkColorType,
     kARGB_4444_SkColorType, kRGBA_8888_SkColorType, kRGB_888x_SkColorType,
@@ -14,8 +30,9 @@ void draw(SkCanvas* canvas) {
     kGray_8_SkColorType, kRGBA_F16_SkColorType
                                  } ) {
         SkImageInfo info = SkImageInfo::Make(1, 1, colorType, kOpaque_SkAlphaType);
+        const char* colorTypeStr = color_type_to_string(colorType);
         SkDebugf("color: k" "%s" "_SkColorType" "%*s" "bytesPerPixel: %d\n",
-                colors[colorType], 13 - strlen(colors[colorType]), " ",
+                colorTypeStr, 13 - strlen(colorTypeStr), " ",
                 info.bytesPerPixel());
     }
 }
