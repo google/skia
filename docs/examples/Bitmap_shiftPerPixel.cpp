92476bed1cb4926c9519b89f4303bdbf33b21476
Hal Canary <halcanary@google.com>
2019-04-03 13:35:20 -0400

    documentation examples: stop abusing enums
    Change-Id: I40224b3781193f3653395e6bb2ffd6d9543b78f6

--- docs/examples/Bitmap_shiftPerPixel.cpp
+++ docs/examples/Bitmap_shiftPerPixel.cpp
@@ -3,10 +3,25 @@
 #include "fiddle/examples.h"
 // HASH=56ede4b7d45c15d5936f81ac3d74f070
 REG_FIDDLE(Bitmap_shiftPerPixel, 256, 256, true, 0) {
+static const char* color_type_to_string(SkColorType v) {
+    switch (v) {
+        case kUnknown_SkColorType: return "Unknown";
+        case kAlpha_8_SkColorType: return "Alpha_8";
+        case kRGB_565_SkColorType: return "RGB_565";
+        case kARGB_4444_SkColorType: return "ARGB_4444";
+        case kRGBA_8888_SkColorType: return "RGBA_8888";
+        case kRGB_888x_SkColorType: return "RGB_888x";
+        case kBGRA_8888_SkColorType: return "BGRA_8888";
+        case kRGBA_1010102_SkColorType: return "RGBA_1010102";
+        case kRGB_101010x_SkColorType: return "RGB_101010x";
+        case kGray_8_SkColorType: return "Gray_8";
+        case kRGBA_F16Norm_SkColorType: return "RGBA_F16Norm";
+        case kRGBA_F16_SkColorType: return "RGBA_F16";
+        case kRGBA_F32_SkColorType: return "RGBA_F32";
+    }
+}
+
 void draw(SkCanvas* canvas) {
-    const char* colors[] = {"Unknown", "Alpha_8", "RGB_565", "ARGB_4444", "RGBA_8888", "RGB_888x",
-                            "BGRA_8888", "RGBA_1010102", "RGB_101010x", "Gray_8", "RGBA_F16Norm",
-                            "RGBA_F16"};
     SkImageInfo info = SkImageInfo::MakeA8(1, 1);
     SkBitmap bitmap;
     for (SkColorType colorType : {
@@ -16,8 +31,9 @@ void draw(SkCanvas* canvas) {
     kGray_8_SkColorType, kRGBA_F16_SkColorType
                                  } ) {
         bitmap.setInfo(info.makeColorType(colorType));
+        const char* colorTypeStr = color_type_to_string(colorType);
         SkDebugf("color: k" "%s" "_SkColorType" "%*s" "shiftPerPixel: %d\n",
-                colors[colorType], 14 - strlen(colors[colorType]), " ",
+                colorTypeStr, 14 - strlen(colorTypeStr), " ",
                 bitmap.shiftPerPixel());
     }
 }
