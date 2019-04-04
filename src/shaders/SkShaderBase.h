f4a8d6fc94c63efcb9ed9ad6ee9510b8c4365446
Mike Reed <reed@google.com>
2019-04-04 16:04:50 -0400

    Move localmatrix routines to shaderbase, no need to be public.
    Requires https://chromium-review.googlesource.com/c/chromium/src/+/1554040
    
    Bug: skia:8941
    Change-Id: I68593bf794d559845b638e1ecbcc658cc5cfe1b4
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/206165
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>

--- src/shaders/SkShaderBase.h
+++ src/shaders/SkShaderBase.h
@@ -178,6 +178,12 @@ public:
     }
     static void RegisterFlattenables();
 
+    /** DEPRECATED. skbug.com/8941
+     *  If this shader can be represented by another shader + a localMatrix, return that shader and
+     *  the localMatrix. If not, return nullptr and ignore the localMatrix parameter.
+     */
+    virtual sk_sp<SkShader> makeAsALocalMatrixShader(SkMatrix* localMatrix) const;
+
 protected:
     SkShaderBase(const SkMatrix* localMatrix = nullptr);
 
