55bba2ebcbb39d88453de7f5ae1999c4952e8799
Herb Derby <herb@google.com>
2019-04-04 17:23:46 -0400

    SkStrikeInterface Bulk API try 2
    Change-Id: I32c43c6f015dfaa7a5c18caec16000e1db134b2f

--- src/core/SkGlyph.h
+++ src/core/SkGlyph.h
@@ -158,6 +158,17 @@ public:
         return fPathData != nullptr && fPathData->fHasPath ? &fPathData->fPath : nullptr;
     }
 
+    bool hasPath() const {
+        // Find path must have been called to use this call.
+        SkASSERT(fPathData != nullptr);
+
+        return fPathData != nullptr && fPathData->fHasPath;
+    }
+
+    int maxDimension() const {
+        return std::max(fWidth, fHeight);
+    }
+
     // Returns the size allocated on the arena.
     size_t copyImageData(const SkGlyph& from, SkArenaAlloc* alloc);
 
