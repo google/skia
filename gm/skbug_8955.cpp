1a923bda061b521de41dbd1afa1f4ec48c79f5e0
Brian Osman <brianosman@google.com>
2019-04-05 11:35:19 -0400

    Always mark bitmap glyph runs as such, to ensure they are properly regenerated
    If all glyphs were culled (by being too small), we could end up with a
    run that never called setHasBitmap. That would prevent us from looking
    at the view matrix on a subsequent draw, when deciding if we needed to
    regenerate.
    
    Bug: skia:8955
    Change-Id: Ic7a2539762527f91bdb50ab78bdf5801bfda0034

---
+++ gm/skbug_8955.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ToolUtils.h"
+#include "gm.h"
+
+#include "SkPaint.h"
+#include "SkFont.h"
+#include "SkTextBlob.h"
+
+DEF_SIMPLE_GM(skbug_8955, canvas, 100, 100) {
+    SkPaint p;
+    SkFont font;
+    font.setSize(50);
+    auto blob = SkTextBlob::MakeFromText("+", 1, font);
+
+    canvas->save();
+    canvas->scale(0, 0);
+    canvas->drawTextBlob(blob, 30, 60, p);
+    canvas->restore();
+    canvas->drawTextBlob(blob, 30, 60, p);
+}
