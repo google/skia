eaf231684bfacd95d4ad4ecefaf66d255015b29d
Nathaniel Nifong <nifong@google.com>
2019-04-08 16:48:16 -0400

    full collection of externs
    Full list of externs that need exposing
    
    Bug: skia:
    Change-Id: I9abb3d2c595d213463f69f11b1164c965d036023

--- experimental/wasm-skp-debugger/externs.js
+++ experimental/wasm-skp-debugger/externs.js
@@ -7,6 +7,9 @@ var DebuggerView = {
 	_free: function() {},
 	onRuntimeInitialized: function() {},
 	SkpFilePlayer: function() {},
+	MakeWebGLCanvasSurface: function() {},
+	MakeGrContext: function() {},
+	MakeOnScreenGLSurface: function() {},
 
 	ColorType: {
 		RGBA_8888: {},
@@ -25,7 +28,37 @@ var DebuggerView = {
 		_flush: function() {},
 		delete: function() {},
 	},
+
+	SkpDebugPlayer: {
+		loadSkp: function() {},
+		drawTo: function() {},
+		getBounds: function() {},
+		setOverdrawVis: function() {},
+		setGpuOpBounds: function() {},
+		setClipVizColor: function() {},
+		getSize: function() {},
+		deleteCommand: function() {},
+		setCommandVisibility: function() {},
+		jsonCommandList: function() {},
+		lastCommandInfo: function() {},
+	},
 };
+
+// Public API things that are newly declared in the JS should go here.
+// It's not enough to declare them above, because closure can still erase them
+// unless they go on the prototype.
 
 DebuggerView.SkSurface.prototype.flush = function() {};
 DebuggerView.SkSurface.prototype.dispose = function() {};
+
+DebuggerView.SkpDebugPlayer.prototype.loadSkp = function() {};
+DebuggerView.SkpDebugPlayer.prototype.drawTo = function() {};
+DebuggerView.SkpDebugPlayer.prototype.getBounds = function() {};
+DebuggerView.SkpDebugPlayer.prototype.setOverdrawVis = function() {};
+DebuggerView.SkpDebugPlayer.prototype.setGpuOpBounds = function() {};
+DebuggerView.SkpDebugPlayer.prototype.setClipVizColor = function() {};
+DebuggerView.SkpDebugPlayer.prototype.getSize = function() {};
+DebuggerView.SkpDebugPlayer.prototype.deleteCommand = function() {};
+DebuggerView.SkpDebugPlayer.prototype.setCommandVisibility = function() {};
+DebuggerView.SkpDebugPlayer.prototype.jsonCommandList = function() {};
+DebuggerView.SkpDebugPlayer.prototype.lastCommandInfo = function() {};
