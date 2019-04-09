b5c685991faa79d6ac5caeacd48644eca4954426
Mike Reed <reed@google.com>
2019-04-09 19:24:33 -0400

    remove shader factory guard define
    Change-Id: Id2d700556e5c0bc55a97b88180103e34344503ee
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/207040
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Auto-Submit: Mike Reed <reed@google.com>

--- include/core/SkShader.h
+++ include/core/SkShader.h
@@ -28,10 +28,6 @@ class SkRasterPipeline;
 class GrContext;
 class GrFragmentProcessor;
 
-#ifndef SK_SUPPORT_LEGACY_SHADER_FACTORIES
-#define SK_SUPPORT_LEGACY_SHADER_FACTORIES
-#endif
-
 /** \class SkShader
  *
  *  Shaders specify the source color(s) for what is being drawn. If a paint
