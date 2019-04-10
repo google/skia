1208002548e8ce4b0f4f61ddebd200562ce64470
Greg Daniel <egdaniel@google.com>
2019-04-10 19:48:11 +0000

    Revert "Remove unnecessary flush in RenderYUVAToRGBA (take 2)"
    This reverts commit 223845f79241b5333782e2afa82ceeda4950ffa3.
    
    Reason for revert: speculative revert for chrome roll
    
    Original change's description:
    > Remove unnecessary flush in RenderYUVAToRGBA (take 2)
    > 
    > There is no real reason to flush here. If clients need to access the results immediately they should be flushing at the API level.
    > 
    > A reland of: https://skia-review.googlesource.com/c/skia/+/196645
    > 
    > It needs to land after the Chrome CL:
    > 
    > https://chromium-review.googlesource.com/c/chromium/src/+/1560554 (Flush pending work after calls to YUVGrBackendTexturesToSkImage)
    > 
    > Change-Id: I230d7433b5c1b72d8f50fd475033c942826eda94
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/206688
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Robert Phillips <robertphillips@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    
    Change-Id: Ib712184e8c4f03cb4da21ca5c445b37cd3d7cd90
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/207306
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- src/image/SkImage_GpuBase.cpp
+++ src/image/SkImage_GpuBase.cpp
@@ -373,6 +373,10 @@ bool SkImage_GpuBase::RenderYUVAToRGBA(GrContext* ctx, GrRenderTargetContext* re
     paint.addColorFragmentProcessor(std::move(fp));
 
     renderTargetContext->drawRect(GrNoClip(), std::move(paint), GrAA::kNo, SkMatrix::I(), rect);
+
+    // DDL TODO: in the promise image version we must not flush here
+    ctx->priv().flush(renderTargetContext->asSurfaceProxy());
+
     return true;
 }
 
