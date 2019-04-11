a8a707fd872827ea0913a208070d2a296ea3900d
Ben Wagner <benjaminwagner@google.com>
2019-04-11 19:53:56 -0400

    Add LenovoYogaC630 (Win arm64 bot)
    Change-Id: I70f4e01a2c55e85ee5e17a29f7f05a4a4aff2ae2
    Bug: skia:8569 skia:8976

--- infra/bots/recipe_modules/flavor/examples/full.py
+++ infra/bots/recipe_modules/flavor/examples/full.py
@@ -87,6 +87,7 @@ TEST_BUILDERS = [
   ('Test-Ubuntu17-GCC-Golo-GPU-QuadroP400-x86_64-Release-All'
    '-Valgrind_AbandonGpuContext_SK_CPU_LIMIT_SSE41'),
   'Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Release-All-Vulkan_ProcDump',
+  'Test-Win10-MSVC-LenovoYogaC630-GPU-Adreno630-arm64-Debug-All-ANGLE',
 ]
 
 # Default properties used for TEST_BUILDERS.
@@ -105,7 +106,8 @@ def GenTests(api):
       api.test(buildername) +
       api.properties(**defaultProps(buildername))
     )
-    if 'Chromebook' in buildername and not 'Build' in buildername:
+    if (('Chromebook' in buildername or 'LenovoYogaC630' in buildername) and
+        not 'Build' in buildername):
       test += api.step_data(
           'read ssh_machine.json',
           stdout=api.raw_io.output('{"user_ip":"foo@127.0.0.1"}'))
