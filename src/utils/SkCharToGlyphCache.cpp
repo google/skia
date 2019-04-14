375ab3b916381468be62ab78ff109d6d6f743b24
Mike Reed <reed@google.com>
2019-04-14 16:02:19 -0400

    use slope-guess for faster charToGlyph
    Made up a threshold between linear and slope searching.
    For a count of 100 here are the before and after timings.
    The first two lines are being changed. The latter 2 are
    the native look-ups for mac.
    
    Before
    
      15/15  MB	115	2.33µs	2.34µs	2.34µs	2.34µs	0%	▆▁▄█▇▇▂▄█▆	nonrendering	cmap_findcache_charToGlyph
      15/15  MB	66	3.47µs	3.48µs	3.49µs	3.55µs	1%	█▁▁▁▂▁▁▁▁▄	nonrendering	cmap_addcache_charToGlyph
      15/15  MB	1	1.1µs	1.13µs	1.21µs	1.98µs	22%	█▂▁▁▁▁▁▁▁▂	nonrendering	cmap_face_charToGlyph
      15/15  MB	190	1.09µs	1.1µs	1.19µs	1.64µs	17%	▁▁▁▆█▁▁▁▁▁	nonrendering	cmap_font_charToGlyph
    
    After
    
      15/15  MB	447	448ns	449ns	448ns	449ns	0%	▂▅█▅▁▆█▂▁▆	nonrendering	cmap_findcache_charToGlyph
      15/15  MB	95	2.79µs	3.03µs	3µs	3.06µs	3%	▇▇▇▇▇███▄▁	nonrendering	cmap_addcache_charToGlyph
      15/15  MB	1	1.15µs	1.16µs	1.25µs	1.99µs	21%	█▂▁▁▁▁▁▁▁▁	nonrendering	cmap_face_charToGlyph
      15/15  MB	186	1.09µs	1.1µs	1.12µs	1.27µs	5%	█▁▁▁▂▁▁▁▁▁	nonrendering	cmap_font_charToGlyph
    
    
    Bug: skia:
    Change-Id: If7da4eef3cce248393815071f342607f0c8140bb

--- src/utils/SkCharToGlyphCache.cpp
+++ src/utils/SkCharToGlyphCache.cpp
@@ -8,6 +8,8 @@
 #include "SkCharToGlyphCache.h"
 #include "../private/SkTFitsIn.h"
 
+#define NEW_WAY
+
 // TODO: consider faster ways to search big arrays
 //       bsearch
 //       slope-based (since we can compute a slope from first and last values)
@@ -27,11 +29,17 @@ template <typename T> int find_index(const T* base, int count, T value) {
     return ~count;    // append at the end
 }
 
-SkCharToGlyphCache::SkCharToGlyphCache() {}
+SkCharToGlyphCache::SkCharToGlyphCache() {
+    *fK32.append() = 0x80000000;    *fV16.append() = 0;
+    *fK32.append() = 0x7FFFFFFF;    *fV16.append() = 0;
+}
 
 SkCharToGlyphCache::~SkCharToGlyphCache() {}
 
 int SkCharToGlyphCache::findGlyphIndex(SkUnichar unichar) const {
+#ifdef NEW_WAY
+    return this->findGlyphIndex2(unichar);
+#endif
     int index;
     if (SkTFitsIn<uint16_t>(unichar)) {
         int count = f1616.count() >> 1;
@@ -51,6 +59,11 @@ int SkCharToGlyphCache::findGlyphIndex(SkUnichar unichar) const {
 }
 
 void SkCharToGlyphCache::insertCharAndGlyph(int index, SkUnichar unichar, SkGlyphID glyph) {
+#ifdef NEW_WAY
+    this->insertCharAndGlyph2(index, unichar, glyph);
+    return;
+#endif
+
     if (SkTFitsIn<uint16_t>(unichar)) {
         // count is the logical count, since our array is really two arrays back-to-back
         size_t count = f1616.size() >> 1;
@@ -91,6 +104,91 @@ void SkCharToGlyphCache::validate() const {
     SkASSERT(fKey32.size() == fValue16.size());
     for (int i = 1; i < fKey32.count(); ++i) {
         SkASSERT(fKey32[i - 1] < fKey32[i]);
+    }
+#endif
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+constexpr int kSmallCountLimit = 16;
+
+static int find_small(const SkUnichar base[], int count, SkUnichar value) {
+    int index;
+    for (index = 0;; ++index) {
+        if (value <= base[index]) {
+            if (value < base[index]) {
+                index = ~index; // not found
+            }
+            break;
+        }
+    }
+    return index;
+}
+
+static int find_big(const SkUnichar base[], int count, SkUnichar value) {
+    int index;
+    if (value <= base[1]) {
+        index = 1;
+        if (value < base[index]) {
+            index = ~index;
+        }
+    } else if (value >= base[count - 2]) {
+        index = count - 2;
+        if (value > base[index]) {
+            index = ~(index + 1);
+        }
+    } else {
+        index = (count - 2) * (value - base[1]) / (base[count - 2] - base[1]);
+        SkASSERT(index >= 1 && index < count - 1);
+        if (value > base[index]) {
+            for (;; ++index) {
+                if (value <= base[index]) {
+                    if (value < base[index]) {
+                        index = ~index; // not found
+                    }
+                    break;
+                }
+            }
+        } else {
+            for (--index;; --index) {
+                SkASSERT(index >= 0);
+                if (value >= base[index]) {
+                    if (value > base[index]) {
+                        index = ~(index + 1);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return index;
+}
+
+int SkCharToGlyphCache::findGlyphIndex2(SkUnichar unichar) const {
+    int count = fK32.count();
+    int index;
+    if (count <= kSmallCountLimit) {
+        index = find_small(fK32.begin(), count, unichar);
+    } else {
+        index = find_big(fK32.begin(), count, unichar);
+    }
+    if (index >= 0) {
+        return fV16[index];
+    }
+    return index;
+}
+
+void SkCharToGlyphCache::insertCharAndGlyph2(int index, SkUnichar unichar, SkGlyphID glyph) {
+    SkASSERT(fK32.size() == fV16.size());
+    SkASSERT((unsigned)index < fK32.size());
+    SkASSERT(unichar < fK32[index]);
+
+    *fK32.insert(index) = unichar;
+    *fV16.insert(index) = glyph;
+
+#ifdef SK_DEBUG
+    for (int i = 1; i < fK32.count(); ++i) {
+        SkASSERT(fK32[i-1] < fK32[i]);
     }
 #endif
 }
