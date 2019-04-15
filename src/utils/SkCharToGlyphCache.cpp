ff29f2865e858975402c719c72624f1bf5711978
Mike Reed <reed@google.com>
2019-04-15 07:02:54 -0400

    use slope-guess for faster charToGlyph
    Made up a threshold between linear and slope searching.
    For a count of 100 here are the before and after timings.
    The first two lines are being changed. The latter 2 are
    the native look-ups for mac.
    
    Before
    
      15/15  MB	115	2.33µs	2.34µs	2.34µs	2.34µs	0%	▆▁▄█▇▇▂▄█▆	nonrendering	cmap_findcache_charToGlyph
      15/15  MB	66	3.47µs	3.48µs	3.49µs	3.55µs	1%	█▁▁▁▂▁▁▁▁▄	nonrendering	cmap_addcache_charToGlyph
      15/15  MB	1	1.1µs	1.13µs	1.21µs	1.98µs	22%	█▂▁▁▁▁▁▁▁▂	nonrendering	cmap_face_charToGlyph
      15/15  MB	190	1.09µs	1.1µs	1.19µs	1.64µs	17%	▁▁▁▆█▁▁▁▁▁	nonrendering	cmap_font_charToGlyph
    
    After
    
      15/15  MB	447	448ns	449ns	448ns	449ns	0%	▂▅█▅▁▆█▂▁▆	nonrendering	cmap_findcache_charToGlyph
      15/15  MB	95	2.79µs	3.03µs	3µs	3.06µs	3%	▇▇▇▇▇███▄▁	nonrendering	cmap_addcache_charToGlyph
      15/15  MB	1	1.15µs	1.16µs	1.25µs	1.99µs	21%	█▂▁▁▁▁▁▁▁▁	nonrendering	cmap_face_charToGlyph
      15/15  MB	186	1.09µs	1.1µs	1.12µs	1.27µs	5%	█▁▁▁▂▁▁▁▁▁	nonrendering	cmap_font_charToGlyph
    
    
    Bug: skia:
    Change-Id: If7da4eef3cce248393815071f342607f0c8140bb

--- src/utils/SkCharToGlyphCache.cpp
+++ src/utils/SkCharToGlyphCache.cpp
@@ -8,89 +8,94 @@
 #include "SkCharToGlyphCache.h"
 #include "../private/SkTFitsIn.h"
 
-// TODO: consider faster ways to search big arrays
-//       bsearch
-//       slope-based (since we can compute a slope from first and last values)
-//
-//       Also, consider adding sentinels to key arrays so we don't have to check that
-//       index is in rage.
-//
-template <typename T> int find_index(const T* base, int count, T value) {
-    for (int index = 0; index < count; ++index) {
+SkCharToGlyphCache::SkCharToGlyphCache() {
+    *fK32.append() = 0x80000000;    *fV16.append() = 0;
+    *fK32.append() = 0x7FFFFFFF;    *fV16.append() = 0;
+}
+
+SkCharToGlyphCache::~SkCharToGlyphCache() {}
+
+// Determined experimentally. For N much larger, the slope technique is faster.
+// For N much smaller, a simple search is faster.s
+constexpr int kSmallCountLimit = 16;
+
+static int find_simple(const SkUnichar base[], int count, SkUnichar value) {
+    int index;
+    for (index = 0;; ++index) {
         if (value <= base[index]) {
             if (value < base[index]) {
                 index = ~index; // not found
             }
-            return index;
+            break;
         }
     }
-    return ~count;    // append at the end
+    return index;
 }
 
-SkCharToGlyphCache::SkCharToGlyphCache() {}
-
-SkCharToGlyphCache::~SkCharToGlyphCache() {}
-
-int SkCharToGlyphCache::findGlyphIndex(SkUnichar unichar) const {
+static int find_with_slope(const SkUnichar base[], int count, SkUnichar value) {
     int index;
-    if (SkTFitsIn<uint16_t>(unichar)) {
-        int count = f1616.count() >> 1;
-        index = find_index(f1616.begin(), count, SkToU16(unichar));
-        if (index >= 0) {
-            return f1616[count + index];
+    if (value <= base[1]) {
+        index = 1;
+        if (value < base[index]) {
+            index = ~index;
+        }
+    } else if (value >= base[count - 2]) {
+        index = count - 2;
+        if (value > base[index]) {
+            index = ~(index + 1);
         }
     } else {
-        int count = fKey32.count();
-        index = find_index(fKey32.begin(), count, unichar);
-        if (index >= 0) {
-            return fValue16[index];
+        index = (count - 2) * (value - base[1]) / (base[count - 2] - base[1]);
+        SkASSERT(index >= 1 && index < count - 1);
+        if (value > base[index]) {
+            for (;; ++index) {
+                if (value <= base[index]) {
+                    if (value < base[index]) {
+                        index = ~index; // not found
+                    }
+                    break;
+                }
+            }
+        } else {
+            for (--index;; --index) {
+                SkASSERT(index >= 0);
+                if (value >= base[index]) {
+                    if (value > base[index]) {
+                        index = ~(index + 1);
+                    }
+                    break;
+                }
+            }
         }
     }
-    SkASSERT(index < 0);
+    return index;
+}
+
+int SkCharToGlyphCache::findGlyphIndex(SkUnichar unichar) const {
+    int count = fK32.count();
+    int index;
+    if (count <= kSmallCountLimit) {
+        index = find_simple(fK32.begin(), count, unichar);
+    } else {
+        index = find_with_slope(fK32.begin(), count, unichar);
+    }
+    if (index >= 0) {
+        return fV16[index];
+    }
     return index;
 }
 
 void SkCharToGlyphCache::insertCharAndGlyph(int index, SkUnichar unichar, SkGlyphID glyph) {
-    if (SkTFitsIn<uint16_t>(unichar)) {
-        // count is the logical count, since our array is really two arrays back-to-back
-        size_t count = f1616.size() >> 1;
-        SkASSERT((unsigned)index <= count);
+    SkASSERT(fK32.size() == fV16.size());
+    SkASSERT((unsigned)index < fK32.size());
+    SkASSERT(unichar < fK32[index]);
 
-        f1616.setCount(f1616.size() + 2); // make room for key and value
-        uint16_t* base = f1616.begin();
+    *fK32.insert(index) = unichar;
+    *fV16.insert(index) = glyph;
 
-        // now slide twice, to make room for the new key and value
-        uint16_t* src = base + count + index;
-        size_t    amt = count - index;
-        sk_careful_memmove(src + 2, src, amt * sizeof(uint16_t));
-
-        src = base + index;
-        amt = count;
-        sk_careful_memmove(src + 1, src, amt * sizeof(uint16_t));
-
-        // now store the new values
-        base[index] = SkToU16(unichar);
-        base[count + 1 + index] = glyph;
-    } else {
-        SkASSERT(fKey32.size() == fValue16.size());
-        SkASSERT((unsigned)index <= fKey32.size());
-
-        *fKey32.insert(index) = unichar;
-        *fValue16.insert(index) = glyph;
-    }
-    this->validate();
-}
-
-void SkCharToGlyphCache::validate() const {
 #ifdef SK_DEBUG
-    SkASSERT((f1616.count() & 1) == 0);
-    for (int i = 1; i < f1616.count() >> 1; ++i) {
-        SkASSERT(f1616[i - 1] < f1616[i]);
-    }
-
-    SkASSERT(fKey32.size() == fValue16.size());
-    for (int i = 1; i < fKey32.count(); ++i) {
-        SkASSERT(fKey32[i - 1] < fKey32[i]);
+    for (int i = 1; i < fK32.count(); ++i) {
+        SkASSERT(fK32[i-1] < fK32[i]);
     }
 #endif
 }
