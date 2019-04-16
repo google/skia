f317f0757dafe3ca3b4566f327c056045ccabcbd
Mike Klein <mtklein@google.com>
2019-04-16 12:07:23 -0500

    pass SkVx::Vec arguments as const&
    Yet another surprising finding when looking at ARM code generation is
    that passing these values to functions by const& does make a difference,
    even when fully inlined.  I can only guess that the compiler's somehow
    more sure that way that the values won't change?  Anyway, convert all
    skvx functions that take Vec arguments to take const Vec& instead.
    
    This tweak is enough to let the natural implementation of mull()
    actually produce good code generation, so I've promoted that to SkVx.h
    and added a unit test.  Notice in the NEON case we've got a base case at
    N=8 and two recursive cases, one down to 8 as usual when N > 8, but also
    one up to 8 when N < 8.
    
    This also is another big speedup for ARMv7 NEON, bringing it to nearly
    the same speed as ARMv8 NEON on the same device.
    
    Bug: chromium:952502
    Change-Id: I0f19bab45cf02222ccc8090053ea2a4a380f1dfe

--- src/opts/SkBlitRow_opts.h
+++ src/opts/SkBlitRow_opts.h
@@ -41,32 +41,6 @@
 
 namespace SK_OPTS_NS {
 
-#if defined(SK_ARM_HAS_NEON)
-    // With NEON we can do eight u8*u8 -> u16 in one instruction, vmull_u8 (read, mul-long).
-    // TODO(mtklein): I wish I could make this a bit prettier and still get ideal codegen.
-    static inline skvx::Vec<4,uint16_t> mull(skvx::Vec<4,uint8_t> x, skvx::Vec<4,uint8_t> y) {
-        return skvx::to_vec<8,uint16_t>( vmull_u8(skvx::to_vext(skvx::join(x,x)),
-                                                  skvx::to_vext(skvx::join(y,y))) )
-            .lo;
-    }
-    static inline skvx::Vec<16,uint16_t> mull(skvx::Vec<16,uint8_t> x, skvx::Vec<16,uint8_t> y) {
-        uint16x8_t lo = vmull_u8( skvx::to_vext(x.lo), skvx::to_vext(y.lo) ),
-                   hi = vmull_u8( skvx::to_vext(x.hi), skvx::to_vext(y.hi) );
-        // TODO: why can't I get skvx::join() to generate the same code as this?
-        skvx::Vec<16,uint16_t> r;
-        memcpy(&r.lo, &lo, sizeof(lo));
-        memcpy(&r.hi, &hi, sizeof(hi));
-        return r;
-    }
-#else
-    // Nothing special when we don't have NEON... just cast up to 16-bit and multiply.
-    template <int N>
-    static inline skvx::Vec<N,uint16_t> mull(skvx::Vec<N,uint8_t> x, skvx::Vec<N,uint8_t> y) {
-        return skvx::cast<uint16_t>(x)
-             * skvx::cast<uint16_t>(y);
-    }
-#endif
-
 // Blend constant color over count src pixels, writing into dst.
 inline void blit_row_color32(SkPMColor* dst, const SkPMColor* src, int count, SkPMColor color) {
     constexpr int N = 4;  // 8, 16 also reasonable choices
