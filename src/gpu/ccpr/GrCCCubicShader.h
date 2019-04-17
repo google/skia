afd491a6c01c56b58166229331917bf16e338a1c
Chris Dalton <csmartdalton@google.com>
2019-04-17 14:47:17 -0600

    ccpr: Calculate edge coverage in the coverage processor
    Does the opposite-edge coverage calculation for curves in the coverage
    processor, rather than the Shader class. This allows the vertex shader
    implementation to do it more clean and directly, as well as allowing
    the future sample mask implementation to disregard edge coverage
    entirely.
    
    Bug: skia:
    Change-Id: Idaddfff8b22e3223f2a4e286900941969f975c6a

--- src/gpu/ccpr/GrCCCubicShader.h
+++ src/gpu/ccpr/GrCCCubicShader.h
@@ -23,12 +23,12 @@
  */
 class GrCCCubicShader : public GrCCCoverageProcessor::Shader {
 public:
-    void emitSetupCode(GrGLSLVertexGeoBuilder*, const char* pts, const char* wind,
-                       const char** outHull4) const override;
+    void emitSetupCode(
+            GrGLSLVertexGeoBuilder*, const char* pts, const char** outHull4) const override;
 
-    void onEmitVaryings(GrGLSLVaryingHandler*, GrGLSLVarying::Scope, SkString* code,
-                        const char* position, const char* coverage,
-                        const char* cornerCoverage) override;
+    void onEmitVaryings(
+            GrGLSLVaryingHandler*, GrGLSLVarying::Scope, SkString* code, const char* position,
+            const char* coverage, const char* cornerCoverage, const char* wind) override;
 
     void onEmitFragmentCode(GrGLSLFPFragmentBuilder*, const char* outputCoverage) const override;
 
@@ -37,7 +37,6 @@ private:
                           const char* outputCoverage) const;
 
     const GrShaderVar fKLMMatrix{"klm_matrix", kFloat3x3_GrSLType};
-    const GrShaderVar fEdgeDistanceEquation{"edge_distance_equation", kFloat3_GrSLType};
     GrGLSLVarying fKLM_fEdge;
     GrGLSLVarying fGradMatrix;
     GrGLSLVarying fCornerCoverage;
