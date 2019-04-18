2f103658b03e989876b9ad6ab99061465ade5dd2
Jim Van Verth <jvanverth@google.com>
2019-04-18 13:11:01 -0400

    Add Metal support for dynamic buffers
    Bug: skia:8243
    Change-Id: I3600463860fae235c46414a465ea106faa97ea0b

--- src/gpu/mtl/GrMtlBuffer.h
+++ src/gpu/mtl/GrMtlBuffer.h
@@ -9,6 +9,7 @@
 #define GrMtlBuffer_DEFINED
 
 #include "GrGpuBuffer.h"
+#include "GrMtlUniformHandler.h"
 
 #import <metal/metal.h>
 
@@ -23,6 +24,7 @@ public:
     ~GrMtlBuffer() override;
 
     id<MTLBuffer> mtlBuffer() const { return fMtlBuffer; }
+    size_t offset() const { return fOffset; }
 
 protected:
     GrMtlBuffer(GrMtlGpu*, size_t size, GrGpuBufferType intendedType, GrAccessPattern);
@@ -46,9 +48,33 @@ private:
 
     bool fIsDynamic;
     id<MTLBuffer> fMtlBuffer;
-    id<MTLBuffer> fMappedBuffer;
+    size_t        fOffset;       // offset into shared buffer for dynamic buffers
+    id<MTLBuffer> fMappedBuffer; // buffer used by static buffers for uploads
 
     typedef GrGpuBuffer INHERITED;
+};
+
+class GrMtlBufferManager {
+public:
+    GrMtlBufferManager(GrMtlGpu* gpu)
+         : fGpu(gpu), fBufferAllocation(nil), fAllocationSize(0), fNextOffset(0) {}
+
+    ~GrMtlBufferManager() {
+        fBufferAllocation = nil; // Just to be sure
+    }
+
+    id<MTLBuffer> getDynamicAllocation(size_t size, size_t* offset);
+    void setVertexBuffer(id<MTLRenderCommandEncoder>, const GrMtlBuffer*, size_t index);
+    void setFragmentBuffer(id<MTLRenderCommandEncoder>, const GrMtlBuffer*, size_t index);
+    void resetBindings();
+
+private:
+    GrMtlGpu*     fGpu;
+    id<MTLBuffer> fBufferAllocation;
+    size_t        fAllocationSize;
+    size_t        fNextOffset;
+    static constexpr size_t kNumBindings = GrMtlUniformHandler::kLastUniformBinding + 3;
+    id<MTLBuffer> fBufferBindings[kNumBindings];
 };
 
 #endif
