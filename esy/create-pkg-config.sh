49375a5d358805cd29248b48432c3807995dac65
Manuel Hornung <manuel@hornung.in>
2019-04-19 19:21:15 +0200

    Fix pkg-config flag for c++ std

--- esy/create-pkg-config.sh
+++ esy/create-pkg-config.sh
@@ -3,7 +3,7 @@ if which x86_64-w64-mingw32-gcc; then
     platformSpecificFlags=
 else
     if [ "$(uname)" == "Darwin" ]; then
-        platformSpecificFlags="-framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation -std=c++1y -lstdc++"
+        platformSpecificFlags="-framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation"
     else
         # TODO find out what is needed here
         platformSpecificFlags=
@@ -18,6 +18,6 @@ libdir=$cur__lib
 Name: skia
 Description: 2D graphics library
 Version: $cur__version
-Cflags: -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils
-Libs: -L\${libdir} $platformSpecificFlags -lskia
+Cflags: -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils -std=c++1y
+Libs: -L\${libdir} $platformSpecificFlags -lskia -lstdc++
 EOF
