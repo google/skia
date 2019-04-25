e112fa23556c2e790be2cfc691b04f09d6f8b6ad
Brian Osman <brianosman@google.com>
2019-04-25 11:38:07 -0400

    Blacklist GL processor tests on GalaxyS6, too
    It's failing flakily on these devices. Also, these tests are really
    slow, and those devices are on the CQ.
    
    Bug: skia:9019
    Change-Id: I1b2ebd3d80dcec6ee2519d1520e6857917b788c1

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -213,7 +213,7 @@ def dm_flags(api, bot):
         # skbug.com/7376
         blacklist('_ test _ ProcessorCloneTest')
 
-    if 'AndroidOne' in bot or ('Nexus' in bot and 'Nexus5x' not in bot):
+    if 'AndroidOne' in bot or ('Nexus' in bot and 'Nexus5x' not in bot) or 'GalaxyS6' in bot:
       # skbug.com/9019
       blacklist('_ test _ ProcessorCloneTest')
       blacklist('_ test _ GLPrograms')
