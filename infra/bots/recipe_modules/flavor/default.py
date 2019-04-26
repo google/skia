23935888a1028ec20b9d9cff344f13656809dba7
Ben Wagner <benjaminwagner@google.com>
2019-04-26 17:06:04 -0400

    Check event log for all Windows tasks
    Change-Id: Ie72515f5b9c64bb156fee81a0f28d6335b528978
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/210240
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- infra/bots/recipe_modules/flavor/default.py
+++ infra/bots/recipe_modules/flavor/default.py
@@ -273,7 +273,12 @@ class DefaultFlavor(object):
                  self.module.resource('symbolize_stack_trace.py'),
                  args=args,
                  infra_step=False)
-
+    elif 'Win' in self.m.vars.builder_cfg.get('os', ''):
+      with self.m.context(env=env):
+        wrapped_cmd = ['powershell', '-ExecutionPolicy', 'Unrestricted',
+                       '-File',
+                       self.module.resource('win_run_and_check_log.ps1')] + cmd
+        self._run(name, wrapped_cmd)
     else:
       with self.m.context(env=env):
         self._run(name, cmd)
