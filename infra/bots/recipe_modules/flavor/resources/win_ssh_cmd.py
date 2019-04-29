f3f83a8a46195aa0d778f8938c00f87ab20877cb
Ben Wagner <benjaminwagner@google.com>
2019-04-29 15:37:06 -0400

    Don't use tty in win_ssh_cmd.py
    Fixes https://skia-review.googlesource.com/c/skia/+/204264. I should
    have realized before that the '-t -t' option to ssh was the cause of all
    the ANSI escape codes.
    
    Change-Id: I43b745115f4672230bf28469e03fdfbaec602b9a

--- infra/bots/recipe_modules/flavor/resources/win_ssh_cmd.py
+++ infra/bots/recipe_modules/flavor/resources/win_ssh_cmd.py
@@ -11,39 +11,28 @@ import sys
 # Usage: win_ssh_cmd.py <user@host> <cmd shell string> [<fail errorlevel>]
 # Runs the given command over ssh and exits with 0 if the command succeeds or 1
 # if the command fails. The command is considered to fail if the errorlevel is
-# greater than or equal to <fail errorlevel>. Ignores all ANSI escape codes.
+# greater than or equal to <fail errorlevel>.
 
 
 SENTINEL = 'win_ssh_cmd remote command successful'
 
-# Based on https://stackoverflow.com/a/14693789; edited to incorporate OSC
-# based on https://en.wikipedia.org/wiki/ANSI_escape_code#Escape_sequences
-ANSI_ESCAPE_RE = re.compile(r'\x1B(\[[0-?]*[ -/]*[@-~]|\].*(\x1B\\|\x07))')
-
 def main(user_host, cmd, fail_errorlevel):
-  ssh_cmd = ['ssh', '-oConnectTimeout=15', '-oBatchMode=yes', '-t', '-t',
-             user_host,
-             '(' + cmd + ') & if not errorlevel %s echo "%s"' % (
+  ssh_cmd = ['ssh', '-oConnectTimeout=15', '-oBatchMode=yes', user_host,
+             '(' + cmd + ') & if not errorlevel %s echo %s' % (
                  fail_errorlevel, SENTINEL)]
-  # orig_lines stores the full output of ssh_cmd.
-  orig_lines = []
   # True if we saw a line matching SENTINEL.
   saw_sentinel = False
   print >> sys.stderr, 'Original command:\n%s\nFull command:\n%s' % (
       cmd, ' '.join([repr(s) for s in ssh_cmd]))
   proc = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE)
   for line in iter(proc.stdout.readline, ''):
-    orig_lines.append(line)
-    line = ANSI_ESCAPE_RE.sub('', line)
     stripped = line.strip()
     if stripped == SENTINEL:
       saw_sentinel = True
-    elif stripped != '':
+    else:
       print stripped
   proc.wait()
   sys.stdout.flush()
-  print >> sys.stderr, 'Raw output base64-encoded:\n%s' % (
-      base64.b64encode(''.join(orig_lines)))
   if proc.returncode != 0:
     sys.exit(proc.returncode)
   if not saw_sentinel:
