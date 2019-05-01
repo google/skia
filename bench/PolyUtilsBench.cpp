3b5961ce1c7004d7048276c408b5fce1ac07917d
Jim Van Verth <jvanverth@google.com>
2019-05-01 13:28:07 -0400

    More polyutils fixes
    Bug: chromium:954891, oss-fuzz:14079
    Change-Id: Ia1ee0a15ab4bfc93e67188ff02e6aa436432a3be

--- bench/PolyUtilsBench.cpp
+++ bench/PolyUtilsBench.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "bench/Benchmark.h"
+#include "include/core/SkRect.h"
 #include "src/utils/SkPolyUtils.h"
 
 class PolyUtilsBench : public Benchmark {
@@ -70,9 +71,13 @@ protected:
             case Type::kOffsetSimple:
                 if (SkIsSimplePolygon(poly.begin(), poly.count())) {
                     SkTDArray<SkPoint> result;
+                    SkRect bounds;
+                    bounds.setBounds(poly.begin(), poly.count());
                     for (int i = 0; i < loops; i++) {
-                        (void)SkOffsetSimplePolygon(poly.begin(), poly.count(), 10, &result);
-                        (void)SkOffsetSimplePolygon(poly.begin(), poly.count(), -10, &result);
+                        (void)SkOffsetSimplePolygon(poly.begin(), poly.count(), bounds, 10,
+                                                    &result);
+                        (void)SkOffsetSimplePolygon(poly.begin(), poly.count(), bounds, -10,
+                                                    &result);
                     }
                 }
                 break;
