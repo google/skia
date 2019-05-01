de8f401b4c39fb0d9776928f9ca15435bc7dae91
Ethan Nicholas <ethannicholas@google.com>
2019-05-01 14:57:50 -0400

    child processors are now separate functions
    Bug: skia:
    Change-Id: I3c6c876fea9cfcc311fc09c0fdf0375b776004aa

--- src/gpu/gradients/generated/GrTiledGradientEffect.cpp
+++ src/gpu/gradients/generated/GrTiledGradientEffect.cpp
@@ -29,25 +29,24 @@ public:
         (void)makePremul;
         auto colorsAreOpaque = _outer.colorsAreOpaque;
         (void)colorsAreOpaque;
-        SkString _child1("_child1");
-        this->emitChild(_outer.gradLayout_index, &_child1, args);
+        fragBuilder->codeAppendf("half4 _process447;");
+        this->invokeChild(_outer.gradLayout_index, "_process447", args);
         fragBuilder->codeAppendf(
-                "half4 t = %s;\nif (!%s && t.y < 0.0) {\n    %s = half4(0.0);\n} else {\n    @if "
-                "(%s) {\n        half t_1 = t.x - 1.0;\n        half tiled_t = (t_1 - 2.0 * "
+                "half4 t = _process447;\nif (!%s && t.y < 0.0) {\n    %s = half4(0.0);\n} else {\n "
+                "   @if (%s) {\n        half t_1 = t.x - 1.0;\n        half tiled_t = (t_1 - 2.0 * "
                 "floor(t_1 * 0.5)) - 1.0;\n        if (sk_Caps.mustDoOpBetweenFloorAndAbs) {\n     "
                 "       tiled_t = clamp(tiled_t, -1.0, 1.0);\n        }\n        t.x = "
                 "abs(tiled_t);\n    } else {\n        t.x = fract(t.x);\n    }",
-                _child1.c_str(),
                 (_outer.childProcessor(_outer.gradLayout_index).preservesOpaqueInput() ? "true"
                                                                                        : "false"),
                 args.fOutputColor, (_outer.mirror ? "true" : "false"));
         SkString _input0("t");
-        SkString _child0("_child0");
-        this->emitChild(_outer.colorizer_index, _input0.c_str(), &_child0, args);
-        fragBuilder->codeAppendf("\n    %s = %s;\n}\n@if (%s) {\n    %s.xyz *= %s.w;\n}\n",
-                                 args.fOutputColor, _child0.c_str(),
-                                 (_outer.makePremul ? "true" : "false"), args.fOutputColor,
-                                 args.fOutputColor);
+        fragBuilder->codeAppendf("half4 _process1459;");
+        this->invokeChild(_outer.colorizer_index, _input0.c_str(), "_process1459", args);
+        fragBuilder->codeAppendf(
+                "\n    %s = _process1459;\n}\n@if (%s) {\n    %s.xyz *= %s.w;\n}\n",
+                args.fOutputColor, (_outer.makePremul ? "true" : "false"), args.fOutputColor,
+                args.fOutputColor);
     }
 
 private:
