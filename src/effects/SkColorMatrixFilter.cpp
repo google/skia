68eb8c276355687ccd3f56f2279232f7fe1a9ebc
Mike Reed <reed@google.com>
2019-05-02 11:31:43 -0400

    remove deprecated postTranslate255
    Change-Id: Id2bdf73b5111c34f0ed1f36da85548a942568dcb
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/211595
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Auto-Submit: Mike Reed <reed@google.com>

--- src/effects/SkColorMatrixFilter.cpp
+++ src/effects/SkColorMatrixFilter.cpp
@@ -7,7 +7,7 @@
 
 #include "include/effects/SkColorMatrixFilter.h"
 
-static SkScalar byte_to_scale(U8CPU byte) {
+static SkScalar byte_to_unit_float(U8CPU byte) {
     if (0xFF == byte) {
         // want to get this exact
         return 1;
@@ -24,13 +24,13 @@ sk_sp<SkColorFilter> SkColorMatrixFilter::MakeLightingFilter(SkColor mul, SkColo
     }
 
     SkColorMatrix matrix;
-    matrix.setScale(byte_to_scale(SkColorGetR(mul)),
-                    byte_to_scale(SkColorGetG(mul)),
-                    byte_to_scale(SkColorGetB(mul)),
+    matrix.setScale(byte_to_unit_float(SkColorGetR(mul)),
+                    byte_to_unit_float(SkColorGetG(mul)),
+                    byte_to_unit_float(SkColorGetB(mul)),
                     1);
-    matrix.postTranslate255(SkIntToScalar(SkColorGetR(add)),
-                         SkIntToScalar(SkColorGetG(add)),
-                         SkIntToScalar(SkColorGetB(add)),
+    matrix.postTranslate(byte_to_unit_float(SkColorGetR(add)),
+                         byte_to_unit_float(SkColorGetG(add)),
+                         byte_to_unit_float(SkColorGetB(add)),
                          0);
     return SkColorFilters::Matrix(matrix);
 }
