b82ac3660c20fbad3391ccef0e17a8e242702450
Hal Canary <halcanary@google.com>
2019-05-02 10:47:46 -0400

    SkPDF: fix repeat tilemode with alpha
    Also fix the corners of clamp mode with alpha.
    
    Also, add a GM.
    
    Bug:chromium:957275
    
    Change-Id: Icd288ff522e7ea70662380791f5ee2de628a5ef2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/211594
    Auto-Submit: Hal Canary <halcanary@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

---
+++ gm/tilemodes_alpha.cpp
@@ -0,0 +1,27 @@
+// Copyright 2019 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#include "gm/gm.h"
+#include "tools/Resources.h"
+// http://crbug.com/957275
+DEF_SIMPLE_GM(tilemodes_alpha, canvas, 512, 512) {
+    sk_sp<SkImage> image = GetResourceAsImage("images/mandrill_64.png");
+    if (!image) {
+        return;
+    }
+    constexpr SkTileMode kModes[4] = {
+        SkTileMode::kClamp,
+        SkTileMode::kRepeat,
+        SkTileMode::kMirror,
+        SkTileMode::kDecal,
+    };
+    for (int y = 0; y < 4; ++y) {
+        for (int x = 0; x < 4; ++x) {
+            SkRect rect = SkRect::MakeXYWH(128 * x + 1, 128 * y + 1, 126, 126);
+            SkMatrix matrix = SkMatrix::MakeTrans(rect.x(), rect.y());
+            SkPaint paint;
+            paint.setAlphaf(0.5f);
+            paint.setShader(image->makeShader(kModes[x], kModes[y], &matrix));
+            canvas->drawRect(rect, paint);
+        }
+    }
+}
