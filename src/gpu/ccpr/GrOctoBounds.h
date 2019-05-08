15c47a6a0676b5578da83aa083ea8cb2659937ff
Chris Dalton <csmartdalton@google.com>
2019-05-07 18:54:02 -0600

    ccpr: Add a GrOctoBounds class
    Encapsulates all the various code dealing with 45-degree bounding
    boxes into a central GrOctoBounds class.
    
    Bug: skia:
    Change-Id: Ie9d8b2ff325f2e7604c2cc4d3d4d2426af3905a9

---
+++ src/gpu/ccpr/GrOctoBounds.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrOctoBounds_DEFINED
+#define GrOctoBounds_DEFINED
+
+#include "include/core/SkRect.h"
+#include <functional>
+
+/**
+ * This class is composed of two bounding boxes: one in device space, and one in a 45-degree rotated
+ * space.
+ *
+ * The 45-degree bounding box resides in "| 1  -1 | * coords" space.
+ *                                        | 1   1 |
+ *
+ * The intersection of these two boxes defines the bounding octagon of a shape.
+ *
+ * Furthermore, both bounding boxes are fully tightened. This means we can blindly find the
+ * intersections between each diagonal and its vertical and horizontal neighbors, and be left with
+ * 8 points that define a convex (possibly degenerate) octagon.
+ */
+class GrOctoBounds {
+public:
+    const SkRect& bounds() const { return fBounds; }
+    float left() const { return fBounds.left(); }
+    float top() const { return fBounds.top(); }
+    float right() const { return fBounds.right(); }
+    float bottom() const { return fBounds.bottom(); }
+
+
+    // The 45-degree bounding box resides in "| 1  -1 | * coords" space.
+    //                                        | 1   1 |
+    const SkRect& bounds45() const { return fBounds45; }
+    float left45() const { return fBounds45.left(); }
+    float top45() const { return fBounds45.top(); }
+    float right45() const { return fBounds45.right(); }
+    float bottom45() const { return fBounds45.bottom(); }
+
+    void roundOut(SkIRect* out) const { fBounds.roundOut(out); }
+
+    void setLTRB(float l, float t, float r, float b, float l45, float t45, float r45, float b45) {
+        fBounds.setLTRB(l, t, r, b);
+        fBounds45.setLTRB(l45, t45, r45, b45);
+        SkDEBUGCODE(this->assertBoundsAreTight());
+    }
+
+    GrOctoBounds makeOffset(float dx, float dy) const {
+        GrOctoBounds offset;
+        offset.setOffset(*this, dx, dy);
+        return offset;
+    }
+
+    void setOffset(const GrOctoBounds& octoBounds, float dx, float dy) {
+        fBounds = octoBounds.fBounds.makeOffset(dx, dy);
+        fBounds45 = octoBounds.fBounds45.makeOffset(dx - dy, dx + dy);
+        SkDEBUGCODE(this->assertBoundsAreTight());
+    }
+
+    void outset(float radius) {
+        fBounds.outset(radius, radius);
+        fBounds45.outset(radius*SK_ScalarSqrt2, radius*SK_ScalarSqrt2);
+        SkDEBUGCODE(this->assertBoundsAreTight());
+    }
+
+    // The 45-degree bounding box resides in "| 1  -1 | * coords" space.
+    //                                        | 1   1 |
+    //
+    //  i.e., | x45 | = | x - y |
+    //        | y45 | = | x + y |
+    //
+    // These methods transform points between device space and 45-degree space.
+    constexpr static float Get_x45(float x, float y) { return x - y; }
+    constexpr static float Get_y45(float x, float y) { return x + y; }
+    constexpr static float Get_x(float x45, float y45) { return (x45 + y45) * .5f; }
+    constexpr static float Get_y(float x45, float y45) { return (y45 - x45) * .5f; }
+
+#ifdef SK_DEBUG
+    void assertBoundsAreTight() {
+        validateBoundsAreTight([](bool test) { SkASSERT(test); });
+    }
+    void validateBoundsAreTight(const std::function<void(bool)>& validateFn);
+#endif
+
+private:
+    SkRect fBounds;
+    SkRect fBounds45;
+};
+
+#endif
