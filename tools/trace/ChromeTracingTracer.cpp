000a6fa42ec301a5967f2229b81c1a49e64f1f20
Herb Derby <herb@google.com>
2019-05-09 16:59:18 -0400

    Add thread safety annotations.
    Start out with spinlock. I tried to be more extensive, but some
    of our abstractions confused the analysis. Will expand further
    in following CLs.
    
    Change-Id: I3e320c957d8ef427065a2b7e7d2187b7c6b0aef1

--- tools/trace/ChromeTracingTracer.cpp
+++ tools/trace/ChromeTracingTracer.cpp
@@ -65,7 +65,7 @@ void ChromeTracingTracer::createBlock() {
 SkEventTracer::Handle ChromeTracingTracer::appendEvent(const void* data, size_t size) {
     SkASSERT(size > 0 && size <= kBlockSize);
 
-    SkAutoMutexAcquire lock(fMutex);
+    SkAutoSpinlock lock(fMutex);
     if (fCurBlockUsed + size > kBlockSize) {
         fBlocks.push_back(std::move(fCurBlock));
         this->createBlock();
@@ -143,8 +143,8 @@ void ChromeTracingTracer::updateTraceEventDuration(const uint8_t*        categor
                                                    const char*           name,
                                                    SkEventTracer::Handle handle) {
     // We could probably get away with not locking here, but let's be totally safe.
-    SkAutoMutexAcquire lock(fMutex);
-    TraceEvent*        traceEvent = reinterpret_cast<TraceEvent*>(handle);
+    SkAutoSpinlock lock(fMutex);
+    TraceEvent*    traceEvent = reinterpret_cast<TraceEvent*>(handle);
     traceEvent->fClockEnd         = std::chrono::steady_clock::now().time_since_epoch().count();
 }
 
@@ -270,7 +270,7 @@ static void trace_event_to_json(SkJSONWriter*                 writer,
 }
 
 void ChromeTracingTracer::flush() {
-    SkAutoMutexAcquire lock(fMutex);
+    SkAutoSpinlock lock(fMutex);
 
     SkString dirname = SkOSPath::Dirname(fFilename.c_str());
     if (!dirname.isEmpty() && !sk_exists(dirname.c_str(), kWrite_SkFILE_Flag)) {
