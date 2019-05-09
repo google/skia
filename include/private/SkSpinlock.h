164d64139f8ad307ede93f34adefdfae89036c4e
Herb Derby <herb@google.com>
2019-05-09 16:59:18 -0400

    Add thread safety annotations.
    Start out with spinlock. I tried to be more extensive, but some
    of our abstractions confused the analysis. Will expand further
    in following CLs.
    
    Change-Id: I3e320c957d8ef427065a2b7e7d2187b7c6b0aef1

--- include/private/SkSpinlock.h
+++ include/private/SkSpinlock.h
@@ -9,13 +9,14 @@
 #define SkSpinlock_DEFINED
 
 #include "include/core/SkTypes.h"
+#include "include/private/SkThreadAnnotations.h"
 #include <atomic>
 
-class SkSpinlock {
+class SK_CAPABILITY("mutex") SkSpinlock {
 public:
     constexpr SkSpinlock() = default;
 
-    void acquire() {
+    void acquire() SK_ACQUIRE() {
         // To act as a mutex, we need an acquire barrier when we acquire the lock.
         if (fLocked.exchange(true, std::memory_order_acquire)) {
             // Lock was contended.  Fall back to an out-of-line spin loop.
@@ -24,7 +25,7 @@ public:
     }
 
     // Acquire the lock or fail (quickly). Lets the caller decide to do something other than wait.
-    bool tryAcquire() {
+    bool tryAcquire() SK_TRY_ACQUIRE(true) {
         // To act as a mutex, we need an acquire barrier when we acquire the lock.
         if (fLocked.exchange(true, std::memory_order_acquire)) {
             // Lock was contended. Let the caller decide what to do.
@@ -33,7 +34,7 @@ public:
         return true;
     }
 
-    void release() {
+    void release() SK_RELEASE_CAPABILITY() {
         // To act as a mutex, we need a release barrier when we release the lock.
         fLocked.store(false, std::memory_order_release);
     }
@@ -42,6 +43,15 @@ private:
     SK_API void contendedAcquire();
 
     std::atomic<bool> fLocked{false};
+};
+
+class SK_SCOPED_CAPABILITY SkAutoSpinlock {
+public:
+    SkAutoSpinlock(SkSpinlock& mutex) SK_ACQUIRE(mutex) : fSpinlock(mutex) { fSpinlock.acquire(); }
+    ~SkAutoSpinlock() SK_RELEASE_CAPABILITY() { fSpinlock.release(); }
+
+private:
+    SkSpinlock& fSpinlock;
 };
 
 #endif//SkSpinlock_DEFINED
