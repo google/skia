fc7b3c9b14d5873db394715b567845b2ef28ff3f
Brian Salomon <bsalomon@google.com>
2019-05-09 19:42:56 +0000

    Revert "Reland "Release YUVA planes in SkImage_GpuYUVA after flattenning to RGBA.""
    This reverts commit 622b6f5367e2d26dab276df8bd9df0451236808b.
    
    Reason for revert: ANGLE may not be fixed
    
    Original change's description:
    > Reland "Release YUVA planes in SkImage_GpuYUVA after flattenning to RGBA."
    > 
    > This reverts commit bc0c460349142140295593a3a32d507f7b45c622.
    > 
    > Reason for revert: fixed isTextureBacked(), checked cc_unittests passes
    > 
    > Original change's description:
    > > Revert "Release YUVA planes in SkImage_GpuYUVA after flattenning to RGBA."
    > >
    > > This reverts commit a15f355be83136e7827ccfbc9bb9e9ea77f48518.
    > >
    > > Reason for revert: ANGLE failing new test
    > >
    > > Original change's description:
    > > > Release YUVA planes in SkImage_GpuYUVA after flattenning to RGBA.
    > > >
    > > > Also removed unused virtual function from SkImage_GpuBase and override
    > > > on SkImage_GpuYUVA.
    > > >
    > > > Change-Id: Ib47b46b529b16976181cb9453976133d66e0f952
    > > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/212734
    > > > Commit-Queue: Brian Salomon <bsalomon@google.com>
    > > > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > >
    > > TBR=jvanverth@google.com,bsalomon@google.com,robertphillips@google.com
    > >
    > > Change-Id: Ib2566ff8aa99d3698d6044167e70a1d75542f2e9
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/212986
    > > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > > Commit-Queue: Robert Phillips <robertphillips@google.com>
    > 
    > TBR=jvanverth@google.com,bsalomon@google.com,robertphillips@google.com
    > 
    > Change-Id: I3c9454da33a764504e768d02b59a5cd99766aff1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/212988
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    
    TBR=jvanverth@google.com,bsalomon@google.com,robertphillips@google.com
    
    Change-Id: I06632adc69844522c87cd72e8a6b0c2795974fe4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- src/gpu/GrImageTextureMaker.cpp
+++ src/gpu/GrImageTextureMaker.cpp
@@ -91,9 +91,8 @@ std::unique_ptr<GrFragmentProcessor> GrYUVAImageTextureMaker::createFragmentProc
     bool coordsLimitedToConstraintRect,
     const GrSamplerState::Filter* filterOrNullForBicubic) {
 
-    // Check simple cases to see if we need to fall back to flattening the image (or whether it's
-    // already been flattened.)
-    if (!filterOrNullForBicubic || this->domainNeedsDecal() || fImage->fRGBProxy) {
+    // Check simple cases to see if we need to fall back to flattening the image
+    if (!filterOrNullForBicubic || this->domainNeedsDecal()) {
         return this->INHERITED::createFragmentProcessor(textureMatrix, constraintRect,
                                                         filterConstraint,
                                                         coordsLimitedToConstraintRect,
