97ec44d22c6d9a61eed97dd1897361bae6c4f899
Robert Phillips <robertphillips@google.com>
2019-05-09 15:51:12 -0400

    Direct backend object allocation API
    Change-Id: Ie8733a4db8061c67678a41cf239ce4407e967df7

---
+++ src/gpu/GrBackendObject.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/gpu/GrBackendObject.h"
+#include "include/gpu/GrContext.h"
+#include "src/gpu/GrContextPriv.h"
+#include "src/gpu/GrGpu.h"
+
+GrBackendObject::GrBackendObject(GrContext* context, GrBackendTexture backendTex)
+        : fContext(context)
+        , fBackendTex(backendTex) {
+}
+
+GrBackendObject::~GrBackendObject() {
+    if (fContext->abandoned() || !fBackendTex.isValid()) {
+        return;
+    }
+
+    GrGpu* gpu = fContext->priv().getGpu();
+
+    gpu->deleteTestingOnlyBackendTexture(fBackendTex);
+}
+
+sk_sp<GrBackendObject> GrBackendObject::Make(GrContext* context,
+                                             int width, int height,
+                                             SkColorType ct,
+                                             GrMipMapped mipMapped, GrRenderable renderable) {
+    if (context->priv().asDirectContext()) {
+        return nullptr;
+    }
+
+    if (context->abandoned()) {
+        return nullptr;
+    }
+
+    GrGpu* gpu = context->priv().getGpu();
+
+    GrBackendTexture backendTex = gpu->createTestingOnlyBackendTexture(nullptr, width, height, ct,
+                                                                       GrRenderable::kYes == renderable,
+                                                                       mipMapped, 0);
+    if (!backendTex.isValid()) {
+        return nullptr;
+    }
+
+    return sk_sp<GrBackendObject>(new GrBackendObject(context, backendTex));
+}
+
+sk_sp<GrBackendObject> GrBackendObject::Make(GrContext* context,
+                                             int width, int height,
+                                             GrBackendFormat backendFormat,
+                                             GrMipMapped mipMapped, GrRenderable renderable) {
+    if (context->priv().asDirectContext()) {
+        return nullptr;
+    }
+
+    if (context->abandoned()) {
+        return nullptr;
+    }
+
+    GrGpu* gpu = context->priv().getGpu();
+
+    if (!backendFormat.isValid()) {
+        return nullptr;
+    }
+
+#if 0
+    GrBackendTexture backendTex = gpu->createTestingOnlyBackendTexture(nullptr, width, height, ct,
+                                                                       GrRenderable::kYes == renderable,
+                                                                       mipMapped, 0);
+    if (!backendTex.isValid()) {
+        return nullptr;
+    }
+
+    return sk_sp<GrBackendObject>(new GrBackendObject(context, backendTex));
+#else
+    return nullptr;
+#endif
+}
+
