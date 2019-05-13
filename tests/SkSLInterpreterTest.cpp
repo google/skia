44d44761c7a435fbeb072762c237bff941ac4b6b
Brian Osman <brianosman@google.com>
2019-05-13 14:19:12 -0400

    Interpreter: Fix bool literals
    Change-Id: I0cf8e12d5ddb9b89aff39f484d8a017f61e4a20f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/213465
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- tests/SkSLInterpreterTest.cpp
+++ tests/SkSLInterpreterTest.cpp
@@ -204,6 +204,8 @@ DEF_TEST(SkSLInterpreterDo, r) {
             "(true); }", 0, 0, 0, 0, 1.5, 0, 0, 0);
     test(r, "void main(inout half4 color) {do { color.r += 0.5; if (color.r < 5) "
             "continue; if (color.r >= 5) break; } while (true); }", 0, 0, 0, 0, 5, 0, 0, 0);
+    test(r, "void main(inout half4 color) { do { color.r += 0.5; } while (false); }",
+         0, 0, 0, 0, 0.5, 0, 0, 0);
 }
 
 DEF_TEST(SkSLInterpreterFor, r) {
