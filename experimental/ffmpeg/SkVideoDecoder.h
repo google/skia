bcdfbf444ca5d5412f53ab8d81fc559b2fdcc52d
Mike Reed <reed@google.com>
2019-05-15 16:35:50 -0400

    WIP: hack gm to learn ffmpeg
    Not ready for minutia review, but wanted to get high-level feedback.
    
    This only builds/runs with a lot of custom setup,
    which I need to find a way to fix or make optional
    - custom extra_ldflags to find local prebuilt ffmpeg libraries
    - custom config of those libraries, to not bring in the world of external dependencies
    - ffmpeg sources/lib at all
    
    So, some questions regarding ffmpeg itself
    - should we DEPS pull it into our third_party?
    - how can we have it built with a custom config?
    
    Change-Id: If1892b62314af26e56ecb0293850f7554106c3d0

---
+++ experimental/ffmpeg/SkVideoDecoder.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkVideDecoder_DEFINED
+#define SkVideDecoder_DEFINED
+
+#include "include/core/SkImage.h"
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavformat/avio.h"
+#include "libavutil/pixdesc.h"
+}
+
+class SkVideoDecoder {
+public:
+    SkVideoDecoder(GrContext* gr = nullptr);
+    ~SkVideoDecoder();
+
+    void reset();
+    void setGrContext(GrContext* gr) { fGr = gr; }
+
+    bool loadStream(std::unique_ptr<SkStream>);
+    bool rewind();
+
+    SkISize dimensions() const;
+
+    // Returns each image in the video, or nullptr on eof
+    sk_sp<SkImage> nextImage();
+
+    sk_sp<SkImage> lastImage() const { return fLastImg; }
+
+private:
+    GrContext*          fGr = nullptr;  // not owned by us
+
+    std::unique_ptr<SkStream>   fStream;
+
+    AVIOContext*        fStreamCtx = nullptr;
+    AVFormatContext*    fFormatCtx = nullptr;
+    AVCodecContext*     fDecoderCtx = nullptr;
+    int                 fStreamIndex = -1;  // fFormatCtx->stream[...]
+
+    AVPacket            fPacket;
+    AVFrame*            fFrame = nullptr;
+    sk_sp<SkImage>      fLastImg;
+
+    enum Mode {
+        kProcessing_Mode,
+        kDraining_Mode,
+        kDone_Mode,
+    };
+    Mode    fMode = kDone_Mode;
+};
+
+#endif
+
