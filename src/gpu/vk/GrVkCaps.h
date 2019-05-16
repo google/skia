3d6d958330f764aa7ec0ec4881fd858242f507b4
Greg Daniel <egdaniel@google.com>
2019-05-16 15:42:41 -0400

    Add GrCaps call to get swizzle from BackendFormat and ColorType.
    This CL just adds the caps function. Follow on CL will use it thoughout
    the gpu backend to remove GrPixelConfig dependency for swizzle.
    
    Bug: skia:6718
    Change-Id: Id299c72a9dc9f33411d8fd9c806158da20372474

--- src/gpu/vk/GrVkCaps.h
+++ src/gpu/vk/GrVkCaps.h
@@ -164,6 +164,9 @@ public:
     GrBackendFormat getBackendFormatFromGrColorType(GrColorType ct,
                                                     GrSRGBEncoded srgbEncoded) const override;
 
+    GrSwizzle getTextureSwizzle(const GrBackendFormat&, GrColorType) const override;
+    GrSwizzle getOutputSwizzle(const GrBackendFormat&, GrColorType) const override;
+
 private:
     enum VkVendor {
         kAMD_VkVendor = 4098,
@@ -218,7 +221,7 @@ private:
 
         SkTDArray<int> fColorSampleCounts;
     };
-    static const size_t kNumVkFormats = 16;
+    static const size_t kNumVkFormats = 17;
     FormatInfo fFormatTable[kNumVkFormats];
 
     const FormatInfo& getFormatInfo(VkFormat) const;
