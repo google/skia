7bc531f91dffc079cf92ff394330af5495b53528
Robert Phillips <robertphillips@google.com>
2019-05-20 18:53:54 -0400

    Expand backend allocation API to allow an initialization color
    Change-Id: I8e676e80f85f14c81c3cec8d766d0fdc7e25f426

--- src/gpu/mock/GrMockGpu.h
+++ src/gpu/mock/GrMockGpu.h
@@ -121,11 +121,11 @@ private:
     GrStencilAttachment* createStencilAttachmentForRenderTarget(const GrRenderTarget*,
                                                                 int width,
                                                                 int height) override;
-    GrBackendTexture createTestingOnlyBackendTexture(int w, int h, const GrBackendFormat&,
-                                                     GrMipMapped, GrRenderable,
-                                                     const void* pixels = nullptr,
-                                                     size_t rowBytes = 0) override;
-    void deleteTestingOnlyBackendTexture(const GrBackendTexture&) override;
+    GrBackendTexture createBackendTexture(int w, int h, const GrBackendFormat&,
+                                          GrMipMapped, GrRenderable,
+                                          const void* pixels, size_t rowBytes,
+                                          const SkColor4f& color = SkColors::kTransparent) override;
+    void deleteBackendTexture(const GrBackendTexture&) override;
 
 #if GR_TEST_UTILS
     bool isTestingOnlyBackendTexture(const GrBackendTexture&) const override;
