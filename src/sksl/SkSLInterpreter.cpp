12710911dcc2056d8b6697cd0aa540f01a1c47df
Mike Klein <mtklein@google.com>
2019-05-21 11:04:59 -0500

    negateS -> negateI
    The same negate should work fine for unsigned, right?
    This seems to eliminate the need for kInvaid.
    
        ~ $ cat test.c
        int main(void) {
            unsigned u = 0;
            do {
                unsigned nu = -u,
                         ns = -(int)u;
                if (nu != ns) {
                    return 1;
                }
            } while (++u != 0);
    
            return 0;
        }
        ~ $ clang -Os test.c
        ~ $ ./a.out && echo ok
        ok
    
    Change-Id: If4eec8aa24dce634e3196c8631adec4362a3b69b
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/215045
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/sksl/SkSLInterpreter.cpp
+++ src/sksl/SkSLInterpreter.cpp
@@ -163,7 +163,7 @@ void Interpreter::disassemble(const ByteCodeFunction& f) {
             VECTOR_DISASSEMBLE(kMultiplyF, "multiplyf")
             VECTOR_DISASSEMBLE(kMultiplyI, "multiplyi")
             VECTOR_DISASSEMBLE(kNegateF, "negatef")
-            VECTOR_DISASSEMBLE(kNegateS, "negates")
+            VECTOR_DISASSEMBLE(kNegateI, "negatei")
             VECTOR_DISASSEMBLE(kNot, "not")
             VECTOR_DISASSEMBLE(kOrB, "orb")
             VECTOR_DISASSEMBLE(kOrI, "ori")
@@ -431,10 +431,10 @@ void Interpreter::innerRun(const ByteCodeFunction& f, Value* stack, Value* outRe
             case ByteCodeInstruction::kNegateF : sp[ 0].fFloat = -sp[ 0].fFloat;
                                                  break;
 
-            case ByteCodeInstruction::kNegateS4: sp[-3].fSigned = -sp[-3].fSigned;
-            case ByteCodeInstruction::kNegateS3: sp[-2].fSigned = -sp[-2].fSigned;
-            case ByteCodeInstruction::kNegateS2: sp[-1].fSigned = -sp[-1].fSigned;
-            case ByteCodeInstruction::kNegateS : sp[ 0].fSigned = -sp [0].fSigned;
+            case ByteCodeInstruction::kNegateI4: sp[-3].fSigned = -sp[-3].fSigned;
+            case ByteCodeInstruction::kNegateI3: sp[-2].fSigned = -sp[-2].fSigned;
+            case ByteCodeInstruction::kNegateI2: sp[-1].fSigned = -sp[-1].fSigned;
+            case ByteCodeInstruction::kNegateI : sp[ 0].fSigned = -sp [0].fSigned;
                                                  break;
 
             case ByteCodeInstruction::kPop4: POP();
