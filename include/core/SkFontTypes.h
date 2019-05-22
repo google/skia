2c3f5619e8e869a1b93771902bc52ab975ed172a
Ben Wagner <bungeman@google.com>
2019-05-22 15:12:45 -0400

    Remove SkTextEncoding and SkFontHinting macros.
    It appears all known users have been mirgrated to the new enums.
    
    Change-Id: Id64083c18bf8aa5f2ec935c80af3babebad90a2d

--- include/core/SkFontTypes.h
+++ include/core/SkFontTypes.h
@@ -15,21 +15,11 @@ enum class SkTextEncoding {
     kGlyphID,   //!< uses two byte words to represent glyph indices
 };
 
-#define kUTF8_SkTextEncoding    SkTextEncoding::kUTF8
-#define kUTF16_SkTextEncoding   SkTextEncoding::kUTF16
-#define kUTF32_SkTextEncoding   SkTextEncoding::kUTF32
-#define kGlyphID_SkTextEncoding SkTextEncoding::kGlyphID
-
 enum class SkFontHinting {
     kNone,      //!< glyph outlines unchanged
     kSlight,    //!< minimal modification to improve constrast
     kNormal,    //!< glyph outlines modified to improve constrast
     kFull,      //!< modifies glyph outlines for maximum constrast
 };
-
-#define kNo_SkFontHinting       SkFontHinting::kNone
-#define kSlight_SkFontHinting   SkFontHinting::kSlight
-#define kNormal_SkFontHinting   SkFontHinting::kNormal
-#define kFull_SkFontHinting     SkFontHinting::kFull
 
 #endif
