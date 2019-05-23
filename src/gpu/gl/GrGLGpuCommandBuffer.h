a69e498e80ca7635c4ec69cbc7ec3f2f361d25fa
Chris Dalton <csmartdalton@google.com>
2019-05-22 18:14:47 -0600

    Reland "Clear and discard stencil buffers on tilers"
    This is a reland of 7b2c85577d79bf1bda6b57d9af8ad793e2fc4862
    
    Original change's description:
    > Clear and discard stencil buffers on tilers
    > 
    > Bug: skia:
    > Change-Id: I42e99cb75567825ac7751fc0ec56e4c45132628b
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/210425
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    
    Bug: skia:
    Change-Id: Iedc934c378b793558f879a11cd2faf9c12e98b55

--- src/gpu/gl/GrGLGpuCommandBuffer.h
+++ src/gpu/gl/GrGLGpuCommandBuffer.h
@@ -55,8 +55,13 @@ class GrGLGpuRTCommandBuffer : public GrGpuRTCommandBuffer {
 public:
     GrGLGpuRTCommandBuffer(GrGLGpu* gpu) : fGpu(gpu) {}
 
-    void begin() override;
-    void end() override {}
+    void begin() override {
+        fGpu->beginCommandBuffer(fRenderTarget, fColorLoadAndStoreInfo, fStencilLoadAndStoreInfo);
+    }
+
+    void end() override {
+        fGpu->endCommandBuffer(fRenderTarget, fColorLoadAndStoreInfo, fStencilLoadAndStoreInfo);
+    }
 
     void discard() override { }
 
@@ -79,9 +84,8 @@ public:
                                  srcRect.height(), bufferColorType, transferBuffer, offset);
     }
 
-    void set(GrRenderTarget*, GrSurfaceOrigin,
-             const GrGpuRTCommandBuffer::LoadAndStoreInfo&,
-             const GrGpuRTCommandBuffer::StencilLoadAndStoreInfo&);
+    void set(GrRenderTarget*, GrSurfaceOrigin, const LoadAndStoreInfo&, const
+             StencilLoadAndStoreInfo&);
 
     void reset() {
         fRenderTarget = nullptr;
