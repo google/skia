abcc6e50d4c1a9f36743501b82c1b378a99828bf
Mike Reed <reed@google.com>
2019-05-23 15:30:07 -0400

    split RGB into YUV planes
    Change-Id: Ifd1d010068691e187561a25aff45d8b67ae62c39

--- tests/YUVTest.cpp
+++ tests/YUVTest.cpp
@@ -124,3 +124,42 @@ DEF_TEST(Jpeg_YUV_Codec, r) {
     // A PNG should fail.
     codec_yuv(r, "images/arrow.png", nullptr);
 }
+
+#include "include/effects/SkColorMatrix.h"
+#include "src/core/SkYUVMath.h"
+
+// Be sure that the two matrices are inverses of each other
+// (i.e. rgb2yuv and yuv2rgb
+DEF_TEST(YUVMath, reporter) {
+    const SkYUVColorSpace spaces[] = {
+        kJPEG_SkYUVColorSpace,
+        kRec601_SkYUVColorSpace,
+        kRec709_SkYUVColorSpace,
+        kIdentity_SkYUVColorSpace,
+    };
+
+    // Not sure what the theoretical precision we can hope for is, so pick a big value that
+    // passes (when I think we're correct).
+    const float tolerance = 1.0f/(1 << 18);
+
+    for (auto cs : spaces) {
+        float r2y[20], y2r[20];
+        SkColorMatrix_RGB2YUV(cs, r2y);
+        SkColorMatrix_YUV2RGB(cs, y2r);
+
+        SkColorMatrix r2ym, y2rm;
+        r2ym.setRowMajor(r2y);
+        y2rm.setRowMajor(y2r);
+        r2ym.postConcat(y2rm);
+
+        float tmp[20];
+        r2ym.getRowMajor(tmp);
+        for (int i = 0; i < 20; ++i) {
+            float expected = 0;
+            if (i % 6 == 0) {   // diagonal
+                expected = 1;
+            }
+            REPORTER_ASSERT(reporter, SkScalarNearlyEqual(tmp[i], expected, tolerance));
+        }
+    }
+}
