6a5f7e29a8499e0601dd5cd243803f44b186eca3
Mike Reed <reed@google.com>
2019-05-23 15:30:07 -0400

    split RGB into YUV planes
    Change-Id: Ifd1d010068691e187561a25aff45d8b67ae62c39
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/215442
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- gm/wacky_yuv_formats.cpp
+++ gm/wacky_yuv_formats.cpp
@@ -1257,3 +1257,125 @@ private:
 DEF_GM(return new YUVMakeColorSpaceGM();)
 
 }
+
+///////////////
+
+#include "tools/Resources.h"
+#include "src/core/SkYUVMath.h"
+#include "src/core/SkAutoPixmapStorage.h"
+#include "include/effects/SkColorMatrix.h"
+
+static void draw_into_alpha(const SkImage* img, sk_sp<SkColorFilter> cf, const SkPixmap& dst) {
+    auto canvas = SkCanvas::MakeRasterDirect(dst.info(), dst.writable_addr(), dst.rowBytes());
+    canvas->scale(1.0f * dst.width() / img->width(), 1.0f * dst.height() / img->height());
+    SkPaint paint;
+    paint.setFilterQuality(kLow_SkFilterQuality);
+    paint.setColorFilter(cf);
+    paint.setBlendMode(SkBlendMode::kSrc);
+    canvas->drawImage(img, 0, 0, &paint);
+}
+
+static void split_into_yuv(const SkImage* img, SkYUVColorSpace cs, const SkPixmap dst[3]) {
+    float m[20];
+    SkColorMatrix_RGB2YUV(cs, m);
+
+    memcpy(m + 15, m + 0, 5 * sizeof(float));   // copy Y into A
+    draw_into_alpha(img, SkColorFilters::Matrix(m), dst[0]);
+
+    memcpy(m + 15, m + 5, 5 * sizeof(float));   // copy U into A
+    draw_into_alpha(img, SkColorFilters::Matrix(m), dst[1]);
+
+    memcpy(m + 15, m + 10, 5 * sizeof(float));   // copy V into A
+    draw_into_alpha(img, SkColorFilters::Matrix(m), dst[2]);
+}
+
+static void draw_diff(SkCanvas* canvas, SkScalar x, SkScalar y,
+                      const SkImage* a, const SkImage* b) {
+    auto sh = SkShaders::Blend(SkBlendMode::kDifference, a->makeShader(), b->makeShader());
+    SkPaint paint;
+    paint.setShader(sh);
+    canvas->save();
+    canvas->translate(x, y);
+    canvas->drawRect(SkRect::MakeWH(a->width(), a->height()), paint);
+
+    SkColorMatrix cm;
+    cm.setScale(64, 64, 64);
+    paint.setShader(sh->makeWithColorFilter(SkColorFilters::Matrix(cm)));
+    canvas->translate(0, a->height());
+    canvas->drawRect(SkRect::MakeWH(a->width(), a->height()), paint);
+
+    canvas->restore();
+}
+
+// Exercises SkColorMatrix_RGB2YUV for yuv colorspaces, showing the planes, and the
+// resulting (recombined) images (gpu only for now).
+//
+class YUVSplitterGM : public skiagm::GM {
+    sk_sp<SkImage>      fOrig;
+    SkAutoPixmapStorage fStorage[3];
+    SkPixmap            fPM[3];
+
+public:
+    YUVSplitterGM() {}
+
+protected:
+
+    SkString onShortName() override {
+        return SkString("yuv_splitter");
+    }
+
+    SkISize onISize() override {
+        return SkISize::Make(1024, 768);
+    }
+
+    void onOnceBeforeDraw() override {
+        fOrig = GetResourceAsImage("images/mandrill_256.png");
+
+        SkImageInfo info = SkImageInfo::Make(fOrig->width(), fOrig->height(), kAlpha_8_SkColorType,
+                                             kPremul_SkAlphaType);
+        fStorage[0].alloc(info);
+        if (0) {
+            // if you want to scale U,V down by 1/2
+            info = info.makeWH(info.width()/2, info.height()/2);
+        }
+        fStorage[1].alloc(info);
+        fStorage[2].alloc(info);
+        for (int i = 0; i < 3; ++i) {
+            fPM[i] = fStorage[i];
+        }
+    }
+
+    void onDraw(SkCanvas* canvas) override {
+        SkYUVAIndex indices[4];
+        indices[SkYUVAIndex::kY_Index] = {0, SkColorChannel::kR};
+        indices[SkYUVAIndex::kU_Index] = {1, SkColorChannel::kR};
+        indices[SkYUVAIndex::kV_Index] = {2, SkColorChannel::kR};
+        indices[SkYUVAIndex::kA_Index] = {-1, SkColorChannel::kR};
+
+        canvas->translate(fOrig->width(), 0);
+        canvas->save();
+        for (auto cs : {kRec709_SkYUVColorSpace, kRec601_SkYUVColorSpace, kJPEG_SkYUVColorSpace}) {
+            split_into_yuv(fOrig.get(), cs, fPM);
+            auto img = SkImage::MakeFromYUVAPixmaps(canvas->getGrContext(), cs, fPM, indices,
+                                                    fPM[0].info().dimensions(),
+                                                    kTopLeft_GrSurfaceOrigin,
+                                                    false, false, nullptr);
+            if (img) {
+                canvas->drawImage(img, 0, 0, nullptr);
+                draw_diff(canvas, 0, fOrig->height(), fOrig.get(), img.get());
+            }
+            canvas->translate(fOrig->width(), 0);
+        }
+        canvas->restore();
+        canvas->translate(-fOrig->width(), 0);
+
+        canvas->drawImage(SkImage::MakeRasterCopy(fPM[0]), 0, 0, nullptr);
+        canvas->drawImage(SkImage::MakeRasterCopy(fPM[1]), 0, fPM[0].height(), nullptr);
+        canvas->drawImage(SkImage::MakeRasterCopy(fPM[2]),
+                          0, fPM[0].height() + fPM[1].height(), nullptr);
+    }
+
+private:
+    typedef GM INHERITED;
+};
+DEF_GM( return new YUVSplitterGM; )
