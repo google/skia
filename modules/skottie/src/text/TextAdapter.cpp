87a5c31e7f525423979de7dc5cb86c620cc77878
Florin Malita <fmalita@chromium.org>
2019-05-28 12:58:18 -0400

    [skottie] Support multiple text fragments
    In preparation for text range selectors, update Shaper and the text
    adapter logic to deal with multiple text "fragments".
    
    For now, we're still only emitting one fragment per text value.
    
    Change-Id: I0f2ed86da77e9aaf22b9cb138c3a5f8f957393fe

--- modules/skottie/src/text/TextAdapter.cpp
+++ modules/skottie/src/text/TextAdapter.cpp
@@ -9,39 +9,67 @@
 #include "modules/sksg/include/SkSGDraw.h"
 #include "modules/sksg/include/SkSGGroup.h"
 #include "modules/sksg/include/SkSGPaint.h"
+#include "modules/sksg/include/SkSGRect.h"
 #include "modules/sksg/include/SkSGText.h"
+#include "modules/sksg/include/SkSGTransform.h"
 
 namespace skottie {
 
-TextAdapter::TextAdapter(sk_sp<sksg::Group> root)
-    : fRoot(std::move(root))
-    , fTextNode(sksg::TextBlob::Make())
-    , fFillColor(sksg::Color::Make(SK_ColorTRANSPARENT))
-    , fStrokeColor(sksg::Color::Make(SK_ColorTRANSPARENT))
-    , fFillNode(sksg::Draw::Make(fTextNode, fFillColor))
-    , fStrokeNode(sksg::Draw::Make(fTextNode, fStrokeColor))
-    , fHadFill(false)
-    , fHadStroke(false) {
-    // Build a SG fragment with the following general format:
-    //
-    // [Group]
-    //   [Draw]
-    //     [FillPaint]
-    //     [Text]*
-    //   [Draw]
-    //     [StrokePaint]
-    //     [Text]*
-    //
-    // * where the text node is shared
-
-    fFillColor->setAntiAlias(true);
-    fStrokeColor->setAntiAlias(true);
-    fStrokeColor->setStyle(SkPaint::kStroke_Style);
-}
+TextAdapter::TextAdapter(sk_sp<sksg::Group> root) : fRoot(std::move(root)) {}
 
 TextAdapter::~TextAdapter() = default;
 
+struct TextAdapter::FragmentRec {
+    // More text SG props will surface here as we add range selector support.
+    sk_sp<sksg::TransformEffect> fRoot;
+};
+
+TextAdapter::FragmentRec TextAdapter::buildFragment(const skottie::Shaper::Fragment& frag) const {
+    // For a given shaped fragment, build a corresponding SG fragment:
+    //
+    //   [TransformEffect] -> [Transform]
+    //     [Group]
+    //       [Draw] -> [TextBlob*] [FillPaint]
+    //       [Draw] -> [TextBlob*] [StrokePaint]
+    //
+    // * where the blob node is shared
+
+    auto blob_node = sksg::TextBlob::Make(frag.fBlob);
+    blob_node->setPosition(frag.fPos);
+
+    std::vector<sk_sp<sksg::RenderNode>> draws;
+    draws.reserve(static_cast<size_t>(fText.fHasFill) + static_cast<size_t>(fText.fHasStroke));
+
+    SkASSERT(fText.fHasFill || fText.fHasStroke);
+
+    if (fText.fHasFill) {
+        auto fill_paint = sksg::Color::Make(fText.fFillColor);
+        fill_paint->setAntiAlias(true);
+        draws.push_back(sksg::Draw::Make(blob_node, std::move(fill_paint)));
+    }
+    if (fText.fHasStroke) {
+        auto stroke_paint = sksg::Color::Make(fText.fStrokeColor);
+        stroke_paint->setAntiAlias(true);
+        stroke_paint->setStyle(SkPaint::kStroke_Style);
+        draws.push_back(sksg::Draw::Make(blob_node, std::move(stroke_paint)));
+    }
+
+    SkASSERT(!draws.empty());
+
+    auto draws_node = (draws.size() > 1)
+            ? sksg::Group::Make(std::move(draws))
+            : std::move(draws[0]);
+
+    return {
+        sksg::TransformEffect::Make(std::move(draws_node), SkMatrix::I()),
+    };
+}
+
 void TextAdapter::apply() {
+    if (!fText.fHasFill && !fText.fHasStroke) {
+        return;
+    }
+
     const Shaper::TextDesc text_desc = {
         fText.fTypeface,
         fText.fTextSize,
@@ -50,6 +78,17 @@ void TextAdapter::apply() {
         fText.fVAlign,
     };
     const auto shape_result = Shaper::Shape(fText.fText, text_desc, fText.fBox);
+
+    // Rebuild all fragments.
+    // TODO: we can be smarter here and try to reuse the existing SG structure if needed.
+
+    fRoot->clear();
+    fFragments.clear();
+
+    for (const auto& frag : shape_result.fFragments) {
+        fFragments.push_back(this->buildFragment(frag));
+        fRoot->addChild(fFragments.back().fRoot);
+    }
 
 #if (0)
     // Enable for text box debugging/visualization.
@@ -68,43 +107,6 @@ void TextAdapter::apply() {
     fRoot->addChild(sksg::Draw::Make(sksg::Rect::Make(shape_result.computeBounds()),
                                      std::move(bounds_color)));
 #endif
-
-    fTextNode->setBlob(shape_result.fBlob);
-    fTextNode->setPosition(shape_result.fPos);
-
-    fFillColor->setColor(fText.fFillColor);
-    fStrokeColor->setColor(fText.fStrokeColor);
-    fStrokeColor->setStrokeWidth(fText.fStrokeWidth);
-
-    // Turn the state transition into a tri-state value:
-    //   -1: detach node
-    //    0: no change
-    //    1: attach node
-    const auto   fill_change = SkToInt(fText.fHasFill) - SkToInt(fHadFill);
-    const auto stroke_change = SkToInt(fText.fHasStroke) - SkToInt(fHadStroke);
-
-    // Sync SG topology.
-    if (fill_change || stroke_change) {
-        // This is trickier than it should be because sksg::Group only allows adding children
-        // in paint-order.
-        if (stroke_change < 0 || (fHadStroke && fill_change > 0)) {
-            fRoot->removeChild(fStrokeNode);
-        }
-
-        if (fill_change < 0) {
-            fRoot->removeChild(fFillNode);
-        } else if (fill_change > 0) {
-            fRoot->addChild(fFillNode);
-        }
-
-        if (stroke_change > 0 || (fHadStroke && fill_change > 0)) {
-            fRoot->addChild(fStrokeNode);
-        }
-    }
-
-    // Track current state.
-    fHadFill   = fText.fHasFill;
-    fHadStroke = fText.fHasStroke;
 }
 
 } // namespace skottie
