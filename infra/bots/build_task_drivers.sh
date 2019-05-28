7c3e2b85d0e90d0efd8797d878dc9657976dbf18
Eric Boren <borenet@google.com>
2019-05-28 08:36:53 -0400

    [infra] Add UpdateGoDeps bot
    Change-Id: I8cb78d16589b28b2965962caf7a455bf046c4b15

---
+++ infra/bots/build_task_drivers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x -e
+
+export GOCACHE="$(pwd)/cache/go_cache"
+export GOPATH="$(pwd)/cache/gopath"
+export GOROOT="$(pwd)/go/go"
+
+cd skia
+
+#task_drivers_dir=infra/bots/task_drivers
+#for td in $(cd ${task_drivers_dir} && ls); do
+#  go build -o ${1}/${td} ${task_drivers_dir}/${td}/${td}.go
+#done
+
+# Build task drivers from the infra repo.
+export GOBIN="${1}"
+go install -v go.skia.org/infra/infra/bots/task_drivers/update_go_deps
