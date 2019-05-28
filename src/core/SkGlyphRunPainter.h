be5c13b2362dbde3e95d9cdf3199313e3dec47d2
Herb Derby <herb@google.com>
2019-05-28 11:30:33 -0400

    Replace SkStrikeSpec with SkStrikeSpecStorage
    The SkGlyphRunPainterInterface passed a strike cache in,
    but it was only ever needed to get Descriptor. Pass a narrower
    object in that just handles strike specifications.
    
    glyphCache -> strike in several places
    
    Change-Id: I98dd9e527149177e61ea26eba64b7868765ac64e

--- src/core/SkGlyphRunPainter.h
+++ src/core/SkGlyphRunPainter.h
@@ -21,6 +21,7 @@ class GrRenderTargetContext;
 #endif
 
 class SkGlyphRunPainterInterface;
+class SkStrikeSpecStorage;
 
 class SkStrikeCommon {
 public:
@@ -145,28 +146,30 @@ public:
     virtual void startRun(const SkGlyphRun& glyphRun, bool useSDFT) = 0;
 
     virtual void processDeviceMasks(SkSpan<const SkGlyphPos> masks,
-                                    SkStrikeInterface* strike) = 0;
+                                    const SkStrikeSpecStorage& strikeSpec) = 0;
 
     virtual void processSourcePaths(SkSpan<const SkGlyphPos> paths,
-                                    SkStrikeInterface* strike, SkScalar cacheToSourceScale) = 0;
+                                    const SkStrikeSpecStorage& strikeSpec) = 0;
 
     virtual void processDevicePaths(SkSpan<const SkGlyphPos> paths) = 0;
 
     virtual void processSourceSDFT(SkSpan<const SkGlyphPos> masks,
-                                   SkStrikeInterface* strike,
+                                   const SkStrikeSpecStorage& strikeSpec,
                                    const SkFont& runFont,
-                                   SkScalar cacheToSourceScale,
                                    SkScalar minScale,
                                    SkScalar maxScale,
                                    bool hasWCoord) = 0;
 
+    // While strikeSpec has .strikeToSourceRatio(), it should be ignored in favor of the argument.
+    // The fallback code currently calculates all this itself.
+    // TODO: move into strike spec?
     virtual void processSourceFallback(SkSpan<const SkGlyphPos> masks,
-                                       SkStrikeInterface* strike,
-                                       SkScalar cacheToSourceScale,
+                                       const SkStrikeSpecStorage& strikeSpec,
+                                       SkScalar strikeToSourceRatio,
                                        bool hasW) = 0;
 
     virtual void processDeviceFallback(SkSpan<const SkGlyphPos> masks,
-                                       SkStrikeInterface* strike) = 0;
+                                       const SkStrikeSpecStorage& strikeSpec) = 0;
 
 };
 
