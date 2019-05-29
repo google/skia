81ef6d0ecff83274da77b453780fd2ae4b7e949b
Mike Klein <mtklein@google.com>
2019-05-29 12:57:54 -0500

    sketch an skvm
    With all the thinking around a stack-based interpreter,
    I figured I'd sketch out some ideas for a register VM too.
    
    I kind of have the hunch that this is the direction that
    will actually let us replace large amounts of Skia's CPU
    backend with an efficient interpreter or JIT.
    
    Nothing actually evaluates anything right now, but a little
    demo unit test builds and prints out some not-entirely-toy
    programs.
    
    Key ideas:
       - explicitly distinguish uniforms and varyings
         - uniforms are a void* / size_t / ssize_t pun,
           arguments and scalars for control flow
         - varyings are an int/float pun vector, most data
    
       - a Builder type to make this somewhat user friendly,
         and particlarly to keep uniform/varying distinctions
         enforced by the C++ type system
    
       - all values are immutable, identified by the index of
         the operation that creates them in the _very_ simple
         ValOp-list IR.
    
    Change-Id: Ia2b5ba4a3fc27556f5b6ba95cd1ace46d3217403

---
+++ src/skvm/SkVM.h
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkVM_DEFINED
+#define SkVM_DEFINED
+
+#include "include/core/SkTypes.h"
+#include <vector>
+
+namespace skvm {
+
+    enum class Op : int /* 8-bit is probably plenty. */ {
+        jump, jump_if, jump_if_not,
+        store8, store32,
+        label,
+        load8,  load32,
+        splat,
+        add_f32, sub_f32, mul_f32, div_f32, mad_f32,
+        add_i32, sub_i32, mul_i32, div_i32,
+        bit_and, bit_or, bit_xor,
+        shl, shr, sra,
+        to_f32, to_i32,
+    };
+
+    using ID = int;  // Could go 16-bit?
+
+    struct ValOp {
+        Op op;
+        ID x,y,z;
+    };
+
+    void dump(const ValOp*, int n);
+
+    struct Uniform { ID id; };
+    struct Varying { ID id; };
+    struct Label   { ID id; };
+
+    class Builder {
+    public:
+        std::vector<ValOp> valops;
+
+        Label label();
+        void jump       (Label target);
+        void jump_if    (Label target, Uniform condition);
+        void jump_if_not(Label target, Uniform condition);
+
+        void store8 (Uniform ptr, Varying val);
+        void store32(Uniform ptr, Varying val);
+
+        Varying load8 (Uniform ptr);
+        Varying load32(Uniform ptr);
+
+        Uniform arg(int i);
+
+        Varying splat(int   n);
+        Varying splat(float f);
+
+        Varying add_f32(Varying x, Varying y);
+        Varying sub_f32(Varying x, Varying y);
+        Varying mul_f32(Varying x, Varying y);
+        Varying div_f32(Varying x, Varying y);
+        Varying mad_f32(Varying x, Varying y, Varying z);
+
+        Varying add_i32(Varying x, Varying y);
+        Varying sub_i32(Varying x, Varying y);
+        Varying mul_i32(Varying x, Varying y);
+        Varying div_i32(Varying x, Varying y);
+
+        Varying bit_and(Varying x, Varying y);
+        Varying bit_or (Varying x, Varying y);
+        Varying bit_xor(Varying x, Varying y);
+
+        Varying shl(Varying x, int bits);
+        Varying shr(Varying x, int bits);
+        Varying sra(Varying x, int bits);
+
+        Varying to_f32(Varying x);
+        Varying to_i32(Varying x);
+
+    private:
+        ID push(Op, ID, ID, ID);
+    };
+
+    // TODO: real testing
+    // TODO: simple dead code elimination
+    //   - every op that returns void (jumps, stores, i.e. op < Op::Label) is live;
+    //   - every argument to a live op is live;
+    //   - anything else can be eliminated.
+    // TODO: common subexpression elimination (literally, ValOp == ValOp)?
+    // TODO: integer operations on uniforms
+    // TODO: comparison operations on uniforms and varyings
+    // TODO: gatherN
+    // TODO: load_uniformN / broadcastN
+    // TODO: Varying if_then_else()
+    // TODO: 16-bit loads and stores
+    // TODO: 16-bit varyings?
+    // TODO: 64-bit varyings?
+    // TODO: simple interpreter
+    // TODO: decent interpreter
+    // TODO: lower to x86-64 SSE2 / SSE4.1 / AVX2 / AVX-512F
+    // TODO: lower to ARMv8
+    // TODO: lower to ARMv8.2+FP16
+    // TODO: lower to ARMv7 NEON?
+    // TODO: lower to LLVM?
+    // TODO: lower to WebASM?
+}
+
+#endif//SkVM_DEFINED
