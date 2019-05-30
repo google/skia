a14b618272fe8ce7e8b6581eac9120c8fa01958e
Michael Ludwig <michaelludwig@google.com>
2019-05-30 12:45:56 -0400

    Remove old GrFillRectOp factories
    Also renames MakeGeneric() back to Make() and updates tests
    
    Change-Id: I493c40d84ef1a075bddca72939f46a81bea5dea7

--- src/gpu/ops/GrFillRectOp.h
+++ src/gpu/ops/GrFillRectOp.h
@@ -27,14 +27,13 @@ struct SkRect;
  */
 namespace GrFillRectOp {
 
-// FIXME(michaelludwig) - To be renamed Make() after narrow functions are removed
-std::unique_ptr<GrDrawOp> MakeGeneric(GrRecordingContext* context,
-                                      GrPaint&& paint,
-                                      GrAAType aaType,
-                                      GrQuadAAFlags aaFlags,
-                                      const GrPerspQuad& deviceQuad,
-                                      const GrPerspQuad& localQuad,
-                                      const GrUserStencilSettings* stencil = nullptr);
+std::unique_ptr<GrDrawOp> Make(GrRecordingContext* context,
+                               GrPaint&& paint,
+                               GrAAType aaType,
+                               GrQuadAAFlags aaFlags,
+                               const GrPerspQuad& deviceQuad,
+                               const GrPerspQuad& localQuad,
+                               const GrUserStencilSettings* stencil = nullptr);
 
 // Utility function to create a non-AA rect transformed by view. This is used commonly enough in
 // testing and GMs that manage ops without going through GrRTC that it's worth the convenience.
@@ -43,48 +42,6 @@ std::unique_ptr<GrDrawOp> MakeNonAARect(GrRecordingContext* context,
                                         const SkMatrix& view,
                                         const SkRect& rect,
                                         const GrUserStencilSettings* stencil = nullptr);
-
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-// General purpose factory functions that handle per-edge anti-aliasing
-std::unique_ptr<GrDrawOp> MakePerEdge(GrRecordingContext* context,
-                                      GrPaint&& paint,
-                                      GrAAType aaType,
-                                      GrQuadAAFlags edgeAA,
-                                      const SkMatrix& viewMatrix,
-                                      const SkRect& rect,
-                                      const GrUserStencilSettings* stencil = nullptr);
-
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-std::unique_ptr<GrDrawOp> MakePerEdgeWithLocalMatrix(GrRecordingContext* context,
-                                                     GrPaint&& paint,
-                                                     GrAAType aaType,
-                                                     GrQuadAAFlags edgeAA,
-                                                     const SkMatrix& viewMatrix,
-                                                     const SkMatrix& localMatrix,
-                                                     const SkRect& rect,
-                                                     const GrUserStencilSettings* stl = nullptr);
-
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-std::unique_ptr<GrDrawOp> MakePerEdgeWithLocalRect(GrRecordingContext* context,
-                                                   GrPaint&& paint,
-                                                   GrAAType aaType,
-                                                   GrQuadAAFlags edgeAA,
-                                                   const SkMatrix& viewMatrix,
-                                                   const SkRect& rect,
-                                                   const SkRect& localRect,
-                                                   const GrUserStencilSettings* stencil = nullptr);
-
-// Generalization that accepts 2D convex quads instead of just rectangles. If 'localQuad' is not
-// null, this is equivalent to the "WithLocalRect" versions. Quad arrays match SkRect::toQuad order.
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-std::unique_ptr<GrDrawOp> MakePerEdgeQuad(GrRecordingContext* context,
-                                          GrPaint&& paint,
-                                          GrAAType aaType,
-                                          GrQuadAAFlags edgeAA,
-                                          const SkMatrix& viewMatrix,
-                                          const SkPoint quad[4],
-                                          const SkPoint localQuad[4],
-                                          const GrUserStencilSettings* stencil = nullptr);
 
 // Bulk API for drawing quads with a single op
 // TODO(michaelludwig) - remove if the bulk API is not useful for SkiaRenderer
@@ -95,32 +52,6 @@ std::unique_ptr<GrDrawOp> MakeSet(GrRecordingContext* context,
                                   const GrRenderTargetContext::QuadSetEntry quads[],
                                   int quadCount,
                                   const GrUserStencilSettings* stencil = nullptr);
-
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-// Specializations where all edges are treated the same. If the aa type is coverage, then the
-// edges will be anti-aliased, otherwise per-edge AA will be disabled.
-std::unique_ptr<GrDrawOp> Make(GrRecordingContext* context,
-                               GrPaint&& paint,
-                               GrAAType aaType,
-                               const SkMatrix& viewMatrix,
-                               const SkRect& rect,
-                               const GrUserStencilSettings* stencil = nullptr);
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-std::unique_ptr<GrDrawOp> MakeWithLocalMatrix(GrRecordingContext* context,
-                                              GrPaint&& paint,
-                                              GrAAType aaType,
-                                              const SkMatrix& viewMatrix,
-                                              const SkMatrix& localMatrix,
-                                              const SkRect& rect,
-                                              const GrUserStencilSettings* stencil = nullptr);
-// FIXME(michaelludwig) - To be removed after usages replaced with MakeGeneric
-std::unique_ptr<GrDrawOp> MakeWithLocalRect(GrRecordingContext* context,
-                                            GrPaint&& paint,
-                                            GrAAType aaType,
-                                            const SkMatrix& viewMatrix,
-                                            const SkRect& rect,
-                                            const SkRect& localRect,
-                                            const GrUserStencilSettings* stencil = nullptr);
 
 } // namespace GrFillRectOp
 
