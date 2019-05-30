810a1ecf61b93f03e3612b858dc7ce6fb1ba5db0
Julia Lavrova <jlavrova@google.com>
2019-05-30 16:12:56 -0400

    SkParagraph
    Change-Id: I0a4be75fd0c18021c201bcc1edfdfad8556edeff

---
+++ modules/skparagraph/src/ParagraphBuilderImpl.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ParagraphBuilderImpl.h"
+#include "ParagraphImpl.h"
+#include "include/core/SkPaint.h"
+#include "modules/skparagraph/include/ParagraphStyle.h"
+#include "src/core/SkSpan.h"
+#include "unicode/unistr.h"
+
+namespace skia {
+namespace textlayout {
+
+std::shared_ptr<ParagraphBuilder> ParagraphBuilder::make(
+        ParagraphStyle style, sk_sp<FontCollection> fontCollection) {
+    return std::make_shared<ParagraphBuilderImpl>(style, fontCollection);
+}
+
+ParagraphBuilderImpl::ParagraphBuilderImpl(
+        ParagraphStyle style, sk_sp<FontCollection> fontCollection)
+        : ParagraphBuilder(style, fontCollection), fFontCollection(std::move(fontCollection)) {
+    this->setParagraphStyle(style);
+}
+
+ParagraphBuilderImpl::~ParagraphBuilderImpl() = default;
+
+void ParagraphBuilderImpl::setParagraphStyle(const ParagraphStyle& style) {
+    fParagraphStyle = style;
+    fTextStyles.push(fParagraphStyle.getTextStyle());
+    fStyledBlocks.emplace_back(fUtf8.size(), fUtf8.size(), fParagraphStyle.getTextStyle());
+}
+
+void ParagraphBuilderImpl::pushStyle(const TextStyle& style) {
+    this->endRunIfNeeded();
+
+    fTextStyles.push(style);
+    if (!fStyledBlocks.empty() && fStyledBlocks.back().fEnd == fUtf8.size() &&
+        fStyledBlocks.back().fStyle == style) {
+        // Just continue with the same style
+    } else {
+        // Go with the new style
+        fStyledBlocks.emplace_back(fUtf8.size(), fUtf8.size(), fTextStyles.top());
+    }
+}
+
+void ParagraphBuilderImpl::pop() {
+    this->endRunIfNeeded();
+
+    if (fTextStyles.size() > 1) {
+        fTextStyles.pop();
+    } else {
+        // In this case we use paragraph style and skip Pop operation
+        SkDebugf("SkParagraphBuilder.Pop() called too many times.\n");
+    }
+
+    auto top = fTextStyles.top();
+    fStyledBlocks.emplace_back(fUtf8.size(), fUtf8.size(), top);
+}
+
+TextStyle ParagraphBuilderImpl::peekStyle() {
+    this->endRunIfNeeded();
+
+    if (!fTextStyles.empty()) {
+        return fTextStyles.top();
+    } else {
+        SkDebugf("SkParagraphBuilder._styles is empty.\n");
+        return fParagraphStyle.getTextStyle();
+    }
+}
+
+void ParagraphBuilderImpl::addText(const std::u16string& text) {
+    icu::UnicodeString unicode;
+    unicode.setTo((UChar*)text.data());
+    std::string str;
+    unicode.toUTF8String(str);
+    fUtf8.insert(fUtf8.size(), str.c_str());
+}
+
+void ParagraphBuilderImpl::addText(const char* text) {
+    fUtf8.insert(fUtf8.size(), text);
+}
+
+void ParagraphBuilderImpl::endRunIfNeeded() {
+    if (fStyledBlocks.empty()) {
+        return;
+    }
+
+    auto& last = fStyledBlocks.back();
+    if (last.fStart == fUtf8.size()) {
+        fStyledBlocks.pop_back();
+    } else {
+        last.fEnd = fUtf8.size();
+    }
+}
+
+std::unique_ptr<Paragraph> ParagraphBuilderImpl::Build() {
+    this->endRunIfNeeded();
+
+    return std::make_unique<ParagraphImpl>(fUtf8, fParagraphStyle, fStyledBlocks, fFontCollection);
+}
+
+}  // namespace textlayout
+}  // namespace skia
