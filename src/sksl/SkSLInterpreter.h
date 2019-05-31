34374ae4842e5196cc8a3e48d45a16409c6a67a0
Brian Osman <brianosman@google.com>
2019-05-31 11:15:46 -0400

    Interpreter Experiment: Vectorized interpreter
    This is a really rough first pass at making the stack-based interpreter
    operate in a SIMT fashion. Not at all ready for prime-time, but wanted to
    get eyes on it. Left to figure out:
    
    - Basic stuff with inputs/outputs. I have to manually swizzle them into
      the stack lanes (and again on the way out). Need to deal with N%4 != 0.
    - All the indirect loads and stores (possible but involve more typing).
    - Flow control. This is going to require GPU-style instruction masking?
    
    Regardless, there's enough to use this in the runtime color filter GM,
    so I've got that wired up, too. The entire bench (including draw time)
    is over 2x faster, so that's nice.
    
    Change-Id: Ieb38ffe2f55a10f72bdab844c297126fe9bedb6c

--- src/sksl/SkSLInterpreter.h
+++ src/sksl/SkSLInterpreter.h
@@ -8,6 +8,7 @@
 #ifndef SKSL_INTERPRETER
 #define SKSL_INTERPRETER
 
+#include "include/private/SkVx.h"
 #include "src/sksl/SkSLByteCode.h"
 #include "src/sksl/ir/SkSLAppendStage.h"
 #include "src/sksl/ir/SkSLExpression.h"
@@ -80,6 +81,87 @@ private:
     std::unique_ptr<Program> fProgram;
     std::unique_ptr<ByteCode> fByteCode;
     std::vector<Value> fGlobals;
+};
+
+class VecInterpreter {
+public:
+    union SValue {
+        SValue() {}
+
+        SValue(float f)
+        : fFloat(f) {}
+
+        SValue(int32_t s)
+        : fSigned(s) {}
+
+        SValue(uint32_t u)
+        : fUnsigned(u) {}
+
+        SValue(bool b)
+        : fBool(b) {}
+
+        float    fFloat;
+        int32_t  fSigned;
+        uint32_t fUnsigned;
+        bool     fBool;
+    };
+
+    using float4 = skvx::Vec<4, float>;
+    using int4   = skvx::Vec<4, int32_t>;
+    using uint4  = skvx::Vec<4, uint32_t>;
+
+    union VValue {
+        VValue() {}
+
+        VValue(float4 f)
+        : fFloat(f) {}
+
+        VValue(int4 s)
+        : fSigned(s) {}
+
+        VValue(uint4 u)
+        : fUnsigned(u) {}
+
+        float4 fFloat;
+        int4   fSigned;
+        uint4  fUnsigned;
+    };
+
+    enum TypeKind {
+        kFloat_TypeKind,
+        kInt_TypeKind,
+        kBool_TypeKind
+    };
+
+    /**
+     * 'inputs' contains the values of global 'in' variables in source order.
+     */
+    VecInterpreter(std::unique_ptr<Program> program, std::unique_ptr<ByteCode> byteCode,
+                   SValue inputs[] = nullptr);
+
+    /**
+     * Invokes the specified function with the given arguments. 'out' and 'inout' parameters will
+     * result in the 'args' array being modified. The return value is stored in 'outReturn' (may be
+     * null, in which case the return value is discarded).
+     */
+    void run(const ByteCodeFunction& f, SValue args[], SValue* outReturn);
+
+    /**
+     * Updates the global inputs.
+     */
+    void setInputs(SValue inputs[]);
+
+    /**
+     * Print bytecode disassembly to stdout.
+     */
+    void disassemble(const ByteCodeFunction&);
+private:
+    void innerRun(const ByteCodeFunction& f, VValue* stack, SValue* outReturn);
+
+
+    std::unique_ptr<Program> fProgram;
+    std::unique_ptr<ByteCode> fByteCode;
+    std::vector<VValue> fGlobals;
 };
 
 } // namespace
