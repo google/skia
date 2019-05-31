e1dbd939178ac51c8792e012fde36cf182155ce5
Mike Klein <mtklein@google.com>
2019-05-31 10:03:07 -0500

    add unit scaling mode to benchmark/nanobench
    Change-Id: I6106c51ceac59cd533449712a77c957bdf86846e

--- bench/Benchmark.h
+++ bench/Benchmark.h
@@ -79,7 +79,12 @@ public:
 
     virtual void getGpuStats(SkCanvas*, SkTArray<SkString>* keys, SkTArray<double>* values) {}
 
+    // Count of units (pixels, whatever) being exercised, to scale timing by.
+    int getUnits() const { return fUnits; }
+
 protected:
+    void setUnits(int units) { SkASSERT(units > 0); fUnits = units; }
+
     virtual void setupPaint(SkPaint* paint);
 
     virtual const char* onGetName() = 0;
@@ -96,6 +101,8 @@ protected:
     virtual SkIPoint onGetSize();
 
 private:
+    int fUnits = 1;
+
     typedef SkRefCnt INHERITED;
 };
 
