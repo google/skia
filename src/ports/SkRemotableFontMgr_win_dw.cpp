29d28ed6f2776c7bb36cb2dca2589eebc29deb24
Cliff Smolinsky <cliffsmo@microsoft.com>
2019-05-31 16:30:57 -0700

    Call IsUser32AndGdi32Available before calling SystemParametersInfoW
    Since user32 is delayloaded and running the delayload handler in the
    renderer process will crash, all calls into user32 must be gated by
    checks to see whether user32 is available. This change adds that check.
    
    Bug: chromium:969107
    Change-Id: Ic0e2338264f9a4416caf4af9f3744ca4e187366f

--- src/ports/SkRemotableFontMgr_win_dw.cpp
+++ src/ports/SkRemotableFontMgr_win_dw.cpp
@@ -23,6 +23,37 @@
 #include "src/utils/win/SkTScopedComPtr.h"
 
 #include <dwrite.h>
+#include <windows.h>
+#include <processthreadsapi.h>
+
+namespace {
+
+bool IsUser32AndGdi32Available() {
+  static auto is_user32_and_gdi32_available = []() {
+    // If win32k syscalls aren't disabled, then user32 and gdi32 are available.
+
+    typedef decltype(
+        GetProcessMitigationPolicy)* GetProcessMitigationPolicyType;
+    GetProcessMitigationPolicyType get_process_mitigation_policy_func =
+        reinterpret_cast<GetProcessMitigationPolicyType>(GetProcAddress(
+            GetModuleHandle(L"kernel32.dll"), "GetProcessMitigationPolicy"));
+
+    if (!get_process_mitigation_policy_func)
+      return true;
+
+    PROCESS_MITIGATION_SYSTEM_CALL_DISABLE_POLICY policy = {};
+    if (get_process_mitigation_policy_func(GetCurrentProcess(),
+                                           ProcessSystemCallDisablePolicy,
+                                           &policy, sizeof(policy))) {
+      return policy.DisallowWin32kSystemCalls == 0;
+    }
+
+    return true;
+  }();
+  return is_user32_and_gdi32_available;
+}
+
+}
 
 class SK_API SkRemotableFontMgr_DirectWrite : public SkRemotableFontMgr {
 private:
@@ -163,6 +194,10 @@ public:
     }
 
     static HRESULT getDefaultFontFamilyName(SkSMallocWCHAR* name) {
+        if (!IsUser32AndGdi32Available()) {
+            return E_ABORT;
+        }
+
         NONCLIENTMETRICSW metrics;
         metrics.cbSize = sizeof(metrics);
         if (0 == SystemParametersInfoW(SPI_GETNONCLIENTMETRICS,
