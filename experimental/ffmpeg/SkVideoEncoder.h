9c7e0407bbe0cd89c6b8583c7ba5c2e20ac892ea
Mike Reed <reed@google.com>
2019-06-03 11:02:07 -0400

    add simpler addFrame api (helps with threaded producers)
    Change-Id: I458dc2fb59aa32e084b0b03945afd74ff5ee42ae

--- experimental/ffmpeg/SkVideoEncoder.h
+++ experimental/ffmpeg/SkVideoEncoder.h
@@ -27,16 +27,45 @@ public:
     SkVideoEncoder();
     ~SkVideoEncoder();
 
+    /**
+     *  Begina a new recording. Balance this (after adding all of your frames) with a call
+     *  to endRecording().
+     */
     bool beginRecording(SkISize, int fps);
 
+    /**
+     *  Returns the preferred ImageInfo for this recording. Only valid if beginRecording() has
+     *  been called, and endRecording has not been called (yet).
+     */
+    SkImageInfo preferredInfo() const { return fInfo; }
+
+    /**
+     *  If you have your own pixmap, call addFrame(). Note this may fail if it uses an unsupported
+     *  ColorType or AlphaType, or the dimensions don't match those set in beginRecording.
+     *  For best results, use the SkImageInfo returned by preferredInfo().
+     */
+    bool addFrame(const SkPixmap&);
+
+    /**
+     *  As an alternative to calling addFrame(), you can call beginFrame/endFrame, and the encoder
+     *  will manage allocating a surface/canvas for you.
+     *
+     *  SkCanvas* canvas = encoder.beginFrame();
+     *  // your drawing code here, drawing into canvas
+     *  encoder.endFrame();
+     */
     SkCanvas* beginFrame();
     bool endFrame();
 
+    /**
+     *  Call this after having added all of your frames. After calling this, no more frames can
+     *  be added to this recording. To record a new video, call beginRecording().
+     */
     sk_sp<SkData> endRecording();
 
 private:
     void reset();
-    bool init(const SkImageInfo&, int fps);
+    bool init(int fps);
     bool sendFrame(AVFrame*);   // frame can be null
 
     double computeTimeStamp(const AVFrame*) const;
@@ -49,10 +78,13 @@ private:
     AVFrame*        fFrame = nullptr;
     AVPacket*       fPacket = nullptr;
 
-    sk_sp<SkSurface> fSurface;
+    SkImageInfo     fInfo;  // only defined between beginRecording() and endRecording()
     std::unique_ptr<SkRandomAccessWStream> fWStream;
+    int64_t         fCurrentPTS, fDeltaPTS;
 
-    int64_t          fCurrentPTS, fDeltaPTS;
+    // Lazily allocated, iff the client has called beginFrame() for a given recording session.
+    sk_sp<SkSurface> fSurface;
+
 };
 
 #endif
