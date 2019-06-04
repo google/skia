de4057a7b4a130743de0aec9ba9dceae26f335f3
Brian Salomon <bsalomon@google.com>
2019-06-04 15:58:31 -0400

    Add API to invalidate GL texture parameters from GrBackendTexture.
    This is modeled on how we coordinate VkImage layout changes between
    GrContext and clients.
    
    A type GrGLTextureParameters is used to track the current parameter
    state.
    
    When a client creates a GrBackendTexture in order to wrap a resource
    they created a new GrGLTextureParameters is created and the wrapped
    GrGLTexture will share ownership.
    
    When GrContext creates a non-wrapped GrGLTexture, the GrGLTexture
    creates a new GrGLTextureParameters and any GrBackendTextures created
    from that GrGLTexture will share ownership.
    
    Clients indicate parameter changes by calling
    GrBackendTexture::glTextureParametersModified().
    
    We still assume all texture parameters may have changed after a call
    to GrContext::resetContext() (for now). The "timestamp" that is used
    to implement this has been moved from GrGpu to GrGLGpu as there were
    no other use cases.
    
    Change-Id: Ic24e00488fad254a29d5eec6890278b67df6efae
    Bug: skia:7966

---
+++ src/gpu/gl/GrGLTypesPriv.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/private/GrGLTypesPriv.h"
+#include "include/core/SkScalar.h"
+#include "src/gpu/GrSwizzle.h"
+#include "src/gpu/gl/GrGLDefines.h"
+
+GrGLTextureParameters::SamplerOverriddenState::SamplerOverriddenState()
+        // These are the OpenGL defaults.
+        : fMinFilter(GR_GL_NEAREST_MIPMAP_LINEAR)
+        , fMagFilter(GR_GL_LINEAR)
+        , fWrapS(GR_GL_REPEAT)
+        , fWrapT(GR_GL_REPEAT)
+        , fMinLOD(-1000.f)
+        , fMaxLOD(1000.f)
+        , fBorderColorInvalid(false) {}
+
+void GrGLTextureParameters::SamplerOverriddenState::invalidate() {
+    fMinFilter = ~0U;
+    fMagFilter = ~0U;
+    fWrapS = ~0U;
+    fWrapT = ~0U;
+    fMinLOD = SK_ScalarNaN;
+    fMaxLOD = SK_ScalarNaN;
+    fBorderColorInvalid = true;
+}
+
+GrGLTextureParameters::NonsamplerState::NonsamplerState()
+        // These are the OpenGL defaults.
+        : fSwizzleKey(GrSwizzle::RGBA().asKey()), fBaseMipMapLevel(0), fMaxMipMapLevel(1000) {}
+
+void GrGLTextureParameters::NonsamplerState::invalidate() {
+    fSwizzleKey = ~0U;
+    fBaseMipMapLevel = ~0;
+    fMaxMipMapLevel = ~0;
+}
+
+void GrGLTextureParameters::invalidate() {
+    fSamplerOverriddenState.invalidate();
+    fNonsamplerState.invalidate();
+}
+
+void GrGLTextureParameters::set(const SamplerOverriddenState* samplerState,
+                                const NonsamplerState& nonsamplerState,
+                                ResetTimestamp currTimestamp) {
+    if (samplerState) {
+        fSamplerOverriddenState = *samplerState;
+    }
+    fNonsamplerState = nonsamplerState;
+    fResetTimestamp = currTimestamp;
+}
+
+void GrGLBackendTextureInfo::assign(const GrGLBackendTextureInfo& that, bool thisIsValid) {
+    fInfo = that.fInfo;
+    SkSafeRef(that.fParams);
+    if (thisIsValid) {
+        SkSafeUnref(fParams);
+    }
+    fParams = that.fParams;
+}
+
+void GrGLBackendTextureInfo::cleanup() { SkSafeUnref(fParams); }
