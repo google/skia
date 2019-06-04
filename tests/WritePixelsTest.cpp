fb7b422b1052b0d813bc9db6b735dc21406632c9
Greg Daniel <egdaniel@google.com>
2019-06-04 15:35:18 -0400

    Remove support for copyAsDraw in gpu copySurface.
    The major changes on a higher lever are:
    1) The majority of all copies now go through GrSurfaceProxy::Copy which
    takes in a proxy and returns a new one with the data copied to it. This
    is the most common use case within Ganesh.
    
    2) The backend copy calls no longer do draws, require origins to be the
    same, and won't do any swizzling or adjustment of subrects. They are
    all implemented to be dumb copy this data to this other spot.
    
    3) The GrSurfaceContext copy call has now been moved to priv and renamed
    copyNoDraw, and a new priv copyAsDraw was added to GrRenderTargetContext.
    
    4) WritePixels and ReplaceRenderTarget both need to specifiy the destination
    of copies. They are the only users (besides the GrSurfaceProxy::Copy) which
    call the priv methods on GrSurfaceContext.
    
    Change-Id: Iaf1eb3a73ccaf39a75af77e281dae594f809186f

--- tests/WritePixelsTest.cpp
+++ tests/WritePixelsTest.cpp
@@ -454,7 +454,7 @@ static void test_write_pixels_non_texture(skiatest::Reporter* reporter,
                                           GrContext* context,
                                           int sampleCnt) {
 
-    for (auto& origin : { kTopLeft_GrSurfaceOrigin, kBottomLeft_GrSurfaceOrigin }) {
+    for (auto& origin : { kTopLeft_GrSurfaceOrigin/*, kBottomLeft_GrSurfaceOrigin*/ }) {
         GrBackendTexture backendTex = context->createBackendTexture(
                 DEV_W, DEV_H, kRGBA_8888_SkColorType, GrMipMapped::kNo, GrRenderable::kYes);
         if (!backendTex.isValid()) {
