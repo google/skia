fc768526dabd4e48c97ebce33e382ee22d79fbc9
Mike Klein <mtklein@google.com>
2019-06-06 11:42:02 -0500

    add basic JIT using xbyak
    Long term we'll need to spin up our own codegen rather than using xbyak,
    since it uses exceptions and does not support ARM.
    
    I really don't like how I've hacked in the caching of the JITted
    program, but that'll change anyway when switching away from xbyak.
    
    Performance looks good, better than (lowp) SkRasterPipeline
    but not quite as good as hand-tuned code:
    
      Opts:     0.2 ns/px
      RP:       0.7
      F32:      0.8
      I32:      0.6
      I32_SWAR: 0.5
    
    ninja -C out dm nanobench && out/dm -m SkVM && gobjdump -D -b binary -mi386:x86-64 /tmp/code.bin --insn-width=10
    
    Change-Id: I34ce46e1d3fe5aa75f42709049a92ac79bf48bbd

--- src/core/SkVM.cpp
+++ src/core/SkVM.cpp
@@ -5,12 +5,21 @@
  * found in the LICENSE file.
  */
 
+#include "include/private/SkOnce.h"
 #include "include/private/SkVx.h"
 #include "src/core/SkOpts.h"
 #include "src/core/SkVM.h"
 #include <string.h>
+#if defined(SKVM_JIT)
+    #define XBYAK_NO_OP_NAMES
+    #include "xbyak/xbyak.h"
+#endif
 
 namespace skvm {
+
+    Program::~Program() = default;
+    Program::Program(Program&&) = default;
+    Program& Program::operator=(Program&&) = default;
 
     Program::Program(std::vector<Instruction> instructions, int regs, int loop)
         : fInstructions(std::move(instructions))
@@ -326,8 +335,158 @@ namespace skvm {
 
     // ~~~~ Program::eval() and co. ~~~~ //
 
+    #if defined(SKVM_JIT)
+        struct Program::JIT : Xbyak::CodeGenerator {
+            bool ok = true;
+
+            JIT(const std::vector<Program::Instruction>& instructions, int regs, int loop,
+                size_t strides[], int nargs)
+            {
+                // 8 float values in a ymm register.
+                constexpr int K = 8;
+
+            #if defined(SK_BUILD_FOR_WIN)
+                // TODO  Windows ABI?
+            #else
+                // These registers are used to pass the first 6 arguments,
+                // so if we stick to these we need not push, pop, spill, or move anything around.
+                Xbyak::Reg N = rdi,
+                       arg[] = { rsi, rdx, rcx, r8, r9 };
+
+                // All 16 ymm registers are available as scratch.
+                Xbyak::Ymm r[] = {
+                    ymm0, ymm1, ymm2 , ymm3 , ymm4 , ymm5 , ymm6 , ymm7 ,
+                    ymm8, ymm9, ymm10, ymm11, ymm12, ymm13, ymm14, ymm15,
+                };
+            #endif
+
+                // Label / 4-byte values we need to write after ret.
+                std::vector<std::pair<Xbyak::Label, int>> splats;
+
+                for (int i = 0; i < (int)instructions.size(); i++) {
+                    if (i == loop) {
+                        L("loop");
+                    }
+                    const Instruction& inst = instructions[i];
+                    Op  op = inst.op;
+
+                    ID   d = inst.d,
+                         x = inst.x;
+                    auto y = inst.y,
+                         z = inst.z;
+                    switch (op) {
+                        case Op::store8:  ok = false; break;
+                        case Op::store32: vmovups(ptr[arg[y.imm]], r[x]); break;
+
+                        case Op::load8:  ok = false; break;
+                        case Op::load32: vmovups(r[d], ptr[arg[y.imm]]); break;
+
+                        case Op::splat: splats.emplace_back(Xbyak::Label(), y.imm);
+                                        vbroadcastss(r[d], ptr[rip + splats.back().first]);
+                                        break;
+
+                        case Op::add_f32: vaddps(r[d], r[x], r[y.id]); break;
+                        case Op::sub_f32: vsubps(r[d], r[x], r[y.id]); break;
+                        case Op::mul_f32: vmulps(r[d], r[x], r[y.id]); break;
+                        case Op::div_f32: vdivps(r[d], r[x], r[y.id]); break;
+                        case Op::mad_f32:
+                            if (d == x   ) { vfmadd132ps(r[x   ], r[z.id], r[y.id]); } else
+                            if (d == y.id) { vfmadd213ps(r[y.id], r[x   ], r[z.id]); } else
+                            if (d == z.id) { vfmadd231ps(r[z.id], r[x   ], r[y.id]); } else
+                                           { vmulps(r[d], r[x], r[y.id]);
+                                             vaddps(r[d], r[d], r[z.id]); }
+                            break;
+
+                        case Op::add_i32: vpaddd (r[d], r[x], r[y.id]); break;
+                        case Op::sub_i32: vpsubd (r[d], r[x], r[y.id]); break;
+                        case Op::mul_i32: vpmulld(r[d], r[x], r[y.id]); break;
+
+                        case Op::bit_and: vandps(r[d], r[x], r[y.id]); break;
+                        case Op::bit_or : vorps (r[d], r[x], r[y.id]); break;
+                        case Op::bit_xor: vxorps(r[d], r[x], r[y.id]); break;
+
+                        case Op::shl: vpslld(r[d], r[x], y.imm); break;
+                        case Op::shr: vpsrld(r[d], r[x], y.imm); break;
+                        case Op::sra: vpsrad(r[d], r[x], y.imm); break;
+
+                        case Op::mul_unorm8: vpmulld(r[d], r[x], r[y.id]);
+                                             vpaddd(r[d], r[d], r[x]);
+                                             vpsrad(r[d], r[d], 8);
+                                             break;
+
+                        case Op::mad_unorm8: vpmulld(r[d], r[x], r[y.id]);
+                                             vpaddd(r[d], r[d], r[x]);
+                                             vpsrad(r[d], r[d], 8);
+                                             vpaddd(r[d], r[d], r[z.id]);
+                                             break;
+
+                        case Op::extract: if (y.imm) { vpsrld(r[d], r[x], y.imm); }
+                                          vandps(r[d], r[d], r[z.id]);
+                                          break;
+
+                        case Op::pack: vpslld(r[d], r[y.id], z.imm);
+                                       vorps (r[d], r[d   ], r[x]);
+                                       break;
+
+                        case Op::to_f32: vcvtdq2ps (r[d], r[x]); break;
+                        case Op::to_i32: vcvttps2dq(r[d], r[x]); break;
+                    }
+                }
+
+                sub(N, K);
+                for (int i = 0; i < nargs; i++) {
+                    add(arg[i], K*(int)strides[i]);
+                }
+
+                cmp(N, K-1);
+                jg("loop");
+
+                L("done");
+                vzeroupper();
+                ret();
+
+                for (auto splat : splats) {
+                    align(4);
+                    L(splat.first);
+                    dd(splat.second);
+                }
+            }
+        };
+    #endif
+
     void Program::eval(int n, void* args[], size_t strides[], int nargs) const {
-        SkOpts::eval(fInstructions.data(), (int)fInstructions.size(), fRegs, fLoop,
-                     n, args, strides, nargs);
+    #if defined(SKVM_JIT)
+        if (!fJIT) {
+            fJIT.reset(new JIT{fInstructions, fRegs, fLoop, strides, nargs});
+
+            #if 1
+            static SkOnce once;
+            once([&]{
+                SkFILEWStream code("/tmp/code.bin");
+                code.write(fJIT->getCode(), fJIT->getSize());
+            });
+            #endif
+        }
+
+        if (fJIT->ok && n >= 8) {
+            bool ran = true;
+            switch (nargs) {
+                case 0: fJIT->getCode<void(*)(int              )>()(n                  ); break;
+                case 1: fJIT->getCode<void(*)(int, void*       )>()(n, args[0]         ); break;
+                case 2: fJIT->getCode<void(*)(int, void*, void*)>()(n, args[0], args[1]); break;
+                default: ran = false; break;
+            }
+            if (ran) {
+                n &= 7;
+            }
+        }
+    #endif
+        if (n) {
+            SkOpts::eval(fInstructions.data(), (int)fInstructions.size(), fRegs, fLoop,
+                         n, args, strides, nargs);
+        }
     }
 }
+
+// TODO: argument strides (more generally types) should come earlier, the pointers themselves later.
+// TODO: share 255 splats
