9e3169b2f6699ac2560c1bfe7f9d06da8582622d
Greg Daniel <egdaniel@google.com>
2019-06-06 14:38:17 -0400

    Remove version from copy shaders.
    TBR: ethannicholas@google.com
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibb55b03fdf7b41dc9d2d46418f9be7d50977258d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/219398
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- src/gpu/vk/GrVkCopyManager.cpp
+++ src/gpu/vk/GrVkCopyManager.cpp
@@ -39,9 +39,7 @@ GrVkCopyManager::~GrVkCopyManager() {}
 bool GrVkCopyManager::createCopyProgram(GrVkGpu* gpu) {
     TRACE_EVENT0("skia", TRACE_FUNC);
 
-    const GrShaderCaps* shaderCaps = gpu->caps()->shaderCaps();
-    const char* version = shaderCaps->versionDeclString();
-    SkSL::String vertShaderText(version);
+    SkSL::String vertShaderText;
     vertShaderText.append(
         "#extension GL_ARB_separate_shader_objects : enable\n"
         "#extension GL_ARB_shading_language_420pack : enable\n"
@@ -61,7 +59,7 @@ bool GrVkCopyManager::createCopyProgram(GrVkGpu* gpu) {
         "}"
     );
 
-    SkSL::String fragShaderText(version);
+    SkSL::String fragShaderText;
     fragShaderText.append(
         "#extension GL_ARB_separate_shader_objects : enable\n"
         "#extension GL_ARB_shading_language_420pack : enable\n"
