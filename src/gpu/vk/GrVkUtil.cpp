e72515bd4999ec8573fedbc3863ee3519916b163
Robert Phillips <robertphillips@google.com>
2019-06-06 16:15:31 -0400

    Revise GrDataUtils to be more self-contained (take 3)
    This makes the GL and Vk backends share more code and sets up for the Metal implementation.
    
    Change-Id: I781d5b6188fb2d46ae4ec48204fda74fe28b18c0

--- src/gpu/vk/GrVkUtil.cpp
+++ src/gpu/vk/GrVkUtil.cpp
@@ -288,7 +288,7 @@ size_t GrVkBytesPerFormat(VkFormat vkFormat) {
             return 16;
 
         case VK_FORMAT_ETC2_R8G8B8_UNORM_BLOCK:
-            return 8;
+            return 0;
 
         default:
             SK_ABORT("Invalid Vk format");
@@ -308,6 +308,17 @@ bool GrVkFormatIsCompressed(VkFormat vkFormat) {
     }
     SK_ABORT("Invalid format");
     return false;
+}
+
+GrCompression GrVkFormat2Compression(VkFormat vkFormat) {
+    switch (vkFormat) {
+        case VK_FORMAT_ETC2_R8G8B8_UNORM_BLOCK:
+            return GrCompression::kETC1;
+        default:
+            return GrCompression::kNone;
+    }
+    SK_ABORT("Invalid format");
+    return GrCompression::kNone;
 }
 
 size_t GrVkFormatCompressedDataSize(VkFormat vkFormat, int width, int height) {
