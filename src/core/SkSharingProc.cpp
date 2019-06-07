8059344742601ccc8afc11755ddd03b8e44496a1
Nathaniel Nifong <nifong@google.com>
2019-06-07 09:43:02 -0400

    Deserialize MultiPictureDocument based SKP files (with image sharing proc) in wasm debugger.
    Change-Id: I73affae3cd05a2aa6ac1c75c8e049d352bbf3a85

---
+++ src/core/SkSharingProc.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/core/SkSharingProc.h"
+
+#include "include/core/SkData.h"
+#include "include/core/SkImage.h"
+#include "include/core/SkSerialProcs.h"
+
+namespace sharingproc {
+namespace  {
+
+sk_sp<SkData> SerializeImage(SkImage* img, void* ctx) {
+    SharingSerialContext* context = reinterpret_cast<SharingSerialContext*>(ctx);
+    uint32_t id = img->uniqueID(); // get this process's id for the image. these are not hashes.
+    // find out if we have already serialized this, and if so, what its in-file id is.
+    auto iter = context->imageMap.find(id);
+    if (iter == context->imageMap.end()) {
+        // When not present, add its id to the map and return its usual serialized form.
+        context->imageMap[id] = context->imageMap.size();
+        return img->encodeToData();
+    }
+    uint32_t fid = context->imageMap[id];
+    // if present, return only the in-file id we registered the first time we serialized it.
+    return SkData::MakeWithCopy(&fid, sizeof(fid));
+}
+
+sk_sp<SkImage> DeserializeImage(const void* data, size_t length, void* ctx) {
+    SharingDeserialContext* context = reinterpret_cast<SharingDeserialContext*>(ctx);
+    uint32_t fid;
+    // If the data is an image fid, look up an already deserialized image from our map
+    if (length == sizeof(fid)) {
+        memcpy(&fid, data, sizeof(fid));
+        if (fid < 0 || fid >= context->images.size()) {
+            SkDebugf("We do not have the data for image %d.\n", fid);
+            return nullptr;
+        }
+        return context->images[fid];
+    }
+    // Otherwise, the data is an image, deserialise it, store it in our map at its fid.
+    // TODO(nifong): make DeserialProcs accept sk_sp<SkData> so we don't have to copy this.
+    sk_sp<SkData> dataView = SkData::MakeWithCopy(data, length);
+    const sk_sp<SkImage> image = SkImage::MakeFromEncoded(std::move(dataView));
+    context->images.push_back(image);
+    return image;
+}
+
+} // namespace
+
+SkSerialProcs SharingSerializationProcs(SharingSerialContext* ctx) {
+  SkSerialProcs procs;
+  procs.fImageProc = SerializeImage;
+  procs.fImageCtx = ctx;
+  return procs;
+}
+
+SkDeserialProcs SharingDeserializationProcs(SharingDeserialContext* ctx) {
+  SkDeserialProcs procs;
+  procs.fImageProc = DeserializeImage;
+  procs.fImageCtx = ctx;
+  return procs;
+}
+
+} // namespace sharingproc
