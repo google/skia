f53bb4115786a9a4ac4644ea830dac54086ba985
Robert Phillips <robertphillips@google.com>
2019-06-11 15:08:10 -0400

    No longer forward refs and unrefs from the GrSurfaceProxy to the backing GrSurface
    This is setting up for GrIORefProxy to just become and sk_sp.
    
    Change-Id: Ica66565a353de980a7070e0788f1f2b17565baee

--- src/gpu/GrDeinstantiateProxyTracker.cpp
+++ src/gpu/GrDeinstantiateProxyTracker.cpp
@@ -18,7 +18,6 @@ void GrDeinstantiateProxyTracker::addProxy(GrSurfaceProxy* proxy) {
         SkASSERT(proxy != fProxies[i].get());
     }
 #endif
-    proxy->firstRefAccess().ref(fCache);
     fProxies.push_back(sk_sp<GrSurfaceProxy>(proxy));
 }
 
