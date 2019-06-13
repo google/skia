46e35d336e7a01a83e4bfaf30203f83794f47aa3
Robert Phillips <robertphillips@google.com>
2019-06-13 09:49:01 -0400

    Fix up wacky_yuv_formats GM and GL caps
    This CL:
      enables the Y416 row (i.e., uses RGBA_16161616)
      spruces up the GL caps configuration of R16, RG16, RGBA16 and RG16F
    
    Change-Id: I405c5acaf3f31b7ab0e9797e6f5df1d4c41d70d0

--- gm/wacky_yuv_formats.cpp
+++ gm/wacky_yuv_formats.cpp
@@ -999,12 +999,8 @@ static void make_RGBA_16(const GrCaps* caps,
         }
     }
 
-    // For this to work we need GrColorType::kRGBA_16 support, i.e.:
-    //    GL:  RGBA16 (required in GL 3.0, added by GL_EXT_texture_norm16 for ES3.1)
-    //    Vk:  VK_FORMAT_R16G16B16A16_UNORM
-    //    Mtl: MTLPixelFormatRGBA16Unorm
-//    *format = caps->getBackendFormatFromGrColorType(GrColorType::kRGBA_16161616,
-//                                                    GrSRGBEncoded::kNo);
+    *format = caps->getBackendFormatFromGrColorType(GrColorType::kRGBA_16161616,
+                                                    GrSRGBEncoded::kNo);
     return;
 }
 
