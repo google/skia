95c587cef39114f26d780322423e9220b6797c74
Brian Osman <brianosman@google.com>
2019-06-14 14:27:31 -0400

    Shuffle SkSL sources around so compiler and bytecode can be used w/o GPU
    Change-Id: I799be898582cdc3534c65c95cdf61107da2f9bb9

--- modules/particles/src/SkParticleAffector.cpp
+++ modules/particles/src/SkParticleAffector.cpp
@@ -16,11 +16,9 @@
 #include "modules/particles/include/SkParticleData.h"
 #include "src/core/SkMakeUnique.h"
 
-#if SK_SUPPORT_GPU
 #include "src/sksl/SkSLByteCode.h"
 #include "src/sksl/SkSLCompiler.h"
 #include "src/sksl/SkSLExternalValue.h"
-#endif
 
 void SkParticleAffector::apply(const SkParticleUpdateParams& params,
                                SkParticleState ps[], int count) {
@@ -439,7 +437,6 @@ private:
     SkColorCurve fCurve;
 };
 
-#if SK_SUPPORT_GPU
 static const char* kDefaultCode =
     "// float rand; Every read returns a random float [0 .. 1)\n"
     "layout(ctype=float) in uniform float dt;\n"
@@ -527,7 +524,6 @@ private:
         fRandomValue = std::move(rand);
     }
 };
-#endif
 
 void SkParticleAffector::RegisterAffectorTypes() {
     REGISTER_REFLECTED(SkParticleAffector);
@@ -541,9 +537,7 @@ void SkParticleAffector::RegisterAffectorTypes() {
     REGISTER_REFLECTED(SkSizeAffector);
     REGISTER_REFLECTED(SkFrameAffector);
     REGISTER_REFLECTED(SkColorAffector);
-#if SK_SUPPORT_GPU
     REGISTER_REFLECTED(SkInterpreterAffector);
-#endif
 }
 
 sk_sp<SkParticleAffector> SkParticleAffector::MakeLinearVelocity(const SkCurve& angle,
