e5eb3efd3f0bc2c8f379ee990f1a4908666e1a36
Julia Lavrova <jlavrova@google.com>
2019-06-17 10:34:10 -0400

    Refactor font resolution out of font iterator; few minor bug fixes
    Change-Id: Ibc60e972480f3503965af873f36001ed233382ec

--- modules/skparagraph/include/FontCollection.h
+++ modules/skparagraph/include/FontCollection.h
@@ -2,6 +2,7 @@
 #ifndef FontCollection_DEFINED
 #define FontCollection_DEFINED
 
+#include <src/core/SkSpan.h>
 #include <memory>
 #include <set>
 #include "include/core/SkFontMgr.h"
@@ -16,7 +17,7 @@ class FontCollection : public SkRefCnt {
 public:
     FontCollection();
 
-    ~FontCollection();
+    ~FontCollection() = default;
 
     size_t getFontManagersCount() const;
 
@@ -54,6 +55,14 @@ private:
         };
     };
 
+    struct Hash {
+        uint32_t operator()(const std::pair<SkFont, SkScalar>& key) const {
+            return SkTypeface::UniqueID(key.first.getTypeface()) +
+                    SkScalarCeilToInt(key.first.getSize()) + SkScalarCeilToInt(key.second);
+        }
+    };
+
+
     bool fEnableFontFallback;
     SkTHashMap<FamilyKey, sk_sp<SkTypeface>, FamilyKey::Hasher> fTypefaces;
     sk_sp<SkFontMgr> fDefaultFontManager;
@@ -61,6 +70,7 @@ private:
     sk_sp<SkFontMgr> fDynamicFontManager;
     sk_sp<SkFontMgr> fTestFontManager;
     SkString fDefaultFamilyName;
+    bool fHintingOn;
 };
 }  // namespace textlayout
 }  // namespace skia
