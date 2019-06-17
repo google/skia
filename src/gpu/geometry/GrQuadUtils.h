5b1f2069c13086cd1e221dba335de92a981046da
Michael Ludwig <michaelludwig@google.com>
2019-06-17 11:10:30 -0400

    Consolidate quad optimizations into single internal function.
    Routes all non-textured quad draws through single internal function
    
    Change-Id: Ief66864a0ad2d598982c5bf500c8a84ecbf84387

--- src/gpu/geometry/GrQuadUtils.h
+++ src/gpu/geometry/GrQuadUtils.h
@@ -9,8 +9,10 @@
 #define GrQuadUtils_DEFINED
 
 enum class GrQuadAAFlags;
+enum class GrAA : bool;
 enum class GrAAType : unsigned;
 class GrQuad;
+struct SkRect;
 
 namespace GrQuadUtils {
 
@@ -18,6 +20,23 @@ namespace GrQuadUtils {
     // Both outAAType and outEdgeFlags will be updated.
     void ResolveAAType(GrAAType requestedAAType, GrQuadAAFlags requestedEdgeFlags,
                        const GrQuad& quad, GrAAType* outAAtype, GrQuadAAFlags* outEdgeFlags);
+
+    /**
+     * Crops this quad to the provided device-space axis-aligned rectangle. If the intersection of
+     * this quad (projected) and clipDevRect results in a quadrilateral, this returns true. If not,
+     * this quad will be updated to be the smallest quad of the same type such that its intersection
+     * with clipDevRect is visually the same.
+     *
+     * The provided edge flags are updated to reflect edges clipped by clipDevRect (toggling on or
+     * or off based on clipAA policy). The provided local coordinates will be updated to reflect
+     * the updated device coordinates of this quad.
+     *
+     * 'local' may be null, in which case the new local coordinates will not be calculated. This is
+     * useful when it's known a paint does not require local coordinates. However, 'edgeFlags'
+     * cannot be null.
+     */
+    bool CropToRect(const SkRect& cropRect, GrAA cropAA, GrQuadAAFlags* edgeAA, GrQuad* quad,
+                    GrQuad* local=nullptr);
 
 }; // namespace GrQuadUtils
 
