0acfbb273deed4a1020be800c5e46e58889f02cb
Brian Osman <brianosman@google.com>
2019-06-18 17:25:12 -0400

    Add option to use a program binary cache in DM, etc
    Change-Id: I2117e41388962682a40f9db9ffc62150b30c7847

--- tools/flags/CommonFlagsGpu.cpp
+++ tools/flags/CommonFlagsGpu.cpp
@@ -8,6 +8,7 @@
 #include "include/core/SkExecutor.h"
 #include "include/gpu/GrContextOptions.h"
 #include "tools/flags/CommonFlags.h"
+#include "tools/gpu/MemoryCache.h"
 
 
 DEFINE_int(gpuThreads,
@@ -33,6 +34,7 @@ static DEFINE_bool(disableDriverCorrectnessWorkarounds, false,
 static DEFINE_bool(reduceOpListSplitting, false, "Improve opList sorting");
 static DEFINE_bool(dontReduceOpListSplitting, false, "Allow more opList splitting");
 
+static DEFINE_bool(programBinaryCache, false, "Use in-memory program binary cache");
 
 static GpuPathRenderers get_named_pathrenderers_flags(const char* name) {
     if (!strcmp(name, "none")) {
@@ -91,6 +93,11 @@ void SetCtxOptionsFromCommonFlags(GrContextOptions* ctxOptions) {
     ctxOptions->fSuppressGeometryShaders             = FLAGS_noGS;
     ctxOptions->fGpuPathRenderers                    = collect_gpu_path_renderers_from_flags();
     ctxOptions->fDisableDriverCorrectnessWorkarounds = FLAGS_disableDriverCorrectnessWorkarounds;
+
+    if (FLAGS_programBinaryCache) {
+        static sk_gpu_test::MemoryCache gMemoryCache;
+        ctxOptions->fPersistentCache = &gMemoryCache;
+    }
 
     if (FLAGS_reduceOpListSplitting) {
         SkASSERT(!FLAGS_dontReduceOpListSplitting);
