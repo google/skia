c598af1a673b3b7faec8996508a0b9501541bd70
Emircan Uysaler <emircan@google.com>
2019-06-20 11:38:03 -0400

    Enable creating protected vulkan backend
    This CL allows user to indicate that they have a protected content in
    GrVkBackendContext creation which results in protected CommandPool and Queue
    usage.
    
    Bug: skia:9016
    Change-Id: I6a478d688b6988c2c5e5e98f18f58fb21f9d26ae

--- src/gpu/SkGpuDevice.cpp
+++ src/gpu/SkGpuDevice.cpp
@@ -1695,7 +1695,8 @@ SkBaseDevice* SkGpuDevice::onCreateDevice(const CreateInfo& cinfo, const SkPaint
             format, fit, cinfo.fInfo.width(), cinfo.fInfo.height(), config,
             fRenderTargetContext->colorSpaceInfo().refColorSpace(),
             fRenderTargetContext->numStencilSamples(), GrMipMapped::kNo,
-            kBottomLeft_GrSurfaceOrigin, &props));
+            kBottomLeft_GrSurfaceOrigin, &props, SkBudgeted::kYes,
+            fRenderTargetContext->asSurfaceProxy()->isProtected()));
     if (!rtc) {
         return nullptr;
     }
