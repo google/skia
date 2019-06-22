bfba9b5a350055b23211c134c1d3395ed2771659
Brian Osman <brianosman@google.com>
2019-06-22 11:24:04 -0400

    Striped particle update
    Change-Id: Iebe711c45994c4201041b12d171af976bc5e758e

--- modules/particles/src/SkCurve.cpp
+++ modules/particles/src/SkCurve.cpp
@@ -5,10 +5,8 @@
 * found in the LICENSE file.
 */
 
-#include "modules/particles/include/SkCurve.h"
-
 #include "include/utils/SkRandom.h"
-#include "modules/particles/include/SkParticleData.h"
+#include "modules/particles/include/SkCurve.h"
 #include "modules/particles/include/SkReflected.h"
 
 constexpr SkFieldVisitor::EnumStringMapping gCurveSegmentTypeMapping[] = {
@@ -26,13 +24,13 @@ static SkColor4f operator-(SkColor4f c1, SkColor4f c2) {
 }
 
 template <typename T>
-static T eval_cubic(const T* pts, SkScalar x) {
-    SkScalar ix = (1 - x);
+static T eval_cubic(const T* pts, float x) {
+    float ix = (1 - x);
     return pts[0]*(ix*ix*ix) + pts[1]*(3*ix*ix*x) + pts[2]*(3*ix*x*x) + pts[3]*(x*x*x);
 }
 
 template <typename T>
-static T eval_segment(const T* pts, SkScalar x, int type) {
+static T eval_segment(const T* pts, float x, int type) {
     switch (type) {
         case kLinear_SegmentType:
             return pts[0] + (pts[3] - pts[0]) * x;
@@ -44,8 +42,8 @@ static T eval_segment(const T* pts, SkScalar x, int type) {
     }
 }
 
-SkScalar SkCurveSegment::eval(SkScalar x, SkScalar t, bool negate) const {
-    SkScalar result = eval_segment(fMin, x, fType);
+float SkCurveSegment::eval(float x, float t, bool negate) const {
+    float result = eval_segment(fMin, x, fType);
     if (fRanged) {
         result += (eval_segment(fMax, x, fType) - result) * t;
     }
@@ -79,10 +77,8 @@ void SkCurveSegment::visitFields(SkFieldVisitor* v) {
     }
 }
 
-SkScalar SkCurve::eval(const SkParticleUpdateParams& params, SkParticleState& ps) const {
+float SkCurve::eval(float x, SkRandom& random) const {
     SkASSERT(fSegments.count() == fXValues.count() + 1);
-
-    float x = fInput.eval(params, ps);
 
     int i = 0;
     for (; i < fXValues.count(); ++i) {
@@ -91,23 +87,22 @@ SkScalar SkCurve::eval(const SkParticleUpdateParams& params, SkParticleState& ps
         }
     }
 
-    SkScalar rangeMin = (i == 0) ? 0.0f : fXValues[i - 1];
-    SkScalar rangeMax = (i == fXValues.count()) ? 1.0f : fXValues[i];
-    SkScalar segmentX = (x - rangeMin) / (rangeMax - rangeMin);
-    if (!SkScalarIsFinite(segmentX)) {
+    float rangeMin = (i == 0) ? 0.0f : fXValues[i - 1];
+    float rangeMax = (i == fXValues.count()) ? 1.0f : fXValues[i];
+    float segmentX = (x - rangeMin) / (rangeMax - rangeMin);
+    if (!sk_float_isfinite(segmentX)) {
         segmentX = rangeMin;
     }
     SkASSERT(0.0f <= segmentX && segmentX <= 1.0f);
 
     // Always pull t and negate here, so that the stable generator behaves consistently, even if
     // our segments use an inconsistent feature-set.
-    SkScalar t = ps.fRandom.nextF();
-    bool negate = ps.fRandom.nextBool();
+    float t = random.nextF();
+    bool negate = random.nextBool();
     return fSegments[i].eval(segmentX, t, negate);
 }
 
 void SkCurve::visitFields(SkFieldVisitor* v) {
-    v->visit("Input", fInput);
     v->visit("XValues", fXValues);
     v->visit("Segments", fSegments);
 
@@ -121,7 +116,7 @@ void SkCurve::visitFields(SkFieldVisitor* v) {
     }
 }
 
-SkColor4f SkColorCurveSegment::eval(SkScalar x, SkScalar t) const {
+SkColor4f SkColorCurveSegment::eval(float x, float t) const {
     SkColor4f result = eval_segment(fMin, x, fType);
     if (fRanged) {
         result = result + (eval_segment(fMax, x, fType) - result) * t;
@@ -152,10 +147,8 @@ void SkColorCurveSegment::visitFields(SkFieldVisitor* v) {
     }
 }
 
-SkColor4f SkColorCurve::eval(const SkParticleUpdateParams& params, SkParticleState& ps) const {
+SkColor4f SkColorCurve::eval(float x, SkRandom& random) const {
     SkASSERT(fSegments.count() == fXValues.count() + 1);
-
-    float x = fInput.eval(params, ps);
 
     int i = 0;
     for (; i < fXValues.count(); ++i) {
@@ -164,18 +157,17 @@ SkColor4f SkColorCurve::eval(const SkParticleUpdateParams& params, SkParticleSta
         }
     }
 
-    SkScalar rangeMin = (i == 0) ? 0.0f : fXValues[i - 1];
-    SkScalar rangeMax = (i == fXValues.count()) ? 1.0f : fXValues[i];
-    SkScalar segmentX = (x - rangeMin) / (rangeMax - rangeMin);
-    if (!SkScalarIsFinite(segmentX)) {
+    float rangeMin = (i == 0) ? 0.0f : fXValues[i - 1];
+    float rangeMax = (i == fXValues.count()) ? 1.0f : fXValues[i];
+    float segmentX = (x - rangeMin) / (rangeMax - rangeMin);
+    if (!sk_float_isfinite(segmentX)) {
         segmentX = rangeMin;
     }
     SkASSERT(0.0f <= segmentX && segmentX <= 1.0f);
-    return fSegments[i].eval(segmentX, ps.fRandom.nextF());
+    return fSegments[i].eval(segmentX, random.nextF());
 }
 
 void SkColorCurve::visitFields(SkFieldVisitor* v) {
-    v->visit("Input", fInput);
     v->visit("XValues", fXValues);
     v->visit("Segments", fSegments);
 
