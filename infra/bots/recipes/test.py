81eb6323a9e8cae0829c95187f0cf875cc5e12ac
Florin Malita <fmalita@chromium.org>
2019-06-24 10:50:33 -0400

    [skottie] Run resources "GMs" on Lottie bots
    Run the Skottie tests checked under resources/skottie/ on Lottie bots.
    
    Bug: skia:8925
    Change-Id: I240608f1cbc70440cd1a35af52f98a7ef250ec31
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/223182
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -919,7 +919,11 @@ def test_steps(api):
 
     args.extend(['--svgs', api.flavor.device_dirs.svg_dir])
     if 'Lottie' in api.vars.builder_cfg.get('extra_config', ''):
-      args.extend(['--lotties', api.flavor.device_dirs.lotties_dir])
+      args.extend([
+        '--lotties',
+        api.flavor.device_path_join(
+            api.flavor.device_dirs.resource_dir, 'skottie'),
+        api.flavor.device_dirs.lotties_dir])
 
   args.append('--key')
   keys = key_params(api)
