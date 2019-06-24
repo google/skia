562dd8981981bf16aa28ab2fa49be437b43d2f0e
Robert Phillips <robertphillips@google.com>
2019-06-24 13:04:23 -0400

    Reduce GrCaps' reliance on GrPixelConfig
    By itself this CL isn't all that compelling but I believe we need some intermediate path to wean ourselves off of GrPixelConfig. In particular, I believe isFormatTexturable will not need an SkColorType parameter in the future.
    
    Change-Id: I6c2b8a2c4af98c1437b92c58513f34014e551b2e

--- tests/GLProgramsTest.cpp
+++ tests/GLProgramsTest.cpp
@@ -147,12 +147,14 @@ static sk_sp<GrRenderTargetContext> random_render_target_context(GrContext* cont
                                                                  const GrCaps* caps) {
     GrSurfaceOrigin origin = random->nextBool() ? kTopLeft_GrSurfaceOrigin
                                                 : kBottomLeft_GrSurfaceOrigin;
-    int sampleCnt =
-            random->nextBool() ? caps->getRenderTargetSampleCount(2, kRGBA_8888_GrPixelConfig) : 1;
-    // Above could be 0 if msaa isn't supported.
-    sampleCnt = SkTMax(1, sampleCnt);
 
     const GrBackendFormat format = caps->getBackendFormatFromColorType(kRGBA_8888_SkColorType);
+
+    int sampleCnt = random->nextBool()
+                           ? caps->getRenderTargetSampleCount(2, kRGBA_8888_SkColorType, format)
+                           : 1;
+    // Above could be 0 if msaa isn't supported.
+    sampleCnt = SkTMax(1, sampleCnt);
 
     sk_sp<GrRenderTargetContext> renderTargetContext(
         context->priv().makeDeferredRenderTargetContext(format,
