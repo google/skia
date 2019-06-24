5bde1841fbaf0df1a0323c8ac14b75e8158e6da5
Herb Derby <herb@google.com>
2019-06-24 14:53:59 -0400

    Update remote glyph cache read glyph
    Change-Id: Id57fa1169f8944b2d10063fde983cd9d8256b598

--- src/core/SkGlyph.h
+++ src/core/SkGlyph.h
@@ -152,6 +152,12 @@ public:
     bool setImage(SkArenaAlloc* alloc, SkScalerContext* scalerContext);
     bool setImage(SkArenaAlloc* alloc, const void* image);
 
+    // Merge the from glyph into this glyph using alloc to allocate image data. Return true if
+    // image data was allocated. If the image for this glyph has not been initialized, then copy
+    // the width, height, top, left, format, and image into this glyph making a copy of the image
+    // using the alloc.
+    bool setMetricsAndImage(SkArenaAlloc* alloc, const SkGlyph& from);
+
     // Returns true if the image has been set.
     bool setImageHasBeenCalled() const {
         return fImage != nullptr || this->isEmpty() || this->imageTooLarge();
