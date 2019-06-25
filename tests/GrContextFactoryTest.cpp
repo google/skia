6dd0b56127491b0b5ca3951853eaeaf5682267b7
Chris Dalton <csmartdalton@google.com>
2019-06-25 15:34:29 -0600

    Remove "nvpr" configs
    The majority of our gm testing has been disabling nvpr, which doesn't
    match our real-world behavior where we use nvpr whenever available.
    This CL fixes the issue by completely removing the explicit nvpr
    configx. Now if we have nvpr, you get it.
    
    This CL also lowers the nvpr priority in the path renderer chain and
    adds a "Non_NVPR" job on Quadro where we can continue to test our
    non-nvpr codepaths on NVIDIA.
    
    Bug: skia:
    Change-Id: I6a36f1101c8218adcaaf10cab25d2c28e70371f0

--- tests/GrContextFactoryTest.cpp
+++ tests/GrContextFactoryTest.cpp
@@ -15,38 +15,6 @@
 
 using namespace sk_gpu_test;
 
-DEF_GPUTEST(GrContextFactory_NVPRContextOptionHasPathRenderingSupport, reporter, options) {
-    // Test that if NVPR is requested, the context always has path rendering
-    // or the context creation fails.
-    for (int i = 0; i < GrContextFactory::kContextTypeCnt; ++i) {
-        GrContextFactory testFactory(options);
-        // Test that if NVPR is possible, caps are in sync.
-        GrContextFactory::ContextType ctxType = static_cast<GrContextFactory::ContextType>(i);
-        GrContext* context = testFactory.get(ctxType,
-                                           GrContextFactory::ContextOverrides::kRequireNVPRSupport);
-        if (!context) {
-            continue;
-        }
-        REPORTER_ASSERT(reporter,
-                        context->priv().caps()->shaderCaps()->pathRenderingSupport());
-    }
-}
-
-DEF_GPUTEST(GrContextFactory_NoPathRenderingIfNVPRDisabled, reporter, options) {
-    // Test that if NVPR is explicitly disabled, the context has no path rendering support.
-
-    for (int i = 0; i <= GrContextFactory::kLastContextType; ++i) {
-        GrContextFactory testFactory(options);
-        GrContextFactory::ContextType ctxType = (GrContextFactory::ContextType)i;
-        GrContext* context =
-            testFactory.get(ctxType, GrContextFactory::ContextOverrides::kDisableNVPR);
-        if (context) {
-            REPORTER_ASSERT(reporter,
-                            !context->priv().caps()->shaderCaps()->pathRenderingSupport());
-        }
-    }
-}
-
 DEF_GPUTEST(GrContextFactory_abandon, reporter, options) {
     for (int i = 0; i < GrContextFactory::kContextTypeCnt; ++i) {
         GrContextFactory testFactory(options);
