68d7c3dffe42cc24f430568e90e67aad07887f7f
Greg Daniel <egdaniel@google.com>
2019-06-26 15:19:25 -0400

    Removed made-up kSBGRA pixel config.
    We made up this pixel config and don't actually use it ourselves so lets
    kill it for simplicity.
    
    Change-Id: I6ae1c78fe7ada336a2411d295e8836dfeecb2d5c

--- src/gpu/vk/GrVkUtil.cpp
+++ src/gpu/vk/GrVkUtil.cpp
@@ -39,9 +39,6 @@ bool GrPixelConfigToVkFormat(GrPixelConfig config, VkFormat* format) {
         case kSRGBA_8888_GrPixelConfig:
             *format = VK_FORMAT_R8G8B8A8_SRGB;
             return true;
-        case kSBGRA_8888_GrPixelConfig:
-            *format = VK_FORMAT_B8G8R8A8_SRGB;
-            return true;
         case kRGBA_1010102_GrPixelConfig:
             *format = VK_FORMAT_A2B10G10R10_UNORM_PACK32;
             return true;
@@ -112,8 +109,6 @@ bool GrVkFormatPixelConfigPairIsValid(VkFormat format, GrPixelConfig config) {
             return kBGRA_8888_GrPixelConfig == config;
         case VK_FORMAT_R8G8B8A8_SRGB:
             return kSRGBA_8888_GrPixelConfig == config;
-        case VK_FORMAT_B8G8R8A8_SRGB:
-            return kSBGRA_8888_GrPixelConfig == config;
         case VK_FORMAT_R8G8B8_UNORM:
             return kRGB_888_GrPixelConfig == config;
         case VK_FORMAT_R8G8_UNORM:
