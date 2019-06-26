bec92969a2ca14c2f0ff552a7504bc5b0cbc32ef
Eric Boren <borenet@google.com>
2019-06-26 13:18:05 -0400

    [infra] Make infra tests idempotent
    Change-Id: I1a75b211753ea5e097234dd4104d0906cfd04673

---
+++ infra/bots/run_recipe.cmd
@@ -0,0 +1,20 @@
+kitchen.exe cook ^
+    -checkout-dir recipe_bundle ^
+    -mode swarming ^
+     -luci-system-account system ^
+    -cache-dir cache ^
+    -temp-dir tmp ^
+    -known-gerrit-host android.googlesource.com ^
+    -known-gerrit-host boringssl.googlesource.com ^
+    -known-gerrit-host chromium.googlesource.com ^
+    -known-gerrit-host dart.googlesource.com ^
+    -known-gerrit-host fuchsia.googlesource.com ^
+    -known-gerrit-host go.googlesource.com ^
+    -known-gerrit-host llvm.googlesource.com ^
+    -known-gerrit-host skia.googlesource.com ^
+    -known-gerrit-host webrtc.googlesource.com ^
+    -output-result-json ${ISOLATED_OUTDIR}/build_result_filename ^
+    -workdir . ^
+    -recipe "%1" ^
+    -properties "%2" ^
+    -logdog-annotation-url logdog://logs.chromium.org/%3/%SWARMING_TASK_ID%/+/annotations
