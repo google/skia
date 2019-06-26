2c1662e24da766587a3b19c658493124f8135a47
Eric Boren <borenet@google.com>
2019-06-26 07:25:29 -0400

    [infra] Make Isolate*, BundleRecipes, and BuildTaskDrivers idempotent
    This should save about a minute on most jobs.
    
    Change-Id: Ida39672333f9e1ee13a1ab85388e7461b1684f8f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/223796
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -713,7 +713,8 @@ func bundleRecipes(b *specs.TasksCfgBuilder) string {
 		EnvPrefixes: map[string][]string{
 			"PATH": []string{"cipd_bin_packages", "cipd_bin_packages/bin"},
 		},
-		Isolate: relpath("swarm_recipe.isolate"),
+		Idempotent: true,
+		Isolate:    relpath("swarm_recipe.isolate"),
 	})
 	return BUNDLE_RECIPES_NAME
 }
@@ -731,7 +732,8 @@ func buildTaskDrivers(b *specs.TasksCfgBuilder) string {
 		EnvPrefixes: map[string][]string{
 			"PATH": {"cipd_bin_packages", "cipd_bin_packages/bin", "go/go/bin"},
 		},
-		Isolate: "task_drivers.isolate",
+		Idempotent: true,
+		Isolate:    "task_drivers.isolate",
 	})
 	return BUILD_TASK_DRIVERS_NAME
 }
@@ -818,6 +820,7 @@ func isolateCIPDAsset(b *specs.TasksCfgBuilder, name string) string {
 		},
 		Command:    []string{"/bin/cp", "-rL", asset.path, "${ISOLATED_OUTDIR}"},
 		Dimensions: linuxGceDimensions(MACHINE_TYPE_SMALL),
+		Idempotent: true,
 		Isolate:    relpath("empty.isolate"),
 	})
 	return name
