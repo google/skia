e34595175b3b182166f71d8026dd1a6fe1f9509d
Jim Van Verth <jvanverth@google.com>
2019-06-26 10:00:17 -0400

    Address TSAN issues in Metal
    Bug: skia:8243
    Change-Id: Iea6a834e4f4e3a038fbfebf3eee60123333e5ff1

--- src/gpu/mtl/GrMtlResourceProvider.h
+++ src/gpu/mtl/GrMtlResourceProvider.h
@@ -102,9 +102,9 @@ private:
     private:
         id<MTLBuffer> fBuffer;
         size_t        fTotalSize;
-        size_t        fHead;     // where we start allocating
-        size_t        fTail;     // where we start deallocating
         SkSpinlock    fMutex;
+        size_t        fHead SK_GUARDED_BY(fMutex);  // where we start allocating
+        size_t        fTail SK_GUARDED_BY(fMutex);  // where we start deallocating
     };
 
     GrMtlGpu* fGpu;
