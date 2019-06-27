4677d401de85ae198b99103ab4a1a7ca771dc29f
Mike Klein <mtklein@google.com>
2019-06-27 14:05:26 -0400

    steady clock is probably best
    Change-Id: I37eadc5800665af5ee862c4f108fa7c1f5000fa3

--- src/core/SkTime.cpp
+++ src/core/SkTime.cpp
@@ -78,7 +78,7 @@ double SkTime::GetNSecs() {
     clock_gettime(CLOCK_MONOTONIC, &tp);
     return tp.tv_sec * 1e9 + tp.tv_nsec;
 #else
-    auto now = std::chrono::high_resolution_clock::now();
+    auto now = std::chrono::steady_clock::now();
     std::chrono::duration<double, std::nano> ns = now.time_since_epoch();
     return ns.count();
 #endif
