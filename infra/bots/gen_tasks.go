c8554781f8304c73459065714be19dd64940008b
Ben Wagner <benjaminwagner@google.com>
2019-06-28 12:58:01 -0400

    [skqp/release] Update driver on Ubuntu18 Quadro bots
    Also remove "rack" dimension. The os dimension now aligns with the rack
    dimension, so we don't need to worry about it.
    
    Cherry-picked from https://skia-review.googlesource.com/c/skia/+/215087
    
    No-Tree-Checks: true
    No-Presubmit: true
    Bug: chromium:919950
    Change-Id: Ia82cc3532363b33bf274db33a811691a4de82bda
    Commit-Queue: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-By: Eric Boren <borenet@google.com>
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/224537
    Commit-Queue: Eric Boren <borenet@google.com>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -584,14 +584,10 @@ func defaultSwarmDimensions(parts map[string]string) []string {
 					"IntelHD2000":   "8086:0102",
 					"IntelHD405":    "8086:22b1",
 					"IntelIris640":  "8086:5926",
-					"QuadroP400":    "10de:1cb3-384.59",
+					"QuadroP400":    "10de:1cb3-430.14",
 				}[parts["cpu_or_gpu_value"]]
 				if !ok {
 					glog.Fatalf("Entry %q not found in Ubuntu GPU mapping.", parts["cpu_or_gpu_value"])
-				}
-				if parts["os"] == "Ubuntu18" && parts["cpu_or_gpu_value"] == "QuadroP400" {
-					// Ubuntu18 has a slightly newer GPU driver.
-					gpu = "10de:1cb3-415.27"
 				}
 				d["gpu"] = gpu
 			} else if strings.Contains(parts["os"], "Mac") {
@@ -1022,16 +1018,6 @@ func calmbench(b *specs.TasksCfgBuilder, name string, parts map[string]string, c
 	task := kitchenTask(name, "calmbench", "calmbench.isolate", "", swarmDimensions(parts), nil, OUTPUT_PERF)
 	usesGit(task, name)
 	task.Dependencies = append(task.Dependencies, compileTaskName, compileParentName, ISOLATE_SKP_NAME, ISOLATE_SVG_NAME)
-	if parts["cpu_or_gpu_value"] == "QuadroP400" {
-		// Specify "rack" dimension for consistent test results.
-		// See https://bugs.chromium.org/p/chromium/issues/detail?id=784662&desc=2#c34
-		// for more context.
-		if parts["os"] == "Ubuntu18" {
-			task.Dimensions = append(task.Dimensions, "rack:2")
-		} else {
-			task.Dimensions = append(task.Dimensions, "rack:1")
-		}
-	}
 	b.MustAddTask(name, task)
 
 	// Upload results if necessary.
@@ -1185,16 +1171,6 @@ func perf(b *specs.TasksCfgBuilder, name string, parts map[string]string, compil
 	iid := internalHardwareLabel(parts)
 	if iid != nil {
 		task.Command = append(task.Command, fmt.Sprintf("internal_hardware_label=%d", *iid))
-	}
-	if parts["cpu_or_gpu_value"] == "QuadroP400" {
-		// Specify "rack" dimension for consistent test results.
-		// See https://bugs.chromium.org/p/chromium/issues/detail?id=784662&desc=2#c34
-		// for more context.
-		if parts["os"] == "Ubuntu18" {
-			task.Dimensions = append(task.Dimensions, "rack:2")
-		} else {
-			task.Dimensions = append(task.Dimensions, "rack:1")
-		}
 	}
 	b.MustAddTask(name, task)
 
