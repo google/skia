3dafeaa7e2d763706c9a0ba14d9f55097f121fbd
Manas <prometheansacrifice@gmail.com>
2019-06-30 15:00:02 +0530

    Add shared library target

--- esy/build.sh
+++ esy/build.sh
@@ -12,9 +12,12 @@ python tools/git-sync-deps
 ln -s third_party/externals/gyp tools/gyp
 if [[ $OS == 'windows' ]]
 then
-    bin/gn gen $cur__target_dir/out/Release --args='is_debug=false' || exit -1
+    bin/gn gen $cur__target_dir/out/Static --args='is_debug=false' || exit -1
+    bin/gn gen $cur__target_dir/out/Shared --args='is_debug=false is_official_build=true is_component_build=true'
 else
-    bin/gn gen $cur__target_dir/out/Release "--args=cc=\"clang\" cxx=\"clang++\" skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
+    bin/gn gen $cur__target_dir/out/Static"--args=cc=\"clang\" cxx=\"clang++\" skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
 fi
 
-ninja.exe -C $cur__target_dir/out/Release
+ninja.exe -C $cur__target_dir/out/Static
+ninja.exe -C $cur__target_dir/out/Shared
+
