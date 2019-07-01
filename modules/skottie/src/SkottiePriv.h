16661476fe946305bb6d1e4c99b77416ef7fd20b
Florin Malita <fmalita@chromium.org>
2019-07-01 13:57:43 -0400

    [skottie] Cleanup: split off layers into own CUs
    Change-Id: Ia6c27cca33f9dc8f242a50e624341dd0b465b380

--- modules/skottie/src/SkottiePriv.h
+++ modules/skottie/src/SkottiePriv.h
@@ -215,6 +215,37 @@ private:
     using INHERITED = SkNoncopyable;
 };
 
+struct AnimationBuilder::AttachLayerContext {
+    AttachLayerContext(const skjson::ArrayValue&, AnimatorScope*);
+    ~AttachLayerContext();
+
+    const skjson::ArrayValue&               fLayerList;
+    AnimatorScope*                          fScope;
+    SkTHashMap<int, sk_sp<sksg::Transform>> fLayerMatrixMap;
+    sk_sp<sksg::RenderNode>                 fCurrentMatte;
+    sk_sp<sksg::Transform>                  fCameraTransform;
+
+    enum class TransformType { kLayer, kCamera };
+
+    sk_sp<sksg::Transform> attachLayerTransform(const skjson::ObjectValue& jlayer,
+                                                const AnimationBuilder* abuilder,
+                                                TransformType type = TransformType::kLayer);
+
+private:
+    sk_sp<sksg::Transform> attachParentLayerTransform(const skjson::ObjectValue& jlayer,
+                                                      const AnimationBuilder* abuilder,
+                                                      int layer_index);
+
+    sk_sp<sksg::Transform> attachTransformNode(const skjson::ObjectValue& jlayer,
+                                               const AnimationBuilder* abuilder,
+                                               sk_sp<sksg::Transform> parent_transform,
+                                               TransformType type) const;
+
+    sk_sp<sksg::Transform> attachLayerTransformImpl(const skjson::ObjectValue& jlayer,
+                                                    const AnimationBuilder* abuilder,
+                                                    TransformType type, int layer_index);
+};
+
 } // namespace internal
 } // namespace skottie
 
