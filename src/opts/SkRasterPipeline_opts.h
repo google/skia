28f173dc93a47bba8e903130a7ba549d9a81d3a7
Mike Klein <mtklein@google.com>
2019-07-02 13:23:06 -0500

    fix GCC arm64 builds
    These two guards are checking if we're building for aarch64 and thus
    have F16 conversion instructions, but weren't checking if we want to use
    them (if we have them _and_ we're being compiled by Clang).  At head
    we're trying to pass a 2-byte uint16_t to a function expecting an 8-byte
    uint16x4_t, etc.
    
    Change-Id: I21f6cd2100ec81ccdd47c4ec0575107624cd7c5a

--- src/opts/SkRasterPipeline_opts.h
+++ src/opts/SkRasterPipeline_opts.h
@@ -972,7 +972,8 @@ SI F approx_powf(F x, F y) {
 }
 
 SI F from_half(U16 h) {
-#if defined(SK_CPU_ARM64) && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
+#if defined(JUMPER_IS_NEON) && defined(SK_CPU_ARM64) \
+    && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
     return vcvt_f32_f16(h);
 
 #elif defined(JUMPER_IS_HSW) || defined(JUMPER_IS_AVX512)
@@ -992,7 +993,8 @@ SI F from_half(U16 h) {
 }
 
 SI U16 to_half(F f) {
-#if defined(SK_CPU_ARM64) && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
+#if defined(JUMPER_IS_NEON) && defined(SK_CPU_ARM64) \
+    && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
     return vcvt_f16_f32(f);
 
 #elif defined(JUMPER_IS_HSW) || defined(JUMPER_IS_AVX512)
