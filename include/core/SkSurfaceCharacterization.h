737305af42b5f3d30737ac9da3e2536501ae4bb8
Robert Phillips <robertphillips@google.com>
2019-07-02 10:17:55 -0400

    Remove GrPixelConfig from SkSurfaceCharacterization
    Change-Id: I3127e672512a398867f39029c97eb807ea77e217

--- include/core/SkSurfaceCharacterization.h
+++ include/core/SkSurfaceCharacterization.h
@@ -17,6 +17,7 @@
 class SkColorSpace;
 
 #if SK_SUPPORT_GPU
+#include "include/gpu/GrBackendSurface.h"
 // TODO: remove the GrContext.h include once Flutter is updated
 #include "include/gpu/GrContext.h"
 #include "include/gpu/GrContextThreadSafeProxy.h"
@@ -39,7 +40,6 @@ public:
     SkSurfaceCharacterization()
             : fCacheMaxResourceBytes(0)
             , fOrigin(kBottomLeft_GrSurfaceOrigin)
-            , fConfig(kUnknown_GrPixelConfig)
             , fSampleCnt(0)
             , fIsTextureable(Textureable::kYes)
             , fIsMipMapped(MipMapped::kYes)
@@ -67,6 +67,7 @@ public:
     bool isValid() const { return kUnknown_SkColorType != fImageInfo.colorType(); }
 
     const SkImageInfo& imageInfo() const { return fImageInfo; }
+    const GrBackendFormat& backendFormat() const { return fBackendFormat; }
     GrSurfaceOrigin origin() const { return fOrigin; }
     int width() const { return fImageInfo.width(); }
     int height() const { return fImageInfo.height(); }
@@ -92,23 +93,22 @@ private:
     friend class SkDeferredDisplayListRecorder; // for 'config'
     friend class SkSurface; // for 'config'
 
-    GrPixelConfig config() const { return fConfig; }
-
     SkSurfaceCharacterization(sk_sp<GrContextThreadSafeProxy> contextInfo,
                               size_t cacheMaxResourceBytes,
                               const SkImageInfo& ii,
+                              const GrBackendFormat& backendFormat,
                               GrSurfaceOrigin origin,
-                              GrPixelConfig config,
                               int sampleCnt,
-                              Textureable isTextureable, MipMapped isMipMapped,
+                              Textureable isTextureable,
+                              MipMapped isMipMapped,
                               UsesGLFBO0 usesGLFBO0,
                               VulkanSecondaryCBCompatible vulkanSecondaryCBCompatible,
                               const SkSurfaceProps& surfaceProps)
             : fContextInfo(std::move(contextInfo))
             , fCacheMaxResourceBytes(cacheMaxResourceBytes)
             , fImageInfo(ii)
+            , fBackendFormat(backendFormat)
             , fOrigin(origin)
-            , fConfig(config)
             , fSampleCnt(sampleCnt)
             , fIsTextureable(isTextureable)
             , fIsMipMapped(isMipMapped)
@@ -120,8 +120,8 @@ private:
     void set(sk_sp<GrContextThreadSafeProxy> contextInfo,
              size_t cacheMaxResourceBytes,
              const SkImageInfo& ii,
+             const GrBackendFormat& backendFormat,
              GrSurfaceOrigin origin,
-             GrPixelConfig config,
              int sampleCnt,
              Textureable isTextureable,
              MipMapped isMipMapped,
@@ -140,8 +140,8 @@ private:
         fCacheMaxResourceBytes = cacheMaxResourceBytes;
 
         fImageInfo = ii;
+        fBackendFormat = backendFormat;
         fOrigin = origin;
-        fConfig = config;
         fSampleCnt = sampleCnt;
         fIsTextureable = isTextureable;
         fIsMipMapped = isMipMapped;
@@ -154,8 +154,8 @@ private:
     size_t                          fCacheMaxResourceBytes;
 
     SkImageInfo                     fImageInfo;
+    GrBackendFormat                 fBackendFormat;
     GrSurfaceOrigin                 fOrigin;
-    GrPixelConfig                   fConfig;
     int                             fSampleCnt;
     Textureable                     fIsTextureable;
     MipMapped                       fIsMipMapped;
