2f2001ce5bc86adee8c980d36aa998233b3ce0ae
Robert Phillips <robertphillips@google.com>
2019-07-02 11:35:51 -0400

    Add bridge between GrContext::createBackendTexture and SkSurface::MakeFromBackendTexture
    In order to effectively use the explicit backend texture allocation API Chrome needs a way to use them with surface characterizations
    
    Change-Id: Ic61eff9f3b6b0e8280481149d7c08d37a2fe7ec0

--- include/gpu/GrContext.h
+++ include/gpu/GrContext.h
@@ -40,6 +40,7 @@ class GrTextureProxy;
 struct GrVkBackendContext;
 
 class SkImage;
+class SkSurfaceCharacterization;
 class SkSurfaceProps;
 class SkTaskGroup;
 class SkTraceMemoryDump;
@@ -374,13 +375,24 @@ public:
                                           GrRenderable,
                                           GrProtected = GrProtected::kNo);
 
+
+    // If possible, create an uninitialized backend texture that is compatible with the
+    // provided characterization. The client should ensure that the returned backend texture
+    // is valid.
+    // For the Vulkan backend the layout of the created VkImage will be:
+    //      VK_IMAGE_LAYOUT_UNDEFINED.
+    GrBackendTexture createBackendTexture(const SkSurfaceCharacterization& characterization);
+
     // If possible, create a backend texture initialized to a particular color. The client should
     // ensure that the returned backend texture is valid.
     // For the Vulkan backend the layout of the created VkImage will be:
-    //      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
+    //      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL if renderable is kNo
+    //  and VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL if renderable is kYes
     GrBackendTexture createBackendTexture(int width, int height,
-                                          const GrBackendFormat&, const SkColor4f& color,
-                                          GrMipMapped, GrRenderable,
+                                          const GrBackendFormat&,
+                                          const SkColor4f& color,
+                                          GrMipMapped,
+                                          GrRenderable,
                                           GrProtected = GrProtected::kNo);
 
     // If possible, create a backend texture initialized to a particular color. The client should
@@ -388,11 +400,22 @@ public:
     // If successful, the created backend texture will be compatible with the provided
     // SkColorType.
     // For the Vulkan backend the layout of the created VkImage will be:
-    //      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
+    //      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL if renderable is kNo
+    //  and VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL if renderable is kYes
     GrBackendTexture createBackendTexture(int width, int height,
-                                          SkColorType, const SkColor4f& color,
-                                          GrMipMapped, GrRenderable,
+                                          SkColorType,
+                                          const SkColor4f& color,
+                                          GrMipMapped,
+                                          GrRenderable,
                                           GrProtected = GrProtected::kNo);
+
+    // If possible, create a backend texture initialized to a particular color that is
+    // compatible with the provided characterization. The client should ensure that the
+    // returned backend texture is valid.
+    // For the Vulkan backend the layout of the created VkImage will be:
+    //      VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+    GrBackendTexture createBackendTexture(const SkSurfaceCharacterization& characterization,
+                                          const SkColor4f& color);
 
     void deleteBackendTexture(GrBackendTexture);
 
