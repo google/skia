1c186ce64895e2f73a909f93059ce03cca860b03
Herb Derby <herb@google.com>
2019-07-03 14:46:05 -0400

    Update glyph cache benchmark to bulk calls
    Change-Id: I1dd0af4381b98e0c28a28a258f3366be97d19c50

--- bench/SkGlyphCacheBench.cpp
+++ bench/SkGlyphCacheBench.cpp
@@ -25,14 +25,15 @@ static void do_font_stuff(SkFont* font) {
                 *font,  defaultPaint, SkSurfaceProps(0, kUnknown_SkPixelGeometry),
                 SkScalerContextFlags::kNone, SkMatrix::I());
         auto cache = strikeSpec.findOrCreateExclusiveStrike();
-        uint16_t glyphs['z'];
+        SkPackedGlyphID glyphs['z'];
         for (int c = ' '; c < 'z'; c++) {
-            glyphs[c] = font->unicharToGlyph(c);
+            glyphs[c] = SkPackedGlyphID{font->unicharToGlyph(c)};
         }
+        constexpr size_t glyphCount = 'z' - ' ';
+        SkSpan<const SkPackedGlyphID> glyphIDs{&glyphs[SkTo<int>(' ')], glyphCount};
+        const SkGlyph* result[glyphCount];
         for (int lookups = 0; lookups < 10; lookups++) {
-            for (int c = ' '; c < 'z'; c++) {
-                cache->prepareImage(cache->glyph(glyphs[c]));
-            }
+            cache->prepareImages(glyphIDs, result);
         }
     }
 }
