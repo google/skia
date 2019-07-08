431e0d3f97a1ea66ca81c2a7b9ed690d34006d7e
Leon Scroggins III <scroggo@google.com>
2019-07-08 14:03:43 -0400

    WIP: check for uninitialized memory during jpeg encode
    Bug: chromium:951893
    Change-Id: I6392a414795da9b0471e8cd6b373a7fff8f0a1b1

--- src/images/SkJpegEncoder.cpp
+++ src/images/SkJpegEncoder.cpp
@@ -14,6 +14,7 @@
 #include "include/private/SkColorData.h"
 #include "include/private/SkImageInfoPriv.h"
 #include "include/private/SkTemplates.h"
+#include "src/core/SkMSAN.h"
 #include "src/images/SkImageEncoderFns.h"
 #include "src/images/SkJPEGWriteUtility.h"
 
@@ -214,15 +215,26 @@ bool SkJpegEncoder::onEncodeRows(int numRows) {
         return false;
     }
 
+    const size_t srcBytes = SkColorTypeBytesPerPixel(fSrc.colorType()) * fSrc.width();
+    const size_t jpegSrcBytes = fEncoderMgr->cinfo()->input_components * fSrc.width();
+
     const void* srcRow = fSrc.addr(0, fCurrRow);
     for (int i = 0; i < numRows; i++) {
         JSAMPLE* jpegSrcRow = (JSAMPLE*) srcRow;
         if (fEncoderMgr->proc()) {
+            sk_msan_assert_initialized(srcRow, SkTAddOffset<const void>(srcRow, srcBytes));
             fEncoderMgr->proc()((char*)fStorage.get(),
                                 (const char*)srcRow,
                                 fSrc.width(),
                                 fEncoderMgr->cinfo()->input_components);
             jpegSrcRow = fStorage.get();
+            sk_msan_assert_initialized(jpegSrcRow,
+                                       SkTAddOffset<const void>(jpegSrcRow, jpegSrcBytes));
+        } else {
+            // Same as above, but this repetition allows determining whether a
+            // proc was used when msan asserts.
+            sk_msan_assert_initialized(jpegSrcRow,
+                                       SkTAddOffset<const void>(jpegSrcRow, jpegSrcBytes));
         }
 
         jpeg_write_scanlines(fEncoderMgr->cinfo(), &jpegSrcRow, 1);
