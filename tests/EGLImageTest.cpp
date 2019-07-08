7c8af174dd44eeab337c2dc03bb89a6f5cb8db10
Robert Phillips <robertphillips@google.com>
2019-07-08 15:55:24 -0400

    Remove some uses of isTestingOnlyBackendTexture
    These uses seem redundant. The remaining uses are in the test "ResourceCacheWrappedResources" where the call is used to ensure borrowed resources aren't deleted.
    
    Change-Id: I2323a3496330b53e13b84e8b7c20037b841224a9
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/225732
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/EGLImageTest.cpp
+++ tests/EGLImageTest.cpp
@@ -84,14 +84,13 @@ DEF_GPUTEST_FOR_GL_RENDERING_CONTEXTS(EGLImageTest, reporter, ctxInfo) {
 
     // Use GL Context 1 to create a texture unknown to GrContext.
     context1->flush();
-    GrGpu* gpu1 = context1->priv().getGpu();
     static const int kSize = 100;
     backendTexture1 =
         context1->createBackendTexture(kSize, kSize, kRGBA_8888_SkColorType,
                                        SkColors::kTransparent,
                                        GrMipMapped::kNo, GrRenderable::kNo, GrProtected::kNo);
 
-    if (!backendTexture1.isValid() || !gpu1->isTestingOnlyBackendTexture(backendTexture1)) {
+    if (!backendTexture1.isValid()) {
         ERRORF(reporter, "Error creating texture for EGL Image");
         cleanup(glCtx0, externalTexture.fID, glCtx1.get(), context1, &backendTexture1, image);
         return;
