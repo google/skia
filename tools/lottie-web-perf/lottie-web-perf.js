bf9e28b503597910aa7e124a6915c81e26b7a14d
Ravi Mistry <rmistry@google.com>
2019-07-08 18:29:16 +0000

    [lottie-web-perf] Use SVG renderer and turn on chrome tracing
    Parse lottie file to calculate number of frames instead of doing it in the HTML (which would interfere with tracing numbers).
    
    Bug: skia:9237
    Change-Id: Ie965591206b888919fd2c104f329d703bd099c11

--- tools/lottie-web-perf/lottie-web-perf.js
+++ tools/lottie-web-perf/lottie-web-perf.js
@@ -79,8 +79,14 @@ let lottieJS = fs.readFileSync(options.lottie_player, 'utf8');
 let driverHTML = fs.readFileSync('lottie-web-perf.html', 'utf8');
 let lottieJSON = fs.readFileSync(options.input, 'utf8');
 
+// Find number of frames from the lottie JSON.
+let lottieJSONContent = JSON.parse(lottieJSON);
+const totalFrames = lottieJSONContent.op - lottieJSONContent.ip;
+console.log('Total frames: ' + totalFrames);
+
 const app = express();
 app.get('/', (req, res) => res.send(driverHTML));
+app.get('/total_frames', (req, res) => res.send(totalFrames));
 app.get('/res/lottie.js', (req, res) => res.send(lottieJS));
 app.get('/res/lottie.json', (req, res) => res.send(lottieJSON));
 app.listen(options.port, () => console.log('- Local web server started.'))
@@ -91,7 +97,7 @@ async function wait(ms) {
     return ms;
 }
 
-const targetURL = "http://localhost:" + options.port + "/";
+const targetURL = "http://localhost:" + options.port + "/#" + totalFrames;
 
 // Drive chrome to load the web page from the server we have running.
 async function driveBrowser() {
@@ -109,32 +115,31 @@ async function driveBrowser() {
 
   console.log("Loading " + targetURL);
   try {
+    // Start trace.
+    await page.tracing.start({
+      path: options.output,
+      screenshots: false,
+      categories: ["blink", "cc"]
+    });
+
     await page.goto(targetURL, {
       timeout: 20000,
       waitUntil: 'networkidle0'
     });
+
     console.log('- Waiting 20s for run to be done.');
     await page.waitForFunction('window._lottieWebDone === true', {
       timeout: 20000,
     });
+
+    // Stop trace.
+    await page.tracing.stop();
   } catch(e) {
     console.log('Timed out while loading or drawing. Either the JSON file was ' +
                 'too big or hit a bug in the player.', e);
     await browser.close();
     process.exit(0);
   }
-
-  // Write results.
-  var extractResults = function() {
-    return {
-      'frame_avg_us': window._avgFrameTimeUs,
-      'frame_max_us': window._maxFrameTimeUs,
-      'frame_min_us': window._minFrameTimeUs,
-    };
-  }
-  var data = await page.evaluate(extractResults);
-  console.log(data)
-  fs.writeFileSync(options.output, JSON.stringify(data), 'utf-8');
 
   await browser.close();
   // Need to call exit() because the web server is still running.
