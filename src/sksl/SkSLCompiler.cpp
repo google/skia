b6b966aa43a917f575394cf696f1addc2335678e
Brian Osman <brianosman@google.com>
2019-07-08 20:46:51 +0000

    Revert "Make the SkSL interpreter opt-in"
    This reverts commit 5cfbee286ddbd48a82b756c4ceb1d3f3d7e5c120.
    
    Reason for revert: Breaking DM in Google3
    
    Original change's description:
    > Make the SkSL interpreter opt-in
    > 
    > Bug: skia:9230
    > 
    > Change-Id: I50b707a7dd372882a983a4570810dae05c2a3638
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/225723
    > Reviewed-by: Mike Klein <mtklein@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    
    TBR=mtklein@google.com,brianosman@google.com
    
    Change-Id: I5ba231ab75dd44cc1d22d497f9d7e4b732e3aa14
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:9230
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/225938
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -1502,7 +1502,6 @@ bool Compiler::toPipelineStage(const Program& program, String* out,
 #endif
 
 std::unique_ptr<ByteCode> Compiler::toByteCode(Program& program) {
-#if defined(SK_ENABLE_SKSL_INTERPRETER)
     if (!this->optimize(program)) {
         return nullptr;
     }
@@ -1511,9 +1510,6 @@ std::unique_ptr<ByteCode> Compiler::toByteCode(Program& program) {
     if (cg.generateCode()) {
         return result;
     }
-#else
-    ABORT("ByteCode interpreter not enabled");
-#endif
     return nullptr;
 }
 
