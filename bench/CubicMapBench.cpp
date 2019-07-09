21ba586f32ebfd91941add15c7bf8366ead7e169
Mike Reed <reed@google.com>
2019-07-09 10:13:39 -0400

    create outer forloop to support loops=-1
    Change-Id: I1f76a798f3f95afb82ab739ab5343bf52b233c7d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/226079
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Auto-Submit: Mike Reed <reed@google.com>

--- bench/CubicMapBench.cpp
+++ bench/CubicMapBench.cpp
@@ -23,9 +23,11 @@ public:
     }
 
     void onDraw(int loops, SkCanvas*) override {
-        for (int i = 0; i < loops * 100; ++i) {
-            for (SkScalar x = 0; x <= 1; x += 1.0f / 512) {
-                fCMap.computeYFromX(x);
+        for (int outer = 0; outer < 100; ++outer) {
+            for (int i = 0; i < loops; ++i) {
+                for (SkScalar x = 0; x <= 1; x += 1.0f / 512) {
+                    fCMap.computeYFromX(x);
+                }
             }
         }
     }
