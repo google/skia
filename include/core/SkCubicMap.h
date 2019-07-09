bf098438fd98f3fc8ad3c759a73a2b39ff2edf78
Herb Derby <herb@google.com>
2019-07-09 19:04:30 -0400

    CubicMap
    Change-Id: Id3b1c3bb6c7dc14be409ff0c433990784d6e010e

--- include/core/SkCubicMap.h
+++ include/core/SkCubicMap.h
@@ -9,6 +9,7 @@
 #define SkCubicMap_DEFINED
 
 #include "include/core/SkPoint.h"
+#include "include/private/SkNx.h"
 
 /**
  *  Fast evaluation of a cubic ease-in / ease-out curve. This is defined as a parametric cubic
@@ -40,6 +41,82 @@ private:
     SkPoint fCoeff[3];
     Type    fType;
 };
+
+//#define SK_CUBICMAP_TRACK_MAX_ERROR
+
+class SK_API SkCubicMapExperiment {
+public:
+    SkCubicMapExperiment(SkPoint p1, SkPoint p2);
+    float computeYFromX(float x) const;
+
+private:
+#ifdef SK_CUBICMAP_TRACK_MAX_ERROR
+    struct StatsError {
+        void sample(SkScalar a, SkScalar b);
+        ~StatsError();
+
+        SkScalar sum0 = 0;
+        SkScalar sum1 = 0;
+        SkScalar maxError = 0;
+    };
+#else
+    struct StatsError {
+        void sample(SkScalar a, SkScalar b) {}
+        ~StatsError() = default;
+    };
+#endif
+
+    static float EvalPoly(float t, float b) {
+        return b;
+    }
+
+    template <typename... Rest>
+    static float EvalPoly(float t, float m, float b, Rest... rest) {
+        return EvalPoly(t, sk_fmaf(m, t, b), rest...);
+    }
+
+    static SkNx<4,float> EvalPoly(SkNx<4, float> t, SkNx<4, float> b) {
+        return b;
+    }
+
+    template <typename... Rest>
+    static SkNx<4,float> EvalPoly(SkNx<4,float> t, SkNx<4,float> m, SkNx<4,float> b, Rest... rest) {
+        return EvalPoly(t, SkNx_fma(m, t, b), rest...);
+    }
+
+    SkPoint fA, fB, fC;
+    //SkScalar fDA, fDB, fDC;
+
+    SkScalar fT[2], fU[2], fV[2], fW[2], fX[2];
+    SkScalar fSwitchPoint;
+    SkNx<4, float> fPA, fPB, fPC;
+    mutable SkNx<4, float> fPD;
+    //SkNx<4, float> A, B, C;
+    mutable StatsError stats;
+};
+
+inline SkScalar SkCubicMapExperiment::computeYFromX(SkScalar x) const {
+    static const SkScalar epsilon01 = 0.000001f;
+    if (x <= epsilon01) {return x;}
+    if (x >= (1 - epsilon01)) {return x;}
+
+    int i = (x < fSwitchPoint) ? 0 : 1;
+
+    // T + U * sqrt(V - W(X-x))
+    SkScalar t = fT[i] + fU[i] * std::sqrtf(fV[i] - fW[i] * (fX[i] - x));
+    SkASSERT(0 <= t && t <= 1);
+
+    SkNx<4, float> D = {-x, fPD[1], 0, 0};
+
+    SkNx<4, float> XdX = EvalPoly(t, fPA, fPB, fPC, D);
+    SkNx<4, float> inv = XdX.invert();
+
+    t = t - XdX[0] * inv[1];
+
+    stats.sample(x, EvalPoly(t, fA.fX, fB.fX, fC.fX, 0));
+
+    return EvalPoly(t, fA.fY, fB.fY, fC.fY, 0);
+}
 
 #endif
 
--- src/core/SkCubicMap.cpp
+++ src/core/SkCubicMap.cpp
@@ -107,3 +107,76 @@ SkPoint SkCubicMap::computeFromT(float t) const {
     (((a * t + b) * t + c) * t).store(&result);
     return result;
 }
+
+SkCubicMapExperiment::SkCubicMapExperiment(SkPoint P1, SkPoint P2) {
+
+    // 3 * p1 * t + (-6 * p1 + 3 * p2) * t^2 + (1 + 3 * p1 - 3 * p2) * t^3
+    SkPoint P1x3 = P1 * 3,
+            P2x3 = P2 * 3;
+
+    // A, B, C for A * t^3 + B * t^2 + C * t
+    fA = P1x3 - P2x3 + SkPoint{1, 1};
+    fB = P1x3 * -2 + P2x3;
+    fC = P1x3;
+
+    // Derivative of x only.
+    SkScalar DA = 3 * fA.fX,
+             DB = 2 * fB.fX,
+             DC = 1 * fC.fX;
+
+    // The cubic will be sampled at three equally space places (remember the end are 0 and 1), so
+    // there will be 5 points to generate 3 quadratics. The sample arguments .25, .50, .75
+    // result in the values v25, v50, v75.
+
+
+    SkScalar v25 = EvalPoly(0.25f, fA.fX, fB.fX, fC.fX, 0),
+             v50 = EvalPoly(0.50f, fA.fX, fB.fX, fC.fX, 0),
+             v75 = EvalPoly(0.75f, fA.fX, fB.fX, fC.fX, 0);
+
+    // The switch from one quadratic to the next happens at the point v50.
+    fSwitchPoint = v50;
+
+    // (-b + sqrt(b^2 - 4a(c - x))) / 2a
+    // -b/(2a) + 1/(2a) * sqrt(b^2 - 4a(c-x))
+    // T + U * sqrt(V - W(X-x))
+    {
+        // a = 8*(h-2*q), b = -2*(h-4*q), c = 0
+        float a = 8 * (v50 - 2 * v25);
+        float b = -2 * (v50 - 4 * v25);
+        float c = 0;
+        fT[0] = -b / (2 * a);
+        fU[0] = 1 / (2 * a);
+        fV[0] = b * b;
+        fW[0] = 4 * a;
+        fX[0] = c;
+    }
+    {
+        // a = 8*(1 + h - 2*qqq), b = -2*(5 + 7*h - 12*qqq), c = 3 + 6*h - 8*qqq
+        float a = 8 * (1 + v50 - 2 * v75);
+        float b = -2 * (5 + 7 * v50 - 12 * v75);
+        float c = 3 + 6 * v50 - 8 * v75;
+        fT[1] = -b / (2 * a);
+        fU[1] = 1 / (2 * a);
+        fV[1] = b * b;
+        fW[1] = 4 * a;
+        fX[1] = c;
+    }
+
+    fPA = SkNx<4, float>{fA.fX,  0, 0, 0};//.store(fPA);
+    fPB = SkNx<4, float>{fB.fX, DA, 0, 0};//.store(fPB);
+    fPC = SkNx<4, float>{fC.fX, DB, 0, 0};//.store(fPC);
+    fPD = SkNx<4, float>{    0, DC, 0, 0};//.store(fPD);
+}
+
+#ifdef SK_CUBICMAP_TRACK_MAX_ERROR
+void SkCubicMapExperiment::StatsError::sample(SkScalar a, SkScalar b) {
+    SkScalar e = std::abs(a - b);
+    sum0 += 1;
+    sum1 += e;
+    maxError = std::max(maxError, e);
+}
+
+SkCubicMapExperiment::StatsError::~StatsError() {
+    SkDebugf("maxError %g avg %g\n", maxError, sum1/sum0);
+}
+#endif
