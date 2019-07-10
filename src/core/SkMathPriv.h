fd6600e086cb744175b9c4a729deded44df9cce0
Robert Phillips <robertphillips@google.com>
2019-07-10 12:54:36 -0400

    Alter approximate scratch texture binning after 1024
    As the powers-of-2 get larger the coarse binning can burn a lot of VRAM.
    
    Granted it isn't the best metric but, with this CL, the number of textures created and scratch textures reused remains unchanged when running the GMs.
    
    Change-Id: I84abbbae0ed01aabb387671b5ee0e4fcdb82b671

--- src/core/SkMathPriv.h
+++ src/core/SkMathPriv.h
@@ -215,7 +215,7 @@ static inline int SkPrevLog2(uint32_t value) {
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- *  Return the next power of 2 >= n.
+ *  Return the smallest power-of-2 >= n.
  */
 static inline uint32_t GrNextPow2(uint32_t n) {
     return n ? (1 << (32 - SkCLZ(n - 1))) : 1;
