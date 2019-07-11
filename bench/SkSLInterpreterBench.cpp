e729e75500aff643f281e2bebad1622a5169a50b
Brian Osman <brianosman@google.com>
2019-07-11 16:05:23 -0400

    Interpreter: Disallow return statements inside loops or conditionals
    These didn't work correctly, and they're extremely tricky to get right
    in the vectorized execution model (vs. structured control flow). As a
    side effect, determine the maximum stack depth used for the execution
    masking - the same idea will be used for the primary stack in a later
    CL. Add a unit test to verify the new restriction, and fix two places
    that were relying on this feature before.
    
    In addition, boolean external values need to be masks. I may implement
    this in the code-gen at some point, but this is already a fringe
    feature, so just fix the one unit test for now.
    
    Change-Id: I9607ffaf67c7795dbf42e4009180aea8f3e65c44

--- bench/SkSLInterpreterBench.cpp
+++ bench/SkSLInterpreterBench.cpp
@@ -89,10 +89,10 @@ const char* kHighContrastFilterSrc = R"(
     half hue2rgb_Stage2(half p, half q, half t) {
         if (t < 0)  t += 1;
         if (t > 1)  t -= 1;
-        if (t < 1 / 6.)  return p + (q - p) * 6 * t;
-        if (t < 1 / 2.)  return q;
-        if (t < 2 / 3.)  return p + (q - p) * (2 / 3. - t) * 6;
-        return p;
+        return (t < 1 / 6.) ? p + (q - p) * 6 * t
+             : (t < 1 / 2.) ? q
+             : (t < 2 / 3.) ? p + (q - p) * (2 / 3. - t) * 6
+             : p;
     }
     half max(half a, half b) { return a > b ? a : b; }
     half min(half a, half b) { return a < b ? a : b; }
