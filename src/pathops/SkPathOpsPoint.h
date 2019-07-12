b54e9336c6b50efbde72e83083070cecc027fa9b
Mike Reed <reed@google.com>
2019-07-12 10:53:54 -0400

    Revert "Revert "reimpl parts of arctTo using doubles to handle shallow angles""
    This reverts commit bc525d65a1f855d1badee8d95d84fa00c2b34372.
    
    Change-Id: I92550317784be4084412d855cf475b77cd36d6da

--- src/pathops/SkPathOpsPoint.h
+++ src/pathops/SkPathOpsPoint.h
@@ -18,9 +18,10 @@ struct SkDVector {
     double fX;
     double fY;
 
-    void set(const SkVector& pt) {
+    SkDVector& set(const SkVector& pt) {
         fX = pt.fX;
         fY = pt.fY;
+        return *this;
     }
 
     // only used by testing
@@ -84,10 +85,15 @@ struct SkDVector {
         return fX * fX + fY * fY;
     }
 
-    void normalize() {
+    SkDVector& normalize() {
         double inverseLength = 1 / this->length();
         fX *= inverseLength;
         fY *= inverseLength;
+        return *this;
+    }
+
+    bool isFinite() const {
+        return std::isfinite(fX) && std::isfinite(fY);
     }
 };
 
