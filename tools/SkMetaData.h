88078d4543fb8d6267a00cded3c11358cc371efd
Hal Canary <halcanary@google.com>
2019-07-15 14:30:17 -0400

    SkMetaData: remove unused features, use unique_ptr
    Change-Id: Iecd80244b3803b6d5916bfd793741e67f05d0d46

--- tools/SkMetaData.h
+++ tools/SkMetaData.h
@@ -1,143 +1,91 @@
-
 /*
  * Copyright 2006 The Android Open Source Project
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-
-
 #ifndef SkMetaData_DEFINED
 #define SkMetaData_DEFINED
 
 #include "include/core/SkScalar.h"
 
-class SkRefCnt;
+#include <memory>
+#include <cstring>
 
 class SK_API SkMetaData {
 public:
-    /**
-     *  Used to manage the life-cycle of a ptr in the metadata. This is option
-     *  in setPtr, and is only invoked when either copying one metadata to
-     *  another, or when the metadata is destroyed.
-     *
-     *  setPtr(name, ptr, proc) {
-     *      fPtr = proc(ptr, true);
-     *  }
-     *
-     *  copy: A = B {
-     *      A.fPtr = B.fProc(B.fPtr, true);
-     *  }
-     *
-     *  ~SkMetaData {
-     *      fProc(fPtr, false);
-     *  }
-     */
-    typedef void* (*PtrProc)(void* ptr, bool doRef);
-
-    /**
-     *  Implements PtrProc for SkRefCnt pointers
-     */
-    static void* RefCntProc(void* ptr, bool doRef);
-
-    SkMetaData();
-    SkMetaData(const SkMetaData& src);
+    SkMetaData() = default;
     ~SkMetaData();
 
-    SkMetaData& operator=(const SkMetaData& src);
-
-    void reset();
-
-    bool findS32(const char name[], int32_t* value = nullptr) const;
-    bool findScalar(const char name[], SkScalar* value = nullptr) const;
-    const SkScalar* findScalars(const char name[], int* count,
-                                SkScalar values[] = nullptr) const;
-    const char* findString(const char name[]) const;
-    bool findPtr(const char name[], void** value = nullptr, PtrProc* = nullptr) const;
-    bool findBool(const char name[], bool* value = nullptr) const;
-    const void* findData(const char name[], size_t* byteCount = nullptr) const;
-
-    bool hasS32(const char name[], int32_t value) const {
-        int32_t v;
-        return this->findS32(name, &v) && v == value;
-    }
-    bool hasScalar(const char name[], SkScalar value) const {
-        SkScalar v;
-        return this->findScalar(name, &v) && v == value;
-    }
-    bool hasString(const char name[], const char value[]) const {
-        const char* v = this->findString(name);
-        return  (v == nullptr && value == nullptr) ||
-                (v != nullptr && value != nullptr && !strcmp(v, value));
-    }
-    bool hasPtr(const char name[], void* value) const {
-        void* v;
-        return this->findPtr(name, &v) && v == value;
-    }
-    bool hasBool(const char name[], bool value) const {
-        bool    v;
-        return this->findBool(name, &v) && v == value;
-    }
-    bool hasData(const char name[], const void* data, size_t byteCount) const {
-        size_t len;
-        const void* ptr = this->findData(name, &len);
-        return ptr && len == byteCount && !memcmp(ptr, data, len);
+    template <typename T>
+    bool find(const char name[], T* value = nullptr) const {
+        int count = 0;
+        const T* ptr = (const T*)this->find(name, GetType<T>(), &count);
+        if (ptr && count == 1) {
+            if (value) { *value = *ptr; }
+            return true;
+        }
+        return false;
     }
 
-    void setS32(const char name[], int32_t value);
-    void setScalar(const char name[], SkScalar value);
-    SkScalar* setScalars(const char name[], int count, const SkScalar values[] = nullptr);
-    void setString(const char name[], const char value[]);
-    void setPtr(const char name[], void* value, PtrProc proc = nullptr);
-    void setBool(const char name[], bool value);
-    // the data is copied from the input pointer.
-    void setData(const char name[], const void* data, size_t byteCount);
+    template <typename T>
+    const T* finds(const char name[], int* count, T* values = nullptr) const {
+        int n = 0;
+        if (const T* ptr = (const T*)this->find(name, GetType<T>(), &n)) {
+            if (count) { *count = n; }
+            if (values) { std::memcpy(values, ptr, sizeof(T) * n); }
+            return ptr;
+        }
+        return nullptr;
+    }
 
-    bool removeS32(const char name[]);
-    bool removeScalar(const char name[]);
-    bool removeString(const char name[]);
-    bool removePtr(const char name[]);
-    bool removeBool(const char name[]);
-    bool removeData(const char name[]);
+    template <typename T>
+    bool has(const char name[], T value) const {
+        T v{};
+        return this->find<T>(name, &v) && v == value;
+    }
 
-    // helpers for SkRefCnt
-    bool findRefCnt(const char name[], SkRefCnt** ptr = nullptr) {
-        return this->findPtr(name, reinterpret_cast<void**>(ptr));
+    template <typename T>
+    void set(const char name[], T val) { (void)this->set(name, &val, sizeof(T), GetType<T>(), 1); }
+
+    template <typename T>
+    T* sets(const char name[], int count, const T values[]) {
+        return (T*)this->set(name, values, sizeof(T), GetType<T>(), 1);
     }
-    bool hasRefCnt(const char name[], SkRefCnt* ptr) {
-        return this->hasPtr(name, ptr);
+
+
+    bool findS32(const char name[], int32_t* v = nullptr) const { return this->find(name, v); }
+    bool findScalar(const char name[], SkScalar* v = nullptr) const { return this->find(name, v); }
+    const SkScalar* findScalars(const char name[], int* count, SkScalar vs[] = nullptr) const {
+        return this->finds(name, count, vs);
     }
-    void setRefCnt(const char name[], SkRefCnt* ptr) {
-        this->setPtr(name, ptr, RefCntProc);
+    bool findPtr(const char name[], void** v = nullptr) const { return this->find(name, v); }
+    bool findBool(const char name[], bool* v = nullptr) const { return this->find(name, v); }
+    bool hasS32(const char name[], int32_t v) const { return this->has(name, v); }
+    bool hasScalar(const char name[], SkScalar v) const { return this->has(name, v); }
+    bool hasPtr(const char name[], void* v) const { return this->has(name, v); }
+    bool hasBool(const char name[], bool v) const { return this->has(name, v); }
+    void setS32(const char name[], int32_t v) { this->set(name, v); }
+    void setScalar(const char name[], SkScalar v) { this->set(name, v); }
+    SkScalar* setScalars(const char name[], int count, const SkScalar vs[]) {
+        return this->sets(name, count, vs);
     }
-    bool removeRefCnt(const char name[]) {
-        return this->removePtr(name);
-    }
+    void setPtr(const char name[], void* ptr) { this->set(name, ptr); }
+    void setBool(const char name[], bool v) { this->set(name, v); }
 
     enum Type {
         kS32_Type,
         kScalar_Type,
-        kString_Type,
         kPtr_Type,
         kBool_Type,
-        kData_Type,
 
         kTypeCount
     };
 
     struct Rec;
-    class Iter;
-    friend class Iter;
-
     class Iter {
     public:
-        Iter() : fRec(nullptr) {}
         Iter(const SkMetaData&);
-
-        /** Reset the iterator, so that calling next() will return the first
-            data element. This is done implicitly in the constructor.
-        */
-        void reset(const SkMetaData&);
 
         /** Each time next is called, it returns the name of the next data element,
             or null when there are no more elements. If non-null is returned, then the
@@ -147,12 +95,13 @@ public:
         const char* next(Type*, int* count);
 
     private:
-        Rec* fRec;
+        Rec* fRec = nullptr;
     };
 
 public:
+    struct D { void operator()(Rec*); };
     struct Rec {
-        Rec*        fNext;
+        std::unique_ptr<Rec, D> fNext;
         uint16_t    fDataCount; // number of elements
         uint8_t     fDataLen;   // sizeof a single element
         uint8_t     fType;
@@ -161,15 +110,23 @@ public:
         void*       data() { return (this + 1); }
         const char* name() const { return (const char*)this->data() + fDataLen * fDataCount; }
         char*       name() { return (char*)this->data() + fDataLen * fDataCount; }
-
-        static Rec* Alloc(size_t);
-        static void Free(Rec*);
     };
-    Rec*    fRec;
+    std::unique_ptr<Rec, D> fRec;
 
-    const Rec* find(const char name[], Type) const;
+    friend class Iter;
+    template <typename T> static Type GetType();
+
+    const void* find(const char name[], Type type, int* count) const;
     void* set(const char name[], const void* data, size_t len, Type, int count);
     bool remove(const char name[], Type);
+
+
+    SkMetaData(const SkMetaData&) = delete;
+    SkMetaData& operator=(const SkMetaData&) = delete;
 };
 
+template <> inline SkMetaData::Type SkMetaData::GetType<int32_t>()  { return SkMetaData::kS32_Type;    }
+template <> inline SkMetaData::Type SkMetaData::GetType<SkScalar>() { return SkMetaData::kScalar_Type; }
+template <> inline SkMetaData::Type SkMetaData::GetType<void*>()    { return SkMetaData::kPtr_Type;    }
+template <> inline SkMetaData::Type SkMetaData::GetType<bool>()     { return SkMetaData::kBool_Type;   }
 #endif
