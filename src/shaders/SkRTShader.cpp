82d65a09ec2f42b87d3d71d5e600672d990f7d9a
Mike Reed <reed@google.com>
2019-07-15 15:47:00 -0400

    use same signature for shader/colorfilters in sksl
    'void main(int x, int y, inout half4 color)'
    
    For colorfilters, x,y are 0
    For shaders, color is paint-color (or previous effect's color)
    
    Change-Id: I21c2f570702f117d33646bd8fba0ab4def20a118

--- src/shaders/SkRTShader.cpp
+++ src/shaders/SkRTShader.cpp
@@ -12,12 +12,6 @@
 #include "src/core/SkReadBuffer.h"
 #include "src/core/SkWriteBuffer.h"
 
-#if SK_SUPPORT_GPU
-#include "src/gpu/GrFragmentProcessor.h"
-#include "src/gpu/effects/generated/GrMixerEffect.h"
-#include "src/gpu/effects/GrSkSLFP.h"
-#endif
-
 #include "src/sksl/SkSLByteCode.h"
 #include "src/sksl/SkSLCompiler.h"
 
@@ -114,3 +108,100 @@ sk_sp<SkShader> SkRuntimeShaderMaker(SkString sksl, sk_sp<SkData> inputs,
     return sk_sp<SkShader>(new SkRTShader(std::move(sksl), std::move(inputs),
                                           localMatrix, isOpaque));
 }
+
+#if SK_SUPPORT_GPU
+
+#include "include/private/GrRecordingContext.h"
+#include "src/gpu/GrCaps.h"
+#include "src/gpu/GrColorSpaceInfo.h"
+#include "src/gpu/GrRecordingContextPriv.h"
+#include "src/gpu/SkGr.h"
+
+#include "src/gpu/GrFragmentProcessor.h"
+#include "src/gpu/effects/generated/GrMixerEffect.h"
+#include "src/gpu/effects/GrSkSLFP.h"
+
+std::unique_ptr<GrFragmentProcessor> SkRTShader::asFragmentProcessor(const GrFPArgs& args) const {
+
+    static int rtIndex = GrSkSLFP::NewIndex();
+    return GrSkSLFP::Make(args.fContext, rtIndex, "runtime-shader", fSkSL, nullptr, 0);
+
+#if 0
+    const auto lm = this->totalLocalMatrix(args.fPreLocalMatrix, args.fPostLocalMatrix);
+    SkMatrix lmInverse;
+    if (!lm->invert(&lmInverse)) {
+        return nullptr;
+    }
+
+    GrSamplerState::WrapMode wrapModes[] = {tile_mode_to_wrap_mode(fTileModeX),
+        tile_mode_to_wrap_mode(fTileModeY)};
+
+    // If either domainX or domainY are un-ignored, a texture domain effect has to be used to
+    // implement the decal mode (while leaving non-decal axes alone). The wrap mode originally
+    // clamp-to-border is reset to clamp since the hw cannot implement it directly.
+    GrTextureDomain::Mode domainX = GrTextureDomain::kIgnore_Mode;
+    GrTextureDomain::Mode domainY = GrTextureDomain::kIgnore_Mode;
+    if (!args.fContext->priv().caps()->clampToBorderSupport()) {
+        if (wrapModes[0] == GrSamplerState::WrapMode::kClampToBorder) {
+            domainX = GrTextureDomain::kDecal_Mode;
+            wrapModes[0] = GrSamplerState::WrapMode::kClamp;
+        }
+        if (wrapModes[1] == GrSamplerState::WrapMode::kClampToBorder) {
+            domainY = GrTextureDomain::kDecal_Mode;
+            wrapModes[1] = GrSamplerState::WrapMode::kClamp;
+        }
+    }
+
+    // Must set wrap and filter on the sampler before requesting a texture. In two places below
+    // we check the matrix scale factors to determine how to interpret the filter quality setting.
+    // This completely ignores the complexity of the drawVertices case where explicit local coords
+    // are provided by the caller.
+    bool doBicubic;
+    GrSamplerState::Filter textureFilterMode = GrSkFilterQualityToGrFilterMode(
+                                                                               args.fFilterQuality, *args.fViewMatrix, *lm,
+                                                                               args.fContext->priv().options().fSharpenMipmappedTextures, &doBicubic);
+    GrSamplerState samplerState(wrapModes, textureFilterMode);
+    SkScalar scaleAdjust[2] = { 1.0f, 1.0f };
+    sk_sp<GrTextureProxy> proxy(as_IB(fImage)->asTextureProxyRef(args.fContext, samplerState,
+                                                                 scaleAdjust));
+    if (!proxy) {
+        return nullptr;
+    }
+
+    GrPixelConfig config = proxy->config();
+    bool isAlphaOnly = GrPixelConfigIsAlphaOnly(config);
+
+    lmInverse.postScale(scaleAdjust[0], scaleAdjust[1]);
+
+    std::unique_ptr<GrFragmentProcessor> inner;
+    if (doBicubic) {
+        // domainX and domainY will properly apply the decal effect with the texture domain used in
+        // the bicubic filter if clamp to border was unsupported in hardware
+        static constexpr auto kDir = GrBicubicEffect::Direction::kXY;
+        inner = GrBicubicEffect::Make(std::move(proxy), lmInverse, wrapModes, domainX, domainY,
+                                      kDir, fImage->alphaType());
+    } else {
+        if (domainX != GrTextureDomain::kIgnore_Mode || domainY != GrTextureDomain::kIgnore_Mode) {
+            SkRect domain = GrTextureDomain::MakeTexelDomain(
+                                                             SkIRect::MakeWH(proxy->width(), proxy->height()),
+                                                             domainX, domainY);
+            inner = GrTextureDomainEffect::Make(std::move(proxy), lmInverse, domain,
+                                                domainX, domainY, samplerState);
+        } else {
+            inner = GrSimpleTextureEffect::Make(std::move(proxy), lmInverse, samplerState);
+        }
+    }
+    inner = GrColorSpaceXformEffect::Make(std::move(inner), fImage->colorSpace(),
+                                          fImage->alphaType(),
+                                          args.fDstColorSpaceInfo->colorSpace());
+    if (isAlphaOnly) {
+        return inner;
+    } else if (args.fInputColorIsOpaque) {
+        return GrFragmentProcessor::OverrideInput(std::move(inner), SK_PMColor4fWHITE, false);
+    }
+    return GrFragmentProcessor::MulChildByInputAlpha(std::move(inner));
+#endif
+}
+
+#endif
+
