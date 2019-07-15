f5a8c6a360e9027dbf70172a94ac50e8772ae322
Brian Salomon <bsalomon@google.com>
2019-07-15 17:05:05 -0400

    Remove GrGLGpu/GrGLCaps readPixelsSupported.
    This code checks the external format/type that will be used with
    glReadPixels. It tests for the values inherently allowed by the GL
    and for implementation values.
    
    It would need to be refactored to use formats and color types rather
    than GrPixelConfig.
    
    However, the code that calls GrGpu::readPixels and
    GrGpu::transferPixelsFrom already tests
    GrCaps::supportedReadPixelsColorType. The GrGLCaps override only allows
    format/type combinations allowed by ES without implementation queries,
    which is less permissive than the deleted code.
    
    We may get more permissive again but will probably follow a different
    pattern and do this after the format/colortype tables are built out in
    GrGLCaps.
    
    Bug: skia:6718
    Change-Id: I36d9ab496dfe71045dcbce84200a9de3e93440bf

--- src/gpu/gl/GrGLGpu.h
+++ src/gpu/gl/GrGLGpu.h
@@ -229,20 +229,6 @@ private:
                                      GrGLTextureParameters::SamplerOverriddenState* initialState,
                                      const void* data);
 
-    // Checks whether glReadPixels can be called to get pixel values in readConfig from the
-    // render target.
-    bool readPixelsSupported(GrRenderTarget* target, GrPixelConfig readConfig);
-
-    // Checks whether glReadPixels can be called to get pixel values in readConfig from a
-    // render target that has renderTargetConfig. This may have to create a temporary
-    // render target and thus is less preferable than the variant that takes a render target.
-    bool readPixelsSupported(GrPixelConfig renderTargetConfig, GrPixelConfig readConfig);
-
-    // Checks whether glReadPixels can be called to get pixel values in readConfig from a
-    // render target that has the same config as surfaceForConfig. Calls one of the the two
-    // variations above, depending on whether the surface is a render target or not.
-    bool readPixelsSupported(GrSurface* surfaceForConfig, GrPixelConfig readConfig);
-
     bool onReadPixels(GrSurface*, int left, int top, int width, int height, GrColorType,
                       void* buffer, size_t rowBytes) override;
 
