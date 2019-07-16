337eeae7a7be3b02b27e920cb985820711824199
Mike Reed <reed@google.com>
2019-07-16 10:44:27 -0400

    use same signature for shader/colorfilters in sksl
    'void main(int x, int y, inout half4 color)'
    
    For colorfilters, x,y are uninitialized
    For shaders, color is paint-color
    
    Change-Id: I21c2f570702f117d33646bd8fba0ab4def20a118

--- src/gpu/SkGr.cpp
+++ src/gpu/SkGr.cpp
@@ -47,7 +47,7 @@ GR_FP_SRC_STRING SKSL_DITHER_SRC = R"(
 // This controls the range of values added to color channels
 layout(key) in int rangeType;
 
-void main(int x, int y, inout half4 color) {
+void main(float x, float y, inout half4 color) {
     half value;
     half range;
     @switch (rangeType) {
@@ -73,7 +73,7 @@ void main(int x, int y, inout half4 color) {
     } else {
         // Simulate the integer effect used above using step/mod. For speed, simulates a 4x4
         // dither pattern rather than an 8x8 one.
-        half4 modValues = mod(half4(x, y, x, y), half4(2.0, 2.0, 4.0, 4.0));
+        half4 modValues = mod(half4(half(x), half(y), half(x), half(y)), half4(2.0, 2.0, 4.0, 4.0));
         half4 stepValues = step(modValues, half4(1.0, 1.0, 2.0, 2.0));
         value = dot(stepValues, half4(8.0 / 16.0, 4.0 / 16.0, 2.0 / 16.0, 1.0 / 16.0)) - 15.0 / 32.0;
     }
