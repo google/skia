3e9f8e91d788b9d118d777d8d164919b15b1d64f
Brian Osman <brianosman@google.com>
2019-07-16 13:34:14 -0400

    Generate identical shader keys for X and Y blurs when there is no domain
    This mirrors the logic in emitCode, and avoids extra shader compiles.
    
    Change-Id: I2c11e082dfa2b9fc673dc2b8fdf2f0b964407b90

--- src/gpu/effects/GrGaussianConvolutionFragmentProcessor.cpp
+++ src/gpu/effects/GrGaussianConvolutionFragmentProcessor.cpp
@@ -176,7 +176,9 @@ void GrGLConvolutionEffect::GenKey(const GrProcessor& processor, const GrShaderC
             processor.cast<GrGaussianConvolutionFragmentProcessor>();
     uint32_t key = conv.radius();
     key <<= 3;
-    key |= Direction::kY == conv.direction() ? 0x4 : 0x0;
+    if (conv.useBounds()) {
+        key |= Direction::kY == conv.direction() ? 0x4 : 0x0;
+    }
     key |= static_cast<uint32_t>(conv.mode());
     b->add32(key);
 }
