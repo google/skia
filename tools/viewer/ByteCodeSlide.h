bd3192c27ff02f0a53e05dbfb6e9e6758e29193f
Brian Osman <brianosman@google.com>
2019-07-16 16:30:06 -0400

    Interpreter demo slide
    Change-Id: Ib3f75c01862c444519fc67a9f9cc841aaed17ba8

---
+++ tools/viewer/ByteCodeSlide.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ByteCodeSlide_DEFINED
+#define ByteCodeSlide_DEFINED
+
+#include "tools/viewer/Slide.h"
+
+#include "src/sksl/SkSLByteCode.h"
+
+#include <map>
+#include <vector>
+
+class ByteCodeSlide : public Slide {
+public:
+    ByteCodeSlide();
+
+    SkISize getDimensions() const override { return SkISize::MakeEmpty(); }
+    void draw(SkCanvas* canvas) override;
+
+private:
+    SkString fCode;
+
+    std::unique_ptr<SkSL::ByteCode> fByteCode = nullptr;
+    const SkSL::ByteCodeFunction* fMain = nullptr;
+
+    std::vector<SkString> fLines;
+    std::map<int, int> fLookup;
+
+    int fIP = -1;
+    int fCount = 0;
+};
+
+#endif
