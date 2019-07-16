96be3fbda825f04ce456c688153db1042271957c
Florin Malita <fmalita@chromium.org>
2019-07-16 08:45:00 -0400

    Reland Use saveLayer-determined CT in SkGpuDevice::onCreateDevice
    Currently Ganesh always uses the prev render target CT, but saveLayer
    may force a different CT, depending on flags.
    
    This fixes handling of saveLayer/kF16ColorType (added in
    https://skia-review.googlesource.com/c/skia/+/227066).
    
    Change-Id: Ieb8a69e79a0df7d8e4dc82507266d57e8573931b

--- src/gpu/SkGpuDevice.cpp
+++ src/gpu/SkGpuDevice.cpp
@@ -1656,7 +1656,11 @@ SkBaseDevice* SkGpuDevice::onCreateDevice(const CreateInfo& cinfo, const SkPaint
                                                             : SkBackingFit::kExact;
 
     GrColorType colorType = fRenderTargetContext->colorSpaceInfo().colorType();
-    if (colorType == GrColorType::kRGBA_1010102) {
+    if (cinfo.fInfo.colorType() == SkColorType::kRGBA_F16_SkColorType) {
+        // The previous device was kRGBA_F16_SkColorType,
+        // or kF16ColorType was passed to saveLayer.
+        colorType = GrColorType::kRGBA_F16;
+    } else if (colorType == GrColorType::kRGBA_1010102) {
         // If the original device is 1010102, fall back to 8888 so that we have a usable alpha
         // channel in the layer.
         colorType = GrColorType::kRGBA_8888;
