c91087314370f9935528049ef2b2c641e3b914e1
Mike Reed <reed@google.com>
2019-07-18 13:20:04 -0400

    add inverse() intrinsic function to sksl
    Change-Id: Ic30f6e0345e851ea8a942996b9eaf2c894455e3e

--- src/sksl/SkSLByteCode.cpp
+++ src/sksl/SkSLByteCode.cpp
@@ -84,6 +84,9 @@ static const uint8_t* disassemble_instruction(const uint8_t* ip) {
         VECTOR_DISASSEMBLE(kDivideS, "divideS")
         VECTOR_DISASSEMBLE(kDivideU, "divideu")
         VECTOR_MATRIX_DISASSEMBLE(kDup, "dup")
+        case ByteCodeInstruction::kInverse2x2: printf("inverse2x2"); break;
+        case ByteCodeInstruction::kInverse3x3: printf("inverse3x3"); break;
+        case ByteCodeInstruction::kInverse4x4: printf("inverse4x4"); break;
         case ByteCodeInstruction::kLoad: printf("load %d", READ8()); break;
         case ByteCodeInstruction::kLoad2: printf("load2 %d", READ8()); break;
         case ByteCodeInstruction::kLoad3: printf("load3 %d", READ8()); break;
@@ -351,6 +354,8 @@ static T vec_mod(T a, T b) {
     return a - skvx::trunc(a / b) * b;
 }
 
+#define spf(index)  sp[index].fFloat
+
 static void innerRun(const ByteCode* byteCode, const ByteCodeFunction* f, VValue* stack,
                      float* outReturn[], VValue globals[], bool stripedOutput, int N,
                      int baseIndex) {
@@ -527,6 +532,89 @@ static void innerRun(const ByteCode* byteCode, const ByteCodeFunction* f, VValue
                 int count = READ8();
                 memcpy(sp + 1, sp - count + 1, count * sizeof(VValue));
                 sp += count;
+                break;
+            }
+
+            case ByteCodeInstruction::kInverse2x2: {
+                F32 a = sp[-3].fFloat,
+                    b = sp[-2].fFloat,
+                    c = sp[-1].fFloat,
+                    d = sp[ 0].fFloat;
+                F32 idet = F32(1) / (a*d - b*c);
+                sp[-3].fFloat = d * idet;
+                sp[-2].fFloat = -b * idet;
+                sp[-1].fFloat = -c * idet;
+                sp[ 0].fFloat = a * idet;
+                break;
+            }
+            case ByteCodeInstruction::kInverse3x3: {
+                F32 a11 = sp[-8].fFloat, a12 = sp[-5].fFloat, a13 = sp[-2].fFloat,
+                    a21 = sp[-7].fFloat, a22 = sp[-4].fFloat, a23 = sp[-1].fFloat,
+                    a31 = sp[-6].fFloat, a32 = sp[-3].fFloat, a33 = sp[ 0].fFloat;
+                F32 idet = F32(1) / (a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 -
+                                     a11 * a23 * a32 - a12 * a21 * a33 - a13 * a22 * a31);
+                sp[-8].fFloat = (a22 * a33 - a23 * a32) * idet;
+                sp[-7].fFloat = (a23 * a31 - a21 * a33) * idet;
+                sp[-6].fFloat = (a21 * a32 - a22 * a31) * idet;
+                sp[-5].fFloat = (a13 * a32 - a12 * a33) * idet;
+                sp[-4].fFloat = (a11 * a33 - a13 * a31) * idet;
+                sp[-3].fFloat = (a12 * a31 - a11 * a32) * idet;
+                sp[-2].fFloat = (a12 * a23 - a13 * a22) * idet;
+                sp[-1].fFloat = (a13 * a21 - a11 * a23) * idet;
+                sp[ 0].fFloat = (a11 * a22 - a12 * a21) * idet;
+                break;
+            }
+            case ByteCodeInstruction::kInverse4x4: {
+                F32 a00 = spf(-15), a10 = spf(-11), a20 = spf( -7), a30 = spf( -3),
+                    a01 = spf(-14), a11 = spf(-10), a21 = spf( -6), a31 = spf( -2),
+                    a02 = spf(-13), a12 = spf( -9), a22 = spf( -5), a32 = spf( -1),
+                    a03 = spf(-12), a13 = spf( -8), a23 = spf( -4), a33 = spf(  0);
+
+                F32 b00 = a00 * a11 - a01 * a10,
+                    b01 = a00 * a12 - a02 * a10,
+                    b02 = a00 * a13 - a03 * a10,
+                    b03 = a01 * a12 - a02 * a11,
+                    b04 = a01 * a13 - a03 * a11,
+                    b05 = a02 * a13 - a03 * a12,
+                    b06 = a20 * a31 - a21 * a30,
+                    b07 = a20 * a32 - a22 * a30,
+                    b08 = a20 * a33 - a23 * a30,
+                    b09 = a21 * a32 - a22 * a31,
+                    b10 = a21 * a33 - a23 * a31,
+                    b11 = a22 * a33 - a23 * a32;
+
+                F32 idet = F32(1) /
+                           (b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06);
+
+                b00 *= idet;
+                b01 *= idet;
+                b02 *= idet;
+                b03 *= idet;
+                b04 *= idet;
+                b05 *= idet;
+                b06 *= idet;
+                b07 *= idet;
+                b08 *= idet;
+                b09 *= idet;
+                b10 *= idet;
+                b11 *= idet;
+
+                spf(-15) = a11 * b11 - a12 * b10 + a13 * b09;
+                spf(-14) = a02 * b10 - a01 * b11 - a03 * b09;
+                spf(-13) = a31 * b05 - a32 * b04 + a33 * b03;
+                spf(-12) = a22 * b04 - a21 * b05 - a23 * b03;
+                spf(-11) = a12 * b08 - a10 * b11 - a13 * b07;
+                spf(-10) = a00 * b11 - a02 * b08 + a03 * b07;
+                spf( -9) = a32 * b02 - a30 * b05 - a33 * b01;
+                spf( -8) = a20 * b05 - a22 * b02 + a23 * b01;
+                spf( -7) = a10 * b10 - a11 * b08 + a13 * b06;
+                spf( -6) = a01 * b08 - a00 * b10 - a03 * b06;
+                spf( -5) = a30 * b04 - a31 * b02 + a33 * b00;
+                spf( -4) = a21 * b02 - a20 * b04 - a23 * b00;
+                spf( -3) = a11 * b07 - a10 * b09 - a12 * b06;
+                spf( -2) = a00 * b09 - a01 * b07 + a02 * b06;
+                spf( -1) = a31 * b01 - a30 * b03 - a32 * b00;
+                spf(  0) = a20 * b03 - a21 * b01 + a22 * b00;
                 break;
             }
 
@@ -995,6 +1083,8 @@ static void innerRun(const ByteCode* byteCode, const ByteCodeFunction* f, VValue
 } // namespace Interpreter
 
 #endif // SK_ENABLE_SKSL_INTERPRETER
+
+#undef spf
 
 void ByteCodeFunction::disassemble() const {
 #if defined(SK_ENABLE_SKSL_INTERPRETER)
