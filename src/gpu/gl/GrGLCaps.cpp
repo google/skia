b115d5de6549b01582743355c50348d0ffcf0419
Chris Dalton <csmartdalton@google.com>
2019-07-22 11:48:42 -0400

    ccpr: Disable msaa backend on PowerVR
    Change-Id: Iabfc1106fce9926547278ec1335f4888ca86511e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/229002
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- src/gpu/gl/GrGLCaps.cpp
+++ src/gpu/gl/GrGLCaps.cpp
@@ -3259,9 +3259,11 @@ void GrGLCaps::applyDriverCorrectnessWorkarounds(const GrGLContextInfo& ctxInfo,
         fDriverBlacklistCCPR = true;
     }
 
-    // Temporarily disable the MSAA implementation of CCPR on AMD and Qualcomm while we sort out
-    // an internal compiler error and a crash.
-    if (kATI_GrGLVendor == ctxInfo.vendor() || kQualcomm_GrGLVendor == ctxInfo.vendor()) {
+    // Temporarily disable the MSAA implementation of CCPR on various platforms while we work out
+    // specific issues.
+    if (kATI_GrGLVendor == ctxInfo.vendor() ||  // Radeon hts an internal compiler error.
+        kQualcomm_GrGLVendor == ctxInfo.vendor() ||  // Pixel2 crashes in nanobench.
+        kImagination_GrGLVendor == ctxInfo.vendor()  /* PowerVR doesn't draw curves */) {
         fDriverBlacklistMSAACCPR = true;
     }
 
