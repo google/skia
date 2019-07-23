c5bfdbcf0d01e335b18ff9e5992439c7c4cab149
Greg Daniel <egdaniel@google.com>
2019-07-23 15:11:41 -0400

    Move ExternalFormat and Type to FormatInfo.
    This also deletes the ConfigInfoTable in GrGLCaps as there is no more use
    of it.
    
    Additionally with the rework of storing External Io info on the format table
    I rewrote the implimination of supportedReadPixels and supportedWritePixels
    for GL to loop over the supported types looking for a match instead of
    simply defaulting to a base value.
    
    Bug: skia:6718
    Change-Id: I39f77adf6c0b5b38245e55e8a7e18c0b428862d0

--- src/gpu/gl/GrGLTexture.h
+++ src/gpu/gl/GrGLTexture.h
@@ -42,6 +42,8 @@ public:
 
     GrGLenum target() const;
 
+    GrGLFormat texFormat() const { return GrGLFormatFromGLEnum(fFormat); }
+
     bool hasBaseLevelBeenBoundToFBO() const { return fBaseLevelHasBeenBoundToFBO; }
     void baseLevelWasBoundToFBO() { fBaseLevelHasBeenBoundToFBO = true; }
 
