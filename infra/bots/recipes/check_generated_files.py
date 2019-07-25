e172236e7b75148c773ff36a2e0eaf17fc1c45bb
Mike Klein <mtklein@google.com>
2019-07-25 11:33:48 -0500

    rewrite *.gn and #includes on CheckGeneratedFiles bot
    In addition to checking .fp files, this ought to make this bot now also
    check that our .gn files are formatted and our #includes are consistent
    with rewrite_includes.py.
    
    Some .gn files needed formatting; you can see the failure caught on PS 4.
    
    Cq-Include-Trybots: skia.primary:Housekeeper-PerCommit-CheckGeneratedFiles
    Change-Id: Ia6669581406212c986da81f2521e4e9d8d3eadb4
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/229802
    Commit-Queue: Mike Klein <mtklein@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- infra/bots/recipes/check_generated_files.py
+++ infra/bots/recipes/check_generated_files.py
@@ -45,6 +45,22 @@ def RunSteps(api):
       api.step('generate gl interfaces',
                cmd=['make', '-C', 'tools/gpu/gl/interface', 'generate'])
 
+    # Reformat all tracked .gn files.
+    api.run(api.python, 'fetch-gn',
+            script='bin/fetch-gn',
+            infra_step=True)
+    files = api.run(api.step, 'list .gn files',
+                    cmd=['git', 'ls-files', '*.gn'],
+                    stdout=api.m.raw_io.output(),
+                    infra_step=True).stdout
+    for f in files.split():
+      api.run(api.step, 'format ' + f,
+              cmd=['bin/gn', 'format', f])
+
+    # Rewrite #includes.
+    api.run(api.python, 'rewrite #includes',
+            script='tools/rewrite_includes.py')
+
     # Touch all .fp files so that the generated files are rebuilt.
     api.run(
         api.python.inline,
@@ -60,7 +76,7 @@ for r, d, files in os.walk('%s'):
       subprocess.check_call(['touch', path])
 """ % cwd)
 
-    # Regenerate the SKSL files.
+    # Run GN, regenerate the SKSL files, and make sure rewritten #includes work.
     api.build(checkout_root=checkout_root,
               out_dir=api.vars.build_dir.join('out', 'Release'))
 
@@ -94,5 +110,7 @@ def GenTests(api):
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]') +
-      api.path.exists(api.path['start_dir'])
+      api.path.exists(api.path['start_dir']) +
+      api.step_data('list .gn files',
+                    stdout=api.raw_io.output('BUILD.gn\ngn/foo.gn\n'))
   )
