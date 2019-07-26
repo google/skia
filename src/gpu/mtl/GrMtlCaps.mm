d90e6820b2d89c161c583957e7dd0ed2b020839f
Greg Daniel <egdaniel@google.com>
2019-07-26 09:14:01 -0400

    Reland "Move ExternalFormat and Type to FormatInfo."
    This reverts commit e2c5e8c7eeb07a7c2de566851956027cf6c12b07.
    
    Reason for revert: relanding with fix
    
    Original change's description:
    > Revert "Move ExternalFormat and Type to FormatInfo."
    >
    > This reverts commit 80140518ef7627d103e7d80fec599c052280053a.
    >
    > Reason for revert: probably breaking angle
    >
    > Original change's description:
    > > Move ExternalFormat and Type to FormatInfo.
    > >
    > > This also deletes the ConfigInfoTable in GrGLCaps as there is no more use
    > > of it.
    > >
    > > Additionally with the rework of storing External Io info on the format table
    > > I rewrote the implimination of supportedReadPixels and supportedWritePixels
    > > for GL to loop over the supported types looking for a match instead of
    > > simply defaulting to a base value.
    > >
    > > Finally transferFromOffsetAlignment has been rolled into the SupportedRead
    > > instead of being its own query.
    > >
    > > Bug: skia:6718
    > > Change-Id: I39f77adf6c0b5b38245e55e8a7e18c0b428862d0
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/229381
    > > Commit-Queue: Greg Daniel <egdaniel@google.com>
    > > Reviewed-by: Brian Salomon <bsalomon@google.com>
    >
    > TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    >
    > Change-Id: Ifef2e7308fdb4d91d649f08488b798815e0aa5fa
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:6718
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/229896
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    
    Change-Id: Ida09be706b461cf89467fc0082744177e71e8985
    Bug: skia:6718

--- src/gpu/mtl/GrMtlCaps.mm
+++ src/gpu/mtl/GrMtlCaps.mm
@@ -871,60 +871,80 @@ GrSwizzle GrMtlCaps::getOutputSwizzle(const GrBackendFormat& format, GrColorType
     return get_swizzle(format, colorType, true);
 }
 
-GrCaps::SupportedRead GrMtlCaps::supportedReadPixelsColorType(
+GrCaps::SupportedRead GrMtlCaps::onSupportedReadPixelsColorType(
         GrColorType srcColorType, const GrBackendFormat& srcBackendFormat,
         GrColorType dstColorType) const {
     const GrMTLPixelFormat* grMtlFormat = srcBackendFormat.getMtlFormat();
     if (!grMtlFormat) {
-        return {GrSwizzle(), GrColorType::kUnknown};
+        return {GrSwizzle(), GrColorType::kUnknown, 0};
     }
 
+    GrColorType readCT = GrColorType::kUnknown;
     switch (*grMtlFormat) {
         case MTLPixelFormatRGBA8Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888};
+            readCT = GrColorType::kRGBA_8888;
+            break;
         case MTLPixelFormatR8Unorm:
             if (srcColorType == GrColorType::kAlpha_8) {
-                return {GrSwizzle::RGBA(), GrColorType::kAlpha_8};
+                readCT = GrColorType::kAlpha_8;
             } else if (srcColorType == GrColorType::kGray_8) {
-                return {GrSwizzle::RGBA(), GrColorType::kGray_8};
+                readCT = GrColorType::kGray_8;
             }
+            break;
         case MTLPixelFormatBGRA8Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kBGRA_8888};
+            readCT = GrColorType::kBGRA_8888;
+            break;
 #ifdef SK_BUILD_FOR_IOS
         case MTLPixelFormatB5G6R5Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kBGR_565};
+            readCT = GrColorType::kBGR_565;
+            break;
 #endif
         case MTLPixelFormatRGBA16Float:
             if (srcColorType == GrColorType::kRGBA_F16) {
-                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16};
+                readCT = GrColorType::kRGBA_F16;
             } else if (srcColorType == GrColorType::kRGBA_F16_Clamped){
-                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16_Clamped};
+                readCT = GrColorType::kRGBA_F16_Clamped;
             }
+            break;
         case MTLPixelFormatR16Float:
-            return {GrSwizzle::RGBA(), GrColorType::kAlpha_F16};
+            readCT = GrColorType::kAlpha_F16;
+            break;
         case MTLPixelFormatRG8Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_88};
+            readCT = GrColorType::kRG_88;
+            break;
         case MTLPixelFormatRGB10A2Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_1010102};
+            readCT = GrColorType::kRGBA_1010102;
+            break;
 #ifdef SK_BUILD_FOR_IOS
         case MTLPixelFormatABGR4Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kABGR_4444};
+            readCT = GrColorType::kABGR_4444;
+            break;
 #endif
         case MTLPixelFormatRGBA32Float:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_F32};
+            readCT = GrColorType::kRGBA_F32;
+            break;
         case MTLPixelFormatRGBA8Unorm_sRGB:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888_SRGB};
+            readCT = GrColorType::kRGBA_8888_SRGB;
+            break;
         case MTLPixelFormatR16Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kR_16};
+            readCT = GrColorType::kR_16;
+            break;
         case MTLPixelFormatRG16Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_1616};
+            readCT = GrColorType::kRG_1616;
+            break;
         // Experimental (for Y416 and mutant P016/P010)
         case MTLPixelFormatRGBA16Unorm:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_16161616};
+            readCT = GrColorType::kRGBA_16161616;
+            break;
         case MTLPixelFormatRG16Float:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_F16};
+            readCT = GrColorType::kRG_F16;
+            break;
         default:
-            return {GrSwizzle(), GrColorType::kUnknown};
+            // readCT stays as kUnknown
+            break;
     }
+    // Metal requires the destination offset for copyFromTexture to be a multiple of the textures
+    // pixels size.
+    return {GrSwizzle::RGBA(), readCT, static_cast<size_t>(GrColorTypeBytesPerPixel(readCT))};
 }
 
--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -1206,78 +1206,79 @@ GrSwizzle GrVkCaps::getOutputSwizzle(const GrBackendFormat& format, GrColorType 
     return get_swizzle(format, colorType, true);
 }
 
-size_t GrVkCaps::onTransferFromOffsetAlignment(GrColorType bufferColorType) const {
-    // This GrColorType has 32 bpp but the Vulkan pixel format we use for with may have 24bpp
-    // (VK_FORMAT_R8G8B8_...) or may be 32 bpp. We don't support post transforming the pixel data
-    // for transfer-from currently and don't want to have to pass info about the src surface here.
-    if (bufferColorType == GrColorType::kRGB_888x) {
-        return false;
-    }
-    size_t bpp = GrColorTypeBytesPerPixel(bufferColorType);
-    // The VkBufferImageCopy bufferOffset field must be both a multiple of 4 and of a single texel.
-    switch (bpp & 0b11) {
-        // bpp is already a multiple of 4.
-        case 0:     return bpp;
-        // bpp is a multiple of 2 but not 4.
-        case 2:     return 2 * bpp;
-        // bpp is not a multiple of 2.
-        default:    return 4 * bpp;
-    }
-}
-
-GrCaps::SupportedRead GrVkCaps::supportedReadPixelsColorType(
+GrCaps::SupportedRead GrVkCaps::onSupportedReadPixelsColorType(
         GrColorType srcColorType, const GrBackendFormat& srcBackendFormat,
         GrColorType dstColorType) const {
     const VkFormat* vkFormat = srcBackendFormat.getVkFormat();
     if (!vkFormat) {
-        return {GrSwizzle(), GrColorType::kUnknown};
+        return {GrSwizzle(), GrColorType::kUnknown, 0};
+    }
+
+    // The VkBufferImageCopy bufferOffset field must be both a multiple of 4 and of a single texel.
+    size_t offsetAlignment = GrVkBytesPerFormat(*vkFormat);
+    switch (offsetAlignment & 0b11) {
+        // bpp is already a multiple of 4.
+        case 0:
+            break;
+        // bpp is a multiple of 2 but not 4.
+        case 2:
+            offsetAlignment *= 2;
+            break;
+        // bpp is not a multiple of 2.
+        default:
+            offsetAlignment *= 4;
+            break;
     }
 
     switch (*vkFormat) {
         case VK_FORMAT_R8G8B8A8_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888};
+            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888, offsetAlignment};
         case VK_FORMAT_R8_UNORM:
             if (srcColorType == GrColorType::kAlpha_8) {
-                return {GrSwizzle::RGBA(), GrColorType::kAlpha_8};
+                return {GrSwizzle::RGBA(), GrColorType::kAlpha_8, offsetAlignment};
             } else if (srcColorType == GrColorType::kGray_8) {
-                return {GrSwizzle::RGBA(), GrColorType::kGray_8};
+                return {GrSwizzle::RGBA(), GrColorType::kGray_8, offsetAlignment};
+            } else {
+                return {GrSwizzle(), GrColorType::kUnknown, 0};
             }
         case VK_FORMAT_B8G8R8A8_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kBGRA_8888};
+            return {GrSwizzle::RGBA(), GrColorType::kBGRA_8888, offsetAlignment};
         case VK_FORMAT_R5G6B5_UNORM_PACK16:
-            return {GrSwizzle::RGBA(), GrColorType::kBGR_565};
+            return {GrSwizzle::RGBA(), GrColorType::kBGR_565, offsetAlignment};
         case VK_FORMAT_R16G16B16A16_SFLOAT:
             if (srcColorType == GrColorType::kRGBA_F16) {
-                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16};
-            } else if (srcColorType == GrColorType::kRGBA_F16_Clamped){
-                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16_Clamped};
+                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16, offsetAlignment};
+            } else if (srcColorType == GrColorType::kRGBA_F16_Clamped) {
+                return {GrSwizzle::RGBA(), GrColorType::kRGBA_F16_Clamped, offsetAlignment};
+            } else {
+                return {GrSwizzle(), GrColorType::kUnknown, 0};
             }
         case VK_FORMAT_R16_SFLOAT:
-            return {GrSwizzle::RGBA(), GrColorType::kAlpha_F16};
+            return {GrSwizzle::RGBA(), GrColorType::kAlpha_F16, offsetAlignment};
         case VK_FORMAT_R8G8B8_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kRGB_888x};
+            return {GrSwizzle::RGBA(), GrColorType::kRGB_888x, offsetAlignment};
         case VK_FORMAT_R8G8_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_88};
+            return {GrSwizzle::RGBA(), GrColorType::kRG_88, offsetAlignment};
         case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_1010102};
+            return {GrSwizzle::RGBA(), GrColorType::kRGBA_1010102, offsetAlignment};
         case VK_FORMAT_B4G4R4A4_UNORM_PACK16:
-            return {GrSwizzle::RGBA(), GrColorType::kABGR_4444};
+            return {GrSwizzle::RGBA(), GrColorType::kABGR_4444, offsetAlignment};
         case VK_FORMAT_R4G4B4A4_UNORM_PACK16:
-            return {GrSwizzle::RGBA(), GrColorType::kABGR_4444};
+            return {GrSwizzle::RGBA(), GrColorType::kABGR_4444, offsetAlignment};
         case VK_FORMAT_R32G32B32A32_SFLOAT:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_F32};
+            return {GrSwizzle::RGBA(), GrColorType::kRGBA_F32, offsetAlignment};
         case VK_FORMAT_R8G8B8A8_SRGB:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888_SRGB};
+            return {GrSwizzle::RGBA(), GrColorType::kRGBA_8888_SRGB, offsetAlignment};
         case VK_FORMAT_R16_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kR_16};
+            return {GrSwizzle::RGBA(), GrColorType::kR_16, offsetAlignment};
         case VK_FORMAT_R16G16_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_1616};
+            return {GrSwizzle::RGBA(), GrColorType::kRG_1616, offsetAlignment};
         // Experimental (for Y416 and mutant P016/P010)
         case VK_FORMAT_R16G16B16A16_UNORM:
-            return {GrSwizzle::RGBA(), GrColorType::kRGBA_16161616};
+            return {GrSwizzle::RGBA(), GrColorType::kRGBA_16161616, offsetAlignment};
         case VK_FORMAT_R16G16_SFLOAT:
-            return {GrSwizzle::RGBA(), GrColorType::kRG_F16};
+            return {GrSwizzle::RGBA(), GrColorType::kRG_F16, offsetAlignment};
         default:
-            return {GrSwizzle(), GrColorType::kUnknown};
+            return {GrSwizzle(), GrColorType::kUnknown, 0};
     }
 }
