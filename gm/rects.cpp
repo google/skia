eecfd0cbe488e651fb4b28b3829b4ff459e894f9
Mike Reed <reed@google.com>
2019-07-29 17:52:48 -0400

    remove code now that loopers are dead (w.r.t. canvas and paint)
    We can't remove the loopers themselves, as they are still used
    by android and chrome (they just don't ever pass them to skia).
    
    Eventually each of those clients will resolve this, but for now
    we just keep the classes (and tests) in skia.
    
    Change-Id: I5f507e6bb82280f2bc7c0b21eebe59c287aa9265

--- gm/rects.cpp
+++ gm/rects.cpp
@@ -101,18 +101,6 @@ protected:
             fPaints.push_back(p);
         }
 
-#ifdef SK_SUPPORT_LEGACY_DRAWLOOPER
-        {
-            // AA with blur
-            SkPaint p;
-            p.setColor(SK_ColorWHITE);
-            p.setAntiAlias(true);
-            p.setLooper(SkBlurDrawLooper::Make(SK_ColorWHITE,
-                                         SkBlurMask::ConvertRadiusToSigma(SkIntToScalar(10)),
-                                         SkIntToScalar(5), SkIntToScalar(10)));
-            fPaints.push_back(p);
-        }
-#else
         fLooper = SkBlurDrawLooper::Make(SK_ColorWHITE, SkBlurMask::ConvertRadiusToSigma(10),5,10);
         {
             SkPaint p;
@@ -120,7 +108,6 @@ protected:
             p.setAntiAlias(true);
             fPaints.push_back(p);
         }
-#endif
         {
             // AA with stroke style
             SkPaint p;
