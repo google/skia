ff171815a05d33d0aa4e3f009c16a93c53936876
Stephen White <senorblanco@chromium.org>
2019-07-29 14:37:31 -0400

    Dawn backend: first triangle.
    Nothing is cached in this initial version; all the magic happens in
    GrDawnGpuRTCommandBuffer::beginDraw(). It builds the program,
    sets the uniform data, converts the vertex attributes, initializes
    rasterization state and blend state, creates a pipeline and bind group
    and sets them.
    GrDawnGpuProgramBuilder::beginRenderPass() then finishes
    the command encoder and submits the resulting command buffer.
    Stub out lots of things in GrDawnGpu and GrDawnGpuCommandBuffer with asserts.
    Stub out lots of things in GrDawnUniformHandler and GrDawnVaryingHandler.
    Add GrDawnProgramBuilder to GrGLSLShaderBuilder's friends, as the other
    backends do.
    Enable the RTAdjustment uniform, which does the viewing transform.
    Implement buffer map/unmap with a malloc'ed buffer.
    Change-Id: Ie9c160cd18a2c4ef1bde66f86a52227e3bf1b570

---
+++ src/gpu/dawn/GrDawnBuffer.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/dawn/GrDawnBuffer.h"
+#include "src/gpu/dawn/GrDawnGpu.h"
+
+namespace {
+    dawn::BufferUsageBit GrGpuBufferTypeToDawnUsageBit(GrGpuBufferType type) {
+        switch (type) {
+            case GrGpuBufferType::kVertex:
+                return dawn::BufferUsageBit::Vertex;
+            case GrGpuBufferType::kIndex:
+                return dawn::BufferUsageBit::Index;
+            case GrGpuBufferType::kXferCpuToGpu:
+                return dawn::BufferUsageBit::CopySrc;
+            case GrGpuBufferType::kXferGpuToCpu:
+                return dawn::BufferUsageBit::CopyDst;
+            default:
+                SkASSERT(!"buffer type not supported by Dawn");
+                return dawn::BufferUsageBit::Vertex;
+        }
+    }
+}
+
+GrDawnBuffer::GrDawnBuffer(GrDawnGpu* gpu, size_t sizeInBytes, GrGpuBufferType type,
+                           GrAccessPattern pattern)
+    : INHERITED(gpu, sizeInBytes, type, pattern)
+    , fData(nullptr) {
+    dawn::BufferDescriptor bufferDesc;
+    bufferDesc.size = sizeInBytes;
+    bufferDesc.usage = GrGpuBufferTypeToDawnUsageBit(type) | dawn::BufferUsageBit::CopyDst;
+    fBuffer = getDawnGpu()->device().CreateBuffer(&bufferDesc);
+    this->registerWithCache(SkBudgeted::kYes);
+}
+
+GrDawnBuffer::~GrDawnBuffer() {
+    delete[] fData;
+}
+
+void GrDawnBuffer::onMap() {
+    if (this->wasDestroyed()) {
+        return;
+    }
+    fData = new char[size()];
+    fMapPtr = fData;
+}
+
+void GrDawnBuffer::onUnmap() {
+    if (this->wasDestroyed()) {
+        return;
+    }
+    delete[] fData;
+    fData = nullptr;
+}
+
+bool GrDawnBuffer::onUpdateData(const void* src, size_t srcSizeInBytes) {
+    if (this->wasDestroyed()) {
+        return false;
+    }
+    fBuffer.SetSubData(0, srcSizeInBytes, static_cast<const uint8_t*>(src));
+    return true;
+}
+
+GrDawnGpu* GrDawnBuffer::getDawnGpu() const {
+    SkASSERT(!this->wasDestroyed());
+    return static_cast<GrDawnGpu*>(this->getGpu());
+}
