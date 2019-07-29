3e2509a778da8d7fe8e84116c6d5d1daee31659e
Michael Ludwig <michaelludwig@google.com>
2019-07-29 12:04:08 -0400

    Hide SkImageFilter subclasses in CPP files
    Bug: skia:9280
    Change-Id: Ice3624467f5e643f9d3d81da72d77911eeac963e

--- include/effects/SkLightingImageFilter.h
+++ include/effects/SkLightingImageFilter.h
@@ -11,52 +11,35 @@
 #include "include/core/SkColor.h"
 #include "include/core/SkImageFilter.h"
 
-
-class SkImageFilterLight;
 struct SkPoint3;
 
-class SK_API SkLightingImageFilter : public SkImageFilter {
+class SK_API SkLightingImageFilter {
 public:
     static sk_sp<SkImageFilter> MakeDistantLitDiffuse(const SkPoint3& direction,
         SkColor lightColor, SkScalar surfaceScale, SkScalar kd,
-        sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
     static sk_sp<SkImageFilter> MakePointLitDiffuse(const SkPoint3& location,
         SkColor lightColor, SkScalar surfaceScale, SkScalar kd,
-        sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
     static sk_sp<SkImageFilter> MakeSpotLitDiffuse(const SkPoint3& location,
         const SkPoint3& target, SkScalar specularExponent, SkScalar cutoffAngle,
         SkColor lightColor, SkScalar surfaceScale, SkScalar kd,
-        sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
     static sk_sp<SkImageFilter> MakeDistantLitSpecular(const SkPoint3& direction,
-        SkColor lightColor, SkScalar surfaceScale, SkScalar ks,
-        SkScalar shininess, sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
+        SkColor lightColor, SkScalar surfaceScale, SkScalar ks, SkScalar shininess,
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
     static sk_sp<SkImageFilter> MakePointLitSpecular(const SkPoint3& location,
-        SkColor lightColor, SkScalar surfaceScale, SkScalar ks,
-        SkScalar shininess, sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
+        SkColor lightColor, SkScalar surfaceScale, SkScalar ks, SkScalar shininess,
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
     static sk_sp<SkImageFilter> MakeSpotLitSpecular(const SkPoint3& location,
         const SkPoint3& target, SkScalar specularExponent, SkScalar cutoffAngle,
-        SkColor lightColor, SkScalar surfaceScale, SkScalar ks,
-        SkScalar shininess, sk_sp<SkImageFilter> input, const CropRect* cropRect = nullptr);
-    ~SkLightingImageFilter() override;
+        SkColor lightColor, SkScalar surfaceScale, SkScalar ks, SkScalar shininess,
+        sk_sp<SkImageFilter> input, const SkImageFilter::CropRect* cropRect = nullptr);
 
     static void RegisterFlattenables();
 
-protected:
-    SkLightingImageFilter(sk_sp<SkImageFilterLight> light,
-                          SkScalar surfaceScale,
-                          sk_sp<SkImageFilter> input,
-                          const CropRect* cropRect);
-    void flatten(SkWriteBuffer&) const override;
-    const SkImageFilterLight* light() const { return fLight.get(); }
-    inline sk_sp<const SkImageFilterLight> refLight() const;
-    SkScalar surfaceScale() const { return fSurfaceScale; }
-    bool affectsTransparentBlack() const override { return true; }
-
 private:
-    sk_sp<SkImageFilterLight> fLight;
-    SkScalar fSurfaceScale;
-
-    typedef SkImageFilter INHERITED;
+    SkLightingImageFilter() = delete;
 };
 
 #endif
