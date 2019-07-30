d7959706671af3d497c8f3417ef40ea51ea1cb68
Michael Ludwig <michaelludwig@google.com>
2019-07-30 10:03:16 -0400

    Hide SkImageFilter subclasses in CPP files
    Bug: skia:9280
    Change-Id: Ice3624467f5e643f9d3d81da72d77911eeac963e

--- src/effects/imagefilters/SkColorFilterImageFilter.cpp
+++ src/effects/imagefilters/SkColorFilterImageFilter.cpp
@@ -15,51 +15,84 @@
 #include "src/core/SkSpecialSurface.h"
 #include "src/core/SkWriteBuffer.h"
 
+namespace {
+
+class SkColorFilterImageFilterImpl final : public SkImageFilter {
+public:
+    static sk_sp<SkImageFilter> Make(sk_sp<SkColorFilter> cf,
+                                     sk_sp<SkImageFilter> input,
+                                     const CropRect* cropRect = nullptr) {
+        if (!cf) {
+            return nullptr;
+        }
+
+        SkColorFilter* inputCF;
+        if (input && input->isColorFilterNode(&inputCF)) {
+            // This is an optimization, as it collapses the hierarchy by just combining the two
+            // colorfilters into a single one, which the new imagefilter will wrap.
+            sk_sp<SkColorFilter> newCF = cf->makeComposed(sk_sp<SkColorFilter>(inputCF));
+            if (newCF) {
+                return sk_sp<SkImageFilter>(new SkColorFilterImageFilterImpl(
+                        std::move(newCF), sk_ref_sp(input->getInput(0)), cropRect));
+            }
+        }
+
+        return sk_sp<SkImageFilter>(new SkColorFilterImageFilterImpl(
+                std::move(cf), std::move(input), cropRect));
+    }
+
+protected:
+    void flatten(SkWriteBuffer&) const override;
+    sk_sp<SkSpecialImage> onFilterImage(SkSpecialImage* source, const Context&,
+                                        SkIPoint* offset) const override;
+    bool onIsColorFilterNode(SkColorFilter**) const override;
+    bool onCanHandleComplexCTM() const override { return true; }
+    bool affectsTransparentBlack() const override;
+
+private:
+    friend void SkColorFilterImageFilter::RegisterFlattenables();
+    SK_FLATTENABLE_HOOKS(SkColorFilterImageFilterImpl)
+
+    SkColorFilterImageFilterImpl(sk_sp<SkColorFilter> cf, sk_sp<SkImageFilter> input,
+                                 const CropRect* cropRect)
+            : INHERITED(&input, 1, cropRect)
+            , fColorFilter(std::move(cf)) {}
+
+    sk_sp<SkColorFilter> fColorFilter;
+
+    typedef SkImageFilter INHERITED;
+};
+
+} // end namespace
+
 sk_sp<SkImageFilter> SkColorFilterImageFilter::Make(sk_sp<SkColorFilter> cf,
                                                     sk_sp<SkImageFilter> input,
-                                                    const CropRect* cropRect) {
-    if (!cf) {
-        return nullptr;
-    }
-
-    SkColorFilter* inputCF;
-    if (input && input->isColorFilterNode(&inputCF)) {
-        // This is an optimization, as it collapses the hierarchy by just combining the two
-        // colorfilters into a single one, which the new imagefilter will wrap.
-        sk_sp<SkColorFilter> newCF = cf->makeComposed(sk_sp<SkColorFilter>(inputCF));
-        if (newCF) {
-            return sk_sp<SkImageFilter>(new SkColorFilterImageFilter(std::move(newCF),
-                                                                     sk_ref_sp(input->getInput(0)),
-                                                                     cropRect));
-        }
-    }
-
-    return sk_sp<SkImageFilter>(new SkColorFilterImageFilter(std::move(cf),
-                                                             std::move(input),
-                                                             cropRect));
+                                                    const SkImageFilter::CropRect* cropRect) {
+    return SkColorFilterImageFilterImpl::Make(std::move(cf), std::move(input), cropRect);
 }
 
-SkColorFilterImageFilter::SkColorFilterImageFilter(sk_sp<SkColorFilter> cf,
-                                                   sk_sp<SkImageFilter> input,
-                                                   const CropRect* cropRect)
-    : INHERITED(&input, 1, cropRect)
-    , fColorFilter(std::move(cf)) {
+void SkColorFilterImageFilter::RegisterFlattenables() {
+    SK_REGISTER_FLATTENABLE(SkColorFilterImageFilterImpl);
+    // TODO (michaelludwig) - Remove after grace period for SKPs to stop using old name
+    SkFlattenable::Register("SkColorFilterImageFilter", SkColorFilterImageFilterImpl::CreateProc);
 }
 
-sk_sp<SkFlattenable> SkColorFilterImageFilter::CreateProc(SkReadBuffer& buffer) {
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+sk_sp<SkFlattenable> SkColorFilterImageFilterImpl::CreateProc(SkReadBuffer& buffer) {
     SK_IMAGEFILTER_UNFLATTEN_COMMON(common, 1);
     sk_sp<SkColorFilter> cf(buffer.readColorFilter());
     return Make(std::move(cf), common.getInput(0), &common.cropRect());
 }
 
-void SkColorFilterImageFilter::flatten(SkWriteBuffer& buffer) const {
+void SkColorFilterImageFilterImpl::flatten(SkWriteBuffer& buffer) const {
     this->INHERITED::flatten(buffer);
     buffer.writeFlattenable(fColorFilter.get());
 }
 
-sk_sp<SkSpecialImage> SkColorFilterImageFilter::onFilterImage(SkSpecialImage* source,
-                                                              const Context& ctx,
-                                                              SkIPoint* offset) const {
+sk_sp<SkSpecialImage> SkColorFilterImageFilterImpl::onFilterImage(SkSpecialImage* source,
+                                                                  const Context& ctx,
+                                                                  SkIPoint* offset) const {
     SkIPoint inputOffset = SkIPoint::Make(0, 0);
     sk_sp<SkSpecialImage> input(this->filterInput(0, source, ctx, &inputOffset));
 
@@ -116,7 +149,7 @@ sk_sp<SkSpecialImage> SkColorFilterImageFilter::onFilterImage(SkSpecialImage* so
     return surf->makeImageSnapshot();
 }
 
-bool SkColorFilterImageFilter::onIsColorFilterNode(SkColorFilter** filter) const {
+bool SkColorFilterImageFilterImpl::onIsColorFilterNode(SkColorFilter** filter) const {
     SkASSERT(1 == this->countInputs());
     if (!this->cropRectIsSet()) {
         if (filter) {
@@ -127,6 +160,6 @@ bool SkColorFilterImageFilter::onIsColorFilterNode(SkColorFilter** filter) const
     return false;
 }
 
-bool SkColorFilterImageFilter::affectsTransparentBlack() const {
+bool SkColorFilterImageFilterImpl::affectsTransparentBlack() const {
     return fColorFilter->affectsTransparentBlack();
 }
