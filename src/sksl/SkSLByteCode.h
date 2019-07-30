03f7b16d5aa65d7507136e90258c9fd04ad9ccfa
Brian Osman <brianosman@google.com>
2019-07-30 09:29:33 -0400

    Interpreter: Add component-wise compare/mix builtins
    Moved dot() to SkSL, too
    
    Change-Id: I89958a6f28cc8317b109f49915ed0f8520995592

--- src/sksl/SkSLByteCode.h
+++ src/sksl/SkSLByteCode.h
@@ -77,6 +77,9 @@ enum class ByteCodeInstruction : uint16_t {
     kMatrixToMatrix,
     // Followed by three bytes: leftCols (== rightRows), leftRows, rightCols
     kMatrixMultiply,
+    // Masked selection: Stack is ... A1, A2, A3, B1, B2, B3, M1, M2, M3
+    //                   Result:      M1 ? B1 : A1, M2 ? B2 : A2, M3 ? B3 : A3
+    VECTOR(kMix),
     VECTOR_MATRIX(kNegateF),
     VECTOR(kNegateI),
     VECTOR_MATRIX(kMultiplyF),
