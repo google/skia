79b81c9b4709b7226e6a15d91beb0c4ac5ae78c6
Stephen White <senorblanco@chromium.org>
2019-07-31 15:56:26 -0400

    First implementation of GrDawnProgramDataManager.
    Also add GrDawnCaps to friends of GrShaderCaps, as the other backends do.
    
    Change-Id: Iae60cae7168ad9194f7808c2f51f41de10e53071
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/230747
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

---
+++ src/gpu/dawn/GrDawnProgramDataManager.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrDawnProgramDataManager_DEFINED
+#define GrDawnProgramDataManager_DEFINED
+
+#include "src/gpu/dawn/GrDawnUniformHandler.h"
+#include "src/gpu/glsl/GrGLSLProgramDataManager.h"
+#include "dawn/dawncpp.h"
+
+#include "src/core/SkAutoMalloc.h"
+
+class GrDawnGpu;
+class GrDawnUniformBuffer;
+
+class GrDawnProgramDataManager : public GrGLSLProgramDataManager {
+public:
+    typedef GrDawnUniformHandler::UniformInfoArray UniformInfoArray;
+
+    GrDawnProgramDataManager(const UniformInfoArray&,
+                             uint32_t geometryUniformSize,
+                             uint32_t fragmentUniformSize);
+
+    void set1i(UniformHandle, int32_t) const override;
+    void set1iv(UniformHandle, int arrayCount, const int32_t v[]) const override;
+    void set2i(UniformHandle, int32_t, int32_t) const override;
+    void set2iv(UniformHandle, int arrayCount, const int32_t v[]) const override;
+    void set3i(UniformHandle, int32_t, int32_t, int32_t) const override;
+    void set3iv(UniformHandle, int arrayCount, const int32_t v[]) const override;
+    void set4i(UniformHandle, int32_t, int32_t, int32_t, int32_t) const override;
+    void set4iv(UniformHandle, int arrayCount, const int32_t v[]) const override;
+
+    void set1f(UniformHandle, float v0) const override;
+    void set1fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set2f(UniformHandle, float, float) const override;
+    void set2fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set3f(UniformHandle, float, float, float) const override;
+    void set3fv(UniformHandle, int arrayCount, const float v[]) const override;
+    void set4f(UniformHandle, float, float, float, float) const override;
+    void set4fv(UniformHandle, int arrayCount, const float v[]) const override;
+    // matrices are column-major, the first two upload a single matrix, the latter two upload
+    // arrayCount matrices into a uniform array.
+    void setMatrix2f(UniformHandle, const float matrix[]) const override;
+    void setMatrix3f(UniformHandle, const float matrix[]) const override;
+    void setMatrix4f(UniformHandle, const float matrix[]) const override;
+    void setMatrix2fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+    void setMatrix3fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+    void setMatrix4fv(UniformHandle, int arrayCount, const float matrices[]) const override;
+
+    // for nvpr only
+    void setPathFragmentInputTransform(VaryingHandle u, int components,
+                                       const SkMatrix& matrix) const override {
+        SK_ABORT("Only supported in NVPR, which is not in Dawn");
+    }
+
+    void uploadUniformBuffers(dawn::Buffer geometryBuffer, dawn::Buffer fragmentBuffer) const;
+
+private:
+    struct Uniform {
+        uint32_t fBinding;
+        uint32_t fOffset;
+        SkDEBUGCODE(
+            GrSLType    fType;
+            int         fArrayCount;
+        );
+    };
+
+    template<int N> inline void setMatrices(UniformHandle, int arrayCount,
+                                            const float matrices[]) const;
+
+    void* getBufferPtrAndMarkDirty(const Uniform& uni) const;
+
+    uint32_t fGeometryUniformSize;
+    uint32_t fFragmentUniformSize;
+
+    SkTArray<Uniform, true> fUniforms;
+
+    mutable SkAutoMalloc fGeometryUniformData;
+    mutable SkAutoMalloc fFragmentUniformData;
+    mutable bool         fGeometryUniformsDirty;
+    mutable bool         fFragmentUniformsDirty;
+};
+
+#endif
