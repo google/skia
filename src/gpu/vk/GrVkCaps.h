882712275d4ae92a989a5bcb64b5dcea644cb483
Robert Phillips <robertphillips@google.com>
2019-07-31 12:48:31 -0400

    Add GrCaps::getTestingCombinations
    Bug: skia:6718
    Change-Id: I951f57cce229792d994391dbba74dd206bbfb923

--- src/gpu/vk/GrVkCaps.h
+++ src/gpu/vk/GrVkCaps.h
@@ -172,6 +172,10 @@ public:
     GrSwizzle getTextureSwizzle(const GrBackendFormat&, GrColorType) const override;
     GrSwizzle getOutputSwizzle(const GrBackendFormat&, GrColorType) const override;
 
+#if GR_TEST_UTILS
+    std::vector<TestingCombination> getTestingCombinations() const override;
+#endif
+
 private:
     enum VkVendor {
         kAMD_VkVendor = 4098,
