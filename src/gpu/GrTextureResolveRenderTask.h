2d020e24b81b69947abacf92d66c62b3411077aa
Chris Dalton <csmartdalton@google.com>
2019-08-01 15:14:06 -0600

    Initiate regeneration of mipmaps from proxy DAG land
    This allows us to resolve all the textures before executing a command
    buffer, rather than interrupting execution as is currently done in the
    GL backend.
    
    Bug: skia:
    Change-Id: I998546b312d24cf47fc2c837e7c94fbf95571af0

---
+++ src/gpu/GrTextureResolveRenderTask.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrTextureResolveRenderTask_DEFINED
+#define GrTextureResolveRenderTask_DEFINED
+
+#include "src/gpu/GrRenderTask.h"
+#include "src/gpu/GrTextureResolveManager.h"
+
+class GrTextureResolveRenderTask final : public GrRenderTask {
+public:
+    GrTextureResolveRenderTask(
+            sk_sp<GrTextureProxy>, GrTextureResolveManager::ResolveFlags, const GrCaps&);
+
+private:
+    using ResolveFlags = GrTextureResolveManager::ResolveFlags;
+
+    void onPrepare(GrOpFlushState*) override {}
+    bool onIsUsed(GrSurfaceProxy* proxy) const override {
+        SkASSERT(proxy != fTarget.get());  // This case should be handled by GrRenderTask.
+        return false;
+    }
+    void gatherProxyIntervals(GrResourceAllocator*) const override;
+    bool onExecute(GrOpFlushState*) override;
+
+    const ResolveFlags fResolveFlags;
+};
+
+#endif
