bc647fccb5e6d48e580e07f6a4eb9043cfd1852b
Chris Dalton <csmartdalton@google.com>
2019-08-02 14:45:13 -0600

    Initiate regeneration of mipmaps from proxy DAG land
    This allows us to resolve all the textures before executing a command
    buffer, rather than interrupting execution as is currently done in the
    GL backend.
    
    Change-Id: I998546b312d24cf47fc2c837e7c94fbf95571af0

---
+++ src/gpu/GrTextureResolveRenderTask.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/GrTextureResolveRenderTask.h"
+
+#include "src/gpu/GrGpu.h"
+#include "src/gpu/GrMemoryPool.h"
+#include "src/gpu/GrOpFlushState.h"
+#include "src/gpu/GrResourceAllocator.h"
+#include "src/gpu/GrTexturePriv.h"
+
+sk_sp<GrRenderTask> GrTextureResolveRenderTask::Make(
+        sk_sp<GrTextureProxy> textureProxyPtr, ResolveFlags resolveFlags, const GrCaps& caps) {
+    GrTextureProxy* textureProxy = textureProxyPtr.get();
+    sk_sp<GrTextureResolveRenderTask> resolveTask(
+            new GrTextureResolveRenderTask(std::move(textureProxyPtr), resolveFlags));
+
+    // Add the target as a dependency: We will read the existing contents of this texture while
+    // generating mipmap levels and/or resolving MSAA.
+    //
+    // NOTE: This must be called before makeClosed.
+    resolveTask->addDependency(
+            textureProxy, GrMipMapped::kNo, GrTextureResolveManager(nullptr), caps);
+    textureProxy->setLastRenderTask(resolveTask.get());
+
+    // We only resolve the texture; nobody should try to do anything else with this opList.
+    resolveTask->makeClosed(caps);
+
+    if (ResolveFlags::kMipMaps & resolveFlags) {
+        SkASSERT(GrMipMapped::kYes == textureProxy->mipMapped());
+        SkASSERT(textureProxy->mipMapsAreDirty());
+        textureProxy->markMipMapsClean();
+    }
+
+    return resolveTask;
+}
+
+void GrTextureResolveRenderTask::gatherProxyIntervals(GrResourceAllocator* alloc) const {
+    // Huh? This makes the following assert go away:
+    //
+    // https://cs.chromium.org/chromium/src/third_party/skia/src/gpu/GrResourceAllocator.cpp?l=61
+    alloc->addInterval(fTarget.get(), alloc->curOp(), alloc->curOp(),
+                       GrResourceAllocator::ActualUse::kYes);
+    alloc->incOps();
+}
+
+bool GrTextureResolveRenderTask::onExecute(GrOpFlushState* flushState) {
+    GrTexture* texture = fTarget->peekTexture();
+    SkASSERT(texture);
+
+    if (ResolveFlags::kMipMaps & fResolveFlags) {
+        SkASSERT(texture->texturePriv().mipMapsAreDirty());
+        flushState->gpu()->regenerateMipMapLevels(texture);
+    }
+
+    return true;
+}
