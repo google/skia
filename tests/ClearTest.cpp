d344284e770e592ae9a9535780866a48f6841783
Robert Phillips <robertphillips@google.com>
2019-08-02 12:26:22 -0400

    More SkAutoPixmapStorage
    A left over code review request
    
    Change-Id: Ib58abaee4761d14b8ec67f88dc903ac1ed06a58e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/232016
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/ClearTest.cpp
+++ tests/ClearTest.cpp
@@ -20,6 +20,7 @@
 #include "include/gpu/GrContextOptions.h"
 #include "include/private/GrTypesPriv.h"
 #include "include/private/SkColorData.h"
+#include "src/core/SkAutoPixmapStorage.h"
 #include "src/gpu/GrCaps.h"
 #include "src/gpu/GrColor.h"
 #include "src/gpu/GrContextPriv.h"
@@ -35,18 +36,21 @@ static bool check_rect(GrRenderTargetContext* rtc, const SkIRect& rect, uint32_t
                        uint32_t* actualValue, int* failX, int* failY) {
     int w = rect.width();
     int h = rect.height();
-    std::unique_ptr<uint32_t[]> pixels(new uint32_t[w * h]);
-    memset(pixels.get(), ~expectedValue, sizeof(uint32_t) * w * h);
 
     SkImageInfo dstInfo = SkImageInfo::Make(w, h, kRGBA_8888_SkColorType, kPremul_SkAlphaType);
 
-    if (!rtc->readPixels(dstInfo, pixels.get(), 0, {rect.fLeft, rect.fTop})) {
+    SkAutoPixmapStorage readback;
+    readback.alloc(dstInfo);
+
+    readback.erase(~expectedValue);
+    if (!rtc->readPixels(readback.info(), readback.writable_addr(), readback.rowBytes(),
+                         {rect.fLeft, rect.fTop})) {
         return false;
     }
 
     for (int y = 0; y < h; ++y) {
         for (int x = 0; x < w; ++x) {
-            uint32_t pixel = pixels.get()[y * w + x];
+            uint32_t pixel = readback.addr32()[y * w + x];
             if (pixel != expectedValue) {
                 *actualValue = pixel;
                 *failX = x + rect.fLeft;
