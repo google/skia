099a52985077096bd9473076b238004ed1599240
Mike Reed <reed@google.com>
2019-08-03 16:30:46 -0400

    specialize drawAtlas for raster backend
    Also, make shadermixer more flexible, allowing null-shader to mean use paint's color
    Bug: skia:
    Change-Id: I0ff15155e37c0af7931abd34c0883701a47a048a

--- src/core/SkBitmapDevice.h
+++ src/core/SkBitmapDevice.h
@@ -101,6 +101,8 @@ protected:
     void drawGlyphRunList(const SkGlyphRunList& glyphRunList) override;
     void drawVertices(const SkVertices*, const SkVertices::Bone bones[], int boneCount, SkBlendMode,
                       const SkPaint& paint) override;
+    void drawAtlas(const SkImage*, const SkRSXform[], const SkRect[], const SkColor[], int count,
+                   SkBlendMode, const SkPaint&) override;
     void drawDevice(SkBaseDevice*, int x, int y, const SkPaint&) override;
 
     ///////////////////////////////////////////////////////////////////////////
