97d65f601ce00273285aecd7763b1f318d1b39a1
Chris Dalton <csmartdalton@google.com>
2019-08-05 12:01:15 -0600

    Initiate regeneration of mipmaps from proxy DAG land
    This allows us to resolve all the textures before executing a command
    buffer, rather than interrupting execution as is currently done in the
    GL backend.
    
    Change-Id: I998546b312d24cf47fc2c837e7c94fbf95571af0

---
+++ src/gpu/GrTextureResolveRenderTask.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrTextureResolveRenderTask_DEFINED
+#define GrTextureResolveRenderTask_DEFINED
+
+#include "src/gpu/GrRenderTask.h"
+#include "src/gpu/GrTextureResolveManager.h"
+
+class GrTextureResolveRenderTask final : public GrRenderTask {
+public:
+    static sk_sp<GrRenderTask> Make(
+            sk_sp<GrTextureProxy>, GrTextureResolveManager::ResolveFlags, const GrCaps&);
+
+private:
+    using ResolveFlags = GrTextureResolveManager::ResolveFlags;
+
+    GrTextureResolveRenderTask(sk_sp<GrTextureProxy> textureProxy, ResolveFlags resolveFlags)
+            : GrRenderTask(std::move(textureProxy))
+            , fResolveFlags(resolveFlags) {
+        SkASSERT(ResolveFlags::kNone != fResolveFlags);
+    }
+
+    void onPrepare(GrOpFlushState*) override {}
+    bool onIsUsed(GrSurfaceProxy* proxy) const override {
+        SkASSERT(proxy != fTarget.get());  // This case should be handled by GrRenderTask.
+        return false;
+    }
+    void handleInternalAllocationFailure() override {}
+    void gatherProxyIntervals(GrResourceAllocator*) const override;
+    bool onExecute(GrOpFlushState*) override;
+
+    const ResolveFlags fResolveFlags;
+};
+
+#endif
