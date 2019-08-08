04c55735557abb6f99a06813ab0e07fed92ccceb
Herb Derby <herb@google.com>
2019-08-08 17:14:50 -0400

    Introduce SkZip
    SkZip, takes a list of container like things (classes with operator[]),
    and indexes them in sequence.
    
    This is part 1/3 of the whole eco-system.
    
    Change-Id: I4e9ab8ecc78b02f41a8247700233035e934a4067

--- src/core/SkSpan.h
+++ src/core/SkSpan.h
@@ -9,9 +9,10 @@
 #define SkSpan_DEFINED
 
 #include <cstddef>
-#include <string>
-#include <vector>
+#include <tuple>
+#include <type_traits>
 
+#include "include/private/SkTemplates.h"
 #include "include/private/SkTo.h"
 
 template <typename T>
@@ -56,4 +57,61 @@ inline auto SkMakeSpan(Container& c)
         -> SkSpan<typename std::remove_reference<decltype(*(c.data()))>::type> {
     return {c.data(), c.size()};
 }
+
+// Take a list of things that have operator[], and use them all in parallel. The iterators and
+// accessor operator[] for the class produce a tuple of the items.
+template<typename... Ts>
+class SkZip {
+    using ReturnTuple = std::tuple<decltype(std::declval<Ts>()[0])...>;
+
+    class Iterator {
+    public:
+        Iterator(const SkZip* zip, size_t index) : fZip{zip}, fIndex{index} { }
+        Iterator(const Iterator& that) : Iterator{ that.fZip, that.fIndex } { }
+        Iterator& operator++() { ++fIndex; return *this; }
+        Iterator operator++(int) { Iterator tmp(*this); operator++(); return tmp; }
+        bool operator==(const Iterator& rhs) const { return fIndex == rhs.fIndex; }
+        bool operator!=(const Iterator& rhs) const { return fIndex != rhs.fIndex; }
+        ReturnTuple operator*() { return (*fZip)[fIndex]; }
+
+    private:
+        const SkZip* const fZip = nullptr;
+        size_t fIndex = 0;
+    };
+
+public:
+    SkZip(size_t) = delete;
+    explicit SkZip(size_t size, Ts&&... ts)
+            : fPointers{std::forward<Ts>(ts)...}
+            , fSize{size} {}
+
+    template<typename... Us>
+    explicit SkZip(const SkZip<Us...>& that) : fPointers{that.fPointers}, fSize{that.fSize} {}
+
+    ReturnTuple operator[](size_t i) const {
+        return this->index(i);
+    }
+
+    size_t size() const { return fSize; }
+    bool empty() const { return this->size() == 0; }
+    ReturnTuple front() const { return this->index(0); }
+    ReturnTuple back() const { return this->index(this->size() - 1); }
+    Iterator begin() const { return Iterator{this, 0}; }
+    Iterator end() const { return Iterator{this, this->size()}; }
+
+private:
+    ReturnTuple index(size_t i) const {
+        SkASSERT( this->size() > 0);
+        SkASSERT( i < this->size());
+        return indexDetail(i, skstd::make_index_sequence<sizeof...(Ts)>{});
+    }
+
+    template<std::size_t... Is>
+    ReturnTuple indexDetail(size_t i, skstd::index_sequence<Is...>) const {
+        return ReturnTuple((std::get<Is>(fPointers))[i]...);
+    }
+
+    std::tuple<Ts...> fPointers;
+    size_t fSize;
+};
 #endif  // SkSpan_DEFINED
