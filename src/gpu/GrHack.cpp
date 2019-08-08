f8abe03326ade5ffc95d5ac676fb406849d33494
Robert Phillips <robertphillips@google.com>
2019-08-08 13:03:30 -0400

    [M74] Explicit Allocator test harness
    No-Tree-Checks: true
    No-Try: true
    No-Presubmit: true
    Change-Id: I1f8eff409c0f9c5b51a18645cd7464e1bd3a9a9f

---
+++ src/gpu/GrHack.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "GrHack.h"
+
+#include "GrTestUtils.h"
+
+#include "GrTexturePriv.h"
+
+const char* GrPixelConfigToStr(GrPixelConfig config) {
+    switch (config) {
+        case kUnknown_GrPixelConfig:           return "Unknown";
+        case kAlpha_8_GrPixelConfig:           return "Alpha8";
+        case kAlpha_8_as_Alpha_GrPixelConfig:  return "Alpha8_asAlpha";
+        case kAlpha_8_as_Red_GrPixelConfig:    return "Alpha8_asRed";
+        case kGray_8_GrPixelConfig:            return "Gray8";
+        case kGray_8_as_Lum_GrPixelConfig:     return "Gray8_asLum";
+        case kGray_8_as_Red_GrPixelConfig:     return "Gray8_asRed";
+        case kRGB_565_GrPixelConfig:           return "RGB565";
+        case kRGBA_4444_GrPixelConfig:         return "RGBA444";
+        case kRGBA_8888_GrPixelConfig:         return "RGBA8888";
+        case kRGB_888_GrPixelConfig:           return "RGB888";
+        case kRGB_888X_GrPixelConfig:          return "RGB888X";
+        case kRG_88_GrPixelConfig:             return "RG88";
+        case kBGRA_8888_GrPixelConfig:         return "BGRA8888";
+        case kSRGBA_8888_GrPixelConfig:        return "SRGBA8888";
+        case kRGBA_1010102_GrPixelConfig:      return "RGBA1010102";
+        case kRGBA_float_GrPixelConfig:        return "RGBAFloat";
+        case kAlpha_half_GrPixelConfig:        return "AlphaHalf";
+        case kAlpha_half_as_Red_GrPixelConfig: return "AlphaHalf_asRed";
+        case kRGBA_half_GrPixelConfig:         return "RGBAHalf";
+        case kRGB_ETC1_GrPixelConfig:          return "RGBETC1";
+        default:                               return "Unknown";
+    }
+}
+
+void GrTextureCacheInfo::dump() const {
+    SkDebugf("{ %d, %d, %d, %s, %d, %d, %s, %d, %s, %s, %s, %s, %zu }",
+             fRefCnt,
+             fPendingReads,
+             fPendingWrites,
+             GrPixelConfigToStr(fConfig),
+             fWidth,
+             fHeight,
+             fRenderable ? "GrRenderable::kYes" : "GrRenderable::kNo",
+             fSampleCount,
+             (GrMipMapped::kYes == fMipMapped) ? "GrMipMapped::kYes" : "GrMipMapped::kNo",
+             fHasScratchKey ? "true" : "false",
+             fIsWrapped ? "true" : "false",
+             (GrBudgetedType::kBudgeted == fIsBudgeted) ? "GrBudgetedType::kBudgeted" :
+                (GrBudgetedType::kUnbudgetedUncacheable == fIsBudgeted) ? "GrBudgetedType::kUnbudgetedUncacheable"
+                                                                        : "GrBudgetedType::kUnbudgetedCacheable",
+             fBytes);
+}
+
+void GrTextureCacheInfo::computeScratchKey(GrScratchKey* dst) const {
+    GrTexturePriv::ComputeScratchKey(fConfig, fWidth, fHeight, fRenderable,
+                                     fSampleCount, fMipMapped, dst);
+}
+
+void GrCacheState::dump() const {
+
+    SkString nonPurgeableName("nullptr");
+    if (fNumNonPurgeable) {
+        nonPurgeableName.printf("gNonPurgeable-%s", fName.c_str());
+
+        SkDebugf("const GrTextureCacheInfo %s[%d] = {\n",
+                 nonPurgeableName.c_str(),
+                 fNumNonPurgeable);
+        for (int i = 0; i < fNumNonPurgeable; ++i) {
+            const GrTextureCacheInfo* info = this->nonPurgeableResource(i);
+
+            info->dump();
+            SkDebugf(",\n");
+        }
+        SkDebugf("};\n");
+    }
+
+    SkString purgeableName("nullptr");
+    if (fNumPurgeable) {
+        purgeableName.printf("gPurgeable-%s", fName.c_str());
+
+        SkDebugf("const GrTextureCacheInfo gPurgeable-%s[%d] = {\n",
+                 purgeableName.c_str(),
+                 fNumPurgeable);
+        for (int i = 0; i < fNumPurgeable; ++i) {
+            const GrTextureCacheInfo* info = this->purgeableResource(i);
+
+            info->dump();
+            SkDebugf(",\n");
+        }
+        SkDebugf("};\n");
+    }
+
+    SkDebugf("GrCacheState state{ \"%s\", %d, %s, %zu, %d, %s, %zu }; // %d %zu\n",
+             fName.c_str(),
+             fNumNonPurgeable,
+             nonPurgeableName.c_str(),
+             fNonPurgeableBytes,
+             fNumPurgeable,
+             purgeableName.c_str(),
+             fPurgeableBytes,
+             fNumNonPurgeable + fNumPurgeable,
+             fNonPurgeableBytes + fPurgeableBytes);
+}
--- src/gpu/GrResourceCache.cpp
+++ src/gpu/GrResourceCache.cpp
@@ -906,3 +906,89 @@ bool GrResourceCache::isInCache(const GrGpuResource* resource) const {
 }
 
 #endif
+
+#include "GrRenderTarget.h"
+#include "GrHack.h"
+#include "GrTexturePriv.h"
+
+
+static void get_cache_info(GrSurface* s, GrTextureCacheInfo* dst) {
+    dst->fRefCnt = s->getRefCnt();
+    dst->fPendingReads = s->getPendingReads();
+    dst->fPendingWrites = s->getPendingWrites();
+    dst->fConfig = s->config();
+    dst->fWidth = s->width();
+    dst->fHeight = s->height();
+    dst->fRenderable = false;
+    dst->fSampleCount = 1;
+    dst->fMipMapped = GrMipMapped::kNo;
+
+    if (const GrRenderTarget* rt = s->asRenderTarget()) {
+        dst->fRenderable = true;
+        dst->fSampleCount = rt->numColorSamples();
+    }
+
+    if (const GrTexture* t = s->asTexture()) {
+        dst->fMipMapped = t->texturePriv().mipMapped();
+    }
+
+    GrScratchKey expected;
+    dst->computeScratchKey(&expected);
+
+    dst->fHasScratchKey = false;
+    GrScratchKey actual = s->resourcePriv().getScratchKey();
+    if (actual.isValid()) {
+        dst->fHasScratchKey = true;
+        SkASSERT(actual == expected);
+    }
+
+    dst->fIsWrapped = s->resourcePriv().refsWrappedObjects();
+    dst->fIsBudgeted = s->resourcePriv().budgetedType();
+
+    dst->fBytes = s->gpuMemorySize();
+}
+
+
+const GrCacheState* GrResourceCache::getCacheState(int id, const char* label) {
+    SkString name;
+    name.printf("%s-%d", id, label);
+
+    int numNonPurgeableTextures = 0;
+    for (int i = 0; i < fNonpurgeableResources.count(); ++i) {
+        if (fNonpurgeableResources[i]->asSurface()) {
+            numNonPurgeableTextures++;
+        }
+    }
+
+    int numPurgeableTextures = 0;
+    for (int i = 0; i < fPurgeableQueue.count(); ++i) {
+        if (fPurgeableQueue.at(i)->asSurface()) {
+            numPurgeableTextures++;
+        }
+    }
+
+    GrCacheState* state = new GrCacheState(name, numNonPurgeableTextures, numPurgeableTextures);
+
+    size_t bytes = 0;
+    int current = 0;
+    for (int i = 0; i < fNonpurgeableResources.count(); ++i) {
+        if (GrSurface* s = fNonpurgeableResources[i]->asSurface()) {
+            get_cache_info(s, state->nonPurgeableResource(current++));
+            bytes += s->gpuMemorySize();
+        }
+    }
+
+    state->setNonPurgeableBytes(bytes);
+
+    bytes = 0;
+    current = 0;
+    for (int i = 0; i < fPurgeableQueue.count(); ++i) {
+        if (GrSurface* s = fPurgeableQueue.at(i)->asSurface()) {
+            get_cache_info(s, state->purgeableResource(current++));
+            bytes += s->gpuMemorySize();
+        }
+    }
+
+    state->setPurgeableBytes(bytes);
+    return state;
+}
