574df84058f0f91df10056191b97949413b5e883
Herb Derby <herb@google.com>
2019-08-09 18:13:43 -0400

    Add tracing to diff canvas
    Change-Id: Ib604c80fb8c493863b08beaf825a1120e7659256

--- tests/SkRemoteGlyphCacheTest.cpp
+++ tests/SkRemoteGlyphCacheTest.cpp
@@ -218,12 +218,22 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(SkRemoteGlyphCache_StrikeSerialization, repor
     int glyphCount = 10;
     auto serverBlob = buildTextBlob(serverTf, glyphCount);
     auto props = FindSurfaceProps(ctxInfo.grContext());
-    SkTextBlobCacheDiffCanvas cache_diff_canvas(10, 10, props, &server,
-                                                MakeSettings(ctxInfo.grContext()));
-    cache_diff_canvas.drawTextBlob(serverBlob.get(), 0, 0, paint);
-
     std::vector<uint8_t> serverStrikeData;
-    server.writeStrikeData(&serverStrikeData);
+    std::unique_ptr<SkDynamicMemoryWStream> wStream = skstd::make_unique<SkDynamicMemoryWStream>();
+    std::unique_ptr<SkStreamAsset> stream;
+    {
+        SkTextBlobCacheDiffCanvas cacheDiffCanvas(10, 10, props, &server,
+                                                  MakeSettings(ctxInfo.grContext()));
+        SkDynamicMemoryWStream* streamPtr = wStream.get();
+        server.startBlobCapture(std::move(wStream));
+        cacheDiffCanvas.drawTextBlob(serverBlob.get(), 0, 0, paint);
+        auto keepAlive = server.finishBlobCapture();
+        stream = streamPtr->detachAsStream();
+        server.writeStrikeData(&serverStrikeData);
+    }
+
+    auto trace = server.CreateBlobTrace(stream.get());
+    REPORTER_ASSERT(reporter, trace.size() == 1);
 
     // Client.
     auto clientTf = client.deserializeTypeface(serverTfData->data(), serverTfData->size());
---
+++ tools/blob_cache_sim.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "include/core/SkColor.h"
+#include "include/core/SkStream.h"
+#include "src/core/SkRemoteGlyphCache.h"
+#include "src/core/SkScalerContext.h"
+
+int main(int argc, char** argv) {
+    std::unordered_map<uint64_t, uint32_t> counts;
+
+    std::vector<std::string> filenames;
+    for (int i = 1; i < argc; i++) {
+        filenames.emplace_back(argv[i]);
+    }
+
+    for (const auto& filename : filenames) {
+        SkFILEStream in{filename.c_str()};
+        auto trace = SkTextBlobCacheDiffCanvas::CreateBlobTrace(&in);
+        for (const auto& record : trace) {
+            const SkPaint paint = record.paint;
+            bool fastByPass = paint.getStyle() == SkPaint::kFill_Style
+                    && paint.getPathEffect() == nullptr
+                    && paint.getMaskFilter() == nullptr;
+            if (fastByPass) {
+                uint64_t blobID = record.origUniqueID;
+                SkPoint offset = record.offset;
+                SkColor c = SkMaskGamma::CanonicalColor(paint.getColor());
+                uint32_t colorBits =
+                        (SkColorGetR(c) >> 5u) << 6u
+                      | (SkColorGetG(c) >> 5u) << 3u
+                      | SkColorGetB(c) >> 5u;
+
+                SkFixed fx = (SkScalarToFixed(offset.x()) >> 13) & 7;
+                SkFixed fy = (SkScalarToFixed(offset.y()) >> 13) & 7;
+                uint32_t posBits = (fx << 3 | fy) << 12;
+
+                uint64_t blobKey = blobID << 32u | posBits | colorBits;
+                auto lookup = counts.find(blobKey);
+                if (lookup == counts.end()) {
+                    bool ok;
+                    std::tie(lookup, ok) = counts.insert({blobKey, 0});
+                    SkASSERT(ok);
+                }
+                lookup->second += 1;
+                std::cout << std::hex << blobKey << "\n";
+            }
+        }
+    }
+
+    int oneCount = 0;
+    int moreCounts = 0;
+    for (const auto& t : counts) {
+        uint64_t id; uint32_t count;
+        std::tie(id, count) = t;
+        if (count == 1) {
+            oneCount += 1;
+        } else {
+            moreCounts += count;
+        }
+    }
+
+    std::cerr << "ones: " << oneCount << " more: " << moreCounts << "\n";
+    return 0;
+}
