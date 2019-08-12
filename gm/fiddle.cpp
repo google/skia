4fdf107d65655eb338b1106afe2480e9e86d6fc5
Mike Klein <mtklein@google.com>
2019-08-12 13:11:26 -0400

    add GM for skbug.com/9331
    Bug: skia:9331
    Change-Id: I1798503c0e5956ec767d8e0021ccca7a1d2a8ac3

--- gm/fiddle.cpp
+++ gm/fiddle.cpp
@@ -12,4 +12,29 @@ static void draw(SkCanvas* canvas);
 DEF_SIMPLE_GM(fiddle, canvas, 256, 256) { draw(canvas); }
 
 // Paste your fiddle.skia.org code over this stub.
-void draw(SkCanvas*) {}
+void draw(SkCanvas* canvas) {
+    SkPaint p;
+
+    float vals[] = { 13, 17 };
+    p.setPathEffect(SkDashPathEffect::Make(vals, 2, 9));
+
+    SkRect r = { 50, 50, 150, 150 };
+    SkRect clip = {0, 0, 200, 150};
+
+    p.setColor(0x44FF0000);
+    canvas->drawRect(clip, p);
+
+    p.setColor(0xFF000000);
+    p.setStrokeWidth(10);
+    p.setStyle(SkPaint::kStroke_Style);
+
+    canvas->save();
+    canvas->clipRect(clip);
+    canvas->drawRect(r, p);
+    canvas->restore();
+
+    canvas->save();
+    canvas->clipRect(clip.makeOffset(0, 150));
+    canvas->drawRect(r, p);
+    canvas->restore();
+}
