1621e832562ad3227f4097aaeaef3bc7711eeeeb
Stephen White <senorblanco@chromium.org>
2019-08-12 10:20:17 -0400

    Dawn: implement textures & samplers.
    First draft of textures, samplers, texture uploads, texture render
    targets, GrDawnTextureCommandBuffer, wrapped backend textures,
    readPixels().
    Add samplers to Dawn binding group layout.
    Convert filter modes from Gr to Dawn.
    Rename fUniformBindGroup to fBindGroup (since it does more than uniforms now).
    Split combined samplers into separate textures and samplers bindings;
    recombine in the shader with makeSampler().
    Create complete bind group layout before creating UBOs.
    Implement scissor rect.
    Also move all cache registration into Make functions.
    Implement texture copy as texture -> buffer -> texture (for now).
    Implement 4444, 565 and Gray8 pixel configs via conversion to RGBA_8888.
    Set blend color in command buffer.
    Pass shader caps to SkSL.
    Enable flat interpolation, integer support, derivative support in Dawn Caps.
    Reduce the Dawn caps to 6 fragment/sampler combos.
    Add support for Int (1) vertex format.
    
    Change-Id: Id5e9060855f0dfc8c071f84ea6c456aba2a72b35

--- src/gpu/dawn/GrDawnCaps.cpp
+++ src/gpu/dawn/GrDawnCaps.cpp
@@ -8,11 +8,21 @@
 #include "src/gpu/dawn/GrDawnCaps.h"
 
 GrDawnCaps::GrDawnCaps(const GrContextOptions& contextOptions) : INHERITED(contextOptions) {
+    fMipMapSupport = true;
     fBufferMapThreshold = SK_MaxS32;  // FIXME: get this from Dawn?
     fShaderCaps.reset(new GrShaderCaps(contextOptions));
-    fMaxTextureSize = 2048; // FIXME
+    fMaxTextureSize = fMaxRenderTargetSize = 4096; // FIXME
     fMaxVertexAttributes = 16; // FIXME
+    fClampToBorderSupport = false;
     fPerformPartialClearsAsDraws = true;
+
+    fShaderCaps->fFlatInterpolationSupport = true;
+    fShaderCaps->fIntegerSupport = true;
+    // FIXME: each fragment sampler takes two binding slots in Dawn (sampler + texture). Limit to
+    // 6 * 2 = 12, since kMaxBindingsPerGroup is 16 in Dawn, and we need to keep a few for
+    // non-texture bindings. Eventually, we may be able to increase kMaxBindingsPerGroup in Dawn.
+    fShaderCaps->fMaxFragmentSamplers = 6;
+    fShaderCaps->fShaderDerivativeSupport = true;
 
     this->applyOptionsOverrides(contextOptions);
     fShaderCaps->applyOptionsOverrides(contextOptions);
@@ -30,6 +40,9 @@ bool GrDawnCaps::isConfigTexturable(GrPixelConfig config) const {
     switch (config) {
         case kRGBA_8888_GrPixelConfig:
         case kBGRA_8888_GrPixelConfig:
+        case kRGBA_4444_GrPixelConfig:
+        case kRGB_565_GrPixelConfig:
+        case kGray_8_GrPixelConfig:
         case kAlpha_8_GrPixelConfig:
             return true;
         default:
