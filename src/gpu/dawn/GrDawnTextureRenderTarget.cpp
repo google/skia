7b86f4901f650f613e8ffd1ca6143dc3f06c1b26
Stephen White <senorblanco@chromium.org>
2019-08-12 12:57:21 -0400

    First implementation of GrTextureRenderTarget.
    Change-Id: I0e6bc516eb43e7f7062f9d19e7ef8093324b551d

---
+++ src/gpu/dawn/GrDawnTextureRenderTarget.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/dawn/GrDawnTextureRenderTarget.h"
+
+#include "include/core/SkTraceMemoryDump.h"
+#include "include/gpu/GrContext.h"
+#include "src/gpu/GrTexturePriv.h"
+#include "src/gpu/dawn/GrDawnGpu.h"
+
+GrDawnTextureRenderTarget::GrDawnTextureRenderTarget(GrDawnGpu* gpu,
+                                                   dawn::Texture texture,
+                                                   dawn::TextureView textureView,
+                                                   const GrSurfaceDesc& desc,
+                                                   int sampleCnt,
+                                                   const GrDawnImageInfo& info,
+                                                   GrMipMapsStatus mipMapsStatus)
+        : GrSurface(gpu, {desc.fWidth, desc.fHeight}, desc.fConfig, GrProtected::kNo)
+        , GrDawnTexture(gpu, texture, textureView, desc, info, mipMapsStatus)
+        , GrDawnRenderTarget(gpu, desc, sampleCnt, info) {
+}
+
+bool GrDawnTextureRenderTarget::canAttemptStencilAttachment() const {
+    return true;
+}
+
+size_t GrDawnTextureRenderTarget::onGpuMemorySize() const {
+    return GrSurface::ComputeSize(this->config(), this->width(), this->height(),
+                                  1, // F(XME: for MSAA
+                                  this->texturePriv().mipMapped());
+}
