3e059d2eeab6398a24b2000a28563485d9bb4edd
Michael Ludwig <michaelludwig@google.com>
2019-08-11 20:47:23 -0400

    Improve SkImageFilter_Base::Context
    This moves OutputProperties' fields into Context and decouples SkSpecialImage
    makeSurface() functions from SkImageFilter_Base.
    
    It also folds the source SkSpecialImage, previously passed in filterImage(),
    into the new Context type. This allows Context to provide a convenient
    makeSurface() function and it (IMO) improves readability in implementations
    by differentiating 'src' from 'input' images.
    
    Also add some documentation to the context member functions, although a
    follow-up CL is needed to make their return types encode some of the
    coordinate system information that the comments talk about. As part of this
    clean up, it renames ctm() to layerMatrix() to make it clearer that it's not
    necessarily the ctm of the SkCanvas (although ctm() is kept for backwards
    compatibility).
    
    Bug: skia:9282
    Change-Id: I3536a9fd4c2b175ac54ef0f5480ea4304d890bba

--- src/core/SkSpecialImage.h
+++ src/core/SkSpecialImage.h
@@ -11,7 +11,6 @@
 #include "include/core/SkImageInfo.h"
 #include "include/core/SkRefCnt.h"
 #include "include/core/SkSurfaceProps.h"
-#include "src/core/SkImageFilter_Base.h"
 #include "src/core/SkNextID.h"
 
 class GrRecordingContext;
@@ -19,7 +18,6 @@ class GrTextureProxy;
 class SkBitmap;
 class SkCanvas;
 class SkImage;
-struct SkImageInfo;
 class SkPaint;
 class SkPixmap;
 class SkSpecialSurface;
@@ -91,7 +89,8 @@ public:
     /**
      *  Create a new special surface with a backend that is compatible with this special image.
      */
-    sk_sp<SkSpecialSurface> makeSurface(const SkImageFilter_Base::OutputProperties& outProps,
+    sk_sp<SkSpecialSurface> makeSurface(SkColorType colorType,
+                                        const SkColorSpace* colorSpace,
                                         const SkISize& size,
                                         SkAlphaType at = kPremul_SkAlphaType,
                                         const SkSurfaceProps* props = nullptr) const;
@@ -99,8 +98,11 @@ public:
     /**
      * Create a new surface with a backend that is compatible with this special image.
      * TODO: switch this to makeSurface once we resolved the naming issue
+     * TODO (michaelludwig) - This is only used by SkTileImageFilter, which appears should be
+     * updated to work correctly with subsets and then makeTightSurface() can go away entirely.
      */
-    sk_sp<SkSurface> makeTightSurface(const SkImageFilter_Base::OutputProperties& outProps,
+    sk_sp<SkSurface> makeTightSurface(SkColorType colorType,
+                                      const SkColorSpace* colorSpace,
                                       const SkISize& size,
                                       SkAlphaType at = kPremul_SkAlphaType) const;
 
