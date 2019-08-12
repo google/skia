d870af23834448d6d20fa45275ad38c2b30ddd16
Mike Klein <mtklein@google.com>
2019-08-12 13:11:26 -0400

    add GM for skbug.com/9331
    Bug: skia:9331
    Change-Id: I1798503c0e5956ec767d8e0021ccca7a1d2a8ac3

---
+++ gm/bug9331.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm/gm.h"
+#include "include/core/SkCanvas.h"
+#include "include/effects/SkDashPathEffect.h"
+
+// Reproduces skbug.com/9331, drawing differently in debug and release builds.
+DEF_SIMPLE_GM(bug9331, canvas, 256, 256) {
+    SkPaint p;
+
+    float vals[] = { 13, 17 };
+    p.setPathEffect(SkDashPathEffect::Make(vals, 2, 9));
+
+    SkRect r = { 50, 50, 150, 150 };
+    SkRect clip = {0, 0, 200, 150};
+
+    p.setColor(0x44FF0000);
+    canvas->drawRect(clip, p);
+
+    p.setColor(0xFF000000);
+    p.setStrokeWidth(10);
+    p.setStyle(SkPaint::kStroke_Style);
+
+    canvas->save();
+        canvas->clipRect(clip);
+        canvas->drawRect(r, p);
+    canvas->restore();
+
+    canvas->save();
+        canvas->clipRect(clip.makeOffset(0, 150));
+        canvas->drawRect(r, p);
+    canvas->restore();
+}
