2957d14c75c460e617d05cd4fdc3cc94c6f1d6ed
Brian Osman <brianosman@google.com>
2019-08-13 16:19:25 -0400

    Add SkImage::makeWithAssignedColorSpace
    This reinterprets the contents of an image as though they were in a
    different color space. Helpful for tools and debug visualization.
    
    Change-Id: Ia8739bbd73d72249b8bee9d51dfa11d560a2a1f5

--- gm/makecolorspace.cpp
+++ gm/makecolorspace.cpp
@@ -43,7 +43,7 @@ sk_sp<SkImage> make_color_space(sk_sp<SkImage> orig, sk_sp<SkColorSpace> colorSp
     if (colorSpace->gammaIsLinear()) {
         srgb = SkColorSpace::MakeSRGBLinear();
     }
-    return SkImageMakeRasterCopyAndAssignColorSpace(xform.get(), srgb.get());
+    return xform->reinterpretColorSpace(std::move(srgb));
 }
 
 class MakeCSGM : public skiagm::GM {
