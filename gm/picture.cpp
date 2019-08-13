f97a2d884bc68414838c0f3c9157db0479716852
Yuqian Li <liyuqian@google.com>
2019-08-13 13:11:58 -0700

    Test serializing pictures with cull rects
    Bug: skia:9334
    Change-Id: I54a70c529b520538a3c8050a5592bbf419dd1890

--- gm/picture.cpp
+++ gm/picture.cpp
@@ -90,4 +90,60 @@ private:
     typedef skiagm::GM INHERITED;
 };
 
+// Exercise drawing a picture with a cull rect of non-zero top-left corner.
+//
+// See skbug.com/9334, which would fail
+// ```
+//   dm -m picture_cull_rect --config serialize-8888
+// ```
+// until that bug is fixed.
+class PictureCullRectGM : public skiagm::GM {
+public:
+    PictureCullRectGM()
+        : fPicture(nullptr)
+    {}
+
+protected:
+    void onOnceBeforeDraw() override {
+        SkPictureRecorder rec;
+        SkRTreeFactory rtreeFactory;
+        SkCanvas* canvas = rec.beginRecording(100, 100, &rtreeFactory);
+
+        SkPaint paint;
+        paint.setAntiAlias(false);
+
+        SkRect rect = SkRect::MakeLTRB(0, 80, 100, 100);
+
+        // Make picture complex enough to trigger the cull rect and bbh (RTree) computations.
+        // (A single drawRect won't trigger it.)
+        paint.setColor(0x800000FF);
+        canvas->drawRect(rect, paint);
+        canvas->drawOval(rect, paint);
+
+        fPicture = rec.finishRecordingAsPicture();
+
+        SkASSERT(fPicture->cullRect().top() == 80);
+    }
+
+    SkString onShortName() override {
+        return SkString("picture_cull_rect");
+    }
+
+    SkISize onISize() override {
+        return SkISize::Make(120, 120);
+    }
+
+    void onDraw(SkCanvas* canvas) override {
+        canvas->clipRect(SkRect::MakeLTRB(0, 60, 120, 120));
+        canvas->translate(10, 10);
+        canvas->drawPicture(fPicture);
+    }
+
+private:
+    sk_sp<SkPicture> fPicture;
+
+    typedef skiagm::GM INHERITED;
+};
+
 DEF_GM(return new PictureGM;)
+DEF_GM(return new PictureCullRectGM;)
