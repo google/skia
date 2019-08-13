289bca57b199f753fae9748d534b021cb75c54ce
Robert Phillips <robertphillips@google.com>
2019-08-13 13:12:37 -0400

    [M77] Add GR_GL_LUMINANCE8 to get_yuva_config
    This parallels a related CL (https://skia-review.googlesource.com/c/skia/+/232397) which added kLUMINANCE8 handling to GrGLCaps::getYUVAColorTypeFromBackendFormat.
    
    It cherry-picks the following CL back to M77:
    
    https://skia-review.googlesource.com/c/skia/+/231417 (Make GrGLCaps::getYUVAColorTypeFromBackendFormat also map R8 to GrColorType::kGray_8)
    
    and updates the GrGLCaps method get_yuva_config to match. This method no longer exists at ToT Skia so this CL can't be landed at ToT.
    
    The changes to wacky_yuv_formats.cpp are local to Skia - that file is not used/compiled by Chrome.
    
    Bug: 992220
    Change-Id: I1d98ca15adc7fa2a6b56c438699fc089c8e2c22a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/234281
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- gm/wacky_yuv_formats.cpp
+++ gm/wacky_yuv_formats.cpp
@@ -163,7 +163,7 @@ static void setup_yuv_indices(YUVFormat yuvFormat, bool addExtraAlpha, SkYUVAInd
             break;
         case kNV12_YUVFormat:
             yuvaIndices[0].fIndex = 0;
-            yuvaIndices[0].fChannel = SkColorChannel::kA;
+            yuvaIndices[0].fChannel = SkColorChannel::kR;
             yuvaIndices[1].fIndex = 1;
             yuvaIndices[1].fChannel = SkColorChannel::kR;
             yuvaIndices[2].fIndex = 1;
@@ -177,7 +177,7 @@ static void setup_yuv_indices(YUVFormat yuvFormat, bool addExtraAlpha, SkYUVAInd
             break;
         case kNV21_YUVFormat:
             yuvaIndices[0].fIndex = 0;
-            yuvaIndices[0].fChannel = SkColorChannel::kA;
+            yuvaIndices[0].fChannel = SkColorChannel::kR;
             yuvaIndices[1].fIndex = 1;
             yuvaIndices[1].fChannel = SkColorChannel::kG;
             yuvaIndices[2].fIndex = 1;
@@ -191,11 +191,11 @@ static void setup_yuv_indices(YUVFormat yuvFormat, bool addExtraAlpha, SkYUVAInd
             break;
         case kI420_YUVFormat:
             yuvaIndices[0].fIndex = 0;
-            yuvaIndices[0].fChannel = SkColorChannel::kA;
+            yuvaIndices[0].fChannel = SkColorChannel::kR;
             yuvaIndices[1].fIndex = 1;
-            yuvaIndices[1].fChannel = SkColorChannel::kA;
+            yuvaIndices[1].fChannel = SkColorChannel::kR;
             yuvaIndices[2].fIndex = 2;
-            yuvaIndices[2].fChannel = SkColorChannel::kA;
+            yuvaIndices[2].fChannel = SkColorChannel::kR;
             if (addExtraAlpha) {
                 yuvaIndices[3].fIndex = 3;
                 yuvaIndices[3].fChannel = SkColorChannel::kA;
@@ -205,11 +205,11 @@ static void setup_yuv_indices(YUVFormat yuvFormat, bool addExtraAlpha, SkYUVAInd
             break;
         case kYV12_YUVFormat:
             yuvaIndices[0].fIndex = 0;
-            yuvaIndices[0].fChannel = SkColorChannel::kA;
+            yuvaIndices[0].fChannel = SkColorChannel::kR;
             yuvaIndices[1].fIndex = 2;
-            yuvaIndices[1].fChannel = SkColorChannel::kA;
+            yuvaIndices[1].fChannel = SkColorChannel::kR;
             yuvaIndices[2].fIndex = 1;
-            yuvaIndices[2].fChannel = SkColorChannel::kA;
+            yuvaIndices[2].fChannel = SkColorChannel::kR;
             if (addExtraAlpha) {
                 yuvaIndices[3].fIndex = 3;
                 yuvaIndices[3].fChannel = SkColorChannel::kA;
@@ -469,12 +469,18 @@ static void extract_planes(const SkBitmap& bm, SkYUVColorSpace yuvColorSpace, Pl
     SkASSERT(!(bm.width() % 2));
     SkASSERT(!(bm.height() % 2));
 
-    planes->fYFull.allocPixels(SkImageInfo::MakeA8(bm.width(), bm.height()));
-    planes->fUFull.allocPixels(SkImageInfo::MakeA8(bm.width(), bm.height()));
-    planes->fVFull.allocPixels(SkImageInfo::MakeA8(bm.width(), bm.height()));
-    planes->fAFull.allocPixels(SkImageInfo::MakeA8(bm.width(), bm.height()));
-    planes->fUQuarter.allocPixels(SkImageInfo::MakeA8(bm.width()/2, bm.height()/2));
-    planes->fVQuarter.allocPixels(SkImageInfo::MakeA8(bm.width()/2, bm.height()/2));
+    planes->fYFull.allocPixels(SkImageInfo::Make(bm.width(), bm.height(), kGray_8_SkColorType,
+                                                 kPremul_SkAlphaType));
+    planes->fUFull.allocPixels(SkImageInfo::Make(bm.width(), bm.height(), kGray_8_SkColorType,
+                                                 kPremul_SkAlphaType));
+    planes->fVFull.allocPixels(SkImageInfo::Make(bm.width(), bm.height(), kGray_8_SkColorType,
+                                                 kPremul_SkAlphaType));
+    planes->fAFull.allocPixels(SkImageInfo::Make(bm.width(), bm.height(), kAlpha_8_SkColorType,
+                                                 kPremul_SkAlphaType));
+    planes->fUQuarter.allocPixels(SkImageInfo::Make(bm.width()/2, bm.height()/2,
+                                                    kGray_8_SkColorType, kPremul_SkAlphaType));
+    planes->fVQuarter.allocPixels(SkImageInfo::Make(bm.width() / 2, bm.height() / 2,
+                                                    kGray_8_SkColorType, kPremul_SkAlphaType));
 
     for (int y = 0; y < bm.height(); ++y) {
         for (int x = 0; x < bm.width(); ++x) {
@@ -691,7 +697,7 @@ static uint8_t look_up(float x1, float y1, const SkBitmap& bm, SkColorChannel ch
     int x = SkScalarFloorToInt(x1 * bm.width());
     int y = SkScalarFloorToInt(y1 * bm.height());
 
-    if (kAlpha_8_SkColorType == bm.colorType()) {
+    if (kAlpha_8_SkColorType == bm.colorType() || kGray_8_SkColorType == bm.colorType()) {
         SkASSERT(SkColorChannel::kA == channel || SkColorChannel::kR == channel);
         result = *bm.getAddr8(x, y);
     } else if (kRGBA_8888_SkColorType == bm.colorType()) {
@@ -1013,7 +1019,7 @@ static void make_R_16(const GrCaps* caps,
                       GrBackendFormat* format,
                       size_t* rowBytes) {
     SkASSERT(kP016_YUVFormat == yuvFormat || kP010_YUVFormat == yuvFormat);
-    SkASSERT(kAlpha_8_SkColorType == bm.colorType());
+    SkASSERT(kGray_8_SkColorType == bm.colorType() || kAlpha_8_SkColorType == bm.colorType());
 
     uint16_t y16;
     *rowBytes = sizeof(uint16_t) * bm.width();
@@ -1213,6 +1219,7 @@ protected:
                             // know about the intended domain and the domain padding bleeds in
                             counterMod = 1;
                         }
+                        counterMod = 1;
                         switch (counterMod) {
                         case 0:
                             fImages[opaque][cs][format] = SkImage::MakeFromYUVATexturesCopy(
