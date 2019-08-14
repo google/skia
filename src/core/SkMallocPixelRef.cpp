7f8478e3159087567fb715d2149844e163121294
Hal Canary <halcanary@google.com>
2019-08-14 12:19:20 -0400

    SkMallocPixelRef: remove MakeDirect and MakeWithProc from API.
    Not currently used by any clients.
    
    Change-Id: I21d554fb95ffef4f317945ab22c4cfb14fbb6b2a

--- src/core/SkMallocPixelRef.cpp
+++ src/core/SkMallocPixelRef.cpp
@@ -40,16 +40,6 @@ static bool is_valid(const SkImageInfo& info) {
     return true;
 }
 
-sk_sp<SkPixelRef> SkMallocPixelRef::MakeDirect(const SkImageInfo& info,
-                                               void* addr,
-                                               size_t rowBytes) {
-    if (!is_valid(info)) {
-        return nullptr;
-    }
-    return sk_make_sp<SkPixelRef>(info.width(), info.height(), addr, rowBytes);
-}
-
-
 sk_sp<SkPixelRef> SkMallocPixelRef::MakeAllocate(const SkImageInfo& info, size_t rowBytes) {
     if (rowBytes == 0) {
         rowBytes = info.minRowBytes();
@@ -76,33 +66,6 @@ sk_sp<SkPixelRef> SkMallocPixelRef::MakeAllocate(const SkImageInfo& info, size_t
         ~PixelRef() override { sk_free(this->pixels()); }
     };
     return sk_sp<SkPixelRef>(new PixelRef(info.width(), info.height(), addr, rowBytes));
-}
-
-sk_sp<SkPixelRef> SkMallocPixelRef::MakeWithProc(const SkImageInfo& info,
-                                                 size_t rowBytes,
-                                                 void* addr,
-                                                 SkMallocPixelRef::ReleaseProc proc,
-                                                 void* context) {
-    if (!is_valid(info)) {
-        if (proc) {
-            proc(addr, context);
-        }
-        return nullptr;
-    }
-
-    struct PixelRef final : public SkPixelRef {
-        SkMallocPixelRef::ReleaseProc fReleaseProc;
-        void* fReleaseProcContext;
-        PixelRef(int w, int h, void* s, size_t r, SkMallocPixelRef::ReleaseProc proc, void* ctx)
-            : SkPixelRef(w, h, s, r), fReleaseProc(proc), fReleaseProcContext(ctx) {}
-        ~PixelRef() override {
-            if (fReleaseProc) {
-                fReleaseProc(this->pixels(), fReleaseProcContext);
-            }
-        }
-    };
-    return sk_sp<SkPixelRef>(new PixelRef(info.width(), info.height(), addr, rowBytes,
-                                          proc, context));
 }
 
 sk_sp<SkPixelRef> SkMallocPixelRef::MakeWithData(const SkImageInfo& info,
