8a836f4de5755d3aae7e4ced0d5fdd557c4986fe
Michael Ludwig <michaelludwig@google.com>
2019-08-14 11:52:35 -0400

    Move the source image into Context
    This folds the source SkSpecialImage, that was previously passed in as
    an argument to filterImage(), into the filtering Context. This allows
    the Context to provide convenient access to backend properties that
    are only accessible through the source image, but makes it clear in
    calling code that the actual pixels of the source image are not needed.
    
    IMO this improves readability in implementations that previously had
    functions that would take a 'src' and an 'input' image, but would only
    really process the input image's pixels.
    
    Bug: skia:9282
    Change-Id: I42437dcc6819e3d3882bff7851f9b16277a69f06

--- src/effects/imagefilters/SkComposeImageFilter.cpp
+++ src/effects/imagefilters/SkComposeImageFilter.cpp
@@ -25,8 +25,7 @@ public:
     SkRect computeFastBounds(const SkRect& src) const override;
 
 protected:
-    sk_sp<SkSpecialImage> onFilterImage(SkSpecialImage* source, const Context&,
-                                        SkIPoint* offset) const override;
+    sk_sp<SkSpecialImage> onFilterImage(const Context&, SkIPoint* offset) const override;
     SkIRect onFilterBounds(const SkIRect&, const SkMatrix& ctm,
                            MapDirection, const SkIRect* inputRect) const override;
     bool onCanHandleComplexCTM() const override { return true; }
@@ -72,8 +71,7 @@ SkRect SkComposeImageFilterImpl::computeFastBounds(const SkRect& src) const {
     return outer->computeFastBounds(inner->computeFastBounds(src));
 }
 
-sk_sp<SkSpecialImage> SkComposeImageFilterImpl::onFilterImage(SkSpecialImage* source,
-                                                              const Context& ctx,
+sk_sp<SkSpecialImage> SkComposeImageFilterImpl::onFilterImage(const Context& ctx,
                                                               SkIPoint* offset) const {
     // The bounds passed to the inner filter must be filtered by the outer
     // filter, so that the inner filter produces the pixels that the outer
@@ -82,9 +80,9 @@ sk_sp<SkSpecialImage> SkComposeImageFilterImpl::onFilterImage(SkSpecialImage* so
     innerClipBounds = this->getInput(0)->filterBounds(ctx.clipBounds(), ctx.ctm(),
                                                       kReverse_MapDirection, &ctx.clipBounds());
     Context innerContext(ctx.ctm(), innerClipBounds, ctx.cache(), ctx.colorType(),
-                         ctx.colorSpace());
+                         ctx.colorSpace(), ctx.sourceImage());
     SkIPoint innerOffset = SkIPoint::Make(0, 0);
-    sk_sp<SkSpecialImage> inner(this->filterInput(1, source, innerContext, &innerOffset));
+    sk_sp<SkSpecialImage> inner(this->filterInput(1, innerContext, &innerOffset));
     if (!inner) {
         return nullptr;
     }
@@ -93,10 +91,11 @@ sk_sp<SkSpecialImage> SkComposeImageFilterImpl::onFilterImage(SkSpecialImage* so
     outerMatrix.postTranslate(SkIntToScalar(-innerOffset.x()), SkIntToScalar(-innerOffset.y()));
     SkIRect clipBounds = ctx.clipBounds();
     clipBounds.offset(-innerOffset.x(), -innerOffset.y());
-    Context outerContext(outerMatrix, clipBounds, ctx.cache(), ctx.colorType(), ctx.colorSpace());
+    Context outerContext(outerMatrix, clipBounds, ctx.cache(), ctx.colorType(), ctx.colorSpace(),
+                         inner.get());
 
     SkIPoint outerOffset = SkIPoint::Make(0, 0);
-    sk_sp<SkSpecialImage> outer(this->filterInput(0, inner.get(), outerContext, &outerOffset));
+    sk_sp<SkSpecialImage> outer(this->filterInput(0, outerContext, &outerOffset));
     if (!outer) {
         return nullptr;
     }
