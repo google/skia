d18f0f2211667ed85fa5ad2de3bfe7818a63ac8a
Ethan Nicholas <ethannicholas@google.com>
2019-08-15 16:29:38 -0400

    SkSL sample() now permits specification of coordinates
    Bug: skia:
    Change-Id: I16073008ac852f1864bd1d2bd38087a5b661d05a

--- src/gpu/vk/GrVkUniformHandler.h
+++ src/gpu/vk/GrVkUniformHandler.h
@@ -73,6 +73,10 @@ private:
                                           int arrayCount,
                                           const char** outName) override;
 
+    void updateUniformVisibility(UniformHandle u, uint32_t visibility) override {
+        fUniforms[u.toIndex()].fVisibility |= visibility;
+    }
+
     SamplerHandle addSampler(const GrTexture* texture,
                              const GrSamplerState&,
                              const GrSwizzle&,
