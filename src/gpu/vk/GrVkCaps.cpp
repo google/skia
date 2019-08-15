f80454b94a0548ceee827a2d6236cc0b41b49cd7
Ethan Nicholas <ethannicholas@google.com>
2019-08-15 12:36:58 -0400

    Revert "Revert "unified Vulkan uniform buffers""
    This reverts commit 19754b547023accf29f6305108a70538c3d2e532.
    
    Bug: skia:
    Change-Id: Ie531a962934ee1909fecda59a5ee48c98e66f898

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -16,6 +16,7 @@
 #include "src/gpu/vk/GrVkCaps.h"
 #include "src/gpu/vk/GrVkInterface.h"
 #include "src/gpu/vk/GrVkTexture.h"
+#include "src/gpu/vk/GrVkUniformHandler.h"
 #include "src/gpu/vk/GrVkUtil.h"
 
 #ifdef SK_BUILD_FOR_ANDROID
@@ -1553,6 +1554,14 @@ GrCaps::SupportedRead GrVkCaps::onSupportedReadPixelsColorType(
         }
     }
     return {GrColorType::kUnknown, 0};
+}
+
+int GrVkCaps::getFragmentUniformBinding() const {
+    return GrVkUniformHandler::kUniformBinding;
+}
+
+int GrVkCaps::getFragmentUniformSet() const {
+    return GrVkUniformHandler::kUniformBufferDescSet;
 }
 
 #if GR_TEST_UTILS
