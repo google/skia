41108570c975931b4dbeb89bfc78858e1c06199a
Hal Canary <halcanary@google.com>
2019-08-19 15:04:24 -0400

    SkQP: split make_apk script into create_apk and make_apk
    gn_to_bp:  wrap defines in ifndef
    
    create_apk.py assumes you are either run from the aosp tree, or you are
    being run from make_universal_apk.py, which now defers to create_apk.py
    for all functionality, even argument parsing.
    
    tools/skqp/generate_gn_args moved some functionity into skqp_gn_args.py,
    wich is now used by create_apk.py
    
    create_apk now accepts android sdk license for you.
    
    create_apk and make_universal_apk.py now are better about cleaning up
    after expections happen.
    
    Old script make_apk.sh now just points at make_universal_apk.py
    
    Change-Id: I2dba20ef7017987cabb2bd49f070e2b1594785d5

--- tools/skqp/make_apk.sh
+++ tools/skqp/make_apk.sh
@@ -1,49 +1,5 @@
 #! /bin/sh
-
-# Copyright 2018 Google Inc.
+# Copyright 2019 Google Inc.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-set -e
-
-ANDROID_NDK="$1"
-
-if ! [ -d "$ANDROID_NDK" ] || ! [ -x "${ANDROID_NDK}/ndk-build" ]; then
-    printf "\nUsage:\n  %s ANDROID_NDK_PATH\n" "$0" >&2
-    exit 1
-fi
-
-case ":${PATH}:" in
-    */depot_tools:*) ;;
-    *)
-        printf '\ndepot_tools should be in your $PATH.\n' >&2
-        exit 1;;
-esac
-
-if ! [ -d "$ANDROID_HOME" ] || ! [ -x "${ANDROID_HOME}/platform-tools/adb" ]; then
-    printf '\n$ANDROID_HOME not set or is broken.\n' >&2
-    exit 1
-fi
-
-set -x
-
-ARCH=${SKQP_ARCH:-arm}
-
-cd "$(dirname "$0")/../.."
-
-BUILD=out/skqp-${ARCH}
-
-python tools/skqp/generate_gn_args $BUILD "$ANDROID_NDK" $ARCH
-
-GIT_SYNC_DEPS_QUIET=Y tools/git-sync-deps
-
-bin/gn gen $BUILD
-
-rm -rf $BUILD/gen
-
-platform_tools/android/bin/android_build_app -C $BUILD skqp
-
-set +x
-
-printf '\n\nAPK built: "%s/skqp.apk"\n\n' "$(pwd)/$BUILD"
-
+exec python "$(dirname "$0")"/make_universal_apk.py "$@"
