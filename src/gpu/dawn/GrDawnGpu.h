77147eba3841beabbeba35d9d2d6390c83a12b61
Stephen White <senorblanco@chromium.org>
2019-08-20 16:21:27 -0400

    Dawn: implement a ring buffer for uniform uploads.
    GrDawnGpu vends ring buffer slices out of a persistent (larger) buffer.
    
    GrDawnProgramDataManager::setData() now returns a BindGroup containing
    the (possibly new) UBO bindings, as well as the texture and sampler
    bindings.
    Change-Id: Id6694d6f44a815cfbffe4293779bf9bf558a2365

--- src/gpu/dawn/GrDawnGpu.h
+++ src/gpu/dawn/GrDawnGpu.h
@@ -10,6 +10,7 @@
 
 #include "src/gpu/GrGpu.h"
 #include "dawn/dawncpp.h"
+#include "src/gpu/dawn/GrDawnRingBuffer.h"
 
 class GrPipeline;
 class GrDawnGpuRTCommandBuffer;
@@ -82,6 +83,8 @@ public:
 
     sk_sp<GrSemaphore> prepareTextureForCrossContextUsage(GrTexture*) override;
 
+    GrDawnRingBuffer::Slice allocateUniformRingBufferSlice(int size);
+
 private:
     void onResetContext(uint32_t resetBits) override {}
 
@@ -147,6 +150,7 @@ private:
     std::unique_ptr<SkSL::Compiler>                 fCompiler;
     std::unique_ptr<GrDawnGpuRTCommandBuffer>       fRTCommandBuffer;
     std::unique_ptr<GrDawnGpuTextureCommandBuffer>  fTextureCommandBuffer;
+    GrDawnRingBuffer                                fUniformRingBuffer;
 
     typedef GrGpu INHERITED;
 };
