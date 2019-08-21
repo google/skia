fb96b754b8b6795a318aec76824d1a861f28c360
Ben Wagner <bungeman@google.com>
2019-08-21 13:50:33 -0400

    Export only public parts of GrTextureContext.
    Don't export the private bits.
    
    Change-Id: I09cb276cb1a350d19d4c3b4383a9b94d65276528

--- src/gpu/GrTextureContext.h
+++ src/gpu/GrTextureContext.h
@@ -9,12 +9,11 @@
 #define GrTextureContext_DEFINED
 
 #include "src/gpu/GrSurfaceContext.h"
-#include "src/gpu/GrTextureOpList.h"
-#include "src/gpu/GrTextureProxy.h"
 
 class GrContext;
 class GrDrawingManager;
 class GrSurface;
+class GrTextureOpList;
 class GrTextureProxy;
 struct SkIPoint;
 struct SkIRect;
@@ -23,29 +22,29 @@ struct SkIRect;
  * A helper object to orchestrate commands (currently just copies) for GrSurfaces that are
  * GrTextures and not GrRenderTargets.
  */
-class SK_API GrTextureContext : public GrSurfaceContext {
+class GrTextureContext : public GrSurfaceContext {
 public:
-    ~GrTextureContext() override;
+    SK_API ~GrTextureContext() override;
 
-    GrSurfaceProxy* asSurfaceProxy() override { return fTextureProxy.get(); }
-    const GrSurfaceProxy* asSurfaceProxy() const override { return fTextureProxy.get(); }
-    sk_sp<GrSurfaceProxy> asSurfaceProxyRef() override { return fTextureProxy; }
+    SK_API GrSurfaceProxy* asSurfaceProxy() override { return fTextureProxy.get(); }
+    SK_API const GrSurfaceProxy* asSurfaceProxy() const override { return fTextureProxy.get(); }
+    SK_API sk_sp<GrSurfaceProxy> asSurfaceProxyRef() override { return fTextureProxy; }
 
-    GrTextureProxy* asTextureProxy() override { return fTextureProxy.get(); }
-    const GrTextureProxy* asTextureProxy() const override { return fTextureProxy.get(); }
-    sk_sp<GrTextureProxy> asTextureProxyRef() override { return fTextureProxy; }
+    SK_API GrTextureProxy* asTextureProxy() override { return fTextureProxy.get(); }
+    SK_API const GrTextureProxy* asTextureProxy() const override { return fTextureProxy.get(); }
+    SK_API sk_sp<GrTextureProxy> asTextureProxyRef() override { return fTextureProxy; }
 
-    GrRenderTargetProxy* asRenderTargetProxy() override;
-    sk_sp<GrRenderTargetProxy> asRenderTargetProxyRef() override;
+    SK_API GrRenderTargetProxy* asRenderTargetProxy() override;
+    SK_API sk_sp<GrRenderTargetProxy> asRenderTargetProxyRef() override;
 
 protected:
-    GrTextureContext(GrRecordingContext*,
-                     sk_sp<GrTextureProxy>,
-                     GrColorType,
-                     SkAlphaType,
-                     sk_sp<SkColorSpace>);
+    SK_API GrTextureContext(GrRecordingContext*,
+                            sk_sp<GrTextureProxy>,
+                            GrColorType,
+                            SkAlphaType,
+                            sk_sp<SkColorSpace>);
 
-    SkDEBUGCODE(void validate() const override;)
+    SkDEBUGCODE(SK_API void validate() const override;)
 
 private:
     friend class GrDrawingManager; // for ctor
