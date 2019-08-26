6430d301664f03c7da15905f4170d0ec9e60be86
Jim Van Verth <jvanverth@google.com>
2019-08-26 14:55:12 -0400

    Implement fences and semaphores for Metal.
    GrFence is implemented by a single MTLSharedEvent where we increase the
    value with each new invocation. GrSemaphore uses a MTLEvent (the
    assumption here is that we are signaling and waiting on the same device)
    with an associated value that defaults to 1. For generating a large
    number of GrSemaphores at once it should be possible to use the same
    MTLEvent but with different assigned values.
    
    Bug: skia:8243
    Change-Id: Ic7de2d9d295fbe51e67bc7c3c4354257cb0774d4

---
+++ src/gpu/mtl/GrMtlSemaphore.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrMtlSemaphore_DEFINED
+#define GrMtlSemaphore_DEFINED
+
+#include "include/gpu/GrBackendSemaphore.h"
+#include "include/private/GrTypesPriv.h"
+#include "src/gpu/GrSemaphore.h"
+
+#include <Metal/Metal.h>
+
+class GrMtlGpu;
+
+class GrMtlSemaphore : public GrSemaphore {
+public:
+    static sk_sp<GrMtlSemaphore> Make(GrMtlGpu* gpu, bool isOwned);
+
+    static sk_sp<GrMtlSemaphore> MakeWrapped(GrMtlGpu* gpu,
+                                             id<MTLEvent> event,
+                                             uint64_t value,
+                                             GrWrapOwnership ownership);
+
+    id<MTLEvent> event() const { return fEvent; }
+    uint64_t value() const { return fValue; }
+
+    GrBackendSemaphore backendSemaphore() const override;
+
+private:
+    GrMtlSemaphore(GrMtlGpu* gpu, id<MTLEvent> event, uint64_t value, bool isOwned);
+
+    void onRelease() override;
+    void onAbandon() override;
+
+    id<MTLEvent> fEvent;
+    uint64_t     fValue;
+
+    typedef GrSemaphore INHERITED;
+};
+
+#endif
