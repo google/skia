261cf75aac8829a71b37fb49c87ec67dc4c6576c
Chris Dalton <csmartdalton@google.com>
2019-08-26 12:07:30 -0600

    Reland "Initiate MSAA resolves during DAG generation"
    This is a reland of 804f6a0fe7f3c7df5ac69891841ee9cdf6d1121a
    
    Original change's description:
    > Initiate MSAA resolves during DAG generation
    > 
    > Adds an "fIsMSAADirty" flag to GrRenderTargetProxy and switches to
    > resolving MSAA in GrTextureResolveRenderTask. This completes our push
    > to resolve textures outside of render passes.
    > 
    > For the time being, we only store a dirty flag on the proxy and still
    > rely on the GrRenderTarget itself to track the actual dirty rect. This
    > will be followed by a CL that moves the dirty rect out of
    > GrRenderTarget and into the proxy.
    > 
    > Bug: skia:
    > Change-Id: I21219a58028bdb4590940210e565133093cd34b3
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/235672
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > Reviewed-by: Robert Phillips <robertphillips@google.com>
    
    Bug: skia:
    Change-Id: I805b3af1404eb7919ae937cff3dfa97921e32c69

--- src/gpu/GrDrawingManager.cpp
+++ src/gpu/GrDrawingManager.cpp
@@ -294,6 +294,10 @@ GrSemaphoresSubmitted GrDrawingManager::flush(GrSurfaceProxy* proxies[], int num
                     [](GrSurfaceProxy* p, GrMipMapped mipMapped) {
                 SkASSERT(!p->asTextureProxy() || !p->asTextureProxy()->texPriv().isDeferred());
                 SkASSERT(GrSurfaceProxy::LazyState::kNot == p->lazyInstantiationState());
+                if (p->requiresManualMSAAResolve()) {
+                    // The onFlush callback is responsible for ensuring MSAA gets resolved.
+                    SkASSERT(p->asRenderTargetProxy() && !p->asRenderTargetProxy()->isMSAADirty());
+                }
                 if (GrMipMapped::kYes == mipMapped) {
                     // The onFlush callback is responsible for regenerating mips if needed.
                     SkASSERT(p->asTextureProxy() && !p->asTextureProxy()->mipMapsAreDirty());
@@ -509,19 +513,30 @@ GrSemaphoresSubmitted GrDrawingManager::flushSurfaces(GrSurfaceProxy* proxies[],
     GrSemaphoresSubmitted result = this->flush(proxies, numProxies, access, info,
                                                GrPrepareForExternalIORequests());
     for (int i = 0; i < numProxies; ++i) {
-        if (!proxies[i]->isInstantiated()) {
+        GrSurfaceProxy* proxy = proxies[i];
+        if (!proxy->isInstantiated()) {
             return result;
         }
-        GrSurface* surface = proxies[i]->peekSurface();
-        if (auto* rt = surface->asRenderTarget()) {
-            gpu->resolveRenderTarget(rt);
+        // In the flushSurfaces case, we need to resolve MSAA immediately after flush. This is
+        // because the client will call through to this method when drawing into a target created by
+        // wrapBackendTextureAsRenderTarget, and will expect the original texture to be fully
+        // resolved upon return.
+        if (proxy->requiresManualMSAAResolve()) {
+            auto* rtProxy = proxy->asRenderTargetProxy();
+            SkASSERT(rtProxy);
+            if (rtProxy->isMSAADirty()) {
+                SkASSERT(rtProxy->peekRenderTarget());
+                gpu->resolveRenderTarget(rtProxy->peekRenderTarget());
+                rtProxy->markMSAAResolved();
+            }
         }
         // If, after a flush, any of the proxies of interest have dirty mipmaps, regenerate them in
         // case their backend textures are being stolen.
         // (This special case is exercised by the ReimportImageTextureWithMipLevels test.)
         // FIXME: It may be more ideal to plumb down a "we're going to steal the backends" flag.
-        if (auto* textureProxy = proxies[i]->asTextureProxy()) {
+        if (auto* textureProxy = proxy->asTextureProxy()) {
             if (textureProxy->mipMapsAreDirty()) {
+                SkASSERT(textureProxy->peekTexture());
                 gpu->regenerateMipMapLevels(textureProxy->peekTexture());
                 textureProxy->markMipMapsClean();
             }
