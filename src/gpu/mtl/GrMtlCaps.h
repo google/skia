2c6c1653c50d2587fbe21f61d43435b490922362
Brian Salomon <bsalomon@google.com>
2019-08-28 12:22:42 -0400

    Start on separating texture creation from uploading.
    Change-Id: I65fb1e60eed8f9d0896d686d3baeb10b57ff8f39

--- src/gpu/mtl/GrMtlCaps.h
+++ src/gpu/mtl/GrMtlCaps.h
@@ -78,8 +78,6 @@ public:
         return fColorTypeToFormatTable[idx];
     }
 
-    bool canClearTextureOnCreation() const override { return true; }
-
     GrSwizzle getTextureSwizzle(const GrBackendFormat&, GrColorType) const override;
     GrSwizzle getOutputSwizzle(const GrBackendFormat&, GrColorType) const override;
 
