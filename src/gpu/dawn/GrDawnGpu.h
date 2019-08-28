2a65415ddf1429a408337339d10398add9d11069
Stephen White <senorblanco@chromium.org>
2019-08-28 11:00:09 -0400

    Dawn: submit all command buffers in a single call.
    All command buffers are now appended to a single vector, and submitted
    submitted in onFinishFlush() in a single submit.
    
    In order to do this, implement a centralized command encoder for copies.
    This command encoder is used for buffer-to-buffer, buffer-to-texture and
    texture-to-buffer copies.
    
    Change-Id: If1092ae4edbe03c995d4cb80e0dc124516d63dad

--- src/gpu/dawn/GrDawnGpu.h
+++ src/gpu/dawn/GrDawnGpu.h
@@ -50,6 +50,7 @@ public:
 
     void testingOnly_flushGpuAndSync() override;
 #endif
+    void flush();
 
     GrStencilAttachment* createStencilAttachmentForRenderTarget(const GrRenderTarget*,
                                                                 int width,
@@ -82,6 +83,7 @@ public:
     sk_sp<GrSemaphore> prepareTextureForCrossContextUsage(GrTexture*) override;
 
     GrDawnRingBuffer::Slice allocateUniformRingBufferSlice(int size);
+    void appendCommandBuffer(dawn::CommandBuffer commandBuffer);
 
 private:
     void onResetContext(uint32_t resetBits) override {}
@@ -147,6 +149,8 @@ private:
     std::unique_ptr<SkSL::Compiler>                 fCompiler;
     std::unique_ptr<GrDawnOpsRenderPass>            fOpsRenderPass;
     GrDawnRingBuffer                                fUniformRingBuffer;
+    dawn::CommandEncoder                            fCopyEncoder;
+    std::vector<dawn::CommandBuffer>                fCommandBuffers;
 
     typedef GrGpu INHERITED;
 };
