4cc9e07fd924a2776d4cd20a014de60dd2906b5c
Herb Derby <herb@google.com>
2019-08-29 18:18:52 -0400

    WIP simplify common code to be faster
    Change-Id: I22767ecc9926a4c6c329fbce049184d73bb8a929

---
+++ src/core/SkGlyphBuffer.h
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkGlyphBuffer_DEFINED
+#define SkGlyphBuffer_DEFINED
+
+#include "src/core/SkGlyph.h"
+#include "src/core/SkZip.h"
+
+class SkStrikeForGPU;
+
+class SkBag {
+public:
+    SkBag() = default;
+    SkBag& operator= (SkPackedGlyphID packedID) {
+        fV.packedID = packedID;
+        return *this;
+    }
+    SkBag& operator= (SkGlyph* glyph) {
+        fV.glyph = glyph;
+        return *this;
+    }
+    SkBag& operator= (const SkPath* path) {
+        fV.path = path;
+        return *this;
+    }
+
+    operator SkPackedGlyphID() { return fV.packedID; }
+    operator SkGlyph*() { return fV.glyph; }
+    operator const SkPath*() { return fV.path; }
+
+private:
+    union {
+        SkPackedGlyphID packedID;
+        SkGlyph* glyph;
+        const SkPath* path;
+    } fV = { .glyph = nullptr };
+};
+
+class SkSourceGlyphBuffer {
+public:
+    SkSourceGlyphBuffer() = default;
+
+    void reset(SkZip<const SkGlyphID, const SkPoint> source) {
+        fRejectedGlyphIDs.reset(source.size());
+        fRejectedPositions.reset(source.size());
+
+        // Start with rejects pointing to internal buffers.
+        fRejects = SkZip<SkGlyphID, SkPoint>{source.size(), fRejectedGlyphIDs, fRejectedPositions};
+        fSource = source;
+    }
+
+    void reject(size_t index) {
+        fRejects[fRejectSize++] = fSource[index];
+    }
+
+    void reject(size_t index, int rejectedMaxDimension) {
+        fRejectedMaxDimension = SkTMax(fRejectedMaxDimension, rejectedMaxDimension);
+        this->reject(index);
+    }
+
+    SkZip<const SkGlyphID, const SkPoint> flipRejectsToSource() {
+        fSource = fRejects.first(fRejectSize);
+        fRejectSize = 0;
+        return fSource;
+    }
+
+    SkZip<const SkGlyphID, const SkPoint> source() const { return fSource; }
+
+    int rejectedMaxDimension() const { return fRejectedMaxDimension; }
+
+private:
+    SkZip<const SkGlyphID, const SkPoint> fSource;
+    size_t fRejectSize{0};
+    int fRejectedMaxDimension{0};
+    SkZip<SkGlyphID, SkPoint> fRejects;
+    SkAutoTMalloc<SkGlyphID> fRejectedGlyphIDs;
+    SkAutoTMalloc<SkPoint> fRejectedPositions;
+};
+
+class SkDrawableGlyphBuffer {
+public:
+    void startSource(const SkZip<const SkGlyphID, const SkPoint>& source, SkPoint origin);
+
+    void startDevice(
+            const SkZip<const SkGlyphID, const SkPoint>& source,
+            SkPoint origin, const SkMatrix& viewMatrix, const SkStrikeForGPU& strike);
+
+    void push_back(SkGlyph* glyph, size_t from) {
+        SkASSERT(fDrawableSize <= from);
+        fPositions[fDrawableSize] = fPositions[from];
+        fMultiBuffer[fDrawableSize] = glyph;
+        fDrawableSize++;
+    }
+
+    void push_back(const SkPath* path, size_t from) {
+        SkASSERT(fDrawableSize <= from);
+        fPositions[fDrawableSize] = fPositions[from];
+        fMultiBuffer[fDrawableSize] = path;
+        fDrawableSize++;
+    }
+
+    SkZip<SkBag, SkPoint> drawable() {
+        return SkZip<SkBag, SkPoint>{fDrawableSize, fMultiBuffer, fPositions};
+    }
+
+    SkZip<SkBag, SkPoint> input() {
+        return SkZip<SkBag, SkPoint>{fInputSize, fMultiBuffer, fPositions};
+    }
+
+    bool inputEmpty() const { return fInputSize != 0; }
+
+private:
+    void reset(size_t size) {
+        if (size > fBufferSize) {
+            fMultiBuffer.reset(size);
+            fPositions.reset(size);
+            fBufferSize = size;
+        }
+        fInputSize = size;
+        fDrawableSize = 0;
+    }
+    size_t fBufferSize{0};
+    size_t fInputSize{0};
+    size_t fDrawableSize{0};
+    SkAutoTMalloc<SkBag> fMultiBuffer;
+    SkAutoTMalloc<SkPoint> fPositions;
+};
+
+#endif  // SkGlyphBuffer_DEFINED
