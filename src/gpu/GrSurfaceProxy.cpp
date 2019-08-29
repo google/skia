b1adc8c7ed2c3d8c5203f356a642a6349583d6b5
Robert Phillips <robertphillips@google.com>
2019-08-29 15:02:32 -0400

    [M77] Fix exactify for explicit resource allocation
    Bug: b/138674523
    Change-Id: I660e42d1e8bdde2194d8aec71eed791e5793d01e

--- src/gpu/GrSurfaceProxy.cpp
+++ src/gpu/GrSurfaceProxy.cpp
@@ -406,7 +406,7 @@ GrInternalSurfaceFlags GrSurfaceProxy::testingOnly_getFlags() const {
 }
 #endif
 
-void GrSurfaceProxyPriv::exactify() {
+void GrSurfaceProxyPriv::exactify(bool allocatedCaseOnly) {
     SkASSERT(GrSurfaceProxy::LazyState::kFully != fProxy->lazyInstantiationState());
     if (this->isExact()) {
         return;
@@ -422,6 +422,15 @@ void GrSurfaceProxyPriv::exactify() {
         // used for additional draws.
         fProxy->fWidth = fProxy->fTarget->width();
         fProxy->fHeight = fProxy->fTarget->height();
+        return;
+    }
+
+    // In the post-implicit-allocation world we can't convert this proxy to be exact fit
+    // at this point. With explicit allocation switching this to exact will result in a
+    // different allocation at flush time. With implicit allocation, allocation would occur
+    // at draw time (rather than flush time) so this pathway was encountered less often (if
+    // at all).
+    if (allocatedCaseOnly) {
         return;
     }
 
