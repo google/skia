ffbe1dcab77f47d379405c235ac6a63c292a79f5
Herb Derby <herb@google.com>
2019-08-29 18:18:52 -0400

    WIP simplify common code to be faster
    Change-Id: I22767ecc9926a4c6c329fbce049184d73bb8a929

--- src/core/SkGlyphRunPainter.h
+++ src/core/SkGlyphRunPainter.h
@@ -12,6 +12,7 @@
 #include "src/core/SkDistanceFieldGen.h"
 #include "src/core/SkGlyphRun.h"
 #include "src/core/SkScalerContext.h"
+#include "src/core/SkStrikeCommon.h"
 #include "src/core/SkTextBlobPriv.h"
 
 #if SK_SUPPORT_GPU
@@ -23,18 +24,9 @@ class GrRenderTargetContext;
 class SkGlyphRunPainterInterface;
 class SkStrikeSpec;
 
-class SkStrikeCommon {
-public:
-    static SkVector PixelRounding(bool isSubpixel, SkAxisAlignment axisAlignment);
-
-    // An atlas consists of plots, and plots hold glyphs. The minimum a plot can be is 256x256.
-    // This means that the maximum size a glyph can be is 256x256.
-    static constexpr uint16_t kSkSideTooBigForAtlas = 256;
-};
-
 class SkGlyphRunListPainter {
 public:
-    // Constructor for SkBitmpapDevice.
+    // Constructor for SkBitmapDevice.
     SkGlyphRunListPainter(const SkSurfaceProps& props,
                           SkColorType colorType,
                           SkColorSpace* cs,
@@ -158,15 +150,15 @@ public:
 
     virtual void startRun(const SkGlyphRun& glyphRun, bool useSDFT) = 0;
 
-    virtual void processDeviceMasks(SkSpan<const SkGlyphPos> masks,
+    virtual void processDeviceMasks(SkGlyphinator inator,
                                     const SkStrikeSpec& strikeSpec) = 0;
 
-    virtual void processSourcePaths(SkSpan<const SkGlyphPos> paths,
+    virtual void processSourcePaths(SkGlyphinator inator,
                                     const SkStrikeSpec& strikeSpec) = 0;
 
     virtual void processDevicePaths(SkSpan<const SkGlyphPos> paths) = 0;
 
-    virtual void processSourceSDFT(SkSpan<const SkGlyphPos> masks,
+    virtual void processSourceSDFT(SkGlyphinator inator,
                                    const SkStrikeSpec& strikeSpec,
                                    const SkFont& runFont,
                                    SkScalar minScale,
--- src/core/SkStrikeForGPU.h
+++ src/core/SkStrikeForGPU.h
@@ -13,6 +13,7 @@
 #include "include/core/SkTypes.h"
 #include "src/core/SkGlyph.h"
 #include "src/core/SkSpan.h"
+#include "src/core/SkZip.h"
 
 #include <memory>
 
@@ -20,6 +21,7 @@ class SkDescriptor;
 class SkGlyph;
 class SkMaskFilter;
 class SkPathEffect;
+class SkStrikeInterface;
 class SkTypeface;
 
 // TODO: rename SkScalerContextEffects -> SkStrikeEffects
@@ -46,6 +48,57 @@ struct SkPathPos {
     SkPoint position;
 };
 
+struct SkGlyphIDPos {
+    size_t n;
+    const SkGlyphID* ids;
+    const SkPoint* positions;
+};
+
+struct SkGlyphinator {
+    size_t n = 0;
+    union MultiElement {
+        SkGlyphID glyphID;
+        SkPackedGlyphID packedID;
+        const SkGlyph* glyph;
+        const SkPath* path;
+    }* glyphs{nullptr};
+
+    SkPoint* positions{nullptr};
+
+    SkZip<MultiElement, SkPoint> zip() const {
+        return SkZip<MultiElement, SkPoint>{n, glyphs, positions};
+    }
+    bool empty() const { return n == 0; }
+    size_t size() const { return n; }
+    SkGlyphinator first(size_t firstN) {
+        return SkGlyphinator{firstN, glyphs, positions};
+    }
+
+    struct Storage {
+        size_t fSize{0};
+        SkAutoTMalloc<MultiElement> fMultiBuffer;
+        SkAutoTMalloc<SkPoint> fPositions;
+        void ensure(size_t size) {
+            if (size > fSize) {
+                fMultiBuffer.reset(size);
+                fPositions.reset(size);
+                fSize = size;
+            }
+        }
+
+        SkGlyphinator makeInator(size_t size) {
+            return SkGlyphinator{size, fMultiBuffer, fPositions};
+        }
+
+        SkGlyphinator makeForDeviceSpace(SkStrikeInterface* strike,
+                                                const SkMatrix& viewMatrix,
+                                                const SkPoint& origin,
+                                                SkGlyphIDPos glyphPos);
+
+        SkGlyphinator makeForSourceSpace(const SkPoint& origin, SkGlyphIDPos glyphPos);
+    };
+};
+
 class SkStrikeForGPU {
 public:
     virtual ~SkStrikeForGPU() = default;
@@ -63,6 +116,16 @@ public:
                                  size_t n,
                                  int maxDimension,
                                  SkGlyphPos results[]) = 0;
+
+
+    virtual SkGlyphinator prepareForMaskDrawing(
+            SkGlyphinator glyphPos, std::vector<size_t>& rejectIndices) = 0;
+
+    virtual SkGlyphinator prepareForSDFTDrawing(
+            SkGlyphinator glyphPos, std::vector<size_t>& rejectIndices) = 0;
+
+    virtual SkGlyphinator prepareForPathDrawing(
+            SkGlyphinator glyphPos, std::vector<size_t>& rejectIndices) = 0;
 
     // rounding() and subpixelMask are used to calculate the subpixel position of a glyph.
     // The per component (x or y) calculation is:
@@ -99,4 +162,44 @@ public:
                                                     const SkScalerContextEffects& effects,
                                                     const SkTypeface& typeface) = 0;
 };
+
+inline SkGlyphinator SkGlyphinator::Storage::makeForDeviceSpace(
+        SkStrikeInterface* strike,
+        const SkMatrix& viewMatrix,
+        const SkPoint& origin,
+        SkGlyphIDPos glyphPos) {
+    size_t runSize = glyphPos.n;
+    this->ensure(runSize);
+
+    // Add rounding and origin.
+    SkMatrix matrix = viewMatrix;
+    matrix.preTranslate(origin.x(), origin.y());
+    SkPoint rounding = strike->rounding();
+    matrix.postTranslate(rounding.x(), rounding.y());
+    matrix.mapPoints(fPositions, glyphPos.positions, runSize);
+
+    SkIPoint mask = strike->subpixelMask();
+
+    for (size_t i = 0; i < runSize; i++) {
+        SkFixed subX = SkScalarToFixed(fPositions[i].x()) & mask.x(),
+                subY = SkScalarToFixed(fPositions[i].y()) & mask.y();
+        fMultiBuffer[i].packedID = SkPackedGlyphID{glyphPos.ids[i], subX, subY};
+    }
+
+    return this->makeInator(runSize);
+}
+
+inline SkGlyphinator SkGlyphinator::Storage::makeForSourceSpace(
+        const SkPoint& origin, SkGlyphIDPos glyphPos) {
+    size_t runSize = glyphPos.n;
+    this->ensure(runSize);
+
+    SkMatrix::MakeTrans(origin.x(), origin.y()).mapPoints(fPositions, glyphPos.positions, runSize);
+
+    for (size_t i = 0; i < glyphPos.n; i++) {
+        fMultiBuffer[i].packedID = SkPackedGlyphID{glyphPos.ids[i]};
+    }
+
+    return this->makeInator(runSize);
+}
 #endif  //SkStrikeInterface_DEFINED
