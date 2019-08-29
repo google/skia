497dbd0392a879158815a860820b72b2b2402aa9
Ethan Nicholas <ethannicholas@google.com>
2019-08-29 13:09:06 -0400

    SkSL sample() now permits specification of coordinates
    Bug: skia:
    Change-Id: I16073008ac852f1864bd1d2bd38087a5b661d05a

--- src/gpu/glsl/GrGLSLProgramBuilder.cpp
+++ src/gpu/glsl/GrGLSLProgramBuilder.cpp
@@ -196,7 +196,8 @@ SkString GrGLSLProgramBuilder::emitAndInstallFragProc(
         }
     }
 
-    const GrShaderVar* coordVars = fTransformedCoordVars.begin() + transformedCoordVarsIdx;
+    const GrGLSLPrimitiveProcessor::TransformVar* coordVars = fTransformedCoordVars.begin() +
+                                                              transformedCoordVarsIdx;
     GrGLSLFragmentProcessor::TransformedCoordVars coords(&fp, coordVars);
     GrGLSLFragmentProcessor::TextureSamplers textureSamplers(&fp, texSamplers.begin());
     GrGLSLFragmentProcessor::EmitArgs args(&fFS,
