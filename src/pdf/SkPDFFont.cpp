c5d992cdbccec221a26378720080b8d5745acdb8
Ben Wagner <bungeman@google.com>
2019-08-30 19:28:09 -0400

    Better DW and W
    Change-Id: I40d8b27ab1344d546de3958b3198b675bda81b76

--- src/pdf/SkPDFFont.cpp
+++ src/pdf/SkPDFFont.cpp
@@ -369,7 +369,7 @@ static void emit_subset_type0(const SkPDFFont& font, SkPDFDocument* doc) {
     SkScalar defaultWidth = 0;
     {
         std::unique_ptr<SkPDFArray> widths = SkPDFMakeCIDGlyphWidthsArray(
-                *face, &font.glyphUsage(), &defaultWidth);
+                *face, font.glyphUsage(), &defaultWidth);
         if (widths && widths->size() > 0) {
             newCIDFont->insertObject("W", std::move(widths));
         }
