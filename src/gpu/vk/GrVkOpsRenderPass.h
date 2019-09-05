f0c681e459405291ac2db87d536d94d81c177317
Greg Daniel <egdaniel@google.com>
2019-09-05 14:07:41 -0400

    Begin renderpasses immediately in GrVkOpsRenderPass.
    This change will allow us to easily switch between rendering to a
    primary vs secondary command buffer.
    
    Change-Id: Iaa066f24b64a686db41a1554b0d75c185714ef8c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/239100
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>

--- src/gpu/vk/GrVkOpsRenderPass.h
+++ src/gpu/vk/GrVkOpsRenderPass.h
@@ -23,24 +23,6 @@ class GrVkRenderPass;
 class GrVkRenderTarget;
 class GrVkSecondaryCommandBuffer;
 
-/** Base class for tasks executed on primary command buffer, between secondary command buffers. */
-class GrVkPrimaryCommandBufferTask {
-public:
-    virtual ~GrVkPrimaryCommandBufferTask();
-
-    struct Args {
-        GrGpu* fGpu;
-        GrSurface* fSurface;
-    };
-
-    virtual void execute(const Args& args) = 0;
-
-protected:
-    GrVkPrimaryCommandBufferTask();
-    GrVkPrimaryCommandBufferTask(const GrVkPrimaryCommandBufferTask&) = delete;
-    GrVkPrimaryCommandBufferTask& operator=(const GrVkPrimaryCommandBufferTask&) = delete;
-};
-
 class GrVkOpsRenderPass : public GrOpsRenderPass, private GrMesh::SendToGpuImpl {
 public:
     GrVkOpsRenderPass(GrVkGpu*);
@@ -69,7 +51,9 @@ public:
 #endif
 
 private:
-    void init();
+    void init(const GrOpsRenderPass::LoadAndStoreInfo&,
+              const GrOpsRenderPass::StencilLoadAndStoreInfo&,
+              const SkPMColor4f& clearColor);
 
     // Called instead of init when we are drawing to a render target that already wraps a secondary
     // command buffer.
@@ -78,6 +62,8 @@ private:
     bool wrapsSecondaryCommandBuffer() const;
 
     GrGpu* gpu() override;
+
+    GrVkCommandBuffer* currentCommandBuffer();
 
     // Bind vertex and index buffers
     void bindGeometry(const GrGpuBuffer* indexBuffer,
@@ -131,37 +117,11 @@ private:
 
     void addAdditionalRenderPass();
 
-    enum class LoadStoreState {
-        kUnknown,
-        kStartsWithClear,
-        kStartsWithDiscard,
-        kLoadAndStore,
-    };
-
-    struct CommandBufferInfo {
-        const GrVkRenderPass* fRenderPass;
-        std::unique_ptr<GrVkSecondaryCommandBuffer> fCommandBuffer;
-        int fNumPreCmds = 0;
-        VkClearValue fColorClearValue;
-        SkRect fBounds;
-        bool fIsEmpty = true;
-        LoadStoreState fLoadStoreState = LoadStoreState::kUnknown;
-
-        GrVkSecondaryCommandBuffer* currentCmdBuf() {
-            return fCommandBuffer.get();
-        }
-    };
-
-    SkTArray<CommandBufferInfo>                 fCommandBufferInfos;
-    GrTRecorder<GrVkPrimaryCommandBufferTask>   fPreCommandBufferTasks{1024};
+    std::unique_ptr<GrVkSecondaryCommandBuffer> fCurrentSecondaryCommandBuffer;
+    const GrVkRenderPass*                       fCurrentRenderPass;
+    bool                                        fCurrentCBIsEmpty = true;
+    SkIRect                                     fBounds;
     GrVkGpu*                                    fGpu;
-    GrVkPipelineState*                          fLastPipelineState = nullptr;
-    SkPMColor4f                                 fClearColor;
-    VkAttachmentLoadOp                          fVkColorLoadOp;
-    VkAttachmentStoreOp                         fVkColorStoreOp;
-    VkAttachmentLoadOp                          fVkStencilLoadOp;
-    VkAttachmentStoreOp                         fVkStencilStoreOp;
-    int                                         fCurrentCmdInfo = -1;
 
 #ifdef SK_DEBUG
     // When we are actively recording into the GrVkOpsRenderPass we set this flag to true. This
