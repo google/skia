72f2cdbf6e0cc2a9e20b6f9fc5824bb490e798ce
Ethan Nicholas <ethannicholas@google.com>
2019-09-09 09:25:45 -0400

    Fixed skslc assertion failure discovered by fuzzer
    This particular issue probably wouldn't ever occur in hand-written code, but
    such is fuzzing...
    
    Bug: oss-fuzz:17061
    Change-Id: Ia5da7b75afef8b49bb19ddb7f1ea8696053c2e54

--- src/sksl/ir/SkSLFunctionDeclaration.h
+++ src/sksl/ir/SkSLFunctionDeclaration.h
@@ -93,7 +93,9 @@ struct FunctionDeclaration : public Symbol {
             }
         }
         if (fReturnType.kind() == Type::kGeneric_Kind) {
-            SkASSERT(genericIndex != -1);
+            if (genericIndex == -1) {
+                return false;
+            }
             *outReturnType = fReturnType.coercibleTypes()[genericIndex];
         } else {
             *outReturnType = &fReturnType;
