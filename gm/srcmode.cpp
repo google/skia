c287b64128379d038bb03d150282145e289e34c5
Brian Salomon <bsalomon@google.com>
2019-09-10 11:42:06 -0400

    Remove unused GM deferred canvas feature
    Change-Id: I57b7fab34a46dd03a045800989e73f9001bdb6cb

--- gm/srcmode.cpp
+++ gm/srcmode.cpp
@@ -119,11 +119,11 @@ class SrcModeGM : public skiagm::GM {
         }
     }
 
-    static sk_sp<SkSurface> compat_surface(SkCanvas* canvas, const SkISize& size, bool skipGPU) {
+    static sk_sp<SkSurface> compat_surface(SkCanvas* canvas, const SkISize& size) {
         SkImageInfo info = SkImageInfo::MakeN32Premul(size);
 
         bool callNewSurface = true;
-        if (canvas->getGrContext() && skipGPU) {
+        if (canvas->getGrContext()) {
             callNewSurface = false;
         }
         sk_sp<SkSurface> surface = callNewSurface ? canvas->makeSurface(info) : nullptr;
@@ -135,7 +135,7 @@ class SrcModeGM : public skiagm::GM {
     }
 
     void onDraw(SkCanvas* canvas) override {
-        auto surf(compat_surface(canvas, this->getISize(), this->isCanvasDeferred()));
+        auto surf(compat_surface(canvas, this->getISize()));
         surf->getCanvas()->drawColor(SK_ColorWHITE);
         this->drawContent(surf->getCanvas());
         surf->draw(canvas, 0, 0, nullptr);
