8fd17c2c5b41910a61ff00826254b0d5abc8aa1a
Brian Salomon <bsalomon@google.com>
2019-09-10 09:17:38 -0400

    Reland "Separate texture creation from uploading in GrGpu subclasses."
    This is a reland of a7398246cb80746994df430261ed72822a1f60ba
    
    Original change's description:
    > Separate texture creation from uploading in GrGpu subclasses.
    > 
    > GrGpu base class still allows creation with initial data, but separated
    > at subclass level into create and then write pixels.
    > 
    > GrGpu handles determining which levels need clearing and GrGpu
    > subclasses take a mask and clear levels with mask bit set.
    > 
    > GrGLGpu uses three pronged clear strategy:
    > glClearTexImage() if supported, glClear() if format is FBO bindable, and
    > lastly glTexSubImage2D with zero'ed buffer.
    > 
    > Change-Id: I65fb1e60eed8f9d0896d686d3baeb10b57ff8f39
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/236676
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    
    Change-Id: I54cda3b6a4b017a94ef1f50bb3748c45a24d8936

--- src/gpu/mtl/GrMtlGpu.h
+++ src/gpu/mtl/GrMtlGpu.h
@@ -139,8 +139,8 @@ private:
                                      int renderTargetSampleCnt,
                                      SkBudgeted budgeted,
                                      GrProtected,
-                                     const GrMipLevel texels[],
-                                     int mipLevelCount) override;
+                                     int mipLevelCount,
+                                     uint32_t levelClearMask) override;
     sk_sp<GrTexture> onCreateCompressedTexture(int width, int height, const GrBackendFormat&,
                                                SkImage::CompressionType, SkBudgeted,
                                                const void* data) override {
