120e7d6766e4015528894e1c5c7f0d73bb091bf7
Brian Salomon <bsalomon@google.com>
2019-09-11 10:29:22 -0400

    Make building in Chrome with base/logging.h included in SkTypes.h work.
    Doesn't add base/logging.h, just fixes conflicts with doing so (at least
    on Linux build.)
    
    Change-Id: I6f5d1000f0c6ddf77d6c4319f243fc5c6ef38475
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/240684
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/core/SkSpinlock.cpp
+++ src/core/SkSpinlock.cpp
@@ -7,16 +7,17 @@
 
 #include "include/private/SkSpinlock.h"
 
+// Renamed from "pause" to avoid conflict with function defined in unistd.h
 #if SK_CPU_SSE_LEVEL >= SK_CPU_SSE_LEVEL_SSE2
     #include <emmintrin.h>
-    static void pause() { _mm_pause(); }
+    static void do_pause() { _mm_pause(); }
 #else
-    static void pause() { /*spin*/ }
+    static void do_pause() { /*spin*/ }
 #endif
 
 void SkSpinlock::contendedAcquire() {
     // To act as a mutex, we need an acquire barrier when we acquire the lock.
     while (fLocked.exchange(true, std::memory_order_acquire)) {
-        pause();
+        do_pause();
     }
 }
