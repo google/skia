e7bc066f2ba56a3c9d59fbd2e7cdad204d7d8c62
Nathaniel Nifong <nifong@google.com>
2019-09-11 10:58:21 -0400

    Assert there are some sources when runing skpbench
    Change-Id: I6009dfc7c6eabe04b3b6fdf68f38995d718b4fef

--- tools/skpbench/_adb_path.py
+++ tools/skpbench/_adb_path.py
@@ -19,16 +19,16 @@ def join(*pathnames):
 def basename(pathname):
   return pathname.rsplit('/', maxsplit=1)[-1]
 
-def find_skps(skps):
+def find_files(dirs, file_extension):
   # root first, in case skps reside in a protected directory
   __ADB.root()
-  escapedskps = [re.sub(r'([^a-zA-Z0-9_/\.\*\?\[\!\]])', r'\\\1', x)
-                 for x in skps]
+  escapeddirs = [re.sub(r'([^a-zA-Z0-9_/\.\*\?\[\!\]])', r'\\\1', x)
+                 for x in dirs]
   return __ADB.check('''\
     for PATHNAME in %s; do
       if [ -d "$PATHNAME" ]; then
-        find "$PATHNAME" -maxdepth 1 -name *.skp
+        find "$PATHNAME" -maxdepth 1 -name *.%s
       else
         echo "$PATHNAME"
       fi
-    done''' % ' '.join(escapedskps)).splitlines()
+    done''' % (' '.join(escapeddirs).splitlines(), file_extension)
