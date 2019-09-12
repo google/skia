9ec33d1748db01a18e97ee19877f2b0633fd316b
Stephen White <senorblanco@chromium.org>
2019-09-12 13:05:08 -0400

    Update Dawn backend to recent GrGpu::onCreateTexture() change.
    Change-Id: I9e329406f70eb1a0f8b86088ee9c2b7baa32f9af
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/241046
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Stephen White <senorblanco@chromium.org>

--- src/gpu/dawn/GrDawnGpu.h
+++ src/gpu/dawn/GrDawnGpu.h
@@ -116,8 +116,8 @@ private:
                                      int renderTargetSampleCnt,
                                      SkBudgeted,
                                      GrProtected,
-                                     const GrMipLevel texels[],
-                                     int mipLevelCount) override;
+                                     int mipLevelCount,
+                                     uint32_t levelClearMask) override;
 
     sk_sp<GrTexture> onCreateCompressedTexture(int width, int height, const GrBackendFormat&,
                                                SkImage::CompressionType, SkBudgeted,
