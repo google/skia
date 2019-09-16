c5430b1e78636d229035ec77a332bc43daad366a
Brian Osman <brianosman@google.com>
2019-09-16 16:17:21 -0400

    Fix license comment in skottie_ios
    Change-Id: I6591ca76578c5d43d43b87dde2582b19e6d1cf25

--- experimental/skottie_ios/main.mm
+++ experimental/skottie_ios/main.mm
@@ -1,5 +1,5 @@
 // Copyright 2019 Google LLC.
-// Use of this source cofcee is governed by a BSD-style license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 #include "experimental/skottie_ios/SkMetalViewBridge.h"
 #include "experimental/skottie_ios/SkottieMtkView.h"
