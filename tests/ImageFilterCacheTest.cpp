d613e06521c0ba1314186165f1d430d8bdf1d5c4
Michael Ludwig <michaelludwig@google.com>
2019-09-19 13:43:37 -0400

    Define coord space tagged geometry for image filters
    This adds new template wrappers around SkIRect, SkRect, SkIPoint, SkPoint,
    SkIVector, and SkVector. The templates define how to transform by matrices
    and translations, so they enforce the semantic difference between a point
    and a direction. This makes transforming between coordinate spaces fairly
    straight forward to define. A limited set of functions are defined to
    modify and access state in a coordinate-space safe manner on these new
    types. I imagine new functions can be added as use cases are uncovered.
    
    This CL also defines the new bounds calculating functions for image filters
    to use the geometric wrappers with tagged coordinate spaces. However, the
    implementations of these functions are defined in terms of the old
    bounds virtuals, so there is not much usage of the new API.
    
    One change from the old system is that the node-specific effects and the
    child aggregation logic will no longer be split into separate virtuals.
    Instead, a helper function will be defined to do the default recursion, so
    that the common case can explicitly call that aggregation logic w/o relying
    on the parent class to magically handle it.
    
    Bug: skia:
    Change-Id: I2d9b0dcf6b435990988efccef5f17b1d9edef843

--- tests/ImageFilterCacheTest.cpp
+++ tests/ImageFilterCacheTest.cpp
@@ -47,11 +47,12 @@ static void test_find_existing(skiatest::Reporter* reporter,
 
     SkIPoint offset = SkIPoint::Make(3, 4);
     auto filter = make_filter();
-    cache->set(key1, filter.get(), skif::FilterResult<For::kOutput>(image, offset));
+    cache->set(key1, filter.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
 
     skif::FilterResult<For::kOutput> foundImage;
     REPORTER_ASSERT(reporter, cache->get(key1, &foundImage));
-    REPORTER_ASSERT(reporter, offset == foundImage.origin());
+    REPORTER_ASSERT(reporter, offset == SkIPoint(foundImage.origin()));
 
     REPORTER_ASSERT(reporter, !cache->get(key2, &foundImage));
 }
@@ -75,8 +76,8 @@ static void test_dont_find_if_diff_key(skiatest::Reporter* reporter,
 
     SkIPoint offset = SkIPoint::Make(3, 4);
     auto filter = make_filter();
-    cache->set(key0, filter.get(), skif::FilterResult<For::kOutput>(image, offset));
-
+    cache->set(key0, filter.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
     skif::FilterResult<For::kOutput> foundImage;
     REPORTER_ASSERT(reporter, !cache->get(key1, &foundImage));
     REPORTER_ASSERT(reporter, !cache->get(key2, &foundImage));
@@ -96,14 +97,16 @@ static void test_internal_purge(skiatest::Reporter* reporter, const sk_sp<SkSpec
 
     SkIPoint offset = SkIPoint::Make(3, 4);
     auto filter1 = make_filter();
-    cache->set(key1, filter1.get(), skif::FilterResult<For::kOutput>(image, offset));
+    cache->set(key1, filter1.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
 
     skif::FilterResult<For::kOutput> foundImage;
     REPORTER_ASSERT(reporter, cache->get(key1, &foundImage));
 
     // This should knock the first one out of the cache
     auto filter2 = make_filter();
-    cache->set(key2, filter2.get(), skif::FilterResult<For::kOutput>(image, offset));
+    cache->set(key2, filter2.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
 
     REPORTER_ASSERT(reporter, cache->get(key2, &foundImage));
     REPORTER_ASSERT(reporter, !cache->get(key1, &foundImage));
@@ -123,8 +126,10 @@ static void test_explicit_purging(skiatest::Reporter* reporter,
     SkIPoint offset = SkIPoint::Make(3, 4);
     auto filter1 = make_filter();
     auto filter2 = make_filter();
-    cache->set(key1, filter1.get(), skif::FilterResult<For::kOutput>(image, offset));
-    cache->set(key2, filter2.get(), skif::FilterResult<For::kOutput>(image, offset));
+    cache->set(key1, filter1.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
+    cache->set(key2, filter2.get(), skif::FilterResult<For::kOutput>(
+               image, skif::IPoint<In::kLayer, For::kOutput>(offset)));
     SkDEBUGCODE(REPORTER_ASSERT(reporter, 2 == cache->count());)
 
     skif::FilterResult<For::kOutput> foundImage;
