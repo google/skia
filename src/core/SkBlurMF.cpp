cc3e4d39bb54690ea75d75acb15bc123046ffb4e
Robert Phillips <robertphillips@google.com>
2019-09-19 15:50:57 -0400

    Remove a usage of GrPixelConfig in SkGpuBlurUtils::GaussianBlur (take 2)
    Bug: skia:6718
    Change-Id: I6a0be1cab5543c3d76a041fcdc9ae9be4a3b179b

--- src/core/SkBlurMF.cpp
+++ src/core/SkBlurMF.cpp
@@ -63,6 +63,8 @@ public:
                              const GrShape& shape) const override;
     sk_sp<GrTextureProxy> filterMaskGPU(GrRecordingContext*,
                                         sk_sp<GrTextureProxy> srcProxy,
+                                        GrColorType srcColorType,
+                                        SkAlphaType srcAlphaType,
                                         const SkMatrix& ctm,
                                         const SkIRect& maskRect) const override;
 #endif
@@ -868,6 +870,8 @@ bool SkBlurMaskFilterImpl::canFilterMaskGPU(const GrShape& shape,
 
 sk_sp<GrTextureProxy> SkBlurMaskFilterImpl::filterMaskGPU(GrRecordingContext* context,
                                                           sk_sp<GrTextureProxy> srcProxy,
+                                                          GrColorType srcColorType,
+                                                          SkAlphaType srcAlphaType,
                                                           const SkMatrix& ctm,
                                                           const SkIRect& maskRect) const {
     // 'maskRect' isn't snapped to the UL corner but the mask in 'src' is.
@@ -876,19 +880,23 @@ sk_sp<GrTextureProxy> SkBlurMaskFilterImpl::filterMaskGPU(GrRecordingContext* co
     SkScalar xformedSigma = this->computeXformedSigma(ctm);
     SkASSERT(xformedSigma > 0);
 
+
+
+
     // If we're doing a normal blur, we can clobber the pathTexture in the
     // gaussianBlur.  Otherwise, we need to save it for later compositing.
     bool isNormalBlur = (kNormal_SkBlurStyle == fBlurStyle);
     auto renderTargetContext = SkGpuBlurUtils::GaussianBlur(context,
                                                             srcProxy,
+                                                            srcColorType,
+                                                            srcAlphaType,
                                                             SkIPoint::Make(0, 0),
                                                             nullptr,
                                                             clipRect,
                                                             SkIRect::EmptyIRect(),
                                                             xformedSigma,
                                                             xformedSigma,
-                                                            GrTextureDomain::kIgnore_Mode,
-                                                            kPremul_SkAlphaType);
+                                                            GrTextureDomain::kIgnore_Mode);
     if (!renderTargetContext) {
         return nullptr;
     }
