66ca3e256a2f9e2efc13b918b2b32837a5b66cbf
Robert Phillips <robertphillips@google.com>
2019-09-24 13:00:42 -0400

    Fix accessing off end of pixmap for R16G16_float in SSE2/SSE4.1 mode
    Bug: 1005615, oss-fuzz:17491
    Change-Id: Ice8686890efad4693ba436ec74a99d9e73f25d8f

--- src/opts/SkRasterPipeline_opts.h
+++ src/opts/SkRasterPipeline_opts.h
@@ -734,7 +734,8 @@ namespace SK_OPTS_NS {
             if (tail > 1) {
                 _01 = _mm_loadl_pd(_01, (const double*)ptr);            // r0 g0 r1 g1 00 00 00 00
                 if (tail > 2) {
-                    _01 = _mm_loadh_pi(_01, (__m64 const* )(ptr + 4));  // r0 g0 r1 g1 r2 g2 00 00
+                  _01 = _mm_insert_epi16(_01, *(ptr+4), 4);             // r0 g0 r1 g1 r2 00 00 00
+                  _01 = _mm_insert_epi16(_01, *(ptr+5), 5);             // r0 g0 r1 g1 r2 g2 00 00
                 }
             } else {
                 _01 = _mm_loadl_pi(_01, (__m64 const*)ptr + 0);         // r0 g0 00 00 00 00 00 00
