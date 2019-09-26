5c8f1e7ce080d57f6b7bf361405507036ce2129e
Ethan Nicholas <ethannicholas@google.com>
2019-09-26 09:34:20 -0400

    remove 'in uniform' support from GrSkSLFP, make rules more clear
    Bug: skia:
    Change-Id: Iaa4d33c1bfb295d87343411ba6aacc8fae68ef9c

--- src/gpu/SkGr.cpp
+++ src/gpu/SkGr.cpp
@@ -46,7 +46,7 @@
 
 GR_FP_SRC_STRING SKSL_DITHER_SRC = R"(
 // This controls the range of values added to color channels
-layout(key) in int rangeType;
+in int rangeType;
 
 void main(float x, float y, inout half4 color) {
     half value;
