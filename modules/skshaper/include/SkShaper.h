d73cc83418f47798f0fc5d11dc5cc465fa0b3233
Ben Wagner <bungeman@google.com>
2019-10-01 17:54:28 -0400

    Allow better font fallback.
    The intent is to allow the creation of a MakeFontMgrRunIterator which
    uses the passed font's typeface as the primary typeface, but uses a
    given family name and style as for the request for fallback fonts. This
    allows the user to provide the actual request for the primary typeface
    as opposed to making a request based on the resolved primary typeface
    (which may not be the right thing to do).
    
    To support this, the selection of language for fallback is also added.
    Since this information is already in the language iterator, this change
    makes makes the font iterator the lowest priority iterator for consume,
    allowing the font iterator to rely on the current value of the language
    iterator to provide the language.
    
    In order to allow these changes to be exercised, this also adds a few
    generic 'Make' methods for bidi and script. These new methods will use
    the best available implementation. These are needed since the most
    capable implementations may not always be available (such as on our
    testing ios builds).
    
    Change-Id: I1b8d9c9007058adcb2a26e0581d903b835a6118f

--- modules/skshaper/include/SkShaper.h
+++ modules/skshaper/include/SkShaper.h
@@ -50,6 +50,25 @@ public:
         /** Return true if consume should no longer be called. */
         virtual bool atEnd() const = 0;
     };
+    class FontRunIterator : public RunIterator {
+    public:
+        virtual const SkFont& currentFont() const = 0;
+    };
+    class BiDiRunIterator : public RunIterator {
+    public:
+        /** The unicode bidi embedding level (even ltr, odd rtl) */
+        virtual uint8_t currentLevel() const = 0;
+    };
+    class ScriptRunIterator : public RunIterator {
+    public:
+        /** Should be iso15924 codes. */
+        virtual SkFourByteTag currentScript() const = 0;
+    };
+    class LanguageRunIterator : public RunIterator {
+    public:
+        /** Should be BCP-47, c locale names may also work. */
+        virtual const char* currentLanguage() const = 0;
+    };
 
 private:
     template <typename RunIteratorSubclass>
@@ -66,13 +85,14 @@ private:
     };
 
 public:
-    class FontRunIterator : public RunIterator {
-    public:
-        virtual const SkFont& currentFont() const = 0;
-    };
     static std::unique_ptr<FontRunIterator>
     MakeFontMgrRunIterator(const char* utf8, size_t utf8Bytes,
                            const SkFont& font, sk_sp<SkFontMgr> fallback);
+    static std::unique_ptr<SkShaper::FontRunIterator>
+    MakeFontMgrRunIterator(const char* utf8, size_t utf8Bytes,
+                           const SkFont& font, sk_sp<SkFontMgr> fallback,
+                           const char* requestName, SkFontStyle requestStyle,
+                           const SkShaper::LanguageRunIterator*);
     class TrivialFontRunIterator : public TrivialRunIterator<FontRunIterator> {
     public:
         TrivialFontRunIterator(const SkFont& font, size_t utf8Bytes)
@@ -82,11 +102,8 @@ public:
         SkFont fFont;
     };
 
-    class BiDiRunIterator : public RunIterator {
-    public:
-        /** The unicode bidi embedding level (even ltr, odd rtl) */
-        virtual uint8_t currentLevel() const = 0;
-    };
+    static std::unique_ptr<BiDiRunIterator>
+    MakeBiDiRunIterator(const char* utf8, size_t utf8Bytes, uint8_t bidiLevel);
     #ifdef SK_SHAPER_HARFBUZZ_AVAILABLE
     static std::unique_ptr<BiDiRunIterator>
     MakeIcuBiDiRunIterator(const char* utf8, size_t utf8Bytes, uint8_t bidiLevel);
@@ -100,11 +117,8 @@ public:
         uint8_t fBidiLevel;
     };
 
-    class ScriptRunIterator : public RunIterator {
-    public:
-        /** Should be iso15924 codes. */
-        virtual SkFourByteTag currentScript() const = 0;
-    };
+    static std::unique_ptr<ScriptRunIterator>
+    MakeScriptRunIterator(const char* utf8, size_t utf8Bytes, SkFourByteTag script);
     #ifdef SK_SHAPER_HARFBUZZ_AVAILABLE
     static std::unique_ptr<ScriptRunIterator>
     MakeHbIcuScriptRunIterator(const char* utf8, size_t utf8Bytes);
@@ -118,11 +132,6 @@ public:
         SkFourByteTag fScript;
     };
 
-    class LanguageRunIterator : public RunIterator {
-    public:
-        /** Should be BCP-47, c locale names may also work. */
-        virtual const char* currentLanguage() const = 0;
-    };
     static std::unique_ptr<LanguageRunIterator>
     MakeStdLanguageRunIterator(const char* utf8, size_t utf8Bytes);
     class TrivialLanguageRunIterator : public TrivialRunIterator<LanguageRunIterator> {
