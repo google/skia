d543fbbd6fc68e07a4d7199995691fad1fae427a
Herb Derby <herb@google.com>
2019-10-09 23:46:21 +0000

    Revert "Draw perspective text as paths."
    This reverts commit 76826fc91c0abcbd1c8aa4dbc3db4a9d9419e73f.
    
    Reason for revert: Missing some characters
    
    Original change's description:
    > Draw perspective text as paths.
    > 
    > Perspective glyphs can vary in screen size so it's unclear which SDF
    > level is best, and even if we choose one for an entire subrun it's
    > possible that a given glyph will have artifacts if it's too big.
    > Instead we fall back to paths.
    > 
    > Bug: skia:9515
    > Change-Id: I88f03b25651df0222459f5dbd03eee9465b97487
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/247437
    > Commit-Queue: Herb Derby <herb@google.com>
    > Reviewed-by: Jim Van Verth <jvanverth@google.com>
    
    TBR=jvanverth@google.com,herb@google.com
    
    Change-Id: I5e39566c35b49323913eb72ff89ecbc91faac8ce
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:9515
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/247462
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- src/core/SkStrikeSpec.cpp
+++ src/core/SkStrikeSpec.cpp
@@ -181,7 +181,6 @@ SkStrikeSpec::MakeSDFT(const SkFont& font, const SkPaint& paint,
                        const SkSurfaceProps& surfaceProps, const SkMatrix& deviceMatrix,
                        const GrTextContext::Options& options) {
     SkStrikeSpec storage;
-    SkASSERT(!deviceMatrix.hasPerspective());
 
     SkPaint dfPaint = GrTextContext::InitDistanceFieldPaint(paint);
     SkFont dfFont = GrTextContext::InitDistanceFieldFont(
