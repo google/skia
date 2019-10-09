d10f5f0feec7c544835aac8d5e09f56969227e24
Robert Phillips <robertphillips@google.com>
2019-10-09 15:14:59 -0400

    Plumb GrTextureProxies down into emitSampler/addSampler
    This is still far away from this goal but, the goal here is to be able to create a program w/o requiring instantiated proxies.
    
    Bug: skia:9455
    Change-Id: I9526a3a737438c5331fef4a621be4786ef43c080

--- src/gpu/dawn/GrDawnUniformHandler.h
+++ src/gpu/dawn/GrDawnUniformHandler.h
@@ -35,7 +35,7 @@ public:
 private:
     explicit GrDawnUniformHandler(GrGLSLProgramBuilder* program);
 
-    SamplerHandle addSampler(const GrTexture*, const GrSamplerState&, const GrSwizzle&,
+    SamplerHandle addSampler(const GrTextureProxy*, const GrSamplerState&, const GrSwizzle&,
                              const char* name, const GrShaderCaps*) override;
     const char* samplerVariable(SamplerHandle handle) const override;
     GrSwizzle samplerSwizzle(SamplerHandle handle) const override;
