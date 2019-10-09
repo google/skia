835cbdaf70dd30a00e30998d17cbff53e92f93a5
Robert Phillips <robertphillips@google.com>
2019-10-09 15:41:50 -0400

    Pass GrProgramInfo further down Vulkan call chain
    This just resolves some lingering TODOs from:
    
    https://skia-review.googlesource.com/c/skia/+/244118 (Add GrProgramInfo to centralize management of program information)
    
    Bug: skia:9455
    Change-Id: I58e8a4e039ba8369b381725be196dacf3bcd07c5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/247436
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/gpu/vk/GrVkOpsRenderPass.cpp
+++ src/gpu/vk/GrVkOpsRenderPass.cpp
@@ -450,15 +450,14 @@ GrVkPipelineState* GrVkOpsRenderPass::prepareDrawState(
     // same place (i.e., the target renderTargetProxy) that had best agree.
     SkASSERT(programInfo.origin() == fOrigin);
 
-    // TODO: just pass in GrProgramInfo
-    pipelineState->setAndBindUniforms(fGpu, fRenderTarget, fOrigin,
-                                      programInfo.primProc(), programInfo.pipeline(), currentCB);
+    pipelineState->setAndBindUniforms(fGpu, fRenderTarget, programInfo, currentCB);
 
     // Check whether we need to bind textures between each GrMesh. If not we can bind them all now.
     if (!programInfo.hasDynamicPrimProcTextures()) {
-        // TODO: just pass in GrProgramInfo
+        auto proxies = programInfo.hasFixedPrimProcTextures() ? programInfo.fixedPrimProcTextures()
+                                                              : nullptr;
         pipelineState->setAndBindTextures(fGpu, programInfo.primProc(), programInfo.pipeline(),
-                                          programInfo.primProcProxies(), currentCB);
+                                          proxies, currentCB);
     }
 
     if (!programInfo.pipeline().isScissorEnabled()) {
@@ -573,8 +572,7 @@ void GrVkOpsRenderPass::onDraw(const GrProgramInfo& programInfo,
         if (hasDynamicTextures) {
             auto meshProxies = programInfo.dynamicPrimProcTextures(i);
             pipelineState->setAndBindTextures(fGpu, programInfo.primProc(), programInfo.pipeline(),
-                                              meshProxies,
-                                              this->currentCommandBuffer());
+                                              meshProxies, this->currentCommandBuffer());
         }
         SkASSERT(pipelineState);
         mesh.sendToGpu(this);
