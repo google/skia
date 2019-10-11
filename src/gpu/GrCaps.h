d00b83c4abeffcbb8cab81f2cd09fda61cb876d6
Greg Daniel <egdaniel@google.com>
2019-10-11 10:16:46 -0400

    Remove GrPixelConfig version of CompressedDataSize.
    Bug: skia:6718
    Change-Id: I0f9c7d89e732c59bdbe76ab45d42934029106efb

--- src/gpu/GrCaps.h
+++ src/gpu/GrCaps.h
@@ -159,7 +159,11 @@ public:
     }
 
     virtual bool isFormatSRGB(const GrBackendFormat&) const = 0;
-    virtual bool isFormatCompressed(const GrBackendFormat&) const = 0;
+
+    // Callers can optionally pass in an SkImage::CompressionType which will be filled in with the
+    // correct type if the GrBackendFormat is compressed.
+    virtual bool isFormatCompressed(const GrBackendFormat&,
+                                    SkImage::CompressionType* compressionType = nullptr) const = 0;
 
     // TODO: Once we use the supportWritePixels call for uploads, we can remove this function and
     // instead only have the version that takes a GrBackendFormat.
