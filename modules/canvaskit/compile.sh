c42f7a60f04b160030471cee0d01a11b28ba937f
Hal Canary <halcanary@google.com>
2019-10-15 10:15:08 -0400

    canvaskit: add helpful message to compile.sh
    Change-Id: I48a48c8d2f3853fd948cadfc7cc93f41d72e2ea2

--- modules/canvaskit/compile.sh
+++ modules/canvaskit/compile.sh
@@ -9,7 +9,15 @@ set -ex
 BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 # This expects the environment variable EMSDK to be set
 if [[ ! -d $EMSDK ]]; then
-  echo "Be sure to set the EMSDK environment variable."
+  cat >&2 << "EOF"
+Be sure to set the EMSDK environment variable to the location of Emscripten SDK.
+
+To install the Emscripten SDK:
+
+  export EMSDK=~/EmscriptenSDK
+  git clone https://github.com/emscripten-core/emsdk.git "$EMSDK"
+  ( cd "$EMSDK"; ./emsdk install latest; ./emsdk activate latest; )
+EOF
   exit 1
 fi
 
