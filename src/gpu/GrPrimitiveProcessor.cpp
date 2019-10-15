42d70740499684427ebf0fe93e6d02ca44eb1fed
Robert Phillips <robertphillips@google.com>
2019-10-15 15:10:08 -0400

    Remove GrProgramDesc's need for the GrGpu
    Although not strictly necessary, being able to compute the program key w/o needing access to the Gpu will give us more flexibility.
    
    Bug: skia:9455
    Change-Id: I0cd76d79a3008b69be18636be370e7aaa531bc65

--- src/gpu/GrPrimitiveProcessor.cpp
+++ src/gpu/GrPrimitiveProcessor.cpp
@@ -53,22 +53,22 @@ static inline GrSamplerState::Filter clamp_filter(GrTextureType type,
     return requestedFilter;
 }
 
-GrPrimitiveProcessor::TextureSampler::TextureSampler(GrTextureType textureType,
-                                                     const GrSamplerState& samplerState,
-                                                     const GrSwizzle& swizzle,
-                                                     uint32_t extraSamplerKey) {
-    this->reset(textureType, samplerState, swizzle, extraSamplerKey);
+GrPrimitiveProcessor::TextureSampler::TextureSampler(const GrSamplerState& samplerState,
+                                                     const GrBackendFormat& backendFormat,
+                                                     const GrSwizzle& swizzle) {
+                                                     //uint32_t extraSamplerKey) {
+    this->reset(samplerState, backendFormat, swizzle); // , extraSamplerKey);
 }
 
-void GrPrimitiveProcessor::TextureSampler::reset(GrTextureType textureType,
-                                                 const GrSamplerState& samplerState,
-                                                 const GrSwizzle& swizzle,
-                                                 uint32_t extraSamplerKey) {
+void GrPrimitiveProcessor::TextureSampler::reset(const GrSamplerState& samplerState,
+                                                 const GrBackendFormat& backendFormat,
+                                                 const GrSwizzle& swizzle) {
+//                                                 uint32_t extraSamplerKey) {
     fSamplerState = samplerState;
-    fSamplerState.setFilterMode(clamp_filter(textureType, samplerState.filter()));
+    fSamplerState.setFilterMode(clamp_filter(backendFormat.textureType(), samplerState.filter()));
+    fBackendFormat = backendFormat;
     fSwizzle = swizzle;
-    fTextureType = textureType;
-    fExtraSamplerKey = extraSamplerKey;
+//    fExtraSamplerKey = extraSamplerKey;
     fIsInitialized = true;
 }
 
