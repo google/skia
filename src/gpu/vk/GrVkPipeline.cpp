1d1c0c9c66577b57b090974a08258d6e4adeb350
Robert Phillips <robertphillips@google.com>
2019-10-17 13:26:25 -0400

    Reduce input to GrProgramDesc::Build method(s)
    This moves the computation of the GrStencilSettings into GrProgramDesc::Build for the Vulkan backend
    
    This makes it clear that the stencilSettings are determined by the GrProgramInfo.
    
    Note that this does change some behavior in that the number of stencil bits is now determined (at set up for) the number requested not the number actually available on the attached stencil buffer.
    
    Bug: skia:9455
    Change-Id: Ia5fa4e30e34952c1a492b8d571094abf2291b2a4

--- src/gpu/vk/GrVkPipeline.cpp
+++ src/gpu/vk/GrVkPipeline.cpp
@@ -237,9 +237,11 @@ static void setup_stencil_op_state(
     opState->reference = stencilFace.fRef;
 }
 
-static void setup_depth_stencil_state(
-        const GrStencilSettings& stencilSettings, GrSurfaceOrigin origin,
-        VkPipelineDepthStencilStateCreateInfo* stencilInfo) {
+static void setup_depth_stencil_state(const GrProgramInfo& programInfo,
+                                      VkPipelineDepthStencilStateCreateInfo* stencilInfo) {
+    GrStencilSettings stencilSettings = programInfo.stencilSettings();
+    GrSurfaceOrigin origin = programInfo.origin();
+
     memset(stencilInfo, 0, sizeof(VkPipelineDepthStencilStateCreateInfo));
     stencilInfo->sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
     stencilInfo->pNext = nullptr;
@@ -498,7 +500,6 @@ static void setup_dynamic_state(VkPipelineDynamicStateCreateInfo* dynamicInfo,
 GrVkPipeline* GrVkPipeline::Create(
         GrVkGpu* gpu,
         const GrProgramInfo& programInfo,
-        const GrStencilSettings& stencil,
         VkPipelineShaderStageCreateInfo* shaderStageInfo, int shaderStageCount,
         GrPrimitiveType primitiveType, VkRenderPass compatibleRenderPass, VkPipelineLayout layout,
         VkPipelineCache cache) {
@@ -515,7 +516,7 @@ GrVkPipeline* GrVkPipeline::Create(
     setup_input_assembly_state(primitiveType, &inputAssemblyInfo);
 
     VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-    setup_depth_stencil_state(stencil, programInfo.origin(), &depthStencilInfo);
+    setup_depth_stencil_state(programInfo, &depthStencilInfo);
 
     VkPipelineViewportStateCreateInfo viewportInfo;
     setup_viewport_scissor_state(&viewportInfo);
