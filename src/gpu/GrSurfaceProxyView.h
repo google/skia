2791f224caa41144bdbd1f289469032a012d0bb0
Greg Daniel <egdaniel@google.com>
2019-10-21 16:16:35 -0400

    Add GrSurfaceProxyView and add support for it on GrRenderTargetContext.
    Bug: skia:9556
    Change-Id: I681c364cef6867feb0ae06a53cd7d264d7163e35

---
+++ src/gpu/GrSurfaceProxyView.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrSurfaceProxyView_DEFINED
+#define GrSurfaceProxyView_DEFINED
+
+#include "include/core/SkRefCnt.h"
+#include "include/gpu/GrTypes.h"
+#include "src/gpu/GrSurfaceProxy.h"
+#include "src/gpu/GrSwizzle.h"
+
+class GrSurfaceProxyView {
+public:
+    GrSurfaceProxyView(sk_sp<GrSurfaceProxy> proxy, GrSurfaceOrigin origin, GrSwizzle swizzle)
+            : fProxy(proxy), fOrigin(origin), fSwizzle(swizzle) {}
+
+    GrSurfaceProxyView(GrSurfaceProxyView&& view)
+            : fProxy(std::move(view.fProxy)), fOrigin(view.fOrigin), fSwizzle(view.fSwizzle) {}
+
+    GrSurfaceProxy* asSurfaceProxy() { return fProxy.get(); }
+    const GrSurfaceProxy* asSurfaceProxy() const { return fProxy.get(); }
+    sk_sp<GrSurfaceProxy> asSurfaceProxyRef() const { return fProxy; }
+
+    GrTextureProxy* asTextureProxy() { return fProxy->asTextureProxy(); }
+    const GrTextureProxy* asTextureProxy() const { return fProxy->asTextureProxy(); }
+    sk_sp<GrTextureProxy> asTextureProxyRef() {
+        return sk_ref_sp(fProxy->asTextureProxy());
+    }
+
+    GrRenderTargetProxy* asRenderTargetProxy() { return fProxy->asRenderTargetProxy(); }
+    const GrRenderTargetProxy* asRenderTargetProxy() const { return fProxy->asRenderTargetProxy(); }
+    sk_sp<GrRenderTargetProxy> asRenderTargetProxyRef() {
+        return sk_ref_sp(fProxy->asRenderTargetProxy());
+    }
+
+    GrSurfaceOrigin origin() const { return fOrigin; }
+    const GrSwizzle& swizzle() const { return fSwizzle; }
+
+private:
+    sk_sp<GrSurfaceProxy> fProxy;
+    GrSurfaceOrigin fOrigin;
+    GrSwizzle fSwizzle;
+};
+
+#endif
+
