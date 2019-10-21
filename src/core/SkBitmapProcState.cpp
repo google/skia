d9cc9e84954b1ed9d5b4cd55bab519c29d2361f5
Mike Klein <mtklein@google.com>
2019-10-21 12:02:30 -0500

    remove unused SkBitmapProcState fields
    None of these are used.
    
    Change-Id: Ia8c0eb88db747da55fd03ff1632820a521ed1842

--- src/core/SkBitmapProcState.cpp
+++ src/core/SkBitmapProcState.cpp
@@ -179,7 +179,6 @@ bool SkBitmapProcInfo::init(const SkMatrix& inv, const SkPaint& paint) {
     }
     fPixmap = fBMState->pixmap();
     fInvMatrix = fBMState->invMatrix();
-    fRealInvMatrix = fBMState->invMatrix();
     fPaintColor = paint.getColor();
     fFilterQuality = fBMState->quality();
     SkASSERT(fFilterQuality <= kLow_SkFilterQuality);
@@ -246,10 +245,8 @@ bool SkBitmapProcState::chooseProcs() {
     SkASSERT(fFilterQuality < kHigh_SkFilterQuality);
 
     fInvProc            = SkMatrixPriv::GetMapXYProc(fInvMatrix);
-    fInvSx              = SkScalarToFixed        (fInvMatrix.getScaleX());
     fInvSxFractionalInt = SkScalarToFractionalInt(fInvMatrix.getScaleX());
     fInvKy              = SkScalarToFixed        (fInvMatrix.getSkewY());
-    fInvKyFractionalInt = SkScalarToFractionalInt(fInvMatrix.getSkewY());
 
     fAlphaScale = SkAlpha255To256(SkColorGetA(fPaintColor));
 
