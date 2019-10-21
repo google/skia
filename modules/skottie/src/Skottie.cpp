556f45e821f68835621e00c48979741c7a11e02c
Florin Malita <fmalita@chromium.org>
2019-10-21 11:33:28 -0400

    [skottie] Frame-index seek API (seekFrame)
    Change-Id: Id870629e1747a9f70cbf4d3e770366df1299a519

--- modules/skottie/src/Skottie.cpp
+++ modules/skottie/src/Skottie.cpp
@@ -542,6 +542,7 @@ sk_sp<Animation> Animation::Builder::make(const char* data, size_t data_len) {
                                           inPoint,
                                           outPoint,
                                           duration,
+                                          fps,
                                           flags));
 }
 
@@ -553,13 +554,12 @@ sk_sp<Animation> Animation::Builder::makeFromFile(const char path[]) {
 }
 
 Animation::Animation(std::unique_ptr<sksg::Scene> scene, SkString version, const SkSize& size,
-                     SkScalar inPoint, SkScalar outPoint, SkScalar duration, uint32_t flags)
+                     SkScalar inPoint, SkScalar outPoint, SkScalar duration, SkScalar fps,
+                     uint32_t flags)
     : fScene(std::move(scene))
-    , fVersion(std::move(version))
-    , fSize(size)
+    , fInfo({std::move(version), size, fps, duration})
     , fInPoint(inPoint)
     , fOutPoint(outPoint)
-    , fDuration(duration)
     , fFlags(flags) {
 
     // In case the client calls render before the first tick.
@@ -580,7 +580,7 @@ void Animation::render(SkCanvas* canvas, const SkRect* dstR, RenderFlags renderF
 
     SkAutoCanvasRestore restore(canvas, true);
 
-    const SkRect srcR = SkRect::MakeSize(this->size());
+    const SkRect srcR = SkRect::MakeSize(fInfo.fSize);
     if (dstR) {
         canvas->concat(SkMatrix::MakeRectToRect(srcR, *dstR, SkMatrix::kCenter_ScaleToFit));
     }
@@ -597,7 +597,7 @@ void Animation::render(SkCanvas* canvas, const SkRect* dstR, RenderFlags renderF
     fScene->render(canvas);
 }
 
-void Animation::seek(SkScalar t, sksg::InvalidationController* ic) {
+void Animation::seekFrame(SkScalar t, sksg::InvalidationController* ic) {
     TRACE_EVENT0("skottie", TRACE_FUNC);
 
     if (!fScene)
@@ -606,13 +606,15 @@ void Animation::seek(SkScalar t, sksg::InvalidationController* ic) {
     // Per AE/Lottie semantics out_point is exclusive.
     const auto kLastValidFrame = std::nextafter(fOutPoint, fInPoint);
 
-    fScene->animate(SkTPin(fInPoint + t * (fOutPoint - fInPoint), fInPoint, kLastValidFrame), ic);
+    fScene->animate(SkTPin(fInPoint + t, fInPoint, kLastValidFrame), ic);
+}
+
+void Animation::seek(SkScalar t, sksg::InvalidationController *ic) {
+    this->seekFrame(t * (fOutPoint - fInPoint), ic);
 }
 
 void Animation::seekFrameTime(double t, sksg::InvalidationController* ic) {
-    if (double dur = this->duration()) {
-        this->seek((SkScalar)(t / dur), ic);
-    }
+    this->seekFrame(t * fInfo.fFPS, ic);
 }
 
 sk_sp<Animation> Animation::Make(const char* data, size_t length) {
