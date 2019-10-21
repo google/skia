8d5fcae3ec970ff8195ad46a3904dfb5c54540c8
deepak1556 <hop2deep@gmail.com>
2019-10-21 14:59:54 -0700

    build: fix compilation on macOS with 10.14 SDK
    typename 'B0' is defined in sys/termios.h
    
    Bug: none
    Change-Id: I6a2e6eb1a91f123459087280b70493dab795bbd3

--- include/private/SkTLogic.h
+++ include/private/SkTLogic.h
@@ -62,9 +62,9 @@ template<> struct make_index_sequence<1> : skstd::index_sequence<0>{};
 struct monostate {};
 
 template<typename...> struct conjunction : std::true_type { };
-template<typename B0> struct conjunction<B0> : B0 { };
-template<typename B0, typename... Bs>
-struct conjunction<B0, Bs...> : std::conditional<bool(B0::value), conjunction<Bs...>, B0>::type { };
+template<typename T> struct conjunction<T> : T { };
+template<typename T, typename... Ts>
+struct conjunction<T, Ts...> : std::conditional<bool(T::value), conjunction<Ts...>, T>::type { };
 }  // namespace skstd
 
 // The sknonstd namespace contains things we would like to be proposed and feel std-ish.
