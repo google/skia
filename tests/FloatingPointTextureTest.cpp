0f5cda4827a2fe70cb029c266c811c6a8e107299
Brian Salomon <bsalomon@google.com>
2019-10-21 15:04:52 -0400

    Simplify sk_gpu_test::MakeTextureProxyFromData.
    Make it take GrImageInfo.
    
    Change-Id: I9ec16e9b935fbe3e829b4669f715c17873a1793a

--- tests/FloatingPointTextureTest.cpp
+++ tests/FloatingPointTextureTest.cpp
@@ -47,9 +47,10 @@ void runFPTest(skiatest::Reporter* reporter, GrContext* context, T min, T max, T
     }
 
     for (auto origin : {kTopLeft_GrSurfaceOrigin, kBottomLeft_GrSurfaceOrigin}) {
-        auto fpProxy = sk_gpu_test::MakeTextureProxyFromData(context, GrRenderable::kYes, DEV_W,
-                                                             DEV_H, colorType, kPremul_SkAlphaType,
-                                                             origin, controlPixelData.begin(), 0);
+        auto fpProxy = sk_gpu_test::MakeTextureProxyFromData(
+                context, GrRenderable::kYes, origin,
+                {colorType, kPremul_SkAlphaType, nullptr, DEV_W, DEV_H},
+                controlPixelData.begin(), 0);
         // Floating point textures are NOT supported everywhere
         if (!fpProxy) {
             continue;
--- tools/gpu/ProxyUtils.cpp
+++ tools/gpu/ProxyUtils.cpp
@@ -18,67 +18,39 @@ namespace sk_gpu_test {
 
 sk_sp<GrTextureProxy> MakeTextureProxyFromData(GrContext* context,
                                                GrRenderable renderable,
-                                               int width,
-                                               int height,
-                                               GrColorType colorType, SkAlphaType alphaType,
                                                GrSurfaceOrigin origin,
-                                               const void* data, size_t rowBytes) {
+                                               const GrImageInfo& imageInfo,
+                                               const void* data,
+                                               size_t rowBytes) {
     if (context->priv().abandoned()) {
         return nullptr;
     }
 
     const GrCaps* caps = context->priv().caps();
 
-    const GrBackendFormat format = caps->getDefaultBackendFormat(colorType, renderable);
+    const GrBackendFormat format = caps->getDefaultBackendFormat(imageInfo.colorType(), renderable);
     if (!format.isValid()) {
         return nullptr;
     }
 
     sk_sp<GrTextureProxy> proxy;
-    if (kBottomLeft_GrSurfaceOrigin == origin) {
-        // We (soon will) only support using kBottomLeft with wrapped textures.
-        auto backendTex = context->createBackendTexture(
-                width, height, format, SkColors::kTransparent, GrMipMapped::kNo, renderable,
-                GrProtected::kNo);
-        if (!backendTex.isValid()) {
-            return nullptr;
-        }
-
-        // Adopt ownership so our caller doesn't have to worry about deleting the backend texture.
-        if (GrRenderable::kYes == renderable) {
-            proxy = context->priv().proxyProvider()->wrapRenderableBackendTexture(
-                    backendTex, origin, 1, colorType, kAdopt_GrWrapOwnership, GrWrapCacheable::kNo,
-                    nullptr, nullptr);
-        } else {
-            proxy = context->priv().proxyProvider()->wrapBackendTexture(
-                    backendTex, colorType, origin, kAdopt_GrWrapOwnership,
-                    GrWrapCacheable::kNo, kRW_GrIOType);
-        }
-
-        if (!proxy) {
-            context->deleteBackendTexture(backendTex);
-            return nullptr;
-        }
-
-    } else {
-        GrSurfaceDesc desc;
-        desc.fConfig = GrColorTypeToPixelConfig(colorType);
-        desc.fWidth = width;
-        desc.fHeight = height;
-        proxy = context->priv().proxyProvider()->createProxy(format, desc, renderable, 1, origin,
-                                                             GrMipMapped::kNo, SkBackingFit::kExact,
-                                                             SkBudgeted::kYes, GrProtected::kNo);
-        if (!proxy) {
-            return nullptr;
-        }
+    GrSurfaceDesc desc;
+    desc.fConfig = GrColorTypeToPixelConfig(imageInfo.colorType());
+    desc.fWidth = imageInfo.width();
+    desc.fHeight = imageInfo.height();
+    proxy = context->priv().proxyProvider()->createProxy(format, desc, renderable, 1, origin,
+                                                         GrMipMapped::kNo, SkBackingFit::kExact,
+                                                         SkBudgeted::kYes, GrProtected::kNo);
+    if (!proxy) {
+        return nullptr;
     }
-
-    auto sContext = context->priv().makeWrappedSurfaceContext(proxy, colorType, alphaType, nullptr);
+    auto sContext = context->priv().makeWrappedSurfaceContext(proxy, imageInfo.colorType(),
+                                                              imageInfo.alphaType(),
+                                                              imageInfo.refColorSpace(), nullptr);
     if (!sContext) {
         return nullptr;
     }
-    if (!sContext->writePixels({colorType, alphaType, nullptr, width, height}, data, rowBytes,
-                               {0, 0}, context)) {
+    if (!sContext->writePixels(imageInfo, data, rowBytes, {0, 0}, context)) {
         return nullptr;
     }
     return proxy;
