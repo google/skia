11eeb94bfec368895665ccdf898024aba8e6943d
Michael Ludwig <michaelludwig@google.com>
2019-10-23 19:14:54 +0000

    Revert "hide color filters from blitters"
    This reverts commit cba64462d2662006ba9b94c7d4676ad4daf097bb.
    
    Reason for revert: ASAN failures: https://logs.chromium.org/logs/skia/4812b6e5916f3d11/+/steps/symbolized_dm/0/stdout
    
    Original change's description:
    > hide color filters from blitters
    > 
    > This is all guarded by the old define we never rebaselined.
    > 
    > Had to make some upgrades to SkColorFilterShader to support
    > replacing an SkPaint's shader and color filter with a single
    > merged SkColorFilterShader.  Our pipeline's order of operations
    > is
    > 
    >    1) shader
    >    2) scale by paint alpha
    >    3) color filter
    > 
    > but SkColorFilterShader had no way to get that paint alpha in
    > the middle there, so I added one.
    > 
    > Change-Id: Ibc1448c1b421747086030337c1a30b8f0af4aa5e
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/250296
    > Commit-Queue: Mike Klein <mtklein@google.com>
    > Reviewed-by: Mike Reed <reed@google.com>
    
    TBR=mtklein@google.com,reed@google.com
    
    Change-Id: Icc3a3916ca763de979c01d65746d74c49e14544c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/250261
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- src/shaders/SkColorFilterShader.cpp
+++ src/shaders/SkColorFilterShader.cpp
@@ -8,7 +8,6 @@
 #include "include/core/SkShader.h"
 #include "include/core/SkString.h"
 #include "src/core/SkArenaAlloc.h"
-#include "src/core/SkRasterPipeline.h"
 #include "src/core/SkReadBuffer.h"
 #include "src/core/SkWriteBuffer.h"
 #include "src/shaders/SkColorFilterShader.h"
@@ -17,12 +16,9 @@
 #include "src/gpu/GrFragmentProcessor.h"
 #endif
 
-SkColorFilterShader::SkColorFilterShader(sk_sp<SkShader> shader,
-                                         float alpha,
-                                         sk_sp<SkColorFilter> filter)
+SkColorFilterShader::SkColorFilterShader(sk_sp<SkShader> shader, sk_sp<SkColorFilter> filter)
     : fShader(std::move(shader))
     , fFilter(std::move(filter))
-    , fAlpha (alpha)
 {
     SkASSERT(fShader);
     SkASSERT(fFilter);
@@ -34,21 +30,17 @@ sk_sp<SkFlattenable> SkColorFilterShader::CreateProc(SkReadBuffer& buffer) {
     if (!shader || !filter) {
         return nullptr;
     }
-    return sk_make_sp<SkColorFilterShader>(shader, 1.0f, filter);
+    return sk_make_sp<SkColorFilterShader>(shader, filter);
 }
 
 void SkColorFilterShader::flatten(SkWriteBuffer& buffer) const {
     buffer.writeFlattenable(fShader.get());
-    SkASSERT(fAlpha == 1.0f);  // Not exposed in public API SkShader::makeWithColorFilter().
     buffer.writeFlattenable(fFilter.get());
 }
 
 bool SkColorFilterShader::onAppendStages(const SkStageRec& rec) const {
     if (!as_SB(fShader)->appendStages(rec)) {
         return false;
-    }
-    if (fAlpha != 1.0f) {
-        rec.fPipeline->append(SkRasterPipeline::scale_1_float, &fAlpha);
     }
     fFilter->appendStages(rec, fShader->isOpaque());
     return true;
@@ -65,9 +57,6 @@ std::unique_ptr<GrFragmentProcessor> SkColorFilterShader::asFragmentProcessor(
     if (!fp1) {
         return nullptr;
     }
-
-    // TODO I guess, but it shouldn't come up as used today.
-    SkASSERT(fAlpha == 1.0f);
 
     auto fp2 = fFilter->asFragmentProcessor(args.fContext, *args.fDstColorInfo);
     if (!fp2) {
@@ -86,5 +75,5 @@ sk_sp<SkShader> SkShader::makeWithColorFilter(sk_sp<SkColorFilter> filter) const
     if (!filter) {
         return sk_ref_sp(base);
     }
-    return sk_make_sp<SkColorFilterShader>(sk_ref_sp(base), 1.0f, filter);
+    return sk_make_sp<SkColorFilterShader>(sk_ref_sp(base), filter);
 }
