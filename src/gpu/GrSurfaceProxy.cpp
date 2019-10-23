ed6cd00c1d215d1e5f2292108ee2b1095d58c72c
Robert Phillips <robertphillips@google.com>
2019-10-23 11:16:16 -0400

    Rename worstCase to whenAllocated in GrSurfaceProxy
    These names are more appropriate after:
    
    https://skia-review.googlesource.com/c/skia/+/241356 (Always create an approx-size texture for approx-fit proxies))
    
    Note that this CL does have one substantive change in GrCoordTransform::hasSameEffectAs where we use our knowledge of the size at which the backing stores will be allocated to remove a sly usage of GrSurfaceProxy::underlyingUniqueID.
    
    Change-Id: Ibbb8cb23c4503f267abbdf4794610f3262a6f515

--- src/gpu/GrSurfaceProxy.cpp
+++ src/gpu/GrSurfaceProxy.cpp
@@ -266,7 +266,7 @@ void GrSurfaceProxy::computeScratchKey(GrScratchKey* key) const {
         mipMapped = tp->mipMapped();
     }
 
-    GrTexturePriv::ComputeScratchKey(this->config(), this->worstCaseDimensions(), renderable,
+    GrTexturePriv::ComputeScratchKey(this->config(), this->dimensionsWhenAllocated(), renderable,
                                      sampleCount, mipMapped, fIsProtected, key);
 }
 
@@ -285,7 +285,7 @@ GrOpsTask* GrSurfaceProxy::getLastOpsTask() {
     return fLastRenderTask ? fLastRenderTask->asOpsTask() : nullptr;
 }
 
-SkISize GrSurfaceProxy::worstCaseDimensions() const {
+SkISize GrSurfaceProxy::dimensionsWhenAllocated() const {
     SkASSERT(!this->isFullyLazy());
     if (fTarget) {
         return fTarget->dimensions();
