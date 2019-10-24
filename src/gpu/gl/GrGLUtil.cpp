d2c67f44f8c2351e60e6ee224a060e916cd44f34
Brian Salomon <bsalomon@google.com>
2019-10-24 14:58:40 -0400

    Reland "Allow direct readback for formats in non-ES OpenGL"
    This is a reland of 162d16891fa07768d9fb9fbcd357a06878284907
    
    Original change's description:
    > Allow direct readback for formats in non-ES OpenGL
    > 
    > Bug: skia:8962
    > Change-Id: I85a2e228892a54cc2a0abd607d034515a9af12af
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/250258
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    
    Bug: skia:8962
    Change-Id: Id878df9747653f2b34e551073a4562277e789e9d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/250657
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/gpu/gl/GrGLUtil.cpp
+++ src/gpu/gl/GrGLUtil.cpp
@@ -461,18 +461,26 @@ GrGLRenderer GrGLGetRendererFromStrings(const char* rendererString,
         }
 
         // The AMD string can have a somewhat arbitrary preamble (see skbug.com/7195)
-        if (const char* amdString = strstr(rendererString, "Radeon")) {
+        static constexpr char kRadeonStr[] = "Radeon ";
+        if (const char* amdString = strstr(rendererString, kRadeonStr)) {
+            amdString += strlen(kRadeonStr);
             char amdGeneration, amdTier, amdRevision;
-            n = sscanf(amdString, "Radeon (TM) R9 M%c%c%c",
-                                       &amdGeneration, &amdTier, &amdRevision);
+            // Sometimes there is a (TM) and sometimes not.
+            static constexpr char kTMStr[] = "(TM) ";
+            if (!strncmp(amdString, kTMStr, strlen(kTMStr))) {
+                amdString += strlen(kTMStr);
+            }
+            n = sscanf(amdString, "R9 M%c%c%c", &amdGeneration, &amdTier, &amdRevision);
             if (3 == n) {
-                if ('4' == amdGeneration) {
+                if ('3' == amdGeneration) {
+                    return kAMDRadeonR9M3xx_GrGLRenderer;
+                } else if ('4' == amdGeneration) {
                     return kAMDRadeonR9M4xx_GrGLRenderer;
                 }
             }
 
             char amd0, amd1, amd2;
-            n = sscanf(amdString, "Radeon HD 7%c%c%c Series", &amd0, &amd1, &amd2);
+            n = sscanf(amdString, "HD 7%c%c%c Series", &amd0, &amd1, &amd2);
             if (3 == n) {
                 return kAMDRadeonHD7xxx_GrGLRenderer;
             }
