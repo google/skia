7a13b461e6e08eaea9dea2a90ca4724e9c1979eb
Mike Klein <mtklein@google.com>
2019-11-05 07:46:02 -0600

    x86-64 JIT support for Op::index
    This lets shaders that use 'x' JIT on x86.
    
    I started with paddd and {0,-1,-2,-3,...}, which worked fine but on
    second thought seemed a bit odd.  I've switched to psubd and
    {0,1,2,3,...} but I've left in support for paddd with a memory arg.
    
    gm/skvm.cpp now JITs fully again and continues to draw the same as
    the interpreter did.
    
    Simplify embedded data alignment a little... memory operands don't
    need full register alignment in AVX like they used to in SSE.  So
    just align everything to the vector element size like we do on ARM,
    and reorder [splats,bytes_masks,iota] to match the order we declare
    and handle them in the code above.
    
    Add unit tests for vpaddd + vpsubd.
    
    Cq-Include-Trybots: skia.primary:Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-SK_USE_SKVM_BLITTER
    Change-Id: I6b8d060450cca7f437a1d2a597a8a0e0e8d51b33
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/252797
    Commit-Queue: Mike Klein <mtklein@google.com>
    Reviewed-by: Mike Reed <reed@google.com>

--- src/core/SkVM.cpp
+++ src/core/SkVM.cpp
@@ -841,6 +841,8 @@ namespace skvm {
     }
 
     void Assembler::vpshufb(Ymm dst, Ymm x, Label* l) { this->op(0x66,0x380f,0x00, dst,x,l); }
+    void Assembler::vpaddd (Ymm dst, Ymm x, Label* l) { this->op(0x66,  0x0f,0xfe, dst,x,l); }
+    void Assembler::vpsubd (Ymm dst, Ymm x, Label* l) { this->op(0x66,  0x0f,0xfa, dst,x,l); }
 
     void Assembler::vbroadcastss(Ymm dst, Label* l) { this->op(0x66,0x380f,0x18, dst, (Ymm)0, l); }
     void Assembler::vbroadcastss(Ymm dst, Xmm src)  { this->op(0x66,0x380f,0x18, dst, (Ymm)src); }
@@ -1663,6 +1665,7 @@ namespace skvm {
         };
         SkTHashMap<int, LabelAndReg> splats,
                                      bytes_masks;
+        LabelAndReg                  iota;
 
         auto warmup = [&](Val id) {
             const Builder::Instruction& inst = instructions[id];
@@ -1846,6 +1849,11 @@ namespace skvm {
 
                 case Op::uniform32: a->vbroadcastss(dst(), arg[imm&0xffff], imm>>16);
                                     break;
+
+                case Op::index: a->vmovd_direct((A::Xmm)tmp(), N);
+                                a->vbroadcastss(tmp(), (A::Xmm)tmp());
+                                a->vpsubd(dst(), tmp(), &iota.label);
+                                break;
 
                 case Op::splat: a->vbroadcastss(dst(), &splats.find(imm)->label);
                                 break;
@@ -2077,22 +2085,10 @@ namespace skvm {
             exit();
         }
 
-        bytes_masks.foreach([&](int imm, LabelAndReg* entry) {
-            // One 16-byte pattern for ARM tbl, that same pattern twice for x86-64 vpshufb.
-        #if defined(__x86_64__)
-            a->align(32);
-        #elif defined(__aarch64__)
-            a->align(4);
-        #endif
-
-            a->label(&entry->label);
-            int mask[4];
-            bytes_control(imm, mask);
-            a->bytes(mask, sizeof(mask));
-        #if defined(__x86_64__)
-            a->bytes(mask, sizeof(mask));
-        #endif
-        });
+        // Except for explicit aligned load and store instructions, AVX allows
+        // memory operands to be unaligned.  So even though bytes_masks and
+        // iota use 32-byte patterns on x86, we need only align them to 4
+        // bytes, the element size and required alignment on ARM.
 
         splats.foreach([&](int imm, LabelAndReg* entry) {
             // vbroadcastss 4 bytes on x86-64, or simply load 16-bytes on aarch64.
@@ -2105,6 +2101,27 @@ namespace skvm {
             a->word(imm);
         #endif
         });
+
+        bytes_masks.foreach([&](int imm, LabelAndReg* entry) {
+            // One 16-byte pattern for ARM tbl, that same pattern twice for x86-64 vpshufb.
+            a->align(4);
+            a->label(&entry->label);
+            int mask[4];
+            bytes_control(imm, mask);
+            a->bytes(mask, sizeof(mask));
+        #if defined(__x86_64__)
+            a->bytes(mask, sizeof(mask));
+        #endif
+        });
+
+        if (!iota.label.references.empty()) {
+            a->align(4);
+            a->label(&iota.label);
+            for (int i = 0; i < K; i++) {
+                a->word(i);
+            }
+        }
+
 
         return true;
     }
