f523c8e3f78c97038d5967efcde2488e81b2c657
Herb Derby <herb@google.com>
2019-11-11 18:24:48 -0500

    WS: SkStrike
    Change-Id: I2419ebbd53e50d39a1469d97f1b7736b0db73da6

--- src/core/SkStrike.cpp
+++ src/core/SkStrike.cpp
@@ -125,8 +125,7 @@ SkGlyph* SkStrike::mergeGlyphAndImage(SkPackedGlyphID toID, const SkGlyph& from)
     return glyph;
 }
 
-const SkGlyph* SkStrike::getCachedGlyphAnySubPix(SkGlyphID glyphID,
-                                                     SkPackedGlyphID vetoID) const {
+const SkGlyph* SkStrike::getCachedGlyphAnySubPix(SkGlyphID glyphID, SkPackedGlyphID vetoID) const {
     for (SkFixed subY = 0; subY < SK_Fixed1; subY += SK_FixedQuarter) {
         for (SkFixed subX = 0; subX < SK_Fixed1; subX += SK_FixedQuarter) {
             SkPackedGlyphID packedGlyphID{glyphID, subX, subY};
