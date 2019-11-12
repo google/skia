258755c6bd05b8e4ee34a7335316ab3c656ef9fb
Robert Phillips <robertphillips@google.com>
2019-11-12 09:46:09 -0500

    Use a priori knowledge about the number of stencil bits in Dawn, Metal and Vulkan backends
    The goal here is to centralize more of the program key creation w/in GrProgramInfo. For Dawn,
    Metal and Vulkan, afaict, the number of stencil bits is always 8. We can use this information
     to stop passing the GrStencilSettings object around. For GL, the number of stencil bits is
     variable but it is never part of the key.
    
    Bug: skia:9455
    Change-Id: I8fd2bea2422c5b9df69fc184d3a82013eef5407e

--- src/gpu/GrStencilSettings.h
+++ src/gpu/GrStencilSettings.h
@@ -48,15 +48,15 @@ static constexpr int kGrStencilOpCount = 1 + (int)GrStencilOp::kDecClamp;
 class GrStencilSettings {
 public:
     GrStencilSettings() { this->setDisabled(); }
-    GrStencilSettings(const GrUserStencilSettings& user, bool hasStencilClip, int numStencilBits) {
-        this->reset(user, hasStencilClip, numStencilBits);
+    GrStencilSettings(const GrUserStencilSettings& user, bool hasStencilClip, int numStencilBits, bool foo) {
+        this->reset2(user, hasStencilClip, numStencilBits);
     }
     GrStencilSettings(const GrStencilSettings& that) { this->reset(that); }
     GrStencilSettings& operator=(const GrStencilSettings& that) { this->reset(that); return *this; }
 
     void invalidate() { fFlags |= kInvalid_PrivateFlag; }
     void setDisabled() { fFlags = kAll_StencilFlags; }
-    void reset(const GrUserStencilSettings&, bool hasStencilClip, int numStencilBits);
+    void reset2(const GrUserStencilSettings&, bool hasStencilClip, int numStencilBits);
     void reset(const GrStencilSettings&);
 
     bool isValid() const { return !(fFlags & kInvalid_PrivateFlag); }
