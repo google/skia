ea3699d5b35b9d4db21a33d98d906ddf561b982a
Chris Dalton <csmartdalton@google.com>
2019-11-14 05:28:54 +0000

    Revert submission
    Reason for revert: Crash on Win10 GTX660 and bad rendering on Adreno
    
    Change-Id: Id8834f424130029cb7e1cad5b6ef827f2c809cd6

--- src/gpu/glsl/GrGLSLFragmentShaderBuilder.cpp
+++ src/gpu/glsl/GrGLSLFragmentShaderBuilder.cpp
@@ -291,8 +291,7 @@ void GrGLSLFragmentShaderBuilder::onFinalize() {
 
     if (CustomFeatures::kSampleLocations & fProgramBuilder->processorFeatures()) {
         const SkTArray<SkPoint>& sampleLocations = fProgramBuilder->getSampleLocations();
-        this->definitions().appendf("const float2 _sampleOffsets[%i] = float2[%i](",
-                                    sampleLocations.count(), sampleLocations.count());
+        this->definitions().append("const float2 _sampleOffsets[] = float2[](");
         for (int i = 0; i < sampleLocations.count(); ++i) {
             SkPoint offset = sampleLocations[i] - SkPoint::Make(.5f, .5f);
             if (kBottomLeft_GrSurfaceOrigin == this->getSurfaceOrigin()) {
