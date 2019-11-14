7dea9a7b3d664e3f55e72f53155bc418d6b1e6a0
Chris Dalton <csmartdalton@google.com>
2019-11-13 18:04:13 -0700

    Reland "Implement sample mask and sample locations support in Vulkan"
    This is a reland of 8b915a0c27652d5932ff4013ba98c57baf553c3c
    
    Original change's description:
    > Implement sample mask and sample locations support in Vulkan
    > 
    > Change-Id: I372695ec5360def42a8a997675993264740b0da4
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/252038
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > Reviewed-by: Chris Dalton <csmartdalton@google.com>
    
    Change-Id: I20de36719db52ad4dfc5290101d48a8fd9601c11

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -473,6 +473,10 @@ void GrVkCaps::initGrCaps(const GrVkInterface* vkInterface,
     static const uint32_t kMaxVertexAttributes = 64;
     fMaxVertexAttributes = SkTMin(properties.limits.maxVertexInputAttributes, kMaxVertexAttributes);
 
+    if (properties.limits.standardSampleLocations) {
+        fSampleLocationsSupport = true;
+    }
+
     if (extensions.hasExtension(VK_EXT_SAMPLE_LOCATIONS_EXTENSION_NAME, 1)) {
         // We "disable" multisample by colocating all samples at pixel center.
         fMultisampleDisableSupport = true;
@@ -548,7 +552,7 @@ void GrVkCaps::initShaderCaps(const VkPhysicalDeviceProperties& properties,
     // to be true with Vulkan as well.
     shaderCaps->fPreferFlatInterpolation = kQualcomm_VkVendor != properties.vendorID;
 
-    // GrShaderCaps
+    shaderCaps->fSampleMaskSupport = true;
 
     shaderCaps->fShaderDerivativeSupport = true;
 
