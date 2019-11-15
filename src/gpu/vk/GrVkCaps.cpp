cb73b6250ec57b2d0e1d778753713140baef4d6d
Chris Dalton <csmartdalton@google.com>
2019-11-15 14:05:02 -0700

    Enable msaa ccpr on vulkan
    TBR=egdaniel@google.com
    
    Bug: skia:9643
    Change-Id: Ie06891734b700c940b996a63c7b1264ce0f2597c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/254808
    Reviewed-by: Chris Dalton <csmartdalton@google.com>
    Commit-Queue: Chris Dalton <csmartdalton@google.com>
    Auto-Submit: Chris Dalton <csmartdalton@google.com>

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -441,10 +441,6 @@ void GrVkCaps::applyDriverCorrectnessWorkarounds(const VkPhysicalDevicePropertie
     // GrCaps workarounds
     ////////////////////////////////////////////////////////////////////////////
 
-    // Temporarily disable the MSAA implementation of CCPR while we work out a crash on Win10
-    // GTX660 and incorrect rendring on Adreno.
-    fDriverBlacklistMSAACCPR = true;
-
     if (kARM_VkVendor == properties.vendorID) {
         fInstanceAttribSupport = false;
         fAvoidWritePixelsFastPath = true; // bugs.skia.org/8064
