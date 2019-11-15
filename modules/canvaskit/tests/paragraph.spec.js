049126716000b0ef5e8bce92d647c2a64dc50243
Kevin Lubick <kjlubick@google.com>
2019-11-15 14:48:55 -0500

    [canvaskit] Expose more SkParagraph methods
    Bug: skia:9469
    
    Change-Id: I922a877fc20d29b95e68f93842505df5dead6934
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/254960
    Reviewed-by: Julia Lavrova <jlavrova@google.com>

--- modules/canvaskit/tests/paragraph.spec.js
+++ modules/canvaskit/tests/paragraph.spec.js
@@ -85,6 +85,23 @@ describe('CanvasKit\'s Path Behavior', function() {
 
             paragraph.layout(wrapTo);
 
+            expect(paragraph.didExceedMaxLines()).toBeTruthy();
+            expect(paragraph.getAlphabeticBaseline()).toBeCloseTo(21.377, 3);
+            expect(paragraph.getHeight()).toEqual(240);
+            expect(paragraph.getIdeographicBaseline()).toBeCloseTo(27.236, 3);
+            expect(paragraph.getLongestLine()).toBeCloseTo(142.129, 3);
+            expect(paragraph.getMaxIntrinsicWidth()).toBeCloseTo(1444.250, 3);
+            expect(paragraph.getMaxWidth()).toEqual(200);
+            expect(paragraph.getMinIntrinsicWidth()).toBeCloseTo(172.360, 3);
+            expect(paragraph.getWordBoundary(8)).toEqual({
+                start: 0,
+                end: 14,
+            });
+            expect(paragraph.getWordBoundary(25)).toEqual({
+                start: 25,
+                end: 26,
+            });
+
             canvas.drawRect(CanvasKit.LTRBRect(10, 10, wrapTo+10, 230), paint);
             canvas.drawParagraph(paragraph, 10, 10);
 
