2ccb69208907c6ed753a8f9beeb5704830483fc0
Stephen White <senorblanco@chromium.org>
2019-11-19 17:04:45 -0500

    Roll dawn and adjust for new SwapChain API.
    Split GrDawnImageInfo into GrDawnTextureInfo and GrDawnRenderTargetInfo.
    The former holds only a wgpu::Texture, and the latter holds only a
    wgpu::TextureView. This split is necessary because Dawn SwapChains now
    vend TextureViews, not Textures.
    
    The TextureView held by GrDawnRenderTargetInfo is always 1-mip, since
    it's a requirement for rendering to a texture in Dawn.
    
    Change-Id: Id6e99b5e4bf18f97e939170856a665e2038253ea

--- src/gpu/GrBackendSurface.cpp
+++ src/gpu/GrBackendSurface.cpp
@@ -288,7 +288,7 @@ SkString GrBackendFormat::toStr() const {
 #ifdef SK_DAWN
 GrBackendTexture::GrBackendTexture(int width,
                                    int height,
-                                   const GrDawnImageInfo& dawnInfo)
+                                   const GrDawnTextureInfo& dawnInfo)
         : fIsValid(true)
         , fWidth(width)
         , fHeight(height)
@@ -441,7 +441,7 @@ GrBackendTexture& GrBackendTexture::operator=(const GrBackendTexture& that) {
 }
 
 #ifdef SK_DAWN
-bool GrBackendTexture::getDawnImageInfo(GrDawnImageInfo* outInfo) const {
+bool GrBackendTexture::getDawnTextureInfo(GrDawnTextureInfo* outInfo) const {
     if (this->isValid() && GrBackendApi::kDawn == fBackend) {
         *outInfo = fDawnInfo;
         return true;
@@ -638,7 +638,7 @@ GrBackendRenderTarget::GrBackendRenderTarget(int width,
                                              int height,
                                              int sampleCnt,
                                              int stencilBits,
-                                             const GrDawnImageInfo& dawnInfo)
+                                             const GrDawnRenderTargetInfo& dawnInfo)
         : fIsValid(true)
         , fWidth(width)
         , fHeight(height)
@@ -789,7 +789,7 @@ GrBackendRenderTarget& GrBackendRenderTarget::operator=(const GrBackendRenderTar
 }
 
 #ifdef SK_DAWN
-bool GrBackendRenderTarget::getDawnImageInfo(GrDawnImageInfo* outInfo) const {
+bool GrBackendRenderTarget::getDawnRenderTargetInfo(GrDawnRenderTargetInfo* outInfo) const {
     if (this->isValid() && GrBackendApi::kDawn == fBackend) {
         *outInfo = fDawnInfo;
         return true;
@@ -869,6 +869,13 @@ GrBackendFormat GrBackendRenderTarget::getBackendFormat() const {
             GrMtlTextureInfo mtlInfo;
             SkAssertResult(this->getMtlTextureInfo(&mtlInfo));
             return GrBackendFormat::MakeMtl(GrGetMTLPixelFormatFromMtlTextureInfo(mtlInfo));
+        }
+#endif
+#ifdef SK_DAWN
+        case GrBackendApi::kDawn: {
+            GrDawnRenderTargetInfo dawnInfo;
+            SkAssertResult(this->getDawnRenderTargetInfo(&dawnInfo));
+            return GrBackendFormat::MakeDawn(dawnInfo.fFormat);
         }
 #endif
         case GrBackendApi::kMock:
