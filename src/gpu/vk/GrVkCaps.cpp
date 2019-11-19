a0f975bacb2f1197ba3a303c3db13603d1b94ac5
Chris Dalton <csmartdalton@google.com>
2019-11-18 20:07:42 -0700

    Reland "Reland "Enable msaa ccpr on vulkan""
    This is a reland of 935a35d972e56e1616393a2361ebf005c1e09bfb
    
    Original change's description:
    > Reland "Enable msaa ccpr on vulkan"
    > 
    > This is a reland of cb73b6250ec57b2d0e1d778753713140baef4d6d
    > 
    > Original change's description:
    > > Enable msaa ccpr on vulkan
    > >
    > > TBR=egdaniel@google.com
    > >
    > > Bug: skia:9643
    > > Change-Id: Ie06891734b700c940b996a63c7b1264ce0f2597c
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/254808
    > > Reviewed-by: Chris Dalton <csmartdalton@google.com>
    > > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > > Auto-Submit: Chris Dalton <csmartdalton@google.com>
    > 
    > Bug: skia:9643
    > Change-Id: I2537f03fad41ebddabbae94312b3836f7a948639
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/255008
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    
    Bug: skia:9643
    Change-Id: Ibade6deba89282f62af5fc835e1b775bce2bc5a5

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -385,13 +385,14 @@ void GrVkCaps::init(const GrContextOptions& contextOptions, const GrVkInterface*
     this->initStencilFormat(vkInterface, physDev);
 
     if (!contextOptions.fDisableDriverCorrectnessWorkarounds) {
-        this->applyDriverCorrectnessWorkarounds(properties);
+        this->applyDriverCorrectnessWorkarounds(properties, extensions);
     }
 
     this->finishInitialization(contextOptions);
 }
 
-void GrVkCaps::applyDriverCorrectnessWorkarounds(const VkPhysicalDeviceProperties& properties) {
+void GrVkCaps::applyDriverCorrectnessWorkarounds(const VkPhysicalDeviceProperties& properties,
+                                                 const GrVkExtensions& extensions) {
     if (kQualcomm_VkVendor == properties.vendorID) {
         fMustDoCopiesFromOrigin = true;
         // Transfer doesn't support this workaround.
@@ -441,9 +442,17 @@ void GrVkCaps::applyDriverCorrectnessWorkarounds(const VkPhysicalDevicePropertie
     // GrCaps workarounds
     ////////////////////////////////////////////////////////////////////////////
 
-    // Temporarily disable the MSAA implementation of CCPR while we work out a crash on Win10
-    // GTX660 and incorrect rendring on Adreno.
-    fDriverBlacklistMSAACCPR = true;
+    // The GTX660 bot experiences crashes when running msaa ccpr with 8k textures.
+    // (We get VK_ERROR_DEVICE_LOST when calling vkGetFenceStatus.)
+    // ((Checking for mixed samples is an easy way to detect pre-maxwell architectures.))
+    bool isNVIDIAPreMaxwell = (kNvidia_VkVendor == properties.vendorID) &&
+            !extensions.hasExtension(VK_NV_FRAMEBUFFER_MIXED_SAMPLES_EXTENSION_NAME, 1);
+
+    if (isNVIDIAPreMaxwell || fDriverBugWorkarounds.max_texture_size_limit_4096) {
+        fMaxTextureSize = SkTMin(fMaxTextureSize, 4096);
+        fMaxRenderTargetSize = SkTMin(fMaxRenderTargetSize, fMaxTextureSize);
+        fMaxPreferredRenderTargetSize = SkTMin(fMaxPreferredRenderTargetSize, fMaxRenderTargetSize);
+    }
 
     if (kARM_VkVendor == properties.vendorID) {
         fInstanceAttribSupport = false;
@@ -500,9 +509,6 @@ void GrVkCaps::initGrCaps(const GrVkInterface* vkInterface,
     // give the minimum max size across all configs. So for simplicity we will use that for now.
     fMaxRenderTargetSize = SkTMin(properties.limits.maxImageDimension2D, (uint32_t)INT_MAX);
     fMaxTextureSize = SkTMin(properties.limits.maxImageDimension2D, (uint32_t)INT_MAX);
-    if (fDriverBugWorkarounds.max_texture_size_limit_4096) {
-        fMaxTextureSize = SkTMin(fMaxTextureSize, 4096);
-    }
     // Our render targets are always created with textures as the color
     // attachment, hence this min:
     fMaxRenderTargetSize = SkTMin(fMaxTextureSize, fMaxRenderTargetSize);
