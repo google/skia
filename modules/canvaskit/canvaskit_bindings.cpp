72058ef4d7a378db2f0217f0f1438674983dacef
Mike Reed <reed@google.com>
2019-11-21 11:46:46 -0500

    Use flat version of path-direction enum
    Change-Id: I00077d9f2b14b3e983e6a46ef6f560cabdb1678d

--- modules/canvaskit/canvaskit_bindings.cpp
+++ modules/canvaskit/canvaskit_bindings.cpp
@@ -193,8 +193,8 @@ void ApplyAddArc(SkPath& orig, const SkRect& oval, SkScalar startAngle, SkScalar
 }
 
 void ApplyAddOval(SkPath& orig, const SkRect& oval, bool ccw, unsigned start) {
-    orig.addOval(oval, ccw ? SkPath::Direction::kCCW_Direction :
-                             SkPath::Direction::kCW_Direction, start);
+    orig.addOval(oval, ccw ? SkPathDirection::kCCW_Direction :
+                             SkPathDirection::kCW_Direction, start);
 }
 
 void ApplyAddPath(SkPath& orig, const SkPath& newPath,
@@ -212,8 +212,8 @@ void ApplyAddPath(SkPath& orig, const SkPath& newPath,
 void ApplyAddRect(SkPath& path, SkScalar left, SkScalar top,
                   SkScalar right, SkScalar bottom, bool ccw) {
     path.addRect(left, top, right, bottom,
-                 ccw ? SkPath::Direction::kCCW_Direction :
-                 SkPath::Direction::kCW_Direction);
+                 ccw ? SkPathDirection::kCCW_Direction :
+                 SkPathDirection::kCW_Direction);
 }
 
 void ApplyAddRoundRect(SkPath& path, SkScalar left, SkScalar top,
@@ -222,7 +222,7 @@ void ApplyAddRoundRect(SkPath& path, SkScalar left, SkScalar top,
     // See comment below for uintptr_t explanation
     const SkScalar* radii = reinterpret_cast<const SkScalar*>(rPtr);
     path.addRoundRect(SkRect::MakeLTRB(left, top, right, bottom), radii,
-                      ccw ? SkPath::Direction::kCCW_Direction : SkPath::Direction::kCW_Direction);
+                      ccw ? SkPathDirection::kCCW_Direction : SkPathDirection::kCW_Direction);
 }
 
 
@@ -238,7 +238,7 @@ void ApplyArcToAngle(SkPath& p, SkRect& oval, SkScalar startAngle, SkScalar swee
 void ApplyArcToArcSize(SkPath& orig, SkScalar rx, SkScalar ry, SkScalar xAxisRotate,
                        bool useSmallArc, bool ccw, SkScalar x, SkScalar y) {
     auto arcSize = useSmallArc ? SkPath::ArcSize::kSmall_ArcSize : SkPath::ArcSize::kLarge_ArcSize;
-    auto sweep = ccw ? SkPath::Direction::kCCW_Direction : SkPath::Direction::kCW_Direction;
+    auto sweep = ccw ? SkPathDirection::kCCW_Direction : SkPathDirection::kCW_Direction;
     orig.arcTo(rx, ry, xAxisRotate, arcSize, sweep, x, y);
 }
 
