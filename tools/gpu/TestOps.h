3d9c69e847c2b7c3f95c0986c2a97bfb693f53ca
Brian Salomon <bsalomon@google.com>
2019-11-22 15:36:35 -0500

    Test texture domain effect with local matrix.
    Add a common way to make rect op for testing that uses a GP with a local
    matrix.
    
    Change-Id: I958d1230bd5067b2e4b60fcd374e2f7718681e43

---
+++ tools/gpu/TestOps.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef TestRectOp_DEFINED
+#define TestRectOp_DEFINED
+
+#include "include/core/SkRefCnt.h"
+#include "include/private/GrRecordingContext.h"
+#include "src/gpu/GrRecordingContextPriv.h"
+#include "src/gpu/ops/GrMeshDrawOp.h"
+
+class GrPaint;
+
+namespace sk_gpu_test::test_ops {
+
+/**
+ * Fully specified device space rect op. The test Op draws a rectangle with local coords and a
+ * local matrix. It is important to test effects in the presence of GP local matrices. Our standard
+ * rect drawing code doesn't exercise this because it applies any local matrix to pre-transformed
+ * local coord vertex attributes.
+ */
+std::unique_ptr<GrDrawOp> MakeRect(GrRecordingContext*,
+                                   GrPaint&&,
+                                   const SkRect& drawRect,
+                                   const SkRect& localRect,
+                                   const SkMatrix& localM = SkMatrix::I());
+
+/**
+ * A simpler version of MakeRect that takes a single color FP instead of a full paint. Uses
+ * SkBlendMode::kSrcOver.
+ */
+std::unique_ptr<GrDrawOp> MakeRect(GrRecordingContext*,
+                                   std::unique_ptr<GrFragmentProcessor>,
+                                   const SkRect& drawRect,
+                                   const SkRect& localRect,
+                                   const SkMatrix& localM = SkMatrix::I());
+
+/**
+ * A simpler version of MakeRect that uses the same rect as the device space rect to draw as well as
+ * the local rect. The local matrix is identity.
+ */
+std::unique_ptr<GrDrawOp> MakeRect(GrRecordingContext*, GrPaint&&, const SkRect& rect);
+
+}  // namespace sk_gpu_test::test_ops
+
+#endif
