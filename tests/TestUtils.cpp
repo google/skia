25a6fe4af2a3ba043e861332b796960be3447b1d
Brian Salomon <bsalomon@google.com>
2019-11-21 23:21:48 -0500

    Revert "Revert "Make FP optimizations helpers use SkAlphaType not GrColorType""
    This reverts commit 078e8faa26d8b1f33a92e57f587be011150d1776.
    
    Change-Id: I67b2970584db5a1afbf9928b77c5444947ef71a3

--- tests/TestUtils.cpp
+++ tests/TestUtils.cpp
@@ -81,9 +81,8 @@ void TestCopyFromSurface(skiatest::Reporter* reporter,
                          GrColorType colorType,
                          uint32_t expectedPixelValues[],
                          const char* testName) {
-    sk_sp<GrTextureProxy> dstProxy = GrSurfaceProxy::Copy(context, proxy, colorType,
-                                                          GrMipMapped::kNo, SkBackingFit::kExact,
-                                                          SkBudgeted::kYes);
+    sk_sp<GrTextureProxy> dstProxy = GrSurfaceProxy::Copy(context, proxy, GrMipMapped::kNo,
+                                                          SkBackingFit::kExact, SkBudgeted::kYes);
     SkASSERT(dstProxy);
 
     auto dstContext = context->priv().makeWrappedSurfaceContext(std::move(dstProxy), colorType,
