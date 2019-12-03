26710fd218742ff777414a0b657dc539d80f3e2d
Mike Klein <mtklein@google.com>
2019-12-03 11:14:24 -0600

    remove SkColorSpaceXformSteps::Required
    It's error-prone, and now unused.
    
    Change-Id: Ibae848bdb121695521e66d6e8544a82678e22f7c

--- src/core/SkColorSpaceXformSteps.cpp
+++ src/core/SkColorSpaceXformSteps.cpp
@@ -12,14 +12,6 @@
 
 // TODO(mtklein): explain the logic of this file
 
-bool SkColorSpaceXformSteps::Required(SkColorSpace* src, SkColorSpace* dst) {
-    // Any SkAlphaType will work fine here as long as we use the same one.
-    SkAlphaType at = kPremul_SkAlphaType;
-    return 0 != SkColorSpaceXformSteps(src, at,
-                                       dst, at).flags.mask();
-    // TODO(mtklein): quicker impl. that doesn't construct an SkColorSpaceXformSteps?
-}
-
 SkColorSpaceXformSteps::SkColorSpaceXformSteps(SkColorSpace* src, SkAlphaType srcAT,
                                                SkColorSpace* dst, SkAlphaType dstAT) {
     // Opaque outputs are treated as the same alpha type as the source input.
