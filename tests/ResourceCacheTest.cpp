226cbf6bc3d775147cc72140fa4a2eaba7fe2c45
Greg Daniel <egdaniel@google.com>
2019-12-04 11:14:29 -0500

    Remove unneeded abandon/release check in GrGpuResource message processing.
    Change-Id: Ib11405be82e8ff7fd1edcaca2f19c44cce2abbd9

--- tests/ResourceCacheTest.cpp
+++ tests/ResourceCacheTest.cpp
@@ -1592,6 +1592,9 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(ResourceMessagesAfterAbandon, reporter, ctxIn
 
     REPORTER_ASSERT(reporter, 0 == freed);
 
+    // We must delete the backend texture before abandoning the context in vulkan. We just do it
+    // for all the backends for consistency.
+    context->deleteBackendTexture(backend);
     context->abandonContext();
 
     REPORTER_ASSERT(reporter, 1 == freed);
@@ -1602,8 +1605,6 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(ResourceMessagesAfterAbandon, reporter, ctxIn
     SkMessageBus<GrTextureFreedMessage>::Post(msg);
 
     context->purgeUnlockedResources(false);
-
-    context->deleteBackendTexture(backend);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
