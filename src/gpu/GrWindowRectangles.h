b6d51df8996226a93682d6ca5696f6699504c58a
Mike Klein <mtklein@google.com>
2019-12-04 12:57:53 -0600

    refactoring GrWindowRectangles
    I think I've refactored and simplified GrWindowRectangles
    about as much as I can, and yet GCC 8 Release mips64el is
    still unhappy in just the same way.  :/  Anything jump out?
    
    Commit: false
    Change-Id: I97d69a0a6272dcf68b6320b07de9cab874b6b21c

--- src/gpu/GrWindowRectangles.h
+++ src/gpu/GrWindowRectangles.h
@@ -15,109 +15,63 @@ class GrWindowRectangles {
 public:
     constexpr static int kMaxWindows = 8;
 
-    GrWindowRectangles() : fCount(0) {}
-    GrWindowRectangles(const GrWindowRectangles& that) : fCount(0) { *this = that; }
-    ~GrWindowRectangles() { SkSafeUnref(this->rec()); }
+    GrWindowRectangles() { this->reset(); }
 
-    GrWindowRectangles makeOffset(int dx, int dy) const;
+    int  count() const { return fCount; }
+    bool empty() const { return this->count() == 0; }
 
-    bool empty() const { return !fCount; }
-    int count() const { return fCount; }
-    const SkIRect* data() const;
+    const SkIRect* data() const {
+        return fHeap ? fHeap->windows
+                     : &fInlineWindow;
+    }
 
-    void reset();
-    GrWindowRectangles& operator=(const GrWindowRectangles&);
+    void reset() {
+        fCount        = 0;
+        fInlineWindow = {0,0,0,0};
+        fHeap         = nullptr;
+    }
 
-    SkIRect& addWindow(const SkIRect& window) { return this->addWindow() = window; }
-    SkIRect& addWindow();
+    SkIRect& addWindow(const SkIRect& window) { return (this->addWindow() = window); }
+    SkIRect& addWindow() {
+        SkASSERT(fCount < kMaxWindows);
+
+        if (fCount == 0) {
+            fCount = 1;
+            return fInlineWindow;
+        }
+
+        if (!fHeap || !fHeap->unique()) {
+            sk_sp<Rec> heap = sk_make_sp<Rec>();
+            std::copy_n(this->data(), fCount, heap->windows);
+            fHeap = std::move(heap);
+        }
+        return fHeap->windows[fCount++];
+    }
 
     bool operator!=(const GrWindowRectangles& that) const { return !(*this == that); }
-    bool operator==(const GrWindowRectangles&) const;
+    bool operator==(const GrWindowRectangles& that) const {
+        return this->count() == that.count()
+            && std::equal(this->data(), this->data() + this->count(), that.data());
+    }
+
+    GrWindowRectangles makeOffset(int dx, int dy) const {
+        if (dx == 0 && dy == 0) {
+            return *this;
+        }
+
+        GrWindowRectangles result;
+        for (int i = 0; i < this->count(); i++) {
+            result.addWindow(this->data()[i].makeOffset(dx,dy));
+        }
+        return result;
+    }
 
 private:
-    struct Rec;
+    struct Rec : public GrNonAtomicRef<Rec> { SkIRect windows[kMaxWindows]; };
 
-    const Rec* rec() const { return fCount <= 1 ? nullptr : fRec; }
-
-    int fCount;
-    union {
-        SkIRect   fLocalWindow; // If fCount <= 1
-        Rec*      fRec;         // If fCount >  1.
-    };
+    int        fCount;
+    SkIRect    fInlineWindow;
+    sk_sp<Rec> fHeap;
 };
-
-struct GrWindowRectangles::Rec : public GrNonAtomicRef<Rec> {
-    Rec(const SkIRect* windows, int numWindows) {
-        SkASSERT(numWindows < kMaxWindows);
-        memcpy(fData, windows, sizeof(SkIRect) * numWindows);
-    }
-    Rec() = default;
-
-    SkIRect fData[kMaxWindows];
-};
-
-inline const SkIRect* GrWindowRectangles::data() const {
-    return fCount <= 1 ? &fLocalWindow : fRec->fData;
-}
-
-inline void GrWindowRectangles::reset() {
-    SkSafeUnref(this->rec());
-    fCount = 0;
-}
-
-inline GrWindowRectangles& GrWindowRectangles::operator=(const GrWindowRectangles& that) {
-    SkSafeUnref(this->rec());
-    fCount = that.fCount;
-    if (fCount <= 1) {
-        fLocalWindow = that.fLocalWindow;
-    } else {
-        fRec = SkRef(that.fRec);
-    }
-    return *this;
-}
-
-inline GrWindowRectangles GrWindowRectangles::makeOffset(int dx, int dy) const {
-    if (!dx && !dy) {
-        return *this;
-    }
-    GrWindowRectangles result;
-    result.fCount = fCount;
-    SkIRect* windows;
-    if (result.fCount > 1) {
-        result.fRec = new Rec();
-        windows = result.fRec->fData;
-    } else {
-        windows = &result.fLocalWindow;
-    }
-    for (int i = 0; i < fCount; ++i) {
-        windows[i] = this->data()[i].makeOffset(dx, dy);
-    }
-    return result;
-}
-
-inline SkIRect& GrWindowRectangles::addWindow() {
-    SkASSERT(fCount < kMaxWindows);
-    if (fCount == 0) {
-        fCount = 1;
-        return fLocalWindow;
-    }
-    if (fCount == 1) {
-        fRec = new Rec(&fLocalWindow, 1);
-    } else if (!fRec->unique()) { // Simple copy-on-write.
-        fRec->unref();
-        fRec = new Rec(fRec->fData, fCount);
-    }
-    return fRec->fData[fCount++];
-}
-
-inline bool GrWindowRectangles::operator==(const GrWindowRectangles& that) const {
-    if (fCount != that.fCount) {
-        return false;
-    }
-    if (fCount > 1 && fRec == that.fRec) {
-        return true;
-    }
-    return !fCount || !memcmp(this->data(), that.data(), sizeof(SkIRect) * fCount);
-}
 
 #endif
