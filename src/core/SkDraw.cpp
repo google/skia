4417a01de76faf8ac33812df4a8b49b17015f8b1
Mike Reed <reed@google.com>
2019-12-05 12:30:18 -0500

    clip paths in perspective (as needed)
    Change-Id: I3807312a20e8c9699b9ede8706a6e47bb354480a

--- src/core/SkDraw.cpp
+++ src/core/SkDraw.cpp
@@ -956,6 +956,12 @@ void SkDraw::drawPath(const SkPath& origSrcPath, const SkPaint& origPaint,
     // avoid possibly allocating a new path in transform if we can
     SkPath* devPathPtr = pathIsMutable ? pathPtr : tmpPath;
 
+    // pre-clip if needed right before we apply the CTM
+    SkPath perspectiveClippedPath;
+    if (SkPathPriv::PerspectiveClip(*pathPtr, *matrix, &perspectiveClippedPath)) {
+        pathPtr = &perspectiveClippedPath;
+    }
+
     // transform the path into device space
     pathPtr->transform(*matrix, devPathPtr);
 
