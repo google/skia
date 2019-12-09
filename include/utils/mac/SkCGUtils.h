99733bb34a72348f5e25806e5aed181d77906acb
Hal Canary <halcanary@google.com>
2019-12-09 10:47:44 -0500

    SkCGUtils: add SkCGDrawPixmap
      - replace getBitmapInfo with simpler get_bitmap_info.
    
      - Use more unique_ptrs to manage CG objects.
    
    Change-Id: I77d338ec1754ab87c491c3565d4237c6b605128d

--- include/utils/mac/SkCGUtils.h
+++ include/utils/mac/SkCGUtils.h
@@ -74,5 +74,13 @@ static inline CGImageRef SkCreateCGImageRef(const SkBitmap& bm) {
  */
 void SkCGDrawBitmap(CGContextRef, const SkBitmap&, float x, float y);
 
+/**
+ *  Draw the bitmap into the specified CG context. The pixmap will be converted
+ *  to a CGImage using the generic RGB colorspace. (x,y) specifies the position
+ *  of the top-left corner of the bitmap. The bitmap is converted using the
+ *  colorspace returned by CGColorSpaceCreateDeviceRGB()
+ */
+void SkCGDrawPixmap(CGContextRef, const SkPixmap&, float x, float y);
+
 #endif  // defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 #endif  // SkCGUtils_DEFINED
