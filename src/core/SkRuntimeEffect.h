9a3b87e97b95185bede74b6e9a27e850f89f460d
Brian Osman <brianosman@google.com>
2019-12-11 15:10:46 -0500

    Major refactor of runtime effects
    Adds SkRuntimeEffect, a ref-counted object to hold SkSL,
    a Compiler, and compiled Program, along with uniform info.
    
    Removes the old SkSLFP factory and factory cache types.
    Caching is done by simply holding on to the effect (or
    the shader/color filter factories, which own an effect).
    
    The effect class is simply ref'd by the factories, the
    shader and color filter types, and the FP.
    
    Change-Id: I0c1401505cb234e83943ad4ef2db6680f4c69fb9

---
+++ src/core/SkRuntimeEffect.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkRuntimeEffect_DEFINED
+#define SkRuntimeEffect_DEFINED
+
+#include "include/core/SkString.h"
+#include "src/sksl/SkSLCompiler.h"
+
+namespace SkSL {
+class ByteCode;
+struct Program;
+struct Variable;
+}
+
+class SkRuntimeEffect : public SkRefCnt {
+public:
+    static sk_sp<SkRuntimeEffect> Make(SkString sksl);
+
+    const SkString& source() const { return fSkSL; }
+    int index() const { return fIndex; }
+    bool isValid() const { return fBaseProgram != nullptr; }
+
+    std::unique_ptr<SkSL::Program> getSpecialization(const void* inputs, size_t inputSize);
+
+    // Returns ByteCode, ErrorCount, ErrorText
+    std::tuple<std::unique_ptr<SkSL::ByteCode>, int, SkString> toByteCode();
+
+private:
+    SkRuntimeEffect(SkString sksl);
+
+    int fIndex;
+    SkString fSkSL;
+
+    SkSL::Compiler fCompiler;
+    std::unique_ptr<SkSL::Program> fBaseProgram;
+    std::vector<const SkSL::Variable*> fInAndUniformVars;
+
+    friend class GrGLSLSkSLFP;
+    friend class GrSkSLFP;
+};
+
+#endif
