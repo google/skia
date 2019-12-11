e1fa70000a30b2f4bea6fe25d5f009953acac933
Florin Malita <fmalita@chromium.org>
2019-12-11 11:16:11 -0500

    [skottie] Invert effect support
    https://helpx.adobe.com/after-effects/using/channel-effects.html#invert_effect
    
    Change-Id: Iac8e291ab9cb57714c50f1e40cecb66b3dc64ee1
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/259276
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Mike Klein <mtklein@google.com>

---
+++ modules/skottie/src/effects/InvertEffect.cpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "modules/skottie/src/effects/Effects.h"
+
+#include "modules/skottie/src/SkottieAdapter.h"
+#include "modules/skottie/src/SkottieValue.h"
+#include "modules/sksg/include/SkSGColorFilter.h"
+#include "src/utils/SkJSON.h"
+
+namespace skottie {
+namespace internal {
+
+namespace {
+
+class InvertEffectAdapter final : public DiscardableAdaptorBase {
+public:
+    static sk_sp<InvertEffectAdapter> Make(const skjson::ArrayValue& jprops,
+                                           sk_sp<sksg::RenderNode> layer,
+                                           const AnimationBuilder* abuilder) {
+        enum : size_t {
+            kChannel_Index = 0,
+        };
+
+        auto      adapter = sk_sp<InvertEffectAdapter>(new InvertEffectAdapter(std::move(layer)));
+        auto* raw_adapter = adapter.get();
+
+        abuilder->bindProperty<ScalarValue>(EffectBuilder::GetPropValue(jprops, kChannel_Index),
+            [raw_adapter](const ScalarValue& c) {
+                raw_adapter->fChannel = c;
+            });
+
+        return adapter;
+    }
+
+    const sk_sp<sksg::ExternalColorFilter>& renderNode() const { return fColorFilter; }
+
+private:
+    explicit InvertEffectAdapter(sk_sp<sksg::RenderNode> layer)
+        : fColorFilter(sksg::ExternalColorFilter::Make(std::move(layer))) {}
+
+    void onSync() override {
+        struct STColorMatrix {
+            std::array<float,4> scale,
+                                trans;
+            bool                hsla;
+        };
+
+        const auto stcm = [this]() -> STColorMatrix {
+            // https://helpx.adobe.com/after-effects/using/channel-effects.html#invert_effect
+            enum : uint8_t {
+                kRGB_Channel =  1,
+                  kR_Channel =  2,
+                  kG_Channel =  3,
+                  kB_Channel =  4,
+
+                // NB: HLS vs. HSL
+                kHLS_Channel =  6,
+                  kH_Channel =  7,
+                  kL_Channel =  8,
+                  kS_Channel =  9,
+
+                  // kYIQ_Channel = ?,
+                  // kLum_Channel = ?,
+                  // kIPC_Channel = ?,
+                  // kQAC_Channel = ?,
+
+                  kA_Channel = 16,
+            };
+
+            switch (static_cast<uint8_t>(fChannel)) {
+            case   kR_Channel: return { {-1, 1, 1, 1}, {  1,0,0,0}, false}; // r' = 1 - r
+            case   kG_Channel: return { { 1,-1, 1, 1}, {  0,1,0,0}, false}; // g' = 1 - g
+            case   kB_Channel: return { { 1, 1,-1, 1}, {  0,0,1,0}, false}; // b' = 1 - b
+            case   kA_Channel: return { { 1, 1, 1,-1}, {  0,0,0,1}, false}; // a' = 1 - a
+            case kRGB_Channel: return { {-1,-1,-1, 1}, {  1,1,1,0}, false};
+
+            case   kH_Channel: return { {-1, 1, 1, 1}, {.5f,0,0,0},  true}; // h' = .5 - h
+            case   kS_Channel: return { { 1,-1, 1, 1}, {  0,1,0,0},  true}; // s' = 1 - s
+            case   kL_Channel: return { { 1, 1,-1, 1}, {  0,0,1,0},  true}; // l' = 1 - l
+            case kHLS_Channel: return { {-1,-1,-1, 1}, {.5f,1,1,0},  true};
+
+                      default: return { { 1, 1, 1, 1}, {  0,0,0,0}, false};
+            }
+
+            SkUNREACHABLE;
+        }();
+
+        const float m[] = {
+            stcm.scale[0],             0,             0,             0, stcm.trans[0],
+                        0, stcm.scale[1],             0,             0, stcm.trans[1],
+                        0,             0, stcm.scale[2],             0, stcm.trans[2],
+                        0,             0,             0, stcm.scale[3], stcm.trans[3],
+
+        };
+
+        fColorFilter->setColorFilter(stcm.hsla ? SkColorFilters::HSLAMatrix(m)
+                                               : SkColorFilters::Matrix(m));
+    }
+
+    const sk_sp<sksg::ExternalColorFilter> fColorFilter;
+
+    float fChannel = 0;
+};
+
+} // namespace
+
+sk_sp<sksg::RenderNode> EffectBuilder::attachInvertEffect(const skjson::ArrayValue& jprops,
+                                                          sk_sp<sksg::RenderNode> layer) const {
+    return fBuilder->attachDiscardableAdapter<InvertEffectAdapter>(jprops,
+                                                                   std::move(layer),
+                                                                   fBuilder);
+}
+
+} // namespace internal
+} // namespace skottie
