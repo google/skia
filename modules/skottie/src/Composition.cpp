46a331b93f54d8b3bce88792dd8679beef11a751
Florin Malita <fmalita@chromium.org>
2019-12-12 15:43:53 -0500

    [skottie] Cascading track matte support
    Currently, we treat track matte source layers (tagged with td:1) as single-shot mask triggers:
    we apply once to the following layer, then move on.
    
    But track mattes can cascade: a layer with a matte can itself be applied as a track matte for the
    following layer.
    
    Also, for matte/masking purposes, only the layer content is being considered (ignoring blend mode
    and any masks applied to the matte itself).
    
    To support this, refactor the layer attachment code:
    
      - instead of tracking the presence of a single-shot matte source, always track
        previous layer content trees
      - instead of triggering matte attachment in the presence of a matte source, trigger based on
        the matte *target* property (tt: X)
      - log errors on unknown matte modes
    
    Change-Id: I6c71d4007e1e27d3f3a139344bbf367d7bc6e29d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/259820
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- modules/skottie/src/Composition.cpp
+++ modules/skottie/src/Composition.cpp
@@ -172,14 +172,6 @@ CompositionBuilder::CompositionBuilder(const AnimationBuilder& abuilder,
 
 CompositionBuilder::~CompositionBuilder() = default;
 
-void CompositionBuilder::pushMatte(sk_sp<sksg::RenderNode> matte) {
-    fCurrentMatte = std::move(matte);
-}
-
-sk_sp<sksg::RenderNode> CompositionBuilder::popMatte() {
-    return std::move(fCurrentMatte);
-}
-
 LayerBuilder* CompositionBuilder::layerBuilder(int layer_index) {
     if (layer_index < 0) {
         return nullptr;
@@ -202,10 +194,12 @@ sk_sp<sksg::RenderNode> CompositionBuilder::build(const AnimationBuilder& abuild
     std::vector<sk_sp<sksg::RenderNode>> layers;
     layers.reserve(fLayerBuilders.size());
 
+    LayerBuilder* prev_layer = nullptr;
     for (auto& lbuilder : fLayerBuilders) {
-        if (auto layer = lbuilder.buildRenderTree(abuilder, this)) {
+        if (auto layer = lbuilder.buildRenderTree(abuilder, this, prev_layer)) {
             layers.push_back(std::move(layer));
         }
+        prev_layer = &lbuilder;
     }
 
     if (layers.empty()) {
