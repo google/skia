cb15ddb66b0744e9453e6fdec11c497f20a94a5c
Florin Malita <fmalita@chromium.org>
2019-12-12 15:19:00 -0500

    [skottie] Cascading matte support
    Change-Id: I6c71d4007e1e27d3f3a139344bbf367d7bc6e29d

--- modules/skottie/src/Layer.cpp
+++ modules/skottie/src/Layer.cpp
@@ -363,7 +363,8 @@ bool LayerBuilder::hasMotionBlur(const CompositionBuilder* cbuilder) const {
 }
 
 sk_sp<sksg::RenderNode> LayerBuilder::buildRenderTree(const AnimationBuilder& abuilder,
-                                                      CompositionBuilder* cbuilder) {
+                                                      CompositionBuilder* cbuilder,
+                                                      const LayerBuilder* prev_layer) {
     AnimationBuilder::LayerInfo layer_info = {
         cbuilder->fSize,
         ParseDefault<float>(fJlayer["ip"], 0.0f),
@@ -479,20 +480,26 @@ sk_sp<sksg::RenderNode> LayerBuilder::buildRenderTree(const AnimationBuilder& ab
         return nullptr;
     }
 
-    if (auto matte = cbuilder->popMatte()) {
-        // There is a pending matte (|layer| is a matte target).
-        static constexpr sksg::MaskEffect::Mode gMaskModes[] = {
+    // Stash the content tree in case it is needed for later mattes.
+    fContentTree = layer;
+
+    // Optional matte.
+    size_t matte_mode;
+    if (prev_layer && Parse(fJlayer["tt"], &matte_mode)) {
+        static constexpr sksg::MaskEffect::Mode gMatteModes[] = {
             sksg::MaskEffect::Mode::kAlphaNormal, // tt: 1
             sksg::MaskEffect::Mode::kAlphaInvert, // tt: 2
             sksg::MaskEffect::Mode::kLumaNormal,  // tt: 3
             sksg::MaskEffect::Mode::kLumaInvert,  // tt: 4
         };
-        const auto matteType = ParseDefault<size_t>(fJlayer["tt"], 1) - 1;
 
-        if (matteType < SK_ARRAY_COUNT(gMaskModes)) {
+        if (matte_mode > 0 && matte_mode <= SK_ARRAY_COUNT(gMatteModes)) {
+            // The current layer is masked with the previous layer *content*.
             layer = sksg::MaskEffect::Make(std::move(layer),
-                                           std::move(matte),
-                                           gMaskModes[matteType]);
+                                           prev_layer->fContentTree,
+                                           gMatteModes[matte_mode - 1]);
+        } else {
+            abuilder.log(Logger::Level::kError, nullptr, "Unknown matte mode: %zu\n", matte_mode);
         }
     }
 
@@ -504,7 +511,6 @@ sk_sp<sksg::RenderNode> LayerBuilder::buildRenderTree(const AnimationBuilder& ab
 
     if (ParseDefault<bool>(fJlayer["td"], false)) {
         // |layer| is a matte.  We apply it as a mask to the next layer.
-        cbuilder->pushMatte(std::move(layer));
         return nullptr;
     }
 
