22e77fceb9014812d8e14be37f3de2fa48f8bd7a
Brian Osman <brianosman@google.com>
2019-12-12 11:55:41 -0500

    Major refactor of runtime effects
    Adds SkRuntimeEffect, a ref-counted object to hold SkSL,
    a Compiler, and compiled Program, along with uniform info.
    
    Removes the old SkSLFP factory and factory cache types.
    Caching is done by simply holding on to the effect (or
    the shader/color filter factories, which own an effect).
    
    The effect class is simply ref'd by the factories, the
    shader and color filter types, and the FP.
    
    Change-Id: I0c1401505cb234e83943ad4ef2db6680f4c69fb9

---
+++ src/core/SkRuntimeEffect.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/core/SkRuntimeEffect.h"
+#include "src/sksl/SkSLByteCode.h"
+#include "src/sksl/SkSLCompiler.h"
+#include "src/sksl/ir/SkSLVarDeclarations.h"
+
+static inline int new_sksl_index() {
+    static std::atomic<int> nextIndex{ 0 };
+    return nextIndex++;
+}
+
+sk_sp<SkRuntimeEffect> SkRuntimeEffect::Make(SkString sksl) {
+    return sk_sp<SkRuntimeEffect>(new SkRuntimeEffect(std::move(sksl)));
+}
+
+SkRuntimeEffect::SkRuntimeEffect(SkString sksl)
+        : fIndex(new_sksl_index())
+        , fSkSL(std::move(sksl)) {
+    fBaseProgram = fCompiler.convertProgram(SkSL::Program::kPipelineStage_Kind,
+                                            SkSL::String(fSkSL.c_str(), fSkSL.size()),
+                                            SkSL::Program::Settings());
+    if (!fBaseProgram) {
+        return;
+    }
+    SkASSERT(!fCompiler.errorCount());
+
+    for (const auto& e : *fBaseProgram) {
+        if (e.fKind == SkSL::ProgramElement::kVar_Kind) {
+            SkSL::VarDeclarations& v = (SkSL::VarDeclarations&) e;
+            for (const auto& varStatement : v.fVars) {
+                const SkSL::Variable& var = *((SkSL::VarDeclaration&) * varStatement).fVar;
+                if ((var.fModifiers.fFlags & SkSL::Modifiers::kIn_Flag) ||
+                    (var.fModifiers.fFlags & SkSL::Modifiers::kUniform_Flag)) {
+                    fInAndUniformVars.push_back(&var);
+                }
+                // "in uniform" doesn't make sense outside of .fp files
+                SkASSERT((var.fModifiers.fFlags & SkSL::Modifiers::kIn_Flag) == 0 ||
+                    (var.fModifiers.fFlags & SkSL::Modifiers::kUniform_Flag) == 0);
+                // "layout(key)" doesn't make sense outside of .fp files; all 'in' variables are
+                // part of the key
+                SkASSERT(!var.fModifiers.fLayout.fKey);
+            }
+        }
+    }
+}
+
+static std::tuple<const SkSL::Type*, int> strip_array(const SkSL::Type* type) {
+    int arrayCount = 0;
+    if (type->kind() == SkSL::Type::kArray_Kind) {
+        arrayCount = type->columns();
+        type = &type->componentType();
+    }
+    return std::make_tuple(type, arrayCount);
+}
+
+std::unique_ptr<SkSL::Program> SkRuntimeEffect::getSpecialization(const void* inputs,
+                                                                  size_t inputSize) {
+    std::unordered_map<SkSL::String, SkSL::Program::Settings::Value> inputMap;
+    size_t offset = 0;
+    for (const auto& v : fInAndUniformVars) {
+        auto [type, arrayCount] = strip_array(&v->fType);
+        arrayCount = SkTMax(1, arrayCount);
+        SkSL::String name(v->fName);
+        if (type == fCompiler.context().fInt_Type.get() ||
+            type == fCompiler.context().fShort_Type.get()) {
+            offset = SkAlign4(offset);
+            if (v->fModifiers.fFlags & SkSL::Modifiers::kIn_Flag) {
+                int32_t v = *(int32_t*)(((uint8_t*)inputs) + offset);
+                inputMap.insert(std::make_pair(name, SkSL::Program::Settings::Value(v)));
+            }
+            offset += sizeof(int32_t) * arrayCount;
+        } else if (type == fCompiler.context().fFloat_Type.get() ||
+                   type == fCompiler.context().fHalf_Type.get()) {
+            offset = SkAlign4(offset);
+            if (v->fModifiers.fFlags & SkSL::Modifiers::kIn_Flag) {
+                float v = *(float*)(((uint8_t*)inputs) + offset);
+                inputMap.insert(std::make_pair(name, SkSL::Program::Settings::Value(v)));
+            }
+            offset += sizeof(float) * arrayCount;
+        } else if (type == fCompiler.context().fBool_Type.get()) {
+            if (v->fModifiers.fFlags & SkSL::Modifiers::kIn_Flag) {
+                bool v = *(((bool*)inputs) + offset);
+                inputMap.insert(std::make_pair(name, SkSL::Program::Settings::Value(v)));
+            }
+            offset += sizeof(bool) * arrayCount;
+        } else if (type == fCompiler.context().fFloat2_Type.get() ||
+                   type == fCompiler.context().fHalf2_Type.get()) {
+            offset = SkAlign4(offset) + sizeof(float) * 2 * arrayCount;
+        } else if (type == fCompiler.context().fFloat3_Type.get() ||
+                   type == fCompiler.context().fHalf3_Type.get()) {
+            offset = SkAlign4(offset) + sizeof(float) * 3 * arrayCount;
+        } else if (type == fCompiler.context().fFloat4_Type.get() ||
+                   type == fCompiler.context().fHalf4_Type.get()) {
+            offset = SkAlign4(offset) + sizeof(float) * 4 * arrayCount;
+        } else if (type == fCompiler.context().fFragmentProcessor_Type.get()) {
+            // do nothing
+        } else {
+            printf("can't handle input var: %s\n", SkSL::String(v->fType.fName).c_str());
+            SkASSERT(false);
+        }
+    }
+
+    std::unique_ptr<SkSL::Program> specialized = fCompiler.specialize(*fBaseProgram, inputMap);
+    bool optimized = fCompiler.optimize(*specialized);
+    if (!optimized) {
+        SkDebugf("%s\n", fCompiler.errorText().c_str());
+        SkASSERT(false);
+    }
+    return specialized;
+}
+
+std::tuple<std::unique_ptr<SkSL::ByteCode>, int, SkString> SkRuntimeEffect::toByteCode() {
+    auto byteCode = fCompiler.toByteCode(*fBaseProgram);
+    return std::make_tuple(std::move(byteCode), fCompiler.errorCount(),
+                           SkString(fCompiler.errorText().c_str()));
+}
