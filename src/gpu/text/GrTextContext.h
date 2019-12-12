2e92461b515af04feccb6f4193a4e0a3f8294085
Herb Derby <herb@google.com>
2019-12-12 16:37:03 -0500

    switch to SkPoint for the idea of drawingOrigin
    Start making distinctions between three different uses
    of matrices and origins.
    
    * draw{M|O} - the information provided by the draw command.
    * initial{M|O} - the drawing information captured when the
                     GrTextBlob was created.
    * current{M|O} - the matrix and origin used to create the
                     data that is in the SubRun's vertices.
    
    Where {M|O} == Matrix or Origin.
    
    Rearrange computeTranslation and calculate_translation to
    return vectors instead of taking out parameters.
    Remove init() call.
    
    Change-Id: I8644d95a560d7065d48424cafa51eaebe303d8b1

--- src/gpu/text/GrTextContext.h
+++ src/gpu/text/GrTextContext.h
@@ -46,7 +46,7 @@ public:
     static std::unique_ptr<GrTextContext> Make(const Options& options);
 
     void drawGlyphRunList(GrRecordingContext*, GrTextTarget*, const GrClip&,
-                          const SkMatrix& drawMatrix, const SkSurfaceProps&, const SkGlyphRunList&);
+                          const SkMatrix& drawingMatrix, const SkSurfaceProps&, const SkGlyphRunList&);
 
     std::unique_ptr<GrDrawOp> createOp_TestingOnly(GrRecordingContext*,
                                                    GrTextContext*,
