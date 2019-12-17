f11fd1f6e72d22d63cb0a9932058cb5b6801a252
Florin Malita <fmalita@chromium.org>
2019-12-17 11:32:21 -0500

    Python3 tweaks
    Change-Id: Ie951106bbd7a91dd76b30a56ede9313c88e657ab
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/260499
    Reviewed-by: Mike Klein <mtklein@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- gn/checkpath.py
+++ gn/checkpath.py
@@ -3,7 +3,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import print_function
+
 import os
 import sys
 
-print all(os.path.exists(a) for a in sys.argv[1:])
+print(all(os.path.exists(a) for a in sys.argv[1:]))
