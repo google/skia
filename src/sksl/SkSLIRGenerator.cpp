3794db84e141861052fd5a58e518c70e9a7350a4
Brian Salomon <bsalomon@google.com>
2019-12-17 14:24:36 -0500

    Blend functions as SkSL built ins.
    GPU-only for right now.
    
    Use in GrGLSLBlend::AppendMode.
    
    Remove unused GrGLSLBlend::AppendRegionOp.
    
    Change-Id: I23affe255a1adafa55f3a07ca6291dc84e18e213
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/254678
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/sksl/SkSLIRGenerator.cpp
+++ src/sksl/SkSLIRGenerator.cpp
@@ -132,6 +132,7 @@ static void fill_caps(const SKSL_CAPS_CLASS& caps,
     CAP(mustEnableSpecificAdvBlendEqs);
     CAP(mustDeclareFragmentShaderOutput);
     CAP(mustDoOpBetweenFloorAndAbs);
+    CAP(mustGuardDivisionEvenAfterExplicitZeroCheck);
     CAP(atan2ImplementedAsAtanYOverX);
     CAP(canUseAnyFunctionInShader);
     CAP(floatIs32Bits);
