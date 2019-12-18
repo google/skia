8066d6d1b16fbc515a39bc4de4d7fefb0a42a232
Hal Canary <halcanary@google.com>
2019-12-18 16:26:19 -0500

    skottie_ios_app: Add OpenGL.
      - Consolodate Skottie logic into SkottieViewController class that no longer
        knows about which backend we use.
    
      - Abstract out SkiaViewController interdace which SkottieViewController
        implements.
    
      - Create three classes SkiaGLView, SkiaUIView, and SkiaMtkView, which all
        accept SkiaViewController objects but override GLKView, UIView, and
        MTKView.
    
      - SkAnimationDraw and SkTimeKeeper now SkiaViewController
        implementation details, no longer shared in headers.
    
    Cq-Include-Trybots: skia/skia.primary:Build-Mac-Clang-arm64-Debug-iOS_Metal
    Cq-Include-Trybots: skia/skia.primary:Build-Mac-Clang-arm64-Debug-iOS
    Change-Id: I96ff2911d63da7d5327c81f91996b2a1b12c4419

---
+++ tools/skottie_ios_app/SkiaUIView.h
@@ -0,0 +1,18 @@
+// Copyright 2019 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#ifndef SkiaUIView_DEFINED
+#define SkiaUIView_DEFINED
+
+#import <UIKit/UIKit.h>
+
+#include "tools/skottie_ios_app/SkiaViewController.h"
+
+// A UIView that uses a CPU-backed SkSurface to draw.
+@interface SkiaUIView : UIView
+@property (strong) SkiaViewController* controller;
+
+// Override of the UIView interface.
+- (void)drawRect:(CGRect)rect;
+@end
+
+#endif  // SkiaUIView_DEFINED
