0ea860dae4a353b18db5530f50c391d4a3b38887
Hal Canary <halcanary@google.com>
2019-12-18 16:26:19 -0500

    skottie_ios_app: Add OpenGL.
    Commit: false
    Change-Id: I96ff2911d63da7d5327c81f91996b2a1b12c4419

---
+++ tools/skottie_ios_app/SkiaViewController.h
@@ -0,0 +1,18 @@
+// Copyright 2019 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#ifndef SkiaViewController_DEFINED
+#define SkiaViewController_DEFINED
+
+class SkCanvas;
+
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
+
+// An interface that draws to a Skia canvas.
+@interface SkiaViewController : NSObject
+- (void)draw:(CGRect)rect toCanvas:(SkCanvas*)canvas atSize:(CGSize)size;
+
+// Return the current paused state.
+- (bool)isPaused;
+@end
+#endif  // SkiaViewController_DEFINED
