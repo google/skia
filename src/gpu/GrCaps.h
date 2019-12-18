1299ba4cec2bf33bb14b73cac0680cef81a14ce0
Chris Dalton <csmartdalton@google.com>
2019-12-17 21:41:04 -0700

    Add a kWireframe flag to GrPipeline
    Adds the flag to GrPipeline, as well as a 'w' keystroke in viewer for
    toggling the alternate global wireframe mode.
    
    Change-Id: I385c31320a5fa2cec79b527a91fa876b19cdff89

--- src/gpu/GrCaps.h
+++ src/gpu/GrCaps.h
@@ -54,6 +54,7 @@ public:
     bool instanceAttribSupport() const { return fInstanceAttribSupport; }
     bool mixedSamplesSupport() const { return fMixedSamplesSupport; }
     bool conservativeRasterSupport() const { return fConservativeRasterSupport; }
+    bool wireframeSupport() const { return fWireframeSupport; }
     // This flag indicates that we never have to resolve MSAA. In practice, it means that we have
     // an MSAA-render-to-texture extension: Any render target we create internally will use the
     // extension, and any wrapped render target is the client's responsibility.
@@ -490,6 +491,7 @@ protected:
     bool fInstanceAttribSupport                      : 1;
     bool fMixedSamplesSupport                        : 1;
     bool fConservativeRasterSupport                  : 1;
+    bool fWireframeSupport                           : 1;
     bool fMSAAResolvesAutomatically                  : 1;
     bool fUsePrimitiveRestart                        : 1;
     bool fPreferClientSideDynamicBuffers             : 1;
