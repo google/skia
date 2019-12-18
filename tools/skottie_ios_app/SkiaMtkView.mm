6c0ff1ccf6c1e64f0468cc0584019e1c48d99f0c
Hal Canary <halcanary@google.com>
2019-12-18 16:26:19 -0500

    skottie_ios_app: Add OpenGL.
      - Consolodate Skottie logic into SkottieViewController class that no longer
        knows about which backend we use.
    
      - Abstract out SkiaViewController interdace which SkottieViewController
        implements.
    
      - Create three classes SkiaGLView, SkiaUIView, and SkiaMtkView, which all
        accept SkiaViewController objects but override GLKView, UIView, and
        MTKView.
    
      - SkAnimationDraw and SkTimeKeeper now SkiaViewController
        implementation details, no longer shared in headers.
    
    Cq-Include-Trybots: skia/skia.primary:Build-Mac-Clang-arm64-Debug-iOS_Metal
    Cq-Include-Trybots: skia/skia.primary:Build-Mac-Clang-arm64-Debug-iOS
    Change-Id: I96ff2911d63da7d5327c81f91996b2a1b12c4419

---
+++ tools/skottie_ios_app/SkiaMtkView.mm
@@ -0,0 +1,41 @@
+// Copyright 2019 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include "tools/skottie_ios_app/SkiaMtkView.h"
+
+#include "include/core/SkSurface.h"
+#include "tools/skottie_ios_app/SkMetalViewBridge.h"
+
+@implementation SkiaMtkView
+
+- (id)initWithFrame:(CGRect)frame {
+    [super initWithFrame:frame];
+    SkMtkViewConfigForSkia(self);
+}
+
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    // TODO(halcanary): Use the rect and the InvalidationController to speed up rendering.
+    SkiaViewController* viewController = [self controller];
+    if (!viewController || ![[self currentDrawable] texture] || ![self grContext]) {
+        return;
+    }
+    CGSize size = [self drawableSize];
+    sk_sp<SkSurface> surface = SkMtkViewToSurface(self, [self grContext]);
+    if (!surface) {
+        NSLog(@"error: no sksurface");
+        return;
+    }
+    [viewController draw:rect toCanvas:surface->getCanvas() atSize:size];
+    surface->flush();
+    surface = nullptr;
+
+    id<MTLCommandBuffer> commandBuffer = [[self queue] commandBuffer];
+    [commandBuffer presentDrawable:[self currentDrawable]];
+    [commandBuffer commit];
+
+    bool paused = [viewController isPaused];
+    [self setEnableSetNeedsDisplay:paused];
+    [self setPaused:paused];
+}
+@end
