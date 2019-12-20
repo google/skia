9062b828005bc9783b643fab8e0f3b7e2da28d65
Ben Wagner <benjaminwagner@google.com>
2019-12-20 16:05:06 -0500

    Add xSAN jobs for Vulkan
    Change-Id: Ib77bb0e14e0957ca48817f09246042a3c38519a4

--- infra/bots/recipe_modules/flavor/default.py
+++ infra/bots/recipe_modules/flavor/default.py
@@ -192,9 +192,9 @@ class DefaultFlavor(object):
       if 'Mac' in self.m.vars.builder_cfg.get('os', ''):
         env['ASAN_OPTIONS'] = 'symbolize=1'  # Mac doesn't support detect_leaks.
       else:
-        env['ASAN_OPTIONS'] = 'symbolize=1 detect_leaks=1'
-      env[ 'LSAN_OPTIONS'] = 'symbolize=1 print_suppressions=1'
-      env['UBSAN_OPTIONS'] = 'symbolize=1 print_stacktrace=1'
+        env['ASAN_OPTIONS'] = 'symbolize=1 detect_leaks=1 fast_unwind_on_malloc=0'
+      env[ 'LSAN_OPTIONS'] = 'symbolize=1 print_suppressions=1 fast_unwind_on_malloc=0'
+      env['UBSAN_OPTIONS'] = 'symbolize=1 print_stacktrace=1 fast_unwind_on_malloc=0'
 
     if 'TSAN' in extra_tokens:
       # We don't care about malloc(), fprintf, etc. used in signal handlers.
