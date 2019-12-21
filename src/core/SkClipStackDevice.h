6ec826085b2a818d29dba56880056ae392561366
Mike Reed <reed@google.com>
2019-12-21 13:43:03 +0000

    Reland "make clipIsWideOpen virtual, so device's don't have to rely on regions"
    This reverts commit ebf5fd11de100c22d73e291013847fabd6eb4aae.
    
    Reason for revert: didn't help flutter roll -- relanding
    
    Original change's description:
    > Revert "make clipIsWideOpen virtual, so device's don't have to rely on regions"
    > 
    > This reverts commit af203a677aacb4291eb77f8cc76d223be2e89bce.
    > 
    > Reason for revert: breaking the flutter roll?
    > 
    > Original change's description:
    > > make clipIsWideOpen virtual, so device's don't have to rely on regions
    > > 
    > > Change-Id: I362e641f3cf6ae31fac8ffc5980148f43874f6dd
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/261285
    > > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > > Commit-Queue: Mike Reed <reed@google.com>
    > 
    > TBR=bsalomon@google.com,fmalita@chromium.org,reed@google.com,michaelludwig@google.com
    > 
    > Change-Id: I13f4dd4419a3b30961c61834faf6198ff7dc3619
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/261378
    > Reviewed-by: Mike Reed <reed@google.com>
    > Commit-Queue: Mike Reed <reed@google.com>
    
    TBR=bsalomon@google.com,fmalita@chromium.org,reed@google.com,michaelludwig@google.com
    
    Change-Id: I5dd4c079fa131c58f565246c66b0edfbb7f6316a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/261379
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- src/core/SkClipStackDevice.h
+++ src/core/SkClipStackDevice.h
@@ -32,6 +32,7 @@ protected:
     void onClipRegion(const SkRegion& deviceRgn, SkClipOp) override;
     void onSetDeviceClipRestriction(SkIRect* mutableClipRestriction) override;
     bool onClipIsAA() const override;
+    bool onClipIsWideOpen() const override;
     void onAsRgnClip(SkRegion*) const override;
     ClipType onGetClipType() const override;
 
