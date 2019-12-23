8231db329f3a6d2315e694d3a5b97aa2f214294d
Hal Canary <halcanary@google.com>
2019-12-23 13:04:41 -0500

    iOS: send SkDebugf to NSLog and stderr
    Change-Id: Ia6ac62386a285acdd1ecb820c8c9f9ba8b7452ce

--- src/ports/SkDebug_stdio.cpp
+++ src/ports/SkDebug_stdio.cpp
@@ -6,7 +6,26 @@
  */
 
 #include "include/core/SkTypes.h"
-#if !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
+#if defined(SK_BUILD_FOR_IOS)
+
+#include <stdarg.h>
+#include <CoreFoundation/CFString.h>
+
+struct NSString;
+extern "C" void NSLogv(NSString *format, va_list args);
+
+void SkDebugf(const char* format, ...) {
+    va_list args, argsCopy;
+    va_start(args, format);
+    va_copy(argsCopy, args);
+    vfprintf(stderr, format, argsCopy);
+    va_end(argsCopy);
+    CFStringRef fmt = CFStringCreateWithCString(nullptr, format, kCFStringEncodingUTF8);
+    NSLogv((NSString*)fmt, args);
+    CFRelease(fmt);
+    va_end(args);
+}
+#elif !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
 
 #include <stdarg.h>
 #include <stdio.h>
