661b91037c8a406a4b7bc86eed4a618f7b9a0dbd
Hal Canary <halcanary@google.com>
2019-12-23 13:04:41 -0500

    iOS: optionally redirect SkDebugf to NSLog
    Add "-DSK_IOS_DEBUGF_TO_NSLOG" to extra_cflags to enable.
    
    Change-Id: Ia6ac62386a285acdd1ecb820c8c9f9ba8b7452ce

--- src/ports/SkDebug_stdio.cpp
+++ src/ports/SkDebug_stdio.cpp
@@ -6,7 +6,23 @@
  */
 
 #include "include/core/SkTypes.h"
-#if !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
+#if defined(SK_BUILD_FOR_IOS) && defined(SK_IOS_DEBUGF_TO_NSLOG)
+
+#include <stdarg.h>
+#include <CoreFoundation/CFString.h>
+
+struct NSString;
+extern "C" void NSLogv(NSString *format, va_list args);
+
+void SkDebugf(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    CFStringRef fmt = CFStringCreateWithCString(nullptr, format, kCFStringEncodingUTF8);
+    NSLogv((NSString*)fmt, args);
+    CFRelease(fmt);
+    va_end(args);
+}
+#elif !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
 
 #include <stdarg.h>
 #include <stdio.h>
