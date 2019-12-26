fd7cea772cf95d9b55c3b81223c93ad1193a872f
Matthew Leibowitz <mattleibow@live.com>
2019-12-26 10:53:52 +0200

    Add more SkFont APIs

--- include/c/sk_paint.h
+++ include/c/sk_paint.h
@@ -50,32 +50,35 @@ SK_C_API sk_path_effect_t* sk_paint_get_path_effect(sk_paint_t* cpaint);
 SK_C_API void sk_paint_set_path_effect(sk_paint_t* cpaint, sk_path_effect_t* effect);  
 SK_C_API bool sk_paint_get_fill_path(const sk_paint_t*, const sk_path_t* src, sk_path_t* dst, const sk_rect_t* cullRect, float resScale);
 
-// SK_C_API sk_font_t* sk_font_new();
-// SK_C_API sk_font_t* sk_font_with_size(const sk_font_t* font, float size);
-// SK_C_API bool sk_font_is_force_auto_hinting(const sk_font_t* font);
-// SK_C_API void sk_font_set_force_auto_hinting(sk_font_t* font, bool value);
-// SK_C_API bool sk_font_is_embedded_bitmaps(const sk_font_t* font);
-// SK_C_API void sk_font_set_embedded_bitmaps(sk_font_t* font, bool value);
-// SK_C_API bool sk_font_is_subpixel(const sk_font_t* font);
-// SK_C_API void sk_font_set_subpixel(sk_font_t* font, bool value);
-// SK_C_API bool sk_font_is_linear_metrics(const sk_font_t* font);
-// SK_C_API void sk_font_set_linear_metrics(sk_font_t* font, bool value);
-// SK_C_API bool sk_font_is_embolden(const sk_font_t* font);
-// SK_C_API void sk_font_set_embolden(sk_font_t* font, bool value);
-// SK_C_API bool sk_font_is_baseline_snap(const sk_font_t* font);
-// SK_C_API void sk_font_set_baseline_snap(sk_font_t* font, bool value);
-// SK_C_API sk_font_edging_t sk_font_get_edging(const sk_font_t* font);
-// SK_C_API void sk_font_set_edging(sk_font_t* font, sk_font_edging_t value);
-// SK_C_API sk_font_hinting_t sk_font_get_hinting(const sk_font_t* font);
-// SK_C_API void sk_font_set_hinting(sk_font_t* font, sk_font_hinting_t value);
-// SK_C_API sk_typeface_t* sk_font_get_typeface(const sk_font_t* font);
-// SK_C_API void sk_font_set_typeface(sk_font_t* font, sk_typeface_t* value);
-// SK_C_API float sk_font_get_size(const sk_font_t* font);
-// SK_C_API void sk_font_set_size(sk_font_t* font, float value);
-// SK_C_API float sk_font_get_scale_x(const sk_font_t* font);
-// SK_C_API void sk_font_set_scale_x(sk_font_t* font, float value);
-// SK_C_API float sk_font_get_skew_x(const sk_font_t* font);
-// SK_C_API void sk_font_set_skew_x(sk_font_t* font, float value);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+SK_C_API sk_font_t* sk_font_new();
+SK_C_API sk_font_t* sk_font_new(sk_typeface_t* typeface, float size, float scaleX, float skewX);
+SK_C_API void sk_font_delete(sk_font_t* font);
+SK_C_API bool sk_font_is_force_auto_hinting(const sk_font_t* font);
+SK_C_API void sk_font_set_force_auto_hinting(sk_font_t* font, bool value);
+SK_C_API bool sk_font_is_embedded_bitmaps(const sk_font_t* font);
+SK_C_API void sk_font_set_embedded_bitmaps(sk_font_t* font, bool value);
+SK_C_API bool sk_font_is_subpixel(const sk_font_t* font);
+SK_C_API void sk_font_set_subpixel(sk_font_t* font, bool value);
+SK_C_API bool sk_font_is_linear_metrics(const sk_font_t* font);
+SK_C_API void sk_font_set_linear_metrics(sk_font_t* font, bool value);
+SK_C_API bool sk_font_is_embolden(const sk_font_t* font);
+SK_C_API void sk_font_set_embolden(sk_font_t* font, bool value);
+SK_C_API bool sk_font_is_baseline_snap(const sk_font_t* font);
+SK_C_API void sk_font_set_baseline_snap(sk_font_t* font, bool value);
+SK_C_API sk_font_edging_t sk_font_get_edging(const sk_font_t* font);
+SK_C_API void sk_font_set_edging(sk_font_t* font, sk_font_edging_t value);
+SK_C_API sk_font_hinting_t sk_font_get_hinting(const sk_font_t* font);
+SK_C_API void sk_font_set_hinting(sk_font_t* font, sk_font_hinting_t value);
+SK_C_API sk_typeface_t* sk_font_get_typeface(const sk_font_t* font);
+SK_C_API void sk_font_set_typeface(sk_font_t* font, sk_typeface_t* value);
+SK_C_API float sk_font_get_size(const sk_font_t* font);
+SK_C_API void sk_font_set_size(sk_font_t* font, float value);
+SK_C_API float sk_font_get_scale_x(const sk_font_t* font);
+SK_C_API void sk_font_set_scale_x(sk_font_t* font, float value);
+SK_C_API float sk_font_get_skew_x(const sk_font_t* font);
+SK_C_API void sk_font_set_skew_x(sk_font_t* font, float value);
 
 SK_C_PLUS_PLUS_END_GUARD
 
