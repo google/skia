fe5a823b4abbb3862e08a243ca2e397d7fe82c92
Brian Osman <brianosman@google.com>
2019-12-26 08:43:05 -0500

    Add SkRuntimeEffect::makeShader
    This makes the effect *be* the factory, so we can get rid of
    SkRuntimeShaderFactory (and later, SkColorFilterFactory).
    
    Change-Id: I2cd95f1380a0ec1e65fe5a18b1226c042598a8bd

--- gm/runtimeshader.cpp
+++ gm/runtimeshader.cpp
@@ -7,20 +7,11 @@
 
 #include "gm/gm.h"
 #include "include/core/SkCanvas.h"
-#include "include/core/SkColorFilter.h"
 #include "include/core/SkData.h"
-#include "include/core/SkImage.h"
 #include "include/core/SkPaint.h"
-#include "include/core/SkRefCnt.h"
 #include "include/core/SkSize.h"
 #include "include/core/SkString.h"
-#include "src/core/SkColorFilterPriv.h"
-#include "src/core/SkReadBuffer.h"
-#include "src/core/SkWriteBuffer.h"
-#include "src/shaders/SkRTShader.h"
-#include "tools/Resources.h"
-
-#include <stddef.h>
+#include "src/core/SkRuntimeEffect.h"
 
 const char* gProg = R"(
     uniform half4 gColor;
@@ -30,33 +21,26 @@ const char* gProg = R"(
     }
 )";
 
-static sk_sp<SkShader> gShader;
-
 class RuntimeShader : public skiagm::GM {
-    sk_sp<SkData> fData;
-
     bool runAsBench() const override { return true; }
 
     SkString onShortName() override { return SkString("runtime_shader"); }
 
     SkISize onISize() override { return {512, 256}; }
 
-    void onOnceBeforeDraw() override {
-        // use global to pass gl persistent cache test in dm
-        if (!gShader) {
-            SkMatrix localM;
-            localM.setRotate(90, 128, 128);
-
-            fData = SkData::MakeUninitialized(sizeof(SkColor4f));
-            SkColor4f* c = (SkColor4f*)fData->writable_data();
-            *c = {1, 0, 0, 1};
-            gShader = SkRuntimeShaderFactory(SkString(gProg), true).make(fData, &localM);
-        }
-    }
-
     void onDraw(SkCanvas* canvas) override {
+        // static to pass gl persistent cache test in dm
+        static sk_sp<SkRuntimeEffect> gEffect = std::get<0>(SkRuntimeEffect::Make(SkString(gProg)));
+        SkASSERT(gEffect);
+
+        SkMatrix localM;
+        localM.setRotate(90, 128, 128);
+
+        SkColor4f inputColor = { 1, 0, 0, 1 };
+        auto shader = gEffect->makeShader(SkData::MakeWithCopy(&inputColor, sizeof(inputColor)),
+                                          nullptr, 0, &localM, true);
         SkPaint p;
-        p.setShader(gShader);
+        p.setShader(std::move(shader));
         canvas->drawRect({0, 0, 256, 256}, p);
     }
 };
