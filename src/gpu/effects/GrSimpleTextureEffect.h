460b6c1dd8067769bf0613e41053e7097d8ba383
Florin Malita <fmalita@google.com>
2019-12-29 19:04:14 +0000

    Revert "Clean up GrSimpleTextureEffect."
    This reverts commit 4d745d62e32de358fabf9ec70eb455a1a15b5bd3.
    
    Reason for revert: broke Chromium tests
    
    Original change's description:
    > Clean up GrSimpleTextureEffect.
    > 
    > Make it look human written instead of machine generated.
    > 
    > Use perspective texturing builtin sample() when coords are three component
    > rather than dividing to make them 2d.
    > 
    > Change-Id: I9694857beaf35733077d0472d1eae43b5da933d0
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/261677
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    
    TBR=bsalomon@google.com,brianosman@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I8d7a5c5553434528785aa3dd2e5538f893c1cc40
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/261679
    Reviewed-by: Florin Malita <fmalita@google.com>
    Commit-Queue: Florin Malita <fmalita@google.com>

--- src/gpu/effects/GrSimpleTextureEffect.h
+++ src/gpu/effects/GrSimpleTextureEffect.h
@@ -7,50 +7,66 @@
 
 #ifndef GrSimpleTextureEffect_DEFINED
 #define GrSimpleTextureEffect_DEFINED
+#include "include/core/SkTypes.h"
 
-#include "include/core/SkImageInfo.h"
-#include "include/core/SkMatrix.h"
 #include "src/gpu/GrCoordTransform.h"
 #include "src/gpu/GrFragmentProcessor.h"
-
 class GrSimpleTextureEffect : public GrFragmentProcessor {
 public:
-    /** Uses kClamp wrap mode in both dimensions. */
-    static std::unique_ptr<GrFragmentProcessor> Make(
-            sk_sp<GrSurfaceProxy>,
-            SkAlphaType,
-            const SkMatrix&,
-            GrSamplerState::Filter = GrSamplerState::Filter::kNearest);
+    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrSurfaceProxy> proxy,
+                                                     SkAlphaType alphaType,
+                                                     const SkMatrix& matrix) {
+        return std::unique_ptr<GrFragmentProcessor>(
+                new GrSimpleTextureEffect(std::move(proxy), matrix, alphaType,
+                                          GrSamplerState(GrSamplerState::WrapMode::kClamp,
+                                                         GrSamplerState::Filter::kNearest)));
+    }
 
-    /** Allows full specification of the sampling parameters. */
-    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrSurfaceProxy>,
-                                                     SkAlphaType,
-                                                     const SkMatrix&,
-                                                     GrSamplerState);
+    /* clamp mode */
+    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrSurfaceProxy> proxy,
+                                                     SkAlphaType alphaType,
+                                                     const SkMatrix& matrix,
+                                                     GrSamplerState::Filter filter) {
+        return std::unique_ptr<GrFragmentProcessor>(new GrSimpleTextureEffect(
+                std::move(proxy), matrix, alphaType,
+                GrSamplerState(GrSamplerState::WrapMode::kClamp, filter)));
+    }
 
+    static std::unique_ptr<GrFragmentProcessor> Make(sk_sp<GrSurfaceProxy> proxy,
+                                                     SkAlphaType alphaType,
+                                                     const SkMatrix& matrix,
+                                                     const GrSamplerState& p) {
+        return std::unique_ptr<GrFragmentProcessor>(
+                new GrSimpleTextureEffect(std::move(proxy), matrix, alphaType, p));
+    }
+    GrSimpleTextureEffect(const GrSimpleTextureEffect& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
-
     const char* name() const override { return "SimpleTextureEffect"; }
+    GrCoordTransform imageCoordTransform;
+    TextureSampler image;
+    SkMatrix44 matrix;
 
 private:
-    GrCoordTransform fCoordTransform;
-    TextureSampler fSampler;
-
-    GrSimpleTextureEffect(const GrSimpleTextureEffect& src);
-
-    inline GrSimpleTextureEffect(sk_sp<GrSurfaceProxy>, SkAlphaType, const SkMatrix&,
-                                 GrSamplerState);
-
+    GrSimpleTextureEffect(sk_sp<GrSurfaceProxy> image, SkMatrix44 matrix, SkAlphaType alphaType,
+                          GrSamplerState samplerParams)
+            : INHERITED(kGrSimpleTextureEffect_ClassID,
+                        (OptimizationFlags)ModulateForSamplerOptFlags(
+                                alphaType,
+                                samplerParams.wrapModeX() ==
+                                                GrSamplerState::WrapMode::kClampToBorder ||
+                                        samplerParams.wrapModeY() ==
+                                                GrSamplerState::WrapMode::kClampToBorder))
+            , imageCoordTransform(matrix, image.get())
+            , image(std::move(image), samplerParams)
+            , matrix(matrix) {
+        this->setTextureSamplerCnt(1);
+        this->addCoordTransform(&imageCoordTransform);
+    }
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
-
     void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
-
     bool onIsEqual(const GrFragmentProcessor&) const override;
-
     const TextureSampler& onTextureSampler(int) const override;
-
     GR_DECLARE_FRAGMENT_PROCESSOR_TEST
-
     typedef GrFragmentProcessor INHERITED;
 };
 #endif
