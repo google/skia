9df7afc2cba2e4329ae0b90824d0c7c1c92574d8
Mike Reed <reed@google.com>
2020-01-03 15:07:03 -0500

    WIP: experimental SkM4 matrix
    If we use this in SkCanvas, I expect I may compose this in a
    CanvasMatrix (new type), so I don't have to dirty this API with bridge
    methods that look like SkMatrix...
    
    
    Change-Id: I68eac942919fa5418081e789f31710a1e2a752da

---
+++ src/core/SkM4.cpp
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/private/SkM4.h"
+#include "include/private/SkVx.h"
+
+typedef skvx::Vec<4, float> sk4f;
+
+bool SkM4::operator==(const SkM4& other) const {
+    if (this == &other) {
+        return true;
+    }
+
+    sk4f a0 = sk4f::Load(fMat +  0);
+    sk4f a1 = sk4f::Load(fMat +  4);
+    sk4f a2 = sk4f::Load(fMat +  8);
+    sk4f a3 = sk4f::Load(fMat + 12);
+
+    sk4f b0 = sk4f::Load(other.fMat +  0);
+    sk4f b1 = sk4f::Load(other.fMat +  4);
+    sk4f b2 = sk4f::Load(other.fMat +  8);
+    sk4f b3 = sk4f::Load(other.fMat + 12);
+
+    auto eq = (a0 == b0) & (a1 == b1) & (a2 == b2) & (a3 == b3);
+    return (eq[0] & eq[1] & eq[2] & eq[3]) == ~0;
+}
+
+SkM4& SkM4::setConcat(const SkM4& a, const SkM4& b) {
+    sk4f c0 = sk4f::Load(a.fMat +  0);
+    sk4f c1 = sk4f::Load(a.fMat +  4);
+    sk4f c2 = sk4f::Load(a.fMat +  8);
+    sk4f c3 = sk4f::Load(a.fMat + 12);
+
+    auto compute = [&](sk4f r) {
+        return skvx::mad(c0, r[0], skvx::mad(c1, r[1], skvx::mad(c2, r[2], c3 * r[3])));
+    };
+
+    sk4f m0 = compute(sk4f::Load(b.fMat +  0));
+    sk4f m1 = compute(sk4f::Load(b.fMat +  4));
+    sk4f m2 = compute(sk4f::Load(b.fMat +  8));
+    sk4f m3 = compute(sk4f::Load(b.fMat + 12));
+
+    m0.store(fMat +  0);
+    m1.store(fMat +  4);
+    m2.store(fMat +  8);
+    m3.store(fMat + 12);
+    return *this;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+/** We always perform the calculation in doubles, to avoid prematurely losing
+    precision along the way. This relies on the compiler automatically
+    promoting our SkMScalar values to double (if needed).
+ */
+double SkM4::determinant() const {
+    double a00 = fMat[0];
+    double a01 = fMat[1];
+    double a02 = fMat[2];
+    double a03 = fMat[3];
+    double a10 = fMat[4];
+    double a11 = fMat[5];
+    double a12 = fMat[6];
+    double a13 = fMat[7];
+    double a20 = fMat[8];
+    double a21 = fMat[9];
+    double a22 = fMat[10];
+    double a23 = fMat[11];
+    double a30 = fMat[12];
+    double a31 = fMat[13];
+    double a32 = fMat[14];
+    double a33 = fMat[15];
+
+    double b00 = a00 * a11 - a01 * a10;
+    double b01 = a00 * a12 - a02 * a10;
+    double b02 = a00 * a13 - a03 * a10;
+    double b03 = a01 * a12 - a02 * a11;
+    double b04 = a01 * a13 - a03 * a11;
+    double b05 = a02 * a13 - a03 * a12;
+    double b06 = a20 * a31 - a21 * a30;
+    double b07 = a20 * a32 - a22 * a30;
+    double b08 = a20 * a33 - a23 * a30;
+    double b09 = a21 * a32 - a22 * a31;
+    double b10 = a21 * a33 - a23 * a31;
+    double b11 = a22 * a33 - a23 * a32;
+
+    // Calculate the determinant
+    return b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool SkM4::invert(SkM4* inverse) const {
+    double a00 = fMat[0];
+    double a01 = fMat[1];
+    double a02 = fMat[2];
+    double a03 = fMat[3];
+    double a10 = fMat[4];
+    double a11 = fMat[5];
+    double a12 = fMat[6];
+    double a13 = fMat[7];
+    double a20 = fMat[8];
+    double a21 = fMat[9];
+    double a22 = fMat[10];
+    double a23 = fMat[11];
+    double a30 = fMat[12];
+    double a31 = fMat[13];
+    double a32 = fMat[14];
+    double a33 = fMat[15];
+
+    double b00 = a00 * a11 - a01 * a10;
+    double b01 = a00 * a12 - a02 * a10;
+    double b02 = a00 * a13 - a03 * a10;
+    double b03 = a01 * a12 - a02 * a11;
+    double b04 = a01 * a13 - a03 * a11;
+    double b05 = a02 * a13 - a03 * a12;
+    double b06 = a20 * a31 - a21 * a30;
+    double b07 = a20 * a32 - a22 * a30;
+    double b08 = a20 * a33 - a23 * a30;
+    double b09 = a21 * a32 - a22 * a31;
+    double b10 = a21 * a33 - a23 * a31;
+    double b11 = a22 * a33 - a23 * a32;
+
+    // Calculate the determinant
+    double det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+    double invdet = sk_ieee_double_divide(1.0, det);
+    // If det is zero, we want to return false. However, we also want to return false
+    // if 1/det overflows to infinity (i.e. det is denormalized). Both of these are
+    // handled by checking that 1/det is finite.
+    if (!SkScalarIsFinite(SkScalar(invdet))) {
+        return false;
+    }
+
+    b00 *= invdet;
+    b01 *= invdet;
+    b02 *= invdet;
+    b03 *= invdet;
+    b04 *= invdet;
+    b05 *= invdet;
+    b06 *= invdet;
+    b07 *= invdet;
+    b08 *= invdet;
+    b09 *= invdet;
+    b10 *= invdet;
+    b11 *= invdet;
+
+    SkScalar tmp[16] = {
+        SkScalar(a11 * b11 - a12 * b10 + a13 * b09),
+        SkScalar(a02 * b10 - a01 * b11 - a03 * b09),
+        SkScalar(a31 * b05 - a32 * b04 + a33 * b03),
+        SkScalar(a22 * b04 - a21 * b05 - a23 * b03),
+        SkScalar(a12 * b08 - a10 * b11 - a13 * b07),
+        SkScalar(a00 * b11 - a02 * b08 + a03 * b07),
+        SkScalar(a32 * b02 - a30 * b05 - a33 * b01),
+        SkScalar(a20 * b05 - a22 * b02 + a23 * b01),
+        SkScalar(a10 * b10 - a11 * b08 + a13 * b06),
+        SkScalar(a01 * b08 - a00 * b10 - a03 * b06),
+        SkScalar(a30 * b04 - a31 * b02 + a33 * b00),
+        SkScalar(a21 * b02 - a20 * b04 - a23 * b00),
+        SkScalar(a11 * b07 - a10 * b09 - a12 * b06),
+        SkScalar(a00 * b09 - a01 * b07 + a02 * b06),
+        SkScalar(a31 * b01 - a30 * b03 - a32 * b00),
+        SkScalar(a20 * b03 - a21 * b01 + a22 * b00),
+    };
+    if (!SkScalarsAreFinite(tmp, 16)) {
+        return false;
+    }
+    memcpy(inverse->fMat, tmp, sizeof(tmp));
+    return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void SkM4::dump() const {
+    static const char* format = "|%g %g %g %g|\n"
+                                "|%g %g %g %g|\n"
+                                "|%g %g %g %g|\n"
+                                "|%g %g %g %g|\n";
+    SkDebugf(format,
+             fMat[0], fMat[4], fMat[8],  fMat[12],
+             fMat[1], fMat[5], fMat[9],  fMat[13],
+             fMat[2], fMat[6], fMat[10], fMat[14],
+             fMat[3], fMat[7], fMat[11], fMat[15]);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+SkM4::SkM4(const SkMatrix& src) {
+    this->operator=(src);
+}
+
+SkM4& SkM4::operator=(const SkMatrix& src) {
+    fMat[0]  = src[SkMatrix::kMScaleX];
+    fMat[1]  = src[SkMatrix::kMSkewX];
+    fMat[2]  = 0;
+    fMat[3]  = src[SkMatrix::kMTransX];
+    fMat[4]  = src[SkMatrix::kMSkewY];
+    fMat[5]  = src[SkMatrix::kMScaleY];
+    fMat[6]  = 0;
+    fMat[7]  = src[SkMatrix::kMTransY];
+    fMat[8]  = 0;
+    fMat[9]  = 0;
+    fMat[10] = 1;
+    fMat[11] = 0;
+    fMat[12] = src[SkMatrix::kMPersp0];
+    fMat[13] = src[SkMatrix::kMPersp1];
+    fMat[14] = 0;
+    fMat[15] = src[SkMatrix::kMPersp2];
+    return *this;
+}
+
+SkM4::operator SkMatrix() const {
+    SkMatrix dst;
+
+    dst[SkMatrix::kMScaleX] = fMat[0];
+    dst[SkMatrix::kMSkewX]  = fMat[4];
+    dst[SkMatrix::kMTransX] = fMat[12];
+
+    dst[SkMatrix::kMSkewY]  = fMat[4];
+    dst[SkMatrix::kMScaleY] = fMat[5];
+    dst[SkMatrix::kMTransY] = fMat[13];
+
+    dst[SkMatrix::kMPersp0] = fMat[3];
+    dst[SkMatrix::kMPersp1] = fMat[7];
+    dst[SkMatrix::kMPersp2] = fMat[15];
+
+    return dst;
+}
