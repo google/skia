467df0b5da2cf72a00d993edfc0e55adc78ddf3b
Mike Reed <reed@google.com>
2020-01-08 10:18:06 -0500

    Revert "Revert "Extend SkCanvas matrix stack to be 4x4, but with (basically) the same public API.""
    This reverts commit 268ed57d71e7aa7230d0c1a5722cf84a65f43e61.
    
    Change-Id: I3c48a0aa17649351a246c1fbab5449f2d59aaf84

---
+++ src/core/SkCanvasMatrix.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkCanvasMatrix_DEFINED
+#define SkCanvasMatrix_DEFINED
+
+#include "include/core/SkMatrix.h"
+#include "include/private/SkM44.h"
+
+class SkCanvasMatrix {
+#ifdef SK_SUPPORT_LEGACY_CANVAS_MATRIX_33
+    SkMatrix fM;
+#else
+    SkM44 fM;
+#endif
+public:
+    SkCanvasMatrix& operator=(const SkMatrix& other) { fM = other; return *this; }
+
+    void reset() { fM.setIdentity(); }
+    void preTranslate(SkScalar x, SkScalar y) { fM.preTranslate(x, y); }
+    void preConcat(const SkMatrix& m) { fM.preConcat(m); }
+
+#ifdef SK_SUPPORT_LEGACY_CANVAS_MATRIX_33
+    operator SkMatrix() const { return fM; }
+    bool isScaleTranslate() const { return fM.isScaleTranslate(); }
+    bool rectStaysRect() const { return fM.rectStaysRect(); }
+
+    float getScaleX() const { return fM.getScaleX(); }
+    float getScaleY() const { return fM.getScaleY(); }
+    float getTranslateX() const { return fM.getTranslateX(); }
+    float getTranslateY() const { return fM.getTranslateY(); }
+
+    bool invert(SkMatrix* inv) const { return fM.invert(inv); }
+
+    bool mapRect(SkRect* dst, const SkRect& src) { return fM.mapRect(dst, src); }
+#else
+    operator SkMatrix() const { return fM.asM33(); }
+    bool isScaleTranslate() const {
+        return fM.atColMajor(1) == 0 && fM.atColMajor(2) == 0 && fM.atColMajor(3) == 0 && fM.atColMajor(4) == 0 &&
+               fM.atColMajor(6) == 0 && fM.atColMajor(7) == 0 && fM.atColMajor(8) == 0 && fM.atColMajor(9) == 0 &&
+               fM.atColMajor(11) == 0 && fM.atColMajor(15) == 1;
+    }
+    bool rectStaysRect() const { return fM.asM33().rectStaysRect(); }
+
+    float getScaleX() const { return fM.atColMajor(0); }
+    float getScaleY() const { return fM.atColMajor(5); }
+    float getTranslateX() const { return fM.atColMajor(12); }
+    float getTranslateY() const { return fM.atColMajor(13); }
+
+    bool invert(SkMatrix* inv) const { return fM.asM33().invert(inv); }
+
+    bool mapRect(SkRect* dst, const SkRect& src) { return fM.asM33().mapRect(dst, src); }
+
+    void preConcat(const SkM44& m) { fM.setConcat(fM, m); }
+#endif
+};
+
+#endif
