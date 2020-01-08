403c807971892049532870707729f0bc3e79e93a
Mike Reed <reed@google.com>
2020-01-08 10:40:39 -0500

    Revert "Revert "Extend SkCanvas matrix stack to be 4x4, but with (basically) the same public API.""
    The reason for the assert was breaking an assert, that if the CTM was scale/translate, that after
    a preTranslate, it should still be that.
    
    This is true... unless the new translate values are non-finite. In that case, we might turn a zero
    into a NaN, (0 * non_finite --> nan), so we either have to require finite args (which we don't
    at the moment) or we can't make this assert. This re-land removes that assert.
    
    This reverts commit 268ed57d71e7aa7230d0c1a5722cf84a65f43e61.
    
    Change-Id: I3c48a0aa17649351a246c1fbab5449f2d59aaf84
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/263023
    Commit-Queue: Mike Reed <reed@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: Mike Reed <reed@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkMatrix.cpp
+++ src/core/SkMatrix.cpp
@@ -68,22 +68,6 @@ SkMatrix& SkMatrix::set9(const SkScalar buffer[]) {
     return *this;
 }
 
-SkMatrix& SkMatrix::setAll(SkScalar scaleX, SkScalar skewX,  SkScalar transX,
-                           SkScalar skewY,  SkScalar scaleY, SkScalar transY,
-                           SkScalar persp0, SkScalar persp1, SkScalar persp2) {
-    fMat[kMScaleX] = scaleX;
-    fMat[kMSkewX]  = skewX;
-    fMat[kMTransX] = transX;
-    fMat[kMSkewY]  = skewY;
-    fMat[kMScaleY] = scaleY;
-    fMat[kMTransY] = transY;
-    fMat[kMPersp0] = persp0;
-    fMat[kMPersp1] = persp1;
-    fMat[kMPersp2] = persp2;
-    this->setTypeMask(kUnknown_Mask);
-    return *this;
-}
-
 SkMatrix& SkMatrix::setAffine(const SkScalar buffer[]) {
     fMat[kMScaleX] = buffer[kAScaleX];
     fMat[kMSkewX]  = buffer[kASkewX];
