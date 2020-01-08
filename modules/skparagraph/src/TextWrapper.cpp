169931fa4dd337b4c64ab841e58aed4b453022a5
Julia Lavrova <jlavrova@google.com>
2020-01-08 16:53:53 -0500

    Ellipsis attached to cluster, not word
    Change-Id: I3bcef7e57a8f95a12743325363463a3a4039776e

--- modules/skparagraph/src/TextWrapper.cpp
+++ modules/skparagraph/src/TextWrapper.cpp
@@ -72,23 +72,25 @@ void TextWrapper::lookAhead(SkScalar maxWidth, Cluster* endOfClusters) {
     }
 }
 
-void TextWrapper::moveForward() {
-    do {
-        if (!fWords.empty()) {
-            fEndLine.extend(fWords);
-        } else if (!fClusters.empty()) {
-            fEndLine.extend(fClusters);
-            fTooLongWord = false;
-            fTooLongCluster = false;
-        } else if (!fClip.empty() || (fTooLongWord && fTooLongCluster)) {
-            // Flutter: forget the clipped cluster but keep the metrics
-            fEndLine.metrics().add(fClip.metrics());
-            fTooLongWord = false;
-            fTooLongCluster = false;
-        } else {
-            break;
+void TextWrapper::moveForward(bool hasEllipsis) {
+
+    if (!fWords.empty()) {
+        fEndLine.extend(fWords);
+        if (!fTooLongWord && !hasEllipsis) {
+            return;
         }
-    } while (fTooLongWord || fTooLongCluster);
+    }
+    if (!fClusters.empty()) {
+        fEndLine.extend(fClusters);
+        if (!fTooLongCluster) {
+            return;
+        }
+    }
+
+    if (!fClip.empty()) {
+        // Flutter: forget the clipped cluster but keep the metrics
+        fEndLine.metrics().add(fClip.metrics());
+    }
 }
 
 // Special case for start/end cluster since they can be clipped
@@ -169,7 +171,7 @@ void TextWrapper::breakTextIntoLines(ParagraphImpl* parent,
 
     auto span = parent->clusters();
     if (span.size() == 0) {
-      return;
+        return;
     }
     auto maxLines = parent->paragraphStyle().getMaxLines();
     auto& ellipsisStr = parent->paragraphStyle().getEllipsis();
@@ -185,10 +187,10 @@ void TextWrapper::breakTextIntoLines(ParagraphImpl* parent,
     while (fEndLine.endCluster() != end) {
 
         reset();
-
+        auto exceededLines = !endlessLine && fLineNumber >= maxLines;
         fEndLine.metrics().clean();
         lookAhead(maxWidth, end);
-        moveForward();
+        moveForward(exceededLines && !ellipsisStr.isEmpty());
 
         // Do not trim end spaces on the naturally last line of the left aligned text
         trimEndSpaces(align);
@@ -203,8 +205,6 @@ void TextWrapper::breakTextIntoLines(ParagraphImpl* parent,
             fEndLine.endCluster() < end - 1 &&
             SkScalarIsFinite(maxWidth) &&
             !ellipsisStr.isEmpty();
-
-        auto exceededLines = !endlessLine && fLineNumber >= maxLines;
 
         // TODO: perform ellipsis work here
 
