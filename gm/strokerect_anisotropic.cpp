24041c2b5bd837dac940b3a3d64312392602928c
Robert Phillips <robertphillips@google.com>
2020-01-08 13:22:18 -0500

    update
    Bug: 935303
    Change-Id: I3ff9bc73cad5ad5b8e13ef5aa7b46bbf4835753b

---
+++ gm/strokerect_anisotropic.cpp
@@ -0,0 +1,80 @@
+/*
+* Copyright 2020 Google Inc.
+*
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file.
+*/
+
+#include "gm/gm.h"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkPath.h"
+
+static void draw_sqooshed_rect(SkCanvas* canvas, SkVector xlate, const SkPaint& p) {
+    canvas->save();
+        canvas->translate(xlate.fX, xlate.fY);
+        canvas->scale(0.03f, 2.0f);
+        canvas->drawRect(SkRect::MakeLTRB(-500, -10, 500, 10), p);
+    canvas->restore();
+}
+
+/*
+ * This GM is intended to wring out any lingering anisotropic
+ * stroke rect bugs. It contains a repro case for crbug.com/935303
+ * The pattern is:
+ *
+ *         miter @       miter @      bevel @       bevel @
+ *         whole pixels  half pixels  whole pixels  half pixels
+ *
+ *   AA
+ *
+ * non-AA
+ *
+ */
+class StrokeRectAnisotropicGM : public skiagm::GM {
+public:
+    StrokeRectAnisotropicGM() {}
+
+protected:
+
+    SkString onShortName() override {
+        return SkString("strokerect_anisotropic");
+    }
+
+    SkISize onISize() override {
+        return SkISize::Make(256, 256);
+    }
+
+    void onDraw(SkCanvas* canvas) override {
+
+        SkPaint aaPaint;
+        aaPaint.setColor(SkColorSetARGB(255, 0, 0, 0));
+        aaPaint.setAntiAlias(true);
+        aaPaint.setStrokeWidth(10);
+        aaPaint.setStyle(SkPaint::kStroke_Style);
+
+        SkPaint bwPaint;
+        bwPaint.setColor(SkColorSetARGB(255, 0, 0, 0));
+        bwPaint.setStrokeWidth(10);
+        bwPaint.setStyle(SkPaint::kStroke_Style);
+
+        draw_sqooshed_rect(canvas, {  20.5f, 35.5f }, aaPaint);
+        draw_sqooshed_rect(canvas, {  60.0f, 35.0f }, aaPaint);
+//        draw_sqooshed_rect(canvas, {  20.5f, 105.5f }, bwPaint);
+//        draw_sqooshed_rect(canvas, {  55.0f, 105.0f }, bwPaint);
+
+#if 0
+        aaPaint.setStrokeJoin(SkPaint::kBevel_Join);
+        bwPaint.setStrokeJoin(SkPaint::kBevel_Join);
+
+        draw_sqooshed_rect(canvas, {  90.5f, 35.5f }, aaPaint);
+        draw_sqooshed_rect(canvas, { 125.0f, 35.0f }, aaPaint);
+        draw_sqooshed_rect(canvas, {  90.5f, 105.5f }, bwPaint);
+        draw_sqooshed_rect(canvas, { 125.0f, 105.0f }, bwPaint);
+#endif
+    }
+
+private:
+    typedef GM INHERITED;
+};
+DEF_GM(return new StrokeRectAnisotropicGM;)
+
