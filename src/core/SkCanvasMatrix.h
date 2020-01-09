094cf05a34ac900859b8e8ae4aec0afcb9131bc9
Mike Reed <reed@google.com>
2020-01-09 08:49:44 -0500

    Remove legacy canvas-matrix impl
    Chrome (the only user of the flag) is now updated.
    
    Change-Id: I137f471588238f2b1cd4bc6013affb8075d0c561

--- src/core/SkCanvasMatrix.h
+++ src/core/SkCanvasMatrix.h
@@ -12,32 +12,15 @@
 #include "include/private/SkM44.h"
 
 class SkCanvasMatrix {
-#ifdef SK_SUPPORT_LEGACY_CANVAS_MATRIX_33
-    SkMatrix fM;
-#else
     SkM44 fM;
-#endif
 public:
     SkCanvasMatrix& operator=(const SkMatrix& other) { fM = other; return *this; }
 
     void reset() { fM.setIdentity(); }
     void preTranslate(SkScalar x, SkScalar y) { fM.preTranslate(x, y); }
     void preConcat(const SkMatrix& m) { fM.preConcat(m); }
+    void preConcat44(const SkScalar m[]) { fM.setConcat(fM, m); }
 
-#ifdef SK_SUPPORT_LEGACY_CANVAS_MATRIX_33
-    operator SkMatrix() const { return fM; }
-    bool isScaleTranslate() const { return fM.isScaleTranslate(); }
-    bool rectStaysRect() const { return fM.rectStaysRect(); }
-
-    float getScaleX() const { return fM.getScaleX(); }
-    float getScaleY() const { return fM.getScaleY(); }
-    float getTranslateX() const { return fM.getTranslateX(); }
-    float getTranslateY() const { return fM.getTranslateY(); }
-
-    bool invert(SkMatrix* inv) const { return fM.invert(inv); }
-
-    bool mapRect(SkRect* dst, const SkRect& src) { return fM.mapRect(dst, src); }
-#else
     operator SkMatrix() const { return fM.asM33(); }
     // the legacy check was just for the 3x3 portion, so we only check those
     bool isScaleTranslate() const {
@@ -54,9 +37,6 @@ public:
     bool invert(SkMatrix* inv) const { return fM.asM33().invert(inv); }
 
     bool mapRect(SkRect* dst, const SkRect& src) { return fM.asM33().mapRect(dst, src); }
-
-    void preConcat44(const SkScalar m[]) { fM.setConcat(fM, m); }
-#endif
 };
 
 #endif
