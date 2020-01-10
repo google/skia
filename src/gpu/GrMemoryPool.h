e9af00da65f7dbe01ff8e272c9c9f17757fe0786
Michael Ludwig <michaelludwig@google.com>
2020-01-10 15:41:24 -0500

    memroy pool wip
    Change-Id: Ia6c8709642369b66b88eb1bc46264fb2aa9b62ab

--- src/gpu/GrMemoryPool.h
+++ src/gpu/GrMemoryPool.h
@@ -8,147 +8,137 @@
 #ifndef GrMemoryPool_DEFINED
 #define GrMemoryPool_DEFINED
 
-#include "include/private/GrTypesPriv.h"
-
-#include "include/core/SkRefCnt.h"
+#include "src/gpu/GrBlockAllocator.h"
 
 #ifdef SK_DEBUG
-#include "include/private/SkTHash.h"
+    #include "include/private/SkTHash.h"
 #endif
 
 /**
- * Allocates memory in blocks and parcels out space in the blocks for allocation
- * requests. It is optimized for allocate / release speed over memory
- * efficiency. The interface is designed to be used to implement operator new
- * and delete overrides. All allocations are expected to be released before the
- * pool's destructor is called. Allocations will be aligned to
- * sizeof(std::max_align_t).
+ * Allocates memory in blocks and parcels out space in the blocks for allocation requests. All
+ * heap-allocated blocks will It is optimized for allocate / release speed over memory efficiency.
+ * The interface is designed to be used to implement operator new and delete overrides. All
+ * allocations are expected to be released before the pool's destructor is called. Allocations will
+ * be aligned to sizeof(std::max_align_t).
+ *
+ * All allocated objects must be released back to the memory pool before it can be destroyed.
  */
-class GrMemoryPool {
+class GrMemoryPool : protected GrBlockAllocator {
 public:
     // Guaranteed alignment of pointer returned by allocate().
     static constexpr size_t kAlignment = alignof(std::max_align_t);
-    // Minimum size this class will allocate at once.
+
+    // Smallest block size allocated on the heap (not the smallest reservation via allocate()).
     static constexpr size_t kMinAllocationSize = 1 << 10;
 
     /**
-     * Prealloc size is the amount of space to allocate at pool creation
-     * time and keep around until pool destruction. The min alloc size is
-     * the smallest allowed size of additional allocations. Both sizes are
-     * adjusted to ensure that they are at least as large as kMinAllocationSize.
+     * Prealloc size is the amount of space to allocate at pool creation time and keep around until
+     * pool destruction.
      *
-     * Both sizes are what the pool will end up allocating from the system, and
-     * portions of the allocated memory is used for internal bookkeeping.
+     * The size is what the pool will end up allocating from the system, and portions of the
+     * allocated memory are used for internal bookkeeping. 'preallocSize' is clamped to the min and
+     * max allocation sizes.
      */
-    static std::unique_ptr<GrMemoryPool> Make(size_t preallocSize, size_t minAllocSize);
-    void operator delete(void* p) { ::operator delete(p); }
+    static std::unique_ptr<GrMemoryPool> Make(size_t preallocSize) {
+        return GrBlockAllocator::Prealloc<GrMemoryPool>(std::max(preallocSize, kMinAllocationSize));
+    }
 
     ~GrMemoryPool();
+    void operator delete(void* p) { ::operator delete(p); }
 
     /**
-     * Allocates memory. The memory must be freed with release().
+     * Allocates memory. The memory must be freed with release() before the GrMemoryPool is deleted.
      */
     void* allocate(size_t size);
-
     /**
-     * p must have been returned by allocate()
+     * p must have been returned by allocate().
      */
     void release(void* p);
 
     /**
      * Returns true if there are no unreleased allocations.
      */
-    bool isEmpty() const { return fTail == fHead && !fHead->fLiveCount; }
+    bool isEmpty() const {
+        // If size is the same as preallocSize, there aren't any heap blocks, so currentBlock()
+        // is the inline head block.
+        return 0 == this->size() && 0 == *this->liveCount();
+    }
 
     /**
      * Returns the total allocated size of the GrMemoryPool minus any preallocated amount
+     * (does not include the preallocSize for historic reasons).
      */
-    size_t size() const { return fSize; }
+    size_t size() const { return this->GrBlockAllocator::size() - this->preallocSize(); }
 
     /**
      * Returns the preallocated size of the GrMemoryPool
      */
-    size_t preallocSize() const { return fHead->fSize; }
-
+    using GrBlockAllocator::preallocSize;
 
 private:
-    GrMemoryPool(void* preallocStart, size_t preallocSize, size_t minAllocSize);
+    friend class GrBlockAllocator; // For private constructor
+    friend class GrOpMemoryPool;   // ""
 
-    struct BlockHeader;
-
-    static BlockHeader* CreateBlock(size_t size);
-    static BlockHeader* InitBlock(void* mem, size_t blockSize);
-
-    static void DeleteBlock(BlockHeader* block);
-
-    void validate();
-
-    struct BlockHeader {
-#ifdef SK_DEBUG
-        uint32_t     fBlockSentinal;  ///< known value to check for bad back pointers to blocks
-#endif
-        BlockHeader* fNext;      ///< doubly-linked list of blocks.
-        BlockHeader* fPrev;
-        int          fLiveCount; ///< number of outstanding allocations in the
-                                 ///< block.
-        intptr_t     fCurrPtr;   ///< ptr to the start of blocks free space.
-        intptr_t     fPrevPtr;   ///< ptr to the last allocation made
-        size_t       fFreeSize;  ///< amount of free space left in the block.
-        size_t       fSize;      ///< total allocated size of the block
-    };
-
-    static const uint32_t kAssignedMarker = 0xCDCDCDCD;
-    static const uint32_t kFreedMarker    = 0xEFEFEFEF;
-
+    // GrMemoryPool per-allocation overhead so that it can always identify the block owning each
     struct AllocHeader {
+        AllocHeader(Block block, int offset, int size);
+
 #ifdef SK_DEBUG
-        uint32_t fSentinal;      ///< known value to check for memory stomping (e.g., (CD)*)
-        int32_t fID;             ///< ID that can be used to track down leaks by clients.
+        int   fSentinel; // known value to check for memory stomping (e.g., (CD)*)
+        int   fID;       // ID that can be used to track down leaks by clients.
 #endif
-        BlockHeader* fHeader;    ///< pointer back to the block header in which an alloc resides
+
+        Block fBlock;  // block owning the allocation
+        int   fOffset; // offset from block to the start of the allocation
+        int   fSize;   // total bytes for the allocation
     };
 
-    size_t                            fSize;
-    size_t                            fMinAllocSize;
-    BlockHeader*                      fHead;
-    BlockHeader*                      fTail;
+    GrMemoryPool(void* headBlock, int blockSize);
+
+    // GrMemoryPool uses the block allocator's per-block metadata to track the number of live allocs
+    const int* liveCount() const { return BlockData(this->currentBlock()); }
+    int* liveCount() { return BlockData(this->currentBlock()); }
+    int* liveCount(Block b) { return BlockData(b); }
+
 #ifdef SK_DEBUG
-    int                               fAllocationCnt;
-    int                               fAllocBlockCnt;
-    SkTHashSet<int32_t>               fAllocatedIDs;
+    void validate() const;
+
+    SkTHashSet<int> fAllocatedIDs;
+    int             fAllocationCount;
 #endif
-
-    friend class GrOpMemoryPool;
-
-    static constexpr size_t kHeaderSize  = GrAlignTo(sizeof(BlockHeader), kAlignment);
-    static constexpr size_t kPerAllocPad = GrAlignTo(sizeof(AllocHeader), kAlignment);
 };
 
 class GrOp;
 
-class GrOpMemoryPool {
+/**
+ * A specialized variant on GrMemoryPool that provides convenience functions for allocating and
+ * releasing GrOps.
+ */
+class GrOpMemoryPool : private GrMemoryPool {
 public:
-    static std::unique_ptr<GrOpMemoryPool> Make(size_t preallocSize, size_t minAllocSize);
-    void operator delete(void* p) { ::operator delete(p); }
+    static std::unique_ptr<GrOpMemoryPool> Make(size_t preallocSize) {
+        return GrBlockAllocator::Prealloc<GrOpMemoryPool>(std::max(preallocSize,
+                                                                   kMinAllocationSize));
+    }
 
-    ~GrOpMemoryPool();
+    void operator delete(void* p) { ::operator delete(p); }
 
     template <typename Op, typename... OpArgs>
     std::unique_ptr<Op> allocate(OpArgs&&... opArgs) {
-        auto mem = this->pool()->allocate(sizeof(Op));
+        auto mem = this->allocate(sizeof(Op));
         return std::unique_ptr<Op>(new (mem) Op(std::forward<OpArgs>(opArgs)...));
     }
 
-    void* allocate(size_t size) { return this->pool()->allocate(size); }
-
     void release(std::unique_ptr<GrOp> op);
 
-    bool isEmpty() const { return this->pool()->isEmpty(); }
+    using GrMemoryPool::allocate;
+    using GrMemoryPool::isEmpty;
 
 private:
-    GrMemoryPool* pool() const;
+    friend class GrBlockAllocator; // For private constructor
 
-    GrOpMemoryPool() = default;
+    GrOpMemoryPool(void* headBlock, int blockSize)
+            : GrMemoryPool(headBlock, blockSize) {}
 };
 
 #endif
