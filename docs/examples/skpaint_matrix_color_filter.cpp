4963cfc3f7a6b89c0a186038a525be076e1dd12f
Hal Canary <halcanary@google.com>
2020-01-10 15:22:09 -0500

    add docs/examples
    ignore offscreen, srgb, and animated fiddles for now.
    
    Change-Id: I923131b684865698e6cda138b004930e11f504d5

---
+++ docs/examples/skpaint_matrix_color_filter.cpp
@@ -0,0 +1,26 @@
+// Copyright 2020 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#include "tools/fiddle/examples.h"
+REG_FIDDLE(skpaint_matrix_color_filter, 256, 128, false, 3) {
+void f(SkCanvas* c, SkScalar x, SkScalar y, SkScalar colorMatrix[20]) {
+    SkPaint paint;
+    paint.setColorFilter(SkColorFilters::Matrix(colorMatrix));
+    c->drawImage(image, x, y, &paint);
+}
+void draw(SkCanvas* c) {
+    c->scale(0.25, 0.25);
+    SkScalar colorMatrix1[20] = {
+        0, 1, 0, 0, 0,
+        0, 0, 1, 0, 0,
+        1, 0, 0, 0, 0,
+        0, 0, 0, 1, 0};
+    f(c, 0, 0, colorMatrix1);
+
+    SkScalar grayscale[20] = {
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.0f,  0.0f,  0.0f,  1.0f, 0.0f};
+    f(c, 512, 0, grayscale);
+}
+}  // END FIDDLE
