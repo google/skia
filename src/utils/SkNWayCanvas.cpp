12516d095040720a4b60062029e76e6014d7c127
Mike Reed <reed@google.com>
2020-01-10 14:01:03 -0500

    Override didTranslate, and add virtual for didScale
    This completes pushing through the new virtual didConcat44() to our
    subclasses, and introduces didScale() for future optimizations. We
    don't call didScale yet, until external subclasses are also updated.
    
    Change-Id: Ia26b48e76e323037082e8f2ee83673c26b99ebed

--- src/utils/SkNWayCanvas.cpp
+++ src/utils/SkNWayCanvas.cpp
@@ -124,6 +124,14 @@ void SkNWayCanvas::didSetMatrix(const SkMatrix& matrix) {
     this->INHERITED::didSetMatrix(matrix);
 }
 
+void SkNWayCanvas::didScale(SkScalar x, SkScalar y) {
+    Iter iter(fList);
+    while (iter.next()) {
+        iter->scale(x, y);
+    }
+    this->INHERITED::didScale(x, y);
+}
+
 void SkNWayCanvas::onClipRect(const SkRect& rect, SkClipOp op, ClipEdgeStyle edgeStyle) {
     Iter iter(fList);
     while (iter.next()) {
