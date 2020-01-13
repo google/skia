9403c3897f815082dd9421f5133565b698c0037d
Mike Reed <reed@google.com>
2020-01-13 14:40:56 +0000

    Revert "Reland "Revert "allow clients to opt into new didTranslate/didScale"""
    This reverts commit f66bba6018832ed8fc6f4e1ad39fc2952f8e190c.
    
    Reason for revert: sheriff thinks it may be the bot, so landing
    
    Original change's description:
    > Reland "Revert "allow clients to opt into new didTranslate/didScale""
    > 
    > This reverts commit d7ce7ac8d11e78e9452c0c59a20c207e9b8865fa.
    > 
    > Reason for revert: breaks Test-Debian10-GCC-GCE-CPU-AVX2-x86-Debug-All-Docker
    > 
    > Original change's description:
    > > Revert "Revert "allow clients to opt into new didTranslate/didScale""
    > > 
    > > This reverts commit 4a46758db8a83ec23ade5319333db321bf0f7788.
    > > 
    > > Add guard to Flutter
    > > 
    > > Change-Id: Ief0e5cb36af13c8f00a36a617d0384622012d644
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/263937
    > > Reviewed-by: Mike Reed <reed@google.com>
    > > Commit-Queue: Mike Reed <reed@google.com>
    > 
    > TBR=reed@google.com
    > 
    > Change-Id: I3291c4dfe18d6e751e61f55ed9b22a01f0c8ad72
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/263860
    > Reviewed-by: Mike Reed <reed@google.com>
    > Commit-Queue: Mike Reed <reed@google.com>
    
    TBR=reed@google.com
    
    Change-Id: I3111a034291c2320e5ff33f8c2072354f836440f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/263939
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- src/core/SkCanvasMatrix.h
+++ src/core/SkCanvasMatrix.h
@@ -18,6 +18,7 @@ public:
 
     void reset() { fM.setIdentity(); }
     void preTranslate(SkScalar x, SkScalar y) { fM.preTranslate(x, y); }
+    void preScale(SkScalar x, SkScalar y) { fM.preScale(x, y); }
     void preConcat(const SkMatrix& m) { fM.preConcat(m); }
     void preConcat44(const SkScalar m[]) { fM.setConcat(fM, m); }
 
