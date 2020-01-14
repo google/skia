8135cd53729dadf94a5f238d0ccdf8947b504c94
Kevin Lubick <kjlubick@google.com>
2020-01-14 08:34:58 -0500

    Add support for reading .skp files in CanvasKit.
    Bug: skia:9760
    Change-Id: Ifecd819a0e4b6b8f8818ce593afd546e10c36df3

--- modules/canvaskit/skp.js
+++ modules/canvaskit/skp.js
@@ -1,11 +1,24 @@
 CanvasKit._extraInitializations = CanvasKit._extraInitializations || [];
 CanvasKit._extraInitializations.push(function() {
+  // data is a TypedArray or ArrayBuffer e.g. from fetch().then(resp.arrayBuffer())
+  CanvasKit.MakeSkPicture = function(data) {
+    data = new Uint8Array(data);
+
+    var iptr = CanvasKit._malloc(data.byteLength);
+    CanvasKit.HEAPU8.set(data, iptr);
+    var pic = CanvasKit._MakeSkPicture(iptr, data.byteLength);
+    if (!pic) {
+      SkDebug('Could not decode picture');
+      return null;
+    }
+    return pic;
+  }
+
   // The serialized format of an SkPicture (informally called an "skp"), is not something
-  // that clients should ever rely on. It is useful when filing bug reports, but that's
-  // about it. The format may change at anytime and no promises are made for backwards
-  // or forward compatibility.
-  CanvasKit.SkPicture.prototype.DEBUGONLY_saveAsFile = function(skpName) {
-    var data = this.DEBUGONLY_serialize();
+  // that clients should ever rely on. The format may change at anytime and no promises
+  // are made for backwards or forward compatibility.
+  CanvasKit.SkPicture.prototype.saveAsFile = function(skpName) {
+    var data = this.serialize();
     if (!data) {
       SkDebug('Could not serialize to skpicture.');
       return;
