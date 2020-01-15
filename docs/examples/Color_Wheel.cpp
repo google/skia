2cd5d43f022caeb462d3d6519cafbf38d166354a
Hal Canary <halcanary@google.com>
2020-01-15 15:34:55 -0500

    [minor] docs/examples: cleanup
    Change-Id: I2dfec6de7c3ccd3f6b8ed63a9235c527fc648a1e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/264647
    Reviewed-by: Hal Canary <halcanary@google.com>
    Commit-Queue: Hal Canary <halcanary@google.com>

--- docs/examples/Color_Wheel.cpp
+++ docs/examples/Color_Wheel.cpp
@@ -2,45 +2,46 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 #include "tools/fiddle/examples.h"
 REG_FIDDLE(Color_Wheel, 256, 256, false, 0) {
-static void draw_center_letter(SkCanvas* canvas, const char* str , const SkFont& font,
-                               SkScalar x, SkScalar y, SkColor4f color) {
-    SkRect bnds;
-    font.measureText(str, strlen(str), SkTextEncoding::kUTF8, &bnds);
-    canvas->drawString(str, x - bnds.centerX(), y - bnds.centerY(), font, SkPaint(color));
+void draw_color_wheel(SkCanvas* canvas, float scale) {
+    const float stroke = scale * 3 / 64;
+    const float size = scale * 9 / 32;
+    SkAutoCanvasRestore autoCanvasRestore(canvas, true);
+    canvas->translate(0.5f * scale, 0.5f * scale);
+    canvas->clear(0);
+    canvas->drawCircle({0, 0}, (scale - stroke) * 0.5f, SkPaint(SkColors::kWhite));
+    {
+        SkPaint sweep;
+        const SkMatrix rotate = SkMatrix::MakeAll(0, -1, 0, 1, 0, 0, 0, 0, 1);
+        constexpr unsigned kColorCount = 7;
+        static const SkColor4f kColors[kColorCount] = {
+            SkColors::kRed,  SkColors::kYellow,  SkColors::kGreen, SkColors::kCyan,
+            SkColors::kBlue, SkColors::kMagenta, SkColors::kRed};
+        sweep.setShader(SkGradientShader::MakeSweep(0, 0, kColors, nullptr, nullptr,
+                                                    kColorCount, 0, &rotate));
+        sweep.setStyle(SkPaint::kStroke_Style);
+        sweep.setStrokeWidth(stroke);
+        sweep.setAntiAlias(true);
+        canvas->drawCircle({0, 0}, (scale - stroke) * 0.5f, sweep);
+    }
+    sk_sp<SkFontMgr> fontMgr = SkFontMgr::RefDefault();
+    SkFont font(fontMgr->legacyMakeTypeface(nullptr, SkFontStyle::Bold()), size);
+    const struct { const char* str; float radius; float angle; SkColor4f color;} kLetters[] = {
+        {"K", 0,   0, SkColors::kBlack},
+        {"R", 1,  90, SkColors::kRed},
+        {"Y", 1, 150, SkColors::kYellow},
+        {"G", 1, 210, SkColors::kGreen},
+        {"C", 1, 270, SkColors::kCyan},
+        {"B", 1, 330, SkColors::kBlue},
+        {"M", 1,  30, SkColors::kMagenta},
+    };
+    for (const auto& v : kLetters) {
+        SkRect bnds;
+        font.measureText(v.str, strlen(v.str), SkTextEncoding::kUTF8, &bnds);
+        constexpr double pi_over_180 = 3.141592653589793 / 180;
+        float x = (float)(0.3 * scale * v.radius * cos(pi_over_180 * v.angle) - bnds.centerX());
+        float y = (float)(0.3 * scale * v.radius * sin(pi_over_180 * v.angle) - bnds.centerY());
+        canvas->drawString(v.str, x, y, font, SkPaint(v.color));
+    }
 }
-
-void draw(SkCanvas* canvas) {
-     constexpr float SCALE = 256;
-     constexpr float STROKE = 12;
-     SkAutoCanvasRestore autoCanvasRestore(canvas, true);
-     canvas->translate(0.5f * SCALE, 0.5f * SCALE);
-     canvas->clear(0);
-     canvas->drawCircle({0, 0}, (SCALE - STROKE) * 0.5f, SkPaint(SkColors::kWhite));
-     {
-         SkPaint sweep;
-         const SkMatrix rotate = SkMatrix::MakeAll(0, -1, 0, 1, 0, 0, 0, 0, 1);
-         static const SkColor4f kColors[7] = {
-             SkColors::kRed,  SkColors::kYellow,  SkColors::kGreen, SkColors::kCyan,
-             SkColors::kBlue, SkColors::kMagenta, SkColors::kRed};
-         sweep.setShader(
-                 SkGradientShader::MakeSweep(0, 0, kColors, nullptr, nullptr, 7, 0, &rotate));
-         sweep.setStyle(SkPaint::kStroke_Style);
-         sweep.setStrokeWidth(STROKE);
-         canvas->drawCircle({0, 0}, (SCALE - STROKE) * 0.5f, sweep);
-     }
-     const double sqrt_3_over_2 = 0.8660254037844387;
-     const SkScalar D = 0.3f * SCALE;
-     const SkScalar X = (float)(D * sqrt_3_over_2);
-     const SkScalar Y = D * 0.5f;
-
-     sk_sp<SkFontMgr> fontMgr = SkFontMgr::RefDefault();
-     SkFont font(fontMgr->legacyMakeTypeface(nullptr, SkFontStyle::Bold()), 0.28125f * SCALE);
-     draw_center_letter(canvas, "K", font,  0,  0, SkColors::kBlack);
-     draw_center_letter(canvas, "R", font,  0,  D, SkColors::kRed);
-     draw_center_letter(canvas, "G", font, -X, -Y, SkColors::kGreen);
-     draw_center_letter(canvas, "B", font,  X, -Y, SkColors::kBlue);
-     draw_center_letter(canvas, "C", font,  0, -D, SkColors::kCyan);
-     draw_center_letter(canvas, "M", font,  X,  Y, SkColors::kMagenta);
-     draw_center_letter(canvas, "Y", font, -X,  Y, SkColors::kYellow);
-}
+void draw(SkCanvas* canvas) { draw_color_wheel(canvas, 256); }
 }  // END FIDDLE
