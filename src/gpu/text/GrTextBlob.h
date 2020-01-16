c9b4dda5e37886d455cc48eae25f862bad258a11
Herb Derby <herb@google.com>
2020-01-16 15:41:54 -0500

    Isolate updating grTextStrike
    I'm isolating this code into a single routine, so I can change it
    over to using a bulk API from the strike. The goal is to remove
    all uses of bulk metrics and images.
    
    Change-Id: I8c1c9a4e619962c8e35084c026a4c4553fbeb577

--- src/gpu/text/GrTextBlob.h
+++ src/gpu/text/GrTextBlob.h
@@ -312,10 +312,7 @@ private:
     GrAtlasManager* fFullAtlasManager;
     SkTLazy<SkBulkGlyphMetricsAndImages> fMetricsAndImages;
     SubRun* fSubRun;
-    struct {
-        bool regenTextureCoordinates:1;
-        bool regenStrike:1;
-    } fActions = {false, false};
+    bool fRegenerateTextureCoordinates;
 };
 
 // -- GrTextBlob::SubRun ---------------------------------------------------------------------------
@@ -360,6 +357,7 @@ public:
     void translateVerticesIfNeeded(const SkMatrix& drawMatrix, SkPoint drawOrigin);
     void updateVerticesColorIfNeeded(GrColor newColor);
     void updateTexCoords(int begin, int end);
+    void updateStrikeIfNeeded(SkBulkGlyphMetricsAndImages& metricsAndImages, GrStrikeCache* grStrikeCache);
 
     // df properties
     void setUseLCDText(bool useLCDText);
