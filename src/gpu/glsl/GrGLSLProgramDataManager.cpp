658693f891533862d2b07bd0aee8b3d2b427f8b7
Mike Reed <reed@google.com>
2020-01-20 20:31:40 -0500

    use SkM44 internally
    Change-Id: Id782ac1cc8b8d7f6621970e44e1f9729964d2a94

--- src/gpu/glsl/GrGLSLProgramDataManager.cpp
+++ src/gpu/glsl/GrGLSLProgramDataManager.cpp
@@ -8,7 +8,7 @@
 #include "src/gpu/glsl/GrGLSLProgramDataManager.h"
 
 #include "include/core/SkMatrix.h"
-#include "include/core/SkMatrix44.h"
+#include "include/private/SkM44.h"
 
 void GrGLSLProgramDataManager::setSkMatrix(UniformHandle u, const SkMatrix& matrix) const {
     float mt[] = {
@@ -25,8 +25,8 @@ void GrGLSLProgramDataManager::setSkMatrix(UniformHandle u, const SkMatrix& matr
     this->setMatrix3f(u, mt);
 }
 
-void GrGLSLProgramDataManager::setSkMatrix44(UniformHandle u, const SkMatrix44& matrix) const {
+void GrGLSLProgramDataManager::setSkMatrix44(UniformHandle u, const SkM44& matrix) const {
     float mt[16];
-    matrix.asColMajorf(mt);
+    matrix.getColMajor(mt);
     this->setMatrix4f(u, mt);
 }
