24ef95222070b18300a525867d76600c0f630449
Ben Wagner <benjaminwagner@google.com>
2020-01-20 21:15:18 -0500

    Add clang_mac asset
    Change-Id: I58f2f34c3af8fc66b075502c2bc1a3a7772fe258

---
+++ infra/bots/assets/clang_mac/create.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Create the asset."""
+
+
+import argparse
+import common
+import subprocess
+import utils
+
+
+# The following are copied from
+# https://cs.chromium.org/chromium/src/tools/clang/scripts/update.py
+CLANG_REVISION = 'c2443155a0fb245c8f17f2c1c72b6ea391e86e81'
+CLANG_SVN_REVISION = 'n332890'
+CLANG_SUB_REVISION = 1
+
+PACKAGE_VERSION = '%s-%s-%s' % (CLANG_SVN_REVISION, CLANG_REVISION[:8],
+                                CLANG_SUB_REVISION)
+
+CDS_URL = 'https://commondatastorage.googleapis.com/chromium-browser-clang'
+
+
+# These are derived from above constants.
+URL_PREFIX = CDS_URL + '/Mac/'
+CDS_FILE = "clang-%s.tgz" % PACKAGE_VERSION
+CDS_FULL_URL = URL_PREFIX + CDS_FILE
+
+
+def create_asset(target_dir):
+  """Create the asset."""
+  with utils.tmp_dir():
+    subprocess.check_call(["curl", CDS_FULL_URL, "-o", "clang_mac.tgz"])
+    subprocess.check_call(['tar', '--extract', '--gunzip', '--file',
+                           'clang_mac.tgz', '--directory', target_dir])
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--target_dir', '-t', required=True)
+  args = parser.parse_args()
+  create_asset(args.target_dir)
+
+
+if __name__ == '__main__':
+  main()
