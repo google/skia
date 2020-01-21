99c54f0290bbd55fba5394a6e4344205d2244063
Ethan Nicholas <ethannicholas@google.com>
2020-01-21 16:06:47 +0000

    Revert "Complete rewrite of the SkSL interpreter"
    This reverts commit 2cde3a13205cdb45e661e9559d34ce9582a1da20.
    
    Reason for revert: breaking the Chrome roll
    
    Original change's description:
    > Complete rewrite of the SkSL interpreter
    > 
    > Change-Id: Idf4037b04c22f8ace5c1ef16c7a28d8c3df92e91
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/250817
    > Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    
    TBR=brianosman@google.com,ethannicholas@google.com
    
    Change-Id: If0fbc78118173e0cacbe1e01cabe3331e35aa49e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/265516
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>

--- src/opts/SkRasterPipeline_opts.h
+++ src/opts/SkRasterPipeline_opts.h
@@ -10,7 +10,7 @@
 
 #include "include/core/SkTypes.h"
 #include "src/core/SkUtils.h"  // unaligned_{load,store}
-#include "src/sksl/SkSLInterpreter.h"
+#include "src/sksl/SkSLByteCode.h"
 
 // Every function in this file should be marked static and inline using SI.
 #if defined(__clang__)
@@ -2711,6 +2711,7 @@ STAGE(interpreter, SkRasterPipeline_InterpreterCtx* c) {
 
     float*  args[]  = { xx, yy, rr, gg, bb, aa };
     float** in_args = args;
+    int     in_count = 6;
 
     if (c->shaderConvention) {
         // our caller must have called seed_shader to set these
@@ -2722,14 +2723,15 @@ STAGE(interpreter, SkRasterPipeline_InterpreterCtx* c) {
         sk_unaligned_store(aa, F(c->paintColor.fA));
     } else {
         in_args += 2;   // skip x,y
+        in_count = 4;
         sk_unaligned_store(rr, r);
         sk_unaligned_store(gg, g);
         sk_unaligned_store(bb, b);
         sk_unaligned_store(aa, a);
     }
 
-    c->interpreter->setUniforms((float*) c->inputs);
-    SkAssertResult(c->interpreter->runStriped(c->fn, tail ? tail : N, (float**) in_args));
+    SkAssertResult(c->byteCode->runStriped(c->fn, tail ? tail : N, in_args, in_count,
+                                           nullptr, 0, (const float*)c->inputs, c->ninputs));
 
     r = sk_unaligned_load<F>(rr);
     g = sk_unaligned_load<F>(gg);
