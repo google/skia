0b4109c851684ef679d186f7ae31d7b088e69ab2
Robert Phillips <robertphillips@google.com>
2020-01-22 14:52:31 -0500

    Add SkImage::MakeRasterFromCompressed and make MakeFromCompressed fall back to decompression for unsupported compression types
    This is intended to make using compressed textures easier since developers can just provided compressed data and it will be uploaded to the GPU in some way, shape or form.
    
    Change-Id: Ieed008f083d6e3594eaa9a02bc5348e00ee60d2a

--- src/gpu/GrDataUtils.h
+++ src/gpu/GrDataUtils.h
@@ -14,6 +14,7 @@
 #include "src/gpu/GrSwizzle.h"
 
 class GrImageInfo;
+class SkBitmap;
 
 size_t GrCompressedDataSize(SkImage::CompressionType, SkISize baseDimensions,
                             SkTArray<size_t>* individualMipOffsets, GrMipMapped);
@@ -50,5 +51,13 @@ bool GrClearImage(const GrImageInfo& dstInfo, void* dst, size_t dstRB, SkColor4f
  *   transparent pixmaps -> kBC1_RGBA8_UNORM
  */
 void GrTwoColorBC1Compress(const SkPixmap& pixmap, SkColor otherColor, char* dstPixels);
+
+/*
+ * This method will decompress the bottommost level in 'data' into 'dst'.
+ */
+bool GrDecompress(sk_sp<SkData> data,
+                  SkISize dimensions,
+                  SkImage::CompressionType compressionType,
+                  SkBitmap* dst);
 
 #endif
