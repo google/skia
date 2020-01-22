77e1f84a84a7d9dd4beae2e7c83ce63f00500cc2
Herb Derby <herb@google.com>
2020-01-22 18:09:16 -0500

    Specialize SkRectanzier to SkRectanizerSkyline
    It looks like the pow2 rectanizer has never been used. Remove
    the unneeded abstraction for rectanizer everywhere.
    
    Change-Id: Iba33f1c6faf37201d03928ce8409751c212480a0
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/265983
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- tests/GpuRectanizerTest.cpp
+++ tests/GpuRectanizerTest.cpp
@@ -8,28 +8,25 @@
 #include "include/core/SkSize.h"
 #include "include/private/SkTDArray.h"
 #include "include/utils/SkRandom.h"
-#include "src/gpu/GrRectanizer_pow2.h"
-#include "src/gpu/GrRectanizer_skyline.h"
+#include "src/gpu/GrRectanizerSkyline.h"
 #include "tests/Test.h"
 
 static const int kWidth = 1024;
 static const int kHeight = 1024;
 
 // Basic test of a GrRectanizer-derived class' functionality
-static void test_rectanizer_basic(skiatest::Reporter* reporter, GrRectanizer* rectanizer) {
+static void test_rectanizer_basic(skiatest::Reporter* reporter, GrRectanizerSkyline* rectanizer) {
     REPORTER_ASSERT(reporter, kWidth == rectanizer->width());
     REPORTER_ASSERT(reporter, kHeight == rectanizer->height());
 
     SkIPoint16 loc;
 
     REPORTER_ASSERT(reporter, rectanizer->addRect(50, 50, &loc));
-    REPORTER_ASSERT(reporter, rectanizer->percentFull() > 0.0f);
     rectanizer->reset();
-    REPORTER_ASSERT(reporter, rectanizer->percentFull() == 0.0f);
 }
 
 static void test_rectanizer_inserts(skiatest::Reporter*,
-                                    GrRectanizer* rectanizer,
+                                    GrRectanizerSkyline* rectanizer,
                                     const SkTDArray<SkISize>& rects) {
     int i;
     for (i = 0; i < rects.count(); ++i) {
@@ -49,13 +46,6 @@ static void test_skyline(skiatest::Reporter* reporter, const SkTDArray<SkISize>&
     test_rectanizer_inserts(reporter, &skylineRectanizer, rects);
 }
 
-static void test_pow2(skiatest::Reporter* reporter, const SkTDArray<SkISize>& rects) {
-    GrRectanizerPow2 pow2Rectanizer(kWidth, kHeight);
-
-    test_rectanizer_basic(reporter, &pow2Rectanizer);
-    test_rectanizer_inserts(reporter, &pow2Rectanizer, rects);
-}
-
 DEF_GPUTEST(GpuRectanizer, reporter, factory) {
     SkTDArray<SkISize> rects;
     SkRandom rand;
@@ -66,5 +56,4 @@ DEF_GPUTEST(GpuRectanizer, reporter, factory) {
     }
 
     test_skyline(reporter, rects);
-    test_pow2(reporter, rects);
 }
