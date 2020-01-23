3ee282429ed7ec2cb3bca25c4ad592ec12072b0c
Ethan Nicholas <ethannicholas@google.com>
2020-01-23 16:49:41 -0500

    Revert "Revert "Complete rewrite of the SkSL interpreter""
    This reverts commit 99c54f0290bbd55fba5394a6e4344205d2244063.
    
    Change-Id: I010ac4fdb6c5b6bfbdf63f4dcac5dbf962b0ad9c

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -77,14 +77,17 @@ static const char* SKSL_PIPELINE_INCLUDE =
 namespace SkSL {
 
 static void grab_intrinsics(std::vector<std::unique_ptr<ProgramElement>>* src,
-               std::map<StringFragment, std::pair<std::unique_ptr<ProgramElement>, bool>>* target) {
-    for (auto& element : *src) {
+               std::map<String, std::pair<std::unique_ptr<ProgramElement>, bool>>* target) {
+    for (auto iter = src->begin(); iter != src->end(); ) {
+        std::unique_ptr<ProgramElement>& element = *iter;
         switch (element->fKind) {
             case ProgramElement::kFunction_Kind: {
                 FunctionDefinition& f = (FunctionDefinition&) *element;
-                StringFragment name = f.fDeclaration.fName;
-                SkASSERT(target->find(name) == target->end());
-                (*target)[name] = std::make_pair(std::move(element), false);
+                SkASSERT(f.fDeclaration.fBuiltin);
+                String key = f.fDeclaration.declaration();
+                SkASSERT(target->find(key) == target->end());
+                (*target)[key] = std::make_pair(std::move(element), false);
+                iter = src->erase(iter);
                 break;
             }
             case ProgramElement::kEnum_Kind: {
@@ -92,6 +95,7 @@ static void grab_intrinsics(std::vector<std::unique_ptr<ProgramElement>>* src,
                 StringFragment name = e.fTypeName;
                 SkASSERT(target->find(name) == target->end());
                 (*target)[name] = std::make_pair(std::move(element), false);
+                iter = src->erase(iter);
                 break;
             }
             default:
@@ -278,11 +282,13 @@ Compiler::Compiler(Flags flags)
     this->processIncludeFile(Program::kPipelineStage_Kind, SKSL_PIPELINE_INCLUDE,
                              strlen(SKSL_PIPELINE_INCLUDE), fGpuSymbolTable, &fPipelineInclude,
                              &fPipelineSymbolTable);
-    std::vector<std::unique_ptr<ProgramElement>> interpIntrinsics;
     this->processIncludeFile(Program::kGeneric_Kind, SKSL_INTERP_INCLUDE,
                              strlen(SKSL_INTERP_INCLUDE), symbols, &fInterpreterInclude,
                              &fInterpreterSymbolTable);
-    grab_intrinsics(&interpIntrinsics, &fInterpreterIntrinsics);
+    grab_intrinsics(&fInterpreterInclude, &fInterpreterIntrinsics);
+    // need to hang on to the source so that FunctionDefinition.fSource pointers in this file
+    // remain valid
+    fInterpreterIncludeSource = std::move(fIRGenerator->fFile);
 }
 
 Compiler::~Compiler() {
@@ -1623,10 +1629,8 @@ std::unique_ptr<ByteCode> Compiler::toByteCode(Program& program) {
     }
     fSource = program.fSource.get();
     std::unique_ptr<ByteCode> result(new ByteCode());
-    ByteCodeGenerator cg(fContext.get(), &program, this, result.get());
-    bool success = cg.generateCode();
-    fSource = nullptr;
-    if (success) {
+    ByteCodeGenerator cg(&program, this, result.get());
+    if (cg.generateCode()) {
         return result;
     }
 #else
