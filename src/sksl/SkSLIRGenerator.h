b91d426659f940dd485079ed8d6b58b361a179a6
Ethan Nicholas <ethannicholas@google.com>
2020-01-23 16:41:20 -0500

    Revert "Revert "Complete rewrite of the SkSL interpreter""
    This reverts commit 99c54f0290bbd55fba5394a6e4344205d2244063.
    
    Change-Id: I010ac4fdb6c5b6bfbdf63f4dcac5dbf962b0ad9c

--- src/sksl/SkSLIRGenerator.h
+++ src/sksl/SkSLIRGenerator.h
@@ -159,7 +159,7 @@ private:
     std::shared_ptr<SymbolTable> fSymbolTable;
     // Symbols which have definitions in the include files. The bool tells us whether this
     // intrinsic has been included already.
-    std::map<StringFragment, std::pair<std::unique_ptr<ProgramElement>, bool>>* fIntrinsics = nullptr;
+    std::map<String, std::pair<std::unique_ptr<ProgramElement>, bool>>* fIntrinsics = nullptr;
     // holds extra temp variable declarations needed for the current function
     std::vector<std::unique_ptr<Statement>> fExtraVars;
     int fLoopLevel;
