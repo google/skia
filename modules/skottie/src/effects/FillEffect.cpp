36a69b52e1227051a82858afbff58600d878976d
Florin Malita <fmalita@chromium.org>
2020-01-30 09:58:17 -0500

    [skottie] Cleanup: convert remaining effects to new pattern
    Also add a couple of tests for missing coverage.
    
    TBR=
    Change-Id: I420c71d73657c5d003fda94a4c43dde20a1a6b87

--- modules/skottie/src/effects/FillEffect.cpp
+++ modules/skottie/src/effects/FillEffect.cpp
@@ -7,6 +7,7 @@
 
 #include "modules/skottie/src/effects/Effects.h"
 
+#include "modules/skottie/src/Adapter.h"
 #include "modules/skottie/src/SkottieValue.h"
 #include "modules/sksg/include/SkSGColorFilter.h"
 #include "modules/sksg/include/SkSGPaint.h"
@@ -15,44 +16,62 @@
 namespace skottie {
 namespace internal {
 
+namespace  {
+
+class FillAdapter final : public AnimatablePropertyContainer {
+public:
+    static sk_sp<FillAdapter> Make(const skjson::ArrayValue& jprops,
+                                   sk_sp<sksg::RenderNode> layer,
+                                   const AnimationBuilder& abuilder) {
+        return sk_sp<FillAdapter>(new FillAdapter(jprops, std::move(layer), abuilder));
+    }
+
+    const auto& node() const { return fFilterNode; }
+
+private:
+    FillAdapter(const skjson::ArrayValue& jprops,
+                sk_sp<sksg::RenderNode> layer,
+                const AnimationBuilder& abuilder)
+        : fColorNode(sksg::Color::Make(SK_ColorBLACK))
+        , fFilterNode(sksg::ModeColorFilter::Make(std::move(layer),
+                                                  fColorNode,
+                                                  SkBlendMode::kSrcIn)) {
+        enum : size_t {
+         // kFillMask_Index = 0,
+         // kAllMasks_Index = 1,
+               kColor_Index = 2,
+         //   kInvert_Index = 3,
+         // kHFeather_Index = 4,
+         // kVFeather_Index = 5,
+             kOpacity_Index = 6,
+        };
+
+        EffectBinder(jprops, abuilder, this)
+            .bind(  kColor_Index, fColor  )
+            .bind(kOpacity_Index, fOpacity);
+
+        abuilder.dispatchColorProperty(fColorNode);
+    }
+
+    void onSync() override {
+        const auto c = ValueTraits<VectorValue>::As<SkColor>(fColor);
+        const auto a =
+                sk_float_round2int_no_saturate(SkColorGetA(c) * SkTPin(fOpacity, 0.0f, 1.0f));
+        fColorNode->setColor(SkColorSetA(c, a));
+    }
+
+    const sk_sp<sksg::Color>           fColorNode;
+    const sk_sp<sksg::ModeColorFilter> fFilterNode;
+
+    VectorValue fColor;
+    ScalarValue fOpacity = 1;
+};
+
+} // namespace
+
 sk_sp<sksg::RenderNode> EffectBuilder::attachFillEffect(const skjson::ArrayValue& jprops,
                                                         sk_sp<sksg::RenderNode> layer) const {
-    enum : size_t {
-        kFillMask_Index = 0,
-        kAllMasks_Index = 1,
-        kColor_Index    = 2,
-        kInvert_Index   = 3,
-        kHFeather_Index = 4,
-        kVFeather_Index = 5,
-        kOpacity_Index  = 6,
-
-        kMax_Index      = kOpacity_Index,
-    };
-
-    if (jprops.size() <= kMax_Index) {
-        return nullptr;
-    }
-
-    const skjson::ObjectValue*   color_prop = jprops[  kColor_Index];
-    const skjson::ObjectValue* opacity_prop = jprops[kOpacity_Index];
-    if (!color_prop || !opacity_prop) {
-        return nullptr;
-    }
-    sk_sp<sksg::Color> color_node = fBuilder->attachColor(*color_prop, "v");
-    if (!color_node) {
-        return nullptr;
-    }
-
-    fBuilder->bindProperty<ScalarValue>((*opacity_prop)["v"],
-        [color_node](const ScalarValue& o) {
-            const auto c = color_node->getColor();
-            const auto a = sk_float_round2int_no_saturate(SkTPin(o, 0.0f, 1.0f) * 255);
-            color_node->setColor(SkColorSetA(c, a));
-        });
-
-    return sksg::ModeColorFilter::Make(std::move(layer),
-                                       std::move(color_node),
-                                       SkBlendMode::kSrcIn);
+    return fBuilder->attachDiscardableAdapter<FillAdapter>(jprops, std::move(layer), *fBuilder);
 }
 
 } // namespace internal
