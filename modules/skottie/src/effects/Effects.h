3c65e5f2276807f8a1dd173d7b1e765eefe4f664
Florin Malita <fmalita@chromium.org>
2020-01-30 09:58:17 -0500

    [skottie] Cleanup: convert remaining effects to new pattern
    Change-Id: I420c71d73657c5d003fda94a4c43dde20a1a6b87

--- modules/skottie/src/effects/Effects.h
+++ modules/skottie/src/effects/Effects.h
@@ -71,6 +71,28 @@ private:
     const SkSize              fLayerSize;
 };
 
+// Syntactic sugar/helper.
+class EffectBinder {
+public:
+    EffectBinder(const skjson::ArrayValue& jprops,
+                 const AnimationBuilder& abuilder,
+                 AnimatablePropertyContainer* acontainer)
+        : fProps(jprops)
+        , fBuilder(abuilder)
+        , fContainer(acontainer) {}
+
+    template <typename T>
+    const EffectBinder& bind(size_t prop_index, T& value) const {
+        fContainer->bind(fBuilder, EffectBuilder::GetPropValue(fProps, prop_index), value);
+
+        return *this;
+    }
+
+private:
+    const skjson::ArrayValue&    fProps;
+    const AnimationBuilder&      fBuilder;
+    AnimatablePropertyContainer* fContainer;
+};
 
 /**
  * Base class for mask-filter-related effects.
