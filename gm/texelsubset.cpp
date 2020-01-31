2ee0a225c169199115c9ef0a96e413bfec7ef8b0
Brian Salomon <bsalomon@google.com>
2020-01-30 21:22:50 -0500

    Improvements to texture_domain GMs
    Rename to texel_subset.
    
    Test different x/y wrap modes.
    
    Use a real image that is non-square.
    
    Add dividers to group MakeTexelSubset() with Make() reference draw.
    
    Use a smaller image, fewer matrices, and only one subset rect to
    make room for the additional mode combinations.
    
    Change-Id: I3fab54cb7f160b92c7e5192e64a8a09f5c837492

--- gm/texelsubset.cpp
+++ gm/texelsubset.cpp
@@ -26,6 +26,7 @@
 #include "src/gpu/GrRenderTargetContextPriv.h"
 #include "src/gpu/GrSamplerState.h"
 #include "src/gpu/GrTextureProxy.h"
+#include "tools/Resources.h"
 #include "tools/gpu/TestOps.h"
 
 #include <memory>
@@ -35,15 +36,15 @@ namespace skiagm {
 /**
  * This GM directly exercises GrTextureEffect::MakeTexelSubset.
  */
-class TextureDomainEffect : public GpuGM {
+class TexelSubset : public GpuGM {
 public:
-    TextureDomainEffect(GrSamplerState::Filter filter) : fFilter(filter) {
+    TexelSubset(GrSamplerState::Filter filter) : fFilter(filter) {
         this->setBGColor(0xFFFFFFFF);
     }
 
 protected:
     SkString onShortName() override {
-        SkString name("texture_domain_effect");
+        SkString name("texel_subset");
         if (fFilter == GrSamplerState::Filter::kBilerp) {
             name.append("_bilerp");
         } else if (fFilter == GrSamplerState::Filter::kMipMap) {
@@ -53,37 +54,20 @@ protected:
     }
 
     SkISize onISize() override {
-        const SkScalar canvasWidth =
-                kDrawPad + 2 * ((kTargetWidth + 2 * kDrawPad) * GrSamplerState::kWrapModeCount +
-                                kTestPad * GrSamplerState::kWrapModeCount);
-        return SkISize::Make(SkScalarCeilToInt(canvasWidth), 800);
+        int n = GrSamplerState::kWrapModeCount;
+        if (fFilter != GrSamplerState::Filter::kNearest) {
+            // Account for not supporting kMirror or kMirrorRepeat with filtering.
+            n -= 2;
+        }
+        const SkScalar w = kTestPad + 2 * n * (fBitmap.width()  + 2 * kDrawPad + kTestPad);
+        const SkScalar h = kTestPad + 2 * n * (fBitmap.height() + 2 * kDrawPad + kTestPad);
+        return SkISize::Make(SkScalarCeilToInt(w), SkScalarCeilToInt(h));
     }
 
     void onOnceBeforeDraw() override {
-        fBitmap.allocN32Pixels(kTargetWidth, kTargetHeight);
-        SkCanvas canvas(fBitmap);
-        canvas.clear(0x00000000);
-        SkPaint paint;
-
-        SkColor colors1[] = { SK_ColorCYAN, SK_ColorLTGRAY, SK_ColorGRAY };
-        paint.setShader(SkGradientShader::MakeSweep(65.f, 75.f, colors1, nullptr,
-                                                    SK_ARRAY_COUNT(colors1)));
-        canvas.drawOval(SkRect::MakeXYWH(-5.f, -5.f, kTargetWidth + 10.f, kTargetHeight + 10.f),
-                        paint);
-
-        SkColor colors2[] = { SK_ColorMAGENTA, SK_ColorLTGRAY, SK_ColorYELLOW };
-        paint.setShader(SkGradientShader::MakeSweep(45.f, 55.f, colors2, nullptr,
-                                                    SK_ARRAY_COUNT(colors2)));
-        paint.setBlendMode(SkBlendMode::kDarken);
-        canvas.drawOval(SkRect::MakeXYWH(-5.f, -5.f, kTargetWidth + 10.f, kTargetHeight + 10.f),
-                        paint);
-
-        SkColor colors3[] = { SK_ColorBLUE, SK_ColorLTGRAY, SK_ColorGREEN };
-        paint.setShader(SkGradientShader::MakeSweep(25.f, 35.f, colors3, nullptr,
-                                                    SK_ARRAY_COUNT(colors3)));
-        paint.setBlendMode(SkBlendMode::kLighten);
-        canvas.drawOval(SkRect::MakeXYWH(-5.f, -5.f, kTargetWidth + 10.f, kTargetHeight + 10.f),
-                        paint);
+        GetResourceAsBitmap("images/mandrill_64.png", &fBitmap);
+        // Make the bitmap non-square to detect any width/height confusion.
+        fBitmap.extractSubset(&fBitmap, SkIRect::MakeSize(fBitmap.dimensions()).makeInset(0, 5));
     }
 
     DrawResult onDraw(GrContext* context, GrRenderTargetContext* renderTargetContext,
@@ -100,52 +84,55 @@ protected:
 
         SkTArray<SkMatrix> textureMatrices;
         textureMatrices.push_back() = SkMatrix::I();
-        textureMatrices.push_back() = SkMatrix::MakeScale(1.5f, 0.85f);
-        textureMatrices.push_back();
-        textureMatrices.back().setRotate(45.f, proxy->width() / 2.f, proxy->height() / 2.f);
+        textureMatrices.push_back().setRotate(45.f, proxy->width() / 2.f, proxy->height() / 2.f);
+        textureMatrices.back().postScale(1.1f, 0.85f);
 
-        const SkIRect texelDomains[] = {
-            fBitmap.bounds(),
-            SkIRect::MakeXYWH(fBitmap.width() / 4 - 1, fBitmap.height() / 4 - 1,
-                              fBitmap.width() / 2 + 2, fBitmap.height() / 2 + 2),
-        };
+        auto texelSubset = SkIRect::MakeXYWH(fBitmap.width() / 6 - 1, fBitmap.height() / 5 - 1,
+                                             fBitmap.width() / 2 + 2, fBitmap.height() / 2 + 2);
 
-        sk_sp<GrTextureProxy> subsetProxies[SK_ARRAY_COUNT(texelDomains)];
-        for (size_t d = 0; d < SK_ARRAY_COUNT(texelDomains); ++d) {
-            SkBitmap subset;
-            fBitmap.extractSubset(&subset, texelDomains[d]);
-            subset.setImmutable();
-            GrBitmapTextureMaker maker(context, subset);
-            std::tie(subsetProxies[d], std::ignore) = maker.refTextureProxy(mipMapped);
-        }
+        sk_sp<GrTextureProxy> subsetProxy;
+        SkBitmap subsetBmp;
+        fBitmap.extractSubset(&subsetBmp, texelSubset);
+        subsetBmp.setImmutable();
+        GrBitmapTextureMaker subsetMaker(context, subsetBmp);
+        std::tie(subsetProxy, std::ignore) = subsetMaker.refTextureProxy(mipMapped);
 
         SkRect localRect = SkRect::Make(fBitmap.bounds()).makeOutset(kDrawPad, kDrawPad);
 
+        auto size = this->onISize();
+
         SkScalar y = kDrawPad + kTestPad;
+        SkRect drawRect;
         for (int tm = 0; tm < textureMatrices.count(); ++tm) {
-            for (size_t d = 0; d < SK_ARRAY_COUNT(texelDomains); ++d) {
+            for (int mx = 0; mx < GrSamplerState::kWrapModeCount; ++mx) {
                 SkScalar x = kDrawPad + kTestPad;
-                for (int m = 0; m < GrSamplerState::kWrapModeCount; ++m) {
-                    auto wm = static_cast<GrSamplerState::WrapMode>(m);
+                auto wmx = static_cast<GrSamplerState::WrapMode>(mx);
+                if (fFilter != GrSamplerState::Filter::kNearest &&
+                    (wmx == GrSamplerState::WrapMode::kRepeat ||
+                     wmx == GrSamplerState::WrapMode::kMirrorRepeat)) {
+                    // [Mirror] Repeat mode doesn't produce correct results with bilerp
+                    // filtering
+                    continue;
+                }
+                for (int my = 0; my < GrSamplerState::kWrapModeCount; ++my) {
+                    auto wmy = static_cast<GrSamplerState::WrapMode>(my);
                     if (fFilter != GrSamplerState::Filter::kNearest &&
-                        (wm == GrSamplerState::WrapMode::kRepeat ||
-                         wm == GrSamplerState::WrapMode::kMirrorRepeat)) {
-                        // [Mirror] Repeat mode doesn't produce correct results with bilerp
-                        // filtering
+                        (wmy == GrSamplerState::WrapMode::kRepeat ||
+                         wmy == GrSamplerState::WrapMode::kMirrorRepeat)) {
                         continue;
                     }
-                    GrSamplerState sampler(wm, fFilter);
+                    GrSamplerState sampler(wmx, wmy, fFilter);
                     const auto& caps = *context->priv().caps();
                     auto fp1 = GrTextureEffect::MakeTexelSubset(proxy,
                                                                 fBitmap.alphaType(),
                                                                 textureMatrices[tm],
                                                                 sampler,
-                                                                texelDomains[d],
+                                                                texelSubset,
                                                                 caps);
                     if (!fp1) {
                         continue;
                     }
-                    auto drawRect = localRect.makeOffset(x, y);
+                    drawRect = localRect.makeOffset(x, y);
                     // Throw a translate in the local matrix just to test having something other
                     // than identity. Compensate with an offset local rect.
                     static constexpr SkVector kT = {-100, 300};
@@ -159,21 +146,37 @@ protected:
                     x += localRect.width() + kTestPad;
 
                     SkMatrix subsetTextureMatrix = SkMatrix::Concat(
-                            SkMatrix::MakeTrans(-texelDomains[d].topLeft()), textureMatrices[tm]);
+                            SkMatrix::MakeTrans(-texelSubset.topLeft()), textureMatrices[tm]);
 
                     // Now draw with a subsetted proxy using fixed function texture sampling rather
                     // than a texture subset as a comparison.
                     drawRect = localRect.makeOffset(x, y);
-                    auto fp2 = GrTextureEffect::Make(subsetProxies[d], fBitmap.alphaType(),
+                    auto fp2 = GrTextureEffect::Make(subsetProxy, fBitmap.alphaType(),
                                                      subsetTextureMatrix,
-                                                     GrSamplerState(wm, fFilter), caps);
+                                                     GrSamplerState(wmx, wmy, fFilter), caps);
                     if (auto op = sk_gpu_test::test_ops::MakeRect(context, std::move(fp2), drawRect,
                                                                   localRect)) {
                         renderTargetContext->priv().testingOnly_addDrawOp(std::move(op));
                     }
+                    if (my < GrSamplerState::kWrapModeCount - 1) {
+                        SkPaint paint;
+                        SkScalar midX = drawRect.right() + kTestPad / 2.f;
+                        canvas->drawLine({midX, 0}, {midX, (float)size.fHeight}, paint);
+                    }
                     x += localRect.width() + kTestPad;
                 }
+                if (mx < GrSamplerState::kWrapModeCount - 1) {
+                    SkPaint paint;
+                    SkScalar midY = drawRect.bottom() + kTestPad / 2.f;
+                    canvas->drawLine({0, midY}, {(float)size.fWidth, midY}, paint);
+                }
                 y += localRect.height() + kTestPad;
+            }
+            if (tm < textureMatrices.count() - 1) {
+                SkPaint paint;
+                paint.setColor(SK_ColorRED);
+                SkScalar midY = drawRect.bottom() + kTestPad / 2.f;
+                canvas->drawLine({0, midY}, {(float)size.fWidth, midY}, paint);
             }
         }
         return DrawResult::kOk;
@@ -182,16 +185,14 @@ protected:
 private:
     static constexpr SkScalar kDrawPad = 10.f;
     static constexpr SkScalar kTestPad = 10.f;
-    static constexpr int      kTargetWidth = 100;
-    static constexpr int      kTargetHeight = 100;
     SkBitmap fBitmap;
     GrSamplerState::Filter fFilter;
 
     typedef GM INHERITED;
 };
 
-DEF_GM(return new TextureDomainEffect(GrSamplerState::Filter::kNearest);)
-DEF_GM(return new TextureDomainEffect(GrSamplerState::Filter::kBilerp);)
-DEF_GM(return new TextureDomainEffect(GrSamplerState::Filter::kMipMap);)
+DEF_GM(return new TexelSubset(GrSamplerState::Filter::kNearest);)
+DEF_GM(return new TexelSubset(GrSamplerState::Filter::kBilerp);)
+DEF_GM(return new TexelSubset(GrSamplerState::Filter::kMipMap);)
 
 }
