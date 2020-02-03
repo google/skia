af99f3ee60177fe5f20f5d7e995540852f8b0d66
Florin Malita <fmalita@chromium.org>
2020-02-03 12:09:15 -0500

    [skottie] Cleanup: finalize adapter conversion
    Convert remaining bindProperty clients to adapters, and remove the
    legacy API.
    
    TBR=
    Change-Id: I06eda513a75b55a1ba7eae328b0916e738c9ddd7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/268309
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

--- modules/skottie/src/layers/PrecompLayer.cpp
+++ modules/skottie/src/layers/PrecompLayer.cpp
@@ -7,6 +7,7 @@
 
 #include "modules/skottie/src/SkottiePriv.h"
 
+#include "modules/skottie/src/Animator.h"
 #include "modules/skottie/src/Composition.h"
 #include "modules/skottie/src/SkottieJson.h"
 #include "modules/skottie/src/SkottieValue.h"
@@ -18,15 +19,73 @@
 namespace skottie {
 namespace internal {
 
+namespace  {
+
+// "Animates" time based on the layer's "tm" property.
+class TimeRemapper final : public AnimatablePropertyContainer {
+public:
+    TimeRemapper(const skjson::ObjectValue& jtm, const AnimationBuilder* abuilder, float scale)
+        : fScale(scale) {
+        this->bind(*abuilder, jtm, fT);
+    }
+
+    float t() const { return fT * fScale; }
+
+private:
+    void onSync() override {
+        // nothing to sync - we just track t
+    }
+
+    const float fScale;
+
+    ScalarValue fT = 0;
+};
+
+// Applies a bias/scale/remap t-adjustment to child animators.
+class CompTimeMapper final : public sksg::GroupAnimator {
+public:
+    CompTimeMapper(sksg::AnimatorList&& layer_animators,
+                   sk_sp<TimeRemapper> remapper,
+                   float time_bias, float time_scale)
+        : INHERITED(std::move(layer_animators))
+        , fRemapper(std::move(remapper))
+        , fTimeBias(time_bias)
+        , fTimeScale(time_scale) {}
+
+    void onTick(float t) override {
+        if (fRemapper) {
+            // When time remapping is active, |t| is fully driven externally.
+            fRemapper->tick(t);
+            t = fRemapper->t();
+        } else {
+            t = (t + fTimeBias) * fTimeScale;
+        }
+
+        this->INHERITED::onTick(t);
+    }
+
+private:
+    const sk_sp<TimeRemapper> fRemapper;
+    const float               fTimeBias,
+                              fTimeScale;
+
+    using INHERITED = sksg::GroupAnimator;
+};
+
+} // namespace
+
 sk_sp<sksg::RenderNode> AnimationBuilder::attachPrecompLayer(const skjson::ObjectValue& jlayer,
                                                              LayerInfo* layer_info) const {
-    const skjson::ObjectValue* time_remap = jlayer["tm"];
-    // Empirically, a time mapper supersedes start/stretch.
-    const auto start_time = time_remap ? 0.0f : ParseDefault<float>(jlayer["st"], 0.0f),
-             stretch_time = time_remap ? 1.0f : ParseDefault<float>(jlayer["sr"], 1.0f);
+    sk_sp<TimeRemapper> time_remapper;
+    if (const skjson::ObjectValue* jtm = jlayer["tm"]) {
+        time_remapper = sk_make_sp<TimeRemapper>(*jtm, this, fFrameRate);
+    }
+
+    const auto start_time = ParseDefault<float>(jlayer["st"], 0.0f),
+             stretch_time = ParseDefault<float>(jlayer["sr"], 1.0f);
     const auto requires_time_mapping = !SkScalarNearlyEqual(start_time  , 0) ||
                                        !SkScalarNearlyEqual(stretch_time, 1) ||
-                                       time_remap;
+                                       time_remapper;
 
     // Precomp layers are sized explicitly.
     layer_info->fSize = SkSize::Make(ParseDefault<float>(jlayer["w"], 0.0f),
@@ -42,45 +101,14 @@ sk_sp<sksg::RenderNode> AnimationBuilder::attachPrecompLayer(const skjson::Objec
             return CompositionBuilder(*this, layer_info->fSize, jcomp).build(*this);
         });
 
-    // Applies a bias/scale/remap t-adjustment to child animators.
-    class CompTimeMapper final : public sksg::GroupAnimator {
-    public:
-        CompTimeMapper(sksg::AnimatorList&& layer_animators, float time_bias, float time_scale)
-            : INHERITED(std::move(layer_animators))
-            , fTimeBias(time_bias)
-            , fTimeScale(time_scale) {}
-
-        void onTick(float t) override {
-            // When time remapping is active, |t| is driven externally.
-            if (fRemappedTime.isValid()) {
-                t = *fRemappedTime.get();
-            }
-
-            this->INHERITED::onTick((t + fTimeBias) * fTimeScale);
-        }
-
-        void remapTime(float t) { fRemappedTime.set(t); }
-
-    private:
-        const float    fTimeBias,
-                       fTimeScale;
-        SkTLazy<float> fRemappedTime;
-
-        using INHERITED = sksg::GroupAnimator;
-    };
-
     if (requires_time_mapping) {
         const auto t_bias  = -start_time,
                    t_scale = sk_ieee_float_divide(1, stretch_time);
-        auto time_mapper = sk_make_sp<CompTimeMapper>(local_scope->release(), t_bias,
+        auto time_mapper = sk_make_sp<CompTimeMapper>(local_scope->release(),
+                                                      std::move(time_remapper),
+                                                      t_bias,
                                                       sk_float_isfinite(t_scale) ? t_scale : 0);
-        if (time_remap) {
-            auto  frame_rate = fFrameRate;
-            this->bindProperty<ScalarValue>(*time_remap,
-                    [time_mapper, frame_rate](const ScalarValue& t) {
-                        time_mapper->remapTime(t * frame_rate);
-                    });
-        }
+
         fCurrentAnimatorScope->push_back(std::move(time_mapper));
     }
 
