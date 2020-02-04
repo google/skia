b8ffaecc32d984d395be46d88699e630e2e6bd33
Herb Derby <herb@google.com>
2020-02-04 16:02:18 -0500

    Add a mutex to SkStrike to regulate multithreaded access
    This is not needed currently because the SkStrikeCache
    still uses the remove/replace method of multi-threading.
    This will let me see any overhead problems. The next CL
    will be a multi-threaded test.
    
    Change-Id: Ida8255cc86816fa21b4315a4bbccfbfab5ca475c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/268683
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- src/core/SkStrike.h
+++ src/core/SkStrike.h
@@ -40,28 +40,28 @@ public:
              const SkFontMetrics&);
 
     // Return a glyph.  Create it if it doesn't exist, and initialize with the prototype.
-    SkGlyph* glyphFromPrototype(const SkGlyphPrototype& p, void* image = nullptr);
+    SkGlyph* glyphFromPrototype(const SkGlyphPrototype& p, void* image = nullptr) SK_EXCLUDES(fMu);
 
     // Return a glyph or nullptr if it does not exits in the strike.
-    SkGlyph* glyphOrNull(SkPackedGlyphID id) const;
+    SkGlyph* glyphOrNull(SkPackedGlyphID id) const SK_EXCLUDES(fMu);
 
     // Lookup (or create if needed) the toGlyph using toID. If that glyph is not initialized with
     // an image, then use the information in from to initialize the width, height top, left,
     // format and image of the toGlyph. This is mainly used preserving the glyph if it was
     // created by a search of desperation.
-    SkGlyph* mergeGlyphAndImage(SkPackedGlyphID toID, const SkGlyph& from);
+    SkGlyph* mergeGlyphAndImage(SkPackedGlyphID toID, const SkGlyph& from) SK_EXCLUDES(fMu);
 
     // If the path has never been set, then add a path to glyph.
-    const SkPath* preparePath(SkGlyph* glyph, const SkPath* path);
+    const SkPath* preparePath(SkGlyph* glyph, const SkPath* path) SK_EXCLUDES(fMu);
 
     /** Return the number of glyphs currently cached. */
-    int countCachedGlyphs() const;
+    int countCachedGlyphs() const SK_EXCLUDES(fMu);
 
     /** If the advance axis intersects the glyph's path, append the positions scaled and offset
         to the array (if non-null), and set the count to the updated array length.
     */
     void findIntercepts(const SkScalar bounds[2], SkScalar scale, SkScalar xPos,
-                        SkGlyph* , SkScalar* array, int* count);
+                        SkGlyph* , SkScalar* array, int* count) SK_EXCLUDES(fMu);
 
 
     /** Return the vertical metrics for this strike.
@@ -77,38 +77,41 @@ public:
     const SkDescriptor& getDescriptor() const override;
 
     SkSpan<const SkGlyph*> metrics(SkSpan<const SkGlyphID> glyphIDs,
-                                   const SkGlyph* results[]);
+                                   const SkGlyph* results[]) SK_EXCLUDES(fMu);
 
     SkSpan<const SkGlyph*> preparePaths(SkSpan<const SkGlyphID> glyphIDs,
-                                        const SkGlyph* results[]);
+                                        const SkGlyph* results[]) SK_EXCLUDES(fMu);
 
     SkSpan<const SkGlyph*> prepareImages(SkSpan<const SkPackedGlyphID> glyphIDs,
-                                         const SkGlyph* results[]);
+                                         const SkGlyph* results[]) SK_EXCLUDES(fMu);
 
-    void prepareForDrawingMasksCPU(SkDrawableGlyphBuffer* drawables);
+    void prepareForDrawingMasksCPU(SkDrawableGlyphBuffer* drawables) SK_EXCLUDES(fMu);
 
-    void prepareForDrawingPathsCPU(SkDrawableGlyphBuffer* drawables);
+    void prepareForDrawingPathsCPU(SkDrawableGlyphBuffer* drawables) SK_EXCLUDES(fMu);
 
     void prepareForMaskDrawing(
-            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override;
+            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override SK_EXCLUDES(fMu);
 
     void prepareForSDFTDrawing(
-            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override;
+            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override SK_EXCLUDES(fMu);
 
     void prepareForPathDrawing(
-            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override;
+            SkDrawableGlyphBuffer* drawables, SkSourceGlyphBuffer* rejects) override SK_EXCLUDES(fMu);
 
     void onAboutToExitScope() override;
 
     /** Return the approx RAM usage for this cache. */
-    size_t getMemoryUsed() const { return fMemoryUsed; }
+    size_t getMemoryUsed() const SK_EXCLUDES(fMu) {
+        SkAutoMutexExclusive lock{fMu};
+        return fMemoryUsed;
+    }
 
-    void dump() const;
+    void dump() const SK_EXCLUDES(fMu);
 
     SkScalerContext* getScalerContext() const { return fScalerContext.get(); }
 
 #ifdef SK_DEBUG
-    void forceValidate() const;
+    void forceValidate() const SK_EXCLUDES(fMu);
     void validate() const;
 #else
     void validate() const {}
@@ -144,50 +147,54 @@ private:
         }
     };
 
-    SkGlyph* makeGlyph(SkPackedGlyphID);
+    SkGlyph* makeGlyph(SkPackedGlyphID) SK_REQUIRES(fMu);
 
     template <typename Fn>
-    void commonFilterLoop(SkDrawableGlyphBuffer* drawables, Fn&& fn);
+    void commonFilterLoop(SkDrawableGlyphBuffer* drawables, Fn&& fn) SK_REQUIRES(fMu);
 
     // Return a glyph. Create it if it doesn't exist, and initialize the glyph with metrics and
     // advances using a scaler.
-    SkGlyph* glyph(SkPackedGlyphID packedID);
+    SkGlyph* glyph(SkPackedGlyphID packedID) SK_REQUIRES(fMu);
 
-    const void* prepareImage(SkGlyph* glyph);
+    const void* prepareImage(SkGlyph* glyph) SK_REQUIRES(fMu);
 
     // If the path has never been set, then use the scaler context to add the glyph.
-    const SkPath* preparePath(SkGlyph*);
+    const SkPath* preparePath(SkGlyph*) SK_REQUIRES(fMu);
+
+    SkGlyph* internalGlyphOrNull(SkPackedGlyphID packedID) const SK_REQUIRES(fMu);
 
     enum PathDetail {
         kMetricsOnly,
         kMetricsAndPath
     };
+
     // internalPrepare will only be called with a mutex already held.
     SkSpan<const SkGlyph*> internalPrepare(
             SkSpan<const SkGlyphID> glyphIDs,
             PathDetail pathDetail,
-            const SkGlyph** results);
+            const SkGlyph** results) SK_REQUIRES(fMu);
 
     const SkAutoDescriptor                 fDesc;
     const std::unique_ptr<SkScalerContext> fScalerContext;
     const SkFontMetrics                    fFontMetrics;
+    const SkGlyphPositionRoundingSpec      fRoundingSpec;
+
+    mutable SkMutex fMu;
 
     // Map from a combined GlyphID and sub-pixel position to a SkGlyph*.
     // The actual glyph is stored in the fAlloc. This structure provides an
     // unchanging pointer as long as the strike is alive.
-    SkTHashTable<SkGlyph*, SkPackedGlyphID, GlyphMapHashTraits> fGlyphMap;
+    SkTHashTable<SkGlyph*, SkPackedGlyphID, GlyphMapHashTraits> fGlyphMap SK_GUARDED_BY(fMu);
 
     // so we don't grow our arrays a lot
     static constexpr size_t kMinGlyphCount = 8;
     static constexpr size_t kMinGlyphImageSize = 16 /* height */ * 8 /* width */;
     static constexpr size_t kMinAllocAmount = kMinGlyphImageSize * kMinGlyphCount;
 
-    SkArenaAlloc            fAlloc {kMinAllocAmount};
+    SkArenaAlloc            fAlloc SK_GUARDED_BY(fMu) {kMinAllocAmount};
 
     // Tracks (approx) how much ram is tied-up in this strike.
-    size_t                  fMemoryUsed;
-
-    const SkGlyphPositionRoundingSpec fRoundingSpec;
+    size_t                  fMemoryUsed SK_GUARDED_BY(fMu) {sizeof(SkStrike)};
 };
 
 #endif  // SkStrike_DEFINED
