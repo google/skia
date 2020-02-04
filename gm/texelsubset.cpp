cb36f89f0bb9d44a11551252762a9c9ffad78f77
Brian Salomon <bsalomon@google.com>
2020-02-04 13:09:47 -0500

    start
    Change-Id: Ie2d299106120b5c6cc40365e8f300cc426dd5489

--- gm/texelsubset.cpp
+++ gm/texelsubset.cpp
@@ -61,13 +61,9 @@ protected:
     }
 
     SkISize onISize() override {
-        int n = GrSamplerState::kWrapModeCount;
-        if (fFilter != GrSamplerState::Filter::kNearest) {
-            // Account for not supporting kMirror or kMirrorRepeat with filtering.
-            n -= 2;
-        }
-        int w = kTestPad + 2 * n * (kImageSize.width()  + 2 * kDrawPad + kTestPad);
-        int h = kTestPad + 2 * n * (kImageSize.height() + 2 * kDrawPad + kTestPad);
+        static constexpr int n = GrSamplerState::kWrapModeCount;
+        int w = kTestPad + 2*n*(kImageSize.width()  + 2 * kDrawPad + kTestPad);
+        int h = kTestPad + 2*n*(kImageSize.height() + 2 * kDrawPad + kTestPad);
         return {w, h};
     }
 
@@ -130,20 +126,8 @@ protected:
             for (int mx = 0; mx < GrSamplerState::kWrapModeCount; ++mx) {
                 SkScalar x = kDrawPad + kTestPad;
                 auto wmx = static_cast<GrSamplerState::WrapMode>(mx);
-                if (fFilter != GrSamplerState::Filter::kNearest &&
-                    (wmx == GrSamplerState::WrapMode::kRepeat ||
-                     wmx == GrSamplerState::WrapMode::kMirrorRepeat)) {
-                    // [Mirror] Repeat mode doesn't produce correct results with bilerp
-                    // filtering
-                    continue;
-                }
                 for (int my = 0; my < GrSamplerState::kWrapModeCount; ++my) {
                     auto wmy = static_cast<GrSamplerState::WrapMode>(my);
-                    if (fFilter != GrSamplerState::Filter::kNearest &&
-                        (wmy == GrSamplerState::WrapMode::kRepeat ||
-                         wmy == GrSamplerState::WrapMode::kMirrorRepeat)) {
-                        continue;
-                    }
                     GrSamplerState sampler(wmx, wmy, fFilter);
                     const auto& caps = *context->priv().caps();
                     auto fp1 = GrTextureEffect::MakeTexelSubset(view.proxyRef(),
