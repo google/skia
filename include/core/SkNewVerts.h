f89c120bd4f72f958abdbcfe888d8a8b7ff7d205
Mike Reed <reed@google.com>
2020-02-04 15:06:03 -0500

    experiment with per-vertex data in vertices
    Change-Id: Id03031e28c3b601a49d915e874beadcc8da18b4f

---
+++ include/core/SkNewVerts.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkVertices_DEFINED
+#define SkVertices_DEFINED
+
+#include "include/core/SkData.h"
+#include "include/core/SkPoint.h"
+#include "include/core/SkRect.h"
+#include "include/core/SkRefCnt.h"
+
+/**
+ * An immutable set of vertex data that can be used with SkCanvas::drawVertices.
+ */
+class SK_API SkNewVerts : public SkNVRefCnt<SkNewVerts> {
+public:
+    enum VertexMode {
+        kTriangles_VertexMode,
+        kTriangleStrip_VertexMode,
+        kTriangleFan_VertexMode,
+
+        kLast_VertexMode = kTriangleFan_VertexMode,
+    };
+
+    /**
+     *  Create a vertices by copying the specified arrays. texs, colors, boneIndices, and
+     *  boneWeights may be nullptr, and indices is ignored if indexCount == 0.
+     *
+     *  boneIndices and boneWeights must either both be nullptr or both point to valid data.
+     *  If specified, they must both contain 'vertexCount' entries.
+     */
+    static sk_sp<SkNewVerts> MakeCopy(VertexMode mode, int vertexCount,
+                                      const SkPoint vertices[],
+                                      int perVertexDataCount,
+                                      SkScalar vertData[],  // [vertexCount * perVertexDataCount]
+                                      int indexCount,
+                                      const uint16_t indices[]);
+
+    struct Sizes;
+
+    class Builder {
+    public:
+        Builder(VertexMode mode, int vertexCount, int perVertDataCount, int indexCount);
+
+        bool isValid() const { return fVertices != nullptr; }
+
+        // if the builder is invalid, these will return 0
+        int vertexCount() const;
+        int perVertexDataCount() const;
+        int indexCount() const;
+
+        SkPoint*  positions();
+        SkScalar* vertexData();
+        uint16_t* indices();        // returns null if there are no indices
+
+        // Detach the built vertices object. After the first call, this will always return null.
+        sk_sp<SkVertices> detach();
+
+    private:
+        Builder(VertexMode mode, int vertexCount, int indexCount, bool isVolatile, const Sizes&);
+
+        void init(VertexMode mode, int vertexCount, int indexCount, bool isVolatile, const Sizes&);
+
+        // holds a partially complete object. only completed in detach()
+        sk_sp<SkVertices> fVertices;
+        // Extra storage for intermediate vertices in the case where the client specifies indexed
+        // triangle fans. These get converted to indexed triangles when the Builder is finalized.
+        std::unique_ptr<uint8_t[]> fIntermediateFanIndices;
+
+        friend class SkVertices;
+    };
+
+    uint32_t uniqueID() const { return fUniqueID; }
+    VertexMode mode() const { return fMode; }
+    const SkRect& bounds() const { return fBounds; }
+
+    bool hasIndices() const { return SkToBool(this->indices()); }
+
+    int vertexCount() const { return fVertexCnt; }
+    int perVertexDataCount() const { return fVDataCnt; }
+    int indexCount() const { return fIndexCnt; }
+
+    const SkPoint* positions() const { return fPositions; }
+    const SkScalar* perVertexData() const { return fVData; }
+    const uint16_t* indices() const { return fIndices; }
+
+    // returns approximate byte size of the vertices object
+    size_t approximateSize() const;
+
+    /**
+     *  Recreate a vertices from a buffer previously created by calling encode().
+     *  Returns null if the data is corrupt or the length is incorrect for the contents.
+     */
+    static sk_sp<SkVertices> Decode(const void* buffer, size_t length);
+
+    /**
+     *  Pack the vertices object into a byte buffer. This can be used to recreate the vertices
+     *  by calling Decode() with the buffer.
+     */
+    sk_sp<SkData> encode() const;
+
+private:
+    SkVertices() {}
+
+    // these are needed since we've manually sized our allocation (see Builder::init)
+    friend class SkNVRefCnt<SkVertices>;
+    void operator delete(void* p);
+
+    static sk_sp<SkVertices> Alloc(int vCount, int iCount, uint32_t builderFlags,
+                                   size_t* arraySize);
+
+    // we store this first, to pair with the refcnt in our base-class, so we don't have an
+    // unnecessary pad between it and the (possibly 8-byte aligned) ptrs.
+    uint32_t fUniqueID;
+
+    // these point inside our allocation, so none of these can be "freed"
+    SkPoint*     fPositions;
+    SkPoint*     fTexs;
+    SkColor*     fColors;
+    BoneIndices* fBoneIndices;
+    BoneWeights* fBoneWeights;
+    uint16_t*    fIndices;
+
+    SkRect  fBounds;    // computed to be the union of the fPositions[]
+    int     fVertexCnt;
+    int     fIndexCnt;
+
+    bool fIsVolatile;
+
+    VertexMode fMode;
+    // below here is where the actual array data is stored.
+};
+
+#endif
