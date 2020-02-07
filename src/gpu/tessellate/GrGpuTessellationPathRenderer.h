f8635e965905b9a29443b46af387be5f9de34db0
Chris Dalton <csmartdalton@google.com>
2020-02-07 12:44:07 -0700

    Add a simple atlas to the GPU tessellation path renderer
    If a path is small and simple enough, we now try to atlas it. There
    is only one atlas and it caps at 2048x2048. Once it runs out of room,
    everything just draws direct. The atlas is rendered using the existing
    GrTessellatePathOp. It provides alpha8 coverage even for msaa render
    targets.
    
    Change-Id: I715da9ce7347b6f1ef8e28b3e13ab47f6eade1c7

--- src/gpu/tessellate/GrGpuTessellationPathRenderer.h
+++ src/gpu/tessellate/GrGpuTessellationPathRenderer.h
@@ -8,18 +8,38 @@
 #ifndef GrGpuTessellationPathRenderer_DEFINED
 #define GrGpuTessellationPathRenderer_DEFINED
 
+#include "src/gpu/GrDynamicAtlas.h"
+#include "src/gpu/GrOnFlushResourceProvider.h"
 #include "src/gpu/GrPathRenderer.h"
+#include <map>
 
 // This is the tie-in point for path rendering via GrTessellatePathOp.
-class GrGpuTessellationPathRenderer : public GrPathRenderer {
+class GrGpuTessellationPathRenderer : public GrPathRenderer, public GrOnFlushCallbackObject {
+public:
+    GrGpuTessellationPathRenderer(const GrCaps&);
     StencilSupport onGetStencilSupport(const GrShape& shape) const override {
         // TODO: Single-pass (e.g., convex) paths can have full support.
         return kStencilOnly_StencilSupport;
     }
-
     CanDrawPath onCanDrawPath(const CanDrawPathArgs&) const override;
     bool onDrawPath(const DrawPathArgs&) override;
     void onStencilPath(const StencilPathArgs&) override;
+    void preFlush(GrOnFlushResourceProvider*, const uint32_t* opsTaskIDs,
+                  int numOpsTaskIDs) override;
+
+private:
+    SkPath* getAtlasUberPath(SkPathFillType fillType, bool antialias) {
+        int idx = (int)antialias << 1;
+        idx |= (int)fillType & 1;
+        return &fAtlasUberPaths[idx];
+    }
+    // Allocates space in fAtlas if the path is small and simple enough, and if there is room.
+    bool tryAddPathToAtlas(const GrCaps&, const SkMatrix&, const SkPath&, GrAAType,
+                           SkIRect* devIBounds, SkIVector* devToAtlasOffset);
+    void renderAtlas(GrOnFlushResourceProvider*);
+
+    GrDynamicAtlas fAtlas;
+    SkPath fAtlasUberPaths[4];  // 2 fillTypes * 2 antialias modes.
 };
 
 #endif
