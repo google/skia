3c3afaeb0eefaa059eda809a92f717e245ab1236
Weston Tracey <westont@google.com>
2020-02-07 15:27:13 -0500

    [infra] Reland "Use ccache for linux default builds."
    This reverts commit 5e27550437628018e922d0ab81630ffa71bef62c.
    
    Reason for revert: Will be modified with fix.
    
    Original change's description:
    > Revert "Use ccache for linux default builds."
    >
    > This reverts commit 9f3eef796f631977c08fb1771f6507e0a8e55d99.
    >
    > Reason for revert: breaking https://chromium-swarm.appspot.com/tasklist?f=sk_name%3AHousekeeper-PerCommit-CheckGeneratedFiles
    >
    > Original change's description:
    > > Use ccache for linux default builds.
    > >
    > > Change-Id: I6bd2439c59f165d56f093185aba818d186b62761
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/268903
    > > Commit-Queue: Weston Tracey <westont@google.com>
    > > Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>
    >
    > TBR=benjaminwagner@google.com,westont@google.com
    >
    > Change-Id: I3c5a8f456d03c880a24294e61ae116bd5a1e3db8
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/269481
    > Reviewed-by: Leon Scroggins <scroggo@google.com>
    > Commit-Queue: Leon Scroggins <scroggo@google.com>
    
    TBR=benjaminwagner@google.com,scroggo@google.com,westont@google.com
    
    Change-Id: I6937e44421d8705ca80649bc7df474944d7206bb

--- infra/bots/recipe_modules/build/default.py
+++ infra/bots/recipe_modules/build/default.py
@@ -135,6 +135,19 @@ def compile_fn(api, checkout_root, out_dir):
     args['skia_compile_processors'] = 'true'
     args['skia_generate_workarounds'] = 'true'
 
+  # ccache + clang-tidy.sh chokes on the argument list.
+  if api.vars.is_linux and 'Tidy' not in extra_tokens:
+    args['cc_wrapper'] = '"%s"' % api.vars.slave_dir.join('ccache_linux', 'bin', 'ccache')
+    env['CCACHE_DIR'] = api.vars.cache_dir.join('ccache')
+    # Compilers are unpacked from cipd with bogus timestamps, only contribute
+    # compiler content to hashes. If Ninja ever uses absolute paths to changing
+    # directories we'll also need to set a CCACHE_BASEDIR.
+    env['CCACHE_COMPILERCHECK'] = 'content'
+    # As of 2020-02-07, the sum of each Debian9-Clang-x86
+    # non-flutter/android/chromebook build takes less than 75G cache space.
+    env['CCACHE_MAXSIZE'] = '75G'
+    env['CCACHE_MAXFILES'] = '0'
+
   if compiler == 'Clang' and api.vars.is_linux:
     cc  = clang_linux + '/bin/clang'
     cxx = clang_linux + '/bin/clang++'
