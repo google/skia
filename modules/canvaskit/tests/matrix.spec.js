187b11a2adbfa4fd7ab8faa92071812e17acc322
Nathaniel Nifong <nifong@google.com>
2020-02-17 11:08:06 -0500

    Expose 4x4 matrices on canvas in a way similar to SimpleMatrix, add example.
    Bug: skia:9866
    Change-Id: I718455743e482e4f60a462027b629dc19b1dbad3

--- modules/canvaskit/tests/matrix.spec.js
+++ modules/canvaskit/tests/matrix.spec.js
@@ -1,1 +1,24 @@
-//TODO test mapPoints and such
+describe('CanvasKit\'s Matrix Helpers', function() {
+
+  let expectArrayCloseTo = function(a, b, precision) {
+    expect(a.length).toEqual(b.length);
+    for (let i=0; i<a.length; i++) {
+      expect(a[i]).toBeCloseTo(b[i], precision);
+    }
+  };
+
+  it('3x3 matrix times its inverse is identity', function(done) {
+    Promise.all([LoadCanvasKit]).then(catchException(done, () => {
+      let a = [
+        99, 88, -77,
+        -66, 55, 44,
+        33, 22, 11,
+      ];
+      expectArrayCloseTo([0,0], [0.0000001, -0.0000001], 3);
+      expectArrayCloseTo(
+        CanvasKit.SkMatrix.multiply(a, CanvasKit.SkMatrix.invert(a)),
+        CanvasKit.SkMatrix.identity(), 9);
+      done();
+    }));
+  });
+});
