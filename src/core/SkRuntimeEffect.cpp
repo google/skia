74ffb47f6e901e1b574429c78ed4f16192765c20
Mike Reed <reed@google.com>
2020-02-18 16:51:47 -0500

    allow client to pass null if there are no uniforms
    Explicitly check for empty data's in readbuffer
    
    Change-Id: I990ed71c847af472252b6efc3abde83fc741abfa

--- src/core/SkRuntimeEffect.cpp
+++ src/core/SkRuntimeEffect.cpp
@@ -296,7 +296,10 @@ SkRuntimeEffect::ByteCodeResult SkRuntimeEffect::toByteCode(const void* inputs) 
 sk_sp<SkShader> SkRuntimeEffect::makeShader(sk_sp<SkData> inputs,
                                             sk_sp<SkShader> children[], size_t childCount,
                                             const SkMatrix* localMatrix, bool isOpaque) {
-    return inputs && inputs->size() == this->inputSize() && childCount == fChildren.size()
+    if (!inputs) {
+        inputs = SkData::MakeEmpty();
+    }
+    return inputs->size() == this->inputSize() && childCount == fChildren.size()
         ? sk_sp<SkShader>(new SkRTShader(sk_ref_sp(this), std::move(inputs), localMatrix,
                                          children, childCount, isOpaque))
         : nullptr;
@@ -308,7 +311,10 @@ sk_sp<SkColorFilter> SkRuntimeEffect::makeColorFilter(sk_sp<SkData> inputs,
     extern sk_sp<SkColorFilter> SkMakeRuntimeColorFilter(sk_sp<SkRuntimeEffect>, sk_sp<SkData>,
                                                          sk_sp<SkColorFilter>[], size_t);
 
-    return inputs && inputs->size() == this->inputSize() && childCount == fChildren.size()
+    if (!inputs) {
+        inputs = SkData::MakeEmpty();
+    }
+    return inputs->size() == this->inputSize() && childCount == fChildren.size()
         ? SkMakeRuntimeColorFilter(sk_ref_sp(this), std::move(inputs), children, childCount)
         : nullptr;
 }
