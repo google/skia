d1ec0c1a70b18d1a923a19d08c77961a40a07199
Dominic Mazzoni <dmazzoni@chromium.org>
2020-02-19 10:59:29 -0800

    Re-land: Support adding attributes to PDF document structure nodes.
    Originally landed: https://skia-review.googlesource.com/c/skia/+/268878
    Reverted: https://skia-review.googlesource.com/c/skia/+/271858
    
    The issue was with compilation when PDF support is disabled. See
    the diff between patchsets 1 and 2.
    
    This is an important part of writing a tagged PDF. Many of the nodes
    in the document structure tree need additional attributes, just like
    in HTML.
    
    This change aims to add support for a few useful attributes, not to
    be comprehensive.
    
    Bug: chromium:1039816
    Change-Id: I15f8b6c41d4fdaa4b6e21775ab6d26ec57eb0f5d

--- src/pdf/SkDocument_PDF_None.cpp
+++ src/pdf/SkDocument_PDF_None.cpp
@@ -8,8 +8,14 @@
 #include "include/core/SkCanvas.h"
 #include "include/docs/SkPDFDocument.h"
 
+class SkPDFArray {};
+
 sk_sp<SkDocument> SkPDF::MakeDocument(SkWStream*, const SkPDF::Metadata&) { return nullptr; }
 
 void SkPDF::SetNodeId(SkCanvas* c, int n) {
     c->drawAnnotation({0, 0, 0, 0}, "PDF_Node_Key", SkData::MakeWithCopy(&n, sizeof(n)).get());
 }
+
+SkPDF::AttributeList::AttributeList() = default;
+
+SkPDF::AttributeList::~AttributeList() = default;
