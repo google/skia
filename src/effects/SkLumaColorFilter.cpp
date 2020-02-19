5bdef05bd3929c7debafc3ad85d77ae79ba9a176
Mike Reed <reed@google.com>
2020-02-19 09:35:35 -0500

    Use runtimeeffets for LumaColorFilter
    Snags:
    
    1. color.rgb = 0;
    2. lum = clamp(lum, 0.0, 1.0);
    3. effect->makeColorFilter(nullptr)
    
    Change-Id: Ie2f370a14e773d5688baeba1c90105fbf029fcce

--- src/effects/SkLumaColorFilter.cpp
+++ src/effects/SkLumaColorFilter.cpp
@@ -5,41 +5,21 @@
  * found in the LICENSE file.
  */
 
+#include "include/core/SkData.h"
 #include "include/core/SkString.h"
 #include "include/effects/SkLumaColorFilter.h"
-#include "include/private/SkColorData.h"
-#include "src/core/SkEffectPriv.h"
-#include "src/core/SkRasterPipeline.h"
+#include "include/effects/SkRuntimeEffect.h"
 
-#if SK_SUPPORT_GPU
-#include "include/gpu/GrContext.h"
-#include "src/gpu/effects/generated/GrLumaColorFilterEffect.h"
-#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
-#include "src/gpu/glsl/GrGLSLFragmentShaderBuilder.h"
-#endif
-
-bool SkLumaColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const {
-    rec.fPipeline->append(SkRasterPipeline::bt709_luminance_or_luma_to_alpha);
-    rec.fPipeline->append(SkRasterPipeline::clamp_0);
-    rec.fPipeline->append(SkRasterPipeline::clamp_1);
-    return true;
-}
+const char code[] = R"(
+    void main(inout half4 color) {
+        const half3 SK_ITU_BT709_LUM_COEFF = half3(0.2126, 0.7152, 0.0722);
+        half luma = dot(SK_ITU_BT709_LUM_COEFF, color.rgb);
+//      luma = saturate(luma);
+        color = half4(0, 0, 0, luma);
+    }
+)";
 
 sk_sp<SkColorFilter> SkLumaColorFilter::Make() {
-    return sk_sp<SkColorFilter>(new SkLumaColorFilter);
+    auto [effect, err] = SkRuntimeEffect::Make(SkString(code));
+    return effect ? effect->makeColorFilter(nullptr) : nullptr;
 }
-
-SkLumaColorFilter::SkLumaColorFilter() : INHERITED() {}
-
-sk_sp<SkFlattenable> SkLumaColorFilter::CreateProc(SkReadBuffer&) {
-    return Make();
-}
-
-void SkLumaColorFilter::flatten(SkWriteBuffer&) const {}
-
-#if SK_SUPPORT_GPU
-std::unique_ptr<GrFragmentProcessor> SkLumaColorFilter::asFragmentProcessor(
-        GrRecordingContext*, const GrColorInfo&) const {
-    return GrLumaColorFilterEffect::Make();
-}
-#endif
