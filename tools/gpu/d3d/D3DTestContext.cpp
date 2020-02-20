02d1754037e2b8561a2e4da2d114ad16045cfcbf
Jim Van Verth <jvanverth@google.com>
2020-02-20 17:55:19 -0500

    Create D3D device and queue
    Bug: skia:9935
    Change-Id: Ib6548f413ca3a8befb553d2d47354b400c9162b9

--- tools/gpu/d3d/D3DTestContext.cpp
+++ tools/gpu/d3d/D3DTestContext.cpp
@@ -10,10 +10,9 @@
 #ifdef SK_DIRECT3D
 
 #include "include/gpu/GrContext.h"
+#include "tools/gpu/d3d/D3DTestUtils.h"
 
 namespace {
-
-// TODO: Implement D3DFenceSync
 
 class D3DTestContextImpl : public sk_gpu_test::D3DTestContext {
 public:
@@ -23,8 +22,14 @@ public:
         if (sharedContext) {
             // take from the given context
             ownsContext = false;
-        } else {
+            backendContext = sharedContext->getD3DBackendContext();
+        }
+        else {
             // create our own
+            if (!sk_gpu_test::CreateD3DBackendContext(&backendContext)) {
+                return nullptr;
+            }
+
             ownsContext = true;
         }
         return new D3DTestContextImpl(backendContext, ownsContext);
@@ -54,7 +59,6 @@ protected:
 private:
     D3DTestContextImpl(const GrD3DBackendContext& backendContext, bool ownsContext)
             : D3DTestContext(backendContext, ownsContext) {
-// TODO       fFenceSync.reset(new D3DFenceSync(backendContext));
     }
 
     void onPlatformMakeNotCurrent() const override {}
