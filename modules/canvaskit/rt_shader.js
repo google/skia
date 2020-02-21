6801b350bcfaeaf2c3d93b71e671f1418359cd08
Nathaniel Nifong <nifong@google.com>
2020-02-21 10:42:32 -0500

    Add 3D example with light and bump shader.
    Change-Id: I53485c5f47e0fef120c8a1bd6d0620fddfb3cde9

--- modules/canvaskit/rt_shader.js
+++ modules/canvaskit/rt_shader.js
@@ -10,3 +10,14 @@ CanvasKit._extraInitializations.push(function() {
     return this._makeShader(fptr, floats.length * 4, !!isOpaque, matrix);
   }
 });
+CanvasKit._extraInitializations.push(function() {
+  // children is an array of
+  CanvasKit.SkRuntimeEffect.prototype.makeShaderWithChildren = function(floats, children, isOpaque, matrix) {
+    var fptr = copy1dArray(floats, CanvasKit.HEAPF32);
+    var chilptr = copy1dArray(children, CanvasKit.HEAPU32);
+    if (!matrix) {
+      return this._makeShaderWithChildren(fptr, floats.length*4, chilptr, children.length*4, !!isOpaque);
+    }
+    return this._makeShaderWithChildren(fptr, floats.length*4, chilptr, children.length*4, !!isOpaque, matrix);
+  }
+});
