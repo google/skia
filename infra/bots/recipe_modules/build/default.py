0afe28727b84a18908e4445c13d97ffc77da51eb
Ben Wagner <benjaminwagner@google.com>
2020-02-21 18:21:26 -0500

    [chrome/m80] Update XCode
    Chromium now requires the 10.15 SDK
    (https://bugs.chromium.org/p/chromium/issues/detail?id=1023913), which
    breaks the CommandBuffer bot because it's trying to compile with XCode
    10.3, which provides the 10.14 SDK.
    
    What's worse, due to reasons I don't fully understand, after failing to
    compile CommandBuffer due to the dependence on the 10.15 SDK, the ninja
    out dir is poisoned for future compiles that don't depend on the 10.15
    SDK.
    
    Also includes a code snippet from
    https://skia-review.googlesource.com/c/skia/+/264429 to find the xSAN
    dylibs.
    
    Change-Id: I3cd7661a5ea984ea6899e7a5e8cf878581538d9e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/272736
    Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>
    (cherry picked from commit 998066127e0d236d77131278bb7ac03644201fd0)

--- infra/bots/recipe_modules/build/default.py
+++ infra/bots/recipe_modules/build/default.py
@@ -6,18 +6,6 @@
 from . import util
 
 
-# XCode build is listed in parentheses after the version at
-# https://developer.apple.com/news/releases/, or on Wikipedia here:
-# https://en.wikipedia.org/wiki/Xcode#Version_comparison_table
-# Use lowercase letters.
-# When updating XCODE_BUILD_VERSION, you will also need to update
-# XCODE_CLANG_VERSION.
-XCODE_BUILD_VERSION = '10g8'
-# Wikipedia lists the Clang version here:
-# https://en.wikipedia.org/wiki/Xcode#Toolchain_versions
-XCODE_CLANG_VERSION = '10.0.1'
-
-
 def build_command_buffer(api, chrome_dir, skia_dir, out):
   api.run(api.python, 'build command_buffer',
       script=skia_dir.join('tools', 'build_command_buffer.py'),
@@ -93,6 +81,11 @@ def compile_fn(api, checkout_root, out_dir):
   env = {}
 
   if os == 'Mac':
+    # XCode build is listed in parentheses after the version at
+    # https://developer.apple.com/news/releases/, or on Wikipedia here:
+    # https://en.wikipedia.org/wiki/Xcode#Version_comparison_table
+    # Use lowercase letters.
+    XCODE_BUILD_VERSION = '11c29'
     extra_cflags.append(
         '-DDUMMY_xcode_build_version=%s' % XCODE_BUILD_VERSION)
     mac_toolchain_cmd = api.vars.slave_dir.join(
@@ -323,13 +316,18 @@ def copy_build_products(api, src, dst):
         util.DEFAULT_BUILD_PRODUCTS)
 
   if os == 'Mac' and any('SAN' in t for t in extra_tokens):
-    # Hardcoding this path because it should only change when we upgrade to a
-    # new Xcode.
-    lib_dir = api.vars.cache_dir.join(
-        'Xcode.app', 'Contents', 'Developer', 'Toolchains',
-        'XcodeDefault.xctoolchain', 'usr', 'lib', 'clang', XCODE_CLANG_VERSION,
-        'lib', 'darwin')
-    dylibs = api.file.glob_paths('find xSAN dylibs', lib_dir,
+    # The XSAN dylibs are in
+    # Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib
+    # /clang/11.0.0/lib/darwin, where 11.0.0 could change in future versions.
+    xcode_clang_ver_dirs = api.file.listdir(
+        'find XCode Clang version',
+        api.vars.cache_dir.join(
+            'Xcode.app', 'Contents', 'Developer', 'Toolchains',
+            'XcodeDefault.xctoolchain', 'usr', 'lib', 'clang'),
+        test_data=['11.0.0'])
+    assert len(xcode_clang_ver_dirs) == 1
+    dylib_dir = xcode_clang_ver_dirs[0].join('lib', 'darwin')
+    dylibs = api.file.glob_paths('find xSAN dylibs', dylib_dir,
                                  'libclang_rt.*san_osx_dynamic.dylib',
                                  test_data=[
                                      'libclang_rt.asan_osx_dynamic.dylib',
