1117113e5b6db47019154d25b52557063f9d0824
Julia Lavrova <jlavrova@google.com>
2020-02-25 23:00:36 +0000

    Revert "remove private copy of SkM44.h"
    This reverts commit f8ef81b35e8f91360d034a5283052071c07f7d22.
    
    Reason for revert: Build broken
    
    Original change's description:
    > remove private copy of SkM44.h
    > 
    > Change-Id: Ifd47b929bafd40d190722366a0c1e2b856220ab3
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/272836
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: Mike Reed <reed@google.com>
    
    TBR=fmalita@chromium.org,reed@google.com
    
    Change-Id: I0da4ad43d5eaf5813ec489bc9117d4b8fc2b930e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

---
+++ include/private/SkM44.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkM44_private_DEFINED
+#define SkM44_private_DEFINED
+
+#include "include/core/SkM44.h"
+
+#endif
