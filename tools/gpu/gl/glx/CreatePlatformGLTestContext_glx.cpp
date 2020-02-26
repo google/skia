9e202263143952337a49b2be869585ccbf21358e
Greg Daniel <egdaniel@google.com>
2020-02-26 01:05:32 +0000

    Revert "Update nanobench and skpbench to use flush API for gpu syncing."
    This reverts commit 02497d40161fc75c46fa47c28e6fef30310147b4.
    
    Reason for revert: trying to test a change from before this CL, not actually reverting (as of now)
    
    Original change's description:
    > Update nanobench and skpbench to use flush API for gpu syncing.
    > 
    > This also allows us to remove all the one off Fence code that we
    > implemented in all the backend TestContexts
    > 
    > Change-Id: I9ff7ba4690cf3f19a180f51fc510991a112bb62c
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/272456
    > Reviewed-by: Chris Dalton <csmartdalton@google.com>
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    
    TBR=egdaniel@google.com,csmartdalton@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I00be0d2635d8773b890db82b0a14d9972e02d97b

--- tools/gpu/gl/glx/CreatePlatformGLTestContext_glx.cpp
+++ tools/gpu/gl/glx/CreatePlatformGLTestContext_glx.cpp
@@ -64,6 +64,7 @@ private:
     void onPlatformMakeNotCurrent() const override;
     void onPlatformMakeCurrent() const override;
     std::function<void()> onPlatformGetAutoContextRestore() const override;
+    void onPlatformSwapBuffers() const override;
     GrGLFuncPtr onPlatformGetProcAddress(const char*) const override;
 
     GLXContext fContext;
@@ -365,6 +366,10 @@ std::function<void()> GLXGLTestContext::onPlatformGetAutoContextRestore() const 
         return nullptr;
     }
     return context_restorer();
+}
+
+void GLXGLTestContext::onPlatformSwapBuffers() const {
+    glXSwapBuffers(fDisplay, fGlxPixmap);
 }
 
 GrGLFuncPtr GLXGLTestContext::onPlatformGetProcAddress(const char* procName) const {
