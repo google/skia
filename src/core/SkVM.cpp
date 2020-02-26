e4661d9e302f08b3f38d6df274f484da06be3d5b
Mike Klein <mtklein@google.com>
2020-02-26 15:11:04 -0600

    turn on mem2reg, set mcpu
    I've also added in a disabled path to do quasi -O1 / -Os
    passes, but mem2reg is really all we need, and I think we
    can eliminate the need for even that.
    
    Codegen looks great now, e.g.
    
    (lldb) dis -s fJITEntry -c 40
        0x10a48c000: movabsq $0x10a48d000, %rax        ; imm = 0x10A48D000
        0x10a48c00a: vbroadcastss (%rax), %zmm0
        0x10a48c010: cmpq   $0xf, %rdi
        0x10a48c014: jbe    0x10a48c04d
        0x10a48c016: nopw   %cs:(%rax,%rax)
        0x10a48c020: vmovups %zmm0, (%rsi)
        0x10a48c026: addq   $-0x10, %rdi
        0x10a48c02a: addq   $0x40, %rsi
        0x10a48c02e: cmpq   $0xf, %rdi
        0x10a48c032: ja     0x10a48c020
        0x10a48c034: jmp    0x10a48c04d
        0x10a48c036: nopw   %cs:(%rax,%rax)
        0x10a48c040: movl   $0x2a, (%rsi)
        0x10a48c046: decq   %rdi
        0x10a48c049: addq   $0x4, %rsi
        0x10a48c04d: testq  %rdi, %rdi
        0x10a48c050: jne    0x10a48c040
        0x10a48c052: vzeroupper
        0x10a48c055: retq
    
    Change-Id: Ief5d12548d5b1a683060b2b5d207022d673fe761

--- src/core/SkVM.cpp
+++ src/core/SkVM.cpp
@@ -20,8 +20,11 @@
     #include <llvm/Bitcode/BitcodeWriter.h>
     #include <llvm/ExecutionEngine/ExecutionEngine.h>
     #include <llvm/IR/IRBuilder.h>
+    #include <llvm/IR/LegacyPassManager.h>
     #include <llvm/IR/Verifier.h>
     #include <llvm/Support/TargetSelect.h>
+    #include <llvm/Transforms/IPO/PassManagerBuilder.h>
+    #include <llvm/Transforms/Utils.h>
 #endif
 
 bool gSkVMJITViaDylib{false};
@@ -1901,7 +1904,17 @@ namespace skvm {
         auto mod = std::make_unique<llvm::Module>("", ctx);
         // All the scary bare pointers from here on are owned by ctx or mod, I think.
 
-        const int K = 8;   // Primary vector width.
+        const char* mcpu = "x86-64";
+        int K = 4;
+        if (true && SkCpu::Supports(SkCpu::HSW)) {
+            mcpu = "haswell";
+            K    = 8;
+        }
+        if (true && SkCpu::Supports(SkCpu::SKX)) {
+            mcpu = "skylake-avx512";
+            K    = 16;
+        }
+
         llvm::Type        *ptr = llvm::Type::getInt8Ty(ctx)->getPointerTo();
       //llvm::Type        *f32 = llvm::Type::getFloatTy(ctx);
         llvm::IntegerType *i32 = llvm::Type::getInt32Ty(ctx),
@@ -2030,6 +2043,20 @@ namespace skvm {
 
         SkASSERT(false == llvm::verifyModule(*mod));
 
+        llvm::legacy::FunctionPassManager fpm(mod.get());
+    #if 0
+        llvm::PassManagerBuilder pmb;
+        pmb. OptLevel = 1;
+        pmb.SizeLevel = 1;
+        // TargetMachine::adjustPassManager(pmb)
+        pmb.populateFunctionPassManager(fpm);
+        while (fpm.run(*fn));
+    #else
+        fpm.add(llvm::createPromoteMemoryToRegisterPass());
+        fpm.run(*fn);
+        SkASSERT(!fpm.run(*fn));
+    #endif
+
         if (false) {
             SkString path = SkStringPrintf("/tmp/%s.bc", debug_name);
             std::error_code err;
@@ -2045,7 +2072,7 @@ namespace skvm {
 
         fEE = llvm::EngineBuilder(std::move(mod))
                     .setEngineKind(llvm::EngineKind::JIT)
-                    .setOptLevel(llvm::CodeGenOpt::Less)
+                    .setMCPU(mcpu)
                     .create();
         if (fEE) {
             fJITEntry = (void*)fEE->getFunctionAddress(debug_name);
