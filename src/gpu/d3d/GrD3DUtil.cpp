930ce7ac14d8ed7188449e6f7b62c56d1aafdf90
Jim Van Verth <jvanverth@google.com>
2020-02-27 15:59:23 -0500

    Add GrD3DUtil.
    Trying to stage the texture format CLs in a little at a time.
    
    Bug: skia:9935
    Change-Id: I0fc6fe09fdb55d6c24b53af7edbcc8e8fc857ed7

---
+++ src/gpu/d3d/GrD3DUtil.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/d3d/GrD3DUtil.h"
+
+#include "src/gpu/GrContextPriv.h"
+#include "src/gpu/GrDataUtils.h"
+#include "src/gpu/d3d/GrD3DGpu.h"
+#include "src/sksl/SkSLCompiler.h"
+
+bool GrDxgiFormatIsCompressed(DXGI_FORMAT format) {
+    switch (format) {
+        case DXGI_FORMAT_BC1_UNORM:
+            return true;
+        default:
+            return false;
+    }
+    SkUNREACHABLE;
+}
+
+SkImage::CompressionType GrDxgiFormatToCompressionType(DXGI_FORMAT format) {
+    switch (format) {
+        case DXGI_FORMAT_BC1_UNORM:    return SkImage::CompressionType::kBC1_RGBA8_UNORM;
+        default:                       return SkImage::CompressionType::kNone;
+    }
+    SkUNREACHABLE;
+}
