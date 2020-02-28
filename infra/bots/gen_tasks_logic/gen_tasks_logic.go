b99f5a2ada6d659b4f2589752ee00a3e9512474f
Ben Wagner <benjaminwagner@google.com>
2020-02-28 16:12:05 -0500

    [infra] Upgrade iPhone7, iPhone8, and iPadPro to iOS 13.3.1.
    Change-Id: I2d10bf0b9b7ac3833c60b7c6dca2d05ab7e99cc4

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -571,7 +571,7 @@ func (b *builder) defaultSwarmDimensions(parts map[string]string) []string {
 			"Win2019":  DEFAULT_OS_WIN,
 			"Win7":     "Windows-7-SP1",
 			"Win8":     "Windows-8.1-SP0",
-			"iOS":      "iOS-11.4.1",
+			"iOS":      "iOS-13.3.1",
 		}[os]
 		if !ok {
 			glog.Fatalf("Entry %q not found in OS mapping.", os)
@@ -587,6 +587,10 @@ func (b *builder) defaultSwarmDimensions(parts map[string]string) []string {
 		if parts["model"] == "LenovoYogaC630" {
 			// This is currently a unique snowflake.
 			d["os"] = "Windows-10"
+		}
+		if parts["model"] == "iPhone6" {
+			// Not ready to upgrade iPhone6 yet.
+			d["os"] = "iOS-11.4.1"
 		}
 	} else {
 		d["os"] = DEFAULT_OS_DEBIAN
@@ -1112,9 +1116,11 @@ func (b *builder) maybeAddIosDevImage(name string, t *specs.TaskSpec) {
 		if m := iosRegex.FindStringSubmatch(dim); len(m) >= 2 {
 			var asset string
 			switch m[1] {
-			// Other patch versions for 11.4 can be added here.
+			// Other patch versions can be added to the same case.
 			case "11.4.1":
 				asset = "ios-dev-image-11.4"
+			case "13.3.1":
+				asset = "ios-dev-image-13.3"
 			default:
 				glog.Fatalf("Unable to determine correct ios-dev-image asset for %s. If %s is a new iOS release, you must add a CIPD package containing the corresponding iOS dev image; see ios-dev-image-11.4 for an example.", name, m[1])
 			}
