5a109c7c9b287695cdb6b1ddc49a401de023b3f5
Mike Reed <reed@google.com>
2020-02-29 13:52:39 -0500

    Do we still need bones on vertices for canvas
    Bug: skia:9984
    Change-Id: I11669df7a8ebaf4b1af3f6908bfd1bafaa74d0b6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/274280
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- gm/skinning.cpp
+++ gm/skinning.cpp
@@ -16,6 +16,8 @@
 #include "include/core/SkString.h"
 #include "include/core/SkVertices.h"
 
+#ifdef SK_SUPPORT_VERTICES_BONES
+
 #include <stdint.h>
 
 using namespace skiagm;
@@ -209,3 +211,5 @@ DEF_GM(return new SkinningGM(true, true);)
 DEF_GM(return new SkinningGM(false, true);)
 DEF_GM(return new SkinningGM(true, false);)
 DEF_GM(return new SkinningGM(false, false);)
+
+#endif
