de72d1eb2f83e365fc1f494df8862c2dbb0b1c09
Eric Boren <borenet@google.com>
2020-03-02 14:46:09 -0500

    [recipes] Compute nanobench flags in gen_tasks
    Change-Id: Ia37e0b9cd2f6b6c6e9a728741bec642cdba9f7b1

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -9,10 +9,12 @@ package gen_tasks_logic
 */
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"regexp"
@@ -1519,7 +1521,40 @@ func (b *builder) perf(name string, parts map[string]string, compileTaskName str
 		recipe = "perf_skottiewasm_lottieweb"
 		isolate = "lottie_web.isolate"
 	}
-	task := b.kitchenTask(name, recipe, isolate, "", b.swarmDimensions(parts), EXTRA_PROPS, OUTPUT_PERF)
+	doUpload := strings.Contains(name, "Release") && b.doUpload(name)
+	extraProps := map[string]string{}
+	for k, v := range EXTRA_PROPS {
+		extraProps[k] = v
+	}
+	if recipe == "perf" {
+		nanoFlagsScript := filepath.Join(CheckoutRoot(), "infra", "bots", "recipe_modules", "vars", "resources", "nanobench_flags.py")
+		args := []string{
+			nanoFlagsScript,
+			"--bot", name,
+			"--parts", marshalJson(parts),
+			"--revision", specs.PLACEHOLDER_REVISION,
+			"--issue", specs.PLACEHOLDER_ISSUE,
+			"--patchset", specs.PLACEHOLDER_PATCHSET,
+			"--patch_storage", specs.PLACEHOLDER_PATCH_STORAGE,
+		}
+		if doUpload {
+			args = append(args, "--do_upload")
+		}
+		out, err := exec.Command("python", args...).CombinedOutput()
+		if err != nil {
+			glog.Fatal(err)
+		}
+		var res struct {
+			NanoFlags []string          `json:"nanobench_flags"`
+			NanoProps map[string]string `json:"nanobench_properties"`
+		}
+		if err := json.NewDecoder(bytes.NewBuffer(out)).Decode(&res); err != nil {
+			glog.Fatal(err)
+		}
+		extraProps["nanobench_flags"] = marshalJson(res.NanoFlags)
+		extraProps["nanobench_properties"] = marshalJson(res.NanoProps)
+	}
+	task := b.kitchenTask(name, recipe, isolate, "", b.swarmDimensions(parts), extraProps, OUTPUT_PERF)
 	task.CipdPackages = append(task.CipdPackages, pkgs...)
 	if !strings.Contains(name, "LottieWeb") {
 		// Perf.+LottieWeb doesn't require anything in Skia to be compiled.
@@ -1562,7 +1597,7 @@ func (b *builder) perf(name string, parts map[string]string, compileTaskName str
 	b.MustAddTask(name, task)
 
 	// Upload results if necessary.
-	if strings.Contains(name, "Release") && b.doUpload(name) {
+	if doUpload {
 		uploadName := fmt.Sprintf("%s%s%s", PREFIX_UPLOAD, b.jobNameSchema.Sep, name)
 		extraProps := map[string]string{
 			"gs_bucket": b.cfg.GsBucketNano,
