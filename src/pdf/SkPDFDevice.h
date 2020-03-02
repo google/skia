077e106111949099c335f788d9249d55d26a6db1
Mike Reed <reed@google.com>
2020-03-02 14:57:09 -0500

    remove bones from SkVertices
    Bug: skia:9984
    Change-Id: I7c4f33ee82087eb1b0a60243183f0c68e310f10d

--- src/pdf/SkPDFDevice.h
+++ src/pdf/SkPDFDevice.h
@@ -88,8 +88,7 @@ public:
                        const SkPaint&,
                        SkCanvas::SrcRectConstraint) override;
     void drawGlyphRunList(const SkGlyphRunList& glyphRunList) override;
-    void drawVertices(const SkVertices*, const SkVertices::Bone bones[], int boneCount, SkBlendMode,
-                      const SkPaint&) override;
+    void drawVertices(const SkVertices*, SkBlendMode, const SkPaint&) override;
     void drawDevice(SkBaseDevice*, int x, int y,
                     const SkPaint&) override;
 
