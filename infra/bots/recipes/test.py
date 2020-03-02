7e1078df70c4a1829f4676412c1f710ad77377b3
Eric Boren <borenet@google.com>
2020-03-02 07:09:32 -0500

    [recipes] Compute DM flags in gen_tasks
    Change-Id: I3d82d78161373ec0536cbe6df196c753547501a8

--- infra/bots/recipes/test.py
+++ infra/bots/recipes/test.py
@@ -6,8 +6,7 @@
 # Recipe module for Skia Swarming test.
 
 
-import imp
-import os
+import json
 
 
 DEPS = [
@@ -101,26 +100,12 @@ def test_steps(api):
       use_hash_file = True
 
   # Find DM flags.
-  dm_flags_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               os.pardir, 'recipe_modules', 'vars', 'resources',
-                               'dm_flags.py')
-  dm_flags = imp.load_source('dm_flags', dm_flags_path)
-  issue = str(api.vars.issue) if api.vars.is_trybot else ''
-  patchset = str(api.vars.patchset) if api.vars.is_trybot else ''
-  patch_storage = str(api.vars.patch_storage) if api.vars.is_trybot else ''
-  args = dm_flags.dm_flags(
-    bot=api.vars.builder_name,
-    parts=api.vars.builder_cfg,
-    task_id=api.properties['task_id'],
-    revision=api.properties['revision'],
-    issue=str(api.vars.issue) if api.vars.is_trybot else '',
-    patchset=str(api.vars.patchset) if api.vars.is_trybot else '',
-    patch_storage=str(api.vars.patch_storage) if api.vars.is_trybot else '',
-    buildbucket_build_id=api.properties.get('buildbucket_build_id', ''),
-    swarming_bot_id=api.vars.swarming_bot_id,
-    swarming_task_id=api.vars.swarming_task_id,
-    internal_hardware_label=api.vars.internal_hardware_label,
-  )
+  args = json.loads(api.properties['dm_flags'])
+  for i, arg in enumerate(args):
+    if arg == '${SWARMING_BOT_ID}':
+      args[i] = api.vars.swarming_bot_id
+    elif arg == '${SWARMING_TASK_ID}':
+      args[i] = api.vars.swarming_task_id
 
   # Paths to required resources.
   args.extend([
@@ -171,70 +156,10 @@ def RunSteps(api):
 
 
 TEST_BUILDERS = [
-  'Test-Android-Clang-AndroidOne-GPU-Mali400MP2-arm-Release-All-Android',
-  'Test-Android-Clang-GalaxyS6-GPU-MaliT760-arm64-Debug-All-Android',
-  ('Test-Android-Clang-GalaxyS6-GPU-MaliT760-arm64-Debug-All'
-   '-Android_NoGPUThreads'),
-  ('Test-Android-Clang-GalaxyS7_G930FD-GPU-MaliT880-arm64-Release-All'
-   '-Android_Vulkan'),
-  'Test-Android-Clang-MotoG4-CPU-Snapdragon617-arm-Release-All-Android',
-  'Test-Android-Clang-NVIDIA_Shield-GPU-TegraX1-arm64-Debug-All-Android_CCPR',
-  'Test-Android-Clang-Nexus5-GPU-Adreno330-arm-Release-All-Android',
-  'Test-Android-Clang-Nexus7-CPU-Tegra3-arm-Release-All-Android',
-  'Test-Android-Clang-Pixel-GPU-Adreno530-arm64-Debug-All-Android_Vulkan',
   'Test-Android-Clang-Pixel-GPU-Adreno530-arm-Debug-All-Android_ASAN',
   'Test-Android-Clang-Pixel2XL-GPU-Adreno540-arm64-Debug-All-Android',
-  'Test-Android-Clang-Pixel3-GPU-Adreno630-arm64-Debug-All-Android_Vulkan',
-  'Test-Android-Clang-Pixel3a-GPU-Adreno615-arm64-Debug-All-Android',
-  ('Test-ChromeOS-Clang-AcerChromebookR13Convertible-GPU-PowerVRGX6250-'
-   'arm-Debug-All'),
-  'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-ASAN',
-  'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-BonusConfigs',
-  'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-shard_00_10-Coverage',
-  'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-MSAN',
-  ('Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All'
-   '-SK_USE_DISCARDABLE_SCALEDIMAGECACHE'),
   'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Release-All-Lottie',
-  ('Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Release-All'
-   '-SK_FORCE_RASTER_PIPELINE_BLITTER'),
-  'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Release-All-TSAN',
-  'Test-Debian9-Clang-GCE-GPU-SwiftShader-x86_64-Release-All-SwiftShader',
-  'Test-Debian9-Clang-NUC5PPYH-GPU-IntelHD405-x86_64-Release-All-Vulkan',
-  'Test-Debian9-Clang-NUC7i5BNK-GPU-IntelIris640-x86_64-Debug-All-Vulkan',
-  'Test-Debian10-GCC-GCE-CPU-AVX2-x86_64-Debug-All-Docker',
-  'Test-iOS-Clang-iPhone6-GPU-PowerVRGX6450-arm64-Release-All-Metal',
-  ('Test-Mac10.13-Clang-MacBook10.1-GPU-IntelHD615-x86_64-Release-All'
-   '-NativeFonts'),
-  'Test-Mac10.13-Clang-MacBookPro11.5-CPU-AVX2-x86_64-Debug-All-PDF',
-  'Test-Mac10.13-Clang-MacBookPro11.5-CPU-AVX2-x86_64-Release-All',
-  'Test-Mac10.13-Clang-MacBookPro11.5-GPU-RadeonHD8870M-x86_64-Debug-All-Metal',
-  ('Test-Mac10.13-Clang-MacMini7.1-GPU-IntelIris5100-x86_64-Debug-All'
-   '-CommandBuffer'),
-  'Test-Mac10.14-Clang-MacBookAir7.2-GPU-IntelHD6000-x86_64-Debug-All',
-  'Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-Vulkan',
-  ('Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Release-All'
-   '-Valgrind_AbandonGpuContext_SK_CPU_LIMIT_SSE41'),
-  ('Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Release-All'
-   '-Valgrind_PreAbandonGpuContext_SK_CPU_LIMIT_SSE41'),
-  'Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-DDL1',
-  'Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-DDL3',
-  'Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Release-All-BonusConfigs',
-  'Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-GpuTess',
-  'Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-NonNVPR',
-  ('Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Release-All'
-   '-ReleaseAndAbandonGpuContext'),
-  'Test-Win10-Clang-NUC5i7RYH-CPU-AVX2-x86_64-Debug-All-NativeFonts_GDI',
-  'Test-Win10-Clang-NUC5i7RYH-GPU-IntelIris6100-x86_64-Release-All-ANGLE',
-  'Test-Win10-Clang-NUCD34010WYKH-GPU-IntelHD4400-x86_64-Release-All-ANGLE',
-  'Test-Win10-Clang-ShuttleA-GPU-GTX660-x86_64-Release-All-Vulkan',
-  'Test-Win10-Clang-ShuttleA-GPU-RadeonHD7770-x86_64-Release-All-Vulkan',
   'Test-Win10-Clang-ShuttleC-GPU-GTX960-x86_64-Debug-All-ANGLE',
-  'Test-Win10-MSVC-LenovoYogaC630-GPU-Adreno630-arm64-Debug-All-ANGLE',
-  'Test-Win2019-Clang-GCE-CPU-AVX2-x86_64-Debug-All-FAAA',
-  'Test-Win2019-Clang-GCE-CPU-AVX2-x86_64-Debug-All-FSAA',
-  'Test-iOS-Clang-iPadPro-GPU-PowerVRGT7800-arm64-Release-All',
-  'Test-Mac10.13-Clang-MacBook10.1-GPU-IntelHD615-x86_64-Debug-All-CommandBuffer',
-  'Test-Android-Clang-TecnoSpark3Pro-GPU-PowerVRGE8320-arm-Debug-All-Android',
 ]
 
 
@@ -244,6 +169,9 @@ def GenTests(api):
       api.test(builder) +
       api.properties(buildername=builder,
                      buildbucket_build_id='123454321',
+                     dm_flags=('["dm","--dummy","--flags",'
+                               '"--swarming-bot-id","${SWARMING_BOT_ID}",'
+                               '"--swarming-task-id","${SWARMING_TASK_ID}"]'),
                      revision='abc123',
                      path_config='kitchen',
                      gold_hashes_url='https://example.com/hashes.txt',
@@ -268,156 +196,3 @@ def GenTests(api):
       test += api.platform('win', 64)
 
     yield test
-
-  builder = 'Test-Win8-Clang-Golo-CPU-AVX-x86-Debug-All'
-  yield (
-    api.test('trybot') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   task_id='task_12345') +
-    api.platform('win', 64) +
-    api.properties(patch_storage='gerrit') +
-    api.properties.tryserver(
-          buildername=builder,
-          gerrit_project='skia',
-          gerrit_url='https://skia-review.googlesource.com/',
-      )+
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    )
-  )
-
-  builder = 'Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All'
-  yield (
-    api.test('failed_dm') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   task_id='task_12345') +
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    ) +
-    api.step_data('symbolized dm', retcode=1)
-  )
-
-  builder = 'Test-Android-Clang-Nexus7-GPU-Tegra3-arm-Release-All-Android'
-  yield (
-    api.test('failed_get_hashes') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   task_id='task_12345') +
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    ) +
-    api.step_data('get uninteresting hashes', retcode=1)
-  )
-
-  builder = 'Test-Android-Clang-Nexus7-GPU-Tegra3-arm-Debug-All-Android'
-  retry_step_name = ('push [START_DIR]/skia/resources/* '
-                     '/sdcard/revenge_of_the_skiabot/resources.push '
-                     '[START_DIR]/skia/resources/file1')
-  yield (
-    api.test('failed_push') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   task_id='task_12345') +
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    ) +
-    api.step_data('get swarming bot id',
-                  stdout=api.raw_io.output('build123-m2--device5')) +
-    api.step_data(retry_step_name, retcode=1) +
-    api.step_data(retry_step_name + ' (attempt 2)', retcode=1) +
-    api.step_data(retry_step_name + ' (attempt 3)', retcode=1)
-  )
-
-  retry_step_name = 'adb pull.pull /sdcard/revenge_of_the_skiabot/dm_out'
-  yield (
-    api.test('failed_pull') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   task_id='task_12345') +
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    ) +
-    api.step_data('dm', retcode=1) +
-    api.step_data(retry_step_name, retcode=1) +
-    api.step_data(retry_step_name + ' (attempt 2)', retcode=1) +
-    api.step_data(retry_step_name + ' (attempt 3)', retcode=1)
-  )
-
-  yield (
-    api.test('internal_bot_5') +
-    api.properties(buildername=builder,
-                   buildbucket_build_id='123454321',
-                   revision='abc123',
-                   path_config='kitchen',
-                   swarm_out_dir='[SWARM_OUT_DIR]',
-                   gold_hashes_url='https://example.com/hashes.txt',
-                   internal_hardware_label='5',
-                   task_id='task_12345') +
-    api.path.exists(
-        api.path['start_dir'].join('skia'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skimage', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'skp', 'VERSION'),
-        api.path['start_dir'].join('skia', 'infra', 'bots', 'assets',
-                                     'svg', 'VERSION'),
-        api.path['start_dir'].join('tmp', 'uninteresting_hashes.txt')
-    )
-  )
