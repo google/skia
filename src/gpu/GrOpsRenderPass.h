cea2ab84bc38dbb8ad8fd303309371c9950c0849
Chris Dalton <csmartdalton@google.com>
2020-03-03 18:25:47 -0700

    Migrate the GPU tessellator to the new bind/draw GrOpsRenderPass API
    Change-Id: I4f42a95dad4a5f5221c0150f76e0941873a8726b

--- src/gpu/GrOpsRenderPass.h
+++ src/gpu/GrOpsRenderPass.h
@@ -50,9 +50,11 @@ public:
     // Signals the end of recording to the GrOpsRenderPass and that it can now be submitted.
     virtual void end() = 0;
 
-    // Updates the internal pipeline state for drawing with the provided GrProgramInfo.
-    // Enters an internal "bad" state if the pipeline could not be set.
-    void bindPipeline(const GrProgramInfo&, const SkRect& drawBounds);
+    // Updates the internal pipeline state for drawing with the provided GrProgramInfo. If an
+    // optional scissor rect is provided, then this method calls setScissor for convenience after
+    // binding the pipeline. Enters an internal "bad" state if the pipeline could not be set.
+    void bindPipeline(const GrProgramInfo&, const SkRect& drawBounds,
+                      const SkIRect* optionalScissorRect = nullptr);
 
     // The scissor rect is always dynamic state and therefore not stored on GrPipeline. If scissor
     // test is enabled on the current pipeline, then the client must call setScissorRect() before
@@ -69,7 +71,9 @@ public:
     //
     // If the current program does not use textures, this is a no-op.
     void bindTextures(const GrPrimitiveProcessor&, const GrPipeline&,
-                      const GrSurfaceProxy* const primProcTextures[]);
+                      const GrSurfaceProxy* const primProcTextures[] = nullptr);
+    void bindTextures(const GrPrimitiveProcessor&, const GrPipeline&,
+                      const GrSurfaceProxy& singlePrimProcTexture);
 
     void bindBuffers(const GrBuffer* indexBuffer, const GrBuffer* instanceBuffer,
                      const GrBuffer* vertexBuffer, GrPrimitiveRestart = GrPrimitiveRestart::kNo);
