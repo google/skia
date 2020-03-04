0ae1b3d075e96e6b21130c62f1b4a524525ebbd8
Mike Reed <reed@google.com>
2020-03-04 17:47:43 -0500

    detect when the localmatrix forces us into a perspective stage
    Bug: skia:10004
    Bug: 1057418
    Change-Id: I84a02b5b56e435b7e30d0ca9ae48e689025b63b8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/275080
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- gm/patch.cpp
+++ gm/patch.cpp
@@ -93,7 +93,8 @@ const SkPoint gTexCoords[SkPatchUtils::kNumCorners] = {
 };
 
 
-static void dopatch(SkCanvas* canvas, const SkColor colors[], sk_sp<SkImage> img = nullptr) {
+static void dopatch(SkCanvas* canvas, const SkColor colors[], sk_sp<SkImage> img,
+                    const SkMatrix* localMatrix) {
     SkPaint paint;
 
     const SkBlendMode modes[] = {
@@ -109,7 +110,7 @@ static void dopatch(SkCanvas* canvas, const SkColor colors[], sk_sp<SkImage> img
     if (img) {
         SkScalar w = img->width();
         SkScalar h = img->height();
-        shader = img->makeShader();
+        shader = img->makeShader(localMatrix);
         texStorage[0].set(0, 0);
         texStorage[1].set(w, 0);
         texStorage[2].set(w, h);
@@ -156,19 +157,28 @@ DEF_SIMPLE_GM(patch_primitive, canvas, 1500, 1100) {
     const SkColor colors[SkPatchUtils::kNumCorners] = {
         SK_ColorRED, SK_ColorGREEN, SK_ColorBLUE, SK_ColorCYAN
     };
-    dopatch(canvas, colors);
+    dopatch(canvas, colors, nullptr, nullptr);
 }
 DEF_SIMPLE_GM(patch_image, canvas, 1500, 1100) {
     const SkColor colors[SkPatchUtils::kNumCorners] = {
         SK_ColorRED, SK_ColorGREEN, SK_ColorBLUE, SK_ColorCYAN
     };
-    dopatch(canvas, colors, GetResourceAsImage("images/mandrill_128.png"));
+    dopatch(canvas, colors, GetResourceAsImage("images/mandrill_128.png"), nullptr);
+}
+DEF_SIMPLE_GM(patch_image_persp, canvas, 1500, 1100) {
+    const SkColor colors[SkPatchUtils::kNumCorners] = {
+        SK_ColorRED, SK_ColorGREEN, SK_ColorBLUE, SK_ColorCYAN
+    };
+    SkMatrix localM;
+    localM.reset();
+    localM[6] = 0.00001f;    // force perspective
+    dopatch(canvas, colors, GetResourceAsImage("images/mandrill_128.png"), &localM);
 }
 DEF_SIMPLE_GM(patch_alpha, canvas, 1500, 1100) {
     const SkColor colors[SkPatchUtils::kNumCorners] = {
         SK_ColorRED, 0x0000FF00, SK_ColorBLUE, 0x00FF00FF,
     };
-    dopatch(canvas, colors);
+    dopatch(canvas, colors, nullptr, nullptr);
 }
 
 // These two should look the same (one patch, one simple path)
