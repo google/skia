bc173386560fe96433fb111847fc3aa2575f7296
Mike Reed <reed@google.com>
2020-03-09 09:08:00 -0400

    WIP: clipShader
    1. pass shader+op down to device
    2. bitmapdevice pass it down to rasterclip
    3. rasterclip only ever stores at most one shader
       - if there is a ctm, fold that into (another) shader
       - if the op is difference, invert the sense of alpha
       - if there was a previous shader, compose with it
    4. pass through to rasterpipelineblitter
    5. it prepends the colorPipeline with the clipShader, and stashes its
       results in a buffer (fClipShaderBuffer)
    6. in each blit, scale/lerp from the buffer before storing the result
    
    Having trouble with lowp and my sloppy store_src.
    store_src stores 4 lanes (RGBA) but stores them as either floats or u16
    
    To apply the clip I want to load the alpha lane (via either scale_native or lerp_native)
    but the buffer is the 4th lane I stored.. but to find that, I need to
    know if I'm highp or lowp.
    
    For now I cheat and just use the first channel (red). Seems like the
    optimal fix is to introduce a special stage that just stores the lane
    I want (alpha) -- this will be faster as well...
    
    Change-Id: I07c7a8a20b9ae95cdcc9954237d115e63819f7c1

--- src/core/SkCoreBlitters.h
+++ src/core/SkCoreBlitters.h
@@ -166,7 +166,7 @@ private:
 
 // Neither of these ever returns nullptr, but this first factory may return a SkNullBlitter.
 SkBlitter* SkCreateRasterPipelineBlitter(const SkPixmap&, const SkPaint&, const SkMatrix& ctm,
-                                         SkArenaAlloc*);
+                                         SkArenaAlloc*, sk_sp<SkShader> clipShader);
 // Use this if you've pre-baked a shader pipeline, including modulating with paint alpha.
 // This factory never returns an SkNullBlitter.
 SkBlitter* SkCreateRasterPipelineBlitter(const SkPixmap&, const SkPaint&,
