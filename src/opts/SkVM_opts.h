a67d1aebf27c1ca1d8766656d8ffe7cc4bf06479
Mike Klein <mtklein@google.com>
2020-03-09 17:36:00 -0500

    add missing license line to SkVM_opts.h
    Change-Id: Ie66eccaae13bd1499b3b21bd1bc3c08f867e58aa
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/276139
    Auto-Submit: Mike Klein <mtklein@google.com>
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/opts/SkVM_opts.h
+++ src/opts/SkVM_opts.h
@@ -1,4 +1,5 @@
 // Copyright 2020 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 #ifndef SkVM_opts_DEFINED
 #define SkVM_opts_DEFINED
