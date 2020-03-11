2a002cbbda9f313d098a165cda76a3cbc88ebf04
Robert Phillips <robertphillips@google.com>
2020-03-11 13:49:36 -0400

    Clean up programInfo creation in GrMeshDrawOp-derived Ops
    This CL:
      Renames the existing createProgramInfo method to onCreateProgramInfo and makes it a virtual on GrMeshDrawOp
    
      Moves the non-virtual createProgramInfo (helper) calls to GrMeshDrawOp
    
      Changes onCreateProgramInfo to not return a ProgramInfo*. This is setting up to handle ops that create >1 programInfo (e.g., AAHairlineOp)
    
    This CL leaves the following ops in need of an onCreateProgramInfo implementation:
    
    AAHairlineOp
    AAFlatteningConvexPathOp
    GrAtlasTextOp
    GrShadowRRectOp
    SmallPathOp
    GrTessellatingPathRenderer (https://skia-review.googlesource.com/c/skia/+/276401)
    GrTextureOp
    PrimitiveProcessorTest::Op
    
    
    Bug: skia:9455
    Change-Id: Id7e2b8a40cac86ede6bf3c0e544da5500ff47d8a

--- src/gpu/ops/GrAAHairLinePathRenderer.cpp
+++ src/gpu/ops/GrAAHairLinePathRenderer.cpp
@@ -859,6 +859,14 @@ private:
                                      const SkMatrix* geometryProcessorViewM,
                                      const SkMatrix* geometryProcessorLocalM);
 
+    void onCreateProgramInfo(const GrCaps*,
+                             SkArenaAlloc*,
+                             const GrSurfaceProxyView* outputView,
+                             GrAppliedClip&&,
+                             const GrXferProcessor::DstProxyView&) override {
+        // TODO [PI]: implement
+    }
+
     void onPrepareDraws(Target*) override;
     void onExecute(GrOpFlushState*, const SkRect& chainBounds) override;
 
