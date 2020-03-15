f147454260f3f9ac28953d3df5f9018f040a4d4b
Herb Derby <herb@google.com>
2020-03-15 12:55:16 -0400

    Check fullness and purge on every strike lookup
    In the old code, the cache was checked and perged on every reattach
    of a strike. That was left out of the multi-threaded code. Add the
    check and purge back in to maintain cache size.
    
    * Removed minimum cache size from setCacheSizeLimit.
    
    Bug: skia:10046
    Change-Id: I2438c83f04b6da8133c161a29604c3c3d7f58cd8

---
+++ tests/SkStrikeCacheTest.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/core/SkStrikeCache.h"
+#include "src/core/SkStrikeSpec.h"
+#include "tests/Test.h"
+#include "tools/ToolUtils.h"
+
+DEF_TEST(SkStrikeCache_CachePurge, Reporter) {
+    SkStrikeCache cache;
+
+    sk_sp<SkTypeface> typeface =
+            ToolUtils::create_portable_typeface("serif", SkFontStyle::Italic());
+
+    SkFont font;
+    font.setEdging(SkFont::Edging::kAntiAlias);
+    font.setSubpixel(true);
+    font.setTypeface(typeface);
+
+    SkPaint defaultPaint;
+    SkStrikeSpec strikeSpec = SkStrikeSpec::MakeMask(
+            font, defaultPaint, SkSurfaceProps(0, kUnknown_SkPixelGeometry),
+            SkScalerContextFlags::kNone, SkMatrix::I());
+
+    // Initially empty cache
+    SkASSERT(cache.getTotalMemoryUsed() == 0);
+
+    {
+        auto strike = strikeSpec.findOrCreateStrike(&cache);
+    }
+
+    // Stuff in cache.
+    SkASSERT(cache.getTotalMemoryUsed() > 0);
+
+    cache.purgeAll();
+
+    // Purged cache.
+    SkASSERT(cache.getTotalMemoryUsed() == 0);
+
+    // Smallest cache.
+    cache.setCacheSizeLimit(0);
+    {
+        auto strike = strikeSpec.findOrCreateStrike(&cache);
+    }
+    SkASSERT(cache.getTotalMemoryUsed() == 0);
+
+
+}
