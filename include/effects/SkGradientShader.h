ac171cb679b3ebd4dbc61394ab4e127e5a4997c5
Kevin Lubick <kjlubick@google.com>
2020-03-18 14:48:44 -0400

    Add more fiddles for the rest of gradient shaders
    https://fiddle.skia.org/c/eca52ffc02fccd271d83a8c5ee76d039
    https://fiddle.skia.org/c/0489e4ea1aebc99779708516aa481f94
    https://fiddle.skia.org/c/c0b445f02164b65e66a0f398afd04786
    
    Change-Id: I4ac4d2138e3ba5aac37ceff57c41befec6a64932

--- include/effects/SkGradientShader.h
+++ include/effects/SkGradientShader.h
@@ -116,6 +116,8 @@ public:
                         intermediate values must be strictly increasing.
         @param  count   Must be >= 2. The number of colors (and pos if not NULL) entries
         @param  mode    The tiling mode
+
+        example: https://fiddle.skia.org/c/@GradientShader_MakeRadial
     */
     static sk_sp<SkShader> MakeRadial(const SkPoint& center, SkScalar radius,
                                       const SkColor colors[], const SkScalar pos[], int count,
@@ -139,6 +141,8 @@ public:
                         intermediate values must be strictly increasing.
         @param  count   Must be >= 2. The number of colors (and pos if not NULL) entries
         @param  mode    The tiling mode
+
+        example: https://fiddle.skia.org/c/@GradientShader_MakeRadial
     */
     static sk_sp<SkShader> MakeRadial(const SkPoint& center, SkScalar radius,
                                       const SkColor4f colors[], sk_sp<SkColorSpace> colorSpace,
@@ -156,6 +160,8 @@ public:
      *  returns NULL if the inputs are invalid. The gradient interprets the
      *  two circles according to the following HTML spec.
      *  http://dev.w3.org/html5/2dcontext/#dom-context-2d-createradialgradient
+
+     *  example: https://fiddle.skia.org/c/@GradientShader_MakeTwoPointConical
      */
     static sk_sp<SkShader> MakeTwoPointConical(const SkPoint& start, SkScalar startRadius,
                                                const SkPoint& end, SkScalar endRadius,
@@ -175,6 +181,8 @@ public:
      *  returns NULL if the inputs are invalid. The gradient interprets the
      *  two circles according to the following HTML spec.
      *  http://dev.w3.org/html5/2dcontext/#dom-context-2d-createradialgradient
+
+     *  example: https://fiddle.skia.org/c/@GradientShader_MakeTwoPointConical
      */
     static sk_sp<SkShader> MakeTwoPointConical(const SkPoint& start, SkScalar startRadius,
                                                const SkPoint& end, SkScalar endRadius,
@@ -206,6 +214,8 @@ public:
         @param  mode       Tiling mode: controls drawing outside of the gradient angular range.
         @param  startAngle Start of the angular range, corresponding to pos == 0.
         @param  endAngle   End of the angular range, corresponding to pos == 1.
+
+        example: https://fiddle.skia.org/c/@GradientShader_MakeSweep
     */
     static sk_sp<SkShader> MakeSweep(SkScalar cx, SkScalar cy,
                                      const SkColor colors[], const SkScalar pos[], int count,
@@ -238,6 +248,8 @@ public:
         @param  mode       Tiling mode: controls drawing outside of the gradient angular range.
         @param  startAngle Start of the angular range, corresponding to pos == 0.
         @param  endAngle   End of the angular range, corresponding to pos == 1.
+
+        example: https://fiddle.skia.org/c/@GradientShader_MakeSweep
     */
     static sk_sp<SkShader> MakeSweep(SkScalar cx, SkScalar cy,
                                      const SkColor4f colors[], sk_sp<SkColorSpace> colorSpace,
