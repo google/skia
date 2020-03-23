165b68ebb6ad21adae9f7a099ee0f78de93e1424
Mike Reed <reed@google.com>
2020-03-22 21:10:38 -0400

    Add onProgram to ModeColorFilter
    Change-Id: Id82cfc632f378054f5ecded2c5a77d2824982288
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/278420
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/core/SkModeColorFilter.cpp
+++ src/core/SkModeColorFilter.cpp
@@ -17,6 +17,7 @@
 #include "src/core/SkModeColorFilter.h"
 #include "src/core/SkRasterPipeline.h"
 #include "src/core/SkReadBuffer.h"
+#include "src/core/SkVM.h"
 #include "src/core/SkValidationUtils.h"
 #include "src/core/SkWriteBuffer.h"
 
@@ -68,6 +69,15 @@ bool SkModeColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaqu
     rec.fPipeline->append_constant_color(rec.fAlloc, color.premul().vec());
     SkBlendMode_AppendStages(fMode, rec.fPipeline);
     return true;
+}
+
+skvm::Color SkModeColorFilter::onProgram(skvm::Builder* p, skvm::Color c,
+                                         SkColorSpace* dstCS,
+                                         skvm::Uniforms* uniforms, SkArenaAlloc*) const {
+    skvm::Color dst = c,
+                src = p->uniformPremul(SkColor4f::FromColor(fColor), sk_srgb_singleton(),
+                                       uniforms, dstCS);
+    return p->blend(fMode, src,dst);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
