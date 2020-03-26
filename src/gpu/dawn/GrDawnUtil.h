476af340cd6de15700daedfa2f1c03af117729b1
Brian Salomon <bsalomon@google.com>
2020-03-26 16:45:05 -0400

    Add getter for channels to GrBackendFormat
    Bug: skia:10078
    
    Change-Id: I0172a87d836a606a55e60b73ba255a41313e9c12

--- src/gpu/dawn/GrDawnUtil.h
+++ src/gpu/dawn/GrDawnUtil.h
@@ -19,4 +19,14 @@ size_t GrDawnRoundRowBytes(size_t rowBytes);
 const char* GrDawnFormatToStr(wgpu::TextureFormat format);
 #endif
 
+static constexpr uint32_t GrDawnFormatChannels(wgpu::TextureFormat format) {
+    switch (format) {
+        case wgpu::TextureFormat::RGBA8Unorm:   return kRGBA_SkColorChannelFlags;
+        case wgpu::TextureFormat::BGRA8Unorm:   return kRGBA_SkColorChannelFlags;
+        case wgpu::TextureFormat::R8Unorm:      return kRed_SkColorChannelFlag;
+
+        default:                                return 0;
+    }
+}
+
 #endif // GrDawnUtil_DEFINED
