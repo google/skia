265de3a93c6ec3f21e1b1e01e89e4a4c712f9d6c
Mike Reed <reed@google.com>
2020-03-26 08:38:25 -0400

    Include all tests, enlarge labels
    Need to expand the size of highcontrastcolorfilter gm, to show all cases.
    Also enlarge labels so they can be read.
    
    Change-Id: I8f9278e7ce2f06a9a6921d70f1f38dc18f9023d7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/279336
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- gm/highcontrastfilter.cpp
+++ gm/highcontrastfilter.cpp
@@ -39,9 +39,9 @@ static void draw_label(SkCanvas* canvas, const SkHighContrastConfig& config) {
     char labelBuffer[256];
     const char* invertStr =
         (config.fInvertStyle == InvertStyle::kInvertBrightness ?
-             "InvBrightness" :
+             "InvBright" :
             (config.fInvertStyle == InvertStyle::kInvertLightness ?
-                 "InvLightness" : "NoInvert"));
+                 "InvLight" : "NoInvert"));
 
     snprintf(labelBuffer, sizeof(labelBuffer), "%s%s contrast=%.1f",
              config.fGrayscale ? "Gray " : "",
@@ -50,8 +50,8 @@ static void draw_label(SkCanvas* canvas, const SkHighContrastConfig& config) {
 
     SkFont font;
     font.setTypeface(ToolUtils::create_portable_typeface());
-    font.setSize(0.05f);
-    font.setEdging(SkFont::Edging::kAlias);
+    font.setSize(0.075f);
+    font.setEdging(SkFont::Edging::kAntiAlias);
 
     size_t len = strlen(labelBuffer);
 
@@ -129,7 +129,7 @@ protected:
     }
 
     SkISize onISize() override {
-        return SkISize::Make(600, 420);
+        return SkISize::Make(800, 420);
     }
 
     void onDraw(SkCanvas* canvas) override {
