99c972b0adb055439fa81011e24bee5b31a733ef
Ben Wagner <bungeman@google.com>
2020-03-27 10:34:20 -0400

    Fix gcc9 warning around size of memset.
    GCC version 9 added a diagnostic about
    
    ‘memset’ used with length equal to number of elements without multiplication by element size [-Werror=memset-elt-size]
    
    In SkSLInterpreter.h, make it obvous to both the compiler and reader of
    the code that the intention is to memset the first element of the array.
    
    Change-Id: I9531eb220a39d3fc31e9faad3649f39204e74dc1

--- src/sksl/SkSLInterpreter.h
+++ src/sksl/SkSLInterpreter.h
@@ -238,13 +238,13 @@ public:
      bool run(const ByteCodeFunction* f, Vector args[], Vector** outResult) {
         SkASSERT(f);
         VectorI condStack[MASK_STACK_SIZE];
-        memset(condStack, 255, sizeof(VectorI));
+        memset(&condStack[0], 255, sizeof(condStack[0]));
         VectorI maskStack[MASK_STACK_SIZE];
-        memset(maskStack, 255, sizeof(VectorI));
+        memset(&maskStack[0], 255, sizeof(maskStack[0]));
         VectorI loopStack[LOOP_STACK_SIZE];
-        memset(loopStack, 255, sizeof(VectorI));
+        memset(&loopStack[0], 255, sizeof(loopStack[0]));
         VectorI continueStack[LOOP_STACK_SIZE];
-        memset(continueStack, 0, sizeof(VectorI));
+        memset(&continueStack[0], 0, sizeof(continueStack[0]));
         Vector* stack = fMemory + MEMORY_SIZE;
         int stackCount = f->fStackSlotCount + f->fParameterSlotCount;
         stack -= stackCount;
@@ -291,13 +291,13 @@ public:
         for (int i = 0; i < count; i += width) {
             int lanes = std::min(width, count - i);
             size_t size = lanes * sizeof(float);
-            memset(maskStack, 255, sizeof(VectorI));
-            memset(loopStack, 255, sizeof(VectorI));
+            memset(&maskStack[0], 255, sizeof(maskStack[0]));
+            memset(&loopStack[0], 255, sizeof(loopStack[0]));
             for (int j = lanes; j < width; ++j) {
                 maskStack[0][j] = 0;
                 loopStack[0][j] = 0;
             }
-            memset(continueStack, 0, sizeof(VectorI));
+            memset(&continueStack[0], 0, sizeof(continueStack[0]));
             for (int j = 0; j < f->fParameterSlotCount; ++j) {
                 memcpy(stack + j, &args[j][i], size);
             }
