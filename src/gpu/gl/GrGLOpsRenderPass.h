49c67188863598bc6fc277c95d69a27db0c54472
Chris Dalton <csmartdalton@google.com>
2020-03-31 12:29:47 -0600

    Use glDraw.*BaseInstance calls to avoid deferred buffer binding
    Change-Id: I968dab317673051acc65f87ea76a0d657d89b3d2

--- src/gpu/gl/GrGLOpsRenderPass.h
+++ src/gpu/gl/GrGLOpsRenderPass.h
@@ -40,8 +40,8 @@ public:
 private:
     GrGpu* gpu() override { return fGpu; }
 
-    void setupGeometry(const GrBuffer* vertexBuffer, int baseVertex, const GrBuffer* instanceBuffer,
-                       int baseInstance);
+    void bindVertexBuffer(const GrBuffer*, int baseVertex);
+    void bindInstanceBuffer(const GrBuffer*, int baseInstance);
 
     const void* offsetForBaseIndex(int baseIndex) const {
         if (!fIndexPointer) {
