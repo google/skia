c69aad41f3c80865404d2986bb05edddb6ad91f1
Florin Malita <fmalita@chromium.org>
2020-04-01 10:48:36 -0400

    [skottie] Vector-encoded paths
    Change-Id: I822797fceae561b52b709bf258163bbcc6b565fb

--- modules/skottie/src/SkottieValue.h
+++ modules/skottie/src/SkottieValue.h
@@ -32,7 +32,6 @@ struct ValueTraits {
     template <typename U>
     static U As(const T&);
 
-    static bool CanLerp(const T&, const T&);
     static bool Lerp(const T&, const T&, float, T*);
 };
 
@@ -40,35 +39,10 @@ using ScalarValue = SkScalar;
 using   Vec2Value = SkV2;
 using VectorValue = std::vector<float>;
 
-struct BezierVertex {
-    SkPoint fInPoint,  // "in" control point, relative to the vertex
-            fOutPoint, // "out" control point, relative to the vertex
-            fVertex;
-
-    bool operator==(const BezierVertex& other) const {
-        return fInPoint  == other.fInPoint
-            && fOutPoint == other.fOutPoint
-            && fVertex   == other.fVertex;
-    }
-
-    bool operator!=(const BezierVertex& other) const { return !(*this == other); }
-};
-
 struct ShapeValue {
-    std::vector<BezierVertex> fVertices;
-    bool                      fClosed   : 1,
-                              fVolatile : 1;
+    std::vector<float> fData;
 
-    ShapeValue() : fClosed(false), fVolatile(false) {}
-    ShapeValue(const ShapeValue&)            = default;
-    ShapeValue(ShapeValue&&)                 = default;
-    ShapeValue& operator=(const ShapeValue&) = default;
-
-    bool operator==(const ShapeValue& other) const {
-        return fVertices == other.fVertices && fClosed == other.fClosed;
-    }
-
-    bool operator!=(const ShapeValue& other) const { return !(*this == other); }
+    operator SkPath() const;
 };
 
 } // namespace skottie
