b08cc024cde2faced097c283a564aba210a2c70e
Brian Osman <brianosman@google.com>
2020-04-02 11:38:40 -0400

    Switch back to stack-based SkSL interpreter
    It's slower, but code size is quite a bit smaller, memory usage is
    smaller, and we think that mapping it to SkVM is just as easy.
    
    This effectively reverts all of the following commits:
    
    "Fix gcc9 warning around size of memset."
    https://skia-review.googlesource.com/c/skia/+/279861
    
    "Remove unused (and misleading) 'instruction' from SkSLInterpreter.h"
    https://skia-review.googlesource.com/c/skia/+/278177
    
    "Interpreter: Fix intrinsics when called with vector types"
    https://skia-review.googlesource.com/c/skia/+/272721
    
    "Make it easier to add vector versions of byte code instructions"
    https://skia-review.googlesource.com/c/skia/+/272527
    
    "Interpreter: Support returns from runStriped"
    https://skia-review.googlesource.com/c/skia/+/268941
    
    "add SkSLInterpreter vector instructions"
    https://skia-review.googlesource.com/c/skia/+/266560
    
    "Fix crash when editing particle scripts"
    https://skia-review.googlesource.com/c/skia/+/269487
    
    "Revert "Revert "Complete rewrite of the SkSL interpreter"""
    https://skia-review.googlesource.com/c/skia/+/266205
    
    Change-Id: I4258596399c4ca94489d4faf8aacfec88afeee13
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/281205
    Reviewed-by: Mike Klein <mtklein@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/sksl/SkSLCompiler.h
+++ src/sksl/SkSLCompiler.h
@@ -215,8 +215,8 @@ private:
 
     Position position(int offset);
 
-    std::map<String, std::pair<std::unique_ptr<ProgramElement>, bool>> fGPUIntrinsics;
-    std::map<String, std::pair<std::unique_ptr<ProgramElement>, bool>> fInterpreterIntrinsics;
+    std::map<StringFragment, std::pair<std::unique_ptr<ProgramElement>, bool>> fGPUIntrinsics;
+    std::map<StringFragment, std::pair<std::unique_ptr<ProgramElement>, bool>> fInterpreterIntrinsics;
     std::unique_ptr<ASTFile> fGpuIncludeSource;
     std::shared_ptr<SymbolTable> fGpuSymbolTable;
     std::vector<std::unique_ptr<ProgramElement>> fVertexInclude;
@@ -227,7 +227,6 @@ private:
     std::shared_ptr<SymbolTable> fGeometrySymbolTable;
     std::vector<std::unique_ptr<ProgramElement>> fPipelineInclude;
     std::shared_ptr<SymbolTable> fPipelineSymbolTable;
-    std::unique_ptr<ASTFile> fInterpreterIncludeSource;
     std::vector<std::unique_ptr<ProgramElement>> fInterpreterInclude;
     std::shared_ptr<SymbolTable> fInterpreterSymbolTable;
 
