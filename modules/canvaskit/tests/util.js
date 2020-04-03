bdffb438b3ff98a5b2d0a18eb59406d09e6ed447
Kevin Lubick <kjlubick@google.com>
2020-04-03 09:37:09 -0400

    [canvaskit] Add gm helper functions to tests.
    Change-Id: Iced4abb57f66572035ab5d1a54b374055e8aaa58

--- modules/canvaskit/tests/util.js
+++ modules/canvaskit/tests/util.js
@@ -2,6 +2,72 @@
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
 
+const _commonGM = (it, pause, name, callback, assetsToFetchOrPromisesToWaitOn) => {
+    const fetchPromises = [];
+    for (const assetOrPromise of assetsToFetchOrPromisesToWaitOn) {
+        // https://stackoverflow.com/a/9436948
+        if (typeof assetOrPromise === 'string' || assetOrPromise instanceof String) {
+            const newPromise = fetch(assetOrPromise)
+                .then((response) => response.arrayBuffer());
+            fetchPromises.push(newPromise);
+        } else if (typeof assetOrPromise.then === 'function') {
+            fetchPromises.push(assetOrPromise);
+        } else {
+            throw 'Neither a string nor a promise ' + assetOrPromise;
+        }
+    }
+    it('draws gm '+name, (done) => {
+        const surface = CanvasKit.MakeCanvasSurface('test');
+        expect(surface).toBeTruthy('Could not make surface')
+        if (!surface) {
+            done();
+            return;
+        }
+        // if fetchPromises is empty, the returned promise will
+         // resolve right away and just call the callback.
+        Promise.all(fetchPromises).then((values) => {
+            try {
+                callback(surface.getCanvas(), values);
+            } catch (e) {
+                console.log(`gm ${name} failed with error`, e);
+                expect(e).toBeFalsy();
+                debugger;
+                done();
+            }
+            surface.flush();
+            if (pause) {
+                reportSurface(surface, name, null);
+            } else {
+                reportSurface(surface, name, done);
+            }
+        }).catch((e) => {
+            console.log(`could not load assets for gm ${name}`, e);
+            debugger;
+            done();
+        });
+    })
+}
+
+const gm = (name, callback, ...assetsToFetchOrPromisesToWaitOn) => {
+    _commonGM(it, false, name, callback, assetsToFetchOrPromisesToWaitOn);
+}
+
+const fgm = (name, callback, ...assetsToFetchOrPromisesToWaitOn) => {
+    _commonGM(fit, false, name, callback, assetsToFetchOrPromisesToWaitOn);
+}
+
+const force_gm = (name, callback, ...assetsToFetchOrPromisesToWaitOn) => {
+    _commonGM(fit, false, name, callback, assetsToFetchOrPromisesToWaitOn);
+}
+
+const skip_gm = (name, callback, ...assetsToFetchOrPromisesToWaitOn) => {
+    _commonGM(xit, false, name, callback, assetsToFetchOrPromisesToWaitOn);
+}
+
+const pause_gm = (name, callback, ...assetsToFetchOrPromisesToWaitOn) => {
+    _commonGM(fit, true, name, callback, assetsToFetchOrPromisesToWaitOn);
+}
+
 function reportSurface(surface, testname, done) {
     // In docker, the webgl canvas is blank, but the surface has the pixel
     // data. So, we copy it out and draw it to a normal canvas to take a picture.
