da8ee09bc41bc4d3ccc17bae6b2995bf9b2d9300
Tyler Freeman <fuego@google.com>
2020-04-07 10:42:11 -0700

    SVG: Added font-size and font-weight attributes to Text node
    Change-Id: Ic929cb0119aefae490110648df6e9ea57700c6db

--- experimental/svg/model/SkSVGText.h
+++ experimental/svg/model/SkSVGText.h
@@ -25,6 +25,8 @@ class SkSVGText final : public SkSVGShape {
   void setY(const SkSVGLength&);
   void setFontFamily(const SkSVGStringType&);
   void setFontSize(const SkSVGLength&);
+  void setFontStyle(const SkSVGStringType&);
+  void setFontWeight(const SkSVGStringType&);
   void setText(const SkSVGStringType&);
   void setTextAnchor(const SkSVGStringType&);
 
@@ -36,6 +38,9 @@ class SkSVGText final : public SkSVGShape {
 
   SkPath onAsPath(const SkSVGRenderContext&) const override;
 
+  void loadFont(const SkString font_family);
+  void reloadFont();
+
  private:
   SkSVGText();
   SkSVGLength fX = SkSVGLength(0);
@@ -43,6 +48,8 @@ class SkSVGText final : public SkSVGShape {
   SkSVGStringType fText;
   sk_sp<SkTypeface> fTypeface;
   SkSVGLength fFontSize;
+  SkSVGStringType fFontStyle;
+  SkSVGStringType fFontWeight;
   SkTextUtils::Align fTextAlign = SkTextUtils::Align::kLeft_Align;
   typedef SkSVGShape INHERITED;
 };
