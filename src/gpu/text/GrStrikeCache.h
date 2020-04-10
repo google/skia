4a1c0b43ded079fe57e82ebf583ac3fc992f63bd
Robert Phillips <robertphillips@google.com>
2020-04-10 10:55:28 -0400

    Make GrTextStrike::addGlyphToAtlas a static method in GrTextBlob.cpp
    The text strike has very little to do with adding a glyph to an atlas.
    
    Change-Id: I3f9da3d70587ef80aa7c20f1aa511ffdb09e7411

--- src/gpu/text/GrStrikeCache.h
+++ src/gpu/text/GrStrikeCache.h
@@ -31,19 +31,6 @@ public:
 
     GrGlyph* getGlyph(SkPackedGlyphID);
 
-    // returns true if glyph successfully added to texture atlas, false otherwise.  If the glyph's
-    // mask format has changed, then addGlyphToAtlas will draw a clear box.  This will almost never
-    // happen.
-    // TODO we can handle some of these cases if we really want to, but the long term solution is to
-    // get the actual glyph image itself when we get the glyph metrics.
-    GrDrawOpAtlas::ErrorCode addGlyphToAtlas(const SkGlyph&,
-                                             GrMaskFormat expectedMaskFormat,
-                                             bool needsPadding,
-                                             GrResourceProvider*,
-                                             GrDeferredUploadTarget*,
-                                             GrAtlasManager*,
-                                             GrGlyph*);
-
 private:
     struct HashTraits {
         // GetKey and Hash for the the hash table.
