87cb00e896130846dfbf92030d1d52061f2bb8c6
Greg Daniel <egdaniel@google.com>
2020-04-11 10:16:17 -0400

    Testing flush stuff.
    Change-Id: I50e9df47b86bef559a14bb3942446d2e7958bc19

--- src/gpu/GrContext.cpp
+++ src/gpu/GrContext.cpp
@@ -320,10 +320,11 @@ GrSemaphoresSubmitted GrContext::flush(const GrFlushInfo& info,
     }
 
     bool submitted = false;
-    if (this->drawingManager()->flush(nullptr, 0, SkSurface::BackendSurfaceAccess::kNoAccess,
+    if (this->drawingManager()->flush2(nullptr, 0, SkSurface::BackendSurfaceAccess::kNoAccess,
                                       info, externalRequests)) {
-        bool forceSync = SkToBool(info.fFlags & kSyncCpu_GrFlushFlag);
-        submitted = this->drawingManager()->submitToGpu(forceSync);
+        //bool forceSync = SkToBool(info.fFlags & kSyncCpu_GrFlushFlag);
+        //submitted = this->drawingManager()->submitToGpu(forceSync);
+        submitted = true;
     }
 
     if (!submitted || (!this->priv().caps()->semaphoreSupport() && info.fNumSemaphores)) {
