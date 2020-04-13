6061f5271171fa01756797efad08c4a9f01db9d3
Jim Van Verth <jvanverth@google.com>
2020-04-13 11:02:21 -0400

    Add GrD3DStencilAttachment
    Bug: skia:9935
    Change-Id: I249b819d3cc1387aecde200907657ffcd67d1833

---
+++ src/gpu/d3d/GrD3DStencilAttachment.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/d3d/GrD3DStencilAttachment.h"
+
+#include "src/gpu/d3d/GrD3DGpu.h"
+
+GrD3DStencilAttachment::GrD3DStencilAttachment(GrD3DGpu* gpu,
+                                               const Format& format,
+                                               const D3D12_RESOURCE_DESC& desc,
+                                               const GrD3DTextureResourceInfo& info,
+                                               sk_sp<GrD3DResourceState> state)
+    : GrStencilAttachment(gpu, desc.Width, desc.Height, format.fStencilBits,
+                          desc.SampleDesc.Count)
+    , GrD3DTextureResource(info, state) {
+    this->registerWithCache(SkBudgeted::kYes);
+}
+
+GrD3DStencilAttachment* GrD3DStencilAttachment::Make(GrD3DGpu* gpu,
+                                                     int width,
+                                                     int height,
+                                                     int sampleCnt,
+                                                     const Format& format) {
+
+
+    D3D12_RESOURCE_DESC resourceDesc;
+    resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+    resourceDesc.Alignment = 0;  // default alignment
+    resourceDesc.Width = width;
+    resourceDesc.Height = height;
+    resourceDesc.DepthOrArraySize = 1;
+    resourceDesc.MipLevels = 1;
+    resourceDesc.Format = format.fInternalFormat;
+    resourceDesc.SampleDesc.Count = sampleCnt;
+    resourceDesc.SampleDesc.Quality = 0; // quality levels are only supported for tiled resources
+                                         // so ignore for now
+    resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;  // use driver-selected swizzle
+    resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+    GrD3DTextureResourceInfo info;
+    // TODO: it'd be better if we initialized the resource in DEPTH_WRITE state instead of COMMON
+    if (!GrD3DTextureResource::InitTextureResourceInfo(gpu, resourceDesc, GrProtected::kNo, &info)) {
+        return nullptr;
+    }
+
+    sk_sp<GrD3DResourceState> state(new GrD3DResourceState(info.fResourceState));
+    GrD3DStencilAttachment* stencil = new GrD3DStencilAttachment(gpu, format, resourceDesc,
+                                                                 info, std::move(state));
+    return stencil;
+}
+
+size_t GrD3DStencilAttachment::onGpuMemorySize() const {
+    uint64_t size = this->width();
+    size *= this->height();
+    size *= GrD3DCaps::GetStencilFormatTotalBitCount(this->dxgiFormat());
+    size *= this->numSamples();
+    return static_cast<size_t>(size / 8);
+}
+
+void GrD3DStencilAttachment::onRelease() {
+    GrD3DGpu* gpu = this->getD3DGpu();
+    this->releaseResource(gpu);
+
+    GrStencilAttachment::onRelease();
+}
+
+void GrD3DStencilAttachment::onAbandon() {
+    GrD3DGpu* gpu = this->getD3DGpu();
+    this->releaseResource(gpu);
+
+    GrStencilAttachment::onAbandon();
+}
+
+GrD3DGpu* GrD3DStencilAttachment::getD3DGpu() const {
+    SkASSERT(!this->wasDestroyed());
+    return static_cast<GrD3DGpu*>(this->getGpu());
+}
