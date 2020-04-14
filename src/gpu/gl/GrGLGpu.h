3b87b7b57eb4b044c01e73a251756ce98b8a03fc
Greg Daniel <egdaniel@google.com>
2020-04-14 15:27:10 -0400

    Only call glFlush in submit when it is required.
    Bug: chromium:1070474
    Change-Id: I9131c3d931ec0d861fff4d92549d5d3fce8a123f

--- src/gpu/gl/GrGLGpu.h
+++ src/gpu/gl/GrGLGpu.h
@@ -609,6 +609,16 @@ private:
         return &fHWBufferState[typeAsUInt];
     }
 
+    enum class ForceFlush {
+        kYes,
+        kNo,
+    };
+
+    // This calls glFlush if it is required for previously called operations.
+    void glFlushIfRequired(ForceFlush forceFlush = ForceFlush::kNo);
+
+    void setNeedsFlush() { fNeedsGLFlush = true; }
+
     struct {
         GrBlendEquation fEquation;
         GrBlendCoeff    fSrcCoeff;
@@ -704,6 +714,11 @@ private:
         GrGLsync fSync;
     };
     std::list<FinishCallback> fFinishCallbacks;
+
+    // If we've called a command that requires us to call glFlush than this will be set to true
+    // since we defer calling flush until submit time. When we call submitToGpu if this is true then
+    // we call glFlush and reset this to false.
+    bool fNeedsGLFlush = false;
 
     SkDEBUGCODE(bool fIsExecutingCommandBuffer_DebugOnly = false);
 
