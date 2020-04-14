0fcbaf251a73ca0213e59d80427926652a39101d
Weston Tracey <westont@google.com>
2020-04-14 10:37:22 -0400

    Move Pixelbook build jobs to Docker Debian9.
    -Make generated build tasks for Pixelbook use Debian9...Docker and rename those in jobs.json
    ---Non-Pixelbook ChromeOS build tasks now use regular Debian10, as they seem to handle it fine.
    ---Removed 2 Build-Debian9-Clang-arm-.*-Chromebook_GLES jobs, since they are now unused.
    -Added docker logic to chromebook.py, refactored to handle the differing directory structure (TODO to simplify)
    ---Added docker-chromeos-compile.sh to run in the container.
    ---util function to convert to string that can be used in the python or passed to bash (cribbed from docker.py, will consolidate in later CL)
    -Prerequisite for upgrading the last 10% of the fleet to Debian10
    
    Bug: skia:9438
    Change-Id: I0727aad2d00383e568ddbeea98d56d42bcca26e5

--- infra/bots/recipe_modules/build/chromebook.py
+++ infra/bots/recipe_modules/build/chromebook.py
@@ -3,22 +3,49 @@
 # found in the LICENSE file.
 
 from . import util
+import os
+
+IMAGES = {
+    # Used to build ChromeOS for Pixelbook in Debian9, to align GLIBC versions.
+    'Debian9': (
+        'gcr.io/skia-public/debian9@sha256:'
+        'f48edc56b621a44ee2f985555f9b09c2802da15d56bb4e43666faabbec80d569'),
+}
 
 def compile_fn(api, checkout_root, out_dir):
   skia_dir      = checkout_root.join('skia')
   configuration = api.vars.builder_cfg.get('configuration')
   target_arch   = api.vars.builder_cfg.get('target_arch')
+  os_name = api.vars.builder_cfg.get('os', '')
+  builder_name = api.vars.builder_name
 
-  clang_linux = api.vars.slave_dir.join('clang_linux')
+  # Currently we mount this directory to /SRC in Docker.
+  # TODO(westont): Add docker module API to match binding to actual structure.
+  top_level = '/SRC' if 'Docker' in builder_name else str(api.vars.slave_dir)
+
+  clang_linux = os.path.join(top_level, 'clang_linux')
   # This is a pretty typical arm-linux-gnueabihf sysroot
-  sysroot_dir = api.vars.slave_dir.join('armhf_sysroot')
+  sysroot_dir = os.path.join(top_level, 'armhf_sysroot')
+
+  args = {
+    'cc': "%s" % os.path.join(clang_linux, 'bin','clang'),
+    'cxx': "%s" % os.path.join(clang_linux, 'bin','clang++'),
+    'extra_cflags' : [],
+    'extra_ldflags' : [],
+    'extra_asmflags' : [],
+    'target_cpu': target_arch,
+    'skia_use_fontconfig': False,
+    'skia_use_system_freetype2': False,
+    'skia_use_egl': True,
+    'werror': True,
+  }
 
   if 'arm' == target_arch:
     # This is the extra things needed to link against for the chromebook.
     #  For example, the Mali GL drivers.
-    gl_dir = api.vars.slave_dir.join('chromebook_arm_gles')
-    env = {'LD_LIBRARY_PATH': sysroot_dir.join('lib')}
-    extra_asmflags = [
+    gl_dir = os.path.join(top_level, 'chromebook_arm_gles')
+    env = {'LD_LIBRARY_PATH': os.path.join(sysroot_dir, 'lib')}
+    args['extra_asmflags'] = [
       '--target=armv7a-linux-gnueabihf',
       '--sysroot=%s' % sysroot_dir,
       '-march=armv7-a',
@@ -26,71 +53,67 @@ def compile_fn(api, checkout_root, out_dir):
       '-mthumb',
     ]
 
-    extra_cflags = [
+    args['extra_cflags'] = [
       '--target=armv7a-linux-gnueabihf',
       '--sysroot=%s' % sysroot_dir,
-      '-I%s' % gl_dir.join('include'),
-      '-I%s' % sysroot_dir.join('include'),
-      '-I%s' % sysroot_dir.join('include', 'c++', '6'),
-      '-I%s' % sysroot_dir.join('include', 'c++', '6', 'arm-linux-gnueabihf'),
+      '-I%s' % os.path.join(gl_dir, 'include'),
+      '-I%s' % os.path.join(sysroot_dir, 'include'),
+      '-I%s' % os.path.join(sysroot_dir, 'include', 'c++', '6'),
+      '-I%s' % os.path.join(sysroot_dir, 'include', 'c++', '6', 'arm-linux-gnueabihf'),
       '-DMESA_EGL_NO_X11_HEADERS',
       '-U_GLIBCXX_DEBUG',
     ]
 
-    extra_ldflags = [
+    args['extra_ldflags'] = [
       '--target=armv7a-linux-gnueabihf',
       '--sysroot=%s' % sysroot_dir,
       '-static-libstdc++', '-static-libgcc',
       # use sysroot's ld which can properly link things.
-      '-B%s' % sysroot_dir.join('bin'),
+      '-B%s' % os.path.join(sysroot_dir, 'bin'),
       # helps locate crt*.o
-      '-B%s' % sysroot_dir.join('gcc-cross'),
+      '-B%s' % os.path.join(sysroot_dir, 'gcc-cross'),
       # helps locate libgcc*.so
-      '-L%s' % sysroot_dir.join('gcc-cross'),
-      '-L%s' % sysroot_dir.join('lib'),
-      '-L%s' % gl_dir.join('lib'),
+      '-L%s' % os.path.join(sysroot_dir, 'gcc-cross'),
+      '-L%s' % os.path.join(sysroot_dir, 'lib'),
+      '-L%s' % os.path.join(gl_dir, 'lib'),
     ]
   else:
-    gl_dir = api.vars.slave_dir.join('chromebook_x86_64_gles')
+    gl_dir = os.path.join(top_level,'chromebook_x86_64_gles')
     env = {}
-    extra_asmflags = []
-    extra_cflags = [
+    args['extra_asmflags'] = []
+    args['extra_cflags'] = [
       '-DMESA_EGL_NO_X11_HEADERS',
-      '-I%s' % gl_dir.join('include'),
+      '-I%s' % os.path.join(gl_dir, 'include'),
     ]
-    extra_ldflags = [
-      '-L%s' % gl_dir.join('lib'),
+    args['extra_ldflags'] = [
+      '-L%s' % os.path.join(gl_dir, 'lib'),
       '-static-libstdc++', '-static-libgcc',
       '-fuse-ld=lld',
     ]
 
-  quote = lambda x: '"%s"' % x
-  args = {
-    'cc': quote(clang_linux.join('bin','clang')),
-    'cxx': quote(clang_linux.join('bin','clang++')),
-    'target_cpu': quote(target_arch),
-    'skia_use_fontconfig': 'false',
-    'skia_use_system_freetype2': 'false',
-    'skia_use_egl': 'true',
-    'werror': 'true',
-  }
-  extra_cflags.append('-DDUMMY_clang_linux_version=%s' %
+  args['extra_cflags'].append('-DDUMMY_clang_linux_version=%s' %
                       api.run.asset_version('clang_linux', skia_dir))
 
   if configuration != 'Debug':
-    args['is_debug'] = 'false'
-  args['extra_asmflags'] = repr(extra_asmflags).replace("'", '"')
-  args['extra_cflags'] = repr(extra_cflags).replace("'", '"')
-  args['extra_ldflags'] = repr(extra_ldflags).replace("'", '"')
+    args['is_debug'] = False
 
-  gn_args = ' '.join('%s=%s' % (k,v) for (k,v) in sorted(args.iteritems()))
   gn = skia_dir.join('bin', 'gn')
+
+  if os_name == 'Debian9' and 'Docker' in builder_name:
+    script = api.build.resource('docker-chromeos-compile.sh')
+    image_hash = IMAGES[os_name]
+    # Invalidate incremental build cache if image changes.
+    args['extra_cflags'].append('-DDUMMY_docker_image=%s' % image_hash)
+    api.docker.run('Run build script in Docker', image_hash,
+                   checkout_root, out_dir, script, args=[util.py_to_gn(args)])
+    return
 
   with api.context(cwd=skia_dir, env=env):
     api.run(api.python, 'fetch-gn',
             script=skia_dir.join('bin', 'fetch-gn'),
             infra_step=True)
-    api.run(api.step, 'gn gen', cmd=[gn, 'gen', out_dir, '--args=' + gn_args])
+    api.run(api.step, 'gn gen',
+            cmd=[gn, 'gen', out_dir, '--args=' + util.py_to_gn(args)])
     api.run(api.step, 'ninja',
             cmd=['ninja', '-C', out_dir, 'nanobench', 'dm'])
 
