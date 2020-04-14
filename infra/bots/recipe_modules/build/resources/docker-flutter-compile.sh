548a4297a535e9ab237a76a0caa48d66b49a0b3c
Weston Tracey <westont@google.com>
2020-04-14 12:55:12 -0400

    Add Deb9 Flutter_Android Docker job, see how far it gets before iterating on recipes.
    Bug: skia:9438
    Change-Id: I60c62425f21214b4ae6a810a787418a8975d204e

---
+++ infra/bots/recipe_modules/build/resources/docker-flutter-compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This assumes it is being run inside a docker container with the following
+# mounts:
+#  /SRC: Swarming start dir
+#  /OUT: output directory for gn and ninja
+
+set -e
+set -x
+
+export PATH="/SRC/recipe_bundle/depot_tools:${PATH}"
+
+cd /mnt/pd0/s/w/ir/cache/work/flutter/src
+flutter/tools/gn "$@"
+pwd
+ls ../.. -R
+#ls gen/third_party/dart/runtime
+#cat gen/third_party/dart/runtime/version.cc
+# This is the same as /OUT, but necessary since GN sets a command to do a
+# version check via relative path (../../flutter/third_party/gn/gn)
+ninja -v -C /mnt/pd0/s/w/ir/cache/work/flutter/src/out/android_release  -j100
