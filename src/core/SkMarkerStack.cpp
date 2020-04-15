03e20ffa9ae14264015b7d69c6fd669344f1d0f2
Brian Osman <brianosman@google.com>
2020-04-15 13:34:19 -0400

    Cache inverse matrices in SkMarkerStack
    Change-Id: I8dc74feaee91e99c294348b8092f5839fa9fa693
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/283676
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/core/SkMarkerStack.cpp
+++ src/core/SkMarkerStack.cpp
@@ -8,28 +8,46 @@
 #include "src/core/SkMarkerStack.h"
 
 void SkMarkerStack::setMarker(uint32_t id, const SkM44& mx, void* boundary) {
+    // We compute and cache the inverse here. Most clients are only interested in that, and we'll
+    // be fetching matrices from this far more often than we insert them.
+    SkM44 inv;
+    SkAssertResult(mx.invert(&inv));
+
     // Look if we've already seen id in this save-frame.
     // If so, replace, else append
-    for (int i = fStack.size() - 1; i >= 0; --i) {
-        auto& m = fStack[i];
-        if (m.fBoundary != boundary) {   // we've gone past the save-frame
-            break;                      // fall out so we append
+    for (auto it = fStack.rbegin(); it != fStack.rend(); ++it) {
+        if (it->fBoundary != boundary) {   // we've gone past the save-frame
+            break;                         // fall out so we append
         }
-        if (m.fID == id) {    // in current frame, so replace
-            m.fMatrix = mx;
+        if (it->fID == id) {    // in current frame, so replace
+            it->fMatrix = mx;
+            it->fMatrixInverse = inv;
             return;
         }
     }
     // if we get here, we should append a new marker
-    fStack.push_back({boundary, mx, id});
+    fStack.push_back({boundary, mx, inv, id});
 }
 
 bool SkMarkerStack::findMarker(uint32_t id, SkM44* mx) const {
     // search from top to bottom, so we find the most recent id
-    for (int i = fStack.size() - 1; i >= 0; --i) {
-        if (fStack[i].fID == id) {
+    for (auto it = fStack.rbegin(); it != fStack.rend(); ++it) {
+        if (it->fID == id) {
             if (mx) {
-                *mx = fStack[i].fMatrix;
+                *mx = it->fMatrix;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+bool SkMarkerStack::findMarkerInverse(uint32_t id, SkM44* mx) const {
+    // search from top to bottom, so we find the most recent id
+    for (auto it = fStack.rbegin(); it != fStack.rend(); ++it) {
+        if (it->fID == id) {
+            if (mx) {
+                *mx = it->fMatrixInverse;
             }
             return true;
         }
--- src/core/SkMarkerStack.h
+++ src/core/SkMarkerStack.h
@@ -19,12 +19,14 @@ public:
 
     void setMarker(uint32_t id, const SkM44& mx, void* boundary);
     bool findMarker(uint32_t id, SkM44* mx) const;
+    bool findMarkerInverse(uint32_t id, SkM44* mx) const;
     void restore(void* boundary);
 
 private:
     struct Rec {
         void*       fBoundary;
         SkM44       fMatrix;
+        SkM44       fMatrixInverse;
         uint32_t    fID;
     };
     std::vector<Rec> fStack;
