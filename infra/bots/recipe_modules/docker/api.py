57980e225b1c061b7232945acb1619363a877728
Weston Tracey <westont@google.com>
2020-04-15 13:47:27 -0400

    which ninja tho which ninja??
    Add Deb9 Flutter_Android Docker job, see how far it gets before iterating on recipes.
    
    Bug: skia:9438
    Change-Id: I60c62425f21214b4ae6a810a787418a8975d204e

--- infra/bots/recipe_modules/docker/api.py
+++ infra/bots/recipe_modules/docker/api.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 
+import os
 import posixpath
 from recipe_engine import recipe_api
 
@@ -62,7 +63,8 @@ class DockerApi(recipe_api.RecipeApi):
 
     # Run.
     cmd = [
-      'docker', 'run', '--shm-size=2gb', '--rm',
+      'docker', 'run', '--shm-size=2gb', '--rm', '--user',
+      '%d:%d' % (os.getuid(), os.getgid()),
       '--mount', 'type=bind,source=%s,target=%s' %
                  (src_dir, src_dir if match_directory_structure else MOUNT_SRC),
       '--mount', 'type=bind,source=%s,target=%s' %
