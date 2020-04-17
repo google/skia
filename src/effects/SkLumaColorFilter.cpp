bf8494848e64a0f2647b4d30b5682c3a20574888
Mike Reed <reed@google.com>
2020-04-17 09:51:57 -0400

    [sksl] move SkLumaColorFilter to pure sksl
    Change-Id: I09b08a1e0f39e77cfcf0e4767dbfb8558ca7fc13

--- src/effects/SkLumaColorFilter.cpp
+++ src/effects/SkLumaColorFilter.cpp
@@ -7,51 +7,26 @@
 
 #include "include/core/SkString.h"
 #include "include/effects/SkLumaColorFilter.h"
-#include "include/private/SkColorData.h"
-#include "src/core/SkEffectPriv.h"
-#include "src/core/SkRasterPipeline.h"
-#include "src/core/SkVM.h"
-
-#if SK_SUPPORT_GPU
-#include "include/gpu/GrContext.h"
-#include "src/gpu/effects/generated/GrLumaColorFilterEffect.h"
-#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
-#include "src/gpu/glsl/GrGLSLFragmentShaderBuilder.h"
-#endif
-
-bool SkLumaColorFilter::onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const {
-    rec.fPipeline->append(SkRasterPipeline::bt709_luminance_or_luma_to_alpha);
-    rec.fPipeline->append(SkRasterPipeline::clamp_0);
-    rec.fPipeline->append(SkRasterPipeline::clamp_1);
-    return true;
-}
-
-skvm::Color SkLumaColorFilter::onProgram(skvm::Builder* p, skvm::Color c,
-                                         SkColorSpace* dstCS,
-                                         skvm::Uniforms* uniforms, SkArenaAlloc* alloc) const {
-    return {
-        p->splat(0.0f),
-        p->splat(0.0f),
-        p->splat(0.0f),
-        clamp01(c.r * 0.2126f + c.g * 0.7152f + c.b * 0.0722f),
-    };
-}
+#include "include/effects/SkRuntimeEffect.h"
 
 sk_sp<SkColorFilter> SkLumaColorFilter::Make() {
-    return sk_sp<SkColorFilter>(new SkLumaColorFilter);
+    auto [effect, err] = SkRuntimeEffect::Make(SkString(R"(
+        void main(inout half4 c) {
+            c.a = clamp(c.r * 0.2126 + c.g * 0.7152 + c.b * 0.0722, 0, 1);
+            c.r = c.g = c.b = 0;
+        }
+    )"));
+    if (!effect) { SkDebugf("luma error %s\n", err.c_str()); }
+    return effect ? effect->makeColorFilter(nullptr) : nullptr;
 }
 
-SkLumaColorFilter::SkLumaColorFilter() : INHERITED() {}
+// SkLumaColorFilter no longer serialized 4/17/2020
 
-sk_sp<SkFlattenable> SkLumaColorFilter::CreateProc(SkReadBuffer&) {
-    return Make();
+#include "src/core/SkColorFilterPriv.h"
+
+void SkColorFilterPriv::RegisterLegacyLuma() {
+    SkFlattenable::Register("SkLumaColorFilter",
+                            [](SkReadBuffer& buffer) -> sk_sp<SkFlattenable> {
+        return SkLumaColorFilter::Make();
+    });
 }
-
-void SkLumaColorFilter::flatten(SkWriteBuffer&) const {}
-
-#if SK_SUPPORT_GPU
-std::unique_ptr<GrFragmentProcessor> SkLumaColorFilter::asFragmentProcessor(
-        GrRecordingContext*, const GrColorInfo&) const {
-    return GrLumaColorFilterEffect::Make();
-}
-#endif
