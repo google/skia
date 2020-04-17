7b51eefd38bed4ec0fc4548a79acf20f7dab453c
Julia Lavrova <jlavrova@google.com>
2020-04-17 15:08:09 -0400

    Small bugs in tests
    Placeholders should not be taken in MinIntrinsicWidth.
    Placeholders should allow Inf in some style values (as weird as it
    sounds)
    Bugs: skia:10138, skia:10159
    Change-Id: I3ad3b17f82fa93b0d3952e35ab658b7207179574
    
    Change-Id: I6ecc57b6ce778faf84b4d5752d24552b12c69fdb

--- modules/skparagraph/src/ParagraphImpl.cpp
+++ modules/skparagraph/src/ParagraphImpl.cpp
@@ -24,7 +24,7 @@ using ICUBiDi  = std::unique_ptr<UBiDi, SkFunctionWrapper<decltype(ubidi_close),
 
 SkScalar littleRound(SkScalar a) {
     // This rounding is done to match Flutter tests. Must be removed..
-  return SkScalarRoundToScalar(a * 100.0)/100.0;
+    return SkScalarRoundToScalar(a * 100.0)/100.0;
 }
 
 /** Replaces invalid utf-8 sequences with REPLACEMENT CHARACTER U+FFFD. */
@@ -214,6 +214,7 @@ void ParagraphImpl::layout(SkScalar rawWidth) {
         (fParagraphStyle.unlimited_lines() && fParagraphStyle.ellipsized())) {
         fMinIntrinsicWidth = fMaxIntrinsicWidth;
     }
+
     //SkDebugf("layout('%s', %f): %f %f\n", fText.c_str(), rawWidth, fMinIntrinsicWidth, fMaxIntrinsicWidth);
 }
 
@@ -853,20 +854,25 @@ std::vector<TextBox> ParagraphImpl::getRectsForPlaceholders() {
   if (fText.isEmpty()) {
        return boxes;
   }
+  if (fPlaceholders.size() == 1) {
+       // We always have one fake placeholder
+       return boxes;
+  }
   for (auto& line : fLines) {
       line.iterateThroughVisualRuns(
               true,
               [&boxes, &line](const Run* run, SkScalar runOffset, TextRange textRange,
                               SkScalar* width) {
-                  auto context =
-                          line.measureTextInsideOneRun(textRange, run, runOffset, 0, true, false);
+                  auto context = line.measureTextInsideOneRun(textRange, run, runOffset, 0, true, false);
                   *width = context.clip.width();
-                  if (run->placeholderStyle() == nullptr) {
+
+                  if (textRange.width() == 0) {
                       return true;
                   }
-                  if (run->textRange().width() == 0) {
+                  if (!run->isPlaceholder()) {
                       return true;
                   }
+
                   SkRect clip = context.clip;
                   clip.offset(line.offset());
 
@@ -879,7 +885,17 @@ std::vector<TextBox> ParagraphImpl::getRectsForPlaceholders() {
                   return true;
               });
   }
-
+  /*
+  SkDebugf("getRectsForPlaceholders('%s'): %d\n", fText.c_str(), boxes.size());
+  for (auto& r : boxes) {
+      r.rect.fLeft = littleRound(r.rect.fLeft);
+      r.rect.fRight = littleRound(r.rect.fRight);
+      r.rect.fTop = littleRound(r.rect.fTop);
+      r.rect.fBottom = littleRound(r.rect.fBottom);
+      SkDebugf("[%f:%f * %f:%f] %s\n", r.rect.fLeft, r.rect.fRight, r.rect.fTop, r.rect.fBottom,
+               (r.direction == TextDirection::kLtr ? "left" : "right"));
+  }
+  */
   return boxes;
 }
 
