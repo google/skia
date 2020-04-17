4a8aef2c3a5d9b88fe67ce89585be108a711ffe1
Brian Salomon <bsalomon@google.com>
2020-04-17 11:36:00 -0400

    Disable testing of D3D9 ANGLE backend on NVIDIA
    Bug: skia:10149
    
    Change-Id: Ia9b0bf2ed2042ba75088409612f6b091c5052ea9

--- tools/gpu/GrContextFactory.cpp
+++ tools/gpu/GrContextFactory.cpp
@@ -181,6 +181,16 @@ ContextInfo GrContextFactory::getContextInfoInternal(ContextType type, ContextOv
                 case kANGLE_D3D9_ES2_ContextType:
                     glCtx = MakeANGLETestContext(ANGLEBackend::kD3D9, ANGLEContextVersion::kES2,
                                                  glShareContext).release();
+                    // Chrome will only run on D3D9 with NVIDIA for 2012 and earlier drivers.
+                    // (<= 269.73). We get shader link failures when testing on recent drivers
+                    // using this backend.
+                    if (glCtx) {
+                        auto [backend, vendor, renderer] = GrGLGetANGLEInfo(glCtx->gl());
+                        if (vendor == GrGLANGLEVendor::kNVIDIA) {
+                            delete glCtx;
+                            return ContextInfo();
+                        }
+                    }
                     break;
                 case kANGLE_D3D11_ES2_ContextType:
                     glCtx = MakeANGLETestContext(ANGLEBackend::kD3D11, ANGLEContextVersion::kES2,
