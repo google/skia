1bf423c9e8a3f4305050187e164f28b73caf9e49
Mike Reed <reed@google.com>
2020-04-17 10:17:32 -0400

    [sksl] move SkLumaColorFilter to pure sksl
    Don't land until we can rely on a JIT for clients that care about
    raster performance.
    
    Change-Id: I09b08a1e0f39e77cfcf0e4767dbfb8558ca7fc13

--- include/effects/SkLumaColorFilter.h
+++ include/effects/SkLumaColorFilter.h
@@ -9,9 +9,6 @@
 #define SkLumaColorFilter_DEFINED
 
 #include "include/core/SkColorFilter.h"
-#include "include/core/SkRefCnt.h"
-
-class SkRasterPipeline;
 
 /**
  *  SkLumaColorFilter multiplies the luma of its input into the alpha channel,
@@ -30,30 +27,9 @@ class SkRasterPipeline;
  *  not luminance, a dot-product of linear color channels.  So at least
  *  SkLumaColorFilter and feColorMatrix+luminanceToAlpha agree there.)
  */
-
- #include "include/core/SkFlattenable.h"
-
-class SK_API SkLumaColorFilter : public SkColorFilter {
+class SK_API SkLumaColorFilter {
 public:
     static sk_sp<SkColorFilter> Make();
-
-#if SK_SUPPORT_GPU
-    std::unique_ptr<GrFragmentProcessor> asFragmentProcessor(GrRecordingContext*,
-                                                             const GrColorInfo&) const override;
-#endif
-
-protected:
-    void flatten(SkWriteBuffer&) const override;
-
-private:
-    SK_FLATTENABLE_HOOKS(SkLumaColorFilter)
-
-    SkLumaColorFilter();
-    bool onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const override;
-    skvm::Color onProgram(skvm::Builder*, skvm::Color,
-                          SkColorSpace* dstCS, skvm::Uniforms*, SkArenaAlloc*) const override;
-
-    typedef SkColorFilter INHERITED;
 };
 
 #endif
