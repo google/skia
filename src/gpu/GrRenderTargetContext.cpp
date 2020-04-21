777f239a65a9ab0147ef5b81cc3b4c042b3f00d5
Brian Salomon <bsalomon@google.com>
2020-04-21 16:38:05 +0000

    Revert "Notify RTC when OpsTask is closed so it can drop ownership"
    This reverts commit 198393b2de5d9632a84773034485cf486d57d3db.
    
    Reason for revert: android crash
    
    Original change's description:
    > Notify RTC when OpsTask is closed so it can drop ownership
    > 
    > Change-Id: I95d32ed89447995541f33bf80730876ce9c0747a
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/284519
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    
    Change-Id: I5133fa1b8f90182864ffbee3b60bfd5781dc16bd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/284728
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>

--- src/gpu/GrRenderTargetContext.cpp
+++ src/gpu/GrRenderTargetContext.cpp
@@ -398,9 +398,6 @@ void GrRenderTargetContext::onValidate() const {
 
 GrRenderTargetContext::~GrRenderTargetContext() {
     ASSERT_SINGLE_OWNER
-    if (fOpsTask) {
-        fOpsTask->setClosedObserver(nullptr);
-    }
 }
 
 inline GrAAType GrRenderTargetContext::chooseAAType(GrAA aa) {
@@ -426,7 +423,7 @@ GrOpsTask* GrRenderTargetContext::getOpsTask() {
     ASSERT_SINGLE_OWNER
     SkDEBUGCODE(this->validate();)
 
-    if (!fOpsTask) {
+    if (!fOpsTask || fOpsTask->isClosed()) {
         sk_sp<GrOpsTask> newOpsTask =
                 this->drawingManager()->newOpsTask(this->writeSurfaceView(), fManagedOpsTask);
         if (fOpsTask && fNumStencilSamples > 0) {
@@ -437,10 +434,9 @@ GrOpsTask* GrRenderTargetContext::getOpsTask() {
             // values?
             newOpsTask->setInitialStencilContent(GrOpsTask::StencilContent::kPreserved);
         }
-        newOpsTask->setClosedObserver(this);
         fOpsTask = std::move(newOpsTask);
     }
-    SkASSERT(!fOpsTask->isClosed());
+
     return fOpsTask.get();
 }
 
@@ -2657,9 +2653,4 @@ bool GrRenderTargetContext::blitTexture(GrSurfaceProxyView view, const SkIRect& 
                              clippedSrcRect.height()),
             SkRect::Make(clippedSrcRect));
     return true;
-}
-
-void GrRenderTargetContext::wasClosed(const GrOpsTask& task) {
-    SkASSERT(&task == fOpsTask.get());
-    fOpsTask.reset();
 }
