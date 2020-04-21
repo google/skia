fc265cf3aa84fcde0c1255d0510deefc4655862d
Brian Osman <brianosman@google.com>
2020-04-21 15:43:47 -0400

    Remove SkCanvasMatrix
    Makes it more explicit when we're calling asM33 and dropping Z.
    The IsScaleTranslate helper could have been static in SkCanvas,
    but we're probably going to need it when we start pushing SkM44
    down to SkDevice.
    
    Change-Id: Ia013c7f59cdbac78b5a04fdcaafb62a0a626cb53

--- src/core/SkDevice.cpp
+++ src/core/SkDevice.cpp
@@ -15,7 +15,6 @@
 #include "include/core/SkShader.h"
 #include "include/core/SkVertices.h"
 #include "include/private/SkTo.h"
-#include "src/core/SkCanvasMatrix.h"
 #include "src/core/SkDraw.h"
 #include "src/core/SkGlyphRun.h"
 #include "src/core/SkImageFilterCache.h"
@@ -58,8 +57,8 @@ void SkBaseDevice::setDeviceCoordinateSystem(const SkMatrix& deviceToGlobal,
     }
 }
 
-void SkBaseDevice::setGlobalCTM(const SkCanvasMatrix& ctm) {
-    fLocalToDevice = ctm;
+void SkBaseDevice::setGlobalCTM(const SkM44& ctm) {
+    fLocalToDevice = ctm.asM33();
     fLocalToDevice.normalizePerspective();
     if (!fGlobalToDevice.isIdentity()) {
         // Map from the global CTM state to this device's coordinate system.
@@ -97,7 +96,8 @@ bool SkBaseDevice::getLocalToMarker(uint32_t id, SkM44* localToMarker) const {
     SkM44 globalToMarker;
     if (fMarkerStack && fMarkerStack->findMarkerInverse(id, &globalToMarker)) {
         if (localToMarker) {
-            *localToMarker = globalToMarker * SkMatrix::Concat(fDeviceToGlobal, fLocalToDevice);
+            *localToMarker =
+                    globalToMarker * SkM44(SkMatrix::Concat(fDeviceToGlobal, fLocalToDevice));
         }
         return true;
     }
