9d5fac80a18e9d24a8c5dbe15181cab55604208f
Robert Phillips <robertphillips@google.com>
2020-04-22 08:28:58 -0400

    Allow SkThreadPool to be non-borrowing
    The GPU thread has privileged access to the GPU so its work can't be easily borrowed.
    
    Change-Id: I1eae4c86ff1c36cc1248f74fc48d76b1c243f0b2

--- include/core/SkExecutor.h
+++ include/core/SkExecutor.h
@@ -17,8 +17,10 @@ public:
     virtual ~SkExecutor();
 
     // Create a thread pool SkExecutor with a fixed thread count, by default the number of cores.
-    static std::unique_ptr<SkExecutor> MakeFIFOThreadPool(int threads = 0);
-    static std::unique_ptr<SkExecutor> MakeLIFOThreadPool(int threads = 0);
+    static std::unique_ptr<SkExecutor> MakeFIFOThreadPool(int threads = 0,
+                                                          bool allowBorrowing = true);
+    static std::unique_ptr<SkExecutor> MakeLIFOThreadPool(int threads = 0,
+                                                          bool allowBorrowing = true);
 
     // There is always a default SkExecutor available by calling SkExecutor::GetDefault().
     static SkExecutor& GetDefault();
