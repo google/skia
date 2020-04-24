d5cba9d0005500c245f19eb4e269145244902792
Mike Klein <mtklein@google.com>
2020-04-24 07:44:50 -0500

    add SkColorSpace param to SkPixmap::erase()
    We don't have a way to erase with a non-sRGB color.
    
    Update the P3 gm to test this, removing the SkBitmap erase case where
    there's no option for even an SkColor4f, let alone non-sRGB.  I'm not
    sure it's really important to have one when we've got this on pixmap.
    
    Updated release notes.
    
    Change-Id: Ie98270d3f83e041593b4c6b2da8e325b36d4ff18
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/285341
    Commit-Queue: Mike Klein <mtklein@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- gm/p3.cpp
+++ gm/p3.cpp
@@ -152,21 +152,6 @@ DEF_SIMPLE_GM(p3, canvas, 450, 1300) {
 
     canvas->translate(0,80);
 
-    // Draw a P3 red bitmap, using SkBitmap::eraseColor().
-    {
-        SkBitmap bm;
-        bm.allocPixels(SkImageInfo::Make(60,60, kRGBA_F16_SkColorType, kPremul_SkAlphaType, p3));
-
-        bm.eraseColor(0xffff0000/*in P3*/);
-
-        canvas->drawBitmap(bm, 10,10);
-        compare_pixel("drawBitmap P3 red, from SkBitmap::eraseColor()",
-                      canvas, 10,10,
-                      {1,0,0,1}, p3.get());
-    }
-
-    canvas->translate(0,80);
-
     // Draw a P3 red bitmap, using SkPixmap::erase().
     {
         SkBitmap bm;
@@ -175,7 +160,7 @@ DEF_SIMPLE_GM(p3, canvas, 450, 1300) {
         // At the moment only SkPixmap has an erase() that takes an SkColor4f.
         SkPixmap pm;
         SkAssertResult(bm.peekPixels(&pm));
-        SkAssertResult(pm.erase({1,0,0,1} /*in p3*/));
+        SkAssertResult(pm.erase({1,0,0,1}, p3.get()));
 
         canvas->drawBitmap(bm, 10,10);
         compare_pixel("drawBitmap P3 red, from SkPixmap::erase",
@@ -193,7 +178,7 @@ DEF_SIMPLE_GM(p3, canvas, 450, 1300) {
         // At the moment only SkPixmap has an erase() that takes an SkColor4f.
         SkPixmap pm;
         SkAssertResult(bm.peekPixels(&pm));
-        SkAssertResult(pm.erase({1,0,0,1} /*in p3*/));
+        SkAssertResult(pm.erase({1,0,0,1}, p3.get()));
 
         SkPaint paint;
         paint.setShader(bm.makeShader(SkTileMode::kRepeat, SkTileMode::kRepeat));
