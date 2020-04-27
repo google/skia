8f60b0dbea8e6054e0a6ef2fb850cbc0fcf3e954
Kevin Lubick <kjlubick@google.com>
2020-04-27 13:06:49 -0400

    [infra] Add POC task driver
    Change-Id: I0f8972b62c938389772872adea2c96acc29ca503

---
+++ infra/bots/task_drivers/hello-world/hello-world.go
@@ -0,0 +1,14 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Hello world\ncalled with %q\n", os.Args)
+}
