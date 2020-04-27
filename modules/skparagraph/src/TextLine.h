94202a8b49bf3a5d26a6f62431fda2d0e5c8149b
Julia Lavrova <jlavrova@google.com>
2020-04-27 15:49:53 -0400

    Hide more of leftToRight checks
    Moving some code down from paragraph to line.
    Change-Id: I74010cb0c07f3b4c087f0f5c338588d93b3bd220
    
    Change-Id: I9408951fe8d05a5956e4bbe4b50c9ef3f3dc1f9c

--- modules/skparagraph/src/TextLine.h
+++ modules/skparagraph/src/TextLine.h
@@ -84,6 +84,11 @@ public:
     void setMaxRunMetrics(const InternalLineMetrics& metrics) { fMaxRunMetrics = metrics; }
     InternalLineMetrics getMaxRunMetrics() const { return fMaxRunMetrics; }
 
+    bool isFirstLine();
+    bool isLastLine();
+    std::vector<TextBox> getRectsForRange(TextRange textRange, RectHeightStyle rectHeightStyle, RectWidthStyle rectWidthStyle);
+    PositionWithAffinity getGlyphPositionAtCoordinate(SkScalar dx);
+
     ClipContext measureTextInsideOneRun(TextRange textRange,
                                         const Run* run,
                                         SkScalar runOffsetInLine,
