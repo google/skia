615d3795a6e8f9be8183b6630771bf0d7f3ee007
Chris Dalton <csmartdalton@google.com>
2020-05-04 16:06:12 -0600

    Hide SkContourMeasure's usage of RawIter behind an "Impl" class
    Change-Id: I33c432022b7d16faa0a3e57fdddf0b1dedccc5c2

--- include/core/SkContourMeasure.h
+++ include/core/SkContourMeasure.h
@@ -120,29 +120,9 @@ public:
     sk_sp<SkContourMeasure> next();
 
 private:
-    SkPath::RawIter fIter;
-    SkPath          fPath;
-    SkScalar        fTolerance;
-    bool            fForceClosed;
+    class Impl;
 
-    // temporary
-    SkTDArray<SkContourMeasure::Segment>  fSegments;
-    SkTDArray<SkPoint>  fPts; // Points used to define the segments
-
-    SkContourMeasure* buildSegments();
-
-    SkScalar compute_line_seg(SkPoint p0, SkPoint p1, SkScalar distance, unsigned ptIndex);
-    SkScalar compute_quad_segs(const SkPoint pts[3], SkScalar distance,
-                               int mint, int maxt, unsigned ptIndex);
-    SkScalar compute_conic_segs(const SkConic& conic, SkScalar distance,
-                                                         int mint, const SkPoint& minPt,
-                                                         int maxt, const SkPoint& maxPt,
-                                unsigned ptIndex);
-    SkScalar compute_cubic_segs(const SkPoint pts[4], SkScalar distance,
-                                int mint, int maxt, unsigned ptIndex);
-
-    SkContourMeasureIter(const SkContourMeasureIter&) = delete;
-    SkContourMeasureIter& operator=(const SkContourMeasureIter&) = delete;
+    std::unique_ptr<Impl> fImpl;
 };
 
 #endif
