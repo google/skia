13dd35d48d58f417a3357f4a96cc7a7d6ea06c41
Robert Phillips <robertphillips@google.com>
2020-05-04 09:13:29 -0400

    Move program info creation into GrSimpleMeshDrawOpHelper
    Change-Id: I39ac7aa7823e7e987f73f2982e42d6fce935811b

--- src/gpu/ops/GrSimpleMeshDrawOpHelper.h
+++ src/gpu/ops/GrSimpleMeshDrawOpHelper.h
@@ -16,6 +16,8 @@
 #include "src/gpu/ops/GrMeshDrawOp.h"
 #include <new>
 
+#include "src/gpu/GrProgramInfo.h"
+
 struct SkRect;
 
 /**
@@ -108,8 +110,20 @@ public:
         friend class GrSimpleMeshDrawOpHelper;
     };
 
+    void createProgramInfo(const GrCaps* caps,
+                           SkArenaAlloc* arena,
+                           const GrSurfaceProxyView* dstView,
+                           GrAppliedClip&& appliedClip,
+                           const GrXferProcessor::DstProxyView& dstProxyView) {
+        SkASSERT(!fProgramInfo);
+
+//        fProgramInfo = arena->make<GrProgramInfo>();
+    }
+
     void visitProxies(const GrOp::VisitProxyFunc& func) const {
-        if (fProcessors) {
+        if (fProgramInfo) {
+            fProgramInfo->visitProxies(func);
+        } else if (fProcessors) {
             fProcessors->visitProxies(func);
         }
     }
@@ -175,12 +189,19 @@ public:
     }
 
     GrPipeline::InputFlags pipelineFlags() const { return fPipelineFlags; }
+    GrProcessorSet* processors() { return fProcessors; }
+
+protected:
 
 protected:
     GrProcessorSet::Analysis finalizeProcessors(
             const GrCaps& caps, const GrAppliedClip*, const GrUserStencilSettings*,
             bool hasMixedSampledCoverage, GrClampType, GrProcessorAnalysisCoverage geometryCoverage,
             GrProcessorAnalysisColor* geometryColor);
+
+    // If this op is prePrepared the created programInfo will be stored here for use in
+    // onExecute. In the prePrepared case it will have been stored in the record-time arena.
+    GrProgramInfo*  fProgramInfo = nullptr;
 
     GrProcessorSet* fProcessors;
     GrPipeline::InputFlags fPipelineFlags;
