3e45651fb99801e52b450e1179d54ddf0c57e251
Adlai Holler <adlai@google.com>
2020-05-06 16:03:37 -0400

    Reland "Support large kernels on GPU in matrix convolution effect"
    This reverts commit 41e377d1baf0dbf1355ec644a6fe40f6da48fc43.
    
    Reason for revert: fixed issues
    
    Bug: skia:8449
    Change-Id: I0c4389f0efa92c6da69253b2304ad9a072750965

--- src/core/SkGpuBlurUtils.cpp
+++ src/core/SkGpuBlurUtils.cpp
@@ -81,8 +81,9 @@ static std::unique_ptr<GrRenderTargetContext> convolve_gaussian_2d(GrRecordingCo
     SkIPoint kernelOffset = SkIPoint::Make(radiusX, radiusY);
     GrPaint paint;
     auto wm = SkTileModeToWrapMode(mode);
-    auto conv = GrMatrixConvolutionEffect::MakeGaussian(std::move(srcView), srcBounds, size, 1.0,
-                                                        0.0, kernelOffset, wm, true, sigmaX, sigmaY,
+    auto conv = GrMatrixConvolutionEffect::MakeGaussian(context, std::move(srcView), srcBounds,
+                                                        size, 1.0, 0.0, kernelOffset, wm, true,
+                                                        sigmaX, sigmaY,
                                                         *renderTargetContext->caps());
     paint.addColorFragmentProcessor(std::move(conv));
     paint.setPorterDuffXPFactory(SkBlendMode::kSrc);
@@ -390,7 +391,8 @@ std::unique_ptr<GrRenderTargetContext> GaussianBlur(GrRecordingContext* context,
         int radiusY = sigma_radius(sigmaY);
         // For really small blurs (certainly no wider than 5x5 on desktop GPUs) it is faster to just
         // launch a single non separable kernel vs two launches.
-        if (sigmaX > 0 && sigmaY > 0 && (2 * radiusX + 1) * (2 * radiusY + 1) <= MAX_KERNEL_SIZE) {
+        const int kernelSize = (2 * radiusX + 1) * (2 * radiusY + 1);
+        if (sigmaX > 0 && sigmaY > 0 && kernelSize <= GrMatrixConvolutionEffect::kMaxUniformSize) {
             // Apply the proxy offset to src bounds and offset directly
             return convolve_gaussian_2d(context, std::move(srcView), srcColorType, srcBounds,
                                         dstBounds, radiusX, radiusY, sigmaX, sigmaY, mode,
