491a91e084e461db612bfb32d4b57ba5e416b5d2
Florin Malita <fmalita@chromium.org>
2020-05-07 16:44:18 -0400

    [skottie] Cleanup: rename ImageLayer -> FootageLayer
    ...since it's used for both image and video Lottie layers.
    
    TBR=
    Change-Id: I52e85d70d4adbda61dfa3b33acdf4eb17ddbf332

--- modules/skottie/src/SkottiePriv.h
+++ modules/skottie/src/SkottiePriv.h
@@ -176,7 +176,7 @@ private:
 
     struct AttachLayerContext;
     struct AttachShapeContext;
-    struct ImageAssetInfo;
+    struct FootageAssetInfo;
     struct LayerInfo;
 
     void parseAssets(const skjson::ArrayValue*);
@@ -191,13 +191,13 @@ private:
     sk_sp<sksg::RenderNode> attachShape(const skjson::ArrayValue*, AttachShapeContext*) const;
     sk_sp<sksg::RenderNode> attachAssetRef(const skjson::ObjectValue&,
         const std::function<sk_sp<sksg::RenderNode>(const skjson::ObjectValue&)>&) const;
-    const ImageAssetInfo* loadImageAsset(const skjson::ObjectValue&) const;
-    sk_sp<sksg::RenderNode> attachImageAsset(const skjson::ObjectValue&, LayerInfo*) const;
+    const FootageAssetInfo* loadFootageAsset(const skjson::ObjectValue&) const;
+    sk_sp<sksg::RenderNode> attachFootageAsset(const skjson::ObjectValue&, LayerInfo*) const;
 
     sk_sp<sksg::RenderNode> attachExternalPrecompLayer(const skjson::ObjectValue&,
                                                        const LayerInfo&) const;
 
-    sk_sp<sksg::RenderNode> attachImageLayer  (const skjson::ObjectValue&, LayerInfo*) const;
+    sk_sp<sksg::RenderNode> attachFootageLayer(const skjson::ObjectValue&, LayerInfo*) const;
     sk_sp<sksg::RenderNode> attachNullLayer   (const skjson::ObjectValue&, LayerInfo*) const;
     sk_sp<sksg::RenderNode> attachPrecompLayer(const skjson::ObjectValue&, LayerInfo*) const;
     sk_sp<sksg::RenderNode> attachShapeLayer  (const skjson::ObjectValue&, LayerInfo*) const;
@@ -248,14 +248,14 @@ private:
         mutable bool               fIsAttaching; // Used for cycle detection
     };
 
-    struct ImageAssetInfo {
+    struct FootageAssetInfo {
         sk_sp<ImageAsset> fAsset;
         SkISize           fSize;
     };
 
-    SkTHashMap<SkString, AssetInfo>              fAssets;
-    SkTHashMap<SkString, FontInfo>               fFonts;
-    mutable SkTHashMap<SkString, ImageAssetInfo> fImageAssetCache;
+    SkTHashMap<SkString, AssetInfo>                fAssets;
+    SkTHashMap<SkString, FontInfo>                 fFonts;
+    mutable SkTHashMap<SkString, FootageAssetInfo> fImageAssetCache;
 
     using INHERITED = SkNoncopyable;
 };
