faae8aa7655ad38c536d82500581781dec7eeea6
Adlai Holler <adlai@google.com>
2020-05-07 21:20:16 -0400

    Reland "Support large kernels on GPU in matrix convolution effect"
    This reverts commit 41e377d1baf0dbf1355ec644a6fe40f6da48fc43.
    
    Reason for revert: fixed issues
    
    Bug: skia:8449
    Change-Id: I0c4389f0efa92c6da69253b2304ad9a072750965

--- gm/matrixconvolution.cpp
+++ gm/matrixconvolution.cpp
@@ -22,6 +22,7 @@
 #include "include/core/SkTypeface.h"
 #include "include/effects/SkGradientShader.h"
 #include "include/effects/SkImageFilters.h"
+#include "src/gpu/effects/GrMatrixConvolutionEffect.h"
 #include "tools/ToolUtils.h"
 
 #include <vector>
@@ -79,7 +80,7 @@ protected:
                 return SkImageFilters::MatrixConvolution({3,3}, kernel.data(), /* gain */ 0.3f, /* bias */ SkIntToScalar(100), kernelOffset, tileMode, convolveAlpha, nullptr, cropRect);
             }
             case kLarge_KernelFixture: {
-                // Intentionally go over the MAX_KERNEL_SIZE limit and trigger CPU fallback.
+                static_assert(49 > GrMatrixConvolutionEffect::kMaxUniformSize);
                 // All 1s except center value, which is -47 (sum of 1).
                 std::vector<SkScalar> kernel(49, SkIntToScalar(1));
                 kernel[24] = SkIntToScalar(-47);
