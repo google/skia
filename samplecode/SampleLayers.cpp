b5c5a49df5a5ed0e3911a55da4b4d9932ed5fda4
Mike Reed <reed@google.com>
2020-05-17 15:46:30 -0400

    SkCamera is gone, use SkM44 instead
    Change-Id: I991d74d977f0f2ccfd9c8540868f47d77fa982fe

--- samplecode/SampleLayers.cpp
+++ samplecode/SampleLayers.cpp
@@ -16,7 +16,6 @@
 #include "include/core/SkTime.h"
 #include "include/core/SkTypeface.h"
 #include "include/effects/SkGradientShader.h"
-#include "include/utils/SkCamera.h"
 #include "include/utils/SkInterpolator.h"
 #include "samplecode/Sample.h"
 #include "src/core/SkClipOpPriv.h"
