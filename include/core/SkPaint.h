69e2e7ce71eacc5a7808ad3696d01fa48cf22672
John Stiles <johnstiles@google.com>
2020-05-18 16:08:36 -0400

    Clarify doc comments to document meaning of 'hairline'.
    Change-Id: I51c39e2e644b8353afbdb8bdda32263796d7e951

--- include/core/SkPaint.h
+++ include/core/SkPaint.h
@@ -316,9 +316,10 @@ public:
     */
     SkScalar getStrokeWidth() const { return fWidth; }
 
-    /** Sets the thickness of the pen used by the paint to
-        outline the shape.
-        Has no effect if width is less than zero.
+    /** Sets the thickness of the pen used by the paint to outline the shape.
+        A stroke-width of zero is treated as "hairline" width. Hairlines are always exactly one
+        pixel wide in device space (they do not get thicker as the canvas scales).
+        Negative stroke-widths are invalid; setting a negative width will have no effect.
 
         @param width  zero thickness for hairline; greater than zero for pen thickness
 
