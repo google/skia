0f524936897ac3941373f003d52c239db9cf3612
Greg Daniel <egdaniel@google.com>
2020-05-20 13:10:41 -0400

    Add submittedProc to GrFlushInfo.
    Bug: skia:10118
    Change-Id: Iad848310d0f2fb22f19e9890209548fda103bd27

--- src/gpu/GrGpu.h
+++ src/gpu/GrGpu.h
@@ -843,6 +843,8 @@ private:
     void validateStagingBuffers() const;
 #endif
 
+    void callSubmittedProcs(bool success);
+
     uint32_t fResetBits;
     // The context owns us, not vice-versa, so this ptr is not ref'ed by Gpu.
     GrContext* fContext;
@@ -853,6 +855,15 @@ private:
     StagingBufferList                fAvailableStagingBuffers;
     StagingBufferList                fActiveStagingBuffers;
     StagingBufferList                fBusyStagingBuffers;
+
+    struct SubmittedProc {
+        SubmittedProc(GrGpuSubmittedProc proc, GrGpuSubmittedContext context)
+                : fProc(proc), fContext(context) {}
+
+        GrGpuSubmittedProc fProc;
+        GrGpuSubmittedContext fContext;
+    };
+    SkSTArray<4, SubmittedProc> fSubmittedProcs;
 
     friend class GrPathRendering;
     typedef SkRefCnt INHERITED;
