317dce5c81c04e20cc631e0b843311f78bb7fe15
Herb Derby <herb@google.com>
2020-05-21 14:46:06 -0400

    Drop SkTextBlobs with > 2M glyphs.
    This will guard against buffer overflows
    for large text blobs.
    
    Bug: chromium:1080481
    Change-Id: I13a10869babfa149a70c2f4caebb3a1ae4452b77
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/291456
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Mike Reed <reed@google.com>

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -2629,6 +2629,19 @@ void SkCanvas::drawTextBlob(const SkTextBlob* blob, SkScalar x, SkScalar y,
     TRACE_EVENT0("skia", TRACE_FUNC);
     RETURN_ON_NULL(blob);
     RETURN_ON_FALSE(blob->bounds().makeOffset(x, y).isFinite());
+
+    // Overflow if more than 2^21 glyphs stopping a buffer overflow latter in the stack.
+    // See chromium:1080481
+    // TODO: can consider unrolling a few at a time if this limit becomes a problem.
+    int totalGlyphCount = 0;
+    constexpr int kMaxGlyphCount = 1 << 21;
+    SkTextBlob::Iter i(*blob);
+    SkTextBlob::Iter::Run r;
+    while (i.next(&r)) {
+        int glyphsLeft = kMaxGlyphCount - totalGlyphCount;
+        RETURN_ON_FALSE(r.fGlyphCount <= glyphsLeft);
+        totalGlyphCount += r.fGlyphCount;
+    }
     this->onDrawTextBlob(blob, x, y, paint);
 }
 
