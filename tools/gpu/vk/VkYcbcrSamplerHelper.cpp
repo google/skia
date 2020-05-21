06f5691444144c4e1a40b7be176a1ae651c9e511
Robert Phillips <robertphillips@google.com>
2020-05-21 10:02:00 -0400

    Add ycbcrimage GM
    Change-Id: Ie51a38e73960f1bb4e209505b8b13d1d4144633d

---
+++ tools/gpu/vk/VkYcbcrSamplerHelper.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "tools/gpu/vk/VkYcbcrSamplerHelper.h"
+
+VkYcbcrSamplerTestHelper1::~VkYcbcrSamplerTestHelper1() {
+}
---
+++ tools/gpu/vk/VkYcbcrSamplerHelper.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VkYcbcrSamplerHelper_DEFINED
+#define VkYcbcrSamplerHelper_DEFINED
+
+#include "include/core/SkTypes.h"
+
+#ifdef SK_VULKAN
+
+class VkYcbcrSamplerTestHelper1 {
+public:
+    VkYcbcrSamplerTestHelper1() {}
+    ~VkYcbcrSamplerTestHelper1();
+};
+
+#endif // SK_VULKAN
+
+#endif // VkYcbcrSamplerHelper_DEFINED
