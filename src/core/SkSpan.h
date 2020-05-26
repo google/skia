1f382dce517d3ebf1ba28dfbd2322befdd3ed59c
Herb Derby <herb@google.com>
2020-05-26 12:58:59 -0400

    SkSpan cleanup
    Change-Id: I4a99796f1aa2551fb9721cc4ee2441d114ec249d

--- src/core/SkSpan.h
+++ src/core/SkSpan.h
@@ -19,41 +19,45 @@ public:
     template <typename U, typename = typename std::enable_if<std::is_same<const U, T>::value>::type>
     constexpr SkSpan(const SkSpan<U>& that) : fPtr(that.data()), fSize{that.size()} {}
     constexpr SkSpan(const SkSpan& o) = default;
+    template<size_t N> constexpr SkSpan(T(&a)[N]) : SkSpan{a, N} { }
+    template<typename Container>
+    constexpr SkSpan(Container& c) : SkSpan{skstd::data(c), skstd::size(c)} { }
+
     constexpr SkSpan& operator=(const SkSpan& that) {
         fPtr = that.fPtr;
         fSize = that.fSize;
         return *this;
     }
-    constexpr T& operator [] (size_t i) const { return fPtr[i]; }
+    constexpr T& operator [] (size_t i) const {
+        SkASSERT(i < this->size());
+        return fPtr[i];
+    }
     constexpr T& front() const { return fPtr[0]; }
     constexpr T& back()  const { return fPtr[fSize - 1]; }
     constexpr T* begin() const { return fPtr; }
     constexpr T* end() const { return fPtr + fSize; }
-    constexpr const T* cbegin() const { return fPtr; }
-    constexpr const T* cend() const { return fPtr + fSize; }
+    constexpr const T* cbegin() const { return this->begin(); }
+    constexpr const T* cend() const { return this->end(); }
     constexpr auto rbegin() const { return std::make_reverse_iterator(this->end()); }
     constexpr auto rend() const { return std::make_reverse_iterator(this->begin()); }
     constexpr auto crbegin() const { return std::make_reverse_iterator(this->cend()); }
     constexpr auto crend() const { return std::make_reverse_iterator(this->cbegin()); }
-    constexpr T* data() const { return fPtr; }
+    constexpr T* data() const { return this->begin(); }
     constexpr int count() const { return SkTo<int>(fSize); }
     constexpr size_t size() const { return fSize; }
     constexpr bool empty() const { return fSize == 0; }
     constexpr size_t size_bytes() const { return fSize * sizeof(T); }
     constexpr SkSpan<T> first(size_t prefixLen) const {
         SkASSERT(prefixLen <= this->size());
-        if (prefixLen == 0) { return SkSpan{}; }
         return SkSpan{fPtr, prefixLen};
     }
     constexpr SkSpan<T> last(size_t postfixLen) const {
         SkASSERT(postfixLen <= this->size());
-        if (postfixLen == 0) { return SkSpan{}; }
         return SkSpan{fPtr + (this->size() - postfixLen), postfixLen};
     }
     constexpr SkSpan<T> subspan(size_t offset, size_t count) const {
         SkASSERT(offset <= this->size());
         SkASSERT(count <= this->size() - offset);
-        if (count == 0) { return SkSpan{}; }
         return SkSpan{fPtr + offset, count};
     }
 
@@ -62,15 +66,8 @@ private:
     size_t fSize;
 };
 
-template <typename T, typename S>
-inline constexpr SkSpan<T> SkMakeSpan(T* p, S s) { return SkSpan<T>{p, SkTo<size_t>(s)}; }
+template<typename Container>
+SkSpan(Container&) -> SkSpan<std::remove_pointer_t<decltype(
+                                skstd::data(std::declval<Container&>()))>>;
 
-template <size_t N, typename T>
-inline constexpr SkSpan<T> SkMakeSpan(T(&a)[N]) { return SkSpan<T>{a, N}; }
-
-template <typename Container>
-inline auto SkMakeSpan(Container& c)
-        -> SkSpan<typename std::remove_reference<decltype(*(c.data()))>::type> {
-    return {c.data(), c.size()};
-}
 #endif  // SkSpan_DEFINED
