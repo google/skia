fe5f5c639493201310bbc481cff192208d69ee0b
Adlai Holler <adlai@google.com>
2020-05-26 12:22:31 -0400

    Support sharing promise images between DDLs
    - Move the promise image creation API from SkDDLRecorder to GrContext.
    - Have DDLTileHelper share one SKP and one set of promise images across all tiles.
    - Add a fuzzer that spams DDL creation and playback with shared promise images.
    - Remove an incorrect assert from SkDDL dtor. Rob confirmed it's outdated.
    - Update the documentation of our promise image creation callbacks API.
    
    Bug: skia:10286
    Change-Id: Ie35976958454fc520f3c9d860e6285441260c9f7

--- tools/DDLTileHelper.h
+++ tools/DDLTileHelper.h
@@ -42,7 +42,8 @@ public:
                                    const DDLPromiseImageHelper& helper);
 
         // Create the DDL for this tile (i.e., fill in 'fDisplayList').
-        void createDDL();
+        void createDDL(const sk_sp<SkPicture>&,
+                       const SkTArray<sk_sp<SkImage>>& promiseImages);
 
         // Precompile all the programs required to draw this tile's DDL
         void precompile(GrContext*);
@@ -50,7 +51,7 @@ public:
         // Just draw the re-inflated per-tile SKP directly into this tile w/o going through a DDL
         // first. This is used for determining the overhead of using DDLs (i.e., it replaces
         // a 'createDDL' and 'draw' pair.
-        void drawSKPDirectly(GrContext*);
+        void drawSKPDirectly(GrContext*, const sk_sp<SkPicture>&);
 
         // Replay the recorded DDL into the tile surface - filling in 'fBackendTexture'.
         void draw(GrContext*);
@@ -87,9 +88,6 @@ public:
         // TODO: fix the ref-order so we don't need 'fTileSurface' here
         sk_sp<SkSurface>          fTileSurface;
 
-        sk_sp<SkPicture>          fReconstitutedPicture;
-        SkTArray<sk_sp<SkImage>>  fPromiseImages;    // All the promise images in the
-                                                     // reconstituted picture
         std::unique_ptr<SkDeferredDisplayList> fDisplayList;
     };
 
@@ -98,7 +96,7 @@ public:
                   const SkIRect& viewport,
                   int numDivisions);
 
-    void createSKPPerTile(SkData* compressedPictureData, const DDLPromiseImageHelper& helper);
+    void createSKP(SkData* compressedPictureData, const DDLPromiseImageHelper& helper);
 
     void kickOffThreadedWork(SkTaskGroup* recordingTaskGroup,
                              SkTaskGroup* gpuTaskGroup,
@@ -132,12 +130,16 @@ public:
     void deleteBackendTextures(SkTaskGroup*, GrContext*);
 
 private:
+    GrContext*                             fContext;
     int                                    fNumDivisions; // number of tiles along a side
     SkAutoTArray<TileData>                 fTiles;        // 'fNumDivisions' x 'fNumDivisions'
 
     std::unique_ptr<SkDeferredDisplayList> fComposeDDL;
 
     const SkSurfaceCharacterization        fDstCharacterization;
+    sk_sp<SkPicture>                       fReconstitutedPicture;
+    SkTArray<sk_sp<SkImage>>               fPromiseImages; // All the promise images in the
+                                                           // reconstituted picture
 };
 
 #endif
