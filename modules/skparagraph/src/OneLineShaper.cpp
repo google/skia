6e2f9c810677bc844849c17fff43a11b9dd82e79
Julia Lavrova <jlavrova@google.com>
2020-05-27 10:38:35 -0400

    Turning off glyph boundaries calculations (needed mainly for zalgo)
    Change-Id: I53a5bba6bf3b790aab59af0067a9708eb52ae842

--- modules/skparagraph/src/OneLineShaper.cpp
+++ modules/skparagraph/src/OneLineShaper.cpp
@@ -211,7 +211,9 @@ void OneLineShaper::finish(TextRange blockText, SkScalar height, SkScalar& advan
             auto index = i - glyphs.start;
             if (i < glyphs.end) {
                 piece->fGlyphs[index] = run->fGlyphs[i];
-                piece->fBounds[index] = run->fBounds[i];
+                if (run->fBounds.size() > 0) {
+                    piece->fBounds[index] = run->fBounds[i];
+                }
             }
             piece->fClusterIndexes[index] = run->fClusterIndexes[i];
             piece->fOffsets[index] = run->fOffsets[i];
