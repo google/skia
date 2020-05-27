678f43b901c32e52d87383dbc63ce6d20b19e302
Chris Dalton <csmartdalton@google.com>
2020-05-26 22:30:34 -0600

    Add an msaa flag to CanvasKit viewer
    Adds a simple system for the user to supply flags via the location hash.
    
      e.g., "http://.../viewer.html#msaa:8"
    
    Implements the msaa flag by rendering to a multisampled offscreen
    framebuffer, then blitting it to the main canvas framebuffer.
    
    Change-Id: I7f2b8b769e491f2169fd6b967a72a8ea9c8ffb8b

--- modules/canvaskit/canvaskit_bindings.cpp
+++ modules/canvaskit/canvaskit_bindings.cpp
@@ -67,7 +67,7 @@
 #include "include/gpu/gl/GrGLInterface.h"
 #include "include/gpu/gl/GrGLTypes.h"
 
-#include <GL/gl.h>
+#include <GLES3/gl3.h>
 #include <emscripten/html5.h>
 #endif
 
@@ -146,6 +146,11 @@ sk_sp<GrContext> MakeGrContext(EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context)
     auto interface = GrGLMakeNativeInterface();
     // setup contexts
     sk_sp<GrContext> grContext(GrContext::MakeGL(interface));
+    if (grContext) {
+        // Bump the default resource cache limit.
+        constexpr static int kResourceCacheBytes = 256 * 1024 * 1024;
+        grContext->setResourceCacheLimit(kResourceCacheBytes);
+    }
     return grContext;
 }
 
@@ -191,6 +196,68 @@ sk_sp<SkSurface> MakeRenderTarget(sk_sp<GrContext> grContext, SimpleImageInfo si
                              kBottomLeft_GrSurfaceOrigin,
                              nullptr, true));
     return surface;
+}
+
+static void delete_wrapped_framebuffer(SkSurface::ReleaseContext context) {
+    GLuint framebuffer = (GLuint)context;
+    glDeleteFramebuffers(1, &framebuffer);
+}
+
+sk_sp<SkSurface> MakeOffscreenFramebuffer(sk_sp<GrContext> grContext, int width, int height,
+                                          int sampleCnt) {
+    GLuint colorBuffer;
+    glGenRenderbuffers(1, &colorBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, colorBuffer);
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, 8, GL_RGBA8, width, height);
+
+    GLuint stencilBuffer;
+    glGenRenderbuffers(1, &stencilBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, stencilBuffer);
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, 8, GL_STENCIL_INDEX8, width, height);
+
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER,
+                              colorBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER,
+                              stencilBuffer);
+
+    // Unbind "framebuffer" before orphaning its renderbuffers. (Otherwise they are spec'd to be
+    // detached from the currently bound framebuffer.)
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDeleteRenderbuffers(1, &colorBuffer);
+    glDeleteRenderbuffers(1, &stencilBuffer);
+
+    grContext->resetContext(kRenderTarget_GrGLBackendState);
+
+    GrGLFramebufferInfo glInfo;
+    glInfo.fFBOID = framebuffer;
+    glInfo.fFormat = GL_RGBA8;
+    GrBackendRenderTarget backendRenderTarget(width, height, sampleCnt, 8, glInfo);
+    return SkSurface::MakeFromBackendRenderTarget(grContext.get(), backendRenderTarget,
+                                                  kBottomLeft_GrSurfaceOrigin,
+                                                  SkColorType::kRGBA_8888_SkColorType, nullptr,
+                                                  nullptr, &delete_wrapped_framebuffer,
+                                                  (SkSurface::ReleaseContext)framebuffer);
+
+}
+
+enum class GLFilter {
+    kNearest = GL_NEAREST,
+    kLinear = GL_LINEAR
+};
+
+void BlitOffscreenFramebuffer(sk_sp<SkSurface> surface, int srcX0, int srcY0, int srcX1, int srcY1,
+                              int dstX0, int dstY0, int dstX1, int dstY1, GLFilter filter) {
+  GrGLFramebufferInfo glInfo;
+  auto backendRT = surface->getBackendRenderTarget(SkSurface::kFlushWrite_BackendHandleAccess);
+  backendRT.getGLFramebufferInfo(&glInfo);
+  glBindFramebuffer(GL_READ_FRAMEBUFFER, glInfo.fFBOID);
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+  glBlitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, GL_COLOR_BUFFER_BIT,
+                    (GLenum)filter);
+  surface->getContext()->resetContext(kRenderTarget_GrGLBackendState);
 }
 #endif
 
@@ -709,6 +776,8 @@ EMSCRIPTEN_BINDINGS(Skia) {
     function("MakeOnScreenGLSurface", &MakeOnScreenGLSurface);
     function("MakeRenderTarget", select_overload<sk_sp<SkSurface>(sk_sp<GrContext>, int, int)>(&MakeRenderTarget));
     function("MakeRenderTarget", select_overload<sk_sp<SkSurface>(sk_sp<GrContext>, SimpleImageInfo)>(&MakeRenderTarget));
+    function("MakeOffscreenFramebuffer", &MakeOffscreenFramebuffer);
+    function("BlitOffscreenFramebuffer", &BlitOffscreenFramebuffer);
 
     constant("gpu", true);
 #endif
@@ -1694,6 +1763,12 @@ EMSCRIPTEN_BINDINGS(Skia) {
         .value("Triangles",       SkVertices::VertexMode::kTriangles_VertexMode)
         .value("TrianglesStrip",  SkVertices::VertexMode::kTriangleStrip_VertexMode)
         .value("TriangleFan",     SkVertices::VertexMode::kTriangleFan_VertexMode);
+
+#ifdef SK_GL
+    enum_<GLFilter>("GLFilter")
+        .value("NEAREST",   GLFilter::kNearest)
+        .value("LINEAR",    GLFilter::kLinear);
+#endif
 
     // A value object is much simpler than a class - it is returned as a JS
     // object and does not require delete().
