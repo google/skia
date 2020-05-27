3dc3a8af2cc01aaf66c640f82a21084e33a9096a
Ethan Nicholas <ethannicholas@google.com>
2020-05-27 10:28:20 -0400

    Revert "Revert "SkSL function inlining""
    This reverts commit 1b63b4ac6933ccddb15bcf650cd5747d5eba44d0.
    
    Change-Id: I8120bb10cecc6889f4f4fd7b4c3a61d250e49219

--- src/sksl/ir/SkSLBreakStatement.h
+++ src/sksl/ir/SkSLBreakStatement.h
@@ -20,6 +20,10 @@ struct BreakStatement : public Statement {
     BreakStatement(int offset)
     : INHERITED(offset, kBreak_Kind) {}
 
+    int nodeCount() const override {
+        return 1;
+    }
+
     std::unique_ptr<Statement> clone() const override {
         return std::unique_ptr<Statement>(new BreakStatement(fOffset));
     }
