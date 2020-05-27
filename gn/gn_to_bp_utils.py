ac9491370abea9e312db0130c2153bd71186700c
Mike Reed <reed@google.com>
2020-05-27 16:34:19 -0400

    gms for SkParagraph
    Change-Id: If84de6242a1048ff9f29264ac6afe060ce3856eb

--- gn/gn_to_bp_utils.py
+++ gn/gn_to_bp_utils.py
@@ -36,7 +36,12 @@ def GrabDependentValues(js, name, value_type, list_to_extend, exclude):
   # Skia components, gms, tests, etc).
   for dep in js['targets'][name]['deps']:
     if 'modules' in dep:
-      continue   # Modules require special handling -- skip for now.
+      skip = true
+      for white_list in ['skottie', 'skparagraph']:
+        if white_list in dep:
+          skip = false
+      if skip:
+        continue   # Modules require special handling -- skip for now.
     if 'third_party' in dep:
       continue   # We've handled all third-party DEPS as static or shared_libs.
     if 'none' in dep:
