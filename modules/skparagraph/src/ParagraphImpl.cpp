af6de9f8418319cf7d29fc08ac9149e90d1ec085
Ben Wagner <bungeman@google.com>
2020-05-28 12:03:40 -0400

    Condition SkLoadICU on SK_USING_THIRD_PARTY_ICU.
    This is how SkShaper_harfbuzz currently avoids using SkLoadICU when it
    may not exist.
    
    Change-Id: I4ff9a6dc4297db97481cce1de53da9921d47a4ad
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/292566
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Weston Tracey <westont@google.com>

--- modules/skparagraph/src/ParagraphImpl.cpp
+++ modules/skparagraph/src/ParagraphImpl.cpp
@@ -9,7 +9,10 @@
 #include "modules/skparagraph/src/TextWrapper.h"
 #include "src/core/SkSpan.h"
 #include "src/utils/SkUTF.h"
+
+#if defined(SK_USING_THIRD_PARTY_ICU)
 #include "third_party/icu/SkLoadICU.h"
+#endif
 
 #include <unicode/ustring.h>
 #include <algorithm>
@@ -52,9 +55,11 @@ TextRange operator*(const TextRange& a, const TextRange& b) {
 }
 
 bool TextBreaker::initialize(SkSpan<const char> text, UBreakIteratorType type) {
+    #if defined(SK_USING_THIRD_PARTY_ICU)
     if (!SkLoadICU()) {
         return false;
     }
+    #endif
 
     UErrorCode status = U_ZERO_ERROR;
     fIterator = nullptr;
