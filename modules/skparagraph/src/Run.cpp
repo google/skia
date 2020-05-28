5c62c426a9e22d6aa74a811f84e96c492bb48f7c
Julia Lavrova <jlavrova@google.com>
2020-05-28 15:58:57 -0400

    Consolidating all ICU work in one place + caching the results
    Change-Id: I6c91fe3b065217a70be9cfc43a4f3e4049f87258

--- modules/skparagraph/src/Run.cpp
+++ modules/skparagraph/src/Run.cpp
@@ -11,19 +11,6 @@
 #include "modules/skshaper/include/SkShaper.h"
 #include "src/utils/SkUTF.h"
 
-#include <unicode/uchar.h>
-#include <algorithm>
-#include <utility>
-
-namespace {
-
-SkUnichar utf8_next(const char** ptr, const char* end) {
-    SkUnichar val = SkUTF::NextUTF8(ptr, end);
-    return val < 0 ? 0xFFFD : val;
-}
-
-}
-
 namespace skia {
 namespace textlayout {
 
@@ -45,7 +32,7 @@ Run::Run(ParagraphImpl* master,
     fUtf8Range = info.utf8Range;
     fOffset = SkVector::Make(offsetX, 0);
     fGlyphs.push_back_n(info.glyphCount);
-    fBounds.push_back_n(0);//info.glyphCount);
+    fBounds.push_back_n(info.glyphCount); // Keep it empty and there will be no bounds calculation; it's just experimental
     fPositions.push_back_n(info.glyphCount + 1);
     fOffsets.push_back_n(info.glyphCount + 1);
     fClusterIndexes.push_back_n(info.glyphCount + 1);
@@ -322,21 +309,6 @@ void Run::updateMetrics(InternalLineMetrics* endlineMetrics) {
     endlineMetrics->add(this);
 }
 
-void Cluster::setIsWhiteSpaces() {
-
-    fWhiteSpaces = false;
-
-    auto span = fMaster->text(fTextRange);
-    const char* ch = span.begin();
-    while (ch < span.end()) {
-        auto unichar = utf8_next(&ch, span.end());
-        if (!u_isWhitespace(unichar)) {
-            return;
-        }
-    }
-    fWhiteSpaces = true;
-}
-
 SkScalar Cluster::sizeToChar(TextIndex ch) const {
     if (ch < fTextRange.start || ch >= fTextRange.end) {
         return 0;
@@ -393,6 +365,18 @@ SkFont Cluster::font() const {
     return fMaster->run(fRunIndex).font();
 }
 
+bool Cluster::isHardBreak() const {
+    return fMaster->testFlag(fTextRange.end, IcuFlagTypes::kHardLineBreak);
+}
+
+bool Cluster::isSoftBreak() const {
+    return fMaster->testFlag(fTextRange.end, IcuFlagTypes::kSoftLineBreak);
+}
+
+bool Cluster::isGraphemeBreak() const {
+    return fMaster->testFlag(fTextRange.end, IcuFlagTypes::kGrapheme);
+}
+
 Cluster::Cluster(ParagraphImpl* master,
         RunIndex runIndex,
         size_t start,
@@ -409,9 +393,15 @@ Cluster::Cluster(ParagraphImpl* master,
         , fWidth(width)
         , fSpacing(0)
         , fHeight(height)
-        , fHalfLetterSpacing(0.0)
-        , fWhiteSpaces(false)
-        , fBreakType(None) {
+        , fHalfLetterSpacing(0.0) {
+    size_t len = 0;
+    fMaster->scanIcuFlags(
+        fTextRange,
+        IcuFlagTypes::kWhiteSpace,
+        [&](size_t index, unsigned flags){
+            len += 1;
+    });
+    fIsWhiteSpaces = (len == this->fTextRange.width());
 }
 
 }  // namespace textlayout
