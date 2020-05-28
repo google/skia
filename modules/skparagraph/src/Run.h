ee0ced2ee611d386b71bb600cd7e88c48dd9dcb6
Julia Lavrova <jlavrova@google.com>
2020-05-28 15:58:57 -0400

    Consolidating all ICU work in one place + caching the results
    Change-Id: I6c91fe3b065217a70be9cfc43a4f3e4049f87258

--- modules/skparagraph/src/Run.h
+++ modules/skparagraph/src/Run.h
@@ -268,9 +268,7 @@ public:
             , fWidth()
             , fSpacing(0)
             , fHeight()
-            , fHalfLetterSpacing(0.0)
-            , fWhiteSpaces(false)
-            , fBreakType(None) {}
+            , fHalfLetterSpacing(0.0) {}
 
     Cluster(ParagraphImpl* master,
             RunIndex runIndex,
@@ -295,14 +293,11 @@ public:
         fWidth += shift;
     }
 
-    void setBreakType(BreakType type) { fBreakType = type; }
-    bool isWhitespaces() const { return fWhiteSpaces; }
-    bool canBreakLineAfter() const {
-        return fBreakType == SoftLineBreak || fBreakType == HardLineBreak;
-    }
-    bool isHardBreak() const { return fBreakType == HardLineBreak; }
-    bool isSoftBreak() const { return fBreakType == SoftLineBreak; }
-    bool isGraphemeBreak() const { return fBreakType == GraphemeBreak; }
+    bool isWhitespaces() const { return fIsWhiteSpaces; }
+    bool isHardBreak() const;
+    bool isSoftBreak() const;
+    bool isGraphemeBreak() const;
+    bool canBreakLineAfter() const { return isHardBreak() || isSoftBreak(); }
     size_t startPos() const { return fStart; }
     size_t endPos() const { return fEnd; }
     SkScalar width() const { return fWidth; }
@@ -321,8 +316,6 @@ public:
     SkFont font() const;
 
     SkScalar trimmedWidth(size_t pos) const;
-
-    void setIsWhiteSpaces();
 
     bool contains(TextIndex ch) const { return ch >= fTextRange.start && ch < fTextRange.end; }
 
@@ -349,8 +342,7 @@ private:
     SkScalar fSpacing;
     SkScalar fHeight;
     SkScalar fHalfLetterSpacing;
-    bool fWhiteSpaces;
-    BreakType fBreakType;
+    bool fIsWhiteSpaces;
 };
 
 class InternalLineMetrics {
