92eaffcc7236e3c447c0778997ac293dce634f9f
Ben Wagner <bungeman@google.com>
2020-05-27 22:13:55 -0400

    Pre-allocate space for single run fRunsInVisualOrder.
    Most lines have one run in them, so avoid extra allocations in this
    case. As a side effect, mark TextLine as moveable but not mem-movable.
    
    The TextLine class used to be used as a mem-movable class, but the
    addition of an SkSTArray made it non-mem-movable. Correct this and make
    TextLine moveable and document its non-copyable nature. This avoids
    ASAN use after free issues.
    
    Change-Id: Icf45a464004e9f270ec46e1c2ddcf29fd356c90a

--- modules/skparagraph/src/TextLine.h
+++ modules/skparagraph/src/TextLine.h
@@ -29,6 +29,10 @@ public:
     };
 
     TextLine() = default;
+    TextLine(const TextLine&) = delete;
+    TextLine& operator=(const TextLine&) = delete;
+    TextLine(TextLine&&) = default;
+    TextLine& operator=(TextLine&&) = default;
     ~TextLine() = default;
 
     TextLine(ParagraphImpl* master,
@@ -126,8 +130,8 @@ private:
     TextRange fTextWithWhitespacesRange;
     ClusterRange fClusterRange;
     ClusterRange fGhostClusterRange;
-
-    SkTArray<size_t, true> fRunsInVisualOrder;
+    // Avoid the malloc/free in the common case of one run per line
+    SkSTArray<1, size_t, true> fRunsInVisualOrder;
     SkVector fAdvance;                  // Text size
     SkVector fOffset;                   // Text position
     SkScalar fShift;                    // Let right
