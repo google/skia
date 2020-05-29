f7f5fb3e557e3ed2a5b3e7a9e8d429a02e473332
Dominic Mazzoni <dmazzoni@chromium.org>
2020-05-28 20:23:12 -0700

    Tagged PDFs should output an IDTree.
    The PDF spec requires that tagged PDFs have a unique
    alphanumeric ID for each structure element node, and an
    IDTree structure that maps IDs to an indirect reference
    to the PDF node containing that element.
    
    This patch adds support for the IDTree, and also fixes
    the IDs we output for each node to be strings (not names)
    and makes them zero-padded so that we can easily output
    the IDs in sorted lexicographic order by making that match
    numeric order.
    
    PDFs output by Skia now pass CommonLook's
    Integrity Validator; these ID issues were flagged
    as errors previously.
    
    Bug: chromium:607777
    Bug: skia:8148
    Change-Id: I9b367fe90aa018c21f34abd186e83c53cdde34cc

--- src/pdf/SkPDFTag.h
+++ src/pdf/SkPDFTag.h
@@ -8,6 +8,8 @@
 #ifndef SkPDFTag_DEFINED
 #define SkPDFTag_DEFINED
 
+#include <map>
+
 #include "include/docs/SkPDFDocument.h"
 #include "include/private/SkTArray.h"
 #include "include/private/SkTHash.h"
@@ -28,15 +30,26 @@ public:
     SkPDFIndirectReference makeStructTreeRoot(SkPDFDocument* doc);
 
 private:
+    // An entry in a map from a node ID to an indirect reference to its
+    // corresponding structure element node.
+    struct IDTreeEntry {
+        int nodeId;
+        SkPDFIndirectReference ref;
+    };
+
     static void Copy(SkPDF::StructureElementNode& node,
                      SkPDFTagNode* dst,
                      SkArenaAlloc* arena,
                      SkTHashMap<int, SkPDFTagNode*>* nodeMap);
+    SkPDFIndirectReference PrepareTagTreeToEmit(SkPDFIndirectReference parent,
+                                                SkPDFTagNode* node,
+                                                SkPDFDocument* doc);
 
     SkArenaAlloc fArena;
     SkTHashMap<int, SkPDFTagNode*> fNodeMap;
     SkPDFTagNode* fRoot = nullptr;
     SkTArray<SkTArray<SkPDFTagNode*>> fMarksPerPage;
+    std::vector<IDTreeEntry> fIdTreeEntries;
 
     SkPDFTagTree(const SkPDFTagTree&) = delete;
     SkPDFTagTree& operator=(const SkPDFTagTree&) = delete;
