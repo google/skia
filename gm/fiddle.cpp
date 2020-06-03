cb76e36753f19fae936883d7661be5401ef82343
Mike Klein <mtklein@google.com>
2020-06-03 14:00:00 -0500

    test attribute push
    Trying to minimize my issue from
    https://skia-review.googlesource.com/c/skia/+/293599.
    
    Commit: false
    Change-Id: I4f996be16c735238ce1285fbd19b04d772aa33c3

--- gm/fiddle.cpp
+++ gm/fiddle.cpp
@@ -6,10 +6,33 @@
  */
 
 #include "gm/gm.h"
-#include "skia.h"
+#include "include/core/SkCanvas.h"
 
-static void draw(SkCanvas* canvas);
-DEF_SIMPLE_GM(fiddle, canvas, 256, 256) { draw(canvas); }
+#pragma clang attribute push(__attribute__((target("avx2,f16c,fma"))), apply_to=function)
+#define __SSE__ 1
+#define __SSE2__ 1
+#define __SSE3__ 1
+#define __SSSE3__ 1
+#define __SSE4_1__ 1
+#define __SSE4_2__ 1
+#define __AVX__ 1
+#define __F16C__ 1
+#define __AVX2__ 1
+#define __FMA__ 1
 
-// Paste your fiddle.skia.org code over this stub.
-void draw(SkCanvas*) {}
+#include <immintrin.h>
+
+using F32 = float __attribute__((ext_vector_type(8)));
+
+DEF_SIMPLE_GM(fiddle, canvas, 256, 256) {
+    auto cube = [](F32 x) {
+        return _mm256_mul_ps(x,x) * x;
+    };
+
+    float w = canvas->getBaseLayerSize().width();
+    F32 x = {0+w,1+w,2+w,3+w, 4+w,5+w,6+w,7+w};
+
+    canvas->clear(SkColorSetA(cube(x)[7], 0xff));  // 0xff159457, a pretty green
+}
+
+#pragma clang attribute pop
