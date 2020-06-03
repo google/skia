e91dc61aa2187413c1a05b383ca3148404bf9fd3
Greg Daniel <egdaniel@google.com>
2020-06-03 13:41:33 -0400

    Add API to allow updating GrBackendSurfaceMutableState during flush.
    This is currently only supported for the Vulkan backend
    
    Bug: skia:10254
    Change-Id: I9274799098dc00dec5abcbcec95ce7cc23fec537

--- include/gpu/GrTypes.h
+++ include/gpu/GrTypes.h
@@ -12,6 +12,9 @@
 #include "include/core/SkTypes.h"
 #include "include/gpu/GrConfig.h"
 
+class GrBackendSurfaceMutableState;
+class GrBackendTexture;
+class GrBackendRenderTarget;
 class GrBackendSemaphore;
 class SkImage;
 class SkSurface;
@@ -317,8 +320,15 @@ struct GrPrepareForExternalIORequests {
     int fNumSurfaces = 0;
     SkSurface** fSurfaces = nullptr;
     bool* fPrepareSurfaceForPresent = nullptr;
+    int fNumBackendTextures = 0;
+    GrBackendTexture* fBackendTextures = nullptr;
+    GrBackendSurfaceMutableState* fBackendTextureStates = nullptr;
+    int fNumBackendRenderTarget = 0;
+    GrBackendRenderTarget* fBackendRenderTargets = nullptr;
+    GrBackendSurfaceMutableState* fBackendRenderTargetStates = nullptr;
 
-    bool hasRequests() const { return fNumImages || fNumSurfaces; }
+    bool hasRequests() const { return fNumImages || fNumSurfaces || fNumBackendTextures ||
+                                      fNumBackendRenderTarget; }
 };
 
 #endif
