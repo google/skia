a15f2bfb6f6e15fbe08821895a070e23d234a3d0
Ethan Nicholas <ethannicholas@google.com>
2020-06-03 16:41:22 +0000

    Reland "Revert "Omit dead SkSL functions""
    This reverts commit fd1173ac719c3c21b363ffdad6b2bb55f7f00113.
    
    Reason for revert: TSAN failure: https://chromium-swarm.appspot.com/task?id=4c93823229fa6a10
    
    Original change's description:
    > Revert "Revert "Omit dead SkSL functions""
    > 
    > This reverts commit 7c969f26bc21b20cb7f4dea497fbdc36d3029709.
    > 
    > Change-Id: I8fb99f271e2ecaeb83d570cc2d2cf8851bc776f0
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/293338
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    
    TBR=bsalomon@google.com,brianosman@google.com,ethannicholas@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: Iab3ee5a336074676ebd0b761922ececf189752ef
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/293843
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -1448,20 +1448,6 @@ bool Compiler::optimize(Program& program) {
                 this->scanCFG((FunctionDefinition&) element);
             }
         }
-        // we wait until after analysis to remove dead functions so that we still report errors
-        // even in unused code
-        if (program.fSettings.fRemoveDeadFunctions) {
-            for (auto iter = program.fElements.begin(); iter != program.fElements.end(); ) {
-                if ((*iter)->fKind == ProgramElement::kFunction_Kind) {
-                    const FunctionDefinition& f = (const FunctionDefinition&) **iter;
-                    if (!f.fDeclaration.fCallCount && f.fDeclaration.fName != "main") {
-                        iter = program.fElements.erase(iter);
-                        continue;
-                    }
-                }
-                ++iter;
-            }
-        }
         if (program.fKind != Program::kFragmentProcessor_Kind) {
             for (auto iter = program.fElements.begin(); iter != program.fElements.end();) {
                 if ((*iter)->fKind == ProgramElement::kVar_Kind) {
