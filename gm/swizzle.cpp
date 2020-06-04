eed56f0fe5a2101361439023aa083724b7e72296
John Stiles <johnstiles@google.com>
2020-06-04 13:30:51 -0400

    Update GrFragmentProcessor::SwizzleOutput to use a child FP.
    We are updating FPs to receive their input via a child FP where
    possible, instead of relying on the input color.
    
    This CL also adds a GM test for SwizzleOutput, since this did not appear
    to be covered by existing unit tests.
    
    Change-Id: I3d8176395bb42eab7ff471c9137597402b5b3a69
    Bug: skia:10217
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/293884
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

---
+++ gm/swizzle.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm/gm.h"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkImageInfo.h"
+#include "include/core/SkMatrix.h"
+#include "include/core/SkRect.h"
+#include "include/core/SkTypes.h"
+#include "include/gpu/GrContext.h"
+#include "src/gpu/GrBitmapTextureMaker.h"
+#include "src/gpu/GrContextPriv.h"
+#include "src/gpu/GrFragmentProcessor.h"
+#include "src/gpu/GrRenderTargetContext.h"
+#include "src/gpu/GrRenderTargetContextPriv.h"
+#include "src/gpu/ops/GrFillRectOp.h"
+#include "tools/Resources.h"
+#include "tools/ToolUtils.h"
+
+DEF_SIMPLE_GPU_GM(swizzle, ctx, rtCtx, canvas, 512, 512) {
+    SkRect bounds = SkRect::MakeIWH(512, 512);
+
+    SkBitmap bmp;
+    GetResourceAsBitmap("images/mandrill_512_q075.jpg", &bmp);
+    GrBitmapTextureMaker maker(ctx, bmp, GrImageTexGenPolicy::kDraw);
+    auto view = maker.view(GrMipMapped::kNo);
+    if (!view) {
+        return;
+    }
+    std::unique_ptr<GrFragmentProcessor> imgFP =
+        GrTextureEffect::Make(std::move(view), bmp.alphaType(), SkMatrix());
+    auto fp = GrFragmentProcessor::SwizzleOutput(std::move(imgFP), GrSwizzle("grb1"));
+
+    GrPaint grPaint;
+    grPaint.addColorFragmentProcessor(std::move(fp));
+
+    rtCtx->priv().testingOnly_addDrawOp(
+        GrFillRectOp::MakeNonAARect(ctx, std::move(grPaint), SkMatrix(), bounds));
+}
