0dd52fd4a4e99a61e4806f6a1a4c677174b22599
John Stiles <johnstiles@google.com>
2020-06-04 11:07:21 -0400

    Fix invalid code generation with @setData and 'in fragmentProcessor?'.
    Without this change, the generated code for an FP that includes a
    fragment processor and a @setData block will erroneously include:
      auto inputFP = _outer.inputFP;
      (void)inputFP;
    This is not a valid member name. (inputFP_index does exist.)
    There does not appear to be any reason to generate this line.
    
    Change-Id: I324878c0768959e56be33cfd8a85a2aa42794c7c
    Bug: skia:10217

--- src/sksl/SkSLCPPCodeGenerator.cpp
+++ src/sksl/SkSLCPPCodeGenerator.cpp
@@ -973,7 +973,7 @@ void CPPCodeGenerator::writeSetData(std::vector<const Variable*>& uniforms) {
                                     "const GrFragmentProcessor& _proc) override {\n",
                  pdman);
     bool wroteProcessor = false;
-    for (const auto u : uniforms) {
+    for (const Variable* u : uniforms) {
         if (is_uniform_in(*u)) {
             if (!wroteProcessor) {
                 this->writef("        const %s& _outer = _proc.cast<%s>();\n", fullName, fullName);
@@ -1046,11 +1046,12 @@ void CPPCodeGenerator::writeSetData(std::vector<const Variable*>& uniforms) {
         for (const auto& p : fProgram) {
             if (ProgramElement::kVar_Kind == p.fKind) {
                 const VarDeclarations& decls = (const VarDeclarations&) p;
-                for (const auto& raw : decls.fVars) {
-                    VarDeclaration& decl = (VarDeclaration&) *raw;
-                    String nameString(decl.fVar->fName);
+                for (const std::unique_ptr<Statement>& raw : decls.fVars) {
+                    const VarDeclaration& decl = static_cast<VarDeclaration&>(*raw);
+                    const Variable& variable = *decl.fVar;
+                    String nameString(variable.fName);
                     const char* name = nameString.c_str();
-                    if (decl.fVar->fType.kind() == Type::kSampler_Kind) {
+                    if (variable.fType.kind() == Type::kSampler_Kind) {
                         this->writef("        const GrSurfaceProxyView& %sView = "
                                      "_outer.textureSampler(%d).view();\n",
                                      name, samplerIndex);
@@ -1058,20 +1059,23 @@ void CPPCodeGenerator::writeSetData(std::vector<const Variable*>& uniforms) {
                                      name, name);
                         this->writef("        (void) %s;\n", name);
                         ++samplerIndex;
-                    } else if (needs_uniform_var(*decl.fVar)) {
+                    } else if (needs_uniform_var(variable)) {
                         this->writef("        UniformHandle& %s = %sVar;\n"
                                      "        (void) %s;\n",
                                      name, HCodeGenerator::FieldName(name).c_str(), name);
-                    } else if (SectionAndParameterHelper::IsParameter(*decl.fVar) &&
-                               decl.fVar->fType != *fContext.fFragmentProcessor_Type) {
+                    } else if (SectionAndParameterHelper::IsParameter(variable) &&
+                               variable.fType != *fContext.fFragmentProcessor_Type) {
                         if (!wroteProcessor) {
                             this->writef("        const %s& _outer = _proc.cast<%s>();\n", fullName,
                                          fullName);
                             wroteProcessor = true;
                         }
-                        this->writef("        auto %s = _outer.%s;\n"
-                                     "        (void) %s;\n",
-                                     name, name, name);
+
+                        if (variable.fType.nonnullable() != *fContext.fFragmentProcessor_Type) {
+                            this->writef("        auto %s = _outer.%s;\n"
+                                         "        (void) %s;\n",
+                                         name, name, name);
+                        }
                     }
                 }
             }
@@ -1119,7 +1123,7 @@ void CPPCodeGenerator::writeClone() {
             String fieldName = HCodeGenerator::CoordTransformName(s.fArgument, i);
             this->writef("\n, %s(src.%s)", fieldName.c_str(), fieldName.c_str());
         }
-        for (const auto& param : fSectionAndParameterHelper.getParameters()) {
+        for (const Variable* param : fSectionAndParameterHelper.getParameters()) {
             String fieldName = HCodeGenerator::FieldName(String(param->fName).c_str());
             if (param->fType.nonnullable() == *fContext.fFragmentProcessor_Type) {
                 this->writef("\n, %s_index(src.%s_index)",
