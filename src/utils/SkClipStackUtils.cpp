7a71aae99adac11868a2a343c5df709f1a6ac187
Michael Ludwig <michaelludwig@google.com>
2020-06-04 09:31:40 -0400

    clipShader experimental impl on GPU
    Change-Id: I0af800900a7fbd9d16af0058ee0754358ebc3875

--- src/utils/SkClipStackUtils.cpp
+++ src/utils/SkClipStackUtils.cpp
@@ -14,6 +14,11 @@ void SkClipStack_AsPath(const SkClipStack& cs, SkPath* path) {
 
     SkClipStack::Iter iter(cs, SkClipStack::Iter::kBottom_IterStart);
     while (const SkClipStack::Element* element = iter.next()) {
+        if (element->getDeviceSpaceType() == SkClipStack::Element::DeviceSpaceType::kShader) {
+            // TODO: Handle DeviceSpaceType::kShader somehow; it can't be turned into an SkPath
+            // but perhaps the pdf backend can apply shaders in another way.
+            continue;
+        }
         SkPath operand;
         if (element->getDeviceSpaceType() != SkClipStack::Element::DeviceSpaceType::kEmpty) {
             element->asDeviceSpacePath(&operand);
