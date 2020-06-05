ee287221c28d6ba56c61ed4179f9323b1968c686
Mike Klein <mtklein@google.com>
2020-06-05 08:52:17 -0500

    port skx opts to old style
    Change-Id: I364365455cc3580f38ddd189f00050ea9e2238a3

--- src/opts/SkOpts_skx.cpp
+++ src/opts/SkOpts_skx.cpp
@@ -5,4 +5,15 @@
  * found in the LICENSE file.
  */
 
-// Intentionally empty, to be filled in.
+#include "src/core/SkOpts.h"
+
+#define SK_OPTS_NS skx
+#include "src/opts/SkBlitRow_opts.h"
+#include "src/opts/SkVM_opts.h"
+
+namespace SkOpts {
+    void Init_skx() {
+        blit_row_s32a_opaque = SK_OPTS_NS::blit_row_s32a_opaque;
+        interpret_skvm = SK_OPTS_NS::interpret_skvm;
+    }
+}
