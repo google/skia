21dd7cdd94fe4a4f5b7079c703ceebcd5f77303e
Mike Klein <mtklein@google.com>
2020-06-05 06:57:30 -0500

    add structure for normal SKX opts
    My experience porting the old opts over to the current SKX opts setup
    was so bad that I don't want to try any more, and think it's probably
    safer to port the SKX code to the old setup.
    
    Need to hook up dependents (Chromium and Google3 I think) before we can
    move the actual SXK opts code over.
    
    Change-Id: Ibb8bc4a083cb104cd39f27cbfbc16e9eedd9bd46

---
+++ src/opts/SkOpts_skx.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// Intentionally empty, to be filled in.
