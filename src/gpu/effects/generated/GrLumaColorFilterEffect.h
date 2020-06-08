75da65158b5dcaae0db278d908e018b1021c65a4
John Stiles <johnstiles@google.com>
2020-06-08 14:32:14 -0400

    Update GrLumaColorFilterEffect to use a child FP.
    We are updating FPs to receive their input via a child FP where
    possible, instead of relying on the input color.
    
    Change-Id: I377fbf676185caa8c8a5b60b7a58bf98dc6d4da7
    Bug: skia:10217
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295078
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- src/gpu/effects/generated/GrLumaColorFilterEffect.h
+++ src/gpu/effects/generated/GrLumaColorFilterEffect.h
@@ -19,22 +19,35 @@ class GrLumaColorFilterEffect : public GrFragmentProcessor {
 public:
 #include "include/private/SkColorData.h"
 
-    SkPMColor4f constantOutputForConstantInput(const SkPMColor4f& input) const override {
+    SkPMColor4f constantOutputForConstantInput(const SkPMColor4f& inColor) const override {
+        SkPMColor4f input = this->numChildProcessors()
+                                    ? ConstantOutputForConstantInput(
+                                              this->childProcessor(inputFP_index), inColor)
+                                    : inColor;
         float luma = SK_ITU_BT709_LUM_COEFF_R * input.fR + SK_ITU_BT709_LUM_COEFF_G * input.fG +
                      SK_ITU_BT709_LUM_COEFF_B * input.fB;
         return {0, 0, 0, SkTPin(luma, 0.0f, 1.0f)};
     }
-    static std::unique_ptr<GrFragmentProcessor> Make() {
-        return std::unique_ptr<GrFragmentProcessor>(new GrLumaColorFilterEffect());
+    static std::unique_ptr<GrFragmentProcessor> Make(std::unique_ptr<GrFragmentProcessor> inputFP) {
+        return std::unique_ptr<GrFragmentProcessor>(
+                new GrLumaColorFilterEffect(std::move(inputFP)));
     }
     GrLumaColorFilterEffect(const GrLumaColorFilterEffect& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
     const char* name() const override { return "LumaColorFilterEffect"; }
+    int inputFP_index = -1;
 
 private:
-    GrLumaColorFilterEffect()
+    GrLumaColorFilterEffect(std::unique_ptr<GrFragmentProcessor> inputFP)
             : INHERITED(kGrLumaColorFilterEffect_ClassID,
-                        (OptimizationFlags)kConstantOutputForConstantInput_OptimizationFlag) {}
+                        (OptimizationFlags)(inputFP ? ProcessorOptimizationFlags(inputFP.get())
+                                                    : kAll_OptimizationFlags) &
+                                kConstantOutputForConstantInput_OptimizationFlag) {
+        if (inputFP) {
+            inputFP_index = this->numChildProcessors();
+            this->registerChildProcessor(std::move(inputFP));
+        }
+    }
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
     void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
     bool onIsEqual(const GrFragmentProcessor&) const override;
