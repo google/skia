26ae40f668304b8afeb98525e5d23fcc91619964
Florin Malita <fmalita@chromium.org>
2020-06-08 12:03:48 -0400

    Reland "[skrive] Initial parser plumbing"
    This reverts commit e923ae1be4b5c9e728409f0d73ba8f70d5fd3cee.
    
    
    Original change's description:
    > Revert "[skrive] Initial parser plumbing"
    > 
    > This reverts commit 00a96f2c994c0eb8f9706cf50e849fa8bcab5c66.
    > 
    > Reason for revert: breaking wuffs 
    > 
    > Original change's description:
    > > [skrive] Initial parser plumbing
    > > 
    > > TBR=
    > > Change-Id: If96d81840b5bb3bc8c6e26f8aacca4d9dd8896e3
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/293677
    > > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > > Commit-Queue: Florin Malita <fmalita@chromium.org>
    > 
    > TBR=fmalita@chromium.org,reed@google.com
    > 
    > Change-Id: I63258d560cc8ee8d069e6417d987342a74a3f0c2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295001
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: Florin Malita <fmalita@chromium.org>
    
    TBR=fmalita@chromium.org,reed@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: I0a671bb7476ee92c365fbea1f156c73c6954fe40
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295003
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>

---
+++ experimental/skrive/src/reader/JsonReader.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "experimental/skrive/src/reader/StreamReader.h"
+#include "include/core/SkString.h"
+#include "src/utils/SkJSON.h"
+
+#include <algorithm>
+#include <iterator>
+#include <memory>
+
+namespace skrive::internal {
+namespace {
+
+StreamReader::BlockType block_type(const char* type_name) {
+    static constexpr struct TypeMapEntry {
+        const char*             name;
+        StreamReader::BlockType block_type;
+    } gTypeMap[] = {
+        {"artboard" , StreamReader::BlockType::kActorArtboard },
+        {"artboards", StreamReader::BlockType::kArtboards     },
+        {"node"     , StreamReader::BlockType::kActorNode     },
+        {"nodes"    , StreamReader::BlockType::kComponents    },
+    };
+
+    const TypeMapEntry key = { type_name, StreamReader::BlockType::kUnknown };
+    const auto* map_entry = std::lower_bound(std::begin(gTypeMap),
+                                             std::end  (gTypeMap),
+                                             key,
+                                             [](const TypeMapEntry& a, const TypeMapEntry& b) {
+                                                 return strcmp(a.name, b.name) < 0;
+                                             });
+
+    return (map_entry != std::end(gTypeMap) && !strcmp(map_entry->name, key.name))
+        ? map_entry->block_type
+        : StreamReader::BlockType::kUnknown;
+}
+
+class JsonReader final : public StreamReader {
+public:
+    explicit JsonReader(std::unique_ptr<skjson::DOM> dom)
+        : fDom(std::move(dom)) {
+        fContextStack.push_back({&fDom->root(), 0});
+    }
+
+    ~JsonReader() override {
+        SkASSERT(fContextStack.size() == 1);
+    }
+
+private:
+    template <typename T>
+    const T* readProp(const char label[]) const {
+        const auto* current_container = fContextStack.back().fContainer;
+
+        return current_container->is<skjson::ObjectValue>()
+                ? static_cast<const T*>(current_container->as<skjson::ObjectValue>()[label])
+                : nullptr;
+    }
+
+    bool readBool(const char label[]) override {
+        const auto* jbool = this->readProp<skjson::BoolValue>(label);
+
+        return jbool ? **jbool : false;
+    }
+
+    float readFloat(const char label[]) override {
+        const auto* jnum = this->readProp<skjson::NumberValue>(label);
+
+        return jnum ? static_cast<float>(**jnum) : 0.0f;
+    }
+
+    SkString readString(const char label[]) override {
+        const auto* jstr = this->readProp<skjson::StringValue>(label);
+
+        return SkString(jstr ? jstr->begin() : nullptr);
+    }
+
+    // "Blocks" map to either objects or arrays.  For object containers, the block type is encoded
+    // as the key; for array containers, the type is an explicit "type" property *inside* the block
+    // entry - which must be an object in this case.
+    BlockType openBlock() override {
+        switch (fContextStack.back().fContainer->getType()) {
+            case skjson::Value::Type::kObject: return this->openObjectBlock();
+            case skjson::Value::Type::kArray:  return this->openArrayBlock();
+            default: break;
+        }
+        SkUNREACHABLE;
+    }
+
+    BlockType openObjectBlock() {
+        auto& ctx = fContextStack.back();
+        const auto& container = ctx.fContainer->as<skjson::ObjectValue>();
+
+        while (ctx.fMemberIndex < container.size()) {
+            const auto& m = container[ctx.fMemberIndex];
+            if (m.fValue.is<skjson::ObjectValue>() || m.fValue.is<skjson::ArrayValue>()) {
+                fContextStack.push_back({&m.fValue, 0});
+                return block_type(m.fKey.begin());
+            }
+
+            ctx.fMemberIndex++;
+        }
+
+        return BlockType::kEoB;
+    }
+
+    BlockType openArrayBlock() {
+        auto& ctx = fContextStack.back();
+        const auto& container = ctx.fContainer->as<skjson::ArrayValue>();
+
+        while (ctx.fMemberIndex < container.size()) {
+            const auto& m = container[ctx.fMemberIndex];
+            if (m.is<skjson::ObjectValue>()) {
+                if (const skjson::StringValue* jtype = m.as<skjson::ObjectValue>()["type"]) {
+                    fContextStack.push_back({&m, 0});
+                    return block_type(jtype->begin());
+                }
+            }
+
+            ctx.fMemberIndex++;
+        }
+
+        return BlockType::kEoB;
+    }
+
+    void closeBlock() override {
+        SkASSERT(fContextStack.size() > 1);
+        fContextStack.pop_back();
+        fContextStack.back().fMemberIndex++;
+    }
+
+    struct ContextRec {
+        const skjson::Value* fContainer;
+        size_t               fMemberIndex;
+    };
+
+    const std::unique_ptr<skjson::DOM> fDom;
+
+    std::vector<ContextRec>            fContextStack;
+};
+
+} // namespace
+
+std::unique_ptr<StreamReader> MakeJsonStreamReader(const char json[], size_t len) {
+    auto dom = std::make_unique<skjson::DOM>(json, len);
+
+    return dom->root().is<skjson::ObjectValue>() ? std::make_unique<JsonReader>(std::move(dom))
+                                                 : nullptr;
+}
+
+} // namespace skrive::internal
