5b74f00d836cb345872cf45cf8b1607c64baa10f
Adlai Holler <adlai@google.com>
2020-06-08 13:23:34 -0400

    Clean up SkShaper_coretext.cpp & SkCGBase
    - Use SkUniqueCFRef.
    - Use CFCopyDescription to get dictionary description.
    - Use CGFLOAT_IS_DOUBLE define.
    - Always include CoreGraphics.h directly - no need for ApplicationServices.h
    
    Change-Id: If0276cab0182d33aacf1060a734b8b025e16f99e

--- src/utils/mac/SkCGBase.h
+++ src/utils/mac/SkCGBase.h
@@ -11,41 +11,20 @@
 #include "include/core/SkTypes.h"
 #if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 
-#ifdef SK_BUILD_FOR_MAC
-#import <ApplicationServices/ApplicationServices.h>
-#endif
-
-#ifdef SK_BUILD_FOR_IOS
 #include <CoreGraphics/CoreGraphics.h>
-#endif
 
 // Skia extensions for types in CGBase.h
 
 static inline CGFloat SkScalarToCGFloat(SkScalar scalar) {
-    if (sizeof(CGFloat) == sizeof(float)) {
-        return SkScalarToFloat(scalar);
-    } else {
-        SkASSERT(sizeof(CGFloat) == sizeof(double));
-        return (CGFloat) SkScalarToDouble(scalar);
-    }
+    return CGFLOAT_IS_DOUBLE ? SkScalarToDouble(scalar) : SkScalarToFloat(scalar);
 }
 
 static inline SkScalar SkScalarFromCGFloat(CGFloat cgFloat) {
-    if (sizeof(CGFloat) == sizeof(float)) {
-        return SkFloatToScalar(cgFloat);
-    } else {
-        SkASSERT(sizeof(CGFloat) == sizeof(double));
-        return SkDoubleToScalar(cgFloat);
-    }
+    return CGFLOAT_IS_DOUBLE ? SkDoubleToScalar(cgFloat) : SkFloatToScalar(cgFloat);
 }
 
 static inline float SkFloatFromCGFloat(CGFloat cgFloat) {
-    if (sizeof(CGFloat) == sizeof(float)) {
-        return cgFloat;
-    } else {
-        SkASSERT(sizeof(CGFloat) == sizeof(double));
-        return static_cast<float>(cgFloat);
-    }
+    return CGFLOAT_IS_DOUBLE ? static_cast<float>(cgFloat) : cgFloat;
 }
 
 #endif
