cd6898a0a656fa1311d419452e56f956ebd7ab1d
Nathaniel Nifong <nifong@google.com>
2020-06-08 15:26:40 -0400

    Accept Flat Uint32Array, Float32Array, or 2d Float32Array as the color argument to MakeLinearGradient
    TODO:
      1. Accept a Color builder or a TypeArray from CanvasKit.Malloc
      2. Apply the same treatment to all other gradient functions, MakeSkVertices, and drawAtlas
    Change-Id: I94fa67a3c00d7b1ecdc004af4ffd3193404c1a30

--- modules/canvaskit/helper.js
+++ modules/canvaskit/helper.js
@@ -258,6 +258,34 @@ function copy2dArray(arr, dest, ptr) {
   return ptr;
 }
 
+// Copys an array of colors to wasm, returning an object with the pointer
+// and info necessary to use the copied colors.
+// Accepts either a flat Float32Array, flat Uint32Array or Array of Float32Arrays.
+// If color is an object that was allocated with CanvasKit.Malloc, it's pointer is
+// returned and no extra copy is performed.
+function copyFlexibleColorArray(colors) {
+  var result = {
+    colorPtr: nullptr,
+    count: colors.length,
+    colorType: CanvasKit.ColorType.RGBA_F32,
+  }
+  if (colors instanceof Float32Array) {
+    result.colorPtr = copy1dArray(colors, "HEAPF32");
+    result.count = colors.length / 4;
+
+  } else if (colors instanceof Uint32Array) {
+    result.colorPtr = copy1dArray(colors, "HEAPU32");
+    result.colorType = CanvasKit.ColorType.RGBA_8888;
+
+  } else if (colors instanceof Array && colors[0] instanceof Float32Array) {
+    result.colorPtr = copy2dArray(colors, "HEAPF32");
+  } else {
+    console.log('Not a color array '+colors.constructor);
+    return nullptr;
+  }
+  return result;
+}
+
 var defaultPerspective = Float32Array.of(0, 0, 1);
 
 var _scratch3x3MatrixPtr = nullptr;
