1859519a928384cb1baffabe2a1eef551716b99c
Florin Malita <fmalita@chromium.org>
2020-06-07 22:39:39 -0400

    [skrive] Initial parser plumbing
    TBR=
    Change-Id: If96d81840b5bb3bc8c6e26f8aacca4d9dd8896e3

---
+++ experimental/skrive/src/SkRive.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "experimental/skrive/include/SkRive.h"
+
+namespace skrive {
+
+} // namespace skrive
