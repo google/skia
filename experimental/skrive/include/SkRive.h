575826635ee47d93e538b0e25fc40a2925143e55
Florin Malita <fmalita@chromium.org>
2020-06-08 15:40:39 +0000

    Reland "[skrive] Initial parser plumbing"
    This reverts commit e923ae1be4b5c9e728409f0d73ba8f70d5fd3cee.
    
    
    Original change's description:
    > Revert "[skrive] Initial parser plumbing"
    > 
    > This reverts commit 00a96f2c994c0eb8f9706cf50e849fa8bcab5c66.
    > 
    > Reason for revert: breaking wuffs 
    > 
    > Original change's description:
    > > [skrive] Initial parser plumbing
    > > 
    > > TBR=
    > > Change-Id: If96d81840b5bb3bc8c6e26f8aacca4d9dd8896e3
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/293677
    > > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > > Commit-Queue: Florin Malita <fmalita@chromium.org>
    > 
    > TBR=fmalita@chromium.org,reed@google.com
    > 
    > Change-Id: I63258d560cc8ee8d069e6417d987342a74a3f0c2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295001
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: Florin Malita <fmalita@chromium.org>
    
    TBR=fmalita@chromium.org,reed@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: I0a671bb7476ee92c365fbea1f156c73c6954fe40

---
+++ experimental/skrive/include/SkRive.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkRive_DEFINED
+#define SkRive_DEFINED
+
+#endif // SkRive_DEFINED
