23239e332de89d434253c181d64e719efcb591cf
Mike Reed <reed@google.com>
2020-06-09 17:14:20 -0400

    move shader-specific checks out of blitter chooser
    Funny
    There are slight diffs, because we *were* forcing ourselves onto
    raster-pipeline whenever we saw kHigh_SkFilterQuality... even for shaders
    that don't respect filtering! (like gradients).
    
    
    Change-Id: Ia9d2931651be79b5ee913e97e26f53b777a3a8e3

--- src/core/SkBlitter.cpp
+++ src/core/SkBlitter.cpp
@@ -644,6 +644,8 @@ SkBlitter* SkBlitterClipper::apply(SkBlitter* blitter, const SkRegion* clip,
 
 bool SkBlitter::UseRasterPipelineBlitter(const SkPixmap& device, const SkPaint& paint,
                                          const SkMatrix& matrix) {
+    SkASSERT(paint.getColorFilter() == nullptr);
+
     if (gSkForceRasterPipelineBlitter) {
         return true;
     }
@@ -655,10 +657,7 @@ bool SkBlitter::UseRasterPipelineBlitter(const SkPixmap& device, const SkPaint& 
 
     // The legacy blitters cannot handle any of these complex features (anymore).
     if (device.alphaType() == kUnpremul_SkAlphaType        ||
-        matrix.hasPerspective()                            ||
-        paint.getColorFilter()                             ||
         paint.getBlendMode() > SkBlendMode::kLastCoeffMode ||
-        paint.getFilterQuality() == kHigh_SkFilterQuality  ||
         (mf && mf->getFormat() == SkMask::k3D_Format)) {
         return true;
     }
