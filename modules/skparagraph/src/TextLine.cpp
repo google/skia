252e970d2cf44ad11b9e019eebc6a82de0339ee5
Julia Lavrova <jlavrova@google.com>
2020-06-09 10:58:02 -0400

    Removing fOffsets array from Run; optimizing copyTo
    Change-Id: I4df661f800436ddb2b2bdb0cf19017e651332971

--- modules/skparagraph/src/TextLine.cpp
+++ modules/skparagraph/src/TextLine.cpp
@@ -368,15 +368,16 @@ void TextLine::paintText(SkCanvas* canvas, TextRange textRange, const TextStyle&
     }
 
     // TODO: This is the change for flutter, must be removed later
-    SkScalar correctedBaseline = SkScalarFloorToScalar(this->baseline() + 0.5);
     SkTextBlobBuilder builder;
-    context.run->copyTo(builder, SkToU32(context.pos), context.size, SkVector::Make(context.fTextShift, correctedBaseline));
+    context.run->copyTo(builder, SkToU32(context.pos), context.size);
     if (context.clippingNeeded) {
         canvas->save();
         canvas->clipRect(extendHeight(context).makeOffset(this->offset()));
     }
 
-    canvas->drawTextBlob(builder.make(), this->offset().fX, this->offset().fY, paint);
+    SkScalar correctedBaseline = SkScalarFloorToScalar(this->baseline() + 0.5);
+    canvas->drawTextBlob(builder.make(),
+        this->offset().fX + context.fTextShift, this->offset().fY + correctedBaseline, paint);
 
     if (context.clippingNeeded) {
         canvas->restore();
@@ -403,7 +404,7 @@ void TextLine::paintShadow(SkCanvas* canvas, TextRange textRange, const TextStyl
         }
 
         SkTextBlobBuilder builder;
-        context.run->copyTo(builder, context.pos, context.size, SkVector::Make(context.fTextShift, shiftDown));
+        context.run->copyTo(builder, context.pos, context.size);
 
         if (context.clippingNeeded) {
             canvas->save();
@@ -411,7 +412,10 @@ void TextLine::paintShadow(SkCanvas* canvas, TextRange textRange, const TextStyl
             clip.offset(this->offset());
             canvas->clipRect(clip);
         }
-        canvas->drawTextBlob(builder.make(), this->offset().fX + shadow.fOffset.x(), this->offset().fY + shadow.fOffset.y(), paint);
+        canvas->drawTextBlob(builder.make(),
+            this->offset().fX + shadow.fOffset.x() + context.fTextShift,
+            this->offset().fY + shadow.fOffset.y() + shiftDown,
+            paint);
 
         if (context.clippingNeeded) {
             canvas->restore();
