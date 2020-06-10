d44964a4b271ac0a27dd17694d818590910cd066
Brian Osman <brianosman@google.com>
2020-06-10 16:08:54 -0400

    Moved SampleMatrix computation out of SectionAndParameterHelper
    This will let us share this code with the pipeline stage code generator,
    which doesn't use the helper.
    
    Change-Id: I8df3a71d448697d98b1fcbabda524e48e42db591

--- src/sksl/SkSLSectionAndParameterHelper.h
+++ src/sksl/SkSLSectionAndParameterHelper.h
@@ -9,7 +9,6 @@
 #define SKSL_SECTIONANDPARAMETERHELPER
 
 #include "src/sksl/SkSLErrorReporter.h"
-#include "src/sksl/SkSLSampleMatrix.h"
 #include "src/sksl/ir/SkSLProgram.h"
 #include "src/sksl/ir/SkSLSection.h"
 #include "src/sksl/ir/SkSLVarDeclarations.h"
@@ -65,8 +64,6 @@ public:
 
     bool hasCoordOverrides(const Variable& fp);
 
-    SampleMatrix getMatrix(const Variable& fp);
-
     static bool IsParameter(const Variable& var) {
         return (var.fModifiers.fFlags & Modifiers::kIn_Flag) &&
                -1 == var.fModifiers.fLayout.fBuiltin;
@@ -117,16 +114,6 @@ private:
     bool hasCoordOverrides(const Expression& e, const Variable& fp);
 
     bool hasCoordOverrides(const ProgramElement& p, const Variable& fp);
-
-    SampleMatrix getMatrix(const Statement& s, const Variable& fp);
-
-    SampleMatrix getMatrix(const Expression& e, const Variable& fp);
-
-    SampleMatrix getMatrix(const Statement* s, const Variable& fp);
-
-    SampleMatrix getMatrix(const Expression* e, const Variable& fp);
-
-    SampleMatrix getMatrix(const ProgramElement& p, const Variable& fp);
 
     const Program& fProgram;
     std::vector<const Variable*> fParameters;
