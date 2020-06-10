f018d948bcfc20001557d5f14d4c5bd7256a060f
John Stiles <johnstiles@google.com>
2020-06-10 12:31:14 -0400

    Update GrRectBlurEffect to use a child FP.
    We are updating FPs to receive their input via a child FP where
    possible, instead of relying on the input color.
    
    Additionally, rewrote the FP logic slightly to operate on half2s where
    possible instead of operating on single half-values at a time.
    
    Change-Id: I87ffa11cf723fab614729789556e76ba82b3c2f4
    Bug: skia:10217

--- src/core/SkBlurMF.cpp
+++ src/core/SkBlurMF.cpp
@@ -763,8 +763,9 @@ bool SkBlurMaskFilterImpl::directFilterMaskGPU(GrRecordingContext* context,
 
     if (devRRect.isRect() || SkRRectPriv::IsCircle(devRRect)) {
         if (devRRect.isRect()) {
-            fp = GrRectBlurEffect::Make(context, *context->priv().caps()->shaderCaps(),
-                                        devRRect.rect(), xformedSigma);
+            fp = GrRectBlurEffect::Make(
+                    /*inputFP=*/nullptr, context, *context->priv().caps()->shaderCaps(),
+                    devRRect.rect(), xformedSigma);
         } else {
             fp = GrCircleBlurFragmentProcessor::Make(/*inputFP=*/nullptr, context, devRRect.rect(),
                                                      xformedSigma);
