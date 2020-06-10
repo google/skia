ee2ffc3b43d0edbb4a4dcbe67658c70d40822abb
Ben Wagner <bungeman@google.com>
2020-06-10 18:07:40 -0400

    Delete .a file before writing static library.
    The ar utility only adds symbols. As a result removed symbols are
    sometimes still present in later builds, leading to great confusion.
    Delete the .a file before recreating it with ar.
    
    This is similar to what Chromium currently does in gcc_toolchain.gni.
    However, we cannot always just use 'rm' because of the build for Android
    on Windows, so this introduces 'rm.py' which is just like 'cp.py' but
    without the copy part.
    
    Bug: skia:10363
    Change-Id: Icc0c3d18dab1e48ccfec47386662c7b4d2dc8811
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295569
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

---
+++ gn/rm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Google Inc.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import shutil
+import sys
+
+dst, = sys.argv[1:]
+
+if os.path.exists(dst):
+  if os.path.isdir(dst):
+    shutil.rmtree(dst)
+  else:
+    os.remove(dst)
