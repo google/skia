60ef77791b975dbf1e1df46c25a37b7bd42b9d59
Lingyun Cai <lingyun.cai@intel.com>
2020-06-10 10:34:35 +0800

    Add AVX512 implementations for SkSwizzler opts
    nanobench performance improvements are observed using icelake cpu.
    
                            before      after       improvement
    RGBA_to_BGRA            144  ns     48.9 ns     66%
    RGBA_to_bgrA            322  ns     137  ns     57%
    RGBA_to_rgbA            321  ns     136  ns     58%
    RGB_to_BGR1*            160  ns     150  ns     6%
    RGB_to_RGB1*            161  ns     163  ns     -
    grayA_to_rgbA           177  ns     79.6 ns     55%
    grayA_to_RGBA           110  ns     62.2 ns     43%
    gray_to_RGB1*           90.1 ns     70.1 ns     22%
    inverted_CMYK_to_BGR1   308  ns     133  ns     57%
    inverted_CMYK_to_RGB1   309  ns     135  ns     56%
    
    *Note. For RGB_to_BGR1, RGB_to_RGB1 and gray_to_RGB1, better perf
    is observed using AVX2 intrinsics over AVX-512 intrinsics. So use
    AVX2 intrinsics instead.
    
    Change-Id: Iaf1685be46fc015e2431665337b8562ea3318cf6

--- src/opts/SkOpts_skx.cpp
+++ src/opts/SkOpts_skx.cpp
@@ -9,11 +9,19 @@
 
 #define SK_OPTS_NS skx
 #include "src/opts/SkBlitRow_opts.h"
+#include "src/opts/SkSwizzler_opts.h"
 #include "src/opts/SkVM_opts.h"
 
 namespace SkOpts {
     void Init_skx() {
         blit_row_s32a_opaque = SK_OPTS_NS::blit_row_s32a_opaque;
         interpret_skvm = SK_OPTS_NS::interpret_skvm;
+        RGBA_to_BGRA          = SK_OPTS_NS::RGBA_to_BGRA;
+        RGBA_to_rgbA          = SK_OPTS_NS::RGBA_to_rgbA;
+        RGBA_to_bgrA          = SK_OPTS_NS::RGBA_to_bgrA;
+        grayA_to_RGBA         = SK_OPTS_NS::grayA_to_RGBA;
+        grayA_to_rgbA         = SK_OPTS_NS::grayA_to_rgbA;
+        inverted_CMYK_to_RGB1 = SK_OPTS_NS::inverted_CMYK_to_RGB1;
+        inverted_CMYK_to_BGR1 = SK_OPTS_NS::inverted_CMYK_to_BGR1;
     }
 }
