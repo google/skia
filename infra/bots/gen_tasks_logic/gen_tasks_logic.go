51891392d8612a0bc8717d5ceab16ab8df5e1e00
Kevin Lubick <kjlubick@google.com>
2020-06-10 15:10:16 -0400

    [canvaskit] Add benchmarks on SKPs
    For each skp in the corpus, we start a fresh instance of
    Chromium (via puppeteer), draw the skp and measure that time.
    This process is repeated a fixed amount of repetitions
    and the median, the average, and the std deviation is reported
    to perf (as well as the individual datapoints as an FYI).
    
    Importantly (and something we'll need to change about
    SkottieFrames), we measure the average time between frames
    after unlocking the framerate. This ensures we account for
    the time needed by the GPU to actually draw (flush() returns
    after the GPU has all the instructions, but not necessarily
    has been able to draw).
    
    This implementation is very similar to the SkottieFrames
    code; a notable deviation is the repetitions are handled
    outside of the html, i.e. a new chrome window per run.
    
    I explored using content_shell, but noticed that requires
    building Chromium, which our infrastructure is not set up
    to do well.
    
    Change-Id: I14fdbdc951604d3fdf06e81a4be7e614d0e53c03
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295079
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Nathaniel Nifong <nifong@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -443,7 +443,7 @@ func (b *jobBuilder) deriveCompileTaskName() string {
 				"ReleaseAndAbandonGpuContext", "CCPR", "FSAA", "FAAA", "FDAA", "NativeFonts", "GDI",
 				"NoGPUThreads", "ProcDump", "DDL1", "DDL3", "T8888", "DDLTotal", "DDLRecord", "9x9",
 				"BonusConfigs", "SkottieTracing", "SkottieWASM", "GpuTess", "NonNVPR", "Mskp",
-				"Docker", "PDF", "SkVM", "Puppeteer", "SkottieFrames"}
+				"Docker", "PDF", "SkVM", "Puppeteer", "SkottieFrames", "RenderSKP"}
 			keep := make([]string, 0, len(ec))
 			for _, part := range ec {
 				if !In(part, ignore) {
@@ -1401,41 +1401,64 @@ func (b *jobBuilder) puppeteer() {
 	compileTaskName := b.compile()
 	b.addTask(b.Name, func(b *taskBuilder) {
 		b.defaultSwarmDimensions()
-		b.isolate("perf_puppeteer.isolate")
-		b.cmd(
-			"./perf_puppeteer_skottie_frames",
-			"--project_id", "skia-swarming-bots",
-			"--git_hash", specs.PLACEHOLDER_REVISION,
-			"--task_id", specs.PLACEHOLDER_TASK_ID,
-			"--task_name", b.Name,
-			"--canvaskit_bin_path", "./build",
-			"--lotties_path", "./lotties_with_assets",
-			"--node_bin_path", "./node/node/bin",
-			"--benchmark_path", "./tools/perf-canvaskit-puppeteer",
-			"--output_path", OUTPUT_PERF,
-			"--os_trace", b.parts["os"],
-			"--model_trace", b.parts["model"],
-			"--cpu_or_gpu_trace", b.parts["cpu_or_gpu"],
-			"--cpu_or_gpu_value_trace", b.parts["cpu_or_gpu_value"],
-			"--alsologtostderr",
-		)
-		b.serviceAccount(b.cfg.ServiceAccountCompile)
-		// This CIPD package was made by hand with the following invocation:
-		//   cipd create -name skia/internal/lotties_with_assets -in ./lotties/ -tag version:0
-		//   cipd acl-edit skia/internal/lotties_with_assets -reader group:project-skia-external-task-accounts
-		//   cipd acl-edit skia/internal/lotties_with_assets -reader user:pool-skia@chromium-swarm.iam.gserviceaccount.com
-		// Where lotties is a hand-selected set of lottie animations and (optionally) assets used in
-		// them (e.g. fonts, images).
-		b.cipd(&specs.CipdPackage{
-			Name:    "skia/internal/lotties_with_assets",
-			Path:    "lotties_with_assets",
-			Version: "version:0",
-		})
 		b.usesNode()
 		b.cipd(CIPD_PKG_LUCI_AUTH)
 		b.dep(b.buildTaskDrivers(), compileTaskName)
 		b.output(OUTPUT_PERF)
 		b.timeout(20 * time.Minute)
+		b.isolate("perf_puppeteer.isolate")
+		b.serviceAccount(b.cfg.ServiceAccountCompile)
+
+		if b.extraConfig("SkottieFrames") {
+			b.cmd(
+				"./perf_puppeteer_skottie_frames",
+				"--project_id", "skia-swarming-bots",
+				"--git_hash", specs.PLACEHOLDER_REVISION,
+				"--task_id", specs.PLACEHOLDER_TASK_ID,
+				"--task_name", b.Name,
+				"--canvaskit_bin_path", "./build",
+				"--lotties_path", "./lotties_with_assets",
+				"--node_bin_path", "./node/node/bin",
+				"--benchmark_path", "./tools/perf-canvaskit-puppeteer",
+				"--output_path", OUTPUT_PERF,
+				"--os_trace", b.parts["os"],
+				"--model_trace", b.parts["model"],
+				"--cpu_or_gpu_trace", b.parts["cpu_or_gpu"],
+				"--cpu_or_gpu_value_trace", b.parts["cpu_or_gpu_value"],
+				"--alsologtostderr",
+			)
+			// This CIPD package was made by hand with the following invocation:
+			//   cipd create -name skia/internal/lotties_with_assets -in ./lotties/ -tag version:0
+			//   cipd acl-edit skia/internal/lotties_with_assets -reader group:project-skia-external-task-accounts
+			//   cipd acl-edit skia/internal/lotties_with_assets -reader user:pool-skia@chromium-swarm.iam.gserviceaccount.com
+			// Where lotties is a hand-selected set of lottie animations and (optionally) assets used in
+			// them (e.g. fonts, images).
+			b.cipd(&specs.CipdPackage{
+				Name:    "skia/internal/lotties_with_assets",
+				Path:    "lotties_with_assets",
+				Version: "version:0",
+			})
+		} else if b.extraConfig("RenderSKP") {
+			b.cmd(
+				"./perf_puppeteer_render_skps",
+				"--project_id", "skia-swarming-bots",
+				"--git_hash", specs.PLACEHOLDER_REVISION,
+				"--task_id", specs.PLACEHOLDER_TASK_ID,
+				"--task_name", b.Name,
+				"--canvaskit_bin_path", "./build",
+				"--skps_path", "./skp",
+				"--node_bin_path", "./node/node/bin",
+				"--benchmark_path", "./tools/perf-canvaskit-puppeteer",
+				"--output_path", OUTPUT_PERF,
+				"--os_trace", b.parts["os"],
+				"--model_trace", b.parts["model"],
+				"--cpu_or_gpu_trace", b.parts["cpu_or_gpu"],
+				"--cpu_or_gpu_value_trace", b.parts["cpu_or_gpu_value"],
+				"--alsologtostderr",
+			)
+			b.asset("skp")
+		}
+
 	})
 
 	// Upload results to Perf after.
