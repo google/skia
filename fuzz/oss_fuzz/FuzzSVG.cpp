ea2cdf4afbedeccc009e99c65d3b9c2928d92e09
Zepeng Hu <zepenghu@google.com>
2020-06-10 12:38:27 +0000

    add svg fuzzer

--- fuzz/oss_fuzz/FuzzSVG.cpp
+++ fuzz/oss_fuzz/FuzzSVG.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2020 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #include "include/core/SkData.h"
 #include "include/core/SkStream.h"
 #include "experimental/svg/model/SkSVGDOM.h"
