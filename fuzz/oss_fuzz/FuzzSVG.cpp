ccb429cd23b69c31e96582bf0bdb97d133b194ae
Zepeng Hu <zepenghu@google.com>
2020-06-10 12:46:35 +0000

    add svg fuzzer
    Change-Id: I5c4c978c35462e41379939e92fb354dbb40606f8

---
+++ fuzz/oss_fuzz/FuzzSVG.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "experimental/svg/model/SkSVGDOM.h"
+#include "include/core/SkData.h"
+#include "include/core/SkStream.h"
+void FuzzSVG(sk_sp<SkData> bytes) {
+    SkMemoryStream stream(bytes);
+    auto dom = SkSVGDOM::MakeFromStream(stream);
+    if (!dom) {
+        return;
+    }
+}
+
+#if defined(IS_FUZZING_WITH_LIBFUZZER)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    auto bytes = SkData::MakeWithoutCopy(data, size);
+    FuzzSVG(bytes);
+    return 0;
+}
+#endif
