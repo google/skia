ff69cf937221b2975e582e7c2ef96a23765b7373
Robert Phillips <robertphillips@google.com>
2020-06-11 08:47:17 -0400

    Munge
    Change-Id: I75451f6ca934f7b59c48349c77234856d0946a12

---
+++ tools/gpu/vk/VkTestHelper.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef VkTestHelper_DEFINED
+#define VkTestHelper_DEFINED
+
+#include "include/core/SkTypes.h"
+
+#ifdef SK_VULKAN
+
+#include "include/core/SkRefCnt.h"
+#include "include/gpu/vk/GrVkBackendContext.h"
+#include "include/gpu/vk/GrVkExtensions.h"
+
+class GrContext;
+class SkSurface;
+
+#define DECLARE_VK_PROC(name) PFN_vk##name fVk##name
+
+class VkTestHelper {
+public:
+    VkTestHelper(bool isProtected) : fIsProtected(isProtected) {}
+
+    ~VkTestHelper() {
+        this->cleanup();
+    }
+
+    bool init();
+
+    GrContext* grContext() { return fGrContext1.get(); }
+
+   private:
+       void cleanup();
+
+       DECLARE_VK_PROC(DestroyInstance);
+       DECLARE_VK_PROC(DeviceWaitIdle);
+       DECLARE_VK_PROC(DestroyDevice);
+
+       DECLARE_VK_PROC(GetPhysicalDeviceFormatProperties);
+       DECLARE_VK_PROC(GetPhysicalDeviceMemoryProperties);
+
+       DECLARE_VK_PROC(CreateImage);
+       DECLARE_VK_PROC(DestroyImage);
+       DECLARE_VK_PROC(GetImageMemoryRequirements);
+       DECLARE_VK_PROC(AllocateMemory);
+       DECLARE_VK_PROC(FreeMemory);
+       DECLARE_VK_PROC(BindImageMemory);
+       DECLARE_VK_PROC(MapMemory);
+       DECLARE_VK_PROC(UnmapMemory);
+       DECLARE_VK_PROC(FlushMappedMemoryRanges);
+       DECLARE_VK_PROC(GetImageSubresourceLayout);
+
+       bool fIsProtected = false;
+       VkDevice fDevice1 = VK_NULL_HANDLE;
+
+       GrVkExtensions fExtensions1;
+       VkPhysicalDeviceFeatures2 fFeatures1 = {};
+       VkDebugReportCallbackEXT fDebugCallback1 = VK_NULL_HANDLE;
+       PFN_vkDestroyDebugReportCallbackEXT fDestroyDebugCallback1 = nullptr;
+       GrVkBackendContext fBackendContext1;
+       sk_sp<GrContext> fGrContext1;
+};
+
+#undef DECLARE_VK_PROC
+
+#endif // SK_VULKAN
+#endif // VkTestHelper_DEFINED
+
