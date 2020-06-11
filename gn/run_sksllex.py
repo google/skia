0c8a191f0b54e63ad69991c4c0fc7c8f585b633f
John Stiles <johnstiles@google.com>
2020-06-11 10:50:44 -0400

    Reland "Fetch clang-format automatically when compiling .fp files."
    This is a reland of a25422faa462e8cfb00af1a3818547abbbb9c7f9
    
    Original change's description:
    > Fetch clang-format automatically when compiling .fp files.
    > 
    > On a freshly fetched repo, setting `skia_compile_processors = true` will
    > fail to compile because clang-format is missing from the bin directory.
    > This CL automatically runs fetch-clang-format for you when clang-format
    > is absent.
    > 
    > Change-Id: Ieeb359176072e92ca235316c820310333732f608
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295780
    > Reviewed-by: Mike Klein <mtklein@google.com>
    > Commit-Queue: John Stiles <johnstiles@google.com>
    
    Change-Id: Ib397716771f742f192ebd6ed7ec0a9915b2400c5

--- gn/run_sksllex.py
+++ gn/run_sksllex.py
@@ -11,11 +11,17 @@ import sys
 
 sksllex = sys.argv[1]
 clangFormat = sys.argv[2]
-src = sys.argv[3]
+fetchClangFormat = sys.argv[3]
+src = sys.argv[4]
+
 try:
     subprocess.check_output([sksllex, src + "/sksl/lex/sksl.lex", "Lexer",
                              "Token", src + "/sksl/SkSLLexer.h", src +
                              "/sksl/SkSLLexer.cpp"])
+
+    if not os.path.isfile(clangFormat):
+        subprocess.check_call(fetchClangFormat)
+
     subprocess.check_call(clangFormat + " -i \"" + src + "/sksl/SkSLLexer.h\"",
                           shell=True)
     subprocess.check_call(clangFormat + " -i \"" + src +
