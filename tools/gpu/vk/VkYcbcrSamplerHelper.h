85aa42820e2435de98a20bdf9dab277fdd2a0311
Robert Phillips <robertphillips@google.com>
2020-06-11 10:54:43 -0400

    Move VkYcbcrSamplerHelper's context functionality into VkTestHelper
    This should make the YCbCr sampler helper usable in a GM (with a normal Vk context).
    
    Change-Id: I75451f6ca934f7b59c48349c77234856d0946a12
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/295766
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tools/gpu/vk/VkYcbcrSamplerHelper.h
+++ tools/gpu/vk/VkYcbcrSamplerHelper.h
@@ -14,66 +14,35 @@
 
 #include "include/core/SkRefCnt.h"
 #include "include/gpu/GrBackendSurface.h"
-#include "include/gpu/vk/GrVkBackendContext.h"
-#include "include/gpu/vk/GrVkExtensions.h"
 
 class GrContext;
+class GrVkGpu;
 class SkImage;
 
-#define DECLARE_VK_PROC(name) PFN_vk##name fVk##name
-
+// This helper will create a YCbCr-backed SkImage for Vulkan. Images of this format are
+// particularly interesting because their samplers are immutable.
 class VkYcbcrSamplerHelper {
 public:
-    VkYcbcrSamplerHelper();
+    VkYcbcrSamplerHelper(GrContext*);
     ~VkYcbcrSamplerHelper();
 
-    bool init();
-
-    bool isYCbCrSupported() const;
+    // This is wrt the supplied GrContext
+    bool isYCbCrSupported();
 
     sk_sp<SkImage> createI420Image(uint32_t width, uint32_t height);
-
-    GrContext* getGrContext() { return fGrContext.get(); }
 
     static int GetExpectedY(int x, int y, int width, int height);
     static std::pair<int, int> GetExpectedUV(int x, int y, int width, int height);
 
 private:
-    GrVkExtensions fExtensions;
-    VkPhysicalDeviceFeatures2 fFeatures = {};
-    VkDebugReportCallbackEXT fDebugCallback = VK_NULL_HANDLE;
+    GrVkGpu* vkGpu();
 
-    DECLARE_VK_PROC(DestroyInstance);
-    DECLARE_VK_PROC(DeviceWaitIdle);
-    DECLARE_VK_PROC(DestroyDevice);
-
-    DECLARE_VK_PROC(GetPhysicalDeviceFormatProperties);
-    DECLARE_VK_PROC(GetPhysicalDeviceMemoryProperties);
-
-    DECLARE_VK_PROC(CreateImage);
-    DECLARE_VK_PROC(DestroyImage);
-    DECLARE_VK_PROC(GetImageMemoryRequirements);
-    DECLARE_VK_PROC(AllocateMemory);
-    DECLARE_VK_PROC(FreeMemory);
-    DECLARE_VK_PROC(BindImageMemory);
-    DECLARE_VK_PROC(MapMemory);
-    DECLARE_VK_PROC(UnmapMemory);
-    DECLARE_VK_PROC(FlushMappedMemoryRanges);
-    DECLARE_VK_PROC(GetImageSubresourceLayout);
-
-    VkDevice fDevice = VK_NULL_HANDLE;
-
-    PFN_vkDestroyDebugReportCallbackEXT fDestroyDebugCallback = nullptr;
-
-    GrVkBackendContext fBackendContext;
-    sk_sp<GrContext> fGrContext;
+    GrContext* fContext;
 
     VkImage fImage = VK_NULL_HANDLE;
     VkDeviceMemory fImageMemory = VK_NULL_HANDLE;
     GrBackendTexture fTexture;
 };
-
-#undef DECLARE_VK_PROC
 
 #endif // SK_VULKAN
 
