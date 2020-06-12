6b5388364c5e0fa22323170038114c21487bfeeb
Herb Derby <herb@google.com>
2020-06-12 15:46:06 -0400

    GrTextContext::Options -> GrSDFTOptions
    Rename GrTextContext::Options to GrSDFTOptions. Remove GrTextContext.
    
    Change-Id: Ic89966cfe630cc6ee757977a6d63ae1da9e39ef0

--- src/gpu/GrContext.cpp
+++ src/gpu/GrContext.cpp
@@ -32,7 +32,7 @@
 #include "src/gpu/effects/GrSkSLFP.h"
 #include "src/gpu/text/GrStrikeCache.h"
 #include "src/gpu/text/GrTextBlobCache.h"
-#include "src/gpu/text/GrTextContext.h"
+#include "src/gpu/text/GrSDFTOptions.h"
 #include "src/image/SkImage_GpuBase.h"
 #include "src/image/SkSurface_Gpu.h"
 #include <atomic>
