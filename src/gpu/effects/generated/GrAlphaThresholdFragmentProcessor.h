8caca6cff47127e9085ceb028880da8197472ce2
Brian Salomon <bsalomon@google.com>
2020-06-15 09:25:51 -0400

    GrAlphaThresholdFragmentProcessor uses child processor for mask.
    Bug: skia:10139
    
    Change-Id: I526f8ca77de0ed6ab1fe16b22201043ebf5c3716

--- src/gpu/effects/generated/GrAlphaThresholdFragmentProcessor.h
+++ src/gpu/effects/generated/GrAlphaThresholdFragmentProcessor.h
@@ -20,51 +20,42 @@
 class GrAlphaThresholdFragmentProcessor : public GrFragmentProcessor {
 public:
     static std::unique_ptr<GrFragmentProcessor> Make(std::unique_ptr<GrFragmentProcessor> inputFP,
-                                                     GrSurfaceProxyView mask,
+                                                     std::unique_ptr<GrFragmentProcessor> maskFP,
                                                      float innerThreshold,
-                                                     float outerThreshold,
-                                                     const SkIRect& bounds) {
+                                                     float outerThreshold) {
         return std::unique_ptr<GrFragmentProcessor>(new GrAlphaThresholdFragmentProcessor(
-                std::move(inputFP), std::move(mask), innerThreshold, outerThreshold, bounds));
+                std::move(inputFP), std::move(maskFP), innerThreshold, outerThreshold));
     }
     GrAlphaThresholdFragmentProcessor(const GrAlphaThresholdFragmentProcessor& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
     const char* name() const override { return "AlphaThresholdFragmentProcessor"; }
-    GrCoordTransform maskCoordTransform;
     int inputFP_index = -1;
-    TextureSampler mask;
+    int maskFP_index = -1;
     float innerThreshold;
     float outerThreshold;
 
 private:
     GrAlphaThresholdFragmentProcessor(std::unique_ptr<GrFragmentProcessor> inputFP,
-                                      GrSurfaceProxyView mask,
+                                      std::unique_ptr<GrFragmentProcessor> maskFP,
                                       float innerThreshold,
-                                      float outerThreshold,
-                                      const SkIRect& bounds)
+                                      float outerThreshold)
             : INHERITED(kGrAlphaThresholdFragmentProcessor_ClassID,
                         (OptimizationFlags)(inputFP ? ProcessorOptimizationFlags(inputFP.get())
                                                     : kAll_OptimizationFlags) &
                                 (kCompatibleWithCoverageAsAlpha_OptimizationFlag |
                                  ((outerThreshold >= 1.0) ? kPreservesOpaqueInput_OptimizationFlag
                                                           : kNone_OptimizationFlags)))
-            , maskCoordTransform(
-                      SkMatrix::Translate(SkIntToScalar(-bounds.x()), SkIntToScalar(-bounds.y())),
-                      mask.proxy(),
-                      mask.origin())
-            , mask(std::move(mask))
             , innerThreshold(innerThreshold)
             , outerThreshold(outerThreshold) {
         if (inputFP) {
             inputFP_index = this->registerChildProcessor(std::move(inputFP));
         }
-        this->setTextureSamplerCnt(1);
-        this->addCoordTransform(&maskCoordTransform);
+        SkASSERT(maskFP);
+        maskFP_index = this->registerChildProcessor(std::move(maskFP));
     }
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
     void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
     bool onIsEqual(const GrFragmentProcessor&) const override;
-    const TextureSampler& onTextureSampler(int) const override;
     GR_DECLARE_FRAGMENT_PROCESSOR_TEST
     typedef GrFragmentProcessor INHERITED;
 };
