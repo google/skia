fc9b115c469e23d8aac2d7bfe883b7cde64b0a62
Robert Phillips <robertphillips@google.com>
2020-06-15 13:31:44 -0400

    [Omnibus] Add option to DDL test harness to better match OOP-R behavior
    Change-Id: Ifa3002af73eb9926f653fb4c4bf4542c0749d658

--- gm/ycbcrimage.cpp
+++ gm/ycbcrimage.cpp
@@ -16,8 +16,42 @@
 #include "include/core/SkSize.h"
 #include "include/core/SkString.h"
 #include "include/gpu/GrContext.h"
+#include "src/gpu/GrContextPriv.h"
+#include "tools/gpu/vk/VkTestHelper.h"
 #include "tools/gpu/vk/VkYcbcrSamplerHelper.h"
 
+// A GrContext/VkYcbcrSamplerHelper pair that allows the YCbCr image's backing memory to live
+// beyond the lifespan of the SkImage.
+class YCbCrCallbackContext {
+public:
+    static void Release(void* releaseContext) {
+        YCbCrCallbackContext* ycbcrContxt = reinterpret_cast<YCbCrCallbackContext*>(releaseContext);
+
+        ycbcrContxt->fYCbCrHelper.destroyBackendTexture();
+        delete ycbcrContxt;
+    }
+
+    YCbCrCallbackContext(GrContext* context) : fYCbCrHelper(context) {}
+
+    skiagm::DrawResult allocVkMemory(int width, int height, SkString* errorMsg) {
+        if (!fYCbCrHelper.isYCbCrSupported()) {
+            *errorMsg = "YCbCr sampling not supported.";
+            return skiagm::DrawResult::kSkip;
+        }
+
+        if (!fYCbCrHelper.createBackendTexture(width, height)) {
+            *errorMsg = "Failed to create I420 backend texture.";
+            return skiagm::DrawResult::kFail;
+        }
+
+        return skiagm::DrawResult::kOk;
+    }
+
+    const GrBackendTexture& backendTexture() { return fYCbCrHelper.backendTexture(); }
+
+private:
+    VkYcbcrSamplerHelper fYCbCrHelper;
+};
 
 namespace skiagm {
 
@@ -38,6 +72,44 @@ protected:
         return SkISize::Make(2*kPad+kImageSize, 2*kPad+kImageSize);
     }
 
+    DrawResult createYCbCrImage(GrContext* context, SkString* errorMsg) {
+        std::unique_ptr<YCbCrCallbackContext> ycbcrContext(new YCbCrCallbackContext(context));
+
+        DrawResult result = ycbcrContext->allocVkMemory(kImageSize, kImageSize, errorMsg);
+        if (result != DrawResult::kOk) {
+            return result;
+        }
+
+        SkASSERT(!fYCbCrImage);
+        fYCbCrImage = SkImage::MakeFromTexture(context, ycbcrContext->backendTexture(),
+                                               kTopLeft_GrSurfaceOrigin, kRGB_888x_SkColorType,
+                                               kPremul_SkAlphaType, nullptr,
+                                               YCbCrCallbackContext::Release, ycbcrContext.get());
+        if (!fYCbCrImage) {
+            *errorMsg = "Failed to create I420 image.";
+            return DrawResult::kFail;
+        }
+
+        ycbcrContext.release();
+        return DrawResult::kOk;
+    }
+
+    DrawResult onGpuSetup(GrContext* context, SkString* errorMsg) override {
+        SkASSERT(context->priv().asDirectContext());
+
+        if (context->backend() != GrBackendApi::kVulkan) {
+            *errorMsg = "This GM requires a Vulkan context.";
+            return DrawResult::kSkip;
+        }
+
+        DrawResult result = this->createYCbCrImage(context, errorMsg);
+        if (result != DrawResult::kOk) {
+            return result;
+        }
+
+        return DrawResult::kOk;
+    }
+
     DrawResult onDraw(GrContext* context, GrRenderTargetContext*, SkCanvas* canvas,
                       SkString* errorMsg) override {
         if (context->backend() != GrBackendApi::kVulkan) {
@@ -45,36 +117,20 @@ protected:
             return DrawResult::kSkip;
         }
 
-        VkYcbcrSamplerHelper ycbcrHelper(context);
-        if (!ycbcrHelper.isYCbCrSupported()) {
-            *errorMsg = "YCbCr sampling not supported.";
-            return DrawResult::kSkip;
-        }
-
-        sk_sp<SkImage> ycbcrImage = ycbcrHelper.createI420Image(kImageSize, kImageSize);
-        if (!ycbcrImage) {
-            *errorMsg = "Failed to create I420 image.";
-            return DrawResult::kFail;
-        }
+        SkASSERT(fYCbCrImage);
 
         SkPaint paint;
         paint.setFilterQuality(kLow_SkFilterQuality);
 
-        canvas->drawImage(ycbcrImage, kPad, kPad, &paint);
-
-        // The VkYcbcrSamplerHelper holds the actual memory for 'ycbcrImage' so nothing can
-        // be allowed to exist beyond this method.
-        GrFlushInfo flushInfo;
-        flushInfo.fFlags = kSyncCpu_GrFlushFlag;
-        context->flush(flushInfo);
-        context->submit(true);
-
+        canvas->drawImage(fYCbCrImage, kPad, kPad, &paint);
         return DrawResult::kOk;
     }
 
 private:
     static const int kImageSize = 112;
     static const int kPad = 8;
+
+    sk_sp<SkImage> fYCbCrImage;
 
     typedef GpuGM INHERITED;
 };
