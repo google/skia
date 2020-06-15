f146d56415eba7f9e1ba868ef8605cb9a40ce723
Robert Phillips <robertphillips@google.com>
2020-06-15 14:26:58 -0400

    Refine VkYcbcrSamplerHelper to only holding backend memory for YCbCr textures
    This arrangement allows the backend texture to outlive the YCbCr SkImage.
    
    Change-Id: I34939d05bf1091c8efcacb687dc1900729d4cbe5

--- gm/ycbcrimage.cpp
+++ gm/ycbcrimage.cpp
@@ -16,8 +16,13 @@
 #include "include/core/SkSize.h"
 #include "include/core/SkString.h"
 #include "include/gpu/GrContext.h"
+#include "src/gpu/GrContextPriv.h"
 #include "tools/gpu/vk/VkYcbcrSamplerHelper.h"
 
+static void release_ycbcrhelper(void* releaseContext) {
+    VkYcbcrSamplerHelper* ycbcrHelper = reinterpret_cast<VkYcbcrSamplerHelper*>(releaseContext);
+    delete ycbcrHelper;
+}
 
 namespace skiagm {
 
@@ -38,6 +43,49 @@ protected:
         return SkISize::Make(2*kPad+kImageSize, 2*kPad+kImageSize);
     }
 
+    DrawResult createYCbCrImage(GrContext* context, SkString* errorMsg) {
+        std::unique_ptr<VkYcbcrSamplerHelper> ycbcrHelper(new VkYcbcrSamplerHelper(context));
+
+        if (!ycbcrHelper->isYCbCrSupported()) {
+            *errorMsg = "YCbCr sampling not supported.";
+            return skiagm::DrawResult::kSkip;
+        }
+
+        if (!ycbcrHelper->createBackendTexture(kImageSize, kImageSize)) {
+            *errorMsg = "Failed to create I420 backend texture.";
+            return skiagm::DrawResult::kFail;
+        }
+
+        SkASSERT(!fYCbCrImage);
+        fYCbCrImage = SkImage::MakeFromTexture(context, ycbcrHelper->backendTexture(),
+                                               kTopLeft_GrSurfaceOrigin, kRGB_888x_SkColorType,
+                                               kPremul_SkAlphaType, nullptr,
+                                               release_ycbcrhelper, ycbcrHelper.get());
+        if (!fYCbCrImage) {
+            *errorMsg = "Failed to create I420 image.";
+            return DrawResult::kFail;
+        }
+
+        ycbcrHelper.release();
+        return DrawResult::kOk;
+    }
+
+    DrawResult onGpuSetup(GrContext* context, SkString* errorMsg) {
+        SkASSERT(context->priv().asDirectContext());
+
+        if (context->backend() != GrBackendApi::kVulkan) {
+            *errorMsg = "This GM requires a Vulkan context.";
+            return DrawResult::kSkip;
+        }
+
+        DrawResult result = this->createYCbCrImage(context, errorMsg);
+        if (result != DrawResult::kOk) {
+            return result;
+        }
+
+        return DrawResult::kOk;
+    }
+
     DrawResult onDraw(GrContext* context, GrRenderTargetContext*, SkCanvas* canvas,
                       SkString* errorMsg) override {
         if (context->backend() != GrBackendApi::kVulkan) {
@@ -45,36 +93,25 @@ protected:
             return DrawResult::kSkip;
         }
 
-        VkYcbcrSamplerHelper ycbcrHelper(context);
-        if (!ycbcrHelper.isYCbCrSupported()) {
-            *errorMsg = "YCbCr sampling not supported.";
-            return DrawResult::kSkip;
+        DrawResult result = this->onGpuSetup(context, errorMsg);
+        if (result != DrawResult::kOk) {
+            return result;
         }
 
-        sk_sp<SkImage> ycbcrImage = ycbcrHelper.createI420Image(kImageSize, kImageSize);
-        if (!ycbcrImage) {
-            *errorMsg = "Failed to create I420 image.";
-            return DrawResult::kFail;
-        }
+        SkASSERT(fYCbCrImage);
 
         SkPaint paint;
         paint.setFilterQuality(kLow_SkFilterQuality);
 
-        canvas->drawImage(ycbcrImage, kPad, kPad, &paint);
-
-        // The VkYcbcrSamplerHelper holds the actual memory for 'ycbcrImage' so nothing can
-        // be allowed to exist beyond this method.
-        GrFlushInfo flushInfo;
-        flushInfo.fFlags = kSyncCpu_GrFlushFlag;
-        context->flush(flushInfo);
-        context->submit(true);
-
+        canvas->drawImage(fYCbCrImage, kPad, kPad, &paint);
         return DrawResult::kOk;
     }
 
 private:
     static const int kImageSize = 112;
     static const int kPad = 8;
+
+    sk_sp<SkImage> fYCbCrImage;
 
     typedef GpuGM INHERITED;
 };
