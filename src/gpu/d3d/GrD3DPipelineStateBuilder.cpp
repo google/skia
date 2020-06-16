5ca9d1a3414ad89ea4f96a3286f7dc146ee76f2f
Brian Osman <brianosman@google.com>
2020-06-16 15:25:33 -0400

    Fix caching of HLSL in D3D backend
    We were always tagging the stored shaders as SkSL, so they (naturally)
    didn't compile when we loaded them.
    
    Change-Id: I96062808751b6233c9e90b29150f66270d0dd198
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/296836
    Commit-Queue: Brian Osman <brianosman@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/gpu/d3d/GrD3DPipelineStateBuilder.cpp
+++ src/gpu/d3d/GrD3DPipelineStateBuilder.cpp
@@ -628,7 +628,7 @@ sk_sp<GrD3DPipelineState> GrD3DPipelineStateBuilder::finalize() {
             sk_sp<SkData> key =
                     SkData::MakeWithoutCopy(this->desc().asKey(), this->desc().initialKeyLength());
             sk_sp<SkData> data = GrPersistentCacheUtils::PackCachedShaders(
-                    cacheSkSL ? kSKSL_Tag : kSKSL_Tag, hlsl, inputs, kGrShaderTypeCount);
+                    cacheSkSL ? kSKSL_Tag : kHLSL_Tag, hlsl, inputs, kGrShaderTypeCount);
             persistentCache->store(*key, *data);
         }
     }
