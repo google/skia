b5090bfb8fa0376cae7f48109d071a3f8379945f
Brian Osman <brianosman@google.com>
2020-06-16 12:41:25 -0400

    Hook up persistent shader cache for D3D
    Currently supports SkSL & HLSL. We should see if we can safely serialize
    the shader blobs returned by D3DCompile.
    
    We can't (yet) view the HLSL in Viewer - the shader viewer/editor needs
    an overhaul to support backend-specific source formats (MSL, HLSL,
    disassembled SPIRV). Going to look into that in a separate CL.
    
    Change-Id: Ife1fd1d3d724674b24793df2d86c52a9b8f7de23

--- src/gpu/d3d/GrD3DPipelineStateBuilder.h
+++ src/gpu/d3d/GrD3DPipelineStateBuilder.h
@@ -45,11 +45,13 @@ private:
 
     sk_sp<GrD3DPipelineState> finalize();
 
-    void compileD3DProgram(SkSL::Program::Kind kind,
-                           const SkSL::String& sksl,
-                           const SkSL::Program::Settings& settings,
-                           ID3DBlob** shader,
-                           SkSL::Program::Inputs* outInputs);
+    bool loadHLSLFromCache(SkReadBuffer* reader, gr_cp<ID3DBlob> shaders[]);
+
+    gr_cp<ID3DBlob> compileD3DProgram(SkSL::Program::Kind kind,
+                                      const SkSL::String& sksl,
+                                      const SkSL::Program::Settings& settings,
+                                      SkSL::Program::Inputs* outInputs,
+                                      SkSL::String* outHLSL);
 
     GrGLSLUniformHandler* uniformHandler() override { return &fUniformHandler; }
     const GrGLSLUniformHandler* uniformHandler() const override { return &fUniformHandler; }
