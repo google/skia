0795361b45e41c071ee6fdd96f85faab2c474747
Ben Wagner <bungeman@google.com>
2020-06-17 11:24:53 -0400

    SkUNREACHABLE should fail, not be UB.
    The use of __builtin_unreachable() and __assume(false) are actually
    injections of undefined behavior. They do not crash and they may allow
    the compiler to assume that any values which could cause that location
    to be reached cannot occur. They should only be used after code which
    cannot return but the compiler cannot know that (such as inline assembly
    which does not return and calls to functions which should have been
    declared noreturn but were not).
    
    Replace their use with __builtin_trap() and __failfast(). These are
    similar to __builtin_debugtrap() or __debugbreak() but also indicate
    that execution should not be resumed.
    
    Change-Id: I46c1362f4e86944cc8e03f6f5837875ac71b69f3
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/297024
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>

--- include/core/SkTypes.h
+++ include/core/SkTypes.h
@@ -265,9 +265,14 @@
 
 #if !defined(SkUNREACHABLE)
 #  if defined(_MSC_VER) && !defined(__clang__)
-#    define SkUNREACHABLE __assume(false)
+#    include <intrin.h>
+#    define FAST_FAIL_INVALID_ARG                 5
+// See https://developercommunity.visualstudio.com/content/problem/1128631/code-flow-doesnt-see-noreturn-with-extern-c.html
+// for why this is wrapped. Hopefully removable after msvc++ 19.27 is no longer supported.
+[[noreturn]] static inline void sk_fast_fail() { __fastfail(FAST_FAIL_INVALID_ARG); }
+#    define SkUNREACHABLE sk_fast_fail()
 #  else
-#    define SkUNREACHABLE __builtin_unreachable()
+#    define SkUNREACHABLE __builtin_trap()
 #  endif
 #endif
 
