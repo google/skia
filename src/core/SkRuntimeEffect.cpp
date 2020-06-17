d8e4292d5e39567fcaa96b7b677e95b4c2c24dad
Ethan Nicholas <ethannicholas@google.com>
2020-06-17 13:25:37 -0400

    Revert "Revert "fixed sample(..., matrix) with runtime effects""
    This reverts commit 77968f0d32a6678c0a32b75a45d14b5b6c810b59.
    
    Change-Id: I834582c1964ea8e784b4642b766122e8624939fc

--- src/core/SkRuntimeEffect.cpp
+++ src/core/SkRuntimeEffect.cpp
@@ -27,7 +27,6 @@
 #if SK_SUPPORT_GPU
 #include "include/private/GrRecordingContext.h"
 #include "src/gpu/GrColorInfo.h"
-#include "src/gpu/GrFPArgs.h"
 #include "src/gpu/effects/GrMatrixEffect.h"
 #include "src/gpu/effects/GrSkSLFP.h"
 #endif
