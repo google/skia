10567aa103f63f7df739e346b908e5b8c0cb260e
Florin Malita <fmalita@chromium.org>
2020-06-19 14:21:33 -0400

    [skrive] Reset the node system
    Tacking another hierarchy on top of SkSG doesn't work well.
    
    Let's start fresh.
    
    Change-Id: Ieb379b57e1a77df3c62048d3be7e81e1429f9b23

---
+++ experimental/skrive/tests/DomTypes.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "experimental/skrive/include/SkRive.h"
+#include "tests/Test.h"
+
+template <typename T, typename U>
+void check_type(skiatest::Reporter* reporter, const sk_sp<U>& node, bool expected) {
+    REPORTER_ASSERT(reporter, node->template is<T>() == expected);
+    REPORTER_ASSERT(reporter, !!static_cast<const T*>(*node.get()) == expected);
+}
+
+DEF_TEST(SkRive_DomTypes, reporter) {
+    {
+        auto node = sk_make_sp<skrive::Node>();
+
+        check_type<skrive::Component>(reporter, node, true);
+        check_type<skrive::Node     >(reporter, node, true);
+        check_type<skrive::Drawable >(reporter, node, false);
+        check_type<skrive::Shape    >(reporter, node, false);
+    }
+
+    {
+        auto node = sk_make_sp<skrive::Shape>();
+
+        check_type<skrive::Component>(reporter, node, true);
+        check_type<skrive::Node     >(reporter, node, true);
+        check_type<skrive::Drawable >(reporter, node, true);
+        check_type<skrive::Shape    >(reporter, node, true);
+    }
+}
