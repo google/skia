18da170cb72276d3ac6a2a6c8307a022f565e652
Mike Reed <reed@google.com>
2020-06-22 15:46:56 -0400

    WIP: add pathbuilder
        235.97  	makepath_detach_reserve	nonrendering
        274.56  	makepath_snapshot_reserve	nonrendering
        537.61  	makepath_path_reserve	nonrendering
        472.98  	makepath_detach_noreserve	nonrendering
        482.56  	makepath_snapshot_noreserve	nonrendering
        750.83 ?	makepath_path_noreserve	nonrendering
    
    - note: doesn't inject movetos if the caller forgets. should we?
    - doesn't try to track convexity or direction yet (e.g. for addrect)
    
    Bug: skia:9000
    Change-Id: I346537e899b08946c5778042a021f464006b029c

---
+++ tests/PathBuilderTest.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/core/SkPathBuilder.h"
+#include "src/core/SkPathPriv.h"
+#include "tests/Test.h"
+
+static void is_empty(skiatest::Reporter* reporter, const SkPath& p) {
+    REPORTER_ASSERT(reporter, p.getBounds().isEmpty());
+    REPORTER_ASSERT(reporter, p.countPoints() == 0);
+}
+
+DEF_TEST(pathbuilder, reporter) {
+    SkPathBuilder b;
+
+    is_empty(reporter, b.snapshot());
+    is_empty(reporter, b.detach());
+
+    b.moveTo(10, 10).lineTo(20, 20).quadTo(30, 10, 10, 20);
+
+    SkPath p0 = b.snapshot();
+    SkPath p1 = b.snapshot();
+    SkPath p2 = b.detach();
+
+    REPORTER_ASSERT(reporter, p0.getBounds() == SkRect::MakeLTRB(10, 10, 30, 20));
+    REPORTER_ASSERT(reporter, p0.countPoints() == 4);
+
+    REPORTER_ASSERT(reporter, p0 == p1);
+    REPORTER_ASSERT(reporter, p0 == p2);
+
+    is_empty(reporter, b.snapshot());
+    is_empty(reporter, b.detach());
+}
+
+static bool check_points(const SkPath& path, const SkPoint expected[], size_t count) {
+    std::vector<SkPoint> iter_pts;
+
+    for (auto [v, p, w] : SkPathPriv::Iterate(path)) {
+        switch (v) {
+            case SkPathVerb::kMove:
+                iter_pts.push_back(p[0]);
+                break;
+            case SkPathVerb::kLine:
+                iter_pts.push_back(p[1]);
+                break;
+            case SkPathVerb::kQuad:
+            case SkPathVerb::kConic:
+                iter_pts.push_back(p[1]);
+                iter_pts.push_back(p[2]);
+                break;
+            case SkPathVerb::kCubic:
+                iter_pts.push_back(p[1]);
+                iter_pts.push_back(p[2]);
+                iter_pts.push_back(p[3]);
+                break;
+            case SkPathVerb::kClose:
+                break;
+        }
+    }
+    if (iter_pts.size() != count) {
+        return false;
+    }
+    for (size_t i = 0; i < count; ++i) {
+        if (iter_pts[i] != expected[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+DEF_TEST(pathbuilder_missing_move, reporter) {
+    SkPathBuilder b;
+
+    b.lineTo(10, 10).lineTo(20, 30);
+    const SkPoint pts0[] = {
+        {0, 0}, {10, 10}, {20, 30},
+    };
+    REPORTER_ASSERT(reporter, check_points(b.snapshot(), pts0, SK_ARRAY_COUNT(pts0)));
+
+    b.reset().moveTo(20, 20).lineTo(10, 10).lineTo(20, 30).close().lineTo(60, 60);
+    const SkPoint pts1[] = {
+        {20, 20}, {10, 10}, {20, 30},
+        {20, 20}, {60, 60},
+    };
+    REPORTER_ASSERT(reporter, check_points(b.snapshot(), pts1, SK_ARRAY_COUNT(pts1)));
+}
