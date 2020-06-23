d79197dfb1ab22bd56afa13132c407516a22b906
Greg Daniel <egdaniel@google.com>
2020-06-23 10:54:57 -0400

    Add api to GrVkMemoryAllocator to return VkResult from allocate calls.
    Change-Id: I344e5d70126cce210bb9fe442ae040d13ab74c2f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/298218
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Peng Huang <penghuang@google.com>

--- src/gpu/vk/GrVkMemory.h
+++ src/gpu/vk/GrVkMemory.h
@@ -42,9 +42,9 @@ namespace GrVkMemory {
     // For the Flush and Invalidate calls, the offset should be relative to the GrVkAlloc. Thus this
     // will often be 0. The client does not need to make sure the offset and size are aligned to the
     // nonCoherentAtomSize, the internal calls will handle that.
-    void FlushMappedAlloc(const GrVkGpu* gpu, const GrVkAlloc& alloc, VkDeviceSize offset,
+    void FlushMappedAlloc(GrVkGpu* gpu, const GrVkAlloc& alloc, VkDeviceSize offset,
                           VkDeviceSize size);
-    void InvalidateMappedAlloc(const GrVkGpu* gpu, const GrVkAlloc& alloc, VkDeviceSize offset,
+    void InvalidateMappedAlloc(GrVkGpu* gpu, const GrVkAlloc& alloc, VkDeviceSize offset,
                                VkDeviceSize size);
 
     // Helper for aligning and setting VkMappedMemoryRange for flushing/invalidating noncoherent
