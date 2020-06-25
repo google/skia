84054fa89a24ffb63367d3dbc4f3042da191def4
Robert Phillips <robertphillips@google.com>
2020-06-25 12:59:38 -0400

    Make gpuSetup draw a error message on failure
    Since gpuSetup can preempt draw's execution it needs to draw the error message too.
    
    This is pulled out of the gpuSetup refactoring.
    
    Change-Id: Iafe06d924fc1b694c59aa3100e9fbe95c4773222

--- tools/viewer/GMSlide.cpp
+++ tools/viewer/GMSlide.cpp
@@ -15,6 +15,8 @@
 #include "tools/viewer/GMSlide.h"
 
 GMSlide::GMSlide(std::unique_ptr<skiagm::GM> gm) : fGM(std::move(gm)) {
+    fGM->setMode(skiagm::GM::kSample_Mode);
+
     fName.printf("GM_%s", fGM->getName());
 }
 
@@ -23,13 +25,12 @@ GMSlide::~GMSlide() = default;
 void GMSlide::draw(SkCanvas* canvas) {
     SkString msg;
 
-    fGM->setMode(skiagm::GM::kSample_Mode);
+    auto result = fGM->gpuSetup(canvas->getGrContext(), canvas, &msg);
+    if (result != skiagm::GM::DrawResult::kOk) {
+        return;
+    }
 
-    fGM->gpuSetup(canvas->getGrContext(), &msg);
-    // Do we care about timing the draw of the background (once)?
-    // Does the GM ever rely on drawBackground to lazily compute something?
-    fGM->drawBackground(canvas);
-    fGM->drawContent(canvas, &msg);
+    fGM->draw(canvas, &msg);
 }
 
 bool GMSlide::animate(double nanos) { return fGM->animate(nanos); }
