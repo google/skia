80f15ee7b5f25c349171fcf3f5ed2df7b2fa69e5
Robert Phillips <robertphillips@google.com>
2020-06-25 11:25:49 -0400

    Adjust how GM::getSetup is handled in the tools
    Change-Id: I7a49ff49030b4c8aba1f0798a3742641030fe44f

--- bench/GMBench.cpp
+++ bench/GMBench.cpp
@@ -8,6 +8,8 @@
 #include "bench/GMBench.h"
 
 GMBench::GMBench(std::unique_ptr<skiagm::GM> gm) : fGM(std::move(gm)) {
+    fGM->setMode(skiagm::GM::kBench_Mode);
+
     fName.printf("GM_%s", fGM->getName());
 }
 
@@ -19,13 +21,26 @@ bool GMBench::isSuitableFor(Backend backend) {
     return kNonRendering_Backend != backend;
 }
 
+void GMBench::onPerCanvasPreDraw(SkCanvas* canvas) {
+    if (fGM->gpuSetup(canvas->getGrContext(), canvas) != skiagm::DrawResult::kOk) {
+        fGpuSetupFailed = true;
+    }
+
+    fGM->onceBeforeDraw();
+}
+
+void GMBench::onPerCanvasPostDraw(SkCanvas*) {
+    fGM->gpuTeardown();
+    fGpuSetupFailed = false;
+}
+
 void GMBench::onDraw(int loops, SkCanvas* canvas) {
-    fGM->setMode(skiagm::GM::kBench_Mode);
-    // Do we care about timing the draw of the background (once)?
-    // Does the GM ever rely on drawBackground to lazily compute something?
+    if (fGpuSetupFailed) {
+        return;
+    }
+
     fGM->drawBackground(canvas);
     for (int i = 0; i < loops; ++i) {
-        SkAutoCanvasRestore acr(canvas, true);
         fGM->drawContent(canvas);
     }
 }
