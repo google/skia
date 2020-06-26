d2f68533a82e9509ea942248ba3f63b0b5589dad
Zepeng Hu <zepenghu@google.com>
2020-06-26 19:06:08 +0000

    working on FuzzCanvas
    Change-Id: Iaf6114bb0e2929c73549ff398c3db5592e736ea2

--- fuzz/FuzzCanvas.cpp
+++ fuzz/FuzzCanvas.cpp
@@ -23,6 +23,7 @@
 #include "include/core/SkTypeface.h"
 #include "include/docs/SkPDFDocument.h"
 #include "include/private/SkTo.h"
+#include "include/svg/SkSVGCanvas.h"
 #include "include/utils/SkNullCanvas.h"
 #include "src/core/SkOSFile.h"
 #include "src/core/SkPicturePriv.h"
@@ -957,6 +958,18 @@ static SkTDArray<uint8_t> make_fuzz_text(Fuzz* fuzz, const SkFont& font, SkTextE
     return array;
 }
 
+static const char* make_fuzz_string(Fuzz* fuzz) {
+    int len;
+    fuzz->nextRange(&len, 0, kMaxGlyphCount);
+    std::string str = "";
+    for (int i = 0; i < len; i++) {
+        char ch;
+        fuzz->next(&ch);
+        str += ch;
+    }
+    return str.c_str();
+}
+
 static sk_sp<SkTextBlob> make_fuzz_textblob(Fuzz* fuzz) {
     SkTextBlobBuilder textBlobBuilder;
     int8_t runCount;
@@ -1016,7 +1029,7 @@ static void fuzz_canvas(Fuzz* fuzz, SkCanvas* canvas, int depth = 9) {
         SkPaint paint;
         SkFont font;
         unsigned drawCommand;
-        fuzz->nextRange(&drawCommand, 0, 53);
+        fuzz->nextRange(&drawCommand, 0, 62);
         switch (drawCommand) {
             case 0:
                 canvas->flush();
@@ -1488,6 +1501,75 @@ static void fuzz_canvas(Fuzz* fuzz, SkCanvas* canvas, int depth = 9) {
                                      blendMode, paint);
                 break;
             }
+            case 54: {
+                SkColor color;
+                fuzz->next(&color);
+                canvas->drawColor(color);
+                break;
+            }
+            case 55: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                SkPoint p0, p1;
+                fuzz->next(&p0, &p1);
+                canvas->drawLine(p0, p1, paint);
+            }
+            case 56: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                SkIRect r;
+                fuzz->next(&r);
+                canvas->drawIRect(r, paint);
+                break;
+            }
+            case 57: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                SkScalar radius;
+                SkPoint center;
+                fuzz->next(&radius, &center);
+                canvas->drawCircle(center, radius, paint);
+                break;
+            }
+            case 58: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                SkRect oval;
+                SkScalar startAngle, sweepAngle;
+                bool useCenter;
+                fuzz->next(&oval, &startAngle, &sweepAngle, &useCenter);
+                canvas->drawArc(oval, startAngle, sweepAngle, useCenter, paint);
+                break;
+            }
+            case 59: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                SkRect rect;
+                SkScalar rx, ry;
+                fuzz->next(&rect, &rx, &ry);
+                canvas->drawRoundRect(rect, rx, ry, paint);
+                break;
+            }
+            case 60: {
+                fuzz_paint(fuzz, &paint, depth - 1);
+                font = fuzz_font(fuzz);
+                const char* str = make_fuzz_string(fuzz);
+                SkScalar x, y;
+                fuzz->next(&x, &y);
+                canvas->drawString(str, x, y, font, paint);
+                break;
+            }
+            case 61: {
+                SkPoint cubics[12];
+                SkColor colors[4];
+                SkPoint texCoords[4];
+                SkBlendMode mode;
+                fuzz->nextN(cubics, 12);
+                fuzz->nextN(colors, 4);
+                fuzz->nextN(texCoords, 4);
+                fuzz->nextEnum(&mode, SkBlendMode::kLastMode);
+                canvas->drawPatch(cubics, colors, texCoords, mode, paint);
+                break;
+            }
+            case 62: {
+
+                break;
+            }
             default:
                 SkASSERT(false);
                 break;
@@ -1672,4 +1754,13 @@ DEF_FUZZ(_DumpCanvas, fuzz) {
     writer.flush();
     sk_sp<SkData> json = stream.detachAsData();
     fwrite(json->data(), json->size(), 1, stdout);
+}
+
+DEF_FUZZ(SVGCanvas, fuzz) {
+
+    SkNullWStream stream;
+    SkRect bounds = SkRect::MakeIWH(150, 150);
+    std::unique_ptr<SkCanvas> canvas = SkSVGCanvas::Make(bounds, &stream);
+
+    fuzz_canvas(fuzz, canvas.get());
 }
