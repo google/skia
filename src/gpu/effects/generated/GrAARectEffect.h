2b14ca49f0f0a56de8b86087f5009af48f3e4b62
Michael Ludwig <michaelludwig@google.com>
2020-06-27 14:55:12 -0400

    Write SkSL visitor pattern and consolidate sampling analysis
    Change-Id: I802d610dbda512cf3823c58f349307b3926aa58f

--- src/gpu/effects/generated/GrAARectEffect.h
+++ src/gpu/effects/generated/GrAARectEffect.h
@@ -14,16 +14,14 @@
 #include "include/core/SkM44.h"
 #include "include/core/SkTypes.h"
 
+
 #include "src/gpu/GrCoordTransform.h"
 #include "src/gpu/GrFragmentProcessor.h"
 
 class GrAARectEffect : public GrFragmentProcessor {
 public:
-    static std::unique_ptr<GrFragmentProcessor> Make(std::unique_ptr<GrFragmentProcessor> inputFP,
-                                                     GrClipEdgeType edgeType,
-                                                     SkRect rect) {
-        return std::unique_ptr<GrFragmentProcessor>(
-                new GrAARectEffect(std::move(inputFP), edgeType, rect));
+    static std::unique_ptr<GrFragmentProcessor> Make(std::unique_ptr<GrFragmentProcessor> inputFP, GrClipEdgeType edgeType, SkRect rect) {
+        return std::unique_ptr<GrFragmentProcessor>(new GrAARectEffect(std::move(inputFP), edgeType, rect));
     }
     GrAARectEffect(const GrAARectEffect& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
@@ -31,23 +29,18 @@ public:
     int inputFP_index = -1;
     GrClipEdgeType edgeType;
     SkRect rect;
-
 private:
-    GrAARectEffect(std::unique_ptr<GrFragmentProcessor> inputFP,
-                   GrClipEdgeType edgeType,
-                   SkRect rect)
-            : INHERITED(kGrAARectEffect_ClassID,
-                        (OptimizationFlags)(inputFP ? ProcessorOptimizationFlags(inputFP.get())
-                                                    : kAll_OptimizationFlags) &
-                                kCompatibleWithCoverageAsAlpha_OptimizationFlag)
-            , edgeType(edgeType)
-            , rect(rect) {
+    GrAARectEffect(std::unique_ptr<GrFragmentProcessor> inputFP, GrClipEdgeType edgeType, SkRect rect)
+    : INHERITED(kGrAARectEffect_ClassID, (OptimizationFlags)
+    (inputFP ? ProcessorOptimizationFlags(inputFP.get()) : kAll_OptimizationFlags) &
+    kCompatibleWithCoverageAsAlpha_OptimizationFlag
+)
+    , edgeType(edgeType)
+    , rect(rect) {
         if (inputFP) {
-            inputFP_index = this->registerChild(std::move(inputFP));
-        }
-    }
+            inputFP_index = this->registerChild(std::move(inputFP));       }    }
     GrGLSLFragmentProcessor* onCreateGLSLInstance() const override;
-    void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
+    void onGetGLSLProcessorKey(const GrShaderCaps&,GrProcessorKeyBuilder*) const override;
     bool onIsEqual(const GrFragmentProcessor&) const override;
     GR_DECLARE_FRAGMENT_PROCESSOR_TEST
     typedef GrFragmentProcessor INHERITED;
