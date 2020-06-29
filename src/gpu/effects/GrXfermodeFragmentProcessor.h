e350b040882453d178cf82fd70ad468264bb53a4
John Stiles <johnstiles@google.com>
2020-06-29 17:43:13 -0400

    Compose again
    Change-Id: I7ab453f3313e70ae25e5e70f86373a64ff02072f

--- src/gpu/effects/GrXfermodeFragmentProcessor.h
+++ src/gpu/effects/GrXfermodeFragmentProcessor.h
@@ -15,22 +15,12 @@ class GrFragmentProcessor;
 
 namespace GrXfermodeFragmentProcessor {
 
-/** The color input to the returned processor is treated as the src and the passed in processor is
-    the dst. */
-std::unique_ptr<GrFragmentProcessor> MakeFromDstProcessor(std::unique_ptr<GrFragmentProcessor> dst,
-                                                          SkBlendMode mode);
-
-/** The color input to the returned processor is treated as the dst and the passed in processor is
-    the src. */
-std::unique_ptr<GrFragmentProcessor> MakeFromSrcProcessor(std::unique_ptr<GrFragmentProcessor> src,
-                                                          SkBlendMode mode);
-
-/** Takes the input color, which is assumed to be unpremultiplied, passes it as an opaque color
-    to both src and dst. The outputs of a src and dst are blended using mode and the original
-    input's alpha is applied to the blended color to produce a premul output. */
-std::unique_ptr<GrFragmentProcessor> MakeFromTwoProcessors(std::unique_ptr<GrFragmentProcessor> src,
-                                                           std::unique_ptr<GrFragmentProcessor> dst,
-                                                           SkBlendMode mode);
+/** Blends src and dst inputs according to the blend mode.
+ *  If either input is null, the input color (sk_InColor) is used instead.
+ */
+std::unique_ptr<GrFragmentProcessor> Make(std::unique_ptr<GrFragmentProcessor> src,
+                                          std::unique_ptr<GrFragmentProcessor> dst,
+                                          SkBlendMode mode);
 
 };
 
