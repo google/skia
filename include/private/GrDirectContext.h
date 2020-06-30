a21393243552180dd6bdcf2951f9e30e922519cb
Robert Phillips <robertphillips@google.com>
2020-06-30 09:27:52 -0400

    Make the GrDirectContext a thing and move it to include/private
    Change-Id: I8298a0649bc95843d20bee33ba7fe1d7e73bb839

---
+++ include/private/GrDirectContext.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrDirectContext_DEFINED
+#define GrDirectContext_DEFINED
+
+#include "include/gpu/GrContext.h"
+
+class GrAtlasManager;
+
+class GrDirectContext : public GrContext {
+public:
+    GrDirectContext(GrBackendApi backend, const GrContextOptions& options);
+
+    ~GrDirectContext() override;
+
+    void abandonContext() override;
+
+    void releaseResourcesAndAbandonContext() override;
+
+    void freeGpuResources() override;
+
+protected:
+    bool init() override;
+
+    GrAtlasManager* onGetAtlasManager() override { return fAtlasManager; }
+
+private:
+    GrAtlasManager* fAtlasManager;
+
+    typedef GrContext INHERITED;
+};
+
+
+#endif
