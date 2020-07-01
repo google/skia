d2f870c9110272a65cbc27da5ec670f3bd17494e
John Stiles <johnstiles@google.com>
2020-07-01 11:12:19 -0400

    Fix modifier key handling in OS X to allow command-keys to work.
    A variety of modifier key handling issues are addressed in this CL:
    - Added a skui::Key for the Super key (this is ImGui's name for command)
    - Added OS X event handling for `flagsChanged` (sent when modifier keys
      are pressed)
    - OS X manually tracks modifier key state and sends key-up and key-down
      events to the ImGuiLayer as necessary
    - OS X does not send key-up events when hotkeys are pressed, so these
      are manually synthesized and sent to ImGui (otherwise hotkeys are
      repeated forever)
    - Replaced hardcoded Virtual Key valus in OS X code with named constants
    - Our custom bitmask type was lacking the ability to XOR
    
    This CL does NOT enable the OS X clipboard; this uses the ImGui internal
    clipboard.
    
    Change-Id: I76b55215858bfb6441dbef18ad638426fa8bc073
    Bug: skia:10338
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/300182
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- include/private/SkBitmaskEnum.h
+++ include/private/SkBitmaskEnum.h
@@ -41,6 +41,17 @@ std::enable_if_t<sknonstd::is_bitmask_enum<E>::value, E&> constexpr operator&=(E
 }
 
 template <typename E>
+std::enable_if_t<sknonstd::is_bitmask_enum<E>::value, E> constexpr operator^(E l, E r) {
+    using U = std::underlying_type_t<E>;
+    return static_cast<E>(static_cast<U>(l) ^ static_cast<U>(r));
+}
+
+template <typename E>
+std::enable_if_t<sknonstd::is_bitmask_enum<E>::value, E&> constexpr operator^=(E& l, E r) {
+    return l = l ^ r;
+}
+
+template <typename E>
 std::enable_if_t<sknonstd::is_bitmask_enum<E>::value, E> constexpr operator~(E e) {
     return static_cast<E>(~static_cast<std::underlying_type_t<E>>(e));
 }
