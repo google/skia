ffb9647dc267576206c01c3f54476eeafbb7ba5f
Greg Daniel <egdaniel@google.com>
2020-07-01 15:14:31 -0400

    Add GrStagingBufferManager and use GrGpuBuffers to manage caching.
    This is a my alternative proposal for staging buffer updates. This is
    no a finished CL but has the core of the change so you can see what it
    does. The key change for this CL is that we are relying more on
    GrGpuBuffers. In general the GrStagingBufferManager here only handles
    currently active buffers. The busy on gpu buffers are handled by the
    backends by storing ref to the GrGpuBuffers. The backends don't need
    to explicitly "recycle" these buffers. All they need to do is drop their
    refs when it is safe to reuse them. We can then rely on our
    GrResourceCache to manage the caching of these buffers for us. This has
    the added benefit that all these buffers will now partake in our gpu
    memory budgeting.
    
    Some things to note about this current state of this CL:
    1) It relies on the CL list in its chain to update GrDawnBuffer to better
    handle Xfer buffers.
    2) Before landing we need to make sure our GrResourceCache is correctly
    able to cache and allocate these xfer buffers. I don't think we've
    every done that before and tested it (it may be working just fine).
    3) I haven't deleted any of the old staging buffer code yet. This way
    you can get a fresh look at what the new world will look like.
    4) I have changed any of the callers who want a slice, but that is a
    pretty trivial swap out change.
    
    Overall I think this is the way to go. The main reasoning is backend apis
    don't have to implement any virtuals or subclass anything. Also the fact
    that these buffers will now be part of our global resource management
    and budgeting I think is a huge win.
    
    Change-Id: I5db9a3c52133978ea89d6c0de440f434fbf91a51

--- src/gpu/vk/GrVkGpu.cpp
+++ src/gpu/vk/GrVkGpu.cpp
@@ -177,6 +177,7 @@ GrVkGpu::GrVkGpu(GrContext* context, const GrContextOptions& options,
         , fQueue(backendContext.fQueue)
         , fQueueIndex(backendContext.fGraphicsQueueIndex)
         , fResourceProvider(this)
+        , fStagingBufferManager(this->getContext()->priv().resourceProvider())
         , fDisconnected(false)
         , fProtectedContext(backendContext.fProtectedContext) {
     SkASSERT(!backendContext.fOwnsInstanceAndDevice);
@@ -278,6 +279,8 @@ void GrVkGpu::destroyResources() {
         fSemaphoresToSignal[i]->unref();
     }
     fSemaphoresToSignal.reset();
+
+    fStagingBufferManager.reset();
 
     // must call this just before we destroy the command pool and VkDevice
     fResourceProvider.destroyResources(VK_ERROR_DEVICE_LOST == res);
@@ -2066,6 +2069,14 @@ void GrVkGpu::addFinishedCallback(sk_sp<GrRefCntedCallback> finishedCallback) {
 }
 
 bool GrVkGpu::onSubmitToGpu(bool syncCpu) {
+    if (fStagingBufferManager.hasBuffers()) {
+        GrVkPrimaryCommandBuffer* currentCB = this->currentCommandBuffer();
+        auto moveStagingBuffers = [currentCB](sk_sp<GrGpuBuffer> buffer) {
+            currentCB->addGpuBuffer(std::move(buffer));
+        };
+        fStagingBufferManager.detachBuffers(moveStagingBuffers);
+    }
+
     if (syncCpu) {
         return this->submitCommandBuffer(kForce_SyncQueue);
     } else {
