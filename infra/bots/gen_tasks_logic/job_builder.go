e68958271ac0080ce5c8909cea0fb66ed195e9e1
Ravi Mistry <rmistry@google.com>
2020-07-06 14:54:36 -0400

    Rename G3 framework bot
    Change-Id: I0906c6e2df0af405ae3a2138b0fdeb99a9375853

--- infra/bots/gen_tasks_logic/job_builder.go
+++ infra/bots/gen_tasks_logic/job_builder.go
@@ -154,10 +154,6 @@ func (b *jobBuilder) genTasksForJob() {
 			// Android Framework compile tasks use a different recipe.
 			b.androidFrameworkCompile()
 			return
-		} else if b.extraConfig("G3") && b.extraConfig("Framework") {
-			// G3 compile tasks use a different recipe.
-			b.g3FrameworkCompile()
-			return
 		} else {
 			b.compile()
 			return
@@ -180,8 +176,14 @@ func (b *jobBuilder) genTasksForJob() {
 
 	// Test bots.
 	if b.role("Test") {
-		b.dm()
-		return
+		if b.extraConfig("G3") && b.extraConfig("Framework") && b.extraConfig("Canary") {
+			// G3 framework tasks use a different recipe.
+			b.g3FrameworkCanary()
+			return
+		} else {
+			b.dm()
+			return
+		}
 	}
 	if b.role("FM") {
 		b.fm()
