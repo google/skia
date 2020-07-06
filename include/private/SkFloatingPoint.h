8e7bd7868cdc4f4266652ec906c41588bb4fa124
Stephan Hartmann <stha09@googlemail.com>
2020-07-06 12:50:33 +0000

    GCC: fix unknown attribute warnings for no_sanitize
    Clang and GCC use different syntax to add no_sanitize
    attribute on functions. This results in a large amount
    of warnings for GCC, because responsible header is included
    very often. Solve this by defining compiler specific macro
    locally.
    
    Change-Id: I589a851493788283c13fe4c7fb99eaa7ad753487

--- include/private/SkFloatingPoint.h
+++ include/private/SkFloatingPoint.h
@@ -159,7 +159,7 @@ static inline int64_t sk_float_saturate2int64(float x) {
 // Cast double to float, ignoring any warning about too-large finite values being cast to float.
 // Clang thinks this is undefined, but it's actually implementation defined to return either
 // the largest float or infinity (one of the two bracketing representable floats).  Good enough!
-[[clang::no_sanitize("float-cast-overflow")]]
+SK_ATTRIBUTE(no_sanitize("float-cast-overflow"))
 static inline float sk_double_to_float(double x) {
     return static_cast<float>(x);
 }
@@ -226,12 +226,12 @@ static inline float sk_float_rsqrt(float x) {
 // IEEE defines how float divide behaves for non-finite values and zero-denoms, but C does not
 // so we have a helper that suppresses the possible undefined-behavior warnings.
 
-[[clang::no_sanitize("float-divide-by-zero")]]
+SK_ATTRIBUTE(no_sanitize("float-divide-by-zero"))
 static inline float sk_ieee_float_divide(float numer, float denom) {
     return numer / denom;
 }
 
-[[clang::no_sanitize("float-divide-by-zero")]]
+SK_ATTRIBUTE(no_sanitize("float-divide-by-zero"))
 static inline double sk_ieee_double_divide(double numer, double denom) {
     return numer / denom;
 }
