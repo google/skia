c8be630b6003f4ddf5963b8042f42d754c7e7efc
Zepeng Hu <zepenghu@google.com>
2020-07-08 15:24:21 +0000

    Add SkSL Binary Fuzzer
    Change-Id: Ib0385edd0f5ec2f23744aa517135a6955c53ba38

--- fuzz/oss_fuzz/FuzzSKSL2Pipeline.cpp
+++ fuzz/oss_fuzz/FuzzSKSL2Pipeline.cpp
@@ -5,25 +5,58 @@
  * found in the LICENSE file.
  */
 
+#include "include/core/SkCanvas.h"
+#include "include/core/SkPaint.h"
+#include "include/core/SkSurface.h"
+#include "include/effects/SkRuntimeEffect.h"
 #include "src/gpu/GrShaderCaps.h"
 #include "src/sksl/SkSLCompiler.h"
 
 #include "fuzz/Fuzz.h"
 
 bool FuzzSKSL2Pipeline(sk_sp<SkData> bytes) {
+    sk_sp<SkData> numPtr = SkData::MakeSubset(bytes.get(), 0, 1);
+    uint8_t num = *(uint8_t*) numPtr->data();
+    sk_sp<SkData> codeBytes = SkData::MakeSubset(bytes.get(), num, bytes->size() - num);
+
     SkSL::Compiler compiler;
     SkSL::Program::Settings settings;
     sk_sp<GrShaderCaps> caps = SkSL::ShaderCapsFactory::Default();
     settings.fCaps = caps.get();
     std::unique_ptr<SkSL::Program> program = compiler.convertProgram(
                                                     SkSL::Program::kPipelineStage_Kind,
-                                                    SkSL::String((const char*) bytes->data(),
-                                                                 bytes->size()),
+                                                    SkSL::String((const char*) codeBytes->data(),
+                                                                 codeBytes->size()),
                                                     settings);
     SkSL::PipelineStageArgs args;
     if (!program || !compiler.toPipelineStage(*program, &args)) {
         return false;
     }
+    SkRuntimeEffect::EffectResult tuple = SkRuntimeEffect::Make(
+        SkString((const char*) codeBytes->data(), codeBytes->size())
+    );
+    SkRuntimeEffect* effect = std::get<0>(tuple).get();
+    if (!effect || effect->inputSize() > num) { // if there is not enough uniform bytes to be passed in
+        return false;
+    }
+
+    sk_sp<SkData> uniformBytes = SkData::MakeSubset(bytes.get(), 0, effect->inputSize());
+    SkMatrix localM;
+    localM.setRotate(90, 128, 128);
+    auto shader = effect->makeShader(codeBytes, nullptr, 0, &localM, true);
+    if (!shader) {
+        return false;
+    }
+    SkPaint paint;
+    paint.setShader(std::move(shader));
+
+    sk_sp<SkSurface> s = SkSurface::MakeRasterN32Premul(128, 128);
+    if (!s) {
+        return false;
+    }
+
+    s->getCanvas()->drawPaint(paint);
+
     return true;
 }
 
