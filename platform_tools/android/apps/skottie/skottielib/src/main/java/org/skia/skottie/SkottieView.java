120e426b94bfad759a612195a7b9ddc7c194b999
Jorge Betancourt <jmbetancourt@google.com>
2020-07-08 11:25:15 -0400

    turn skottie view into a view group so we can swap out TextureView and SurfaceView
    Change-Id: Iec87cf169c7d013e6727df8a9343b64b9d64243e

--- platform_tools/android/apps/skottie/skottielib/src/main/java/org/skia/skottie/SkottieView.java
+++ platform_tools/android/apps/skottie/skottielib/src/main/java/org/skia/skottie/SkottieView.java
@@ -10,54 +10,83 @@ package org.skia.skottie;
 import android.content.Context;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.SurfaceView;
 import android.view.TextureView;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class SkottieView extends TextureView {
+public class SkottieView extends FrameLayout {
 
     private SkottieAnimation mAnimation;
+    private View backingView;
 
-    public SkottieView(Context context) {
-        super(context);
-        init();
-    }
-
+    //TODO add and access custom attributes for SkottieView
     public SkottieView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
-    public SkottieView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
+    private SkottieView(Context context, SkottieViewBuilder builder) {
+        super(context);
+        // create the backing view
+        if (builder.advancedFeatures) {
+            // backing view must be SurfaceView
+            backingView = new SurfaceView(context, builder.attrs, builder.defStyleAttr);
+        } else {
+            backingView = new TextureView(context, builder.attrs, builder.defStyleAttr);
+            ((TextureView)backingView).setOpaque(false);
+        }
+        backingView.setLayoutParams(new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT));
+        addView(backingView);
     }
 
-    public SkottieView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
-    }
-
-    private void init() {
-        setOpaque(false);
-    }
-
+    //TODO handle SurfaceView
     public void setSource(InputStream inputStream) {
-        mAnimation = SkottieRunner.getInstance().createAnimation(this, inputStream);
+        mAnimation = SkottieRunner.getInstance().createAnimation(((TextureView)backingView), inputStream);
     }
 
-    public void setSkottieResource(int resId) {
-        InputStream inputStream = getResources().openRawResource(resId);
-        mAnimation = SkottieRunner.getInstance().createAnimation(this, inputStream);
+    public void setSource(int resId) {
+        InputStream inputStream = backingView.getResources().openRawResource(resId);
+        mAnimation = SkottieRunner.getInstance().createAnimation(((TextureView)backingView), inputStream);
     }
 
-    public void setSkottieURI(Context context, Uri uri) throws FileNotFoundException {
+    public void setSource(Context context, Uri uri) throws FileNotFoundException {
         InputStream inputStream = context.getContentResolver().openInputStream(uri);
-        mAnimation = SkottieRunner.getInstance().createAnimation(this, inputStream);
+        mAnimation = SkottieRunner.getInstance().createAnimation(((TextureView)backingView), inputStream);
     }
 
     public SkottieAnimation getSkottieAnimation() {
         return mAnimation;
+    }
+
+    // Builder accessed by user to generate SkottieViews
+    public static class SkottieViewBuilder {
+        protected AttributeSet attrs;
+        protected int defStyleAttr;
+
+        // if true, backing view will be surface view
+        protected boolean advancedFeatures;
+        // TODO private variable backgroundColor
+
+        public void setAttrs(AttributeSet attrs) {
+          this.attrs = attrs;
+        }
+
+        public void setDefStyleAttr(int defStyleAttr) {
+          this.defStyleAttr = defStyleAttr;
+        }
+
+        public void setAdvancedFeatures(boolean advancedFeatures) {
+          this.advancedFeatures = advancedFeatures;
+        }
+
+        public SkottieView build(Context context) {
+          return new SkottieView(context, this);
+        }
     }
 }
