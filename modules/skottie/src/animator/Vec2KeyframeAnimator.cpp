ad0030185723f8eadc8cdf87770b9810a74b25aa
Florin Malita <fmalita@chromium.org>
2020-07-08 07:52:20 -0400

    [skottie] Auto-orient fixes
    Two issues:
    
    1) For static keyframes (start_value == end_value) AE yields horizontal
       orientation (0 tangent).  We technically have the same logic in
       Skottie, but our value deduplication logic interferes: the two
       consecutive equal values are consolidated, and the result ends up
       holding the spatial lerp info for the next frame => our hold frames
       auto-orient for the beginning of the next keyframe.
    
       Fix: skip value deduplication when spatial lerp is present.
    
    2) The very last keyframe is always static and holds no spatial info.
       AE retains the orientation of the previous frame, but Skottie yields
       0 tangent.
    
       Fix: the easiest way to accomplish AE semantics is to detect when
       we're dealing with the last keyframe, and swap with the previous
       keyframe with an adjust weight of 1 (to select the end value).  This
       produces the same lerp result (because keyframed values are always
       contiguous) and also respects the orientation of the prev frame.
    
    TBR=
    Change-Id: Id661f7804533e95b747722457489a7ef759572a4

--- modules/skottie/src/animator/Vec2KeyframeAnimator.cpp
+++ modules/skottie/src/animator/Vec2KeyframeAnimator.cpp
@@ -55,11 +55,6 @@ public:
 
     private:
         void backfill_spatial(const SpatialValue& val) {
-            if (fTi == SkV2{0,0} && fTo == SkV2{0,0}) {
-                // no tangents => linear
-                return;
-            }
-
             SkASSERT(!fValues.empty());
             auto& prev_val = fValues.back();
             SkASSERT(!prev_val.cmeasure);
@@ -110,13 +105,16 @@ public:
                 return false;
             }
 
-            this->backfill_spatial(val);
+            if (fPendingSpatial) {
+                this->backfill_spatial(val);
+            }
 
             // Track the last keyframe spatial tangents (checked on next parseValue).
-            fTi = ParseDefault<SkV2>(jkf["ti"], {0,0});
-            fTo = ParseDefault<SkV2>(jkf["to"], {0,0});
+            fTi             = ParseDefault<SkV2>(jkf["ti"], {0,0});
+            fTo             = ParseDefault<SkV2>(jkf["to"], {0,0});
+            fPendingSpatial = fTi != SkV2{0,0} || fTo != SkV2{0,0};
 
-            if (fValues.empty() || val.v2 != fValues.back().v2) {
+            if (fValues.empty() || val.v2 != fValues.back().v2 || fPendingSpatial) {
                 fValues.push_back(std::move(val));
             }
 
@@ -130,6 +128,7 @@ public:
         float*                    fRotTarget; // optional
         SkV2                      fTi{0,0},
                                   fTo{0,0};
+        bool                      fPendingSpatial = false;
     };
 
 private:
@@ -155,7 +154,33 @@ private:
     }
 
     StateChanged onSeek(float t) override {
-        const auto& lerp_info = this->getLERPInfo(t);
+        auto get_lerp_info = [this](float t) {
+            auto lerp_info = this->getLERPInfo(t);
+
+            // When tracking rotation/orientation, the last keyframe requires special handling:
+            // it doesn't store any spatial information but it is expected to maintain the
+            // previous orientation (per AE semantics).
+            //
+            // The easiest way to achieve this is to actually swap with the previous keyframe,
+            // with an adjusted weight of 1.
+            const auto vidx = lerp_info.vrec0.idx;
+            if (fRotTarget && vidx == fValues.size() - 1 && vidx > 0) {
+                SkASSERT(!fValues[vidx].cmeasure);
+                SkASSERT(lerp_info.vrec1.idx == vidx);
+
+                // Change LERPInfo{0, SIZE - 1, SIZE - 1}
+                // to     LERPInfo{1, SIZE - 2, SIZE - 1}
+                lerp_info.weight = 1;
+                lerp_info.vrec0  = {vidx - 1};
+
+                // This yields equivalent lerp results because keyframed values are contiguous
+                // i.e frame[n-1].end_val == frame[n].start_val.
+            }
+
+            return lerp_info;
+        };
+
+        const auto lerp_info = get_lerp_info(t);
 
         const auto& v0 = fValues[lerp_info.vrec0.idx];
         if (v0.cmeasure) {
