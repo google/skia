52a35335e7f372fcea8ed05a530bf7d771011b64
Zepeng Hu <zepenghu@google.com>
2020-07-09 12:51:12 +0000

    Add SkSL Binary Fuzzer
    Change-Id: Ib0385edd0f5ec2f23744aa517135a6955c53ba38

---
+++ fuzz/oss_fuzz/FuzzSkRuntimeEffect.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/core/SkCanvas.h"
+#include "include/core/SkPaint.h"
+#include "include/core/SkSurface.h"
+#include "include/effects/SkRuntimeEffect.h"
+#include "src/gpu/GrShaderCaps.h"
+
+#include "fuzz/Fuzz.h"
+
+/**
+ * The fuzzer will take in the bytes and divide into two parts. For one part, it will first
+ * reserve 256 bytes and then allocate the bytes with same size as effect->inputSize() to
+ * uniformBytes. The uniformBytes is intended to create makeShader()
+ * The other part is codeBytes, the original bytes minus 256 bytes, which will be treated
+ * as sksl code, intending to create SkRuntimeEffect.
+ */
+bool FuzzSkRuntimeEffect(sk_sp<SkData> bytes) {
+    constexpr size_t kReservedBytes = 256;
+    printf("start\n");
+    if (bytes->size() < kReservedBytes) {
+        return false;
+    }
+    printf("reach 29\n");
+    sk_sp<SkData> codeBytes = SkData::MakeSubset(bytes.get(), 0, bytes->size() - kReservedBytes);
+
+    SkRuntimeEffect::EffectResult tuple = SkRuntimeEffect::Make(
+        SkString((const char*) codeBytes->data(), codeBytes->size())
+    );
+    SkRuntimeEffect* effect = std::get<0>(tuple).get();
+
+    if (!effect || effect->inputSize() > kReservedBytes) { // if there is not enough uniform bytes
+        return false;
+    }
+    printf("reach 40\n");
+    sk_sp<SkData> uniformBytes = SkData::MakeSubset(bytes.get(), bytes->size() - 256,
+                                                                effect->inputSize());
+    SkMatrix localM;
+    localM.setRotate(90, 128, 128); // an arbitrary matrix
+    auto shader = effect->makeShader(uniformBytes, nullptr, 0, &localM, true);
+    printf("reach 45\n");
+    if (!shader) {
+        return false;
+    }
+    SkPaint paint;
+    paint.setShader(std::move(shader));
+
+    sk_sp<SkSurface> s = SkSurface::MakeRasterN32Premul(128, 128);
+    if (!s) {
+        return false;
+    }
+    printf("reach 56\n");
+    s->getCanvas()->drawPaint(paint);
+
+    return true;
+}
+
+#if defined(IS_FUZZING_WITH_LIBFUZZER)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if (size > 3000) {
+        return 0;
+    }
+    auto bytes = SkData::MakeWithoutCopy(data, size);
+    FuzzSkRuntimeEffect(bytes);
+    return 0;
+}
+#endif
