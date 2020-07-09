cbcbc367b89b1e9f30854df5481150c759709240
Jorge Betancourt <jmbetancourt@google.com>
2020-07-09 19:12:39 -0400

    configure attributes for SkottieView to be initialized from XML layout
    Change-Id: I9eb1702ed11f23873ffe7fbc60d9f9d885d3073c

--- platform_tools/android/apps/skottie/skottielib/src/main/java/org/skia/skottie/SkottieView.java
+++ platform_tools/android/apps/skottie/skottielib/src/main/java/org/skia/skottie/SkottieView.java
@@ -8,8 +8,10 @@
 package org.skia.skottie;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.TextureView;
 
@@ -18,27 +20,57 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import org.skia.skottielib.R;
 
 public class SkottieView extends FrameLayout {
 
     private SkottieAnimation mAnimation;
     private View backingView;
 
-    //TODO add and access custom attributes for SkottieView
+    // SkottieView constructor when initialized in XML layout
     public SkottieView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme()
+            .obtainStyledAttributes(attrs, R.styleable.SkottieView, 0, 0);
+        try {
+            switch (a.getInteger(R.styleable.SkottieView_backing_view, -1)) {
+                case 0:
+                    backingView = new TextureView(context);
+                    ((TextureView)backingView).setOpaque(false);
+                    break;
+                case 1:
+                    backingView = new SurfaceView(context);
+                    int bg = a.getColor(R.styleable.SkottieView_background_color, -1);
+                    if (bg == -1) {
+                        throw new RuntimeException("background_color attribute needed for SurfaceView");
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("backing_view attribute needed to specify between texture_view or surface_view");
+            }
+        } catch(RuntimeException err) {
+            Log.e("Missing Attribute", err.getMessage());
+        } finally {
+            a.recycle();
+        }
+        initBackingArray();
     }
-
+    
+    // SkottieView builder constructor
     private SkottieView(Context context, SkottieViewBuilder builder) {
-        super(context);
+        super(context, builder.attrs, builder.defStyleAttr);
         // create the backing view
         if (builder.advancedFeatures) {
             // backing view must be SurfaceView
-            backingView = new SurfaceView(context, builder.attrs, builder.defStyleAttr);
+            backingView = new SurfaceView(context);
         } else {
-            backingView = new TextureView(context, builder.attrs, builder.defStyleAttr);
+            backingView = new TextureView(context);
             ((TextureView)backingView).setOpaque(false);
         }
+        initBackingArray();
+    }
+
+    private void initBackingArray() {
         backingView.setLayoutParams(new ViewGroup.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT));
