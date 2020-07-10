106dd504b027e155444a5e708925ce6c965633d4
Robert Phillips <robertphillips@google.com>
2020-07-10 15:59:29 -0400

    Update some of the dox code to GrDirectContext
    Change-Id: I487a973004c4f080fc5802128770b417d54628e2

--- docs/examples/Surface_characterize.cpp
+++ docs/examples/Surface_characterize.cpp
@@ -7,7 +7,7 @@ REG_FIDDLE(Surface_characterize, 256, 64, false, 0) {
 void draw(SkCanvas* canvas) {
     SkPaint paint;
     paint.setTextSize(32);
-    GrContext* context = canvas->getGrContext();
+    auto context = canvas->recordingContext();
     if (!context) {
          canvas->drawString("GPU only!", 20, 40, paint);
          return;
