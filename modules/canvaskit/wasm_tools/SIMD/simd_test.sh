e810d2702bcef0c3cc6f0037ff5c3ee2c790d321
Elliot Evans <elliotevans@google.com>
2020-07-14 16:30:25 -0600

    fix simd and perf issues, add a tool for puppeteer perf data
    Add Puppeteer code for perfing debug and experimental_SIMD builds of CanvasKit against all .skp's
    
    Change-Id: I311629a1420301dda41f7ec57ce1403b05fd949b

---
+++ modules/canvaskit/wasm_tools/SIMD/simd_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Requires that emscripten and wasm2wat are added to your PATH.
+# Requires, and is verified to work with
+# - wasm2wat 1.0.13 (1.0.17)
+#   - install from here: https://github.com/WebAssembly/wabt
+# - emscripten 1.39.16
+# - Chrome Canary 86.0.4186.0 with chrome://flags#enable-webassembly-simd enabled
+#
+# Example usage: ./build_simd_test.sh simd_float_test.wasm
+
+wasm2wat --enable-simd $1 > output/simd_test.wat
+
+# The following lines output all SIMD operations produced in the output WASM.
+# Useful for checking that SIMD instructions are actually being used.
+# e.g. for the following C++ code:
+#  auto vec1 = skvx::Vec<2, double>({11.f, -22.f}) + skvx::Vec<2, double>({13.f, -1.f});
+# it is expected that the f64x2.add operation is present in the output WASM.
+echo "The following WASM SIMD operations were used in the compiled code:"
+grep -f wasm_simd_types.txt output/simd_test.wat
