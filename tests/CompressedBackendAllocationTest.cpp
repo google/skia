e4de86f915b429994262d6e654d1589a6c31aac0
Greg Daniel <egdaniel@google.com>
2020-07-14 15:05:42 +0000

    Revert "Add GrContext api to update compressed backend textures."
    This reverts commit 2c180304dc239a44770df7bf2649f3138f25922c.
    
    Reason for revert: attempted workaround did not fix techno spark so needs further investigation
    
    Original change's description:
    > Add GrContext api to update compressed backend textures.
    > 
    > Bug: chromium:1099255
    > Change-Id: I0c3f25ddb037e47e3b910fa89c3d8b3aa27b3114
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/302265
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    
    Change-Id: Ib5433def02dc5dad97dcdbd4476ced6de2361e6a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1099255

--- tests/CompressedBackendAllocationTest.cpp
+++ tests/CompressedBackendAllocationTest.cpp
@@ -157,23 +157,8 @@ static void test_compressed_color_init(GrContext* context,
 
     check_compressed_mipmaps(context, img, compression, expectedColors, mipMapped,
                              reporter, "colorinit");
-    check_readback(context, img, compression, color, reporter, "solid readback");
-
-    SkColor4f newColor;
-    newColor.fR = color.fB;
-    newColor.fG = color.fR;
-    newColor.fB = color.fG;
-    newColor.fA = color.fA;
-
-    bool result = context->updateCompressedBackendTexture(backendTex, newColor, nullptr, nullptr);
-    // Since we were able to create the compressed texture we should be able to update it.
-    REPORTER_ASSERT(reporter, result);
-
-    SkColor4f expectedNewColors[6] = {newColor, newColor, newColor, newColor, newColor, newColor};
-
-    check_compressed_mipmaps(context, img, compression, expectedNewColors, mipMapped, reporter,
-                             "colorinit");
-    check_readback(context, std::move(img), compression, newColor, reporter, "solid readback");
+    check_readback(context, std::move(img), compression, color, reporter,
+                   "solid readback");
 
     context->deleteBackendTexture(backendTex);
 }
@@ -244,30 +229,7 @@ static void test_compressed_data_init(GrContext* context,
 
     check_compressed_mipmaps(context, img, compression, expectedColors,
                              mipMapped, reporter, "pixmap");
-    check_readback(context, img, compression, expectedColors[0], reporter, "data readback");
-
-    SkColor4f expectedColorsNew[6] = {
-        {1.0f, 1.0f, 0.0f, 1.0f},  // Y
-        {1.0f, 0.0f, 0.0f, 1.0f},  // R
-        {0.0f, 1.0f, 0.0f, 1.0f},  // G
-        {0.0f, 0.0f, 1.0f, 1.0f},  // B
-        {0.0f, 1.0f, 1.0f, 1.0f},  // C
-        {1.0f, 0.0f, 1.0f, 1.0f},  // M
-    };
-
-    std::unique_ptr<const char[]> dataNew(
-            make_compressed_data(compression, expectedColorsNew, mipMapped));
-    size_t dataNewSize =
-            SkCompressedDataSize(compression, {32, 32}, nullptr, mipMapped == GrMipMapped::kYes);
-
-    bool result = context->updateCompressedBackendTexture(backendTex, dataNew.get(), dataNewSize,
-                                                          nullptr, nullptr);
-    // Since we were able to create the compressed texture we should be able to update it.
-    REPORTER_ASSERT(reporter, result);
-
-    check_compressed_mipmaps(context, img, compression, expectedColorsNew, mipMapped, reporter,
-                             "pixmap");
-    check_readback(context, std::move(img), compression, expectedColorsNew[0], reporter,
+    check_readback(context, std::move(img), compression, expectedColors[0], reporter,
                    "data readback");
 
     context->deleteBackendTexture(backendTex);
