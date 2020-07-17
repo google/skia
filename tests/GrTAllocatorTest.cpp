f21fcb1296499138ccfc11b184fa10f29b535f91
Michael Ludwig <michaelludwig@google.com>
2020-07-17 16:44:29 -0400

    Keep a scratch block around for reuse in GrBlockAllocator
    This can optimize cases in a allocate-release loop that moves back
    and forth across the end of one block and the start of another. Before
    this would malloc a new block and then delete it.
    
    It also enables reserve() in higher-level data collections w/o blocking
    bytes left in the current tail block.
    
    Change-Id: Ide16e9038384fcb188164fc9620a8295f6880b9f

--- tests/GrTAllocatorTest.cpp
+++ tests/GrTAllocatorTest.cpp
@@ -22,9 +22,13 @@ struct C {
 
     int fID;
 
+    // Under the hood, GrTAllocator and GrBlockAllocator round up to max_align_t. If 'C' was just
+    // 4 bytes, that often means the internal blocks can squeeze a few extra instances in. This
+    // is fine, but makes predicting a little trickier, so make sure C is a bit bigger.
+    int fPadding[4];
+
     static int gInstCnt;
 };
-
 int C::gInstCnt = 0;
 }
 
@@ -156,6 +160,63 @@ static void run_allocator_test(GrTAllocator<C, N>* allocator, skiatest::Reporter
     check_allocator(allocator, 100, 10, reporter);
 }
 
+
+template<int N>
+static void run_reserve_test(skiatest::Reporter* reporter) {
+    constexpr int kItemsPerBlock = N + 4; // Make this a number > 1, even if N starting items == 1
+
+    GrTAllocator<C, N> list(kItemsPerBlock);
+    size_t initialSize = list.allocator()->totalSize();
+    // Should be able to add N instances of T w/o changing size from initialSize
+    for (int i = 0; i < N; ++i) {
+        list.push_back(C(i));
+    }
+    REPORTER_ASSERT(reporter, initialSize == list.allocator()->totalSize());
+
+    // Reserve room for 2*kItemsPerBlock items
+    list.reserve(2 * kItemsPerBlock);
+    REPORTER_ASSERT(reporter, list.count() == N); // count shouldn't change though
+
+    size_t reservedSize = list.allocator()->totalSize();
+    REPORTER_ASSERT(reporter, reservedSize >= initialSize + 2 * kItemsPerBlock * sizeof(C));
+    for (int i = 0; i < 2 * kItemsPerBlock; ++i) {
+        list.push_back(C(i));
+    }
+    REPORTER_ASSERT(reporter, reservedSize == list.allocator()->totalSize());
+
+    // Make the next block partially fully (N > 0 but < kItemsPerBlock)
+    for (int i = 0; i < N; ++i) {
+        list.push_back(C(i));
+    }
+
+    // Reserve room again for 2*kItemsPerBlock, but reserve should automatically take account of the
+    // (kItemsPerBlock-N) that are still available in the active block
+    list.reserve(2 * kItemsPerBlock);
+    int extraReservedCount = kItemsPerBlock + N;
+    // Because GrTAllocator normally allocates blocks in fixed sizes, and extraReservedCount >
+    // items-per-block, it will always use that size and not that of the growth policy.
+    REPORTER_ASSERT(reporter, (size_t) list.allocator()->testingOnly_scratchBlockSize() >=
+                                       extraReservedCount * sizeof(C));
+
+    reservedSize = list.allocator()->totalSize();
+    for (int i = 0; i < 2 * kItemsPerBlock; ++i) {
+        list.push_back(C(i));
+    }
+    REPORTER_ASSERT(reporter, reservedSize == list.allocator()->totalSize());
+
+    // If we reserve a count < items-per-block, it will use the fixed size from the growth policy.
+    list.reserve(2);
+    REPORTER_ASSERT(reporter, (size_t) list.allocator()->testingOnly_scratchBlockSize() >=
+                                       kItemsPerBlock * sizeof(C));
+
+    // Ensure the reservations didn't initialize any more D's than anticipated
+    int expectedInstanceCount = 2 * (N + 2 * kItemsPerBlock);
+    REPORTER_ASSERT(reporter, expectedInstanceCount == C::gInstCnt);
+
+    list.reset();
+    REPORTER_ASSERT(reporter, 0 == C::gInstCnt);
+}
+
 DEF_TEST(GrTAllocator, reporter) {
     // Test combinations of allocators with and without stack storage and with different block sizes
     GrTAllocator<C> a1(1);
@@ -175,4 +236,10 @@ DEF_TEST(GrTAllocator, reporter) {
 
     GrTAllocator<C, 4> sa4;
     run_allocator_test(&sa4, reporter);
+
+    run_reserve_test<1>(reporter);
+    run_reserve_test<2>(reporter);
+    run_reserve_test<3>(reporter);
+    run_reserve_test<4>(reporter);
+    run_reserve_test<5>(reporter);
 }
