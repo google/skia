da886924f37a3ffc4e069d80524e83d10d433e6d
Chris Dalton <csmartdalton@google.com>
2020-07-17 17:38:54 -0700

    Stop discarding stencil buffers on tilers
    Bug: b/160958008
    Test: SystemWebViewShell.apk
          - Tap 3 dot menu
          - Tap "Animation test"
          - Check "Rounded corner" checkbox
    Change-Id: I67851cb4be1a95f9f1d813846f5d4ba4b1535d9c

--- src/gpu/GrCaps.h
+++ src/gpu/GrCaps.h
@@ -74,9 +74,13 @@ public:
     // Should we discard stencil values after a render pass? (Tilers get better performance if we
     // always load stencil buffers with a "clear" op, and then discard the content when finished.)
     bool discardStencilValuesAfterRenderPass() const {
+        // b/160958008
+        return false;
+#if 0
         // This method is actually just a duplicate of preferFullscreenClears(), with a descriptive
         // name for the sake of readability.
         return this->preferFullscreenClears();
+#endif
     }
 
     bool preferVRAMUseOverFlushes() const { return fPreferVRAMUseOverFlushes; }
