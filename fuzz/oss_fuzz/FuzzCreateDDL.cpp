36b907dfeccef719397deb40ea6abf7405e718f9
Zepeng Hu <zepenghu@google.com>
2020-07-20 18:51:07 +0000

    add create ddl fuzzer
    This is an attempt to fuzz the creation of SkSurfaceCharacterization,
    SkDeferredDisplayRecorder, and SkDeferredDisplayList
    
    Change-Id: I9cd9736813be3abc82430bd4eeb559d6993ecbd4

---
+++ fuzz/oss_fuzz/FuzzCreateDDL.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2020 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/core/SkCanvas.h"
+#include "include/core/SkDeferredDisplayList.h"
+#include "include/core/SkDeferredDisplayListRecorder.h"
+#include "include/core/SkPaint.h"
+#include "include/core/SkSurface.h"
+#include "include/core/SkSurfaceCharacterization.h"
+#include "include/gpu/GrDirectContext.h"
+#include "include/private/GrTypesPriv.h"
+#include "src/gpu/GrShaderCaps.h"
+#include "tools/gpu/GrContextFactory.h"
+
+#include "fuzz/Fuzz.h"
+
+#include <tuple>
+
+static SkSurfaceCharacterization make_characterization(GrDirectContext* dContext, SkImageInfo ii) {
+    size_t maxResourceBytes = dContext->getResourceCacheLimit();
+    SkSurfaceCharacterization c;
+    if (!dContext->colorTypeSupportedAsSurface(kRGBA_8888_SkColorType)) {
+        SkDebugf("kRGBA_8888 is not supported in the backend %s", GrBackendApiToStr(dContext->backend()));
+        return {};
+    }
+
+    GrBackendFormat backendFormat = dContext->defaultBackendFormat(kRGBA_8888_SkColorType,
+                                                                GrRenderable::kYes);
+    if (!backendFormat.isValid()) {
+        SkDebugf("kRGBA_8888 is not supported in the backend %s",
+            GrBackendApiToStr(dContext->backend()));
+        return {};
+    }
+    c = dContext->threadSafeProxy()->createCharacterization(
+                                maxResourceBytes, ii, backendFormat, 1,
+                                kTopLeft_GrSurfaceOrigin, SkSurfaceProps(0x0, kRGB_H_SkPixelGeometry), true,
+                                false, true, GrProtected::kNo);
+
+
+    if (!c.isValid()) {
+        SkDebugf("Could not create Characterization %s",
+            GrBackendApiToStr(dContext->backend()));
+        return {};
+    }
+    return c;
+}
+static sk_sp<SkDeferredDisplayList> make_ddl(Fuzz* fuzz, GrDirectContext* dContext, SkSurfaceCharacterization c) {
+    SkDeferredDisplayListRecorder r(c);
+    SkCanvas* canvas = r.getCanvas();
+    if (!canvas) {
+        SkDebugf("Could not create canvas %s",
+            GrBackendApiToStr(dContext->backend()));
+        return nullptr;
+    }
+
+    SkPaint paint;
+    fuzz->next(&paint); // TODO(zepeng) should be same in FuzzCanvas.cpp
+    SkRect tile;
+    fuzz->next(&tile);
+
+    canvas->drawRect(tile, SkPaint());
+    return r.detach();
+}
+
+static sk_sp<SkSurface> make_surface(GrDirectContext* dContext, const SkImageInfo& ii) {
+    auto surface = SkSurface::MakeRenderTarget(dContext, SkBudgeted::kYes, ii,
+        1, GrSurfaceOrigin::kBottomLeft_GrSurfaceOrigin, nullptr);
+    return surface;
+}
+
+static void draw_ddl(sk_sp<SkSurface> surface, sk_sp<SkDeferredDisplayList> ddl) {
+    surface->draw(std::move(ddl));
+}
+
+static std::tuple<sk_sp<SkSurface>, SkSurfaceCharacterization> create_surface_characterization(GrDirectContext* dContext) {
+    SkImageInfo ii = SkImageInfo::Make(64, 64, kRGBA_8888_SkColorType,
+                                    kPremul_SkAlphaType, SkColorSpace::MakeSRGB());
+    SkSurfaceCharacterization c = make_characterization(dContext, ii);
+    if (!c.isValid()) {
+       return {};
+    }
+
+    auto surface = make_surface(dContext, ii);
+    if (!surface) {
+        return {};
+    }
+    return {surface, c};
+}
+bool FuzzCreateDDL(Fuzz* fuzz) {
+    sk_gpu_test::GrContextFactory factory;
+    sk_gpu_test::ContextInfo ctxInfo = factory.getContextInfo(
+        factory.ContextType::kGL_ContextType);
+    GrDirectContext* dContext = ctxInfo.directContext();
+
+    std::tuple<sk_sp<SkSurface>, SkSurfaceCharacterization> tuple =
+        create_surface_characterization(dContext);
+    auto surface = std::get<0>(tuple);
+    auto c = std::get<1>(tuple);
+
+    sk_sp<SkDeferredDisplayList> ddl = make_ddl(fuzz, dContext, c);
+    if (!ddl) {
+        SkDebugf("Could not create ddl %s",
+            GrBackendApiToStr(dContext->backend()));
+        return false;
+    }
+    draw_ddl(std::move(surface), std::move(ddl));
+    return true;
+}
+
+#if defined(IS_FUZZING_WITH_LIBFUZZER)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if (size > 3000) {
+        return 0;
+    }
+    auto bytes = SkData::MakeWithoutCopy(data, size);
+    FuzzCreateDDL(bytes);
+    return 0;
+}
+#endif
