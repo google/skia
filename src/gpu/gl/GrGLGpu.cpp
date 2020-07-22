4b4a6c43772e0b916321c5977ac160c6edef20b7
Greg Daniel <egdaniel@google.com>
2020-07-22 12:09:26 -0400

    Reland "Add GrContext api to update compressed backend textures."
    This reverts commit ceebe424b1475ebdcf55f769e58b5e1a45b272c9.
    
    Reason for revert: relanding with fix
    
    Original change's description:
    > Revert "Add GrContext api to update compressed backend textures."
    > 
    > This reverts commit 2c180304dc239a44770df7bf2649f3138f25922c.
    > 
    > Reason for revert: attempted workaround did not fix techno spark so needs further investigation
    > 
    > Original change's description:
    > > Add GrContext api to update compressed backend textures.
    > > 
    > > Bug: chromium:1099255
    > > Change-Id: I0c3f25ddb037e47e3b910fa89c3d8b3aa27b3114
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/302265
    > > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > > Commit-Queue: Greg Daniel <egdaniel@google.com>
    > 
    > TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    > 
    > Change-Id: Ib5433def02dc5dad97dcdbd4476ced6de2361e6a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: chromium:1099255
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/302576
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com,robertphillips@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:1099255
    Change-Id: Ie238a56b7f12fea8b6e251a050e5e2f4b20d2ede

--- src/gpu/gl/GrGLGpu.cpp
+++ src/gpu/gl/GrGLGpu.cpp
@@ -1491,6 +1491,25 @@ bool GrGLGpu::onUpdateCompressedBackendTexture(const GrBackendTexture& backendTe
     }
 
     this->bindTextureToScratchUnit(info.fTarget, info.fID);
+
+    // If we have mips make sure the base level is set to 0 and the max level set to numMipLevels-1
+    // so that the uploads go to the right levels.
+    if (backendTexture.hasMipMaps() && this->glCaps().mipmapLevelAndLodControlSupport()) {
+        auto params = backendTexture.getGLTextureParams();
+        GrGLTextureParameters::NonsamplerState nonsamplerState = params->nonsamplerState();
+        if (params->nonsamplerState().fBaseMipMapLevel != 0) {
+            GL_CALL(TexParameteri(info.fTarget, GR_GL_TEXTURE_BASE_LEVEL, 0));
+            nonsamplerState.fBaseMipMapLevel = 0;
+        }
+        int numMipLevels =
+                SkMipmap::ComputeLevelCount(backendTexture.width(), backendTexture.height()) + 1;
+        if (params->nonsamplerState().fMaxMipmapLevel != (numMipLevels - 1)) {
+            GL_CALL(TexParameteri(info.fTarget, GR_GL_TEXTURE_MAX_LEVEL, numMipLevels - 1));
+            nonsamplerState.fBaseMipMapLevel = numMipLevels - 1;
+        }
+        params->set(nullptr, nonsamplerState, fResetTimestampForTextureParameters);
+    }
+
     bool result = this->uploadCompressedTexData(
             compression, glFormat, backendTexture.dimensions(),  mipMapped, GR_GL_TEXTURE_2D,
             rawData, rawDataSize);
@@ -2594,19 +2613,17 @@ void GrGLGpu::bindTexture(int unitIdx, GrSamplerState samplerState, const GrSwiz
     GrGLTextureParameters::SamplerOverriddenState newSamplerState;
     if (fSamplerObjectCache) {
         fSamplerObjectCache->bindSampler(unitIdx, samplerState);
-        if (this->glCaps().mustSetTexParameterMinFilterToEnableMipmapping()) {
+        if (this->glCaps().mustSetAnyTexParameterToEnableMipmapping()) {
             if (samplerState.mipmapped() == GrMipmapped::kYes) {
                 GrGLenum minFilter = filter_to_gl_min_filter(samplerState.filter(),
                                                              samplerState.mipmapMode());
                 const GrGLTextureParameters::SamplerOverriddenState& oldSamplerState =
                         texture->parameters()->samplerOverriddenState();
-                if (setAll || oldSamplerState.fMinFilter != minFilter) {
-                    this->setTextureUnit(unitIdx);
-                    GL_CALL(TexParameteri(target, GR_GL_TEXTURE_MIN_FILTER, minFilter));
-                    newSamplerState = oldSamplerState;
-                    newSamplerState.fMinFilter = minFilter;
-                    samplerStateToRecord = &newSamplerState;
-                }
+                this->setTextureUnit(unitIdx);
+                GL_CALL(TexParameteri(target, GR_GL_TEXTURE_MIN_FILTER, minFilter));
+                newSamplerState = oldSamplerState;
+                newSamplerState.fMinFilter = minFilter;
+                samplerStateToRecord = &newSamplerState;
             }
         }
     } else {
