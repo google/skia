e40ced7d9a84562d8d8be24e51236081d7c64ce9
Brian Salomon <bsalomon@google.com>
2020-07-22 10:21:17 -0400

    A few more suggest override clang 12 warning tests
    Change-Id: If8af486e741a6698cdb111b77dab4dda5ad1c938
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/305077
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>
    Auto-Submit: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- src/gpu/mtl/GrMtlUtil.mm
+++ src/gpu/mtl/GrMtlUtil.mm
@@ -119,7 +119,6 @@ id<MTLLibrary> GrCompileMtlShaderLibrary(const GrMtlGpu* gpu,
 class MtlCompileResult : public SkRefCnt {
 public:
     MtlCompileResult() : fCompiledObject(nil), fError(nil) {}
-    ~MtlCompileResult() = default;
     void set(id compiledObject, NSError* error) {
         SkAutoMutexExclusive automutex(fMutex);
         fCompiledObject = compiledObject;
