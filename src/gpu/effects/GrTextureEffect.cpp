bfe3fb275eacdb42acefe884e8d3bd76d137cca4
Brian Salomon <bsalomon@google.com>
2020-07-24 10:20:02 -0400

    Fix Mac bot??
    Change-Id: I0b5d96b53211781b0912f47c5331eca7ef1afecd

--- src/gpu/effects/GrTextureEffect.cpp
+++ src/gpu/effects/GrTextureEffect.cpp
@@ -300,7 +300,7 @@ GrTextureEffect::ShaderMode GrTextureEffect::GetShaderMode(Wrap wrap,
                     SkUNREACHABLE;
                 case MipmapMode::kLinear:
                     switch (filter) {
-                        case Filter::kNearest: return ShaderMode::kRepeat_Nearest_Mipmap;
+                        case Filter::kNearest: //return ShaderMode::kRepeat_Nearest_Mipmap;
                         case Filter::kLinear:  return ShaderMode::kRepeat_Linear_Mipmap;
                     }
                     SkUNREACHABLE;
@@ -791,6 +791,9 @@ GrTextureEffect::GrTextureEffect(GrSurfaceProxyView view,
         , fClamp(sampling.fShaderClamp)
         , fShaderModes{sampling.fShaderModes[0], sampling.fShaderModes[1]}
         , fLazyProxyNormalization(lazyProxyNormalization) {
+    if (fSamplerState.mipmapped() == GrMipmapped::kYes) {
+        fSamplerState.setFilterMode(GrSamplerState::Filter::kLinear);
+    }
     // We always compare the range even when it isn't used so assert we have canonical don't care
     // values.
     SkASSERT(fShaderModes[0] != ShaderMode::kNone || (fSubset.fLeft == 0 && fSubset.fRight == 0));
@@ -822,10 +825,23 @@ std::unique_ptr<GrFragmentProcessor> GrTextureEffect::TestCreate(GrProcessorTest
     Wrap wrapModes[2];
     GrTest::TestWrapModes(testData->fRandom, wrapModes);
 
-    Filter filter = testData->fRandom->nextBool() ? Filter::kLinear : Filter::kNearest;
+    GrSamplerState::Filter filter;
     MipmapMode mm = MipmapMode::kNone;
     if (view.asTextureProxy()->mipmapped() == GrMipmapped::kYes) {
-        mm = testData->fRandom->nextBool() ? MipmapMode::kLinear : MipmapMode::kNone;
+        switch (testData->fRandom->nextULessThan(3)) {
+            case 0:
+                filter = Filter::kNearest;
+                break;
+            case 1:
+                filter = Filter::kLinear;
+                break;
+            default:
+                filter = Filter::kLinear;
+                mm = MipmapMode::kLinear;
+                break;
+        }
+    } else {
+        filter = testData->fRandom->nextBool() ? Filter::kLinear : Filter::kNearest;
     }
     GrSamplerState params(wrapModes, filter, mm);
 
--- src/gpu/ops/GrTextureOp.cpp
+++ src/gpu/ops/GrTextureOp.cpp
@@ -475,6 +475,9 @@ private:
             , fTextureColorSpaceXform(std::move(textureColorSpaceXform))
             , fDesc(nullptr)
             , fMetadata(proxyView.swizzle(), filter, mm, Subset(!!subsetRect), saturate) {
+        if (mm == GrSamplerState::MipmapMode::kLinear) {
+            filter = GrSamplerState::Filter::kLinear;
+        }
         // Clean up disparities between the overall aa type and edge configuration and apply
         // optimizations based on the rect and matrix when appropriate
         GrQuadUtils::ResolveAAType(aaType, quad->fEdgeFlags, quad->fDevice,
@@ -528,6 +531,10 @@ private:
                         GrSamplerState::MipmapMode::kNone,
                         Subset::kNo,
                         saturate) {
+        if (mm == GrSamplerState::MipmapMode::kLinear) {
+            filter = GrSamplerState::Filter::kLinear;
+        }
+
         // Update counts to reflect the batch op
         fMetadata.fProxyCount = SkToUInt(proxyRunCnt);
         fMetadata.fTotalQuadCount = SkToUInt(cnt);
@@ -536,8 +543,8 @@ private:
 
         GrAAType netAAType = GrAAType::kNone; // aa type maximally compatible with all dst rects
         Subset netSubset = Subset::kNo;
-        GrSamplerState::Filter netFilter = GrSamplerState::Filter::kNearest;
-        GrSamplerState::MipmapMode netMM = GrSamplerState::MipmapMode::kNone;
+        GrSamplerState::Filter netFilter = filter;
+        GrSamplerState::MipmapMode netMM = mm;
 
         const GrSurfaceProxy* curProxy = nullptr;
 
@@ -1338,12 +1345,24 @@ GR_DRAW_OP_TEST_DEFINE(TextureOp) {
     srcRect.fBottom = random->nextRangeScalar(0.f, proxy->height()) + proxy->height() / 2.f;
     SkMatrix viewMatrix = GrTest::TestMatrixPreservesRightAngles(random);
     SkPMColor4f color = SkPMColor4f::FromBytes_RGBA(SkColorToPremulGrColor(random->nextU()));
-    GrSamplerState::Filter filter = (GrSamplerState::Filter)random->nextULessThan(
-            static_cast<uint32_t>(GrSamplerState::Filter::kLast) + 1);
+
+    int f = random->nextULessThan(3);
+    while (mipMapped == GrMipmapped::kNo && f == 2) {
+        f = random->nextULessThan(3);
+    }
+    GrSamplerState::Filter filter;
     GrSamplerState::MipmapMode mm = GrSamplerState::MipmapMode::kNone;
-    if (mipMapped == GrMipmapped::kYes) {
-        mm = (GrSamplerState::MipmapMode)random->nextULessThan(
-                static_cast<uint32_t>(GrSamplerState::MipmapMode::kLast) + 1);
+    switch (f) {
+        case 0:
+            filter = GrSamplerState::Filter::kNearest;
+            break;
+        case 1:
+            filter = GrSamplerState::Filter::kLinear;
+            break;
+        default:
+            filter = GrSamplerState::Filter::kLinear;
+            mm = GrSamplerState::MipmapMode::kLinear;
+            break;
     }
 
     auto texXform = GrTest::TestColorXform(random);
