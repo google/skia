fbe05efac99cc3d949186d90e740bfeffda23f82
Ethan Nicholas <ethannicholas@google.com>
2020-07-24 17:16:54 -0400

    SkSL include files are now stored in a binary format
    This speeds up compiler construction, because we no longer have to parse
    and process a bunch of SkSL source code during startup.
    
    Change-Id: I6d6bd9b5ce78b1661be691708ab84bf399c6df8b

---
+++ gn/dehydrate_sksl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Copyright 2020 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+import sys
+
+skslc = sys.argv[1]
+output = sys.argv[2]
+includes = sys.argv[3:]
+
+for inc in includes:
+    print("Recompiling " + inc + "...")
+    try:
+        noExt, _ = os.path.splitext(inc)
+        head, tail = os.path.split(noExt)
+        if head.endswith("generated"):
+            targetDir = head
+        else:
+            targetDir = os.path.join(head, "generated")
+        if not os.path.isdir(targetDir):
+            os.mkdir(targetDir)
+        target = os.path.join(targetDir, tail)
+        subprocess.check_output([skslc, inc, target + ".dehydrated.sksl"])
+        contents = open(inc, "r").read()
+        open(target + ".c.inc", "w").write("R\"SKSL(\n" + contents + ")SKSL\";")
+    except subprocess.CalledProcessError as err:
+        print("### Error compiling " + inc + ":")
+        print(err.output)
+        exit(1)
+open(output, 'w')
--- src/gpu/effects/generated/GrColorMatrixFragmentProcessor.cpp
+++ src/gpu/effects/generated/GrColorMatrixFragmentProcessor.cpp
@@ -42,12 +42,12 @@ public:
         fragBuilder->codeAppendf(
                 R"SkSL(half4 inputColor = %s;
 @if (%s) {
-    half4 inlineResult_fp_0;
-    half4 inlineArg_fp_1_0 = inputColor;
+    half4 _inlineResult_fp_0;
+    half4 _inlineArg_fp_1_0 = inputColor;
     {
-        inlineResult_fp_0 = half4(inlineArg_fp_1_0.xyz / max(inlineArg_fp_1_0.w, 9.9999997473787516e-05), inlineArg_fp_1_0.w);
+        _inlineResult_fp_0 = half4(_inlineArg_fp_1_0.xyz / max(_inlineArg_fp_1_0.w, 9.9999997473787516e-05), _inlineArg_fp_1_0.w);
     }
-    inputColor = inlineResult_fp_0;
+    inputColor = _inlineResult_fp_0;
 
 }
 %s = %s * inputColor + %s;
