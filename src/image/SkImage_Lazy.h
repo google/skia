3fc7904e5715038a8599cd86f64e380877cc9693
Brian Salomon <bsalomon@google.com>
2020-07-24 11:27:45 -0400

    Remove GrYUVProvider.
    This abstraction had one subclass. That subclass was used only as
    an implementation detail of SkImage_Lazy. Move the functionality
    into SkImage_Lazy.
    
    Change-Id: Icd8754114dc33ae957e542b372086e8caa0408a3

--- src/image/SkImage_Lazy.h
+++ src/image/SkImage_Lazy.h
@@ -44,8 +44,10 @@ public:
                       CachingHint) const override;
 #if SK_SUPPORT_GPU
     GrSurfaceProxyView refView(GrRecordingContext*, GrMipmapped) const override;
-    sk_sp<SkCachedData> getPlanes(SkYUVASizeInfo*, SkYUVAIndex[4],
-                                  SkYUVColorSpace*, const void* planes[4]) override;
+    sk_sp<SkCachedData> getPlanes(SkYUVASizeInfo*,
+                                  SkYUVAIndex[4],
+                                  SkYUVColorSpace*,
+                                  const void* planes[4]) const override;
 #endif
     sk_sp<SkData> onRefEncoded() const override;
     sk_sp<SkImage> onMakeSubset(const SkIRect&, GrDirectContext*) const override;
@@ -73,6 +75,14 @@ public:
 
 private:
     void addUniqueIDListener(sk_sp<SkIDChangeListener>) const;
+#if SK_SUPPORT_GPU
+    GrSurfaceProxyView textureProxyViewFromPlanes(GrRecordingContext* ctx,
+                                                  SkISize dimensions,
+                                                  GrColorType colorType,
+                                                  SkColorSpace* srcColorSpace,
+                                                  SkColorSpace* dstColorSpace,
+                                                  SkBudgeted budgeted) const;
+#endif
 
     class ScopedGenerator;
 
