cf7d2c0981bb736de8f6d6c72cae999574b59779
Julia Lavrova <jlavrova@google.com>
2020-07-27 18:59:18 +0000

    Reland "Attach whitespaces to the neighbor unresolved blocks"
    This reverts commit 143646297e0af3d76e9bad780e6a07e2bf3fdeb0.
    
    Reason for revert: Have to fix it and reland again
    
    Original change's description:
    > Revert "Attach whitespaces to the neighbor unresolved blocks"
    >
    > This reverts commit 131c5ad6f1afe7a2d373031b960580c1b6b4796e.
    >
    > Reason for revert: Blocking the G3 roll
    >
    > Original change's description:
    > > Attach whitespaces to the neighbor unresolved blocks
    > >
    > > Fix the situation when an unresolved {arabic} text is broken into
    > > many small runs by resolved english spaces.
    > >
    > > Bug: skia:10487
    > > Change-Id: I3a739501c0fb7e0fc845e68392e1d214df9302db
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/304000
    > > Commit-Queue: Julia Lavrova <jlavrova@google.com>
    > > Reviewed-by: Ben Wagner <bungeman@google.com>
    >
    > TBR=bungeman@google.com,jlavrova@google.com
    >
    > Change-Id: Iaa338dd5fb5c9962df2ee32bafbc089da0e2b8a1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:10487
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/305797
    > Reviewed-by: Robert Phillips <robertphillips@google.com>
    > Commit-Queue: Robert Phillips <robertphillips@google.com>
    
    TBR=bungeman@google.com,robertphillips@google.com,jlavrova@google.com
    
    Bug: skia:10487
    Change-Id: If8a85254fd536f465d80766de0406053d90c96a3

--- modules/skparagraph/src/ParagraphUtil.h
+++ modules/skparagraph/src/ParagraphUtil.h
@@ -10,6 +10,7 @@ namespace textlayout {
 SkString SkStringFromU16String(const std::u16string& utf16text);
 SkUnichar nextUtf8Unit(const char** ptr, const char* end);
 bool isControl(SkUnichar utf8);
+bool isWhitespace(SkUnichar utf8);
 }
 }
 
