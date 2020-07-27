ab0cabcd1824d8383e3ed5d3b43eb9b3a2c48cfe
Stan Iliev <stani@google.com>
2020-07-27 19:51:51 -0400

    Skottie perf metric improvements
    Add 2 lottie files given by wearos.
    Measure startup time.
    Measure total time for 7 seconds starting from second frame.
    
    Test: Ran tests on wearos and android.
    Change-Id: I87ddcd116df0cd39df42636cca518a91c3be0329

--- platform_tools/android/apps/skottie/run_perf.sh
+++ platform_tools/android/apps/skottie/run_perf.sh
@@ -12,11 +12,11 @@ if [ ! -f "trace_processor" ]; then
     chmod +x trace_processor
 fi
 
-declare -a testnames=("movie_loading" "uk" "white_material_wave_loading"
+declare -a testnames=("star" "movie_loading" "uk" "white_material_wave_loading"
             "check_animation" "confetti" "gears"
             "hand_sanitizer" "heart_preloader" "i_was_scared_after_that_brouhaha"
             "im_thirsty" "true_will" "workout_monkey_stay_healthy"
-            "ripple_loading_animation" "signature" "asdasd")
+            "ripple_loading_animation" "signature" "asdasd" "celebration" "check")
 
 adb root
 # get out of the lock screen
@@ -31,8 +31,8 @@ renderer_names=(lottie_hw lottie_sw skottie)
 for renderer in {0,2}
 do
 echo "renderer " ${renderer_names[${renderer}]}
-# iterate over each of the 15 lottie files
-for file in {0..14}
+# iterate over each of the 18 lottie files
+for file in {0..17}
 do
 if [[ $1 == "delay" ]]; then
   # start program first and wait 7 seconds for program to load fully
