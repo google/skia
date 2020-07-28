5890fec2db920849e86aac71d35d9b93002bcb20
Robert Phillips <robertphillips@google.com>
2020-07-28 15:23:42 -0400

    Skip the compressed_texture* GMs on the Pixel3
    Bug: skia:10546
    Change-Id: Ic34cb2680b7ddf47ca8b8a0c862bd9194c001ae5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/306318
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -608,6 +608,13 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 		skip("_ gm _ gpu_blur_utils_ref")
 	}
 
+	if b.model("Pixel3") {
+		// skbug.com/10546
+		skip("vkddl gm _ compressed_textures_nmof")
+		skip("vkddl gm _ compressed_textures_npot")
+		skip("vkddl gm _ compressed_textures")
+	}
+
 	if b.model("TecnoSpark3Pro") {
 		// skbug.com/9421
 		skip("_ test _ InitialTextureClear")
