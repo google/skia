052046ee5d4868585e922aab58812dae8acda94b
Ben Wagner <bungeman@google.com>
2020-07-28 10:26:57 -0400

    Mark sk_abort_no_print [[noreturn]].
    It isn't expected that sk_abort_no_print should return (it should
    abort). This also allows removal of the SkUNREACHABLE from SK_ABORT.
    
    Also make sk_abort_no_print actually not able to return.
    
    Change-Id: Ibd5eda019820ed7e91de37a048efdfb0d1097aff

--- src/ports/SkMemory_malloc.cpp
+++ src/ports/SkMemory_malloc.cpp
@@ -9,6 +9,11 @@
 
 #include <cstdlib>
 
+#if defined(SK_DEBUG) && defined(SK_BUILD_FOR_WIN)
+#include <intrin.h>
+#include <winnt.h>
+#endif
+
 #define SK_DEBUGFAILF(fmt, ...) \
     SkASSERT((SkDebugf(fmt"\n", __VA_ARGS__), false))
 
@@ -36,9 +41,9 @@ void sk_abort_no_print() {
     _set_abort_behavior(0, _WRITE_ABORT_MSG);
 #endif
 #if defined(SK_DEBUG) && defined(SK_BUILD_FOR_WIN)
-    __debugbreak();
+    __fastfail(FAST_FAIL_INVALID_ARG);
 #elif defined(__clang__)
-    __builtin_debugtrap();
+    __builtin_trap();
 #else
     abort();
 #endif
