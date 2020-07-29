7dcc8e2b06de893dd95eff4710a2fdbcb82e77ab
Robert Phillips <robertphillips@google.com>
2020-07-29 15:35:53 -0400

    Add debugging info for Chrome
    Change-Id: I8f08fa4d5db5dbfe4a28145737895b655a145b08

--- src/gpu/ops/GrTextureOp.cpp
+++ src/gpu/ops/GrTextureOp.cpp
@@ -806,6 +806,25 @@ private:
     int numQuads() const final { return this->totNumQuads(); }
 #endif
 
+    static const char* bar(GrAAType aaType) {
+        switch (aaType) {
+        case GrAAType::kNone: return "kNone";
+        case GrAAType::kCoverage: return "kCoverage";
+        case GrAAType::kMSAA: return "kMSAA";
+        }
+
+        return "kUnknown";
+    }
+
+    int numInChain() const {
+        int count = 0;
+
+        for (const auto& op : ChainRange<TextureOp>(this)) {
+            ++count;
+        }
+        return count;
+    }
+
     void characterize(Desc* desc) const {
         GrQuad::Type quadType = GrQuad::Type::kAxisAligned;
         ColorType colorType = ColorType::kNone;
@@ -813,10 +832,13 @@ private:
         Subset subset = Subset::kNo;
         GrAAType overallAAType = fMetadata.aaType();
 
+        printf("initialAA %s num-in-chain %d\n", bar(overallAAType), this->numInChain());
+
         desc->fNumProxies = 0;
         desc->fNumTotalQuads = 0;
         int maxQuadsPerMesh = 0;
 
+        int opIndex = 0;
         for (const auto& op : ChainRange<TextureOp>(this)) {
             if (op.fQuads.deviceQuadType() > quadType) {
                 quadType = op.fQuads.deviceQuadType();
@@ -837,11 +859,15 @@ private:
 
             if (op.fMetadata.aaType() == GrAAType::kCoverage) {
                 overallAAType = GrAAType::kCoverage;
+                printf("changing to kCoverage in op %d\n", opIndex);
             }
+
+            ++opIndex;
         }
 
         SkASSERT(desc->fNumTotalQuads == this->numChainedQuads());
 
+        //$$
         SkASSERT(!CombinedQuadCountWillOverflow(overallAAType, false, desc->fNumTotalQuads));
 
         auto indexBufferOption = GrQuadPerEdgeAA::CalcIndexBufferOption(overallAAType,
