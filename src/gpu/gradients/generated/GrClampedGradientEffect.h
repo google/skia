afff5bd830fd70a7eea8ab38265a87a35af01117
John Stiles <johnstiles@google.com>
2020-07-31 12:33:11 -0400

    Update gradient colorizers to take coords instead of colors.
    Gradient layout FPs were passing a position to their colorizers via
    sk_InColor.x. This worked, but it makes more sense to pass coordinates
    to children by sampling the child at explicit coordinates.
    
    Change-Id: I60520f2e54e04bc9c4d8562e6b2256b95e0de5b4
    Bug: skia:10548

--- src/gpu/gradients/generated/GrClampedGradientEffect.h
+++ src/gpu/gradients/generated/GrClampedGradientEffect.h
@@ -54,7 +54,7 @@ private:
             , makePremul(makePremul)
             , colorsAreOpaque(colorsAreOpaque) {
         SkASSERT(colorizer);
-        this->registerChild(std::move(colorizer), SkSL::SampleUsage::PassThrough());
+        this->registerChild(std::move(colorizer), SkSL::SampleUsage::Explicit());
         SkASSERT(gradLayout);
         this->registerChild(std::move(gradLayout), SkSL::SampleUsage::PassThrough());
     }
