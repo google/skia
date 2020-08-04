270a53d229a9272c2ff0e21800e3d87d2f9133ee
John Stiles <johnstiles@google.com>
2020-08-04 16:45:47 -0400

    Add SkWrapUnique to SkTemplates.
    This allows an existing object to be wrapped in a unique_ptr without
    restating its typename.
    
    This parallels Chromium's `base::WrapUnique` or Abseil's
    `absl::WrapUnique`. The in-depth rationale and use case are explained
    pretty thoroughly at https://abseil.io/tips/126 or http://go/totw/126
    
    Change-Id: I15ee404d9d8a83047218cbece74c95cf8a1de7a3

--- include/private/SkTemplates.h
+++ include/private/SkTemplates.h
@@ -451,15 +451,49 @@ private:
 
 using SkAutoFree = std::unique_ptr<void, SkFunctionWrapper<void(void*), sk_free>>;
 
-template<typename C, std::size_t... Is>
+template <typename C, std::size_t... Is>
 constexpr auto SkMakeArrayFromIndexSequence(C c, std::index_sequence<Is...>)
--> std::array<std::result_of_t<C(std::size_t)>, sizeof...(Is)> {
+        -> std::array<std::result_of_t<C(std::size_t)>, sizeof...(Is)> {
     return {{ c(Is)... }};
 }
 
-template<size_t N, typename C> constexpr auto SkMakeArray(C c)
--> std::array<std::result_of_t<C(std::size_t)>, N> {
+template <size_t N, typename C>
+constexpr auto SkMakeArray(C c) -> std::array<std::result_of_t<C(std::size_t)>, N> {
     return SkMakeArrayFromIndexSequence(c, std::make_index_sequence<N>{});
+}
+
+/**
+ * Adopts ownership from a raw pointer and transfers it to the returned `std::unique_ptr`, whose
+ * type is deduced. Because of this deduction, *do not* specify the template type `T` when calling
+ * `SkWrapUnique`.
+ *
+ * Example:
+ *   Foo* MakeFoo(int);
+ *   auto x = SkWrapUnique(MakeFoo(123));  // 'x' is std::unique_ptr<X>.
+ *
+ * Do not call SkWrapUnique with an explicit type, as in `SkWrapUnique<X>(MakeFoo(123))`.  The
+ * purpose of SkWrapUnique is to automatically deduce the pointer type. If you wish to make the type
+ * explicit, just use `std::unique_ptr` directly.
+ *
+ *   auto x = std::unique_ptr<X>(MakeFoo(123));
+ *                  - or -
+ *   std::unique_ptr<X> x(MakeFoo(123));
+ *
+ * While `SkWrapUnique` is useful for capturing the output of a raw pointer factory, prefer
+ * 'std::make_unique<T>(args...)' over 'SkWrapUnique(new T(args...))'.
+ *
+ *   auto x = SkWrapUnique(new Foo(123));  // works, but nonideal.
+ *   auto x = std::make_unique<Foo>(123);  // safer, standard, avoids raw 'new'.
+ *
+ * Note that `SkWrapUnique(p)` is valid only if `delete p` is a valid expression. In particular,
+ * `SkWrapUnique()` cannot wrap pointers to arrays, functions or void, and it must not be used to
+ * capture pointers obtained from array-new expressions (even though that would compile!).
+ */
+template <typename T>
+std::unique_ptr<T> SkWrapUnique(T* ptr) {
+    static_assert(!std::is_array<T>::value, "array types are unsupported");
+    static_assert(std::is_object<T>::value, "non-object types are unsupported");
+    return std::unique_ptr<T>(ptr);
 }
 
 #endif
