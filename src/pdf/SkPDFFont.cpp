6f0545e04aff5f0704199a9406eb61badbbc8305
Ben Wagner <bungeman@google.com>
2020-08-04 13:36:52 -0400

    Rename kMultiMaster_FontFlag to kVariable_FontFlag
    The older font format is mostly unused now. Use the new name for fonts
    with parameteriazable glyph outlines.
    
    Change-Id: Ia632f79684b2c4399d216855cfe0822f535189c7

--- src/pdf/SkPDFFont.cpp
+++ src/pdf/SkPDFFont.cpp
@@ -167,7 +167,7 @@ const std::vector<SkUnichar>& SkPDFFont::GetUnicodeMap(const SkTypeface* typefac
 }
 
 SkAdvancedTypefaceMetrics::FontType SkPDFFont::FontType(const SkAdvancedTypefaceMetrics& metrics) {
-    if (SkToBool(metrics.fFlags & SkAdvancedTypefaceMetrics::kMultiMaster_FontFlag) ||
+    if (SkToBool(metrics.fFlags & SkAdvancedTypefaceMetrics::kVariable_FontFlag) ||
         SkToBool(metrics.fFlags & SkAdvancedTypefaceMetrics::kNotEmbeddable_FontFlag)) {
         // force Type3 fallback.
         return SkAdvancedTypefaceMetrics::kOther_Font;
