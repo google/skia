c29323f06ec2cba29aa01536d222d32d26a54339
John Stiles <johnstiles@google.com>
2020-08-04 17:35:36 -0400

    Add SkWrapUnique to SkTemplates.
    This allows an existing object to be wrapped in a unique_ptr without
    restating its typename.
    
    This parallels Chromium's `base::WrapUnique` or Abseil's
    `absl::WrapUnique`. The in-depth rationale and use case are explained
    pretty thoroughly at https://abseil.io/tips/126 or http://go/totw/126
    
    DO NOT SUBMIT: I'd like to use this inside SkSL compiler code so this
    may not be the best place for this method to live.
    
    Change-Id: I15ee404d9d8a83047218cbece74c95cf8a1de7a3

---
+++ include/private/SkPtrUtil.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkPtrUtil_DEFINED
+#define SkPtrUtil_DEFINED
+
+#include <memory>
+#include <type_traits>
+
+/**
+ * Adopts ownership from a raw pointer and transfers it to the returned `std::unique_ptr`, whose
+ * type is deduced. Because of this deduction, *do not* specify the template type `T` when calling
+ * `SkWrapUnique`.
+ *
+ * Example:
+ *   Foo* MakeFoo(int);
+ *   auto x = SkWrapUnique(MakeFoo(123));  // 'x' is std::unique_ptr<X>.
+ *
+ * Do not call SkWrapUnique with an explicit type, as in `SkWrapUnique<X>(MakeFoo(123))`.  The
+ * purpose of SkWrapUnique is to automatically deduce the pointer type. If you wish to make the type
+ * explicit, just use `std::unique_ptr` directly.
+ *
+ *   auto x = std::unique_ptr<X>(MakeFoo(123));
+ *                  - or -
+ *   std::unique_ptr<X> x(MakeFoo(123));
+ *
+ * While `SkWrapUnique` is useful for capturing the output of a raw pointer factory, prefer
+ * 'std::make_unique<T>(args...)' over 'SkWrapUnique(new T(args...))'.
+ *
+ *   auto x = SkWrapUnique(new Foo(123));  // works, but nonideal.
+ *   auto x = std::make_unique<Foo>(123);  // safer, standard, avoids raw 'new'.
+ *
+ * Note that `SkWrapUnique(p)` is valid only if `delete p` is a valid expression. In particular,
+ * `SkWrapUnique()` cannot wrap pointers to arrays, functions or void, and it must not be used to
+ * capture pointers obtained from array-new expressions (even though that would compile!).
+ */
+template <typename T>
+std::unique_ptr<T> SkWrapUnique(T* ptr) {
+    static_assert(!std::is_array<T>::value, "array types are unsupported");
+    static_assert(std::is_object<T>::value, "non-object types are unsupported");
+    return std::unique_ptr<T>(ptr);
+}
+
+#endif
