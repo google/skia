83a0fe16f83bb1469c29c740a514d5d7d9fc360a
Mike Reed <reed@google.com>
2020-08-04 10:35:44 -0400

    SPath for ownerless (can live on stack) Paths
    Change-Id: I882b4f4c60e80235ca83c86c926e905b269a7afd

---
+++ src/core/SkSPath.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#include "include/core/SkPoint.h"
+#include "include/core/SkRect.h"
+#include "src/core/SkSpan.h"
+
+class SkSPath {
+public:
+    SkSPath(SkSpan<SkPoint> pts, SkSpan<uint8_t> verbs, SkSpan<float> weights,
+            SkPathFillType ft, const SkRect& bounds, unsigned segmentMask)
+        : fPoints(points)
+        , fVerbs(verbs)
+        , fWeights(weights)
+        , fBounds(bounds)
+        , fFillType(ft)
+        , fSegmentMask(segmentMask)
+    {}
+
+    // Computes bounds and segmentMask
+    //
+    SkSPath(SkSpan<SkPoint> pts, SkSpan<uint8_t> verbs, SkSpan<float> weights, SkPathFillType ft);
+
+    SkSpan<SkPoint> fPoints;
+    SkSpan<uint8_t> fVerbs;
+    SkSpan<float>   fWeights;
+
+    SkRect          fBounds;
+
+    SkPathFillType  fFillType;
+    uint8_t         fSegmentMask;
+};
