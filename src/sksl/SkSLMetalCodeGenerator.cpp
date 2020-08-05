8b76f8afdd21830ee5c56a093e38b672ac7ba502
John Stiles <johnstiles@google.com>
2020-08-05 11:14:33 -0400

    Fix out-of-bounds array access discovered by fuzzer.
    The root cause of this error is that Metal does not support upcasting
    a small matrix into a larger matrix (I was unfamiliar with this GLSL
    ability). Proper support in Metal for that type of cast will be added in
    a followup CL. For now, this CL adds defensive bounds-checking to
    placate ASAN.
    
    Change-Id: Ieb0d6b14f9bc7ecc9ef6223364c606612dc79e43
    Bug: oss-fuzz:22776

--- src/sksl/SkSLMetalCodeGenerator.cpp
+++ src/sksl/SkSLMetalCodeGenerator.cpp
@@ -439,33 +439,38 @@ String MetalCodeGenerator::getMatrixConstructHelper(const Constructor& c) {
             fExtraFunctions.printf("%s", rowSeparator);
             rowSeparator = ", ";
 
-            const Type& argType = args[argIndex]->fType;
-            switch (argType.kind()) {
-                case Type::kScalar_Kind: {
-                    fExtraFunctions.printf("x%zu", argIndex);
-                    break;
+            if (argIndex < args.size()) {
+                const Type& argType = args[argIndex]->fType;
+                switch (argType.kind()) {
+                    case Type::kScalar_Kind: {
+                        fExtraFunctions.printf("x%zu", argIndex);
+                        break;
+                    }
+                    case Type::kVector_Kind: {
+                        fExtraFunctions.printf("x%zu[%d]", argIndex, argPosition);
+                        break;
+                    }
+                    case Type::kMatrix_Kind: {
+                        fExtraFunctions.printf("x%zu[%d][%d]", argIndex,
+                                               argPosition / argType.rows(),
+                                               argPosition % argType.rows());
+                        break;
+                    }
+                    default: {
+                        SkDEBUGFAIL("incorrect type of argument for matrix constructor");
+                        fExtraFunctions.printf("<error>");
+                        break;
+                    }
                 }
-                case Type::kVector_Kind: {
-                    fExtraFunctions.printf("x%zu[%d]", argIndex, argPosition);
-                    break;
-                }
-                case Type::kMatrix_Kind: {
-                    fExtraFunctions.printf("x%zu[%d][%d]", argIndex,
-                                           argPosition / argType.rows(),
-                                           argPosition % argType.rows());
-                    break;
-                }
-                default: {
-                    SkDEBUGFAIL("incorrect type of argument for matrix constructor");
-                    fExtraFunctions.printf("<error>");
-                    break;
-                }
-            }
 
-            ++argPosition;
-            if (argPosition >= argType.columns() * argType.rows()) {
-                ++argIndex;
-                argPosition = 0;
+                ++argPosition;
+                if (argPosition >= argType.columns() * argType.rows()) {
+                    ++argIndex;
+                    argPosition = 0;
+                }
+            } else {
+                SkDEBUGFAIL("not enough arguments for matrix constructor");
+                fExtraFunctions.printf("<error>");
             }
         }
     }
