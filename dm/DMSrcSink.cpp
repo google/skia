18d1b5d03234380dddb358c3a02daeea42fa0688
Leon Scroggins <scroggo@google.com>
2020-08-05 14:42:41 +0000

    Revert "Add an SkImageGenerator that uses NDK APIs"
    This reverts commit cfef980939de1c1f6e21a43b16423f0912e87074.
    
    Reason for revert: Breaking Google3 roll
    
    Original change's description:
    > Add an SkImageGenerator that uses NDK APIs
    > 
    > Bug: skia:10369
    > Bug: skia:10371
    > 
    > This will allow Skia clients developing for Android 11+ to rely on
    > Android's NDK APIs for decoding, which will allow them to decode
    > without including their own decoding libraries (e.g. libjpeg-turbo).
    > Using these APIs also provides support for static HEIF images.
    > 
    > Run ImageGenSrc in kPlatform_Mode on Android to verify decoding
    > visually.
    > 
    > Add tests and a grayscale png.
    > 
    > Update some test bots running Android R to specify ndk_api so they will
    > run the new code.
    > 
    > Change-Id: Ica782339b2414d472ede0b61729a127ce41892a5
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/305689
    > Reviewed-by: Mike Klein <mtklein@google.com>
    > Commit-Queue: Leon Scroggins <scroggo@google.com>
    
    TBR=djsollen@google.com,mtklein@google.com,scroggo@google.com,brianosman@google.com,reed@google.com
    
    Change-Id: Ifed506a76a0ff5903d101c1bf7330d319b8376a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:10369
    Bug: skia:10371

--- dm/DMSrcSink.cpp
+++ dm/DMSrcSink.cpp
@@ -24,7 +24,6 @@
 #include "include/gpu/GrBackendSurface.h"
 #include "include/gpu/GrDirectContext.h"
 #include "include/ports/SkImageGeneratorCG.h"
-#include "include/ports/SkImageGeneratorNDK.h"
 #include "include/ports/SkImageGeneratorWIC.h"
 #include "include/private/SkImageInfoPriv.h"
 #include "include/private/SkTLogic.h"
@@ -924,8 +923,6 @@ Result ImageGenSrc::draw(GrDirectContext*, SkCanvas* canvas) const {
             gen = SkImageGeneratorCG::MakeFromEncodedCG(encoded);
 #elif defined(SK_BUILD_FOR_WIN)
             gen = SkImageGeneratorWIC::MakeFromEncodedWIC(encoded);
-#elif defined(SK_ENABLE_NDK_DECODING)
-            gen = SkImageGeneratorNDK::MakeFromEncodedNDK(encoded);
 #endif
             if (!gen) {
                 return Result::Fatal("Could not create platform image generator.");
