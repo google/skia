f5812a042ea33970f6cb908b6659bae0353a9dfc
Robert Phillips <robertphillips@google.com>
2020-08-06 10:20:18 -0400

    Split the small path renderer into record-time and flush-time pieces
    For the small path renderer to work w/ DDLs the actual allocation of
    the atlases and the positioning of paths in the atlas has to happen
    at flush time. This change makes the small path renderer more like the
    text rendering system.
    
    Change-Id: Id54b8b27b7684fc9bd6e6e2c933fba51e7d35174

--- src/gpu/text/GrAtlasManager.h
+++ src/gpu/text/GrAtlasManager.h
@@ -13,8 +13,77 @@
 #include "src/gpu/GrOnFlushResourceProvider.h"
 #include "src/gpu/GrProxyProvider.h"
 
+#include "src/core/SkTDynamicHash.h"
+#include "src/core/SkTInternalLList.h"
+
 class GrGlyph;
+class GrSmallPathShapeData;
+class GrSmallPathShapeDataKey;
+class GrStyledShape;
 class GrTextStrike;
+class ShapeDataKey;
+
+//#include "src/core/SkOpts.h"
+//#include "src/gpu/geometry/GrStyledShape.h"
+
+
+
+class GrSmallPathAtlasMgr : public GrOnFlushCallbackObject,
+                            public GrDrawOpAtlas::EvictionCallback,
+                            public GrDrawOpAtlas::GenerationCounter {
+public:
+    GrSmallPathAtlasMgr();
+    ~GrSmallPathAtlasMgr() override;
+
+    bool initAtlas(GrProxyProvider*, const GrCaps*);
+
+    GrDrawOpAtlas* atlas() { return fAtlas.get(); }
+
+    GrSmallPathShapeData* findOrCreate(const GrStyledShape&, int desiredDimension);
+    GrSmallPathShapeData* findOrCreate(const GrStyledShape&, const SkMatrix& ctm);
+
+    void updateCacheInfo(GrSmallPathShapeData*, GrDrawOpAtlas::AtlasLocator&, const SkRect& bounds);
+
+    void setUseToken(GrSmallPathShapeData*, GrDeferredUploadToken);
+
+    // GrOnFlushCallbackObject overrides
+    //
+    // Note: because this class is associated with a path renderer we want it to be removed from
+    // the list of active OnFlushCallbackObjects in an freeGpuResources call (i.e., we accept the
+    // default retainOnFreeGpuResources implementation).
+    void preFlush(GrOnFlushResourceProvider* onFlushRP,
+                  const uint32_t* /* opsTaskIDs */,
+                  int /* numOpsTaskIDs */) override {
+        if (fAtlas) {
+            fAtlas->instantiate(onFlushRP);
+        }
+    }
+
+    void postFlush(GrDeferredUploadToken startTokenForNextFlush,
+                   const uint32_t* /* opsTaskIDs */,
+                   int /* numOpsTaskIDs */) override {
+        if (fAtlas) {
+            fAtlas->compact(startTokenForNextFlush);
+        }
+    }
+
+    const GrSurfaceProxyView* getViews(unsigned int* numActiveProxies) {
+        *numActiveProxies = fAtlas->numActivePages();
+        return fAtlas->getViews();
+    }
+
+private:
+    GrSmallPathShapeData* findOrCreate(const GrSmallPathShapeDataKey&);
+
+    using ShapeCache = SkTDynamicHash<GrSmallPathShapeData, GrSmallPathShapeDataKey>;
+    typedef SkTInternalLList<GrSmallPathShapeData> ShapeDataList;
+
+    void evict(GrDrawOpAtlas::PlotLocator) override;
+
+    std::unique_ptr<GrDrawOpAtlas> fAtlas;
+    ShapeCache fShapeCache;
+    ShapeDataList fShapeList;
+};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /** The GrAtlasManager manages the lifetime of and access to GrDrawOpAtlases.
