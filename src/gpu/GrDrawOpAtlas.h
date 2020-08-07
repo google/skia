0475aef96a55f90c7d3e703c4b60ca48d2e875af
Robert Phillips <robertphillips@google.com>
2020-08-07 14:35:56 -0400

    Update how the GrSmallPathRenderer's cache entries are created and destroyed
    The main thrust of this CL is to centralize creation and deletion
    of the small path renderer's cache entries within the nascent
    GrSmallPathAtlasMgr class.
    
    Change-Id: I13ccaca428e8c019540624dbcd0703540418f9a0

--- src/gpu/GrDrawOpAtlas.h
+++ src/gpu/GrDrawOpAtlas.h
@@ -87,6 +87,12 @@ public:
             return fGenID != 0 || fPlotIndex != 0 || fPageIndex != 0;
         }
 
+        void makeInvalid() {
+            fGenID = 0;
+            fPlotIndex = 0;
+            fPageIndex = 0;
+        }
+
         bool operator==(const PlotLocator& other) const {
             return fGenID == other.fGenID &&
                    fPlotIndex == other.fPlotIndex &&
@@ -107,6 +113,8 @@ public:
     class AtlasLocator {
     public:
         std::array<uint16_t, 4> getUVs() const;
+
+        void invalidatePlotLocator() { fPlotLocator.makeInvalid(); }
 
         // TODO: Remove the small path renderer's use of this for eviction
         PlotLocator plotLocator() const { return fPlotLocator; }
