b6cafd97a92cfb9022b23679faa64bf4162e6334
Robert Phillips <robertphillips@google.com>
2020-08-07 14:55:10 -0400

    Split the small path renderer into record-time and flush-time pieces
    For the small path renderer to work w/ DDLs the actual allocation of
    the atlases and the positioning of paths in the atlas has to happen
    at flush time. This change makes the small path renderer more like the
    text rendering system.
    
    Change-Id: Id54b8b27b7684fc9bd6e6e2c933fba51e7d35174

--- src/gpu/ops/GrSmallPathRenderer.h
+++ src/gpu/ops/GrSmallPathRenderer.h
@@ -8,60 +8,25 @@
 #ifndef GrSmallPathRenderer_DEFINED
 #define GrSmallPathRenderer_DEFINED
 
-#include "src/gpu/GrDrawOpAtlas.h"
-#include "src/gpu/GrOnFlushResourceProvider.h"
 #include "src/gpu/GrPathRenderer.h"
-#include "src/gpu/geometry/GrRect.h"
-#include "src/gpu/geometry/GrStyledShape.h"
 
-#include "src/core/SkTDynamicHash.h"
-
+class GrDrawOp;
 class GrRecordingContext;
-class GrSmallPathShapeData;
-class GrSmallPathShapeDataKey;
+class GrStyledShape;
 
-class GrSmallPathRenderer : public GrPathRenderer,
-                            public GrOnFlushCallbackObject,
-                            public GrDrawOpAtlas::EvictionCallback,
-                            public GrDrawOpAtlas::GenerationCounter {
+class GrSmallPathRenderer : public GrPathRenderer {
 public:
     GrSmallPathRenderer();
     ~GrSmallPathRenderer() override;
 
     const char* name() const final { return "Small"; }
 
-    // GrOnFlushCallbackObject overrides
-    //
-    // Note: because this class is associated with a path renderer we want it to be removed from
-    // the list of active OnFlushBackkbackObjects in an freeGpuResources call (i.e., we accept the
-    // default retainOnFreeGpuResources implementation).
-
-    void preFlush(GrOnFlushResourceProvider* onFlushRP, const uint32_t*, int) override {
-        if (fAtlas) {
-            fAtlas->instantiate(onFlushRP);
-        }
-    }
-
-    void postFlush(GrDeferredUploadToken startTokenForNextFlush,
-                   const uint32_t* /*opsTaskIDs*/, int /*numOpsTaskIDs*/) override {
-        if (fAtlas) {
-            fAtlas->compact(startTokenForNextFlush);
-        }
-    }
-
-    using ShapeCache = SkTDynamicHash<GrSmallPathShapeData, GrSmallPathShapeDataKey>;
-    typedef SkTInternalLList<GrSmallPathShapeData> ShapeDataList;
-
     static std::unique_ptr<GrDrawOp> createOp_TestingOnly(GrRecordingContext*,
                                                           GrPaint&&,
                                                           const GrStyledShape&,
                                                           const SkMatrix& viewMatrix,
-                                                          GrDrawOpAtlas* atlas,
-                                                          ShapeCache*,
-                                                          ShapeDataList*,
                                                           bool gammaCorrect,
                                                           const GrUserStencilSettings*);
-    struct PathTestStruct;
 
 private:
     class SmallPathOp;
@@ -73,12 +38,6 @@ private:
     CanDrawPath onCanDrawPath(const CanDrawPathArgs&) const override;
 
     bool onDrawPath(const DrawPathArgs&) override;
-
-    void evict(GrDrawOpAtlas::PlotLocator) override;
-
-    std::unique_ptr<GrDrawOpAtlas> fAtlas;
-    ShapeCache fShapeCache;
-    ShapeDataList fShapeList;
 
     typedef GrPathRenderer INHERITED;
 };
