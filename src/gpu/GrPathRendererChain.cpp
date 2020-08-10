ec832b67bc14c2c655d2b97dc90c1eb9a1da2319
Robert Phillips <robertphillips@google.com>
2020-08-10 09:02:56 -0400

    Depower
    Change-Id: Ie6a2196a640ce9be7a1e1c6d7bbdfd41413c42d0

--- src/gpu/GrPathRendererChain.cpp
+++ src/gpu/GrPathRendererChain.cpp
@@ -16,6 +16,7 @@
 #include "src/gpu/GrRecordingContextPriv.h"
 #include "src/gpu/GrShaderCaps.h"
 #include "src/gpu/ccpr/GrCoverageCountingPathRenderer.h"
+#include "src/gpu/geometry/GrStyledShape.h"
 #include "src/gpu/ops/GrAAConvexPathRenderer.h"
 #include "src/gpu/ops/GrAAHairLinePathRenderer.h"
 #include "src/gpu/ops/GrAALinearizingConvexPathRenderer.h"
@@ -59,7 +60,7 @@ GrPathRendererChain::GrPathRendererChain(GrRecordingContext* context, const Opti
     }
     if (options.fGpuPathRenderers & GpuPathRenderers::kSmall) {
         auto spr = sk_make_sp<GrSmallPathRenderer>();
-        context->priv().addOnFlushCallbackObject(spr.get());
+        spr->addToOnFlushCallbacks(context);
         fChain.push_back(std::move(spr));
     }
     if (options.fGpuPathRenderers & GpuPathRenderers::kStencilAndCover) {
