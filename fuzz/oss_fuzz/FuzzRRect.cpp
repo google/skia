4d7ad7e6b9b06cb36ff71ade22d67fe7e7fe08af
Mike Klein <mtklein@google.com>
2020-08-11 12:08:55 -0500

    add SkRRect::readFromMemory() fuzzer
    Change-Id: I2102056847efe9e56ba6e8150e0d451bffc8f204

---
+++ fuzz/oss_fuzz/FuzzRRect.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020 Google, LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/core/SkRRect.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* buf, size_t size) {
+    SkRRect rrect;
+    (void)rrect.readFromMemory(buf, size);
+    return 0;
+}
