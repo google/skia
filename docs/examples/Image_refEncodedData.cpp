1ab356a8afd7d273a76d1ac1fa810750d63bc156
Robert Phillips <robertphillips@google.com>
2020-08-13 14:43:50 -0400

    Update more example code to GrDirectContext/GrRecordingContext pair
    Change-Id: Ic12b8292a09be097e99d2dc912d6d5188e645fbd

--- docs/examples/Image_refEncodedData.cpp
+++ docs/examples/Image_refEncodedData.cpp
@@ -1,28 +1,36 @@
-#if 0  // Disabled until updated to use current API.
 // Copyright 2019 Google LLC.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 #include "tools/fiddle/examples.h"
 // HASH=80856fe921ce36f8d5a32d8672bccbfc
 REG_FIDDLE(Image_refEncodedData, 256, 256, false, 3) {
 void draw(SkCanvas* canvas) {
+    auto dContext = GrAsDirectContext(canvas->recordingContext());
+    if (!dContext) {
+        return;
+    }
+
     struct {
         const char* name;
         sk_sp<SkImage> image;
-    } tests[] = { { "image", image }, { "bitmap", SkImage::MakeFromBitmap(source) },
-          { "texture", SkImage::MakeFromTexture(canvas->getGrContext(), backEndTexture,
-                            kTopLeft_GrSurfaceOrigin, kRGBA_8888_SkColorType,
-                            kOpaque_SkAlphaType, nullptr) } };
+    } tests[] = {
+        { "image", image },
+        { "bitmap", SkImage::MakeFromBitmap(source) },
+        { "texture", SkImage::MakeFromTexture(dContext, backEndTexture, kTopLeft_GrSurfaceOrigin,
+                                              kRGBA_8888_SkColorType, kOpaque_SkAlphaType,
+                                              nullptr) }
+    };
     SkString string;
     SkPaint paint;
+    SkFont font;
+
     for (const auto& test : tests ) {
         if (!test.image) {
             string.printf("no %s", test.name);
         } else {
             string.printf("%s" "encoded %s", test.image->refEncodedData() ? "" : "no ", test.name);
         }
-        canvas->drawString(string, 10, 20, paint);
+        canvas->drawString(string, 10, 20, font, paint);
         canvas->translate(0, 20);
     }
 }
 }  // END FIDDLE
-#endif  // Disabled until updated to use current API.
