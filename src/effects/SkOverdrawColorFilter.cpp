a92d83a90ab5f4f43c8ed53f68bc2f03a77d9f9f
Brian Osman <brianosman@google.com>
2020-08-13 16:59:48 -0400

    SkRuntimeEffect SkSL has a new signature for main()
    There is no more 'inout half4 color'. Effects return their output color.
    If an effect wants the input color, it must use the (already existing)
    approach of sampling a nullptr input shader.
    
    The change is guarded for Chromium (so we can update their runtime color
    filters in skia_renderer.cc).
    
    For the GPU backend, FPs can now override usesExplicitReturn to indicate
    that their emitCode will generate a return statement. If that's true,
    then writeProcessorFunction doesn't inject the automatic return of the
    output color, and emitFragProc will *always* wrap that FP in a helper
    function, even as a top-level FP. GrSkSLFP opts in to this behavior, so
    that the user-supplied return becomes the actual return in the FP's
    emitCode.
    
    Adapting the skvm code to this wasn't too bad: It looks fragile (what
    happens if there are multiple returns?), but that's not really possible
    today, without varying control flow.
    
    Bug: skia:10613
    
    Change-Id: I205b81fd87dd32bab30b6d6d5fc78853485da036

--- src/effects/SkOverdrawColorFilter.cpp
+++ src/effects/SkOverdrawColorFilter.cpp
@@ -18,14 +18,17 @@ sk_sp<SkColorFilter> SkOverdrawColorFilter::MakeWithSkColors(const SkColor color
         uniform half4 color3;
         uniform half4 color4;
         uniform half4 color5;
+        in shader input;
 
-        void main(inout half4 color) {
+        half4 main() {
+            half4 color = sample(input);
             half alpha = 255.0 * color.a;
             color = alpha < 0.5 ? color0
                   : alpha < 1.5 ? color1
                   : alpha < 2.5 ? color2
                   : alpha < 3.5 ? color3
                   : alpha < 4.5 ? color4 : color5;
+            return color;
         }
     )"));
     if (effect) {
@@ -34,7 +37,8 @@ sk_sp<SkColorFilter> SkOverdrawColorFilter::MakeWithSkColors(const SkColor color
         for (int i = 0; i < kNumColors; ++i) {
             premul[i] = SkColor4f::FromColor(colors[i]).premul();
         }
-        return effect->makeColorFilter(std::move(data));
+        sk_sp<SkColorFilter> input = nullptr;
+        return effect->makeColorFilter(std::move(data), &input, 1);
     }
     return nullptr;
 }
