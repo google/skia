d7d7d153ce6b8e7c1472e4446995053181e50734
Robert Phillips <robertphillips@google.com>
2020-08-13 14:56:33 -0400

    Update more example code to GrDirectContext/GrRecordingContext pair
    Change-Id: Ic12b8292a09be097e99d2dc912d6d5188e645fbd

--- docs/examples/Image_MakeFromTexture.cpp
+++ docs/examples/Image_MakeFromTexture.cpp
@@ -6,15 +6,17 @@ REG_FIDDLE(Image_MakeFromTexture, 256, 128, false, 3) {
 void draw(SkCanvas* canvas) {
     auto dContext = GrAsDirectContext(canvas->recordingContext());
     if (!dContext) {
-       return;
+        return;
     }
+
     canvas->scale(.25f, .25f);
     int x = 0;
     for (auto origin : { kBottomLeft_GrSurfaceOrigin, kTopLeft_GrSurfaceOrigin } ) {
         sk_sp<SkImage> image = SkImage::MakeFromTexture(dContext, backEndTexture,
-               origin, kRGBA_8888_SkColorType, kOpaque_SkAlphaType, nullptr);
+                                                        origin, kRGBA_8888_SkColorType,
+                                                        kOpaque_SkAlphaType, nullptr);
         canvas->drawImage(image, x, 0);
-    x += 512;
+        x += 512;
     }
 }
 }  // END FIDDLE
