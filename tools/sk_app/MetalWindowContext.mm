a496e3ff07a7da40d0c0ed2664e2fc93b407e38a
Jim Van Verth <jvanverth@google.com>
2020-08-14 11:42:35 -0400

    Fix MetalWindowContext::swapBuffers.
    It looks like we were expecting the drawable to be retained outside
    of swapBuffers(), which isn't always the case. Move the release
    to the end of the method to retain locally until we're done with it.
    
    Bug: skia:10597
    Change-Id: Ieff6a3add0554b5b221700aff88fefd7c8502f41
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/309724
    Reviewed-by: Adlai Holler <adlai@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

--- tools/sk_app/MetalWindowContext.mm
+++ tools/sk_app/MetalWindowContext.mm
@@ -88,15 +88,16 @@ sk_sp<SkSurface> MetalWindowContext::getBackbufferSurface() {
 }
 
 void MetalWindowContext::swapBuffers() {
-    // ARC is off in sk_app, so we need to release the CF ref manually
     id<CAMetalDrawable> currentDrawable = (id<CAMetalDrawable>)fDrawableHandle;
-    CFRelease(fDrawableHandle);
 
     id<MTLCommandBuffer> commandBuffer = [fQueue commandBuffer];
     commandBuffer.label = @"Present";
 
     [commandBuffer presentDrawable:currentDrawable];
     [commandBuffer commit];
+    // ARC is off in sk_app, so we need to release the CF ref manually
+    CFRelease(fDrawableHandle);
+    fDrawableHandle = nil;
 }
 
 void MetalWindowContext::setDisplayParams(const DisplayParams& params) {
