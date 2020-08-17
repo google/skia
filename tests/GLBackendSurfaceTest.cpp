229322a28cd49e252e1d3be37b72a61b49c6a1e8
Robert Phillips <robertphillips@google.com>
2020-08-17 11:27:40 -0400

    Remove some (presumably) unneeded calls to textureParamsModified in GrGLGpu
    Just wrapping the backend texture shouldn't invalidate any parameters.
    
    Change-Id: I02859e519b2c8e9612533b2230da753d5fff97b7

--- tests/GLBackendSurfaceTest.cpp
+++ tests/GLBackendSurfaceTest.cpp
@@ -48,6 +48,7 @@ static bool params_valid(const GrGLTextureParameters& parameters, const GrGLCaps
 
 DEF_GPUTEST_FOR_ALL_GL_CONTEXTS(GLTextureParameters, reporter, ctxInfo) {
     auto dContext = ctxInfo.directContext();
+    auto caps = static_cast<const GrGLCaps*>(dContext->priv().caps());
 
     GrBackendTexture backendTex = dContext->createBackendTexture(
             1, 1, kRGBA_8888_SkColorType, GrMipmapped::kNo, GrRenderable::kNo, GrProtected::kNo);
@@ -76,19 +77,10 @@ DEF_GPUTEST_FOR_ALL_GL_CONTEXTS(GLTextureParameters, reporter, ctxInfo) {
     GrGLTextureParameters::NonsamplerState invalidNSState;
     invalidNSState.invalidate();
 
-    // After wrapping we should assume the client's texture can be in any state.
-    REPORTER_ASSERT(reporter, params_invalid(*parameters));
-
     auto surf = SkSurface::MakeRenderTarget(
             dContext, SkBudgeted::kYes,
             SkImageInfo::Make(1, 1, kRGBA_8888_SkColorType, kPremul_SkAlphaType), 1, nullptr);
     REPORTER_ASSERT(reporter, surf);
-    surf->getCanvas()->drawImage(wrappedImage, 0, 0);
-    surf->flushAndSubmit();
-
-    auto caps = static_cast<const GrGLCaps*>(dContext->priv().caps());
-    // Now the texture should be in a known state.
-    REPORTER_ASSERT(reporter, params_valid(*parameters, caps));
 
     // Test invalidating from the GL backend texture.
     backendTex.glTextureParametersModified();
@@ -104,8 +96,8 @@ DEF_GPUTEST_FOR_ALL_GL_CONTEXTS(GLTextureParameters, reporter, ctxInfo) {
     REPORTER_ASSERT(reporter, params_invalid(*parameters));
 
     // Check that we can do things like assigning the backend texture to invalid one, assign an
-    // invalid one, assin a backend texture to inself etc. Success here is that we don't hit any of
-    // our ref counting asserts.
+    // invalid one, assign a backend texture to itself etc. Success here is that we don't hit any
+    // of our ref counting asserts.
     REPORTER_ASSERT(reporter, GrBackendTexture::TestingOnly_Equals(backendTex, backendTexCopy));
 
     GrBackendTexture invalidTexture;
