8454e893063fbe2dedc340110199a2dfa27e3971
Kevin Lubick <kjlubick@google.com>
2020-08-19 14:30:58 -0400

    [canvaskit] Remove fringe EGL dependency.
    This removes about 5 kb of wasm code size (2 kb gzipped),
    20 kb of js code size (4kb gzipped).
    I think I can remove getproc (which is a somewhat large
    function made up of a lot of string data) with some clever
    adjustments of macros in a follow-on CL.
    
    Change-Id: If3a4b30681e13abddea8e84d62297e90316ed7cf

---
+++ src/gpu/gl/webgl/GrGLMakeNativeInterface_webgl.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "include/gpu/gl/GrGLAssembleInterface.h"
+#include "include/gpu/gl/GrGLInterface.h"
+#include "src/gpu/gl/GrGLUtil.h"
+
+#include "emscripten/html5.h"
+
+static GrGLFuncPtr webgl_get_gl_proc(void* ctx, const char name[]) {
+    SkASSERT(nullptr == ctx);
+
+    return (GrGLFuncPtr) emscripten_webgl_get_proc_address(name);
+}
+
+sk_sp<const GrGLInterface> GrGLMakeNativeInterface() {
+    return GrGLMakeAssembledInterface(nullptr, webgl_get_gl_proc);
+}
+
+const GrGLInterface* GrGLCreateNativeInterface() { return GrGLMakeNativeInterface().release(); }
