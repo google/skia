d6917f8b544229db333423b845d563eda939b63e
Mike Klein <mtklein@google.com>
2020-08-19 08:49:01 -0500

    Reland "remove sksl interpreter guards"
    This is a reland of 7f1117e88611a9df157864884bb55040aa65867d
    
    Original change's description:
    > remove sksl interpreter guards
    > 
    > This is only used by particles, benchmarks, and tests,
    > and should be swept away by dead code elimination otherwise.
    > 
    > Change-Id: I10462d6ae0a08dd8219fc49325160ec6790632af
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/311759
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Commit-Queue: Mike Klein <mtklein@google.com>
    
    Change-Id: I066ebc66537db104175fc8f1feaeba6079e57173

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -1815,7 +1815,6 @@ bool Compiler::toPipelineStage(Program& program, PipelineStageArgs* outArgs) {
 #endif
 
 std::unique_ptr<ByteCode> Compiler::toByteCode(Program& program) {
-#if defined(SK_ENABLE_SKSL_INTERPRETER)
     if (!this->optimize(program)) {
         return nullptr;
     }
@@ -1827,9 +1826,6 @@ std::unique_ptr<ByteCode> Compiler::toByteCode(Program& program) {
     if (success) {
         return result;
     }
-#else
-    ABORT("ByteCode interpreter not enabled");
-#endif
     return nullptr;
 }
 
