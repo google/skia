397f4f12242b9513cce5e112dcaa08bc08a8ed96
Brian Salomon <bsalomon@google.com>
2020-08-22 14:43:35 -0400

    A new way to specify YUVA planar data from SkCodec to SkImage_Lazy
    Tunnels through SkImageGenerator as well.
    
    The new SkCodec interface doesn't assume three 8 bit planes.
    
    New SkYUVASpec more clearly defines chroma subsampling and siting of
    the planes.
    
    The intent is to use this for other YUVA APIs as well, in particular
    SkImage factories in the future.
    
    In this change we convert to the SkYUVASpec to SkYUVASizeInfo
    and SkYUVAIndex[4] representation. But the intent is to use
    the SkYUVASpec representation throughout the pipeline once
    legacy APIs are removed.
    
    orientation GM is replicated to test a variety of chroma
    subsampling configs.
    
    Bug: skia:10632
    
    Change-Id: I3fad35752b87cac16c51b24824331f2ae7d458d3

--- src/codec/SkCodec.cpp
+++ src/codec/SkCodec.cpp
@@ -167,6 +167,35 @@ SkCodec::SkCodec(SkEncodedInfo&& info, XformFormat srcFormat, std::unique_ptr<Sk
 
 SkCodec::~SkCodec() {}
 
+bool SkCodec::queryYUVAInfo(SkYUVAInfo* yuvaInfo, SkColorType* colorTypes, size_t* rowBytes) const {
+    if (!yuvaInfo || !colorTypes || !rowBytes) {
+        return false;
+    }
+    if (!this->onGetYUVAInfo(yuvaInfo, colorTypes, rowBytes)) {
+        return false;
+    }
+    SkISize planeDimensions[SkYUVAInfo::kMaxPlanes];
+    int numPlanes = yuvaInfo->expectedPlaneDims(planeDimensions);
+    // Validate that the returned color types and row bytes are OK for the expected plane sizes.
+    for (int i = 0; i < numPlanes; ++i) {
+        SkImageInfo ii = SkImageInfo::Make(planeDimensions[i], colorTypes[i], kPremul_SkAlphaType);
+        if (!ii.validRowBytes(rowBytes[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+SkCodec::Result SkCodec::getYUVAPlanes(const SkPixmap planes[SkYUVAInfo::kMaxPlanes]) {
+    if (!planes) {
+        return kInvalidInput;
+    }
+    if (!this->rewindIfNeeded()) {
+        return kCouldNotRewind;
+    }
+    return this->onGetYUVAPlanes(planes);
+}
+
 bool SkCodec::conversionSupported(const SkImageInfo& dst, bool srcIsOpaque, bool needsColorXform) {
     if (!valid_alpha(dst.alphaType(), srcIsOpaque)) {
         return false;
