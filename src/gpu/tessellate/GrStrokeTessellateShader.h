ba00f05d8bf82909c24b7104a9dc80802c190a1a
Chris Dalton <csmartdalton@google.com>
2020-08-21 20:36:15 -0600

    Add "radial edges" to tessellated stroking
    Tessellated stroking works by creating stroke-width edges orthogonal
    to the curve at set locations and then connecting them with a
    triangle strip. In the past these edges have been spaced evenly in the
    parametric sense, but this does not work for areas of tight curvature
    like cusps. To work around this we would do expensive CPU work to chop
    curves around points of strong curvature and insert combinations of
    lines and round joins.
    
    This CL introduces a second set of orthogonal edges called "radial
    edges". While parametric edges are spaced evenly in the parametric
    sense, radial edges divide the curve's _rotation_ into even steps. The
    tessellation shader evaluates both sets of edges (parametric and
    radial) and sorts them into a single triangle strip. With this new
    combined set of edges we can draw any curve and the CPU never has to
    worry about curvature.
    
    Bug: skia:10419
    Change-Id: I49291cc2efdf0d26835abbe9a9d21643bce0b4da

--- src/gpu/tessellate/GrStrokeTessellateShader.h
+++ src/gpu/tessellate/GrStrokeTessellateShader.h
@@ -14,34 +14,53 @@
 
 class GrGLSLUniformHandler;
 
-// Tessellates a batch of stroke patches directly to the canvas. A patch is either a "cubic"
-// (single stroked bezier curve with butt caps) or a "join". A patch is defined by 5 points as
-// follows:
+// Tessellates a batch of stroke patches directly to the canvas.
 //
-//   P0..P3      : Represent the cubic control points.
-//   (P4.x == 0) : The patch is a cubic and the shader decides how many linear segments to produce.
-//   (P4.x < 0)  : The patch is still a cubic, but will be linearized into exactly |P4.x| segments.
-//   (P4.x == 1) : The patch is an outer bevel join.
-//   (P4.x == 2) : The patch is an outer miter join.
-//                 (NOTE: If miterLimitOrZero == 0, then miter join patches are illegal.)
-//   (P4.x == 3) : The patch is an outer round join.
-//   (P4.x == 4) : The patch is an inner and outer round join.
-//   P4.y        : Represents the stroke radius.
+// Current limitations: (These restrictions will hopefully all be lifted in the future.)
 //
-// If a patch is a join, P0 must equal P3, P1 must equal the control point coming into the junction,
-// and P2 must equal the control point going out. It's imperative that a junction's control points
-// match the control points of their neighbor cubics exactly, or the rasterization might not be
-// water tight. (Also note that if P1==P0 or P2==P3, the junction needs to be given its neighbor's
-// opposite cubic control point.)
+//     * A given curve must not have inflection points. Chop curves at inflection points on CPU
+//       before sending them down.
+//
+//     * A given curve must not rotate more than 180 degrees. Chop curves that rotate more than 180
+//       degrees at midtangent before sending them down.
+//
+//     * It is illegal for P1 and P2 to both be coincident with P0 or P3. If this is the case, send
+//       the curve [P0, P0, P3, P3] instead.
+//
+// Tessellated stroking works by creating stroke-width, orthogonal edges at set locations along the
+// curve and then connecting them with a triangle strip. These orthogonal edges come from two
+// different sets: "parametric edges" and "radial edges". Parametric edges are spaced evenly in the
+// parametric sense, and radial edges divide the curve's _rotation_ into even steps. The
+// tessellation shader evaluates both sets of edges and sorts them into a single triangle strip.
+// With this combined set of edges we can stroke any curve, regardless of curvature.
+//
+// A patch is either a "cubic" (single stroked bezier curve with butt caps) or a "join". A patch is
+// defined by 5 points as follows:
+//
+//   P0..P3           : Represent the cubic control points.
+//   (P4.x == 0)      : The patch is a normal cubic.
+//   (abs(P4.x) == 1) : The patch is a bevel join.
+//   (abs(P4.x) == 2) : The patch is a miter join.
+//                      (NOTE: If miterLimitOrZero == 0, then miter join patches are illegal.)
+//   (abs(P4.x) >= 3) : The patch is a round join.
+//   (P4.x < 0)       : The patch join is double sided. (Positive value joins only draw on the
+//                      outer side of their junction.)
+//   P4.y             : Represents the stroke radius.
+//
+// If a patch is a join, P0 must equal the control point coming into the junction, P1 and P2 must
+// equal the junction point, and P3 must equal the control point going out. It's imperative that a
+// junction's control points match the control points of their neighbor cubics exactly, or the
+// seaming might not be water tight. (Also note that if P1==P0 or P2==P3, the junction needs to be
+// given its neighbor's opposite cubic control point.)
 //
 // To use this shader, construct a GrProgramInfo with a primitiveType of "kPatches" and a
 // tessellationPatchVertexCount of 5.
 class GrStrokeTessellateShader : public GrPathShader {
 public:
-    constexpr static float kBevelJoinType = -1;
-    constexpr static float kMiterJoinType = -2;
-    constexpr static float kRoundJoinType = -3;
-    constexpr static float kInternalRoundJoinType = -4;
+    constexpr static float kStandardCubicType = 0;
+    constexpr static float kBevelJoinType = 1;
+    constexpr static float kMiterJoinType = 2;
+    constexpr static float kRoundJoinType = 3;
 
     constexpr static int kNumVerticesPerPatch = 5;
 
