2d8dfe76cfcd9901ae848c868260420b2d096443
Ravi Mistry <rmistry@google.com>
2020-08-25 15:22:34 -0400

    Debug RecreateSKPs bot upload
    Change-Id: I2755a005e937b6b5c7e0cd4be87ec36d0509949b

--- infra/bots/recipes/recreate_skps.py
+++ infra/bots/recipes/recreate_skps.py
@@ -59,8 +59,10 @@ def RunSteps(api):
     with api.context(env=gn_env):
       api.run(api.step, 'GN', cmd=[gn, 'gen', out_dir])
 
+    """
     # Build Chrome.
     api.run(api.step, 'Build Chrome', cmd=['ninja', '-C', out_dir, 'chrome'])
+    """
 
   # Clean up the output dir.
   output_dir = api.path['start_dir'].join('skp_output')
@@ -68,6 +70,7 @@ def RunSteps(api):
     api.run.rmtree(output_dir)
   api.file.ensure_directory('makedirs skp_output', output_dir)
 
+  """
   # Capture the SKPs.
   asset_dir = skia_dir.join('infra', 'bots', 'assets', 'skp')
   cmd = ['python', asset_dir.join('create.py'),
@@ -78,14 +81,17 @@ def RunSteps(api):
     cmd.append('--upload_to_partner_bucket')
   with api.context(cwd=skia_dir):
     api.run(api.step, 'Recreate SKPs', cmd=cmd)
+  """
 
   # Upload the SKPs.
-  if 'DryRun' not in api.properties['buildername']:
+  if 'DryRun' in api.properties['buildername']:
     cmd = ['python',
            skia_dir.join('infra', 'bots', 'upload_skps.py'),
            '--target_dir', output_dir,
            '--chromium_path', src_dir]
-    with api.context(cwd=skia_dir, env=api.infra.go_env):
+    upload_env = api.infra.go_env
+    upload_env['GCLIENT_PY3'] = 0
+    with api.context(cwd=skia_dir, env=upload_env):
       api.run(api.step, 'Upload SKPs', cmd=cmd)
 
 
