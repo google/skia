1c1de596b7de56d197e12075e97e9852c00a0f1a
John Stiles <johnstiles@google.com>
2020-08-25 15:46:21 -0400

    Allow .fp files to return a color from main().
    Existing .fp files that assign their output to sk_OutColor will continue
    to work as always. An error will be generated if an .fp file tries to
    mix-and-match return statements and sk_OutColor assignment; only one or
    the other is allowed within a single .fp file.
    
    Change-Id: I53bc0e8b79b2fdd64a020a7e5b35a905f9b05c94
    Bug: skia:10549

--- src/gpu/effects/generated/GrRRectBlurEffect.h
+++ src/gpu/effects/generated/GrRRectBlurEffect.h
@@ -31,6 +31,7 @@ public:
     GrRRectBlurEffect(const GrRRectBlurEffect& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
     const char* name() const override { return "RRectBlurEffect"; }
+    bool usesExplicitReturn() const override;
     float sigma;
     SkRect rect;
     float cornerRadius;
