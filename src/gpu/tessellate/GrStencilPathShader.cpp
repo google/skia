f9d5940fef55233addd492d6d26418437dc130aa
Chris Dalton <csmartdalton@google.com>
2020-08-26 08:39:45 -0600

    Use exp2 instead of ldexp/shifts in the middle out cubic shader
    Change-Id: I6cb55aa6df3b5477adf036eea34079ff2ec33488
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/313150
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Chris Dalton <csmartdalton@google.com>

--- src/gpu/tessellate/GrStencilPathShader.cpp
+++ src/gpu/tessellate/GrStencilPathShader.cpp
@@ -291,23 +291,18 @@ class GrMiddleOutCubicShader::Impl : public GrStencilPathShader::Impl {
     void onEmitCode(EmitArgs& args, GrGPArgs* gpArgs) override {
         const auto& shader = args.fGP.cast<GrMiddleOutCubicShader>();
         args.fVaryingHandler->emitAttributes(shader);
-        args.fVertBuilder->defineConstant("kMaxResolveLevel", kMaxResolveLevel);
+        args.fVertBuilder->defineConstantf("int", "kMaxVertexID", "%i", 1 << kMaxResolveLevel);
+        args.fVertBuilder->defineConstantf("float", "kInverseMaxVertexID", "exp2(-%i.0)",
+                                           kMaxResolveLevel);
         args.fVertBuilder->codeAppend(R"(
                 float4x2 P = float4x2(inputPoints_0_1, inputPoints_2_3);
                 float2 point;
-                if (sk_VertexID > (1 << kMaxResolveLevel)) {
+                if (sk_VertexID > kMaxVertexID) {
                     // This is a special index value that wants us to emit a specific point.
                     point = P[sk_VertexID & 3];
-                } else {)");
-        // Evaluate the cubic at T=(sk_VertexID / 2^kMaxResolveLevel).
-        if (args.fShaderCaps->fpManipulationSupport()) {
-            args.fVertBuilder->codeAppend(R"(
-                    float T = ldexp(sk_VertexID, -kMaxResolveLevel);)");
-        } else {
-            args.fVertBuilder->codeAppend(R"(
-                    float T = sk_VertexID / float(1 << kMaxResolveLevel);)");
-        }
-        args.fVertBuilder->codeAppend(R"(
+                } else {
+                    // Evaluate the cubic at T = (sk_VertexID / 2^kMaxResolveLevel).
+                    float T = sk_VertexID * kInverseMaxVertexID;
                     float2 ab = mix(P[0], P[1], T);
                     float2 bc = mix(P[1], P[2], T);
                     float2 cd = mix(P[2], P[3], T);
