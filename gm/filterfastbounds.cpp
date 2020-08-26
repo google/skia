a14387696494c01071b87687bbae19eaeef974f6
Mike Reed <reed@google.com>
2020-08-26 12:56:51 -0400

    Revert "Revert "use pathbuilder""
    This reverts commit 43e3e91883c8599f5e379d53da8b5895cc17c238.
    
    Change-Id: I43f6c195f96b1601225801ffaa05a3133c236eba

--- gm/filterfastbounds.cpp
+++ gm/filterfastbounds.cpp
@@ -65,14 +65,11 @@ static void draw_drrect(SkCanvas* canvas, const SkRect& r, const SkPaint& p) {
 }
 
 static void draw_path(SkCanvas* canvas, const SkRect& r, const SkPaint& p) {
-    SkPath path;
-
-    path.moveTo(r.fLeft, r.fTop);
-    path.lineTo(r.fLeft, r.fBottom);
-    path.lineTo(r.fRight, r.fBottom);
-    path.close();
-
-    canvas->drawPath(path, p);
+    canvas->drawPath(SkPath::Polygon({
+        {r.fLeft, r.fTop},
+        {r.fLeft, r.fBottom},
+        {r.fRight, r.fBottom},
+    }, true), p);
 }
 
 static void draw_points(SkCanvas* canvas, const SkRect& r, const SkPaint& p) {
