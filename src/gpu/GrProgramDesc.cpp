f21cc3de7be1d74fa25dba55dad69e77bd63661e
Brian Salomon <bsalomon@google.com>
2020-08-27 14:55:30 -0400

    Remove use of texture swizzle in GL backend.
    This has always been a potential source of a bug. If the same texture is
    used twice in a shader with different swizzles we would overwrite the
    swizzle for the first use by that of the second use since there is
    only one fixed function swizzle per texture. It's not part of the
    sampler state.
    
    We set the swizzle when it is a feature, but always to RGBA.
    
    Also, highly speculative that this may improve ANGLE D3D11 ES3
    performance compared to ES2.
    
    Bug: skia:10644
    Change-Id: I8877afc3043c5ddaafd26ea9f9bd372303328c71

--- src/gpu/GrProgramDesc.cpp
+++ src/gpu/GrProgramDesc.cpp
@@ -49,10 +49,7 @@ static uint32_t sampler_key(GrTextureType textureType, const GrSwizzle& swizzle,
     int samplerTypeKey = texture_type_key(textureType);
 
     static_assert(2 == sizeof(swizzle.asKey()));
-    uint16_t swizzleKey = 0;
-    if (caps.shaderCaps()->textureSwizzleAppliedInShader()) {
-        swizzleKey = swizzle.asKey();
-    }
+    uint16_t swizzleKey = swizzle.asKey();
     return SkToU32(samplerTypeKey | swizzleKey << kSamplerOrImageTypeKeyBits);
 }
 
