14878597ba0ac9c0fa96fb82ff6a82ac393ce0c3
Robert Phillips <robertphillips@google.com>
2020-09-03 09:53:18 -0400

    Create cpu-side blurred RRect mask when recording DDLs
    Bug: 1108408
    Change-Id: Ib66b04cb4ab7bddc08fa3857f7bc186948afb0b2

--- src/gpu/effects/GrGaussianConvolutionFragmentProcessor.cpp
+++ src/gpu/effects/GrGaussianConvolutionFragmentProcessor.cpp
@@ -104,7 +104,7 @@ void GrGaussianConvolutionFragmentProcessor::Impl::GenKey(const GrProcessor& pro
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static void fill_in_1D_gaussian_kernel(float* kernel, float gaussianSigma, int radius) {
+void SkFillIn1DGaussianKernel(float* kernel, float gaussianSigma, int radius) {
     const float twoSigmaSqrd = 2.0f * gaussianSigma * gaussianSigma;
     int width = radius_to_width(radius);
     if (SkScalarNearlyZero(twoSigmaSqrd, SK_ScalarNearlyZero)) {
@@ -171,7 +171,7 @@ GrGaussianConvolutionFragmentProcessor::GrGaussianConvolutionFragmentProcessor(
         , fDirection(direction) {
     this->registerChild(std::move(child), SkSL::SampleUsage::Explicit());
     SkASSERT(radius <= kMaxKernelRadius);
-    fill_in_1D_gaussian_kernel(fKernel, gaussianSigma, fRadius);
+    SkFillIn1DGaussianKernel(fKernel, gaussianSigma, fRadius);
     this->setUsesSampleCoordsDirectly();
 }
 
