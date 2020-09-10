83ae37403f4280646d897ec66926627920c066af
Robert Phillips <robertphillips@google.com>
2020-09-10 17:23:36 -0400

    Add a shared thread-safe uniquely-keyed proxy-view cache
    The DDL recorders need to share many uniquely keyed proxies between
    themselves and w/ the direct context. This CL adds the shared
    cache and uses it for blurred RRect masks.
    
    Bug: 1108408
    Change-Id: I1ec9477dffe870e5973f8a334a65b1013a4ca3dd

---
+++ src/gpu/GrThreadSafeUniquelyKeyedProxyViewCache.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/GrThreadSafeUniquelyKeyedProxyViewCache.h"
+
+#include "src/gpu/GrSurfaceProxyView.h"
+
+GrThreadSafeUniquelyKeyedProxyViewCache::GrThreadSafeUniquelyKeyedProxyViewCache() {}
+
+#ifdef SK_DEBUG
+int GrThreadSafeUniquelyKeyedProxyViewCache::numEntries() const {
+    SkAutoSpinlock lock{fSpinLock};
+
+    return fUniquelyKeyedProxyViews.count();
+}
+
+size_t GrThreadSafeUniquelyKeyedProxyViewCache::approxBytesUsed() const {
+    SkAutoSpinlock lock{fSpinLock};
+
+    return fUniquelyKeyedProxyViews.approxBytesUsed();
+}
+#endif
+
+void GrThreadSafeUniquelyKeyedProxyViewCache::dropAllRefs() {
+    SkAutoSpinlock lock{fSpinLock};
+
+    fUniquelyKeyedProxyViews.reset();
+}
+
+void GrThreadSafeUniquelyKeyedProxyViewCache::dropAllUniqueRefs() {
+    SkAutoSpinlock lock{fSpinLock};
+
+
+}
+
+GrSurfaceProxyView GrThreadSafeUniquelyKeyedProxyViewCache::find(const GrUniqueKey& key) {
+    SkAutoSpinlock lock{fSpinLock};
+
+    Entry** tmp = fUniquelyKeyedProxyViews.find(&key);
+    if (tmp) {
+        return (*tmp)->fView;
+    }
+
+    return {};
+}
+
+GrSurfaceProxyView GrThreadSafeUniquelyKeyedProxyViewCache::internalAdd(const GrUniqueKey& key,
+                                                                        const GrSurfaceProxyView& view) {
+    Entry** tmp = fUniquelyKeyedProxyViews.find(&key);
+    if (!tmp) {
+        Entry* newT = new Entry(key, view);
+        tmp = fUniquelyKeyedProxyViews.set(&newT->fKey, newT);
+    }
+
+    return (*tmp)->fView;
+}
+
+GrSurfaceProxyView GrThreadSafeUniquelyKeyedProxyViewCache::add(const GrUniqueKey& key,
+                                                                const GrSurfaceProxyView& view) {
+    SkAutoSpinlock lock{fSpinLock};
+
+    return this->internalAdd(key, view);
+}
