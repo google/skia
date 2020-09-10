6212c6786556579bc2804c436b4c2ad05c92dbef
Julia Lavrova <jlavrova@google.com>
2020-09-10 11:07:31 -0400

    An attempt to make font resolution more predictable.
    In case the default font family is not there.
    Bug: skia:10701
    
    Change-Id: Ie1155d820bfcc25870bedcd0310b0e779d55e942

--- modules/skparagraph/src/FontCollection.cpp
+++ modules/skparagraph/src/FontCollection.cpp
@@ -85,6 +85,9 @@ std::vector<sk_sp<SkTypeface>> FontCollection::findTypefaces(const std::vector<S
 
     if (typefaces.empty()) {
         sk_sp<SkTypeface> match = matchTypeface(fDefaultFamilyName, fontStyle);
+        if (!match) {
+            match = SkTypeface::MakeDefault();
+        }
         if (match) {
             typefaces.emplace_back(std::move(match));
         }
