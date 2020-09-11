e4383b8f1e4d608fd1d7a52ad6c56f319cefd798
Ben Wagner <bungeman@google.com>
2020-09-11 14:05:11 -0400

    Add SkFILEStream constructor which takes a size.
    Allows the user to limit the stream to a subset of the entire file.
    SkFILEStream uses the current FILE position as the base position for the
    stream; this allows the user to further limit the length of the stream
    to a range of bytes in the file.
    
    Change-Id: I36131ff19e75b65d501eabdfd9368f8fbb2e177f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/316357
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- include/core/SkStream.h
+++ include/core/SkStream.h
@@ -313,6 +313,13 @@ public:
      */
     explicit SkFILEStream(FILE* file);
 
+    /** Initialize the stream with an existing C FILE stream.
+     *  The current position of the C FILE stream will be considered the
+     *  beginning of the SkFILEStream and size bytes later will be the end.
+     *  The C FILE stream will be closed in the destructor.
+     */
+    explicit SkFILEStream(FILE* file, size_t size);
+
     ~SkFILEStream() override;
 
     static std::unique_ptr<SkFILEStream> Make(const char path[]) {
@@ -345,6 +352,7 @@ public:
     size_t getLength() const override;
 
 private:
+    explicit SkFILEStream(FILE*, size_t size, size_t start);
     explicit SkFILEStream(std::shared_ptr<FILE>, size_t end, size_t start);
     explicit SkFILEStream(std::shared_ptr<FILE>, size_t end, size_t start, size_t current);
 
