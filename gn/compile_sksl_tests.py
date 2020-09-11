65ee87ac77ce8f5e5236a9431aa108caeec41b36
John Stiles <johnstiles@google.com>
2020-09-11 16:19:55 -0400

    Support testing error cases in our SkSL unit test goldens.
    Change-Id: I56e19153597e2c4393c5821314b82937828c0d50

--- gn/compile_sksl_tests.py
+++ gn/compile_sksl_tests.py
@@ -9,30 +9,32 @@ import os
 import subprocess
 import sys
 
+def compile(skslc, input, target, extension):
+    target += extension
+    try:
+        subprocess.check_output([skslc, input, target], stderr=subprocess.STDOUT)
+
+    except subprocess.CalledProcessError as err:
+        dst = open(target, 'wb')
+        dst.write("### Compilation failed\n\n")
+        dst.write(err.output)
+        dst.close()
+
 skslc = sys.argv[1]
 inputs = sys.argv[2:]
 
 for input in inputs:
     print("Recompiling " + input + "...")
-    try:
-        noExt, ext = os.path.splitext(input)
-        head, tail = os.path.split(noExt)
-        targetDir = os.path.join(head, "golden")
-        if not os.path.isdir(targetDir):
-            os.mkdir(targetDir)
-        target = os.path.join(targetDir, tail)
-        if ext == ".fp":
-            subprocess.check_output([skslc, input, target + ".h"],
-                                    stderr=subprocess.STDOUT)
-            subprocess.check_output([skslc, input, target + ".cpp"],
-                                    stderr=subprocess.STDOUT)
-        elif ext == ".sksl":
-            subprocess.check_output([skslc, input, target + ".glsl"],
-                                    stderr=subprocess.STDOUT)
-        else:
-            print("### Unrecognized file type for " + input + ", skipped")
-
-    except subprocess.CalledProcessError as err:
-        print("### Error compiling " + input + ":")
-        print(err.output)
-        exit(1)
+    noExt, ext = os.path.splitext(input)
+    head, tail = os.path.split(noExt)
+    targetDir = os.path.join(head, "golden")
+    if not os.path.isdir(targetDir):
+        os.mkdir(targetDir)
+    target = os.path.join(targetDir, tail)
+    if ext == ".fp":
+        compile(skslc, input, target, ".h")
+        compile(skslc, input, target, ".cpp")
+    elif ext == ".sksl":
+        compile(skslc, input, target, ".glsl")
+    else:
+        print("### Unrecognized file type for " + input + ", skipped")
