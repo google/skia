d8dfd7a224d9a93c39028b3e553b91528bf288d7
Adlai Holler <adlai@google.com>
2020-09-11 16:01:09 -0400

    Elevate caps methods to GrRecordingContext
    This partially unblocks Chrome from migrating from
    SkCanvas-getGrContext to SkCanvas-recordingContext.
    
    Change-Id: I1100387497ba8b482005bb1d147e9768590afe95
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/316449
    Commit-Queue: Adlai Holler <adlai@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Adlai Holler <adlai@google.com>

--- include/gpu/GrRecordingContext.h
+++ include/gpu/GrRecordingContext.h
@@ -64,6 +64,21 @@ public:
     }
 
     /**
+     * Gets the maximum supported texture size.
+     */
+    SK_API int maxTextureSize() const;
+
+    /**
+     * Gets the maximum supported render target size.
+     */
+    SK_API int maxRenderTargetSize() const;
+
+    /**
+     * Can a SkImage be created with the given color type.
+     */
+    SK_API bool colorTypeSupportedAsImage(SkColorType) const;
+
+    /**
      * Gets the maximum supported sample count for a color type. 1 is returned if only non-MSAA
      * rendering is supported for the color type. 0 is returned if rendering to this color type
      * is not supported at all.
