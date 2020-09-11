f40c81682f339829bcea70607bac0cff8785ddf9
Kevin Lubick <kjlubick@google.com>
2020-09-11 13:08:40 -0400

    [fuzz] Standardize, document, and backport fuzzing defines.
    We had several defines around the code base that were not
    very descriptive. Additionally, we had a patch of extra
    runtime restrictions living in oss-fuzz that were applied
    when fuzzing over there for some fuzzers.
    
    This has all be consolidated and controlled via the defines
    documented in site/dev/testing/fuzz.md
    
    As such, we can remove one of the patches that is in oss-fuzz,
    taking us closer to being able to fuzz in the CI/CQ.
    
    PS 1 renames existing fuzz defines to the new schema.
    PS 2-3 backports skia.diff from oss-fuzz and changes those
    definitions to have the _GREATLY modifier.
    
    Change-Id: Ia4ad96f30c1e9620a2123b510e97c6f501a2e257
    Docs-Preview: https://skia.org/?cl=316443
    Bug: skia:10713

--- src/ports/SkDebug_stdio.cpp
+++ src/ports/SkDebug_stdio.cpp
@@ -11,10 +11,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#if defined(SK_INCLUDE_LIBFUZZER_ENTRYPOINTS)
+  // libfuzzer output is hard to read with excess logging.
+  #define SkDebugf(...) (void)
+#else
 void SkDebugf(const char format[], ...) {
+
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
     va_end(args);
 }
+#endif//defined(SK_INCLUDE_LIBFUZZER_ENTRYPOINTS)
+
 #endif//!defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
