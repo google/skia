34cb5752fd6d23a50eb041735477243d0ba284ef
John Stiles <johnstiles@google.com>
2020-09-14 12:38:05 -0400

    Support testing error cases in our SkSL unit test goldens.
    Change-Id: I56e19153597e2c4393c5821314b82937828c0d50
    Bug: skia:10694

---
+++ tests/sksl/errors/golden/DuffsDevice.glsl
@@ -0,0 +1,4 @@
+### Compilation failed
+
+error: 5: expected expression, but found 'case'
+1 error
---
+++ tests/sksl/errors/golden/OpenArray.glsl
@@ -0,0 +1,8 @@
+### Compilation failed:
+
+void main(inout float4 color) { color.r[ = ( color.g ); }
+
+
+### Errors:
+error: 1: expected expression, but found '='
+1 error
