e6f1bbadf01a3d1f1d19707ecd80710b6ccd5d97
Ethan Nicholas <ethannicholas@google.com>
2020-09-14 13:22:21 -0400

    Renamed SkSLCompiler::is_constant and made its behavior consistent with IRGenerator
    IRGenerator's getConstantInt handles const variables, but is_constant
    didn't, and this was causing a (fixed in a prior CL) assertion failure
    in switch case handling. This CL renames the function to make its intent
    more clear, fixes the logic, and adds some more tests to exercise it.
    The tests caught a minor missed optimization in PrefixExpression, which
    is also fixed as part of this CL.
    
    Change-Id: I0894bc874d8ab1f7f6be1f80618124b9138815a1

--- src/sksl/ir/SkSLPrefixExpression.h
+++ src/sksl/ir/SkSLPrefixExpression.h
@@ -46,6 +46,11 @@ struct PrefixExpression : public Expression {
                                                              irGenerator.fContext,
                                                              fOffset,
                                                              -fOperand->as<FloatLiteral>().fValue));
+        }
+        if (fOperand->kind() == Expression::Kind::kIntLiteral) {
+            return std::unique_ptr<Expression>(new IntLiteral(irGenerator.fContext,
+                                                              fOffset,
+                                                              -fOperand->as<IntLiteral>().fValue));
 
         }
         return nullptr;
