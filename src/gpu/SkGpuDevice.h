25967b98affbccf035e51e1b52ee8e9ff29f87bf
Michael Ludwig <michaelludwig@google.com>
2020-09-15 17:43:19 -0400

    Enable novel GrClipStack on bots, disable elsewhere
    As a result of this change, the new GrClipStack will run on all of our
    default bots. However, the SK_DISABLE_NEW_GR_CLICK_STACK define is used
    to explicitly disable the clip stack when we build for the Android
    Framework, Google3, Flutter, and Fuchsia. These projects can have staging
    controlled from within the skia repo. This CL in chromium also disables
    the new clip stack: https://chromium-review.googlesource.com/c/chromium/src/+/2412768
    and must land before this CL does in Skia.
    
    When GrClipStack originally landed, I had it disabled by checking the
    value of the define SK_USE_NEW_GR_CLICK_STACK for 0 or 1. To be a little
    simpler, and work with the flutter and fuchsia gn defines, this CL
    switches GrClipStack control over to SK_DISABLE_NEW_GR_CLICK_STACK and
    it just checks for whether or not it's defined.
    
    Bug: skia:10205
    Change-Id: I6b8bd18290844c02839fe99fdf629b48ffd86f27

--- src/gpu/SkGpuDevice.h
+++ src/gpu/SkGpuDevice.h
@@ -25,14 +25,17 @@ class SkSpecialImage;
 class SkSurface;
 class SkVertices;
 
-#ifndef SK_USE_NEW_GR_CLIP_STACK
-// NOTE: If this is non-zero, SkGpuDevice extends SkBaseDevice directly and manages its clip stack
+// NOTE: when not defined, SkGpuDevice extends SkBaseDevice directly and manages its clip stack
 // using GrClipStack. When false, SkGpuDevice continues to extend SkClipStackDevice and uses
 // SkClipStack and GrClipStackClip to manage the clip stack.
-#define SK_USE_NEW_GR_CLIP_STACK 0
+#if !defined(SK_DISABLE_NEW_GR_CLIP_STACK)
+    // For staging purposes, disable this for Android Framework and Google3
+    #if defined(SK_BUILD_FOR_ANDROID_FRAMEWORK) || defined(SK_BUILD_FOR_GOOGLE3)
+        #define SK_DISABLE_NEW_GR_CLIP_STACK
+    #endif
 #endif
 
-#if SK_USE_NEW_GR_CLIP_STACK
+#if !defined(SK_DISABLE_NEW_GR_CLIP_STACK)
     #include "src/core/SkDevice.h"
     #include "src/gpu/GrClipStack.h"
     #define BASE_DEVICE   SkBaseDevice
@@ -143,7 +146,7 @@ protected:
     bool onReadPixels(const SkPixmap&, int, int) override;
     bool onWritePixels(const SkPixmap&, int, int) override;
 
-#if SK_USE_NEW_GR_CLIP_STACK
+#if !defined(SK_DISABLE_NEW_GR_CLIP_STACK)
     void onSave() override { fClip.save(); }
     void onRestore() override { fClip.restore(); }
 
