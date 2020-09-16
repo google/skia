20039a044a585990cced5917a19d748a515d0535
Brian Osman <brianosman@google.com>
2020-09-16 16:21:40 -0400

    SkSL: Only allow bitwise ops on integral types
    We were allowing these ops on floating point types. The resulting GLSL
    would fail to compile. We also allowed >>= and <<= on vectors (but not
    any of the other bitwise ops).
    
    The newly added unit tests were failing (eg, not catching errors). New
    results are correct.
    
    Bug: skia:10707
    Change-Id: I97b769f1ce59261361109a71061b42dc8ef3c74b

---
+++ tests/sksl/errors/golden/BitShiftFloatMatrix.glsl
@@ -0,0 +1,13 @@
+### Compilation failed:
+
+error: 5: type mismatch: '>>=' cannot operate on 'float2x2', 'int'
+error: 6: type mismatch: '<<=' cannot operate on 'float2x2', 'int'
+error: 7: type mismatch: '&=' cannot operate on 'float2x2', 'int'
+error: 8: type mismatch: '|=' cannot operate on 'float2x2', 'int'
+error: 9: type mismatch: '^=' cannot operate on 'float2x2', 'int'
+error: 11: type mismatch: '>>' cannot operate on 'float2x2', 'int'
+error: 12: type mismatch: '<<' cannot operate on 'float2x2', 'int'
+error: 13: type mismatch: '&' cannot operate on 'float2x2', 'int'
+error: 14: type mismatch: '|' cannot operate on 'float2x2', 'int'
+error: 15: type mismatch: '^' cannot operate on 'float2x2', 'int'
+10 errors
