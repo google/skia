7916d78b321e888e2d4cca12adf30bd3008a277d
John Stiles <johnstiles@google.com>
2020-09-16 18:35:39 -0400

    Migrate several SkSLFP tests to golden outputs.
    Change-Id: I83a38f2c953a560fea3483e95e31df532b90773e

---
+++ tests/sksl/errors/golden/GrNoFragmentProcessorParams.cpp
@@ -0,0 +1,5 @@
+### Compilation failed:
+
+error: 3: parameters of type 'fragmentProcessor' not allowed
+error: 6: unknown identifier 'helper'
+2 errors
---
+++ tests/sksl/fp/GrKeyIn.fp
@@ -0,0 +1,4 @@
+layout(key) in half4 color;
+void main() {
+    sk_OutColor = color;
+}
