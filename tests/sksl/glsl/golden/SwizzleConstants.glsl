6e49a372de44c586ff00a7b223d0f40c2a78fe7b
John Stiles <johnstiles@google.com>
2020-09-16 13:40:54 -0400

    Avoid redundant zeros and ones in swizzle constructor.
    Bug: skia:10721
    Change-Id: Ib9e51b736bafb6e4493db4f50e9835fbd86c415d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/317247
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/glsl/golden/SwizzleConstants.glsl
+++ tests/sksl/glsl/golden/SwizzleConstants.glsl
@@ -12,20 +12,20 @@ void main() {
     sk_FragColor = vec4(vec3(v.x, 1.0, 0.0), 1.0);
     sk_FragColor = vec4(vec3(v.yz, 1.0).zxy, 1.0);
     sk_FragColor = vec4(vec3(v.y, 0.0, 1.0).yxz, 1.0);
-    sk_FragColor = vec4(vec3(v.z, 1.0, 1.0).yzx, 1.0);
+    sk_FragColor = vec4(vec2(v.z, 1.0).yyx, 1.0);
     sk_FragColor = v;
     sk_FragColor = vec4(v.xyz, 1.0);
     sk_FragColor = vec4(v.xyw, 0.0).xywz;
     sk_FragColor = vec4(v.xy, 1.0, 0.0);
     sk_FragColor = vec4(v.xzw, 1.0).xwyz;
     sk_FragColor = vec4(v.xz, 0.0, 1.0).xzyw;
-    sk_FragColor = vec4(v.xw, 1.0, 1.0).xzwy;
-    sk_FragColor = vec4(v.x, 1.0, 0.0, 1.0);
+    sk_FragColor = vec3(v.xw, 1.0).xzzy;
+    sk_FragColor = vec3(v.x, 1.0, 0.0).xyzy;
     sk_FragColor = vec4(v.yzw, 1.0).wxyz;
     sk_FragColor = vec4(v.yz, 0.0, 1.0).zxyw;
     sk_FragColor = vec4(v.yw, 0.0, 1.0).zxwy;
-    sk_FragColor = vec4(v.y, 1.0, 1.0, 1.0).yxzw;
-    sk_FragColor = vec4(v.zw, 0.0, 0.0).zwxy;
-    sk_FragColor = vec4(v.z, 0.0, 0.0, 1.0).yzxw;
-    sk_FragColor = vec4(v.w, 0.0, 1.0, 1.0).yzwx;
+    sk_FragColor = vec2(v.y, 1.0).yxyy;
+    sk_FragColor = vec3(v.zw, 0.0).zzxy;
+    sk_FragColor = vec3(v.z, 0.0, 1.0).yyxz;
+    sk_FragColor = vec3(v.w, 0.0, 1.0).yzzx;
 }
