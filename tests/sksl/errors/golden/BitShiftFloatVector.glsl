2468aebd8cc508e69c3762d002b126685b1972c6
Brian Osman <brianosman@google.com>
2020-09-16 16:21:40 -0400

    Add SkSL unit tests for bitwise ops on floats
    These should not be allowed (and the resulting GLSL will
    fail to compile).
    
    Bug: skia:10707
    Change-Id: I97b769f1ce59261361109a71061b42dc8ef3c74b

---
+++ tests/sksl/errors/golden/BitShiftFloatVector.glsl
@@ -0,0 +1,11 @@
+### Compilation failed:
+
+error: 7: type mismatch: '&=' cannot operate on 'float2', 'int'
+error: 8: type mismatch: '|=' cannot operate on 'float2', 'int'
+error: 9: type mismatch: '^=' cannot operate on 'float2', 'int'
+error: 11: type mismatch: '>>' cannot operate on 'float2', 'int'
+error: 12: type mismatch: '<<' cannot operate on 'float2', 'int'
+error: 13: type mismatch: '&' cannot operate on 'float2', 'int'
+error: 14: type mismatch: '|' cannot operate on 'float2', 'int'
+error: 15: type mismatch: '^' cannot operate on 'float2', 'int'
+8 errors
