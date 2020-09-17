dfaf24d4af21c10dd05bc0c333fb9bdc0f40759b
Mike Reed <reed@google.com>
2020-09-16 22:28:24 -0400

    Refactor API for mipmap-builder
    A couple of API variants
    - builder.make(image)
    - image.make(builder)
    
    Pref?
    
    Change-Id: Ie56256390b96d3fdbe39f89784276947047df656

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -662,10 +662,6 @@ SkPixmap SkMipmapBuilder::level(int index) const {
     return pm;
 }
 
-sk_sp<SkMipmap> SkMipmapBuilder::detach() {
-    return std::move(fMM);
-}
-
 bool SkImage::hasMipmaps() const {
     return as_IB(this)->onPeekMips() != nullptr;
 }
@@ -681,4 +677,8 @@ sk_sp<SkImage> SkImage::withMipmaps(sk_sp<SkMipmap> mips) const {
 
 sk_sp<SkImage> SkImage::withDefaultMipmaps() const {
     return this->withMipmaps(nullptr);
+}
+
+sk_sp<SkImage> SkMipmapBuilder::attachTo(const SkImage* src) {
+    return src->withMipmaps(fMM);
 }
