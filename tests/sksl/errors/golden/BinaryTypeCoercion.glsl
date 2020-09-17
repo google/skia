de6491e60fb68c65ed88d16bbba8c6a91339d022
John Stiles <johnstiles@google.com>
2020-09-17 14:06:03 -0400

    Migrate SkSL error tests to golden outputs.
    Change-Id: Ic8f4730d035981c32b4ddb48e5e919b0396b6d93

---
+++ tests/sksl/errors/golden/BinaryTypeCoercion.glsl
@@ -0,0 +1,11 @@
+### Compilation failed:
+
+error: 11: type mismatch: '=' cannot operate on 'half', 'float'
+error: 20: type mismatch: '=' cannot operate on 'half4', 'float4'
+error: 27: type mismatch: '=' cannot operate on 'half4', 'float4'
+error: 34: type mismatch: '=' cannot operate on 'half4', 'float4'
+error: 41: type mismatch: '=' cannot operate on 'half4', 'float4'
+error: 49: type mismatch: '=' cannot operate on 'half4', 'float4'
+error: 56: type mismatch: '=' cannot operate on 'half4x4', 'float4x4'
+error: 63: type mismatch: '*=' cannot operate on 'float4x4', 'float4'
+8 errors
