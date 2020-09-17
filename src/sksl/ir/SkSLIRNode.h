5eff300345d1b837e3c5a90a21d84f415133be88
Ethan Nicholas <ethannicholas@google.com>
2020-09-17 11:10:14 -0400

    Revert "Reland "Revert "moved BinaryExpression's data into IRNode"""
    This reverts commit bf66ffbbd4ce79bc423fe469a2f90acebd3dba91.
    
    Change-Id: I3bddef8cc6864d0eec254857b1a53a8984753cc4

--- src/sksl/ir/SkSLIRNode.h
+++ src/sksl/ir/SkSLIRNode.h
@@ -8,11 +8,18 @@
 #ifndef SKSL_IRNODE
 #define SKSL_IRNODE
 
+#include "src/sksl/SkSLASTNode.h"
 #include "src/sksl/SkSLLexer.h"
 #include "src/sksl/SkSLString.h"
 
+#include <algorithm>
+#include <vector>
+
 namespace SkSL {
 
+struct Expression;
+struct Statement;
+class SymbolTable;
 class Type;
 
 /**
@@ -21,12 +28,19 @@ class Type;
  */
 class IRNode {
 public:
-    IRNode(int offset, int kind, const Type* type = nullptr)
-    : fOffset(offset)
-    , fKind(kind)
-    , fType(type) {}
+    virtual ~IRNode();
 
-    virtual ~IRNode() {}
+    IRNode& operator=(const IRNode& other) {
+        // Need to have a copy assignment operator because Type requires it, but can't use the
+        // default version until we finish migrating away from std::unique_ptr children. For now,
+        // just assert that there are no children (we could theoretically clone them, but we never
+        // actually copy nodes containing children).
+        SkASSERT(other.fExpressionChildren.empty());
+        fKind = other.fKind;
+        fOffset = other.fOffset;
+        fData = other.fData;
+        return *this;
+    }
 
     virtual String description() const = 0;
 
@@ -35,15 +49,148 @@ public:
     int fOffset;
 
     const Type& type() const {
-        SkASSERT(fType);
-        return *fType;
+        switch (fData.fKind) {
+            case NodeData::Kind::kType:
+                return *this->typeData();
+            case NodeData::Kind::kTypeToken:
+                return *this->typeTokenData().fType;
+            default:
+                SkUNREACHABLE;
+        }
     }
 
 protected:
+    struct BlockData {
+        std::shared_ptr<SymbolTable> fSymbolTable;
+        // if isScope is false, this is just a group of statements rather than an actual
+        // language-level block. This allows us to pass around multiple statements as if they were a
+        // single unit, with no semantic impact.
+        bool fIsScope;
+    };
+
+    struct TypeTokenData {
+        const Type* fType;
+        Token::Kind fToken;
+    };
+
+    struct NodeData {
+        char fBytes[std::max({sizeof(BlockData),
+                              sizeof(Type*),
+                              sizeof(TypeTokenData)})];
+
+        enum class Kind {
+            kBlock,
+            kType,
+            kTypeToken,
+        } fKind;
+
+        NodeData() = default;
+
+        NodeData(BlockData data)
+            : fKind(Kind::kBlock) {
+            BlockData& dest = *reinterpret_cast<BlockData*>(fBytes);
+            new(&dest.fSymbolTable) std::shared_ptr<SymbolTable>(data.fSymbolTable);
+            dest.fIsScope = data.fIsScope;
+        }
+
+        NodeData(const Type* data)
+            : fKind(Kind::kType) {
+            memcpy(fBytes, &data, sizeof(data));
+        }
+
+        NodeData(TypeTokenData data)
+            : fKind(Kind::kTypeToken) {
+            memcpy(fBytes, &data, sizeof(data));
+        }
+
+        ~NodeData() {
+            if (fKind == Kind::kBlock) {
+                reinterpret_cast<BlockData*>(fBytes)->fSymbolTable.~shared_ptr<SymbolTable>();
+            }
+        }
+    };
+
+    IRNode(int offset, int kind, BlockData data, std::vector<std::unique_ptr<Statement>> stmts);
+
+    IRNode(int offset, int kind, const Type* data = nullptr);
+
+    IRNode(int offset, int kind, TypeTokenData data);
+
+    IRNode(const IRNode& other);
+
+    Expression& expressionChild(int index) const {
+        SkASSERT(index >= 0 && index < (int) fExpressionChildren.size());
+        return *fExpressionChildren[index];
+    }
+
+    std::unique_ptr<Expression>& expressionPointer(int index) {
+        SkASSERT(index >= 0 && index < (int) fExpressionChildren.size());
+        return fExpressionChildren[index];
+    }
+
+    const std::unique_ptr<Expression>& expressionPointer(int index) const {
+        SkASSERT(index >= 0 && index < (int) fExpressionChildren.size());
+        return fExpressionChildren[index];
+    }
+
+    int expressionChildCount() const {
+        return fExpressionChildren.size();
+    }
+
+
+    Statement& statementChild(int index) const {
+        SkASSERT(index >= 0 && index < (int) fStatementChildren.size());
+        return *fStatementChildren[index];
+    }
+
+    std::unique_ptr<Statement>& statementPointer(int index) {
+        SkASSERT(index >= 0 && index < (int) fStatementChildren.size());
+        return fStatementChildren[index];
+    }
+
+    const std::unique_ptr<Statement>& statementPointer(int index) const {
+        SkASSERT(index >= 0 && index < (int) fStatementChildren.size());
+        return fStatementChildren[index];
+    }
+
+    int statementChildCount() const {
+        return fStatementChildren.size();
+    }
+
+    BlockData& blockData() {
+        SkASSERT(fData.fKind == NodeData::Kind::kBlock);
+        return *reinterpret_cast<BlockData*>(fData.fBytes);
+    }
+
+    const BlockData& blockData() const {
+        SkASSERT(fData.fKind == NodeData::Kind::kBlock);
+        return *reinterpret_cast<const BlockData*>(fData.fBytes);
+    }
+
+    const Type* typeData() const {
+        SkASSERT(fData.fKind == NodeData::Kind::kType);
+        return *reinterpret_cast<const Type* const*>(fData.fBytes);
+    }
+
+    const TypeTokenData& typeTokenData() const {
+        SkASSERT(fData.fKind == NodeData::Kind::kTypeToken);
+        return *reinterpret_cast<const TypeTokenData*>(fData.fBytes);
+    }
+
     int fKind;
 
-private:
-    const Type* fType;
+    NodeData fData;
+
+    // Needing two separate vectors is a temporary issue. Ideally, we'd just be able to use a single
+    // vector of nodes, but there are various spots where we take pointers to std::unique_ptr<>,
+    // and it isn't safe to pun std::unique_ptr<IRNode> to std::unique_ptr<Statement / Expression>.
+    // And we can't update the call sites to expect std::unique_ptr<IRNode> while there are still
+    // old-style nodes around.
+    // When the transition is finished, we'll be able to drop the unique_ptrs and just handle
+    // <IRNode> directly.
+    std::vector<std::unique_ptr<Expression>> fExpressionChildren;
+    std::vector<std::unique_ptr<Statement>> fStatementChildren;
+
 };
 
 }  // namespace SkSL
