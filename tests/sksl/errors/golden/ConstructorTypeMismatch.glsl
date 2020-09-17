1ce8ff02fbd50b529f4ab59dde3eddedbe404a8e
John Stiles <johnstiles@google.com>
2020-09-17 16:05:52 -0400

    Migrate SkSL error tests to golden outputs.
    Change-Id: Ic8f4730d035981c32b4ddb48e5e919b0396b6d93

---
+++ tests/sksl/errors/golden/ConstructorTypeMismatch.glsl
@@ -0,0 +1,11 @@
+### Compilation failed:
+
+error: 4: expected 'float', but found 'bool'
+error: 5: 'bool2' is not a valid parameter to 'float2' constructor
+error: 6: 'float2' is not a valid parameter to 'bool2' constructor
+error: 7: cannot construct 'bool'
+error: 8: cannot construct 'Foo'
+error: 9: invalid argument to 'float' constructor (expected a number or bool, but found 'Foo')
+error: 10: 'Foo' is not a valid parameter to 'float2' constructor
+error: 11: expected 'float', but found 'bool'
+8 errors
