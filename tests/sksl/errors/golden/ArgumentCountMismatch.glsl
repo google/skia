d1022ba3562a7008d9ac26413bf76b4f2e4ebb35
John Stiles <johnstiles@google.com>
2020-09-17 14:35:27 -0400

    Migrate SkSL error tests to golden outputs.
    Change-Id: Ic8f4730d035981c32b4ddb48e5e919b0396b6d93

---
+++ tests/sksl/errors/golden/ArgumentCountMismatch.glsl
@@ -0,0 +1,4 @@
+### Compilation failed:
+
+error: 6: call to 'foo' expected 1 argument, but found 2
+1 error
---
+++ tests/sksl/errors/golden/ReturnFromVoid.glsl
@@ -0,0 +1,4 @@
+### Compilation failed:
+
+error: 1: may not return a value from a void function
+1 error
