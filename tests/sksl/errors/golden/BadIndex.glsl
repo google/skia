87d521fa24e08dda9f4220fb6bb951884e535621
John Stiles <johnstiles@google.com>
2020-09-17 17:22:40 -0400

    Migrate SkSL error tests to golden outputs.
    Change-Id: Ic8f4730d035981c32b4ddb48e5e919b0396b6d93

---
+++ tests/sksl/errors/golden/BadIndex.glsl
@@ -0,0 +1,5 @@
+### Compilation failed:
+
+error: 1: expected array, but found 'int'
+error: 2: expected array, but found 'float'
+2 errors
---
+++ tests/sksl/errors/golden/DuplicateSymbol.glsl
@@ -0,0 +1,5 @@
+### Compilation failed:
+
+error: 1: symbol 'x' was already defined
+error: 5: symbol 'main' was already defined
+2 errors
