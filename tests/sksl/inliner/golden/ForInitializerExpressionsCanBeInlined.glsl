23028374d865cbe5f272aec7d58aa8b9f097387e
John Stiles <johnstiles@google.com>
2020-09-18 12:06:08 -0400

    Remove inliner from IR generation stage.
    There is no need to inline code during IR generation, as the optimizer
    can now handle this.
    
    Change-Id: If272bfb98e945a75ec91fb4aa026e5631ac51b5b

--- tests/sksl/inliner/golden/ForInitializerExpressionsCanBeInlined.glsl
+++ tests/sksl/inliner/golden/ForInitializerExpressionsCanBeInlined.glsl
@@ -7,7 +7,7 @@ vec4 grow(vec4 v) {
     return v + vec4(0.125);
 }
 void main() {
-    for (sk_FragColor = vec4(0.0625);
-    shouldLoop(sk_FragColor); sk_FragColor = grow(sk_FragColor)) {
+    for (sk_FragColor = vec4(0.0625);shouldLoop(sk_FragColor); sk_FragColor = grow(sk_FragColor)) {
     }
+
 }
