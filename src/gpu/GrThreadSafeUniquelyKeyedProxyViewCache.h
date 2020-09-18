fbb3520609854667b88d0c1663f7bff42ab0d73e
Robert Phillips <robertphillips@google.com>
2020-09-18 16:16:33 -0400

    Add MRU to GrThreadSafeUniquelyKeyedProxyViewCache
    Bug: 1108408
    Change-Id: I54ae1512340408e61c2fb2f408b006d6f7697cf7

--- src/gpu/GrThreadSafeUniquelyKeyedProxyViewCache.h
+++ src/gpu/GrThreadSafeUniquelyKeyedProxyViewCache.h
@@ -11,6 +11,7 @@
 #include "include/private/SkSpinlock.h"
 #include "src/core/SkArenaAlloc.h"
 #include "src/core/SkTDynamicHash.h"
+#include "src/core/SkTInternalLList.h"
 #include "src/gpu/GrSurfaceProxyView.h"
 
 // Ganesh creates a lot of utility textures (e.g., blurred-rrect masks) that need to be shared
@@ -61,49 +62,32 @@ private:
         // Note: the unique key is stored here bc it is never attached to a proxy or a GrTexture
         GrUniqueKey        fKey;
         GrSurfaceProxyView fView;
-        Entry*             fNext = nullptr;
+
+        SK_DECLARE_INTERNAL_LLIST_INTERFACE(Entry);
 
         // for SkTDynamicHash
         static const GrUniqueKey& GetKey(const Entry& e) { return e.fKey; }
         static uint32_t Hash(const GrUniqueKey& key) { return key.hash(); }
     };
 
-    Entry* getEntry(const GrUniqueKey& key, const GrSurfaceProxyView& view) {
-        Entry* newEntry;
-        if (fFreeEntryList) {
-            newEntry = fFreeEntryList;
-            fFreeEntryList = newEntry->fNext;
-            newEntry->fNext = nullptr;
-
-            newEntry->fKey = key;
-            newEntry->fView = view;
-        } else {
-            newEntry = fEntryAllocator.make<Entry>(key, view);
-        }
-
-        return newEntry;
-    }
-
-    void recycleEntry(Entry* dead) {
-        dead->fKey.reset();
-        dead->fView.reset();
-        dead->fNext = fFreeEntryList;
-        fFreeEntryList = dead;
-    }
+    Entry* getEntry(const GrUniqueKey&, const GrSurfaceProxyView&) SK_REQUIRES(fSpinLock);
+    void recycleEntry(Entry*)  SK_REQUIRES(fSpinLock);
 
     GrSurfaceProxyView internalAdd(const GrUniqueKey&,
                                    const GrSurfaceProxyView&)  SK_REQUIRES(fSpinLock);
 
     mutable SkSpinlock fSpinLock;
 
-    SkTDynamicHash<Entry, GrUniqueKey> fUniquelyKeyedProxyViews  SK_GUARDED_BY(fSpinLock);
+    SkTDynamicHash<Entry, GrUniqueKey> fUniquelyKeyedProxyViewMap  SK_GUARDED_BY(fSpinLock);
+    // The head of this list is the MRU
+    SkTInternalLList<Entry>            fUniquelyKeyedProxyViewList  SK_GUARDED_BY(fSpinLock);
 
     // TODO: empirically determine this from the skps
     static const int kInitialArenaSize = 64 * sizeof(Entry);
 
     char                         fStorage[kInitialArenaSize];
     SkArenaAlloc                 fEntryAllocator{fStorage, kInitialArenaSize, kInitialArenaSize};
-    Entry*                       fFreeEntryList = nullptr;
+    Entry*                       fFreeEntryList  SK_GUARDED_BY(fSpinLock);
 };
 
 #endif // GrThreadSafeUniquelyKeyedProxyViewCache_DEFINED
