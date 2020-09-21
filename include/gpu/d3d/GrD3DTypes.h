43f6c9d019ab2ebd431ee03939d4d503fb1740c4
Jim Van Verth <jvanverth@google.com>
2020-09-21 14:28:03 -0400

    Add AMD allocator to D3D12 backend.
    Adds base class GrD3DAlloc and GrD3DMemoryAllocator, and a reference
    to a GrD3DMemoryAllocator in GrBackendContext and a reference to a
    GrD3DAlloc in GrD3DTextureResourceInfo. Internally, we override this
    base class to define the AMD memory allocator.
    
    Change-Id: I033924b0247ea330969b1398f25985e7a84aec11
    Bug: skia:9935

--- include/gpu/d3d/GrD3DTypes.h
+++ include/gpu/d3d/GrD3DTypes.h
@@ -21,6 +21,7 @@
 // You should only include this header if you need the Direct3D definitions and are
 // prepared to rename those identifiers. Otherwise use GrD3DTypesMinimal.h.
 
+#include "include/core/SkRefCnt.h"
 #include "include/gpu/d3d/GrD3DTypesMinimal.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
@@ -28,11 +29,27 @@
 
 using Microsoft::WRL::ComPtr;
 
+// interface classes for the GPU memory allocator
+class GrD3DAlloc : public SkRefCnt {
+public:
+    ~GrD3DAlloc() override = default;
+};
+
+class GrD3DMemoryAllocator : public SkRefCnt {
+public:
+    virtual ComPtr<ID3D12Resource> createResource(D3D12_HEAP_TYPE, const D3D12_RESOURCE_DESC*,
+                                                  D3D12_RESOURCE_STATES initialResourceState,
+                                                  sk_sp<GrD3DAlloc>* allocation,
+                                                  const D3D12_CLEAR_VALUE*, REFIID riidResource,
+                                                  void** ppvResource) = 0;
+};
+
 // Note: there is no notion of Borrowed or Adopted resources in the D3D backend,
 // so Ganesh will ref fResource once it's asked to wrap it.
 // Clients are responsible for releasing their own ref to avoid memory leaks.
 struct GrD3DTextureResourceInfo {
     ComPtr<ID3D12Resource>   fResource;
+    sk_sp<GrD3DAlloc>        fAlloc;
     D3D12_RESOURCE_STATES    fResourceState;
     DXGI_FORMAT              fFormat;
     uint32_t                 fLevelCount;
@@ -41,6 +58,7 @@ struct GrD3DTextureResourceInfo {
 
     GrD3DTextureResourceInfo()
             : fResource(nullptr)
+            , fAlloc(nullptr)
             , fResourceState(D3D12_RESOURCE_STATE_COMMON)
             , fFormat(DXGI_FORMAT_UNKNOWN)
             , fLevelCount(0)
@@ -48,12 +66,14 @@ struct GrD3DTextureResourceInfo {
             , fProtected(GrProtected::kNo) {}
 
     GrD3DTextureResourceInfo(const ComPtr<ID3D12Resource>& resource,
+                             const sk_sp<GrD3DAlloc> alloc,
                              D3D12_RESOURCE_STATES resourceState,
                              DXGI_FORMAT format,
                              uint32_t levelCount,
                              unsigned int sampleQualityLevel,
                              GrProtected isProtected = GrProtected::kNo)
             : fResource(resource)
+            , fAlloc(alloc)
             , fResourceState(resourceState)
             , fFormat(format)
             , fLevelCount(levelCount)
@@ -63,6 +83,7 @@ struct GrD3DTextureResourceInfo {
     GrD3DTextureResourceInfo(const GrD3DTextureResourceInfo& info,
                              GrD3DResourceStateEnum resourceState)
             : fResource(info.fResource)
+            , fAlloc(info.fAlloc)
             , fResourceState(static_cast<D3D12_RESOURCE_STATES>(resourceState))
             , fFormat(info.fFormat)
             , fLevelCount(info.fLevelCount)
