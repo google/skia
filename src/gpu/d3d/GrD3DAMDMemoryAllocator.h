9fcb29b16eba65d50feb84239730b776bca41369
Jim Van Verth <jvanverth@google.com>
2020-09-21 16:07:17 -0400

    Add AMD allocator to D3D12 backend.
    Adds base class GrD3DAlloc and GrD3DMemoryAllocator, and a reference
    to a GrD3DMemoryAllocator in GrBackendContext and a reference to a
    GrD3DAlloc in GrD3DTextureResourceInfo. Internally, we override this
    base class to define the AMD memory allocator.
    
    Change-Id: I033924b0247ea330969b1398f25985e7a84aec11
    Bug: skia:9935
    BYPASS_INCLUSIVE_LANGUAGE_REASON=adding_third_party_library

---
+++ src/gpu/d3d/GrD3DAMDMemoryAllocator.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrD3DAMDMemoryAllocator_DEFINED
+#define GrD3DAMDMemoryAllocator_DEFINED
+
+#include "include/core/SkRefCnt.h"
+#include "include/gpu/d3d/GrD3DTypes.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-dynamic-exception-spec"
+#include "src/D3D12MemAlloc.h"
+#pragma clang diagnostic pop
+
+class GrD3DAMDMemoryAllocator : public GrD3DMemoryAllocator {
+public:
+    static sk_sp<GrD3DMemoryAllocator> Make(IDXGIAdapter* adapter, ID3D12Device* device);
+
+    ~GrD3DAMDMemoryAllocator() override { fAllocator->Release(); }
+
+    ComPtr<ID3D12Resource> createResource(D3D12_HEAP_TYPE, const D3D12_RESOURCE_DESC*,
+                                          D3D12_RESOURCE_STATES initialResourceState,
+                                          sk_sp<GrD3DAlloc>* allocation,
+                                          const D3D12_CLEAR_VALUE*, REFIID riidResource,
+                                          void** ppvResource) override;
+
+    class Alloc : public GrD3DAlloc {
+    public:
+        Alloc(D3D12MA::Allocation* allocation) : fAllocation(allocation) {}
+        ~Alloc() override {
+            fAllocation->Release();
+        }
+    private:
+        D3D12MA::Allocation* fAllocation;
+    };
+
+private:
+    GrD3DAMDMemoryAllocator(D3D12MA::Allocator* allocator) : fAllocator(allocator) {}
+
+    D3D12MA::Allocator* fAllocator;
+};
+
+#endif
