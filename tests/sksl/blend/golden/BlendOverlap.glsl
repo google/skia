167933e0660de00ef801dd7a558e2899e43ecadb
John Stiles <johnstiles@google.com>
2020-09-21 16:48:47 -0400

    Optimize blend functions to work with the inliner.
    Change-Id: I7828fb53f54827f9db2a65965140a28978fadf29

--- tests/sksl/blend/golden/BlendOverlap.glsl
+++ tests/sksl/blend/golden/BlendOverlap.glsl
@@ -1,13 +1,31 @@
 
 uniform vec4 src, dst;
-float _blend_overlay_component(float sc, float sa, float dc, float da) {
-    if (2.0 * dc <= da) {
-        return (2.0 * sc) * dc;
-    }
-    return sa * da - (2.0 * (da - dc)) * (sa - sc);
+float _blend_overlay_component(vec2 s, vec2 d) {
+    return 2.0 * d.x <= d.y ? (2.0 * s.x) * d.x : s.y * d.y - (2.0 * (d.y - d.x)) * (s.y - s.x);
 }
 vec4 blend_overlay(vec4 src, vec4 dst) {
-    vec4 result = vec4(_blend_overlay_component(src.x, src.w, dst.x, dst.w), _blend_overlay_component(src.y, src.w, dst.y, dst.w), _blend_overlay_component(src.z, src.w, dst.z, dst.w), src.w + (1.0 - src.w) * dst.w);
+    float _0_blend_overlay_component;
+    vec2 _1_s = src.xw;
+    vec2 _2_d = dst.xw;
+    {
+        _0_blend_overlay_component = 2.0 * _2_d.x <= _2_d.y ? (2.0 * _1_s.x) * _2_d.x : _1_s.y * _2_d.y - (2.0 * (_2_d.y - _2_d.x)) * (_1_s.y - _1_s.x);
+    }
+    float _3_blend_overlay_component;
+    vec2 _4_s = src.yw;
+    vec2 _5_d = dst.yw;
+    {
+        _3_blend_overlay_component = 2.0 * _5_d.x <= _5_d.y ? (2.0 * _4_s.x) * _5_d.x : _4_s.y * _5_d.y - (2.0 * (_5_d.y - _5_d.x)) * (_4_s.y - _4_s.x);
+    }
+    float _6_blend_overlay_component;
+    vec2 _7_s = src.zw;
+    vec2 _8_d = dst.zw;
+    {
+        _6_blend_overlay_component = 2.0 * _8_d.x <= _8_d.y ? (2.0 * _7_s.x) * _8_d.x : _7_s.y * _8_d.y - (2.0 * (_8_d.y - _8_d.x)) * (_7_s.y - _7_s.x);
+    }
+    vec4 result = vec4(_0_blend_overlay_component, _3_blend_overlay_component, _6_blend_overlay_component, src.w + (1.0 - src.w) * dst.w);
+
+
+
     result.xyz += dst.xyz * (1.0 - src.w) + src.xyz * (1.0 - dst.w);
     return result;
 }
