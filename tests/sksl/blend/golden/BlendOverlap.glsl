24e39b53e034eead99ef1fa9732aab9df5255c4e
John Stiles <johnstiles@google.com>
2020-09-21 17:19:56 -0400

    Restructure blend functions to allow for smarter inlining.
    Early returns cause the inliner to generate suboptimal code. For our
    built-ins, let's avoid them.
    
    Patterns like this challenge the inliner:
        if (x) return y;
        return z;
    
    But this can be replaced by equivalent code that inlines better:
        return x ? y : z;
    
    Or, if a ternary is awkward, this also does a better job:
        if (x) return y;
        else return z;
    
    In a few cases like HardLight and Overlay, this allows the inliner to
    avoid generating a do-while(false) block for control flow.
    
    Change-Id: I921c929122297c40476ff15b4da631fc1581e308
    Bug: skia:10737

--- tests/sksl/blend/golden/BlendOverlap.glsl
+++ tests/sksl/blend/golden/BlendOverlap.glsl
@@ -1,10 +1,7 @@
 #version 400
 uniform vec4 src, dst;
 float _blend_overlay_component(float sc, float sa, float dc, float da) {
-    if (2.0 * dc <= da) {
-        return (2.0 * sc) * dc;
-    }
-    return sa * da - (2.0 * (da - dc)) * (sa - sc);
+    return 2.0 * dc <= da ? (2.0 * sc) * dc : sa * da - (2.0 * (da - dc)) * (sa - sc);
 }
 vec4 blend_overlay(vec4 src, vec4 dst) {
     float _0_blend_overlay_component;
@@ -12,52 +9,25 @@ vec4 blend_overlay(vec4 src, vec4 dst) {
     float _2_sa = src.w;
     float _3_dc = dst.x;
     float _4_da = dst.w;
-    do {
-        if (2.0 * _3_dc <= _4_da) {
-            {
-                _0_blend_overlay_component = (2.0 * _1_sc) * _3_dc;
-                break;
-            }
-        }
-        {
-            _0_blend_overlay_component = _2_sa * _4_da - (2.0 * (_4_da - _3_dc)) * (_2_sa - _1_sc);
-            break;
-        }
-    } while (false);
+    {
+        _0_blend_overlay_component = 2.0 * _3_dc <= _4_da ? (2.0 * _1_sc) * _3_dc : _2_sa * _4_da - (2.0 * (_4_da - _3_dc)) * (_2_sa - _1_sc);
+    }
     float _5_blend_overlay_component;
     float _6_sc = src.y;
     float _7_sa = src.w;
     float _8_dc = dst.y;
     float _9_da = dst.w;
-    do {
-        if (2.0 * _8_dc <= _9_da) {
-            {
-                _5_blend_overlay_component = (2.0 * _6_sc) * _8_dc;
-                break;
-            }
-        }
-        {
-            _5_blend_overlay_component = _7_sa * _9_da - (2.0 * (_9_da - _8_dc)) * (_7_sa - _6_sc);
-            break;
-        }
-    } while (false);
+    {
+        _5_blend_overlay_component = 2.0 * _8_dc <= _9_da ? (2.0 * _6_sc) * _8_dc : _7_sa * _9_da - (2.0 * (_9_da - _8_dc)) * (_7_sa - _6_sc);
+    }
     float _10_blend_overlay_component;
     float _11_sc = src.z;
     float _12_sa = src.w;
     float _13_dc = dst.z;
     float _14_da = dst.w;
-    do {
-        if (2.0 * _13_dc <= _14_da) {
-            {
-                _10_blend_overlay_component = (2.0 * _11_sc) * _13_dc;
-                break;
-            }
-        }
-        {
-            _10_blend_overlay_component = _12_sa * _14_da - (2.0 * (_14_da - _13_dc)) * (_12_sa - _11_sc);
-            break;
-        }
-    } while (false);
+    {
+        _10_blend_overlay_component = 2.0 * _13_dc <= _14_da ? (2.0 * _11_sc) * _13_dc : _12_sa * _14_da - (2.0 * (_14_da - _13_dc)) * (_12_sa - _11_sc);
+    }
     vec4 result = vec4(_0_blend_overlay_component, _5_blend_overlay_component, _10_blend_overlay_component, src.w + (1.0 - src.w) * dst.w);
 
 
