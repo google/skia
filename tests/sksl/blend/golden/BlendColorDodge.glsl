a9f2870c57135f3536dd1b86a4520f57178b2e1e
John Stiles <johnstiles@google.com>
2020-09-21 17:19:56 -0400

    Restructure blend functions to allow for smarter inlining.
    Early returns can cause the inliner to generate suboptimal code. We
    control our built-ins, so let's avoid them where we can.
    
    Patterns like this challenge the inliner:
        if (x) return y;
        return z;
    
    But this can be replaced by equivalent code that inlines better:
        return x ? y : z;
    
    Or, if a ternary can't be used, this also does a better job:
        if (x) return y;
        else return z;
    
    In a few cases like HardLight and Overlap, this allows the inliner to
    avoid generating a do-while(false) block for control flow.
    
    Change-Id: I921c929122297c40476ff15b4da631fc1581e308
    Bug: skia:10737

--- tests/sksl/blend/golden/BlendColorDodge.glsl
+++ tests/sksl/blend/golden/BlendColorDodge.glsl
@@ -1,9 +1,7 @@
 #version 400
 uniform vec4 src, dst;
 float _guarded_divide(float n, float d) {
-    {
-        return n / d;
-    }
+    return n / d;
 }
 float _color_dodge_component(float sc, float sa, float dc, float da) {
     if (dc == 0.0) {
@@ -12,17 +10,16 @@ float _color_dodge_component(float sc, float sa, float dc, float da) {
         float d = sa - sc;
         if (d == 0.0) {
             return (sa * da + sc * (1.0 - da)) + dc * (1.0 - sa);
-        }
-        float _1_guarded_divide;
-        float _2_n = dc * sa;
-        {
+        } else {
+            float _1_guarded_divide;
+            float _2_n = dc * sa;
             {
                 _1_guarded_divide = _2_n / d;
             }
-        }
-        d = min(da, _1_guarded_divide);
+            d = min(da, _1_guarded_divide);
 
-        return (d * sa + sc * (1.0 - da)) + dc * (1.0 - sa);
+            return (d * sa + sc * (1.0 - da)) + dc * (1.0 - sa);
+        }
     }
 }
 vec4 blend_color_dodge(vec4 src, vec4 dst) {
