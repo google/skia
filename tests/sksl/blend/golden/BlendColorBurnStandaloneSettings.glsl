7753033bdaa45ad93bb0037383611a451ae9e14c
John Stiles <johnstiles@google.com>
2020-09-21 17:19:56 -0400

    Restructure blend functions to allow for smarter inlining.
    Early returns cause the inliner to generate suboptimal code. We control
    our built-ins, so let's avoid them where we can.
    
    Patterns like this challenge the inliner:
        if (x) return y;
        return z;
    
    But this can be replaced by equivalent code that inlines better:
        return x ? y : z;
    
    Or, if a ternary can't be used, this also does a better job:
        if (x) return y;
        else return z;
    
    In a few cases like HardLight and Overlap, this allows the inliner to
    avoid generating a do-while(false) block for control flow.
    
    Change-Id: I921c929122297c40476ff15b4da631fc1581e308
    Bug: skia:10737

--- tests/sksl/blend/golden/BlendColorBurnStandaloneSettings.glsl
+++ tests/sksl/blend/golden/BlendColorBurnStandaloneSettings.glsl
@@ -1,26 +1,23 @@
 
 uniform vec4 src, dst;
 float _guarded_divide(float n, float d) {
-    {
-        return n / d;
-    }
+    return n / d;
 }
 float _color_burn_component(float sc, float sa, float dc, float da) {
     if (da == dc) {
         return (sa * da + sc * (1.0 - da)) + dc * (1.0 - sa);
     } else if (sc == 0.0) {
         return dc * (1.0 - sa);
-    }
-    float _1_guarded_divide;
-    float _2_n = (da - dc) * sa;
-    {
+    } else {
+        float _1_guarded_divide;
+        float _2_n = (da - dc) * sa;
         {
             _1_guarded_divide = _2_n / sc;
         }
-    }
-    float d = max(0.0, da - _1_guarded_divide);
+        float d = max(0.0, da - _1_guarded_divide);
 
-    return (d * sa + sc * (1.0 - da)) + dc * (1.0 - sa);
+        return (d * sa + sc * (1.0 - da)) + dc * (1.0 - sa);
+    }
 }
 vec4 blend_color_burn(vec4 src, vec4 dst) {
     return vec4(_color_burn_component(src.x, src.w, dst.x, dst.w), _color_burn_component(src.y, src.w, dst.y, dst.w), _color_burn_component(src.z, src.w, dst.z, dst.w), src.w + (1.0 - src.w) * dst.w);
