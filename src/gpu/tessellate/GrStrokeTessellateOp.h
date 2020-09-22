da6f8ce944f70003cee192231dd812be15cd598b
Chris Dalton <csmartdalton@google.com>
2020-09-22 17:27:06 -0600

    Merge joins into the same tessellation patch as their following stroke
    Implements the previous join as a sub-section of the tessellation
    patch. This cuts the number of vertex shader invocations in half since
    we are no longer treating joins as their own patch. (This therefore
    cuts the amount of inflection/midtangent chopping work in half also.)
    
    This required a lot of modifications to GrStrokePatchBuilder.cpp, so
    this CL also finishes up the chopping logic in that file for when there
    aren't enough tessellation segments to render a curve.
    
    Bug: skia:10419
    Change-Id: I3da081fe756c97aeeb65e27f1319a29763b4ad34

--- src/gpu/tessellate/GrStrokeTessellateOp.h
+++ src/gpu/tessellate/GrStrokeTessellateOp.h
@@ -26,7 +26,7 @@ private:
     //
     // Patches can overlap, so until a stencil technique is implemented, the provided paint must be
     // a constant blended color.
-    GrStrokeTessellateOp(GrAAType, const SkMatrix&, const SkPath&, const SkStrokeRec&, GrPaint&&);
+    GrStrokeTessellateOp(GrAAType, const SkMatrix&, const SkStrokeRec&, const SkPath&, GrPaint&&);
 
     const char* name() const override { return "GrStrokeTessellateOp"; }
     void visitProxies(const VisitProxyFunc& fn) const override { fProcessors.visitProxies(fn); }
@@ -40,21 +40,15 @@ private:
     void onPrepare(GrOpFlushState* state) override;
     void onExecute(GrOpFlushState*, const SkRect& chainBounds) override;
 
-    struct PathStroke {
-        PathStroke(const SkPath& path, const SkStrokeRec& stroke) : fPath(path), fStroke(stroke) {}
-        SkPath fPath;
-        SkStrokeRec fStroke;
-    };
-
-    GrSTArenaList<PathStroke> fPathStrokes;
-    int fTotalCombinedVerbCnt;
-
     const GrAAType fAAType;
-    SkMatrix fViewMatrix;
-    float fMatrixScale;
-    float fMiterLimitOrZero = 0;  // Zero if there is not a stroke with a miter join type.
+    const SkMatrix fViewMatrix;
+    const float fMatrixScale;
+    const SkStrokeRec fStroke;
     SkPMColor4f fColor;
     GrProcessorSet fProcessors;
+
+    GrSTArenaList<SkPath> fPaths;
+    int fTotalCombinedVerbCnt;
 
     // S=1 because we will almost always fit everything into one single chunk.
     SkSTArray<1, GrStrokePatchBuilder::PatchChunk> fPatchChunks;
