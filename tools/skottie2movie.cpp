8b5ed02eb02c2373c1a0dfd25c2fe0f4479bb055
Florin Malita <fmalita@chromium.org>
2020-09-22 08:51:43 -0400

    Fix skia_use_ffmpeg builds
    ... post https://skia-review.googlesource.com/c/skia/+/317109
    
    Change-Id: I7286fd6d7598a50a1db452f04a28317c600f4984
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/318477
    Commit-Queue: Adlai Holler <adlai@google.com>
    Auto-Submit: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Adlai Holler <adlai@google.com>

--- tools/skottie2movie.cpp
+++ tools/skottie2movie.cpp
@@ -146,7 +146,7 @@ int main(int argc, char** argv) {
                                                 SkSurface::RescaleGamma::kSrc,
                                                 kNone_SkFilterQuality,
                                                 read_pixels_cb, &asyncRec);
-                surf->getContext()->submit();
+                context->submit();
             } else {
                 SkPixmap pm;
                 SkAssertResult(surf->peekPixels(&pm));
