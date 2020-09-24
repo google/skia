ee1d521cc1d3c3d5ad392a46e0ac4fe505334fc6
John Stiles <johnstiles@google.com>
2020-09-24 17:49:49 -0400

    Add golden outputs for the Metal backend.
    This CL also updates the blend tests to use sk_FragColor instead of
    returning a half4, as the Metal backend assumes that a fragment
    processor's `main` will return void and always synthesizes a
    `return *_out;` at the end.
    
    (context link:
    https://osscs.corp.google.com/android/platform/superproject/+/master:external/skqp/src/sksl/SkSLMetalCodeGenerator.cpp;l=803;drc=842d31b14159626054e01dd32826563a8f4214bf )
    
    BYPASS_INCLUSIVE_LANGUAGE_REASON=see http://b/168134166
    
    Change-Id: I330a456bf25ee72d3a29c59cd624625378ae80a0
    Bug: skia:10649, skia:10757, skia:10758, skia:10759, skia:10760, skia:10761, skia:10762

--- tests/sksl/blend/golden/BlendDarken.glsl
+++ tests/sksl/blend/golden/BlendDarken.glsl
@@ -1,5 +1,6 @@
 #version 400
-uniform vec4 src, dst;
+out vec4 sk_FragColor;
+in vec4 src, dst;
 vec4 blend_src_over(vec4 src, vec4 dst) {
     return src + (1.0 - src.w) * dst;
 }
@@ -13,7 +14,7 @@ vec4 blend_darken(vec4 src, vec4 dst) {
     result.xyz = min(result.xyz, (1.0 - dst.w) * src.xyz + dst.xyz);
     return result;
 }
-vec4 main() {
+void main() {
     vec4 _0_blend_darken;
     {
         vec4 _3_blend_src_over;
@@ -26,6 +27,6 @@ vec4 main() {
         _0_blend_darken = _1_result;
     }
 
-    return _0_blend_darken;
+    sk_FragColor = _0_blend_darken;
 
 }
---
+++ tests/sksl/shared/golden/ModifiersDeclaration.metal
@@ -0,0 +1,30 @@
+#include <metal_stdlib>
+#include <simd/simd.h>
+using namespace metal;
+struct Inputs {
+};
+struct Outputs {
+    float4 sk_FragColor [[color(0)]];
+};
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+thread ;
+fragment Outputs fragmentMain(Inputs _in [[stage_in]], bool _frontFacing [[front_facing]], float4 _fragCoord [[position]]) {
+    Outputs _outputStruct;
+    thread Outputs* _out = &_outputStruct;
+    return *_out;
+}
