5f2240a00ef315169567ac080df6c9bdb7fe4945
Ben Wagner <bungeman@google.com>
2020-09-24 14:49:00 -0400

    Reland "Remove use of legacy display globals."
    This is a reland of c1916c34febe0c6f9805b5d9fc18349ec8b74008
    
    Original change's description:
    > Remove use of legacy display globals.
    >
    > In the ongoing effort to remove the display globals from Skia, allow
    > their use only if SK_LEGACY_SURFACE_PROPS is defined. Do not define this
    > in a normal Skia build and remove all use from Skia code.
    >
    > Change-Id: I9ff550f5db246b9024aac687a1bc01321f1be4c8
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/319343
    > Reviewed-by: Herb Derby <herb@google.com>
    > Commit-Queue: Ben Wagner <bungeman@google.com>
    
    Change-Id: I61a2ac058fafc99653e3304876cf4b97350dac8b

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -523,7 +523,11 @@ void SkCanvas::init(sk_sp<SkBaseDevice> device) {
 
 SkCanvas::SkCanvas()
     : fMCStack(sizeof(MCRec), fMCRecStorage, sizeof(fMCRecStorage))
+#ifdef SK_LEGACY_SURFACE_PROPS
     , fProps(SkSurfaceProps::kLegacyFontHost_InitType)
+#else
+    , fProps()
+#endif
 {
     inc_canvas();
 
@@ -541,7 +545,11 @@ SkCanvas::SkCanvas(int width, int height, const SkSurfaceProps* props)
 
 SkCanvas::SkCanvas(const SkIRect& bounds)
     : fMCStack(sizeof(MCRec), fMCRecStorage, sizeof(fMCRecStorage))
+#ifdef SK_LEGACY_SURFACE_PROPS
     , fProps(SkSurfaceProps::kLegacyFontHost_InitType)
+#else
+    , fProps()
+#endif
 {
     inc_canvas();
 
@@ -571,7 +579,11 @@ SkCanvas::SkCanvas(const SkBitmap& bitmap, const SkSurfaceProps& props)
 SkCanvas::SkCanvas(const SkBitmap& bitmap, std::unique_ptr<SkRasterHandleAllocator> alloc,
                    SkRasterHandleAllocator::Handle hndl)
     : fMCStack(sizeof(MCRec), fMCRecStorage, sizeof(fMCRecStorage))
+#ifdef SK_LEGACY_SURFACE_PROPS
     , fProps(SkSurfaceProps::kLegacyFontHost_InitType)
+#else
+    , fProps()
+#endif
     , fAllocator(std::move(alloc))
 {
     inc_canvas();
@@ -584,9 +596,7 @@ SkCanvas::SkCanvas(const SkBitmap& bitmap) : SkCanvas(bitmap, nullptr, nullptr) 
 
 #ifdef SK_BUILD_FOR_ANDROID_FRAMEWORK
 SkCanvas::SkCanvas(const SkBitmap& bitmap, ColorBehavior)
-    : fMCStack(sizeof(MCRec), fMCRecStorage, sizeof(fMCRecStorage))
-    , fProps(SkSurfaceProps::kLegacyFontHost_InitType)
-    , fAllocator(nullptr)
+    : fMCStack(sizeof(MCRec), fMCRecStorage, sizeof(fMCRecStorage)), fProps(), fAllocator(nullptr)
 {
     inc_canvas();
 
