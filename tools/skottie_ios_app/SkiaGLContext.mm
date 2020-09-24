078d499001240a69fa7464c40cf872ed9220b8b0
Ben Wagner <bungeman@google.com>
2020-09-24 14:49:00 -0400

    Remove use of legacy display globals.
    In the ongoing effort to remove the display globals from Skia, allow
    their use only if SK_LEGACY_SURFACE_PROPS is defined. Do not define this
    in a normal Skia build and remove all use from Skia code.
    
    Change-Id: I9ff550f5db246b9024aac687a1bc01321f1be4c8

--- tools/skottie_ios_app/SkiaGLContext.mm
+++ tools/skottie_ios_app/SkiaGLContext.mm
@@ -25,7 +25,7 @@ static void configure_glkview_for_skia(GLKView* view) {
 static sk_sp<SkSurface> make_gl_surface(GrDirectContext* dContext, int width, int height) {
     static constexpr int kStencilBits = 8;
     static constexpr int kSampleCount = 1;
-    static const SkSurfaceProps surfaceProps = SkSurfaceProps::kLegacyFontHost_InitType;
+    static const SkSurfaceProps surfaceProps;
     if (!dContext || width <= 0 || height <= 0) {
         return nullptr;
     }
