c78d02b0512f6118c8f7197ba11c74a6446fcfd1
John Stiles <johnstiles@google.com>
2020-09-25 16:02:07 -0400

    Add unit tests for assignment and invalid field access.
    Change-Id: I8b755ae0078d6353e24834cd15603091d681114c
    Bug: skia:10766

---
+++ tests/sksl/errors/golden/BadFieldAccess.glsl
@@ -0,0 +1,8 @@
+### Compilation failed:
+
+error: 3: type 'S' does not have a field named 'missing'
+error: 4: not a function
+error: 5: type mismatch: '=' cannot operate on 'float', 'bool3'
+error: 6: too many components in swizzle mask 'missing'
+error: 7: expected array, but found 'float'
+5 errors
