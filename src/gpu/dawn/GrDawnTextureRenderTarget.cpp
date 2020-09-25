e0cb8722e88fff2fd72df44402f50a7b08e5fce5
Stephen White <senorblanco@chromium.org>
2020-09-25 11:49:52 -0400

    Dawn: implement automatic mipmap generation (WIP).
    Change-Id: I71e1808d78f45eee68df7f124100f5b563f29da3

--- src/gpu/dawn/GrDawnTextureRenderTarget.cpp
+++ src/gpu/dawn/GrDawnTextureRenderTarget.cpp
@@ -13,12 +13,11 @@
 
 GrDawnTextureRenderTarget::GrDawnTextureRenderTarget(GrDawnGpu* gpu,
                                                      SkISize dimensions,
-                                                     wgpu::TextureView textureView,
                                                      int sampleCnt,
                                                      const GrDawnTextureInfo& textureInfo,
                                                      GrMipmapStatus mipmapStatus)
         : GrSurface(gpu, dimensions, GrProtected::kNo)
-        , GrDawnTexture(gpu, dimensions, textureView, textureInfo, mipmapStatus)
+        , GrDawnTexture(gpu, dimensions, textureInfo, mipmapStatus)
         , GrDawnRenderTarget(gpu, dimensions, sampleCnt,
                              GrDawnRenderTargetInfo(textureInfo)) {}
 
