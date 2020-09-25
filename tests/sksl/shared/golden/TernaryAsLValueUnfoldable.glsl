c14defb8bfeedcbf9af4b9e12d8b057c07fbe409
John Stiles <johnstiles@google.com>
2020-09-25 14:40:44 -0400

    Disallow assignment to unfoldable ternaries in SkSL.
    GLSL does not support assigning to ternaries, and will fail to compile
    and/or generate non-functional shaders if we pass in a shader that tries
    to assign into a ternary expression.
    
    If SkSL is able to completely eliminate the ternary (e.g. if it boils
    down to a simple `true ? x : y` or `false ? x : y`), SkSL can strip out
    the ternary entirely and generate valid GLSL. This case is harmless and
    so it is still allowed.
    
    Change-Id: I960f119fb9934f998697634e6c4e519cd77d3780
    Bug: skia:10767
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/319679
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/shared/golden/TernaryAsLValueUnfoldable.glsl
+++
@@ -1,8 +0,0 @@
-
-out vec4 sk_FragColor;
-void main() {
-    float r, g;
-    sqrt(1.0) > 0.0 ? r : g = sqrt(1.0);
-    sqrt(0.0) > 0.0 ? r : g = sqrt(0.0);
-    sk_FragColor = vec4(r, g, 1.0, 1.0);
-}
