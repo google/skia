7a6935a52814a871c0c9fd6f2bac08134ebc1c5c
John Stiles <johnstiles@google.com>
2020-09-25 23:51:39 -0400

    Update OutParams unit test to demonstrate SkSL failure.
    Swizzles in combination with out params are currently broken in SkSL.
    They are fixed in the followup CL at http://review.skia.org/319917
    
    Change-Id: I22d9436a15631e6ee2acf9fc312a8634cf3b5407
    Bug: skia:10756
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/319918
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- tests/sksl/shared/golden/OutParams.glsl
+++ tests/sksl/shared/golden/OutParams.glsl
@@ -1,10 +1,3 @@
+### Compilation failed:
 
-out vec4 sk_FragColor;
-void main() {
-    sk_FragColor = vec4(1.0, 2.0, 3.0, 4.0);
-    sk_FragColor = vec4(mat2(2.0)[0][0], mat3(3.0)[0][0], mat4(4.0)[0][0], 1.0);
-    sk_FragColor = vec4(1.0, 2.0, 3.0, 4.0);
-    sk_FragColor = vec4(1.0, 2.0, 3.0, 4.0);
-    sk_FragColor = vec4(mat2(2.0)[0][0], mat3(3.0)[0][0], mat4(4.0)[0][0], 1.0);
-    sk_FragColor = vec4(1.0, bvec2(false).x ? 1.0 : 0.0, bvec3(true).x ? 1.0 : 0.0, bvec4(false).x ? 1.0 : 0.0);
-}
+
