5328e3cd8c644614bc875fff6a2bb0f2da0832fd
Robert Phillips <robertphillips@google.com>
2020-09-28 14:03:01 -0400

    Add a shared thread-safe uniquely-keyed proxy-view cache
    The DDL recorders need to share many uniquely keyed proxies between
    themselves and w/ the direct context. This CL adds the shared
    cache and uses it for blurred RRect masks.
    
    Bug: 1108408
    Change-Id: I1ec9477dffe870e5973f8a334a65b1013a4ca3dd

--- gm/path_stroke_with_zero_length.cpp
+++ gm/path_stroke_with_zero_length.cpp
@@ -132,7 +132,10 @@ static const SkColor kSuccessGreen = 0x7F1B9E77;
 static void draw_zero_length_capped_paths(SkCanvas* canvas, bool aa) {
     auto rContext = canvas->recordingContext();
     auto dContext = GrAsDirectContext(rContext);
-    SkASSERT(dContext || !rContext); // not supported in DDL.
+    if (!dContext) {
+        // not supported in DDL.
+        return;
+    }
     canvas->translate(kCellPad, kCellPad);
 
     SkImageInfo info = canvas->imageInfo().makeWH(kCellWidth, kCellHeight);
@@ -195,7 +198,9 @@ DEF_SIMPLE_GM_BG(zero_length_paths_bw, canvas, kTotalWidth, kTotalHeight, SK_Col
 static void draw_zero_length_capped_paths_dbl_contour(SkCanvas* canvas, bool aa) {
     auto rContext = canvas->recordingContext();
     auto dContext = GrAsDirectContext(rContext);
-    SkASSERT(dContext || !rContext); // not supported in DDL.
+    if (!dContext) {
+        return;  // not supported in DDL.
+    }
     canvas->translate(kCellPad, kCellPad);
 
     SkImageInfo info = canvas->imageInfo().makeWH(kCellWidth, kCellHeight);
