4750cc66a561ccd4d8ab68a99c65e908ad4b41bc
Robert Phillips <robertphillips@google.com>
2020-09-28 13:27:01 -0400

    Add a shared thread-safe uniquely-keyed proxy-view cache
    The DDL recorders need to share many uniquely keyed proxies between
    themselves and w/ the direct context. This CL adds the shared
    cache and uses it for blurred RRect masks.
    
    Bug: 1108408
    Change-Id: I1ec9477dffe870e5973f8a334a65b1013a4ca3dd

--- tools/skpbench/skpbench.cpp
+++ tools/skpbench/skpbench.cpp
@@ -472,6 +472,8 @@ void print_result(const std::vector<Sample>& samples, const char* config, const 
     fflush(stdout);
 }
 
+//extern int numHWMask, numSWMask;
+
 int main(int argc, char** argv) {
     CommandLineFlags::SetUsage(
             "Use skpbench.py instead. "
@@ -645,6 +647,8 @@ int main(int argc, char** argv) {
             exitf(ExitErr::kIO, "failed to save png to \"%s\"", FLAGS_png[0]);
         }
     }
+
+//    SkDebugf("------- HW %d SW %d\n", numHWMask, numSWMask);
 
     return(0);
 }
