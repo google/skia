ebf131727bb5df13ec5347e6a046d33abf2fb6ca
Michael Ludwig <michaelludwig@google.com>
2020-09-28 10:04:06 -0400

    Update SkClipOp::kMax_EnumValue to match non-deprecated values
    Even when expanding clip ops were disabled, the max value allowed the
    fuzzer to produce those disabled values. This should fix the fuzzer to
    only produce valid intersect or difference clip ops.
    
    Bug: chromium:1132687
    Change-Id: I7368ff36a00e6b0e87ecbae5ba647d249299b9ca

--- include/core/SkClipOp.h
+++ include/core/SkClipOp.h
@@ -19,15 +19,14 @@ enum class SkClipOp {
     kXOR_deprecated               = 3,
     kReverseDifference_deprecated = 4,
     kReplace_deprecated           = 5,
-#else
-    kExtraEnumNeedInternallyPleaseIgnoreWillGoAway2   = 2,
-    kExtraEnumNeedInternallyPleaseIgnoreWillGoAway3   = 3,
-    kExtraEnumNeedInternallyPleaseIgnoreWillGoAway4   = 4,
-    kExtraEnumNeedInternallyPleaseIgnoreWillGoAway5   = 5,
-#endif
 
     // Used internally for validation, can only shrink to 1 when the deprecated flag is gone
     kMax_EnumValue = 5,
+#else
+    kMax_EnumValue = 1
+#endif
+
+
 };
 
 #endif
