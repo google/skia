280491a0ab63acce4bf0cee39ac292663a60761e
John Stiles <johnstiles@google.com>
2020-09-29 15:30:30 -0400

    Allow more types of expressions to be directly inlined.
    This significantly reduces the number of temporary variables generated
    by the inliner.
    
    Change-Id: Ifed226ecc87b096ec1e38752c0c38ae32bd31578
    Bug: skia:10737

--- tests/sksl/blend/golden/BlendOverlapStandaloneSettings.glsl
+++ tests/sksl/blend/golden/BlendOverlapStandaloneSettings.glsl
@@ -2,33 +2,27 @@
 out vec4 sk_FragColor;
 in vec4 src, dst;
 void main() {
-    vec4 _3_blend_overlay;
+    vec4 _1_blend_overlay;
     {
+        float _3_blend_overlay_component;
+        {
+            _3_blend_overlay_component = 2.0 * dst.x <= dst.w ? (2.0 * src.x) * dst.x : src.w * dst.w - (2.0 * (dst.w - dst.x)) * (src.w - src.x);
+        }
+        float _4_blend_overlay_component;
+        {
+            _4_blend_overlay_component = 2.0 * dst.y <= dst.w ? (2.0 * src.y) * dst.y : src.w * dst.w - (2.0 * (dst.w - dst.y)) * (src.w - src.y);
+        }
         float _5_blend_overlay_component;
-        vec2 _6_s = src.xw;
-        vec2 _7_d = dst.xw;
         {
-            _5_blend_overlay_component = 2.0 * _7_d.x <= _7_d.y ? (2.0 * _6_s.x) * _7_d.x : _6_s.y * _7_d.y - (2.0 * (_7_d.y - _7_d.x)) * (_6_s.y - _6_s.x);
+            _5_blend_overlay_component = 2.0 * dst.z <= dst.w ? (2.0 * src.z) * dst.z : src.w * dst.w - (2.0 * (dst.w - dst.z)) * (src.w - src.z);
         }
-        float _8_blend_overlay_component;
-        vec2 _9_s = src.yw;
-        vec2 _10_d = dst.yw;
-        {
-            _8_blend_overlay_component = 2.0 * _10_d.x <= _10_d.y ? (2.0 * _9_s.x) * _10_d.x : _9_s.y * _10_d.y - (2.0 * (_10_d.y - _10_d.x)) * (_9_s.y - _9_s.x);
-        }
-        float _11_blend_overlay_component;
-        vec2 _12_s = src.zw;
-        vec2 _13_d = dst.zw;
-        {
-            _11_blend_overlay_component = 2.0 * _13_d.x <= _13_d.y ? (2.0 * _12_s.x) * _13_d.x : _12_s.y * _13_d.y - (2.0 * (_13_d.y - _13_d.x)) * (_12_s.y - _12_s.x);
-        }
-        vec4 _4_result = vec4(_5_blend_overlay_component, _8_blend_overlay_component, _11_blend_overlay_component, src.w + (1.0 - src.w) * dst.w);
+        vec4 _2_result = vec4(_3_blend_overlay_component, _4_blend_overlay_component, _5_blend_overlay_component, src.w + (1.0 - src.w) * dst.w);
 
 
 
-        _4_result.xyz += dst.xyz * (1.0 - src.w) + src.xyz * (1.0 - dst.w);
-        _3_blend_overlay = _4_result;
+        _2_result.xyz += dst.xyz * (1.0 - src.w) + src.xyz * (1.0 - dst.w);
+        _1_blend_overlay = _2_result;
     }
-    sk_FragColor = _3_blend_overlay;
+    sk_FragColor = _1_blend_overlay;
 
 }
