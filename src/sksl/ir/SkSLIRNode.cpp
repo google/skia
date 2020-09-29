ad2941e6ec8255d6504f29f90f1fd44dc47c2e9a
Ethan Nicholas <ethannicholas@google.com>
2020-09-29 17:03:15 -0400

    Revert "Revert "moved SkSL Enum data into IRNode""
    This reverts commit ff22910286a7fe9cdb2476bf2f5261bb93fd614b.
    
    Change-Id: I86619819aae169a2cb8d59ad7ccecf26423f2aa9

--- src/sksl/ir/SkSLIRNode.cpp
+++ src/sksl/ir/SkSLIRNode.cpp
@@ -23,6 +23,11 @@ IRNode::IRNode(int offset, int kind, const BoolLiteralData& data)
 , fKind(kind)
 , fData(data) {}
 
+IRNode::IRNode(int offset, int kind, const EnumData& data)
+: fOffset(offset)
+, fKind(kind)
+, fData(data) {}
+
 IRNode::IRNode(int offset, int kind, const IntLiteralData& data)
 : fOffset(offset)
 , fKind(kind)
