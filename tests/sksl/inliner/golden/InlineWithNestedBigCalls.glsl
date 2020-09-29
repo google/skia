36b433967bbc48278aa5f683558069f8a816533e
John Stiles <johnstiles@google.com>
2020-09-29 12:19:09 -0400

    Rescan on any update; bad workaround for http://review.skia.org/320274
    Change-Id: I33e3f0d1b6eb6d2830b9a8cc5809c146dbf73bfc

--- tests/sksl/inliner/golden/InlineWithNestedBigCalls.glsl
+++ tests/sksl/inliner/golden/InlineWithNestedBigCalls.glsl
@@ -1,108 +1,85 @@
-### Compilation failed:
 
-=================================================================
-==94626==ERROR: AddressSanitizer: heap-use-after-free on address 0x60200001a038 at pc 0x000109924365 bp 0x7ffee63eea60 sp 0x7ffee63eea58
-READ of size 8 at 0x60200001a038 thread T0
-    #0 0x109924364 in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator->() const memory:2592
-    #1 0x10992303e in SkSL::BasicBlock::dump() const SkSLCFGGenerator.cpp:67
-    #2 0x10992249a in SkSL::CFG::dump() const SkSLCFGGenerator.cpp:58
-    #3 0x1099cf021 in SkSL::Compiler::scanCFG(SkSL::FunctionDefinition&) SkSLCompiler.cpp:1489
-    #4 0x1099dd8e7 in SkSL::Compiler::optimize(SkSL::Program&) SkSLCompiler.cpp:1688
-    #5 0x1099da976 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1671
-    #6 0x10980ec76 in main SkSLMain.cpp:242
-    #7 0x7fff7205ecc8 in start+0x0 (libdyld.dylib:x86_64+0x1acc8)
+out vec4 sk_FragColor;
+uniform float val;
+void main() {
+    float _1_x = val;
+    {
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        ++_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        --_1_x;
+        _1_x = 456.0;
+    }
 
-0x60200001a038 is located 8 bytes inside of 16-byte region [0x60200001a030,0x60200001a040)
-freed by thread T0 here:
-    #0 0x10b567c6d in wrap__ZdlPv+0x7d (libclang_rt.asan_osx_dynamic.dylib:x86_64h+0x55c6d)
-    #1 0x10982c38c in std::__1::_DeallocateCaller::__do_call(void*) new:334
-    #2 0x10982c330 in std::__1::_DeallocateCaller::__do_deallocate_handle_size(void*, unsigned long) new:292
-    #3 0x1099f8d9c in std::__1::_DeallocateCaller::__do_deallocate_handle_size_align(void*, unsigned long, unsigned long) new:268
-    #4 0x1099f8d2c in std::__1::__libcpp_deallocate(void*, unsigned long, unsigned long) new:340
-    #5 0x109a0d211 in std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >::deallocate(std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, unsigned long) memory:1872
-    #6 0x109a0cc6c in std::__1::allocator_traits<std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::deallocate(std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, unsigned long) memory:1594
-    #7 0x109a2a2db in std::__1::__vector_base<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~__vector_base() vector:464
-    #8 0x109febd9d in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~vector() vector:555
-    #9 0x109fe5e71 in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~vector() vector:550
-    #10 0x109fe5da0 in SkSL::IRNode::~IRNode() SkSLIRNode.cpp:51
-    #11 0x109a17684 in SkSL::Expression::~Expression() SkSLExpression.h:27
-    #12 0x109c49b54 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #13 0x109c47c61 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #14 0x109c47cc1 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #15 0x109a925c1 in std::__1::default_delete<SkSL::Expression>::operator()(SkSL::Expression*) const memory:2368
-    #16 0x109a91fad in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::reset(SkSL::Expression*) memory:2623
-    #17 0x1099bdc8d in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator=(std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&) memory:2542
-    #18 0x1099bfa54 in SkSL::delete_left(SkSL::BasicBlock*, std::__1::__wrap_iter<SkSL::BasicBlock::Node*>*, bool*, bool*) SkSLCompiler.cpp:719
-    #19 0x1099b9a05 in SkSL::Compiler::simplifyExpression(std::__1::unordered_map<SkSL::Variable const*, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, std::__1::hash<SkSL::Variable const*>, std::__1::equal_to<SkSL::Variable const*>, std::__1::allocator<std::__1::pair<SkSL::Variable const* const, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*> > >&, SkSL::BasicBlock&, std::__1::__wrap_iter<SkSL::BasicBlock::Node*>*, std::__1::unordered_set<SkSL::Variable const*, std::__1::hash<SkSL::Variable const*>, std::__1::equal_to<SkSL::Variable const*>, std::__1::allocator<SkSL::Variable const*> >*, bool*, bool*) SkSLCompiler.cpp:906
-    #20 0x1099cf18f in SkSL::Compiler::scanCFG(SkSL::FunctionDefinition&) SkSLCompiler.cpp:1492
-    #21 0x1099dd8e7 in SkSL::Compiler::optimize(SkSL::Program&) SkSLCompiler.cpp:1688
-    #22 0x1099da976 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1671
-    #23 0x10980ec76 in main SkSLMain.cpp:242
-    #24 0x7fff7205ecc8 in start+0x0 (libdyld.dylib:x86_64+0x1acc8)
+    float _3_x = 456.0;
+    {
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        ++_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        --_3_x;
+        _3_x = 123.0;
+    }
 
-previously allocated by thread T0 here:
-    #0 0x10b56784d in wrap__Znwm+0x7d (libclang_rt.asan_osx_dynamic.dylib:x86_64h+0x5584d)
-    #1 0x1099f4d14 in std::__1::__libcpp_allocate(unsigned long, unsigned long) new:253
-    #2 0x109a0bcf1 in std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >::allocate(unsigned long, void const*) memory:1869
-    #3 0x109a0b7e8 in std::__1::allocator_traits<std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::allocate(std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&, unsigned long) memory:1586
-    #4 0x109a0b44c in std::__1::__split_buffer<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&>::__split_buffer(unsigned long, unsigned long, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&) __split_buffer:318
-    #5 0x109a0a52b in std::__1::__split_buffer<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&>::__split_buffer(unsigned long, unsigned long, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&) __split_buffer:317
-    #6 0x109a28531 in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::reserve(unsigned long) vector:1586
-    #7 0x109c468a4 in SkSL::BinaryExpression::BinaryExpression(int, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*) SkSLBinaryExpression.h:55
-    #8 0x109bb3647 in SkSL::BinaryExpression::BinaryExpression(int, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*) SkSLBinaryExpression.h:54
-    #9 0x109d6765b in std::__1::__unique_if<SkSL::BinaryExpression>::__unique_single std::__1::make_unique<SkSL::BinaryExpression, int&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*>(int&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&, SkSL::Token::Kind&&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&, SkSL::Type const*&&) memory:3033
-    #10 0x109d6ea24 in SkSL::Inliner::inlineStatement(int, std::__1::unordered_map<SkSL::Variable const*, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::hash<SkSL::Variable const*>, std::__1::equal_to<SkSL::Variable const*>, std::__1::allocator<std::__1::pair<SkSL::Variable const* const, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > > >*, SkSL::SymbolTable*, SkSL::Expression const*, bool, SkSL::Statement const&) SkSLInliner.cpp:487
-    #11 0x109d7f684 in SkSL::Inliner::inlineCall(SkSL::FunctionCall*, SkSL::SymbolTable*) SkSLInliner.cpp:686
-    #12 0x109bb6a8f in SkSL::IRGenerator::call(int, SkSL::FunctionDeclaration const&, std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >) SkSLIRGenerator.cpp:2115
-    #13 0x109bba663 in SkSL::IRGenerator::call(int, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >) SkSLIRGenerator.cpp:2206
-    #14 0x109bd9561 in SkSL::IRGenerator::convertCallExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:2742
-    #15 0x109ba521a in SkSL::IRGenerator::convertExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1318
-    #16 0x109bd925e in SkSL::IRGenerator::convertCallExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:2736
-    #17 0x109ba521a in SkSL::IRGenerator::convertExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1318
-    #18 0x109bd9a71 in SkSL::IRGenerator::convertFieldExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:2746
-    #19 0x109ba5276 in SkSL::IRGenerator::convertExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1320
-    #20 0x109bd6832 in SkSL::IRGenerator::convertBinaryExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1920
-    #21 0x109ba4fa9 in SkSL::IRGenerator::convertExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1313
-    #22 0x109b8c1c8 in SkSL::IRGenerator::convertExpressionStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:697
-    #23 0x109b7f8eb in SkSL::IRGenerator::convertSingleStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:243
-    #24 0x109b90453 in SkSL::IRGenerator::convertStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:264
-    #25 0x109b80d73 in SkSL::IRGenerator::convertBlock(SkSL::ASTNode const&) SkSLIRGenerator.cpp:285
-    #26 0x109bc39bd in SkSL::IRGenerator::convertFunction(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1084
-    #27 0x109c28413 in SkSL::IRGenerator::convertProgram(SkSL::Program::Kind, char const*, unsigned long, std::__1::vector<std::__1::unique_ptr<SkSL::ProgramElement, std::__1::default_delete<SkSL::ProgramElement> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ProgramElement, std::__1::default_delete<SkSL::ProgramElement> > > >*) SkSLIRGenerator.cpp:2852
-    #28 0x1099da0f7 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1658
-    #29 0x10980ec76 in main SkSLMain.cpp:242
+    sk_FragColor = vec4(123.0);
 
-SUMMARY: AddressSanitizer: heap-use-after-free memory:2592 in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator->() const
-Shadow bytes around the buggy address:
-  0x1c04000033b0: fa fa fd fa fa fa fd fa fa fa fd fa fa fa fd fa
-  0x1c04000033c0: fa fa fd fa fa fa fd fa fa fa fd fa fa fa fd fa
-  0x1c04000033d0: fa fa fd fa fa fa fd fa fa fa fd fa fa fa fd fd
-  0x1c04000033e0: fa fa fd fa fa fa fd fa fa fa 00 fa fa fa fd fd
-  0x1c04000033f0: fa fa fd fd fa fa fd fa fa fa 00 fa fa fa fd fd
-=>0x1c0400003400: fa fa 00 00 fa fa fd[fd]fa fa fd fa fa fa fd fa
-  0x1c0400003410: fa fa fd fa fa fa fd fa fa fa 00 fa fa fa fd fa
-  0x1c0400003420: fa fa 00 fa fa fa fd fd fa fa 00 00 fa fa 00 00
-  0x1c0400003430: fa fa fd fd fa fa fd fd fa fa 00 fa fa fa 00 00
-  0x1c0400003440: fa fa 00 fa fa fa fd fd fa fa fd fd fa fa fa fa
-  0x1c0400003450: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
-Shadow byte legend (one shadow byte represents 8 application bytes):
-  Addressable:           00
-  Partially addressable: 01 02 03 04 05 06 07 
-  Heap left redzone:       fa
-  Freed heap region:       fd
-  Stack left redzone:      f1
-  Stack mid redzone:       f2
-  Stack right redzone:     f3
-  Stack after return:      f5
-  Stack use after scope:   f8
-  Global redzone:          f9
-  Global init order:       f6
-  Poisoned by user:        f7
-  Container overflow:      fc
-  Array cookie:            ac
-  Intra object redzone:    bb
-  ASan internal:           fe
-  Left alloca redzone:     ca
-  Right alloca redzone:    cb
-  Shadow gap:              cc
-==94626==ABORTING
+}
--- tests/sksl/shared/golden/SwitchContainingDeadCode.glsl
+++ tests/sksl/shared/golden/SwitchContainingDeadCode.glsl
@@ -1,102 +1,13 @@
-### Compilation failed:
 
-=================================================================
-==95165==ERROR: AddressSanitizer: heap-use-after-free on address 0x602000019e38 at pc 0x0001097fb365 bp 0x7ffee6517a60 sp 0x7ffee6517a58
-READ of size 8 at 0x602000019e38 thread T0
-    #0 0x1097fb364 in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator->() const memory:2592
-    #1 0x1097fa03e in SkSL::BasicBlock::dump() const SkSLCFGGenerator.cpp:67
-    #2 0x1097f949a in SkSL::CFG::dump() const SkSLCFGGenerator.cpp:58
-    #3 0x1098a6021 in SkSL::Compiler::scanCFG(SkSL::FunctionDefinition&) SkSLCompiler.cpp:1489
-    #4 0x1098b48e7 in SkSL::Compiler::optimize(SkSL::Program&) SkSLCompiler.cpp:1688
-    #5 0x1098b1976 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1671
-    #6 0x1096e5c76 in main SkSLMain.cpp:242
-    #7 0x7fff7205ecc8 in start+0x0 (libdyld.dylib:x86_64+0x1acc8)
-
-0x602000019e38 is located 8 bytes inside of 16-byte region [0x602000019e30,0x602000019e40)
-freed by thread T0 here:
-    #0 0x10b439c6d in wrap__ZdlPv+0x7d (libclang_rt.asan_osx_dynamic.dylib:x86_64h+0x55c6d)
-    #1 0x10970338c in std::__1::_DeallocateCaller::__do_call(void*) new:334
-    #2 0x109703330 in std::__1::_DeallocateCaller::__do_deallocate_handle_size(void*, unsigned long) new:292
-    #3 0x1098cfd9c in std::__1::_DeallocateCaller::__do_deallocate_handle_size_align(void*, unsigned long, unsigned long) new:268
-    #4 0x1098cfd2c in std::__1::__libcpp_deallocate(void*, unsigned long, unsigned long) new:340
-    #5 0x1098e4211 in std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >::deallocate(std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, unsigned long) memory:1872
-    #6 0x1098e3c6c in std::__1::allocator_traits<std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::deallocate(std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, unsigned long) memory:1594
-    #7 0x1099012db in std::__1::__vector_base<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~__vector_base() vector:464
-    #8 0x109ec2d9d in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~vector() vector:555
-    #9 0x109ebce71 in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::~vector() vector:550
-    #10 0x109ebcda0 in SkSL::IRNode::~IRNode() SkSLIRNode.cpp:51
-    #11 0x1098ee684 in SkSL::Expression::~Expression() SkSLExpression.h:27
-    #12 0x109b20b54 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #13 0x109b1ec61 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #14 0x109b1ecc1 in SkSL::BinaryExpression::~BinaryExpression() SkSLBinaryExpression.h:49
-    #15 0x1099695c1 in std::__1::default_delete<SkSL::Expression>::operator()(SkSL::Expression*) const memory:2368
-    #16 0x109968fad in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::reset(SkSL::Expression*) memory:2623
-    #17 0x109894c8d in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator=(std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&) memory:2542
-    #18 0x109896a54 in SkSL::delete_left(SkSL::BasicBlock*, std::__1::__wrap_iter<SkSL::BasicBlock::Node*>*, bool*, bool*) SkSLCompiler.cpp:719
-    #19 0x109890a05 in SkSL::Compiler::simplifyExpression(std::__1::unordered_map<SkSL::Variable const*, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*, std::__1::hash<SkSL::Variable const*>, std::__1::equal_to<SkSL::Variable const*>, std::__1::allocator<std::__1::pair<SkSL::Variable const* const, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >*> > >&, SkSL::BasicBlock&, std::__1::__wrap_iter<SkSL::BasicBlock::Node*>*, std::__1::unordered_set<SkSL::Variable const*, std::__1::hash<SkSL::Variable const*>, std::__1::equal_to<SkSL::Variable const*>, std::__1::allocator<SkSL::Variable const*> >*, bool*, bool*) SkSLCompiler.cpp:906
-    #20 0x1098a618f in SkSL::Compiler::scanCFG(SkSL::FunctionDefinition&) SkSLCompiler.cpp:1492
-    #21 0x1098b48e7 in SkSL::Compiler::optimize(SkSL::Program&) SkSLCompiler.cpp:1688
-    #22 0x1098b1976 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1671
-    #23 0x1096e5c76 in main SkSLMain.cpp:242
-    #24 0x7fff7205ecc8 in start+0x0 (libdyld.dylib:x86_64+0x1acc8)
-
-previously allocated by thread T0 here:
-    #0 0x10b43984d in wrap__Znwm+0x7d (libclang_rt.asan_osx_dynamic.dylib:x86_64h+0x5584d)
-    #1 0x1098cbd14 in std::__1::__libcpp_allocate(unsigned long, unsigned long) new:253
-    #2 0x1098e2cf1 in std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >::allocate(unsigned long, void const*) memory:1869
-    #3 0x1098e27e8 in std::__1::allocator_traits<std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::allocate(std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&, unsigned long) memory:1586
-    #4 0x1098e244c in std::__1::__split_buffer<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&>::__split_buffer(unsigned long, unsigned long, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&) __split_buffer:318
-    #5 0x1098e152b in std::__1::__split_buffer<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&>::__split_buffer(unsigned long, unsigned long, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > >&) __split_buffer:317
-    #6 0x1098ff531 in std::__1::vector<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, std::__1::allocator<std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> > > >::reserve(unsigned long) vector:1586
-    #7 0x109b1d8a4 in SkSL::BinaryExpression::BinaryExpression(int, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*) SkSLBinaryExpression.h:55
-    #8 0x109a8a647 in SkSL::BinaryExpression::BinaryExpression(int, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*) SkSLBinaryExpression.h:54
-    #9 0x109adaa2b in std::__1::__unique_if<SkSL::BinaryExpression>::__unique_single std::__1::make_unique<SkSL::BinaryExpression, int const&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Token::Kind&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >, SkSL::Type const*&>(int const&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&, SkSL::Token::Kind&, std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >&&, SkSL::Type const*&) memory:3033
-    #10 0x109aaf8a0 in SkSL::IRGenerator::convertBinaryExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1962
-    #11 0x109a7bfa9 in SkSL::IRGenerator::convertExpression(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1313
-    #12 0x109a631c8 in SkSL::IRGenerator::convertExpressionStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:697
-    #13 0x109a568eb in SkSL::IRGenerator::convertSingleStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:243
-    #14 0x109a67453 in SkSL::IRGenerator::convertStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:264
-    #15 0x109a5f610 in SkSL::IRGenerator::convertSwitch(SkSL::ASTNode const&) SkSLIRGenerator.cpp:682
-    #16 0x109a566e7 in SkSL::IRGenerator::convertSingleStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:232
-    #17 0x109a67453 in SkSL::IRGenerator::convertStatement(SkSL::ASTNode const&) SkSLIRGenerator.cpp:264
-    #18 0x109a57d73 in SkSL::IRGenerator::convertBlock(SkSL::ASTNode const&) SkSLIRGenerator.cpp:285
-    #19 0x109a9a9bd in SkSL::IRGenerator::convertFunction(SkSL::ASTNode const&) SkSLIRGenerator.cpp:1084
-    #20 0x109aff413 in SkSL::IRGenerator::convertProgram(SkSL::Program::Kind, char const*, unsigned long, std::__1::vector<std::__1::unique_ptr<SkSL::ProgramElement, std::__1::default_delete<SkSL::ProgramElement> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ProgramElement, std::__1::default_delete<SkSL::ProgramElement> > > >*) SkSLIRGenerator.cpp:2852
-    #21 0x1098b10f7 in SkSL::Compiler::convertProgram(SkSL::Program::Kind, SkSL::String, SkSL::Program::Settings const&, std::__1::vector<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> >, std::__1::allocator<std::__1::unique_ptr<SkSL::ExternalValue, std::__1::default_delete<SkSL::ExternalValue> > > > const*) SkSLCompiler.cpp:1658
-    #22 0x1096e5c76 in main SkSLMain.cpp:242
-    #23 0x7fff7205ecc8 in start+0x0 (libdyld.dylib:x86_64+0x1acc8)
-
-SUMMARY: AddressSanitizer: heap-use-after-free memory:2592 in std::__1::unique_ptr<SkSL::Expression, std::__1::default_delete<SkSL::Expression> >::operator->() const
-Shadow bytes around the buggy address:
-  0x1c0400003370: fa fa 00 fa fa fa fd fa fa fa fd fd fa fa fd fd
-  0x1c0400003380: fa fa fd fd fa fa 00 fa fa fa fd fa fa fa fd fa
-  0x1c0400003390: fa fa fd fa fa fa fd fa fa fa 00 fa fa fa fd fd
-  0x1c04000033a0: fa fa fd fa fa fa fd fd fa fa 00 fa fa fa fd fa
-  0x1c04000033b0: fa fa fd fa fa fa fd fd fa fa 00 fa fa fa fd fd
-=>0x1c04000033c0: fa fa fd fa fa fa fd[fd]fa fa 00 fa fa fa fd fd
-  0x1c04000033d0: fa fa 00 fa fa fa 00 fa fa fa 00 00 fa fa 00 fa
-  0x1c04000033e0: fa fa fd fa fa fa 00 00 fa fa fd fd fa fa 00 00
-  0x1c04000033f0: fa fa 00 00 fa fa 00 00 fa fa fd fd fa fa fd fd
-  0x1c0400003400: fa fa fd fd fa fa 00 00 fa fa fd fd fa fa 00 00
-  0x1c0400003410: fa fa fd fd fa fa fd fd fa fa fd fd fa fa fd fd
-Shadow byte legend (one shadow byte represents 8 application bytes):
-  Addressable:           00
-  Partially addressable: 01 02 03 04 05 06 07 
-  Heap left redzone:       fa
-  Freed heap region:       fd
-  Stack left redzone:      f1
-  Stack mid redzone:       f2
-  Stack right redzone:     f3
-  Stack after return:      f5
-  Stack use after scope:   f8
-  Global redzone:          f9
-  Global init order:       f6
-  Poisoned by user:        f7
-  Container overflow:      fc
-  Array cookie:            ac
-  Intra object redzone:    bb
-  ASan internal:           fe
-  Left alloca redzone:     ca
-  Right alloca redzone:    cb
-  Shadow gap:              cc
-==95165==ABORTING
+out vec4 sk_FragColor;
+void main() {
+    switch (int(sqrt(2.0))) {
+        case 0:
+            ;
+        case 1:
+            ;
+        default:
+            ;
+    }
+    sk_FragColor = vec4(2.0);
+}
