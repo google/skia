b0b3fa2905449527325ed81c564f76136fe7f5b4
John Stiles <johnstiles@google.com>
2020-09-30 13:19:17 -0400

    Inline comprex expressions directly when only read once.
    In practice, this doesn't actually improve anything. I was hoping to
    see improvement in the blend expressions, but the read-once check occurs
    on the function before it is optimized. So cases like "guarded_divide"
    see two reads (even though it will later optimize away one of them).
    
    Change-Id: Ib346a4fdcc210e79e0a66856e82446070d64d6a2
    Bug: skia:10786

--- tests/sksl/inliner/golden/TrivialArgumentsInlineDirectly.glsl
+++ tests/sksl/inliner/golden/TrivialArgumentsInlineDirectly.glsl
@@ -56,45 +56,38 @@ void main() {
     }
 
 
-    bool _1_b = !true;
     {
-        sk_FragColor = _1_b ? sk_FragColor.xxxx : sk_FragColor.yyyy;
+        sk_FragColor = !true ? sk_FragColor.xxxx : sk_FragColor.yyyy;
     }
 
 
-    float _2_h = -s.h;
     {
-        sk_FragColor = vec4(_2_h);
+        sk_FragColor = vec4(-s.h);
     }
 
 
-    vec2 _3_h2 = s.ah4[ui].yw;
     {
-        sk_FragColor = _3_h2.xyxy;
+        sk_FragColor = s.ah4[ui].ywyw;
     }
 
 
-    vec3 _4_h3 = s.h4.yyy + s.h4.zzz;
     {
-        sk_FragColor = _4_h3.xyzx;
+        sk_FragColor = (s.h4.yyy + s.h4.zzz).xyzx;
     }
 
 
-    vec3 _5_h3 = vec3(s.h);
     {
-        sk_FragColor = _5_h3.xyzx;
+        sk_FragColor = vec3(s.h).xyzx;
     }
 
 
-    vec4 _6_h4 = vec4(s.h);
     {
-        sk_FragColor = _6_h4;
+        sk_FragColor = vec4(s.h);
     }
 
 
-    vec4 _7_h4 = vec3(s.h4.y, 0.0, 1.0).xyyz;
     {
-        sk_FragColor = _7_h4;
+        sk_FragColor = vec3(s.h4.y, 0.0, 1.0).xyyz;
     }
 
 
