aac5728a8260858316f2e0d93f2c06817a3d6db5
Robert Phillips <robertphillips@google.com>
2020-09-30 10:23:38 -0400

    Make GrRRectBlurEffect use the thread-safe uniquely-keyed view cache (take 2)
    This yields a ~8% performance improvement on OOP-R/DDL-like rendering of the simpleblurroundrect GM.
    
    Bug: 1108408
    Change-Id: I710c75b73a395b2280abf0c5af528e0f6731326d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/311720
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/320917

--- src/gpu/GrProxyProvider.h
+++ src/gpu/GrProxyProvider.h
@@ -61,7 +61,7 @@ public:
 
     /**
      * A helper that uses findOrCreateProxyByUniqueKey() to find a proxy and if found creates a view
-     *a view for the found proxy using the passed in origin and color type. It is assumed that if
+     * a view for the found proxy using the passed in origin and color type. It is assumed that if
      * the proxy is renderable then it was created by GrRenderTargetContext::MakeWithFallback and
      * the fallback color type will be used to create the view.
      */
