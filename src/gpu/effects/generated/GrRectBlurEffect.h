a6919c655ec6f96a6d51ccc33d6dd2ab292e1434
Robert Phillips <robertphillips@google.com>
2020-09-30 11:20:13 -0400

    Make GrRectBlurEffect use the thread-safe view cache
    This, maybe, yields a slight perf improvement for the blur_matrix_rect
    GM but, regardless, is good form.
    
    Bug: 1108408
    Change-Id: I6d589cc0f08f4264419ab33f27ac5a503cd036e8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/320933
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- src/gpu/effects/generated/GrRectBlurEffect.h
+++ src/gpu/effects/generated/GrRectBlurEffect.h
@@ -25,14 +25,17 @@
 #include "src/gpu/GrProxyProvider.h"
 #include "src/gpu/GrRecordingContextPriv.h"
 #include "src/gpu/GrShaderCaps.h"
+#include "src/gpu/GrThreadSafeUniquelyKeyedProxyViewCache.h"
 #include "src/gpu/effects/GrTextureEffect.h"
 
 #include "src/gpu/GrFragmentProcessor.h"
 
 class GrRectBlurEffect : public GrFragmentProcessor {
 public:
-    static std::unique_ptr<GrFragmentProcessor> MakeIntegralFP(GrRecordingContext* context,
+    static std::unique_ptr<GrFragmentProcessor> MakeIntegralFP(GrRecordingContext* rContext,
                                                                float sixSigma) {
+        auto threadSafeViewCache = rContext->priv().threadSafeViewCache();
+
         int width = SkCreateIntegralTable(sixSigma, nullptr);
 
         static const GrUniqueKey::Domain kDomain = GrUniqueKey::GenerateDomain();
@@ -43,11 +46,10 @@ public:
 
         SkMatrix m = SkMatrix::Scale(width / sixSigma, 1.f);
 
-        GrProxyProvider* proxyProvider = context->priv().proxyProvider();
-        if (sk_sp<GrTextureProxy> proxy = proxyProvider->findOrCreateProxyByUniqueKey(key)) {
-            GrSwizzle swizzle = context->priv().caps()->getReadSwizzle(proxy->backendFormat(),
-                                                                       GrColorType::kAlpha_8);
-            GrSurfaceProxyView view{std::move(proxy), kTopLeft_GrSurfaceOrigin, swizzle};
+        GrSurfaceProxyView view = threadSafeViewCache->find(key);
+
+        if (view) {
+            SkASSERT(view.origin() == kTopLeft_GrSurfaceOrigin);
             return GrTextureEffect::Make(std::move(view), kPremul_SkAlphaType, m,
                                          GrSamplerState::Filter::kLinear);
         }
@@ -57,13 +59,15 @@ public:
             return {};
         }
 
-        GrBitmapTextureMaker maker(context, bitmap, GrImageTexGenPolicy::kNew_Uncached_Budgeted);
-        auto view = maker.view(GrMipmapped::kNo);
+        GrBitmapTextureMaker maker(rContext, bitmap, GrImageTexGenPolicy::kNew_Uncached_Budgeted);
+        view = maker.view(GrMipmapped::kNo);
         if (!view) {
             return {};
         }
+
+        view = threadSafeViewCache->add(key, view);
+
         SkASSERT(view.origin() == kTopLeft_GrSurfaceOrigin);
-        proxyProvider->assignUniqueKeyToProxy(key, view.asTextureProxy());
         return GrTextureEffect::Make(std::move(view), kPremul_SkAlphaType, m,
                                      GrSamplerState::Filter::kLinear);
     }
