ed7536ff8d22845a84f03edda16a0706f1b15ae0
Brian Salomon <bsalomon@google.com>
2020-09-30 21:04:11 -0400

    Start removal of tool usage of SkSurface::MakeFromBackendTextureAsRenderTarget.
    Adds a new helper that creates a GrBackendRenderTarget using
    GrGpu and then wraps it in a SkSurface. Uses the SkSurface
    release proc to delete the BERT using GrGpu.
    
    Upgrades GrGpu::createTestingOnlyBackendRenderTarget to create MSAA
    buffers and to clear to an initial color.
    
    Updates many tests/tool to call sites to use the helper instead of
    SkSurface::MakeFromBackendTextureAsRenderTarget.
    
    Bug: skia:9832
    
    Change-Id: I73a8f0ce09dc6523729af0814464c6b6657fda06

--- src/image/SkSurface_Gpu.cpp
+++ src/image/SkSurface_Gpu.cpp
@@ -618,11 +618,10 @@ sk_sp<SkSurface> SkSurface::MakeFromBackendRenderTarget(GrContext* context,
                                                         const SkSurfaceProps* props,
                                                         SkSurface::RenderTargetReleaseProc relProc,
                                                         SkSurface::ReleaseContext releaseContext) {
-    SkScopeExit callProc([&] {
-        if (relProc) {
-            relProc(releaseContext);
-        }
-    });
+    sk_sp<GrRefCntedCallback> releaseHelper;
+    if (relProc) {
+        releaseHelper.reset(new GrRefCntedCallback(relProc, releaseContext));
+    }
 
     if (!context) {
         return nullptr;
@@ -638,13 +637,15 @@ sk_sp<SkSurface> SkSurface::MakeFromBackendRenderTarget(GrContext* context,
         return nullptr;
     }
 
-    auto rtc = GrRenderTargetContext::MakeFromBackendRenderTarget(
-            context, grColorType, std::move(colorSpace), rt, origin, props, relProc,
-            releaseContext);
+    auto rtc = GrRenderTargetContext::MakeFromBackendRenderTarget(context,
+                                                                  grColorType,
+                                                                  std::move(colorSpace),
+                                                                  rt,
+                                                                  origin,
+                                                                  props, std::move(releaseHelper));
     if (!rtc) {
         return nullptr;
     }
-    callProc.clear();
 
     auto device = SkGpuDevice::Make(context, std::move(rtc), SkGpuDevice::kUninit_InitContents);
     if (!device) {
@@ -760,12 +761,12 @@ sk_sp<SkSurface> SkSurface::MakeFromAHardwareBuffer(GrContext* context,
 }
 #endif
 
-void SkSurface::flushAndSubmit() {
+void SkSurface::flushAndSubmit(bool syncCpu) {
     this->flush(BackendSurfaceAccess::kNoAccess, GrFlushInfo());
 
     auto direct = GrAsDirectContext(this->recordingContext());
     if (direct) {
-        direct->submit();
+        direct->submit(syncCpu);
     }
 }
 
