e3b35e84197865bc841b8a72a6064ff94fcbe6e7
Jim Van Verth <jvanverth@google.com>
2020-10-02 14:57:40 -0400

    Add support for light position relative to device space origin
    Bug: skia:10781
    Change-Id: I14d530f006e5541ed6e22bfbeb29c7441c21fb8e

--- include/private/SkShadowFlags.h
+++ include/private/SkShadowFlags.h
@@ -16,8 +16,10 @@ enum SkShadowFlags {
     kTransparentOccluder_ShadowFlag = 0x01,
     /** Don't try to use analytic shadows. */
     kGeometricOnly_ShadowFlag = 0x02,
+    /** Light position is relative to device space origin after total CTM is applied. */
+    kRelativeLightPosition_ShadowFlag = 0x04,
     /** mask for all shadow flags */
-    kAll_ShadowFlag = 0x03
+    kAll_ShadowFlag = 0x07
 };
 
 #endif
