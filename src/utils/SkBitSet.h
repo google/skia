4a13ba18fe9ae89345e9acddd657fe694bbf6241
John Stiles <johnstiles@google.com>
2020-10-05 16:02:38 -0400

    Update message on static assertion.
    Change-Id: I4d71b1cab2812b43de61cf066568e15c05f9dc24

--- src/utils/SkBitSet.h
+++ src/utils/SkBitSet.h
@@ -143,8 +143,8 @@ private:
 
     using Chunk = uint32_t;
     static_assert(std::numeric_limits<Chunk>::radix == 2);
+    static_assert(ChunkBits == sizeof(Chunk)*CHAR_BIT, "SkBitSet must use every bit in a Chunk");
     static constexpr size_t ChunkBits = std::numeric_limits<Chunk>::digits;
-    static_assert(ChunkBits == sizeof(Chunk)*CHAR_BIT, "It would work, but don't waste bits.");
     std::unique_ptr<Chunk, SkFunctionWrapper<void(void*), sk_free>> fChunks;
 
     Chunk* chunkFor(size_t index) const {
