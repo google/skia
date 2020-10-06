3027103bb9e2e9d37485ca54b9b53a5796ff6a88
Matthew Leibowitz <mattleibow@live.com>
2020-10-06 23:49:08 +0200

    Parse out the orientation in the SubIFD of EXIF
    Orientation information is sometimes stored in
    the SubIFD section of EXIF, so read that. This is
    just a matter of searching for the SubIFD offset
    value in the EXIF tags and then parsing the
    values from there onwards. The data format is
    the same as the EXIF data.
    
    Change-Id: I5384ffc1c4a9a0c7d3fc8510ef4da2f278cb8b97

--- src/codec/SkParseEncodedOrigin.cpp
+++ src/codec/SkParseEncodedOrigin.cpp
@@ -8,6 +8,56 @@
 #include "include/core/SkTypes.h"
 #include "src/codec/SkCodecPriv.h"
 
+bool SkParseEncodedOrigin(const uint8_t* exifData, size_t data_length, uint64_t offset,
+                          bool littleEndian, SkEncodedOrigin* orientation) {
+    // Require that the marker is at least large enough to contain the number of entries.
+    if (data_length < offset + 2) {
+        return false;
+    }
+    uint32_t numEntries = get_endian_short(exifData + offset, littleEndian);
+
+    // Tag (2 bytes), Datatype (2 bytes), Number of elements (4 bytes), Data (4 bytes)
+    const uint32_t kEntrySize = 12;
+    const auto max = SkTo<uint32_t>((data_length - offset - 2) / kEntrySize);
+    numEntries = std::min(numEntries, max);
+
+    // Advance the data to the start of the entries.
+    auto data = exifData;
+    data += offset + 2;
+
+    const uint16_t kOriginTag = 0x112;
+    const uint16_t kOriginType = 3; // uint16_t
+    const uint16_t kSubIFDOffsetTag = 0x8769;
+    const uint16_t kSubIFDOffsetType = 4; // uint32_t
+
+    uint32_t subIFDOffset = 0;
+
+    for (uint32_t i = 0; i < numEntries; i++, data += kEntrySize) {
+        uint16_t tag = get_endian_short(data, littleEndian);
+        uint16_t type = get_endian_short(data + 2, littleEndian);
+        uint32_t count = get_endian_int(data + 4, littleEndian);
+
+        if (kOriginTag == tag && kOriginType == type && 1 == count) {
+            uint16_t val = get_endian_short(data + 8, littleEndian);
+            if (0 < val && val <= kLast_SkEncodedOrigin) {
+                *orientation = (SkEncodedOrigin)val;
+                return true;
+            }
+        } else if (kSubIFDOffsetTag == tag && kSubIFDOffsetType == type && 1 == count) {
+            uint32_t val = get_endian_int(data + 8, littleEndian);
+            if (0 < val && val < data_length) {
+                subIFDOffset = val;
+            }
+        }
+    }
+
+    if (subIFDOffset > 0) {
+        return SkParseEncodedOrigin(exifData, data_length, subIFDOffset, littleEndian, orientation);
+    }
+
+    return false;
+}
+
 bool SkParseEncodedOrigin(const uint8_t* data, size_t data_length, SkEncodedOrigin* orientation) {
     SkASSERT(orientation);
     bool littleEndian;
@@ -16,46 +66,10 @@ bool SkParseEncodedOrigin(const uint8_t* data, size_t data_length, SkEncodedOrig
         return false;
     }
 
-    auto getEndianInt = [](const uint8_t* data, bool littleEndian) -> uint32_t {
-        if (littleEndian) {
-            return (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | (data[0]);
-        }
-
-        return (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | (data[3]);
-    };
-
     // Get the offset from the start of the marker.
     // Though this only reads four bytes, use a larger int in case it overflows.
-    uint64_t offset = getEndianInt(data + 4, littleEndian);
+    uint64_t offset = get_endian_int(data + 4, littleEndian);
 
-    // Require that the marker is at least large enough to contain the number of entries.
-    if (data_length < offset + 2) {
-        return false;
-    }
-    uint32_t numEntries = get_endian_short(data + offset, littleEndian);
-
-    // Tag (2 bytes), Datatype (2 bytes), Number of elements (4 bytes), Data (4 bytes)
-    const uint32_t kEntrySize = 12;
-    const auto max = SkTo<uint32_t>((data_length - offset - 2) / kEntrySize);
-    numEntries = std::min(numEntries, max);
-
-    // Advance the data to the start of the entries.
-    data += offset + 2;
-
-    const uint16_t kOriginTag = 0x112;
-    const uint16_t kOriginType = 3;
-    for (uint32_t i = 0; i < numEntries; i++, data += kEntrySize) {
-        uint16_t tag = get_endian_short(data, littleEndian);
-        uint16_t type = get_endian_short(data + 2, littleEndian);
-        uint32_t count = getEndianInt(data + 4, littleEndian);
-        if (kOriginTag == tag && kOriginType == type && 1 == count) {
-            uint16_t val = get_endian_short(data + 8, littleEndian);
-            if (0 < val && val <= kLast_SkEncodedOrigin) {
-                *orientation = (SkEncodedOrigin) val;
-                return true;
-            }
-        }
-    }
-
-    return false;
+    // Start to read the EXIF data from the buffer.
+    return SkParseEncodedOrigin(data, data_length, offset, littleEndian, orientation);
 }
