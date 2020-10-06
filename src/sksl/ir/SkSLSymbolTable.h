c788bd5b6736d5ebff1f1df0209a2ad033591025
John Stiles <johnstiles@google.com>
2020-10-06 16:46:25 -0400

    Remove dedicated name from SymbolTable entries.
    We now assume that the Symbol's name() is its actual name, instead of
    having it passed it in by the caller. This simplifies calling code, and
    also removes redundant symbol names from the Dehydrator, saving a few
    hundred bytes here and there.
    
    Change-Id: I3e3d65b238ea58ab52f5dca205458d6f45c06c3d

--- src/sksl/ir/SkSLSymbolTable.h
+++ src/sksl/ir/SkSLSymbolTable.h
@@ -34,12 +34,12 @@ public:
     const Symbol* operator[](StringFragment name);
 
     void addAlias(StringFragment name, const Symbol* symbol);
-    void addWithoutOwnership(StringFragment name, const Symbol* symbol);
+    void addWithoutOwnership(const Symbol* symbol);
 
     template <typename T>
-    const T* add(StringFragment name, std::unique_ptr<T> symbol) {
+    const T* add(std::unique_ptr<T> symbol) {
         const T* ptr = symbol.get();
-        this->addWithoutOwnership(name, ptr);
+        this->addWithoutOwnership(ptr);
         this->takeOwnershipOfSymbol(std::move(symbol));
         return ptr;
     }
