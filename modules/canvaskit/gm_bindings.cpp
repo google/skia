197ef05665b953fa8b8501b1ba8c5a45869481d2
Kevin Lubick <kjlubick@google.com>
2020-10-07 08:55:43 -0400

    libgm_wasm.a is made
    Change-Id: I2babef848ca60f7db74e4adf27b8952a66bdeee1

---
+++ modules/canvaskit/gm_bindings.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include "gm/gm.h"
+
+using namespace emscripten;
+
+static void ListGMs() {
+    SkDebugf("Hello world\n");
+    for (skiagm::GMFactory fact : skiagm::GMRegistry::Range()) {
+        std::unique_ptr<skiagm::GM> gm(fact());
+        SkDebugf("gm %s\n", gm->getName());
+    }
+}
+
+DEF_SIMPLE_GM(hello_world_wasm, canvas, 360, 240) {
+  SkDebugf("Hello world %p\n", canvas);
+}
+
+EMSCRIPTEN_BINDINGS(GMs) {
+    function("ListGMs", &ListGMs);
+}
