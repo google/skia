6288e8be7072bbbe86d3e64f4d55e011ce69d6dd
Kevin Lubick <kjlubick@google.com>
2020-10-07 08:09:38 -0400

    [canvaskit] Remove Sk from nearly all function/type names.
    This is a massive breaking change for all existing users of CanvasKit.
    
    It will be (one of the only) changes in 0.19.0 to make the transition
    easier.
    
    Suggested reviewing order:
     - index.d.ts (to see type changes). Notice SkPicture still has Sk
       prefix, but no other types do (this felt "right" since Sk is
       part of the name of the type, but I can be swayed on this).
     - canvaskit-wasm-tests.ts
     - tests/*.spec.js
     - interface.js and helper.js
     - html examples
     - markdown files
    
    Change-Id: I3b3d3815df2078f986893df3c70101d6248c117d
    Docs-Preview: https://skia.org/?cl=322617
    Bug: skia:10717

--- modules/canvaskit/release.js
+++ modules/canvaskit/release.js
@@ -1,5 +1,5 @@
-function SkDebug(msg) {
+function Debug(msg) {
   // by leaving this blank, closure optimizes out calls (and the messages)
   // which trims down code size and marginally improves runtime speed.
 }
-/** @const */ var skIsDebug = false;
+/** @const */ var IsDebug = false;
