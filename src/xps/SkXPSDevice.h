7cc7f4d97aea3b06d0f3eb7c5780b2f74b6b1c01
Michael Ludwig <michaelludwig@google.com>
2020-10-07 13:50:34 -0400

    SkDevice::drawSpecial accepts arbitrary matrix v2
    This opens up the door to render image filter results with an arbitrary
    matrix, so we can skip the matrix filter DAG hack. It should also let
    me simplify the handling of backdrop image filters and correctly apply
    perspective to image filters.
    
    This is essentially the same as
    https://skia-review.googlesource.com/c/skia/+/317862
    but not parented to the CL that experimented with SkImagePaint.
    
    Some of the function declaration re-org that was done in its prior CL
    has been directly integrated into this CL.
    
    Bug: skia:9545
    Change-Id: Iad882bd306f3a4064fe60cfc0526226287ea7e29

--- src/xps/SkXPSDevice.h
+++ src/xps/SkXPSDevice.h
@@ -94,8 +94,7 @@ protected:
                        SkCanvas::SrcRectConstraint) override;
     void drawGlyphRunList(const SkGlyphRunList& glyphRunList) override;
     void drawVertices(const SkVertices*, SkBlendMode, const SkPaint&) override;
-    void drawDevice(SkBaseDevice*, int x, int y,
-                    const SkPaint&) override;
+    void drawDevice(SkBaseDevice*, const SkPaint&) override;
 
 private:
     class TypefaceUse {
