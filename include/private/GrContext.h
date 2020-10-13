87ced1eb9691f5654d4a821c637c6ae54b564c4f
Adlai Holler <adlai@google.com>
2020-10-13 13:29:15 -0400

    Migrate GrContextPriv & ivars to GrDirectContext
    After this, GrContextPriv gets renamed and GrContext gets removed.
    
    Change-Id: I49b034e4669876becb50b93c5fe3af887481e1ba

--- include/private/GrContext.h
+++ include/private/GrContext.h
@@ -9,18 +9,7 @@
 #define GrContext_DEFINED
 
 #include "include/core/SkTypes.h"
-#include "include/gpu/GrContextOptions.h"
 #include "include/gpu/GrRecordingContext.h"
-
-class GrAtlasManager;
-class GrClientMappedBufferManager;
-class GrContextPriv;
-class GrGpu;
-class GrResourceCache;
-class GrResourceProvider;
-class GrSmallPathAtlasMgr;
-class GrStrikeCache;
-class SkTaskGroup;
 
 /**
  * This deprecated class is being merged into GrDirectContext and removed.
@@ -32,40 +21,10 @@ class SK_API GrContext : public GrRecordingContext {
 public:
     ~GrContext() override;
 
-    // Provides access to functions that aren't part of the public API.
-    GrContextPriv priv();
-    const GrContextPriv priv() const;  // NOLINT(readability-const-return-type)
 protected:
     GrContext(sk_sp<GrContextThreadSafeProxy>);
 
-    virtual GrAtlasManager* onGetAtlasManager() = 0;
-    virtual GrSmallPathAtlasMgr* onGetSmallPathAtlasMgr() = 0;
-
 private:
-    friend class GrDirectContext; // for access to fGpu
-
-    // fTaskGroup must appear before anything that uses it (e.g. fGpu), so that it is destroyed
-    // after all of its users. Clients of fTaskGroup will generally want to ensure that they call
-    // wait() on it as they are being destroyed, to avoid the possibility of pending tasks being
-    // invoked after objects they depend upon have already been destroyed.
-    std::unique_ptr<SkTaskGroup>            fTaskGroup;
-    std::unique_ptr<GrStrikeCache>          fStrikeCache;
-    sk_sp<GrGpu>                            fGpu;
-    std::unique_ptr<GrResourceCache>        fResourceCache;
-    std::unique_ptr<GrResourceProvider>     fResourceProvider;
-
-    bool                                    fDidTestPMConversions;
-    // true if the PM/UPM conversion succeeded; false otherwise
-    bool                                    fPMUPMConversionsRoundTrip;
-
-    GrContextOptions::PersistentCache*      fPersistentCache;
-    GrContextOptions::ShaderErrorHandler*   fShaderErrorHandler;
-
-    std::unique_ptr<GrClientMappedBufferManager> fMappedBufferManager;
-
-    // TODO: have the GrClipStackClip use renderTargetContexts and rm this friending
-    friend class GrContextPriv;
-
     using INHERITED = GrRecordingContext;
 };
 
