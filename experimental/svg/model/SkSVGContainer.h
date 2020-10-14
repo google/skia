ddad82136e34a1abcef9d5a52bf185e6a96d730d
Florin Malita <fmalita@chromium.org>
2020-10-14 17:39:23 -0400

    [svg] Relocate out of experimental
    Change-Id: Ib289dbdcd80c16a01c47805e7242f2e08bebc165

--- experimental/svg/model/SkSVGContainer.h
+++ experimental/svg/model/SkSVGContainer.h
@@ -1,36 +1,10 @@
 /*
- * Copyright 2016 Google Inc.
+ * Copyright 2020 Google Inc.
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-#ifndef SkSVGContainer_DEFINED
-#define SkSVGContainer_DEFINED
+// Transitional forwarding header.  To be removed.
+#include "modules/svg/include/SkSVGContainer.h"
 
-#include "experimental/svg/model/SkSVGTransformableNode.h"
-#include "include/private/SkTArray.h"
-
-class SkSVGContainer : public SkSVGTransformableNode {
-public:
-    ~SkSVGContainer() override = default;
-
-    void appendChild(sk_sp<SkSVGNode>) override;
-
-protected:
-    explicit SkSVGContainer(SkSVGTag);
-
-    void onRender(const SkSVGRenderContext&) const override;
-
-    SkPath onAsPath(const SkSVGRenderContext&) const override;
-
-    bool hasChildren() const final;
-
-    // TODO: add some sort of child iterator, and hide the container.
-    SkSTArray<1, sk_sp<SkSVGNode>, true> fChildren;
-
-private:
-    using INHERITED = SkSVGTransformableNode;
-};
-
-#endif // SkSVGContainer_DEFINED
