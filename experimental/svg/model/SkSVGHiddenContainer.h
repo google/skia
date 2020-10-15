4ee12d096d8e37bd2a65d1466c6296bbf1eb0bde
Florin Malita <fmalita@chromium.org>
2020-10-15 09:22:35 -0400

    [svg] Relocate out of experimental
    Move the SVG rendering code to modules/svg, and componentize.
    Also split into include/src/utils.
    
    As external clients still reference the old header locations,
    introduce temporary forwarding headers to facilitate the migration.
    
    Change-Id: Ib289dbdcd80c16a01c47805e7242f2e08bebc165

--- experimental/svg/model/SkSVGHiddenContainer.h
+++ experimental/svg/model/SkSVGHiddenContainer.h
@@ -1,23 +1,10 @@
 /*
- * Copyright 2016 Google Inc.
+ * Copyright 2020 Google Inc.
  *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-#ifndef SkSVGHiddenContainer_DEFINED
-#define SkSVGHiddenContainer_DEFINED
+// Transitional forwarding header.  To be removed.
+#include "modules/svg/include/SkSVGHiddenContainer.h"
 
-#include "experimental/svg/model/SkSVGContainer.h"
-
-class SkSVGHiddenContainer : public SkSVGContainer {
-protected:
-    explicit SkSVGHiddenContainer(SkSVGTag t) : INHERITED(t) {}
-
-    void onRender(const SkSVGRenderContext&) const final {}
-
-private:
-    using INHERITED = SkSVGContainer;
-};
-
-#endif // SkSVGHiddenContainer_DEFINED
