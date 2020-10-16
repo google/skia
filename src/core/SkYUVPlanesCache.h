b0659bc04298df258380595135ab32f58fe55355
Brian Salomon <bsalomon@google.com>
2020-10-16 17:35:07 -0400

    Remove legacy SkImageGenerator YUV planes interface
    Bug: skia:10632
    Change-Id: Iaed6b51e03ef9023a74d77f3262104c1677c416f

--- src/core/SkYUVPlanesCache.h
+++ src/core/SkYUVPlanesCache.h
@@ -8,42 +8,30 @@
 #ifndef SkYUVPlanesCache_DEFINED
 #define SkYUVPlanesCache_DEFINED
 
-#include "include/core/SkImageInfo.h"
-#include "include/core/SkYUVAIndex.h"
-#include "include/core/SkYUVASizeInfo.h"
-#include "src/core/SkCachedData.h"
+#include "include/core/SkTypes.h"
 
+class SkCachedData;
 class SkResourceCache;
+class SkYUVAPixmaps;
 
 class SkYUVPlanesCache {
 public:
     /**
-     * The Info struct contains data about the 4 Y, U, V, and A planes of memory stored
-     * contiguously, in that order, as a single block of memory within SkYUVPlanesCache.
-     *
-     * fSizeInfo: fWidth, fHeight, and fWidthBytes of each of the Y, U, V, and A planes.
-     * fColorSpace: color space that will be used for the YUV -> RGB conversion.
-     *
-     * TODO: Replace this with SkYUVAPixmaps
-     */
-    struct Info {
-        SkYUVASizeInfo  fSizeInfo;
-        SkYUVAIndex     fYUVAIndices[SkYUVAIndex::kIndexCount];
-        SkYUVColorSpace fColorSpace;
-        SkPixmap        fPlanes[SkYUVASizeInfo::kMaxCount];
-    };
-    /**
-     * On success, return a ref to the SkCachedData that holds the pixels.
+     * On success, return a ref to the SkCachedData that holds the pixel data. The SkYUVAPixmaps
+     * contains a description of the YUVA data and has a SkPixmap for each plane that points
+     * into the SkCachedData.
      *
      * On failure, return nullptr.
      */
-    static SkCachedData* FindAndRef(uint32_t genID, Info* info,
+    static SkCachedData* FindAndRef(uint32_t genID,
+                                    SkYUVAPixmaps* pixmaps,
                                     SkResourceCache* localCache = nullptr);
 
     /**
-     * Add a pixelRef ID and its YUV planes data to the cache.
+     * Add a pixelRef ID and its YUV planes data to the cache. The SkYUVAPixmaps should contain
+     * SkPixmaps that store their pixel data in the SkCachedData.
      */
-    static void Add(uint32_t genID, SkCachedData* data, Info* info,
+    static void Add(uint32_t genID, SkCachedData* data, const SkYUVAPixmaps& pixmaps,
                     SkResourceCache* localCache = nullptr);
 };
 
