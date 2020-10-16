fd0b15801ff8e4960f0822bfb517b03812f8eb4c
Michael Ludwig <michaelludwig@google.com>
2020-10-16 10:38:31 -0400

    Visualize perspective scaling in filter bounds sample
    Bug: skia:9074
    Change-Id: Icdac64276e0a403950fd990a619d523b0ee784de
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/326942
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- src/core/SkMatrixPriv.h
+++ src/core/SkMatrixPriv.h
@@ -171,6 +171,15 @@ public:
                m.rc(3,3) == 1;
 
     }
+
+    // Returns the differential area scale factor for a local point 'p' that will be transformed
+    // by 'm' (which may have perspective). If 'm' does not have perspective, this scale factor is
+    // constant regardless of 'p'; when it does have perspective, it is specific to that point.
+    //
+    // This can be crudely thought of as "device pixel area" / "local pixel area" at 'p'.
+    //
+    // Returns positive infinity if the transformed homogeneous point has w <= 0.
+    static SkScalar DifferentialScale(const SkMatrix& m, const SkPoint& p);
 };
 
 #endif
