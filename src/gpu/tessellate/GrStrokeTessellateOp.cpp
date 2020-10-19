873926b73208a1f30fab43ec36e84a1dd8d9eeb5
Chris Dalton <csmartdalton@google.com>
2020-10-19 16:04:45 -0600

    Fix the formula for fNumRadialSegmentsPerRadian in GrStrokePatchBuilder
    GrStrokePatchBuilder had the wrong formula. This CL moves the formula to
    GrStrokeTessellateOp and calculates it in one spot for the builder and
    tessellator both. It also fixes a bug that this was hiding behind this
    formula error and updates some variable names.
    
    Bug: skia:10419
    Change-Id: I908d3960b16cac8dca0d40ff4116a3f5c5beed06

--- src/gpu/tessellate/GrStrokeTessellateOp.cpp
+++ src/gpu/tessellate/GrStrokeTessellateOp.cpp
@@ -26,14 +26,18 @@ GrStrokeTessellateOp::GrStrokeTessellateOp(GrAAType aaType, const SkMatrix& view
         : GrDrawOp(ClassID())
         , fAAType(aaType)
         , fViewMatrix(viewMatrix)
-        , fMatrixScale(fViewMatrix.getMaxScale())
         , fStroke(stroke)
+        , fMatrixScale(fViewMatrix.getMaxScale())
+        , fParametricIntolerance(fMatrixScale *
+                                 GrTessellationPathRenderer::kLinearizationIntolerance)
+        , fNumRadialSegmentsPerRadian(.5f / acosf(
+                std::max(1 - 2/(fParametricIntolerance * fStroke.getWidth()), -1.f)))
         , fColor(get_paint_constant_blended_color(paint))
         , fProcessors(std::move(paint))
         , fPaths(path)
         , fTotalCombinedVerbCnt(path.countVerbs()) {
     SkASSERT(fAAType != GrAAType::kCoverage);  // No mixed samples support yet.
-    SkASSERT(fMatrixScale >= 0);
+    SkASSERT(fParametricIntolerance >= 0);
     SkRect devBounds = path.getBounds();
     float inflationRadius = fStroke.getInflationRadius();
     devBounds.outset(inflationRadius, inflationRadius);
@@ -91,8 +95,10 @@ void GrStrokeTessellateOp::prePrepareColorProgram(SkArenaAlloc* arena,
                                                   const GrXferProcessor::DstProxyView& dstProxyView,
                                                   GrXferBarrierFlags renderPassXferBarriers,
                                                   const GrCaps& caps) {
-    auto* strokeShader = arena->make<GrStrokeTessellateShader>(fStroke, fMatrixScale, fViewMatrix,
-                                                               fColor);
+    auto* strokeShader = arena->make<GrStrokeTessellateShader>(fStroke, fMatrixScale,
+                                                               fParametricIntolerance,
+                                                               fNumRadialSegmentsPerRadian,
+                                                               fViewMatrix, fColor);
     auto pipelineFlags = GrPipeline::InputFlags::kNone;
     if (GrAAType::kNone != fAAType) {
         pipelineFlags |= GrPipeline::InputFlags::kHWAntialias;
@@ -111,8 +117,8 @@ void GrStrokeTessellateOp::onPrepare(GrOpFlushState* flushState) {
                                      flushState->detachAppliedClip(), flushState->dstProxyView(),
                                      flushState->renderPassBarriers(), flushState->caps());
     }
-    GrStrokePatchBuilder builder(flushState, &fPatchChunks, fMatrixScale, fStroke,
-                                 fTotalCombinedVerbCnt);
+    GrStrokePatchBuilder builder(flushState, &fPatchChunks, fStroke, fParametricIntolerance,
+                                 fNumRadialSegmentsPerRadian, fTotalCombinedVerbCnt);
     for (const SkPath& path : fPaths) {
         builder.addPath(path);
     }
