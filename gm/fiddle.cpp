587431fef79480feb9875609fde2dfc670c2f577
Brian Salomon <bsalomon@google.com>
2020-10-19 16:39:25 -0400

    example for chromium:1138810
    Bug: skia:1138810
    Change-Id: Ib5a2e04de50c441f57f5e4b6194c3f9829323dc9

--- gm/fiddle.cpp
+++ gm/fiddle.cpp
@@ -12,4 +12,13 @@ static void draw(SkCanvas* canvas);
 DEF_SIMPLE_GM(fiddle, canvas, 256, 256) { draw(canvas); }
 
 // Paste your fiddle.skia.org code over this stub.
-void draw(SkCanvas*) {}
+void draw(SkCanvas* canvas) {
+    SkPaint paint;
+    paint.setColor(SK_ColorBLUE);
+    paint.setMaskFilter(SkMaskFilter::MakeBlur(kNormal_SkBlurStyle, 35));
+    SkRRect rrect = SkRRect::MakeRectXY(SkRect::MakeLTRB(SkBits2Float(0x430d33c9), /* 141.202286 */
+      -133689.734375 * 1/10.f,
+     SkBits2Float(0x45368a87), /* 2920.657959 */
+     SkBits2Float(0x41cd4000)  /* 25.656250 */), 38.f, 38.f);
+   canvas->drawRRect(rrect, paint);
+}
