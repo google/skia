14c37006f2a3f8538a55d76fcfd6510fda745870
Kevin Lubick <kjlubick@google.com>
2020-10-21 16:12:55 -0400

    [canvaskit] Compile in most gms for testing
    The few we omit fail to compile or link.
    
    Bug: skia:10812
    Change-Id: I660d64b6f9bfe63949a12506d29e9a8d73898e6b

--- modules/canvaskit/compile_gm.sh
+++ modules/canvaskit/compile_gm.sh
@@ -145,6 +145,12 @@ if [[ `uname` != "Linux" ]]; then
   STRICTNESS=""
 fi
 
+# These gms do not compile or link with the WASM code. Thus, we omit them.
+GLOBIGNORE="gm/cgms.cpp:"\
+"gm/compressed_textures.cpp:"\
+"gm/fiddle.cpp:"\
+"gm/xform.cpp:"\
+"gm/video_decoder.cpp"
 # Emscripten prefers that the .a files go last in order, otherwise, it
 # may drop symbols that it incorrectly thinks aren't used. One day,
 # Emscripten will use LLD, which may relax this requirement.
@@ -160,10 +166,7 @@ EMCC_DEBUG=1 ${EMCXX} \
     --pre-js $BASE_DIR/gm.js \
     tools/Resources.cpp \
     $BASE_DIR/gm_bindings.cpp \
-    gm/bleed.cpp \
-    gm/blurs.cpp \
-    gm/complexclip.cpp \
-    gm/gm.cpp \
+    gm/*.cpp \
     $GM_LIB \
     $BUILD_DIR/libskshaper.a \
     $SHAPER_LIB \
