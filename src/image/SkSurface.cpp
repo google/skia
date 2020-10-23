1d7860c1d6f990939268d04e732c67c603a4b4eb
Ben Wagner <bungeman@google.com>
2020-10-23 11:29:03 -0400

    Remove code guarded by SK_LEGACY_SURFACE_PROPS.
    This removes the last of the SkFontHost LCD globals and the
    SkSurfaceProps::kLegacyFontHost_InitType.
    
    Bug: skia:3934
    Change-Id: Ic2342a3ea3dbcd075d6817cbd3fc27274e376b8d

--- src/image/SkSurface.cpp
+++ src/image/SkSurface.cpp
@@ -8,7 +8,6 @@
 #include <atomic>
 #include <cmath>
 #include "include/core/SkCanvas.h"
-#include "include/core/SkFontLCDConfig.h"
 #include "include/gpu/GrBackendSurface.h"
 #include "src/core/SkAutoPixmapStorage.h"
 #include "src/core/SkImagePriv.h"
@@ -16,39 +15,6 @@
 #include "src/image/SkSurface_Base.h"
 
 SkSurfaceProps::SkSurfaceProps() : fFlags(0), fPixelGeometry(kUnknown_SkPixelGeometry) {}
-
-#ifdef SK_LEGACY_SURFACE_PROPS
-static SkPixelGeometry compute_default_geometry() {
-    SkFontLCDConfig::LCDOrder order = SkFontLCDConfig::GetSubpixelOrder();
-    if (SkFontLCDConfig::kNONE_LCDOrder == order) {
-        return kUnknown_SkPixelGeometry;
-    } else {
-        // Bit0 is RGB(0), BGR(1)
-        // Bit1 is H(0), V(1)
-        const SkPixelGeometry gGeo[] = {
-            kRGB_H_SkPixelGeometry,
-            kBGR_H_SkPixelGeometry,
-            kRGB_V_SkPixelGeometry,
-            kBGR_V_SkPixelGeometry,
-        };
-        int index = 0;
-        if (SkFontLCDConfig::kBGR_LCDOrder == order) {
-            index |= 1;
-        }
-        if (SkFontLCDConfig::kVertical_LCDOrientation == SkFontLCDConfig::GetSubpixelOrientation()){
-            index |= 2;
-        }
-        return gGeo[index];
-    }
-}
-
-SkSurfaceProps::SkSurfaceProps(InitType) : fFlags(0), fPixelGeometry(compute_default_geometry()) {}
-
-SkSurfaceProps::SkSurfaceProps(uint32_t flags, InitType)
-    : fFlags(flags)
-    , fPixelGeometry(compute_default_geometry())
-{}
-#endif
 
 SkSurfaceProps::SkSurfaceProps(uint32_t flags, SkPixelGeometry pg)
     : fFlags(flags), fPixelGeometry(pg)
