9ff7d20b66d71369cce117202ef201a927dbed4f
Ethan Nicholas <ethannicholas@google.com>
2020-10-26 13:31:01 -0400

    Made IRNode smaller
    This backs off a bit on the "stuff everything into IRNode" push, moving
    the data of the big nodes back into them and significantly cutting the
    size of IRNode. This isn't a complete revert of the changes to these
    nodes - now that all of the fields have been hidden behind accessor
    methods, it's merely an implementation detail where the data actually
    lives, so these changes are much smaller and more targeted.
    
    Change-Id: I84c770245f26dfe36651d0f482543505bd7f71ef

--- src/sksl/ir/SkSLSymbol.h
+++ src/sksl/ir/SkSLSymbol.h
@@ -36,20 +36,11 @@ public:
         SkASSERT(kind >= Kind::kFirst && kind <= Kind::kLast);
     }
 
-    Symbol(int offset, const FieldData& data)
-    : INHERITED(offset, (int) Kind::kField, data) {}
-
-    Symbol(int offset, const FunctionDeclarationData& data)
-    : INHERITED(offset, (int) Kind::kFunctionDeclaration, data) {}
-
     Symbol(int offset, const SymbolAliasData& data)
     : INHERITED(offset, (int) Kind::kSymbolAlias, data) {}
 
     Symbol(int offset, const UnresolvedFunctionData& data)
     : INHERITED(offset, (int) Kind::kUnresolvedFunction, data) {}
-
-    Symbol(int offset, const VariableData& data)
-    : INHERITED(offset, (int) Kind::kVariable, data) {}
 
     Symbol& operator=(const Symbol&) = default;
 
