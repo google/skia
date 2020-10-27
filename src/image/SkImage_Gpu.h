10b33cc102994566262da1a631a028ae1676c7bd
Brian Salomon <bsalomon@google.com>
2020-10-27 09:38:18 -0400

    Add SkImage::MakeFromYUVATexturesCopyToExternal
    Updated API for creating a RGBA texture-backed image from RGBA texture
    planes with user-provided backing store using GrYUVABackendTextures.
    
    Ultimately we would like to remove all such APIs and have the client
    make a SkSurface and draw a YUVA texture image to the surface but
    a recent attempt to do that in Chrome caused a not yet understood
    perf regression.
    
    Bug: skia:10632
    Change-Id: I89411216948682f13281a91a7575d5f345badda7

--- src/image/SkImage_Gpu.h
+++ src/image/SkImage_Gpu.h
@@ -85,11 +85,14 @@ public:
                                              PromiseImageTextureContext textureContext,
                                              PromiseImageApiVersion);
 
-    static sk_sp<SkImage> ConvertYUVATexturesToRGB(GrRecordingContext*, SkYUVColorSpace,
-                                                   const GrBackendTexture [],
-                                                   const SkYUVAIndex [4],
-                                                   SkISize, GrSurfaceOrigin,
-                                                   GrRenderTargetContext*);
+    static sk_sp<SkImage> ConvertYUVATexturesToRGB(GrRecordingContext*,
+                                                   SkYUVColorSpace,
+                                                   const GrBackendTexture[],
+                                                   const SkYUVAIndex[SkYUVAIndex::kIndexCount],
+                                                   SkISize,
+                                                   GrSurfaceOrigin,
+                                                   GrRenderTargetContext*,
+                                                   sk_sp<GrRefCntedCallback> callback = nullptr);
 
 private:
     GrSurfaceProxyView fView;
