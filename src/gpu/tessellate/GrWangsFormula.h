a10afae275e0cba6d6540a4ed1e950afed266c5d
Chris Dalton <csmartdalton@google.com>
2020-10-29 11:05:49 -0600

    Add GrVx.h
    Adds grvx, Ganesh's addendum to skvx. Here we introduce familiar names
    and operations from GPU languages, as well as functions that are
    approximate and/or have LSB differences from platform to platform.
    
    The initial implementation has: fast_fma, fast_acos, and
    fast_angle_between_vectors. When a function is approximate, its error
    range is well documented and tested.
    
    Also establishes GrWangsFormula as the first user of grvx.
    
    Bug: skia:10419
    Change-Id: Id0682599cf9c0303eff386095afc3ef9f3a7fa1b

--- src/gpu/tessellate/GrWangsFormula.h
+++ src/gpu/tessellate/GrWangsFormula.h
@@ -10,7 +10,7 @@
 
 #include "include/core/SkPoint.h"
 #include "include/private/SkFloatingPoint.h"
-#include "include/private/SkVx.h"
+#include "src/gpu/GrVx.h"
 #include "src/gpu/tessellate/GrVectorXform.h"
 
 // Wang's formulas for cubics and quadratics (1985) give us the minimum number of evenly spaced (in
@@ -18,21 +18,20 @@
 // lines stay within a distance of "1/intolerance" pixels from the true curve.
 namespace GrWangsFormula {
 
-using float2 = skvx::Vec<2, float>;
-using float4 = skvx::Vec<4, float>;
-
-SK_ALWAYS_INLINE static float root4(float x) {
-    // rsqrt() is quicker than sqrt(), and 1/sqrt(1/sqrt(x)) == sqrt(sqrt(x)).
-    return (x != 0) ? sk_float_rsqrt(sk_float_rsqrt(x)) : 0;
+// Returns the 4th root of x for use with Wang's formulas below. Since the purpose of these formulas
+// is to guide linear approximations, precision is not exact and near-zero values may be clipped.
+SK_ALWAYS_INLINE static float fast_root4(float x) {
+    return sk_float_rsqrt(sk_float_rsqrt(std::max(x, 1e-2f)));
 }
 
-SK_ALWAYS_INLINE static int ceil_log2_sqrt_sqrt(float x) {
+SK_ALWAYS_INLINE static int root4_log2(float x) {
     return (sk_float_nextlog2(x) + 3) >> 2;  // i.e., "ceil(log2(sqrt(sqrt(f))))
 }
 
 // Constant term for the quatratic formula.
-constexpr float quadratic_constant(float intolerance) {
-    return .25f * intolerance;
+constexpr float quadratic_constant(float intolerance) { return .25f * intolerance; }
+constexpr float quadratic_constant_pow2(float intolerance) {
+    return (.25f * .25f) * (intolerance * intolerance);
 }
 
 // Returns Wang's formula for the given quadratic, raised to the 4th power. (Refer to
@@ -40,14 +39,14 @@ constexpr float quadratic_constant(float intolerance) {
 // The math is quickest when we calculate this value raised to the 4th power.
 SK_ALWAYS_INLINE static float quadratic_pow4(float intolerance, const SkPoint pts[],
                                              const GrVectorXform& vectorXform = GrVectorXform()) {
-    float2 p0 = float2::Load(pts);
-    float2 p1 = float2::Load(pts + 1);
-    float2 p2 = float2::Load(pts + 2);
-    float2 v = p0 + p1*-2 + p2;
+    using grvx::float2, skvx::bit_pun;
+    float2 p0 = bit_pun<float2>(pts[0]);
+    float2 p1 = bit_pun<float2>(pts[1]);
+    float2 p2 = bit_pun<float2>(pts[2]);
+    float2 v = grvx::fast_fma<2>(-2, p1, p0 + p2);
     v = vectorXform(v);
     float2 vv = v*v;
-    float k = quadratic_constant(intolerance);
-    return k*k * (vv[0] + vv[1]);
+    return (vv[0] + vv[1]) * quadratic_constant_pow2(intolerance);
 }
 
 // Returns the minimum number of evenly spaced (in the parametric sense) line segments that the
@@ -55,18 +54,19 @@ SK_ALWAYS_INLINE static float quadratic_pow4(float intolerance, const SkPoint pt
 // "1/intolerance" pixels from the true curve.
 SK_ALWAYS_INLINE static float quadratic(float intolerance, const SkPoint pts[],
                                         const GrVectorXform& vectorXform = GrVectorXform()) {
-    return root4(quadratic_pow4(intolerance, pts, vectorXform));
+    return fast_root4(quadratic_pow4(intolerance, pts, vectorXform));
 }
 
 // Returns the log2 value of Wang's formula for the given quadratic, rounded up to the next int.
 SK_ALWAYS_INLINE static int quadratic_log2(float intolerance, const SkPoint pts[],
                                            const GrVectorXform& vectorXform = GrVectorXform()) {
-    return ceil_log2_sqrt_sqrt(quadratic_pow4(intolerance, pts, vectorXform));
+    return root4_log2(quadratic_pow4(intolerance, pts, vectorXform));
 }
 
 // Constant term for the cubic formula.
-constexpr float cubic_constant(float intolerance) {
-    return .75f * intolerance;
+constexpr float cubic_constant(float intolerance) { return .75f * intolerance; }
+constexpr float cubic_constant_pow2(float intolerance) {
+    return (.75f * .75f) * (intolerance * intolerance);
 }
 
 // Returns Wang's formula for the given cubic, raised to the 4th power. (Refer to
@@ -74,15 +74,15 @@ constexpr float cubic_constant(float intolerance) {
 // The math is quickest when we calculate this value raised to the 4th power.
 SK_ALWAYS_INLINE static float cubic_pow4(float intolerance, const SkPoint pts[],
                                          const GrVectorXform& vectorXform = GrVectorXform()) {
+    using grvx::float4, skvx::bit_pun;
     float4 p01 = float4::Load(pts);
     float4 p12 = float4::Load(pts + 1);
     float4 p23 = float4::Load(pts + 2);
-    float4 v = p01 + p12*-2 + p23;
+    float4 v = grvx::fast_fma<4>(-2, p12, p01 + p23);
     v = vectorXform(v);
     float4 vv = v*v;
-    float2 m = skvx::max(vv.lo, vv.hi);
-    float k = cubic_constant(intolerance);
-    return k*k * (m[0] + m[1]);
+    auto m = skvx::max(vv.lo, vv.hi);
+    return (m[0] + m[1]) * cubic_constant_pow2(intolerance);
 }
 
 // Returns the minimum number of evenly spaced (in the parametric sense) line segments that the
@@ -90,13 +90,13 @@ SK_ALWAYS_INLINE static float cubic_pow4(float intolerance, const SkPoint pts[],
 // "1/intolerance" pixels from the true curve.
 SK_ALWAYS_INLINE static float cubic(float intolerance, const SkPoint pts[],
                                     const GrVectorXform& vectorXform = GrVectorXform()) {
-    return root4(cubic_pow4(intolerance, pts, vectorXform));
+    return fast_root4(cubic_pow4(intolerance, pts, vectorXform));
 }
 
 // Returns the log2 value of Wang's formula for the given cubic, rounded up to the next int.
 SK_ALWAYS_INLINE static int cubic_log2(float intolerance, const SkPoint pts[],
                                        const GrVectorXform& vectorXform = GrVectorXform()) {
-    return ceil_log2_sqrt_sqrt(cubic_pow4(intolerance, pts, vectorXform));
+    return root4_log2(cubic_pow4(intolerance, pts, vectorXform));
 }
 
 // Returns the maximum number of line segments a cubic with the given device-space bounding box size
@@ -104,15 +104,15 @@ SK_ALWAYS_INLINE static int cubic_log2(float intolerance, const SkPoint pts[],
 // maximize its value by placing control points on specific corners of the bounding box.
 SK_ALWAYS_INLINE static float worst_case_cubic(float intolerance, float devWidth, float devHeight) {
     float k = cubic_constant(intolerance);
-    return SkScalarSqrt(2*k * SkVector::Length(devWidth, devHeight));
+    return sqrtf(2*k * SkVector::Length(devWidth, devHeight));
 }
 
 // Returns the maximum log2 number of line segments a cubic with the given device-space bounding box
 // size would ever need to be divided into.
 SK_ALWAYS_INLINE static int worst_case_cubic_log2(float intolerance, float devWidth,
                                                   float devHeight) {
-    float k = cubic_constant(intolerance);
-    return ceil_log2_sqrt_sqrt(4*k*k * (devWidth * devWidth + devHeight * devHeight));
+    float kk = cubic_constant_pow2(intolerance);
+    return root4_log2(4*kk * (devWidth * devWidth + devHeight * devHeight));
 }
 
 }  // namespace GrWangsFormula
