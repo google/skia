a5b765c4973d6055d68d68a2024dde6e3c212446
Chris Dalton <csmartdalton@google.com>
2020-10-28 23:59:32 -0600

    Add GrPathUtils::findCubicConvex180Chops
    This method finds the locations a cubic needs to be chopped at before
    it can be passed to the stroke tessellation shader. It's an integral
    part of CPU stroke preparation and therefore extremely perf sensitive.
    
    Bug: skia:10419
    Change-Id: Ib23c2583b8cfc78814ce52425f7af2c8b2f8b420

---
+++ bench/GrPathUtilsBench.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "bench/Benchmark.h"
+#include "src/gpu/geometry/GrPathUtils.h"
+
+class FindCubicConvex180Chops : public Benchmark {
+public:
+    FindCubicConvex180Chops(const std::array<SkPoint,4>& pts, const char* suffix) : fPts(pts) {
+        fName.printf("GrPathUtils_findCubicConvex180Chops%s", suffix);
+    }
+
+private:
+    const char* onGetName() override { return fName.c_str(); }
+    bool isSuitableFor(Backend backend) final { return backend == kNonRendering_Backend; }
+    void onDraw(int loops, SkCanvas*) final {
+        float T[2] = {0};
+        int iters = 50000 * loops;
+        for (int i = 0; i < iters; ++i) {
+            int count = GrPathUtils::findCubicConvex180Chops(fPts.data(), T);
+            if (T[0] == 200.7f) {
+                // This will never happen. Pretend to use the result to keep the compiler honest.
+                SkDebugf("%i%f%f", count, T[0], T[1]);
+            }
+        }
+    }
+
+    SkString fName;
+    std::array<SkPoint,4> fPts;
+};
+
+DEF_BENCH(return new FindCubicConvex180Chops({{{0,0}, {100,0}, {50,100}, {100,100}}}, "_inflect1");)
+DEF_BENCH(return new FindCubicConvex180Chops({{{0,0}, {50,0}, {100,50}, {100,100}}}, "_loop");)
--- src/gpu/geometry/GrPathUtils.h
+++ src/gpu/geometry/GrPathUtils.h
@@ -151,6 +151,17 @@ inline void convertQuadToCubic(const SkPoint p[3], SkPoint out[4]) {
     out[3] = bit_pun<SkPoint>(p2);
 }
 
+// Finds 0, 1, or 2 T values at which to chop the given curve in order to guarantee the resulting
+// cubics are convex and rotate no more than 180 degrees.
+//
+//   - If the given cubic is "serpentine", then the T values are its inflection points.
+//
+//   - If the cubic is linear, then the T values are any cusp points were it turns around 180 deg.
+//
+//   - Otherwise the T value is the point at which rotation reaches 180 degrees (if any).
+//
+int findCubicConvex180Chops(const SkPoint[], float T[2]);
+
 }  // namespace GrPathUtils
 
 #endif
