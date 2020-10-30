9116b1f421d4daada3be5d181e976ff81f8d598d
Tyler Denniston <tdenniston@google.com>
2020-10-30 16:01:54 -0400

    [svg] Add several skeleton classes for filters
    Added:
    
    - SkSVGFilter: This corresponds to a <filter> element. Also added some
      of the attributes of this element.
    - SkSVGFe: This will be the base class of all <fe*> elements.
    - SkSVGFilterContext: This will hold the contextual mapping of string id
      -> image filter result, for constructing pipelines.
    
    Bug: skia:10841
    Change-Id: I15a29d39411a6255ab4e11f022baa10554b2bce6

---
+++ modules/svg/src/SkSVGFilterContext.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/effects/SkImageFilters.h"
+#include "modules/svg/include/SkSVGFilterContext.h"
+
+sk_sp<SkImageFilter> SkSVGFilterContext::findResultById(const SkString& id) const {
+    return nullptr;
+}
