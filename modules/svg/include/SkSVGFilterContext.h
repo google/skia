fcc955c8a3112d9b5e88e36c019d65ead6998538
Tyler Denniston <tdenniston@google.com>
2020-11-06 16:05:10 -0500

    [svg] Implement feComposite, basic filter result storage
    - Plumbing to store filter results by id and resolve them as inputs
      when referenced
    - Added implementation of feComposite filter
    - Added call to resolve input in feColorMatrix
    - Bugfix to SkSVGFilterType operator==
    
    The tests filters-color-01-b and filters-composite-03-b should now be
    passing.
    
    Bug: skia:10841
    Change-Id: I2cd099c60ac21710f25184806c5cc537656b42af

--- modules/svg/include/SkSVGFilterContext.h
+++ modules/svg/include/SkSVGFilterContext.h
@@ -8,25 +8,37 @@
 #ifndef SkSVGFilterContext_DEFINED
 #define SkSVGFilterContext_DEFINED
 
+#include <vector>
 #include "include/core/SkRect.h"
 #include "include/core/SkRefCnt.h"
-#include "include/private/SkTHash.h"
+#include "include/core/SkString.h"
 
 class SkImageFilter;
-class SkPicture;
 class SkString;
+class SkSVGFeInputType;
+class SkSVGRenderContext;
 
 class SkSVGFilterContext {
 public:
     SkSVGFilterContext(const SkRect& filterEffectsRegion)
             : fFilterEffectsRegion(filterEffectsRegion) {}
 
-    sk_sp<SkImageFilter> findResultById(const SkString& id) const;
-
     const SkRect& filterEffectsRegion() const { return fFilterEffectsRegion; }
 
+    void registerResult(const SkString&, const sk_sp<SkImageFilter>&);
+
+    sk_sp<SkImageFilter> resolveInput(const SkSVGRenderContext&, const SkSVGFeInputType&) const;
+
 private:
+    struct Result {
+        SkString fId;
+        sk_sp<SkImageFilter> fResult;
+    };
+
+    sk_sp<SkImageFilter> findResultById(const SkString&) const;
+
     SkRect fFilterEffectsRegion;
+    std::vector<Result> fResults;
 };
 
 #endif  // SkSVGFilterContext_DEFINED
--- modules/svg/src/SkSVGFilterContext.cpp
+++ modules/svg/src/SkSVGFilterContext.cpp
@@ -7,7 +7,43 @@
 
 #include "include/effects/SkImageFilters.h"
 #include "modules/svg/include/SkSVGFilterContext.h"
+#include "modules/svg/include/SkSVGNode.h"
+#include "modules/svg/include/SkSVGRenderContext.h"
+#include "modules/svg/include/SkSVGTypes.h"
 
 sk_sp<SkImageFilter> SkSVGFilterContext::findResultById(const SkString& id) const {
+    for (auto it = fResults.rbegin(), ite = fResults.rend(); it != ite; ++it) {
+        if (it->fId == id) {
+            return it->fResult;
+        }
+    }
     return nullptr;
+}
+
+void SkSVGFilterContext::registerResult(const SkString& id, const sk_sp<SkImageFilter>& result) {
+    SkASSERT(!id.isEmpty());
+    fResults.push_back({id, result});
+}
+
+sk_sp<SkImageFilter> SkSVGFilterContext::resolveInput(const SkSVGRenderContext& ctx,
+                                                      const SkSVGFeInputType& inputType) const {
+    switch (inputType.type()) {
+        case SkSVGFeInputType::Type::kSourceGraphic:
+            return nullptr;
+        case SkSVGFeInputType::Type::kFillPaint:
+            return SkImageFilters::Paint(*ctx.fillPaint());
+        case SkSVGFeInputType::Type::kStrokePaint: {
+            // The paint filter doesn't handle stroke paints properly, so convert to fill for simplicity.
+            // TODO: Paint filter is deprecated, but the replacement (SkShaders::*())
+            //       requires some extra work to handle all paint features (gradients, etc).
+            SkPaint p = *ctx.strokePaint();
+            p.setStyle(SkPaint::kFill_Style);
+            return SkImageFilters::Paint(p);
+        }
+        case SkSVGFeInputType::Type::kFilterPrimitiveReference:
+            return findResultById(inputType.id());
+        default:
+            SkDebugf("unhandled filter input type %d\n", inputType.type());
+            return nullptr;
+    }
 }
