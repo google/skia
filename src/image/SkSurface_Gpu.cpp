5bc5dacbaaaa3903b547435c43726a8b41415374
Robert Phillips <robertphillips@google.com>
2020-11-06 14:08:31 -0500

    Change UnrefDDLTask to just be the DDLTask
    Change-Id: Ib5ff9f3f3d9cbc8860e3a2ba74e6da4355197662

--- src/image/SkSurface_Gpu.cpp
+++ src/image/SkSurface_Gpu.cpp
@@ -367,7 +367,7 @@ bool SkSurface_Gpu::onDraw(sk_sp<const SkDeferredDisplayList> ddl) {
         return false;
     }
 
-    direct->priv().copyRenderTasksFromDDL(std::move(ddl), rtc->asRenderTargetProxy());
+    direct->priv().createDDLTask(std::move(ddl), rtc->asRenderTargetProxy());
     return true;
 }
 
