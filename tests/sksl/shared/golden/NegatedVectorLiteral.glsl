cf27b4f7444d2760c6973cd4e57ad691c673b505
John Stiles <johnstiles@google.com>
2020-11-06 11:37:05 -0500

    Improve constant folding for int vectors.
    This implements constant folding optimizations on int vectors
    (== != + - * /) that were previously only supported on float vectors.
    
    Bug: skia:10908
    Change-Id: Ibf61ab43eb7ae2ce8e99cce21cc55777359817e5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/332424
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/shared/golden/NegatedVectorLiteral.glsl
+++ tests/sksl/shared/golden/NegatedVectorLiteral.glsl
@@ -9,10 +9,10 @@ void main() {
     }
 
     {
-        sk_FragColor.x = float(ivec4(-1) == ivec4(ivec2(-1), ivec2(-1)) ? 1 : 0);
-        sk_FragColor.y = float(ivec4(1) != ivec4(-1) ? 1 : 0);
-        sk_FragColor.z = float(ivec4(-2) == ivec4(-2, ivec3(-2)) ? 1 : 0);
-        sk_FragColor.w = float(ivec2(1, -2) == ivec2(1, -2) ? 1 : 0);
+        sk_FragColor.x = 1.0;
+        sk_FragColor.y = 1.0;
+        sk_FragColor.z = 1.0;
+        sk_FragColor.w = 1.0;
     }
 
 }
