7b93fada48780ad2b9f4e489d6c2298814d55d8c
Leon Scroggins <scroggo@google.com>
2020-11-06 17:05:36 -0500

    Handle EXIF orientation in SkAnimatedImage
    Bug: skia:10914
    Bug: b/163595585
    
    In a WebP image, it is possible to combine animation with an EXIF
    orientation. While SkAndroidCodec attempts to handle orientation itself
    by decoding into temporary memory and then drawing through a matrix,
    this doesn't work directly when compositing a P-frame into a prior
    frame. SkAnimatedImage already uses an SkMatrix to handle cropping and
    scaling, so update that matrix to include the orientation.
    
    Make SkAnimatedImage a friend of SkAndroidCodec. This allows the former
    to have the same ExifOrientationBehavior specified by the latter, and to
    recreate the latter so it does not try to handle the orientation itself.
    
    Clip SkAnimatedImage to its bounds. Android's AnimatedImageDrawable
    performs its own clip, but this makes a crop rect work for other
    clients.
    
    Update getCurrentFrame to take cropping, scaling, and orientation into
    account. This method is used by CanvasKit, which does not use cropping
    or scaling, but will now properly orient an animation with an EXIF
    orientation.
    
    Add a GM that exercises the various combinations of ways SkAnimatedImage
    can be used:
    - via newPictureSnapshot (as in Android) versus getCurrentFrame
    - scaling down to a dimension that can be output from the
      SkAndroidCodec, versus up, which SkAnimatedImage scales
    - with a crop rect
    - with a post processor
    
    Change-Id: If1854e9aea23fc4afddf75d39132b38e3fbc6071

--- include/codec/SkAndroidCodec.h
+++ include/codec/SkAndroidCodec.h
@@ -290,5 +290,7 @@ private:
     const SkImageInfo               fInfo;
     const ExifOrientationBehavior   fOrientationBehavior;
     std::unique_ptr<SkCodec>        fCodec;
+
+    friend class SkAnimatedImage;
 };
 #endif // SkAndroidCodec_DEFINED
