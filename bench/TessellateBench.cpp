fbbf909700c859da2a5688b0dc14d9c244168ff2
Chris Dalton <csmartdalton@google.com>
2020-11-09 14:11:49 -0700

    Add a mock implementation of GrMeshDrawOp::Target
    Moves the "BenchmarkTarget" class from tessellation benchmarks into a
    mock header where it can be reused by other tests and benchmarks.
    
    Change-Id: I344d9ba3d391ff99e10c4ab238684b0b6ada87d0

--- bench/TessellateBench.cpp
+++ bench/TessellateBench.cpp
@@ -10,6 +10,7 @@
 #include "src/core/SkPathPriv.h"
 #include "src/gpu/GrDirectContextPriv.h"
 #include "src/gpu/GrOpFlushState.h"
+#include "src/gpu/mock/GrMockOpTarget.h"
 #include "src/gpu/tessellate/GrMiddleOutPolygonTriangulator.h"
 #include "src/gpu/tessellate/GrPathTessellateOp.h"
 #include "src/gpu/tessellate/GrResolveLevelCounter.h"
@@ -19,6 +20,22 @@
 
 // This is the number of cubics in desk_chalkboard.skp. (There are no quadratics in the chalkboard.)
 constexpr static int kNumCubicsInChalkboard = 47182;
+
+static sk_sp<GrDirectContext> make_mock_context() {
+    GrMockOptions mockOptions;
+    mockOptions.fDrawInstancedSupport = true;
+    mockOptions.fMaxTessellationSegments = 64;
+    mockOptions.fMapBufferFlags = GrCaps::kCanMap_MapFlag;
+    mockOptions.fConfigOptions[(int)GrColorType::kAlpha_8].fRenderability =
+            GrMockOptions::ConfigOptions::Renderability::kMSAA;
+    mockOptions.fConfigOptions[(int)GrColorType::kAlpha_8].fTexturable = true;
+    mockOptions.fIntegerSupport = true;
+
+    GrContextOptions ctxOptions;
+    ctxOptions.fGpuPathRenderers = GpuPathRenderers::kTessellation;
+
+    return GrDirectContext::MakeMock(&mockOptions, ctxOptions);
+}
 
 static SkPath make_cubic_path() {
     SkRandom rand;
@@ -30,96 +47,6 @@ static SkPath make_cubic_path() {
     }
     return path;
 }
-
-// This is a dummy GrMeshDrawOp::Target implementation that just gives back pointers into
-// pre-allocated CPU buffers, rather than allocating and mapping GPU buffers.
-class BenchmarkTarget : public GrMeshDrawOp::Target {
-public:
-    BenchmarkTarget() {
-        GrMockOptions mockOptions;
-        mockOptions.fDrawInstancedSupport = true;
-        mockOptions.fMaxTessellationSegments = 64;
-        mockOptions.fMapBufferFlags = GrCaps::kCanMap_MapFlag;
-        mockOptions.fConfigOptions[(int)GrColorType::kAlpha_8].fRenderability =
-                GrMockOptions::ConfigOptions::Renderability::kMSAA;
-        mockOptions.fConfigOptions[(int)GrColorType::kAlpha_8].fTexturable = true;
-        mockOptions.fIntegerSupport = true;
-
-        GrContextOptions ctxOptions;
-        ctxOptions.fGpuPathRenderers = GpuPathRenderers::kTessellation;
-
-        fMockContext = GrDirectContext::MakeMock(&mockOptions, ctxOptions);
-    }
-    const GrDirectContext* mockContext() const { return fMockContext.get(); }
-    const GrCaps& caps() const override { return *fMockContext->priv().caps(); }
-    GrThreadSafeCache* threadSafeCache() const override {
-        return fMockContext->priv().threadSafeCache();
-    }
-    GrResourceProvider* resourceProvider() const override {
-        return fMockContext->priv().resourceProvider();
-    }
-    GrSmallPathAtlasMgr* smallPathAtlasManager() const override { return nullptr; }
-    void resetAllocator() { fAllocator.reset(); }
-    SkArenaAlloc* allocator() override { return &fAllocator; }
-    void putBackVertices(int vertices, size_t vertexStride) override { /* no-op */ }
-
-    void* makeVertexSpace(size_t vertexSize, int vertexCount, sk_sp<const GrBuffer>*,
-                          int* startVertex) override {
-        if (vertexSize * vertexCount > sizeof(fStaticVertexData)) {
-            SK_ABORT("FATAL: wanted %zu bytes of static vertex data; only have %zu.\n",
-                     vertexSize * vertexCount, sizeof(fStaticVertexData));
-        }
-        *startVertex = 0;
-        return fStaticVertexData;
-    }
-
-    void* makeVertexSpaceAtLeast(size_t vertexSize, int minVertexCount, int fallbackVertexCount,
-                                 sk_sp<const GrBuffer>*, int* startVertex,
-                                 int* actualVertexCount) override {
-        if (vertexSize * minVertexCount > sizeof(fStaticVertexData)) {
-            SK_ABORT("FATAL: wanted %zu bytes of static vertex data; only have %zu.\n",
-                     vertexSize * minVertexCount, sizeof(fStaticVertexData));
-        }
-        *startVertex = 0;
-        *actualVertexCount = sizeof(fStaticVertexData) / vertexSize;
-        return fStaticVertexData;
-    }
-
-    GrDrawIndexedIndirectCommand* makeDrawIndexedIndirectSpace(
-            int drawCount, sk_sp<const GrBuffer>* buffer, size_t* offsetInBytes) override {
-        int staticBufferCount = (int)SK_ARRAY_COUNT(fStaticDrawIndexedIndirectData);
-        if (drawCount > staticBufferCount) {
-            SK_ABORT("FATAL: wanted %i static drawIndexedIndirect elements; only have %i.\n",
-                     drawCount, staticBufferCount);
-        }
-        return fStaticDrawIndexedIndirectData;
-    }
-
-#define UNIMPL(...) __VA_ARGS__ override { SK_ABORT("unimplemented."); }
-    UNIMPL(void recordDraw(const GrGeometryProcessor*, const GrSimpleMesh[], int,
-                           const GrSurfaceProxy* const[], GrPrimitiveType))
-    UNIMPL(uint16_t* makeIndexSpace(int, sk_sp<const GrBuffer>*, int*))
-    UNIMPL(uint16_t* makeIndexSpaceAtLeast(int, int, sk_sp<const GrBuffer>*, int*, int*))
-    UNIMPL(GrDrawIndirectCommand* makeDrawIndirectSpace(int, sk_sp<const GrBuffer>*, size_t*))
-    UNIMPL(void putBackIndices(int))
-    UNIMPL(GrRenderTargetProxy* proxy() const)
-    UNIMPL(const GrSurfaceProxyView* writeView() const)
-    UNIMPL(const GrAppliedClip* appliedClip() const)
-    UNIMPL(GrAppliedClip detachAppliedClip())
-    UNIMPL(const GrXferProcessor::DstProxyView& dstProxyView() const)
-    UNIMPL(GrXferBarrierFlags renderPassBarriers() const)
-    UNIMPL(GrStrikeCache* strikeCache() const)
-    UNIMPL(GrAtlasManager* atlasManager() const)
-    UNIMPL(SkTArray<GrSurfaceProxy*, true>* sampledProxyArray())
-    UNIMPL(GrDeferredUploadTarget* deferredUploadTarget())
-#undef UNIMPL
-
-private:
-    sk_sp<GrDirectContext> fMockContext;
-    SkPoint fStaticVertexData[4800000];
-    GrDrawIndexedIndirectCommand fStaticDrawIndexedIndirectData[32];
-    SkSTArenaAllocWithReset<1024 * 1024> fAllocator;
-};
 
 // This serves as a base class for benchmarking individual methods on GrPathTessellateOp.
 class GrPathTessellateOp::TestingOnly_Benchmark : public Benchmark {
@@ -144,7 +71,7 @@ public:
 
 private:
     void onDelayedSetup() override {
-        fTarget = std::make_unique<BenchmarkTarget>();
+        fTarget = std::make_unique<GrMockOpTarget>(make_mock_context());
     }
 
     void onDraw(int loops, SkCanvas*) final {
@@ -172,7 +99,7 @@ private:
     virtual void runBench(GrMeshDrawOp::Target*, GrPathTessellateOp*) = 0;
 
     GrPathTessellateOp fOp;
-    std::unique_ptr<BenchmarkTarget> fTarget;
+    std::unique_ptr<GrMockOpTarget> fTarget;
     SkString fName;
 };
 
@@ -280,7 +207,7 @@ private:
     bool isSuitableFor(Backend backend) final { return backend == kNonRendering_Backend; }
 
     void onDelayedSetup() override {
-        fTarget = std::make_unique<BenchmarkTarget>();
+        fTarget = std::make_unique<GrMockOpTarget>(make_mock_context());
         fPath.reset().moveTo(0, 0);
         for (int i = 0; i < kNumCubicsInChalkboard/2; ++i) {
             fPath.cubicTo(100, 0, 50, 100, 100, 100);
@@ -305,7 +232,7 @@ private:
 
     const float fMatrixScale;
     SkString fName;
-    std::unique_ptr<BenchmarkTarget> fTarget;
+    std::unique_ptr<GrMockOpTarget> fTarget;
     SkPath fPath;
     SkStrokeRec fStrokeRec = SkStrokeRec(SkStrokeRec::kFill_InitStyle);
 };
