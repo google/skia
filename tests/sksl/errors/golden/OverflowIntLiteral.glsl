76013704ad8c47e065226d318d7a37606c1193e3
John Stiles <johnstiles@google.com>
2020-11-10 14:17:42 -0500

    Add unit tests for overflowing int and uint literal limits.
    At present, we do not report any error; the values wrap silently.
    
    Change-Id: I8c435cfdd81f6c2e5fd87e9c39c708138bf4ec82
    Bug: skia:10932
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/333676
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

---
+++ tests/sksl/errors/golden/OverflowIntLiteral.glsl
@@ -0,0 +1,12 @@
+
+out vec4 sk_FragColor;
+int intMin = -2147483648;
+int intMinMinusOne = 2147483647;
+int intMax = 2147483647;
+int intMaxPlusOne = -2147483648;
+void main() {
+    sk_FragColor.x = float(intMin);
+    sk_FragColor.x = float(intMax);
+    sk_FragColor.x = float(intMinMinusOne);
+    sk_FragColor.x = float(intMaxPlusOne);
+}
