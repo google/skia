5b8241411eee277388d730acd0ab94a33fec9531
John Stiles <johnstiles@google.com>
2020-11-09 22:46:03 -0500

    Limit struct nesting depth to a maximum of eight levels.
    This addresses a sanitizer issue discovered in
    https://oss-fuzz.com/testcase-detail/4908118777266176 (it has not been
    assigned an oss-fuzz bug number yet; coming soon)
    
    This puts an upper bound on struct nesting, again to prevent memory-
    layout and other recursive type-handling code from overflowing the
    stack. Coincidentally, while researching GLSL behavior around this bug,
    I learned that WebGL has a similar limitation but caps nested structs to
    4 deep. (I could not find any documented GLSL upper bound.)
    
    Note that both the GLSL and Metal outputs for StructMaxDepth are badly
    malformed. (Structs cannot be embedded within another struct in GLSL;
    structs SA7 and below are never declared in GLSL; the array list for SA7
    is backwards in GLSL; Metal is missing structs SA1 through SA8; Metal
    puts the array list on the type instead of the variable name.)
    These issues will be addressed in separate CLs.
    
    Change-Id: I0f1059b6faa400cd0647dd7010ec839f73779a36
    Bug: skia:10922, skia:10923

---
+++ tests/sksl/shared/golden/StructMaxDepth.glsl
@@ -0,0 +1,21 @@
+
+in struct S8 {
+    struct S7 {
+        struct S6 {
+            struct S5 {
+                struct S4 {
+                    struct S3 {
+                        struct S2 {
+                            struct S1 {
+                                int x;
+                            } x;
+                        } x;
+                    } x;
+                } x;
+            } x;
+        } x;
+    } x;
+} s8;
+in struct SA8 {
+    SA7[8][7][6][5][4][3][2][1] x;
+} sa8[1][2][3][4][5][6][7][8];
