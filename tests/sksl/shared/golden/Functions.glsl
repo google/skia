5a6ee574e66e9e9b3774bcafccdd1476e226a664
John Stiles <johnstiles@google.com>
2020-11-09 22:59:55 -0500

    Add unit test demonstrating array function param bug.
    A GLSL function like:
        void fn(int x[1][2][3]) {...}
    
    Will emit SkSL with the array dimensions in reverse order:
        void fn(int x[3][2][1]) {...}
    
    Trying to invoke the function will fail because it expects a reverse-
    dimensioned array.
    
    Change-Id: I24431aabd2f6111b5493f63f0a85f9c78514d522
    Bug: skia:10924

--- tests/sksl/shared/golden/Functions.glsl
+++ tests/sksl/shared/golden/Functions.glsl
@@ -1,23 +1,4 @@
+### Compilation failed:
 
-out vec4 sk_FragColor;
-void main() {
-    float x = 10.0;
-    {
-        float _1_y[2];
-        float _2_z;
-
-        _1_y[0] = 10.0;
-        _1_y[1] = 20.0;
-        float _3_0_foo;
-        {
-            _3_0_foo = _1_y[0] * _1_y[1];
-        }
-
-        _2_z = _3_0_foo;
-
-        x = _2_z;
-    }
-
-
-    sk_FragColor = vec4(x);
-}
+error: 17: expected 'float[3][2]', but found 'float[2][3]'
+1 error
