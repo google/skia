c1885c2540f8ebf74e39ab7e10d2747c9f0c9aa4
Robert Phillips <robertphillips@google.com>
2020-11-11 09:39:27 -0500

    Revert "Change UnrefDDLTask to just be the DDLTask"
    This CL reverts the following 3 CLs due to coupled perf and memory regressions:
    
    88e8bb2 https://skia-review.googlesource.com/c/skia/+/333576 (Fix UMR (valgrind) bug in GrDrawingManager)
    cca1976 https://skia-review.googlesource.com/c/skia/+/333463 (Remove the bulk add-renderTasks entry point from the drawing manager)
    02dd0ed https://skia-review.googlesource.com/c/skia/+/332600 (Change UnrefDDLTask to just be the DDLTask)
    
    Bug: 1147659 1147634 1147650 skia:10936
    Change-Id: I937d656c4126eac954a9d7930a4034488d6ef5df

--- src/image/SkSurface_Gpu.cpp
+++ src/image/SkSurface_Gpu.cpp
@@ -367,7 +367,7 @@ bool SkSurface_Gpu::onDraw(sk_sp<const SkDeferredDisplayList> ddl, int xOffset, 
         return false;
     }
 
-    direct->priv().createDDLTask(std::move(ddl), rtc->asRenderTargetProxy());
+    direct->priv().copyRenderTasksFromDDL(std::move(ddl), rtc->asRenderTargetProxy());
     return true;
 }
 
