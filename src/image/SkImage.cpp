ebde44b69745ab3c29c77aa85ba7e43ae9ac9335
Mike Reed <reed@google.com>
2020-11-11 18:19:18 -0500

    Add temporary Make for SkSamplingOptions
    Change-Id: Icf9d192880caa16a82f9774cc99c97e096f8e678

--- src/image/SkImage.cpp
+++ src/image/SkImage.cpp
@@ -147,12 +147,6 @@ sk_sp<SkShader> SkImage::makeShader(SkTileMode tmx, SkTileMode tmy,
                                options, localMatrix);
 }
 
-sk_sp<SkShader> SkImage::makeShader(SkTileMode tmx, SkTileMode tmy,
-                                    const SkMatrix* localMatrix, SkFilterQuality filtering) const {
-    return SkImageShader::Make(sk_ref_sp(const_cast<SkImage*>(this)), tmx, tmy, localMatrix,
-                               SkImageShader::FilterEnum(filtering));
-}
-
 sk_sp<SkData> SkImage::encodeToData(SkEncodedImageFormat type, int quality) const {
     // Context TODO: Elevate GrDirectContext requirement to public API.
     auto dContext = as_IB(this)->directContext();
@@ -638,4 +632,26 @@ sk_sp<SkImage> SkImage::withDefaultMipmaps() const {
 
 sk_sp<SkImage> SkMipmapBuilder::attachTo(const SkImage* src) {
     return src->withMipmaps(fMM);
+}
+
+/////////////
+
+SkSamplingOptions SkSamplingOptions::Make(SkFilterQuality fq) {
+    switch (fq) {
+        case SkFilterQuality::kLow_SkFilterQuality:
+            return SkSamplingOptions({
+                SkSamplingMode::kLinear,
+                SkMipmapMode::kNone
+            });
+        case SkFilterQuality::kMedium_SkFilterQuality:
+            return SkSamplingOptions({
+                SkSamplingMode::kLinear,
+                SkMipmapMode::kNearest
+            });
+        case SkFilterQuality::kHigh_SkFilterQuality:
+            return SkSamplingOptions({1.0f/3, 1.0f/3});
+        case SkFilterQuality::kNone_SkFilterQuality:
+            break;  // fall out
+    }
+    return SkSamplingOptions(); // kNone_SkFilterQuality
 }
