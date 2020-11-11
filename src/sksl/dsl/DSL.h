f3d2bd78b5f16f6923a0a4e47c38882fe568b1be
Ethan Nicholas <ethannicholas@google.com>
2020-11-11 13:02:12 -0500

    DSL experiment
    Change-Id: I3058089338e20eebc3da18ac5571801abcaab564

---
+++ src/sksl/dsl/DSL.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_DSL
+#define SKSL_DSL
+
+#include "src/sksl/SkSLContext.h"
+#include "src/sksl/dsl/Function.h"
+#include "src/sksl/dsl/Statement.h"
+#include "src/sksl/dsl/Type.h"
+#include "src/sksl/dsl/Var.h"
+#include "src/sksl/dsl/priv/BlockCreator.h"
+#include "src/sksl/dsl/priv/DSLWriter.h"
+
+namespace skslcode {
+
+extern Var sk_FragColor;
+extern Var sk_FragCoord;
+
+enum SwizzleComponent : int8_t {
+    R,
+    G,
+    B,
+    A,
+    X,
+    Y,
+    Z,
+    W,
+    ZERO,
+    ONE
+};
+
+template<class... Stmts>
+std::unique_ptr<SkSL::Statement> Block(Stmts... stmts) {
+    return BlockCreator<Stmts...>(std::move(stmts)...).block();
+}
+
+std::unique_ptr<SkSL::Statement> Block(SkSL::StatementArray stmts);
+
+std::unique_ptr<SkSL::Statement> Do(Statement stmt, Expression test);
+
+std::unique_ptr<SkSL::Statement> If(Expression test, Statement ifTrue);
+
+std::unique_ptr<SkSL::Statement> If(Expression test, Statement ifTrue, Statement ifFalse);
+
+Expression Swizzle(Expression base, SwizzleComponent a);
+
+Expression Swizzle(Expression base, SwizzleComponent a, SwizzleComponent b);
+
+Expression Swizzle(Expression base, SwizzleComponent a, SwizzleComponent b, SwizzleComponent c);
+
+Expression Swizzle(Expression base, SwizzleComponent a, SwizzleComponent b, SwizzleComponent c,
+                   SwizzleComponent d);
+
+Expression Ternary(Expression test, Expression ifTrue, Expression ifFalse);
+
+std::unique_ptr<SkSL::Statement> While(Expression test, Statement stmt);
+
+Expression dot(Expression x, Expression y);
+
+Expression saturate(Expression x);
+
+void Start(const GrFragmentProcessor* currentProcessor,
+           const GrGLSLFragmentProcessor::EmitArgs* args);
+
+void End();
+
+} // namespace skslcode
+
+#endif
---
+++ src/sksl/dsl/priv/DSLWriter.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_DSLWRITER
+#define SKSL_DSLWRITER
+
+#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
+#include "src/sksl/SkSLModifiersPool.h"
+#include "src/sksl/dsl/Expression.h"
+#include "src/sksl/ir/SkSLExpressionStatement.h"
+#include "src/sksl/ir/SkSLProgram.h"
+#include "src/sksl/ir/SkSLStatement.h"
+
+namespace SkSL {
+    class Compiler;
+    class Context;
+    class IRGenerator;
+    class SymbolTable;
+    class Type;
+} // namespace sksl
+
+namespace skslcode {
+
+class DSLWriter {
+public:
+    ~DSLWriter();
+
+    SkSL::Compiler& compiler() {
+        return fCompiler;
+    }
+
+    SkSL::IRGenerator& irGenerator();
+
+    const SkSL::Context& context();
+
+    const std::shared_ptr<SkSL::SymbolTable>& symbolTable();
+
+    SkSL::ModifiersPool::Handle modifiers(SkSL::Modifiers modifiers);
+
+    SkSL::String name(const char* name);
+
+    // called by Var's constructor so that we know to add a declaration to the next function
+    void addVar(Var* var) {
+        fVars.push_back(var);
+    }
+
+    SkSL::StatementArray pendingDeclarations();
+
+    std::vector<std::unique_ptr<SkSL::ProgramElement>>& programElements() {
+        return fProgramElements;
+    }
+
+    const GrFragmentProcessor* currentProcessor() {
+        return fCurrentProcessor;
+    }
+
+    void setCurrentProcessor(const GrFragmentProcessor* processor) {
+        fCurrentProcessor = processor;
+    }
+
+    const GrGLSLFragmentProcessor::EmitArgs* currentEmitArgs() {
+        return fEmitArgs;
+    }
+
+    void setCurrentEmitArgs(const GrGLSLFragmentProcessor::EmitArgs* emitArgs) {
+        fEmitArgs = emitArgs;
+    }
+
+    static DSLWriter& Instance();
+
+private:
+    DSLWriter(SkSL::Compiler* compiler)
+        : fCompiler(*compiler) {
+        SkSL::ParsedModule module = fCompiler.moduleForProgramKind(SkSL::Program::kFragment_Kind);
+        SkSL::IRGenerator& ir = this->irGenerator();
+        ir.fSymbolTable = module.fSymbols;
+        ir.pushSymbolTable();
+    }
+
+    SkSL::Program::Settings fSettings;
+    SkSL::Compiler& fCompiler;
+    int fNameCount = 0;
+    SkTArray<Var*> fVars;
+    std::vector<std::unique_ptr<SkSL::ProgramElement>> fProgramElements;
+    const GrFragmentProcessor* fCurrentProcessor;
+    const GrGLSLFragmentProcessor::EmitArgs* fEmitArgs;
+};
+
+} // namespace skslcode
+
+#endif
