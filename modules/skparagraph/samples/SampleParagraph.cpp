1be4fd0b3bf639c687e3ec30df930c6e62a4fd8e
Julia Lavrova <jlavrova@google.com>
2020-11-11 20:49:40 +0000

    Revert "Some testing images in Google3 look slightly different with this change. Need to change them, too."
    This reverts commit b776a105e531e9dd91ffb78307dd7dd1f7b78700.
    
    Reason for revert: This will be the first step of a staged change.
    
    Original change's description:
    > Some testing images in Google3 look slightly different with this change. Need to change them, too.
    >
    > Revert "Roll back #10487 fix (so now spaces always resolved in the main font)"
    >
    > This reverts commit ff5d8700b3a7771a77bb841ca03e3789de03ee3b.
    >
    > Reason for revert: <INSERT REASONING HERE>
    >
    > Original change's description:
    > > Roll back #10487 fix (so now spaces always resolved in the main font)
    > >
    > > Bug: skia:10715
    > > Change-Id: I06bb881998d00228c79a218967eda6468cbc5bed
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/334053
    > > Reviewed-by: Ben Wagner <bungeman@google.com>
    > > Commit-Queue: Julia Lavrova <jlavrova@google.com>
    >
    > TBR=bungeman@google.com,jlavrova@google.com
    >
    > Change-Id: Icc1f262f60fa2ec9b4425ce3088b5eaac9313374
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:10715
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/334158
    > Reviewed-by: Julia Lavrova <jlavrova@google.com>
    > Commit-Queue: Julia Lavrova <jlavrova@google.com>
    
    TBR=bungeman@google.com,jlavrova@google.com
    
    Change-Id: I0f2f46fc21eec795026ea98696f68112915f7979
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:10715

--- modules/skparagraph/samples/SampleParagraph.cpp
+++ modules/skparagraph/samples/SampleParagraph.cpp
@@ -3151,6 +3151,66 @@ private:
     using INHERITED = Sample;
 };
 
+class ParagraphView52 : public ParagraphView_Base {
+protected:
+    SkString name() override { return SkString("Paragraph52"); }
+
+    void onDrawContent(SkCanvas* canvas) override {
+        canvas->drawColor(SK_ColorWHITE);
+        //const char* text = "😀😃😄 ABC 😀😃😄 DEF GHI";
+
+        auto fontCollection = sk_make_sp<FontCollection>();
+        fontCollection->setDefaultFontManager(SkFontMgr::RefDefault());
+        fontCollection->enableFontFallback();
+
+
+        {
+        const char* text = " 😀 😃";
+        ParagraphStyle paragraph_style;
+        paragraph_style.turnHintingOff();
+        ParagraphBuilderImpl builder(paragraph_style, fontCollection);
+
+        TextStyle text_style;
+        //text_style.setFontFamilies({SkString("sans-serif")});
+        text_style.setFontFamilies({SkString("Roboto"), SkString("Noto Color Emoji")});
+        text_style.setFontSize(40);
+        text_style.setColor(SK_ColorBLACK);
+        builder.pushStyle(text_style);
+        builder.addText(text, strlen(text));
+        builder.pop();
+
+        auto paragraph = builder.Build();
+        paragraph->layout(width());
+
+        paragraph->paint(canvas, 0, 0);
+        }
+
+        {
+        const char* text = " 😀 A";
+        ParagraphStyle paragraph_style;
+        paragraph_style.turnHintingOff();
+        ParagraphBuilderImpl builder(paragraph_style, fontCollection);
+
+        TextStyle text_style;
+        //text_style.setFontFamilies({SkString("sans-serif")});
+        text_style.setFontFamilies({SkString("Roboto"), SkString("Noto Color Emoji")});
+        text_style.setFontSize(40);
+        text_style.setColor(SK_ColorBLACK);
+        builder.pushStyle(text_style);
+        builder.addText(text, strlen(text));
+        builder.pop();
+
+        auto paragraph = builder.Build();
+        paragraph->layout(width());
+
+        paragraph->paint(canvas, 0, 400);
+        }
+
+    }
+
+private:
+    using INHERITED = Sample;
+};
 
 }  // namespace
 
@@ -3204,3 +3264,4 @@ DEF_SAMPLE(return new ParagraphView48();)
 DEF_SAMPLE(return new ParagraphView49();)
 DEF_SAMPLE(return new ParagraphView50();)
 DEF_SAMPLE(return new ParagraphView51();)
+DEF_SAMPLE(return new ParagraphView52();)
