d32c57d7426efdabfdf11de74e7a46071a06e8df
Mike Klein <mtklein@google.com>
2020-11-12 15:48:30 +0000

    Revert "add ERMS (enhanced rep mov/sto) SkOpts slice"
    This reverts commit 26ad8ccdeca1e8a065c7d45d3620d270da45acd6.
    
    Reason for revert: gonna need to teach MSAN about this to reland.
    
    Original change's description:
    > add ERMS (enhanced rep mov/sto) SkOpts slice
    >
    > Intel's got two CPUID bits indicating the speed of rep mov/sto
    > (memcpy/memset),
    >
    >     - ERMS, Enhanced Rep Mov/Sto, older, large copies are fast?
    >     - FSRM, Fast Short Rep Mov, newer, small copies are fast?
    >
    > ERMS has been around a long time on Intel, but is relatively recent on
    > Ryzen, and FSRM is new across the board.  The startup cost for
    > ERMS-but-not-FSRM copies really is noticeable, so we cut over to the
    > previous SSE/AVX routines when N is small.
    >
    > I've left the memset benchmarks as I found them most useful when
    > tuning the small/large cutoff in this CL.
    >
    > Change-Id: I3ac4e3f34796aba0ea86aabbe9dda7526919456a
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/332580
    > Reviewed-by: Herb Derby <herb@google.com>
    > Commit-Queue: Mike Klein <mtklein@google.com>
    
    TBR=mtklein@google.com,herb@google.com
    
    Change-Id: I3264af132272dbbaac8fc8b62e139a6a112bbadb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/334342
    Reviewed-by: Mike Klein <mtklein@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- src/core/SkOpts.cpp
+++ src/core/SkOpts.cpp
@@ -111,7 +111,6 @@ namespace SkOpts {
     void Init_avx();
     void Init_hsw();
     void Init_skx();
-    void Init_erms();
     void Init_crc32();
 
     static void init() {
@@ -133,8 +132,6 @@ namespace SkOpts {
         #if SK_CPU_SSE_LEVEL < SK_CPU_SSE_LEVEL_SKX
             if (SkCpu::Supports(SkCpu::SKX)) { Init_skx(); }
         #endif
-
-        if (SkCpu::Supports(SkCpu::ERMS)) { Init_erms(); }
 
     #elif defined(SK_CPU_ARM64)
         if (SkCpu::Supports(SkCpu::CRC32)) { Init_crc32(); }
