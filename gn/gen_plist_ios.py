8152dd8f083af3dfb8b41f5f4732d454336c3210
Jim Van Verth <jvanverth@google.com>
2020-11-16 10:24:30 -0500

    Add product bundle name to iOS Xcode projects
    Before this you'd need to set the bundle name to launch the app
    via the Xcode debugger. With this it's set automatically.
    
    Change-Id: Ic84a6c8ba020580d5ff4afa9c104efbc2360b60e

--- gn/gen_plist_ios.py
+++ gn/gen_plist_ios.py
@@ -10,7 +10,9 @@ import sys
 
 # Arguments to the script:
 #  app              path to binary to package, e.g. out/Debug/gen/dm
-app, = sys.argv[1:]
+#  bundle_prefix    the first part of the bundle ID, e.g. com.google (no trailing '.')
+#                   the app name will be appended to this to create the full bundle ID
+app,bundle_prefix = sys.argv[1:3]
 
 out, app = os.path.split(app)
 
@@ -22,7 +24,7 @@ with open(os.path.join(out, app + '_Info.plist'), 'w') as f:
     <key>CFBundleVersion</key> <string>0.1.0</string>
     <key>CFBundleShortVersionString</key> <string>0.1.0</string>
     <key>CFBundleExecutable</key> <string>{app}</string>
-    <key>CFBundleIdentifier</key> <string>com.google.{app}</string>
+    <key>CFBundleIdentifier</key> <string>{bundle_prefix}.{app}</string>
     <key>CFBundlePackageType</key> <string>APPL</string>
     <key>LSRequiresIPhoneOS</key> <true/>
     <key>UIDeviceFamily</key> <array>
@@ -32,4 +34,4 @@ with open(os.path.join(out, app + '_Info.plist'), 'w') as f:
     <key>UILaunchStoryboardName</key> <string>LaunchScreen</string>
   </dict>
 </plist>
-'''.format(app=app))
+'''.format(app=app, bundle_prefix=bundle_prefix))
