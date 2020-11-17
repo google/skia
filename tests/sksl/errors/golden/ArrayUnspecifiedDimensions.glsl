cd113214a35d672b43afd59942b980bf67be78cc
John Stiles <johnstiles@google.com>
2020-11-17 10:48:52 -0500

    Disallow unsized array dimensions on size fields past the frontmost.
    This was slightly complicated by the fact that this syntax indicates an
    array with a known size:
    
        float[] x = float[](1, 2, 3, 4);
    
    Of course, the size is 4; it's just never explicitly stated in the
    code. (The SkSL parser never actually deduces the size, but it doesn't
    apparently have a need to; we don't do much in the way of optimization
    for arrays.) However, this prevents us from simply failing whenever we
    parse "[]" in non-builtin code; we need to keep scanning and see if the
    variable is initialized. We already check this in the
    ArrayConstructors.sksl test file.
    
    Change-Id: I5b86958e81bd9bf5edf28a617cecf95c1875583e
    Bug: skia:10957

---
+++ tests/sksl/errors/golden/ArrayUnspecifiedDimensions.glsl
@@ -0,0 +1,17 @@
+### Compilation failed:
+
+error: 1: arrays without an explicit size must use an initializer expression
+error: 2: array size must be specified
+error: 3: array size must be specified
+error: 4: array size must be specified
+error: 5: array size must be specified
+error: 6: array size must be specified
+error: 7: array size must be specified
+error: 8: array size must be specified
+error: 9: array size must be specified
+error: 10: array size must be specified
+error: 11: array size must be specified
+error: 12: expected 'int', but found 'float'
+error: 13: expected 'int', but found 'bool'
+error: 14: arrays without an explicit size must use an initializer expression
+14 errors
