a186c355741a43143cb4396f6397e610a50ab6d0
Robert Phillips <robertphillips@google.com>
2020-11-17 13:22:05 -0500

    Update the TopoSort test ...
    ... to ensure that, if the topological sort fails, all the nodes still appear in the result.
    
    Change-Id: Ic4acaab2662c872662246b7c1f5471e17c5ba98e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/335639
    Reviewed-by: Adlai Holler <adlai@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/TopoSortTest.cpp
+++ tests/TopoSortTest.cpp
@@ -15,8 +15,10 @@ typedef void (*CreateGraphPF)(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph);
 
 /* Simple diamond
  *       3
+ *      . .
  *     /   \
  *    1     2
+ *    .     .
  *     \   /
  *       0
  */
@@ -31,10 +33,13 @@ static void create_graph0(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
 
 /* Simple chain
  *     3
+ *     ^
  *     |
  *     2
+ *     ^
  *     |
  *     1
+ *     ^
  *     |
  *     0
  */
@@ -46,10 +51,12 @@ static void create_graph1(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
     (*graph)[2]->dependsOn((*graph)[3].get());
 }
 
-/* Loop
+/* Simple Loop
  *       2
+ *      / .
  *     /   \
- *    0 --- 1
+ *    .     \
+ *    0 ---> 1
  */
 static void create_graph2(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
     ToolUtils::TopoTestNode::AllocNodes(graph, 3);
@@ -61,12 +68,16 @@ static void create_graph2(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
 
 /* Double diamond
  *       6
+ *      . .
  *     /   \
  *    4     5
+ *    .     .
  *     \   /
  *       3
+ *      . .
  *     /   \
  *    1     2
+ *    .     .
  *     \   /
  *       0
  */
@@ -86,8 +97,10 @@ static void create_graph3(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
 
 /* Two independent diamonds
  *       3           7
+ *      . .         . .
  *     /   \       /   \
  *    1     2     5     6
+ *    .     .     .     .
  *     \   /       \   /
  *       0           4
  */
@@ -105,6 +118,47 @@ static void create_graph4(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
     (*graph)[6]->dependsOn((*graph)[7].get());
 }
 
+/* Two linked diamonds w/ two loops
+ *       5     6
+ *      / .   . \
+ *     .   \ /   .
+ *    2     3     4
+ *     \    .    /
+ *      .  / \  .
+ *       0     1
+ */
+static void create_graph5(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
+    ToolUtils::TopoTestNode::AllocNodes(graph, 7);
+
+    (*graph)[0]->dependsOn((*graph)[3].get());
+    (*graph)[1]->dependsOn((*graph)[3].get());
+    (*graph)[2]->dependsOn((*graph)[0].get());
+    (*graph)[3]->dependsOn((*graph)[5].get());
+    (*graph)[3]->dependsOn((*graph)[6].get());
+    (*graph)[4]->dependsOn((*graph)[1].get());
+    (*graph)[5]->dependsOn((*graph)[2].get());
+    (*graph)[6]->dependsOn((*graph)[4].get());
+}
+
+/* Two disjoint loops
+ *       2          5
+ *      / .        / .
+ *     /   \      /   \
+ *    .     \    .     \
+ *    0 ---> 1   3 ---> 4
+ */
+static void create_graph6(SkTArray<sk_sp<ToolUtils::TopoTestNode>>* graph) {
+    ToolUtils::TopoTestNode::AllocNodes(graph, 6);
+
+    (*graph)[0]->dependsOn((*graph)[1].get());
+    (*graph)[1]->dependsOn((*graph)[2].get());
+    (*graph)[2]->dependsOn((*graph)[0].get());
+
+    (*graph)[3]->dependsOn((*graph)[4].get());
+    (*graph)[4]->dependsOn((*graph)[5].get());
+    (*graph)[5]->dependsOn((*graph)[3].get());
+}
+
 DEF_TEST(TopoSort, reporter) {
     SkRandom rand;
 
@@ -117,6 +171,8 @@ DEF_TEST(TopoSort, reporter) {
         { create_graph2, false },
         { create_graph3, true  },
         { create_graph4, true  },
+        { create_graph5, false },
+        { create_graph6, false },
     };
 
     for (size_t i = 0; i < SK_ARRAY_COUNT(tests); ++i) {
@@ -124,14 +180,27 @@ DEF_TEST(TopoSort, reporter) {
 
         (tests[i].fCreate)(&graph);
 
+        const int numNodes = graph.count();
+
         ToolUtils::TopoTestNode::Shuffle(&graph, &rand);
 
         bool actualResult = GrTTopoSort<ToolUtils::TopoTestNode>(&graph);
         REPORTER_ASSERT(reporter, actualResult == tests[i].fExpectedResult);
+        REPORTER_ASSERT(reporter, numNodes == graph.count());
 
         if (tests[i].fExpectedResult) {
-            for (int j = 0; j < graph.count(); ++j) {
-                REPORTER_ASSERT(reporter, graph[j]->check());
+            for (const auto& node : graph) {
+                REPORTER_ASSERT(reporter, node->check());
+            }
+        } else {
+            // When the topological sort fails all the nodes should still appear in the result
+            // but their order can be somewhat arbitrary.
+            std::vector<bool> seen(numNodes, false);
+
+            for (const auto& node : graph) {
+                SkASSERT(node);
+                SkASSERT(!seen[node->id()]);
+                seen[node->id()] = true;
             }
         }
 
