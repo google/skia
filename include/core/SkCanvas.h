b27ba538ecd0bb52981c33a9f5731022cd165bdd
Michael Ludwig <michaelludwig@google.com>
2020-11-19 11:30:04 -0500

    Simplify layer bounds syncing and no-device error handling in SkCanvas::internalSaveLayer
    This corrects some subtle bugs that can occur with recording canvas or
    if a device fails to be created for a new layer, where the stashed
    matrix would not be restored properly. Since no new DeviceCM would get
    added in those cases, the canvas' total matrix wouldn't get fixed in the
    paired onRestore() and it would remain dirty for the remainder of the
    canvas's lifetime.
    
    After this change, the underlying SkDevice's bounds are also kept in
    sync with the intent of the saveLayer when kNoLayer_Strategy is used.
    Previously, the bounds would be applied to the canvas' conservative clip
    and quick reject bounds, but the device would remain un-updated. As we
    move towards SkNoPixelsDevice taking over the conservative clip bounds,
    this ensures bounds remain up to date within a saveLayer/restore pair
    even if no layer was allocated.
    
    Change-Id: I5ca389bdd624ea7278106da863a96e9d8f90e2d1
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/335861
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- include/core/SkCanvas.h
+++ include/core/SkCanvas.h
@@ -2561,7 +2561,7 @@ protected:
     // returns false if the entire rectangle is entirely clipped out
     // If non-NULL, The imageFilter parameter will be used to expand the clip
     // and offscreen bounds for any margin required by the filter DAG.
-    bool clipRectBounds(const SkRect* bounds, SaveLayerFlags flags, SkIRect* intersection,
+    bool clipRectBounds(const SkRect* bounds, SkIRect* intersection,
                         const SkImageFilter* imageFilter = nullptr);
 
     SkBaseDevice* getTopDevice() const;
@@ -2605,8 +2605,6 @@ private:
         SkIPoint          fDeviceOrigin;
         bool              fDone;
     };
-
-    static bool BoundsAffectsClip(SaveLayerFlags);
 
     static void DrawDeviceWithFilter(SkBaseDevice* src, const SkImageFilter* filter,
                                      SkBaseDevice* dst, const SkIPoint& dstOrigin,
