a3f9754bf7ac4fdffd807a9352a544952e344c8d
John Stiles <johnstiles@google.com>
2020-11-19 11:06:47 -0500

    Optimize swizzled multiple-argument constructors.
    This will reorder constructors with swizzles applied, such as
        `half4(1, 2, 3, 4).xxyz` --> `half4(1, 1, 2, 3)`
        `half4(1, colRGB).yzwx` --> `half4(colRGB.x, colRGB.y, colRGB.z, 1)`
    
    Note that, depending on the swizzle components, some elements of the
    constructor may be duplicated and others may be eliminated. The
    optimizer makes sure to leave the swizzle alone if it would duplicate
    anything non-trivial, or if it would eliminate anything with a side
    effect.
    
    Change-Id: I470fda217ae8cf5828406b89a5696ca6aebf608d
    Bug: skia:10954

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -1117,6 +1117,7 @@ void Compiler::simplifyExpression(DefinitionMap& definitions,
 
                     // No fUsage change: `half4(foo).xy` and `half2(foo)` have equivalent reference
                     // counts.
+                    optimizationContext->fUpdated = true;
                     if (!try_replace_expression(&b, iter, &replacement)) {
                         optimizationContext->fNeedsRescan = true;
                         return;
@@ -1125,7 +1126,99 @@ void Compiler::simplifyExpression(DefinitionMap& definitions,
                     break;
                 }
 
-                // TODO(skia:10954): Optimize the multiple-argument case.
+                // Swizzles can duplicate some elements and discard others, e.g.
+                // `half4(1, 2, 3, 4).xxz` --> `half3(1, 1, 3)`. However, there are constraints:
+                // - Expressions with side effects need to occur exactly once, even if they
+                //   would otherwise be swizzle-eliminated
+                // - Non-trivial expressions should not be repeated, but elimination is OK.
+                //
+                // Look up the argument for the constructor at each index. This is typically simple
+                // but for weird cases like `half4(bar.yz, half2(foo))`, it can be harder than it
+                // seems. This example would result in:
+                //     argMap[0] = {.fArgIndex = 0, .fComponent = 0}   (bar.yz     .x)
+                //     argMap[1] = {.fArgIndex = 0, .fComponent = 1}   (bar.yz     .y)
+                //     argMap[2] = {.fArgIndex = 1, .fComponent = 0}   (half2(foo) .x)
+                //     argMap[3] = {.fArgIndex = 1, .fComponent = 1}   (half2(foo) .y)
+                struct ConstructorArgMap {
+                    int8_t fArgIndex;
+                    int8_t fComponent;
+                };
+
+                int numConstructorArgs = base.type().columns();
+                ConstructorArgMap argMap[4] = {};
+                int writeIdx = 0;
+                for (int argIdx = 0; argIdx < (int) base.arguments().size(); ++argIdx) {
+                    const Expression& expr = *base.arguments()[argIdx];
+                    int argWidth = expr.type().columns();
+                    for (int componentIdx = 0; componentIdx < argWidth; ++componentIdx) {
+                        argMap[writeIdx].fArgIndex = argIdx;
+                        argMap[writeIdx].fComponent = componentIdx;
+                        ++writeIdx;
+                    }
+                }
+                SkASSERT(writeIdx == numConstructorArgs);
+
+                // Count up the number of times each constructor argument is used by the
+                // swizzle.
+                //    `half4(bar.yz, half2(foo)).xwxy` -> { 3, 1 }
+                // - bar.yz    is referenced 3 times, by `.x_xy`
+                // - half(foo) is referenced 1 time,  by `._w__`
+                int8_t exprUsed[4] = {};
+                for (int c : s.components()) {
+                    exprUsed[argMap[c].fArgIndex]++;
+                }
+
+                bool safeToOptimize = true;
+                for (int index = 0; index < numConstructorArgs; ++index) {
+                    int8_t constructorArgIndex = argMap[index].fArgIndex;
+                    const Expression& baseArg = *base.arguments()[constructorArgIndex];
+
+                    // Check that non-trivial expressions are not swizzled in more than once.
+                    if (exprUsed[constructorArgIndex] > 1 && !baseArg.isConstantOrUniform()) {
+                        safeToOptimize = false;
+                        break;
+                    }
+                    // Check that side-effect-bearing expressions are swizzled in exactly once.
+                    if (exprUsed[constructorArgIndex] != 1 && baseArg.hasSideEffects()) {
+                        safeToOptimize = false;
+                        break;
+                    }
+                }
+
+                if (safeToOptimize) {
+                    // Create a new constructor, with the arguments from the original constructor
+                    // reordered and swizzled to match the original swizzle permutation. Note that
+                    // we expect followup passes to optimize things like `bar.yz.x` into `bar.y`.
+                    ExpressionArray newArgs;
+                    newArgs.reserve_back(swizzleSize);
+                    for (int c : s.components()) {
+                        const ConstructorArgMap& argument = argMap[c];
+                        const Expression& baseArg = *base.arguments()[argument.fArgIndex];
+
+                        if (baseArg.type().typeKind() == Type::TypeKind::kScalar) {
+                            SkASSERT(argument.fComponent == 0);
+                            newArgs.push_back(baseArg.clone());
+                        } else {
+                            SkASSERT(argument.fComponent < baseArg.type().columns());
+                            newArgs.push_back(std::make_unique<Swizzle>(
+                                    *fContext, baseArg.clone(),
+                                    ComponentArray{argument.fComponent}));
+                        }
+                    }
+                    replacement = std::make_unique<Constructor>(
+                            base.fOffset,
+                            &componentType.toCompound(*fContext, swizzleSize, /*rows=*/1),
+                            std::move(newArgs));
+
+                    // Remove references within 'expr', add references within 'optimized'
+                    optimizationContext->fUpdated = true;
+                    optimizationContext->fUsage->replace(expr, replacement.get());
+                    if (!try_replace_expression(&b, iter, &replacement)) {
+                        optimizationContext->fNeedsRescan = true;
+                        return;
+                    }
+                    SkASSERT((*iter)->isExpression());
+                }
                 break;
             }
             break;
