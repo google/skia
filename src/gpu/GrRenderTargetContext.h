e863697cf31e6a651e7437cf119e40cea80e0af8
Robert Phillips <robertphillips@google.com>
2020-11-20 11:27:28 -0500

    [Blocked] Hold renderTasks via a unique_ptr
    What is blocking this CL is a conflict between the drawingManager's DAG
    and the renderTargetContexts. The DAG is the final owner of the
    renderTasks but the RTCs have reffing pointers to their opsTasks
    while they are being generated.
    
    In the world with partial flushing and opList-splitting reduction,
    it seems like a RTC/renderTask pair could survive a flush which would
    be awkward w/ unique_ptrs (where only the DAG held a ref). In this
    case the drawingManager would be making decisions about the lifetime
    of the renderTasks w/o knowing about the existence of the RTC.
    The drawing manager could more robustly track the RTCs (by having
    a separate list of active ones) and that could signal when a renderTask
    needs to continue existing.
    
    Of course, what does it mean for a RTC to extend the life of a
    renderTask if the drawing manager is done with it? That seems to
    argue that, in that case, we should orphan the RTC.
    
    Change-Id: Ic43f2a24580e1376fe9d714a8e99a8a60d4972da

--- src/gpu/GrRenderTargetContext.h
+++ src/gpu/GrRenderTargetContext.h
@@ -589,7 +589,7 @@ public:
 #if GR_TEST_UTILS
     bool testingOnly_IsInstantiated() const { return this->asSurfaceProxy()->isInstantiated(); }
     void testingOnly_SetPreserveOpsOnFullClear() { fPreserveOpsOnFullClear_TestingOnly = true; }
-    GrOpsTask* testingOnly_PeekLastOpsTask() { return fOpsTask.get(); }
+    GrOpsTask* testingOnly_PeekLastOpsTask1() { return fOpsTask1; }
 #endif
 
 private:
@@ -709,7 +709,7 @@ private:
 
     // In MDB-mode the GrOpsTask can be closed by some other renderTargetContext that has picked
     // it up. For this reason, the GrOpsTask should only ever be accessed via 'getOpsTask'.
-    sk_sp<GrOpsTask> fOpsTask;
+    GrOpsTask* fOpsTask1;
 
     SkSurfaceProps fSurfaceProps;
     bool fFlushTimeOpsTask;
