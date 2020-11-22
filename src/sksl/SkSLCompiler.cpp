56fc3ec02148783c43afb7ebedb877e9853a4f0c
John Stiles <johnstiles@google.com>
2020-11-21 23:26:07 -0500

    Flatten out constructors nested inside constructors.
    - float4(float2(1, 2), 3, 4)   -->  float4(1, 2, 3, 4)
    - half3(z, half2(fn(x), y*2))  -->  half3(z, fn(x), y*2)
    
    Single-argument constructors will be ignored by this optimization; these
    might be casts or splats.
    
    This had an unexpected side benefit of simplifying some Metal output,
    as we need to output fewer Metal matrix construction helper functions
    when matrices use more simple scalars for construction.
    
    Change-Id: I0a161db060c107e35247901619291bf83801cb11

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -1075,6 +1075,58 @@ void Compiler::simplifyExpression(DefinitionMap& definitions,
             }
             break;
         }
+        case Expression::Kind::kConstructor: {
+            // Find constructors embedded inside constructors and flatten them out where possible.
+            //   -  float4(float2(1, 2), 3, 4)                -->  float4(1, 2, 3, 4)
+            //   -  float4(w, float3(sin(x), cos(y), tan(z))) -->  float4(w, sin(x), cos(y), tan(z))
+            // Leave single-argument constructors alone, though. These might be casts or splats.
+            Constructor& c = expr->as<Constructor>();
+            if (c.type().columns() > 1) {
+                // Inspect each constructor argument to see if it's a candidate for flattening.
+                // Remember matched arguments in a bitfield, "argsToOptimize".
+                int argsToOptimize = 0;
+                int currBit = 1;
+                for (const std::unique_ptr<Expression>& arg : c.arguments()) {
+                    if (arg->is<Constructor>()) {
+                        Constructor& inner = arg->as<Constructor>();
+                        if (inner.arguments().size() > 1 &&
+                            inner.type().componentType() == c.type().componentType()) {
+                            argsToOptimize |= currBit;
+                        }
+                    }
+                    currBit <<= 1;
+                }
+                if (argsToOptimize) {
+                    // We found at least one argument that could be flattened out. Re-walk the
+                    // constructor args and flatten the candidates we found during our initial pass.
+                    ExpressionArray flattened;
+                    flattened.reserve_back(c.type().columns());
+                    currBit = 1;
+                    for (const std::unique_ptr<Expression>& arg : c.arguments()) {
+                        if (argsToOptimize & currBit) {
+                            Constructor& inner = arg->as<Constructor>();
+                            for (const std::unique_ptr<Expression>& innerArg : inner.arguments()) {
+                                flattened.push_back(innerArg->clone());
+                            }
+                        } else {
+                            flattened.push_back(arg->clone());
+                        }
+                        currBit <<= 1;
+                    }
+                    auto optimized = std::unique_ptr<Expression>(
+                            new Constructor(c.fOffset, &c.type(), std::move(flattened)));
+                    // No fUsage change; no references have been added or removed anywhere.
+                    optimizationContext->fUpdated = true;
+                    if (!try_replace_expression(&b, iter, &optimized)) {
+                        optimizationContext->fNeedsRescan = true;
+                        return;
+                    }
+                    SkASSERT((*iter)->isExpression());
+                    break;
+                }
+            }
+            break;
+        }
         case Expression::Kind::kSwizzle: {
             Swizzle& s = expr->as<Swizzle>();
             // Detect identity swizzles like `foo.rgba`.
