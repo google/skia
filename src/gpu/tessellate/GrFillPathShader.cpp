96307c60ba9254a9ac7a97e1e0e743bbeabecb6d
Chris Dalton <csmartdalton@google.com>
2020-11-22 23:52:47 -0700

    Add conic fill support to the tessellator
    This started out as a sandbox to experiment with a Wang's formula
    analog for rational quadratics, but it quickly became apparent that
    running Wang's formula on the down-projected points was an upper bound
    on what the rational version would have been (for both w<1 and w>1).
    
    This CL therefore adds conic support by upgrading the tessellation
    shaders to use ratoinal cubics, converting every path verb to a
    rational cubic, and then running Wang's formula on the down-projected
    points. In the future we can always drop in a better formula if we
    work one out.
    
    Bug: skia:10419
    Change-Id: I97021ea56afea54fdbe76745bacd3251e350fd97

--- src/gpu/tessellate/GrFillPathShader.cpp
+++ src/gpu/tessellate/GrFillPathShader.cpp
@@ -63,49 +63,60 @@ GrGLSLPrimitiveProcessor* GrFillPathShader::createGLSLInstance(const GrShaderCap
 void GrFillTriangleShader::emitVertexCode(Impl*, GrGLSLVertexBuilder* v, const char* viewMatrix,
                                           GrGLSLUniformHandler* uniformHandler) const {
     v->codeAppendf(R"(
-            localcoord = input_point;
-            vertexpos = (%s * float3(localcoord, 1)).xy;)", viewMatrix);
+    localcoord = input_point;
+    vertexpos = (%s * float3(localcoord, 1)).xy;)", viewMatrix);
 }
 
 void GrFillCubicHullShader::emitVertexCode(Impl*, GrGLSLVertexBuilder* v, const char* viewMatrix,
                                            GrGLSLUniformHandler* uniformHandler) const {
     v->codeAppend(R"(
-            float4x2 P = float4x2(input_points_0_1, input_points_2_3);
+    float4x2 P = float4x2(input_points_0_1, input_points_2_3);
+    if (isnan(P[3].y)) {
+        // This curve is actually a conic. Convert the control points to a trapeziodal hull
+        // that circumcscribes the conic.
+        float w = P[3].x;
+        float2 p1w = P[1] * w;
+        float T = .51;  // Bias outward a bit to ensure we cover the outermost samples.
+        float2 c1 = mix(P[0], p1w, T);
+        float2 c2 = mix(P[2], p1w, T);
+        float iw = 1 / mix(1, w, T);
+        P = float4x2(P[0], c1 * iw, c2 * iw, P[2]);
+    }
 
-            // Translate the points to v0..3 where v0=0.
-            float2 v1 = P[1] - P[0], v2 = P[2] - P[0], v3 = P[3] - P[0];
+    // Translate the points to v0..3 where v0=0.
+    float2 v1 = P[1] - P[0], v2 = P[2] - P[0], v3 = P[3] - P[0];
 
-            // Reorder the points so v2 bisects v1 and v3.
-            if (sign(determinant(float2x2(v2,v1))) == sign(determinant(float2x2(v2,v3)))) {
-                float2 tmp = P[2];
-                if (sign(determinant(float2x2(v1,v2))) != sign(determinant(float2x2(v1,v3)))) {
-                    P[2] = P[1];  // swap(P2, P1)
-                    P[1] = tmp;
-                } else {
-                    P[2] = P[3];  // swap(P2, P3)
-                    P[3] = tmp;
-                }
-            }
+    // Reorder the points so v2 bisects v1 and v3.
+    if (sign(determinant(float2x2(v2,v1))) == sign(determinant(float2x2(v2,v3)))) {
+        float2 tmp = P[2];
+        if (sign(determinant(float2x2(v1,v2))) != sign(determinant(float2x2(v1,v3)))) {
+            P[2] = P[1];  // swap(P2, P1)
+            P[1] = tmp;
+        } else {
+            P[2] = P[3];  // swap(P2, P3)
+            P[3] = tmp;
+        }
+    }
 
-            // Find the "turn direction" of each corner and net turn direction.
-            float4 dir;
-            float netdir = 0.0;
-            for (int i = 0; i < 4; ++i) {
-                float2 prev = P[i] - P[(i + 3) & 3], next = P[(i + 1) & 3] - P[i];
-                dir[i] = sign(determinant(float2x2(prev, next)));
-                netdir += dir[i];
-            }
+    // Find the "turn direction" of each corner and net turn direction.
+    float4 dir;
+    float netdir = 0.0;
+    for (int i = 0; i < 4; ++i) {
+        float2 prev = P[i] - P[(i + 3) & 3], next = P[(i + 1) & 3] - P[i];
+        dir[i] = sign(determinant(float2x2(prev, next)));
+        netdir += dir[i];
+    }
 
-            // sk_VertexID comes in fan order. Convert to strip order.
-            int vertexidx = sk_VertexID;
-            vertexidx ^= vertexidx >> 1;
+    // sk_VertexID comes in fan order. Convert to strip order.
+    int vertexidx = sk_VertexID;
+    vertexidx ^= vertexidx >> 1;
 
-            // Remove the non-convex vertex, if any.
-            if (dir[vertexidx] != sign(netdir)) {
-                vertexidx = (vertexidx + 1) & 3;
-            }
+    // Remove the non-convex vertex, if any.
+    if (dir[vertexidx] != sign(netdir)) {
+        vertexidx = (vertexidx + 1) & 3;
+    }
 
-            localcoord = P[vertexidx];)");
+    localcoord = P[vertexidx];)");
 
     v->codeAppendf("vertexpos = (%s * float3(localcoord, 1)).xy;", viewMatrix);
 }
@@ -118,14 +129,14 @@ void GrFillBoundingBoxShader::emitVertexCode(Impl* impl, GrGLSLVertexBuilder* v,
             nullptr, kVertex_GrShaderFlag, kFloat4_GrSLType, "path_bounds", &pathBounds);
 
     v->codeAppendf(R"(
-            // Use sk_VertexID and uniforms (instead of vertex data) to find vertex positions.
-            float2 T = float2(sk_VertexID & 1, sk_VertexID >> 1);
-            localcoord = mix(%s.xy, %s.zw, T);
-            vertexpos = (%s * float3(localcoord, 1)).xy;
+    // Use sk_VertexID and uniforms (instead of vertex data) to find vertex positions.
+    float2 T = float2(sk_VertexID & 1, sk_VertexID >> 1);
+    localcoord = mix(%s.xy, %s.zw, T);
+    vertexpos = (%s * float3(localcoord, 1)).xy;
 
-            // Outset to avoid possible T-junctions with extreme edges of the path.
-            float2x2 M2 = float2x2(%s);
-            float2 devoutset = .25 * sign(M2 * (T - .5));
-            localcoord += inverse(M2) * devoutset;
-            vertexpos += devoutset;)", pathBounds, pathBounds, viewMatrix, viewMatrix);
+    // Outset to avoid possible T-junctions with extreme edges of the path.
+    float2x2 M2 = float2x2(%s);
+    float2 devoutset = .25 * sign(M2 * (T - .5));
+    localcoord += inverse(M2) * devoutset;
+    vertexpos += devoutset;)", pathBounds, pathBounds, viewMatrix, viewMatrix);
 }
