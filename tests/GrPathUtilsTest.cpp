a29943f78ff064a4dd79e599d0b1f511db30a1f4
Chris Dalton <csmartdalton@google.com>
2020-11-23 20:19:14 -0700

    Add cusp detection to GrPathUtils::findCubicConvex180Chops
    The stroker needs to know the cusps in order to draw circles around
    them.
    
    Bug: skia:10419
    Change-Id: I05b7e9f4a5ed06bd36450e73edfaf36c4b3f5a6c

--- tests/GrPathUtilsTest.cpp
+++ tests/GrPathUtilsTest.cpp
@@ -19,18 +19,23 @@ static bool is_linear(const SkPoint p[4]) {
 }
 
 static void check_cubic_convex_180(skiatest::Reporter* r, const SkPoint p[4]) {
+    bool areCusps = false;
     float inflectT[2], convex180T[2];
     if (int inflectN = SkFindCubicInflections(p, inflectT)) {
         // The curve has inflections. findCubicConvex180Chops should return the inflection
         // points.
-        int convex180N = GrPathUtils::findCubicConvex180Chops(p, convex180T);
+        int convex180N = GrPathUtils::findCubicConvex180Chops(p, convex180T, &areCusps);
         REPORTER_ASSERT(r, inflectN == convex180N);
+        if (!areCusps) {
+            REPORTER_ASSERT(r, inflectN == 1 ||
+                            fabsf(inflectT[0] - inflectT[1]) >= SK_ScalarNearlyZero);
+        }
         for (int i = 0; i < convex180N; ++i) {
             REPORTER_ASSERT(r, SkScalarNearlyEqual(inflectT[i], convex180T[i]));
         }
     } else {
         float totalRotation = SkMeasureNonInflectCubicRotation(p);
-        int convex180N = GrPathUtils::findCubicConvex180Chops(p, convex180T);
+        int convex180N = GrPathUtils::findCubicConvex180Chops(p, convex180T, &areCusps);
         SkPoint chops[10];
         SkChopCubicAt(p, chops, convex180T, convex180N);
         float radsSum = 0;
@@ -53,6 +58,9 @@ static void check_cubic_convex_180(skiatest::Reporter* r, const SkPoint p[4]) {
                 REPORTER_ASSERT(r, SkScalarNearlyEqual(
                     SkMeasureNonInflectCubicRotation(chops + 3), totalRotation - SK_ScalarPI));
             }
+            REPORTER_ASSERT(r, !areCusps);
+        } else {
+            REPORTER_ASSERT(r, areCusps);
         }
     }
 }
@@ -81,6 +89,37 @@ DEF_TEST(GrPathUtils_findCubicConvex180Chops, r) {
     SkPoint quad[4] = {{0,0}, {2,2}, {4,2}, {6,0}};
     float T[2];
     REPORTER_ASSERT(r, GrPathUtils::findCubicConvex180Chops(quad, T) == 0);
+
+    // Now test that cusps and near-cusps get flagged as cusps.
+    SkPoint cusp[4] = {{0,0}, {1,1}, {1,0}, {0,1}};
+    bool areCusps = false;
+    REPORTER_ASSERT(r, GrPathUtils::findCubicConvex180Chops(cusp, T, &areCusps) == 1);
+    REPORTER_ASSERT(r, areCusps == true);
+
+    // Find the height of the right side of "cusp" at which the distance between its inflection
+    // points is kEpsilon (in parametric space).
+    constexpr static double kEpsilon = 1.0 / (1 << 11);
+    constexpr static double kEpsilonSquared = kEpsilon * kEpsilon;
+    double h = (1 - kEpsilonSquared) / (3 * kEpsilonSquared + 1);
+    double dy = (1 - h) / 2;
+    cusp[1].fY = (float)(1 - dy);
+    cusp[2].fY = (float)(0 + dy);
+    REPORTER_ASSERT(r, SkFindCubicInflections(cusp, T) == 2);
+    REPORTER_ASSERT(r, SkScalarNearlyEqual(T[1] - T[0], (float)kEpsilon, (float)kEpsilonSquared));
+
+    // Ensure two inflection points barely more than kEpsilon apart do not get flagged as cusps.
+    cusp[1].fY = (float)(1 - 1.1 * dy);
+    cusp[2].fY = (float)(0 + 1.1 * dy);
+    areCusps = false;
+    REPORTER_ASSERT(r, GrPathUtils::findCubicConvex180Chops(cusp, T, &areCusps) == 2);
+    REPORTER_ASSERT(r, areCusps == false);
+
+    // Ensure two inflection points barely less than kEpsilon apart do get flagged as cusps.
+    cusp[1].fY = (float)(1 - .9 * dy);
+    cusp[2].fY = (float)(0 + .9 * dy);
+    areCusps = false;
+    REPORTER_ASSERT(r, GrPathUtils::findCubicConvex180Chops(cusp, T, &areCusps) == 1);
+    REPORTER_ASSERT(r, areCusps == true);
 }
 
 DEF_TEST(GrPathUtils_convertToCubic, r) {
