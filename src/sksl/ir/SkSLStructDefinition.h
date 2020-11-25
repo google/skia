b3b087161e7f6787df57195878a644e0e9932d73
John Stiles <johnstiles@google.com>
2020-11-25 15:56:56 -0500

    Add global struct definitions to SkSL.
    Previously, GLSL and Metal code generators would emit a struct wherever
    the type was first used in the code, regardless of where it was
    originally defined or what scope the type needs to live in. This CL adds
    a ProgramElement for struct definitions, so that structs will now appear
    at the top-level as they were originally defined. In the case of Metal,
    some special handling is also needed to handle the Globals struct
    properly.
    
    Not yet fully supported:
    - No special handling for structs declared inside functions yet
    - No support for structs in separate scopes with overlapping names
    The severity of the remaining issues depends mostly on whether we want
    to support structs inside functions in Runtime Effects.
    
    Change-Id: Ia95d4529506cb3fa6da63f5cb548199a93e1c0c5
    Bug: skia:10922, skia:10923, skia:10925, skia:10926

---
+++ src/sksl/ir/SkSLStructDefinition.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_STRUCTDEFINITION
+#define SKSL_STRUCTDEFINITION
+
+#include <memory>
+
+#include "src/sksl/ir/SkSLProgramElement.h"
+#include "src/sksl/ir/SkSLSymbolTable.h"
+#include "src/sksl/ir/SkSLType.h"
+
+namespace SkSL {
+
+/**
+ * A struct at global scope, as in:
+ *
+ * struct RenderData {
+ *   float3 color;
+ *   bool highQuality;
+ * };
+ */
+class StructDefinition final : public ProgramElement {
+public:
+    static constexpr Kind kProgramElementKind = Kind::kStructDefinition;
+
+    StructDefinition(int offset, const Type& type, std::shared_ptr<SymbolTable> typeOwner)
+    : INHERITED(offset, kProgramElementKind)
+    , fType(&type)
+    , fTypeOwner(std::move(typeOwner)) {}
+
+    const Type& type() const {
+        return *fType;
+    }
+
+    std::shared_ptr<SymbolTable> typeOwner() const {
+        return fTypeOwner;
+    }
+
+    std::unique_ptr<ProgramElement> clone() const override {
+        return std::make_unique<StructDefinition>(fOffset, this->type(),
+                                                  SymbolTable::WrapIfBuiltin(this->typeOwner()));
+    }
+
+    String description() const override {
+        String s = "struct ";
+        s += this->type().name();
+        s += " { ";
+        for (const auto& f : this->type().fields()) {
+            s += f.fModifiers.description();
+            s += f.fType->description();
+            s += " ";
+            s += f.fName;
+            s += "; ";
+        }
+        s += "};";
+        return s;
+    }
+
+private:
+    const Type* fType = nullptr;
+    std::shared_ptr<SymbolTable> fTypeOwner;
+
+    using INHERITED = ProgramElement;
+};
+
+}  // namespace SkSL
+
+#endif
