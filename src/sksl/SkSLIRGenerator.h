cc1795038ef06563edbfa363c35856c3bba0788c
John Stiles <johnstiles@google.com>
2020-11-25 14:39:30 -0500

    Add global struct definitions to SkSL.
    Change-Id: Ia95d4529506cb3fa6da63f5cb548199a93e1c0c5

--- src/sksl/SkSLIRGenerator.h
+++ src/sksl/SkSLIRGenerator.h
@@ -34,6 +34,7 @@ namespace SkSL {
 
 class ExternalValue;
 class FunctionCall;
+class StructDefinition;
 struct ParsedModule;
 struct Swizzle;
 
@@ -209,6 +210,7 @@ private:
     std::unique_ptr<Expression> convertIndexExpression(const ASTNode& expression);
     std::unique_ptr<Expression> convertPostfixExpression(const ASTNode& expression);
     std::unique_ptr<Expression> convertScopeExpression(const ASTNode& expression);
+    std::unique_ptr<StructDefinition> convertStructDefinition(const ASTNode& expression);
     std::unique_ptr<Expression> convertTypeField(int offset, const Type& type,
                                                  StringFragment field);
     std::unique_ptr<Expression> convertField(std::unique_ptr<Expression> base,
