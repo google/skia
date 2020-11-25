b9461b9d0d83e9105d848e8e906cede89d09baa8
John Stiles <johnstiles@google.com>
2020-11-25 13:39:20 -0500

    Remove accidentally-commited conflict markers.
    Change-Id: Ifc1f0921d983ee09d7bc2632aeca41689f1bf0c3

--- tests/sksl/shared/golden/Operators.glsl
+++ tests/sksl/shared/golden/Operators.glsl
@@ -1,4 +1,27 @@
-### Compilation failed:
 
-error: 22: expected expression, but found '<<'
-1 error
+void main() {
+    float x = 1.0;
+    float y = 2.0;
+
+    int z = 3;
+    x = -6.0;
+    y = -1.0;
+    z = 8;
+    bool b = false == true || 2.0 >= sqrt(2.0);
+    bool c = sqrt(2.0) > 2.0;
+    bool d = b ^^ c;
+    bool e = b && c;
+    bool f = b || c;
+    x += 12.0;
+    x -= 12.0;
+    x *= (y /= float(z = 10));
+    z |= 0;
+    z &= -1;
+    z ^= 0;
+    z >>= 2;
+    z <<= 4;
+    z %= 5;
+    x = float((vec2(sqrt(1.0)) , 6));
+    y = (((((b ? 1.0 : 0.0) * (c ? 1.0 : 0.0)) * (d ? 1.0 : 0.0)) * (e ? 1.0 : 0.0)) * (f ? 1.0 : 0.0) , 6.0);
+    z = (vec2(sqrt(1.0)) , 6);
+}
