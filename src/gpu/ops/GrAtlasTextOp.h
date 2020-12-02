f6acb1fec8ccf24bede8aa861afcfd15bae09c3f
Herb Derby <herb@google.com>
2020-12-02 17:44:54 -0500

    geom linked list
    Change-Id: I32af5724e7abeca1ddb6d38b26afbff7919cbc76

--- src/gpu/ops/GrAtlasTextOp.h
+++ src/gpu/ops/GrAtlasTextOp.h
@@ -20,8 +20,8 @@ public:
     DEFINE_OP_CLASS_ID
 
     ~GrAtlasTextOp() override {
-        for (const auto& g : fGeometries.items()) {
-            g.fBlob->unref();
+        for (const Geometry* g = fHead; g != nullptr; g = g->fNext) {
+            g->fBlob->unref();
         }
     }
 
@@ -29,18 +29,31 @@ public:
     static const int kIndicesPerGlyph = 6;
 
     struct Geometry {
+        Geometry(const GrAtlasSubRun& subRun,
+                 const SkMatrix& drawMatrix,
+                 SkPoint drawOrigin,
+                 SkIRect clipRect,
+                 GrTextBlob* blob,
+                 const SkPMColor4f& color)
+             : fSubRun{subRun}
+             , fDrawMatrix{drawMatrix}
+             , fDrawOrigin{drawOrigin}
+             , fClipRect{clipRect}
+             , fBlob{blob}
+             , fColor {color} {}
         void fillVertexData(void* dst, int offset, int count) const;
 
         const GrAtlasSubRun& fSubRun;
-        const SkMatrix       fDrawMatrix;
-        const SkPoint        fDrawOrigin;
-        const SkIRect        fClipRect;
-        GrTextBlob* const    fBlob;  // mutable to make unref call in Op dtor.
+        const SkMatrix fDrawMatrix;
+        const SkPoint fDrawOrigin;
+        const SkIRect fClipRect;
+        GrTextBlob* const fBlob;  // mutable to make unref call in Op dtor.
 
         // Color is updated after processor analysis if it was determined the shader resolves to
         // a constant color that we then evaluate on the CPU.
         // TODO: This can be made const once processor analysis is separated from op creation.
-        SkPMColor4f          fColor;
+        SkPMColor4f fColor;
+        Geometry* fNext{nullptr};
     };
 
     const char* name() const override { return "AtlasTextOp"; }
@@ -78,23 +91,6 @@ public:
 private:
     friend class GrOp; // for ctor
 
-    GrAtlasTextOp(MaskType maskType,
-                  bool needsTransform,
-                  int glyphCount,
-                  SkRect deviceRect,
-                  const Geometry& geo,
-                  GrPaint&& paint);
-
-    GrAtlasTextOp(MaskType maskType,
-                  bool needsTransform,
-                  int glyphCount,
-                  SkRect deviceRect,
-                  SkColor luminanceColor,
-                  bool useGammaCorrectDistanceTable,
-                  uint32_t DFGPFlags,
-                  const Geometry& geo,
-                  GrPaint&& paint);
-
     struct FlushInfo {
         sk_sp<const GrBuffer> fVertexBuffer;
         sk_sp<const GrBuffer> fIndexBuffer;
@@ -105,9 +101,34 @@ private:
         int fNumDraws = 0;
     };
 
+    GrAtlasTextOp(MaskType maskType,
+                  bool needsTransform,
+                  int glyphCount,
+                  SkRect deviceRect,
+                  Geometry* geo,
+                  GrPaint&& paint);
+
+    GrAtlasTextOp(MaskType maskType,
+                  bool needsTransform,
+                  int glyphCount,
+                  SkRect deviceRect,
+                  SkColor luminanceColor,
+                  bool useGammaCorrectDistanceTable,
+                  uint32_t DFGPFlags,
+                  Geometry* geo,
+                  GrPaint&& paint);
+
     GrProgramInfo* programInfo() override {
         // TODO [PI]: implement
         return nullptr;
+    }
+
+    void addGeometry(Geometry* geometry) {
+        *fTail = geometry;
+        // The geometry may have many entries. Find the end.
+        do {
+            fTail = &(*fTail)->fNext;
+        } while (*fTail != nullptr);
     }
 
     void onCreateProgramInfo(const GrCaps*,
@@ -181,14 +202,6 @@ private:
                                           const GrSurfaceProxyView* views,
                                           unsigned int numActiveViews) const;
 
-    // The minimum number of Geometry we will try to allocate as ops are merged together.
-    // The atlas text op holds one Geometry inline. When combined with the linear growth policy,
-    // the total number of geometries follows 6, 18, 36, 60, 90 (the deltas are 6*n).
-    static constexpr auto kMinGeometryAllocated = 6;
-
-    GrTBlockList<Geometry> fGeometries{kMinGeometryAllocated,
-                                       GrBlockAllocator::GrowthPolicy::kLinear};
-
     GrProcessorSet fProcessors;
     int fNumGlyphs; // Sum of glyphs in each geometry's subrun
 
@@ -205,6 +218,9 @@ private:
     // Only used for distance fields; per-channel luminance for LCD, or gamma-corrected luminance
     // for single-channel distance fields.
     const SkColor fLuminanceColor{0};
+
+    Geometry* fHead;
+    Geometry** fTail{&fHead};
 
     using INHERITED = GrMeshDrawOp;
 };
