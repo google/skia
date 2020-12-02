a169f17a14eb13636111072aabe3336512d8e76e
John Stiles <johnstiles@google.com>
2020-12-02 12:29:07 -0500

    Fix flipped array dimensions in SkSL.
    The proper approach for creating multi-dimensional array types is
    complicated, so I added a function in SymbolTable which does it the
    right way (addArrayDimensions). I found all the places in SkSL which
    created arrays from base types and size arrays, and refactored them to
    call addArrayDimensions instead of doing it manually.
    
    I believe that this approach fixes a bunch of minor issues with multi-
    dimensional array types; some are visible in the current codegen output,
    and others are latent bugs. e.g. in some instances, a Variable's type()
    was silently holding flipped array dimensions, but this never led to
    a visible bug because we ended up using the VarDeclaration's baseType()
    plus sizes() everywhere that the type was used. (In particular, this
    caused debugging headaches in http://review.skia.org/340137 where I'd
    use a Variable's type and suddenly its array dimensions would be wrong.)
    
    Change-Id: Idd6a86aa5d1dce8918d02a53bcc2f7d7886e3ac5
    Bug: skia:11016, skia:10924

--- src/sksl/ir/SkSLSymbolTable.h
+++ src/sksl/ir/SkSLSymbolTable.h
@@ -8,8 +8,10 @@
 #ifndef SKSL_SYMBOLTABLE
 #define SKSL_SYMBOLTABLE
 
+#include "include/private/SkTArray.h"
 #include "include/private/SkTHash.h"
 #include "src/sksl/SkSLErrorReporter.h"
+#include "src/sksl/SkSLString.h"
 #include "src/sksl/ir/SkSLSymbol.h"
 
 #include <memory>
@@ -70,6 +72,13 @@ public:
         fOwnedNodes.push_back(std::move(node));
         return ptr;
     }
+
+    /**
+     * Given baseType = `float` and dimensions = {1, 2, 3}, creates the full chain of required
+     * array types in the symbol table: `float[3]`, `float[2][3]`, and `float[1][2][3]`. The
+     * fully-dimensioned array type is returned. `kUnsizedArray` can be passed as a `[]` dimension.
+     */
+    const Type* addArrayDimensions(const Type* type, const SkTArray<int>& dimensions);
 
     // Call fn for every symbol in the table.  You may not mutate anything.
     template <typename Fn>
