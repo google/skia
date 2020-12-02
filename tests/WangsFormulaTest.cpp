78b207dad7ac0168286b05d3c04e836b0db80036
Tyler Denniston <tdenniston@google.com>
2020-12-02 11:11:25 -0500

    Add failing test for quadratic Wang's formula
    Change-Id: I39e76dca5f77389833ba3c94930e6b67c2b1bc97

--- tests/WangsFormulaTest.cpp
+++ tests/WangsFormulaTest.cpp
@@ -295,3 +295,50 @@ DEF_TEST(WangsFormula_worst_case_cubic, r) {
         });
     }
 }
+
+// Ensure Wang's formula for quads produces max error within tolerance.
+DEF_TEST(WangsFormula_quad_within_tol, r) {
+    SkRandom rand;
+    for_random_beziers(3, &rand, [&r](const SkPoint pts[]) {
+        const int nsegs = static_cast<int>(
+                std::ceil(wangs_formula_quadratic_reference_impl(kIntolerance, pts)));
+
+        const float tdelta = 1.f / nsegs;
+        for (int j = 0; j < nsegs; ++j) {
+            const float tmin = j * tdelta, tmax = (j + 1) * tdelta;
+
+            // Get section of quad in [tmin,tmax]
+            const SkPoint* sectionPts;
+            SkPoint tmp0[5];
+            SkPoint tmp1[5];
+            if (tmin == 0) {
+                if (tmax == 1) {
+                    sectionPts = pts;
+                } else {
+                    SkChopQuadAt(pts, tmp0, tmax);
+                    sectionPts = tmp0;
+                }
+            } else {
+                SkChopQuadAt(pts, tmp0, tmin);
+                if (tmax == 1) {
+                    sectionPts = tmp0 + 2;
+                } else {
+                    SkChopQuadAt(tmp0 + 2, tmp1, (tmax - tmin) / (1 - tmin));
+                    sectionPts = tmp1;
+                }
+            }
+
+            // For quads, max distance from baseline is always at t=0.5.
+            SkPoint p;
+            p = SkEvalQuadAt(sectionPts, 0.5f);
+
+            // Get distance of p to baseline
+            const SkPoint n = {sectionPts[2].fY - sectionPts[0].fY,
+                               sectionPts[0].fX - sectionPts[2].fX};
+            const float d = std::abs((p - sectionPts[0]).dot(n)) / n.length();
+
+            // Check distance is within specified tolerance
+            REPORTER_ASSERT(r, d <= (1.f / kIntolerance), "%f", d);
+        }
+    });
+}
