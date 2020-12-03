f6412a0bc4bcdcd931aeedf09e50ad4ed856dd5d
Robert Phillips <robertphillips@google.com>
2020-12-03 12:47:41 -0500

    [M88 Cherrypick] Stop overflow of windows rects in GrClipStack
    Bug: skia:10989 b/174268976 1155178
    Change-Id: Id9af60782dd90ab65a51301a5d6368109f466b9f

--- include/gpu/mock/GrMockTypes.h
+++ include/gpu/mock/GrMockTypes.h
@@ -114,6 +114,7 @@ struct GrMockOptions {
     uint32_t fMapBufferFlags = 0;
     int fMaxTextureSize = 2048;
     int fMaxRenderTargetSize = 2048;
+    int fMaxWindowRectangles = 0;
     int fMaxVertexAttributes = 16;
     int fMaxTessellationSegments = 0;
     ConfigOptions fConfigOptions[kGrColorTypeCnt];
