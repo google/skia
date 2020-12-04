39b3fb3dc3b703514c83f55a49821615616675ab
Brian Salomon <bsalomon@google.com>
2020-12-04 13:10:12 -0500

    Take origin when creating/updating backend texture using pixmaps
    Change-Id: I511c7556f97f19d7f162031c245019d2da33c129

--- tools/gpu/BackendTextureImageFactory.cpp
+++ tools/gpu/BackendTextureImageFactory.cpp
@@ -19,27 +19,20 @@ sk_sp<SkImage> MakeBackendTextureImage(GrDirectContext* dContext,
                                        const SkPixmap& pixmap,
                                        GrRenderable renderable,
                                        GrSurfaceOrigin origin) {
-    const SkPixmap* src = &pixmap;
-    SkAutoPixmapStorage temp;
-    if (origin == kBottomLeft_GrSurfaceOrigin) {
-        temp.alloc(src->info());
-        auto s = static_cast<const char*>(src->addr(0, pixmap.height() - 1));
-        auto d = static_cast<char*>(temp.writable_addr(0, 0));
-        for (int y = 0; y < temp.height(); ++y, s -= pixmap.rowBytes(), d += temp.rowBytes()) {
-            std::copy_n(s, temp.info().minRowBytes(), d);
-        }
-        src = &temp;
-    }
-    auto mbet = ManagedBackendTexture::MakeWithData(dContext, src, 1, renderable, GrProtected::kNo);
+    auto mbet = ManagedBackendTexture::MakeWithData(dContext,
+                                                    pixmap,
+                                                    origin,
+                                                    renderable,
+                                                    GrProtected::kNo);
     if (!mbet) {
         return nullptr;
     }
     return SkImage::MakeFromTexture(dContext,
                                     mbet->texture(),
                                     origin,
-                                    src->colorType(),
-                                    src->alphaType(),
-                                    src->refColorSpace(),
+                                    pixmap.colorType(),
+                                    pixmap.alphaType(),
+                                    pixmap.refColorSpace(),
                                     ManagedBackendTexture::ReleaseProc,
                                     mbet->releaseContext());
 }
