cd0c27b48da19410fd9df383e50e476ef1a724c7
Michael Ludwig <michaelludwig@google.com>
2020-12-04 12:10:32 -0500

    Improve CanvasState cross-library tests
    Several things are accomplished as part of this:
    1. The canvas_state_lib build target is brought back so that we can
       actually test the canvas state sharing across library boundaries.
    2. The canvas state helper functions are updated to work with DLLs
       (confirmed tests pass on Windows in cross library mode)
    3. The tests now always run, and the cross-state define only changes
       what version of the helper functions are used
    4. Updated the dlopen code in the test to use the SkOSLibrary ports
       instead of calling dlopen/dlclose directly.
    5. Fix bugs in SkCanvasStateUtils that were uncovered as part of
       always running these tests.
    6. Switches the define away from SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
       to SK_TEST_CANVAS_STATE_CROSS_LIBRARY, since these tests are not
       strictly speaking testing the unclipped layer feature.
    
    Change-Id: Id4ae41e4bc3fb8227367deac899878ecf9d7b98e

--- src/ports/SkOSLibrary_win.cpp
+++ src/ports/SkOSLibrary_win.cpp
@@ -18,4 +18,8 @@ void* SkGetProcedureAddress(void* library, const char* functionName) {
     return reinterpret_cast<void*>(::GetProcAddress((HMODULE)library, functionName));
 }
 
+bool SkFreeDynamicLibrary(void* library) {
+    return FreeLibrary((HMODULE)library);
+}
+
 #endif//defined(SK_BUILD_FOR_WIN)
--- tests/CanvasStateHelpers.h
+++ tests/CanvasStateHelpers.h
@@ -10,10 +10,21 @@
 
 #include "include/core/SkTypes.h"
 
-#ifdef SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
+// See CanvasStateTest. These functions are either linked in to 'dm' directly (when this flag is
+// not defined), or built in a shared library that is dlopened by the test. In that case, they
+// should not be visible in 'dm', but the shared library will not have this flag set and compiles
+// them as expected.
+#if !defined(SK_TEST_CANVAS_STATE_CROSS_LIBRARY)
+
 class SkCanvas;
 class SkCanvasState;
 class SkRegion;
+
+#if defined(SK_BUILD_FOR_WIN)
+#define EXPORT _declspec(dllexport)
+#else
+#define EXPORT
+#endif
 
 /*
  *  Helper function to perform drawing to an SkCanvas. Used by both
@@ -28,7 +39,7 @@ void complex_layers_draw(SkCanvas* canvas, float left, float top,
  *  Used by test_complex_layers test in CanvasStateTest. Marked as extern
  *  so it can be called from a separate library.
  */
-extern "C" bool complex_layers_draw_from_canvas_state(SkCanvasState* state,
+extern "C" bool EXPORT complex_layers_draw_from_canvas_state(SkCanvasState* state,
         float left, float top, float right, float bottom, int32_t spacer);
 
 /*
@@ -44,9 +55,9 @@ void complex_clips_draw(SkCanvas* canvas, int32_t left, int32_t top,
  *  Used by test_complex_clips test in CanvasStateTest. Marked as extern
  *  so it can be called from a separate library.
  */
-extern "C" bool complex_clips_draw_from_canvas_state(SkCanvasState* state,
+extern "C" bool EXPORT complex_clips_draw_from_canvas_state(SkCanvasState* state,
         int32_t left, int32_t top, int32_t right, int32_t bottom, int32_t clipOp,
         int32_t regionRects, int32_t* rectCoords);
 
-#endif // SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
+#endif // SK_TEST_CANVAS_STATE_CROSS_LIBRARY
 #endif // CanvasStateHelpers_DEFINED
