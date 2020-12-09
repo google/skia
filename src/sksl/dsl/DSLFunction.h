a3b8ac76e506dd4f1d25697f6a4255f991d48447
Ethan Nicholas <ethannicholas@google.com>
2020-12-09 16:09:41 -0500

    Revert "Revert "Initial land of SkSL DSL.""
    This reverts commit dd213e9d46f9542aadb5658edb430c95f476d57e.
    
    Change-Id: I43be020dd1b07dc13862150a9d95493f8c48b3b1
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/342622
    Auto-Submit: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>

---
+++ src/sksl/dsl/DSLFunction.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_DSL_FUNCTION
+#define SKSL_DSL_FUNCTION
+
+#include "src/sksl/SkSLString.h"
+#include "src/sksl/dsl/DSLBlock.h"
+#include "src/sksl/dsl/DSLType.h"
+#include "src/sksl/dsl/priv/DSLWriter.h"
+#include "src/sksl/ir/SkSLBlock.h"
+#include "src/sksl/ir/SkSLFunctionDefinition.h"
+
+namespace SkSL {
+
+class Block;
+class Variable;
+
+namespace dsl {
+
+class DSLType;
+
+class DSLFunction {
+public:
+    template<class... Parameters>
+    DSLFunction(const DSLType& returnType, const char* name, Parameters&... parameters)
+        : fReturnType(returnType.skslType()) {
+        std::vector<const Variable*> parameterArray;
+        parameterArray.reserve(sizeof...(parameters));
+        (parameterArray.push_back(parameters.var()), ...);
+        SkSL::SymbolTable& symbols = *DSLWriter::SymbolTable();
+        fDecl = symbols.add(std::make_unique<SkSL::FunctionDeclaration>(
+                                                 /*offset=*/-1,
+                                                 DSLWriter::Modifiers(SkSL::Modifiers()),
+                                                 DSLWriter::Name(name),
+                                                 std::move(parameterArray), &fReturnType,
+                                                 /*builtin=*/false));
+    }
+
+    virtual ~DSLFunction() = default;
+
+    template<class... Stmt>
+    void define(Stmt... stmts) {
+        DSLBlock block = DSLBlock(DSLStatement(std::move(stmts))...);
+        this->define(std::move(block));
+    }
+
+    void define(DSLBlock block) {
+        DSLWriter::ProgramElements().emplace_back(new SkSL::FunctionDefinition(/*offset=*/-1,
+                                                                               fDecl,
+                                                                               /*builtin=*/false,
+                                                                               block.release()));
+    }
+
+protected:
+    const SkSL::Type& fReturnType;
+    const SkSL::FunctionDeclaration* fDecl;
+};
+
+} // namespace dsl
+
+} // namespace SkSL
+
+#endif
