e69a08b415a5028de8859152ce12fe2441f14de3
Tyler Denniston <tdenniston@google.com>
2020-12-09 14:16:25 -0500

    use pres attrs
    add fields
    
    
    add stop-color and stop-opacity pres attrs
    
    Change-Id: I0909507b0ecbd21732b3f80c46a343f5a0a9bf7a

--- modules/svg/src/SkSVGStop.cpp
+++ modules/svg/src/SkSVGStop.cpp
@@ -16,29 +16,11 @@ void SkSVGStop::setOffset(const SkSVGLength& offset) {
     fOffset = offset;
 }
 
-void SkSVGStop::setStopColor(const SkSVGStopColor& color) {
-    fStopColor = color;
-}
-
-void SkSVGStop::setStopOpacity(const SkSVGNumberType& opacity) {
-    fStopOpacity = SkTPin<SkScalar>(opacity, 0, 1);
-}
-
 void SkSVGStop::onSetAttribute(SkSVGAttribute attr, const SkSVGValue& v) {
     switch (attr) {
     case SkSVGAttribute::kOffset:
         if (const auto* offset = v.as<SkSVGLengthValue>()) {
             this->setOffset(*offset);
-        }
-        break;
-    case SkSVGAttribute::kStopColor:
-        if (const auto* color = v.as<SkSVGStopColorValue>()) {
-            this->setStopColor(*color);
-        }
-        break;
-    case SkSVGAttribute::kStopOpacity:
-        if (const auto* opacity = v.as<SkSVGNumberValue>()) {
-            this->setStopOpacity(*opacity);
         }
         break;
     default:
