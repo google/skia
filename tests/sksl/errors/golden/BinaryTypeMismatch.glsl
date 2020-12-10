2abd307437905ea7d41ec1f475aa714cace90597
Jorge Betancourt <jmbetancourt@google.com>
2020-12-10 14:55:58 +0000

    Reland "Revert "Initial land of SkSL DSL.""
    This reverts commit a3b8ac76e506dd4f1d25697f6a4255f991d48447.
    
    Reason for revert: Need to revert again, red tree.
    
    Original change's description:
    > Revert "Revert "Initial land of SkSL DSL.""
    >
    > This reverts commit dd213e9d46f9542aadb5658edb430c95f476d57e.
    >
    > Change-Id: I43be020dd1b07dc13862150a9d95493f8c48b3b1
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/342622
    > Auto-Submit: Ethan Nicholas <ethannicholas@google.com>
    > Reviewed-by: John Stiles <johnstiles@google.com>
    > Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    
    TBR=brianosman@google.com,ethannicholas@google.com,johnstiles@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: I8e967ef8ecb7f01dc578d38264e2600b04e9b62d

--- tests/sksl/errors/golden/BinaryTypeMismatch.glsl
+++ tests/sksl/errors/golden/BinaryTypeMismatch.glsl
@@ -1,11 +1,10 @@
 ### Compilation failed:
 
 error: 1: type mismatch: '*' cannot operate on 'int', 'bool'
-error: 2: expected 'bool', but found 'int'
-error: 2: expected 'bool', but found 'float'
+error: 2: type mismatch: '||' cannot operate on 'int', 'float'
 error: 3: type mismatch: '==' cannot operate on 'float2', 'int'
 error: 4: type mismatch: '!=' cannot operate on 'float2', 'int'
 error: 6: type mismatch: '<' cannot operate on 'float2', 'float2'
 error: 7: type mismatch: '<' cannot operate on 'float2', 'float'
 error: 8: type mismatch: '<' cannot operate on 'float', 'float2'
-8 errors
+7 errors
