5509c41a73f604b7b3ce60b45bf5680c7fd87633
John Stiles <johnstiles@google.com>
2020-12-10 18:03:10 -0500

    Add support for scalar `faceforward` intrinsic in Metal.
    Change-Id: I8eae896f5a859b59a1ba0b29dc95d5aa070c12ae

--- tests/sksl/intrinsics/golden/FaceForward.glsl
+++ tests/sksl/intrinsics/golden/FaceForward.glsl
@@ -3,6 +3,10 @@ out vec4 sk_FragColor;
 in float a;
 in float b;
 in float c;
+in vec4 d;
+in vec4 e;
+in vec4 f;
 void main() {
     sk_FragColor.x = faceforward(a, b, c);
+    sk_FragColor = faceforward(d, e, f);
 }
