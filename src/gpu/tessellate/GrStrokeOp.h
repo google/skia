55bb32d05cf01c904a84b8fd815cff62425a6249
Chris Dalton <csmartdalton@google.com>
2020-12-11 11:36:35 -0700

    Don't store tessellation tolerances on GrStrokeOp
    Instead we calculate these values on the fly as needed. This is
    necessary for hairlines because the tessellator will operate on them
    in post-transform space, which requires different tolerances than the
    setup code.
    
    Bug: skia:10419
    Change-Id: Ia8ffa8858b45949521c085ccbe5712b3842f785f

--- src/gpu/tessellate/GrStrokeOp.h
+++ src/gpu/tessellate/GrStrokeOp.h
@@ -67,14 +67,6 @@ protected:
     const GrAAType fAAType;
     const SkMatrix fViewMatrix;
     const SkStrokeRec fStroke;
-    // Controls the number of parametric segments the tessellator adds for each curve. The
-    // tessellator will add enough parametric segments so that the center of each one falls within
-    // 1/parametricIntolerance local path units from the true curve.
-    const float fParametricIntolerance;
-    // Controls the number of radial segments the tessellator adds for each curve. The tessellator
-    // will add this number of radial segments for each radian of rotation, in order to guarantee
-    // smoothness.
-    const float fNumRadialSegmentsPerRadian;
     SkPMColor4f fColor;
     bool fNeedsStencil = false;
     GrProcessorSet fProcessors;
