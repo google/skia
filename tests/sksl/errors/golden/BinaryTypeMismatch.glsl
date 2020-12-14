f02634d8fb8e0c3387b6440aba7640f6a2a4f3e5
Ethan Nicholas <ethannicholas@google.com>
2020-12-14 16:04:22 -0500

    Revert "Reland "Reland "Reland "Revert "Initial land of SkSL DSL."""""
    This reverts commit 346dd53ac087c37691edbba6923fe4ddaeaaa23a.
    
    Change-Id: I93bb18438cc6c2ad43d058d6c3f95bcc65d0cea9

--- tests/sksl/errors/golden/BinaryTypeMismatch.glsl
+++ tests/sksl/errors/golden/BinaryTypeMismatch.glsl
@@ -1,10 +1,11 @@
 ### Compilation failed:
 
 error: 1: type mismatch: '*' cannot operate on 'int', 'bool'
-error: 2: type mismatch: '||' cannot operate on 'int', 'float'
+error: 2: expected 'bool', but found 'int'
+error: 2: expected 'bool', but found 'float'
 error: 3: type mismatch: '==' cannot operate on 'float2', 'int'
 error: 4: type mismatch: '!=' cannot operate on 'float2', 'int'
 error: 6: type mismatch: '<' cannot operate on 'float2', 'float2'
 error: 7: type mismatch: '<' cannot operate on 'float2', 'float'
 error: 8: type mismatch: '<' cannot operate on 'float', 'float2'
-7 errors
+8 errors
