724b00080cc2deb10997096a88b1e12f12d0dc50
John Stiles <johnstiles@google.com>
2020-12-15 17:55:47 -0500

    Improve inlined code for functions with a single exit.
    DO NOT SUBMIT: variable declarations need to be migrated to the top.
    
    Change-Id: I6789064a321daf43db2e1cef4915f25ed74d6131

--- tests/sksl/blend/golden/BlendSrcOutStandaloneSettings.glsl
+++ tests/sksl/blend/golden/BlendSrcOutStandaloneSettings.glsl
@@ -3,9 +3,6 @@ out vec4 sk_FragColor;
 in vec4 src;
 in vec4 dst;
 void main() {
-    vec4 _0_blend_src_out;
-    _0_blend_src_out = (1.0 - dst.w) * src;
-
-    sk_FragColor = _0_blend_src_out;
+    sk_FragColor = (1.0 - dst.w) * src;
 
 }
