ceec0a51683c1a1feeda691bdef0134b54998190
Jim Van Verth <jvanverth@google.com>
2020-12-16 15:00:08 -0500

    Fixing directional light bounds
    Change-Id: Ia7d39c5187f976627d017ac4abecbe1d1dc62712

--- src/gpu/GrSurfaceDrawContext.cpp
+++ src/gpu/GrSurfaceDrawContext.cpp
@@ -1231,7 +1231,7 @@ bool GrSurfaceDrawContext::drawFastShadow(const GrClip* clip,
     SkPoint3 devLightPos = rec.fLightPos;
     bool directional = SkToBool(rec.fFlags & kDirectionalLight_ShadowFlag);
     if (directional) {
-        ((SkPoint*)&devLightPos.fX)->normalize();
+        devLightPos.normalize();
     } else {
         // transform light
         viewMatrix.mapPoints((SkPoint*)&devLightPos.fX, 1);
