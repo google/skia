7420b400396cd9a4daaacbe2136bdcbd29c0da9d
Mike Reed <reed@google.com>
2020-12-17 15:57:55 -0500

    Use explicit sampling in SkDraw
    ... on our way to adding the parameter to SkDevice
    
    Bug: skia:7650
    Change-Id: Iebde07e747741dfb47888426210bf2f231f295aa

--- src/core/SkImagePriv.h
+++ src/core/SkImagePriv.h
@@ -24,14 +24,15 @@ enum {kSkBlitterContextSize = 3332};
 // If alloc is non-nullptr, it will be used to allocate the returned SkShader, and MUST outlive
 // the SkShader.
 sk_sp<SkShader> SkMakeBitmapShader(const SkBitmap& src, SkTileMode, SkTileMode,
-                                   const SkMatrix* localMatrix, SkCopyPixelsMode);
+                                   const SkSamplingOptions&, const SkMatrix* localMatrix,
+                                   SkCopyPixelsMode);
 
 // Convenience function to return a shader that implements the shader+image behavior defined for
 // drawImage/Bitmap where the paint's shader is ignored when the bitmap is a color image, but
 // properly compose them together when it is an alpha image. This allows the returned paint to
 // be assigned to a paint clone without discarding the original behavior.
 sk_sp<SkShader> SkMakeBitmapShaderForPaint(const SkPaint& paint, const SkBitmap& src,
-                                           SkTileMode, SkTileMode,
+                                           SkTileMode, SkTileMode, const SkSamplingOptions&,
                                            const SkMatrix* localMatrix, SkCopyPixelsMode);
 
 /**
