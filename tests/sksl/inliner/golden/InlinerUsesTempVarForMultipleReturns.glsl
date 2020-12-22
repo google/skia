f2ce4e91a2a5c2f49ffd816f52202f47498a89d8
John Stiles <johnstiles@google.com>
2020-12-22 10:04:21 -0500

    Test that the inliner uses a temp var for return statements.
    We have a handful of tests that demonstrate this behavior indirectly,
    but lacked a focused test.
    
    Change-Id: I895cc4e3bebf30721ed649244e42bf170cc6ec06
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/346497
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

---
+++ tests/sksl/inliner/golden/InlinerUsesTempVarForMultipleReturns.glsl
@@ -0,0 +1,13 @@
+#version 400
+out vec4 sk_FragColor;
+uniform vec4 color;
+void main() {
+    vec4 _0_MakeTempVar;
+    if (color.x < color.y) {
+        _0_MakeTempVar = color.xxxx;
+    } else {
+        _0_MakeTempVar = color.yyyy;
+    }
+    sk_FragColor = _0_MakeTempVar;
+
+}
---
+++ tests/sksl/inliner/golden/InlinerUsesTempVarForReturnsInsideBlockWithVar.glsl
@@ -0,0 +1,12 @@
+#version 400
+out vec4 sk_FragColor;
+uniform vec4 color;
+void main() {
+    vec4 _0_MakeTempVar;
+    {
+        vec4 _1_d = color * 0.75;
+        _0_MakeTempVar = _1_d.xxxx;
+    }
+    sk_FragColor = _0_MakeTempVar;
+
+}
