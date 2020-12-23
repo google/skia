30b67293522ebb4499d789d7b9bcfd29eedbd19c
Brian Osman <brianosman@google.com>
2020-12-23 13:02:09 -0500

    Add/test integer versions of vector-relational intrinsics
    These are part of the GLSL ES 1.0 spec.
    
    Bug: skia:10913
    Change-Id: I61bfcadc92209fb3a6aa07b3d6ad579460ad2ecd
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/347049
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- src/sksl/SkSLVMGenerator.cpp
+++ src/sksl/SkSLVMGenerator.cpp
@@ -1032,13 +1032,21 @@ Value SkVMGenerator::writeIntrinsicCall(const FunctionCall& c) {
         }
 
         case Intrinsic::kLessThan:
-            return binary([](skvm::F32 x, skvm::F32 y) { return x < y; });
+            return nk == Type::NumberKind::kFloat
+                           ? binary([](skvm::F32 x, skvm::F32 y) { return x < y; })
+                           : binary([](skvm::I32 x, skvm::I32 y) { return x < y; });
         case Intrinsic::kLessThanEqual:
-            return binary([](skvm::F32 x, skvm::F32 y) { return x <= y; });
+            return nk == Type::NumberKind::kFloat
+                           ? binary([](skvm::F32 x, skvm::F32 y) { return x <= y; })
+                           : binary([](skvm::I32 x, skvm::I32 y) { return x <= y; });
         case Intrinsic::kGreaterThan:
-            return binary([](skvm::F32 x, skvm::F32 y) { return x > y; });
+            return nk == Type::NumberKind::kFloat
+                           ? binary([](skvm::F32 x, skvm::F32 y) { return x > y; })
+                           : binary([](skvm::I32 x, skvm::I32 y) { return x > y; });
         case Intrinsic::kGreaterThanEqual:
-            return binary([](skvm::F32 x, skvm::F32 y) { return x >= y; });
+            return nk == Type::NumberKind::kFloat
+                           ? binary([](skvm::F32 x, skvm::F32 y) { return x >= y; })
+                           : binary([](skvm::I32 x, skvm::I32 y) { return x >= y; });
 
         case Intrinsic::kEqual:
             return nk == Type::NumberKind::kFloat
