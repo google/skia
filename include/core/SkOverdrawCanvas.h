1fa691725c58b251998f9bf45663ed0e6050f0a8
Mike Reed <reed@google.com>
2020-12-24 09:15:26 -0500

    Add new virts, hide old ones
    Add virtuals for the draw methods that now take sampling/filtermode.
    
    Add a flag that can remove the older virtuals, once each client has
    stopped overriding them. In that situation, the older public methods
    will simplify extract the sampling from the paint, and call the new
    public methods.
    
    e.g.
    
    drawImage_sampling --> onDrawImage2
    
    old:
    drawImage_nosampling --> onDrawImage
    
    new:
    drawImage_nosampling --> drawImage_sampling
    
    
    TODO:
    Plumb through sampling in SkGpuDevice.cpp
    
    Change-Id: I8b0029727295caa983e8148fc743a55cfbecd043

--- include/core/SkOverdrawCanvas.h
+++ include/core/SkOverdrawCanvas.h
@@ -37,10 +37,19 @@ public:
     void onDrawAtlas(const SkImage*, const SkRSXform[], const SkRect[], const SkColor[],
                      int, SkBlendMode, const SkRect*, const SkPaint*) override;
     void onDrawPath(const SkPath&, const SkPaint&) override;
-    void onDrawImage(const SkImage*, SkScalar, SkScalar, const SkPaint*) override;
-    void onDrawImageRect(const SkImage*, const SkRect*, const SkRect&, const SkPaint*,
-                         SrcRectConstraint) override;
-    void onDrawImageLattice(const SkImage*, const Lattice&, const SkRect&, const SkPaint*) override;
+#ifdef SK_SUPPORT_LEGACY_ONDRAWIMAGERECT
+    void onDrawImage(const SkImage*, SkScalar left, SkScalar top, const SkPaint*) override;
+    void onDrawImageRect(const SkImage*, const SkRect* src, const SkRect& dst,
+                         const SkPaint*, SrcRectConstraint) override;
+    void onDrawImageLattice(const SkImage*, const Lattice& lattice, const SkRect& dst,
+                            const SkPaint*) override;
+#endif
+    void onDrawImage2(const SkImage*, SkScalar, SkScalar, const SkSamplingOptions&,
+                      const SkPaint*) override;
+    void onDrawImageRect2(const SkImage*, const SkRect&, const SkRect&, const SkSamplingOptions&,
+                          const SkPaint*, SrcRectConstraint) override;
+    void onDrawImageLattice2(const SkImage*, const Lattice&, const SkRect&, SkFilterMode,
+                             const SkPaint*) override;
     void onDrawDrawable(SkDrawable*, const SkMatrix*) override;
     void onDrawPicture(const SkPicture*, const SkMatrix*, const SkPaint*) override;
 
