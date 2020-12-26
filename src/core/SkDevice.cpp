3b7034369cecfea484262c31aa0f9b47d52d9675
Mike Reed <reed@google.com>
2020-12-25 21:37:30 -0500

    Add new virts, hide old ones
    Add virtuals for the draw methods that now take sampling/filtermode.
    
    drawImage
    drawImageRect
    drawImageLattice
    drawAtlas
    
    Add a flag that can remove the older virtuals, once each client has
    stopped overriding them. In that situation, the older public methods
    will simplify extract the sampling from the paint, and call the new
    public methods.
    
    e.g.
    
    drawImage_sampling --> onDrawImage2
    
    old:
    drawImage_nosampling --> onDrawImage
    
    new:
    drawImage_nosampling --> drawImage_sampling
    
    
    TODO:
    Plumb through sampling in SkGpuDevice.cpp
    
    Bug: skia:11105, skia:7650
    Change-Id: I8b0029727295caa983e8148fc743a55cfbecd043

--- src/core/SkDevice.cpp
+++ src/core/SkDevice.cpp
@@ -206,7 +206,7 @@ static SkPoint* quad_to_tris(SkPoint tris[6], const SkPoint quad[4]) {
 
 void SkBaseDevice::drawAtlas(const SkImage* atlas, const SkRSXform xform[],
                              const SkRect tex[], const SkColor colors[], int quadCount,
-                             SkBlendMode mode, const SkPaint& paint) {
+                             SkBlendMode mode, SkFilterMode filter, const SkPaint& paint) {
     const int triCount = quadCount << 1;
     const int vertexCount = triCount * 3;
     uint32_t flags = SkVertices::kHasTexCoords_BuilderFlag;
@@ -232,7 +232,7 @@ void SkBaseDevice::drawAtlas(const SkImage* atlas, const SkRSXform xform[],
         }
     }
     SkPaint p(paint);
-    p.setShader(atlas->makeShader(SkSamplingOptions()));
+    p.setShader(atlas->makeShader(SkSamplingOptions(filter, SkMipmapMode::kNone)));
     this->drawVertices(builder.detach().get(), mode, p);
 }
 
