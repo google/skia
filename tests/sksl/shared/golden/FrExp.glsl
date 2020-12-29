67a477b85b600286366166bd716c70c7195d8f06
John Stiles <johnstiles@google.com>
2020-12-29 12:27:50 -0500

    Remove stale FrExp test files.
    FrExp testing was moved to the intrinsic tests as part of
    http://review.skia.org/341977, and the shared versions were removed from
    sksl_tests.gni at that time.
    
    Change-Id: Ife7f3622034d97a77b60d5a98c01f71630c161d6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/348183
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- tests/sksl/shared/golden/FrExp.glsl
+++
@@ -1,9 +0,0 @@
-
-out vec4 sk_FragColor;
-void main() {
-    int exp1;
-    float a = frexp(0.5, exp1);
-    sk_FragColor = vec4(float(exp1));
-    ivec3 exp3;
-    sk_FragColor.xyz = frexp(vec3(3.5), exp3);
-}
