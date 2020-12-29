863c0e70c2355c728ef9882e35ba6d4c37bf915d
Mike Reed <reed@google.com>
2020-12-28 22:31:12 -0500

    add filter param to drawAtlas
    Change-Id: I8e72870775736c8bab7b077208b275372f632bc7

--- src/core/SkBitmapDevice.h
+++ src/core/SkBitmapDevice.h
@@ -97,7 +97,7 @@ protected:
     void drawGlyphRunList(const SkGlyphRunList& glyphRunList) override;
     void drawVertices(const SkVertices*, SkBlendMode, const SkPaint&) override;
     void drawAtlas(const SkImage*, const SkRSXform[], const SkRect[], const SkColor[], int count,
-                   SkBlendMode, const SkPaint&) override;
+                   SkBlendMode, const SkSamplingOptions&, const SkPaint&) override;
 
     ///////////////////////////////////////////////////////////////////////////
 
