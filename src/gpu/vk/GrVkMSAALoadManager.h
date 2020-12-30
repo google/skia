cf2c29cdd894991a59628501127aae235bf896f6
Greg Daniel <egdaniel@google.com>
2020-12-30 15:09:32 -0500

    Add support for manually loading vk resolve in msaa with extra subpass.
    This fills out the GrVkOpsRenderPass and connects all the wires to have
    things hooked up correctly. This CL won't actually enable things as the
    GrVkCap is still disabled and will be landed separately.
    
    Bug: skia:10979
    Change-Id: I249fea4cbab3ba636c794986de080ceaf2d4769b

---
+++ src/gpu/vk/GrVkMSAALoadManager.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrVkMSAALoadManager_DEFINED
+#define GrVkMSAALoadManager_DEFINED
+
+#include "include/gpu/GrTypes.h"
+#include "include/gpu/vk/GrVkTypes.h"
+#include "src/gpu/GrNativeRect.h"
+#include "src/gpu/GrPrimitiveProcessor.h"
+#include "src/gpu/vk/GrVkDescriptorSetManager.h"
+
+class GrSurface;
+class GrVkCommandBuffer;
+class GrVkGpu;
+class GrVkRenderPass;
+class GrVkUniformBuffer;
+class GrVkMeshBuffer;
+struct SkIRect;
+
+class GrVkMSAALoadManager {
+public:
+    GrVkMSAALoadManager();
+
+    ~GrVkMSAALoadManager();
+
+    bool loadMSAAFromResolve(GrVkGpu* gpu,
+                             GrVkCommandBuffer* commandBuffer,
+                             const GrVkRenderPass& renderPass,
+                             GrSurface* dst,
+                             GrSurface* src,
+                             const SkIRect& srcRect);
+
+    void destroyResources(GrVkGpu* gpu);
+
+private:
+    bool createMSAALoadProgram(GrVkGpu* gpu);
+
+    // Everything below is only created once and shared by all msaa load pipelines
+    VkShaderModule fVertShaderModule;
+    VkShaderModule fFragShaderModule;
+    VkPipelineShaderStageCreateInfo fShaderStageInfo[2];
+
+    // All pipelines used by this class use the same VkPipelineLayout. Therefore, unlike regular
+    // GrVkPipelines, we have the manager own the layout instead of the GrVkPipeline.
+    VkPipelineLayout fPipelineLayout;
+
+    std::unique_ptr<GrVkUniformBuffer> fUniformBuffer;
+};
+
+#endif
+
