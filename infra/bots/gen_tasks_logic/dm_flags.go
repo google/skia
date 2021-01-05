237911a4d8914ee4dfc8e3ee251000f1c4534836
Brian Salomon <bsalomon@google.com>
2021-01-05 12:33:26 -0500

    Add more comprehensive test for GPU write pixels.
    Similar to existing SurfaceContextReadPixels but for writes. Tries all
    combinations of src/dst color type and alpha type for write pixels.
    Always reads back pixels for verification using the ImageInfo of the
    tested surface context.
    
    Bug: skia:8862
    Bug: skia:11130
    
    Change-Id: Id01f6aa511f00c4be47c32746dca872368cd5d82
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/348886
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -968,6 +968,10 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 		match = append(match, "~gradients_view_perspective_nodither") //skia:6972
 	}
 
+	if b.gpu("PowerVRGX6450") && b.extraConfig("Metal") {
+		match = append(match, "~^SurfaceContextWritePixels$") //skia:11130
+	}
+
 	if b.arch("arm") && b.extraConfig("ASAN") {
 		// TODO: can we run with env allocator_may_return_null=1 instead?
 		match = append(match, "~BadImage")
