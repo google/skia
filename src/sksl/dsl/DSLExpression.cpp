5d52a1416e6ff7cc4c631bb3ceb209fd5a7bfcec
Ethan Nicholas <ethannicholas@google.com>
2021-01-06 13:52:04 -0500

    Beginnings of the SkSL DSL framework
    The code at this point doesn't do anything useful, but establishes some
    of the basic types and patterns.
    
    Change-Id: I580a9e75ffa3162879893450fb7d1f0905a10687

---
+++ src/sksl/dsl/DSLExpression.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/sksl/dsl/DSLExpression.h"
+
+#include "src/sksl/SkSLCompiler.h"
+#include "src/sksl/SkSLIRGenerator.h"
+#include "src/sksl/dsl/priv/DSLWriter.h"
+#include "src/sksl/ir/SkSLBinaryExpression.h"
+#include "src/sksl/ir/SkSLBoolLiteral.h"
+#include "src/sksl/ir/SkSLFloatLiteral.h"
+#include "src/sksl/ir/SkSLIntLiteral.h"
+
+namespace SkSL {
+
+namespace dsl {
+
+DSLExpression::DSLExpression() {}
+
+DSLExpression::DSLExpression(std::unique_ptr<SkSL::Expression> expression)
+    : fExpression(DSLWriter::Check(std::move(expression))) {}
+
+DSLExpression::DSLExpression(float value)
+    : fExpression(std::make_unique<SkSL::FloatLiteral>(DSLWriter::Context(),
+                                                       /*offset=*/-1,
+                                                       value)) {}
+
+DSLExpression::DSLExpression(int value)
+    : fExpression(std::make_unique<SkSL::IntLiteral>(DSLWriter::Context(),
+                                                     /*offset=*/-1,
+                                                     value)) {}
+
+DSLExpression::DSLExpression(bool value)
+    : fExpression(std::make_unique<SkSL::BoolLiteral>(DSLWriter::Context(),
+                                                     /*offset=*/-1,
+                                                     value)) {}
+
+DSLExpression::~DSLExpression() {
+    SkASSERTF(fExpression == nullptr,
+              "Expression destroyed without being incorporated into output tree");
+}
+
+std::unique_ptr<SkSL::Expression> DSLExpression::release() {
+    return std::move(fExpression);
+}
+
+} // namespace dsl
+
+} // namespace SkSL
