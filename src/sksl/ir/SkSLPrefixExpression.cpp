d333895ec6812f77491cb2ef57aed9f06fa84b95
John Stiles <johnstiles@google.com>
2021-01-07 12:17:53 -0500

    Remove compile-time constant support from PrefixExpression.
    This is not actually necessary now that constantPropagate can fully
    flatten out unary negation into its constant operands. The compilation
    results don't change at all.
    
    Change-Id: I7ab55bd3720413609d799dd866e1703973cb2626

--- src/sksl/ir/SkSLPrefixExpression.cpp
+++ src/sksl/ir/SkSLPrefixExpression.cpp
@@ -13,31 +13,41 @@ namespace SkSL {
 
 std::unique_ptr<Expression> PrefixExpression::constantPropagate(const IRGenerator& irGenerator,
                                                                 const DefinitionMap& definitions) {
-    if (this->isNegationOfCompileTimeConstant()) {
-        if (this->operand()->is<FloatLiteral>()) {
-            // Converts -floatLiteral(1) to floatLiteral(-1).
-            return std::make_unique<FloatLiteral>(irGenerator.fContext, fOffset,
-                                                  -this->operand()->as<FloatLiteral>().value());
-        }
-        if (this->operand()->is<IntLiteral>()) {
-            // Converts -intLiteral(1) to intLiteral(-1).
-            return std::make_unique<IntLiteral>(irGenerator.fContext, fOffset,
-                                                -this->operand()->as<IntLiteral>().value());
-        }
-        if (this->operand()->is<Constructor>()) {
-            // We've found a negated constant vector, e.g.:
-            //     -float4(float3(floatLiteral(1)), floatLiteral(2))
-            // To optimize this, the outer negation is removed and each argument is negated:
-            //     float4(-float3(floatLiteral(1)), -floatLiteral(2))
-            // After another optimization pass:
-            //     float4(float3(-floatLiteral(1)), floatLiteral(-2))
-            // Steady state is reached after another optimization pass:
-            //     float4(float3(floatLiteral(-1)), floatLiteral(-2))
-            std::unique_ptr<Expression> result = this->operand()->clone();
-            for (std::unique_ptr<Expression>& arg : result->as<Constructor>().arguments()) {
-                arg = std::make_unique<PrefixExpression>(Token::Kind::TK_MINUS, std::move(arg));
+    const Expression& operand = *this->operand();
+    if (operand.isCompileTimeConstant() && this->getOperator() == Token::Kind::TK_MINUS) {
+        // Constant-propagate negation onto compile-time constants.
+        switch (this->operand()->kind()) {
+            case Expression::Kind::kFloatLiteral:
+                // Convert -floatLiteral(1) to floatLiteral(-1).
+                return std::make_unique<FloatLiteral>(fOffset,
+                                                      -operand.as<FloatLiteral>().value(),
+                                                      &operand.type());
+
+            case Expression::Kind::kIntLiteral:
+                // Convert -intLiteral(1) to intLiteral(-1).
+                return std::make_unique<IntLiteral>(fOffset,
+                                                    -operand.as<IntLiteral>().value(),
+                                                    &operand.type());
+
+            case Expression::Kind::kConstructor: {
+                // We've found a negated constant vector, e.g.:
+                //     -float4(float3(floatLiteral(1)), floatLiteral(2))
+                // To optimize this, the outer negation is removed and each argument is
+                // negated:
+                //     float4(-float3(floatLiteral(1)), -floatLiteral(2))
+                // After another optimization pass:
+                //     float4(float3(-floatLiteral(1)), floatLiteral(-2))
+                // Steady state is reached after another optimization pass:
+                //     float4(float3(floatLiteral(-1)), floatLiteral(-2))
+                std::unique_ptr<Expression> result = operand.clone();
+                for (std::unique_ptr<Expression>& arg : result->as<Constructor>().arguments()) {
+                    arg = std::make_unique<PrefixExpression>(Token::Kind::TK_MINUS, std::move(arg));
+                }
+                return result;
             }
-            return result;
+
+            default:
+                break;
         }
     }
     return nullptr;
