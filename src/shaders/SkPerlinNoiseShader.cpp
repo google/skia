3c9a8103c2fbd6dd315b6d32d868bd9c909b5a11
Brian Salomon <bsalomon@google.com>
2021-01-06 19:30:00 -0500

    Revert "Revert "Fix GPU improved noise impl and add to perlinnoise GM.""
    This reverts commit 9730dc4041f53765f62f69ea3e57fe08c1760993.
    
    Bug: skia:10536
    Cq-Include-Trybots:Test-Ubuntu18-Clang-Golo-GPU-QuadroP400-x86_64-Release-All-Valgrind_PreAbandonGpuContext_SK_CPU_LIMIT_SSE41
    Change-Id: I6a61ec6e52d7c9e71516b1b5949d698f6576e653

--- src/shaders/SkPerlinNoiseShader.cpp
+++ src/shaders/SkPerlinNoiseShader.cpp
@@ -1222,7 +1222,7 @@ void GrGLImprovedPerlinNoise::emitCode(EmitArgs& args) {
     };
     SkString samplePerm = this->invokeChild(0, "half4(1)", args, "float2(x, 0.5)");
     SkString permFuncName = fragBuilder->getMangledFunctionName("perm");
-    SkString permCode = SkStringPrintf("return %s.r * 255;", samplePerm.c_str());
+    SkString permCode = SkStringPrintf("return %s.a * 255;", samplePerm.c_str());
     fragBuilder->emitFunction(kHalf_GrSLType, permFuncName.c_str(),
                               {permArgs, SK_ARRAY_COUNT(permArgs)}, permCode.c_str());
 
@@ -1372,6 +1372,9 @@ std::unique_ptr<GrFragmentProcessor> SkPerlinNoiseShaderImpl::asFragmentProcesso
         const SkBitmap& gradientBitmap = paintingData->getGradientBitmap();
         SkASSERT(SkIsPow2(gradientBitmap.width()) && SkIsPow2(gradientBitmap.height()));
         auto gradientView = GrMakeCachedBitmapProxyView(context, gradientBitmap);
+        if (!permutationsView || !gradientView) {
+            return nullptr;
+        }
         return GrImprovedPerlinNoiseEffect::Make(fNumOctaves,
                                                  fSeed,
                                                  std::move(paintingData),
