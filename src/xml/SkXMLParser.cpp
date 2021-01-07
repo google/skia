0491c88fbee67d34375e85a4a178341e805592b3
Florin Malita <fmalita@chromium.org>
2021-01-07 12:13:37 -0500

    Bump SkXMLParser buffer size
    Current release value is smallish (512) and debug is terribly
    inefficient (5).  Bump to 4K for both release/debug builds.
    
    (I think the small debug value was meant to artificially stress Expat
    streaming, but it seems unnecessary nowadays).
    
    Change-Id: I459afce5dde9789da84baaa68c4db9164911ffa1

--- src/xml/SkXMLParser.cpp
+++ src/xml/SkXMLParser.cpp
@@ -153,7 +153,7 @@ bool SkXMLParser::parse(SkStream& docStream)
     // Disable entity processing, to inhibit internal entity expansion. See expat CVE-2013-0340.
     XML_SetEntityDeclHandler(ctx.fXMLParser, entity_decl_handler);
 
-    static const int kBufferSize = 512 SkDEBUGCODE( - 507);
+    static constexpr int kBufferSize = 4096;
     bool done = false;
     do {
         void* buffer = XML_GetBuffer(ctx.fXMLParser, kBufferSize);
