bd058477e251d3c876defb58ca8f78bb0f7efc1d
John Stiles <johnstiles@google.com>
2021-01-07 13:47:53 -0500

    Constant-propagate the ! prefix onto constant boolean expressions.
    This will flatten out expressions such as `!false` or `!true`. We
    already had a similar fix-up at IR generation time which handled simple
    cases, but this will catch more complicated ones like `!sk_Caps.xxxxx`
    (since caps bits are only flattened out at constant propagation time).
    
    Change-Id: I04282809d9a784266a64dbcafd097f3b0662806c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/351497
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/sksl/ir/SkSLPrefixExpression.cpp
+++ src/sksl/ir/SkSLPrefixExpression.cpp
@@ -7,7 +7,10 @@
 
 #include "src/sksl/ir/SkSLPrefixExpression.h"
 
+#include "src/sksl/ir/SkSLBoolLiteral.h"
 #include "src/sksl/ir/SkSLConstructor.h"
+#include "src/sksl/ir/SkSLFloatLiteral.h"
+#include "src/sksl/ir/SkSLIntLiteral.h"
 
 namespace SkSL {
 
@@ -34,33 +37,41 @@ static std::unique_ptr<Expression> negate_operand(const Expression& operand) {
 
 std::unique_ptr<Expression> PrefixExpression::constantPropagate(const IRGenerator& irGenerator,
                                                                 const DefinitionMap& definitions) {
-    if (this->operand()->isCompileTimeConstant() && this->getOperator() == Token::Kind::TK_MINUS) {
-        // Constant-propagate negation onto compile-time constants.
-        switch (this->operand()->kind()) {
-            case Expression::Kind::kFloatLiteral:
-            case Expression::Kind::kIntLiteral:
-                return negate_operand(*this->operand());
+    if (this->operand()->isCompileTimeConstant()) {
+        if (this->getOperator() == Token::Kind::TK_MINUS) {
+            // Constant-propagate negation onto compile-time constants.
+            switch (this->operand()->kind()) {
+                case Expression::Kind::kFloatLiteral:
+                case Expression::Kind::kIntLiteral:
+                    return negate_operand(*this->operand());
 
-            case Expression::Kind::kConstructor: {
-                // We've found a negated constant vector, e.g.:
-                //     -float4(float3(floatLiteral(1)), floatLiteral(2))
-                // To optimize this, the outer negation is removed and each argument is negated:
-                //     float4(-float3(floatLiteral(1)), floatLiteral(-2))
-                // Steady state is reached after another optimization pass:
-                //     float4(float3(floatLiteral(-1)), floatLiteral(-2))
-                const Constructor& constructor = this->operand()->as<Constructor>();
-                ExpressionArray args;
-                args.reserve_back(constructor.arguments().size());
-                for (const std::unique_ptr<Expression>& arg : constructor.arguments()) {
-                    args.push_back(negate_operand(*arg));
+                case Expression::Kind::kConstructor: {
+                    // We've found a negated constant vector, e.g.:
+                    //     -float4(float3(floatLiteral(1)), floatLiteral(2))
+                    // To optimize this, the outer negation is removed and each argument is negated:
+                    //     float4(-float3(floatLiteral(1)), floatLiteral(-2))
+                    // Steady state is reached after another optimization pass:
+                    //     float4(float3(floatLiteral(-1)), floatLiteral(-2))
+                    const Constructor& constructor = this->operand()->as<Constructor>();
+                    ExpressionArray args;
+                    args.reserve_back(constructor.arguments().size());
+                    for (const std::unique_ptr<Expression>& arg : constructor.arguments()) {
+                        args.push_back(negate_operand(*arg));
+                    }
+                    return std::make_unique<Constructor>(constructor.fOffset,
+                                                         &constructor.type(),
+                                                         std::move(args));
                 }
-                return std::make_unique<Constructor>(constructor.fOffset,
-                                                     &constructor.type(),
-                                                     std::move(args));
-            }
 
-            default:
-                break;
+                default:
+                    break;
+            }
+        } else if (this->getOperator() == Token::Kind::TK_LOGICALNOT) {
+            if (this->operand()->is<BoolLiteral>()) {
+                // Convert !boolLiteral(true) to boolLiteral(false).
+                const BoolLiteral& b = this->operand()->as<BoolLiteral>();
+                return std::make_unique<BoolLiteral>(b.fOffset, !b.value(), &b.type());
+            }
         }
     }
     return nullptr;
