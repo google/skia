e89f8fd23db1719b8be7375439c7c38d029ccede
John Stiles <johnstiles@google.com>
2021-01-08 18:20:28 -0500

    Move constant folding to a separate file.
    This doesn't change any logic, just makes the IR generator a few
    hundred lines shorter.
    
    Change-Id: I92010191ee9283c33499c819d65fc85913f25824

---
+++ src/sksl/SkSLConstantFolder.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_CONSTANT_FOLDER
+#define SKSL_CONSTANT_FOLDER
+
+#include <memory>
+
+#include "src/sksl/SkSLLexer.h"
+
+namespace SkSL {
+
+class Context;
+class ErrorReporter;
+class Expression;
+
+/**
+ * Performs constant folding on IR expressions. This simplifies expressions containing
+ * compile-time constants, such as replacing `IntLiteral(2) + IntLiteral(2)` with `IntLiteral(4)`.
+ */
+class ConstantFolder {
+public:
+    /** Simplifies the binary expression `left OP right`. Returns null if it can't be simplified. */
+    static std::unique_ptr<Expression> Simplify(const Context& context,
+                                                ErrorReporter& errors,
+                                                const Expression& left,
+                                                Token::Kind op,
+                                                const Expression& right);
+};
+
+}  // namespace SkSL
+
+#endif  // SKSL_CONSTANT_FOLDER
