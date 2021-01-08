2a735ba1cb32d7688c73845e92cbc2173a3b98e4
Mike Klein <mtklein@google.com>
2021-01-08 10:25:38 -0600

    add colorspace2 GM
    I figured while I was thinking of this I'd flesh it out with different
    strategies for creating this same color space transform chain.  This
    new GM uses canvas->makeSurface() to create its midCS waypoint.
    
    Change-Id: Id72dbc6010496342daa12f793111bb3b96be94fd
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/351951
    Auto-Submit: Mike Klein <mtklein@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@google.com>

--- gm/colorspace.cpp
+++ gm/colorspace.cpp
@@ -8,6 +8,7 @@
 #include "gm/gm.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkFont.h"
+#include "include/core/SkSurface.h"
 #include "tools/Resources.h"
 
 static const skcms_TransferFunction gTFs[] = {
@@ -31,7 +32,7 @@ static const skcms_Matrix3x3 gGamuts[] = {
 static const int W = 128,
                  H = 128;
 
-// This GM demonstrates that our color space management is self-consistent.
+// These GMs demonstrate that our color space management is self-consistent.
 // (Important to note, self-consistent, not necessarily correct in an objective sense.)
 //
 // Let's let,
@@ -45,17 +46,24 @@ static const int W = 128,
 //
 // which would convert that img's pixels from imgCS to dstCS while drawing.
 //
-// But before we draw in this GM we convert the image to an arbitrarily different color space,
+// But before we draw in these GMs, we convert the image to an arbitrarily different color space,
 // letting midCS range over the cross-product gTFs × gGamuts:
 //
 //     canvas->drawImage(img->makeColorSpace(midCS), 0,0);
 //
 // This converts img first from imgCS to midCS, treating midCS as a destination color space,
 // and then draws that midCS image to the dstCS canvas, treating midCS as a source color space.
-//
 // This should draw a grid of images that look identical except for small precision loss.
+//
+// If instead of calling SkImage::makeColorSpace() we use SkCanvas::makeSurface() to create a
+// midCS offscreen, we construct the same logical imgCS -> midCS -> dstCS transform chain while
+// exercising different drawing code paths.  Both strategies should draw roughly the same.
 
-DEF_SIMPLE_GM(colorspace, canvas, W*SK_ARRAY_COUNT(gTFs), H*SK_ARRAY_COUNT(gGamuts)) {
+namespace {
+    enum Strategy { SkImage_makeColorSpace, SkCanvas_makeSurface };
+}
+
+static void draw_colorspace_gm(Strategy strategy, SkCanvas* canvas) {
     if (!canvas->imageInfo().colorSpace()) {
         canvas->drawString("This GM only makes sense with color-managed drawing.",
                            W,H, SkFont{}, SkPaint{});
@@ -77,10 +85,31 @@ DEF_SIMPLE_GM(colorspace, canvas, W*SK_ARRAY_COUNT(gTFs), H*SK_ARRAY_COUNT(gGamu
         canvas->save();
         for (skcms_TransferFunction tf : gTFs) {
             sk_sp<SkColorSpace> midCS = SkColorSpace::MakeRGB(tf, gamut);
-            canvas->drawImage(img->makeColorSpace(midCS), 0,0);
+
+            switch (strategy) {
+                case SkImage_makeColorSpace: {
+                    canvas->drawImage(img->makeColorSpace(midCS), 0,0);
+                } break;
+
+                case SkCanvas_makeSurface: {
+                    sk_sp<SkSurface> offscreen =
+                        canvas->makeSurface(canvas->imageInfo().makeColorSpace(midCS));
+                    offscreen->getCanvas()->drawImage(img, 0,0);
+                    canvas->drawImage(offscreen->makeImageSnapshot(), 0,0);
+                } break;
+            }
+
             canvas->translate(W, 0);
         }
         canvas->restore();
         canvas->translate(0, H);
     }
+}
+
+DEF_SIMPLE_GM(colorspace, canvas, W*SK_ARRAY_COUNT(gTFs), H*SK_ARRAY_COUNT(gGamuts)) {
+    draw_colorspace_gm(SkImage_makeColorSpace, canvas);
+}
+
+DEF_SIMPLE_GM(colorspace2, canvas, W*SK_ARRAY_COUNT(gTFs), H*SK_ARRAY_COUNT(gGamuts)) {
+    draw_colorspace_gm(SkCanvas_makeSurface, canvas);
 }
