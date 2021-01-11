d10762553312a2ff943c9275c05b800e5b0842b2
Weston Tracey <westont@google.com>
2021-01-11 15:24:07 +0000

    Revert "[infra] Use Mac10.15.7 and xcode12.3 for all builds."
    This reverts commit f5aed172c69300b3314f9558125fa3d303950893.
    
    Reason for revert: Broke 11phone11 (ios 13.6) testing.
    
    Original change's description:
    > [infra] Use Mac10.15.7 and xcode12.3 for all builds.
    >
    > Bug: skia:11129
    > Change-Id: I314d51988e4cafc9dfea60bc70a57f46faa8666a
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/345396
    > Reviewed-by: Eric Boren <borenet@google.com>
    
    TBR=borenet@google.com,westont@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: skia:11129
    Change-Id: Ib4484042ebaf493edcc8c5ee21960251bc80aa42

--- infra/bots/recipe_modules/build/default.py
+++ infra/bots/recipe_modules/build/default.py
@@ -94,13 +94,18 @@ def compile_fn(api, checkout_root, out_dir):
   args = {'werror': 'true'}
   env = {}
 
-  if os == 'Mac':
+  if os == 'Mac' or os == 'Mac10.15.5' or os == 'Mac10.15.7':
     # XCode build is listed in parentheses after the version at
     # https://developer.apple.com/news/releases/, or on Wikipedia here:
     # https://en.wikipedia.org/wiki/Xcode#Version_comparison_table
     # Use lowercase letters.
-    # https://chrome-infra-packages.appspot.com/p/infra_internal/ios/xcode
-    XCODE_BUILD_VERSION = '12c33'
+    XCODE_BUILD_VERSION = '11c29'
+    if os == 'Mac10.15.5':
+      XCODE_BUILD_VERSION = '11e503a'
+    if os == 'Mac10.15.7':
+      # https://chrome-infra-packages.appspot.com/p/infra_internal/ios/xcode
+      # '12b45b' is not available, so we use '12b5044c'.
+      XCODE_BUILD_VERSION = '12b5044c'
     extra_cflags.append(
         '-DDUMMY_xcode_build_version=%s' % XCODE_BUILD_VERSION)
     mac_toolchain_cmd = api.vars.workdir.join(
