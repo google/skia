1b1c73ad500b25aa16ec333a175796514a77978c
Ben Wagner <bungeman@google.com>
2021-01-11 13:36:11 -0500

    Remove deprecated SkBase64 interface.
    No longer used and troublesome to use, so remove.
    
    Change-Id: Idad9dd3325443f7e546465abf551de9784d9f829
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/352512
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>

--- include/utils/SkBase64.h
+++ include/utils/SkBase64.h
@@ -18,15 +18,6 @@ public:
         kBadCharError
     };
 
-    /** @deprecated */
-    SkBase64();
-    /** @deprecated, required user to `delete skbase64.getData()` */
-    Error decode(const char* src, size_t length);
-    /** @deprecated */
-    char* getData() { return fData; }
-    /** @deprecated */
-    size_t getDataSize() { return fLength; }
-
     /**
        Base64 encodes src into dst.
 
@@ -55,12 +46,6 @@ public:
     */
     static Error SK_WARN_UNUSED_RESULT Decode(const void* src, size_t  srcLength,
                                                     void* dst, size_t* dstLength);
-
-private:
-    /** @deprecated */
-    size_t fLength;
-    /** @deprecated */
-    char* fData;
 };
 
 #endif // SkBase64_DEFINED
