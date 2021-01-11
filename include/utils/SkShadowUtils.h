842fbf46b6bcadcfc031c520be56b4652cfeef0a
Jim Van Verth <jvanverth@google.com>
2021-01-11 11:01:09 -0500

    Add SkShadowUtils::GetLocalBounds.
    Generates a bounding rect that encompasses both shadows for a given path,
    relative to the path local space.
    
    Bug: skia:11146
    Change-Id: Ib8edb1072ed9cbfe36285523330be95fdf661d6b

--- include/utils/SkShadowUtils.h
+++ include/utils/SkShadowUtils.h
@@ -14,6 +14,7 @@
 #include "include/private/SkShadowFlags.h"
 
 class SkCanvas;
+class SkMatrix;
 class SkPath;
 class SkResourceCache;
 
@@ -44,6 +45,30 @@ public:
                            const SkPoint3& lightPos, SkScalar lightRadius,
                            SkColor ambientColor, SkColor spotColor,
                            uint32_t flags = SkShadowFlags::kNone_ShadowFlag);
+
+    /**
+     * Generate bounding box for shadows relative to path. Includes both the ambient and spot
+     * shadow bounds.
+     *
+     * @param ctm  Current transformation matrix to device space.
+     * @param path  The occluder used to generate the shadows.
+     * @param zPlaneParams  Values for the plane function which returns the Z offset of the
+     *  occluder from the canvas based on local x and y values (the current matrix is not applied).
+     * @param lightPos  Generally, the 3D position of the light relative to the canvas plane.
+     *                  If kDirectionalLight_ShadowFlag is set, this specifies a vector pointing
+     *                  towards the light.
+     * @param lightRadius  Generally, the radius of the disc light.
+     *                     If DirectionalLight_ShadowFlag is set, this specifies the amount of
+     *                     blur when the occluder is at Z offset == 1. The blur will grow linearly
+     *                     as the Z value increases.
+     * @param flags  Options controlling opaque occluder optimizations, shadow appearance,
+     *               and light position. See SkShadowFlags.
+     * @param bounds Return value for shadow bounding box.
+     * @return Returns true if successful, false otherwise.
+     */
+    static bool GetLocalBounds(const SkMatrix& ctm, const SkPath& path,
+                               const SkPoint3& zPlaneParams, const SkPoint3& lightPos,
+                               SkScalar lightRadius, uint32_t flags, SkRect* bounds);
 
     /**
      * Helper routine to compute color values for one-pass tonal alpha.
