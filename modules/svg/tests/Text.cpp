ee7253482cc571b80cad4f09674cf9110d6e3efb
Florin Malita <fmalita@chromium.org>
2021-01-12 12:04:20 -0500

    [svg TextPath support
      - introduce SkSVGTextPath and update the text render logic to
        instantiate new SkSVGTextContexts in onRenderText() overrides
        instead of renderText() root -- this is to observe spec semantics [1]
        requiring <text> and <textPath> to always start a new chunk,
        regardless of their relative nesting.
      - expand SkSVGTextContext to also store PathData when used in the scope
        of a textPath
      - PathData caches SkContourMeasures, for path position lookup
    
    
    [1] https://www.w3.org/TR/SVG11/text.html#TextLayout
    
    Change-Id: I462eada7c086646afdc1bc84f08ec2368613f1c0

--- modules/svg/tests/Text.cpp
+++ modules/svg/tests/Text.cpp
@@ -7,6 +7,7 @@
 
 #include <vector>
 
+#include "include/utils/SkNoDrawCanvas.h"
 #include "modules/svg/src/SkSVGTextPriv.h"
 #include "tests/Test.h"
 
@@ -153,8 +154,13 @@ DEF_TEST(Svg_Text_PosProvider, r) {
         b->setX(tst.xb);
         b->setY(tst.yb);
 
-        SkSVGTextContext tctx(SkSVGPresentationContext(), nullptr);
-        SkSVGLengthContext lctx({0,0});
+        const SkSVGIDMapper mapper;
+        const SkSVGLengthContext lctx({0,0});
+        const SkSVGPresentationContext pctx;
+        SkNoDrawCanvas canvas(0, 0);
+        const SkSVGRenderContext ctx(&canvas, nullptr, mapper, lctx, pctx, nullptr);
+
+        SkSVGTextContext tctx(ctx);
         SkSVGTextContext::ScopedPosResolver pa(*a, lctx, &tctx, tst.offseta);
         SkSVGTextContext::ScopedPosResolver pb(*b, lctx, &tctx, tst.offsetb);
 
