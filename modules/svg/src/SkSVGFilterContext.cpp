12b9a9a8b8afab04d92157432046f931b46f85c1
Tyler Denniston <tdenniston@google.com>
2021-01-13 15:57:11 -0500

    [svg] Perform colorspace conversions for filter effects
    A filter effect can optionally be specified to operate in either sRGB
    or linearRGB, according to the SVG spec:
    
    https://www.w3.org/TR/SVG11/painting.html#ColorInterpolationProperties
    
    This CL adds any necessary conversion steps (SkColorFilters) while
    constructing the filter DAG. The default filter effect color space is
    linearRGB. We should now be passing the filters-gauss-* W3C tests.
    
    Specific changes:
    - Tag filter effect results with their colorspace when storing them in
      the filter context map
    - Add an SkColorFolor conversion step as necessary when resolving filter
      effect inputs
    
    Bug: skia:10841
    Change-Id: Ide12698ea64c4d40f09df93a60718788809086fa

--- modules/svg/src/SkSVGFilterContext.cpp
+++ modules/svg/src/SkSVGFilterContext.cpp
@@ -5,15 +5,33 @@
  * found in the LICENSE file.
  */
 
+#include "include/core/SkColorFilter.h"
 #include "include/effects/SkImageFilters.h"
 #include "modules/svg/include/SkSVGFilterContext.h"
 #include "modules/svg/include/SkSVGNode.h"
 #include "modules/svg/include/SkSVGRenderContext.h"
 #include "modules/svg/include/SkSVGTypes.h"
 
-sk_sp<SkImageFilter> SkSVGFilterContext::findResultById(const SkSVGStringType& id) const {
-    const Result* res = fResults.find(id);
-    return res ? res->fImageFilter : nullptr;
+namespace {
+
+sk_sp<SkImageFilter> ConvertFilterColorspace(sk_sp<SkImageFilter>&& input,
+                                             SkSVGColorspace src,
+                                             SkSVGColorspace dst) {
+    if (src == dst) {
+        return std::move(input);
+    } else if (src == SkSVGColorspace::kSRGB && dst == SkSVGColorspace::kLinearRGB) {
+        return SkImageFilters::ColorFilter(SkColorFilters::SRGBToLinearGamma(), input);
+    } else {
+        SkASSERT(src == SkSVGColorspace::kLinearRGB && dst == SkSVGColorspace::kSRGB);
+        return SkImageFilters::ColorFilter(SkColorFilters::LinearToSRGBGamma(), input);
+    }
+}
+
+}  // namespace
+
+const SkSVGFilterContext::Result* SkSVGFilterContext::findResultById(
+        const SkSVGStringType& id) const {
+    return fResults.find(id);
 }
 
 const SkRect& SkSVGFilterContext::filterPrimitiveSubregion(const SkSVGFeInputType& input) const {
@@ -25,18 +43,24 @@ const SkRect& SkSVGFilterContext::filterPrimitiveSubregion(const SkSVGFeInputTyp
 
 void SkSVGFilterContext::registerResult(const SkSVGStringType& id,
                                         const sk_sp<SkImageFilter>& result,
-                                        const SkRect& subregion) {
+                                        const SkRect& subregion,
+                                        SkSVGColorspace resultColorspace) {
     SkASSERT(!id.isEmpty());
-    fResults[id] = {result, subregion};
+    fResults[id] = {result, subregion, resultColorspace};
 }
 
 sk_sp<SkImageFilter> SkSVGFilterContext::resolveInput(const SkSVGRenderContext& ctx,
-                                                      const SkSVGFeInputType& inputType) const {
+                                                      const SkSVGFeInputType& inputType,
+                                                      SkSVGColorspace colorspace) const {
+    SkSVGColorspace inputCS = SkSVGColorspace::kSRGB;
+    sk_sp<SkImageFilter> result;
     switch (inputType.type()) {
         case SkSVGFeInputType::Type::kSourceGraphic:
-            return nullptr;
+            // Do nothing.
+            break;
         case SkSVGFeInputType::Type::kFillPaint:
-            return SkImageFilters::Paint(*ctx.fillPaint());
+            result = SkImageFilters::Paint(*ctx.fillPaint());
+            break;
         case SkSVGFeInputType::Type::kStrokePaint: {
             // The paint filter doesn't handle stroke paints properly, so convert to fill for
             // simplicity.
@@ -44,12 +68,21 @@ sk_sp<SkImageFilter> SkSVGFilterContext::resolveInput(const SkSVGRenderContext& 
             //       requires some extra work to handle all paint features (gradients, etc).
             SkPaint p = *ctx.strokePaint();
             p.setStyle(SkPaint::kFill_Style);
-            return SkImageFilters::Paint(p);
+            result = SkImageFilters::Paint(p);
+            break;
         }
-        case SkSVGFeInputType::Type::kFilterPrimitiveReference:
-            return findResultById(inputType.id());
+        case SkSVGFeInputType::Type::kFilterPrimitiveReference: {
+            const Result* res = findResultById(inputType.id());
+            if (res) {
+                result = res->fImageFilter;
+                inputCS = res->fColorspace;
+            }
+            break;
+        }
         default:
             SkDebugf("unhandled filter input type %d\n", inputType.type());
             return nullptr;
     }
+
+    return ConvertFilterColorspace(std::move(result), inputCS, colorspace);
 }
