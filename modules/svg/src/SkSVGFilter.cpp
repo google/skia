7d1b9fb8fe9b22e3cafbab00b3cb47eab147123f
Tyler Denniston <tdenniston@google.com>
2021-01-13 13:11:19 -0500

    [svg] Perform colorspace conversions for filter effects
    A filter effect can optionally be specified to operate in either sRGB
    or linearRGB, according to the SVG spec:
    
    https://www.w3.org/TR/SVG11/painting.html#ColorInterpolationProperties
    
    This CL adds any necessary conversion steps (SkColorFilters) while
    constructing the filter DAG. The default filter effect color space is
    linearRGB. We should now be passing the filters-gauss-* W3C tests.
    
    Specific changes:
    - Tag filter effect results with their colorspace when storing them in
      the filter context map
    - Add an SkColorFolor conversion step as necessary when resolving filter
      effect inputs
    
    Bug: skia:10841
    Change-Id: Ide12698ea64c4d40f09df93a60718788809086fa

--- modules/svg/src/SkSVGFilter.cpp
+++ modules/svg/src/SkSVGFilter.cpp
@@ -5,6 +5,7 @@
  * found in the LICENSE file.
  */
 
+#include "include/core/SkColorFilter.h"
 #include "include/effects/SkImageFilters.h"
 #include "modules/svg/include/SkSVGFe.h"
 #include "modules/svg/include/SkSVGFilter.h"
@@ -46,6 +47,7 @@ SkRect SkSVGFilter::resolveFilterRegion(const SkSVGRenderContext& ctx) const {
 sk_sp<SkImageFilter> SkSVGFilter::buildFilterDAG(const SkSVGRenderContext& ctx) const {
     sk_sp<SkImageFilter> filter;
     SkSVGFilterContext fctx(resolveFilterRegion(ctx), fPrimitiveUnits);
+    SkSVGColorspace cs = SkSVGColorspace::kSRGB;
     for (const auto& child : fChildren) {
         if (!SkSVGFe::IsFilterEffect(child)) {
             continue;
@@ -62,13 +64,18 @@ sk_sp<SkImageFilter> SkSVGFilter::buildFilterDAG(const SkSVGRenderContext& ctx) 
         feNode.applyProperties(&localCtx);
 
         // TODO: there are specific composition rules that need to be followed
-        // TODO: perform colorspace conversions depending on 'color-interpolation-filters' setting
-        // of the current node and its inputs.
+        cs = feNode.resolveColorspace(ctx);
         filter = feNode.makeImageFilter(localCtx, fctx);
 
         if (!feResultType.isEmpty()) {
-            fctx.registerResult(feResultType, filter, feNode.resolveFilterSubregion(localCtx, fctx));
+            fctx.registerResult(
+                    feResultType, filter, feNode.resolveFilterSubregion(localCtx, fctx), cs);
         }
+    }
+
+    // Convert to final destination colorspace
+    if (cs != SkSVGColorspace::kSRGB) {
+        filter = SkImageFilters::ColorFilter(SkColorFilters::LinearToSRGBGamma(), filter);
     }
 
     return filter;
