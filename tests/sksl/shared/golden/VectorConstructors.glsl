fb08243368740d88facbbcf37baf615640007dac
John Stiles <johnstiles@google.com>
2021-01-13 13:09:13 -0500

    Update VectorConstructor unit test with mixed-type ctors.
    GLSL allows mixed types inside a vector constructor, but SkSL currently
    doesn't handle it well; some cases don't compile, and others generate
    bad code. This will be fixed in a followup CL.
    
    Change-Id: Ia98b498f320b8fa91595404730f6cdc836615140
    Bug: skia:11164

--- tests/sksl/shared/golden/VectorConstructors.glsl
+++ tests/sksl/shared/golden/VectorConstructors.glsl
@@ -1,12 +1,11 @@
+### Compilation failed:
 
-out vec4 sk_FragColor;
-vec2 v1 = vec2(1.0);
-vec2 v2 = vec2(1.0, 2.0);
-vec2 v3 = vec2(1.0);
-vec3 v4 = vec3(vec2(1.0), 1.0);
-ivec2 v5 = ivec2(1);
-ivec2 v6 = ivec2(vec2(1.0, 2.0));
-vec2 v7 = vec2(ivec2(1, 2));
-void main() {
-    sk_FragColor.x = (((((v1.x + v2.x) + v3.x) + v4.x) + float(v5.x)) + float(v6.x)) + v7.x;
-}
+error: 10: expected 'int', but found 'float'
+error: 12: expected 'float', but found 'bool'
+error: 13: expected 'float', but found 'bool'
+error: 14: 'bool2' is not a valid parameter to 'float2' constructor
+error: 15: expected 'bool', but found 'float'
+error: 16: 'float2' is not a valid parameter to 'bool2' constructor
+error: 17: expected 'bool', but found 'float'
+error: 21: unknown identifier 'v10'
+8 errors
