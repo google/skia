033b898d4478c6dcb49e40b6e39ff146b0369d72
John Stiles <johnstiles@google.com>
2021-01-13 17:13:23 -0500

    Enforce stricter type coercion rules in SkSL.
    Literals are still flexible; we still allow `1` to coerce to float.
    However, we no longer accept code like `int x = sqrt(2);` or
    `int x = 0; float y = x;` without an explicit cast.
    
    Change-Id: Ieb294a4877447e2336252f876e8bc489d1e4a59a
    Bug: skia:11164

--- tests/sksl/errors/golden/IntFloatMismatch.glsl
+++ tests/sksl/errors/golden/IntFloatMismatch.glsl
@@ -2,4 +2,17 @@
 
 error: 12: type mismatch: '=' cannot operate on 'int', 'float'
 error: 13: type mismatch: '=' cannot operate on 'int', 'float'
-2 errors
+error: 14: type mismatch: '=' cannot operate on 'float', 'int'
+error: 15: type mismatch: '+' cannot operate on 'int', 'float'
+error: 16: type mismatch: '-' cannot operate on 'int', 'float'
+error: 17: type mismatch: '*' cannot operate on 'int', 'float'
+error: 18: type mismatch: '/' cannot operate on 'int', 'float'
+error: 19: type mismatch: '+' cannot operate on 'int', 'float'
+error: 20: type mismatch: '-' cannot operate on 'int', 'float'
+error: 21: type mismatch: '*' cannot operate on 'int', 'float'
+error: 22: type mismatch: '/' cannot operate on 'int', 'float'
+error: 23: type mismatch: '+' cannot operate on 'float', 'int'
+error: 24: type mismatch: '-' cannot operate on 'float', 'int'
+error: 25: type mismatch: '*' cannot operate on 'float', 'int'
+error: 26: type mismatch: '/' cannot operate on 'float', 'int'
+15 errors
