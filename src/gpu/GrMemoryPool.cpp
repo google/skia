b1d7149f2dd3f867bc46ec108beb259d9d93d256
John Stiles <johnstiles@google.com>
2021-01-12 19:39:07 -0500

    Reland "Scrub memory released from a GrMemoryPool in debug mode."
    This is a reland of c0315a72d9492f56b4353e269170baaa13d201dc
    
    Original change's description:
    > Scrub memory released from a GrMemoryPool in debug mode.
    >
    > In debug mode, we now overwrite released memory with 0xDD. This is
    > intended to make use-after-free errors easier to catch while debugging.
    >
    > Change-Id: I04a4c5abcfef5f3f604a2430da15a8b5125239af
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/352956
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Commit-Queue: John Stiles <johnstiles@google.com>
    
    Change-Id: Id79cadb196868b467fb6d9107eb313bdc147b4f0

--- src/gpu/GrMemoryPool.cpp
+++ src/gpu/GrMemoryPool.cpp
@@ -95,8 +95,6 @@ void* GrMemoryPool::allocate(size_t size) {
 }
 
 void GrMemoryPool::release(void* p) {
-    // NOTE: if we needed it, (p - block) would equal the original alignedOffset value returned by
-    // GrBlockAllocator::allocate()
     Header* header = reinterpret_cast<Header*>(reinterpret_cast<intptr_t>(p) - sizeof(Header));
 
 #if defined(SK_SANITIZE_ADDRESS)
@@ -112,6 +110,16 @@ void GrMemoryPool::release(void* p) {
 #endif
 
     GrBlockAllocator::Block* block = fAllocator.owningBlock<kAlignment>(header, header->fStart);
+
+#if defined(SK_DEBUG)
+    // (p - block) matches the original alignedOffset value from GrBlockAllocator::allocate().
+    intptr_t alignedOffset = (intptr_t)p - (intptr_t)block;
+    SkASSERT(p == block->ptr(alignedOffset));
+
+    // Scrub the block contents to prevent use-after-free errors.
+    memset(p, 0xDD, header->fEnd - alignedOffset);
+#endif
+
     int alive = block->metadata();
     if (alive == 1) {
         // This was last allocation in the block, so remove it
