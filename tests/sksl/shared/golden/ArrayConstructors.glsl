3fc836346163fc41d20d4907523fbe3ecf29a7d9
Brian Osman <brianosman@google.com>
2021-01-14 14:18:10 -0500

    Enforce (valid) array sizes in many more places
    Bug: skia:11013
    Bug: skia:11127
    Change-Id: Ib08dfe9aeec96bf605661665d6f166419d27e8bc

--- tests/sksl/shared/golden/ArrayConstructors.glsl
+++ tests/sksl/shared/golden/ArrayConstructors.glsl
@@ -1,8 +1,8 @@
 
 out vec4 sk_FragColor;
-float test1[] = float[](1.0, 2.0, 3.0, 4.0);
-vec2 test2[] = vec2[](vec2(1.0, 2.0), vec2(3.0, 4.0));
-mat4 test3[] = mat4[]();
+float test1[4] = float[4](1.0, 2.0, 3.0, 4.0);
+vec2 test2[2] = vec2[2](vec2(1.0, 2.0), vec2(3.0, 4.0));
+mat4 test3[1] = mat4[1]();
 void main() {
     sk_FragColor.x = (test1[0] + test2[0].x) + test3[0][0].x;
 }
