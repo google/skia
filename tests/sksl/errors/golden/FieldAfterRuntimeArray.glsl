3b18f5eda53562e38cda91e5a241e9da0f7221e7
Brian Osman <brianosman@google.com>
2021-01-14 14:18:10 -0500

    Enforce (valid) array sizes in many more places
    Unsized arrays are now allowed in exactly one place: On the declaration
    of an interface block. This satisfies the one existing use-case, which
    is the gl_in (sk_in) declaration for geometry shaders. There is no other
    useful scenario, and most of our backends don't support them anyway.
    
    Several spots were using less strict checks when attaching sizes to
    arrays, allowing for zero or negative-sized arrays, so those are all
    fixed now.
    
    The existing tests that initialize arrays are still a problem, because
    Metal doesn't support that (neither does GLES2). Also, ArrayConstructors
    has gone from generating an error in the Vulkan backend, to invalid
    SPIR-V.
    
    Bug: skia:11013
    Bug: skia:11127
    Change-Id: Ib08dfe9aeec96bf605661665d6f166419d27e8bc

--- tests/sksl/errors/golden/FieldAfterRuntimeArray.glsl
+++ tests/sksl/errors/golden/FieldAfterRuntimeArray.glsl
@@ -1,6 +1,5 @@
 ### Compilation failed:
 
-error: 1: arrays without an explicit size must use an initializer expression
-error: 2: initializers are not permitted on interface block fields
-error: 2: only the last entry in an interface block may be a runtime-sized array
-3 errors
+error: 1: array must have a size
+error: 2: array must have a size
+2 errors
