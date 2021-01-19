8ec4971ce855b4155d02cdb94624c0cbaddb2ffc
John Stiles <johnstiles@google.com>
2021-01-19 18:50:40 -0500

    Migrate some SkSL folding tests to a GM slide.
    Change-Id: I2ccb427a907975ae84aee19d8e68d774b2cb638c

--- tests/sksl/folding/golden/BoolFolding.glsl
+++ tests/sksl/folding/golden/BoolFolding.glsl
@@ -1,10 +1,9 @@
 
-out vec4 sk_FragColor;
-void main() {
-    sk_FragColor.x = 1.0;
-    sk_FragColor.x = -2.0;
-    sk_FragColor.x = 3.0;
-    sk_FragColor.x = -4.0;
-    sk_FragColor.x = 5.0;
-    sk_FragColor.x = -6.0;
+vec4 main() {
+    bool _1_g = true == true;
+    bool _2_h = false == true;
+    bool _3_i = true == true;
+    bool _4_j = false == true;
+    return ((_1_g && !_2_h) && _3_i) && !_4_j ? vec4(0.0, 1.0, 0.0, 1.0) : vec4(1.0, 0.0, 0.0, 1.0);
+
 }
