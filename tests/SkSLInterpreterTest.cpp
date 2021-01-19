5ad0025fe3633d734b544d798f11f276ddf2cd08
Mike Klein <mtklein@google.com>
2021-01-19 21:27:41 +0000

    Revert "Support indexing by loop variables in SkVMGenerator"
    This reverts commit ebf569004fef4dfb7fcc66b8b8c721fb774d6b78.
    
    Reason for revert: std::clamp is c++17
    
    Original change's description:
    > Support indexing by loop variables in SkVMGenerator
    >
    > Bug: skia:11096
    > Change-Id: I25a91bacf1c3455ac67422fb0e59b9b152c2054a
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/354667
    > Commit-Queue: Brian Osman <brianosman@google.com>
    > Reviewed-by: Mike Klein <mtklein@google.com>
    
    TBR=mtklein@google.com,brianosman@google.com,johnstiles@google.com
    
    Change-Id: I0590cf7fe626fb59be3381b5e8eb66a9a2a9e8cb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:11096

--- tests/SkSLInterpreterTest.cpp
+++ tests/SkSLInterpreterTest.cpp
@@ -669,7 +669,8 @@ DEF_TEST(SkSLInterpreterCompound, r) {
         REPORTER_ASSERT(r, out == 8);
     }
 
-    {
+    // TODO: Doesn't work until SkVM generator supports indexing-by-loop variable
+    if (false) {
         float in[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
         float out = 0;
         skvm::Program p = build(sums);
@@ -689,7 +690,8 @@ DEF_TEST(SkSLInterpreterCompound, r) {
         REPORTER_ASSERT(r, out == gRects[2]);
     }
 
-    {
+    // TODO: Doesn't work until SkVM generator supports indexing-by-loop variable
+    if (false) {
         ManyRects in;
         memset(&in, 0, sizeof(in));
         in.fNumRects = 2;
