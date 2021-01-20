286e37b86f7fd140fccc0e1a20e171e8a6057e51
Dominik Röttsches <drott@chromium.org>
2021-01-20 18:08:00 +0200

    WIP: color v1 paint
    Change-Id: I8357cd0c7ac0039bb2eac18f21fa343bf61871eb

--- src/ports/SkFontHost_FreeType_common.h
+++ src/ports/SkFontHost_FreeType_common.h
@@ -46,7 +46,9 @@ protected:
         : INHERITED(std::move(typeface), effects, desc)
     {}
 
-    void generateGlyphImage(FT_Face face, const SkGlyph& glyph, const SkMatrix& bitmapTransform);
+    void generateGlyphImage(FT_Face face,
+                            const SkGlyph& glyph,
+                            const SkMatrix& bitmapTransform);
     bool generateGlyphPath(FT_Face face, SkPath* path);
     bool generateFacePath(FT_Face face, SkGlyphID glyphID, SkPath* path);
 private:
