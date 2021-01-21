e72049ec5023baaaf6ca3ea864b4e8e363d6490a
Tyler Denniston <tdenniston@google.com>
2021-01-21 14:44:51 -0500

    [svg] Implement feDisplacementMap
    https://www.w3.org/TR/SVG11/filters.html#feDisplacementMapElement
    
    Bug: skia:10841
    Change-Id: Icf1a560b2d83e7954207fb1740e77193362b9fd4

--- modules/svg/include/SkSVGFe.h
+++ modules/svg/include/SkSVGFe.h
@@ -21,7 +21,8 @@ public:
         return node->tag() == SkSVGTag::kFeTurbulence || node->tag() == SkSVGTag::kFeColorMatrix ||
                node->tag() == SkSVGTag::kFeComposite || node->tag() == SkSVGTag::kFeFlood ||
                node->tag() == SkSVGTag::kFeGaussianBlur || node->tag() == SkSVGTag::kFeOffset ||
-               node->tag() == SkSVGTag::kFeBlend || node->tag() == SkSVGTag::kFeMorphology;
+               node->tag() == SkSVGTag::kFeBlend || node->tag() == SkSVGTag::kFeMorphology ||
+               node->tag() == SkSVGTag::kFeDisplacementMap;
     }
 
     sk_sp<SkImageFilter> makeImageFilter(const SkSVGRenderContext& ctx,
@@ -46,7 +47,7 @@ public:
     SVG_OPTIONAL_ATTR(X, SkSVGLength)
     SVG_OPTIONAL_ATTR(Y, SkSVGLength)
     SVG_OPTIONAL_ATTR(Width, SkSVGLength)
-    SVG_OPTIONAL_ATTR(Height, SkSVGLength)
+    SVG_OPTIONAL_ATTR(Height, SkSVGLength
 
 protected:
     explicit SkSVGFe(SkSVGTag t) : INHERITED(t) {}
