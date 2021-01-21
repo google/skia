8df6827fe072a4ed7d8869213a129cca24aef728
John Stiles <johnstiles@google.com>
2021-01-21 16:45:52 -0500

    Add checkpointing functionality to SkSLLexer.
    Change-Id: I571c7981dbec3c43a894fe599f2e2a167bc380df

--- src/sksl/SkSLLexer.h
+++ src/sksl/SkSLLexer.h
@@ -135,6 +135,10 @@ public:
 
     Token next();
 
+    int32_t getCheckpoint() const { return fOffset; }
+
+    int32_t rewindToCheckpoint(int32_t checkpoint) { fOffset = checkpoint; }
+
 private:
     const char* fText;
     int32_t fLength;
