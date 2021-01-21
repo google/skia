9e623f90c2cbd2ad432619daa152939fadd77c67
John Stiles <johnstiles@google.com>
2021-01-21 17:46:38 -0500

    Migrate some SkSL folding tests to a GM slide.
    Change-Id: I2ccb427a907975ae84aee19d8e68d774b2cb638c

---
+++ tests/SkSLTest.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm/gm.h"
+#include "include/core/SkBitmap.h"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkData.h"
+#include "include/core/SkFont.h"
+#include "include/core/SkPaint.h"
+#include "include/core/SkSize.h"
+#include "include/core/SkString.h"
+#include "include/core/SkSurface.h"
+#include "include/effects/SkGradientShader.h"
+#include "include/effects/SkImageFilters.h"
+#include "include/effects/SkRuntimeEffect.h"
+#include "include/utils/SkRandom.h"
+#include "tests/Test.h"
+#include "tools/Resources.h"
+#include "tools/ToolUtils.h"
+
+static void test(skiatest::Reporter* r, const char* testFile) {
+    SkString resourcePath = SkStringPrintf("sksl/%s", testFile);
+    sk_sp<SkData> shaderData = GetResourceAsData(resourcePath.c_str());
+    if (!shaderData) {
+        ERRORF(r, "%s: Unable to load file", testFile);
+        return;
+    }
+
+    SkString shaderString{reinterpret_cast<const char*>(shaderData->bytes()), shaderData->size()};
+    auto [effect, error] = SkRuntimeEffect::Make(shaderString);
+    if (!effect) {
+        ERRORF(r, "%s: %s", testFile, error.c_str());
+        return;
+    }
+
+    SkRuntimeShaderBuilder builder(effect);
+    sk_sp<SkShader> shader = builder.makeShader(/*localMatrix=*/nullptr, /*isOpaque=*/true);
+    if (!shader) {
+        ERRORF(r, "%s: Unable to build shader", testFile);
+        return;
+    }
+
+    static const SkRect kRect = SkRect::MakeWH(1, 1);
+
+    SkBitmap bitmap;
+    bitmap.allocN32Pixels(kRect.width(), kRect.height());
+
+    SkCanvas canvas(bitmap);
+    canvas.clear(0x00000000);
+
+    SkPaint paintShader;
+    paintShader.setShader(shader);
+    canvas.drawRect(kRect, paintShader);
+
+    SkColor color = bitmap.getColor(0, 0);
+    REPORTER_ASSERT(r, color == SkColorSetARGB(0xFF, 0x00, 0xFF, 0x00),
+                       "Expected: solid green. Actual: A=%02X R=%02X G=%02X B=%02X.",
+                       SkColorGetA(color), SkColorGetR(color), SkColorGetG(color), SkColorGetB(color));
+}
+
+DEF_TEST(SkSLBoolFolding, r)             { test(r, "folding/BoolFolding.sksl"); }
+DEF_TEST(SkSLFloatFolding, r)            { test(r, "folding/FloatFolding.sksl"); }
+DEF_TEST(SkSLIntFoldingES2, r)           { test(r, "folding/IntFoldingES2.sksl"); }
+DEF_TEST(SkSLMatrixFoldingES2, r)        { test(r, "folding/MatrixFoldingES2.sksl"); }
+DEF_TEST(SkSLShortCircuitBoolFolding, r) { test(r, "folding/ShortCircuitBoolFolding.sksl"); }
+DEF_TEST(SkSLVectorScalarFolding, r)     { test(r, "folding/VectorScalarFolding.sksl"); }
+
+/*
+TODO(johnstiles): enable these tests when Runtime Effects have support for ES3
+
+DEF_TEST(SkSLIntFoldingES3, r)           { test(r, "folding/IntFoldingES3.sksl"); }
+DEF_TEST(SkSLMatrixFoldingES3, r)        { test(r, "folding/MatrixFoldingES3.sksl"); }
+*/
