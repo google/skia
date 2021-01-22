4b146137731d81006e1e193a474f1e9fb668410c
Chris Dalton <csmartdalton@google.com>
2021-01-21 17:56:05 -0700

    Remove the offthread triangulation from GrPathTessellateOp
    Rather than copying these triangles twice, we can run "pathToPolys"
    during onPrePrepare and "polysToTriangles" during onPrepare.
    
    Also adds a benchmark for normal and inner-fan triangulation.
    
    Bug: skia:10419
    Change-Id: Id301afde5de11d93ae026e75e42ac03a50867687

---
+++ src/gpu/GrInnerFanTriangulator.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrInnerFanTriangulator_DEFINED
+#define GrInnerFanTriangulator_DEFINED
+
+#include "src/gpu/GrTriangulator.h"
+
+// Triangulates the inner polygon(s) of a path (i.e., the triangle fan for a Redbook rendering
+// method). When combined with the outer curves and breadcrumb triangles, these produce a complete
+// path. If a breadcrumbCollector is not provided, pathToPolys fails upon self intersection.
+class GrInnerFanTriangulator : private GrTriangulator {
+public:
+    using GrTriangulator::BreadcrumbTriangleCollector;
+
+    GrInnerFanTriangulator(const SkPath& path, BreadcrumbTriangleCollector* breadcrumbCollector)
+            : GrTriangulator(path) {
+        fCullCollinearVertices = false;
+        fDisallowSelfIntersection = !breadcrumbCollector;
+        fBreadcrumbTriangles = breadcrumbCollector;
+    }
+
+    int pathToTriangles(GrEagerVertexAllocator* vertexAlloc, bool* isLinear) {
+        return this->polysToTriangles(this->pathToPolys(isLinear), vertexAlloc);
+    }
+
+    Poly* pathToPolys(bool* isLinear) {
+        return this->GrTriangulator::pathToPolys(0, SkRect::MakeEmpty(), isLinear);
+    }
+
+    int polysToTriangles(Poly* polys, GrEagerVertexAllocator* vertexAlloc) {
+        return this->GrTriangulator::polysToTriangles(polys, vertexAlloc);
+    }
+};
+
+#endif
