2c52f4d764c6d354d1532c2dd21faa02e18abf40
Ethan Nicholas <ethannicholas@google.com>
2021-01-22 14:44:01 -0500

    Added basic SkSL DSL statements
    (and Ternary for good measure)
    
    Change-Id: I4afa121d54ab9ba8d0814693ce53da7cb73ef340

--- src/sksl/dsl/DSLCore.cpp
+++ src/sksl/dsl/DSLCore.cpp
@@ -7,7 +7,12 @@
 
 #include "src/sksl/dsl/DSLCore.h"
 
+#include "src/sksl/SkSLCompiler.h"
+#include "src/sksl/SkSLIRGenerator.h"
 #include "src/sksl/dsl/priv/DSLWriter.h"
+#include "src/sksl/ir/SkSLDoStatement.h"
+#include "src/sksl/ir/SkSLForStatement.h"
+#include "src/sksl/ir/SkSLIfStatement.h"
 
 namespace SkSL {
 
@@ -25,6 +30,70 @@ void End() {
 
 void SetErrorHandler(ErrorHandler* errorHandler) {
     DSLWriter::SetErrorHandler(errorHandler);
+}
+class DSLCore {
+public:
+    static DSLStatement Declare(DSLVar& var, DSLExpression initialValue) {
+        if (!var.fDeclaration) {
+            DSLWriter::ReportError("Declare failed (was the variable already declared?)");
+            return DSLStatement();
+        }
+        VarDeclaration& decl = var.fDeclaration->as<SkSL::VarDeclaration>();
+        std::unique_ptr<Expression> expr = initialValue.coerceAndRelease(decl.var().type());
+        if (expr) {
+            decl.fValue = std::move(expr);
+        }
+        return DSLStatement(std::move(var.fDeclaration));
+    }
+
+    static DSLStatement Do(DSLStatement stmt, DSLExpression test) {
+        return DSLWriter::IRGenerator().convertDo(stmt.release(), test.release());
+    }
+
+    static DSLStatement For(DSLStatement initializer, DSLExpression test, DSLExpression next,
+                            DSLStatement stmt) {
+        return DSLWriter::IRGenerator().convertFor(/*offset=*/-1, initializer.release(),
+                                                   test.release(), next.release(), stmt.release());
+    }
+
+    static DSLStatement If(DSLExpression test, DSLStatement ifTrue, DSLStatement ifFalse) {
+        return DSLWriter::IRGenerator().convertIf(/*offset=*/-1, /*isStatic=*/false, test.release(),
+                                                  ifTrue.release(), ifFalse.release());
+    }
+
+    static DSLExpression Ternary(DSLExpression test, DSLExpression ifTrue, DSLExpression ifFalse) {
+        return DSLWriter::IRGenerator().convertTernaryExpression(test.release(), ifTrue.release(),
+                                                                 ifFalse.release());
+    }
+
+    static DSLStatement While(DSLExpression test, DSLStatement stmt) {
+        return DSLWriter::IRGenerator().convertWhile(/*offset=*/-1, test.release(), stmt.release());
+    }
+};
+
+DSLStatement Declare(DSLVar& var, DSLExpression initialValue) {
+    return DSLCore::Declare(var, std::move(initialValue));
+}
+
+DSLStatement Do(DSLStatement stmt, DSLExpression test) {
+    return DSLCore::Do(std::move(stmt), std::move(test));
+}
+
+DSLStatement For(DSLStatement initializer, DSLExpression test, DSLExpression next,
+                 DSLStatement stmt) {
+    return DSLCore::For(std::move(initializer), std::move(test), std::move(next), std::move(stmt));
+}
+
+DSLStatement If(DSLExpression test, DSLStatement ifTrue, DSLStatement ifFalse) {
+    return DSLCore::If(std::move(test), std::move(ifTrue), std::move(ifFalse));
+}
+
+DSLExpression Ternary(DSLExpression test, DSLExpression ifTrue, DSLExpression ifFalse) {
+    return DSLCore::Ternary(std::move(test), std::move(ifTrue), std::move(ifFalse));
+}
+
+DSLStatement While(DSLExpression test, DSLStatement stmt) {
+    return DSLCore::While(std::move(test), std::move(stmt));
 }
 
 } // namespace dsl
---
+++ src/sksl/dsl/DSLStatement.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_DSL_STATEMENT
+#define SKSL_DSL_STATEMENT
+
+#include "include/core/SkString.h"
+#include "include/core/SkTypes.h"
+#include "src/sksl/ir/SkSLIRNode.h"
+
+#include <memory>
+
+class GrGLSLShaderBuilder;
+
+namespace SkSL {
+
+class Statement;
+
+namespace dsl {
+
+class DSLBlock;
+class DSLExpression;
+class DSLVar;
+
+class DSLStatement {
+public:
+    DSLStatement() {}
+
+    DSLStatement(DSLExpression expr);
+
+    DSLStatement(DSLBlock block);
+
+    DSLStatement(DSLStatement&&) = default;
+
+    ~DSLStatement() {
+        SkASSERTF(!fStatement, "Statement destroyed without being incorporated into program");
+    }
+
+    std::unique_ptr<SkSL::Statement> release() {
+        return std::move(fStatement);
+    }
+
+private:
+    DSLStatement(std::unique_ptr<SkSL::Statement> stmt);
+
+    DSLStatement(std::unique_ptr<SkSL::Expression> expr);
+
+    std::unique_ptr<SkSL::Statement> fStatement;
+
+    friend class DSLBlock;
+    friend class DSLCore;
+};
+
+} // namespace dsl
+
+} // namespace SkSL
+
+#endif
