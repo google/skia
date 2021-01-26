2b153edc3df6a936a58402402a50994df6a4e1b7
Tyler Denniston <tdenniston@google.com>
2021-01-26 12:49:05 -0500

    [svg] Add filter effect light source classes
    These three classes represent light source elements that will eventually
    be used for feSpecularLighting and feDiffuseLighting. Currently they are
    unused.
    
    Spec: https://www.w3.org/TR/SVG11/filters.html#LightSourceDefinitions
    Bug: skia:10841
    Change-Id: Ic7824671662b8cd88cf627affc54173d5e881b7d

--- modules/svg/src/SkSVGDOM.cpp
+++ modules/svg/src/SkSVGDOM.cpp
@@ -21,6 +21,7 @@
 #include "modules/svg/include/SkSVGFeComposite.h"
 #include "modules/svg/include/SkSVGFeFlood.h"
 #include "modules/svg/include/SkSVGFeGaussianBlur.h"
+#include "modules/svg/include/SkSVGFeLightSource.h"
 #include "modules/svg/include/SkSVGFeMorphology.h"
 #include "modules/svg/include/SkSVGFeOffset.h"
 #include "modules/svg/include/SkSVGFeTurbulence.h"
@@ -265,10 +266,13 @@ SortedDictionaryEntry<sk_sp<SkSVGNode>(*)()> gTagFactories[] = {
     { "feBlend"       , []() -> sk_sp<SkSVGNode> { return SkSVGFeBlend::Make();        }},
     { "feColorMatrix" , []() -> sk_sp<SkSVGNode> { return SkSVGFeColorMatrix::Make();  }},
     { "feComposite"   , []() -> sk_sp<SkSVGNode> { return SkSVGFeComposite::Make();    }},
+    { "feDistantLight", []() -> sk_sp<SkSVGNode> { return SkSVGFeDistantLight::Make(); }},
     { "feFlood"       , []() -> sk_sp<SkSVGNode> { return SkSVGFeFlood::Make();        }},
     { "feGaussianBlur", []() -> sk_sp<SkSVGNode> { return SkSVGFeGaussianBlur::Make(); }},
     { "feMorphology"  , []() -> sk_sp<SkSVGNode> { return SkSVGFeMorphology::Make();   }},
     { "feOffset"      , []() -> sk_sp<SkSVGNode> { return SkSVGFeOffset::Make();       }},
+    { "fePointLight"  , []() -> sk_sp<SkSVGNode> { return SkSVGFePointLight::Make();   }},
+    { "feSpotLight"   , []() -> sk_sp<SkSVGNode> { return SkSVGFeSpotLight::Make();    }},
     { "feTurbulence"  , []() -> sk_sp<SkSVGNode> { return SkSVGFeTurbulence::Make();   }},
     { "filter"        , []() -> sk_sp<SkSVGNode> { return SkSVGFilter::Make();         }},
     { "g"             , []() -> sk_sp<SkSVGNode> { return SkSVGG::Make();              }},
