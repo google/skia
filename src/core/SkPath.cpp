02e29f68e832df3dc9aa686b3eceddad9e3f650a
Mike Reed <reed@google.com>
2021-01-26 17:59:22 -0500

    Faster (conservative) IsAxisAlighed
    This is used to decide when we can keep the convex bit during path.transform.
    This new version is much faster than the old, but can return a false negative
    if there are multiple contours.
    
    Change-Id: Ied7ca97d87b9224a3ecd9b77b22b823964809a52

--- src/core/SkPath.cpp
+++ src/core/SkPath.cpp
@@ -1656,11 +1656,12 @@ void SkPath::transform(const SkMatrix& matrix, SkPath* dst, SkApplyPerspectiveCl
         // If we can land a robust convex scan-converter, we may be able to relax/remove this
         // check, and keep convex paths marked as such after a general transform...
         //
-        if (matrix.isScaleTranslate() && SkPathPriv::IsAxisAligned(*this)) {
-            dst->setConvexity(convexity);
-        } else {
-            dst->setConvexity(SkPathConvexity::kUnknown);
+        if (convexity == SkPathConvexity::kConvex &&
+            (!matrix.isScaleTranslate() || !SkPathPriv::IsAxisAligned(*this))) {
+            // Not safe to still assume we're convex...
+            convexity = SkPathConvexity::kUnknown;
         }
+        dst->setConvexity(convexity);
 
         if (this->getFirstDirection() == SkPathFirstDirection::kUnknown) {
             dst->setFirstDirection(SkPathFirstDirection::kUnknown);
@@ -3841,4 +3842,20 @@ bool SkPathPriv::PerspectiveClip(const SkPath& path, const SkMatrix& matrix, SkP
 
 int SkPathPriv::GenIDChangeListenersCount(const SkPath& path) {
     return path.fPathRef->genIDChangeListenerCount();
+}
+
+bool SkPathPriv::IsAxisAligned(const SkPath& path) {
+    // Conservative (quick) test to see if all segments are axis-aligned.
+    // Multiple contours might give a false-negative, but for speed, we ingore that
+    // and just look at the raw points.
+
+    const SkPoint* pts = path.fPathRef->points();
+    const int count = path.fPathRef->countPoints();
+
+    for (int i = 1; i < count; ++i) {
+        if (pts[i-1].fX != pts[i].fX && pts[i-1].fY != pts[i].fY) {
+            return false;
+        }
+    }
+    return true;
 }
