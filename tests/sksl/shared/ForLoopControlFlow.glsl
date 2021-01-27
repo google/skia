2bb861f7b8537be84291859c705e73fead44582a
John Stiles <johnstiles@google.com>
2021-01-27 09:56:04 -0500

    Reland "Add SkSL for-loop control flow test to dm."
    This reverts commit 578f1acbe8a47178efb21df49561c365226f9d95.
    
    Reason for revert: updated test to pass on Mac Intel 5100/6000
    
    Original change's description:
    > Revert "Add SkSL for-loop control flow test to dm."
    >
    > This reverts commit a0c266283a8ef230ad0436dbf885d2b616e7302b.
    >
    > Reason for revert: failing on Mac Intel
    >
    > Original change's description:
    > > Add SkSL for-loop control flow test to dm.
    > >
    > > While loops and do-while loops remain untested in dm, as they are not
    > > supported in ES2 (and therefore not available in Runtime Effects).
    > >
    > > Change-Id: I2f1bfccccd571cc4ced096bc18ebbb9ecc9f9b4a
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/359556
    > > Commit-Queue: John Stiles <johnstiles@google.com>
    > > Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    > > Auto-Submit: John Stiles <johnstiles@google.com>
    > > Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    >
    > TBR=brianosman@google.com,ethannicholas@google.com,johnstiles@google.com
    >
    > Change-Id: I45335d16a695644eaeb8a535298c0efcc616c1ce
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/359840
    > Reviewed-by: John Stiles <johnstiles@google.com>
    > Commit-Queue: John Stiles <johnstiles@google.com>
    
    TBR=brianosman@google.com,ethannicholas@google.com,johnstiles@google.com
    
    Change-Id: I2dc6e870393708a12286658001b723f25a6aec4a

--- tests/sksl/shared/ForLoopControlFlow.glsl
+++ tests/sksl/shared/ForLoopControlFlow.glsl
@@ -4,7 +4,7 @@ uniform vec4 colorWhite;
 vec4 main() {
     vec4 x = colorWhite;
     for (float r = -5.0;r < 5.0; r += 1.0) {
-        x.x = abs(r);
+        x.x = clamp(r, 0.0, 1.0);
         if (x.x == 0.0) break;
     }
     for (float b = 5.0;b >= 0.0; b -= 1.0) {
