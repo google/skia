9ab33b5a58e61572af454535319602fbb35e0ede
Herb Derby <herb@google.com>
2021-01-28 10:57:40 -0500

    WIP: add printf for atlas behavior
    Change-Id: I384f1da4775c6d09170996dd21689124efe2cc7c

--- src/gpu/GrGpu.cpp
+++ src/gpu/GrGpu.cpp
@@ -418,7 +418,8 @@ bool GrGpu::writePixels(GrSurface* surface, int left, int top, int width, int he
                         GrColorType surfaceColorType, GrColorType srcColorType,
                         const GrMipLevel texels[], int mipLevelCount, bool prepForTexSampling) {
     TRACE_EVENT0("skia.gpu", TRACE_FUNC);
-    ATRACE_ANDROID_FRAMEWORK_ALWAYS("Upload %ix%i Texture", width, height);
+    ATRACE_ANDROID_FRAMEWORK_ALWAYS("Texture upload(%u) %ix%i",
+                                    surface->uniqueID().asUInt(), width, height);
     SkASSERT(surface);
     SkASSERT(!surface->framebufferOnly());
 
