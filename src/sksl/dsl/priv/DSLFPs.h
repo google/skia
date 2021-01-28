868f424091bd14dd8b22734c852f970602f809ba
Ethan Nicholas <ethannicholas@google.com>
2021-01-28 14:03:09 -0500

    Initial support for DSL FPs
    Change-Id: I1abbe881f925ac6db4f7d672a391aadd349a33b5

---
+++ src/sksl/dsl/priv/DSLFPs.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SKSL_DSL_FPS
+#define SKSL_DSL_FPS
+
+#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
+#include "src/sksl/dsl/DSL.h"
+
+#if !defined(SKSL_STANDALONE) && SK_SUPPORT_GPU
+
+namespace SkSL {
+
+namespace dsl {
+
+void PushFP(GrGLSLFragmentProcessor* processor, GrGLSLFragmentProcessor::EmitArgs* emitArgs);
+
+void PopFP();
+
+DSLVar sk_InColor();
+
+} // namespace dsl
+
+} // namespace SkSL
+
+#endif // !defined(SKSL_STANDALONE) && SK_SUPPORT_GPU
+
+#endif
