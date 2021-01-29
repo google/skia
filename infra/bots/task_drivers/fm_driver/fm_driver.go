23dc9cae429445aa91193b46b1341bc5b3009175
Mike Klein <mtklein@google.com>
2021-01-29 11:37:36 -0600

    let fm_driver.go decide what to do for each bot
    We can now mimic bots locally by running fm_driver.go like this,
    
        go run **/fm_driver.go -bot $BOT out/fm
    
    where $BOT is like FM-Debian10-Clang-GCE-CPU-AVX2-x86_64-Debug-All.
    
    As a demo, skip aarectmodes and GoodHash on Debian10.
    
    Change-Id: Iec215182dce9f05b8aa6807e837daa0618e2669f

--- infra/bots/task_drivers/fm_driver/fm_driver.go
+++ infra/bots/task_drivers/fm_driver/fm_driver.go
@@ -21,23 +21,43 @@ import (
 	"go.skia.org/infra/task_driver/go/td"
 )
 
-type work struct {
-	Sources []string
-	Flags   []string
+func filter(in []string, test func(string) bool) (out []string) {
+	for _, s := range in {
+		if test(s) {
+			out = append(out, s)
+		}
+	}
+	return
+}
+
+func botJobs(name string, gms []string, tests []string) (jobs [][]string) {
+	parts := strings.Split(name, "-")
+	OS := parts[1]
+
+	// For no reason but as a demo, skip GM aarectmodes and test GoodHash.
+	if OS == "Debian10" {
+		gms = filter(gms, func(s string) bool { return s != "aarectmodes" })
+		tests = filter(tests, func(s string) bool { return s != "GoodHash" })
+	}
+
+	jobs = append(jobs, append([]string{"b=cpu"}, gms...))
+	jobs = append(jobs, append([]string{"b=cpu"}, tests...))
+	jobs = append(jobs, append([]string{"b=cpu", "skvm=true"}, gms...))
+	return
 }
 
 func main() {
 	var (
 		projectId = flag.String("project_id", "", "ID of the Google Cloud project.")
 		taskId    = flag.String("task_id", "", "ID of this task.")
-		taskName  = flag.String("task_name", "", "Name of the task.")
+		bot       = flag.String("bot", "", "Name of the task.")
 		output    = flag.String("o", "", "Dump JSON step data to the given file, or stdout if -.")
 		local     = flag.Bool("local", true, "Running locally (else on the bots)?")
 
 		resources = flag.String("resources", "resources", "Passed to fm -i.")
 		script    = flag.String("script", "", "File (or - for stdin) with one job per line.")
 	)
-	ctx := td.StartRun(projectId, taskId, taskName, output, local)
+	ctx := td.StartRun(projectId, taskId, bot, output, local)
 	defer td.EndRun(ctx)
 
 	actualStderr := os.Stderr
@@ -81,6 +101,11 @@ func main() {
 	tests := query("--listTests")
 
 	// Parse a job like "gms b=cpu ct=8888" into a struct of Sources to run under given Flags.
+	type work struct {
+		Sources []string
+		Flags   []string
+	}
+
 	parse := func(job []string) *work {
 		w := &work{}
 
@@ -140,6 +165,11 @@ func main() {
 		if err := scanner.Err(); err != nil {
 			td.Fatal(ctx, err)
 		}
+	}
+
+	// If we're a bot (or acting as if we are one), add its jobs.
+	if *bot != "" {
+		jobs = append(jobs, botJobs(*bot, gms, tests)...)
 	}
 
 	// We'll kick off workers to run FM with `-s <Sources...> <Flags...>` from parsed jobs.
