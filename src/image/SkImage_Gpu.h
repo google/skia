cc9f3e73f3189d54d27882d083fc094141ed25e4
Brian Salomon <bsalomon@google.com>
2021-02-01 13:18:18 -0500

    Replace SkImage_Base::view() and ::refView() with new asView().
    Make SkImage_Gpu and SkImage_GpuYUVA have their own implementations
    rather than sharing one in SkImage_GpuBase.
    
    New function takes a GrImageTexGenPolicy to enable caller to
    force a new texture to be made and choose it's budgeting status rather
    than receive a cached view or a view owned by the image.
    
    Bug: skia:11208
    Change-Id: I6b389442bf9752276a83b21021070e3190610cd7
    Cq-Include-Trybots: luci.skia.skia.primary:Test-iOS-Clang-iPhone6-GPU-PowerVRGX6450-arm64-Release-All-Metal;luci.chromium.try:linux-rel,mac11.0-blink-rel,linux-blink-rel,win10-blink-rel,gpu-fyi-try-win10-nvidia-sk-dawn-rel-64,gpu-fyi-try-linux-nvidia-skv,gpu-fyi-try-linux-intel-skv,gpu-fyi-try-linux-intel-sk-dawn-rel,gpu-fyi-try-android-p-pixel-2-skv-32,gpu-fyi-try-android-m-nexus-5x-skgl-64

--- src/image/SkImage_Gpu.h
+++ src/image/SkImage_Gpu.h
@@ -12,7 +12,6 @@
 #include "src/gpu/GrGpuResourcePriv.h"
 #include "src/gpu/GrSurfaceProxyPriv.h"
 #include "src/gpu/GrSurfaceProxyView.h"
-#include "src/gpu/SkGr.h"
 #include "src/image/SkImage_GpuBase.h"
 
 class GrDirectContext;
@@ -21,7 +20,7 @@ class GrTexture;
 
 class SkBitmap;
 
-class SkImage_Gpu : public SkImage_GpuBase {
+class SkImage_Gpu final : public SkImage_GpuBase {
 public:
     SkImage_Gpu(sk_sp<GrImageContext>, uint32_t uniqueID, GrSurfaceProxyView, SkColorType,
                 SkAlphaType, sk_sp<SkColorSpace>);
@@ -44,16 +43,11 @@ public:
 
     GrSemaphoresSubmitted onFlush(GrDirectContext*, const GrFlushInfo&) override;
 
-    GrTextureProxy* peekProxy() const override {
-        return fView.asTextureProxy();
-    }
+    GrTextureProxy* peekProxy() const override { return fView.asTextureProxy(); }
 
-    const GrSurfaceProxyView* view(GrRecordingContext* context) const override {
-        if (!fView.proxy()) {
-            return nullptr;
-        }
-        return &fView;
-    }
+    GrSurfaceProxyView asView(GrRecordingContext*,
+                              GrMipmapped,
+                              GrImageTexGenPolicy = kDefaultTexGenPolicy) const override;
 
     GrBackendTexture onGetBackendTexture(bool flushPendingGrContextIO,
                                          GrSurfaceOrigin* origin) const final;
