63247ac206bd7056c50f447efe6f81e03f2b6f9a
John Stiles <johnstiles@google.com>
2021-02-01 11:52:10 -0500

    Update various shared/ tests to run in dm.
    These tests have updated to return green on success, or red on failure.
    Some tests were modified slightly to conform to ES2 limitations, or
    split into separate ES2 and ES3 parts.
    
    Change-Id: Ib47aeca217aef33f3c4b5999d93afed5d42a1e62

--- tests/sksl/shared/DeadStripFunctions.glsl
+++ tests/sksl/shared/DeadStripFunctions.glsl
@@ -1,16 +1,22 @@
 
 out vec4 sk_FragColor;
-vec4 blend_dst(vec4 src, vec4 dst) {
-    return dst;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 unpremul(vec4 color) {
+    return vec4(color.xyz / max(color.w, 9.9999997473787516e-05), color.w);
 }
 vec4 live_fn(vec4 a, vec4 b) {
     return a + b;
 }
-void main() {
+vec4 main() {
+    vec4 a;
+    vec4 b;
+
     {
-        sk_FragColor = live_fn(vec4(3.0), vec4(-3.0));
+        a = live_fn(vec4(3.0), vec4(-5.0));
     }
     {
-        sk_FragColor = blend_dst(vec4(7.0), vec4(-7.0));
+        b = unpremul(vec4(1.0));
     }
+    return a != vec4(0.0) && b != vec4(0.0) ? colorGreen : colorRed;
 }
--- tests/sksl/shared/EmptyBlocksES3.metal
+++ tests/sksl/shared/EmptyBlocksES3.metal
@@ -9,7 +9,7 @@ struct Outputs {
 fragment Outputs fragmentMain(Inputs _in [[stage_in]], bool _frontFacing [[front_facing]], float4 _fragCoord [[position]]) {
     Outputs _out;
     (void)_out;
-    float4 color = float4(0.0, 0.0, 0.0, 0.0);
+    float4 color = float4(0.0);
     for (int counter = 0;counter < 10; ++counter) ;
     for (int counter = 0;counter < 10; ++counter) {
     }
