5ef9111ace93d76b657a5d8fe3d67d0697ad3654
Ethan Nicholas <ethannicholas@google.com>
2021-02-02 17:00:05 -0500

    Added DSL FP codeAppend function.
    This also adds the relevant Start() / End() calls so that the DSL
    is put into the correct state.
    
    Change-Id: I844b0ab5dff06e3f7b0a69458bf4442a5da0f33e

--- src/gpu/glsl/GrGLSLShaderBuilder.h
+++ src/gpu/glsl/GrGLSLShaderBuilder.h
@@ -19,6 +19,12 @@
 
 class GrGLSLColorSpaceXformHelper;
 
+namespace SkSL {
+namespace dsl {
+class DSLStatement;
+} // namespace dsl
+} // namespace SkSL
+
 /**
   base class for all shaders builders
 */
@@ -108,6 +114,8 @@ public:
     void codeAppend(const char* str) { this->code().append(str); }
 
     void codeAppend(const char* str, size_t length) { this->code().append(str, length); }
+
+    void codeAppend(SkSL::dsl::DSLStatement stmt);
 
     void codePrependf(const char format[], ...) SK_PRINTF_LIKE(2, 3) {
        va_list args;
