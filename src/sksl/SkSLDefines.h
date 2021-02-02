dbef8b7a9083c457d507c494224e51ede5abf0a6
John Stiles <johnstiles@google.com>
2021-02-02 18:35:09 -0500

    Move the default inline threshold to SkSLDefines.h.
    Change-Id: Idf3f95ce3d0467c8273c627227c2e6bb16d04614

--- src/sksl/SkSLDefines.h
+++ src/sksl/SkSLDefines.h
@@ -22,4 +22,12 @@
 using SKSL_INT = int64_t;
 using SKSL_FLOAT = float;
 
+namespace SkSL {
+// Functions larger than this (measured in IR nodes) will not be inlined. This growth factor
+// accounts for the number of calls being inlined--i.e., a function called five times (that is, with
+// five inlining opportunities) would be considered 5x larger than if it were called once. This
+// default threshold value is arbitrary, but tends to work well in practice.
+static constexpr int kDefaultInlineThreshold = 50;
+}
+
 #endif
