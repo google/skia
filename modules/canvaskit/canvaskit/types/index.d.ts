d9c0b6e1cca06ab26bc2150aade05288a9f656ee
Kevin Lubick <kjlubick@google.com>
2021-02-03 11:26:12 -0500

    [canvaskit] Add error callback for runtime effect constructor
    Change-Id: I9bdbafb653398ccbb72e6e762ec4b6af294f9110

--- modules/canvaskit/canvaskit/types/index.d.ts
+++ modules/canvaskit/canvaskit/types/index.d.ts
@@ -3101,8 +3101,10 @@ export interface RuntimeEffectFactory {
     /**
      * Compiles a RuntimeEffect from the given shader code.
      * @param sksl - Source code for a shader written in SkSL
+     * @param callback - will be called with any compilation error. If not provided, errors will
+     *                   be printed to console.log().
      */
-    Make(sksl: string): RuntimeEffect | null;
+    Make(sksl: string, callback?: (err: string) => void): RuntimeEffect | null;
 }
 
 /**
