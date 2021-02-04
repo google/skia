5be67cb37d9596f2e867104f7afeae1c3e986bfa
John Stiles <johnstiles@google.com>
2021-02-04 12:14:02 -0500

    Migrate additional SkSL tests to run in dm.
    - MultipleAssignments
    - NegatedVectorLiteral
    - NumberCasts
    - OutParams
    - OutParamsTricky
    
    Change-Id: I8fe5108b6b4b9b1ecbc8fb8b14d03b949425c0ed
    Bug: skia:11009, skia:11269

--- tests/sksl/shared/NegatedVectorLiteral.glsl
+++ tests/sksl/shared/NegatedVectorLiteral.glsl
@@ -1,14 +1,21 @@
 
 out vec4 sk_FragColor;
-void main() {
-    sk_FragColor.x = 1.0;
-    sk_FragColor.y = 1.0;
-    sk_FragColor.z = 1.0;
-    sk_FragColor.w = 1.0;
-
-    sk_FragColor.x = 1.0;
-    sk_FragColor.y = 1.0;
-    sk_FragColor.z = 1.0;
-    sk_FragColor.w = 1.0;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+bool test_int() {
+    ivec4 result;
+    result.x = 1;
+    result.y = 1;
+    result.z = 1;
+    result.w = 1;
+    return bool(((result.x * result.y) * result.z) * result.w);
+}
+vec4 main() {
+    vec4 _1_result;
+    _1_result.x = 1.0;
+    _1_result.y = 1.0;
+    _1_result.z = 1.0;
+    _1_result.w = 1.0;
+    return bool(((_1_result.x * _1_result.y) * _1_result.z) * _1_result.w) && test_int() ? colorGreen : colorRed;
 
 }
