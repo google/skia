877c0ecd8c5c203f072b3a34e794185f7579b304
John Stiles <johnstiles@google.com>
2021-02-05 16:58:05 -0500

    Migrate the last SkSL tests in shared/ to dm.
    The leftover tests in shared/ are not easily testable as Runtime
    Effects; they do things that ES2 doesn't support or use a feature not
    exposed directly by Runtime Effects.
    
    Change-Id: I7ebe170cf713c4a0d2dbef333c1fcbac2410c67f
    Bug: skia:11009

--- tests/sksl/shared/UnusedVariables.glsl
+++ tests/sksl/shared/UnusedVariables.glsl
@@ -1,10 +1,10 @@
 
 out vec4 sk_FragColor;
-void main() {
+vec4 main() {
     float b = 2.0;
 
     float d = 3.0;
     b++;
     d++;
-    sk_FragColor = vec4(b, b, d, d);
+    return vec4(float(b == 2.0), float(b == 3.0), float(d == 5.0), float(d == 4.0));
 }
