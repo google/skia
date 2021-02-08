6d7357c1438920ee8b0a377d56f4ab1d77bdc9e0
Mike Reed <reed@google.com>
2021-02-08 10:49:57 -0500

    Work to improve blendmode dox
    Change-Id: I0ea12ad5c651aa6bd2f9b22c1ad539415d5da9bf
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/367760
    Reviewed-by: Mike Klein <mtklein@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>

--- include/core/SkBlendMode.h
+++ include/core/SkBlendMode.h
@@ -10,38 +10,66 @@
 
 #include "include/core/SkTypes.h"
 
+/**
+ *  Blends are operators that take in two colors (source, destination) and return a new color.
+ *  Many of these operate the same on all 4 components: red, green, blue, alpha. For these,
+ *  we just document what happens to one component, rather than naming each one separately.
+ *
+ *  Different SkColorTypes have different representations for color components:
+ *      8-bit: 0..255
+ *      6-bit: 0..63
+ *      5-bit: 0..31
+ *      4-bit: 0..15
+ *     floats: 0...1
+ *
+ *  The documentation is expressed as if the component values are always 0..1 (floats).
+ *
+ *  For brevity, the documentation uses the following abbreviations
+ *  s  : source
+ *  d  : destination
+ *  sa : source alpha
+ *  da : destination alpha
+ *
+ *  Results are appreviated
+ *  r  : if all 4 components are computed in the same manner
+ *  ra : result alpha component
+ *  rc : result "color": red, green, blue components
+ */
 enum class SkBlendMode {
-    kClear,                           //!< replaces destination with zero: fully transparent
-    kSrc,                             //!< replaces destination
-    kDst,                             //!< preserves destination
-    kSrcOver,                         //!< source over destination
-    kDstOver,                         //!< destination over source
-    kSrcIn,                           //!< source trimmed inside destination
-    kDstIn,                           //!< destination trimmed by source
-    kSrcOut,                          //!< source trimmed outside destination
-    kDstOut,                          //!< destination trimmed outside source
-    kSrcATop,                         //!< source inside destination blended with destination
-    kDstATop,                         //!< destination inside source blended with source
-    kXor,                             //!< each of source and destination trimmed outside the other
-    kPlus,                            //!< sum of colors
-    kModulate,                        //!< product of premultiplied colors; darkens destination
-    kScreen,                //!< multiply inverse of pixels, inverting result; brightens destination
+    kClear,         //!< r = 0
+    kSrc,           //!< r = s
+    kDst,           //!< r = d
+    kSrcOver,       //!< r = s + (1-sa)*d
+    kDstOver,       //!< r = d + (1-da)*s
+    kSrcIn,         //!< r = s * da
+    kDstIn,         //!< r = d * sa
+    kSrcOut,        //!< r = s * (1-da)
+    kDstOut,        //!< r = d * (1-sa)
+    kSrcATop,       //!< r = s*da + d*(1-sa)
+    kDstATop,       //!< r = d*sa + s*(1-da)
+    kXor,           //!< r = s*(1-da) + d*(1-sa)
+    kPlus,          //!< r = min(s + d, 1)
+    kModulate,      //!< r = s*d
+    kScreen,        //!< r = s + d - s*d
+
+    kOverlay,       //!< multiply or screen, depending on destination
+    kDarken,        //!< rc = s + d - max(s*da, d*sa), ra = kSrcOver
+    kLighten,       //!< rc = s + d - min(s*da, d*sa), ra = kSrcOver
+    kColorDodge,    //!< brighten destination to reflect source
+    kColorBurn,     //!< darken destination to reflect source
+    kHardLight,     //!< multiply or screen, depending on source
+    kSoftLight,     //!< lighten or darken, depending on source
+    kDifference,    //!< rc = s + d - 2*(min(s*da, d*sa)), ra = kSrcOver
+    kExclusion,     //!< rc = s + d - two(s*d), ra = kSrcOver
+    kMultiply,      //!< r = s*(1-da) + d*(1-sa) + s*d
+
+    kHue,           //!< hue of source with saturation and luminosity of destination
+    kSaturation,    //!< saturation of source with hue and luminosity of destination
+    kColor,         //!< hue and saturation of source with luminosity of destination
+    kLuminosity,    //!< luminosity of source with hue and saturation of destination
+
     kLastCoeffMode     = kScreen,     //!< last porter duff blend mode
-    kOverlay,                         //!< multiply or screen, depending on destination
-    kDarken,                          //!< darker of source and destination
-    kLighten,                         //!< lighter of source and destination
-    kColorDodge,                      //!< brighten destination to reflect source
-    kColorBurn,                       //!< darken destination to reflect source
-    kHardLight,                       //!< multiply or screen, depending on source
-    kSoftLight,                       //!< lighten or darken, depending on source
-    kDifference,                      //!< subtract darker from lighter with higher contrast
-    kExclusion,                       //!< subtract darker from lighter with lower contrast
-    kMultiply,                        //!< multiply source with destination, darkening image
     kLastSeparableMode = kMultiply,   //!< last blend mode operating separately on components
-    kHue,                           //!< hue of source with saturation and luminosity of destination
-    kSaturation,                    //!< saturation of source with hue and luminosity of destination
-    kColor,                         //!< hue and saturation of source with luminosity of destination
-    kLuminosity,                    //!< luminosity of source with hue and saturation of destination
     kLastMode          = kLuminosity, //!< last valid value
 };
 
