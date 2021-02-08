c59e0d98d5f9c860b5158f54a103e48ef5629ced
Herb Derby <herb@google.com>
2021-02-08 17:57:03 -0500

    allocation system for properly aligned bytes
    GrBagOfBytes parcels out bytes with a given size and alignment.
    It is very careful about checking all values coming so that
    no calculations overflows.
    
    If the current block is not big enough to satisfy the request
    bytes, a new block will be allocated to satisfy the
    requested bytes.
    
    Change-Id: If7b5e812f903b51ba4936605cd0dc298dda8821a

--- src/gpu/text/GrTextBlob.h
+++ src/gpu/text/GrTextBlob.h
@@ -8,6 +8,7 @@
 #ifndef GrTextBlob_DEFINED
 #define GrTextBlob_DEFINED
 
+#include <algorithm>
 #include <limits>
 
 #include "include/core/SkPoint3.h"
@@ -36,6 +37,125 @@ class SkMatrixProvider;
 class SkSurfaceProps;
 class SkTextBlob;
 class SkTextBlobRunIterator;
+
+// GrBagOfBytes parcels out bytes with a given size and alignment.
+class GrBagOfBytes {
+public:
+    GrBagOfBytes(char* block, size_t blockSize, size_t firstHeapAllocation);
+    explicit GrBagOfBytes(size_t firstHeapAllocation = 0);
+    ~GrBagOfBytes();
+
+    // Given a requestedSize round up to the smallest size that accounts for all the per block
+    // overhead and alignment. It crashes if requestedSize is negative or too big.
+    static constexpr int MinimumSizeWithOverhead(int requestedSize, int assumedAlignment = 1) {
+        return CalculateMinimumSizeWithOverhead(
+                requestedSize, assumedAlignment, sizeof(Block), kMaxAlignment);
+    }
+
+    static constexpr int CalculateMinimumSizeWithOverhead(
+            int requestedSize, int assumedAlignment, int blockSize, int maxAlignment) {
+        SkASSERT_RELEASE(0 <= requestedSize && requestedSize < kMaxByteSize);
+        auto diffOrZero = [](int a, int b) {int diff = a - b; return diff > 0 ? diff : 0; };
+        const int biasGivenAlignment = diffOrZero(maxAlignment, assumedAlignment);
+
+        auto alignUp = [](int size, int alignment) {return (size + (alignment - 1)) & -alignment;};
+
+        // minimumSize is the minimum number of bytes needed to assure that at least
+        // requestedSize bytes is allocated plus the overhead of the block constrained to placing
+        // the block on a kMaxAlignment address.
+        // For example given memory from malloc where alignof(max_align_t) = 8:
+        //    assumedAlignment = 8, requestedSize = 63, sizeof(Block) = 16, kMaxAlignment = 16,
+        //    biasGivenAlignment = 8
+        //       temp = 63 + 16 + 8 = 87
+        //       minimumSize = alignUp(87, 8) = (87 + 7) & ~7 = 88
+        // This results in a situation where 8 bytes may be unused trapped above the block. If
+        // byte 64 is aligned to 16 then the block is placed there, else the block is placed at
+        // byte 72.
+        //
+        // Note: on 64-bit machines with alignof(max_align_t) of 16, and 32-bit machines with
+        // alignof(max_align_t) of 8 there are no unused bytes.
+
+        int minimumSize =
+                alignUp(requestedSize + blockSize + biasGivenAlignment, assumedAlignment);
+
+        // If minimumSize is > 32k then round to a 4K boundary unless it is too close to the
+        // maximum int. The > 32K heuristic is from the JEMalloc behavior.
+        constexpr int k32K = (1 << 15);
+        if (minimumSize >= k32K && minimumSize < std::numeric_limits<int>::max() - k4K) {
+            minimumSize = alignUp(minimumSize, k4K);
+        }
+
+        return minimumSize;
+    }
+
+    template <int size>
+    using Storage = std::array<char, MinimumSizeWithOverhead(size, 1)>;
+
+    // Returns the number of bytes suitable for holding n Ts.
+    template <typename T> char* allocateBytesFor(int n = 1) {
+        static_assert(alignof(T) <= kMaxAlignment, "Alignment is too big for arena");
+        static_assert(sizeof(T) < kMaxByteSize, "Size is too big for arena");
+        constexpr int kMaxN = kMaxByteSize / sizeof(T);
+        SkASSERT_RELEASE(0 <= n && n < kMaxN);
+
+        int size = sizeof(T) * (n ? n : 1);
+        return this->allocateBytes(size, alignof(T));
+    }
+
+    char* alignedBytes(int unsafeSize, int unsafeAlignment);
+
+private:
+    // 16 seems to be a good number for alignment. If a use case for larger alignments is found,
+    // we can turn this into a template parameter.
+    static constexpr int kMaxAlignment = std::max(16, (int)alignof(max_align_t));
+    // The largest size that can be allocated. In larger sizes, the block is rounded up to 4K
+    // chunks. Leave a 4K of slop.
+    static constexpr int k4K = (1 << 12);
+    // This should never overflow with the calculations done on the code.
+    static constexpr int kMaxByteSize = std::numeric_limits<int>::max() - k4K;
+
+    // The Block starts at the location pointed to by fEndByte.
+    // Beware. Order is important here. The destructor for fPrevious must be called first because
+    // the Block is embedded in fBlockStart. Destructors are run in reverse order.
+    struct Block {
+        Block(char* previous, char* startOfBlock);
+        // The start of the originally allocated bytes. This is the thing that must be deleted.
+        char* const fBlockStart;
+        Block* const fPrevious;
+    };
+
+    // Note: fCapacity is the number of bytes remaining, and is subtracted from fEndByte to
+    // generate the location of the object.
+    char* allocateBytes(int size, int alignment) {
+        fCapacity = fCapacity & -alignment;
+        if (fCapacity < size) {
+            this->needMoreBytes(size, alignment);
+        }
+        char* const ptr = fEndByte - fCapacity;
+        SkASSERT(((intptr_t)ptr & (alignment - 1)) == 0);
+        SkASSERT(fCapacity >= size);
+        fCapacity -= size;
+        return ptr;
+    }
+
+    // Adjust fEndByte and fCapacity give a new block starting at bytes with size.
+    void setupBytesAndCapacity(char* bytes, int size);
+
+    // Adjust fEndByte and fCapacity to satisfy the size and alignment request.
+    void needMoreBytes(int size, int alignment);
+
+    // This points to the highest kMaxAlignment address in the allocated block. The address of
+    // the current end of allocated data is given by fEndByte - fCapacity. While the negative side
+    // of this pointer are the bytes to be allocated. The positive side points to the Block for
+    // this memory. In other words, the pointer to the Block structure for these bytes is
+    // reinterpret_cast<Block*>(fEndByte).
+    char* fEndByte{nullptr};
+
+    // The number of bytes remaining in this block.
+    int fCapacity{0};
+
+    SkFibBlockSizes<kMaxByteSize> fFibProgression;
+};
 
 // -- GrAtlasSubRun --------------------------------------------------------------------------------
 // GrAtlasSubRun is the API that GrAtlasTextOp uses to generate vertex data for drawing.
