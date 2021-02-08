e99dcb6998410f669634142c80f170043335b24b
John Stiles <johnstiles@google.com>
2021-02-08 14:18:19 -0500

    Migrate the last SkSL tests in shared/ to dm.
    The leftover tests in shared/ are not easily testable as Runtime
    Effects; they do things that ES2 doesn't support or use a feature not
    exposed directly by Runtime Effects.
    
    Change-Id: I7ebe170cf713c4a0d2dbef333c1fcbac2410c67f
    Bug: skia:11009

--- tests/sksl/shared/UnaryPositiveNegative.glsl
+++ tests/sksl/shared/UnaryPositiveNegative.glsl
@@ -1,5 +1,11 @@
 
 out vec4 sk_FragColor;
-void main() {
-    sk_FragColor.xy = vec2(-1.0);
+uniform vec4 colorWhite;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    vec2 x = colorWhite.xy;
+    x = x;
+    x = -x;
+    return x == vec2(-1.0) ? colorGreen : colorRed;
 }
