fd03a7fcf1ed0942644afaae7ed7c58fd36572f1
Robert Phillips <robertphillips@google.com>
2021-02-09 12:22:00 -0500

    Handle overflow of accumulated rect perimeter in SkDaskPath cull_path
    Bug:1175370
    Change-Id: Ic6760d38c29c440d2d4fcb225a5c556e56a992f6

--- src/utils/SkDashPath.cpp
+++ src/utils/SkDashPath.cpp
@@ -191,12 +191,14 @@ static bool cull_path(const SkPath& srcPath, const SkStrokeRec& rec,
         SkPoint pts[4];  // Rects are all moveTo and lineTo, so we'll only use pts[0] and pts[1].
         SkAssertResult(SkPath::kMove_Verb == iter.next(pts));
 
+        int iPt = 0;
         SkScalar accum = 0;  // Sum of unculled edge lengths to keep the phase correct.
         while (iter.next(pts) == SkPath::kLine_Verb) {
             // Notice this vector v and accum work with the original unclipped length.
             SkVector v = pts[1] - pts[0];
 
-            if (clip_line(pts, bounds, intervalLength, SkScalarMod(accum, intervalLength))) {
+//            float pP = SkScalarMod(accum, intervalLength);
+            if (clip_line(pts, bounds, intervalLength, accum)) {
                 // pts[0] may have just been changed by clip_line().
                 // If that's not where we ended the previous lineTo(), we need to moveTo() there.
                 SkPoint last;
@@ -209,6 +211,8 @@ static bool cull_path(const SkPath& srcPath, const SkStrokeRec& rec,
             // We either just traveled v.fX horizontally or v.fY vertically.
             SkASSERT(v.fX == 0 || v.fY == 0);
             accum += SkScalarAbs(v.fX + v.fY);
+            accum = SkScalarMod(accum, intervalLength);
+            ++iPt;
         }
         return !dstPath->isEmpty();
     }
