741d6ca09ebfdf18d814c4d3ca8444dfbf511272
Greg Daniel <egdaniel@google.com>
2021-02-09 15:21:33 -0500

    Reland "Only store resources in the GrResourceCache::fScratchMap that are available to be scratch."
    This reverts commit 4a0bc2b344d7b8c178adf8bd821add2a1645c643.
    
    Reason for revert: reland with crash workaround
    
    Original change's description:
    > Revert "Only store resources in the GrResourceCache::fScratchMap that are available to be scratch."
    >
    > This reverts commit 1a2326363a724ff9512b04455f7004ef810bc02f.
    >
    > Reason for revert: breaking win10 quadro400 perf bot on vk and vkmsaa
    >
    > Original change's description:
    > > Only store resources in the GrResourceCache::fScratchMap that are available to be scratch.
    > >
    > > Currently when we create a scratch resource, we immediately add it to
    > > scratch map and it will stay there until we delete the resource. The one
    > > exception to this is adding a unique key will remove a resource from
    > > the scratch map. This means there are resources in the scratch map that
    > > can't be returned when looking for a scratch because they are either
    > > already in use by something else or their budget was changed to
    > > unbudgeted. This means everything time we do a scratch lookup, even
    > > after finding the list of resources that match a key, we still have to
    > > iterate that list to see if we can use that resource or not.
    > >
    > > The problem comes when we may have lots of resources that all match the
    > > same key (think 1000s of identical buffers). Then the cost of iterating
    > > this list starts to get very high.
    > >
    > > This change makes it so only resources that can actively be used as a
    > > scratch at that moment are stored in the scratch map. Thus when we find
    > > a scratch resource we pull it out of the scratch map. When that resources
    > > refs go back to zero it is added back to the scratch map. Similar removal
    > > is also now used for changing a resource to and from budgeted.
    > >
    > > Change-Id: I52b415d0e035dfc589f3d712be85799a56827bf0
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/367976
    > > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > > Commit-Queue: Greg Daniel <egdaniel@google.com>
    >
    > TBR=egdaniel@google.com,bsalomon@google.com
    >
    > Change-Id: I1e57e10e75f930adfecb0e4167c1d6269798c893
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/368236
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Greg Daniel <egdaniel@google.com>
    
    TBR=egdaniel@google.com,bsalomon@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: Ied3995b963f8383954fc4a53a1de9e17234e5e6c

--- src/gpu/vk/GrVkGpu.cpp
+++ src/gpu/vk/GrVkGpu.cpp
@@ -54,7 +54,8 @@
 #include <unistd.h>
 #endif // !defined(SK_BUILD_FOR_WIN)
 
-#if defined(SK_BUILD_FOR_WIN) && defined(SK_DEBUG)
+//#if defined(SK_BUILD_FOR_WIN) && defined(SK_DEBUG)
+#if defined(SK_BUILD_FOR_WIN)
 #include "src/core/SkLeanWindows.h"
 #endif
 
@@ -244,6 +245,8 @@ GrVkGpu::GrVkGpu(GrDirectContext* direct, const GrVkBackendContext& backendConte
 }
 
 void GrVkGpu::destroyResources() {
+    SkDebugf("In GrVkGpu destroyResources\n");
+
     if (fMainCmdPool) {
         fMainCmdPool->getPrimaryCommandBuffer()->end(this, /*abandoningBuffer=*/true);
         fMainCmdPool->close();
@@ -276,18 +279,28 @@ void GrVkGpu::destroyResources() {
 }
 
 GrVkGpu::~GrVkGpu() {
+    SkDebugf("In GrVkGpu dtor\n");
+
     if (!fDisconnected) {
+        SkDebugf("In GrVkGpu dtor b4 destroyResources\n");
+
         this->destroyResources();
     }
     // We don't delete the memory allocator until the very end of the GrVkGpu lifetime so that
     // clients can continue to delete backend textures even after a context has been abandoned.
+    SkDebugf("In GrVkGpu dtor b4 reset allocator\n");
+
     fMemoryAllocator.reset();
 }
 
 
 void GrVkGpu::disconnect(DisconnectType type) {
+    SkDebugf("In GrVkGpu disconnect\n");
+
     INHERITED::disconnect(type);
     if (!fDisconnected) {
+        SkDebugf("In GrVkGpu disconnect b4 destroy resources\n");
+
         this->destroyResources();
 
         fSemaphoresToWaitOn.reset();
@@ -2209,7 +2222,7 @@ void GrVkGpu::finishOutstandingGpuWork() {
             sleep(1);  // In seconds
     #endif
         }
-    #endif
+#endif
 }
 
 void GrVkGpu::onReportSubmitHistograms() {
