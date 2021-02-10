86443fb3fbe4fa69b6a2c369697314432e2e59b4
Brian Salomon <bsalomon@google.com>
2021-02-10 11:12:06 -0500

    xfer in
    Change-Id: I129480755a2bd8370295eb702b92fb0576f8a73b

--- src/gpu/GrSurfaceContext.cpp
+++ src/gpu/GrSurfaceContext.cpp
@@ -497,24 +497,53 @@ bool GrSurfaceContext::writePixels(GrDirectContext* dContext, GrPixmap src, SkIP
                      src.rowBytes() != src.info().minRowBytes();
     bool convert = premul || unpremul || needColorConversion || makeTight ||
                    (src.colorType() != allowedColorType) || flip;
-
-    if (convert || !src.ownsPixels()) {
+    bool shouldMap = caps->mapBufferFlags() & GrCaps::kCanMap_MapFlag;
+    sk_sp<GrGpuBuffer> buffer;
+    if (convert || shouldMap) {
         GrImageInfo tmpInfo(allowedColorType,
                             this->colorInfo().alphaType(),
                             this->colorInfo().refColorSpace(),
                             src.dimensions());
-        GrPixmap tmp = GrPixmap::Allocate(tmpInfo);
-
-        SkAssertResult(GrConvertPixels(tmp, src, flip));
-
-        src = tmp;
-        pt.fY = flip ? dstSurface->height() - pt.fY - tmpInfo.height() : pt.fY;
+        GrPixmap tmp;
+        if (caps->mapBufferFlags() & GrCaps::kCanMap_MapFlag) {
+            size_t rb = tmpInfo.minRowBytes();
+            size_t size = tmpInfo.height()*rb;
+            buffer = dContext->priv().resourceProvider()->createBuffer(
+                    size,
+                    GrGpuBufferType::kXferCpuToGpu,
+                    kDynamic_GrAccessPattern);
+            void* map;
+            if (buffer && (map = buffer->map())) {
+                tmp = GrPixmap(tmpInfo, map, rb);
+            }
+        }
+        if (!tmp.hasPixels() && convert) {
+            tmp = GrPixmap::Allocate(tmpInfo);
+        }
+        if (tmp.hasPixels()) {
+            SkAssertResult(GrConvertPixels(tmp, src, flip));
+            src = tmp;
+            pt.fY = flip ? dstSurface->height() - pt.fY - tmpInfo.height() : pt.fY;
+            if (buffer) {
+                buffer->unmap();
+            }
+        }
     }
 
+    if (buffer) {
+        return dContext->priv().drawingManager()->newWritePixelsTask(
+                this->asSurfaceProxyRef(),
+                SkIRect::MakePtSize(pt, src.dimensions()),
+                src.colorType(),
+                dstColorType,
+                std::move(buffer),
+                0,
+                src.rowBytes());
+    }
     GrMipLevel level;
     level.fPixels = src.addr();
     level.fRowBytes = src.rowBytes();
-    return dContext->priv().drawingManager()->newWritePixelsTask(
+    bool result = dContext->priv().drawingManager()->newWritePixelsTask(
             this->asSurfaceProxyRef(),
             SkIRect::MakePtSize(pt, src.dimensions()),
             src.colorType(),
@@ -522,6 +551,10 @@ bool GrSurfaceContext::writePixels(GrDirectContext* dContext, GrPixmap src, SkIP
             &level,
             1,
             src.pixelStorage());
+    if (result && !src.ownsPixels()) {
+        dContext->priv().flushSurface(caps->preferVRAMUseOverFlushes() ? dstProxy : nullptr);
+    }
+    return result;
 }
 
 void GrSurfaceContext::asyncRescaleAndReadPixels(GrDirectContext* dContext,
