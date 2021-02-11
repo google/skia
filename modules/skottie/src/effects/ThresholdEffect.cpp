05c08512b962a58f10aed3cde1a62f1cf6a757e1
Florin Malita <fmalita@chromium.org>
2021-02-10 19:13:33 -0500

    [skottie] Threshold effect
    WIP
    
    Change-Id: I9d726b4526f2bbe584f11c77ee2e6829e5148956

---
+++ modules/skottie/src/effects/ThresholdEffect.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "modules/skottie/src/effects/Effects.h"
+
+#include "include/effects/SkRuntimeEffect.h"
+#include "modules/skottie/src/Adapter.h"
+#include "modules/skottie/src/SkottieJson.h"
+#include "modules/skottie/src/SkottieValue.h"
+
+#include <array>
+
+namespace skottie::internal {
+
+namespace  {
+
+} // namespace
+
+} // namespace skottie::internal
