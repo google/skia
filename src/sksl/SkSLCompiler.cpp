2f7f1d5a6c888b40f2ef691dd39a6e11c0a9cc46
John Stiles <johnstiles@google.com>
2021-02-11 11:43:09 -0500

    Fix use-after-free discovered by the fuzzer.
    The simplification which replaces unnecessarily nested constructors is
    creating a new constructor from whole cloth, obliterating the original
    constructor and its arguments. This can invalidate the definition map.
    For now, this is fixed by forcing a rescan when this optimization is
    applied.
    
    Change-Id: I8a46bccf3b6422282eaf95409cd4fa50b8ad3187
    Bug: oss-fuzz:29994

--- src/sksl/SkSLCompiler.cpp
+++ src/sksl/SkSLCompiler.cpp
@@ -945,15 +945,14 @@ void Compiler::simplifyExpression(DefinitionMap& definitions,
                         }
                         currBit <<= 1;
                     }
-                    auto optimized = std::unique_ptr<Expression>(
-                            new Constructor(c.fOffset, &c.type(), std::move(flattened)));
-                    // No fUsage change; no references have been added or removed anywhere.
+                    std::unique_ptr<Expression> replacement(new Constructor(c.fOffset, &c.type(),
+                                                                            std::move(flattened)));
+                    // We're replacing an expression with a cloned version; we'll need a rescan.
+                    // No fUsage change: `float2(float(x), y)` and `float2(x, y)` have equivalent
+                    // reference counts.
+                    try_replace_expression(&b, iter, &replacement);
                     optimizationContext->fUpdated = true;
-                    if (!try_replace_expression(&b, iter, &optimized)) {
-                        optimizationContext->fNeedsRescan = true;
-                        return;
-                    }
-                    SkASSERT((*iter)->isExpression());
+                    optimizationContext->fNeedsRescan = true;
                     break;
                 }
             }
