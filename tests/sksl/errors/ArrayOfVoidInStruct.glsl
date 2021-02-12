d89dd25ac21d7240f2f18615bfdccfa318053d66
John Stiles <johnstiles@google.com>
2021-02-12 16:45:16 -0500

    Fix assertion when arrays are double-declared.
    Multi-dimensional arrays aren't legal in GLSL/SkSL, so this should be
    caught and flagged as an error. The parser now verifies that a
    variable's type isn't an array-type before accepting a `[` token to
    open an array on the variable name.
    
    This CL also refactors the IR generator's `convertArraySize` method to
    make sure that various checks are made for all callers. Originally this
    restructuring was used to verify array multi-dimensionality, but that
    didn't detect errors inside struct declarations (which get no error
    checking inside the IR generator) so the IR generator updates no longer
    need to check the array dimensions.
    
    Bug: skia:11322
    Change-Id: Id33f4bdfb544019ddf995a8196c3c09cfe5a4525

---
+++ tests/sksl/errors/ArrayOfVoidInStruct.glsl
@@ -0,0 +1,6 @@
+### Compilation failed:
+
+error: 4: opaque type 'void' is not permitted in a struct
+error: 5: opaque type 'void' is not permitted in a struct
+error: 6: multi-dimensional arrays are not supported
+3 errors
