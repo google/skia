9615ee7e120cf82ac468b24eef307cb9e9d39525
Herb Derby <herb@google.com>
2021-02-18 10:42:35 -0500

    give GrAtlasTextOp::Geometry a proper dtor
    Change-Id: Ieca57234de90ba904207f07760f32fbfa2dd0aee

--- src/gpu/ops/GrAtlasTextOp.cpp
+++ src/gpu/ops/GrAtlasTextOp.cpp
@@ -86,15 +86,18 @@ auto GrAtlasTextOp::Geometry::Make(GrRecordingContext* rc,
                                    const SkMatrix& drawMatrix,
                                    SkPoint drawOrigin,
                                    SkIRect clipRect,
-                                   GrTextBlob* blob,
+                                   sk_sp<GrTextBlob> blob,
                                    const SkPMColor4f& color) -> Geometry* {
     auto arena = rc->priv().recordTimeAllocator();
-    return arena->make<Geometry>(subRun,
-                                 drawMatrix,
-                                 drawOrigin,
-                                 clipRect,
-                                 SkRef(blob),
-                                 color);
+    // Bypass the automatic dtor behavior in SkArenaAlloc. I'm leaving this up to the Op to run
+    // all geometry dtors for now.
+    void* geo = arena->makeBytesAlignedTo(sizeof(Geometry), alignof(Geometry));
+    return new (geo) Geometry{subRun,
+                              drawMatrix,
+                              drawOrigin,
+                              clipRect,
+                              std::move(blob),
+                              color};
 }
 
 
