cc75fa8f958baba33a346712f2c2be800ac0bbf4
Michael Ludwig <michaelludwig@google.com>
2021-02-18 13:47:31 -0500

    Remove deprecated merge image filter header
    Bug: skia:11230
    Change-Id: I50a9fb86a9054b96b3088b06f625dea418f53db8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/371962
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- src/ports/SkGlobalInitialization_default.cpp
+++ src/ports/SkGlobalInitialization_default.cpp
@@ -128,6 +128,7 @@
         SkRegisterLightingImageFilterFlattenables();
         SkRegisterMagnifierImageFilterFlattenable();
         SkRegisterMatrixConvolutionImageFilterFlattenable();
+        SkRegisterMergeImageFilterFlattenable();
         SK_REGISTER_FLATTENABLE(SkLocalMatrixImageFilter);
         SK_REGISTER_FLATTENABLE(SkMatrixImageFilter);
     }
