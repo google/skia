957aadefc8417786fa8b8ac70e8bd425082eba45
Brian Osman <brianosman@google.com>
2021-02-18 12:58:54 -0500

    Remove  GLSL memory qualifiers from SkSL
    These were all unused, and only implemented on one backend.
    
    Change-Id: Ibd2fcef1a971e6c1bd9da0784c5d852a60708484

--- tests/sksl/errors/BadModifiers.glsl
+++ tests/sksl/errors/BadModifiers.glsl
@@ -1,33 +1,23 @@
 ### Compilation failed:
 
-error: 3: 'const' is not permitted here
-error: 3: 'in' is not permitted here
-error: 3: 'out' is not permitted here
-error: 3: 'uniform' is not permitted here
-error: 3: 'flat' is not permitted here
-error: 3: 'noperspective' is not permitted here
-error: 3: 'readonly' is not permitted here
-error: 3: 'writeonly' is not permitted here
-error: 3: 'coherent' is not permitted here
-error: 3: 'volatile' is not permitted here
-error: 3: 'restrict' is not permitted here
-error: 3: 'buffer' is not permitted here
-error: 3: 'varying' is not permitted here
-error: 6: 'const' is not permitted here
-error: 6: 'uniform' is not permitted here
-error: 6: 'flat' is not permitted here
-error: 6: 'noperspective' is not permitted here
-error: 6: 'readonly' is not permitted here
-error: 6: 'writeonly' is not permitted here
-error: 6: 'coherent' is not permitted here
-error: 6: 'volatile' is not permitted here
-error: 6: 'restrict' is not permitted here
-error: 6: 'buffer' is not permitted here
+error: 1: 'const' is not permitted here
+error: 1: 'in' is not permitted here
+error: 1: 'out' is not permitted here
+error: 1: 'uniform' is not permitted here
+error: 1: 'flat' is not permitted here
+error: 1: 'noperspective' is not permitted here
+error: 1: 'buffer' is not permitted here
+error: 1: 'varying' is not permitted here
+error: 4: 'const' is not permitted here
+error: 4: 'uniform' is not permitted here
+error: 4: 'flat' is not permitted here
+error: 4: 'noperspective' is not permitted here
+error: 4: 'buffer' is not permitted here
+error: 4: 'sk_has_side_effects' is not permitted here
+error: 4: 'varying' is not permitted here
+error: 4: 'inline' is not permitted here
+error: 6: 'in uniform' variables only permitted within fragment processors
+error: 6: 'varying' is only permitted in runtime effects
 error: 6: 'sk_has_side_effects' is not permitted here
-error: 6: 'varying' is not permitted here
 error: 6: 'inline' is not permitted here
-error: 9: 'in uniform' variables only permitted within fragment processors
-error: 9: 'varying' is only permitted in runtime effects
-error: 9: 'sk_has_side_effects' is not permitted here
-error: 9: 'inline' is not permitted here
-30 errors
+20 errors
