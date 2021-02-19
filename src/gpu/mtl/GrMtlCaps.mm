788904b348c1125f57abbd72e2a383e1c5441835
Chris Dalton <csmartdalton@google.com>
2021-02-19 01:10:32 -0700

    Set vertexIDSupport in Metal
    Change-Id: I34f83087305dbf1bbe180f9d5b465f912e563587

--- src/gpu/mtl/GrMtlCaps.mm
+++ src/gpu/mtl/GrMtlCaps.mm
@@ -466,7 +466,7 @@ void GrMtlCaps::initShaderCaps() {
     shaderCaps->fDstReadInShaderSupport = shaderCaps->fFBFetchSupport;
 
     shaderCaps->fIntegerSupport = true;
-    shaderCaps->fVertexIDSupport = false;
+    shaderCaps->fVertexIDSupport = true;
 
     // Metal uses IEEE float and half floats so assuming those values here.
     shaderCaps->fFloatIs32Bits = true;
