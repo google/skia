06f65908eb3187082697c277e843011025537cd6
John Stiles <johnstiles@google.com>
2021-02-22 16:46:54 -0500

    Fix switch optimization pass.
    The optimizer now properly recognizes all types of exits from a switch
    statement. Break, continue and return are all potential exits and need
    to be considered when determining the exit path from the switch.
    
    Previously, dead code elimination was hiding the effects of this bug
    from us, but it meant that an optimized switch had the potential to
    generate lots of worthless IR nodes which then needed to be detected and
    eliminated by the CFG. In particular, this affected the enum form of
    blend, causing a catastrophic amount of extra work to be done.
    
    Change-Id: If857e38cadfc016884624ea4db25a273ad3dce5b
    Bug: skia:11352

--- tests/sksl/inliner/EnumsCanBeInlinedSafely.glsl
+++ tests/sksl/inliner/EnumsCanBeInlinedSafely.glsl
@@ -2,13 +2,6 @@
 out vec4 sk_FragColor;
 vec4 helper();
 void main() {
-    for (int _1_loop = 0;_1_loop < 1; _1_loop++) {
-        {
-            {
-                continue;
-            }
-        }
-    }
     sk_FragColor = vec4(0.5, 0.5, 0.5, 1.0);
 
 }
