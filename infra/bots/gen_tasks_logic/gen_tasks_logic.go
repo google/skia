a1f2b0eb1cfe0453724fd8629cd6c3273386fbd7
Weston Tracey <westont@google.com>
2021-02-22 14:02:53 -0500

    Revert "[infra] Use xcode 11.4.1 for iphone11 jobs."
    This reverts commit 5c78a54b42608d07659d70f7f5e830e68cba5fc1.
    
    Reason for revert: Trying to get iphone11s to happily test builds from xcode12.3.
    
    Original change's description:
    > [infra] Use xcode 11.4.1 for iphone11 jobs.
    >
    > In the short term this gives us iphone11 coverage again, but I still
    > need to sort out why xcode 12.3 builds won't work, since this adds
    > complexity and xcode version churn on builders.
    >
    > Bug: skia:11129
    > Change-Id: Ic477b26e1cffc1d3124832cf26ec391969a617cf
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/358516
    > Commit-Queue: Weston Tracey <westont@google.com>
    > Reviewed-by: Eric Boren <borenet@google.com>
    
    TBR=borenet@google.com,westont@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: skia:11129
    Change-Id: I12f43284b372088e3ac6598d9df818247be9baa7

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -573,6 +573,7 @@ func (b *taskBuilder) defaultSwarmDimensions() {
 		}
 		if b.parts["model"] == "iPhone11" {
 			d["os"] = "iOS-13.6"
+			d["id"] = "skia-rpi-001"
 		}
 		if b.parts["model"] == "iPadPro" {
 			d["os"] = "iOS-13.6"
