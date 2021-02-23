3cdc8b6044b3ef9b2ef4a4a44db66bd1ddadf29c
Chris Dalton <csmartdalton@google.com>
2021-02-22 17:27:28 -0700

    Speed up handling of "bowtie" joins in the hw tessellator
    Bowtie joins are only used on internal chops, and in that case, they
    are only needed on cusps. Rather than measuring the angle to see if we
    need the bowtie, we can just detect if the chop was at a cusp instead.
    
    Bug: chromium:1172543
    Change-Id: I5efe4df1fb10dd850b65e0f85415d7791e196cd3

--- src/gpu/tessellate/GrStrokeHardwareTessellator.cpp
+++ src/gpu/tessellate/GrStrokeHardwareTessellator.cpp
@@ -503,12 +503,26 @@ private:
                          SkPoint endPt) {
         if (prevJoinType == JoinType::kBowtie) {
             SkASSERT(fHasLastControlPoint);
-            // Bowties need to go in their own patch if they will have >1 segment. TODO: Investigate
-            // if an optimization like "x < fCosRadiansPerSegment" would be worth it.
+            // Bowtie joins are only used on internal chops, and internal chops almost always have
+            // continuous tangent angles (i.e., the ending tangent of the first chop and the
+            // beginning tangent of the second both point in the same direction). The tangents will
+            // only ever not point in the same direction if we chopped at a cusp point, so that's
+            // the only time we actually need a bowtie.
             SkPoint nextControlPoint = (p[1] == p[0]) ? p[2] : p[1];
-            float rotation = SkMeasureAngleBetweenVectors(p[0] - fLastControlPoint,
-                                                          nextControlPoint - p[0]);
-            if (rotation * fTolerances.fNumRadialSegmentsPerRadian > 1) {
+            SkVector a = p[0] - fLastControlPoint;
+            SkVector b = nextControlPoint - p[0];
+            float ab_cosTheta = a.dot(b);
+            float ab_pow2 = a.dot(a) * b.dot(b);
+            // This will only be true if cosTheta * abs(cosTheta) !~= 1, meaning the tangents don't
+            // point in the same direction:
+            //
+            //          1 !~= cosTheta * abs(cosTheta)
+            //     (ab)^2 !~= (ab)^2 * cosTheta * abs(cosTheta)
+            //     (ab)^2 !~= (ab * cosTheta) * (ab * abs(cosTheta))
+            //     (ab)^2 !~= (ab * cosTheta) * abs(ab * cosTheta)  [ab is always positive]
+            //
+            if (!SkScalarNearlyEqual(ab_pow2, ab_cosTheta * fabsf(ab_cosTheta),
+                                     ab_pow2 * SK_ScalarNearlyZero)) {
                 this->internalJoinTo(prevJoinType, p[0], nextControlPoint);
                 fLastControlPoint = p[0];  // Disables the join section of this patch.
                 prevJoinFitsInPatch = true;
