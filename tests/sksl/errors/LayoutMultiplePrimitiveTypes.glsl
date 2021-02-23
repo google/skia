c2c03bde5f41fa24175160a04bda85ddcd52ea84
Brian Osman <brianosman@google.com>
2021-02-23 12:54:22 -0500

    Add flags for all layout qualifiers, check for duplicates
    Now, even if a qualifier has a default value, we will know that it
    appeared in the text. We can use that to check for redundant qualifiers
    (as is being done here), and in the IR generator to prevent any use of
    certain qualifiers, depending on context. (eg, runtime effects, wrong
    shader stage, on a parameter declaration, etc.)
    
    Bug: skia:11301
    Change-Id: I2cd6ad35c2b4c4d6f87ade97e80aea84dc16ee4b

---
+++ tests/sksl/errors/LayoutMultiplePrimitiveTypes.glsl
@@ -0,0 +1,8 @@
+### Compilation failed:
+
+error: 3: more than one primitive type layout qualifier
+error: 4: more than one primitive type layout qualifier
+error: 5: more than one primitive type layout qualifier
+error: 6: more than one primitive type layout qualifier
+error: 7: more than one primitive type layout qualifier
+5 errors
