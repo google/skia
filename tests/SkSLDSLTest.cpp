9d9674fd2356256ac33c57df08d25a56b606853f
John Stiles <johnstiles@google.com>
2021-02-23 13:38:01 -0500

    Optimize switch statements in SwitchStatement::Make.
    Change-Id: I90920f41bc386dfa7a980ae7510f6681231a5120

--- tests/SkSLDSLTest.cpp
+++ tests/SkSLDSLTest.cpp
@@ -39,7 +39,8 @@ public:
     }
 
     ~ExpectError() override {
-        REPORTER_ASSERT(fReporter, !fMsg);
+        REPORTER_ASSERT(fReporter, !fMsg,
+                        "Error mismatch: expected:\n%sbut no error occurred\n", fMsg);
         SetErrorHandler(nullptr);
     }
 
@@ -1116,25 +1117,26 @@ DEF_GPUTEST_FOR_MOCK_CONTEXT(DSLSwitch, r, ctxInfo) {
 
     Var a(kFloat, "a"), b(kInt, "b");
 
-    Statement x = Switch(5,
+    Statement x = Switch(b,
         Case(0, a = 0, Break()),
         Case(1, a = 1, Continue()),
         Case(2, a = 2  /*Fallthrough*/),
         Default(Discard())
     );
     EXPECT_EQUAL(x, R"(
-        switch (5) {
+        switch (b) {
             case 0: (a = 0.0); break;
             case 1: (a = 1.0); continue;
             case 2: (a = 2.0);
             default: discard;
         }
     )");
-    Statement y = Switch(b);
-    EXPECT_EQUAL(y, "switch (b) {}");
 
-    Statement z = Switch(b, Default(), Case(0), Case(1));
-    EXPECT_EQUAL(z, "switch (b) { default: case 0: case 1: }");
+    EXPECT_EQUAL(Switch(b),
+                "switch (b) {}");
+
+    EXPECT_EQUAL(Switch(b, Default(), Case(0), Case(1)),
+                "switch (b) { default: case 0: case 1: }");
 
     {
         ExpectError error(r, "error: duplicate case value '0'\n");
--- tests/sksl/shared/Discard.metal
+++ tests/sksl/shared/Discard.metal
@@ -1,16 +1,4 @@
-#include <metal_stdlib>
-#include <simd/simd.h>
-using namespace metal;
-struct Inputs {
-};
-struct Outputs {
-    float4 sk_FragColor [[color(0)]];
-};
-fragment Outputs fragmentMain(Inputs _in [[stage_in]], bool _frontFacing [[front_facing]], float4 _fragCoord [[position]]) {
-    Outputs _out;
-    (void)_out;
-    {
-        discard_fragment();
-    }
-    return _out;
-}
+### Compilation failed:
+
+error: 7: unreachable
+1 error
