60d8746494a33fec5f2835ddaa9f643385793334
Mike Klein <mtklein@google.com>
2021-02-24 08:29:14 -0600

    retry fast adb push
    This time, we create a temporary host copy to resolve symlinks, then
    push the copy.  And might as well try --sync while I'm here.
    
    I've changed the process of pushing packages to ignore version files,
    hoping the brute ADB push speed is fast enough.  I'm still deleting the
    version file, which should mean any bots running code at head (or older
    than head if we land this) will still be able to make sense of the state
    we leave behind.
    
    Change-Id: I84de90f13177393e4d8a599ef880545538b6af81

--- infra/bots/recipe_modules/flavor/examples/full.py
+++ infra/bots/recipe_modules/flavor/examples/full.py
@@ -5,6 +5,7 @@
 
 DEPS = [
   'flavor',
+  'recipe_engine/path',
   'recipe_engine/platform',
   'recipe_engine/properties',
   'recipe_engine/raw_io',
@@ -157,14 +158,15 @@ def GenTests(api):
   )
 
   yield (
-      api.test('failed_read_version') +
+      api.test('resolve_cipd_symlinks') +
       api.properties(buildername=builder,
                      repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]') +
-      api.step_data('read /sdcard/revenge_of_the_skiabot/SK_IMAGE_VERSION',
-                    retcode=1)
+      api.path.exists(
+          api.path['start_dir'].join('skia', 'resources', '.cipd')
+      )
   )
 
   yield (
