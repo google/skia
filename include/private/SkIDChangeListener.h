abda98367b1c8433a37a1b720e582ff14be0be0e
Adlai Holler <adlai@google.com>
2021-02-26 10:04:53 -0500

    Remove bug-prone SkIDChangeListener singleThreaded arg
    A given object may be unique, but its owner may not be unique
    and another thread may ref the object via the owner at any time.
    
    This happens e.g. when sharing SkPictures between threads.
    
    Bug: skia:10286
    Change-Id: I51b5239338a81aaa4d67db05f01f2c7c24182096

--- include/private/SkIDChangeListener.h
+++ include/private/SkIDChangeListener.h
@@ -49,7 +49,7 @@ public:
          * Add a new listener to the list. It must not already be deregistered. Also clears out
          * previously deregistered listeners.
          */
-        void add(sk_sp<SkIDChangeListener> listener, bool singleThreaded = false);
+        void add(sk_sp<SkIDChangeListener> listener);
 
         /**
          * The number of registered listeners (including deregisterd listeners that are yet-to-be
@@ -58,10 +58,10 @@ public:
         int count();
 
         /** Calls changed() on all listeners that haven't been deregistered and resets the list. */
-        void changed(bool singleThreaded = false);
+        void changed();
 
         /** Resets without calling changed() on the listeners. */
-        void reset(bool singleThreaded = false);
+        void reset();
 
     private:
         SkMutex fMutex;
