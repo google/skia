08748ec64421926d9e5c0a7500f7dc241b30b0ef
John Stiles <johnstiles@google.com>
2021-02-25 20:49:26 -0500

    Convert IRGenerator::convertDo to DoStatement::Make.
    We currently don't have any optimizations for do-statements so the
    primary benefit is moving code out of IRGenerator.
    
    Change-Id: Ibc4d1ecd87ebef572e742dfdb15821cf4ac0f047
    Bug: skia:11342

---
+++ src/sksl/ir/SkSLDoStatement.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/sksl/SkSLContext.h"
+#include "src/sksl/SkSLProgramSettings.h"
+#include "src/sksl/ir/SkSLDoStatement.h"
+
+namespace SkSL {
+
+std::unique_ptr<Statement> DoStatement::Make(const Context& context,
+                                             std::unique_ptr<Statement> stmt,
+                                             std::unique_ptr<Expression> test) {
+    if (context.fConfig->strictES2Mode()) {
+        context.fErrors.error(stmt->fOffset, "do-while loops are not supported");
+        return nullptr;
+    }
+    test = context.fTypes.fBool->coerceExpression(std::move(test), context);
+    if (!test) {
+        return nullptr;
+    }
+    return std::make_unique<DoStatement>(stmt->fOffset, std::move(stmt), std::move(test));
+}
+
+std::unique_ptr<Statement> DoStatement::clone() const {
+    return std::make_unique<DoStatement>(fOffset, this->statement()->clone(),
+                                         this->test()->clone());
+}
+
+String DoStatement::description() const {
+    return "do " + this->statement()->description() +
+           " while (" + this->test()->description() + ");";
+}
+
+}  // namespace SkSL
+
