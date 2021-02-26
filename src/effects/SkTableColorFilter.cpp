d8ce95e34c67bb54f34976d952d57bc67a8fc8ab
Mike Klein <mtklein@google.com>
2021-02-26 07:39:53 -0600

    always work from fBitmap in table CF
    No point holding two copies of our data, an SkBitmap and fStorage array.
    The GPU backend wants a bitmap, so make the CPU backend work from that.
    
    Updating the flattened format removed the last use of SkPackBits.
    
    Change-Id: I97f75ebe9324ec5eed37dfe6282e10d0fe08711f

--- src/effects/SkTableColorFilter.cpp
+++ src/effects/SkTableColorFilter.cpp
@@ -5,11 +5,9 @@
  * found in the LICENSE file.
  */
 
-#include "include/effects/SkTableColorFilter.h"
-
 #include "include/core/SkBitmap.h"
 #include "include/core/SkString.h"
-#include "include/core/SkUnPreMultiply.h"
+#include "include/effects/SkTableColorFilter.h"
 #include "include/private/SkColorData.h"
 #include "include/private/SkTo.h"
 #include "src/core/SkArenaAlloc.h"
@@ -20,63 +18,20 @@
 #include "src/core/SkVM.h"
 #include "src/core/SkWriteBuffer.h"
 
-static const uint8_t kIdentityTable[] = {
-    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-    0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
-    0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-    0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-    0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
-    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-    0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-    0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
-    0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
-    0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-    0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-    0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-    0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF
-};
-
 class SkTable_ColorFilter : public SkColorFilterBase {
 public:
     SkTable_ColorFilter(const uint8_t tableA[], const uint8_t tableR[],
                         const uint8_t tableG[], const uint8_t tableB[]) {
-        uint8_t* dst = fStorage;
-        if (tableA) {
-            memcpy(dst, tableA, 256);
-            dst += 256;
-            fFlags |= kA_Flag;
-        }
-        if (tableR) {
-            memcpy(dst, tableR, 256);
-            dst += 256;
-            fFlags |= kR_Flag;
-        }
-        if (tableG) {
-            memcpy(dst, tableG, 256);
-            dst += 256;
-            fFlags |= kG_Flag;
-        }
-        if (tableB) {
-            memcpy(dst, tableB, 256);
-            fFlags |= kB_Flag;
-        }
-
         fBitmap.allocPixels(SkImageInfo::MakeA8(256, 4));
-        uint8_t* bitmapPixels = fBitmap.getAddr8(0, 0);
-        int offset = 0;
-        static const unsigned kFlags[] = { kA_Flag, kR_Flag, kG_Flag, kB_Flag };
-
-        for (int x = 0; x < 4; ++x) {
-            if (!(fFlags & kFlags[x])) {
-                memcpy(bitmapPixels, kIdentityTable, sizeof(kIdentityTable));
-            } else {
-                memcpy(bitmapPixels, fStorage + offset, 256);
-                offset += 256;
-            }
-            bitmapPixels += 256;
+        uint8_t *a = fBitmap.getAddr8(0,0),
+                *r = fBitmap.getAddr8(0,1),
+                *g = fBitmap.getAddr8(0,2),
+                *b = fBitmap.getAddr8(0,3);
+        for (int i = 0; i < 256; i++) {
+            a[i] = tableA ? tableA[i] : i;
+            r[i] = tableR ? tableR[i] : i;
+            g[i] = tableG ? tableG[i] : i;
+            b[i] = tableB ? tableB[i] : i;
         }
         fBitmap.setImmutable();
     }
@@ -86,29 +41,17 @@ public:
                                    GrRecordingContext*, const GrColorInfo&) const override;
 #endif
 
-    enum {
-        kA_Flag = 1 << 0,
-        kR_Flag = 1 << 1,
-        kG_Flag = 1 << 2,
-        kB_Flag = 1 << 3,
-    };
-
     bool onAppendStages(const SkStageRec& rec, bool shaderIsOpaque) const override {
-        const uint8_t *r = kIdentityTable,
-                      *g = kIdentityTable,
-                      *b = kIdentityTable,
-                      *a = kIdentityTable;
-        const uint8_t* ptr = fStorage;
-        if (fFlags & kA_Flag) { a = ptr; ptr += 256; }
-        if (fFlags & kR_Flag) { r = ptr; ptr += 256; }
-        if (fFlags & kG_Flag) { g = ptr; ptr += 256; }
-        if (fFlags & kB_Flag) { b = ptr;             }
-
         SkRasterPipeline* p = rec.fPipeline;
         if (!shaderIsOpaque) {
             p->append(SkRasterPipeline::unpremul);
         }
 
+        // Notice fBitmap is in a,r,g,b order, but the byte_tables stage takes r,g,b,a pointers.
+        const uint8_t *a = fBitmap.getAddr8(0,0),
+                      *r = fBitmap.getAddr8(0,1),
+                      *g = fBitmap.getAddr8(0,2),
+                      *b = fBitmap.getAddr8(0,3);
         struct Tables { const uint8_t *r, *g, *b, *a; };
         p->append(SkRasterPipeline::byte_tables, rec.fAlloc->make<Tables>(Tables{r,g,b,a}));
 
@@ -130,105 +73,31 @@ public:
         };
 
         c = unpremul(c);
-
-        const uint8_t* ptr = fStorage;
-        if (fFlags & kA_Flag) {
-            c.a = apply_table_to_component(c.a, ptr);
-            ptr += 256;
-        }
-        if (fFlags & kR_Flag) {
-            c.r = apply_table_to_component(c.r, ptr);
-            ptr += 256;
-        }
-        if (fFlags & kG_Flag) {
-            c.g = apply_table_to_component(c.g, ptr);
-            ptr += 256;
-        }
-        if (fFlags & kB_Flag) {
-            c.b = apply_table_to_component(c.b, ptr);
-        }
+        c.a = apply_table_to_component(c.a, fBitmap.getAddr8(0,0));
+        c.r = apply_table_to_component(c.r, fBitmap.getAddr8(0,1));
+        c.g = apply_table_to_component(c.g, fBitmap.getAddr8(0,2));
+        c.b = apply_table_to_component(c.b, fBitmap.getAddr8(0,3));
         return premul(c);
     }
 
-protected:
-    void flatten(SkWriteBuffer&) const override;
-
-private:
+    void flatten(SkWriteBuffer& buffer) const override {
+        buffer.writeByteArray(fBitmap.getAddr8(0,0), 4*256);
+    }
     SK_FLATTENABLE_HOOKS(SkTable_ColorFilter)
 
+private:
     SkBitmap fBitmap;
-
-    uint8_t fStorage[256 * 4];
-    unsigned int fFlags = 0;
-
-    friend class SkTableColorFilter;
-
-    using INHERITED = SkColorFilter;
 };
-
-static const uint8_t kCountNibBits[] = {
-    0, 1, 1, 2,
-    1, 2, 2, 3,
-    1, 2, 2, 3,
-    2, 3, 3, 4
-};
-
-#include "src/effects/SkPackBits.h"
-
-void SkTable_ColorFilter::flatten(SkWriteBuffer& buffer) const {
-    uint8_t storage[5*256];
-    int count = kCountNibBits[fFlags & 0xF];
-    size_t size = SkPackBits::Pack8(fStorage, count * 256, storage, sizeof(storage));
-
-    buffer.write32(fFlags);
-    buffer.writeByteArray(storage, size);
-}
 
 sk_sp<SkFlattenable> SkTable_ColorFilter::CreateProc(SkReadBuffer& buffer) {
-    const int flags = buffer.read32();
-    const size_t count = kCountNibBits[flags & 0xF];
-    SkASSERT(count <= 4);
-
-    uint8_t packedStorage[5*256];
-    size_t packedSize = buffer.getArrayCount();
-    if (!buffer.validate(packedSize <= sizeof(packedStorage))) {
-        return nullptr;
+    uint8_t argb[4*256];
+    if (buffer.readByteArray(argb, sizeof(argb))) {
+        return SkTableColorFilter::MakeARGB(argb+0*256,
+                                            argb+1*256,
+                                            argb+2*256,
+                                            argb+3*256);
     }
-    if (!buffer.readByteArray(packedStorage, packedSize)) {
-        return nullptr;
-    }
-
-    uint8_t unpackedStorage[4*256];
-    size_t unpackedSize = SkPackBits::Unpack8(packedStorage, packedSize,
-                              unpackedStorage, sizeof(unpackedStorage));
-    // now check that we got the size we expected
-    if (!buffer.validate(unpackedSize == count*256)) {
-        return nullptr;
-    }
-
-    const uint8_t* a = nullptr;
-    const uint8_t* r = nullptr;
-    const uint8_t* g = nullptr;
-    const uint8_t* b = nullptr;
-    const uint8_t* ptr = unpackedStorage;
-
-    if (flags & kA_Flag) {
-        a = ptr;
-        ptr += 256;
-    }
-    if (flags & kR_Flag) {
-        r = ptr;
-        ptr += 256;
-    }
-    if (flags & kG_Flag) {
-        g = ptr;
-        ptr += 256;
-    }
-    if (flags & kB_Flag) {
-        b = ptr;
-        ptr += 256;
-    }
-    return SkTableColorFilter::MakeARGB(a, r, g, b);
+    return nullptr;
 }
 
 #if SK_SUPPORT_GPU
