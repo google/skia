9a6c3e805ad78738896bcb467fd6652143b081c7
Herb Derby <herb@google.com>
2021-03-01 14:19:36 -0500

    encapsulate GrSDFTOptions better
    The logic for picking what type of sub run to create is spread
    over several locations. Gather that altogether in drawingType().
    
    Have GrSDFT close over all the data needed to calculate the
    drawing type. This reduces plumbing to the processGlyphRun
    routine.
    
    The next CL should rename GrSDFTOptions to GrSDFTControl.
    
    Change-Id: I99e74c11af6d3b3d9919e54fe1e7286fcfbf1bfb

--- src/gpu/GrRecordingContextPriv.cpp
+++ src/gpu/GrRecordingContextPriv.cpp
@@ -7,8 +7,17 @@
 
 #include "src/gpu/GrRecordingContextPriv.h"
 
+#include "src/gpu/GrCaps.h"
 #include "src/gpu/GrDrawingManager.h"
 
 void GrRecordingContextPriv::moveRenderTasksToDDL(SkDeferredDisplayList* ddl) {
     fContext->drawingManager()->moveRenderTasksToDDL(ddl);
+}
+
+GrSDFTOptions GrRecordingContextPriv::getSDFTOptions(bool useSDFTForSmallText) const {
+    return GrSDFTOptions{
+            this->caps()->shaderCaps()->supportsDistanceFieldText(),
+            useSDFTForSmallText,
+            this->options().fMinDistanceFieldFontSize,
+            this->options().fGlyphsAsPathsFontSize};
 }
