91bca7c391e9d2e2c291d1b82b064911d154318a
John Stiles <johnstiles@google.com>
2021-03-02 14:59:41 -0500

    Add const to SkSL layout keys.
    This will allow @if statements to work properly with layout(key) values
    even with control-flow analysis disabled.
    
    Change-Id: Ib76b8c5553621815decda7fb0eefccb2c82df02b
    Bug: skia:11319

--- src/sksl/ir/SkSLLayout.h
+++ src/sksl/ir/SkSLLayout.h
@@ -243,7 +243,7 @@ struct Layout {
             result = "layout (" + result + ")";
         }
         if (fFlags & kKey_Flag) {
-            result += "/* key */";
+            result += "/* key */ const";
         }
         return result;
     }
