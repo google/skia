7a4aa62b4572068bc30d2396c2eebfd93f80606e
John Stiles <johnstiles@google.com>
2021-03-02 17:55:39 -0500

    Disable control-flow analysis in SkSL. (Performance experiment)
    This CL will be used to test for potential performance regressions (or
    improvements?) that we might incur by disabling this optimization pass.
    
    It will be reverted in ~1 day.
    
    Change-Id: I775cdb0c95df81fa25ebbd66e4ff01f64c660f68

--- tests/sksl/intrinsics/MinInt.glsl
+++ tests/sksl/intrinsics/MinInt.glsl
@@ -6,5 +6,7 @@ uniform vec4 colorRed;
 vec4 main() {
     ivec4 intValues = ivec4(testInputs * 100.0);
     ivec4 intGreen = ivec4(colorGreen * 100.0);
-    return ((((((min(intValues.x, 50) == -125 && min(intValues.xy, 50) == ivec2(-125, 0)) && min(intValues.xyz, 50) == ivec3(-125, 0, 50)) && min(intValues, 50) == ivec4(-125, 0, 50, 50)) && min(intValues.x, intGreen.x) == -125) && min(intValues.xy, intGreen.xy) == ivec2(-125, 0)) && min(intValues.xyz, intGreen.xyz) == ivec3(-125, 0, 0)) && min(intValues, intGreen) == ivec4(-125, 0, 0, 100) ? colorGreen : colorRed;
+    ivec4 expectedA = ivec4(-125, 0, 50, 50);
+    ivec4 expectedB = ivec4(-125, 0, 0, 100);
+    return ((((((min(intValues.x, 50) == expectedA.x && min(intValues.xy, 50) == expectedA.xy) && min(intValues.xyz, 50) == expectedA.xyz) && min(intValues, 50) == expectedA) && min(intValues.x, intGreen.x) == expectedB.x) && min(intValues.xy, intGreen.xy) == expectedB.xy) && min(intValues.xyz, intGreen.xyz) == expectedB.xyz) && min(intValues, intGreen) == expectedB ? colorGreen : colorRed;
 }
--- tests/sksl/metal/CastHalf4ToMat2x2.metal
+++ tests/sksl/metal/CastHalf4ToMat2x2.metal
@@ -6,9 +6,13 @@ struct Inputs {
 struct Outputs {
     float4 sk_FragColor [[color(0)]];
 };
+float2x2 float2x2_from_float4(float4 x0) {
+    return float2x2(float2(x0[0], x0[1]), float2(x0[2], x0[3]));
+}
 fragment Outputs fragmentMain(Inputs _in [[stage_in]], bool _frontFacing [[front_facing]], float4 _fragCoord [[position]]) {
     Outputs _out;
     (void)_out;
-    _out.sk_FragColor = float2x2(float2(1.0, 2.0), float2(3.0, 4.0))[0].xyxy;
+    float2x2 m1 = float2x2_from_float4(float4(1.0, 2.0, 3.0, 4.0));
+    _out.sk_FragColor = m1[0].xyxy;
     return _out;
 }
