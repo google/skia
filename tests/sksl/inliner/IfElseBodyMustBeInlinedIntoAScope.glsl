10945c74a288dc7eed102e5c3b0d44bdc537d412
John Stiles <johnstiles@google.com>
2021-03-02 14:39:30 -0500

    Migrate if-statement simplifyStatement logic to IfStatement::Make.
    This performs essentially the same simplifications as before, just at
    a different phase of compilation.
    
    Change-Id: Ia88df6857d4089962505cd1281798fda74fd0b02
    Bug: skia:11343, skia:11319

--- tests/sksl/inliner/IfElseBodyMustBeInlinedIntoAScope.glsl
+++ tests/sksl/inliner/IfElseBodyMustBeInlinedIntoAScope.glsl
@@ -3,8 +3,7 @@ out vec4 sk_FragColor;
 uniform vec4 color;
 void main() {
     vec4 c = color;
-    if (c.x >= 0.5) {
-    } else {
+    if (c.x >= 0.5) ; else {
         c = color + vec4(0.125);
     }
     sk_FragColor = c;
