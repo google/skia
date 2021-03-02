b62c074b0c80dfbff5231cba2fa20c499aa73672
Robert Phillips <robertphillips@google.com>
2021-03-02 14:01:59 -0500

    Add GrThreadSafePipelineBuilder_Base
    GrThreadSafePipelineBuilder_Base is the generic, base object the
    GrContextThreadSafeProxy will hold. Each backend will create a
    backend-specific version that is shared between the direct context
    and the (possibly many) utility contexts.
    
    Right now GrThreadSafePipelineBuilder_Base just holds the pipeline
    creation stats. Relatedly only GrGLGpu::ProgramCache and
    GrVkResourceProvider::PipelineStateCache currently derive from
    the new class (since they are the only backends that generate
    pipeline stats).
    
    Change-Id: I58f441c5c2b870bb5970c29cba19d1775864d52e

--- src/gpu/GrDirectContextPriv.cpp
+++ src/gpu/GrDirectContextPriv.cpp
@@ -9,6 +9,7 @@
 
 #include "include/gpu/GrContextThreadSafeProxy.h"
 #include "include/gpu/GrDirectContext.h"
+#include "include/private/GrThreadSafePipelineBuilder_Base.h"
 #include "src/gpu/GrAuditTrail.h"
 #include "src/gpu/GrContextThreadSafeProxyPriv.h"
 #include "src/gpu/GrDrawingManager.h"
@@ -112,14 +113,20 @@ void GrDirectContextPriv::resetGpuStats() const {
 
 void GrDirectContextPriv::dumpGpuStats(SkString* out) const {
 #if GR_GPU_STATS
-    return fContext->fGpu->stats()->dump(out);
+    fContext->fGpu->stats()->dump(out);
+    if (auto builder = fContext->fGpu->pipelineBuilder()) {
+        builder->stats()->dump(out);
+    }
 #endif
 }
 
 void GrDirectContextPriv::dumpGpuStatsKeyValuePairs(SkTArray<SkString>* keys,
                                                     SkTArray<double>* values) const {
 #if GR_GPU_STATS
-    return fContext->fGpu->stats()->dumpKeyValuePairs(keys, values);
+    fContext->fGpu->stats()->dumpKeyValuePairs(keys, values);
+    if (auto builder = fContext->fGpu->pipelineBuilder()) {
+        builder->stats()->dumpKeyValuePairs(keys, values);
+    }
 #endif
 }
 
