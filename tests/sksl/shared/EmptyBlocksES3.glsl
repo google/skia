bf09e32ea9910dd64cbf53e3d3e28ec7c65fd017
John Stiles <johnstiles@google.com>
2021-03-04 10:46:39 -0500

    Disallow unscoped for blocks which declare a variable.
    This should be legal, and we support this, but some versions of Android
    do not: http://screen/3bkQewHF3xUMn5v There's no point in allowing
    these shaders to exist; they can't compile on real-world clients, and
    these vardecls are borderline meaningless (as they aren't reachable by
    any other statements).
    
    Change-Id: Ie1351933c90caee9124eeab8983364ec030b2653

--- tests/sksl/shared/EmptyBlocksES3.glsl
+++ tests/sksl/shared/EmptyBlocksES3.glsl
@@ -2,10 +2,12 @@
 out vec4 sk_FragColor;
 vec4 main() {
     vec4 color = vec4(0.0);
-    for (int counter = 0;counter < 10; ++counter) ;
     for (int counter = 0;counter < 10; ++counter) {
     }
-    for (int counter = 0;counter < 10; ++counter) ;
+    for (int counter = 0;counter < 10; ++counter) {
+    }
+    for (int counter = 0;counter < 10; ++counter) {
+    }
     if (sqrt(1.0) == 1.0) color.y = 1.0;
     if (sqrt(1.0) == 2.0) ; else color.w = 1.0;
     while (sqrt(1.0) == 2.0) ;
