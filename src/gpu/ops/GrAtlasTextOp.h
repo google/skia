39bfbc81557759f93ad25ddec178f396fd85c65d
Herb Derby <herb@google.com>
2021-03-04 16:06:31 -0500

    add noexcept to GrAtlasTextOp delete
    Change-Id: Icb2d8ce9ac75454b89342aa737d563cf45eaba3e

--- src/gpu/ops/GrAtlasTextOp.h
+++ src/gpu/ops/GrAtlasTextOp.h
@@ -13,7 +13,7 @@
 #include "src/gpu/ops/GrMeshDrawOp.h"
 #include "src/gpu/text/GrTextBlob.h"
 
-#if !defined(SK_BUILD_FOR_IOS) || \
+#if !defined(SK_BUILD_FOR_FLUTTER) && !defined(SK_BUILD_FOR_IOS) || \
         (defined(__IPHONE_9_0) && __IPHONE_OS_VERSION_MIN_REQUIRED > __IPHONE_9_0)
     #define GR_HAS_THREAD_LOCAL
 #endif
@@ -32,7 +32,7 @@ public:
 
 #if !defined(GR_OP_ALLOCATE_USE_POOL) && defined(GR_HAS_THREAD_LOCAL)
     void* operator new(size_t s);
-    void operator delete(void* b);
+    void operator delete(void* b) noexcept;
 #endif
 
     static const int kVerticesPerGlyph = GrAtlasSubRun::kVerticesPerGlyph;
