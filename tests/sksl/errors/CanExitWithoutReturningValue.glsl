1b71b0f1c9d4c043ee9ef3ea978277da867b20cd
John Stiles <johnstiles@google.com>
2021-03-04 12:22:22 -0500

    Detect functions that fail to return a value, without using CFG.
    This check now runs at function finalization time, before constant
    propagation has occurred; this affected the "DeadIfStatement" test.
    
    Our detection isn't smart enough to realize that a loop will run zero
    times, so it treats `for` and `while` loops as always running at least
    once. This isn't strictly correct, but it actually mirrors how the CFG
    implementation works anyway. The only downside is that we would not flag
    code like `for (i=0; i<0; ++i) { return x; }` as an error.
    
    Change-Id: I5e43a6ee3a3993045559f0fb0646d36112543a94
    Bug: skia:11377

--- tests/sksl/errors/CanExitWithoutReturningValue.glsl
+++ tests/sksl/errors/CanExitWithoutReturningValue.glsl
@@ -1,4 +1,16 @@
 ### Compilation failed:
 
-error: 4: function 'is_detected' exits without returning a value
-1 error
+error: 6: function 'if_only' can exit without returning a value
+error: 7: function 'return_on_if_but_not_else' can exit without returning a value
+error: 8: function 'return_on_else_but_not_if' can exit without returning a value
+error: 10: function 'for_with_conditional_return' can exit without returning a value
+error: 11: function 'for_with_conditional_break' can exit without returning a value
+error: 12: function 'for_with_conditional_continue' can exit without returning a value
+error: 14: function 'switch_empty' can exit without returning a value
+error: 17: function 'switch_with_no_default' can exit without returning a value
+error: 23: function 'switch_with_break' can exit without returning a value
+error: 30: function 'switch_with_continue' can exit without returning a value
+error: 37: function 'switch_with_fallthrough_off_bottom' can exit without returning a value
+error: 44: function 'switch_with_conditional_break' can exit without returning a value
+error: 51: function 'switch_with_conditional_continue' can exit without returning a value
+13 errors
