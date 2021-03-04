bc68b8afecdc836d7993e6f41f402d530b0a680c
John Stiles <johnstiles@google.com>
2021-03-04 16:00:20 -0500

    Detect functions that fail to return a value, without using CFG.
    This check now runs at function finalization time, before constant
    propagation has occurred; this affected the "DeadIfStatement" test.
    
    Our detection isn't smart enough to realize that a loop will run zero
    times, so it treats `for` and `while` loops as always running at least
    once. This isn't strictly correct, but it actually mirrors how the CFG
    implementation works anyway. The only downside is that we would not flag
    code like `for (i=0; i<0; ++i) { return x; }` as an error.
    
    Change-Id: I5e43a6ee3a3993045559f0fb0646d36112543a94
    Bug: skia:11377

--- src/sksl/SkSLIRGenerator.cpp
+++ src/sksl/SkSLIRGenerator.cpp
@@ -931,22 +931,10 @@ void IRGenerator::finalizeFunction(FunctionDefinition& f) {
         ~Finalizer() override {
             SkASSERT(!fBreakableLevel);
             SkASSERT(!fContinuableLevel);
-
-            if (!fEncounteredReturnValue && this->functionReturnsValue()) {
-                // It's a non-void function, but it never created a result expression--that is, it
-                // never returned anything.
-                fIRGenerator->errorReporter().error(
-                        fFunction->fOffset,
-                        "function '" + fFunction->name() + "' exits without returning a value");
-            }
         }
 
         bool functionReturnsValue() const {
             return fFunction->returnType() != *fIRGenerator->fContext.fTypes.fVoid;
-        }
-
-        bool encounteredReturnValue() const {
-            return fEncounteredReturnValue;
         }
 
         bool visitStatement(Statement& stmt) override {
@@ -968,7 +956,6 @@ void IRGenerator::finalizeFunction(FunctionDefinition& f) {
                             // Coerce return expression to the function's return type.
                             returnStmt.setExpression(fIRGenerator->coerce(
                                     std::move(returnStmt.expression()), returnType));
-                            fEncounteredReturnValue = true;
                         } else {
                             // Returning something from a function with a void return type.
                             fIRGenerator->errorReporter().error(returnStmt.fOffset,
@@ -1023,12 +1010,17 @@ void IRGenerator::finalizeFunction(FunctionDefinition& f) {
         int fBreakableLevel = 0;
         // how deeply nested we are in continuable constructs (for, do).
         int fContinuableLevel = 0;
-        // have we found a return statement with a return value?
-        bool fEncounteredReturnValue = false;
 
         using INHERITED = ProgramWriter;
     };
-    Finalizer(this, &f.declaration()).visitStatement(*f.body());
+
+    Finalizer finalizer{this, &f.declaration()};
+    finalizer.visitStatement(*f.body());
+
+    if (finalizer.functionReturnsValue() && Analysis::CanExitWithoutReturningValue(f)) {
+        this->errorReporter().error(f.fOffset, "function '" + f.declaration().name() +
+                                               "' can exit without returning a value");
+    }
 }
 
 void IRGenerator::convertFunction(const ASTNode& f) {
--- tests/sksl/errors/CanExitWithoutReturningValue.glsl
+++ tests/sksl/errors/CanExitWithoutReturningValue.glsl
@@ -1,4 +1,20 @@
 ### Compilation failed:
 
-error: 4: function 'is_detected' exits without returning a value
-1 error
+error: 8: function 'if_only' can exit without returning a value
+error: 9: function 'return_on_if_but_not_else' can exit without returning a value
+error: 10: function 'return_on_else_but_not_if' can exit without returning a value
+error: 12: function 'for_with_conditional_return' can exit without returning a value
+error: 13: function 'for_with_conditional_break' can exit without returning a value
+error: 14: function 'for_with_conditional_continue' can exit without returning a value
+error: 16: function 'bad_if_else_chain' can exit without returning a value
+error: 29: function 'conditional_inside_do_loop' can exit without returning a value
+error: 35: function 'switch_empty' can exit without returning a value
+error: 39: function 'switch_with_no_default' can exit without returning a value
+error: 46: function 'switch_with_break' can exit without returning a value
+error: 54: function 'switch_with_continue' can exit without returning a value
+error: 62: function 'switch_with_fallthrough_off_bottom' can exit without returning a value
+error: 70: function 'switch_with_conditional_break' can exit without returning a value
+error: 78: function 'switch_with_conditional_continue' can exit without returning a value
+error: 86: function 'switch_with_conditional_if_then_return' can exit without returning a value
+error: 93: function 'switch_with_conditional_break_then_fallthrough' can exit without returning a value
+17 errors
