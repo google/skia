4e2fd029b1feeae9fc950aa7e2791940a90e836d
John Stiles <johnstiles@google.com>
2021-03-05 13:13:30 -0500

    Optimize ternary tests that check a const variable.
    This enables the ternary to be optimized away in code like:
       const bool SHINY = true;
       color = SHINY ? add_shine(x) : x; // to --> `color = add_shine(x);`
    
    Without constant propagation.
    
    Also, I added a unit test for ternary expression simplification; I
    wasn't able to find an existing one.
    
    When the optimization flag is disabled, this CL actually removes the
    optimization of `true ? x : y` --> `x` entirely; previously, this
    substitution would be made regardless of optimization settings.
    
    Change-Id: I93a8b9d4027902d35f8a19cfd6417170b209d056
    Bug: skia:11343

---
+++ tests/sksl/shared/TernaryExpression.glsl
@@ -0,0 +1,10 @@
+
+out vec4 sk_FragColor;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    bool ok = true;
+    ok = colorGreen.y == 1.0 ? true : false;
+    ok = ok && (colorGreen.x == 1.0 ? false : true);
+    return ok ? colorGreen : colorRed;
+}
