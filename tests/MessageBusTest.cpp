0408afc573a86ccf7541aa3fdcf255bca6afc432
Peng Huang <penghuang@chromium.org>
2021-03-06 21:16:15 -0500

    Fix GrGpuBuffer::onRelease() crash problem.
    The crash is because method GrGpuBuffer::onRelease() is called on wrong
    thread. SkMessageBus::Post(const Message& m) takes a const ref of the
    message, so there is a little possibility the GPU thread received and
    handled the message before SkMessageBus::Post() is returned. In that
    case, the caller of SkMessageBus::Post() (AsyncReadResult) still holds
    the last ref of the GrGpuBuffer, and then GrGpuBuffer() will just be
    released on the wrong thread.
    
    Bug: chromium:1185489
    Change-Id: I28665dbb1db7925d59ec574e9e26385e845ff4df
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/380696
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Peng Huang <penghuang@chromium.org>

--- tests/MessageBusTest.cpp
+++ tests/MessageBusTest.cpp
@@ -22,17 +22,19 @@ static inline bool SkShouldPostMessageToBus(const TestMessage&, uint32_t) {
 }
 
 }  // namespace
-DECLARE_SKMESSAGEBUS_MESSAGE(TestMessage)
+
+DECLARE_SKMESSAGEBUS_MESSAGE(TestMessage, true)
 
 DEF_TEST(MessageBus, r) {
+    using TestMessageBus = SkMessageBus<TestMessage>;
     // Register two inboxes to receive all TestMessages.
-    SkMessageBus<TestMessage>::Inbox inbox1, inbox2;
+    TestMessageBus::Inbox inbox1, inbox2;
 
     // Send two messages.
     const TestMessage m1 = { 5, 4.2f };
     const TestMessage m2 = { 6, 4.3f };
-    SkMessageBus<TestMessage>::Post(m1);
-    SkMessageBus<TestMessage>::Post(m2);
+    TestMessageBus::Post(std::move(m1));
+    TestMessageBus::Post(std::move(m2));
 
     // Make sure we got two.
     SkTArray<TestMessage> messages;
@@ -43,7 +45,7 @@ DEF_TEST(MessageBus, r) {
 
     // Send another; check we get just that one.
     const TestMessage m3 = { 1, 0.3f };
-    SkMessageBus<TestMessage>::Post(m3);
+    TestMessageBus::Post(m3);
     inbox1.poll(&messages);
     REPORTER_ASSERT(r, 1 == messages.count());
     REPORTER_ASSERT(r, 1 == messages[0].x);
@@ -62,6 +64,60 @@ DEF_TEST(MessageBus, r) {
 
 namespace {
 
+struct TestMessageRefCnt : public SkRefCnt {
+    TestMessageRefCnt(int i, float f) : x(i), y(f) {}
+
+    int x;
+    float y;
+};
+
+static inline bool SkShouldPostMessageToBus(const sk_sp<TestMessageRefCnt>&, uint32_t) {
+    return true;
+}
+
+}  // namespace
+
+DECLARE_SKMESSAGEBUS_MESSAGE(sk_sp<TestMessageRefCnt>, false)
+
+DEF_TEST(MessageBusSp, r) {
+    // Register two inboxes to receive all TestMessages.
+    using TestMessageBus = SkMessageBus<sk_sp<TestMessageRefCnt>, false>;
+    TestMessageBus::Inbox inbox1;
+
+    // Send two messages.
+    auto m1 = sk_make_sp<TestMessageRefCnt>(5, 4.2f);
+    auto m2 = sk_make_sp<TestMessageRefCnt>(6, 4.3f);
+    TestMessageBus::Post(std::move(m1));
+    TestMessageBus::Post(std::move(m2));
+
+    // Make sure we got two.
+    SkTArray<sk_sp<TestMessageRefCnt>> messages;
+    inbox1.poll(&messages);
+    REPORTER_ASSERT(r, 2 == messages.count());
+    REPORTER_ASSERT(r, messages[0]->unique());
+    REPORTER_ASSERT(r, messages[1]->unique());
+    REPORTER_ASSERT(r, 5 == messages[0]->x);
+    REPORTER_ASSERT(r, 6 == messages[1]->x);
+
+    // Send another; check we get just that one.
+    auto m3 = sk_make_sp<TestMessageRefCnt>(1, 0.3f);
+    TestMessageBus::Post(std::move(m3));
+    inbox1.poll(&messages);
+    REPORTER_ASSERT(r, 1 == messages.count());
+    REPORTER_ASSERT(r, messages[0]->unique());
+    REPORTER_ASSERT(r, 1 == messages[0]->x);
+
+    // Send another without std::move(), it should trigger SkASSERT().
+    // auto m4 = sk_make_sp<TestMessageRefCnt>(1, 0.3f);
+    // TestMessageBus::Post(m4);
+
+    // Nothing was sent since the last read.
+    inbox1.poll(&messages);
+    REPORTER_ASSERT(r, 0 == messages.count());
+}
+
+namespace {
+
 struct AddressedMessage {
     uint32_t fInboxID;
 };
@@ -76,15 +132,16 @@ static inline bool SkShouldPostMessageToBus(const AddressedMessage& msg, uint32_
 
 }  // namespace
 
-DECLARE_SKMESSAGEBUS_MESSAGE(AddressedMessage)
+DECLARE_SKMESSAGEBUS_MESSAGE(AddressedMessage, true)
 
 DEF_TEST(MessageBus_SkShouldPostMessageToBus, r) {
-    SkMessageBus<AddressedMessage>::Inbox inbox1(1), inbox2(2);
+    using AddressedMessageBus = SkMessageBus<AddressedMessage>;
+    AddressedMessageBus::Inbox inbox1(1), inbox2(2);
 
-    SkMessageBus<AddressedMessage>::Post({0});  // Should go to both
-    SkMessageBus<AddressedMessage>::Post({1});  // Should go to inbox1
-    SkMessageBus<AddressedMessage>::Post({2});  // Should go to inbox2
-    SkMessageBus<AddressedMessage>::Post({3});  // Should go nowhere
+    AddressedMessageBus::Post({0});  // Should go to both
+    AddressedMessageBus::Post({1});  // Should go to inbox1
+    AddressedMessageBus::Post({2});  // Should go to inbox2
+    AddressedMessageBus::Post({3});  // Should go nowhere
 
     SkTArray<AddressedMessage> messages;
     inbox1.poll(&messages);
