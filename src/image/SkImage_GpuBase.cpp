5c880d12f3ec098b53cf0032be44073ae9e363ce
Robert Phillips <robertphillips@google.com>
2021-03-12 17:17:57 -0500

    Use DirectContextID to control GrBackendTextureImageGenerator borrowing
    Bug: skia:11728
    Change-Id: I514f917577a4166c2834f72fc8c64ab85b259938

--- src/image/SkImage_GpuBase.cpp
+++ src/image/SkImage_GpuBase.cpp
@@ -259,7 +259,8 @@ sk_sp<GrTextureProxy> SkImage_GpuBase::MakePromiseImageLazyProxy(
             // In the future the GrSurface class hierarchy refactoring should eliminate this
             // difficulty by removing the virtual inheritance.
             if (fTexture) {
-                SkMessageBus<GrTextureFreedMessage, uint32_t>::Post({fTexture, fTextureContextID});
+                GrTextureFreedMessage msg { fTexture, fTextureContextID };
+                SkMessageBus<GrTextureFreedMessage, GrDirectContext::DirectContextID>::Post(msg);
             }
         }
 
@@ -313,7 +314,7 @@ sk_sp<GrTextureProxy> SkImage_GpuBase::MakePromiseImageLazyProxy(
             // our destructor.
             auto dContext = fTexture->getContext();
             dContext->priv().getResourceCache()->insertDelayedTextureUnref(fTexture);
-            fTextureContextID = dContext->priv().contextID();
+            fTextureContextID = dContext->directContextID();
             return {std::move(tex), kReleaseCallbackOnInstantiation, kKeySyncMode};
         }
 
@@ -321,7 +322,7 @@ sk_sp<GrTextureProxy> SkImage_GpuBase::MakePromiseImageLazyProxy(
         PromiseImageTextureFulfillProc fFulfillProc;
         sk_sp<GrRefCntedCallback> fReleaseHelper;
         GrTexture* fTexture = nullptr;
-        uint32_t fTextureContextID = SK_InvalidUniqueID;
+        GrDirectContext::DirectContextID fTextureContextID;
         bool fFulfillProcFailed = false;
     } callback(fulfillProc, std::move(releaseHelper));
 
