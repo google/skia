6d20babf719147cfb847cd6ddc3a2015693807a9
John Stiles <johnstiles@google.com>
2021-03-16 23:09:36 -0400

    Improve do-while test in CanExitWithoutReturningValue.
    This put the coverage for do-while loops on par with for loops.
    
    Change-Id: I53e0d733edd02a6a139792a8d74c68116453e5ff

--- tests/sksl/errors/CanExitWithoutReturningValue.glsl
+++ tests/sksl/errors/CanExitWithoutReturningValue.glsl
@@ -6,15 +6,17 @@ error: 8: function 'return_on_else_but_not_if' can exit without returning a valu
 error: 10: function 'for_with_conditional_return' can exit without returning a value
 error: 11: function 'for_with_conditional_break' can exit without returning a value
 error: 12: function 'for_with_conditional_continue' can exit without returning a value
-error: 14: function 'bad_if_else_chain' can exit without returning a value
-error: 27: function 'conditional_inside_do_loop' can exit without returning a value
-error: 33: function 'switch_empty' can exit without returning a value
-error: 37: function 'switch_with_no_default' can exit without returning a value
-error: 44: function 'switch_with_break' can exit without returning a value
-error: 52: function 'switch_with_continue' can exit without returning a value
-error: 60: function 'switch_with_fallthrough_off_bottom' can exit without returning a value
-error: 68: function 'switch_with_conditional_break' can exit without returning a value
-error: 76: function 'switch_with_conditional_continue' can exit without returning a value
-error: 84: function 'switch_with_conditional_if_then_return' can exit without returning a value
-error: 91: function 'switch_with_conditional_break_then_fallthrough' can exit without returning a value
-17 errors
+error: 14: function 'do_with_conditional_return' can exit without returning a value
+error: 15: function 'do_with_conditional_break' can exit without returning a value
+error: 16: function 'do_with_conditional_continue' can exit without returning a value
+error: 18: function 'bad_if_else_chain' can exit without returning a value
+error: 32: function 'switch_empty' can exit without returning a value
+error: 36: function 'switch_with_no_default' can exit without returning a value
+error: 43: function 'switch_with_break' can exit without returning a value
+error: 51: function 'switch_with_continue' can exit without returning a value
+error: 59: function 'switch_with_fallthrough_off_bottom' can exit without returning a value
+error: 67: function 'switch_with_conditional_break' can exit without returning a value
+error: 75: function 'switch_with_conditional_continue' can exit without returning a value
+error: 83: function 'switch_with_conditional_if_then_return' can exit without returning a value
+error: 90: function 'switch_with_conditional_break_then_fallthrough' can exit without returning a value
+19 errors
