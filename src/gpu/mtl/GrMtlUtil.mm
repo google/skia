abe75c7468756346d3827247be76d2c39abf70ca
Jim Van Verth <jvanverth@google.com>
2021-03-17 11:38:54 -0400

    Add Metal compile options
    Change-Id: Ica821dbfd5b30f9e0cceb1eed9443905987f292a

--- src/gpu/mtl/GrMtlUtil.mm
+++ src/gpu/mtl/GrMtlUtil.mm
@@ -96,13 +96,19 @@ id<MTLLibrary> GrCompileMtlShaderLibrary(const GrMtlGpu* gpu,
                                                    length:msl.size()
                                                  encoding:NSUTF8StringEncoding
                                              freeWhenDone:NO];
+    MTLCompileOptions* options = [[MTLCompileOptions alloc] init];
+    options.fastMathEnabled = true;
+    if (@available(macOS 11.0, iOS 13.0, *)) {
+        options.preserveInvariance = true;
+    }
+
     NSError* error = nil;
 #if defined(SK_BUILD_FOR_MAC)
     id<MTLLibrary> compiledLibrary = GrMtlNewLibraryWithSource(gpu->device(), nsSource,
-                                                               nil, &error);
+                                                               options, &error);
 #else
     id<MTLLibrary> compiledLibrary = [gpu->device() newLibraryWithSource:nsSource
-                                                                 options:nil
+                                                                 options:options
                                                                    error:&error];
 #endif
     if (!compiledLibrary) {
