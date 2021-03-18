59c4161b9fe3bd5e4ea40fb0088bf6acfc2720f3
Chris Dalton <csmartdalton@google.com>
2021-03-17 21:10:17 -0600

    Reland "Add tooling support for dmsaa"
    This is a reland of 188443be8d18b270a9f5810448d667e6ba009cb0
    
    Original change's description:
    > Add tooling support for dmsaa
    >
    > Adds a "fAlwaysAntiAlias" flag to GrContextOptions that can be set from
    > tooling code. When dmsaa is set, SkGpuDevice draws everything
    > antialiased. Adds new "gldmsaa" and "glesdmsaa" configs and creates bots
    > to run them.
    >
    > Bug: skia:11396
    > Change-Id: I165e89434b733f7b02312cea0e6649812528083b
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/384936
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    
    Bug: skia:11396
    Change-Id: Icb45097e0a34543dc577fa32f19a692e90643a35

--- tools/skpbench/skpbench.cpp
+++ tools/skpbench/skpbench.cpp
@@ -566,6 +566,7 @@ int main(int argc, char** argv) {
     // Create a context.
     GrContextOptions ctxOptions;
     SetCtxOptionsFromCommonFlags(&ctxOptions);
+    ctxOptions.fAlwaysAntialias = config->getUseDMSAA();
     sk_gpu_test::GrContextFactory factory(ctxOptions);
     sk_gpu_test::ContextInfo ctxInfo =
         factory.getContextInfo(config->getContextType(), config->getContextOverrides());
