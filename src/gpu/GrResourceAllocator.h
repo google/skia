a46c54687d1498ebc352595fa33f76ef07ef7c09
Adlai Holler <adlai@google.com>
2021-03-18 15:54:23 -0400

    Use GrUniqueKey has instead of SkGoodHash
    I think SkGoodHash(GrUniqueKey) is causing MSAN failures
    because the SkSTArray, if not full, contains uninitialized bytes.
    
    Plus, we already know the hash so let's use it.
    
    Bug: skia:10877
    Change-Id: Ia89d0c7566e723765b1e86f59cfb5a7ec8922b5c

--- src/gpu/GrResourceAllocator.h
+++ src/gpu/GrResourceAllocator.h
@@ -121,7 +121,11 @@ private:
     typedef SkTMultiMap<Register, GrScratchKey, FreePoolTraits> FreePoolMultiMap;
 
     typedef SkTHashMap<uint32_t, Interval*, GrCheapHash>    IntvlHash;
-    typedef SkTHashMap<GrUniqueKey, Register*>              UniqueKeyRegisterHash;
+
+    struct UniqueKeyHash {
+        uint32_t operator()(const GrUniqueKey& key) const { return key.hash(); }
+    };
+    typedef SkTHashMap<GrUniqueKey, Register*, UniqueKeyHash> UniqueKeyRegisterHash;
 
     // Each proxy – with some exceptions – is assigned a register. After all assignments are made,
     // another pass is performed to instantiate and assign actual surfaces to the proxies. Right
