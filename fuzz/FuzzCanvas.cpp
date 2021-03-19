f33b47abff1dbf32f8083fb0d524c431a1fddb10
Mike Reed <reed@google.com>
2021-03-19 16:00:52 -0400

    FilterQuality is no longer used
    Change-Id: I6dea9344d8a19010d6e22ee8d0cd2b795910a82c

--- fuzz/FuzzCanvas.cpp
+++ fuzz/FuzzCanvas.cpp
@@ -830,7 +830,6 @@ static void fuzz_paint(Fuzz* fuzz, SkPaint* paint, int depth) {
     paint->setDither(       make_fuzz_t<bool>(fuzz));
     paint->setColor(        make_fuzz_t<SkColor>(fuzz));
     paint->setBlendMode(    make_fuzz_enum_range<SkBlendMode>(fuzz, SkBlendMode::kLastMode));
-    SkPaintPriv::SetFQ(paint, make_fuzz_enum_range<SkFilterQuality>(fuzz, kLast_SkFilterQuality));
     paint->setStyle(        make_fuzz_enum_range<SkPaint::Style>(fuzz,
                                                  SkPaint::Style::kStrokeAndFill_Style));
     paint->setShader(       make_fuzz_shader(fuzz, depth - 1));
