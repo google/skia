1ea9e0e54e9b922e05768812d0dd2d9a2b8bd61b
Herb Derby <herb@google.com>
2021-03-19 11:04:06 -0400

    allow Cluster::Cluster to inline
    Move the ctor Cluster::Cluster into ParagraphImpl.cpp to allow
    inlining. This results in about a 5% speed up using means.
            min     median  mean    max
    before: 57.2µs  61.3µs  60.1µs  64.1µs
    after:  55.8µs  58.4µs  57.3µs  59.4µs
    
    Change-Id: Ie4cfcae9fde601ccf4a42aec69a853cc0bddb377

--- modules/skparagraph/src/ParagraphImpl.cpp
+++ modules/skparagraph/src/ParagraphImpl.cpp
@@ -303,6 +303,68 @@ bool ParagraphImpl::computeCodeUnitProperties() {
     return true;
 }
 
+static bool is_ascii_7bit_space(int c) {
+    SkASSERT(c >= 0 && c <= 127);
+
+    // Extracted from https://en.wikipedia.org/wiki/Whitespace_character
+    //
+    enum WS {
+        kHT    = 9,
+        kLF    = 10,
+        kVT    = 11,
+        kFF    = 12,
+        kCR    = 13,
+        kSP    = 32,    // too big to use as shift
+    };
+#define M(shift)    (1 << (shift))
+    constexpr uint32_t kSpaceMask = M(kHT) | M(kLF) | M(kVT) | M(kFF) | M(kCR);
+    // we check for Space (32) explicitly, since it is too large to shift
+    return (c == kSP) || (c <= 31 && (kSpaceMask & M(c)));
+#undef M
+}
+
+Cluster::Cluster(ParagraphImpl* owner,
+                 RunIndex runIndex,
+                 size_t start,
+                 size_t end,
+                 SkSpan<const char> text,
+                 SkScalar width,
+                 SkScalar height)
+        : fOwner(owner)
+        , fRunIndex(runIndex)
+        , fTextRange(text.begin() - fOwner->text().begin(), text.end() - fOwner->text().begin())
+        , fGraphemeRange(EMPTY_RANGE)
+        , fStart(start)
+        , fEnd(end)
+        , fWidth(width)
+        , fSpacing(0)
+        , fHeight(height)
+        , fHalfLetterSpacing(0.0) {
+    size_t whiteSpacesBreakLen = 0;
+    size_t intraWordBreakLen = 0;
+
+    const char* ch = text.begin();
+    if (text.end() - ch == 1 && *(unsigned char*)ch <= 0x7F) {
+        // I am not even sure it's worth it if we do not save a unicode call
+        if (is_ascii_7bit_space(*ch)) {
+            ++whiteSpacesBreakLen;
+        }
+    } else {
+        for (auto i = fTextRange.start; i < fTextRange.end; ++i) {
+            if (fOwner->codeUnitHasProperty(i, CodeUnitFlags::kPartOfWhiteSpaceBreak)) {
+                ++whiteSpacesBreakLen;
+            }
+            if (fOwner->codeUnitHasProperty(i, CodeUnitFlags::kPartOfIntraWordBreak)) {
+                ++intraWordBreakLen;
+            }
+        }
+    }
+
+    fIsWhiteSpaceBreak = whiteSpacesBreakLen == fTextRange.width();
+    fIsIntraWordBreak = intraWordBreakLen == fTextRange.width();
+    fIsHardBreak = fOwner->codeUnitHasProperty(fTextRange.end, CodeUnitFlags::kHardLineBreakBefore);
+}
+
 // Clusters in the order of the input text
 void ParagraphImpl::buildClusterTable() {
     int cluster_count = 1;
