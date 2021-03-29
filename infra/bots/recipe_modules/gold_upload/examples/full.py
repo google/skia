3167d8eb9286da7bb445d8472a5bca415af86f6a
Weston Tracey <westont@google.com>
2021-03-29 10:01:34 -0400

    [infra] Make gold_upload module for direct-from-test uploads.
    Bug: skia:11785
    Change-Id: I6358e83242483cde3c8a726111fe9ed07fb09bda

---
+++ infra/bots/recipe_modules/gold_upload/examples/full.py
@@ -0,0 +1,36 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+# Recipe which runs the Skia gsutils tests.
+
+
+DEPS = [
+  'gold_upload',
+  'recipe_engine/path',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+  'run',
+  'vars',
+]
+
+
+def RunSteps(api):
+  api.vars.setup()
+  api.gold_upload.upload()
+
+def GenTests(api):
+  yield (
+      api.test('upload_tests') +
+      api.properties(buildername='Test-Android-Clang-Pixel2XL-Some-GPU-arm64-Debug-All',
+                     repository='https://skia.googlesource.com/skia.git',
+                     gs_bucket='skia-infra-gm',
+                     patch_ref='89/456789/12',
+                     patch_set=7,
+                     patch_issue=1234,
+                     revision='abc123',
+                     path_config='kitchen',
+                     swarm_out_dir='[SWARM_OUT_DIR]')
+  )
