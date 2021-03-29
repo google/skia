1e1fcd8de94cebea86610c8ef3558e050702dfd3
Florin Malita <fmalita@chromium.org>
2021-03-29 13:38:38 -0400

    [svg] Fix handling of <rect> rx/ry
    Both rx/ry are optional and clamped to half width/height, respectively.
    
    Bug: skia:11802
    Change-Id: I7b1af7042fdcaa70c0c6b4bfb15d1cb8848e251e

--- modules/svg/src/SkSVGRect.cpp
+++ modules/svg/src/SkSVGRect.cpp
@@ -5,6 +5,8 @@
  * found in the LICENSE file.
  */
 
+#include <tuple>
+
 #include "include/core/SkCanvas.h"
 #include "include/core/SkRect.h"
 #include "modules/svg/include/SkSVGRect.h"
@@ -24,11 +26,39 @@ bool SkSVGRect::parseAndSetAttribute(const char* n, const char* v) {
 }
 
 SkRRect SkSVGRect::resolve(const SkSVGLengthContext& lctx) const {
-    const SkRect rect = lctx.resolveRect(fX, fY, fWidth, fHeight);
-    const SkScalar rx = lctx.resolve(fRx, SkSVGLengthContext::LengthType::kHorizontal);
-    const SkScalar ry = lctx.resolve(fRy, SkSVGLengthContext::LengthType::kVertical);
+    const auto rect = lctx.resolveRect(fX, fY, fWidth, fHeight);
 
-    return SkRRect::MakeRectXY(rect, rx ,ry);
+    // https://www.w3.org/TR/SVG11/shapes.html#RectElementRXAttribute:
+    //
+    //   - Let rx and ry be length values.
+    //   - If neither ‘rx’ nor ‘ry’ are properly specified, then set both rx and ry to 0.
+    //   - Otherwise, if a properly specified value is provided for ‘rx’, but not for ‘ry’,
+    //     then set both rx and ry to the value of ‘rx’.
+    //   - Otherwise, if a properly specified value is provided for ‘ry’, but not for ‘rx’,
+    //     then set both rx and ry to the value of ‘ry’.
+    //   - Otherwise, both ‘rx’ and ‘ry’ were specified properly. Set rx to the value of ‘rx’
+    //     and ry to the value of ‘ry’.
+    //   - If rx is greater than half of ‘width’, then set rx to half of ‘width’.
+    //   - If ry is greater than half of ‘height’, then set ry to half of ‘height’.
+    //   - The effective values of ‘rx’ and ‘ry’ are rx and ry, respectively.
+    //
+    auto radii = [this]() {
+        return fRx.isValid()
+                ? fRy.isValid()
+                    ? std::make_tuple(*fRx, *fRy)
+                    : std::make_tuple(*fRx, *fRx)
+                : fRy.isValid()
+                    ? std::make_tuple(*fRy, *fRy)
+                    : std::make_tuple(SkSVGLength(0), SkSVGLength(0));
+    };
+
+    const auto [ rxlen, rylen ] = radii();
+    const auto rx = std::min(lctx.resolve(rxlen, SkSVGLengthContext::LengthType::kHorizontal),
+                             rect.width() / 2),
+               ry = std::min(lctx.resolve(rylen, SkSVGLengthContext::LengthType::kVertical),
+                             rect.height() / 2);
+
+    return SkRRect::MakeRectXY(rect, rx, ry);
 }
 
 void SkSVGRect::onDraw(SkCanvas* canvas, const SkSVGLengthContext& lctx,
