6e7d2b3b720ec527f1e1d3e7d60d13dd840c07ae
Mike Reed <reed@google.com>
2021-03-29 16:38:01 -0400

    Add drawPatch to canvaskit
    Change-Id: I66214398f020ab6e824c5509babe5107f6b46d7e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/388837
    Auto-Submit: Mike Reed <reed@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Mike Reed <reed@google.com>

--- modules/canvaskit/interface.js
+++ modules/canvaskit/interface.js
@@ -613,6 +613,32 @@ CanvasKit.onRuntimeInitialized = function() {
     this._drawOval(oPtr, paint);
   };
 
+  CanvasKit.Canvas.prototype.drawPatch = function(cubics, colors, texs, mode, paint) {
+    if (cubics.length < 24) {
+        throw "Need 12 cubic points";
+    }
+    if (colors && colors.length < 4) {
+        throw "Need 4 colors";
+    }
+    if (texs && texs.length < 8) {
+        throw "Need 4 shader coordinates";
+    }
+
+    const cubics_ptr =          copy1dArray(cubics, 'HEAPF32');
+    const colors_ptr = colors ? copy1dArray(assureIntColors(colors), 'HEAPU32') : nullptr;
+    const texs_ptr   = texs   ? copy1dArray(texs,   'HEAPF32') : nullptr;
+    if (!mode) {
+        mode = CanvasKit.BlendMode.Modulate;
+    }
+
+    this._drawPatch(cubics_ptr, colors_ptr, texs_ptr, mode, paint);
+
+    freeArraysThatAreNotMallocedByUsers(texs_ptr,   texs);
+    freeArraysThatAreNotMallocedByUsers(colors_ptr, colors);
+    freeArraysThatAreNotMallocedByUsers(cubics_ptr, cubics);
+  };
+
+
   // points is a 1d array of length 2n representing n points where the even indices
   // will be treated as x coordinates and the odd indices will be treated as y coordinates.
   // Like other APIs, this accepts a malloced type array or malloc obj.
