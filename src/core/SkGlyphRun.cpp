4b153fe84d2975ae9b81fba1b7ee8adec081be5c
Herb Derby <herb@google.com>
2021-03-30 14:00:19 -0400

    remove unused functions from glyph run classes
    Change-Id: I7f49789a9409a2edad496fef2e80e2884e160f91

--- src/core/SkGlyphRun.cpp
+++ src/core/SkGlyphRun.cpp
@@ -65,15 +65,6 @@ bool SkGlyphRunList::anyRunsLCD() const {
     return false;
 }
 
-bool SkGlyphRunList::anyRunsSubpixelPositioned() const {
-    for (const auto& r : fGlyphRuns) {
-        if (r.font().isSubpixel()) {
-            return true;
-        }
-    }
-    return false;
-}
-
 bool SkGlyphRunList::allFontsFinite() const {
     for (const auto& r : fGlyphRuns) {
         if (!SkFontPriv::IsFinite(r.font())) {
@@ -189,15 +180,6 @@ void SkGlyphRunBuilder::simplifyTextBlobIgnoringRSXForm(const SkTextBlobRunItera
             break;
         }
         case SkTextBlobRunIterator::kRSXform_Positioning: break;
-    }
-}
-
-void SkGlyphRunBuilder::drawGlyphsWithPositions(const SkFont& font,
-                                            SkSpan<const SkGlyphID> glyphIDs, const SkPoint* pos) {
-    if (!glyphIDs.empty()) {
-        this->initialize(glyphIDs.size());
-        this->simplifyDrawPosText(font, glyphIDs, pos);
-        this->makeGlyphRunList(nullptr, SkPoint::Make(0, 0));
     }
 }
 
