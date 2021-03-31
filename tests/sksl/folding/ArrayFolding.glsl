3e64d74281a1988bf0e2bd2f85a5e0cdcd3343f4
John Stiles <johnstiles@google.com>
2021-03-31 10:18:45 -0400

    Add test for folding of array == and !=.
    We have all the moving parts necessary to support this, but do not
    actually attempt to fold these expressions today.
    
    Change-Id: I6730a9a2d1da9ac613ee58889d651f3ff65b1d2d

---
+++ tests/sksl/folding/ArrayFolding.glsl
@@ -0,0 +1,10 @@
+
+out vec4 sk_FragColor;
+uniform vec4 colorRed;
+uniform vec4 colorGreen;
+vec4 main() {
+    int _0_x[3] = int[3](1, 2, 3);
+    int _1_xx[3] = int[3](1, 2, 3);
+    int _2_y[3] = int[3](1, 2, 4);
+    return ((_0_x == _1_xx && !(_0_x != _1_xx)) && _0_x != _2_y) && !(_0_x == _2_y) ? colorGreen : colorRed;
+}
