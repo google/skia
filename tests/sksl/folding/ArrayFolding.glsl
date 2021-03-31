1e46e013caa0366aa5771f45807fb7840ff58c7f
John Stiles <johnstiles@google.com>
2021-03-31 11:07:17 -0400

    Add support for constant-folded array inequality checks.
    This isn't something I expect to occur often. However, we have always
    report that array-ctors with constant arguments are compile-time
    constants, and `compareConstant` is supposed to be implemented for all
    compile-time constants.
    
    It's OK if we don't want to land this change, but in that case we should
    probably leave a comment or something calling out the inconsistency, or
    maybe arrays shouldn't report that they are compile-time constants at
    all.
    
    Change-Id: I0bbfe2a3f78c29c14f69c3b6faca71067a0e45c8

--- tests/sksl/folding/ArrayFolding.glsl
+++ tests/sksl/folding/ArrayFolding.glsl
@@ -3,7 +3,5 @@ out vec4 sk_FragColor;
 uniform vec4 colorRed;
 uniform vec4 colorGreen;
 vec4 main() {
-    const int _0_x[3] = int[3](1, 2, 3);
-    const int _2_y[3] = int[3](1, 2, 4);
-    return _0_x != _2_y && !(_0_x == _2_y) ? colorGreen : colorRed;
+    return true ? colorGreen : colorRed;
 }
