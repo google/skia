aea59baa169a427b7a0f06480a9319de89cd7ba0
Robert Phillips <robertphillips@google.com>
2021-03-31 16:51:46 -0400

    Enforce that only the recordTimeArena is only available for DDLRecordingContexts
    Change-Id: If57aaa3067cfe441fd080892e6e5aa933251380d

--- src/core/SkDeferredDisplayList.cpp
+++ src/core/SkDeferredDisplayList.cpp
@@ -24,6 +24,7 @@ SkDeferredDisplayList::SkDeferredDisplayList(const SkSurfaceCharacterization& ch
                                              sk_sp<LazyProxyData> lazyProxyData)
         : fCharacterization(characterization)
 #if SK_SUPPORT_GPU
+        , fArenas(true)
         , fTargetProxy(std::move(targetProxy))
         , fLazyProxyData(std::move(lazyProxyData))
 #endif
