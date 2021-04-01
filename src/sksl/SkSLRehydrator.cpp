d7e39b41dd956169b3452eabd6347b2965fa2009
John Stiles <johnstiles@google.com>
2021-04-01 16:30:38 -0400

    Add rehydration support for ConstructorArray.
    This was accidentally overlooked during the initial migration.
    
    Change-Id: I8d50207ecfdc5228e5f2b34bf0ed1bee33b5d650

--- src/sksl/SkSLRehydrator.cpp
+++ src/sksl/SkSLRehydrator.cpp
@@ -16,6 +16,7 @@
 #include "src/sksl/ir/SkSLBinaryExpression.h"
 #include "src/sksl/ir/SkSLBreakStatement.h"
 #include "src/sksl/ir/SkSLConstructor.h"
+#include "src/sksl/ir/SkSLConstructorArray.h"
 #include "src/sksl/ir/SkSLConstructorDiagonalMatrix.h"
 #include "src/sksl/ir/SkSLContinueStatement.h"
 #include "src/sksl/ir/SkSLDiscardStatement.h"
@@ -457,6 +458,12 @@ std::unique_ptr<Expression> Rehydrator::expression() {
             auto ctor = Constructor::Convert(fContext, /*offset=*/-1, *type, std::move(args));
             SkASSERT(ctor);
             return ctor;
+        }
+        case Rehydrator::kConstructorArray_Command: {
+            const Type* type = this->type();
+            ExpressionArray args = this->expressionArray();
+            SkASSERT(args.size() == 1);
+            return ConstructorArray::Make(fContext, /*offset=*/-1, *type, std::move(args));
         }
         case Rehydrator::kConstructorDiagonalMatrix_Command: {
             const Type* type = this->type();
