62af3c0490605f0ad468eb91b09c8a94fd847a90
John Stiles <johnstiles@google.com>
2021-04-02 17:25:35 -0400

    Add scalar-cast
    Change-Id: Iff8477f3797c83059c823ca9287493b7f30db71b

--- src/sksl/SkSLGLSLCodeGenerator.h
+++ src/sksl/SkSLGLSLCodeGenerator.h
@@ -21,6 +21,7 @@
 #include "src/sksl/ir/SkSLBinaryExpression.h"
 #include "src/sksl/ir/SkSLBoolLiteral.h"
 #include "src/sksl/ir/SkSLConstructor.h"
+#include "src/sksl/ir/SkSLConstructorScalarCast.h"
 #include "src/sksl/ir/SkSLDoStatement.h"
 #include "src/sksl/ir/SkSLExtension.h"
 #include "src/sksl/ir/SkSLFieldAccess.h"
@@ -134,7 +135,7 @@ protected:
 
     virtual void writeFunctionCall(const FunctionCall& c);
 
-    void writeConstructor(const Constructor& c, Precedence parentPrecedence);
+    void writeConstructorAsNecessary(const AnyConstructor& c, Precedence parentPrecedence);
 
     void writeAnyConstructor(const AnyConstructor& c, Precedence parentPrecedence);
 
