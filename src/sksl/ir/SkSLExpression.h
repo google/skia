a55354039922fa9f66ed52bdc5529f271ca1a26b
John Stiles <johnstiles@google.com>
2021-04-03 00:07:13 -0400

    Represent vector-cast constructors with ConstructorVectorCast.
    Change-Id: I28c1f337f64d6f20fb86bc0f58e225af4bd7b26c

--- src/sksl/ir/SkSLExpression.h
+++ src/sksl/ir/SkSLExpression.h
@@ -35,6 +35,7 @@ public:
         kConstructorDiagonalMatrix,
         kConstructorScalarCast,
         kConstructorSplat,
+        kConstructorVectorCast,
         kDefined,
         kExternalFunctionCall,
         kExternalFunctionReference,
@@ -85,8 +86,8 @@ public:
     }
 
     bool isAnyConstructor() const {
-        static_assert((int)Kind::kConstructorSplat + 1 == (int)Kind::kDefined);
-        return this->kind() >= Kind::kConstructor && this->kind() <= Kind::kConstructorSplat;
+        static_assert((int)Kind::kConstructorVectorCast + 1 == (int)Kind::kDefined);
+        return this->kind() >= Kind::kConstructor && this->kind() <= Kind::kConstructorVectorCast;
     }
 
     /**
