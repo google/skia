63175e879abb1763cbd66bf5022cb71d1e7d6ef6
Robert Phillips <robertphillips@google.com>
2021-04-05 13:33:56 -0400

    fd
    Change-Id: I60bc781ccb69b5fa23398a2955cc04d939ffcc51

--- modules/canvaskit/compile_gm.sh
+++ modules/canvaskit/compile_gm.sh
@@ -129,6 +129,8 @@ echo "Compiling bitcode"
   ${GN_FONT} \
   skia_use_expat=true \
   skia_enable_ccpr=true \
+  skia_enable_nga=false \
+  skia_enable_oga=true \
   skia_enable_svg=true \
   skia_enable_skshaper=true \
   skia_enable_skparagraph=true \
