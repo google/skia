fafa0548e3e7fe70c933802b498de3119b70a45e
Kevin Lubick <kjlubick@google.com>
2021-04-05 15:50:09 -0400

    [canvaskit] Proof of Concept for Typescript-build structure
    This CL demonstrates the following
     - Interface code written in TS
     - Having support for modular builds (e.g. making Paragraph
       optional)
     - Avoiding monolithic interface files
     - Using eslint to enforce style and avoid errors.
     - Working debug and release build (using example.html)
    
    Change-Id: Ibe9a214d5897b09920cef01f6e95302f3cf30d5c

---
+++ experimental/tskit/bindings/memory.ts
@@ -0,0 +1,29 @@
+/// <reference path="embind.d.ts" />
+/// <reference path="public_api.d.ts" />
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace memory {
+  declare const Module: embind.EmbindModule;
+  export const nullptr = 0;
+
+  export const copy1dArray = (arr: any[] | public_api.TypedArray,
+    dest: Heaps, ptr?: number): number => {
+    if (!arr || !arr.length) {
+      return nullptr;
+    }
+    const bytesPerElement = Module[dest].BYTES_PER_ELEMENT;
+    if (!ptr) {
+      ptr = Module._malloc(arr.length * bytesPerElement);
+    }
+    Module[dest].set(arr, ptr / bytesPerElement);
+    return ptr;
+  };
+
+  export const freeIfNecessary = (ptr: number, arr: any[] | public_api.TypedArray): void => {
+    if (arr && !(arr as any)._ck) {
+      Module._free(ptr);
+    }
+  };
+
+  // Recommended over enums by Effective Typescript pg 197.
+  export type Heaps = 'HEAPF32' | 'HEAPU8' | 'HEAPU16' | 'HEAPU32' | 'HEAP8' | 'HEAP16' | 'HEAP32';
+}
