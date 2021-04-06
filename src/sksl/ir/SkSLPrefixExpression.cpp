a132cc50f773c967391125206e8e0dca21688132
John Stiles <johnstiles@google.com>
2021-04-06 17:09:02 -0400

    Represent vector/matrix composition with the same dedicated type.
    Change-Id: Ia2781c8a9dd3b4ba55ef93e33ac252eaeec844ac

--- src/sksl/ir/SkSLPrefixExpression.cpp
+++ src/sksl/ir/SkSLPrefixExpression.cpp
@@ -11,9 +11,9 @@
 #include "src/sksl/ir/SkSLBoolLiteral.h"
 #include "src/sksl/ir/SkSLConstructor.h"
 #include "src/sksl/ir/SkSLConstructorArray.h"
+#include "src/sksl/ir/SkSLConstructorComposite.h"
 #include "src/sksl/ir/SkSLConstructorDiagonalMatrix.h"
 #include "src/sksl/ir/SkSLConstructorSplat.h"
-#include "src/sksl/ir/SkSLConstructorVector.h"
 #include "src/sksl/ir/SkSLFloatLiteral.h"
 #include "src/sksl/ir/SkSLIntLiteral.h"
 
@@ -76,11 +76,11 @@ static std::unique_ptr<Expression> negate_operand(const Context& context,
             }
             break;
 
-        case Expression::Kind::kConstructorVector:
+        case Expression::Kind::kConstructorComposite:
             // Convert `-vecN(literal, ...)` into `vecN(-literal, ...)`.
             if (context.fConfig->fSettings.fOptimize && value->isCompileTimeConstant()) {
-                ConstructorVector& ctor = operand->as<ConstructorVector>();
-                return ConstructorVector::Make(
+                ConstructorComposite& ctor = operand->as<ConstructorComposite>();
+                return ConstructorComposite::Make(
                         context, ctor.fOffset, ctor.type(),
                         negate_operands(context, std::move(ctor.arguments())));
             }
