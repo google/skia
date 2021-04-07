acd310612b262042f340daac8b3db2cff162e371
Chris Dalton <csmartdalton@google.com>
2021-04-07 15:02:05 -0600

    Rename glrt::fRTFBOID -> fMultisampleFBOID
    Bug: skia:11396
    Change-Id: If519b52ec79de4b2abeac2c5658f766f1c7e8d9c

--- src/gpu/gl/GrGLRenderTarget.h
+++ src/gpu/gl/GrGLRenderTarget.h
@@ -19,17 +19,18 @@ class GrGLAttachment;
 
 class GrGLRenderTarget : public GrRenderTarget {
 public:
-    bool alwaysClearStencil() const override { return 0 == fRTFBOID; }
+    bool alwaysClearStencil() const override { return 0 == this->renderFBOID(); }
 
     // set fSingleSampleFBOID to this value to indicate that it is multisampled but
     // Gr doesn't know how to resolve it.
     enum { kUnresolvableFBOID = 0 };
 
     struct IDs {
-        GrGLuint                   fRTFBOID;
+        GrGLuint                   fMultisampleFBOID;
         GrBackendObjectOwnership   fRTFBOOwnership;
         GrGLuint                   fSingleSampleFBOID;
         GrGLuint                   fMSColorRenderbufferID;
+        int                        fTotalMemorySamplesPerPixel;
     };
 
     static sk_sp<GrGLRenderTarget> MakeWrapped(GrGLGpu*,
@@ -42,7 +43,9 @@ public:
     // The following two functions return the same ID when a texture/render target is not
     // multisampled, and different IDs when it is multisampled.
     // FBO ID used to render into
-    GrGLuint renderFBOID() const { return fRTFBOID; }
+    GrGLuint renderFBOID() const {
+        return (this->numSamples() > 1) ? fMultisampleFBOID : fSingleSampleFBOID;
+    }
     // FBO ID that has texture ID attached.
     GrGLuint singleSampleFBOID() const { return fSingleSampleFBOID; }
 
@@ -71,7 +74,7 @@ protected:
     void onAbandon() override;
     void onRelease() override;
 
-    int numSamplesOwnedPerPixel() const { return fNumSamplesOwnedPerPixel; }
+    int totalMemorySamplesPerPixel() const { return fTotalMemorySamplesPerPixel; }
 
 private:
     // Constructor for instances wrapping backend objects.
@@ -85,11 +88,7 @@ private:
 
     size_t onGpuMemorySize() const override;
 
-    int msaaSamples() const;
-    // The number total number of samples, including both MSAA and resolve texture samples.
-    int totalSamples() const;
-
-    GrGLuint    fRTFBOID;
+    GrGLuint    fMultisampleFBOID;
     GrGLuint    fSingleSampleFBOID;
     GrGLuint    fMSColorRenderbufferID;
     GrGLFormat  fRTFormat;
@@ -99,7 +98,7 @@ private:
     // The RenderTarget needs to be able to report its VRAM footprint even after abandon and
     // release have potentially zeroed out the IDs (e.g., so the cache can reset itself). Since
     // the IDs are just required for the computation in totalSamples we cache that result here.
-    int         fNumSamplesOwnedPerPixel;
+    int fTotalMemorySamplesPerPixel;
 
     using INHERITED = GrRenderTarget;
 };
