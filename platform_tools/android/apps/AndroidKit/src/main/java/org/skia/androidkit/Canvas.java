b74f9b2ee33abfa37f1a890c437c438994a499ff
Florin Malita <fmalita@chromium.org>
2021-04-09 09:48:15 -0400

    [ak] Bind canvas to native Bitmap
    Change-Id: I22ba54e65a9cdf498e97afefe8bcc6cd88db0c95

--- platform_tools/android/apps/AndroidKit/src/main/java/org/skia/androidkit/Canvas.java
+++ platform_tools/android/apps/AndroidKit/src/main/java/org/skia/androidkit/Canvas.java
@@ -4,8 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.util.Log;
 
-import java.lang.reflect.Method;
-
 public class Canvas {
     private long mNativeCanvasWrapper;
     private Bitmap mBitmap;
@@ -16,30 +14,30 @@ public class Canvas {
         Log.d(TAG, "loading lib");
         System.loadLibrary("androidkit");
     }
+
     public Canvas(Bitmap bitmap) {
         if (!bitmap.isMutable()) {
             throw new IllegalStateException("Immutable bitmap passed to Canvas constructor");
         }
-        // --------- ugly class reflection to get the @hide method -----------
-        Object bitmapProxy = null;
-        try {
-            Class c = Class.forName("android.graphics.Bitmap");
-            Method m = c.getMethod("getNativeInstance");
-            bitmapProxy = m.invoke(bitmap);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        mNativeCanvasWrapper = nInitRaster(Long.parseLong(bitmapProxy.toString()));
-        // --------------------------------------------------------------------
+
         mBitmap = bitmap;
+        mNativeCanvasWrapper = nCreateRaster(bitmap);
+    }
+
+    @Override
+    protected void finalize() throws Throwable
+    {
+        nFinalize(mNativeCanvasWrapper, mBitmap);
     }
 
     public void drawRect(float left, float right, float top, float bottom, Paint paint) {
-        //nDrawRect(mNativeCanvasWrapper, left, right, top, bottom, paint.getNativeInstance());
+        nDrawRect(mNativeCanvasWrapper, left, right, top, bottom, paint);
     }
 
-    private static native long nInitRaster(long bitmapHandle);
-    private static native long nDrawRect(long canvasProxy, float left, float right, float top, float bottom, long paintProxy);
+    private static native long nCreateRaster(Bitmap bitmap);
+    private static native void nFinalize(long canvasWrapper, Bitmap bitmap);
+
+    private static native void nDrawRect(long canvasWrapper,
+                                         float left, float right, float top, float bottom, Paint p);
 
 }
