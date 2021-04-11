0f7019723f53187fbd8e602009eb315d26559acc
John Stiles <johnstiles@google.com>
2021-04-11 10:13:42 -0400

    Optimize Block in response to Pinpoint findings.
    Pinpoint discovered some regressions in Block::Make.
    
    Block::Make attempts to identify Blocks that contain zero or one actual
    Statements; in that case it can avoid allocating a Block and just return
    the Statement. The original logic which did this check worked by
    removing the empty Statements from the array, then shrinking the array.
    For a large array of nested Statements, this could end up taking a
    nontrivial amount of time as it would scan every statement for
    emptiness.
    
    I've rewritten this logic to be more efficient; it now stops as soon as
    two non-empty Statements are found, and makes no attempt to eliminate
    empty statements. It's more lines of code, but much less work.
    
    If this doesn't resolve Pinpoint's woes we can remove this logic
    altogether, as its only value is to avoid an unnecessary allocation and
    generate slightly-smaller but overall equivalent IR.
    
    Change-Id: I2d18a0e09df266629d63b6796f40501d8610f756
    Bug: chromium:1187973, chromium:1187991, chromium:1188002, chromium:1188025
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/395417
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/sksl/ir/SkSLBlock.cpp
+++ src/sksl/ir/SkSLBlock.cpp
@@ -12,34 +12,48 @@
 
 namespace SkSL {
 
-static void eliminate_empty_statements(StatementArray* statements) {
-    // Remove all the statements which are empty.
-    std::unique_ptr<Statement>* iter = std::remove_if(
-            statements->begin(), statements->end(), [](const std::unique_ptr<Statement>& stmt) {
-                return stmt->isEmpty();
-            });
-
-    // Shrink the statement array to match.
-    statements->resize(std::distance(statements->begin(), iter));
-}
-
 std::unique_ptr<Statement> Block::MakeUnscoped(int offset, StatementArray statements) {
-    eliminate_empty_statements(&statements);
+    // If the Block is completely empty, synthesize a Nop.
     if (statements.empty()) {
         return Nop::Make();
     }
-    if (statements.size() == 1) {
-        return std::move(statements.front());
+
+    if (statements.size() > 1) {
+        // The statement array contains multiple statements, but some of those might be no-ops.
+        // If the statement array only contains one real statement, we can return that directly and
+        // avoid creating an additional Block node.
+        std::unique_ptr<Statement>* foundStatement = nullptr;
+        for (std::unique_ptr<Statement>& stmt : statements) {
+            if (!stmt->isEmpty()) {
+                if (!foundStatement) {
+                    // We found a single non-empty statement. Remember it and keep looking.
+                    foundStatement = &stmt;
+                    continue;
+                }
+                // We found more than one non-empty statement. We actually do need a Block.
+                return std::make_unique<Block>(offset, std::move(statements),
+                                               /*symbols=*/nullptr, /*isScope=*/false);
+            }
+        }
+
+        // The array wrapped one valid Statement. Avoid allocating a Block by returning it directly.
+        if (foundStatement) {
+            return std::move(*foundStatement);
+        }
+
+        // The statement array contained nothing but empty statements!
+        // In this case, we don't actually need to allocate a Block.
+        // We can just return one of those empty statements. Fall through to...
     }
-    return std::make_unique<Block>(offset, std::move(statements),
-                                   /*symbols=*/nullptr, /*isScope=*/false);
+
+    return std::move(statements.front());
 }
 
 std::unique_ptr<Block> Block::Make(int offset,
                                    StatementArray statements,
                                    std::shared_ptr<SymbolTable> symbols,
                                    bool isScope) {
-    // Nothing to optimize here--eliminate_empty_statements doesn't actually improve the generated
+    // Nothing to optimize here--eliminating empty statements doesn't actually improve the generated
     // code, and we promise to return a Block.
     return std::make_unique<Block>(offset, std::move(statements), std::move(symbols), isScope);
 }
