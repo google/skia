edc0dfbe4e3963a4d5771c575099f168c39caf9a
Florin Malita <fmalita@chromium.org>
2021-04-12 12:25:48 -0400

    [androidkit] Split Canvas backing store into Surface
    Add a Surface abstraction to manage the canvas backing store (equivalent
    to SkSurface).
    
    This allows relieving Canvas of buffer management duties - we can now
    focus solely on rendering APIs at this level.
    
    At the moment, only a Bitmap-backed surface is implemented -- but other
    native surface types will be added.
    
    Natively, a SkSurface owns its internal SkCanvas.  To avoid adding
    fragile scoping deps, the AndroidKit Canvas holds a ref to its Surface,
    ensuring it doesn't get GCed while the Canvas is still alive.
    
    Also relocate native code to 'src'.
    
    Change-Id: I46738472536cf24524428dbd36969f2b99d251e3

--- platform_tools/android/apps/androidkitdemo/src/main/java/org/skia/androidkitdemo1/MainActivity.java
+++ platform_tools/android/apps/androidkitdemo/src/main/java/org/skia/androidkitdemo1/MainActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import org.skia.androidkit.Canvas;
+import org.skia.androidkit.Surface;
 
 public class MainActivity extends Activity {
-    private static final String TAG = "ANDROIDKIT DEMO";
-
+    static {
+        System.loadLibrary("androidkit");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
         Bitmap bmp = Bitmap.createBitmap(200, 200, conf);
-        Canvas canvas = new Canvas(bmp);
+        Surface surface = new Surface(bmp);
         Paint p = new Paint();
         p.setColor(Color.RED);
-        canvas.drawRect(0, 0, 100, 100, p);
+        surface.getCanvas().drawRect(0, 0, 100, 100, p);
         ImageView image = findViewById(R.id.image);
         image.setImageBitmap(bmp);
     }
