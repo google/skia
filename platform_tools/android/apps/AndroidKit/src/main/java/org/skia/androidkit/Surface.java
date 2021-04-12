51a70ed341e4f5ac3e4a2b792e014e2ebfd77e30
Florin Malita <fmalita@chromium.org>
2021-04-12 12:25:48 -0400

    [androidkit] Split Canvas backing store into Surface
    Add a Surface abstraction to manage the canvas backing store (equivalent
    to SkSurface).
    
    This allows relieving Canvas of buffer management duties - we can now
    focus solely on rendering APIs at this level.
    
    At the moment, only a Bitmap-backed surface is implemented -- but other
    native surface types will be added.
    
    Also relocate native code to 'src'.
    
    Change-Id: I46738472536cf24524428dbd36969f2b99d251e3

---
+++ platform_tools/android/apps/AndroidKit/src/main/java/org/skia/androidkit/Surface.java
@@ -0,0 +1,55 @@
+package org.skia.androidkit;
+
+import android.graphics.Bitmap;
+import org.skia.androidkit.Canvas;
+
+public class Surface {
+    private long mNativeInstance;
+    private Canvas mCanvas;
+
+    /**
+     * Create a Surface backed by the provided Bitmap.
+     *
+     * The Bitmap must be mutable and its pixels are locked for the lifetime of the Surface.
+     */
+    public Surface(Bitmap bitmap) {
+        this(CreateBitmapInstance(bitmap));
+    }
+
+    /**
+     * The Canvas associated with this Surface.
+     */
+    public Canvas getCanvas() {
+        return mCanvas;
+    }
+
+    /**
+     * Releases any resources associated with this Surface.
+     */
+    public void release() {
+        nRelease(mNativeInstance);
+        mNativeInstance = 0;
+    }
+
+    @Override
+    protected void finalize() throws Throwable
+    {
+        release();
+    }
+
+    private Surface(long native_instance) {
+        mNativeInstance = native_instance;
+        mCanvas = new Canvas(this, nGetNativeCanvas(native_instance));
+    }
+
+    private static long CreateBitmapInstance(Bitmap bitmap) {
+        if (!bitmap.isMutable()) {
+            throw new IllegalStateException("Immutable bitmap passed to Surface constructor");
+        }
+        return nCreateBitmap(bitmap);
+    }
+
+    private static native long nCreateBitmap(Bitmap bitmap);
+    private static native void nRelease(long nativeInstance);
+    private static native long nGetNativeCanvas(long nativeInstance);
+}
