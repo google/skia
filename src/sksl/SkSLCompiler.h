15fd1e4c7304bc222746990cf20e92b074968b1b
John Stiles <johnstiles@google.com>
2021-04-13 11:49:25 -0400

    Fork CPPCodeGenerator into a DSL-based version.
    The long term goal is for the DSLCPPCodeGenerator to replace the
    CPPCodeGenerator entirely, but we will need both to coexist while DSL is
    still under development.
    
    Currently, the DSLCPPCodeGenerator is cloned from CPPCodeGenerator and
    emits almost exactly the same code (it adds a comment at the top to
    distinguish its output). Its output will change in followup CLs.
    
    This CL also allows skslc to recognize the `_dsl.cpp` output suffix and
    generate code using DSLCPPCodeGenerator instead of CPPCodeGenerator.
    This allows test DSL FPs to be created for inspection.
    
    Change-Id: If5136279c307ea53a9df3a292caa18344c1eb259
    Bug: skia:11854

--- src/sksl/SkSLCompiler.h
+++ src/sksl/SkSLCompiler.h
@@ -134,6 +134,8 @@ public:
 #if defined(SKSL_STANDALONE) || GR_TEST_UTILS
     bool toCPP(Program& program, String name, OutputStream& out);
 
+    bool toDSLCPP(Program& program, String name, OutputStream& out);
+
     bool toH(Program& program, String name, OutputStream& out);
 #endif
 
