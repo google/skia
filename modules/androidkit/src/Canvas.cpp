b403d542eeaff20d3e2b9435caa3cfb41f3e2304
Florin Malita <fmalita@chromium.org>
2021-04-13 10:05:27 -0400

    [androidkit] Register native methods explicitly
    Use RegisterNatives() to bind native methods, instead of relying on
    dynamic linker naming conventions.
    
    Change-Id: I602f2efe73e3b9c3c7dfaf7af2307dcfb4cd935e

--- modules/androidkit/src/Canvas.cpp
+++ modules/androidkit/src/Canvas.cpp
@@ -21,14 +21,22 @@ SkPaint skpaint(JNIEnv* env, jobject jpaint) {
     return paint;
 }
 
-}  // namespace
-
-extern "C" JNIEXPORT void
-JNICALL
-Java_org_skia_androidkit_Canvas_nDrawRect(JNIEnv* env, jobject, jlong native_instance,
-                                          jfloat left, jfloat top, jfloat right, jfloat bottom,
-                                          jobject paint) {
+void Canvas_DrawRect(JNIEnv* env, jobject, jlong native_instance,
+                     jfloat left, jfloat top, jfloat right, jfloat bottom, jobject paint) {
     if (auto* canvas = reinterpret_cast<SkCanvas*>(native_instance)) {
         canvas->drawRect(SkRect::MakeLTRB(left, top, right, bottom), skpaint(env, paint));
     }
+}
+
+}  // namespace
+
+int register_androidkit_Canvas(JNIEnv* env) {
+    static const JNINativeMethod methods[] = {
+        {"nDrawRect", "(JFFFFLandroid/graphics/Paint;)V", reinterpret_cast<void*>(Canvas_DrawRect)},
+    };
+
+    const auto clazz = env->FindClass("org/skia/androidkit/Canvas");
+    return clazz
+        ? env->RegisterNatives(clazz, methods, SK_ARRAY_COUNT(methods))
+        : JNI_ERR;
 }
