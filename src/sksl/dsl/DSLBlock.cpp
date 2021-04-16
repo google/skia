bbf8c6ed0e846ca9afc6158965c8e7fd046d3cca
John Stiles <johnstiles@google.com>
2021-04-16 12:22:00 -0400

    Incorporate top-level DSL blocks into fragment processors.
    A Block() at the top-level scope would previously disappear silently.
    
    Change-Id: Ic3bac058361658d2e5bd8de1e3718dedf4cef1f7
    Bug: skia:11854

--- src/sksl/dsl/DSLBlock.cpp
+++ src/sksl/dsl/DSLBlock.cpp
@@ -8,7 +8,12 @@
 #include "include/sksl/DSLBlock.h"
 
 #include "include/sksl/DSLStatement.h"
+#include "src/sksl/dsl/priv/DSLWriter.h"
 #include "src/sksl/ir/SkSLBlock.h"
+
+#if !defined(SKSL_STANDALONE) && SK_SUPPORT_GPU
+#include "src/gpu/glsl/GrGLSLFragmentShaderBuilder.h"
+#endif
 
 namespace SkSL {
 
@@ -16,6 +21,16 @@ namespace dsl {
 
 DSLBlock::DSLBlock(SkSL::StatementArray statements)
     : fStatements(std::move(statements)) {}
+
+DSLBlock::~DSLBlock() {
+#if !defined(SKSL_STANDALONE) && SK_SUPPORT_GPU
+    if (!fStatements.empty() && DSLWriter::InFragmentProcessor()) {
+        DSLWriter::CurrentEmitArgs()->fFragBuilder->codeAppend(this->release());
+        return;
+    }
+#endif
+    SkASSERTF(fStatements.empty(), "Block destroyed without being incorporated into program");
+}
 
 std::unique_ptr<SkSL::Statement> DSLBlock::release() {
     return std::make_unique<SkSL::Block>(/*offset=*/-1, std::move(fStatements));
