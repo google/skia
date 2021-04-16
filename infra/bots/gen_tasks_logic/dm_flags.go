69552d5b72178db44aa01c289b82e96599dbdfe0
Adlai Holler <adlai@google.com>
2021-04-16 13:32:28 -0400

    Lower GPU budget to 16MB on some desktop bots
    This bot also runs reduceOpsTaskSplitting, so this allows us
    to test the over-memory-budget fallback behavior.
    
    At 16MB my desktop falls back on the following SKPs: desk_carsvg,
    desk_twitter, desk_yahoosports, desk_linkedin, desk_facebook.
    
    Bug: skia:10877
    Change-Id: I60934e990581f762b72c077c6e8a40cc0c1d3d8c
    Cq-Include-Trybots: luci.skia.skia.primary:Test-Debian10-Clang-NUC7i5BNK-GPU-IntelIris640-x86_64-Debug-All-ASAN_Vulkan,Test-Debian10-Clang-NUC7i5BNK-GPU-IntelIris640-x86_64-Debug-All-DDL3_ASAN

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -340,13 +340,6 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 			skip("_ test _ GLBackendAllocationTest")
 		}
 
-		// skbug.com/9033 - these devices run out of memory on this test
-		// when opList splitting reduction is enabled
-		if b.gpu() && (b.model("Nexus7", "NVIDIA_Shield", "Nexus5x") ||
-			(b.os("Win10") && b.gpu("GTX660") && b.extraConfig("Vulkan"))) {
-			skip("_", "gm", "_", "savelayer_clipmask")
-		}
-
 		// skbug.com/9043 - these devices render this test incorrectly
 		// when opList splitting reduction is enabled
 		if b.gpu() && b.extraConfig("Vulkan") && (b.gpu("RadeonR9M470X", "RadeonHD7770")) {
@@ -439,6 +432,12 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 
 		if b.model(REDUCE_OPS_TASK_SPLITTING_MODELS...) {
 			args = append(args, "--reduceOpsTaskSplitting", "true")
+		}
+
+		// Test reduceOpsTaskSplitting fallback when over budget.
+		if b.Name == "Test-Debian10-Clang-NUC7i5BNK-GPU-IntelIris640-x86_64-Debug-All-ASAN_Vulkan" ||
+			b.Name == "Test-Debian10-Clang-NUC7i5BNK-GPU-IntelIris640-x86_64-Debug-All-DDL3_ASAN" {
+			args = append(args, "--gpuResourceCacheLimit", "16777216")
 		}
 
 		// Test rendering to wrapped dsts on a few bots
