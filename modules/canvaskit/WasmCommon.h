0dd1b13dfb5ea18ec1288ab01b9ae177e07572c5
Mike Reed <reed@google.com>
2021-04-16 15:48:53 -0400

    WIP: Expose proposed Shaper API to JS
    Requires: https://skia-review.googlesource.com/c/skia/+/392837
    
    Change-Id: I3b779b699fbcade7702049a83b98db8dfe86433d

--- modules/canvaskit/WasmCommon.h
+++ modules/canvaskit/WasmCommon.h
@@ -19,7 +19,26 @@ using JSArray = emscripten::val;
 using JSObject = emscripten::val;
 using JSString = emscripten::val;
 using SkPathOrNull = emscripten::val;
+using TypedArray = emscripten::val;
 using Uint8Array = emscripten::val;
+using Uint16Array = emscripten::val;
+using Uint32Array = emscripten::val;
 using Float32Array = emscripten::val;
+
+/**
+ *  Create a typed-array (in the JS heap) and initialize it with the provided
+ *  data (from the wasm heap). The caller is responsible for matching the type of data
+ *  with the specified arrayType.
+ *
+ *  TODO: can we specialize this on T and provide the correct string?
+ *          e.g. T==uint8_t --> "Uint8Array"
+ */
+template <typename T>
+TypedArray MakeTypedArray(int count, const T src[], const char arrayType[]) {
+    emscripten::val length = emscripten::val(count);
+    emscripten::val jarray = emscripten::val::global(arrayType).new_(count);
+    jarray.call<void>("set", val(typed_memory_view(count, src)));
+    return jarray;
+}
 
 #endif
