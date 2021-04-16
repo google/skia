b592e0f173ceeb06fd259e027c73e8c29f26b390
Robert Phillips <robertphillips@google.com>
2021-04-16 14:03:01 -0400

    Playground
    Change-Id: Ib73dd51f3f558aa6c6b98aa611da116d8906bf7f

---
+++ experimental/ddlbench/Cmds.h
@@ -0,0 +1,48 @@
+// Copyright 2021 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#ifndef Cmds_DEFINED
+#define Cmds_DEFINED
+
+class SkCanvas;
+class FakeCanvas;
+
+#include "include/core/SkColor.h"
+#include "include/core/SkRect.h"
+
+class Cmd {
+public:
+    Cmd(int id) : fID(id) {}
+
+    int id() const { return fID; }
+
+    virtual void execute(FakeCanvas*) const = 0;
+    virtual void execute(SkCanvas*) const = 0;
+    virtual void dump() const = 0;
+
+protected:
+    const int fID;
+
+private:
+};
+
+class RectCmd : public Cmd {
+public:
+    RectCmd(int id, SkRect r, SkColor c) : Cmd(id), fRect(r), fColor(c) {}
+
+    void execute(FakeCanvas* c) const override;
+
+    void execute(SkCanvas* c) const override;
+
+    void dump() const override {
+        SkDebugf("%d: drawRect", fID);
+    }
+
+protected:
+
+private:
+    SkRect  fRect;
+    SkColor fColor;
+};
+
+#endif // Cmds_DEFINED
