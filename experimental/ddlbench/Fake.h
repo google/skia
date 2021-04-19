808600d8b5cf78561a7565b502ca4413b0b3490a
Robert Phillips <robertphillips@google.com>
2021-04-19 11:52:29 -0400

    Playground
    Change-Id: Ib73dd51f3f558aa6c6b98aa611da116d8906bf7f

---
+++ experimental/ddlbench/Fake.h
@@ -0,0 +1,168 @@
+// Copyright 2021 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#ifndef Fake_DEFINED
+#define Fake_DEFINED
+
+#include "include/core/SkBitmap.h"
+#include "include/core/SkColor.h"
+#include "include/core/SkMatrix.h"
+#include "include/core/SkTypes.h"
+
+#include <vector>
+
+class Cmd;
+class SkBitmap;
+
+class Key {
+public:
+    // field:  |  transparent  |  depth   |  material  |
+    // bits:   |      1        |   3      |     4      |
+    static const uint32_t kMaterialShift = 0;
+    static const uint32_t kNumMaterialBits = 4;
+    static const uint32_t kMaterialMask = (0x1 << kNumMaterialBits) - 1;
+
+    static const uint32_t kDepthShift = kNumMaterialBits;
+    static const uint32_t kNumDepthBits = 3;
+    static const uint32_t kDepthMask = (0x1 << kNumDepthBits) - 1;
+
+    static const uint32_t kTransparentShift = kNumMaterialBits + kNumDepthBits;
+    static const uint32_t kNumTransparentBits = 1;
+    static const uint32_t kTransparentMask = (0x1 << kNumTransparentBits) - 1;
+
+    Key() : fKey(0) {}
+    Key(uint32_t depth, uint32_t material, bool transparent) {
+        SkASSERT(!(depth & ~kDepthMask));
+        SkASSERT(!(material & ~kMaterialMask));
+
+        fKey = (transparent ? 0x1 : 0x0) << kTransparentShift |
+               (depth & kDepthMask) << kDepthShift |
+               (material & kMaterialMask) << kMaterialShift;
+    }
+
+    bool transparent() const {
+        return (fKey >> kTransparentShift) & kTransparentMask;
+    }
+
+    uint32_t depth() const {
+        return (fKey >> kDepthShift) & kDepthMask;
+    }
+
+    uint32_t material() const {
+        return (fKey >> kMaterialShift) & kMaterialMask;
+    }
+
+    void dump() const {
+        SkDebugf("transparent: %d depth: %d mat: %d\n",
+                 this->transparent(),
+                 this->depth(),
+                 this->material());
+    }
+
+    bool operator>(const Key& other) const { return fKey > other.fKey; }
+    bool operator<(const Key& other) const { return fKey < other.fKey; }
+
+private:
+    uint64_t fKey;
+};
+
+class KeyAndCmd {
+public:
+    Key  fKey;
+    Cmd* fCmd;
+};
+
+class FakePaint {
+public:
+    FakePaint() {}
+
+    void setColor(SkColor c) { fColor = c; }
+    SkColor getColor() const { return fColor; }
+
+protected:
+
+private:
+    SkColor fColor = SK_ColorBLACK;
+};
+
+class FakeDevice {
+public:
+    FakeDevice(SkBitmap bm) : fBM(bm) {
+        SkASSERT(bm.width() == 256 && bm.height() == 256);
+
+        memset(fZBuffer, 0, sizeof(fZBuffer));
+    }
+
+    ~FakeDevice() {
+    }
+
+    void drawRect(int id, uint32_t z, SkIRect r, FakePaint p);
+
+    void sort() {
+        std::sort(fSortedCmds.begin(), fSortedCmds.end(),
+                  [](const KeyAndCmd& a, const KeyAndCmd& b) {
+                      return a.fKey < b.fKey;
+                  });
+    }
+
+    void finalize();
+
+    void getOrder(std::vector<int>*) const;
+
+protected:
+
+private:
+    bool                   fFinalized = false;
+    std::vector<KeyAndCmd> fSortedCmds;
+
+    SkBitmap               fBM;
+    uint32_t               fZBuffer[256][256];
+};
+
+class FakeCanvas {
+public:
+    FakeCanvas(SkBitmap& bm) {
+        fStateStack.push_back({bm, SkMatrix::I()});
+    }
+
+    void saveLayer() {
+        SkASSERT(!fFinalized);
+
+    }
+
+    void drawRect(int id, SkIRect r, FakePaint p);
+
+    void clipRect(SkIRect r) {
+        SkASSERT(!fFinalized);
+
+    }
+
+    void translate(SkVector trans) {
+        SkASSERT(!fFinalized);
+
+    }
+
+    void finalize();
+
+    std::vector<int> getOrder() const;
+
+protected:
+
+private:
+    uint32_t nextZ() {
+        return fNextZ++;
+    }
+
+    class State {
+    public:
+        FakeDevice fDevice;
+        SkMatrix   fCTM;
+    };
+
+    int                    fNextZ = 1;
+    bool                   fFinalized = false;
+    std::vector<State>     fStateStack;
+};
+
+
+#endif // Fake_DEFINED
