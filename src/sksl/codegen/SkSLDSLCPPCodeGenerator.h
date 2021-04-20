f43b9d90c50938c65d51cc2ba5fbc8cabc5c2274
John Stiles <johnstiles@google.com>
2021-04-20 19:59:23 +0000

    Revert "Implement statements and expressions in DSL C++ code generator."
    This reverts commit 16cbfb41dff107ca3388bacf34201311d3c5f14d.
    
    Reason for revert: using ES3 features, breaks on ANGLE ES2 bots
    
    Original change's description:
    > Implement statements and expressions in DSL C++ code generator.
    >
    > This CL removes the bulk of the existing C++ code generator, especially
    > all the complex format-string assembly code. It has been replaced with
    > actual DSL code generation. Simple IR can now be successfully translated
    > to a working DSL fragment processor.
    >
    > This CL also adds a simple test harness which is patterned after the
    > existing SkSLTest; it renders a pixel, reads it back, and fails the test
    > if the result isn't solid green (RGBA=0101).
    >
    > This CL doesn't implement every feature. Some obvious gaps include:
    > - Sampling from children
    > - Uniforms/inputs of any kind
    > - Function calls of any kind
    >
    > Change-Id: Ib80c23fe1ba4453f7c3cb43b65f93c5ea0deb709
    > Bug: skia:11854
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/396757
    > Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Commit-Queue: John Stiles <johnstiles@google.com>
    
    TBR=brianosman@google.com,ethannicholas@google.com,johnstiles@google.com
    
    Change-Id: I4f3e7667bf1e3a5539d0248b6c47d9ae2296aa88
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:11854

--- src/sksl/codegen/SkSLDSLCPPCodeGenerator.h
+++ src/sksl/codegen/SkSLDSLCPPCodeGenerator.h
@@ -27,32 +27,6 @@ public:
 private:
     using Precedence = Operator::Precedence;
 
-    void writeAnyConstructor(const AnyConstructor& c, Precedence parentPrecedence) override;
-
-    void writeBlock(const Block& b);
-
-    void writeCastConstructor(const AnyConstructor& c, Precedence parentPrecedence) override;
-
-    void writeDoStatement(const DoStatement& d);
-
-    void writeForStatement(const ForStatement& f);
-
-    void writeFunctionBody(const Block& b);
-
-    void writeIfStatement(const IfStatement& r) override;
-
-    void writeReturnStatement(const ReturnStatement& r) override;
-
-    void writeStatement(const Statement& s);
-
-    void writeSwitchStatement(const SwitchStatement& s) override;
-
-    void writeTernaryExpression(const TernaryExpression& t, Precedence parentPrecedence) override;
-
-    void writeVar(const Variable& var);
-
-    void writeVarDeclaration(const VarDeclaration& var, bool global);
-
     void writef(const char* s, va_list va) SK_PRINTF_LIKE(2, 0);
 
     void writef(const char* s, ...) SK_PRINTF_LIKE(2, 3);
@@ -65,13 +39,23 @@ private:
 
     String getTypeName(const Type& type) override;
 
-    String getDSLType(const Type& type);
+    void writeBinaryExpression(const BinaryExpression& b, Precedence parentPrecedence) override;
 
-    String getDSLModifiers(const Modifiers& type);
+    void writeIntLiteral(const IntLiteral& i) override;
 
     void writeSwizzle(const Swizzle& swizzle) override;
 
     void writeVariableReference(const VariableReference& ref) override;
+
+    String getSamplerHandle(const Variable& var);
+
+    void writeIfStatement(const IfStatement& s) override;
+
+    void writeReturnStatement(const ReturnStatement& s) override;
+
+    void writeSwitchStatement(const SwitchStatement& s) override;
+
+    String getSampleVarName(const char* prefix, int sampleCounter);
 
     void writeFunctionCall(const FunctionCall& c) override;
 
@@ -88,6 +72,7 @@ private:
     void addUniform(const Variable& var);
 
     // writes a printf escape that will be filled in at runtime by the given C++ expression string
+    void writeRuntimeValue(const Type& type, const Layout& layout, const String& cppCode);
     String formatRuntimeValue(const Type& type, const Layout& layout, const String& cppCode,
                               std::vector<String>* formatArgs);
 
@@ -99,11 +84,17 @@ private:
 
     void writePrivateVarValues();
 
+    void writeCodeAppend(const String& code);
+
+    String assembleCodeAndFormatArgPrintf(const String& code);
+
     bool writeEmitCode(std::vector<const Variable*>& uniforms);
 
     void writeSetData(std::vector<const Variable*>& uniforms);
 
     void writeGetKey();
+
+    void writeOnTextureSampler();
 
     void writeClone();
 
@@ -111,12 +102,39 @@ private:
 
     void writeTest();
 
+    // If the returned C++ is included in the generated code, then the variable name stored in
+    // cppVar will refer to a valid SkString that matches the Expression. Successful returns leave
+    // the output buffer (and related state) unmodified.
+    //
+    // In the simplest cases, this will return "SkString {cppVar}(\"{e}\");", while more advanced
+    // cases will properly insert format arguments.
+    String convertSKSLExpressionToCPP(const Expression& e, const String& cppVar);
+
+    // Process accumulated sksl to split it into appended code sections, properly interleaved with
+    // the extra emit code blocks, based on statement/block locations and the inserted tokens
+    // from newExtraEmitCodeBlock(). It is necessary to split the sksl after the program has been
+    // fully walked since many elements redirect fOut to simultaneously build header sections and
+    // bodies that are then concatenated; due to this it is not possible to split the sksl emission
+    // on the fly.
+    void flushEmittedCode();
+
+    // Start a new extra emit code block for accumulating C++ code. This will insert a token into
+    // the sksl stream to mark the fence between previous complete sksl statements and where the
+    // C++ code added to the new block will be added to emitCode(). These tokens are removed by
+    // flushEmittedCode() as it consumes them before passing pure sksl to writeCodeAppend().
+    void newExtraEmitCodeBlock();
+
+    // Append CPP code to the current extra emit code block.
+    void addExtraEmitCodeLine(const String& toAppend);
+
     int getChildFPIndex(const Variable& var) const;
 
     String fName;
     String fFullName;
     SectionAndParameterHelper fSectionAndParameterHelper;
+    std::vector<String> fExtraEmitCodeBlocks;
 
+    std::vector<String> fFormatArgs;
     // true if the sksl declared its main() function with a float2 parameter AND referenced that
     // parameter in its body.
     bool fAccessSampleCoordsDirectly = false;
@@ -126,6 +144,14 @@ private:
 
     // True while compiling the main() function of the FP.
     bool fInMain = false;
+
+    // Gives unique but predictable names to invocations of sample().
+    int fSampleCounter = 0;
+
+    // if not null, we are accumulating SkSL for emitCode into fOut, which
+    // replaced the original buffer with a StringStream. The original buffer is
+    // stored here for restoration.
+    OutputStream* fCPPBuffer = nullptr;
 
     using INHERITED = GLSLCodeGenerator;
 };
