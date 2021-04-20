ea890a758ea2a5518afa77f8746f8bd62dbd4912
Chris Dalton <csmartdalton@google.com>
2021-04-20 00:45:50 -0600

    Implement a fixed count stroke tessellator
    This new tessellator renders strokes as fixed-count triangle strip
    instances. Any extra triangles not needed by the instance are emitted
    as degenerate triangles. Since it draws in order, this tessellator
    allows us to batch dynamic colors even when hw tessellation is not
    supported.
    
    Bug: skia:10419
    Change-Id: If03a8b76319471ae4d4580dda019b69204d9197b

---
+++ src/gpu/tessellate/GrStrokeFixedCountTessellator.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef GrStrokeFixedCountTessellator_DEFINED
+#define GrStrokeFixedCountTessellator_DEFINED
+
+#include "src/gpu/GrVertexChunkArray.h"
+#include "src/gpu/tessellate/GrStrokeTessellator.h"
+
+// Renders strokes as fixed-count triangle strip instances. Any extra triangles not needed by the
+// instance are emitted as degenerate triangles.
+class GrStrokeFixedCountTessellator : public GrStrokeTessellator {
+public:
+    GrStrokeFixedCountTessellator(ShaderFlags shaderFlags, const SkMatrix& viewMatrix,
+                                  PathStrokeList* pathStrokeList)
+            : GrStrokeTessellator(GrStrokeTessellateShader::Mode::kFixedCount, shaderFlags,
+                                  viewMatrix, pathStrokeList) {
+    }
+
+    void prepare(GrMeshDrawOp::Target*, int totalCombinedVerbCnt) override;
+    void draw(GrOpFlushState*) const override;
+
+private:
+    GrVertexChunkArray fInstanceChunks;
+    int fFixedVertexCount = 0;
+};
+
+#endif
