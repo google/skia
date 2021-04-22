608e7967f7bd0142f17f41a63c6371a60b1d5c46
John Stiles <johnstiles@google.com>
2021-04-22 17:54:38 -0400

    Remove fSupportsTracking flag from UniformCTypeMapper.
    This flag was always set to true for every type.
    
    Change-Id: Icbbe9a3c49adf0c49e6d912f978560b178eb633c

--- src/sksl/codegen/SkSLCPPCodeGenerator.cpp
+++ src/sksl/codegen/SkSLCPPCodeGenerator.cpp
@@ -682,7 +682,7 @@ void CPPCodeGenerator::writePrivateVars() {
                 // state. Note that tracked variables must be uniform in's and that is validated
                 // before writePrivateVars() is called.
                 const UniformCTypeMapper* mapper = UniformCTypeMapper::Get(fContext, var);
-                SkASSERT(mapper && mapper->supportsTracking());
+                SkASSERT(mapper);
 
                 String name = HCodeGenerator::FieldName(String(var.name()).c_str());
                 // The member statement is different if the mapper reports a default value
@@ -1042,8 +1042,6 @@ void CPPCodeGenerator::writeSetData(std::vector<const Variable*>& uniforms) {
             }
 
             if (isTracked) {
-                SkASSERT(mapper->supportsTracking());
-
                 String prevVar = HCodeGenerator::FieldName(name) + "Prev";
                 this->writef("%sif (%s) {\n"
                              "%s    %s;\n"
@@ -1362,14 +1360,6 @@ bool CPPCodeGenerator::generateCode() {
                             + "'s type is not supported for use as a 'uniform in'");
                     return false;
                 }
-                if (decl.var().modifiers().fLayout.fFlags & Layout::kTracked_Flag) {
-                    if (!mapper->supportsTracking()) {
-                        fErrors.error(decl.fOffset, String(decl.var().name())
-                                + "'s type does not support state tracking");
-                        return false;
-                    }
-                }
-
             } else {
                 // If it's not a uniform_in, it's an error to be tracked
                 if (decl.var().modifiers().fLayout.fFlags & Layout::kTracked_Flag) {
