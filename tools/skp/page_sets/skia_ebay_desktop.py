15e53755dce9a36322c06b59f8df70ee55a52a5a
Ravi Mistry <rmistry@google.com>
2021-04-23 14:31:12 -0400

    Add wait time to ebay pageset
    Bug: skia:11898
    Change-Id: I0bb58f1d8e9c6ad48148d50b840f152fc158f071

--- tools/skp/page_sets/skia_ebay_desktop.py
+++ tools/skp/page_sets/skia_ebay_desktop.py
@@ -20,8 +20,7 @@ class SkiaDesktopPage(page_module.Page):
     self.archive_data_file = 'data/skia_ebay_desktop.json'
 
   def RunNavigateSteps(self, action_runner):
-    action_runner.Navigate(self.url)
-    action_runner.Wait(15)
+    action_runner.Navigate(self.url, timeout_in_seconds=120)
 
 
 class SkiaEbayDesktopPageSet(story.StorySet):
