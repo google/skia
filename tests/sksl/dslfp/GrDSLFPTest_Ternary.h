298a9e269b1b2df311b15f04a2019d971af3ee7e
John Stiles <johnstiles@google.com>
2021-04-23 23:57:12 -0400

    Support 'layout(when)' expressions in DSL C++.
    This required some changes to how we name variables in DSL.
    When-expressions are designed to expect a local C++ variable with the
    same name as the layout key. This constraint means our DSLVar variables
    CANNOT have the same name as the layout key. Now, all DSL variables are
    given a prefix. We try to keep the code tidy by using just a leading
    underscore as the prefix, where it's safe to do so. (The C++ naming
    rules put some underscore-names out of bounds, but underscore followed
    by a lowercase letter is safe.)
    
    Change-Id: Iaa8878042329b9909096f05712d5cf636ea01822
    Bug: skia:11854

--- tests/sksl/dslfp/GrDSLFPTest_Ternary.h
+++ tests/sksl/dslfp/GrDSLFPTest_Ternary.h
@@ -14,19 +14,25 @@
 
 class GrDSLFPTest_Ternary : public GrFragmentProcessor {
 public:
-    static std::unique_ptr<GrFragmentProcessor> Make(SkPMColor4f colorGreen, SkPMColor4f colorRed) {
-        return std::unique_ptr<GrFragmentProcessor>(new GrDSLFPTest_Ternary(colorGreen, colorRed));
+    static std::unique_ptr<GrFragmentProcessor> Make(bool primaryColors, SkPMColor4f colorGreen, SkPMColor4f colorRed, SkPMColor4f colorOrange, SkPMColor4f colorPurple) {
+        return std::unique_ptr<GrFragmentProcessor>(new GrDSLFPTest_Ternary(primaryColors, colorGreen, colorRed, colorOrange, colorPurple));
     }
     GrDSLFPTest_Ternary(const GrDSLFPTest_Ternary& src);
     std::unique_ptr<GrFragmentProcessor> clone() const override;
     const char* name() const override { return "DSLFPTest_Ternary"; }
+    bool primaryColors;
     SkPMColor4f colorGreen;
     SkPMColor4f colorRed;
+    SkPMColor4f colorOrange;
+    SkPMColor4f colorPurple;
 private:
-    GrDSLFPTest_Ternary(SkPMColor4f colorGreen, SkPMColor4f colorRed)
+    GrDSLFPTest_Ternary(bool primaryColors, SkPMColor4f colorGreen, SkPMColor4f colorRed, SkPMColor4f colorOrange, SkPMColor4f colorPurple)
     : INHERITED(kGrDSLFPTest_Ternary_ClassID, kNone_OptimizationFlags)
+    , primaryColors(primaryColors)
     , colorGreen(colorGreen)
-    , colorRed(colorRed) {
+    , colorRed(colorRed)
+    , colorOrange(colorOrange)
+    , colorPurple(colorPurple) {
     }
     std::unique_ptr<GrGLSLFragmentProcessor> onMakeProgramImpl() const override;
     void onGetGLSLProcessorKey(const GrShaderCaps&, GrProcessorKeyBuilder*) const override;
