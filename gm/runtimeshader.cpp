e29e363fe3a811c28b0d4d8002f16d5b3975a459
Brian Osman <brianosman@google.com>
2021-04-26 17:02:57 -0400

    Add runtime effect unsharp GM
    Demonstrates the bug that was present in
    https://skia-review.googlesource.com/c/skia/+/397320
    
    Change-Id: Ia6693cd765b5bd2acda147798b79a05ec31bbce5

--- gm/runtimeshader.cpp
+++ gm/runtimeshader.cpp
@@ -216,6 +216,44 @@ public:
 };
 DEF_GM(return new SpiralRT;)
 
+class UnsharpRT : public RuntimeShaderGM {
+public:
+    UnsharpRT() : RuntimeShaderGM("unsharp_rt", {512, 256}, R"(
+        uniform shader input;
+        half4 main(float2 xy) {
+            half4 c = sample(input, xy) * 5;
+            c -= sample(input, xy + float2( 1,  0));
+            c -= sample(input, xy + float2(-1,  0));
+            c -= sample(input, xy + float2( 0,  1));
+            c -= sample(input, xy + float2( 0, -1));
+            return c;
+        }
+    )") {}
+
+    sk_sp<SkImage> fMandrill;
+
+    void onOnceBeforeDraw() override {
+        fMandrill      = GetResourceAsImage("images/mandrill_256.png");
+        this->RuntimeShaderGM::onOnceBeforeDraw();
+    }
+
+    void onDraw(SkCanvas* canvas) override {
+        // First we draw the unmodified image
+        canvas->drawImage(fMandrill,      0,   0);
+
+        // Now draw the image with our unsharp mask applied
+        SkRuntimeShaderBuilder builder(fEffect);
+        const SkSamplingOptions sampling(SkFilterMode::kNearest);
+        builder.child("input") = fMandrill->makeShader(sampling);
+
+        SkPaint paint;
+        paint.setShader(builder.makeShader(nullptr, true));
+        canvas->translate(256, 0);
+        canvas->drawRect({ 0, 0, 256, 256 }, paint);
+    }
+};
+DEF_GM(return new UnsharpRT;)
+
 class ColorCubeRT : public RuntimeShaderGM {
 public:
     ColorCubeRT() : RuntimeShaderGM("color_cube_rt", {512, 512}, R"(
