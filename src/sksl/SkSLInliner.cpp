a94e0261ef436b46ec63f2f72a654ce97b278901
John Stiles <johnstiles@google.com>
2021-04-27 17:29:59 -0400

    Add Make factory method to VariableReference.
    There aren't any optimization opportunities here, but it was the only
    expression/statement type left without a Make or Convert factory.
    
    Change-Id: I4c413a3b2619261f9e974528fa45211383e56fa7
    Bug: skia:11342
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/402017
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

--- src/sksl/SkSLInliner.cpp
+++ src/sksl/SkSLInliner.cpp
@@ -550,7 +550,7 @@ std::unique_ptr<Statement> Inliner::inlineStatement(int offset,
                                                      variable.type().clone(symbolTableForStatement),
                                                      isBuiltinCode,
                                                      variable.storage());
-            (*varMap)[&variable] = std::make_unique<VariableReference>(offset, clonedVar.get());
+            (*varMap)[&variable] = VariableReference::Make(offset, clonedVar.get());
             auto result = VarDeclaration::Make(*fContext,
                                                clonedVar.get(),
                                                decl.baseType().clone(symbolTableForStatement),
@@ -646,7 +646,7 @@ Inliner::InlinedCall Inliner::inlineCall(FunctionCall* call,
                                                       symbolTable.get(), Modifiers{},
                                                       caller->isBuiltin(), &noInitialValue);
         inlineStatements.push_back(std::move(var.fVarDecl));
-        resultExpr = std::make_unique<VariableReference>(/*offset=*/-1, var.fVarSymbol);
+        resultExpr = VariableReference::Make(/*offset=*/-1, var.fVarSymbol);
     }
 
     // Create variables in the extra statements to hold the arguments, and assign the arguments to
@@ -670,7 +670,7 @@ Inliner::InlinedCall Inliner::inlineCall(FunctionCall* call,
                                                       symbolTable.get(), param->modifiers(),
                                                       caller->isBuiltin(), &arguments[i]);
         inlineStatements.push_back(std::move(var.fVarDecl));
-        varMap[param] = std::make_unique<VariableReference>(/*offset=*/-1, var.fVarSymbol);
+        varMap[param] = VariableReference::Make(/*offset=*/-1, var.fVarSymbol);
     }
 
     for (const std::unique_ptr<Statement>& stmt : body.children()) {
