29e7bbc3df7225ba09c979df1a5200b45b416c33
Robert Phillips <robertphillips@google.com>
2021-04-27 16:11:49 -0400

    Playground
    Change-Id: Ib73dd51f3f558aa6c6b98aa611da116d8906bf7f

---
+++ experimental/ddlbench/Cmds.cpp
@@ -0,0 +1,76 @@
+// Copyright 2021 Google LLC.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include "experimental/ddlbench/Cmds.h"
+#include "experimental/ddlbench/Fake.h"
+
+#include "include/core/SkBitmap.h"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkColor.h"
+#include "include/core/SkPaint.h"
+#include "include/effects/SkGradientShader.h"
+
+void RectCmd::execute(FakeCanvas* c) const {
+    FakePaint p;
+    p.setColor(fColors[0]);
+    c->drawRect(fID, fRect, p);
+}
+
+static bool is_opaque(SkColor c) {
+    return 0xFF == SkColorGetA(c);
+}
+
+void RectCmd::rasterize(uint32_t zBuffer[256][256], SkBitmap* dstBM, unsigned int z) const {
+    for (int y = fRect.fTop; y < fRect.fBottom; ++y) {
+        for (int x = fRect.fLeft; x < fRect.fRight; ++x) {
+            if (z > zBuffer[x][y]) {
+                zBuffer[x][y] = z;
+
+                SkColor c = this->evalColor(x, y, fColors);
+
+                if (is_opaque(c)) {
+                    *dstBM->getAddr32(x, y) = c;
+                } else {
+                    SkColor4f bot = SkColor4f::FromColor(*dstBM->getAddr32(x, y));
+                    SkColor4f top = SkColor4f::FromColor(c);
+                    SkColor4f result = {
+                        top.fA * top.fR + (1.0f - top.fA) * bot.fR,
+                        top.fA * top.fG + (1.0f - top.fA) * bot.fG,
+                        top.fA * top.fB + (1.0f - top.fA) * bot.fB,
+                                 top.fA + (1.0f - top.fA) * bot.fA
+                    };
+                    *dstBM->getAddr32(x, y) = result.toSkColor();
+                }
+            }
+        }
+    }
+}
+
+void RectCmd::execute(SkCanvas* c) const {
+
+    SkPaint p;
+    if (fMaterialID == 1) {
+        p.setColor(fColors[0]);
+    } else if (fMaterialID == 2) {
+        SkPoint pts[] = { { 0.0f, 0.0f, }, { 256.0f, 256.0f } };
+        p.setShader(SkGradientShader::MakeLinear(pts, fColors, nullptr, 2,
+                                                 SkTileMode::kClamp, 0, nullptr));
+    } else {
+        SkASSERT(fMaterialID == 3);
+
+        SkColor4f colors[2] = {
+            SkColor4f::FromColor(fColors[0]),
+            SkColor4f::FromColor(fColors[1])
+        };
+        auto shader = SkGradientShader::MakeRadial(SkPoint::Make(128.0f, 128.0f),
+                                                   128.0f,
+                                                   colors,
+                                                   nullptr,
+                                                   nullptr,
+                                                   2,
+                                                   SkTileMode::kRepeat);
+        p.setShader(std::move(shader));
+    }
+
+    c->drawRect(SkRect::Make(fRect), p);
+}
