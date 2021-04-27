657eb15f3e7314d0210fb42105569bbbec9fffbe
Jim Van Verth <jvanverth@google.com>
2021-04-27 13:45:52 -0400

    Force expected MSL version to match the features that we require.
    Recently SkSL switched to make use of the MSL array<> feature, but in
    some circumstances we can end up compiling with a version that doesn't
    support that. This CL will force the required MSL version.
    
    Bug: skia:11918
    Change-Id: Id5d1ca05d70ec20841ebb1f2b43031374e292407

--- src/gpu/mtl/GrMtlUtil.mm
+++ src/gpu/mtl/GrMtlUtil.mm
@@ -96,13 +96,17 @@ id<MTLLibrary> GrCompileMtlShaderLibrary(const GrMtlGpu* gpu,
                                                    length:msl.size()
                                                  encoding:NSUTF8StringEncoding
                                              freeWhenDone:NO];
+    MTLCompileOptions* options = [[MTLCompileOptions alloc] init];
+
     NSError* error = nil;
 #if defined(SK_BUILD_FOR_MAC)
+    options.languageVersion = MTLLanguageVersion2_0;
     id<MTLLibrary> compiledLibrary = GrMtlNewLibraryWithSource(gpu->device(), nsSource,
-                                                               nil, &error);
+                                                               options, &error);
 #else
+    options.languageVersion = MTLLanguageVersion1_2;
     id<MTLLibrary> compiledLibrary = [gpu->device() newLibraryWithSource:nsSource
-                                                                 options:nil
+                                                                 options:options
                                                                    error:&error];
 #endif
     if (!compiledLibrary) {
