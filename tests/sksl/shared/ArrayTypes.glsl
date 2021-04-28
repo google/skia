dcf7823474fdee2a890fbaf82e610c7d75daf508
John Stiles <johnstiles@google.com>
2021-04-28 16:03:05 -0400

    Fix array-of-struct name mangling in Pipeline stage.
    There aren't many cases where array-of-struct types are useful in
    ES2, but it looks like function parameters are one such case:
    http://screen/7Fnc7GhewAkUK3j
    
    Change-Id: I23410a3824a3c202c12147d6939586cc0e55a9ce

--- tests/sksl/shared/ArrayTypes.glsl
+++ tests/sksl/shared/ArrayTypes.glsl
@@ -1,5 +1,12 @@
 
 out vec4 sk_FragColor;
+struct S {
+    vec2 v;
+};
+void initialize_vS(out S z[2]) {
+    z[0].v = vec2(0.0, 1.0);
+    z[1].v = vec2(2.0, 1.0);
+}
 vec4 main() {
     vec2 x[2];
     x[0] = vec2(0.0, 0.0);
@@ -7,5 +14,7 @@ vec4 main() {
     vec2 y[2];
     y[0] = vec2(0.0, 1.0);
     y[1] = vec2(-1.0, 2.0);
-    return vec4(x[0].x * x[0].y, x[1].x - x[1].y, y[0].x / y[0].y, y[1].x + y[1].y);
+    S z[2];
+    initialize_vS(z);
+    return vec4(x[0].x * x[0].y + z[0].v.x, x[1].x - x[1].y * z[0].v.y, (y[0].x / y[0].y) / z[1].v.x, y[1].x + y[1].y * z[1].v.y);
 }
