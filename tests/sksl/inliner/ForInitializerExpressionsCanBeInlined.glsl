c5aaf320ed0b1eba52e8034ca080f5bea2a7ef9b
John Stiles <johnstiles@google.com>
2021-04-28 14:07:10 -0400

    Loosen ES3 restrictions in Runtime Effects for debugging.
    This CL adds a RuntimeEffect option flag which skips over the various
    `strictES2Mode` checks sprinkled throughout IR generation.
    
    Runtime Effects still won't allow a lot of ES3 things (the Pipeline
    stage will reject unsupported statement types, SkVM doesn't support most
    non-ES2 constructs, etc). However, this change will give us the ability
    to test many more features involving arrays and structs that previously
    were off-limits due to ES2 restrictions, and will shore up some
    legitimate gaps in our testing. This is a useful starting point to allow
    for improved test coverage.
    
    Change-Id: I4a5bc43914e65fc7e59f1cecb76a0ec5a7f05f2f
    Bug: skia:11209

--- tests/sksl/inliner/ForInitializerExpressionsCanBeInlined.glsl
+++ tests/sksl/inliner/ForInitializerExpressionsCanBeInlined.glsl
@@ -8,7 +8,7 @@ vec4 grow_h4h4(vec4 v) {
     return v + vec4(0.125);
 }
 vec4 main() {
-    for (sk_FragColor = vec4(0.0625);shouldLoop_bh4(sk_FragColor); sk_FragColor = grow_h4h4(sk_FragColor)) {
+    for (vec4 color = vec4(0.0625);shouldLoop_bh4(color); color = grow_h4h4(color)) {
     }
     return colorGreen;
 }
