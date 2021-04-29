a6cc82e3cf01780e9e1cc4e0d824daf07ec40ae5
John Stiles <johnstiles@google.com>
2021-04-29 17:41:00 +0000

    Revert "Optimize away swizzles of constant variables."
    This reverts commit 7b253d34ee0ef9fc514b030dab76aac940b70325.
    
    Reason for revert: asserting on some bots
    
    Original change's description:
    > Optimize away swizzles of constant variables.
    >
    > Change-Id: I49807f18ea54e85c2b8f1419278c54aa2d6f8fac
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/402581
    > Commit-Queue: John Stiles <johnstiles@google.com>
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    
    TBR=brianosman@google.com,ethannicholas@google.com,johnstiles@google.com
    
    Change-Id: I530e6923e3c56f503508c70258c25f160f8985bc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/402640
    Reviewed-by: John Stiles <johnstiles@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- tests/sksl/folding/SwizzleFolding.glsl
+++ tests/sksl/folding/SwizzleFolding.glsl
@@ -3,7 +3,16 @@ out vec4 sk_FragColor;
 uniform vec4 colorRed;
 uniform vec4 colorGreen;
 vec4 main() {
+    const vec4 _0_colorWhite = vec4(1.0);
+    const vec2 _1_point = vec2(40.0, 60.0);
     bool _2_ok = true;
-    _2_ok = _2_ok && colorGreen != colorRed;
+    _2_ok = _2_ok && (((_1_point.x >= 0.0 && _1_point.x <= 100.0) && _1_point.y >= 0.0) && _1_point.y <= 100.0);
+    _2_ok = _2_ok && _0_colorWhite.x == 1.0;
+    _2_ok = _2_ok && _0_colorWhite.x + _0_colorWhite.y == 2.0;
+    _2_ok = _2_ok && (_0_colorWhite.x + _0_colorWhite.y) + _0_colorWhite.z == 3.0;
+    _2_ok = _2_ok && ((_0_colorWhite.x + _0_colorWhite.y) + _0_colorWhite.z) + _0_colorWhite.w == 4.0;
+    _2_ok = _2_ok && colorGreen * _0_colorWhite.x != colorRed * _0_colorWhite.y;
+    const vec2 _3_pointOffset = _1_point.yx + _0_colorWhite.xz;
+    _2_ok = _2_ok && _3_pointOffset == vec2(61.0, 41.0);
     return _2_ok ? colorGreen : colorRed;
 }
