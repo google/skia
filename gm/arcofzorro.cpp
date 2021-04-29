eb0e9deaaa39965cb0cbc30632e90ef39da36b57
Robert Phillips <robertphillips@google.com>
2021-04-29 17:07:49 -0400

    Resource ref tracing
    Change-Id: I65a0218d4e89583affa86f6c6d0b015c4841116b

--- gm/arcofzorro.cpp
+++ gm/arcofzorro.cpp
@@ -8,83 +8,124 @@
 #include "gm/gm.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkColor.h"
+#include "include/core/SkMaskFilter.h"
 #include "include/core/SkPaint.h"
+#include "include/core/SkPath.h"
 #include "include/core/SkRect.h"
 #include "include/core/SkScalar.h"
 #include "include/core/SkSize.h"
 #include "include/core/SkString.h"
+#include "include/effects/SkImageFilters.h"
 #include "include/utils/SkRandom.h"
 
 namespace skiagm {
+
+#define CROSS_INNER_RADIUS 3
+#define CROSS_SIZE 18
 
 // This GM draws a lot of arcs in a 'Z' shape. It particularly exercises
 // the 'drawArc' code near a singularly of its processing (i.e., near the
 // edge of one of its underlying quads).
 class ArcOfZorroGM : public GM {
 public:
-    ArcOfZorroGM() {
+    ArcOfZorroGM(bool old) : fOld(old) {
         this->setBGColor(0xFFCCCCCC);
     }
 
 protected:
 
     SkString onShortName() override {
-        return SkString("arcofzorro");
+        if (fOld) {
+            return SkString("arcofzorro_old");
+        } else {
+            return SkString("arcofzorro_new");
+        }
     }
 
     SkISize onISize() override {
-        return SkISize::Make(1000, 1000);
+        return SkISize::Make(200, 200);
+    }
+
+    /*
+        Budget: 268435456 bytes
+                        Entry Count: current 818 (818 budgeted, 0 wrapped, 800 locked, 818 scratch), high 818
+                        Entry Bytes: current 3993856 (budgeted 3993856, 1.5% full, 0 unbudgeted) high 3993856
+        Textures Created: 801
+     */
+    void drawCross1(float x, float y, SkPaint crossPaint, SkCanvas* canvas) {
+        canvas->drawLine(x - CROSS_SIZE / 2, y, x - CROSS_INNER_RADIUS, y, crossPaint);
+        canvas->drawLine(x + CROSS_SIZE / 2, y, x + CROSS_INNER_RADIUS, y, crossPaint);
+        canvas->drawLine(x, y - CROSS_SIZE / 2, x, y - CROSS_INNER_RADIUS, crossPaint);
+        canvas->drawLine(x, y + CROSS_SIZE / 2, x, y + CROSS_INNER_RADIUS, crossPaint);
+    }
+
+    /*
+        Budget: 268435456 bytes
+                        Entry Count: current 34 (34 budgeted, 0 wrapped, 17 locked, 33 scratch), high 34
+                        Entry Bytes: current 798976 (budgeted 798976, 0.3% full, 0 unbudgeted) high 798976
+        Textures Created: 17
+     */
+    void drawCross2(float x, float y, SkPaint crossPaint, SkCanvas* canvas) {
+        float quantX = 0.25f * SkScalarRoundToScalar(4 * x);
+        float quantY = 0.25f * SkScalarRoundToScalar(4 * y);
+
+        canvas->save();
+           canvas->translate(quantX, quantY);
+           canvas->drawPath(fPath, crossPaint);
+        canvas->restore();
     }
 
     void onDraw(SkCanvas* canvas) override {
-        SkRandom rand;
 
-        SkRect rect = SkRect::MakeXYWH(10, 10, 200, 200);
+        fPath.moveTo(-CROSS_SIZE / 2, 0);
+        fPath.lineTo(-CROSS_INNER_RADIUS, 0);
+        fPath.moveTo(CROSS_SIZE / 2, 0);
+        fPath.lineTo(CROSS_INNER_RADIUS, 0);
+        fPath.moveTo(0, -CROSS_SIZE / 2);
+        fPath.lineTo(0, -CROSS_INNER_RADIUS);
+        fPath.moveTo(0, CROSS_SIZE / 2);
+        fPath.lineTo(0, CROSS_INNER_RADIUS);
+        fPath.setIsVolatile(false);
 
         SkPaint p;
-
+        p.setColor(SK_ColorYELLOW);
+        p.setStrokeWidth(3);
+        p.setAntiAlias(true);
         p.setStyle(SkPaint::kStroke_Style);
-        p.setStrokeWidth(35);
-        int xOffset = 0, yOffset = 0;
-        int direction = 0;
+        p.setStrokeCap(SkPaint::kRound_Cap);
 
-        for (float arc = 134.0f; arc < 136.0f; arc += 0.01f) {
-            SkColor color = rand.nextU();
-            color |= 0xff000000;
-            p.setColor(color);
+        SkPaint shadowP;
+        shadowP.setColor(SK_ColorBLACK);
+        shadowP.setStrokeWidth(3);
+        shadowP.setAntiAlias(true);
+        shadowP.setStyle(SkPaint::kStroke_Style);
+        shadowP.setStrokeCap(SkPaint::kRound_Cap);
+        shadowP.setMaskFilter(SkMaskFilter::MakeBlur(kNormal_SkBlurStyle, 6.85085f));
 
-            canvas->save();
-            canvas->translate(SkIntToScalar(xOffset), SkIntToScalar(yOffset));
-            canvas->drawArc(rect, 0, arc, false, p);
-            canvas->restore();
+        SkRandom random;
+        for (int i = 0; i < 200; ++i) {
+            float x = 100.0f + random.nextRangeF(-50.0f, 50.0f);
+            float y = 100.0f + random.nextRangeF(-50.0f, 50.0f);
 
-            switch (direction) {
-            case 0:
-                xOffset += 10;
-                if (xOffset >= 700) {
-                    direction = 1;
-                }
-                break;
-            case 1:
-                xOffset -= 10;
-                yOffset += 10;
-                if (xOffset < 50) {
-                    direction = 2;
-                }
-                break;
-            case 2:
-                xOffset += 10;
-                break;
+            if (fOld) {
+                this->drawCross1(x + 4, y + 1, shadowP, canvas);
+                this->drawCross1(x, y, p, canvas);
+            } else {
+                this->drawCross2(x + 4, y + 1, shadowP, canvas);
+                this->drawCross1(x, y, p, canvas);
             }
         }
-
     }
 
 private:
+    SkPath fPath;
+    bool   fOld;
+
     using INHERITED = GM;
 };
 
 //////////////////////////////////////////////////////////////////////////////
 
-DEF_GM(return new ArcOfZorroGM;)
+DEF_GM(return new ArcOfZorroGM(true);)
+DEF_GM(return new ArcOfZorroGM(false);)
 }  // namespace skiagm
