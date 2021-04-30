5276ba274b38826e3415326f08607bf6cdc08238
Brian Salomon <bsalomon@google.com>
2021-04-30 10:58:34 -0400

    Add --reducedshaders to fm
    Sometimes I like to dump the shaders from reduced shader mode.
    
    Bug: skia:11844
    Change-Id: I33c986116e326a023c432d582e3653267f311bca
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/402999
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Mike Klein <mtklein@google.com>

--- tools/fm/fm.cpp
+++ tools/fm/fm.cpp
@@ -56,14 +56,16 @@ static DEFINE_string(at    , "premul", "The alpha type for any raster backend.")
 static DEFINE_string(gamut ,   "srgb", "The color gamut for any raster backend.");
 static DEFINE_string(tf    ,   "srgb", "The transfer function for any raster backend.");
 static DEFINE_bool  (legacy,    false, "Use a null SkColorSpace instead of --gamut and --tf?");
-static DEFINE_bool  (skvm  ,    false, "Use SkVMBlitter when supported?");
-static DEFINE_bool  (jit   ,     true, "JIT SkVM?");
-static DEFINE_bool  (dylib ,    false, "JIT SkVM via dylib?");
 
-static DEFINE_int   (samples ,         0, "Samples per pixel in GPU backends.");
-static DEFINE_bool  (stencils,      true, "If false, avoid stencil buffers in GPU backends.");
-static DEFINE_bool  (dit     ,     false, "Use device-independent text in GPU backends.");
-static DEFINE_string(surf    , "default", "Backing store for GPU backend surfaces.");
+static DEFINE_bool  (skvm ,    false, "Use SkVMBlitter when supported?");
+static DEFINE_bool  (jit  ,     true, "JIT SkVM?");
+static DEFINE_bool  (dylib,    false, "JIT SkVM via dylib?");
+
+static DEFINE_bool  (reducedshaders,    false, "Use reduced shader set for any GPU backend.");
+static DEFINE_int   (samples       ,         0, "Samples per pixel in GPU backends.");
+static DEFINE_bool  (stencils      ,      true, "If false, avoid stencil buffers in GPU backends.");
+static DEFINE_bool  (dit           ,     false, "Use device-independent text in GPU backends.");
+static DEFINE_string(surf          , "default", "Backing store for GPU backend surfaces.");
 
 static DEFINE_bool(       preAbandonGpuContext, false, "Abandon the GrContext before drawing.");
 static DEFINE_bool(          abandonGpuContext, false, "Abandon the GrContext after drawing.");
@@ -389,6 +391,7 @@ int main(int argc, char** argv) {
 
     GrContextOptions baseOptions;
     SetCtxOptionsFromCommonFlags(&baseOptions);
+    baseOptions.fReducedShaderVariations = FLAGS_reducedshaders;
 
     sk_gpu_test::MemoryCache memoryCache;
     if (!FLAGS_writeShaders.isEmpty()) {
