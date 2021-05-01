8f76918322c40c5100add0c5760e3c32402c50d9
Chris Dalton <csmartdalton@google.com>
2021-04-30 18:32:02 -0600

    Don't attempt to use stencil on wrapped, stencil-less targets
    We can't attach stencil to a wrapped render target. If we find
    ourselves rendering to one that doesn't have stencil already, don't
    issue any ops that use stencil.
    
    Bug: chromium:1196353
    Change-Id: I9db93f31a4f8556103be72eb708755e4eaf78136

--- src/gpu/GrSurfaceDrawContext.cpp
+++ src/gpu/GrSurfaceDrawContext.cpp
@@ -923,7 +923,6 @@ bool GrSurfaceDrawContext::stencilPath(const GrHardClip* clip,
     canDrawArgs.fSurfaceProps = &fSurfaceProps;
     canDrawArgs.fAAType = (doStencilMSAA == GrAA::kYes) ? GrAAType::kMSAA : GrAAType::kNone;
     canDrawArgs.fHasUserStencilSettings = false;
-    canDrawArgs.fTargetIsWrappedVkSecondaryCB = this->wrapsVkSecondaryCB();
     GrPathRenderer* pr = this->drawingManager()->getPathRenderer(
             canDrawArgs, false, GrPathRendererChain::DrawType::kStencil);
     if (!pr) {
@@ -1616,8 +1615,6 @@ bool GrSurfaceDrawContext::drawAndStencilPath(const GrHardClip* clip,
     canDrawArgs.fSurfaceProps = &fSurfaceProps;
     canDrawArgs.fClipConservativeBounds = &clipConservativeBounds;
     canDrawArgs.fAAType = aaType;
-    SkASSERT(!this->wrapsVkSecondaryCB());
-    canDrawArgs.fTargetIsWrappedVkSecondaryCB = false;
     canDrawArgs.fHasUserStencilSettings = hasUserStencilSettings;
 
     // Don't allow the SW renderer
@@ -1780,7 +1777,6 @@ void GrSurfaceDrawContext::drawShapeUsingPathRenderer(const GrClip* clip,
     canDrawArgs.fPaint = &paint;
     canDrawArgs.fSurfaceProps = &fSurfaceProps;
     canDrawArgs.fClipConservativeBounds = &clipConservativeBounds;
-    canDrawArgs.fTargetIsWrappedVkSecondaryCB = this->wrapsVkSecondaryCB();
     canDrawArgs.fHasUserStencilSettings = false;
     canDrawArgs.fAAType = aaType;
 
