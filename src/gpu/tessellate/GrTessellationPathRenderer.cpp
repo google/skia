dbed5775017a6ef6f988ea8dd2bf90c833e14ae4
Chris Dalton <csmartdalton@google.com>
2021-05-03 18:38:19 -0600

    Re-enable tessellation for cacheable paths
    Now that the fixed count stroke tessellator has landed, these should
    hopefully not regress performance.
    
    Bug: skia:11138
    Bug: skia:11139
    Change-Id: Ifdcd817705d394ea6d0f932711ed725a66a7fc91
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/403956
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Chris Dalton <csmartdalton@google.com>

--- src/gpu/tessellate/GrTessellationPathRenderer.cpp
+++ src/gpu/tessellate/GrTessellationPathRenderer.cpp
@@ -137,15 +137,6 @@ GrPathRenderer::CanDrawPath GrTessellationPathRenderer::onCanDrawPath(
         !args.fProxy->canUseStencil(*args.fCaps)) {
         return CanDrawPath::kNo;
     }
-    // On platforms that don't have native support for indirect draws and/or hardware tessellation,
-    // we find that cached triangulations of strokes can render slightly faster. Let cacheable paths
-    // go to the triangulator on these platforms for now.
-    // (crbug.com/1163441, skbug.com/11138, skbug.com/11139)
-    if (!args.fCaps->nativeDrawIndirectSupport() &&
-        !args.fCaps->shaderCaps()->tessellationSupport() &&
-        shape.hasUnstyledKey()) {  // Is the path cacheable?
-        return CanDrawPath::kNo;
-    }
     return CanDrawPath::kYes;
 }
 
