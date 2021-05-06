9ac9bb2b51501714872b5828adc4582c58571c06
John Stiles <johnstiles@google.com>
2021-05-06 09:01:22 -0400

    Optimize intrinsic vector comparisons of constants.
    If every argument passed to lessThan/greaterThan/lessThanEqual/
    greaterThanEqual/equal/notEqual() is a compile-time constant, we now
    detect this and optimize away the function call entirely.
    
    Change-Id: I3415d21be6ef51b38b682a792bd118fad51957f5
    Bug: skia:10835

--- tests/sksl/intrinsics/Equal.glsl
+++ tests/sksl/intrinsics/Equal.glsl
@@ -2,6 +2,15 @@
 out vec4 sk_FragColor;
 uniform vec4 a;
 uniform vec4 b;
+uniform uvec2 c;
+uniform uvec2 d;
+uniform ivec3 e;
+uniform ivec3 f;
 void main() {
+    bvec4 expectTTFF = bvec4(true, true, false, false);
+    bvec4 expectFFTT = bvec4(false, false, true, true);
     sk_FragColor.x = float(equal(a, b).x ? 1 : 0);
+    sk_FragColor.y = float(equal(c, d).y ? 1 : 0);
+    sk_FragColor.z = float(equal(e, f).z ? 1 : 0);
+    sk_FragColor.w = float(any(expectTTFF) || any(expectFFTT) ? 1 : 0);
 }
