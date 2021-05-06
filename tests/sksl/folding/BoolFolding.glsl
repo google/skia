28181e70ce2d517167cf4e9b2a78e746a63f2680
John Stiles <johnstiles@google.com>
2021-05-05 22:17:18 -0400

    Optimize away intrinsics any() and all() when their inputs are known.
    If every argument passed to any() or all() is a compile-time constant,
    we now detect this and optimize away the function call entirely.
    
    Future CLs will perform a similar optimization on other intrinsic calls
    which can be detected and eliminated at compile time, like lessThan().
    
    Change-Id: Ie55aff538b1ccaf2b3bcf9a69573a85f081b7ade
    Bug: skia:10835
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/404417
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- tests/sksl/folding/BoolFolding.glsl
+++ tests/sksl/folding/BoolFolding.glsl
@@ -13,5 +13,9 @@ vec4 main() {
     bool _7_h = false;
     bool _8_i = true;
     bool _9_j = false;
-    return ((((((((_0_a && !_1_b) && _2_c) && !_3_d) && _4_e) && !_5_f) && _6_g) && !_7_h) && _8_i) && !_9_j ? colorGreen : colorRed;
+    bool _10_k = true;
+    bool _11_l = false;
+    bool _12_m = true;
+    bool _13_n = false;
+    return ((((((((((((_0_a && !_1_b) && _2_c) && !_3_d) && _4_e) && !_5_f) && _6_g) && !_7_h) && _8_i) && !_9_j) && _10_k) && !_11_l) && _12_m) && !_13_n ? colorGreen : colorRed;
 }
