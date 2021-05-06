c4f3f96ca30a30b9f61aa915a5cfaf30da220e7f
Michael Ludwig <michaelludwig@google.com>
2021-05-06 09:09:51 -0400

    Use Wang's formula for quadratic and cubic point counts
     - most of the small diffs are because I moved GrWangsFormula.h out
       of the tessellate/ directory and into the geometry/ directory since
       it's more general than HW tessellation.
    
    The previous implementation was based on the heuristic that the distance
    from the true curve to the line segment would be divided by 4 every time
    the curve was recursively subdivided. This was a reasonable
    approximation if the curve had balanced curvature on both sides of the
    split. However, in the case of the new GM's curve, the left half was
    already very linear and the right half had much higher curves.
    
    This lead to the approximation reporting fewer points than required.
    Theoretically, those few points that weren't utilized by the left half
    of the curve could have been made available to the right half, but the
    implementation of that would be tricky.
    
    Instead, it now uses Wang's formula to compute the number of points.
    Since recursive subdivision leads to linearly spaced samples assuming it
    can't stop early, this point count represents a valid upper bound on
    what's needed. It also then ensures both left and right halves of a
    curve have the point counts they might need w/o updating the
    generation implementations. However, since the recursive point
    generation exits once each section has reached the error tolerance, in
    scenarios where the prior approximation was reasonable, we'll end up
    using fewer points than reported by Wang's. Hopefully that means there
    is negligible performance regression since we won't be increasing
    vertex counts by that much (except where needed for correctness).
    
    Bug: skia:11886
    Change-Id: Iba39dbe4de82011775524583efd461b10c9259fe

--- src/gpu/geometry/GrPathUtils.cpp
+++ src/gpu/geometry/GrPathUtils.cpp
@@ -11,8 +11,19 @@
 #include "src/core/SkMathPriv.h"
 #include "src/core/SkPointPriv.h"
 #include "src/core/SkUtils.h"
+#include "src/gpu/geometry/GrWangsFormula.h"
 
 static const SkScalar gMinCurveTol = 0.0001f;
+
+static float tolerance_to_wangs_precision(float srcTol) {
+    // You should have called scaleToleranceToSrc, which guarantees this
+    SkASSERT(srcTol >= gMinCurveTol);
+
+    // The GrPathUtil API defines tolerance as the max distance the linear segment can be from
+    // the real curve. Wang's formula guarantees the linear segments will be within 1/precision
+    // of the true curve, so precision = 1/srcTol
+    return 1.f / srcTol;
+}
 
 SkScalar GrPathUtils::scaleToleranceToSrc(SkScalar devTol,
                                           const SkMatrix& viewM,
@@ -47,34 +58,10 @@ SkScalar GrPathUtils::scaleToleranceToSrc(SkScalar devTol,
 }
 
 uint32_t GrPathUtils::quadraticPointCount(const SkPoint points[], SkScalar tol) {
-    // You should have called scaleToleranceToSrc, which guarantees this
-    SkASSERT(tol >= gMinCurveTol);
-
-    SkScalar d = SkPointPriv::DistanceToLineSegmentBetween(points[1], points[0], points[2]);
-    if (!SkScalarIsFinite(d)) {
-        return kMaxPointsPerCurve;
-    } else if (d <= tol) {
-        return 1;
-    } else {
-        // Each time we subdivide, d should be cut in 4. So we need to
-        // subdivide x = log4(d/tol) times. x subdivisions creates 2^(x)
-        // points.
-        // 2^(log4(x)) = sqrt(x);
-        SkScalar divSqrt = SkScalarSqrt(d / tol);
-        if (((SkScalar)SK_MaxS32) <= divSqrt) {
-            return kMaxPointsPerCurve;
-        } else {
-            int temp = SkScalarCeilToInt(divSqrt);
-            int pow2 = GrNextPow2(temp);
-            // Because of NaNs & INFs we can wind up with a degenerate temp
-            // such that pow2 comes out negative. Also, our point generator
-            // will always output at least one pt.
-            if (pow2 < 1) {
-                pow2 = 1;
-            }
-            return std::min(pow2, kMaxPointsPerCurve);
-        }
-    }
+    // quadratic_log2 bit-shifted to the left gives us the next power of two greater than
+    // calling GrWangsFormula::quadratic w/o having to compute a 4th root.
+    int wangsLog2 = GrWangsFormula::quadratic_log2(tolerance_to_wangs_precision(tol), points);
+    return std::min(1 << wangsLog2, kMaxPointsPerCurve);
 }
 
 uint32_t GrPathUtils::generateQuadraticPoints(const SkPoint& p0,
@@ -102,35 +89,9 @@ uint32_t GrPathUtils::generateQuadraticPoints(const SkPoint& p0,
     return a + b;
 }
 
-uint32_t GrPathUtils::cubicPointCount(const SkPoint points[],
-                                           SkScalar tol) {
-    // You should have called scaleToleranceToSrc, which guarantees this
-    SkASSERT(tol >= gMinCurveTol);
-
-    SkScalar d = std::max(
-        SkPointPriv::DistanceToLineSegmentBetweenSqd(points[1], points[0], points[3]),
-        SkPointPriv::DistanceToLineSegmentBetweenSqd(points[2], points[0], points[3]));
-    d = SkScalarSqrt(d);
-    if (!SkScalarIsFinite(d)) {
-        return kMaxPointsPerCurve;
-    } else if (d <= tol) {
-        return 1;
-    } else {
-        SkScalar divSqrt = SkScalarSqrt(d / tol);
-        if (((SkScalar)SK_MaxS32) <= divSqrt) {
-            return kMaxPointsPerCurve;
-        } else {
-            int temp = SkScalarCeilToInt(SkScalarSqrt(d / tol));
-            int pow2 = GrNextPow2(temp);
-            // Because of NaNs & INFs we can wind up with a degenerate temp
-            // such that pow2 comes out negative. Also, our point generator
-            // will always output at least one pt.
-            if (pow2 < 1) {
-                pow2 = 1;
-            }
-            return std::min(pow2, kMaxPointsPerCurve);
-        }
-    }
+uint32_t GrPathUtils::cubicPointCount(const SkPoint points[], SkScalar tol) {
+    int wangsLog2 = GrWangsFormula::cubic_log2(tolerance_to_wangs_precision(tol), points);
+    return std::min(1 << wangsLog2, kMaxPointsPerCurve);
 }
 
 uint32_t GrPathUtils::generateCubicPoints(const SkPoint& p0,
