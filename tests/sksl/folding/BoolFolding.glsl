1dc2d0fe0fa05573f58c64ca522132c94f73674d
John Stiles <johnstiles@google.com>
2021-05-06 23:55:19 -0400

    Allow compile-time intrinsic evaluation to read const variables.
    Previously, the code neglected to resolve constant variables into
    values. This meant that expressions like `lessThan(zero, one)` could not
    be compile-time evaluated even when `zero` and `one` have known values.
    
    Change-Id: I2f5ce303e3dcc682be14e4d2485e24dd7c59212e
    Bug: skia:10835
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/405536
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

--- tests/sksl/folding/BoolFolding.glsl
+++ tests/sksl/folding/BoolFolding.glsl
@@ -13,9 +13,9 @@ vec4 main() {
     bool _7_h = false;
     bool _8_i = true;
     bool _9_j = false;
-    bool _10_k = true;
-    bool _11_l = false;
-    bool _12_m = true;
-    bool _13_n = false;
-    return ((((((((((((_0_a && !_1_b) && _2_c) && !_3_d) && _4_e) && !_5_f) && _6_g) && !_7_h) && _8_i) && !_9_j) && _10_k) && !_11_l) && _12_m) && !_13_n ? colorGreen : colorRed;
+    bool _12_k = true;
+    bool _13_l = false;
+    bool _14_m = true;
+    bool _15_n = false;
+    return ((((((((((((_0_a && !_1_b) && _2_c) && !_3_d) && _4_e) && !_5_f) && _6_g) && !_7_h) && _8_i) && !_9_j) && _12_k) && !_13_l) && _14_m) && !_15_n ? colorGreen : colorRed;
 }
