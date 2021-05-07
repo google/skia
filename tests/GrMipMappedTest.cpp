1180453e7c9930b407bc1750ee60d4ad88f1b72d
Jim Van Verth <jvanverth@google.com>
2021-05-07 15:47:19 -0400

    Direct3D: enable MipmappedTest
    Change-Id: I9c3cad96b40491a54cab4edb8f56639d6cc16665
    Bug: skia:10446

--- tests/GrMipMappedTest.cpp
+++ tests/GrMipMappedTest.cpp
@@ -12,6 +12,9 @@
 #include "include/core/SkSurface.h"
 #include "include/gpu/GrBackendSurface.h"
 #include "include/gpu/GrDirectContext.h"
+#ifdef SK_DIRECT3D
+#include "include/gpu/d3d/GrD3DTypes.h"
+#endif
 #include "src/gpu/GrBackendTextureImageGenerator.h"
 #include "src/gpu/GrDirectContextPriv.h"
 #include "src/gpu/GrDrawingManager.h"
@@ -227,6 +230,24 @@ DEF_GPUTEST_FOR_RENDERING_CONTEXTS(GrBackendTextureImageMipMappedTest, reporter,
                         REPORTER_ASSERT(reporter, origImageInfo.fTexture != genImageInfo.fTexture);
                     } else {
                         REPORTER_ASSERT(reporter, origImageInfo.fTexture == genImageInfo.fTexture);
+                    }
+                } else {
+                    ERRORF(reporter, "Failed to get GrMtlTextureInfo");
+                }
+#endif
+#ifdef SK_DIRECT3D
+            } else if (GrBackendApi::kDirect3D == genBackendTex.backend()) {
+                GrD3DTextureResourceInfo genImageInfo;
+                GrD3DTextureResourceInfo origImageInfo;
+                if (genBackendTex.getD3DTextureResourceInfo(&genImageInfo) &&
+                    backendTex.getD3DTextureResourceInfo(&origImageInfo)) {
+                    if (requestMipmapped == GrMipmapped::kYes && betMipmapped == GrMipmapped::kNo) {
+                        // We did a copy so the texture resources should be different
+                        REPORTER_ASSERT(reporter,
+                                        origImageInfo.fResource != genImageInfo.fResource);
+                    } else {
+                        REPORTER_ASSERT(reporter,
+                                        origImageInfo.fResource == genImageInfo.fResource);
                     }
                 } else {
                     ERRORF(reporter, "Failed to get GrMtlTextureInfo");
