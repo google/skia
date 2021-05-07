c887fa6c2c512340608436a7b6d9788099658d62
John Stiles <johnstiles@google.com>
2021-05-06 23:58:44 -0400

    Evaluate various single-argument float intrinsics at compile time.
    Change-Id: I539f918294332310dcd6fe12fab163c0b6216f65

--- tests/sksl/intrinsics/Log.glsl
+++ tests/sksl/intrinsics/Log.glsl
@@ -1,6 +1,9 @@
 
 out vec4 sk_FragColor;
-uniform float a;
-void main() {
-    sk_FragColor.x = log(a);
+uniform vec4 input;
+uniform vec4 expected;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    return ((((((log(input.x) == expected.x && log(input.xy) == expected.xy) && log(input.xyz) == expected.xyz) && log(input) == expected) && -inf.0 == expected.x) && vec2(-inf.0, -inf.0) == expected.xy) && vec3(-inf.0, -inf.0, -inf.0) == expected.xyz) && vec4(-inf.0, -inf.0, -inf.0, -inf.0) == expected ? colorGreen : colorRed;
 }
