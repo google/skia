614fb1ef9c88a16b9326511498824ccb540614f9
Ethan Nicholas <ethannicholas@google.com>
2021-05-10 16:13:10 -0400

    Removed early_fragment_tests layout qualifier
    This layout qualifier is not actually used anywhere.
    
    Change-Id: I817c9affdd00e492c70f251eb52680644b7ff3f7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/406141
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- include/private/SkSLLayout.h
+++ include/private/SkSLLayout.h
@@ -21,26 +21,25 @@ struct Layout {
     enum Flag {
         kOriginUpperLeft_Flag            = 1 <<  0,
         kOverrideCoverage_Flag           = 1 <<  1,
-        kEarlyFragmentTests_Flag         = 1 <<  2,
-        kPushConstant_Flag               = 1 <<  3,
-        kBlendSupportAllEquations_Flag   = 1 <<  4,
-        kTracked_Flag                    = 1 <<  5,
-        kSRGBUnpremul_Flag               = 1 <<  6,
-        kKey_Flag                        = 1 <<  7,
+        kPushConstant_Flag               = 1 <<  2,
+        kBlendSupportAllEquations_Flag   = 1 <<  3,
+        kTracked_Flag                    = 1 <<  4,
+        kSRGBUnpremul_Flag               = 1 <<  5,
+        kKey_Flag                        = 1 <<  6,
 
         // These flags indicate if the qualifier appeared, regardless of the accompanying value.
-        kLocation_Flag                   = 1 <<  8,
-        kOffset_Flag                     = 1 <<  9,
-        kBinding_Flag                    = 1 << 10,
-        kIndex_Flag                      = 1 << 11,
-        kSet_Flag                        = 1 << 12,
-        kBuiltin_Flag                    = 1 << 13,
-        kInputAttachmentIndex_Flag       = 1 << 14,
-        kPrimitive_Flag                  = 1 << 15,
-        kMaxVertices_Flag                = 1 << 16,
-        kInvocations_Flag                = 1 << 17,
-        kWhen_Flag                       = 1 << 18,
-        kCType_Flag                      = 1 << 19,
+        kLocation_Flag                   = 1 <<  7,
+        kOffset_Flag                     = 1 <<  8,
+        kBinding_Flag                    = 1 <<  9,
+        kIndex_Flag                      = 1 << 10,
+        kSet_Flag                        = 1 << 11,
+        kBuiltin_Flag                    = 1 << 12,
+        kInputAttachmentIndex_Flag       = 1 << 13,
+        kPrimitive_Flag                  = 1 << 14,
+        kMaxVertices_Flag                = 1 << 15,
+        kInvocations_Flag                = 1 << 16,
+        kWhen_Flag                       = 1 << 17,
+        kCType_Flag                      = 1 << 18,
     };
 
     enum Primitive {
@@ -183,9 +182,6 @@ struct Layout {
         }
         if (fFlags & kOverrideCoverage_Flag) {
             result += separator() + "override_coverage";
-        }
-        if (fFlags & kEarlyFragmentTests_Flag) {
-            result += separator() + "early_fragment_tests";
         }
         if (fFlags & kBlendSupportAllEquations_Flag) {
             result += separator() + "blend_support_all_equations";
