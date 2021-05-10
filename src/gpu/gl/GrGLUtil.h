f2f7caeed77b3b6bf8f8bcc8155b1cf53330dd61
Adlai Holler <adlai@google.com>
2021-05-10 18:03:42 -0400

    Disable CCPR on Radeon Vega
    Not to be confused with Radeon Pro Vega, the Vega 3
    on our bots has trouble with CCPR.
    
    Bug: skia:11899
    Change-Id: I008d897b88f4dd3b8493254ff2228c5fd331cc42

--- src/gpu/gl/GrGLUtil.h
+++ src/gpu/gl/GrGLUtil.h
@@ -142,6 +142,7 @@ enum class GrGLRenderer {
     kAMDRadeonR9M4xx,     // AMD Radeon R9 M400 Series
     kAMDRadeonPro5xxx,    // AMD Radeon Pro 5000 Series
     kAMDRadeonProVegaxx,  // AMD Radeon Pro Vega
+    kAMDRadeonVegaxx,     // AMD Radeon Vega
 
     kOther
 };
