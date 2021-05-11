3ad0980f29bd5d64449096751ed7824fb1e042f2
John Stiles <johnstiles@google.com>
2021-05-10 22:34:44 -0400

    Optimize remaining simple 1-argument intrinsics.
    Aside from sqrt() and normalize(), we now optimize all the intrinsics
    which take a single argument as input, and return that argument with
    each of its components permuted as output.
    
    This CL also introduces a minor restriction--we no longer optimize
    intrinsics which evaluate to inf or nan, such as `inversesqrt(-1)`.
    These will be left in the source as-is.
    
    Change-Id: I4919b3c18a2df81accd6daf2f650b9f587ff43fc

--- tests/sksl/intrinsics/Degrees.glsl
+++ tests/sksl/intrinsics/Degrees.glsl
@@ -1,6 +1,9 @@
 
 out vec4 sk_FragColor;
-uniform float a;
-void main() {
-    sk_FragColor.x = degrees(a);
+uniform vec4 input;
+uniform vec4 expected;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    return ((((((degrees(input.x) == expected.x && degrees(input.xy) == expected.xy) && degrees(input.xyz) == expected.xyz) && degrees(input) == expected) && 90.0 == expected.x) && vec2(90.0, 180.0) == expected.xy) && vec3(90.0, 180.0, 270.0) == expected.xyz) && vec4(90.0, 180.0, 270.0, 360.0) == expected ? colorGreen : colorRed;
 }
--- tests/sksl/intrinsics/Inversesqrt.glsl
+++ tests/sksl/intrinsics/Inversesqrt.glsl
@@ -1,6 +1,10 @@
 
 out vec4 sk_FragColor;
-uniform float a;
-void main() {
-    sk_FragColor.x = inversesqrt(a);
+uniform vec4 input;
+uniform vec4 expected;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    const vec4 negativeVal = vec4(-1.0, -4.0, -16.0, -64.0);
+    return ((((((((((inversesqrt(input.x) == expected.x && inversesqrt(input.xy) == expected.xy) && inversesqrt(input.xyz) == expected.xyz) && inversesqrt(input) == expected) && 1.0 == expected.x) && vec2(1.0, 0.5) == expected.xy) && vec3(1.0, 0.5, 0.25) == expected.xyz) && vec4(1.0, 0.5, 0.25, 0.125) == expected) && inversesqrt(-1.0) == expected.x) && inversesqrt(vec2(-1.0, -4.0)) == expected.xy) && inversesqrt(vec3(-1.0, -4.0, -16.0)) == expected.xyz) && inversesqrt(negativeVal) == expected ? colorGreen : colorRed;
 }
