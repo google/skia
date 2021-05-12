6357293588856f7ccdc90d5e2c517f903c0a4b02
Robert Kroeger <rjkroege@google.com>
2021-05-12 17:48:45 +0000

    Revert "When running on ANGLE GL backend use the underlying GL driver info"
    This reverts commit bb006430ae8234762cfef89a08919b4690af1b49.
    
    Reason for revert: https://crbug.com/1208437 in Chrome: multiple
    breakage on Android devices.
    
    Original change's description:
    > When running on ANGLE GL backend use the underlying GL driver info
    > to setup caps.
    >
    > Bug: 1203705
    > Change-Id: Ia50dfc6aef44848d81c735dc98dafdc692e8da40
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/406117
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Brian Salomon <bsalomon@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1203705
    Change-Id: Idb3183ddb8c2564005871c27d19feff6c4292d7f

--- src/gpu/gl/GrGLContext.h
+++ src/gpu/gl/GrGLContext.h
@@ -23,8 +23,8 @@ struct GrContextOptions;
  */
 class GrGLContextInfo {
 public:
-    GrGLContextInfo(GrGLContextInfo&&) = default;
-    GrGLContextInfo& operator=(GrGLContextInfo&&) = default;
+    GrGLContextInfo(const GrGLContextInfo&) = delete;
+    GrGLContextInfo& operator=(const GrGLContextInfo&) = delete;
 
     virtual ~GrGLContextInfo() {}
 
@@ -48,17 +48,7 @@ public:
 
     const GrGLExtensions& extensions() const { return fInterface->fExtensions; }
 
-    /**
-     * Makes a version of this context info that strips the "angle-ness". It will report kUnknown
-     * for angleBackend() and report this info's angleRenderer() as renderer() and similiar for
-     * driver(), driverVersion(), and vendor().
-     */
-    GrGLContextInfo makeNonAngle() const;
-
 protected:
-    GrGLContextInfo& operator=(const GrGLContextInfo&) = default;
-    GrGLContextInfo(const GrGLContextInfo&) = default;
-
     struct ConstructorArgs {
         sk_sp<const GrGLInterface>          fInterface;
         GrGLDriverInfo                      fDriverInfo;
