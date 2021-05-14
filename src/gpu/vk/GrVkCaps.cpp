192bb4bfd1bbd060aa395860e2e157ea285c308d
Chris Dalton <csmartdalton@google.com>
2021-05-13 22:37:49 -0600

    Reland "Delete the index buffer from middle-out tessellation"
    This is a reland of 0d0b1b3b56da6d13506d34756c79a79c4929950f
    
    Original change's description:
    > Delete the index buffer from middle-out tessellation
    >
    > This gives us more flexibility for customizing triangulations in
    > future modes. It is also hopefully cheaper than the extra memory
    > indirection from indexed draws.
    >
    > Bug: skia:10419
    > Bug: chromium:1202607
    > Change-Id: Iba41a35a634edf8f962c3d604c7e035e7a85801d
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/407296
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    
    Bug: skia:10419
    Bug: chromium:1202607
    Change-Id: I73487087fba09d3ab95d8fa8bd4e2435ba802075

--- src/gpu/vk/GrVkCaps.cpp
+++ src/gpu/vk/GrVkCaps.cpp
@@ -716,7 +716,7 @@ void GrVkCaps::initShaderCaps(const VkPhysicalDeviceProperties& properties,
     shaderCaps->fIntegerSupport = true;
     shaderCaps->fNonsquareMatrixSupport = true;
     shaderCaps->fVertexIDSupport = true;
-    shaderCaps->fFPManipulationSupport = true;
+    shaderCaps->fBitManipulationSupport = true;
 
     // Assume the minimum precisions mandated by the SPIR-V spec.
     shaderCaps->fFloatIs32Bits = true;
