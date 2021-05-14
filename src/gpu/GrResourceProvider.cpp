32321534e1633d561a24b994ebd3cd5c0ce1f327
Greg Daniel <egdaniel@google.com>
2021-05-14 12:51:50 -0400

    Round up DMSAA attachments to pow2 if supported.
    Change-Id: I03f5ea649209b9894753c981872edbd7d718288c

--- src/gpu/GrResourceProvider.cpp
+++ src/gpu/GrResourceProvider.cpp
@@ -516,7 +516,7 @@ bool GrResourceProvider::attachStencilAttachment(GrRenderTarget* rt, bool useMSA
         GrUniqueKey sbKey;
 
 #if 0
-        if (this->caps()->oversizedStencilSupport()) {
+        if (this->caps()->oversizedAttachmentSupport()) {
             width  = SkNextPow2(width);
             height = SkNextPow2(height);
         }
@@ -548,6 +548,7 @@ bool GrResourceProvider::attachStencilAttachment(GrRenderTarget* rt, bool useMSA
              stencil->numSamples() == num_stencil_samples(rt, useMSAASurface, *this->caps()));
     return stencil != nullptr;
 }
+
 sk_sp<GrAttachment> GrResourceProvider::getDiscardableMSAAAttachment(SkISize dimensions,
                                                                      const GrBackendFormat& format,
                                                                      int sampleCnt,
@@ -559,6 +560,8 @@ sk_sp<GrAttachment> GrResourceProvider::getDiscardableMSAAAttachment(SkISize dim
     if (this->isAbandoned()) {
         return nullptr;
     }
+
+    dimensions = MakeApprox(dimensions);
 
     if (!fCaps->validateSurfaceParams(
                 dimensions, format, GrRenderable::kYes, sampleCnt, GrMipmapped::kNo)) {
@@ -578,6 +581,7 @@ sk_sp<GrAttachment> GrResourceProvider::getDiscardableMSAAAttachment(SkISize dim
     if (msaaAttachment) {
         return msaaAttachment;
     }
+
     msaaAttachment = this->makeMSAAAttachment(dimensions, format, sampleCnt, isProtected);
     if (msaaAttachment) {
         this->assignUniqueKeyToResource(key, msaaAttachment.get());
