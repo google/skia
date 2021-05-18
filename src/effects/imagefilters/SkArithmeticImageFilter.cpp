77a5bc32126d56682b5351dd0690a5e18b3e1742
Brian Osman <brianosman@google.com>
2021-05-18 16:24:15 -0400

    Remove GrArithmeticProcessor, use GrRuntimeFPBuilder instead
    This also converts "enforcePMColor" to a uniform (rather than baking it
    in). It's easier to write this way, and I can't imagine it made that
    much difference to performance.
    
    Change-Id: Idd202678740077412a2fb4e5122ebb9118f1d7bf
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/410096
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- src/effects/imagefilters/SkArithmeticImageFilter.cpp
+++ src/effects/imagefilters/SkArithmeticImageFilter.cpp
@@ -23,11 +23,7 @@
 #include "src/gpu/GrSurfaceDrawContext.h"
 #include "src/gpu/GrTextureProxy.h"
 #include "src/gpu/SkGr.h"
-#include "src/gpu/effects/generated/GrArithmeticProcessor.h"
-#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
-#include "src/gpu/glsl/GrGLSLFragmentShaderBuilder.h"
-#include "src/gpu/glsl/GrGLSLProgramDataManager.h"
-#include "src/gpu/glsl/GrGLSLUniformHandler.h"
+#include "src/gpu/effects/GrSkSLFP.h"
 #endif
 
 namespace {
@@ -307,6 +303,35 @@ SkIRect SkArithmeticImageFilter::onFilterBounds(const SkIRect& src,
 
 #if SK_SUPPORT_GPU
 
+std::unique_ptr<GrFragmentProcessor> make_arithmetic_fp(
+        std::unique_ptr<GrFragmentProcessor> srcFP,
+        std::unique_ptr<GrFragmentProcessor> dstFP,
+        const SkV4& k,
+        bool enforcePMColor) {
+    static constexpr char kCode[] = R"(
+        uniform shader srcFP;
+        uniform shader dstFP;
+        uniform half4 k;
+        uniform half pmClamp;
+        half4 main(float2 xy) {
+            half4 src = sample(srcFP, xy);
+            half4 dst = sample(dstFP, xy);
+            half4 color = saturate(k.x * src * dst +
+                                   k.y * src +
+                                   k.z * dst +
+                                   k.w);
+            color.rgb = min(color.rgb, max(color.a, pmClamp));
+            return color;
+        }
+    )";
+    auto builder = GrRuntimeFPBuilder::Make<kCode, SkRuntimeEffect::MakeForShader>();
+    builder.child("srcFP") = std::move(srcFP);
+    builder.child("dstFP") = std::move(dstFP);
+    builder.uniform("k") = k;
+    builder.uniform("pmClamp") = enforcePMColor ? 0.0f : 1.0f;
+    return builder.makeFP();
+}
+
 sk_sp<SkSpecialImage> SkArithmeticImageFilter::filterImageGPU(
         const Context& ctx,
         sk_sp<SkSpecialImage> background,
@@ -374,7 +399,7 @@ sk_sp<SkSpecialImage> SkArithmeticImageFilter::filterImageGPU(
                                              foreground->alphaType(),
                                              ctx.colorSpace(),
                                              kPremul_SkAlphaType);
-        fp = GrArithmeticProcessor::Make(std::move(fgFP), std::move(fp), fK, fEnforcePMColor);
+        fp = make_arithmetic_fp(std::move(fgFP), std::move(fp), fK, fEnforcePMColor);
     }
 
     GrImageInfo info(ctx.grColorType(), kPremul_SkAlphaType, ctx.refColorSpace(), bounds.size());
