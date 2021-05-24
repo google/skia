45215208ae3c96ef2603c2edad2ea7cbeab904ac
Ethan Nicholas <ethannicholas@google.com>
2021-05-24 13:04:18 -0400

    Added skstd::optional
    This is needed by the upcoming DSLParser.
    
    Change-Id: I54a0714e55feeb78894df766b14c795970f2c2d4

---
+++ tests/SkTOptionalTest.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/private/SkTArray.h"
+#include "include/private/SkTOptional.h"
+
+#include "tests/Test.h"
+
+DEF_TEST(SkTOptional, r) {
+    {
+        skstd::optional<int> o;
+        REPORTER_ASSERT(r, !o);
+    }
+
+    {
+        skstd::optional<const char*> o("test");
+        REPORTER_ASSERT(r, o);
+        REPORTER_ASSERT(r, !strcmp(*o, "test"));
+    }
+
+    {
+        skstd::optional<SkTArray<int>> o;
+        REPORTER_ASSERT(r, !o);
+
+        o = skstd::optional<SkTArray<int>>(50);
+        REPORTER_ASSERT(r, o);
+        REPORTER_ASSERT(r, o->capacity() == 50);
+
+        o = skstd::optional<SkTArray<int>>(SkTArray<int>(100));
+        REPORTER_ASSERT(r, o);
+        REPORTER_ASSERT(r, o->capacity() == 100);
+
+        o = skstd::optional<SkTArray<int>>();
+        REPORTER_ASSERT(r, !o);
+    }
+}
