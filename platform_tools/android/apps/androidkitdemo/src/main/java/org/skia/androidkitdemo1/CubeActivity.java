addc0c81e267f8533c0ce9857422a13ac06f9034
Jorge Betancourt <jmbetancourt@google.com>
2021-05-26 13:19:24 -0400

    [WIP] light cube faces on AndroidKit demo
    Change-Id: I7c126a922f1b54311d2e95814d83fd885c53a9ec

--- platform_tools/android/apps/androidkitdemo/src/main/java/org/skia/androidkitdemo1/CubeActivity.java
+++ platform_tools/android/apps/androidkitdemo/src/main/java/org/skia/androidkitdemo1/CubeActivity.java
@@ -19,6 +19,7 @@ import org.skia.androidkit.Color;
 import org.skia.androidkit.Image;
 import org.skia.androidkit.Matrix;
 import org.skia.androidkit.Paint;
+import org.skia.androidkit.RuntimeShaderBuilder;
 import org.skia.androidkit.SamplingOptions;
 import org.skia.androidkit.Shader;
 import org.skia.androidkit.Surface;
@@ -44,6 +45,33 @@ class Face {
 }
 
 class CubeRenderer extends SurfaceRenderer {
+    private static final String lightingShader =
+    "uniform shader color_map;                                              " +
+    "uniform shader normal_map;                                             " +
+
+    "uniform float4x4 localToWorld;                                         " +
+    "uniform float4x4 localToWorldAdjInv;                                   " +
+    "uniform float3   lightPos;                                             " +
+
+    "float3 convert_normal_sample(half4 c) {                                " +
+    "    float3 n = 2 * c.rgb - 1;                                          " +
+    "    n.y = -n.y;                                                        " +
+    "    return n;                                                          " +
+    "}                                                                      " +
+
+    "half4 main(float2 p) {                                                 " +
+    "    float3 norm = convert_normal_sample(sample(normal_map, p));        " +
+    "    float3 plane_norm = normalize(localToWorldAdjInv * norm.xyz0).xyz; " +
+
+    "    float3 plane_pos = (localToWorld * p.xy01).xyz;                    " +
+    "    float3 light_dir = normalize(lightPos - plane_pos);                " +
+
+    "    float ambient = 0.2;                                               " +
+    "    float dp = dot(plane_norm, light_dir);                             " +
+    "    float scale = min(ambient + max(dp, 0), 1);                        " +
+
+    "    return sample(color_map, p) * scale.xxx1;                          " +
+    "}";
     // TODO: make these relative to surface size
     private float mCubeSideLength = 500;
     private int DX = 200;
@@ -60,18 +88,18 @@ class CubeRenderer extends SurfaceRenderer {
     private Face[] faces;
 
     public CubeRenderer(Resources res) {
-        Paint brickPaint = new Paint();
-
-        try {
-            Image image = Image.fromStream(res.openRawResource(R.raw.brickwork_texture));
-            Shader shader =
-                image.makeShader(TileMode.REPEAT, TileMode.REPEAT,
-                                 new SamplingOptions(SamplingOptions.FilterMode.LINEAR));
-            brickPaint.setShader(shader);
-        } catch (Exception e) {}
+//        Paint brickPaint = new Paint();
+//
+//        try {
+//            Image image = Image.fromStream(res.openRawResource(R.raw.brickwork_texture));
+//            Shader shader =
+//                image.makeShader(TileMode.REPEAT, TileMode.REPEAT,
+//                                 new SamplingOptions(SamplingOptions.FilterMode.LINEAR));
+//            brickPaint.setShader(shader);
+//        } catch (Exception e) {}
 
         faces = new Face[] {
-            new Face(0, -rot/2, brickPaint),
+            new Face(0, -rot/2, new Paint().setColor(new Color(1, 0, 1, 1))),
             new Face(0, 0     , new Paint().setColor(new Color(1, 0, 0, 1))),
             new Face(0, rot   , new Paint().setColor(new Color(0, 1, 0, 1))),
             new Face(rot/2, 0 , new Paint().setColor(new Color(0, 0, 1, 1))),
@@ -113,7 +141,20 @@ class CubeRenderer extends SurfaceRenderer {
             canvas.concat(localToWorld);
 
             if (front(canvas.getLocalToDevice())) {
-                canvas.drawRect(0, 0, mCubeSideLength, mCubeSideLength, f.paint);
+                RuntimeShaderBuilder builder = new RuntimeShaderBuilder(lightingShader);
+                builder.setUniform("lightPos", 0, 0, 0);
+                builder.setUniform("localToWorld", localToWorld);
+                float[] a = localToWorld.getRowMajor();
+                Matrix inverse = Matrix.makeInverse(new Matrix(a[0],  a[1],  a[2],  0,
+                                                               a[4],  a[5],  a[6],  0,
+                                                               a[8],  a[9],  a[10], 0,
+                                                               0,     0,     0,     1));
+                Matrix normals = Matrix.makeTranspose(inverse);
+                builder.setUniform("localToWorldAdjInv", normals);
+                Paint p = new Paint();
+                p.setShader(builder.makeShader());
+                p.setColor(new Color(1, 0, 0, 1));
+                canvas.drawRect(0, 0, mCubeSideLength, mCubeSideLength, p);
             }
             canvas.restore();
         }
