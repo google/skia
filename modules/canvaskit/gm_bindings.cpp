afe6788b4e5f3f377190886a1b6cd75866f9dc1a
Kevin Lubick <kjlubick@google.com>
2021-05-26 13:40:22 -0400

    [canvaskit] Introduce self-documenting pointer types
    Change-Id: I72dfe6098dae699d85cf87b6432e27cf845c832d

--- modules/canvaskit/gm_bindings.cpp
+++ modules/canvaskit/gm_bindings.cpp
@@ -94,7 +94,7 @@ static sk_sp<SkData> getResource(const char* name) {
   return it->second;
 }
 
-static void LoadResource(std::string name, uintptr_t /* byte* */ bPtr, size_t len) {
+static void LoadResource(std::string name, WASMPointerU8 bPtr, size_t len) {
   const uint8_t* bytes = reinterpret_cast<const uint8_t*>(bPtr);
   auto data = SkData::MakeFromMalloc(bytes, len);
   gResources[name] = std::move(data);
