a9452f17b6642d0f773b1280e61749c3dad1a131
Brian Salomon <bsalomon@google.com>
2021-05-27 15:05:09 -0400

    One function for computing rt-adjust uniform once flip is determined.
    Add some comments explaining why different APIs flip the way they do.
    
    Remove flip from Metal code generator and reverse the way flipping
    occurs in Metal backend.
    
    Change-Id: Ibec71e8d96c66d4b34b5ce6417056674090f979c

--- src/gpu/mtl/GrMtlPipelineState.mm
+++ src/gpu/mtl/GrMtlPipelineState.mm
@@ -146,9 +146,12 @@ void GrMtlPipelineState::setRenderTargetState(const GrRenderTarget* rt, GrSurfac
         fRenderTargetState.fRenderTargetSize = dimensions;
         fRenderTargetState.fRenderTargetOrigin = origin;
 
-        float rtAdjustmentVec[4];
-        fRenderTargetState.getRTAdjustmentVec(rtAdjustmentVec);
-        fDataManager.set4fv(fBuiltinUniformHandles.fRTAdjustmentUni, 1, rtAdjustmentVec);
+        // The client will mark a swap buffer as kTopLeft when making a SkSurface because
+        // Metal's framebuffer space has (0, 0) at the top left. This agrees with Skia's device
+        // coords. However, in NDC (-1, -1) is the bottom left. So we flip when origin is kTopLeft.
+        bool flip = (origin == kTopLeft_GrSurfaceOrigin);
+        std::array<float, 4> v = SkSL::Compiler::GetRTAdjustVector(dimensions, flip);
+        fDataManager.set4fv(fBuiltinUniformHandles.fRTAdjustmentUni, 1, v.data());
     }
 }
 
