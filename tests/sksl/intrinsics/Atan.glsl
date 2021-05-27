caadec4796e5f88161112c73e3010a18627fd98e
John Stiles <johnstiles@google.com>
2021-05-27 19:49:56 -0400

    Implement compile-time optimization for atan().
    This is one of the few compile-time constant functions with a variable
    number of arguments, but it's otherwise pretty normal as intrinsics go.
    
    Change-Id: Ie852e10f37d73d53f69e806550872bc015f802d6
    Bug: skia:12034

--- tests/sksl/intrinsics/Atan.glsl
+++ tests/sksl/intrinsics/Atan.glsl
@@ -1,12 +1,10 @@
 
 out vec4 sk_FragColor;
-uniform float a;
-uniform float b;
-uniform vec4 c;
-uniform vec4 d;
-void main() {
-    sk_FragColor.x = atan(a);
-    sk_FragColor.x = atan(a, b);
-    sk_FragColor = atan(c);
-    sk_FragColor = atan(c, d);
+uniform vec4 input;
+uniform vec4 expected;
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    const vec4 constVal2 = vec4(1.0);
+    return ((((((((((((((atan(input.x) == expected.x && atan(input.xy) == expected.xy) && atan(input.xyz) == expected.xyz) && atan(input) == expected) && 0.0 == expected.x) && vec2(0.0, 0.0) == expected.xy) && vec3(0.0, 0.0, 0.0) == expected.xyz) && vec4(0.0, 0.0, 0.0, 0.0) == expected) && atan(input.x, 1.0) == expected.x) && atan(input.xy, vec2(1.0)) == expected.xy) && atan(input.xyz, vec3(1.0)) == expected.xyz) && atan(input, constVal2) == expected) && 0.0 == expected.x) && vec2(0.0, 0.0) == expected.xy) && vec3(0.0, 0.0, 0.0) == expected.xyz) && vec4(0.0, 0.0, 0.0, 0.0) == expected ? colorGreen : colorRed;
 }
