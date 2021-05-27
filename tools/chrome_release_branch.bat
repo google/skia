f5a382bf90b5da7d8a8d389ba35d89ef74a975cd
Eric Boren <borenet@google.com>
2021-05-27 09:29:23 -0400

    [infra] Delete bin/try* and tools/chrome_release_branch*
    These have been replaced by `sk try` and `sk release-branch`,
    respectively.
    
    Change-Id: Idc297dd7c84a87a09f69f310de495f91fcce36b9

--- tools/chrome_release_branch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Copyright 2019 Google Inc.
-::
-:: Use of this source code is governed by a BSD-style license that can be
-:: found in the LICENSE file.
-setlocal
-python -u "%~dp0\chrome_release_branch.py" %*
