1c155651c4ecede7ed8577f0cd312ca42144b63c
Leon Scroggins III <scroggo@google.com>
2021-06-01 13:23:13 -0400

    SkWuffsCodec: ignore too much pixel data
    Bug: skia:12055
    Bug: chromium:1213633
    Bug: chromium:1214134
    
    The old decoder was lenient on images with extra pixel data, so use
    WUFFS_GIF__QUIRK_IGNORE_TOO_MUCH_PIXEL_DATA to match the behavior.
    
    Change-Id: I626e8cad4d2466d3a076b9fed659e96b6373cb80
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/414417
    Commit-Queue: Leon Scroggins <scroggo@google.com>
    Reviewed-by: Nigel Tao <nigeltao@google.com>
    (cherry picked from commit 816833dfde8a45ea2704025b7d1588aa2199c9bb)

--- src/codec/SkWuffsCodec.cpp
+++ src/codec/SkWuffsCodec.cpp
@@ -113,6 +113,10 @@ static SkCodec::Result reset_and_decode_image_config(wuffs_gif__decoder*       d
         SkCodecPrintf("initialize: %s", status.message());
         return SkCodec::kInternalError;
     }
+
+    // See https://bugs.chromium.org/p/skia/issues/detail?id=12055
+    decoder->set_quirk_enabled(WUFFS_GIF__QUIRK_IGNORE_TOO_MUCH_PIXEL_DATA, true);
+
     while (true) {
         status = decoder->decode_image_config(imgcfg, b);
         if (status.repr == nullptr) {
