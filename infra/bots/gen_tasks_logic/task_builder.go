a0659891e86fd56f622e0b7f79802e5495aa71f5
Eric Boren <borenet@google.com>
2021-06-01 15:20:58 -0400

    [infra] Roll Infra dep to update CIPD packages
    This is intended to pick up https://skia-review.googlesource.com/c/buildbot/+/414458 which updates the tasks to use cpython3, but it also updates the luci-auth, kitchen, and git packages.
    
    Also includes fixes needed after https://skia-review.googlesource.com/c/buildbot/+/387896
    
    Bug: chromium:1210114
    Change-Id: I2003be762b59a08a6b972b6563e4f3a9920b082d

--- infra/bots/gen_tasks_logic/task_builder.go
+++ infra/bots/gen_tasks_logic/task_builder.go
@@ -284,7 +284,7 @@ func (b *taskBuilder) usesPython() {
 	// TODO(borenet): This handling of the Python package is hacky and bad.
 	pythonPkgs := cipd.PkgsPython[b.cipdPlatform()]
 	b.cipd(pythonPkgs[1])
-	if b.os("Mac10.15") && b.model("VMware7.1") {
+	if b.os("Mac") {
 		b.cipd(pythonPkgs[0])
 	}
 	if b.matchOs("Win") || b.matchExtraConfig("Win") {
