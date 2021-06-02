326627aeae616987061c8c391e1dd117d46e5dff
Eric Boren <borenet@google.com>
2021-06-02 08:26:23 -0400

    [infra] Roll Infra dep to update CIPD packages
    This is intended to pick up https://skia-review.googlesource.com/c/buildbot/+/414458 which updates the tasks to use cpython3, but it also updates the luci-auth, kitchen, and git packages.
    
    Also includes fixes needed after https://skia-review.googlesource.com/c/buildbot/+/387896
    
    Bug: chromium:1210114
    Change-Id: I2003be762b59a08a6b972b6563e4f3a9920b082d

--- infra/bots/gen_tasks_logic/task_builder.go
+++ infra/bots/gen_tasks_logic/task_builder.go
@@ -284,11 +284,21 @@ func (b *taskBuilder) usesPython() {
 	// TODO(borenet): This handling of the Python package is hacky and bad.
 	pythonPkgs := cipd.PkgsPython[b.cipdPlatform()]
 	b.cipd(pythonPkgs[1])
-	if b.os("Mac10.15") && b.model("VMware7.1") {
+	if b.os("Mac") {
 		b.cipd(pythonPkgs[0])
+		b.cipd(&cipd.Package{
+			Name:    "infra/3pp/tools/cpython3/mac-amd64",
+			Path:    "cipd_bin_packages",
+			Version: "version:2@3.9.5.chromium.17",
+		})
 	}
 	if b.matchOs("Win") || b.matchExtraConfig("Win") {
 		b.cipd(pythonPkgs[0])
+		b.cipd(&cipd.Package{
+			Name:    "infra/3pp/tools/cpython3/windows-amd64",
+			Path:    "cipd_bin_packages",
+			Version: "version:2@3.9.5.chromium.17",
+		})
 	}
 
 	b.cache(&specs.Cache{
@@ -296,6 +306,7 @@ func (b *taskBuilder) usesPython() {
 		Path: "cache/vpython",
 	})
 	b.env("VPYTHON_VIRTUALENV_ROOT", "cache/vpython")
+	b.env("VPYTHON_LOG_TRACE", "1")
 }
 
 func (b *taskBuilder) usesNode() {
