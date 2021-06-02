af3f686e9bc5077909f59fe77f66d4c7753839f1
Ethan Nicholas <ethannicholas@google.com>
2021-06-02 13:09:34 -0400

    Converted DSL APIs to use StringFragment
    This is a prerequisite for DSLParser, which will be using
    StringFragments. This CL also adds a new StringFragment-based DSLType
    constructor which will be used by DSLParser..
    
    Change-Id: I228fba32efb0c680455149712a944489b9168bf4

--- src/sksl/dsl/DSLType.cpp
+++ src/sksl/dsl/DSLType.cpp
@@ -15,6 +15,21 @@ namespace SkSL {
 
 namespace dsl {
 
+DSLType::DSLType(StringFragment name) {
+    const SkSL::Symbol* symbol = (*DSLWriter::SymbolTable())[name];
+    if (symbol) {
+        if (symbol->is<SkSL::Type>()) {
+            fSkSLType = &symbol->as<SkSL::Type>();
+        } else {
+            DSLWriter::ReportError(String::printf("symbol '%*s' is not a type", (int) name.length(),
+                                                  name.data()).c_str());
+        }
+    } else {
+        DSLWriter::ReportError(String::printf("no symbol named '%*s'", (int) name.length(),
+                                              name.data()).c_str());
+    }
+}
+
 bool DSLType::isBoolean() const {
     return this->skslType().isBoolean();
 }
@@ -177,7 +192,7 @@ DSLType Array(const DSLType& base, int count) {
     return DSLWriter::SymbolTable()->addArrayDimension(&base.skslType(), count);
 }
 
-DSLType Struct(const char* name, SkTArray<DSLField> fields) {
+DSLType Struct(StringFragment name, SkTArray<DSLField> fields) {
     std::vector<SkSL::Type::Field> skslFields;
     skslFields.reserve(fields.count());
     for (const DSLField& field : fields) {
