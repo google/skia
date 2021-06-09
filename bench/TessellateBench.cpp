9e67891b72da6e6bef1e66f829bbaaff9d1569e1
Chris Dalton <csmartdalton@google.com>
2021-06-09 21:43:56 +0000

    Revert "Wean tessellation off SkPathOps"
    This reverts commit 6b2121d0ec5719e60d8b61621de2ae54e78183c6.
    
    Reason for revert: Perf regressions:
    
    https://perf.skia.org/t/?begin=1623217407&end=1623217409&subset=all
    
    Original change's description:
    > Wean tessellation off SkPathOps
    >
    > Now that chopping and culling are fully implemented, and the indirect
    > tessellators are gone, we don't need to crop huge paths anymore. The
    > recursive chopping and culling will just do their thing.
    >
    > Bug: skia:9795
    > Bug: skia:11268
    > Bug: chromium:800804
    > Change-Id: Ie34920c59af67035cf37eead69c5335693703001
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/416080
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    > Reviewed-by: Mike Reed <reed@google.com>
    
    TBR=csmartdalton@google.com,reed@google.com
    
    Change-Id: I265bd6991627b435a2a656cf9080b7e9d17e5ded
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:9795
    Bug: skia:11268
    Bug: chromium:800804
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/417238
    Reviewed-by: Chris Dalton <csmartdalton@google.com>
    Commit-Queue: Chris Dalton <csmartdalton@google.com>

--- bench/TessellateBench.cpp
+++ bench/TessellateBench.cpp
@@ -116,7 +116,7 @@ DEF_PATH_TESS_BENCH(GrPathOuterCurveTessellator, make_cubic_path(8), SkMatrix::I
     GrPipeline noVaryingsPipeline(GrScissorTest::kDisabled, SkBlendMode::kSrcOver,
                                   GrSwizzle::RGBA());
     auto tess = GrPathCurveTessellator::Make(&arena, fMatrix, SK_PMColor4fTRANSPARENT,
-                                             GrPathCurveTessellator::DrawInnerFan::kNo,
+                                             GrPathTessellator::DrawInnerFan::kNo,
                                              fTarget->caps().minPathVerbsForHwTessellation(),
                                              noVaryingsPipeline, fTarget->caps());
     tess->prepare(fTarget.get(), SkRectPriv::MakeLargest(), fPath, nullptr);
