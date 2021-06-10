662be1982549e54a75b2a1e062ba13d648d3af4e
Ethan Nicholas <ethannicholas@google.com>
2021-06-10 12:07:34 -0400

    Added DSLSymbols
    These are new APIs to manage symbols and symbol tables from DSL code,
    needed for the upcoming DSLParser.
    
    Change-Id: Ic8d86aebfbcdeb84bd872d8727cdfafbb8db2fd6

---
+++ src/sksl/dsl/DSLSymbols.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/sksl/DSLSymbols.h"
+
+#include "src/sksl/SkSLIRGenerator.h"
+#include "src/sksl/dsl/priv/DSLWriter.h"
+
+namespace SkSL {
+
+namespace dsl {
+
+void PushSymbolTable() {
+    DSLWriter::IRGenerator().pushSymbolTable();
+}
+
+void PopSymbolTable() {
+    DSLWriter::IRGenerator().popSymbolTable();
+}
+
+std::shared_ptr<SymbolTable> CurrentSymbolTable() {
+    return DSLWriter::IRGenerator().symbolTable();
+}
+
+DSLExpression Symbol(skstd::string_view name) {
+    return DSLWriter::IRGenerator().convertIdentifier(/*offset=*/-1, name);
+}
+
+bool IsType(skstd::string_view name) {
+    const SkSL::Symbol* s = (*CurrentSymbolTable())[name];
+    return s && s->is<Type>();
+}
+
+void AddToSymbolTable(DSLVar& var) {
+    CurrentSymbolTable()->addWithoutOwnership(&DSLWriter::Var(var));
+}
+
+const String* Retain(String string) {
+    return CurrentSymbolTable()->takeOwnershipOfString(std::move(string));
+}
+
+} // namespace dsl
+
+} // namespace SkSL
