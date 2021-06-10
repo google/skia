86d8b5b8d7440269df04e912733ae0aa08c03bcd
Brian Osman <brianosman@google.com>
2021-06-10 14:27:53 -0400

    Support specializing uniforms in runtime FPs
    Change-Id: Icb69b87049488a3baf234d45fe1e6a3c96a16d5e

---
+++ gm/runtimefpspecialized.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "gm/gm.h"
+#include "src/core/SkRuntimeEffectPriv.h"
+#include "src/gpu/GrDirectContextPriv.h"
+#include "src/gpu/GrPaint.h"
+#include "src/gpu/GrSurfaceDrawContext.h"
+#include "src/gpu/effects/GrSkSLFP.h"
+
+struct FpAndKey {
+    std::unique_ptr<GrFragmentProcessor> fp;
+    SkTArray<uint32_t, true>             key;
+};
+
+DEF_SIMPLE_GPU_GM(runtimefpspecialized, ctx, sdCtx, canvas, 232, 232) {
+    // Constant color, but with a similar option to GrOverrideInputFragmentProcessor
+    // specialize decides if the color is inserted in the SkSL as a literal, or left as a uniform
+    auto make_color_fp = [&](SkPMColor4f color, bool specialize) {
+        auto effect = SkMakeRuntimeEffect(SkRuntimeEffect::MakeForShader, R"(
+            uniform half4 color;
+            half4 main(float2 xy) { return color; }
+        )");
+        FpAndKey result;
+        result.fp = GrSkSLFP::Make(
+                std::move(effect), "color_fp", "color", SpecializeIf(specialize, color));
+        GrProcessorKeyBuilder builder(&result.key);
+        result.fp->getGLSLProcessorKey(*sdCtx->caps()->shaderCaps(), &builder);
+        return result;
+    };
+
+    int x = 10, y = 10;
+
+    auto nextCol = [&] { x += (64 + 10); };
+    auto nextRow = [&] { x = 10; y += (64 + 10); };
+
+    auto draw = [&](std::unique_ptr<GrFragmentProcessor> fp) {
+        GrPaint paint;
+        paint.setColorFragmentProcessor(std::move(fp));
+        sdCtx->drawRect(nullptr,
+                        std::move(paint),
+                        GrAA::kNo,
+                        SkMatrix::Translate(x, y),
+                        SkRect::MakeIWH(64, 64));
+        nextCol();
+    };
+
+    auto uRed   = make_color_fp({1, 0, 0, 1}, false),
+         uGreen = make_color_fp({0, 1, 0, 1}, false),
+         sRed   = make_color_fp({1, 0, 0, 1}, true),
+         sGreen = make_color_fp({0, 1, 0, 1}, true);
+
+    // uRed and uGreen should have the same key - they just have different uniforms
+    SkASSERT(uRed.key == uGreen.key);
+    // sRed and sGreen should have keys that are different from the uniform case, and each other
+    SkASSERT(sRed.key != uRed.key);
+    SkASSERT(sGreen.key != uRed.key);
+    SkASSERT(sRed.key != sGreen.key);
+
+    draw(std::move(uRed.fp));
+    draw(std::move(uGreen.fp));
+    nextRow();
+
+    draw(std::move(sRed.fp));
+    draw(std::move(sGreen.fp));
+}
