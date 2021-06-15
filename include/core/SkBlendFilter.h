c3fb772f20d7a808f4c12d5b13486f64704d21d6
John Stiles <johnstiles@google.com>
2021-06-15 17:12:08 -0400

    Add SkBlendFilter class; thread into SkVMBlitter.
    Like SkColorFilter, SkShader, etc., this has a public-facing component
    (SkBlendFilter) and a private subclass (SkBlendFilterBase) which can be
    obtained via a helper function (as_BFB). At present there are no public-
    facing methods, but the type needs to be exposed to be usable by the
    outside world.
    
    SkBlendFilterBase exists so that SkRuntimeEffect can subclass it. It
    provides a `program` method with the parameters that blending will use.
    
    Change-Id: I75c772fd4108a9c21fbda84201a8b23d3750a0df
    Bug: skia:12080

---
+++ include/core/SkBlendFilter.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkBlendFilter_DEFINED
+#define SkBlendFilter_DEFINED
+
+#include "include/core/SkFlattenable.h"
+
+/**
+ * SkBlendFilter represents a custom blend function in the Skia pipeline. When a blend filter is
+ * present in a paint, the SkBlendMode is ignored. A blend filter combines a source color (the
+ * result of our paint) and destination color (from the canvas) into a final color.
+ */
+class SK_API SkBlendFilter : public SkFlattenable {
+private:
+    SkBlendFilter() = default;
+    friend class SkBlendFilterBase;
+
+    using INHERITED = SkFlattenable;
+};
+
+#endif
