c3951c14bc25e4566fea24ecbfe0600e045b0007
Robert Phillips <robertphillips@google.com>
2021-06-15 11:01:47 -0400

    [ngatoy] Omnibus
    Bug: skia:11837
    Change-Id: I169b370e2c77fccb36ba5c73a36be8a7f1a39efc

--- experimental/ngatoy/Cmds.cpp
+++ experimental/ngatoy/Cmds.cpp
@@ -36,15 +36,25 @@ RectCmd::RectCmd(ID id,
                  SkIRect r,
                  const FakePaint& p,
                  sk_sp<FakeMCBlob> state)
-    : Cmd(id)
-    , fPaintersOrder(paintersOrder)
-    , fRect(r)
-    , fPaint(p)
-    , fMCState(std::move(state)) {
+        : Cmd(id)
+        , fPaintersOrder(paintersOrder)
+        , fRect(r)
+        , fPaint(p)
+        , fMCState(std::move(state)) {
 }
 
 uint32_t RectCmd::getSortZ() const {
-    return fPaintersOrder.toUInt();
+    uint32_t maxClipZ = 0;
+    for (auto s : fMCState->mcStates()) {
+        for (auto c : s.cmds()) {
+            uint32_t clipZ = c->getPaintersOrderWhenPopped().toUInt();
+
+            maxClipZ = std::max(maxClipZ, clipZ);
+        }
+    }
+
+    return maxClipZ != 0 ? maxClipZ+1 : fPaintersOrder.toUInt();
+
 }
 
 uint32_t RectCmd::getDrawZ() const {
@@ -52,30 +62,10 @@ uint32_t RectCmd::getDrawZ() const {
 }
 
 SortKey RectCmd::getKey() {
-    return SortKey(fPaint.isTransparent(), fMCState->id(), this->getSortZ(), fPaint.toID());
-}
-
-static void apply_diff(FakeCanvas* c, const FakeMCBlob& desired, const FakeMCBlob* prior) {
-    int prefix = desired.determineSharedPrefix(prior);
-
-    if (prior) {
-        for (int j = prefix; j < prior->count(); ++j) {
-            c->restore();
-        }
-    }
-
-    for (int j = prefix; j < desired.count(); ++j) {
-        desired[j].apply(c);
-    }
+    return SortKey(fPaint.isTransparent(), this->getSortZ(), fPaint.toID());
 }
 
 void RectCmd::execute(FakeCanvas* c) const {
-    if (fMCState) {
-        // When replaying the test case 'fMCState' will be null. When actually executing the Cmd
-        // after sorting, 'fMCState' will be non-null.
-        apply_diff(c, *fMCState, c->snapState().get());
-    }
-
     c->drawRect(fID, fRect, fPaint);
 }
 
@@ -95,6 +85,7 @@ void RectCmd::execute(SkCanvas* c) const {
                                                  SkTileMode::kClamp));
     } else {
         SkASSERT(fPaint.toID() == kRadialMat);
+
         auto shader = SkGradientShader::MakeRadial(SkPoint::Make(128.0f, 128.0f),
                                                    128.0f,
                                                    colors,
@@ -115,14 +106,15 @@ static bool is_opaque(SkColor c) {
 void RectCmd::rasterize(uint32_t zBuffer[256][256], SkBitmap* dstBM) const {
 
     uint32_t z = this->getDrawZ();
+    SkIRect scissor = fMCState->scissor();
 
     for (int y = fRect.fTop; y < fRect.fBottom; ++y) {
         for (int x = fRect.fLeft; x < fRect.fRight; ++x) {
-            if (fMCState->clipped(x, y)) {
-                continue;
-            }
-
             if (z > zBuffer[x][y]) {
+                if (!scissor.contains(x, y)) {
+                    continue;
+                }
+
                 zBuffer[x][y] = z;
 
                 SkColor c = fPaint.evalColor(x, y);
@@ -150,34 +142,33 @@ ClipCmd::ClipCmd(ID id, PaintersOrder paintersOrderWhenAdded, SkIRect r)
         : Cmd(id)
         , fRect(r)
         , fPaintersOrderWhenAdded(paintersOrderWhenAdded) {
+    SkASSERT(fPaintersOrderWhenAdded.isValid());
 }
 
 uint32_t ClipCmd::getSortZ() const {
-    // Not used this iteration
-    SkASSERT(0);
-
     SkASSERT(fPaintersOrderWhenAdded.isValid());
 
     return fPaintersOrderWhenAdded.toUInt();
 }
 
 uint32_t ClipCmd::getDrawZ() const {
-    // Not used this iteration
-    SkASSERT(0);
+    SkASSERT(fPaintersOrderWhenPopped.isValid());
 
-    return 0;
+    return fPaintersOrderWhenPopped.toUInt();
 }
 
 SortKey ClipCmd::getKey() {
-    // Not used this iteration
-    SkASSERT(0);
+    return SortKey(false, this->getSortZ(), kInvalidMat);
+}
 
-    return SortKey(false, 0, this->getSortZ(), kInvalidMat);
+void ClipCmd::pop(PaintersOrder paintersOrderWhenPopped) {
+    SkASSERT(!fPaintersOrderWhenPopped.isValid() && paintersOrderWhenPopped.isValid());
+    fPaintersOrderWhenPopped = paintersOrderWhenPopped;
 }
 
 void ClipCmd::execute(FakeCanvas* c) const {
     // This call is creating the 'real' ClipCmd for the "actual" case
-    SkASSERT(!fPaintersOrderWhenAdded.isValid());
+    SkASSERT(!fPaintersOrderWhenAdded.isValid() && !fPaintersOrderWhenPopped.isValid());
 
     c->clipRect(fID, fRect);
 }
@@ -187,9 +178,6 @@ void ClipCmd::execute(SkCanvas* c) const {
 }
 
 void ClipCmd::rasterize(uint32_t zBuffer[256][256], SkBitmap* /* dstBM */) const {
-    // Not used this iteration
-    SkASSERT(0);
-
     uint32_t drawZ = this->getDrawZ();
 
     for (int y = fRect.fTop; y < fRect.fBottom; ++y) {
