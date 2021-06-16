1308fc70a628b722216b59e83249e2f85cb0a3ba
Robert Phillips <robertphillips@google.com>
2021-06-16 10:00:20 -0400

    [ngatoy] Fix memory issues
    Bug: skia:11837
    Change-Id: Idab6e49fece3f1fce2b7a32a57734845bb72867d

--- experimental/ngatoy/Cmds.h
+++ experimental/ngatoy/Cmds.h
@@ -12,21 +12,21 @@ class SortKey;
 
 #include "include/core/SkColor.h"
 #include "include/core/SkRect.h"
+#include "include/core/SkRefCnt.h"
 
 #include "experimental/ngatoy/Fake.h"
 #include "experimental/ngatoy/ngatypes.h"
 
-class Cmd {
+//------------------------------------------------------------------------------------------------
+class Cmd : public SkRefCnt {
 public:
     Cmd() : fID(ID::Invalid()) {}
     Cmd(ID id) : fID(id) {}
-    virtual ~Cmd() {}
+    ~Cmd() override {}
 
     ID id() const { return fID; }
 
     virtual SortKey getKey() = 0;
-
-    virtual const FakeMCBlob* state() const { return nullptr; }
 
     // To generate the actual image
     virtual void execute(FakeCanvas*) const = 0;
@@ -49,7 +49,7 @@ class SaveCmd : public Cmd {
 public:
     SaveCmd() : Cmd() {}
 
-    SortKey getKey() override { SkASSERT(0); return {}; }
+    SortKey getKey() override;
 
     void execute(FakeCanvas*) const override;
     void execute(SkCanvas*) const override;
@@ -72,7 +72,7 @@ class RestoreCmd : public Cmd {
 public:
     RestoreCmd() : Cmd() {}
 
-    SortKey getKey() override { SkASSERT(0); return {}; }
+    SortKey getKey() override;
 
     void execute(FakeCanvas*) const override;
     void execute(SkCanvas*) const override;
@@ -91,13 +91,14 @@ private:
 //------------------------------------------------------------------------------------------------
 class RectCmd : public Cmd {
 public:
+    RectCmd(ID, SkIRect, const FakePaint&);  // for creating the test cases
     RectCmd(ID, PaintersOrder, SkIRect, const FakePaint&, sk_sp<FakeMCBlob> state);
 
     uint32_t getSortZ() const;
     uint32_t getDrawZ() const;
 
     SortKey getKey() override;
-    const FakeMCBlob* state() const override { return fMCState.get(); }
+    const FakeMCBlob* state() const { return fMCState.get(); }
 
     void execute(FakeCanvas*) const override;
     void execute(SkCanvas*) const override;
@@ -123,6 +124,7 @@ private:
 class ClipCmd : public Cmd {
 public:
     ClipCmd(ID, PaintersOrder paintersOrderWhenAdded, SkIRect r);
+    ~ClipCmd() override;
 
     uint32_t getSortZ() const;
     uint32_t getDrawZ() const;
@@ -147,4 +149,5 @@ private:
 };
 
 //------------------------------------------------------------------------------------------------
+
 #endif // Cmds_DEFINED
