162ab1bbc477952d19d52770374de111a7421edc
John Stiles <johnstiles@google.com>
2021-06-16 22:37:15 -0400

    Reland "Add SkRuntimeBlender class."
    This is a reland of 6034941cc28a28a7c9fc79d6cb815fed188cdfb6
    
    Original change's description:
    > Add SkRuntimeBlender class.
    >
    > This class is returned by SkRuntimeEffect::makeBlender when a runtime
    > blend is returned. SkRuntimeBlendBuilder is also added as a convenience
    > class to simplify creation and uniform setup.
    >
    > Our ability to add tests is limited in this CL because the SkPaint does
    > not contain an SkBlender yet. We do have one test which builds an
    > SkBlender, but there's not much we can do with it. Testing will be
    > bulked up in the next CL.
    >
    > Change-Id: Ib2d7d04186690ec0d2238fc990a19d9e6b786ce3
    > Bug: skia:12080
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/417006
    > Commit-Queue: John Stiles <johnstiles@google.com>
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    
    Bug: skia:12080
    Change-Id: Ie41be141ba3787452f9d5c72946ebc748a5d6176

--- include/effects/SkRuntimeEffect.h
+++ include/effects/SkRuntimeEffect.h
@@ -8,6 +8,7 @@
 #ifndef SkRuntimeEffect_DEFINED
 #define SkRuntimeEffect_DEFINED
 
+#include "include/core/SkBlender.h"
 #include "include/core/SkColorFilter.h"
 #include "include/core/SkData.h"
 #include "include/core/SkImageInfo.h"
@@ -21,7 +22,6 @@
 #include <vector>
 
 class GrRecordingContext;
-class SkBlender;
 class SkFilterColorProgram;
 class SkImage;
 
@@ -249,6 +249,7 @@ private:
 #endif
 
     friend class SkRTShader;            // fBaseProgram, fMain
+    friend class SkRuntimeBlender;      //
     friend class SkRuntimeColorFilter;  //
 
     friend class SkFilterColorProgram;
@@ -417,6 +418,23 @@ public:
 
 private:
     using INHERITED = SkRuntimeEffectBuilder<sk_sp<SkShader>>;
+};
+
+/**
+ * SkRuntimeBlendBuilder is a utility to simplify creation and uniform setup of runtime blenders.
+ */
+class SK_API SkRuntimeBlendBuilder : public SkRuntimeEffectBuilder<sk_sp<SkBlender>> {
+public:
+    explicit SkRuntimeBlendBuilder(sk_sp<SkRuntimeEffect>);
+    ~SkRuntimeBlendBuilder();
+
+    SkRuntimeBlendBuilder(const SkRuntimeBlendBuilder&) = delete;
+    SkRuntimeBlendBuilder& operator=(const SkRuntimeBlendBuilder&) = delete;
+
+    sk_sp<SkBlender> makeBlender();
+
+private:
+    using INHERITED = SkRuntimeEffectBuilder<sk_sp<SkBlender>>;
 };
 
 #endif
