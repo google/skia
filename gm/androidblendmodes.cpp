42ae789dcb6c1b1c77ce6ee3ae721668bbd374d6
John Stiles <johnstiles@google.com>
2021-06-17 17:17:18 -0400

    Add GM slide `runtime_androidblendmodes`.
    This GM should display the same result as `androidblendmodes`, but it
    uses a Runtime Blender to combine its layers instead of SkBlendMode.
    
    Change-Id: Ica53d01096f1258f8fe3f4730fa60c48d947fc8c
    Bug: skia:12080

--- gm/androidblendmodes.cpp
+++ gm/androidblendmodes.cpp
@@ -8,6 +8,7 @@
 #include "gm/gm.h"
 #include "include/core/SkBitmap.h"
 #include "include/core/SkBlendMode.h"
+#include "include/core/SkBlender.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkColor.h"
 #include "include/core/SkFont.h"
@@ -17,14 +18,16 @@
 #include "include/core/SkSize.h"
 #include "include/core/SkString.h"
 #include "include/core/SkTypeface.h"
+#include "include/effects/SkRuntimeEffect.h"
 #include "include/utils/SkTextUtils.h"
 #include "tools/ToolUtils.h"
 
 #include <initializer_list>
+#include <unordered_map>
 
 namespace skiagm {
 
-// This GM recreates the blend mode images from the Android documentation
+// This GM recreates the blend mode images from the Android documentation.
 class AndroidBlendModesGM : public GM {
 public:
     AndroidBlendModesGM() {
@@ -32,9 +35,7 @@ public:
     }
 
 protected:
-    SkString onShortName() override {
-        return SkString("androidblendmodes");
-    }
+    virtual void onSetBlend(SkPaint* paint, SkBlendMode blend) = 0;
 
     SkISize onISize() override {
         return SkISize::Make(kNumCols * kBitmapSize, kNumRows * kBitmapSize);
@@ -72,7 +73,9 @@ protected:
 
         SkPaint p;
         canvas->drawImage(fCompositeDst.asImage(), 0, 0, SkSamplingOptions(), &p);
-        p.setBlendMode(mode);
+
+        this->onSetBlend(&p, mode);
+
         canvas->drawImage(fCompositeSrc.asImage(), 0, 0, SkSamplingOptions(), &p);
     }
 
@@ -132,5 +135,131 @@ private:
 
 //////////////////////////////////////////////////////////////////////////////
 
-DEF_GM(return new AndroidBlendModesGM;)
+// This GM recreates the blend mode images from the Android documentation using SkBlendMode.
+class NativeAndroidBlendModesGM : public AndroidBlendModesGM {
+public:
+    SkString onShortName() override {
+        return SkString("androidblendmodes");
+    }
+
+    void onSetBlend(SkPaint* paint, SkBlendMode blend) override {
+        paint->setBlendMode(blend);
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+// This GM recreates the blend mode images from the Android documentation using a Runtime Blender.
+class RuntimeAndroidBlendModesGM : public AndroidBlendModesGM {
+public:
+    SkString onShortName() override {
+        return SkString("runtime_androidblendmodes");
+    }
+
+    sk_sp<SkBlender> getRuntimeBlend(SkBlendMode mode) {
+        if (sk_sp<SkBlender> effect = fBlendMap[mode]) {
+            return effect;
+        }
+
+        const char* func;
+        switch (mode) {
+            case SkBlendMode::kPlus:     func = "blend_plus"; break;
+            case SkBlendMode::kClear:    func = "blend_clear"; break;
+            case SkBlendMode::kDarken:   func = "blend_darken"; break;
+            case SkBlendMode::kDst:      func = "blend_dst"; break;
+            case SkBlendMode::kDstATop:  func = "blend_dst_atop"; break;
+            case SkBlendMode::kDstIn:    func = "blend_dst_in"; break;
+            case SkBlendMode::kDstOut:   func = "blend_dst_out"; break;
+            case SkBlendMode::kDstOver:  func = "blend_dst_over"; break;
+            case SkBlendMode::kLighten:  func = "blend_lighten"; break;
+            case SkBlendMode::kModulate: func = "blend_modulate"; break;
+            case SkBlendMode::kOverlay:  func = "blend_overlay"; break;
+            case SkBlendMode::kScreen:   func = "blend_screen"; break;
+            case SkBlendMode::kSrc:      func = "blend_src"; break;
+            case SkBlendMode::kSrcATop:  func = "blend_src_atop"; break;
+            case SkBlendMode::kSrcIn:    func = "blend_src_in"; break;
+            case SkBlendMode::kSrcOut:   func = "blend_src_out"; break;
+            case SkBlendMode::kSrcOver:  func = "blend_src_over"; break;
+            case SkBlendMode::kXor:      func = "blend_xor"; break;
+            default:                     func = "invalid"; break;
+        }
+
+        constexpr char kShaderText[] = R"(
+half4 blend_clear(half4 src, half4 dst) { return half4(0); }
+
+half4 blend_src(half4 src, half4 dst) { return src; }
+
+half4 blend_dst(half4 src, half4 dst) { return dst; }
+
+half4 blend_src_over(half4 src, half4 dst) { return src + (1 - src.a)*dst; }
+
+half4 blend_dst_over(half4 src, half4 dst) { return (1 - dst.a)*src + dst; }
+
+half4 blend_src_in(half4 src, half4 dst) { return (src == half4(0) ? half4(0) : src*dst.a); }
+
+half4 blend_dst_in(half4 src, half4 dst) { return blend_src_in(dst, src); }
+
+half4 blend_src_out(half4 src, half4 dst) { return (1 - dst.a)*src; }
+
+half4 blend_dst_out(half4 src, half4 dst) { return (1 - src.a)*dst; }
+
+half4 blend_src_atop(half4 src, half4 dst) { return dst.a*src + (1 - src.a)*dst; }
+
+half4 blend_dst_atop(half4 src, half4 dst)  { return  (1 - dst.a) * src + src.a*dst; }
+
+half4 blend_xor(half4 src, half4 dst) { return (1 - dst.a)*src + (1 - src.a)*dst; }
+
+half4 blend_plus(half4 src, half4 dst) { return min(src + dst, 1); }
+
+half4 blend_modulate(half4 src, half4 dst) { return src*dst; }
+
+half4 blend_screen(half4 src, half4 dst) { return src + (1 - src)*dst; }
+
+half _blend_overlay_component(half2 s, half2 d) {
+    return (2*d.x <= d.y)
+            ? 2*s.x*d.x
+            : s.y*d.y - 2*(d.y - d.x)*(s.y - s.x);
+}
+
+half4 blend_overlay(half4 src, half4 dst) {
+    half4 result = half4(_blend_overlay_component(src.ra, dst.ra),
+                         _blend_overlay_component(src.ga, dst.ga),
+                         _blend_overlay_component(src.ba, dst.ba),
+                         src.a + (1 - src.a)*dst.a);
+    result.rgb += dst.rgb*(1 - src.a) + src.rgb*(1 - dst.a);
+    return result;
+}
+
+half4 blend_darken(half4 src, half4 dst) {
+    half4 result = blend_src_over(src, dst);
+    result.rgb = min(result.rgb, (1 - dst.a)*src.rgb + dst.rgb);
+    return result;
+}
+
+half4 blend_lighten(half4 src, half4 dst) {
+    half4 result = blend_src_over(src, dst);
+    result.rgb = max(result.rgb, (1 - dst.a)*src.rgb + dst.rgb);
+    return result;
+}
+
+half4 main(half4 src, half4 dst) {
+    return %s(src, dst);
+}
+)";
+        SkString code = SkStringPrintf(kShaderText, func);
+        SkRuntimeEffect::Result result = SkRuntimeEffect::MakeForBlender(std::move(code));
+        SkASSERT(result.effect);
+        return (fBlendMap[mode] = result.effect->makeBlender(/*uniforms=*/nullptr));
+    }
+
+    void onSetBlend(SkPaint* paint, SkBlendMode blend) override {
+        paint->experimental_setBlender(this->getRuntimeBlend(blend));
+    }
+
+private:
+    std::unordered_map<SkBlendMode, sk_sp<SkBlender>> fBlendMap;
+};
+
+DEF_GM(return new NativeAndroidBlendModesGM;)
+DEF_GM(return new RuntimeAndroidBlendModesGM;)
 }  // namespace skiagm
