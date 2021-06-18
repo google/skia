c5d72f60cc36dfac7e5942ec3d53ef5338361fbb
Herb Derby <herb@google.com>
2021-06-18 20:31:43 +0000

    Revert "Don't keep "outResultsFile" open in nanobench"
    This reverts commit 2e1381ec86bc794968fca51572dd7bd37d6508c7.
    
    Reason for revert: This causes corrupted JSON output from
                       nanobench
    
    Bug: skia:12078
    
    Original change's description:
    > Don't keep "outResultsFile" open in nanobench
    >
    > There is a bug on Pixel and Pixel2 devices where the program
    > eventually terminates with a non-zero exit code. Closing the
    > outResultsFile between JSON flushes seems to fix it (for whatever
    > reason).
    >
    > Bug: b/143074513
    > Change-Id: I935e982e88758fda19292129c8031f8501cca615
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/249821
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Commit-Queue: Chris Dalton <csmartdalton@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b/143074513
    Change-Id: Iad8e4423acbc520f49365859020776110087dacc

--- src/core/SkOSFile.h
+++ src/core/SkOSFile.h
@@ -17,9 +17,8 @@
 #include "include/private/SkTemplates.h"
 
 enum SkFILE_Flags {
-    kRead_SkFILE_Flag    = 0x01,
-    kWrite_SkFILE_Flag   = 0x02,
-    kAppend_SkFILE_Flag  = 0x04
+    kRead_SkFILE_Flag   = 0x01,
+    kWrite_SkFILE_Flag  = 0x02
 };
 
 FILE* sk_fopen(const char path[], SkFILE_Flags);
