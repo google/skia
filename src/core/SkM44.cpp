71d54de10ebce39e9892efdbdc50cd27199b5303
John Stiles <johnstiles@google.com>
2021-06-21 17:53:09 -0400

    Migrate matrix-inversion code to a dedicated file.
    This allows SkM44, SkMatrix44, and optimize_intrinsic_call to all share
    the same matrix inversion logic.
    
    Support for 2x2 and 3x3 matrix inversion was also added to
    SkMatrixInvert.h, but is not currently used anywhere in Skia besides
    optimize_intrinsic_call. (The perspective case of SkMatrix::ComputeInv
    could probably be adapted to use the shared 3x3 code.)
    
    Change-Id: Ie55aab536b4bd0c0a04f2c24527747670ca68fbb
    Bug: skia:12054

--- src/core/SkM44.cpp
+++ src/core/SkM44.cpp
@@ -9,6 +9,7 @@
 #include "include/core/SkMatrix.h"
 #include "include/private/SkVx.h"
 
+#include "src/core/SkMatrixInvert.h"
 #include "src/core/SkMatrixPriv.h"
 #include "src/core/SkPathPriv.h"
 
@@ -245,76 +246,8 @@ void SkM44::normalizePerspective() {
     promoting our SkScalar values to double (if needed).
  */
 bool SkM44::invert(SkM44* inverse) const {
-    double a00 = fMat[0];
-    double a01 = fMat[1];
-    double a02 = fMat[2];
-    double a03 = fMat[3];
-    double a10 = fMat[4];
-    double a11 = fMat[5];
-    double a12 = fMat[6];
-    double a13 = fMat[7];
-    double a20 = fMat[8];
-    double a21 = fMat[9];
-    double a22 = fMat[10];
-    double a23 = fMat[11];
-    double a30 = fMat[12];
-    double a31 = fMat[13];
-    double a32 = fMat[14];
-    double a33 = fMat[15];
-
-    double b00 = a00 * a11 - a01 * a10;
-    double b01 = a00 * a12 - a02 * a10;
-    double b02 = a00 * a13 - a03 * a10;
-    double b03 = a01 * a12 - a02 * a11;
-    double b04 = a01 * a13 - a03 * a11;
-    double b05 = a02 * a13 - a03 * a12;
-    double b06 = a20 * a31 - a21 * a30;
-    double b07 = a20 * a32 - a22 * a30;
-    double b08 = a20 * a33 - a23 * a30;
-    double b09 = a21 * a32 - a22 * a31;
-    double b10 = a21 * a33 - a23 * a31;
-    double b11 = a22 * a33 - a23 * a32;
-
-    // Calculate the determinant
-    double det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-
-    double invdet = sk_ieee_double_divide(1.0, det);
-    // If det is zero, we want to return false. However, we also want to return false if 1/det
-    // overflows to infinity (i.e. det is denormalized). All of this is subsumed by our final check
-    // at the bottom (that all 16 scalar matrix entries are finite).
-
-    b00 *= invdet;
-    b01 *= invdet;
-    b02 *= invdet;
-    b03 *= invdet;
-    b04 *= invdet;
-    b05 *= invdet;
-    b06 *= invdet;
-    b07 *= invdet;
-    b08 *= invdet;
-    b09 *= invdet;
-    b10 *= invdet;
-    b11 *= invdet;
-
-    SkScalar tmp[16] = {
-        SkDoubleToScalar(a11 * b11 - a12 * b10 + a13 * b09),
-        SkDoubleToScalar(a02 * b10 - a01 * b11 - a03 * b09),
-        SkDoubleToScalar(a31 * b05 - a32 * b04 + a33 * b03),
-        SkDoubleToScalar(a22 * b04 - a21 * b05 - a23 * b03),
-        SkDoubleToScalar(a12 * b08 - a10 * b11 - a13 * b07),
-        SkDoubleToScalar(a00 * b11 - a02 * b08 + a03 * b07),
-        SkDoubleToScalar(a32 * b02 - a30 * b05 - a33 * b01),
-        SkDoubleToScalar(a20 * b05 - a22 * b02 + a23 * b01),
-        SkDoubleToScalar(a10 * b10 - a11 * b08 + a13 * b06),
-        SkDoubleToScalar(a01 * b08 - a00 * b10 - a03 * b06),
-        SkDoubleToScalar(a30 * b04 - a31 * b02 + a33 * b00),
-        SkDoubleToScalar(a21 * b02 - a20 * b04 - a23 * b00),
-        SkDoubleToScalar(a11 * b07 - a10 * b09 - a12 * b06),
-        SkDoubleToScalar(a00 * b09 - a01 * b07 + a02 * b06),
-        SkDoubleToScalar(a31 * b01 - a30 * b03 - a32 * b00),
-        SkDoubleToScalar(a20 * b03 - a21 * b01 + a22 * b00),
-    };
-    if (!SkScalarsAreFinite(tmp, 16)) {
+    SkScalar tmp[16];
+    if (!SkInvert4x4Matrix(fMat, tmp)) {
         return false;
     }
     memcpy(inverse->fMat, tmp, sizeof(tmp));
