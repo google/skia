11da9d99e5f336b0cc816f63a03da50c54f2092d
Florin Malita <fmalita@chromium.org>
2021-06-23 10:40:47 -0400

    [androidkit] Add remaining gradients
    Consolidate the common logic in a Gradient base class, and add
    radial/conical/sweep factories.
    
    Change-Id: Ife15ae78e7c6df48cfa67fe20054f9bd9a8a1e90

---
+++ platform_tools/android/apps/AndroidKit/src/main/java/org/skia/androidkit/Gradient.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.skia.androidkit;
+
+import android.support.annotation.Nullable;
+import java.lang.IllegalArgumentException;
+
+public class Gradient extends Shader {
+    protected interface GradientFactory {
+        long makeGradient(float[] colors, float[] pos, int tileMode, long nativeLocalMatrix);
+    }
+
+    protected Gradient(Color[] colors, float[] pos, TileMode tm, Matrix lm,
+                       GradientFactory gf) throws IllegalArgumentException {
+        super(makeGradient(colors, pos, tm, lm, gf));
+    }
+
+    private static long makeGradient(Color[] colors, float[] pos, TileMode tm, Matrix lm,
+                                     GradientFactory gf) throws IllegalArgumentException {
+        if (colors.length != pos.length) {
+            throw new IllegalArgumentException("Expecting equal-length colors and positions.");
+        }
+
+        float[] fcolors = new float[colors.length * 4];
+
+        for (int i = 0; i < colors.length; ++i) {
+            fcolors[4*i + 0] = colors[i].r();
+            fcolors[4*i + 1] = colors[i].g();
+            fcolors[4*i + 2] = colors[i].b();
+            fcolors[4*i + 3] = colors[i].a();
+        }
+
+        return gf.makeGradient(fcolors, pos, tm.ordinal(), lm != null ? lm.getNativeInstance() : 0);
+    }
+}
