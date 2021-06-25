f74eea7488dba5f1b7681ac8614b58b4f06b8f5a
Mike Reed <reed@google.com>
2021-06-25 10:13:44 -0400

    Refactor textToGlyphs closer to the source
    Bug: skia:12113
    Change-Id: I61c5789885a31d23c583227481afa87e7e784c7a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/421919
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>

--- src/core/SkFont.cpp
+++ src/core/SkFont.cpp
@@ -150,65 +150,10 @@ void SkFont::unicharsToGlyphs(const SkUnichar uni[], int count, SkGlyphID glyphs
     this->getTypefaceOrDefault()->unicharsToGlyphs(uni, count, glyphs);
 }
 
-class SkConvertToUTF32 {
-public:
-    SkConvertToUTF32() {}
-
-    const SkUnichar* convert(const void* text, size_t byteLength, SkTextEncoding encoding) {
-        const SkUnichar* uni;
-        switch (encoding) {
-            case SkTextEncoding::kUTF8: {
-                uni = fStorage.reset(byteLength);
-                const char* ptr = (const char*)text;
-                const char* end = ptr + byteLength;
-                for (int i = 0; ptr < end; ++i) {
-                    fStorage[i] = SkUTF::NextUTF8(&ptr, end);
-                }
-            } break;
-            case SkTextEncoding::kUTF16: {
-                uni = fStorage.reset(byteLength);
-                const uint16_t* ptr = (const uint16_t*)text;
-                const uint16_t* end = ptr + (byteLength >> 1);
-                for (int i = 0; ptr < end; ++i) {
-                    fStorage[i] = SkUTF::NextUTF16(&ptr, end);
-                }
-            } break;
-            case SkTextEncoding::kUTF32:
-                uni = (const SkUnichar*)text;
-                break;
-            default:
-                SK_ABORT("unexpected enum");
-        }
-        return uni;
-    }
-
-private:
-    SkAutoSTMalloc<256, SkUnichar> fStorage;
-};
-
 int SkFont::textToGlyphs(const void* text, size_t byteLength, SkTextEncoding encoding,
                          SkGlyphID glyphs[], int maxGlyphCount) const {
-    if (0 == byteLength) {
-        return 0;
-    }
-
-    SkASSERT(text);
-
-    int count = SkFontPriv::CountTextElements(text, byteLength, encoding);
-    if (!glyphs || count > maxGlyphCount) {
-        return count;
-    }
-
-    if (encoding == SkTextEncoding::kGlyphID) {
-        memcpy(glyphs, text, count << 1);
-        return count;
-    }
-
-    SkConvertToUTF32 storage;
-    const SkUnichar* uni = storage.convert(text, byteLength, encoding);
-
-    this->getTypefaceOrDefault()->unicharsToGlyphs(uni, count, glyphs);
-    return count;
+    return this->getTypefaceOrDefault()->textToGlyphs(text, byteLength, encoding,
+                                                      glyphs, maxGlyphCount);
 }
 
 SkScalar SkFont::measureText(const void* text, size_t length, SkTextEncoding encoding,
