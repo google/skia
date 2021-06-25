1dac79296993ec2ec83e4a6ddfda488500ce051e
John Stiles <johnstiles@google.com>
2021-06-25 11:11:17 -0400

    Fix class name in debug message.
    Change-Id: Id0e727b58e7415a28bb67ae0c1bc03f953fb4e9c

--- src/core/SkBlendModeBlender.cpp
+++ src/core/SkBlendModeBlender.cpp
@@ -73,7 +73,7 @@ std::unique_ptr<GrFragmentProcessor> SkBlendModeBlender::asFragmentProcessor(
     // Using a SkBlendModeBlender on the GPU side isn't supported; we should use GrXferProcessor to
     // perform this blend instead. The Xfer processor is able to perform coefficient-based blends
     // without readback, so it's more efficient.
-    SkDEBUGFAIL("SkBuiltInBlender::asFragmentProcessor is not supported");
+    SkDEBUGFAIL("SkBlendModeBlender::asFragmentProcessor is not supported");
     return nullptr;
 }
 #endif
