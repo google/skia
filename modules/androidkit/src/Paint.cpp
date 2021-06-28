8c8ee74bf9e72c49e593f7b5b73454a2c45ae457
Florin Malita <fmalita@chromium.org>
2021-06-28 12:58:35 -0400

    [androidkit] Color filter support
    Add MatrixColorFilter, HSLAMatrixColorFilter and ComposeColorFilter.
    
    Change-Id: I047368adcd13452566a40e91a6f594dd525efd5f

--- modules/androidkit/src/Paint.cpp
+++ modules/androidkit/src/Paint.cpp
@@ -7,46 +7,53 @@
 
 #include <jni.h>
 
+#include "include/core/SkColorFilter.h"
 #include "include/core/SkPaint.h"
 #include "include/core/SkShader.h"
 #include "include/effects/SkImageFilters.h"
 
 namespace {
 
-static jlong Paint_Create(JNIEnv* env, jobject) {
+static jlong Paint_Create(JNIEnv*, jobject) {
     return reinterpret_cast<jlong>(new SkPaint);
 }
 
-static void Paint_Release(JNIEnv* env, jobject, jlong native_paint) {
+static void Paint_Release(JNIEnv*, jobject, jlong native_paint) {
     delete reinterpret_cast<SkPaint*>(native_paint);
 }
 
-static void Paint_SetColor(JNIEnv* env, jobject, jlong native_paint,
+static void Paint_SetColor(JNIEnv*, jobject, jlong native_paint,
                            float r, float g, float b, float a) {
     if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
         paint->setColor4f({r, g, b, a});
     }
 }
 
-static void Paint_SetStroke(JNIEnv* env, jobject, jlong native_paint, jboolean stroke) {
+static void Paint_SetStroke(JNIEnv*, jobject, jlong native_paint, jboolean stroke) {
     if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
         paint->setStroke(stroke);
     }
 }
 
-static void Paint_SetStrokeWidth(JNIEnv* env, jobject, jlong native_paint, jfloat width) {
+static void Paint_SetStrokeWidth(JNIEnv*, jobject, jlong native_paint, jfloat width) {
     if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
         paint->setStrokeWidth(width);
     }
 }
 
-static void Paint_SetShader(JNIEnv* env, jobject, jlong native_paint, jlong native_shader) {
+static void Paint_SetColorFilter(JNIEnv*, jobject, jlong native_paint, jlong native_cf) {
+    if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
+        paint->setColorFilter(sk_ref_sp(reinterpret_cast<SkColorFilter*>(native_cf)));
+    }
+}
+
+static void Paint_SetShader(JNIEnv*, jobject, jlong native_paint, jlong native_shader) {
     if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
         paint->setShader(sk_ref_sp(reinterpret_cast<SkShader*>(native_shader)));
     }
 }
 
-static void Paint_SetImageFilter(JNIEnv* env, jobject, jlong native_paint, jlong native_filter) {
+static void Paint_SetImageFilter(JNIEnv*, jobject, jlong native_paint, jlong native_filter) {
     if (auto* paint = reinterpret_cast<SkPaint*>(native_paint)) {
         paint->setImageFilter(sk_ref_sp(reinterpret_cast<SkImageFilter*>(native_filter)));
     }
@@ -61,6 +68,7 @@ int register_androidkit_Paint(JNIEnv* env) {
         {"nSetColor"       , "(JFFFF)V", reinterpret_cast<void*>(Paint_SetColor)},
         {"nSetStroke"      , "(JZ)V"   , reinterpret_cast<void*>(Paint_SetStroke)},
         {"nSetStrokeWidth" , "(JF)V"   , reinterpret_cast<void*>(Paint_SetStrokeWidth)},
+        {"nSetColorFilter" , "(JJ)V"   , reinterpret_cast<void*>(Paint_SetColorFilter)},
         {"nSetShader"      , "(JJ)V"   , reinterpret_cast<void*>(Paint_SetShader)},
         {"nSetImageFilter" , "(JJ)V"   , reinterpret_cast<void*>(Paint_SetImageFilter)},
     };
