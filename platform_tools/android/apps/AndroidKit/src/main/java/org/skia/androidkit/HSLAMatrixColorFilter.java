5a9ceebfb9eb21e5772c8aca3f120aafc64fdd5f
Florin Malita <fmalita@chromium.org>
2021-06-28 16:34:14 -0400

    [androidkit] Color filter support
    Add MatrixColorFilter, HSLAMatrixColorFilter and ComposeColorFilter.
    
    Change-Id: I047368adcd13452566a40e91a6f594dd525efd5f

---
+++ platform_tools/android/apps/AndroidKit/src/main/java/org/skia/androidkit/HSLAMatrixColorFilter.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.skia.androidkit;
+
+import java.lang.IllegalArgumentException;
+
+public class HSLAMatrixColorFilter extends ColorFilter {
+    public HSLAMatrixColorFilter(float[] m) throws IllegalArgumentException {
+        super(makeNative(m));
+    }
+
+    private static long makeNative(float[] m) throws IllegalArgumentException {
+        if (m.length != 20) {
+            throw new IllegalArgumentException("Expecting an array of 20 floats.");
+        }
+
+        return nMakeHSLAMatrix(m);
+    }
+
+    private static native long nMakeHSLAMatrix(float[] m);
+};
