8bfa1578a3f7c6bb8833b45e9931e35db554a137
Jim Van Verth <jvanverth@google.com>
2021-06-28 10:39:31 -0400

    Reland "Metal: First pass at async pipelineState creation."
    This reverts commit 688d3180ab9d9b523279b34f3299a33953845cb4.
    
    Reason for revert: Fixing error
    
    Original change's description:
    > Revert "Metal: First pass at async pipelineState creation."
    >
    > This reverts commit adf36be2f9ac7020b6b57e0c9a645488c36aa122.
    >
    > Reason for revert: breaking Flutter roll
    >
    > Original change's description:
    > > Metal: First pass at async pipelineState creation.
    > >
    > > Trying a basic approach to generating pipelineStates asynchronously.
    > > Rather than managing the caching ourselves, we depend on the Apple
    > > cache. When a pipelineState is created during the regular path, it
    > > should find it in the Apple cache and return immediately.
    > >
    > > To avoid too much duplication of the shader compilation step we cache
    > > the MTLLibrarys locally until the final PSO is cached.
    > >
    > > Bug: skia:12141
    > > Change-Id: Id8f6ba7caee33b4c7f6a5af0e8ad5b84993b1246
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/421321
    > > Reviewed-by: Brian Osman <brianosman@google.com>
    > > Commit-Queue: Jim Van Verth <jvanverth@google.com>
    >
    > TBR=jvanverth@google.com,brianosman@google.com,chinmaygarde@google.com
    >
    > Change-Id: Iad7c4c87c22d0d002809c215878439fdb098f54a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:12141
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/422117
    > Reviewed-by: Tyler Denniston <tdenniston@google.com>
    > Commit-Queue: Tyler Denniston <tdenniston@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: skia:12141
    Change-Id: Ibf6aa5383b6aabbf538dab679f3f83241a4504d9

--- src/gpu/mtl/GrMtlPipelineStateBuilder.h
+++ src/gpu/mtl/GrMtlPipelineStateBuilder.h
@@ -24,8 +24,9 @@ class GrMtlPipelineState;
 class SkReadBuffer;
 
 struct GrMtlPrecompiledLibraries {
-    // TODO: wrap this in sk_cfp<> or unique_ptr<> when we remove ARC
-    id<MTLRenderPipelineState> fPipelineState;
+    // TODO: wrap these in sk_cfp<> or unique_ptr<> when we remove ARC
+    id<MTLLibrary> fVertexLibrary;
+    id<MTLLibrary> fFragmentLibrary;
     bool fRTHeight = false;
 };
 
