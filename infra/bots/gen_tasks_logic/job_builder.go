8c319ccdf4ba038f63d69ce387de7b2008141136
Ravi Mistry <rmistry@google.com>
2021-06-29 14:35:15 -0400

    Run CreateDockerImage bots only on main branch
    Change-Id: I82f432a7b072c52623bebea12486f54ff751807a

--- infra/bots/gen_tasks_logic/job_builder.go
+++ infra/bots/gen_tasks_logic/job_builder.go
@@ -230,7 +230,7 @@ func (b *jobBuilder) finish() {
 		b.trigger(specs.TRIGGER_NIGHTLY)
 	} else if b.frequency("Weekly") {
 		b.trigger(specs.TRIGGER_WEEKLY)
-	} else if b.extraConfig("Flutter", "CommandBuffer") {
+	} else if b.extraConfig("Flutter", "CommandBuffer", "CreateDockerImage") {
 		b.trigger(specs.TRIGGER_MAIN_ONLY)
 	} else if b.frequency("OnDemand") || b.role("Canary") {
 		b.trigger(specs.TRIGGER_ON_DEMAND)
