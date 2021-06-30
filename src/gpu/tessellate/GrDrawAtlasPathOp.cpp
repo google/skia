29f2e84a6640845df3f6f7cd11991a52b7bf02ce
Chris Dalton <csmartdalton@google.com>
2021-06-29 19:42:13 -0600

    Finish sk_VertexID workarounds for tessellated fills
    At this point all tessellated fills are capable of drawing when
    sk_VertexID is not supported.
    
    Bug: chromium:1220246
    Change-Id: Icb1deef1558b09d45fadcaa4092023bf0931e398

--- src/gpu/tessellate/GrDrawAtlasPathOp.cpp
+++ src/gpu/tessellate/GrDrawAtlasPathOp.cpp
@@ -21,7 +21,7 @@ namespace {
 class DrawAtlasPathShader : public GrGeometryProcessor {
 public:
     DrawAtlasPathShader(const GrTextureProxy* atlasProxy, GrSwizzle swizzle, bool isInverseFill,
-                        bool usesLocalCoords)
+                        bool usesLocalCoords, const GrShaderCaps& shaderCaps)
             : GrGeometryProcessor(kDrawAtlasPathShader_ClassID)
             , fAtlasAccess(GrSamplerState::Filter::kNearest, atlasProxy->backendFormat(), swizzle)
             , fAtlasDimensions(atlasProxy->backingStoreDimensions())
@@ -39,6 +39,11 @@ public:
             fAttribs.emplace_back("viewmatrix_trans", kFloat2_GrVertexAttribType, kFloat2_GrSLType);
         }
         this->setInstanceAttributes(fAttribs.data(), fAttribs.count());
+        if (!shaderCaps.vertexIDSupport()) {
+            constexpr static Attribute kUnitCoordAttrib("unit_coord", kFloat2_GrVertexAttribType,
+                                                        kFloat2_GrSLType);
+            this->setVertexAttributes(&kUnitCoordAttrib, 1);
+        }
         this->setTextureSamplerCnt(1);
     }
 
@@ -77,16 +82,22 @@ class DrawAtlasPathShader::Impl : public GrGLSLGeometryProcessor {
         fAtlasAdjustUniform = args.fUniformHandler->addUniform(
                 nullptr, kVertex_GrShaderFlag, kFloat2_GrSLType, "atlas_adjust", &atlasAdjust);
 
+        if (args.fShaderCaps->vertexIDSupport()) {
+            // If we don't have sk_VertexID support then "unit_coord" already came in as a vertex
+            // attrib.
+            args.fVertBuilder->codeAppendf(R"(
+            float2 unit_coord = float2(sk_VertexID & 1, sk_VertexID >> 1);)");
+        }
+
         args.fVertBuilder->codeAppendf(R"(
-        float2 T = float2(sk_VertexID & 1, sk_VertexID >> 1);
         float2 devtopleft = float2(dev_xywh.xy);)");
 
         if (shader.fIsInverseFill) {
             args.fVertBuilder->codeAppendf(R"(
-            float2 devcoord = mix(drawbounds.xy, drawbounds.zw, T);)");
+            float2 devcoord = mix(drawbounds.xy, drawbounds.zw, unit_coord);)");
         } else {
             args.fVertBuilder->codeAppendf(R"(
-            float2 devcoord = abs(float2(dev_xywh.zw)) * T + devtopleft;)");
+            float2 devcoord = abs(float2(dev_xywh.zw)) * unit_coord + devtopleft;)");
         }
 
         args.fVertBuilder->codeAppendf(R"(
@@ -185,6 +196,8 @@ void GrDrawAtlasPathOp::onPrePrepare(GrRecordingContext*, const GrSurfaceProxyVi
     SK_ABORT("DDL support not implemented for GrDrawAtlasPathOp.");
 }
 
+GR_DECLARE_STATIC_UNIQUE_KEY(gUnitQuadBufferKey);
+
 void GrDrawAtlasPathOp::onPrepare(GrOpFlushState* state) {
     SkArenaAlloc* arena = state->allocator();
 
@@ -200,7 +213,7 @@ void GrDrawAtlasPathOp::onPrepare(GrOpFlushState* state) {
     GrSwizzle swizzle = state->caps().getReadSwizzle(fAtlasProxy->backendFormat(),
                                                      GrColorType::kAlpha_8);
     auto shader = arena->make<DrawAtlasPathShader>(fAtlasProxy.get(), swizzle, fIsInverseFill,
-                                                   fUsesLocalCoords);
+                                                   fUsesLocalCoords, *state->caps().shaderCaps());
     fProgram = arena->make<GrProgramInfo>(state->writeView(), pipeline,
                                           &GrUserStencilSettings::kUnused, shader,
                                           GrPrimitiveType::kTriangleStrip, 0,
@@ -217,12 +230,21 @@ void GrDrawAtlasPathOp::onPrepare(GrOpFlushState* state) {
                     GrVertexWriter::If(fUsesLocalCoords, instance->fViewMatrixIfUsingLocalCoords));
         }
     }
+
+    if (!state->caps().shaderCaps()->vertexIDSupport()) {
+        constexpr static SkPoint kUnitQuad[4] = {{0,0}, {0,1}, {1,0}, {1,1}};
+
+        GR_DEFINE_STATIC_UNIQUE_KEY(gUnitQuadBufferKey);
+
+        fVertexBufferIfNoIDSupport = state->resourceProvider()->findOrMakeStaticBuffer(
+                GrGpuBufferType::kVertex, sizeof(kUnitQuad), kUnitQuad, gUnitQuadBufferKey);
+    }
 }
 
 void GrDrawAtlasPathOp::onExecute(GrOpFlushState* state, const SkRect& chainBounds) {
     SkASSERT(fAtlasProxy->isInstantiated());
     state->bindPipelineAndScissorClip(*fProgram, this->bounds());
     state->bindTextures(fProgram->geomProc(), *fAtlasProxy, fProgram->pipeline());
-    state->bindBuffers(nullptr, std::move(fInstanceBuffer), nullptr);
+    state->bindBuffers(nullptr, std::move(fInstanceBuffer), fVertexBufferIfNoIDSupport);
     state->drawInstanced(fInstanceCount, fBaseInstance, 4, 0);
 }
