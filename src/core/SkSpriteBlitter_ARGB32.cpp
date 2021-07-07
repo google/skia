379804d5fbc084c3e88ee34f884eda5b1894591b
Mike Reed <reed@google.com>
2021-07-07 11:07:25 -0400

    Only use legacy sprite for src and srcover
    Change-Id: I5e7b50b598268696ae00d408f458673e971b0795

--- src/core/SkSpriteBlitter_ARGB32.cpp
+++ src/core/SkSpriteBlitter_ARGB32.cpp
@@ -12,20 +12,20 @@
 #include "src/core/SkArenaAlloc.h"
 #include "src/core/SkBlitRow.h"
 #include "src/core/SkSpriteBlitter.h"
-#include "src/core/SkXfermodePriv.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 
 class Sprite_D32_S32 : public SkSpriteBlitter {
 public:
-    Sprite_D32_S32(const SkPixmap& src, U8CPU alpha)  : INHERITED(src) {
+    Sprite_D32_S32(const SkPixmap& src, SkBlendMode mode, U8CPU alpha)  : INHERITED(src) {
         SkASSERT(src.colorType() == kN32_SkColorType);
+        SkASSERT(mode == SkBlendMode::kSrcOver || mode == SkBlendMode::kSrc);
 
         unsigned flags32 = 0;
         if (255 != alpha) {
             flags32 |= SkBlitRow::kGlobalAlpha_Flag32;
         }
-        if (!src.isOpaque()) {
+        if (!src.isOpaque() && mode == SkBlendMode::kSrcOver) {
             flags32 |= SkBlitRow::kSrcPixelAlpha_Flag32;
         }
 
@@ -56,40 +56,6 @@ private:
     using INHERITED = SkSpriteBlitter;
 };
 
-///////////////////////////////////////////////////////////////////////////////
-
-class Sprite_D32_S32A_Xfer: public SkSpriteBlitter {
-public:
-    Sprite_D32_S32A_Xfer(const SkPixmap& source, const SkPaint& paint) : SkSpriteBlitter(source) {
-        fXfermode = SkXfermode::Peek(paint.getBlendMode());
-        SkASSERT(fXfermode);
-    }
-
-    void blitRect(int x, int y, int width, int height) override {
-        SkASSERT(width > 0 && height > 0);
-        uint32_t* SK_RESTRICT dst = fDst.writable_addr32(x, y);
-        const uint32_t* SK_RESTRICT src = fSource.addr32(x - fLeft, y - fTop);
-        size_t dstRB = fDst.rowBytes();
-        size_t srcRB = fSource.rowBytes();
-        SkXfermode* xfermode = fXfermode;
-
-        do {
-            xfermode->xfer32(dst, src, width, nullptr);
-
-            dst = (uint32_t* SK_RESTRICT)((char*)dst + dstRB);
-            src = (const uint32_t* SK_RESTRICT)((const char*)src + srcRB);
-        } while (--height != 0);
-    }
-
-protected:
-    SkXfermode* fXfermode;
-
-private:
-    using INHERITED = SkSpriteBlitter;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-
 SkSpriteBlitter* SkSpriteBlitter::ChooseL32(const SkPixmap& source, const SkPaint& paint,
                                             SkArenaAlloc* allocator) {
     SkASSERT(allocator != nullptr);
@@ -101,16 +67,10 @@ SkSpriteBlitter* SkSpriteBlitter::ChooseL32(const SkPixmap& source, const SkPain
         return nullptr;
     }
 
-    U8CPU alpha = paint.getAlpha();
-
     if (source.colorType() == kN32_SkColorType) {
-        if (paint.isSrcOver()) {
-            // this can handle alpha, but not xfermode
-            return allocator->make<Sprite_D32_S32>(source, alpha);
-        }
-        if (255 == alpha) {
-            // this can handle an xfermode, but not alpha
-            return allocator->make<Sprite_D32_S32A_Xfer>(source, paint);
+        const auto bm = paint.asBlendMode();
+        if (bm == SkBlendMode::kSrcOver || bm == SkBlendMode::kSrc) {
+            return allocator->make<Sprite_D32_S32>(source, bm.value(), paint.getAlpha());
         }
     }
     return nullptr;
