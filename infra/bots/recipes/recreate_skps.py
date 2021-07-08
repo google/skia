490b20ab2e03041a414f3a73be8d9f6fb9182b47
Eric Boren <borenet@google.com>
2021-07-08 08:50:36 -0400

    [infra] Update RecreateSKPs recipe after asset changes
    Change-Id: I0c913bdf252d3ecdc65577281dfa4e0ea9027ff8

--- infra/bots/recipes/recreate_skps.py
+++ infra/bots/recipes/recreate_skps.py
@@ -62,33 +62,43 @@ def RunSteps(api):
     # Build Chrome.
     api.run(api.step, 'Build Chrome', cmd=['ninja', '-C', out_dir, 'chrome'])
 
-  # Clean up the output dir.
-  output_dir = api.path['start_dir'].join('skp_output')
-  if api.path.exists(output_dir):
-    api.run.rmtree(output_dir)
-  api.file.ensure_directory('makedirs skp_output', output_dir)
-
-  # Capture the SKPs.
-  asset_dir = skia_dir.join('infra', 'bots', 'assets', 'skp')
-  cmd = ['python', asset_dir.join('create.py'),
-         '--chrome_src_path', src_dir,
-         '--browser_executable', src_dir.join('out', 'Release', 'chrome'),
-         '--target_dir', output_dir]
-  if 'DryRun' not in api.properties['buildername']:
-    cmd.append('--upload_to_partner_bucket')
   with api.context(cwd=skia_dir):
+    # Fetch `sk`
+    api.run(api.step, 'Fetch SK',
+            cmd=['python3', skia_dir.join('bin', 'fetch-sk')])
+
+    # Recreate SKPs.
+    script = skia_dir.join('infra', 'bots', 'assets', 'skp',
+                           'create_and_upload.py')
+    cmd = [
+      'vpython3', '-u', script,
+      '--chrome_src_path', src_dir,
+      '--browser_executable', out_dir.join('chrome'),
+    ]
+    if 'DryRun' in api.properties['buildername']:
+      cmd.append('--dry_run')
     api.run(api.step, 'Recreate SKPs', cmd=cmd)
 
-  # Upload the SKPs.
-  if 'DryRun' not in api.properties['buildername']:
-    cmd = ['python',
-           skia_dir.join('infra', 'bots', 'upload_skps.py'),
-           '--target_dir', output_dir,
-           '--chromium_path', src_dir]
-    upload_env = api.infra.go_env
-    upload_env['GCLIENT_PY3'] = 0
-    with api.context(cwd=skia_dir, env=upload_env):
-      api.run(api.step, 'Upload SKPs', cmd=cmd)
+    if 'DryRun' in api.properties['buildername']:
+      return
+
+    # Regenerate tasks.json.
+    api.run(api.step, 'Regenerate tasks.json',
+            cmd=['go', 'run', './infra/bots/gen_tasks.go'])
+    
+    # Upload a CL.
+    commit_msg = '''Update SKP version
+
+Automatic commit by the RecreateSKPs bot.
+'''
+    api.run(api.step, 'git commit',
+            cmd=['git', 'commit', '-a', '-m', commit_msg])
+    cmd = [
+      'git', 'cl', 'upload',
+      '--bypass-hooks', '--use-commit-queue',
+      '--tbrs=rmistry@google.com',
+    ]
+    api.run(api.step, 'git cl upload', cmd=cmd)
 
 
 def GenTests(api):
@@ -115,12 +125,12 @@ def GenTests(api):
   )
 
   yield (
-      api.test('failed_upload') +
+      api.test('failed_recreate') +
       api.properties(buildername=builder,
                      repository='https://skia.googlesource.com/skia.git',
                      revision='abc123',
                      path_config='kitchen',
                      swarm_out_dir='[SWARM_OUT_DIR]') +
       api.path.exists(api.path['start_dir'].join('skp_output')) +
-      api.step_data('Upload SKPs', retcode=1)
+      api.step_data('Recreate SKPs', retcode=1)
   )
