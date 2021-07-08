c22dce4bc24bd6bc05f24e8ab29aa1ec1dbfd17b
Mike Reed <reed@google.com>
2021-07-08 10:43:31 -0400

    Arithmetic blender
    ... to aid in removing special ArithmeticImageFilter.
    
    Can we use something like this to reimpl the ArithmeticImageFilter
    --> BlenderImageFilter?
    
    I see some special checking in filterBounds for ArithIF -- is that
    required? Is that generalizable for any BlenderImageFilter?
    If we can generalize blend[er]imagefilter, let's just move this
    logic into Chrome -- no need for Arithmetic to appear in Skia
    in any form!
    
    Re: https://skia-review.googlesource.com/c/skia/+/424436
    
    Change-Id: Ifa0ff2fa25de6385f6f60505e73ecc28463a80b3

---
+++ src/effects/SkBlenders.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/effects/SkBlenders.h"
+#include "include/effects/SkRuntimeEffect.h"
+
+sk_sp<SkBlender> SkBlenders::Arithmetic(float k1, float k2, float k3, float k4,
+                                        bool enforcePremul) {
+    if (!SkScalarIsFinite(k1) ||
+        !SkScalarIsFinite(k2) ||
+        !SkScalarIsFinite(k3) ||
+        !SkScalarIsFinite(k4)) {
+        return nullptr;
+    }
+
+    // Are we nearly a SkBlendMode?
+    const struct {
+        float       k1, k2, k3, k4;
+        SkBlendMode mode;
+    } table[] = {
+        { 0, 1, 0, 0, SkBlendMode::kSrc   },
+        { 0, 0, 1, 0, SkBlendMode::kDst   },
+        { 0, 0, 0, 0, SkBlendMode::kClear },
+    };
+    for (const auto& t : table) {
+        if (SkScalarNearlyEqual(k1, t.k1) &&
+            SkScalarNearlyEqual(k2, t.k2) &&
+            SkScalarNearlyEqual(k3, t.k3) &&
+            SkScalarNearlyEqual(k4, t.k4)) {
+            return SkBlender::Mode(t.mode);
+        }
+    }
+
+    // If we get here, we need the actual blender effect.
+
+    static SkRuntimeEffect* gArithmeticEffect = []{
+        const char prog[] = R"(
+            uniform half4   k;
+            uniform half    pmClamp;
+
+            half4 main(half4 src, half4 dst) {
+                half4 c = k.x * src * dst + k.y * src + k.z * dst + k.w;
+                c.rgb = min(c.rgb, max(c.a, pmClamp));
+                // rely on skia to saturate our alpha
+                return c;
+            }
+        )";
+        auto result = SkRuntimeEffect::MakeForBlender(SkString(prog));
+        SkASSERTF(result.effect, "SkBlenders::Arithmetic: %s", result.errorText.c_str());
+        return result.effect.release();
+    }();
+
+    const float array[] = {
+        k1, k2, k3, k4,
+        enforcePremul ? 0.0f : 1.0f,
+    };
+    return gArithmeticEffect->makeBlender(SkData::MakeWithCopy(array, sizeof(array)));
+}
