2a4285f0124a7858bbcf29a83985aaf87d2109a1
John Stiles <johnstiles@google.com>
2021-07-09 14:08:33 -0400

    Enforce consistency in swizzle domains.
    GLSL disallows mixing swizzle domains within a single swizzle:
    http://screen/93eHNQDbx35hMdk
    SkSL now disallows it as well.
    
    Change-Id: Ied2e11ee04285b143a864e28cac30335f01aad0e
    Bug: skia:10621

---
+++ tests/sksl/errors/SwizzleDomain.glsl
@@ -0,0 +1,19 @@
+### Compilation failed:
+
+error: 5: invalid swizzle mask 'xyra'
+error: 6: invalid swizzle mask 'zxtq'
+error: 7: invalid swizzle mask 'wwRB'
+error: 9: invalid swizzle mask 'rgxy'
+error: 10: invalid swizzle mask 'bast'
+error: 11: invalid swizzle mask 'gbLT'
+error: 13: invalid swizzle mask 'sxyz'
+error: 14: invalid swizzle mask 'tpbb'
+error: 15: invalid swizzle mask 'qsTR'
+error: 17: invalid swizzle mask 'LTxy'
+error: 18: invalid swizzle mask 'TRba'
+error: 19: invalid swizzle mask 'RBst'
+error: 21: invalid swizzle mask 'xrsL'
+error: 22: invalid swizzle mask 'ygtT'
+error: 23: invalid swizzle mask 'zbpR'
+error: 24: invalid swizzle mask 'waqB'
+16 errors
