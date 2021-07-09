4957a501f45ea69aa6408f6c4a031373ee283dd0
John Stiles <johnstiles@google.com>
2021-07-09 12:11:49 -0400

    Add support for matrix-to-vector conversions in SkSL.
    GLSL supports casting vec4 into mat2 and vice versa, so SkSL should have
    equivalent support. This CL allows the Compound constructor to take a
    matrix as input, and fixes up backends to do the right thing when a
    matrix shows up in the compound-constructor path.
    
    Change-Id: I13289ad0a27ba59bddc3706093820594efebc693
    Bug: skia:12067

---
+++ tests/sksl/errors/MatrixToVectorCast3x3.glsl
@@ -0,0 +1,7 @@
+### Compilation failed:
+
+error: 4: invalid argument to 'half' constructor (expected a number or bool, but found 'half3x3')
+error: 5: 'half3x3' is not a valid parameter to 'half2' constructor
+error: 6: 'half3x3' is not a valid parameter to 'half3' constructor
+error: 7: 'half3x3' is not a valid parameter to 'half4' constructor
+4 errors
---
+++ tests/sksl/errors/MatrixToVectorCastTooSmall.glsl
@@ -0,0 +1,7 @@
+### Compilation failed:
+
+error: 4: invalid argument to 'half' constructor (expected a number or bool, but found 'half2x2')
+error: 5: 'half2x2' is not a valid parameter to 'half2' constructor
+error: 6: 'half2x2' is not a valid parameter to 'half3' constructor
+error: 7: global variable initializer must be a constant expression
+4 errors
