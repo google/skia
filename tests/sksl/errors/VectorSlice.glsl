ce7cdcd7b48fb8d84a736cf81515ff85cfbe9d6b
John Stiles <johnstiles@google.com>
2021-07-12 15:48:27 -0400

    Add test for SkSL vector-slice constructors.
    SkSL does not support shrinking a vector via casting. Use a swizzle
    instead.
    
    Change-Id: Ieba78a05dad9c55f44c765924e28f0c7e1667a67
    Bug: skia:12193
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/427198
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

---
+++ tests/sksl/errors/VectorSlice.glsl
@@ -0,0 +1,12 @@
+### Compilation failed:
+
+error: 7: invalid arguments to 'half3' constructor (expected 3 scalars, but found 4)
+error: 8: invalid arguments to 'half2' constructor (expected 2 scalars, but found 4)
+error: 9: invalid argument to 'half' constructor (expected a number or bool, but found 'half4')
+error: 12: invalid arguments to 'int3' constructor (expected 3 scalars, but found 4)
+error: 13: invalid arguments to 'int2' constructor (expected 2 scalars, but found 4)
+error: 14: invalid argument to 'int' constructor (expected a number or bool, but found 'int4')
+error: 17: invalid arguments to 'bool3' constructor (expected 3 scalars, but found 4)
+error: 18: invalid arguments to 'bool2' constructor (expected 2 scalars, but found 4)
+error: 19: invalid argument to 'bool' constructor (expected a number or bool, but found 'bool4')
+9 errors
