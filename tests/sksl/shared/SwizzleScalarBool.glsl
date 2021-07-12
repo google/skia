8add92585ccb56766d070fd5964e7e80f22dc7cf
John Stiles <johnstiles@google.com>
2021-07-12 15:05:54 -0400

    Allow swizzles on Boolean scalar expressions.
    Boolean vector expressions already allowed swizzles (see the
    SwizzleBoolConstants.sksl test), but scalars had been inadvertently
    disallowed.
    
    Change-Id: I89e7139db50981f0ee1a9a5086b02603e57f967d
    Bug: skia:12195

--- tests/sksl/shared/SwizzleScalarBool.glsl
+++ tests/sksl/shared/SwizzleScalarBool.glsl
@@ -1,4 +1,11 @@
-### Compilation failed:
 
-error: 5: cannot swizzle value of type 'bool'
-1 error
+out vec4 sk_FragColor;
+uniform float unknownInput;
+vec4 main() {
+    bool b = bool(unknownInput);
+    bvec4 b4 = bvec4(b);
+    b4 = bvec4(bvec2(b), false, true);
+    b4 = bvec4(false, b, true, false);
+    b4 = bvec4(false, b, false, b);
+    return vec4(b4);
+}
