7dc26fadc90b3bd1635f4eb26aa1325361c1a6f1
Ben Wagner <bungeman@google.com>
2021-07-14 17:43:22 -0400

    Call hb_face_count before hb_face_create
    As pointed out at https://github.com/harfbuzz/harfbuzz/issues/248 one
    should call hb_face_count to do a sanitizer check before calling
    hb_face_create.
    
    Change-Id: I71795962a027188d702274f84e3bfa6c15fcd130
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/428322
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>

--- modules/skshaper/src/SkShaper_harfbuzz.cpp
+++ modules/skshaper/src/SkShaper_harfbuzz.cpp
@@ -271,13 +271,16 @@ HBFace create_hb_face(const SkTypeface& typeface) {
     HBFace face;
     if (typefaceAsset && typefaceAsset->getMemoryBase()) {
         HBBlob blob(stream_to_blob(std::move(typefaceAsset)));
-        face.reset(hb_face_create(blob.get(), (unsigned)index));
-        // hb_face_create always succeeds. Check number of glyphs to force a minimal
-        // sanitization step. hb_face_get_upem doesn't work as it defaults to 1000.
+        // hb_face_create always succeeds. Check that the format is minimally recognized first.
         // hb_face_create_for_tables may still create a working hb_face.
         // See https://github.com/harfbuzz/harfbuzz/issues/248 .
-        if (face && hb_face_get_glyph_count(face.get()) == 0) {
-            face.reset();
+        unsigned int num_hb_faces = hb_face_count(blob.get());
+        if (0 < num_hb_faces && (unsigned)index < num_hb_faces) {
+            face.reset(hb_face_create(blob.get(), (unsigned)index));
+            // Check the number of glyphs as a basic sanitization step.
+            if (face && hb_face_get_glyph_count(face.get()) == 0) {
+                face.reset();
+            }
         }
     }
     if (!face) {
