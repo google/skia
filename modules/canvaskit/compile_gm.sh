ad858e76e339d37743c0c389b061f88599663ecf
Kevin Lubick <kjlubick@google.com>
2021-07-22 12:13:21 -0400

    [canvaskit] Fix gm compilation
    This removes some files that were deleted in early July 2021
    
    Change-Id: I3a79a0d58eea16b4e14a548261f1279497860275
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/431259
    Reviewed-by: Brian Osman <brianosman@google.com>

--- modules/canvaskit/compile_gm.sh
+++ modules/canvaskit/compile_gm.sh
@@ -173,16 +173,6 @@ fi
 GMS_TO_BUILD="gm/*.cpp"
 TESTS_TO_BUILD="tests/*.cpp"
 
-# DSL FP tests need to include compiled test shaders from the `dslfp` subdirectory.
-TESTS_TO_BUILD+=\
-" tests/sksl/dslfp/GrDSLFPTest_DoStatement.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_ForStatement.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_IfStatement.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_SwitchStatement.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_Swizzle.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_Ternary.dsl.cpp"\
-" tests/sksl/dslfp/GrDSLFPTest_WhileStatement.dsl.cpp"
-
 # When developing locally, it can be faster to focus only on the gms or tests you care about
 # (since they all have to be recompiled/relinked) every time. To do so, mark the following as true
 if false; then
