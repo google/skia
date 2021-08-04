9fdcc517b2becce1738d6f2b01fa939da5f16f71
John Stiles <johnstiles@google.com>
2021-08-04 13:27:05 -0400

    Write test demonstrating bug with array narrowing conversions.
    We don't currently support this. There's no explicit syntax to cast an
    array's type, but it can be implicitly required in some situations, like
    `halfArray == floatArray` (when fAllowNarrowingConversions is on).
    
    Change-Id: I00fe0ddd4f2682b2950e828dd78bb941d5f0430e
    Bug: skia:12248
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/436560
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/sksl/SkSLMain.cpp
+++ src/sksl/SkSLMain.cpp
@@ -208,6 +208,9 @@ static bool detect_shader_settings(const SkSL::String& text,
                     static auto s_version450CoreCaps = Factory::Version450Core();
                     *caps = s_version450CoreCaps.get();
                 }
+                if (settingsText.consumeSuffix(" AllowNarrowingConversions")) {
+                    settings->fAllowNarrowingConversions = true;
+                }
                 if (settingsText.consumeSuffix(" ForceHighPrecision")) {
                     settings->fForceHighPrecision = true;
                 }
---
+++ tests/sksl/shared/ArrayNarrowingConversions.glsl
@@ -0,0 +1,4 @@
+### Compilation failed:
+
+error: 12: type mismatch: '==' cannot operate on 'int[2]', 'short[2]'
+1 error
