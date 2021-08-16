9ded59bbadb354e20d316e36db0ff9a8aa64e54c
Brian Osman <brianosman@google.com>
2021-08-16 21:30:57 +0000

    Revert "Update rewrite_includes.py for python3"
    This reverts commit bd40fb55bb115b41f75924dcfebc2ddd63169aa4.
    
    Reason for revert: I give up.
    
    Original change's description:
    > Update rewrite_includes.py for python3
    >
    > Change-Id: Ib1b6f8c17554f8121bd2351b5b214e1b2a79f758
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/439776
    > Reviewed-by: Kevin Lubick <kjlubick@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    
    TBR=kjlubick@google.com,brianosman@google.com,skcq-be@skia-corp.google.com.iam.gserviceaccount.com
    
    Change-Id: I6be08249f805cce00d35e28e457fe5628b447629
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/439940
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- tools/rewrite_includes.py
+++ tools/rewrite_includes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python2
 #
 # Copyright 2019 Google Inc.
 #
@@ -6,11 +6,11 @@
 # found in the LICENSE file.
 
 
+from __future__ import print_function
+import StringIO
 import argparse
 import os
 import sys
-
-from io import StringIO
 
 
 parser = argparse.ArgumentParser()
@@ -89,7 +89,7 @@ for file_path in to_rewrite():
     lines = open(file_path).readlines()
 
     # Write it back out again line by line with substitutions for #includes.
-    output = StringIO() if args.dry_run else open(file_path, 'w')
+    output = StringIO.StringIO() if args.dry_run else open(file_path, 'wb')
 
     includes = []
     for line in lines:
