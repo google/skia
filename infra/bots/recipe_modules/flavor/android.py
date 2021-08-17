957ed75731e015482bbd71ad512867aa8bdfbcd5
Leon Scroggins <scroggo@google.com>
2021-08-17 17:40:34 +0000

    Revert "Update bots' NDK to r23"
    This reverts commit 3bce5d13972fec9e773583eda5d3357a70c25e9e.
    
    Reason for revert: Speculative fix for ASAN failures on waterfall
    
    Original change's description:
    > Update bots' NDK to r23
    >
    > Bug: skia:12273
    > Bug: skia:10754
    >
    > The latest version (r23) includes APIs from Android 12. It also allows
    > referencing enums and structs even if __ANDROID_API__ is not set to the
    > level that included them. This allows code to reference them and use
    > dlsym to access the methods. This will ultimately allow Skia (or client
    > - in this case Flutter) to use a single build to use the NDK APIs if
    > present and fail gracefully if not.
    >
    > With r23, the Mac version of the NDK is now in a DMG, so rewrite its
    > script to properly download, mount, and eject it.
    >
    > Also update the path to asan_device_setup. (See
    > Iae6515b7e78c7660b4fb9fe32fd969ba563c4517.)
    >
    > Change-Id: I4b2eb4f49a1d45a3c18d2399f6b8006668c310ea
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/439336
    > Reviewed-by: Eric Boren <borenet@google.com>
    > Reviewed-by: Derek Sollenberger <djsollen@google.com>
    > Commit-Queue: Leon Scroggins <scroggo@google.com>
    
    TBR=borenet@google.com,djsollen@google.com,scroggo@google.com,skcq-be@skia-corp.google.com.iam.gserviceaccount.com
    
    Change-Id: I77f803c51cba6672958210b5270b8b4301e12301
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:12273
    Bug: skia:10754
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/440176
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Commit-Queue: Leon Scroggins <scroggo@google.com>

--- infra/bots/recipe_modules/flavor/android.py
+++ infra/bots/recipe_modules/flavor/android.py
@@ -351,7 +351,7 @@ if actual_freq != str(freq):
   def _asan_setup_path(self):
     return self.m.vars.workdir.join(
         'android_ndk_linux', 'toolchains', 'llvm', 'prebuilt', 'linux-x86_64',
-        'lib64', 'clang', '12.0.5', 'bin', 'asan_device_setup')
+        'lib64', 'clang', '9.0.8', 'bin', 'asan_device_setup')
 
 
   def install(self):
