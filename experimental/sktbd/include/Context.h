fab00952d3619c64574657a64f1b59b6a4c9e7e9
Robert Phillips <robertphillips@google.com>
2021-09-20 17:15:16 -0400

    Establish beachhead
    Change-Id: Ib585bf93da5386c353c66baeb00027d31bdbec8f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/450480
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

---
+++ experimental/sktbd/include/Context.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef Context_DEFINED
+#define Context_DEFINED
+
+namespace sktbd {
+
+class Context {
+public:
+    Context();
+private:
+};
+
+} // namespace sktbd
+
+#endif // Context_DEFINED
+
---
+++ experimental/sktbd/src/Context.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "experimental/sktbd/include/Context.h"
+
+namespace sktbd {
+
+Context::Context() {}
+
+} // namespace sktbd
