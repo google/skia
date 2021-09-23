45e3838006df8bc393fe6f4ca1cff999b41167f4
John Stiles <johnstiles@google.com>
2021-09-23 16:26:53 -0400

    Rewrite switch statements in GLSL strict-ES2 mode.
    Once this lands, switch statements will work everywhere--Metal, SPIR-V,
    GLSL, and SkVM.
    
    Change-Id: I2797d0a872de8be77bb9f7aa6acb93421d571d70
    Bug: skia:12450
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/452356
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

---
+++ tests/sksl/runtime/SwitchDefaultOnly.glsl
@@ -0,0 +1,9 @@
+
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+vec4 main() {
+    int _tmpSwitchValue1 = int(colorGreen.y), _tmpSwitchFallthrough0 = 0; 
+    for (int _tmpSwitchLoop2 = 0; _tmpSwitchLoop2 < 1; _tmpSwitchLoop2++) {
+        return colorGreen;
+    }
+}
---
+++ tests/sksl/runtime/SwitchWithFallthrough.glsl
@@ -0,0 +1,53 @@
+
+uniform vec4 colorGreen;
+uniform vec4 colorRed;
+bool switch_fallthrough_twice_bi(int value) {
+    bool ok = false;
+    int _tmpSwitchValue1 = value, _tmpSwitchFallthrough0 = 0; 
+    for (int _tmpSwitchLoop2 = 0; _tmpSwitchLoop2 < 1; _tmpSwitchLoop2++) {
+        if ((_tmpSwitchValue1 == 0)) {
+            break;
+            _tmpSwitchFallthrough0 = 1;
+        }
+        if ((_tmpSwitchFallthrough0 > 0) || (_tmpSwitchValue1 == 1)) {
+            {
+            }
+            _tmpSwitchFallthrough0 = 1;
+        }
+        if ((_tmpSwitchFallthrough0 > 0) || (_tmpSwitchValue1 == 2)) {
+            {
+            }
+            _tmpSwitchFallthrough0 = 1;
+        }
+        if ((_tmpSwitchFallthrough0 > 0) || (_tmpSwitchValue1 == 3)) {
+            ok = true;
+            break;
+            _tmpSwitchFallthrough0 = 1;
+        }
+        break;
+    }
+    return ok;
+}
+vec4 main() {
+    int x = int(colorGreen.y);
+    bool _0_ok = false;
+    int _tmpSwitchValue4 = x, _tmpSwitchFallthrough3 = 0; 
+    for (int _tmpSwitchLoop5 = 0; _tmpSwitchLoop5 < 1; _tmpSwitchLoop5++) {
+        if ((_tmpSwitchValue4 == 2)) {
+            break;
+            _tmpSwitchFallthrough3 = 1;
+        }
+        if ((_tmpSwitchFallthrough3 > 0) || (_tmpSwitchValue4 == 1)) {
+            {
+            }
+            _tmpSwitchFallthrough3 = 1;
+        }
+        if ((_tmpSwitchFallthrough3 > 0) || (_tmpSwitchValue4 == 0)) {
+            _0_ok = true;
+            break;
+            _tmpSwitchFallthrough3 = 1;
+        }
+        break;
+    }
+    return _0_ok && switch_fallthrough_twice_bi(x) ? colorGreen : colorRed;
+}
