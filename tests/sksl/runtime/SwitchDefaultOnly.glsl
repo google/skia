d26d0e6a47aea2575f29b4582a4f662a17a8fff2
Brian Osman <brianosman@google.com>
2021-09-24 13:10:15 +0000

    Revert "Rewrite switch statements in GLSL strict-ES2 mode."
    This reverts commit 45e3838006df8bc393fe6f4ca1cff999b41167f4.
    
    Reason for revert: Also need to rewrite them in actual ES2 mode.
    
    Original change's description:
    > Rewrite switch statements in GLSL strict-ES2 mode.
    >
    > Once this lands, switch statements will work everywhere--Metal, SPIR-V,
    > GLSL, and SkVM.
    >
    > Change-Id: I2797d0a872de8be77bb9f7aa6acb93421d571d70
    > Bug: skia:12450
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/452356
    > Commit-Queue: John Stiles <johnstiles@google.com>
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    
    Bug: skia:12450
    Change-Id: I92656ed40289872405c0873f2c56a52b04e35b1d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/452556
    Auto-Submit: Brian Osman <brianosman@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- tests/sksl/runtime/SwitchDefaultOnly.glsl
+++
@@ -1,9 +0,0 @@
-
-uniform vec4 colorGreen;
-uniform vec4 colorRed;
-vec4 main() {
-    int _tmpSwitchValue1 = int(colorGreen.y), _tmpSwitchFallthrough0 = 0; 
-    for (int _tmpSwitchLoop2 = 0; _tmpSwitchLoop2 < 1; _tmpSwitchLoop2++) {
-        return colorGreen;
-    }
-}
