d240c6bebfceefcb6f5c9d5cdff1b4623a1da20c
John Stiles <johnstiles@google.com>
2021-09-30 12:31:30 -0400

    Rename external function named `external` in test code.
    `external` is a GLSL reserved word: http://screen/85m4iRwvJRadKbV
    
    Change-Id: I4b4bff378a75f83a7824c7cfd33b9802bea257a7
    Bug: skia:11115
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/454738
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Ethan Nicholas <ethannicholas@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

--- tests/SkSLInterpreterTest.cpp
+++ tests/SkSLInterpreterTest.cpp
@@ -894,9 +894,9 @@ DEF_TEST(SkSLInterpreterExternalFunction, r) {
     GrShaderCaps caps(GrContextOptions{});
     SkSL::Compiler compiler(&caps);
     SkSL::Program::Settings settings;
-    const char* src = "float main() { return external(25); }";
+    const char* src = "float main() { return externalSqrt(25); }";
     std::vector<std::unique_ptr<SkSL::ExternalFunction>> externalFunctions;
-    externalFunctions.push_back(std::make_unique<ExternalSqrt>("external", compiler));
+    externalFunctions.push_back(std::make_unique<ExternalSqrt>("externalSqrt", compiler));
     settings.fExternalFunctions = &externalFunctions;
     std::unique_ptr<SkSL::Program> program = compiler.convertProgram(
             SkSL::ProgramKind::kGeneric, SkSL::String(src), settings);
