319d75d6f60fbedbaf16e5454bc849854b03ddee
John Stiles <johnstiles@google.com>
2021-10-15 17:18:09 -0400

    Add error test for unary-negating and unary-plussing arrays.
    At present, we only detect four errors here. We should detect six.
    
    Change-Id: I226854ab930a273695c42cf2f7bdb1d5cd97e50b
    Bug: oss-fuzz:39998
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/459882
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>

---
+++ tests/sksl/errors/ArrayNegation.glsl
@@ -0,0 +1,7 @@
+### Compilation failed:
+
+error: 2: '-' cannot operate on 'int4[123]'
+error: 4: '-' cannot operate on 'half3[123]'
+error: 5: '-' cannot operate on 'bool2[123]'
+error: 6: '-' cannot operate on 'half4x4[123]'
+4 errors
