cb04e3981f3f2a102f7c69644cfa5f43f0ac56fd
Joe Gregorio <jcgregorio@google.com>
2021-10-25 17:21:10 -0400

    [mac] CPU dimensions are too precise and miss new Macminis in the pool.
    - Drop the following dimensions:
       reserved_for_xcode_version:*
       gpu:none
    
    - Loosen the cpu dimension.
    - Restrict to cores:12 to avoid using the VMs.
    
    The failing iPhone6 bots are failing on the main waterfall today
    so they are WAI.
    
    See also: https://bugs.chromium.org/p/chromium/issues/detail?id=1257420
    
    Change-Id: I9f0eef7c6904e652a011937ab99c465e9d483172
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/463141
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -793,7 +793,7 @@ func (b *taskBuilder) defaultSwarmDimensions() {
 				"Pixel2XL":        {"taimen", "PPR1.180610.009"},
 				"Pixel3":          {"blueline", "PQ1A.190105.004"},
 				"Pixel3a":         {"sargo", "QP1A.190711.020"},
-				"Pixel4":          {"flame", "RPB2.200611.009"}, // R Preview
+				"Pixel4":          {"flame", "RPB2.200611.009"},       // R Preview
 				"Pixel4a":         {"sunfish", "AOSP.MASTER_7819821"}, // Pixel4a flashed with an Android HWASan build.
 				"Pixel4XL":        {"coral", "QD1A.190821.011.C4"},
 				"Pixel5":          {"redfin", "RD1A.200810.022.A4"},
@@ -957,8 +957,10 @@ func (b *taskBuilder) defaultSwarmDimensions() {
 			// Use many-core machines for Build tasks.
 			d["machine_type"] = MACHINE_TYPE_LARGE
 		} else if d["os"] == DEFAULT_OS_MAC {
-			// Mac CPU bots.
-			d["cpu"] = "x86-64-E5-2697_v2"
+			// Mac CPU bots are no longer VMs.
+			d["cpu"] = "x86-64"
+			d["cores"] = "12"
+			delete(d, "gpu")
 		}
 	}
 
@@ -1246,12 +1248,6 @@ func (b *jobBuilder) compile() string {
 				}
 				if b.extraConfig("iOS") {
 					b.asset("provisioning_profile_ios")
-				}
-				// See skbug.com/11129 for more
-				if b.compiler("Xcode11.4.1") {
-					b.dimension("reserved_for_xcode_version:11.4.1")
-				} else {
-					b.dimension("reserved_for_xcode_version:newest")
 				}
 			}
 		})
