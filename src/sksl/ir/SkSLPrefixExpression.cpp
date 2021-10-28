f2d016f12e224d4a85cd690577cc9a8411731f2a
John Stiles <johnstiles@google.com>
2021-10-28 11:27:36 -0400

    Fix for fuzzer-discovered error with negation.
    Yesterday's negation-related changes (http://review.skia.org/464123)
    exposed a flaw that the fuzzer was able to exploit. We were previously
    able to assume that `simplify_negation` would always return a non-null
    expression; in some cases, that is no longer true.
    
    Change-Id: Ia585232b0e35fafe0c642384a59ef94ce743ffd5
    Bug: oss-fuzz:40427
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/464916
    Commit-Queue: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/sksl/ir/SkSLPrefixExpression.cpp
+++ src/sksl/ir/SkSLPrefixExpression.cpp
@@ -66,8 +66,11 @@ static std::unique_ptr<Expression> simplify_negation(const Context& context,
             // Convert `-matrix(literal)` into `matrix(-literal)`.
             if (context.fConfig->fSettings.fOptimize && value->isCompileTimeConstant()) {
                 const ConstructorDiagonalMatrix& ctor = value->as<ConstructorDiagonalMatrix>();
-                return ConstructorDiagonalMatrix::Make(context, originalExpr.fLine, ctor.type(),
-                                                      simplify_negation(context, *ctor.argument()));
+                if (std::unique_ptr<Expression> simplified = simplify_negation(context,
+                                                                               *ctor.argument())) {
+                    return ConstructorDiagonalMatrix::Make(context, originalExpr.fLine, ctor.type(),
+                                                           std::move(simplified));
+                }
             }
             break;
 
@@ -75,8 +78,11 @@ static std::unique_ptr<Expression> simplify_negation(const Context& context,
             // Convert `-vector(literal)` into `vector(-literal)`.
             if (context.fConfig->fSettings.fOptimize && value->isCompileTimeConstant()) {
                 const ConstructorSplat& ctor = value->as<ConstructorSplat>();
-                return ConstructorSplat::Make(context, originalExpr.fLine, ctor.type(),
-                                              simplify_negation(context, *ctor.argument()));
+                if (std::unique_ptr<Expression> simplified = simplify_negation(context,
+                                                                               *ctor.argument())) {
+                    return ConstructorSplat::Make(context, originalExpr.fLine, ctor.type(),
+                                                  std::move(simplified));
+                }
             }
             break;
 
---
+++ tests/sksl/errors/Ossfuzz40427.glsl
@@ -0,0 +1,5 @@
+### Compilation failed:
+
+error: 1: integer is out of range for type 'uint': -1
+error: 1: expected 'uint', but found 'uint2'
+2 errors
