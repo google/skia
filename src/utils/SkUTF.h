90c9424cdffb1353fd3a1cf8ea3468c516749d6f
Kevin Lubick <kjlubick@google.com>
2021-11-03 13:39:19 -0400

    Remove SPI from static inline functions
    Change-Id: I94e32f56a0cadcccac399de4da62a1091b5d5071
    Bug: skia:12584
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/467416
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- src/utils/SkUTF.h
+++ src/utils/SkUTF.h
@@ -81,13 +81,13 @@ SK_SPI int UTF16ToUTF8(char dst[], int dstCapacity, const uint16_t src[], size_t
  * Given a UTF-16 code point, returns true iff it is a leading surrogate.
  * https://unicode.org/faq/utf_bom.html#utf16-2
  */
-SK_SPI static inline bool IsLeadingSurrogateUTF16(uint16_t c) { return ((c) & 0xFC00) == 0xD800; }
+static inline bool IsLeadingSurrogateUTF16(uint16_t c) { return ((c) & 0xFC00) == 0xD800; }
 
 /**
  * Given a UTF-16 code point, returns true iff it is a trailing surrogate.
  * https://unicode.org/faq/utf_bom.html#utf16-2
  */
-SK_SPI static inline bool IsTrailingSurrogateUTF16(uint16_t c) { return ((c) & 0xFC00) == 0xDC00; }
+static inline bool IsTrailingSurrogateUTF16(uint16_t c) { return ((c) & 0xFC00) == 0xDC00; }
 
 
 }  // namespace SkUTF
