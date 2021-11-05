4415b596794615efc49adf07484485f340be1835
Robert Phillips <robertphillips@google.com>
2021-11-05 13:40:42 -0400

    Disable SVGs and tests on iPhone6 to prevent OOM
    When the device crashes we get no data from the run so skipping tests is preferable.
    
    Bug: skia:12617
    Change-Id: I3f69cb91cdf4e1e9659c81f5a6c717e0d911c2fa
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/468278
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -823,6 +823,8 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 	if b.model("iPhone6") {
 		skip("_", "gm", "_", "verylargebitmap")
 		skip("_", "gm", "_", "verylarge_picture_image")
+		skip("_", "svg", "_", "A_large_blank_world_map_with_oceans_marked_in_blue.svg")
+		skip("_", "tests", "_", "ImageFilterBlurLargeImage_Gpu")
 	}
 	if b.matchOs("Mac") && b.cpu() {
 		// skia:6992
