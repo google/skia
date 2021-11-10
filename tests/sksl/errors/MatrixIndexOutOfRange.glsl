d92f9c2f8d494c577f23d77aa576cf316d9a7049
John Stiles <johnstiles@google.com>
2021-11-10 14:02:18 -0500

    Add tests for matrix constant-expressions.
    We should support constant-expressions involving matrices (GLSL ES2
    does, WebGL does). We currently don't. We do properly report out-of-
    range indexing, but we don't optimize away valid matrix index
    expressions or allow matrices to be indexed in a constant-expression
    context.
    
    Change-Id: If58aa4c5f15abef421a412957072f3617b4176df
    Bug: skia:12472
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/469818
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

---
+++ tests/sksl/errors/MatrixIndexOutOfRange.glsl
@@ -0,0 +1,19 @@
+### Compilation failed:
+
+error: 1: index -1 out of range for 'half2x4'
+error: 4: index 2 out of range for 'half2x4'
+error: 5: index 3 out of range for 'half2x4'
+error: 6: index 4 out of range for 'half2x4'
+error: 7: index 1000000000 out of range for 'half2x4'
+error: 9: index -1 out of range for 'half3x3'
+error: 13: index 3 out of range for 'half3x3'
+error: 14: index 4 out of range for 'half3x3'
+error: 15: index 1000000000 out of range for 'half3x3'
+error: 17: index -1 out of range for 'half4x2'
+error: 22: index 4 out of range for 'half4x2'
+error: 23: index 1000000000 out of range for 'half4x2'
+error: 25: index -1 out of range for 'half3x3'
+error: 29: index 3 out of range for 'half3x3'
+error: 30: index 4 out of range for 'half3x3'
+error: 31: index 1000000000 out of range for 'half3x3'
+16 errors
