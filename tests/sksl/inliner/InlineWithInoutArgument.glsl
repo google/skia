74410023acb796c460dabe6c05442ec32088e171
John Stiles <johnstiles@google.com>
2021-11-11 08:56:16 -0500

    Add tests for inlining functions with out/inout params.
    Functions which don't write to their out params should be safe to
    inline, but we currently don't recognize this.
    
    Change-Id: I753e48067c7be4473675ef6c95e61af17dc5ae41
    Bug: skia:12636
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/470298
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/inliner/InlineWithInoutArgument.glsl
+++ tests/sksl/inliner/InlineWithInoutArgument.glsl
@@ -1,11 +1,24 @@
 
 out vec4 sk_FragColor;
 uniform vec4 colorGreen;
-void outParameter_vh4(inout vec4 x) {
+void outParameterWrite_vh4(out vec4 x) {
+    x = colorGreen;
+}
+void inoutParameterWrite_vh4(inout vec4 x) {
     x *= x;
 }
+void inoutParameterRead_vh4(inout vec4 x) {
+}
+void inoutParameterIgnore_vh4(inout vec4 x) {
+}
+void outParameterIgnore_vh4(out vec4 x) {
+}
 vec4 main() {
-    vec4 c = colorGreen;
-    outParameter_vh4(c);
+    vec4 c;
+    outParameterWrite_vh4(c);
+    inoutParameterWrite_vh4(c);
+    inoutParameterRead_vh4(c);
+    inoutParameterIgnore_vh4(c);
+    outParameterIgnore_vh4(c);
     return c;
 }
