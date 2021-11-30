4fb7dec70608ffeda5a205e0dba3d8d20b8a52b3
Erik Rose <erikrose@google.com>
2021-11-29 19:26:28 -0500

    [infra] Change the iOS arch to arm64 so we get the right CIPD packages for the rpi2 machines.
    Change-Id: Id44d46f57aa27b2c7e5abc9949444b244c4b7ee7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/477645
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Erik Rose <erikrose@google.com>

--- infra/bots/gen_tasks_logic/task_builder.go
+++ infra/bots/gen_tasks_logic/task_builder.go
@@ -283,7 +283,7 @@ func (b *taskBuilder) cipdPlatform() string {
 	} else if b.matchOs("Android", "ChromeOS") {
 		return cipd.PlatformLinuxArm64
 	} else if b.matchOs("iOS") {
-		return cipd.PlatformLinuxArmv6l
+		return cipd.PlatformLinuxArm64
 	} else {
 		return cipd.PlatformLinuxAmd64
 	}
