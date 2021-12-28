aa09c782a8c1636c31b90bce1d498224b50b31ee
John Stiles <johnstiles@google.com>
2021-12-28 14:58:36 -0500

    Add test for structs/interface-blocks with invalid member names.
    Structs already handled this appropriately, but interface blocks did not
    guard against naming their member variables built-in type names like
    "float" or "bool".
    
    Change-Id: I12ec054b3f158b83e35031449cf2a088ff8d0dc2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/489596
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

---
+++ tests/sksl/errors/InterfaceBlockMemberReservedName.glsl
@@ -0,0 +1,7 @@
+
+IB {
+    float float;
+} ib;
+vec4 main() {
+    return vec4(ib.float);
+}
---
+++ tests/sksl/errors/StructMemberReservedName.glsl
@@ -0,0 +1,4 @@
+### Compilation failed:
+
+error: 2: expected an identifier, but found type 'float'
+1 error
