866db186fafa22f7c75e91572f49bb960d213e9a
John Stiles <johnstiles@google.com>
2022-01-13 08:48:46 -0500

    Simplify control flow in switch test to unbreak iOS.
    This looks like the GLSL driver in iOS generates wrong results when
    returning a value from inside a switch.
    
    Change-Id: I478a045c64c3dae9824f86f52e0c7f8f9685c9af
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/494476
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- tests/sksl/folding/SwitchCaseFolding.glsl
+++ tests/sksl/folding/SwitchCaseFolding.glsl
@@ -2,24 +2,24 @@
 out vec4 sk_FragColor;
 uniform vec4 colorRed;
 uniform vec4 colorGreen;
-bool test_bi(int one) {
-    switch (one) {
-        case 0:
-            return false;
-        case 1:
-            return true;
-        case 2:
-            return false;
-        case 3:
-            return false;
-        case 4:
-            return false;
-        case 5:
-            return false;
-        default:
-            return false;
-    }
-}
 vec4 main() {
-    return test_bi(int(colorGreen.y)) ? colorGreen : colorRed;
+    vec4 color = colorRed;
+    switch (int(colorGreen.y)) {
+        case 0:
+            break;
+        case 1:
+            color = colorGreen;
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+    }
+    return color;
 }
