3ed2981da29e6677692309ea653f33eaaae1dc19
John Stiles <johnstiles@google.com>
2022-01-25 11:40:50 -0500

    Update test to demonstrate out-of-range value in error.
    SkSL is somehow interpreting a large positive value as a negative one.
    
    Change-Id: I299e0bf389a9fcbfe697741bd33a54df07748753
    Bug: skia:12863
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/499556
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- tests/sksl/errors/ArrayOfInvalidSize.glsl
+++ tests/sksl/errors/ArrayOfInvalidSize.glsl
@@ -11,14 +11,16 @@ error: 7: expected 'int', but found 'bool'
 error: 8: expected 'int', but found 'int2'
 error: 9: missing index in '[]'
 error: 10: integer is out of range for type 'int': 4000000000
-error: 12: array size must be positive
+error: 11: integer is out of range for type 'int': -9223372036854775808
 error: 13: array size must be positive
 error: 14: array size must be positive
-error: 15: array size must be an integer
-error: 16: array size out of bounds
-error: 17: array size must be an integer
+error: 15: array size must be positive
+error: 16: array size must be an integer
+error: 17: array size out of bounds
 error: 18: array size must be an integer
 error: 19: array size must be an integer
-error: 20: expected array dimension
-error: 21: integer is out of range for type 'int': 4000000000
-21 errors
+error: 20: array size must be an integer
+error: 21: expected array dimension
+error: 22: integer is out of range for type 'int': 4000000000
+error: 23: integer is out of range for type 'int': -9223372036854775808
+23 errors
