24a8aa9d1f638dd88801a394c32f831a4533f222
Julia Lavrova <jlavrova@google.com>
2022-02-01 09:50:40 -0500

    Updating the API description for Flutter
    bug: skia:12850
    Change-Id: I78148b2d99701a9127c377ddf709c89a885fcfc2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/502312
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Julia Lavrova <jlavrova@google.com>

--- modules/skparagraph/include/ParagraphBuilder.h
+++ modules/skparagraph/include/ParagraphBuilder.h
@@ -39,12 +39,12 @@ public:
 
     virtual TextStyle peekStyle() = 0;
 
-    // Adds text to the builder. Forms the proper runs to use the upper-most style
-    // on the style_stack
+    // Adds UTF16-encoded text to the builder. Forms the proper runs to use the upper-most style
+    // on the style_stack.
     virtual void addText(const std::u16string& text) = 0;
 
-    // Adds text to the builder, using the top-most style on on the style_stack.
-    virtual void addText(const char* text) = 0; // Don't use this one - going away soon
+    // Adds UTF8-encoded text to the builder, using the top-most style on the style_stack.
+    virtual void addText(const char* text) = 0;
     virtual void addText(const char* text, size_t len) = 0;
 
     // Pushes the information required to leave an open space, where Flutter may
