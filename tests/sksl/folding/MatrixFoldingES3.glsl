78eb43826f62961a3abcb553c3169b9be26872f3
John Stiles <johnstiles@google.com>
2022-02-07 14:09:54 -0500

    Fill out matrix-folding ES3 tests.
    This mirrors a lot of the existing matrix ES2 tests, but using
    non-square matrices. This is still important because a lot of subtle
    bugs can slip through the cracks when rows == columns.
    
    Change-Id: I626c4c2b176c8280da64513d16f59e76e726cbe7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/505218
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- tests/sksl/folding/MatrixFoldingES3.glsl
+++ tests/sksl/folding/MatrixFoldingES3.glsl
@@ -2,7 +2,21 @@
 out vec4 sk_FragColor;
 uniform vec4 colorRed;
 uniform vec4 colorGreen;
+bool test_eq_half_b() {
+    bool ok = true;
+    return ok;
+}
+bool test_matrix_op_matrix_float_b() {
+    bool ok = true;
+    ok = ok && mat3x2(1.0, 4.0, 2.0, 5.0, 3.0, 6.0) * mat2x3(7.0, 9.0, 11.0, 8.0, 10.0, 12.0) == mat2(58.0, 139.0, 64.0, 154.0);
+    return ok;
+}
+bool test_matrix_op_matrix_half_b() {
+    bool ok = true;
+    ok = ok && mat3x2(1.0, 4.0, 2.0, 5.0, 3.0, 6.0) * mat2x3(7.0, 9.0, 11.0, 8.0, 10.0, 12.0) == mat2(58.0, 139.0, 64.0, 154.0);
+    return ok;
+}
 vec4 main() {
     bool _0_ok = true;
-    return _0_ok ? colorGreen : colorRed;
+    return ((_0_ok && test_eq_half_b()) && test_matrix_op_matrix_float_b()) && test_matrix_op_matrix_half_b() ? colorGreen : colorRed;
 }
