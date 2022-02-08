28991c1a34714285426b2ca30494de91d64f6c4a
John Stiles <johnstiles@google.com>
2022-02-08 12:17:28 -0500

    Reland "Verify that tests in errors/ actually generate the expected errors."
    This reverts commit 43539c22a2963f5db8f790c32aed8bf3a992ed68.
    
    Reason for revert: UB fixed at http://review.skia.org/505678
    
    Original change's description:
    > Revert "Verify that tests in errors/ actually generate the expected errors."
    >
    > This reverts commit 8d646c127a7bed796eed927c8b9cff3a1ccedb06.
    >
    > Reason for revert: triggering UBSAN
    > http://screen/887FeQtZWs2A6oo
    >
    > Original change's description:
    > > Verify that tests in errors/ actually generate the expected errors.
    > >
    > > Error expectations are embedded in the source with a special *%%*
    > > marker, like this:
    > >
    > >      /*%%*
    > >      expected 'foo', but found 'bar'
    > >      'baz' is not a valid identifier
    > >      *%%*/
    > >
    > > This unit test compiles every effect in errors/ and verifies that it
    > > makes an error. It also verifies that the errors returned include the
    > > expectations from the *%%* marker section, in the listed order, if any
    > > expectations have been listed. (Error expectations are not meant to be
    > > exhaustive; additional errors are allowed.)
    > >
    > > In this CL, I've manually attached error expectations to the first few
    > > error tests. A followup CL will (mechanically) add expectations to every
    > > error test, based on their current error reports.
    > >
    > > Change-Id: I4add30fef6419c4d3f8d2a221c5aeb53eee35ae7
    > > Bug: skia:12665
    > > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/505399
    > > Auto-Submit: John Stiles <johnstiles@google.com>
    > > Reviewed-by: Brian Osman <brianosman@google.com>
    > > Commit-Queue: Brian Osman <brianosman@google.com>
    >
    > Bug: skia:12665
    > Change-Id: I3bcdbe9fc1abab13656d6462b73f6439967fd96f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/505642
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    
    Bug: skia:12665
    Change-Id: I49e23869f4ef383a0b076006e319e0a6d7191cad
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/505643
    Reviewed-by: Ethan Nicholas <ethannicholas@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- tests/sksl/errors/ArrayOfVoid.glsl
+++ tests/sksl/errors/ArrayOfVoid.glsl
@@ -1,10 +1,10 @@
 ### Compilation failed:
 
-error: 3: type 'void' may not be used in an array
+error: 1: type 'void' may not be used in an array
+error: 2: type 'void' may not be used in an array
 error: 4: type 'void' may not be used in an array
+error: 4: function 'funcF' can exit without returning a value
+error: 5: type 'void' may not be used in an array
 error: 6: type 'void' may not be used in an array
-error: 6: function 'funcF' can exit without returning a value
 error: 7: type 'void' may not be used in an array
-error: 8: type 'void' may not be used in an array
-error: 9: type 'void' may not be used in an array
 7 errors
