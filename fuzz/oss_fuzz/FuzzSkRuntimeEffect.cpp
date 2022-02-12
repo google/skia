e4d3d3947caa32557b5daa8c9c297c1d7e8ccfca
John Stiles <johnstiles@google.com>
2022-02-11 20:07:54 -0500

    Synthesize uniforms/children in the SkRuntimeEffect fuzzer.
    Previously, we used a fixed portion of the fuzzer input for uniform
    data, and assumed zero children. Now we synthesize uniform and child
    inputs based on the needs of the input shader. We no longer need to
    reserve the last 256 bytes of each shader for uniform data; a followup
    CL will remove the special corpus that this fuzzer used.
    
    The contents of each uniform/child is not particularly important, since
    we don't care what the final paint looks like. Each uniform is filled in
    with increasing values in each slot, starting at zero (0, 1, 2, 3, ...).
    Each child is a simple built-in shader/filter/blender.
    
    (Fuzzer corpus change: https://github.com/google/oss-fuzz/pull/7266 )
    
    Change-Id: Idc8bfcb17c10a470ef35fafc1789b915495e1d67
    Bug: skia:12781
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/507924
    Reviewed-by: Arman Uguray <armansito@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- fuzz/oss_fuzz/FuzzSkRuntimeEffect.cpp
+++ fuzz/oss_fuzz/FuzzSkRuntimeEffect.cpp
@@ -6,45 +6,83 @@
  */
 
 #include "include/core/SkCanvas.h"
+#include "include/core/SkColorFilter.h"
 #include "include/core/SkPaint.h"
+#include "include/core/SkShader.h"
 #include "include/core/SkSurface.h"
+#include "include/effects/SkBlenders.h"
 #include "include/effects/SkRuntimeEffect.h"
 #include "src/gpu/GrShaderCaps.h"
 
 #include "fuzz/Fuzz.h"
 
-static constexpr size_t kReservedBytes = 256;
 /**
- * The fuzzer will take in the bytes and divide into two parts.
- * original bytes : [... code bytes ... | 256 bytes]
- * The first part is codeBytes, the original bytes minus 256 bytes, which will be treated
- * as sksl code, intending to create SkRuntimeEffect.
- * For the second part, it will first reserve 256 bytes and then allocate bytes with same size
- * as effect->inputSize() to uniformBytes. The uniformBytes is intended to create makeShader().
- * Note that if uniformBytes->size() != effect->inputSize() the shader won't be created.
+ * The fuzzer treats the input bytes as an SkSL program. The requested number of uniforms and
+ * children are automatically synthesized to match the program's needs.
  *
  * We fuzz twice, with two different settings for inlining in the SkSL compiler. By default, the
  * compiler inlines most small to medium functions. This can hide bugs related to function-calling.
- * So we run the fuzzer once with inlining disabled, and again with it enabled (aggressively).
+ * So we run the fuzzer once with inlining disabled, and again with it enabled.
  * This gives us better coverage, and eases the burden on the fuzzer to inject useless noise into
  * functions to suppress inlining.
  */
-static bool FuzzSkRuntimeEffect_Once(sk_sp<SkData> bytes, const SkRuntimeEffect::Options& options) {
-    if (bytes->size() < kReservedBytes) {
-        return false;
-    }
-    sk_sp<SkData> codeBytes = SkData::MakeSubset(bytes.get(), 0, bytes->size() - kReservedBytes);
-
+static bool FuzzSkRuntimeEffect_Once(sk_sp<SkData> codeBytes,
+                                     const SkRuntimeEffect::Options& options) {
     SkString shaderText{static_cast<const char*>(codeBytes->data()), codeBytes->size()};
     SkRuntimeEffect::Result result = SkRuntimeEffect::MakeForShader(shaderText, options);
     SkRuntimeEffect* effect = result.effect.get();
-
-    if (!effect || effect->uniformSize() > kReservedBytes) { // if there is not enough uniform bytes
+    if (!effect) {
         return false;
     }
-    sk_sp<SkData> uniformBytes =
-            SkData::MakeSubset(bytes.get(), bytes->size() - kReservedBytes, effect->uniformSize());
-    auto shader = effect->makeShader(uniformBytes, /*children=*/{});
+
+    // Create storage for our uniforms.
+    sk_sp<SkData> uniformBytes = SkData::MakeZeroInitialized(effect->uniformSize());
+    void* uniformData = uniformBytes->writable_data();
+
+    for (const SkRuntimeEffect::Uniform& u : effect->uniforms()) {
+        // We treat scalars, vectors, matrices and arrays the same. We just figure out how many
+        // uniform slots need to be filled, and write consecutive numbers into those slots.
+        static_assert(sizeof(int) == 4 && sizeof(float) == 4);
+        size_t numFields = u.sizeInBytes() / 4;
+
+        if (u.type == SkRuntimeEffect::Uniform::Type::kInt ||
+            u.type == SkRuntimeEffect::Uniform::Type::kInt2 ||
+            u.type == SkRuntimeEffect::Uniform::Type::kInt3 ||
+            u.type == SkRuntimeEffect::Uniform::Type::kInt4) {
+            int intVal = 0;
+            while (numFields--) {
+                // Assign increasing integer values to each slot (0, 1, 2, ...).
+                *static_cast<int*>(uniformData) = intVal++;
+                uniformData = static_cast<int*>(uniformData) + 1;
+            }
+        } else {
+            float floatVal = 0.0f;
+            while (numFields--) {
+                // Assign increasing float values to each slot (0.0, 1.0, 2.0, ...).
+                *static_cast<float*>(uniformData) = floatVal++;
+                uniformData = static_cast<float*>(uniformData) + 1;
+            }
+        }
+    }
+
+    // Create valid children for any requested child effects.
+    std::vector<SkRuntimeEffect::ChildPtr> children;
+    children.reserve(effect->children().size());
+    for (const SkRuntimeEffect::Child& c : effect->children()) {
+        switch (c.type) {
+            case SkRuntimeEffect::ChildType::kShader:
+                children.push_back(SkShaders::Color(SK_ColorRED));
+                break;
+            case SkRuntimeEffect::ChildType::kColorFilter:
+                children.push_back(SkColorFilters::Blend(SK_ColorBLUE, SkBlendMode::kModulate));
+                break;
+            case SkRuntimeEffect::ChildType::kBlender:
+                children.push_back(SkBlenders::Arithmetic(0.50f, 0.25f, 0.10f, 0.05f, false));
+                break;
+        }
+    }
+
+    sk_sp<SkShader> shader = effect->makeShader(uniformBytes, SkMakeSpan(children));
     if (!shader) {
         return false;
     }
