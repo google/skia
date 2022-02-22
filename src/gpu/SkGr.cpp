046e3f4b42bff2d2d8f5888847aa0741edb9d249
Brian Osman <brianosman@google.com>
2022-02-22 22:28:51 +0000

    Revert "Don't dither const shaders that don't have a GP override"
    This reverts commit 05a4b12dbdf279280c691be0ccf31b718bde10c7.
    
    Reason for revert: Speculative fix for b/220676062
    
    Original change's description:
    > Don't dither const shaders that don't have a GP override
    >
    > https://gold.skia.org/detail?test=gradients_no_texture&digest=008c796df4da7fbb6718e96db2c36c90
    >
    > If paintFP was only non-null because it came from the main SkShader, we
    > should defer to SkPaintPriv's check that the shader is not a constant.
    > We don't have a isConstant() for FPs so this assumes that if the GP
    > overrides it with a new FP it probably is doing something spatially
    > interesting and does need the dithering.
    >
    > Bug: skia:12516
    > Change-Id: I7935ba8f0baf91e7b7ba07280452871c60ffd3e8
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/509996
    > Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    > Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    > Reviewed-by: Brian Osman <brianosman@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    
    Bug: skia:12516
    Change-Id: I9c45f457d42cd5890cc110617e1636ffa6669503
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/511808
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/gpu/SkGr.cpp
+++ src/gpu/SkGr.cpp
@@ -548,9 +548,7 @@ static inline bool skpaint_to_grpaint_impl(
 
 #ifndef SK_IGNORE_GPU_DITHER
     GrColorType ct = dstColorInfo.colorType();
-    // Checking shaderFP.has_value() detects the SkPaintToGrPaintReplaceShader scenario even if the
-    // parameter was initialized with a nullptr (signaling a GP varying color).
-    const bool shaderOverride = SkToBool(paintFP) && (shaderFP.has_value() || primColorBlender);
+    const bool shaderOverride = SkToBool(paintFP);
     if (SkPaintPriv::ShouldDither(skPaint, GrColorTypeToSkColorType(ct), shaderOverride)) {
         float ditherRange = dither_range_for_config(ct);
         paintFP = make_dither_effect(
