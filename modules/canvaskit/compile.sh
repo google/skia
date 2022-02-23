e1c79a91898ec290ba7fe0d222c922ba03c3f39c
Kevin Lubick <kjlubick@google.com>
2022-02-23 09:49:19 -0500

    [canvaskit] Add legacy drawvertices mode to npm build
    Should help with https://github.com/flutter/flutter/issues/98531
    until Flutter removes https://github.com/google/skia/blob/c25fd021edd04dabe4acad54a6288d6645dc2af8/gn/flutter_defines.gni#L17
    
    Change-Id: I151c17dbea969b787fb785b999f9d760ae3b25ea
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/511976
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Yegor Jbanov <yjbanov@google.com>

--- modules/canvaskit/compile.sh
+++ modules/canvaskit/compile.sh
@@ -138,6 +138,11 @@ if [[ $@ == *no_alias_font* ]]; then
   ENABLE_ALIAS_FONT="false"
 fi
 
+LEGACY_DRAW_VERTICES="false"
+if [[ $@ == *legacy_draw_vertices* ]]; then
+  LEGACY_DRAW_VERTICES="true"
+fi
+
 GN_SHAPER="skia_use_icu=true skia_use_system_icu=false skia_use_harfbuzz=true skia_use_system_harfbuzz=false"
 if [[ $@ == *primitive_shaper* ]] || [[ $@ == *no_font* ]]; then
   echo "Using the primitive shaper instead of the harfbuzz/icu one"
@@ -242,6 +247,7 @@ echo "Compiling"
   skia_canvaskit_enable_font=${ENABLE_FONT} \
   skia_canvaskit_enable_embedded_font=${ENABLE_EMBEDDED_FONT} \
   skia_canvaskit_enable_alias_font=${ENABLE_ALIAS_FONT} \
+  skia_canvaskit_legacy_draw_vertices_blend_mode=${LEGACY_DRAW_VERTICES} \
   skia_canvaskit_enable_paragraph=${ENABLE_PARAGRAPH}"
 
 ${NINJA} -C ${BUILD_DIR} canvaskit.js
