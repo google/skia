a7bf23d17e39895bad0fa690c4ac464e1cb84513
John Stiles <johnstiles@google.com>
2022-03-22 09:06:12 -0400

    Rename ES2 error tests from 'H' through 'M' to .rts.
    In a few cases, this involved splitting a test into two (an ES2-
    compatible portion and a ES3+ portion).
    
    Bug: skia:13042
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Arman Uguray <armansito@google.com>
    Commit-Queue: Arman Uguray <armansito@google.com>
    Change-Id: I3a776846a3d7d19cb19fa68c67842f7dbafcb420
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/524099
    Reviewed-by: Derek Sollenberger <djsollen@google.com>

---
+++ tests/sksl/errors/MatrixIndexOutOfRangeES3.glsl
@@ -0,0 +1,11 @@
+### Compilation failed:
+
+error: 1: index -1 out of range for 'half2x4'
+error: 4: index 2 out of range for 'half2x4'
+error: 5: index 3 out of range for 'half2x4'
+error: 6: index 4 out of range for 'half2x4'
+error: 7: index 1000000000 out of range for 'half2x4'
+error: 9: index -1 out of range for 'half4x2'
+error: 14: index 4 out of range for 'half4x2'
+error: 15: index 1000000000 out of range for 'half4x2'
+8 errors
