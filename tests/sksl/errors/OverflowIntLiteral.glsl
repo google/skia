141f2873a901e8fabaddf5912b8c413f6f21f2f8
John Stiles <johnstiles@google.com>
2022-03-22 09:23:32 -0400

    Rename ES2 error tests 'O' through 'P' to .rts.
    A few tests were divided into a Runtime Effect-compatible .rts test, and
    a Runtime Effect-incompatible .sksl test.
    
    Change-Id: Ib52554892685bdc44fe3622ab314960ee0962b90
    Bug: skia:13042
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/523377
    Reviewed-by: Arman Uguray <armansito@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/errors/OverflowIntLiteral.glsl
+++ tests/sksl/errors/OverflowIntLiteral.glsl
@@ -1,9 +1,6 @@
 ### Compilation failed:
 
-error: 4: integer is out of range for type 'int': -2147483649
-error: 6: integer is out of range for type 'int': 2147483648
-error: 8: integer is out of range for type 'short': -32769
-error: 9: integer is out of range for type 'short': 32768
-error: 11: integer is out of range for type 'int': 2147483648
-error: 12: integer is out of range for type 'short': 32768
-6 errors
+error: 2: integer is out of range for type 'int': -2147483649
+error: 4: integer is out of range for type 'int': 2147483648
+error: 5: integer is out of range for type 'int': 2147483648
+3 errors
--- tests/sksl/errors/PrivateTypes.glsl
+++ tests/sksl/errors/PrivateTypes.glsl
@@ -1,9 +1,8 @@
 ### Compilation failed:
 
-error: 2: type '$mat' is private
-error: 6: type '$bvec' is private
-error: 9: type '$ivec' is private
-error: 10: type '$genType' is private
-error: 11: type '$mat' is private
-error: 12: type '$floatLiteral' is private
-6 errors
+error: 2: type '$bvec' is private
+error: 5: type '$ivec' is private
+error: 6: type '$genType' is private
+error: 7: type '$mat' is private
+error: 8: type '$floatLiteral' is private
+5 errors
