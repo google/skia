9f3c6bce488365ec198d9f7d9857ab6739031cf4
Derek Sollenberger <djsollen@google.com>
2022-04-07 13:07:56 +0000

    Revert "[sksl][wgsl] Implement codegen for simplest complete program"
    This reverts commit f379b259a55e61e4ca173bf392c43ec20ca9e0e4.
    
    Reason for revert: breaking the linux GCC x86 and x86_64 builders
    
    Original change's description:
    > [sksl][wgsl] Implement codegen for simplest complete program
    >
    > - Implemented much of the statement/expression/program-element
    >   parsing/dispatch boilerplate with support for only those expressions
    >   that are necessary to successfully compile shared/HelloWorld.sksl
    >
    > - Implemented functions and fragment/vertex program entry points
    >
    > Most of the code for HelloWorld.sksl is now generated, however the
    > output program is still missing a correct propagation of varying stage
    > inputs and output parameters to subroutines.
    >
    > Bug: skia:13092
    > Change-Id: Ic51af5a041761f58234d45e3746206ed080e0910
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/526758
    > Reviewed-by: John Stiles <johnstiles@google.com>
    > Commit-Queue: Arman Uguray <armansito@google.com>
    
    Bug: skia:13092
    Change-Id: Ib92f8637acf011fd7c9274a906dcb43952b881e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/528298
    Auto-Submit: Derek Sollenberger <djsollen@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- src/sksl/codegen/SkSLWGSLCodeGenerator.h
+++ src/sksl/codegen/SkSLWGSLCodeGenerator.h
@@ -8,25 +8,13 @@
 #ifndef SKSL_WGSLCODEGENERATOR
 #define SKSL_WGSLCODEGENERATOR
 
-#include "include/sksl/SkSLOperator.h"
 #include "src/sksl/codegen/SkSLCodeGenerator.h"
 
 namespace SkSL {
 
-class AnyConstructor;
-class BinaryExpression;
-class Block;
-class ConstructorCompound;
-class Expression;
-class ExpressionStatement;
 class FunctionDeclaration;
 class FunctionDefinition;
-class Literal;
 class ProgramElement;
-class ReturnStatement;
-class Statement;
-class VarDeclaration;
-class Variable;
 
 /**
  * Convert a Program into WGSL code.
@@ -61,7 +49,6 @@ public:
 
 private:
     using INHERITED = CodeGenerator;
-    using Precedence = Operator::Precedence;
 
     // Write output content while correctly handling indentation.
     void write(std::string_view s);
@@ -73,31 +60,6 @@ private:
     void writePipelineIODeclaration(Modifiers modifiers, const Type& type, std::string_view name);
     void writeUserDefinedVariableDecl(const Type& type, std::string_view name, int location);
     void writeBuiltinVariableDecl(const Type& type, std::string_view name, Builtin kind);
-
-    // Write a function definition.
-    void writeFunction(const FunctionDefinition& f);
-    void writeFunctionDeclaration(const FunctionDeclaration& f);
-
-    // Write the program entry point.
-    void writeEntryPoint(const FunctionDefinition& f);
-
-    // Writers for supported statement types.
-    void writeStatement(const Statement& s);
-    void writeStatements(const StatementArray& statements);
-    void writeBlock(const Block& b);
-    void writeExpressionStatement(const ExpressionStatement& s);
-    void writeReturnStatement(const ReturnStatement& s);
-    void writeVarDeclaration(const VarDeclaration& varDecl);
-
-    // Writers for expressions.
-    void writeExpression(const Expression& e, Precedence parentPrecedence);
-    void writeBinaryExpression(const BinaryExpression& b, Precedence parentPrecedence);
-    void writeLiteral(const Literal& l);
-
-    // Constructor expressions
-    void writeAnyConstructor(const AnyConstructor& c, Precedence parentPrecedence);
-    void writeConstructorCompound(const ConstructorCompound& c, Precedence parentPrecedence);
-    void writeConstructorCompoundVector(const ConstructorCompound& c, Precedence parentPrecedence);
 
     // Generic recursive ProgramElement visitor.
     void writeProgramElement(const ProgramElement& e);
