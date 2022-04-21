b6cd339d9c6f4c41513ac68560b18129a8d098ee
John Stiles <johnstiles@google.com>
2022-04-21 15:18:39 -0400

    Migrate Graphite shader code into sksl_graphite_frag.
    This allows us to pre-parse and analyze the code once at startup,
    instead of every time a shader is assembled.
    
    In order to make changes to `sksl_graphite_frag.sksl`, please set
    `skia_compile_modules = true` in your GN args. This will cause the
    dehydrated code to be regenerated at build time. This should not slow
    down normal builds; it only rebuilds when dirty.
    
    Change-Id: I08f2005399ea3a12726946db1e3f2c39f9c24df8
    Bug: skia:13110
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/532398
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- src/core/SkShaderCodeDictionary.cpp
+++ src/core/SkShaderCodeDictionary.cpp
@@ -285,32 +285,8 @@ static constexpr SkUniform kGradientUniforms[kNumGradientUniforms] = {
         { "padding", SkSLType::kFloat2 } // TODO: add automatic uniform padding
 };
 
-static const char *kLinearGradient4Name = "linear_grad_4_shader";
-static const char *kLinearGradient4SkSL =
-        // TODO: This should use local coords
-        "half4 linear_grad_4_shader(float4 colorsParam[4],\n"
-        "                           float offsetsParam[4],\n"
-        "                           float2 point0Param,\n"
-        "                           float2 point1Param,\n"
-        "                           float radius0Param,\n"
-        "                           float radius1Param,\n"
-        "                           float2 padding) {\n"
-        "    float2 pos = sk_FragCoord.xy;\n"
-        "    float2 delta = point1Param - point0Param;\n"
-        "    float2 pt = pos - point0Param;\n"
-        "    float t = dot(pt, delta) / dot(delta, delta);\n"
-        "    float4 result = colorsParam[0];\n"
-        "    result = mix(result, colorsParam[1],\n"
-        "                 clamp((t-offsetsParam[0])/(offsetsParam[1]-offsetsParam[0]),\n"
-        "                       0, 1));\n"
-        "    result = mix(result, colorsParam[2],\n"
-        "                 clamp((t-offsetsParam[1])/(offsetsParam[2]-offsetsParam[1]),\n"
-        "                       0, 1));\n"
-        "    result = mix(result, colorsParam[3],\n"
-        "                 clamp((t-offsetsParam[2])/(offsetsParam[3]-offsetsParam[2]),\n"
-        "                 0, 1));\n"
-        "    return half4(result);\n"
-        "}\n";
+static constexpr char kLinearGradient4Name[] = "sk_linear_grad_4_shader";
+static constexpr char kLinearGradient4SkSL[] = "";
 
 //--------------------------------------------------------------------------------------------------
 static constexpr int kNumSolidShaderUniforms = 1;
@@ -318,11 +294,8 @@ static constexpr SkUniform kSolidShaderUniforms[kNumSolidShaderUniforms] = {
         { "color", SkSLType::kFloat4 }
 };
 
-static const char* kSolidShaderName = "solid_shader";
-static const char* kSolidShaderSkSL =
-        "half4 solid_shader(float4 colorParam) {\n"
-        "    return half4(colorParam);\n"
-        "}\n";
+static constexpr char kSolidShaderName[] = "sk_solid_shader";
+static constexpr char kSolidShaderSkSL[] = "";
 
 //--------------------------------------------------------------------------------------------------
 static constexpr int kNumImageShaderUniforms = 5;
@@ -344,33 +317,8 @@ static_assert(1 == static_cast<int>(SkTileMode::kRepeat), "ImageShader code depe
 static_assert(2 == static_cast<int>(SkTileMode::kMirror), "ImageShader code depends on SkTileMode");
 static_assert(3 == static_cast<int>(SkTileMode::kDecal),  "ImageShader code depends on SkTileMode");
 
-static const char* kImageShaderName = "compute_coords";
-static const char* kImageShaderSkSL =
-        "const int kClamp         = 0;\n"
-        "const int kRepeat        = 1;\n"
-        "const int kMirrorRepeat  = 2;\n"
-        "const int kClampToBorder = 3;\n"
-        "\n"
-        "float tile(int tm, float f, float origin, float length) {\n"
-        "    if (tm == kClamp) {\n"
-        "        return clamp(f, origin, origin+length) / length;"
-        "    } else if (tm == kRepeat) {\n"
-        "        return mod(f - origin, length) / length;\n"
-        "    } else if (tm == kMirrorRepeat) {\n"
-        "        // for now, just repeat\n"
-        "        return mod(f - origin, length) / length;\n"
-        "    } else { // kClampToBorder\n"
-        "        // For now, just clamp\n"
-        "        return clamp(f, origin, origin+length) / length;"
-        "    }\n"
-        "}\n"
-        "\n"
-        "float2 compute_coords(float4 subset, int tmX, int tmY) {\n"
-        "    float2 subsetWH = (subset.zw - subset.xy);\n"
-        "    float2 coords = float2(tile(tmX, sk_FragCoord.x, subset.x, subsetWH.x),\n"
-        "                           tile(tmY, sk_FragCoord.y, subset.y, subsetWH.y));\n"
-        "    return coords;\n"
-        "}\n";
+static constexpr char kImageShaderName[] = "sk_compute_coords";
+static constexpr char kImageShaderSkSL[] = "";
 
 static constexpr int kNumImageShaderFields = 1;
 static constexpr DataPayloadField kImageShaderFields[kNumImageShaderFields] = {
@@ -426,74 +374,8 @@ static constexpr SkUniform kBlendShaderUniforms[kNumBlendShaderUniforms] = {
 
 static constexpr int kNumBlendShaderChildren = 2;
 
-// Note: we're counting on the compiler to inline this code and trim it down to just the used
-// branch(es) in the blend-mode case. In the blend-shader case it should remain un-inlined.
-static const char* kBlendHelperName = "blend";
-static const char* kBlendHelperSkSL =
-        "const int kClear      = 0;\n"
-        "const int kSrc        = 1;\n"
-        "const int kDst        = 2;\n"
-        "const int kSrcOver    = 3;\n"
-        "const int kDstOver    = 4;\n"
-        "const int kSrcIn      = 5;\n"
-        "const int kDstIn      = 6;\n"
-        "const int kSrcOut     = 7;\n"
-        "const int kDstOut     = 8;\n"
-        "const int kSrcATop    = 9;\n"
-        "const int kDstATop    = 10;\n"
-        "const int kXor        = 11;\n"
-        "const int kPlus       = 12;\n"
-        "const int kModulate   = 13;\n"
-        "const int kScreen     = 14;\n"
-        "const int kOverlay    = 15;\n"
-        "const int kDarken     = 16;\n"
-        "const int kLighten    = 17;\n"
-        "const int kColorDodge = 18;\n"
-        "const int kColorBurn  = 19;\n"
-        "const int kHardLight  = 20;\n"
-        "const int kSoftLight  = 21;\n"
-        "const int kDifference = 22;\n"
-        "const int kExclusion  = 23;\n"
-        "const int kMultiply   = 24;\n"
-        "const int kHue        = 25;\n"
-        "const int kSaturation = 26;\n"
-        "const int kColor      = 27;\n"
-        "const int kLuminosity = 28;\n"
-        "\n"
-        "half4 blend(int mode, half4 src, half4 dst) {\n"
-        "    switch (mode) {\n"
-        "        case kClear:      { return blend_clear(src, dst); }\n"
-        "        case kSrc:        { return blend_src(src, dst); }\n"
-        "        case kDst:        { return blend_dst(src, dst); }\n"
-        "        case kSrcOver:    { return blend_src_over(src, dst); }\n"
-        "        case kDstOver:    { return blend_dst_over(src, dst); }\n"
-        "        case kSrcIn:      { return blend_src_in(src, dst); }\n"
-        "        case kDstIn:      { return blend_dst_in(src, dst); }\n"
-        "        case kSrcOut:     { return blend_src_out(src, dst); }\n"
-        "        case kDstOut:     { return blend_dst_out(src, dst); }\n"
-        "        case kSrcATop:    { return blend_src_atop(src, dst); }\n"
-        "        case kDstATop:    { return blend_dst_atop(src, dst); }\n"
-        "        case kXor:        { return blend_xor(src, dst); }\n"
-        "        case kPlus:       { return blend_plus(src, dst); }\n"
-        "        case kModulate:   { return blend_modulate(src, dst); }\n"
-        "        case kScreen:     { return blend_screen(src, dst); }\n"
-        "        case kOverlay:    { return blend_overlay(src, dst); }\n"
-        "        case kDarken:     { return blend_darken(src, dst); }\n"
-        "        case kLighten:    { return blend_lighten(src, dst); }\n"
-        "        case kColorDodge: { return blend_color_dodge(src, dst); }\n"
-        "        case kColorBurn:  { return blend_color_burn(src, dst); }\n"
-        "        case kHardLight:  { return blend_hard_light(src, dst); }\n"
-        "        case kSoftLight:  { return blend_soft_light(src, dst); }\n"
-        "        case kDifference: { return blend_difference(src, dst); }\n"
-        "        case kExclusion:  { return blend_exclusion(src, dst); }\n"
-        "        case kMultiply:   { return blend_multiply(src, dst); }\n"
-        "        case kHue:        { return blend_hue(src, dst); }\n"
-        "        case kSaturation: { return blend_saturation(src, dst); }\n"
-        "        case kColor:      { return blend_color(src, dst); }\n"
-        "        case kLuminosity: { return blend_luminosity(src, dst); }\n"
-        "        default: return half4(0);  // Avoids 'blend can exit without returning a value' error\n"
-        "    }\n"
-        "}\n";
+static constexpr char kBlendHelperName[] = "sk_blend";
+static constexpr char kBlendHelperSkSL[] = "";
 
 std::string GenerateBlendShaderGlueCode(const std::string& resultName,
                                         int entryIndex,
@@ -520,11 +402,8 @@ std::string GenerateBlendShaderGlueCode(const std::string& resultName,
 }
 
 //--------------------------------------------------------------------------------------------------
-static const char* kErrorName = "error";
-static const char* kErrorSkSL =
-        "half4 error() {\n"
-        "    return half4(1.0, 0.0, 1.0, 1.0);\n"
-        "}\n";
+static constexpr char kErrorName[] = "sk_error";
+static constexpr char kErrorSkSL[] = "";
 
 //--------------------------------------------------------------------------------------------------
 static constexpr int kNumFixedFunctionBlenderFields = 1;
