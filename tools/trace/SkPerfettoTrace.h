bfc9b9484092e969664aed59c0bb410fecfc107e
Nicolette Prevost <nicolettep@google.com>
2022-05-25 10:02:23 -0400

    Add Perfetto library (gn & bazel) and bare-bones SkPerfTrace class
    First incremental step to incorporating Perfetto tracing into Skia, more CLs to follow
    
    NOTE: The presubmit check is failing. This appears to be due to the known issue where the presubmit check bugs out if the DEPS file is edited, which it was on this CL (modified to include Perfetto).
    
    Bug: skia:13303
    Change-Id: I908be0392b520e8da14b34588b842bf6d955bd93
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/543081
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Nicolette Prevost <nicolettep@google.com>

---
+++ tools/trace/SkPerfettoTrace.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkPerfettoTrace_DEFINED
+#define SkPerfettoTrace_DEFINED
+
+#include "include/utils/SkEventTracer.h"
+#include "tools/trace/EventTracingPriv.h"
+#include "third_party/externals/perfetto/sdk/perfetto.h"
+
+PERFETTO_DEFINE_CATEGORIES();
+
+/**
+ * This class is used to support Perfetto tracing. It hooks into the SkEventTracer system.
+ */
+class SkPerfettoTrace : public SkEventTracer {
+public:
+    SkPerfettoTrace();
+    ~SkPerfettoTrace() override;
+
+    SkEventTracer::Handle addTraceEvent(char phase,
+                                        const uint8_t* categoryEnabledFlag,
+                                        const char* name,
+                                        uint64_t id,
+                                        int numArgs,
+                                        const char** argNames,
+                                        const uint8_t* argTypes,
+                                        const uint64_t* argValues,
+                                        uint8_t flags) override;
+
+
+    void updateTraceEventDuration(const uint8_t* categoryEnabledFlag,
+                                  const char* name,
+                                  SkEventTracer::Handle handle) override;
+
+    const uint8_t* getCategoryGroupEnabled(const char* name) override;
+
+    const char* getCategoryGroupName(const uint8_t* categoryEnabledFlag) override;
+
+private:
+    SkPerfettoTrace(const SkPerfettoTrace&) = delete;
+    SkPerfettoTrace& operator=(const SkPerfettoTrace&) = delete;
+    SkEventTracingCategories fCategories;
+};
+
+#endif
