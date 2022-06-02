455addeff687a26005ce429e753c2a4285a81243
Kevin Lubick <kjlubick@google.com>
2022-06-02 08:34:35 -0400

    [bazel] Add 4 CI jobs, 1 CQ job to test Bazel builds
    Needed to fix the Dawn and Harfbuzz Bazel rules.
    
    Change-Id: I21f63c970bdc972b97e42ef85d82d7f478bd45e2
    Bug: skia:12541
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/545721
    Reviewed-by: Eric Boren <borenet@google.com>

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -2099,6 +2099,9 @@ func (b *jobBuilder) runWasmGMTests() {
 // The reason we need this mapping is because Buildbucket build names cannot have / or : in them.
 var shorthandToLabel = map[string]string{
 	"modules_canvaskit_canvaskit_wasm": "//modules/canvaskit:canvaskit_wasm",
+	"example_hello_world_dawn":         "//example:hello_world_dawn",
+	"example_hello_world_gl":           "//example:hello_world_gl",
+	"example_hello_world_vulkan":       "//example:hello_world_vulkan",
 }
 
 // bazelBuild adds a task which builds the specified single-target label (//foo:bar) or
