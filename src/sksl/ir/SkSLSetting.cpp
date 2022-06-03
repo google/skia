48805a6c019d06acb6624a1c72663791ebba997e
Brian Osman <brianosman@google.com>
2022-06-03 15:48:30 -0400

    Simplify SkSL::Setting code dramatically
    These values are always bool, so we can eliminate several levels of
    indirection.
    
    Change-Id: I074e9bd7a8f984776f4c710f640d761dee4258c2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/546862
    Reviewed-by: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/sksl/ir/SkSLSetting.cpp
+++ src/sksl/ir/SkSLSetting.cpp
@@ -20,80 +20,22 @@
 
 namespace SkSL {
 
-// Helper classes for converting caps fields to Expressions and Types in the CapsLookupTable.
 namespace {
 
-class CapsLookupMethod {
-public:
-    virtual ~CapsLookupMethod() {}
-    virtual const Type* type(const Context& context) const = 0;
-    virtual std::unique_ptr<Expression> value(const Context& context, Position pos) const = 0;
-};
-
-class BoolCapsLookup : public CapsLookupMethod {
-public:
-    using CapsFn = bool (ShaderCaps::*)() const;
-
-    BoolCapsLookup(const CapsFn& fn) : fGetCap(fn) {}
-
-    const Type* type(const Context& context) const override {
-        return context.fTypes.fBool.get();
-    }
-    std::unique_ptr<Expression> value(const Context& context, Position pos) const override {
-        return Literal::MakeBool(context, pos, (context.fCaps.*fGetCap)());
-    }
-
-private:
-    CapsFn fGetCap;
-};
-
-class IntCapsLookup : public CapsLookupMethod {
-public:
-    using CapsFn = int (ShaderCaps::*)() const;
-
-    IntCapsLookup(const CapsFn& fn) : fGetCap(fn) {}
-
-    const Type* type(const Context& context) const override {
-        return context.fTypes.fInt.get();
-    }
-    std::unique_ptr<Expression> value(const Context& context, Position pos) const override {
-        return Literal::MakeInt(context, pos, (context.fCaps.*fGetCap)());
-    }
-
-private:
-    CapsFn fGetCap;
-};
-
-class CapsLookupTable {
-public:
-    using Pair = std::pair<const char*, CapsLookupMethod*>;
-
-    CapsLookupTable(std::initializer_list<Pair> capsLookups) {
-        for (auto& entry : capsLookups) {
-            fMap.set(entry.first, std::unique_ptr<CapsLookupMethod>(entry.second));
-        }
-    }
-
-    const CapsLookupMethod* lookup(std::string_view name) const {
-        std::unique_ptr<CapsLookupMethod>* iter = fMap.find(name);
-        return iter ? iter->get() : nullptr;
-    }
-
-private:
-    SkTHashMap<std::string_view, std::unique_ptr<CapsLookupMethod>> fMap;
-};
+using CapsFn = bool (ShaderCaps::*)() const;
+using CapsLookupTable = SkTHashMap<std::string_view, CapsFn>;
 
 static const CapsLookupTable& caps_lookup_table() {
     // Create a lookup table that converts strings into the equivalent ShaderCaps methods.
     static CapsLookupTable* sCapsLookupTable = new CapsLookupTable({
-    #define CAP(T, name) CapsLookupTable::Pair{#name, new T##CapsLookup{&ShaderCaps::name}}
-        CAP(Bool, mustDoOpBetweenFloorAndAbs),
-        CAP(Bool, mustGuardDivisionEvenAfterExplicitZeroCheck),
-        CAP(Bool, atan2ImplementedAsAtanYOverX),
-        CAP(Bool, floatIs32Bits),
-        CAP(Bool, integerSupport),
-        CAP(Bool, builtinDeterminantSupport),
-        CAP(Bool, rewriteMatrixVectorMultiply),
+    #define CAP(name) CapsLookupTable::Pair{#name, &ShaderCaps::name}
+        CAP(mustDoOpBetweenFloorAndAbs),
+        CAP(mustGuardDivisionEvenAfterExplicitZeroCheck),
+        CAP(atan2ImplementedAsAtanYOverX),
+        CAP(floatIs32Bits),
+        CAP(integerSupport),
+        CAP(builtinDeterminantSupport),
+        CAP(rewriteMatrixVectorMultiply),
     #undef CAP
     });
     return *sCapsLookupTable;
@@ -101,37 +43,24 @@ static const CapsLookupTable& caps_lookup_table() {
 
 }  // namespace
 
-static const Type* get_type(const Context& context, Position pos, std::string_view name) {
-    if (const CapsLookupMethod* caps = caps_lookup_table().lookup(name)) {
-        return caps->type(context);
-    }
-
-    context.fErrors->error(pos, "unknown capability flag '" + std::string(name) + "'");
-    return nullptr;
-}
-
-static std::unique_ptr<Expression> get_value(const Context& context, Position pos,
-                                             const std::string_view& name) {
-    if (const CapsLookupMethod* caps = caps_lookup_table().lookup(name)) {
-        return caps->value(context, pos);
-    }
-
-    context.fErrors->error(pos, "unknown capability flag '" + std::string(name) + "'");
-    return nullptr;
-}
-
 std::unique_ptr<Expression> Setting::Convert(const Context& context, Position pos,
                                              const std::string_view& name) {
     SkASSERT(context.fConfig);
 
+    const CapsFn* getCap = caps_lookup_table().find(name);
+
+    if (!getCap) {
+        context.fErrors->error(pos, "unknown capability flag '" + std::string(name) + "'");
+        return nullptr;
+    }
+
     if (context.fConfig->fSettings.fReplaceSettings) {
         // Insert the settings value directly into the IR.
-        return get_value(context, pos, name);
+        return Literal::MakeBool(context, pos, (context.fCaps.*(*getCap))());
     }
 
     // Generate a Setting IRNode.
-    const Type* type = get_type(context, pos, name);
-    return type ? std::make_unique<Setting>(pos, name, type) : nullptr;
+    return std::make_unique<Setting>(pos, name, context.fTypes.fBool.get());
 }
 
 }  // namespace SkSL
