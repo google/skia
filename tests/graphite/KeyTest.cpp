23564bd50042bacd4d22c8ffcee0fd2fb9eaf877
John Stiles <johnstiles@google.com>
2022-06-14 11:35:43 -0400

    Split KeyTest into three separate tests with narrower scope.
    The tests themselves are the same, just split up into their logical
    groupings. From the principle go/unit-testing-overview#properties
    
    "Focused. Above all, unit tests are narrow in scope, validating the
    correctness of individual pieces of code rather than the correctness of
    the system as a whole."
    
    Change-Id: If4219c7ea447155314db885a1e1f0beb3c444b74
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/549836
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/graphite/KeyTest.cpp
+++ tests/graphite/KeyTest.cpp
@@ -32,49 +32,51 @@ SkPaintParamsKey create_key(SkPaintParamsKeyBuilder* builder, int dummySnippetID
 
 } // anonymous namespace
 
-// This is intended to be a unit test of the SkPaintParamsKeyBuilder and SkPaintParamsKey.
-DEF_GRAPHITE_TEST_FOR_CONTEXTS(KeyTest, reporter, context) {
+// These are intended to be unit tests of the SkPaintParamsKeyBuilder and SkPaintParamsKey.
+DEF_GRAPHITE_TEST_FOR_CONTEXTS(KeyWithInvalidCodeSnippetIDTest, reporter, context) {
 
     auto dict = context->priv().shaderCodeDictionary();
-
     SkPaintParamsKeyBuilder builder(dict, SkBackend::kGraphite);
 
-    // invalid code snippet ID
-    {
-        SkPaintParamsKey key = create_key(&builder, kBuiltInCodeSnippetIDCount, 32);
-        // key creation fails
-        REPORTER_ASSERT(reporter, key.isErrorKey());
-    }
+    // Invalid code snippet ID, key creation fails.
+    SkPaintParamsKey key = create_key(&builder, kBuiltInCodeSnippetIDCount, /*size=*/32);
+    REPORTER_ASSERT(reporter, key.isErrorKey());
+}
+
+DEF_GRAPHITE_TEST_FOR_CONTEXTS(KeyValidBlockSizeTest, reporter, context) {
+
+    auto dict = context->priv().shaderCodeDictionary();
+    SkPaintParamsKeyBuilder builder(dict, SkBackend::kGraphite);
 
     // _Just_ on the edge of being too big
-    {
-        static const int kMaxBlockDataSize = SkPaintParamsKey::kMaxBlockSize -
-                                             SkPaintParamsKey::kBlockHeaderSizeInBytes;
+    static const int kMaxBlockDataSize = SkPaintParamsKey::kMaxBlockSize -
+                                         SkPaintParamsKey::kBlockHeaderSizeInBytes;
+    static constexpr SkPaintParamsKey::DataPayloadField kDataFields[] = {
+            {"data", SkPaintParamsKey::DataPayloadType::kByte, kMaxBlockDataSize},
+    };
 
-        static constexpr int kNumFields1 = 1;
-        static constexpr SkPaintParamsKey::DataPayloadField kDataFields1[kNumFields1] = {
-            { "data", SkPaintParamsKey::DataPayloadType::kByte, kMaxBlockDataSize },
-        };
+    int dummySnippetID = dict->addUserDefinedSnippet("keyAlmostTooBig", SkMakeSpan(kDataFields));
+    SkPaintParamsKey key = create_key(&builder, dummySnippetID, kMaxBlockDataSize);
 
-        int dummySnippetID1 = dict->addUserDefinedSnippet("keyAlmostTooBig",
-                                                          SkMakeSpan(kDataFields1, kNumFields1));
+    // Key is created successfully.
+    REPORTER_ASSERT(reporter, key.sizeInBytes() == SkPaintParamsKey::kMaxBlockSize);
+}
 
-        SkPaintParamsKey key = create_key(&builder, dummySnippetID1, kMaxBlockDataSize);
-        REPORTER_ASSERT(reporter, key.sizeInBytes() == SkPaintParamsKey::kMaxBlockSize);
-    }
+DEF_GRAPHITE_TEST_FOR_CONTEXTS(KeyTooLargeBlockSizeTest, reporter, context) {
 
-    // Too big
-    {
-        static constexpr int kNumFields2 = 1;
-        static constexpr SkPaintParamsKey::DataPayloadField kDataFields2[kNumFields2] = {
-                { "data", SkPaintParamsKey::DataPayloadType::kByte, 1024 },
-        };
+    auto dict = context->priv().shaderCodeDictionary();
+    SkPaintParamsKeyBuilder builder(dict, SkBackend::kGraphite);
 
-        int dummySnippetID2 = dict->addUserDefinedSnippet("keyTooBig",
-                                                          SkMakeSpan(kDataFields2, kNumFields2));
+    // Too big by one byte
+    static const int kBlockDataSize = SkPaintParamsKey::kMaxBlockSize -
+                                      SkPaintParamsKey::kBlockHeaderSizeInBytes + 1;
+    static constexpr SkPaintParamsKey::DataPayloadField kDataFields[] = {
+            {"data", SkPaintParamsKey::DataPayloadType::kByte, kBlockDataSize},
+    };
 
-        SkPaintParamsKey key = create_key(&builder, dummySnippetID2, 1024);
-        // key creation fails
-        REPORTER_ASSERT(reporter, key.isErrorKey());
-    }
+    int dummySnippetID = dict->addUserDefinedSnippet("keyTooBig", SkMakeSpan(kDataFields));
+    SkPaintParamsKey key = create_key(&builder, dummySnippetID, kBlockDataSize);
+
+    // Key creation fails.
+    REPORTER_ASSERT(reporter, key.isErrorKey());
 }
