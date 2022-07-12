6f7b78e5f4351e1f2be9e25412bd22be424f581a
Aaron Krajeski <aaronhk@chromium.org>
2022-07-12 07:29:19 -0400

    SkBitmap::erase(SkColor4f c, const SkIRect& area) was never implemented
    Its defined in the header file though.
    
    Bug: skia:13329
    Change-Id: I5f9551aea269af1295c8c04433f813e8ff2fcec5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/557737
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkBitmap.cpp
+++ src/core/SkBitmap.cpp
@@ -425,6 +425,10 @@ void SkBitmap::erase(SkColor c, const SkIRect& area) const {
     this->erase(SkColor4f::FromColor(c), nullptr, area);
 }
 
+void SkBitmap::erase(SkColor4f c, const SkIRect& area) const {
+    this->erase(c, nullptr, area);
+}
+
 void SkBitmap::eraseColor(SkColor4f c, SkColorSpace* colorSpace) const {
     this->erase(c, colorSpace, SkIRect::MakeWH(this->width(), this->height()));
 }
