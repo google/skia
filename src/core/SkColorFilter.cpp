c1613acdfa54126e0f42c8af36fd6b07c9562629
John Stiles <johnstiles@google.com>
2022-07-19 16:09:10 -0400

    Implement makeComposed with a Runtime color filter.
    This saves us from needing to do anything special to implement color
    filter composition in Graphite.
    
    Change-Id: I7d0a153430956049f7f1552ec94fc38aede10cb0
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/560456
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkColorFilter.cpp
+++ src/core/SkColorFilter.cpp
@@ -180,30 +180,6 @@ public:
         return c ? fOuter->program(p, c, dst, uniforms, alloc) : skvm::Color{};
     }
 
-#if SK_SUPPORT_GPU
-    GrFPResult asFragmentProcessor(std::unique_ptr<GrFragmentProcessor> inputFP,
-                                   GrRecordingContext* context,
-                                   const GrColorInfo& dstColorInfo) const override {
-        // Unfortunately, we need to clone the input before we know we need it. This lets us return
-        // the original FP if either internal color filter fails.
-        auto inputClone = inputFP ? inputFP->clone() : nullptr;
-
-        auto [innerSuccess, innerFP] =
-                fInner->asFragmentProcessor(std::move(inputFP), context, dstColorInfo);
-        if (!innerSuccess) {
-            return GrFPFailure(std::move(inputClone));
-        }
-
-        auto [outerSuccess, outerFP] =
-                fOuter->asFragmentProcessor(std::move(innerFP), context, dstColorInfo);
-        if (!outerSuccess) {
-            return GrFPFailure(std::move(inputClone));
-        }
-
-        return GrFPSuccess(std::move(outerFP));
-    }
-#endif
-
     SK_FLATTENABLE_HOOKS(SkComposeColorFilter)
 
 protected:
@@ -237,7 +213,22 @@ sk_sp<SkColorFilter> SkColorFilter::makeComposed(sk_sp<SkColorFilter> inner) con
         return sk_ref_sp(this);
     }
 
+#ifdef SK_ENABLE_SKSL
+    sk_sp<SkRuntimeEffect> effect = SkMakeCachedRuntimeEffect(
+        SkRuntimeEffect::MakeForColorFilter,
+        "uniform colorFilter outer;"
+        "uniform colorFilter inner;"
+        "half4 main(half4 color) {"
+            "return outer.eval(inner.eval(color));"
+        "}"
+    );
+    SkASSERT(effect);
+
+    sk_sp<SkColorFilter> inputs[] = {sk_ref_sp(this), std::move(inner)};
+    return effect->makeColorFilter(/*uniforms=*/nullptr, inputs, std::size(inputs));
+#else
     return sk_sp<SkColorFilter>(new SkComposeColorFilter(sk_ref_sp(this), std::move(inner)));
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
