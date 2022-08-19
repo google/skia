6450b5a553b46813e0173084f15cda622cac1d16
Brian Salomon <bsalomon@google.com>
2022-08-19 08:21:19 -0400

    [ganesh] Disable GL buffer transfers on Wembley device
    This fixes the mesh_updates GM on this device.
    
    Bug:12720
    
    Change-Id: Id4e591c17aa3fcc0acf7efdbcc8202250a65f8af
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/571184
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Brian Salomon <bsalomon@google.com>

--- src/gpu/ganesh/gl/GrGLCaps.cpp
+++ src/gpu/ganesh/gl/GrGLCaps.cpp
@@ -4220,6 +4220,16 @@ void GrGLCaps::applyDriverCorrectnessWorkarounds(const GrGLContextInfo& ctxInfo,
         fDisableTessellationPathRenderer = true;
     }
 
+    // The Wembley device draws the mesh_update GM incorrectly when using transfer buffers. Buffer
+    // to buffer transfers affect draws earlier in the GL command sequence.
+    // Android API: 31
+    // GL_VERSION : OpenGL ES 3.2 build 1.13@5720833
+    // GL_RENDERER: PowerVR Rogue GE8300
+    // GL_VENDOR  : Imagination Technologies
+    if (ctxInfo.renderer() == GrGLRenderer::kPowerVRRogue) {
+        fTransferFromBufferToBufferSupport = false;
+    }
+
 #ifdef SK_BUILD_FOR_WIN
     // glDrawElementsIndirect fails GrMeshTest on every Win10 Intel bot.
     if (ctxInfo.driver() == GrGLDriver::kIntel ||
