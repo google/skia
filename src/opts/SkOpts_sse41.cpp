25f5c633adcdec9dd1cc282f01e7df4e04df5d77
John Stiles <johnstiles@google.com>
2022-09-12 15:03:04 +0000

    Revert "Delete empty SkOpts files"
    This reverts commit 21665b48d3d1aa8ead872c7b42cab3017388bd9b.
    
    Reason for revert: breaking iwyu http://screen/4JBLn8ZmzwGzKgf
    
    Original change's description:
    > Delete empty SkOpts files
    >
    > See also
    >  - https://chromium-review.googlesource.com/c/chromium/src/+/3888599
    >  - https://pdfium-review.googlesource.com/c/pdfium/+/97910/
    >
    > The "skia_arch_defaults" section of the generated BP file now
    > has two explicitly empty lists instead of two implicitly empty
    > ones. The use of those lists should probably be removed in the
    > Android build, but I know not where that is.
    >
    > Change-Id: I8239837b7f40163bea6b78a840c67926026a5a15
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/579696
    > Reviewed-by: Brian Osman <brianosman@google.com>
    
    Change-Id: Ifffabab8744da0400014b9fd3289495177b995f9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/579999
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

---
+++ src/opts/SkOpts_sse41.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// Intentionally empty, to be cleaned up.
