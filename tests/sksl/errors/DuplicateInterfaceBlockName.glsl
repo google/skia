5a78a864925e10f2bca03f72be2436ca8239367d
John Stiles <johnstiles@google.com>
2022-10-17 12:33:25 -0400

    Demonstrate error with interface blocks.
    We don't put interface block names in the symbol table. This means we
    can add functions with the same name as an interface block, or have
    two interface blocks with the same name, and no error is reported.
    
    I added some non-code to main() to induce an error; otherwise we will
    fail the SkSLError tests (which enforce that everything in errors/
    must trigger at least one error).
    
    Change-Id: Ic206116995d5f4ea3433b59d30c9744ce301990c
    Bug: skia:13820
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/592379
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

---
+++ tests/sksl/errors/DuplicateInterfaceBlockName.glsl
@@ -0,0 +1,9 @@
+### Compilation failed:
+
+error: 6: unknown identifier 'Files'
+    Files in errors/ must contain an error.
+    ^^^^^
+error: 6: expected ';', but found 'in'
+    Files in errors/ must contain an error.
+          ^^
+2 errors
