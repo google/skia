4ea9d356817ccdd91b9f615e172a91910607a4c6
John Stiles <johnstiles@google.com>
2022-10-25 11:48:26 -0400

    Add test for multiple overloads of main().
    This asserts in the SPIR-V backend, and (as you might expect) does not
    result in a valid program from any generator, but we do not flag it as
    an error in the frontend today.
    
    Change-Id: Ia6241a813add006f55844336ee211396fe54c9d6
    Bug: oss-fuzz:52714
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/595396
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Arman Uguray <armansito@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

---
+++ tests/sksl/errors/OverloadedMain.glsl
@@ -0,0 +1,9 @@
+### Compilation failed:
+
+error: 5: no type named 'TODO'
+TODO(johnstiles): overloading main should be forbidden
+^^^^
+error: 5: expected a type, but found '('
+TODO(johnstiles): overloading main should be forbidden
+    ^
+2 errors
