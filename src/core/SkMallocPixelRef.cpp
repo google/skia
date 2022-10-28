57ef1ee8484046b56a08c98fb22a0d1827ac79e9
Kevin Lubick <kjlubick@google.com>
2022-10-28 10:27:25 -0400

    Boost SkMallocPixelRef fuzzing limit
    100k was probably a bit too small, only allowing a ~150 x ~150 bitmap.
    
    10m lets us get to ~1500 x ~1500 which is hopefully a better
    balance between reasonable and fuzzer performance.
    
    Change-Id: Ifde8cb28c89b648ed4af0c9b1ee081d87c755482
    Bug: oss-fuzz:52328
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/597476
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- src/core/SkMallocPixelRef.cpp
+++ src/core/SkMallocPixelRef.cpp
@@ -35,7 +35,7 @@ sk_sp<SkPixelRef> SkMallocPixelRef::MakeAllocate(const SkImageInfo& info, size_t
         return nullptr;
     }
 #if defined(SK_BUILD_FOR_FUZZER)
-    if (size > 100000) {
+    if (size > 10000000) {
         return nullptr;
     }
 #endif
