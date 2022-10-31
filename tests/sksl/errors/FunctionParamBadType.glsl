e90ce26c100304e9bd912c0420adba7ee9d2ff58
John Stiles <johnstiles@google.com>
2022-10-31 13:49:19 -0400

    Demonstrate parse error with missing types.
    Change-Id: I243b0b2fe133adf53a19bb321e994246b82fa298
    Bug: skia:13383
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/598083
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

---
+++ tests/sksl/errors/FunctionParamBadType.glsl
@@ -0,0 +1,12 @@
+### Compilation failed:
+
+error: 1: no type named 'Type1'
+void func(Type1 param1, Type2 param2, Type3 param3) {}
+          ^^^^^
+error: 1: no type named 'param1'
+void func(Type1 param1, Type2 param2, Type3 param3) {}
+                ^^^^^^
+error: 1: expected a type, but found ','
+void func(Type1 param1, Type2 param2, Type3 param3) {}
+                      ^
+3 errors
