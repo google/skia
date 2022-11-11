9e08687d01b962a829f28025e2d07115851c8c17
Brian Osman <brianosman@google.com>
2022-11-11 12:53:28 -0500

    Reland "Fix rect-dashing logic"
    This is a reland of commit 29b9dbd7a8edd2454c45dd81dfd68464cd4a314c
    ... guarded for chromium's web tests.
    
    Original change's description:
    > Fix rect-dashing logic
    >
    > This block of code was attempting to detect when the rect stroke ended
    > inside an "on" region. However, the math was faulty, producing false
    > positives (and potential false negatives as well).
    >
    > Rather than adjusting the total path length by the initial dash length
    > (which doesn't really make sense), we adjust it by the initial phase.
    >
    > Bug: pdfium:1871
    > Change-Id: Ic07b6b0c4456e3a565742148a0e9a6265df41854
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/604156
    > Reviewed-by: Ben Wagner <bungeman@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    
    Bug: pdfium:1871
    Change-Id: I4f715949d5be3e3a73279cbe288375f6cb3ced73
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/604160
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/gpu/ganesh/GrStyle.cpp
+++ src/gpu/ganesh/GrStyle.cpp
@@ -149,7 +149,7 @@ bool GrStyle::applyPathEffect(SkPath* dst, SkStrokeRec* strokeRec, const SkPath&
                                        &initialIndex, &intervalLength);
         if (!SkDashPath::InternalFilter(dst, src, strokeRec,
                                         nullptr, intervals, intervalCnt,
-                                        initialLength, initialIndex, intervalLength,
+                                        initialLength, initialIndex, intervalLength, phase,
                                         SkDashPath::StrokeRecApplication::kDisallow)) {
             return false;
         }
