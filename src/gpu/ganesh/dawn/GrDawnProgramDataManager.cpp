439b658d0aa2802ec006aeed80e2b7f20c04321c
Arman Uguray <armansito@google.com>
2022-12-02 12:38:55 -0800

    [ganesh][dawn] Account for padding when allocating a uniform buffer
    Dawn enforces that buffers holding host-shareable data are sized to
    account for any padding that may be inserted to account for alignment
    and layout requirements.
    
    The Dawn backend now allocates uniform buffers to accommodate any
    trailing bytes as required by WGSL's "RequiredAlignOf" rules for the
    uniform address space.
    
    Change-Id: I6cdaa55f4140f7f432cc6a7e184c7bd26f01ca4b
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/613021
    Commit-Queue: Arman Uguray <armansito@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- src/gpu/ganesh/dawn/GrDawnProgramDataManager.cpp
+++ src/gpu/ganesh/dawn/GrDawnProgramDataManager.cpp
@@ -11,7 +11,17 @@
 
 GrDawnProgramDataManager::GrDawnProgramDataManager(const UniformInfoArray& uniforms,
                                                    uint32_t uniformBufferSize)
-    : GrUniformDataManager(uniforms.count(), uniformBufferSize) {
+        : GrUniformDataManager(
+                  uniforms.count(),
+                  // Dawn uses a std140-like layout for uniform data which requires certain types to
+                  // have an alignment of 16. This layout will often involve padding to be inserted
+                  // at the end of data last uniform entry as described in
+                  // https://www.w3.org/TR/WGSL/#alignment-and-size.
+                  //
+                  // Dawn enforces buffers to appropriately sized to accommodate such padding, even
+                  // if it involves trailing bytes that will never get written or read. We make sure
+                  // that the buffers we bind for uniforms abide by this validation rule.
+                  SkAlignTo(uniformBufferSize, 16)) {
     memset(fUniformData.get(), 0, uniformBufferSize);
     // We must add uniforms in same order is the UniformInfoArray so that UniformHandles already
     // owned by other objects will still match up here.
