6c4e64ff76d297c7393057761139edf316d8e48b
Herb Derby <herb@google.com>
2023-01-03 17:19:30 -0500

    Fix forward skia::internal for Android code
    Android sets SK_SUPPORT_LEGACY_ALPHA_BITMAP_AS_COVERAGE
    which has a SkAutoTMalloc not in the correct namespace.
    
    Change-Id: I89aec05d37ff2aefe5f90086c3de2dcb66552f9a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/624734
    Reviewed-by: Brian Osman <brianosman@google.com>
    Auto-Submit: Herb Derby <herb@google.com>
    Commit-Queue: Herb Derby <herb@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- src/core/SkDraw.cpp
+++ src/core/SkDraw.cpp
@@ -1017,7 +1017,7 @@ void SkDraw::drawBitmapAsMask(const SkBitmap& bitmap, const SkSamplingOptions& s
         }
 
         // allocate (and clear) our temp buffer to hold the transformed bitmap
-        SkAutoTMalloc<uint8_t> storage(size);
+        skia::internal::SkAutoTMalloc<uint8_t> storage(size);
         mask.fImage = storage.get();
         memset(mask.fImage, 0, size);
 
