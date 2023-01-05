54dbda29090818d5dcc7ae956750d4a0cb7485e8
Kevin Lubick <kjlubick@google.com>
2023-01-05 14:24:29 -0500

    Add shim includes for Android
    We have some CLs out to allow us to remove these shims:
     - http://ag/c/platform/frameworks/base/+/20884743
     - http://ag/c/platform/frameworks/base/+/20886442
     - http://ag/c/platform/frameworks/base/+/20884923
    
    Change-Id: I06339dd500b8054a4b0bc7ab3b832ddcd5da6cb9
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/626657
    Reviewed-by: Nolan Scobie <nscobie@google.com>

---
+++ include/private/SkMalloc.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+// Temporary work around until we remove uses of this private file from Android
+#include "include/private/base/SkMalloc.h"
