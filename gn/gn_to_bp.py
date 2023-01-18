ad00c1e0f852470ae045a02ca4bcce667d9cf78a
Victor Chang <vichang@google.com>
2023-01-18 11:20:55 +0000

    Avoid exporting libandroidicu headers
    The headers are mixed up with libicu headers when compiling
    other modules using both skia and libicu.
    
    Change-Id: Ie9ba0bc027479b317d00a16c429bcfae345776b7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/631096
    Reviewed-by: Jorge Betancourt <jmbetancourt@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>
    Auto-Submit: Victor Chang <vichang@google.com>

--- gn/gn_to_bp.py
+++ gn/gn_to_bp.py
@@ -292,9 +292,6 @@ cc_defaults {
             "libjpegdecoder",
             "libjpegencoder",
         ],
-        export_shared_lib_headers: [
-            "libandroidicu",
-        ],
       },
       host: {
         shared_libs: [
