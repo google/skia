04576d0e5cd8f48bb971fb6846309b73512468b8
Ben Wagner <bungeman@google.com>
2023-01-30 17:35:49 -0500

    [pdf] Really retain zero glyph in subset
    In modern versions of HarfBuzz it is not enough to simply add the
    .notdef glyph 0 to the list of glyphs to retain, one must also set the
    HB_SUBSET_FLAGS_NOTDEF_OUTLINE flag to retain its outline. Set this flag
    if glyph 0 is on the list of used glyphs.
    
    Change-Id: Ie64897cd02a7312c7f007f82e69df09b93711f10
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/635603
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Herb Derby <herb@google.com>

--- src/pdf/SkPDFSubsetFont.cpp
+++ src/pdf/SkPDFSubsetFont.cpp
@@ -49,10 +49,14 @@ struct SkPDFHarfBuzzSubset {
     // This is the HarfBuzz 3.0 interface.
     // hb_subset_flags_t does not exist in 2.0. It isn't dependent on T, so inline the value of
     // HB_SUBSET_FLAGS_RETAIN_GIDS until 2.0 is no longer supported.
-    static HBFace Make(T input, hb_face_t* face) {
+    static HBFace Make(T input, hb_face_t* face, bool retainZeroGlyph) {
         // TODO: When possible, check if a font is 'tricky' with FT_IS_TRICKY.
         // If it isn't known if a font is 'tricky', retain the hints.
-        hb_subset_input_set_flags(input, 2/*HB_SUBSET_FLAGS_RETAIN_GIDS*/);
+        unsigned int flags = 0x2u/*HB_SUBSET_FLAGS_RETAIN_GIDS*/;
+        if (retainZeroGlyph) {
+            flags |= 0x40u/*HB_SUBSET_FLAGS_NOTDEF_OUTLINE*/;
+        }
+        hb_subset_input_set_flags(input, flags);
         return HBFace(hb_subset_or_fail(face, input));
     }
 };
@@ -65,7 +69,7 @@ struct SkPDFHarfBuzzSubset<T, void_t<
 {
     // This is the HarfBuzz 2.0 (non-public) interface, used if it exists.
     // This code should be removed as soon as all users are migrated to the newer API.
-    static HBFace Make(T input, hb_face_t* face) {
+    static HBFace Make(T input, hb_face_t* face, bool) {
         hb_subset_input_set_retain_gids(input, true);
         // TODO: When possible, check if a font is 'tricky' with FT_IS_TRICKY.
         // If it isn't known if a font is 'tricky', retain the hints.
@@ -91,7 +95,8 @@ static sk_sp<SkData> subset_harfbuzz(sk_sp<SkData> fontData,
     hb_set_t* glyphs = hb_subset_input_glyph_set(input.get());
     glyphUsage.getSetValues([&glyphs](unsigned gid) { hb_set_add(glyphs, gid);});
 
-    HBFace subset = SkPDFHarfBuzzSubset<hb_subset_input_t*>::Make(input.get(), face.get());
+    HBFace subset = SkPDFHarfBuzzSubset<hb_subset_input_t*>::Make(input.get(), face.get(),
+                                                                  glyphUsage.has(0));
     if (!subset) {
         return nullptr;
     }
