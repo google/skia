ad3f8d9c449ac11ea7aca73ec5827f85b338cf02
Christopher Cameron <ccameron@chromium.org>
2023-01-30 23:14:22 +0100

    SkJpegCodec: Add SkJpegSourceMgr (reland)
    The current skjpeg_source_mgr (in SkJpegUtility.cpp/h) is effectively
    two implementations (buffered vs in-memory) controlled via if
    statements.
    
    Turn these into two implementations of a pure virtual interface
    SkJpegSourceMgr. The reason for this it that there will be a third
    interface added for buffered non-seekable sources (which will
    run an SkJpegSegmentScanner on the data as it is read).
    
    Leave the pre-existing skjpeg_source_mgr in place because it may
    be used in other places. It will be removed separately.
    
    This was reverted because of Chromium roll issues related to adding
    the new SkJpegSourceMgr.cpp/h files.
    
    Bug: skia:14031
    Change-Id: If7ccf386dd17de8eaf468bb8e81714402173c146
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/633465
    Commit-Queue: Christopher Cameron <ccameron@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/codec/SkJpegSourceMgr.cpp
+++ src/codec/SkJpegSourceMgr.cpp
@@ -5,4 +5,106 @@
  * found in the LICENSE file.
  */
 
-// This file is intentionally left blank.
+#include "src/codec/SkJpegSourceMgr.h"
+
+#include "include/core/SkData.h"
+#include "include/core/SkRefCnt.h"
+#include "include/core/SkStream.h"
+#include "src/codec/SkCodecPriv.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SkJpegMemorySourceMgr
+
+class SkJpegMemorySourceMgr : public SkJpegSourceMgr {
+public:
+    SkJpegMemorySourceMgr(SkStream* stream) : fStream(stream) {}
+    ~SkJpegMemorySourceMgr() override {}
+
+    void initSource(const uint8_t*& nextInputByte, size_t& bytesInBuffer) override {
+        nextInputByte = reinterpret_cast<const uint8_t*>(fStream->getMemoryBase());
+        bytesInBuffer = static_cast<size_t>(fStream->getLength());
+    }
+    bool fillInputBuffer(const uint8_t*& nextInputByte, size_t& bytesInBuffer) override {
+        // The whole JPEG data is expected to reside in the supplied memory buffer, so any request
+        // for more data beyond the given buffer size is treated as an error.
+        SkCodecPrintf("Asked to re-fill a memory-mapped stream.\n");
+        return false;
+    }
+    bool skipInputBytes(size_t bytesToSkip,
+                        const uint8_t*& nextInputByte,
+                        size_t& bytesInBuffer) override {
+        if (bytesToSkip > bytesInBuffer) {
+            SkCodecPrintf("Asked to read past end of a memory-mapped stream.\n");
+            return false;
+        }
+        nextInputByte += bytesToSkip;
+        bytesInBuffer -= bytesToSkip;
+        return true;
+    }
+
+private:
+    SkStream* const fStream;  // unowned.
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SkJpegBufferedSourceMgr
+
+class SkJpegBufferedSourceMgr : public SkJpegSourceMgr {
+public:
+    SkJpegBufferedSourceMgr(SkStream* stream) : fStream(stream) {}
+    ~SkJpegBufferedSourceMgr() override {}
+
+    void initSource(const uint8_t*& nextInputByte, size_t& bytesInBuffer) override {
+        constexpr size_t kBufferSize = 1024;
+        fBuffer = SkData::MakeUninitialized(kBufferSize);
+        nextInputByte = fBuffer->bytes();
+        bytesInBuffer = 0;
+    }
+    bool fillInputBuffer(const uint8_t*& nextInputByte, size_t& bytesInBuffer) override {
+        size_t bytesRead = fStream->read(fBuffer->writable_data(), fBuffer->size());
+        if (bytesRead == 0) {
+            // Fail if we read zero bytes (libjpeg will accept any non-zero number of bytes).
+            SkCodecPrintf("Hit end of file reading a buffered stream.\n");
+            return false;
+        }
+        nextInputByte = fBuffer->bytes();
+        bytesInBuffer = bytesRead;
+        return true;
+    }
+    bool skipInputBytes(size_t bytesToSkip,
+                        const uint8_t*& nextInputByte,
+                        size_t& bytesInBuffer) override {
+        // Skip through the already-read (or already in memory) buffer.
+        if (bytesToSkip <= bytesInBuffer) {
+            nextInputByte += bytesToSkip;
+            bytesInBuffer -= bytesToSkip;
+            return true;
+        }
+        bytesToSkip -= bytesInBuffer;
+
+        // Fail if we skip past the end of the stream.
+        if (fStream->skip(bytesToSkip) != bytesToSkip) {
+            SkCodecPrintf("Failed to skip through buffered stream.\n");
+            return false;
+        }
+
+        bytesInBuffer = 0;
+        nextInputByte = fBuffer->bytes();
+        return true;
+    }
+
+private:
+    SkStream* const fStream;  // unowned.
+    sk_sp<SkData> fBuffer;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SkJpegSourceMgr
+
+// static
+std::unique_ptr<SkJpegSourceMgr> SkJpegSourceMgr::Make(SkStream* stream) {
+    if (stream->hasLength() && stream->getMemoryBase()) {
+        return std::make_unique<SkJpegMemorySourceMgr>(stream);
+    }
+    return std::make_unique<SkJpegBufferedSourceMgr>(stream);
+}
--- src/codec/SkJpegSourceMgr.h
+++ src/codec/SkJpegSourceMgr.h
@@ -5,4 +5,30 @@
  * found in the LICENSE file.
  */
 
-// This file is intentionally left blank.
+#ifndef SkJpegSourceMgr_codec_DEFINED
+#define SkJpegSourceMgr_codec_DEFINED
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+class SkStream;
+
+/*
+ * Interface to adapt an SkStream to the jpeg_source_mgr interface. This interface has different
+ * implementations for SkStreams with different capabilities.
+ */
+class SkJpegSourceMgr {
+public:
+    static std::unique_ptr<SkJpegSourceMgr> Make(SkStream* stream);
+    virtual ~SkJpegSourceMgr() {}
+
+    // Interface called by libjpeg via its jpeg_source_mgr interface.
+    virtual void initSource(const uint8_t*& nextInputByte, size_t& bytesInBuffer) = 0;
+    virtual bool fillInputBuffer(const uint8_t*& nextInputByte, size_t& bytesInBuffer) = 0;
+    virtual bool skipInputBytes(size_t bytes,
+                                const uint8_t*& nextInputByte,
+                                size_t& bytesInBuffer) = 0;
+};
+
+#endif
