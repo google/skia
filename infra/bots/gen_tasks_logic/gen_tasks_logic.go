e76d3aae0ac05d74e3e9d2716768ea7fb316aa42
Kevin Lubick <kjlubick@google.com>
2023-02-02 12:41:17 -0500

    Move several GCE perf jobs onto bare-metal machines
    This removes Perf.+PathKit jobs as well - they are not
    providing useful/actionable data anymore.
    
    Change-Id: Ib46f5e9017058022f6a99582a15b23b59961c8f8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/637376
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -755,6 +755,8 @@ func (b *jobBuilder) deriveCompileTaskName() string {
 		}
 		if b.extraConfig("PathKit") {
 			ec = []string{"PathKit"}
+			// We prefer to compile this in the cloud because we have more resources there
+			jobNameMap["os"] = "Debian10"
 		}
 		if b.extraConfig("CanvasKit", "SkottieWASM", "Puppeteer") {
 			if b.cpu() {
@@ -762,7 +764,8 @@ func (b *jobBuilder) deriveCompileTaskName() string {
 			} else {
 				ec = []string{"CanvasKit"}
 			}
-
+			// We prefer to compile this in the cloud because we have more resources there
+			jobNameMap["os"] = "Debian10"
 		}
 		if len(ec) > 0 {
 			jobNameMap["extra_config"] = strings.Join(ec, "_")
@@ -917,6 +920,7 @@ func (b *taskBuilder) defaultSwarmDimensions() {
 					"MacBookPro11.5": "x86-64-i7-4870HQ",
 					"MacMini7.1":     "x86-64-i5-4278U",
 					"NUC5i7RYH":      "x86-64-i7-5557U",
+					"NUC9i7QN":       "x86-64-i7-9750H",
 				},
 				"AVX512": {
 					"GCE":  "x86-64-Skylake_GCE",
@@ -1959,7 +1963,7 @@ func (b *jobBuilder) perf() {
 		b.kitchenTask(recipe, OUTPUT_PERF)
 		b.cas(cas)
 		b.swarmDimensions()
-		if b.extraConfig("CanvasKit", "Docker", "PathKit") {
+		if b.extraConfig("Docker") {
 			b.usesDocker()
 		}
 		if compileTaskName != "" {
