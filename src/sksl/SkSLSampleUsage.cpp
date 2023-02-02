0ded510a41df84272a611651facb466b5b08fd60
John Stiles <johnstiles@google.com>
2023-02-01 20:01:15 -0500

    Eliminate SampleUsage::constructor.
    This seems to be a vestige from our .fp code generator.
    
    Change-Id: Iab7f4b11a467f11d6c0f6596995cc8d0c9a003c2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/636820
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- src/sksl/SkSLSampleUsage.cpp
+++ src/sksl/SkSLSampleUsage.cpp
@@ -8,13 +8,12 @@
 #include "include/private/SkSLSampleUsage.h"
 
 #include <algorithm>
-#include <string>
 
 namespace SkSL {
 
 SampleUsage SampleUsage::merge(const SampleUsage& other) {
-    // This function is only used when processing SkSL, to determine the combined SampleUsage for
-    // a child fp/shader/etc. We should never see matrix sampling here.
+    // This function is only used in Analysis::MergeSampleUsageVisitor to determine the combined
+    // SampleUsage for a child fp/shader/etc. We should never see matrix sampling here.
     SkASSERT(fKind != Kind::kUniformMatrix && other.fKind != Kind::kUniformMatrix);
 
     static_assert(Kind::kExplicit > Kind::kPassThrough);
@@ -22,18 +21,6 @@ SampleUsage SampleUsage::merge(const SampleUsage& other) {
     fKind = std::max(fKind, other.fKind);
 
     return *this;
-}
-
-std::string SampleUsage::constructor() const {
-    // This function is only used when processing SkSL. We should never see matrix sampling here.
-    SkASSERT(fKind != Kind::kUniformMatrix);
-
-    switch (fKind) {
-        case Kind::kNone:        return "SkSL::SampleUsage()";
-        case Kind::kPassThrough: return "SkSL::SampleUsage::PassThrough()";
-        case Kind::kExplicit:    return "SkSL::SampleUsage::Explicit()";
-        default: SkUNREACHABLE;
-    }
 }
 
 }  // namespace SkSL
