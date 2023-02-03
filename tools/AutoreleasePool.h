83a3d8b16c94cf0b62ce63302718a59428849585
Jim Van Verth <jvanverth@google.com>
2023-02-03 13:34:11 -0500

    Enable AutoreleasePool for MacOS/iOS in general.
    Dawn-Metal also needs an AutoreleasePool, and it's good practice
    to have it running on these platforms in any case.
    
    Change-Id: I03da4763f9204e1beed6849a4e342603268a3195
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/638026
    Reviewed-by: John Stiles <johnstiles@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

--- tools/AutoreleasePool.h
+++ tools/AutoreleasePool.h
@@ -12,7 +12,7 @@
  * Helper class for managing an autorelease pool for Metal. On other platforms this will
  * do nothing so there's no need to #ifdef it out.
  */
-#ifdef SK_METAL
+#if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 class AutoreleasePool {
 public:
     AutoreleasePool();
