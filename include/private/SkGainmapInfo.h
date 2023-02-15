b1800a8b9595435e86f2d16b95eeb86d196a37ad
Christopher Cameron <ccameron@chromium.org>
2023-02-15 11:38:55 +0100

    SkGainmapInfo: Fix typos in comments
    Bug: skia:14031
    Change-Id: Iaab47b3a33f653a89d99807d5aa54d10c1f93742
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/643578
    Commit-Queue: Christopher Cameron <ccameron@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- include/private/SkGainmapInfo.h
+++ include/private/SkGainmapInfo.h
@@ -11,15 +11,15 @@
 #include "include/core/SkColor.h"
 
 /**
- *  Gainmap rendering parameters. Suppose our display has HDR to SDR ratio of H, and we wish to
+ *  Gainmap rendering parameters. Suppose our display has HDR to SDR ratio of H and we wish to
  *  display an image with gainmap on this display. Let B be the pixel value from the base image
  *  in a color space that has the primaries of the base image and a linear transfer function. Let
  *  G be the pixel value from the gainmap. Let D be the output pixel in the same color space as B.
  *  The value of D is computed as follows:
  *
  *  First, let W be a weight parameter determing how much the gainmap will be applied.
- *    W = clamp((log(H)                - log(fDisplayRatioHdr)) /
-                (log(fDisplayRatioHdr) - log(fDisplayRatioSdr), 0, 1)
+ *    W = clamp((log(H)                - log(fDisplayRatioSdr)) /
+ *              (log(fDisplayRatioHdr) - log(fDisplayRatioSdr), 0, 1)
  *
  *  Next, let L be the gainmap value in log space. We compute this from the value G that was
  *  sampled from the texture as follows:
@@ -31,7 +31,9 @@
  *  If the base image is HDR then compute:
  *    D = (B + fEpsilonHdr) * exp(L * (W - 1)) - fEpsilonSdr
  *
- *  In the above math, log() is a natural logarithm and exp() is natural exponentiation.
+ *  In the above math, log() is a natural logarithm and exp() is natural exponentiation. Note,
+ *  however, that the base used for the log() and exp() functions does not affect the results of
+ *  the computation (it cancels out, as long as the same base is used throughout).
  */
 struct SkGainmapInfo {
     /**
