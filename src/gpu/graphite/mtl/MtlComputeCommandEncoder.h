3ed2ffd3b148fc2b456a97aa163f8147734a835f
Arman Uguray <armansito@google.com>
2023-02-15 20:50:15 -0800

    [graphite][mtl] Support incremental resource updates in compute encoder
    MtlComputeCommandEncoder now keeps track of assigned resources and only
    sends the command to update them if they have changed. This now also
    supports offset-only updates of buffer bindings.
    
    This now matches the behavior for resource updates in
    MtlRenderCommandEncoder.
    
    Bug: b/240604614
    Change-Id: I76b7b954401d5680e395e673a2dcf1efc0a7e3f9
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/644558
    Commit-Queue: Arman Uguray <armansito@google.com>
    Reviewed-by: Jim Van Verth <jvanverth@google.com>

--- src/gpu/graphite/mtl/MtlComputeCommandEncoder.h
+++ src/gpu/graphite/mtl/MtlComputeCommandEncoder.h
@@ -49,22 +49,43 @@ public:
 
     void setBuffer(id<MTLBuffer> buffer, NSUInteger offset, NSUInteger index) {
         SkASSERT(buffer != nil);
-        // TODO(skia:13580): As with the setVertexBufferOffset:atIndex: and
-        // setFragmentBufferOffset:atIndex: methods of MTLRenderCommandEncoder,
-        // Apple recommends using setBufferOffset:atIndex: to avoid rebinding a buffer when only
-        // updating its offset. Consider tracking buffers/offsets by index and limiting calls to
-        // setBuffer:offset:atIndex.
-        [(*fCommandEncoder) setBuffer:buffer offset:offset atIndex:index];
+        SkASSERT(index < kMaxExpectedBuffers);
+        if (@available(macOS 10.11, iOS 8.3, *)) {
+            if (fBuffers[index] == buffer) {
+                this->setBufferOffset(offset, index);
+                return;
+            }
+        }
+        if (fBuffers[index] != buffer || fBufferOffsets[index] != offset) {
+            [(*fCommandEncoder) setBuffer:buffer offset:offset atIndex:index];
+            fBuffers[index] = buffer;
+            fBufferOffsets[index] = offset;
+        }
+    }
+
+    void setBufferOffset(NSUInteger offset, NSUInteger index)
+            SK_API_AVAILABLE(macos(10.11), ios(0.3)) {
+        SkASSERT(index < kMaxExpectedBuffers);
+        if (fBufferOffsets[index] != offset) {
+            [(*fCommandEncoder) setBufferOffset:offset atIndex:index];
+            fBufferOffsets[index] = offset;
+        }
     }
 
     void setTexture(id<MTLTexture> texture, NSUInteger index) {
-        SkASSERT(texture != nil);
-        [(*fCommandEncoder) setTexture:texture atIndex:index];
+        SkASSERT(index < kMaxExpectedTextures);
+        if (fTextures[index] != texture) {
+            [(*fCommandEncoder) setTexture:texture atIndex:index];
+            fTextures[index] = texture;
+        }
     }
 
     void setSamplerState(id<MTLSamplerState> sampler, NSUInteger index) {
-        SkASSERT(sampler != nil);
-        [(*fCommandEncoder) setSamplerState:sampler atIndex:index];
+        SkASSERT(index < kMaxExpectedTextures);
+        if (fSamplers[index] != sampler) {
+            [(*fCommandEncoder) setSamplerState:sampler atIndex:index];
+            fSamplers[index] = sampler;
+        }
     }
 
     void dispatchThreadgroups(const WorkgroupSize& globalSize, const WorkgroupSize& localSize) {
@@ -79,10 +100,21 @@ public:
     void endEncoding() { [(*fCommandEncoder) endEncoding]; }
 
 private:
+    inline static constexpr int kMaxExpectedBuffers = 16;
+    inline static constexpr int kMaxExpectedTextures = 16;
+
     MtlComputeCommandEncoder(const SharedContext* sharedContext,
                              sk_cfp<id<MTLComputeCommandEncoder>> encoder)
             : Resource(sharedContext, Ownership::kOwned, skgpu::Budgeted::kYes)
-            , fCommandEncoder(std::move(encoder)) {}
+            , fCommandEncoder(std::move(encoder)) {
+        for (int i = 0; i < kMaxExpectedBuffers; i++) {
+            fBuffers[i] = nil;
+        }
+        for (int i = 0; i < kMaxExpectedTextures; i++) {
+            fTextures[i] = nil;
+            fSamplers[i] = nil;
+        }
+    }
 
     void freeGpuData() override { fCommandEncoder.reset(); }
 
@@ -90,7 +122,11 @@ private:
 
     id<MTLComputePipelineState> fCurrentComputePipelineState = nil;
 
-    // TODO(skia:13580): Keep track of texture/sampler and buffer resources?
+    id<MTLBuffer> fBuffers[kMaxExpectedBuffers];
+    NSUInteger    fBufferOffsets[kMaxExpectedBuffers];
+
+    id<MTLTexture>      fTextures[kMaxExpectedTextures];
+    id<MTLSamplerState> fSamplers[kMaxExpectedTextures];
 };
 
 }  // namespace skgpu::graphite
