dc4608e1d6fc269ca0710be6a98e031fabfaa395
Herb Derby <herb@google.com>
2023-03-10 09:03:39 -0500

    Change Copyright mark to block style
    Change-Id: I4dc7849f9d018331ebbcad96beb79c614419e207
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/655216
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: Herb Derby <herb@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkFontMetricsPriv.cpp
+++ src/core/SkFontMetricsPriv.cpp
@@ -1,6 +1,9 @@
-// Copyright 2023 Google LLC.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 #include "src/core/SkFontMetricsPriv.h"
 
 #include "src/core/SkReadBuffer.h"
