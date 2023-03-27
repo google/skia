34fa7d997c337b303781ed7b6c97ee9b92f33fda
John Stiles <johnstiles@google.com>
2023-03-27 12:31:44 -0400

    Update LLDB visualizer to include skia_private namespace.
    This fixes visualization of types which use AutoTArray, particularly
    SkTHashMap.
    
    Change-Id: I4dfe16a6a007bb20763b6a580b0aa2e80ab2dd83
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/662096
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: Herb Derby <herb@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- platform_tools/debugging/lldb/skia.py
+++ platform_tools/debugging/lldb/skia.py
@@ -217,7 +217,7 @@ def __lldb_init_module(debugger, dict):
     debugger.HandleCommand(
         'type summary add --summary-string "size=${svar%#}" -e -x "^SkS?TArray<.+>$" -w skia')
     debugger.HandleCommand(
-        'type synthetic add -l skia.AutoTArray_SynthProvider -x "^AutoS?TArray<.+>$" -w skia')
+        'type synthetic add -l skia.AutoTArray_SynthProvider -x "^skia_private::AutoS?TArray<.+>$" -w skia')
     debugger.HandleCommand(
-        'type summary add --summary-string "size=${svar%#}" -e -x "^AutoS?TArray<.+>$" -w skia')
+        'type summary add --summary-string "size=${svar%#}" -e -x "^skia_private::AutoS?TArray<.+>$" -w skia')
     debugger.HandleCommand("type category enable skia")
