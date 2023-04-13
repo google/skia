ad2e2165030621a0da9eb14d8fc01c2fcf48da14
Jim Van Verth <jvanverth@google.com>
2023-04-13 15:30:02 -0400

    [Direct3D] Check for null transfer buffer in readPixels.
    Also changed to use the ResourceProvider so we can recycle buffers.
    
    Change-Id: Idb7be607255e90669613bd8b68bf242d052997bc
    Bug: skia:14255
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/671059
    Auto-Submit: Jim Van Verth <jvanverth@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>
    Commit-Queue: Nicolette Prevost <nicolettep@google.com>

--- src/gpu/ganesh/d3d/GrD3DGpu.cpp
+++ src/gpu/ganesh/d3d/GrD3DGpu.cpp
@@ -16,6 +16,8 @@
 #include "src/core/SkMipmap.h"
 #include "src/gpu/ganesh/GrBackendUtils.h"
 #include "src/gpu/ganesh/GrDataUtils.h"
+#include "src/gpu/ganesh/GrDirectContextPriv.h"
+#include "src/gpu/ganesh/GrResourceProvider.h"
 #include "src/gpu/ganesh/GrTexture.h"
 #include "src/gpu/ganesh/GrThreadSafePipelineBuilder.h"
 #include "src/gpu/ganesh/d3d/GrD3DAMDMemoryAllocator.h"
@@ -606,10 +608,16 @@ bool GrD3DGpu::onReadPixels(GrSurface* surface,
     fDevice->GetCopyableFootprints(&desc, 0, 1, 0, &placedFootprint,
                                    nullptr, nullptr, &transferTotalBytes);
     SkASSERT(transferTotalBytes);
-    // TODO: implement some way of reusing buffers instead of making a new one every time.
-    sk_sp<GrGpuBuffer> transferBuffer = this->createBuffer(transferTotalBytes,
-                                                           GrGpuBufferType::kXferGpuToCpu,
-                                                           kDynamic_GrAccessPattern);
+    GrResourceProvider* resourceProvider =
+        this->getContext()->priv().resourceProvider();
+    sk_sp<GrGpuBuffer> transferBuffer = resourceProvider->createBuffer(
+            transferTotalBytes,
+            GrGpuBufferType::kXferGpuToCpu,
+            kDynamic_GrAccessPattern,
+            GrResourceProvider::ZeroInit::kNo);
+    if (!transferBuffer) {
+        return false;
+    }
 
     this->readOrTransferPixels(texResource, rect, transferBuffer, placedFootprint);
     this->submitDirectCommandList(SyncQueue::kForce);
