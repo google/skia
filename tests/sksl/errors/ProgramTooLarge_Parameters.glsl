f5715362aac554ff3108b9e2a56b8ac8a6ef1acb
John Stiles <johnstiles@google.com>
2023-04-13 19:43:38 +0000

    Revert "Enforce program stack limits on function parameters."
    This reverts commit fa089d61014b77048daddb89300a3ab7cdf601bf.
    
    
    Original change's description:
    > Enforce program stack limits on function parameters.
    >
    > Previously, a function's parameter list did not count against its
    > stack size limit.
    >
    > Bug: chromium:1432603
    > Change-Id: If49dce98f3155f3144a766c26b5a3a39401ce1b2
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/670236
    > Auto-Submit: John Stiles <johnstiles@google.com>
    > Commit-Queue: John Stiles <johnstiles@google.com>
    > Reviewed-by: Kevin Lubick <kjlubick@google.com>
    > Reviewed-by: Nicolette Prevost <nicolettep@google.com>
    > (cherry picked from commit 4dc748f14c6650cb45c7086a39af1760bfda41d2)
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/670339
    > Reviewed-by: John Stiles <johnstiles@google.com>
    
    Bug: chromium:1432603
    Change-Id: I44439362f560200e30e6eeb56a86b0f84ee2a930
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/671176
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- tests/sksl/errors/ProgramTooLarge_Parameters.glsl
+++
@@ -1,6 +0,0 @@
-### Compilation failed:
-
-error: 10: variable 'big_chungus' exceeds the stack size limit
-          S no_report /*we don't need to report overflows past the first*/) {}
-                                                                            ^^
-1 error
