1c2d2876434874d9bbdbaf25daca5a7c48dacbfa
Alec Mouri <alecmouri@google.com>
2023-04-28 20:31:02 -0700

    Increase precision of csc uniforms on ganesh
    In particular, PQ's transfer function requires really high precision,
    so using halfs instead of floats creates artifacts on some GPUs.
    
    Bug: b/278122024
    Change-Id: If403103f6f420588869aad00b228e81dd838e94e
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/687083
    Commit-Queue: Alec Mouri <alecmouri@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/gpu/ganesh/glsl/GrGLSLColorSpaceXformHelper.h
+++ src/gpu/ganesh/glsl/GrGLSLColorSpaceXformHelper.h
@@ -29,16 +29,16 @@ public:
         if (colorSpaceXform) {
             fFlags = colorSpaceXform->fSteps.flags;
             if (this->applySrcTF()) {
-                fSrcTFVar = uniformHandler->addUniformArray(nullptr, visibility, SkSLType::kHalf,
+                fSrcTFVar = uniformHandler->addUniformArray(nullptr, visibility, SkSLType::kFloat,
                                                             "SrcTF", kNumTransferFnCoeffs);
                 fSrcTFType = skcms_TransferFunction_getType(&colorSpaceXform->fSteps.srcTF);
             }
             if (this->applyGamutXform()) {
-                fGamutXformVar = uniformHandler->addUniform(nullptr, visibility, SkSLType::kHalf3x3,
+                fGamutXformVar = uniformHandler->addUniform(nullptr, visibility, SkSLType::kFloat3x3,
                                                             "ColorXform");
             }
             if (this->applyDstTF()) {
-                fDstTFVar = uniformHandler->addUniformArray(nullptr, visibility, SkSLType::kHalf,
+                fDstTFVar = uniformHandler->addUniformArray(nullptr, visibility, SkSLType::kFloat,
                                                             "DstTF", kNumTransferFnCoeffs);
                 fDstTFType = skcms_TransferFunction_getType(&colorSpaceXform->fSteps.dstTFInv);
             }
