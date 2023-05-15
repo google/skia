7fd7956727bbdf7d3c956e7dd62fa26e2b58bc3a
Brian Osman <brianosman@google.com>
2023-05-15 15:40:26 -0400

    Add src/cityhash
    This is a clone of https://github.com/google/cityhash,
    trimmed down to the just the bits we need, similar to
    chromium's base/third_party/cityhash.
    
    Minor tweaks to the source code:
    - Removed include of config.h
    - Changed include <city.h> to "src/cityhash/city.h"
    - Removed CRC stuff
    - Fixed shadowed variables in CityHash32
    - Wrapped in skia_private namespace, to prevent ODR violations
    
    Change-Id: I0411bcb529484dd3d69df3c421c5dff15e403927
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/699459
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- bazel/exporter_tool/main.go
+++ bazel/exporter_tool/main.go
@@ -53,6 +53,8 @@ var gniExportDescs = []exporter.GNIExportDesc{
 				"//src/base:private_hdrs",
 				"//src/base:skslc_srcs",
 				"//src/base:srcs",
+				"//src/cityhash:private_hdrs",
+				"//src/cityhash:srcs",
 				"//src/core:core_hdrs",
 				"//src/core:core_skslc_hdrs",
 				"//src/core:core_skslc_srcs",
