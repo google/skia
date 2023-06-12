b1d36560f3bcc65952285e3c2b2e793654a4e596
Dominik Röttsches <drott@chromium.org>
2023-06-12 14:50:57 +0300

    [Fontations] No .0.as_ref() boilerplate, simpler tag conversion
    Define a with_font() method to run a closer on a FontRef returning an
    Option<T> to run operations on a font or return early. This also makes
    it easier to use the ?-operator and chain success/failure. Original
    suggestion by Chad Brokaw.
    
    Use built-in methods for converting from u32 to tag.
    
    Bug: skia:14259
    Change-Id: If993fedbef30c1ad062d0b11f55bb973a64a56fe
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/710636
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Dominik Röttsches <drott@google.com>

--- src/ports/fontations/src/ffi.rs
+++ src/ports/fontations/src/ffi.rs
@@ -17,18 +17,14 @@ use crate::ffi::SkPathWrapper;
 
 fn lookup_glyph_or_zero(font_ref: &BridgeFontRef, codepoint: u32) -> u16 {
     font_ref
-        .0
-        .as_ref()
-        .and_then(|f| f.charmap().map(codepoint))
-        .map_or(0, |id| id.to_u16())
+        .with_font(|f| Some(f.charmap().map(codepoint)?.to_u16()))
+        .unwrap_or_default()
 }
 
 fn num_glyphs(font_ref: &BridgeFontRef) -> u16 {
     font_ref
-        .0
-        .as_ref()
-        .and_then(|f| f.maxp().ok())
-        .map_or(0, |maxp| maxp.num_glyphs())
+        .with_font(|f| Some(f.maxp().ok()?.num_glyphs()))
+        .unwrap_or_default()
 }
 
 struct PathWrapperPen<'a> {
@@ -69,21 +65,20 @@ fn get_path(
     coords: &BridgeNormalizedCoords,
     path_wrapper: Pin<&mut SkPathWrapper>,
 ) -> bool {
-    font_ref.0.as_ref().map_or(false, |f| {
-        let mut cx = Context::new();
-        let mut scaler = cx
-            .new_scaler()
-            .size(Size::new(size))
-            .normalized_coords(coords.0.into_iter())
-            .build(f);
-        let mut pen_dump = PathWrapperPen {
-            path_wrapper: path_wrapper,
-        };
-        match scaler.outline(GlyphId::new(glyph_id), &mut pen_dump) {
-            Ok(_) => true,
-            _ => false,
-        }
-    })
+    font_ref
+        .with_font(|f| {
+            let mut cx = Context::new();
+            let mut scaler = cx
+                .new_scaler()
+                .size(Size::new(size))
+                .normalized_coords(coords.0.into_iter())
+                .build(f);
+            let mut pen_dump = PathWrapperPen {
+                path_wrapper: path_wrapper,
+            };
+            scaler.outline(GlyphId::new(glyph_id), &mut pen_dump).ok()
+        })
+        .is_some()
 }
 
 fn advance_width_or_zero(
@@ -92,19 +87,18 @@ fn advance_width_or_zero(
     coords: &BridgeNormalizedCoords,
     glyph_id: u16,
 ) -> f32 {
-    font_ref.0.as_ref().map_or(0.0, |f| {
-        GlyphMetrics::new(f, Size::new(size), coords.0.coords())
-            .advance_width(GlyphId::new(glyph_id))
-            .unwrap_or(0.0)
-    })
+    font_ref
+        .with_font(|f| {
+            GlyphMetrics::new(f, Size::new(size), coords.0.coords())
+                .advance_width(GlyphId::new(glyph_id))
+        })
+        .unwrap_or_default()
 }
 
 fn units_per_em_or_zero(font_ref: &BridgeFontRef) -> u16 {
     font_ref
-        .0
-        .as_ref()
-        .and_then(|f| f.head().ok())
-        .map_or(0, |head| head.units_per_em())
+        .with_font(|f| Some(f.head().ok()?.units_per_em()))
+        .unwrap_or_default()
 }
 
 fn convert_metrics(skrifa_metrics: &Metrics) -> ffi::Metrics {
@@ -128,18 +122,19 @@ fn get_skia_metrics(
     size: f32,
     coords: &BridgeNormalizedCoords,
 ) -> ffi::Metrics {
-    font_ref.0.as_ref().map_or(ffi::Metrics::default(), |f| {
-        let fontations_metrics = Metrics::new(f, Size::new(size), coords.0.coords());
-        convert_metrics(&fontations_metrics)
-    })
+    font_ref
+        .with_font(|f| {
+            let fontations_metrics = Metrics::new(f, Size::new(size), coords.0.coords());
+            Some(convert_metrics(&fontations_metrics))
+        })
+        .unwrap_or_default()
 }
 
 fn get_localized_strings<'a>(font_ref: &'a BridgeFontRef<'a>) -> Box<BridgeLocalizedStrings<'a>> {
     Box::new(BridgeLocalizedStrings {
-        localized_strings: match font_ref.0.as_ref() {
-            Some(font_ref) => font_ref.localized_strings(StringId::FAMILY_NAME),
-            _ => LocalizedStrings::default(),
-        },
+        localized_strings: font_ref
+            .with_font(|f| Some(f.localized_strings(StringId::FAMILY_NAME)))
+            .unwrap_or_default(),
     })
 }
 
@@ -163,9 +158,8 @@ fn localized_name_next(
 }
 
 fn english_or_first_font_name(font_ref: &BridgeFontRef, name_id: StringId) -> Option<String> {
-    font_ref.0.as_ref().and_then(|font_ref| {
-        font_ref
-            .localized_strings(name_id)
+    font_ref.with_font(|f| {
+        f.localized_strings(name_id)
             .english_or_first()
             .map(|localized_string| localized_string.to_string())
     })
@@ -206,32 +200,27 @@ fn font_ref_is_valid(bridge_font_ref: &BridgeFontRef) -> bool {
 
 use crate::ffi::SkiaDesignCoordinate;
 
-fn u32tag_to_tag(tag: u32) -> Tag {
-    Tag::from_be_bytes([
-        ((tag >> 24) & 0xFF) as u8,
-        ((tag >> 16) & 0xFF) as u8,
-        ((tag >> 8) & 0xFF) as u8,
-        ((tag >> 0) & 0xFF) as u8,
-    ])
-}
-
 fn resolve_into_normalized_coords(
     font_ref: &BridgeFontRef,
     design_coords: &[SkiaDesignCoordinate],
 ) -> Box<BridgeNormalizedCoords> {
     let variation_tuples = design_coords
         .into_iter()
-        .map(|coord| (u32tag_to_tag(coord.axis), coord.value));
+        .map(|coord| (Tag::from_be_bytes(coord.axis.to_be_bytes()), coord.value));
     Box::new(BridgeNormalizedCoords(
         font_ref
-            .0
-            .as_ref()
-            .map(|f| f.axes().location(variation_tuples))
+            .with_font(|f| Some(f.axes().location(variation_tuples)))
             .unwrap_or_default(),
     ))
 }
 
 struct BridgeFontRef<'a>(Option<FontRef<'a>>);
+
+impl<'a> BridgeFontRef<'a> {
+    fn with_font<T>(&'a self, f: impl FnOnce(&'a FontRef) -> Option<T>) -> Option<T> {
+        f(self.0.as_ref()?)
+    }
+}
 
 struct BridgeNormalizedCoords(Location);
 
