10a43e57e0a68e3575c5fbbeeabee84635d5c33b
Brian Osman <brianosman@google.com>
2023-06-27 13:29:34 -0400

    Reland "Fix link errors when XML (expat) is not included"
    This is a reland of commit 0cc002156f50ff48e2b8a45d35a464c8cfc2c724
    
    Guards XMP tests with SK_XML (similar to other existing tests that rely
    on the same functionality). Ideally, we'd fix how the XMP code is wired
    into the build (particularly Bazel), but this unblocks the simple fix.
    
    Original change's description:
    > Fix link errors when XML (expat) is not included
    >
    > This fixes clients like pdfium, who enable codecs, but not XML.
    >
    > Change-Id: I8d62444089aaa6acb9a585e3e17030098e925656
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/717198
    > Commit-Queue: Brian Osman <brianosman@google.com>
    > Reviewed-by: Kevin Lubick <kjlubick@google.com>
    
    Bug: skia:14600
    Change-Id: I7ebd28b723191604361c8b914f1157bdf1b65579
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/720357
    Reviewed-by: James Godfrey-Kittle <jamesgk@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/codec/SkXmp.cpp
+++ src/codec/SkXmp.cpp
@@ -7,8 +7,11 @@
 
 #include "include/private/SkXmp.h"
 
+#include "include/core/SkData.h" // IWYU pragma: keep
+
+#if defined(SK_XML)
+
 #include "include/core/SkColor.h"
-#include "include/core/SkData.h"
 #include "include/core/SkScalar.h"
 #include "include/core/SkStream.h"
 #include "include/private/SkGainmapInfo.h"
@@ -660,3 +663,15 @@ std::unique_ptr<SkXmp> SkXmp::Make(sk_sp<SkData> xmpStandard, sk_sp<SkData> xmpE
     (void)xmp->parseDom(xmpExtended, /*extended=*/true);
     return xmp;
 }
+
+#else  // !defined(SK_XML)
+
+std::unique_ptr<SkXmp> SkXmp::Make(sk_sp<SkData> xmpData) {
+    return nullptr;
+}
+
+std::unique_ptr<SkXmp> SkXmp::Make(sk_sp<SkData> xmpStandard, sk_sp<SkData> xmpExtended) {
+    return nullptr;
+}
+
+#endif
