c769464a8b79c2e4116cfdc5971aa531d6a426c6
Herb Derby <herb@google.com>
2023-07-11 14:31:55 -0400

    Update constants used by SkFloatingPoint.h
    Use inline constexpr for constants, and use
    numeric_limits. This allows SkFloatingPoint.h to remove
    the include cfloat. Add cfloat to all the files that
    actually use it.
    
    Change-Id: I7bbb12a8400ebc4dde9b953b25fb29c41608f811
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/721977
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Herb Derby <herb@google.com>

--- docs/examples/RRect_height.cpp
+++ docs/examples/RRect_height.cpp
@@ -1,6 +1,7 @@
 // Copyright 2019 Google LLC.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 #include "tools/fiddle/examples.h"
+#include <cfloat>
 // HASH=5a3eb1755164a7becec33cec6e6eca31
 REG_FIDDLE(RRect_height, 256, 256, true, 0) {
 void draw(SkCanvas* canvas) {
