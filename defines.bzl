google/skia

commit 4728980564b11ae9448e0b82797cc359bcd32137

[infra] Add rule to build final debugger-app image
Add a Bazel rule to create the final debugger-app Docker image.
The base debugger-app docker image is created in the Skia infra
repo, but needs CanvasKit inserted to create a new completed image.
CanvasKit is added to the final Docker image via Bazel rules
(see http://review.skia.org/718016).

Bug: skia:14345
Change-Id: Idbadbd1883bc04793242c1db5d477c2a8363f058
Reviewed-on: https://skia-review.googlesource.com/c/skia/+/718017
Reviewed-by: Kevin Lubick <kjlubick@google.com>
Commit-Queue: Chris Mumford <cmumford@google.com>

.bazelrc
@@ -105,6 +105,8 @@ build --flag_alias=ck_enable_skp_serialization=//modules/canvaskit:enable_skp_se
 build --flag_alias=ck_disable_skp_serialization=no//modules/canvaskit:enable_skp_serialization
 build --flag_alias=ck_enable_runtime_effect=//modules/canvaskit:enable_runtime_effect
 build --flag_alias=ck_disable_runtime_effect=no//modules/canvaskit:enable_runtime_effect
+build --flag_alias=ck_enable_debugger=//modules/canvaskit:enable_debugger
+build --flag_alias=ck_disable_debugger=no//modules/canvaskit:enable_debugger
 
 # =============================================================================
 # REMOTE BUILD EXECUTION

WORKSPACE.bazel
@@ -518,3 +518,11 @@ load(
     "@io_bazel_rules_docker//container:container.bzl",
     "container_pull",
 )
+
+# Pulls the gcr.io/skia-public/debugger-app-base container.
+container_pull(
+    name = "debugger-app-base",
+    digest = "sha256:a8be3b12ad179d02176f2d8dbf6408561e581c4dc0e24a02fd1b3d0152f31e76",
+    registry = "gcr.io",
+    repository = "skia-public/debugger-app-base",
+)

bazel/buildrc
@@ -87,10 +87,15 @@ build:ck_webgl2 --with_gl_standard=webgl_standard --gpu_backend=gl_backend \
 # CPU build needs legacy shader context otherwise SkPerlinNoiseShader does not render
 build:ck_cpu --enable_sksl --enable_legacy_shader_context
 
+# flags for using the CanvasKit debugger.
+build:ck_debugger --ck_enable_debugger
+
 build:ck_full_webgl2_release --config=canvaskit_full --config=ck_webgl2 --config=release
 build:ck_full_webgl2_debug --config=canvaskit_full --config=ck_webgl2 --config=debug
 build:ck_full_cpu_release --config=canvaskit_full --config=ck_cpu --config=release
 build:ck_full_cpu_debug --config=canvaskit_full --config=ck_cpu --config=debug
+build:ck_full_webgl2_debug_debugger --config=canvaskit_full --config=ck_webgl2 \
+                                    --config=debug --config=ck_debugger
 # TODO(kjlubick) We should be able to configure testing on Chrome or Firefox with this.
 build:ck_full_webgl2_release_chrome --config=ck_full_webgl2_release
 build:ck_full_cpu_release_chrome --config=ck_full_cpu_release

defines.bzl
@@ -58,6 +58,9 @@ GENERAL_DEFINES = [
 }) + select({
     "//src/lazy:enable_discardable_memory_true": ["SK_USE_DISCARDABLE_SCALEDIMAGECACHE"],
     "//src/lazy:enable_discardable_memory_false": [],
+}) + select({
+    "//modules/canvaskit:enable_debugger_true": ["SK_BUILD_FOR_DEBUGGER"],
+    "//conditions:default": [],
 })
 
 GPU_DEFINES = select_multi({

infra/debugger-app/BUILD.bazel
@@ -1,0 +1,23 @@
+load("//bazel:skia_app_container.bzl", "skia_app_container")
+
+# Modify the debugger-app container by injecting the artifacts from this
+# repository on which it depends.
+skia_app_container(
+    name = "debugger_container",
+    base_image = "@debugger-app-base//image",
+    dirs = {
+        "/usr/local/share/debugger-app/": [
+            [
+                # This brings in all the build files.
+                "//modules/canvaskit:canvaskit",
+                "0644",
+            ],
+            [
+                "//modules/canvaskit:version.js",
+                "0644",
+            ],
+        ],
+    },
+    entrypoint = "/usr/local/bin/debugger-app",
+    repository = "skia-public/debugger-app-final",
+)

infra/debugger-app/Makefile
@@ -1,0 +1,14 @@
+BAZEL?=bazelisk
+
+.PHONY: build
+build:
+	$(BAZEL) run //infra/debugger-app:debugger_container \
+	--config=ck_full_webgl2_debug_debugger \
+	--workspace_status_command=bazel/get_workspace_status.sh
+
+# Review section in README.md before running this target
+.PHONY: push_debugger_I_am_really_sure
+push_debugger_I_am_really_sure:
+	$(BAZEL) run //infra/debugger-app:push_debugger_container \
+	--config=ck_full_webgl2_debug_debugger \
+	--workspace_status_command=bazel/get_workspace_status.sh

modules/canvaskit/BUILD.bazel
@@ -171,6 +171,12 @@ CK_LINKOPTS = BASE_LINKOPTS + [
     ],
     ":enable_runtime_effect_false": [],
 }) + select({
+    ":enable_debugger_true": [
+        "--pre-js",
+        "modules/canvaskit/debugger.js",
+    ],
+    ":enable_debugger_false": [],
+}) + select({
     ":include_matrix_js_true": [
         "--pre-js",
         "modules/canvaskit/matrix.js",
@@ -226,7 +232,10 @@ JS_INTERFACE_FILES = [
     "htmlcanvas/preamble.js",
     "htmlcanvas/radialgradient.js",
     "htmlcanvas/util.js",
-]
+] + select({
+    ":enable_debugger_true": ["debugger.js"],
+    ":enable_debugger_false": [],
+})
 
 CK_SRCS = [
     "canvaskit_bindings.cpp",
@@ -243,6 +252,9 @@ CK_SRCS = [
 }) + select({
     ":enable_skottie_true": ["skottie_bindings.cpp"],
     ":enable_skottie_false": [],
+}) + select({
+    ":enable_debugger_true": ["debugger_bindings.cpp"],
+    ":enable_debugger_false": [],
 })
 
 CK_COPTS = [
@@ -273,6 +285,11 @@ cc_binary(
             "//modules/skottie:utils",
         ],
         ":enable_skottie_false": [],
+    }) + select({
+        ":enable_debugger_true": [
+            "//tools/debugger",
+        ],
+        ":enable_debugger_false": [],
     }),
 )
 
@@ -281,6 +298,7 @@ wasm_cc_binary(
     # Whatever is before the dot will be the name of the output js and wasm, aka "the stem".
     # https://github.com/emscripten-core/emsdk/blob/82ad00499a42abde16b363239d2bc83bf5d863ab/bazel/emscripten_toolchain/wasm_cc_binary.bzl#L91
     cc_target = ":canvaskit.build",
+    visibility = ["//infra/debugger-app:__pkg__"],
 )
 
 bool_flag(
@@ -318,6 +336,11 @@ bool_flag(
     default = False,
 )
 
+bool_flag(
+    name = "enable_debugger",
+    default = False,
+)
+
 karma_test(
     name = "canvaskit_js_tests",
     srcs = [
@@ -346,4 +369,12 @@ karma_test(
         ":canvaskit/canvaskit.wasm",
         "//modules/canvaskit/tests/assets:test_assets",
     ],
+)
+
+genrule(
+    name = "make version file",
+    srcs = ["make_version.sh"],
+    outs = ["version.js"],
+    cmd = "$< $@",
+    visibility = ["//infra:__subpackages__"],
 )

modules/canvaskit/make_version.sh
@@ -1,0 +1,22 @@
+#!/bin/bash -e
+
+# Copyright 2023 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Write the SKIA_VERSION to a JavaScript file.
+
+if [ "$1" == "" ]
+then
+  echo "Must supply output version.js file path." >&2
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+VERSION_JS_PATH=$1
+GIT_REVISION=$(git -C ${SCRIPT_DIR} rev-parse HEAD)
+OUTPUT_DIR=$(dirname ${VERSION_JS_PATH})
+
+mkdir -p $(dirname ${VERSION_JS_PATH})
+echo "const SKIA_VERSION = '${GIT_REVISION}';" > ${VERSION_JS_PATH}

commit 224675af83d444b918bd39ce69de47b874dca292

[infra] Reland "Add rule to build final debugger-app image"
This is a relanding of commit 4728980564b11ae9448e0b82797cc359bcd32137
which broke the Blaze build of Skia because there was no BUILD file
in modules/canvaskit. This version adds a new `build_for_debugger`
flag in //bazel/common_config_settings which the autoroller will
migrate to bazel/common_config_settings/BUILD.

Bug: skia:14345
Change-Id: I243ea4234acab2cec3f7e4db466594df386ea693
Reviewed-on: https://skia-review.googlesource.com/c/skia/+/727037
Reviewed-by: Kevin Lubick <kjlubick@google.com>
Commit-Queue: Chris Mumford <cmumford@google.com>

.bazelrc
@@ -82,6 +82,8 @@ build --flag_alias=with_fontations=//bazel/common_config_settings:use_fontations
 build --flag_alias=with_no_harfbuzz=no//bazel/common_config_settings:use_harfbuzz
 build --flag_alias=with_icu=//bazel/common_config_settings:use_icu
 build --flag_alias=with_no_icu=no//bazel/common_config_settings:use_icu
+build --flag_alias=enable_build_for_debugger=//bazel/common_config_settings:build_for_debugger
+build --flag_alias=disable_build_for_debugger=no//bazel/common_config_settings:build_for_debugger
 
 # Flags used by Skia tools, not to be used by clients
 build --flag_alias=force_cpu_tests=//tests:force_cpu_tests

WORKSPACE.bazel
@@ -518,3 +518,11 @@ load(
     "@io_bazel_rules_docker//container:container.bzl",
     "container_pull",
 )
+
+# Pulls the gcr.io/skia-public/debugger-app-base container.
+container_pull(
+    name = "debugger-app-base",
+    digest = "sha256:a8be3b12ad179d02176f2d8dbf6408561e581c4dc0e24a02fd1b3d0152f31e76",
+    registry = "gcr.io",
+    repository = "skia-public/debugger-app-base",
+)

bazel/buildrc
@@ -87,10 +87,15 @@ build:ck_webgl2 --with_gl_standard=webgl_standard --gpu_backend=gl_backend \
 # CPU build needs legacy shader context otherwise SkPerlinNoiseShader does not render
 build:ck_cpu --enable_sksl --enable_legacy_shader_context
 
+# flags for using the CanvasKit debugger.
+build:ck_debugger --enable_build_for_debugger
+
 build:ck_full_webgl2_release --config=canvaskit_full --config=ck_webgl2 --config=release
 build:ck_full_webgl2_debug --config=canvaskit_full --config=ck_webgl2 --config=debug
 build:ck_full_cpu_release --config=canvaskit_full --config=ck_cpu --config=release
 build:ck_full_cpu_debug --config=canvaskit_full --config=ck_cpu --config=debug
+build:ck_full_webgl2_debug_debugger --config=canvaskit_full --config=ck_webgl2 \
+                                    --config=debug --config=ck_debugger
 # TODO(kjlubick) We should be able to configure testing on Chrome or Firefox with this.
 build:ck_full_webgl2_release_chrome --config=ck_full_webgl2_release
 build:ck_full_cpu_release_chrome --config=ck_full_cpu_release

bazel/common_config_settings/BUILD.bazel
@@ -228,6 +228,11 @@ bool_flag(
     default = False,
 )
 
+bool_flag(
+    name = "build_for_debugger",
+    default = False,
+)
+
 # These are some helpers to mean "either the fontmgr was enabled or its factory was"
 
 selects.config_setting_group(

defines.bzl
@@ -58,6 +58,9 @@ GENERAL_DEFINES = [
 }) + select({
     "//src/lazy:enable_discardable_memory_true": ["SK_USE_DISCARDABLE_SCALEDIMAGECACHE"],
     "//src/lazy:enable_discardable_memory_false": [],
+}) + select({
+    "//bazel/common_config_settings:build_for_debugger_true": ["SK_BUILD_FOR_DEBUGGER"],
+    "//conditions:default": [],
 })
 
 GPU_DEFINES = select_multi({

infra/debugger-app/BUILD.bazel
@@ -1,0 +1,23 @@
+load("//bazel:skia_app_container.bzl", "skia_app_container")
+
+# Modify the debugger-app container by injecting the artifacts from this
+# repository on which it depends.
+skia_app_container(
+    name = "debugger_container",
+    base_image = "@debugger-app-base//image",
+    dirs = {
+        "/usr/local/share/debugger-app/": [
+            [
+                # This brings in all the build files.
+                "//modules/canvaskit:canvaskit",
+                "0644",
+            ],
+            [
+                "//modules/canvaskit:version.js",
+                "0644",
+            ],
+        ],
+    },
+    entrypoint = "/usr/local/bin/debugger-app",
+    repository = "skia-public/debugger-app-final",
+)

infra/debugger-app/Makefile
@@ -1,0 +1,14 @@
+BAZEL?=bazelisk
+
+.PHONY: build
+build:
+	$(BAZEL) run //infra/debugger-app:debugger_container \
+	--config=ck_full_webgl2_debug_debugger \
+	--workspace_status_command=bazel/get_workspace_status.sh
+
+# Review section in README.md before running this target
+.PHONY: push_debugger_I_am_really_sure
+push_debugger_I_am_really_sure:
+	$(BAZEL) run //infra/debugger-app:push_debugger_container \
+	--config=ck_full_webgl2_debug_debugger \
+	--workspace_status_command=bazel/get_workspace_status.sh

modules/canvaskit/BUILD.bazel
@@ -171,6 +171,12 @@ CK_LINKOPTS = BASE_LINKOPTS + [
     ],
     ":enable_runtime_effect_false": [],
 }) + select({
+    "//bazel/common_config_settings:build_for_debugger_true": [
+        "--pre-js",
+        "modules/canvaskit/debugger.js",
+    ],
+    "//bazel/common_config_settings:build_for_debugger_false": [],
+}) + select({
     ":include_matrix_js_true": [
         "--pre-js",
         "modules/canvaskit/matrix.js",
@@ -226,7 +232,10 @@ JS_INTERFACE_FILES = [
     "htmlcanvas/preamble.js",
     "htmlcanvas/radialgradient.js",
     "htmlcanvas/util.js",
-]
+] + select({
+    "//bazel/common_config_settings:build_for_debugger_true": ["debugger.js"],
+    "//bazel/common_config_settings:build_for_debugger_false": [],
+})
 
 CK_SRCS = [
     "canvaskit_bindings.cpp",
@@ -243,6 +252,9 @@ CK_SRCS = [
 }) + select({
     ":enable_skottie_true": ["skottie_bindings.cpp"],
     ":enable_skottie_false": [],
+}) + select({
+    "//bazel/common_config_settings:build_for_debugger_true": ["debugger_bindings.cpp"],
+    "//bazel/common_config_settings:build_for_debugger_false": [],
 })
 
 CK_COPTS = [
@@ -273,6 +285,11 @@ cc_binary(
             "//modules/skottie:utils",
         ],
         ":enable_skottie_false": [],
+    }) + select({
+        "//bazel/common_config_settings:build_for_debugger_true": [
+            "//tools/debugger",
+        ],
+        "//bazel/common_config_settings:build_for_debugger_false": [],
     }),
 )
 
@@ -281,6 +298,7 @@ wasm_cc_binary(
     # Whatever is before the dot will be the name of the output js and wasm, aka "the stem".
     # https://github.com/emscripten-core/emsdk/blob/82ad00499a42abde16b363239d2bc83bf5d863ab/bazel/emscripten_toolchain/wasm_cc_binary.bzl#L91
     cc_target = ":canvaskit.build",
+    visibility = ["//infra/debugger-app:__pkg__"],
 )
 
 bool_flag(
@@ -346,4 +364,12 @@ karma_test(
         ":canvaskit/canvaskit.wasm",
         "//modules/canvaskit/tests/assets:test_assets",
     ],
+)
+
+genrule(
+    name = "make version file",
+    srcs = ["make_version.sh"],
+    outs = ["version.js"],
+    cmd = "$< $@",
+    visibility = ["//infra:__subpackages__"],
 )

modules/canvaskit/make_version.sh
@@ -1,0 +1,22 @@
+#!/bin/bash -e
+
+# Copyright 2023 Google LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Write the SKIA_VERSION to a JavaScript file.
+
+if [ "$1" == "" ]
+then
+  echo "Must supply output version.js file path." >&2
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+VERSION_JS_PATH=$1
+GIT_REVISION=$(git -C ${SCRIPT_DIR} rev-parse HEAD)
+OUTPUT_DIR=$(dirname ${VERSION_JS_PATH})
+
+mkdir -p $(dirname ${VERSION_JS_PATH})
+echo "const SKIA_VERSION = '${GIT_REVISION}';" > ${VERSION_JS_PATH}

tools/debugger/BUILD.bazel
@@ -20,7 +20,10 @@ skia_cc_library(
         "DrawCommand.h",
         "JsonWriteBuffer.h",
     ],
-    visibility = ["//tests:__subpackages__"],
+    visibility = [
+        "//modules:__subpackages__",
+        "//tests:__subpackages__",
+    ],
     deps = [
         "//:skia_internal",
         "//tools:sk_sharing_proc",