2b91b7cd66bd8a51adef74d171c4f43245aef386
Michael Ludwig <michaelludwig@google.com>
2023-08-01 15:14:41 -0400

    Skip ImageFilterCropRect_GPU test on Iris655 ANGLE
    Bug: b/294080402
    Change-Id: I3de8ca08f3d57f9dba65e7dcba7c5e925bf8259a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/733839
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -1167,6 +1167,7 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 
 	if b.extraConfig("ANGLE") && b.matchOs("Win") && b.matchGpu("IntelIris(540|655|Xe)") {
 		skip(ALL, "tests", ALL, "SkSLSwitchDefaultOnly_GPU") // skia:12465
+		skip(ALL, "tests", ALL, "ImageFilterCropRect_Gpu") // b/294080402
 	}
 
 	if b.extraConfig("Dawn") {
