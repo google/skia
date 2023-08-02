6009cc6d7d80d6573030d4d7d7ce8ce4b29bbee1
John Stiles <johnstiles@google.com>
2023-08-02 09:13:14 -0400

    Fix variable-shadow warnings from Clang 17.
    Change-Id: I9cbb4ae50088d3d0ea6d494e6f9e7c1d8a719ab2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/734156
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- src/opts/SkUtils_opts.h
+++ src/opts/SkUtils_opts.h
@@ -16,21 +16,21 @@ namespace SK_OPTS_NS {
     template <typename T>
     static void memsetT(T buffer[], T value, int count) {
     #if defined(SK_CPU_SSE_LEVEL) && SK_CPU_SSE_LEVEL >= SK_CPU_SSE_LEVEL_AVX
-        static constexpr int N = 32 / sizeof(T);
+        static constexpr int VecSize = 32 / sizeof(T);
     #else
-        static constexpr int N = 16 / sizeof(T);
+        static constexpr int VecSize = 16 / sizeof(T);
     #endif
-        static_assert(N > 0, "T is too big for memsetT");
-        // Create an N-wide version of value
-        skvx::Vec<N,T> wideValue(value);
-        while (count >= N) {
-            // N at a time, copy the values into the destination buffer
+        static_assert(VecSize > 0, "T is too big for memsetT");
+        // Create an vectorized version of value
+        skvx::Vec<VecSize,T> wideValue(value);
+        while (count >= VecSize) {
+            // Copy the value into the destination buffer (VecSize elements at a time)
             wideValue.store(buffer);
-            buffer += N;
-            count  -= N;
+            buffer += VecSize;
+            count  -= VecSize;
         }
-        // If count was not an even multiple of N, take care of the last few.
-        while (count --> 0) {
+        // If count was not an even multiple of VecSize, take care of the last few.
+        while (count-- > 0) {
             *buffer++ = value;
         }
     }
