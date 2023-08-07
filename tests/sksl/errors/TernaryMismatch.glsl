08447e03c7363b7d195eab1d4fe1ec902c2e97d3
John Stiles <johnstiles@google.com>
2023-08-07 15:32:29 -0400

    Improve errors with ternary ops.
    Previously, we disallowed ternary expressions with a void result,
    but the error message was slightly unclear; we also disallowed
    ternary expressions that contained an array, but only in ES2.
    
    It turns out that WebGL2 explicitly disallows ternaries that
    evaluate to void or to arrays or s-of-a's. I've improved the
    error message in the void case, and made the array case invalid
    all the time instead of just in ES2.
    
    (https://registry.khronos.org/webgl/specs/latest/2.0/#5.26)
    
    Change-Id: I6dbdc98a89dd127d5e7dcdbac51ad17130a9b661
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/736358
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>

--- tests/sksl/errors/TernaryMismatch.glsl
+++ tests/sksl/errors/TernaryMismatch.glsl
@@ -1,9 +1,12 @@
 ### Compilation failed:
 
-error: 1: ternary operator result mismatch: 'bool', 'float'
+error: 2: ternary operator result mismatch: 'bool', 'float'
 void bool_or_float()   { float x = 5 > 2 ? true : 1.0; }
                                            ^^^^^^^^^^
-error: 2: ternary operator result mismatch: 'float3', 'float'
+error: 3: ternary operator result mismatch: 'float3', 'float'
 void float3_or_float() { float x = 5 > 2 ? float3(1) : 1.0; }
                                            ^^^^^^^^^^^^^^^
-2 errors
+error: 4: ternary expression of type 'void' is not allowed
+void void_or_void()    { 5 > 2 ? void_function() : void_function(); }
+                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+3 errors
