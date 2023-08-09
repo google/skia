e25500a4da454e0b89c8bbe65b5f8aa7e3f3fdf7
Kevin Lubick <kjlubick@google.com>
2023-08-09 08:58:26 -0400

    Treat fontations ffi.rs.h as explicit output
    Currently, the GN build sometimes fails if GN thought it did not have
    to rebuild libfontations_ffi.a but the bazel-bin folder did not have the
    header ffi.rs.h that was also required.
    
    Now the bazel_build.py script can copy multiple files and not just into
    the immediate GN folder. I also added documentation to that script to
    help rationalize the functionality.
    
    Change-Id: Iadf4475fa6ccdad80e08789e4276de045350a7f2
    Bug: b/40045269
    Cq-Include-Trybots: luci.skia.skia.primary:Build-Debian10-Clang-x86_64-Debug-Fontations,Build-Mac-Clang-x86_64-Debug-Fontations
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/738779
    Reviewed-by: Dominik Röttsches <drott@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- gn/bazel_build.py
+++ gn/bazel_build.py
@@ -4,6 +4,20 @@
 #
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+#
+# This script runs bazel build [target] [additional_arg] [additional_arg]
+# and then copies each of the specified outputs from the bazel-bin output
+# folder under the current working directory. This allows GN to shell out
+# to Bazel to generate or compile files, but still have them show up where
+# GN expects (in the output folder).
+#
+# The first argument passed in to this script is the Bazel target.
+# subsequent arguments are treated either as Bazel flags (if they start with
+# --) or output files to copy (if they do not).
+#
+# Output files may contain an equals sign (=), which means the first part is
+# the Bazel file to copy and the second part is where to copy it. If omitted,
+# the file will go immediately under the GN output directory.
 
 import hashlib
 import os
@@ -12,21 +26,42 @@ import subprocess
 import sys
 
 target = sys.argv[1]
-output = sys.argv[2]
+outputs = {}
+bazel_args = []
+for arg in sys.argv[2:]:
+    if arg.startswith("--"):
+        bazel_args.append(arg)
+    else:
+        if "=" in arg:
+            # "../../bazel-bin/src/ports/ffi.h=src/core/ffi.h" means to
+            # copy ../../bazel-bin/src/ports/ffi.h to
+            # $GN_OUTPUT/src/core/ffi.h
+            bazel_file, output_path = arg.split("=")
+            outputs[bazel_file] = output_path
+        else:
+            # "../../bazel-bin/src/ports/libstuff.a" means to copy
+            # ../../bazel-bin/src/ports/libstuff.a to
+            # $GN_OUTPUT/libstuff.a
+            outputs[arg] = os.path.basename(arg)
 
 print("Invoking bazelisk from ", os.getcwd())
-
 # Forward the remaining args to the bazel invocation
-subprocess.run(["bazelisk", "build", target ] + sys.argv[3:], check=True)
+subprocess.run(["bazelisk", "build", target ] + bazel_args, check=True)
 
-expected_output = os.path.join(os.getcwd(), os.path.basename(output))
-if not os.path.exists(expected_output):
-    shutil.copyfile(output, expected_output)
-    os.chmod(expected_output, 0o755)
-else:
-    created_hash = hashlib.sha256(open(output, 'rb').read()).hexdigest()
-    existing_hash = hashlib.sha256(open(expected_output, 'rb').read()).hexdigest()
-    if created_hash != existing_hash:
-        os.remove(expected_output)
-        shutil.copyfile(output, expected_output)
+for bazel_file, output_path in outputs.items():
+    # GN expects files to be created underneath the output directory from which
+    # this script is invoked.
+    expected_output = os.path.join(os.getcwd(), output_path)
+    if not os.path.exists(expected_output):
+        shutil.copyfile(bazel_file, expected_output)
         os.chmod(expected_output, 0o755)
+    else:
+        # GN uses timestamps to determine if it should re-build a file. If the
+        # two files match (via hash) we should not re-copy the output, which would
+        # re-trigger subsequent rebuilds.
+        created_hash = hashlib.sha256(open(bazel_file, 'rb').read()).hexdigest()
+        existing_hash = hashlib.sha256(open(expected_output, 'rb').read()).hexdigest()
+        if created_hash != existing_hash:
+            os.remove(expected_output)
+            shutil.copyfile(bazel_file, expected_output)
+            os.chmod(expected_output, 0o755)
