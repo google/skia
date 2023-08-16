f30893561924659685b4c0fcffc2ee23be7246fd
John Stiles <johnstiles@google.com>
2023-08-16 08:49:32 -0400

    Add nanobench test for a Graphite 'small' SkSL shader.
    Just like Ganesh, I've taken the fragment shader used by
    GM_lcdtext for our "small" benchmark.
    
    Change-Id: I03041649d147afe6892c6f89d8d4fe623ad0a164
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/743236
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Jim Van Verth <jvanverth@google.com>

--- bench/SkSLBench.cpp
+++ bench/SkSLBench.cpp
@@ -501,6 +501,7 @@ COMPILER_BENCH(tiny, "void main() { sk_FragColor = half4(1); }");
 #define GRAPHITE_BENCH(name, text) /* Graphite is disabled */
 #endif
 
+// This fragment shader is from the third tile on the top row of GM_gradients_2pt_conical_outside.
 GRAPHITE_BENCH(graphite_large, R"(
 layout(location=0) in flat int shadingSsboIndexVar;
 layout(location=1) in float2 localCoordsVar;
@@ -561,6 +562,47 @@ void main()
 	half4 outputCoverage;
 	outputCoverage = analytic_rrect_coverage_fn(sk_FragCoord, jacobian, edgeDistances, xRadii, yRadii, strokeParams, perPixelControl);
 	sk_FragColor = outColor_5 * outputCoverage;
+}
+)");
+
+// This fragment shader is taken from GM_lcdtext.
+GRAPHITE_BENCH(graphite_small, R"(
+layout(location=0) in flat int shadingSsboIndexVar;
+layout(location=1) in float2 textureCoords;
+layout(location=2) in half texIndex;
+layout(location=3) in half maskFormat;
+layout (binding=1) uniform StepUniforms
+{
+	layout(offset=0) float4x4 subRunDeviceMatrix;
+	layout(offset=64) float4x4 deviceToLocal;
+	layout(offset=128) float2 atlasSizeInv;
+}
+;
+struct FSUniformData
+{
+	// 0 - SolidColor uniforms
+	float4 color_0;
+}
+;
+layout (binding=2) buffer FSUniforms
+{
+	FSUniformData fsUniformData[];
+}
+;
+layout(binding=0) uniform sampler2D text_atlas_0;
+layout(binding=1) uniform sampler2D text_atlas_1;
+layout(binding=2) uniform sampler2D text_atlas_2;
+layout(binding=3) uniform sampler2D text_atlas_3;
+void main()
+{
+	half4 initialColor = half4(0);
+	// [0] SolidColor
+	half4 outColor_0 = sk_solid_shader(fsUniformData[shadingSsboIndexVar].color_0);
+	// [1] SrcOver
+	half4 outColor_1 = outColor_0;
+	half4 outputCoverage;
+	outputCoverage = bitmap_text_coverage_fn(sample_indexed_atlas(textureCoords, int(texIndex), text_atlas_0, text_atlas_1, text_atlas_2, text_atlas_3), int(maskFormat));
+	sk_FragColor = outColor_1 * outputCoverage;
 }
 )");
 
