76898dad9fda119aa14b05fba18d32965c54015e
Robert Phillips <robertphillips@google.com>
2023-08-23 14:54:58 -0400

    Add unit test for bitmap-backed SkImage cache key extraction
    Change-Id: I679699159baf368deeddc20e2910f15a98e3e114
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/746479
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>

--- tests/graphite/CacheKeyTest.cpp
+++ tests/graphite/CacheKeyTest.cpp
@@ -5,6 +5,7 @@
  * found in the LICENSE file.
  */
 
+#include "include/core/SkBitmap.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkColorSpace.h"
 #include "include/core/SkPaint.h"
@@ -34,11 +35,25 @@ sk_sp<SkPicture> create_picture(int width, int height) {
     return recorder.finishRecordingAsPicture();
 }
 
+
+SkBitmap create_bitmap(int width, int height) {
+    SkImageInfo ii = SkImageInfo::Make(width, height, kRGBA_8888_SkColorType, kPremul_SkAlphaType);
+
+    SkBitmap bm;
+    bm.allocPixels(ii);
+
+    bm.eraseColor(SK_ColorMAGENTA);
+
+    bm.setImmutable();
+    return bm;
+}
+
 } // anonymous namespace
 
 // In this test we just iterate through the cases we expect to work and verify that rewrapping the
 // base SkPicture doesn't block finding the earlier cached image.
-DEF_GRAPHITE_TEST_FOR_ALL_CONTEXTS(CacheKeyTest, reporter, context, CtsEnforcement::kNextRelease) {
+DEF_GRAPHITE_TEST_FOR_ALL_CONTEXTS(CacheKeyTest_Picture, reporter, context,
+                                   CtsEnforcement::kNextRelease) {
     std::unique_ptr<Recorder> recorder = context->makeRecorder();
 
     RecorderOptions options = ToolUtils::CreateTestingRecorderOptions();
@@ -78,5 +93,37 @@ DEF_GRAPHITE_TEST_FOR_ALL_CONTEXTS(CacheKeyTest, reporter, context, CtsEnforceme
                 }
             }
         }
+    }
+}
+
+// In this test we just iterate through the cases we expect to work and verify that rewrapping the
+// subsetted SkBitmap doesn't block finding the earlier cached image.
+DEF_GRAPHITE_TEST_FOR_ALL_CONTEXTS(CacheKeyTest_Bitmap, reporter, context,
+                                   CtsEnforcement::kNextRelease) {
+    std::unique_ptr<Recorder> recorder = context->makeRecorder();
+
+    RecorderOptions options = ToolUtils::CreateTestingRecorderOptions();
+
+    sk_sp<ImageProvider> provider = options.fImageProvider;
+
+    SkBitmap orig = create_bitmap(128, 128);
+    SkBitmap subset;
+    orig.extractSubset(&subset, SkIRect::MakeXYWH(32, 32, 64, 64));
+    SkASSERT(orig.getGenerationID() == subset.getGenerationID());
+
+    for (bool mipmapped : { false, true }) {
+        sk_sp<SkImage> image1 = SkImages::RasterFromBitmap(subset);
+
+        sk_sp<SkImage> result1 = provider->findOrCreate(recorder.get(),
+                                                        image1.get(),
+                                                        {mipmapped});
+
+        sk_sp<SkImage> image2 = SkImages::RasterFromBitmap(subset);
+        REPORTER_ASSERT(reporter, image1->uniqueID() != image2->uniqueID());
+
+        sk_sp<SkImage> result2 = provider->findOrCreate(recorder.get(),
+                                                        image2.get(),
+                                                        {mipmapped});
+        REPORTER_ASSERT(reporter, result1 == result2);
     }
 }
