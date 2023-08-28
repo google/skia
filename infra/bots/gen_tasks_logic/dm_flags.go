13f83fec18db1d1921c0ec927cc17f1df0fb688e
Jim Van Verth <jvanverth@google.com>
2023-08-28 14:22:11 -0400

    Re-enable *AsyncReadPixels on Adreno GL.
    Vulkan is still failing in some cases.
    
    Bug: b/296440036
    Change-Id: I5683f32fe5fec7aea38b560d330942ca9a5cc251
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/748317
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

--- infra/bots/gen_tasks_logic/dm_flags.go
+++ infra/bots/gen_tasks_logic/dm_flags.go
@@ -1080,8 +1080,6 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 	}
 
 	if b.matchGpu("Adreno[3456]") { // disable broken tests on Adreno 3/4/5/6xx
-		skip(ALL, "tests", ALL, "ImageAsyncReadPixels")   // b/296440036
-		skip(ALL, "tests", ALL, "SurfaceAsyncReadPixels") // b/296440036
 		skip(ALL, "tests", ALL, "SkSLArrayCast_Ganesh")       // skia:12332
 		skip(ALL, "tests", ALL, "SkSLArrayComparison_Ganesh") // skia:12332
 		skip(ALL, "tests", ALL, "SkSLCommaSideEffects_Ganesh")
@@ -1111,6 +1109,8 @@ func (b *taskBuilder) dmFlags(internalHardwareLabel string) {
 	}
 
 	if b.matchGpu("Adreno[56]") && b.extraConfig("Vulkan") { // disable broken tests on Adreno 5/6xx Vulkan
+		skip(ALL, "tests", ALL, "ImageAsyncReadPixels") // b/296440036
+		skip(ALL, "tests", ALL, "SurfaceAsyncReadPixels") // b/296440036
 		skip(ALL, "tests", ALL, "SkSLInoutParameters_Ganesh")        // skia:12869
 		skip(ALL, "tests", ALL, "SkSLOutParams_Ganesh")              // skia:11919
 		skip(ALL, "tests", ALL, "SkSLOutParamsDoubleSwizzle_Ganesh") // skia:11919
