ce2c94883cb565bccbe7b6b6466408cb004df478
John Stiles <johnstiles@google.com>
2023-08-28 10:10:39 -0400

    Enable WGSL in the Dawn Graphite backend.
    Change-Id: I2c7efab456bdc9a4d13e641ee23d203cdfa11195
    Bug: b/40044196
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/748316
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- include/gpu/graphite/ContextOptions.h
+++ include/gpu/graphite/ContextOptions.h
@@ -80,7 +80,7 @@ struct SK_API ContextOptions {
      * rely on Tint's SPIR-V Reader. When true, we emit native WGSL.
      * TODO(b/40044196): once WGSL is stable, remove this flag and always emit WGSL.
      */
-    bool fEnableWGSL = false;
+    bool fEnableWGSL = true;
 
     static constexpr size_t kDefaultContextBudget = 256 * (1 << 20);
     /**
