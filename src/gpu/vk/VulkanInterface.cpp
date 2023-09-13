b38989859b81cba40dc7e83d99333dd51bef8c1b
John Stiles <johnstiles@google.com>
2023-09-13 18:27:29 +0000

    Fix GCC warning about missing function.
    GCC catches a live one--we were going through the motions of
    logging an error, without actually calling any function.
    
    Change-Id: I9ef20a6b70951770c9266ae753cdb259415bf7d0
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/755516
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- src/gpu/vk/VulkanInterface.cpp
+++ src/gpu/vk/VulkanInterface.cpp
@@ -257,13 +257,13 @@ VulkanInterface::VulkanInterface(VulkanGetProc getProc,
 }
 
 #ifdef SK_DEBUG
-    static int kIsDebug = 1;
+    constexpr int kIsDebug = 1;
 #else
-    static int kIsDebug = 0;
+    constexpr int kIsDebug = 0;
 #endif
 
-#define RETURN_FALSE_INTERFACE                                                             \
-    if (kIsDebug) { ("%s:%d VulkanInterface::validate() failed.\n", __FILE__, __LINE__); } \
+#define RETURN_FALSE_INTERFACE                                                                     \
+    if (kIsDebug) { SkDebugf("%s:%d VulkanInterface::validate() failed.\n", __FILE__, __LINE__); } \
     return false;
 
 bool VulkanInterface::validate(uint32_t instanceVersion,
