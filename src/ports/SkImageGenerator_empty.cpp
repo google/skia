b584f7efa5a6fd4c6f63bcb0d32023d7f47a7b33
Kevin Lubick <kjlubick@google.com>
2023-10-13 08:26:56 -0400

    Remove empty filegroups
    See also http://cl/573188827
    
    Change-Id: Iffdda970d079e7454c02e0114ce6c85ec47c7034
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/765756
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- src/ports/SkImageGenerator_empty.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2023 Google LLC
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-// TODO(kjlubick) Remove this file after updating G3
