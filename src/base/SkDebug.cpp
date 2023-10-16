751358929d1fadb9a2e8d23f59a544919689813a
Kevin Lubick <kjlubick@google.com>
2023-10-16 16:53:05 -0400

    Move SkDebug.cpp from core to base
    The symbol defined there is declared in include/private/base,
    so we can't put it in the src/core module.
    
    Change-Id: I60ed9fd6c84c301511986c52b7f4e1349f6d16ab
    Bug: b/40045064
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/766681
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Auto-Submit: Kevin Lubick <kjlubick@google.com>

--- src/base/SkDebug.cpp
+++ src/base/SkDebug.cpp
@@ -4,8 +4,10 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#include "include/private/base/SkDebug.h"
 
-#include "include/core/SkTypes.h"
+#include "include/private/base/SkAssert.h" // IWYU pragma: keep
+#include "include/private/base/SkAttributes.h" // IWYU pragma: keep
 
 #if defined(SK_BUILD_FOR_GOOGLE3)
 void SkDebugfForDumpStackTrace(const char* data, void* unused) {
