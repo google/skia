f0ef94b13f97fcc5f7fade2e9c35b0e30f185aec
James Godfrey-Kittle <jamesgk@google.com>
2023-10-16 16:39:42 -0400

    Reland "[graphite] Test that failed dst copies don't cause a crash"
    This is a reland of commit 64980a5a3adc80f1379d54f1570491e92e244fa2,
    which with review.skia.org/767556 should no longer break any builds.
    
    Original change's description:
    > [graphite] Test that failed dst copies don't cause a crash
    >
    > This adds a test which would segfault if not for review.skia.org/755577
    > and review.skia.org/764417.
    >
    > Bug: chromium:1489392
    > Change-Id: Iaa83005f0a46522e754de536a86ed912a6e84061
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/766600
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    
    Bug: chromium:1489392
    Change-Id: I1aa2051a0a6cb77ba7cefa61664340ac6cb8f925
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/767656
    Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    Reviewed-by: Arman Uguray <armansito@google.com>

---
+++ tests/graphite/DrawPassTest.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "tests/Test.h"
+
+#include "src/gpu/graphite/ContextPriv.h"
+#include "src/gpu/graphite/DrawList.h"
+#include "src/gpu/graphite/DrawPass.h"
+#include "src/gpu/graphite/RecorderPriv.h"
+#include "src/gpu/graphite/RendererProvider.h"
+
+namespace skgpu::graphite {
+
+// Tests that creating a draw pass that fails a dst copy doesn't result in a segfault.
+DEF_GRAPHITE_TEST_FOR_ALL_CONTEXTS(DrawPassTestFailedDstCopy,
+                                   reporter,
+                                   context,
+                                   CtsEnforcement::kNextRelease) {
+    const Caps* caps = context->priv().caps();
+    std::unique_ptr<Recorder> recorder = context->makeRecorder();
+
+    // Define a paint that requires a dst copy.
+    SkPaint paint;
+    PaintParams paintParams{paint, nullptr, DstReadRequirement::kTextureCopy, false};
+
+    // Define a draw that uses the paint, but is larger than the max texture size. In this case the
+    // dst copy will fail.
+    const SkIRect drawSize = SkIRect::MakeWH(caps->maxTextureSize() + 1, 1);
+    std::unique_ptr<DrawList> drawList = std::make_unique<DrawList>();
+    drawList->recordDraw(recorder->priv().rendererProvider()->analyticRRect(),
+                         Transform::Identity(),
+                         Geometry(Shape(SkRect::Make(drawSize))),
+                         Clip(Rect::Infinite(), Rect::Infinite(), drawSize),
+                         DrawOrder(DrawOrder::kClearDepth.next()),
+                         &paintParams,
+                         nullptr);
+
+    // Attempt to make a draw pass with the draw.
+    static constexpr SkISize targetSize = SkISize::Make(1, 1);
+    static constexpr SkColorType targetColorType = kN32_SkColorType;
+    static constexpr SkAlphaType targetAlphaType = kPremul_SkAlphaType;
+    const SkImageInfo targetInfo = SkImageInfo::Make(targetSize, targetColorType, targetAlphaType);
+    sk_sp<TextureProxy> target = TextureProxy::Make(
+            caps,
+            targetSize,
+            caps->getDefaultSampledTextureInfo(
+                    targetColorType, Mipmapped::kNo, Protected::kNo, Renderable::kYes),
+            Budgeted::kNo);
+    std::unique_ptr<DrawPass> drawPass = DrawPass::Make(recorder.get(),
+                                                        std::move(drawList),
+                                                        target,
+                                                        targetInfo,
+                                                        {LoadOp::kClear, StoreOp::kStore},
+                                                        {0.0f, 0.0f, 0.0f, 0.0f});
+
+    // Make sure creating the draw pass failed.
+    REPORTER_ASSERT(reporter, !drawPass);
+}
+
+}  // namespace skgpu::graphite
