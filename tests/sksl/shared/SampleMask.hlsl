502277be15cf43581797de922df9e7e0cdb9239f
Brian Osman <brianosman@google.com>
2023-10-24 15:32:42 -0400

    Update test expectations after SPIRV-Cross change
    A recent roll altered the declaration of gl_SampleMask
    
    https://chromium.googlesource.com/external/github.com/KhronosGroup/SPIRV-Cross/+/5d7ee6ad11121ab79bc16075e070b6b1fd81b828
    
    Change-Id: I0864b2bdd82468d4909431c693da69b37a2cf568
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/770596
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- tests/sksl/shared/SampleMask.hlsl
+++ tests/sksl/shared/SampleMask.hlsl
@@ -1,5 +1,5 @@
-static int gl_SampleMaskIn;
-static int gl_SampleMask;
+static uint gl_SampleMaskIn[1];
+static uint gl_SampleMask[1];
 static float4 sk_FragColor;
 
 struct SPIRV_Cross_Input
@@ -15,33 +15,33 @@ struct SPIRV_Cross_Output
 
 void clear_samplemask_v()
 {
-    gl_SampleMask = int(0u);
+    gl_SampleMask[0] = 0u;
 }
 
 void reset_samplemask_v()
 {
-    gl_SampleMask = int(uint(gl_SampleMaskIn));
+    gl_SampleMask[0] = uint(gl_SampleMaskIn[0]);
 }
 
 float4 samplemaskin_as_color_h4()
 {
-    return float(uint(gl_SampleMaskIn)).xxxx;
+    return float(uint(gl_SampleMaskIn[0])).xxxx;
 }
 
 void frag_main()
 {
     clear_samplemask_v();
     reset_samplemask_v();
-    gl_SampleMask = int(4294967295u);
+    gl_SampleMask[0] = 4294967295u;
     sk_FragColor = samplemaskin_as_color_h4() * 0.00390625f;
 }
 
 SPIRV_Cross_Output main(SPIRV_Cross_Input stage_input)
 {
-    gl_SampleMaskIn = stage_input.gl_SampleMaskIn;
+    gl_SampleMaskIn[0] = stage_input.gl_SampleMaskIn;
     frag_main();
     SPIRV_Cross_Output stage_output;
-    stage_output.gl_SampleMask = gl_SampleMask;
+    stage_output.gl_SampleMask = gl_SampleMask[0];
     stage_output.sk_FragColor = sk_FragColor;
     return stage_output;
 }
