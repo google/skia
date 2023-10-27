7a6a1ddea1a7ce9654ea16d47391a3378ef55609
Nicolette Prevost <nicolettep@google.com>
2023-10-27 09:46:17 -0400

    Add VK_NV_present_barrier to list of unsupported debug layer extensions
    * We get a lot of validation warnings about how the VK_NV_present_barrier extension is not supported by the validation layer. This CL adds it to a list of unused NVIDIA extensions.
    
    * Also alphabetized the list just because
    
    Change-Id: I7c9e5fbef7e939fc7b1283a41a29514e56dfd4a5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/771677
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Nicolette Prevost <nicolettep@google.com>

--- tools/gpu/vk/VkTestUtils.cpp
+++ tools/gpu/vk/VkTestUtils.cpp
@@ -773,10 +773,11 @@ bool CreateVkBackendContext(PFN_vkGetInstanceProcAddr getInstProc,
 
             // This is an nvidia extension that isn't supported by the debug layers so we get lots
             // of warnings. We don't actually use it, so it is easiest to just not enable it.
-            if (0 == strcmp(deviceExtensions[i].extensionName, "VK_NV_low_latency") ||
+            if (0 == strcmp(deviceExtensions[i].extensionName, "VK_EXT_provoking_vertex") ||
                 0 == strcmp(deviceExtensions[i].extensionName, "VK_NV_acquire_winrt_display") ||
                 0 == strcmp(deviceExtensions[i].extensionName, "VK_NV_cuda_kernel_launch") ||
-                0 == strcmp(deviceExtensions[i].extensionName, "VK_EXT_provoking_vertex")) {
+                0 == strcmp(deviceExtensions[i].extensionName, "VK_NV_low_latency") ||
+                0 == strcmp(deviceExtensions[i].extensionName, "VK_NV_present_barrier")) {
                 continue;
             }
 
