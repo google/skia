0d9175bb5cfa403d00b6733a5035a7eda856f971
Kevin Lubick <kjlubick@google.com>
2023-11-14 15:03:17 -0500

    Remove dependency from TestFontCollection to default SkFontMgr
    Bug: b/305780908
    Change-Id: Id9f0f3e31ceae2391d3a15e6f5a77a13b224e547
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/778519
    Reviewed-by: Julia Lavrova <jlavrova@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- modules/skparagraph/utils/TestFontCollection.cpp
+++ modules/skparagraph/utils/TestFontCollection.cpp
@@ -1,9 +1,22 @@
 // Copyright 2019 Google LLC.
-#include "modules/skparagraph/src/ParagraphImpl.h"
 #include "modules/skparagraph/utils/TestFontCollection.h"
+
+#include "include/core/SkStream.h"
+#include "modules/skparagraph/src/ParagraphImpl.h"
 #include "src/base/SkUTF.h"
 #include "src/core/SkOSFile.h"
 #include "tools/Resources.h"
+
+#if defined(SK_TYPEFACE_FACTORY_FREETYPE)
+#include "src/ports/SkFontHost_FreeType_common.h"
+#elif defined(SK_TYPEFACE_FACTORY_CORETEXT)
+#include "src/ports/SkTypeface_mac_ct.h"
+#elif defined(SK_TYPEFACE_FACTORY_DIRECTWRITE)
+#include "src/ports/SkTypeface_win_dw.h"
+#endif
+
+#include <memory>
+#include <utility>
 
 namespace skia {
 namespace textlayout {
@@ -40,14 +53,24 @@ bool TestFontCollection::addFontFromFile(const std::string& path, const std::str
     SkString file_path;
     file_path.printf("%s/%s", fResourceDir.c_str(), path.c_str());
 
-    auto data = SkData::MakeFromFileName(file_path.c_str());
-    if (!data) {
+    std::unique_ptr<SkStreamAsset> file = SkFILEStream::Make(file_path.c_str());
+    if (!file) {
         return false;
     }
+#if defined(SK_TYPEFACE_FACTORY_FREETYPE)
+    sk_sp<SkTypeface> face =
+            SkTypeface_FreeType::MakeFromStream(std::move(file), SkFontArguments());
+#elif defined(SK_TYPEFACE_FACTORY_CORETEXT)
+    sk_sp<SkTypeface> face = SkTypeface_Mac::MakeFromStream(std::move(file), SkFontArguments());
+#elif defined(SK_TYPEFACE_FACTORY_DIRECTWRITE)
+    sk_sp<SkTypeface> face = DWriteFontTypeface::MakeFromStream(std::move(file), SkFontArguments());
+#else
+    sk_sp<SkTypeface> face = nullptr;
+#endif
     if (familyName.empty()) {
-        fFontProvider->registerTypeface(SkTypeface::MakeFromData(data));
+        fFontProvider->registerTypeface(std::move(face));
     } else {
-        fFontProvider->registerTypeface(SkTypeface::MakeFromData(data), SkString(familyName.c_str()));
+        fFontProvider->registerTypeface(std::move(face), SkString(familyName.c_str()));
     }
 
     return true;
--- modules/skparagraph/utils/TestFontCollection.h
+++ modules/skparagraph/utils/TestFontCollection.h
@@ -3,10 +3,14 @@
 #include "modules/skparagraph/include/TypefaceFontProvider.h"
 #include "src/core/SkFontDescriptor.h"
 
+#include <string>
+
 namespace skia {
 namespace textlayout {
 class TestFontCollection : public FontCollection {
 public:
+    // if load is true, will load the fonts (using Freetype, Core Text, or DirectWrite) from
+    // resourceDir.
     TestFontCollection(const std::string& resourceDir, bool testOnly = false, bool loadFonts = true);
 
     size_t fontsFound() const { return fFontsFound; }
