google/skia

commit 05bab9a29d621cb155c4fba80d7e8bb50927bdd7

[svgcanvas] Don't emit default fill and stroke attributes
Default fill: "black".

Default stroke: "none".

Change-Id: I8069f8c4a852cf43e281b6fdcdc58062637c984b
Bug: skia:9344
Reviewed-on: https://skia-review.googlesource.com/c/skia/+/235829
Reviewed-by: Mike Reed <reed@google.com>
Commit-Queue: Florin Malita <fmalita@chromium.org>

src/svg/SkSVGDevice.cpp
@@ -298,10 +298,13 @@ private:
 void SkSVGDevice::AutoElement::addPaint(const SkPaint& paint, const Resources& resources) {
     SkPaint::Style style = paint.getStyle();
     if (style == SkPaint::kFill_Style || style == SkPaint::kStrokeAndFill_Style) {
-        this->addAttribute("fill", resources.fPaintServer);
+        static constexpr char kDefaultFill[] = "rgb(0,0,0)";
+        if (!resources.fPaintServer.equals(kDefaultFill)) {
+            this->addAttribute("fill", resources.fPaintServer);
 
-        if (SK_AlphaOPAQUE != SkColorGetA(paint.getColor())) {
-            this->addAttribute("fill-opacity", svg_opacity(paint.getColor()));
+            if (SK_AlphaOPAQUE != SkColorGetA(paint.getColor())) {
+                this->addAttribute("fill-opacity", svg_opacity(paint.getColor()));
+            }
         }
     } else {
         SkASSERT(style == SkPaint::kStroke_Style);
@@ -340,7 +343,7 @@ void SkSVGDevice::AutoElement::addPaint(const SkPaint& paint, const Resources& r
         }
     } else {
         SkASSERT(style == SkPaint::kFill_Style);
-        this->addAttribute("stroke", "none");
+        // SVG default stroke value is "none".
     }
 }
 

tests/SVGDeviceTest.cpp
@@ -412,4 +412,44 @@ DEF_TEST(SVGDevice_textpath, reporter) {
     REPORTER_ASSERT(reporter, pathElement, "path element not found");
 }
 
+DEF_TEST(SVGDevice_fill_stroke, reporter) {
+    struct {
+        SkColor        color;
+        SkPaint::Style style;
+        const char*    expected_fill;
+        const char*    expected_stroke;
+    } gTests[] = {
+        { SK_ColorBLACK, SkPaint::kFill_Style  , nullptr       , nullptr        },
+        { SK_ColorBLACK, SkPaint::kStroke_Style, "none"        , "rgb(0,0,0)"   },
+        { SK_ColorRED  , SkPaint::kFill_Style  , "rgb(255,0,0)", nullptr        },
+        { SK_ColorRED  , SkPaint::kStroke_Style, "none"        , "rgb(255,0,0)" },
+    };
+
+    for (const auto& tst : gTests) {
+        SkPaint p;
+        p.setColor(tst.color);
+        p.setStyle(tst.style);
+
+        SkDOM dom;
+        {
+            MakeDOMCanvas(&dom)->drawRect(SkRect::MakeWH(100, 100), p);
+        }
+
+        const auto* root = dom.finishParsing();
+        REPORTER_ASSERT(reporter, root, "root element not found");
+        const auto* rect = dom.getFirstChild(root, "rect");
+        REPORTER_ASSERT(reporter, rect, "rect element not found");
+        const auto* fill = dom.findAttr(rect, "fill");
+        REPORTER_ASSERT(reporter, !!fill == !!tst.expected_fill);
+        if (fill) {
+            REPORTER_ASSERT(reporter, strcmp(fill, tst.expected_fill) == 0);
+        }
+        const auto* stroke = dom.findAttr(rect, "stroke");
+        REPORTER_ASSERT(reporter, !!stroke == !!tst.expected_stroke);
+        if (stroke) {
+            REPORTER_ASSERT(reporter, strcmp(stroke, tst.expected_stroke) == 0);
+        }
+    }
+}
+
 #endif

commit 4c964f8c4738a0e38c0fe15bb3dbc5c2849236c2

SkSVGDevice: don't skip fill-opacity when fill is black
Commit 05bab9a29d621cb155c4fba80d7e8bb50927bdd7 introduced a regression
that caused the fill-opacity attribute to not be omitted when the
svg paths have a black (default) fill.

The two attributes are orthogonal and the fact that the fill is black
should not cause the fill-opacity attribute to not be emitted.
This is the case when we draw a black shape that is semi (or fully)
transparent.

Bug: b/309068101
Change-Id: Ic0bef56404d5369f486552da03b82348037caf5f
Signed-off-by: Joey Bevilacqua <bevilacquajoey@gmail.com>
Reviewed-on: https://skia-review.googlesource.com/c/skia/+/780277
Reviewed-by: Florin Malita <fmalita@google.com>
Commit-Queue: Florin Malita <fmalita@google.com>

AUTHORS
@@ -47,6 +47,7 @@ Jacek Caban <cjacek@gmail.com>
 James Clarke <james@clarkezone.io>
 Jeff Muizelaar <jmuizelaar@mozilla.com>
 JetBrains <*@jetbrains.com>
+Joey Bevilacqua <bevilacquajoey@gmail.com>
 Jongdeok Kim <jongdeok.kim@navercorp.com>
 Jordan Williams <jordan@jwillikers.com>
 Jose Mayol <jei.mayol@gmail.com>

src/svg/SkSVGDevice.cpp
@@ -369,10 +369,9 @@ void SkSVGDevice::AutoElement::addPaint(const SkPaint& paint, const Resources& r
         static constexpr char kDefaultFill[] = "black";
         if (!resources.fPaintServer.equals(kDefaultFill)) {
             this->addAttribute("fill", resources.fPaintServer);
-
-            if (SK_AlphaOPAQUE != SkColorGetA(paint.getColor())) {
-                this->addAttribute("fill-opacity", svg_opacity(paint.getColor()));
-            }
+        }
+        if (SK_AlphaOPAQUE != SkColorGetA(paint.getColor())) {
+            this->addAttribute("fill-opacity", svg_opacity(paint.getColor()));
         }
     } else {
         SkASSERT(style == SkPaint::kStroke_Style);

tests/SVGDeviceTest.cpp
@@ -459,6 +459,42 @@ DEF_TEST(SVGDevice_fill_stroke, reporter) {
     }
 }
 
+DEF_TEST(SVGDevice_fill_opacity_black_fill, reporter) {
+    struct {
+        SkColor     color;
+        const char* expected_fill_opacity;
+    } gTests[] = {
+        // Semi-transparent black
+        {  SkColorSetARGB(0x33, 0x00, 0x00, 0x00), "0.2" },
+        // Opaque black
+        {  SkColorSetARGB(0xFF, 0x00, 0x00, 0x00), nullptr },
+    };
+
+    for (const auto& tst : gTests) {
+        SkPaint p;
+        p.setColor(tst.color);
+        p.setStyle(SkPaint::kFill_Style);
+
+        SkDOM dom;
+        {
+            auto svgCanvas = MakeDOMCanvas(&dom);
+            SkRect bounds{0, 0, SkIntToScalar(100), SkIntToScalar(100)};
+            svgCanvas->drawRect(bounds, p);
+        }
+
+        const SkDOM::Node* rootElement = dom.finishParsing();
+        ABORT_TEST(reporter, !rootElement, "root element not found");
+
+        const SkDOM::Node* rectElement = dom.getFirstChild(rootElement, "rect");
+        ABORT_TEST(reporter, !rectElement, "rect element not found");
+        const auto* fill_opacity = dom.findAttr(rectElement, "fill-opacity");
+        REPORTER_ASSERT(reporter, !!fill_opacity == !!tst.expected_fill_opacity);
+        if (fill_opacity) {
+            REPORTER_ASSERT(reporter, strcmp(fill_opacity, tst.expected_fill_opacity) == 0);
+        }
+    }
+}
+
 DEF_TEST(SVGDevice_fill_rect_hex, reporter) {
     SkDOM dom;
     SkPaint paint;