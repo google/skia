6e442416432609be9d1ba8a310d90c16ef9fed7f
Kevin Lubick <kjlubick@google.com>
2023-11-30 15:18:08 -0500

    [pdf] Test yuv and cmyk images with ICC profiles
    Change-Id: I36923827ed4fef8adea69b7b0227ea18b2abbd40
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/784983
    Reviewed-by: Brian Osman <brianosman@google.com>

--- tests/PDFJpegEmbedTest.cpp
+++ tests/PDFJpegEmbedTest.cpp
@@ -61,20 +61,34 @@ DEF_TEST(SkPDF_JpegEmbedTest, r) {
     const char test[] = "SkPDF_JpegEmbedTest";
     sk_sp<SkData> mandrillData(load_resource(r, test, "images/mandrill_512_q075.jpg"));
     sk_sp<SkData> cmykData(load_resource(r, test, "images/CMYK.jpg"));
-    if (!mandrillData || !cmykData) {
+    sk_sp<SkData> yuvICCData(load_resource(r, test, "images/crbug999986.jpeg"));
+    sk_sp<SkData> cmykICCData(load_resource(r, test, "images/crbug1465627.jpeg"));
+
+    if (!mandrillData || !cmykData || !yuvICCData || !cmykICCData) {
+        REPORTER_ASSERT(r, false, "Could not load all images");
         return;
     }
     ////////////////////////////////////////////////////////////////////////////
     SkDynamicMemoryWStream pdf;
     auto document = SkPDF::MakeDocument(&pdf);
-    SkCanvas* canvas = document->beginPage(642, 1028);
+    SkCanvas* canvas = document->beginPage(642, 2048);
 
     canvas->clear(SK_ColorLTGRAY);
 
     sk_sp<SkImage> im1(SkImages::DeferredFromEncodedData(mandrillData));
-    canvas->drawImage(im1.get(), 65.0, 0.0);
+    canvas->drawImage(im1, 65.0, 0.0);
     sk_sp<SkImage> im2(SkImages::DeferredFromEncodedData(cmykData));
-    canvas->drawImage(im2.get(), 0.0, 512.0);
+    canvas->drawImage(im2, 0.0, 512.0);
+
+    // This should be the same blue as the dark spot seen in the image.
+    SkPaint bluePaint;
+    bluePaint.setColor(SkColorSetARGB(255, 0, 59, 103));
+    canvas->drawRect(SkRect::MakeXYWH(0, 1000, 642, 24), bluePaint);
+    sk_sp<SkImage> im3(SkImages::DeferredFromEncodedData(yuvICCData));
+    canvas->drawImage(im3, 0.0, 1024.0);
+
+    sk_sp<SkImage> im4(SkImages::DeferredFromEncodedData(cmykICCData));
+    canvas->drawImage(im4, 0.0, 1536.0);
 
     document->endPage();
     document->close();
@@ -88,6 +102,13 @@ DEF_TEST(SkPDF_JpegEmbedTest, r) {
     // This JPEG uses a nonstandard colorspace - it can not be
     // embedded into the PDF directly.
     REPORTER_ASSERT(r, !is_subset_of(cmykData.get(), pdfData.get()));
+
+    if ((false)) {
+        SkFILEWStream s("/tmp/jpegembed.pdf");
+        REPORTER_ASSERT(r, s.write(pdfData->data(), pdfData->size()));
+        s.flush();
+        s.fsync();
+    }
 }
 
 #ifdef SK_SUPPORT_PDF
