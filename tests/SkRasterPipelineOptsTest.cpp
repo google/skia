c0a39cf4df66fa17e92a31f92fb14ffcd68ee52f
Brian Osman <brianosman@google.com>
2023-12-15 17:24:54 -0500

    Add GCC support to Raster Pipeline
    The majority of changes come from two basic facts:
    
    1) GCC does not allow implicit conversion of scalars to vectors. To deal
       with this, I added promotion helper functions. These are zero-cost on
       clang. For a few functions that are called *often* with scalars, I
       added overloads to limit the noise later in the file.
    
    2) GCC does not allow implicit conversion between integral vector types
       and the __mXXXi types that appear in intrinsic signatures. (It does
       allow float vectors to implicitly convert to __mXXX.) Therefore, all
       of the integer intrinsic arguments and return values need explicit
       casts. (Now that we've turned off lax-vector-conversions in clang,
       most of these were already fixed, but a few more are caught by GCC).
    
    I've monitored code-gen before and after this change on both compilers.
    clang had some trivial code motion (reordering of instructions within a
    few stage functions), but no new overhead at all. GCC only had scalar RP
    previously, and how has a vectorized implementation that's comparable to
    clang's (many functions appear slightly worse, but not dramatically).
    
    Bug: b/316617002
    Bug: b/318414038
    Change-Id: I9c88acbf8ecf0adfcb1225c81b9e8ec3644620c2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/790319
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- tests/SkRasterPipelineOptsTest.cpp
+++ tests/SkRasterPipelineOptsTest.cpp
@@ -31,6 +31,10 @@
 #pragma GCC diagnostic pop
 #endif
 
+constexpr auto F_ = SK_OPTS_NS::F_;
+using F = SK_OPTS_NS::F;
+using I32 = SK_OPTS_NS::I32;
+
 template <size_t N>
 static std::array<int32_t, N> make_masks(int bits) {
     // Make an array of masks that correspond to the bit pattern of `bits`.
@@ -44,7 +48,6 @@ static std::array<int32_t, N> make_masks(int bits) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Any, r) {
-    using I32 = SK_OPTS_NS::I32;
     static constexpr size_t N = sizeof(I32) / sizeof(int32_t);
 
     for (int value = 0; value < (1 << N); ++value) {
@@ -59,7 +62,6 @@ DEF_TEST(SkRasterPipelineOpts_Any, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_All, r) {
-    using I32 = SK_OPTS_NS::I32;
     static constexpr size_t N = sizeof(I32) / sizeof(int32_t);
 
     for (int value = 0; value < (1 << N); ++value) {
@@ -74,13 +76,11 @@ DEF_TEST(SkRasterPipelineOpts_All, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Sin, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float Pi = SK_ScalarPI;
     constexpr float kTolerance = 0.000875f;
     for (float rad = -5*Pi; rad <= 5*Pi; rad += 0.1f) {
-        F result = SK_OPTS_NS::sin_(rad);
-        F expected = sk_float_sin(rad);
+        F result = SK_OPTS_NS::sin_(F_(rad));
+        F expected = F_(sk_float_sin(rad));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -88,13 +88,11 @@ DEF_TEST(SkRasterPipelineOpts_Sin, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Cos, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float Pi = SK_ScalarPI;
     constexpr float kTolerance = 0.000875f;
     for (float rad = -5*Pi; rad <= 5*Pi; rad += 0.1f) {
-        F result = SK_OPTS_NS::cos_(rad);
-        F expected = sk_float_cos(rad);
+        F result = SK_OPTS_NS::cos_(F_(rad));
+        F expected = F_(sk_float_cos(rad));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -102,8 +100,6 @@ DEF_TEST(SkRasterPipelineOpts_Cos, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Tan, r) {
-    using F = SK_OPTS_NS::F;
-
     // Our tangent diverges more as we get near infinities (x near +- Pi/2),
     // so we bring in the domain a little.
     constexpr float Pi = SK_ScalarPI;
@@ -113,8 +109,8 @@ DEF_TEST(SkRasterPipelineOpts_Tan, r) {
     // Test against various multiples of Pi, to check our periodicity
     for (float period : {0.0f, -3*Pi, 3*Pi}) {
         for (float rad = -Pi/2 + kEpsilon; rad <= Pi/2 - kEpsilon; rad += 0.01f) {
-            F result = SK_OPTS_NS::tan_(rad + period);
-            F expected = sk_float_tan(rad);
+            F result = SK_OPTS_NS::tan_(F_(rad + period));
+            F expected = F_(sk_float_tan(rad));
             F delta = SK_OPTS_NS::abs_(expected - result);
 
             REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -123,12 +119,10 @@ DEF_TEST(SkRasterPipelineOpts_Tan, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Asin, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.00175f;
     for (float x = -1; x <= 1; x += 1.0f/64) {
-        F result = SK_OPTS_NS::asin_(x);
-        F expected = asinf(x);
+        F result = SK_OPTS_NS::asin_(F_(x));
+        F expected = F_(asinf(x));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -136,12 +130,10 @@ DEF_TEST(SkRasterPipelineOpts_Asin, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Acos, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.00175f;
     for (float x = -1; x <= 1; x += 1.0f/64) {
-        F result = SK_OPTS_NS::acos_(x);
-        F expected = acosf(x);
+        F result = SK_OPTS_NS::acos_(F_(x));
+        F expected = F_(acosf(x));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -149,12 +141,10 @@ DEF_TEST(SkRasterPipelineOpts_Acos, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Atan, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.00175f;
     for (float x = -10.0f; x <= 10.0f; x += 0.1f) {
-        F result = SK_OPTS_NS::atan_(x);
-        F expected = atanf(x);
+        F result = SK_OPTS_NS::atan_(F_(x));
+        F expected = F_(atanf(x));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -162,13 +152,11 @@ DEF_TEST(SkRasterPipelineOpts_Atan, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Atan2, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.00175f;
     for (float y = -3.0f; y <= 3.0f; y += 0.1f) {
         for (float x = -3.0f; x <= 3.0f; x += 0.1f) {
-            F result = SK_OPTS_NS::atan2_(y, x);
-            F expected = sk_float_atan2(y, x);
+            F result = SK_OPTS_NS::atan2_(F_(y), F_(x));
+            F expected = F_(sk_float_atan2(y, x));
             F delta = SK_OPTS_NS::abs_(expected - result);
 
             REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -177,12 +165,10 @@ DEF_TEST(SkRasterPipelineOpts_Atan2, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Log2, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.001f;
     for (float value : {0.25f, 0.5f, 1.0f, 2.0f, 4.0f, 8.0f}) {
-        F result = SK_OPTS_NS::approx_log2(value);
-        F expected = std::log2(value);
+        F result = SK_OPTS_NS::approx_log2(F_(value));
+        F expected = F_(std::log2(value));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
@@ -190,17 +176,15 @@ DEF_TEST(SkRasterPipelineOpts_Log2, r) {
 }
 
 DEF_TEST(SkRasterPipelineOpts_Pow2, r) {
-    using F = SK_OPTS_NS::F;
-
     constexpr float kTolerance = 0.001f;
     for (float value : {-80, -5, -2, -1, 0, 1, 2, 3, 5}) {
-        F result = SK_OPTS_NS::approx_pow2(value);
-        F expected = std::pow(2.0, value);
+        F result = SK_OPTS_NS::approx_pow2(F_(value));
+        F expected = F_(std::pow(2.0, value));
         F delta = SK_OPTS_NS::abs_(expected - result);
 
         REPORTER_ASSERT(r, SK_OPTS_NS::all(delta < kTolerance));
     }
 
-    F result = SK_OPTS_NS::approx_pow2(160);
+    F result = SK_OPTS_NS::approx_pow2(F_(160));
     REPORTER_ASSERT(r, SK_OPTS_NS::all(result == INFINITY));
 }
