3807f5569d4ddcd2b7e995e9d0e0a45b9711d4e7
Herb Derby <herb@google.com>
2023-12-19 09:03:43 -0500

    Compare two segments at a given y
    Compare if Segment s0 < Segment s1 at a given y taking
    into account slope.
    
    The paper is here:
    https://drive.google.com/file/d/1lYw5m8ryC5C1DiG1NIHOV5tp4U4GdgH2/view?usp=drive_link
    
    To see how this change fits in to future CLs consult:
    https://skia-review.googlesource.com/c/skia/+/784551
    
    Bug: b/297220808
    Change-Id: If6eeb507660ad6f77c524bcfae7413f672c9aba8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/791736
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- modules/bentleyottmann/tests/MyersTest.cpp
+++ modules/bentleyottmann/tests/MyersTest.cpp
@@ -8,6 +8,7 @@
 namespace myers {
 extern bool slope_s0_less_than_slope_s1(const Segment& s0, const Segment& s1);
 extern bool segment_less_than_upper_to_insert(const Segment& segment, const Segment& to_insert);
+extern bool s0_less_than_s1_at_y(const Segment& s0, const Segment& s1, int32_t y);
 }  // namespace myers
 
 using namespace myers;
@@ -97,4 +98,29 @@ DEF_TEST(MFC_segment_less_than_upper_to_insert, r) {
     // The insertion point is between s1 and s0.
     REPORTER_ASSERT(r, *insertionPoint == s0);
     REPORTER_ASSERT(r, *(insertionPoint-1) == s1);
+}
+
+DEF_TEST(MFC_less_than_at_y, r) {
+    {
+        Segment s0 = {{0, 0}, {2, 2}},
+                s1 = {{0, 0}, {-2, 2}};
+        REPORTER_ASSERT(r, !s0_less_than_s1_at_y(s0, s1, 1));
+        REPORTER_ASSERT(r, s0_less_than_s1_at_y(s1, s0, 1));
+    }
+    {  // cross at 0 use slope to break tie.
+        Segment s0 = {{-2, -2}, {2, 2}},
+                s1 = {{2, -2}, {-2, 2}};
+        REPORTER_ASSERT(r, s0_less_than_s1_at_y(s0, s1, -1));
+        REPORTER_ASSERT(r, !s0_less_than_s1_at_y(s1, s0, -1));
+        REPORTER_ASSERT(r, !s0_less_than_s1_at_y(s0, s1, 0));
+        REPORTER_ASSERT(r, s0_less_than_s1_at_y(s1, s0, 0));
+        REPORTER_ASSERT(r, !s0_less_than_s1_at_y(s0, s1, 1));
+        REPORTER_ASSERT(r, s0_less_than_s1_at_y(s1, s0, 1));
+    }
+    {
+        Segment s0 = {{-2, -100}, {-2, 89}},
+                s1 = {{6, -70}, {-2, 72}};
+
+        REPORTER_ASSERT(r, !s0_less_than_s1_at_y(s0, s1, 72));
+    }
 }
