be4814b7c3e7805e5d9be646a94c76428172ab10
Jim Van Verth <jvanverth@google.com>
2023-12-19 18:03:52 +0000

    Revert "[Fontations-backend] Use new outline API"
    This reverts commit 95da971fcf9fe2639fc700a709ad0eef37fa62a1.
    
    Reason for revert: Looks like this is breaking the Android build in the Chrome roller.
    
    Original change's description:
    > [Fontations-backend] Use new outline API
    >
    > Use new outline API, needed for holding hinter state in the future. See
    > [1] for details. Old API will be removed. Fetch patch version update
    > for Fontations in the process, needed for making this change.
    >
    > [1] https://github.com/googlefonts/fontations/pull/727
    >
    > Bug: b/40045335
    > Cq-Include-Trybots: luci.skia.skia.primary:Build-Debian10-Clang-x86_64-Debug-Fontations,Build-Mac-Clang-x86_64-Debug-Fontations,Test-Debian10-Clang-GCE-CPU-AVX2-x86_64-Debug-All-Fontations,Test-Mac12-Clang-MacBookPro16.2-CPU-AppleIntel-x86_64-Debug-All-Fontations
    > Change-Id: Ie008afa18ab4d1b88934ac77ce32a9c571c748c5
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/791156
    > Reviewed-by: Ben Wagner <bungeman@google.com>
    > Commit-Queue: Dominik Röttsches <drott@google.com>
    > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    
    Bug: b/40045335
    Change-Id: Ia732926c6b2755149081caed0e5b22127516e9a0
    Cq-Include-Trybots: luci.skia.skia.primary:Build-Debian10-Clang-x86_64-Debug-Fontations,Build-Mac-Clang-x86_64-Debug-Fontations,Test-Debian10-Clang-GCE-CPU-AVX2-x86_64-Debug-All-Fontations,Test-Mac12-Clang-MacBookPro16.2-CPU-AppleIntel-x86_64-Debug-All-Fontations
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/792096
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- src/ports/fontations/src/ffi.rs
+++ src/ports/fontations/src/ffi.rs
@@ -7,7 +7,7 @@ use read_fonts::{FileRef, FontRef, ReadError, TableProvider};
 use skrifa::{
     instance::{Location, Size},
     metrics::{GlyphMetrics, Metrics},
-    outline::DrawSettings,
+    scale::Context,
     setting::VariationSetting,
     string::{LocalizedStrings, StringId},
     MetadataProvider, Tag,
@@ -71,15 +71,16 @@ fn get_path(
 ) -> bool {
     font_ref
         .with_font(|f| {
-            // TODO(https://issues.skia.org/issues/317020057): Store OutlineGlyphCollection
-            // at ScalerContext or SkTypeface level.
-            let glyph = f.outline_glyphs().get(GlyphId::new(glyph_id))?;
-            let draw_settings = DrawSettings::unhinted(Size::new(size), &coords.normalized_coords);
-
+            let mut cx = Context::new();
+            let mut scaler = cx
+                .new_scaler()
+                .size(Size::new(size))
+                .normalized_coords(coords.normalized_coords.into_iter())
+                .build(f);
             let mut pen_dump = PathWrapperPen {
                 path_wrapper: path_wrapper,
             };
-            match glyph.draw(draw_settings, &mut pen_dump) {
+            match scaler.outline(GlyphId::new(glyph_id), &mut pen_dump) {
                 Err(_) => None,
                 Ok(metrics) => {
                     scaler_metrics.has_overlaps = metrics.has_overlaps;
