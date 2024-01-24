c0544410fc8ef51cd9450fd0e5eab89daeabf2c1
John Stiles <johnstiles@google.com>
2024-01-24 16:53:25 -0500

    Allow Skia to build properly when is_component_build is set.
    Skottie will now be disabled when is_component_build is enabled;
    unfortunately, Skottie uses a lot of non-public API internally,
    and previously would cause link errors during the build:
    https://paste.googleplex.com/6040246309879808
    
    This CL also eliminates the GN argument
    `skia_canvaskit_enable_skottie`. Use `skia_enable_skottie`
    instead.
    
    Change-Id: I87270beb023b5967ea421cd516e1ef8f60470f15
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/805336
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- modules/canvaskit/compile.sh
+++ modules/canvaskit/compile.sh
@@ -237,6 +237,7 @@ echo "Compiling"
   skia_enable_ganesh=${ENABLE_GANESH} \
   skia_enable_graphite=${ENABLE_GRAPHITE} \
   skia_build_for_debugger=${DEBUGGER_ENABLED} \
+  skia_enable_skottie=${ENABLE_SKOTTIE} \
   skia_enable_sksl_tracing=${ENABLE_SKSL_TRACE} \
   \
   ${GN_SHAPER} \
@@ -251,7 +252,6 @@ echo "Compiling"
   skia_canvaskit_profile_build=${PROFILE_BUILD} \
   skia_canvaskit_enable_skp_serialization=${SERIALIZE_SKP} \
   skia_canvaskit_enable_effects_deserialization=${DESERIALIZE_EFFECTS} \
-  skia_canvaskit_enable_skottie=${ENABLE_SKOTTIE} \
   skia_canvaskit_include_viewer=${INCLUDE_VIEWER} \
   skia_canvaskit_enable_pathops=${ENABLE_PATHOPS} \
   skia_canvaskit_enable_matrix_helper=${ENABLE_MATRIX} \
