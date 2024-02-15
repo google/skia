4f12de5f24d5b53e9e58adf6bfb5e84b141b3296
Leandro Lovisolo <lovisolo@google.com>
2024-02-15 22:32:59 +0000

    [bazel] GaneshGLSurfaceManager.cpp: Rewrite makeGLESSurfaceManager() to be more like makeVulkanSurfaceManager().
    In https://skia-review.googlesource.com/c/skia/+/811033 I implemented GaneshVulkanSurfaceManager.cpp, which required defining a makeVulkanSurfaceManager() function. In that CL, I argued that it's better for the make***SurfaceManager() functions to take all surface parameters as inputs, even if some of those parameters are always the same for all configs. The reasoning is that it makes it easier to quickly see what "ingredients" go into making a surface.
    
    This CL does the same for makeGLESSurfaceManager().
    
    Bug: b/40045301
    Change-Id: I33e6e267599302aa9dd98e8bc56b379e3ceaca7a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/811244
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- tools/testrunners/common/surface_manager/GaneshGLSurfaceManager.cpp
+++ tools/testrunners/common/surface_manager/GaneshGLSurfaceManager.cpp
@@ -12,6 +12,7 @@
 #include "include/gpu/GrTypes.h"
 #include "include/gpu/ganesh/SkSurfaceGanesh.h"
 #include "include/private/base/SkAssert.h"
+#include "tools/gpu/BackendSurfaceFactory.h"
 #include "tools/gpu/GrContextFactory.h"
 #include "tools/testrunners/common/surface_manager/SurfaceManager.h"
 
@@ -47,49 +48,122 @@ private:
     sk_sp<SkSurface> fSurface;
 };
 
-std::unique_ptr<SurfaceManager> makeGLESSurfaceManager(std::string config,
-                                                       SurfaceOptions surfaceOptions,
-                                                       GrContextOptions grContextOptions,
-                                                       int sampleCount) {
+enum class SurfaceType { kDefault, kBackendTexture, kBackendRenderTarget };
+
+std::unique_ptr<SurfaceManager> makeGLESSurfaceManager(
+        std::string config,
+        SurfaceOptions surfaceOptions,
+        GrContextOptions grContextOptions,
+        sk_gpu_test::GrContextFactory::ContextOverrides contextOverrides,
+        SkColorInfo colorInfo,
+        SurfaceType surfaceType,
+        uint32_t surfaceFlags,
+        int sampleCount) {
     if (surfaceOptions.modifyGrContextOptions) {
         surfaceOptions.modifyGrContextOptions(&grContextOptions);
     }
+
+    // Based on
+    // https://skia.googlesource.com/skia/+/8da85ea79d1ba2b3f32d25178eb21f2ebda83437/dm/DMSrcSink.cpp#1579.
     auto contextFactory = std::make_unique<sk_gpu_test::GrContextFactory>(grContextOptions);
-    sk_gpu_test::ContextInfo contextInfo = contextFactory.get()->getContextInfo(
-            skgpu::ContextType::kGLES, sk_gpu_test::GrContextFactory::ContextOverrides::kNone);
+    sk_gpu_test::ContextInfo contextInfo =
+            contextFactory.get()->getContextInfo(skgpu::ContextType::kGLES, contextOverrides);
     GrDirectContext* context = contextInfo.directContext();
     SkASSERT_RELEASE(context);
 
-    SkColorInfo colorInfo(kRGBA_8888_SkColorType, kPremul_SkAlphaType, SkColorSpace::MakeSRGB());
-    SkImageInfo info = SkImageInfo::Make({surfaceOptions.width, surfaceOptions.height}, colorInfo);
-    SkSurfaceProps props(/* flags= */ 0, kRGB_H_SkPixelGeometry);
-    sk_sp<SkSurface> surface = SkSurfaces::RenderTarget(
-            context, skgpu::Budgeted::kNo, info, /* sampleCount= */ 1, &props);
+    // Based on
+    // https://skia.googlesource.com/skia/+/8da85ea79d1ba2b3f32d25178eb21f2ebda83437/dm/DMSrcSink.cpp#1524.
+    SkImageInfo imageInfo = SkImageInfo::Make({surfaceOptions.width, surfaceOptions.height}, colorInfo);
+    SkSurfaceProps surfaceProps(surfaceFlags, kRGB_H_SkPixelGeometry);
+    sk_sp<SkSurface> surface;
+    switch (surfaceType) {
+        default:
+        case SurfaceType::kDefault:
+            surface = SkSurfaces::RenderTarget(
+                    context, skgpu::Budgeted::kNo, imageInfo, sampleCount, &surfaceProps);
+            break;
+
+        case SurfaceType::kBackendTexture:
+            surface = sk_gpu_test::MakeBackendTextureSurface(context,
+                                                             imageInfo,
+                                                             kTopLeft_GrSurfaceOrigin,
+                                                             sampleCount,
+                                                             skgpu::Mipmapped::kNo,
+                                                             skgpu::Protected::kNo,
+                                                             &surfaceProps);
+            break;
+
+        case SurfaceType::kBackendRenderTarget:
+            surface = sk_gpu_test::MakeBackendRenderTargetSurface(context,
+                                                                  imageInfo,
+                                                                  kBottomLeft_GrSurfaceOrigin,
+                                                                  sampleCount,
+                                                                  skgpu::Protected::kNo,
+                                                                  &surfaceProps);
+            break;
+    }
     SkASSERT_RELEASE(surface);
 
     return std::make_unique<GaneshGLSurfaceManager>(
             std::move(contextFactory), contextInfo, context, surface, config, colorInfo);
 }
 
+// Based on the configurations defined here[1], the configuration parsing logic here[2], and the
+// sink selection logic here[3].
+//
+// [1]
+// https://skia.googlesource.com/skia/+/8da85ea79d1ba2b3f32d25178eb21f2ebda83437/tools/flags/CommonFlagsConfig.cpp#40
+// [2]
+// https://skia.googlesource.com/skia/+/8da85ea79d1ba2b3f32d25178eb21f2ebda83437/tools/flags/CommonFlagsConfig.cpp#610
+// [3]
+// https://skia.googlesource.com/skia/+/8da85ea79d1ba2b3f32d25178eb21f2ebda83437/dm/DM.cpp#1017
 std::unique_ptr<SurfaceManager> SurfaceManager::FromConfig(std::string config,
                                                            SurfaceOptions surfaceOptions) {
     if (config == "gles") {
         return makeGLESSurfaceManager(
-                config, surfaceOptions, GrContextOptions(), /* sampleCount= */ 1);
+                config,
+                surfaceOptions,
+                GrContextOptions(),
+                sk_gpu_test::GrContextFactory::ContextOverrides::kNone,
+                SkColorInfo(kRGBA_8888_SkColorType, kPremul_SkAlphaType, SkColorSpace::MakeSRGB()),
+                SurfaceType::kDefault,
+                SkSurfaceProps::kDefault_Flag,
+                /* sampleCount= */ 1);
     }
     if (config == "gles_msaa4") {
         return makeGLESSurfaceManager(
-                config, surfaceOptions, GrContextOptions(), /* sampleCount= */ 4);
+                config,
+                surfaceOptions,
+                GrContextOptions(),
+                sk_gpu_test::GrContextFactory::ContextOverrides::kNone,
+                SkColorInfo(kRGBA_8888_SkColorType, kPremul_SkAlphaType, SkColorSpace::MakeSRGB()),
+                SurfaceType::kDefault,
+                SkSurfaceProps::kDefault_Flag,
+                /* sampleCount= */ 4);
     }
     if (config == "gles_msaa8") {
         return makeGLESSurfaceManager(
-                config, surfaceOptions, GrContextOptions(), /* sampleCount= */ 8);
+                config,
+                surfaceOptions,
+                GrContextOptions(),
+                sk_gpu_test::GrContextFactory::ContextOverrides::kNone,
+                SkColorInfo(kRGBA_8888_SkColorType, kPremul_SkAlphaType, SkColorSpace::MakeSRGB()),
+                SurfaceType::kDefault,
+                SkSurfaceProps::kDefault_Flag,
+                /* sampleCount= */ 8);
     }
     if (config == "gles_msaa8_noReduceOpsTaskSplitting") {
         GrContextOptions grContextOptions;
         grContextOptions.fReduceOpsTaskSplitting = GrContextOptions::Enable::kNo;
         return makeGLESSurfaceManager(
-                config, surfaceOptions, grContextOptions, /* sampleCount= */ 8);
+                config,
+                surfaceOptions,
+                grContextOptions,
+                sk_gpu_test::GrContextFactory::ContextOverrides::kNone,
+                SkColorInfo(kRGBA_8888_SkColorType, kPremul_SkAlphaType, SkColorSpace::MakeSRGB()),
+                SurfaceType::kDefault,
+                SkSurfaceProps::kDefault_Flag,
+                /* sampleCount= */ 8);
     }
     return nullptr;
 }
