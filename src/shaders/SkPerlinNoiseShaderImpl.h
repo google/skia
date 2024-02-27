8589595721d09bd54ca694f75263846f400096da
John Stiles <johnstiles@google.com>
2024-02-27 09:41:09 -0500

    Delete legacy shadeSpan implementation of Perlin Noise.
    A CL was set up to rebaseline Chrome at https://crrev.com/c/5323882,
    but it came up with zero image errors.
    
    Bug: b/40045243
    Change-Id: I9c0ae2920ebaf56dcb931a57317ad2de0553d8f0
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/817622
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>

--- src/shaders/SkPerlinNoiseShaderImpl.h
+++ src/shaders/SkPerlinNoiseShaderImpl.h
@@ -9,7 +9,6 @@
 
 #include "include/core/SkAlphaType.h"
 #include "include/core/SkBitmap.h"
-#include "include/core/SkColor.h"
 #include "include/core/SkColorType.h"
 #include "include/core/SkFlattenable.h"
 #include "include/core/SkImageInfo.h"
@@ -20,6 +19,7 @@
 #include "include/core/SkTypes.h"
 #include "include/private/base/SkFloatingPoint.h"
 #include "include/private/base/SkMath.h"
+#include "include/private/base/SkOnce.h"
 #include "src/shaders/SkShaderBase.h"
 
 #include <algorithm>
@@ -27,15 +27,10 @@
 #include <cstring>
 #include <memory>
 
-class SkArenaAlloc;
 class SkReadBuffer;
 enum class SkPerlinNoiseShaderType;
-class SkWriteBuffer;
-
-#ifdef SK_RASTER_PIPELINE_PERLIN_NOISE
-#include "include/private/base/SkOnce.h"
 struct SkStageRec;
-#endif
+class SkWriteBuffer;
 
 class SkPerlinNoiseShader : public SkShaderBase {
 private:
@@ -46,7 +41,7 @@ private:
 
 public:
     struct StitchData {
-        StitchData() : fWidth(0), fWrapX(0), fHeight(0), fWrapY(0) {}
+        StitchData() = default;
 
         StitchData(SkScalar w, SkScalar h)
                 : fWidth(std::min(SkScalarRoundToInt(w), SK_MaxS32 - kPerlinNoise))
@@ -59,10 +54,10 @@ public:
                    fWrapY == other.fWrapY;
         }
 
-        int fWidth;  // How much to subtract to wrap for stitching.
-        int fWrapX;  // Minimum value to wrap.
-        int fHeight;
-        int fWrapY;
+        int fWidth = 0;  // How much to subtract to wrap for stitching.
+        int fWrapX = 0;  // Minimum value to wrap.
+        int fHeight = 0;
+        int fWrapY = 0;
     };
 
     struct PaintingData {
@@ -107,13 +102,11 @@ public:
                 , fNoiseBitmap(that.fNoiseBitmap) {
             memcpy(fLatticeSelector, that.fLatticeSelector, sizeof(fLatticeSelector));
             memcpy(fNoise, that.fNoise, sizeof(fNoise));
-            memcpy(fGradient, that.fGradient, sizeof(fGradient));
         }
 
         int fSeed;
         uint8_t fLatticeSelector[kBlockSize];
         uint16_t fNoise[4][kBlockSize][2];
-        SkPoint fGradient[4][kBlockSize];
         SkISize fTileSize;
         SkVector fBaseFrequency;
         StitchData fStitchDataInit;
@@ -122,7 +115,7 @@ public:
         SkBitmap fPermutationsBitmap;
         SkBitmap fNoiseBitmap;
 
-        inline int random() {
+        int random() {
             // See https://www.w3.org/TR/SVG11/filters.html#feTurbulenceElement
             // m = kRandMaximum, 2**31 - 1 (2147483647)
             static constexpr int kRandAmplitude = 16807;  // 7**5; primitive root of m
@@ -192,15 +185,13 @@ public:
             static constexpr SkScalar kInvBlockSizef = 1.0 / SkIntToScalar(kBlockSize);
             for (int channel = 0; channel < 4; ++channel) {
                 for (int i = 0; i < kBlockSize; ++i) {
-                    fGradient[channel][i] =
+                    SkPoint gradient =
                             SkPoint::Make((fNoise[channel][i][0] - kBlockSize) * kInvBlockSizef,
                                           (fNoise[channel][i][1] - kBlockSize) * kInvBlockSizef);
-                    fGradient[channel][i].normalize();
+                    gradient.normalize();
                     // Put the normalized gradient back into the noise data
-                    fNoise[channel][i][0] =
-                            SkScalarRoundToInt((fGradient[channel][i].fX + 1) * kHalfMax16bits);
-                    fNoise[channel][i][1] =
-                            SkScalarRoundToInt((fGradient[channel][i].fY + 1) * kHalfMax16bits);
+                    fNoise[channel][i][0] = SkScalarRoundToInt((gradient.fX + 1) * kHalfMax16bits);
+                    fNoise[channel][i][1] = SkScalarRoundToInt((gradient.fY + 1) * kHalfMax16bits);
                 }
             }
         }
@@ -265,25 +256,6 @@ public:
 
     ShaderType type() const override { return ShaderType::kPerlinNoise; }
 
-    class PerlinNoiseShaderContext : public Context {
-    public:
-        PerlinNoiseShaderContext(const SkPerlinNoiseShader& shader, const ContextRec&);
-
-        void shadeSpan(int x, int y, SkPMColor[], int count) override;
-
-    private:
-        SkPMColor shade(const SkPoint& point, StitchData& stitchData) const;
-        SkScalar calculateTurbulenceValueForPoint(int channel,
-                                                  StitchData& stitchData,
-                                                  const SkPoint& point) const;
-        SkScalar noise2D(int channel,
-                         const StitchData& stitchData,
-                         const SkPoint& noiseVector) const;
-
-        SkMatrix fMatrix;
-        PaintingData fPaintingData;
-    };
-
     SkPerlinNoiseShaderType noiseType() const { return fType; }
     int numOctaves() const { return fNumOctaves; }
     bool stitchTiles() const { return fStitchTiles; }
@@ -296,15 +268,10 @@ public:
                 fTileSize, fSeed, fBaseFrequencyX, fBaseFrequencyY, mat);
     }
 
-#ifdef SK_RASTER_PIPELINE_PERLIN_NOISE
     bool appendStages(const SkStageRec& rec, const SkShaders::MatrixRec& mRec) const override;
-#endif
 
 protected:
     void flatten(SkWriteBuffer&) const override;
-#ifdef SK_ENABLE_LEGACY_SHADERCONTEXT
-    Context* onMakeContext(const ContextRec&, SkArenaAlloc*) const override;
-#endif
 
 private:
     SK_FLATTENABLE_HOOKS(SkPerlinNoiseShader)
@@ -317,10 +284,8 @@ private:
     const SkISize fTileSize;
     const bool fStitchTiles;
 
-#ifdef SK_RASTER_PIPELINE_PERLIN_NOISE
     mutable SkOnce fInitPaintingDataOnce;
     std::unique_ptr<PaintingData> fPaintingData;
-#endif
 
     friend void SkRegisterPerlinNoiseShaderFlattenable();
 };
