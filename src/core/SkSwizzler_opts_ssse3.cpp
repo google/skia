e3c3a52730f0994f6b3e81d87ad0f0d4aab38bf8
Herb Derby <herb@google.com>
2024-02-28 11:36:11 -0500

    Only define _opts functions when needed
    Add guards to the SSSE3 and the HSW specific files to only generate
    the definitions from the .h when needed.
    
    Change-Id: I5286ba0146c8f917b30af4170487b8aae342154a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/820176
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/core/SkSwizzler_opts_ssse3.cpp
+++ src/core/SkSwizzler_opts_ssse3.cpp
@@ -10,7 +10,9 @@
 #include "src/core/SkOptsTargets.h"
 #include "src/core/SkSwizzlePriv.h"
 
-#if defined(SK_CPU_X86) && !defined(SK_ENABLE_OPTIMIZE_SIZE)
+#if defined(SK_CPU_X86) && \
+    !defined(SK_ENABLE_OPTIMIZE_SIZE) && \
+    SK_CPU_SSE_LEVEL < SK_CPU_SSE_LEVEL_SSSE3
 
 // The order of these includes is important:
 // 1) Select the target CPU architecture by defining SK_OPTS_TARGET and including SkOpts_SetTarget
