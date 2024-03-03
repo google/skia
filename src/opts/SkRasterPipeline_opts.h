10d2d15c0b70cb39d7cfc5ebcfd83ce7bf174d8d
John Stiles <johnstiles@google.com>
2024-03-03 15:41:23 -0500

    Use nmad in pow/log computation.
    These are used in SkSL, and in various skcms transfer functions.
    The code size reduction isn't apparent here because the code size
    bots only build SSE2 (which doesn't have a FMA op) and ARM32.
    Local Haswell and ARM64 builds are several dozen instructions
    smaller with this change.
    
    This change does not seem to cause any ripple effect in Chrome at
    all; we are likely getting identical results since SSE2 does not
    have a fused multiply-subtract op.
    
    Change-Id: I6c4d833571eb0788ccc3f78d2e43b4c46ff903a5
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/820835
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/opts/SkRasterPipeline_opts.h
+++ src/opts/SkRasterPipeline_opts.h
@@ -981,10 +981,7 @@ SI F approx_log2(F x) {
 
     // ... but using the mantissa to refine its error is _much_ better.
     F m = sk_bit_cast<F>((sk_bit_cast<U32>(x) & 0x007fffff) | 0x3f000000);
-    return e
-         - 124.225514990f
-         -   1.498030302f * m
-         -   1.725879990f / (0.3520887068f + m);
+    return nmad(m, 1.498030302f, e - 124.225514990f) - 1.725879990f / (0.3520887068f + m);
 }
 
 SI F approx_log(F x) {
@@ -996,8 +993,7 @@ SI F approx_pow2(F x) {
     constexpr float kInfinityBits = 0x7f800000;
 
     F f = fract(x);
-    F approx = x + 121.274057500f;
-      approx -= f * 1.490129070f;
+    F approx = nmad(f, 1.490129070f, x + 121.274057500f);
       approx += 27.728023300f / (4.84252568f - f);
       approx *= 1.0f * (1<<23);
       approx  = min(max(approx, F0), F_(kInfinityBits));  // guard against underflow/overflow
