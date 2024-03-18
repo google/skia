e8822d85af59532d1a1eed4afb44d0f7c8e24e46
Dominik Röttsches <drott@chromium.org>
2024-03-18 17:29:54 +0200

    [Fontations-backend] Add default axes to filtered collection
    `SkTypeface::getVariationDesignPosition()` expects default axes
    to be present and set to the default value. Prepopulate the argument to
    Skrifa's AxisCollection::filter() with the default axes and their
    values, then let the user specified values override them. This fixes the
    behavior of getVariationDesignPosition() (in particular for its return
    value = number of axes) also for a situation where no axis coordinate
    position was provided in SkFontArguments when creating the SkTypeface().
    
    Test:
    
    Improves test pass rate when running:
        $ dm -m 'TypefaceAxes' --fontations
    
    Bug: skia:328418459
    Change-Id: I067f11c9a7e3cd52cb051a6317f3e053276282e8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/827936
    Reviewed-by: Dominik Röttsches <drott@google.com>
    Commit-Queue: Dominik Röttsches <drott@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

--- src/ports/fontations/src/ffi.rs
+++ src/ports/fontations/src/ffi.rs
@@ -683,8 +683,18 @@ fn resolve_into_normalized_coords(
         .map(|coord| (Tag::from_be_bytes(coord.axis.to_be_bytes()), coord.value));
     let bridge_normalized_coords = font_ref
         .with_font(|f| {
+            let merged_defaults_with_user = f
+                .axes()
+                .iter()
+                .map(|axis| (axis.tag(), axis.default_value()))
+                .chain(design_coords.iter().map(|user_coord| {
+                    (
+                        Tag::from_be_bytes(user_coord.axis.to_be_bytes()),
+                        user_coord.value,
+                    )
+                }));
             Some(BridgeNormalizedCoords {
-                filtered_user_coords: f.axes().filter(variation_tuples.clone()).collect(),
+                filtered_user_coords: f.axes().filter(merged_defaults_with_user).collect(),
                 normalized_coords: f.axes().location(variation_tuples),
             })
         })
