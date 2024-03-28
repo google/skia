e25b0f9006a40cdc044f8a9982a820d80d9f1fcb
Sophie Wen <sophiewen@google.com>
2024-03-28 17:00:27 +0000

    skia: Fix implicit conversion warning
    This fixes an implicit conversion warning that may surface, i.e. in
    https://chromium-review.googlesource.com/c/chromium/src/+/5368879.
    
    Bug: b/326434696
    Change-Id: Id095e107c0a118a51c986fb2a2d9019ee3b89f4b
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/833441
    Reviewed-by: Florin Malita <fmalita@google.com>
    Commit-Queue: Sophie Wen <sophiewen@google.com>

--- include/private/SkEncodedInfo.h
+++ include/private/SkEncodedInfo.h
@@ -15,6 +15,7 @@
 #include "include/core/SkImageInfo.h"
 #include "include/core/SkRefCnt.h"
 #include "include/core/SkTypes.h"
+#include "include/private/base/SkTo.h"
 #include "modules/skcms/skcms.h"
 
 #include <cstdint>
@@ -165,8 +166,13 @@ public:
                 break;
         }
 
-        return SkEncodedInfo(width, height, color, alpha,
-                bitsPerComponent, colorDepth, std::move(profile));
+        return SkEncodedInfo(width,
+                             height,
+                             color,
+                             alpha,
+                             SkToU8(bitsPerComponent),
+                             SkToU8(colorDepth),
+                             std::move(profile));
     }
 
     /*
