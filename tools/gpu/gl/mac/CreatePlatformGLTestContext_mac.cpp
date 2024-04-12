e335a0a11aa08b3926d179e84e26eae229a2e779
K Lubick <kjlubick@google.com>
2024-04-12 11:45:54 -0400

    Remove guards for old versions of mac
    Reading the AvailabilityMacros.h source, I'm not even sure these guards
    were being used correctly.
    
    https://opensource.apple.com/source/xnu/xnu-2050.7.9/EXTERNAL_HEADERS/AvailabilityMacros.h.auto.html
    
    Change-Id: Ic7191a5fb1464f99da9d4f41de746ecff6f4b343
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/839798
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Auto-Submit: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>

--- tools/gpu/gl/mac/CreatePlatformGLTestContext_mac.cpp
+++ tools/gpu/gl/mac/CreatePlatformGLTestContext_mac.cpp
@@ -9,7 +9,6 @@
 
 #include "tools/gpu/gl/GLTestContext.h"
 
-#include <AvailabilityMacros.h>
 #include <OpenGL/OpenGL.h>
 #include <dlfcn.h>
 
@@ -45,36 +44,28 @@ MacGLTestContext::MacGLTestContext(MacGLTestContext* shareContext)
     // If that fails, we try again with only the base parameters.
     CGLPixelFormatAttribute attributes[] = {
         // base parameters
-#if MAC_OS_X_VERSION_10_7
         kCGLPFAOpenGLProfile,
         (CGLPixelFormatAttribute) kCGLOGLPVersion_3_2_Core,
-#endif
         kCGLPFADoubleBuffer,
 
-#if MAC_OS_X_VERSION_10_8
         // eGPU parameters
         kCGLPFAAllowOfflineRenderers,  // Enables e-GPU.
         kCGLPFANoRecovery,  // Disallows software rendering.
         kCGLPFARendererID, (CGLPixelFormatAttribute)kCGLRendererATIRadeonX4000ID, // Select Radeon
-#endif
         (CGLPixelFormatAttribute)NULL
     };
-#if MAC_OS_X_VERSION_10_8
     static const int kFirstEGPUParameter = 3;
     SkASSERT(kCGLPFAAllowOfflineRenderers == attributes[kFirstEGPUParameter]);
-#endif
 
     CGLPixelFormatObj pixFormat;
     GLint npix;
     CGLChoosePixelFormat(attributes, &pixFormat, &npix);
 
-#if MAC_OS_X_VERSION_10_8
     if (nullptr == pixFormat) {
         // Move the NULL-termination up to remove the eGPU parameters and try again
         attributes[kFirstEGPUParameter] = (CGLPixelFormatAttribute)NULL;
         CGLChoosePixelFormat(attributes, &pixFormat, &npix);
     }
-#endif
     if (nullptr == pixFormat) {
         SkDebugf("CGLChoosePixelFormat failed.");
         return;
