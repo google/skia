a9b6705071f55c3859268aa9554325b7e3ff4f2a
Michael Ludwig <michaelludwig@google.com>
2024-04-13 09:02:44 -0400

    [graphite] Add TextureProxyView::replaceSwizzle()
    Also fixes an issue where makeSwizzle() was discarding any non-default
    origin that was set on the view.
    
    Bug: b/333909822
    Change-Id: I0956f47a502a21be2d48d770dca92c6d0fcb0771
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/840276
    Reviewed-by: James Godfrey-Kittle <jamesgk@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- src/gpu/graphite/TextureProxyView.h
+++ src/gpu/graphite/TextureProxyView.h
@@ -64,20 +64,25 @@ public:
     sk_sp<TextureProxy> refProxy() const { return fProxy; }
 
     Swizzle swizzle() const { return fSwizzle; }
+    Origin origin() const { return fOrigin; }
 
     void concatSwizzle(Swizzle swizzle) {
         fSwizzle = skgpu::Swizzle::Concat(fSwizzle, swizzle);
     }
 
+    // makeSwizzle returns a new view with 'swizzle' composed on to this view's existing swizzle
     TextureProxyView makeSwizzle(Swizzle swizzle) const & {
-        return {fProxy, Swizzle::Concat(fSwizzle, swizzle)};
+        return {fProxy, Swizzle::Concat(fSwizzle, swizzle), fOrigin};
     }
 
     TextureProxyView makeSwizzle(Swizzle swizzle) && {
-        return {std::move(fProxy), Swizzle::Concat(fSwizzle, swizzle)};
+        return {std::move(fProxy), Swizzle::Concat(fSwizzle, swizzle), fOrigin};
     }
 
-    Origin origin() const { return fOrigin; }
+    // resetSwizzle returns a new view that uses 'swizzle' and disregards this view's prior swizzle.
+    TextureProxyView replaceSwizzle(Swizzle swizzle) const {
+        return {fProxy, swizzle, fOrigin};
+    }
 
     void reset() {
         *this = {};
