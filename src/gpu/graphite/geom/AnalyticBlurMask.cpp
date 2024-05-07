ebef7514011ad695c3ea14b8d6303997c4865367
James Godfrey-Kittle <jamesgk@google.com>
2024-05-07 10:37:16 -0400

    [graphite] Disable analytic blur for rrects
    Bug: b/238762890
    Bug: b/337908722
    Change-Id: Ief8a0715e91171c4c4ced371169a40a94465c3bc
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/848936
    Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- src/gpu/graphite/geom/AnalyticBlurMask.cpp
+++ src/gpu/graphite/geom/AnalyticBlurMask.cpp
@@ -73,10 +73,12 @@ std::optional<AnalyticBlurMask> AnalyticBlurMask::Make(Recorder* recorder,
         return MakeCircle(recorder, localToDevice, deviceSigma, srcRect, devRect);
     }
 
-    if (devRRectIsValid && SkRRectPriv::IsSimpleCircular(devRRect) &&
-        localToDevice.isScaleTranslate()) {
-        return MakeRRect(recorder, localToDevice, deviceSigma, srcRRect, devRRect);
-    }
+    // TODO(b/338032240) When this is enabled on Chrome we see regressions in some cases (see
+    // b/337908722). Profiling shows that most of the time is spent creating a blur mask on the CPU.
+    // if (devRRectIsValid && SkRRectPriv::IsSimpleCircular(devRRect) &&
+    //     localToDevice.isScaleTranslate()) {
+    //     return MakeRRect(recorder, localToDevice, deviceSigma, srcRRect, devRRect);
+    // }
 
     return std::nullopt;
 }
