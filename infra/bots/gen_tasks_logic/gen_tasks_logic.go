76f88de905be487829e697a2f5bbe757c56c1954
Kaylee Lubick <kjlubick@google.com>
2024-05-08 14:48:19 -0400

    Run gofmt
    Change-Id: Ib4ce04f09a535b7c078217724923c2c7f11c92a7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/851157
    Reviewed-by: John Stiles <johnstiles@google.com>

--- infra/bots/gen_tasks_logic/gen_tasks_logic.go
+++ infra/bots/gen_tasks_logic/gen_tasks_logic.go
@@ -2179,16 +2179,16 @@ type labelAndSavedOutputDir struct {
 // label or "target pattern" https://bazel.build/docs/build#specifying-build-targets
 // The reason we need this mapping is because Buildbucket build names cannot have / or : in them.
 var shorthandToLabel = map[string]labelAndSavedOutputDir{
-	"all_tests":                      {"//tests:linux_rbe_tests", ""},
-	"core":                           {"//:core", ""},
-	"cpu_8888_benchmark_test":        {"//bench:cpu_8888_test", ""},
-	"cpu_gms":                        {"//gm:cpu_gm_tests", ""},
-	"full_library":                   {"//tools:full_build", ""},
-	"ganesh_gl":                      {"//:ganesh_gl", ""},
-	"hello_bazel_world_test":         {"//gm:hello_bazel_world_test", ""},
-	"modules_canvaskit":              {"//modules/canvaskit:canvaskit", ""},
-	"modules_canvaskit_js_tests":     {"//modules/canvaskit:canvaskit_js_tests", ""},
-	"skottie_tool_gpu":               {"//modules/skottie:skottie_tool_gpu", ""},
+	"all_tests":                  {"//tests:linux_rbe_tests", ""},
+	"core":                       {"//:core", ""},
+	"cpu_8888_benchmark_test":    {"//bench:cpu_8888_test", ""},
+	"cpu_gms":                    {"//gm:cpu_gm_tests", ""},
+	"full_library":               {"//tools:full_build", ""},
+	"ganesh_gl":                  {"//:ganesh_gl", ""},
+	"hello_bazel_world_test":     {"//gm:hello_bazel_world_test", ""},
+	"modules_canvaskit":          {"//modules/canvaskit:canvaskit", ""},
+	"modules_canvaskit_js_tests": {"//modules/canvaskit:canvaskit_js_tests", ""},
+	"skottie_tool_gpu":           {"//modules/skottie:skottie_tool_gpu", ""},
 
 	// Note: these paths are relative to the WORKSPACE in //example/external_client
 	"decode_everything":  {"//:decode_everything", ""},
