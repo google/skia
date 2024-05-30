1b969342c87a1a537f3140d3f642705d9e8ebb47
Greg Daniel <egdaniel@google.com>
2024-05-30 14:07:13 -0400

    [Graphite] Fix key creation for vulkan uniform desc sets.
    Previously we were not indexing the key build using the actual
    binding index which was causing use to match things incorrectly.
    For example a DescSet that used bindings 0 and 1 was matching with
    one use 0 and 2.
    
    Change-Id: I2c70e079fead2e929ec353bf6018360edf3320bc
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/859979
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>
    Reviewed-by: John Stiles <johnstiles@google.com>

--- src/gpu/graphite/vk/VulkanResourceProvider.cpp
+++ src/gpu/graphite/vk/VulkanResourceProvider.cpp
@@ -292,21 +292,23 @@ UniqueKey make_ubo_bind_group_key(SkSpan<DescriptorData> requestedDescriptors,
                 &uniqueKey, kBufferBindGroupDomain, 6, "GraphicsPipelineBufferDescSet");
 
         for (uint32_t i = 0; i < VulkanGraphicsPipeline::kNumUniformBuffers; ++i) {
-            if (i < requestedDescriptors.size()) {
-                int descriptorBindingIndex = requestedDescriptors[i].fBindingIndex;
-                SkASSERT(static_cast<unsigned long>(descriptorBindingIndex) <
-                         bindUniformBufferInfo.size());
-                const auto& bindInfo = bindUniformBufferInfo[descriptorBindingIndex];
-                const VulkanBuffer* boundBuffer =
-                        static_cast<const VulkanBuffer*>(bindInfo.fBuffer);
-                SkASSERT(boundBuffer);
-                const uint32_t bindingSize = bindInfo.fBindingSize;
-                builder[2 * i] = boundBuffer->uniqueID().asUInt();
-                builder[2 * i + 1] = bindingSize;
-            } else {
-                builder[2 * i] = 0;
-                builder[2 * i + 1] = 0;
-            }
+            builder[2 * i] = 0;
+            builder[2 * i + 1] = 0;
+        }
+
+        for (uint32_t i = 0; i < requestedDescriptors.size(); ++i) {
+            int descriptorBindingIndex = requestedDescriptors[i].fBindingIndex;
+            SkASSERT(static_cast<unsigned long>(descriptorBindingIndex) <
+                        bindUniformBufferInfo.size());
+            SkASSERT(static_cast<unsigned long>(descriptorBindingIndex) <
+                     VulkanGraphicsPipeline::kNumUniformBuffers);
+            const auto& bindInfo = bindUniformBufferInfo[descriptorBindingIndex];
+            const VulkanBuffer* boundBuffer =
+                    static_cast<const VulkanBuffer*>(bindInfo.fBuffer);
+            SkASSERT(boundBuffer);
+            const uint32_t bindingSize = bindInfo.fBindingSize;
+            builder[2 * descriptorBindingIndex] = boundBuffer->uniqueID().asUInt();
+            builder[2 * descriptorBindingIndex + 1] = bindingSize;
         }
 
         builder.finish();
