a6ac2f8e82b40b8f230231ce038a715533e14853
John Stiles <johnstiles@google.com>
2024-07-15 17:02:16 -0400

    Mark unsupported subpass-input types as reserved.
    Bug: b/343194653
    Change-Id: I61cc404a78c3e94702571034a8ec534af6f016b6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/877678
    Reviewed-by: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

---
+++ tests/sksl/errors/ReservedNameSubpassInput.glsl
@@ -0,0 +1,15 @@
+### Compilation failed:
+
+error: 1: name 'isubpassInput' is reserved
+int isubpassInput;
+    ^^^^^^^^^^^^^
+error: 2: name 'isubpassInputMS' is reserved
+int isubpassInputMS;
+    ^^^^^^^^^^^^^^^
+error: 3: name 'usubpassInput' is reserved
+int usubpassInput;
+    ^^^^^^^^^^^^^
+error: 4: name 'usubpassInputMS' is reserved
+int usubpassInputMS;
+    ^^^^^^^^^^^^^^^
+4 errors
