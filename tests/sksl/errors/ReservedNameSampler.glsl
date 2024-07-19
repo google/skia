0a366e43f6ec22f92a7ffd34790377d4c6fe0540
John Stiles <johnstiles@google.com>
2024-07-19 15:11:11 -0400

    Mark unsupported texture/sampler types as reserved.
    This should be the last of the new reserved tokens. The remaining
    unhandled words will be allowed to pass through safely.
    
    Bug: b/343194653
    Change-Id: I946e6d4f792269ae5d7f54b9a199eb63c121b810
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/879641
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- tests/sksl/errors/ReservedNameSampler.glsl
+++ tests/sksl/errors/ReservedNameSampler.glsl
@@ -3,88 +3,121 @@
 error: 1: name 'sampler1D' is reserved
 int sampler1D;
     ^^^^^^^^^
-error: 2: name 'sampler1DShadow' is reserved
+error: 2: name 'sampler1DArray' is reserved
+int sampler1DArray;
+    ^^^^^^^^^^^^^^
+error: 3: name 'sampler1DArrayShadow' is reserved
+int sampler1DArrayShadow;
+    ^^^^^^^^^^^^^^^^^^^^
+error: 4: name 'sampler1DShadow' is reserved
 int sampler1DShadow;
     ^^^^^^^^^^^^^^^
-error: 3: name 'sampler2DRectShadow' is reserved
+error: 5: name 'sampler2DArray' is reserved
+int sampler2DArray;
+    ^^^^^^^^^^^^^^
+error: 6: name 'sampler2DArrayShadow' is reserved
+int sampler2DArrayShadow;
+    ^^^^^^^^^^^^^^^^^^^^
+error: 7: name 'sampler2DMS' is reserved
+int sampler2DMS;
+    ^^^^^^^^^^^
+error: 8: name 'sampler2DMSArray' is reserved
+int sampler2DMSArray;
+    ^^^^^^^^^^^^^^^^
+error: 9: name 'sampler2DRectShadow' is reserved
 int sampler2DRectShadow;
     ^^^^^^^^^^^^^^^^^^^
-error: 4: name 'sampler2DShadow' is reserved
+error: 10: name 'sampler2DShadow' is reserved
 int sampler2DShadow;
     ^^^^^^^^^^^^^^^
-error: 5: name 'sampler3D' is reserved
+error: 11: name 'sampler3D' is reserved
 int sampler3D;
     ^^^^^^^^^
-error: 6: name 'sampler3DRect' is reserved
+error: 12: name 'sampler3DRect' is reserved
 int sampler3DRect;
     ^^^^^^^^^^^^^
-error: 7: name 'samplerCube' is reserved
+error: 13: name 'samplerBuffer' is reserved
+int samplerBuffer;
+    ^^^^^^^^^^^^^
+error: 14: name 'samplerCube' is reserved
 int samplerCube;
     ^^^^^^^^^^^
-error: 9: name 'isampler1D' is reserved
+error: 15: name 'samplerCubeArray' is reserved
+int samplerCubeArray;
+    ^^^^^^^^^^^^^^^^
+error: 16: name 'samplerCubeArrayShadow' is reserved
+int samplerCubeArrayShadow;
+    ^^^^^^^^^^^^^^^^^^^^^^
+error: 17: name 'samplerCubeShadow' is reserved
+int samplerCubeShadow;
+    ^^^^^^^^^^^^^^^^^
+error: 18: name 'samplerShadow' is reserved
+int samplerShadow;
+    ^^^^^^^^^^^^^
+error: 20: name 'isampler1D' is reserved
 int isampler1D;
     ^^^^^^^^^^
-error: 10: name 'isampler2D' is reserved
+error: 21: name 'isampler2D' is reserved
 int isampler2D;
     ^^^^^^^^^^
-error: 11: name 'isampler3D' is reserved
+error: 22: name 'isampler3D' is reserved
 int isampler3D;
     ^^^^^^^^^^
-error: 12: name 'isampler1DArray' is reserved
+error: 23: name 'isampler1DArray' is reserved
 int isampler1DArray;
     ^^^^^^^^^^^^^^^
-error: 13: name 'isampler2DArray' is reserved
+error: 24: name 'isampler2DArray' is reserved
 int isampler2DArray;
     ^^^^^^^^^^^^^^^
-error: 14: name 'isampler2DRect' is reserved
+error: 25: name 'isampler2DRect' is reserved
 int isampler2DRect;
     ^^^^^^^^^^^^^^
-error: 15: name 'isampler2DMS' is reserved
+error: 26: name 'isampler2DMS' is reserved
 int isampler2DMS;
     ^^^^^^^^^^^^
-error: 16: name 'isampler2DMSArray' is reserved
+error: 27: name 'isampler2DMSArray' is reserved
 int isampler2DMSArray;
     ^^^^^^^^^^^^^^^^^
-error: 17: name 'isamplerBuffer' is reserved
+error: 28: name 'isamplerBuffer' is reserved
 int isamplerBuffer;
     ^^^^^^^^^^^^^^
-error: 18: name 'isamplerCube' is reserved
+error: 29: name 'isamplerCube' is reserved
 int isamplerCube;
     ^^^^^^^^^^^^
-error: 19: name 'isamplerCubeArray' is reserved
+error: 30: name 'isamplerCubeArray' is reserved
 int isamplerCubeArray;
     ^^^^^^^^^^^^^^^^^
-error: 21: name 'usampler1D' is reserved
+error: 32: name 'usampler1D' is reserved
 int usampler1D;
     ^^^^^^^^^^
-error: 22: name 'usampler2D' is reserved
+error: 33: name 'usampler2D' is reserved
 int usampler2D;
     ^^^^^^^^^^
-error: 23: name 'usampler3D' is reserved
+error: 34: name 'usampler3D' is reserved
 int usampler3D;
     ^^^^^^^^^^
-error: 24: name 'usampler1DArray' is reserved
+error: 35: name 'usampler1DArray' is reserved
 int usampler1DArray;
     ^^^^^^^^^^^^^^^
-error: 25: name 'usampler2DArray' is reserved
+error: 36: name 'usampler2DArray' is reserved
 int usampler2DArray;
     ^^^^^^^^^^^^^^^
-error: 26: name 'usampler2DRect' is reserved
+error: 37: name 'usampler2DRect' is reserved
 int usampler2DRect;
     ^^^^^^^^^^^^^^
-error: 27: name 'usampler2DMS' is reserved
+error: 38: name 'usampler2DMS' is reserved
 int usampler2DMS;
     ^^^^^^^^^^^^
-error: 28: name 'usampler2DMSArray' is reserved
+error: 39: name 'usampler2DMSArray' is reserved
 int usampler2DMSArray;
     ^^^^^^^^^^^^^^^^^
-error: 29: name 'usamplerBuffer' is reserved
+error: 40: name 'usamplerBuffer' is reserved
 int usamplerBuffer;
     ^^^^^^^^^^^^^^
-error: 30: name 'usamplerCube' is reserved
+error: 41: name 'usamplerCube' is reserved
 int usamplerCube;
     ^^^^^^^^^^^^
-error: 31: name 'usamplerCubeArray' is reserved
+error: 42: name 'usamplerCubeArray' is reserved
 int usamplerCubeArray;
     ^^^^^^^^^^^^^^^^^
-29 errors
+40 errors
--- tests/sksl/errors/ReservedNameTexture.glsl
+++ tests/sksl/errors/ReservedNameTexture.glsl
@@ -1,69 +1,96 @@
 ### Compilation failed:
 
-error: 1: name 'itexture1D' is reserved
+error: 1: name 'texture1D' is reserved
+int texture1D;
+    ^^^^^^^^^
+error: 2: name 'texture1DArray' is reserved
+int texture1DArray;
+    ^^^^^^^^^^^^^^
+error: 3: name 'texture2DArray' is reserved
+int texture2DArray;
+    ^^^^^^^^^^^^^^
+error: 4: name 'texture2DMS' is reserved
+int texture2DMS;
+    ^^^^^^^^^^^
+error: 5: name 'texture2DMSArray' is reserved
+int texture2DMSArray;
+    ^^^^^^^^^^^^^^^^
+error: 6: name 'texture3D' is reserved
+int texture3D;
+    ^^^^^^^^^
+error: 7: name 'textureBuffer' is reserved
+int textureBuffer;
+    ^^^^^^^^^^^^^
+error: 8: name 'textureCube' is reserved
+int textureCube;
+    ^^^^^^^^^^^
+error: 9: name 'textureCubeArray' is reserved
+int textureCubeArray;
+    ^^^^^^^^^^^^^^^^
+error: 11: name 'itexture1D' is reserved
 int itexture1D;
     ^^^^^^^^^^
-error: 2: name 'itexture2D' is reserved
+error: 12: name 'itexture2D' is reserved
 int itexture2D;
     ^^^^^^^^^^
-error: 3: name 'itexture3D' is reserved
+error: 13: name 'itexture3D' is reserved
 int itexture3D;
     ^^^^^^^^^^
-error: 4: name 'itexture1DArray' is reserved
+error: 14: name 'itexture1DArray' is reserved
 int itexture1DArray;
     ^^^^^^^^^^^^^^^
-error: 5: name 'itexture2DArray' is reserved
+error: 15: name 'itexture2DArray' is reserved
 int itexture2DArray;
     ^^^^^^^^^^^^^^^
-error: 6: name 'itexture2DRect' is reserved
+error: 16: name 'itexture2DRect' is reserved
 int itexture2DRect;
     ^^^^^^^^^^^^^^
-error: 7: name 'itexture2DMS' is reserved
+error: 17: name 'itexture2DMS' is reserved
 int itexture2DMS;
     ^^^^^^^^^^^^
-error: 8: name 'itexture2DMSArray' is reserved
+error: 18: name 'itexture2DMSArray' is reserved
 int itexture2DMSArray;
     ^^^^^^^^^^^^^^^^^
-error: 9: name 'itextureBuffer' is reserved
+error: 19: name 'itextureBuffer' is reserved
 int itextureBuffer;
     ^^^^^^^^^^^^^^
-error: 10: name 'itextureCube' is reserved
+error: 20: name 'itextureCube' is reserved
 int itextureCube;
     ^^^^^^^^^^^^
-error: 11: name 'itextureCubeArray' is reserved
+error: 21: name 'itextureCubeArray' is reserved
 int itextureCubeArray;
     ^^^^^^^^^^^^^^^^^
-error: 13: name 'utexture1D' is reserved
+error: 23: name 'utexture1D' is reserved
 int utexture1D;
     ^^^^^^^^^^
-error: 14: name 'utexture2D' is reserved
+error: 24: name 'utexture2D' is reserved
 int utexture2D;
     ^^^^^^^^^^
-error: 15: name 'utexture3D' is reserved
+error: 25: name 'utexture3D' is reserved
 int utexture3D;
     ^^^^^^^^^^
-error: 16: name 'utexture1DArray' is reserved
+error: 26: name 'utexture1DArray' is reserved
 int utexture1DArray;
     ^^^^^^^^^^^^^^^
-error: 17: name 'utexture2DArray' is reserved
+error: 27: name 'utexture2DArray' is reserved
 int utexture2DArray;
     ^^^^^^^^^^^^^^^
-error: 18: name 'utexture2DRect' is reserved
+error: 28: name 'utexture2DRect' is reserved
 int utexture2DRect;
     ^^^^^^^^^^^^^^
-error: 19: name 'utexture2DMS' is reserved
+error: 29: name 'utexture2DMS' is reserved
 int utexture2DMS;
     ^^^^^^^^^^^^
-error: 20: name 'utexture2DMSArray' is reserved
+error: 30: name 'utexture2DMSArray' is reserved
 int utexture2DMSArray;
     ^^^^^^^^^^^^^^^^^
-error: 21: name 'utextureBuffer' is reserved
+error: 31: name 'utextureBuffer' is reserved
 int utextureBuffer;
     ^^^^^^^^^^^^^^
-error: 22: name 'utextureCube' is reserved
+error: 32: name 'utextureCube' is reserved
 int utextureCube;
     ^^^^^^^^^^^^
-error: 23: name 'utextureCubeArray' is reserved
+error: 33: name 'utextureCubeArray' is reserved
 int utextureCubeArray;
     ^^^^^^^^^^^^^^^^^
-22 errors
+31 errors
