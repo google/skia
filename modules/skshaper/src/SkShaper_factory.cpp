d528658273db7354a2d84d4f4f7f1f8b238544f4
Eric Boren <borenet@google.com>
2024-08-19 14:40:01 +0000

    [bazel] Fix skshaper build for Windows
    Without this, I get an error about SkFontMgr being an incomplete type.
    
    Bug: b/256860862
    Change-Id: Iaaf52e936f3360b96008fcbde7f62ad25315f083
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/890838
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- modules/skshaper/src/SkShaper_factory.cpp
+++ modules/skshaper/src/SkShaper_factory.cpp
@@ -4,7 +4,10 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
 #include "modules/skshaper/include/SkShaper_factory.h"
+
+#include "include/core/SkFontMgr.h" // IWYU pragma: keep
 
 namespace {
 class PrimitiveFactory final : public SkShapers::Factory {
