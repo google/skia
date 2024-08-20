8bd493b850f1a75482af8f30cb492cd70645498c
James Godfrey-Kittle <jamesgk@google.com>
2024-08-20 14:35:00 -0400

    [ganesh] Avoid int overflow when combining RegionOps
    Bug: b/360758697
    Change-Id: I46eb92ac6ed71646fb05a910f8d577ec851e3b3f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/891636
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    (cherry picked from commit efd38e98b22e3335e4d2bb562c1d2a610990eaca)
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/892184
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- src/gpu/ganesh/ops/RegionOp.cpp
+++ src/gpu/ganesh/ops/RegionOp.cpp
@@ -8,6 +8,7 @@
 #include "src/gpu/ganesh/ops/RegionOp.h"
 
 #include "include/core/SkRegion.h"
+#include "src/base/SkSafeMath.h"
 #include "src/core/SkMatrixPriv.h"
 #include "src/gpu/BufferWriter.h"
 #include "src/gpu/ganesh/GrCaps.h"
@@ -116,8 +117,14 @@ private:
 
         int numRegions = fRegions.size();
         int numRects = 0;
+
+        SkSafeMath safeMath;
         for (int i = 0; i < numRegions; i++) {
-            numRects += fRegions[i].fRegion.computeRegionComplexity();
+            numRects = safeMath.addInt(numRects, fRegions[i].fRegion.computeRegionComplexity());
+        }
+        if (!safeMath) {
+            // This is a nonsensical draw, so we can just drop it.
+            return;
         }
 
         if (!numRects) {
