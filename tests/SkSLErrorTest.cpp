e4e8af427a45bbdde293325e16683f60e1eb82cf
Brian Osman <brianosman@google.com>
2024-08-21 15:06:26 -0400

    Ignore more deleted test files on Android devices
    Bug: 40044088
    Change-Id: Ie4d2066b68fec925ad348f4ae675d93f865ba7ae
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/892218
    Commit-Queue: Brian Osman <brianosman@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- tests/SkSLErrorTest.cpp
+++ tests/SkSLErrorTest.cpp
@@ -115,6 +115,12 @@ static void test_expect_fail(skiatest::Reporter* r, const char* testFile, SkSL::
         "sksl/errors/UnspecifiedBinding.compute",
 
         "sksl/runtime_errors/ReservedNameISampler2D.rts",
+        "sksl/runtime_errors/ProgramTooLarge_BlocklessLoops.rts",
+        "sksl/runtime_errors/ProgramTooLarge_Extreme.rts",
+        "sksl/runtime_errors/ProgramTooLarge_FlatLoop.rts",
+        "sksl/runtime_errors/ProgramTooLarge_Functions.rts",
+        "sksl/runtime_errors/ProgramTooLarge_NestedLoops.rts",
+        "sksl/runtime_errors/ProgramTooLarge_SplitLoops.rts",
 
 #ifdef SK_ENABLE_OPTIMIZE_SIZE
         "sksl/errors/ArrayInlinedIndexOutOfRange.sksl",
