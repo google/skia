4c66b7e42027c14fb48809031230ce0b575c2cfd
Kaylee Lubick <kjlubick@google.com>
2024-08-21 14:53:28 -0400

    Remove unnecessary GL shim files
    Follow-up from http://review.skia.org/842504 now that all clients
    have been migrated.
    
    Change-Id: I4abecb483ec3ed27fa9f5654ab1f68d6b37a25f9
    Bug: b/293490566
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/892216
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- include/gpu/gl/egl/GrGLMakeEGLInterface.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2021 Google LLC
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-// TODO(kjlubick, egdaniel) remove these shim files after clients migrated
-#include "include/gpu/ganesh/gl/egl/GrGLMakeEGLInterface.h"
--- include/gpu/gl/epoxy/GrGLMakeEpoxyEGLInterface.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2024 Google LLC
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-// TODO(kjlubick, egdaniel) remove these shim files after clients migrated
-#include "include/gpu/ganesh/gl/epoxy/GrGLMakeEpoxyEGLInterface.h"
