20b42bf48559cef3d1f6ca6ee92b872cf3dc8d90
kylechar <kylechar@chromium.org>
2024-09-04 16:50:59 -0400

    [graphite] Disable SSBOs for Dawn/Vulkan
    Graphite/Dawn/Vulkan is hitting dawn device loss due to out-of-bounds
    memory access in a shader on Android. The root cause hasn't been worked
    out yet so disabled SSBOs with the Vulkan backend for the time being.
    This unblocks starting a finch experiment on Android.
    
    Bug: chromium:344963958
    Change-Id: I7eb9e0bdd649c2c89ed9b9c3227e9ffda47f388c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/896480
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>

--- src/gpu/graphite/dawn/DawnCaps.cpp
+++ src/gpu/graphite/dawn/DawnCaps.cpp
@@ -457,9 +457,12 @@ void DawnCaps::initCaps(const DawnBackendContext& backendContext, const ContextO
 #if !defined(__EMSCRIPTEN__)
     // TODO(b/318817249): In D3D11, SSBOs trigger FXC compiler failures when attempting to unroll
     // loops.
+    // TODO(b/344963958): SSBOs contribute to OOB shader memory access and dawn device loss on
+    // Android. Once the problem is fixed SSBOs can be enabled again.
     fStorageBufferSupport = info.backendType != wgpu::BackendType::D3D11 &&
                             info.backendType != wgpu::BackendType::OpenGL &&
-                            info.backendType != wgpu::BackendType::OpenGLES;
+                            info.backendType != wgpu::BackendType::OpenGLES &&
+                            info.backendType != wgpu::BackendType::Vulkan;
 #else
     // WASM doesn't provide a way to query the backend, so can't tell if we are on d3d11 or not.
     // Pessimistically assume we could be. Once b/318817249 is fixed, this can go away and SSBOs
