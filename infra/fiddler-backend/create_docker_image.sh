fbe6d854289a8769409ed40e496064f39d0250e0
Kaylee Lubick <kjlubick@google.com>
2024-09-04 13:49:14 -0400

    Add Dockerfile/script to build fiddler backend
    See also http://go/louhicl/159520 and http://review.skia.org/896598
    
    This draws heavy inspiration from https://github.com/google/skia/blob/main/docker/skia-release/Dockerfile
    
    Change-Id: Ie04558f3dccc17429badf965f8c8859deff8a77d
    Bug: b/361559681
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/895196
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

---
+++ infra/fiddler-backend/create_docker_image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright 2024 Google, LLC
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# To be run on Louhi to create the fiddler final image which has both the
+# compiled go code (fiddler) and the pre-compiled C++ fiddle and sec_wrap binary.
+
+# BASE_DIR is where this script is
+BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
+
+# Run docker build from the Skia root so the entire Skia repo is part of the
+# Docker context
+cd $BASE_DIR/../../ && docker build --tag fiddler-final . --file infra/fiddler-backend/Dockerfile
