b6bab0fde4263cf556bdc1263f57390b9414bad3
Kaylee Lubick <kjlubick@google.com>
2024-09-06 08:58:51 -0400

    Add debugging to fiddler-backend Docker creation
    Failing mysteriously https://louhi.dev/6316342352543744/execution-detail/4814501665177600
    
    Step #1: ./create_docker_image.sh: 1: Bad substitution
    Step #1: unable to prepare context: unable to evaluate symlinks in Dockerfile path: lstat /infra: no such file or directory
    Finished Step #1
    
    Change-Id: I5d1f45eae7095463338d36ea84e9598ea67e1f4a
    Bug: b/361559681
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/897296
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/fiddler-backend/create_docker_image.sh
+++ infra/fiddler-backend/create_docker_image.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 2024 Google, LLC
 #
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+set -ex
 
 # To be run on Louhi to create the fiddler final image which has both the
 # compiled go code (fiddler) and the pre-compiled C++ fiddle and sec_wrap binary.
@@ -12,4 +14,4 @@ BASE_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 
 # Run docker build from the Skia root so the entire Skia repo is part of the
 # Docker context
-cd $BASE_DIR/../../ && docker build --tag fiddler-final . --file infra/fiddler-backend/Dockerfile
+cd $BASE_DIR/../../ && pwd && ls -ahl && docker build --tag fiddler-final . --file infra/fiddler-backend/Dockerfile
