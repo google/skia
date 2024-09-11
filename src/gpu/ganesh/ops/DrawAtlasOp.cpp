2b40b50ea423e11073b742b3bd785975a6019046
James Godfrey-Kittle <jamesgk@google.com>
2024-09-11 16:18:40 -0400

    [ganesh] Avoid int overflow in DrawAtlasOpImpl
    Bug: b/365884464
    Change-Id: I4dc9f259165c88c1d7ae5dc38c6cae02ca18f509
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/898756
    Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>

--- src/gpu/ganesh/ops/DrawAtlasOp.cpp
+++ src/gpu/ganesh/ops/DrawAtlasOp.cpp
@@ -146,6 +146,7 @@ DrawAtlasOpImpl::DrawAtlasOpImpl(GrProcessorSet* processorSet, const SkPMColor4f
         : GrMeshDrawOp(ClassID()), fHelper(processorSet, aaType), fColor(color) {
     SkASSERT(xforms);
     SkASSERT(rects);
+    SkASSERT(spriteCount >= 0);
 
     fViewMatrix = viewMatrix;
     Geometry& installedGeo = fGeoData.push_back();
@@ -159,6 +160,11 @@ DrawAtlasOpImpl::DrawAtlasOpImpl(GrProcessorSet* processorSet, const SkPMColor4f
     if (colors) {
         texOffset += sizeof(GrColor);
         vertexStride += sizeof(GrColor);
+    }
+
+    // Bail out if we'd overflow from a really large draw
+    if (spriteCount > SK_MaxS32 / static_cast<int>(4 * vertexStride)) {
+        return;
     }
 
     // Compute buffer size and alloc buffer
