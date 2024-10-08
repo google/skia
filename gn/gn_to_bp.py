38e2598c487b539530a82047f6955b2691072947
Kaylee Lubick <kjlubick@google.com>
2024-10-08 12:29:29 +0000

    Revert "Enable CrabbyAvif for Android framework"
    This reverts commit d0e49fde37656fe69a78705e74beabb8534d00e2.
    
    Reason for revert: Breaking Android roll http://ag/29738240
    
    Original change's description:
    > Enable CrabbyAvif for Android framework
    >
    > Enable the crabbyavif dependency on the Android framework by
    > adding the relevant dependency in gn_to_bp.py.
    >
    > This is similar to how we include the current SkHeifDecoder
    > dependency (which is also Android only).
    >
    > Test: Applied the changes and tested the Android framework build with the resulting Android.bp and config files.
    > Bug: b/323753814
    > Change-Id: Id24a00aa7852696cbbdc5b9f3ee3873eed488dbf
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/906896
    > Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    > Commit-Queue: Vignesh Venkat <vigneshv@google.com>
    
    Bug: b/323753814
    Change-Id: I509fd54c7de2537f4fc7b4fdf1ad0fdc68624453
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/907483
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>

--- gn/gn_to_bp.py
+++ gn/gn_to_bp.py
@@ -277,9 +277,6 @@ cc_defaults {
         shared_libs: [
             "libheif",
         ],
-        static_libs: [
-            "libcrabbyavif_ffi",
-        ],
       },
       darwin: {
         host_ldlibs: [
@@ -497,11 +494,9 @@ def generate_args(target_os, enable_gpu, renderengine = False):
 
   if target_os == '"android"' and not renderengine:
     d['skia_use_libheif']  = 'true'
-    d['skia_use_crabbyavif'] = 'true'
     d['skia_use_jpeg_gainmaps'] = 'true'
   else:
     d['skia_use_libheif']  = 'false'
-    d['skia_use_crabbyavif'] = 'false'
 
   if renderengine:
     d['skia_use_libpng_decode'] = 'false'
