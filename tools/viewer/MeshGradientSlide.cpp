c4e3592ca1e58ed3e433907726f386863c6cf4f4
Florin Malita <fmalita@google.com>
2024-11-04 14:31:30 -0500

    [viewer] Add AE mesh gradient sample
    Introduce a gradient abstraction plus an AE-inspired implementation
    (sksl-based).  This should be extensible for other gradients types.
    
    Since runtime effects expect uniform data in continuous memory, flatten
    the vertex structure and store uvs/pos/colors as separate vectors, to
    minimize friction.
    
    Change-Id: Iddc12a5d58cf5dd05388824dd841061523afe9b0
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/916316
    Reviewed-by: Max Hudnell <maxhudnell@google.com>
    Commit-Queue: Florin Malita <fmalita@google.com>

--- tools/viewer/MeshGradientSlide.cpp
+++ tools/viewer/MeshGradientSlide.cpp
@@ -9,6 +9,11 @@
 #include "include/core/SkColor.h"
 #include "include/core/SkCubicMap.h"
 #include "include/core/SkPoint.h"
+#include "include/core/SkRefCnt.h"
+#include "include/core/SkShader.h"
+#include "include/core/SkString.h"
+#include "include/effects/SkRuntimeEffect.h"
+#include "include/private/base/SkDebug.h"
 #include "src/base/SkRandom.h"
 #include "tools/viewer/Slide.h"
 
@@ -20,10 +25,95 @@
 
 namespace {
 
-struct ColorVertex {
-    SkPoint   fUV,    // position, in normalized [0..1] space
-              fPos;   // draw position (post animation), in normalized space
-    SkColor4f fColor;
+class GradientRenderer : public SkRefCnt {
+public:
+    virtual void draw(SkCanvas*) const = 0;
+
+    virtual sk_sp<SkShader> asShader() const = 0;
+
+    virtual void updateVertices(SkSpan<const SkPoint> vert_pos,
+                                SkSpan<const SkColor4f> vert_colors) = 0;
+};
+
+class AEGradientRenderer final : public GradientRenderer {
+public:
+    void draw(SkCanvas* canvas) const override {
+        SkPaint paint;
+        paint.setShader(fShader);
+        canvas->drawRect(SkRect::MakeWH(1, 1), paint);
+    }
+
+    sk_sp<SkShader> asShader() const override { return fShader; }
+
+    void updateVertices(SkSpan<const SkPoint> vert_pos,
+                        SkSpan<const SkColor4f> vert_colors) override {
+        SkASSERT(vert_pos.size() == vert_colors.size());
+        const auto vert_count = vert_pos.size();
+
+        if (!vert_count) {
+            return;
+        }
+
+        // Effect compilation is expensive, so we cache and only recompile when the count changes.
+        if (vert_count != fCachedCount) {
+            this->buildEffect(vert_count);
+            fCachedCount = vert_count;
+        }
+
+        SkRuntimeEffectBuilder builder(fEffect);
+        builder.uniform("u_vertcolors").set(vert_colors.data(), vert_colors.size());
+        builder.uniform("u_vertpos")   .set(vert_pos.data()   , vert_pos.size());
+
+        fShader = builder.makeShader();
+    }
+
+private:
+    void buildEffect(size_t vert_count) {
+        static constexpr char gAEGradientSkSL[] = R"(
+            uniform half4  u_vertcolors[%zu];
+            uniform float2 u_vertpos[%zu];
+
+            half4 main(float2 xy) {
+                half4 c = half4(0);
+                float w_acc = 0;
+
+                for (int i = 0; i < %zu; ++i) {
+                    float d = distance(xy, u_vertpos[i]);
+                    float w = 1 / (d * d);
+
+                    c += u_vertcolors[i] * w;
+                    w_acc += w;
+                }
+
+                return c / w_acc;
+            }
+        )";
+
+        const auto res = SkRuntimeEffect::MakeForShader(
+                            SkStringPrintf(gAEGradientSkSL, vert_count, vert_count, vert_count));
+        if (!res.effect) {
+            SkDEBUGF("%s\n", res.errorText.c_str());
+        }
+
+        fEffect = res.effect;
+
+        SkASSERT(fEffect);
+    }
+
+    sk_sp<SkRuntimeEffect> fEffect;
+    sk_sp<SkShader>        fShader;
+
+    size_t                 fCachedCount = 0;
+};
+
+static constexpr struct RendererChoice {
+    const char* fName;
+    sk_sp<GradientRenderer>(*fFactory)();
+} gGradientRenderers[] = {
+    {
+        "AfterEffects Gradient",
+        []() -> sk_sp<GradientRenderer> { return sk_make_sp<AEGradientRenderer>(); }
+    },
 };
 
 float lerp(float a, float b, float t) {
@@ -32,16 +122,16 @@ float lerp(float a, float b, float t) {
 
 static constexpr struct VertexAnimator {
     const char* fName;
-    void (*fAanimate)(std::vector<ColorVertex>& verts, float t);
+    void (*fAanimate)(SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t);
 } gVertexAnimators[] = {
     {
         "Wigglynator",
-        [](std::vector<ColorVertex>& verts, float t) {
-            const float radius = t*0.2f/(std::sqrt(verts.size()) - 1);
-            for (size_t i = 0; i < verts.size(); ++i) {
+        [](SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t) {
+            const float radius = t*0.2f/(std::sqrt(uvs.size()) - 1);
+            for (size_t i = 0; i < uvs.size(); ++i) {
                 const float phase = i*SK_FloatPI*0.31f,
                             angle = phase + t*SK_FloatPI*2;
-                verts[i].fPos = verts[i].fUV + SkVector{
+                pos[i] = uvs[i] + SkVector{
                     radius*std::cos(angle),
                     radius*std::sin(angle),
                 };
@@ -52,69 +142,72 @@ static constexpr struct VertexAnimator {
         "Squircillator",
         // Pull all vertices towards center, proportionally, such that the outer square edge
         // is mapped to a circle for t == 1.
-        [](std::vector<ColorVertex>& verts, float t) {
-            for (auto& v : verts) {
+        [](SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t) {
+            for (size_t i = 0; i < uvs.size(); ++i) {
                 // remap to [-.5,.5]
-                const auto uv = (v.fUV - SkPoint{0.5,0.5});
+                const auto uv = (uvs[i] - SkPoint{0.5,0.5});
 
                 // Distance from center to outer edge for the line pasing through uv.
                 const auto d = uv.length()*0.5f/std::max(std::abs(uv.fX), std::abs(uv.fY));
                 // Scale needed to pull the outer edge to the r=0.5 circle at t == 1.
                 const auto s = lerp(1, (0.5f / d), t);
 
-                v.fPos = uv*s + SkPoint{0.5, 0.5};
+                pos[i] = uv*s + SkPoint{0.5, 0.5};
             }
         },
     },
     {
         "Twirlinator",
         // Rotate vertices proportional to their distance to center.
-        [](std::vector<ColorVertex>& verts, float t) {
+        [](SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t) {
             static constexpr float kMaxRotate = SK_FloatPI*4;
 
-            for (auto& v : verts) {
+            for (size_t i = 0; i < uvs.size(); ++i) {
                 // remap to [-.5,.5]
-                const auto uv = (v.fUV - SkPoint{0.5,0.5});
+                const auto uv = (uvs[i] - SkPoint{0.5,0.5});
                 const auto angle = kMaxRotate * t * uv.length();
 
-                v.fPos = SkMatrix::RotateRad(angle).mapPoint(uv) + SkPoint{0.5, 0.5};
+                pos[i] = SkMatrix::RotateRad(angle).mapPoint(uv) + SkPoint{0.5, 0.5};
             }
         },
     },
     {
         "Cylinderator",
         // Simulate a cylinder rolling sideways across the 1x1 uv space.
-        [](std::vector<ColorVertex>& verts, float t) {
+        [](SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t) {
             static constexpr float kCylRadius = .2f;
 
             const auto cyl_pos = t;
 
-            for (auto& v : verts) {
-                if (v.fUV.fX <= cyl_pos) {
-                    v.fPos = v.fUV;
+            for (size_t i = 0; i < uvs.size(); ++i) {
+                if (uvs[i].fX <= cyl_pos) {
+                    pos[i] = uvs[i];
                     continue;
                 }
 
-                const auto arc_len = v.fUV.fX - cyl_pos,
+                const auto arc_len = uvs[i].fX - cyl_pos,
                            arc_ang = arc_len/kCylRadius;
 
-                v.fPos = SkPoint{
+                pos[i] = SkPoint{
                     cyl_pos + std::sin(arc_ang)*kCylRadius,
-                    v.fUV.fY,
+                    uvs[i].fY,
                 };
             }
         },
     },
     {
         "None",
-        [](std::vector<ColorVertex>& verts, float) { for (auto& v : verts) { v.fPos = v.fUV; } },
+        [](SkSpan<const SkPoint> uvs, SkSpan<SkPoint> pos, float t) {
+            memcpy(pos.data(), uvs.data(), uvs.size() * sizeof(SkPoint));
+        },
     },
 };
 
 class MeshGradientSlide final : public Slide {
 public:
     MeshGradientSlide()
-        : fTimeMapper({0.5f, 0}, {0.5f, 1})
+        : fCurrentRenderer(gGradientRenderers[0].fFactory())
+        , fTimeMapper({0.5f, 0}, {0.5f, 1})
         , fVertedCountTimeMapper({0, 0.5f}, {0.5f, 1})
     {
             fName = "MeshGradient";
@@ -140,14 +233,7 @@ public:
                           (fSize.fHeight - mesh_size) * 0.5f);
         canvas->scale(mesh_size, mesh_size);
 
-        SkPaint paint;
-        paint.setAntiAlias(true);
-
-        // TODO: render the actual mesh
-        for (const auto& v : fVertices) {
-            paint.setColor4f(v.fColor);
-            canvas->drawCircle(v.fPos, 0.005f, paint);
-        }
+        fCurrentRenderer->draw(canvas);
 
         this->drawControls();
     }
@@ -176,7 +262,9 @@ public:
             const float t =
                 std::abs((std::fmod((nanos - fTimeBase) * 0.000000001 * fAnimationSpeed, 2) - 1));
 
-            fCurrentAnimator->fAanimate(fVertices, fTimeMapper.computeYFromX(t));
+            fCurrentAnimator->fAanimate(fVertUVs, fVertPos, fTimeMapper.computeYFromX(t));
+
+            fCurrentRenderer->updateVertices(fVertPos, fVertColors);
 
             // TODO: mesh triangulation
         }
@@ -186,13 +274,17 @@ public:
 
 private:
     void updateMesh(size_t new_count) {
-        const size_t current_count = fVertices.size();
+        SkASSERT(fVertUVs.size() == fVertPos.size());
+        SkASSERT(fVertUVs.size() == fVertColors.size());
+        const size_t current_count = fVertUVs.size();
 
-        fVertices.resize(new_count);
+        fVertUVs.resize(new_count);
+        fVertPos.resize(new_count);
+        fVertColors.resize(new_count);
 
         for (size_t i = current_count; i < new_count; ++i) {
-            const SkPoint uv = { fRNG.nextF(), fRNG.nextF() };
-            fVertices[i] = { uv, uv, SkColor4f{ fRNG.nextF(), fRNG.nextF(), fRNG.nextF(), 1.f }};
+            fVertUVs[i] = fVertPos[i] = { fRNG.nextF(), fRNG.nextF() };
+            fVertColors[i] = { fRNG.nextF(), fRNG.nextF(), fRNG.nextF(), 1.f };
         }
 
         if (new_count < current_count) {
@@ -205,6 +297,21 @@ private:
 
     void drawControls() {
         ImGui::Begin("Mesh Options");
+
+        if (ImGui::BeginCombo("Gradient Type", fCurrentRendererChoice->fName)) {
+            for (const auto& renderer : gGradientRenderers) {
+                const auto is_selected = (fCurrentRendererChoice->fName == renderer.fName);
+                if (ImGui::Selectable(renderer.fName) && !is_selected) {
+                    fCurrentRendererChoice = &renderer;
+                    fCurrentRenderer = renderer.fFactory();
+                    fCurrentRenderer->updateVertices(fVertPos, fVertColors);
+                }
+                if (is_selected) {
+                    ImGui::SetItemDefaultFocus();
+                }
+            }
+            ImGui::EndCombo();
+        }
 
         if (ImGui::BeginCombo("Animator", fCurrentAnimator->fName)) {
             for (const auto& anim : gVertexAnimators) {
@@ -220,9 +327,9 @@ private:
             ImGui::EndCombo();
         }
 
-        if (ImGui::SliderInt("Vertex Count", &fVertexCountTo, 3, 256)) {
+        if (ImGui::SliderInt("Vertex Count", &fVertexCountTo, 3, 64)) {
             fVertexCountTimeBase = 0;
-            fVertexCountFrom = fVertices.size();
+            fVertexCountFrom = fVertUVs.size();
         }
 
         ImGui::SliderFloat("Speed", &fAnimationSpeed, 0.25, 4, "%.2f");
@@ -230,21 +337,27 @@ private:
         ImGui::End();
     }
 
-    SkSize                   fSize;
-    std::vector<ColorVertex> fVertices;
-    SkRandom                 fRNG;
+    SkSize                  fSize;
+    std::vector<SkPoint>    fVertUVs;
+    std::vector<SkPoint>    fVertPos;    // fVertUVs after animation
+    std::vector<SkColor4f>  fVertColors;
+
+    SkRandom                fRNG;
+
+    sk_sp<GradientRenderer> fCurrentRenderer;
 
     // Animation state
-    const SkCubicMap         fTimeMapper,
-                             fVertedCountTimeMapper;
-    double                   fTimeBase               = 0,
-                             fVertexCountTimeBase    = 0;
-    int                      fVertexCountFrom        = 0,
-                             fVertexCountTo          = 64;
+    const SkCubicMap        fTimeMapper,
+                            fVertedCountTimeMapper;
+    double                  fTimeBase               = 0,
+                            fVertexCountTimeBase    = 0;
+    int                     fVertexCountFrom        = 0,
+                            fVertexCountTo          = 16;
 
     // UI stuff
-    const VertexAnimator*    fCurrentAnimator = &gVertexAnimators[0];
-    float                    fAnimationSpeed  = 1.f;
+    const RendererChoice*   fCurrentRendererChoice  = &gGradientRenderers[0];
+    const VertexAnimator*   fCurrentAnimator        = &gVertexAnimators[0];
+    float                   fAnimationSpeed         = 1.f;
 };
 
 }  // anonymous namespace
