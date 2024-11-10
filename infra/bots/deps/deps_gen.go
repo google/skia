3333292a62c14231e67c6aac0940ad1243c0c081
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2024-11-10 12:10:07 +0000

    Manual roll ANGLE from 2a61126bb36c to 7fea539cc99b (6 revisions)
    Manual roll requested by robertphillips@google.com
    
    https://chromium.googlesource.com/angle/angle.git/+log/2a61126bb36c..7fea539cc99b
    
    2024-11-09 abdolrashidi@google.com Vulkan: Remove extra non-conformant flag checks
    2024-11-08 hocheung@chromium.org [code health] Remove underscores from test names in ANGLE (1/N)
    2024-11-08 i.nazarov@samsung.com Revert "spirv::Print without ANGLE_ENABLE_ASSERTS -> compile error"
    2024-11-08 yuxinhu@google.com Skip KHR-GLES31.core.tessellation_shader.single.primitive* on win
    2024-11-08 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 12b75c58255f to 12e843b4aad1 (4 revisions)
    2024-11-08 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from 369ac471ea04 to 4201d3dd229a (705 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC brettos@google.com,jmadill@google.com,robertphillips@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Debian10-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-AlphaR2-GPU-RadeonR9M470X-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC5i7RYH-GPU-IntelIris6100-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC6i5SYK-GPU-IntelIris540-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC8i5BEK-GPU-IntelIris655-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUCD34010WYKH-GPU-IntelHD4400-x86_64-Debug-All-ANGLE
    Tbr: brettos@google.com,robertphillips@google.com
    Change-Id: Ie827b417294df14d4429d5998f6c7d72d292581c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/918231
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -29,7 +29,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "2a61126bb36c30202b59c50ad060f72c26a67216",
+		Version: "7fea539cc99bed8fd315cfbc5026952a133ac3ae",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
