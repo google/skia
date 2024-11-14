2e189b519997ca9b24847c7d58f06d75d0b72012
Eric Boren <borenet@google.com>
2024-11-14 20:11:03 +0000

    [infra] Support running Android tests on hosts other than RPI
    Currently there's an assumption that we need to use the linux-arm64 cpython3 package when running Android tasks.  This is not the only assumption in gen_tasks_logic, but it appears to be the only blocker for the lab migration.
    
    Bug: b/378663110
    Change-Id: Iab90467add3afa9a275abab45403c8ce4548a8ce
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/918756
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>

--- infra/bots/gen_tasks_logic/task_builder.go
+++ infra/bots/gen_tasks_logic/task_builder.go
@@ -394,9 +394,21 @@ func (b *taskBuilder) cipdPlatform() string {
 
 // usesPython adds attributes to tasks which use python.
 func (b *taskBuilder) usesPython() {
-	pythonPkgs, ok := cipd.PkgsPython[b.cipdPlatform()]
+	// This is sort of a hack to work around the fact that the upstream CIPD
+	// package definitions separate out the platforms for some packages, which
+	// causes some complications when we don't know which platform we're going
+	// to run on, for example Android tasks which may run on RPI in our lab or
+	// on a Linux server elsewhere. Just grab an arbitrary set of Python
+	// packages and then replace the fixed platform with the `${platform}`
+	// placeholder. This does introduce the possibility of failure in cases
+	// where the package does not exist at a given tag for a given platform.
+	fakePlatform := cipd.PlatformLinuxAmd64
+	pythonPkgs, ok := cipd.PkgsPython[fakePlatform]
 	if !ok {
-		panic("No Python packages for platform " + b.cipdPlatform())
+		panic("No Python packages for platform " + fakePlatform)
+	}
+	for _, pkg := range pythonPkgs {
+		pkg.Name = strings.Replace(pkg.Name, fakePlatform, "${platform}", 1)
 	}
 	b.cipd(pythonPkgs...)
 	b.addToPATH(
@@ -462,6 +474,8 @@ func (b *taskBuilder) goPlatform() (string, string) {
 		arch = "amd64"
 	} else if b.matchArch("Arm64") || b.matchBazelHost("on_rpi") || b.matchOs("Android", "ChromeOS", "iOS") {
 		// Tests on Android/ChromeOS/iOS are hosted on RPI.
+		// WARNING: This assumption is not necessarily true with Android devices
+		// hosted in other environments.
 		arch = "arm64"
 	} else if b.isLinux() || b.isMac() || b.isWindows() {
 		arch = "amd64"
