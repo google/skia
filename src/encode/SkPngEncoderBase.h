bb7aebebaddd932e537ec44d96f1b8af4c6e6057
Lukasz Anforowicz <lukasza@chromium.org>
2024-12-06 15:10:13 +0000

    Reapply "[rust png] Extract `SkPngEncoderBase::onEncodeRows`."
    This reverts commit ccfe075e1eb8dad8a32abcacf9f5940fa0f9fdcf.  This is
    effectively a reland of http://review.skia.org/923336 that also includes
    a Bazel fix from http://review.skia.org/927418.
    
    Original CL description follows:
    
    This CL extracts some `src/encode/SkPngEncoderImpl.cpp` code and moves
    it into a reusable `src/encode/SkPngEncoderBase.h/.cpp` (continuing the
    effort from http://review.skia.org/922676).  In particular,
    `SkPngEncoderImpl::onEncodeRows` has been moved into
    `SkPngEncoderBase::onEncodeRows` and the `libpng`-specific parts have
    been abstracted away into `onEncodeRow` and `onFinishEncode` virtual
    methods.  The motivation is the desire to reuse this code from
    `SkPngRustEncoderImpl` in a future CL.
    
    Bug: chromium:379312510
    Bug: b/382576498
    Change-Id: I23214cfbebf4d01d217a01649f9a28578544fca7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/927457
    Reviewed-by: Jim Van Verth <jvanverth@google.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@google.com>

--- src/encode/SkPngEncoderBase.h
+++ src/encode/SkPngEncoderBase.h
@@ -7,31 +7,58 @@
 #ifndef SkPngEncoderBase_DEFINED
 #define SkPngEncoderBase_DEFINED
 
-#include <optional>
-#include <utility>
+#include <cstddef>
+#include <cstdint>
 
+#include <optional>
+
+#include "include/encode/SkEncoder.h"
+#include "include/private/SkEncodedInfo.h"
 #include "src/encode/SkImageEncoderFns.h"
 
-struct SkEncodedInfo;
 struct SkImageInfo;
+class SkPixmap;
+template <typename T> class SkSpan;
 
-// This namespace implements functionality shared between `SkPngEncoderImpl` and
+// This class implements functionality shared between `SkPngEncoderImpl` and
 // `SkPngRustEncoderImpl` (the latter is from `experimental/rust_png`).
-namespace SkPngEncoderBase {
+class SkPngEncoderBase : public SkEncoder {
+public:
+    struct TargetInfo {
+        SkEncodedInfo fDstInfo;
+        transform_scanline_proc fTransformProc;
+        size_t fDstRowSize;
+    };
 
-// Gets the `SkEncodedInfo` that `srcInfo` should be converted into before
-// encoding and a `transform_scanline_proc` that can transform source rows into
-// ready-to-encode rows.
-//
-// For example, `kRGBA_F32_SkColorType` source will be encoded as
-// `SkEncodedInfo::kRGBA_Color` with 16 `bitsPerComponent`.  Depending on
-// `src`'s alpha type, such transformation can be handled by either
-// `transform_scanline_F32` or `transform_scanline_F32_premul`.
-//
-// Returns `std::nullopt` if `srcInfo` is not supported by the PNG encoder.
-std::optional<std::pair<SkEncodedInfo, transform_scanline_proc>> getTargetInfo(
-        const SkImageInfo& srcInfo);
+    // Gets the `fDstInfo` that `srcInfo` should be converted into before
+    // encoding and a `fTransformProc` that can transform source rows into
+    // ready-to-encode rows (and the `fDstRowSize` of such rows).
+    //
+    // For example, `kRGBA_F32_SkColorType` source will be encoded as
+    // `SkEncodedInfo::kRGBA_Color` with 16 `bitsPerComponent`.  Depending on
+    // `src`'s alpha type, such transformation can be handled by either
+    // `transform_scanline_F32` or `transform_scanline_F32_premul`.
+    //
+    // Returns `std::nullopt` if `srcInfo` is not supported by the PNG encoder.
+    static std::optional<TargetInfo> getTargetInfo(const SkImageInfo& srcInfo);
 
-}  // namespace SkPngEncoderBase
+protected:
+    SkPngEncoderBase(TargetInfo targetInfo, const SkPixmap& src);
+
+    // SkEncoder override:
+    bool onEncodeRows(int numRows) final;
+
+    // Called from `onEncodeRows` to encode the given `row` (in `dstInfo` format
+    // that was passed to the `SkPngEncoderBase`'s constructor).
+    virtual bool onEncodeRow(SkSpan<const uint8_t> row) = 0;
+
+    // Called from `onEncodeRows` to finalize the encoded PNG (e.g. write the
+    // `IEND` chunk).
+    virtual bool onFinishEncoding() = 0;
+
+private:
+    TargetInfo fTargetInfo;
+    bool fFinishedEncoding = false;
+};
 
 #endif  // SkPngEncoderBase_DEFINED
