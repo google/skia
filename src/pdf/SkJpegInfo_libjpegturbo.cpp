21ebe346f05c9a9f89b74713e2c5251e1aa38e56
Kaylee Lubick <kjlubick@google.com>
2024-12-11 14:53:12 -0500

    Remove old, empty SkJpegInfo files
    These were made empty in http://review.skia.org/784553 so we should
    be able to delete them now.
    
    Change-Id: Ie87f6dafe40b921e79dafe131d7da3df72735b37
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/928818
    Reviewed-by: Max Hudnell <maxhudnell@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>

--- src/pdf/SkJpegInfo_libjpegturbo.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2015 Google Inc.
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- *
- * This file can parse information from a JPEG using libjpeg (which must be compiled in).
- */
-
