ff5d7b5c72905bac4c3fb81bea58bfaf7c4dba3f
Kaylee Lubick <kjlubick@google.com>
2024-12-18 16:35:36 -0500

    Remove slow path for SkBitmapDevice::drawRRect
    Chromium had been using this path that does not use the
    nine-patch optimization [1] for 11 years [2].
    
    After https://crrev.com/c/6335820, they no longer are setting
    this define (and seeing drawing speeds for blurred axis-aligned
    rrects improve by up to 90%) so we can delete this code.
    
    [1] http://review.skia.org/953496 (adds docs and fixes)
    [2] https://codereview.chromium.org/68773005/
    
    Change-Id: Icde77331cf6883edd49a5cbce99aeafdc7126353
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/931376
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>

--- src/core/SkBitmapDevice.cpp
+++ src/core/SkBitmapDevice.cpp
@@ -353,19 +353,11 @@ void SkBitmapDevice::drawRect(const SkRect& r, const SkPaint& paint) {
 }
 
 void SkBitmapDevice::drawOval(const SkRect& oval, const SkPaint& paint) {
-    // call the VIRTUAL version, so any subclasses who do handle drawPath aren't
-    // required to override drawOval.
     this->drawPath(SkPath::Oval(oval), paint, true);
 }
 
 void SkBitmapDevice::drawRRect(const SkRRect& rrect, const SkPaint& paint) {
-#ifdef SK_IGNORE_BLURRED_RRECT_OPT
-    // call the VIRTUAL version, so any subclasses who do handle drawPath aren't
-    // required to override drawRRect.
-    this->drawPath(SkPath::RRect(rrect), paint, true);
-#else
     LOOP_TILER( drawRRect(rrect, paint), Bounder(rrect.getBounds(), paint))
-#endif
 }
 
 void SkBitmapDevice::drawPath(const SkPath& path,
