42e38a8610c0b01fb76574b6f44864f32f06ea09
Michael Ludwig <michaelludwig@google.com>
2025-01-07 09:51:54 -0500

    [graphite] Consolidate ycbcr sampler handling in Dawn backend
    Change-Id: Ie3fad58fa7fa3257cbf1301d96fed74f77e753e4
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/934377
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>

--- src/gpu/graphite/dawn/DawnGraphiteUtilsPriv.h
+++ src/gpu/graphite/dawn/DawnGraphiteUtilsPriv.h
@@ -41,53 +41,16 @@ bool DawnCompileWGSLShaderModule(const DawnSharedContext* sharedContext,
                                  ShaderErrorHandler*);
 
 #if !defined(__EMSCRIPTEN__)
-namespace ycbcrUtils {
 
 bool DawnDescriptorIsValid(const wgpu::YCbCrVkDescriptor&);
 
 bool DawnDescriptorUsesExternalFormat(const wgpu::YCbCrVkDescriptor&);
 
-static constexpr int kUsesExternalFormatBits  = 1;
-static constexpr int kYcbcrModelBits          = 3;
-static constexpr int kYcbcrRangeBits          = 1;
-static constexpr int kXChromaOffsetBits       = 1;
-static constexpr int kYChromaOffsetBits       = 1;
-// wgpu::FilterMode contains Undefined/Nearest/Linear entries (Linear is 2).
-static constexpr int kChromaFilterBits        = 2;
-static constexpr int kForceExplicitReconBits  = 1;
-static constexpr int kComponentBits           = 3;
+bool DawnDescriptorsAreEquivalent(const wgpu::YCbCrVkDescriptor&, const wgpu::YCbCrVkDescriptor&);
 
-static constexpr int kUsesExternalFormatShift = 0;
-static constexpr int kYcbcrModelShift         = kUsesExternalFormatShift + kUsesExternalFormatBits;
-static constexpr int kYcbcrRangeShift         = kYcbcrModelShift         + kYcbcrModelBits;
-static constexpr int kXChromaOffsetShift      = kYcbcrRangeShift         + kYcbcrRangeBits;
-static constexpr int kYChromaOffsetShift      = kXChromaOffsetShift      + kXChromaOffsetBits;
-static constexpr int kChromaFilterShift       = kYChromaOffsetShift      + kYChromaOffsetBits;
-static constexpr int kForceExplicitReconShift = kChromaFilterShift       + kChromaFilterBits;
-static constexpr int kComponentRShift         = kForceExplicitReconShift + kForceExplicitReconBits;
-static constexpr int kComponentGShift         = kComponentRShift         + kComponentBits;
-static constexpr int kComponentBShift         = kComponentGShift         + kComponentBits;
-static constexpr int kComponentAShift         = kComponentBShift         + kComponentBits;
+ImmutableSamplerInfo DawnDescriptorToImmutableSamplerInfo(const wgpu::YCbCrVkDescriptor&);
+wgpu::YCbCrVkDescriptor DawnDescriptorFromImmutableSamplerInfo(ImmutableSamplerInfo);
 
-static constexpr uint32_t kUseExternalFormatMask =
-        ((1 << kUsesExternalFormatBits) - 1) << kUsesExternalFormatShift;
-static constexpr uint32_t kYcbcrModelMask =
-        ((1 << kYcbcrModelBits) - 1) << kYcbcrModelShift;
-static constexpr uint32_t kYcbcrRangeMask =
-        ((1 << kYcbcrRangeBits) - 1) << kYcbcrRangeShift;
-static constexpr uint32_t kXChromaOffsetMask =
-        ((1 << kXChromaOffsetBits) - 1) << kXChromaOffsetShift;
-static constexpr uint32_t kYChromaOffsetMask =
-        ((1 << kYChromaOffsetBits) - 1) << kYChromaOffsetShift;
-static constexpr uint32_t kChromaFilterMask =
-        ((1 << kChromaFilterBits) - 1) << kChromaFilterShift;
-static constexpr uint32_t kForceExplicitReconMask =
-        ((1 << kForceExplicitReconBits) - 1) << kForceExplicitReconShift;
-static constexpr uint32_t kComponentRMask = ((1 << kComponentBits) - 1) << kComponentRShift;
-static constexpr uint32_t kComponentBMask = ((1 << kComponentBits) - 1) << kComponentBShift;
-static constexpr uint32_t kComponentGMask = ((1 << kComponentBits) - 1) << kComponentGShift;
-static constexpr uint32_t kComponentAMask = ((1 << kComponentBits) - 1) << kComponentAShift;
-} // namespace ycbcrUtils
 #endif // !defined(__EMSCRIPTEN__)
 
 } // namespace skgpu::graphite
