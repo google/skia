3013d6d3f0641ec4e6de9c2c0cefa4b4ac5f4e67
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-01-24 08:27:33 -0800

    Roll skcms from 03700ab28aad to 12776964e26d (1 revision)
    https://skia.googlesource.com/skcms.git/+log/03700ab28aad..12776964e26d
    
    2025-01-24 jsimmons@google.com Decode color channels in 10101010_XR and 101010x_XR formats using formulas based on Apple documentation
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skcms-skia-autoroll
    Please CC kjlubick@google.com,robertphillips@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Canary-Chromium
    Tbr: kjlubick@google.com,robertphillips@google.com
    Change-Id: Ic8c3342affb2c3cdd1438e2195fadaef18a1bba7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/941977
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- modules/skcms/src/Transform_inl.h
+++ modules/skcms/src/Transform_inl.h
@@ -904,24 +904,18 @@ STAGE(load_1010102, NoCtx) {
 }
 
 STAGE(load_101010x_XR, NoCtx) {
-    static constexpr float min = -0.752941f;
-    static constexpr float max = 1.25098f;
-    static constexpr float range = max - min;
     U32 rgba = load<U32>(src + 4*i);
-    r = cast<F>((rgba >>  0) & 0x3ff) * (1/1023.0f) * range + min;
-    g = cast<F>((rgba >> 10) & 0x3ff) * (1/1023.0f) * range + min;
-    b = cast<F>((rgba >> 20) & 0x3ff) * (1/1023.0f) * range + min;
+    r = cast<F>(((rgba >>  0) & 0x3ff) - 384) / 510.0f;
+    g = cast<F>(((rgba >> 10) & 0x3ff) - 384) / 510.0f;
+    b = cast<F>(((rgba >> 20) & 0x3ff) - 384) / 510.0f;
 }
 
 STAGE(load_10101010_XR, NoCtx) {
-    static constexpr float min = -0.752941f;
-    static constexpr float max = 1.25098f;
-    static constexpr float range = max - min;
     U64 rgba = load<U64>(src + 8 * i);
-    r = cast<F>((rgba >>  (0+6)) & 0x3ff) * (1/1023.0f) * range + min;
-    g = cast<F>((rgba >> (16+6)) & 0x3ff) * (1/1023.0f) * range + min;
-    b = cast<F>((rgba >> (32+6)) & 0x3ff) * (1/1023.0f) * range + min;
-    a = cast<F>((rgba >> (48+6)) & 0x3ff) * (1/1023.0f) * range + min;
+    r = cast<F>(((rgba >> ( 0+6)) & 0x3ff) - 384) / 510.0f;
+    g = cast<F>(((rgba >> (16+6)) & 0x3ff) - 384) / 510.0f;
+    b = cast<F>(((rgba >> (32+6)) & 0x3ff) - 384) / 510.0f;
+    a = cast<F>(((rgba >> (48+6)) & 0x3ff) - 384) / 510.0f;
 }
 
 STAGE(load_161616LE, NoCtx) {
@@ -1310,12 +1304,9 @@ FINAL_STAGE(store_8888, NoCtx) {
 }
 
 FINAL_STAGE(store_101010x_XR, NoCtx) {
-    static constexpr float min = -0.752941f;
-    static constexpr float max = 1.25098f;
-    static constexpr float range = max - min;
-    store(dst + 4*i, cast<U32>(to_fixed(((r - min) / range) * 1023)) <<  0
-                   | cast<U32>(to_fixed(((g - min) / range) * 1023)) << 10
-                   | cast<U32>(to_fixed(((b - min) / range) * 1023)) << 20);
+    store(dst + 4*i, cast<U32>(to_fixed((r * 510) + 384)) <<  0
+                   | cast<U32>(to_fixed((g * 510) + 384)) << 10
+                   | cast<U32>(to_fixed((b * 510) + 384)) << 20);
 }
 
 FINAL_STAGE(store_1010102, NoCtx) {
