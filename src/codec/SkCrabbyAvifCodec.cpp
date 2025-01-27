e3878596ccddfb3409b1b82754801977504ab5ab
Vignesh Venkatasubramanian <vigneshv@google.com>
2025-01-27 19:06:39 +0000

    SkCrabbyAvifCodec: Perform scaling after crop/subset
    There are three post-decode operations that we perform:
    Cropping, Scaling and Subset computation.
    
    The right order to perform these operations is:
    1) Cropping (as described by the CleanAperture property). This has to be
       the first step to ensure that we don't accidentally expose the
       non-cropped portions of the image to the subsequent operations.
    2) Subset computation (as requested by options.fSubset).
    3) Scaling (to match dstInfo.dimensions() if necessary). This has to be
       the last step to ensure that we never fill in more pixels than what
       is requested by dstInfo.dimensions().
    
    In the existing code, scaling is done first which is incorrect.
    
    Bug: b/390822777
    Change-Id: I4545e0b7cced4887c904d5c02f4e859bc8a7a011
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/941916
    Auto-Submit: Vignesh Venkat <vigneshv@google.com>
    Commit-Queue: Noelle Scobie <nscobie@google.com>
    Reviewed-by: Noelle Scobie <nscobie@google.com>

--- src/codec/SkCrabbyAvifCodec.cpp
+++ src/codec/SkCrabbyAvifCodec.cpp
@@ -374,29 +374,20 @@ SkCodec::Result SkCrabbyAvifCodec::onGetPixels(const SkImageInfo& dstInfo,
     if (fGainmapOnly && !fAvifDecoder->image->gainMap) {
         return kInvalidInput;
     }
+
+    // At this point we have the decoded image. Now we have to perform cropping, subset computation
+    // and scaling. The right order of these operations is:
+    // 1) Cropping (as described by the CleanAperture property). This has to be the first step to
+    //    ensure that we don't accidentally expose the non-cropped portions of the image to the
+    //    subsequent operations.
+    // 2) Subset computation (as requested by options.fSubset).
+    // 3) Scaling (to match dstInfo.dimensions() if necessary). This has to be the last step to
+    //    ensure that we never fill in more pixels than what is requested by dstInfo.dimensions().
+
     crabbyavif::avifImage* image =
             fGainmapOnly ? fAvifDecoder->image->gainMap->image : fAvifDecoder->image;
     using AvifImagePtr =
             std::unique_ptr<crabbyavif::avifImage, decltype(&crabbyavif::crabby_avifImageDestroy)>;
-
-    AvifImagePtr scaled_image{nullptr, crabbyavif::crabby_avifImageDestroy};
-    if (this->dimensions() != dstInfo.dimensions() && !options.fSubset) {
-        // |image| contains plane pointers which point to Android MediaCodec's buffers. Those
-        // buffers are read-only and hence we cannot scale in place. Make a copy of the image and
-        // scale the copied image.
-        scaled_image.reset(crabbyavif::crabby_avifImageCreateEmpty());
-        result = crabbyavif::crabby_avifImageCopy(
-            scaled_image.get(), image, crabbyavif::AVIF_PLANES_ALL);
-        if (result != crabbyavif::AVIF_RESULT_OK) {
-            return kInvalidInput;
-        }
-        image = scaled_image.get();
-        result = crabbyavif::avifImageScale(
-                image, dstInfo.width(), dstInfo.height(), &fAvifDecoder->diag);
-        if (result != crabbyavif::AVIF_RESULT_OK) {
-            return kInvalidInput;
-        }
-    }
 
     // cropped_image is a view into the underlying image. It can be safely deleted once the pixels
     // are converted into RGB (or when it goes out of scope in one of the error paths).
@@ -427,6 +418,25 @@ SkCodec::Result SkCrabbyAvifCodec::onGetPixels(const SkImageInfo& dstInfo,
             return kInvalidInput;
         }
         image = subset_image.get();
+    }
+
+    AvifImagePtr scaled_image{nullptr, crabbyavif::crabby_avifImageDestroy};
+    if (dstInfo.width() != image->width || dstInfo.height() != image->height) {
+        // |image| contains plane pointers which point to Android MediaCodec's buffers. Those
+        // buffers are read-only and hence we cannot scale in place. Make a copy of the image and
+        // scale the copied image.
+        scaled_image.reset(crabbyavif::crabby_avifImageCreateEmpty());
+        result = crabbyavif::crabby_avifImageCopy(
+            scaled_image.get(), image, crabbyavif::AVIF_PLANES_ALL);
+        if (result != crabbyavif::AVIF_RESULT_OK) {
+            return kInvalidInput;
+        }
+        image = scaled_image.get();
+        result = crabbyavif::avifImageScale(
+                image, dstInfo.width(), dstInfo.height(), &fAvifDecoder->diag);
+        if (result != crabbyavif::AVIF_RESULT_OK) {
+            return kInvalidInput;
+        }
     }
 
     crabbyavif::avifRGBImage rgbImage;
