371239eb12ff1b91014236c85ca7a2cccc64d083
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-01-28 20:01:26 -0800

    Roll ANGLE from 0878d8543f62 to f231d94b280f (9 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/0878d8543f62..f231d94b280f
    
    2025-01-28 cclao@google.com Vulkan: Add kBufferMemoryBarrierData to mirror image
    2025-01-28 a.annestrand@samsung.com CL/VK: Enable dispatch region chunking
    2025-01-28 a.annestrand@samsung.com CL/VK: Implementation of Compute Pipeline Cache.
    2025-01-28 geofflang@chromium.org WebGPU: Implement no-op gl_PointSize
    2025-01-28 romanl@google.com Clean up Galaxy expectations for A/S23 and S24 Exynos/QC
    2025-01-28 cnorthrop@google.com restricted_trace_perf: Support --fps-limit
    2025-01-28 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 7425f0646395 to ef9be82521e9 (20 revisions)
    2025-01-28 angle-autoroll@skia-public.iam.gserviceaccount.com Roll SwiftShader from a87fa39f6446 to 86cf34f50cbe (1 revision)
    2025-01-28 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from 44e2240f3d75 to 1ecb21e0f634 (630 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC fmalita@google.com,jmadill@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Debian10-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-AlphaR2-GPU-RadeonR9M470X-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC5i7RYH-GPU-IntelIris6100-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC6i5SYK-GPU-IntelIris540-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUCD34010WYKH-GPU-IntelHD4400-x86_64-Debug-All-ANGLE
    Tbr: fmalita@google.com
    Test: Test: restricted_trace_perf.py --fps-limit 30 ...
    Change-Id: I81f5d134750e3b5fbb233de120fabb2d0ee425a6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/943637
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "0878d8543f62a75c467a94d35da30ffcfb7636ec",
+		Version: "f231d94b280fc9117b8610fbefecf66cc3593155",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
