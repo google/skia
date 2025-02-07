7086ce945d7c11796249c01195a2210e154d5133
Kaylee Lubick <kjlubick@google.com>
2025-02-07 17:13:57 -0500

    Add util for dumping a buffer (e.g. mask)
    It is sometimes nice to visualize a buffer (e.g. an SkMask)
    without needing to draw it to a real SkCanvas.
    
    Example output:
    ```
      :#@@@@@@@@@@@@@#:
     +@@@@@@@@@@@@@@@@@+
    :@@@@@@@@@@@@@@@@@@@:
    %@@@@@@@@@@@@@@@@@@@%
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    @@@@@@@@@@@@@@@@@@@@@
    %@@@@@@@@@@@@@@@@@@@%
    :@@@@@@@@@@@@@@@@@@@:
     +@@@@@@@@@@@@@@@@@+
      .*#@@@@@@@@@@@#*.
    ```
    
    Change-Id: Ie137aeb163ed6dea593266c290eaee3383d307fb
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/948936
    Commit-Queue: Nicolette Prevost <nicolettep@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>

--- src/core/SkDebugUtils.h
+++ src/core/SkDebugUtils.h
@@ -8,7 +8,14 @@
 #ifndef SkDebugUtils_DEFINED
 #define SkDebugUtils_DEFINED
 
+#include "include/core/SkString.h"
 #include "include/core/SkTileMode.h"
+#include "include/private/base/SkAssert.h"
+#include "include/private/base/SkDebug.h"
+#include "include/private/base/SkTPin.h"
+
+#include <cstdint>
+#include <string>
 
 static constexpr const char* SkTileModeToStr(SkTileMode tm) {
     switch (tm) {
@@ -19,5 +26,27 @@ static constexpr const char* SkTileModeToStr(SkTileMode tm) {
     }
     SkUNREACHABLE;
 }
+
+#if defined(SK_DEBUG)
+inline void SkDumpBuffer(uint8_t const* const buffer, int w, int h, int rowBytes) {
+    SkASSERT(buffer);
+
+    const std::string shades = " .:-=+*%#@";
+
+    for (int y = 0; y < h; ++y) {
+        SkString line;
+        for (int x = 0; x < w; ++x) {
+            uint8_t pixelValue = buffer[y * rowBytes + x];
+            int idx = static_cast<int>(pixelValue * shades.length() / 256);
+            idx = SkTPin(idx, 0, (int)shades.length() - 1);
+            line += shades[idx];
+        }
+        SkDebugf("%s\n", line.c_str());
+    }
+}
+#else
+inline void SkDumpBuffer(uint8_t*, int, int, int) {}
+#endif
+
 
 #endif // SkDebugUtils_DEFINED
