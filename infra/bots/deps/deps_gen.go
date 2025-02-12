f31c733c86c42fb86675d8872f5b5b59e1b422d8
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-02-11 20:01:35 -0800

    Roll SwiftShader from 5017ce3c800e to 720189cae847 (1 revision)
    https://swiftshader.googlesource.com/SwiftShader.git/+log/5017ce3c800e..720189cae847
    
    2025-02-10 swiftshader.regress@gmail.com Regres: Update test lists @ 5017ce3c
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/swiftshader-skia-autoroll
    Please CC bsalomon@google.com,jamesgk@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in SwiftShader: https://bugs.chromium.org/p/swiftshader/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Test-Debian10-Clang-GCE-GPU-SwiftShader-x86_64-Debug-All-SwiftShader
    Bug: None
    Tbr: jamesgk@google.com
    Change-Id: Ia6bf5603d270bbab67b21a76ec874165bfd887ab
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/950432
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -249,7 +249,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"swiftshader.googlesource.com/SwiftShader": {
 		Id:      "swiftshader.googlesource.com/SwiftShader",
-		Version: "5017ce3c800e1410da6a3257098a8370591de5fa",
+		Version: "720189cae8478484f51531a9eed9abc6d8635271",
 		Path:    "third_party/externals/swiftshader",
 	},
 }
