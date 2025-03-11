d1c5490ca749e79171704d35f4c6ff8c829eab69
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-03-10 21:01:36 -0700

    Roll ANGLE from 6c2737be88ac to f38a081cdea4 (6 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/6c2737be88ac..f38a081cdea4
    
    2025-03-10 mpdenton@chromium.org WGPU: when copying to mip levels, copy correct size
    2025-03-10 lexa.knyazev@gmail.com D3D11: Set SV_Position interpolation for conservative depth
    2025-03-10 xinyu.fang@arm.com Add validation of invalid parameters for certain APIs
    2025-03-10 jwata@google.com infra: Add $build/siso properties to builders
    2025-03-10 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from adee401e9cba to b65ddf3969c1 (6 revisions)
    2025-03-10 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from c86846c05457 to 546a0c348066 (804 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC danieldilan@google.com,jmadill@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Debian10-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-AlphaR2-GPU-RadeonR9M470X-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUC6i5SYK-GPU-IntelIris540-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win10-Clang-NUCD34010WYKH-GPU-IntelHD4400-x86_64-Debug-All-ANGLE
    Tbr: danieldilan@google.com
    Change-Id: Ic180eff867acc8f46f7b1c1b1679c0f24d320a9c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/961660
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "6c2737be88ac40f63a258c30e6a68d29feca0b1f",
+		Version: "f38a081cdea49a85c592a7261fcb3e754c039418",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
