296e2ed38a5bc364b8aad2392e113e4d0a3a8c00
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-05-07 21:01:20 -0700

    Roll ANGLE from b373892b9d96 to df8a704788de (10 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/b373892b9d96..df8a704788de
    
    2025-05-08 lexa.knyazev@gmail.com Fix duplicate extension commands autogen
    2025-05-07 sunnyps@chromium.org metal: Synchronize imported IOSurfaces with command buffer
    2025-05-07 agrieve@chromium.org Use a depfile rather than exec_script to list inputs for commit_id.py
    2025-05-07 agrieve@google.com Don't use exec_script() to get versionCode when build_with_chromium
    2025-05-07 mavis.deng@arm.com Return GL_INVALID_ENUM for invalid target of CopyImageSubData
    2025-05-07 lexa.knyazev@gmail.com Autogen support checks for robust client memory entry points
    2025-05-07 neil.zhang@arm.com Fix glValidateProgram() not work as spec
    2025-05-07 lexa.knyazev@gmail.com Autogen support checks for PLS entry points
    2025-05-07 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 82f44a3a6738 to 645c800a7790 (7 revisions)
    2025-05-07 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from a753f6d4bddd to 4d44e5915a7e (674 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC jmadill@google.com,robertphillips@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: robertphillips@google.com
    Change-Id: I002bb75b34b0620908e051e9f24c68b461274f72
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/991436
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "b373892b9d961a75586ca1abd3edee59950e6b8e",
+		Version: "df8a704788de9552f286186950611def9bc9c332",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
