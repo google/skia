7daef46b59b5441636d3855a1f24e60913ff45df
Kaylee Lubick <kjlubick@google.com>
2025-05-30 08:58:36 -0400

    Make CodeSize.+NoPatch ignore symlinks that don't exist
    Change-Id: If79edda012da52316c7b8a467f7454cf4c4bc50a
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1000977
    Reviewed-by: Eric Boren <borenet@google.com>

--- infra/bots/recipe_modules/infra/resources/copytree.py
+++ infra/bots/recipe_modules/infra/resources/copytree.py
@@ -6,4 +6,4 @@
 import shutil
 import sys
 
-shutil.copytree(sys.argv[1], sys.argv[2], dirs_exist_ok=True)
+shutil.copytree(sys.argv[1], sys.argv[2], dirs_exist_ok=True, ignore_dangling_symlinks=True)
