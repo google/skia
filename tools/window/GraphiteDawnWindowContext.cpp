11d3a15350dc4ade06213bf39b9bf66f51b5ce89
Kai Ninomiya <kainino@chromium.org>
2025-06-23 19:17:40 -0700

    [graphite] Handle Dawn breaking change in WGPUInstanceDescriptor
    Needed in order to update Dawn to match the standard webgpu.h, here:
    https://dawn-review.googlesource.com/c/dawn/+/245242
    
    Bug: chromium:414868137
    Change-Id: Ibf65ffc68246a238e41daa5b1f3cf784c0bd93fb
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1011536
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- tools/window/GraphiteDawnWindowContext.cpp
+++ tools/window/GraphiteDawnWindowContext.cpp
@@ -32,7 +32,13 @@ GraphiteDawnWindowContext::GraphiteDawnWindowContext(std::unique_ptr<const Displ
         : WindowContext(std::move(params)), fSurfaceFormat(surfaceFormat) {
     wgpu::InstanceDescriptor desc{};
     // need for WaitAny with timeout > 0
+#ifdef WGPU_BREAKING_CHANGE_INSTANCE_FEATURES_LIMITS
+    static const auto kTimedWaitAny = wgpu::InstanceFeatureName::TimedWaitAny;
+    desc.requiredFeatureCount = 1;
+    desc.requiredFeatures = &kTimedWaitAny;
+#else
     desc.capabilities.timedWaitAnyEnable = true;
+#endif
     fInstance = std::make_unique<dawn::native::Instance>(&desc);
 }
 
