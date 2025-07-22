626ab317e2679f0e8d63d78e8a8654e22f954227
Kaylee Lubick <kjlubick@google.com>
2025-07-22 16:35:06 -0400

    Fix make_image_tag.sh
    Follow-up to https://review.skia.org/1024996
    
    Change-Id: I97b3acf9357986ffbbf44e9a38d8d1a5aaaf260f
    Bug: b/367315534
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1025317
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- infra/docker/make_image_tag.sh
+++ infra/docker/make_image_tag.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Copyright 2025 Google LLC
 #
-# Build a tag for the Docker image based on the current datetime, user, and
-# git revision.
+# Build a tag for the Docker image based on the current git revision.
+# Stage Manager cares about the format of this when determining which
+# version to deploy or not.
 
 set -e
+
+HASH=`git rev-parse HEAD`
 
 echo "git-${HASH}"
