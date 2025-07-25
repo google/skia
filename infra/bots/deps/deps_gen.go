61d292f6920ac48fa051461ca7527b3da35a3a70
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-07-24 21:01:34 -0700

    Roll Dawn from b6b32cb6d1a1 to 3d2423f23524 (14 revisions)
    https://dawn.googlesource.com/dawn.git/+log/b6b32cb6d1a1..3d2423f23524
    
    2025-07-25 dawn-autoroll@skia-public.iam.gserviceaccount.com Roll DirectX Shader Compiler from d64d34ced89e to 97b5edbc4398 (1 revision)
    2025-07-25 hao.x.li@intel.com Workaround for D3D12 BeginRenderPass issue on Intel ACM/ARL
    2025-07-25 jrprice@google.com [tint] Add string type
    2025-07-25 rop@google.com Remove dupe dependency: vulkan-deps
    2025-07-24 bajones@chromium.org Expose PrimitiveID feature in Dawn
    2025-07-24 dsinclair@chromium.org Fix formatting for extension.
    2025-07-24 jrprice@google.com [wgsl] Add `print(<expr>)` builtin
    2025-07-24 jrprice@google.com [wgsl] Add chromium_print language feature
    2025-07-24 lehoangquyen@chromium.org Refactor WaitForQueueSerial to eliminate race condition
    2025-07-24 cwallez@chromium.org [dawn][opengl] Add for binding_array<texture> builtin emulation.
    2025-07-24 shanxing.mei@intel.com Add e2e tests for Tier1 read-only storage texture access
    2025-07-24 dawn-autoroll@skia-public.iam.gserviceaccount.com Roll SwiftShader from b347398f5e86 to a84d1801cf7b (1 revision)
    2025-07-24 dawn-autoroll@skia-public.iam.gserviceaccount.com Roll DirectX Shader Compiler from c78ed99263d4 to d64d34ced89e (1 revision)
    2025-07-24 dawn-autoroll@skia-public.iam.gserviceaccount.com Roll ANGLE from 1c245d908ff3 to f94510ab8d9e (5 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/dawn-skia-autoroll
    Please CC cwallez@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Dawn: https://bugs.chromium.org/p/dawn/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Debug-Graphite_Dawn_Vulkan;skia/skia.primary:Test-Mac14-Clang-MacMini9.1-GPU-AppleM1-arm64-Debug-All-ASAN_Graphite_Dawn_Metal;skia/skia.primary:Test-Win10-MSVC-Golo-GPU-QuadroP400-x86_64-Release-All-Graphite_Dawn_D3D12;skia/skia.primary:Test-Win10-MSVC-Golo-GPU-QuadroP400-x86_64-Release-All-Graphite_Dawn_D3D11
    Bug: None
    Tbr: cwallez@google.com
    Change-Id: I094bf4a0e4855d90f111037e2c4296dec327c8c6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1026997
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -154,7 +154,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"dawn.googlesource.com/dawn": {
 		Id:      "dawn.googlesource.com/dawn",
-		Version: "b6b32cb6d1a14c9b1cd442ae13d9183b27399019",
+		Version: "3d2423f23524ab9dd6bd7bc23a3030f23f30caa5",
 		Path:    "third_party/externals/dawn",
 	},
 	"infra/3pp/tools/ninja": {
