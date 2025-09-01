a09476c6e3ef5936b1950c49806b10193c1446d0
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-09-01 09:58:29 -0700

    Roll vulkan-deps from 416838cab1f8 to f55240464c0e (3 revisions)
    https://chromium.googlesource.com/vulkan-deps.git/+log/416838cab1f8..f55240464c0e
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/vulkan-deps-skia-autoroll
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE
    Bug: None
    Tbr: kjlubick@google.com
    Change-Id: Iadf6b52fc998310787e1d03397cd1bc61fa9a4c3
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1048970
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -144,7 +144,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/vulkan-deps": {
 		Id:      "chromium.googlesource.com/vulkan-deps",
-		Version: "416838cab1f81afd26d18a561dfc29431fbb5adb",
+		Version: "f55240464c0e7ad9fa42130618a2e0532a44c3b3",
 		Path:    "third_party/externals/vulkan-deps",
 	},
 	"chromium.googlesource.com/webm/libwebp": {
