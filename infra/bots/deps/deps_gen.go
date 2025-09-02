15f2139e8eb1d114cfa2c863b567417dab028261
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-09-01 21:01:38 -0700

    Roll ANGLE from 1e3e4eb7f98c to bb55ea10fcef (3 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/1e3e4eb7f98c..bb55ea10fcef
    
    2025-09-01 cnorthrop@google.com Tests: Add Minecraft Vibrant Visuals trace
    2025-09-01 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from 659c6d688f03 to ddfbb7d832f0 (842 revisions)
    2025-09-01 angle-autoroll@skia-public.iam.gserviceaccount.com Roll VK-GL-CTS from 3718ed21d713 to 15469c3d0039 (15 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC jmadill@google.com,kjlubick@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: kjlubick@google.com
    Test: Test: angle_trace_tests --gtest_filter=*minecraft_vibrant_visuals
    Change-Id: Ib3a813aa1a90b44d3bf98a5e9c98b436bafde085
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1049004
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "1e3e4eb7f98c05e92a113524eeffb5357f68f506",
+		Version: "bb55ea10fcef3759e4db7ef8a473a9ceac2c6aa6",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
