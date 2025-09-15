c69f6234783b9c2a1e304bed3a1187cb1030ad30
Kaylee Lubick <kjlubick@google.com>
2025-09-15 19:00:36 +0000

    Assume LazyTexture images have successfully been pinned.
    Inspired by a patch from caihui yang [1].
    
    In SkImage_AndroidFactories.cpp, `DeferredFromAHardwareBuffer`
    creates one of these LazyTexture images via
    `DeferredFromTextureGenerator`. If these are passed into
    `PinAsTexture`, we can return true because the data in the image
    is already in the GPU cache (or can be there quickly). If we
    return false, then [2] fails and we do unnecessary work to
    unpin the other images (evicting them from the cache) and then
    not using the pin shortcut to do work ahead of time.
    
    [1] https://skia-review.googlesource.com/c/skia/+/1054976
    [2] http://ac/frameworks/base/libs/hwui/pipeline/skia/SkiaDisplayList.cpp?l=100-105
    
    Change-Id: I58b73e508420bd517ed99fa3a9654cafd27bb580
    Bug: b/444322854
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1055656
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: John Reck <jreck@google.com>

--- src/gpu/ganesh/image/SkImage_LazyTexture.h
+++ src/gpu/ganesh/image/SkImage_LazyTexture.h
@@ -17,11 +17,23 @@ class SkPixmap;
 class SkRecorder;
 struct SkIRect;
 
+/**
+ * SkImage_LazyTexture is a GPU-backed SkImage that defers the creation of its backing texture
+ * until it is actually required for a draw. It is instantiated with a GrTextureGenerator, which is
+ * a factory object that knows how to create the texture on demand.
+ *
+ * This class is the concrete implementation for SkImages created from platform-specific objects
+ * like Android's AHardwareBuffer. In that case, the generator wraps the hardware buffer and the
+ * "generation" of the texture is simply the act of importing that buffer into the graphics API
+ * (e.g., Vulkan or GL).
+ */
 class SkImage_LazyTexture final : public SkImage_Lazy {
 public:
     SkImage_LazyTexture(SkImage_Lazy::Validator *validator) : SkImage_Lazy(validator) {}
 
     bool readPixelsProxy(GrDirectContext*, const SkPixmap&) const override;
+
+    SkImage_Base::Type type() const override { return SkImage_Base::Type::kLazyTexture; }
 
     sk_sp<SkImage> onMakeSubset(SkRecorder*, const SkIRect&, RequiredProperties) const override;
 };
