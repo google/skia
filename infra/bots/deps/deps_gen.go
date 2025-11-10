03106e44499dd693c4c38f565f38811a53b55354
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-11-09 20:01:50 -0800

    Roll ANGLE from eea1fcf95a58 to cc7e0e04887e (5 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/eea1fcf95a58..cc7e0e04887e
    
    2025-11-08 roberto_rodriguez2@apple.com Clamp up to base level in Metal backend
    2025-11-08 syoussefi@chromium.org Port needs-#extension compiler tests to end2end
    2025-11-07 angle-autoroll@skia-public.iam.gserviceaccount.com Roll SwiftShader from 153470c12fdf to f474b0ce14a6 (2 revisions)
    2025-11-07 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 2599d7d26547 to a34bdd1b0084 (7 revisions)
    2025-11-07 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from befe71267627 to a8efe4138221 (535 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC bungeman@google.com,jmadill@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: bungeman@google.com
    Change-Id: Ifc72c972b0d25a6fe3abf5820099f3dbc27567ac
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1096998
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "eea1fcf95a58e1217012e344cc10750144600591",
+		Version: "cc7e0e04887ef2b007bce363cc700041d87d187a",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
