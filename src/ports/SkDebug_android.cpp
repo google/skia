7da0103004073208ad427e6f2eb202d2c65a67a6
Noelle Scobie <nscobie@google.com>
2025-12-09 21:58:36 +0000

    Make SkDebugf calls log at Android's WARN level in RenderEngine
    Change-Id: I678b973ecc9475a70cbad67abc54b38ea4b44fbe
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1121498
    Auto-Submit: Noelle Scobie <nscobie@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>

--- src/ports/SkDebug_android.cpp
+++ src/ports/SkDebug_android.cpp
@@ -33,7 +33,23 @@ void SkDebugf(const char format[], ...) {
         fflush(stdout);
     }
 
+    // Calls to SkDebugf in Android's SurfaceFligner / RenderEngine are often critical for
+    // investigating high-priority issues, even for production builds. This temporary hack forces
+    // all Skia logging in RenderEngine to the WARN level, which ensures easier filtering in bug
+    // reports, and anecdotally may also reduce the chance of dropping logs (TBD). HWUI and other
+    // non-framework builds of Skia are currently left at DEBUG to de-risk potential log spam.
+    //
+    // TODO: consider printing to logcat's "system" log buffer instead of the app-available "main"
+    // buffer when Skia is used in SurfaceFlinger. This would require turning SkDebugf (or its
+    // replacement) into a variadic templated function to call __android_log_buf_print, or for
+    // Android's liblog to expose its internal __android_log_buf_vprint, which accepts a va_list.
+    // The "system" buffer would only be available when RenderEngine is built into SurfaceFlinger,
+    // and could not be used in e.g. tests, benchmarks, etc.
+#if defined(SK_IN_RENDERENGINE)
+    __android_log_vprint(ANDROID_LOG_WARN, LOG_TAG, format, args1);
+#else
     __android_log_vprint(ANDROID_LOG_DEBUG, LOG_TAG, format, args1);
+#endif  // defined(SK_IN_RENDERENGINE)
 
     va_end(args1);
 }
