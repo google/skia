b35e8691fdd077d6feb88e24cbf6e0505ea57a7e
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-12-09 20:01:48 -0800

    Roll ANGLE from 78bb5a33a9e9 to c2a048f9a1db (7 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/78bb5a33a9e9..c2a048f9a1db
    
    2025-12-10 cclao@google.com Vulkan: Expand skipped syncVal to include vkCmdEndRenderingKHR
    2025-12-10 aurora.zhang@arm.com glTexStorage* should reject paletted formats
    2025-12-09 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from 0c1ca61e9286 to 98e307118c58 (691 revisions)
    2025-12-09 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 53bfad22ed15 to 7d1d0f981647 (7 revisions)
    2025-12-09 angle-autoroll@skia-public.iam.gserviceaccount.com Roll SwiftShader from 518a9f63228d to ff4435d3f92d (2 revisions)
    2025-12-09 cnorthrop@google.com FrameCapture: Skip invalid IDs when tracking max resource
    2025-12-09 cclao@google.com Vulkan: Temporary remove the assertion to unblock roller
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC jmadill@google.com,kjlubick@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: kjlubick@google.com
    Test: Test: Pokemon Go combat
    Change-Id: Ic37e891fc812994d0be3ebfdd48f8a4c88753921
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1120783
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "78bb5a33a9e94f57b64a3e21111cbb96fb643e1c",
+		Version: "c2a048f9a1db1b8292c6112f356d86c869914aea",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
