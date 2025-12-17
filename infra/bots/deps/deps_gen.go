b1569739f431d0b0f974f639c9356b06400682e9
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-12-16 20:01:30 -0800

    Roll ANGLE from 1d38433509dc to 4f4f02fe5be1 (14 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/1d38433509dc..4f4f02fe5be1
    
    2025-12-17 tsepez@google.com Document and test current MemoryBuffer/ScratchBuffer behavior
    2025-12-17 angle-autoroll@skia-public.iam.gserviceaccount.com Manual roll vulkan-deps from 11b41a627ae1 to 6fb994b0e576 (2 revisions)
    2025-12-17 hocheung@chromium.org Replace erase(remove) idiom with c++20 std::erase[_if]()
    2025-12-17 cnorthrop@google.com Vulkan: Disable vertexInputDynamicState on PowerVR
    2025-12-16 angle-autoroll@skia-public.iam.gserviceaccount.com Manual roll vulkan-deps from 19e77f45335c to 11b41a627ae1 (5 revisions)
    2025-12-16 ynovikov@chromium.org Add mac-arm64-test to CQ
    2025-12-16 tsepez@google.com Make MemoryBuffer more compatible with angle::Span<>.
    2025-12-16 angle-autoroll@skia-public.iam.gserviceaccount.com Roll SwiftShader from ff4435d3f92d to 5aa3ef64770d (1 revision)
    2025-12-16 angle-autoroll@skia-public.iam.gserviceaccount.com Roll VK-GL-CTS from 10470174fc3a to 2e03601bd4ee (2 revisions)
    2025-12-16 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from 1cc108abdaf3 to 700e7b728472 (571 revisions)
    2025-12-16 hocheung@chromium.org Add an `std::erase_if` overload for `angle::FixedVector`
    2025-12-16 syoussefi@chromium.org IR: Port the RemoveUnusedFramebufferFetch transformation
    2025-12-16 yuxinhu@google.com Implement alignment methods for PackedSPIRVBlockEncoder
    2025-12-16 hocheung@chromium.org Avoid default framebuffer in flush thread
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC brettos@google.com,jmadill@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win10-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: brettos@google.com
    Test: Test: ANGLE trace tests
    Change-Id: I1cccdd1f0344d9442315bc2dc9792a08c2aa159d
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1126917
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "1d38433509dcac7913bd4e424d1d4bc2de216ec7",
+		Version: "4f4f02fe5be107d457a283c443b020fde28aecc1",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
