a471f253b9411b37f7771c92d8f3472038d1219d
Jorge Betancourt <jmbetancourt@google.com>
2026-02-06 14:38:22 -0500

    add placeholder files to fix the flutter roller
    These are not listed in our BUILD files and are only here to fix Flutter's build errors while we rename these files.
    
    https://github.com/flutter/flutter/pull/181980
    
    Change-Id: I1d7c827ad7adc286bc58d502acfc03c00efabd76
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1159619
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>
    Auto-Submit: Jorge Betancourt <jmbetancourt@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>

---
+++ src/ports/SkDebug_win.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/ports/SkLog_win.cpp"
