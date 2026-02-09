6a2cb751db413c1980d1f95993dce01bd40133f5
Jorge Betancourt <jmbetancourt@google.com>
2026-02-09 14:59:52 -0500

    remove legacy debug files
    Change-Id: Id815567b429a5340564df6797a3aedf24a488930
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1161596
    Auto-Submit: Jorge Betancourt <jmbetancourt@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>

--- src/ports/SkDebug_android.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2026 Google LLC
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-#include "src/ports/SkLog_android.cpp"
