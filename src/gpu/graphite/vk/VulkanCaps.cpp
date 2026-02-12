229f0cdc7ceacd12c7a31ae000bef86b7f75cb59
Sharadh Rajaraman <rsharadh@google.com>
2026-02-12 08:47:41 -0800

    Revert "Disable VK_EXT_rasterization_order_attachment_access on ARM"
    This reverts commit b302b797844f1d819052341946d00fe32f7c8c80.
    
    Reason for revert: Can no longer reproduce the memory regression on ARM devices. 
    
    Additionally, the driver doesn't do anything meaningful with the rasterization order access colour bit flags. 
    
    Original change's description:
    > Disable VK_EXT_rasterization_order_attachment_access on ARM
    >
    > Due to an unexplained memory regression. Once that is figured out, we
    > could enable it again.
    >
    > Bug: b/437907749
    > Change-Id: I2acb4133ebb8fe168238c1d61d144302a1726e31
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1036876
    > Reviewed-by: Noelle Scobie <nscobie@google.com>
    > Commit-Queue: Shahbaz Youssefi <syoussefi@google.com>
    
    Bug: b/437907749
    Change-Id: I7d972f1255588dc3399923138c68a90cdd2d571f
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1164536
    Commit-Queue: Sharadh Rajaraman <rsharadh@google.com>
    Reviewed-by: Noelle Scobie <nscobie@google.com>

--- src/gpu/graphite/vk/VulkanCaps.cpp
+++ src/gpu/graphite/vk/VulkanCaps.cpp
@@ -248,9 +248,7 @@ void VulkanCaps::init(const ContextOptions& contextOptions,
     }
 
     // Note: ARM GPUs have always been coherent, do not add a subpass self-dependency even if the
-    // application hasn't enabled this feature as it comes with a performance cost on this GPU. Use
-    // of VK_EXT_rasterization_order_attachment_access is disabled on ARM due to an unexplained
-    // memory regression (b/437907749).
+    // application hasn't enabled this feature as it comes with a performance cost on this GPU.
     //
     // Imagination GPUs are also coherent but only within the same sample when sample-shading.
     // VK_EXT_rasterization_order_attachment_access indicates coherence when input attachment read
@@ -258,7 +256,7 @@ void VulkanCaps::init(const ContextOptions& contextOptions,
     // this extension. This is not a problem for Graphite however, which does not enable sample
     // shading (nor would it read color from other samples even if it did).
     fSupportsRasterizationOrderColorAttachmentAccess =
-            enabledFeatures.fRasterizationOrderColorAttachmentAccess && vendorID != kARM_VkVendor;
+            enabledFeatures.fRasterizationOrderColorAttachmentAccess;
     fIsInputAttachmentReadCoherent = fSupportsRasterizationOrderColorAttachmentAccess ||
                                      vendorID == kARM_VkVendor || vendorID == kImagination_VkVendor;
 
