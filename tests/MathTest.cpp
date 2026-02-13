a73881b68051d4e95c25d6fd785f173f89b170ee
Michael Reed <mike@reedtribe.org>
2026-02-13 17:11:01 -0500

    Remove bespoke CLZ and CTZ implemenatations
    Change-Id: I3e380eb76f27981dfacf0bddfd0f4feae72244d7
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1163816
    Reviewed-by: Florin Malita <fmalita@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Mike Reed <mike@reedtribe.org>

--- tests/MathTest.cpp
+++ tests/MathTest.cpp
@@ -25,44 +25,6 @@
 #include <cstddef>
 #include <cstdint>
 
-static void test_clz(skiatest::Reporter* reporter) {
-    REPORTER_ASSERT(reporter, 32 == SkCLZ(0));
-    REPORTER_ASSERT(reporter, 31 == SkCLZ(1));
-    REPORTER_ASSERT(reporter, 1 == SkCLZ(1 << 30));
-    REPORTER_ASSERT(reporter, 1 == SkCLZ((1 << 30) | (1 << 24) | 1));
-    REPORTER_ASSERT(reporter, 0 == SkCLZ(~0U));
-
-    SkRandom rand;
-    for (int i = 0; i < 1000; ++i) {
-        uint32_t mask = rand.nextU();
-        // need to get some zeros for testing, but in some obscure way so the
-        // compiler won't "see" that, and work-around calling the functions.
-        mask >>= (mask & 31);
-        int intri = SkCLZ(mask);
-        int porta = SkCLZ_portable(mask);
-        REPORTER_ASSERT(reporter, intri == porta, "mask:%u intri:%d porta:%d", mask, intri, porta);
-    }
-}
-
-static void test_ctz(skiatest::Reporter* reporter) {
-    REPORTER_ASSERT(reporter, 32 == SkCTZ(0));
-    REPORTER_ASSERT(reporter, 0 == SkCTZ(1));
-    REPORTER_ASSERT(reporter, 30 == SkCTZ(1 << 30));
-    REPORTER_ASSERT(reporter, 2 == SkCTZ((1 << 30) | (1 << 24) | (1 << 2)));
-    REPORTER_ASSERT(reporter, 0 == SkCTZ(~0U));
-
-    SkRandom rand;
-    for (int i = 0; i < 1000; ++i) {
-        uint32_t mask = rand.nextU();
-        // need to get some zeros for testing, but in some obscure way so the
-        // compiler won't "see" that, and work-around calling the functions.
-        mask >>= (mask & 31);
-        int intri = SkCTZ(mask);
-        int porta = SkCTZ_portable(mask);
-        REPORTER_ASSERT(reporter, intri == porta, "mask:%u intri:%d porta:%d", mask, intri, porta);
-    }
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 
 static float sk_fsel(float pred, float result_ge, float result_lt) {
@@ -532,9 +494,6 @@ DEF_TEST(Math, reporter) {
 
     // disable for now
     if ((false)) test_blend31();  // avoid bit rot, suppress warning
-
-    test_clz(reporter);
-    test_ctz(reporter);
 }
 
 template <typename T> struct PairRec {
