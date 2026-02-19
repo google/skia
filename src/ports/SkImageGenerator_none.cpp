0ff35758edb78ee77372ae6dc148164ecdbd6294
Kaylee Lubick <kjlubick@google.com>
2026-02-19 10:24:56 -0500

    Remove some files that are no longer used
    4 of these have been empty for a while. GrBackendSurfacePriv.h
    was accidentally included in https://review.skia.org/1159676
    (src/gpu/ganesh/d3d/GrD3DBackendSurfacePriv.h was correct).
    
    Change-Id: I89be38ea3a5e10ed899cdf6e2431bfbbca6350de
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1168416
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>
    Reviewed-by: Jorge Betancourt <jmbetancourt@google.com>

--- src/ports/SkImageGenerator_none.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2015 The Android Open Source Project
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-// TODO(kjlubick) remove this empty file after updating Chromium
-
