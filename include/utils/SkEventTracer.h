a7665f11022cb12dc33a6f427e58052e4e99ebb8
Michael Ludwig <michaelludwig@google.com>
2026-03-11 10:52:37 -0400

    SkEventTracer::SetInstance always leaks
    Bug: b/487750530
    Change-Id: Id2c0dc3df560c0484d22e448b93b37e6c2e492c3
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1183796
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Florin Malita <fmalita@google.com>

--- include/utils/SkEventTracer.h
+++ include/utils/SkEventTracer.h
@@ -28,15 +28,13 @@ public:
     /**
      * If this is the first call to SetInstance or GetInstance then the passed instance is
      * installed and true is returned. Otherwise, false is returned. In either case ownership of the
-     * tracer is transferred and it will be deleted when no longer needed.
+     * tracer is transferred.
      *
-     * Not deleting the tracer on process exit should not cause problems as
-     * the whole heap is about to go away with the process. This can also
-     * improve performance by reducing the amount of work needed.
-     *
-     * @param leakTracer Do not delete tracer on process exit.
+     * On failure, the tracer is deleted; on success the tracer will be held until process exit, and
+     * will not have its destructor run. This avoids race conditions when using Skia from multiple
+     * threads in the midst of application teardown.
      */
-    static bool SetInstance(SkEventTracer*, bool leakTracer = false);
+    static bool SetInstance(SkEventTracer*);
 
     /**
      * Gets the event tracer. If this is the first call to SetInstance or GetIntance then a default
