98fc58b4a40b52a179d678e7c5b873e2fc01ce4a
Michael Ludwig <michaelludwig@google.com>
2026-03-19 08:57:00 -0700

    Revert "Fix VkTestUtils's VulkanPreferredFeatures usage"
    This reverts commit 72009acc508c4ddeee63fc8a60c53a232b907258.
    
    Reason for revert: several different failures on the tree:
    Failed vulkan call. Error: -8, skgpu::VulkanMemory::AllocImageMemory (isProtected:1, forceDedicatedMemory:1, useLazyAllocation:0)
     from https://logs.chromium.org/logs/skia/771f91706a413e11/+/steps/symbolized_dm/0/stdout
    and
    	../../../../../skia/tests/VkHardwareBufferTest.cpp:589	Function ptr for vkGetSemaphoreFdKHR could not be acquired
     from https://logs.chromium.org/logs/skia/771f94ac22d74111/+/steps/dm/0/stdout
    
    Original change's description:
    > Fix VkTestUtils's VulkanPreferredFeatures usage
    >
    > * This CL fixes extension mismanagement issues and includes updates for clarity and style modernization in the touched areas (verified by confirming that VK_KHR_driver_properties extension is included as expected after previously being inadvertently excluded).
    >
    > The issue:
    > * This is a follow-up to the landed change "Remove redundant or outdated extension usage in Vulkan tests" (https://skia-review.git.corp.google.com/c/skia/+/1174856), which exposed an issue with our usage of VulkanPreferredFeatures where we would pass in only the list of extensions the tests want to enable. Instead, we should be providing the list of all *available* extensions on the device.
    >
    > * This misuse, combined with TestUtils removing extension names that should be redundant (i.e. already checked for in VulkanPreferredFeatures), led to certain key extensions/features no longer being utilized. Notably, this included VK_KHR_driver_properties. This meant driver correctness workarounds could not be enabled, which also (likely artificially) led to perf improvements.
    >
    > Anticipated impact of this fix:
    > * This change is anticipated to trigger perf regressions from the current state. In reality, we should just be returning to baseline performance just before the extension removal CL landed.
    >
    > * Once this lands and we get perf numbers, I plan to determine whether actually not enabling certain extensions would get us performance wins without encountering driver correctness issues.
    >
    > Bug: b/487695539
    > Change-Id: Idef9ac665485a50b6bd032d5138855f359f7858d
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1181116
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Nicolette Prevost <nicolettep@google.com>
    
    Bug: b/487695539
    Change-Id: I03029e7cee5c0cc20b555f8874ace7b3d5fb6eb6
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1190516
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Nicolette Prevost <nicolettep@google.com>

--- tools/gpu/vk/VkTestUtils.h
+++ tools/gpu/vk/VkTestUtils.h
@@ -24,11 +24,12 @@ class VulkanExtensions;
 
 namespace sk_gpu_test {
     struct TestVkFeatures {
-        VkPhysicalDeviceFeatures2 deviceFeatures {};
+        VkPhysicalDeviceFeatures2 deviceFeatures;
 
-        // Structs from skiaFeatures may be chained into deviceFeatures and therefore must share the
-        // same lifetime.
+        // protectedMemoryFeatures and structs from skiaFeatures may be chained into deviceFeatures,
+        // so must share the same lifetime.
         skgpu::VulkanPreferredFeatures skiaFeatures;
+        VkPhysicalDeviceProtectedMemoryFeatures protectedMemoryFeatures;
     };
 
     bool LoadVkLibraryAndGetProcAddrFuncs(PFN_vkGetInstanceProcAddr*);
