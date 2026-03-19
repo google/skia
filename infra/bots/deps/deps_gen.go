7ac9f8d18035b3481950a82c891812330b56c513
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-03-18 21:01:22 -0700

    Roll ANGLE from f7571cf4c526 to aef03d88aba5 (4 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/f7571cf4c526..aef03d88aba5
    
    2026-03-19 syoussefi@chromium.org Add tricky sRGB decode vs SKIP_DECODE vs texelFetch tests
    2026-03-18 vikassoni@chromium.org [ANGLE]: Allow matching GL_BGRA_EXT variants for sub-image updates.
    2026-03-18 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from 459765ea146e to 313687770d81 (7 revisions)
    2026-03-18 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from a371ede85b90 to c8d5fee03227 (719 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC brettos@google.com,jmadill@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win11-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: brettos@google.com
    Change-Id: Ice148bb84ea1392ba7f969ea67de2080fcd140b8
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1189717
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -24,7 +24,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "f7571cf4c526b7a9cd0c88424ed6244fac1b55ac",
+		Version: "aef03d88aba59e8308976d10c856e7216ed8f6dc",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/deps/icu": {
