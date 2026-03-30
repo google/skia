dfd8f800280001167cd5a7fb83a69e9bed6197da
Kaylee Lubick <kjlubick@google.com>
2026-03-30 13:20:53 -0700

    Remove extraneous cases from SkImageShader.cpp
    Found these while reading the code. Not sure why they are there in the
    original CLs:
     - https://review.skia.org/231109
     - https://review.skia.org/121464
    
    Change-Id: I6fd7da4280f4ece68dc6702b8a2997d70e7b00cc
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1198401
    Auto-Submit: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: Max Hudnell <maxhudnell@google.com>
    Commit-Queue: Max Hudnell <maxhudnell@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>

--- src/shaders/SkImageShader.cpp
+++ src/shaders/SkImageShader.cpp
@@ -692,11 +692,10 @@ bool SkImageShader::appendStages(const SkStageRec& rec, const SkShaders::MatrixR
     // Check for fast-path stages.
     // TODO: Could we use the fast-path stages for each level when doing linear mipmap filtering?
     SkColorType ct = upper.pm.colorType();
-    if (true
-        && (ct == kRGBA_8888_SkColorType || ct == kBGRA_8888_SkColorType)
-        && !sampling.useCubic && sampling.filter == SkFilterMode::kLinear
-        && sampling.mipmap != SkMipmapMode::kLinear
-        && fTileModeX == SkTileMode::kClamp && fTileModeY == SkTileMode::kClamp) {
+    if ((ct == kRGBA_8888_SkColorType || ct == kBGRA_8888_SkColorType) &&
+        !sampling.useCubic && sampling.filter == SkFilterMode::kLinear &&
+        sampling.mipmap != SkMipmapMode::kLinear &&
+        fTileModeX == SkTileMode::kClamp && fTileModeY == SkTileMode::kClamp) {
         // Check bounding box of points we will sample to see if we can use lowp
         // and not over/under flow.
         bool shouldUseHighPBilerp = false;
@@ -724,10 +723,9 @@ bool SkImageShader::appendStages(const SkStageRec& rec, const SkShaders::MatrixR
         }
         return append_misc();
     }
-    if (true
-        && (ct == kRGBA_8888_SkColorType || ct == kBGRA_8888_SkColorType)
-        && sampling.useCubic
-        && fTileModeX == SkTileMode::kClamp && fTileModeY == SkTileMode::kClamp) {
+    if ((ct == kRGBA_8888_SkColorType || ct == kBGRA_8888_SkColorType) &&
+        sampling.useCubic &&
+        fTileModeX == SkTileMode::kClamp && fTileModeY == SkTileMode::kClamp) {
 
         p->append(SkRasterPipelineOp::bicubic_clamp_8888, upper.gather);
         if (ct == kBGRA_8888_SkColorType) {
