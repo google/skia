413fad7a0d8896491383cec7bd59718f5ec911f5
Jorge Betancourt <jmbetancourt@google.com>
2026-05-27 16:40:48 -0400

    Add SkLog implementation to SkSLMinify
    https://review.skia.org/1247176 changes SK_ABORT to invoke SKIA_LOG which expands into SkLog, while SK_ABORT used to expand into SkDebugf. This tool would define a custom implementation of SkDebugf, now it needs to define SkLog for itself.
    
    Change-Id: I21bbedf8635f237d6b27b0f333c5b5dab88c2a7b
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1247397
    Auto-Submit: Jorge Betancourt <jmbetancourt@google.com>
    Reviewed-by: Florin Malita <fmalita@google.com>
    Commit-Queue: Florin Malita <fmalita@google.com>

--- tools/sksl-minify/SkSLMinify.cpp
+++ tools/sksl-minify/SkSLMinify.cpp
@@ -40,6 +40,13 @@ void SkDebugf(const char format[], ...) {
     va_end(args);
 }
 
+void SkLog(SkLogPriority, const char format[], ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+}
+
 namespace SkOpts {
     size_t raster_pipeline_highp_stride = 1;
 }
