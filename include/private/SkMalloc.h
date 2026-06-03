fd7a0a9ea1fadf3644957730a5dea289bb64d125
Jorge Betancourt <jmbetancourt@google.com>
2026-06-03 14:31:13 -0400

    Add staging SkMalloc.h file to private includes
    Required for https://crrev.com/c/7896961
    
    Change-Id: Id22943b2a4890ab5b0e849fc6bc2eb8c7902b1be
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1253857
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>

---
+++ include/private/SkMalloc.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+ */
+
+// This serves only to temporarily stage Chrome changes as we move everything out of private/base.
+#include "include/private/base/SkMalloc.h"
