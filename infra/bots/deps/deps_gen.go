2c014dc494f1d7f0063ab1fd849d9292a0875a65
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-07-24 12:20:48 -0700

    Manual roll ANGLE from e316181208fd to 92de898b7a32 (10 revisions)
    Manual roll requested by robertphillips@google.com
    
    https://chromium.googlesource.com/angle/angle.git/+log/e316181208fd..92de898b7a32
    
    2026-07-24 syoussefi@chromium.org Fix YUV target validation of color attachment index
    2026-07-24 syoussefi@chromium.org Skip flaky no-storage-image-bound test more broadly
    2026-07-24 jisunnie.lee@samsung.com Add program binary blob compression/decompression
    2026-07-24 syoussefi@chromium.org Skip flaky test on Mac/Intel/Metal
    2026-07-24 cclao@google.com Vulkan: Properly handle tile memory allocation failure
    2026-07-24 syoussefi@chromium.org Skip flaky no-storage-image-bound test
    2026-07-24 syoussefi@chromium.org WebGPU: Disable tests on Mac/AMD
    2026-07-24 lehoangquyen@chromium.org Metal: Fix index buffer rewriting OOB write and rendering bug
    2026-07-24 angle-autoroll@skia-public.iam.gserviceaccount.com Roll vulkan-deps from afc9d8707b53 to 808860b25bf7 (4 revisions)
    2026-07-24 angle-autoroll@skia-public.iam.gserviceaccount.com Roll Chromium from f24f519928ff to d6a338e7de51 (793 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC jlavrova@google.com,jmadill@google.com,robertphillips@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win11-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: jlavrova@google.com,robertphillips@google.com
    Change-Id: I2b0852dedd91becca06de51e96dff6e2a69041cb
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1303656
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -29,7 +29,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "e316181208fdc735a7fcfbc7ac7ffb03044f06bd",
+		Version: "92de898b7a321801e1a83b950a68eba07e882f29",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/agents": {
