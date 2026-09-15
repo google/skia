531ba94da032cdfe9c161a8e954c34d510d9914c
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-14 23:09:32 -0700

    Roll vulkan-deps from d60dc547ea78 to ad9434a40d67 (10 revisions)
    https://chromium.googlesource.com/vulkan-deps.git/+log/d60dc547ea78..ad9434a40d67
    
    Also rolling transitive DEPS:
      https://skia.googlesource.com/external/github.com/KhronosGroup/SPIRV-Tools/+log/d6f63db7031c181c146963206da772445704d5a0..9abb194732777e849290dbd35e874bf3b91ba17e
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/vulkan-deps-skia-autoroll
    Please CC michaelludwig@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Test-Win11-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE
    Bug: None
    Tbr: michaelludwig@google.com
    Change-Id: I83355929874b4c4ce017afaf4935ac7220f33869
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1360961
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -164,7 +164,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/vulkan-deps": {
 		Id:      "chromium.googlesource.com/vulkan-deps",
-		Version: "d60dc547ea783f8053e51094b9f12ebd286323e4",
+		Version: "ad9434a40d674a3360dc321866ae6fa3fff2d47e",
 		Path:    "third_party/externals/vulkan-deps",
 	},
 	"chromium.googlesource.com/webm/libwebp": {
@@ -219,7 +219,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia.googlesource.com/external/github.com/KhronosGroup/SPIRV-Tools": {
 		Id:      "skia.googlesource.com/external/github.com/KhronosGroup/SPIRV-Tools",
-		Version: "d6f63db7031c181c146963206da772445704d5a0",
+		Version: "9abb194732777e849290dbd35e874bf3b91ba17e",
 		Path:    "third_party/externals/spirv-tools",
 	},
 	"skia.googlesource.com/external/github.com/emscripten-core/emsdk": {
