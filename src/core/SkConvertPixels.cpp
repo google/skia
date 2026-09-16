aa3f3abb0bcb86fb2f7269ebe7d8ed9ca5f485fb
Michael Ludwig <michaelludwig@google.com>
2026-09-16 14:22:17 -0400

    Remove unpremul SkOpts swizzler functions
    The portable versions were never used by SkConvertPixels, and
    from recent perf regressions switching to using the unpremul
    swizzler functions in TextureFormatXferFn instead of a
    load_8888, swap_rb, unpremul, store_8888 raster pipeline, it now
    appears that RP is faster even than the NEON code that was specialized
    in SkOpts.
    
    Let's remove code that is not utilized often and could easily bit rot.
    
    Bug: 323034773
    Bug: 560129844
    Change-Id: I1338195580068208f0683ea5b9ced0e8f597b043
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1363397
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>

--- src/core/SkConvertPixels.cpp
+++ src/core/SkConvertPixels.cpp
@@ -54,9 +54,7 @@ static bool swizzle_or_premul(const SkImageInfo& dstInfo,       void* dstPixels,
         !is_8888(srcInfo.colorType()) ||
         steps.fFlags.linearize         ||
         steps.fFlags.gamut_transform   ||
-#if !defined(SK_ARM_HAS_NEON)
         steps.fFlags.unpremul          ||
-#endif
         steps.fFlags.encode) {
         return false;
     }
@@ -68,9 +66,6 @@ static bool swizzle_or_premul(const SkImageInfo& dstInfo,       void* dstPixels,
     if (steps.fFlags.premul) {
         fn = swapRB ? SkOpts::RGBA_to_bgrA
                     : SkOpts::RGBA_to_rgbA;
-    } else if (steps.fFlags.unpremul) {
-        fn = swapRB ? SkOpts::rgbA_to_BGRA
-                    : SkOpts::rgbA_to_RGBA;
     } else {
         // If we're not swizzling, we ought to have used rect_memcpy().
         SkASSERT(swapRB);
