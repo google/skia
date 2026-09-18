5e4e4b904089e3d62f967217efda29a4abc6b76b
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-17 21:01:27 -0700

    Roll ANGLE from 5bafad4847ef to 7acd4fe29034 (8 revisions)
    https://chromium.googlesource.com/angle/angle.git/+log/5bafad4847ef..7acd4fe29034
    
    2026-09-18 syoussefi@chromium.org Vulkan: Don't use VK_KHR_vertex_attribute_divisor if insufficient
    2026-09-17 abdolrashidi@google.com Vulkan: Fix shader read stage mask updates
    2026-09-17 cclao@google.com Add ThreadSafeDisplayImpl for backend
    2026-09-17 zork@google.com EGL: Reset thread state on makeCurrent error
    2026-09-17 cclao@google.com Pack the initialized flag into ThreadSafeDisplay::mRefCount
    2026-09-17 bajones@chromium.org GL: Change mipmap workaround from flush to forcefullyFlush
    2026-09-17 cclao@google.com Restrict EGLSync API to only use ThreadSafeDisplay
    2026-09-17 syoussefi@chromium.org CL/Vulkan: Track memory using const refs
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/angle-skia-autoroll
    Please CC jmadill@google.com,michaelludwig@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE: https://bugs.chromium.org/p/angleproject/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Release-ANGLE;skia/skia.primary:Build-Mac-Clang-arm64-Release-ANGLE;skia/skia.primary:Test-Win11-Clang-Golo-GPU-QuadroP400-x86_64-Debug-All-ANGLE;skia/skia.primary:Test-Win11-Clang-Dell3930-GPU-GTX1660-x86_64-Debug-All-ANGLE
    Tbr: michaelludwig@google.com
    Test: Test: angle_end2end_tests --gtest_filter=EGLMultiContextTest.*
    Change-Id: I8cffe582ac1c32e5cf52547bc0f4f02f418f30a9
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1364999
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -29,7 +29,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"chromium.googlesource.com/angle/angle": {
 		Id:      "chromium.googlesource.com/angle/angle",
-		Version: "5bafad4847efc3f16df83a22371dfe93590a2fe4",
+		Version: "7acd4fe29034e15b2330a26ac03df8b6262c8e02",
 		Path:    "third_party/externals/angle2",
 	},
 	"chromium.googlesource.com/chromium/agents": {
